2a78b2a0a82069d404bf9ddfb0f5bbc90661fa4f
Florian Renaut <florian.renaut@corp.ovh.com>
2018-03-01 16:08:36 +0100

    feat(chore): bump angular-ui-router to version ^1.0.0.

--- packages/components/ng-at-internet-ui-router-plugin/dist/at-internet-ui-router-plugin.js
+++ packages/components/ng-at-internet-ui-router-plugin/dist/at-internet-ui-router-plugin.js
@@ -10,43 +10,43 @@
  */
 angular.module("atInternetUiRouterPlugin", ["ng-at-internet", "ui.router"])
 
-/**
- * @ngdoc service
- * @require ng-at-internet, ui.router
- * @name atInternetUiRouterPluginProvider
- * @description
- * Provider allowing configuration for atInternetUiRouterPlugin.
- */
+    /**
+     * @ngdoc service
+     * @require ng-at-internet, ui.router
+     * @name atInternetUiRouterPluginProvider
+     * @description
+     * Provider allowing configuration for atInternetUiRouterPlugin.
+     */
     .provider("atInternetUiRouterPlugin", function () {
 
         var stateTrackEnabled = false;
         var stateFilters = [];
 
-    /**
-     * @ngdoc function
-     * @name atInternetUiRouterPluginProvider.setTrackStateChange
-     * @methodOf atInternetUiRouterPluginProvider
-     * @param {Boolean} state True to enable automatic state tracking, false otherwise.
-     * @description
-     * Enable or disable automatic state tracking (sends a page tracking data on each state change).
-     */
+        /**
+         * @ngdoc function
+         * @name atInternetUiRouterPluginProvider.setTrackStateChange
+         * @methodOf atInternetUiRouterPluginProvider
+         * @param {Boolean} state True to enable automatic state tracking, false otherwise.
+         * @description
+         * Enable or disable automatic state tracking (sends a page tracking data on each state change).
+         */
         this.setTrackStateChange = function (state) {
             stateTrackEnabled = state;
         };
 
-    /**
-     * @ngdoc function
-     * @name atInternetUiRouterPluginProvider.addStateFilter
-     * @param {Function} callback Callback to apply to each state name before sending it.
-     * @description
-     * Apply the given callback function to each state name before sending it.
-     * Example :
-     * ```javascript
-     * atInternetUiRouterPluginProvider.addStateNameFilter = function (stateName) {
-     *     return stateName.replace("foo", "bar");
-     * }
-     * ```
-     */
+        /**
+         * @ngdoc function
+         * @name atInternetUiRouterPluginProvider.addStateFilter
+         * @param {Function} callback Callback to apply to each state name before sending it.
+         * @description
+         * Apply the given callback function to each state name before sending it.
+         * Example :
+         * ```javascript
+         * atInternetUiRouterPluginProvider.addStateNameFilter = function (stateName) {
+         *     return stateName.replace("foo", "bar");
+         * }
+         * ```
+         */
         this.addStateNameFilter = function (callback) {
             stateFilters.push(callback);
         };
@@ -54,64 +54,69 @@ angular.module("atInternetUiRouterPlugin", ["ng-at-internet", "ui.router"])
         this.$get = function () {
             return {
 
-            /**
-             * @ngdoc
-             * @name isStateTrackEnabled
-             * @methodOf atInternetUiRouterPlugin
-             * @description returns True if automatic state traking is enabled, false otherwise.
-             */
+                /**
+                 * @ngdoc
+                 * @name isStateTrackEnabled
+                 * @methodOf atInternetUiRouterPlugin
+                 * @description returns True if automatic state traking is enabled, false otherwise.
+                 */
                 isStateTrackEnabled: function () {
                     return stateTrackEnabled;
                 },
 
-            /**
-             * @ngdoc
-             * @name getStateFilters
-             * @methodOf atInternetUiRouterPlugin
-             * @description returns the list of state name filers.
-             */
+                /**
+                 * @ngdoc
+                 * @name getStateFilters
+                 * @methodOf atInternetUiRouterPlugin
+                 * @description returns the list of state name filers.
+                 */
                 getStateFilters: function () {
                     return stateFilters;
                 }
             };
         };
     })
-    .config(["$stateProvider", function ($stateProvider) {
+    .config(["$transitionsProvider", function ($transitionsProvider) {
 
-        $stateProvider.decorator("atInternetStateDecorator", function (state) {
-            state.resolve.atinternet = ["$injector", "atInternet", "atInternetUiRouterPlugin", function ($injector, atInternet, atInternetUiRouterPlugin) {
+        $transitionsProvider.onBefore({}, function (transition) {
+            transition.addResolvable({
+                token: "atInternetStateDecorator",
+                deps: ["$injector", "atInternet", "atInternetUiRouterPlugin"],
+                resolveFn: function ($injector, atInternet, atInternetUiRouterPlugin) {
+                    var state = transition.to();
+                    var options = state.atInternet;
+                    var ignore = options && options.ignore;
+                    var trackPage = {};
 
-                var options = state.self.atInternet;
-                var ignore = options && options.ignore;
-                var trackPage = {};
-
-                if (atInternetUiRouterPlugin.isStateTrackEnabled() && !ignore) {
-                    trackPage.name = state.self.name;
-                    if (options) {
-                        if (options.rename) {
-                            trackPage.name = options.rename;
-                            if (angular.isFunction(options.rename)) {
-                                trackPage.name = $injector.invoke(options.rename);
+                    if (atInternetUiRouterPlugin.isStateTrackEnabled() && !ignore) {
+                        trackPage.name = state.name;
+                        if (options) {
+                            if (options.rename) {
+                                trackPage.name = options.rename;
+                                if (angular.isFunction(options.rename)) {
+                                    trackPage.name = $injector.invoke(options.rename);
+                                }
+                            }
+                            if (options.level2) {
+                                trackPage.level2 = options.level2;
+                                if (angular.isFunction(options.level2)) {
+                                    trackPage.level2 = $injector.invoke(options.level2);
+                                }
                             }
                         }
-                        if (options.level2) {
-                            trackPage.level2 = options.level2;
-                            if (angular.isFunction(options.level2)) {
-                                trackPage.level2 = $injector.invoke(options.level2);
-                            }
-                        }
-                    }
 
-                // apply state filters if any
-                    _.forEach(atInternetUiRouterPlugin.getStateFilters(), function (filter) {
-                        if (_.isFunction(filter)) {
-                            trackPage.name = filter.apply(null, [trackPage.name]);
+                        // apply state filters if any
+                        _.forEach(atInternetUiRouterPlugin.getStateFilters(), function (filter) {
+                            if (_.isFunction(filter)) {
+                                trackPage.name = filter.apply(null, [trackPage.name]);
+                            }
+                        });
+                        if (trackPage.name) {
+                            atInternet.trackPage(trackPage);
                         }
-                    });
-                    if (trackPage.name) {
-                        atInternet.trackPage(trackPage);
                     }
                 }
-            }];
+            });
+            return transition;
         });
     }]);
--- packages/components/ng-at-internet-ui-router-plugin/docs/js/docs-setup.js
+++ packages/components/ng-at-internet-ui-router-plugin/docs/js/docs-setup.js
@@ -10,7 +10,8 @@ NG_DOCS={
       "type": "overview",
       "moduleName": "atInternetUiRouterPlugin",
       "shortDescription": "atInternetUiRouterPlugin",
-      "keywords": "at-internet atinternetuirouterplugin automatic docs enabled false filers getstatefilters isstatetrackenabled list overview plugin returns traking true ui-router"
+      "keywords": "at-internet atinternetuirouterplugin automatic docs enabled false filers getstatefilters isstatetrackenabled list overview plugin returns traking true ui-router",
+      "isDeprecated": false
     },
     {
       "section": "docs",
@@ -19,7 +20,8 @@ NG_DOCS={
       "type": "service",
       "moduleName": "atInternetUiRouterPluginProvider",
       "shortDescription": "Provider allowing configuration for atInternetUiRouterPlugin.",
-      "keywords": "allowing atinternetuirouterplugin atinternetuirouterpluginprovider automatic change configuration data disable docs enable false function ng-at-internet provider router service settrackstatechange tracking true ui"
+      "keywords": "allowing atinternetuirouterplugin atinternetuirouterpluginprovider automatic change configuration data disable docs enable false function ng-at-internet provider router service settrackstatechange tracking true ui",
+      "isDeprecated": false
     },
     {
       "section": "docs",
@@ -28,7 +30,8 @@ NG_DOCS={
       "type": "function",
       "moduleName": "atInternetUiRouterPluginProvider",
       "shortDescription": "Apply the given callback function to each state name before sending it.",
-      "keywords": "addstatefilter addstatenamefilter apply atinternetuirouterpluginprovider bar callback docs example foo function javascript replace return sending statename"
+      "keywords": "addstatefilter addstatenamefilter apply atinternetuirouterpluginprovider bar callback docs example foo function javascript replace return sending statename",
+      "isDeprecated": false
     }
   ],
   "apis": {
