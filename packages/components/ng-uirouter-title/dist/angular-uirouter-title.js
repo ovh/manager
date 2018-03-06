2bd7471ef2127e5e66858050ccdcfd479753c33d
Florian Renaut <florian.renaut@corp.ovh.com>
2018-03-06 13:59:23 +0100

    fix(injector): fix dependency injection when $title depends on another resolve.

--- packages/components/ng-uirouter-title/dist/angular-uirouter-title.js
+++ packages/components/ng-uirouter-title/dist/angular-uirouter-title.js
@@ -1,19 +1,20 @@
 "use strict";
 
 angular.module("angular.uirouter.title", [])
-.run(["$q", "$transitions", "$injector", function ($q, $transitions, $injector) {
+.run(["$transitions", function ($transitions) {
     var originalTitle;
     $transitions.onSuccess({}, function (transition) {
         var state = transition.to();
-        if (state.resolve && angular.isFunction(state.resolve.$title)) {
-            return $q.when($injector.invoke(state.resolve.$title)).then(function (title) {
+        transition.promise.finally(function () {
+            if (state.resolve && state.resolve.$title) {
+                var title = transition.injector().get("$title");
                 if (!originalTitle) {
                     originalTitle = document.title;
                 }
                 document.title = title;
-            });
-        } else {
-            document.title = originalTitle;
-        }
+            } else {
+                document.title = originalTitle;
+            }
+        });
     });
 }]);
