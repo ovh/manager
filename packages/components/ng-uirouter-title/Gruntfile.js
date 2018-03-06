6f1870a72994be1e69625de279c6280f408bc2d8
Joffrey Leveugle <joffrey.leveugle@corp.ovh.com>
2018-03-06 17:11:09 +0100

    chore(gruntfile): update release task

--- packages/components/ng-uirouter-title/Gruntfile.js
+++ packages/components/ng-uirouter-title/Gruntfile.js
@@ -145,14 +145,15 @@ module.exports = function (grunt) {
 
     grunt.registerTask('test', ['jshint', 'karma']);
 
-    /**
-     * --type=patch
-     * --type=minor
-     * --type=major
-     */
-    grunt.registerTask('release', 'Release', function () {
-        var type = grunt.option('type') || 'patch';
-        grunt.task.run(['test', 'bump-only:' + type, 'buildProd', 'bump-commit']);
-    });
+    // Increase version number. Type = minor|major|patch
+    grunt.registerTask("release", "Release", function () {
+        var type = grunt.option("type");
 
+        if (type && ~["patch", "minor", "major"].indexOf(type)) {
+            grunt.task.run(["bump-only:" + type]);
+        } else {
+            grunt.verbose.or.write("You try to release in a weird version type [" + type + "]").error();
+            grunt.fail.warn("Please try with --type=patch|minor|major");
+        }
+    });
 };
