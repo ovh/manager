9b749313c5e44ed247bd9a75714d9d8d6c4d25e4
Florian Renaut <florian.renaut@corp.ovh.com>
2018-03-06 14:46:31 +0100

    fix(title): avoid undefined title.

--- packages/components/ng-uirouter-title/dist/angular-uirouter-title.js
+++ packages/components/ng-uirouter-title/dist/angular-uirouter-title.js
@@ -2,19 +2,9 @@
 
 angular.module("angular.uirouter.title", [])
 .run(["$transitions", function ($transitions) {
-    var originalTitle;
     $transitions.onSuccess({}, function (transition) {
-        var state = transition.to();
         transition.promise.finally(function () {
-            if (state.resolve && state.resolve.$title) {
-                var title = transition.injector().get("$title");
-                if (!originalTitle) {
-                    originalTitle = document.title;
-                }
-                document.title = title;
-            } else {
-                document.title = originalTitle;
-            }
+            document.title = transition.injector().get("$title");
         });
     });
 }]);
