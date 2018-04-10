9bedfdc7ede048f3f605e7cd5a47cfecab11c9bc
CDS Agent <jean-christophe.alleman@corp.ovh.com>
2018-04-10 09:25:07 +0000

    Release v3.1.0

--- packages/components/ng-ovh-actions-menu/docs/js/docs-setup.js
+++ packages/components/ng-ovh-actions-menu/docs/js/docs-setup.js
@@ -10,7 +10,8 @@ NG_DOCS={
       "type": "overview",
       "moduleName": "actionsMenu",
       "shortDescription": "An actions menu gives the opportunity to group a set of actions available for a specific context under a single menu.",
-      "keywords": "_an actions actionsmenu animation confirmation context customizing docs group menu open opportunity overview set single specific todo translate width"
+      "keywords": "_an actions actionsmenu animation confirmation context customizing docs group menu open opportunity overview set single specific todo translate width",
+      "isDeprecated": false
     },
     {
       "section": "docs",
@@ -19,7 +20,8 @@ NG_DOCS={
       "type": "service",
       "moduleName": "actionsMenu",
       "shortDescription": "actionsMenuProvider allows developper to configure the path of the translation file.",
-      "keywords": "actionsmenu actionsmenuprovider allows angular change config configure configured default developper docs file location method module mymanagerapp path service set settranslationpath translation translations"
+      "keywords": "actionsmenu actionsmenuprovider allows angular change config configure configured default developper docs file location method module mymanagerapp path service set settranslationpath translation translations",
+      "isDeprecated": false
     },
     {
       "section": "docs",
@@ -28,7 +30,8 @@ NG_DOCS={
       "type": "directive",
       "moduleName": "actionsMenu",
       "shortDescription": "This is the main directive of the module. It&#39;s creating a popover with desired actions inside.",
-      "keywords": "$scope actions actions-menu-options actions-menu-popover-settings actionsmenu actionsmenuitem actionsoptions add angular beautiful bootstrap bottom-right button class controller creating data-actions-menu-options data-actions-menu-popover-settings desired directive displayed docs example factory feel filled-check filled-error free function github google href html http icon informations inside io isopen list main module my-custom-class my-font my-font-actions my-manager mymanagermodule mytestcontroller open options placement popover popover-class popover-placement popoversettings state1 supported title trigger view"
+      "keywords": "$scope actions actions-menu-options actions-menu-popover-settings actionsmenu actionsmenuitem actionsoptions add angular beautiful bootstrap bottom-right button class controller creating data-actions-menu-options data-actions-menu-popover-settings desired directive displayed docs example factory feel filled-check filled-error free function github google href html http icon informations inside io isopen list main module my-custom-class my-font my-font-actions my-manager mymanagermodule mytestcontroller open options placement popover popover-class popover-placement popoversettings state1 supported title trigger view",
+      "isDeprecated": false
     },
     {
       "section": "docs",
@@ -37,7 +40,8 @@ NG_DOCS={
       "type": "directive",
       "moduleName": "actionsMenu",
       "shortDescription": "This directive represent an item into an actions menu.",
-      "keywords": "action actionmenuitem actions actions-menu-item actions-menu-item-on-click actionsmenu callback called clicked directive docs function included instance item menu module offside parent represent scope"
+      "keywords": "action actionmenuitem actions actions-menu-item actions-menu-item-on-click actionsmenu callback called clicked directive docs function included instance item menu module offside parent represent scope",
+      "isDeprecated": false
     },
     {
       "section": "docs",
@@ -46,7 +50,8 @@ NG_DOCS={
       "type": "object",
       "moduleName": "actionsMenu",
       "shortDescription": "Factory that describe an actions menu.",
-      "keywords": "action actionmenu actionmenuitemoptions actions actionsmenu actionsmenuitem actionsmenuitems add addactionitem angular array beautiful controller created creating describe directive docs factory filled-check function google href http icon instance item items list menu method module mymanagerapp mytestctrl object options title var"
+      "keywords": "action actionmenu actionmenuitemoptions actions actionsmenu actionsmenuitem actionsmenuitems add addactionitem angular array beautiful controller created creating describe directive docs factory filled-check function google href http icon instance item items list menu method module mymanagerapp mytestctrl object options title var",
+      "isDeprecated": false
     },
     {
       "section": "docs",
@@ -55,7 +60,8 @@ NG_DOCS={
       "type": "object",
       "moduleName": "actionsMenu",
       "shortDescription": "Factory that describe an item into an actions menu.",
-      "keywords": "$sce action actionmenuitem actions actionsmenu actionsmenuitem add addsubaction anchor angular applied attribute beautiful called check class click clicked code controller created creating current defined describe details directive docs escaped event example external factory false filled-check full function getfullsref google hassubactions href html http icon instance item items level levels link manage manager menu method module mymanagerapp mytestctrl object offside onclick option options param2 params pass redirected representation return sref stateparams string subactionoptions subactions subitems svg tag target title true trustashtml var"
+      "keywords": "$sce action actionmenuitem actions actionsmenu actionsmenuitem add addsubaction anchor angular applied attribute beautiful called check class click clicked code controller created creating current defined describe details directive docs escaped event example external factory false filled-check full function getfullsref google hassubactions href html http icon instance item items level levels link manage manager menu method module mymanagerapp mytestctrl object offside onclick option options param2 params pass redirected representation return sref stateparams string subactionoptions subactions subitems svg tag target title true trustashtml var",
+      "isDeprecated": false
     },
     {
       "section": "docs",
@@ -64,7 +70,8 @@ NG_DOCS={
       "type": "service",
       "moduleName": "actionsMenu",
       "shortDescription": "This service enable you to load translation file.",
-      "keywords": "actionsmenu configured docs enable file load loaded method path service translation"
+      "keywords": "actionsmenu configured docs enable file load loaded method path service translation",
+      "isDeprecated": false
     }
   ],
   "apis": {
