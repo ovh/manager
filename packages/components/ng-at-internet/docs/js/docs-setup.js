6b41b48dfb12de40ceb16034d7ea87e161624cc6
CDS Agent <jfdurk@hotmail.fr>
2018-06-06 21:20:28 +0000

    Release v3.1.1

--- packages/components/ng-at-internet/docs/js/docs-setup.js
+++ packages/components/ng-at-internet/docs/js/docs-setup.js
@@ -10,7 +10,8 @@ NG_DOCS={
       "type": "service",
       "moduleName": "atInternet",
       "shortDescription": "Angular service wrapping the AtInternet tracking Javascript library.",
-      "keywords": "action amount angular associated atinternet atinternet-solutions automatically chapter1 chapter2 chapter3 click clickdata code configuration configure configured country countrycode currency currencycode current custom customer customobject data data-structure def default defaults disable docs download enable eu event eventdata events exit false free function generated getconfig hit hits http identified identifier included informations javascript js level2 library main manager navigation objective order orderid pagedata price pricetaxfree product productdata project promise provide provider quantity resolved returns send sending service setdefaults setdefaultspromise setenabled setted simplified smarttag status sub-section sub-sub-section supplied tax trackclick trackevent tracking trackorder trackpage true type unique validated values visitor visitorid warning wrapping your-action your-page your-product"
+      "keywords": "action amount angular associated atinternet atinternet-solutions automatically chapter1 chapter2 chapter3 click clickdata code configuration configure configured country countrycode currency currencycode current custom customer customobject data data-structure def default defaults disable docs download enable eu event eventdata events exit false free function generated getconfig hit hits http identified identifier included informations javascript js level2 library main manager navigation objective order orderid pagedata price pricetaxfree product productdata project promise provide provider quantity resolved returns send sending service setdefaults setdefaultspromise setenabled setted simplified smarttag status sub-section sub-sub-section supplied tax trackclick trackevent tracking trackorder trackpage true type unique validated values visitor visitorid warning wrapping your-action your-page your-product",
+      "isDeprecated": false
     },
     {
       "section": "docs",
@@ -19,7 +20,8 @@ NG_DOCS={
       "type": "directive",
       "moduleName": "atInternetClick",
       "shortDescription": "Simple attribute directive to track clicks on DOM elements.",
-      "keywords": "at-internet-click atinternet atinternetclick atinternetprovider attribute clickme clicks directive docs dom elements example foo html ng-click object replaced simple track trackclick trackon"
+      "keywords": "at-internet-click atinternet atinternetclick atinternetprovider attribute clickme clicks directive docs dom elements example foo html ng-click object replaced simple track trackclick trackon",
+      "isDeprecated": true
     },
     {
       "section": "docs",
@@ -28,7 +30,8 @@ NG_DOCS={
       "type": "service",
       "moduleName": "atInternetProvider",
       "shortDescription": "Provider allowing configuration for atInternet service.",
-      "keywords": "allowing atinternet atinternetprovider check configuration configure console data def default disable docs enable false function getdefaults getdefaultspromise isdefaultset javascript js library logging logs method promise provider retrieve service set setdebug setdefaults setdefaultspromise setenabled setted smarttag tracking true values"
+      "keywords": "allowing atinternet atinternetprovider check configuration configure console data def default disable docs enable false function getdefaults getdefaultspromise isdefaultset javascript js library logging logs method promise provider retrieve service set setdebug setdefaults setdefaultspromise setenabled setted smarttag tracking true values",
+      "isDeprecated": false
     },
     {
       "section": "docs",
@@ -37,7 +40,8 @@ NG_DOCS={
       "type": "directive",
       "moduleName": "trackOn",
       "shortDescription": "Simple attribute directive to track events on DOM elements.",
-      "keywords": "atinternetprovider attribute click data-track-name data-track-on data-track-type directive docs dom elements events example html myaction navigation simple track trackon"
+      "keywords": "atinternetprovider attribute click data-track-name data-track-on data-track-type directive docs dom elements events example html myaction navigation simple track trackon",
+      "isDeprecated": false
     }
   ],
   "apis": {
