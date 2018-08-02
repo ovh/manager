dc5a9ba73e63952486200fe5768aae21133fe574
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-08-02 16:51:47 +0200

    fix: remove jshint

--- packages/components/ng-ovh-jquery-ui-draggable/Gruntfile.js
+++ packages/components/ng-ovh-jquery-ui-draggable/Gruntfile.js
@@ -126,19 +126,6 @@ module.exports = function (grunt) {
             }
         },
 
-        // JS Check
-        jshint: {
-            options: {
-                jshintrc: ".jshintrc",
-                reporter: require("jshint-stylish")
-            },
-            js: [
-                "<%= srcdir %>/*.js",
-                "<%= srcdir %>/*/*.js",
-                "!<%= srcdir %>/**/*.spec.js"
-            ]
-        },
-
         // Check complexity
         complexity: {
             generic: {
@@ -203,7 +190,6 @@ module.exports = function (grunt) {
     grunt.registerTask("test", function () {
         grunt.task.run([
             "clean",
-            "jshint",
             "eslint",
             "complexity",
             "karma"
