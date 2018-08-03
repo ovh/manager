6fec5cf0e2f582a2af11d9e4a7e0fdbbc10a5178
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-08-03 08:36:22 +0200

    fix: remove jshint

--- packages/components/ng-ovh-toaster/Gruntfile.js
+++ packages/components/ng-ovh-toaster/Gruntfile.js
@@ -141,19 +141,6 @@ module.exports = function (grunt) {
             }
         },
 
-        // JS Check
-        jshint: {
-            options: {
-                jshintrc: ".jshintrc",
-                reporter: require("jshint-stylish")
-            },
-            js: [
-                "<%= srcdir %>/*.js",
-                "<%= srcdir %>/*/*.js",
-                "!<%= srcdir %>/**/*.spec.js"
-            ]
-        },
-
         // Check complexity
         complexity: {
             generic: {
@@ -218,7 +205,6 @@ module.exports = function (grunt) {
     grunt.registerTask("test", function () {
         grunt.task.run([
             "clean",
-            "jshint",
             "eslint",
             "complexity",
             "karma"
