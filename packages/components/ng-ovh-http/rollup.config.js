3c7081ca9dae515e712a95648f356754187a623d
Florian Renaut <florian.renaut@corp.ovh.com>
2018-11-20 11:26:08 +0100

    build: upgrade component-rollup-config

--- packages/components/ng-ovh-http/rollup.config.js
+++ packages/components/ng-ovh-http/rollup.config.js
@@ -1,5 +1,16 @@
-import config from '@ovh-ux/component-rollup-config';
+import rollupConfig from '@ovh-ux/component-rollup-config';
 
-export default config({
+const config = rollupConfig({
   input: 'src/ovh-angular-http.js',
 });
+
+export default [
+  config.cjs(),
+  config.umd({
+    output: {
+      globals: {
+        angular: 'angular',
+      },
+    },
+  }),
+];
