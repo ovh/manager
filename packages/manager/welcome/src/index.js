1db663b54a330195ae5abd7e2f74fa024ce55770
jleveugle <joffrey.leveugle@corp.ovh.com>
2018-12-05 12:24:19 +0100

    fix(welcome): remove @ovh-ux/manager-core (not used)

--- packages/manager/welcome/src/index.js
+++ packages/manager/welcome/src/index.js
@@ -1,7 +1,6 @@
 import angular from 'angular';
 import '@uirouter/angularjs';
 import ocLazyLoad from 'oclazyload';
-import '@ovh-ux/manager-core';
 
 import routing from './manager-welcome.routes';
 
@@ -10,7 +9,6 @@ const moduleName = 'ovhManagerWelcome';
 angular.module(moduleName, [
   ocLazyLoad,
   'ui.router',
-  'ovhManagerCore',
 ]).config(routing);
 
 export default moduleName;
