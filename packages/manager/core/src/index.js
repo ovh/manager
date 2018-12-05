d9b4e2fec49cccdf373ad58b71e6980936a83381
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2018-12-05 11:47:30 +0100

    feat(core): add translation configuration
    add angular-dynamic-locale^0.1.37
    add angular-translate-loader-pluggable@^1.3.1

--- packages/manager/core/src/index.js
+++ packages/manager/core/src/index.js
@@ -1,13 +1,13 @@
 import angular from 'angular';
-import 'angular-translate';
+import _ from 'lodash';
 import translateAsyncLoader from '@ovh-ux/translate-async-loader';
 
-import 'ng-at-internet';
+import 'angular-dynamic-locale';
+import 'angular-translate';
+import 'angular-translate-loader-pluggable';
 import 'ovh-api-services';
 
-import translateLoaderPluggable from 'angular-translate-loader-pluggable';
 import translateServiceProvider from './translate/translate.service';
-
 import sessionService from './session/session.service';
 
 import {
@@ -18,10 +18,10 @@ const moduleName = 'ovhManagerCore';
 
 angular
   .module(moduleName, [
+    'angular-translate-loader-pluggable',
     'ovh-api-services',
     'pascalprecht.translate',
-    translateLoaderPluggable.name,
-    'ng-at-internet',
+    'tmh.dynamicLocale',
     translateAsyncLoader,
   ])
   .constant('constants', {})
@@ -42,10 +42,80 @@ angular
 
     translatePluggableLoaderProvider.useLoader('asyncLoader');
 
+    $translateProvider.useLoaderCache(true);
+    $translateProvider.useSanitizeValueStrategy('sanitizeParameters');
+    $translateProvider.useMissingTranslationHandler('translateMissingTranslationHandler');
+
     $translateProvider.preferredLanguage(defaultLanguage);
     $translateProvider.use(defaultLanguage);
     $translateProvider.fallbackLanguage(LANGUAGES.fallback);
   })
-  .service('SessionService', sessionService);
+  .service('SessionService', sessionService)
+  // Fix security issue: https://github.com/angular-translate/angular-translate/issues/1418
+  .factory('translateMissingTranslationHandler', $sanitize => translationId => $sanitize(translationId))
+  .run((tmhDynamicLocaleCache, tmhDynamicLocale, TranslateService) => {
+    const injectAngularLocale = lang => tmhDynamicLocaleCache.put(lang, angular.injector(['ngLocale']).get('$locale'));
+    const defaultLanguage = TranslateService.getUserLocale();
+    const angularLocale = _.kebabCase(defaultLanguage);
+
+    let angularLocalePromise;
+    switch (angularLocale) {
+      case 'cs-cz':
+        angularLocalePromise = import('angular-i18n/angular-locale_cs-cz.js');
+        break;
+      case 'de-de':
+        angularLocalePromise = import('angular-i18n/angular-locale_de-de.js');
+        break;
+      case 'en-gb':
+      case 'en-asia':
+        angularLocalePromise = import('angular-i18n/angular-locale_en-gb.js');
+        break;
+      case 'en-ca':
+        angularLocalePromise = import('angular-i18n/angular-locale_en-ca.js');
+        break;
+      case 'en-us':
+        angularLocalePromise = import('angular-i18n/angular-locale_en-us.js');
+        break;
+      case 'en-au':
+        angularLocalePromise = import('angular-i18n/angular-locale_en-au.js');
+        break;
+      case 'en-sg':
+        angularLocalePromise = import('angular-i18n/angular-locale_en-sg.js');
+        break;
+      case 'es-es':
+        angularLocalePromise = import('angular-i18n/angular-locale_es-es.js');
+        break;
+      case 'fi-fi':
+        angularLocalePromise = import('angular-i18n/angular-locale_fi-fi.js');
+        break;
+      case 'fr-ca':
+        angularLocalePromise = import('angular-i18n/angular-locale_fr-ca.js');
+        break;
+      case 'it-it':
+        angularLocalePromise = import('angular-i18n/angular-locale_it-it.js');
+        break;
+      case 'lt-lt':
+        angularLocalePromise = import('angular-i18n/angular-locale_lt-lt.js');
+        break;
+      case 'nl-nl':
+        angularLocalePromise = import('angular-i18n/angular-locale_nl-nl.js');
+        break;
+      case 'pl-pl':
+        angularLocalePromise = import('angular-i18n/angular-locale_pl-pl.js');
+        break;
+      case 'pt-pt':
+        angularLocalePromise = import('angular-i18n/angular-locale_pt-pt.js');
+        break;
+      case 'fr-fr':
+      default:
+        angularLocalePromise = import('angular-i18n/angular-locale_fr-fr.js');
+        break;
+    }
+
+    angularLocalePromise
+      .then(() => injectAngularLocale(angularLocale))
+      .then(() => tmhDynamicLocale.set(angularLocale));
+  });
+
 
 export default moduleName;
