440fbc539746082fc4fd9e948d479f9f8ac5b525
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2018-12-10 14:33:02 +0100

    fix: update ng-uirouter-title usage
    replace angular-uirouter-title by @ovh-ux/ng-uirouter-title@^2.0.0-beta.2

--- packages/manager/freefax/src/freefax.component.js
+++ packages/manager/freefax/src/freefax.component.js
@@ -3,7 +3,7 @@ import angular from 'angular';
 
 import '@ovh-ux/manager-core';
 import '@ovh-ux/telecom-universe-components';
-import '@ovh-ux/angular-uirouter-title';
+import '@ovh-ux/ng-uirouter-title';
 import 'ovh-angular-ui-confirm-modal';
 import 'ovh-api-services';
 
@@ -26,7 +26,7 @@ export default angular
     'telecomUniverseComponents',
     'ui.router',
     'ovh-angular-ui-confirm-modal',
-    'angular.uirouter.title',
+    'ngUirouterTitle',
     credit,
     faxConfiguration,
     notifications,
