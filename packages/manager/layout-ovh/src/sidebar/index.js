266e605f9494a6c47db5cb6be4b8d585215a92c1
jleveugle <joffrey.leveugle@corp.ovh.com>
2018-12-11 16:52:09 +0100

    fix(i18n): load translations with annotations instead of import

--- packages/manager/layout-ovh/src/sidebar/index.js
+++ packages/manager/layout-ovh/src/sidebar/index.js
@@ -16,13 +16,6 @@ angular
   })
   .constant('SIDEBAR_CONFIG', SIDEBAR_CONFIG)
   .constant('SIDEBAR_STATE_MAPPING_SERVICE', STATE_MAPPING_SERVICE)
-  .run(($translate, asyncLoader) => {
-    asyncLoader.addTranslations(
-      import(`./translations/Messages_${$translate.use()}.xml`)
-        .catch(() => import(`./translations/Messages_${$translate.fallbackLanguage()}.xml`))
-        .then(x => x.default),
-    );
-    $translate.refresh();
-  });
+  .run(/* @ngTranslationsInject ./translations */);
 
 export default moduleName;
