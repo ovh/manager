b7f54b7f39571f8102c80aeeef96c0e122d19c7a
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2018-12-12 09:24:18 +0100

    fix: use initial title if $title is not defined
    add lodash@4.17.11 as dependency

--- packages/components/ng-uirouter-title/src/ng-uirouter-title.js
+++ packages/components/ng-uirouter-title/src/ng-uirouter-title.js
@@ -1,15 +1,33 @@
 import angular from 'angular';
 import '@uirouter/angularjs';
+import find from 'lodash/find';
+import get from 'lodash/get';
+import has from 'lodash/has';
 
 const moduleName = 'ngUirouterTitle';
 angular
   .module(moduleName, [
     'ui.router',
   ])
-  .run(/* ngInject */ ($transitions) => {
+  .run(/* ngInject */($transitions) => {
+    const initialTitle = document.title;
+
     $transitions.onSuccess({}, (transition) => {
+      const getStateTitle = (state) => {
+        const stateTitle = find(
+          get(state, 'resolvables'),
+          resolvable => resolvable.token === '$title',
+        );
+        if (!stateTitle && has(state, 'parent')) {
+          return getStateTitle(get(state, 'parent'));
+        }
+        return stateTitle;
+      };
+
+      const stateTitle = getStateTitle(transition.$to());
+
       transition.promise.finally(() => {
-        document.title = transition.injector().get('$title');
+        document.title = stateTitle ? transition.injector().get('$title') : initialTitle;
       });
     });
   });
