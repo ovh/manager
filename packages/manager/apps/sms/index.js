ec5ae22bbf7963366099450b6ad0fcdf4dd449f0
jleveugle <joffrey.leveugle@corp.ovh.com>
2019-01-04 17:39:24 +0100

    fix: sort import to fix undefined lodash

--- packages/manager/apps/sms/index.js
+++ packages/manager/apps/sms/index.js
@@ -1,6 +1,6 @@
-import '@ovh-ux/manager-sms';
 import 'script-loader!jquery'; // eslint-disable-line
 import 'script-loader!lodash'; // eslint-disable-line
+import '@ovh-ux/manager-sms';
 
 import angular from 'angular';
 
