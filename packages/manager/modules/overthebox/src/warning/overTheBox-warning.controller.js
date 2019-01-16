c1bf19bb41e042c7461141f1ae6f1458d9b616da
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-01-16 14:59:16 +0100

    fix(overthebox): add warning module

--- packages/manager/modules/overthebox/src/warning/overTheBox-warning.controller.js
+++ packages/manager/modules/overthebox/src/warning/overTheBox-warning.controller.js
@@ -1,13 +1,9 @@
-import angular from 'angular';
-
-const moduleName = 'ovhManagerOtbWarning';
-
-angular.module(moduleName, [])
-  .controller('OrderOverTheBoxWarningCtrl', function orderOverTheBoxWarningCtrl(CORE_URLS, CORE_REDIRECT_URLS) {
+export default class {
+  /* @ngInject */
+  constructor(CORE_URLS, CORE_REDIRECT_URLS) {
     this.overTheBoxManager = CORE_URLS.overTheBoxManager;
     this.guide = CORE_URLS.guides.overTheBox;
     this.home = CORE_URLS.guides.home;
     this.paymentMeans = CORE_REDIRECT_URLS.paymentMeans;
-  });
-
-export default moduleName;
+  }
+}
