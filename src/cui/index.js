c68421daee6697ca81bd2faae4e6641825d21da7
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-02-15 17:00:27 +0100

    feat(cui-guide): add cui guide component
    * add `ovh-ui-kit@^2.24.2` dependency
    * ignore `viewBox` attribute in htmlhint `attr-lowercase` rule
    
    ref: OM-304

--- src/cui/index.js
+++ src/cui/index.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 
 import dropdownMenu from './dropdown-menu';
+import guideComponent from './guide-component';
 import message from './message';
 import tabs from './tabs';
 
@@ -9,6 +10,7 @@ const moduleName = 'ngOvhCloudUniverseComponentsCui';
 angular
   .module(moduleName, [
     dropdownMenu,
+    guideComponent,
     message,
     tabs,
   ]);
