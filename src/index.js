67e0b4ea0a51c2e32650e3e2347071442e317525
Cyrille Bourgois <cyrille.bourgois@gmail.com>
2019-02-21 14:44:15 +0100

    feat(directives): add directives (#10)
    * add `cucAutofocus` directive
    * add `cucAutoselect` directive
    * add `cucClickEnterOnKeypress` directive
    * add `cucSmoothScrollHere` directive
    
    ref: OM-314

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 import angular from 'angular';
 
 import cui from './cui';
+import cucAutofocus from './autofocus';
+import cucAutoSelect from './autoselect';
+import cucClickEnterOnKeypress from './clickEnterOnKeypress';
 import cucMessage from './message';
 import cucNavigation from './navigation';
 import cucPoll from './poll';
 import cucProducts from './products';
+import cucSmoothScrollHere from './smoothScrollHere';
 
 const moduleName = 'ngOvhCloudUniverseComponents';
 
 angular
   .module(moduleName, [
     cui,
+    cucAutofocus,
+    cucAutoSelect,
+    cucClickEnterOnKeypress,
     cucMessage,
     cucNavigation,
     cucPoll,
     cucProducts,
+    cucSmoothScrollHere,
   ]);
 
 export default moduleName;
