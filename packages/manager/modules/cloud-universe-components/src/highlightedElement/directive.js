0bf7c500f4a25c0504677cce0aa0126caf33fb26
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-02-26 21:16:42 +0100

    feat(cloud-universe-components): add highlighted element
    ref: OM-318

---
+++ packages/manager/modules/cloud-universe-components/src/highlightedElement/directive.js
@@ -0,0 +1,38 @@
+import angular from 'angular';
+
+export default () => ({
+  restrict: 'A',
+  link($scope, $elem, $attrs) {
+    let ids = [];
+
+    $attrs.$observe('cucHighlightedElement', (_ids) => {
+      ids = _ids ? _ids.split(',') : [];
+    });
+
+    $scope.$on('cuc-highlighted-element.show', (e, _ids) => {
+      if (!_ids || !ids.length) {
+        $elem.addClass('cuc-highlighted-element-active');
+      } else {
+        _ids = _ids.split(','); // eslint-disable-line
+        angular.forEach(_ids, (item) => {
+          if (~ids.indexOf(item.trim())) {
+            $elem.addClass('cuc-highlighted-element-active');
+          }
+        });
+      }
+    });
+
+    $scope.$on('cuc-highlighted-element.hide', (e, _ids) => {
+      if (!_ids || !ids.length) {
+        $elem.removeClass('cuc-highlighted-element-active');
+      } else {
+        _ids = _ids.split(','); // eslint-disable-line
+        angular.forEach(_ids, (item) => {
+          if (~ids.indexOf(item.trim())) {
+            $elem.removeClass('cuc-highlighted-element-active');
+          }
+        });
+      }
+    });
+  },
+});
