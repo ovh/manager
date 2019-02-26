98be7e75a3d1257fed2a3ff00ebf8d08c1c1cee1
Florian Renaut <florian.renaut@corp.ovh.com>
2019-02-26 13:27:40 +0100

    fix(vrack): add trads, add ovh-ui-kit

--- packages/manager/modules/vrack/src/add/index.js
+++ packages/manager/modules/vrack/src/add/index.js
@@ -19,7 +19,7 @@ angular
       controller,
       controllerAs: 'VrackAddCtrl',
       translations: {
-        value: ['../../common', '..', '.'],
+        value: ['..', '.'],
         format: 'json',
       },
     });
