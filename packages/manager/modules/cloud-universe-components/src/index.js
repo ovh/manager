c51b8615629af1728bd2c3825ab3546f7868752e
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-02-27 21:38:01 +0100

    feat(cloud-universe-components): add cloud components
    * add consumption chart directive
    * add monitoring chart directive
    * add region service
    * add space mater directive
    * add user pref service
    
    ref: OM-316

--- packages/manager/modules/cloud-universe-components/src/index.js
+++ packages/manager/modules/cloud-universe-components/src/index.js
@@ -4,14 +4,19 @@ import cucAutofocus from './autofocus';
 import cucAutoSelect from './autoselect';
 import cucClickEnterOnKeypress from './clickEnterOnKeypress';
 import cucConfig from './config';
+import cucConsumption from './consumption';
 import cucFeatureAvailability from './featureAvailability';
 import cucHelper from './helper';
 import cucMessage from './message';
+import cucMonitoring from './monitoring';
 import cucNavigation from './navigation';
 import cucOrderedHash from './orderedHash';
 import cucPoll from './poll';
+import cucRegion from './region';
 import cucProducts from './products';
 import cucSmoothScrollHere from './smoothScrollHere';
+import cucSpaceMeter from './space-meter';
+import cucUserPref from './user-pref';
 import cucVrack from './vrack';
 import cui from './cui';
 
@@ -24,14 +29,19 @@ angular
     cucAutoSelect,
     cucClickEnterOnKeypress,
     cucConfig,
+    cucConsumption,
     cucFeatureAvailability,
     cucHelper,
     cucMessage,
+    cucMonitoring,
     cucNavigation,
     cucOrderedHash,
+    cucRegion,
     cucPoll,
     cucProducts,
     cucSmoothScrollHere,
+    cucSpaceMeter,
+    cucUserPref,
     cucVrack,
     cui,
   ]);
