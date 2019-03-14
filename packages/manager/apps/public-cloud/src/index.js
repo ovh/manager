2af687d9150251f32701585c041df8fb773920da
jleveugle <joffrey.leveugle@corp.ovh.com>
2019-03-14 17:22:47 +0100

    feat: init public cloud app

---
+++ packages/manager/apps/public-cloud/src/index.js
@@ -0,0 +1,28 @@
+import 'script-loader!jquery'; // eslint-disable-line
+import 'script-loader!lodash'; // eslint-disable-line
+
+import angular from 'angular';
+import core from '@ovh-ux/manager-core';
+import ngOvhApiWrappers from '@ovh-ux/ng-ovh-api-wrappers';
+
+import 'ovh-ui-kit/dist/oui.css';
+import 'ovh-ui-kit-bs/dist/ovh-ui-kit-bs.css';
+
+import navbar from './navbar';
+import sidebar from './sidebar';
+
+import './index.scss';
+
+import controller from './index.controller';
+import service from './index.service';
+
+angular
+  .module('ovhStack', [
+    core,
+    navbar,
+    ngOvhApiWrappers,
+    sidebar,
+  ])
+  .controller('PublicCloudController', controller)
+  .service('publicCloud', service)
+  .run(/* @ngTranslationsInject:json ./translations */);
