2e79dd109e0dbf49929d89c1b6161f3f3dd35da2
jleveugle <joffrey.leveugle@corp.ovh.com>
2019-03-18 14:36:16 +0100

    fix: add data-, ngInject and strict-di

--- packages/manager/apps/public-cloud/src/index.service.js
+++ packages/manager/apps/public-cloud/src/index.service.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 
-export default /* @ngInject */ class PublicCloud {
+export default class PublicCloud {
+  /* @ngInject */
   constructor(iceberg) {
     this.iceberg = iceberg;
   }
@@ -10,7 +11,7 @@ export default /* @ngInject */ class PublicCloud {
       .iceberg('/cloud/project')
       .query()
       .expand('CachedObjectList-Cursor')
-      .sort('description') // Doesn't work as long as cache is not enaled
+      .sort('description') // Doesn't work as long as cache is not enabled
       .execute()
       .$promise
       .then(projects => _.sortBy(projects, 'description')); // Fallback
