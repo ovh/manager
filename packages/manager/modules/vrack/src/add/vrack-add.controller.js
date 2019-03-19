da0f4240870cef8681ea2086ca92a85dd2967beb
Florian Renaut <florian.renaut@corp.ovh.com>
2019-03-19 16:42:34 +0100

    feat(vrack): add vrack module & app

---
+++ packages/manager/modules/vrack/src/add/vrack-add.controller.js
@@ -0,0 +1,76 @@
+import pick from 'lodash/pick';
+
+export default /* @ngInject */ function (
+  $q,
+  $translate,
+  $state,
+  $rootScope,
+  CucCloudMessage,
+  VrackService,
+  OvhApiOrder,
+  TARGET,
+) {
+  const self = this;
+
+  self.TARGET = TARGET;
+
+  this.loaders = {
+    loading: false,
+    validationPending: false,
+  };
+
+  this.model = {
+    agreements: [],
+    contractsAccepted: false,
+    purchaseOrderUrl: '',
+  };
+
+  this.getVrackContract = function getVrackContract() {
+    return OvhApiOrder.Vrack().New().v6().get({
+      quantity: 1,
+    }).$promise.then((data) => {
+      self.model.agreements = data.contracts;
+    }).catch((error) => {
+      CucCloudMessage.error($translate.instant('vrack_error_reason', { message: error.data.message }));
+    });
+  };
+
+  this.addVrack = function addVrack() {
+    self.loaders.loading = true;
+    return OvhApiOrder.Vrack().New().v6().create({
+      quantity: this.model.quantityToOrder,
+    }, {}).$promise.then((data) => {
+      CucCloudMessage.success($translate.instant('vrack_adding_success', { data: pick(data, ['url', 'orderId']) }));
+      self.model.purchaseOrderUrl = data.url;
+      self.loaders.validationPending = true;
+    }).catch((error) => {
+      CucCloudMessage.error($translate.instant('vrack_error_reason', { message: error.data.message }));
+    }).finally(() => {
+      self.loaders.loading = false;
+    });
+  };
+
+  function init() {
+    self.loaders.loading = true;
+    self.vrackOrderUrl = null;
+
+    const promise = {
+      vrackOrderUrl: VrackService.getOrderUrl(),
+    };
+
+    if (self.TARGET !== 'US') {
+      promise.vrackContract = self.getVrackContract();
+    }
+
+    return $q
+      .all(promise)
+      .then((results) => {
+        self.vrackOrderUrl = results.vrackOrderUrl;
+      })
+      .finally(() => {
+        self.loaders.loading = false;
+      });
+  }
+
+  init();
+}
