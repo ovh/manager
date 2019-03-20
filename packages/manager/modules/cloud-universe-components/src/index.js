1e343b9ad73a23bbb6fd24e221240d1f10bb6fce
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-03-20 14:56:30 +0100

    feat(cloud-univer-components): add services and filter
    * add `cucBytes` filter
    * add `CucPriceHelper` service
    * add `CucFlavorService` service

--- packages/manager/modules/cloud-universe-components/src/index.js
+++ packages/manager/modules/cloud-universe-components/src/index.js
@@ -2,11 +2,13 @@ import angular from 'angular';
 
 import cucAutofocus from './autofocus';
 import cucAutoSelect from './autoselect';
+import cucBytes from './bytes';
 import cucClickEnterOnKeypress from './clickEnterOnKeypress';
 import cucConfig from './config';
 import cucConsumption from './consumption';
 import cucCurrency from './currency';
 import cucFeatureAvailability from './featureAvailability';
+import cucFlavor from './flavor';
 import cucHelper from './helper';
 import cucHighlightedElement from './highlightedElement';
 import cucMessage from './message';
@@ -14,6 +16,7 @@ import cucMonitoring from './monitoring';
 import cucNavigation from './navigation';
 import cucOrderedHash from './orderedHash';
 import cucPoll from './poll';
+import cucPrice from './price';
 import cucRegion from './region';
 import cucProducts from './products';
 import cucSmoothScrollHere from './smoothScrollHere';
@@ -29,11 +32,13 @@ angular
     cui,
     cucAutofocus,
     cucAutoSelect,
+    cucBytes,
     cucClickEnterOnKeypress,
     cucConfig,
     cucConsumption,
     cucCurrency,
     cucFeatureAvailability,
+    cucFlavor,
     cucHelper,
     cucHighlightedElement,
     cucMessage,
@@ -42,6 +47,7 @@ angular
     cucOrderedHash,
     cucRegion,
     cucPoll,
+    cucPrice,
     cucProducts,
     cucSmoothScrollHere,
     cucSpaceMeter,
---
+++ packages/manager/modules/cloud-universe-components/src/price/index.js
@@ -0,0 +1,14 @@
+import angular from 'angular';
+import 'ovh-api-services';
+
+import service from './service';
+
+const moduleName = 'cucPrice';
+
+angular
+  .module(moduleName, [
+    'ovh-api-services',
+  ])
+  .service('CucPriceHelper', service);
+
+export default moduleName;
