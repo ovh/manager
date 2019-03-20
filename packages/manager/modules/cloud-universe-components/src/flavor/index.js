411ae4f0118ac5806a5e6853a7a33c9636927c3e
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-03-20 14:56:30 +0100

    feat(cloud-univer-components): add services and filter
    * add `cucBytes` filter
    * add `CucPriceHelper` service
    * add `CucFlavorService` service

---
+++ packages/manager/modules/cloud-universe-components/src/flavor/index.js
@@ -0,0 +1,24 @@
+import angular from 'angular';
+
+import cucBytes from '../bytes';
+
+import {
+  FLAVORTYPE_CATEGORY,
+  INSTANCE_CPU_FREQUENCY,
+  INSTANCE_NUMBER_OF_GPUS,
+} from './constants';
+
+import service from './service';
+
+const moduleName = 'cucFlavor';
+
+angular
+  .module(moduleName, [
+    cucBytes,
+  ])
+  .constant('CUC_FLAVOR_FLAVORTYPE_CATEGORY', FLAVORTYPE_CATEGORY)
+  .constant('CUC_FLAVOR_INSTANCE_CPU_FREQUENCY', INSTANCE_CPU_FREQUENCY)
+  .constant('CUC_FLAVOR_INSTANCE_NUMBER_OF_GPUS', INSTANCE_NUMBER_OF_GPUS)
+  .service('CucFlavorService', service);
+
+export default moduleName;
