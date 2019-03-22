831f09aee118013adf2515b83029070842577317
jleveugle <joffrey.leveugle@corp.ovh.com>
2019-03-22 10:33:53 +0100

    style(eslint): disable rules on block instead of line per line

--- packages/manager/apps/pci/index.js
+++ packages/manager/apps/pci/index.js
@@ -1,17 +1,18 @@
 import angular from 'angular';
 
-import 'script-loader!jquery'; // eslint-disable-line
-import 'script-loader!lodash'; // eslint-disable-line
-import 'script-loader!jquery-ui/ui/minified/core.min'; // eslint-disable-line
-import 'script-loader!jquery-ui/ui/minified/widget.min'; // eslint-disable-line
-import 'script-loader!jquery-ui/ui/minified/mouse.min'; // eslint-disable-line
-import 'script-loader!jquery-ui/ui/minified/draggable.min'; // eslint-disable-line
-import 'script-loader!messenger/build/js/messenger.js'; // eslint-disable-line
-import 'script-loader!messenger/build/js/messenger-theme-future.js'; // eslint-disable-line
-import 'script-loader!messenger/build/js/messenger-theme-flat.js'; // eslint-disable-line
-import 'script-loader!messenger/build/js/messenger-theme-flat.js'; // eslint-disable-line
-import 'script-loader!jsplumb'; // eslint-disable-line
-import 'script-loader!angular-ui-validate/dist/validate.js'; // eslint-disable-line
+/* eslint-disable import/no-webpack-loader-syntax, import/extensions */
+import 'script-loader!jquery';
+import 'script-loader!lodash';
+import 'script-loader!jquery-ui/ui/minified/core.min';
+import 'script-loader!jquery-ui/ui/minified/widget.min';
+import 'script-loader!jquery-ui/ui/minified/mouse.min';
+import 'script-loader!jquery-ui/ui/minified/draggable.min';
+import 'script-loader!messenger/build/js/messenger.js';
+import 'script-loader!messenger/build/js/messenger-theme-future.js';
+import 'script-loader!messenger/build/js/messenger-theme-flat.js';
+import 'script-loader!jsplumb';
+import 'script-loader!angular-ui-validate/dist/validate.js';
+/* eslint-enable import/no-webpack-loader-syntax, import/extensions */
 
 import '@ovh-ux/manager-pci';
 
