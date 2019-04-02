03f020772764e8eed555cec5cb94f030b1bdbe5e
jleveugle <joffrey.leveugle@corp.ovh.com>
2019-04-02 17:30:01 +0200

    feat(project): user are now able to edit project and define default

--- packages/manager/modules/pci/src/projects/project/edit/index.js
+++ packages/manager/modules/pci/src/projects/project/edit/index.js
@@ -2,31 +2,22 @@ import angular from 'angular';
 
 import '@ovh-ux/manager-core';
 import '@ovh-ux/ng-ovh-api-wrappers'; // should be a peer dependency of ovh-api-services
+import '@ovh-ux/ng-ovh-user-pref';
 import 'angular-translate';
 import 'ovh-api-services';
 import 'ovh-ui-angular';
 
+import routing from './edit.routing';
 
-import edit from './edit';
-import instances from './instances';
-import kubernetes from './kubernetes';
-import legacy from './legacy';
-import storages from './storages';
-import routing from './project.routing';
-
-const moduleName = 'ovhManagerPciProject';
+const moduleName = 'ovhManagerPciProjectEdit';
 
 angular
   .module(moduleName, [
-    edit,
-    instances,
-    kubernetes,
-    legacy,
+    'ngOvhUserPref',
     'oui',
     'ovhManagerCore',
     'ovh-api-services',
     'pascalprecht.translate',
-    storages,
   ])
   .config(routing);
 
