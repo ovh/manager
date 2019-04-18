f468b0dfe5faff444a1ca120be2a416403f55b63
jleveugle <joffrey.leveugle@corp.ovh.com>
2019-04-18 15:40:18 +0200

    feat: add ui-router breadcrumb module

---
+++ packages/components/ng-ovh-uirouter-breadcrumb/src/ng-ovh-uirouter-breadcrumb.controller.js
@@ -0,0 +1,46 @@
+export default class ngUirouterbreadcrumbController {
+  /* @ngInject */
+  constructor($injector, $location, $q, $state, $transitions) {
+    this.$injector = $injector;
+    this.$location = $location;
+    this.$q = $q;
+    this.$state = $state;
+    this.$transitions = $transitions;
+
+    this.breadcrumb = null;
+  }
+
+  $onInit() {
+    this.$transitions.onSuccess({}, (transition) => {
+      transition.promise.finally(() => {
+        this.breadcrumb = [];
+        let state = this.$state.$current;
+
+        while (state.parent) {
+          const breadcrumbResolvable = state.resolvables.find(resolvable => resolvable.token === 'breadcrumb');
+
+          if (!(state.self.abstract && !breadcrumbResolvable)) {
+            const entry = {
+              name: state.name,
+              promise: breadcrumbResolvable
+                ? transition.injector(state.name).getAsync('breadcrumb')
+                : this.$q.when(state.name),
+              url: state.self.abstract
+                ? null
+                : this.$state.href(state.name, this.$state.params, { absolute: true }),
+              active: this.$state.is(state.name),
+            };
+
+            this.breadcrumb.unshift(entry);
+
+            entry.promise.then((value) => {
+              entry.name = value;
+            });
+          }
+
+          state = state.parent.self.$$state();
+        }
+      });
+    });
+  }
+}
