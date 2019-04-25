1a51c66df65a33512d5e9335460bcb7eea68fcc6
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-04-25 11:49:22 +0200

    feat: add instance creation breadcrumb

--- packages/manager/modules/pci/src/projects/project/instances/add/add.routing.js
+++ packages/manager/modules/pci/src/projects/project/instances/add/add.routing.js
@@ -3,6 +3,9 @@ export default /* @ngInject */($stateProvider) => {
     url: '/new',
     component: 'ovhManagerPciInstancesAdd',
     resolve: {
+      breadcrumb: /* @ngInject */ $translate => $translate
+        .refresh()
+        .then(() => $translate.instant('pci_projects_project_instances_add_title')),
       goBack: ($state, projectId) => () => $state.go('pci.projects.project.instances', { projectId }),
     },
   });
