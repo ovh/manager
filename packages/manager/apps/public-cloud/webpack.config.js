bfa4da0a38597d6bf0a14cc97782f1ffda352bb9
Joffrey LEVEUGLE <joffrey.leveugle@corp.ovh.com>
2019-04-26 17:53:55 +0200

    * feat: add onboarding page & add feedback button

--- packages/manager/apps/public-cloud/webpack.config.js
+++ packages/manager/apps/public-cloud/webpack.config.js
@@ -1,12 +1,18 @@
 const merge = require('webpack-merge');
 const path = require('path');
 const webpackConfig = require('@ovh-ux/manager-webpack-config');
+const webpack = require('webpack');
 
 module.exports = (env = {}) => {
   const { config } = webpackConfig({
     template: './src/index.html',
     basePath: './src',
     root: path.resolve(__dirname, './src'),
+    assets: {
+      files: [
+        { from: path.resolve(__dirname, '../../../../node_modules/@ovh-ux/manager-pci/dist/assets'), to: 'assets' },
+      ],
+    },
   }, env);
 
   return merge(config, {
@@ -15,6 +21,11 @@ module.exports = (env = {}) => {
       path: path.join(__dirname, 'dist'),
       filename: '[name].[chunkhash].bundle.js',
     },
+    plugins: [
+      new webpack.DefinePlugin({
+        __FEEDBACK_URL__: process.env.FEEDBACK_URL ? `'${process.env.FEEDBACK_URL}'` : 'null',
+      }),
+    ],
     resolve: {
       modules: [
         './node_modules',
