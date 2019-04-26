e49d14d4a0c679951f2316a522671a015010b001
jleveugle <joffrey.leveugle@corp.ovh.com>
2019-04-26 11:50:58 +0200

    refactor: improve code following reviews

--- packages/manager/apps/public-cloud/src/components/beta-warning/index.js
+++ packages/manager/apps/public-cloud/src/components/beta-warning/index.js
@@ -1,6 +1,5 @@
 import angular from 'angular';
 import 'ovh-ui-angular';
-import ngOvhSidebarMenu from '@ovh-ux/ng-ovh-sidebar-menu';
 
 import component from './beta-warning.component';
 
@@ -11,9 +10,8 @@ const moduleName = 'ovh-manager-betaWarning';
 angular
   .module(moduleName, [
     'oui',
-    ngOvhSidebarMenu,
   ])
-  .component('betaWarning', component)
+  .component('publicCloudBetaWarning', component)
   .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
