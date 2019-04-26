b01e954422e64b4187e04fccc9e9e5dc4d1475be
Joffrey LEVEUGLE <joffrey.leveugle@corp.ovh.com>
2019-04-26 17:53:55 +0200

    * feat: add onboarding page & add feedback button

--- packages/manager/apps/public-cloud/src/index.controller.js
+++ packages/manager/apps/public-cloud/src/index.controller.js
@@ -1,7 +1,11 @@
+import feedback from './feedback-icon.svg';
+
 export default class PublicCloudController {
   /* @ngInject */
   constructor($state, $transitions) {
     this.$state = $state;
+    this.feedbackUrl = __FEEDBACK_URL__;
+    this.feedback = feedback;
     this.$transitions = $transitions;
 
     this.$transitions.onSuccess({}, () => {
