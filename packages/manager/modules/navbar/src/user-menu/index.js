50cacf34ca686c19d3a3e6143d276e0cf5832083
Marie JONES <marie.jones@corp.ovh.com>
2019-04-29 17:06:09 +0200

    fix: enable user logout

--- packages/manager/modules/navbar/src/user-menu/index.js
+++ packages/manager/modules/navbar/src/user-menu/index.js
@@ -3,6 +3,7 @@ import 'angular-translate';
 import 'ovh-ui-angular';
 
 import '@ovh-ux/ng-at-internet';
+import '@ovh-ux/ng-ovh-sso-auth';
 
 import menuHeader from '../navbar-menu-header';
 
@@ -13,6 +14,7 @@ const moduleName = 'ovhManagerNavbarUserMenu';
 angular
   .module(moduleName, [
     'ngAtInternet',
+    'ngOvhSsoAuth',
     'oui',
     'pascalprecht.translate',
     menuHeader,
