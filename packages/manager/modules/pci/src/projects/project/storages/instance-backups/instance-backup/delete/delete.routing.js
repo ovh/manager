e7f538f8584c9906351de59f8a70e0e27a230dd6
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-04-30 18:20:11 +0200

    fix: update instance backups

--- packages/manager/modules/pci/src/projects/project/storages/instance-backups/instance-backup/delete/delete.routing.js
+++ packages/manager/modules/pci/src/projects/project/storages/instance-backups/instance-backup/delete/delete.routing.js
@@ -8,16 +8,20 @@ export default /* @ngInject */($stateProvider) => {
         },
       },
       layout: 'modal',
+      translations: {
+        value: ['.'],
+        format: 'json',
+      },
       resolve: {
         instanceBackupId: /* @ngInject */$transition$ => $transition$.params().instanceBackupId,
-        goBack: /* @ngInject */ ($rootScope, $state, projectId) => (reload = false) => {
-          if (reload) {
-            $rootScope.$emit('pci_storages_instance-backups_refresh');
-          }
-          return $state.go('pci.projects.project.storages.instance-backups', {
-            projectId,
-          });
-        },
+        instanceBackup: /* @ngInject */ (
+          PciProjectStorageInstanceBackupService,
+          projectId,
+          instanceBackupId,
+        ) => PciProjectStorageInstanceBackupService
+          .get(projectId, instanceBackupId),
+
+        goBack: /* @ngInject */ goToInstanceBackups => goToInstanceBackups,
       },
     });
 };
--- packages/manager/modules/pci/src/projects/project/storages/instance-backups/instance-backup/delete/index.js
+++ packages/manager/modules/pci/src/projects/project/storages/instance-backups/instance-backup/delete/index.js
@@ -1,25 +1,24 @@
 import angular from 'angular';
-import '@ovh-ux/ng-translate-async-loader';
 import '@uirouter/angularjs';
-import 'angular-translate';
-import 'ovh-ui-angular';
-import 'ovh-api-services';
+import 'oclazyload';
 
-import component from './delete.component';
-import routing from './delete.routing';
-
-const moduleName = 'pciProjectStorageInstanceBackupsDelete';
+const moduleName = 'pciProjectStorageInstanceBackupsDeleteLazyLoading';
 
 angular
   .module(moduleName, [
     'ui.router',
-    'oui',
-    'ovh-api-services',
-    'ngTranslateAsyncLoader',
-    'pascalprecht.translate',
+    'oc.lazyLoad',
   ])
-  .config(routing)
-  .component('pciProjectStorageInstanceBackupsDelete', component)
-  .run(/* @ngTranslationsInject:json ./translations */);
+  .config(/* @ngInject */($stateProvider) => {
+    $stateProvider.state('pci.projects.project.storages.instance-backups.delete.**', {
+      url: '/delete?instanceBackupId',
+      lazyLoad: ($transition$) => {
+        const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+
+        return import('./delete.module')
+          .then(mod => $ocLazyLoad.inject(mod.default || mod));
+      },
+    });
+  });
 
 export default moduleName;
