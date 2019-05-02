667f777997b9c6da8004a0b74c679c663eca86e0
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-05-02 14:48:18 +0200

    feat: add breadcrumb service

--- packages/components/ng-ovh-uirouter-breadcrumb/src/ng-ovh-uirouter-breadcrumb.controller.js
+++ packages/components/ng-ovh-uirouter-breadcrumb/src/ng-ovh-uirouter-breadcrumb.controller.js
@@ -1,51 +1,17 @@
 export default class ngUirouterBreadcrumbController {
   /* @ngInject */
-  constructor($injector, $location, $q, $state, $transitions) {
-    this.$injector = $injector;
-    this.$location = $location;
-    this.$q = $q;
-    this.$state = $state;
-    this.$transitions = $transitions;
+  constructor(
+    ngOvhUirouterBreadcrumbService,
+  ) {
+    this.ngOvhUirouterBreadcrumbService = ngOvhUirouterBreadcrumbService;
 
-    this.breadcrumb = null;
+    this.breadcrumb = [];
   }
 
   $onInit() {
-    this.$transitions.onSuccess({}, (transition) => {
-      transition.promise.finally(() => {
-        this.breadcrumb = [];
-        let state = this.$state.$current;
-
-        while (state.parent) {
-          const breadcrumbResolvable = state.resolvables.find(resolvable => resolvable.token === 'breadcrumb');
-
-          if (!state.self.abstract || breadcrumbResolvable) {
-            const entry = {
-              name: state.name,
-              promise: breadcrumbResolvable
-                ? transition.injector(state.name).getAsync('breadcrumb')
-                : this.$q.when(state.name),
-              url: state.self.abstract
-                ? null
-                : this.$state.href(state.name, this.$state.params, { absolute: true }),
-              active: this.$state.is(state.name),
-            };
-
-            this.breadcrumb.unshift(entry);
-
-            entry.promise.then((value) => {
-              if (value) {
-                entry.name = value;
-              } else {
-                this.breadcrumb = this.breadcrumb
-                  .filter(breadcrumbEntry => breadcrumbEntry !== entry);
-              }
-            });
-          }
-
-          state = state.parent.self.$$state();
-        }
-      });
+    this.ngOvhUirouterBreadcrumbService.subscribe((breadcrumb) => {
+      this.breadcrumb = breadcrumb;
     });
+    this.breadcrumb = this.ngOvhUirouterBreadcrumbService.getBreadcrumb();
   }
 }
---
+++ packages/components/ng-ovh-uirouter-breadcrumb/src/ng-ovh-uirouter-breadcrumb.service.js
@@ -0,0 +1,66 @@
+export default class BreadcrumbService {
+  /* @ngInject */
+  constructor(
+    $q,
+    $state,
+  ) {
+    this.$q = $q;
+    this.$state = $state;
+    this.breadcrumb = [];
+    this.listeners = [];
+  }
+
+  getBreadcrumb() {
+    return this.bredcrumb;
+  }
+
+  buildBreadcrumb(transition) {
+    return transition.promise.then(() => {
+      this.breadcrumb = [];
+
+      let state = this.$state.$current;
+
+      while (state.parent) {
+        const breadcrumbResolvable = state.resolvables.find(resolvable => resolvable.token === 'breadcrumb');
+
+        if (!state.self.abstract || breadcrumbResolvable) {
+          const entry = {
+            name: state.name,
+            promise: breadcrumbResolvable
+              ? transition.injector(state.name).getAsync('breadcrumb')
+              : this.$q.when(state.name),
+            url: state.self.abstract
+              ? null
+              : this.$state.href(state.name, this.$state.params, { absolute: true }),
+            active: this.$state.is(state.name),
+          };
+
+          this.breadcrumb.unshift(entry);
+
+          entry.promise.then((value) => {
+            if (value) {
+              entry.name = value;
+            } else {
+              this.breadcrumb = this.breadcrumb
+                .filter(breadcrumbEntry => breadcrumbEntry !== entry);
+            }
+          });
+        }
+
+        state = state.parent.self.$$state();
+      }
+
+      return this.$q.all(this.breadcrumb.map(entry => entry.promise))
+        .then(() => this.notifyAll())
+        .then(() => this.breadcrumb);
+    });
+  }
+
+  subscribe(listener) {
+    this.listeners.push(listener);
+  }
+
+  notifyAll() {
+    this.listeners.forEach(listener => listener(this.breadcrumb));
+  }
+}
