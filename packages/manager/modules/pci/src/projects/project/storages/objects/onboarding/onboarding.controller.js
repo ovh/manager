310395312b0a4645d57d682cd7935c8eb128a86f
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-05-03 12:00:43 +0200

    feat(pci.project.storage.objects): add empty page
    ref: MANAGER-2280

---
+++ packages/manager/modules/pci/src/projects/project/storages/objects/onboarding/onboarding.controller.js
@@ -0,0 +1,26 @@
+import reduce from 'lodash/reduce';
+import { GUIDES } from './onboarding.constants';
+
+export default class PciStorageObjectsOnboardingController {
+  /* @ngInject */
+  constructor(
+    $translate,
+  ) {
+    this.$translate = $translate;
+  }
+
+  $onInit() {
+    this.guides = reduce(
+      GUIDES,
+      (list, guide) => ([
+        ...list,
+        {
+          ...guide,
+          title: this.$translate.instant(`pci_projects_project_storages_objects_onboarding_guides_${guide.id}_title`),
+          description: this.$translate.instant(`pci_projects_project_storages_objects_onboarding_guides_${guide.id}_description`),
+        },
+      ]),
+      [],
+    );
+  }
+}
