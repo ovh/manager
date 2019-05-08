d12639aff44839ab0349702cefc48503e8efbd7f
jleveugle <joffrey.leveugle@corp.ovh.com>
2019-05-08 23:21:01 +0200

    feat(us): configure us side and remove useless links

--- packages/manager/apps/public-cloud/src/index.service.js
+++ packages/manager/apps/public-cloud/src/index.service.js
@@ -1,10 +1,23 @@
+import sortBy from 'lodash/sortBy';
+
 export default class PublicCloud {
   /* @ngInject */
-  constructor(iceberg) {
+  constructor(coreConfig, iceberg, OvhApiCloudProject) {
+    this.coreConfig = coreConfig;
     this.iceberg = iceberg;
+    this.OvhApiCloudProject = OvhApiCloudProject;
   }
 
   getProjects(filters = []) {
+    // Don't have Iceberg in US -> Fallback by ovh-api-services
+    if (this.coreConfig.isRegion('US')) {
+      return this.OvhApiCloudProject
+        .v6()
+        .queryDetails()
+        .then(projects => sortBy(projects, 'description'));
+    }
+
+    // Use Iceberg
     return filters.reduce(
       (promise, {
         field,
