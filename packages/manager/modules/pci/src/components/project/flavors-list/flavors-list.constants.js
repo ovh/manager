40e3f3b47c512dffcb3d0c764e6867d696f75cba
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-05-10 11:18:29 +0200

    fix(pci.flavors-list): add frequency (#504)
    ref: MSB-2

--- packages/manager/modules/pci/src/components/project/flavors-list/flavors-list.constants.js
+++ packages/manager/modules/pci/src/components/project/flavors-list/flavors-list.constants.js
@@ -29,7 +29,22 @@ export const LEGACY_FLAVORS = /eg|sp|hg|vps-ssd/;
 
 export const SSD_DISK_TYPES = [/ssd/, /nvme/];
 
+export const CPU_FREQUENCY = {
+  'ovh.vps-ssd': 2.4,
+  'ovh.cpu': 3.1,
+  'ovh.ram': 2.4,
+  'ovh.ceph.eg': 2.3,
+  'ovh.ssd.ram': 2.4,
+  'ovh.ssd.cpu': 3.1,
+  'ovh.ssd.eg': 2.3,
+  'ovh.ssd.gpu': 3.1,
+  'ovh.ssd.gpu2': 3.1,
+  'ovh.ssd.gpu3': 3.1,
+  'ovh.raid-nvme.t1': 2.1,
+};
+
 export default {
+  CPU_FREQUENCY,
   DEFAULT_OS,
   FLEX_TYPE,
   LEGACY_FLAVORS,
