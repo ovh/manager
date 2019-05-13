34c0ec66cccdca9d79c091ad3da3a4aded1cb0ee
Marie JONES <marie.jones@corp.ovh.com>
2019-05-13 13:25:07 +0200

    feat(pci.kubernetes): add onboarding

---
+++ packages/manager/modules/pci/src/projects/project/kubernetes/onboarding/onboarding.routing.js
@@ -0,0 +1,14 @@
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider
+    .state('pci.projects.project.kubernetes.onboarding', {
+      url: '/onboarding',
+      component: 'pciProjectKubernetesOnboarding',
+      redirectTo: transition => transition
+        .injector()
+        .getAsync('kubernetes')
+        .then(kubernetes => (kubernetes.length > 0 ? { state: 'pci.projects.project.kubernetes' } : false)),
+      resolve: {
+        breadcrumb: () => null, // Hide breadcrumb
+      },
+    });
+};
