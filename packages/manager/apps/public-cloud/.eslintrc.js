e35013a3f8b17797418325e3840b9d0d5b700181
jleveugle <joffrey.leveugle@corp.ovh.com>
2019-05-13 18:32:46 +0200

    build: manage __NG_APP_INJECTIONS

--- packages/manager/apps/public-cloud/.eslintrc.js
+++ packages/manager/apps/public-cloud/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     "__FEEDBACK_URL__": true,
     "__WEBPACK_REGION__": true,
     "__NODE_ENV__": true,
+    "__NG_APP_INJECTIONS__": true,
     moment: true,
     Tour: true,
   },
