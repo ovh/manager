5069bd128847f57ad974acf48c91e3ba1182a335
Marie JONES <marie.jones@corp.ovh.com>
2019-05-15 16:22:50 +0200

    Revert "fix(pci.sidebar): allow Kubernetes in CA region"
    This reverts commit d1e84001a731098b098bf39de1e1b3297b4ca80f.

--- packages/manager/modules/pci/src/projects/project/sidebar/sidebar.constant.js
+++ packages/manager/modules/pci/src/projects/project/sidebar/sidebar.constant.js
@@ -71,7 +71,7 @@ export const MENU = [
         translation: 'cloud_sidebar_orchestration_kubernetes',
       },
     ],
-    regions: ['EU', 'CA'],
+    regions: ['EU'],
     translation: 'cloud_sidebar_orchestration',
   },
   {
