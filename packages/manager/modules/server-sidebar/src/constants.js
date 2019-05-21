06be27887c085b0c1a09f9b3f68a792306a38901
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-05-21 02:45:17 +0200

    feat: add server-sidebar module (#639)

---
+++ packages/manager/modules/server-sidebar/src/constants.js
@@ -0,0 +1,31 @@
+export const DEDICATED = 'DEDICATED';
+export const CLOUD = 'CLOUD';
+
+export const MANAGER_URLS = {
+  EU: {
+    DEDICATED: {
+      FR: 'https://www.ovh.com/manager/dedicated/',
+    },
+    CLOUD: {
+      FR: 'https://www.ovh.com/manager/cloud/repsac/',
+    },
+  },
+  CA: {
+    DEDICATED: {
+      FR: 'https://ca.ovh.com/manager/dedicated/',
+    },
+    CLOUD: {
+      FR: 'https://ca.ovh.com/manager/cloud/repsac/',
+    },
+  },
+  US: {
+    DEDICATED: {
+      FR: 'https://us.ovhcloud.com/manager/dedicated/',
+    },
+    CLOUD: {
+      FR: 'https://us.ovhcloud.com/manager/cloud/repsac/',
+    },
+  },
+};
+
+export default { DEDICATED, CLOUD, MANAGER_URLS };
