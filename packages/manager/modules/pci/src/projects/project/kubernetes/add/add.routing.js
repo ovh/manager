2383b3512fe20233f55c3c4b52e598484bfaed3c
Marie JONES <marie.jones@corp.ovh.com>
2019-05-22 15:36:16 +0200

    fix(pci.kube): redirect to onboarding if no region

--- packages/manager/modules/pci/src/projects/project/kubernetes/add/add.routing.js
+++ packages/manager/modules/pci/src/projects/project/kubernetes/add/add.routing.js
@@ -3,6 +3,11 @@ export default /* @ngInject */ ($stateProvider) => {
     .state('pci.projects.project.kubernetes.add', {
       url: '/new',
       component: 'ovhManagerPciProjectKubernetesAdd',
+      redirectTo:
+        transition => transition
+          .injector()
+          .getAsync('regions')
+          .then(regions => (regions.length === 0 ? { state: 'pci.projects.project.kubernetes.onboarding' } : false)),
       resolve: {
         goBack: /* @ngInject */ goToKubernetes => goToKubernetes,
 
