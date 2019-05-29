e066c0096c2a12df959bddc5ff8a4c49265519d3
Marie JONES <marie.jones@corp.ovh.com>
2019-05-29 09:33:47 +0200

    fix(pci.projects.edit): refresh when project is edited

---
+++ packages/manager/modules/pci/src/projects/project/edit/edit.component.js
@@ -0,0 +1,12 @@
+import controller from './edit.controller';
+import template from './edit.html';
+
+export default {
+  bindings: {
+    defaultProject: '<',
+    onUpdate: '<',
+    project: '<',
+  },
+  controller,
+  template,
+};
