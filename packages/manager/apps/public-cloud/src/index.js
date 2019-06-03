5c4f4c70c33f51305319bd01cd525fba9398c1c9
jean-christophe.alleman <jean-christophe.alleman@corp.ovh.com>
2019-06-03 15:00:15 +0200

    fix: console.log

--- packages/manager/apps/public-cloud/src/index.js
+++ packages/manager/apps/public-cloud/src/index.js
@@ -73,7 +73,6 @@ angular
   })
   .run(/* @ngInject */ ($state) => {
     $state.defaultErrorHandler((error) => {
-      console.log(error);
       if (error.type === RejectType.ERROR) {
         $state.go('pci.error', {
           detail: {
