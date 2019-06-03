11a198a983478a672aa644d05b47fa7ad8afe313
Marie JONES <marie.jones@corp.ovh.com>
2019-06-03 09:03:55 +0200

    fix(pci.projects.instance): display currency according to sub not locale

--- packages/manager/modules/pci/src/components/project/flavor-billing/flavor-billing.controller.js
+++ packages/manager/modules/pci/src/components/project/flavor-billing/flavor-billing.controller.js
@@ -1,9 +1,21 @@
 export default class FlavorBillingController {
+  /* @ngInject */
+  constructor(TranslateService) {
+    this.TranslateService = TranslateService;
+  }
+
   $onInit() {
     this.monthly = this.monthlyBilling === true;
     this.number = this.number || 1;
 
     this.disabled = this.disabled || false;
+  }
+
+  set flavor(flavor) {
+    if (flavor) {
+      this.prices = flavor.prices;
+      this.PriceFormatter = new Intl.NumberFormat(this.TranslateService.getUserLocale().replace('_', '-'), { style: 'currency', currency: flavor.prices.hourly.currencyCode });
+    }
   }
 
   selectBilling(monthlyBilling) {
@@ -12,5 +24,9 @@ export default class FlavorBillingController {
 
   getPrice(price) {
     return this.number * price.value;
+  }
+
+  formatPrice(price) {
+    return this.PriceFormatter.format(this.getPrice(price));
   }
 }
