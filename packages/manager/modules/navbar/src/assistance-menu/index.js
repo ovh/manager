8dfccdf19a21c389245332d6ff5163a76cd7e8ea
Marie JONES <14836007+marie-j@users.noreply.github.com>
2019-06-14 10:57:26 +0200

    feat(navbar): uniformize assistance request between all subs (#845)

--- packages/manager/modules/navbar/src/assistance-menu/index.js
+++ packages/manager/modules/navbar/src/assistance-menu/index.js
@@ -2,14 +2,11 @@ import angular from 'angular';
 import 'angular-translate';
 
 import '@ovh-ux/ng-at-internet';
-import '@ovh-ux/ng-ovh-otrs';
 import '@ovh-ux/ng-ovh-chatbot';
 
 import 'ovh-ui-angular';
 
 import menuHeader from '../navbar-menu-header';
-
-import { ASSISTANCE_URLS } from './constants';
 
 import component from './component';
 
@@ -20,14 +17,10 @@ angular
     'ovhManagerCore',
     'ngAtInternet',
     'ngOvhChatbot',
-    'ngOvhOtrs',
     'oui',
     'pascalprecht.translate',
     menuHeader,
   ])
-  .config(/* @ngInject */ (OtrsPopupProvider, coreConfigProvider) => {
-    OtrsPopupProvider.setBaseUrlTickets(ASSISTANCE_URLS[coreConfigProvider.getRegion()].ticket);
-  })
   .component('ovhManagerNavbarAssistanceMenu', component)
   .run(/* @ngTranslationsInject:json ./translations */);
 
