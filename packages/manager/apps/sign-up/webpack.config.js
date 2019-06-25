5b93aeae20137d54d26f75f5f19fadb53473e937
jean-christophe.alleman <jean-christophe.alleman@corp.ovh.com>
2019-06-25 16:32:38 +0200

    feat(sign-up): add some fields for activity step

--- packages/manager/apps/sign-up/webpack.config.js
+++ packages/manager/apps/sign-up/webpack.config.js
@@ -27,6 +27,7 @@ module.exports = (env = {}) => {
         'browser',
         'main',
       ],
+      symlinks: false,
     },
   });
 };
--- packages/manager/modules/sign-up/src/form/form.constants.js
+++ packages/manager/modules/sign-up/src/form/form.constants.js
@@ -10,6 +10,13 @@ export const ENUM_TRANSLATION_RULES = [{
 }, {
   fieldName: 'language',
   sort: true,
+}, {
+  fieldName: 'corporationType',
+  sort: true,
+}, {
+  fieldName: 'area',
+  dependsOfCountry: true,
+  sort: true,
 }];
 
 export const MODEL_DEBOUNCE_DELAY = 500;
