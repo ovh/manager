fd7a5b859937dce426ca44ff83de0aabb88a1318
jleveugle <joffrey.leveugle@corp.ovh.com>
2019-06-27 12:31:01 +0200

    Revert "build: add new module @ovh-ux/manager-webpack-dev-server"
    This reverts commit 3663c6821899e3edd0fb6632d7485cc835619fe2.

--- packages/manager/tools/webpack-dev-server/src/index.js
+++
@@ -1,9 +0,0 @@
-const sso = require('./sso');
-const proxy = require('./proxy');
-const config = require('./config.js');
-
-module.exports = {
-  sso,
-  proxy,
-  config,
-};
--- packages/manager/tools/webpack-dev-server/src/proxy/index.js
+++
@@ -1,9 +0,0 @@
-const aapi = require('./aapi');
-const dev = require('./dev');
-const v6 = require('./v6');
-
-module.exports = {
-  aapi,
-  dev,
-  v6,
-};
