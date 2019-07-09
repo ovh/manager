053d0a4b9e73771a780d669550ae89a2a7a3a051
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-07-09 15:29:30 +0200

    fix(pci.storage.container): display gateway url for archive storage
    ref: MANAGER-3035, DTRUN-2259

--- packages/manager/modules/pci/src/projects/project/storages/containers/container.class.js
+++ packages/manager/modules/pci/src/projects/project/storages/containers/container.class.js
@@ -1,5 +1,7 @@
 import find from 'lodash/find';
 
+import { STORAGE_GATEWAY } from './containers.constants';
+
 export default class Container {
   constructor(resource) {
     this.publicUrl = null;
@@ -8,5 +10,9 @@ export default class Container {
 
   getObjectById(objectId) {
     return find(this.objects, { name: objectId });
+  }
+
+  get storageGateway() {
+    return STORAGE_GATEWAY.replace('REGION', this.region.toLowerCase());
   }
 }
