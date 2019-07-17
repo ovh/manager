5ea40db63299458be4d69a60c4a29903f3f5c083
Ganesh Kumar <ganesh.kumar@corp.ovh.com>
2019-07-17 11:14:40 +0530

    fix(pci-analytics-data-platform): deploy ui nan
    fix for issue where some controls becom nan when the input fields are emptied
    
    closes MANAGER-3036

--- packages/manager/modules/pci/src/projects/project/analytics-data-platform/deploy/storage/storage.controller.js
+++ packages/manager/modules/pci/src/projects/project/analytics-data-platform/deploy/storage/storage.controller.js
@@ -1,10 +1,13 @@
+import isUndefined from 'lodash/isUndefined';
+
 import {
   ANALYTICS_DATA_PLATFORM_INPUT_NUMBER_PATTERN,
 } from '../../analytics-data-platform.constants';
 
 export default class {
   /* @ngInject */
-  constructor() {
+  constructor($translate) {
+    this.$translate = $translate;
     this.INPUT_NUMBER_PATTERN = ANALYTICS_DATA_PLATFORM_INPUT_NUMBER_PATTERN;
     this.data = {
       hdfsEffectiveStorage: 0,
@@ -19,5 +22,19 @@ export default class {
 
   dataChange() {
     this.onDataChange({ data: this.data });
+  }
+
+  getStoragePerNode() {
+    return isUndefined(this.data.hdfsEffectiveStorage) ? '-'
+      : `${((this.data.hdfsEffectiveStorage * this.selectedCapability.hdfsReplicationFactor) / this.nodesConfig.worker.count)} ${this.$translate.instant('analytics_data_platform_common_unit_gb')}`;
+  }
+
+  getTotalStorage() {
+    return isUndefined(this.data.hdfsEffectiveStorage) ? '-'
+      : `${(this.data.hdfsEffectiveStorage * this.selectedCapability.hdfsReplicationFactor)} ${this.$translate.instant('analytics_data_platform_common_unit_gb')}`;
+  }
+
+  getTotalEffectiveStorage() {
+    return isUndefined(this.data.edgeNodeStorage) ? '-' : `${(this.data.edgeNodeStorage * this.nodesConfig.edge.count)} ${this.$translate.instant('analytics_data_platform_common_unit_gb')}`;
   }
 }
