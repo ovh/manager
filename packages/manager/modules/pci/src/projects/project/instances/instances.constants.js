f7fceacf2d91c8b05b371179a6c2f80d0b9ced40
Marie JONES <marie.jones@corp.ovh.com>
2019-07-22 13:25:33 +0200

    feat(pci.instances): warn about extra bandwidth cost

--- packages/manager/modules/pci/src/projects/project/instances/instances.constants.js
+++ packages/manager/modules/pci/src/projects/project/instances/instances.constants.js
@@ -1,8 +1,17 @@
+export const BANDWIDTH_CONSUMPTION = 'bandwidth_instance';
+
+export const BANDWIDTH_LIMIT = 1025;
+
+export const BANDWIDTH_OUT_INVOICE = 'bandwidth_instance_out consumption';
+
 export const INSTANCE_BACKUP_CONSUMPTION = 'snapshot.consumption';
 
 export const INSTANCE_HELP_REFERENCE_KEY = 'PCI_PROJECTS_INSTANCES_HELP_SHOW_';
 
 export default {
+  BANDWIDTH_CONSUMPTION,
+  BANDWIDTH_LIMIT,
+  BANDWIDTH_OUT_INVOICE,
   INSTANCE_BACKUP_CONSUMPTION,
   INSTANCE_HELP_REFERENCE_KEY,
 };
