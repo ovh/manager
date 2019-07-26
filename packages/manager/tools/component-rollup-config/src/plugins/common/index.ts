e4a42c18e95792f726a5297a493d70a838419bf4
Florian Renaut <florian.renaut@corp.ovh.com>
2019-07-26 13:12:37 +0200

    fix(component-rollup-config): translationNormalize regex
    Co-Authored-By: Frédéric Espiau <7319147+FredericEspiau@users.noreply.github.com>

--- packages/manager/tools/component-rollup-config/src/plugins/common/index.ts
+++ packages/manager/tools/component-rollup-config/src/plugins/common/index.ts
@@ -4,7 +4,7 @@ const translationNormalize = (text) => {
       .toString()
       .replace(/&#13;\n/g, ' ') // carriage returns
       .replace(/&#160;/g, ' ') // spaces
-      .replace(/\{(\s?\d\s?)\}/g, '{{t$1}}'); // {0} => {{t0}}
+      .replace(/\{(\s*\d\s*)\}/g, '{{t$1}}'); // {0} => {{t0}}
     return filtered;
   }
   return text;
