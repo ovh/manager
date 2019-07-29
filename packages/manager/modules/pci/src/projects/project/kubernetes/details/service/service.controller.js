a54eae8b0321dd34deaf83472f0cfb200935b28c
Ravindra.Adireddy <raadired@LAPTOPC02SY20FGTFL.local>
2019-07-29 15:57:56 +0530

    fix(kubernetes): read only user cant access the service page
    manager shows error page with 403 error when read only user tries to access kubernetes dsshboard page
    
    Closes #MBP-529

--- packages/manager/modules/pci/src/projects/project/kubernetes/details/service/service.controller.js
+++ packages/manager/modules/pci/src/projects/project/kubernetes/details/service/service.controller.js
@@ -1,4 +1,4 @@
-import { KUBECONFIG_URL, KUBECTL_URL } from './service.constants';
+import { CONFIG_FILENAME, KUBECONFIG_URL, KUBECTL_URL } from './service.constants';
 import { STATUS } from '../constants';
 
 export default class KubernetesServiceCtrl {
@@ -9,6 +9,7 @@ export default class KubernetesServiceCtrl {
     $translate,
     CucCloudMessage,
     CucControllerHelper,
+    CucServiceHelper,
     Kubernetes,
   ) {
     this.$scope = $scope;
@@ -16,14 +17,16 @@ export default class KubernetesServiceCtrl {
     this.$translate = $translate;
     this.CucCloudMessage = CucCloudMessage;
     this.CucControllerHelper = CucControllerHelper;
+    this.CucServiceHelper = CucServiceHelper;
     this.Kubernetes = Kubernetes;
   }
 
   $onInit() {
+    this.CONFIG_FILENAME = CONFIG_FILENAME;
     this.KUBECONFIG_URL = KUBECONFIG_URL;
     this.KUBECTL_URL = KUBECTL_URL;
     this.STATUS = STATUS;
-
+    this.loadingKubeConfig = false;
     this.loadMessages();
   }
 
@@ -37,7 +40,14 @@ export default class KubernetesServiceCtrl {
   }
 
   downloadConfigFile() {
-    // Set yml extension manually as there is no MIME type yet
-    this.CucControllerHelper.constructor.downloadContent({ fileContent: this.kubernetesConfig.content, fileName: `${this.kubernetesConfig.fileName}.yml` });
+    this.loadingKubeConfig = true;
+    return this.getKubeConfig()
+      .then((config) => {
+        // Set yml extension manually as there is no MIME type yet
+        this.CucControllerHelper.constructor
+          .downloadContent({ fileContent: config.content, fileName: `${config.fileName}.yml` });
+      })
+      .catch(error => this.CucServiceHelper.errorHandler('kube_service_file_error')(error))
+      .finally(() => { this.loadingKubeConfig = false; });
   }
 }
