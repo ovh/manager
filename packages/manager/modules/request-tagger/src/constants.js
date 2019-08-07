e2c9a5a3ab83c2b9aefae5baaf47b2721be988c0
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-08-07 11:09:57 +0200

    feat: add request-tagger module
    BREAKING CHANGE: `@ovh-ux/manager-core` has now a dependency to
    `@ovh-ux/ng-ovh-request-tagger`

---
+++ packages/manager/modules/request-tagger/src/constants.js
@@ -0,0 +1,26 @@
+export const HEADER_NAVIGATION_ID = 'X-OVH-MANAGER-NAVIGATION-ID';
+export const HEADER_REQUEST_ID = 'X-OVH-MANAGER-REQUEST-ID';
+export const HEADER_PAGE = 'X-OVH-MANAGER-PAGE';
+
+export const DEFAULT_PAGE = 'bootstrap';
+
+export const ROUTES_PREFIX = [
+  '/engine/apiv6',
+  '/engine/apiv7',
+  '/engine/2api',
+];
+
+export const ROUTES_HEADERS_OVERRIDE = {
+  '^/engine/2api/service': {
+    [HEADER_PAGE]: 'sidebar',
+  },
+};
+
+export default {
+  DEFAULT_PAGE,
+  HEADER_NAVIGATION_ID,
+  HEADER_REQUEST_ID,
+  HEADER_PAGE,
+  ROUTES_PREFIX,
+  ROUTES_HEADERS_OVERRIDE,
+};
