2c295d30e3b9e915393b935c28b3aca18d13bf49
frenauvh <florian.renaut@corp.ovh.com>
2019-08-12 13:49:35 +0200

    fix(dedicated): remove ovh-module-exchange deprecated component

--- packages/manager/apps/dedicated/src/app/components/ovh-module-exchange/ovh-module-exchange.js
+++
@@ -1,4 +0,0 @@
-angular.module('App').run(($translate, asyncLoader) => {
-  asyncLoader.addTranslations(import(`ovh-module-exchange/src/exchange/translations/Messages_${$translate.use()}.xml`).then(x => x.default));
-  $translate.refresh();
-});
