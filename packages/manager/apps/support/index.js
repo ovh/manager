3bce897d665a6e544957dd2749e38db56850a84a
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-09-04 15:52:04 +0200

    feat(support): create the app
    BREAKING CHANGE

---
+++ packages/manager/apps/support/index.js
@@ -0,0 +1,23 @@
+import managerSupport from '@ovh-ux/manager-support';
+import ngOvhOtrs from '@ovh-ux/ng-ovh-otrs';
+import uiRouterAngularJs from '@uirouter/angularjs';
+import angular from 'angular';
+
+/* eslint-disable import/no-webpack-loader-syntax, import/extensions */
+import 'script-loader!jquery';
+import 'script-loader!moment/min/moment-with-locales.min.js';
+/* eslint-enable import/no-webpack-loader-syntax, import/extensions */
+
+import { state } from './index.routing';
+
+angular
+  .module('supportApp', [
+    managerSupport,
+    ngOvhOtrs,
+    uiRouterAngularJs,
+  ])
+  .config(/* @ngInject */ ($stateProvider) => {
+    $stateProvider.state(state.name, state);
+  })
+  .config(/* @ngInject */ OtrsPopupProvider => OtrsPopupProvider
+    .setBaseUrlTickets('/support/tickets'));
---
+++ packages/manager/apps/support/index.routing.js
@@ -0,0 +1,9 @@
+export const state = {
+  name: 'app',
+  redirectTo: 'support',
+  url: '',
+};
+
+export default {
+  state,
+};
