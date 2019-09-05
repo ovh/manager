b6c3177450c7c9021b0eb335494da4886ee4037d
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-09-05 13:47:12 +0200

    fix(public-cloud): change URL to new Support url

--- packages/manager/apps/public-cloud/src/index.constants.js
+++ packages/manager/apps/public-cloud/src/index.constants.js
@@ -1,9 +1,9 @@
 export const DEFAULT_PROJECT_KEY = 'PUBLIC_CLOUD_DEFAULT_PROJECT';
 
 export const OTRS_URLS = {
-  CA: 'https://ca.ovh.com/manager/index.html#/ticket',
-  EU: 'https://www.ovh.com/manager/dedicated/index.html#/ticket',
-  US: 'https://us.ovhcloud.com/manager/dedicated/index.html#/ticket',
+  CA: 'https://ca.ovh.com/manager/index.html#/support/tickets',
+  EU: 'https://www.ovh.com/manager/dedicated/index.html#/support/tickets',
+  US: 'https://us.ovhcloud.com/manager/dedicated/index.html#/support/tickets',
 };
 
 export default {
