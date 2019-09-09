f6cb3b35346a75e84643b1c8430306e0fbe1f8f5
frenauvh <florian.renaut@corp.ovh.com>
2019-09-09 11:20:08 +0200

    fix(support): comment ticket creation temporarily

--- packages/manager/modules/support/src/tickets/tickets.controller.js
+++ packages/manager/modules/support/src/tickets/tickets.controller.js
@@ -83,14 +83,12 @@ export default class SupportController {
   }
 
   openSupport() {
-    this.goToTicketCreation();
-    /*
+    // this.goToTicketCreation();
     if (!this.OtrsPopupService.isLoaded()) {
       this.OtrsPopupService.init();
     } else {
       this.OtrsPopupService.toggle();
     }
-    */
   }
 
   getTickets() {
