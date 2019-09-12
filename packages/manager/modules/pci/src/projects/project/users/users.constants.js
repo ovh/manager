af01198718cda33011295133d6a3ba4699985d67
varun257 <varun257@gmail.com>
2019-09-12 19:57:56 +0530

    fix(rclone-modal-text): change rclone text (#1128)

--- packages/manager/modules/pci/src/projects/project/users/users.constants.js
+++ packages/manager/modules/pci/src/projects/project/users/users.constants.js
@@ -1,5 +1,7 @@
 export const HORIZON_LINK = 'https://horizon.cloud.ovh.net/openstackdashboard?username={username}';
+export const ALPHA_CHARACTERS_REGEX = /^[a-zA-Z-]+$/;
 
 export default {
   HORIZON_LINK,
+  ALPHA_CHARACTERS_REGEX,
 };
