70c6dde4caa3db7c68a1c7dca8b6942760cad9af
Stephanie Moallic <stephanie.moallic@corp.ovh.com>
2019-09-17 16:22:45 +0200

    feat(sms.options.blacklist): use component for blacklist

--- packages/manager/modules/sms/src/options/blacklist/index.js
+++ packages/manager/modules/sms/src/options/blacklist/index.js
@@ -1,21 +1,11 @@
 import angular from 'angular';
-import controller from './telecom-sms-options-blacklist.controller';
-import template from './telecom-sms-options-blacklist.html';
+import routing from './telecom-sms-options-blacklist.routing';
+import telecomSmsOptionsBlacklist from './telecom-sms-options-blacklist.component';
 
 const moduleName = 'ovhManagerSmsOptionsBlacklist';
 
-angular.module(moduleName, []).config(($stateProvider) => {
-  $stateProvider.state('sms.service.options.blacklist', {
-    url: '/blacklist',
-    views: {
-      'smsView@sms.service': {
-        template,
-        controller,
-        controllerAs: 'SmsOptionsBlacklistCtrl',
-      },
-    },
-    translations: { value: ['.'], format: 'json' },
-  });
-});
+angular.module(moduleName, [])
+  .component('telecomSmsOptionsBlacklist', telecomSmsOptionsBlacklist)
+  .config(routing);
 
 export default moduleName;
---
+++ packages/manager/modules/sms/src/options/blacklist/telecom-sms-options-blacklist.component.js
@@ -0,0 +1,7 @@
+import controller from './telecom-sms-options-blacklist.controller';
+import template from './telecom-sms-options-blacklist.html';
+
+export default {
+  controller,
+  template,
+};
