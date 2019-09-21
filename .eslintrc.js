cb42a00e20b1522d27e0bfcdd9c977162ced7279
Antoine Leblanc <ant.leblanc@gmail.com>
2019-09-21 21:21:07 +0200

    feat: add prettier
    Signed-off-by: Antoine Leblanc <antoine.leblanc@corp.ovh.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,23 @@ const path = require('path');
 module.exports = {
   root: true,
   parser: 'babel-eslint',
-  extends: 'airbnb-base',
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'angular',
+    'markdown',
+    'prettier',
+  ],
+  extends: ['airbnb-base', 'prettier'],
   env: {
     'angular/mocks': true,
     browser: true,
     jquery: true,
   },
-  plugins: [
-    'angular',
-    'markdown',
-  ],
   rules: {
     'no-bitwise': ['error', { allow: ['~'] }],
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
     'import/no-unresolved': 0,
+    'prettier/prettier': 'error',
   },
   settings: {
     'import/resolver': {
@@ -25,8 +28,8 @@ module.exports = {
           path.resolve(__dirname, 'packages/manager/apps'),
           path.resolve(__dirname, 'packages/manager/modules'),
           path.resolve(__dirname, 'packages/manager/tools'),
-        ]
-      }
-    }
+        ],
+      },
+    },
   },
-}
+};
