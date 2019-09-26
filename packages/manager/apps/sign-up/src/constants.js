acca3a90516174d960aec3546a3c5faab41e96aa
Jisay <jean-christophe.alleman@corp.ovh.com>
2019-09-26 09:21:40 +0200

    fix(sign-up): clean phone by adding prefix if not present (#1317)
    * fix(sign-up): clean phone by adding prefix if not present
    
    * fix(sign-up): set sessionStorage value for tracking
    
    * fix(sign-up): phone number clean and validation
    
    * fix(sign-up): change logo again
    
    * build(sign-up): remove img assets
    
    * fix(sign-up): phone number clean
    
    * fix(sign-up): add some router param to allow auto state refresh

--- packages/manager/apps/sign-up/src/constants.js
+++ packages/manager/apps/sign-up/src/constants.js
@@ -23,11 +23,14 @@ export const HOME_PAGE = {
   WS: 'https://www.ovh.com/us/es/',
 };
 
+export const OVH_LOGO = 'https://eu.api.ovh.com/images/com-square-bichro.png';
+
 export const SANITIZATION = {
   regex: /^\s*(?:(?:(?:https?|ftp|file|blob):\/\/(?:(?:(?:[^./?#]+\.)*(?:ovh|(?:ovhcloud(?=\.com))|(?:ovhtelecom(?=\.fr))|(?:ovh-hosting(?=\.fi))|soyoustart|kimsufi|runabove)\.(?:com|net|org|ovh|co\.uk|com\.tn|cz|de|es|eu|fi|fr|ie|it|lt|ma|nl|pl|pt|sn|uk|us))|localhost|127\.0\.0\.1|[\w-]+\.uxci(-ca|-us)?\.ovh)(?::\d+)?)|data:image)(?:\/|$)/i,
 };
 
 export default {
   HOME_PAGE,
+  OVH_LOGO,
   SANITIZATION,
 };
