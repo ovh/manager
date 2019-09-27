4a68527c3c6f25d245768edd95f21b0b034ee2b2
Marie JONES <marie.jones@corp.ovh.com>
2019-09-27 15:15:15 +0200

    refactor: remove unecessary stateparams

--- packages/manager/apps/telecom/src/app/error-page/error-page.routing.js
+++ packages/manager/apps/telecom/src/app/error-page/error-page.routing.js
@@ -1,16 +1,7 @@
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('telecomError', {
     params: {
-      code: {
-        type: 'any',
-      },
-      context: {
-        type: 'any',
-      },
       detail: null,
-      message: {
-        type: 'any',
-      },
     },
     url: '/error',
     views: {
@@ -18,15 +9,7 @@ export default /* @ngInject */ ($stateProvider) => {
     },
     resolve: {
       cancelLink: /* @ngInject */ $state => $state.href('telecom'),
-      error: /* @ngInject */ ($transition$) => {
-        const stateParams = $transition$.params();
-        const error = {
-          ...stateParams,
-          code: stateParams.code,
-        };
-
-        return error;
-      },
+      error: /* @ngInject */ $transition$ => $transition$.params(),
       submitAction: /* @ngInject */ $window => () => $window.location.reload(),
       translationsRefresh: /* @ngInject */ $translate => $translate.refresh(),
     },
