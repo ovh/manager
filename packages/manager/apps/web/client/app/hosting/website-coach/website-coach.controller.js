c807e889c4e13581fefaa2e23589af1782b4f6ba
Axel Peter <axel.peter@live.com>
2019-10-02 11:35:32 +0200

    fix: code review

--- packages/manager/apps/web/client/app/hosting/website-coach/website-coach.controller.js
+++ packages/manager/apps/web/client/app/hosting/website-coach/website-coach.controller.js
@@ -1,7 +1,8 @@
-import { VISUALIZER_IMG_SRC } from './website-coach.constant';
+import { USER_INFOS_URL, VISUALIZER_IMG_SRC } from './website-coach.constant';
 
 export default class {
   constructor() {
+    this.USER_INFOS_URL = USER_INFOS_URL;
     this.VISUALIZER_IMG_SRC = VISUALIZER_IMG_SRC;
   }
 
