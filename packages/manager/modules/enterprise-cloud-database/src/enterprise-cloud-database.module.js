b05b565ba749748b4ac6276c3659d6eb96cb7b5f
Ganesh Kumar <ganesh.kumar@corp.ovh.com>
2019-10-09 16:25:50 +0530

    fix(enterprise-cloud-database): cleanup and mock data removal

--- packages/manager/modules/enterprise-cloud-database/src/enterprise-cloud-database.module.js
+++ packages/manager/modules/enterprise-cloud-database/src/enterprise-cloud-database.module.js
@@ -5,7 +5,6 @@ import paymentInfoComponent from './payment-info';
 import create from './create';
 import enterpriseCloudDatabaseComponent from './enterprise-cloud-database.component';
 import enterpriseCloudDatabaseService from './enterprise-cloud-database.service';
-import mockData from './mock-data';
 import priceComponent from './price';
 import routing from './enterprise-cloud-database.routing';
 import service from './service';
@@ -22,7 +21,6 @@ angular
   .config(routing)
   .component('enterpriseCloudDatabaseComponent', enterpriseCloudDatabaseComponent)
   .service('enterpriseCloudDatabaseService', enterpriseCloudDatabaseService)
-  .service('mockData', mockData)
   .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
