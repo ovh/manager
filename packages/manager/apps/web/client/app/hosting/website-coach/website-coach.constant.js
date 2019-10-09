86316aa5ad41b7105caef36946bbb6e52a040686
Axel Peter <15101925+AxelPeter@users.noreply.github.com>
2019-10-09 12:43:04 +0200

    feat(hosting.website-coach): add website coach page (#1390)
    ref: MBP-598

---
+++ packages/manager/apps/web/client/app/hosting/website-coach/website-coach.constant.js
@@ -0,0 +1,7 @@
+export const USER_INFOS_URL = '#/useraccount/infos';
+export const VISUALIZER_IMG_SRC = 'assets/images/website-coach-browser.png';
+
+export default {
+  USER_INFOS_URL,
+  VISUALIZER_IMG_SRC,
+};
