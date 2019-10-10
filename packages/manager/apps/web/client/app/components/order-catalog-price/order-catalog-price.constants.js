68375ae774c92fbb4ab4dca5c7ed1c4ea0b69f2a
Axel Peter <axel.peter@live.com>
2019-10-10 15:13:40 +0200

    refactor(hosting): replace wuc-ovh-tabs with oui-header-tabs

--- packages/manager/apps/web/client/app/components/order-catalog-price/order-catalog-price.constants.js
+++ packages/manager/apps/web/client/app/components/order-catalog-price/order-catalog-price.constants.js
@@ -2,10 +2,17 @@ export const ASIA_FORMAT = ['SG', 'ASIA', 'AU'];
 export const FRENCH_FORMAT = ['CZ', 'ES', 'FR', 'GB', 'IE', 'IT', 'LT', 'MA', 'NL', 'PL', 'PT', 'TN'];
 export const GERMAN_FORMAT = ['DE', 'FI', 'SN'];
 export const US_FORMAT = ['CA', 'WE', 'WS', 'QC', 'US'];
+export const INTERVAL_UNIT = {
+  DAY: 'day',
+  MONTH: 'month',
+  NONE: 'none',
+  YEAR: 'year',
+};
 
 export default {
   ASIA_FORMAT,
   FRENCH_FORMAT,
   GERMAN_FORMAT,
   US_FORMAT,
+  INTERVAL_UNIT,
 };
