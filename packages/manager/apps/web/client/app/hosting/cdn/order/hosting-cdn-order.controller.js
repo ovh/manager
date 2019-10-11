83d21bfe841cde9b4bf25a93f8cd0eabd65fb003
Axel Peter <axel.peter@live.com>
2019-10-11 16:52:05 +0200

    refactor(hosting.cdn): update cdn to new pattern
    Signed-off-by: Axel Peter <axel.peter@live.com>

--- packages/manager/apps/web/client/app/hosting/cdn/order/hosting-cdn-order.controller.js
+++ packages/manager/apps/web/client/app/hosting/cdn/order/hosting-cdn-order.controller.js
@@ -1,140 +1,146 @@
 import get from 'lodash/get';
 
-angular
-  .module('App')
-  .controller(
-    'HostingOrderCdnCtrl',
-    ($scope, $q, $translate, $window, HostingOptionOrder, Hosting, Alerter, $rootScope) => {
-      $scope.loading = {
-        init: true,
-        duration: true,
-        model: false,
-        details: false,
-        order: false,
-      };
-      $scope.model = {
-        offer: null,
-        duration: null,
-        contract: null,
-      };
-      const hosting = $scope.currentActionData;
+export default class {
+  /* @ngInject */
+  constructor($scope, $rootScope, $q, $translate, $window, HostingOptionOrder, Hosting, Alerter) {
+    this.$scope = $scope;
+    this.$rootScope = $rootScope;
+    this.$q = $q;
+    this.$translate = $translate;
+    this.$window = $window;
+    this.HostingOptionOrder = HostingOptionOrder;
+    this.Hosting = Hosting;
+    this.Alerter = Alerter;
+  }
 
-      $scope.isOrderable = false;
-      $scope.availableOffers = [];
-      $scope.durations = [];
-      $scope.details = {};
+  $onInit() {
+    this.loading = {
+      init: true,
+      duration: true,
+      model: false,
+      details: false,
+      order: false,
+    };
 
-      $scope.loadOrder = () => {
-        $scope.loading.init = true;
-        HostingOptionOrder.isOptionOrderable('cdn').then((isOrderable) => {
-          if (isOrderable) {
-            $scope.isOrderable = true;
-          }
-          $scope.loading.model = true;
-          HostingOptionOrder.getOrderEnums('hosting.web.CdnOfferEnum').then((models) => {
-            if (!$scope.isPerfOffer() && !$scope.hosting.isCloudWeb) {
-              $scope.availableOffers = models.filter(offer => offer !== 'CDN_BUSINESS_FREE');
-            } else {
-              $scope.availableOffers = ['CDN_BUSINESS_FREE'];
-            }
+    this.model = {
+      offer: null,
+      duration: null,
+      contract: null,
+    };
 
-            [$scope.model.offer] = $scope.availableOffers;
-            $scope.loading.model = false;
-            $scope.loading.init = false;
+    this.isOrderable = false;
+    this.availableOffers = [];
+    this.durations = [];
+    this.details = {};
+  }
 
-            if ($scope.availableOffers.length === 1) {
-              $rootScope.$broadcast('wizard-goToStep', 3);
-              $scope.getDuration();
-            }
-          });
-        });
-      };
-
-      $scope.isPerfOffer = () => Hosting.constructor.isPerfOffer((hosting
-        || $scope.hostingProxy).offer);
-
-      $scope.getDuration = () => {
-        const queue = [];
-        $scope.loading.duration = true;
-
-        HostingOptionOrder.getOrderDurations('cdn', {
-          offer: $scope.model.offer,
-        })
-          .then((durations) => {
-            $scope.durations = durations;
-
-            if ($scope.durations.length === 1) {
-              [$scope.model.duration] = $scope.durations;
-            }
-
-            $scope.loading.details = true;
-            angular.forEach($scope.durations, (duration) => {
-              queue.push(HostingOptionOrder.getOrderDetailsForDuration('cdn', duration, {
-                offer: $scope.model.offer,
-              }).then((details) => {
-                $scope.details[duration] = details;
-              }));
-            });
-            $q.all(queue).then(() => {
-              $scope.loading.details = false;
-            });
-            $scope.loading.duration = false;
-          })
-          .catch((err) => {
-            Alerter.alertFromSWS(
-              $translate.instant('hosting_dashboard_cdn_order_error'),
-              get(err, 'data', err),
-              $scope.alerts.main,
-            );
-            $scope.resetAction();
-          });
-      };
-
-      $scope.isStepValid = (step) => {
-        switch (step) {
-          case 1:
-            return (
-              !$scope.loading.init && $scope.isOrderable && $scope.model.offer
-            );
-          case 2:
-            return (
-              $scope.model.offer
-              && $scope.model.duration
-              && !$scope.loading.details
-            );
-          case 3:
-            return (
-              $scope.model.offer
-              && $scope.model.duration
-              && $scope.model.contract
-            );
-          default:
-            return null;
+  loadOrder() {
+    this.loading.init = true;
+    this.HostingOptionOrder.isOptionOrderable('cdn').then((isOrderable) => {
+      if (isOrderable) {
+        this.isOrderable = true;
+      }
+      this.loading.model = true;
+      this.HostingOptionOrder.getOrderEnums('hosting.web.CdnOfferEnum').then((models) => {
+        if (!this.isPerfOffer() && !this.hosting.isCloudWeb) {
+          this.availableOffers = models.filter(offer => offer !== 'CDN_BUSINESS_FREE');
+        } else {
+          this.availableOffers = ['CDN_BUSINESS_FREE'];
         }
-      };
 
-      $scope.makeOrder = () => {
-        $scope.loading.order = true;
-        HostingOptionOrder.makeOrder('cdn', $scope.model.duration, {
-          offer: $scope.model.offer,
-        })
-          .then((order) => {
-            Alerter.success(
-              $translate.instant('hosting_dashboard_cdn_order_success', { t0: order.url }),
-              $scope.alerts.main,
-            );
-            $window.open(order.url, '_blank');
-          })
-          .catch((err) => {
-            Alerter.alertFromSWS(
-              $translate.instant('hosting_dashboard_cdn_order_error'),
-              get(err, 'data', err),
-              $scope.alerts.main,
-            );
-          })
-          .finally(() => {
-            $scope.resetAction();
-          });
-      };
-    },
-  );
+        [this.model.offer] = this.availableOffers;
+        this.loading.model = false;
+        this.loading.init = false;
+
+        if (this.availableOffers.length === 1) {
+          this.$rootScope.$broadcast('wizard-goToStep', 3);
+          this.getDuration();
+        }
+      });
+    });
+  }
+
+  getDuration() {
+    const queue = [];
+    this.loading.duration = true;
+
+    this.HostingOptionOrder.getOrderDurations('cdn', {
+      offer: this.model.offer,
+    })
+      .then((durations) => {
+        this.durations = durations;
+
+        if (this.durations.length === 1) {
+          [this.model.duration] = this.durations;
+        }
+
+        this.loading.details = true;
+        angular.forEach(this.durations, (duration) => {
+          queue.push(this.HostingOptionOrder.getOrderDetailsForDuration('cdn', duration, {
+            offer: this.model.offer,
+          }).then((details) => {
+            this.details[duration] = details;
+          }));
+        });
+        this.$q.all(queue).then(() => {
+          this.loading.details = false;
+        });
+        this.loading.duration = false;
+      })
+      .catch((err) => {
+        this.Alerter.alertFromSWS(
+          this.$translate.instant('hosting_dashboard_cdn_order_error'),
+          get(err, 'data', err),
+          this.$scope.alerts.main,
+        );
+        this.goBack();
+      });
+  }
+
+  isStepValid(step) {
+    switch (step) {
+      case 1:
+        return (
+          !this.loading.init && this.isOrderable && this.model.offer
+        );
+      case 2:
+        return (
+          this.model.offer
+          && this.model.duration
+          && !this.loading.details
+        );
+      case 3:
+        return (
+          this.model.offer
+          && this.model.duration
+          && this.model.contract
+        );
+      default:
+        return null;
+    }
+  }
+
+  makeOrder() {
+    this.loading.order = true;
+    this.HostingOptionOrder.makeOrder('cdn', this.model.duration, {
+      offer: this.model.offer,
+    })
+      .then((order) => {
+        this.Alerter.success(
+          this.$translate.instant('hosting_dashboard_cdn_order_success', { t0: order.url }),
+          this.$scope.alerts.main,
+        );
+        this.$window.open(order.url, '_blank');
+      })
+      .catch((err) => {
+        this.Alerter.alertFromSWS(
+          this.$translate.instant('hosting_dashboard_cdn_order_error'),
+          get(err, 'data', err),
+          this.$scope.alerts.main,
+        );
+      })
+      .finally(() => {
+        this.goBack();
+      });
+  }
+}
