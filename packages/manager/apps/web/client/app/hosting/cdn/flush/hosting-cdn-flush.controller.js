9c3465c185f66aa646cf021accc96e2129620362
Axel Peter <axel.peter@live.com>
2019-10-11 16:52:05 +0200

    refactor(hosting.cdn): update cdn to new pattern
    Signed-off-by: Axel Peter <axel.peter@live.com>

--- packages/manager/apps/web/client/app/hosting/cdn/flush/hosting-cdn-flush.controller.js
+++ packages/manager/apps/web/client/app/hosting/cdn/flush/hosting-cdn-flush.controller.js
@@ -1,41 +1,39 @@
 import get from 'lodash/get';
 
-angular.module('App').controller(
-  'HostingFlushCdnCtrl',
-  class HostingFlushCdnCtrl {
-    constructor($scope, $rootScope, $stateParams, $translate, Hosting, Alerter) {
-      this.$scope = $scope;
-      this.$rootScope = $rootScope;
-      this.$stateParams = $stateParams;
-      this.$translate = $translate;
-      this.Hosting = Hosting;
-      this.Alerter = Alerter;
-    }
+export default class HostingFlushCdnCtrl {
+  /* @ngInject */
+  constructor($scope, $rootScope, $stateParams, $translate, Hosting, Alerter) {
+    this.$scope = $scope;
+    this.$rootScope = $rootScope;
+    this.$stateParams = $stateParams;
+    this.$translate = $translate;
+    this.Hosting = Hosting;
+    this.Alerter = Alerter;
+  }
 
-    $onInit() {
-      this.loading = false;
-      this.$scope.flushCdn = () => this.flushCdn();
-    }
+  $onInit() {
+    this.loading = false;
+    this.$scope.flushCdn = () => this.flushCdn();
+  }
 
-    flushCdn() {
-      this.Hosting.flushCdn(this.$stateParams.productId)
-        .then(() => {
-          this.Alerter.success(
-            this.$translate.instant('hosting_dashboard_cdn_flush_success'),
-            this.$scope.alerts.main,
-          );
-          this.$rootScope.$broadcast('hosting.cdn.flush.refresh');
-        })
-        .catch((err) => {
-          this.Alerter.alertFromSWS(
-            this.$translate.instant('hosting_dashboard_cdn_flush_error'),
-            get(err, 'data', err),
-            this.$scope.alerts.main,
-          );
-        })
-        .finally(() => {
-          this.$scope.resetAction();
-        });
-    }
-  },
-);
+  flushCdn() {
+    this.Hosting.flushCdn(this.$stateParams.productId)
+      .then(() => {
+        this.Alerter.success(
+          this.$translate.instant('hosting_dashboard_cdn_flush_success'),
+          this.$scope.alerts.main,
+        );
+        this.$rootScope.$broadcast('hosting.cdn.flush.refresh');
+      })
+      .catch((err) => {
+        this.Alerter.alertFromSWS(
+          this.$translate.instant('hosting_dashboard_cdn_flush_error'),
+          get(err, 'data', err),
+          this.$scope.alerts.main,
+        );
+      })
+      .finally(() => {
+        this.$scope.resetAction();
+      });
+  }
+}
