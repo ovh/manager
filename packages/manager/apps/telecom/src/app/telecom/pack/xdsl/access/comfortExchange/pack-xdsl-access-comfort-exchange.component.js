a54d5c03e258ca648f47aad93b71fd3c184920c3
Stéphanie MOALLIC <stephanie.moallic@gmail.com>
2019-10-17 09:27:01 +0200

    feat(pack.xdsl): add access exchange for modem (#1346)
    ref: UXCT-187

---
+++ packages/manager/apps/telecom/src/app/telecom/pack/xdsl/access/comfortExchange/pack-xdsl-access-comfort-exchange.component.js
@@ -0,0 +1,11 @@
+import controller from './pack-xdsl-access-comfort-exchange.controller';
+import template from './pack-xdsl-access-comfort-exchange.html';
+
+export default {
+  bindings: {
+    xdslId: '<',
+    openedRMAs: '<',
+  },
+  controller,
+  template,
+};
