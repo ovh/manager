8832896229a9e8bbaea7adc873f08e29335eb347
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-10-24 14:51:28 +0200

    feat(public-cloud): remove references to otrs

--- packages/manager/apps/public-cloud/src/index.constants.js
+++ packages/manager/apps/public-cloud/src/index.constants.js
@@ -1,12 +1,5 @@
 export const DEFAULT_PROJECT_KEY = 'PUBLIC_CLOUD_DEFAULT_PROJECT';
 
-export const OTRS_URLS = {
-  CA: 'https://ca.ovh.com/manager/index.html#/support/tickets',
-  EU: 'https://www.ovh.com/manager/dedicated/index.html#/support/tickets',
-  US: 'https://us.ovhcloud.com/manager/dedicated/index.html#/support/tickets',
-};
-
 export default {
   DEFAULT_PROJECT_KEY,
-  OTRS_URLS,
 };
