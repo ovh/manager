98d8633e6ba40607527941849feed4b4fef75117
Antoine Leblanc <ant.leblanc@gmail.com>
2019-10-25 18:08:11 +0200

    build(rollup): improve configuration
    Signed-off-by: Antoine Leblanc <ant.leblanc@gmail.com>

--- packages/components/ng-ovh-sso-auth-modal-plugin/rollup.config.js
+++ packages/components/ng-ovh-sso-auth-modal-plugin/rollup.config.js
@@ -1,18 +1,23 @@
 import rollupConfig from '@ovh-ux/component-rollup-config';
 
 const config = rollupConfig({
-  input: 'src/index.js',
+  input: './src/index.js',
 });
 
-export default [
+const outputs = [
   config.es(),
-  config.cjs(),
-  config.umd({
+];
+
+if (process.env.BUILD === 'production') {
+  outputs.push(config.cjs());
+  outputs.push(config.umd({
     output: {
       globals: {
         angular: 'angular',
         jquery: '$',
       },
     },
-  }),
-];
+  }));
+}
+
+export default outputs;
