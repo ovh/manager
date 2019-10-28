4ea11053d5650d4d84cc25523ed174cf74ef1d67
Antoine Leblanc <ant.leblanc@gmail.com>
2019-10-28 20:47:22 +0100

    feat: init @ovh-ux/sao-ovh-manager-app module
    ref: MANAGER-3820
    
    Signed-off-by: Antoine Leblanc <antoine.leblanc@corp.ovh.com>
    
    BREAKING CHANGE: init module @ovh-ux/sao-ovh-manager-app

---
+++ packages/manager/tools/sao-ovh-manager-app/template/src/index.js
@@ -0,0 +1,9 @@
+<% const pascalcasedName = this.camelcase(name, { pascalCase: true }) -%>import 'script-loader!jquery'; // eslint-disable-line
+import '@ovh-ux/manager-<%= name %>';
+
+import angular from 'angular';
+
+angular
+  .module('<%= name %>App', [
+    'ovhManager<%= pascalcasedName %>',
+  ]);
