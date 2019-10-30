6ff7bd46ba4055d6fe8def822f3770247b061251
Marie JONES <marie.jones@corp.ovh.com>
2019-10-30 18:19:07 +0100

    fix: import exchange module for exchange account renew
    ref: DTRSD-4820
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/billing/autoRenew/actions/exchange/exchange-renew.module.js
+++ packages/manager/apps/dedicated/client/app/billing/autoRenew/actions/exchange/exchange-renew.module.js
@@ -1,9 +1,13 @@
+import moduleExchange from '@ovh-ux/manager-exchange';
+
+
 import routing from './exchange-renew.routing';
 
 const moduleName = 'ovhManagerBillingAutorenewExchangeRenew';
 
 angular.module(moduleName, [
   'ui.router',
+  moduleExchange,
 ])
   .config(routing);
 
