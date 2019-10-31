069b7aa5c4d6a44796867ef8b94ab4877827db98
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2019-10-31 15:20:25 +0100

    refactor(mfa-enrollment): use @ovh-ux/mfa-enrollment module
    ref: MANAGER-3453
    
    Signed-off-by: Antoine Leblanc <antoine.leblanc@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/app.routes.js
+++ packages/manager/apps/dedicated/client/app/app.routes.js
@@ -1,6 +1,5 @@
 import UserModel from './user/User.class';
 
-let alreadyShowMFA = false;
 angular
   .module('App')
   .config(/* @ngInject */ ($stateProvider) => {
@@ -9,23 +8,30 @@ angular
       resolve: {
         currentUser: /* @ngInject */ User => User.getUser()
           .then(user => new UserModel(user)),
-        enrollMFA: /* @ngInject */ ($q, $state, OvhApiAuth) => OvhApiAuth.v6()
-          .shouldDisplayMFAEnrollment()
-          .$promise
-          .then((shouldDisplayMFA) => {
-            if ((shouldDisplayMFA.value === 'true' || shouldDisplayMFA.value === 'forced')
-                && !alreadyShowMFA) {
-              alreadyShowMFA = true;
-              $state.go('app.mfaEnrollment', {
-                forced: shouldDisplayMFA.value === 'forced',
-              });
-            }
-          }).catch(() => $q.resolve()),
         rootState: () => 'app.configuration',
       },
       templateUrl: 'app.html',
       translations: { value: ['common', 'double-authentication', 'user-contracts'], format: 'json' },
       url: '',
+    });
+
+    $stateProvider.state('app.mfaEnrollment', {
+      url: '/mfa-enrollment',
+      views: {
+        'app@': {
+          component: 'mfaEnrollment',
+        },
+      },
+      params: {
+        forced: {
+          dynamic: true,
+        },
+      },
+      translations: { value: ['.'], format: 'json' },
+      resolve: {
+        forced: /* @ngInject */ $transition$ => $transition$.params().forced,
+        from: /* @ngInject */ $transition$ => $transition$.$from().name,
+      },
     });
 
     // CDN & NAS
