d9e45a1544b67f59fc3fa99f0b7cad4896e07564
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-11-05 16:33:19 +0100

    feat(dedicated): add vps module
    ref: MANAGER-3758
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/app.js
+++ packages/manager/apps/dedicated/client/app/app.js
@@ -37,6 +37,7 @@ import uiRouter, { RejectType } from '@uirouter/angularjs';
 
 import moduleExchange from '@ovh-ux/manager-exchange';
 import ovhManagerVeeamEnterprise from '@ovh-ux/manager-veeam-enterprise';
+import ovhManagerVps from '@ovh-ux/manager-vps';
 import account from './account';
 import config from './config/config';
 import contactsService from './account/contacts/service/contacts-service.module';
@@ -115,6 +116,7 @@ angular
     ovhManagerBanner,
     ovhManagerEnterpriseCloudDatabase,
     ovhManagerNavbar,
+    ovhManagerVps,
     ovhPaymentMethod,
     'pascalprecht.translate',
     preload,
