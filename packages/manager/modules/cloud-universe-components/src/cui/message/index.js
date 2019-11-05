9a46b21f0e9ab14bd1ba3d3b50e450e43fa14ee4
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-11-05 11:52:20 +0100

    fix(cuc): add missing translations
    ref: MANAGER-3911
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/modules/cloud-universe-components/src/cui/message/index.js
+++ packages/manager/modules/cloud-universe-components/src/cui/message/index.js
@@ -1,13 +1,17 @@
 import angular from 'angular';
+import '@ovh-ux/ng-translate-async-loader';
+import 'angular-translate';
 
 import component from './component';
-
 
 const moduleName = 'cuiMessage';
 
 angular
   .module(moduleName, [
+    'ngTranslateAsyncLoader',
+    'pascalprecht.translate',
   ])
-  .component('cuiMessageContainer', component);
+  .component('cuiMessageContainer', component)
+  .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
