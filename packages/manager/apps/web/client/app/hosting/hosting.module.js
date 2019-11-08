68273690a6d6820d19462eb5980346d403827148
Axel Peter <axel.peter@live.com>
2019-11-08 15:48:41 +0100

    feat(hosting.database.order-private): use order cart api
    Signed-off-by: Axel Peter <axel.peter@live.com>
    Signed-off-by: Jérémy De-Cesare <jeremy.de-cesare@corp.ovh.com>

--- packages/manager/apps/web/client/app/hosting/hosting.module.js
+++ packages/manager/apps/web/client/app/hosting/hosting.module.js
@@ -1,8 +1,8 @@
 import routing from './hosting.routes';
 
 import cdn from './cdn/hosting-cdn.modules';
+import database from './database/hosting-database.module';
 import generalInformations from './general-informations/general-informations.module';
-import sqlDatabase from './sql-database/hosting-sql-database.module';
 import websiteCoach from './website-coach/website-coach.module';
 
 const moduleName = 'ovhManagerHosting';
@@ -10,10 +10,10 @@ const moduleName = 'ovhManagerHosting';
 angular
   .module(moduleName, [
     cdn,
+    database,
     generalInformations,
     'oui',
     'pascalprecht.translate',
-    sqlDatabase,
     websiteCoach,
   ])
   .config(routing);
