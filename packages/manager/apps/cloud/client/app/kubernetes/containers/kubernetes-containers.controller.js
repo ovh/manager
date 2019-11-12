91f65cee9384af5385ec656379c24c981be68f6a
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-11-12 10:40:21 +0100

    refactor(cloud): remove kubernetes sources (#1759)
    * `kubernetes` product is available through `@ovh-ux/manager-pci` module
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/cloud/client/app/kubernetes/containers/kubernetes-containers.controller.js
+++
@@ -1,5 +0,0 @@
-angular.module('managerApp').controller('KubernetesContainersCtrl', class KubernetesContainersCtrl {
-  constructor(KUBERNETES) {
-    this.KUBERNETES = KUBERNETES;
-  }
-});
--- packages/manager/apps/cloud/client/app/kubernetes/kubernetes.component.js
+++
@@ -1,8 +0,0 @@
-angular.module('managerApp')
-  .component('kubernetes', {
-    bindings: {
-      serviceName: '@',
-    },
-    templateUrl: 'app/kubernetes/kubernetes.html',
-    controller: 'KubernetesCtrl',
-  });
