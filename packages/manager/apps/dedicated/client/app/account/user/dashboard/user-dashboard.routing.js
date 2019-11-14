81aaa2740aa8f9b73c942c4428074642b1ec1bf9
Marie JONES <marie.jones@corp.ovh.com>
2019-11-14 09:21:15 +0100

    refactor(dedicated.account.user): modularize dashboard
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/account/user/dashboard/user-dashboard.routing.js
+++ packages/manager/apps/dedicated/client/app/account/user/dashboard/user-dashboard.routing.js
@@ -4,34 +4,26 @@ import {
   USER_DASHBOARD_SHORTCUTS,
 } from './user-dashboard.constants';
 
-import component from './user-dasboard.component';
-import './user-dashboard.less';
-
-angular
-  .module('UserAccount')
-  .config(/* @ngInject */ ($stateProvider) => {
-    const name = 'app.account.user.dashboard';
-
-    $stateProvider.state(name, {
-      url: '/dashboard',
-      component: 'userAccountDashboard',
-      translations: {
-        format: 'json',
-        value: ['./', '../support-level'],
-      },
-      resolve: {
-        user: /* @ngInject */ currentUser => currentUser,
-        lastBill: /* @ngInject */ OvhApiMeBillIceberg => OvhApiMeBillIceberg
-          .query()
-          .expand('CachedObjectList-Pages')
-          .sort('date', 'DESC')
-          .limit(1)
-          .execute(null, true)
-          .$promise
-          .then(lastBill => head(lastBill.data)),
-        shortcuts: /* @ngInject */ $state => USER_DASHBOARD_SHORTCUTS
-          .map(shortcut => ({ ...shortcut, href: $state.href(shortcut.state) })),
-      },
-    });
-  })
-  .component('userAccountDashboard', component);
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state('app.account.user.dashboard', {
+    url: '/dashboard',
+    component: 'userAccountDashboard',
+    translations: {
+      format: 'json',
+      value: ['./', '../support-level'],
+    },
+    resolve: {
+      user: /* @ngInject */ currentUser => currentUser,
+      lastBill: /* @ngInject */ OvhApiMeBillIceberg => OvhApiMeBillIceberg
+        .query()
+        .expand('CachedObjectList-Pages')
+        .sort('date', 'DESC')
+        .limit(1)
+        .execute(null, true)
+        .$promise
+        .then(lastBill => head(lastBill.data)),
+      shortcuts: /* @ngInject */ $state => USER_DASHBOARD_SHORTCUTS
+        .map(shortcut => ({ ...shortcut, href: $state.href(shortcut.state) })),
+    },
+  });
+};
