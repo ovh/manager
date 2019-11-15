8910996158d1e7f1defb9f52c166b7f5d1b73ecc
Marie JONES <marie.jones@corp.ovh.com>
2019-11-15 08:41:42 +0100

    refactor(dedicated.account.user.advanced): modularize section
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/account/user/advanced/user-advanced.controller.js
+++ packages/manager/apps/dedicated/client/app/account/user/advanced/user-advanced.controller.js
@@ -1,39 +1,51 @@
-angular.module('UserAccount').controller('UserAccount.controllers.advanced', [
-  'UserAccount.services.Infos',
-  'Alerter',
-  '$translate',
-  function UserAccountAdvancedController(userAccountServiceInfos, Alerter, $translate) {
-    this.isLoadingDeveloperMode = false;
+export default /* @ngInject */ function (userAccountServiceInfos, Alerter, $translate) {
+  this.isLoadingDeveloperMode = false;
 
-    this.$ngInit = () => {
-      this.isLoadingDeveloperMode = true;
-      userAccountServiceInfos
-        .getDeveloperMode()
-        .then((developmentMode) => {
-          this.developmentMode = developmentMode;
-        })
-        .finally(() => {
-          this.isLoadingDeveloperMode = false;
-        });
-    };
+  this.beta = {
+    active: this.betaFlag,
+  };
 
-    this.updateDevelopmentMode = () => {
-      this.isLoadingDeveloperMode = true;
-      const successKey = this.developmentMode.enabled ? 'user_account_advanced_section_developer_alert_success_enabled' : 'user_account_advanced_section_developer_alert_success_disabled';
-      userAccountServiceInfos
-        .updateDeveloperMode(this.developmentMode)
-        .then(() => {
-          const zone = 'useraccount.alerts.dashboardAdvanced';
-          return Alerter.success($translate.instant(successKey), zone);
-        }, () => {
-          const zone = 'useraccount.alerts.dashboardAdvanced';
-          return Alerter.error($translate.instant('user_account_advanced_section_developer_alert_error'), zone);
-        })
-        .finally(() => {
-          this.isLoadingDeveloperMode = false;
-        });
-    };
+  this.$ngInit = () => {
+    this.isLoadingDeveloperMode = true;
+    userAccountServiceInfos
+      .getDeveloperMode()
+      .then((developmentMode) => {
+        this.developmentMode = developmentMode;
+      })
+      .finally(() => {
+        this.isLoadingDeveloperMode = false;
+      });
+  };
 
-    this.$ngInit();
-  },
-]);
+  this.updateDevelopmentMode = () => {
+    this.isLoadingDeveloperMode = true;
+    const successKey = this.developmentMode.enabled ? 'user_account_advanced_section_developer_alert_success_enabled' : 'user_account_advanced_section_developer_alert_success_disabled';
+    userAccountServiceInfos
+      .updateDeveloperMode(this.developmentMode)
+      .then(() => {
+        const zone = 'useraccount.alerts.dashboardAdvanced';
+        return Alerter.success($translate.instant(successKey), zone);
+      }, () => {
+        const zone = 'useraccount.alerts.dashboardAdvanced';
+        return Alerter.error($translate.instant('user_account_advanced_section_developer_alert_error'), zone);
+      })
+      .finally(() => {
+        this.isLoadingDeveloperMode = false;
+      });
+  };
+
+  this.onBetaChange = (beta) => {
+    this.isUpdatingBeta = true;
+    this.updateBeta(beta)
+      .then(() => {
+        this.beta.active = beta;
+        Alerter.success('Beta Update successful', 'useraccount.alerts.dashboardAdvanced');
+      })
+      .catch(() => Alerter.error('Beta Update failed', 'useraccount.alerts.dashboardAdvanced'))
+      .finally(() => {
+        this.isUpdatingBeta = false;
+      });
+  };
+
+  this.$ngInit();
+}
