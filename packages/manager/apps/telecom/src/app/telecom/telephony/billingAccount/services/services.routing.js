230df70ce418749617cb594161a346ad14febe22
Marie JONES <marie.jones@corp.ovh.com>
2019-11-15 17:30:24 +0100

    feat(telecom.telephony): upgrade datagrid with data, filters and sorting
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/telecom/src/app/telecom/telephony/billingAccount/services/services.routing.js
+++ packages/manager/apps/telecom/src/app/telecom/telephony/billingAccount/services/services.routing.js
@@ -108,6 +108,28 @@ export default /* @ngInject */ ($stateProvider) => {
       ),
       sort: /* @ngInject */ resources => get(resources.headers, 'x-pagination-sort'),
       sortOrder: /* @ngInject */ resources => get(resources.headers, 'x-pagination-sort-order'),
+      schema: /* @ngInject */ OvhApiTelephony => OvhApiTelephony
+        .v6()
+        .schema()
+        .$promise,
+      telephonyFeatureTypes: /* @ngInject */ schema => get(schema.models, 'telephony.TypeEnum').enum,
+      telephonyServiceTypes: /* @ngInject */ schema => get(schema.models, 'telephony.TypeServiceEnum').enum,
+
+      getServiceLink: /* @ngInject */ $state => (service) => {
+        if (['alias'].includes(service.serviceType)) {
+          return $state.href('telecom.telephony.billingAccount.alias', { serviceName: service.serviceName });
+        }
+
+        if (['fax', 'voicefax'].includes(service.featureType)) {
+          return $state.href('telecom.telephony.billingAccount.fax', { serviceName: service.serviceName });
+        }
+
+        if (['carrierSip'].includes(service.featureType)) {
+          return $state.href('telecom.telephony.billingAccount.carrierSip', { serviceName: service.serviceName });
+        }
+
+        return $state.href('telecom.telephony.billingAccount.line', { serviceName: service.serviceName });
+      },
     },
   });
 };
