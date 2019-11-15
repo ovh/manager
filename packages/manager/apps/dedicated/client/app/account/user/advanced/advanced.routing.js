18d8ec7b496940e9abc3c00931916008cbb4df2c
Marie JONES <marie.jones@corp.ovh.com>
2019-11-15 08:41:42 +0100

    refactor(dedicated.account.user.advanced): modularize section
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/account/user/advanced/advanced.routing.js
+++ packages/manager/apps/dedicated/client/app/account/user/advanced/advanced.routing.js
@@ -1,12 +1,7 @@
-angular
-  .module('UserAccount')
-  .config(/* @ngInject */($stateProvider) => {
-    const name = 'app.account.user.advanced';
-    $stateProvider.state(name, {
-      url: '/advanced',
-      templateUrl: 'account/user/advanced/user-advanced.html',
-      controller: 'UserAccount.controllers.advanced',
-      controllerAs: 'advancedCtrl',
-      translations: ['../'],
-    });
+export default /* @ngInject */($stateProvider) => {
+  $stateProvider.state('app.account.user.advanced', {
+    url: '/advanced',
+    component: 'accountUserAdvanced',
+    translations: ['../'],
   });
+};
