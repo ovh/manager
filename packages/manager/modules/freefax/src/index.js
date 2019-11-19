45ffa685b392b9a9a004e952391642dc69b4d351
Marie JONES <marie.jones@corp.ovh.com>
2019-11-19 17:25:47 +0100

    refactor(freefax): move files according to url
    This will allow to add subroute under /freefax
    
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/freefax/src/index.js
+++ packages/manager/modules/freefax/src/index.js
@@ -4,24 +4,27 @@ import 'oclazyload';
 
 import '@ovh-ux/manager-core';
 
-const moduleName = 'ovhManagerFreeFax';
+import freefax from './freefax';
+
+const moduleName = 'ovhManagerFreeFaxes';
 
 angular
   .module(moduleName, [
     'ui.router',
     'ovhManagerCore',
     'oc.lazyLoad',
-  ])
-  .config(/* @ngInject */($stateProvider) => {
-    $stateProvider.state('freefax.**', {
-      url: '/freefax/:serviceName',
-      lazyLoad: ($transition$) => {
-        const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+    freefax,
+  ]);
+// .config(/* @ngInject */($stateProvider) => {
+//   $stateProvider.state('freefax.**', {
+//     url: '/freefax/:serviceName',
+//     lazyLoad: ($transition$) => {
+//       const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
 
-        return import('./freefax.component')
-          .then(mod => $ocLazyLoad.inject(mod.default || mod));
-      },
-    });
-  });
+//       return import('./freefax.component')
+//         .then(mod => $ocLazyLoad.inject(mod.default || mod));
+//     },
+//   });
+// });
 
 export default moduleName;
