29facb72189363d21002b4de8b384ee1752f9467
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-11-20 09:52:05 +0100

    feat(telecom.pack): add packs list for beta
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

---
+++ packages/manager/apps/telecom/src/app/telecom/pack/internet-access/packs/packs.routing.js
@@ -0,0 +1,37 @@
+import { ListLayoutHelper } from '@ovh-ux/ng-ovh-telecom-universe-components';
+
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state('telecom.packs.internet-access.packs', {
+    url: `?${ListLayoutHelper.urlQueryParams}`,
+    params: ListLayoutHelper.stateParams,
+    component: 'telecomPackInternetAccessPacks',
+    resolve: {
+      apiPath: () => '/pack/xdsl',
+      ...ListLayoutHelper.stateResolves,
+      loadResource: /* @ngInject */ OvhApiPackXdsl => resource => OvhApiPackXdsl
+        .Access()
+        .v6()
+        .getServices({
+          packId: resource.packName,
+        })
+        .$promise
+        .then(services => ({
+          ...resource,
+          numServices: services.length,
+        }))
+        .catch(() => resource),
+      getPackLink: /* @ngInject */ $state => ({ packName }) => $state.href(
+        'telecom.packs.pack',
+        {
+          packName,
+        },
+      ),
+      getPackServicesLink: /* @ngInject */ $state => ({ packName }) => $state.href(
+        'telecom.packs.pack',
+        {
+          packName,
+        },
+      ),
+    },
+  });
+};
