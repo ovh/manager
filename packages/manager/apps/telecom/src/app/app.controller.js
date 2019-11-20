b32a2de8b9fac2dfd81f311679bc030aaed994ee
Marie JONES <marie.jones@corp.ovh.com>
2019-11-20 18:35:16 +0100

    feat(telecom): add globals search for beta accounts
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/telecom/src/app/app.controller.js
+++ packages/manager/apps/telecom/src/app/app.controller.js
@@ -1,8 +1,24 @@
 export default class TelecomAppCtrl {
   /* @ngInject */
-  constructor($transitions) {
+  constructor($q, $state, $transitions, ovhUserPref) {
     this.displayFallbackMenu = false;
     $transitions.onStart({}, () => this.closeSidebar());
+
+    this.$q = $q;
+    this.$state = $state;
+    this.ovhUserPref = ovhUserPref;
+  }
+
+  $onInit() {
+    const beta = localStorage.getItem('ACCOUNT_BETA_FEATURES');
+    return (beta ? this.$q.resolve(beta)
+      : this.ovhUserPref.getValue('ACCOUNT_BETA_FEATURES'))
+      .then(() => {
+        this.globalSearchLink = this.$state.href('telecomSearch', {});
+      })
+      .catch(() => {
+        this.globalSearchLink = null;
+      });
   }
 
   openSidebar() {
