ca43a2a5a3b7c0e15493815cc138e11e78030820
jleveugle <joffrey.leveugle@corp.ovh.com>
2019-11-20 12:03:54 +0100

    feat: add search engine
    Signed-off-by: jleveugle <joffrey.leveugle@corp.ovh.com>

---
+++ packages/manager/apps/telecom/src/app/search/search.routing.js
@@ -0,0 +1,36 @@
+import controller from './search.controller';
+import template from './search.html';
+
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state('telecomSearch', {
+    url: '/search?q',
+    controller,
+    controllerAs: '$ctrl',
+    template,
+    translations: {
+      format: 'json',
+      value: ['.'],
+    },
+    params: {
+      q: {
+        value: null,
+        squash: true,
+      },
+    },
+    resolve: {
+      query: $transition$ => $transition$.params().q,
+      services: (apiv7, query) => apiv7('/telephony/*/service?$aggreg=1')
+        .query()
+        .addFilter('serviceName', 'like', [`%${query}%`])
+        .execute()
+        .$promise,
+      billingAccount: (query, iceberg) => iceberg('/telephony')
+        .query()
+        .expand('CachedObjectList-Pages')
+        .addFilter('billingAccount', 'like', [`%25${query}%25`])
+        .execute()
+        .$promise
+        .then(({ data }) => data),
+    },
+  });
+};
