835e28d8d6ffdd0fc7ed8d4ad45e5efd85acee07
Marie JONES <marie.jones@corp.ovh.com>
2019-11-20 10:21:16 +0100

    feat(freefax): add freefax listing view
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/freefax/src/index.js
+++ packages/manager/modules/freefax/src/index.js
@@ -4,6 +4,9 @@ import 'oclazyload';
 
 import '@ovh-ux/manager-core';
 
+import { ListLayoutHelper } from '@ovh-ux/ng-ovh-telecom-universe-components';
+
+import component from './freefaxes.component';
 import freefax from './freefax';
 
 const moduleName = 'ovhManagerFreeFaxes';
@@ -20,6 +23,19 @@ angular
       url: '/freefax',
       abstract: true,
     });
-  });
+
+    $stateProvider.state('freefaxes.index', {
+      url: `?${ListLayoutHelper.urlQueryParams}`,
+      component: 'ovhManagerFreefaxes',
+      params: ListLayoutHelper.stateParams,
+      resolve: {
+        apiPath: () => '/freefax',
+        ...ListLayoutHelper.stateResolves,
+        getFreefaxLink: /* @ngInject */ $state => fax => $state.href('freefaxes.freefax', { serviceName: fax.number }),
+      },
+    });
+  })
+  .component('ovhManagerFreefaxes', component)
+  .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
