dcd5563ddded18f1285b47bbdea54314c573991e
Marie JONES <marie.jones@corp.ovh.com>
2019-11-20 15:54:48 +0100

    refactor(telecom.telephony): extract tab links through the routing
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/telecom/src/app/telecom/telephony/billingAccount/billingAccount.routing.js
+++ packages/manager/apps/telecom/src/app/telecom/telephony/billingAccount/billingAccount.routing.js
@@ -31,6 +31,18 @@ export default /* @ngInject */ ($stateProvider) => {
         }).$promise.then(data => $translate.instant('telephony_page_title', { name: data.description || $stateParams.billingAccount }, null, null, 'escape')).catch(() => $translate('telephony_page_title', { name: $stateParams.billingAccount }));
       },
       isBetaActive: /* @ngInject */ betaPreferenceService => betaPreferenceService.isBetaActive(),
+      currentActiveLink: /* @ngInject */ ($transition$, $state) => () => $state
+        .href($state.current.name, $transition$.params()),
+
+      billingAccountLink: /* @ngInject */ ($state, billingAccountId) => $state.href('telecom.telephony.billingAccount', { billingAccount: billingAccountId }),
+      servicesLink: /* @ngInject */ ($state, billingAccountId) => $state.href('telecom.telephony.billingAccount.services', { billingAccount: billingAccountId }),
+      administrationLink: /* @ngInject */ ($state, billingAccountId) => $state.href('telecom.telephony.billingAccount.administration', { billingAccount: billingAccountId }),
+      billingLink: /* @ngInject */ ($state, billingAccountId) => $state.href('telecom.telephony.billingAccount.billing', { billingAccount: billingAccountId }),
+      orderAliasLink: /* @ngInject */ ($state, billingAccountId) => $state.href('telecom.telephony.billingAccount.orderAlias', { billingAccount: billingAccountId }),
+      phonebookLink: /* @ngInject */ ($state, billingAccountId) => $state.href('telecom.telephony.billingAccount.phonebook', { billingAccount: billingAccountId }),
+      abbreviatedNumbersLink: /* @ngInject */ ($state, billingAccountId) => $state.href('telecom.telephony.billingAccount.abbreviatedNumbers', { billingAccount: billingAccountId }),
+      manageContactsLink: /* @ngInject */ ($state, billingAccountId) => $state.href('telecom.telephony.billingAccount.manageContacts', { billingAccount: billingAccountId }),
+      guidesLink: /* @ngInject */ ($state, billingAccountId) => $state.href('telecom.telephony.billingAccount.guides', { billingAccount: billingAccountId }),
     },
     translations: { value: ['..', '.', './dashboard'], format: 'json' },
   });
