2892154e0ac0f1375b07e567fdc6b035316db088
jleveugle <joffrey.leveugle@corp.ovh.com>
2019-11-21 09:45:11 +0100

    build: raise up limit for listeners and kill process if error

--- scripts/worker/build_module.js
+++ scripts/worker/build_module.js
@@ -3,4 +3,5 @@ const execa = require('execa');
 
 execa
   .command(`cd ${workerData.location} && npm run build --if-present`, { shell: true })
-  .then(() => parentPort.postMessage(`done ${workerData.name}`));
+  .then(() => parentPort.postMessage(`done - ${workerData.name}`))
+  .catch(err => parentPort.postMessage(`error - ${err}`));
