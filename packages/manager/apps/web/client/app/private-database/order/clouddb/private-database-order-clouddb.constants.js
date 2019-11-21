87c77bdd3e5340a11d1acdd1de923a2f1f87ccec
Jérémy De-Cesare <jeremy.de-cesare@corp.ovh.com>
2019-11-21 17:11:34 +0100

    feat(clouddb): rework order cloud db feature
    Signed-off-by: Jérémy De-Cesare <jeremy.de-cesare@corp.ovh.com>

---
+++ packages/manager/apps/web/client/app/private-database/order/clouddb/private-database-order-clouddb.constants.js
@@ -0,0 +1,11 @@
+export const DATACENTER_CONFIGURATION_KEY = 'dc';
+export const ENGINE_CONFIGURATION_KEY = 'engine';
+export const PLAN_CODE_TEMPLATE = 'cloud-db-xxxx-instance';
+export const PRODUCT_NAME = 'cloudDB';
+
+export default {
+  DATACENTER_CONFIGURATION_KEY,
+  ENGINE_CONFIGURATION_KEY,
+  PLAN_CODE_TEMPLATE,
+  PRODUCT_NAME,
+};
