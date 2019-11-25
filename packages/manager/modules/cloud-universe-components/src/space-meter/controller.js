76d7651ac277699506a386920b50ef424f2ee953
varun257 <varun257@gmail.com>
2019-11-25 12:23:53 +0530

    feat(cloud-universe-components): replace uib-progress by oui-progress
    Signed-off-by: Varun <varun.shivaprasad@corp.ovh.com>

---
+++ packages/manager/modules/cloud-universe-components/src/space-meter/controller.js
@@ -0,0 +1,11 @@
+import has from 'lodash/has';
+
+export default class CucConfirmationModalController {
+  /* @ngInject */
+  getMaxSize() {
+    if (!has(this.usage, 'size.value')) {
+      return null;
+    }
+    return parseInt(this.usage.size.value, 10);
+  }
+}
