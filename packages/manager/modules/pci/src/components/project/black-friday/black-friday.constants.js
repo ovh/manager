bf6d22a75e3e9d5d739e1b0855b0d4db2b6d26bd
frenauvh <florian.renaut@corp.ovh.com>
2019-11-26 15:54:54 +0100

    fix(pci): black friday subsidiaries
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

---
+++ packages/manager/modules/pci/src/components/project/black-friday/black-friday.constants.js
@@ -0,0 +1,24 @@
+export const SUBSIDIARIES = [
+  'FR',
+  'ES',
+  'IT',
+  'PL',
+  'PT',
+  'IE',
+  'DE',
+  'UK',
+  'CA',
+  'QC',
+  'WS',
+  'WE',
+  'AU',
+  'SG',
+  'ASIA',
+  'NL',
+  'MA',
+  'TN',
+];
+
+export default {
+  SUBSIDIARIES,
+};
