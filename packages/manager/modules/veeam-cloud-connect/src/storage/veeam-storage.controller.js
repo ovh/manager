edf781989f62eaefd2105df090b7b73d9cf32030
frenauvh <florian.renaut@corp.ovh.com>
2019-11-28 17:09:37 +0100

    refactor(veeam-cloud-connect): remove uibModal
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/modules/veeam-cloud-connect/src/storage/veeam-storage.controller.js
+++ packages/manager/modules/veeam-cloud-connect/src/storage/veeam-storage.controller.js
@@ -1,8 +1,3 @@
-import storageAddTemplate from './add/veeam-storage-add.html';
-import updateQuotaTemplate from './update-quota/veeam-storage-update-quota.html';
-import storageAddCtrl from './add/veeam-storage-add.controller';
-import updateQuotaCtrl from './update-quota/veeam-storage-update-quota.controller';
-
 export default class VeeamCloudConnectStorageCtrl {
   /* @ngInject */
   constructor($stateParams, $translate, CucControllerHelper,
@@ -37,54 +32,8 @@ export default class VeeamCloudConnectStorageCtrl {
     return this.CucRegionService.getTranslatedMicroRegion(region.toUpperCase());
   }
 
-  addStorage() {
-    if (this.actions.data.addStorage.available) {
-      this.CucControllerHelper.modal.showModal({
-        modalConfig: {
-          template: storageAddTemplate,
-          controller: storageAddCtrl,
-          controllerAs: 'VeeamCloudConnectStorageAddCtrl',
-          resolve: {
-            serviceName: () => this.$stateParams.serviceName,
-          },
-        },
-      })
-        .then((result) => {
-          this.VeeamCloudConnectService.startPolling(this.$stateParams.serviceName, result.data)
-            .then(this.storageInfos.load.bind(this));
-        })
-        .catch(err => this.VeeamCloudConnectService.unitOfWork.messages.push({
-          text: err.message,
-          type: 'error',
-        }));
-    } else {
-      this.CucControllerHelper.modal.showWarningModal({
-        title: this.$translate.instant('veeam_common_action_unavailable'),
-        message: this.actions.data.addStorage.reason,
-      });
-    }
-  }
-
   updateQuota(inventoryName) {
-    this.CucControllerHelper.modal.showModal({
-      modalConfig: {
-        template: updateQuotaTemplate,
-        controller: updateQuotaCtrl,
-        controllerAs: 'VeeamCloudConnectStorageUpdateQuotaCtrl',
-        resolve: {
-          inventoryName: () => inventoryName,
-          serviceName: () => this.$stateParams.serviceName,
-        },
-      },
-    })
-      .then((result) => {
-        this.VeeamCloudConnectService.startPolling(this.$stateParams.serviceName, result.data)
-          .then(this.storageInfos.load.bind(this));
-      })
-      .catch(error => this.VeeamCloudConnectService.unitOfWork.messages.push({
-        text: error.message,
-        type: 'error',
-      }));
+    this.goToStorageQuota(inventoryName);
   }
 
   $onInit() {
