8e9f1dd147487d96d3f4994673295e1359889b8f
Marie JONES <marie.jones@corp.ovh.com>
2019-12-19 14:41:49 +0100

    feat(web.email-domain): update order funnel
    ref: MANAGER-4027
    
    Co-authored-by: frenauvh <florian.renaut@corp.ovh.com>
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/web/client/app/email-domain/order/email-domain-order.controller.js
+++ packages/manager/apps/web/client/app/email-domain/order/email-domain-order.controller.js
@@ -1,176 +1,105 @@
-import filter from 'lodash/filter';
-import map from 'lodash/map';
+import find from 'lodash/find';
+import get from 'lodash/get';
+import includes from 'lodash/includes';
 
-angular
-  .module('App')
-  .controller(
-    'MXPlanOrderCtrl',
-    class MXPlanOrderCtrl {
-      constructor(
-        $q,
-        $scope,
-        $stateParams,
-        $translate,
-        $window,
+import { ORDER_TRACKING_URLS } from './order.constants';
 
-        Alerter,
-        atInternet,
-        Domain,
-        MXPlan,
-        User,
-      ) {
-        this.$q = $q;
-        this.$scope = $scope;
-        this.$stateParams = $stateParams;
-        this.$translate = $translate;
-        this.$window = $window;
+export default class MXPlanOrderCtrl {
+  /* @ngInject */
+  constructor($q, $timeout, $translate, $window, Alerter, coreConfig) {
+    this.$q = $q;
+    this.$timeout = $timeout;
+    this.$translate = $translate;
+    this.$window = $window;
+    this.Alerter = Alerter;
+    this.coreConfig = coreConfig;
+  }
 
-        this.Alerter = Alerter;
-        this.atInternet = atInternet;
-        this.Domain = Domain;
-        this.MXPlan = MXPlan;
-        this.User = User;
-      }
+  $onInit() {
+    this.currentIndex = 0;
 
-      $onInit() {
-        this.list = {
-          domains: [],
-          offers: [],
-        };
-        this.loading = {
-          bc: false,
-          init: false,
-          offers: false,
-        };
-        this.order = null;
-        this.selectedOrder = {
-          config: {
-            domain: this.$stateParams.domain,
-            offer: null,
-          },
-          duration: null,
-          contractsValidated: false,
-        };
-        this.urls = {};
+    this.model = {
+      selectedDomain: find(this.domains, this.domain),
+      product: this.products[0],
+      autoPayWithPreferredPaymentMethod: null,
+      contractsAccepted: false,
+    };
 
-        this.$scope.alerts = {
-          order: 'mxPlan.alerts.order',
-          durations: 'mxPlan.alerts.order.duration',
-        };
+    this.loading = {
+      domain: false,
+      checkout: false,
+    };
+  }
 
-        this.initialLoad()
-          .then(() => {
-            if (this.selectedOrder.config.domain) {
-              this.fetchOffers();
-            }
-          });
-      }
+  getProductTaxAmount({ planCode }) {
+    const { plans } = this.catalog;
+    const pricings = get(find(plans, { planCode }), 'pricings');
+    const price = find(pricings, ({ capacities }) => includes(capacities, 'installation'));
+    return price.tax;
+  }
 
-      initialLoad() {
-        this.loading.init = true;
-        this.User.getUrlOfEndsWithSubsidiary('MX_PLAN').then((page) => {
-          this.urls.mxPlanProductDescription = page;
-        });
+  onDomainChange() {
+    this.loading.domain = true;
+    return this.deleteItem(this.item)
+      .catch((error) => this.Alerter.error(
+        this.$translate.instant('mxPlan_order_checkout_error', { message: get(error, 'data.message') }),
+      ))
+      .finally(() => {
+        this.loading.domain = false;
+      });
+  }
 
-        return this.Domain.getDomains()
-          .then((domains) => { this.list.domains = domains; })
-          .catch(() => this.Alerter.error(this.$translate.instant('mxPlan_order_step1_error'), this.$scope.alerts.order))
-          .finally(() => { this.loading.init = false; });
-      }
+  loadValidation() {
+    this.loading.checkout = true;
+    return this.addConfiguration(this.item, this.model.product, this.model.selectedDomain)
+      .then(({ item }) => {
+        this.item = item;
+        return this.getCheckout();
+      })
+      .then((checkout) => {
+        this.checkout = checkout;
+      })
+      .catch((error) => {
+        this.Alerter.error(
+          this.$translate.instant('mxPlan_order_checkout_error', { message: get(error, 'data.message') }),
+        );
+      })
+      .finally(() => {
+        this.loading.checkout = false;
+      });
+  }
 
-      fetchOffers() {
-        this.loading.offers = true;
-        this.selectedOrder.offer = null;
-        this.list.offers = [];
-
-        return this.MXPlan
-          .getOrderModels(this.selectedOrder.config.domain)
-          .then((response) => { this.list.offers = response; })
-          .catch((err) => {
-            this.list.offers = filter(err, (offer) => offer.offer);
-            const errors = filter(err, (offer) => offer.message);
-            if (this.list.offers.length <= 0) {
-              this.Alerter.error(this.$translate.instant('mxPlan_order_step1_error', { t0: errors[0].message }), this.$scope.alerts.order);
-            }
-          })
-          .finally(() => { this.loading.offers = false; });
-      }
-
-      selectOffer() {
-        this.selectedOrder.contractsValidated = false;
-        if (this.selectedOrder.config.domain
-          && this.selectedOrder.config.domain.length
-          && this.selectedOrder.config.offer
-          && this.selectedOrder.config.offer.length) {
-          this.order = null;
-          this.selectedOrder.duration = null;
-          this.getDurations();
+  validateCheckout() {
+    this.loading.checkout = true;
+    return this.order(this.model.autoPayWithPreferredPaymentMethod)
+      .then((order) => {
+        if (order.url && !this.model.autoPayWithPreferredPaymentMethod) {
+          this.$window.open(order.url, '_blank');
+          return this.Alerter.success(
+            this.$translate.instant('mxPlan_order_generated_purchase_success', {
+              orderId: order.orderId,
+              orderUrl: order.url,
+            }),
+          );
         }
-      }
 
-      getDurations() {
-        this.loading.durations = true;
-        this.Alerter.resetMessage(this.$scope.alerts.durations);
-        this.durations = {
-          available: null,
-          details: {},
-        };
+        this.orderTrackingLink = `${ORDER_TRACKING_URLS[this.coreConfig.getRegion()]}/${order.orderId}`;
 
-        return this.MXPlan
-          .orderDuration(this.selectedOrder.config.domain, this.selectedOrder.config.offer)
-          .then((durations) => {
-            this.durations.available = durations;
-            return this.loadPrices(durations);
-          })
-          .catch((err) => this.Alerter.alertFromSWS(this.$translate.instant('mxPlan_order_step2_duration_fail'), err, this.$scope.alerts.durations))
-          .finally(() => { this.loading.durations = false; });
-      }
-
-      loadPrices(durations) {
-        this.loading.prices = true;
-        const queue = map(durations, (duration) => this.MXPlan
-          .orderPrice(this.selectedOrder.config.domain, this.selectedOrder.config.offer, duration)
-          .then((details) => {
-            this.durations.details[duration] = details;
-          }));
-
-        return this.$q
-          .all(queue)
-          .then(() => {
-            if (durations && durations.length === 1) {
-              [this.selectedOrder.duration] = durations;
-            }
-          })
-          .catch((err) => this.Alerter.alertFromSWS(this.$translate.instant('mxPlan_order_step2_price_fail'), err, this.$scope.alerts.order))
-          .finally(() => {
-            this.loading.prices = false;
-          });
-      }
-
-      generateBc() {
-        this.loading.bc = true;
-        this.MXPlan
-          .orderMxPlan(
-            this.selectedOrder.config.domain,
-            this.selectedOrder.offer.offer,
-            this.selectedOrder.offer.duration,
-          )
-          .then((details) => { this.order = details; })
-          .catch((err) => this.Alerter.alertFromSWS(this.$translate.instant('mxPlan_order_step3_fail'), err, this.$scope.alerts.order))
-          .catch(() => { this.loading.bc = false; });
-      }
-
-      openBc() {
-        this.atInternet.trackOrder({
-          name: `[mxplan]::MX-${this.selectedOrder.offer.offer}[MX-${this.selectedOrder.offer.offer}]`,
-          page: 'web::payment-pending',
-          orderId: this.order.orderId,
-          priceTaxFree: this.order.prices.withoutTax.value,
-          price: this.order.prices.withTax.value,
-          status: 1,
+        return this.orderTrackingLink;
+      })
+      .catch((error) => this.Alerter.error(
+        this.$translate.instant('mxPlan_order_error', { message: get(error, 'message') }),
+      ))
+      .finally(() => {
+        this.$timeout(() => {
+          this.loading.checkout = false;
         });
-        this.$window.open(this.order.url);
-        this.$onInit();
-      }
-    },
-  );
+      });
+  }
+
+  onPaymentError() {
+    return this.Alerter.error(
+      this.$translate.instant('mxPlan_order_error_payment'),
+    );
+  }
+}
