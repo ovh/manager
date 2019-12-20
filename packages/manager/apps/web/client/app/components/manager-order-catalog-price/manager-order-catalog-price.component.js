397cde2257ffe8870dfe884e56f8c88338144fe1
Marie JONES <marie.jones@corp.ovh.com>
2019-12-20 16:39:17 +0100

    feat(web.catalog-price): add option to pass non-ucents prices
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/web/client/app/components/manager-order-catalog-price/manager-order-catalog-price.component.js
+++ packages/manager/apps/web/client/app/components/manager-order-catalog-price/manager-order-catalog-price.component.js
@@ -4,6 +4,7 @@ import template from './manager-order-catalog-price.html';
 const component = {
   bindings: {
     block: '<',
+    convertToUcents: '<',
     interval: '<?',
     price: '<',
     tax: '<',
--- packages/manager/apps/web/client/app/components/manager-order-catalog-price/manager-order-catalog-price.controller.js
+++ packages/manager/apps/web/client/app/components/manager-order-catalog-price/manager-order-catalog-price.controller.js
@@ -22,6 +22,12 @@ export default class {
       this.block = true;
     }
 
+    if (!isUndefined(this.$attrs.convertToUcents) && this.$attrs.convertToUcents === '') {
+      this.convertToUcents = true;
+      this.price *= 100000000;
+      this.tax *= 100000000;
+    }
+
     this.ovhSubsidiary = get(this.user, 'ovhSubsidiary', '');
   }
 
