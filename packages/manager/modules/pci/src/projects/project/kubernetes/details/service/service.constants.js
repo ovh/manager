b9f414afb57d82d70ffb5933672a40a845978310
radireddy <adireddyravindra@gmail.com>
2020-01-13 13:21:39 +0530

    feat(kubernetes): show version EOL message (#2042)
    ref: K8S-1729
    
    Signed-off-by: Ravindra Adireddy <adireddyravindra@gmail.com>

--- packages/manager/modules/pci/src/projects/project/kubernetes/details/service/service.constants.js
+++ packages/manager/modules/pci/src/projects/project/kubernetes/details/service/service.constants.js
@@ -6,8 +6,12 @@ export const KUBECONFIG_URL =
 export const KUBECTL_URL =
   'https://kubernetes.io/docs/reference/kubectl/overview/';
 
+export const VERSIONS_GUIDE_URL =
+  'https://docs.ovh.com/gb/en/kubernetes/eos-eol-policies/';
+
 export default {
   CONFIG_FILENAME,
   KUBECONFIG_URL,
   KUBECTL_URL,
+  VERSIONS_GUIDE_URL,
 };
