44885ff8ddfbb3c58f779538a0974ace1132bbdc
Jisay <jean-christophe.alleman@corp.ovh.com>
2020-01-21 10:22:11 +0100

    fix(pci.projects.new): manage hipay return
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

--- packages/manager/modules/pci/src/projects/new/payment/component.js
+++ packages/manager/modules/pci/src/projects/new/payment/component.js
@@ -15,5 +15,6 @@ export default {
     onCartFinalized: '<',
     onAskCreditPayment: '<',
     globalLoading: '<',
+    paymentStatus: '<',
   },
 };
--- packages/manager/modules/pci/src/projects/new/payment/controller.js
+++ packages/manager/modules/pci/src/projects/new/payment/controller.js
@@ -196,6 +196,22 @@ export default class PciProjectNewPaymentCtrl {
         onMessage: () => this.refreshMessages(),
       },
     );
+
+    // check if addPayment status is in URL
+    // and no paymentMethod needs to be added
+    // and there is a default payment method.
+    // In this case it means that a payment method has been added
+    // and that the APIs eligibility AND payment are sync.
+    // In other case, for example: when eligibility requires a payment method
+    // and there is a paymentStatus in URL, the onIntegrationSubmitSuccess will be triggered
+    // automatically.
+    if (
+      !this.eligibility.isAddPaymentMethodRequired() &&
+      this.defaultPaymentMethod &&
+      this.paymentStatus === 'success'
+    ) {
+      this.manageProjectCreation();
+    }
   }
 
   /* -----  End of Hooks  ------ */
