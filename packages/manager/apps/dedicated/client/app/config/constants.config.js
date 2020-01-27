2b1c6ccb0526aeb427aec9b068e1f989111b54b5
Florian Renaut <florian.renaut@corp.ovh.com>
2020-01-27 13:51:02 +0100

    fix(dedicated): update dedicated server order link for us (#2143)
    ref: MANAGER-4245
    
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/config/constants.config.js
+++ packages/manager/apps/dedicated/client/app/config/constants.config.js
@@ -1181,8 +1181,7 @@ const constants = {
         exchangeOrder: 'https://www.ovh.com/ca/en/emails/hosted-exchange/',
         faq: 'https://www.ovh.com/ca/en/dedicated-servers/faq.xml',
         faqVps: 'https://www.ovh.com/ca/en/vps/faq-help.xml',
-        dedicatedOrder:
-          'https://us.ovhcloud.com/order/dedicated/#/dedicated/select',
+        dedicatedOrder: 'https://us.ovhcloud.com/dedicated-servers/prices',
         vrackOrder:
           "https://us.ovhcloud.com/order/express/#/express/review?products=~(~(planCode~'vrack~quantity~1~productId~'vrack))",
         cloudProjectOrder:
