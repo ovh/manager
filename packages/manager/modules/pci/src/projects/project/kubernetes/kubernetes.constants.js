762fd147d2316e9afc4e55781881ccaae06f0356
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2020-01-29 17:30:49 +0100

    fix(pci): update models used to retrieve kubernetes version
    ref: DTRSD-7842
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/modules/pci/src/projects/project/kubernetes/kubernetes.constants.js
+++ packages/manager/modules/pci/src/projects/project/kubernetes/kubernetes.constants.js
@@ -1,6 +1,6 @@
 export const REGION = 'GRA5';
 
-export const VERSION_ENUM_KEY = "models['cloud.kube.Version'].enum";
+export const VERSION_ENUM_KEY = "models['cloud.kube.VersionEnum'].enum";
 
 export default {
   REGION,
