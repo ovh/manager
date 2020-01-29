74054539c527f35e4ec72dcd05a407d4aab7b912
Marie JONES <marie.jones@corp.ovh.com>
2020-01-29 18:28:16 +0100

    feat(pci.projects.private-registry): add component for offer selection
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

---
+++ packages/manager/modules/pci/src/projects/project/private-registry/components/plan-upgrade/RegistryPlan.class.js
@@ -0,0 +1,31 @@
+import get from 'lodash/get';
+
+export default class RegistyPlan {
+  constructor(resource) {
+    Object.assign(this, resource);
+  }
+
+  get capacity() {
+    return get(this, 'registryLimits.imageStorage', 0);
+  }
+
+  get simultaneousConnections() {
+    return get(this, 'blobs.technical.connection.total', 0);
+  }
+
+  get concurrency() {
+    return get(this, 'blobs.technical.connection.clients.concurrency', 0);
+  }
+
+  get connectionsNumber() {
+    return get(this, 'blobs.technical.connection.clients.number', 0);
+  }
+
+  hasUnlimitedBandwidth() {
+    return get(this, 'blobs.technical.bandwidth.unlimited');
+  }
+
+  hasVulnerabilityScanning() {
+    return get(this, 'features.vulnerability');
+  }
+}
