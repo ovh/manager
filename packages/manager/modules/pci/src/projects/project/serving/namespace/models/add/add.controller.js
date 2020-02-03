9dafea66e37d0fc51f94bc242c83b430fce0ee40
acarreir <adrien.carreira@corp.ovh.com>
2020-02-03 14:07:30 +0100

    feat(pci.serving): add error message in add model
    Signed-off-by: acarreir <adrien.carreira@corp.ovh.com>

--- packages/manager/modules/pci/src/projects/project/serving/namespace/models/add/add.controller.js
+++ packages/manager/modules/pci/src/projects/project/serving/namespace/models/add/add.controller.js
@@ -35,11 +35,9 @@ export default class PciServingNamespaceModelsAddController {
 
     [this.model.flavor] = this.flavors;
 
-    this.workflowTemplates = [
-      this.BUILD_IMAGE,
-      this.PRESET_IMAGE,
-    ];
+    this.workflowTemplates = [this.BUILD_IMAGE, this.PRESET_IMAGE];
 
+    this.error = false;
     this.folders = [];
     this.files = [];
     this.getContainerFiles();
@@ -48,44 +46,62 @@ export default class PciServingNamespaceModelsAddController {
   getContainerFiles() {
     this.containerLoading = true;
     this.PciProjectStorageContainersService.getContainer(
-      this.projectId, this.namespace.containerId,
-    ).then((container) => {
-      this.folders = container.objects.map(({ name }) => {
-        const split = name.split('/');
-        split.pop();
-        return split.join('/');
-      }).filter((path) => path !== '');
+      this.projectId,
+      this.namespace.containerId,
+    )
+      .then((container) => {
+        this.folders = container.objects
+          .map(({ name }) => {
+            const split = name.split('/');
+            split.pop();
+            return split.join('/');
+          })
+          .filter((path) => path !== '');
 
-      this.files = container.objects.map(({ name }) => name).filter((path) => {
-        for (let i = 0; i < EXTENSION.length; i += 1) {
-          if (path.endsWith(EXTENSION[i])) {
-            return true;
-          }
-        }
-        return false;
+        this.files = container.objects
+          .map(({ name }) => name)
+          .filter((path) => {
+            for (let i = 0; i < EXTENSION.length; i += 1) {
+              if (path.endsWith(EXTENSION[i])) {
+                return true;
+              }
+            }
+            return false;
+          });
+      })
+      .finally(() => {
+        this.containerLoading = false;
       });
-    }).finally(() => {
-      this.containerLoading = false;
-    });
   }
 
   addModel() {
+    this.error = false;
     this.isAdding = true;
 
-    this.OvhManagerPciServingModelsService.add(this.projectId, this.namespaceId, {
-      id: this.model.id,
-      storagePath: this.model.storagePath,
-      flavor: this.model.flavor.id,
-      workflowTemplate: this.model.workflowTemplate,
-      imageId: get(this.model.imageId, 'id'),
-    }).then(() => this.goBack(
-      this.$translate.instant('pci_projects_project_serving_namespace_models_add_success'),
-    ))
-      .catch((error) => this.goBack(
-        this.$translate.instant('pci_projects_project_serving_namespace_models_add_error', {
-          message: get(error, 'data.message'),
-        }), 'error',
-      ));
+    this.OvhManagerPciServingModelsService.add(
+      this.projectId,
+      this.namespaceId,
+      {
+        id: this.model.id,
+        storagePath: this.model.storagePath,
+        flavor: this.model.flavor.id,
+        workflowTemplate: this.model.workflowTemplate,
+        imageId: get(this.model.imageId, 'id'),
+      },
+    )
+      .then(() =>
+        this.goBack(
+          this.$translate.instant(
+            'pci_projects_project_serving_namespace_models_add_success',
+          ),
+        ),
+      )
+      .catch((error) => {
+        this.error = get(error, 'data.message');
+      })
+      .finally(() => {
+        this.isAdding = false;
+      });
   }
 
   resetMode() {
