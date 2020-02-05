af83611ea368ce9298ece4e4572153d8ea0b0c09
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2020-02-05 10:20:53 +0100

    build(web): add extras config files in webpack
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/web/webpack.config.js
+++ packages/manager/apps/web/webpack.config.js
@@ -68,7 +68,8 @@ module.exports = (env = {}) => {
   );
 
   // Extra config files
-  const extras = glob.sync('./.extras-EU/**/*.js');
+  const extrasRegion = glob.sync(`./.extras-EU/**/*.js`);
+  const extras = glob.sync('./.extras/**/*.js');
 
   return merge(config, {
     entry: _.assign(
@@ -84,6 +85,7 @@ module.exports = (env = {}) => {
       },
       bundles,
       extras.length > 0 ? { extras } : {},
+      extrasRegion.length > 0 ? { extrasRegion } : {},
     ),
     output: {
       path: path.resolve(__dirname, 'dist'),
