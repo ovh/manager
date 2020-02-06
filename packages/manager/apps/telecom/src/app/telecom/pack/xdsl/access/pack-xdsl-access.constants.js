90f82a7474244150f77505788f738ffb43f9e23e
Stephanie Moallic <stephanie.moallic@corp.ovh.com>
2020-02-06 17:42:30 +0100

    feat(xdsl.access): retrieve price and model for exchange modem from api
    ref: UXCT-230
    
    Signed-off-by: Stephanie Moallic <stephanie.moallic@corp.ovh.com>

--- packages/manager/apps/telecom/src/app/telecom/pack/xdsl/access/pack-xdsl-access.constants.js
+++ packages/manager/apps/telecom/src/app/telecom/pack/xdsl/access/pack-xdsl-access.constants.js
@@ -1,5 +1,13 @@
 export const XDSL_NO_INCIDENT_CODE = 404;
 
+export const XDSL_EXCHANGE_MODEM = {
+  errBase: 'ERR0',
+  errorCodeLast: 'ERR001',
+  errorCodeRMA: 'ERR002',
+  errorCodeSDSL: 'ERR004',
+};
+
 export default {
   XDSL_NO_INCIDENT_CODE,
+  XDSL_EXCHANGE_MODEM,
 };
