121270e627b7a0b4f0b7d3f73c485e9fbf1b56f3
Ravindra Adireddy <adireddyravindra@gmail.com>
2020-02-06 14:28:07 +0530

    feat(notifications): notification menu for manager hub
    change existing notification menu as per manager hub notification menu design
    
    Closes #MANAGER-4292
    
    Signed-off-by: Ravindra Adireddy <adireddyravindra@gmail.com>

--- packages/manager/modules/navbar/src/notifications-menu/constants.js
+++ packages/manager/modules/navbar/src/notifications-menu/constants.js
@@ -1,14 +1,17 @@
 export const ACKNOWLEDGED_STATUS = ['acknowledged', 'completed', 'unknown'];
 
-export const ACTIVE_STATUS = ['acknowledged', 'delivered'];
+export const ACTIVE_STATUS = 'delivered';
 
-export const ANIMATED_STATUS = ['incident', 'error', 'warning'];
+export const COMPLETED_STATUS = 'completed';
 
 export const REFRESH_TIME = 60000;
+
+export const MAX_NOTIFICATIONS = 10;
 
 export default {
   ACKNOWLEDGED_STATUS,
   ACTIVE_STATUS,
-  ANIMATED_STATUS,
+  COMPLETED_STATUS,
   REFRESH_TIME,
+  MAX_NOTIFICATIONS,
 };
