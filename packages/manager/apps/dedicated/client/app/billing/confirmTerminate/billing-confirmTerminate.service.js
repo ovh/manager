8a5dbce2393b8c791b329a81be33864f8a8e1d8e
Marie JONES <marie.jones@corp.ovh.com>
2020-02-11 10:20:32 +0100

    refactor(billing.confirm-terminate): modularize and use component
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/billing/confirmTerminate/billing-confirmTerminate.service.js
+++ packages/manager/apps/dedicated/client/app/billing/confirmTerminate/billing-confirmTerminate.service.js
@@ -1,68 +1,66 @@
-angular
-  .module('Billing.services')
-  .service('BillingTerminate', function BillingTerminate($q, OvhHttp) {
-    this.getServiceTypeFromPrefix = function getServiceTypeFromPrefix(
-      serviceApiPrefix,
-    ) {
-      return serviceApiPrefix
-        .replace(/^\//, '')
-        .replace(/\/\{.+$/, '')
-        .replace(/\//g, '_');
-    };
+export default /* @ngInject */ function BillingTerminate($q, OvhHttp) {
+  this.getServiceTypeFromPrefix = function getServiceTypeFromPrefix(
+    serviceApiPrefix,
+  ) {
+    return serviceApiPrefix
+      .replace(/^\//, '')
+      .replace(/\/\{.+$/, '')
+      .replace(/\//g, '_');
+  };
 
-    this.getServiceApi = function getServiceApi(serviceId, forceRefresh) {
-      const params = {
-        rootPath: 'apiv6',
-        cache: 'billingTerminateService',
-      };
-      if (forceRefresh) {
-        delete params.cache;
-      }
-      return OvhHttp.get(`/service/${serviceId}`, params);
+  this.getServiceApi = function getServiceApi(serviceId, forceRefresh) {
+    const params = {
+      rootPath: 'apiv6',
+      cache: 'billingTerminateService',
     };
+    if (forceRefresh) {
+      delete params.cache;
+    }
+    return OvhHttp.get(`/service/${serviceId}`, params);
+  };
 
-    this.getServiceInfo = function getServiceInfo(serviceId) {
-      let serviceType;
-      return this.getServiceApi(serviceId)
-        .then((serviceApi) => {
-          serviceType = this.getServiceTypeFromPrefix(serviceApi.route.path);
-          return serviceApi.route.url.replace(
-            serviceApi.resource.name,
-            window.encodeURIComponent(serviceApi.resource.name),
-          );
-        })
-        .then((url) =>
-          OvhHttp.get(`${url}/serviceInfos`, {
-            rootPath: 'apiv6',
-          }),
-        )
-        .then((serviceInfos) => ({ ...serviceInfos, serviceType }));
-    };
-
-    this.confirmTermination = function confirmTermination(
-      serviceId,
-      serviceName,
-      futureUse,
-      reason,
-      commentary,
-      token,
-    ) {
-      return this.getServiceApi(serviceId)
-        .then((serviceApi) =>
-          serviceApi.route.url.replace(
-            serviceApi.resource.name,
-            window.encodeURIComponent(serviceApi.resource.name),
-          ),
-        )
-        .then((url) =>
-          OvhHttp.post(`${url}/confirmTermination`, {
-            rootPath: 'apiv6',
-            data: {
-              reason,
-              commentary,
-              token,
-            },
-          }),
+  this.getServiceInfo = function getServiceInfo(serviceId) {
+    let serviceType;
+    return this.getServiceApi(serviceId)
+      .then((serviceApi) => {
+        serviceType = this.getServiceTypeFromPrefix(serviceApi.route.path);
+        return serviceApi.route.url.replace(
+          serviceApi.resource.name,
+          window.encodeURIComponent(serviceApi.resource.name),
         );
-    };
-  });
+      })
+      .then((url) =>
+        OvhHttp.get(`${url}/serviceInfos`, {
+          rootPath: 'apiv6',
+        }),
+      )
+      .then((serviceInfos) => ({ ...serviceInfos, serviceType }));
+  };
+
+  this.confirmTermination = function confirmTermination(
+    serviceId,
+    serviceName,
+    futureUse,
+    reason,
+    commentary,
+    token,
+  ) {
+    return this.getServiceApi(serviceId)
+      .then((serviceApi) =>
+        serviceApi.route.url.replace(
+          serviceApi.resource.name,
+          window.encodeURIComponent(serviceApi.resource.name),
+        ),
+      )
+      .then((url) =>
+        OvhHttp.post(`${url}/confirmTermination`, {
+          rootPath: 'apiv6',
+          data: {
+            reason,
+            commentary,
+            token,
+          },
+        }),
+      );
+  };
+}
