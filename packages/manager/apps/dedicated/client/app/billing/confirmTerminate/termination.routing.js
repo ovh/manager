303bec55730b2ae457faa0c4ab1c8f973aa1420f
Marie JONES <marie.jones@corp.ovh.com>
2020-02-11 10:20:32 +0100

    refactor(billing.confirm-terminate): modularize and use component
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/billing/confirmTerminate/termination.routing.js
+++ packages/manager/apps/dedicated/client/app/billing/confirmTerminate/termination.routing.js
@@ -1,11 +1,7 @@
-import template from './billing-confirmTerminate.html';
-
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('app.account.billing.confirmTerminate', {
     url: '/confirmTerminate?id&token',
-    template,
-    controller: 'Billing.controllers.TerminateServiceCtrl',
-    controllerAs: 'TerminateServiceCtrl',
+    component: 'billingConfirmTermination',
     translations: { value: ['..'], format: 'json' },
   });
 };
