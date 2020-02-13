876eff1cf6e99982982f0c758352f0caac522dba
Marie JONES <marie.jones@corp.ovh.com>
2020-02-13 08:50:13 +0100

    feat(dedicated.server.interfaces): add guides url for all subs
    ref: MANAGER-4147
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/dedicated/server/interfaces/interfaces.constants.js
+++ packages/manager/apps/dedicated/client/app/dedicated/server/interfaces/interfaces.constants.js
@@ -1,4 +1,16 @@
 export const GUIDES = {
+  ASIA: 'https://docs.ovh.com/asia/en/dedicated/ola-manager/',
+  AU: 'https://docs.ovh.com/au/en/dedicated/ola-manager/',
+  CA: 'https://docs.ovh.com/ca/en/dedicated/ola-manager/',
+  DE: 'https://docs.ovh.com/de/dedicated/ola-manager/',
+  ES: 'https://docs.ovh.com/es/dedicated/ola-manager/',
+  FR: 'https://docs.ovh.com/fr/dedicated/ola-manager/',
+  GB: 'https://docs.ovh.com/gb/en/dedicated/ola-manager/',
+  IE: 'https://docs.ovh.com/ie/en/dedicated/ola-manager/',
+  IT: 'https://docs.ovh.com/it/dedicated/ola-manager/',
+  PL: 'https://docs.ovh.com/pl/dedicated/ola-manager/',
+  PT: 'https://docs.ovh.com/pt/dedicated/ola-manager/',
+  SG: 'https://docs.ovh.com/sg/en/dedicated/ola-manager/',
   default: 'https://docs.ovh.com/gb/en/dedicated/ola-manager/',
 };
 
