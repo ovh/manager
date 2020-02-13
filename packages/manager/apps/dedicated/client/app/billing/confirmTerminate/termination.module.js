07c1d3450a5be2951760f306ad980df4f97581df
Marie JONES <marie.jones@corp.ovh.com>
2020-02-13 18:12:50 +0100

    feat(billing.confirm-terminate): revamp termination form
    ref: MANAGER-2414
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/billing/confirmTerminate/termination.module.js
+++ packages/manager/apps/dedicated/client/app/billing/confirmTerminate/termination.module.js
@@ -8,6 +8,7 @@ angular
   .module(moduleName, ['ui.router'])
   .config(routing)
   .service('BillingTerminate', service)
-  .component('billingConfirmTermination', component);
+  .component('billingConfirmTermination', component)
+  .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
