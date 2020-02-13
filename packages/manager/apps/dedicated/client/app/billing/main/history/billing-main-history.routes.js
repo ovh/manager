74953378d3beb0f1d523b2367733ce250b70266f
Marie JONES <marie.jones@corp.ovh.com>
2020-02-13 17:11:15 +0100

    feat(billing.history): add date filter to url (#2231)
    ref: MANAGER-4297
    
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/billing/main/history/billing-main-history.routes.js
+++ packages/manager/apps/dedicated/client/app/billing/main/history/billing-main-history.routes.js
@@ -1,12 +1,26 @@
+import controller from './billing-main-history.controller';
+import template from './billing-main-history.html';
+
 angular.module('Billing').config(($stateProvider) => {
   $stateProvider.state('app.account.billing.main.history', {
-    url: '/history',
-    templateUrl: 'billing/main/history/billing-main-history.html',
-    controller: 'BillingMainHistoryCtrl',
+    url: '/history?filters',
+    params: {
+      filters: {
+        value: '[]',
+        squash: true,
+      },
+    },
+    template,
+    controller,
     controllerAs: '$ctrl',
     translations: {
       value: ['../history', './postalMailOptions'],
       format: 'json',
+    },
+    resolve: {
+      filters: /* @ngInject */ ($transition$) => $transition$.params().filters,
+      onListParamsChange: /* @ngInject */ ($state) => (params) =>
+        $state.go('app.account.billing.main.history', params),
     },
   });
 });
