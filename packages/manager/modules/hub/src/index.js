76593c077a42daa9cb0b218955e3588c5f1239c9
Ganesh Kumar <ganeshkumar_1989@yahoo.co.in>
2020-02-14 15:14:03 +0530

    fix(hub-tile): add hub tile component (#2227)
    ref: MANAGER-4300
    
    Signed-off-by: Ganesh Kumar <ganesh.kumar@corp.ovh.com>

--- packages/manager/modules/hub/src/index.js
+++ packages/manager/modules/hub/src/index.js
@@ -4,12 +4,15 @@ import '@ovh-ux/manager-core';
 import '@uirouter/angularjs';
 import 'angular-translate';
 
+import ovhManagerHubTile from './components/tile';
+
 const moduleName = 'ovhManagerHub';
 
 angular.module(moduleName, [
   'ovhManagerCore',
   'pascalprecht.translate',
   'ui.router',
+  ovhManagerHubTile,
 ]);
 
 export default moduleName;
