2b5b7ad325306f505b94ecd260295da2d8067872
frenauvh <florian.renaut@corp.ovh.com>
2020-02-17 10:18:50 +0100

    feat(hub): add billing summary tile
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

---
+++ packages/manager/modules/hub/src/components/billing-summary/component.js
@@ -0,0 +1,11 @@
+import controller from './controller';
+import template from './template.html';
+
+export default {
+  controller,
+  template,
+  bindings: {
+    bills: '<?',
+    debt: '<?',
+  },
+};
