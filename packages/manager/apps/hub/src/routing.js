927a1304208830920e80b4d59fd568b4fc72059a
Marie JONES <marie.jones@corp.ovh.com>
2020-02-20 16:38:17 +0100

    feat(hub): add product listing
    ref: MANAGER-4301
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/hub/src/routing.js
+++ packages/manager/apps/hub/src/routing.js
@@ -1,6 +1,16 @@
 export default /* @ngInject */ ($stateProvider, $urlRouterProvider) => {
   $stateProvider.state('app', {
     url: '/',
+    component: 'hubDashboard',
+    resolve: {
+      data: /* @ngInject */ ($http) =>
+        $http
+          .get('/hub', {
+            serviceType: 'aapi',
+          })
+          .then(({ data }) => data),
+      services: /* @ngInject */ (data) => data.data.services.data.data,
+    },
   });
 
   $urlRouterProvider.otherwise('/');
