108c9962f7a6ea6d1b2af390365cdab63c6e972c
Ganesh Kumar <ganesh.kumar@corp.ovh.com>
2020-02-25 16:13:38 +0530

    feat(veeam-backup): fix price component
    bug fixes
    
    Signed-off-by: Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/dedicatedCloud/datacenter/backup/components/offers/index.js
+++ packages/manager/apps/dedicated/client/app/dedicatedCloud/datacenter/backup/components/offers/index.js
@@ -1,12 +1,13 @@
 import angular from 'angular';
 
 import component from './component';
+import price from '../price';
 import './styles.less';
 
 const moduleName = 'ovhManagerDedicatedCloudBackupOffers';
 
 angular
-  .module(moduleName, [])
+  .module(moduleName, [price])
   .component('dedicatedCloudDatacenterBackupOffers', component)
   .run(/* @ngTranslationsInject:json ./translations */);
 
