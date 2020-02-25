c6eb6b2db9dfa49062411bfddb79ba94f400bf9f
Marie JONES <marie.jones@corp.ovh.com>
2020-02-25 16:34:56 +0100

    refactor: extract reusable logic to a service
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/components/ng-ovh-order-tracking/src/index.js
+++ packages/components/ng-ovh-order-tracking/src/index.js
@@ -4,7 +4,9 @@ import 'angular-translate';
 import 'ovh-api-services';
 import 'ovh-ui-angular';
 
+import { ERROR_STATUS } from './order-tracking.constants';
 import component from './order-tracking.component';
+import service from './order-tracking.service';
 import './index.scss';
 
 const moduleName = 'ngOvhOrderTracking';
@@ -17,6 +19,9 @@ angular
     'pascalprecht.translate',
   ])
   .component('ovhOrderTrackingComponent', component)
+  .service('OrderTracking', service)
   .run(/* @ngTranslationsInject:json ./translations */);
+
+export { ERROR_STATUS };
 
 export default moduleName;
---
+++ packages/components/ng-ovh-order-tracking/src/order-tracking.constants.js
@@ -0,0 +1,10 @@
+export const ERROR_STATUS = [
+  'FRAYD_DOCS_REQUESTED',
+  'FRAUD_MANUAL_REVIEW',
+  'FRAUD_REFUSED',
+  'notPaid',
+];
+
+export default {
+  ERROR_STATUS,
+};
