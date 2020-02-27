5a3351a273b049b754e4d740232b6146e91f5203
Ganesh Kumar <ganesh.kumar@corp.ovh.com>
2020-02-27 15:01:52 +0530

    fix(dedicatedcloud-datacenter-backup-new): cleanup ui flow
    add preview mode for offer selection, add spinner for order handling,
    conditions url addition, api call bug fixes
    
    ref: MANAGER-4442
    
    Signed-off-by: Ganesh Kumar <ganesh.kumar@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/dedicatedCloud/datacenter/backup/new/new.routing.js
+++ packages/manager/apps/dedicated/client/app/dedicatedCloud/datacenter/backup/new/new.routing.js
@@ -2,6 +2,16 @@ export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('app.dedicatedClouds.datacenter.backup.new', {
     url: '/new',
     component: 'ovhManagerDedicatedCloudBackupNew',
+    redirectTo: (transition) =>
+      transition
+        .injector()
+        .getAsync('backup')
+        .then((backup) => {
+          if (!(backup.isInactive() || backup.isLegacy())) {
+            return { state: 'app.dedicatedClouds.datacenter.backup' };
+          }
+          return false;
+        }),
     translations: { value: ['.'], format: 'json' },
   });
 };
