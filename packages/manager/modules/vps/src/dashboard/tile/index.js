49eaa909c08301c1242624b72164e2a326af1c59
Jérémy De-Cesare <jeremy.de-cesare@corp.ovh.com>
2020-02-27 16:27:39 +0100

    feat(vps): add main information tile
    Signed-off-by: Jérémy De-Cesare <jeremy.de-cesare@corp.ovh.com>

---
+++ packages/manager/modules/vps/src/dashboard/tile/index.js
@@ -0,0 +1,9 @@
+import angular from 'angular';
+
+import informationTile from './information';
+
+const moduleName = 'ovhManagerVpsDashboardTile';
+
+angular.module(moduleName, [informationTile]);
+
+export default moduleName;
