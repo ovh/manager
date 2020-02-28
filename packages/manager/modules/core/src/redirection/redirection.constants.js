14e1351b295b4a5b3b585c480122c102734341c3
frenauvh <florian.renaut@corp.ovh.com>
2020-02-28 10:05:51 +0100

    feat(core): add guides urls
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/modules/core/src/redirection/redirection.constants.js
+++ packages/manager/modules/core/src/redirection/redirection.constants.js
@@ -4,6 +4,8 @@ const managerRoot = {
   US: 'https://us.ovhcloud.com',
 };
 
+const guidesRoot = 'https://docs.ovh.com';
+
 const URI = {
   autorenew: '#/billing/autorenew',
   billing: '#/billing/history',
@@ -21,6 +23,25 @@ export default {
     autorenew: `${managerRoot.EU}/manager/dedicated/${URI.autorenew}`,
     billing: `${managerRoot.EU}/manager/dedicated/${URI.billing}`,
     contacts: `${managerRoot.EU}/manager/dedicated/${URI.contacts}`,
+    guides: {
+      home: {
+        CZ: `${guidesRoot}/cz/cs/`,
+        DE: `${guidesRoot}/de/`,
+        ES: `${guidesRoot}/es/`,
+        FI: `${guidesRoot}/fi/`,
+        FR: `${guidesRoot}/fr/`,
+        GB: `${guidesRoot}/gb/en/`,
+        IE: `${guidesRoot}/ie/en/`,
+        IT: `${guidesRoot}/it/`,
+        LT: `${guidesRoot}/lt/`,
+        MA: `${guidesRoot}/gb/en/`,
+        NL: `${guidesRoot}/nl/`,
+        PL: `${guidesRoot}/pl/`,
+        PT: `${guidesRoot}/pt/`,
+        SN: `${guidesRoot}/gb/en/`,
+        TN: `${guidesRoot}/gb/en/`,
+      },
+    },
     help: 'https://help.ovhcloud.com',
     orders: `${managerRoot.EU}/manager/dedicated/${URI.orders}`,
     paymentMethod: `${managerRoot.EU}/manager/dedicated/${URI.paymentMethod}`,
@@ -35,6 +56,17 @@ export default {
     autorenew: `${managerRoot.CA}/manager/dedicated/${URI.autorenew}`,
     billing: `${managerRoot.CA}/manager/${URI.billing}`,
     contacts: '',
+    guides: {
+      home: {
+        ASIA: `${guidesRoot}/ca/en/`,
+        AU: `${guidesRoot}/ca/en/`,
+        CA: `${guidesRoot}/ca/en/`,
+        QC: `${guidesRoot}/ca/fr/`,
+        SG: `${guidesRoot}/ca/en/`,
+        WE: `${guidesRoot}/ca/en/`,
+        WS: `${guidesRoot}/ca/en/`,
+      },
+    },
     help: 'https://help.ovhcloud.com',
     orders: `${managerRoot.CA}/manager/${URI.orders}`,
     paymentMethod: `${managerRoot.CA}/manager/${URI.paymentMethod}`,
@@ -48,6 +80,11 @@ export default {
   US: {
     billing: `${managerRoot.US}/manager/dedicated/${URI.billing}`,
     contacts: '',
+    guides: {
+      home: {
+        US: 'https://support.us.ovhcloud.com',
+      },
+    },
     help: 'https://help.ovhcloud.com',
     paymentMethod: `${managerRoot.US}/manager/dedicated/${URI.paymentMethod}`,
     services: `${managerRoot.US}/manager/dedicated/${URI.services}`,
