2aefd1131e16f54d04082f943460e01efa290930
Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>
2020-03-02 13:15:52 +0530

    feat(veeam-backup): spla licence check
    show add licence if spla licence is not there

---
+++ packages/manager/apps/dedicated/client/app/dedicatedCloud/datacenter/backup/splaLicence/index.js
@@ -0,0 +1,21 @@
+import angular from 'angular';
+import '@uirouter/angularjs';
+import '@ovh-ux/ng-translate-async-loader';
+import 'angular-translate';
+
+import component from './component';
+import routing from './routing';
+
+const moduleName = 'ovhManagerDedicatedCloudBackupSplaLicence';
+
+angular
+  .module(moduleName, [
+    'ngTranslateAsyncLoader',
+    'pascalprecht.translate',
+    'ui.router',
+  ])
+  .config(routing)
+  .component('dedicatedCloudDatacenterBackupSplaLicence', component)
+  .run(/* @ngTranslationsInject:json ./translations */);
+
+export default moduleName;
