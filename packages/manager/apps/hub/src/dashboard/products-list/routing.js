08bc945f723ade836d081c570d1799d1969163b4
Marie JONES <marie.jones@corp.ovh.com>
2020-03-04 17:18:08 +0100

    feat: add filters to product page
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/hub/src/dashboard/products-list/routing.js
+++ packages/manager/apps/hub/src/dashboard/products-list/routing.js
@@ -1,30 +1,66 @@
 import find from 'lodash/find';
 import get from 'lodash/get';
 import head from 'lodash/head';
+import isEmpty from 'lodash/isEmpty';
 import map from 'lodash/map';
-// import { BillingService } from '@ovh-ux/manager-models';
+import startCase from 'lodash/startCase';
+
+import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
+import { DEFAULT_NUMBER_OF_COLUMNS } from './constants';
+
+const matchingTypes = {
+  boolean: 'boolean',
+  enum: 'string',
+  long: 'number',
+  string: 'string',
+};
+
+const mapApiProperties = (properties) =>
+  map(properties, (value, name) => ({
+    title: name,
+    property: name,
+    type: /Enum/.test(value.type)
+      ? matchingTypes.enum
+      : matchingTypes[value.type],
+    searchable: true,
+    filterable: true,
+    sortable: true,
+  }));
+
+const getFirstColumnTemplate = (propertyId) => ({
+  title: propertyId,
+  property: propertyId,
+  template: `
+    <a data-ng-href="{{ $row.url }}" data-ng-bind="$row.${propertyId}"></a>
+  `,
+  searchable: true,
+  filterable: true,
+  sortable: 'asc',
+});
 
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('app.products', {
-    url: ':product',
+    url: `:product?columns${ListLayoutHelper.urlQueryParams}`,
+    params: {
+      columns: {
+        squash: true,
+        value: '[]',
+      },
+      ...ListLayoutHelper.stateParams,
+    },
     component: 'hubProductListing',
     resolve: {
+      ...ListLayoutHelper.stateResolves,
       productType: /* @ngInject */ ($transition$) =>
         $transition$.params().product.toUpperCase(),
-      resourcePath: /* @ngInject */ (productType, services) => {
-        const products = services[productType];
-        return get(head(get(products, 'data')), 'route.path');
-      },
-      products: /* @ngInject */ ($http, resourcePath) => {
-        const basePath = resourcePath.replace(/\/\{[a-zA-Z]+\}/g, '');
-        return $http
-          .get(basePath, {
-            headers: {
-              'X-Pagination-Mode': 'CachedobjectList-Pages',
-            },
-          })
-          .then(({ data }) => data);
-      },
+      products: /* @ngInject */ (productType, services) =>
+        services[productType].data,
+      resourcePath: /* @ngInject */ (products) =>
+        get(head(products), 'route.path'),
+      propertyId: /* @ngInject */ (products) =>
+        get(head(products), 'propertyId'),
+      apiPath: /* @ngInject */ (resourcePath) =>
+        resourcePath.replace(/\/\{[a-zA-Z]+\}/g, ''),
       schema: /* @ngInject */ ($http, resourcePath) =>
         $http
           .get('/')
@@ -44,11 +80,31 @@ export default /* @ngInject */ ($stateProvider) => {
         );
         return schema.models[model];
       },
-      columns: /* @ngInject */ (dataModel) =>
-        map(dataModel.properties, (value, name) => ({
-          title: name,
-          property: name,
-        })),
+      displayedColumns: /* @ngInject */ ($transition$) =>
+        JSON.parse($transition$.params().columns),
+      onColumnChange: /* @ngInject */ ($state, $transition$) => (id, columns) =>
+        $state.go('.', {
+          ...$transition$.params(),
+          columns: JSON.stringify(
+            map(
+              columns.filter(({ hidden }) => !hidden),
+              'name',
+            ),
+          ),
+        }),
+      columns: /* @ngInject */ (dataModel, displayedColumns, propertyId) => {
+        const columns = mapApiProperties(dataModel.properties).filter(
+          ({ title, type }) => type && title !== propertyId,
+        );
+        columns.unshift(getFirstColumnTemplate(propertyId));
+        return columns.map((column, index) => ({
+          ...column,
+          name: startCase(column.name),
+          hidden: isEmpty(displayedColumns)
+            ? index > DEFAULT_NUMBER_OF_COLUMNS
+            : !displayedColumns.includes(column.title),
+        }));
+      },
     },
   });
 };
