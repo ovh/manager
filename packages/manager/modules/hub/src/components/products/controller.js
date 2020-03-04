48b73443f498bbbd2ab01b369fcb2e95ec5f84b8
Marie JONES <marie.jones@corp.ovh.com>
2020-03-04 17:19:35 +0100

    feat(products): limit display to four products
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

---
+++ packages/manager/modules/hub/src/components/products/controller.js
@@ -0,0 +1,7 @@
+import { DISPLAYED_PRODUCTS_NUMBER } from './constants';
+
+export default class ProductsController {
+  constructor() {
+    this.DISPLAYED_PRODUCTS_NUMBER = DISPLAYED_PRODUCTS_NUMBER;
+  }
+}
