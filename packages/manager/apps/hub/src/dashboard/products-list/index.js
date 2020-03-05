22c1318a51b34ff7007a4889f34da99ab01f4b27
Marie JONES <marie.jones@corp.ovh.com>
2020-03-05 10:07:20 +0100

    feat: add special configuration for exchange service
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/hub/src/dashboard/products-list/index.js
+++ packages/manager/apps/hub/src/dashboard/products-list/index.js
@@ -2,10 +2,14 @@ import angular from 'angular';
 
 import ovhManagerHub from '@ovh-ux/manager-hub';
 
+import exchangeRouting from './exchange.routing';
 import routing from './routing';
 
 const moduleName = 'ovhManagerHubProductListingPage';
 
-angular.module(moduleName, [ovhManagerHub]).config(routing);
+angular
+  .module(moduleName, [ovhManagerHub])
+  .config(routing)
+  .config(exchangeRouting);
 
 export default moduleName;
--- packages/manager/modules/hub/src/components/product-list/controller.js
+++ packages/manager/modules/hub/src/components/product-list/controller.js
@@ -1,3 +1,9 @@
 import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
 
-export default class ManagerHubBillingProductList extends ListLayoutHelper.ListLayoutCtrl {}
+export default class ManagerHubBillingProductList extends ListLayoutHelper.ListLayoutCtrl {
+  $onInit() {
+    this.datagridId = `dg-${this.productType}`;
+
+    super.$onInit();
+  }
+}
