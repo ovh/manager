dc48823876ed29ce40e78eb28b702c8576968cc3
Jisay <jean-christophe.alleman@corp.ovh.com>
2020-03-11 16:56:27 +0100

    feat(vps2020): add upgrade from configuration tile
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

---
+++ packages/manager/modules/vps/src/dashboard/tile/configuration/component.js
@@ -0,0 +1,15 @@
+import controller from './controller';
+import template from './index.html';
+
+export default {
+  name: 'vpsDashboardTileConfiguration',
+  controller,
+  template,
+  bindings: {
+    configurationTile: '<',
+    catalog: '<',
+    vps: '<',
+    stateVps: '<',
+    goToUpgrade: '<',
+  },
+};
