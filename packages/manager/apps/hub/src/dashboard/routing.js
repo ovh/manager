cafa24c1fd457bad76ae8b55db0a0412ff61a9c1
Marie JONES <marie.jones@corp.ovh.com>
2020-03-12 14:11:52 +0100

    feat(products): limit number of tiles displayed (#2485)
    ref: MANAGER-4640
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/hub/src/dashboard/routing.js
+++ packages/manager/apps/hub/src/dashboard/routing.js
@@ -1,15 +1,21 @@
 import filter from 'lodash/filter';
 import groupBy from 'lodash/groupBy';
+import map from 'lodash/map';
 
 export default /* @ngInject */ ($stateProvider, $urlRouterProvider) => {
   $stateProvider.state('app.dashboard', {
     url: '/',
     resolve: {
-      products: /* @ngInject */ (catalog) =>
-        groupBy(
-          filter(catalog.data, ({ highlight }) => highlight),
-          'universe',
-        ),
+      products: /* @ngInject */ (catalog, services) =>
+        services.count === 0
+          ? groupBy(
+              filter(catalog.data, ({ highlight }) => highlight),
+              'universe',
+            )
+          : map(services.data, (service, productType) => ({
+              ...service,
+              productType,
+            })),
 
       goToProductPage: /* @ngInject */ ($state) => (product) =>
         product.includes('EXCHANGE')
