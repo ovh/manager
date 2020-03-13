7dab9d0ae2a8337d083ff997ddb9c8b3182df9e8
Jisay <jean-christophe.alleman@corp.ovh.com>
2020-03-13 16:26:29 +0100

    feat(vps2020): call upgrade API
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

---
+++ packages/manager/modules/vps/src/dashboard/tile/configuration/upgrade/constants.js
@@ -0,0 +1,8 @@
+export const ORDER_TRACKING_URLS = {
+  EU: 'https://www.ovh.com/manager/dedicated/#/billing/order',
+  CA: 'https://ca.ovh.com/manager/#/billing/order',
+};
+
+export default {
+  ORDER_TRACKING_URLS,
+};
