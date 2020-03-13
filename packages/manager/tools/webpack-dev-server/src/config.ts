e0c6fd6a18cd1a1980714988da0677c426876f9d
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2020-03-13 15:07:07 +0100

    feat(manager-webpack-dev-server): use process env as region fallback
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/tools/webpack-dev-server/src/config.ts
+++ packages/manager/tools/webpack-dev-server/src/config.ts
@@ -6,8 +6,12 @@ import Sso from './sso';
 import serverProxy from './proxy';
 
 export = (env) => {
-  const region = (env.region || process.env.npm_package_config_region || 'eu')
-    .toLowerCase();
+  const region = (
+    env.region ||
+    process.env.REGION ||
+    process.env.npm_package_config_region ||
+    'eu'
+  ).toLowerCase();
   const proxy = [serverProxy.v6(region)];
   const sso = new Sso(region);
 
@@ -15,9 +19,7 @@ export = (env) => {
     proxy.unshift(serverProxy.aapi);
   }
   if (env.dev) {
-    proxy.unshift(
-      ...env.dev.map(config => serverProxy.dev(config)),
-    );
+    proxy.unshift(...env.dev.map((config) => serverProxy.dev(config)));
   }
   return {
     mode: 'development',
@@ -35,7 +37,10 @@ export = (env) => {
       host: env.host || process.env.npm_package_config_host || 'localhost',
       https: env.https || yn(process.env.npm_package_config_https) || false,
       overlay: true,
-      port: env.port || Number.parseInt(process.env.npm_package_config_port, 10) || 9000,
+      port:
+        env.port ||
+        Number.parseInt(process.env.npm_package_config_port, 10) ||
+        9000,
       proxy,
     },
   };
