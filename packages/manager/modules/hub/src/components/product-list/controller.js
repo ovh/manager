6c1ddfc05e30e31dac8d91868ad47b4271ae08f2
Marie JONES <marie.jones@corp.ovh.com>
2020-03-13 08:37:18 +0100

    fix(products-list): handle pagination change with static rows (#2493)
    ref: MANAGER-4657
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/hub/src/components/product-list/controller.js
+++ packages/manager/modules/hub/src/components/product-list/controller.js
@@ -1,9 +1,27 @@
 import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
+import get from 'lodash/get';
 
 export default class ManagerHubBillingProductList extends ListLayoutHelper.ListLayoutCtrl {
   $onInit() {
     this.datagridId = `dg-${this.productType}`;
 
     super.$onInit();
+  }
+
+  loadStaticRows() {
+    return this.loadPage().then(({ meta }) => {
+      const offset = get(
+        this.ouiDatagridService,
+        `datagrids.${this.datagridId}.paging.offset`,
+      );
+      // $q is injected in super controller
+      return this.$q.resolve({
+        data: this.rows.slice(
+          offset - 1,
+          offset + parseInt(this.paginationSize, 10),
+        ),
+        meta,
+      });
+    });
   }
 }
