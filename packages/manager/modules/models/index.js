82d79c3aed4751b3a08cf2b77561d713c05a4217
Frederic Espiau <frederic.espiau@corp.ovh.com>
2020-03-16 11:18:11 +0100

    feat(models): add utilitarian Price class
    Signed-off-by: Frederic Espiau <frederic.espiau@corp.ovh.com>

--- packages/manager/modules/models/index.js
+++ packages/manager/modules/models/index.js
@@ -1,8 +1,9 @@
 import BillingService from './BillingService/BillingService.class';
 import Certificate from './certificate/Certificate.class';
+import Price from './Price/Price.class';
 import Ticket from './support/ticket/ticket.class';
 import User from './user/User.class';
 
-export { BillingService, Certificate, Ticket, User };
+export { BillingService, Certificate, Price, Ticket, User };
 
-export default { BillingService, Certificate, Ticket, User };
+export default { BillingService, Certificate, Price, Ticket, User };
