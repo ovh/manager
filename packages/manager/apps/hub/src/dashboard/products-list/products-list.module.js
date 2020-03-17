fc15c4926548bf346915052bad60069f116f7007
Marie JONES <marie.jones@corp.ovh.com>
2020-03-17 15:57:28 +0100

    feat(products-list): add special products configuration
    Add configuration for ip, sharepoint and vracks
    
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/hub/src/dashboard/products-list/products-list.module.js
+++ packages/manager/apps/hub/src/dashboard/products-list/products-list.module.js
@@ -3,6 +3,10 @@ import angular from 'angular';
 import ovhManagerHub from '@ovh-ux/manager-hub';
 
 import exchangeRouting from './exchange.routing';
+import ipRouting from './ip.routing';
+import sharepointRouting from './sharepoint.routing';
+import vrackRouting from './vrack.routing';
+
 import routing from './routing';
 
 const moduleName = 'ovhManagerHubProductListingPage';
@@ -10,6 +14,9 @@ const moduleName = 'ovhManagerHubProductListingPage';
 angular
   .module(moduleName, [ovhManagerHub])
   .config(routing)
-  .config(exchangeRouting);
+  .config(exchangeRouting)
+  .config(ipRouting)
+  .config(sharepointRouting)
+  .config(vrackRouting);
 
 export default moduleName;
--- packages/manager/apps/hub/src/dashboard/products-list/routing.js
+++ packages/manager/apps/hub/src/dashboard/products-list/routing.js
@@ -3,6 +3,21 @@ import find from 'lodash/find';
 import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
 import { urlQueryParams, params, component, resolves } from './config';
 
+export const genericProductResolve = {
+  apiPath: /* @ngInject */ (resourcePath) =>
+    resourcePath.replace(/\/\{[a-zA-Z]+\}/g, ''),
+  schema: /* @ngInject */ ($http, resourcePath) =>
+    $http
+      .get('/')
+      .then(({ data: schema }) =>
+        find(schema.apis, ({ path }) =>
+          new RegExp(`^${path}/`).test(resourcePath),
+        ),
+      )
+      .then(({ path }) => $http.get(`${path}.json`))
+      .then(({ data }) => data),
+};
+
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('app.dashboard.products', {
     url: `:product?${urlQueryParams}`,
@@ -11,20 +26,9 @@ export default /* @ngInject */ ($stateProvider) => {
     resolve: {
       ...ListLayoutHelper.stateResolves,
       ...resolves,
+      ...genericProductResolve,
       productType: /* @ngInject */ ($transition$) =>
         $transition$.params().product.toUpperCase(),
-      apiPath: /* @ngInject */ (resourcePath) =>
-        resourcePath.replace(/\/\{[a-zA-Z]+\}/g, ''),
-      schema: /* @ngInject */ ($http, resourcePath) =>
-        $http
-          .get('/')
-          .then(({ data: schema }) =>
-            find(schema.apis, ({ path }) =>
-              new RegExp(`^${path}/`).test(resourcePath),
-            ),
-          )
-          .then(({ path }) => $http.get(`${path}.json`))
-          .then(({ data }) => data),
     },
   });
 };
