7c1fde6107fb85b084a6b27a1c0962e3c2fdd6ba
Marie JONES <marie.jones@corp.ovh.com>
2020-03-19 17:43:37 +0100

    feat: redirect dedicated for CA
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/exchange/src/header/header.constants.js
+++ packages/manager/modules/exchange/src/header/header.constants.js
@@ -1,6 +1,6 @@
 export const AUTORENEW_URL = {
   EU: 'https://www.ovh.com/manager/dedicated/#/billing/autoRenew?searchText=',
-  CA: 'https://ca.ovh.com/manager/#/billing/autoRenew?searchText=',
+  CA: 'https://ca.ovh.com/manager/dedicated/#/billing/autoRenew?searchText=',
 };
 
 export default { AUTORENEW_URL };
