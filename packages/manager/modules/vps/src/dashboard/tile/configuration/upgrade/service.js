f81a9ff685ddf03a4f968d5b0bf6ae2fdc0172e2
Jisay <1774679+Jisay@users.noreply.github.com>
2020-03-19 16:59:53 +0100

    feat(vps2020): detect upgrade in progress
    Signed-off-by: Jisay <1774679+Jisay@users.noreply.github.com>

--- packages/manager/modules/vps/src/dashboard/tile/configuration/upgrade/service.js
+++ packages/manager/modules/vps/src/dashboard/tile/configuration/upgrade/service.js
@@ -1,7 +1,18 @@
+import head from 'lodash/head';
+import includes from 'lodash/includes';
+
 export default class VpsUpgradeService {
   /* @ngInject */
-  constructor($http) {
+  constructor($http, CucOvhPoll, VpsTaskService) {
     this.$http = $http;
+    this.CucOvhPoll = CucOvhPoll;
+    this.VpsTaskService = VpsTaskService;
+  }
+
+  getAvailableUpgrades(serviceName) {
+    return this.$http
+      .get(`/order/upgrade/vps/${serviceName}`)
+      .then(({ data }) => data);
   }
 
   getUpgrade(serviceName, planCode, params) {
@@ -18,5 +29,23 @@ export default class VpsUpgradeService {
         data: httpData,
       })
       .then(({ data }) => data);
+  }
+
+  getUpgradeTask(serviceName) {
+    return this.VpsTaskService.getPendingTasks(
+      serviceName,
+      'upgradeVm',
+    ).then((tasks) => head(tasks));
+  }
+
+  startUpgradeTaskPolling(upgradeTask, options = {}) {
+    const fullOptions = {
+      pollFunction: (task) =>
+        this.VpsTaskService.getTask(this.serviceName, task.id),
+      stopCondition: (task) => includes(['done', 'error'], task.state),
+      ...options,
+    };
+
+    return this.CucOvhPoll.poll({ item: upgradeTask, ...fullOptions });
   }
 }
