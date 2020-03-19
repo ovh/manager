73d7ed274f54caf7e6daee91733afdfd2853aeec
Marie JONES <marie.jones@corp.ovh.com>
2020-03-19 17:43:37 +0100

    feat: redirect dedicated for CA
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/iplb/src/iplb-url.constants.js
+++ packages/manager/modules/iplb/src/iplb-url.constants.js
@@ -2,14 +2,14 @@ export const RENEW_URL = {
   EU:
     'https://www.ovh.com/manager/dedicated/#/billing/autoRenew?selectedType={serviceType}&searchText={serviceName}',
   CA:
-    'https://ca.ovh.com/manager/#/billing/autorenew?selectedType={serviceType}&searchText={serviceName}',
+    'https://ca.ovh.com/manager/dedicated/#/billing/autorenew?selectedType={serviceType}&searchText={serviceName}',
 };
 
 export const CONTACTS_URL = {
   EU:
     'https://www.ovh.com/manager/dedicated/#/useraccount/contacts?tab=SERVICES&serviceName={serviceName}',
   CA:
-    'https://ca.ovh.com/manager/#/useraccount/contacts?tab=SERVICES&serviceName={serviceName}',
+    'https://ca.ovh.com/manager/dedicated/#/useraccount/contacts?tab=SERVICES&serviceName={serviceName}',
 };
 
 export const GUIDE_HOME_URL = 'https://docs.ovh.com';
