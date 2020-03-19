ba2fc07a2cf3828fa13a853fb6dabb9a3fd7c1b8
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2020-03-19 14:36:57 +0100

    build(web): avoid multiple entries in webpack config
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/web/webpack.config.js
+++ packages/manager/apps/web/webpack.config.js
@@ -2,11 +2,7 @@ const merge = require('webpack-merge');
 const path = require('path');
 const fs = require('fs');
 const glob = require('glob');
-const _ = require('lodash');
 const webpack = require('webpack');
-
-const folder = './client/app';
-const bundles = {};
 
 const webpackConfig = require('@ovh-ux/manager-webpack-config');
 
@@ -43,17 +39,6 @@ function getNgAppInjections(region) {
 
   return ngAppInjections || 'null';
 }
-
-fs.readdirSync(folder).forEach((file) => {
-  const stats = fs.lstatSync(`${folder}/${file}`);
-  if (file === 'components') return;
-  if (stats.isDirectory()) {
-    const jsFiles = glob.sync(`${folder}/${file}/**/!(*.module).js`);
-    if (jsFiles.length > 0) {
-      bundles[file] = jsFiles;
-    }
-  }
-});
 
 module.exports = (env = {}) => {
   const { config } = webpackConfig(
@@ -94,21 +79,11 @@ module.exports = (env = {}) => {
   const extras = glob.sync('./.extras/**/*.js');
 
   return merge(config, {
-    entry: _.assign(
-      {
-        app: [
-          './client/app/index.js',
-          './client/app/app.js',
-          './client/app/app.routes.js',
-          './client/app/app.controller.js',
-        ]
-          .concat(glob.sync('./client/app/**/*.module.js'))
-          .concat(glob.sync('./client/app/components/**/!(*.module).js')),
-      },
-      bundles,
-      extras.length > 0 ? { extras } : {},
-      extrasRegion.length > 0 ? { extrasRegion } : {},
-    ),
+    entry: {
+      main: path.resolve('./client/app/index.js'),
+      ...(extras.length > 0 ? { extras } : {}),
+      ...(extrasRegion.length > 0 ? { extrasRegion } : {}),
+    },
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: '[name].[chunkhash].bundle.js',
