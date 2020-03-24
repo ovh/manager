f1e32a1596ee3503d9db222e0010b37a414df086
Jisay <1774679+Jisay@users.noreply.github.com>
2020-03-24 10:51:40 +0100

    fix(vps2020): rebase issues
    Signed-off-by: Jisay <1774679+Jisay@users.noreply.github.com>

--- packages/manager/modules/vps/src/dashboard/tile/index.js
+++ packages/manager/modules/vps/src/dashboard/tile/index.js
@@ -1,9 +1,10 @@
 import angular from 'angular';
 
+import configurationTile from './configuration';
 import informationTile from './information';
 
 const moduleName = 'ovhManagerVpsDashboardTile';
 
-angular.module(moduleName, [informationTile]);
+angular.module(moduleName, [configurationTile, informationTile]);
 
 export default moduleName;
