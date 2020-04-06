e18365054f92c7273d2c5217063444f67d99e8c4
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2020-04-06 17:37:08 +0200

    feat(nasha): lazy load nasha root state
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/modules/nasha/src/index.js
+++ packages/manager/modules/nasha/src/index.js
@@ -1,80 +1,39 @@
 import angular from 'angular';
-
 import '@ovh-ux/manager-core';
-import '@ovh-ux/ng-ovh-cloud-universe-components';
-import '@ovh-ux/ng-ovh-doc-url';
-import '@ovh-ux/ng-ovh-responsive-popover';
-import '@ovh-ux/ng-ui-router-layout';
-
 import '@uirouter/angularjs';
-import 'angular-translate';
-import 'angular-ui-bootstrap';
-import 'ovh-api-services';
-import 'ovh-ui-angular';
+import 'oclazyload';
 
-import NashaCtrl from './controller';
-import NashaAddCtrl from './add/nasha-add.controller';
-import NashaOrderCompleteCtrl from './order/controller';
-import NashaUnavailableCtrl from './add/nasha-unavailable.controller';
-
-import informationTemplate from './information/nasha-information.html';
-import orderTemplate from './order/template.html';
-import usageHelpTemplate from './information/nasha-information-usage-help.html';
-
-import NashaAddService from './add/nasha-add.service';
-import partition from './partition';
-import routing from './routing';
-
-import './styles.less';
-import 'ovh-ui-kit-bs/dist/ovh-ui-kit-bs.css';
-import 'ovh-ui-kit/dist/oui.css';
-import './add/styles.less';
-
-const moduleName = 'ovhManagerNasha';
+const moduleName = 'ovhManagerNashaLazyLoading';
 
 angular
-  .module(moduleName, [
-    'ovhManagerCore',
-    'pascalprecht.translate',
-    'ui.router',
-    'ovh-api-services',
-    'ngOvhCloudUniverseComponents',
-    'ngOvhDocUrl',
-    'ngUiRouterLayout',
-    'ui.bootstrap',
-    'oui',
-    'ngOvhResponsivePopover',
-    partition,
-  ])
-  .config(routing)
+  .module(moduleName, ['ui.router', 'oc.lazyLoad', 'ovhManagerCore'])
   .config(
-    /* @ngInject */ (
-      $qProvider,
-      ovhDocUrlProvider,
-      TranslateServiceProvider,
-    ) => {
-      ovhDocUrlProvider.setUserLocale(TranslateServiceProvider.getUserLocale());
-      $qProvider.errorOnUnhandledRejections(false);
-    },
-  )
-  .controller('NashaCtrl', NashaCtrl)
-  .controller('NashaAddCtrl', NashaAddCtrl)
-  .controller('NashaOrderCompleteCtrl', NashaOrderCompleteCtrl)
-  .controller('NashaUnavailableCtrl', NashaUnavailableCtrl)
-  .service('NashaAddService', NashaAddService)
-  .run(/* @ngTranslationsInject:json ./translations */)
-  .run(
-    /* @ngInject */ ($templateCache) => {
-      $templateCache.put(
-        'nasha/information/nasha-information.html',
-        informationTemplate,
-      );
-      $templateCache.put(
-        'nasha/information/nasha-information-usage-help.html',
-        usageHelpTemplate,
-      );
-      $templateCache.put('nasha/order/template.html', orderTemplate);
+    /* @ngInject */ ($stateProvider) => {
+      const lazyLoad = ($transition$) => {
+        const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+
+        return import('./nasha.module').then((mod) =>
+          $ocLazyLoad.inject(mod.default || mod),
+        );
+      };
+
+      $stateProvider
+        .state('nasha.**', {
+          url: '/paas/nasha/:nashaId',
+          lazyLoad,
+        })
+        .state('nasha-order-complete.**', {
+          url: '/nasha/order/complete',
+          lazyLoad,
+        })
+        .state('nasha-add.**', {
+          url: '/nasha/new',
+          lazyLoad,
+        })
+        .state('nasha-unavailable.**', {
+          url: '/nasha/unavailable',
+          lazyLoad,
+        });
     },
   );
-
 export default moduleName;
