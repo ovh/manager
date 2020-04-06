190b39c689b1b8a9f960efff7d64bbcd8c233cd4
Marie JONES <marie.jones@corp.ovh.com>
2020-04-06 14:48:05 +0200

    Revert "refactor(billing.confirm-terminate): modularize and use component"
    This reverts commit 303bec55730b2ae457faa0c4ab1c8f973aa1420f.

--- packages/manager/apps/dedicated/client/app/billing/confirmTerminate/termination.routing.js
+++ packages/manager/apps/dedicated/client/app/billing/confirmTerminate/termination.routing.js
@@ -1,7 +1,11 @@
+import template from './billing-confirmTerminate.html';
+
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('app.account.billing.confirmTerminate', {
     url: '/confirmTerminate?id&token',
-    component: 'billingConfirmTermination',
+    template,
+    controller: 'Billing.controllers.TerminateServiceCtrl',
+    controllerAs: 'TerminateServiceCtrl',
     translations: { value: ['..'], format: 'json' },
   });
 };
