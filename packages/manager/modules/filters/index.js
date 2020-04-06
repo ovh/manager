282994622503964c766f310e7c8d1d60e65f4821
Jérémy De-Cesare <jeremy.de-cesare@corp.ovh.com>
2020-04-06 16:11:48 +0200

    feat(modules): add filters modules
    Signed-off-by: Jérémy De-Cesare <jeremy.de-cesare@corp.ovh.com>

---
+++ packages/manager/modules/filters/index.js
@@ -0,0 +1,9 @@
+import angular from 'angular';
+
+import bandwidth from './bandwidth';
+
+const moduleName = 'ovhManagerFilters';
+
+angular.module(moduleName, [bandwidth]);
+
+export default moduleName;
