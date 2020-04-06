faf2755a34c41691f55f11f1ea8ca463cb9bf550
Marie JONES <marie.jones@corp.ovh.com>
2020-04-06 15:10:57 +0200

    feat(billing.confirm-terminate): revamp termination form
    This reverts commit 6fc249d8003cac1cec35eccbd350a3dac5e4f553.
    
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/billing/confirmTerminate/termination.module.js
+++ packages/manager/apps/dedicated/client/app/billing/confirmTerminate/termination.module.js
@@ -2,12 +2,16 @@ import component from './confirm-terminate.component';
 import service from './billing-confirmTerminate.service';
 import routing from './termination.routing';
 
+import legacyService from './legacy/termination-legacy.service';
+
 const moduleName = 'ovhManagerBillingTermination';
 
 angular
   .module(moduleName, ['ui.router'])
   .config(routing)
   .service('BillingTerminate', service)
-  .component('billingConfirmTermination', component);
+  .service('BillingTerminateLegacy', legacyService)
+  .component('billingConfirmTermination', component)
+  .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
