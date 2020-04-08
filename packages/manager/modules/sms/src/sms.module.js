22914675eb9e1bc87baf917210955fe13efb38e5
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2020-04-08 14:41:49 +0200

    feat(sms): lazy load sms root state
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

---
+++ packages/manager/modules/sms/src/sms.module.js
@@ -0,0 +1,27 @@
+import angular from 'angular';
+
+import '@ovh-ux/manager-core';
+import '@uirouter/angularjs';
+import 'oclazyload';
+import 'ovh-api-services';
+
+import routing from './sms.routing';
+import component from './sms.component';
+
+import 'ovh-ui-kit/dist/oui.css';
+import 'ovh-ui-kit-bs/dist/ovh-ui-kit-bs.css';
+
+const moduleName = 'ovhManagerSms';
+
+angular
+  .module(moduleName, [
+    'ui.router',
+    'oc.lazyLoad',
+    'ovh-api-services',
+    'ovhManagerCore',
+  ])
+  .config(routing)
+  .component('ovhManagerSms', component)
+  .run(/* @ngTranslationsInject:json ./translations */);
+
+export default moduleName;
