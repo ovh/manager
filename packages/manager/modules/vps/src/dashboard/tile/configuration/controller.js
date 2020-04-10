45463e6bdbdadef403c99ae7dd5ce952d37aaf7e
Jisay <1774679+Jisay@users.noreply.github.com>
2020-04-10 11:42:08 +0200

    fix(vps2020): fix upfront display
    Signed-off-by: Jisay <1774679+Jisay@users.noreply.github.com>

--- packages/manager/modules/vps/src/dashboard/tile/configuration/controller.js
+++ packages/manager/modules/vps/src/dashboard/tile/configuration/controller.js
@@ -3,6 +3,7 @@ import VpsConfigurationTile from './service';
 export default class VpsDashboardTileConfigurationCtrl {
   constructor() {
     this.currentPlan = VpsConfigurationTile.currentPlan;
+    this.isUpfront = VpsConfigurationTile.isUpfront;
   }
 
   $onInit() {
--- packages/manager/modules/vps/src/dashboard/tile/configuration/upgrade/controller.js
+++ packages/manager/modules/vps/src/dashboard/tile/configuration/upgrade/controller.js
@@ -1,13 +1,7 @@
-import get from 'lodash/get';
-import startsWith from 'lodash/startsWith';
+import VpsConfigurationTile from '../service';
 
 export default class VpsDashboardTileConfigurationUpgradeCtrl {
   $onInit() {
-    const firstPlanPricingMode = get(
-      this.resolve.configurationTile.upgrades,
-      `${this.resolve.upgradeType}.plan.prices[0].pricingMode`,
-    );
-
-    this.isUpfront = startsWith(firstPlanPricingMode, 'upfront');
+    this.isUpfront = VpsConfigurationTile.isUpfront;
   }
 }
