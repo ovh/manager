14ba0a1d84d45f84b1a8a34a4f6ae7452a83261b
Jérémy De-Cesare <jeremy.de-cesare@corp.ovh.com>
2020-04-10 15:24:27 +0200

    feat(vps): handle new range cart options
    Signed-off-by: Jérémy De-Cesare <jeremy.de-cesare@corp.ovh.com>

--- packages/manager/modules/vps/src/additional-disk/vps-additional-disk.component.js
+++ packages/manager/modules/vps/src/additional-disk/vps-additional-disk.component.js
@@ -2,6 +2,9 @@ import controller from './vps-additional-disk.controller';
 import template from './vps-additional-disk.html';
 
 export default {
+  bindings: {
+    tabSummary: '<',
+  },
   controller,
   name: 'ovhManagerVpsAdditionnalDisk',
   template,
--- packages/manager/modules/vps/src/veeam/vps-veeam.component.js
+++ packages/manager/modules/vps/src/veeam/vps-veeam.component.js
@@ -6,6 +6,7 @@ export default {
   bindings: {
     goToVeeamRestore: '<',
     goToVeeamMount: '<',
+    tabSummary: '<',
   },
   name: 'vpsVeeam',
   template,
