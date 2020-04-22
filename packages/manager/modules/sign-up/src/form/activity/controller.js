baa5a7d17d8c053b8291844884a9f8695f01f6af
Jisay <1774679+Jisay@users.noreply.github.com>
2020-04-22 11:55:17 +0200

    fix(sign-up): use differents labels depending on selected legalform (#2768)
    * fix(sign-up): use differents labels depending on selected legalform
    
    Signed-off-by: Jisay <1774679+Jisay@users.noreply.github.com>
    
    * fix(sign-up): change labels
    
    Signed-off-by: Jisay <1774679+Jisay@users.noreply.github.com>

---
+++ packages/manager/modules/sign-up/src/form/activity/controller.js
@@ -0,0 +1,42 @@
+import get from 'lodash/get';
+
+import { COUNTRIES_VAT_LABEL } from './constants';
+
+export default class OvhSignUpActivityCtrl {
+  /* @ngInject */
+  constructor($filter) {
+    this.$filter = $filter;
+  }
+
+  /**
+   * Get the right vat field label depending on the selected legalform and selected country.
+   * If selected country has a dedicated vat label, add it to the field label.
+   * As some vat label in certain countries has special chars use an other sanitize strategy
+   * in order to be right encoded in HTML view.
+   */
+  getVatFieldLabel() {
+    const vatLabel = get(
+      COUNTRIES_VAT_LABEL,
+      this.signUpFormCtrl.model.country.toUpperCase(),
+    );
+
+    if (vatLabel) {
+      return this.$filter('translateDefault')(
+        `sign_up_activity_field_vat_${this.signUpFormCtrl.model.legalform}_more`,
+        'sign_up_activity_field_vat_more',
+        { vatLabel },
+        undefined,
+        false,
+        'escapeParameters',
+      );
+    }
+    return this.$filter('translateDefault')(
+      `sign_up_activity_field_vat_${this.signUpFormCtrl.model.legalform}`,
+      `sign_up_activity_field_vat`,
+      { vatLabel },
+      undefined,
+      false,
+      'escapeParameters',
+    );
+  }
+}
