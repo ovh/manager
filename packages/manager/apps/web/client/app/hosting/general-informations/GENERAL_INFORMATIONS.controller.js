e3a1e75c76c15ccef89a98f6babbf6075b19adfa
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2020-05-06 08:47:20 +0200

    feat(hosting): remove main domain information (#1935)
    ref: MANAGER-4179, MANAGER-3329
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/web/client/app/hosting/general-informations/GENERAL_INFORMATIONS.controller.js
+++ packages/manager/apps/web/client/app/hosting/general-informations/GENERAL_INFORMATIONS.controller.js
@@ -1,5 +1,4 @@
 import head from 'lodash/head';
-import includes from 'lodash/includes';
 import isEmpty from 'lodash/isEmpty';
 import isObject from 'lodash/isObject';
 
@@ -224,31 +223,6 @@ export default class HostingGeneralInformationsCtrl {
       type: 'action',
     });
     this.$state.go('app.hosting.upgrade', { productId: this.serviceName });
-  }
-
-  changeMainDomain() {
-    this.atInternet.trackClick({
-      name: 'web::hostname::general-informations::change-main-domain',
-      type: 'action',
-    });
-    this.$scope.setAction(
-      'change-main-domain/hosting-change-main-domain',
-      this.$scope.hosting,
-    );
-  }
-
-  isHostingOffer() {
-    return !includes(
-      [
-        'KIMSUFI_2015',
-        '__60_FREE',
-        'DEMO_1_G',
-        'START_1_M',
-        'START_10_M',
-        '_ASPFREE',
-      ],
-      this.$scope.hosting.offer,
-    );
   }
 
   goToBoostTab() {
