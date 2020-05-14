ef32b6b0d98d306aa27670fcb69ef96ecbd573bb
Marie JONES <marie.jones@corp.ovh.com>
2020-05-14 16:59:27 +0200

    feat(billing.confirm-terminate): use legacy for US
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/billing/confirmTerminate/termination.module.js
+++ packages/manager/apps/dedicated/client/app/billing/confirmTerminate/termination.module.js
@@ -2,12 +2,15 @@ import component from './confirm-terminate.component';
 import service from './billing-confirmTerminate.service';
 import routing from './termination.routing';
 
+import legacyService from './legacy/termination-legacy.service';
+
 const moduleName = 'ovhManagerBillingTermination';
 
 angular
   .module(moduleName, ['ui.router'])
   .config(routing)
   .service('BillingTerminate', service)
+  .service('BillingTerminateLegacy', legacyService)
   .component('billingConfirmTermination', component)
   .run(/* @ngTranslationsInject:json ./translations */);
 
