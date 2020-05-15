d00f083b68af1176b6e45449e7b62d705befed3f
Marie JONES <marie.jones@corp.ovh.com>
2020-05-15 08:19:57 +0200

    feat(domain.webhosting): display available modules depending on offer (#2944)
    'cloudweb1' offer is only available in EU so it can't be considered as a default plan code
    
    ref: MANAGER-5102
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/web/client/app/domain/webhosting/order/steps/module/domain-webhosting-order-steps-module.controller.js
+++ packages/manager/apps/web/client/app/domain/webhosting/order/steps/module/domain-webhosting-order-steps-module.controller.js
@@ -1,35 +1,39 @@
-import chunk from 'lodash/chunk';
 import sortBy from 'lodash/sortBy';
 
 import { LATEST_MODULES } from './domain-webhosting-order-steps-module.constants';
 
 export default class {
-  $onInit() {
-    this.availableModules = chunk(
-      sortBy(
-        this.availableModules
-          .filter(({ planCode }) =>
-            LATEST_MODULES.map(({ label }) => label).includes(planCode),
-          )
-          .map((module) => {
-            const { id, recommendedOffer } = LATEST_MODULES.find(
-              ({ label }) => label === module.planCode,
-            );
+  getModules() {
+    this.module = undefined;
+    this.isLoading = true;
+    return this.getAvailableModules({ offer: this.stepper.cartOption.offer })
+      .then((availableModules) => {
+        this.availableModules = sortBy(
+          availableModules
+            .filter(({ planCode }) =>
+              LATEST_MODULES.map(({ label }) => label).includes(planCode),
+            )
+            .map((module) => {
+              const { id, recommendedOffer } = LATEST_MODULES.find(
+                ({ label }) => label === module.planCode,
+              );
 
-            const [{ duration, pricingMode }] = module.prices;
+              const [{ duration, pricingMode }] = module.prices;
 
-            return {
-              id,
-              duration,
-              ...module,
-              pricingMode,
-              recommendedOffer,
-            };
-          }),
-        'id',
-      ),
-      3,
-    );
+              return {
+                id,
+                duration,
+                ...module,
+                pricingMode,
+                recommendedOffer,
+              };
+            }),
+          'id',
+        );
+      })
+      .finally(() => {
+        this.isLoading = false;
+      });
   }
 
   updateModule() {
