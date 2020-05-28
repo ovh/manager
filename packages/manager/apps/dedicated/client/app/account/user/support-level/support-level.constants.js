9c799fc8b7c81295ca3460c7209af19637e882df
Marie JONES <marie.jones@corp.ovh.com>
2020-05-28 16:22:56 +0200

    refactor(support-level): move class to model module
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/account/user/support-level/support-level.constants.js
+++ packages/manager/apps/dedicated/client/app/account/user/support-level/support-level.constants.js
@@ -1,32 +1,5 @@
 export const API_MODEL_SUPPORT_LEVEL = 'me.SupportLevel.LevelTypeEnum';
 
-export const INACTIVE_SUPPORT_LEVEL = ['premium-accredited'];
-
-export const AVAILABLE_SUPPORT_LEVEL = ['business', 'enterprise'];
-
-export const FULL_SUPPORT_LEVEL_SUBSIDIARIES = [
-  'ASIA',
-  'AU',
-  'CA',
-  'CZ',
-  'DE',
-  'ES',
-  'FI',
-  'FR',
-  'GB',
-  'IE',
-  'IT',
-  'LT',
-  'NL',
-  'PL',
-  'PT',
-  'QC',
-  'SG',
-  'SN',
-  'WE',
-  'WS',
-];
-
 export const URLS = {
   ASIA: {
     standard: 'https://www.ovhcloud.com/asia/support-levels/standard/',
@@ -170,8 +143,5 @@ export const URLS = {
 
 export default {
   API_MODEL_SUPPORT_LEVEL,
-  AVAILABLE_SUPPORT_LEVEL,
-  FULL_SUPPORT_LEVEL_SUBSIDIARIES,
-  INACTIVE_SUPPORT_LEVEL,
   URLS,
 };
---
+++ packages/manager/modules/models/support/level/support-level.constants.js
@@ -0,0 +1,32 @@
+export const INACTIVE_SUPPORT_LEVEL = ['premium-accredited'];
+
+export const AVAILABLE_SUPPORT_LEVEL = ['business', 'enterprise'];
+
+export const FULL_SUPPORT_LEVEL_SUBSIDIARIES = [
+  'ASIA',
+  'AU',
+  'CA',
+  'CZ',
+  'DE',
+  'ES',
+  'FI',
+  'FR',
+  'GB',
+  'IE',
+  'IT',
+  'LT',
+  'NL',
+  'PL',
+  'PT',
+  'QC',
+  'SG',
+  'SN',
+  'WE',
+  'WS',
+];
+
+export default {
+  AVAILABLE_SUPPORT_LEVEL,
+  FULL_SUPPORT_LEVEL_SUBSIDIARIES,
+  INACTIVE_SUPPORT_LEVEL,
+};
