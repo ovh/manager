21f6de06a1d71e1c91bc42c4224552602656e293
Marie JONES <marie.jones@corp.ovh.com>
2020-06-04 08:25:39 +0200

    fix(domain.zone): revert "feat(domain.zone): detach zone" (#3066)
    This reverts commit 49f63f65de39c9d324100867c2e308cc00328494.

--- packages/manager/apps/web/client/app/domain/zone/zone.module.js
+++ packages/manager/apps/web/client/app/domain/zone/zone.module.js
@@ -1,13 +1,9 @@
 import routing from './zone.routing';
 
 import activate from './activate/activate.module';
-import detach from './detach/detach.module';
 
 const moduleName = 'ovhManagerWebDomainZone';
 
-angular
-  .module(moduleName, [activate, detach])
-  .config(routing)
-  .run(/* @ngTranslationsInject:json ./translations */);
+angular.module(moduleName, [activate]).config(routing);
 
 export default moduleName;
--- packages/manager/apps/web/client/app/domain/zone/zone.routing.js
+++ packages/manager/apps/web/client/app/domain/zone/zone.routing.js
@@ -1,29 +1,5 @@
-import get from 'lodash/get';
 import controller from './domain-zone-dns.controller';
 import template from './domain-zone-dns.html';
-
-const commonResolves = {
-  detachZoneOptions: /* @ngInject */ (
-    $q,
-    ovhManagerProductOffersDetachService,
-    serviceInfos,
-  ) =>
-    serviceInfos
-      ? ovhManagerProductOffersDetachService.getAvailableDetachPlancodes(
-          serviceInfos.serviceId,
-        )
-      : $q.resolve([]),
-  serviceInfos: /* @ngInject */ ($http, $q, zoneOption) =>
-    zoneOption
-      ? $http
-          .get(`/domain/zone/${get(zoneOption, 'serviceName')}/serviceInfos`)
-          .then(({ data }) => data)
-      : $q.resolve(),
-  zoneOption: /* @ngInject */ ($http, domainName) =>
-    $http
-      .get(`/domain/${domainName}/options`)
-      .then((options) => options.data.zone),
-};
 
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('app.domain.product.zone', {
@@ -39,13 +15,8 @@ export default /* @ngInject */ ($stateProvider) => {
       rename: 'ZONE',
     },
     resolve: {
-      ...commonResolves,
       activateZone: /* @ngInject */ ($state) => () =>
         $state.go('app.domain.product.zone.activate'),
-      detachZoneLink: /* @ngInject */ ($state, domainName) =>
-        $state.href('app.domain.product.zone.detach', {
-          productId: domainName,
-        }),
       goToZone: /* @ngInject */ ($state, Alerter) => (
         message = false,
         type = 'success',
@@ -78,29 +49,10 @@ export default /* @ngInject */ ($stateProvider) => {
       rename: 'ZONE',
     },
     resolve: {
-      ...commonResolves,
       activateZone: /* @ngInject */ ($state) => () =>
         $state.go('app.domain.alldom.zone.activate'),
-      detachZoneLink: /* @ngInject */ ($state, domainName) =>
-        $state.href('app.domain.alldom.zone.detach', {
-          productId: domainName,
-        }),
-      goToZone: /* @ngInject */ ($state, Alerter) => (
-        message = false,
-        type = 'success',
-      ) => {
-        const reload = message && type === 'success';
-
-        const promise = $state.go('app.domain.alldom.zone', null, {
-          reload,
-        });
-
-        if (message) {
-          promise.then(() => Alerter[type](message, 'domain_alert_main'));
-        }
-
-        return promise;
-      },
+      goToZone: /* @ngInject */ ($state) => () =>
+        $state.go('app.domain.alldom.zone'),
     },
   });
 };
