2d95ceab1fc49e6dbe31d24eaa312ec663038eab
frenauvh <florian.renaut@corp.ovh.com>
2020-06-09 09:21:46 +0200

    fix(support): filter serviceTypes with own services
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/modules/support/src/tickets/new-ticket/issues-form/issues-form.service.js
+++ packages/manager/modules/support/src/tickets/new-ticket/issues-form/issues-form.service.js
@@ -1,6 +1,20 @@
+import compact from 'lodash/compact';
+import get from 'lodash/get';
+import snakeCase from 'lodash/snakeCase';
+import uniq from 'lodash/uniq';
+
+function getServiceTypeName(serviceType) {
+  const route = get(serviceType, 'route.path', '')
+    .replace(/\/\{[^}]+\}/g, '') // remove dynamic parts {serviceName} etc
+    .replace(/^\//, '') // remove first slash
+    .replace(/\//g, '_'); // replace remaining slashes with underscore
+  return snakeCase(route); // fooBar => foo_bar
+}
+
 export default class {
   /* @ngInject */
-  constructor($translate, OvhApiService, OvhApiSupport) {
+  constructor($http, $translate, OvhApiService, OvhApiSupport) {
+    this.$http = $http;
     this.$translate = $translate;
     this.OvhApiService = OvhApiService;
     this.OvhApiSupport = OvhApiSupport;
@@ -30,5 +44,43 @@ export default class {
           a.label.localeCompare(b.label, 'en', { sensitivity: 'base' }),
         ),
     );
+  }
+
+  filterOwnServiceTypes(serviceTypes) {
+    return this.$http
+      .get('/services', {
+        serviceType: 'apiv6',
+        headers: {
+          'X-Pagination-Mode': 'CachedObjectList-Pages',
+          'X-Pagination-Size': 5000,
+        },
+      })
+      .then(({ data }) => data)
+      .then((items) =>
+        compact(uniq(items.map((item) => getServiceTypeName(item)))),
+      )
+      .then((ownServiceTypes) =>
+        serviceTypes.filter((serviceType) => {
+          if (/\{|\*/.test(serviceType.route)) {
+            // L2 service
+            switch (serviceType.route) {
+              case '/telephony/*/line':
+                return ownServiceTypes.includes('telephony_lines');
+              case '/telephony/*/number':
+                return ownServiceTypes.includes('telephony_aliases');
+              case '/telephony/*/carrierSip':
+                return ownServiceTypes.includes('telephony_lines');
+              default: {
+                const parentServiceType = snakeCase(
+                  get(serviceType.route.match(/([^*{]+)\//), '1', ''),
+                );
+                return ownServiceTypes.includes(parentServiceType);
+              }
+            }
+          }
+          // L1 service
+          return ownServiceTypes.includes(serviceType.name);
+        }),
+      );
   }
 }
