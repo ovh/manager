4936c1fcfabc553f4d476498f41210e8c1ba2328
Yann Pauly <yann.pauly@ovhcloud.com>
2020-06-11 12:47:08 +0200

    feat(pci.dataprocessing): added price computation
    Signed-off-by: Yann Pauly <yann.pauly@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/data-processing/index.js
+++ packages/manager/modules/pci/src/projects/project/data-processing/index.js
@@ -3,6 +3,7 @@ import ovhManagerCore from '@ovh-ux/manager-core';
 import ngOvhCloudUniverseComponents from '@ovh-ux/ng-ovh-cloud-universe-components';
 import ngAtInternet from '@ovh-ux/ng-at-internet';
 import ngAtInternetUiRouterPlugin from '@ovh-ux/ng-at-internet-ui-router-plugin';
+import ovhManagerCatalogPrice from '@ovh-ux/manager-catalog-price';
 import '@uirouter/angularjs';
 import 'oclazyload';
 
@@ -16,6 +17,7 @@ angular
     ngAtInternetUiRouterPlugin,
     ovhManagerCore,
     ngOvhCloudUniverseComponents,
+    ovhManagerCatalogPrice,
   ])
   .config(
     /* @ngInject */ ($stateProvider) => {
--- packages/manager/modules/pci/src/projects/project/data-processing/submit-job/spark-sizing/spark-sizing.component.js
+++ packages/manager/modules/pci/src/projects/project/data-processing/submit-job/spark-sizing/spark-sizing.component.js
@@ -10,5 +10,8 @@ export default {
     validate: '<',
     templates: '<',
     jobParameters: '<',
+    projectId: '<',
+    prices: '<',
+    user: '<',
   },
 };
