d884fe6b303c7dc617e3ef33d32f4846bbab13b6
Cyril Biencourt <cyril.biencourt@corp.ovh.com>
2020-06-11 20:42:29 +0200

    fix(dedicatedcloud): change the way to fetch the catalog
    Signed-off-by: Cyril Biencourt <cyril.biencourt@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/dedicatedCloud/datacenter/backup/backup.service.js
+++ packages/manager/apps/dedicated/client/app/dedicatedCloud/datacenter/backup/backup.service.js
@@ -17,14 +17,15 @@ import BackupOffer from './backup-offer.class';
 export default class BackupService {
   /* @ngInject */
   constructor(
+    $http,
     $q,
     OvhApiDedicatedCloudDatacenter,
     OvhApiOrder,
     WucOrderCartService,
   ) {
+    this.$http = $http;
     this.$q = $q;
     this.backupApi = OvhApiDedicatedCloudDatacenter.Backup().v6();
-    this.catalogApi = OvhApiOrder.CatalogFormatted().v6();
     this.cartApi = OvhApiOrder.Cart().v6();
     this.WucOrderCartService = WucOrderCartService;
   }
@@ -117,8 +118,13 @@ export default class BackupService {
   }
 
   getCatalog(ovhSubsidiary) {
-    return this.catalogApi.get({ catalogName: 'privateCloud', ovhSubsidiary })
-      .$promise;
+    return this.$http
+      .get(`2api/sws/dedicatedcloud/catalog`, {
+        params: {
+          ovhSubsidiary,
+        },
+      })
+      .then((data) => get(data, 'data'));
   }
 
   getBackupOffers(serviceName, datacenterId, ovhSubsidiary) {
