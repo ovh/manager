4d27948b9e2363c9cbe7e8cb88219b133d06149a
Stephanie Moallic <stephanie.moallic@corp.ovh.com>
2020-07-01 13:42:03 +0200

    fix(overthebox): resolve error with library moment not found
    Signed-off-by: Stephanie Moallic <stephanie.moallic@corp.ovh.com>

--- packages/manager/apps/overthebox/src/index.js
+++ packages/manager/apps/overthebox/src/index.js
@@ -1,5 +1,6 @@
 import 'script-loader!jquery'; // eslint-disable-line
 import 'script-loader!lodash'; // eslint-disable-line
+import 'script-loader!moment/min/moment.min.js'; // eslint-disable-line
 
 import angular from 'angular';
 import ngOvhApiWrappers from '@ovh-ux/ng-ovh-api-wrappers';
