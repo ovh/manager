ca7414d227b5c95582d75b1ec12d8d6502e1120a
frenauvh <florian.renaut@corp.ovh.com>
2020-07-10 15:20:38 +0200

    feat(navbar-fragment): add navbar fragment
    BREAKING CHANGE: first release
    
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

---
+++ packages/manager/fragments/navbar/src/constants.js
@@ -0,0 +1,22 @@
+export const SECONDARY_UNIVERSES = [];
+
+export const MESSAGES = {
+  accountSidebarHide: 'ovh.account-sidebar.hide',
+  accountSidebarReady: 'ovh.account-sidebar.ready',
+  accountSidebarToggle: 'ovh.account-sidebar.toggle',
+
+  navbarSearch: 'navbar.search',
+
+  navigationSidebarShow: 'navigation-sidebar.show',
+
+  notificationsCount: 'ovh.notifications.count',
+  notificationsHide: 'ovh.notifications.hide',
+  notificationsToggle: 'ovh.notifications.toggle',
+
+  localeChange: 'locale.change',
+};
+
+export default {
+  MESSAGES,
+  SECONDARY_UNIVERSES,
+};
