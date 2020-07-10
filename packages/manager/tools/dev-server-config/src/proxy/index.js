4fd108ca585dec3b70666f4cca1267abc6928abf
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2020-07-10 14:36:19 +0200

    feat(dev-server-config): add registry proxy config
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/tools/dev-server-config/src/proxy/index.js
+++ packages/manager/tools/dev-server-config/src/proxy/index.js
@@ -1,9 +1,11 @@
 const aapi = require('./aapi');
 const dev = require('./dev');
 const v6 = require('./v6');
+const registry = require('./registry');
 
 module.exports = {
   aapi,
   dev,
   v6,
+  registry,
 };
