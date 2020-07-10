3a50b373a7b9285406e7cda5a675eb6b65b00829
frenauvh <florian.renaut@corp.ovh.com>
2020-07-10 15:09:53 +0200

    refactor(core): move fetch configuration to manager-config
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/modules/config/src/index.js
+++ packages/manager/modules/config/src/index.js
@@ -1,7 +1,37 @@
 import _Environment from './environment';
+import 'whatwg-fetch';
+
+export const HOSTNAME_REGIONS = {
+  'www.ovh.com': 'EU',
+  'ca.ovh.com': 'CA',
+  'us.ovhcloud.com': 'US',
+};
 
 export const Environment = _Environment;
 
-export default {
-  Environment,
+export const fetchConfiguration = () => {
+  return fetch('/engine/2api/configuration', {
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+      Accept: 'application/json',
+    },
+    credentials: 'same-origin',
+  })
+    .then((response) => {
+      if (response.status === 401) {
+        window.location.assign(
+          `/auth?action=disconnect&onsuccess=${encodeURIComponent(
+            window.location.href,
+          )}`,
+        );
+      }
+      return response.json();
+    })
+    .then((config) => {
+      _Environment.setRegion(config.region);
+      return config;
+    })
+    .catch(() => ({
+      region: HOSTNAME_REGIONS[window.location.hostname],
+    }));
 };
