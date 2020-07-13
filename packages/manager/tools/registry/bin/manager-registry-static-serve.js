290ce370dd533b003d59c832f31a8730a0d98653
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2020-07-13 09:14:44 +0200

    feat(registry): add static commands
    * add generate-manifest command to allow to generate manifests for a
      static registry folder
    * add serve command to allow to serve a static registry folder
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/tools/registry/bin/manager-registry-static-serve.js
+++ packages/manager/tools/registry/bin/manager-registry-static-serve.js
@@ -4,13 +4,13 @@ const path = require('path');
 const program = require('commander');
 const pkg = require('../package.json');
 
-const devServer = require('../src/dev');
+const serveStatic = require('../src/serveStatic');
 
 program
   .version(pkg.version)
-  .arguments('<fragmentsPath>')
+  .arguments('<registryPath>')
   .option('-p, --port <port>', 'server port', 8888)
-  .action((fragmentsPath, cmd) => {
-    devServer(path.resolve(fragmentsPath), cmd.port);
+  .action((registryPath, cmd) => {
+    serveStatic(path.resolve(registryPath), cmd.port);
   })
   .parse(process.argv);
