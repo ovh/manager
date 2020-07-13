9b6281c1593ad279afc377ec0b32963e5679edc4
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2020-07-13 11:43:21 +0200

    feat(registry-dev): add fallbackRegistry option in dev command
    allow to serve a dev registry with fallback to a remote registry
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/tools/registry/src/dev.js
+++ packages/manager/tools/registry/src/dev.js
@@ -5,59 +5,98 @@ const express = require('express');
 
 const manifestBuilder = require('./builder/manifest');
 const devStorage = require('./storage/dev-storage');
+const remoteStorage = require('./storage/remote-storage');
+const mergeInfos = require('./storage/merge-infos');
+
 const {
   REGISTRY_FILE,
   FRAGMENT_DEFINITION_FILE,
 } = require('./storage/constants');
 
-module.exports = (rootPath, port = 8888) => {
-  return devStorage.readInfos(rootPath).then((infos) => {
-    const app = express();
+module.exports = (rootPath, port = 8888, { fallbackRegistry }) => {
+  return Promise.all([
+    devStorage.readInfos(rootPath),
+    fallbackRegistry ? remoteStorage.readInfos(fallbackRegistry) : [],
+  ])
+    .then(([devInfos, fallbackInfos]) => mergeInfos(fallbackInfos, devInfos))
+    .then((infos) => {
+      const app = express();
 
-    app.get(`/${REGISTRY_FILE}`, (request, response) => {
-      response.status(200).json(manifestBuilder.buildRegistryManifest(infos));
-    });
+      app.get(`/${REGISTRY_FILE}`, (request, response) => {
+        response.status(200).json(manifestBuilder.buildRegistryManifest(infos));
+      });
 
-    app.get(`/:fragment/${FRAGMENT_DEFINITION_FILE}`, (request, response) => {
-      const { fragment } = request.params;
-      const manifest = manifestBuilder.buildFragmentManifest(infos, fragment);
-      if (manifest) {
-        response.status(200).json(manifest);
-      } else {
-        response.status(404).send(`Fragment "${fragment}" not found`);
-      }
-    });
+      app.get(`/:fragment/${FRAGMENT_DEFINITION_FILE}`, (request, response) => {
+        const { fragment } = request.params;
+        const manifest = manifestBuilder.buildFragmentManifest(infos, fragment);
+        if (manifest) {
+          response.status(200).json(manifest);
+        } else {
+          response.status(404).send(`Fragment "${fragment}" not found`);
+        }
+      });
 
-    app.get('/:fragment/:version/*', (request, response) => {
-      const { fragment, version } = request.params;
-      const fragmentVersion = infos.find(
-        (fragmentInfos) =>
-          fragmentInfos.name === fragment &&
-          fragmentInfos.versions.includes(version),
-      );
-      if (fragmentVersion) {
-        const fragmentDistPath = path.resolve(rootPath, fragment, 'dist');
+      app.get('/:fragment/:version/*', (request, response) => {
+        const { fragment, version } = request.params;
         const filepath = request.params[0] ? request.params[0] : '';
-        fs.exists(path.resolve(fragmentDistPath, filepath), (exists) => {
-          if (exists) {
-            response.sendFile(filepath, {
-              root: fragmentDistPath,
-            });
-          } else {
-            response
+        const fragmentVersion = infos.find(
+          (fragmentInfos) =>
+            fragmentInfos.name === fragment &&
+            fragmentInfos.versions.includes(version),
+        );
+
+        if (!fragmentVersion) {
+          return response.status(404).send(`Fragment "${fragment}" not found`);
+        }
+
+        return devStorage
+          .hasFragmentVersion(rootPath, fragment, version)
+          .then((hasDevVersion) => {
+            if (hasDevVersion) {
+              const fragmentDistPath = path.resolve(rootPath, fragment, 'dist');
+
+              return fs.exists(
+                path.resolve(fragmentDistPath, filepath),
+                (exists) => {
+                  if (exists) {
+                    return response.sendFile(filepath, {
+                      root: fragmentDistPath,
+                    });
+                  }
+                  return response
+                    .status(404)
+                    .send(
+                      `File "${filepath}" not found in fragment "${fragment}"`,
+                    );
+                },
+              );
+            }
+            if (fallbackRegistry) {
+              return remoteStorage
+                .getFileContent(fallbackRegistry, fragment, version, filepath)
+                .then((content) => response.status(200).send(content))
+                .catch(() =>
+                  response
+                    .status(404)
+                    .send(
+                      `File "${filepath}" not found in fragment "${fragment}" from fallback registry "${fallbackRegistry}"`,
+                    ),
+                );
+            }
+            return response
               .status(404)
-              .send(`File "${filepath}" not found in fragment "${fragment}"`);
-          }
-        });
-      } else {
-        response.status(404).send(`Fragment "${fragment}" not found`);
+              .send(`Fragment "${fragment}" not found`);
+          });
+      });
+
+      app.listen(port);
+
+      console.log(`Serve: ${rootPath} - localhost:${port}`);
+      if (fallbackRegistry) {
+        console.log(`Fallback registry: ${fallbackRegistry}`);
       }
+
+      return app;
     });
-
-    app.listen(port);
-    console.log(`Serve: ${rootPath} - localhost:${port}`);
-
-    return app;
-  });
 };
 /* eslint-enable no-console */
