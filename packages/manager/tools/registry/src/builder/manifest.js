748a66307b5d7982480b9dc1d0d71d7db5ddbd21
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2020-07-13 12:10:19 +0200

    fix(registry-manifest): sort fragments and versions
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/tools/registry/src/builder/manifest.js
+++ packages/manager/tools/registry/src/builder/manifest.js
@@ -1,11 +1,22 @@
+const semverCompare = require('semver/functions/compare');
+
 module.exports = {
   buildRegistryManifest: (infos) => {
     return {
-      fragments: infos.map((fragment) => fragment.name),
+      fragments: infos.map((fragment) => fragment.name).sort(),
     };
   },
 
   buildFragmentManifest: (infos, fragmentName) => {
-    return infos.find((fragment) => fragment.name === fragmentName);
+    const fragmentManifest = infos.find(
+      (fragment) => fragment.name === fragmentName,
+    );
+    if (fragmentManifest) {
+      return {
+        ...fragmentManifest,
+        versions: fragmentManifest.versions.sort(semverCompare),
+      };
+    }
+    return fragmentManifest;
   },
 };
