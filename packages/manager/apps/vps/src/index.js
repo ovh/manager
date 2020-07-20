889f537134b53d461ade9f59a066ca035c312601
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2020-07-20 13:59:26 +0200

    fix(vps-app): add babel-polyfill
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/vps/src/index.js
+++ packages/manager/apps/vps/src/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/extensions, import/no-webpack-loader-syntax */
+import 'babel-polyfill';
 import 'script-loader!jquery';
 import 'script-loader!moment/min/moment-with-locales.min';
 import 'script-loader!jsurl/lib/jsurl';
