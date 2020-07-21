a3e23e380b0c501370bb632afc9ea65c827e1101
Stephanie Moallic <stephanie.moallic@corp.ovh.com>
2020-07-21 15:35:11 +0200

    fix(overthebox): fix update with ui-kit v4
    Signed-off-by: Stephanie Moallic <stephanie.moallic@corp.ovh.com>

--- packages/manager/modules/overthebox/src/overthebox/details/index.js
+++ packages/manager/modules/overthebox/src/overthebox/details/index.js
@@ -2,7 +2,6 @@ import angular from 'angular';
 import ngTranslateAsyncLoader from '@ovh-ux/ng-translate-async-loader';
 import uiRouter from '@uirouter/angularjs';
 import angularTranslate from 'angular-translate';
-import 'ovh-ui-angular';
 
 import ovhManagerOtbWarning from '../warning';
 
@@ -19,7 +18,6 @@ angular
     uiRouter,
     angularTranslate,
     ovhManagerOtbWarning,
-    'oui',
   ])
   .component('overTheBoxDetails', component)
   .constant('OVERTHEBOX_DETAILS', constant)
--- packages/manager/modules/overthebox/src/overtheboxes.module.js
+++ packages/manager/modules/overthebox/src/overtheboxes.module.js
@@ -8,9 +8,6 @@ import '@ovh-ux/manager-core';
 import component from './overtheboxes.component';
 import routing from './overtheboxes.routing';
 
-import '@ovh-ux/ui-kit/dist/css/oui.css';
-import 'ovh-ui-kit-bs/dist/css/oui-bs3.css';
-
 const moduleName = 'ovhManagerOverTheBoxes';
 
 angular
