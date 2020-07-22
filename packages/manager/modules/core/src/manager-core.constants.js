181c98bf45295db8ede284a1a758261cce6a3a2d
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2020-07-22 10:18:43 +0200

    fix(core): use locale from config
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/modules/core/src/manager-core.constants.js
+++ packages/manager/modules/core/src/manager-core.constants.js
@@ -1,62 +1,3 @@
-export const LANGUAGES = {
-  available: [
-    {
-      name: 'Deutsch',
-      key: 'de_DE',
-    },
-    {
-      name: 'English',
-      key: 'en_GB',
-    },
-    {
-      name: 'Español',
-      key: 'es_ES',
-    },
-    {
-      name: 'Français',
-      key: 'fr_FR',
-    },
-    {
-      name: 'Français (Canadien)',
-      key: 'fr_CA',
-    },
-    {
-      name: 'Italiano',
-      key: 'it_IT',
-    },
-    {
-      name: 'Lietuviškai',
-      key: 'lt_LT',
-    },
-    {
-      name: 'Polski',
-      key: 'pl_PL',
-    },
-    {
-      name: 'Português',
-      key: 'pt_PT',
-    },
-    {
-      name: 'Suomi',
-      key: 'fi_FI',
-    },
-  ],
-  defaultLoc: 'fr_FR',
-  fallback: 'fr_FR',
-  preferred: {
-    en: {
-      US: 'GB',
-      CA: 'GB',
-      EU: 'GB',
-    },
-    fr: {
-      US: 'CA',
-      CA: 'CA',
-      EU: 'FR',
-    },
-  },
-};
-
 export const REDIRECT_URLS = {
   support: 'https://www.ovh.com/manager/dedicated/index.html#/ticket',
   billing: 'https://www.ovh.com/manager/dedicated/index.html#/billing/history',
@@ -607,7 +548,6 @@ export const HOSTNAME_REGIONS = {
 
 export default {
   HOSTNAME_REGIONS,
-  LANGUAGES,
   MANAGER_URLS,
   REDIRECT_URLS,
   URLS,
