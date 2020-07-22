c90bf71380ba0157a0f55fedba67eed7ed1b0411
Marie JONES <marie.jones@corp.ovh.com>
2020-07-22 17:39:13 +0200

    refactor: use layout for sms listing
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/sms/src/sms.module.js
+++ packages/manager/modules/sms/src/sms.module.js
@@ -1,12 +1,13 @@
 import angular from 'angular';
 
+import 'angular-translate';
 import '@ovh-ux/manager-core';
 import '@uirouter/angularjs';
 import 'oclazyload';
 import 'ovh-api-services';
 
+import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
 import routing from './sms.routing';
-import component from './sms.component';
 
 import '@ovh-ux/ui-kit/dist/css/oui.css';
 import 'ovh-ui-kit-bs/dist/css/oui-bs3.css';
@@ -19,9 +20,10 @@ angular
     'oc.lazyLoad',
     'ovh-api-services',
     'ovhManagerCore',
+    'pascalprecht.translate',
+    ListLayoutHelper.moduleName,
   ])
   .config(routing)
-  .component('ovhManagerSms', component)
   .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
--- packages/manager/modules/sms/src/sms.routing.js
+++ packages/manager/modules/sms/src/sms.routing.js
@@ -1,23 +1,28 @@
-import get from 'lodash/get';
 import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
+import { sms } from '@ovh-ux/manager-product-listing-configuration';
 
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('sms.index', {
     url: `?${ListLayoutHelper.urlQueryParams}`,
     views: {
       '': {
-        component: 'ovhManagerSms',
+        component: 'managerListLayout',
       },
     },
     params: ListLayoutHelper.stateParams,
     resolve: {
       apiPath: () => '/sms',
-      ...ListLayoutHelper.stateResolves,
-      schema: /* @ngInject */ (OvhApiSms) => OvhApiSms.v6().schema().$promise,
-      smsStatusTypes: /* @ngInject */ (schema) =>
-        get(schema.models, 'sms.StatusAccountEnum').enum,
+      header: /* @ngInject */ ($translate) => $translate.instant('sms_title'),
+      datagridId: () => 'sms',
+      defaultFilterColumn: () => 'name',
+      columnConfig: () => sms.getConfig(),
 
-      getSmsLink: /* @ngInject */ ($state) => ({ name: serviceName }) =>
+      ...ListLayoutHelper.stateResolves,
+
+      schema: /* @ngInject */ (OvhApiSms) => OvhApiSms.v6().schema().$promise,
+      dataModel: /* @ngInject */ (schema) => schema.models['sms.Account'],
+
+      getServiceNameLink: /* @ngInject */ ($state) => ({ name: serviceName }) =>
         $state.href('sms.service.dashboard', {
           serviceName,
         }),
@@ -25,6 +30,14 @@ export default /* @ngInject */ ($stateProvider) => {
         $state.go('sms.service.dashboard', {
           serviceName,
         }),
+
+      options: /* @ngInject */ ($translate, viewSms) => [
+        {
+          id: 'details',
+          label: $translate.instant('sms_view_sms_label'),
+          callback: (item) => viewSms(item),
+        },
+      ],
     },
   });
 };
