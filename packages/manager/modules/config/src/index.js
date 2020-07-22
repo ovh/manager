4c786275764dd2e4c194710f8cacce38e3b35f4f
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2020-07-22 10:15:20 +0200

    feat(config): add user locale in config
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/modules/config/src/index.js
+++ packages/manager/modules/config/src/index.js
@@ -1,7 +1,24 @@
 import _Environment from './environment';
 
+import {
+  convertLanguageFromOVHToBCP47 as _convertLanguageFromOVHToBCP47,
+  detectUserLocale as _detectUserLocale,
+  findLanguage as _findLanguage,
+} from './locale';
+
+import { LANGUAGES as _LANGUAGES } from './locale/locale.constants';
+
 export const Environment = _Environment;
 
+export const convertLanguageFromOVHToBCP47 = _convertLanguageFromOVHToBCP47;
+export const detectUserLocale = _detectUserLocale;
+export const findLanguage = _findLanguage;
+export const LANGUAGES = _LANGUAGES;
+
 export default {
+  convertLanguageFromOVHToBCP47,
   Environment,
+  detectUserLocale,
+  findLanguage,
+  LANGUAGES,
 };
---
+++ packages/manager/modules/config/src/locale/locale.constants.js
@@ -0,0 +1,67 @@
+export const LANGUAGES = {
+  available: [
+    {
+      name: 'Deutsch',
+      key: 'de_DE',
+    },
+    {
+      name: 'English',
+      key: 'en_GB',
+    },
+    {
+      name: 'Español',
+      key: 'es_ES',
+    },
+    {
+      name: 'Français',
+      key: 'fr_FR',
+    },
+    {
+      name: 'Français (Canadien)',
+      key: 'fr_CA',
+    },
+    {
+      name: 'Italiano',
+      key: 'it_IT',
+    },
+    {
+      name: 'Lietuviškai',
+      key: 'lt_LT',
+    },
+    {
+      name: 'Polski',
+      key: 'pl_PL',
+    },
+    {
+      name: 'Português',
+      key: 'pt_PT',
+    },
+    {
+      name: 'Suomi',
+      key: 'fi_FI',
+    },
+  ],
+  defaultLoc: 'fr_FR',
+  fallback: 'fr_FR',
+  preferred: {
+    en: {
+      US: 'GB',
+      CA: 'GB',
+      EU: 'GB',
+    },
+    fr: {
+      US: 'CA',
+      CA: 'CA',
+      EU: 'FR',
+    },
+  },
+};
+
+export const localeStorageKey = 'univers-selected-language';
+export const localeRegex = /^([a-zA-Z]+)(?:[_-]([a-zA-Z]+))?$/;
+
+export default {
+  LANGUAGES,
+  localeStorageKey,
+  localeRegex,
+};
