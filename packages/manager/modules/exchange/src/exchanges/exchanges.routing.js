fd7a5b11f830045929d1dedffeb85085e4b9b4df
Marie JONES <marie.jones@corp.ovh.com>
2020-07-27 17:26:29 +0200

    feat(exchange): add listing page
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

---
+++ packages/manager/modules/exchange/src/exchanges/exchanges.routing.js
@@ -0,0 +1,30 @@
+import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
+
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state('app.exchanges', {
+    url: `/configuration/exchange?${ListLayoutHelper.urlQueryParams}`,
+    component: 'managerListLayout',
+    params: ListLayoutHelper.stateParams,
+    resolve: {
+      ...ListLayoutHelper.stateResolves,
+      apiPath: () => '/email/exchange',
+      resources: /* @ngInject */ ($http) =>
+        $http
+          .get('/exchanges', {
+            serviceType: 'aapi',
+          })
+          .then(({ data }) => data),
+      dataModel: () => 'email.exchange.ExchangeService',
+      staticResources: () => true,
+      defaultFilterColumn: () => 'domain',
+      header: /* @ngInject */ ($translate) =>
+        $translate.instant('exchange_title'),
+      customizableColumns: () => true,
+      getServiceNameLink: /* @ngInject */ ($state) => ({ domain }) =>
+        $state.href('app.exchange', {
+          organization: domain,
+          productId: domain,
+        }),
+    },
+  });
+};
