23855219cd81819f5f7c644675d409df6b1c4119
Marie JONES <marie.jones@corp.ovh.com>
2020-07-27 14:52:17 +0200

    feat(hostings): add listing page
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

---
+++ packages/manager/apps/web/client/app/hosting/index.js
@@ -0,0 +1,32 @@
+import angular from 'angular';
+import '@uirouter/angularjs';
+import 'oclazyload';
+
+const moduleName = 'ovhManagerHostingsLazyLoading';
+
+angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
+  /* @ngInject */ ($stateProvider) => {
+    $stateProvider.state('app.hosting.**', {
+      url: '/configuration/hosting/:productId?tab',
+      lazyLoad: ($transition$) => {
+        const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+
+        return import('./hosting.module').then((mod) =>
+          $ocLazyLoad.inject(mod.default || mod),
+        );
+      },
+    });
+
+    $stateProvider.state('app.hostings.**', {
+      url: '/configuration/hosting',
+      lazyLoad: ($transition$) => {
+        const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+
+        return import('./hostings/hostings.module').then((mod) =>
+          $ocLazyLoad.inject(mod.default || mod),
+        );
+      },
+    });
+  },
+);
+export default moduleName;
