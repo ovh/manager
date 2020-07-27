58e358c81336a91d9b2f9a570b37fa9711f033b6
Marie JONES <marie.jones@corp.ovh.com>
2020-07-27 14:25:53 +0200

    feat(dns-zone): add listing page for DNS zone
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

---
+++ packages/manager/apps/web/client/app/dns-zone/index.js
@@ -0,0 +1,37 @@
+import angular from 'angular';
+import '@uirouter/angularjs';
+import 'oclazyload';
+
+import service from './dns-zone.service';
+
+const moduleName = 'ovhManagerDNSZonesLazyLoading';
+
+angular
+  .module(moduleName, ['ui.router', 'oc.lazyLoad'])
+  .config(
+    /* @ngInject */ ($stateProvider) => {
+      $stateProvider.state('app.domain.zone.**', {
+        url: '/configuration/zones',
+        lazyLoad: ($transition$) => {
+          const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+
+          return import('./zones/zones.module').then((mod) =>
+            $ocLazyLoad.inject(mod.default || mod),
+          );
+        },
+      });
+
+      $stateProvider.state('app.domain.dns-zone.**', {
+        url: '/configuration/zone/:productId',
+        lazyLoad: ($transition$) => {
+          const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+
+          return import('./zone.module').then((mod) =>
+            $ocLazyLoad.inject(mod.default || mod),
+          );
+        },
+      });
+    },
+  )
+  .service('DNSZoneService', service);
+export default moduleName;
