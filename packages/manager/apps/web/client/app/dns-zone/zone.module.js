6fcbefb0937b358e6af638050320f0e98dbcf38d
Marie JONES <marie.jones@corp.ovh.com>
2020-07-27 14:25:53 +0200

    feat(dns-zone): add listing page for DNS zone
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/web/client/app/dns-zone/zone.module.js
+++ packages/manager/apps/web/client/app/dns-zone/zone.module.js
@@ -5,14 +5,12 @@ import redirection from './redirection';
 import tasks from './tasks';
 
 import routing from './dns-zone.routing';
-import service from './dns-zone.service';
 
 const moduleName = 'ovhManagerWebDomainDnsZone';
 
 angular
   .module(moduleName, [dashboard, dynhost, newDnsZone, redirection, tasks])
   .config(routing)
-  .service('DNSZoneService', service)
   .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
