437687cdbc6139ef25fe703fa5dbadbb51f2b309
Marie JONES <marie.jones@corp.ovh.com>
2020-07-28 09:15:59 +0200

    feat(email-pro): add listing page
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/emailpro/src/index.js
+++ packages/manager/modules/emailpro/src/index.js
@@ -14,6 +14,16 @@ angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
       );
     };
 
+    $stateProvider.state('app.emails-pro.**', {
+      url: '/configuration/email_pro',
+      lazyLoad: ($transition$) => {
+        const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+
+        return import('./email-pro/email-pro.module').then((mod) =>
+          $ocLazyLoad.inject(mod.default || mod),
+        );
+      },
+    });
     $stateProvider
       .state('app.email-pro.**', {
         url: '/configuration/email_pro/:productId?tab',
