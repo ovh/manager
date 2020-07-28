4c0dfa1e4c77b01a34cc9f940ca4bbfbfcefbc4b
Marie JONES <marie.jones@corp.ovh.com>
2020-07-28 16:55:31 +0200

    fix(product-list): update to use latest list layout helper
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/hub/src/dashboard/products-list/exchange.routing.js
+++ packages/manager/apps/hub/src/dashboard/products-list/exchange.routing.js
@@ -1,40 +1,23 @@
-import { pick } from 'lodash-es';
-
 import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
-import { urlQueryParams, params, component, resolves } from './config';
+import { component, resolves } from './config';
 
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('app.dashboard.email_exchange_service', {
-    url: `email_exchange_service?${urlQueryParams}`,
-    params,
+    url: `email_exchange_service?${ListLayoutHelper.urlQueryParams}`,
+    params: ListLayoutHelper.stateParams,
     component,
     resolve: {
       ...resolves,
-      ...pick(ListLayoutHelper.stateResolves, [
-        'onListParamsChange',
-        'filter',
-        'sort',
-        'sortOrder',
-      ]),
+      staticResources: () => true,
+
       productType: /* @ngInject */ () => 'EMAIL_EXCHANGE_SERVICE',
       apiPath: /* @ngInject */ () => '/email/exchange',
-      schema: /* @ngInject */ ($http, apiPath) =>
-        $http.get(`${apiPath}.json`).then(({ data }) => data),
       resources: /* @ngInject */ ($http) =>
         $http
           .get('/exchanges', {
             serviceType: 'aapi',
           })
           .then(({ data }) => data),
-      staticResources: () => true,
-      paginationNumber: /* @ngInject */ ($transition$) =>
-        $transition$.paramsChanged().filter &&
-        !$transition$.paramsChanged().page
-          ? 1
-          : $transition$.params().page,
-      paginationSize: /* @ngInject */ ($transition$) =>
-        $transition$.params().pageSize,
-      paginationTotalCount: /* @ngInject */ (resources) => resources.length,
     },
     atInternet: {
       rename: 'app::dashboard::products::email-exchange-service',
--- packages/manager/apps/hub/src/dashboard/products-list/sharepoint.routing.js
+++ packages/manager/apps/hub/src/dashboard/products-list/sharepoint.routing.js
@@ -1,26 +1,16 @@
-import { pick } from 'lodash-es';
-
 import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
-import { urlQueryParams, params, component, resolves } from './config';
+import { component, resolves } from './config';
 
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('app.dashboard.ms_services_sharepoint', {
-    url: `ms_services_sharepoint?${urlQueryParams}`,
-    params,
+    url: `ms_services_sharepoint?${ListLayoutHelper.urlQueryParams}`,
+    params: ListLayoutHelper.stateParams,
     component,
     resolve: {
       ...resolves,
-      ...pick(ListLayoutHelper.stateResolves, [
-        'onListParamsChange',
-        'filter',
-        'sort',
-        'sortOrder',
-      ]),
       productType: () => 'MS_SERVICES_SHAREPOINT',
       apiPath: () => '/msServices',
       resourcePath: () => '/msServices/{serviceName}/sharepoint',
-      schema: /* @ngInject */ ($http, apiPath) =>
-        $http.get(`${apiPath}.json`).then(({ data }) => data),
       resources: /* @ngInject */ ($http) =>
         $http
           .get('/sharepoints', {
@@ -28,14 +18,6 @@ export default /* @ngInject */ ($stateProvider) => {
           })
           .then(({ data }) => data),
       staticResources: () => true,
-      paginationNumber: /* @ngInject */ ($transition$) =>
-        $transition$.paramsChanged().filter &&
-        !$transition$.paramsChanged().page
-          ? 1
-          : $transition$.params().page,
-      paginationSize: /* @ngInject */ ($transition$) =>
-        $transition$.params().pageSize,
-      paginationTotalCount: /* @ngInject */ (resources) => resources.length,
     },
     atInternet: {
       rename: 'app::dashboard::products::ms-services-sharepoint',
