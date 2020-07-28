0cebb999df7ed82f386d014f6b2df5ed5909fd9c
Adrien Carreira <adrien.carreira@ovhcloud.com>
2020-07-28 16:23:44 +0200

    feat(pci.serving): training
    Signed-off-by: Adrien Carreira <adrien.carreira@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/training/jobs/info/index.js
+++ packages/manager/modules/pci/src/projects/project/training/jobs/info/index.js
@@ -7,7 +7,7 @@ const moduleName = 'ovhManagerPciTrainingJobsInfoLazyLoading';
 angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
   /* @ngInject */ ($stateProvider) => {
     $stateProvider.state('pci.projects.project.training.jobs.info.**', {
-      url: '/info',
+      url: '/:jobId',
       lazyLoad: ($transition$) => {
         const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
 
--- packages/manager/modules/pci/src/projects/project/training/jobs/list/list.routing.js
+++ packages/manager/modules/pci/src/projects/project/training/jobs/list/list.routing.js
@@ -1,6 +1,6 @@
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('pci.projects.project.training.jobs.list', {
-    url: '/list',
+    url: '/',
     component: 'pciProjectTrainingJobsListComponent',
     resolve: {
       breadcrumb: () => null, // Hide breadcrumb
