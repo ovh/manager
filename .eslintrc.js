eb673ff2ecc92c3bf117d7e13c4ff181cf5b4d0c
Antoine Leblanc <antoine.leblanc@ovhcloud.com>
2020-07-29 10:26:34 +0200

    build(eslint): remove extra settings entry
    Remove dependency `eslint-import-resolver-lerna`.
    uses: `yarn remove -W eslint-import-resolver-lerna`
    
    Signed-off-by: Antoine Leblanc <antoine.leblanc@ovhcloud.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 module.exports = {
   root: true,
   parser: 'babel-eslint',
@@ -23,16 +21,5 @@ module.exports = {
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
     'import/no-unresolved': 0,
     'prettier/prettier': 'error',
-  },
-  settings: {
-    'import/resolver': {
-      'eslint-import-resolver-lerna': {
-        packages: [
-          path.resolve(__dirname, 'packages/manager/apps'),
-          path.resolve(__dirname, 'packages/manager/modules'),
-          path.resolve(__dirname, 'packages/manager/tools'),
-        ],
-      },
-    },
   },
 };
