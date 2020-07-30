b9b7744a1ec4afc193ec076597e416e8509a9230
Marie JONES <marie.jones@corp.ovh.com>
2020-07-30 17:39:08 +0200

    feat(dedicated-cloud): add listing page
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/dedicatedCloud/index.js
+++ packages/manager/apps/dedicated/client/app/dedicatedCloud/index.js
@@ -2,10 +2,27 @@ import angular from 'angular';
 import '@uirouter/angularjs';
 import 'oclazyload';
 
-const moduleName = 'ovhManagerDedicatedCloudLazyloading';
+const moduleName = 'ovhManagerDedicatedCloudLazyLoading';
 
 angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
   /* @ngInject */ ($stateProvider) => {
+    $stateProvider.state('app.dedicatedCloud', {
+      url: '/configuration/dedicated_cloud',
+      abstract: true,
+      template: '<div data-ui-view></div>',
+    });
+
+    $stateProvider.state('app.dedicatedCloud.index.**', {
+      url: '',
+      lazyLoad: ($transition$) => {
+        const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+
+        return import('./dedicatedClouds/dedicatedClouds.module').then((mod) =>
+          $ocLazyLoad.inject(mod.default || mod),
+        );
+      },
+    });
+
     $stateProvider.state('app.dedicatedClouds.**', {
       url: '/configuration/dedicated_cloud/:productId',
       lazyLoad: ($transition$) => {
