8d80cd44015b0c02f6bff4d2eb7a524a628128d7
Marie JONES <marie.jones@corp.ovh.com>
2020-08-03 13:59:05 +0200

    feat(iplb): add listing page
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/iplb/src/index.js
+++ packages/manager/modules/iplb/src/index.js
@@ -2,6 +2,8 @@ import angular from 'angular';
 import '@uirouter/angularjs';
 import 'oclazyload';
 
+import iplbTemplate from './template.html';
+
 const moduleName = 'ovhManagerIplbLazyLoading';
 
 angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
@@ -14,12 +16,35 @@ angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
               `,
         abstract: true,
       })
-      .state('network.iplb.**', {
+      .state('network.iplb', {
         url: '/iplb',
+        abstract: true,
+        views: {
+          networkContainer: {
+            template: iplbTemplate,
+          },
+        },
+        translations: {
+          value: ['../common', '.'],
+          format: 'json',
+        },
+      })
+      .state('network.iplb.detail.**', {
+        url: '/:serviceName',
         lazyLoad: ($transition$) => {
           const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
 
           return import('./iplb.module').then((mod) =>
+            $ocLazyLoad.inject(mod.default || mod),
+          );
+        },
+      })
+      .state('network.iplb.index.**', {
+        url: '',
+        lazyLoad: ($transition$) => {
+          const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+
+          return import('./iplb/iplb.module').then((mod) =>
             $ocLazyLoad.inject(mod.default || mod),
           );
         },
---
+++ packages/manager/modules/iplb/src/iplb/iplb.routing.js
@@ -0,0 +1,25 @@
+import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
+
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state('network.iplb.index', {
+    url: `?${ListLayoutHelper.urlQueryParams}`,
+    views: {
+      iplbContainer: {
+        component: 'managerListLayout',
+      },
+    },
+    params: ListLayoutHelper.stateParams,
+    resolve: {
+      ...ListLayoutHelper.stateResolves,
+      apiPath: () => '/ipLoadbalancing',
+      dataModel: () => 'ipLoadbalancing.Ip',
+      defaultFilterColumn: () => 'serviceName',
+      header: /* @ngInject */ ($translate) => $translate.instant('iplb_title'),
+      customizableColumns: () => true,
+      getServiceNameLink: /* @ngInject */ ($state) => ({ serviceName }) =>
+        $state.href('network.iplb.detail', {
+          serviceName,
+        }),
+    },
+  });
+};
