99b52b45032fa6f00ae3829d534df1ca6856dc0d
Marie JONES <marie.jones@corp.ovh.com>
2020-08-03 11:27:07 +0200

    feat(nas): add listing page
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/app.js
+++ packages/manager/apps/dedicated/client/app/app.js
@@ -61,6 +61,7 @@ import ovhManagerPccDashboard from './dedicatedCloud/dashboard';
 import ovhManagerPccResourceUpgrade from './dedicatedCloud/resource/upgrade';
 
 import dedicatedServer from './dedicated/server';
+import dedicatedNas from './dedicated/nas';
 
 import datacenterBackup from './dedicatedCloud/datacenter/backup';
 import userContracts from './user-contracts';
@@ -88,6 +89,7 @@ angular
       dedicatedCloudDatacenterDashboardDeleteDrp,
       dedicatedCloudTerminate,
       dedicatedCloudDashboard,
+      dedicatedNas,
       dedicatedServer,
       dedicatedUniverseComponents,
       'directives',
---
+++ packages/manager/apps/dedicated/client/app/dedicated/nas/nas/nas.routing.js
@@ -0,0 +1,28 @@
+import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
+
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state('app.networks.nas.index', {
+    url: `?${ListLayoutHelper.urlQueryParams}`,
+    views: {
+      nasView: {
+        component: 'managerListLayout',
+      },
+    },
+    params: ListLayoutHelper.stateParams,
+    resolve: {
+      ...ListLayoutHelper.stateResolves,
+      apiPath: () => '/dedicated/nas',
+      dataModel: () => 'dedicated.nas.Nas',
+      defaultFilterColumn: () => 'serviceName',
+      header: () => 'NAS',
+      customizableColumns: () => true,
+      getServiceNameLink: /* @ngInject */ ($state) => ({
+        serviceName: nasId,
+      }) =>
+        $state.href('app.networks.nas.details', {
+          nasId,
+          nasType: 'nas',
+        }),
+    },
+  });
+};
