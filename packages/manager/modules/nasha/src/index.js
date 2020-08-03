01d8a980205c5934ef59dfac9f35a6a3da875d3e
Marie JONES <marie.jones@corp.ovh.com>
2020-08-03 10:12:43 +0200

    feat(nasha): add listing page
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/nasha/src/index.js
+++ packages/manager/modules/nasha/src/index.js
@@ -31,6 +31,16 @@ angular
           url: '/paas/nasha/:nashaId',
           lazyLoad,
         })
+        .state('nashas.**', {
+          url: '/paas/nasha',
+          lazyLoad: ($transition$) => {
+            const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+
+            return import('./nashas/nasha.module').then((mod) =>
+              $ocLazyLoad.inject(mod.default || mod),
+            );
+          },
+        })
         .state('nasha-add.**', {
           url: '/nasha/new',
           lazyLoad,
