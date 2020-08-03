12e17bdf100438d273a5f7ac88543536f378bd58
Marie JONES <marie.jones@corp.ovh.com>
2020-08-03 11:49:49 +0200

    feat(veeam-cc): add listing page
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/veeam-cloud-connect/src/index.js
+++ packages/manager/apps/veeam-cloud-connect/src/index.js
@@ -15,4 +15,9 @@ angular
       const defaultLanguage = Environment.getUserLanguage();
       moment.locale(defaultLanguage);
     },
+  )
+  .config(
+    /* @ngInject */ ($urlRouterProvider) => {
+      $urlRouterProvider.otherwise('/paas/veeam');
+    },
   );
