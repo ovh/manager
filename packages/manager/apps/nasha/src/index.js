5334e7c17b807ceac00df93c9cb1f4c6e7ae6a0d
Marie JONES <marie.jones@corp.ovh.com>
2020-08-03 10:25:32 +0200

    perf(nasha): import oui css in app instead of module
    This will prevent multiple css dist import and stylesheet overwrite
    
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/nasha/src/index.js
+++ packages/manager/apps/nasha/src/index.js
@@ -3,6 +3,9 @@
 import 'script-loader!jquery';
 import 'script-loader!moment/min/moment.min.js';
 
+import 'ovh-ui-kit-bs/dist/css/oui-bs3.css';
+import '@ovh-ux/ui-kit/dist/css/oui.css';
+
 /* eslint-enable import/no-webpack-loader-syntax, import/extensions */
 
 import { Environment } from '@ovh-ux/manager-config';
