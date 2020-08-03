4cfede427934b6c43ff57d025d29216fe90b0e96
Marie JONES <marie.jones@corp.ovh.com>
2020-08-03 17:09:59 +0200

    feat(cda): add listing page
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/cloud/client/app/cda/cda.js
+++ packages/manager/apps/cloud/client/app/cda/cda.js
@@ -1,18 +1,5 @@
-angular
-  .module('managerApp')
-  .config(($stateProvider) => {
-    $stateProvider.state('paas.cda', {
-      url: '/cda',
-      template: '<div data-ui-view="cdaDetails"></div>',
-      translations: {
-        format: 'json',
-        value: ['.'],
-      },
-      abstract: true,
-    });
-  })
-  .run(($transitions, CdaService) => {
-    $transitions.onSuccess({ to: 'paas.cda.**' }, (transition) => {
-      CdaService.initDetails(transition.params().serviceName);
-    });
+angular.module('managerApp').run(($transitions, CdaService) => {
+  $transitions.onSuccess({ to: 'paas.cda.**' }, (transition) => {
+    CdaService.initDetails(transition.params().serviceName);
   });
+});
