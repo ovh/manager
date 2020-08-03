1935bd87e6a87bd87a487e55f2f3c2ed44aacc65
Marie JONES <marie.jones@corp.ovh.com>
2020-08-03 13:59:05 +0200

    feat(iplb): add listing page
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/iplb/src/routing.js
+++ packages/manager/modules/iplb/src/routing.js
@@ -1,32 +1,19 @@
-import iplbTemplate from './template.html';
 import iplbDetailTemplate from './iplb-detail.html';
 
 export default /* @ngInject */ ($stateProvider) => {
-  $stateProvider
-    .state('network.iplb', {
-      url: '/iplb',
-      views: {
-        networkContainer: {
-          template: iplbTemplate,
-        },
+  $stateProvider.state('network.iplb.detail', {
+    url: '/{serviceName}',
+    redirectTo: 'network.iplb.detail.home',
+    views: {
+      iplbContainer: {
+        template: iplbDetailTemplate,
+        controller: 'IpLoadBalancerDetailCtrl',
+        controllerAs: 'ctrl',
       },
-      translations: {
-        value: ['../common', '.'],
-        format: 'json',
-      },
-    })
-    .state('network.iplb.detail', {
-      url: '/{serviceName}',
-      views: {
-        iplbContainer: {
-          template: iplbDetailTemplate,
-          controller: 'IpLoadBalancerDetailCtrl',
-          controllerAs: 'ctrl',
-        },
-      },
-      translations: {
-        value: ['../cloud', './configuration', '../vrack/modals'],
-        format: 'json',
-      },
-    });
+    },
+    translations: {
+      value: ['../cloud', './configuration', '../vrack/modals'],
+      format: 'json',
+    },
+  });
 };
