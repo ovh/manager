c2212e9c7597fb1ce6b1ae6155eecda71947d691
Marie JONES <marie.jones@corp.ovh.com>
2020-08-04 17:22:25 +0200

    fix(billing.autorenew): import renew constant
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/billing/src/index.js
+++ packages/manager/modules/billing/src/index.js
@@ -1,9 +1,12 @@
 import angular from 'angular';
 
 import servicesActions from './components/services-actions';
+import { RENEW_URL } from './components/services-actions/service-actions.constants';
 
 const moduleName = 'ovhManagerBilling';
 
 angular.module(moduleName, [servicesActions]);
+
+export { RENEW_URL };
 
 export default moduleName;
