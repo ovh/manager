71329f533c4fee47f218dcb087195229b4ef6f0c
Marie JONES <marie.jones@corp.ovh.com>
2020-08-04 08:43:32 +0200

    feat(catalog): add filters to url (#3315)
    ref: MANAGER-5380
    
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/hub/src/dashboard/catalog/routing.js
+++ packages/manager/apps/hub/src/dashboard/catalog/routing.js
@@ -2,13 +2,37 @@ import { filter, map, uniq } from 'lodash-es';
 
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('app.dashboard.catalog', {
-    url: 'catalog',
+    url: 'catalog?q&universes&categories',
     component: 'hubCatalog',
+    params: {
+      q: {
+        value: null,
+        squash: true,
+        dynamic: true,
+        type: 'string',
+      },
+      universes: {
+        value: '[]',
+        squash: true,
+        dynamic: true,
+      },
+      categories: {
+        value: '[]',
+        squash: true,
+        dynamic: true,
+      },
+    },
     resolve: {
       products: /* @ngInject */ (catalog) =>
         filter(catalog.data, ({ category }) => category),
       universes: /* @ngInject */ (products) =>
         uniq(map(products, 'universe')).sort(),
+      selectedUniverses: /* @ngInject */ ($transition$) =>
+        JSON.parse($transition$.params().universes),
+      selectedCategories: /* @ngInject */ ($transition$) =>
+        JSON.parse($transition$.params().categories),
+      searchText: /* @ngInject */ ($transition$) => $transition$.params().q,
+      onSearch: /* @ngInject */ ($state) => (params) => $state.go('.', params),
       breadcrumb: /* @ngInject */ ($translate) =>
         $translate.instant('manager_hub_catalog_breadcrumb'),
     },
