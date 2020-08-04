2b86609001ec513f6b765ab61cb9ed46f94105d0
Antoine Leblanc <antoine.leblanc@ovhcloud.com>
2020-08-04 12:31:14 +0200

    fix(kubernetes.details.node-pool.add): remove non-supported character
    Closes #3065
    
    Signed-off-by: Antoine Leblanc <antoine.leblanc@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/kubernetes/details/node-pool/add/add.constants.js
+++ packages/manager/modules/pci/src/projects/project/kubernetes/details/node-pool/add/add.constants.js
@@ -1,4 +1,4 @@
-export const NODE_POOL_NAME_REGEX = "^[a-z0-9]([-\.a-z0-9]*[a-z0-9])?$";
+export const NODE_POOL_NAME_REGEX = '^[a-z0-9]([-a-z0-9]*[a-z0-9])?$';
 
 export default {
   NODE_POOL_NAME_REGEX,
