1bdc5b631dede495946c25816f364c73b3edd98d
Marie JONES <marie.jones@corp.ovh.com>
2020-08-06 17:25:11 +0200

    feat: add breadcrumb (#3401)
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/telecom-task/src/index.js
+++ packages/manager/apps/telecom-task/src/index.js
@@ -2,6 +2,13 @@ import 'script-loader!jquery'; // eslint-disable-line
 import 'script-loader!lodash'; // eslint-disable-line
 
 import angular from 'angular';
+
+import ngUiRouterBreadcrumb from '@ovh-ux/ng-ui-router-breadcrumb';
 import ovhManagerTelecomTask from '@ovh-ux/manager-telecom-task';
 
-angular.module('telecomTaskApp', [ovhManagerTelecomTask]);
+angular
+  .module('telecomTaskApp', [ngUiRouterBreadcrumb, ovhManagerTelecomTask])
+  .config(
+    /* @ngInject */ ($urlRouterProvider) =>
+      $urlRouterProvider.otherwise('/task'),
+  );
