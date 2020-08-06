01620dd06a9fbca1b1d0979d82e55c761167e2bf
Marie JONES <marie.jones@corp.ovh.com>
2020-08-06 15:52:44 +0200

    feat: add breadcrumb
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/overthebox/src/index.js
+++ packages/manager/apps/overthebox/src/index.js
@@ -4,8 +4,18 @@ import 'script-loader!moment/min/moment.min.js'; // eslint-disable-line
 
 import angular from 'angular';
 import ngOvhApiWrappers from '@ovh-ux/ng-ovh-api-wrappers';
+import ngUiRouterBreadcrumb from '@ovh-ux/ng-ui-router-breadcrumb';
 import ovhManagerOtb from '@ovh-ux/manager-overthebox';
 
 import 'ovh-manager-webfont/dist/css/ovh-font.css';
 
-angular.module('overtheboxApp', [ngOvhApiWrappers, ovhManagerOtb]);
+angular
+  .module('overtheboxApp', [
+    ngOvhApiWrappers,
+    ngUiRouterBreadcrumb,
+    ovhManagerOtb,
+  ])
+  .config(
+    /* @ngInject */ ($urlRouterProvider) =>
+      $urlRouterProvider.otherwise('/overTheBox'),
+  );
