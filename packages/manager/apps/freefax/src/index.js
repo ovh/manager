ed19f2975267de8e86fdabe3820e4f007f56d481
Marie JONES <marie.jones@corp.ovh.com>
2020-08-06 14:01:57 +0200

    feat: add breadcrumb
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/freefax/src/index.js
+++ packages/manager/apps/freefax/src/index.js
@@ -6,6 +6,16 @@ import 'regenerator-runtime/runtime';
 
 import angular from 'angular';
 import ngOvhApiWrappers from '@ovh-ux/ng-ovh-api-wrappers';
+import uiRouterBreadcrumb from '@ovh-ux/ng-ui-router-breadcrumb';
 import ovhManagerFreeFax from '@ovh-ux/manager-freefax';
 
-angular.module('freefaxApp', [ngOvhApiWrappers, ovhManagerFreeFax]);
+angular
+  .module('freefaxApp', [
+    ngOvhApiWrappers,
+    ovhManagerFreeFax,
+    uiRouterBreadcrumb,
+  ])
+  .config(
+    /* @ngInject */ ($urlRouterProvider) =>
+      $urlRouterProvider.otherwise('/freefax'),
+  );
