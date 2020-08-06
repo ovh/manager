b632b62ff6db3b5ddc563a3ea46758740a1b68d2
Marie JONES <marie.jones@corp.ovh.com>
2020-08-06 17:14:59 +0200

    feat: add breadcrumb (#3400)
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/telecom-dashboard/src/index.js
+++ packages/manager/apps/telecom-dashboard/src/index.js
@@ -1,7 +1,13 @@
 import 'script-loader!jquery'; // eslint-disable-line
 import 'script-loader!lodash'; // eslint-disable-line
+
+import ngUiRouterBreadcrumb from '@ovh-ux/ng-ui-router-breadcrumb';
 import telecomDashboard from '@ovh-ux/manager-telecom-dashboard';
 
 import angular from 'angular';
 
-angular.module('telecomDashboardApp', [telecomDashboard]);
+angular
+  .module('telecomDashboardApp', [ngUiRouterBreadcrumb, telecomDashboard])
+  .config(
+    /* @ngInject */ ($urlRouterProvider) => $urlRouterProvider.otherwise('/'),
+  );
