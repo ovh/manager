2c68a0ecca43dc2ac9df006c5ffde374712cd4b4
Marie JONES <marie.jones@corp.ovh.com>
2020-08-07 22:14:10 +0200

    fix(private-database): expose service
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/web/client/app/private-database/index.js
+++ packages/manager/apps/web/client/app/private-database/index.js
@@ -2,31 +2,37 @@ import angular from 'angular';
 import '@uirouter/angularjs';
 import 'oclazyload';
 
+import service from './private-database.service';
+
 const moduleName = 'ovhManagerPrivateDatabasesLazyLoading';
 
-angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
-  /* @ngInject */ ($stateProvider) => {
-    $stateProvider.state('app.private-database.**', {
-      url: '/configuration/private_database/:productId',
-      lazyLoad: ($transition$) => {
-        const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+angular
+  .module(moduleName, ['ui.router', 'oc.lazyLoad'])
+  .config(
+    /* @ngInject */ ($stateProvider) => {
+      $stateProvider.state('app.private-database.**', {
+        url: '/configuration/private_database/:productId',
+        lazyLoad: ($transition$) => {
+          const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
 
-        return import('./private-database.module').then((mod) =>
-          $ocLazyLoad.inject(mod.default || mod),
-        );
-      },
-    });
+          return import('./private-database.module').then((mod) =>
+            $ocLazyLoad.inject(mod.default || mod),
+          );
+        },
+      });
 
-    $stateProvider.state('app.private-databases.**', {
-      url: '/configuration/private_database',
-      lazyLoad: ($transition$) => {
-        const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+      $stateProvider.state('app.private-databases.**', {
+        url: '/configuration/private_database',
+        lazyLoad: ($transition$) => {
+          const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
 
-        return import('./private-databases/databases.module').then((mod) =>
-          $ocLazyLoad.inject(mod.default || mod),
-        );
-      },
-    });
-  },
-);
+          return import('./private-databases/databases.module').then((mod) =>
+            $ocLazyLoad.inject(mod.default || mod),
+          );
+        },
+      });
+    },
+  )
+  .service('PrivateDatabase', service);
+
 export default moduleName;
