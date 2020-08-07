86d345bd1f502ee6abf44502c9e942e33c8c4715
Marie JONES <marie.jones@corp.ovh.com>
2020-08-07 22:14:10 +0200

    fix(private-database): expose service
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/web/client/app/private-database/private-database.module.js
+++ packages/manager/apps/web/client/app/private-database/private-database.module.js
@@ -1,6 +1,5 @@
 import controller from './private-database.controller';
 import routing from './private-database.routing';
-import service from './private-database.service';
 import tabController from './private-database-tabs.controller';
 
 import orderCloudDb from './order/clouddb';
@@ -12,7 +11,6 @@ angular
   .config(routing)
   .controller('PrivateDatabaseCtrl', controller)
   .controller('PrivateDatabaseTabsCtrl', tabController)
-  .service('PrivateDatabase', service)
   .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
