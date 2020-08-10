b48eda331c34aa39415f2cb8085888a8665a434a
Marie JONES <marie.jones@corp.ovh.com>
2020-08-10 16:26:59 +0200

    refactor(dns-zone): revamp state and url architecture
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/web/client/app/dns-zone/redirection/dns-zone-redirection.routing.js
+++ packages/manager/apps/web/client/app/dns-zone/redirection/dns-zone-redirection.routing.js
@@ -1,10 +1,10 @@
 import template from '../../domain/redirection/REDIRECTION.html';
 
 export default /* @ngInject */ ($stateProvider) => {
-  $stateProvider.state('app.domain.dns-zone.redirection', {
+  $stateProvider.state('app.zone.details.redirection', {
     url: '/redirection',
     views: {
-      'dnsZoneView@app.domain.dns-zone': {
+      'dnsZoneView@app.zone.details': {
         controller: 'controllers.Domain.Redirection',
         controllerAs: 'ctrlDomainRedirection',
         template,
