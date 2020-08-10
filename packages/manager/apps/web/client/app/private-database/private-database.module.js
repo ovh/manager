f4e71120804ba497d9308bb660183f8628137ff7
Marie JONES <marie.jones@corp.ovh.com>
2020-08-10 11:12:13 +0200

    refactor(private-database): move listing page as first file level
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/web/client/app/private-database/private-database.module.js
+++ packages/manager/apps/web/client/app/private-database/private-database.module.js
@@ -1,16 +1,21 @@
-import controller from './private-database.controller';
+import angular from 'angular';
+import 'angular-translate';
+import '@ovh-ux/ui-kit';
+import ngTranslateAsyncLoader from '@ovh-ux/ng-translate-async-loader';
+import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
+
 import routing from './private-database.routing';
-import tabController from './private-database-tabs.controller';
 
-import orderCloudDb from './order/clouddb';
-
-const moduleName = 'ovhManagerWebPrivateDatabaseModule';
+const moduleName = 'ovhManagerPrivateDatabase';
 
 angular
-  .module(moduleName, [orderCloudDb])
+  .module(moduleName, [
+    ngTranslateAsyncLoader,
+    'oui',
+    'pascalprecht.translate',
+    ListLayoutHelper.moduleName,
+  ])
   .config(routing)
-  .controller('PrivateDatabaseCtrl', controller)
-  .controller('PrivateDatabaseTabsCtrl', tabController)
   .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
