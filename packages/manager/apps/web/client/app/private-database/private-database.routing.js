bd198649bbb1b3c260314149028d1a3ba06f11dd
Marie JONES <marie.jones@corp.ovh.com>
2020-08-10 11:12:13 +0200

    refactor(private-database): move listing page as first file level
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/web/client/app/private-database/private-database.routing.js
+++ packages/manager/apps/web/client/app/private-database/private-database.routing.js
@@ -1,52 +1,24 @@
+import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
+
 export default /* @ngInject */ ($stateProvider) => {
-  $stateProvider.state('app.private-database', {
-    url: '/configuration/private_database/:productId',
-    templateUrl: 'private-database/private-database.html',
-    controller: 'PrivateDatabaseCtrl',
-    reloadOnSearch: false,
+  $stateProvider.state('app.private-databases', {
+    url: `/configuration/private_database?${ListLayoutHelper.urlQueryParams}`,
+    component: 'managerListLayout',
+    params: ListLayoutHelper.stateParams,
     resolve: {
-      navigationInformations: [
-        'Navigator',
-        '$rootScope',
-        (Navigator, $rootScope) => {
-          // eslint-disable-next-line no-param-reassign
-          $rootScope.currentSectionInformation = 'private_database';
-          return Navigator.setNavigationInformation({
-            leftMenuVisible: true,
-            configurationSelected: true,
-          });
-        },
-      ],
+      ...ListLayoutHelper.stateResolves,
+      apiPath: () => '/hosting/privateDatabase',
+      dataModel: () => 'hosting.privateDatabase.Service',
+      defaultFilterColumn: () => 'serviceName',
+      header: /* @ngInject */ ($translate) =>
+        $translate.instant('private_databases_title'),
+      customizableColumns: () => true,
+      getServiceNameLink: /* @ngInject */ ($state) => ({
+        serviceName: productId,
+      }) =>
+        $state.href('app.private-database', {
+          productId,
+        }),
     },
-    translations: {
-      value: ['../private-database', '../hosting'],
-      format: 'json',
-    },
-  });
-
-  $stateProvider.state('app.private-database-order', {
-    url: '/configuration/private_database',
-    templateUrl: 'private-database/order/private-database-order.html',
-    controller: 'PrivateDatabaseOrderCtrl',
-    resolve: {
-      navigationInformations: [
-        'Navigator',
-        '$rootScope',
-        (Navigator, $rootScope) => {
-          // eslint-disable-next-line no-param-reassign
-          $rootScope.currentSectionInformation = 'private_database';
-          return Navigator.setNavigationInformation({
-            leftMenuVisible: true,
-            configurationSelected: true,
-          });
-        },
-      ],
-    },
-    translations: { value: ['../private-database'], format: 'json' },
-  });
-
-  $stateProvider.state('app.sql-order', {
-    redirectTo: 'app.private-database-order-clouddb',
-    url: '/configuration/sql_order',
   });
 };
