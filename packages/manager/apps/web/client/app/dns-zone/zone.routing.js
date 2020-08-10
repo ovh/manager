f565ac46eed8b8a3da9bd65aa277e1a65749888a
Marie JONES <marie.jones@corp.ovh.com>
2020-08-10 16:26:59 +0200

    refactor(dns-zone): revamp state and url architecture
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/web/client/app/dns-zone/zone.routing.js
+++ packages/manager/apps/web/client/app/dns-zone/zone.routing.js
@@ -1,19 +1,21 @@
 import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
 
 export default /* @ngInject */ ($stateProvider) => {
-  $stateProvider.state('app.domain.zone', {
-    url: `/configuration/zones?${ListLayoutHelper.urlQueryParams}`,
+  $stateProvider.state('app.zone.index', {
+    url: `?${ListLayoutHelper.urlQueryParams}`,
     component: 'managerListLayout',
     params: ListLayoutHelper.stateParams,
     resolve: {
       ...ListLayoutHelper.stateResolves,
+      schema: /* @ngInject */ ($http) =>
+        $http.get('/domain.json').then(({ data }) => data),
       apiPath: () => '/domain/zone',
       dataModel: () => 'domain.zone.Zone',
       defaultFilterColumn: () => 'name',
       header: /* @ngInject */ ($translate) => $translate.instant('zones_title'),
       customizableColumns: () => true,
       getServiceNameLink: /* @ngInject */ ($state) => ({ name: productId }) =>
-        $state.href('app.domain.dns-zone.details', {
+        $state.href('app.zone.details', {
           productId,
         }),
     },
