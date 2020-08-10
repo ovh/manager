ae2291b7852969ac6f4ac90a635cd532e26ca80b
Marie JONES <marie.jones@corp.ovh.com>
2020-08-10 20:03:37 +0200

    refactor: move listing page as first architecture level
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/emailpro/src/email-pro.routing.js
+++ packages/manager/modules/emailpro/src/email-pro.routing.js
@@ -19,4 +19,23 @@ export default /* @ngInject */ ($stateProvider) => {
         }),
     },
   });
+
+  $stateProvider.state('app.mxplan', {
+    url: `/configuration/email_mxplan?${ListLayoutHelper.urlQueryParams}`,
+    component: 'managerListLayout',
+    params: ListLayoutHelper.stateParams,
+    resolve: {
+      ...ListLayoutHelper.stateResolves,
+      apiPath: () => '/email/mxplan',
+      dataModel: () => 'email.mxplan.Service',
+      defaultFilterColumn: () => 'domain',
+      header: /* @ngInject */ ($translate) =>
+        $translate.instant('emails_mx_plan_title'),
+      customizableColumns: () => true,
+      getServiceNameLink: /* @ngInject */ ($state) => ({ domain: productId }) =>
+        $state.href('app.email.mxplan', {
+          productId,
+        }),
+    },
+  });
 };
