162710877b9a094d6378f2542191fea35f81f3ef
Marie JONES <marie.jones@corp.ovh.com>
2020-08-10 20:03:37 +0200

    refactor: move listing page as first architecture level
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/emailpro/src/dashboard/emailproServices.module.js
+++ packages/manager/modules/emailpro/src/dashboard/emailproServices.module.js
@@ -2,11 +2,11 @@ import angular from 'angular';
 import ovhManagerCore from '@ovh-ux/manager-core';
 
 import emailproService from './emailpro.service';
-import ApiEmailPro from './api/emailpro-api.service';
-import EmailProDomains from './domain/emailpro-domain.service';
-import EmailProExternalContacts from './external-contact/emailpro-external-contact.service';
-import EmailProPassword from './password/emailpro-password.service';
-import EmailproMxPlanMailingLists from './mailing-list/emailpro-mailing-list.service';
+import ApiEmailPro from '../api/emailpro-api.service';
+import EmailProDomains from '../domain/emailpro-domain.service';
+import EmailProExternalContacts from '../external-contact/emailpro-external-contact.service';
+import EmailProPassword from '../password/emailpro-password.service';
+import EmailproMxPlanMailingLists from '../mailing-list/emailpro-mailing-list.service';
 
 const moduleName = 'Module.emailpro.services';
 
