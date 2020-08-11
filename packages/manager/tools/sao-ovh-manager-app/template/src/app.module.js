a649eddb7019ecf018f85631c4aec50e0f8763f0
Marie JONES <marie.jones@corp.ovh.com>
2020-08-11 13:30:57 +0200

    feat: update template to use env executor
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

---
+++ packages/manager/tools/sao-ovh-manager-app/template/src/app.module.js
@@ -0,0 +1,36 @@
+<% const pascalcasedName = this.camelcase(name, { pascalCase: true }) -%>
+import { Environment } from '@ovh-ux/manager-config';
+import angular from 'angular';
+import uiRouter from '@uirouter/angularjs';
+import ovhManagerCore from '@ohv-ux/manager-core';
+import { detach as detachPreloader } from '@ovh-ux/manager-preloader';
+import ngOvhUiRouterLineProgress from '@ovh-ux/ng-ui-router-line-progress';
+import ngUiRouterBreadcrumb from '@ovh-ux/ng-ui-router-breadcrumb';
+
+import <%= pascalcasedName %> from '@ovh-ux/manager-<%= name %>';
+
+Environment.setVersion(__VERSION__);
+
+const moduleName = '<%= pascalcasedName %>App';
+
+angular
+  .module(moduleName, [
+    ovhManagerCore,
+    ngOvhUiRouterLineProgress,
+    ngUiRouterBreadcrumb,
+    uiRouter,
+    <%= pascalcasedName %>
+  ])
+  .config(
+    /* @ngInject */ ($locationProvider) => $locationProvider.hashPrefix(''),
+  )
+  .run(
+    /* @ngInject */ ($rootScope, $transitions) => {
+      const unregisterHook = $transitions.onSuccess({}, () => {
+        detachPreloader();
+        unregisterHook();
+      });
+    },
+  );
+
+export default moduleName;
