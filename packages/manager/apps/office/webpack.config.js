9e5a741742a68964834b9a62126823d34a23a5d3
Marie JONES <marie.jones@corp.ovh.com>
2020-08-14 12:11:24 +0200

    feat: init standalone app
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

---
+++ packages/manager/apps/office/webpack.config.js
@@ -0,0 +1,36 @@
+const _ = require('lodash');
+const path = require('path');
+const webpack = require('webpack'); // eslint-disable-line
+const merge = require('webpack-merge');
+const webpackConfig = require('@ovh-ux/manager-webpack-config');
+
+module.exports = (env = {}) => {
+  const REGION = _.upperCase(env.region || process.env.REGION || 'EU');
+
+  const { config } = webpackConfig(
+    {
+      template: './src/index.html',
+      basePath: './src',
+      lessPath: ['./node_modules'],
+      root: path.resolve(__dirname, './src'),
+    },
+    REGION ? Object.assign(env, { region: REGION }) : env,
+  );
+
+  return merge(config, {
+    entry: path.resolve('./src/index.js'),
+    resolve: {
+      modules: [
+        './node_modules',
+        path.resolve(__dirname, 'node_modules'),
+        path.resolve(__dirname, '../../../node_modules'),
+      ],
+      mainFields: ['module', 'browser', 'main'],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        __WEBPACK_REGION__: `'${REGION}'`,
+      }),
+    ],
+  });
+};
