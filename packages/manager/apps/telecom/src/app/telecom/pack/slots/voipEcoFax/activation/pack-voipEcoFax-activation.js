9cad7cdcd736853adf31cf3d48286d066196bb51
Marie JONES <marie.jones@corp.ovh.com>
2020-08-14 18:05:32 +0200

    refactor: revamp state and url
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/telecom/src/app/telecom/pack/slots/voipEcoFax/activation/pack-voipEcoFax-activation.js
+++ packages/manager/apps/telecom/src/app/telecom/pack/slots/voipEcoFax/activation/pack-voipEcoFax-activation.js
@@ -1,14 +1,10 @@
 angular.module('managerApp').config(($stateProvider) => {
   $stateProvider.state('telecom.packs.pack.voipEcoFax-activation', {
     url: '/voipEcoFax/activation',
-    views: {
-      'packView@telecom.packs': {
-        templateUrl:
-          'app/telecom//pack/slots/voipEcoFax/activation/pack-voipEcoFax-activation.html',
-        controller: 'PackFaxActivationCtrl',
-        controllerAs: 'PackFaxActivationCtrl',
-      },
-    },
+    templateUrl:
+      'app/telecom//pack/slots/voipEcoFax/activation/pack-voipEcoFax-activation.html',
+    controller: 'PackFaxActivationCtrl',
+    controllerAs: 'PackFaxActivationCtrl',
     translations: { value: ['.'], format: 'json' },
   });
 });
