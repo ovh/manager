4a3536a78f71a20d06cd914db46ca0d745ea4d02
Antoine Leblanc <antoine.leblanc@ovhcloud.com>
2020-08-14 16:05:02 +0200

    refactor(cui): cleanup some components and directives
    * Remove extra/unused components
    * Remove extra/unused directives
    * Remove extra styles
    
    Remove following components:
    - `cuiPageHeader`
    - `cuiPageHeaderTitle`
    - `cuiGuideComponent`
    - `cuiTabs`
    
    Remove following directives:
    - `cuiTab`
    
    Relates to: DTRSD-1799
    
    Signed-off-by: Antoine Leblanc <antoine.leblanc@ovhcloud.com>

--- packages/manager/modules/cloud-universe-components/src/cui/page/index.js
+++ packages/manager/modules/cloud-universe-components/src/cui/page/index.js
@@ -1,19 +1,13 @@
 import angular from 'angular';
 
-import guideComponent from '../guide-component';
-
 import contentComponent from './content/component';
-import headerComponent from './header/component';
-import headerTitleComponent from './header/title/component';
 
 import './index.less';
 
 const moduleName = 'cuiPage';
 
 angular
-  .module(moduleName, [guideComponent])
-  .component('cuiPageContentTitle', contentComponent)
-  .component('cuiPageHeader', headerComponent)
-  .component('cuiPageHeaderTitle', headerTitleComponent);
+  .module(moduleName, [])
+  .component('cuiPageContentTitle', contentComponent);
 
 export default moduleName;
