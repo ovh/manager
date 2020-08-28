196c319bf6a7129477f482dac6b2a81e058d7a97
Marie JONES <marie.jones@corp.ovh.com>
2020-08-28 12:12:23 +0200

    fix(billing.history): fix redirection to pay debt
    ref: MANAGER-5558
    
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/billing/main/history/billing-main-history.routes.js
+++ packages/manager/apps/dedicated/client/app/billing/main/history/billing-main-history.routes.js
@@ -155,6 +155,10 @@ export default /* @ngInject */ ($stateProvider, coreConfigProvider) => {
         hasDefaultPaymentMethod: /* @ngInject */ (ovhPaymentMethod) =>
           ovhPaymentMethod.hasDefaultPaymentMethod(),
         payDebt: /* @ngInject */ ($state) => (debtId) =>
+          $state.go('app.account.billing.main.history.pay-debt', {
+            debtId,
+          }),
+        seeDebt: /* @ngInject */ ($state) => (debtId) =>
           $state.go('app.account.billing.main.history.debt.details', {
             debtId,
           }),
