184c45a3bfda82564033f396ad9d9e3128ba66d0
Marie JONES <marie.jones@corp.ovh.com>
2020-08-31 16:32:14 +0200

    feat: add breadcrumb
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/nasha/src/partition/access/delete/delete.routing.js
+++ packages/manager/modules/nasha/src/partition/access/delete/delete.routing.js
@@ -1,17 +1,21 @@
 export default /* @ngInject */ ($stateProvider) => {
-  $stateProvider.state('nasha.dashboard.nasha-partition-access.delete', {
-    url: '/delete',
-    params: {
-      access: null,
-    },
-    views: {
-      modal: {
-        component: 'nashaPartitionAccessDeleteComponent',
+  $stateProvider.state(
+    'nasha.dashboard.partition.nasha-partition-access.delete',
+    {
+      url: '/delete',
+      params: {
+        access: null,
+      },
+      views: {
+        modal: {
+          component: 'nashaPartitionAccessDeleteComponent',
+        },
+      },
+      layout: 'modal',
+      resolve: {
+        access: /* @ngInject */ ($transition$) => $transition$.params().access,
+        breadcrumb: () => null,
       },
     },
-    layout: 'modal',
-    resolve: {
-      access: /* @ngInject */ ($transition$) => $transition$.params().access,
-    },
-  });
+  );
 };
