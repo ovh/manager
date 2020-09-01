2d95882425d6ad2dd4de3271af80fa51cc62f47c
Marie JONES <marie.jones@corp.ovh.com>
2020-09-01 17:50:35 +0200

    refactor: revamp state and url architecture
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/server-sidebar/src/sidebar.constants.js
+++ packages/manager/modules/server-sidebar/src/sidebar.constants.js
@@ -216,7 +216,7 @@ export const PAAS_CONFIG = {
     'paas.cda',
     'paas.veeam.detail',
     'veeam-cloud-connect',
-    'veeam-enterprise',
+    'veeam-enterprise.details',
   ],
   types: [
     {
@@ -241,7 +241,7 @@ export const PAAS_CONFIG = {
     },
     {
       path: '/veeam/veeamEnterprise',
-      state: 'veeam-enterprise.dashboard',
+      state: 'veeam-enterprise.details.dashboard',
       stateParams: ['serviceName'],
       icon: 'ovh-font ovh-font-veeam',
       app: [DEDICATED],
