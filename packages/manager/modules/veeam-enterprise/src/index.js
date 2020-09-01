433ac7ff811be4c11d79f4a27f47bb7bd8e1f92f
Marie JONES <marie.jones@corp.ovh.com>
2020-09-01 17:50:35 +0200

    refactor: revamp state and url architecture
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/veeam-enterprise/src/index.js
+++ packages/manager/modules/veeam-enterprise/src/index.js
@@ -6,29 +6,51 @@ import '@ovh-ux/ui-kit/dist/css/oui.css';
 
 const moduleName = 'ovhManagerVeeamEnterpriseLazyLoading';
 
-angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
-  /* @ngInject */ ($stateProvider) => {
-    $stateProvider.state('veeam-enterprise.**', {
-      url: '/paas/veeam-enterprise/{serviceName}',
-      lazyLoad: ($transition$) => {
-        const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+angular
+  .module(moduleName, ['ui.router', 'oc.lazyLoad'])
+  .config(
+    /* @ngInject */ ($stateProvider, $urlRouterProvider) => {
+      $stateProvider.state('veeam-enterprise', {
+        url: '/veeam-enterprise',
+        redirectTo: 'veeam-enterprise.index',
+        template: '<div ui-view></div>',
+      });
 
-        return import('./details/veeam-enterprise.module').then((mod) =>
-          $ocLazyLoad.inject(mod.default || mod),
+      $stateProvider.state('veeam-enterprise.details.**', {
+        url: '/{serviceName}',
+        lazyLoad: ($transition$) => {
+          const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+
+          return import('./details/veeam-enterprise.module').then((mod) =>
+            $ocLazyLoad.inject(mod.default || mod),
+          );
+        },
+      });
+
+      $stateProvider.state('veeam-enterprise.index.**', {
+        url: '',
+        lazyLoad: ($transition$) => {
+          const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+
+          return import('./veeam.module').then((mod) =>
+            $ocLazyLoad.inject(mod.default || mod),
+          );
+        },
+      });
+
+      $urlRouterProvider.when(/^\/paas\/veeam-enterprise/, () => {
+        window.location.href = window.location.href.replace(
+          '/paas/veeam-enterprise',
+          '/veeam-enterprise',
         );
-      },
-    });
-
-    $stateProvider.state('veeams-enterprise.**', {
-      url: '/paas/veeam-enterprise',
-      lazyLoad: ($transition$) => {
-        const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
-
-        return import('./veeam.module').then((mod) =>
-          $ocLazyLoad.inject(mod.default || mod),
-        );
-      },
-    });
-  },
-);
+      });
+    },
+  )
+  .run(
+    /* @ngInject */ ($translate, $transitions) => {
+      $transitions.onBefore({ to: 'veeam-enterprise.**' }, () =>
+        $translate.refresh(),
+      );
+    },
+  );
 export default moduleName;
