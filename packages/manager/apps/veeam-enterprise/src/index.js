6f7e8ea8cba8c36c5776960df7008602bb3511ed
Marie JONES <marie.jones@corp.ovh.com>
2020-09-02 08:45:09 +0200

    feat: add breadcrumb
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/veeam-enterprise/src/index.js
+++ packages/manager/apps/veeam-enterprise/src/index.js
@@ -5,16 +5,21 @@ import 'script-loader!moment/min/moment-with-locales.min';
 
 import angular from 'angular';
 
+import ngUiRouterBreadcrumb from '@ovh-ux/ng-ui-router-breadcrumb';
 import ovhManagerCore from '@ovh-ux/manager-core';
 import ovhManagerVeeamEnterprise from '@ovh-ux/manager-veeam-enterprise';
 
 import { momentConfiguration } from './config';
 
 angular
-  .module('veeamEnterpriseApp', [ovhManagerCore, ovhManagerVeeamEnterprise])
+  .module('veeamEnterpriseApp', [
+    ngUiRouterBreadcrumb,
+    ovhManagerCore,
+    ovhManagerVeeamEnterprise,
+  ])
   .config(momentConfiguration)
   .config(
     /* @ngInject */ ($urlRouterProvider) => {
-      $urlRouterProvider.otherwise('/paas/veeam-enterprise');
+      $urlRouterProvider.otherwise('/veeam-enterprise');
     },
   );
