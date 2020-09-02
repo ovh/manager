9bf0e5fdf4f2dae57fe68856389ecc121e5d0bcd
frenauvh <florian.renaut@corp.ovh.com>
2020-09-02 16:47:38 +0200

    fix(dedicated): monitoring units
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/dedicated/server/dashboard/dashboard.controller.js
+++ packages/manager/apps/dedicated/client/app/dedicated/server/dashboard/dashboard.controller.js
@@ -95,7 +95,7 @@ export default class DedicatedServerDashboard {
     const yLabel =
       unit === 'bps'
         ? this.$translate.instant(
-            'server_configuration_mitigation_statistics_unit_KB',
+            'server_configuration_mitigation_statistics_unit_B',
           )
         : unit;
     this.options = {
