b1076d6c3bc86110eca065923c73d59a546ca3f2
Ganesh Kumar <ganesh.kumar@corp.ovh.com>
2020-09-08 11:28:38 +0530

    fix(dedicated): hpc enhance
    reload task on dashboard
    
    Fixes #MANAGER-5466
    
    Signed-off-by: Ganesh Kumar <ganesh.kumar@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/dedicatedCloud/dashboard/dedicatedCloud-dashboard.routing.js
+++ packages/manager/apps/dedicated/client/app/dedicatedCloud/dashboard/dedicatedCloud-dashboard.routing.js
@@ -1,5 +1,10 @@
+import head from 'lodash/head';
+import map from 'lodash/map';
+import set from 'lodash/set';
+
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('app.dedicatedClouds.dashboard', {
+    cache: false,
     views: {
       pccView: 'pccDashboard',
     },
@@ -14,6 +19,30 @@ export default /* @ngInject */ ($stateProvider) => {
               'dedicated-cloud:mailingListSubscription',
             ),
           ),
+      vCenterUpgradeTask: /* @ngInject */ ($http, currentService) =>
+        $http
+          .get(`/dedicatedCloud/${currentService.serviceName}/task`, {
+            params: {
+              name: 'maintenanceUpgradeVcenter',
+              state: 'todo',
+            },
+          })
+          .then((response) => {
+            return map(response.data, (taskId) => {
+              return $http
+                .get(
+                  `/dedicatedCloud/${currentService.serviceName}/task/${taskId}`,
+                )
+                .then((taskResponse) => taskResponse.data);
+            });
+          })
+          .then((tasks) => {
+            return set(
+              currentService,
+              'vcenterUpgradePendingTask',
+              head(tasks),
+            );
+          }),
     },
   });
 };
