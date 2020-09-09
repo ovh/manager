3b49c34f38cace3ee0d69f94779a9e5fa5146977
Stephanie Moallic <stephanie.moallic@corp.ovh.com>
2020-09-09 17:54:52 +0200

    refactor(overthebox.configure): refactoring configure controller
    ref: UXCT-305
    
    Signed-off-by: Stephanie Moallic <stephanie.moallic@corp.ovh.com>

--- packages/manager/modules/overthebox/src/overthebox/configure/overTheBox-configure.controller.js
+++ packages/manager/modules/overthebox/src/overthebox/configure/overTheBox-configure.controller.js
@@ -1,21 +1,19 @@
 export default class OverTheBoxConfigureCtrl {
   /* @ngInject */
-  constructor($translate, $state, OvhApiOverTheBox, TucToastError) {
+  constructor($translate, OvhApiOverTheBox, TucToastError) {
     this.$translate = $translate;
-    this.$state = $state;
     this.OvhApiOverTheBox = OvhApiOverTheBox;
     this.TucToastError = TucToastError;
   }
 
   $onInit() {
-    this.orderHash = this.$state.href('order-overTheBox');
     this.loading = true;
     return this.OvhApiOverTheBox.v6()
       .getServices()
       .$promise.then((services) => {
         this.services = services;
         if (services.length === 0) {
-          this.$state.go('order-overTheBox');
+          this.goOrderOverTheBox();
         }
         return services;
       })
