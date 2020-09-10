a35b59366fcadbb0c88b029c31ecd7744047b882
Jisay <1774679+Jisay@users.noreply.github.com>
2020-09-10 09:24:53 +0200

    fix: multisite logs url (#3538)
    ref: DTRSD-16672
    
    Signed-off-by: Jisay <1774679+Jisay@users.noreply.github.com>

--- packages/manager/apps/web/client/app/hosting/multisite/logs/hosting-multisite-logs.controller.js
+++ packages/manager/apps/web/client/app/hosting/multisite/logs/hosting-multisite-logs.controller.js
@@ -1,14 +1,23 @@
-import isString from 'lodash/isString';
-
 angular.module('App').controller(
   'HostingTabDomainsMultisiteLogs',
   class HostingTabDomainsMultisiteLogs {
-    constructor($scope, $stateParams, $translate, Alerter, Hosting, constants) {
+    constructor(
+      $q,
+      $scope,
+      $stateParams,
+      $translate,
+      Alerter,
+      Hosting,
+      HostingStatistics,
+      constants,
+    ) {
+      this.$q = $q;
       this.$scope = $scope;
       this.$stateParams = $stateParams;
       this.$translate = $translate;
       this.Alerter = Alerter;
       this.Hosting = Hosting;
+      this.HostingStatistics = HostingStatistics;
       this.constants = constants;
     }
 
@@ -17,7 +26,7 @@ angular.module('App').controller(
         const domain = this.$scope.domains.list.results[
           elm['0'].dataset.domainIndex
         ];
-        if (!domain.logUrlGenerated) {
+        if (!domain.logUrlGenerated && !domain.logsLoading) {
           this.generateLogHref(domain);
         }
       });
@@ -25,28 +34,40 @@ angular.module('App').controller(
 
     /* eslint-disable no-param-reassign */
     generateLogHref(domain) {
-      domain.logUrlGenerated = true;
-      if (isString(domain.ownLog) && !domain.ownLogToken) {
-        domain.logsLoading = true;
-        this.Hosting.getUserLogsToken(this.$stateParams.productId, {
-          params: { attachedDomain: domain.name, remoteCheck: true },
+      domain.logsLoading = true;
+      this.$q
+        .all({
+          ownLogs: this.HostingStatistics.getLogs(
+            this.$stateParams.productId,
+            domain.name,
+          ),
+          userLogsToken: this.Hosting.getUserLogsToken(
+            this.$stateParams.productId,
+            {
+              params: {
+                attachedDomain: domain.name,
+                remoteCheck: true,
+              },
+            },
+          ),
         })
-          .then((result) => {
-            domain.logUrl = `${this.$scope.logs.logs}?token=${result}`;
-          })
-          .catch(() => {
-            this.Alerter.error(
-              this.$translate.instant(
-                'hosting_tab_DOMAINS_multisite_logs_generation_error',
-              ),
-              this.$scope.alerts.main,
-            );
-          })
-          .finally(() => {
-            domain.logsLoading = false;
-          });
-      }
-      return false;
+        .then(({ ownLogs, userLogsToken }) => {
+          if (ownLogs.logs) {
+            domain.logUrl = `${ownLogs.logs}?token=${userLogsToken}`;
+            domain.logUrlGenerated = true;
+          }
+        })
+        .catch(() => {
+          this.Alerter.error(
+            this.$translate.instant(
+              'hosting_tab_DOMAINS_multisite_logs_generation_error',
+            ),
+            this.$scope.alerts.main,
+          );
+        })
+        .finally(() => {
+          domain.logsLoading = false;
+        });
     }
     /* eslint-enable no-param-reassign */
   },
