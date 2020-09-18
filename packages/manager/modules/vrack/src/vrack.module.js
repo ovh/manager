cbdd613896e7337e909368f8c0d328dacd2b9d28
Marie JONES <marie.jones@corp.ovh.com>
2020-09-18 16:29:00 +0200

    feat: move listing page as first architecture level
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/vrack/src/vrack.module.js
+++ packages/manager/modules/vrack/src/vrack.module.js
@@ -1,52 +1,29 @@
 import angular from 'angular';
-import 'angular-ui-bootstrap';
-import '@uirouter/angularjs';
-
-import '@ovh-ux/manager-cloud-styles';
-import '@ovh-ux/manager-core';
-import '@ovh-ux/ng-ovh-cloud-universe-components';
-import '@ovh-ux/ng-ovh-toaster';
-import 'ovh-api-services';
+import 'angular-translate';
 import '@ovh-ux/ui-kit';
+import '@uirouter/angularjs';
+import ngTranslateAsyncLoader from '@ovh-ux/ng-translate-async-loader';
+import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
 
-import 'font-awesome/css/font-awesome.css';
-import 'ovh-manager-webfont/dist/css/ovh-font.css';
-import '@ovh-ux/ui-kit/dist/css/oui.css';
-import 'ovh-ui-kit-bs/dist/css/oui-bs3.css';
-
-import './vrack.less';
-import './vrack-mapper.less';
-
-import actionsPartials from './partials/actions.html';
-import availablePartials from './partials/available.html';
-import component from './vrack.component';
-import mappedPartials from './partials/mapped.html';
 import routing from './vrack.routing';
-import vrackAdd from './add';
-import vrackMoveDialog from './move-dialog';
 
-const moduleName = 'ovhManagerVrackDetails';
+const moduleName = 'ovhManagerVRack';
 
 angular
   .module(moduleName, [
-    'ui.router',
-    'ui.bootstrap',
+    ngTranslateAsyncLoader,
     'oui',
-    'ovh-api-services',
-    'ovhManagerCore',
-    'ngOvhCloudUniverseComponents',
-    'ngOvhToaster',
+    'pascalprecht.translate',
+    ListLayoutHelper.moduleName,
     'ui.router',
-    vrackAdd,
-    vrackMoveDialog,
   ])
-  .component('ovhManagerVrackComponent', component)
   .config(routing)
+  .run(/* @ngTranslationsInject:json ./translations */)
   .run(
-    /* @ngInject */ ($templateCache) => {
-      $templateCache.put('vrack/partials/actions.html', actionsPartials);
-      $templateCache.put('vrack/partials/available.html', availablePartials);
-      $templateCache.put('vrack/partials/mapped.html', mappedPartials);
+    /* @ngInject */ ($translate, $transitions) => {
+      $transitions.onBefore({ to: 'vrack-home.**' }, () =>
+        $translate.refresh(),
+      );
     },
   );
 
