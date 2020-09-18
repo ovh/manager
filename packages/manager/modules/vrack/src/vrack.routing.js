c5e352bd82ddb7cc3d65197908606f9b5fa17fc4
Marie JONES <marie.jones@corp.ovh.com>
2020-09-18 16:29:00 +0200

    feat: move listing page as first architecture level
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/vrack/src/vrack.routing.js
+++ packages/manager/modules/vrack/src/vrack.routing.js
@@ -1,14 +1,35 @@
+import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
+
 export default /* @ngInject */ ($stateProvider) => {
-  $stateProvider.state('vrack', {
-    url: '/vrack/:vrackId',
-    component: 'ovhManagerVrackComponent',
+  $stateProvider.state('vrack-home', {
+    url: `/vrack?${ListLayoutHelper.urlQueryParams}`,
+    component: 'managerListLayout',
+    params: ListLayoutHelper.stateParams,
     resolve: {
-      goToMoveDialog: /* @ngInject */ ($state) => (service) =>
-        $state.go('vrack.move', { service }),
-    },
-    translations: {
-      value: ['.'],
-      format: 'json',
+      ...ListLayoutHelper.stateResolves,
+      apiPath: /* @ngInject */ () => '/vrack',
+      dataModel: () => 'vrack.vrack',
+      resources: /* @ngInject */ ($http) =>
+        $http
+          .get('/vracks', {
+            serviceType: 'aapi',
+          })
+          .then(({ data }) => data),
+
+      loadResource: /* @ngInject */ () => (service) => ({
+        ...service,
+        serviceName: service.id,
+      }),
+      staticResources: () => true,
+      defaultFilterColumn: () => 'serviceName',
+      header: /* @ngInject */ ($translate) => $translate.instant('vrack_title'),
+      customizableColumns: () => true,
+      getServiceNameLink: /* @ngInject */ ($state) => ({
+        serviceName: vrackId,
+      }) =>
+        $state.href('vrack', {
+          vrackId,
+        }),
     },
   });
 };
