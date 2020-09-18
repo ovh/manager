970e5005609c0455ae69a9f13563d61cfc23aceb
Marie JONES <marie.jones@corp.ovh.com>
2020-09-18 17:02:20 +0200

    feat: revamp state architecture
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/server-sidebar/src/sidebar.constants.js
+++ packages/manager/modules/server-sidebar/src/sidebar.constants.js
@@ -325,7 +325,7 @@ export const DEDICATED_NETWORK_CONFIG = {
   regions: ['EU', 'CA', 'US'],
   namespace: [undefined, HPC_NAMESPACE],
   icon: 'oui-icon oui-icon-bandwidth_concept',
-  loadOnState: ['vrack', 'cloud-connect'],
+  loadOnState: ['vrack.dashboard', 'cloud-connect'],
   children: [
     {
       id: 'vrack',
@@ -333,7 +333,7 @@ export const DEDICATED_NETWORK_CONFIG = {
       types: [
         {
           path: '/vrack',
-          state: 'vrack',
+          state: 'vrack.dashboard',
           stateParams: ['vrackId'],
           app: [DEDICATED],
           namespace: [undefined, HPC_NAMESPACE],
