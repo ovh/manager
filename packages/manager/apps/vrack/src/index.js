9bf982365eb737cd9ec2b6558bab5b54c764691e
Marie JONES <marie.jones@corp.ovh.com>
2020-09-18 17:28:56 +0200

    feat: add breadcrumb
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/vrack/src/index.js
+++ packages/manager/apps/vrack/src/index.js
@@ -9,17 +9,20 @@ import 'script-loader!messenger/build/js/messenger-theme-flat.js';
 /* eslint-enable import/no-webpack-loader-syntax, import/extensions */
 
 import angular from 'angular';
+import ngUiRouterBreadcrumb from '@ovh-ux/ng-ui-router-breadcrumb';
 import ovhManagerVrack from '@ovh-ux/manager-vrack';
 
 import './index.scss';
 
-angular.module('vrackApp', [ovhManagerVrack]).config(
-  /* @ngInject */ ($qProvider) => {
-    $qProvider.errorOnUnhandledRejections(false);
-  },
+angular
+  .module('vrackApp', [ngUiRouterBreadcrumb, ovhManagerVrack])
+  .config(
+    /* @ngInject */ ($qProvider) => {
+      $qProvider.errorOnUnhandledRejections(false);
+    },
   )
   .config(
     /* @ngInject */ ($urlRouterProvider) => {
       $urlRouterProvider.otherwise('/vrack');
     },
-);
+  );
