ae6c86cea9c4a3e2b546cb2369171498b14400a4
Marie JONES <marie.jones@corp.ovh.com>
2020-09-21 11:41:06 +0200

    feat(mailing-list): add intermediate state
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/emailpro/src/mailing-list/moderators/moderators.routing.js
+++ packages/manager/modules/emailpro/src/mailing-list/moderators/moderators.routing.js
@@ -1,7 +1,7 @@
 import template from './emailpro-mailing-list-moderators-view.html';
 
 export default /* @ngInject */ ($stateProvider) => {
-  $stateProvider.state('mxplan.dashboard.mailing-list.moderators', {
+  $stateProvider.state('mxplan.dashboard.mailing-list.dashboard.moderators', {
     url: '/moderators',
     template,
     controller: 'EmailProMXPlanMailingListsModeratorsCtrl',
