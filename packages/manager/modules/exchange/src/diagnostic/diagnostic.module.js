e1eca096f0c995ea31a9d982530ce67437d3603a
Marie JONES <marie.jones@corp.ovh.com>
2020-09-21 16:53:12 +0200

    feat: add breadcrumb
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/exchange/src/diagnostic/diagnostic.module.js
+++ packages/manager/modules/exchange/src/diagnostic/diagnostic.module.js
@@ -9,6 +9,7 @@ const moduleName = 'ovhManagerExchangeDashboardDiagnostic';
 
 angular
   .module(moduleName, [ngTranslateAsyncLoader, 'oui', 'pascalprecht.translate'])
-  .config(routing);
+  .config(routing)
+  .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
