d315449c3337eca86f0aa892630770ae9ca28646
Marie JONES <marie.jones@corp.ovh.com>
2020-09-22 11:05:51 +0200

    fix(domain.dns): update state
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/web/client/app/domain/dns/domain-dns.state.js
+++ packages/manager/apps/web/client/app/domain/dns/domain-dns.state.js
@@ -1,18 +1,21 @@
 import controller from './domain-dns.controller';
 import template from './DNS.html';
 
-const state = {
-  url: '/dns',
-  views: {
-    domainView: {
-      controller,
-      controllerAs: '$ctrl',
-      template,
+export default /* @ngInject */ ($stateProvider) => {
+  const state = {
+    url: '/dns',
+    views: {
+      domainView: {
+        controller,
+        controllerAs: '$ctrl',
+        template,
+      },
     },
-  },
-  atInternet: {
-    rename: 'DNS',
-  },
-};
+    atInternet: {
+      rename: 'DNS',
+    },
+  };
 
-export default state;
+  $stateProvider.state('app.domain.product.dns', { ...state });
+  $stateProvider.state('app.alldom.domain.dns', { ...state });
+};
