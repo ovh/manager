2a48525dfc132eea88b63a7360b119f999791bd1
Jérémy De-Cesare <jeremy.de-cesare@corp.ovh.com>
2020-09-22 09:14:09 +0200

    feat(sms.batches): add batch details page
    ref: MANAGER-5201
    Signed-off-by: Jérémy De-Cesare <jeremy.de-cesare@corp.ovh.com>

---
+++ packages/manager/modules/sms/src/sms/batches/details/delete/routing.js
@@ -0,0 +1,26 @@
+import component from './telecom-sms-batches-history-details-delete.component';
+
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state('sms.service.batches.details.delete', {
+    params: {
+      outgoing: {},
+    },
+    views: {
+      modal: {
+        component: component.name,
+      },
+    },
+    resolve: {
+      deleteOutgoing: /* @ngInject */ ($http, serviceName) => (outgoingId) =>
+        $http({
+          url: `/sms/${serviceName}/outgoing/${outgoingId}`,
+          method: 'DELETE',
+        }),
+      outgoing: /* @ngInject */ ($transition$) =>
+        $transition$.params().outgoing,
+      goBackToDetails: /* @ngInject */ ($state) => (options) =>
+        $state.go('sms.service.batches.details', {}, options),
+    },
+    layout: 'modal',
+  });
+};
---
+++ packages/manager/modules/sms/src/sms/batches/details/routing.js
@@ -0,0 +1,47 @@
+import moment from 'moment';
+
+import component from './telecom-sms-batches-details.component';
+
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state('sms.service.batches.details', {
+    url: '/:batchId',
+    views: {
+      'smsView@sms.service': {
+        component: component.name,
+      },
+    },
+    params: {
+      previousState: '^',
+    },
+    resolve: {
+      batch: /* @ngInject */ ($transition$, batches) =>
+        batches.find(({ id }) => id === $transition$.params().batchId),
+      getOutgoingSms: /* @ngInject */ (iceberg, serviceName) => (batch) =>
+        iceberg(`/sms/${serviceName}/outgoing`)
+          .query()
+          .expand('CachedObjectList-Pages')
+          .execute({
+            'creationDatetime.from': batch.createdAt,
+            'creationDatetime.to': moment().format(),
+            batchId: batch.id,
+          })
+          .$promise.then(({ data }) => data),
+      getPttDetails: /* @ngInject */ ($http) => (outgoing) =>
+        $http({
+          url: '/sms/ptts',
+          method: 'GET',
+          params: {
+            ptt: outgoing.ptt,
+          },
+        }).then(({ data }) => data),
+      goBack: /* @ngInject */ ($state, $transition$) => (options) =>
+        $state.go($transition$.params().previousState, {}, options),
+      goToDelete: /* @ngInject */ ($state) => (outgoing) =>
+        $state.go('sms.service.batches.details.delete', {
+          outgoing,
+        }),
+      outgoingSms: /* @ngInject */ (batch, getOutgoingSms) =>
+        getOutgoingSms(batch),
+    },
+  });
+};
