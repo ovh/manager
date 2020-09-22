59dda067d34f468e4eb4f1c965bf2e0f00a5643a
Marie JONES <marie.jones@corp.ovh.com>
2020-09-22 10:29:08 +0200

    fix(domain.optin): update link to whois optin
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/web/client/app/domain/optin/index.js
+++ packages/manager/apps/web/client/app/domain/optin/index.js
@@ -1,11 +1,14 @@
 import component from './optin.component';
 import controller from './optin.controller';
 
+import routing from './routes';
+
 const moduleName = 'domainOptin';
 
 angular
   .module(moduleName, [])
   .component('domainOptin', component)
-  .controller('DomainOptinCtrl', controller);
+  .controller('DomainOptinCtrl', controller)
+  .config(routing);
 
 export default moduleName;
