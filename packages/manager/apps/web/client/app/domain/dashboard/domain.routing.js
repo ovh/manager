8ec29659edbd75a26be6193c3b8af1933827f2ad
Marie JONES <marie.jones@corp.ovh.com>
2020-09-22 11:05:51 +0200

    fix(domain.dns): update state
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/web/client/app/domain/dashboard/domain.routing.js
+++ packages/manager/apps/web/client/app/domain/dashboard/domain.routing.js
@@ -2,7 +2,6 @@ import clone from 'lodash/clone';
 import isEmpty from 'lodash/isEmpty';
 
 import anycastState from '../anycast/domain-dns-anycast.state';
-import dnsState from '../dns/domain-dns.state';
 import redirectionState from '../redirection/domain-redirection.state';
 import dynHostState from '../dynhost/domain-dynhost.state';
 import glueState from '../glue/domain-glue.state';
@@ -27,6 +26,7 @@ const commonResolves = {
           dynHost: false,
         }))
       : { dynHost: false },
+  breadcrumb: /* @ngInject */ (domainName) => domainName,
 };
 
 export default /* @ngInject */ ($stateProvider) => {
@@ -143,7 +143,6 @@ export default /* @ngInject */ ($stateProvider) => {
       `app.domain.${stateType}.anycast`,
       clone(anycastState),
     );
-    $stateProvider.state(`app.domain.${stateType}.dns`, clone(dnsState));
     $stateProvider.state(
       `app.domain.${stateType}.redirection`,
       clone(redirectionState),
