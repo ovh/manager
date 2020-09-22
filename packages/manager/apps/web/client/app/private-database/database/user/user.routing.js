b7e176183a5f4a9cbf3d45a3dc5599017c2c02ab
Marie JONES <marie.jones@corp.ovh.com>
2020-09-22 17:34:16 +0200

    feat(private-database.user): add intermediate state
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/web/client/app/private-database/database/user/user.routing.js
+++ packages/manager/apps/web/client/app/private-database/database/user/user.routing.js
@@ -1,9 +1,12 @@
 import template from './private-database-database-user.html';
 
 export default /* @ngInject */ ($stateProvider) => {
-  $stateProvider.state('app.private-database.dashboard.database.user', {
+  $stateProvider.state(
+    'app.private-database.dashboard.database.dashboard.user',
+    {
     url: '/user',
     template,
     controller: 'PrivateDatabaseUserDatabaseCtrl',
-  });
+    },
+  );
 };
--- packages/manager/apps/web/client/app/private-database/user/grants/grants.routing.js
+++ packages/manager/apps/web/client/app/private-database/user/grants/grants.routing.js
@@ -1,7 +1,7 @@
 import template from './private-database-user-grants.html';
 
 export default /* @ngInject */ ($stateProvider) => {
-  $stateProvider.state('app.private-database.dashboard.user.grants', {
+  $stateProvider.state('app.private-database.dashboard.user.dashboard.grants', {
     url: '/grants',
     template,
     controller: 'PrivateDatabaseUsersGrantsCtrl',
