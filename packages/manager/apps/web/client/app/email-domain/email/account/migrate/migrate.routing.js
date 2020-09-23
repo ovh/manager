728f1dfc807d9520f0a76343b2db532ff4d7983f
Marie JONES <marie.jones@corp.ovh.com>
2020-09-23 11:07:16 +0200

    feat(email-domain.account): add intermediate state
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/web/client/app/email-domain/email/account/migrate/migrate.routing.js
+++ packages/manager/apps/web/client/app/email-domain/email/account/migrate/migrate.routing.js
@@ -1,7 +1,7 @@
 import template from './email-domain-email-account-migrate.html';
 
 export default /* @ngInject */ ($stateProvider) => {
-  $stateProvider.state('app.email.domain.email.migrate', {
+  $stateProvider.state('app.email.domain.email.account.migrate', {
     url: '/migrate',
     template,
     controller: 'EmailsMigrateAccountCtrl',
