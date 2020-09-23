15176c668ca74e8767c12fd282ee377e3cd3169e
Marie JONES <marie.jones@corp.ovh.com>
2020-09-23 10:39:07 +0200

    feat(dns-zone): add breadcrumb
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/web/client/app/dns-zone/redirection/index.js
+++ packages/manager/apps/web/client/app/dns-zone/redirection/index.js
@@ -2,6 +2,9 @@ import routing from './dns-zone-redirection.routing';
 
 const moduleName = 'ovhManagerWebDomainZoneRedirection';
 
-angular.module(moduleName, []).config(routing);
+angular
+  .module(moduleName, [])
+  .config(routing)
+  .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
