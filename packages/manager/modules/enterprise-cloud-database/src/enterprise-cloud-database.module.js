0fe423f619d2e05e206fd69e53172263a80bae95
Marie JONES <marie.jones@corp.ovh.com>
2020-09-24 14:53:46 +0200

    perf: load dependencies only when required
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/enterprise-cloud-database/src/enterprise-cloud-database.module.js
+++ packages/manager/modules/enterprise-cloud-database/src/enterprise-cloud-database.module.js
@@ -1,6 +1,14 @@
 import angular from 'angular';
 import 'font-awesome/css/font-awesome.css';
 
+import '@ovh-ux/ng-ui-router-layout';
+import '@ovh-ux/ng-ovh-payment-method';
+import '@ovh-ux/ng-ovh-cloud-universe-components';
+
+import '@ovh-ux/ui-kit/dist/css/oui.css';
+import 'ovh-manager-webfont/dist/css/ovh-font.css';
+import 'ovh-ui-kit-bs/dist/css/oui-bs3.css';
+
 import paymentInfoComponent from './payment-info';
 import create from './create';
 import enterpriseCloudDatabaseComponent from './enterprise-cloud-database.component';
@@ -9,10 +17,20 @@ import priceComponent from './price';
 import routing from './enterprise-cloud-database.routing';
 import service from './service';
 
+import './index.scss';
+
 const moduleName = 'enterpriseCloudDatabase';
 
 angular
-  .module(moduleName, [paymentInfoComponent, create, priceComponent, service])
+  .module(moduleName, [
+    'ngUiRouterLayout',
+    'ngOvhPaymentMethod',
+    'ngOvhCloudUniverseComponents',
+    paymentInfoComponent,
+    create,
+    priceComponent,
+    service,
+  ])
   .config(routing)
   .component(
     'enterpriseCloudDatabaseComponent',
