f8f01a78298feb2fc57db4d6c49c3ca0efba1dd9
Marie JONES <marie.jones@corp.ovh.com>
2020-09-24 14:45:56 +0200

    feat: add env executor
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/enterprise-cloud-database/src/app.module.js
+++ packages/manager/apps/enterprise-cloud-database/src/app.module.js
@@ -10,12 +10,30 @@ import 'script-loader!messenger/build/js/messenger-theme-flat.js';
 
 import angular from 'angular';
 import '@uirouter/angularjs';
+import { detach as detachPreloader } from '@ovh-ux/manager-preloader';
 
 import ovhManagerEnterpriseCloudDatabase from '@ovh-ux/manager-enterprise-cloud-database';
 
 import './index.scss';
 
-angular.module('enterpriseCloudDatabaseApp', [
+const moduleName = 'enterpriseCloudDatabaseApp';
+
+angular
+  .module(moduleName, [
   'ui.router',
   ovhManagerEnterpriseCloudDatabase,
-]);
+  ])
+  .config(
+    /* @ngInject */ ($urlRouterProvider) =>
+      $urlRouterProvider.otherwise('/enterprise-cloud-database'),
+  )
+  .run(
+    /* @ngInject */ ($transitions) => {
+      const unregisterHook = $transitions.onSuccess({}, () => {
+        detachPreloader();
+        unregisterHook();
+      });
+    },
+  );
+
+export default moduleName;
