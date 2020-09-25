a36c88970536f7c961cc78bce0be6abe009d5590
Marie JONES <marie.jones@corp.ovh.com>
2020-09-25 11:47:21 +0200

    feat(billing): add intermediate state
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/billing/main/history/debt/details/billing-main-history-debt-details.routes.js
+++ packages/manager/apps/dedicated/client/app/billing/main/history/debt/details/billing-main-history-debt-details.routes.js
@@ -1,10 +1,14 @@
-angular.module('App').config(($stateProvider) => {
-  $stateProvider.state('app.account.billing.main.history.debt.details', {
+angular
+  .module('App')
+  .config(($stateProvider) => {
+    $stateProvider.state(
+      'app.account.billing.main.history.details.debt.details',
+      {
     url: '/details',
     templateUrl:
       'billing/main/history/debt/details/billing-main-history-debt-details.html',
     controller: 'BillingHistoryDebtDetailsCtrl',
     controllerAs: '$ctrl',
-    translations: { value: ['.'], format: 'json' },
-  });
-});
+      },
+    );
+  })
