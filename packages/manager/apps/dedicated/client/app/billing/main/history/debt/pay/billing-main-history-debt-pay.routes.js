7ec25febe910148271368d2b0e754b024dbec324
Marie JONES <marie.jones@corp.ovh.com>
2020-09-25 11:54:57 +0200

    feat(billing): add breadcrumb
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/billing/main/history/debt/pay/billing-main-history-debt-pay.routes.js
+++ packages/manager/apps/dedicated/client/app/billing/main/history/debt/pay/billing-main-history-debt-pay.routes.js
@@ -13,5 +13,8 @@ angular.module('App').config(($stateProvider) => {
       redirectTo: 'app.account.billing.main.history',
     },
     translations: { value: ['.'], format: 'json' },
+    resolve: {
+      breadcrumb: () => null,
+    },
   });
 });
