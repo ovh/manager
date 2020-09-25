ae6092647be048e8e8694e0b4130de4aad9883ad
Marie JONES <marie.jones@corp.ovh.com>
2020-09-25 11:54:57 +0200

    feat(billing): add breadcrumb
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/billing/main/payments/request/billing-payments-request.routes.js
+++ packages/manager/apps/dedicated/client/app/billing/main/payments/request/billing-payments-request.routes.js
@@ -1,12 +1,18 @@
-angular.module('App').config(($stateProvider, coreConfigProvider) => {
-  if (coreConfigProvider.getRegion() === 'US') {
-    $stateProvider.state('app.account.billing.main.payments.request', {
-      url: '/request',
-      templateUrl:
-        'billing/main/payments/request/billing-payments-request.html',
-      controller: 'BillingHistoryRequestCtrl',
-      controllerAs: '$ctrl',
-      translations: { value: ['../request'], format: 'json' },
-    });
-  }
-});
+angular
+  .module('App')
+  .config(($stateProvider, coreConfigProvider) => {
+    if (coreConfigProvider.getRegion() === 'US') {
+      $stateProvider.state('app.account.billing.main.payments.request', {
+        url: '/request',
+        templateUrl:
+          'billing/main/payments/request/billing-payments-request.html',
+        controller: 'BillingHistoryRequestCtrl',
+        controllerAs: '$ctrl',
+        resolve: {
+          breadcrumb: /* @ngInject */ ($translate) =>
+            $translate.instant('billing_payments_request_title'),
+        },
+      });
+    }
+  })
+  .run(/* @ngTranslationsInject:json ./translations */);
