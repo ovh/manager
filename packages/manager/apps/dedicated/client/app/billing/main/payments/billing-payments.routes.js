760e12dd727fe1cebae06ba43f5b1ffad9509e95
Marie JONES <marie.jones@corp.ovh.com>
2020-09-25 11:54:57 +0200

    feat(billing): add breadcrumb
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/billing/main/payments/billing-payments.routes.js
+++ packages/manager/apps/dedicated/client/app/billing/main/payments/billing-payments.routes.js
@@ -1,9 +1,15 @@
-angular.module('Billing').config(($stateProvider) => {
-  $stateProvider.state('app.account.billing.main.payments', {
-    url: '/payments',
-    templateUrl: 'billing/main/payments/billing-payments.html',
-    controller: 'Billing.PaymentsCtrl',
-    controllerAs: '$ctrl',
-    translations: { value: ['../payments'], format: 'json' },
-  });
-});
+angular
+  .module('Billing')
+  .config(($stateProvider) => {
+    $stateProvider.state('app.account.billing.main.payments', {
+      url: '/payments',
+      templateUrl: 'billing/main/payments/billing-payments.html',
+      controller: 'Billing.PaymentsCtrl',
+      controllerAs: '$ctrl',
+      resolve: {
+        breadcrumb: /* @ngInject */ ($translate) =>
+          $translate.instant('billing_main_payments_title'),
+      },
+    });
+  })
+  .run(/* @ngTranslationsInject:json ./translations */);
