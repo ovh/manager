d52f5961c8ad8d8d3b55c9e47ec64cabfbf0fe0c
Marie JONES <marie.jones@corp.ovh.com>
2020-09-25 13:21:37 +0200

    feat(useraccount): add breadcrumb
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>
    Co-authored-by: Antoine Leblanc <antoine.leblanc@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/account/user/emails/user-emails.routing.js
+++ packages/manager/apps/dedicated/client/app/account/user/emails/user-emails.routing.js
@@ -1,23 +1,35 @@
 import includes from 'lodash/includes';
 
-angular.module('UserAccount').config(
-  /* @ngInject */ ($stateProvider, coreConfigProvider) => {
-    const name = 'app.account.user.emails';
-    const nameDetails = 'app.account.user.emailsDetails';
+angular
+  .module('UserAccount')
+  .config(
+    /* @ngInject */ ($stateProvider, coreConfigProvider) => {
+      const name = 'app.account.user.emails';
+      const nameDetails = 'app.account.user.emails.emailsDetails';
 
-    if (includes(['EU', 'CA'], coreConfigProvider.getRegion())) {
-      $stateProvider.state(name, {
-        url: '/emails',
-        templateUrl: 'account/user/emails/user-emails.html',
-        controller: 'UserAccount.controllers.emails',
-        translations: ['../'],
-      });
+      if (includes(['EU', 'CA'], coreConfigProvider.getRegion())) {
+        $stateProvider.state(name, {
+          url: '/emails',
+          templateUrl: 'account/user/emails/user-emails.html',
+          controller: 'UserAccount.controllers.emails',
+          translations: ['../'],
+          resolve: {
+            breadcrumb: /* @ngInject */ ($translate) =>
+              $translate.instant('user_emails'),
+          },
+        });
 
-      $stateProvider.state(nameDetails, {
-        url: '/emails/:emailId',
-        templateUrl: 'account/user/emails/details/user-emails-details.html',
-        controller: 'UserAccount.controllers.emails.details',
-      });
-    }
-  },
-);
+        $stateProvider.state(nameDetails, {
+          url: '/:emailId',
+          templateUrl: 'account/user/emails/details/user-emails-details.html',
+          controller: 'UserAccount.controllers.emails.details',
+          resolve: {
+            emailId: /* @ngInject */ ($transition$) =>
+              $transition$.params().emailId,
+            breadcrumb: /* @ngInject */ (emailId) => emailId,
+          },
+        });
+      }
+    },
+  )
+  .run(/* @ngTranslationsInject:json ./translations */);
