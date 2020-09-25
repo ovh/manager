79c042b46905b7c7a0fcf5b836e7e193cce44972
Marie JONES <marie.jones@corp.ovh.com>
2020-09-25 16:50:44 +0200

    feat(dedicated.housing): add listing page
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/app.js
+++ packages/manager/apps/dedicated/client/app/app.js
@@ -62,6 +62,7 @@ import errorPage from './error';
 
 import dedicatedServer from './dedicated/server';
 import dedicatedNas from './dedicated/nas';
+import dedicatedHousing from './dedicated/housing';
 import userContracts from './user-contracts';
 
 import { TRACKING } from './at-internet.constants';
@@ -84,6 +85,7 @@ angular
       cdn,
       contactsService,
       dedicatedCloud,
+      dedicatedHousing,
       dedicatedNas,
       dedicatedServer,
       dedicatedUniverseComponents,
