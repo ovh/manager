548e4c68d88bfdbbf64120a9d9908c28e6368b3b
jjuret <julien.juret@gmail.com>
2020-09-28 12:25:34 +0200

    fix(deps): add a missing dep ng-ui-router-layout on sms app
    * fix(dependencies): dependencie for modal, it is not work at standalone
    * fix(dependencies): peer dep for angular-bootstrap
    * fix(dependencies): original dependencie bootstrap
    * fix(deps): remove peer dep because isn't needed
    
    Signed-off-by: jjuret <julien.juret@gmail.com>

--- packages/manager/modules/sms/src/sms/batches/pending/index.js
+++ packages/manager/modules/sms/src/sms/batches/pending/index.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import '@uirouter/angularjs';
+import ngUiRouterLayout from '@ovh-ux/ng-ui-router-layout';
 
 import cancel from './cancel';
 import component from './telecom-sms-batches-pending.component';
@@ -8,7 +9,7 @@ import routing from './routing';
 const moduleName = 'ovhManagerSmsBatchesPendingModule';
 
 angular
-  .module(moduleName, ['ui.router', cancel])
+  .module(moduleName, ['ui.router', ngUiRouterLayout, cancel])
   .component(component.name, component)
   .config(routing)
   .run(/* @ngTranslationsInject:json ./translations */);
