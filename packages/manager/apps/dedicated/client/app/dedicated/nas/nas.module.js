c7b94251dbbb3947a212e5e110733d974b7289e8
Marie JONES <marie.jones@corp.ovh.com>
2020-09-29 10:16:11 +0200

    refactor(dedicated.nas): move list page as first architecture level
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/dedicated/nas/nas.module.js
+++ packages/manager/apps/dedicated/client/app/dedicated/nas/nas.module.js
@@ -1,1 +1,19 @@
-angular.module('App').constant('NASHA_URL', '/manager/cloud/index.html#/nasha');
+import angular from 'angular';
+import '@ovh-ux/ui-kit';
+import '@uirouter/angularjs';
+import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
+
+import routing from './nas.routing';
+
+const moduleName = 'ovhManagerNAS';
+
+angular
+  .module(moduleName, [
+    'oui',
+    'pascalprecht.translate',
+    ListLayoutHelper.moduleName,
+    'ui.router',
+  ])
+  .config(routing);
+
+export default moduleName;
