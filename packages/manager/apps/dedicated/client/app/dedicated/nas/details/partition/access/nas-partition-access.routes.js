2c8d6cace70e202febdff1a76a62078936d892ab
Marie JONES <marie.jones@corp.ovh.com>
2020-09-29 11:30:56 +0200

    feat(dedicated.nas): add breadcrumb
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/dedicated/nas/details/partition/access/nas-partition-access.routes.js
+++ packages/manager/apps/dedicated/client/app/dedicated/nas/details/partition/access/nas-partition-access.routes.js
@@ -5,11 +5,16 @@ angular
       'app.dedicated-nas.details.partition.dashboard.access',
       {
         url: '/access',
-    templateUrl:
-      'dedicated/nas/details/partition/access/nas-partition-access.html',
-    controller: 'NasPartitionAccessCtrl',
-    controllerAs: '$ctrl',
-    reloadOnSearch: false,
+        templateUrl:
+          'dedicated/nas/details/partition/access/nas-partition-access.html',
+        controller: 'NasPartitionAccessCtrl',
+        controllerAs: '$ctrl',
+        reloadOnSearch: false,
+        resolve: {
+          breadcrumb: /* @ngInject */ ($translate) =>
+            $translate.instant('nas_details_partition_access'),
+        },
       },
     );
   })
+  .run(/* @ngTranslationsInject:json ./translations */);
--- packages/manager/apps/dedicated/client/app/dedicated/nas/nas.routing.js
+++ packages/manager/apps/dedicated/client/app/dedicated/nas/nas.routing.js
@@ -23,6 +23,7 @@ export default /* @ngInject */ ($stateProvider) => {
           nasId,
           nasType: 'nas',
         }),
+      hideBreadcrumb: () => true,
     },
   });
 };
