72dd3c2bc394881e5412a60f021992e6f82a0775
Marie JONES <marie.jones@corp.ovh.com>
2020-09-29 11:29:03 +0200

    feat(dedicated.nas.partition): add intermediate state
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/dedicated/nas/details/partition/snapshot/nas-partition-snapshot.routes.js
+++ packages/manager/apps/dedicated/client/app/dedicated/nas/details/partition/snapshot/nas-partition-snapshot.routes.js
@@ -1,10 +1,13 @@
 angular.module('App').config(($stateProvider) => {
-  $stateProvider.state('app.dedicated-nas.details.partition.snapshot', {
-    url: '/:partitionName/snapshot',
+  $stateProvider.state(
+    'app.dedicated-nas.details.partition.dashboard.snapshot',
+    {
+      url: '/snapshot',
     templateUrl:
       'dedicated/nas/details/partition/snapshot/nas-partition-snapshot.html',
     controller: 'NasPartitionSnapshotCtrl',
     controllerAs: '$ctrl',
     reloadOnSearch: false,
-  });
+    },
+  );
 });
