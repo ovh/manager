f72e9364125ee856a919baf3430946d930760f50
Marie JONES <marie.jones@corp.ovh.com>
2020-09-29 11:30:56 +0200

    feat(dedicated.nas): add breadcrumb
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/dedicated/nas/details/partition/snapshot/nas-partition-snapshot.routes.js
+++ packages/manager/apps/dedicated/client/app/dedicated/nas/details/partition/snapshot/nas-partition-snapshot.routes.js
@@ -3,11 +3,15 @@ angular.module('App').config(($stateProvider) => {
     'app.dedicated-nas.details.partition.dashboard.snapshot',
     {
       url: '/snapshot',
-    templateUrl:
-      'dedicated/nas/details/partition/snapshot/nas-partition-snapshot.html',
-    controller: 'NasPartitionSnapshotCtrl',
-    controllerAs: '$ctrl',
-    reloadOnSearch: false,
+      templateUrl:
+        'dedicated/nas/details/partition/snapshot/nas-partition-snapshot.html',
+      controller: 'NasPartitionSnapshotCtrl',
+      controllerAs: '$ctrl',
+      reloadOnSearch: false,
+      resolve: {
+        breadcrumb: /* @ngInject */ ($translate) =>
+          $translate.instant('nas_details_partition_snapshot'),
+      },
     },
   );
 });
