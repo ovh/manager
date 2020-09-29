d5630cae850b98f55d9a457735e76d25f1026f05
Marie JONES <marie.jones@corp.ovh.com>
2020-09-29 10:54:02 +0200

    refactor(dedicated.nas): revamp state and url
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/server-sidebar/src/sidebar.constants.js
+++ packages/manager/modules/server-sidebar/src/sidebar.constants.js
@@ -115,7 +115,7 @@ export const NETWORKS_CONFIG = {
     },
     {
       path: '/dedicated/nas',
-      state: 'app.networks.nas.details',
+      state: 'app.dedicated-nas.details',
       stateParams: ['nasId'],
       icon: 'ovh-font ovh-font-cloudnas',
       app: [DEDICATED],
@@ -132,7 +132,11 @@ export const NETWORKS_CONFIG = {
       searchKeys: ['NAS', 'NASHA', 'NAS-HA'],
     },
   ],
-  loadOnState: ['nasha.dashboard.nasha-partitions', 'app.networks'],
+  loadOnState: [
+    'nasha.dashboard.nasha-partitions',
+    'app.networks',
+    'dedicated-nas',
+  ],
   icon: 'ovh-font ovh-font-network',
   app: [DEDICATED],
   regions: ['EU', 'CA'],
