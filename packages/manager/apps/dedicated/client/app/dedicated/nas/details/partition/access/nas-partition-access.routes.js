aa181dbe67d8a1456edeb72c7a28bb8cd109f35b
Marie JONES <marie.jones@corp.ovh.com>
2020-09-29 11:29:03 +0200

    feat(dedicated.nas.partition): add intermediate state
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/dedicated/nas/details/partition/access/nas-partition-access.routes.js
+++ packages/manager/apps/dedicated/client/app/dedicated/nas/details/partition/access/nas-partition-access.routes.js
@@ -1,10 +1,15 @@
-angular.module('App').config(($stateProvider) => {
-  $stateProvider.state('app.dedicated-nas.details.partition.access', {
-    url: '/:partitionName/access',
+angular
+  .module('App')
+  .config(($stateProvider) => {
+    $stateProvider.state(
+      'app.dedicated-nas.details.partition.dashboard.access',
+      {
+        url: '/access',
     templateUrl:
       'dedicated/nas/details/partition/access/nas-partition-access.html',
     controller: 'NasPartitionAccessCtrl',
     controllerAs: '$ctrl',
     reloadOnSearch: false,
-  });
-});
+      },
+    );
+  })
