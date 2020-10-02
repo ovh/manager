0f1dd560493da61a84d9cf4d2129bd0234440987
Marie JONES <marie.jones@corp.ovh.com>
2020-10-02 11:39:52 +0200

    feat(license): add breadcrumb
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/license/spla/add/licence-spla-add.routes.js
+++ packages/manager/apps/dedicated/client/app/license/spla/add/licence-spla-add.routes.js
@@ -1,8 +1,15 @@
 angular.module('App').config(($stateProvider) => {
   $stateProvider.state('app.license.dashboard.spla-add', {
     url: '/spla/add',
-    templateUrl: 'license/spla/add/license-spla-add.html',
-    controller: 'LicenseSplaAddCtrl',
+    views: {
+      modal: {
+        templateUrl: 'license/spla/add/license-spla-add.html',
+        controller: 'LicenseSplaAddCtrl',
+      },
+    },
     layout: 'modal',
+    resolve: {
+      hideBreadcrumb: () => true,
+    },
   });
 });
