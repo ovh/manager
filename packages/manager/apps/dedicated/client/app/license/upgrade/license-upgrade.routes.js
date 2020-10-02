2968b8b70b11bee20e4b43b4415f363a37710281
Marie JONES <marie.jones@corp.ovh.com>
2020-10-02 11:32:53 +0200

    refactor(license): revamp state and url
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/license/upgrade/license-upgrade.routes.js
+++ packages/manager/apps/dedicated/client/app/license/upgrade/license-upgrade.routes.js
@@ -1,6 +1,8 @@
-angular.module('Module.license').config(($stateProvider) => {
-  $stateProvider.state('app.license.upgrade', {
-    url: '/:licenseId/upgrade',
+angular
+  .module('Module.license')
+  .config(($stateProvider) => {
+    $stateProvider.state('app.license.detail.upgrade', {
+      url: '/upgrade',
     templateUrl: 'license/upgrade/license-upgrade.html',
     controller: 'LicenseUpgradeCtrl',
     translations: { value: ['..'], format: 'json' },
