85f1f362b25486baacb3172e590a4401ff87e4e6
Marie JONES <marie.jones@corp.ovh.com>
2020-10-02 11:39:52 +0200

    feat(license): add breadcrumb
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/license/upgrade/license-upgrade.routes.js
+++ packages/manager/apps/dedicated/client/app/license/upgrade/license-upgrade.routes.js
@@ -3,8 +3,13 @@ angular
   .config(($stateProvider) => {
     $stateProvider.state('app.license.detail.upgrade', {
       url: '/upgrade',
-    templateUrl: 'license/upgrade/license-upgrade.html',
-    controller: 'LicenseUpgradeCtrl',
-    translations: { value: ['..'], format: 'json' },
-  });
-});
+      templateUrl: 'license/upgrade/license-upgrade.html',
+      controller: 'LicenseUpgradeCtrl',
+      translations: { value: ['..'], format: 'json' },
+      resolve: {
+        breadcrumb: /* @ngInject */ ($translate) =>
+          $translate.instant('license_upgrade'),
+      },
+    });
+  })
+  .run(/* @ngTranslationsInject:json ./translations */);
