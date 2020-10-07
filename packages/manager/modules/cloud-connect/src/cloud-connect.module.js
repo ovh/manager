b9e7a7b8a522b8f7a83b911285ebd93a7e597893
Marie JONES <marie.jones@corp.ovh.com>
2020-10-07 15:24:50 +0200

    feat: add product listing page
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/cloud-connect/src/cloud-connect.module.js
+++ packages/manager/modules/cloud-connect/src/cloud-connect.module.js
@@ -1,18 +1,6 @@
 import angular from 'angular';
 
-import '@ovh-ux/manager-core';
-import 'angular-translate';
-import 'ovh-api-services';
-import '@ovh-ux/ng-at-internet';
-import '@ovh-ux/ng-ovh-cloud-universe-components';
-
-import '@ovh-ux/ui-kit';
-
-import '@ovh-ux/ui-kit/dist/css/oui.css';
-
-import './style.scss';
-
-import component from './cloud-connect.component';
+import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
 import details from './details';
 import routing from './cloud-connect.routing';
 import service from './cloud-connect.service';
@@ -20,16 +8,8 @@ import service from './cloud-connect.service';
 const moduleName = 'ovhCloudConnect';
 
 angular
-  .module(moduleName, [
-    'ngOvhCloudUniverseComponents',
-    'oui',
-    'ovh-api-services',
-    'ovhManagerCore',
-    'pascalprecht.translate',
-    details,
-  ])
+  .module(moduleName, [ListLayoutHelper.moduleName, details])
   .config(routing)
-  .component('cloudConnect', component)
   .service('cloudConnectService', service)
   .run(/* @ngTranslationsInject:json ./translations */);
 
