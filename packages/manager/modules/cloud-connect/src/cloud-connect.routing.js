2eb2d58967c96a6630ce4f5460ae71a4fdef2ba8
Marie JONES <marie.jones@corp.ovh.com>
2020-10-07 15:24:50 +0200

    feat: add product listing page
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/cloud-connect/src/cloud-connect.routing.js
+++ packages/manager/modules/cloud-connect/src/cloud-connect.routing.js
@@ -1,13 +1,34 @@
 import get from 'lodash/get';
+import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
 import { GUIDELINK } from './cloud-connect.constants';
 
 export default /* @ngInject */ ($stateProvider) => {
-  $stateProvider.state('cloud-connect', {
-    url: '/cloud-connect',
-    component: 'cloudConnect',
-    resolve: {
-      user: /* @ngInject */ (OvhApiMe) => OvhApiMe.v6().get().$promise,
-      guideUrl: /* @ngInject */ (user) => get(GUIDELINK, user.ovhSubsidiary),
-    },
-  });
+  $stateProvider
+    .state('cloud-connect', {
+      url: '/cloud-connect',
+      template: '<div ui-view></div>',
+      resolve: {
+        user: /* @ngInject */ (OvhApiMe) => OvhApiMe.v6().get().$promise,
+        guideUrl: /* @ngInject */ (user) => get(GUIDELINK, user.ovhSubsidiary),
+      },
+    })
+    .state('cloud-connect.index', {
+      url: `?${ListLayoutHelper.urlQueryParams}`,
+      component: 'managerListLayout',
+      params: ListLayoutHelper.stateParams,
+      resolve: {
+        ...ListLayoutHelper.stateResolves,
+        apiPath: () => '/ovhCloudConnect',
+        dataModel: () => 'ovhcloudconnect.Service',
+        defaultFilterColumn: () => 'uuid',
+        header: () => 'OVHcloud Connect',
+        customizableColumns: () => true,
+        getServiceNameLink: /* @ngInject */ ($state) => ({
+          uuid: ovhCloudConnectId,
+        }) =>
+          $state.href('cloud-connect.details', {
+            ovhCloudConnectId,
+          }),
+      },
+    });
 };
