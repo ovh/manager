c82ec7e5c973727cc62c5a44955f859d045a16da
Marie JONES <marie.jones@corp.ovh.com>
2020-10-07 11:37:51 +0200

    fix: add missing dependencies
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/cloud-connect/src/app.module.js
+++ packages/manager/apps/cloud-connect/src/app.module.js
@@ -1,5 +1,6 @@
 import 'script-loader!jquery'; // eslint-disable-line
-import '@ovh-ux/manager-cloud-connect';
 import angular from 'angular';
+import '@ovh-ux/manager-cloud-connect';
+import ovhManagerCore from '@ovh-ux/manager-core';
 
-angular.module('cloudConnectApp', ['ovhManagerCloudConnect']);
+angular.module('cloudConnectApp', ['ovhManagerCloudConnect', ovhManagerCore]);
