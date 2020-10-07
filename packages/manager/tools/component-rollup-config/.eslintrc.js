51544f4c2288c1b22c68976af2fdb819f55adc78
Antoine Leblanc <antoine.leblanc@ovhcloud.com>
2020-10-07 09:43:44 +0200

    refactor(eslint): define overrides for .md files from root config file
    Following rules are disabled when writing JavaScript code block into
    Markdown files:
    - import/no-extraneous-dependencies
    - no-undef
    - no-unused-vars
    
    Signed-off-by: Antoine Leblanc <antoine.leblanc@ovhcloud.com>

--- packages/manager/tools/component-rollup-config/.eslintrc.js
+++ packages/manager/tools/component-rollup-config/.eslintrc.js
@@ -1,11 +1,4 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  overrides: [{
-    files: ["**/*.md"],
-    rules: {
-      "no-undef": "off",
-      "import/no-extraneous-dependencies": "off"
-    }
-  }]
 };
