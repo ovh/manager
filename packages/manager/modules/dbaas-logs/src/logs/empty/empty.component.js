1f1bd7efe2b0215db075087e9d2bd777c519f7f6
jjuret <julien.juret@gmail.com>
2020-10-07 17:47:46 +0200

    feat(ldp): add new order process + empty state
    ref : MANAGER-5660
    
    Signed-off-by: jjuret <julien.juret@gmail.com>

---
+++ packages/manager/modules/dbaas-logs/src/logs/empty/empty.component.js
@@ -0,0 +1,14 @@
+import controller from './empty.controller';
+import template from './empty.html';
+
+export default {
+  controller,
+  template,
+  transclude: true,
+  bindings: {
+    imageAlt: '<',
+    imageSource: '<',
+    guides: '<?',
+    onGuideClick: '&',
+  },
+};
