40b10613597c98128468a17438976e086ed95aa9
Marie JONES <marie.jones@corp.ovh.com>
2020-10-07 15:20:24 +0200

    fix: import moment in app not module
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/cloud-connect/src/app.module.js
+++ packages/manager/apps/cloud-connect/src/app.module.js
@@ -1,4 +1,5 @@
 import 'script-loader!jquery'; // eslint-disable-line
+import 'script-loader!moment/min/moment.min.js'; // eslint-disable-line
 import angular from 'angular';
 import ovhManagerCloudConnect from '@ovh-ux/manager-cloud-connect';
 import ovhManagerCore from '@ovh-ux/manager-core';
@@ -6,14 +7,30 @@ import ovhManagerCore from '@ovh-ux/manager-core';
 import { detach as detachPreloader } from '@ovh-ux/manager-preloader';
 
 const moduleName = 'cloudConnectApp';
+angular
+  .module(moduleName, [ovhManagerCore, ovhManagerCloudConnect])
+  .run(
+    /* @ngInject */ ($translate) => {
+      let lang = $translate.use();
 
-angular.module(moduleName, [ovhManagerCloudConnect, ovhManagerCore]).run(
-  /* @ngInject */ ($transitions) => {
-    const unregisterHook = $transitions.onSuccess({}, () => {
-      detachPreloader();
-      unregisterHook();
-    });
-  },
-);
+      if (['en_GB', 'es_US', 'fr_CA'].includes(lang)) {
+        lang = lang.toLowerCase().replace('_', '-');
+      } else {
+        [lang] = lang.split('_');
+      }
+
+      return import(`script-loader!moment/locale/${lang}.js`).then(() =>
+        moment.locale(lang),
+      );
+    },
+  )
+  .run(
+    /* @ngInject */ ($transitions) => {
+      const unregisterHook = $transitions.onSuccess({}, () => {
+        detachPreloader();
+        unregisterHook();
+      });
+    },
+  );
 
 export default moduleName;
