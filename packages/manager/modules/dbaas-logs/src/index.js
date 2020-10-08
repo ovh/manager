00bbedc4817b45dec573988f45fa2736b5c8889c
Marie JONES <marie.jones@corp.ovh.com>
2020-10-08 16:12:11 +0200

    fix: set doc url locale
    ref: MANAGER-5755
    
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/dbaas-logs/src/index.js
+++ packages/manager/modules/dbaas-logs/src/index.js
@@ -3,6 +3,9 @@ import angular from 'angular';
 import '@ovh-ux/manager-core';
 import '@uirouter/angularjs';
 import 'angular-translate';
+import '@ovh-ux/ng-ovh-doc-url';
+
+import { Environment } from '@ovh-ux/manager-config';
 
 import logs from './logs/logs.module';
 
@@ -10,11 +13,17 @@ const moduleName = 'ovhManagerDbaasLogs';
 
 angular
   .module(moduleName, [
+    'ngOvhDocUrl',
     'ovhManagerCore',
     'pascalprecht.translate',
     'ui.router',
     logs,
   ])
+  .config(
+    /* @ngInject */ (ovhDocUrlProvider) => {
+      ovhDocUrlProvider.setUserLocale(Environment.getUserLocale());
+    },
+  )
   .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
