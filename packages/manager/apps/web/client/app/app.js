54c9acd668ecfe2a26c7e3d81e5e8f5860ced3c9
Cyril Biencourt <cyril.biencourt@corp.ovh.com>
2020-10-14 15:08:45 +0200

    feat(navbar): rename web & server universes
    ref: MANAGER-5806
    
    Signed-off-by: Cyril Biencourt <cyril.biencourt@corp.ovh.com>

--- packages/manager/apps/web/client/app/app.js
+++ packages/manager/apps/web/client/app/app.js
@@ -511,16 +511,6 @@ angular
     );
   })
   .constant('UNIVERSE', 'WEB')
-  .constant('MANAGER_URLS', {
-    web: 'https://www.ovh.com/manager/web/index.html#/',
-    dedicated: 'https://www.ovh.com/manager/dedicated/index.html#/',
-    cloud: 'https://www.ovh.com/manager/cloud/index.html#/',
-    telecom: 'https://www.ovhtelecom.fr/manager/index.html#/',
-    sunrise: 'https://www.ovh.com/manager/sunrise/index.html#/',
-    portal: 'https://www.ovh.com/manager/portal/index.html#/',
-    partners: 'https://www.ovh.com/manager/partners/',
-    labs: 'https://www.ovh.com/manager/sunrise/uxlabs/#!/',
-  })
   .run(
     /* @ngInject */ ($rootScope, $state) => {
       $state.defaultErrorHandler((error) => {
