dcdceb6f613cfe5a158ab14a35d1d4fd534abed7
Antoine Leblanc <ant.leblanc@gmail.com>
2020-10-14 07:37:32 +0200

    build(eslint): remove extra overrides configuration
    Signed-off-by: Antoine Leblanc <ant.leblanc@gmail.com>

--- packages/manager/tools/webpack-config/.eslintrc.js
+++ packages/manager/tools/webpack-config/.eslintrc.js
@@ -1,10 +1,4 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  overrides: [{
-    files: ["**/*.md"],
-    rules: {
-      "no-undef": "off"
-    }
-  }]
 };
