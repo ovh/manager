551e6bd8fef57dcefd58eea3e06d5820789c47a2
Marie JONES <marie.jones@corp.ovh.com>
2020-10-15 15:39:25 +0200

    refactor: add component for service billing status
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

---
+++ packages/manager/modules/billing/src/components/service-status/service-status.component.js
@@ -0,0 +1,10 @@
+import controller from './service-status.controller';
+import template from './service-status.html';
+
+export default {
+  bindings: {
+    service: '<',
+  },
+  controller,
+  template,
+};
