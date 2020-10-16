3f4ab01c7cd6d3df5bc071ab9eb27a551ed73365
Marie JONES <marie.jones@corp.ovh.com>
2020-10-16 10:35:20 +0200

    feat: create billing tile
    BREAKING CHANGE: @ovh-ux/ng-ovh-feature-flipping is now a peer dep of module
    
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

---
+++ packages/manager/modules/billing/src/components/subscription-tile/subscription-tile.component.js
@@ -0,0 +1,16 @@
+import controller from './subscription-tile.controller';
+import template from './subscription-tile.html';
+
+export default {
+  bindings: {
+    onError: '&?',
+    withEngagement: '<?',
+    servicePath: '@',
+  },
+  controller,
+  template,
+  transclude: {
+    contactActions: '?billingContactActions',
+    extraInformation: '?billingExtraInformation',
+  },
+};
