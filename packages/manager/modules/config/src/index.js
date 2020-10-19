293b588d439c48bb276416fd078ee884bc28ad54
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2020-10-19 15:01:55 +0200

    fix: export localeStorageKey constant as default
    ref: MANAGER-5648
    
    Signed-off-by: Antoine Leblanc <antoine.leblanc@corp.ovh.com>

--- packages/manager/modules/config/src/index.js
+++ packages/manager/modules/config/src/index.js
@@ -6,7 +6,10 @@ import {
   findLanguage as _findLanguage,
 } from './locale';
 
-import { LANGUAGES as _LANGUAGES } from './locale/locale.constants';
+import {
+  LANGUAGES as _LANGUAGES,
+  localeStorageKey as _localeStorageKey,
+} from './locale/locale.constants';
 
 export const Environment = _Environment;
 
@@ -14,6 +17,7 @@ export const convertLanguageFromOVHToBCP47 = _convertLanguageFromOVHToBCP47;
 export const detectUserLocale = _detectUserLocale;
 export const findLanguage = _findLanguage;
 export const LANGUAGES = _LANGUAGES;
+export const localeStorageKey = _localeStorageKey;
 
 export default {
   convertLanguageFromOVHToBCP47,
@@ -21,4 +25,5 @@ export default {
   detectUserLocale,
   findLanguage,
   LANGUAGES,
+  localeStorageKey, // TODO(@antleblanc): Remove it on February 4th, 2021.
 };
