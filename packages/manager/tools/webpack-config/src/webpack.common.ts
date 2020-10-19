8f94a9127c3d94b119fc0dadff4700e78b993a18
Marie JONES <marie.jones@corp.ovh.com>
2020-10-19 11:32:29 +0200

    Revert "feat: add babel transform runtime plugin"
    This reverts commit b52bbea101592787e5fd3c5628c9842eb90eff5d.

--- packages/manager/tools/webpack-config/src/webpack.common.ts
+++ packages/manager/tools/webpack-config/src/webpack.common.ts
@@ -21,7 +21,7 @@ const cacheLoader = {
 
 // The common webpack configuration
 
-export = (opts) => {
+export = opts => {
   const lessLoaderOptions = {
     sourceMap: true,
     plugins: [
@@ -79,10 +79,14 @@ export = (opts) => {
     ],
 
     resolve: {
-      modules: ['./node_modules', path.resolve('./node_modules')],
+      modules: [
+        './node_modules',
+        path.resolve('./node_modules'),
+      ],
     },
 
     resolveLoader: {
+
       // webpack module resolution paths
       modules: [
         './node_modules', // #1 check in module's relative node_module directory
@@ -92,6 +96,7 @@ export = (opts) => {
 
     module: {
       rules: [
+
         // load HTML files as string (raw-loader)
         {
           test: /\.html$/,
@@ -198,7 +203,6 @@ export = (opts) => {
                   require.resolve('@babel/plugin-proposal-private-methods'), // private methods
                   require.resolve('@babel/plugin-syntax-dynamic-import'), // dynamic es6 imports
                   require.resolve('babel-plugin-angularjs-annotate'), // ng annotate
-                  require.resolve('@babel/plugin-transform-runtime'),
                 ],
                 shouldPrintComment: (val) => !/@ngInject/.test(val),
               },
@@ -215,10 +219,7 @@ export = (opts) => {
           use: [
             cacheLoader,
             {
-              loader: path.resolve(
-                __dirname,
-                './loaders/translation-ui-router.js',
-              ),
+              loader: path.resolve(__dirname, './loaders/translation-ui-router.js'),
               options: {
                 subdirectory: 'translations',
                 filtering: false,
@@ -235,10 +236,7 @@ export = (opts) => {
           use: [
             cacheLoader,
             {
-              loader: path.resolve(
-                __dirname,
-                './loaders/translation-inject.js',
-              ),
+              loader: path.resolve(__dirname, './loaders/translation-inject.js'),
               options: {
                 filtering: false,
               },
@@ -253,6 +251,7 @@ export = (opts) => {
       runtimeChunk: 'single',
       // bundle spliting configuration
       splitChunks: {
+
         // vendors bundle containing node_modules source code
         cacheGroups: {
           bower: {
@@ -277,6 +276,7 @@ export = (opts) => {
           },
         },
       },
+
     }, // \optimization
   };
 };
