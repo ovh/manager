be33bb819c79b478c229722db3c0862dfbac33db
jjuret <julien.juret@gmail.com>
2020-10-20 16:21:48 +0200

    refactor(pack): refactor xdsl resiliation
    Signed-off-by: jjuret <julien.juret@gmail.com>

--- packages/manager/apps/telecom/src/app/telecom/pack/xdsl/index.js
+++ packages/manager/apps/telecom/src/app/telecom/pack/xdsl/index.js
@@ -2,6 +2,8 @@ import angular from 'angular';
 
 import access from './access';
 import modem from './modem';
+import meetings from './meetings';
+import resiliation from './resiliation';
 
 import component from './pack-xdsl.component';
 import routing from './pack-xdsl.routing';
@@ -10,7 +12,7 @@ import templates from './pack-xdsl.templates';
 const moduleName = 'ovhManagerTelecomPackXdsl';
 
 angular
-  .module(moduleName, [access, modem])
+  .module(moduleName, [access, modem, meetings, resiliation])
   .component('packXdsl', component)
   .config(routing)
   .run(templates)
