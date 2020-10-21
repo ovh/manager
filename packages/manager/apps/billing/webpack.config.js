a43751e12d91bd2898804da7637b0d9868c70323
Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>
2020-10-21 11:48:18 +0530

    refactor(billing): added billing app
    Signed-off-by: Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>

---
+++ packages/manager/apps/billing/webpack.config.js
@@ -0,0 +1,44 @@
+const _ = require('lodash');
+const path = require('path');
+const webpack = require('webpack'); // eslint-disable-line
+const merge = require('webpack-merge');
+const webpackConfig = require('@ovh-ux/manager-webpack-config');
+
+module.exports = (env = {}) => {
+  const REGION = _.upperCase(env.region || process.env.REGION || 'EU');
+
+  const { config } = webpackConfig(
+    {
+      template: './src/index.html',
+      basePath: './src',
+      lessPath: ['./node_modules'],
+      root: path.resolve(__dirname, './src'),
+    },
+    REGION ? Object.assign(env, { region: REGION }) : env,
+  );
+
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      WEBPACK_ENV: {
+        production: JSON.stringify(env.production),
+      },
+    }),
+  );
+
+  return merge(config, {
+    entry: path.resolve('./src/index.js'),
+    resolve: {
+      modules: [
+        './node_modules',
+        path.resolve(__dirname, 'node_modules'),
+        path.resolve(__dirname, '../../../node_modules'),
+      ],
+      mainFields: ['module', 'browser', 'main'],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        __WEBPACK_REGION__: `'${REGION}'`,
+      }),
+    ],
+  });
+};
