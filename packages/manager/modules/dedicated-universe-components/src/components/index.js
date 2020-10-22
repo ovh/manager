d69030126e35737b22983013c8c6e56661799058
Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>
2020-10-22 12:21:36 +0530

    refactor(dedicated): dedicated universal components
    Signed-off-by: Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>

--- packages/manager/modules/dedicated-universe-components/src/components/index.js
+++ packages/manager/modules/dedicated-universe-components/src/components/index.js
@@ -2,8 +2,10 @@ import angular from 'angular';
 
 import user from './user';
 import noDefaultMeansOfPayment from './no-default-means-of-payment';
+import stepper from './stepper';
+import OvhHttp from './ovh-angular-http/ovh-angular-http.module';
 
-const moduleName = 'dedicatedUniverseComponents';
-angular.module(moduleName, [user, noDefaultMeansOfPayment]);
+const moduleName = 'ducComponents';
+angular.module(moduleName, [user, noDefaultMeansOfPayment, stepper, OvhHttp]);
 
 export default moduleName;
