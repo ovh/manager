ef4974cd00f8a1b77b053c4793199e55bd994785
Stéphanie MOALLIC <stephanie.moallic@gmail.com>
2020-10-22 16:37:38 +0200

    feat(xdsl.modem): add acs backend config (#3646)
    Add ACS backend configuration into modem tab.
    
    ref: UXCT-278
    
    Signed-off-by: Stephanie Moallic <stephanie.moallic@corp.ovh.com>

--- packages/manager/apps/telecom/src/app/telecom/pack/xdsl/modem/index.js
+++ packages/manager/apps/telecom/src/app/telecom/pack/xdsl/modem/index.js
@@ -5,11 +5,12 @@ import '@ovh-ux/ng-ovh-telecom-universe-components';
 import component from './pack-xdsl-modem.component';
 import { PACK_XDSL_MODEM } from './pack-xdsl-modem.constant';
 import routing from './pack-xdsl-modem.routing';
+import acsBackend from './acsBackend';
 
 const moduleName = 'ovhManagerTelecomPackXdslModem';
 
 angular
-  .module(moduleName, ['ngOvhTelecomUniverseComponents'])
+  .module(moduleName, ['ngOvhTelecomUniverseComponents', acsBackend])
   .component('packXdslModem', component)
   .config(routing)
   .constant('PACK_XDSL_MODEM', PACK_XDSL_MODEM)
