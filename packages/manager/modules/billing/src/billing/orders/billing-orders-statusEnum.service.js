1770d0a875a533790ab5697763705e383c7d40c9
Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>
2020-10-23 12:54:26 +0530

    refactor(billing): migrate orders
    Signed-off-by: Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>

--- packages/manager/modules/billing/src/billing/orders/billing-orders-statusEnum.service.js
+++ packages/manager/modules/billing/src/billing/orders/billing-orders-statusEnum.service.js
@@ -3,27 +3,25 @@ import map from 'lodash/map';
 
 import snakeCase from 'lodash/snakeCase';
 
-angular
-  .module('Billing.services')
-  .service('BillingOrderStatusEnum', function BillingOrderStatusEnum(
-    BillingApiSchema,
-    $q,
-  ) {
-    let orderStatusEnum;
+export default /* @ngInject */ function BillingOrderStatusEnum(
+  BillingApiSchema,
+  $q,
+) {
+  let orderStatusEnum;
 
-    this.getEnum = function getEnum() {
-      if (orderStatusEnum) {
-        return $q.when(orderStatusEnum);
-      }
+  this.getEnum = function getEnum() {
+    if (orderStatusEnum) {
+      return $q.when(orderStatusEnum);
+    }
 
-      return BillingApiSchema.getSchema('me').then((schema) => {
-        orderStatusEnum = fromPairs(
-          map(schema.models['billing.order.OrderStatusEnum'].enum, (status) => {
-            const key = snakeCase(status).toUpperCase();
-            return [key, status];
-          }),
-        );
-        return orderStatusEnum;
-      });
-    };
-  });
+    return BillingApiSchema.getSchema('me').then((schema) => {
+      orderStatusEnum = fromPairs(
+        map(schema.models['billing.order.OrderStatusEnum'].enum, (status) => {
+          const key = snakeCase(status).toUpperCase();
+          return [key, status];
+        }),
+      );
+      return orderStatusEnum;
+    });
+  };
+}
