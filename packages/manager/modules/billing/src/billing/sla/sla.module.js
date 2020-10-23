1e14fc6c53a8f24adfc91eacb6fcf1d7f184257d
Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>
2020-10-23 16:29:38 +0530

    refactor(billing): migrate sla
    Signed-off-by: Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>

--- packages/manager/modules/billing/src/billing/sla/sla.module.js
+++ packages/manager/modules/billing/src/billing/sla/sla.module.js
@@ -1,7 +1,13 @@
 import routing from './sla.routing';
+import service from './billing-sla.service';
+import controller from './billing-sla.controller';
 
 const moduleName = 'ovhManagerBillingSla';
 
-angular.module(moduleName, ['ui.router']).config(routing);
+angular
+  .module(moduleName, ['ui.router'])
+  .config(routing)
+  .controller('Billing.controllers.Sla', controller)
+  .service('BillingSla', service);
 
 export default moduleName;
