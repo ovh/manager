8c22200831fb31f27760601b5f48940a09c6d6e4
Marie JONES <marie.jones@corp.ovh.com>
2020-10-28 10:37:46 +0100

    fix(project.users): only use region with storage capacity
    ref: DTRSD-22922
    
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/pci/src/projects/project/users/users.constants.js
+++ packages/manager/modules/pci/src/projects/project/users/users.constants.js
@@ -2,7 +2,10 @@ export const HORIZON_LINK =
   'https://horizon.cloud.ovh.net/auth/login?username={username}';
 export const ALPHA_CHARACTERS_REGEX = /^[a-zA-Z-]+$/;
 
+export const REGION_CAPACITY = 'storage';
+
 export default {
   HORIZON_LINK,
   ALPHA_CHARACTERS_REGEX,
+  REGION_CAPACITY,
 };
