4cf38707abd617a6669fa64d005758a7d2e17f6f
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2020-10-30 13:51:07 +0100

    feat(server): move dedicatedCloud in hpc namespace
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/modules/server-sidebar/src/constants.js
+++ packages/manager/modules/server-sidebar/src/constants.js
@@ -2,8 +2,11 @@ export const DEDICATED = 'DEDICATED';
 export const CLOUD = 'CLOUD';
 export const WEB = 'WEB';
 
+export const HPC_NAMESPACE = 'hpc';
+
 export default {
   DEDICATED,
   CLOUD,
   WEB,
+  HPC_NAMESPACE,
 };
