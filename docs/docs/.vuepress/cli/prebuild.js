7dab529a91a7134688a9f8b3888192456318c552
Antoine Leblanc <antoine.leblanc@ovhcloud.com>
2020-11-02 11:35:58 +0100

    build(dev-deps): upgrade VuePress to v1.7.1
    uses: `$ yarn upgrade-interactive --latest vuepress`
    - vuepress@1.7.1
    
    Add new workspace `docs` (single package).
    Update prebuild cli command script.
    
    Signed-off-by: Antoine Leblanc <antoine.leblanc@ovhcloud.com>

--- docs/docs/.vuepress/cli/prebuild.js
+++ docs/docs/.vuepress/cli/prebuild.js
@@ -3,13 +3,13 @@ const path = require('path');
 const pick = require('lodash/pick');
 
 // Get all workspaces defined in the `package.json` file.
-const { workspaces } = require('../../../package.json');
+const { workspaces } = require('./../../../../package.json');
 
 // Keep the bare minimum entries from all package.json files.
 const entries = ['name', 'version', 'description', 'repository'];
 
 // Get all packages contained inside the monorepo.
-const directoryPath = path.resolve();
+const directoryPath = path.resolve('../');
 const packages = getMonorepoPackages(directoryPath).map(
   ({ location, package: pkg }) => ({
     location,
@@ -21,7 +21,7 @@ const packages = getMonorepoPackages(directoryPath).map(
 const groupedWorkspaces = workspaces.map((workspace) => {
   const packagesList = packages.filter((pkg) => {
     // Remove the pattern `/*` from the workspace name.
-    if (pkg.location.includes(workspace.slice(0, -2))) {
+    if (workspace.includes('/*') && pkg.location.includes(workspace.slice(0, -2))) {
       return Object.assign(pkg, {
         location: pkg.location.replace(directoryPath, ''),
       });
