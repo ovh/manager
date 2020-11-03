8637ef16f547420ba4f87d6081ae12038a0e17a4
Jisay <1774679+Jisay@users.noreply.github.com>
2020-11-03 08:44:01 +0100

    fix(loaders): add es6 proposals for translations webpack plugins (#3816)
    Signed-off-by: Jisay <1774679+Jisay@users.noreply.github.com>
    Co-authored-by: Antoine Leblanc <antoine.leblanc@ovhcloud.com>

--- packages/manager/tools/webpack-config/src/loaders/translation-inject.ts
+++ packages/manager/tools/webpack-config/src/loaders/translation-inject.ts
@@ -1,13 +1,21 @@
 import { get, merge, pick } from 'lodash';
 import { getOptions } from 'loader-utils';
 import { Parser } from 'acorn';
+import classFields from 'acorn-class-fields';
+import optionalChaining from 'acorn-optional-chaining';
+import privateMethods from 'acorn-private-methods';
 import componentConfig from '@ovh-ux/component-rollup-config';
 import dynamicImport from 'acorn-dynamic-import';
 
 export = function injectTranslationsLoader(source) {
   const options = merge({ filtering: false }, getOptions(this));
   const translationInject = get(componentConfig, 'plugins.translationInject');
-  const parser = Parser.extend(dynamicImport);
+
+  const parser = Parser.extend(dynamicImport)
+    .extend(classFields)
+    .extend(optionalChaining)
+    .extend(privateMethods);
+
   return get(
     translationInject(
       pick(options, ['subdirectory', 'filtering']),
