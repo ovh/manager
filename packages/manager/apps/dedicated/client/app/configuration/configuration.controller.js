4e2f7db361654d61437bbf524a085dbac954298d
Cyril Biencourt <cyril.biencourt@ovhcloud.com>
2020-11-11 07:57:30 +0100

    fix(dedicated): filter guides by universe
    ref: MANAGER-5967
    
    Signed-off-by: Cyril Biencourt <cyril.biencourt@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/configuration/configuration.controller.js
+++ packages/manager/apps/dedicated/client/app/configuration/configuration.controller.js
@@ -1,70 +1,53 @@
-import isArray from 'lodash/isArray';
-import isEmpty from 'lodash/isEmpty';
+import get from 'lodash/get';
 import isString from 'lodash/isString';
 
 angular.module('App').controller(
   'configurationCtrl',
   class ConfigurationCtrl {
-    constructor($q, $translate, constants, coreConfig, OvhHttp, User) {
-      this.$q = $q;
+    constructor($scope, $translate, constants, User) {
+      this.$scope = $scope;
       this.$translate = $translate;
       this.constants = constants;
-      this.coreConfig = coreConfig;
-      this.OvhHttp = OvhHttp;
       this.User = User;
     }
 
     $onInit() {
       this.currentLanguage = this.$translate.use();
       this.fallbackLanguage = this.$translate.fallbackLanguage();
+      this.sectionNames = this.isHPC ? ['pcc'] : ['sd'];
       this.urlToAllGuides = this.getURLFromSection(
         this.constants.TOP_GUIDES.all,
       );
 
-      return this.buildingGuideURLs().then(() => this.gettingHelpCenterURLs());
+      this.$scope.$on('switchUniverse', (event, universe) => {
+        this.sectionNames = universe === 'hpc' ? ['pcc'] : ['sd'];
+        this.buildingGuideURLs();
+      });
+
+      return this.gettingHelpCenterURLs().then(() => this.buildingGuideURLs());
     }
 
     getURLFromSection(section) {
       if (isString(section)) {
         return section;
       }
-      return section[this.currentLanguage] || section[this.fallbackLanguage];
+      return (
+        get(section, this.currentLanguage) ||
+        get(section, this.fallbackLanguage)
+      );
     }
 
     buildingGuideURLs() {
-      return this.fetchingGuideSectionNames().then((sectionNames) => {
-        this.sections = sectionNames.reduce(
-          (sections, sectionName) => ({
-            ...sections,
-            [sectionName]: {
-              name: sectionName,
-              links: this.getURLFromSection(
-                this.constants.TOP_GUIDES[sectionName],
-              ),
-            },
-          }),
-          {},
-        );
-      });
-    }
-
-    fetchingGuideSectionNames() {
-      const sectionNames = ['sd'];
-
-      if (this.coreConfig.getRegion() === 'US') {
-        return this.OvhHttp.get('/dedicatedCloud', {
-          rootPath: 'apiv6',
-        }).then((ids) => {
-          if (isArray(ids) && !isEmpty(ids)) {
-            return sectionNames;
-          }
-
-          return [];
-        });
-      }
-
-      sectionNames.push('pcc');
-      return this.$q.when(sectionNames);
+      this.sections = this.sectionNames.reduce(
+        (sections, name) => ({
+          ...sections,
+          [name]: {
+            name,
+            links: this.getURLFromSection(get(this.constants.TOP_GUIDES, name)),
+          },
+        }),
+        {},
+      );
     }
 
     gettingHelpCenterURLs() {
