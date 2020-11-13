2d33bf09732d06d4423780dbe409ac01c73e9d1f
frenauvh <florian.renaut@corp.ovh.com>
2020-11-13 15:37:21 +0100

    feat(loader): drop xml translation
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>
    
    BREAKING CHANGE: xml translation files are not supported anymore

--- packages/manager/tools/webpack-config/src/loaders/translation-xml.ts
+++
@@ -1,9 +0,0 @@
-import { get, merge } from 'lodash';
-import { getOptions } from 'loader-utils';
-import componentConfig from '@ovh-ux/component-rollup-config';
-
-export = function translationsXMLLoader(source) {
-  const options = merge({ filtering: false }, getOptions(this));
-  const translationXML = get(componentConfig, 'plugins.translationXML');
-  return get(translationXML(options).transform(source, this.resourcePath), 'code', source);
-};
