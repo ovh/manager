d36490c81c943f04113f9352073dbc4f15d6ad01
Marie JONES <marie.jones@corp.ovh.com>
2020-11-16 15:07:15 +0100

    refactor(dedicated-cloud): move listing page as first architecture level
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

---
+++ packages/manager/apps/dedicated/client/app/dedicatedCloud/details/dedicatedCloud.module.js
@@ -0,0 +1,33 @@
+import angular from 'angular';
+
+import confirmTerminate from '../confirm-terminate';
+import dashboard from '../dashboard/dedicatedCloud-dashboard.module';
+import datacenter from '../datacenter';
+import datacenters from '../datacenters';
+import dedicatedCloudComponent from '../../components/dedicated-cloud';
+import license from '../license';
+import operation from '../operation';
+import security from '../security';
+import servicePackUpgrade from '../service-pack/upgrade';
+import user from '../user';
+
+import routing from './dedicatedCloud.routing';
+
+const moduleName = 'ovhManagerDedicatedCloudModule';
+
+angular
+  .module(moduleName, [
+    confirmTerminate,
+    dashboard,
+    datacenter,
+    datacenters,
+    dedicatedCloudComponent,
+    license,
+    operation,
+    security,
+    servicePackUpgrade,
+    user,
+  ])
+  .config(routing);
+
+export default moduleName;
