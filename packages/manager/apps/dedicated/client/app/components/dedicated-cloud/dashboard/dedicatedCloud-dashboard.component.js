0ee6456d39e2ddb18db037a451b3a44e5c5a4879
Ganesh Kumar <ganesh.kumar@corp.ovh.com>
2020-11-17 21:27:00 +0530

    fix(dedicated): user rights text
    fix text for essentials and premier offers
    
    Fixes #MANAGER-5997
    
    Signed-off-by: Ganesh Kumar <ganesh.kumar@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/components/dedicated-cloud/dashboard/dedicatedCloud-dashboard.component.js
+++ packages/manager/apps/dedicated/client/app/components/dedicated-cloud/dashboard/dedicatedCloud-dashboard.component.js
@@ -25,6 +25,7 @@ export default {
     onConfigurationOnlyUpgrade: '<',
     orderSecurityOption: '<',
     orderVmwareOption: '<',
+    pccType: '<',
     productId: '<',
     setMessage: '<',
     surveyUrl: '<',
