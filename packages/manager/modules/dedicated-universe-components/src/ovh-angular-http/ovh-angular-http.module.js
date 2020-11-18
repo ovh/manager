6e03dbb32e4866a155129724597141e121c48308
Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>
2020-11-18 16:54:43 +0530

    refactor(duc): refactor common components
    ref #MANAGER-5821
    
    Signed-off-by: Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>

--- packages/manager/modules/dedicated-universe-components/src/ovh-angular-http/ovh-angular-http.module.js
+++ packages/manager/modules/dedicated-universe-components/src/ovh-angular-http/ovh-angular-http.module.js
@@ -1,8 +1,12 @@
 import set from 'lodash/set';
 
-angular.module('App').config((OvhHttpProvider, constants) => {
+const moduleName = 'ducOvhAngularHttp';
+
+angular.module(moduleName, []).config((OvhHttpProvider, constants) => {
   set(OvhHttpProvider, 'rootPath', constants.swsProxyPath);
   set(OvhHttpProvider, 'clearCacheVerb', ['POST', 'PUT', 'DELETE']);
   set(OvhHttpProvider, 'returnSuccessKey', 'data'); // By default, request return response.data
   set(OvhHttpProvider, 'returnErrorKey', 'data'); // By default, request return error.data
 });
+
+export default moduleName;
