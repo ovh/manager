4297583f3052a4d48eb1b4b9f747e230a0488230
Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>
2020-11-18 16:21:54 +0530

    refactor(duc): moved common components
    ref #MANAGER-5821
    
    Signed-off-by: Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>

--- packages/manager/modules/dedicated-universe-components/src/directives/sshkeySwitch/sshkeySwitch.js
+++ packages/manager/modules/dedicated-universe-components/src/directives/sshkeySwitch/sshkeySwitch.js
@@ -1,36 +1,39 @@
-export default /* @ngInject */ function sshkeySwitchDirective(ducSshkeyRegex) {
-  return {
-    template:
-      '<div><span class="label label-default ml-2" data-ng-class="{\'label-primary\': selectedType == keytype.name}" data-ng-repeat="keytype in keytypes" data-ng-bind="keytype.name"></span></div>',
-    restrict: 'A',
-    require: '?ngModel',
-    replace: true,
-    scope: {
-      isValid: '=ngModel',
-      sshkeySwitchKey: '@ducSshkeySwitchKey',
-    },
-    link: function postLink($scope) {
-      $scope.isValid = false;
-      $scope.selectedType = false;
-      $scope.keytypes = ducSshkeyRegex;
+angular.module('UserAccount').directive('sshkeySwitch', [
+  'sshkey-regex',
+  function sshkeySwitchDirective(SSHKEY_REGEX) {
+    return {
+      template:
+        '<div><span class="label label-default ml-2" data-ng-class="{\'label-primary\': selectedType == keytype.name}" data-ng-repeat="keytype in keytypes" data-ng-bind="keytype.name"></span></div>',
+      restrict: 'A',
+      require: '?ngModel',
+      replace: true,
+      scope: {
+        isValid: '=ngModel',
+        sshkeySwitchKey: '@sshkeySwitchKey',
+      },
+      link: function postLink($scope) {
+        $scope.isValid = false;
+        $scope.selectedType = false;
+        $scope.keytypes = SSHKEY_REGEX;
 
-      let i = 0;
-      let found = false;
+        let i = 0;
+        let found = false;
 
-      $scope.$watch('sshkeySwitchKey', (val) => {
-        found = false;
-        const valTrimmed = val.trim().replace(/\n/, '');
-        /* eslint-disable */
-          for (i = ducSshkeyRegex.length; i--; ) {
-            if (ducSshkeyRegex[i].regex.test(valTrimmed)) {
-              found = ducSshkeyRegex[i].name;
+        $scope.$watch('sshkeySwitchKey', (val) => {
+          found = false;
+          const valTrimmed = val.trim().replace(/\n/, '');
+          /* eslint-disable */
+          for (i = SSHKEY_REGEX.length; i--; ) {
+            if (SSHKEY_REGEX[i].regex.test(valTrimmed)) {
+              found = SSHKEY_REGEX[i].name;
               break;
             }
           }
           /* eslint-enable */
-        $scope.selectedType = found;
-        $scope.isValid = !!found;
-      });
-    },
-  };
-}
+          $scope.selectedType = found;
+          $scope.isValid = !!found;
+        });
+      },
+    };
+  },
+]);
