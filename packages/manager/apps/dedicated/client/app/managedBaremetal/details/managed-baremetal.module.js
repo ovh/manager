6ccefca062f46a3c7e865bd6d278368005eaa097
Marie JONES <marie.jones@corp.ovh.com>
2020-11-20 09:49:50 +0100

    feat(managed-baremetal): add listing page
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

---
+++ packages/manager/apps/dedicated/client/app/managedBaremetal/details/managed-baremetal.module.js
@@ -0,0 +1,45 @@
+import angular from 'angular';
+
+import get from 'lodash/get'
+
+import confirmTerminate from '../confirm-terminate';
+import dashboard from '../dashboard/dashboard.module';
+import datacenter from '../datacenter';
+import datacenters from '../datacenters';
+import dedicatedCloudComponent from '../../components/dedicated-cloud';
+import license from '../license';
+import operation from '../operation';
+import routing from './managed-baremetal.routing';
+import security from '../security';
+import servicePackUpgrade from '../service-pack/upgrade';
+import user from '../user';
+
+const moduleName = 'managedBaremetalModule';
+
+angular
+  .module(moduleName, [
+    confirmTerminate,
+    dashboard,
+    datacenter,
+    datacenters,
+    dedicatedCloudComponent,
+    license,
+    operation,
+    security,
+    servicePackUpgrade,
+    user,
+  ])
+  .config(routing)
+  .run(
+    /* @ngInject */ ($state, $transitions) => {
+      $transitions.onError({}, (transition) => {
+        const error = transition.error();
+        if (get(error, 'detail.code') === 460) {
+          error.handled = true;
+          $state.go('app.expired', { error });
+        }
+      });
+    },
+  );
+
+export default moduleName;
