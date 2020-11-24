607d5347329b5866f27a1055ee8c243ffbea1824
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2020-11-24 15:54:03 +0100

    perf(chatbot): delay the chatbot initialization
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/public-cloud/src/app.module.js
+++ packages/manager/apps/public-cloud/src/app.module.js
@@ -129,6 +129,7 @@ angular
     /* @ngInject */ ($rootScope, $transitions) => {
       const unregisterHook = $transitions.onSuccess({}, () => {
         detachPreloader();
+        $rootScope.$broadcast('app:started');
         unregisterHook();
       });
     },
