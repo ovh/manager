11e0dcc03aa2dc06896c622d94f74cd608a33641
Antoine Leblanc <antoine.leblanc@ovhcloud.com>
2020-11-27 10:36:12 +0100

    feat(ldp): add new order process + empty state
    This reverts commit daf6668672d55fb3d42b15db9c702caccf80c48f.
    ref: MANAGER-5660
    
    Signed-off-by: Antoine Leblanc <antoine.leblanc@ovhcloud.com>

---
+++ packages/manager/modules/dbaas-logs/src/logs/onboarding/onboarding.constants.js
@@ -0,0 +1,21 @@
+export const GUIDES = [
+  {
+    id: 'overview',
+    link: 'https://www.ovh.com/fr/data-platforms/logs/',
+    tracker: 'dbaas::logs::onboarding::go-to-presentation',
+  },
+  {
+    id: 'quick_start',
+    link: 'https://docs.ovh.com/fr/logs-data-platform/',
+    tracker: 'dbaas::logs::onboarding::go-to-doc',
+  },
+  {
+    id: 'community',
+    link: 'https://community.ovh.com/c/platform/data-platforms',
+    tracker: 'dbaas::logs::onboarding::go-to-community',
+  },
+];
+
+export default {
+  GUIDES,
+};
