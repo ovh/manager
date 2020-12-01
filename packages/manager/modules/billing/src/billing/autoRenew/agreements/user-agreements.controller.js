213efc76373d2feeac52b5de0c6aeebf45fd89c7
Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>
2020-12-01 15:28:56 +0530

    refactor(billing): fix review comments
    Signed-off-by: Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>

--- packages/manager/modules/billing/src/billing/autoRenew/agreements/user-agreements.controller.js
+++ packages/manager/modules/billing/src/billing/autoRenew/agreements/user-agreements.controller.js
@@ -1,112 +1,104 @@
 import get from 'lodash/get';
 
-export default [
-  '$scope',
-  '$translate',
-  'Alerter',
-  'atInternet',
-  'gotoAcceptAllAgreements',
-  'UserAccountServicesAgreements',
-  function UserAccountAgreementsController(
-    $scope,
-    $translate,
-    Alerter,
-    atInternet,
-    gotoAcceptAllAgreements,
-    Service,
-  ) {
-    function init() {
-      $scope.loading = true;
-      $scope.list = [];
+export default /* @ngInject */ function UserAccountAgreementsController(
+  $scope,
+  $translate,
+  Alerter,
+  atInternet,
+  gotoAcceptAllAgreements,
+  UserAccountServicesAgreements,
+) {
+  function init() {
+    $scope.loading = true;
+    $scope.list = [];
 
-      $scope.getToValidate();
-    }
+    $scope.getToValidate();
+  }
 
-    $scope.loaders = {
-      toActivate: true,
-      toActivateList: true,
-    };
+  $scope.loaders = {
+    toActivate: true,
+    toActivateList: true,
+  };
 
-    $scope.toActivate = [];
+  $scope.toActivate = [];
 
-    $scope.agreed = {};
+  $scope.agreed = {};
 
-    $scope.gotoAcceptAllAgreements = gotoAcceptAllAgreements;
+  $scope.gotoAcceptAllAgreements = gotoAcceptAllAgreements;
 
-    $scope.loadAgreementsList = function loadAgreementsList(count, offset) {
-      init();
+  $scope.loadAgreementsList = function loadAgreementsList(count, offset) {
+    init();
 
-      Service.getList(count, offset)
-        .then(
-          (agreements) => {
-            $scope.list = agreements;
-          },
-          (err) => {
-            Alerter.error(
-              `${$translate.instant('user_agreements_error')} ${get(
-                err,
-                'message',
-              ) || err}`,
-              'agreements_alerter',
-            );
-          },
-        )
-        .then(() => {
-          $scope.loading = false;
-        });
-    };
-
-    $scope.getToValidate = function getToValidate() {
-      $scope.toActivate = [];
-      $scope.loaders.toActivate = true;
-
-      Service.getToValidate().then(
+    UserAccountServicesAgreements.getList(count, offset)
+      .then(
         (agreements) => {
-          $scope.toActivate = agreements;
-          $scope.loaders.toActivate = false;
+          $scope.list = agreements;
         },
-        angular.noop,
-        (contract) => {
-          $scope.toActivate.push(contract);
-          $scope.agreed[contract.id] = false;
+        (err) => {
+          Alerter.error(
+            `${$translate.instant('user_agreements_error')} ${get(
+              err,
+              'message',
+            ) || err}`,
+            'agreements_alerter',
+          );
         },
-      );
-    };
-
-    $scope.accept = function accept(contract) {
-      atInternet.trackClick({
-        name:
-          'dedicated::account::billing::autorenew::agreements::go-to-accept-agreement',
-        type: 'action',
+      )
+      .then(() => {
+        $scope.loading = false;
       });
-      $scope.loaders[`accept_${contract.id}`] = true;
+  };
 
-      Service.accept(contract)
-        .then(
-          () => {
-            $scope.getToValidate();
-            $scope.$broadcast('paginationServerSide.reload', 'agreementsList');
-          },
-          (d) => {
-            Alerter.set(
-              'alert-danger',
-              `${$translate.instant('user_agreement_details_error')} ${get(
-                d,
-                'data.message',
-                d,
-              )}`,
-              null,
-              'agreements_alerter',
-            );
-          },
-        )
-        .finally(() => {
-          $scope.loaders[`accept_${contract.id}`] = false;
-        });
-    };
+  $scope.getToValidate = function getToValidate() {
+    $scope.toActivate = [];
+    $scope.loaders.toActivate = true;
 
-    $scope.resetMessages = function resetMessages() {
-      Alerter.resetMessage('agreements_alerter');
-    };
-  },
-];
+    UserAccountServicesAgreements.getToValidate().then(
+      (agreements) => {
+        $scope.toActivate = agreements;
+        $scope.loaders.toActivate = false;
+      },
+      angular.noop,
+      (contract) => {
+        $scope.toActivate.push(contract);
+        $scope.agreed[contract.id] = false;
+      },
+    );
+  };
+
+  $scope.accept = function accept(contract) {
+    atInternet.trackClick({
+      name:
+        'dedicated::account::billing::autorenew::agreements::go-to-accept-agreement',
+      type: 'action',
+    });
+    $scope.loaders[`accept_${contract.id}`] = true;
+
+    UserAccountServicesAgreements.accept(contract)
+      .then(
+        () => {
+          $scope.getToValidate();
+          $scope.$broadcast('paginationServerSide.reload', 'agreementsList');
+        },
+        (d) => {
+          Alerter.set(
+            'alert-danger',
+            `${$translate.instant('user_agreement_details_error')} ${get(
+              d,
+              'data.message',
+              d,
+            )}`,
+            null,
+            'agreements_alerter',
+          );
+        },
+      )
+      .finally(() => {
+        $scope.loaders[`accept_${contract.id}`] = false;
+      });
+  };
+
+  $scope.resetMessages = function resetMessages() {
+    Alerter.resetMessage('agreements_alerter');
+  };
+}
