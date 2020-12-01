6743a9a68e62c051dcd7f699c253f41cd944078e
frenauvh <florian.renaut@corp.ovh.com>
2020-12-01 08:20:57 +0100

    fix(fragment-navbar): handle case of 0 unread notifications
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/fragments/navbar/src/notifications.jsx
+++ packages/manager/fragments/navbar/src/notifications.jsx
@@ -6,13 +6,11 @@ import { MESSAGES } from './constants';
 
 function NavbarNotifications() {
   const { t } = useTranslation();
-  const [notificationsCount, setNotificationsCount] = useState(0);
+  const [notificationsCount, setNotificationsCount] = useState(null);
 
   useEffect(() => {
-    listen(({ id, count }) => {
-      if (id === MESSAGES.notificationsCount) {
-        setNotificationsCount(count);
-      }
+    listen(MESSAGES.notificationsCount, ({ count }) => {
+      setNotificationsCount(count);
     });
   }, []);
 
@@ -23,7 +21,7 @@ function NavbarNotifications() {
       className={`oui-navbar-link oui-navbar-link_icon oui-navbar-link_tertiary ${style.navbarLink}`}
       title={t('navbar_notifications')}
       aria-label={t('navbar_notifications')}
-      disabled={notificationsCount === 0}
+      disabled={notificationsCount === null}
       onClick={() => {
         emit({
           id: MESSAGES.accountSidebarHide,
@@ -34,7 +32,9 @@ function NavbarNotifications() {
       }}
     >
       <span className="oui-icon oui-icon-bell" aria-hidden="true">
-        <span className="oui-icon__badge">{notificationsCount}</span>
+        {notificationsCount !== null && (
+          <span className="oui-icon__badge">{notificationsCount}</span>
+        )}
       </span>
     </button>
   );
