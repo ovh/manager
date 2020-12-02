bdf0b6d595266a1076bbac553f9b0f6eb3d20a52
Marie JONES <marie.jones@corp.ovh.com>
2020-12-02 11:56:11 +0100

    fix: avoid loading unecessary dependencies
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/dbaas-logs/src/index.js
+++ packages/manager/modules/dbaas-logs/src/index.js
@@ -1,9 +1,6 @@
 import angular from 'angular';
 
 import '@ovh-ux/manager-core';
-import '@ovh-ux/ng-at-internet';
-import '@uirouter/angularjs';
-import 'angular-translate';
 import '@ovh-ux/ng-ovh-doc-url';
 
 import { Environment } from '@ovh-ux/manager-config';
@@ -12,19 +9,10 @@ import logs from './logs';
 
 const moduleName = 'ovhManagerDbaasLogs';
 
-angular
-  .module(moduleName, [
-    'ngOvhDocUrl',
-    'ovhManagerCore',
-    'ngAtInternet',
-    'pascalprecht.translate',
-    'ui.router',
-    logs,
-  ])
-  .config(
-    /* @ngInject */ (ovhDocUrlProvider) => {
-      ovhDocUrlProvider.setUserLocale(Environment.getUserLocale());
-    },
-  );
+angular.module(moduleName, ['ngOvhDocUrl', 'ovhManagerCore', logs]).config(
+  /* @ngInject */ (ovhDocUrlProvider) => {
+    ovhDocUrlProvider.setUserLocale(Environment.getUserLocale());
+  },
+);
 
 export default moduleName;
