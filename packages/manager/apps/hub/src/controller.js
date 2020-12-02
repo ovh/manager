35fd1db185a4fec219e1683abe035d244b2e8055
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2020-12-02 14:05:58 +0100

    feat(chatbot): use feature flipping to enable chatbot
    ref: MANAGER-6021
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/hub/src/controller.js
+++ packages/manager/apps/hub/src/controller.js
@@ -4,17 +4,32 @@ import { Environment } from '@ovh-ux/manager-config';
 
 export default class HubController {
   /* @ngInject */
-  constructor($document, $scope) {
+  constructor($document, $scope, $rootScope, ovhFeatureFlipping) {
     this.$document = $document;
     this.$scope = $scope;
+    this.$rootScope = $rootScope;
     this.chatbotEnabled = false;
+    this.ovhFeatureFlipping = ovhFeatureFlipping;
   }
 
   $onInit() {
     this.currentLanguage = Environment.getUserLanguage();
     this.user = Environment.getUser();
     const unregisterListener = this.$scope.$on('app:started', () => {
-      this.chatbotEnabled = true;
+      const CHATBOT_FEATURE = 'chatbot';
+      this.ovhFeatureFlipping
+        .checkFeatureAvailability(CHATBOT_FEATURE)
+        .then((featureAvailability) => {
+          this.chatbotEnabled = featureAvailability.isFeatureAvailable(
+            CHATBOT_FEATURE,
+          );
+          if (this.chatbotEnabled) {
+            this.$rootScope.$broadcast(
+              'ovh-chatbot:enable',
+              this.chatbotEnabled,
+            );
+          }
+        });
       unregisterListener();
     });
   }
