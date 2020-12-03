869307f77a947f612506eef21721b8932dd68ae4
frenauvh <florian.renaut@corp.ovh.com>
2020-12-03 16:09:15 +0100

    feat(dbaas-logs): add universe config
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/apps/dbaas-logs/src/index.js
+++ packages/manager/apps/dbaas-logs/src/index.js
@@ -1,11 +1,13 @@
 import 'script-loader!jquery'; // eslint-disable-line
 import 'whatwg-fetch';
 import { attach as attachPreloader } from '@ovh-ux/manager-preloader';
-import { bootstrapApplication } from '@ovh-ux/manager-core';
+import { bootstrapApplication, UNIVERSES } from '@ovh-ux/manager-core';
 
 attachPreloader();
 
-bootstrapApplication().then(({ region }) => {
+bootstrapApplication({
+  universe: UNIVERSES.BARE_METAL_CLOUD,
+}).then(({ region }) => {
   import(`./config-${region}`)
     .catch(() => {})
     .then(() => import('./app.module'))
