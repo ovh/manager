f5f2cec3fa0ff9eee8bcf6dae0f1f16f2637cece
Ganesh Kumar <ganesh.kumar@ovhcloud.com>
2020-12-14 15:03:46 +0530

    fix(dedicated): expired service
    add error page for expired services along with the side-bar
    
    Fixes #MANAGER-5965
    
    Signed-off-by: Ganesh Kumar <ganesh.kumar@ovhcloud.com>

---
+++ packages/manager/apps/dedicated/client/app/expired/expired.routing.js
@@ -0,0 +1,18 @@
+import get from 'lodash/get';
+
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state('app.expired', {
+    url: '/expired',
+    params: {
+      error: null,
+    },
+    component: 'managerErrorPage',
+    resolve: {
+      cancelLink: /* @ngInject */ ($state) =>
+        $state.href('app.configuration', { reload: true }),
+      error: /* @ngInject */ ($transition$) =>
+        get($transition$.params(), 'error'),
+      translationsRefresh: /* @ngInject */ ($translate) => $translate.refresh(),
+    },
+  });
+};
