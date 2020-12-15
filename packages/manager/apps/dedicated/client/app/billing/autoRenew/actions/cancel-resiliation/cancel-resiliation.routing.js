5a3fe04fc00481decfe36d017ba36fa611b68062
JeremyDec <jeremy.de-cesare@ovhcloud.com>
2020-12-15 17:13:27 +0100

    feat(billing.autorenew): adapt existing cancel resiliation modal
    ref: MANAGER-2379
    Signed-off-by: JeremyDec <jeremy.de-cesare@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/billing/autoRenew/actions/cancel-resiliation/cancel-resiliation.routing.js
+++ packages/manager/apps/dedicated/client/app/billing/autoRenew/actions/cancel-resiliation/cancel-resiliation.routing.js
@@ -10,16 +10,44 @@ export default /* @ngInject */ ($stateProvider) => {
     translations: { value: ['.'], format: 'json' },
     resolve: {
       goBack: /* @ngInject */ (goToAutorenew) => goToAutorenew,
-      cancelResiliation: /* @ngInject */ (BillingAutoRenew) => (service) => {
+      cancelResiliation: /* @ngInject */ (
+        BillingAutoRenew,
+        engagement,
+        hasEndRuleStrategies,
+        setReactivateEngagementStrategy,
+      ) => (service) => {
+        if (engagement && hasEndRuleStrategies) {
+          return setReactivateEngagementStrategy();
+        }
+
         service.cancelResiliation();
         return BillingAutoRenew.updateService(service);
       },
+      engagement: /* @ngInject */ (Server, service) =>
+        (service.canHaveEngagement()
+          ? Server.getSelected(service.serviceId)
+          : Promise.resolve({ engagement: null })
+        ).then(({ engagement }) => engagement),
+      hasEndRuleStrategies: /* @ngInject */ (engagement, endStrategies) =>
+        engagement &&
+        engagement.endRule &&
+        engagement.endRule.possibleStrategies.includes(
+          endStrategies.REACTIVATE_ENGAGEMENT,
+        ),
       serviceId: /* @ngInject */ ($transition$) =>
         $transition$.params().serviceId,
       serviceType: /* @ngInject */ ($transition$) =>
         $transition$.params().serviceType,
       service: /* @ngInject */ (BillingAutoRenew, serviceId, serviceType) =>
         BillingAutoRenew.getService(serviceId, serviceType),
+      setReactivateEngagementStrategy: /* @ngInject */ (
+        $http,
+        endStrategies,
+        service,
+      ) => () =>
+        $http.put(`/services/${service.id}/billing/engagement/endRule`, {
+          strategy: endStrategies.REACTIVATE_ENGAGEMENT,
+        }),
       trackClickInformation: () => ({
         name: 'autorenew::cancel-resiliation',
         type: 'action',
