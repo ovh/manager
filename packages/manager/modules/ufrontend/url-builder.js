0f7def0c87db2c44c4cb97128f2407f777cfded8
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2020-12-17 09:22:23 +0100

    feat(url-builder): add params pattern replacement
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/modules/ufrontend/url-builder.js
+++ packages/manager/modules/ufrontend/url-builder.js
@@ -1,40 +1,83 @@
 import { Environment } from '@ovh-ux/manager-config';
 
-const buildQueryString = (data) =>
-  Object.keys(data)
-    .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
-    .join('&');
-
-export const buildURL = (application, url, query) => {
-  let applicationURL = Environment.getApplicationURL(application);
+const getApplicationBaseURL = (applicationName) => {
+  const applicationURL = Environment.getApplicationURL(applicationName);
   const currentApplicationURL = Environment.getApplicationURL(
     Environment.getApplicationName(),
   );
 
   if (applicationURL === currentApplicationURL) {
-    applicationURL = `${window.location.origin}${window.location.pathname}`;
+    return `${window.location.origin}${window.location.pathname}`;
   }
 
-  let queryString = query ? buildQueryString(query) : '';
+  return applicationURL;
+};
 
+const buildURLPattern = (pattern, params) => {
+  let url = pattern;
+  let filteredParams = params;
+
+  if (pattern.includes(':') && params) {
+    const PARAM_REGEXP = /:(\w+)/g;
+
+    const urlParamTemplates = [...pattern.matchAll(PARAM_REGEXP)].map(
+      ([, name]) => name,
+    );
+
+    urlParamTemplates.forEach((urlParam) => {
+      if (params[urlParam]) {
+        url = url.replace(`:${urlParam}`, encodeURIComponent(params[urlParam]));
+      }
+    });
+
+    filteredParams = Object.keys(params).reduce((queryParams, paramName) => {
+      if (!urlParamTemplates.includes(paramName)) {
+        return {
+          ...queryParams,
+          [paramName]: params[paramName],
+        };
+      }
+      return queryParams;
+    }, {});
+  }
+
+  return { url, params: filteredParams };
+};
+
+const buildQueryString = (data) =>
+  Object.keys(data)
+    .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+    .join('&');
+
+export const buildURL = (application, path, params) => {
+  const applicationURL = getApplicationBaseURL(application);
+
+  const { url: buildedPath, params: queryObject } = buildURLPattern(
+    path,
+    params,
+  );
+
+  let queryString = queryObject ? buildQueryString(queryObject) : '';
   if (queryString) {
-    queryString = url.includes('?') ? `&${queryString}` : `?${queryString}`;
+    queryString = buildedPath.includes('?')
+      ? `&${queryString}`
+      : `?${queryString}`;
   }
 
-  return `${applicationURL}${url}${queryString}`;
+  return `${applicationURL}${buildedPath}${queryString}`;
 };
 
 export const buildURLs = (routes) => {
   if (Array.isArray(routes)) {
-    return routes.map(({ application, url, query }) =>
-      buildURL(application, url, query),
+    return routes.map(({ application, path, params }) =>
+      buildURL(application, path, params),
     );
   }
   return Object.keys(routes).reduce((result, name) => {
-    const { application, url, query } = routes[name];
+    const { application, path, params } = routes[name];
     return {
       ...result,
-      [name]: buildURL(application, url, query),
+      [name]: buildURL(application, path, params),
     };
   }, {});
 };
