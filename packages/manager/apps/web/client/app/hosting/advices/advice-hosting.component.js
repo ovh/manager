f4da1f2021f2131313b9ad18686745461345fbae
Ganesh Kumar <ganesh.kumar@ovhcloud.com>
2020-12-24 13:39:51 +0530

    fix(web): hosting advices
    Fixes #MANAGER-6069
    
    Signed-off-by: Ganesh Kumar <ganesh.kumar@ovhcloud.com>

---
+++ packages/manager/apps/web/client/app/hosting/advices/advice-hosting.component.js
@@ -0,0 +1,10 @@
+import controller from './advice-hosting.controller';
+import template from './advice-hosting.html';
+
+export default {
+  bindings: {
+    hosting: '<',
+  },
+  controller,
+  template,
+};
--- packages/manager/apps/web/client/app/hosting/general-informations/general-informations.module.js
+++ packages/manager/apps/web/client/app/hosting/general-informations/general-informations.module.js
@@ -1,9 +1,10 @@
 import controller from './GENERAL_INFORMATIONS.controller';
+import hostingAdvices from '../advices';
 
 const moduleName = 'ovhManagerHostingGeneralInformations';
 
 angular
-  .module(moduleName, [])
+  .module(moduleName, [hostingAdvices])
   .controller('hostingGeneralInformationsCtrl', controller);
 
 export default moduleName;
