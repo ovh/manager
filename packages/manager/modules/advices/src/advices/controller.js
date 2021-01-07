89417b492d3261d9d848b6fecde8d6b03de71152
Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>
2021-01-07 17:33:23 +0530

    feat(advices): add impression tracking
    add impression tracking for advices
    
    ref #MANAGER-6186
    
    Signed-off-by: Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>

--- packages/manager/modules/advices/src/advices/controller.js
+++ packages/manager/modules/advices/src/advices/controller.js
@@ -11,6 +11,9 @@ export default class AdvicesCtrl {
     this.loadAdvices()
       .then((adviceGroups) => {
         this.adviceGroups = adviceGroups;
+        adviceGroups.forEach((adviceGroup) => {
+          this.trackImpressions(adviceGroup.advices);
+        });
       })
       .finally(() => {
         this.loading = false;
@@ -27,16 +30,28 @@ export default class AdvicesCtrl {
       .catch(() => []); // do not show any advices on error
   }
 
+  /**
+   * Tracks advice impression click and calls on click handler
+   * @param {Object} advice
+   */
   onClick(advice) {
-    if (advice.tag) {
-      this.atInternet.trackClick({
-        name: advice.tag,
-        type: 'action',
-      });
-    }
-
+    this.trackClickImpression(advice);
     if (typeof this.onAdviceClick === 'function') {
       this.onAdviceClick({ advice });
     }
+  }
+
+  trackImpressions(advices) {
+    advices.forEach(
+      (advice) =>
+        advice.impression && this.atInternet.trackImpression(advice.impression),
+    );
+  }
+
+  trackClickImpression(advice) {
+    return (
+      advice.impression &&
+      this.atInternet.trackClickImpression(advice.impression)
+    );
   }
 }
