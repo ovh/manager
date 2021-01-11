90f8b35e4d4f720915be06f663f8e8b6b9c18cc3
Marie JONES <marie.jones@corp.ovh.com>
2021-01-11 10:01:55 +0100

    fix(private-database): remove selectedTab usage
    ref: MANAGER-6204
    
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/web/client/app/private-database/state/state.routing.js
+++ packages/manager/apps/web/client/app/private-database/state/state.routing.js
@@ -1,10 +1,12 @@
+import controller from './private-database-state.controller';
 import template from './private-database-state.html';
 
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('app.private-database.dashboard.state', {
     url: '',
     template,
-    controller: 'PrivateDatabaseStateCtrl',
+    controller,
+    controllerAs: 'stateCtrl',
     resolve: {
       breadcrumb: () => null,
     },
