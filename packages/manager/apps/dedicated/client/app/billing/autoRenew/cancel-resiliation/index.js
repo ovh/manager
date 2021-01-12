78b8a0219e702d1cc3a106b3e83c44933d7edaaa
Marie JONES <marie.jones@corp.ovh.com>
2021-01-12 09:45:24 +0100

    fix(autorenew.cancel-resiliation): fix lazyloading
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/billing/autoRenew/cancel-resiliation/index.js
+++ packages/manager/apps/dedicated/client/app/billing/autoRenew/cancel-resiliation/index.js
@@ -8,9 +8,9 @@ const moduleName = 'ovhManagerBillingAutorenewCancelResiliationLazyLoading';
 angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
   /* @ngInject */ ($stateProvider) => {
     $stateProvider.state(
-      'app.account.billing.autorenew.service.cancel-resiliation.**',
+      'app.account.billing.autorenew.cancel-resiliation.**',
       {
-        url: '/cancel-resiliation',
+        url: '/cancel-resiliation?serviceId&serviceType',
         lazyLoad: ($transition$) => {
           const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
           return import('./cancel-resiliation.module').then((mod) =>
