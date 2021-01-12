676b98884b1f492038fd388df26d0f7f65034f66
Marie JONES <marie.jones@corp.ovh.com>
2021-01-12 14:39:50 +0100

    refactor: use env executor
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

---
+++ packages/manager/apps/cda/src/app.module.js
@@ -0,0 +1,45 @@
+import 'script-loader!jquery'; // eslint-disable-line
+import { Environment } from '@ovh-ux/manager-config';
+import angular from 'angular';
+import '@ovh-ux/manager-cda';
+import ovhManagerCore from '@ovh-ux/manager-core';
+import ngOvhCloudUniverseComponents from '@ovh-ux/ng-ovh-cloud-universe-components';
+
+import { detach as detachPreloader } from '@ovh-ux/manager-preloader';
+
+import './index.scss';
+
+Environment.setRegion(__WEBPACK_REGION__);
+
+const moduleName = 'cdaApp';
+
+angular
+  .module(moduleName, [
+    ovhManagerCore,
+    'ovhManagerCda',
+    ngOvhCloudUniverseComponents,
+  ])
+  .config(
+    /* @ngInject */ (CucConfigProvider) => {
+      CucConfigProvider.setRegion(Environment.getRegion());
+    },
+  )
+  .config(
+    /* @ngInject */ ($qProvider) => {
+      $qProvider.errorOnUnhandledRejections(false);
+    },
+  )
+  .config(
+    /* @ngInject */ ($urlRouterProvider) =>
+      $urlRouterProvider.otherwise('/cda'),
+  )
+  .run(
+    /* @ngInject */ ($transitions) => {
+      const unregisterHook = $transitions.onSuccess({}, () => {
+        detachPreloader();
+        unregisterHook();
+      });
+    },
+  );
+
+export default moduleName;
