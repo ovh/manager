78fc87d9c5722557a5ea99535d62d44d5f433276
Marie JONES <marie.jones@corp.ovh.com>
2021-01-12 14:39:50 +0100

    refactor: use env executor
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/cda/src/index.js
+++ packages/manager/apps/cda/src/index.js
@@ -1,21 +1,18 @@
 import 'script-loader!jquery'; // eslint-disable-line
+import 'whatwg-fetch';
+import { attach as attachPreloader } from '@ovh-ux/manager-preloader';
+import { bootstrapApplication } from '@ovh-ux/manager-core';
 import { Environment } from '@ovh-ux/manager-config';
-import angular from 'angular';
-import '@ovh-ux/manager-cda';
 
-import './index.scss';
+attachPreloader(Environment.getUserLanguage());
 
-Environment.setRegion(__WEBPACK_REGION__);
-
-angular
-  .module('cdaApp', ['ovhManagerCda'])
-  .config(
-    /* @ngInject */ (CucConfigProvider, coreConfigProvider) => {
-      CucConfigProvider.setRegion(coreConfigProvider.getRegion());
-    },
-  )
-  .config(
-    /* @ngInject */ ($qProvider) => {
-      $qProvider.errorOnUnhandledRejections(false);
-    },
-  );
+bootstrapApplication().then(({ region }) => {
+  import(`./config-${region}`)
+    .catch(() => {})
+    .then(() => import('./app.module'))
+    .then(({ default: application }) => {
+      angular.bootstrap(document.body, [application], {
+        strictDi: true,
+      });
+    });
+});
