a0c13073e5d7220f55a8221161377993af950706
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2021-01-18 19:38:07 +0100

    feat(pci.projects.new.config): implement support of hds option
    ref: MANAGER-6134
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/new/config/components/index.js
+++ packages/manager/modules/pci/src/projects/new/config/components/index.js
@@ -1,9 +1,10 @@
 import angular from 'angular';
 
 import banner from './banner';
+import hdsOption from '../../../../components/hds';
 
 const moduleName = 'pciProjectNewConfigComponents';
 
-angular.module(moduleName, [banner]);
+angular.module(moduleName, [banner, hdsOption]);
 
 export default moduleName;
--- packages/manager/modules/pci/src/projects/new/config/controller.js
+++ packages/manager/modules/pci/src/projects/new/config/controller.js
@@ -3,7 +3,9 @@ import template from 'lodash/template';
 
 export default class PciProjectNewConfigCtrl {
   /* @ngInject */
-  constructor(pciProjectNew) {
+  constructor($q, orderCart, pciProjectNew) {
+    this.$q = $q;
+    this.orderCart = orderCart;
     this.pciProjectNew = pciProjectNew;
   }
 
@@ -18,26 +20,81 @@ export default class PciProjectNewConfigCtrl {
     }).join(', ');
   }
 
-  /* -----  End of Helpers  ------ */
-
-  /* =============================
-  =            Events            =
-  ============================== */
-
-  onProjectConfigFormSubmit() {
-    let setDescrPromise = Promise.resolve(true);
-
+  setCartProjectItem() {
     if (
       this.model.description &&
       !this.cart.projectItem.descriptionConfiguration
     ) {
-      setDescrPromise = this.pciProjectNew.setCartProjectItemDescription(
+      return this.pciProjectNew.setCartProjectItemDescription(
         this.cart,
         this.model.description,
       );
     }
 
-    return setDescrPromise.then(() => this.goToPayment());
+    return this.$q.when();
+  }
+
+  setHdsOptionItem() {
+    const { cartId, projectItem, hdsItem } = this.cart;
+    const { hds: hdsIsChecked } = this.model;
+
+    if (this.hds.isValidSupportLevel) {
+      if (hdsIsChecked && !hdsItem) {
+        const priceMode = this.hds.option.prices.find(({ capacities }) =>
+          capacities.includes('renew'),
+        );
+        return this.orderCart
+          .addNewOptionToProject(cartId, {
+            duration: priceMode.duration,
+            itemId: projectItem.itemId, // Add HDS option to project
+            planCode: this.hds.option.planCode,
+            pricingMode: priceMode.pricingMode,
+            quantity: 1,
+          })
+          .then((option) => this.cart.addItem(option));
+      }
+      if (!hdsIsChecked && hdsItem) {
+        return this.orderCart
+          .deleteItem(cartId, hdsItem.itemId)
+          .then(() => this.cart.deleteItem(hdsItem));
+      }
+    }
+
+    return this.$q.when();
+  }
+
+  sendTracking() {
+    this.trackClick('PublicCloud::pci::projects::new_project_continue');
+    if (this.model.hds) {
+      this.trackClick('PublicCloud::pci::projects::new_project_with_HDS');
+    }
+  }
+
+  /* -----  End of Helpers  ------ */
+
+  /* =============================
+  =            Events            =
+  ============================== */
+  onProjectConfigFormSubmit() {
+    this.sendTracking();
+    return this.setCartProjectItem().then(() => this.goToPayment());
+  }
+
+  onHdsCheckboxChanged(hdsCheckbox) {
+    this.model.hds = hdsCheckbox.status;
+    this.hds.isInprogressRequest = true;
+
+    this.setHdsOptionItem()
+      .then(() => this.getSummary())
+      .then((summary) => {
+        this.summary = summary;
+      })
+      .catch(() => {
+        this.model.hds = false;
+      })
+      .finally(() => {
+        this.hds.isInprogressRequest = false;
+      });
   }
 
   /* -----  End of Events  ------ */
