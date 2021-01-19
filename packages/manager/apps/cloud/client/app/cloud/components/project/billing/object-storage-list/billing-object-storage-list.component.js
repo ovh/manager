586c696846c87cf464f9575f6f42d64d2b6b729f
frenauvh <florian.renaut@corp.ovh.com>
2021-01-19 16:20:02 +0100

    refactor(cloud): remove cloud app, redirect to dedicated app
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/apps/cloud/client/app/cloud/components/project/billing/object-storage-list/billing-object-storage-list.component.js
+++
@@ -1,8 +0,0 @@
-angular.module('managerApp').component('objectStorageList', {
-  templateUrl:
-    'app/cloud/components/project/billing/object-storage-list/billing-object-storage-list.component.html',
-  controller: 'BillingObjectStorageListComponentCtrl',
-  bindings: {
-    storages: '<',
-  },
-});
