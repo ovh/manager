9262d8582367da522783a766be7d62d45f0bde23
Florian Renaut <florian.renaut@corp.ovh.com>
2021-01-20 17:44:07 +0100

    refactor(cloud-universe-components): remove dead code (#4310)
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/modules/cloud-universe-components/src/smoothScrollHere/index.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular';
-
-import directive from './directive';
-
-const moduleName = 'cucSmoothScrollHere';
-
-angular.module(moduleName, []).directive('cucSmoothScrollHere', directive);
-
-export default moduleName;
