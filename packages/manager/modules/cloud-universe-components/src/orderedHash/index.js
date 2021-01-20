1ac7dc186de68a75779bf0153594e9fa59b90aa2
frenauvh <florian.renaut@corp.ovh.com>
2021-01-20 11:10:59 +0100

    refactor(cloud-universe-components): remove dead code
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/modules/cloud-universe-components/src/orderedHash/index.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular';
-
-import factory from './factory';
-
-const moduleName = 'cucOrderedHash';
-
-angular.module(moduleName, []).factory('CucOrderedHashFactory', factory);
-
-export default moduleName;
