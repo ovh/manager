07b93e18b94e37960f3fd398de7b6e0e1ef6d559
Florian Renaut <florian.renaut@corp.ovh.com>
2021-01-20 17:11:46 +0100

    refactor(ng-ovh-stop-event): remove unused component (#4311)
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/components/ng-ovh-stop-event/src/index.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular';
-
-import directive from './directive';
-
-const moduleName = 'ngOvhStopEvent';
-
-angular.module(moduleName, []).directive('ovhStopEvent', directive);
-
-export default moduleName;
