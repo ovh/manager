0837ad958f76fa8f2fea605349ec422dd507f7c6
Antoine Leblanc <antoine.leblanc@ovhcloud.com>
2021-01-20 17:38:12 +0100

    refactor: remove @ovh-ux/ng-ovh-jquery-ui-droppable module
    Signed-off-by: Antoine Leblanc <antoine.leblanc@ovhcloud.com>

--- packages/components/ng-ovh-jquery-ui-droppable/src/index.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular';
-
-import directive from './directive';
-
-const moduleName = 'ngOvhJqueryUiDroppable';
-
-angular.module(moduleName, []).directive('droppable', directive);
-
-export default moduleName;
