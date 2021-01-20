d161107af0cc6d2ff48978af27fdd5b74015f03d
Florian Renaut <florian.renaut@corp.ovh.com>
2021-01-20 17:44:07 +0100

    refactor(cloud-universe-components): remove dead code (#4310)
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/modules/cloud-universe-components/src/clickEnterOnKeypress/index.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular';
-
-import directive from './directive';
-
-const moduleName = 'cucClickEnterOnKeypress';
-
-angular.module(moduleName, []).directive('cucClickEnterOnKeypress', directive);
-
-export default moduleName;
