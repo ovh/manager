b387fa09cf7f9f6630ffab82758b74dd4112b8e4
Antoine Leblanc <antoine.leblanc@ovhcloud.com>
2021-01-22 08:51:57 +0100

    refactor: remove @ovh-ux/ng-ovh-slider module
    Signed-off-by: Antoine Leblanc <antoine.leblanc@ovhcloud.com>

--- packages/components/ng-ovh-slider/src/index.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular';
-
-import directive from './directive';
-
-const moduleName = 'ngOvhSlider';
-
-angular.module(moduleName, []).directive('ovhSlider', directive);
-
-export default moduleName;
