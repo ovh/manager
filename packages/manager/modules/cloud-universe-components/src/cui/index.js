91ce598177ce2500b8ce76906c9d304d93ac8aa3
Marie JONES <marie.jones@corp.ovh.com>
2021-01-29 11:55:42 +0100

    refactor: remove cui-page legacy style
    ref: MANAGER-6333
    
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/cloud-universe-components/src/cui/index.js
+++ packages/manager/modules/cloud-universe-components/src/cui/index.js
@@ -4,11 +4,10 @@ import advancedOptions from './advanced-options';
 import accordian from './accordion';
 import message from './message';
 import modal from './modal';
-import page from './page';
 
 import './grid.less';
 
 const moduleName = 'ngOvhCloudUniverseComponentsCui';
 
-angular.module(moduleName, [advancedOptions, accordian, message, modal, page]);
+angular.module(moduleName, [advancedOptions, accordian, message, modal]);
 export default moduleName;
