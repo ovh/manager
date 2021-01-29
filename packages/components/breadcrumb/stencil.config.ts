b9f3970f3daa16ac47a636b3e58228fd5fccb37d
Marie JONES <marie.jones@corp.ovh.com>
2021-01-29 10:07:47 +0100

    feat: add angularjs output target
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/components/breadcrumb/stencil.config.ts
+++ packages/components/breadcrumb/stencil.config.ts
@@ -1,15 +1,16 @@
 import { Config } from '@stencil/core';
+import { angularJSOutputTarget } from "@ovh/stencil-angularjs-output-target";
 
 export const config: Config = {
   namespace: 'breadcrumb',
   taskQueue: 'async',
   outputTargets: [
     {
-      type: 'dist-custom-elements-bundle',
-    },
-    {
       type: 'dist',
       esmLoaderPath: '../loader'
+    },
+    {
+      type: 'dist-custom-elements-bundle',
     },
     {
       type: 'docs-readme'
@@ -17,6 +18,14 @@ export const config: Config = {
     {
       type: 'www',
       serviceWorker: null // disable service workers
-    }
+    },
+    angularJSOutputTarget({
+      componentCorePackage: "@ovh-ux/manager-breadcrumb",
+      proxiesFile: "../ng-breadcrumb/src/breadcrumb.js",
+      excludeComponents: [],
+      applyPolyfills: true,
+      defineCustomElements: true,
+      wrappedComponentsPrefix: "ovh",
+    }),
   ]
 };
