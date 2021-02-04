9ed376062a77955c1cc093f161ea7d5230157374
varun257 <varun257@gmail.com>
2021-02-04 07:58:07 +0530

    feat(webpaas): rename platform sh as webpaas
    ref: MANAGER-5618
    
    Signed-off-by: varun257 <varun257@gmail.com>

--- packages/manager/modules/platform-sh/src/components/project-template/controller.js
+++
@@ -1,9 +0,0 @@
-import isFunction from 'lodash/isFunction';
-
-export default class PlatformShProjectTemplateCtrl {
-onSelected(template) {
-    if (isFunction(this.onSelect)) {
-      this.onSelect(template);
-    }
-  }
-}
