0321b1f168766efbe70f7c7923ae3d5ad0d6d267
Benjamin Garnier <benjamin.garnier@ovhcloud.com>
2021-02-10 08:58:47 +0100

    feat: new review and new correction ref: MANAGER-5654
    Signed-off-by: Benjamin Garnier <benjamin.garnier@ovhcloud.com>

--- packages/manager/modules/storage/src/netapp/netapp.controller.js
+++ packages/manager/modules/storage/src/netapp/netapp.controller.js
@@ -1,27 +1,21 @@
+import { Environment } from '@ovh-ux/manager-config';
+
 export default class NetappCtrl {
   /* @ngInject */
-  constructor(coreConfig, NetappService, CucRegionService, CORE_URLS, User) {
-    this.coreConfig = coreConfig;
-    this.NetappService = NetappService;
-    this.CucRegionService = CucRegionService;
+  constructor(CORE_URLS, User) {
     this.CORE_URLS = CORE_URLS;
     this.User = User;
   }
 
+  isStatusIn(sublist) {
+    return sublist.includes(this.service.status);
+  }
+
   initGuides() {
-    this.guidesLoading = true;
-    return this.User.getUser()
-      .then((user) => {
-        this.guideUrl =
-          this.CORE_URLS.guides.home[user.ovhSubsidiary] +
-          this.CORE_URLS.guides.cda;
-      })
-      .catch((error) => {
-        this.$log.error(error);
-      })
-      .finally(() => {
-        this.guidesLoading = false;
-      });
+    const user = Environment.getUser();
+    this.guideUrl =
+      this.CORE_URLS.guides.home[user.ovhSubsidiary] +
+      this.CORE_URLS.guides.cda;
   }
 
   $onInit() {
