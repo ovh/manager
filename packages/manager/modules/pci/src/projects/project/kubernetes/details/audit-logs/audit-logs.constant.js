cec5852e35286840f96cd106eb9bae5ae473597b
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2021-02-23 18:11:43 +0100

    feat(project.kube.details.audit-logs): add audit logs tabs
    ref: MANAGER-6385
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

---
+++ packages/manager/modules/pci/src/projects/project/kubernetes/details/audit-logs/audit-logs.constant.js
@@ -0,0 +1,15 @@
+export const LOGS_INFO = {
+  DEFAULT: 'https://docs.ovh.com/fr/logs-data-platform/kubernetes-fluent-bit/',
+};
+
+export const HTTP_STATUS = {
+  MAX_SUCCESS_CODE: 299,
+  MIN_REDIRECTION_CODE: 300,
+  MAX_ERROR_CODE: 499,
+  MIN_INTERNAL_ERROR_CODE: 500,
+};
+
+export default {
+  LOGS_INFO,
+  HTTP_STATUS,
+};
