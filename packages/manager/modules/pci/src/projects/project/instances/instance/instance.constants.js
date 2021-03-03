c96ee6d3d5b09f0649bd6a74a0ce608a5cd9dba1
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2021-03-03 18:43:47 +0100

    feat(instances.instance): add instance constants values
    ref: MANAGER-6487
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

---
+++ packages/manager/modules/pci/src/projects/project/instances/instance/instance.constants.js
@@ -0,0 +1,9 @@
+export const EDIT_PAGE_SECTIONS = {
+  IMAGES: 'imagesSection',
+  FLAVORS: 'flavorsSection',
+  BILLING_PERIOD: 'billingPeriodSection',
+};
+
+export default {
+  EDIT_PAGE_SECTIONS,
+};
