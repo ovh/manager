cd77b2bd111d7cf21c46d15d249598851c40bd52
CDS Agent <cds@ovh.net>
2021-03-04 14:55:59 +0100

    Revert "fix(ip): display pagination for parking (#4499)"
    This reverts commit 1498d27571fbe1014b4e990f06e592d46e44e812.

--- packages/manager/apps/dedicated/client/app/ip/ip/ip-ip.controller.js
+++ packages/manager/apps/dedicated/client/app/ip/ip/ip-ip.controller.js
@@ -201,7 +201,7 @@ angular
         $scope.loading.alerts = true;
         const params = {
           extras: true,
-          pageSize: 5000,
+          pageSize: 10,
         };
         const ipsPromise = ips
           ? $q.when(ips)
@@ -259,6 +259,8 @@ angular
           params.type = 'failover';
         } else if ($scope.serviceName === '_PARK') {
           params.type = 'failover';
+          params.pageSize = 10;
+          params.pageNumber = 1;
           params.serviceName = $scope.serviceName;
           $location.search('page', params.pageNumber);
           $location.search('pageSize', params.pageSize);
