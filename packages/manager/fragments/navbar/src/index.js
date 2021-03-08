dfa45c5d438df1d84f6a154dea00a9540c175970
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2021-03-08 10:28:45 +0100

    feat(environment): add environment context
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/fragments/navbar/src/index.js
+++ packages/manager/fragments/navbar/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import i18next from 'i18next';
 import registerFragment from '@ovh-ux/ufrontend/fragment';
 import { initReactI18next } from 'react-i18next';
-import { Environment } from '@ovh-ux/manager-config';
 import Navbar from './navbar.jsx';
 import fallbackTranslations from './translations/navbar_fr_FR.json';
 
-registerFragment('navbar').then(({ parent, config }) => {
-  const locale = Environment.getUserLocale();
+import { Provider as EnvironmentProvider } from './environment';
+
+registerFragment('navbar').then(({ parent, environment }) => {
+  const locale = environment.getUserLocale();
   import(`./translations/navbar_${locale}.json`)
     .catch(() => ({ default: fallbackTranslations }))
     .then(({ default: trads }) => {
@@ -28,7 +29,12 @@ registerFragment('navbar').then(({ parent, config }) => {
 
       ReactDOM.render(
         <React.StrictMode>
-          <Navbar universe={config.universe} user={config.user} />
+          <EnvironmentProvider value={environment}>
+            <Navbar
+              universe={environment.getUniverse()}
+              user={environment.getUser()}
+            />
+          </EnvironmentProvider>
         </React.StrictMode>,
         parent,
       );
