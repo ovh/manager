be202ea773c79e9d12ba03cbef98c6cbc491df2e
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2021-03-08 17:40:23 +0100

    fix(template): update app templates
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/tools/sao-ovh-manager-app/template/src/app.module.js
+++ packages/manager/tools/sao-ovh-manager-app/template/src/app.module.js
@@ -1,8 +1,7 @@
 <% const pascalcasedName = this.camelcase(name, { pascalCase: true }) -%>
-import { Environment } from '@ovh-ux/manager-config';
 import angular from 'angular';
 import uiRouter from '@uirouter/angularjs';
-import ovhManagerCore from '@ovh-ux/manager-core';
+import { registerCoreModule } from '@ovh-ux/manager-core';
 import { detach as detachPreloader } from '@ovh-ux/manager-preloader';
 import ngOvhUiRouterLineProgress from '@ovh-ux/ng-ui-router-line-progress';
 import ngUiRouterBreadcrumb from '@ovh-ux/ng-ui-router-breadcrumb';
@@ -12,54 +11,63 @@ import '@ovh-ux/ui-kit/dist/css/oui.css';
 
 import <%= pascalcasedName %> from '@ovh-ux/manager-<%= name %>';
 
-Environment.setVersion(__VERSION__);
+export default (containerEl, environment) => {
+  const moduleName = '<%= pascalcasedName %>App';
 
-const moduleName = '<%= pascalcasedName %>App';
-
-angular
-  .module(moduleName, [
-    ovhManagerCore,
-    ngOvhUiRouterLineProgress,
-    ngUiRouterBreadcrumb,
-    uiRouter,
-    errorPage,
-    <%= pascalcasedName %>
-  ])
-  .config(
-    /* @ngInject */ ($locationProvider) => $locationProvider.hashPrefix(''),
-  )
-  .config(
-    /* @ngInject */ ($urlRouterProvider) => {
-      $urlRouterProvider.otherwise('/<%= name %>');
-    },
-  )
-  .run(
-    /* @ngInject */ ($transitions) => {
-      const unregisterHook = $transitions.onSuccess({}, () => {
-        detachPreloader();
-        unregisterHook();
-      });
-    },
-  )
-  .run(
-    /* @ngInject */ ($state) => {
-      $state.defaultErrorHandler((error) => {
-        if (error.type === RejectType.ERROR) {
-          $state.go(
-            'error',
-            {
-              detail: {
-                message: get(error.detail, 'data.message'),
-                code: has(error.detail, 'headers')
-                  ? error.detail.headers('x-ovh-queryId')
-                  : null,
+  angular
+    .module(
+      moduleName,
+      [
+        registerCoreModule(environment),
+        ngOvhUiRouterLineProgress,
+        ngUiRouterBreadcrumb,
+        uiRouter,
+        errorPage,
+        <%= pascalcasedName %>,
+        ...get(__NG_APP_INJECTIONS__, environment.getRegion(), []),
+      ].filter(isString),
+    )
+    .config(
+      /* @ngInject */($locationProvider) => $locationProvider.hashPrefix(''),
+    )
+    .config(
+      /* @ngInject */($urlRouterProvider) => {
+        $urlRouterProvider.otherwise('/<%= name %>');
+      },
+    )
+    .run(
+      /* @ngInject */($rootScope, $transitions) => {
+        const unregisterHook = $transitions.onSuccess({}, () => {
+          detachPreloader();
+          $rootScope.$broadcast('app:started');
+          unregisterHook();
+        });
+      },
+    )
+    .run(
+      /* @ngInject */($state) => {
+        $state.defaultErrorHandler((error) => {
+          if (error.type === RejectType.ERROR) {
+            $state.go(
+              'error',
+              {
+                detail: {
+                  message: get(error.detail, 'data.message'),
+                  code: has(error.detail, 'headers')
+                    ? error.detail.headers('x-ovh-queryId')
+                    : null,
+                },
               },
-            },
-            { location: false }
-          )
-        }
-      });
-    },
-  )
+              { location: false }
+            )
+          }
+        });
+      },
+    );
 
-export default moduleName;
+  angular.bootstrap(containerEl, [moduleName], {
+    strictDi: true,
+  });
+
+  return moduleName;
+};
