28088eb298c50ca9b18c4adc429a501edfa2e8c2
Marie JONES <marie.jones@corp.ovh.com>
2021-03-09 09:54:57 +0100

    fix(ip): paginate ip parking (#4512)
    ref: DTRSD-27805
    
    This reverts commit 0aa614ff00e104fc8f12d44dabfb424c16a791f4.
    This reverts commit a6fc269ac403699b764d0ddaf33a7bb9ca856347.
    
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/ip/ip/ip-ip.controller.js
+++ packages/manager/apps/dedicated/client/app/ip/ip/ip-ip.controller.js
@@ -1,5 +1,4 @@
 import find from 'lodash/find';
-import filter from 'lodash/filter';
 import get from 'lodash/get';
 import map from 'lodash/map';
 import orderBy from 'lodash/orderBy';
@@ -260,10 +259,9 @@ angular
           params.type = 'failover';
         } else if ($scope.serviceName === '_PARK') {
           params.type = 'failover';
-          params.pageSize = 5000;
-          params.pageNumber = 1;
-          $location.search('page', undefined);
-          $location.search('pageSize', undefined);
+          params.serviceName = $scope.serviceName;
+          $location.search('page', params.pageNumber);
+          $location.search('pageSize', params.pageSize);
         } else if ($scope.serviceName && $scope.serviceName !== '_ALL') {
           params.serviceName = $scope.serviceName;
         }
@@ -291,12 +289,6 @@ angular
                 },
               };
             });
-            if ($scope.serviceName === '_PARK') {
-              $scope.ipsList = filter(
-                $scope.ipsList,
-                (ip) => !get(ip, 'routedTo.serviceName'),
-              );
-            }
             $scope.ipsList.forEach(checkIps);
           })
           .catch((error) => {
