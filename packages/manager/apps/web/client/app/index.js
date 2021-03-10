a1fd7adb3576fa06b74564f9987c1ae15126d9ae
Antoine Leblanc <antoine.leblanc@ovhcloud.com>
2021-03-10 16:31:16 +0100

    fix(preloader): display message in preloader
    Signed-off-by: Antoine Leblanc <antoine.leblanc@ovhcloud.com>

--- packages/manager/apps/web/client/app/index.js
+++ packages/manager/apps/web/client/app/index.js
@@ -1,12 +1,19 @@
 import 'script-loader!jquery'; // eslint-disable-line
 import 'whatwg-fetch';
-import { attach as attachPreloader } from '@ovh-ux/manager-preloader';
+import {
+  attach as attachPreloader,
+  displayMessage,
+} from '@ovh-ux/manager-preloader';
 import { bootstrapApplication } from '@ovh-ux/manager-core';
 import { Environment } from '@ovh-ux/manager-config';
 
 attachPreloader(Environment.getUserLanguage());
 
-bootstrapApplication('web').then(({ region }) => {
+bootstrapApplication('web').then(({ region, message }) => {
+  if (message) {
+    displayMessage(message, Environment.getUserLanguage());
+  }
+
   import(`./config-${region}`)
     .catch(() => {})
     .then(() => import('./app.module'))
