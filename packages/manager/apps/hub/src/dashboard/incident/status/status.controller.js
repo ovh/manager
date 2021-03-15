0a66660cd11352a2b18ffb0fdd4d82643a172dbc
Cyril Biencourt <cyril.biencourt@ovhcloud.com>
2021-03-15 16:10:03 +0100

    feat(hub): add datacenters and regions to incident services status
    Signed-off-by: Cyril Biencourt <cyril.biencourt@ovhcloud.com>

--- packages/manager/apps/hub/src/dashboard/incident/status/status.controller.js
+++ packages/manager/apps/hub/src/dashboard/incident/status/status.controller.js
@@ -1,5 +1,5 @@
 import { Environment } from '@ovh-ux/manager-config';
-import { FAQ } from './status.constants';
+import { DATACENTERS, FAQ, REGIONS } from './status.constants';
 
 export default class StatusController {
   $onInit() {
@@ -12,8 +12,31 @@ export default class StatusController {
         hideOperators: true,
         values: this.translatedServiceEnum,
       },
+      datacenters: {
+        hideOperators: true,
+        values: DATACENTERS,
+      },
+      regions: {
+        hideOperators: true,
+        values: REGIONS,
+      },
     };
 
     this.faqLink = FAQ[Environment.getUserLocale()] || FAQ.en_GB;
+
+    this.datagridParameters = [
+      {
+        name: 'region',
+        hidden:
+          this.services.length > 0 &&
+          !Object.keys(this.services[0]).includes('region'),
+      },
+      {
+        name: 'datacenter',
+        hidden:
+          this.services.length > 0 &&
+          !Object.keys(this.services[0]).includes('datacenter'),
+      },
+    ];
   }
 }
