aef7be52fc8b38008d4a5a9dc4bcfc017a46551c
lizardK <cyril.biencourt@corp.ovh.com>
2021-03-15 16:35:15 +0100

    feat(hub): add datacenters and regions to incident services status (#4550)
    ref: https://github.com/ovh/manager/pull/4550
    
    Signed-off-by: Cyril Biencourt <cyril.biencourt@ovhcloud.com>

--- packages/manager/apps/hub/src/dashboard/incident/status/status.constants.js
+++ packages/manager/apps/hub/src/dashboard/incident/status/status.constants.js
@@ -3,6 +3,28 @@ export const FAQ = {
   en_GB: 'https://help.ovhcloud.com/en-gb/faq/strasbourg-incident/',
 };
 
+export const DATACENTERS = {
+  SBG1: 'SBG1',
+  SBG2: 'SBG2',
+  SBG3: 'SBG3',
+  SBG4: 'SBG4',
+  SBG5: 'SBG5',
+  SBG6: 'SBG6',
+};
+
+export const REGIONS = {
+  'os-sbg1': 'os-sbg1',
+  'os-sbg2': 'os-sbg2',
+  'os-sbg3': 'os-sbg3',
+  'os-sbg4': 'os-sbg4',
+  'os-sbg5': 'os-sbg5',
+  'os-sbg6': 'os-sbg6',
+  'os-pcs-sbg': 'os-pcs-sbg',
+  'os-pca-sbg': 'os-pca-sbg',
+};
+
 export default {
+  DATACENTERS,
   FAQ,
+  REGIONS,
 };
