a7371465b3ad3866cba2408fc2c0dd41ac0e6a7a
JeremyDec <jeremy.de-cesare@ovhcloud.com>
2021-03-16 16:05:46 +0100

    refactor(telephony.contact): rename controller alias
    Signed-off-by: JeremyDec <jeremy.de-cesare@ovhcloud.com>

--- packages/manager/apps/telecom/src/app/telecom/telephony/fax/contact/contact.routing.js
+++ packages/manager/apps/telecom/src/app/telecom/telephony/fax/contact/contact.routing.js
@@ -7,7 +7,7 @@ export default /* @ngInject */ ($stateProvider) => {
         'faxInnerView@telecom.telephony.billingAccount.fax.dashboard': {
           templateUrl: 'app/telecom/telephony/service/contact/contact.html',
           controller: 'TelecomTelephonyServiceContactCtrl',
-          controllerAs: 'ServiceContactCtrl',
+          controllerAs: '$ctrl',
         },
       },
       resolve: {
