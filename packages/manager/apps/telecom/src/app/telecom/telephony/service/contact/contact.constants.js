aaa5f23041acb1d03fc8749bf5e8be0fa0de3ed6
JeremyDec <jeremy.de-cesare@ovhcloud.com>
2021-03-16 15:51:46 +0100

    refactor(telephony.contact): use oui components and simplify edit
    Signed-off-by: JeremyDec <jeremy.de-cesare@ovhcloud.com>

--- packages/manager/apps/telecom/src/app/telecom/telephony/service/contact/contact.constants.js
+++ packages/manager/apps/telecom/src/app/telecom/telephony/service/contact/contact.constants.js
@@ -9,6 +9,38 @@ export const DIRECTORY_INFO = {
   },
 };
 
+export const DIRECTORY_WAY_NUMBER_EXTRA_ENUM = [
+  'bis',
+  'ter',
+  'quater',
+  'quinquies',
+  'sexto',
+  'septimo',
+  'octimo',
+  'nono',
+  'A',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'S',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+
 export default {
   DIRECTORY_INFO,
+  DIRECTORY_WAY_NUMBER_EXTRA_ENUM,
 };
