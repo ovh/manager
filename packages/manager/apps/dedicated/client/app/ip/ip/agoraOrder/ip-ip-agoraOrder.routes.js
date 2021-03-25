dca5187daefca32bfc888c46f1e13fa5ee712ec8
Marie JONES <marie.jones@corp.ovh.com>
2021-03-25 18:30:24 +0100

    fix(ip.order): hide breadcrumb
    ref: MANAGER-6629
    
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/ip/ip/agoraOrder/ip-ip-agoraOrder.routes.js
+++ packages/manager/apps/dedicated/client/app/ip/ip/agoraOrder/ip-ip-agoraOrder.routes.js
@@ -16,6 +16,7 @@ export default /* @ngInject */ ($stateProvider) => {
       },
     },
     resolve: {
+      hideBreadcrumb: () => true,
       breadcrumb: () => null,
     },
     layout: 'modal',
