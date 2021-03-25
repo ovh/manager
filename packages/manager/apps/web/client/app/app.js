0c121f7132369c7535985e95e854294891da7f81
Marie JONES <marie.jones@corp.ovh.com>
2021-03-25 18:06:32 +0100

    Revert "fix: remove non needed component (#4563)"
    This reverts commit b3aa13f8b1e7f32328f992b1d0195de7473a0c15.

--- packages/manager/apps/web/client/app/app.js
+++ packages/manager/apps/web/client/app/app.js
@@ -31,6 +31,7 @@ import ngUiRouterLayout from '@ovh-ux/ng-ui-router-layout';
 import ngUiRouterLineProgress from '@ovh-ux/ng-ui-router-line-progress';
 import ovhManagerAccountSidebar from '@ovh-ux/manager-account-sidebar';
 import ovhNotificationsSidebar from '@ovh-ux/manager-notifications-sidebar';
+import ovhManagerAccountMigration from '@ovh-ux/manager-account-migration';
 import ovhManagerBanner from '@ovh-ux/manager-banner';
 import ovhManagerCatalogPrice from '@ovh-ux/manager-catalog-price';
 import ovhManagerIncidentBanner from '@ovh-ux/manager-incident-banner';
@@ -122,6 +123,7 @@ angular
       'ovh-api-services',
       ovhManagerMfaEnrollment,
       ovhManagerAtInternetConfiguration,
+      ovhManagerAccountMigration,
       ovhManagerBanner,
       ovhManagerCatalogPrice,
       ovhManagerIncidentBanner,
