8062384c73baa9f95fb08a81baef1b426ee39467
JeremyDec <jeremy.de-cesare@ovhcloud.com>
2021-03-26 18:14:28 +0100

    refactor(web.hosting): use product offers for cdn orders
    Signed-off-by: JeremyDec <jeremy.de-cesare@ovhcloud.com>

--- packages/manager/apps/web/client/app/hosting/cdn/order/hosting-cdn-order.component.js
+++ packages/manager/apps/web/client/app/hosting/cdn/order/hosting-cdn-order.component.js
@@ -1,31 +1,17 @@
-import controller from './hosting-cdn-order.controller';
 import template from './hosting-cdn-order.html';
 
 export default {
-  controller,
   template,
   bindings: {
-    autoPayWithPreferredPaymentMethod: '<',
-    isAutoPayable: '<',
-    autoPayFreeOffer: '<',
-    catalogAddons: '<',
-    catalogAddon: '<',
-    prepareCart: '<',
-    checkoutCart: '<',
     goBack: '<',
-    goBackWithError: '<',
-    isOptionFree: '<',
-    serviceName: '<',
-    serviceOption: '<',
     user: '<',
-    serviceInfo: '<',
-    cdnProperties: '<',
     hasCDN: '<',
-    isV1CDN: '<',
-    isIncludedCDN: '<',
-    isPayableCDN: '<',
-    isV2CDN: '<',
     cdnCase: '<',
-    trackClick: '<',
+
+    onError: '<',
+    onSuccess: '<',
+    pricingType: '<',
+    workflowType: '<',
+    workflowOptions: '<',
   },
 };
