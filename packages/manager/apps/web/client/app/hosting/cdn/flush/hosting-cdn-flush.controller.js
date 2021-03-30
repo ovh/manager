820b0e3f158bff9038b827d996fbe20b2ced495f
JeremyDec <jeremy.de-cesare@ovhcloud.com>
2021-03-30 17:57:18 +0200

    feat(web.hosting.cdn): move cdn actions to specific button and modal
    ref: MANAGER-6586
    Signed-off-by: JeremyDec <jeremy.de-cesare@ovhcloud.com>

--- packages/manager/apps/web/client/app/hosting/cdn/flush/hosting-cdn-flush.controller.js
+++ packages/manager/apps/web/client/app/hosting/cdn/flush/hosting-cdn-flush.controller.js
@@ -1,21 +1,14 @@
-import get from 'lodash/get';
 import { HOSTING_CDN_ORDER_CDN_VERSION_V1 } from '../order/hosting-cdn-order.constant';
 
 export default class HostingFlushCdnCtrl {
   /* @ngInject */
   constructor(
-    $scope,
-    $rootScope,
-    $stateParams,
     $translate,
     atInternet,
     Hosting,
     HostingCdnSharedService,
     Alerter,
   ) {
-    this.$scope = $scope;
-    this.$rootScope = $rootScope;
-    this.$stateParams = $stateParams;
     this.$translate = $translate;
     this.atInternet = atInternet;
     this.Hosting = Hosting;
@@ -25,10 +18,8 @@ export default class HostingFlushCdnCtrl {
 
   $onInit() {
     this.loading = false;
-    this.$scope.flushCdn = () => this.flushCdn();
     this.isV1CDN =
-      get(this.$scope.cdnProperties, 'version', '') ===
-      HOSTING_CDN_ORDER_CDN_VERSION_V1;
+      this.cdnProperties.version === HOSTING_CDN_ORDER_CDN_VERSION_V1;
 
     this.atInternet.trackPage({
       name: this.isV1CDN
@@ -38,26 +29,27 @@ export default class HostingFlushCdnCtrl {
   }
 
   flushCdn() {
+    this.loading = true;
     const flushPromise = this.isV1CDN
       ? this.flushV1CDN()
       : this.flushSharedCDN();
     return flushPromise
       .then(() => {
-        this.Alerter.success(
+        this.onFlushSuccess();
+        return this.goBack(
           this.$translate.instant('hosting_dashboard_cdn_flush_success'),
-          this.$scope.alerts.main,
         );
-        this.$rootScope.$broadcast('hosting.cdn.flush.refresh');
       })
       .catch((err) => {
-        this.Alerter.alertFromSWS(
-          this.$translate.instant('hosting_dashboard_cdn_flush_error'),
-          get(err, 'data', err),
-          this.$scope.alerts.main,
+        return this.goBack(
+          `${this.$translate.instant('hosting_dashboard_cdn_flush_error')} ${
+            err.data
+          }`,
+          'error',
         );
       })
       .finally(() => {
-        this.$scope.resetAction();
+        this.loading = false;
       });
   }
 
@@ -66,7 +58,7 @@ export default class HostingFlushCdnCtrl {
    */
   flushV1CDN() {
     this.sendTrackClick('web::hosting::cdn::empty-cache::confirm');
-    return this.Hosting.flushCdn(this.$stateParams.productId);
+    return this.Hosting.flushCdn(this.serviceName);
   }
 
   /**
@@ -74,11 +66,9 @@ export default class HostingFlushCdnCtrl {
    */
   flushSharedCDN() {
     this.sendTrackClick('web::hosting::multisites::purge-cdn::confirm');
-    const { serviceName } = this.$scope.hosting;
-    const { domain } = this.$scope.currentActionData;
     return this.HostingCdnSharedService.flushCDNDomainCache(
-      serviceName,
-      domain.domain,
+      this.serviceName,
+      this.domain,
     );
   }
 
--- packages/manager/apps/web/client/app/hosting/multisite/hosting-multisite.controller.js
+++ packages/manager/apps/web/client/app/hosting/multisite/hosting-multisite.controller.js
@@ -33,6 +33,12 @@ angular
     ) => {
       atInternet.trackPage({ name: 'web::hosting::multisites' });
 
+      $scope.goToCdnConfiguration = function goToCdnConfiguration(domain) {
+        $state.go('app.hosting.dashboard.multisite.cdnConfiguration', {
+          domain,
+        });
+      };
+
       $scope.domains = null;
       $scope.sslLinked = [];
       $scope.HOSTING = HOSTING;
@@ -173,6 +179,14 @@ angular
           });
       };
 
+      $scope.canEditCdn = function canEditCdn(domain) {
+        return (
+          domain.cdn === 'ACTIVE' &&
+          domain.status !== HOSTING_STATUS.CREATING &&
+          $scope.cdnProperties.version === 'cdn-hosting'
+        );
+      };
+
       $scope.detachDomain = (domain) => {
         sendTrackClick('web::hosting::multisites::detach-domain');
         $scope.setAction('multisite/delete/hosting-multisite-delete', domain);
@@ -233,11 +247,6 @@ angular
           domain,
           domainName: domain.domain,
         });
-      };
-
-      $scope.flushCdn = (domain) => {
-        sendTrackClick('web::hosting::multisites::purge-cdn');
-        $scope.setAction('/cdn/flush/hosting-cdn-flush', { domain });
       };
 
       $scope.$on('hostingDomain.restart.done', (response, data) => {
