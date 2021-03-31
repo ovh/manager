8656949c8ac5dc7a6f63b01ecc7707e13f8e4e94
Marie JONES <marie.jones@corp.ovh.com>
2021-03-31 09:13:35 +0200

    feat: update wording for new billing status (#4640)
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/billing/src/components/service-status/service-status.constants.js
+++ packages/manager/modules/billing/src/components/service-status/service-status.constants.js
@@ -2,6 +2,7 @@ export const SERVICE_STATES = {
   error: ['expired', 'delete_at_expiration'],
   success: ['auto', 'automatic'],
   warning: ['manual', 'manualPayment'],
+  info: ['billing_suspended'],
 };
 
 export default {
