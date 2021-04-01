427e93e095634b9abba3466831b512ec4c543f39
Marie JONES <marie.jones@corp.ovh.com>
2021-04-01 15:08:20 +0200

    fix(dedicated-cloud): fix terminate resiliation
    ref: DTRSD-33069
    
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/dedicatedCloud/details/dedicatedCloud.routing.js
+++ packages/manager/apps/dedicated/client/app/dedicatedCloud/details/dedicatedCloud.routing.js
@@ -275,10 +275,10 @@ export default /* @ngInject */ ($stateProvider, $urlServiceProvider) => {
   // like #/configuration/dedicated_cloud/pcc-123456?action=confirmcancel&token=myToken
   // make a redirect to the new url of ui route
   $urlServiceProvider.rules.when(
-    '/configuration/dedicated_cloud/:productId?action&token',
+    '/dedicated_cloud/:productId?action&token',
     (match) => {
       if (match.action === 'confirmcancel') {
-        return `/configuration/dedicated_cloud/${match.productId}/terminate-confirm?token=${match.token}`;
+        return `/dedicated_cloud/${match.productId}/terminate-confirm?token=${match.token}`;
       }
 
       return false;
