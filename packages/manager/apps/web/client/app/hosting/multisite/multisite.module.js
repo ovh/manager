7014cacff00549f67ee19c5f0e99046789508908
JeremyDec <jeremy.de-cesare@ovhcloud.com>
2021-04-09 14:29:07 +0200

    feat(web.hosting.multisite): add statistics of domain
    ref: MANAGER-6586
    Signed-off-by: JeremyDec <jeremy.de-cesare@ovhcloud.com>

--- packages/manager/apps/web/client/app/hosting/multisite/multisite.module.js
+++ packages/manager/apps/web/client/app/hosting/multisite/multisite.module.js
@@ -1,11 +1,13 @@
 import cdnConfiguration from './cdnConfiguration';
 
+import statistics from './statistics/hosting-multisite-statistics.component';
 import routing from './multisite.routing';
 
 const moduleName = 'ovhManagerHostingMultisite';
 
 angular
   .module(moduleName, [cdnConfiguration])
+  .component('hostingMultisiteStatistics', statistics)
   .config(routing)
   .run(/* @ngTranslationsInject:json ./translations */);
 
