618f5543760434bf05503d509c14cb8dc6e97d2b
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2021-04-15 18:28:47 +0200

    feat(cookie): add cookie policy service
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/modules/cookie-policy/src/index.js
+++ packages/manager/modules/cookie-policy/src/index.js
@@ -3,8 +3,17 @@ import angular from 'angular';
 import 'angular-translate';
 import '@ovh-ux/ng-at-internet';
 
+import service from './service';
+
 const moduleName = 'ovhManagerCookiePolicy';
 
-angular.module(moduleName, ['pascalprecht.translate']);
+angular
+  .module(moduleName, ['pascalprecht.translate'])
+  .service('cookiePolicy', service)
+  .run(
+    /* @ngInject */ (cookiePolicy) => {
+      cookiePolicy.checkConsent();
+    },
+  );
 
 export default moduleName;
