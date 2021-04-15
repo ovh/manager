b052d608bef616dc20ce91df71a784c651c6b4ec
varun257 <varun257@gmail.com>
2021-04-15 19:40:44 +0530

    fix(webpaas): fix webpaas plan issue
    resolves #MANAGER-5671
    
    Signed-off-by: varun257 <varun257@gmail.com>

--- packages/manager/modules/web-paas/src/web-paas.service.js
+++ packages/manager/modules/web-paas/src/web-paas.service.js
@@ -1,5 +1,6 @@
 import map from 'lodash/map';
 import set from 'lodash/set';
+import compact from 'lodash/compact';
 import Project from './project.class';
 import Plan from './plan.class';
 import { BETA_PLAN_CODE } from './web-paas.constants';
@@ -56,8 +57,10 @@ export default class WebPaasService {
       set(
         catalog,
         'plans',
-        map(catalog.plans, (plan) =>
-          plan.planCode === BETA_PLAN_CODE ? new Plan(plan) : null,
+        compact(
+          map(catalog.plans, (plan) =>
+            plan.planCode === BETA_PLAN_CODE ? new Plan(plan) : null,
+          ),
         ),
       );
       return catalog;
