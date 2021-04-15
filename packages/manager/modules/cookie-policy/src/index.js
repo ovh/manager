685ce4d6051556e73253e2a5a9543646ac1afb98
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2021-04-15 17:16:46 +0200

    feat(cookie): add cookie policy module
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
    
    BREAKING CHANGE: add cookie policy module

---
+++ packages/manager/modules/cookie-policy/src/index.js
@@ -0,0 +1,10 @@
+import angular from 'angular';
+
+import 'angular-translate';
+import '@ovh-ux/ng-at-internet';
+
+const moduleName = 'ovhManagerCookiePolicy';
+
+angular.module(moduleName, ['pascalprecht.translate']);
+
+export default moduleName;
