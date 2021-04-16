a0ca241a21183bbd4ff5fc70638cbea8721e0667
Marie JONES <marie.jones@corp.ovh.com>
2021-04-16 17:14:24 +0200

    feat(migration): update content for rise servers (#4784)
    ref: MANAGER-6707
    
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/incident-banner/src/migration/index.js
+++ packages/manager/modules/incident-banner/src/migration/index.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 
 import 'angular-translate';
 import '@ovh-ux/ng-at-internet';
+import '@ovh-ux/ng-ovh-feature-flipping';
 
 import banner from './migration-banner.component';
 
@@ -10,7 +11,11 @@ import './migration.scss';
 const moduleName = 'ovhManagerIncidentMigrationBanner';
 
 angular
-  .module(moduleName, ['ngAtInternet', 'pascalprecht.translate'])
+  .module(moduleName, [
+    'ngAtInternet',
+    'ngOvhFeatureFlipping',
+    'pascalprecht.translate',
+  ])
   .component('ovhManagerIncidentMigrationBanner', banner)
   .run(/* @ngTranslationsInject:json ./translations */);
 
