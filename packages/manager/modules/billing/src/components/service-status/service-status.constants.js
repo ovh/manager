8012ad35399a282a684e814ec4e2f9de8c64525e
Marie JONES <marie.jones@corp.ovh.com>
2021-04-21 19:23:25 +0200

    feat: add forced manual status (#4807)
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/billing/src/components/service-status/service-status.constants.js
+++ packages/manager/modules/billing/src/components/service-status/service-status.constants.js
@@ -2,7 +2,7 @@ export const SERVICE_STATES = {
   error: ['expired', 'delete_at_expiration'],
   success: ['auto', 'automatic'],
   warning: ['manual', 'manualPayment'],
-  info: ['billing_suspended'],
+  info: ['billing_suspended', 'forced_manual'],
 };
 
 export default {
