89af9903ee9e4d560b3987c1bcbd223e1104fb3c
Stephanie Moallic <stephanie.moallic@corp.ovh.com>
2021-04-27 14:29:52 +0200

    feat(modem.ports): add internal port end for port mapping
    - use constants for min, max and placeholder
    - rework on constants file for port mapping
    - use capital letters for constants
    - fix error on internal ports check
    - remove required for internal port end (which is not)
    - fix label translation for internal port end
    - use form-control to display required field

---
+++ packages/manager/apps/telecom/src/app/telecom/pack/xdsl/modem/router/ports/ports.constants.js
@@ -0,0 +1,13 @@
+export const PACK_XDSL = {
+  min: 1,
+  max: 65535,
+  maxlength: 5,
+  portExternalPortStartPlaceHolder: '80',
+  portExternalPortEndPlaceHolder: '85',
+  internalPortPlaceHolder: '8080',
+  internalPortEndPlaceHolder: '8085',
+};
+
+export default {
+  PACK_XDSL,
+};
