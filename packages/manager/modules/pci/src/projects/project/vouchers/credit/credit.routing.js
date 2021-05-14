0922534660ea28ed0b50c3cfb377f85aea951e47
mzahas <mohammed.zahaf.ext@ovhcloud.com>
2021-05-14 16:13:37 +0200

    feat(project.vouchers.credit): setup credit module
    ref: DTRSD-37421
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/vouchers/credit/credit.routing.js
+++ packages/manager/modules/pci/src/projects/project/vouchers/credit/credit.routing.js
@@ -1,20 +1,13 @@
-import controller from './credit.controller';
-import template from './credit.html';
-
-import controllerAgora from './credit-agora.controller';
-import templateAgora from './credit-agora.html';
-
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('pci.projects.project.vouchers.credit', {
-    url: '/credit',
+    url: '/credit/buy',
     layout: 'modal',
     views: {
       modal: {
-        controllerProvider: /* @ngInject */ (coreConfig) =>
-          coreConfig.isRegion('US') ? controllerAgora : controller,
-        controllerAs: '$ctrl',
-        templateProvider: /* @ngInject */ (coreConfig) =>
-          coreConfig.isRegion('US') ? templateAgora : template,
+        componentProvider: /* @ngInject */ (coreConfig) =>
+          coreConfig.isRegion('US')
+            ? 'pciProjectsProjectVouchersCreditAgora'
+            : 'pciProjectsProjectVouchersCreditLegacy',
       },
     },
     resolve: {
