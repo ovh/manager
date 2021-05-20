fea85af2df34be7502703f7e4f48a45927b668cf
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2021-05-20 16:07:57 +0200

    feat(project.notebooks.add): create notebook page
    ref: MANAGER-6886
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

---
+++ packages/manager/modules/pci/src/projects/project/notebooks/add/notebook.constants.js
@@ -0,0 +1,29 @@
+export const NOTEBOOK_NAME_REGEX = '^[a-z0-9]([-a-z0-9]*[a-z0-9])?$';
+export const NOTEBOOK_LABELS = {
+  MAX_CHARS: 15,
+  MAX_ITEMS: 10,
+};
+export const NOTEBOOK_PRIVACY_SETTINGS = {
+  RESTRICTED: 'restricted',
+  PUBLIC: 'public',
+};
+export const NOTEBOOK_RESOURCES = {
+  STANDARD: 'standard', // CPU flavors
+  ADVANCED: 'advanced', // GPU flavors
+  STANDARD_FLAVOR: 'cpu',
+  ADVANCED_FLAVOR: 'gpu',
+  NB_RESOURCES: 1,
+};
+export const NOTEBOOK_ATTACH_STORAGE = {
+  MOUNT_PATH_REGEX: '^\\/(\\S)*$',
+  PERMISSION_READ_ONLY: 'RO',
+  PERMISSION_READ_WRITE: 'RW',
+  MAX_VOLUMES: 10,
+};
+
+export default {
+  NOTEBOOK_NAME_REGEX,
+  NOTEBOOK_LABELS,
+  NOTEBOOK_PRIVACY_SETTINGS,
+  NOTEBOOK_RESOURCES,
+};
