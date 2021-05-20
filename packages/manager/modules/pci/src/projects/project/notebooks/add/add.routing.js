85ef9633b94d7ce261f313d10624e1f1c5c07df1
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2021-05-20 16:07:57 +0200

    feat(project.notebooks.add): create notebook page
    ref: MANAGER-6886
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/notebooks/add/add.routing.js
+++ packages/manager/modules/pci/src/projects/project/notebooks/add/add.routing.js
@@ -6,11 +6,41 @@ export default /* @ngInject */ ($stateProvider) => {
       breadcrumb: /* @ngInject */ ($translate) =>
         $translate.instant('pci_notebook_add_title'),
 
+      addStorageLink: /* @ngInject */ ($state, projectId) =>
+        $state.href('pci.projects.project.storages.blocks.add', {
+          projectId,
+        }),
+
       onNotebookAdd: /* @ngInject */ (goToNotebook) => (
         notebookInfo,
         message,
         type,
       ) => goToNotebook(notebookInfo, message, type),
+
+      editors: /* @ngInject */ (projectId, NotebookService) =>
+        NotebookService.getEditors(projectId),
+
+      frameworks: /* @ngInject */ (projectId, NotebookService) =>
+        NotebookService.getFrameworks(projectId).then((frameworks) => {
+          return frameworks.map((framework) => ({
+            ...framework,
+            versions: framework.versions.map((version) => ({ version })),
+          }));
+        }),
+
+      regions: /* @ngInject */ (projectId, NotebookService) =>
+        NotebookService.getRegions(projectId).then((regions) =>
+          regions.map((region) => ({
+            name: region,
+            hasEnoughQuota: () => true,
+          })),
+        ),
+
+      volumes: /* @ngInject */ (projectId, NotebookService) =>
+        NotebookService.getVolumes(projectId),
+
+      prices: /* @ngInject */ (projectId, CucPriceHelper) =>
+        CucPriceHelper.getPrices(projectId),
     },
   });
 };
