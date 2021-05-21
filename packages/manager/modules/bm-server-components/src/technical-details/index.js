18cbcb3bad05098ff57a232ba00ef22aebcf8adc
Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>
2021-05-21 14:18:21 +0530

    feat(bm-server-components): add technical details tile component
    ref: MANAGER-6935
    
    Signed-off-by: Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>

--- packages/manager/modules/bm-server-components/src/technical-details/index.js
+++ packages/manager/modules/bm-server-components/src/technical-details/index.js
@@ -3,24 +3,22 @@ import angular from 'angular';
 import '@ovh-ux/ui-kit';
 import 'angular-translate';
 import '@ovh-ux/ng-translate-async-loader';
-import '@ovh-ux/ng-at-internet';
-import ovhManagerCore from '@ovh-ux/manager-core';
+import ngAtInternet from '@ovh-ux/ng-at-internet';
+import ngOvhFeatureFlipping from '@ovh-ux/ng-ovh-feature-flipping';
 
-import Datacenter from '../datacenter';
 import component from './component';
 
-const moduleName = 'ovhManagerBmServerGeneralInfoTileComponent';
+const moduleName = 'ovhManagerBmServerTechnicalDetailsTileComponent';
 
 angular
   .module(moduleName, [
     'oui',
     'pascalprecht.translate',
     'ngTranslateAsyncLoader',
-    'ngAtInternet',
-    ovhManagerCore,
-    Datacenter,
+    ngAtInternet,
+    ngOvhFeatureFlipping,
   ])
-  .component('bmServerGeneralInfo', component)
+  .component('bmServerTechnicalDetails', component)
   .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
