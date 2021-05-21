8afcae66033d705d2d846c1a62e735c3d8f5cdf6
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2021-05-21 14:09:33 +0200

    feat(prj.notebooks.add.cmp.config): create notebook config section
    ref: MANAGER-6886
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

---
+++ packages/manager/modules/pci/src/projects/project/notebooks/add/components/notebook-configuration/notebook-configuration.controller.js
@@ -0,0 +1,71 @@
+import { NOTEBOOK_LABELS, NOTEBOOK_NAME_REGEX } from '../../notebook.constants';
+
+export default class NotebookConfigurationController {
+  static createLabel(key = null, value = null) {
+    return { key, value };
+  }
+
+  constructor() {
+    this.NOTEBOOK_NAME_REGEX = NOTEBOOK_NAME_REGEX;
+    this.NOTEBOOK_LABELS = NOTEBOOK_LABELS;
+
+    this.myLabels = [
+      NotebookConfigurationController.createLabel(),
+      NotebookConfigurationController.createLabel(),
+    ];
+  }
+
+  addLabel(label) {
+    this.notebookModel.labels.push(label);
+  }
+
+  removeLabel(label) {
+    const labelIndex = this.notebookModel.labels.findIndex(
+      ({ key, value }) => label.key === key && label.value === value,
+    );
+    this.notebookModel.labels.splice(labelIndex, labelIndex >= 0 ? 1 : 0);
+  }
+
+  labelKeyExist(label) {
+    const { labels } = this.notebookModel;
+    const foundLabel = labels.find(({ key }) => label.key === key);
+    return (
+      labels.length > 0 && foundLabel !== undefined && label !== foundLabel
+    );
+  }
+
+  maxLabelsIsReached() {
+    return this.notebookModel.labels.length >= NOTEBOOK_LABELS.MAX_ITEMS;
+  }
+
+  canAddNewLabel() {
+    return (
+      this.notebookModel.labels.length === 0 ||
+      !(this.maxLabelsIsReached() || this.notebookConfigurationForm.$invalid)
+    );
+  }
+
+  onAddLabelClick() {
+    if (!this.maxLabelsIsReached()) {
+      this.addLabel(NotebookConfigurationController.createLabel());
+    }
+  }
+
+  onKeyLabelEdit(label, labelId) {
+    if (this.labelKeyExist(label)) {
+      return this.notebookConfigurationForm[labelId].$setValidity(
+        'duplicateLabelKey',
+        false,
+      );
+    }
+
+    return this.notebookConfigurationForm[labelId].$setValidity(
+      'duplicateLabelKey',
+      true,
+    );
+  }
+
+  onRemoveLabelClick(label) {
+    this.removeLabel(label);
+  }
+}
