090a43ff01f3189ef4f47acbfb8a19cb8d6bd6f0
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2021-05-24 15:12:49 +0200

    feat(prj.notebooks.add.cmp.review): create notebook review section
    ref: MANAGER-6886
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

---
+++ packages/manager/modules/pci/src/projects/project/notebooks/add/components/notebook-review/notebook-review.component.js
@@ -0,0 +1,13 @@
+import controller from './notebook-review.controller';
+import template from './notebook-review.html';
+
+export default {
+  bindings: {
+    projectId: '<',
+    convertNotebookModel: '<',
+    displayNotebookReview: '<',
+    notebookModel: '<',
+  },
+  controller,
+  template,
+};
---
+++ packages/manager/modules/pci/src/projects/project/notebooks/add/components/notebook-review/notebook-review.controller.js
@@ -0,0 +1,42 @@
+import { NOTEBOOK_AUTOMATION_INFO } from '../../../Notebook.constants';
+
+export default class NotebookReviewController {
+  /* @ngInject */
+  constructor(coreConfig, CucRegionService) {
+    this.coreConfig = coreConfig;
+    this.CucRegionService = CucRegionService;
+  }
+
+  $onInit() {
+    this.notebookObject = this.convertNotebookModel(this.notebookModel);
+  }
+
+  getEditorInfo() {
+    const { editor } = this.notebookModel.selected;
+    return `${editor.name}`;
+  }
+
+  getFrameworkInfo() {
+    const { framework } = this.notebookModel.selected;
+    return `${framework.model.name} - ${framework.version.version}`;
+  }
+
+  getRegionInfo() {
+    const { region } = this.notebookModel.selected;
+    const regionInfo = this.CucRegionService.getRegion(region.name);
+
+    return `${regionInfo.country} - ${regionInfo.macroRegion.text} (${region.name})`;
+  }
+
+  getResourceInfo() {
+    const { resource } = this.notebookModel.selected;
+    return `${resource.flavor.id} - x${resource.flavor.resourcesPerUnit.cpu}`;
+  }
+
+  getAutomationInfoLink() {
+    return (
+      NOTEBOOK_AUTOMATION_INFO[this.coreConfig.getUser().ovhSubsidiary] ||
+      NOTEBOOK_AUTOMATION_INFO.DEFAULT
+    );
+  }
+}
