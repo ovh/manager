db7f18c32a39b6d064319ace2b255fe280423d76
Ganesh Kumar <ganesh.kumar@ovhcloud.com>
2021-05-25 18:50:00 +0530

    fix(dedicated): tracking for vps and server terminate
    Ref #MANAGER-6680
    
    Signed-off-by: Ganesh Kumar <ganesh.kumar@ovhcloud.com>

--- packages/manager/modules/vps/src/dashboard/resiliation/vps-resiliation.routing.js
+++ packages/manager/modules/vps/src/dashboard/resiliation/vps-resiliation.routing.js
@@ -36,8 +36,36 @@ export default /* @ngInject */ ($stateProvider) => {
             ({ data }) =>
               data.models['services.billing.engagement.EndStrategyEnum']?.enum,
           ),
-      onSuccess: /* @ngInject */ (goBack) => (successMessage) =>
-        goBack(successMessage),
+      onSuccess: /* @ngInject */ (atInternet, goBack) => (successMessage) => {
+        atInternet.trackClick({
+          name: 'vps::detail::dashboard::resiliation::confirm',
+          type: 'action',
+        });
+        return goBack(successMessage);
+      },
+      goBack: /* @ngInject */ ($state, atInternet, CucCloudMessage) => (
+        message = false,
+        type = 'success',
+        data,
+        options = {},
+      ) => {
+        const state = 'vps.detail.dashboard';
+        const promise = $state.go(state, data, {
+          reload: message && type === 'success',
+          ...options,
+        });
+        if (message) {
+          promise.then(() => {
+            CucCloudMessage[type]({ textHtml: message }, state);
+          });
+        } else {
+          atInternet.trackClick({
+            name: 'vps::detail::dashboard::resiliation::cancel',
+            type: 'action',
+          });
+        }
+        return promise;
+      },
       serviceId: /* @ngInject */ (serviceInfo) => serviceInfo.serviceId,
       service: /* @ngInject */ ($http, serviceId) =>
         $http
