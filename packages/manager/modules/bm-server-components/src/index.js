48fb38798a2dc8dc6d65685f2be70d14d802a927
Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>
2021-05-25 19:02:55 +0530

    feat(baremetal-server-components): add edit name component
    ref: MANAGER-6931
    
    Signed-off-by: Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>

--- packages/manager/modules/bm-server-components/src/index.js
+++ packages/manager/modules/bm-server-components/src/index.js
@@ -1,15 +1,18 @@
 import bmServerGeneralInfoTile from './general-info-tile';
 import bmServerTechnicalDetailsTile from './technical-details';
 import bmServerNetworkTile from './network';
+import bmServerEditName from './edit-name';
 
 export {
   bmServerGeneralInfoTile,
   bmServerTechnicalDetailsTile,
   bmServerNetworkTile,
+  bmServerEditName,
 };
 
 export default {
   bmServerGeneralInfoTile,
   bmServerTechnicalDetailsTile,
   bmServerNetworkTile,
+  bmServerEditName,
 };
