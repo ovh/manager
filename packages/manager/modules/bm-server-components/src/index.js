05822da94baec00579da816b3a9536b0aa77abbe
Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>
2021-05-26 18:21:45 +0530

    feat(baremetal-server-components): add network tile component
    ref: MANAGER-6934
    
    Signed-off-by: Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>

--- packages/manager/modules/bm-server-components/src/index.js
+++ packages/manager/modules/bm-server-components/src/index.js
@@ -1,9 +1,15 @@
 import bmServerGeneralInfoTile from './general-info-tile';
 import bmServerTechnicalDetailsTile from './technical-details';
+import bmServerNetworkTile from './network';
 
-export { bmServerGeneralInfoTile, bmServerTechnicalDetailsTile };
+export {
+  bmServerGeneralInfoTile,
+  bmServerTechnicalDetailsTile,
+  bmServerNetworkTile,
+};
 
 export default {
   bmServerGeneralInfoTile,
   bmServerTechnicalDetailsTile,
+  bmServerNetworkTile,
 };
