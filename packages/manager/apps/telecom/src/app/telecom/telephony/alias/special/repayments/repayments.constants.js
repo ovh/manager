89cb9b31650b7dbcdd4cca239e5f4f60b4968494
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2021-05-26 14:17:12 +0200

    fix(repayments): update repayments process and display
    ref: MANAGER-6392
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/telecom/src/app/telecom/telephony/alias/special/repayments/repayments.constants.js
+++ packages/manager/apps/telecom/src/app/telecom/telephony/alias/special/repayments/repayments.constants.js
@@ -3,21 +3,6 @@ export const CALLED_FEES_PREFIX = {
   be: ['0032800'],
 };
 
-export const GROUP_REPAYMENTS_PREFIX = {
-  fr: ['0033806', '003381', '003382', '003389'],
-  be: [
-    '003278',
-    '003270',
-    '0032900',
-    '0032902',
-    '0032903',
-    '0032904',
-    '0032905',
-    '0032906',
-    '0032907',
-  ],
-};
-
 export const SPECIAL_NUMBER_PREFIX = {
   france: ['00338'],
   belgium: ['0032800', '003278', '0032900', '003270'],
@@ -27,7 +12,6 @@ export const LIMIT = 500;
 
 export default {
   CALLED_FEES_PREFIX,
-  GROUP_REPAYMENTS_PREFIX,
   SPECIAL_NUMBER_PREFIX,
   LIMIT,
 };
