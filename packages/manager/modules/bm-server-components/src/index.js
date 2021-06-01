1a039307bc3664683284d8fa91abcbcf78b2e14a
Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>
2021-06-01 12:27:11 +0530

    feat(bm-server-components): add ipmi component
    ref: MANAGER-6937
    
    Signed-off-by: Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>

--- packages/manager/modules/bm-server-components/src/index.js
+++ packages/manager/modules/bm-server-components/src/index.js
@@ -1,9 +1,24 @@
 import bmServerGeneralInfoTile from './general-info-tile';
 import bmServerTechnicalDetailsTile from './technical-details';
+import bmServerIpmi from './ipmi';
+import bmServerIpmiRestart from './ipmi/restart';
+import bmServerIpmiOrderKvm from './ipmi/order-kvm';
+import pollingService from './polling/polling.service';
 
-export { bmServerGeneralInfoTile, bmServerTechnicalDetailsTile };
+export {
+  bmServerGeneralInfoTile,
+  bmServerTechnicalDetailsTile,
+  bmServerIpmi,
+  bmServerIpmiRestart,
+  bmServerIpmiOrderKvm,
+  pollingService,
+};
 
 export default {
   bmServerGeneralInfoTile,
   bmServerTechnicalDetailsTile,
+  bmServerIpmi,
+  bmServerIpmiRestart,
+  bmServerIpmiOrderKvm,
+  pollingService,
 };
---
+++ packages/manager/modules/bm-server-components/src/ipmi/restart/index.js
@@ -0,0 +1,28 @@
+import angular from 'angular';
+
+import '@ovh-ux/ui-kit';
+import 'angular-translate';
+import '@ovh-ux/ng-translate-async-loader';
+import '@ovh-ux/ng-at-internet';
+import ovhManagerCore from '@ovh-ux/manager-core';
+import ngOvhUtils from '@ovh-ux/ng-ovh-utils';
+
+import component from './component';
+import service from '../service';
+
+const moduleName = 'ovhManagerBmServerIpmiRestartComponent';
+
+angular
+  .module(moduleName, [
+    'oui',
+    'pascalprecht.translate',
+    'ngTranslateAsyncLoader',
+    'ngAtInternet',
+    ovhManagerCore,
+    ngOvhUtils,
+  ])
+  .component('bmServerIpmiRestart', component)
+  .service('IpmiService', service)
+  .run(/* @ngTranslationsInject:json ./translations */);
+
+export default moduleName;
