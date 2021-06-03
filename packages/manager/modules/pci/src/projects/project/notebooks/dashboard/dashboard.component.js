eda347d8a9a58805bb1d52ffd0d4de1c8e747d93
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2021-06-03 18:17:07 +0200

    feat(notebooks.dashboard): setup attach_data tab
    ref: MANAGER-6890
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/notebooks/dashboard/dashboard.component.js
+++ packages/manager/modules/pci/src/projects/project/notebooks/dashboard/dashboard.component.js
@@ -2,10 +2,12 @@ import template from './dashboard.html';
 
 export default {
   bindings: {
-    currentActiveLink: '<',
     notebook: '<',
-    generalInformationLink: '<',
     guideUrl: '<',
+    currentActiveLink: '<',
+    generalInformationLink: '<',
+    attachDataLink: '<',
+    userAndRolesLink: '<',
   },
   template,
 };
