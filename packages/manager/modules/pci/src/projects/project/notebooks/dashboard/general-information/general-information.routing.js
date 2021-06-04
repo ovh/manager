03d9a21407d52c83e118cceecd28105173ecaccb
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2021-06-04 20:38:53 +0200

    feat(project.notebooks.dashboard.gi): complete general_info tab
    ref: MANAGER-6889
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/notebooks/dashboard/general-information/general-information.routing.js
+++ packages/manager/modules/pci/src/projects/project/notebooks/dashboard/general-information/general-information.routing.js
@@ -8,8 +8,58 @@ export default /* @ngInject */ ($stateProvider) => {
     },
     resolve: {
       breadcrumb: () => false,
+
       goBack: /* @ngInject */ (notebook, goToNotebook) => (message, type) =>
         goToNotebook(notebook, message, type),
+
+      flavors: /* @ngInject */ (projectId, notebook, NotebookService) =>
+        NotebookService.getFlavors(projectId, notebook.region),
+
+      goToAddTag: /* @ngInject */ ($state, projectId, notebook) => () => {
+        return $state.go(
+          'pci.projects.project.notebooks.dashboard.general-information.tags-add',
+          {
+            projectId,
+            notebook,
+          },
+        );
+      },
+
+      goToAttachData: /* @ngInject */ ($state, projectId, notebook) => () =>
+        $state.go('pci.projects.project.notebooks.dashboard.attach-data', {
+          projectId,
+          notebookId: notebook.id,
+        }),
+
+      goToDeleteNotebook: /* @ngInject */ ($state, projectId, notebook) => () =>
+        $state.go(
+          'pci.projects.project.notebooks.dashboard.general-information.delete-notebook',
+          {
+            projectId,
+            notebookId: notebook.id,
+          },
+        ),
+
+      goToStartNotebook: /* @ngInject */ ($state, projectId, notebook) => () =>
+        $state.go(
+          'pci.projects.project.notebooks.dashboard.general-information.start-notebook',
+          {
+            projectId,
+            notebookId: notebook.id,
+          },
+        ),
+
+      goToStopNotebook: /* @ngInject */ ($state, projectId, notebook) => () =>
+        $state.go(
+          'pci.projects.project.notebooks.dashboard.general-information.stop-notebook',
+          {
+            projectId,
+            notebookId: notebook.id,
+          },
+        ),
+
+      openLiveCodeEditor: /* @ngInject */ ($window, notebook) => () =>
+        $window.open(notebook.status.url, '_blank'),
     },
   });
 };
