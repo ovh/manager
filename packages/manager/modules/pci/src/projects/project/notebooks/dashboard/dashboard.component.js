2027b361b04e0718c649c9e3329614ca3ad348d3
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2021-06-04 20:36:03 +0200

    feat(project.notebooks.dashboard): setup general_info tab
    ref: MANAGER-6889
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/notebooks/dashboard/dashboard.component.js
+++ packages/manager/modules/pci/src/projects/project/notebooks/dashboard/dashboard.component.js
@@ -1,13 +1,19 @@
+import controller from './dashboard.controller';
 import template from './dashboard.html';
 
 export default {
   bindings: {
     notebook: '<',
     guideUrl: '<',
+    reloadState: '<',
+    needRefresh: '<',
+    killTasks: '<',
+    waitNotebookToStartOrStop: '<',
     currentActiveLink: '<',
     generalInformationLink: '<',
     attachDataLink: '<',
     userAndRolesLink: '<',
   },
+  controller,
   template,
 };
