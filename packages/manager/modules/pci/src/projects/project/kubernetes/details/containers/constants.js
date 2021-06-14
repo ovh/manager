7d03e9ac87b334218ffe565a9aa33e20b6564798
radireddy <adireddyravindra@gmail.com>
2021-06-14 18:53:54 +0530

    feat: enable kubernetes in US (#4965)
    ref: MANAGER-6599
    
    Signed-off-by: Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/kubernetes/details/containers/constants.js
+++ packages/manager/modules/pci/src/projects/project/kubernetes/details/containers/constants.js
@@ -1,5 +1,8 @@
-export const DASHBOARD_URL =
-  'https://labs.ovh.com/kubernetes-k8s/documentation/dashboard-installation';
+export const DASHBOARD_URL = {
+  DEFAULT:
+    'https://labs.ovh.com/kubernetes-k8s/documentation/dashboard-installation',
+  US: 'https://support.us.ovhcloud.com/hc/en-us/articles/1500004918182',
+};
 
 export default {
   DASHBOARD_URL,
