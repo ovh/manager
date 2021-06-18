27175d3503cae604da2e9accc74d16e6d01c726a
Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
2021-06-18 15:52:56 +0200

    feat(hub): fetch notifications in carousel
    ref: MANAGER-6678
    
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
    
    BREAKING CHANGE : Carousel fetches notifications by default if items is undefined

--- packages/manager/modules/hub/src/components/carousel/carousel.controller.js
+++ packages/manager/modules/hub/src/components/carousel/carousel.controller.js
@@ -1,8 +1,25 @@
 export default class {
   /* @ngInject */
-  constructor(atInternet) {
+  constructor(atInternet, $http, $q, $translate) {
     this.atInternet = atInternet;
     this.index = 0;
+    this.$http = $http;
+    this.$q = $q;
+    this.$translate = $translate;
+  }
+
+  $onInit() {
+    if (!this.items) {
+      this.$http
+        .get('/hub/notifications', {
+          serviceType: 'aapi',
+        })
+        .then((data) => {
+          this.items = this.filterNotifications(
+            data.data.data.notifications.data,
+          );
+        });
+    }
   }
 
   nextAlert() {
@@ -16,6 +33,27 @@ export default class {
       name: `${this.trackingPrefix}::alert::action`,
       type: 'action',
     });
+  }
+
+  filterNotifications(notificationsData, productType) {
+    const mappedNotifications = map(
+      filter(notificationsData, (notification) => {
+        return ['warning', 'error'].includes(notification.level);
+      }),
+      (notification) => ({
+        ...notification,
+        // force sanitization to null as this causes issues with UTF-8 characters
+        description: this.$translate.instant(
+          'manager_hub_notification_warning',
+          { content: notification.description },
+          undefined,
+          false,
+          null,
+        ),
+      }),
+    );
+
+    return mappedNotifications.filter(({ type }) => type === productType);
   }
 
   switchToAlert(index) {
