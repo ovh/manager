f70395dfbf9b58022b839341d94ba3f9dc5dc5aa
Marie JONES <marie.jones@corp.ovh.com>
2021-06-24 09:40:53 +0200

    fix(account.contact.update): add lazyloading to contact update (#5164)
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/account/contacts/update/user-contacts-update.module.js
+++ packages/manager/apps/dedicated/client/app/account/contacts/update/user-contacts-update.module.js
@@ -1,11 +1,13 @@
 import angular from 'angular';
 
+import service from '../user-contacts.service';
 import routing from './user.contacts-update.routes';
 
 const moduleName = 'UserAccountContactsUpdate';
 
 angular
   .module(moduleName, ['oui', 'pascalprecht.translate', 'ui.router'])
+  .service('UserAccountContactUpdateService', service)
   .config(routing)
   .run(/* @ngTranslationsInject:json ./translations */);
 
