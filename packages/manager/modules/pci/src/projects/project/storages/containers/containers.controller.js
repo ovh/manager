fb2d0204583ba0cd9b5faddf8bc45fcfb7af4d52
jjuret <julien.juret.ext@ovhcloud.com>
2021-06-25 14:50:39 +0200

    feat(dashboard): update list for new options
    Signed-off-by: jjuret <julien.juret.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/containers/containers.controller.js
+++ packages/manager/modules/pci/src/projects/project/storages/containers/containers.controller.js
@@ -1,12 +1,42 @@
 export default class PciStoragesContainersController {
   /* @ngInject */
-  constructor($translate, CucCloudMessage) {
+  constructor($translate, $http, CucCloudMessage) {
     this.$translate = $translate;
+    this.$http = $http;
     this.CucCloudMessage = CucCloudMessage;
+    this.publicToggleLoading = false;
   }
 
   $onInit() {
     this.loadMessages();
+  }
+
+  onPublicToggle(data) {
+    this.publicToggleLoading = true;
+    return this.$http
+      .put(`/cloud/project/${this.projectId}/storage/${data.id}`, {
+        containerType: data.public ? 'private' : 'public',
+      })
+      .then(() =>
+        this.CucCloudMessage.success(
+          this.$translate.instant(
+            data.public
+              ? 'pci_projects_project_storages_containers_toggle_private_succeed'
+              : 'pci_projects_project_storages_containers_toggle_public_succeed',
+            { name: data.name },
+          ),
+          'pci.projects.project.storages.containers.container',
+        ),
+      )
+      .catch(() =>
+        this.CucCloudMessage.error(
+          'yay',
+          'pci.projects.project.storages.containers.container',
+        ),
+      )
+      .finally(() => {
+        this.publicToggleLoading = false;
+      });
   }
 
   loadMessages() {
