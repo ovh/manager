7aa43b97637b324b1c69429054e5cf02732a4dea
JeremyDec <jeremy.de-cesare@ovhcloud.com>
2021-07-05 15:22:50 +0200

    feat(netapp): initialize module
    ref: MANAGER-7302
    Signed-off-by: JeremyDec <jeremy.de-cesare@ovhcloud.com>
    
    BREAKING CHANGE: init @ovh-ux/manager-netapp module

---
+++ packages/manager/modules/netapp/src/module.js
@@ -0,0 +1,27 @@
+import angular from 'angular';
+
+import '@ovh-ux/manager-core';
+import '@uirouter/angularjs';
+import 'angular-translate';
+
+import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
+
+import dashboard from './dashboard';
+import onboarding from './onboarding';
+import routing from './routing';
+
+const moduleName = 'ovhManagerNetApp';
+
+angular
+  .module(moduleName, [
+    'ovhManagerCore',
+    'pascalprecht.translate',
+    'ui.router',
+    onboarding,
+    dashboard,
+    ListLayoutHelper.moduleName,
+  ])
+  .config(routing)
+  .run(/* @ngTranslationsInject:json ./translations */);
+
+export default moduleName;
