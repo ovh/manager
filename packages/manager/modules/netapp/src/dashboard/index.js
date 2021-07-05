105717172a9019e4206fdca3b44caedd01998534
JeremyDec <jeremy.de-cesare@ovhcloud.com>
2021-07-05 15:22:50 +0200

    feat(netapp): initialize module
    ref: MANAGER-7302
    Signed-off-by: JeremyDec <jeremy.de-cesare@ovhcloud.com>
    
    BREAKING CHANGE: init @ovh-ux/manager-netapp module

---
+++ packages/manager/modules/netapp/src/dashboard/index.js
@@ -0,0 +1,22 @@
+import angular from 'angular';
+import '@uirouter/angularjs';
+import 'oclazyload';
+
+const moduleName = 'ovhManagerNetAppDashboardLazyLoading';
+
+angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
+  /* @ngInject */ ($stateProvider) => {
+    $stateProvider.state('netapp.dashboard.**', {
+      url: '/:serviceName',
+      lazyLoad: ($transition$) => {
+        const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+
+        return import('./module').then((mod) =>
+          $ocLazyLoad.inject(mod.default || mod),
+        );
+      },
+    });
+  },
+);
+
+export default moduleName;
