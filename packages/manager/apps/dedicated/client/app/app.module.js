12a3b106217878fa71d457da9e269366b6ed4e68
JeremyDec <jeremy.de-cesare@ovhcloud.com>
2021-07-05 15:49:16 +0200

    feat(dedicated): add netapp module
    ref: MANAGER-7302
    Signed-off-by: JeremyDec <jeremy.de-cesare@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/app.module.js
+++ packages/manager/apps/dedicated/client/app/app.module.js
@@ -91,6 +91,7 @@ import ovhManagerDbaasLogs from '@ovh-ux/manager-dbaas-logs';
 import ovhManagerMfaEnrollment from '@ovh-ux/mfa-enrollment';
 import ovhManagerNasha from '@ovh-ux/manager-nasha';
 import ovhManagerNavbar from '@ovh-ux/manager-navbar';
+import ovhManagerNetapp from '@ovh-ux/manager-netapp';
 import ovhManagerServerSidebar from '@ovh-ux/manager-server-sidebar';
 import ovhManagerSupport from '@ovh-ux/manager-support';
 import ovhPaymentMethod from '@ovh-ux/ng-ovh-payment-method';
@@ -211,6 +212,7 @@ export default (containerEl, environment) => {
         ovhManagerMetrics,
         ovhManagerNasha,
         ovhManagerNavbar,
+        ovhManagerNetapp,
         ovhManagerVps,
         ovhManagerVrack,
         ovhManagerCloudConnect,
