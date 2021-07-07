c5cc12a2ccd7d0517286a471fcb6de4a93c53554
Marie JONES <marie.jones@corp.ovh.com>
2021-07-07 09:22:20 +0200

    fix(template.listing): update lazyloading template
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/tools/sao-ovh-manager-module/template/src/module.js
+++ packages/manager/tools/sao-ovh-manager-module/template/src/module.js
@@ -23,6 +23,11 @@ angular
     ListLayoutHelper.moduleName,
   ])
   .config(routing)
-  .run(/* @ngTranslationsInject:json ./translations */);
+  .run(/* @ngTranslationsInject:json ./translations */)
+  .run(
+    /* @ngInject */ ($translate, $transitions) => {
+      $transitions.onBefore({ to: 'app.**' }, () => $translate.refresh());
+    },
+  );
 
 export default moduleName;
