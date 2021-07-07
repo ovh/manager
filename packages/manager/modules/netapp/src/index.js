9aec1c11ac07f8bcf3c4d5788eb4fd0881de7536
Marie JONES <marie.jones@corp.ovh.com>
2021-07-07 09:20:52 +0200

    fix: update lazyloading and state order
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/netapp/src/index.js
+++ packages/manager/modules/netapp/src/index.js
@@ -2,23 +2,12 @@ import angular from 'angular';
 import '@uirouter/angularjs';
 import 'oclazyload';
 
-import template from './template.html';
-
 const moduleName = 'ovhManagerNetAppLazyLoading';
 
 angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
   /* @ngInject */ ($stateProvider) => {
-    $stateProvider.state('netapp', {
+    $stateProvider.state('netapp.**', {
       url: '/netapp',
-      template,
-      redirectTo: 'netapp.index',
-      resolve: {
-        breadcrumb: /* @ngInject */ ($translate) =>
-          $translate.instant('netapp_title'),
-      },
-    });
-    $stateProvider.state('netapp.index.**', {
-      url: '',
       lazyLoad: ($transition$) => {
         const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
 
