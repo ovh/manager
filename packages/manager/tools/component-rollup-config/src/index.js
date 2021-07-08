b9f1835ef4ead5a032c89d7927bdb08a02a0b889
Jisay <1774679+Jisay@users.noreply.github.com>
2021-07-08 11:03:44 +0200

    fix(deps): upgrade `rollup` to `v2.50.6`
    Bump the version of rollup in order to use last features such as private class instance methods and accessors or optional chaining.
    
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

--- packages/manager/tools/component-rollup-config/src/index.js
+++ packages/manager/tools/component-rollup-config/src/index.js
@@ -97,9 +97,6 @@ const generateConfig = (opts, pluginsOpts) =>
           babelrc: false,
           exclude: 'node_modules/**',
           plugins: [
-            '@babel/plugin-proposal-class-properties',
-            '@babel/plugin-proposal-optional-chaining',
-            '@babel/plugin-proposal-private-methods',
             '@babel/plugin-syntax-dynamic-import',
             'babel-plugin-angularjs-annotate',
           ],
