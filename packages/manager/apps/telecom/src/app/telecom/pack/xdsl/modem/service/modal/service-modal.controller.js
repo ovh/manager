fecf969f826744441d98912743787dc3387a5635
Stephanie Moallic <stephanie.moallic@corp.ovh.com>
2021-07-19 10:14:06 +0200

    fix(xdsl.service.modal): fix modal service for content sharing ref: UXCT-414
    Signed-off-by: Stephanie Moallic <stephanie.moallic@corp.ovh.com>

--- packages/manager/apps/telecom/src/app/telecom/pack/xdsl/modem/service/modal/service-modal.controller.js
+++ packages/manager/apps/telecom/src/app/telecom/pack/xdsl/modem/service/modal/service-modal.controller.js
@@ -1,17 +1,14 @@
-angular.module('managerApp').controller(
-  'XdslModemServiceModalCtrl',
-  class XdslModemServiceModalCtrl {
-    /* @ngInject */
-    constructor($uibModalInstance) {
-      this.$uibModalInstance = $uibModalInstance;
-    }
+export default class XdslModemServiceModalCtrl {
+  /* @ngInject */
+  constructor($uibModalInstance) {
+    this.$uibModalInstance = $uibModalInstance;
+  }
 
-    cancel() {
-      this.$uibModalInstance.close('cancel');
-    }
+  cancel() {
+    this.$uibModalInstance.close('cancel');
+  }
 
-    close() {
-      this.$uibModalInstance.close('confirm');
-    }
-  },
-);
+  close() {
+    this.$uibModalInstance.close('confirm');
+  }
+}
