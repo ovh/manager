5349c294157ae97eae8eee6ab11cf444a98f2ab2
varun257 <varun257@gmail.com>
2021-07-20 11:21:04 +0530

    Feat/add user popup (#5217)
    * feat(dashboard): update list for new options
    
    redirect to users and roles if there are no users
    
    Signed-off-by: varun257 <varun257@gmail.com>
    
    resolves #MANAGER-7047

--- packages/manager/modules/pci/src/projects/project/storages/containers/containers.controller.js
+++ packages/manager/modules/pci/src/projects/project/storages/containers/containers.controller.js
@@ -47,6 +47,39 @@ export default class PciStoragesContainersController {
       });
   }
 
+  onPublicToggle(data) {
+    this.publicToggleLoading = true;
+    return this.$http
+      .put(`/cloud/project/${this.projectId}/storage/${data.id}`, {
+        containerType: data.public ? 'private' : 'public',
+      })
+      .then(() =>
+        this.CucCloudMessage.success(
+          this.$translate.instant(
+            data.public
+              ? 'pci_projects_project_storages_containers_toggle_private_succeed'
+              : 'pci_projects_project_storages_containers_toggle_public_succeed',
+            { name: data.name },
+          ),
+          'pci.projects.project.storages.containers.container',
+        ),
+      )
+      .catch((err) =>
+        this.CucCloudMessage.error(
+          this.$translate.instant(
+            'pci_projects_project_storages_containers_toggle_fail',
+            {
+              message: err.message || err.data?.message,
+            },
+          ),
+          'pci.projects.project.storages.containers.container',
+        ),
+      )
+      .finally(() => {
+        this.publicToggleLoading = false;
+      });
+  }
+
   loadMessages() {
     this.messageHandler = this.CucCloudMessage.subscribe(
       'pci.projects.project.storages.containers',
