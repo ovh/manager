a4ada104a766ffe1d76364b00eb377a750f2beeb
Marie JONES <marie.jones@corp.ovh.com>
2021-07-20 14:52:13 +0200

    feat: add engagement component
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

---
+++ packages/manager/modules/billing-components/src/components/commitment-pricing-mode/commitment-pricing-mode.component.js
@@ -0,0 +1,12 @@
+import controller from './commitment-pricing-mode.controller';
+import template from './commitment-pricing-mode.html';
+
+export default {
+  bindings: {
+    pricingMode: '=',
+    pricingModes: '<',
+    fromCatalog: '<',
+  },
+  controller,
+  template,
+};
