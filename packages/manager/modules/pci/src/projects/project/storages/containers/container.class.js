8eb7b26018f5557f659cc66589939841a268c366
varun257 <varun257@gmail.com>
2021-07-21 10:05:14 +0530

    feat(dashboard): update list for new options (#5227)
    Signed-off-by: varun257 <varun257@gmail.com>

--- packages/manager/modules/pci/src/projects/project/storages/containers/container.class.js
+++ packages/manager/modules/pci/src/projects/project/storages/containers/container.class.js
@@ -1,9 +1,43 @@
 import find from 'lodash/find';
+import { OPENIO_DEFAULT_REGION } from './containers.constants';
 
 export default class Container {
-  constructor(resource) {
-    this.publicUrl = null;
-    Object.assign(this, resource);
+  constructor({
+    name,
+    objects,
+    objectsCount,
+    objectsSize,
+    archive,
+    containerType,
+    cors,
+    id,
+    region,
+    staticUrl,
+    storedBytes,
+    storedObjects,
+    isHighPerfStorage,
+    publicUrl,
+    state,
+  }) {
+    Object.assign(this, {
+      name,
+      objects,
+      objectsCount,
+      objectsSize,
+      archive,
+      containerType,
+      cors,
+      id,
+      region,
+      staticUrl,
+      storedBytes,
+      storedObjects,
+      isHighPerfStorage,
+      publicUrl,
+      state,
+    });
+    this.id = this.id || this.name;
+    this.region = this.region || OPENIO_DEFAULT_REGION;
   }
 
   getObjectById(objectId) {
