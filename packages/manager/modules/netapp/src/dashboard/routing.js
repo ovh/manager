85cb02153a7f5b0c2c05502551e3ba2b0b8813d6
Marie JONES <marie.jones@corp.ovh.com>
2021-07-29 11:05:15 +0200

    feat: add dashboard
    ref: MANAGER-7146
    
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/netapp/src/dashboard/routing.js
+++ packages/manager/modules/netapp/src/dashboard/routing.js
@@ -1,14 +1,34 @@
+import NetApp from './Netapp.class';
+
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('netapp.dashboard', {
     url: '/:serviceName',
-    component: 'ovhManagerNetAppDashboard',
+    views: {
+      netappContainer: {
+        component: 'ovhManagerNetAppDashboard',
+      },
+    },
     resolve: {
       currentActiveLink: /* @ngInject */ ($transition$, $state) => () =>
         $state.href($state.current.name, $transition$.params()),
-      dashboardLink: /* @ngInject */ ($state) =>
-        $state.href('netapp.dashboard'),
+      dashboardLink: /* @ngInject */ ($state, $transition$) =>
+        $state.href('netapp.dashboard', $transition$.params()),
+      createVolumeLink: /* @ngInject */ ($state, $transition$) =>
+        $state.href('netapp.dashboard.volumes.create', $transition$.params()),
       serviceName: /* @ngInject */ ($transition$) =>
         $transition$.params().serviceName,
+      storage: /* @ngInject */ ($http, serviceName) =>
+        $http
+          .get(`/storage/netapp/${serviceName}`)
+          .then(({ data }) => data)
+          .then((storage) => new NetApp(storage)),
+      isCommitmentAvailable: /* @ngInject */ (ovhFeatureFlipping) =>
+        ovhFeatureFlipping
+          .checkFeatureAvailability(['billing:commitment'])
+          .then((commitmentAvailability) =>
+            commitmentAvailability.isFeatureAvailable('billing:commitment'),
+          )
+          .catch(() => false),
       breadcrumb: /* @ngInject */ (serviceName) => serviceName,
     },
   });
