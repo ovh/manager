a51ef2498a47475e135f119692a691837efa21c7
JeremyDec <jeremy.de-cesare@ovhcloud.com>
2021-07-30 08:58:27 +0200

    feat(netapp.module): update module deps
    Signed-off-by: JeremyDec <jeremy.de-cesare@ovhcloud.com>

--- packages/manager/modules/netapp/src/module.js
+++ packages/manager/modules/netapp/src/module.js
@@ -3,6 +3,8 @@ import angular from 'angular';
 import '@ovh-ux/manager-core';
 import '@uirouter/angularjs';
 import 'angular-translate';
+import '@ovh-ux/ng-at-internet';
+import '@ovh-ux/ng-at-internet-ui-router-plugin';
 
 import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
 
@@ -21,6 +23,8 @@ angular
     'ui.router',
     onboarding,
     dashboard,
+    'ngAtInternet',
+    'ngAtInternetUiRouterPlugin',
     ListLayoutHelper.moduleName,
   ])
   .config(routing)
