0e99703c1320996a8ca4dbbd80d64ae6066836c5
JeremyDec <jeremy.de-cesare@ovhcloud.com>
2021-08-04 11:26:50 +0200

    feat(netapp): add create volume
    ref: MANAGER-7148
    
    Signed-off-by: JeremyDec <jeremy.de-cesare@ovhcloud.com>

---
+++ packages/manager/modules/netapp/src/dashboard/volumes/create/controller.js
@@ -0,0 +1,57 @@
+const MINIMUM_VOLUME_SIZE = 100;
+
+export default class VolumeCreateCtrl {
+  /* @ngInject */
+  constructor($http, $translate) {
+    this.$http = $http;
+    this.$translate = $translate;
+  }
+
+  $onInit() {
+    this.protocolList = this.protocolEnum.map((protocol) => ({
+      key: this.$translate.instant(`netapp_volume_create_protocol_${protocol}`),
+      value: protocol,
+    }));
+
+    [this.newVolumeProtocol] = this.protocolList;
+    this.minimumVolumeSize = MINIMUM_VOLUME_SIZE;
+    this.availableVolumeSize = this.getAvailableSize();
+  }
+
+  getAvailableSize() {
+    const storageVolumesSize = this.volumes.reduce(
+      (allSizes, volume) => allSizes + volume.size,
+      0,
+    );
+
+    return this.storage.quota - storageVolumesSize;
+  }
+
+  onCreateVolume() {
+    this.isCreating = true;
+    this.trackClick();
+
+    return this.$http
+      .post(`/storage/netapp/${this.storage.id}/share`, {
+        ...this.newVolume,
+        protocol: this.newVolumeProtocol.value,
+      })
+      .then(({ data: volume }) =>
+        this.goToVolumeDetails(
+          volume.id,
+          this.$translate.instant('netapp_volume_create_success'),
+        ),
+      )
+      .catch((error) =>
+        this.goBack(
+          this.$translate.instant('netapp_volume_create_error', {
+            message: error.data?.message,
+          }),
+          'error',
+        ),
+      )
+      .finally(() => {
+        this.isCreating = false;
+      });
+  }
+}
