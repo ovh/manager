f1d165fb083cd4a3d3a6c89104cca77b8f3ed9d3
JeremyDec <jeremy.de-cesare@ovhcloud.com>
2021-08-04 11:26:50 +0200

    feat(netapp): add create volume
    ref: MANAGER-7148
    
    Signed-off-by: JeremyDec <jeremy.de-cesare@ovhcloud.com>

--- packages/manager/modules/netapp/src/dashboard/volumes/create/module.js
+++ packages/manager/modules/netapp/src/dashboard/volumes/create/module.js
@@ -1,26 +1,25 @@
 import angular from 'angular';
 import '@ovh-ux/manager-core';
+import '@ovh-ux/ng-ui-router-layout';
+import '@ovh-ux/ui-kit';
 import '@uirouter/angularjs';
 import 'angular-translate';
 
 import component from './component';
 import routing from './routing';
 
-import create from './create';
-import deleteModule from './delete';
-
-const moduleName = 'ovhManagerNetAppVolumes';
+const moduleName = 'ovhManagerNetAppVolumesCreate';
 
 angular
   .module(moduleName, [
+    'oui',
     'ovhManagerCore',
     'pascalprecht.translate',
     'ui.router',
-    create,
-    deleteModule,
+    'ngUiRouterLayout',
   ])
   .config(routing)
-  .component('ovhManagerNetAppVolumes', component)
+  .component('ovhManagerNetAppCreateVolume', component)
   .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
