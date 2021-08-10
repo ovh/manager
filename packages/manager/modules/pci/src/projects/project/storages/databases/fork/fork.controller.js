15cfb83c7a69c1e0ca46e29fe2697bd4a2f57a99
Gael Leblan <gael.leblan@ovhcloud.com>
2021-08-10 16:36:22 +0200

    feat: mongodb ga - backup tab - fork configuration page
    PUD-686
    
    Signed-off-by: Gael Leblan <gael.leblan@ovhcloud.com>

---
+++ packages/manager/modules/pci/src/projects/project/storages/databases/fork/fork.controller.js
@@ -0,0 +1,186 @@
+import find from 'lodash/find';
+import filter from 'lodash/filter';
+import get from 'lodash/get';
+import map from 'lodash/map';
+import sortBy from 'lodash/sortBy';
+
+import { NAME_PATTERN } from './fork.constants';
+
+export default class {
+  /* @ngInject */
+  constructor(
+    $anchorScroll,
+    $translate,
+    $q,
+    CucCloudMessage,
+    DatabaseService,
+    Poller,
+  ) {
+    this.$anchorScroll = $anchorScroll;
+    this.$translate = $translate;
+    this.$q = $q;
+    this.CucCloudMessage = CucCloudMessage;
+    this.DatabaseService = DatabaseService;
+    this.Poller = Poller;
+    this.NAME_PATTERN = NAME_PATTERN;
+  }
+
+  $onInit() {
+    this.labAccepted = this.lab.isActivated();
+    this.messageContainer = 'pci.projects.project.storages.databases.fork';
+    this.loadMessages();
+    this.model = {
+      engine: find(this.engines, this.database.engine.name),
+      plan: null,
+      region: null,
+      flavor: null,
+      name: '',
+      privateNetwork: null,
+      subnet: null,
+      usePrivateNetwork: false,
+    };
+    this.orderData = null;
+    this.defaultPrivateNetwork = {
+      id: '',
+      name: this.$translate.instant('pci_database_common_none'),
+    };
+    this.defaultSubnet = {
+      id: '',
+      name: this.$translate.instant('pci_database_common_none'),
+    };
+    this.trackDatabases('configuration', 'page');
+  }
+
+  acceptLab(accepted) {
+    this.labAccepted = accepted;
+  }
+
+  loadMessages() {
+    this.CucCloudMessage.unSubscribe(this.messageContainer);
+    this.messageHandler = this.CucCloudMessage.subscribe(
+      this.messageContainer,
+      { onMessage: () => this.refreshMessages() },
+    );
+  }
+
+  refreshMessages() {
+    this.messages = this.messageHandler.getMessages();
+  }
+
+  setDefaultPlan() {
+    this.model.plan = this.model.engine.selectedVersion.getDefaultPlan(
+      this.model.plan,
+    );
+  }
+
+  setDefaultRegion() {
+    this.model.region = this.model.plan.getDefaultRegion(this.model.region);
+  }
+
+  setDefaultFlavor() {
+    this.model.flavor = this.model.region.getDefaultFlavor(this.model.flavor);
+  }
+
+  onFlavorSelect() {
+    this.model.usePrivateNetwork = !this.model.flavor.supportsPublicNetwork;
+    this.model.privateNetwork = this.defaultPrivateNetwork;
+    this.model.subnet = null;
+    this.model.name = `${this.model.engine.name}-${this.model.flavor.name}-${this.model.plan.name}-`;
+    this.availablePrivateNetworks = [
+      this.defaultPrivateNetwork,
+      ...sortBy(
+        map(
+          filter(this.privateNetworks, (network) =>
+            find(
+              network.regions,
+              (region) =>
+                region.region.startsWith(this.model.region.name) &&
+                region.status === 'ACTIVE',
+            ),
+          ),
+          (privateNetwork) => ({
+            ...privateNetwork,
+            name: `${privateNetwork.vlanId.toString().padStart(4, '0')} - ${
+              privateNetwork.name
+            }`,
+          }),
+        ),
+        ['name'],
+      ),
+    ];
+  }
+
+  isNetworkSelected() {
+    return this.model.usePrivateNetwork
+      ? this.model.privateNetwork.id && this.model.subnet?.id
+      : true;
+  }
+
+  prepareOrderData() {
+    this.orderData = {
+      description: this.model.name,
+      networkId: this.model.privateNetwork.regions?.find(
+        (region) => region.region === this.model.subnet?.ipPools[0].region,
+      )?.openstackId,
+      subnetId: this.model.subnet?.id,
+      nodesList: null,
+      nodesPattern: {
+        flavor: this.model.flavor.name,
+        number: this.model.plan.nodesCount,
+        region: this.model.region.name,
+      },
+      plan: this.model.plan.name,
+      version: this.model.engine.selectedVersion.version,
+    };
+  }
+
+  createDatabase() {
+    this.processingOrder = true;
+    this.CucCloudMessage.flushMessages(this.messageContainer);
+    this.trackDatabases(`config_create_database::${this.model.engine.name}`);
+    this.trackDatabases(
+      `PublicCloud_create_new_database::${this.model.engine.name}::${this.orderData.plan}::${this.orderData.nodesPattern.flavor}`,
+      'action',
+      false,
+    );
+    return this.DatabaseService.activateLab(this.projectId, this.lab)
+      .then(() =>
+        this.DatabaseService.createDatabase(
+          this.projectId,
+          this.model.engine.name,
+          this.orderData,
+        ),
+      )
+      .then((databaseInfo) => {
+        this.trackDatabases(
+          `config_create_database_validated::${this.model.engine.name}`,
+        );
+        return this.onDatabaseFork(
+          {
+            ...databaseInfo,
+            engine: this.model.engine.name,
+          },
+          this.$translate.instant('pci_database_fork_create_database_success'),
+          'info',
+        );
+      })
+      .catch((error) => {
+        this.trackDatabases(
+          `config_create_database_error::${this.model.engine.name}`,
+        );
+        this.CucCloudMessage.error(
+          this.$translate.instant('pci_database_fork_create_database_error', {
+            message: get(error, 'data.message'),
+          }),
+          this.messageContainer,
+        );
+        this.currentStep = 0;
+        this.$anchorScroll('forkMessages');
+        this.processingOrder = false;
+      });
+  }
+
+  $onDestroy() {
+    this.DatabaseService.stopPollingLabActivationStatus(this.lab.id);
+  }
+}
