47f766d15e7890853eaf016051d9425aeba17666
varun257 <varun257@gmail.com>
2021-08-12 16:36:59 +0530

    feat: add nutanix
    Ref #MANAGER-7450
    
    BREAKING CHANGE: add nutanix module
    
    Signed-off-by: varun257 <varun257@gmail.com>

---
+++ packages/manager/modules/nutanix/src/dashboard/controller.js
@@ -0,0 +1,10 @@
+export default class NutanixCtrl {
+  /* @ngInject */
+  constructor($translate) {
+    this.$translate = $translate;
+  }
+
+  $onInit() {
+    console.log('Hello', this.$translate.instant('world'));
+  }
+}
