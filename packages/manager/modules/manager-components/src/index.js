2c7030b1260201494e9939ddf88873f7dc3a6f23
Marie JONES <marie.jones@corp.ovh.com>
2021-08-13 11:48:30 +0200

    feat(inline-editor): add inline property editor
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/manager-components/src/index.js
+++ packages/manager/modules/manager-components/src/index.js
@@ -1,6 +1,7 @@
+import inlinePropertyEditor from './inline-property-editor';
 import region from './region';
 import resourceSelector from './resource-selector';
 
-export { region };
+export { inlinePropertyEditor, region };
 
 export default { resourceSelector };
