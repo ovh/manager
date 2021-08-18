7a43a31d0fa90452fa469c0afc039d46c0b25501
Antoine Leblanc <antoine.leblanc@ovhcloud.com>
2021-08-18 14:30:16 +0200

    feat(dashboard.volumes): add acl tab (#5426)
    ref: MANAGER-7152
    
    Signed-off-by: Antoine Leblanc <antoine.leblanc@ovhcloud.com>

--- packages/manager/modules/netapp/src/dashboard/volumes/dashboard/acl/module.js
+++ packages/manager/modules/netapp/src/dashboard/volumes/dashboard/acl/module.js
@@ -1,27 +1,29 @@
 import angular from 'angular';
 import '@ovh-ux/manager-core';
+import '@ovh-ux/ng-ovh-utils';
+import '@ovh-ux/ng-ui-router-breadcrumb';
+import '@ovh-ux/ui-kit';
 import '@uirouter/angularjs';
 import 'angular-translate';
-
-import { inlinePropertyEditor } from '@ovh-ux/manager-components';
 
 import component from './component';
 import routing from './routing';
 
-import aclModule from './acl';
+import './index.scss';
 
-const moduleName = 'ovhManagerNetAppVolumesDashboard';
+const moduleName = 'ovhManagerNetAppVolumesDashboardAcl';
 
 angular
   .module(moduleName, [
+    'ngUiRouterBreadcrumb',
+    'ngOvhUtils',
+    'oui',
     'ovhManagerCore',
     'pascalprecht.translate',
     'ui.router',
-    inlinePropertyEditor,
-    aclModule,
   ])
   .config(routing)
-  .component('ovhManagerNetAppVolumesDashboard', component)
+  .component('ovhManagerNetAppVolumesDashboardAcl', component)
   .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
--- packages/manager/modules/netapp/src/dashboard/volumes/dashboard/component.js
+++ packages/manager/modules/netapp/src/dashboard/volumes/dashboard/component.js
@@ -6,6 +6,7 @@ export default {
     accessPath: '<',
     currentActiveLink: '<',
     volumeDashboardLink: '<',
+    volumeDashboardAclLink: '<',
     volume: '<',
     updateVolume: '<',
     goToVolumeDashboard: '<',
