39a731c69a4707f69d036bf320b17cf3a39a3290
Gael Leblan <gael.leblan@ovhcloud.com>
2021-08-20 16:56:57 +0200

    feat: mongodb ga - dashboard - reorganize blocks
    PUD-676
    
    Signed-off-by: Gael Leblan <gael.leblan@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/databases/edit-name/index.js
+++ packages/manager/modules/pci/src/projects/project/storages/databases/edit-name/index.js
@@ -7,7 +7,7 @@ const moduleName = 'ovhManagerPciStoragesDatabaseNameLazyloading';
 angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
   /* @ngInject */ ($stateProvider) => {
     $stateProvider.state('pci.projects.project.storages.databases.name.**', {
-      url: '/name',
+      url: '/:databaseId/name',
       lazyLoad: ($transition$) => {
         const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
 
