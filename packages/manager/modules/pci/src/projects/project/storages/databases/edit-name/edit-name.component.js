4e33a2216727031bd64572be907d7775eb0420ca
Gael Leblan <gael.leblan@ovhcloud.com>
2021-08-20 16:56:57 +0200

    feat: mongodb ga - dashboard - reorganize blocks
    PUD-676
    
    Signed-off-by: Gael Leblan <gael.leblan@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/databases/edit-name/edit-name.component.js
+++ packages/manager/modules/pci/src/projects/project/storages/databases/edit-name/edit-name.component.js
@@ -7,6 +7,7 @@ const component = {
     goBack: '<',
     projectId: '<',
     trackDatabases: '<',
+    databaseId: '<',
   },
   template,
   controller,
--- packages/manager/modules/pci/src/projects/project/storages/databases/edit-name/edit-name.routing.js
+++ packages/manager/modules/pci/src/projects/project/storages/databases/edit-name/edit-name.routing.js
@@ -1,6 +1,8 @@
+import find from 'lodash/find';
+
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('pci.projects.project.storages.databases.name', {
-    url: '/name',
+    url: '/:databaseId/name',
     views: {
       modal: {
         component: 'ovhManagerPciProjectDatabaseName',
@@ -9,6 +11,10 @@ export default /* @ngInject */ ($stateProvider) => {
     layout: 'modal',
     resolve: {
       breadcrumb: () => null,
+      databaseId: /* @ngInject */ ($transition$) =>
+        $transition$.params().databaseId,
+      database: /* @ngInject */ (databaseId, databases) =>
+        find(databases, { id: databaseId }),
     },
   });
 };
