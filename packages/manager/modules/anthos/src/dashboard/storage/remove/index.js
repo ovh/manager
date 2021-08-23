1e3e8426f9ac183e65b49457ec73b80d0b7195c1
Ganesh Kumar <ganesh.kumar@ovhcloud.com>
2021-08-23 18:49:31 +0530

    feat(anthos): add storage ctas
    add delete and add svm volume
    
    Ref #MANAGER-7393
    
    Signed-off-by: Ganesh Kumar <ganesh.kumar@ovhcloud.com>
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

---
+++ packages/manager/modules/anthos/src/dashboard/storage/remove/index.js
@@ -0,0 +1,22 @@
+import angular from 'angular';
+import '@uirouter/angularjs';
+import 'oclazyload';
+
+const moduleName = 'ovhManagerAnthosDashboardStorageRemoveLazyloading';
+
+angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
+  /* @ngInject */ ($stateProvider) => {
+    $stateProvider.state('anthos.dashboard.storage.remove.**', {
+      url: '/remove',
+      lazyLoad: ($transition$) => {
+        const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+
+        return import('./module').then((mod) =>
+          $ocLazyLoad.inject(mod.default || mod),
+        );
+      },
+    });
+  },
+);
+
+export default moduleName;
