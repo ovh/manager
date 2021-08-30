a8eee10c21a32281912ae8a2d2d2be477ed14620
mzahas <mohammed.zahaf.ext@ovhcloud.com>
2021-08-30 18:08:58 +0200

    feat(anthos.dashboard.ips.remove-private-ip): add remove ip module
    ref: MANAGER-7394
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

---
+++ packages/manager/modules/anthos/src/dashboard/ips/remove-private-ip/remove-private-ip.routing.js
@@ -0,0 +1,22 @@
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state('anthos.dashboard.ips.remove-private-ip', {
+    url: '/remove-private-ip',
+    params: { privateIp: null },
+    views: {
+      modal: {
+        component: 'anthosDashboardIpsRemovePrivateIp',
+      },
+    },
+    layout: 'modal',
+    onEnter: /* @ngInject */ ($transition$, goBack) => {
+      if (!$transition$.params().privateIp) goBack(null);
+    },
+    resolve: {
+      breacrumb: /* @ngInject */ () => false,
+
+      privateIp: /* @ngInject */ ($transition$) => {
+        return $transition$.params().privateIp;
+      },
+    },
+  });
+};
