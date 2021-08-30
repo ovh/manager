cb59cb28986fb42881ea19b45b2c5a4937459443
mzahas <mohammed.zahaf.ext@ovhcloud.com>
2021-08-30 18:08:58 +0200

    feat(anthos.dashboard.ips.remove-private-ip): add remove ip module
    ref: MANAGER-7394
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

---
+++ packages/manager/modules/anthos/src/dashboard/ips/remove-private-ip/remove-private-ip.controller.js
@@ -0,0 +1,50 @@
+export default class RemovePrivateIpController {
+  /* @ngInject */
+  constructor($translate, Alerter, AnthosTenantsService) {
+    this.$translate = $translate;
+    this.Alerter = Alerter;
+    this.AnthosTenantsService = AnthosTenantsService;
+  }
+
+  removePrivateIp(rangeId) {
+    const privateIpIndex = this.privateIPs.findIndex(
+      ({ id }) => id === rangeId,
+    );
+    if (privateIpIndex >= 0) {
+      this.privateIPs.splice(privateIpIndex, 1);
+    }
+  }
+
+  onRemovePrivateIpClick() {
+    this.isRemoving = true;
+
+    return this.AnthosTenantsService.removeTenantPrivateIP(
+      this.serviceName,
+      this.privateIp.id,
+    )
+      .then(() => {
+        this.removePrivateIp(this.privateIp.id);
+        return this.goBack(
+          this.$translate.instant(
+            'anthos_tenant_dashboard_ips_remove_private_ip_modal_action_remove_success',
+          ),
+          'success',
+        );
+      })
+      .catch((error) => {
+        this.removePrivateIp(this.privateIp.id);
+        return this.goBack(
+          this.$translate.instant(
+            'anthos_tenant_dashboard_ips_remove_private_ip_modal_action_remove_error',
+            {
+              message: error.message || error.data?.message,
+            },
+          ),
+          'error',
+        );
+      })
+      .finally(() => {
+        this.isRemoving = false;
+      });
+  }
+}
