851a2467fd33f0fe02e474ee6bdc6381f1f2f8e8
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2021-09-02 17:44:47 +0200

    feat: add form to create redis user
    PUD-793
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/databases/database/users/add/add.controller.js
+++ packages/manager/modules/pci/src/projects/project/storages/databases/database/users/add/add.controller.js
@@ -12,6 +12,10 @@ export default class {
     this.model = {
       username: '',
       password: '',
+      categories: [],
+      channels: [],
+      commands: [],
+      keys: [],
       selectedRoles: [],
     };
     this.isRolesReadOnly = !this.isFeatureActivated('getRoles');
@@ -21,48 +25,50 @@ export default class {
   }
 
   getUserFromModel() {
-    const user = {};
+    const user = {
+      name: this.model.username,
+    };
     if (this.database.engine === 'redis') {
-      user.categories = [];
-      user.channels = [];
-      user.commands = [];
-      user.keys = [];
-      user.username = this.model.username;
+      user.categories = this.model.categories;
+      user.channels = this.model.channels;
+      user.commands = this.model.commands;
+      user.keys = this.model.keys;
     } else {
-      user.name = this.model.username;
       user.roles = this.model.selectedRoles.map((role) => role.name);
     }
     return user;
   }
 
   addUser() {
-    this.trackDatabases('dashboard::users::add_a_user::define_role_validate');
-    this.processing = true;
-    return this.DatabaseService.addUser(
-      this.projectId,
-      this.database.engine,
-      this.database.id,
-      this.getUserFromModel(),
-    )
-      .then((createdUser) =>
-        this.goBack({
-          textHtml: this.$translate.instant(
-            'pci_databases_users_add_success_message',
-            {
-              username: createdUser.username,
-              password: createdUser.password,
-            },
-          ),
-        }),
+    if (this.model.username) {
+      this.trackDatabases('dashboard::users::add_a_user::define_role_validate');
+      this.processing = true;
+      this.DatabaseService.addUser(
+        this.projectId,
+        this.database.engine,
+        this.database.id,
+        this.getUserFromModel(),
       )
-      .catch((err) =>
-        this.goBack(
-          this.$translate.instant('pci_databases_users_add_error_save', {
-            message: get(err, 'data.message', null),
+        .then((createdUser) =>
+          this.goBack({
+            textHtml: this.$translate.instant(
+              'pci_databases_users_add_success_message',
+              {
+                username: createdUser.username,
+                password: createdUser.password,
+              },
+            ),
           }),
-          'error',
-        ),
-      );
+        )
+        .catch((err) =>
+          this.goBack(
+            this.$translate.instant('pci_databases_users_add_error_save', {
+              message: get(err, 'data.message', null),
+            }),
+            'error',
+          ),
+        );
+    }
   }
 
   cancel(trackingCode) {
