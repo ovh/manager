aeb75d064cd11fb9eeab73e677df40844a18d19f
mzahas <mohammed.zahaf.ext@ovhcloud.com>
2021-09-03 18:28:18 +0200

    fix(anthos.dashboard.ips.assign-private-ip): private ip regex
    ref: MANAGER-7394
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/anthos/src/dashboard/components/assign-private-ip/assign-private-ip.controller.js
+++ packages/manager/modules/anthos/src/dashboard/components/assign-private-ip/assign-private-ip.controller.js
@@ -1,4 +1,4 @@
-import { IPV4_BLOCK_REGEX } from './constant';
+import { IPV4_REGEX } from './constant';
 
 export default class AssignPrivateIpController {
   /* @ngInject */
@@ -7,21 +7,93 @@ export default class AssignPrivateIpController {
     this.Alerter = Alerter;
     this.AnthosTenantsService = AnthosTenantsService;
 
-    this.IPV4_BLOCK_REGEX = IPV4_BLOCK_REGEX;
-    this.privateIP = {
-      range: null,
+    this.ipModel = {
+      form: null,
+      labels: {
+        privateInputIp: 'privateInputIp',
+      },
+      validationKeys: {
+        INVALID_IP_FORMAT: 'INVALID_IP_FORMAT',
+        INVALID_PRIVATE_IP: 'INVALID_PRIVATE_IP',
+        INVALID_RANGE_IP: 'INVALID_RANGE_IP',
+      },
+      privateIp: {
+        range: null,
+      },
     };
   }
 
-  onAssignPrivateIpClick() {
+  static isValidV4Ip(ip) {
+    return IPV4_REGEX.IP_V4_FORMAT.test(ip);
+  }
+
+  static isValidCidrIp(range) {
+    return IPV4_REGEX.IP_CIDR_V4_FORMAT.test(range);
+  }
+
+  static isValidRangeIp(range) {
+    const [rangeStart, rangeEnd] = (range || '').split('-');
+    return (
+      AssignPrivateIpController.isValidV4Ip(rangeStart) &&
+      AssignPrivateIpController.isValidV4Ip(rangeEnd)
+    );
+  }
+
+  static isPrivateIp(ips) {
+    return (
+      ips.findIndex((ip) => {
+        return (
+          IPV4_REGEX.PRIVATE.RANGE_V4_10.test(ip) ||
+          IPV4_REGEX.PRIVATE.RANGE_V4_172.test(ip) ||
+          IPV4_REGEX.PRIVATE.RANGE_V4_192.test(ip)
+        );
+      }) >= 0
+    );
+  }
+
+  static isValidIpFormat(range) {
+    return (
+      AssignPrivateIpController.isValidV4Ip(range) ||
+      AssignPrivateIpController.isValidCidrIp(range) ||
+      AssignPrivateIpController.isValidRangeIp(range)
+    );
+  }
+
+  isValidIp() {
+    const { form, labels, validationKeys, privateIp } = this.ipModel;
+    const { range } = privateIp;
+    const [ip] = (range || '').split('/');
+    const [rangeStart, rangeEnd] = (range || '').split('-');
+    const { INVALID_IP_FORMAT, INVALID_PRIVATE_IP } = validationKeys;
+
+    Object.keys(this.ipModel.validationKeys).forEach((validationKey) => {
+      form[labels.privateInputIp].$setValidity(validationKey, true);
+    });
+
+    if (!AssignPrivateIpController.isValidIpFormat(range)) {
+      form[labels.privateInputIp].$setValidity(INVALID_IP_FORMAT, false);
+      return false;
+    }
+
+    if (
+      !AssignPrivateIpController.isPrivateIp([range, ip, rangeStart, rangeEnd])
+    ) {
+      form[labels.privateInputIp].$setValidity(INVALID_PRIVATE_IP, false);
+      return false;
+    }
+
+    return true;
+  }
+
+  assignPrivateIp() {
     this.isAdding = true;
 
     return this.AnthosTenantsService.addTenantPrivateIP(
       this.serviceName,
-      this.privateIP,
+      this.ipModel.privateIp,
     )
-      .then((privateIP) => {
-        this.privateIPs.push(privateIP);
+      .then((privateIp) => {
+        this.privateIPs.push(privateIp);
         return this.goBack(
           this.$translate.instant(
             'anthos_tenant_dashboard_component_assign_private_ip_modal_action_add_success',
@@ -43,5 +115,13 @@ export default class AssignPrivateIpController {
       .finally(() => {
         this.isAdding = false;
       });
+  }
+
+  onAssignPrivateIpSubmit() {
+    if (this.isValidIp()) {
+      return this.assignPrivateIp();
+    }
+
+    return null;
   }
 }
