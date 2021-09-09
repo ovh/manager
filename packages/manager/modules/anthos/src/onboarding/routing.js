e023fd832348206ec930b4d2b948164176db18a3
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2021-09-09 15:57:35 +0200

    feat(anthos.onboarding): onboarding module
    ref: MANAGER-7390
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/anthos/src/onboarding/routing.js
+++ packages/manager/modules/anthos/src/onboarding/routing.js
@@ -1,20 +1,48 @@
+import { ANTHOS_READ_MORE_GUIDE, GUIDES } from '../anthos.constants';
+import illustration from './assets/anthos.png';
+
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('anthos.onboarding', {
     url: '/onboarding',
-    component: 'AnthosOnboarding',
+    component: 'anthosOnboarding',
     redirectTo: (transition) =>
       transition
         .injector()
-        .getAsync('resources')
-        .then((services) =>
-          services.length !== 0
+        .getAsync('tenants')
+        .then((tenants) => {
+          return tenants.length > 0
             ? {
                 state: 'anthos.index',
               }
-            : false,
-        ),
+            : false;
+        }),
     resolve: {
-      breadcrumb: () => null,
+      breadcrumb: /* @ngInject */ ($translate) =>
+        $translate.instant('anthos_onboarding_breadcrumb'),
+
+      ovhSubsidiary: /* @ngInject */ (coreConfig) =>
+        coreConfig.getUser().ovhSubsidiary,
+
+      illustration: /* @ngInject */ () => illustration,
+
+      guides: /* @ngInject */ ($translate, ovhSubsidiary) => {
+        return [
+          {
+            title: $translate.instant('anthos_onboarding_guides_anthos_title'),
+            description: $translate.instant(
+              'anthos_onboarding_guides_anthos_description',
+            ),
+            link: GUIDES[ovhSubsidiary] || GUIDES.DEFAULT,
+            external: true,
+          },
+        ];
+      },
+
+      anthosReadMoreInfoLink: /* @ngInject */ (ovhSubsidiary) =>
+        ANTHOS_READ_MORE_GUIDE[ovhSubsidiary] || ANTHOS_READ_MORE_GUIDE.DEFAULT,
+
+      tenants: /* @ngInject */ (AnthosTenantsService) =>
+        AnthosTenantsService.getTenants(),
     },
   });
 };
