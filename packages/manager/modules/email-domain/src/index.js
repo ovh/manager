7680afba0b66a2b77ac430fe527a9a1f86402b4f
frenauvh <florian.renaut@corp.ovh.com>
2021-09-10 08:49:31 +0200

    feat(email-domain): module initial import
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>
    
    BREAKING CHANGE: move email domain section to email-domain module

---
+++ packages/manager/modules/email-domain/src/index.js
@@ -0,0 +1,94 @@
+import angular from 'angular';
+import '@uirouter/angularjs';
+import 'oclazyload';
+import '@ovh-ux/ui-kit/dist/css/oui.css';
+import './index.less';
+import './index.scss';
+
+const moduleName = 'ovhManagerEmailDomainLazyLoading';
+
+angular
+  .module(moduleName, ['ui.router', 'oc.lazyLoad'])
+  .config(
+    /* @ngInject */ ($stateProvider, $urlRouterProvider) => {
+      $stateProvider.state('app.email', {
+        url: '/email_domain',
+        template: '<div ui-view></div>',
+        redirectTo: 'app.email.index',
+        resolve: {
+          breadcrumb: () => 'Emails',
+        },
+      });
+
+      $stateProvider.state('app.email.index.**', {
+        url: '',
+        lazyLoad: ($transition$) => {
+          const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+
+          return import('./email.module').then((mod) =>
+            $ocLazyLoad.inject(mod.default || mod),
+          );
+        },
+      });
+
+      $stateProvider.state('app.email.domain.**', {
+        url: '/:productId',
+        lazyLoad: ($transition$) => {
+          const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+
+          return import('./email.module').then((mod) =>
+            $ocLazyLoad.inject(mod.default || mod),
+          );
+        },
+      });
+
+      $urlRouterProvider.when(
+        /^\/configuration\/email-domain/,
+        /* @ngInject */ ($location) => {
+          $location.url($location.url().replace('/configuration', ''));
+        },
+      );
+
+      $stateProvider.state('app.email-delegate', {
+        url: '/email_delegate',
+        template: '<div ui-view></div>',
+        redirectTo: 'app.email-delegate.index',
+        resolve: {
+          breadcrumb: /* @ngInject */ ($translate) =>
+            $translate.instant('emails_domain_delegate_title'),
+        },
+      });
+
+      $stateProvider.state('app.email-delegate.index.**', {
+        url: '',
+        lazyLoad: ($transition$) => {
+          const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+
+          return import('./email.module').then((mod) =>
+            $ocLazyLoad.inject(mod.default || mod),
+          );
+        },
+      });
+
+      $stateProvider.state('app.email-delegate.dashboard.**', {
+        url: '/:productId',
+        lazyLoad: ($transition$) => {
+          const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+
+          return import('./email.module').then((mod) =>
+            $ocLazyLoad.inject(mod.default || mod),
+          );
+        },
+      });
+
+      $urlRouterProvider.when(
+        /^\/configuration\/email-delegate/,
+        /* @ngInject */ ($location) => {
+          $location.url($location.url().replace('/configuration', ''));
+        },
+      );
+    },
+  )
+  .run(/* @ngTranslationsInject:json ./translations */);
+
+export default moduleName;
