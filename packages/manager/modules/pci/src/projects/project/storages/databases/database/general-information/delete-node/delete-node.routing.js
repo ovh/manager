f97dbb363c2536bf39309bb2f4ff99771349e8fa
Ganesh Kumar <ganesh.kumar@ovhcloud.com>
2021-09-14 17:58:55 +0530

    fix(pci.storage.databases): add delete node
    Ref #PUD-673
    
    Signed-off-by: Ganesh Kumar <ganesh.kumar@ovhcloud.com>

---
+++ packages/manager/modules/pci/src/projects/project/storages/databases/database/general-information/delete-node/delete-node.routing.js
@@ -0,0 +1,31 @@
+import { STATUS } from '../../../../../../../components/project/storages/databases/databases.constants';
+
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state(
+    'pci.projects.project.storages.databases.dashboard.general-information.delete-node',
+    {
+      url: '/delete-node',
+      views: {
+        modal: {
+          component: 'ovhManagerPciProjectDatabaseGeneralInformationDeleteNode',
+        },
+      },
+      layout: 'modal',
+      resolve: {
+        breadcrumb: () => null,
+        onNodeDelete: /* @ngInject */ (
+          database,
+          getNodeObject,
+          goToDatabase,
+          pollNodesStatus,
+        ) => (nodeInfo, message, type) => {
+          database.setNodeStatus(nodeInfo, STATUS.DELETING);
+          pollNodesStatus();
+          return goToDatabase(database, message, type);
+        },
+        price: /* @ngInject */ (getCurrentFlavor) =>
+          getCurrentFlavor().nodeMonthlyPrice,
+      },
+    },
+  );
+};
