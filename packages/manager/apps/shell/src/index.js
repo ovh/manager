855fcc269f6ee214b0e88105ab211d093a159814
frenauvh <florian.renaut@corp.ovh.com>
2021-09-16 15:52:09 +0200

    feat(shell): handle iframe messages
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/apps/shell/src/index.js
+++ packages/manager/apps/shell/src/index.js
@@ -1,5 +1,7 @@
 import './index.scss';
 import './App.jsx';
 import initSso from './sso';
+import initRouter from './router';
 
 initSso();
+initRouter(document.querySelector('.manager-shell iframe'));
