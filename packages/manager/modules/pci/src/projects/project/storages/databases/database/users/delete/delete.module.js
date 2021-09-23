129614f6859c0f70194a5be439acb752abd011ab
Gael Leblan <gael.leblan@ovhcloud.com>
2021-09-23 23:20:51 +0200

    fix: fixes the error displayed when deleting a default admin
    PUD-895
    
    Signed-off-by: Gael Leblan <gael.leblan@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/databases/database/users/delete/delete.module.js
+++ packages/manager/modules/pci/src/projects/project/storages/databases/database/users/delete/delete.module.js
@@ -1,5 +1,7 @@
 import angular from 'angular';
 import '@uirouter/angularjs';
+import 'angular-translate';
+import '@ovh-ux/ng-translate-async-loader';
 
 import deleteComponent from '../../../../../../../components/project/users/delete';
 import routing from './delete.routing';
@@ -7,7 +9,14 @@ import routing from './delete.routing';
 const moduleName = 'ovhManagerPciStoragesDatabaseUsersDelete';
 
 angular
-  .module(moduleName, ['ui.router', 'oui', deleteComponent])
-  .config(routing);
+  .module(moduleName, [
+    'ngTranslateAsyncLoader',
+    'pascalprecht.translate',
+    'ui.router',
+    'oui',
+    deleteComponent,
+  ])
+  .config(routing)
+  .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
