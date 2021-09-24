01e8a5777c1b17fed8fe86d89b701a8032735cf1
JeremyDec <jeremy.de-cesare@ovhcloud.com>
2021-09-24 15:42:47 +0200

    feat(netapp): prepare snapshot policies state
    ref: MANAGER-7720
    
    Signed-off-by: JeremyDec <jeremy.de-cesare@ovhcloud.com>

---
+++ packages/manager/modules/netapp/src/dashboard/snapshot-policies/index.js
@@ -0,0 +1,22 @@
+import angular from 'angular';
+import '@uirouter/angularjs';
+import 'oclazyload';
+
+const moduleName = 'ovhManagerNetAppSnapshotPoliciesLazyLoading';
+
+angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
+  /* @ngInject */ ($stateProvider) => {
+    $stateProvider.state('netapp.dashboard.snapshotPolicies.**', {
+      url: '/snapshotPolicies',
+      lazyLoad: ($transition$) => {
+        const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+
+        return import('./module').then((mod) =>
+          $ocLazyLoad.inject(mod.default || mod),
+        );
+      },
+    });
+  },
+);
+
+export default moduleName;
