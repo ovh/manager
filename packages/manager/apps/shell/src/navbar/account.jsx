2c92651db4febd428e11e81b078320e143baeaca
Jisay <jean-christophe.alleman@corp.ovh.com>
2021-09-24 16:50:26 +0200

    feat(shell): add account-sidebar
    ref: MANAGER-7603
    
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

--- packages/manager/apps/shell/src/navbar/account.jsx
+++ packages/manager/apps/shell/src/navbar/account.jsx
@@ -1,26 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import { capitalize, truncate } from 'lodash-es';
-import { emit, listen } from '@ovh-ux/ufrontend/communication';
 import style from './navbar.module.scss';
 import { MESSAGES, TRANSLATE_NAMESPACE } from './constants';
 
-function NavbarAccount({ user }) {
-  const [sidebarReady, setSidebarReady] = useState(false);
+function NavbarAccount({ user, isAccountSidebarOpen, setIsAccountSidebarOpen }) {
   const { t } = useTranslation(TRANSLATE_NAMESPACE);
   const firstName = capitalize(user.firstname);
   const lastName = truncate(capitalize(user.name), {
     length: 10,
   });
-
-  useEffect(() => {
-    listen(({ id }) => {
-      if (id === MESSAGES.accountSidebarReady) {
-        setSidebarReady(true);
-      }
-    });
-  }, []);
 
   return (
     <button
@@ -29,11 +19,8 @@ function NavbarAccount({ user }) {
       className={`oui-navbar-link oui-navbar-link_icon oui-navbar-link_tertiary ${style.navbarLink}`}
       aria-label={t('navbar_account')}
       onClick={() => {
-        emit({
-          id: MESSAGES.accountSidebarToggle,
-        });
+        setIsAccountSidebarOpen(!isAccountSidebarOpen)
       }}
-      disabled={!sidebarReady}
     >
       <span className="oui-navbar-link__wrapper oui-navbar-link__wrapper_border">
         <span
