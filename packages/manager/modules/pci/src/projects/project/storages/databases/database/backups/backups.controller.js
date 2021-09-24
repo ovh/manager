b964284e99c6942c91214c54e5067f88d3504e5e
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2021-09-24 14:40:00 +0200

    fix: display full location name on backups tab
    PUD-915
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/databases/database/backups/backups.controller.js
+++ packages/manager/modules/pci/src/projects/project/storages/databases/database/backups/backups.controller.js
@@ -1,7 +1,8 @@
 export default class {
   /* @ngInject */
-  constructor(CucCloudMessage) {
+  constructor(CucCloudMessage, ovhManagerRegionService) {
     this.CucCloudMessage = CucCloudMessage;
+    this.ovhManagerRegionService = ovhManagerRegionService;
   }
 
   $onInit() {
