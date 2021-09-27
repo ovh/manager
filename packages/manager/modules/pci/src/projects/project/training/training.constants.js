0e3a9b260795b7c38313fad0b4136fcb6544ea99
christophe-rannou <christophe.rannou@ovhcloud.com>
2021-09-27 11:35:47 +0200

    feat(training): add set of predefined images for training job submission (#5523)
    ref: MANAGER-7739
    
    Signed-off-by: Christophe Rannou <christophe.rannou@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/training/training.constants.js
+++ packages/manager/modules/pci/src/projects/project/training/training.constants.js
@@ -1,7 +1,9 @@
 export const GUIDE_URL = 'https://docs.ovh.com/gb/en/ai-training';
 export const COMMUNITY_URL = 'https://community.ovh.com/en/c/Data-AI';
+export const DISCORD_URL = 'https://discord.com/invite/vXVurFfwe9';
 
 export default {
   GUIDE_URL,
   COMMUNITY_URL,
+  DISCORD_URL,
 };
