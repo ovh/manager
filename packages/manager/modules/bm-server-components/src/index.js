0beecf7b5899fb288cbf1d6aa965f28d76c42e6f
varun257 <varun257@gmail.com>
2021-09-27 15:40:15 +0530

    feat(dedicated-server-components): add support tile component
    ref: MANAGER-6936
    
    Signed-off-by: varun257 <varun257@gmail.com>
    Co-authored-by: Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>

--- packages/manager/modules/bm-server-components/src/index.js
+++ packages/manager/modules/bm-server-components/src/index.js
@@ -4,6 +4,7 @@ export { default as serverOsInstallGabarit } from './os-install/gabarit';
 export { default as serverOsInstallProgress } from './os-install/progress';
 export { default as serverOsInstallImage } from './os-install/image';
 export { default as pollingService } from './polling/polling.service';
+export { default as serverSupport } from './support';
 export { default as serverTasks } from './tasks';
 export { default as serverInterventions } from './interventions';
 export { default as serverIpmi } from './ipmi';
