e3b1be93a838e82514282bba3685c814431c0f1f
frenauvh <florian.renaut@corp.ovh.com>
2021-09-28 11:33:47 +0200

    feat(manager-shell): use ovh-shell library
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/apps/shell/src/shell/index.jsx
+++ packages/manager/apps/shell/src/shell/index.jsx
@@ -1,4 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+
+import i18n from '@ovh-ux/shell/plugin/i18n';
+import initShell from '@ovh-ux/shell';
+
+import ApplicationContext from '@/context';
 import Router from '@/core/router';
 import ShellHeader from './header';
 import style from './shell.module.scss';
@@ -6,9 +11,19 @@ import style from './shell.module.scss';
 function Shell() {
   const iframeRef = useRef(null);
   const [iframe, setIframe] = useState(null);
+  const { environment } = useContext(ApplicationContext);
+
   useEffect(() => {
     setIframe(iframeRef.current);
   }, [iframeRef]);
+
+  useEffect(() => {
+    const shell = initShell();
+    shell.registerPlugin('i18n', i18n(environment));
+    shell.connectApi(iframeRef.current);
+    return () => shell.disconnectApi();
+  }, []);
+
   return (
     <div className={style.managerShell}>
       {iframe && <Router iframe={iframe} />}
