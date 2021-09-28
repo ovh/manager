d4cb24a471b4ec3cc0d181662c693a85f5e0aee6
frenauvh <florian.renaut@corp.ovh.com>
2021-09-28 11:27:03 +0200

    feat(manager-shell): create manager-shell application
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>
    Co-authored-by: Jisay <jean-christophe.alleman@corp.ovh.com>
    Co-authored-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

---
+++ packages/manager/apps/shell/vite.config.js
@@ -0,0 +1,67 @@
+const { defineConfig } = require('vite');
+const fs = require('fs');
+const reactRefresh = require('@vitejs/plugin-react-refresh');
+const path = require('path');
+const legacy = require('@vitejs/plugin-legacy');
+
+const { proxy, sso: Sso } = require('@ovh-ux/manager-dev-server-config');
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+function viteOvhDevServerPlugin() {
+  const region = process.env.REGION || 'EU';
+  return {
+    name: 'vite-ovh-dev-server',
+    configureServer(server) {
+      const sso = new Sso(region);
+      const app = express();
+      app.get('/auth', sso.auth.bind(sso));
+      app.get('/auth/check', sso.checkAuth.bind(sso));
+      const v6Proxy = proxy.v6(region);
+      app.use(createProxyMiddleware(v6Proxy.context, v6Proxy));
+      const appDistPath = path.join(
+        __dirname,
+        '../',
+        process.env.APP || '',
+        'dist',
+      );
+      const appEntryPoint = path.join(appDistPath, 'index.html');
+      if (!fs.existsSync(appEntryPoint)) {
+        app.all('/app', (req, res) => {
+          res.status(404).send('Application not found');
+        });
+      } else {
+        app.use('/app', express.static(appDistPath));
+      }
+      server.middlewares.use(app);
+    },
+  };
+}
+
+module.exports = defineConfig({
+  base: './',
+  root: path.resolve(__dirname, 'src'),
+  clearScreen: false,
+  publicDir: 'public',
+  resolve: {
+    alias: {
+      '@': path.resolve(path.join(__dirname, 'src')),
+    },
+  },
+  plugins: [
+    reactRefresh(),
+    legacy({
+      targets: ['defaults'],
+    }),
+    viteOvhDevServerPlugin(),
+  ],
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+    minify: true,
+  },
+  server: {
+    port: 9000,
+    strictPort: true,
+  },
+});
