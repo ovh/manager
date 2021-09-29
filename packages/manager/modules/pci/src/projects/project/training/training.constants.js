5711163121c9ae7b23069552cad265755470d366
Christophe Rannou <christophe.rannou@ovhcloud.com>
2021-09-29 11:00:48 +0200

    feat(ai.training): custom docker link
    Add documentation link to how to create a custom docker image when selecting custom images
    ref: MLS-2129
    
    Signed-off-by: Christophe Rannou <christophe.rannou@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/training/training.constants.js
+++ packages/manager/modules/pci/src/projects/project/training/training.constants.js
@@ -1,9 +1,12 @@
 export const GUIDE_URL = 'https://docs.ovh.com/gb/en/ai-training';
 export const COMMUNITY_URL = 'https://community.ovh.com/en/c/Data-AI';
 export const DISCORD_URL = 'https://discord.com/invite/vXVurFfwe9';
+export const DOC_DOCKER_BUILD_URL =
+  'https://docs.ovh.com/gb/en/ai-training/build-use-custom-image/';
 
 export default {
   GUIDE_URL,
   COMMUNITY_URL,
   DISCORD_URL,
+  DOC_DOCKER_BUILD_URL,
 };
