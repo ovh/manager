a77403cc3b53a7b5f8bf24db62f77d2d571afb08
Gael Leblan <gael.leblan@ovhcloud.com>
2021-10-05 11:48:32 +0200

    feat: migrate metrics to internal endpoints
    PDATA-2939
    
    Signed-off-by: Gael Leblan <gael.leblan@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/data-processing/job-details/job-dashboard/metrics-token/metrics-token.constants.js
+++ packages/manager/modules/pci/src/projects/project/data-processing/job-details/job-dashboard/metrics-token/metrics-token.constants.js
@@ -1,4 +1,5 @@
-export const GRAFANA_URL = 'https://grafana.metrics.ovh.net';
+export const GRAFANA_URL =
+  'https://grafana.dataconvergence.ovh.com:3000/d/VhF0A_Gnz/ovhcloud-data-processing-overview?orgId=2';
 export const GUIDES_MONITOR_JOB_URL =
   'https://docs.ovh.com/gb/en/data-processing/monitor/';
 
