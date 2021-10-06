d8413dc2bf28fbba8679309d4a2c6e5052110cfb
frenauvh <florian.renaut@corp.ovh.com>
2021-10-06 10:02:19 +0200

    feat(shell): add routing configuration
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/components/ovh-shell/src/plugin/routing/application.ts
+++ packages/components/ovh-shell/src/plugin/routing/application.ts
@@ -1,8 +1,13 @@
+import RoutingConfiguration from './configuration';
+
 class Application {
   iframe: HTMLIFrameElement;
 
-  constructor(iframeElement: HTMLIFrameElement) {
+  config: RoutingConfiguration;
+
+  constructor(iframeElement: HTMLIFrameElement, config: RoutingConfiguration) {
     this.iframe = iframeElement;
+    this.config = config;
   }
 
   getURL(): URL {
@@ -13,18 +18,30 @@ class Application {
     this.iframe.contentWindow.location.replace(url);
   }
 
-  getApplicationId(): string {
-    return this.iframe.contentWindow.location.pathname.replace(/\//g, '');
+  getApplicationId(): string | undefined {
+    const path = this.getApplicationPath();
+    const appConfig = this.config.findByPath(path);
+    if (!appConfig) {
+      throw new Error(
+        `Cannot find configuration for application with path '${path}'`,
+      );
+    }
+    return appConfig.id;
   }
 
   getApplicationHash(): string {
     return this.iframe.contentWindow.location.hash.replace(/^[^/]+/, '');
   }
 
+  getApplicationPath(): string {
+    return this.iframe.contentWindow.location.pathname || '/';
+  }
+
   getApplicationRouting(): Record<string, string> {
     return {
       applicationId: this.getApplicationId(),
       applicationHash: this.getApplicationHash(),
+      applicationPath: this.getApplicationPath(),
     };
   }
 
@@ -39,12 +56,19 @@ class Application {
     if (url.pathname === 'blank') {
       url = new URL(window.location.href);
     }
-    if (url.pathname !== `/${applicationId}/`) {
-      url.pathname = `/${applicationId}/`;
+    const config = this.config.findById(applicationId);
+    if (!config) {
+      throw new Error(
+        `Cannot find configuration for application with id '${applicationId}'`,
+      );
     }
-    if (url.hash !== `/${applicationHash}`) {
-      url.hash = `/${applicationHash}`;
-    }
+    const currentAppPath = this.getApplicationPath();
+    // check for path equality before updating otherwise '/foo' would be updated
+    // by '/foo/' and would trigger full page reload in the application
+    url.pathname = this.config.pathEquals(currentAppPath, config.path)
+      ? currentAppPath
+      : config.path;
+    url.hash = `/${applicationHash}`;
     this.setURL(url.href);
   }
 }
