19b6cf459eae2f2d2df26033eac2c923ba1c4505
mzahas <mohammed.zahaf.ext@ovhcloud.com>
2021-10-08 18:13:57 +0200

    feat(web.hosting.multisite): use flush modal
    ref: MANAGER-7778
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/apps/web/client/app/hosting/multisite/hosting-multisite.constants.js
+++ packages/manager/apps/web/client/app/hosting/multisite/hosting-multisite.constants.js
@@ -1,5 +1,12 @@
-export const CDN_ACTIVE = 'ACTIVE';
+export const CDN_STATUS = {
+  ACTIVE: 'ACTIVE',
+};
+
+export const CDN_VERSION = {
+  CDN_HOSTING: 'cdn-hosting',
+};
 
 export default {
-  CDN_ACTIVE,
+  CDN_STATUS,
+  CDN_VERSION,
 };
