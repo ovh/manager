74aa58c10a4902cf3acc407baca9328d75c30bbb
mzahas <mohammed.zahaf.ext@ovhcloud.com>
2021-10-13 17:22:39 +0200

    feat(web.hosting.multisite.cdn-flush): setup flush module
    ref: MANAGER-7779
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

---
+++ packages/manager/apps/web/client/app/hosting/multisite/cdn-flush/cdn-flush.constants.js
@@ -0,0 +1,11 @@
+export const PURGE_TYPE_ENUM = {
+  ALL: 'all', // All is not part of purge enum options, here for consistency
+  EXTENSION: 'extension',
+  FOLDER: 'folder',
+  REGEX: 'regex',
+  URI: 'uri',
+};
+
+export default {
+  PURGE_TYPE_ENUM,
+};
