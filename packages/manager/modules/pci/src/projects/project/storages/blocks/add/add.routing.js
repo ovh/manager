cff3caee1fbd2ff8ae34e73bf41515c8646af9cd
mohammed-zahaf <22635766+mohammed-zahaf@users.noreply.github.com>
2021-10-13 08:35:44 +0200

    feat(storages.blocks.add): add automatically new volumes (#5441)
    ref: MANAGER-7376
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/blocks/add/add.routing.js
+++ packages/manager/modules/pci/src/projects/project/storages/blocks/add/add.routing.js
@@ -1,17 +1,37 @@
+import { VOLUME_ADDON_FAMILY } from '../block.constants';
+
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('pci.projects.project.storages.blocks.add', {
     url: '/new',
     component: 'pciProjectStorageBlocksAdd',
     resolve: {
       goBack: /* @ngInject */ (goToBlockStorage) => goToBlockStorage,
+
       cancelLink: /* @ngInject */ ($state, projectId) =>
         $state.href('pci.projects.project.storages.blocks', {
           projectId,
         }),
+
       quotaUrl: /* @ngInject */ ($state, projectId) =>
         $state.href('pci.projects.project.quota', {
           projectId,
         }),
+
+      volumesAvailability: /* @ngInject */ (
+        projectId,
+        coreConfig,
+        PciProjectStorageBlockService,
+      ) =>
+        PciProjectStorageBlockService.getVolumesAvailability(projectId, {
+          ovhSubsidiary: coreConfig.getUser().ovhSubsidiary,
+          addonFamily: VOLUME_ADDON_FAMILY,
+        }),
+
+      catalog: /* @ngInject */ (coreConfig, PciProjectStorageBlockService) =>
+        PciProjectStorageBlockService.getCatalog(
+          coreConfig.getUser().ovhSubsidiary,
+        ),
+
       breadcrumb: /* @ngInject */ ($translate) =>
         $translate.instant('pci_projects_project_storages_blocks_add_title'),
     },
