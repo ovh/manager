3c77ee68d3f0c4b1cd4a872d430f74c651416d4e
mzahas <mohammed.zahaf.ext@ovhcloud.com>
2021-10-13 18:46:59 +0200

    feat(web.hosting.multisite): setup module
    ref: MANAGER-7779
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/apps/web/client/app/hosting/multisite/hosting-multisite.constants.js
+++ packages/manager/apps/web/client/app/hosting/multisite/hosting-multisite.constants.js
@@ -6,7 +6,14 @@ export const CDN_VERSION = {
   CDN_HOSTING: 'cdn-hosting',
 };
 
+export const CDN_TYPE = {
+  BASIC: 'cdn-basic',
+  SECURITY: 'cdn-security',
+  ADVANCED: 'cdn-advanced',
+};
+
 export default {
   CDN_STATUS,
   CDN_VERSION,
+  CDN_TYPE,
 };
