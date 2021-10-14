ad0d239f150c9bfd42d034f0876834754a6eb989
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2021-10-14 17:55:52 +0200

    fix: pr comment
    PUD-968
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/databases/database/indexes/indexes.controller.js
+++ packages/manager/modules/pci/src/projects/project/storages/databases/database/indexes/indexes.controller.js
@@ -1,4 +1,4 @@
-export default class indexesCtrl {
+export default class IndexesCtrl {
   /* @ngInject */
   constructor($translate, CucCloudMessage, DatabaseService) {
     this.$translate = $translate;
