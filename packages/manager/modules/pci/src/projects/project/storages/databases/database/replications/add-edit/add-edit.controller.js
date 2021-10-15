ab6d533fc331f388e432067d19d95f95abc55e51
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2021-10-15 16:08:21 +0200

    feat: add controls to add page
    PUD-973
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/databases/database/replications/add-edit/add-edit.controller.js
+++ packages/manager/modules/pci/src/projects/project/storages/databases/database/replications/add-edit/add-edit.controller.js
@@ -1,12 +1,16 @@
+import { FORM_RULES, DEFAULT_VALUES } from './add-edit.constants';
+
 export default class {
   /* @ngInject */
   constructor($translate, DatabaseService) {
     this.$translate = $translate;
     this.DatabaseService = DatabaseService;
+    this.FORM_RULES = FORM_RULES;
   }
 
   $onInit() {
     // this.trackDashboard('users::add_a_user', 'page');
+    this.invalidTargetSource = false;
     this.model = {
       source: this.isUpdate ? this.replication.source : null,
       target: this.isUpdate ? this.replication.target : null,
@@ -14,16 +18,35 @@ export default class {
       blacklistedTopics: this.isUpdate
         ? this.replication.blacklistedTopics
         : [],
-      syncGroupOffset: this.isUpdate ? this.replication.syncGroupOffset : false,
-      editSyncInterval: this.isUpdate ? this.replication.editSyncInterval : 60,
+      syncGroupOffset: this.isUpdate
+        ? this.replication.syncGroupOffset
+        : DEFAULT_VALUES.status,
+      editSyncInterval: this.isUpdate
+        ? this.replication.editSyncInterval
+        : DEFAULT_VALUES.editSyncInterval,
       policy: this.isUpdate ? this.replication.policy : null,
-      heartbeat: this.isUpdate ? this.replication.heartbeat : true,
-      status: this.isUpdate ? this.replication.status === 'enabled' : true,
+      heartbeat: this.isUpdate
+        ? this.replication.heartbeat
+        : DEFAULT_VALUES.heartbeat,
+      status: this.isUpdate
+        ? this.replication.status === 'enabled'
+        : DEFAULT_VALUES.status,
     };
   }
 
   cancel() {
     // this.trackDashboard('users::add_a_user::cancel');
     this.goBack();
+  }
+
+  checkTargetAndSourceValidity() {
+    this.invalidTargetSource =
+      this.model.target &&
+      this.model.source &&
+      this.model.target.id === this.model.source.id;
+  }
+
+  addOrEditReplication() {
+    this.model.submitted = true;
   }
 }
