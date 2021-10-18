4c84fa306165513a4a2899390c7269046903b37a
mzahas <mohammed.zahaf.ext@ovhcloud.com>
2021-10-18 17:48:16 +0200

    feat(web.hosting.dashboard): cover the cdn v2 success flush msg
    ref: MANAGER-7825
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/apps/web/client/app/hosting/dashboard/hosting.controller.js
+++ packages/manager/apps/web/client/app/hosting/dashboard/hosting.controller.js
@@ -8,6 +8,8 @@ import set from 'lodash/set';
 import some from 'lodash/some';
 import union from 'lodash/union';
 
+import { HOSTING_CDN_ORDER_CDN_VERSION_V1 } from '../cdn/order/hosting-cdn-order.constant';
+
 export default class {
   /* @ngInject */
   constructor(
@@ -70,12 +72,16 @@ export default class {
     userLogsLink,
     HOSTING_STATUS,
     OVH_ORDER_URLS,
+    HOSTING_OPERATION_STATUS,
+    HOSTING_FLUSH_STATE,
   ) {
     this.$http = $http;
     this.$scope = $scope;
     this.$scope.cdnProperties = cdnProperties;
     this.$scope.cdnRange = cdnRange;
     this.$scope.HOSTING_STATUS = HOSTING_STATUS;
+    this.$scope.HOSTING_OPERATION_STATUS = HOSTING_OPERATION_STATUS;
+    this.$scope.HOSTING_FLUSH_STATE = HOSTING_FLUSH_STATE;
     this.$scope.logs = logs;
     this.$rootScope = $rootScope;
     this.$location = $location;
@@ -291,8 +297,8 @@ export default class {
       }
     };
 
-    this.$scope.$on('hosting.cdn.flush.refresh', () => {
-      this.checkFlushCdnState();
+    this.$scope.$on('hosting.cdn.flush.refresh', (params, operation) => {
+      this.checkFlushCdnState(operation);
     });
 
     this.$scope.$on('$destroy', () => {
@@ -527,30 +533,84 @@ export default class {
     );
   }
 
-  // FLUSH CDN
-  checkFlushCdnState() {
-    this.$scope.flushCdnState = 'check';
-    this.Hosting.checkTaskUnique(this.$stateParams.productId, 'cdn/flush').then(
-      (taskIds) => {
-        if (taskIds && taskIds.length) {
-          this.$scope.flushCdnState = 'doing';
+  /**
+   * call this to execute right check function
+   * @param operation {Object}: operation object, like a task
+   * @returns {Promise}: task or operation promise
+   */
+  checkFlushCdnState(operation) {
+    const { version } = this.$scope.cdnProperties;
+    const isV1Cdn = version === HOSTING_CDN_ORDER_CDN_VERSION_V1;
+
+    if (isV1Cdn) {
+      return this.checkCdnV1Flush();
+    }
+
+    return this.checkCdnV2Flush(operation);
+  }
+
+  checkCdnV1Flush() {
+    this.$scope.flushCdnState = this.$scope.HOSTING_FLUSH_STATE.CHECK;
+
+    return this.Hosting.checkTaskUnique(
+      this.$stateParams.productId,
+      'web/cdn/flush',
+    ).then((taskIds) => {
+      if (taskIds && taskIds.length) {
+        this.$scope.flushCdnState = this.$scope.HOSTING_FLUSH_STATE.DOING;
+        this.$rootScope.$broadcast(this.Hosting.events.tasksChanged);
+        this.Hosting.pollFlushCdn(this.$stateParams.productId, taskIds).then(
+          () => {
+            this.displaySuccessFlushMessage();
+          },
+        );
+      } else {
+        this.$scope.flushCdnState = this.$scope.HOSTING_FLUSH_STATE.OK;
+      }
+    });
+  }
+
+  checkCdnV2Flush(operation) {
+    this.$scope.flushCdnState = this.$scope.HOSTING_FLUSH_STATE.CHECK;
+
+    return this.Hosting.checkSharedCdnOperations(
+      this.$stateParams.productId,
+      'domain_purge',
+    ).then((operations) => {
+      // To cover case where the cache is very low (quickly flushed)
+      // We have to check if the current operation was completed or not
+      const operationIsFinished = operations.all.some(({ id, status }) => {
+        return (
+          id === operation.id &&
+          status === this.$scope.HOSTING_OPERATION_STATUS.DONE
+        );
+      });
+      if (operationIsFinished) this.displaySuccessFlushMessage();
+
+      // To cover active operations(inprogress) that are taken more time
+      if (operations.active.length) {
+        this.$scope.flushCdnState = this.$scope.HOSTING_FLUSH_STATE.DOING;
+        this.$rootScope.$broadcast(this.Hosting.events.tasksChanged);
+
+        this.Hosting.pollSharedFlushCdn(
+          this.$stateParams.productId,
+          operations.active.map(({ id }) => id),
+        ).then(() => {
           this.$rootScope.$broadcast(this.Hosting.events.tasksChanged);
-          this.Hosting.pollFlushCdn(this.$stateParams.productId, taskIds).then(
-            () => {
-              this.$rootScope.$broadcast(this.Hosting.events.tasksChanged);
-              this.$scope.flushCdnState = 'ok';
-              this.Alerter.success(
-                this.$translate.instant(
-                  'hosting_dashboard_cdn_flush_done_success',
-                ),
-                this.$scope.alerts.main,
-              );
-            },
-          );
-        } else {
-          this.$scope.flushCdnState = 'ok';
-        }
-      },
+          this.displaySuccessFlushMessage();
+        });
+      } else {
+        this.$scope.flushCdnState = this.$scope.HOSTING_FLUSH_STATE.OK;
+      }
+    });
+  }
+
+  displaySuccessFlushMessage() {
+    this.$rootScope.$broadcast(this.Hosting.events.tasksChanged);
+    this.$scope.flushCdnState = this.$scope.HOSTING_FLUSH_STATE.OK;
+    this.Alerter.success(
+      this.$translate.instant('hosting_dashboard_cdn_flush_done_success'),
+      this.$scope.alerts.main,
     );
   }
 
