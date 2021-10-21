19d037837f1f198b92bda82869a96bf3886f2e93
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2021-10-21 09:48:56 +0200

    fix: pr comments
    PUD-972
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/databases/database/replications/replications.module.js
+++ packages/manager/modules/pci/src/projects/project/storages/databases/database/replications/replications.module.js
@@ -1,7 +1,6 @@
 import angular from 'angular';
 
 import '@ovh-ux/ng-ovh-cloud-universe-components';
-import '@ovh-ux/ng-ovh-swimming-poll';
 import '@ovh-ux/ui-kit';
 
 import component from './replications.component';
@@ -10,11 +9,7 @@ import routing from './replications.routing';
 const moduleName = 'ovhManagerPciStoragesDatabasesReplications';
 
 angular
-  .module(moduleName, [
-    'ngOvhCloudUniverseComponents',
-    'ngOvhSwimmingPoll',
-    'oui',
-  ])
+  .module(moduleName, ['ngOvhCloudUniverseComponents', 'oui'])
   .config(routing)
   .component('ovhManagerPciStoragesDatabaseReplicationsComponent', component)
   .run(/* @ngTranslationsInject:json ./translations */);
