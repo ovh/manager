b0b9bbf2bd4d335d2a1fa9734a066e95490d12c3
frenauvh <florian.renaut@corp.ovh.com>
2021-10-21 16:34:15 +0200

    feat(web): use ovh-shell
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/apps/web/client/app/index.js
+++ packages/manager/apps/web/client/app/index.js
@@ -4,17 +4,19 @@ import {
   attach as attachPreloader,
   displayMessage,
 } from '@ovh-ux/manager-preloader';
-import { registerApplication } from '@ovh-ux/ufrontend';
 import { findAvailableLocale, detectUserLocale } from '@ovh-ux/manager-config';
+import { client as shellClient } from '@ovh-ux/shell';
 
 attachPreloader(findAvailableLocale(detectUserLocale()));
 
-registerApplication('web').then(({ environment }) => {
+shellClient.init('web').then(({ environment, shell }) => {
   environment.setVersion(__VERSION__);
 
   if (environment.getMessage()) {
     displayMessage(environment.getMessage(), environment.getUserLanguage());
   }
+
+  shell.routing.init();
 
   import(`./config-${environment.getRegion()}`)
     .catch(() => {})
