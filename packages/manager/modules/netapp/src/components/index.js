29f023dbbc06440dd6d26dae1e29d0d2c24209fa
JeremyDec <jeremy.de-cesare@ovhcloud.com>
2021-10-22 11:50:05 +0200

    feat(netapp.snpashot-policies): add snapshot policies list component
    ref: MANAGER-7153
    
    Signed-off-by: JeremyDec <jeremy.de-cesare@ovhcloud.com>

--- packages/manager/modules/netapp/src/components/index.js
+++ packages/manager/modules/netapp/src/components/index.js
@@ -1,9 +1,10 @@
 import angular from 'angular';
 
 import selectCheckboxes from './select-checkboxes';
+import snapshotList from './snapshot-policies';
 
 const moduleName = 'ovhManagerNetAppComponents';
 
-angular.module(moduleName, [selectCheckboxes]);
+angular.module(moduleName, [selectCheckboxes, snapshotList]);
 
 export default moduleName;
