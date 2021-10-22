e7ec24b6820ce4eeb2d86647c203134aa28d5d87
Maël LE GAL <mael.le-gal@corp.ovh.com>
2021-10-22 16:45:08 +0200

    feat(ai-training-notebooks): add possibility to attach ssh keys

--- packages/manager/modules/pci/src/projects/project/training/training.constants.js
+++ packages/manager/modules/pci/src/projects/project/training/training.constants.js
@@ -1,6 +1,7 @@
 export const GUIDE_URL = 'https://docs.ovh.com/gb/en/ai-training';
 export const COMMUNITY_URL = 'https://community.ovh.com/en/c/Data-AI';
 export const DISCORD_URL = 'https://discord.com/invite/vXVurFfwe9';
+export const JOB_MAX_SSH_KEYS = 10;
 export const DOC_DOCKER_BUILD_URL =
   'https://docs.ovh.com/gb/en/ai-training/build-use-custom-image/';
 
@@ -9,4 +10,5 @@ export default {
   COMMUNITY_URL,
   DISCORD_URL,
   DOC_DOCKER_BUILD_URL,
+  JOB_MAX_SSH_KEYS,
 };
