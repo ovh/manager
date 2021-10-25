12b232403ed4c8df3848d5f796b0e502ff1e03b6
Ganesh Kumar <ganesh.kumar@ovhcloud.com>
2021-10-25 13:47:32 +0530

    fix(web.hosting.cdn): add tracking
    Ref #MANAGER-7850
    
    Signed-off-by: Ganesh Kumar <ganesh.kumar@ovhcloud.com>
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/apps/web/client/app/hosting/cdn/order/hosting-cdn-order.routing.js
+++ packages/manager/apps/web/client/app/hosting/cdn/order/hosting-cdn-order.routing.js
@@ -4,6 +4,7 @@ import {
 } from '@ovh-ux/manager-product-offers';
 
 import {
+  HOSTING_CDN_CHANGE_TYPE,
   HOSTING_CDN_ORDER_CATALOG_ADDONS_PLAN_CODE_CDN_BUSINESS,
   HOSTING_CDN_ORDER_CATALOG_ADDONS_PLAN_CODE_CDN_BUSINESS_FREE,
   HOSTING_CDN_ORDER_CDN_VERSION_V1,
@@ -56,6 +57,35 @@ export default /* @ngInject */ ($stateProvider) => {
         'danger',
       ),
 
+    getChangeType: /* @ngInject */ (availablePlans) => (
+      oldPlanCode,
+      newPlanCode,
+    ) => {
+      const oldPlanIndex = availablePlans.findIndex(
+        (plan) => plan.planCode === oldPlanCode,
+      );
+      const newPlanIndex = availablePlans.findIndex(
+        (plan) => plan.planCode === newPlanCode,
+      );
+      return oldPlanIndex < newPlanIndex
+        ? HOSTING_CDN_CHANGE_TYPE.UPGRADE
+        : HOSTING_CDN_CHANGE_TYPE.DOWNGRADE;
+    },
+
+    getChangeDetails: /* @ngInject */ (
+      cdnProperties,
+      getChangeType,
+      workflowOptions,
+    ) => () => {
+      const oldPlanCode = cdnProperties.type;
+      const newPlanCode = workflowOptions.getPlanCode();
+      return {
+        changeType: getChangeType(oldPlanCode, newPlanCode),
+        oldPlanCode,
+        newPlanCode,
+      };
+    },
+
     serviceName: /* @ngInject */ ($transition$) =>
       $transition$.params().productId,
 
@@ -92,8 +122,18 @@ export default /* @ngInject */ ($stateProvider) => {
       return null;
     },
 
-    onError: /* @ngInject */ (goBackWithError) => (error) =>
-      goBackWithError(error.data?.message || error),
+    onError: /* @ngInject */ (
+      atInternet,
+      getChangeDetails,
+      goBackWithError,
+    ) => (error) => {
+      const changeDetails = getChangeDetails();
+      atInternet.trackPage({
+        name: `web_hosting_cdn_order::${changeDetails.changeType}_error::${changeDetails.oldPlanCode}::${changeDetails.newPlanCode}`,
+        type: 'navigation',
+      });
+      return goBackWithError(error.data?.message || error);
+    },
 
     trackClick: /* @ngInject */ (atInternet) => (hit) => {
       atInternet.trackClick({
@@ -104,25 +144,49 @@ export default /* @ngInject */ ($stateProvider) => {
   };
 
   const resolveOrder = {
-    onSuccess: /* @ngInject */ ($translate, goBack) => (checkout) => {
+    onSuccess: /* @ngInject */ (
+      $translate,
+      atInternet,
+      getChangeDetails,
+      goBack,
+    ) => (checkout) => {
       const message = checkout.autoPayWithPreferredPaymentMethod
         ? $translate.instant('hosting_dashboard_cdn_order_success_activation')
         : $translate.instant('hosting_dashboard_cdn_v2_order_success', {
             t0: checkout.url,
           });
+      const changeDetails = getChangeDetails();
+      atInternet.trackPage({
+        name: `web_hosting_cdn_order::${changeDetails.changeType}_validate::${changeDetails.oldPlanCode}::${changeDetails.newPlanCode}`,
+        type: 'navigation',
+      });
       return goBack(message);
     },
 
     pricingType: () => pricingConstants.PRICING_CAPACITIES.RENEW,
     workflowType: () => workflowConstants.WORKFLOW_TYPES.ORDER,
-    workflowOptions: /* @ngInject */ (catalog, serviceName, trackClick) => ({
+    workflowOptions: /* @ngInject */ (
+      catalog,
+      cdnProperties,
+      getChangeType,
+      serviceName,
+      trackClick,
+    ) => ({
       catalog,
       catalogItemTypeName: workflowConstants.CATALOG_ITEM_TYPE_NAMES.ADDON,
       onPricingSubmit: () => {
         trackClick('web::hosting::cdn::order::next');
       },
-      onValidateSubmit: () => {
+      onValidateSubmit() {
+        const oldPlanCode = cdnProperties.type;
+        const newPlanCode = this.getPlanCode();
         trackClick('web::hosting::cdn::order::confirm');
+        trackClick(
+          `web_hosting_cdn_order::${getChangeType(
+            oldPlanCode,
+            newPlanCode,
+          )}::${oldPlanCode}::${newPlanCode}`,
+        );
       },
       productName: HOSTING_PRODUCT_NAME,
       serviceNameToAddProduct: serviceName,
@@ -132,18 +196,30 @@ export default /* @ngInject */ ($stateProvider) => {
       $translate.instant('hosting_cdn_order_breadcrumb'),
   };
   const resolveUpgrade = {
-    onSuccess: /* @ngInject */ ($translate, goBack) => (result) => {
+    onSuccess: /* @ngInject */ (
+      $translate,
+      atInternet,
+      getChangeDetails,
+      goBack,
+    ) => (result) => {
       const message = result.autoPayWithPreferredPaymentMethod
         ? $translate.instant('hosting_dashboard_cdn_upgrade_included_success')
         : $translate.instant('hosting_dashboard_cdn_v2_order_success', {
             t0: result.url,
           });
+      const changeDetails = getChangeDetails();
+      atInternet.trackPage({
+        name: `web_hosting_cdn_order::${changeDetails.changeType}_validate::${changeDetails.oldPlanCode}::${changeDetails.newPlanCode}`,
+        type: 'navigation',
+      });
       return goBack(message);
     },
 
     pricingType: () => pricingConstants.PRICING_CAPACITIES.UPGRADE,
     workflowType: () => workflowConstants.WORKFLOW_TYPES.SERVICES,
     workflowOptions: /* @ngInject */ (
+      cdnProperties,
+      getChangeType,
       ovhManagerProductOffersActionService,
       serviceInfo,
       trackClick,
@@ -174,8 +250,16 @@ export default /* @ngInject */ ($stateProvider) => {
             onPricingSubmit: () => {
               trackClick('web::hosting::cdn::order::next');
             },
-            onValidateSubmit: () => {
+            onValidateSubmit() {
+              const oldPlanCode = cdnProperties.type;
+              const newPlanCode = this.getPlanCode();
               trackClick('web::hosting::cdn::order::confirm');
+              trackClick(
+                `web_hosting_cdn_order::${getChangeType(
+                  oldPlanCode,
+                  newPlanCode,
+                )}::${oldPlanCode}::${newPlanCode}`,
+              );
             },
             serviceId: upgradeServiceId,
           };
