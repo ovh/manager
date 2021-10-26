51eb1f17479f5ea15a2aaf334d383da45a5ec2c2
Ganesh Kumar <ganeshkumar_1989@yahoo.co.in>
2021-10-26 14:27:35 +0530

    fix(dedicated): disable features in dedicated server for trusted nic
    ref: MANAGER-7130
    
    Signed-off-by: Ganesh Kumar <ganesh.kumar@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/dedicated/server/bandwidth/private-order/private-order.module.js
+++ packages/manager/apps/dedicated/client/app/dedicated/server/bandwidth/private-order/private-order.module.js
@@ -1,10 +1,12 @@
+import ovhManagerCatalogPrice from '@ovh-ux/manager-catalog-price';
+
 import component from './private-order.component';
 import routing from './private-order.routing';
 
 const moduleName = 'ovhManagerDedicatedServerBandwidthPrivateOrder';
 
 angular
-  .module(moduleName, ['ui.router'])
+  .module(moduleName, ['ui.router', ovhManagerCatalogPrice])
   .component('dedicatedServerBandwidthPrivateOrder', component)
   .config(routing);
 
