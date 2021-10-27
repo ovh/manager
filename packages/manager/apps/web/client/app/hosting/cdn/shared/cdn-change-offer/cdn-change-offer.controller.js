d28c2eaafecfc1bdfcbd36d6db0d5ade5cdda2d4
mzahas <mohammed.zahaf.ext@ovhcloud.com>
2021-10-27 09:12:39 +0200

    feat(web.hosting.cdn.shared.change-offer): add offer module
    ref: MANAGER-7781
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

---
+++ packages/manager/apps/web/client/app/hosting/cdn/shared/cdn-change-offer/cdn-change-offer.controller.js
@@ -0,0 +1,21 @@
+export default class HostingCdnSharedChangeOfferController {
+  onCdnChangeOfferConfirm() {
+    this.trackClick(
+      'web::hosting::cdn::configure::activate_advanced_purge::upgrade_confirm',
+    );
+
+    this.loading = true;
+    return this.goToCdnChangeOffer().finally(() => {
+      this.loading = false;
+    });
+  }
+
+  onCdnChangeOfferCancel() {
+    this.trackClick(
+      'web::hosting::cdn::configure::activate_advanced_purge::upgrade_error',
+    );
+
+    this.model.options.cache.advanced_purge.enabled = false;
+    return this.goBack();
+  }
+}
