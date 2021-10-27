03230f966281a3f2f619b3b3a6be0ad1f5a06c9c
varun257 <varun257@gmail.com>
2021-10-27 10:08:25 +0530

    fix(nutanix): add onboarding page
    resolves #MANAGER-7852
    
    Signed-off-by: varun257 <varun257@gmail.com>

--- packages/manager/modules/nutanix/src/onboarding/controller.js
+++ packages/manager/modules/nutanix/src/onboarding/controller.js
@@ -1,7 +1,23 @@
+import reduce from 'lodash/reduce';
 import { GUIDES } from './constants';
+import illustration from './assets/Nutanix.png';
 
 export default class NutanixOnboardingCtrl {
   constructor() {
     this.GUIDES = GUIDES;
+  }
+
+  $onInit() {
+    this.guides = reduce(
+      GUIDES,
+      (list, guide) => [
+        ...list,
+        {
+          ...guide,
+        },
+      ],
+      [],
+    );
+    this.illustration = illustration;
   }
 }
