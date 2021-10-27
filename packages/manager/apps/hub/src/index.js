31579457be86ae50568c02c4579b224ae1014e37
frenauvh <florian.renaut@corp.ovh.com>
2021-10-27 15:37:41 +0200

    feat(hub): use ovh-shell client
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/apps/hub/src/index.js
+++ packages/manager/apps/hub/src/index.js
@@ -2,27 +2,32 @@ import 'script-loader!jquery'; // eslint-disable-line
 import 'whatwg-fetch';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import { client as shellClient } from '@ovh-ux/shell';
+import { useShellClient } from '@ovh-ux/shell';
 
 import {
   attach as attachPreloader,
   displayMessage,
 } from '@ovh-ux/manager-preloader';
 
-import { registerApplication, buildURL } from '@ovh-ux/ufrontend';
-import { findAvailableLocale, detectUserLocale } from '@ovh-ux/manager-config';
+import { buildURL } from '@ovh-ux/ufrontend';
+import {
+  fetchConfiguration,
+  findAvailableLocale,
+  detectUserLocale,
+} from '@ovh-ux/manager-config';
 import { BILLING_REDIRECTIONS } from './constants';
 
 attachPreloader(findAvailableLocale(detectUserLocale()));
 
-shellClient.useShellApi().routing.init();
-
-registerApplication('hub').then(({ environment }) => {
+fetchConfiguration('hub').then((environment) => {
   environment.setVersion(__VERSION__);
 
   if (environment.getMessage()) {
     displayMessage(environment.getMessage(), environment.getUserLanguage());
   }
+
+  const shellClient = useShellClient();
+  shellClient.routing.init();
 
   BILLING_REDIRECTIONS.forEach((redirectionRegex) => {
     const hash = window.location.hash.replace('#', '');
