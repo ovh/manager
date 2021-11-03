c77eb212b8f99035a2f95a7af434599b7445738a
Ganesh Kumar <ganeshkumar_1989@yahoo.co.in>
2021-11-03 13:20:22 +0530

    fix(billing.autorenew): track service type on resiliation (#5798)
    ref: MANAGER-7044
    
    Signed-off-by: Ganesh Kumar <ganesh.kumar@ovhcloud.com>

--- packages/manager/modules/billing/src/autoRenew/resiliation/resiliation.routing.js
+++ packages/manager/modules/billing/src/autoRenew/resiliation/resiliation.routing.js
@@ -1,8 +1,10 @@
+import kebabCase from 'lodash/kebabCase';
+
 import { BillingService as Service } from '@ovh-ux/manager-models';
 
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('app.account.billing.autorenew.resiliation', {
-    url: '/resiliation?serviceId&serviceName',
+    url: '/resiliation?serviceId&serviceName&serviceType',
     component: 'ovhManagerBillingResiliation',
     resolve: {
       availableStrategies: /* @ngInjecgt */ (endStrategyEnum, service) =>
@@ -30,10 +32,19 @@ export default /* @ngInject */ ($stateProvider) => {
         $transition$.params().serviceId,
       serviceName: /* @ngInject */ ($transition$) =>
         $transition$.params().serviceName,
+      serviceType: /* @ngInject */ ($transition$) =>
+        $transition$.params().serviceType,
       validate: /* @ngInject */ (BillingService, serviceId) => (strategy) =>
         BillingService.putEndRuleStrategy(serviceId, strategy),
       breadcrumb: /* @ngInject */ ($translate) =>
         $translate.instant('billing_autorenew_resiliate'),
+    },
+    atInternet: {
+      rename: /* @ngInject */ ($state) =>
+        // We're limited with the possible injection as we listen to onBefore hook
+        `dedicated::account::billing::autorenew::${kebabCase(
+          $state.transition.params().serviceType,
+        )}::resiliation`,
     },
   });
 };
