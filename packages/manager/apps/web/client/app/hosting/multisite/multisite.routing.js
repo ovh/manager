5c6da3a0fe488c9d55179a63b7d2f9253e374d05
Lucas Chaigne <lucas.chaigne@ovhcloud.com>
2021-11-04 09:53:18 +0100

    fix: webhosting multisite polling
    ref: MANAGER-3738
    
    Signed-off-by: Lucas Chaigne <lucas.chaigne@ovhcloud.com>

--- packages/manager/apps/web/client/app/hosting/multisite/multisite.routing.js
+++ packages/manager/apps/web/client/app/hosting/multisite/multisite.routing.js
@@ -3,7 +3,6 @@ import template from './MULTISITE.html';
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('app.hosting.dashboard.multisite', {
     url: '/multisite',
-    controller: 'HostingTabDomainsCtrl',
     template,
     resolve: {
       breadcrumb: /* @ngInject */ ($translate) =>
