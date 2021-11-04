af25cce4f7c0f028fdb3845f09579715277d2543
Zakaria Sahmane <77622270+zakariasahmane@users.noreply.github.com>
2021-11-04 10:10:07 +0100

    feat(sva): add sva profile tile and banner
    ref: MANAGER-7115
    
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

---
+++ packages/manager/apps/telecom/src/app/telecom/telephony/svaWallet/statusBanner/status-banner.component.js
@@ -0,0 +1,10 @@
+import template from './status-banner.html';
+import controller from './status-banner.controller';
+
+export default {
+  template,
+  controller,
+  bindings: {
+    svaWallet: '<',
+  },
+};
--- packages/manager/apps/telecom/src/app/telecom/telephony/svaWallet/sva-wallet.constants.js
+++ packages/manager/apps/telecom/src/app/telecom/telephony/svaWallet/sva-wallet.constants.js
@@ -16,6 +16,14 @@ export const getEnumItemValue = (item) =>
 
 export const isKYCValid = (wallet) => wallet.kycStatus === 'VALID';
 export const isKYCUnderReview = (wallet) => wallet.kycStatus === 'UNDER_REVIEW';
+export const isKYCWaitingDocument = (wallet) =>
+  wallet.kycStatus === 'WAITING_FOR_DOCUMENT';
+export const isKYCPending = (wallet) =>
+  ['IN_PROGRESS', 'UNDER_REVIEW', 'WAITING_FOR_DOCUMENT'].includes(
+    wallet.kycStatus,
+  );
+export const isKYCInError = (wallet) =>
+  ['BLOCKED', 'EXPIRED'].includes(wallet.kycStatus);
 
 export default {
   buildEnumList,
