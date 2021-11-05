13cd1efafe553775e7aa575c96e87a93dfb24c1f
Marie JONES <marie.jones@corp.ovh.com>
2021-11-05 16:17:41 +0100

    feat(volumes.snapshot): add policy management
    ref: MANAGER-7512
    
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/netapp/src/dashboard/snapshot-policies/routing.js
+++ packages/manager/modules/netapp/src/dashboard/snapshot-policies/routing.js
@@ -1,5 +1,3 @@
-import SnapshotPolicy from '../SnapshotPolicy.class';
-
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('netapp.dashboard.snapshotPolicies', {
     url: '/snapshotPolicies',
@@ -12,30 +10,8 @@ export default /* @ngInject */ ($stateProvider) => {
     resolve: {
       goToCreateSnapshotPolicies: /* @ngInject */ ($state) => () =>
         $state.go('netapp.dashboard.snapshotPolicies.create'),
-      getSnapshotPolicies: /* @ngInject */ ($http, $q, serviceName) => () =>
-        $http
-          .get(`/storage/netapp/${serviceName}/snapshotPolicy`)
-          .then(({ data: snapshotPolicyIds }) =>
-            $q
-              .all(
-                snapshotPolicyIds.map(({ id }) =>
-                  $http
-                    .get(`/storage/netapp/${serviceName}/snapshotPolicy/${id}`)
-                    .then(
-                      ({ data: snapshotPolicy }) =>
-                        new SnapshotPolicy(snapshotPolicy),
-                    ),
-                ),
-              )
-              .then((snapshotPolicies) =>
-                snapshotPolicies.filter(
-                  (snapshotPolicy) => !snapshotPolicy.isDeleting(),
-                ),
-              ),
-          ),
       snapshotPolicies: /* @ngInject */ (getSnapshotPolicies) =>
         getSnapshotPolicies(),
-
       breadcrumb: /* @ngInject */ ($translate) =>
         $translate.instant('netapp_snapshot_policies_breadcrumb'),
     },
--- packages/manager/modules/netapp/src/dashboard/volumes/dashboard/snapshots/controller.js
+++ packages/manager/modules/netapp/src/dashboard/volumes/dashboard/snapshots/controller.js
@@ -2,7 +2,31 @@ import { MAXIMUM_SNAPSHOT_ALLOWED } from './constants';
 
 export default class NetAppVolumesDashboardSnapshotsController {
   /* @ngInject */
-  constructor() {
+  constructor(Alerter, $translate) {
+    this.Alerter = Alerter;
+    this.$translate = $translate;
     this.MAXIMUM_SNAPSHOT_ALLOWED = MAXIMUM_SNAPSHOT_ALLOWED;
+  }
+
+  $onInit() {
+    this.policyId = this.currentPolicy.id;
+  }
+
+  changePolicy() {
+    return this.applyPolicy(this.policyId)
+      .then(() =>
+        this.Alerter.success(
+          this.$translate.instant(
+            'netapp_volumes_snapshots_policies_apply_success',
+          ),
+        ),
+      )
+      .catch((error) =>
+        this.Alerter.error(
+          `${this.$translate.instant(
+            'netapp_volumes_snapshots_policies_apply_error',
+          )} ${error.message}`,
+        ),
+      );
   }
 }
