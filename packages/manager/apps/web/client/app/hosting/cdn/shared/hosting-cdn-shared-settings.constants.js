eebf2789d159ce38748487a933cbccf3d7a05152
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2021-11-09 15:12:29 +0100

    fix(web.hosting.cdn.shared): update prefetch description
    ref: MANAGER-7984
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/apps/web/client/app/hosting/cdn/shared/hosting-cdn-shared-settings.constants.js
+++ packages/manager/apps/web/client/app/hosting/cdn/shared/hosting-cdn-shared-settings.constants.js
@@ -30,6 +30,45 @@ export const SHARED_CDN_RANGE = {
 };
 
 export const SHARED_CDN_OPTIONS = {
+  PREFETCH: {
+    LINKS: {
+      DEFAULT:
+        'https://docs.ovh.com/gb/en/hosting/guide_to_using_the_geocache_accelerator_on_a_web_hosting_package/',
+      ASIA:
+        'https://docs.ovh.com/asia/en/hosting/guide_to_using_the_geocache_accelerator_on_a_web_hosting_package/',
+      AU:
+        'https://docs.ovh.com/au/en/hosting/guide_to_using_the_geocache_accelerator_on_a_web_hosting_package/',
+      CA:
+        'https://docs.ovh.com/ca/en/hosting/guide_to_using_the_geocache_accelerator_on_a_web_hosting_package/',
+      DE:
+        'https://docs.ovh.com/de/hosting/verwendung_des_geocache_boosters_auf_einem_webhosting/',
+      ES:
+        'https://docs.ovh.com/es/hosting/guia_de_uso_del_acelerador_geocache_en_un_alojamiento_web/',
+      FR:
+        'https://docs.ovh.com/fr/hosting/accelerer-mon-site-web-en-utilisant-le-cdn/',
+      GB:
+        'https://docs.ovh.com/gb/en/hosting/guide_to_using_the_geocache_accelerator_on_a_web_hosting_package/',
+      IE:
+        'https://docs.ovh.com/ie/en/hosting/guide_to_using_the_geocache_accelerator_on_a_web_hosting_package/',
+      IT:
+        'https://docs.ovh.com/it/hosting/guida_allutilizzo_dellacceleratore_geocache_su_un_hosting_web/',
+      PL:
+        'https://docs.ovh.com/pl/hosting/przewodnik_dotyczacy_uslugi_geocache_na_hostingu_www/',
+      PT:
+        'https://docs.ovh.com/pt/hosting/guia_de_utilizacao_do_acelerador_geocache_num_alojamento_web/',
+      QC:
+        'https://docs.ovh.com/ca/fr/hosting/accelerer-mon-site-web-en-utilisant-le-cdn/',
+      SG:
+        'https://docs.ovh.com/sg/en/hosting/guide_to_using_the_geocache_accelerator_on_a_web_hosting_package/',
+      WE:
+        'https://docs.ovh.com/us/en/hosting/guide_to_using_the_geocache_accelerator_on_a_web_hosting_package/',
+      WS:
+        'https://docs.ovh.com/ca/en/hosting/guide_to_using_the_geocache_accelerator_on_a_web_hosting_package/',
+    },
+  },
+  HSTS: {
+    FIXED_NUMBER: 2,
+  },
   QUERY_STRING: {
     SORT_PARAMS: { SORT: 'sorted', IGNORED: 'ignored' },
   },
