b1059e9790cc248f5869aacd7dd76868326aee3d
Jisay <jean-christophe.alleman@corp.ovh.com>
2021-11-10 17:19:48 +0100

    feat(ovh-shell): change init shell method with direct message bus
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

--- packages/components/ovh-shell/src/shell/index.ts
+++ packages/components/ovh-shell/src/shell/index.ts
@@ -1,8 +1,9 @@
 import Shell from './shell';
 import IFrameMessageBus from '../message-bus/iframe';
+import DirectClientMessageBus from '../message-bus/direct-client';
 
-export function initShell(iframe: HTMLIFrameElement) {
-  const shell = new Shell(new IFrameMessageBus(iframe));
+export function initShell() {
+  const shell = new Shell(new DirectClientMessageBus());
   return {
     emitEvent: (eventId: string, data: unknown): void =>
       shell.emitEvent(eventId, data),
@@ -12,6 +13,9 @@ export function initShell(iframe: HTMLIFrameElement) {
     ) => shell.getPluginManager().registerPlugin(pluginId, pluginApi),
     setPluginAvailability: (pluginId: string, availability: boolean) =>
       shell.getPluginManager().setPluginAvailability(pluginId, availability),
+    setIframeMessageBus: (iframe: HTMLIFrameElement) =>
+      shell.setMessageBus(new IFrameMessageBus(iframe)),
+    i18n: () => shell.getPluginManager().plugins.i18n.instance,
   };
 }
 
