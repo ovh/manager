955396aa1e8530f2bfa35d117774adb71bc86b00
frenauvh <florian.renaut@corp.ovh.com>
2021-11-17 16:28:18 +0100

    feat(ovh-shell): add authentication plugin
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

--- packages/components/ovh-shell/src/plugin/index.ts
+++ packages/components/ovh-shell/src/plugin/index.ts
@@ -1,5 +1,6 @@
+import auth from './auth';
 import environment from './environment';
 import i18n from './i18n';
 import routing from './routing';
 
-export default { environment, i18n, routing };
+export default { auth, environment, i18n, routing };
