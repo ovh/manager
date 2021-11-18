66201597d51570e56fb32fec48628dd794c01b04
JeremyDec <jeremy.de-cesare@ovhcloud.com>
2021-11-18 11:10:04 +0100

    fix(support.tickets.new): remove permissive filter
    The support ticket creation in meant to be removed in the manager application. Until then, the basic linky angularjs filter will be used again. This way the ticket rendering in manager will matches the same rendering in CSM tool.
    
    ref: MANAGER-8099
    
    Signed-off-by: JeremyDec <jeremy.de-cesare@ovhcloud.com>

--- packages/manager/modules/support/src/tickets/ticket/message/index.js
+++ packages/manager/modules/support/src/tickets/ticket/message/index.js
@@ -2,7 +2,6 @@ import angular from 'angular';
 
 import component from './message.component';
 import service from './message.service';
-import htmlStringLinky from './helpers/message.linky';
 
 import './message.scss';
 
@@ -11,7 +10,6 @@ const moduleName = 'ovhManagerSupportTicketMessage';
 angular
   .module(moduleName, [])
   .component(component.name, component)
-  .filter('htmlStringLinky', htmlStringLinky)
   .service('ticketMessageService', service);
 
 export default moduleName;
