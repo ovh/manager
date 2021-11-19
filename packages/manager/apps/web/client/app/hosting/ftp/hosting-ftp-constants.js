3bdff331ba6d4a2f0007cfed54f277087d0a3590
Lucas Chaigne <lucas.chaigne@ovhcloud.com>
2021-11-19 13:57:53 +0100

    feat(hosting.ftp): add sftp information in user table (pro offer)
    ref: DTRSD-43496
    
    Signed-off-by: Lucas Chaigne <lucas.chaigne@ovhcloud.com>

---
+++ packages/manager/apps/web/client/app/hosting/ftp/hosting-ftp-constants.js
@@ -0,0 +1,15 @@
+export const SSH_STATE = {
+  ACTIVE: 'active',
+  SFTPONLY: 'sftponly',
+  NONE: 'none',
+};
+
+export const USER_STATE = {
+  OFF: 'off',
+  RW: 'rw',
+};
+
+export default {
+  SSH_STATE,
+  USER_STATE,
+};
