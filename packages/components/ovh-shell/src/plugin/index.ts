704f6d5ad793769ff9d7e77098e02313a3e4833a
Jisay <jean-christophe.alleman@corp.ovh.com>
2021-11-22 11:45:53 +0100

    feat(ovh-shell): add ux plugin
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

--- packages/components/ovh-shell/src/plugin/index.ts
+++ packages/components/ovh-shell/src/plugin/index.ts
@@ -2,5 +2,6 @@ import auth from './auth';
 import environment from './environment';
 import { i18n } from './i18n';
 import routing from './routing';
+import ux from './ux';
 
-export default { auth, environment, i18n, routing };
+export default { auth, environment, i18n, routing, ux };
