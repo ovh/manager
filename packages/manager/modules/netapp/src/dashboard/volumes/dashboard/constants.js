2e3bd75d082fed4110ea02fb796097d80af45ca5
Marie JONES <marie.jones@corp.ovh.com>
2021-11-23 11:33:03 +0100

    fix(volumes): specify volume name pattern
    ref: MANAGER-7856
    
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/netapp/src/dashboard/volumes/dashboard/constants.js
+++ packages/manager/modules/netapp/src/dashboard/volumes/dashboard/constants.js
@@ -2,8 +2,11 @@ export const REDHAT_COMMAND = 'sudo yum install nfs-utils';
 export const UBUNTU_COMMAND = 'sudo apt-get install nfs-common';
 export const FOLDER_CREATION = 'sudo mkdir /media/nfs01';
 
+export const PATTERN = /^[a-zA-Z0-9._-]{1,255}$/;
+
 export default {
   REDHAT_COMMAND,
   UBUNTU_COMMAND,
   FOLDER_CREATION,
+  PATTERN,
 };
