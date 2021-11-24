4054404b347fac25db0aed3f927d53f2dda6ab63
frenauvh <florian.renaut@corp.ovh.com>
2021-11-24 16:34:40 +0100

    feat(shell): update app shell
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/apps/shell/src/index.jsx
+++ packages/manager/apps/shell/src/index.jsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
-import { fetchConfiguration } from '@ovh-ux/manager-config';
 import { plugin, shell as shellApi } from '@ovh-ux/shell';
 
 import { initSso } from '@/core/sso';
@@ -16,34 +15,28 @@ import './index.scss';
 
 initSso();
 
-fetchConfiguration('shell')
-  .then((environment) => {
-    const locale = environment.getUserLocale();
-    i18n
-      .use(initReactI18next)
-      .use(Backend)
-      .init({
-        lng: locale,
-        fallbackLng: 'fr_FR',
-        ns: [], // namespaces to load by default
-        backend: {
-          // path construction for async load, ns: namespace, lng: locale
-          loadPath: './translations/{{ns}}/Messages_{{lng}}.json',
-        },
-      });
-    return environment;
-  })
-  .then((environment) => {
-    // init the Shell
-    const shell = shellApi.initShell();
-    shell.registerPlugin('i18n', plugin.i18n(shell, environment));
-
-    ReactDOM.render(
-      <React.StrictMode>
-        <ApplicationProvider environment={environment} shell={shell}>
-          <Shell />
-        </ApplicationProvider>
-      </React.StrictMode>,
-      document.querySelector('#app'),
-    );
-  });
+shellApi.initShell().then((shell) => {
+  const environment = shell.getEnvironment();
+  const locale = environment.getUserLocale();
+  i18n
+    .use(initReactI18next)
+    .use(Backend)
+    .init({
+      lng: locale,
+      fallbackLng: 'fr_FR',
+      ns: [], // namespaces to load by default
+      backend: {
+        // path construction for async load, ns: namespace, lng: locale
+        loadPath: './translations/{{ns}}/Messages_{{lng}}.json',
+      },
+    });
+  shell.registerPlugin('i18n', plugin.i18n(shell, environment));
+  ReactDOM.render(
+    <React.StrictMode>
+      <ApplicationProvider environment={environment} shell={shell}>
+        <Shell />
+      </ApplicationProvider>
+    </React.StrictMode>,
+    document.querySelector('#app'),
+  );
+});
