0dd288e8bcb7d88ee173cbc3ac49365ad22b3e77
Anoop N <anooparveti@gmail.com>
2021-11-30 22:52:13 +0530

    feat(veeam.order): modifications in auto-backup screen (#5935)
    Modifications in auto-backup screen for cross-sale campaign and added tracking for order and enable auto backup button click
    
    ref: MANAGER-7806
    
    Signed-off-by: Anoop <anooparveti@gmail.com>

--- packages/manager/modules/vps/src/veeam/vps-veeam.component.js
+++ packages/manager/modules/vps/src/veeam/vps-veeam.component.js
@@ -6,6 +6,7 @@ export default {
   bindings: {
     automatedBackupOption: '<',
     goToVeeamMount: '<',
+    goToVeeamOrderPage: '<',
     goToVeeamRestore: '<',
     goToVeeamScheduleBackup: '<',
     goToVpsUpgrade: '<',
