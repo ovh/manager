c9e1cb3c827f3ccbe7fde70fd85cd2316efdbd0d
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2021-12-02 18:00:29 +0100

    fix(identity-check): handle no billingAccount case
    ref: MANAGER-8251
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/modules/telecom-dashboard/src/identity-check/telecom-dashboard-identity-check.service.js
+++ packages/manager/modules/telecom-dashboard/src/identity-check/telecom-dashboard-identity-check.service.js
@@ -2,11 +2,30 @@ const inProgressStatuses = ['doing', 'todo', 'waiting_for_customer'];
 
 export default class {
   /* @ngInject */
-  constructor($http) {
+  constructor($http, $q) {
     this.$http = $http;
+    this.$q = $q;
   }
 
   isIdentityChecked() {
+    return this.$q
+      .all([this.hasBillingAccounts(), this.hasUntrustedBillingAccounts()])
+      .then(([has, hasUntrusted]) => has === true && hasUntrusted === false);
+  }
+
+  hasBillingAccounts() {
+    return this.$http
+      .get('/telephony', {
+        headers: {
+          'X-Pagination-Mode': 'CachedObjectList-Pages',
+          'X-Pagination-Size': 1,
+        },
+      })
+      .then((response) => response.headers('x-pagination-elements') !== '0')
+      .catch(() => null);
+  }
+
+  hasUntrustedBillingAccounts() {
     return this.$http
       .get('/telephony', {
         headers: {
@@ -15,9 +34,8 @@ export default class {
           'X-Pagination-Size': 1,
         },
       })
-      .then((response) => {
-        return response.headers('x-pagination-elements') === '0';
-      });
+      .then((response) => response.headers('x-pagination-elements') !== '0')
+      .catch(() => null);
   }
 
   getLastInProgressProcedure() {
