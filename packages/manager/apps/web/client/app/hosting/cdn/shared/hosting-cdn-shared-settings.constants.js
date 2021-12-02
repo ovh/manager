ad8406f63b7bb85a63ad2061d5a3a5c39bd9e748
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2021-12-02 17:37:13 +0100

    fix(web.hosting): remove useless banners
    ref: MANAGER-8244
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/apps/web/client/app/hosting/cdn/shared/hosting-cdn-shared-settings.constants.js
+++ packages/manager/apps/web/client/app/hosting/cdn/shared/hosting-cdn-shared-settings.constants.js
@@ -7,24 +7,6 @@ export const SHARED_CDN_SETTINGS_RULE_FACTOR_SECOND = 1;
 
 export const SHARED_CDN_SETTINGS_RULES_NB_RULES_BY_PAGE = 5;
 
-export const SHARED_CDN_GET_MORE_INFO = {
-  FR: 'https://www.ovh.com/fr/hebergement-web/cdn.xml',
-  DE: 'https://www.ovh.de/hosting/cdn.xml',
-  ES: 'https://www.ovh.es/hosting/cdn.xml',
-  IT: 'https://www.ovh.it/hosting-web/cdn.xml',
-  NL: 'https://www.ovh.nl/shared-hosting/cdn.xml',
-  PL: 'https://www.ovh.pl/hosting/cdn.xml',
-  PT: 'https://www.ovh.pt/alojamento-partilhado/cdn.xml',
-  GB: 'https://www.ovh.co.uk/web-hosting/cdn.xml',
-  WS: 'https://www.ovh.com/world/es/hosting/cdn.xml',
-  CA: 'https://www.ovh.com/ca/en/web-hosting/cdn.xml',
-  QC: 'https://www.ovh.com/ca/fr/hebergement-web/cdn.xml',
-  MA: 'https://www.ovh.com/ma/hebergement-web/',
-  SN: 'https://www.ovh.sn/hebergement-web/',
-  TN: 'https://www.ovh.com/tn/hebergement-web/',
-  IE: 'https://www.ovh.ie/web-hosting/cdn.xml',
-};
-
 export const SHARED_CDN_RANGE = {
   BASIC: 'BASIC',
   SECURITY: 'SECURITY',
