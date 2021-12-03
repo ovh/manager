dd7ba0efce13d21695541087c75860409d4e9429
frenauvh <florian.renaut@corp.ovh.com>
2021-12-03 10:50:07 +0100

    feat(ovh-shell): initial import
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>
    Co-authored-by: Jisay <jean-christophe.alleman@corp.ovh.com>
    Co-authored-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
    Co-authored-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

---
+++ packages/components/ovh-shell/src/shell/shell.ts
@@ -0,0 +1,73 @@
+import PluginManager from './plugin-manager';
+import IMessageBus from '../message-bus/IMessageBus';
+import {
+  IShellMessage,
+  IShellPluginInvocation,
+  ShellMessageType,
+} from '../common';
+
+export default class Shell {
+  pluginEventHandler: (event: MessageEvent) => void;
+
+  pluginManager: PluginManager;
+
+  messageBus: IMessageBus;
+
+  constructor() {
+    this.pluginManager = new PluginManager();
+    this.pluginEventHandler = null;
+    this.messageBus = null;
+  }
+
+  setMessageBus(bus: IMessageBus) {
+    this.messageBus = bus;
+    this.messageBus.onReceive((data: IShellMessage<unknown>) => {
+      if (data.type === ShellMessageType.PLUGIN_INVOCATION) {
+        this.handlePluginMessage(data.message as IShellPluginInvocation);
+      }
+    });
+  }
+
+  getPluginManager() {
+    return this.pluginManager;
+  }
+
+  emitEvent(eventId: string, data: unknown) {
+    if (this.messageBus) {
+      this.messageBus.send({
+        type: ShellMessageType.EVENT,
+        message: {
+          eventId,
+          data,
+        },
+      });
+    }
+  }
+
+  handlePluginMessage(data: IShellPluginInvocation) {
+    const onError = (error: Error) =>
+      this.messageBus &&
+      this.messageBus.send({
+        type: ShellMessageType.PLUGIN_RESULT,
+        message: {
+          uid: data.uid,
+          error,
+        },
+      });
+
+    const onSuccess = (success: unknown) =>
+      this.messageBus &&
+      this.messageBus.send({
+        type: ShellMessageType.PLUGIN_RESULT,
+        message: {
+          uid: data.uid,
+          success,
+        },
+      });
+
+    this.pluginManager
+      .invokePluginMethod(data as IShellPluginInvocation)
+      .then(onSuccess)
+      .catch(onError);
+  }
+}
