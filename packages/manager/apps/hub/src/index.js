b43f1ca74c2d86cacd7cb3d58e139569b8ed005f
frenauvh <florian.renaut@corp.ovh.com>
2021-12-03 11:01:17 +0100

    feat(hub): use ovh-shell
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>
    Co-authored-by: Jisay <jean-christophe.alleman@corp.ovh.com>
    Co-authored-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
    Co-authored-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/hub/src/index.js
+++ packages/manager/apps/hub/src/index.js
@@ -2,41 +2,46 @@ import 'script-loader!jquery'; // eslint-disable-line
 import 'whatwg-fetch';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import { useShellClient } from '@ovh-ux/shell';
 
 import {
   attach as attachPreloader,
   displayMessage,
 } from '@ovh-ux/manager-preloader';
 
-import { registerApplication, buildURL } from '@ovh-ux/ufrontend';
+import { buildURL } from '@ovh-ux/ufrontend';
 import { findAvailableLocale, detectUserLocale } from '@ovh-ux/manager-config';
 import { BILLING_REDIRECTIONS } from './constants';
 
 attachPreloader(findAvailableLocale(detectUserLocale()));
 
-registerApplication('hub').then(({ environment }) => {
-  environment.setVersion(__VERSION__);
+useShellClient('hub')
+  .then((shellClient) => {
+    return shellClient.environment.getEnvironment();
+  })
+  .then((environment) => {
+    environment.setVersion(__VERSION__);
 
-  if (environment.getMessage()) {
-    displayMessage(environment.getMessage(), environment.getUserLanguage());
-  }
-
-  BILLING_REDIRECTIONS.forEach((redirectionRegex) => {
-    const hash = window.location.hash.replace('#', '');
-    if (redirectionRegex.test(hash)) {
-      window.location.assign(
-        buildURL(
-          environment.getApplicationURL('dedicated'),
-          window.location.hash,
-        ),
-      );
+    if (environment.getMessage()) {
+      displayMessage(environment.getMessage(), environment.getUserLanguage());
     }
-  });
 
-  import(`./config-${environment.getRegion()}`)
-    .catch(() => {})
-    .then(() => import('./app.module'))
-    .then(({ default: startApplication }) => {
-      startApplication(document.body, environment);
+    BILLING_REDIRECTIONS.forEach((redirectionRegex) => {
+      const hash = window.location.hash.replace('#', '');
+      if (redirectionRegex.test(hash)) {
+        window.location.assign(
+          buildURL(
+            environment.getApplicationURL('dedicated'),
+            window.location.hash,
+          ),
+        );
+      }
     });
-});
+
+    import(`./config-${environment.getRegion()}`)
+      .catch(() => {})
+      .then(() => import('./app.module'))
+      .then(({ default: startApplication }) => {
+        startApplication(document.body, environment);
+      });
+  });
