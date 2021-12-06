f6c9a1dbbad5c065b027c70e0e5b9008e754587d
frenauvh <florian.renaut@corp.ovh.com>
2021-12-06 13:54:24 +0100

    refactor(ovh-shell): remove ishell interface
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/components/ovh-shell/src/shell/index.ts
+++ packages/components/ovh-shell/src/shell/index.ts
@@ -1,24 +1,10 @@
 import { Environment, fetchConfiguration } from '@ovh-ux/manager-config';
 
 import Shell from './shell';
-import IFrameMessageBus from '../message-bus/iframe';
 import DirectClientMessageBus from '../message-bus/direct-client';
-import { default as environmentPlugin } from '../plugin/environment';
+import environmentPlugin from '../plugin/environment';
 
-interface IShell {
-  getEnvironment: () => Environment;
-  getShell: () => Shell;
-  emitEvent: (eventId: string, data: unknown) => void;
-  registerPlugin: (
-    pluginId: string,
-    pluginApi: Record<string, CallableFunction>,
-  ) => void;
-  setPluginAvailability: (pluginId: string, availability: boolean) => void;
-  setIframeMessageBus: (iframe: HTMLIFrameElement) => void;
-  i18n: () => Record<string, CallableFunction>;
-}
-
-export function initShell(): Promise<IShell> {
+export function initShell(): Promise<Shell> {
   return fetchConfiguration('shell').then((environment: Environment) => {
     const shell = new Shell();
 
@@ -30,25 +16,7 @@ export function initShell(): Promise<IShell> {
       .getPluginManager()
       .registerPlugin('environment', environmentPlugin(environment));
 
-    return {
-      getEnvironment: () =>
-        shell
-          .getPluginManager()
-          .getPlugin('environment')
-          .getEnvironment(),
-      getShell: () => shell,
-      emitEvent: (eventId: string, data: unknown): void =>
-        shell.emitEvent(eventId, data),
-      registerPlugin: (
-        pluginId: string,
-        pluginApi: Record<string, CallableFunction>,
-      ) => shell.getPluginManager().registerPlugin(pluginId, pluginApi),
-      setPluginAvailability: (pluginId: string, availability: boolean) =>
-        shell.getPluginManager().setPluginAvailability(pluginId, availability),
-      setIframeMessageBus: (iframe: HTMLIFrameElement) =>
-        shell.setMessageBus(new IFrameMessageBus(iframe)),
-      i18n: () => shell.getPluginManager().getPlugin('i18n'),
-    };
+    return shell;
   });
 }
 
