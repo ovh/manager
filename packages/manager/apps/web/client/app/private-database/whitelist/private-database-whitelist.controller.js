3c22465882494a6e166fea1e236c49820d48c0bb
Lucas Chaigne <lucas.chaigne@ovhcloud.com>
2021-12-07 10:03:01 +0100

    feat(private-database): add WebHosting Network for CloudDB
    ref: PUDBLOW-1329
    
    Signed-off-by: Lucas Chaigne <lucas.chaigne@ovhcloud.com>

--- packages/manager/apps/web/client/app/private-database/whitelist/private-database-whitelist.controller.js
+++ packages/manager/apps/web/client/app/private-database/whitelist/private-database-whitelist.controller.js
@@ -1,5 +1,6 @@
 import find from 'lodash/find';
 import forEach from 'lodash/forEach';
+import { WEBHOSTING_NETWORK_STATUS } from './private-database-whitelist.constants';
 
 angular.module('App').controller(
   'PrivateDatabaseWhitelistCtrl',
@@ -8,6 +9,7 @@ angular.module('App').controller(
       Alerter,
       PrivateDatabase,
       $scope,
+      $http,
       $stateParams,
       $translate,
       WhitelistService,
@@ -15,6 +17,7 @@ angular.module('App').controller(
       this.alerter = Alerter;
       this.privateDatabaseService = PrivateDatabase;
       this.$scope = $scope;
+      this.$http = $http;
       this.$stateParams = $stateParams;
       this.$translate = $translate;
       this.whitelistService = WhitelistService;
@@ -25,10 +28,13 @@ angular.module('App').controller(
       this.serviceName = this.$stateParams.productId;
 
       this.getList();
+      this.getWebhostingNetworkStatus();
 
       this.privateDatabaseService.restartPoll(this.serviceName, [
         'whitelist/delete',
         'whitelist/create',
+        'webhostingNetwork/enable',
+        'webhostingNetwork/disable',
       ]);
 
       forEach(statusToWatch, (state) => {
@@ -54,6 +60,15 @@ angular.module('App').controller(
               : this.$scope.lockAction;
           },
         );
+
+        forEach(['enable', 'disable'], (action) => {
+          this.$scope.$on(
+            `privateDatabase.webhostingNetwork.${action}.${state}`,
+            (e, message) => {
+              this.handleWebhostingNetworkUpdate(state, message);
+            },
+          );
+        });
       });
 
       this.$scope.$on('privateDatabase.global.actions.start', (e, taskOpt) => {
@@ -191,6 +206,133 @@ angular.module('App').controller(
           todo,
         );
       }
+    }
+
+    getWebhostingNetworkStatus() {
+      return this.whitelistService
+        .getWebhostingNetworkStatus(this.serviceName)
+        .then((res) => {
+          this.webhostingNetworkStatus = res;
+
+          this.isWebhostingNetworkDisplayOn = [
+            WEBHOSTING_NETWORK_STATUS.enabling,
+            WEBHOSTING_NETWORK_STATUS.enabled,
+          ].includes(this.webhostingNetworkStatus);
+
+          this.isWebhostingNewtworkChangeDisabled = [
+            WEBHOSTING_NETWORK_STATUS.enabling,
+            WEBHOSTING_NETWORK_STATUS.disabling,
+          ].includes(this.webhostingNetworkStatus);
+        })
+        .catch((err) => this.alerter.error(err));
+    }
+
+    onWebhostingNetworkStatusChange() {
+      switch (this.webhostingNetworkStatus) {
+        case WEBHOSTING_NETWORK_STATUS.disabled:
+          this.enableWebhostingNetwork(this.serviceName);
+          break;
+        case WEBHOSTING_NETWORK_STATUS.enabled:
+          this.disableWebhostingNetwork(this.serviceName);
+          break;
+        default:
+          return;
+      }
+
+      this.isWebhostingNewtworkChangeDisabled = true;
+      this.alerter.success(
+        this.$translate.instant(
+          'private_database_tabs_whitelist_webhosting_network_access_success',
+        ),
+        this.$scope.alerts.main,
+      );
+    }
+
+    handleWebhostingNetworkUpdate(state, message) {
+      this.getWebhostingNetworkStatus();
+      this.isWebhostingNewtworkChangeDisabled = false;
+      if (state === 'error') {
+        this.alerter.error(message, this.$scope.alerts.main);
+      }
+    }
+
+    /**
+     * Enable webhosting access to database
+     * @param  {string} serviceName
+     * @return {Promise}
+     */
+    enableWebhostingNetwork(serviceName) {
+      return this.$http
+        .post(
+          `${this.whitelistService.rootPath}/${this.whitelistService.swsProxypassPath}/${serviceName}/webhostingNetwork`,
+        )
+        .then((res) => {
+          const taskId = res.data.id;
+          this.pollWebhostingNetwork(serviceName, taskId, 'enable');
+        })
+        .catch((err) => {
+          this.$scope.$broadcast(
+            'privateDatabase.webhostingNetwork.enable.error',
+            err.data.message,
+          );
+        });
+    }
+
+    /**
+     * Disable webhosting access to database
+     * @param  {string} serviceName
+     * @return {Promise}
+     */
+    disableWebhostingNetwork(serviceName) {
+      return this.$http
+        .delete(
+          `${this.whitelistService.rootPath}/${this.whitelistService.swsProxypassPath}/${serviceName}/webhostingNetwork`,
+        )
+        .then((res) => {
+          const taskId = res.data.id;
+          this.pollWebhostingNetwork(serviceName, taskId, 'disable');
+        })
+        .catch((err) => {
+          this.$scope.$broadcast(
+            'privateDatabase.webhostingNetwork.disable.error',
+            err.data.message,
+          );
+        });
+    }
+
+    /**
+     * Poll task to enable or disable webhosting access to database
+     * @param  {string} serviceName
+     * @param  {number} taskId
+     * @param  {string} eventType
+     * @return {Promise}
+     */
+    pollWebhostingNetwork(serviceName, taskId, eventType) {
+      const opts = {
+        namespace: `webhostingNetwork.${eventType}`,
+        taskId,
+      };
+
+      return this.privateDatabaseService
+        .poll(serviceName, opts)
+        .then((response) =>
+          this.webhostingNetworkEventBroadcast(eventType, 'done', response),
+        )
+        .catch((err) =>
+          this.webhostingNetworkEventBroadcast(eventType, 'error', err),
+        );
+    }
+
+    /**
+     * Broadcast end of task to enable or disable webhosting access to database
+     * @param  {string} eventType
+     * @param  {string} eventResult
+     */
+    webhostingNetworkEventBroadcast(eventType, eventResult, message) {
+      this.$scope.$broadcast(
+        `privateDatabase.webhostingNetwork.${eventType}.${eventResult}`,
+        message,
+      );
     }
   },
 );
