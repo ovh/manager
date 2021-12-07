37224830ab6d0b33ffb38df24a6168ffdbacdb48
frenauvh <florian.renaut@corp.ovh.com>
2021-12-07 10:28:21 +0100

    feat(shell): move auth plugin initialization
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/components/ovh-shell/src/shell/index.ts
+++ packages/components/ovh-shell/src/shell/index.ts
@@ -2,6 +2,7 @@ import { Environment, fetchConfiguration } from '@ovh-ux/manager-config';
 
 import Shell from './shell';
 import DirectClientMessageBus from '../message-bus/direct-client';
+import authenticationPlugin from '../plugin/auth';
 import environmentPlugin from '../plugin/environment';
 
 export function initShell(): Promise<Shell> {
@@ -10,6 +11,9 @@ export function initShell(): Promise<Shell> {
 
     // set message bus
     shell.setMessageBus(new DirectClientMessageBus());
+
+    // register authentication plugin
+    shell.getPluginManager().registerPlugin('auth', authenticationPlugin());
 
     // register environment plugin
     shell
