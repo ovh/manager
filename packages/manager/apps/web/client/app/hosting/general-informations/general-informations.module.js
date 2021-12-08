d79885da784467944db505bea11d37386dcfef50
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2021-12-08 17:57:47 +0100

    feat(web.hosting): add cdn cancel termination dependency
    ref: MANAGER-8265
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/apps/web/client/app/hosting/general-informations/general-informations.module.js
+++ packages/manager/apps/web/client/app/hosting/general-informations/general-informations.module.js
@@ -4,11 +4,12 @@ import controller from './GENERAL_INFORMATIONS.controller';
 import routing from './general-informations.routing';
 
 import cdnTerminate from '../cdn/terminate';
+import cdnCancelTerminate from '../cdn/cancel-terminate';
 
 const moduleName = 'ovhManagerHostingGeneralInformations';
 
 angular
-  .module(moduleName, [ovhManagerAdvices, cdnTerminate])
+  .module(moduleName, [ovhManagerAdvices, cdnTerminate, cdnCancelTerminate])
   .controller('hostingGeneralInformationsCtrl', controller)
   .run(/* @ngTranslationsInject:json ./translations */)
   .run(/* @ngTranslationsInject:json ../translations */)
