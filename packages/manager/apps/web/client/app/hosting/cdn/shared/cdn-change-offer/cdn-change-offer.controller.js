9b6100498bdba80c6f91c4a6c69d0e8a4d27c07c
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2021-12-10 11:43:22 +0100

    fix(web.hosting): required tracking changes
    ref: MANAGER-8246
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/apps/web/client/app/hosting/cdn/shared/cdn-change-offer/cdn-change-offer.controller.js
+++ packages/manager/apps/web/client/app/hosting/cdn/shared/cdn-change-offer/cdn-change-offer.controller.js
@@ -1,6 +1,6 @@
 export default class HostingCdnSharedChangeOfferController {
   onCdnChangeOfferConfirm() {
-    this.trackClick('activate_advanced_purge::upgrade_confirm');
+    this.trackClick('activate_advanced_purge::confirm');
 
     this.loading = true;
     return this.goToCdnChangeOffer().finally(() => {
@@ -9,7 +9,7 @@ export default class HostingCdnSharedChangeOfferController {
   }
 
   onCdnChangeOfferCancel() {
-    this.trackClick('activate_advanced_purge::upgrade_error');
+    this.trackClick('activate_advanced_purge::cancel');
 
     this.model.options.cache.advanced_purge.enabled = false;
     return this.goBack();
