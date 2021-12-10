d43e469929df8b2183327709eb4227e2635b6e19
Jisay <jean-christophe.alleman@corp.ovh.com>
2021-12-10 15:52:00 +0100

    feat(app-shell): use ux plugin in appShell
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

---
+++ packages/manager/apps/container/src/core/header/HeaderProvider.jsx
@@ -0,0 +1,47 @@
+import React, { useContext, useEffect, useState } from 'react';
+
+import HeaderContext from './header.context';
+import { useShell } from '@/context';
+
+export const HeaderProvider = ({ children }) => {
+  let headerContext = useContext(HeaderContext);
+  const uxPlugin = useShell().ux();
+
+  /* ----------- Account sidebar -----------*/
+
+  const [isAccountSidebarVisible, setIsAccountSidebarVisible] = useState(
+    uxPlugin.isAccountSidebarVisible(),
+  );
+
+  useEffect(() => {
+    uxPlugin.toggleAccountSidebarVisibility();
+  }, [isAccountSidebarVisible]);
+
+  /* ----------- Notifications sidebar -----------*/
+
+  const [
+    isNotificationsSidebarVisible,
+    setIsNotificationsSidebarVisible,
+  ] = useState(uxPlugin.isNotificationsSidebarVisible());
+
+  useEffect(() => {
+    uxPlugin.toggleNotificationsSidebarVisibility();
+  }, [isNotificationsSidebarVisible]);
+
+  headerContext = {
+    // account sidebar
+    isAccountSidebarVisible,
+    setIsAccountSidebarVisible,
+    // notifications sidebar
+    isNotificationsSidebarVisible,
+    setIsNotificationsSidebarVisible,
+  };
+
+  return (
+    <HeaderContext.Provider value={headerContext}>
+      {children}
+    </HeaderContext.Provider>
+  );
+};
+
+export default HeaderProvider;
