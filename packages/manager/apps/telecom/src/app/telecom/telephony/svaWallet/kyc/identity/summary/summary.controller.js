0d187057354ca505e10ed667d885b4a9a8c7cc99
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2021-12-15 08:50:39 +0100

    fix(identity.summary): iban is always editable
    ref: MANAGER-7967
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/apps/telecom/src/app/telecom/telephony/svaWallet/kyc/identity/summary/summary.controller.js
+++ packages/manager/apps/telecom/src/app/telecom/telephony/svaWallet/kyc/identity/summary/summary.controller.js
@@ -1,6 +1,5 @@
 import updateBankAccountTemplate from './updateBankAccount/update-bank-account.html';
 import updateBankAccountController from './updateBankAccount/update-bank-account.controller';
-import { canEditBankAccount } from './summary.constants';
 import { formatDocuments } from '../documents/documents.constants';
 import { buildEnumList } from '../../../sva-wallet.constants';
 
@@ -16,7 +15,6 @@ export default class KycIdentitySummaryController {
   $onInit() {
     this.showDocumentSuccessMessage = false;
     this.showIBANSuccessMessage = false;
-    this.isBankAccountEditable = canEditBankAccount(this.bankAccount);
 
     this.prepareDocuments(this.svaWallet.documents);
   }
