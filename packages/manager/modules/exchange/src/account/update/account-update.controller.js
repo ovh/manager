eeae84ce3dbf2db1f537d3f104762f8525bb0f39
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2021-12-17 13:03:11 +0100

    feat(web.exchange): add update exchange module
    ref: MANAGER-8230
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/exchange/src/account/update/account-update.controller.js
+++ packages/manager/modules/exchange/src/account/update/account-update.controller.js
@@ -11,37 +11,34 @@ export default class ExchangeUpdateAccountCtrl {
   /* @ngInject */
   constructor(
     $scope,
-    exchangeAccountOutlook,
-    exchangeAccountTypes,
+    $translate,
     exchangeServiceInfrastructure,
+    exchangeAccountTypes,
     wucExchange,
     wucExchangePassword,
-    exchangeVersion,
     messaging,
     navigation,
-    $translate,
   ) {
-    this.services = {
-      $scope,
-      exchangeAccountOutlook,
-      exchangeAccountTypes,
-      exchangeServiceInfrastructure,
-      wucExchange,
-      wucExchangePassword,
-      exchangeVersion,
-      messaging,
-      navigation,
-      $translate,
+    this.$scope = $scope;
+    this.$translate = $translate;
+    this.exchangeServiceInfrastructure = exchangeServiceInfrastructure;
+    this.exchangeAccountTypes = exchangeAccountTypes;
+    this.wucExchange = wucExchange;
+    this.wucExchangePassword = wucExchangePassword;
+    this.messaging = messaging;
+    this.navigation = navigation;
+  }
+
+  $onInit() {
+    this.$routerParams = this.wucExchange.getParams();
+    this.originalValues = angular.copy(this.emailAccount);
+
+    this.telephonyModel = {
+      phone: { selected: '', number: '' },
+      mobile: { selected: '', number: '' },
+      fax: { selected: '', number: '' },
     };
-
-    this.$routerParams = wucExchange.getParams();
-    this.originalValues = angular.copy(navigation.currentActionData);
-
-    $scope.needsUpdate = () => this.needsUpdate();
-    $scope.loadAccountOptions = () => this.loadAccountOptions();
-    $scope.updateExchangeAccount = () => this.updateExchangeAccount();
-
-    this.selectedAccount = angular.copy(navigation.currentActionData);
+    this.selectedAccount = angular.copy(this.emailAccount);
     this.selectedAccount.oldOutlook = this.selectedAccount.outlook;
     this.selectedAccount.oldDeleteOutlook = this.selectedAccount.deleteOutlook;
     this.selectedAccount.quota = this.selectedAccount.quota
@@ -50,17 +47,32 @@ export default class ExchangeUpdateAccountCtrl {
 
     this.passwordTooltip = null; // set in $scope.loadAccountOptions()
 
-    this.exchange = wucExchange.value;
+    this.exchange = this.wucExchange.value;
 
-    wucExchange.getSharepointService(this.exchange).then((sharepoint) => {
-      this.sharepoint = sharepoint;
-    });
+    this.isFetchingUpdateOptions = true;
+    return this.loadAccountOptions()
+      .then(() => {
+        this.needsUpdate();
+        this.updateExchangeAccount();
+        this.initFields();
+      })
+      .then(() =>
+        this.wucExchange
+          .getSharepointService(this.exchange)
+          .then((sharepoint) => {
+            this.sharepoint = sharepoint;
+          }),
+      )
+      .finally(() => {
+        this.isFetchingUpdateOptions = false;
+      });
+  }
 
-    this.newAccountOptions = {
-      availableDomains: [this.selectedAccount.domain],
-      availableTypes: [this.selectedAccount.accountLicense],
-      quotaArray: [],
-    };
+  initFields() {
+    this.selectedAccount.countryCode =
+      this.countries.find(
+        ({ code }) => code === this.selectedAccount.countryCode,
+      ) || null;
   }
 
   accountIsValid() {
@@ -130,6 +142,66 @@ export default class ExchangeUpdateAccountCtrl {
         modifiedBuffer.accountLicense !== originalValues.accountLicense
           ? modifiedBuffer.accountLicense
           : undefined,
+      initial:
+        modifiedBuffer.initial !== originalValues.initial
+          ? modifiedBuffer.initial
+          : undefined,
+      description:
+        modifiedBuffer.description !== originalValues.description
+          ? modifiedBuffer.description
+          : undefined,
+      forwardingEmail:
+        modifiedBuffer.forwardingEmail !== originalValues.forwardingEmail
+          ? modifiedBuffer.forwardingEmail
+          : undefined,
+      storeCopyOfEmail:
+        modifiedBuffer.storeCopyOfEmail !== originalValues.storeCopyOfEmail
+          ? modifiedBuffer.storeCopyOfEmail
+          : undefined,
+      jobDepartment:
+        modifiedBuffer.jobDepartment !== originalValues.jobDepartment
+          ? modifiedBuffer.jobDepartment
+          : undefined,
+      jobTitle:
+        modifiedBuffer.jobTitle !== originalValues.jobTitle
+          ? modifiedBuffer.jobTitle
+          : undefined,
+      office:
+        modifiedBuffer.office !== originalValues.office
+          ? modifiedBuffer.office
+          : undefined,
+      streetAddress:
+        modifiedBuffer.streetAddress !== originalValues.streetAddress
+          ? modifiedBuffer.streetAddress
+          : undefined,
+      postalCode:
+        modifiedBuffer.postalCode !== originalValues.postalCode
+          ? modifiedBuffer.postalCode
+          : undefined,
+      city:
+        modifiedBuffer.city !== originalValues.city
+          ? modifiedBuffer.city
+          : undefined,
+      countryCode:
+        modifiedBuffer.countryCode?.code !== originalValues.countryCode?.code
+          ? modifiedBuffer.countryCode.code
+          : undefined,
+      region:
+        modifiedBuffer.region !== originalValues.region
+          ? modifiedBuffer.region
+          : undefined,
+      phone:
+        modifiedBuffer.phone !== originalValues.phone
+          ? modifiedBuffer.phone
+          : undefined,
+      mobile:
+        modifiedBuffer.mobile !== originalValues.mobile
+          ? modifiedBuffer.mobile
+          : undefined,
+      fax:
+        modifiedBuffer.fax !== originalValues.fax
+          ? modifiedBuffer.fax
+          : undefined,
     };
 
     return model;
@@ -138,7 +210,7 @@ export default class ExchangeUpdateAccountCtrl {
   getFeaturesToUpdate(originalValues, modifiedBuffer) {
     const model = this.getModelToUpdate(originalValues, modifiedBuffer);
 
-    if (this.services.exchangeServiceInfrastructure.isProvider()) {
+    if (this.exchangeServiceInfrastructure.isProvider()) {
       model.quota =
         originalValues.totalQuota.value &&
         modifiedBuffer.quota !== originalValues.quota
@@ -160,37 +232,35 @@ export default class ExchangeUpdateAccountCtrl {
 
     if (messages.length === 1) {
       if (messages[0].type === 'INFO') {
-        updateAccountMessages.OK = this.services.$translate.instant(
+        updateAccountMessages.OK = this.$translate.instant(
           'exchange_ACTION_update_account_success_message',
         );
       } else if (messages[0].type === 'ERROR') {
-        updateAccountMessages.ERROR = this.services.$translate.instant(
+        updateAccountMessages.ERROR = this.$translate.instant(
           'exchange_ACTION_update_account_error_message',
         );
       }
     } else if (messages.length === 2) {
       if (messages[0].type === messages[1].type) {
         if (messages[0].type === 'INFO') {
-          updateAccountMessages.OK = this.services.$translate.instant(
+          updateAccountMessages.OK = this.$translate.instant(
             'exchange_ACTION_update_account_success_message',
           );
         } else if (messages[0].type === 'ERROR') {
-          updateAccountMessages.ERROR = this.services.$translate.instant(
+          updateAccountMessages.ERROR = this.$translate.instant(
             'exchange_ACTION_update_account_error_message',
           );
         }
-      } else if (
-        messages[0].message === this.services.wucExchange.updateAccountAction
-      ) {
-        updateAccountMessages.PARTIAL = `${this.services.$translate.instant(
+      } else if (messages[0].message === this.wucExchange.updateAccountAction) {
+        updateAccountMessages.PARTIAL = `${this.$translate.instant(
           'exchange_ACTION_update_account_success_message',
-        )} ${this.services.$translate.instant(
+        )} ${this.$translate.instant(
           'exchange_ACTION_change_password_account_error_message_linked',
         )}`;
       } else {
-        updateAccountMessages.PARTIAL = `${this.services.$translate.instant(
+        updateAccountMessages.PARTIAL = `${this.$translate.instant(
           'exchange_ACTION_change_password_account_success_message',
-        )} ${this.services.$translate.instant(
+        )} ${this.$translate.instant(
           'exchange_ACTION_update_account_error_message_linked',
         )}`;
       }
@@ -238,23 +308,23 @@ export default class ExchangeUpdateAccountCtrl {
     }
 
     if (!isEmpty(selectedAccount.password)) {
-      this.simplePasswordFlag = !this.services.wucExchangePassword.passwordSimpleCheck(
+      this.simplePasswordFlag = !this.wucExchangePassword.passwordSimpleCheck(
         selectedAccount.password,
         true,
-        this.newAccountOptions.minPasswordLength,
+        this.updateAccountOptions.minPasswordLength,
       );
 
       // see the password complexity requirements of Microsoft Windows Server (like wucExchange)
       // https://technet.microsoft.com/en-us/library/hh994562%28v=ws.10%29.aspx
-      if (this.newAccountOptions.passwordComplexityEnabled) {
+      if (this.updateAccountOptions.passwordComplexityEnabled) {
         this.simplePasswordFlag =
           this.simplePasswordFlag ||
-          !this.services.wucExchangePassword.passwordComplexityCheck(
+          !this.wucExchangePassword.passwordComplexityCheck(
             selectedAccount.password,
           );
 
         if (selectedAccount.displayName) {
-          this.containsNameFlag = this.services.wucExchangePassword.passwordContainsName(
+          this.containsNameFlag = this.wucExchangePassword.passwordContainsName(
             selectedAccount.password,
             selectedAccount.displayName,
           );
@@ -273,7 +343,7 @@ export default class ExchangeUpdateAccountCtrl {
           includes(selectedAccount.password, selectedAccount.samaccountName)
         ) {
           if (!this.containsSamAccountNameLabel) {
-            this.containsSamAccountNameLabel = this.services.$translate.instant(
+            this.containsSamAccountNameLabel = this.$translate.instant(
               'exchange_ACTION_update_account_step1_password_contains_samaccount_name',
               { t0: selectedAccount.samaccountName },
             );
@@ -289,10 +359,10 @@ export default class ExchangeUpdateAccountCtrl {
     }
   }
 
-  needsUpdate() {
+  isFormFieldsChanged() {
     const modifiedBuffer = this.selectedAccount;
 
-    const needsUpdate =
+    return (
       modifiedBuffer.password != null ||
       this.originalValues.login !== modifiedBuffer.login ||
       this.originalValues.displayName !== modifiedBuffer.displayName ||
@@ -306,30 +376,53 @@ export default class ExchangeUpdateAccountCtrl {
       this.originalValues.accountLicense !== modifiedBuffer.accountLicense ||
       this.originalValues.quota !== modifiedBuffer.quota ||
       this.originalValues.outlook !== modifiedBuffer.outlook ||
-      this.originalValues.deleteOutlook !== modifiedBuffer.deleteOutlook;
+      this.originalValues.deleteOutlook !== modifiedBuffer.deleteOutlook ||
+      this.originalValues.initial !== modifiedBuffer.initial ||
+      this.originalValues.description !== modifiedBuffer.description ||
+      this.originalValues.forwardingEmail !== modifiedBuffer.forwardingEmail ||
+      this.originalValues.storeCopyOfEmail !==
+        modifiedBuffer.storeCopyOfEmail ||
+      this.originalValues.jobDepartment !== modifiedBuffer.jobDepartment ||
+      this.originalValues.jobTitle !== modifiedBuffer.jobTitle ||
+      this.originalValues.office !== modifiedBuffer.office ||
+      this.originalValues.streetAddress !== modifiedBuffer.streetAddress ||
+      this.originalValues.postalCode !== modifiedBuffer.postalCode ||
+      this.originalValues.city !== modifiedBuffer.city ||
+      this.originalValues.countryCode !== modifiedBuffer.countryCode ||
+      this.originalValues.region !== modifiedBuffer.region ||
+      this.originalValues.phone !== modifiedBuffer.phone ||
+      this.originalValues.mobile !== modifiedBuffer.mobile ||
+      this.originalValues.fax !== modifiedBuffer.fax
+    );
+  }
 
-    return needsUpdate && this.accountIsValid() && !this.takenEmailError;
+  needsUpdate() {
+    return (
+      this.isFormFieldsChanged() &&
+      this.accountIsValid() &&
+      !this.takenEmailError
+    );
   }
 
   setQuotaAvailable() {
-    this.newAccountOptions.quotaArray = [];
+    this.updateAccountOptions.quotaArray = [];
 
     for (
-      let i = this.newAccountOptions.maxQuota;
-      i >= this.newAccountOptions.minQuota;
+      let i = this.updateAccountOptions.maxQuota;
+      i >= this.updateAccountOptions.minQuota;
       i -= 1
     ) {
-      this.newAccountOptions.quotaArray.push(i);
+      this.updateAccountOptions.quotaArray.push(i);
     }
   }
 
   canChangePrimary() {
-    return this.newAccountOptions != null;
+    return this.updateAccountOptions != null;
   }
 
   getPasswordPlaceholder() {
     return this.selectedAccount.canBeConfigured
-      ? this.services.$translate.instant(
+      ? this.$translate.instant(
           'exchange_ACTION_update_account_step1_password_placeholder',
         )
       : ' ';
@@ -346,14 +439,14 @@ export default class ExchangeUpdateAccountCtrl {
 
   getCompleteDomain(domainName) {
     const result = find(
-      this.newAccountOptions.availableDomains,
+      this.updateAccountOptions.availableDomains,
       (value) => value.name === domainName,
     );
 
     // if the current domain is not in the domain's list (dummy account),
     // select by default the first available
     if (result == null) {
-      return this.newAccountOptions.availableDomains[0];
+      return this.updateAccountOptions.availableDomains[0];
     }
 
     return result;
@@ -361,23 +454,23 @@ export default class ExchangeUpdateAccountCtrl {
 
   loadAccountOptions() {
     this.noDomainMessage = null;
-    this.services.wucExchange
+    return this.wucExchange
       .fetchingAccountCreationOptions(
         this.$routerParams.organization,
         this.$routerParams.productId,
       )
       .then((data) => {
-        this.newAccountOptions = data;
+        this.updateAccountOptions = data;
 
         this.setQuotaAvailable();
         this.takenEmails = data.takenEmails;
 
         if (isEmpty(data.availableDomains)) {
-          this.services.messaging.writeError(
-            this.services.$translate.instant('exchange_ACTION_add_no_domains'),
+          this.messaging.writeError(
+            this.$translate.instant('exchange_ACTION_add_no_domains'),
           );
-          this.services.navigation.resetAction();
-          this.noDomainMessage = this.services.$translate.instant(
+          this.navigation.resetAction();
+          this.noDomainMessage = this.$translate.instant(
             'exchange_ACTION_add_no_domains',
           );
 
@@ -389,53 +482,51 @@ export default class ExchangeUpdateAccountCtrl {
           );
         }
 
-        this.passwordTooltip = this.newAccountOptions.passwordComplexityEnabled
-          ? this.services.$translate.instant(
+        this.passwordTooltip = this.updateAccountOptions
+          .passwordComplexityEnabled
+          ? this.$translate.instant(
               'exchange_ACTION_update_account_step1_complex_password_tooltip',
-              { t0: this.newAccountOptions.minPasswordLength },
+              { t0: this.updateAccountOptions.minPasswordLength },
             )
-          : this.services.$translate.instant(
+          : this.$translate.instant(
               'exchange_ACTION_update_account_step1_simple_password_tooltip',
-              { t0: this.newAccountOptions.minPasswordLength },
+              { t0: this.updateAccountOptions.minPasswordLength },
             );
       })
       .catch((failure) => {
-        this.services.messaging.writeError(
-          this.services.$translate.instant(
-            'exchange_ACTION_add_account_option_fail',
-          ),
+        this.messaging.writeError(
+          this.$translate.instant('exchange_ACTION_add_account_option_fail'),
           failure,
         );
-        this.services.navigation.resetAction();
+        this.navigation.resetAction();
       });
   }
 
   updateExchangeAccount() {
     if (this.needsUpdate()) {
-      this.services.wucExchange
+      this.wucExchange
         .updateAccount(
           this.$routerParams.organization,
           this.$routerParams.productId,
           this.getFeaturesToUpdate(this.originalValues, this.selectedAccount),
         )
         .then((data) => {
-          this.services.messaging.setMessage(
-            this.getActionMessage(data.messages),
-            data,
-          );
+          this.messaging.setMessage(this.getActionMessage(data.messages), data);
         })
+        .then(() => this.goBack())
         .catch((failure) => {
-          this.services.messaging.writeError(`
-            ${this.services.$translate.instant('exchange_common_error')} ${get(
+          this.messaging.writeError(`
+            ${this.$translate.instant('exchange_common_error')} ${get(
             failure,
             'message',
             failure,
           )}
           `);
-        })
-        .finally(() => {
-          this.services.navigation.resetAction();
         });
     }
+  }
+
+  onCountryPhoneCodeChange(type) {
+    this.selectedAccount[type] = this.telephonyModel[type].number;
   }
 }
