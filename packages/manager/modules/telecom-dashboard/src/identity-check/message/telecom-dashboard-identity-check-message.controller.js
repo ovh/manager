19b42d520a57a84c55a02d2681b2c00964b9ccce
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2021-12-22 14:52:03 +0100

    feat(identity-check): add tracking first part
    add tracking on message / form
    
    ref: MANAGER-8382
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/modules/telecom-dashboard/src/identity-check/message/telecom-dashboard-identity-check-message.controller.js
+++ packages/manager/modules/telecom-dashboard/src/identity-check/message/telecom-dashboard-identity-check-message.controller.js
@@ -1,6 +1,7 @@
 export default class IdentityCheckMessageCtrl {
   /* @ngInject */
-  constructor(coreURLBuilder, IdentityCheckService, TucToastError) {
+  constructor(atInternet, coreURLBuilder, IdentityCheckService, TucToastError) {
+    this.atInternet = atInternet;
     this.IdentityCheckService = IdentityCheckService;
     this.TucToastError = TucToastError;
 
@@ -15,9 +16,25 @@ export default class IdentityCheckMessageCtrl {
     this.IdentityCheckService.isProcedureRequired()
       .then((isProcedureRequired) => {
         this.isMessageShown = isProcedureRequired;
+        // tracking impression message
+        if (this.isMessageShown) {
+          this.atInternet.trackImpression({
+            campaignId: '[telecom-manager-migration]',
+            creation: '[identity-check]',
+            detailedPlacement: '[telecom::dashboard]',
+          });
+        }
       })
       .catch((error) => {
         this.TucToastError(error.data?.message || error.message);
       });
+  }
+
+  trackingImpressionClick() {
+    this.atInternet.trackClickImpression({
+      campaignId: '[telecom-manager-migration]',
+      creation: '[identity-check]',
+      detailedPlacement: '[telecom::dashboard]',
+    });
   }
 }
