bb77a015e795263e51313bdbc3c49cd4db670c68
Anoop N <anooparveti@gmail.com>
2021-12-22 13:46:20 +0530

    fix(hosting.multisite): display cdn upgrade info (#5962)
    Display CDN Upgrade info to display CDN statistics if cdn type is business
    
    ref: DTRSD-54696
    
    Signed-off-by: Anoop <anooparveti@gmail.com>

---
+++ packages/manager/apps/web/client/app/hosting/multisite/statistics/hosting-multisite-statistics.constants.js
@@ -0,0 +1,14 @@
+export const CDN_TYPE_BUSINESS = 'business';
+
+export const CDN_SERIES = {
+  NAME: {
+    HIT: 'hit',
+    MISS: 'miss',
+  },
+  COLORS: {
+    RED: 'red',
+    TURQUOISE: 'turquoise',
+  },
+};
+
+export default { CDN_TYPE_BUSINESS, CDN_SERIES };
