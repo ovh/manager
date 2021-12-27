811ec7e6209af4a346142f7c5b5caea7b3345a37
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2021-12-27 14:02:56 +0100

    feat(anthos.dashboard.host): order additional host
    ref: MANAGER-8315
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/modules/anthos/src/dashboard/host/order/anthos-dashboard-host-order.module.js
+++ packages/manager/modules/anthos/src/dashboard/host/order/anthos-dashboard-host-order.module.js
@@ -3,13 +3,14 @@ import '@ovh-ux/ui-kit';
 import '@uirouter/angularjs';
 import 'angular-translate';
 
-import orderHost from '../../components/order-host';
-import routing from './routing';
+import component from './anthos-dashboard-host-order.component';
+import routing from './anthos-dashboard-host-order.routing';
 
 const moduleName = 'ovhManagerAnthosDashboardHostOrder';
 
 angular
-  .module(moduleName, ['oui', 'pascalprecht.translate', 'ui.router', orderHost])
+  .module(moduleName, ['oui', 'pascalprecht.translate', 'ui.router'])
+  .component('anthosDashboardHostOrder', component)
   .config(routing)
   .run(/* @ngTranslationsInject:json ./translations */);
 
