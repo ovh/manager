ec5e2a55334c09844b063859bd0c2d66da58db3c
Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
2021-12-29 14:13:42 +0100

    feat(tracking): improve plugin class and types
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

--- packages/components/ovh-at-internet/src/index.ts
+++ packages/components/ovh-at-internet/src/index.ts
@@ -2,10 +2,21 @@ declare global {
   interface Window {
     ATInternet: {
       Tracker: {
-        Tag: any;
+        Tag: ATInternetTag;
       };
     };
   }
+}
+
+export interface ATInternetTagOptions {
+  ClientSideUserId: { clientSideMode: string };
+  secure: boolean; // force HTTPS,
+  disableCookie: boolean;
+  [key: string]: any;
+}
+
+export interface ATInternetTag {
+  new (tag: ATInternetTagOptions): ATInternetTagOptions;
 }
 
 export { default } from './ovh-at-internet';
