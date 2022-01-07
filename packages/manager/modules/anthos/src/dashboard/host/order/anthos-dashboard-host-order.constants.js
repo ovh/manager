17ccdf79637e0f4b54254969745d13a1da56c4be
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2022-01-07 13:24:45 +0100

    feat(anthos.dashboard): order hosts tracking
    ref: MANAGER-8319
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/modules/anthos/src/dashboard/host/order/anthos-dashboard-host-order.constants.js
+++ packages/manager/modules/anthos/src/dashboard/host/order/anthos-dashboard-host-order.constants.js
@@ -3,6 +3,7 @@ export const MAX_ADDONS = 10;
 export const PRICE_DURATION = 'P1M';
 export const PRICING_INTERVAL = 1;
 export const PRICING_INTERVAL_UNIT = 'month';
+export const TRACKING_CHUNK = 'order-host';
 
 export default {
   ADDON_PRODUCT_NAME,
@@ -10,4 +11,5 @@ export default {
   PRICE_DURATION,
   PRICING_INTERVAL_UNIT,
   PRICING_INTERVAL,
+  TRACKING_CHUNK,
 };
