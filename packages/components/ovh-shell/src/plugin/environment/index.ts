2333dc55dfe109e88cdff9db67d61de0260e03e2
Jisay <jean-christophe.alleman@corp.ovh.com>
2022-01-07 16:30:14 +0100

    feat(ovh-shell): add onUniverse callback in environment plugin
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

--- packages/components/ovh-shell/src/plugin/environment/index.ts
+++ packages/components/ovh-shell/src/plugin/environment/index.ts
@@ -2,10 +2,23 @@ import { Environment } from '@ovh-ux/manager-config';
 import { ApplicationId } from '@ovh-ux/manager-config/types/application';
 
 export function environment(environment: Environment) {
+  const listeners: CallableFunction[] = [];
+
+  const triggerListeners = (applicationId: string) => {
+    listeners.forEach((listener) => {
+      listener(applicationId);
+    });
+  };
+
   return {
     getEnvironment: (): Environment => environment,
-    setUniverse: (applicationId: ApplicationId) =>
-      environment.setUniverseFromApplicationId(applicationId),
+    setUniverse: (applicationId: ApplicationId) => {
+      environment.setUniverseFromApplicationId(applicationId);
+      triggerListeners(applicationId);
+    },
+    onUniverseChange: (callback: CallableFunction) => {
+      listeners.push(callback);
+    },
   };
 }
 
