7c42430f739a6aaaf763807813f909e3c095c8cc
Jisay <jean-christophe.alleman@corp.ovh.com>
2022-01-07 15:27:17 +0100

    feat(telecom): use shell client
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

--- packages/manager/apps/telecom/src/app/index.js
+++ packages/manager/apps/telecom/src/app/index.js
@@ -4,22 +4,29 @@ import {
   attach as attachPreloader,
   displayMessage,
 } from '@ovh-ux/manager-preloader';
-import { registerApplication } from '@ovh-ux/ufrontend';
+import { useShellClient } from '@ovh-ux/shell';
 import { findAvailableLocale, detectUserLocale } from '@ovh-ux/manager-config';
+
+import { getShellClient, setShellClient } from './shell';
 
 attachPreloader(findAvailableLocale(detectUserLocale()));
 
-registerApplication('telecom').then(({ environment }) => {
-  environment.setVersion(__VERSION__);
+useShellClient('telecom')
+  .then((client) => {
+    setShellClient(client);
+    return client.environment.getEnvironment();
+  })
+  .then((environment) => {
+    environment.setVersion(__VERSION__);
 
-  if (environment.getMessage()) {
-    displayMessage(environment.getMessage(), environment.getUserLanguage());
-  }
+    if (environment.getMessage()) {
+      displayMessage(environment.getMessage(), environment.getUserLanguage());
+    }
 
-  import(`./config-${environment.getRegion()}`)
-    .catch(() => {})
-    .then(() => import('./app.module'))
-    .then(({ default: startApplication }) => {
-      startApplication(document.body, environment);
-    });
-});
+    import(`./config-${environment.getRegion()}`)
+      .catch(() => {})
+      .then(() => import('./app.module'))
+      .then(({ default: startApplication }) => {
+        startApplication(document.body, getShellClient());
+      });
+  });
