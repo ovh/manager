6f6560e75e9a9a7fa06d10294f809cd5aa552d33
Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
2022-01-12 10:42:46 +0100

    feat(cookiepolicy): add cookie policy modal in container
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

---
+++ packages/manager/apps/container/src/cookie-policy/links.js
@@ -0,0 +1,43 @@
+export default {
+  moreInfo: {
+    FR:
+      'https://www.ovh.com/fr/support/documents_legaux/politique_cookies_ovh.xml',
+
+    GB: 'https://www.ovh.co.uk/support/termsofservice/cookies_ovh.xml',
+    IE: 'https://www.ovh.ie/support/termsofservice/cookies_ovh.xml',
+    DE: 'https://www.ovh.de/support/agb/cookies_ovh.xml',
+    ES: 'https://www.ovh.es/soporte/documentos_legales/cookies_ovh.xml',
+    IT: 'https://www.ovh.it/supporto/documenti_legali/cookies_ovh.xml',
+    NL: 'https://www.ovh.nl/support/algemene_voorwaarden/cookies_ovh.xml',
+    PL: 'https://www.ovh.pl/pomoc/dokumenty/cookies_ovh.xml',
+    PT: 'https://www.ovh.pt/suporte/documentos_legais/cookies_ovh.xml',
+    SN: 'https://www.ovh.sn/support/documents_legaux/politique_cookies_ovh.xml',
+    TN:
+      'https://www.ovh.com/tn/support/documents_legaux/politique_cookies_ovh.xml',
+    MA:
+      'https://www.ovh.com/ma/support/documents_legaux/politique_cookies_ovh.xml',
+    QC: 'https://www.ovh.com/ca/fr/support/documents_legaux/cookies_ovh.xml',
+    CA: 'https://www.ovh.com/ca/en/support/termsofservice/cookies_ovh.xml',
+    WE: 'https://www.ovh.com/world/support/termsofservice/cookies_ovh.xml',
+    WS:
+      'https://www.ovh.com/world/es/soporte/documentos_legales/cookies_ovh.xml',
+    AU: 'https://www.ovh.com.au/support/termsofservice/cookies_ovh.xml',
+    SG: 'https://www.ovh.com/sg/support/termsofservice/cookies_ovh.xml',
+    ASIA: 'https://www.ovh.com/asia/support/termsofservice/cookies_ovh.xml',
+  },
+  changeOpinionLink: {
+    FR:
+      'https://www.ovh.com/fr/protection-donnees-personnelles/exercer-vos-droits/',
+    GB: 'https://www.ovh.co.uk/personal-data-protection/exercise-your-rights/',
+    IE: 'https://www.ovh.ie/personal-data-protection/exercising-your-rights/',
+    DE: 'https://www.ovh.de/schutz-personenbezogener-daten/rechte-ausueben/',
+    ES: 'https://www.ovh.es/proteccion-datos-personales/ejercer-derechos/',
+    IT: 'https://www.ovh.it/protezione-dati-personali/esercita-i-tuoi-diritti/',
+    NL:
+      'https://www.ovh.nl/beveiliging-persoonlijke-data/uw-rechten-uitoefenen/',
+    PL: 'https://www.ovh.pl/ochrona-danych-osobowych/korzystanie-z-praw/',
+    PT: 'https://www.ovh.pt/proteger-dados-pessoais/exercer-seus-direitos/',
+    TN:
+      'https://www.ovh.com/tn/protection-donnees-personnelles/exercer-vos-droits/',
+  },
+};
