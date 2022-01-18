1f3b9c111c4e517957bcc55abe956ab8de080b4d
Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
2022-01-18 11:21:47 +0100

    fix(atinternet): expose public functions via plugin
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

--- packages/components/ovh-shell/src/plugin/tracking/index.ts
+++ packages/components/ovh-shell/src/plugin/tracking/index.ts
@@ -1,3 +1,4 @@
+import { ATInternetTagOptions } from '@ovh-ux/ovh-at-internet/types';
 import {
   ClickData,
   EventData,
@@ -11,6 +12,10 @@ import ShellClient from '../../client/shell-client';
 export interface TrackingAPI {
   init(): PromiseLike<void>;
   isTagAvailable(): PromiseLike<boolean>;
+  clearTrackQueue(): PromiseLike<void>;
+  processTrackQueue(): PromiseLike<void>;
+  initTag(): PromiseLike<void>;
+  getTag(): PromiseLike<ATInternetTagOptions>;
   trackClick(data: ClickData): PromiseLike<void>;
   trackPage(data: PageData): PromiseLike<void>;
   trackOrder(data: OrderData): PromiseLike<void>;
@@ -30,6 +35,26 @@ export function exposeTrackingAPI(shellClient: ShellClient): TrackingAPI {
       shellClient.invokePluginMethod<boolean>({
         plugin: 'tracking',
         method: 'isTagAvailable',
+      }),
+    clearTrackQueue: () =>
+      shellClient.invokePluginMethod<void>({
+        plugin: 'tracking',
+        method: 'clearTrackQueue',
+      }),
+    processTrackQueue: () =>
+      shellClient.invokePluginMethod<void>({
+        plugin: 'tracking',
+        method: 'processTrackQueue',
+      }),
+    initTag: () =>
+      shellClient.invokePluginMethod<void>({
+        plugin: 'tracking',
+        method: 'initTag',
+      }),
+    getTag: () =>
+      shellClient.invokePluginMethod<ATInternetTagOptions>({
+        plugin: 'tracking',
+        method: 'getTag',
       }),
     trackClick: (data: ClickData) =>
       shellClient.invokePluginMethod<void>({
