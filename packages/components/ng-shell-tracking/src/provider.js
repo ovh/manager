2695b5ae3139f9d10dbe38721f274c982acc1714
Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
2022-01-18 18:06:03 +0100

    fix(tracking): extend function definitions
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

--- packages/components/ng-shell-tracking/src/provider.js
+++ packages/components/ng-shell-tracking/src/provider.js
@@ -4,11 +4,98 @@ export default class NgAtInternet {
   }
 
   $get() {
-    console.log(this.trackingPlugin);
-    return this.trackingPlugin;
+    return this;
   }
 
   setTracking(trackingPlugin) {
     this.trackingPlugin = trackingPlugin;
+  }
+
+  async init() {
+    return this.trackingPlugin.init();
+  }
+
+  async isTagAvailable() {
+    return this.trackingPlugin.isTagAvailable();
+  }
+
+  async clearTrackQueue() {
+    return this.trackingPlugin.clearTrackQueue();
+  }
+
+  async processTrackQueue() {
+    return this.trackingPlugin.processTrackQueue();
+  }
+
+  async initTag() {
+    return this.trackingPlugin.initTag();
+  }
+
+  async getTag() {
+    return this.trackingPlugin.getTag();
+  }
+
+  async trackClick(data) {
+    return this.trackingPlugin.trackClick(data);
+  }
+
+  async trackPage(data) {
+    return this.trackingPlugin.trackPage(data);
+  }
+
+  async trackEvent(data) {
+    return this.trackingPlugin.trackOrder(data);
+  }
+
+  async trackImpression(data) {
+    return this.trackingPlugin.trackImpression(data);
+  }
+
+  async trackClickImpression(data) {
+    return this.trackingPlugin.trackClickImpression(data);
+  }
+
+  async isEnabled() {
+    return this.trackingPlugin.isEnabled();
+  }
+
+  async setEnabled(state) {
+    return this.trackingPlugin.setEnabled(state);
+  }
+
+  async setDebug(state) {
+    return this.trackingPlugin.setDebug(state);
+  }
+
+  async isDebugActive() {
+    return this.trackingPlugin.isDebugActive();
+  }
+
+  async getRegion() {
+    return this.trackingPlugin.getRegion();
+  }
+
+  async setRegion(region) {
+    return this.trackingPlugin.setRegion(region);
+  }
+
+  async setDefaultsPromise(promise) {
+    return this.trackingPlugin.setDefaultsPromise(promise);
+  }
+
+  async getDefaultsPromise() {
+    return this.trackingPlugin.getDefaultsPromise();
+  }
+
+  async getDefaults() {
+    return this.trackingPlugin.getDefaults();
+  }
+
+  async setDefaults(def) {
+    return this.trackingPlugin.setDefaults(def);
+  }
+
+  async isDefaultSet() {
+    return this.trackingPlugin.isDefaultSet();
   }
 }
