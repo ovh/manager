a0d4f642da5e24527955958151ab1c97c76859b3
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2022-01-18 16:11:51 +0100

    feat(module.ng-layout-helpers): add topbar-cta component
    description: add datagrid support simple button and actions menu
    ref: MANAGER-8199
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

---
+++ packages/manager/modules/ng-layout-helpers/src/list/components/index.js
@@ -0,0 +1,9 @@
+import angular from 'angular';
+
+import topbarCta from './topbar-cta';
+
+const moduleName = 'ovhManagerListLayoutComponents';
+
+angular.module(moduleName, [topbarCta]);
+
+export default moduleName;
