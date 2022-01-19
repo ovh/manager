ffd5bcd766c6154c9dd3fe51760c998375e4bea9
Anoop <anooparveti@gmail.com>
2022-01-19 16:12:20 +0530

    fix(web): display correct payment status
     Display correct payment status next to allDom and domains on domain dashboard
     ref: MANAGER-8525
    
    Signed-off-by: Anoop <anooparveti@gmail.com>

--- packages/manager/apps/web/client/app/domain/general-informations/general-information.constants.js
+++ packages/manager/apps/web/client/app/domain/general-informations/general-information.constants.js
@@ -16,9 +16,27 @@ export const PROTECTION_TYPES = {
   UNLOCKING: 'unlocking',
 };
 
+export const DOMAIN_SERVICE_STATES = {
+  expired: 'expired',
+  delete_at_expiration: 'delete_at_expiration',
+  automatic: 'automatic',
+  manualPayment: 'manualPayment',
+};
+
+export const DOMAIN_STATE_TYPE = {
+  error: [
+    DOMAIN_SERVICE_STATES.expired,
+    DOMAIN_SERVICE_STATES.delete_at_expiration,
+  ],
+  success: [DOMAIN_SERVICE_STATES.automatic],
+  warning: [DOMAIN_SERVICE_STATES.manualPayment],
+};
+
 export default {
   DNSSEC_STATUS,
   OWNER_CHANGE_URL,
   PRODUCT_TYPE,
   PROTECTION_TYPES,
+  DOMAIN_SERVICE_STATES,
+  DOMAIN_STATE_TYPE,
 };
