e842f4aef5c2accdaab7aac0c50bb7b80a22aaa9
Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
2022-01-21 11:06:28 +0100

    fix(tracking): trigger shell cookie modal or angular cookie modal
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

--- packages/manager/apps/container/src/cookie-policy/useCookieModal.js
+++ packages/manager/apps/container/src/cookie-policy/useCookieModal.js
@@ -1,21 +1,40 @@
 import { useCallback } from 'react';
 import { UxModal } from '@ovh-ux/shell';
 import { useCookies } from 'react-cookie';
+import { useApplication } from '@/context';
+
+const trackingEnabled = process.env.NODE_ENV === 'production';
 
 const useCookieModal = (shell) => {
   const [cookies, setCookies] = useCookies(['MANAGER_TRACKING']);
+  const { environment } = useApplication();
+
   return useCallback((node) => {
     if (!node) return;
+    const trackingPlugin = shell.getPlugin('tracking');
 
     const accept = (contentObject) => {
       setCookies('MANAGER_TRACKING', 1);
-      shell.getPlugin('tracking').init();
+      trackingPlugin.setEnabled(trackingEnabled);
+      trackingPlugin.setRegion(environment.getRegion());
+      trackingPlugin.init();
 
       contentObject.hide();
     };
 
     const deny = (contentObject) => {
       setCookies('MANAGER_TRACKING', 0);
+      trackingPlugin.setEnabled(trackingEnabled);
+      trackingPlugin.clearTrackQueue();
+      trackingPlugin.init();
+
+      trackingPlugin.trackClick({
+        type: 'action',
+        name: 'cookie-banner-manager::decline',
+      });
+
+      trackingPlugin.setEnabled(false);
+
       contentObject.hide();
     };
 
@@ -29,6 +48,9 @@ const useCookieModal = (shell) => {
     // Checking if cookie exists
     if (cookies.MANAGER_TRACKING == null) {
       modalContentObj.show();
+    } else if (cookies.MANAGER_TRACKING === '1') {
+      trackingPlugin.setEnabled(trackingEnabled);
+      trackingPlugin.init();
     }
 
     modalContent.querySelector('.accept').onclick = () => {
