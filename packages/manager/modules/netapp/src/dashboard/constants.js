8a1fded90c109231103dd4ec9f2f138ec401fe2b
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2022-01-24 18:21:04 +0100

    feat(modules.netapp): dashboard advices tracking
    ref: MANAGER-8213
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/netapp/src/dashboard/constants.js
+++ packages/manager/modules/netapp/src/dashboard/constants.js
@@ -1,5 +1,14 @@
 export const MINIMUM_VOLUME_SIZE = 100;
 
+export const SUPPORT_SUBSCRIPTION_OFFER_IMPRESSION_TRACKING_DATA = {
+  campaignId: '[advices]',
+  creation: '[support-level-premium]',
+  variant: '[tile]',
+  format: '[netapp]',
+  generalPlacement: '[levels-of-support]',
+};
+
 export default {
   MINIMUM_VOLUME_SIZE,
+  SUPPORT_SUBSCRIPTION_OFFER_IMPRESSION_TRACKING_DATA,
 };
--- packages/manager/modules/netapp/src/dashboard/controller.js
+++ packages/manager/modules/netapp/src/dashboard/controller.js
@@ -1,11 +1,20 @@
+import { SUPPORT_SUBSCRIPTION_OFFER_IMPRESSION_TRACKING_DATA } from './constants';
+
 export default class OvhManagerNetAppDashboardCtrl {
   /* @ngInject */
-  constructor($translate, Alerter) {
+  constructor($translate, atInternet, Alerter) {
     this.$translate = $translate;
+    this.atInternet = atInternet;
     this.Alerter = Alerter;
   }
 
   onBillingInformationError(error) {
     this.Alerter.error(error);
+  }
+
+  onAdviceClick() {
+    this.atInternet.trackImpression(
+      SUPPORT_SUBSCRIPTION_OFFER_IMPRESSION_TRACKING_DATA,
+    );
   }
 }
