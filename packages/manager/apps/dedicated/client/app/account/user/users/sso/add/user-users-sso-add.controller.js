292daef2d4f26cb5c3455c73d95e6fa590429ec6
sebferrer <sebastien@sebferrer.fr>
2022-01-27 17:31:29 +0100

    feat(saml-sp-sso): user account sso feature - link identity provider

---
+++ packages/manager/apps/dedicated/client/app/account/user/users/sso/add/user-users-sso-add.controller.js
@@ -0,0 +1,59 @@
+import get from 'lodash/get';
+
+export default class UserAccountUsersSSOAddCtrl {
+  /* @ngInject */
+  constructor(
+    $scope,
+    coreConfig,
+    UseraccountUsersService,
+    Alerter,
+    $translate,
+  ) {
+    this.$scope = $scope;
+    this.usersService = UseraccountUsersService;
+    this.alerter = Alerter;
+    this.$translate = $translate;
+    this.me = coreConfig.getUser();
+    this.user = {
+      group: 'DEFAULT',
+    };
+    this.loader = false;
+    this.identityProvider = {
+      metadata: null,
+      groupAttributeName: null,
+    };
+  }
+
+  $onInit() {
+    this.$scope.addSSO = this.addSSO.bind(this);
+  }
+
+  addSSO() {
+    this.loader = true;
+
+    this.usersService
+      .addIdentityProvider(this.identityProvider)
+      .then(() => {
+        this.alerter.success(
+          this.$translate.instant('user_users_sso_add_success_message', {
+            login: this.user.login,
+          }),
+          'userUsers',
+        );
+      })
+      .catch((err) => {
+        this.alerter.error(
+          `${this.$translate.instant('user_users_sso_add_error_message')} ${get(
+            err,
+            'message',
+            err,
+          )}`,
+          'userUsers',
+        );
+      })
+      .finally(() => {
+        this.loader = false;
+        this.$scope.resetAction();
+      });
+  }
+}
