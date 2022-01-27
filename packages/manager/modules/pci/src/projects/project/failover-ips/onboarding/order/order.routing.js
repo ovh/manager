9b9cd38b7d803531cd7f7873f01d24be0f185668
Cyril Biencourt <cyril.biencourt@ovhcloud.com>
2022-01-27 11:47:36 +0100

    fix(pci.ips): use common order flow for pci ips
    Signed-off-by: Cyril Biencourt <cyril.biencourt@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/failover-ips/onboarding/order/order.routing.js
+++ packages/manager/modules/pci/src/projects/project/failover-ips/onboarding/order/order.routing.js
@@ -1,70 +1,43 @@
 import filter from 'lodash/filter';
-import map from 'lodash/map';
+import get from 'lodash/get';
+
+const REGIONS = {
+  EU: 'EUROPE',
+  CA: 'CANADA',
+  US: 'USA',
+};
 
 export default /* @ngInject */ ($stateProvider, coreConfigProvider) => {
-  if (!coreConfigProvider.isRegion('US')) {
-    $stateProvider.state('pci.projects.project.failover-ips.onboarding.order', {
-      url: '/order',
-      layout: 'modal',
-      views: {
-        modal: {
-          component: 'pciProjectFailoverIpsLegacyOrder',
-        },
+  $stateProvider.state('pci.projects.project.failover-ips.onboarding.order', {
+    url: '/order',
+    layout: 'modal',
+    views: {
+      modal: {
+        component: 'pciProjectFailoverIpsOrderOrder',
       },
-      resolve: {
-        breadcrumb: /* @ngInject */ () => null,
-        regions: /* @ngInject */ (OvhApiCloudProjectRegion, projectId) =>
-          OvhApiCloudProjectRegion.v6()
-            .query({
-              serviceName: projectId,
-            })
-            .$promise.then((regions) =>
-              Promise.all(
-                map(
-                  regions,
-                  (id) =>
-                    OvhApiCloudProjectRegion.v6().get({
-                      serviceName: projectId,
-                      id,
-                    }).$promise,
+    },
+    resolve: {
+      breadcrumb: /* @ngInject */ () => null,
+      /* @ngInject */
+      products: (OvhApiOrderCatalogFormatted) =>
+        OvhApiOrderCatalogFormatted.v6()
+          .get({
+            catalogName: 'ip',
+            ovhSubsidiary: coreConfigProvider.getUser().ovhSubsidiary,
+          })
+          .$promise.then(({ plans }) =>
+            filter(
+              plans,
+              (offer) =>
+                /failover/.test(offer.planCode) &&
+                offer.invoiceName.includes(
+                  get(REGIONS, coreConfigProvider.getRegion()),
                 ),
-              ),
             ),
+          ),
 
-        goBack: /* @ngInject */ (goToFailoverIpsOnboarding) =>
-          goToFailoverIpsOnboarding,
-      },
-    });
-  } else {
-    $stateProvider.state('pci.projects.project.failover-ips.onboarding.order', {
-      url: '/order',
-      layout: 'modal',
-      views: {
-        modal: {
-          component: 'pciProjectFailoverIpsOrderOrder',
-        },
-      },
-      resolve: {
-        breadcrumb: /* @ngInject */ () => null,
-        /* @ngInject */
-        products: (OvhApiOrderCatalogFormatted) =>
-          OvhApiOrderCatalogFormatted.v6()
-            .get({
-              catalogName: 'ip',
-              ovhSubsidiary: 'US',
-            })
-            .$promise.then(({ plans }) =>
-              filter(
-                plans,
-                (offer) =>
-                  /failover/.test(offer.planCode) &&
-                  /USA/.test(offer.invoiceName),
-              ),
-            ),
-
-        goBack: /* @ngInject */ (goToFailoverIpsOnboarding) =>
-          goToFailoverIpsOnboarding,
-      },
-    });
-  }
+      goBack: /* @ngInject */ (goToFailoverIpsOnboarding) =>
+        goToFailoverIpsOnboarding,
+    },
+  });
 };
