02d638820671178a86f293965ee3969056b87d23
Jisay <jean-christophe.alleman@corp.ovh.com>
2022-02-01 10:17:11 +0100

    feat(pnr): add user account menu
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

---
+++ packages/manager/apps/container/src/container/nav-reshuffle/header/user-account-menu/button.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
+
+import style from './style.module.scss';
+
+const LanguageButton = ({ children, show, onClick }) => {
+  const { t } = useTranslation('user-account-menu');
+  return (
+    <button
+      aria-haspopup={show}
+      aria-expanded={show}
+      aria-label={t('user_account_menu_manage_my_account')}
+      title={t('user_account_menu_manage_my_account')}
+      type="button"
+      className={`${style.button} oui-navbar-link oui-navbar-link_dropdown`}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick(!show);
+      }}
+    >
+      <span className="oui-navbar-link__wrapper">
+        <span className="oui-navbar-link__text">{children}</span>
+      </span>
+    </button>
+  );
+};
+
+LanguageButton.propTypes = {
+  children: PropTypes.node,
+  show: PropTypes.bool,
+  onClick: PropTypes.func,
+};
+
+export default LanguageButton;
