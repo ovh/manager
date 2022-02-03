d4fc098eff18aad852c2dbd4aefbdc9a5614c7b6
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2022-02-03 09:55:00 +0100

    feat(pci.storage.databases): add grafana engine (#6376)
    ref: PUD-1231
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/databases/features.constants.js
+++ packages/manager/modules/pci/src/projects/project/storages/databases/features.constants.js
@@ -22,6 +22,7 @@ const features = {
     DATABASE_TYPES.POSTGRESQL,
     DATABASE_TYPES.REDIS,
     DATABASE_TYPES.OPEN_SEARCH,
+    DATABASE_TYPES.GRAFANA,
   ],
   showSSL: [
     DATABASE_TYPES.MONGO_DB,
@@ -37,6 +38,7 @@ const features = {
     DATABASE_TYPES.KAFKA,
     DATABASE_TYPES.REDIS,
     DATABASE_TYPES.OPEN_SEARCH,
+    DATABASE_TYPES.GRAFANA,
   ],
   host: [
     DATABASE_TYPES.MONGO_DB,
@@ -45,6 +47,7 @@ const features = {
     DATABASE_TYPES.KAFKA,
     DATABASE_TYPES.REDIS,
     DATABASE_TYPES.OPEN_SEARCH,
+    DATABASE_TYPES.GRAFANA,
   ],
   serviceURI: [
     DATABASE_TYPES.MONGO_DB,
@@ -53,6 +56,7 @@ const features = {
     DATABASE_TYPES.KAFKA,
     DATABASE_TYPES.REDIS,
     DATABASE_TYPES.OPEN_SEARCH,
+    DATABASE_TYPES.GRAFANA,
   ],
   hideMiddleColumn: [DATABASE_TYPES.KAFKA_MIRROR_MAKER],
   allowedIpsTab: [
@@ -62,6 +66,7 @@ const features = {
     DATABASE_TYPES.KAFKA,
     DATABASE_TYPES.REDIS,
     DATABASE_TYPES.OPEN_SEARCH,
+    DATABASE_TYPES.GRAFANA,
   ],
   poolsTab: [DATABASE_TYPES.POSTGRESQL],
   showKibanaURI: [DATABASE_TYPES.OPEN_SEARCH],
@@ -80,6 +85,7 @@ const features = {
   replicationsTab: [DATABASE_TYPES.KAFKA_MIRROR_MAKER],
   showServiceIntegration: [DATABASE_TYPES.KAFKA],
   restApi: [DATABASE_TYPES.KAFKA],
+  resetAdminUserFromDashboard: [DATABASE_TYPES.GRAFANA],
 };
 
 export default function isFeatureActivated(feature, databaseType) {
