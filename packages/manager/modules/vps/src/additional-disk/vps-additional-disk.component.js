ddf9ebec425fac717dcbb89c107665dd8a3b1b2b
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2022-02-04 15:01:34 +0100

    feat(vps.additional-disk): improve additional-disk list page
    ref: MANAGER-7322
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/vps/src/additional-disk/vps-additional-disk.component.js
+++ packages/manager/modules/vps/src/additional-disk/vps-additional-disk.component.js
@@ -2,11 +2,12 @@ import controller from './vps-additional-disk.controller';
 import template from './vps-additional-disk.html';
 
 export default {
+  name: 'ovhManagerVpsAdditionalDisk',
+  controller,
+  template,
   bindings: {
     serviceName: '<',
     tabSummary: '<',
+    goToOrderAdditionalDisk: '<',
   },
-  controller,
-  name: 'ovhManagerVpsAdditionnalDisk',
-  template,
 };
