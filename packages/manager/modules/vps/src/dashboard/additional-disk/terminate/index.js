2eb0e1f17aa51c07a428aa30c6693df6b92089c7
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2022-02-07 10:19:52 +0100

    refactor(vps.dashboard): use common terminate additional disk component
    ref: MANAGER-7322
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/vps/src/dashboard/additional-disk/terminate/index.js
+++ packages/manager/modules/vps/src/dashboard/additional-disk/terminate/index.js
@@ -1,15 +1,15 @@
 import angular from 'angular';
 import '@uirouter/angularjs';
 
-import routing from './vps-option-terminate.routing';
-import component from './vps-option-terminate.component';
+import additionalDiskTerminate from '../../../components/additional-disk/terminate';
+
+import routing from './terminate.routing';
 
 const moduleName = 'vpsDashboardTerminateOptionModule';
 
 angular
-  .module(moduleName, ['ui.router'])
+  .module(moduleName, ['ui.router', additionalDiskTerminate])
   .config(routing)
-  .component('vpsDashboardTerminateOption', component)
   .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
