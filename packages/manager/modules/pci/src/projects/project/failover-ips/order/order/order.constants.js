5bb17c3db7fa9eab1bfcce1c8111ead0afa16394
lizardK <cyril.biencourt@corp.ovh.com>
2022-02-09 13:47:50 +0100

    fix(pci.ips): use common order flow for pci ips (#6371)
    Signed-off-by: Cyril Biencourt <cyril.biencourt@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/failover-ips/order/order/order.constants.js
+++ packages/manager/modules/pci/src/projects/project/failover-ips/order/order/order.constants.js
@@ -1,5 +1,8 @@
-export const ORDER_URL =
-  'https://us.ovhcloud.com/order/express/#/express/review?products=';
+export const ORDER_URL = {
+  EU: 'https://www.ovh.com/order/express/#/express/review?products=',
+  CA: 'https://ca.ovh.com/order/express/#/express/review?products=',
+  US: 'https://us.ovhcloud.com/order/express/#/express/review?products=',
+};
 
 export default {
   ORDER_URL,
