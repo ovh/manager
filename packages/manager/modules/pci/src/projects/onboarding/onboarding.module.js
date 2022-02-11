894d4641400d6c3948a3478e19f4f0846e233684
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2022-02-11 14:46:32 +0100

    feat(projects.onboarding): disable project creation for trusted user
    ref: MANAGER-8738
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/onboarding/onboarding.module.js
+++ packages/manager/modules/pci/src/projects/onboarding/onboarding.module.js
@@ -6,12 +6,14 @@ import 'angular-translate';
 import '@ovh-ux/ui-kit';
 
 import routing from './onboarding.routing';
+import component from './onboarding.component';
 
 const moduleName = 'ovhManagerPciProjectsOnboarding';
 
 angular
   .module(moduleName, ['oui', 'ovhManagerCore', 'pascalprecht.translate'])
   .config(routing)
+  .component('pciProjectsOnboarding', component)
   .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
