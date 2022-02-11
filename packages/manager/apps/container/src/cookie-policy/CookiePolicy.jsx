3ff2a800da2d37bd684282f25b1a32e4bddad0ef
Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
2022-02-11 09:08:10 +0100

    fix(translations): use Trans component instead of setHtml
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

--- packages/manager/apps/container/src/cookie-policy/CookiePolicy.jsx
+++ packages/manager/apps/container/src/cookie-policy/CookiePolicy.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+import { useTranslation, Trans } from 'react-i18next';
 import PropTypes from 'prop-types';
 
 import ovhCloudLogo from './assets/logo-ovhcloud.png';
@@ -15,21 +15,6 @@ const CookiePolicy = React.forwardRef((props, ref) => {
   const moreInfoLink = links.moreInfo[ovhSubsidiary];
   const clickHereLink = links.changeOpinionLink[ovhSubsidiary];
 
-  const moreInfoHtml = () => {
-    return {
-      __html: t('cookie_policy_description_1', {
-        url: moreInfoLink,
-      }),
-    };
-  };
-  const clickHereHtml = () => {
-    return {
-      __html: t('cookie_policy_description_5', {
-        url: clickHereLink,
-      }),
-    };
-  };
-
   return (
     <template ref={ref} id="manager-cookie-policy-banner-modal">
       <div className="cookie-policy-modal">
@@ -37,13 +22,29 @@ const CookiePolicy = React.forwardRef((props, ref) => {
           <img src={ovhCloudLogo} alt="ovh-cloud-logo" />
         </div>
         <h4 className="text-center">{t('cookie_policy_title')}</h4>
-        <p dangerouslySetInnerHTML={moreInfoHtml()}></p>
+        <p>
+          <Trans
+            t={t}
+            i18nKey="cookie_policy_description_1"
+            values={{
+              url: moreInfoLink,
+            }}
+          ></Trans>
+        </p>
         <ul>
           <li>{t('cookie_policy_description_3')}</li>
         </ul>
         <p>{t('cookie_policy_description_2')}</p>
         <span>{t('cookie_policy_description_4')}</span>
-        <span dangerouslySetInnerHTML={clickHereHtml()}></span>
+        <span>
+          <Trans
+            t={t}
+            i18nKey="cookie_policy_description_5"
+            values={{
+              url: clickHereLink,
+            }}
+          ></Trans>
+        </span>
         <div className="banner-buttons w-100 d-flex flex-wrap justify-content-center align-items-center flex-column">
           <button className="accept mb-2 oui-button oui-button_primary">
             <span>{t('cookie_policy_accept')}</span>
