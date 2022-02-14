b61e0a8c00d41efcdab285fdf214e138018f26f7
frenauvh <florian.renaut@corp.ovh.com>
2022-02-14 09:41:37 +0100

    feat(container): remove service count mock use services-count 2api
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/apps/container/src/container/nav-reshuffle/sidebar/navigation-tree/pci.js
+++ packages/manager/apps/container/src/container/nav-reshuffle/sidebar/navigation-tree/pci.js
@@ -7,6 +7,7 @@ export default {
         {
           id: 'pci-instances',
           translation: 'sidebar_pci_instances',
+          serviceType: 'CLOUD_PROJECT_INSTANCE',
         },
         {
           id: 'pci-bare-metal',
@@ -21,22 +22,27 @@ export default {
         {
           id: 'pci-block-storage',
           translation: 'sidebar_pci_block_storage',
+          serviceType: 'CLOUD_PROJECT_VOLUME',
         },
         {
           id: 'pci-databases',
           translation: 'sidebar_pci_databases',
+          serviceType: 'CLOUD_PROJECT_DATABASE',
         },
         {
           id: 'pci-cloud-archive',
           translation: 'sidebar_pci_cloud_archive',
+          serviceType: 'CLOUD_PROJECT_STORAGE',
         },
         {
           id: 'pci-volume-snapshot',
           translation: 'sidebar_pci_volume_snapshot',
+          serviceType: 'CLOUD_PROJECT_VOLUME_SNAPSHOT',
         },
         {
           id: 'pci-instance-backup',
           translation: 'sidebar_pci_instance_backup',
+          serviceType: 'CLOUD_PROJECT_SNAPSHOT',
         },
       ],
     },
@@ -47,14 +53,17 @@ export default {
         {
           id: 'pci-load-balancer',
           translation: 'sidebar_pci_load_balancer',
+          serviceType: 'CLOUD_PROJECT_LOADBALANCER',
         },
         {
           id: 'pci-private-network',
           translation: 'sidebar_pci_private_network',
+          serviceType: 'CLOUD_PROJECT_PRIVATE_NETWORK',
         },
         {
           id: 'pci-failover-ip',
           translation: 'sidebar_pci_failover_ip',
+          serviceType: 'CLOUD_PROJECT_IP_FAILOVER',
         },
       ],
     },
@@ -65,14 +74,17 @@ export default {
         {
           id: 'pci-kubernetes',
           translation: 'sidebar_pci_kubernetes',
+          serviceType: 'CLOUD_PROJECT_KUBE',
         },
         {
           id: 'pci-private-registry',
           translation: 'sidebar_pci_private_registry',
+          serviceType: 'CLOUD_PROJECT_CONTAINER_REGISTRY',
         },
         {
           id: 'pci-workflow',
           translation: 'sidebar_pci_workflow',
+          serviceType: 'CLOUD_PROJECT_WORKFLOW_BACKUP',
         },
       ],
     },
@@ -83,18 +95,22 @@ export default {
         {
           id: 'pci-ai-notebooks',
           translation: 'sidebar_pci_ai_notebooks',
+          serviceType: 'CLOUD_PROJECT_AI_NOTEBOOK',
         },
         {
           id: 'pci-ai-training',
           translation: 'sidebar_pci_ai_training',
+          serviceType: 'CLOUD_PROJECT_AI_JOB',
         },
         {
           id: 'pci-ai-app',
           translation: 'sidebar_pci_ai_app',
+          serviceType: 'CLOUD_PROJECT_AI_APP',
         },
         {
           id: 'pci-ml-serving',
           translation: 'sidebar_pci_ml_serving',
+          serviceType: 'CLOUD_PROJECT_AI_SERVING',
         },
       ],
     },
@@ -105,14 +121,17 @@ export default {
         {
           id: 'pci-analytics-data-platform',
           translation: 'sidebar_pci_ai_analytics_data_platform',
+          serviceType: 'ANALYTICS_PLATFORMS',
         },
         {
           id: 'pci-data-processing',
           translation: 'sidebar_pci_data_processing',
+          serviceType: 'CLOUD_PROJECT_DATAPROCESSING_JOBS',
         },
         {
           id: 'pci-logs-data-platform',
           translation: 'sidebar_pci_logs_data_platform',
+          serviceType: 'DBAAS_LOGS',
         },
       ],
     },
@@ -133,6 +152,7 @@ export default {
         {
           id: 'pci-users-roles',
           translation: 'sidebar_pci_users_roles',
+          serviceType: 'CLOUD_PROJECT_USER',
         },
         {
           id: 'pci-quota-region',
@@ -141,6 +161,7 @@ export default {
         {
           id: 'pci-ssh-keys',
           translation: 'sidebar_pci_ssh_keys',
+          serviceType: 'CLOUD_PROJECT_SSHKEY',
         },
         {
           id: 'pci-billing',
@@ -149,6 +170,7 @@ export default {
         {
           id: 'pci-credits-vouchers',
           translation: 'sidebar_pci_credits_vouchers',
+          serviceType: 'CLOUD_PROJECT_CREDIT',
         },
         {
           id: 'pci-contacts-rights',
