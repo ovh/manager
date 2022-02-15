68554582c4709210fe4da55d71bd9033023d1000
Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>
2022-02-15 14:31:28 +0530

    fix: sepa direct debt help links
    the language of sepa direct help links must be same as that of subsidiary
    
    ref: DTRSD-68625
    
    Signed-off-by: Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>

--- packages/manager/modules/billing/src/payment/method/add/views/legacyBillingAddress/constants.js
+++ packages/manager/modules/billing/src/payment/method/add/views/legacyBillingAddress/constants.js
@@ -1,15 +1,16 @@
 export const PERSONAL_DATA_PROTECTION_BY_OVH_LINK = {
-  fr:
+  FR:
     'https://www.ovh.com/fr/protection-donnees-personnelles/privacy_policy.xml',
-  en: 'https://www.ovh.ie/personal-data-protection/privacy_policy.xml',
-  de: 'https://www.ovh.de/schutz-personenbezogener-daten/privacy_policy.xml',
+  DEFAULT: 'https://www.ovh.ie/personal-data-protection/privacy_policy.xml',
+  DE: 'https://www.ovh.de/schutz-personenbezogener-daten/privacy_policy.xml',
 };
 
 export const PERSONAL_DATA_PROTECTION_BY_YOUSIGN_LINK = {
-  fr:
+  FR:
     'https://yousign.com/fr-fr/conditions-generales#souscription-aux-services-et-acceptation-des-conditions-generales',
-  en: 'https://yousign.com/privacy',
-  de: 'https://yousign.com/de-de/datenschutz',
+  DEFAULT: 'https://yousign.com/terms',
+  DE:
+    'https://yousign.com/de-de/nutzungsbedingungen#serviceabonnement-und-akzeptanz-der-allgemeinen-geschaftsbedingungen',
 };
 
 export default {
