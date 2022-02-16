1d006924485f17aff7e096f9ef1b4002d14e93b6
Yann Lojewski <yann.lojewski@gmail.com>
2022-02-16 11:11:37 +0100

    feat(object-storage): move high performance to beta 2 (#6176)
    ref: MANAGER-8122
    
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/containers/containers.constants.js
+++ packages/manager/modules/pci/src/projects/project/storages/containers/containers.constants.js
@@ -58,6 +58,28 @@ export const STORAGE_GATEWAY = {
   US: 'gateways.storage.REGION.cloud.ovh.us',
 };
 
+export const STORAGE_PRICES_LINK = {
+  DEFAULT: 'https://www.ovhcloud.com/en/public-cloud/prices/#storage',
+  DE: 'https://www.ovhcloud.com/de/public-cloud/prices/#storage',
+  AU: 'https://www.ovhcloud.com/en-au/public-cloud/prices/#storage',
+  CA: 'https://www.ovhcloud.com/en-ca/public-cloud/prices/#storage',
+  GB: 'https://www.ovhcloud.com/en-gb/public-cloud/prices/#storage',
+  IE: 'https://www.ovhcloud.com/en-ie/public-cloud/prices/#storage',
+  SG: 'https://www.ovhcloud.com/en-sg/public-cloud/prices/#storage',
+  ES: 'https://www.ovhcloud.com/es-es/public-cloud/prices/#storage',
+  QC: 'https://www.ovhcloud.com/fr-ca/public-cloud/prices/#storage',
+  FR: 'https://www.ovhcloud.com/fr/public-cloud/prices/#storage',
+  MA: 'https://www.ovhcloud.com/fr-ma/public-cloud/prices/#storage',
+  SN: 'https://www.ovhcloud.com/fr-sn/public-cloud/prices/#storage',
+  TN: 'https://www.ovhcloud.com/fr-tn/public-cloud/prices/#storage',
+  IT: 'https://www.ovhcloud.com/it/public-cloud/prices/#storage',
+  NL: 'https://www.ovhcloud.com/nl/public-cloud/prices/#storage',
+  PL: 'https://www.ovhcloud.com/pl/public-cloud/prices/#storage',
+  PT: 'https://www.ovhcloud.com/pt/public-cloud/prices/#armazenamento',
+  WS: 'https://www.ovhcloud.com/es/public-cloud/prices/#storage',
+  WE: 'https://www.ovhcloud.com/en/public-cloud/prices/#storage',
+};
+
 export const X_CONTAINER_HEADERS_REGEX = /^(X-Container|X-Storage)/i;
 
 export const X_AUTH_TOKEN = 'X-Auth-Token';
@@ -96,6 +118,7 @@ export default {
   PUBLIC_CLOUD_PRODUCT_NAME,
 
   STORAGE_GATEWAY,
+  STORAGE_PRICES_LINK,
 
   X_AUTH_TOKEN,
   X_CONTAINER_HEADERS_REGEX,
