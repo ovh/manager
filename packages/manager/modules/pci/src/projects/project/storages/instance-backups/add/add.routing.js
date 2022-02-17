5b8c1057b90a08393ffe85c96b5aa2eed0801c50
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2022-02-17 10:35:06 +0100

    feat(pci.instance-backups): exclude no trusted instance flavors
    ref: MANAGER-8791
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/instance-backups/add/add.routing.js
+++ packages/manager/modules/pci/src/projects/project/storages/instance-backups/add/add.routing.js
@@ -1,6 +1,9 @@
 import find from 'lodash/find';
 import filter from 'lodash/filter';
 
+import { FLAVORS_FEATURES_FLIPPING_MAP } from '../../../instances/instances.constants';
+import { EXCLUDE_FLAVOR_CATEGORIES } from '../../../instances/add/add.constants';
+
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('pci.projects.project.storages.instance-backups.add', {
     url: '/new?instanceBackupId',
@@ -52,6 +55,17 @@ export default /* @ngInject */ ($stateProvider) => {
               status === 'ACTIVE' && find(regions, { region: backup.region }),
           ),
         ),
+
+      excludeCategories: /* @ngInject */ (pciFeatures) => {
+        const flavorCategories = Object.keys(FLAVORS_FEATURES_FLIPPING_MAP);
+        const toExclude = flavorCategories.filter((flavor) => {
+          return !pciFeatures.isFeatureAvailable(
+            FLAVORS_FEATURES_FLIPPING_MAP[flavor],
+          );
+        });
+
+        return EXCLUDE_FLAVOR_CATEGORIES.concat(toExclude);
+      },
 
       goBack: /* @ngInject */ (goToInstanceBackups) => goToInstanceBackups,
     },
