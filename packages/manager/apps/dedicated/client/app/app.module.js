d00391aa0dff4067ab3b1f0514d44b01962c716d
Jisay <jean-christophe.alleman@corp.ovh.com>
2022-02-21 15:53:46 +0100

    feat(dedicated): add chatbot open callback
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/app.module.js
+++ packages/manager/apps/dedicated/client/app/app.module.js
@@ -451,6 +451,13 @@ export default async (containerEl, shellClient) => {
           environment.getApplicationName(),
         );
       },
+    )
+    .run(
+      /* @ngInject */ ($rootScope) => {
+        shellClient.ux.onOpenChatbot(() => {
+          $rootScope.$emit('ovh-chatbot:open');
+        });
+      },
     );
 
   import('./app.bundle').then(() => {
