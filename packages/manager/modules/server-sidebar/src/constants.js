6eb87a3ff739b7e1217c6329845c5fd126562746
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2022-02-24 09:46:07 +0100

    feat(telecom): manage telecom beta sidebar
    ref: MANAGER-8687
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/modules/server-sidebar/src/constants.js
+++ packages/manager/modules/server-sidebar/src/constants.js
@@ -1,6 +1,7 @@
 export const DEDICATED = 'DEDICATED';
 export const WEB = 'WEB';
 export const TELECOM = 'TELECOM';
+export const TELECOM_BETA = 'TELECOM_BETA';
 
 export const HPC_NAMESPACE = 'hpc';
 
@@ -9,4 +10,5 @@ export default {
   WEB,
   TELECOM,
   HPC_NAMESPACE,
+  TELECOM_BETA,
 };
