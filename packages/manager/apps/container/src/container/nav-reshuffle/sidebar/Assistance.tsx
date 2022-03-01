c4e8738ce96265b6b7cc9c8f6dfe69b6336b2c0d
Jisay <jean-christophe.alleman@corp.ovh.com>
2022-03-01 11:28:28 +0100

    feat(pnr): add onboarding menu entry
    ref: MANAGER-8774
    
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

--- packages/manager/apps/container/src/container/nav-reshuffle/sidebar/Assistance.tsx
+++ packages/manager/apps/container/src/container/nav-reshuffle/sidebar/Assistance.tsx
@@ -1,7 +1,10 @@
 import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
+
 import { useURL } from '@/container/common/urls-constants';
 import ApplicationContext from '@/context';
+import useOnboarding from '@/core/onboarding';
+
 import SidebarLink from './SidebarLink';
 
 function AssistanceSidebar(): JSX.Element {
@@ -14,6 +17,7 @@ function AssistanceSidebar(): JSX.Element {
   const urls = useURL(environment);
 
   const hasAdvancedSupport = ['EU', 'CA'].includes(environment.getRegion());
+  const { displayOnboardingWidget } = useOnboarding();
 
   return (
     <ul>
@@ -65,6 +69,10 @@ function AssistanceSidebar(): JSX.Element {
             onClick={() => shell.getPlugin('ux').openChatbot()}
           />
         )}
+        <SidebarLink
+          node={{ translation: 'sidebar_assistance_onboarding' }}
+          onClick={() => displayOnboardingWidget()}
+        />
       </li>
     </ul>
   );
