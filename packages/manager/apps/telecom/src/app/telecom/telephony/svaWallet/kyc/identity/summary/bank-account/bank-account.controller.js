ad910b5e6523a139c72ea937fc2b10f049079336
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2022-03-04 16:26:38 +0100

    fix(sva): account owner is not refreshed when iban is updated
    ref: MANAGER-8899
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

---
+++ packages/manager/apps/telecom/src/app/telecom/telephony/svaWallet/kyc/identity/summary/bank-account/bank-account.controller.js
@@ -0,0 +1,38 @@
+export default class KycIdentitySummaryBankAccountController {
+  /* @ngInject */
+  constructor(ovhPaymentMethodHelper) {
+    this.isOpenModal = false;
+    this.isValidIban = ovhPaymentMethodHelper.isValidIban;
+  }
+
+  $onInit() {
+    this.isEditable = this.svaWallet.kycStatus !== 'BLOCKED';
+    this.isLoading = false;
+  }
+
+  goToEditIban() {
+    this.isOpenModal = true;
+  }
+
+  confirm() {
+    this.errorMessage = null;
+
+    this.isLoading = true;
+    this.saveWalletIban(this.model)
+      .then(() => {
+        this.isOpenModal = false;
+        this.bankAccount = this.model;
+        this.updateIbanSuccessMessage = true;
+      })
+      .catch(({ data: error }) => {
+        this.errorMessage = error.message;
+      })
+      .finally(() => {
+        this.isLoading = false;
+      });
+  }
+
+  cancel() {
+    this.isOpenModal = false;
+  }
+}
