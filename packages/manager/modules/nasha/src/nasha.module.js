bcd8cff41c6c9c1b0c3442e37c8771125662dd77
Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
2022-03-07 15:19:11 +0100

    feat(nasha): add onboarding module
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

--- packages/manager/modules/nasha/src/nasha.module.js
+++ packages/manager/modules/nasha/src/nasha.module.js
@@ -4,11 +4,17 @@ import '@uirouter/angularjs';
 import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
 
 import routing from './nasha.routing';
+import onboarding from './onboarding';
 
 const moduleName = 'ovhManagerNasha';
 
 angular
-  .module(moduleName, ['oui', ListLayoutHelper.moduleName, 'ui.router'])
+  .module(moduleName, [
+    'oui',
+    ListLayoutHelper.moduleName,
+    'ui.router',
+    onboarding,
+  ])
   .config(routing);
 
 export default moduleName;
