c916d3a0331e6536de3e37d085a1f3bcd800e02e
Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
2022-03-07 14:37:14 +0100

    feat(managedbaremetal): add onboarding module
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/managedBaremetal/managed-baremetal.module.js
+++ packages/manager/apps/dedicated/client/app/managedBaremetal/managed-baremetal.module.js
@@ -4,11 +4,12 @@ import '@uirouter/angularjs';
 import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
 
 import routing from './managed-baremetal.routing';
+import onboarding from './onboarding';
 
 const moduleName = 'ovhManagerManagedBaremetal';
 
 angular
-  .module(moduleName, ['ui.router', ListLayoutHelper.moduleName])
+  .module(moduleName, ['ui.router', ListLayoutHelper.moduleName, onboarding])
   .config(routing)
   .run(/* @ngTranslationsInject:json ./translations */);
 
---
+++ packages/manager/apps/dedicated/client/app/managedBaremetal/onboarding/onboarding.module.js
@@ -0,0 +1,13 @@
+import { OnboardingLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
+
+import component from './onboarding.component';
+import routing from './onboarding.routing';
+
+const moduleName = 'managedBaremetalOnboarding';
+
+angular
+  .module(moduleName, [OnboardingLayoutHelper])
+  .component('managedBaremetalOnboardingComponent', component)
+  .config(routing);
+
+export default moduleName;
