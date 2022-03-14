91fc088954a195255741a1c2eabc97bd76df28eb
frenauvh <florian.renaut@corp.ovh.com>
2022-03-14 08:38:33 +0100

    feat(container): add tracking, update urls
    ref: MANAGER-8915
    
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/apps/container/src/navbar/brand.jsx
+++ packages/manager/apps/container/src/navbar/brand.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function NavbarBrand({ targetURL }) {
+function NavbarBrand({ targetURL, onClick }) {
   return (
-    <a className="oui-navbar__brand" href={targetURL} aria-label="OVHcloud">
+    <a
+      className="oui-navbar__brand"
+      href={targetURL}
+      aria-label="OVHcloud"
+      onClick={onClick}
+    >
       <span className="oui-icon oui-icon-ovh" aria-hidden="true"></span>
     </a>
   );
@@ -11,6 +16,11 @@ function NavbarBrand({ targetURL }) {
 
 NavbarBrand.propTypes = {
   targetURL: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+NavbarBrand.defaultProps = {
+  onClick: () => {},
 };
 
 export default NavbarBrand;
