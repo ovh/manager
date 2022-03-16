b5d07d1d21ae7fd5145a99731bfd6b4561eaaa47
Bastien Verdebout <bastien.verdebout@ovhcloud.com>
2022-03-16 14:11:11 +0100

    fix(public cloud): 404 url for AI product documentation

--- packages/manager/modules/pci/src/projects/project/training/onboarding/onboarding.constants.js
+++ packages/manager/modules/pci/src/projects/project/training/onboarding/onboarding.constants.js
@@ -1,13 +1,13 @@
 export const GUIDES = [
   {
     id: 'eai-job-submit',
-    link: 'https://docs.ovh.com/gb/en/ai-training/submit-job/',
+    link: 'https://docs.ovh.com/gb/en/publiccloud/ai/training/submit-job/',
     tracking:
       'public-cloud::pci::projects::project::training::onboarding::go-to-docs-tasks',
   },
   {
     id: 'install-cli',
-    link: 'https://docs.ovh.com/gb/en/ai-training/install-client/',
+    link: 'https://docs.ovh.com/gb/en/publiccloud/ai/cli/install-client/',
     tracking:
       'public-cloud::pci::projects::project::training::onboarding::go-to-docs-install',
   },
