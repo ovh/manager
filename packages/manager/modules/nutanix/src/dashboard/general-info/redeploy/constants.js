d0335c4a3a6bd09c5e90021207224a214d2adcdb
Anoop <anooparveti@gmail.com>
2022-03-16 18:13:44 +0530

    fix(nutanix): hide ips field when prism central type is alone
     Hide 'IPs' field when the selected Prism Central Type is 'Alone' while nutanc cluster redeploy
     ref: DTRSD-77058
    
    Signed-off-by: Anoop <anooparveti@gmail.com>

--- packages/manager/modules/nutanix/src/dashboard/general-info/redeploy/constants.js
+++ packages/manager/modules/nutanix/src/dashboard/general-info/redeploy/constants.js
@@ -5,7 +5,14 @@ export const REDEPLOY_CONFIG_OPTIONS = {
   CUSTOM: 'redeployCustomConfig',
 };
 
-export const PRISM_CENTRAL_TYPES = ['alone', 'scale'];
+export const PRISM_CENTRAL_TYPE_ALONE = 'alone';
+
+export const PRISM_CENTRAL_TYPE_SCALE = 'scale';
+
+export const PRISM_CENTRAL_TYPES = [
+  PRISM_CENTRAL_TYPE_ALONE,
+  PRISM_CENTRAL_TYPE_SCALE,
+];
 
 export const IPV4_REGEX = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
 
@@ -26,6 +33,8 @@ export const CLUSTER_CONFIG_TERMS = {
 export default {
   TRACKING_PREFIX,
   REDEPLOY_CONFIG_OPTIONS,
+  PRISM_CENTRAL_TYPE_ALONE,
+  PRISM_CENTRAL_TYPE_SCALE,
   PRISM_CENTRAL_TYPES,
   IPV4_REGEX,
   IPV4_BLOCK_REGEX,
