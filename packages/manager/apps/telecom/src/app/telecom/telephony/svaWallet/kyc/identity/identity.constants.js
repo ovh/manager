9ef948cd756cabb6570a5d9460361b631a2b7cef
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2022-03-21 14:47:26 +0100

    fix(sva): post review
    i added comment for urlwebsite.
    
    ref: MANAGER-8592
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/apps/telecom/src/app/telecom/telephony/svaWallet/kyc/identity/identity.constants.js
+++ packages/manager/apps/telecom/src/app/telecom/telephony/svaWallet/kyc/identity/identity.constants.js
@@ -1,7 +1,9 @@
 export const REGEX_VALIDATORS = {
   SIRET: /^\d{14}$/,
   APE: /^\d{4}[a-zA-Z]$/,
-  // URLWEBSITE limits the insertion of invalid URLs. While allowing not to put http or https, but if it is present it requires "://". Also allows to leave empty. Valid exemple: http://exemple.com | exemple.com . Invalid exemple: 1234 | https://exemple?com.
+  // URLWEBSITE limits the insertion of invalid URLs. While allowing not to put http or https, but if it is present it requires "://".
+  // Also allows to leave empty.
+  // Valid exemple: http://exemple.com | exemple.com . Invalid exemple: 1234 | https://exemple?com.
   URLWEBSITE: /(?:^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$)|(?:^$)/,
 };
 
