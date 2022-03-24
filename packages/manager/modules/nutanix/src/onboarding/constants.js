18052468c309f56a8657abb57eb3c9879ebd8c7c
radireddy <adireddyravindra@gmail.com>
2022-03-25 01:18:01 +0530

    fix(onboarding): use updated Nutanix onboarding links (#6717)
    ref: MANAGER-8945
    
    Signed-off-by: Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>

--- packages/manager/modules/nutanix/src/onboarding/constants.js
+++ packages/manager/modules/nutanix/src/onboarding/constants.js
@@ -1,15 +1,25 @@
 export const GUIDES = [];
 
 export const CONTACT_US_PAGE_URL = {
-  FR: 'https://www.ovhcloud.com/fr/lp/nutanix/',
-  GB: 'https://www.ovhcloud.com/en-gb/lp/nutanix/',
-  IE: 'https://www.ovhcloud.com/en-ie/lp/nutanix/',
-  CA: 'https://www.ovhcloud.com/en-ca/lp/nutanix/',
-  QC: 'https://www.ovhcloud.com/fr-ca/lp/nutanix/',
-  AU: 'https://www.ovhcloud.com/en-au/lp/nutanix/',
-  SG: 'https://www.ovhcloud.com/en-sg/lp/nutanix/',
-  ASIA: 'https://www.ovhcloud.com/asia/lp/nutanix/',
-  DEFAULT: 'https://www.ovhcloud.com/en/lp/nutanix/',
+  DE: 'https://www.ovhcloud.com/de/hosted-private-cloud/nutanix/',
+  ES: 'https://www.ovhcloud.com/es-es/hosted-private-cloud/nutanix/',
+  IE: 'https://www.ovhcloud.com/en-ie/hosted-private-cloud/nutanix/',
+  IT: 'https://www.ovhcloud.com/it/hosted-private-cloud/nutanix/',
+  NL: 'https://www.ovhcloud.com/nl/hosted-private-cloud/nutanix/',
+  PL: 'https://www.ovhcloud.com/pl/hosted-private-cloud/nutanix/',
+  PT: 'https://www.ovhcloud.com/pt/hosted-private-cloud/nutanix/',
+  GB: 'https://www.ovhcloud.com/en-gb/hosted-private-cloud/nutanix/',
+  CA: 'https://www.ovhcloud.com/en-ca/hosted-private-cloud/nutanix/',
+  QC: 'https://www.ovhcloud.com/fr-ca/hosted-private-cloud/nutanix/',
+  FR: 'https://www.ovhcloud.com/fr/hosted-private-cloud/nutanix/',
+  MA: 'https://www.ovhcloud.com/fr-ma/hosted-private-cloud/nutanix/',
+  SN: 'https://www.ovhcloud.com/fr-sn/hosted-private-cloud/nutanix/',
+  TN: 'https://www.ovhcloud.com/fr-tn/hosted-private-cloud/nutanix/',
+  AU: 'https://www.ovhcloud.com/en-au/hosted-private-cloud/nutanix/',
+  SG: 'https://www.ovhcloud.com/en-sg/hosted-private-cloud/nutanix/',
+  ASIA: 'https://www.ovhcloud.com/asia/hosted-private-cloud/nutanix/',
+  WS: 'https://www.ovhcloud.com/es/hosted-private-cloud/nutanix/',
+  DEFAULT: 'https://www.ovhcloud.com/en/hosted-private-cloud/nutanix/',
 };
 
 export default { GUIDES, CONTACT_US_PAGE_URL };
