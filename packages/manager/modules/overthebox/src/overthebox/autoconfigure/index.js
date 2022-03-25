cbb1aac260ec98047012760d218066139ec8f0a4
Stéphanie MOALLIC <stephanie.moallic@gmail.com>
2022-03-25 10:00:27 +0100

    feat(overthebox.autoconfigure): add firewall configuration (#6396)
    ref: UXCT-433
    
    Signed-off-by: Stephanie Moallic <stephanie.moallic@corp.ovh.com>

--- packages/manager/modules/overthebox/src/overthebox/autoconfigure/index.js
+++ packages/manager/modules/overthebox/src/overthebox/autoconfigure/index.js
@@ -2,11 +2,12 @@ import component from './overTheBox-autoconfigure.component';
 import routing from './overTheBox-autoconfigure.routing';
 
 import dhcp from './dhcp';
+import firewall from './firewall';
 
 const moduleName = 'ovhManagerOtbAutoconfigure';
 
 angular
-  .module(moduleName, ['ui.router', 'ovh-api-services', dhcp])
+  .module(moduleName, ['ui.router', 'ovh-api-services', dhcp, firewall])
   .component('overTheBoxAutoconfigure', component)
   .config(routing)
   .run(/* @ngTranslationsInject:json ./translations */);
