5d064af0d1f7368259b6bf4a6f8306ffca39012f
Wassim DHIF <wassim.dhif@ovhcloud.com>
2022-03-31 18:26:08 +0200

    feat(private-database): add CPU throttle to CloudDB stats
    Signed-off-by: Wassim DHIF <wassim.dhif@ovhcloud.com>

---
+++ packages/manager/apps/web/client/app/private-database/cpu-throttle/private-database-cpu-throttle.service.js
@@ -0,0 +1,20 @@
+angular.module('services').service(
+  'CpuThrottleService',
+  class CpuThrottleService {
+    constructor($http) {
+      this.$http = $http;
+
+      this.swsProxypassPath = 'hosting/privateDatabase';
+
+      this.rootPath = 'apiv6';
+    }
+
+    getCpuThrottleList(serviceName) {
+      return this.$http
+        .get(
+          `${this.rootPath}/${this.swsProxypassPath}/${serviceName}/cpuThrottle`,
+        )
+        .then((res) => res.data);
+    }
+  },
+);
