4364248ca3992d9e756f4c8ba845a7fcb365d6dc
Anoop N <anooparveti@gmail.com>
2022-03-31 10:56:19 +0530

    fix(dedicated): generalized terminate modal for vps dashboard actions (#6774)
    Common modal for terminating automated backup, additional disk, snapshot from VPS dashboard
     ref: MANAGER-7322
    
    Signed-off-by: Anoop <anooparveti@gmail.com>

--- packages/manager/modules/vps/src/dashboard/modal/terminate-option/terminate-option.routing.js
+++ packages/manager/modules/vps/src/dashboard/modal/terminate-option/terminate-option.routing.js
@@ -6,7 +6,7 @@ export default /* @ngInject */ ($stateProvider) => {
     },
     views: {
       modal: {
-        component: 'ovhManagerVpsComponentAdditionalDiskTerminate',
+        component: 'ovhManagerVpsComponentTerminate',
       },
     },
     layout: 'modal',
