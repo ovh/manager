881a3c6699e47deebb3e3633a4a0dbfb52c20966
Anoop <anooparveti@gmail.com>
2022-03-31 19:40:22 +0530

    fix(vps): update additional-disk termination success sentence
     ref: MANAGER-8857
    
    Signed-off-by: Anoop <anooparveti@gmail.com>

--- packages/manager/modules/vps/src/components/terminate/terminate.constants.js
+++ packages/manager/modules/vps/src/components/terminate/terminate.constants.js
@@ -18,6 +18,15 @@ export const TERMINATE_INFO = {
   windows: 'vps_configuration_cancel_option_step1_info',
 };
 
+export const TERMINATE_SUCCESS_INFO = {
+  additionalDisk: 'vps_additional_disk_terminate_action_terminate_success',
+  automatedBackup: 'vps_configuration_cancel_option_cancel_success',
+  ftpBackup: 'vps_configuration_cancel_option_cancel_success',
+  snapshot: 'vps_configuration_cancel_option_cancel_success',
+  veeam: 'vps_configuration_cancel_option_cancel_success',
+  windows: 'vps_configuration_cancel_option_cancel_success',
+};
+
 export const TRACKING_INFO = {
   automatedBackup: 'backup-auto',
   snapshot: 'snapshot',
@@ -27,4 +36,5 @@ export default {
   TRACKING_PREFIX,
   TITLES,
   TRACKING_INFO,
+  TERMINATE_SUCCESS_INFO,
 };
