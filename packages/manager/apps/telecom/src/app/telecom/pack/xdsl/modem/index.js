e88fb23b450f119d86ad0d306c816343376f0ff9
Stéphanie MOALLIC <stephanie.moallic@gmail.com>
2022-04-13 12:26:11 +0200

    fix(modem.templates): revert to display modem templates (#6752)
    ref: UXCT-440
    
    Signed-off-by: Stephanie Moallic <stephanie.moallic@corp.ovh.com>

--- packages/manager/apps/telecom/src/app/telecom/pack/xdsl/modem/index.js
+++ packages/manager/apps/telecom/src/app/telecom/pack/xdsl/modem/index.js
@@ -18,6 +18,7 @@ import ports from './router/ports';
 import router from './router';
 import lan from './router/lan';
 import service from './service';
+import XdslModemTemplates from './templates';
 
 import component from './pack-xdsl-modem.component';
 import { PACK_XDSL_MODEM } from './pack-xdsl-modem.constant';
@@ -49,6 +50,7 @@ angular
     wifi,
     wifiConfig,
     XdslModemDhcpBdhcp,
+    XdslModemTemplates,
   ])
   .component('packXdslModem', component)
   .config(routing)
