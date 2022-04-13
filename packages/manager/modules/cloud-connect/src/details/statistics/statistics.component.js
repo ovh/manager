608d0203677cb5f3597658579a45a99e601ddb13
Stéphanie MOALLIC <stephanie.moallic@gmail.com>
2022-04-13 12:30:11 +0200

    Revert "feat(details): add statistics" (#6529)
    This reverts commit b4ab59831847ec641461230a7b3dd53dbb24023d.
    
    ref: UXCT-423
    
    Signed-off-by: Stephanie Moallic <stephanie.moallic@corp.ovh.com>

---
+++ packages/manager/modules/cloud-connect/src/details/statistics/statistics.component.js
@@ -0,0 +1,10 @@
+import controller from './statistics.controller';
+import template from './template.html';
+
+export default {
+  bindings: {
+    cloudConnect: '<',
+  },
+  controller,
+  template,
+};
