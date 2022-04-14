3392b3960633c6461b0e0bb4e31bfbe7880d6cd6
Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
2022-04-14 15:54:33 +0200

    feat(tracking): create angularjs package that uses shell tracking plugin
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

---
+++ packages/components/ng-shell-tracking/rollup.config.js
@@ -0,0 +1,28 @@
+import rollupConfig from '@ovh-ux/component-rollup-config';
+
+const config = rollupConfig({
+  input: './src/index.js',
+});
+
+const outputs = [
+  config.es({
+    output: {
+      sourcemap: false,
+    },
+  }),
+];
+
+if (process.env.BUILD === 'production') {
+  outputs.push(config.cjs());
+  outputs.push(
+    config.umd({
+      output: {
+        globals: {
+          angular: 'angular',
+        },
+      },
+    }),
+  );
+}
+
+export default outputs;
