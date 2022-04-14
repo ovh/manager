e85e4359fd8a0338ebd3e313994d76735a0a9fde
Jisay <jean-christophe.alleman@corp.ovh.com>
2022-04-14 16:04:01 +0200

    feat(pnr): feedback v2
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

--- packages/manager/apps/container/src/container/nav-reshuffle/feedback/index.tsx
+++ packages/manager/apps/container/src/container/nav-reshuffle/feedback/index.tsx
@@ -1,97 +1,32 @@
-import React, { useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
-import addDays from 'date-fns/addDays';
-import isAfter from 'date-fns/isAfter';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 
+import { useShell } from '@/context/useApplicationContext';
 import useProductNavReshuffle from '@/core/product-nav-reshuffle';
 
-import { LOCAL_STORAGE_ITEM, REDISPLAY_DAYS_INTERVAL } from './constants';
 import style from './style.module.scss';
-import popoverStyle from '../../common/popover.module.scss';
 
 export const NavReshuffleFeedbackWidget = (): JSX.Element => {
   const { t } = useTranslation('nav-reshuffle/feedback');
-  const {
-    feedbackWidgetOpened,
-    openFeedbackWidget,
-    closeFeebackWidget,
-  } = useProductNavReshuffle();
-
-  // check for visibility
-  const isBoxVisible = () => {
-    const storedInfo = localStorage.getItem(LOCAL_STORAGE_ITEM);
-
-    if (!storedInfo) {
-      return true;
-    }
-
-    const { timestamp } = JSON.parse(storedInfo);
-
-    const redisplayDate = addDays(new Date(timestamp), REDISPLAY_DAYS_INTERVAL);
-    return isAfter(new Date(), redisplayDate);
-  };
-
-  const storeFeedbackStatus = () => {
-    return localStorage.setItem(
-      LOCAL_STORAGE_ITEM,
-      JSON.stringify({
-        timestamp: new Date().getTime(),
-      }),
-    );
-  };
+  const { trackClick } = useShell().getPlugin('tracking');
+  const feedbackUrl = useProductNavReshuffle().getFeedbackUrl();
 
   const onGiveFeedbackLinkClick = () => {
-    storeFeedbackStatus();
-    closeFeebackWidget();
+    trackClick('float_band::user_widget::give_feedback');
   };
-
-  const onHideBtnClick = () => {
-    storeFeedbackStatus();
-    closeFeebackWidget();
-  };
-
-  useEffect(() => {
-    // check for box visibility
-    // the box should be visible each 2 days
-    if (!localStorage.getItem(LOCAL_STORAGE_ITEM)) {
-      // if no localStorage item then create one
-      storeFeedbackStatus();
-    }
-    // check if the last put in localStorage is older than 2 days
-    if (isBoxVisible()) {
-      openFeedbackWidget();
-    }
-  }, []);
 
   return (
-    <>
-      {feedbackWidgetOpened && (
-        <div
-          className={`${style.feedbackWidget} ${popoverStyle.popover} oui-popover`}
-        >
-          <div className="oui-popover__content">
-            <h2>{t('title')}</h2>
-            <p>{t('content')}</p>
-            <div className="d-flex flex-row-reverse justify-content-between">
-              <a
-                className="oui-button oui-button_primary"
-                href={'#'}
-                onClick={onGiveFeedbackLinkClick}
-              >
-                {t('give_feedback_link')}
-              </a>
-              <button
-                className="oui-button oui-button_ghost"
-                onClick={onHideBtnClick}
-              >
-                {t('hide_button')}
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
-    </>
+    <a
+      className={`${style.feedbackWidget} oui-link_icon py-1 px-2`}
+      target="_blank"
+      rel="noopener noreferrer"
+      href={feedbackUrl}
+      aria-label={t('title')}
+      onClick={onGiveFeedbackLinkClick}
+    >
+      <i className="oui-icon oui-icon-chat" aria-hidden="true"></i>
+      <span>{t('title')}</span>
+    </a>
   );
 };
 
