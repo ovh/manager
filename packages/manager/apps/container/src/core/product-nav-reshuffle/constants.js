c55a4a1a42c3bfdba44cf241d1f2ace549114f06
Jisay <jean-christophe.alleman@corp.ovh.com>
2022-04-14 16:04:01 +0200

    feat(pnr): feedback v2
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

---
+++ packages/manager/apps/container/src/core/product-nav-reshuffle/constants.js
@@ -0,0 +1,8 @@
+export const FEEDBACK_URLS = {
+  beta1: 'https://survey.ovh.com/index.php/182319?lang=fr',
+  beta2: 'https://survey.ovh.com/index.php/465758?lang=fr',
+};
+
+export default {
+  FEEDBACK_URLS,
+};
