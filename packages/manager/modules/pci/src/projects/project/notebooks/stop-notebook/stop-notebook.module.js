a58143830216bc182f5b8263f7de1e90cb65807e
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2022-04-15 15:39:00 +0200

    fix: pr comment
    MANAGER-9061
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/notebooks/stop-notebook/stop-notebook.module.js
+++ packages/manager/modules/pci/src/projects/project/notebooks/stop-notebook/stop-notebook.module.js
@@ -1,8 +1,5 @@
 import angular from 'angular';
-import '@ovh-ux/ng-translate-async-loader';
 import '@uirouter/angularjs';
-import 'angular-translate';
-import '@ovh-ux/ui-kit';
 
 import stopNotebook from '../components/stop-notebook';
 import routing from './stop-notebook.routing';
