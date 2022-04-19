d3e034fa6abd0f2fbcbf79defbc9eca3f5dc5933
Stéphanie MOALLIC <stephanie.moallic@gmail.com>
2022-04-19 16:42:04 +0200

    feat(pack.resiliation): add new reasons for resiliation survey (#6416)
    - add new reasons for resiliation
    - remove old reasons for resiliation
    - rework on controller class
    - add subsidiary question associated for some reasons
    - send subsidiary to resiliate API
    
    ref: UXCT-439
    
    Signed-off-by: Stephanie Moallic <stephanie.moallic@corp.ovh.com>

--- packages/manager/apps/telecom/src/app/telecom/pack/resiliation/pack-resiliation.routing.js
+++ packages/manager/apps/telecom/src/app/telecom/pack/resiliation/pack-resiliation.routing.js
@@ -1,12 +1,9 @@
-import template from './pack-resiliation.html';
-import controller from './pack-resiliation.controller';
-
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('telecom.packs.pack.resiliation', {
     url: '/resiliation',
-    template,
-    controller,
-    controllerAs: 'PackResiliation',
+    views: {
+      '@telecom.packs': 'packResiliation',
+    },
     resolve: {
       breadcrumb: /* @ngInject */ ($translate) =>
         $translate.instant('pack_resiliation_title'),
