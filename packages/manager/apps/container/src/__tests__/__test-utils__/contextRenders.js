0f3da02d28b164890c3d6a77b5fb525954af2dbc
Jisay <jean-christophe.alleman@corp.ovh.com>
2022-04-19 13:54:55 +0200

    test(pnr): fix unit test after rebase
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

--- packages/manager/apps/container/src/__tests__/__test-utils__/contextRenders.js
+++ packages/manager/apps/container/src/__tests__/__test-utils__/contextRenders.js
@@ -6,7 +6,7 @@ import { shell as shellApi } from '@ovh-ux/shell';
 import i18n from '../config/i18nTestConfig';
 import { ApplicationProvider } from '@/context';
 import { NotificationsProvider } from '@/core/notifications';
-import { ProductNavReshuffleProvider } from '@/core/product-nav-reshuffle';
+import { ContainerProvider } from '@/core/container';
 
 const renderWithI18n = (children, { ...renderOptions }) => {
   return render(
@@ -21,7 +21,7 @@ const renderWithShell = async (children, { environment, ...renderOptions }) => {
   return render(
     <I18nextProvider i18n={i18n}>
       <ApplicationProvider environment={environment} shell={shell}>
-        <ProductNavReshuffleProvider>{children}</ProductNavReshuffleProvider>
+        <ContainerProvider>{children}</ContainerProvider>
       </ApplicationProvider>
     </I18nextProvider>,
     renderOptions,
@@ -38,7 +38,7 @@ const renderWithNotifications = async (
     <I18nextProvider i18n={i18n}>
       <ApplicationProvider environment={environment} shell={shell}>
         <NotificationsProvider environment={environment}>
-          <ProductNavReshuffleProvider>{children}</ProductNavReshuffleProvider>
+          <ContainerProvider>{children}</ContainerProvider>
         </NotificationsProvider>
       </ApplicationProvider>
     </I18nextProvider>,
