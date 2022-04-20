95e9b1776db4660bf743a688179a92cfb2c5240d
frenauvh <florian.renaut@corp.ovh.com>
2022-04-20 12:57:39 +0200

    fix(dbaas-logs): update at-internet-configuration
    Co-authored-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/apps/dbaas-logs/src/components/at-internet/index.js
+++ packages/manager/apps/dbaas-logs/src/components/at-internet/index.js
@@ -1,15 +1,18 @@
 import angular from 'angular';
+import ngAtInternet from '@ovh-ux/ng-at-internet';
 import ovhManagerAtInternetConfiguration from '@ovh-ux/manager-at-internet-configuration';
 
 import TRACKING from './at-internet.constant';
 
 const moduleName = 'dbaasLogsAtInternet';
 
-angular.module(moduleName, [ovhManagerAtInternetConfiguration]).config(
-  /* @ngInject */ (atInternetConfigurationProvider) => {
-    atInternetConfigurationProvider.setConfig(TRACKING);
-    atInternetConfigurationProvider.setPrefix('dedicated');
-  },
-);
+angular
+  .module(moduleName, [ngAtInternet, ovhManagerAtInternetConfiguration])
+  .config(
+    /* @ngInject */ (atInternetConfigurationProvider) => {
+      atInternetConfigurationProvider.setConfig(TRACKING);
+      atInternetConfigurationProvider.setPrefix('dedicated');
+    },
+  );
 
 export default moduleName;
