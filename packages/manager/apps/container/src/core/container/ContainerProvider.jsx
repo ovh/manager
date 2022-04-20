29b5b62d49f8d45cc47cbd04deaa0739af9f4054
Jisay <jean-christophe.alleman@corp.ovh.com>
2022-04-20 11:49:24 +0200

    fix: conflicts
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

--- packages/manager/apps/container/src/core/container/ContainerProvider.jsx
+++ packages/manager/apps/container/src/core/container/ContainerProvider.jsx
@@ -4,15 +4,60 @@ import { useReket } from '@ovh-ux/ovh-reket';
 
 import ContainerContext from './context';
 
+export const BETA_V1 = 1;
+export const BETA_V2 = 2;
+
 export const ContainerProvider = ({ children }) => {
   const reketInstance = useReket();
   const preferenceKey = 'NAV_RESHUFFLE_BETA_ACCESS';
   const [isLoading, setIsLoading] = useState(true);
-  const [isBeta, setIsBeta] = useState(false);
+
+  // true if we should we ask the user if he want to test beta version
   const [askBeta, setAskBeta] = useState(false);
 
-  let isMounted = true;
+  // 1 for beta1, 2 for beta2, otherwise null if not a beta tester
+  const [betaVersion, setBetaVersion] = useState(null);
+
+  // user choice about using or not the beta
+  const [useBeta, setUseBeta] = useState(false);
+
   let containerContext = useContext(ContainerContext);
+
+  /**
+   * Check in feature availability if the user is a beta tester
+   * and returns the beta version if it's the case.
+   */
+  const fetchBetaVersion = async () => {
+    return reketInstance
+      .get(`/feature/pnr:betaV1,pnr:betaV2/availability`, {
+        requestType: 'aapi',
+      })
+      .then((value) => {
+        if (value['pnr:betaV1'] === true) {
+          return BETA_V1;
+        }
+        if (value['pnr:betaV2'] === true) {
+          return BETA_V2;
+        }
+        return null;
+      })
+      .finally(() => null);
+  };
+
+  const fetchBetaChoice = async () => {
+    return reketInstance
+      .get(`/me/preferences/manager/${preferenceKey}`)
+      .then(({ value }) => {
+        setUseBeta(value === 'true');
+      })
+      .catch((error) => {
+        if (error?.status === 404) {
+          setAskBeta(true);
+        } else {
+          throw error;
+        }
+      });
+  };
 
   const updateBetaChoice = async (accept = false) => {
     return reketInstance
@@ -41,29 +86,29 @@ export const ContainerProvider = ({ children }) => {
       });
   };
 
+  /**
+   * Initialisation is done here, we check if the user is a beta tester,
+   * then we check his preferences regarding using or not the beta version.
+   * If he has not preferences saved, we set askBeta to true in order
+   * to provide him the choice.
+   */
   useEffect(() => {
-    reketInstance
-      .get(`/me/preferences/manager/${preferenceKey}`)
-      .then(({ value }) => {
-        setIsBeta(value === 'true');
-      })
-      .catch((error) => {
-        if (error?.status === 404) {
-          setAskBeta(true);
-        } else {
-          throw error;
+    fetchBetaVersion()
+      .then((version) => {
+        setBetaVersion(version);
+        if (version) {
+          return fetchBetaChoice();
         }
+        return null;
       })
-      .finally(() => isMounted && setIsLoading(false));
-    return () => {
-      isMounted = false;
-    };
+      .finally(() => setIsLoading(false));
   }, []);
 
   containerContext = {
     createBetaChoice,
     askBeta,
-    isBeta,
+    betaVersion,
+    useBeta,
     isLoading,
     updateBetaChoice,
   };
