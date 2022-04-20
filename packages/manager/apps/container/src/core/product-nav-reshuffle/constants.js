8e7cce3264f6ea86ec7600271de45b84ae148180
Jisay <jean-christophe.alleman@corp.ovh.com>
2022-04-20 14:35:12 +0200

    feat(pnr): manage feedback URL language
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

--- packages/manager/apps/container/src/core/product-nav-reshuffle/constants.js
+++ packages/manager/apps/container/src/core/product-nav-reshuffle/constants.js
@@ -1,6 +1,6 @@
 export const FEEDBACK_URLS = {
-  beta1: 'https://survey.ovh.com/index.php/182319?lang=fr',
-  beta2: 'https://survey.ovh.com/index.php/465758?lang=fr',
+  beta1: 'https://survey.ovh.com/index.php/182319',
+  beta2: 'https://survey.ovh.com/index.php/465758',
 };
 
 export default {
