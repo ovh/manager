576015c80e5014f418adfc7cbd411b59e17079f6
Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
2022-04-20 16:04:13 +0200

    feat(nasha): add content for nasha onboarding
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

--- packages/manager/modules/nasha/src/onboarding/onboarding.routing.js
+++ packages/manager/modules/nasha/src/onboarding/onboarding.routing.js
@@ -4,6 +4,16 @@ export default /* @ngInject */ ($stateProvider) => {
     component: 'nashaOnboardingComponent',
     resolve: {
       hideBreadcrumb: () => true,
+      cta: ($state) => $state.href('nasha.nasha-add'),
+      resources: /* @ngInject */ ($http) =>
+        $http.get('/dedicated/nasha').then(({ data }) => data),
     },
+    redirectTo: (transition) =>
+      transition
+        .injector()
+        .getAsync('resources')
+        .then((resources) =>
+          resources.length > 0 ? { state: 'app.nasha.index' } : false,
+        ),
   });
 };
