7a762412ef1c0d2f474bfa9d7e590054d590bd47
Jisay <jean-christophe.alleman@corp.ovh.com>
2022-04-21 16:16:42 +0200

    feat(pnr): add empty page for telecom telephony section
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

---
+++ packages/manager/apps/telecom/src/app/telecom/telephony/onboarding/constants.js
@@ -0,0 +1,36 @@
+export const CTA = 'https://www.ovhtelecom.fr/telephonie/voip/';
+
+export const GUIDES = [
+  {
+    title: 'telephony_onboarding_guide1_title',
+    description: 'telephony_onboarding_guide1_description',
+    links: {
+      DEFAULT: 'https://docs.ovh.com/fr/voip/faq-voip/',
+      FR: 'https://docs.ovh.com/fr/voip/faq-voip/',
+    },
+  },
+  {
+    title: 'telephony_onboarding_guide2_title',
+    description: 'telephony_onboarding_guide2_description',
+    links: {
+      DEFAULT:
+        'https://docs.ovh.com/fr/voip/commander-associer-ou-changer-un-telephone/',
+      FR:
+        'https://docs.ovh.com/fr/voip/commander-associer-ou-changer-un-telephone/',
+    },
+  },
+  {
+    title: 'telephony_onboarding_guide3_title',
+    links: {
+      DEFAULT:
+        'https://docs.ovh.com/fr/voip/activer-desactiver-services-depuis-telephone/',
+      FR:
+        'https://docs.ovh.com/fr/voip/activer-desactiver-services-depuis-telephone/',
+    },
+  },
+];
+
+export default {
+  CTA,
+  GUIDES,
+};
