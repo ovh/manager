78707b0705eb2c5ccbc8d84a432b8228cb44de96
Jisay <jean-christophe.alleman@corp.ovh.com>
2022-04-21 16:33:54 +0200

    feat(pnr): add empty page for telecom sms section
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

---
+++ packages/manager/modules/sms/src/onboarding/constants.js
@@ -0,0 +1,44 @@
+export const CTA = 'https://www.ovhtelecom.fr/sms/';
+
+export const GUIDES = [
+  {
+    title: 'sms_onboarding_guide1_title',
+    description: 'sms_onboarding_guide1_description',
+    links: {
+      DEFAULT:
+        'https://docs.ovh.com/gb/en/sms/enable-automatic-recredit-sms-credit/',
+      ES:
+        'https://docs.ovh.com/es/sms/activar-la-recarga-automatica-de-credito-sms/',
+      IE:
+        'https://docs.ovh.com/ie/en/sms/enable-automatic-recredit-sms-credit/',
+      IT:
+        'https://docs.ovh.com/it/sms/attivare-la-ricarica-automatica-del-credito-sms/',
+      PL:
+        'https://docs.ovh.com/pl/sms/aktywowanie-automatycznego-ladowania-zasilen-sms/',
+      GB:
+        'https://docs.ovh.com/gb/en/sms/enable-automatic-recredit-sms-credit/',
+      FR:
+        'https://docs.ovh.com/fr/sms/activer-la-recharge-automatique-du-credit-sms/',
+    },
+  },
+  {
+    title: 'sms_onboarding_guide2_title',
+    description: 'sms_onboarding_guide2_description',
+    links: {
+      DEFAULT: 'https://docs.ovh.com/fr/sms/api_sms_cookbook/',
+      FR: 'https://docs.ovh.com/fr/sms/api_sms_cookbook/',
+    },
+  },
+  {
+    title: 'sms_onboarding_guide3_title',
+    links: {
+      DEFAULT: 'https://docs.ovh.com/fr/sms/gerer-les-sms-avec-reponse/',
+      FR: 'https://docs.ovh.com/fr/sms/gerer-les-sms-avec-reponse/',
+    },
+  },
+];
+
+export default {
+  CTA,
+  GUIDES,
+};
