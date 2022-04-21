003044da0e85ac82dbf0d37e4ba297a561fc1cde
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2022-04-21 16:50:48 +0200

    fix(netapp): improve commitment tracking
    ref: MANAGER-9040
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/modules/netapp/src/dashboard/constants.js
+++ packages/manager/modules/netapp/src/dashboard/constants.js
@@ -1,6 +1,26 @@
 export const MINIMUM_VOLUME_SIZE = 100;
 export const SERVICE_TYPE = 'NETAPP';
 
+const COMMIT_RECOMMIT_IMPRESSION_TRACKING_DATA = {
+  campaignId: '[commit-recommit]',
+  creation: '[batch1]',
+  format: '[link]',
+  generalPlacement: '[netapp]',
+  detailedPlacement: '[commitment]',
+};
+
+export const COMMIT_IMPRESSION_TRACKING_DATA = {
+  ...COMMIT_RECOMMIT_IMPRESSION_TRACKING_DATA,
+  variant: '[commit]',
+};
+
+export const RECOMMIT_IMPRESSION_TRACKING_DATA = {
+  ...COMMIT_RECOMMIT_IMPRESSION_TRACKING_DATA,
+  variant: '[recommit]',
+};
+
 export default {
   MINIMUM_VOLUME_SIZE,
+  COMMIT_IMPRESSION_TRACKING_DATA,
+  RECOMMIT_IMPRESSION_TRACKING_DATA,
 };
