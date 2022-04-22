7f087379be61bcb82bc1281bca6f73453a30c0a0
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2022-04-22 17:06:37 +0200

    feat(pci.projects.new): remove one-shot ticket
    ref: DTRSD-82157
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/new/components/support-ticket-banner/controller.js
+++
@@ -1,8 +0,0 @@
-export default class SupportTicketBannerController {
-  /* @ngInject */
-  constructor(coreConfig, coreURLBuilder) {
-    this.supportTicketLink = coreConfig.isRegion(['EU', 'CA'])
-      ? coreURLBuilder.buildURL('dedicated', '#/support/tickets/new')
-      : '';
-  }
-}
