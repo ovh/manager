52c8381fe258790cc2b3be35e2dbf13aeac4db76
frenauvh <florian.renaut@corp.ovh.com>
2022-04-25 14:48:31 +0200

    feat(container): product nav reshuffle sidebar
    ref: MANAGER-8698
    
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/apps/container/src/container/nav-reshuffle/sidebar/navigation-tree/root.ts
+++ packages/manager/apps/container/src/container/nav-reshuffle/sidebar/navigation-tree/root.ts
@@ -5,28 +5,55 @@ export default {
     {
       id: 'home',
       translation: 'sidebar_home',
-      path: '/manager',
+      routing: {
+        application: 'hub',
+      },
+      count: false,
     },
     services,
     {
       id: 'account',
       translation: 'sidebar_account',
+      routing: {
+        application: 'dedicated',
+        hash: '#/useraccount',
+      },
+      count: false,
     },
     {
       id: 'billing',
       translation: 'sidebar_billing',
+      routing: {
+        application: 'dedicated',
+        hash: '#/billing/history',
+      },
+      count: false,
     },
     {
       id: 'orders',
       translation: 'sidebar_orders',
+      routing: {
+        application: 'dedicated',
+        hash: '#/billing/orders',
+      },
+      count: false,
     },
     {
       id: 'sunrise',
       translation: 'sidebar_sunrise',
+      routing: {
+        application: 'sunrise',
+      },
+      count: false,
+      region: ['EU', 'CA'],
     },
     {
       id: 'marketplace',
       translation: 'sidebar_marketplace',
+      url: 'https://marketplace.ovhcloud.com/',
+      isExternal: true,
+      count: false,
+      region: ['EU'],
     },
   ],
 };
