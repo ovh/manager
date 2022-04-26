34c972c841efd6f705b32d474679ac6e76d5be7b
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2022-04-26 18:07:53 +0200

    feat(web-paas): add onboarding page
    ref: MANAGER-9136
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

---
+++ packages/manager/modules/web-paas/src/onboarding/constants.js
@@ -0,0 +1,17 @@
+export const GUIDES = [
+  {
+    title: 'web_paas_onboarding_guide1_title',
+    description: 'web_paas_onboarding_guide1_description',
+    links: {
+      DEFAULT: '',
+      DE: 'https://docs.ovh.com/de/web-paas/only-in-english/',
+      ES: 'https://docs.ovh.com/es/web-paas/only-in-english/',
+      IT: 'https://docs.ovh.com/it/web-paas/only-in-english/',
+      PL: 'https://docs.ovh.com/pl/web-paas/only-in-english/',
+      PT: 'https://docs.ovh.com/pt/web-paas/only-in-english/',
+      FR: 'https://docs.ovh.com/fr/web-paas/only-in-english/',
+    },
+  },
+];
+
+export default { GUIDES };
