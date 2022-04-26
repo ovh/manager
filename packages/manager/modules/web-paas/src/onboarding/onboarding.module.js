359b7a2d745a641c808a6fefea65b16a42e5bdbf
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2022-04-26 18:07:53 +0200

    feat(web-paas): add onboarding page
    ref: MANAGER-9136
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/modules/web-paas/src/onboarding/onboarding.module.js
+++ packages/manager/modules/web-paas/src/onboarding/onboarding.module.js
@@ -1,15 +1,14 @@
-import angular from 'angular';
-import '@uirouter/angularjs';
+import { OnboardingLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
 
 import component from './onboarding.component';
 import routing from './onboarding.routing';
 
-const moduleName = 'ovhManagerWebPaasOnboarding';
+const moduleName = 'webPaasOnboarding';
 
 angular
-  .module(moduleName, ['ui.router'])
+  .module(moduleName, [OnboardingLayoutHelper])
+  .component('webPaasOnboardingComponent', component)
   .config(routing)
-  .component('webPaasOnboarding', component)
   .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
