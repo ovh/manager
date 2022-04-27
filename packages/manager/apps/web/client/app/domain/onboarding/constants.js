c6546998c8909904a8194b7b289244c1bef676b0
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2022-04-27 10:51:13 +0200

    feat(domain): add onboarding page
    ref: MANAGER-9136
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

---
+++ packages/manager/apps/web/client/app/domain/onboarding/constants.js
@@ -0,0 +1,110 @@
+export const GUIDES = [
+  {
+    title: 'domains_onboarding_guide1_title',
+    description: 'domains_onboarding_guide1_description',
+    links: {
+      DEFAULT: 'https://docs.ovh.com/gb/en/storage/veeam-cloud-connect/',
+      ASIA: 'https://docs.ovh.com/asia/en/storage/veeam-cloud-connect/',
+      DE: 'https://docs.ovh.com/de/storage/veeam-cloud-connect/',
+      ES: 'https://docs.ovh.com/es/storage/veeam-cloud-connect/',
+      IE: 'https://docs.ovh.com/ie/en/storage/veeam-cloud-connect/',
+      IT: 'https://docs.ovh.com/it/storage/veeam-cloud-connect/',
+      PL: 'https://docs.ovh.com/pl/storage/veeam-cloud-connect/',
+      PT: 'https://docs.ovh.com/pt/storage/veeam-cloud-connect/',
+      GB: 'https://docs.ovh.com/gb/en/storage/veeam-cloud-connect/',
+      CA: 'https://docs.ovh.com/ca/en/storage/veeam-cloud-connect/',
+      QC: 'https://docs.ovh.com/ca/fr/storage/veeam-cloud-connect/',
+      AU: 'https://docs.ovh.com/au/en/storage/veeam-cloud-connect/',
+      SG: 'https://docs.ovh.com/sg/en/storage/veeam-cloud-connect/',
+      FR: 'https://docs.ovh.com/fr/storage/veeam-cloud-connect/',
+      WE: 'https://docs.ovh.com/us/en/storage/veeam-cloud-connect/',
+      WS: 'https://docs.ovh.com/us/en/storage/veeam-cloud-connect/',
+      US: 'https://docs.ovh.com/us/en/storage/veeam-cloud-connect/',
+    },
+  },
+  {
+    title: 'domains_onboarding_guide2_title',
+    description: 'domains_onboarding_guide2_description',
+    links: {
+      DEFAULT:
+        'https://docs.ovh.com/gb/en/domains/how-to-change-domain-name-holder/',
+      ASIA:
+        'https://docs.ovh.com/asia/en/domains/how-to-change-domain-name-holder/',
+      DE: 'https://docs.ovh.com/de/domains/wechsel_des_domaininhabers/',
+      ES: 'https://docs.ovh.com/es/domains/cambio-propietario-dominio/',
+      IE:
+        'https://docs.ovh.com/ie/en/domains/how-to-change-domain-name-holder/',
+      IT: 'https://docs.ovh.com/it/domains/cambiare_proprietario_dominio/',
+      PL:
+        'https://docs.ovh.com/pl/domains/zmiana_wlasciciela_domeny_globalnej_com_net_org_info_biz/',
+      PT: 'https://docs.ovh.com/pt/domains/alteracao-titular-dominio/',
+      GB:
+        'https://docs.ovh.com/gb/en/domains/how-to-change-domain-name-holder/',
+      CA:
+        'https://docs.ovh.com/ca/en/domains/how-to-change-domain-name-holder/',
+      QC: 'https://docs.ovh.com/ca/fr/domains/changement-proprietaire-domaine/',
+      AU:
+        'https://docs.ovh.com/au/en/domains/how-to-change-domain-name-holder/',
+      SG:
+        'https://docs.ovh.com/sg/en/domains/how-to-change-domain-name-holder/',
+      FR: 'https://docs.ovh.com/fr/domains/changement-proprietaire-domaine/',
+      WE:
+        'https://docs.ovh.com/us/en/domains/how-to-change-domain-name-holder/',
+      WS:
+        'https://docs.ovh.com/us/en/domains/how-to-change-domain-name-holder/',
+      US:
+        'https://docs.ovh.com/us/en/domains/how-to-change-domain-name-holder/',
+    },
+  },
+  {
+    title: 'domains_onboarding_guide3_title',
+    description: 'domains_onboarding_guide3_description',
+    links: {
+      DEFAULT: 'https://docs.ovh.com/gb/en/domains/transfer-generic-domain/',
+      ASIA: 'https://docs.ovh.com/asia/en/domains/transfer-generic-domain/',
+      DE: 'https://docs.ovh.com/de/domains/transfer-einer-generischen-domain/',
+      ES: 'https://docs.ovh.com/es/domains/transferir-un-dominio-generico/',
+      IE: 'https://docs.ovh.com/ie/en/domains/transfer-generic-domain/',
+      IT:
+        'https://docs.ovh.com/it/domains/trasferire-un-dominio-generico-in-ovh/',
+      PL: 'https://docs.ovh.com/pl/domains/przeniesienie-domeny-globalnej/',
+      PT: 'https://docs.ovh.com/pt/domains/transferir-o-dominio-generico/',
+      GB: 'https://docs.ovh.com/gb/en/domains/transfer-generic-domain/',
+      CA: 'https://docs.ovh.com/ca/en/domains/transfer-generic-domain/',
+      QC:
+        'https://docs.ovh.com/ca/fr/domains/transferer-mon-domaine-generique/',
+      AU: 'https://docs.ovh.com/au/en/domains/transfer-generic-domain/',
+      SG: 'https://docs.ovh.com/sg/en/domains/transfer-generic-domain/',
+      FR: 'https://docs.ovh.com/fr/domains/transferer-mon-domaine-generique/',
+      WE: 'https://docs.ovh.com/us/en/domains/transfer-generic-domain/',
+      WS: 'https://docs.ovh.com/us/en/domains/transfer-generic-domain/',
+      US: 'https://docs.ovh.com/us/en/domains/transfer-generic-domain/',
+    },
+  },
+];
+
+export const CTAS = {
+  DEFAULT: 'https://ovhcloud.com/en/order/webcloud/',
+  ASIA: 'https://ovhcloud.com/en/order/webcloud/',
+  DE: 'https://ovhcloud.com/de/order/webcloud/',
+  ES: 'https://ovhcloud.com/es/order/webcloud/',
+  IE: 'https://ovhcloud.com/en/order/webcloud/',
+  IT: 'https://ovhcloud.com/it/order/webcloud/',
+  NL: 'https://ovhcloud.com/nl/order/webcloud/',
+  PL: 'https://ovhcloud.com/pl/order/webcloud/',
+  PT: 'https://ovhcloud.com/pt/order/webcloud/',
+  GB: 'https://ovhcloud.com/en/order/webcloud/',
+  CA: 'https://ovhcloud.com/en/order/webcloud/',
+  QC: 'https://ovhcloud.com/fr/order/webcloud/',
+  MA: 'https://ovhcloud.com/fr/order/webcloud/',
+  SN: 'https://ovhcloud.com/fr/order/webcloud/',
+  TN: 'https://ovhcloud.com/fr/order/webcloud/',
+  AU: 'https://ovhcloud.com/en/order/webcloud/',
+  SG: 'https://ovhcloud.com/en/order/webcloud/',
+  FR: 'https://ovhcloud.com/fr/order/webcloud/',
+  CZ: 'https://ovhcloud.com/cz/order/webcloud/',
+  FI: 'https://ovhcloud.com/fi/order/webcloud/',
+  LT: 'https://ovhcloud.com/lt/order/webcloud/',
+  WE: 'https://ovhcloud.com/en/order/webcloud/',
+  WS: 'https://ovhcloud.com/en/order/webcloud/',
+};
