c0855e3f3f7a543ad501143d022d1aa2003194b9
Jisay <jean-christophe.alleman@corp.ovh.com>
2022-04-27 17:01:03 +0200

    feat(cdn): add onboarding page
    ref: MANAGER-9135
    
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/cdn/index.js
+++ packages/manager/apps/dedicated/client/app/cdn/index.js
@@ -2,9 +2,11 @@ import angular from 'angular';
 import '@uirouter/angularjs';
 import 'oclazyload';
 
+import onboarding from './onboarding';
+
 const moduleName = 'ovhManagerCdnLazyLoading';
 
-angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
+angular.module(moduleName, ['ui.router', 'oc.lazyLoad', onboarding]).config(
   /* @ngInject */ ($stateProvider) => {
     $stateProvider.state('app.networks.cdn', {
       url: '/cdn',
