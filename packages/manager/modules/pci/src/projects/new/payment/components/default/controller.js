364b96cdddb8ec13d19f99c301cd979a2b2fa5d6
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2022-04-27 17:10:24 +0200

    feat(pci.projects.new.payment): improve default payment method part
    ref: DTRSD-79696
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/new/payment/components/default/controller.js
+++ packages/manager/modules/pci/src/projects/new/payment/components/default/controller.js
@@ -1,3 +1,11 @@
+import 'moment';
+
+const PAYMENTS_TYPES = {
+  CREDIT_CARD: 'CREDIT_CARD',
+  BANK_ACCOUNT: 'BANK_ACCOUNT',
+  DEFERRED_PAYMENT_ACCOUNT: 'DEFERRED_PAYMENT_ACCOUNT',
+};
+
 export default class PciProjectNewPaymentDefaultCtrl {
   /* @ngInject */
   constructor(coreURLBuilder) {
@@ -5,5 +13,23 @@ export default class PciProjectNewPaymentDefaultCtrl {
       payment: { application: 'dedicated', path: '#/billing/payment/method' },
       myAccount: { application: 'dedicated', path: '#/useraccount/dashboard' },
     });
+  }
+
+  getFormatCreditCardNumber() {
+    const { label, paymentType } = this.defaultPaymentMethod;
+    const creditBankList = [
+      PAYMENTS_TYPES.BANK_ACCOUNT,
+      PAYMENTS_TYPES.CREDIT_CARD,
+    ];
+
+    if (creditBankList.includes(paymentType)) {
+      return `····${label.substring(label.length - 4, label.length)}`;
+    }
+
+    return label;
+  }
+
+  getFormatExpirationDate() {
+    return moment(this.defaultPaymentMethod.expirationDate).format('MM/YYYY');
   }
 }
