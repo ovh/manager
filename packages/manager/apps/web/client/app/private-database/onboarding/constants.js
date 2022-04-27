65b51c8dc3b3a4bbd58ee6a21550b5a13da8fcf0
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2022-04-27 09:56:29 +0200

    feat(private-database): add onboarding page
    ref: MANAGER-9136
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

---
+++ packages/manager/apps/web/client/app/private-database/onboarding/constants.js
@@ -0,0 +1,58 @@
+export const GUIDES = [
+  {
+    title: 'private_databases_onboarding_guide1_title',
+    description: 'private_databases_onboarding_guide1_description',
+    links: {
+      DEFAULT:
+        'https://docs.ovh.com/gb/en/clouddb/getting-started-with-clouddb/',
+      DE: 'https://docs.ovh.com/de/clouddb/erste-schritte-mit-clouddb/',
+      ES: 'https://docs.ovh.com/es/clouddb/empezar-con-clouddb/',
+      IE: 'https://docs.ovh.com/ie/en/clouddb/getting-started-with-clouddb/',
+      IT: 'https://docs.ovh.com/it/clouddb/iniziare-a-utilizzare-clouddb/',
+      PL: 'https://docs.ovh.com/pl/clouddb/pierwsze-kroki-z-clouddb/',
+      PT: 'https://docs.ovh.com/pt/clouddb/comecar-com-clouddb/',
+      GB: 'https://docs.ovh.com/gb/en/clouddb/getting-started-with-clouddb/',
+      FR: 'https://docs.ovh.com/fr/clouddb/debuter-avec-clouddb/',
+    },
+  },
+  {
+    title: 'private_databases_onboarding_guide2_title',
+    description: 'private_databases_onboarding_guide2_description',
+    links: {
+      DEFAULT: 'https://docs.ovh.com/gb/en/hosting/create-databases-and-users/',
+      DE: 'https://docs.ovh.com/de/hosting/datenbank-und-benutzer-erstellen/',
+      ES: 'https://docs.ovh.com/es/hosting/crear-bases-de-datos-y-usuarios/',
+      IE: 'https://docs.ovh.com/ie/en/hosting/create-databases-and-users/',
+      IT: 'https://docs.ovh.com/it/hosting/creare-database-e-utente/',
+      PL:
+        'https://docs.ovh.com/pl/hosting/tworzenie-baz-danych-i-uzytkownikow/',
+      PT:
+        'https://docs.ovh.com/pt/hosting/criar-bases-de-dados-e-utilizadores/',
+      GB: 'https://docs.ovh.com/gb/en/hosting/create-databases-and-users/',
+      FR:
+        'https://docs.ovh.com/fr/hosting/creer-bases-de-donnees-et-utilisateurs/',
+    },
+  },
+  {
+    title: 'private_databases_onboarding_guide3_title',
+    description: 'private_databases_onboarding_guide3_description',
+    links: {
+      DEFAULT:
+        'https://docs.ovh.com/gb/en/hosting/connecting-to-database-on-database-server/',
+      DE: 'https://docs.ovh.com/de/hosting/datenbank-verbindung-auf-bdd/',
+      ES:
+        'https://docs.ovh.com/es/hosting/coneccion-base-de-datos-servidor-bdd/',
+      IE:
+        'https://docs.ovh.com/ie/en/hosting/connecting-to-database-on-database-server/',
+      IT: 'https://docs.ovh.com/it/hosting/connessione-database-server-bdd/',
+      PL: 'https://docs.ovh.com/pl/hosting/polaczenie-bazy-danych-serwer-bdd/',
+      PT: 'https://docs.ovh.com/pt/hosting/conexao-base-de-dados-servidor-bdd/',
+      GB:
+        'https://docs.ovh.com/gb/en/hosting/connecting-to-database-on-database-server/',
+      FR:
+        'https://docs.ovh.com/fr/hosting/connexion-base-de-donnees-serveur-bdd/',
+    },
+  },
+];
+
+export default { GUIDES };
