12b0e19dabeb442c50f50544911856a4ea03b7c4
Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
2022-04-27 10:35:58 +0200

    fixup! feat(vps): add onboarding module
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

--- packages/manager/modules/vps/src/onboarding/onboarding.module.js
+++ packages/manager/modules/vps/src/onboarding/onboarding.module.js
@@ -8,6 +8,7 @@ const moduleName = 'vpsOnboarding';
 angular
   .module(moduleName, [OnboardingLayoutHelper])
   .component('vpsOnboardingComponent', component)
-  .config(routing);
+  .config(routing)
+  .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
