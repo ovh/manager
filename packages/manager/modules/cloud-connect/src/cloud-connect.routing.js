4cb6b3802e496873ffbd8876d3123c62a81456e1
Jisay <jean-christophe.alleman@corp.ovh.com>
2022-04-27 16:36:42 +0200

    feat(cloud-connect): add onboarding page
    ref: MANAGER-9135
    
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

--- packages/manager/modules/cloud-connect/src/cloud-connect.routing.js
+++ packages/manager/modules/cloud-connect/src/cloud-connect.routing.js
@@ -32,5 +32,14 @@ export default /* @ngInject */ ($stateProvider) => {
           }),
         hideBreadcrumb: () => true,
       },
+      redirectTo: (transition) =>
+        transition
+          .injector()
+          .getAsync('resources')
+          .then((resources) =>
+            resources.data.length === 0
+              ? { state: 'cloud-connect.onboarding' }
+              : false,
+          ),
     });
 };
