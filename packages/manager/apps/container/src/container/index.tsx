e19ebaeae0a37f8218489c8f4606e90b0a6f11e6
frenauvh <florian.renaut@corp.ovh.com>
2022-04-28 17:37:24 +0200

    fix(container): update isBetaNavigation in ux plugin
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/apps/container/src/container/index.tsx
+++ packages/manager/apps/container/src/container/index.tsx
@@ -3,12 +3,15 @@ import React from 'react';
 import LegacyContainer from '@/container/legacy';
 import NavReshuffleContainer from '@/container/nav-reshuffle';
 import useProductNavReshuffle from '@/core/product-nav-reshuffle';
+import { useShell } from '@/context';
 
 export default function Container(): JSX.Element {
   const { isLoading, betaVersion, useBeta } = useProductNavReshuffle();
+  const shell = useShell();
   if (isLoading) {
     return <></>;
   }
+  shell.getPlugin('ux').setIsBetaNavigation(betaVersion && useBeta);
   return (
     <>
       {betaVersion && useBeta ? <NavReshuffleContainer /> : <LegacyContainer />}
