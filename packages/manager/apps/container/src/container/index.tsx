6ebe1db7d4b3c6e3f2837d9b8a5063a880b0821b
frenauvh <florian.renaut@corp.ovh.com>
2022-04-28 17:37:24 +0200

    fix(container): update menu sidebar in ux plugin
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/apps/container/src/container/index.tsx
+++ packages/manager/apps/container/src/container/index.tsx
@@ -2,17 +2,23 @@ import React from 'react';
 
 import LegacyContainer from '@/container/legacy';
 import NavReshuffleContainer from '@/container/nav-reshuffle';
+import { useShell } from '@/context';
 import useContainer from '@/core/container';
 import { ProductNavReshuffleProvider } from '@/core/product-nav-reshuffle';
 
 export default function Container(): JSX.Element {
   const { isLoading, betaVersion, useBeta } = useContainer();
+  const shell = useShell();
+  const isNavReshuffle = betaVersion && useBeta;
   if (isLoading) {
     return <></>;
   }
+  if (isNavReshuffle) {
+    shell.getPlugin('ux').showMenuSidebar();
+  }
   return (
     <>
-      {betaVersion && useBeta ? (
+      {isNavReshuffle ? (
         <ProductNavReshuffleProvider>
           <NavReshuffleContainer />
         </ProductNavReshuffleProvider>
