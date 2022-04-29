99d0441d234df4ad0fa2213e07294076e114ee9f
Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
2022-04-29 14:45:21 +0200

    feat(veeamcc): add onboarding page
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

--- packages/manager/modules/veeam-cloud-connect/src/veeam.module.js
+++ packages/manager/modules/veeam-cloud-connect/src/veeam.module.js
@@ -4,6 +4,7 @@ import '@ovh-ux/ui-kit';
 import '@uirouter/angularjs';
 import ngTranslateAsyncLoader from '@ovh-ux/ng-translate-async-loader';
 import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
+import onboarding from './onboarding';
 
 import routing from './veeam.routing';
 
@@ -16,6 +17,7 @@ angular
     'pascalprecht.translate',
     ListLayoutHelper.moduleName,
     'ui.router',
+    onboarding,
   ])
   .config(routing)
   .run(/* @ngTranslationsInject:json ./translations */);
