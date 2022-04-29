cbe0ab184854889dc5d762f637efd6f8aed45ee6
Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
2022-04-29 15:42:55 +0200

    feat(dbaaslogs): modify onboarding page
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

--- packages/manager/modules/dbaas-logs/src/logs/onboarding/onboarding.routing.js
+++ packages/manager/modules/dbaas-logs/src/logs/onboarding/onboarding.routing.js
@@ -2,8 +2,27 @@ export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('dbaas-logs.onboarding', {
     url: '/onboarding',
     views: {
-      logsHeader: 'dbaasLogsListHeader',
-      logsContainer: 'dbaasLogsOnboarding',
+      logsContainer: 'dbaasLogsOnboardingComponent',
     },
+    resolve: {
+      hideBreadcrumb: () => true,
+    },
+    redirectTo: (transition) =>
+      transition
+        .injector()
+        .getAsync('OvhApiDbaas')
+        .then((OvhApiDbaas) =>
+          OvhApiDbaas.Logs()
+            .v6()
+            .query()
+            .$promise.then((result) => {
+              return result.length > 0
+                ? 'dbaas-logs.list'
+                : 'dbaas-logs.onboarding';
+            })
+            .catch(() => {
+              return 'dbaas-logs.onboarding';
+            }),
+        ),
   });
 };
