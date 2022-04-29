687ae17d4ff9dd74f5f9874f4e481f675fc609f7
Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
2022-04-29 15:42:55 +0200

    feat(dbaaslogs): modify onboarding page
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

--- packages/manager/modules/dbaas-logs/src/logs/onboarding/index.js
+++ packages/manager/modules/dbaas-logs/src/logs/onboarding/index.js
@@ -2,7 +2,7 @@ import angular from 'angular';
 import '@uirouter/angularjs';
 import 'oclazyload';
 
-const moduleName = 'ovhManagerDbaasLogsOnboardingLazyLoad';
+const moduleName = 'dbaasLogsOnboardingLazyLoading';
 
 angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
   /* @ngInject */ ($stateProvider) => {
