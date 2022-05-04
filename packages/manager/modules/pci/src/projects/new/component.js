d3ec7ad89bf65c0ca8a4d075ec2f54afe2972920
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2022-05-04 17:25:14 +0200

    feat(pci.projects.new): manage voucher redirect
    ref: DTRSD-80175
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/new/component.js
+++ packages/manager/modules/pci/src/projects/new/component.js
@@ -8,6 +8,8 @@ export default {
     projects: '<',
     isHdsAvailable: '<',
     isValidHdsSupportLevel: '<',
+    summary: '<',
+    getSummary: '<',
     trackProjectCreationError: '<',
   },
 };
