1740bb5a506fb3a05e309291f8c3126d997261b7
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2022-05-06 16:31:55 +0200

    feat: add TTL and change config display
    DATATR-16
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/data-processing/submit-job/spark-config/spark-config.constants.js
+++ packages/manager/modules/pci/src/projects/project/data-processing/submit-job/spark-config/spark-config.constants.js
@@ -1,7 +1,15 @@
 export const ARGUMENTS_VALIDATION_PATTERN = /^[\w].*/;
 export const PYTHON_ENV_FILENAME = 'environment.yml';
+export const PYTHON_FILES_CONTENT_TYPES = ['text/x-python'];
+export const JAVA_FILES_CONTENT_TYPES = [
+  'application/java-archive',
+  'application/x-java-archive',
+  'application/x-jar',
+];
 
 export default {
   ARGUMENTS_VALIDATION_PATTERN,
   PYTHON_ENV_FILENAME,
+  PYTHON_FILES_CONTENT_TYPES,
+  JAVA_FILES_CONTENT_TYPES,
 };
