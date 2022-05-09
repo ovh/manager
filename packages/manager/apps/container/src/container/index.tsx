6b94d9c00100af7d37189be08a5dc677d90804a5
Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
2022-05-09 11:48:07 +0200

    fix(container): move cookie and sso modal from legacy container
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

--- packages/manager/apps/container/src/container/index.tsx
+++ packages/manager/apps/container/src/container/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import LegacyContainer from '@/container/legacy';
 import NavReshuffleContainer from '@/container/nav-reshuffle';
@@ -6,6 +6,8 @@ import { useShell } from '@/context';
 import useContainer from '@/core/container';
 import { ProductNavReshuffleProvider } from '@/core/product-nav-reshuffle';
 import { ProgressProvider } from '@/context/progress';
+import CookiePolicy from '@/cookie-policy/CookiePolicy';
+import SSOAuthModal from '@/sso-auth-modal/SSOAuthModal';
 
 export default function Container(): JSX.Element {
   const { isLoading, betaVersion, useBeta } = useContainer();
@@ -18,14 +20,22 @@ export default function Container(): JSX.Element {
     shell.getPlugin('ux').showMenuSidebar();
   }
   return (
-    <ProgressProvider>
-      {isNavReshuffle ? (
-        <ProductNavReshuffleProvider>
-          <NavReshuffleContainer />
-        </ProductNavReshuffleProvider>
-      ) : (
-        <LegacyContainer />
-      )}
-    </ProgressProvider>
+    <>
+      <ProgressProvider>
+        {isNavReshuffle ? (
+          <ProductNavReshuffleProvider>
+            <NavReshuffleContainer />
+          </ProductNavReshuffleProvider>
+        ) : (
+          <LegacyContainer />
+        )}
+      </ProgressProvider>
+      <Suspense fallback="">
+        <SSOAuthModal />
+      </Suspense>
+      <Suspense fallback="...">
+        <CookiePolicy shell={shell} />
+      </Suspense>
+    </>
   );
 }
