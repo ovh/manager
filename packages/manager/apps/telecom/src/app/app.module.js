e5f1281ab12b9b08556d6a3aa26cfea92276e82e
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2022-05-09 11:46:07 +0200

    fix(telecom): use progress from shell
    ref: MANAGER-9220
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/telecom/src/app/app.module.js
+++ packages/manager/apps/telecom/src/app/app.module.js
@@ -183,7 +183,7 @@ export default async (containerEl, shellClient) => {
         ngOvhPaymentMethod,
         ngOvhUiRouterBreadcrumb,
         ngOvhUiRouterLayout,
-        ngOvhUiRouterLineProgress,
+        isTopLevelApplication() ? ngOvhUiRouterLineProgress : null,
         ngOvhUiRouterTitle,
         ngOvhContact,
         ngOvhLineDiagnostics,
@@ -412,9 +412,37 @@ export default async (containerEl, shellClient) => {
     )
     .run(/* @ngTranslationsInject:json ./common/translations */)
     .run(
+      /* @ngInject */ ($transitions) => {
+        // replace ngOvhUiRouterLineProgress if in container
+        if (!isTopLevelApplication()) {
+          $transitions.onBefore({}, (transition) => {
+            if (
+              !transition.ignored() &&
+              transition.from().name !== '' &&
+              transition.entering().length > 0
+            ) {
+              shellClient.ux.startProgress();
+            }
+          });
+
+          $transitions.onSuccess({}, () => {
+            shellClient.ux.stopProgress();
+          });
+
+          $transitions.onError({}, (transition) => {
+            if (!transition.error().redirected) {
+              shellClient.ux.stopProgress();
+            }
+          });
+        }
+      },
+    )
+    .run(
       /* @ngInject */ ($rootScope, $transitions) => {
         const unregisterHook = $transitions.onSuccess({}, () => {
-          detachPreloader();
+          if (isTopLevelApplication()) {
+            detachPreloader();
+          }
           $rootScope.$broadcast('app:started');
           unregisterHook();
         });
