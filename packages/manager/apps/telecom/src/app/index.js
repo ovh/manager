3a54c67a875e927c5fc7a6ef811509bbba2a94f2
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2022-05-09 11:46:07 +0200

    fix(telecom): use progress from shell
    ref: MANAGER-9220
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/telecom/src/app/index.js
+++ packages/manager/apps/telecom/src/app/index.js
@@ -1,27 +1,30 @@
 import 'script-loader!jquery'; // eslint-disable-line
 import 'whatwg-fetch';
-import {
-  attach as attachPreloader,
-  displayMessage,
-} from '@ovh-ux/manager-preloader';
 import { useShellClient } from '@ovh-ux/shell';
-import { findAvailableLocale, detectUserLocale } from '@ovh-ux/manager-config';
+import {
+  findAvailableLocale,
+  detectUserLocale,
+  isTopLevelApplication,
+} from '@ovh-ux/manager-config';
 
 import { getShellClient, setShellClient } from './shell';
 
-attachPreloader(findAvailableLocale(detectUserLocale()));
+if (isTopLevelApplication()) {
+  import('@ovh-ux/manager-preloader')
+    .then(({ attach }) => attach(findAvailableLocale(detectUserLocale())))
+    .catch(() => {});
+}
 
 useShellClient('telecom')
   .then((client) => {
+    if (!isTopLevelApplication()) {
+      client.ux.startProgress();
+    }
     setShellClient(client);
     return client.environment.getEnvironment();
   })
   .then((environment) => {
     environment.setVersion(__VERSION__);
-
-    if (environment.getMessage()) {
-      displayMessage(environment.getMessage(), environment.getUserLanguage());
-    }
 
     import(`./config-${environment.getRegion()}`)
       .catch(() => {})
