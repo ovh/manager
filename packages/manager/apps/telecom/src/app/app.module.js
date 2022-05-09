ba31e136ea1dc3606971e4ac0a4e2ec6c8b7889d
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2022-05-09 21:55:07 +0200

    fix(telecom): use shell preloader
    ref: MANAGER-9248
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/telecom/src/app/app.module.js
+++ packages/manager/apps/telecom/src/app/app.module.js
@@ -431,6 +431,8 @@ export default async (containerEl, shellClient) => {
         const unregisterHook = $transitions.onSuccess({}, () => {
           if (isTopLevelApplication()) {
             detachPreloader();
+          } else {
+            shellClient.ux.hidePreloader();
           }
           $rootScope.$broadcast('app:started');
           unregisterHook();
