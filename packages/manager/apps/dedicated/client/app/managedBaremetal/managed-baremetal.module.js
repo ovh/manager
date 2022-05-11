1669679c2ca2ff0409e1072a3948c4edec194197
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2022-05-11 22:17:59 +0200

    fix(onboarding): update onboarding page
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/managedBaremetal/managed-baremetal.module.js
+++ packages/manager/apps/dedicated/client/app/managedBaremetal/managed-baremetal.module.js
@@ -4,12 +4,11 @@ import '@uirouter/angularjs';
 import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
 
 import routing from './managed-baremetal.routing';
-import onboarding from './onboarding';
 
 const moduleName = 'ovhManagerManagedBaremetal';
 
 angular
-  .module(moduleName, ['ui.router', ListLayoutHelper.moduleName, onboarding])
+  .module(moduleName, ['ui.router', ListLayoutHelper.moduleName])
   .config(routing)
   .run(/* @ngTranslationsInject:json ./translations */);
 
