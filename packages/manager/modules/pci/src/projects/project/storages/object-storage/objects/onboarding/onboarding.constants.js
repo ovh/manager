6bfde2626110a75ccb4fd1b2dacde3e7f36bb589
CDS Translator Agent <89460513+lucas-chaigne@users.noreply.github.com>
2022-05-20 11:00:56 +0000

    feat(public-cloud): update content of onboarding page of object storage
     Update content of onboarding page
     ref: MANAGER-9107
    
    Signed-off-by: sachinrameshn <sachin.ramesh@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/object-storage/objects/onboarding/onboarding.constants.js
+++ packages/manager/modules/pci/src/projects/project/storages/object-storage/objects/onboarding/onboarding.constants.js
@@ -1,22 +1,62 @@
 export const GUIDES = [
   {
     id: 'object-storage-domain',
-    link:
-      'https://docs.ovh.com/gb/en/public-cloud/place-an-object-storage-container-behind-domain-name/',
+    link: {
+      DEFAULT:
+        'https://docs.ovh.com/gb/en/public-cloud/place-an-object-storage-container-behind-domain-name/',
+    },
   },
   {
     id: 'auto-delete-object',
-    link:
-      'https://docs.ovh.com/gb/en/public-cloud/configure-auto-delete-objects/',
+    link: {
+      DEFAULT:
+        'https://docs.ovh.com/gb/en/public-cloud/configure-auto-delete-objects/',
+    },
   },
   {
     id: 'share-object-temporary-url',
-    link:
-      'https://docs.ovh.com/gb/en/public-cloud/share_an_object_via_a_temporary_url/',
+    link: {
+      DEFAULT:
+        'https://docs.ovh.com/gb/en/public-cloud/share_an_object_via_a_temporary_url/',
+    },
   },
   {
     id: 'sync-object-containers',
-    link: 'https://docs.ovh.com/gb/en/public-cloud/sync-object-containers/',
+    link: {
+      DEFAULT:
+        'https://docs.ovh.com/gb/en/public-cloud/sync-object-containers/',
+    },
+  },
+];
+
+export const STANDARD_S3 = [
+  {
+    id: 'getting-started-with-s3',
+    link: {
+      DEFAULT: 'https://docs.ovh.com/gb/en/storage/s3/getting-started-with-s3/',
+      DE: 'https://docs.ovh.com/de/storage/s3/getting-started-with-s3/',
+      ES: 'https://docs.ovh.com/es/storage/s3/getting-started-with-s3/',
+      FR: 'https://docs.ovh.com/fr/storage/s3/debuter-avec-s3/',
+      GB: 'https://docs.ovh.com/gb/en/storage/s3/getting-started-with-s3/',
+      IE: 'https://docs.ovh.com/ie/en/storage/s3/getting-started-with-s3/',
+      IT: 'https://docs.ovh.com/it/storage/s3/getting-started-with-s3/',
+      PL: 'https://docs.ovh.com/pl/storage/s3/getting-started-with-s3/',
+      PT: 'https://docs.ovh.com/pt/storage/s3/getting-started-with-s3/',
+    },
+  },
+  {
+    id: 'storage',
+    link: {
+      DEFAULT: 'https://docs.ovh.com/gb/en/storage/',
+      DE: 'https://docs.ovh.com/de/storage/',
+      ES: 'https://docs.ovh.com/es/storage/',
+      FR: 'https://docs.ovh.com/fr/storage/',
+      GB: 'https://docs.ovh.com/gb/en/storage/',
+      IE: 'https://docs.ovh.com/ie/en/storage/',
+      IT: 'https://docs.ovh.com/it/storage/',
+      PL: 'https://docs.ovh.com/pl/storage/',
+      PT: 'https://docs.ovh.com/pt/storage/',
+    },
   },
 ];
 
