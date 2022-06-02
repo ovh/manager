cc9017bd7e42ad013642f381a524de10060336d7
Hyenne Guillaume <guillaume.hyenne@ovhcloud.com>
2022-06-02 13:41:56 +0200

    feat(dedicated): add description on several section (#6688)
    * fix(dedicated): add description on several section except US
    
    US content to come later
    
    ref: DTRSD-66530 MANAGER-8552 MANAGER-8874 MANAGER-8875 MANAGER-8876 MANAGER-8877
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
    
    * fix(i18n): add missing translations [CDS 1651]
    
    Signed-off-by: CDS Translator Agent <opensource@ovh.net>
    
    * fix(i18n): add missing translations [CDS 1658]
    
    Signed-off-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/billing/src/main/billing-main.controller.js
+++ packages/manager/modules/billing/src/main/billing-main.controller.js
@@ -1,7 +1,14 @@
 export default class BillingMainCtrl {
   /* @ngInject */
-  constructor(coreConfig, isPayAsYouGoAvailable) {
+  constructor($translate, coreConfig, isPayAsYouGoAvailable) {
+    this.$translate = $translate;
     this.coreConfig = coreConfig;
     this.isPayAsYouGoAvailable = isPayAsYouGoAvailable;
+  }
+
+  descriptionOfHeading() {
+    return this.coreConfig.getRegion() !== 'US'
+      ? this.$translate.instant('billing_main_description')
+      : '';
   }
 }
