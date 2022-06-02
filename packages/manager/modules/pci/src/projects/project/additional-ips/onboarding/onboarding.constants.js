25861fbaeede3d6620cde30ca342953286c34124
Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>
2022-06-02 11:16:55 +0530

    feat: added additional ips project
    added new pci project called additional ips
    
    ref: MANAGER-9401
    
    Signed-off-by: Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>
    Signed-off-by: Anoop <anoop.n@ovhcloud.com>

---
+++ packages/manager/modules/pci/src/projects/project/additional-ips/onboarding/onboarding.constants.js
@@ -0,0 +1,27 @@
+export const GUIDES = [
+  {
+    id: 'service_agnostic_public_ip',
+    links: {
+      DEFAULT:
+        'https://docs.ovh.com/fr/public-cloud/create-service-agnostic-public-ip-failover-floating/',
+    },
+  },
+  {
+    id: 'persistent-failover-ip-configuration',
+    links: {
+      FR:
+        'https://docs.ovh.com/fr/public-cloud/configurer_une_ip_failover/#objectif',
+      DEFAULT:
+        'https://docs.ovh.com/gb/en/public-cloud/make-failover-ip-configuration-persistent/',
+    },
+  },
+  {
+    id: 'configure_floating_ip',
+    links: {
+      DEFAULT:
+        'https://docs.ovh.com/fr/public-cloud/configure-floating-ip-with-public-cloud-instance/',
+    },
+  },
+];
+
+export default { GUIDES };
