c24f530b994ca9e41f71b4d2841369dc67a6875c
Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>
2022-06-02 11:16:55 +0530

    feat: added additional ips project
    added new pci project called additional ips
    
    ref: MANAGER-9401
    
    Signed-off-by: Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>

---
+++ packages/manager/modules/pci/src/projects/project/additional-ips/order/order.component.js
@@ -0,0 +1,13 @@
+import controller from './order.controller';
+import template from './add.html';
+
+export default {
+  controller,
+  template,
+  bindings: {
+    instances: '<',
+    createInstanceUrl: '<',
+    projectId: '<',
+    goBack: '<',
+  },
+};
