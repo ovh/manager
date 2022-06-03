73894190a7acfa14c08139c15c1122854e1f191f
Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>
2022-06-03 10:49:16 +0530

    feat: order additional ip
    ref: MANAGER-9401
    
    Signed-off-by: Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/additional-ips/order/order.routing.js
+++ packages/manager/modules/pci/src/projects/project/additional-ips/order/order.routing.js
@@ -3,6 +3,11 @@ export default /* @ngInject */ ($stateProvider) => {
     url: '/order',
     component: 'pciProjectAdditionalIpsOrder',
     resolve: {
+      publicCloudCatalog: /* @ngInject */ (additionalIpService, coreConfig) =>
+        additionalIpService.getPublicCloudCatalog({
+          ovhSubsidiary: coreConfig.getUser().ovhSubsidiary,
+          productName: 'cloud',
+        }),
       breadcrumb: /* @ngInject */ () => null,
       goBack: /* @ngInject */ (goToAdditionalIps) => goToAdditionalIps,
     },
