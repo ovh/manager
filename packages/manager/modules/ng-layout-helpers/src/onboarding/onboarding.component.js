2cc4e5a5e8616e86e6b19c5cdf2cb23328bb2694
frenauvh <florian.renaut@corp.ovh.com>
2022-06-03 14:10:48 +0200

    fix(vps): onboarding footer
    ref: MANAGER-9488
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/modules/ng-layout-helpers/src/onboarding/onboarding.component.js
+++ packages/manager/modules/ng-layout-helpers/src/onboarding/onboarding.component.js
@@ -7,6 +7,7 @@ export default {
   transclude: {
     title: '?onboardingTitle',
     description: '?onboardingDescription',
+    footer: '?onboardingFooter',
   },
   bindings: {
     imageAlt: '<',
