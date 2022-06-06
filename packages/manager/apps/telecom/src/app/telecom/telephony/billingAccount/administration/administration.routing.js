bffccf3840514a08d139b9b10fe993e88de6249a
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2022-06-06 11:09:10 +0200

    feat(telecom.telephony): add reset password white label manager
    ref: MANAGER-8245
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
    Signed-off-by: Anoop <anooparveti@gmail.com>

--- packages/manager/apps/telecom/src/app/telecom/telephony/billingAccount/administration/administration.routing.js
+++ packages/manager/apps/telecom/src/app/telecom/telephony/billingAccount/administration/administration.routing.js
@@ -1,19 +1,18 @@
-import template from './administration.html';
-import controller from './administration.controller';
-
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('telecom.telephony.billingAccount.administration', {
     url: '/administration',
     views: {
       'groupInnerView@telecom.telephony.billingAccount': {
-        template,
-        controller,
-        controllerAs: 'BillingAccountAdministrationCtrl',
+        component: 'telecomBillingAccountAdministrationComponent',
       },
     },
     resolve: {
       breadcrumb: /* @ngInject */ ($translate) =>
         $translate.instant('telephony_group_admin_breadcrumb'),
+
+      canResetWhiteLabelManagerPassword: /* @ngInject */ (
+        telecomBillingAccountAdministrationService,
+      ) => telecomBillingAccountAdministrationService.resellerPanelStatus(),
     },
   });
 };
