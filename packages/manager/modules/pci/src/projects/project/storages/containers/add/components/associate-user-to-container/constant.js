41bede3de9477e23a8430e5228a0ef9b8c18e7a5
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2022-06-15 10:56:57 +0200

    feat(storages.container): component to associate user-credential
    ref: MANAGER-9112
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

---
+++ packages/manager/modules/pci/src/projects/project/storages/containers/add/components/associate-user-to-container/constant.js
@@ -0,0 +1,12 @@
+export const CONTAINER_USER_ASSOCIATION_MODES = {
+  LINKED: 'LINKED',
+  CREATE: 'CREATE',
+};
+
+export const ASSOCIATE_CONTAINER_USER_CONTAINER_MESSAGES =
+  'pci.projects.project.storages.containers.associate-user-to-container';
+
+export default {
+  CONTAINER_USER_ASSOCIATION_MODES,
+  ASSOCIATE_CONTAINER_USER_CONTAINER_MESSAGES,
+};
