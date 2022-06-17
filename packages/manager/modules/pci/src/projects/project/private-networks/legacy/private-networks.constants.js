43c20574fc6b8af70c2a1d8ebe5fc57492720ad3
Anoop N <anooparveti@gmail.com>
2022-06-17 14:29:41 +0530

    feat(pci.project): restrict l3-services features from US region (#7338)
    Restrict L3-services enhancements in Private-networks module from US region
     ref: MANAGER-9472
    
    Signed-off-by: Anoop <anooparveti@gmail.com>

---
+++ packages/manager/modules/pci/src/projects/project/private-networks/legacy/private-networks.constants.js
@@ -0,0 +1,11 @@
+export const DELETING_STATUS = ['DELETING'];
+
+export const VRACK_CREATION_ACTION = 'vrack#create';
+
+export const VRACK_OPERATION_COMPLETED_STATUS = ['completed'];
+
+export default {
+  DELETING_STATUS,
+  VRACK_CREATION_ACTION,
+  VRACK_OPERATION_COMPLETED_STATUS,
+};
