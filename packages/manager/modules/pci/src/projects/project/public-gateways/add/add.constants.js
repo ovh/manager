52785c41de934f1d64f008337b6f663562c74cc1
vks-ovh <102142952+vks-ovh@users.noreply.github.com>
2022-06-17 11:38:11 +0530

    feat(pci.public.gateways): create config for public gateways (#7193)
    Addition of config page for public gateways
     ref: MANAGER-9150
    
    Signed-off-by: vikash singh <vikash.singh@corp.ovh.com>
    Signed-off-by: Anoop <anoop.n@ovhcloud.com>

---
+++ packages/manager/modules/pci/src/projects/project/public-gateways/add/add.constants.js
@@ -0,0 +1,25 @@
+export const PUBLIC_GATEWAYS_READ_MORE_GUIDE = {
+  ALL_GUIDE: {
+    DEFAULT:
+      'https://docs.ovh.com/gb/en/public-cloud/creating-private-network-with-gateway/',
+  },
+};
+
+export const REGIONS_AVAILABILITY_URL =
+  'https://www.ovhcloud.com/en-ie/public-cloud/regions-availability/';
+
+export const AVAILABLE_SUBNET = [
+  '192.168.0.1/24',
+  '10.0.0.1/24',
+  '172.16.0.1/24',
+];
+
+export const PRODUCT_NAME = 'GATEWAY';
+export const DEFAULT_IPVERSION = 4;
+
+export default {
+  PUBLIC_GATEWAYS_READ_MORE_GUIDE,
+  REGIONS_AVAILABILITY_URL,
+  PRODUCT_NAME,
+  DEFAULT_IPVERSION,
+};
