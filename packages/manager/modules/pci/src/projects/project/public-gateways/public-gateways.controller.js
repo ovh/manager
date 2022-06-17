fbc8aa6f299b7b1e2315d3144c0f7c27d3b83ead
vks-ovh <102142952+vks-ovh@users.noreply.github.com>
2022-06-17 11:38:11 +0530

    feat(pci.public.gateways): create config for public gateways (#7193)
    Addition of config page for public gateways
     ref: MANAGER-9150
    
    Signed-off-by: vikash singh <vikash.singh@corp.ovh.com>

--- packages/manager/modules/pci/src/projects/project/public-gateways/public-gateways.controller.js
+++ packages/manager/modules/pci/src/projects/project/public-gateways/public-gateways.controller.js
@@ -24,6 +24,7 @@ export default class PublicGatewaysController {
 
   onAddPublicGatewayClick() {
     this.trackPublicGateways('add');
+    return this.goToAddPublicGateway();
   }
 
   onGoToPrivateNetworkClick() {
