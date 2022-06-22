c603135673ced2dac5de0945557446a65cfff3e0
Antoine Leblanc <antoine.leblanc@ovhcloud.com>
2022-06-22 13:59:18 +0200

    fix(orders): prevent displaying empty page (#7414)
    ref: DTRSD-90534
    
    Relates to #7402
    
    Signed-off-by: Antoine Leblanc <antoine.leblanc@ovhcloud.com>

--- packages/manager/modules/billing/src/orders/billing-orders-main.controller.js
+++ packages/manager/modules/billing/src/orders/billing-orders-main.controller.js
@@ -20,7 +20,9 @@ export default class BillingOrdersMainCtrl {
   }
 
   $onInit() {
-    const CUSTOMER_URLS = this.constants.urls[this.currentUser.ovhSubsidiary];
+    const CUSTOMER_URLS =
+      this.constants.urls[this.currentUser.ovhSubsidiary] ||
+      this.constants.urls.FR;
 
     this.billingGuideUrl = CUSTOMER_URLS.guides.billing;
     this.purchaseOrdersGuideUrl = CUSTOMER_URLS.guides.purchaseOrders;
