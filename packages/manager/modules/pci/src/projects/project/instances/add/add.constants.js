ff063265e242d098fff6ca430c13bae3dcdf9d8d
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2022-06-28 11:28:06 +0200

    fix(pci): allow only vlan 0 for private network of metal instances
    ref: MANAGER-9604
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/instances/add/add.constants.js
+++ packages/manager/modules/pci/src/projects/project/instances/add/add.constants.js
@@ -1,7 +1,9 @@
 export const BANDWIDTH_OUT = 'bandwidth_instance_out.consumption';
 export const EXCLUDE_FLAVOR_CATEGORIES = [];
+export const FILTER_PRIVATE_NETWORK_BAREMETAL = 'ovh.baremetal';
 
 export default {
   BANDWIDTH_OUT,
   EXCLUDE_FLAVOR_CATEGORIES,
+  FILTER_PRIVATE_NETWORK_BAREMETAL,
 };
