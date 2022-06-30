69db692a2e64a60fb5026b1359cf1a3dcc41fa81
Hyenne Guillaume <guillaume.hyenne@ovhcloud.com>
2022-06-30 15:48:57 +0200

    fix(telecom): text header of the Telecom manager Systran (#7411)
    ref: MANAGER-9170
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

---
+++ packages/manager/apps/telecom/src/app/app.constants.js
@@ -0,0 +1,9 @@
+export const SYSTRAN_LOCALE_UNAVAILABLE = 'fr';
+
+export const URL_SURVEY_SYSTRAN =
+  'https://survey.ovh.com/index.php/175287?lang=';
+
+export default {
+  SYSTRAN_LOCALE_UNAVAILABLE,
+  URL_SURVEY_SYSTRAN,
+};
