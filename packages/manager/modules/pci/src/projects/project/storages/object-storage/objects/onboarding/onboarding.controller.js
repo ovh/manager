21f7502b17929d72a08bcc586ca15ed7eb26b9c0
sachinrameshn <104337442+sachinrameshn@users.noreply.github.com>
2022-06-30 17:53:32 +0530

    feat(public-cloud): update content of onboarding page of object storage (#7464)
    Update content of onboarding page
     ref: MANAGER-9107
    
    Signed-off-by: sachinrameshn <sachin.ramesh@ovhcloud.com>
    
    Co-authored-by: CDS Translator Agent <89460513+lucas-chaigne@users.noreply.github.com>
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/object-storage/objects/onboarding/onboarding.controller.js
+++ packages/manager/modules/pci/src/projects/project/storages/object-storage/objects/onboarding/onboarding.controller.js
@@ -1,33 +1,40 @@
-import reduce from 'lodash/reduce';
 import illustration from './assets/objects.png';
 
-import { GUIDES } from './onboarding.constants';
+import { GUIDES, STANDARD_S3 } from './onboarding.constants';
 
 export default class PciStorageObjectsOnboardingController {
   /* @ngInject */
-  constructor($translate, atInternet) {
+  constructor($translate, atInternet, coreConfig, ovhFeatureFlipping) {
     this.$translate = $translate;
     this.atInternet = atInternet;
+    this.user = coreConfig.getUser();
+    this.ovhFeatureFlipping = ovhFeatureFlipping;
   }
 
   $onInit() {
-    this.illustration = illustration;
-    this.guides = reduce(
-      GUIDES,
-      (list, guide) => [
-        ...list,
-        {
-          ...guide,
-          title: this.$translate.instant(
-            `pci_projects_project_storages_objects_onboarding_guides_${guide.id}_title`,
-          ),
-          description: this.$translate.instant(
-            `pci_projects_project_storages_objects_onboarding_guides_${guide.id}_description`,
-          ),
-        },
-      ],
-      [],
-    );
+    const standardS3offer = 'object-storage:standard-s3';
+    this.ovhFeatureFlipping
+      .checkFeatureAvailability(standardS3offer)
+      .then((newRegionFeature) =>
+        newRegionFeature.isFeatureAvailable(standardS3offer),
+      )
+      .then((status) => {
+        this.isS3User = status;
+        const guideList = this.isS3User ? STANDARD_S3 : GUIDES;
+        this.illustration = illustration;
+        this.guides = guideList.map((guide) => {
+          const descriptionKey = `pci_projects_project_storages_objects_onboarding_guides_${guide.id}_description`;
+          const description = this.$translate.instant(descriptionKey);
+          return {
+            ...guide,
+            title: this.$translate.instant(
+              `pci_projects_project_storages_objects_onboarding_guides_${guide.id}_title`,
+            ),
+            description: description !== descriptionKey ? description : '',
+            link: guide.link[this.user.ovhSubsidiary] || guide.link.DEFAULT,
+          };
+        });
+      });
   }
 
   onDocumentationClick(guide) {
