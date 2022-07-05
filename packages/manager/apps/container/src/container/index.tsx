4a7ee4fcaee249ef8a19e2be798bf9a6ed3377a2
Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
2022-07-05 17:58:12 +0200

    feat(livechat): create virtual agent and new Livechat
    ref:MANAGER-6214
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

--- packages/manager/apps/container/src/container/index.tsx
+++ packages/manager/apps/container/src/container/index.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 
+import { Environment } from '@ovh-ux/manager-config';
 import LegacyContainer from '@/container/legacy';
 import NavReshuffleContainer from '@/container/nav-reshuffle';
 import { useShell } from '@/context';
@@ -8,10 +9,24 @@ import { ProductNavReshuffleProvider } from '@/core/product-nav-reshuffle';
 import { ProgressProvider } from '@/context/progress';
 import CookiePolicy from '@/cookie-policy/CookiePolicy';
 import SSOAuthModal from '@/sso-auth-modal/SSOAuthModal';
+import LiveChat from '@/components/LiveChat';
 
 export default function Container(): JSX.Element {
-  const { isLoading, betaVersion, useBeta } = useContainer();
+  const {
+    isLoading,
+    betaVersion,
+    useBeta,
+    chatbotOpen,
+    chatbotReduced,
+    setChatbotReduced,
+  } = useContainer();
   const shell = useShell();
+  const environment: Environment = shell
+    .getPlugin('environment')
+    .getEnvironment();
+  const language = environment.getUserLanguage();
+  const { ovhSubsidiary } = environment.getUser();
+
   const isNavReshuffle = betaVersion && useBeta;
 
   useEffect(() => {
@@ -50,9 +65,21 @@ export default function Container(): JSX.Element {
             <NavReshuffleContainer />
           </ProductNavReshuffleProvider>
         ) : (
-          <LegacyContainer />
+          <>
+            <LegacyContainer />
+          </>
         )}
+        <LiveChat
+          language={language}
+          subsidiary={ovhSubsidiary}
+          open={chatbotOpen}
+          reduced={chatbotReduced}
+          onReduce={() => setChatbotReduced(true)}
+          onClose={() => shell.getPlugin('ux').closeChatbot()}
+          style={{ position: 'absolute' }}
+        ></LiveChat>
       </ProgressProvider>
+
       <Suspense fallback="">
         <SSOAuthModal />
       </Suspense>
