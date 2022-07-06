c09378497e0cddd99028bbe27bec99d1f19eace3
Hyenne Guillaume <guillaume.hyenne@ovhcloud.com>
2022-07-06 11:51:08 +0200

    feat(dedicated.server): hgv2 new upgrade flow (#7428)
    ref: MANAGER-5921
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/dedicated/server/dashboard/upgrade-request/upgrade.constants.js
+++
@@ -1,8 +0,0 @@
-export const ISSUE_TYPE_ID = 38089;
-export const UPGRADE_TRACKING_PREFIX =
-  'dedicated::dedicated::server::dashboard::upgrade';
-
-export default {
-  ISSUE_TYPE_ID,
-  UPGRADE_TRACKING_PREFIX,
-};
---
+++ packages/manager/modules/bm-server-components/src/technical-details/technical-details.constants.js
@@ -0,0 +1,8 @@
+export const UPGRADE_TYPE = {
+  RAM: 'ram',
+  STORAGE: 'storage',
+};
+
+export default {
+  UPGRADE_TYPE,
+};
