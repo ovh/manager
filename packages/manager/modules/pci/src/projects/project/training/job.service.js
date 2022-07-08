3c3ca8565c5d5da4ae7bcedc3334508a65fb2fe6
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2022-07-08 15:51:40 +0200

    fix(pci.ai): filter internal key on jobs volumes when resubmit (#7423)
    ref: DATATR-56
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
    Signed-off-by: Anoop <anooparveti@gmail.com>

--- packages/manager/modules/pci/src/projects/project/training/job.service.js
+++ packages/manager/modules/pci/src/projects/project/training/job.service.js
@@ -1,3 +1,4 @@
+import omit from 'lodash/omit';
 import Job from './job.class';
 
 export default class PciProjectTrainingJobService {
@@ -24,6 +25,31 @@ export default class PciProjectTrainingJobService {
     if (Object.prototype.hasOwnProperty.call(jobSpec, 'shutdown')) {
       // remove shutdown before submitting as it is forbidden server side and irrelevant when resubmitting
       toSubmit = (({ shutdown, ...j }) => j)(jobSpec);
+    }
+    if (jobSpec.volumes) {
+      // API does not accept volumes' internal property. We remove it before sending the request.
+      toSubmit.volumes = jobSpec.volumes.flatMap((volume) => {
+        let toRemove = false;
+        const internalKeys = [];
+        let filteredVolume = volume;
+        Object.keys(volume).forEach((key) => {
+          if (volume[key] && volume[key].internal !== undefined) {
+            if (volume[key].internal === true) {
+              toRemove = true;
+            } else {
+              internalKeys.push(key);
+            }
+          }
+        });
+        if (toRemove) {
+          return [];
+        }
+        // remove all 'internal' keys from object
+        internalKeys.forEach((key) => {
+          filteredVolume = omit(filteredVolume, `${key}.internal`);
+        });
+        return filteredVolume;
+      });
     }
     return this.submit(projectId, toSubmit);
   }
