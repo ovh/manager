52d8fdb03018c2ee77e7d8cda4bfc1e977de447f
sachinrameshn <104337442+sachinrameshn@users.noreply.github.com>
2022-07-11 16:33:58 +0530

    feat(public-cloud): add option to add s3 user in object storage (#7525)
    ref: MANAGER-9099
     add option to add s3 user by creating new user or by generating credentials in object storage
    
    Signed-off-by: sachinrameshn <sachin.ramesh@ovhcloud.com>
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

---
+++ packages/manager/modules/pci/src/projects/project/storages/object-storage/users/add/add.routing.js
@@ -0,0 +1,28 @@
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state(
+    'pci.projects.project.storages.object-storage.users.add',
+    {
+      url: '/new',
+      views: {
+        modal: {
+          component: 'pciProjectObjectStorageUsersAdd',
+        },
+      },
+      layout: 'modal',
+      resolve: {
+        breadcrumb: () => null, // Hide breadcrumb
+        cancel: /* @ngInject */ (goToUsers) => goToUsers,
+        goBack: /* @ngInject */ (goToUsersBanner) => goToUsersBanner,
+        usersCredentials: /* @ngInject */ (
+          projectId,
+          allUserList,
+          PciStoragesUsersService,
+        ) =>
+          PciStoragesUsersService.getUsersCredentials(
+            projectId,
+            allUserList.map((user) => user.id),
+          ),
+      },
+    },
+  );
+};
