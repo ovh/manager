08bcea667ba78feff0bb567690cc0946f7a860ca
sachinrameshn <104337442+sachinrameshn@users.noreply.github.com>
2022-07-12 21:53:19 +0530

    feat(public-cloud): include standard S3 in product listing page (#7526)
    * feat(public-cloud): include standard S3 in product listing page
    
     ref: MANAGER-9094
     include standard S3 in product listing page of object storage
    
    Signed-off-by: sachinrameshn <sachin.ramesh@ovhcloud.com>
    
    * feat(public-cloud): include standard S3 in product listing page
    
     ref: MANAGER-9094
    	include standard S3 and remove some of the columns from the table
    
    Signed-off-by: sachinrameshn <sachin.ramesh@ovhcloud.com>
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/containers/containers.controller.js
+++ packages/manager/modules/pci/src/projects/project/storages/containers/containers.controller.js
@@ -1,6 +1,8 @@
 import find from 'lodash/find';
 import { getCriteria } from '../../project.utils';
 
+import { OBJECT_CONTAINER_OFFERS_TYPES } from './containers.constants';
+
 export default class PciStoragesContainersController {
   /* @ngInject */
   constructor($translate, $http, CucCloudMessage) {
@@ -8,6 +10,7 @@ export default class PciStoragesContainersController {
     this.$http = $http;
     this.CucCloudMessage = CucCloudMessage;
     this.publicToggleLoading = false;
+    this.OBJECT_CONTAINER_OFFERS_TYPES = OBJECT_CONTAINER_OFFERS_TYPES;
   }
 
   $onInit() {
@@ -15,37 +18,6 @@ export default class PciStoragesContainersController {
     this.criteria = getCriteria('id', this.containerId);
     this.publicToggleLoading = false;
     this.hasHighPerformanceStorage = this.hasHighPerformanceStorage();
-  }
-
-  onPublicToggle(container) {
-    this.loadingContainer = container.id;
-    return this.$http
-      .put(`/cloud/project/${this.projectId}/storage/${container.id}`, {
-        containerType: container.state ? 'private' : 'public',
-      })
-      .then(() =>
-        this.CucCloudMessage.success(
-          this.$translate.instant(
-            container.state
-              ? 'pci_projects_project_storages_containers_toggle_private_succeed'
-              : 'pci_projects_project_storages_containers_toggle_public_succeed',
-            { name: container.name },
-          ),
-          'pci.projects.project.storages.containers.container',
-        ),
-      )
-      .catch((error) =>
-        this.CucCloudMessage.error(
-          this.$translate.instant(
-            'pci_projects_project_storages_containers_toggle_fail',
-            { message: error.data?.message },
-          ),
-          'pci.projects.project.storages.containers.container',
-        ),
-      )
-      .finally(() => {
-        this.loadingContainer = null;
-      });
   }
 
   loadMessages() {
