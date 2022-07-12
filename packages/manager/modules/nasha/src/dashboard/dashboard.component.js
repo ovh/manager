27a1791ff73f633d3831be6d7e120d7fbb29ce4b
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2022-07-12 11:05:54 +0200

    fix(nasha.dashboard): create partition link
    ref: MANAGER-9353
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/modules/nasha/src/dashboard/dashboard.component.js
+++ packages/manager/modules/nasha/src/dashboard/dashboard.component.js
@@ -8,7 +8,7 @@ export default {
     dashboardHref: '<',
     editNameHref: '<',
     goToEditName: '<',
-    goToPartitions: '<',
+    goToPartitionsCreate: '<',
     nasha: '<',
     nashaApiUrl: '<',
     reload: '<',
