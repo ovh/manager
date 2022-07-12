ced06b235fa6bb6afc0695332bc0c0a96878d1f3
Anoop N <anooparveti@gmail.com>
2022-07-12 16:27:13 +0530

    feat(pci.project): addition of floating ip section (#7485)
    Addition of Floating IPs listing page, actions to Additional IP
     Refactoring Additional IP section to accommodate Floating IP
     ref: MANAGER-9233
    
    Signed-off-by: Anoop <anooparveti@gmail.com>
    Signed-off-by: Anoop <anoop.n@ovhcloud.com>

---
+++ packages/manager/modules/pci/src/projects/project/additional-ips/floating-ips/floating-ips.component.js
@@ -0,0 +1,15 @@
+import controller from './floating-ips.controller';
+import template from './floating-ips.html';
+
+export default {
+  controller,
+  template,
+  bindings: {
+    floatingIps: '<',
+    goToAdditionalIpOrderPage: '<',
+    goToEditInstance: '<',
+    goToInstance: '<',
+    goToTerminate: '<',
+    projectId: '<',
+  },
+};
