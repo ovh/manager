f67a8a4f42cb38dc81431df46f3d8abfc9109e44
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2022-07-13 13:37:23 +0200

    fix(hub): add ephemeral banner for domains repricing
    ref: MANAGER-9696
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/apps/hub/src/dashboard/dashboard.controller.js
+++ packages/manager/apps/hub/src/dashboard/dashboard.controller.js
@@ -1,5 +1,22 @@
+import { DOMAINS_REPRICING, PRODUCT_TYPE_DOMAIN } from './dashboard.constants';
+
 export default class DashboardController {
   $onInit() {
+    this.hasDomainRepricing = false;
+
+    // Check if the tld of domains are present in the list affected by the domain repricing MANAGER-9696.
+    this.products
+      .then((elm) =>
+        elm.find((item) => item.productType === PRODUCT_TYPE_DOMAIN),
+      )
+      .then((domains) => {
+        this.hasDomainRepricing = !!domains?.data?.find((domain) => {
+          const domainName = domain.resource.name;
+          const tld = domainName.slice(domainName.lastIndexOf('.') + 1);
+          return DOMAINS_REPRICING.includes(tld);
+        });
+      });
+
     return this.trackingPrefix.then((prefix) => {
       this.prefix = prefix;
     });
