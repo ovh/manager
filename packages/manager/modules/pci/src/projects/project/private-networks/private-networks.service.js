74fe42f852ce036bd5dfb841d9f3419053bbfb45
vks-ovh <102142952+vks-ovh@users.noreply.github.com>
2022-07-13 15:29:35 +0530

    refactor(pci.private.networks): private network list page performance (#7540)
    ref: MANAGER-9681
    
    Signed-off-by: vikash singh <vikash.singh@corp.ovh.com>

--- packages/manager/modules/pci/src/projects/project/private-networks/private-networks.service.js
+++ packages/manager/modules/pci/src/projects/project/private-networks/private-networks.service.js
@@ -1,4 +1,8 @@
-import { VRACK_CREATION_ACTION } from './private-networks.constants';
+import sortBy from 'lodash/sortBy';
+import {
+  DELETING_STATUS,
+  VRACK_CREATION_ACTION,
+} from './private-networks.constants';
 
 export default class {
   /* @ngInject */
@@ -19,51 +23,19 @@ export default class {
   }
 
   getPrivateNetworks(serviceName) {
-    this.OvhApiCloudProjectNetworkPrivate.v6().resetQueryCache();
-    return this.OvhApiCloudProjectNetworkPrivate.v6()
-      .query({
-        serviceName,
-      })
-      .$promise.then((privateNetworks) =>
-        this.$q.all(
-          privateNetworks.map((privateNetwork) =>
-            this.getSubnets(serviceName, privateNetwork),
-          ),
-        ),
-      );
-  }
-  // TODO: getSubnets call need to trigger on expanding the rows for which we need to update expandable data grid under UI kit. currently calling it under getPrivateNetworks
-
-  getSubnets(serviceName, privateNetwork) {
-    return this.OvhApiCloudProjectNetworkPrivateSubnet.v6()
-      .query({
-        serviceName,
-        networkId: privateNetwork.id,
-      })
-      .$promise.then((data) => {
-        return {
-          ...privateNetwork,
-          subnet: [
-            ...data.map((subnet) => ({
-              ...subnet,
-              allocatedIp: subnet.ipPools
-                .map((ipPool) => `${ipPool.start} - ${ipPool.end}`)
-                .join(' ,'),
-              dhcp: subnet.ipPools
-                .map((ipPool) =>
-                  ipPool.dhcp === true
-                    ? this.$translate.instant(
-                        'pci_projects_project_network_private_dhcp_active',
-                      )
-                    : this.$translate.instant(
-                        'pci_projects_project_network_private_dhcp_suspended',
-                      ),
-                )
-                .join(),
-            })),
-          ],
-        };
+    return this.$http
+      .get(`/cloud/project/${serviceName}/network/private`)
+      .then(({ data: privateNetworks }) => {
+        return sortBy(privateNetworks, 'vlanId').filter(
+          ({ status }) => !DELETING_STATUS.includes(status),
+        );
       });
+  }
+
+  getSubnets(serviceName, networkId) {
+    return this.$http
+      .get(`/cloud/project/${serviceName}/network/private/${networkId}/subnet`)
+      .then((data) => data);
   }
 
   getVrack(serviceName) {
