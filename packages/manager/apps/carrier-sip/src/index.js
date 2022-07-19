828a38c40d228731062e86f1062cf8455ab3e29b
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2022-07-19 12:04:33 +0200

    refactor(carrier-sip): remove preloader
    ref: MANAGER-9746
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/carrier-sip/src/index.js
+++ packages/manager/apps/carrier-sip/src/index.js
@@ -3,22 +3,10 @@ import 'whatwg-fetch';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-import {
-  attach as attachPreloader,
-  displayMessage,
-} from '@ovh-ux/manager-preloader';
-
 import { registerApplication } from '@ovh-ux/ufrontend';
-import { findAvailableLocale, detectUserLocale } from '@ovh-ux/manager-config';
-
-attachPreloader(findAvailableLocale(detectUserLocale()));
 
 registerApplication('carrier-sip').then(({ environment }) => {
   environment.setVersion(__VERSION__);
-
-  if (environment.getMessage()) {
-    displayMessage(environment.getMessage(), environment.getUserLanguage());
-  }
 
   import(`./config-${environment.getRegion()}`)
     .catch(() => {})
