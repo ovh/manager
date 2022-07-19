179916cc17700a7169e2c384fd16198a040f613d
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2022-07-19 13:27:11 +0200

    refactor(dbaas-logs): remove preloader
    ref: MANAGER-9746
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/dbaas-logs/src/app.module.js
+++ packages/manager/apps/dbaas-logs/src/app.module.js
@@ -8,7 +8,6 @@ import { isString, get } from 'lodash-es';
 
 import uiRouter from '@uirouter/angularjs';
 import { registerCoreModule } from '@ovh-ux/manager-core';
-import { detach as detachPreloader } from '@ovh-ux/manager-preloader';
 import ngOvhUiRouterLineProgress from '@ovh-ux/ng-ui-router-line-progress';
 import ngUiRouterBreadcrumb from '@ovh-ux/ng-ui-router-breadcrumb';
 
@@ -56,7 +55,6 @@ export default (containerEl, environment) => {
     .run(
       /* @ngInject */ ($rootScope, $transitions) => {
         const unregisterHook = $transitions.onSuccess({}, () => {
-          detachPreloader();
           $rootScope.$broadcast('app:started');
           unregisterHook();
         });
