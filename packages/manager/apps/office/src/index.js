58fc99406fd2d39250e503389bf227bdbe46f885
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2022-07-19 14:17:32 +0200

    refactor(office): remove preloader
    ref: MANAGER-9746
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/office/src/index.js
+++ packages/manager/apps/office/src/index.js
@@ -1,21 +1,10 @@
 import 'regenerator-runtime/runtime';
 import 'script-loader!jquery'; // eslint-disable-line
 import 'whatwg-fetch';
-import {
-  attach as attachPreloader,
-  displayMessage,
-} from '@ovh-ux/manager-preloader';
 import { registerApplication } from '@ovh-ux/ufrontend';
-import { findAvailableLocale, detectUserLocale } from '@ovh-ux/manager-config';
-
-attachPreloader(findAvailableLocale(detectUserLocale()));
 
 registerApplication('office').then(({ environment }) => {
   environment.setVersion(__VERSION__);
-
-  if (environment.getMessage()) {
-    displayMessage(environment.getMessage(), environment.getUserLanguage());
-  }
 
   import(`./config-${environment.getRegion()}`)
     .catch(() => {})
