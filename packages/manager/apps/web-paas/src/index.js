3104c46923d9fb6063987eac56caa295e5002d88
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2022-07-19 15:37:36 +0200

    refactor(web-paas): remove preloader
    ref: MANAGER-9746
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/web-paas/src/index.js
+++ packages/manager/apps/web-paas/src/index.js
@@ -2,11 +2,7 @@
 import 'script-loader!jquery';
 /* eslint-enable import/no-webpack-loader-syntax */
 import 'whatwg-fetch';
-import { attach as attachPreloader } from '@ovh-ux/manager-preloader';
 import { registerApplication } from '@ovh-ux/ufrontend';
-import { findAvailableLocale, detectUserLocale } from '@ovh-ux/manager-config';
-
-attachPreloader(findAvailableLocale(detectUserLocale()));
 
 registerApplication('web-paas').then(({ environment }) => {
   environment.setVersion(__VERSION__);
