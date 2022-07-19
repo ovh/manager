33311890f248abdcf09141f38834d87fc6943aa5
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2022-07-19 13:57:28 +0200

    refactor(exchange): remove preloader
    ref: MANAGER-9746
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/exchange/src/app.module.js
+++ packages/manager/apps/exchange/src/app.module.js
@@ -4,7 +4,6 @@ import angular from 'angular';
 import uiRouter from '@uirouter/angularjs';
 import { isString, get } from 'lodash-es';
 import { registerCoreModule } from '@ovh-ux/manager-core';
-import { detach as detachPreloader } from '@ovh-ux/manager-preloader';
 import ngOvhUiRouterLineProgress from '@ovh-ux/ng-ui-router-line-progress';
 import ngUiRouterBreadcrumb from '@ovh-ux/ng-ui-router-breadcrumb';
 
@@ -64,7 +63,6 @@ export default (containerEl, environment) => {
     .run(
       /* @ngInject */ ($rootScope, $transitions) => {
         const unregisterHook = $transitions.onSuccess({}, () => {
-          detachPreloader();
           $rootScope.$broadcast('app:started');
           unregisterHook();
         });
--- packages/manager/apps/exchange/src/index.js
+++ packages/manager/apps/exchange/src/index.js
@@ -3,21 +3,10 @@ import 'script-loader!filesize/lib/filesize.js'; // eslint-disable-line
 import 'script-loader!urijs/src/URI.min.js'; // eslint-disable-line
 import 'script-loader!jquery'; // eslint-disable-line
 import 'whatwg-fetch';
-import {
-  attach as attachPreloader,
-  displayMessage,
-} from '@ovh-ux/manager-preloader';
 import { registerApplication } from '@ovh-ux/ufrontend';
-import { findAvailableLocale, detectUserLocale } from '@ovh-ux/manager-config';
-
-attachPreloader(findAvailableLocale(detectUserLocale()));
 
 registerApplication('exchange').then(({ environment }) => {
   environment.setVersion(__VERSION__);
-
-  if (environment.getMessage()) {
-    displayMessage(environment.getMessage(), environment.getUserLanguage());
-  }
 
   import(`./config-${environment.getRegion()}`)
     .catch(() => {})
