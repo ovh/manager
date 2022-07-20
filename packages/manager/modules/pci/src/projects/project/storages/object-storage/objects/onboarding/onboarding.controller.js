d5ec5d3273087bd7e381c8c8eebc46414a665fdd
sachinrameshn <104337442+sachinrameshn@users.noreply.github.com>
2022-07-20 15:35:29 +0530

    feat(public-cloud): implement the tracking plan for Object storage (#7610)
    ref: MANAGER-9493
     implement the tracking plan for Object Storage
    
    Signed-off-by: sachinrameshn <sachin.ramesh@ovhcloud.com>
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/object-storage/objects/onboarding/onboarding.controller.js
+++ packages/manager/modules/pci/src/projects/project/storages/object-storage/objects/onboarding/onboarding.controller.js
@@ -39,7 +39,7 @@ export default class PciStorageObjectsOnboardingController {
 
   onDocumentationClick(guide) {
     this.atInternet.trackClick({
-      name: `${this.trackingPrefix}onboarding::documentation::${guide.title}`,
+      name: `${this.trackingPrefix}onboarding::documentation::${guide.id}`,
       type: 'action',
     });
   }
