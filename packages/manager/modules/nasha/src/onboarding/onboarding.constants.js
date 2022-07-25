44aba284ae4dfbc729694d56c0b466370f2812a5
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2022-07-25 12:35:45 +0200

    fix(nasha.onboarding): missing onboarding links
    ref: MANAGER-9229
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/modules/nasha/src/onboarding/onboarding.constants.js
+++ packages/manager/modules/nasha/src/onboarding/onboarding.constants.js
@@ -1,11 +1,19 @@
+const GETTING_STARTED_FR = 'https://docs.ovh.com/fr/storage/nas/decouverte/';
+const GETTING_STARTED_GB =
+  'https://docs.ovh.com/gb/en/storage/nas/get-started/';
+const NFS_FR = 'https://docs.ovh.com/fr/storage/nas/nas-nfs/';
+const NFS_GB = 'https://docs.ovh.com/gb/en/storage/nas-nfs/';
+const CIFS_FR = 'https://docs.ovh.com/fr/storage/nas/nas-cifs/';
+const CIFS_GB = 'https://docs.ovh.com/gb/en/storage/nas/nas-cifs/';
+
 export const GUIDES = [
   {
     id: 'getting-started',
     link: {
-      FR: 'https://docs.ovh.com/fr/storage/nas/decouverte/',
+      FR: GETTING_STARTED_FR,
       DE: 'https://docs.ovh.com/de/storage/nas/erste-schritte/',
       CA: 'https://docs.ovh.com/ca/en/storage/nas/get-started/',
-      GB: 'https://docs.ovh.com/gb/en/storage/nas/get-started/',
+      GB: GETTING_STARTED_GB,
       IE: 'https://docs.ovh.com/ie/en/storage/nas/get-started/',
       US: 'https://docs.ovh.com/us/en/storage/nas/get-started/',
       ES: 'https://docs.ovh.com/es/storage/nas/primeros-pasos/',
@@ -15,15 +23,22 @@ export const GUIDES = [
       IT: 'https://docs.ovh.com/it/storage/nas/iniziare/',
       PL: 'https://docs.ovh.com/pl/storage/nas/pierwsze-kroki/',
       PT: 'https://docs.ovh.com/pt/storage/nas/primeiros-passos/',
+      MA: GETTING_STARTED_FR,
+      SN: GETTING_STARTED_FR,
+      TN: GETTING_STARTED_FR,
+      NL: GETTING_STARTED_GB,
+      AU: GETTING_STARTED_GB,
+      SG: GETTING_STARTED_GB,
+      ASIA: GETTING_STARTED_GB,
     },
   },
   {
     id: 'nfs',
     link: {
-      FR: 'https://docs.ovh.com/fr/storage/nas/nas-nfs/',
+      FR: NFS_FR,
       DE: 'https://docs.ovh.com/de/storage/nas-nfs/',
       CA: 'https://docs.ovh.com/ca/en/storage/nas-nfs/',
-      GB: 'https://docs.ovh.com/gb/en/storage/nas-nfs/',
+      GB: NFS_GB,
       IE: 'https://docs.ovh.com/ie/en/storage/nas-nfs/',
       US: 'https://docs.ovh.com/us/en/storage/nas-nfs/',
       ES: 'https://docs.ovh.com/es/storage/nas-nfs/',
@@ -33,15 +48,22 @@ export const GUIDES = [
       IT: 'https://docs.ovh.com/it/storage/nas-nfs/',
       PL: 'https://docs.ovh.com/pl/storage/nas-nfs/',
       PT: 'https://docs.ovh.com/pt/storage/nas-nfs/',
+      MA: NFS_FR,
+      SN: NFS_FR,
+      TN: NFS_FR,
+      NL: NFS_GB,
+      AU: NFS_GB,
+      SG: NFS_GB,
+      ASIA: NFS_GB,
     },
   },
   {
     id: 'cifs',
     link: {
-      FR: 'https://docs.ovh.com/fr/storage/nas/nas-cifs/',
+      FR: CIFS_FR,
       DE: 'https://docs.ovh.com/de/storage/nas/nas-cifs/',
       CA: 'https://docs.ovh.com/ca/en/storage/nas/nas-cifs/',
-      GB: 'https://docs.ovh.com/gb/en/storage/nas/nas-cifs/',
+      GB: CIFS_GB,
       IE: 'https://docs.ovh.com/ie/en/storage/nas/nas-cifs/',
       US: 'https://docs.ovh.com/us/en/storage/nas/nas-cifs/',
       ES: 'https://docs.ovh.com/es/storage/nas/nas-cifs/',
@@ -51,6 +73,13 @@ export const GUIDES = [
       IT: 'https://docs.ovh.com/it/storage/nas/nas-cifs/',
       PL: 'https://docs.ovh.com/pl/storage/nas/nas-cifs/',
       PT: 'https://docs.ovh.com/pt/storage/nas/nas-cifs/',
+      MA: CIFS_FR,
+      SN: CIFS_FR,
+      TN: CIFS_FR,
+      NL: CIFS_GB,
+      AU: CIFS_GB,
+      SG: CIFS_GB,
+      ASIA: CIFS_GB,
     },
   },
 ];
