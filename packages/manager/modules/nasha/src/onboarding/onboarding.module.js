405dcbd44291d7772537bd01e5ff8b7f00de84a5
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2022-07-25 11:19:39 +0200

    refactor(nasha): rebase on develop
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/modules/nasha/src/onboarding/onboarding.module.js
+++ packages/manager/modules/nasha/src/onboarding/onboarding.module.js
@@ -3,6 +3,7 @@ import '@ovh-ux/ng-translate-async-loader';
 import '@uirouter/angularjs';
 import 'angular-translate';
 import '@ovh-ux/ui-kit';
+
 import { OnboardingLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
 
 import component from './onboarding.component';
