86b1bf4978aad7f69baac31ac5a943e86c9bef6d
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2022-07-26 09:54:25 +0200

    fix(nasha.order): catalog order link
    ref: MANAGER-9812
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/modules/nasha/src/order/index.js
+++ packages/manager/modules/nasha/src/order/index.js
@@ -7,16 +7,21 @@ const moduleName = 'ovhManagerNashaOrderLazyLoading';
 
 angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
   /* @ngInject */ ($stateProvider) => {
-    $stateProvider.state('nasha.order.**', {
-      url: '/order',
-      lazyLoad: (transition) =>
-        import('./order.module').then((module) =>
-          transition
-            .injector()
-            .get('$ocLazyLoad')
-            .inject(module.default),
-        ),
-    });
+    $stateProvider
+      .state('nasha.order.**', {
+        url: '/order',
+        lazyLoad: (transition) =>
+          import('./order.module').then((module) =>
+            transition
+              .injector()
+              .get('$ocLazyLoad')
+              .inject(module.default),
+          ),
+      })
+      .state('nasha.new.**', {
+        url: '/new',
+        redirectTo: 'nasha.order',
+      });
   },
 );
 
