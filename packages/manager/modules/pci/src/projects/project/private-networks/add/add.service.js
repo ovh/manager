b9447e4f147c77fe0a212617155a92863cc710a1
Anoop <anooparveti@gmail.com>
2022-07-26 20:49:20 +0530

    feat(pci.project): private network config page enhancements
     ref: MANAGER-9680
    
    Signed-off-by: Anoop <anooparveti@gmail.com>

--- packages/manager/modules/pci/src/projects/project/private-networks/add/add.service.js
+++ packages/manager/modules/pci/src/projects/project/private-networks/add/add.service.js
@@ -1,61 +1,132 @@
-import { NETWORK_ACTIVE_STATUS, DEFAULT_IP } from './add.constants';
+import { DEFAULT_IP } from './add.constants';
 
-export default class {
+export default class PrivateNetworkAddService {
   /* @ngInject */
-  constructor(
-    OvhApiCloudProjectNetworkPrivate,
-    OvhApiCloudProjectNetworkPrivateSubnet,
-    Poller,
-  ) {
-    this.OvhApiCloudProjectNetworkPrivate = OvhApiCloudProjectNetworkPrivate;
-    this.OvhApiCloudProjectNetworkPrivateSubnet = OvhApiCloudProjectNetworkPrivateSubnet;
+  constructor($http, Poller) {
+    this.$http = $http;
     this.Poller = Poller;
   }
 
-  create(serviceName, privateNetwork, subnets) {
-    return this.OvhApiCloudProjectNetworkPrivate.v6()
-      .save(
-        {
-          serviceName,
-        },
-        privateNetwork,
+  create(
+    projectId,
+    region,
+    privateNetworkName,
+    subnet,
+    vlanId = null,
+    gateway,
+  ) {
+    return this.$http
+      .post(`/cloud/project/${projectId}/region/${region}/network`, {
+        name: privateNetworkName,
+        subnet,
+        ...(gateway && { gateway }),
+        ...(typeof vlanId === 'number' && { vlanId }),
+      })
+      .then(({ data: { id } }) =>
+        this.checkPrivateNetworkCreationStatus(projectId, id),
       )
-      .$promise.then(({ id }) =>
-        this.checkPrivateNetworkCreationStatus(serviceName, id),
-      )
-      .then((network) => this.createSubnets(serviceName, network.id, subnets));
+      .then(({ resourceId }) => {
+        this.Poller.kill({ namespace: 'private-network-creation' });
+        return this.getCreatedSubnet(projectId, region, resourceId);
+      });
   }
 
-  createSubnets(serviceName, networkId, subnets) {
-    return Promise.all(
-      subnets.map(
-        (subnet) =>
-          this.OvhApiCloudProjectNetworkPrivateSubnet.v6().save(
-            {
-              serviceName,
-              networkId,
-            },
-            subnet,
-          ).$promise,
-      ),
-    );
-  }
-
-  checkPrivateNetworkCreationStatus(serviceName, networkId) {
-    this.OvhApiCloudProjectNetworkPrivate.v6().resetCache();
+  checkPrivateNetworkCreationStatus(projectId, operationId) {
     return this.Poller.poll(
-      `/cloud/project/${serviceName}/network/private/${networkId}`,
+      `/cloud/project/${projectId}/operation/${operationId}`,
       {},
       {
         method: 'get',
         successRule: {
-          status: NETWORK_ACTIVE_STATUS,
+          status: 'completed',
         },
+        namespace: 'private-network-creation',
       },
     );
   }
 
+  getCreatedSubnet(projectId, region, networkId) {
+    return this.$http
+      .get(
+        `/cloud/project/${projectId}/region/${region}/network/${networkId}/subnet`,
+      )
+      .then(({ data }) => data);
+  }
+
   static generateNetworkAddress(vlanId) {
     return DEFAULT_IP.replace('{vlanId}', vlanId % 255);
+  }
+
+  getAllRegions(projectId) {
+    return this.$http
+      .get(`/cloud/project/${projectId}/region`)
+      .then(({ data }) => data);
+  }
+
+  getRegionInfo(projectId, region) {
+    return this.$http
+      .get(`/cloud/project/${projectId}/region/${region}`)
+      .then(({ data }) => data);
+  }
+
+  getGateways(projectId, regionName) {
+    return this.$http
+      .get(`/cloud/project/${projectId}/region/${regionName}/gateway`)
+      .then(({ data }) => data);
+  }
+
+  enableSnatOnGateway(projectId, region, gatewayId) {
+    return this.$http
+      .post(
+        `/cloud/project/${projectId}/region/${region}/gateway/${gatewayId}/expose`,
+      )
+      .then(({ data }) => data);
+  }
+
+  getSmallestGatewayInfo(ovhSubsidiary) {
+    return this.$http
+      .get('/order/catalog/public/cloud', {
+        params: {
+          ovhSubsidiary,
+          productName: 'cloud',
+        },
+      })
+      .then(({ data }) => {
+        // pick the variants of product with least price
+        const gatewayProducts = data.addons
+          .filter((addon) => addon.product.startsWith('publiccloud-gateway'))
+          .sort(
+            (
+              { pricings: [{ price: priceA }] },
+              { pricings: [{ price: priceB }] },
+            ) => priceA - priceB,
+          )
+          .filter(({ product }, index, arr) => product === arr[0].product);
+        const [monthlyPriceObj] = gatewayProducts.find(({ planCode }) =>
+          planCode.includes('month'),
+        )?.pricings;
+        const [hourlyPriceObj] = gatewayProducts.find(({ planCode }) =>
+          planCode.includes('hour'),
+        )?.pricings;
+        return {
+          size: gatewayProducts[0].product
+            .split('-')
+            .slice(-1)
+            .join(),
+          pricePerMonth: monthlyPriceObj.price,
+          pricePerHour: hourlyPriceObj.price,
+        };
+      });
+  }
+
+  associateGatewayToNetwork(projectId, region, gatewayId, subnetId) {
+    return this.$http
+      .post(
+        `/cloud/project/${projectId}/region/${region}/gateway/${gatewayId}/interface`,
+        {
+          subnetId,
+        },
+      )
+      .then(({ data }) => data);
   }
 }
