a01796e6995690ca18f741bee3210ab82b5cc033
Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
2022-07-27 12:03:13 +0200

    feat(checkbox): add checkbox styles
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

--- packages/components/ovh-themes/ui-kit/components/Checkbox.ts
+++ packages/components/ovh-themes/ui-kit/components/Checkbox.ts
@@ -1,28 +1,71 @@
 // eslint-disable-next-line prettier/prettier
-import type {
-  PartsStyleFunction,
-  PartsStyleObject,
-  SystemStyleFunction,
-  SystemStyleObject,
-} from "@chakra-ui/theme-tools";
+import type { SystemStyleObject } from "@chakra-ui/theme-tools";
 import type { ComponentMultiStyleConfig } from '@chakra-ui/theme';
-
-import { checkboxAnatomy as parts } from "@chakra-ui/anatomy";
+import { checkboxAnatomy as parts } from '@chakra-ui/anatomy';
 
 const controlBaseStyle: SystemStyleObject = {
   fontSize: 'md',
-  lineHeight: 1.5,
   margin: 0,
+  borderColor: 'uikit.500',
+  boxSizing: 'border-box',
+  _invalid: {
+    borderColor: 'error.500',
+    _checked: {
+      background: 'error.500',
+    },
+  },
+  _disabled: {
+    borderColor: 'uikit.500',
+    background: 'white',
+    _checked: {
+      borderColor: 'uikit.500',
+      background: 'uikit.500',
+      color: 'white',
+    },
+    _invalid: {
+      borderColor: 'error.500',
+      color: 'white',
+      _checked: {
+        background: 'error.500',
+      },
+    },
+  },
+  _focus: {
+    boxShadow: 'secondary',
+  },
+};
+
+const containerBaseStyle: SystemStyleObject = {
+  fontWeight: 700,
+  _disabled: {
+    opacity: '0.5',
+  },
+};
+
+const labelBaseStyle: SystemStyleObject = {
+  _checked: {
+    fontWeight: 700,
+  },
+  _disabled: {
+    color: 'uikit.800-text',
+  },
+  lineHeight: 1.5,
+  fontWeight: 500,
+  fontSize: 'md',
+  color: 'uikit.800-text',
+  letterSpacing: '.008rem',
 };
 
 const Checkbox: ComponentMultiStyleConfig = {
   parts: parts.keys,
   baseStyle: {
+    container: containerBaseStyle,
     control: controlBaseStyle,
+    label: labelBaseStyle,
   },
   defaultProps: {
     colorScheme: 'uikit',
-  }
-}
+  },
+};
 
 export default Checkbox;
