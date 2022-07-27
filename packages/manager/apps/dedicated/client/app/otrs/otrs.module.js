600f258b5aa2ac52cae904d7a9be66f067464ab0
Anoop N <anooparveti@gmail.com>
2022-07-27 13:55:09 +0530

    feat(support): align Support ticket view for US region with EU (#7425)
     ref: MANAGER-9530
    
    Signed-off-by: Anoop <anooparveti@gmail.com>

--- packages/manager/apps/dedicated/client/app/otrs/otrs.module.js
+++ packages/manager/apps/dedicated/client/app/otrs/otrs.module.js
@@ -1,6 +1,8 @@
 import ovhManagerCore from '@ovh-ux/manager-core';
 import ngOvhOtrs from '@ovh-ux/ng-ovh-otrs';
+import { supportComponents } from '@ovh-ux/manager-support';
 
+import component from './otrs.component';
 import routing from './otrs.routes';
 import service from './otrs.service';
 import htmlStringLinkyFilter from './filter/html-string-linky.filter';
@@ -17,6 +19,7 @@ angular
     'pascalprecht.translate',
     'ui.bootstrap',
     'ui.router',
+    supportComponents,
   ])
   .config(
     /* @ngInject */ (OtrsPopupProvider, coreURLBuilderProvider) => {
@@ -25,6 +28,7 @@ angular
       );
     },
   )
+  .component('otrsComponent', component)
   .filter('htmlStringLinky', htmlStringLinkyFilter)
   .service('Otrs', service)
   .config(routing)
