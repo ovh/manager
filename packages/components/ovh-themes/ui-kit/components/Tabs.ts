4205cb7fbc05183f78ec9afebc291df5f52fa1e8
Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
2022-07-27 10:10:31 +0200

    feat(tabs): add tab light variant
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

--- packages/components/ovh-themes/ui-kit/components/Tabs.ts
+++ packages/components/ovh-themes/ui-kit/components/Tabs.ts
@@ -1,6 +1,7 @@
 // eslint-disable-next-line prettier/prettier
 import type { ComponentMultiStyleConfig } from '@chakra-ui/theme';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { tabsAnatomy as parts } from '@chakra-ui/anatomy';
 
 const Tabs: ComponentMultiStyleConfig = {
@@ -69,47 +70,80 @@ const Tabs: ComponentMultiStyleConfig = {
 };
 =======
 import { tabsAnatomy as parts } from "@chakra-ui/anatomy";
+=======
+import { tabsAnatomy as parts } from '@chakra-ui/anatomy';
+>>>>>>> feat(tabs): add tab light variant
 
 const Tabs: ComponentMultiStyleConfig = {
   parts: parts.keys,
+  baseStyle: {
+    tab: {
+      height: '14',
+      background: 'white',
+      spacing: '1',
+      borderRadius: '6px',
+      borderBottomRadius: '0px',
+      color: 'uikit.500',
+      fontSize: 'lg',
+      fontWeight: 700,
+      lineHeight: 'shorter',
+      letterSpacing: '0.007em',
+      boxSizing: 'border-box',
+      borderWidth: '.125rem',
+    },
+    tabpanel : {
+      borderBottomRadius: '6px',
+    }
+  },
   variants: {
     enclosed: {
       tab: {
-        height: '14',
-        background: 'white',
-        spacing: '1',
-        borderRadius: '6px',
-        borderBottomRadius: '0px',
         borderColor: 'uikit.100',
-        color: 'uikit.500',
-        fontSize: 'lg',
-        fontWeight: 700,
-        lineHeight: 'shorter',
-        letterSpacing: '0.007em',
         _selected: {
           color: 'uikit.800-text',
           background: 'uikit.50',
           borderColor: 'uikit.100',
           _hover: {
-            background: 'uikit.50'
-          }
+            background: 'uikit.50',
+          },
         },
         _hover: {
           background: 'uikit.100',
-        }
+        },
       },
-      tabpanel : {
+      tabpanel: {
         background: 'uikit.50',
         borderColor: 'uikit.100',
         borderWidth: '1px',
-        borderBottomRadius: '6px'
       },
+    },
+    light: {
+      tab: {
+        borderColor: 'white',
+        borderRadius: '0px',
+        borderWidth: '0',
+        borderBottomWidth: '.125rem',
+        marginRight: '1.5rem',
+        _selected: {
+          borderBottomColor: 'uikit.500',
+        },
+        _hover: {
+          color: 'uikit.700',
+          borderBottomColor: 'uikit.700',
+        }
+      }
     }
   },
   defaultProps: {
+<<<<<<< HEAD
     variant: 'enclosed'
   }
 }
 >>>>>>> feat(tabs): add tabs to chakra theme
+=======
+    variant: 'enclosed',
+  },
+};
+>>>>>>> feat(tabs): add tab light variant
 
 export default Tabs;
