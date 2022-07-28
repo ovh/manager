894ecce014c1b2b3e23590339aaa69ef156fc13a
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2022-07-28 15:59:44 +0200

    fix(object-storage.users.add): fix translation issue
    ref: MANAGER-9823
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/object-storage/users/add/add.module.js
+++ packages/manager/modules/pci/src/projects/project/storages/object-storage/users/add/add.module.js
@@ -4,6 +4,9 @@ import userAdd from './user-add';
 import routing from './add.routing';
 
 const moduleName = 'ovhManagerObjectStorageUsersAdd';
-angular.module(moduleName, [userAdd, 'ui.router']).config(routing);
+angular
+  .module(moduleName, [userAdd, 'ui.router'])
+  .config(routing)
+  .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
