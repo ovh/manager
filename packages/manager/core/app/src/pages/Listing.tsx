dbdc3034f7454332fd8d3c51d24129ba0fffb82f
Florian Renaut <florian.renaut@corp.ovh.com>
2022-08-04 09:28:37 +0200

    feat(react-app): add search field
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/core/app/src/pages/Listing.tsx
+++ packages/manager/core/app/src/pages/Listing.tsx
@@ -1,51 +1,85 @@
 import React, { useEffect, useState } from 'react';
-import { Badge } from '@chakra-ui/react';
-import Listing, { ListingData, ListingState } from '../components/Listing';
+import { Badge, Flex, Spacer, Stack } from '@chakra-ui/react';
+import Listing, { ListingData, ListingState } from '@/components/Listing';
+import SearchInput from '@/components/SearchInput';
 import { listVps, Vps } from '@/api/Vps';
 
+type ListingPageState = {
+  list: ListingState;
+  search: string;
+};
+
 export default function ListingPage(): JSX.Element {
-  const [listingState, setListingState] = useState<ListingState>({
-    currentPage: 1,
-    pageSize: 10,
+  const [state, setState] = useState<ListingPageState>({
+    list: {
+      currentPage: 1,
+      pageSize: 10,
+    },
+    search: '',
   });
-  const [listingData, setListingData] = useState<ListingData<Vps>>();
-  const { currentPage, pageSize, sort } = listingState;
+  const [services, setServices] = useState<ListingData<Vps>>();
 
   useEffect(() => {
-    setListingData(null);
+    const { currentPage, pageSize, sort } = state.list;
+    setServices(null);
     listVps({
       currentPage,
       pageSize,
+      search: {
+        key: 'name',
+        value: state.search,
+      },
       sortBy: sort?.key,
       sortReverse: sort?.reverse,
     }).then(({ totalCount, data }) =>
-      setListingData({
+      setServices({
         total: totalCount,
         items: data,
       }),
     );
-  }, [listingState]);
+  }, [state]);
 
   return (
-    <Listing
-      columns={[
-        {
-          key: 'name',
-          label: 'Name',
-        },
-        {
-          key: 'zone',
-          label: 'Zone',
-        },
-        {
-          key: 'state',
-          label: 'State',
-          renderer: (vps) => <Badge>{vps.state}</Badge>,
-        },
-      ]}
-      data={listingData}
-      state={listingState}
-      onChange={setListingState}
-    />
+    <Stack>
+      <Flex>
+        <Spacer />
+        <SearchInput
+          onSubmit={(value) =>
+            setState({
+              list: {
+                ...state.list,
+                currentPage: 1,
+              },
+              search: value,
+            })
+          }
+        />
+      </Flex>
+      <Listing
+        columns={[
+          {
+            key: 'name',
+            label: 'Name',
+          },
+          {
+            key: 'zone',
+            label: 'Zone',
+          },
+          {
+            key: 'state',
+            label: 'State',
+            renderer: (vps) => <Badge>{vps.state}</Badge>,
+          },
+        ]}
+        data={services}
+        state={state.list}
+        onChange={(list) =>
+          setState({
+            ...state,
+            list,
+          })
+        }
+      />
+    </Stack>
   );
 }
