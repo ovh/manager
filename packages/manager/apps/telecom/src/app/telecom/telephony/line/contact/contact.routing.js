a981de680f7fcbc63b745c4130bef290ed9cd193
Hyenne Guillaume <guillaume.hyenne@ovhcloud.com>
2022-08-08 15:45:56 +0200

    fix(telephony): status update not working in contact section (#7728)
    ref: DTRSD-94377
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/apps/telecom/src/app/telecom/telephony/line/contact/contact.routing.js
+++ packages/manager/apps/telecom/src/app/telecom/telephony/line/contact/contact.routing.js
@@ -13,6 +13,33 @@ export default /* @ngInject */ ($stateProvider) => {
       resolve: {
         breadcrumb: /* @ngInject */ ($translate) =>
           $translate.instant('telephony_line_contact_breadcrumb'),
+        goToContactDirectory: /* @ngInject */ (
+          $state,
+          billingAccount,
+          serviceName,
+          TucToast,
+        ) => (message = false, type = 'success') => {
+          const reload = message && type === 'success';
+
+          const promise = $state.go(
+            'telecom.telephony.billingAccount.line.dashboard.contact',
+            {
+              billingAccount,
+              serviceName,
+            },
+            {
+              reload,
+            },
+          );
+
+          if (message) {
+            promise.then(() => {
+              TucToast[type](message);
+            });
+          }
+
+          return promise;
+        },
       },
     },
   );
