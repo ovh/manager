0fbd30e293dc7aae9bff810f4af7f60697e743b4
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2022-08-10 16:48:44 +0200

    fix(nasha): change pattern for snapshot name
    ref: MANAGER-9688
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/modules/nasha/src/dashboard/partition/snapshots/snapshots.constants.js
+++ packages/manager/modules/nasha/src/dashboard/partition/snapshots/snapshots.constants.js
@@ -1,4 +1,4 @@
-export const CUSTOM_SNAPSHOT_NAME_PATTERN = /^[a-zA-Z0-9,.:-]+$/;
+export const CUSTOM_SNAPSHOT_NAME_PATTERN = /^[a-zA-Z0-9.:-]+$/;
 export const CUSTOM_SNAPSHOT_NAME_PREFIX = 'snap';
 export const CUSTOM_SNAPSHOT_NAME_SEPARATOR = '-';
 export const TRANSLATE_PREFIX = 'nasha_dashboard_partition_snapshots';
