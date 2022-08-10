253f468d8cb988f7dedf058708ed21eb3a994830
brofik <10513484+brofik@users.noreply.github.com>
2022-08-10 09:35:18 +0200

    feat(pci): remove Containers tab from Kubernetes details view (#7491)
    ref: MANAGER-8796
    
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/pci/src/projects/project/kubernetes/details/containers/constants.js
+++
@@ -1,9 +0,0 @@
-export const DASHBOARD_URL = {
-  DEFAULT:
-    'https://labs.ovh.com/kubernetes-k8s/documentation/dashboard-installation',
-  US: 'https://support.us.ovhcloud.com/hc/en-us/articles/1500004918182',
-};
-
-export default {
-  DASHBOARD_URL,
-};
