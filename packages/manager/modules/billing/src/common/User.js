3f45f423e995038f0e12d9127d083bab6e0e8ca4
Anoop N <anooparveti@gmail.com>
2022-08-10 13:20:34 +0530

    refactor(billing): removal of deprecated API usage (#7729)
     refactor(billing): removal of deprecated API usage
    
     Remove /me/availableAutomaticPaymentMeans API usage in Manager
     bump ovh-api-services from 15.0.0 to 16.0.0
     ref: MANAGER-9639
    
    Signed-off-by: Anoop <anooparveti@gmail.com>

--- packages/manager/modules/billing/src/common/User.js
+++ packages/manager/modules/billing/src/common/User.js
@@ -24,27 +24,6 @@ export default /* @ngInject */ function BillingUserService(
       (user) => ['CA', 'QC', 'WE', 'WS'].indexOf(user.ovhSubsidiary) === -1,
     );
 
-  this.getAvailableMeans = () =>
-    OvhHttp.get('/me/availableAutomaticPaymentMeans', {
-      rootPath: 'apiv6',
-    }).then((result) => {
-      const means = [];
-
-      /* Actually, the means is a simple list: ["mean1", "mean2", …]
-       * but this "api end point" retrun an object: {"mean1": true, "mean2": true, …}
-       * for compatibility, keep the simple list presentation */
-      if (result && result && angular.isObject(result)) {
-        /* eslint-disable no-restricted-syntax */
-        for (const mean in result) {
-          if (result[mean]) {
-            means.push(mean);
-          }
-        }
-        /* eslint-enable no-restricted-syntax */
-      }
-      return means;
-    });
-
   this.addCreditCode = (inputCode) =>
     OvhHttp.post('/me/credit/code', {
       rootPath: 'apiv6',
