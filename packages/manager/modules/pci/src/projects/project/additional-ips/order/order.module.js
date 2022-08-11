83899abcd8107c7b29b2a77aa6cc2456359398a7
Anoop N <anooparveti@gmail.com>
2022-08-11 13:44:47 +0530

    feat(pci.project): unify summary step behavior (#7758)
    Unification of Summary step in Additional IP config page
     ref: MANAGER-9800
    
    Signed-off-by: Anoop <anooparveti@gmail.com>
    Signed-off-by: Anoop <anoop.n@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/additional-ips/order/order.module.js
+++ packages/manager/modules/pci/src/projects/project/additional-ips/order/order.module.js
@@ -3,13 +3,20 @@ import 'angular-translate';
 import '@uirouter/angularjs';
 import '@ovh-ux/manager-core';
 
+import gatewayServiceModule from '../../public-gateways/service';
+
 import routing from './order.routing';
 import component from './order.component';
 
 const moduleName = 'ovhManagerPciProjectAdditionalIpsOrder';
 
 angular
-  .module(moduleName, ['ui.router', 'pascalprecht.translate', 'ovhManagerCore'])
+  .module(moduleName, [
+    'ui.router',
+    'pascalprecht.translate',
+    'ovhManagerCore',
+    gatewayServiceModule,
+  ])
   .component('pciProjectAdditionalIpsOrder', component)
   .config(routing)
   .run(/* @ngTranslationsInject:json ./translations */);
