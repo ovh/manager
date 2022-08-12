52fb9f2e62bcaf457ba8a5cd34a1e8b212782c25
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2022-08-12 16:20:07 +0200

    fix(dedicated.server): clean code cisco asa
    ref: MANAGER-9865
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/dedicated/server/details/index.js
+++ packages/manager/apps/dedicated/client/app/dedicated/server/details/index.js
@@ -49,21 +49,6 @@ angular
   .service('Server', service)
   .service('BandwidthVrackOrderService', bandwidthVrackOrderService)
   .service('DedicatedServerFeatureAvailability', featureAvailability)
-  .constant('FIREWALL_RULE_ACTIONS', {
-    ALLOW: 'PERMIT',
-    DENY: 'DENY',
-  })
-  .constant('FIREWALL_RULE_PROTOCOLS', {
-    IPV_4: 'IPv4',
-    UDP: 'UDP',
-    TCP: 'TCP',
-    ICMP: 'ICMP',
-  })
-  .constant('FIREWALL_STATUSES', {
-    ACTIVATED: 'ACTIVATED',
-    DEACTIVATED: 'DEACTIVATED',
-    NOT_CONFIGURED: 'NOT_CONFIGURED',
-  })
   .constant('MITIGATION_STATUSES', {
     ACTIVATED: 'ACTIVATED',
     AUTO: 'AUTO',
