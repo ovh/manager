9b18848b39ce91970913e88f80b526ad635e3362
vks-ovh <102142952+vks-ovh@users.noreply.github.com>
2022-08-17 10:03:35 +0530

    fix(pci.project): add, search and filter is missing (#7798)
    ref: MANAGER-9755
      Using datagrid instead of custom table to show add, search and filter button
    
    Signed-off-by: vikash singh <vikash.singh@corp.ovh.com>

--- packages/manager/modules/pci/src/projects/project/private-networks/private-networks.controller.js
+++ packages/manager/modules/pci/src/projects/project/private-networks/private-networks.controller.js
@@ -1,34 +1,24 @@
-import get from 'lodash/get';
-import set from 'lodash/set';
 import { getCriteria } from '../project.utils';
-import { PRIVATE_NETWORK_LIST, PAGINATION } from './private-networks.constants';
+import { PRIVATE_NETWORK_LIST } from './private-networks.constants';
 
 const CONTAINER_NAME = 'pci.projects.project.privateNetwork';
 
 export default class {
   /* @ngInject */
-  constructor(
-    $translate,
-    CucCloudMessage,
-    OvhApiCloudProjectNetworkPrivateSubnet,
-    PciPrivateNetworks,
-  ) {
+  constructor($translate, CucCloudMessage) {
     this.$translate = $translate;
     this.CucCloudMessage = CucCloudMessage;
-    this.OvhApiCloudProjectNetworkPrivateSubnet = OvhApiCloudProjectNetworkPrivateSubnet;
-    this.PciPrivateNetworks = PciPrivateNetworks;
     this.PRIVATE_NETWORK_LIST = PRIVATE_NETWORK_LIST;
-    this.offset = PAGINATION.OFFSET;
-    this.pageSize = PAGINATION.PAGESIZE;
   }
 
   $onInit() {
     this.loadMessages();
-    this.isSubnetsLoading = false;
-    this.isRowExpanded = {};
     this.criteria = getCriteria('id', this.networkId);
-    this.originalList = [...this.privateNetworks];
-    this.paginate(this.pageSize, this.offset);
+  }
+
+  onAddPrivateNetworkClick() {
+    this.trackClick('PCI_PROJECTS_NETWORK_ADD');
+    return this.createNetwork();
   }
 
   loadMessages() {
@@ -40,56 +30,5 @@ export default class {
 
   refreshMessages() {
     this.messages = this.messageHandler.getMessages();
-  }
-
-  expandRow($row) {
-    this.isRowExpanded[$row.id] = !this.isRowExpanded[$row.id];
-    if (this.isRowExpanded[$row.id] && !$row.subnets) {
-      return this.getSubnets($row);
-    }
-    return null;
-  }
-
-  getSubnets($row) {
-    this.isSubnetsLoading = true;
-    return this.PciPrivateNetworks.getSubnets(this.projectId, $row.id)
-      .then(({ data }) =>
-        set(
-          $row,
-          'subnets',
-          data.map((subnet) => ({
-            ...subnet,
-            allocatedIp: subnet.ipPools
-              .map((ipPool) => `${ipPool.start} - ${ipPool.end}`)
-              .join(' ,'),
-            dhcp: subnet.ipPools
-              .map((ipPool) =>
-                ipPool.dhcp === true
-                  ? this.$translate.instant(
-                      'pci_projects_project_network_private_dhcp_active',
-                    )
-                  : this.$translate.instant(
-                      'pci_projects_project_network_private_dhcp_suspended',
-                    ),
-              )
-              .join(),
-          })),
-        ),
-      )
-      .catch((error) => this.CucCloudMessage.error(get(error, 'data.message')))
-      .finally(() => {
-        this.isSubnetsLoading = false;
-      });
-  }
-
-  onChange({ offset, pageSize }) {
-    this.paginate(pageSize, offset);
-  }
-
-  paginate(pageSize, offset) {
-    this.privateNetworks = this.originalList.slice(
-      offset - 1,
-      offset + pageSize - 1,
-    );
   }
 }
