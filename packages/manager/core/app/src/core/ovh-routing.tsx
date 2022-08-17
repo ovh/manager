d1c4e06d1d383203996f6eb3f97f399d73591c85
Florian Renaut <florian.renaut@corp.ovh.com>
2022-08-17 15:14:10 +0200

    fix(react-app): routing
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/core/app/src/core/ovh-routing.tsx
+++ packages/manager/core/app/src/core/ovh-routing.tsx
@@ -6,6 +6,9 @@ export default function OvhContainerRoutingSync(): JSX.Element {
   const location = useLocation();
   const shell = useShell();
   useEffect(() => {
+    shell.routing.stopListenForHashChange();
+  }, []);
+  useEffect(() => {
     shell.routing.onHashChange();
   }, [location]);
   return undefined;
