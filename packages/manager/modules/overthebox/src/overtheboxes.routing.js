e3ab9feaf602a23d1135f8c08e6678b3da61fe82
Florian Renaut <florian.renaut@corp.ovh.com>
2022-08-24 15:17:35 +0200

    feat(overthebox): add order cta to listing
    ref: MANAGER-9311
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/modules/overthebox/src/overtheboxes.routing.js
+++ packages/manager/modules/overthebox/src/overtheboxes.routing.js
@@ -24,6 +24,9 @@ export default /* @ngInject */ ($stateProvider) => {
         $state.go('overTheBoxes.overTheBox.details', {
           serviceName,
         }),
+      gotoOrder: /* @ngInject */ ($window) => () => {
+        $window.open('https://www.ovhtelecom.fr/overthebox/', '_blank');
+      },
       hideBreadcrumb: () => true,
     },
     redirectTo: (transition) =>
