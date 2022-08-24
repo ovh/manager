a709d8ca7009aafa78e17213e7ef850daaa2b709
Florian Renaut <florian.renaut@corp.ovh.com>
2022-08-24 11:48:09 +0200

    feat(load-balancer): add order cta to listing
    ref: MANAGER-9311
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

---
+++ packages/manager/modules/pci/src/projects/project/load-balancer/load-balancer.order.js
@@ -0,0 +1,18 @@
+export const LOAD_BALANCER_ORDER_URLS = {
+  DEFAULT: 'https://www.ovh.com/world/solutions/load-balancer/',
+  DE: 'https://www.ovh.de/loesungen/load-balancer/',
+  IT: 'https://www.ovh.it/soluzioni/load-balancer/',
+  NL: 'https://www.ovh.nl/oplossing/load-balancer/',
+  PL: 'https://www.ovh.pl/rozwiazania/load-balancer/',
+  PT: 'https://www.ovh.pt/solucoes/load-balancer/',
+  FR: 'https://www.ovh.com/fr/solutions/load-balancer/',
+  WS: 'https://www.ovh.com/world/es/soluciones/load-balancer/',
+};
+
+export function getLoadBalancerOrderUrl(subsidiary) {
+  return (
+    LOAD_BALANCER_ORDER_URLS[subsidiary] || LOAD_BALANCER_ORDER_URLS.DEFAULT
+  );
+}
+
+export default LOAD_BALANCER_ORDER_URLS;
