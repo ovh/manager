82ff72a47120f34c479499486ed1147582dbef45
Florian Renaut <florian.renaut@corp.ovh.com>
2022-08-24 14:46:23 +0200

    feat(office): add order cta to listing
    ref: MANAGER-9311
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/modules/office/src/office.routing.js
+++ packages/manager/modules/office/src/office.routing.js
@@ -1,4 +1,5 @@
 import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
+import { getOfficeOrderUrl } from './office.order';
 
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('office.index', {
@@ -20,6 +21,21 @@ export default /* @ngInject */ ($stateProvider) => {
       header: /* @ngInject */ ($translate) =>
         $translate.instant('office_title'),
       customizableColumns: () => true,
+      topbarOptions: /* @ngInject */ ($translate, $window, coreConfig) => ({
+        cta: {
+          type: 'button',
+          displayed: true,
+          disabled: false,
+          label: $translate.instant('office_order'),
+          value: $translate.instant('office_order'),
+          onClick: () => {
+            $window.open(
+              getOfficeOrderUrl(coreConfig.getUser().ovhSubsidiary),
+              '_blank',
+            );
+          },
+        },
+      }),
       hideBreadcrumb: () => true,
       getServiceNameLink: /* @ngInject */ ($state) => ({ serviceName }) =>
         $state.href('office.product', {
