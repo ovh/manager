b36aa3da56789d733cb82f0a3aa0b023117956bd
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2022-08-24 09:35:04 +0200

    fix(billing-components): add NAS-HA to commitment
    ref: MANAGER-9835, MANAGER-9846
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/modules/billing-components/src/components/commitment/commitment.constants.js
+++ packages/manager/modules/billing-components/src/components/commitment/commitment.constants.js
@@ -3,6 +3,7 @@ export const CATALOG_MAPPING = {
   '/vps/{serviceName}': 'vps',
   '/license/cpanel/{serviceName}': 'licensecPanel',
   '/storage/netapp/{serviceName}': 'netapp',
+  '/dedicated/nasha/{serviceName}': 'nasha',
 };
 
 export default {
