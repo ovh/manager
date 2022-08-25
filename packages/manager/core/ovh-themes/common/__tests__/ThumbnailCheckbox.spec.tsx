7956239071a2c4c6c6a4658f1863e39c07efd48a
Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
2022-08-25 10:23:07 +0200

    feat(checkbox): add thumbnail checkbox
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

--- packages/manager/core/ovh-themes/common/__tests__/ThumbnailCheckbox.spec.tsx
+++ packages/manager/core/ovh-themes/common/__tests__/ThumbnailCheckbox.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import { axe, toHaveNoViolations } from 'jest-axe';
 
 import { ThumbnailCheckbox } from '../components';
