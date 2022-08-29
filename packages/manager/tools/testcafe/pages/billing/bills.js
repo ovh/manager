086436e83f9ce5b7e057e65e37f6050faea84913
Jérémy DE CESARE <jeremy.de-cesare@ovhcloud.com>
2022-08-29 11:46:33 +0200

    refactor(tools): remove testcafe
    ref: MANAGER-9957
    Signed-off-by: Jérémy DE CESARE <jeremy.de-cesare@ovhcloud.com>

--- packages/manager/tools/testcafe/pages/billing/bills.js
+++
@@ -1,7 +0,0 @@
-import ManagerParentPage from '../common/managerParent';
-
-export default class BillsList extends ManagerParentPage {
-  constructor() {
-    super({ currentPageNameInUrl: 'billing/history' });
-  }
-}
--- packages/manager/tools/testcafe/pages/components/navbar.js
+++
@@ -1,9 +0,0 @@
-import { Selector } from 'testcafe';
-
-class Navbar {
-  constructor() {
-    this.navbar = Selector('.oui-navbar');
-  }
-}
-
-export default new Navbar();
