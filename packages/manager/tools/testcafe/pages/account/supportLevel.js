681fa5ad176f1e24aeb6a83d96df97167ee11436
Jérémy DE CESARE <jeremy.de-cesare@ovhcloud.com>
2022-08-29 11:46:33 +0200

    refactor(tools): remove testcafe
    ref: MANAGER-9957
    Signed-off-by: Jérémy DE CESARE <jeremy.de-cesare@ovhcloud.com>

--- packages/manager/tools/testcafe/pages/account/supportLevel.js
+++
@@ -1,7 +0,0 @@
-import ManagerParentPage from '../common/managerParent';
-
-export default class SupportLevel extends ManagerParentPage {
-  constructor() {
-    super({ currentPageNameInUrl: 'useraccount/support/level' });
-  }
-}
--- packages/manager/tools/testcafe/pages/billing/orders.js
+++
@@ -1,7 +0,0 @@
-import ManagerParentPage from '../common/managerParent';
-
-export default class OrdersList extends ManagerParentPage {
-  constructor() {
-    super({ currentPageNameInUrl: 'billing/orders' });
-  }
-}
