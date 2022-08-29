e03788b0bc491950e8e30f9148fb01076f3b6bbd
Jérémy DE CESARE <jeremy.de-cesare@ovhcloud.com>
2022-08-29 11:46:33 +0200

    refactor(tools): remove testcafe
    ref: MANAGER-9957
    Signed-off-by: Jérémy DE CESARE <jeremy.de-cesare@ovhcloud.com>

--- packages/manager/tools/testcafe/pages/hub/dedicatedServerDashboard.js
+++
@@ -1,9 +0,0 @@
-import { Selector } from 'testcafe';
-import ManagerParentPage from '../common/managerParent';
-
-export default class ServersListDashboard extends ManagerParentPage {
-  constructor() {
-    super({ currentPageNameInUrl: '/#/dedicated_server' });
-    this.tableListItems = Selector('[data-navi-id="DEDICATED_SERVER-list"]');
-  }
-}
