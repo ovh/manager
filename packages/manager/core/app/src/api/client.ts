1337fc7b0bb6055211ef506f8cd98cc2c0c6e934
Florian Renaut <florian.renaut@corp.ovh.com>
2022-08-29 13:43:24 +0200

    fix(react-app): refactor apiClient
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/core/app/src/api/client.ts
+++ packages/manager/core/app/src/api/client.ts
@@ -1,40 +1,20 @@
 import axios from 'axios';
 
-export enum RequestTypesEnum {
-  APIV6 = 'apiv6',
-  AAPI = 'aapi',
-  WS = 'ws',
-  NONE = 'none',
-}
+const defaultAxiosConfig = {};
 
-const urlPrefixes = {
-  apiv6: '/engine/apiv6',
-  aapi: '/engine/2api',
-  ws: '/engine/ws',
-  none: '',
-};
-
-const getUrlPrefix = (config: any) => {
-  // get the url prefix of the http request if defined
-  if (config.urlPrefix) {
-    return config.urlPrefix;
-  }
-
-  // get the prefix from request request type
-  if (config.requestType) {
-    return urlPrefixes[config.requestType as RequestTypesEnum];
-  }
-
-  return urlPrefixes[RequestTypesEnum.APIV6];
-};
-
-const apiClient = axios.create({});
-
-apiClient.interceptors.request.use((config) => {
-  return {
-    ...config,
-    url: `${getUrlPrefix(config)}${config.url}`,
-  };
+export const v6 = axios.create({
+  ...defaultAxiosConfig,
+  baseURL: '/engine/apiv6',
 });
 
-export default apiClient;
+export const aapi = axios.create({
+  ...defaultAxiosConfig,
+  baseURL: '/engine/2api',
+});
+
+export const ws = axios.create({
+  ...defaultAxiosConfig,
+  baseURL: '/engine/ws',
+});
+
+export default { v6, aapi, ws };
