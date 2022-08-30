3ea6f8995dbdf46496094e6f6fa6b420dadfb591
Florian Renaut <florian.renaut@corp.ovh.com>
2022-08-30 16:00:22 +0200

    fix(react-app): node listing page
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/core/app/src/pages/NodesListing.tsx
+++ packages/manager/core/app/src/pages/NodesListing.tsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, Link as RouterLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { Badge, Link, Skeleton } from '@chakra-ui/react';
 import { applyFilters, FilterCategories } from '@/api/filters';
 import {
   fetchNutanixNodeMetaInfos,
   listNutanixNodes,
   NutanixNode,
+  NutanixNodeMetaInfos,
 } from '@/api/nutanix';
 
 import Listing, { ListingColumn } from '@/components/Listing';
 import DatacenterLabel from '@/components/DatacenterLabel';
-
 import useListingSearchParams from '@/hooks/useListingSearchParams';
 
 const getStatusBadgeVariant = (status: string) => {
@@ -29,12 +29,16 @@ const getStatusBadgeVariant = (status: string) => {
   }
 };
 
+type ListingNode = NutanixNode & Partial<NutanixNodeMetaInfos>;
+
 export default function NodesListingPage(): JSX.Element {
   const { t } = useTranslation('nutanix');
+  const queryClient = useQueryClient();
   const { serviceId } = useParams();
   const searchParams = useListingSearchParams();
-  const [services, setServices] = useState<NutanixNode[]>([]);
-  const [columns, setColumns] = useState<ListingColumn<NutanixNode>[]>([
+  const [services, setServices] = useState<ListingNode[]>([]);
+  const [filteredServices, setFilteredServices] = useState<ListingNode[]>(null);
+  const [columns, setColumns] = useState<ListingColumn<ListingNode>[]>([
     {
       key: 'displayName',
       label: t('node_name'),
@@ -43,47 +47,35 @@ export default function NodesListingPage(): JSX.Element {
       renderer: ({ item: server }) => (
         <Link
           as={RouterLink}
-          to={`/nutanix/${serviceId}/nodes/${server.serviceName}`}
+          to={`/nutanix/${serviceId}/details/nodes/${server.serviceName}`}
         >
           {server.displayName}
         </Link>
       ),
       search: true,
-      sortable: true,
     },
     {
-      key: 'server_type',
+      key: 'commercialRange',
       label: t('server_type'),
       filterable: FilterCategories.String,
-      hidden: searchParams.isColumnHidden('server_type'),
+      hidden: searchParams.isColumnHidden('commercialRange'),
       renderer: ({ item: server }) => {
-        const query = useQuery(
-          ['nutanix_node_metaInfos', server.serviceName],
-          () => fetchNutanixNodeMetaInfos(server.serviceName),
-          { staleTime: 60 * 1000 },
-        );
-        if (query.isLoading) {
+        if (!server.commercialRange) {
           return <Skeleton height="0.5rem" />;
         }
-        return <>{query.data.commercialRange}</>;
+        return <>{server.commercialRange}</>;
       },
-      sortable: true,
     },
     {
-      key: 'status',
+      key: 'stateLabel',
       label: t('status'),
       filterable: FilterCategories.String,
-      hidden: searchParams.isColumnHidden('status'),
+      hidden: searchParams.isColumnHidden('stateLabel'),
       renderer: ({ item: server }) => {
-        const query = useQuery(
-          ['nutanix_node_metaInfos', server.serviceName],
-          () => fetchNutanixNodeMetaInfos(server.serviceName),
-          { staleTime: 60 * 1000 },
-        );
-        if (query.isLoading) {
+        if (!server.state) {
           return <Skeleton height="0.5rem" />;
         }
-        const { state } = query.data;
+        const { state } = server;
         return (
           <Badge variant={getStatusBadgeVariant(state)}>
             {t(`node_status_${state.toLowerCase()}`)}
@@ -92,40 +84,78 @@ export default function NodesListingPage(): JSX.Element {
       },
     },
     {
-      key: 'location',
+      key: 'datacenter',
       label: t('location'),
-      filterable: FilterCategories.String,
-      hidden: searchParams.isColumnHidden('location'),
+      hidden: searchParams.isColumnHidden('datacenter'),
       renderer: ({ item: server }) => {
-        const query = useQuery(
-          ['nutanix_node_metaInfos', server.serviceName],
-          () => fetchNutanixNodeMetaInfos(server.serviceName),
-          { staleTime: 60 * 1000 },
-        );
-        if (query.isLoading) {
+        if (!server.datacenter) {
           return <Skeleton height="0.5rem" />;
         }
-        return <DatacenterLabel datacenterId={query.data.datacenter} />;
+        return <DatacenterLabel datacenterId={server.datacenter} />;
       },
     },
   ]);
   const [state, setState] = useState(searchParams.getInitialState(columns));
 
+  const { data: nutanixNodes, isLoading: isLoadingNodes } = useQuery(
+    ['nutanix_nodes', serviceId],
+    () => listNutanixNodes(serviceId),
+    { staleTime: 5 * 60 * 1000 },
+  );
+
   useEffect(() => {
-    setServices(null);
-    searchParams.updateState(state);
-    listNutanixNodes(serviceId).then((data) => {
-      setServices(applyFilters(data, state.filters));
+    if (!isLoadingNodes) {
+      setServices(nutanixNodes);
+    }
+  }, [nutanixNodes, isLoadingNodes]);
+
+  // fetch services meta data
+  useEffect(() => {
+    services.forEach(({ serviceName }) => {
+      queryClient
+        .fetchQuery(
+          ['nutanix_node_infos', serviceName],
+          () => fetchNutanixNodeMetaInfos(serviceName),
+          { staleTime: 5 * 60 * 1000 },
+        )
+        .then((infos) => {
+          setServices((oldServices) =>
+            oldServices.map((service) => {
+              if (service.serviceName === serviceName)
+                return {
+                  ...service,
+                  commercialRange: infos.commercialRange,
+                  state: infos.state,
+                  stateLabel: t(`node_status_${infos.state.toLowerCase()}`),
+                  datacenter: infos.datacenter,
+                };
+              return service;
+            }),
+          );
+        });
     });
+  }, [JSON.stringify(services)]);
+
+  // filter services when service list changes
+  useEffect(() => {
+    setFilteredServices(applyFilters(services, state.filters));
+  }, [services]);
+
+  // filter services when filters & sorting changs
+  useEffect(() => {
+    searchParams.updateState(state);
+    setFilteredServices(applyFilters(services, state.filters));
   }, [JSON.stringify(state)]);
 
   return (
     <Listing
       columns={columns}
-      data={{
-        items: services,
-        total: services?.length || 0,
-      }}
+      data={
+        !isLoadingNodes && {
+          items: filteredServices,
+          total: filteredServices?.length || 0,
+        }
+      }
       state={state}
       onChange={setState}
       onColumnsChange={(cols) => {
