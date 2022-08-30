bcc75b9a610da0dc88149b99e2c3483added3e54
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2022-08-30 09:36:39 +0200

    refactor(dev-server-config): remove registry proxy
    ref: MANAGER-9972
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
    
    BREAKING CHANGE: remove registry proxy configuration

--- packages/manager/tools/dev-server-config/src/proxy/index.js
+++ packages/manager/tools/dev-server-config/src/proxy/index.js
@@ -1,11 +1,9 @@
 const aapi = require('./aapi');
 const dev = require('./dev');
 const v6 = require('./v6');
-const registry = require('./registry');
 
 module.exports = {
   aapi,
   dev,
   v6,
-  registry,
 };
