eb827577bcad13ea239d1ed4a3d9421efb986b71
mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>
2022-09-08 19:21:47 +0200

    feat(dedicated.server.monitoring): update monitoring modal
    ref: MANAGER-9918
    
    Signed-off-by: mohammed-zahaf <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/dedicated/server/monitoring/update/dedicated-server-monitoring-update.controller.js
+++ packages/manager/apps/dedicated/client/app/dedicated/server/monitoring/update/dedicated-server-monitoring-update.controller.js
@@ -14,11 +14,24 @@ angular.module('App').controller(
       this.server = null;
       this.isLoading = false;
       this.isUpdating = false;
+      this.monitoringOptions = [
+        { status: 'disabled', settings: { monitoring: false } },
+        {
+          status: 'proactive',
+          settings: { monitoring: true, noIntervention: false },
+        },
+        {
+          status: 'no-proactive',
+          settings: { monitoring: true, noIntervention: true },
+        },
+      ];
 
       this.isLoading = true;
+      console.log('ZM:: onInit', this);
       return this.Server.getSelected(this.$stateParams.productId)
         .then((server) => {
           this.server = server;
+          this.preselectOption();
         })
         .catch((err) => {
           this.$q.reject(err);
@@ -29,67 +42,66 @@ angular.module('App').controller(
         });
     }
 
+    getMonitoringStatus() {
+      const { monitored, noIntervention } = this.server;
+      let monitoringStatus = 'disabled';
+
+      // proactive intervention
+      if (monitored && !noIntervention) {
+        monitoringStatus = 'proactive';
+      }
+
+      // no proactive intervention
+      if (monitored && noIntervention) {
+        monitoringStatus = 'no-proactive';
+      }
+
+      return monitoringStatus;
+    }
+
+    preselectOption() {
+      this.selectedOption = this.monitoringOptions.find(({ status }) => {
+        return status === this.getMonitoringStatus();
+      });
+    }
+
     /**
      * Update monitoring.
      * @return {Promise}
      */
     update() {
-      const { monitored } = this.server;
+      const { monitoring, noIntervention } = this.selectedOption.settings;
+
       this.isUpdating = true;
       return this.Server.updateMonitoring(
         this.$stateParams.productId,
-        !monitored,
-        !monitored, // reset no-intervention to monitored value
+        monitoring,
+        noIntervention, // reset no-intervention to monitored value
       )
         .then(() => {
-          if (monitored === true) {
-            this.Alerter.success(
-              this.$translate.instant(
-                'server_configuration_monitoring_deactivate_success',
-                {
-                  t0: this.server.name,
-                },
-              ),
-              'server_dashboard_alert',
-            );
-          } else {
-            this.Alerter.success(
-              this.$translate.instant(
-                'server_configuration_monitoring_activate_success',
-                {
-                  t0: this.server.name,
-                },
-              ),
-              'server_dashboard_alert',
-            );
-          }
+          const monitoringOption =
+            !monitoring || noIntervention ? 'no-proactive' : 'proactive';
+
+          this.Alerter.success(
+            this.$translate.instant(
+              `server_configuration_monitoring_update_modal_update_${monitoringOption}_request_success`,
+              {
+                serverName: this.server.name,
+              },
+            ),
+            'server_dashboard_alert',
+          );
+
           return this.close();
         })
         .catch((err) => {
-          if (monitored === true) {
-            this.Alerter.error(
-              this.$translate.instant(
-                'server_configuration_monitoring_deactivate_failed',
-                {
-                  t0: this.server.name,
-                },
-              ),
-              err,
-              'server_dashboard_alert',
-            );
-          } else {
-            this.Alerter.error(
-              this.$translate.instant(
-                'server_configuration_monitoring_activate_failed',
-                {
-                  t0: this.server.name,
-                },
-              ),
-              err,
-              'server_dashboard_alert',
-            );
-          }
-          this.$q.reject(err);
+          this.Alerter.error(
+            this.$translate.instant(
+              'server_configuration_monitoring_update_modal_update_request_failed',
+            ),
+            err,
+            'server_dashboard_alert',
+          );
           return this.cancel(err);
         })
         .finally(() => {
