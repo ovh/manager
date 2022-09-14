a8f526f0720c5400b1d2e92d94d62a2bbd6a567c
vks-ovh <102142952+vks-ovh@users.noreply.github.com>
2022-09-14 14:14:30 +0530

    feat(pci.project): instane config page update (#7787)
    ref: MANAGER-9389
    Modification of instance config page
    Signed-off-by: vikash singh <vikash.singh@corp.ovh.com>
    Signed-off-by: Anoop <anoop.n@ovhcloud.com>

--- packages/manager/modules/pci/src/components/project/flavor-billing/flavor-billing.controller.js
+++ packages/manager/modules/pci/src/components/project/flavor-billing/flavor-billing.controller.js
@@ -7,8 +7,13 @@ export default class FlavorBillingController {
   $onInit() {
     this.monthly = this.monthlyBilling === true;
     this.number = this.number || 1;
-
     this.disabled = this.disabled || false;
+    this.defaultGateway = this.addons?.find((addon) =>
+      addon.product?.startsWith('publiccloud-gateway'),
+    );
+    this.defaultFloatingIP = this.addons?.find((addon) =>
+      addon.product?.startsWith('publiccloud-floatingip'),
+    );
   }
 
   set flavor(flavor) {
@@ -30,10 +35,23 @@ export default class FlavorBillingController {
   }
 
   getPrice(price) {
-    return this.number * price.value;
+    return this.number * (price.value || price);
   }
 
   formatPrice(price) {
     return this.PriceFormatter.format(this.getPrice(price));
+  }
+
+  formatAddonsPrice = (price = 0) => price / 100000000;
+
+  getAddOnPrice() {
+    if (this.addons) {
+      return (
+        this.prices.monthly.value +
+        this.formatAddonsPrice(this.defaultGateway?.pricings[0]?.price) +
+        this.formatAddonsPrice(this.defaultFloatingIP?.pricings[0]?.price)
+      );
+    }
+    return null;
   }
 }
