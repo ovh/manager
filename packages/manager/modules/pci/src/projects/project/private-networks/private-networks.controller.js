9d5e8cf3548d164561ede897aa63c2e6097d0bbe
vks-ovh <102142952+vks-ovh@users.noreply.github.com>
2022-09-21 12:55:00 +0530

    fix(pci.project): incorrect value for the gateway (#8009)
    ref: MANAGER-10019
    Addtion of gateway name with IP on private network list page.
    Signed-off-by: vikash singh <vikash.singh@corp.ovh.com>
    Signed-off-by: Anoop <anoop.n@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/private-networks/private-networks.controller.js
+++ packages/manager/modules/pci/src/projects/project/private-networks/private-networks.controller.js
@@ -5,15 +5,83 @@ const CONTAINER_NAME = 'pci.projects.project.privateNetwork';
 
 export default class {
   /* @ngInject */
-  constructor($translate, CucCloudMessage) {
+  constructor($translate, $q, CucCloudMessage, PciPrivateNetworks) {
     this.$translate = $translate;
+    this.$q = $q;
     this.CucCloudMessage = CucCloudMessage;
     this.PRIVATE_NETWORK_LIST = PRIVATE_NETWORK_LIST;
+    this.PciPrivateNetworks = PciPrivateNetworks;
   }
 
   $onInit() {
     this.loadMessages();
+    this.isLoading = false;
     this.criteria = getCriteria('id', this.networkId);
+    this.availableNetworks = null;
+    this.gatewaySubnetObj = null;
+    this.getNetworksSubnet();
+    this.buildGatewaySubnetObj();
+  }
+
+  getNetworksSubnet() {
+    this.isLoading = true;
+    return this.$q
+      .all(
+        this.privateNetworks.map((privateNetwork) =>
+          this.getSubnets(privateNetwork),
+        ),
+      )
+      .then((data) => {
+        this.isLoading = false;
+        this.availableNetworks = [...data];
+        return this.availableNetworks;
+      });
+  }
+
+  getSubnets(network) {
+    return this.PciPrivateNetworks.getSubnets(this.projectId, network.id)
+      .then((data) => {
+        return {
+          ...network,
+          subnet: [
+            ...data.map((subnet) => ({
+              ...subnet,
+              gatewayName: this.gatewaySubnetObj[subnet.id],
+              allocatedIp: subnet.ipPools
+                .map((ipPool) => `${ipPool.start} - ${ipPool.end}`)
+                .join(' ,'),
+              dhcp: subnet.ipPools
+                .map((ipPool) =>
+                  ipPool.dhcp === true
+                    ? this.$translate.instant(
+                        'pci_projects_project_network_private_dhcp_active',
+                      )
+                    : this.$translate.instant(
+                        'pci_projects_project_network_private_dhcp_suspended',
+                      ),
+                )
+                .join(),
+            })),
+          ],
+        };
+      })
+      .catch((error) => {
+        this.CucCloudMessage.error(
+          this.$translate.instant(
+            'pci_projects_project_network_private_error',
+            { message: error?.data?.message },
+          ),
+        );
+      });
+  }
+
+  buildGatewaySubnetObj() {
+    this.gatewaySubnetObj = this.gateways.resources.reduce((acc, item) => {
+      item.interfaces.forEach(({ subnetId }) => {
+        acc[subnetId] = item.name;
+      });
+      return acc;
+    }, {});
   }
 
   onAddPrivateNetworkClick() {
