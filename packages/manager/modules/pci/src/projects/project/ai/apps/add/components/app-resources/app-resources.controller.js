bd277f70a4484987962375360f0884c506affd2d
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2022-09-22 17:07:38 +0200

    feat(pci.ai.apps): datatr-80 - add scaling option (#8001)
    * feat: add scaling option
    
    DATATR-80
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/ai/apps/add/components/app-resources/app-resources.controller.js
+++ packages/manager/modules/pci/src/projects/project/ai/apps/add/components/app-resources/app-resources.controller.js
@@ -1,7 +1,13 @@
 import { map, uniq } from 'lodash';
 
+import {
+  AUTOMATIC_SCALING_RESOURCE_TYPES,
+  APP_SCALING_SETTINGS,
+} from '../../add.constants';
+
+import { APP_SCALING_INFO } from '../../../app.constants';
+
 const RESOURCES_SCALE_MIN_FLAVOR = 1;
-const RESOURCES_REPLICAS_MAX_FLAVOR = 10;
 
 export default class AppResourcesController {
   /* @ngInject */
@@ -14,7 +20,10 @@ export default class AppResourcesController {
     this.selectDefaultFlavor(this.appModel.resource.flavorType);
     this.flavorTypes = uniq(map(this.flavors, 'type'));
     this.RESOURCES_SCALE_MIN_FLAVOR = RESOURCES_SCALE_MIN_FLAVOR;
-    this.RESOURCES_REPLICAS_MAX_FLAVOR = RESOURCES_REPLICAS_MAX_FLAVOR;
+    this.AUTOMATIC_SCALING_RESOURCE_TYPES = Object.values(
+      AUTOMATIC_SCALING_RESOURCE_TYPES,
+    );
+    this.APP_SCALING_SETTINGS = APP_SCALING_SETTINGS;
   }
 
   filterType() {
@@ -59,5 +68,33 @@ export default class AppResourcesController {
     this.appModel.resource.flavor = null;
     this.appModel.resource.flavorType = flavorType;
     this.selectDefaultFlavor(flavorType);
+  }
+
+  getScalingInfoLink() {
+    return (
+      APP_SCALING_INFO[this.user.ovhSubsidiary] || APP_SCALING_INFO.DEFAULT
+    );
+  }
+
+  computeTotalPrice(resourcePrice, partnerPrice) {
+    const replicas = this.appModel.scalingStrategy.autoscaling
+      ? this.appModel.scalingStrategy.automatic.replicasMin
+      : this.appModel.scalingStrategy.fixed.replicas;
+    return (
+      (resourcePrice + partnerPrice) *
+      this.appModel.resource.nbResources *
+      replicas
+    );
+  }
+
+  get price() {
+    return this.computeTotalPrice(
+      this.resourcePriceInUcents,
+      this.partnerPriceInUcents,
+    );
+  }
+
+  get tax() {
+    return this.computeTotalPrice(this.resourcePriceTax, this.partnerPriceTax);
   }
 }
