3a0b753d84c107cee1d4bd02a84fb11409ee3750
Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>
2022-09-23 11:39:27 +0200

    refactor(cold-archive): improvement
    ref: MANAGER-9184
    
    Signed-off-by: Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/cold-archive/index.js
+++ packages/manager/modules/pci/src/projects/project/storages/cold-archive/index.js
@@ -2,25 +2,21 @@ import angular from 'angular';
 import '@uirouter/angularjs';
 import 'oclazyload';
 
-import '@ovh-ux/manager-core';
+const moduleName = 'ovhManagerPciProjectsProjectStoragesColdArchiveLazyloading';
 
-const moduleName = 'ovhManagerPciStoragesColdArchiveLazyloading';
+angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
+  /* @ngInject */ ($stateProvider) => {
+    $stateProvider.state('pci.projects.project.storages.cold-archive.**', {
+      url: '/cold-archive',
+      lazyLoad: ($transition$) => {
+        const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
 
-angular
-  .module(moduleName, ['ui.router', 'ovhManagerCore', 'oc.lazyLoad'])
-  .config(
-    /* @ngInject */ ($stateProvider) => {
-      $stateProvider.state('pci.projects.project.storages.cold-archive.**', {
-        url: '/cold-archive',
-        lazyLoad: ($transition$) => {
-          const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
-
-          return import('./cold-archive.module').then((mod) =>
-            $ocLazyLoad.inject(mod.default || mod),
-          );
-        },
-      });
-    },
-  );
+        return import('./cold-archive.module').then((mod) =>
+          $ocLazyLoad.inject(mod.default || mod),
+        );
+      },
+    });
+  },
+);
 
 export default moduleName;
