63d3690c313276495f88e39e816f6ee7298d7875
Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>
2022-09-23 12:23:03 +0200

    feat(cold-archive.add): configuration page implementation
    ref: MANAGER-9184
    
    Signed-off-by: Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>

---
+++ packages/manager/modules/pci/src/projects/project/storages/cold-archive/add/add.constants.js
@@ -0,0 +1,25 @@
+export const CHECK_PRICES_DOC_LINK = {
+  DEFAULT: 'https://www.ovhcloud.com/en/public-cloud/prices/#',
+  DE: 'https://www.ovhcloud.com/de/public-cloud/prices/#',
+  AU: 'https://www.ovhcloud.com/en-au/public-cloud/prices/#',
+  CA: 'https://www.ovhcloud.com/en-ca/public-cloud/prices/#',
+  GB: 'https://www.ovhcloud.com/en-gb/public-cloud/prices/#',
+  IE: 'https://www.ovhcloud.com/en-ie/public-cloud/prices/#',
+  SG: 'https://www.ovhcloud.com/en-sq/public-cloud/prices/#',
+  ES: 'https://www.ovhcloud.com/es-es/public-cloud/prices/#',
+  QC: 'https://www.ovhcloud.com/fr-ca/public-cloud/prices/#',
+  FR: 'https://www.ovhcloud.com/fr/public-cloud/prices/#',
+  MA: 'https://www.ovhcloud.com/fr-ma/public-cloud/prices/#',
+  SN: 'https://www.ovhcloud.com/fr-sn/public-cloud/prices/#',
+  TN: 'https://www.ovhcloud.com/fr-tn/public-cloud/prices/#',
+  IT: 'https://www.ovhcloud.com/it/public-cloud/prices/#',
+  NL: 'https://www.ovhcloud.com/nl/public-cloud/prices/#',
+  PL: 'https://www.ovhcloud.com/pl/public-cloud/prices/#',
+  PT: 'https://www.ovhcloud.com/pt/public-cloud/prices/#',
+  WS: 'https://www.ovhcloud.com/es/public-cloud/prices/#',
+  WE: 'https://www.ovhcloud.com/en/public-cloud/prices/#',
+};
+
+export default {
+  CHECK_PRICES_DOC_LINK,
+};
