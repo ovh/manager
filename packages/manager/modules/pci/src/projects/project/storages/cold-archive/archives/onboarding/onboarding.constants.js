82dfdd72c2922d8dd6c8115dcaf33659475eef92
Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
2022-09-27 00:24:56 +0200

    feat(cold-archive.onboarding): implement the onboarding component
    ref: MANAGER-9181
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>

---
+++ packages/manager/modules/pci/src/projects/project/storages/cold-archive/archives/onboarding/onboarding.constants.js
@@ -0,0 +1,17 @@
+export const GUIDES = [
+  {
+    id: 'cold-archive-getting-started',
+    link:
+      'https://docs.ovh.com/fr/storage/s3/getting-started-with-cold-archive/',
+  },
+  {
+    id: 's3-getting-started',
+    link: 'https://docs.ovh.com/gb/en/storage/s3/getting-started-with-s3/',
+  },
+  {
+    id: 's3-limitaion',
+    link: 'https://docs.ovh.com/fr/storage/s3/limitations/',
+  },
+];
+
+export default { GUIDES };
