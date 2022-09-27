9d17284ab64d90ba9d3863a9a9d05b3fdf91eb46
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2022-09-27 12:03:49 +0200

    feat(pcc.user): add new active directory
    ref: MANAGER-10027
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

---
+++ packages/manager/apps/dedicated/client/app/dedicatedCloud/users/activeDirectories/add/federation-add.routes.js
@@ -0,0 +1,14 @@
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state('app.dedicatedCloud.details.users.federation-add', {
+    url: '/federation/add',
+    views: {
+      'dedicatedCloudView@app.dedicatedCloud.details':
+        'dedicatedCloudUsersFederationAdd',
+    },
+    resolve: {
+      user: /* @ngInject */ (currentUser) => currentUser,
+      breadcrumb: /* @ngInject */ ($translate) =>
+        $translate.instant('dedicatedCloud_USER_AD_ADD_title'),
+    },
+  });
+};
