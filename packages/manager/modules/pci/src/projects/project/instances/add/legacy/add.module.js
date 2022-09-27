ecb6e191ee5bd6b432e2e3f96225a6ee50239643
Anoop N <anoop.n@ovhcloud.com>
2022-09-27 14:40:53 +0530

    feat(pci.project): instance config US restriction (#8064)
    ref: MANAGER-10089
    
    Signed-off-by: Anoop <anoop.n@ovhcloud.com>

---
+++ packages/manager/modules/pci/src/projects/project/instances/add/legacy/add.module.js
@@ -0,0 +1,40 @@
+import angular from 'angular';
+import '@uirouter/angularjs';
+import '@ovh-ux/ng-translate-async-loader';
+import '@ovh-ux/ng-ovh-cloud-universe-components';
+import 'angular-translate';
+import 'ovh-api-services';
+import '@ovh-ux/manager-filters';
+
+import './add.less';
+
+import flavorBilling from '../../../../../components/project/flavor-billing';
+import flavorsList from '../../../../../components/project/flavors-list';
+import imagesList from '../../../../../components/project/images-list';
+import sshKeys from '../../../../../components/project/instance/ssh-keys';
+import trustedZoneBanner from '../../../../../components/trusted-zone-banner';
+
+import component from './add.component';
+import routing from './add.routing';
+
+const moduleName = 'ovhManagerPciInstancesAdd';
+
+angular
+  .module(moduleName, [
+    'ovhManagerFilters',
+    'ngTranslateAsyncLoader',
+    'ngOvhCloudUniverseComponents',
+    'pascalprecht.translate',
+    'ovh-api-services',
+    'ui.router',
+    flavorBilling,
+    flavorsList,
+    imagesList,
+    sshKeys,
+    trustedZoneBanner,
+  ])
+  .config(routing)
+  .component('ovhManagerPciInstancesAdd', component)
+  .run(/* @ngTranslationsInject:json ./translations */);
+
+export default moduleName;
