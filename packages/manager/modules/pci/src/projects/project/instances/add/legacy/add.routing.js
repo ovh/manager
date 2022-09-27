7731b97af8c3ff5db1ee9ee119c490587a74cbc3
Anoop N <anoop.n@ovhcloud.com>
2022-09-27 14:40:53 +0530

    feat(pci.project): instance config US restriction (#8064)
    ref: MANAGER-10089
    
    Signed-off-by: Anoop <anoop.n@ovhcloud.com>

---
+++ packages/manager/modules/pci/src/projects/project/instances/add/legacy/add.routing.js
@@ -0,0 +1,90 @@
+import find from 'lodash/find';
+import { EXCLUDE_FLAVOR_CATEGORIES } from './add.constants';
+import { FLAVORS_FEATURES_FLIPPING_MAP } from '../../instances.constants';
+
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state('pci.projects.project.instances.add', {
+    url: '/new',
+    component: 'ovhManagerPciInstancesAdd',
+    resolve: {
+      breadcrumb: /* @ngInject */ ($translate) =>
+        $translate.instant('pci_projects_project_instances_add_title'),
+
+      offer: /* @ngInject */ ($q, deals, iceberg, projectId) =>
+        deals.active
+          ? iceberg('/cloud/project/:serviceName/credit')
+              .query()
+              .expand('CachedObjectList-Pages')
+              .execute({ serviceName: projectId })
+              .$promise.then(({ data }) =>
+                find(data, (voucher) =>
+                  deals.descriptionPattern.test(voucher.description),
+                ),
+              )
+              .then((vouchers) =>
+                deals.display.includes('instance-creation-price') && vouchers
+                  ? deals
+                  : null,
+              )
+              .catch(() => null)
+          : $q.when(null),
+
+      privateNetworks: /* @ngInject */ (
+        PciProjectsProjectInstanceService,
+        projectId,
+      ) => PciProjectsProjectInstanceService.getPrivateNetworks(projectId),
+
+      publicNetwork: /* @ngInject */ (
+        PciProjectsProjectInstanceService,
+        projectId,
+      ) => PciProjectsProjectInstanceService.getPublicNetwork(projectId),
+
+      regions: /* @ngInject */ (PciProjectsProjectInstanceService, projectId) =>
+        PciProjectsProjectInstanceService.getAvailablesRegions(projectId),
+
+      cancelLink: /* @ngInject */ ($state, projectId) =>
+        $state.href('pci.projects.project.instances', {
+          projectId,
+        }),
+
+      quotaLink: /* @ngInject */ ($state, projectId) =>
+        $state.href('pci.projects.project.quota', {
+          projectId,
+        }),
+      regionsLink: /* @ngInject */ ($state, projectId) =>
+        $state.href('pci.projects.project.regions', {
+          projectId,
+        }),
+
+      addPrivateNetworksLink: /* @ngInject */ ($state, projectId) =>
+        $state.href('pci.projects.project.privateNetwork', {
+          projectId,
+        }),
+
+      goBack: /* @ngInject */ (goToInstances) => goToInstances,
+
+      prices: /* @ngInject */ (
+        me,
+        PciProjectsProjectInstanceService,
+        project,
+        catalogEndpoint,
+      ) =>
+        PciProjectsProjectInstanceService.getExtraBandwidthCost(
+          catalogEndpoint,
+          project,
+          me,
+        ),
+
+      excludeCategories: /* @ngInject */ (pciFeatures) => {
+        const flavorCategories = Object.keys(FLAVORS_FEATURES_FLIPPING_MAP);
+        const toExclude = flavorCategories.filter((flavor) => {
+          return !pciFeatures.isFeatureAvailable(
+            FLAVORS_FEATURES_FLIPPING_MAP[flavor],
+          );
+        });
+
+        return EXCLUDE_FLAVOR_CATEGORIES.concat(toExclude);
+      },
+    },
+  });
+};
