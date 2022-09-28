21a731cb7903600557c4c4ea34e7d9f91ef60e4f
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2022-09-28 12:53:39 +0200

    feat(dedicated-ip.failover): add unused filter
    ref: MANAGER-9692
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/ip/failover/failover.controller.js
+++ packages/manager/apps/dedicated/client/app/ip/failover/failover.controller.js
@@ -1,1 +1,13 @@
-export default class IpFailoverController {}
+export default class IpFailoverController {
+  constructor($scope) {
+    this.$scope = $scope;
+    this.unusedFilter = false;
+  }
+
+  toggleUnusedFilter() {
+    this.unusedFilter = !this.unusedFilter;
+    this.$scope.$broadcast('ips.table.params', {
+      serviceName: this.unusedFilter ? 'null' : null,
+    });
+  }
+}
