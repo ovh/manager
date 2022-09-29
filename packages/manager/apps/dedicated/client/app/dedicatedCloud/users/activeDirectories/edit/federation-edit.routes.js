cca10835688af58b3feb072a789636f1da98be0e
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2022-09-29 09:49:13 +0200

    feat(pcc.user): add possibility to edit an active directory
    ref: MANAGER-10028
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

---
+++ packages/manager/apps/dedicated/client/app/dedicatedCloud/users/activeDirectories/edit/federation-edit.routes.js
@@ -0,0 +1,20 @@
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state('app.dedicatedCloud.details.users.federation-edit', {
+    url: '/federation/edit',
+    params: {
+      activeDirectory: null,
+    },
+    views: {
+      modal: {
+        component: 'dedicatedCloudUsersActiveDirectoriesEdit',
+      },
+    },
+    layout: { name: 'modal', backdrop: 'static' },
+    resolve: {
+      activeDirectory: /* @ngInject */ ($transition$) =>
+        $transition$.params().activeDirectory,
+      breadcrumb: /* @ngInject */ ($translate) =>
+        $translate.instant('dedicatedCloud_USER_AD_edit_title'),
+    },
+  });
+};
--- packages/manager/apps/dedicated/client/app/dedicatedCloud/users/dedicatedCloud-users.routes.js
+++ packages/manager/apps/dedicated/client/app/dedicatedCloud/users/dedicatedCloud-users.routes.js
@@ -44,6 +44,10 @@ export default /* @ngInject */ ($stateProvider) => {
         }),
       goToAddFederation: /* @ngInject */ ($state) => () =>
         $state.go('app.dedicatedCloud.details.users.federation-add'),
+      goToEditFederation: /* @ngInject */ ($state) => (activeDirectory) =>
+        $state.go('app.dedicatedCloud.details.users.federation-edit', {
+          activeDirectory,
+        }),
       goToImportUser: /* @ngInject */ ($state) => () =>
         $state.go('app.dedicatedCloud.details.users.import'),
       modifyUserRights: /* @ngInject */ ($state) => (userId) =>
