327760db0221cb8bb6cc1a232316d67ead7dd8c1
Florian Renaut <florian.renaut@corp.ovh.com>
2022-09-30 09:34:40 +0200

    feat(webpack): add html minifier in webpack config
    ref: MANAGER-10134
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

---
+++ packages/manager/tools/webpack-config/src/loaders/html-minifier.js
@@ -0,0 +1,18 @@
+const { minify } = require('html-minifier');
+// see : https://github.com/kangax/html-minifier
+
+module.exports = function translationsJson(source) {
+  if (this.mode !== 'production') {
+    return source;
+  }
+  return minify(source, {
+    continueOnParseError: true,
+    collapseWhitespace: true,
+    removeAttributeQuotes: true,
+    removeComments: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    useShortDoctype: true,
+  });
+};
