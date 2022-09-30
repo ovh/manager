020fff4d6caaa3dff05fa63f7298bb155b093514
Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
2022-09-30 08:08:38 +0200

    feat(cold-archive.onboarding): pr fixes
    ref: MANAGER-9181
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>

--- packages/manager/modules/pci/src/projects/project/storages/cold-archive/archives/onboarding/onboarding.constants.js
+++ packages/manager/modules/pci/src/projects/project/storages/cold-archive/archives/onboarding/onboarding.constants.js
@@ -1,22 +1,6 @@
-export const GUIDES = [
-  {
-    id: 'cold-archive-getting-started',
-    link:
-      'https://docs.ovh.com/fr/storage/s3/getting-started-with-cold-archive/',
-  },
-  {
-    id: 's3-getting-started',
-    link: 'https://docs.ovh.com/gb/en/storage/s3/getting-started-with-s3/',
-  },
-  {
-    id: 's3-limitaion',
-    link: 'https://docs.ovh.com/fr/storage/s3/limitations/',
-  },
-];
-
 export const TRACKING = {
   ONBOARDING_PAGE_VISITE: 'onboarding',
   ONBOARDING_CREATE_USER: 'add_a_user',
 };
 
-export default { GUIDES, TRACKING };
+export default { TRACKING };
