cb2db6dbcf970b5d711b8e935a29aa62130cdcd5
Florian Renaut <florian.renaut@corp.ovh.com>
2022-10-04 15:26:49 +0200

    feat: remove mfa-enrollment module
    ref: MANAGER-10083
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/app.routes.js
+++ packages/manager/apps/dedicated/client/app/app.routes.js
@@ -14,25 +14,6 @@ angular.module('App').config(
       url: '',
     });
 
-    $stateProvider.state('app.mfaEnrollment', {
-      url: '/mfa-enrollment',
-      views: {
-        'app@': {
-          component: 'mfaEnrollment',
-        },
-      },
-      params: {
-        forced: {
-          dynamic: true,
-        },
-      },
-      translations: { value: ['.'], format: 'json' },
-      resolve: {
-        forced: /* @ngInject */ ($transition$) => $transition$.params().forced,
-        from: /* @ngInject */ ($transition$) => $transition$.$from().name,
-      },
-    });
-
     $stateProvider.state('app.hpc', {
       url: '/hosted-private-cloud',
       template: '<ui-view></ui-view>',
--- packages/manager/apps/web/client/app/app.routes.js
+++ packages/manager/apps/web/client/app/app.routes.js
@@ -14,25 +14,6 @@ angular.module('App').config(($stateProvider, $urlRouterProvider) => {
     },
   });
 
-  $stateProvider.state('app.mfaEnrollment', {
-    url: '/mfa-enrollment',
-    views: {
-      'app@': {
-        component: 'mfaEnrollment',
-      },
-    },
-    params: {
-      forced: {
-        dynamic: true,
-      },
-    },
-    translations: { value: ['.'], format: 'json' },
-    resolve: {
-      forced: /* @ngInject */ ($transition$) => $transition$.params().forced,
-      from: /* @ngInject */ ($transition$) => $transition$.$from().name,
-    },
-  });
-
   $stateProvider.state('app.microsoft', {
     abstract: true,
     template: '<div ui-view></div>',
