c4601b901be149d3ef7aad61900abf118aa84e88
Jisay <jean-christophe.alleman@corp.ovh.com>
2022-10-07 11:36:25 +0200

    test(container): fix jest test
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

--- packages/manager/apps/container/src/__tests__/__test-utils__/contextRenders.tsx
+++ packages/manager/apps/container/src/__tests__/__test-utils__/contextRenders.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { I18nextProvider } from 'react-i18next';
 import { initShell } from '@ovh-ux/shell';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import i18n from '../config/i18nTestConfig';
 import { ApplicationProvider } from '../../context';
-import { NotificationsProvider } from '../../core/notifications';
 import { ContainerProvider } from '../../core/container';
 
 const renderWithI18n = (children, { ...renderOptions }) => {
@@ -17,33 +17,18 @@ const renderWithI18n = (children, { ...renderOptions }) => {
 
 const renderWithShell = async (children, { environment, ...renderOptions }) => {
   const shell = await initShell();
+  const queryClient = new QueryClient();
 
   return render(
     <I18nextProvider i18n={i18n}>
-      <ApplicationProvider environment={environment} shell={shell}>
-        <ContainerProvider>{children}</ContainerProvider>
-      </ApplicationProvider>
-    </I18nextProvider>,
-    renderOptions,
-  );
-};
-
-const renderWithNotifications = async (
-  children,
-  { environment, ...renderOptions },
-) => {
-  const shell = await initShell();
-
-  return render(
-    <I18nextProvider i18n={i18n}>
-      <ApplicationProvider environment={environment} shell={shell}>
-        <NotificationsProvider environment={environment}>
+      <QueryClientProvider client={queryClient}>
+        <ApplicationProvider environment={environment} shell={shell}>
           <ContainerProvider>{children}</ContainerProvider>
-        </NotificationsProvider>
-      </ApplicationProvider>
+        </ApplicationProvider>
+      </QueryClientProvider>
     </I18nextProvider>,
     renderOptions,
   );
 };
 
-export { renderWithI18n, renderWithShell, renderWithNotifications };
+export { renderWithI18n, renderWithShell };
