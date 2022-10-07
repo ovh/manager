dbddf2dab99efb6cf7ca602dc46f79b5af482e9c
Jisay <jean-christophe.alleman@corp.ovh.com>
2022-10-07 10:17:53 +0200

    refactor(container): use tanstack query in notifications sidebar
    ref: MANAGER-10159
    
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

---
+++ packages/manager/apps/container/src/helpers/dateHelper.ts
@@ -0,0 +1,21 @@
+import parse from 'date-fns/parse';
+import formatDistanceStrict from 'date-fns/formatDistanceStrict';
+// TODO: upgrade prettier
+// eslint-disable-next-line prettier/prettier
+import type { Locale } from 'date-fns';
+
+export const fromNow = async (date: string, locale: string): Promise<string> => {
+  const importLocale = () => import(`date-fns/locale`);
+  const locales = (await importLocale()) as Record<string, Locale> ;
+  return formatDistanceStrict(
+    parse(date, 'yyyy-MM-dd', new Date()),
+    new Date(),
+    {
+      locale: locales[locale.split('_')[0].toLowerCase()],
+      addSuffix: true,
+      roundingMethod: 'round',
+    },
+  );
+};
+
+export default { fromNow };
