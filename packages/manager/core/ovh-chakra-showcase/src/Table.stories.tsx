45ad629e4a659238a5b3e616c86416a6ab41b0d9
Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
2022-10-13 09:01:07 +0200

    feat(chakra): add chakra theme and components
    Signed-off-by: Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>

---
+++ packages/manager/core/ovh-chakra-showcase/src/Table.stories.tsx
@@ -0,0 +1,72 @@
+import {
+  Table,
+  TableContainer,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+  Tfoot,
+} from '@chakra-ui/react';
+
+export default {
+  title: 'Example/Tables',
+  component: Table,
+};
+
+const TemplateTable = () => (
+  <TableContainer>
+    <Table>
+      <Thead>
+        <Tr>
+          <Th>Header</Th>
+          <Th>Header</Th>
+          <Th>Header</Th>
+          <Th>Header</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        <Tr>
+          <Td>Column</Td>
+          <Td>Column</Td>
+          <Td>Column</Td>
+          <Td>Column</Td>
+        </Tr>
+        <Tr>
+          <Td>Column</Td>
+          <Td>Column</Td>
+          <Td>Column</Td>
+          <Td>Column</Td>
+        </Tr>
+        <Tr>
+          <Td>Column</Td>
+          <Td>Column</Td>
+          <Td>Column</Td>
+          <Td>Column</Td>
+        </Tr>
+        <Tr>
+          <Td>Column</Td>
+          <Td>Column</Td>
+          <Td>Column</Td>
+          <Td>Column</Td>
+        </Tr>
+        <Tr>
+          <Td>Column</Td>
+          <Td>Column</Td>
+          <Td>Column</Td>
+          <Td>Column</Td>
+        </Tr>
+      </Tbody>
+      <Tfoot>
+        <Tr>
+          <Th>Footer</Th>
+          <Th>Footer</Th>
+          <Th>Footer</Th>
+          <Th>Footer</Th>
+        </Tr>
+      </Tfoot>
+    </Table>
+  </TableContainer>
+);
+
+export const Basic = TemplateTable.bind({});
