c8920387031b7cca48598cf0e92428597a23e673
vikash singh <vikash.singh@corp.ovh.com>
2022-10-17 19:10:52 +0530

    feat(dedicated.nutanix): update dashboard page
     ref: MANAGER-10073
     Addition of BYOL in Nutanix dashboard
    
    Signed-off-by: vikash singh <vikash.singh@corp.ovh.com>

--- packages/manager/modules/nutanix/src/dashboard/controller.js
+++ packages/manager/modules/nutanix/src/dashboard/controller.js
@@ -1,15 +1,34 @@
-import { GUIDE_URL } from './constants';
+import moment from 'moment';
+
+import { GUIDE_URL, LICENSE_REGISTRATION_ENDS_IN_DAYS } from './constants';
 
 export default class NutanixDashboardCtrl {
   /* @ngInject */
-  constructor($translate, coreConfig) {
+  constructor($translate, atInternet, coreConfig) {
     this.$translate = $translate;
     this.user = coreConfig.getUser();
+    this.atInternet = atInternet;
   }
 
   $onInit() {
     this.nutanixGuideUrl =
       GUIDE_URL.ALL_GUIDE[this.user.ovhSubsidiary] ||
       GUIDE_URL.ALL_GUIDE.DEFAULT;
+  }
+
+  getLicenseRegistrationEndDate() {
+    return moment(this.serviceInfo.creation)
+      .add(moment.duration(LICENSE_REGISTRATION_ENDS_IN_DAYS, 'days'))
+      .format('DD/MM/YYYY');
+  }
+
+  isLicenceRegisterDateExpired() {
+    const diff = moment().diff(moment(this.serviceInfo.creation), 'days');
+    return (
+      diff >= 0 &&
+      diff <= LICENSE_REGISTRATION_ENDS_IN_DAYS &&
+      this.getTechnicalDetails.nutanixCluster.license.edition ===
+        'Personal license'
+    );
   }
 }
