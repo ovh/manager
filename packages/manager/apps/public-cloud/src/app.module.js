a457b95473cb5cc10f1ebe12463f2cc3c88885ba
Antoine Leblanc <antoine.leblanc@ovhcloud.com>
2022-10-31 08:52:43 +0100

    perf: remove `public-cloud-dark-mode` component (#8273)
    Signed-off-by: Antoine Leblanc <antoine.leblanc@ovhcloud.com>

--- packages/manager/apps/public-cloud/src/app.module.js
+++ packages/manager/apps/public-cloud/src/app.module.js
@@ -32,7 +32,6 @@ import '@ovh-ux/ui-kit/dist/css/oui.css';
 import 'ovh-ui-kit-bs/dist/css/oui-bs3.css';
 
 import { initAtInternet } from './components/at-internet';
-import darkMode from './components/dark-mode';
 
 import './assets/theme/default/index.less';
 import './index.scss';
@@ -69,7 +68,6 @@ export default async (containerEl, shellClient) => {
       [
         ...get(__NG_APP_INJECTIONS__, environment.getRegion(), []),
         initAtInternet(shellClient.tracking),
-        darkMode,
         ngAnimate,
         ngUiRouterBreadcrumb,
         ngOvhApiWrappers,
