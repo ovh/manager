121c17460c2122eadabeda151a39846b775540ca
Mohammed ZAHAF <22635766+mohammed-zahaf@users.noreply.github.com>
2022-11-08 14:19:27 +0100

    feat(dedicated.server): baremetal monitoring options (#8354)
    ref: MANAGER-9918
    
    Signed-off-by: Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>
    Signed-off-by: CDS Translator Agent <opensource@ovh.net>
    Signed-off-by: Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/dedicated/server/monitoring/update/dedicated-server-monitoring-update.controller.js
+++ packages/manager/apps/dedicated/client/app/dedicated/server/monitoring/update/dedicated-server-monitoring-update.controller.js
@@ -1,12 +1,23 @@
+import { MONITORING_TRACKING_PREFIX } from './dedicated-server-monitoring-update.constants';
+
 angular.module('App').controller(
   'DedicatedServerMonitoringUpdateCtrl',
   class DedicatedServerMonitoringUpdateCtrl {
-    constructor($q, $stateParams, $state, Alerter, $translate, Server) {
+    constructor(
+      $q,
+      $stateParams,
+      $state,
+      $translate,
+      atInternet,
+      Alerter,
+      Server,
+    ) {
       this.$q = $q;
       this.$stateParams = $stateParams;
       this.$state = $state;
-      this.Alerter = Alerter;
       this.$translate = $translate;
+      this.atInternet = atInternet;
+      this.Alerter = Alerter;
       this.Server = Server;
     }
 
@@ -14,11 +25,29 @@ angular.module('App').controller(
       this.server = null;
       this.isLoading = false;
       this.isUpdating = false;
+      this.monitoringOptions = [
+        {
+          status: 'disabled',
+          settings: { monitoring: false },
+          tracking: 'disabled',
+        },
+        {
+          status: 'proactive',
+          settings: { monitoring: true, noIntervention: false },
+          tracking: 'enable-with-intervention',
+        },
+        {
+          status: 'no-proactive',
+          settings: { monitoring: true, noIntervention: true },
+          tracking: 'enable-without-intervention',
+        },
+      ];
 
       this.isLoading = true;
       return this.Server.getSelected(this.$stateParams.productId)
         .then((server) => {
           this.server = server;
+          this.preselectOption();
         })
         .catch((err) => {
           this.$q.reject(err);
@@ -29,67 +58,98 @@ angular.module('App').controller(
         });
     }
 
+    getMonitoringStatus() {
+      const { monitored, noIntervention } = this.server;
+      let monitoringStatus = 'disabled';
+
+      // proactive intervention
+      if (monitored && !noIntervention) {
+        monitoringStatus = 'proactive';
+      }
+
+      // no proactive intervention
+      if (monitored && noIntervention) {
+        monitoringStatus = 'no-proactive';
+      }
+
+      return monitoringStatus;
+    }
+
+    preselectOption() {
+      this.selectedOption = this.monitoringOptions.find(({ status }) => {
+        return status === this.getMonitoringStatus();
+      });
+    }
+
+    trackClick(hit) {
+      return this.atInternet.trackClick({
+        name: `${MONITORING_TRACKING_PREFIX}::${hit}`,
+        type: 'action',
+      });
+    }
+
+    trackConfirmClick(hit) {
+      return this.atInternet.trackClick({
+        name: `dedicated-server::monitoring-update-confirm::${hit}`,
+        type: 'action',
+      });
+    }
+
+    trackBannerMessages(statusHit, monitoringOptionHit) {
+      return this.atInternet.trackClick({
+        name: `dedicated-server::monitoring-update-banner::${statusHit}::${monitoringOptionHit}`,
+        type: 'action',
+      });
+    }
+
+    close() {
+      return this.$state.go('^');
+    }
+
     /**
      * Update monitoring.
      * @return {Promise}
      */
     update() {
-      const { monitored } = this.server;
+      this.trackClick('confirm');
+      this.trackConfirmClick(this.selectedOption.tracking);
+
+      const { monitoring, noIntervention } = this.selectedOption.settings;
+
       this.isUpdating = true;
       return this.Server.updateMonitoring(
         this.$stateParams.productId,
-        !monitored,
-        !monitored, // reset no-intervention to monitored value
+        monitoring,
+        noIntervention, // reset no-intervention to monitored value
       )
         .then(() => {
-          if (monitored === true) {
-            this.Alerter.success(
-              this.$translate.instant(
-                'server_configuration_monitoring_deactivate_success',
-                {
-                  t0: this.server.name,
-                },
-              ),
-              'server_dashboard_alert',
-            );
-          } else {
-            this.Alerter.success(
-              this.$translate.instant(
-                'server_configuration_monitoring_activate_success',
-                {
-                  t0: this.server.name,
-                },
-              ),
-              'server_dashboard_alert',
-            );
-          }
+          this.trackBannerMessages('success', this.selectedOption.tracking);
+
+          const monitoringOption =
+            !monitoring || noIntervention ? 'no-proactive' : 'proactive';
+
+          this.Alerter.success(
+            this.$translate.instant(
+              `server_configuration_monitoring_update_modal_update_${monitoringOption}_request_success`,
+              {
+                serverName: this.server.name,
+              },
+            ),
+            'server_dashboard_alert',
+          );
+
           return this.close();
         })
         .catch((err) => {
-          if (monitored === true) {
-            this.Alerter.error(
-              this.$translate.instant(
-                'server_configuration_monitoring_deactivate_failed',
-                {
-                  t0: this.server.name,
-                },
-              ),
-              err,
-              'server_dashboard_alert',
-            );
-          } else {
-            this.Alerter.error(
-              this.$translate.instant(
-                'server_configuration_monitoring_activate_failed',
-                {
-                  t0: this.server.name,
-                },
-              ),
-              err,
-              'server_dashboard_alert',
-            );
-          }
-          this.$q.reject(err);
+          this.trackBannerMessages('error', this.selectedOption.tracking);
+
+          this.Alerter.error(
+            this.$translate.instant(
+              'server_configuration_monitoring_update_modal_update_request_failed',
+            ),
+            err,
+            'server_dashboard_alert',
+          );
           return this.cancel(err);
         })
         .finally(() => {
@@ -97,8 +157,10 @@ angular.module('App').controller(
         });
     }
 
-    close() {
-      this.$state.go('^');
+    onCloseClick() {
+      this.trackClick('cancel');
+
+      return this.close();
     }
   },
 );
