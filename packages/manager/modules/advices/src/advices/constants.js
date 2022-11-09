16a40f9cf5aa4cfcd2b483bcfed5e974ab31dded
Mohammed ZAHAF <22635766+mohammed-zahaf@users.noreply.github.com>
2022-11-09 09:27:52 +0100

    feat(apps): scope rs recommendations (#8328)
    ref: MANAGER-10290
    
    Signed-off-by: Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>

---
+++ packages/manager/modules/advices/src/advices/constants.js
@@ -0,0 +1,13 @@
+export const ADVICE_TYPE_ENUM = {
+  RETENTION: 'retention',
+  UPSELL_CROSS_SELL: 'upsell-cross-sell',
+};
+
+export const RECOMMENDER_SYSTEM = {
+  MAX_RECOMMENDATION: 1,
+};
+
+export default {
+  ADVICE_TYPE_ENUM,
+  RECOMMENDER_SYSTEM,
+};
