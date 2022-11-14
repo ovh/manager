a276be404474a4ca2c6f4bf86f924067c8d6028f
Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
2022-11-14 23:51:19 +0100

    feat(storage.cold-archive): refactor priceLink in addContainer view
    ref: MANAGER-10190
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
    Signed-off-by: Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/cold-archive/add/add.routing.js
+++ packages/manager/modules/pci/src/projects/project/storages/cold-archive/add/add.routing.js
@@ -1,18 +1,16 @@
-import { CHECK_PRICES_DOC_LINK } from './add.constants';
-
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('pci.projects.project.storages.cold-archive.add', {
     url: '/new',
-    component: 'pciProjectsProjectStoragesColdArchiveAdd',
+    views: {
+      'coldArchiveView@pci.projects.project.storages.cold-archive': {
+        component: 'pciProjectsProjectStoragesColdArchiveAdd',
+      },
+    },
     resolve: {
       breadcrumb: /* @ngInject */ ($translate) =>
         $translate.instant(
           'pci_projects_project_storages_cold_archive_add_breadcrumb',
         ),
-
-      checkPricesLink: /* @ngInject */ (coreConfig) =>
-        CHECK_PRICES_DOC_LINK[coreConfig.getUser().ovhSubsidiary] ||
-        CHECK_PRICES_DOC_LINK.DEFAULT,
 
       stepper: /* @ngInject */ () => ({
         nameArchiveStep: { name: 'cold_archive_name_archive', display: null },
