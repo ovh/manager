4c023276a901deb888613d4b1cbc0d7a2ffd4e71
lucas-chaigne <89460513+lucas-chaigne@users.noreply.github.com>
2022-11-16 06:47:41 +0100

    fix(domain.order-hosting): remove useless option in canada (#8276)
    ref: WEB-13060
    
    Signed-off-by: Lucas Chaigne <lucas.chaigne@ovhcloud.com>

--- packages/manager/apps/web/client/app/domain/webhosting/order/steps/dnsConfiguration/domain-webhosting-order-steps-dnsConfiguration.controller.js
+++ packages/manager/apps/web/client/app/domain/webhosting/order/steps/dnsConfiguration/domain-webhosting-order-steps-dnsConfiguration.controller.js
@@ -1,4 +1,21 @@
 export default class {
+  /* @ngInject */
+  constructor(ovhFeatureFlipping) {
+    this.ovhFeatureFlipping = ovhFeatureFlipping;
+  }
+
+  $onInit() {
+    const emailDnsConfigurationFeature = 'hosting:email-dns-configuration';
+
+    this.ovhFeatureFlipping
+      .checkFeatureAvailability(emailDnsConfigurationFeature)
+      .then((featureAvailability) => {
+        this.emailDnsConfigurationAvailable = featureAvailability.isFeatureAvailable(
+          emailDnsConfigurationFeature,
+        );
+      });
+  }
+
   enableHelpers() {
     this.isHostingHelpOpen = undefined;
     this.isEmailHelpOpen = undefined;
