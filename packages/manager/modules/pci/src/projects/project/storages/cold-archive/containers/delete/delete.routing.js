06a8cfcb2d3450658d4b606e9955fc5823d6579d
Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
2022-11-17 06:39:52 +0100

    feat(storage.cold-archive): use states constants
    ref: MANAGER-9330
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
    Signed-off-by: Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/cold-archive/containers/delete/delete.routing.js
+++ packages/manager/modules/pci/src/projects/project/storages/cold-archive/containers/delete/delete.routing.js
@@ -1,25 +1,25 @@
-import { COLD_ARCHIVE_TRACKING_PREFIX } from '../../cold-archives.constants';
+import {
+  COLD_ARCHIVE_TRACKING_PREFIX,
+  COLD_ARCHIVE_STATES,
+} from '../../cold-archives.constants';
 
 export default /* @ngInject */ ($stateProvider) => {
-  $stateProvider.state(
-    'pci.projects.project.storages.cold-archive.containers.delete',
-    {
-      url: '/delete?containerName',
-      views: {
-        modal: {
-          component: 'pciStoragesColdArchiveContainersDelete',
-        },
-      },
-      layout: 'modal',
-      resolve: {
-        breadcrumb: () => null,
-
-        containerName: /* @ngInject */ ($transition$) =>
-          $transition$.params().containerName,
-      },
-      atInternet: {
-        rename: `${COLD_ARCHIVE_TRACKING_PREFIX}::delete`,
+  $stateProvider.state(COLD_ARCHIVE_STATES.CONTAINER_DELETE, {
+    url: '/delete?containerName',
+    views: {
+      modal: {
+        component: 'pciStoragesColdArchiveContainersDelete',
       },
     },
-  );
+    layout: 'modal',
+    resolve: {
+      breadcrumb: () => null,
+
+      containerName: /* @ngInject */ ($transition$) =>
+        $transition$.params().containerName,
+    },
+    atInternet: {
+      rename: `${COLD_ARCHIVE_TRACKING_PREFIX}::delete`,
+    },
+  });
 };
