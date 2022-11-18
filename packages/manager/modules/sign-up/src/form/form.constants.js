2b5b5853d7b4ed2aa49809904f8c14f5ad9dfcdb
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2022-11-18 09:04:54 +0100

    fix(sign-up): filter read only params
    ref: DTRSD-101357
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/modules/sign-up/src/form/form.constants.js
+++ packages/manager/modules/sign-up/src/form/form.constants.js
@@ -30,8 +30,19 @@ export const MODEL_DEBOUNCE_DELAY = 500;
 
 export const OVH_SUBSIDIARY_ITEM_NAME = 'ovhSubsidiaryCreationForm';
 
+export const READ_ONLY_PARAMS = [
+  'currency',
+  'email',
+  'nichandle',
+  'ovhCompany',
+  'customerCode',
+  'state',
+  'ovhSubsidiary',
+];
+
 export default {
   ENUM_TRANSLATION_RULES,
   MODEL_DEBOUNCE_DELAY,
   OVH_SUBSIDIARY_ITEM_NAME,
+  READ_ONLY_PARAMS,
 };
