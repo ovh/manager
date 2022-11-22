68766e7b46df3b3f4313ed6f41d545ef14878032
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2022-11-22 16:12:29 +0100

    fix(public-cloud): remove chatbot
    ref: DTCORE-275
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/public-cloud/src/index.controller.js
+++ packages/manager/apps/public-cloud/src/index.controller.js
@@ -4,31 +4,12 @@ import { getShellClient } from './shell';
 
 export default class PublicCloudController {
   /* @ngInject */
-  constructor(
-    $rootScope,
-    $scope,
-    $state,
-    $timeout,
-    $window,
-    atInternet,
-    coreConfig,
-    ovhUserPref,
-    publicCloud,
-    ovhFeatureFlipping,
-  ) {
-    this.$rootScope = $rootScope;
+  constructor($scope, $timeout, atInternet) {
     this.$scope = $scope;
-    this.$state = $state;
     this.$timeout = $timeout;
-    this.$window = $window;
     this.atInternet = atInternet;
-    this.coreConfig = coreConfig;
-    this.ovhUserPref = ovhUserPref;
-    this.publicCloud = publicCloud;
-    this.ovhFeatureFlipping = ovhFeatureFlipping;
     this.isTopLevelApplication = isTopLevelApplication();
 
-    this.chatbotEnabled = false;
     this.shell = getShellClient();
 
     $scope.$on('oui-step-form.submit', (event, { form }) => {
@@ -40,41 +21,12 @@ export default class PublicCloudController {
   }
 
   $onInit() {
-    this.currentLanguage = this.coreConfig.getUserLanguage();
-    this.user = this.coreConfig.getUser();
-
     this.shell.ux.onRequestClientSidebarOpen(() =>
       this.$timeout(() => this.openSidebar()),
     );
-
-    const unregisterListener = this.$scope.$on('app:started', () => {
-      const CHATBOT_FEATURE = 'chatbot';
-      this.ovhFeatureFlipping
-        .checkFeatureAvailability(CHATBOT_FEATURE)
-        .then((featureAvailability) => {
-          this.chatbotEnabled = featureAvailability.isFeatureAvailable(
-            CHATBOT_FEATURE,
-          );
-          if (this.chatbotEnabled) {
-            this.$rootScope.$broadcast(
-              'ovh-chatbot:enable',
-              this.chatbotEnabled,
-            );
-          }
-        });
-      unregisterListener();
-    });
   }
 
   openSidebar() {
     this.$scope.$broadcast('sidebar:open');
-  }
-
-  onChatbotOpen() {
-    this.shell.ux.onChatbotOpen();
-  }
-
-  onChatbotClose(reduced) {
-    this.shell.ux.onChatbotClose(reduced);
   }
 }
