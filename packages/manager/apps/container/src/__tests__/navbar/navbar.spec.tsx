a8e85aeaa76af941aaa641b610f305de0a1df4f0
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2022-11-22 16:44:41 +0100

    fix(container): remove chatbot
    ref: DTCORE-275
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/container/src/__tests__/navbar/navbar.spec.tsx
+++ packages/manager/apps/container/src/__tests__/navbar/navbar.spec.tsx
@@ -24,7 +24,7 @@ const server = setupServer(
     },
   ),
   rest.get(
-    '/engine/2api/feature/chatbot,pnr:betaV1,pnr:betaV2/availability',
+    '/engine/2api/feature/livechat,pnr:betaV1,pnr:betaV2/availability',
     (req, res, ctx) => {
       return res(ctx.json({}));
     },
