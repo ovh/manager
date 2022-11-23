3efe3d83f742494358487db44beeb4397e815aee
vks-ovh <102142952+vks-ovh@users.noreply.github.com>
2022-11-23 18:52:54 +0530

    feat(dedicated.nutanix): add nutanix entry on order menu (#8429)
    ref: DTRSD-100616
    
    Signed-off-by: vikash singh <vikash.singh@corp.ovh.com>

--- packages/manager/modules/server-sidebar/src/order.constants.js
+++ packages/manager/modules/server-sidebar/src/order.constants.js
@@ -342,6 +342,23 @@ export const ORDER_URLS = {
       ES: 'https://www.ovh.es/dominios/',
       FR: 'https://www.ovh.com/fr/domaines/tarifs/',
     },
+    dedicatedNutanixOrder: {
+      CZ: 'https://www.ovhcloud.com/en-ie/hosted-private-cloud/nutanix/',
+      DE: 'https://www.ovhcloud.com/de/hosted-private-cloud/nutanix/',
+      ES: 'https://www.ovhcloud.com/es-es/hosted-private-cloud/nutanix/',
+      FI: 'https://www.ovhcloud.com/en-ie/hosted-private-cloud/nutanix/',
+      FR: 'https://www.ovhcloud.com/fr/hosted-private-cloud/nutanix/',
+      GB: 'https://www.ovhcloud.com/en-gb/hosted-private-cloud/nutanix/',
+      IE: 'https://www.ovhcloud.com/en-ie/hosted-private-cloud/nutanix/',
+      IT: 'https://www.ovhcloud.com/it/hosted-private-cloud/nutanix/',
+      LT: 'https://www.ovhcloud.com/en-ie/hosted-private-cloud/nutanix/',
+      NL: 'https://www.ovhcloud.com/nl/hosted-private-cloud/nutanix/',
+      PL: 'https://www.ovhcloud.com/pl/hosted-private-cloud/nutanix/',
+      PT: 'https://www.ovhcloud.com/pt/hosted-private-cloud/nutanix/',
+      MA: 'https://www.ovhcloud.com/fr-ma/hosted-private-cloud/nutanix/',
+      SN: 'https://www.ovhcloud.com/fr-sn/hosted-private-cloud/nutanix/',
+      TN: 'https://www.ovhcloud.com/fr-tn/hosted-private-cloud/nutanix/',
+    },
   },
   CA: {
     DEDICATED: {
@@ -484,6 +501,15 @@ export const ORDER_URLS = {
       SG: 'https://www.ovh.com/sg/vps/',
       WE: 'https://www.ovh.com/us/vps/',
       WS: 'https://www.ovh.com/us/es/vps/',
+    },
+    dedicatedNutanixOrder: {
+      ASIA: 'https://www.ovhcloud.com/asia/hosted-private-cloud/nutanix/',
+      AU: 'https://www.ovhcloud.com/en-au/hosted-private-cloud/nutanix/',
+      CA: 'https://www.ovhcloud.com/en-ca/hosted-private-cloud/nutanix/',
+      QC: 'https://www.ovhcloud.com/fr-ca/hosted-private-cloud/nutanix/',
+      SG: 'https://www.ovhcloud.com/en-sg/hosted-private-cloud/nutanix/',
+      WE: 'https://www.ovhcloud.com/en/hosted-private-cloud/nutanix/',
+      WS: 'https://www.ovhcloud.com/en/hosted-private-cloud/nutanix/',
     },
   },
   US: {
@@ -717,6 +743,17 @@ export const SIDEBAR_ORDER_CONFIG = [
     external: true,
     app: [DEDICATED],
     tracker: 'dedicated::orders::dedicated-servers-eco::order',
+  },
+  {
+    id: 'order-dedicated-server-nutanix',
+    title: 'dedicated_server_nutanix',
+    feature: 'dedicated-server:nutanixOrder',
+    icon: 'oui-icon oui-icon-nutanix_concept',
+    linkId: 'dedicatedNutanixOrder',
+    target: '_blank',
+    external: true,
+    app: [DEDICATED],
+    tracker: 'dedicated::orders::dedicated-servers-nutanix::order',
   },
 ];
 
