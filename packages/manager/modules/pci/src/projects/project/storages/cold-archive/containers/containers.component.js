b1c404b43ffcf9d8456b8b61bf83ee88c8925d37
Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>
2022-11-28 13:07:50 +0100

    feat(storage.cold-archive): container virtual host
    ref: MANAGER-9330
    
    Signed-off-by: Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/cold-archive/containers/containers.component.js
+++ packages/manager/modules/pci/src/projects/project/storages/cold-archive/containers/containers.component.js
@@ -14,6 +14,7 @@ export default {
     goToDeleteContainerObjects: '<',
     goToArchiveContainer: '<',
     goToRestoreContainer: '<',
+    goToContainerVirtualHost: '<',
     goToContainer: '<',
     scrollToTop: '<',
     guides: '<',
