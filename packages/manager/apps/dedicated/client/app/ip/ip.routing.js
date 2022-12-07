e03cb14583402ac130fa484c9eb103d2af5e45e3
Anoop N <anoop.n@ovhcloud.com>
2022-12-07 11:52:53 +0530

    sync: master to develop (#8511)
    Signed-off-by: Anoop <anoop.n@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/ip/ip.routing.js
+++ packages/manager/apps/dedicated/client/app/ip/ip.routing.js
@@ -1,146 +1,44 @@
 import controller from './ip.controller';
 import template from './ip.html';
 
-import {
-  TRACKING_PREFIX,
-  REPRICING_BANNER_URL,
-  REPRICING_BANNER_DATE_MIN,
-  REPRICING_BANNER_DATE_MAX,
-  REPRICING_BANNER_DATE_CREATION,
-  IP_SERVICE_PATH,
-} from './ip.constant';
-
 const allowByoipFeatureName = 'ip:byoip';
-
-export const listRouting = {
-  reloadOnSearch: false,
-  resolve: {
-    goToDashboard: /* @ngInject */ ($state) => () =>
-      $state.go('app.ip.dashboard'),
-    goToAntispam: /* @ngInject */ ($state) => (ip) =>
-      $state.go('app.ip.dashboard.ip.antispam', {
-        ip: ip.ip,
-      }),
-    goToFirewall: /* @ngInject */ ($state) => (ip) =>
-      $state.go('app.ip.dashboard.ip.firewall', {
-        ip: ip.ip,
-      }),
-    goToGameFirewall: /* @ngInject */ ($state) => (ip) =>
-      $state.go('app.ip.dashboard.ip.game-firewall', {
-        ip: ip.ip,
-      }),
-    goToAgoraOrder: /* @ngInject */ ($state, trackPage) => () => {
-      trackPage('order');
-      return $state.go('app.ip.agora-order');
-    },
-    goToByoipConfiguration: /* @ngInject */ ($state, trackClick) => () => {
-      trackClick('bring-your-own-ip');
-      return $state.go('app.ip.byoip');
-    },
-    breadcrumb: () => null,
-    hideBreadcrumb: () => true,
-    orderIpAvailable: /* @ngInject */ (coreConfig, ovhFeatureFlipping) => {
-      const universe = coreConfig.getUniverse() === 'server' ? 'server' : 'hpc';
-      return ovhFeatureFlipping
-        .checkFeatureAvailability(`ip:order:${universe}`)
-        .then((featureAvailability) =>
-          featureAvailability.isFeatureAvailable(`ip:order:${universe}`),
-        );
-    },
-  },
-};
 
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('app.ip', {
-    url: '/ip?action&ip&serviceType&page&pageSize&ipBlock',
+    url: '/ip?action&ip&serviceName&page&pageSize&ipBlock',
     template,
     controller,
     reloadOnSearch: false,
-    redirectTo: (transition) =>
-      transition
-        .injector()
-        .getAsync('hasAnyIp')
-        .then((hasAnyIp) => `app.ip.${hasAnyIp ? 'dashboard' : 'onboarding'}`),
+    redirectTo: 'app.ip.dashboard',
     resolve: {
-      hasAnyIp: /* @ngInject */ (iceberg) =>
-        iceberg('/ip')
-          .query()
-          .expand('CachedObjectList-Pages')
-          .limit(1)
-          .execute(null, true)
-          .$promise.then(({ data: [ip] }) => !!ip),
-      isRepricingBannerShown: /* @ngInject */ ($q, hasAnyIp, iceberg) => {
-        if (
-          !hasAnyIp ||
-          !moment().isBetween(
-            REPRICING_BANNER_DATE_MIN,
-            REPRICING_BANNER_DATE_MAX,
-          )
-        ) {
-          return $q.when(false);
-        }
-        return iceberg('/services')
-          .query()
-          .expand('CachedObjectList-Pages')
-          .addFilter('route.path', 'eq', IP_SERVICE_PATH)
-          .addFilter(
-            'billing.lifecycle.current.creationDate',
-            'lt',
-            moment(REPRICING_BANNER_DATE_CREATION).toISOString(),
-          )
-          .limit(1)
-          .execute()
-          .$promise.then(({ data: [ip] }) => !!ip);
-      },
-      openBannerRepricePage: (coreConfig) => () =>
-        window.open(
-          REPRICING_BANNER_URL[coreConfig.getUser().ovhSubsidiary] ||
-            REPRICING_BANNER_URL.DEFAULT,
-          '_blank',
-          'noopener',
-        ),
-      trackPage: /* @ngInject */ (atInternet) => (...hits) => {
+      trackingPrefix: () => 'dedicated::ip::dashboard',
+      trackPage: /* @ngInject */ (atInternet, trackingPrefix) => (hit) => {
         atInternet.trackPage({
-          name: [TRACKING_PREFIX, ...hits].join('::'),
+          name: `${trackingPrefix}::${hit}`,
         });
       },
-      trackClick: /* @ngInject */ (atInternet) => (...hits) => {
+      trackClick: /* @ngInject */ (atInternet, trackingPrefix) => (hit) => {
         atInternet.trackClick({
-          name: [TRACKING_PREFIX, ...hits].join('::'),
+          name: `${trackingPrefix}::${hit}`,
           type: 'action',
         });
       },
       dashboardLink: /* @ngInject */ ($transition$, $state) =>
         $state.href('app.ip.dashboard', $transition$.params()),
-      failoverLink: /* @ngInject */ ($transition$, $state) =>
-        $state.href('app.ip.failover', $transition$.params()),
       ipLbLink: /* @ngInject */ ($transition$, $state) =>
         $state.href('app.ip.dashboard.iplb', $transition$.params()),
       currentActiveLink: /* @ngInject */ ($transition$, $state) => () =>
         $state.href($state.current.name, $transition$.params()),
       goToOrganisation: /* @ngInject */ ($state) => () =>
-        $state.go('app.ip.organisation'),
+        $state.go('app.ip.dashboard.organisation'),
       isByoipAvailable: /* @ngInject */ (ovhFeatureFlipping) =>
         ovhFeatureFlipping
           .checkFeatureAvailability(allowByoipFeatureName)
           .then((feature) => feature.isFeatureAvailable(allowByoipFeatureName)),
-      goToByoipConfiguration: /* @ngInject */ ($state) => () => {
-        return $state.go('app.ip.byoip');
-      },
-      goToAgoraOrder: /* @ngInject */ ($state, trackPage) => () => {
-        trackPage('order');
-        return $state.go('app.ip.agora-order');
-      },
       breadcrumb: /* @ngInject */ ($translate) => $translate.instant('ip_ip'),
     },
-  });
-
-  $stateProvider.state('app.ip.dashboard.ip', {
-    url: '/:ip',
-    redirectTo: 'app.ip.dashboard',
-    resolve: {
-      ip: /* @ngInject */ ($transition$) => $transition$.params().ip,
-      breadcrumb: /* @ngInject */ (ip) => ip,
+    atInternet: {
+      rename: 'dedicated::ip::dashboard',
     },
   });
 };
