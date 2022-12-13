700fd68b7934a48ddc04f1c2ef8695d20ee7c993
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2022-12-13 11:47:35 +0100

    fix(apps): define Application Version
    ref: DTCORE-400
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/email-pro/src/index.js
+++ packages/manager/apps/email-pro/src/index.js
@@ -3,10 +3,11 @@ import 'script-loader!jquery'; // eslint-disable-line
 import 'script-loader!filesize/lib/filesize.js'; // eslint-disable-line
 import 'whatwg-fetch';
 import { bootstrapApplication } from '@ovh-ux/manager-core';
+import { defineApplicationVersion } from '@ovh-ux/request-tagger';
+
+defineApplicationVersion(__VERSION__);
 
 bootstrapApplication('email-pro').then((environment) => {
-  environment.setVersion(__VERSION__);
-
   import(`./config-${environment.getRegion()}`)
     .catch(() => {})
     .then(() => import('./app.module'))
--- packages/manager/apps/web-paas/src/index.js
+++ packages/manager/apps/web-paas/src/index.js
@@ -3,10 +3,11 @@ import 'script-loader!jquery';
 /* eslint-enable import/no-webpack-loader-syntax */
 import 'whatwg-fetch';
 import { bootstrapApplication } from '@ovh-ux/manager-core';
+import { defineApplicationVersion } from '@ovh-ux/request-tagger';
+
+defineApplicationVersion(__VERSION__);
 
 bootstrapApplication('web-paas').then((environment) => {
-  environment.setVersion(__VERSION__);
-
   import(`./config-${environment.getRegion()}`)
     .catch(() => {})
     .then(() => import('./app.module'))
