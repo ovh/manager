98c1e3edcf4d4284b33bcbf611ebf323b5e1fd95
Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
2022-12-14 08:48:11 +0100

    feat(storage.cold-archive): add getArchiveEndpoint func to service
    ref: MANAGER-10579
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>

--- packages/manager/modules/pci/src/projects/project/storages/cold-archive/cold-archive.service.js
+++ packages/manager/modules/pci/src/projects/project/storages/cold-archive/cold-archive.service.js
@@ -169,6 +169,10 @@ export default class PciStoragesColdArchiveService {
       );
   }
 
+  getArchiveEndpoint(serviceName, regionName) {
+    return this.$http.get(`/cloud/project/${serviceName}/region/${regionName}`);
+  }
+
   createArchiveContainer(serviceName, regionName, coldArchive) {
     return this.$http
       .post(
