939c95d0f287926e4fcedc4238d644e117bf1966
Florian Renaut <florian.renaut@corp.ovh.com>
2022-12-20 14:18:35 +0100

    feat(core-application): add tanstack query
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/core/application/src/index.tsx
+++ packages/manager/core/application/src/index.tsx
@@ -4,6 +4,7 @@ import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { ApplicationId } from '@ovh-ux/manager-config';
 import { UIKitTheme } from '@ovh-ux/manager-themes';
 import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import './vite-hmr';
 import OvhApplication from './ovh-application';
@@ -37,13 +38,16 @@ export function startApplication(
 ) {
   const root = createRoot(createContainerElement());
   const appRouter = createAppRouter();
+  const queryClient = new QueryClient();
   root.render(
     <React.StrictMode>
-      <ChakraProvider theme={theme}>
-        <OvhApplication name={appName}>
-          <RouterProvider router={appRouter} />
-        </OvhApplication>
-      </ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider theme={theme}>
+          <OvhApplication name={appName}>
+            <RouterProvider router={appRouter} />
+          </OvhApplication>
+        </ChakraProvider>
+      </QueryClientProvider>
     </React.StrictMode>,
   );
 }
