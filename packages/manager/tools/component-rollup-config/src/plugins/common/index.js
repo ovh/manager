365ab8ed3c1e8c77eb56184b9a57effd885cf371
stif59100 <steeve.vanderstocken@ovhcloud.com>
2022-12-22 17:07:04 +0100

    fix(*): fix the ampersand coding issue in translations
    ref: DTRSD-99782
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

--- packages/manager/tools/component-rollup-config/src/plugins/common/index.js
+++ packages/manager/tools/component-rollup-config/src/plugins/common/index.js
@@ -4,6 +4,7 @@ const translationNormalize = (text) => {
       .toString()
       .replace(/&#13;\n/g, ' ') // carriage returns
       .replace(/&#160;/g, ' ') // spaces
+      .replace(/&amp;/g, '&')
       .replace(/\{(\s*\d\s*)\}/g, '{{t$1}}'); // {0} => {{t0}}
     return filtered;
   }
