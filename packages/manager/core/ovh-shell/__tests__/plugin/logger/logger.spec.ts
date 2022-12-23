e089e9359e641204c9569c2cf778c53e2cd25bbd
Jisay <jean-christophe.alleman@corp.ovh.com>
2022-12-23 10:39:05 +0100

    feat(core-shell): add debug to logger plugin
    Signed-off-by: Jisay <jean-christophe.alleman@corp.ovh.com>

--- packages/manager/core/ovh-shell/__tests__/plugin/logger/logger.spec.ts
+++ packages/manager/core/ovh-shell/__tests__/plugin/logger/logger.spec.ts
@@ -24,6 +24,7 @@ defineFeature(feature, (test) => {
   const infoSpy = jest.spyOn(console, 'info');
   const warnSpy = jest.spyOn(console, 'warn');
   const errorSpy = jest.spyOn(console, 'error');
+  const debugSpy = jest.spyOn(console, 'debug');
 
   test('Enable the plugin', ({ given, and, when, then }) => {
     const objectParam = {
@@ -39,11 +40,12 @@ defineFeature(feature, (test) => {
       localStorage.setItem('MANAGER_SHELL_DEBUG', 'true');
     });
 
-    when('I try to log, info, warn, error something', () => {
+    when('I try to log, info, warn, error, debug something', () => {
       loggerPlugin.log('log');
       loggerPlugin.info('info', objectParam);
       loggerPlugin.warn('warn');
       loggerPlugin.error('error', errorParam);
+      loggerPlugin.debug('debug');
     });
 
     then('I should see the result in console', () => {
@@ -51,17 +53,19 @@ defineFeature(feature, (test) => {
       expect(infoSpy).toHaveBeenCalledWith('info', objectParam);
       expect(warnSpy).toHaveBeenCalledWith('warn');
       expect(errorSpy).toHaveBeenCalledWith('error', errorParam);
+      expect(debugSpy).toHaveBeenCalledWith('debug');
     });
   });
 
   test('Disable the plugin', ({ given, when, then }) => {
     givenLoggerPluginInstanciated(given);
 
-    when('I try to log, info, warn, error something', () => {
+    when('I try to log, info, warn, error, debug something', () => {
       loggerPlugin.log('log');
       loggerPlugin.info('info');
       loggerPlugin.warn('warn');
       loggerPlugin.error('error');
+      loggerPlugin.debug('debug');
     });
 
     then('I should not see anything in the console', () => {
@@ -69,6 +73,7 @@ defineFeature(feature, (test) => {
       expect(infoSpy).not.toHaveBeenCalled();
       expect(warnSpy).not.toHaveBeenCalled();
       expect(errorSpy).not.toHaveBeenCalled();
+      expect(debugSpy).not.toHaveBeenCalled();
     });
   });
 });
