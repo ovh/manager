a53cc3631bfbca5df29003ee4d176e692c4227ef
brofik <10513484+brofik@users.noreply.github.com>
2023-01-05 14:18:52 +0100

    feat(project.new): add a voucher banner when a voucher is claimed in the new project view (#8689)
    ref: MANAGER-9862
    
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
    Signed-off-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/pci/src/projects/new/config/component.js
+++ packages/manager/modules/pci/src/projects/new/config/component.js
@@ -15,6 +15,7 @@ export default {
     summary: '<',
     getSummary: '<',
     trackClick: '<',
+    trackPage: '<',
     projectsLink: '<',
     setCartProjectItem: '<',
     onProgressStepClick: '<',
--- packages/manager/modules/pci/src/projects/project/project.component.js
+++ packages/manager/modules/pci/src/projects/project/project.component.js
@@ -23,6 +23,7 @@ export default {
     goToRegion: '<',
     onCreateProjectClick: '<',
     guideUrl: '<',
+    vouchersCreditDetails: '<',
     guideTrackingSectionTags: '<',
     trackClick: '<',
     isMenuSidebarVisible: '<',
