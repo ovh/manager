2d091dac64f0b70c5ce9d37a2025066e51222ff0
Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>
2023-01-06 09:08:18 +0100

    refactor(pci.storage.cold-archive): manage guides
    ref: MANAGER-9188
    
    Signed-off-by: Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/cold-archive/containers/containers.constants.js
+++ packages/manager/modules/pci/src/projects/project/storages/cold-archive/containers/containers.constants.js
@@ -43,7 +43,51 @@ export const CONTAINER_STATUS_OPTIONS = {
   },
 };
 
+export const GUIDES = [
+  {
+    id: 'public-cloud-create-project',
+    links: {
+      DEFAULT:
+        'https://docs.ovh.com/gb/en/public-cloud/premiers-pas-instance-public-cloud/',
+      CA:
+        'https://docs.ovh.com/ca/en/public-cloud/premiers-pas-instance-public-cloud/',
+      DE:
+        'https://docs.ovh.com/de/public-cloud/premiers-pas-instance-public-cloud/',
+      ES:
+        'https://docs.ovh.com/es/public-cloud/premiers-pas-instance-public-cloud/',
+      FR:
+        'https://docs.ovh.com/fr/public-cloud/premiers-pas-instance-public-cloud/',
+      GB:
+        'https://docs.ovh.com/gb/en/public-cloud/premiers-pas-instance-public-cloud/',
+      It:
+        'https://docs.ovh.com/it/public-cloud/premiers-pas-instance-public-cloud/',
+      PL:
+        'https://docs.ovh.com/pl/public-cloud/premiers-pas-instance-public-cloud/',
+      PT:
+        'https://docs.ovh.com/pt/public-cloud/premiers-pas-instance-public-cloud/',
+      QC:
+        'https://docs.ovh.com/ca/fr/public-cloud/premiers-pas-instance-public-cloud/',
+    },
+  },
+  {
+    id: 'storage-backup',
+    links: {
+      DEFAULT: 'https://docs.ovh.com/gb/en/storage/',
+      CA: 'https://docs.ovh.com/ca/en/storage/',
+      DE: 'https://docs.ovh.com/de/storage/',
+      ES: 'https://docs.ovh.com/es/storage/',
+      FR: 'https://docs.ovh.com/fr/storage/',
+      GB: 'https://docs.ovh.com/gb/en/storage/',
+      IT: 'https://docs.ovh.com/it/storage/',
+      PL: 'https://docs.ovh.com/pl/storage/',
+      PT: 'https://docs.ovh.com/pt/storage/',
+      QC: 'https://docs.ovh.com/ca/fr/storage/',
+    },
+  },
+];
+
 export default {
+  GUIDES,
   CONTAINER_STATUS_OPTIONS,
   COLD_ARCHIVE_STATES,
 };
