53d314401cb401caf5d4b95deb9cc46f66716fd1
Hyenne Guillaume <guillaume.hyenne@ovhcloud.com>
2023-01-10 16:55:06 +0100

    fix(dedicated.license): cpanel translation missing for solo and plus (#8678)
    ref: MANAGER-10372
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/license/order/license-order.constants.js
+++ packages/manager/apps/dedicated/client/app/license/order/license-order.constants.js
@@ -9,15 +9,27 @@ export const CPANEL_ADMIN_TYPE = 'admin-cloud';
 
 export const CPANEL_PRO_TYPE = 'pro-cloud';
 
-export const MAX_CPANEL_PRO_ACCOUNTS = 30;
+export const CPANEL_SOLO_TYPE = 'solo-cloud';
+
+export const CPANEL_PLUS_TYPE = 'plus-cloud';
+
+export const MAX_CPANEL_SOLO_ACCOUNTS = 1;
 
 export const MAX_CPANEL_ADMIN_ACCOUNTS = 5;
 
+export const MAX_CPANEL_PRO_ACCOUNTS = 30;
+
+export const MAX_CPANEL_PLUS_ACCOUNTS = 50;
+
 export default {
   CPANEL_PREMIER_TYPES,
+  CPANEL_SOLO_TYPE,
+  CPANEL_PLUS_TYPE,
   CPANEL_ADMIN_TYPE,
   CPANEL_PRO_TYPE,
   MAX_CPANEL_PRO_ACCOUNTS,
   MAX_CPANEL_ADMIN_ACCOUNTS,
+  MAX_CPANEL_SOLO_ACCOUNTS,
+  MAX_CPANEL_PLUS_ACCOUNTS,
   LICENCE_TYPES,
 };
