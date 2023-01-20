cb8d52197eb5e816d249b2ea550b3562da400a44
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2023-01-20 11:32:59 +0100

    fix: pr comments
    DATATR-69
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/ai-dashboard/tabs/home/index.js
+++ packages/manager/modules/pci/src/projects/project/ai-dashboard/tabs/home/index.js
@@ -1,25 +1,22 @@
 import angular from 'angular';
 import '@uirouter/angularjs';
 import 'oclazyload';
-import '@ovh-ux/manager-core';
 
 const moduleName = 'ovhManagerPciAiDashboardHomeLazyLoading';
 
-angular
-  .module(moduleName, ['ui.router', 'ovhManagerCore', 'oc.lazyLoad'])
-  .config(
-    /* @ngInject */ ($stateProvider) => {
-      $stateProvider.state('pci.projects.project.ai-dashboard.home.**', {
-        url: '/home',
-        lazyLoad: ($transition$) => {
-          const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
+  /* @ngInject */ ($stateProvider) => {
+    $stateProvider.state('pci.projects.project.ai-dashboard.home.**', {
+      url: '/home',
+      lazyLoad: ($transition$) => {
+        const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
 
-          return import('./home.module').then((mod) =>
-            $ocLazyLoad.inject(mod.default || mod),
-          );
-        },
-      });
-    },
-  );
+        return import('./home.module').then((mod) =>
+          $ocLazyLoad.inject(mod.default || mod),
+        );
+      },
+    });
+  },
+);
 
 export default moduleName;
