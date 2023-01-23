481a260a05939ec0a655aed3f32adf6467c564fc
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2023-01-23 12:39:31 +0100

    fix: ssl and backup translations (#8697)
    ref: DATATR-169
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/modules/pci/src/components/project/storages/databases/databases.constants.js
+++ packages/manager/modules/pci/src/components/project/storages/databases/databases.constants.js
@@ -8,15 +8,6 @@ export const STATUS = {
   UPDATING: 'UPDATING',
 };
 
-export const SSL_MODE_REQUIRED = ['require', 'required', 'REQUIRED'];
-
-export const SSL_MODE_NA = ['n/a'];
-
-export const SSL_MODE_SSL_TLS = ['TLS/SSL'];
-
 export default {
   STATUS,
-  SSL_MODE_REQUIRED,
-  SSL_MODE_NA,
-  SSL_MODE_SSL_TLS,
 };
