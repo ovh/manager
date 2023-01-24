74d2e584d7f657adbdf119ed5d9253a1a966dbd6
Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
2023-01-24 10:05:03 +0100

    feat(storage.cold-archive): update rclone guides
    ref: MANAGER-10380
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>

--- packages/manager/modules/pci/src/projects/project/storages/cold-archive/users/download-rclone/download-rclone.constants.js
+++ packages/manager/modules/pci/src/projects/project/storages/cold-archive/users/download-rclone/download-rclone.constants.js
@@ -7,15 +7,26 @@ export const COLD_ARCHIVE_STATES = STATES;
 export const COLD_ARCHIVE_DEFAULT_REGION = DEFAULT_REGION;
 
 export const RCLONE_GUIDE = {
-  DEFAULT: 'https://docs.ovh.com/gb/en/storage/object-storage/s3/rclone/',
-  DE: 'https://docs.ovh.com/de/storage/object-storage/s3/rclone/',
-  ES: 'https://docs.ovh.com/es/storage/object-storage/s3/rclone/',
+  DEFAULT: 'https://docs.ovh.com/us/en/storage/object-storage/s3/rclone/',
   FR: 'https://docs.ovh.com/fr/storage/object-storage/s3/rclone/',
-  CA: 'https://docs.ovh.com/ca/storage/object-storage/s3/rclone/',
-  EN: 'https://docs.ovh.com/gb/en/storage/object-storage/s3/rclone/',
+  MA: 'https://docs.ovh.com/fr/storage/object-storage/s3/rclone/',
+  TN: 'https://docs.ovh.com/fr/storage/object-storage/s3/rclone/',
+  SN: 'https://docs.ovh.com/fr/storage/object-storage/s3/rclone/',
+  QC: 'https://docs.ovh.com/ca/fr/storage/object-storage/s3/rclone/',
+  GB: 'https://docs.ovh.com/gb/en/storage/object-storage/s3/rclone/',
+  IE: 'https://docs.ovh.com/ie/en/storage/object-storage/s3/rclone/',
+  WE: 'https://docs.ovh.com/us/en/storage/object-storage/s3/rclone/',
+  CA: 'https://docs.ovh.com/ca/en/storage/object-storage/s3/rclone/',
+  AU: 'https://docs.ovh.com/au/en/storage/object-storage/s3/rclone/',
+  SG: 'https://docs.ovh.com/sg/en/storage/object-storage/s3/rclone/',
+  ASIA: 'https://docs.ovh.com/asia/en/storage/object-storage/s3/rclone/',
+  ES: 'https://docs.ovh.com/es/storage/object-storage/s3/rclone/',
+  WS: 'https://docs.ovh.com/us/es/storage/object-storage/s3/rclone/',
+  PT: 'https://docs.ovh.com/pt/storage/object-storage/s3/rclone/',
   IT: 'https://docs.ovh.com/it/storage/object-storage/s3/rclone/',
   PL: 'https://docs.ovh.com/pl/storage/object-storage/s3/rclone/',
-  PT: 'https://docs.ovh.com/pt/storage/object-storage/s3/rclone/',
+  DE: 'https://docs.ovh.com/de/storage/object-storage/s3/rclone/',
+  NL: 'https://docs.ovh.com/gb/en/storage/object-storage/s3/rclone/',
 };
 
 export const DOWNLOAD_FILENAME = 'rclone.conf';
