fafaa29ebbc4f9b439fb8bcd1f5c85987cefdcfb
Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>
2023-01-25 15:18:56 +0100

    feat(volume-backup): init volume-backup product
    ref: MANAGER-7536
    
    Signed-off-by: Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>

---
+++ packages/manager/modules/pci/src/projects/project/storages/volume-backup/add/index.js
@@ -0,0 +1,27 @@
+import angular from 'angular';
+import '@uirouter/angularjs';
+import 'oclazyload';
+
+import '@ovh-ux/manager-core';
+import { VOLUME_BACKUP_ROUTES } from '../volume-backups.constants';
+
+const moduleName = 'ovhManagerPciProjectStoragesVolumeBackupAddLazyloading';
+
+angular
+  .module(moduleName, ['ui.router', 'ovhManagerCore', 'oc.lazyLoad'])
+  .config(
+    /* @ngInject */ ($stateProvider) => {
+      $stateProvider.state(`${VOLUME_BACKUP_ROUTES.ADD.STATE}.**`, {
+        url: VOLUME_BACKUP_ROUTES.ADD.URL,
+        lazyLoad: ($transition$) => {
+          const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+
+          return import('./add.module').then((mod) =>
+            $ocLazyLoad.inject(mod.default || mod),
+          );
+        },
+      });
+    },
+  );
+
+export default moduleName;
---
+++ packages/manager/modules/pci/src/projects/project/storages/volume-backup/volume-backups.module.js
@@ -0,0 +1,34 @@
+import angular from 'angular';
+import '@uirouter/angularjs';
+import 'angular-translate';
+import '@ovh-ux/ng-translate-async-loader';
+import '@ovh-ux/ng-ovh-cloud-universe-components';
+import '@ovh-ux/ui-kit';
+
+import routing from './volume-backups.routing';
+import component from './volume-backups.component';
+import service from './volume-backup.service';
+
+import addVolumeBackup from './add';
+import dashboard from './dashboard';
+import onboarding from './onboarding';
+
+const moduleName = 'ovhManagerPciProjectStoragesVolumeBackup';
+
+angular
+  .module(moduleName, [
+    'ui.router',
+    'ngTranslateAsyncLoader',
+    'pascalprecht.translate',
+    'oui',
+    'ngOvhCloudUniverseComponents',
+    onboarding,
+    addVolumeBackup,
+    dashboard,
+  ])
+  .config(routing)
+  .component('ovhManagerPciProjectsProjectStoragesVolumeBackup', component)
+  .service('VolumeBackupService', service)
+  .run(/* @ngTranslationsInject:json ./translations */);
+
+export default moduleName;
