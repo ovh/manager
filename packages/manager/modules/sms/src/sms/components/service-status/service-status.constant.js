70b4c91dbfd115514ff88bcfe904ed37c9ba1d9c
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2023-01-26 16:49:17 +0100

    fix(telecom.sms): fix the status label and tooltip
    ref: MANAGER-10821
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/modules/sms/src/sms/components/service-status/service-status.constant.js
+++ packages/manager/modules/sms/src/sms/components/service-status/service-status.constant.js
@@ -1,15 +1,19 @@
 export const SMPP_STATUS = {
+  DELETED: 'DELETED',
   ERROR: 'ERROR',
-  OK: 'OK',
-  INSTALLING: 'INSTALLING',
-  UPDATING_IPS: 'UPDATING_IPS',
+  SUCCESS: 'SUCCESS',
+  UNKNOWN: 'UNKNOWN',
+  UPDATING_IP: 'UPDATING_IP',
+  WAITING_IP: 'WAITING_IP',
 };
 
 export const SMPP_BADAGES_STATUS = {
+  [SMPP_STATUS.DELETED]: 'oui-badge_error',
   [SMPP_STATUS.ERROR]: 'oui-badge_error',
-  [SMPP_STATUS.OK]: 'oui-badge_success',
-  [SMPP_STATUS.INSTALLING]: 'oui-badge_info',
-  [SMPP_STATUS.UPDATING_IPS]: 'oui-badge_info',
+  [SMPP_STATUS.SUCCESS]: 'oui-badge_success',
+  [SMPP_STATUS.UNKNOWN]: 'oui-badge_error',
+  [SMPP_STATUS.UPDATING_IP]: 'oui-badge_info',
+  [SMPP_STATUS.WAITING_IP]: 'oui-badge_info',
 };
 
 export default {
