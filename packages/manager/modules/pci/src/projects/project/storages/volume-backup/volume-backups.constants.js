e7cece677a362a6e6117afd78ef6e63694df26ea
Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>
2023-01-26 14:39:14 +0100

    refactor(volume-backup): improvement
    ref: MANAGER-10568
    
    Signed-off-by: Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/volume-backup/volume-backups.constants.js
+++ packages/manager/modules/pci/src/projects/project/storages/volume-backup/volume-backups.constants.js
@@ -31,9 +31,32 @@ export const VOLUME_BACKUP_POLLER_NAMESPACES = {
 
 export const VOLUME_BACKUP_DEFAULT_REGION = 'RBX';
 
+export const VOLUME_BACKUP_TRACKING = {
+  PREFIX: 'volume-backup',
+};
+
+export const GUIDES = [
+  {
+    id: 'overview',
+    links: {
+      DEFAULT: 'https://docs.ovh.com/en-gb/public-cloud/volume-backup/',
+      ES: 'https://docs.ovh.com/es/public-cloud/volume-backup/',
+      FR: 'https://docs.ovh.com/fr/public-cloud/volume-backup/',
+      GB: 'https://docs.ovh.com/en-gb/public-cloud/volume-backup/',
+      IT: 'https://docs.ovh.com/it/public-cloud/volume-backup/',
+      NL: 'https://docs.ovh.com/nl/public-cloud/volume-backup/',
+      PL: 'https://docs.ovh.com/pl/public-cloud/volume-backup/',
+      PT: 'https://docs.ovh.com/pt/public-cloud/volume-backup/',
+      US: 'https://docs.ovh.com/en-us/public-cloud/volume-backup/',
+    },
+  },
+];
+
 export default {
   VOLUME_BACKUP_ROUTES,
   VOLUME_BACKUP_STATUS,
   VOLUME_BACKUP_POLLER_NAMESPACES,
   VOLUME_BACKUP_DEFAULT_REGION,
+  VOLUME_BACKUP_TRACKING,
+  GUIDES,
 };
