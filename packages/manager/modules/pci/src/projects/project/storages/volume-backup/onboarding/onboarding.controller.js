fdbb690d92e4f5aa7b53aee3c7bbf881df434532
Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>
2023-01-26 14:42:35 +0100

    feat(volume-backup): onboarding page
    ref: MANAGER-10568
    
    Signed-off-by: Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>

---
+++ packages/manager/modules/pci/src/projects/project/storages/volume-backup/onboarding/onboarding.controller.js
@@ -0,0 +1,39 @@
+import reduce from 'lodash/reduce';
+
+import { GUIDES } from '../volume-backups.constants';
+
+export default class PciStorageColdArchivesOnboardingController {
+  /* @ngInject */
+  constructor($translate, coreConfig) {
+    this.$translate = $translate;
+    this.coreConfig = coreConfig;
+  }
+
+  $onInit() {
+    this.guides = reduce(
+      GUIDES,
+      (list, guide) => [
+        ...list,
+        {
+          ...guide,
+          title: this.$translate.instant(
+            `pci_projects_project_storages_volume_backup_guides_${guide.id}_title`,
+          ),
+          description: this.$translate.instant(
+            `pci_projects_project_storages_volume_backup_guides_${guide.id}_description`,
+          ),
+          link:
+            guide.links[this.coreConfig.getUserLanguage().toUpperCase()] ||
+            guide.links.DEFAULT,
+        },
+      ],
+      [],
+    );
+  }
+
+  onAddVolumeBlockStorageClick() {
+    // TODO: tracking
+
+    this.goToAddVolumeBlockStorage();
+  }
+}
---
+++ packages/manager/modules/pci/src/projects/project/storages/volume-backup/onboarding/onboarding.routing.js
@@ -0,0 +1,28 @@
+import {
+  VOLUME_BACKUP_ROUTES,
+  VOLUME_BACKUP_TRACKING,
+} from '../volume-backups.constants';
+
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state(VOLUME_BACKUP_ROUTES.ONBOARDING.STATE, {
+    url: VOLUME_BACKUP_ROUTES.ONBOARDING.URL,
+    component: 'ovhManagerPciProjectsProjectStoragesVolumeBackupOnboarding',
+    atInternet: {
+      rename: `${VOLUME_BACKUP_TRACKING.PREFIX}`,
+    },
+    redirectTo: (transition) =>
+      transition
+        .injector()
+        .getAsync('volumeBackups')
+        .then((volumeBackups) =>
+          volumeBackups.length > 0
+            ? {
+                state: VOLUME_BACKUP_ROUTES.ROOT.STATE,
+              }
+            : false,
+        ),
+    resolve: {
+      breadcrumb: () => null, // Hide breadcrumb
+    },
+  });
+};
