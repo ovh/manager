185bc5035e5a9b5497ee8cd4c387804f0a5b040c
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2023-01-27 11:12:22 +0100

    feat(pci.ai-dashboard): datatr-69 - home page ai dashboard (#8172)
    * feat: add home page
    
    DATATR-69
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/ai-dashboard/tabs/home/home.module.js
+++ packages/manager/modules/pci/src/projects/project/ai-dashboard/tabs/home/home.module.js
@@ -7,14 +7,17 @@ import '@ovh-ux/ui-kit';
 import component from './home.component';
 import routing from './home.routing';
 
+import aiGuideComponent from '../../components/ai-guide';
+
 const moduleName = 'ovhManagerPciAiDashboardHome';
 
 angular
   .module(moduleName, [
-    'ui.router',
-    'oui',
     'ngTranslateAsyncLoader',
     'pascalprecht.translate',
+    'oui',
+    'ui.router',
+    aiGuideComponent,
   ])
   .config(routing)
   .component('pciProjectAiDashboardHome', component)
--- packages/manager/modules/pci/src/projects/project/ai-dashboard/tabs/home/home.routing.js
+++ packages/manager/modules/pci/src/projects/project/ai-dashboard/tabs/home/home.routing.js
@@ -6,6 +6,44 @@ export default /* @ngInject */ ($stateProvider) => {
     },
     resolve: {
       breadcrumb: /* @ngInject */ () => null, // Hide breadcrumb
+      homeAIGuides: /* @ngInject */ (
+        AiDashboardService,
+        projectId,
+        coreConfig,
+      ) =>
+        AiDashboardService.getGuides(
+          projectId,
+          coreConfig
+            .getUserLocale()
+            .replace('_', '-')
+            .toLowerCase(),
+        ),
+      billing: /* @ngInject */ (AiDashboardService, projectId) =>
+        AiDashboardService.getBilling(projectId),
+      goToObjectStorage: /* @ngInject */ ($state, projectId) => () =>
+        $state.go('pci.projects.project.storages.object-storage', {
+          projectId,
+        }),
+      goToAINotebooks: /* @ngInject */ ($state, projectId) => () =>
+        $state.go('pci.projects.project.notebooks', {
+          projectId,
+        }),
+      goToAITraining: /* @ngInject */ ($state, projectId) => () =>
+        $state.go('pci.projects.project.training', {
+          projectId,
+        }),
+      goToAIDeploy: /* @ngInject */ ($state, projectId) => () =>
+        $state.go('pci.projects.project.ai', {
+          projectId,
+        }),
+      goToBilling: /* @ngInject */ ($state, projectId) => () =>
+        $state.go('pci.projects.project.billing', {
+          projectId,
+        }),
+      goToUsersAndTokens: /* @ngInject */ ($state, projectId) => () =>
+        $state.go('pci.projects.project.ai-dashboard.users-tokens', {
+          projectId,
+        }),
     },
   });
 };
