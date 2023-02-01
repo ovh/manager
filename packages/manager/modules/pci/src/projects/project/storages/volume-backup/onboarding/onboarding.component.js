acc4380202a8ce3b5cae55139c2c1f27fd5d8b98
Mohammed ZAHAF <22635766+mohammed-zahaf@users.noreply.github.com>
2023-02-01 12:24:13 +0100

    feat(volume-backup): volume backup guides (#8834)
    ref: MANAGER-10573
    
    Signed-off-by: Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/volume-backup/onboarding/onboarding.component.js
+++ packages/manager/modules/pci/src/projects/project/storages/volume-backup/onboarding/onboarding.component.js
@@ -5,6 +5,7 @@ export default {
   controller,
   template,
   bindings: {
+    guides: '<',
     trackClick: '<',
     goToAddVolumeBlockStorage: '<',
   },
--- packages/manager/modules/pci/src/projects/project/storages/volume-backup/volume-backup.component.js
+++ packages/manager/modules/pci/src/projects/project/storages/volume-backup/volume-backup.component.js
@@ -8,6 +8,7 @@ export default {
     projectId: '<',
     pciFeatureRedirect: '<',
     volumeBackups: '<',
+    guides: '<',
     goBack: '<',
     goToAddVolumeBlockStorage: '<',
     reloadState: '<',
