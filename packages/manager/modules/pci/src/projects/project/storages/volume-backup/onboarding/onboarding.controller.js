15c257b0c3cbff28eb97050dbc30ae148414efb1
Mohammed ZAHAF <22635766+mohammed-zahaf@users.noreply.github.com>
2023-02-01 12:24:13 +0100

    feat(volume-backup): volume backup guides (#8834)
    ref: MANAGER-10573
    
    Signed-off-by: Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/volume-backup/onboarding/onboarding.controller.js
+++ packages/manager/modules/pci/src/projects/project/storages/volume-backup/onboarding/onboarding.controller.js
@@ -1,34 +1,8 @@
-import reduce from 'lodash/reduce';
-
-import { GUIDES } from '../volume-backup.constants';
-
 export default class PciStorageColdArchivesOnboardingController {
   /* @ngInject */
   constructor($translate, coreConfig) {
     this.$translate = $translate;
     this.coreConfig = coreConfig;
-  }
-
-  $onInit() {
-    this.guides = reduce(
-      GUIDES,
-      (list, guide) => [
-        ...list,
-        {
-          ...guide,
-          title: this.$translate.instant(
-            `pci_projects_project_storages_volume_backup_guides_${guide.id}_title`,
-          ),
-          description: this.$translate.instant(
-            `pci_projects_project_storages_volume_backup_guides_${guide.id}_description`,
-          ),
-          link:
-            guide.links[this.coreConfig.getUserLanguage().toUpperCase()] ||
-            guide.links.DEFAULT,
-        },
-      ],
-      [],
-    );
   }
 
   onAddVolumeBlockStorageClick() {
