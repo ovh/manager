aa63820d3c0054713bb85d4fe5edd76693b9faf1
Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>
2023-02-03 09:50:45 +0100

    feat(volume-backup): create volume backup page
    ref: MANAGER-10571
    
    Signed-off-by: Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/volume-backup/create/index.js
+++ packages/manager/modules/pci/src/projects/project/storages/volume-backup/create/index.js
@@ -1,22 +1,22 @@
 import angular from 'angular';
 import '@uirouter/angularjs';
 import 'oclazyload';
-
 import '@ovh-ux/manager-core';
+
 import { VOLUME_BACKUP_ROUTES } from '../volume-backup.constants';
 
-const moduleName = 'ovhManagerPciProjectStoragesVolumeBackupAddLazyloading';
+const moduleName = 'ovhManagerPciProjectStoragesVolumeBackupCreateLazyloading';
 
 angular
   .module(moduleName, ['ui.router', 'ovhManagerCore', 'oc.lazyLoad'])
   .config(
     /* @ngInject */ ($stateProvider) => {
-      $stateProvider.state(`${VOLUME_BACKUP_ROUTES.ADD.STATE}.**`, {
-        url: VOLUME_BACKUP_ROUTES.ADD.URL,
+      $stateProvider.state(`${VOLUME_BACKUP_ROUTES.CREATE.STATE}.**`, {
+        url: VOLUME_BACKUP_ROUTES.CREATE.URL,
         lazyLoad: ($transition$) => {
           const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
 
-          return import('./add.module').then((mod) =>
+          return import('./create.module').then((mod) =>
             $ocLazyLoad.inject(mod.default || mod),
           );
         },
--- packages/manager/modules/pci/src/projects/project/storages/volume-backup/list/list.component.js
+++ packages/manager/modules/pci/src/projects/project/storages/volume-backup/list/list.component.js
@@ -11,6 +11,7 @@ export default {
     guides: '<',
     goToRestoreVolume: '<',
     goToCreateVolume: '<',
+    goToCreateVolumeBackup: '<',
     goToDeleteVolumeBackup: '<',
     goBack: '<',
     goToAddVolumeBlockStorage: '<',
