2887b13e8eeb423a59b1db4f8b60984aaba43fb0
Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
2023-02-07 06:51:27 +0100

    feat(storage.cold-archive): use constants for routing state labels
    ref: MANAGER-10677
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>

--- packages/manager/modules/pci/src/projects/project/storages/cold-archive/onboarding/onboarding.routing.js
+++ packages/manager/modules/pci/src/projects/project/storages/cold-archive/onboarding/onboarding.routing.js
@@ -1,33 +1,32 @@
-import { COLD_ARCHIVE_TRACKING } from '../cold-archives.constants';
+import {
+  COLD_ARCHIVE_TRACKING,
+  COLD_ARCHIVE_STATES,
+} from '../cold-archives.constants';
 
 export default /* @ngInject */ ($stateProvider) => {
-  $stateProvider.state(
-    'pci.projects.project.storages.cold-archive.onboarding',
-    {
-      url: '/onboarding',
-      views: {
-        'coldArchiveView@pci.projects.project.storages.cold-archive': {
-          component: 'pciProjectStorageColdArchiveOnboarding',
-        },
-      },
-      atInternet: {
-        rename: `${COLD_ARCHIVE_TRACKING.PAGE_PREFIX}::${COLD_ARCHIVE_TRACKING.ONBOARDING.MAIN}`,
-      },
-      redirectTo: (transition) =>
-        transition
-          .injector()
-          .getAsync('containers')
-          .then((containers) =>
-            containers.length > 0
-              ? {
-                  state:
-                    'pci.projects.project.storages.cold-archive.containers',
-                }
-              : false,
-          ),
-      resolve: {
-        breadcrumb: () => null, // Hide breadcrumb
+  $stateProvider.state(COLD_ARCHIVE_STATES.ONBOARDING, {
+    url: '/onboarding',
+    views: {
+      'coldArchiveView@pci.projects.project.storages.cold-archive': {
+        component: 'pciProjectStorageColdArchiveOnboarding',
       },
     },
-  );
+    atInternet: {
+      rename: `${COLD_ARCHIVE_TRACKING.PAGE_PREFIX}::${COLD_ARCHIVE_TRACKING.ONBOARDING.MAIN}`,
+    },
+    redirectTo: (transition) =>
+      transition
+        .injector()
+        .getAsync('containers')
+        .then((containers) =>
+          containers.length > 0
+            ? {
+                state: COLD_ARCHIVE_STATES.CONTAINERS,
+              }
+            : false,
+        ),
+    resolve: {
+      breadcrumb: () => null, // Hide breadcrumb
+    },
+  });
 };
