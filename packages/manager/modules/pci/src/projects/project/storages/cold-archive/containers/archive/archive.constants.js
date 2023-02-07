db6c8649cde4ad8daeb3a2107c0ab3ece56e126c
Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
2023-02-07 07:17:17 +0100

    feat(storage.cold-archive): remove redundant constants from containers
    ref: MANAGER-10677
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>

--- packages/manager/modules/pci/src/projects/project/storages/cold-archive/containers/archive/archive.constants.js
+++
@@ -1,7 +0,0 @@
-import {
-  COLD_ARCHIVE_STATES as STATES,
-  REGION as DEFAULT_REGION,
-} from '../../cold-archives.constants';
-
-export const COLD_ARCHIVE_STATES = STATES;
-export const COLD_ARCHIVE_DEFAULT_REGION = DEFAULT_REGION;
