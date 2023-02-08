1d1e50d5886b6399625f5b39639edb27375dcc82
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2023-02-08 09:14:01 +0100

    feat(iam): add entry menu
    ref: MANAGER-10393
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/modules/iam/src/index.js
+++ packages/manager/modules/iam/src/index.js
@@ -1,15 +1,19 @@
 import angular from 'angular';
+import angularTranslate from 'angular-translate';
+import uiRouter from '@uirouter/angularjs';
 
-import '@uirouter/angularjs';
-import 'angular-translate';
+import ngOvhFeatureFlipping from '@ovh-ux/ng-ovh-feature-flipping';
 
 import routing from './iam.routing';
+import constant from './iam.constant';
 
 const moduleName = 'ovhManagerIAM';
 
 angular
-  .module(moduleName, ['ui.router', 'pascalprecht.translate'])
+  .module(moduleName, [angularTranslate, uiRouter, ngOvhFeatureFlipping])
+  .constant('IAM', constant)
   .config(routing)
-  .run(/* @ngTranslationsInject:json ./translations */);
+  .run(/* @ngTranslationsInject:json ./translations */)
+  .run(/* @ngInject */ ($rootScope, IAM) => Object.assign($rootScope, { IAM }));
 
 export default moduleName;
