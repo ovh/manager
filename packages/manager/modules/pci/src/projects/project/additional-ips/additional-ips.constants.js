48608f6b17754f0e4e2327f3c3ebfb5921b278cf
Anoop N <anoop.n@ovhcloud.com>
2023-02-09 16:33:48 +0530

    fix(pci): use plan code to display regions (#8798)
     Use planCode to display regions while creating Gateway and FloatingIP
     ref: MANAGER-10281
    
    Signed-off-by: Anoop <anoop.n@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/additional-ips/additional-ips.constants.js
+++ packages/manager/modules/pci/src/projects/project/additional-ips/additional-ips.constants.js
@@ -6,7 +6,11 @@ export const TABS = {
 export const TRACKING_PREFIX =
   'PublicCloud::pci::projects::project::additional-ips';
 
+export const FLOATING_IP_HOURLY_PLAN_CODE =
+  'floatingip.floatingip.hour.consumption';
+
 export default {
   TABS,
   TRACKING_PREFIX,
+  FLOATING_IP_HOURLY_PLAN_CODE,
 };
