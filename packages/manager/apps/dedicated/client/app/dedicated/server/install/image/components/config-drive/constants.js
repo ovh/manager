8ea5b42af78a9b2d8c3b1049bd822d0de66ef060
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2023-02-13 08:51:28 +0100

    fix(dedicated): ssh key comment can be anything
    ref: DTRSD-103746
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/dedicated/server/install/image/components/config-drive/constants.js
+++ packages/manager/apps/dedicated/client/app/dedicated/server/install/image/components/config-drive/constants.js
@@ -4,15 +4,15 @@ export const DOCUMENTATION_LINK =
 export const SUPPORTED_SSH_KEY_FORMATS = [
   {
     name: 'RSA',
-    regex: /^(ssh-rsa)\s+(A{4}[0-9A-Za-z +/]+[=]{0,3})\s+(\S+)$/,
+    regex: /^(ssh-rsa)\s+(A{4}[0-9A-Za-z +/]+[=]{0,3})(\s+.*)?$/,
   },
   {
     name: 'ECDSA',
-    regex: /^(ecdsa-sha2-nistp[0-9]+)\s+(A{4}[0-9A-Za-z +/]+[=]{0,3})\s+(\S+)$/,
+    regex: /^(ecdsa-sha2-nistp[0-9]+)\s+(A{4}[0-9A-Za-z +/]+[=]{0,3})(\s+.*)?$/,
   },
   {
     name: 'ED25519',
-    regex: /^(ssh-ed25519)\s+(A{4}[0-9A-Za-z +/]+[=]{0,3})\s+(\S+)$/,
+    regex: /^(ssh-ed25519)\s+(A{4}[0-9A-Za-z +/]+[=]{0,3})(\s+.*)?$/,
   },
 ];
 
