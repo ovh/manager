081142295e998fee5b78dcc16e71b6c5b415e990
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2023-02-13 15:57:25 +0100

    fix: pr comments
    datatr-188
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/databases/database/general-information/add-node/add-node.component.js
+++ packages/manager/modules/pci/src/projects/project/storages/databases/database/general-information/add-node/add-node.component.js
@@ -11,6 +11,7 @@ const component = {
     trackDashboard: '<',
     user: '<',
     engines: '<',
+    getCurrentFlavor: '<',
   },
   template,
   controller,
--- packages/manager/modules/pci/src/projects/project/storages/databases/database/general-information/add-node/add-node.routing.js
+++ packages/manager/modules/pci/src/projects/project/storages/databases/database/general-information/add-node/add-node.routing.js
@@ -13,25 +13,6 @@ export default /* @ngInject */ ($stateProvider) => {
       layout: 'modal',
       resolve: {
         breadcrumb: () => null,
-        price: /* @ngInject */ (getCurrentFlavor, engines, database) => {
-          const currentFlavor = getCurrentFlavor();
-          const flavorPrice = currentFlavor.nodeHourlyPrice;
-          const dbEngine = engines.find(
-            (engine) => engine.name === database.engine,
-          );
-          let addedStorageByNode =
-            database.disk.size - currentFlavor.minDiskSize;
-          // if engine is distributed, the stockage is distributed between the nodes, so to get the
-          // size of for one node, we need to divide by the number of nodes
-          if (dbEngine.isDistributedStorage) {
-            addedStorageByNode /= database.nodeNumber;
-          }
-          const additionalStoragePrice = Math.max(
-            0,
-            addedStorageByNode * currentFlavor.hourlyPricePerGB.priceInUcents,
-          );
-          return flavorPrice.priceInUcents + additionalStoragePrice;
-        },
         onNodeAdd: /* @ngInject */ (
           database,
           getNodeObject,
