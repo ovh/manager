009d34b03d6b9e16e5b2e058586771308e631bac
Nicolas BAPTISTA <nicolas.baptista.ext@corp.ovh.com>
2023-02-22 16:39:18 +0100

    feat(dedicated.nasha): rename POST in UI and partition UI
    ref: MANAGER-10906
    
    Signed-off-by: Nicolas BAPTISTA <nicolas.baptista.ext@corp.ovh.com>

--- packages/manager/apps/nasha-react/src/api/nasha-react/index.ts
+++ packages/manager/apps/nasha-react/src/api/nasha-react/index.ts
@@ -4,10 +4,15 @@ import fetchNashaPartition from './GET/2api/partition';
 import fetchNashaServiceInfos from './GET/apiv6/serviceInfos';
 import fetchNashaDetails from './GET/2api/nashaDetails';
 
+import renameNasha from './PUT/apiv6/renameNasha';
+import createNashaPartition from './POST/apiv6/createPartition';
+
 export {
   fetchNashaDetails,
   fetchNashaPartition,
   fetchNashaServiceInfos,
   fetchNashaList,
+  renameNasha,
+  createNashaPartition,
   SELECTED_NAS,
 };
--- packages/manager/apps/nasha-react/src/pages/details/[serviceName]/index.tsx
+++ packages/manager/apps/nasha-react/src/pages/details/[serviceName]/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useParams, Navigate, Outlet } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { Heading } from '@chakra-ui/react';
 import {
   QueryClient,
@@ -11,6 +11,8 @@ import {
   fetchNashaDetails,
   fetchNashaPartition,
   fetchNashaServiceInfos,
+  renameNasha,
+  createNashaPartition,
 } from '../../../api/nasha-react';
 
 const queryClient = new QueryClient();
@@ -42,6 +44,7 @@ function Partition(props: { serviceName: string }) {
 
 function Informations(props: { serviceName: string }) {
   const { serviceName } = props;
+  const [serviceRename, setServiceRename] = useState(serviceName);
   const { isLoading, isError, data } = useQuery(
     ['informations', { serviceName }],
     fetchNashaDetails,
@@ -59,12 +62,39 @@ function Informations(props: { serviceName: string }) {
   if (count === 0) {
     return <></>;
   }
+
+  const handleClickRename = async () => {
+    try {
+      const response = await renameNasha({
+        queryKey: [
+          'renameService',
+          {
+            serviceName,
+            data: { customName: serviceRename },
+          },
+        ],
+      });
+
+      // handle successful response here
+      // eslint-disable-next-line
+      console.log('Service renamed:', response);
+    } catch (error) {
+      // handle error here
+      // eslint-disable-next-line
+      console.error('Error renaming service:', error);
+    }
+  };
+
   return (
     <>
       <ul>{JSON.stringify(data)}</ul>
       <ul>
-        <input type="text" value={data.serviceName} />
-        <button>Modifier</button>
+        <input
+          type="text"
+          value={serviceRename}
+          onChange={(e) => setServiceRename(e.target.value)}
+        />
+        <button onClick={handleClickRename}>Modifier</button>
       </ul>
     </>
   );
@@ -72,6 +102,18 @@ function Informations(props: { serviceName: string }) {
 
 function Configuration(props: { serviceName: string }) {
   const { serviceName } = props;
+  type PostData = {
+    partitionDescription: string | null;
+    partitionName: string;
+    size: number;
+    protocol: string;
+  };
+  const [partitionData, setPartitionData] = useState<PostData>({
+    partitionDescription: '',
+    partitionName: 'partition-name',
+    size: 10,
+    protocol: 'NFS',
+  });
   const { isLoading, isError, data } = useQuery(
     ['informations', { serviceName }],
     fetchNashaDetails,
@@ -89,13 +131,69 @@ function Configuration(props: { serviceName: string }) {
   if (count === 0) {
     return <></>;
   }
+
+  const handleClickPartition = async () => {
+    try {
+      const response = await createNashaPartition({
+        queryKey: [
+          'createPartition',
+          {
+            serviceName,
+            data: partitionData,
+          },
+        ],
+      });
+      // handle successful response here
+      // eslint-disable-next-line
+      console.log('creating partition:', response);
+    } catch (error) {
+      // handle error here
+      // eslint-disable-next-line
+      console.error('Error creating partition:', error);
+    }
+  };
+
   return (
     <>
       <ul>
         {data.use.used.value} / {data.use.size.value} {data.use.size.unit}
       </ul>
       <ul>
-        <button>Créer une partition</button>
+        <input
+          type="text"
+          value={partitionData.partitionName}
+          onChange={(e) =>
+            setPartitionData({
+              ...partitionData,
+              partitionName: e.target.value,
+            })
+          }
+        />
+        <input
+          type="text"
+          value={partitionData.partitionDescription}
+          onChange={(e) =>
+            setPartitionData({
+              ...partitionData,
+              partitionDescription: e.target.value,
+            })
+          }
+        />
+        <input
+          type="text"
+          value={partitionData.size}
+          onChange={(e) =>
+            setPartitionData({ ...partitionData, size: Number(e.target.value) })
+          }
+        />
+        <input
+          type="text"
+          value={partitionData.protocol}
+          onChange={(e) =>
+            setPartitionData({ ...partitionData, protocol: e.target.value })
+          }
+        />
+        <button onClick={handleClickPartition}>Créer</button>
       </ul>
     </>
   );
@@ -140,21 +238,21 @@ export default function NashaReactDashboard() {
     <div>
       <Heading as="h3" size="sm">
         {t('title')}
-        <QueryClientProvider client={queryClient}>
-          <hr />
-          <h2>Informations générales</h2>
-          <div>
-            <h3>Partitions</h3>
-            <Partition serviceName={serviceName} />
-            <h3>Informations</h3>
-            <Informations serviceName={serviceName} />
-            <h3>Configuration</h3>
-            <Configuration serviceName={serviceName} />
-            <h3>Abonnement</h3>
-            <Abonnement serviceName={serviceName} />
-          </div>
-        </QueryClientProvider>
       </Heading>
+      <QueryClientProvider client={queryClient}>
+        <hr />
+        <h2>Informations générales</h2>
+        <div>
+          <h3>Partitions</h3>
+          <Partition serviceName={serviceName} />
+          <h3>Informations</h3>
+          <Informations serviceName={serviceName} />
+          <h3>Configuration</h3>
+          <Configuration serviceName={serviceName} />
+          <h3>Abonnement</h3>
+          <Abonnement serviceName={serviceName} />
+        </div>
+      </QueryClientProvider>
     </div>
   );
 }
