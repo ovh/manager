d565d3fc99dd7069f73e36eef7dce7d8fa671fbe
sachinrameshn <sachin.ramesh@ovhcloud.com>
2023-02-22 11:57:45 +0530

    feat(web): rework license dashboard with new grid
    ref: DTRSD-107340
    
    Signed-off-by: sachinrameshn <sachin.ramesh@ovhcloud.com>

--- packages/manager/modules/office/src/office.routing.js
+++ packages/manager/modules/office/src/office.routing.js
@@ -4,14 +4,14 @@ import { getOfficeOrderUrl } from './office.order';
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('office.index', {
     url: `?${ListLayoutHelper.urlQueryParams}`,
-    component: 'managerListLayout',
+    component: 'microsoftOfficeLicense',
     params: ListLayoutHelper.stateParams,
     redirectTo: (transition) =>
       transition
         .injector()
         .getAsync('resources')
         .then((resources) =>
-          resources.data.length === 0 ? { state: 'office.onboarding' } : false,
+          resources.length === 0 ? { state: 'office.onboarding' } : false,
         ),
     resolve: {
       ...ListLayoutHelper.stateResolves,
@@ -25,11 +25,9 @@ export default /* @ngInject */ ($stateProvider) => {
             },
             serviceType: 'aapi',
           })
-          .then((data) => data),
+          .then(({ data }) => data),
       dataModel: () => 'license.office.OfficeTenant',
       defaultFilterColumn: () => 'serviceName',
-      header: /* @ngInject */ ($translate) =>
-        $translate.instant('office_title'),
       customizableColumns: () => true,
       topbarOptions: /* @ngInject */ (
         $translate,
@@ -55,6 +53,12 @@ export default /* @ngInject */ ($stateProvider) => {
           },
         },
       }),
+      gotoOrder: /* @ngInject */ ($window, coreConfig) => () => {
+        $window.open(
+          getOfficeOrderUrl(coreConfig.getUser().ovhSubsidiary),
+          '_blank',
+        );
+      },
       hideBreadcrumb: () => true,
       getServiceNameLink: /* @ngInject */ ($state) => ({ serviceName }) =>
         $state.href('office.product', {
