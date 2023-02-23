45d651231e6c14d55307e3ff390aae36e9203c1a
stif59100 <steeve.vanderstocken@ovhcloud.com>
2023-02-23 14:41:33 +0100

    fix(dedicated.cloud): add tracking recomendations
    ref: MANAGER-9497
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/components/dedicated-cloud/users/activeDirectories/edit/federation-edit.constant.js
+++ packages/manager/apps/dedicated/client/app/components/dedicated-cloud/users/activeDirectories/edit/federation-edit.constant.js
@@ -19,6 +19,7 @@ export const TRACKING_PREFIX = 'update-active-directory';
 export const TRACKING_TASK_TAG = {
   done: `${TRACKING_PREFIX}-success`,
   error: `${TRACKING_PREFIX}-error`,
+  canceled: `${TRACKING_PREFIX}-canceled`,
 };
 
 export default {
