a08b9be4e8a4a70986a4f564463883295997b119
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2023-02-24 21:29:26 +0100

    feat(iam): set up routes
    ref: MANAGER-10394
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/modules/iam/src/index.js
+++ packages/manager/modules/iam/src/index.js
@@ -3,17 +3,44 @@ import angularTranslate from 'angular-translate';
 import uiRouter from '@uirouter/angularjs';
 
 import ngOvhFeatureFlipping from '@ovh-ux/ng-ovh-feature-flipping';
+import ngOvhUtils from '@ovh-ux/ng-ovh-utils';
 
-import routing from './iam.routing';
-import constant from './iam.constant';
+import { declareRoutes, ROUTES } from '@iam/routes';
+import { assignConstants } from '@iam/constants';
+import { registerTypes } from '@iam/resolves';
+import services from '@iam/services';
+import components from '@iam/components';
+
+// TODO remove the relative paths once the feat/datagrid-pagination-mode branch
+// of @ovh-ux/ui-kit has been merged / published
+import '../../../apps/iam/node_modules/@ovh-ux/ui-kit/dist/js/oui';
+import '../../../apps/iam/node_modules/@ovh-ux/ui-kit/dist/css/oui.css';
+import 'ovh-ui-kit-bs/dist/css/oui-bs3.css';
 
 const moduleName = 'ovhManagerIAM';
+const componentModuleNames = Object.values(components).map(
+  ({ moduleName: componentModuleName }) => componentModuleName,
+);
 
 angular
-  .module(moduleName, [angularTranslate, uiRouter, ngOvhFeatureFlipping])
-  .constant('IAM', constant)
-  .config(routing)
-  .run(/* @ngTranslationsInject:json ./translations */)
-  .run(/* @ngInject */ ($rootScope, IAM) => Object.assign($rootScope, { IAM }));
+  .module(moduleName, [
+    angularTranslate,
+    uiRouter,
+    ngOvhFeatureFlipping,
+    ngOvhUtils,
+    // TODO use the default export of @ovh-ux/ui-kit once the fix/build-export-default-library-name
+    //  branch of @ovh-ux/ui-kit has been merged / published
+    'oui',
+    ...componentModuleNames,
+  ])
+  .config(registerTypes)
+  .config(declareRoutes)
+  .run(assignConstants('IAM', { ROUTES }))
+  .run(/* @ngTranslationsInject:json ./services/translations */)
+  .run(/* @ngTranslationsInject:json ./resolves/translations */);
+
+Object.entries(services).forEach(([serviceName, Service]) => {
+  angular.module(moduleName).service(serviceName, Service);
+});
 
 export default moduleName;
