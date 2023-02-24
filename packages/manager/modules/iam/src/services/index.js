6baaf3cae2a3ba56e68a469f977577e0c5cfc73f
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2023-02-24 20:37:45 +0100

    feat(iam): set up services
    ref: MANAGER-10394
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

---
+++ packages/manager/modules/iam/src/services/index.js
@@ -0,0 +1,9 @@
+import APIV2Service from './apiv2.service';
+import GuideService from './guide.service';
+import PolicyService from './policy.service';
+
+export default {
+  APIV2Service,
+  GuideService,
+  PolicyService,
+};
