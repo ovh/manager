d84c08a5c3877dbbfeae524a10172b896b0af2be
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2023-02-24 20:37:45 +0100

    feat(iam): set up services
    ref: MANAGER-10394
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

---
+++ packages/manager/modules/iam/src/services/apiv2.service.js
@@ -0,0 +1,60 @@
+export default class APIV2Service {
+  #baseURL = 'iam';
+
+  /* @ngInject */
+  constructor($http) {
+    this.$http = $http;
+  }
+
+  /**
+   * Call the given endpoint on the API v2
+   * @param {'get' | 'post' | 'put' | 'delete'} method
+   * @param {string} endpoint
+   * @param {object=} options the $http options
+   * @returns {Promise}
+   */
+  http(method, endpoint, options) {
+    return this.$http({
+      ...options,
+      method,
+      url: `/engine/api/v2/${this.#baseURL}/${endpoint}`,
+      serviceType: 'apiv2',
+    });
+  }
+
+  /**
+   * Call the given endpoint on the API v2 using the 'get' method
+   * @param {string} endpoint
+   * @param {object=} options the $http.get options
+   * @returns {Promise}
+   */
+  get(endpoint, options) {
+    return this.http('get', endpoint, options);
+  }
+
+  /**
+   * Call the given list endpoint on the API v2 that implements the cursor api
+   * @param {string} endpoint
+   * @param {string=} cursor The cursor id
+   * @returns {Promise}
+   */
+  getList(endpoint, { cursor }) {
+    const options = cursor
+      ? { headers: { 'X-Pagination-Cursor': cursor } }
+      : null;
+    return this.get(endpoint, options)
+      .then(({ data, headers }) => ({
+        data,
+        cursor: {
+          next: headers('X-Pagination-Cursor-Next'),
+          prev: headers('X-Pagination-Cursor-Prev'), // not available ATM
+        },
+      }))
+      .catch((error) => {
+        if (error.status === 400 && error.data?.message === 'invalid cursor') {
+          return { cursor: { error: true } };
+        }
+        throw error;
+      });
+  }
+}
