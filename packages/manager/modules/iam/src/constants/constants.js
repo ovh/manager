f8faa3f71089660e54b2358b0faba25cd2168b8e
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2023-02-24 21:46:37 +0100

    feat(iam): set up constants
    ref: MANAGER-10394
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

---
+++ packages/manager/modules/iam/src/constants/constants.js
@@ -0,0 +1,57 @@
+export const ALERT_ID = 'iam_alert';
+
+export const FEATURE = {
+  MAIN: 'iam',
+};
+
+// TODO wait for other guide specifications
+export const GUIDE = {
+  IAM: {
+    FR: 'https://docs.ovh.com/fr/customer/IAM/',
+  },
+  USERS: {
+    ASIA: 'https://docs.ovh.com/asia/en/customer/managing-users/',
+    AUS: 'https://docs.ovh.com/au/en/customer/managing-users/',
+    CA: 'https://docs.ovh.com/ca/en/customer/managing-users/',
+    DE: 'https://docs.ovh.com/de/customer/benutzer-verwalten/',
+    ES: 'https://docs.ovh.com/es/customer/gestion-de-usuarios/',
+    FR: 'https://docs.ovh.com/fr/customer/gestion-utilisateurs/',
+    GB: 'https://docs.ovh.com/gb/en/customer/managing-users/',
+    IE: 'https://docs.ovh.com/ie/en/customer/managing-users/',
+    IT: 'https://docs.ovh.com/it/customer/gestisci_gli_utenti/',
+    PL: 'https://docs.ovh.com/pl/customer/zarzadzanie_uzytkownikami/',
+    PT: 'https://docs.ovh.com/pt/customer/gestao-utilizadores/',
+    SG: 'https://docs.ovh.com/sg/en/customer/managing-users/',
+    US: 'https://docs.ovh.com/us/en/customer/managing-users/',
+    WS: 'https://docs.ovh.com/us/es/customer/gestion-de-usuarios/',
+  },
+  SAMLSSO: {
+    ASIA: 'https://docs.ovh.com/asia/en/customer/connect-saml-sso/',
+    AUS: 'https://docs.ovh.com/au/en/customer/connect-saml-sso/',
+    CA: 'https://docs.ovh.com/ca/en/customer/connect-saml-sso/',
+    DE: 'https://docs.ovh.com/de/customer/connect-saml-sso/',
+    ES: 'https://docs.ovh.com/es/customer/conect-saml-sso/',
+    FR: 'https://docs.ovh.com/fr/customer/connect-saml-sso/',
+    GB: 'https://docs.ovh.com/gb/en/customer/connect-saml-sso/',
+    IE: 'https://docs.ovh.com/ie/en/customer/connect-saml-sso/',
+    IT: 'https://docs.ovh.com/it/customer/connect-saml-sso/',
+    PL: 'https://docs.ovh.com/pl/customer/connect-saml-sso/',
+    PT: 'https://docs.ovh.com/pt/customer/connect-saml-sso/',
+    SG: 'https://docs.ovh.com/sg/en/customer/connect-saml-sso/',
+    US: 'https://docs.ovh.com/us/en/customer/connect-saml-sso/',
+    WS: 'https://docs.ovh.com/us/es/customer/conect-saml-sso/',
+  },
+};
+
+GUIDE.IAM.DEFAULT = GUIDE.IAM.FR;
+GUIDE.USERS.DEFAULT = GUIDE.USERS.US;
+GUIDE.SAMLSSO.DEFAULT = GUIDE.SAMLSSO.US;
+
+export const UNAVAILABLE_STATE_NAME = 'app.account.user';
+
+export default {
+  ALERT_ID,
+  FEATURE,
+  GUIDE,
+  UNAVAILABLE_STATE_NAME,
+};
