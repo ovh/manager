a72c93a7cec22e6c800f66c5a16b880272f9060b
Mohammed ZAHAF <22635766+mohammed-zahaf@users.noreply.github.com>
2023-02-27 14:21:43 +0100

    feat(volume-backup): update guides (#8971)
    ref: MANAGER-10924
    
    Signed-off-by: Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/volume-backup/volume-backup.constants.js
+++ packages/manager/modules/pci/src/projects/project/storages/volume-backup/volume-backup.constants.js
@@ -57,7 +57,53 @@ export const VOLUME_BACKUP_TRACKING = {
 
 export const GUIDES = [
   {
-    id: 'overview',
+    id: 'storages_overview',
+    links: {
+      DEFAULT: 'https://docs.ovh.com/gb/en/storage/',
+      ASIA: 'https://docs.ovh.com/asia/en/storage/',
+      AU: 'https://docs.ovh.com/au/en/storage/',
+      CA: 'https://docs.ovh.com/ca/en/storage/',
+      DE: 'https://docs.ovh.com/de/storage/',
+      ES: 'https://docs.ovh.com/es/storage/',
+      FR: 'https://docs.ovh.com/fr/storage/',
+      GB: 'https://docs.ovh.com/gb/en/storage/',
+      IE: 'https://docs.ovh.com/ie/en/storage/',
+      IT: 'https://docs.ovh.com/it/storage/',
+      PL: 'https://docs.ovh.com/pl/storage/',
+      PT: 'https://docs.ovh.com/pt/storage/',
+      QC: 'https://docs.ovh.com/ca/fr/storage/',
+      SG: 'https://docs.ovh.com/sg/storage/',
+      US: 'https://docs.ovh.com/us/en/storage/',
+      WS: 'https://docs.ovh.com/us/es/storage/',
+    },
+  },
+  {
+    id: 'instances_overview',
+    links: {
+      DEFAULT:
+        'https://docs.ovh.com/gb/en/public-cloud/public-cloud-first-steps/',
+      ASIA:
+        'https://docs.ovh.com/asia/en/public-cloud/public-cloud-first-steps/',
+      AU: 'https://docs.ovh.com/au/en/public-cloud/public-cloud-first-steps/',
+      CA: 'https://docs.ovh.com/ca/en/public-cloud/public-cloud-first-steps/',
+      DE: 'https://docs.ovh.com/de/public-cloud/public-cloud-erste-schritte/',
+      ES: 'https://docs.ovh.com/es/public-cloud/public-cloud-primeros-pasos/',
+      FR:
+        'https://docs.ovh.com/fr/public-cloud/premiers-pas-instance-public-cloud/',
+      GB: 'https://docs.ovh.com/gb/en/public-cloud/public-cloud-first-steps/',
+      IT: 'https://docs.ovh.com/it/public-cloud/primi-passi-public-cloud/',
+      NL: 'https://docs.ovh.com/nl/public-cloud/public-cloud-primeiros-passos/',
+      PL: 'https://docs.ovh.com/pl/public-cloud/public-cloud-primeiros-passos/',
+      PT: 'https://docs.ovh.com/pt/public-cloud/public-cloud-primeiros-passos/',
+      QC:
+        'https://docs.ovh.com/ca/fr/public-cloud/premiers-pas-instance-public-cloud/',
+      US: 'https://docs.ovh.com/us/en/public-cloud/public-cloud-first-steps/',
+      WS:
+        'https://docs.ovh.com/us/es/public-cloud/public-cloud-primeros-pasos/',
+    },
+  },
+  {
+    id: 'storages_volume_backup_overview',
     links: {
       DEFAULT: 'https://docs.ovh.com/en-gb/public-cloud/volume-backup/',
       ES: 'https://docs.ovh.com/es/public-cloud/volume-backup/',
