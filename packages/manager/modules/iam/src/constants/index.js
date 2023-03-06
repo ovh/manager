2c125e49b37160cab124b98dbbf8e897b8fcf4f5
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2023-03-06 08:30:52 +0100

    feat(iam): make constants a module
    ref: MANAGER-10394
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/modules/iam/src/constants/index.js
+++ packages/manager/modules/iam/src/constants/index.js
@@ -1,20 +1,27 @@
+import angular from 'angular';
+
+import { ROUTES } from '@iam/routes';
 import constants from './constants';
 
+// ---------------------------------------------------------------------------------------------------- //
+
 /**
- * Assign the constants to the $rootScope using the given namespace
- * @param {string} namespace
- * @param {any?} otherConstants
- * @returns {function($rootScope)}
+ * Assign the constants to the $rootScope
+ * @param {RootScope} $rootScope
  */
-export const assignConstants = (namespace, otherConstants) => /* @ngInject */ (
-  $rootScope,
-) =>
-  Object.assign($rootScope, {
-    [namespace]: {
-      ...otherConstants,
-      ...constants,
-    },
-  });
+export const assignConstants = /* @ngInject */ ($rootScope) =>
+  Object.assign($rootScope, { IAM: { ...constants, ROUTES } });
+
+// ---------------------------------------------------------------------------------------------------- //
+
+const moduleName = 'ovhManagerIAMConstants';
+
+angular
+  .module(moduleName, [])
+  .run(assignConstants)
+  .run(/* @ngTranslationsInject:json ./translations */);
+
+// ---------------------------------------------------------------------------------------------------- //
 
 export * from './constants';
-export default constants;
+export default moduleName;
