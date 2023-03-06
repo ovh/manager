289692e99a690ca69fb1f81e74d3af810bb27bef
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2023-03-06 08:27:14 +0100

    feat(iam): make routes a module
    ref: MANAGER-10394
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/modules/iam/src/routes/policy.route.js
+++ packages/manager/modules/iam/src/routes/policy.route.js
@@ -2,11 +2,11 @@ import { policy as policyComponent } from '@iam/components';
 import { asResolve, nullBreadcrumbResolve } from '@iam/resolves';
 import policiesRoute from './policies.route';
 
+const name = 'policy';
+const children = [policiesRoute];
 const resolves = [nullBreadcrumbResolve];
 
-export const name = 'policy';
-
-export const state = ({ ROUTES }) => ({
+const state = ({ ROUTES }) => ({
   url: '/policy',
   component: policyComponent.name,
   redirectTo: ROUTES.POLICIES,
@@ -16,10 +16,8 @@ export const state = ({ ROUTES }) => ({
   },
 });
 
-export const children = [policiesRoute];
-
 export default {
   name,
-  state,
   children,
+  state,
 };
