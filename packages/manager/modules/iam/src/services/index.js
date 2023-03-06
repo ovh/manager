08f83591f3eae449ca5786970255a70ab12ca3a7
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2023-03-06 08:25:23 +0100

    feat(iam): make services a module
    ref: MANAGER-10394
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/modules/iam/src/services/index.js
+++ packages/manager/modules/iam/src/services/index.js
@@ -1,9 +1,16 @@
-import APIV2Service from './apiv2.service';
+import angular from 'angular';
+
+import Apiv2Service from './apiv2.service';
 import GuideService from './guide.service';
 import PolicyService from './policy.service';
 
-export default {
-  APIV2Service,
-  GuideService,
-  PolicyService,
-};
+export const moduleName = 'ovhManagerIAMServices';
+
+angular
+  .module(moduleName, [])
+  .service('Apiv2Service', Apiv2Service)
+  .service('GuideService', GuideService)
+  .service('PolicyService', PolicyService)
+  .run(/* @ngTranslationsInject:json ./translations */);
+
+export default moduleName;
