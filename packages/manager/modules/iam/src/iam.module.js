3d100a7b8bed4654081aa514fda2fd982b9448fd
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2023-03-07 07:55:09 +0100

    feat(iam): lazy load iam module
    ref: MANAGER-10394
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

---
+++ packages/manager/modules/iam/src/iam.module.js
@@ -0,0 +1,32 @@
+import angular from 'angular';
+import angularTranslate from 'angular-translate';
+import uiRouter from '@uirouter/angularjs';
+
+import ngOvhFeatureFlipping from '@ovh-ux/ng-ovh-feature-flipping';
+import ngOvhUtils from '@ovh-ux/ng-ovh-utils';
+import uiKit from '@ovh-ux/ui-kit';
+
+import constants from '@iam/constants';
+import resolves from '@iam/resolves';
+import routes from '@iam/routes';
+import services from '@iam/services';
+import components from '@iam/components';
+
+import './index.scss';
+
+const moduleName = 'ovhManagerIAM';
+
+angular.module(moduleName, [
+  angularTranslate,
+  uiRouter,
+  ngOvhFeatureFlipping,
+  ngOvhUtils,
+  uiKit,
+  constants,
+  resolves,
+  routes,
+  services,
+  components,
+]);
+
+export default moduleName;
