120bade721eca96466a75693cdf9bbcfe394be04
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2023-03-14 09:21:21 +0100

    feat(iam): delete a policy
    ref: MANAGER-10395
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/modules/iam/src/components/index.js
+++ packages/manager/modules/iam/src/components/index.js
@@ -2,6 +2,8 @@ import angular from 'angular';
 
 import createPolicyComponent from './createPolicy/createPolicy.component';
 import createPolicyResolves from './createPolicy/createPolicy.resolves';
+import deleteEntityComponent from './deleteEntity/deleteEntity.component';
+import deleteEntityResolves from './deleteEntity/deleteEntity.resolves';
 import iamComponent from './iam/iam.component';
 import policiesComponent from './policies/policies.component';
 import policiesResolves from './policies/policies.resolves';
@@ -12,6 +14,12 @@ const createPolicy = {
   name: 'iamCreatePolicy',
   component: createPolicyComponent,
   resolves: createPolicyResolves,
+};
+
+const deleteEntity = {
+  name: 'iamDeleteEntity',
+  component: deleteEntityComponent,
+  resolves: deleteEntityResolves,
 };
 
 const iam = {
@@ -37,16 +45,18 @@ const moduleName = 'ovhManagerIAMComponents';
 angular
   .module(moduleName, [])
   .component(createPolicy.name, createPolicy.component)
+  .component(deleteEntity.name, deleteEntity.component)
   .component(iam.name, iam.component)
   .component(policies.name, policies.component)
   .component(policy.name, policy.component)
   .run(/*
     @ngTranslationsInject:json
       ./createPolicy/translations
+      ./deleteEntity/translations
       ./iam/translations
       ./policies/translations
       ./policy/translations
   */);
 
-export { createPolicy, iam, policies, policy };
+export { createPolicy, deleteEntity, iam, policies, policy };
 export default moduleName;
