a976db4c0e9c3d66b8f9fe13b69e014055bfe46f
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2023-03-20 18:40:22 +0100

    feat(iam): update identities links
    ref: MANAGER-10783
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

---
+++ packages/manager/modules/iam/src/components/policyIdentities/policyIdentities.component.js
@@ -0,0 +1,13 @@
+import { asBindings } from '@iam/resolves';
+
+import controller from './policyIdentities.controller';
+import resolves from './policyIdentities.resolves';
+import template from './policyIdentities.template.html';
+
+export default {
+  bindings: {
+    ...asBindings(resolves),
+  },
+  controller,
+  template,
+};
--- packages/manager/modules/iam/src/services/index.js
+++ packages/manager/modules/iam/src/services/index.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 
 import Apiv2Service from './apiv2.service';
 import GuideService from './guide.service';
+import IdentityService from './identity.service';
 import PolicyService from './policy.service';
 
 const moduleName = 'ovhManagerIAMServices';
@@ -10,6 +11,7 @@ angular
   .module(moduleName, [])
   .service('Apiv2Service', Apiv2Service)
   .service('GuideService', GuideService)
+  .service('IdentityService', IdentityService)
   .service('PolicyService', PolicyService)
   .run(/* @ngTranslationsInject:json ./translations */);
 
