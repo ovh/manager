9721af016d6a664fb1493b0bdb2491164ce932a9
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2023-03-20 18:40:22 +0100

    feat(iam): update identities links
    ref: MANAGER-10783
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

---
+++ packages/manager/modules/iam/src/components/policyIdentities/policyIdentities.resolves.js
@@ -0,0 +1,15 @@
+import {
+  alertResolve,
+  goBackResolve,
+  goToResolve,
+  onboardingGuidesResolve,
+  policyParamResolve,
+} from '@iam/resolves';
+
+export default [
+  alertResolve,
+  goBackResolve,
+  goToResolve,
+  onboardingGuidesResolve,
+  policyParamResolve,
+];
