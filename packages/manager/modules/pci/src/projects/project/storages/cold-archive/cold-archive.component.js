70a459560b2f554852592eef2c3fcd944a9ea194
Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
2023-03-22 05:44:20 +0100

    feat(storages.cold-archive): routing func to show new container banner
    ref: MANAGER-11029
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>

--- packages/manager/modules/pci/src/projects/project/storages/cold-archive/cold-archive.component.js
+++ packages/manager/modules/pci/src/projects/project/storages/cold-archive/cold-archive.component.js
@@ -13,6 +13,7 @@ export default {
     coldArchiveContainersLink: '<',
     goToAddColdArchive: '<',
     goToColdArchiveContainers: '<',
+    goToColdArchiveContainersWithData: '<',
     guideMenu: '<',
     initGuides: '<',
     containers: '<',
