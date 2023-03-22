038a6c2e6d5dc144aa0327dd1abbfeb6dbe55cd0
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2023-03-22 10:14:49 +0100

    feat(iam): resource group datagrid
    ref: MANAGER-10399
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

---
+++ packages/manager/modules/iam/src/components/resourceGroups/resourceGroups.controller.js
@@ -0,0 +1,44 @@
+import AbstractCursorDatagridController from '../cursorDatagrid/cursorDatagrid.controller';
+
+export default class ResourceGroupsController extends AbstractCursorDatagridController {
+  /* @ngInject */
+  constructor($location, ResourceGroupService, ouiDatagridService) {
+    super($location, ouiDatagridService, 'resourceGroupDatagrid');
+    this.ResourceGroupService = ResourceGroupService;
+  }
+
+  /**
+   * Get the list of resource groups promise
+   * @param {string} cursor The current cursor id
+   * @returns {Promise}
+   */
+  createItemsPromise({ cursor }) {
+    return this.ResourceGroupService.getResourceGroups({ cursor });
+  }
+
+  /**
+   * Go to resourceGroup creation
+   * @returns {Promise}
+   */
+  createResourceGroup() {
+    this.alert.error('createResourceGroup not implemented');
+  }
+
+  /**
+   * Go to resourceGroup edition
+   * @param {string} id The resourceGroup id
+   * @returns {Promise}
+   */
+  editResourceGroup({ id }) {
+    this.alert.error(`editResourceGroup not implemented [id=${id}]`);
+  }
+
+  /**
+   * Go to resourceGroup deletion
+   * @param {string} id The resourceGroup id
+   * @returns {Promise}
+   */
+  deleteResourceGroup({ id }) {
+    this.alert.error(`deleteResourceGroup not implemented [id=${id}]`);
+  }
+}
