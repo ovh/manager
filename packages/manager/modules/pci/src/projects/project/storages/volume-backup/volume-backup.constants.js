dca15747846f6e2f219f7eb44c7ef9fc7fc27a13
Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>
2023-03-29 14:04:35 +0200

    fix(volume-backup): remove us guide link why -> because not ready for prod
    ref: MANAGER-10570
    
    Signed-off-by: Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/volume-backup/volume-backup.constants.js
+++ packages/manager/modules/pci/src/projects/project/storages/volume-backup/volume-backup.constants.js
@@ -122,7 +122,6 @@ export const GUIDES = [
       NL: 'https://docs.ovh.com/nl/public-cloud/volume-backup/',
       PL: 'https://docs.ovh.com/pl/public-cloud/volume-backup/',
       PT: 'https://docs.ovh.com/pt/public-cloud/volume-backup/',
-      US: 'https://docs.ovh.com/en-us/public-cloud/volume-backup/',
     },
   },
   {
