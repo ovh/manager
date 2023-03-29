dc9b396d98e4a9c9decfaa93ab076f01869ee34d
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2023-03-29 17:13:58 +0200

    fix(dedicated.pcc): disable add vdc if nsx pcc
    ref: MANAGER-11008
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/components/dedicated-cloud/datacenters/datacenters.controller.js
+++ packages/manager/apps/dedicated/client/app/components/dedicated-cloud/datacenters/datacenters.controller.js
@@ -1,11 +1,19 @@
 import get from 'lodash/get';
 import includes from 'lodash/includes';
 
+import { REGEX_COMMERCIAL_RANGE_NAME_NSX } from './datacenters.constants';
+
 export default class {
   /* @ngInject */
   constructor($q, DedicatedCloud) {
     this.$q = $q;
     this.DedicatedCloud = DedicatedCloud;
+  }
+
+  $onInit() {
+    this.isNSXPcc = REGEX_COMMERCIAL_RANGE_NAME_NSX.test(
+      this.dedicatedCloud.commercialRange,
+    );
   }
 
   getRight(order) {
