b151c96e79ae7a93247ff692a995f858765becbe
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2023-04-05 10:38:53 +0200

    feat(iam): add advanced mode on onboarding page
    ref: MANAGER-11169
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

---
+++ packages/manager/modules/iam/src/components/advancedModeSwitch/advancedModeSwitch.component.js
@@ -0,0 +1,12 @@
+import { asBindings } from '@iam/resolves';
+import template from './advancedModeSwitch.template.html';
+import resolves from './advancedModeSwitch.resolves';
+import controller from './advancedModeSwitch.controller';
+
+export default {
+  bindings: {
+    ...asBindings(resolves),
+  },
+  template,
+  controller,
+};
