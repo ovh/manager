66167b34aedfc9597ebc5ca53dbe4dd8992e26a8
Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
2023-04-12 06:54:27 +0200

    feat(storage.cold-archive): add billing ui component
    ref: MANAGER-11060
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
    Signed-off-by: Mohammed ZAHAF <mohammed.zahaf.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/components/project/billing/resource-usage/controller.js
+++ packages/manager/modules/pci/src/components/project/billing/resource-usage/controller.js
@@ -1,7 +1,19 @@
+import { COLD_ARCHIVE_FEE_TYPES } from './constants';
+
 export default class BillingResourceUsageCtrl {
   /* @ngInject */
-  constructor(ovhManagerRegionService) {
+  constructor(ovhManagerRegionService, $translate) {
     this.ovhManagerRegionService = ovhManagerRegionService;
+    this.$translate = $translate;
+  }
+
+  getType(type) {
+    if (COLD_ARCHIVE_FEE_TYPES.includes(type)) {
+      return this.$translate.instant(
+        `pci_billing_private_registry_type_cold_archive_${type}_label`,
+      );
+    }
+    return type;
   }
 
   static hourlyQuantity(quantity) {
