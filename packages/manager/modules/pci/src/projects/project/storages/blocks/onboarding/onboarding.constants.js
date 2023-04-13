9fc8bb6af9b1a7f944ee512ddd0de22b23772fe9
Sachin Ramesh <104337442+sachinrameshn@users.noreply.github.com>
2023-04-13 11:27:34 +0530

    fix(pci): add asia guide url in blockstorage onboarding page (#9171)
    ref: MANAGER-11146
    
    Signed-off-by: sachinrameshn <sachin.ramesh@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/blocks/onboarding/onboarding.constants.js
+++ packages/manager/modules/pci/src/projects/project/storages/blocks/onboarding/onboarding.constants.js
@@ -1,19 +1,27 @@
 export const GUIDES = [
   {
     id: 'create-attach-volume',
-    link:
+    defaultlink:
       'https://docs.ovh.com/gb/en/public-cloud/create-an-additional-volume-and-attach-it-to-an-instance/',
+    asialink:
+      'https://docs.ovh.com/asia/en/public-cloud/create_and_configure_an_additional_disk_on_an_instance/',
   },
   {
     id: 'configure-volume',
-    link:
+    defaultlink:
       'https://docs.ovh.com/gb/en/public-cloud/configure-an-additional-volume/',
+    asialink:
+      'https://docs.ovh.com/asia/en/public-cloud/configure-an-additional-volume/',
   },
   {
     id: 'increase-volume-size',
-    link:
+    defaultlink:
       'https://docs.ovh.com/gb/en/public-cloud/increase_the_size_of_an_additional_disk/',
+    asialink:
+      'https://docs.ovh.com/asia/en/public-cloud/increase_the_size_of_an_additional_disk/',
   },
 ];
 
-export default { GUIDES };
+export const IN_SUBSIDIARY = 'IN';
+
+export default { GUIDES, IN_SUBSIDIARY };
