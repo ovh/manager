966ad01f29a4f68144e5cf19ceb90d28a134eb77
Florian Renaut <florian.renaut@corp.ovh.com>
2023-05-03 14:52:50 +0200

    feat(iam): update app for container
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/apps/iam/src/index.js
+++ packages/manager/apps/iam/src/index.js
@@ -1,13 +1,28 @@
 import 'whatwg-fetch';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import { bootstrapApplication } from '@ovh-ux/manager-core';
+import { isTopLevelApplication } from '@ovh-ux/manager-config';
+import { useShellClient } from '@ovh-ux/shell';
 import { defineApplicationVersion } from '@ovh-ux/request-tagger';
 
 defineApplicationVersion(__VERSION__);
 
-bootstrapApplication('iam').then((environment) =>
-  import('./iam.module').then(({ default: startApplication }) =>
-    startApplication(document.body, environment),
-  ),
-);
+useShellClient('iam')
+  .then(async (client) => {
+    if (!isTopLevelApplication()) {
+      client.ux.startProgress();
+    }
+    const environment = await client.environment.getEnvironment();
+    return {
+      environment,
+      client,
+    };
+  })
+  .then(({ environment, client }) =>
+    import(`./config-${environment.getRegion()}`)
+      .catch(() => {})
+      .then(() => import('./iam.module'))
+      .then(({ default: startApplication }) =>
+        startApplication(document.body, client),
+      ),
+  );
