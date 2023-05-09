779add08f72979c31eff45d29fbd0ee252741111
Nicolas BAPTISTA <nicolas.baptista.ext@corp.ovh.com>
2023-05-09 16:36:49 +0200

    feat(dedicated.nasha): first implementation of iceberg
    ref: MANAGER-11384

--- packages/manager/apps/nasha-react/src/pages/index.tsx
+++ packages/manager/apps/nasha-react/src/pages/index.tsx
@@ -6,12 +6,27 @@ import {
   QueryClientProvider,
   useQuery,
 } from '@tanstack/react-query';
+import { fetchIceberg } from '@ovh-ux/manager-core-api/src/iceberg';
 import { services } from '../api/nasha-react';
 
 const queryClient = new QueryClient();
 
+// request with iceberg
+const NASHA_BASE_API_URL = '/dedicated/nasha';
+function getListNashaIceberg() {
+  fetchIceberg({ route: NASHA_BASE_API_URL }).then(({ data }) => {
+    console.log('NASHA BASE API URL:', NASHA_BASE_API_URL);
+    console.log('data:', data);
+    return data;
+  });
+}
+
 function Services() {
   const { isLoading, isError, data } = useQuery(['listNasha'], services);
+  // Iceberg à la place
+  const listNasha = getListNashaIceberg();
+  console.log('listNasha from iceberg:', listNasha);
+
   const { t } = useTranslation('nasha-react');
 
   if (isLoading) {
