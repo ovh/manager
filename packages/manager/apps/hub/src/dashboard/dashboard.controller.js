64fd286083ef2436242c4938a33735d5acec0e84
Sachin Ramesh <104337442+sachinrameshn@users.noreply.github.com>
2023-05-09 13:10:46 +0530

    feat(*): implement tracking for acquisition storage (#9261)
    ref: MANAGER-11348
    
    Signed-off-by: sachinrameshn <sachin.ramesh@ovhcloud.com>

--- packages/manager/apps/hub/src/dashboard/dashboard.controller.js
+++ packages/manager/apps/hub/src/dashboard/dashboard.controller.js
@@ -1,7 +1,9 @@
 import {
-  TRACKING_PREFIX,
-  HIT_PREFIX,
+  SIRET_TRACKING_PREFIX,
+  SIRET_HIT_PREFIX,
   TRACKING_PREFIX_POPUP,
+  KYC_TRACKING_PREFIX,
+  KYC_HIT_PREFIX,
 } from './dashboard.constant';
 
 export default class DashboardController {
@@ -10,9 +12,10 @@ export default class DashboardController {
     this.coreURLBuilder = coreURLBuilder;
     this.atInternet = atInternet;
     this.$window = $window;
-    this.TRACKING_PREFIX = TRACKING_PREFIX;
-    this.HIT_PREFIX = HIT_PREFIX;
+    this.SIRET_TRACKING_PREFIX = SIRET_TRACKING_PREFIX;
+    this.SIRET_HIT_PREFIX = SIRET_HIT_PREFIX;
     this.TRACKING_PREFIX_POPUP = TRACKING_PREFIX_POPUP;
+    this.KYC_HIT_PREFIX = KYC_HIT_PREFIX;
     this.$http = $http;
     this.myIdentitySectionLink = coreURLBuilder.buildURL(
       'dedicated',
@@ -32,6 +35,11 @@ export default class DashboardController {
         if (featureAvailability['identity-documents']) {
           this.$http.get(`/me/procedure/identity`).then(({ data }) => {
             this.showKycBanner = ['open', 'required'].includes(data.status);
+            if (this.showKycBanner)
+              this.atInternet.trackPage({
+                name: KYC_TRACKING_PREFIX,
+                type: 'navigation',
+              });
           });
         }
       });
@@ -57,7 +65,7 @@ export default class DashboardController {
           this.userSiretFR;
         if (this.availableSiretBanner) {
           this.atInternet.trackPage({
-            name: TRACKING_PREFIX,
+            name: SIRET_TRACKING_PREFIX,
             type: 'navigation',
           });
         }
@@ -95,9 +103,9 @@ export default class DashboardController {
     this.availableSiretPopup = false;
   }
 
-  trackClick() {
+  trackClick(hit) {
     this.atInternet.trackClick({
-      name: `${TRACKING_PREFIX}::${HIT_PREFIX}`,
+      name: hit,
       type: 'action',
     });
   }
