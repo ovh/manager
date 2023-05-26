9ad9056b81f53c0c1683d4920836d012a3b3da2a
darsene <72250222+darsene@users.noreply.github.com>
2023-05-26 14:36:23 +0200

    fix(pci): fix wrong links for US manager guides (#9324)
    ref: MANAGER-11050
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/failover-ips/onboarding/onboarding.constants.js
+++ packages/manager/modules/pci/src/projects/project/failover-ips/onboarding/onboarding.constants.js
@@ -1,12 +1,21 @@
 export const GUIDES = [
   {
     id: 'persistent-failover-ip-configuration',
-    link:
-      'https://docs.ovh.com/gb/en/public-cloud/make-failover-ip-configuration-persistent/',
+    link: {
+      DEFAULT:
+        'https://docs.ovh.com/gb/en/public-cloud/make-failover-ip-configuration-persistent/',
+      US:
+        'https://support.us.ovhcloud.com/hc/en-us/articles/360002987280-How-to-Configure-an-Additional-IP-as-an-Alias-in-Ubuntu',
+    },
   },
   {
     id: 'migrating-failover-ip',
-    link: 'https://docs.ovh.com/gb/en/public-cloud/migrating_a_failover_ip/',
+    link: {
+      DEFAULT:
+        'https://docs.ovh.com/gb/en/public-cloud/migrating_a_failover_ip/',
+      US:
+        'https://support.us.ovhcloud.com/hc/en-us/articles/4407515397779-How-to-Move-an-Additional-IP',
+    },
   },
 ];
 
