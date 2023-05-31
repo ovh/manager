0ab1d2df3faa9456d60c153f0c8c9ba6fef510c8
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2023-05-31 10:58:48 +0200

    refactor(iam): kill the delete policy route
    ref: MANAGER-11434
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/modules/iam/src/routes/deletePolicy.route.js
+++ packages/manager/modules/iam/src/routes/deletePolicy.route.js
@@ -1,25 +1,4 @@
-import {
-  entityResolve,
-  policyParamResolve,
-  statementResolve,
-} from '../resolves';
-
-const name = 'deletePolicy';
-
-const state = () => ({
-  url: `/delete/{policy:uuid}`,
-  component: 'iamDeleteEntity',
-  resolve: {
-    breadcrumb: () => null,
-    entity: entityResolve,
-    identity: () => null,
-    policy: policyParamResolve,
-    resourceGroup: () => null,
-    statement: statementResolve,
-  },
-});
-
 export default {
-  name,
-  state,
+  name: 'delete',
+  state: null,
 };
