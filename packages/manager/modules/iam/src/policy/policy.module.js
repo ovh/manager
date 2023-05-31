8a3c07ce1badc27a2e0f8873471bbd1722ec5203
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2023-05-31 10:50:49 +0200

    refactor(iam): kill the policies route
    ref: MANAGER-11434
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/modules/iam/src/policy/policy.module.js
+++ packages/manager/modules/iam/src/policy/policy.module.js
@@ -1,12 +1,14 @@
 import angular from 'angular';
 
+import policies from './policies';
+
 import component from './policy.component';
 import routing from './policy.routing';
 
 const moduleName = 'ovhManagerIAMPolicy';
 
 angular
-  .module(moduleName, [])
+  .module(moduleName, [policies])
   .component('iamPolicy', component)
   .config(routing)
   .run(/* @ngTranslationsInject:json ./translations */);
