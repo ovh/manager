a15a82ab35aaf1571e332350ff705af06ac7a26f
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2023-05-31 14:48:14 +0200

    refactor(iam): fix statement resolves
    ref: MANAGER-11434
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/modules/iam/src/policy/resourceGroups/delete/delete.routing.js
+++ packages/manager/modules/iam/src/policy/resourceGroups/delete/delete.routing.js
@@ -33,7 +33,7 @@ export default /* @ngInject */ ($stateProvider) => {
        * @returns {boolean}
        */
       statement: /* @ngInject */ (entity) =>
-        [ENTITY.POLICY].includes(entity.type),
+        entity.type === ENTITY.RESOURCE_GROUP,
     },
   });
 };
