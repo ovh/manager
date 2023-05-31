47d914124572e42849ac279bf729f1e349594313
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2023-05-31 09:50:08 +0200

    refactor(iam): kill the iam route
    ref: MANAGER-11434
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/modules/iam/src/iam.module.js
+++ packages/manager/modules/iam/src/iam.module.js
@@ -12,23 +12,27 @@ import resolves from './resolves';
 import routes from './routes';
 import services from './services';
 import components from './components';
+import routing from './iam.routing';
 
 import '@ovh-ux/ui-kit/dist/css/oui.css';
 import 'ovh-ui-kit-bs/dist/css/oui-bs3.css';
+import './iam.styles.scss';
 
 const moduleName = 'ovhManagerIAM';
 
-angular.module(moduleName, [
-  OnboardingLayoutHelper,
-  angularTranslate,
-  uiRouter,
-  ngOvhFeatureFlipping,
-  ngOvhUtils,
-  uiKit,
-  resolves,
-  routes,
-  services,
-  components,
-]);
+angular
+  .module(moduleName, [
+    OnboardingLayoutHelper,
+    angularTranslate,
+    uiRouter,
+    ngOvhFeatureFlipping,
+    ngOvhUtils,
+    uiKit,
+    resolves,
+    routes,
+    services,
+    components,
+  ])
+  .config(routing);
 
 export default moduleName;
