f3681a9b4f72d70c9bdc92093137e5408613188e
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2023-05-31 09:50:08 +0200

    refactor(iam): kill the iam route
    ref: MANAGER-11434
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/modules/iam/src/routes/iam.route.js
+++ packages/manager/modules/iam/src/routes/iam.route.js
@@ -1,44 +1,4 @@
-import { FEATURE, UNAVAILABLE_STATE_NAME } from '../iam.constants';
-import {
-  advancedModeResolve,
-  alertResolve,
-  defaultBreadcrumbResolve,
-  featuresResolve,
-  goBackResolve,
-  goToResolve,
-  hasPoliciesResolve,
-  onboardingGuidesResolve,
-  usersManagementLinkResolve,
-} from '../resolves';
-
-const name = 'iam';
-
-const state = () => ({
-  url: '/iam',
-  component: 'iam',
-  redirectTo: (transition) =>
-    transition
-      .injector()
-      .getAsync(featuresResolve.key)
-      .then((featureAvailabilityResult) =>
-        featureAvailabilityResult.isFeatureAvailable(FEATURE.MAIN)
-          ? 'iam.policy'
-          : { state: UNAVAILABLE_STATE_NAME },
-      ),
-  resolve: {
-    advancedMode: advancedModeResolve,
-    alert: alertResolve,
-    breadcrumb: defaultBreadcrumbResolve,
-    features: featuresResolve,
-    goBack: goBackResolve,
-    goTo: goToResolve,
-    hasPolicies: hasPoliciesResolve,
-    onboardingGuides: onboardingGuidesResolve,
-    usersManagementLink: usersManagementLinkResolve,
-  },
-});
-
 export default {
-  name,
-  state,
+  name: 'iam',
+  state: null,
 };
