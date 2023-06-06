2221a792b3b28229b86750d33d2eee5fb8c53fed
David Arsène <david.arsene.ext@ovhcloud.com>
2023-06-06 11:48:46 +0200

    feat(dedicated.carbon): add new carbon calculator uapp
    ref: MANAGER-11509
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

---
+++ packages/manager/apps/carbon-calculator/src/error/error.routing.js
@@ -0,0 +1,26 @@
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state('error', {
+    params: {
+      detail: null,
+    },
+    url: '/error',
+    component: 'managerErrorPage',
+    resolve: {
+      cancelLink: /* @ngInject */ ($state) => $state.href('app'),
+      error: /* @ngInject */ ($transition$) => $transition$.params(),
+      product: /* @ngInject */ ($state, $transition$) => {
+        const { state, params } = $transition$.params().to;
+        // href will include # so split with '/' and take second element
+        const [, product] = $state
+          .href(state, params, { lossy: true })
+          .split('/');
+        // URI params are removed from error tracking
+        return product?.split('?')[0];
+      },
+      submitAction: /* @ngInject */ ($window) => () =>
+        $window.location.reload(),
+      translationsRefresh: /* @ngInject */ ($translate) => $translate.refresh(),
+      breadcrumb: () => null,
+    },
+  });
+};
