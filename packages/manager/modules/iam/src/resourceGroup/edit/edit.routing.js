91b99779772d7dcd97e2cec5dc84321fc4daf39d
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2023-06-09 09:30:10 +0200

    feat(iam): create & edit resource groups
    ref: MANAGER-10401, MANAGER-10518
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

---
+++ packages/manager/modules/iam/src/resourceGroup/edit/edit.routing.js
@@ -0,0 +1,21 @@
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state('iam.resourceGroup.edit', {
+    url: '/edit/{resourceGroup:uuid}',
+    component: 'iamCreateResourceGroup',
+    resolve: {
+      breadcrumb: /* @ngInject */ ($translate, resourceGroup) =>
+        $translate.instant('iam_resource_group_edit', {
+          resourceGroup: resourceGroup.name,
+        }),
+
+      /**
+       * The resource group parameter based on the resourceGroup's id
+       * @returns {Object|null}
+       */
+      resourceGroup: /* @ngInject */ ($transition$, IAMService) => {
+        const { resourceGroup: uuid } = $transition$.params();
+        return uuid ? IAMService.getDetailedResourceGroup(uuid) : null;
+      },
+    },
+  });
+};
