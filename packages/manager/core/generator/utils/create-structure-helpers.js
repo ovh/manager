c6e54f0eb8ea87bf6963eb5fc222ac51e833d3d3
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2023-06-12 16:01:30 +0200

    feat(tracking): add tracking on ods components
    ref: MANAGER-11442
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager/core/generator/utils/create-structure-helpers.js
+++ packages/manager/core/generator/utils/create-structure-helpers.js
@@ -6,8 +6,8 @@ import { join } from 'node:path';
  * into src/api/**
  * Corresponding to the api used by apiv6
  */
-const createApiQueryFilesActions = (apiV6Endpoints, appDirectory) => {
-  return Object.entries(apiV6Endpoints).map(([method, data]) => ({
+export const createApiQueryFilesActions = (apiV6Endpoints, appDirectory) =>
+  Object.entries(apiV6Endpoints).map(([method, data]) => ({
     type: 'add',
     path: join(
       appDirectory,
@@ -15,39 +15,22 @@ const createApiQueryFilesActions = (apiV6Endpoints, appDirectory) => {
     ),
     templateFile: join(
       appDirectory,
-      './conditional-templates/api/services-template.ts.hbs',
+      `./conditional-templates/api/services-template${
+        method.toUpperCase() === 'GET' ? '-get' : ''
+      }.ts.hbs`,
     ),
     data,
   }));
-};
 
 /**
  * Copy all templates files in conditional-templates
  * into app/pages folder
  * Corresponding to the template selected
  */
-const createPages = (templates, appDirectory) => {
-  if (!templates.length) {
-    // no template selected
-    return [
-      {
-        type: 'add',
-        path: join(
-          appDirectory,
-          '../../../apps/{{dashCase appName}}/e2e/home.test.ts',
-        ),
-        force: true,
-        templateFile: join(
-          appDirectory,
-          `./conditional-templates/default/home.test.ts.hbs`,
-        ),
-      },
-    ];
-  }
-  return templates.flatMap((template) => {
-    if (template === 'listing') {
-      return [
-        {
+export const createPages = (templates, appDirectory) =>
+  templates.map((template) =>
+    template === 'listing'
+      ? {
           type: 'add',
           path: join(
             appDirectory,
@@ -58,93 +41,35 @@ const createPages = (templates, appDirectory) => {
             appDirectory,
             `./conditional-templates/${template}/index.tsx.hbs`,
           ),
+        }
+      : {
+          type: 'addMany',
+          destination: join(
+            appDirectory,
+            `../../../apps/{{dashCase appName}}/src/pages/${template}/`,
+          ),
+          templateFiles: join(
+            appDirectory,
+            `./conditional-templates/${template}`,
+          ),
+          base: join(appDirectory, `./conditional-templates/${template}`),
         },
-        {
-          type: 'add',
-          path: join(
-            appDirectory,
-            '../../../apps/{{dashCase appName}}/e2e/home.test.ts',
-          ),
-          force: true,
-          templateFile: join(
-            appDirectory,
-            `./conditional-templates/${template}/listing.test.ts.hbs`,
-          ),
-        },
-      ];
-    }
-    if (template === 'dashboard') {
-      return [
-        {
-          type: 'add',
-          path: join(
-            appDirectory,
-            `../../../apps/{{dashCase appName}}/src/pages/dashboard/[serviceName]/index.tsx`,
-          ),
-          templateFile: join(
-            appDirectory,
-            `./conditional-templates/${template}/index.tsx.hbs`,
-          ),
-        },
-        {
-          type: 'add',
-          path: join(
-            appDirectory,
-            `../../../apps/{{dashCase appName}}/e2e/dashboard.test.ts`,
-          ),
-          templateFile: join(
-            appDirectory,
-            `./conditional-templates/${template}/dashboard.test.ts.hbs`,
-          ),
-        },
-      ];
-    }
-    return [
-      {
-        type: 'add',
-        path: join(
-          appDirectory,
-          `../../../apps/{{dashCase appName}}/src/pages/${template}/index.tsx`,
-        ),
-        templateFile: join(
-          appDirectory,
-          `./conditional-templates/${template}/index.tsx.hbs`,
-        ),
-      },
-      {
-        type: 'add',
-        path: join(
-          appDirectory,
-          `../../../apps/{{dashCase appName}}/e2e/${template}.test.ts`,
-        ),
-        templateFile: join(
-          appDirectory,
-          `./conditional-templates/${template}/${template}.test.ts.hbs`,
-        ),
-      },
-    ];
-  });
-};
+  );
 
 /**
  * Copy all translations files in conditional-translations
  * into public/translations/appName/**
  * Corresponding to the template selected
  */
-const createTranslations = (templates, appName, appDirectory) => {
-  return templates.map((template) => {
-    return {
-      type: 'add',
-      path: join(
-        appDirectory,
-        `../../../apps/{{dashCase appName}}/src/public/translations/${appName}/${template}/Messages_fr_FR.json`,
-      ),
-      templateFile: join(
-        appDirectory,
-        `./conditional-translations/${template}/Messages_fr_FR.json`,
-      ),
-    };
-  });
-};
-
-export { createPages, createTranslations, createApiQueryFilesActions };
+export const createTranslations = (templates, appName, appDirectory) =>
+  templates.map((template) => ({
+    type: 'add',
+    path: join(
+      appDirectory,
+      `../../../apps/{{dashCase appName}}/src/public/translations/${appName}/${template}/Messages_fr_FR.json`,
+    ),
+    templateFile: join(
+      appDirectory,
+      `./conditional-translations/${template}/Messages_fr_FR.json.hbs`,
+    ),
+  }));
