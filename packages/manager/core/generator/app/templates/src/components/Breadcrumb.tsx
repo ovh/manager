38f5b2fe076cc74af892e39137b742a956f8dd62
stif59100 <steeve.vanderstocken@ovhcloud.com>
2023-06-13 14:25:20 +0200

    feat(react.generator): add breadcrumb component
    ref:MANAGER-11584
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

--- packages/manager/core/generator/app/templates/src/components/Breadcrumb.tsx
+++ packages/manager/core/generator/app/templates/src/components/Breadcrumb.tsx
@@ -4,11 +4,18 @@
  */
 
 import React, { useEffect, useState } from 'react';
-import { Link, Params, useMatches } from 'react-router-dom';
+import { Params, useMatches } from 'react-router-dom';
+
+import { OsdsBreadcrumb } from '@ovhcloud/ods-stencil/components/react';
 
 export type BreadcrumbHandleParams = {
   data: unknown;
   params: Params<string>;
+};
+
+type BreadcrumbItem = {
+  label: string;
+  href: string;
 };
 
 type MatchHandle = {
@@ -21,43 +28,34 @@ type Match = {
   params: Params<string>;
 };
 
-export default function BreadcrumbComponent(): JSX.Element {
+function Breadcrumb(): JSX.Element {
   const matches = useMatches();
-  const [crumbs, setCrumbs] = useState<Record<string, string>[]>();
+  const [crumbs, setCrumbs] = useState<BreadcrumbItem[]>([]);
 
   useEffect(() => {
     const items = matches.map(async (match) => {
       const { handle, data, params }: Match = match;
       const breadcrumb = await handle?.breadcrumb;
-      return {
-        pathname: match.pathname,
-        crumb: await breadcrumb?.({ data, params }),
+      const crumb = {
+        label: await breadcrumb?.({ data, params }),
+        href: match.pathname,
       };
+      return crumb;
     });
 
     Promise.all(items)
       .then((breadcrumbs) => {
-        setCrumbs(breadcrumbs.filter(({ crumb }) => !!crumb));
+        const validCrumbs = breadcrumbs.filter(
+          ({ label, href }) => !!label && !!href,
+        );
+        setCrumbs(validCrumbs);
       })
-      .catch((err) => console.error(err));
+      .catch((error) => {
+        console.error('Error fetching breadcrumbs:', error);
+      });
   }, [matches]);
 
-  // no need to display a breadcrumb for a single item
-  if (!crumbs || crumbs.length <= 1) {
-    return <></>;
-  }
-
-  return (
-    <ul>
-      {crumbs.map(({ pathname, crumb }, index) => (
-        <li key={pathname}>
-          {index + 1 < crumbs.length ? (
-            <Link to={pathname}>{crumb}</Link>
-          ) : (
-            <span>{crumb}</span>
-          )}
-        </li>
-      ))}
-    </ul>
-  );
+  return crumbs?.length <= 1 ? <></> : <OsdsBreadcrumb items={crumbs} />;
 }
+
+export default Breadcrumb;
