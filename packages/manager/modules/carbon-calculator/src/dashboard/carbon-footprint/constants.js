d440886128ed147f1c098daebe707658f94867ab
David Arsène <david.arsene.ext@ovhcloud.com>
2023-06-15 19:15:24 +0200

    feat(carbon.dedicated): add downloading carbon footprint invoice
    ref: MANAGER-11513
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

---
+++ packages/manager/modules/carbon-calculator/src/dashboard/carbon-footprint/constants.js
@@ -0,0 +1,11 @@
+export const TASK_STATUS_ENUM = {
+  ERROR: 'ERROR',
+  IN_PROGRESS: 'IN_PROGRESS',
+  SUCCESS: 'SUCCESS',
+};
+
+export const API_PATH = '/me/carbonCalculator/task';
+
+export const API_FETCH_INTERVAL = 3000;
+
+export default { TASK_STATUS_ENUM, API_PATH, API_FETCH_INTERVAL };
--- packages/manager/modules/carbon-calculator/src/dashboard/carbon-footprint/service.js
+++ packages/manager/modules/carbon-calculator/src/dashboard/carbon-footprint/service.js
@@ -1,6 +1,10 @@
+import { API_PATH } from './constants';
+
 export default class CarbonFootprintService {
   /* @ngInject */
-  constructor(coreConfig) {
+  constructor($http, $window, coreConfig) {
+    this.$http = $http;
+    this.$window = $window;
     this.coreConfig = coreConfig;
   }
 
@@ -16,5 +20,21 @@ export default class CarbonFootprintService {
       },
     );
     return dateTimeFormat.format(date);
+  }
+
+  getTask(taskID) {
+    return this.$http.get(`${API_PATH}/${taskID}`).then(({ data }) => data);
+  }
+
+  computeBilling() {
+    const now = new Date();
+    const firstDayOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
+    // expected API date format -> ISO iso8601 (yyyy-MM-dd)
+    const date = firstDayOfMonth.toISOString().substring(0, 10);
+    return this.$http.post(API_PATH, { date }).then(({ data }) => data);
+  }
+
+  downloadBilling(url) {
+    this.$window.location = url;
   }
 }
