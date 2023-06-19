e62b9a4754d2a43609a62e6a80c62a1a81cd3886
Sachin Ramesh <104337442+sachinrameshn@users.noreply.github.com>
2023-06-19 06:55:32 +0530

    feat(dedicated.nutanix): add new api endpoint for cluster (#9157)
    ref: MANAGER-11203
    
    Signed-off-by: sachinrameshn <sachin.ramesh@ovhcloud.com>

--- packages/manager/modules/nutanix/src/service.js
+++ packages/manager/modules/nutanix/src/service.js
@@ -254,7 +254,7 @@ export default class NutanixService {
 
   getNutanixPlans(ovhSubsidiary) {
     return this.$http
-      .get('/order/catalog/public/nutanix', {
+      .get('/order/catalog/public/nutanixStaging', {
         params: {
           ovhSubsidiary,
         },
