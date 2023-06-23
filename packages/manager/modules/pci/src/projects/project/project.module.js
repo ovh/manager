0b1497409132fb89098a6b747a6e18c61c18451c
Quentin Pavy <contact@quentinpavy.com>
2023-06-23 13:31:05 +0200

    feat(pci.octavia): initialize octavia frontend in pci
    ref: MANAGER-11614
    
    Signed-off-by: Quentin Pavy <contact@quentinpavy.com>

--- packages/manager/modules/pci/src/projects/project/project.module.js
+++ packages/manager/modules/pci/src/projects/project/project.module.js
@@ -19,6 +19,7 @@ import inactive from './inactive';
 import instances from './instances';
 import kubernetes from './kubernetes';
 import loadBalancer from './load-balancer';
+import octaviaLoadBalancer from './octavia-load-balancer';
 import notebooks from './notebooks';
 import ai from './ai';
 import sshKeys from './ssh-keys';
@@ -59,6 +60,7 @@ angular
     inactive,
     instances,
     loadBalancer,
+    octaviaLoadBalancer,
     notebooks,
     ai,
     kubernetes,
