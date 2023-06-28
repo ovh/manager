46441f4ea8805df94089110c96e30342c2491468
Quentin Pavy <contact@quentinpavy.com>
2023-06-28 15:30:02 +0200

    feat(pci.octavia): change octavia to uapp
    ref: MANAGER-11614
    
    Signed-off-by: Quentin Pavy <contact@quentinpavy.com>

--- packages/manager/modules/octavia-load-balancer/src/octavia-load-balancer.routing.js
+++ packages/manager/modules/octavia-load-balancer/src/octavia-load-balancer.routing.js
@@ -1,8 +1,8 @@
 export default /* @ngInject */ ($stateProvider) => {
-  $stateProvider.state('pci.projects.project.octavia-load-balancer', {
+  $stateProvider.state('octavia-load-balancer', {
     url: '/octavia-load-balancer',
     template: '<div data-ui-view></div>',
-    redirectTo: 'pci.projects.project.octavia-load-balancer.onboarding',
+    redirectTo: 'octavia-load-balancer.onboarding',
     resolve: {
       breadcrumb: ($translate) => $translate.instant('octavia_lb'),
     },
