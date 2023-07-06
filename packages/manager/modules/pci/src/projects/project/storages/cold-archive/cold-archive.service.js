e56487c5bcfac4e100b6e753f8854129190618d8
Anoop N <anoop.n@ovhcloud.com>
2023-07-06 12:43:44 +0530

    Revert "fix(pci): change HTTP method (#9386)"
    This reverts commit 5c409a87ac9a551fc43026ffb631877c6eb3c96f.
    
    Signed-off-by: Anoop N <anoop.n@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/cold-archive/cold-archive.service.js
+++ packages/manager/modules/pci/src/projects/project/storages/cold-archive/cold-archive.service.js
@@ -38,14 +38,6 @@ export default class PciStoragesColdArchiveService {
   getS3Credentials(projectId, userId) {
     return this.$http
       .get(`/cloud/project/${projectId}/user/${userId}/s3Credentials`)
-      .then(({ data }) => data?.[0]);
-  }
-
-  getS3Secret(projectId, userId, userAccess) {
-    return this.$http
-      .post(
-        `/cloud/project/${projectId}/user/${userId}/s3Credentials/${userAccess}/secret`,
-      )
       .then(({ data }) => data);
   }
 
@@ -72,7 +64,7 @@ export default class PciStoragesColdArchiveService {
       this.getS3Credentials(projectId, user.id)
         .then((data) => ({
           ...user,
-          s3Credentials: data,
+          s3Credentials: data?.[0],
         }))
         .catch(() => null),
     );
@@ -88,10 +80,10 @@ export default class PciStoragesColdArchiveService {
   getAndMapUsersHaveCredentials(projectId, users) {
     const usersCredentialsPromises = users.map((user) =>
       this.getS3Credentials(projectId, user.id).then((data) =>
-        data
+        data.length > 0
           ? {
               ...user,
-              s3Credentials: data,
+              s3Credentials: data?.[0],
             }
           : undefined,
       ),
