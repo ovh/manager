85b044504aa40e70cf6f8f28f74f32a620b1cabc
stif59100 <steeve.vanderstocken@ovhcloud.com>
2023-07-06 10:41:23 +0200

    fix(container.livechat): add level of customer support
    ref:DTRSD-116595
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

--- packages/manager/apps/container/src/components/LiveChat.tsx
+++ packages/manager/apps/container/src/components/LiveChat.tsx
@@ -5,16 +5,34 @@ interface LiveChatProps {
   language: string;
   subsidiary: string;
   open?: boolean;
+  supportLevel: string;
   reduced?: boolean;
   onClose?: () => void;
   onReduce?: () => void;
 }
 
+const getCustomerLevel = (level: string) => {
+  switch (level) {
+    case 'standard':
+      return 'STD';
+    case 'premium':
+      return 'PRM';
+    case 'business':
+      return 'BUS';
+    case 'enterprise':
+      return 'ENT';
+    case 'premium-accredited':
+        return 'PRP';
+    default:
+      return 'STD';
+  }
+};
+
 const LiveChat: React.FC<ComponentProps<LiveChatProps>> = (
-  props: LiveChatProps,
+  { language, open, reduced, subsidiary, supportLevel, ...rest }: LiveChatProps,
 ): JSX.Element => {
-  const { language, open, reduced, subsidiary, ...rest } = props;
-  const url = `https://chat.ovh.com/system/templates/liveChat-manager/STD/${subsidiary}_${language}/docs/index2.html`;
+  const customerLevel = getCustomerLevel(supportLevel);
+  const url = `https://chat.ovh.com/system/templates/liveChat-manager/${customerLevel}/${subsidiary}_${language}/docs/index2.html`;
 
   return (
     <VirtualAgent
@@ -23,7 +41,7 @@ const LiveChat: React.FC<ComponentProps<LiveChatProps>> = (
       url={url}
       agentStarted={open}
       agentReduced={reduced}
-      useStorage={true}
+      useStorage
       buttonColor="#ed642a"
       headerColor="radial-gradient(circle at bottom left, #f98841, #f75a56)"
       customStyles={{
