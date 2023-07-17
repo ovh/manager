9ef5585776079795706b9c10ba6fd9fff30ea607
darsene <72250222+darsene@users.noreply.github.com>
2023-07-17 14:32:18 +0200

    feat(dedicated): add new carbon calculator uapp
    ref: MANAGER-11240
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

---
+++ packages/manager/modules/carbon-calculator/src/dashboard/carbon-balance-composition/constants.js
@@ -0,0 +1,15 @@
+import IMG_MANUFACTURING from './assets/manufacturing.svg';
+import IMG_ELECTRICITY from './assets/rack.svg';
+import IMG_OPERATIONS from './assets/cog.svg';
+import IMG_CARBON from './assets/CO2.svg';
+
+export const ASSETS = {
+  IMG_MANUFACTURING,
+  IMG_ELECTRICITY,
+  IMG_OPERATIONS,
+  IMG_CARBON,
+};
+
+export default {
+  ASSETS,
+};
---
+++ packages/manager/modules/carbon-calculator/src/dashboard/carbon-footprint/component.js
@@ -0,0 +1,8 @@
+import controller from './controller';
+import template from './template.html';
+import 'font-awesome/css/font-awesome.css';
+
+export default {
+  controller,
+  template,
+};
