cf7404d260b5959e61251a3ae9a1c638b4786229
Hyenne Guillaume <guillaume.hyenne@ovhcloud.com>
2023-07-17 17:02:34 +0200

    fix(pcc.vmware): NSX Batch 2 (#9220)
    ref: MANAGER-11064
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
    Signed-off-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/apps/dedicated/client/app/components/dedicated-cloud/users/dedicatedCloud-users.component.js
+++ packages/manager/apps/dedicated/client/app/components/dedicated-cloud/users/dedicatedCloud-users.component.js
@@ -10,6 +10,7 @@ export default {
     enableUser: '<',
     modifyUserRights: '<',
     optionsAvailable: '<',
+    hasNsxOption: '<',
     passwordReset: '<',
     productId: '<',
     setMessage: '<',
