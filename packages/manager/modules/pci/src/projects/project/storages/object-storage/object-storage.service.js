874bf1d1b44edeb1e016a38f7766bb7352857031
brofik <10513484+brofik@users.noreply.github.com>
2023-07-18 16:31:16 +0200

    fix(storage): secure the display of s3 user secret key
    ref: DTRSD-114046
    
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/pci/src/projects/project/storages/object-storage/object-storage.service.js
+++ packages/manager/modules/pci/src/projects/project/storages/object-storage/object-storage.service.js
@@ -29,6 +29,14 @@ export default class PciStoragesObjectStorageService {
   getS3Credentials(projectId, userId) {
     return this.$http
       .get(`/cloud/project/${projectId}/user/${userId}/s3Credentials`)
+      .then(({ data }) => data?.[0]);
+  }
+
+  getS3Secret(projectId, userId, userAccess) {
+    return this.$http
+      .post(
+        `/cloud/project/${projectId}/user/${userId}/s3Credentials/${userAccess}/secret`,
+      )
       .then(({ data }) => data);
   }
 
@@ -55,7 +63,7 @@ export default class PciStoragesObjectStorageService {
       this.getS3Credentials(projectId, user.id)
         .then((data) => ({
           ...user,
-          s3Credentials: data?.[0],
+          s3Credentials: data,
         }))
         .catch(() => null),
     );
@@ -73,7 +81,7 @@ export default class PciStoragesObjectStorageService {
   getAndMapUsersHaveCredentials(projectId, users) {
     const usersCredentialsPromises = users.map((user) =>
       this.getS3Credentials(projectId, user.id).then((data) =>
-        data.length > 0
+        data
           ? {
               ...user,
               s3Credentials: data,
