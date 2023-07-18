c2f4f5eb78fa3702d168acfe0f767b9b4b7183a0
sachinrameshn <sachin.ramesh@ovhcloud.com>
2023-07-18 14:17:02 +0530

    feat(dedicated): initialize new vrack services microapp
    ref: MANAGER-11827
    
    Signed-off-by: sachinrameshn <sachin.ramesh@ovhcloud.com>

---
+++ packages/manager/modules/vrack-services/src/onboarding/index.js
@@ -0,0 +1,22 @@
+import angular from 'angular';
+import '@uirouter/angularjs';
+import 'oclazyload';
+
+const moduleName = 'ovhManagerVrackServicesOnboardingLazyLoading';
+
+angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
+  /* @ngInject */ ($stateProvider) => {
+    $stateProvider.state('app.onboarding.**', {
+      url: '/onboarding',
+      lazyLoad: ($transition$) => {
+        const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+
+        return import('./module').then((mod) =>
+          $ocLazyLoad.inject(mod.default || mod),
+        );
+      },
+    });
+  },
+);
+
+export default moduleName;
