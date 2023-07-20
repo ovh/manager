6b6d6082b38ad56789f19012811475d6ad6155c4
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2023-07-20 14:39:50 +0200

    fix(skipnav): remove useless skipnav links & move translations
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/hub/src/controller.js
+++ packages/manager/apps/hub/src/controller.js
@@ -1,27 +1,13 @@
-import { isString } from 'lodash-es';
 import { getShellClient } from './shell';
 
 export default class HubController {
   /* @ngInject */
-  constructor($document) {
-    this.$document = $document;
+  constructor() {
     this.shell = getShellClient();
     this.isAccountSidebarVisible = false;
   }
 
   async $onInit() {
     this.isAccountSidebarVisible = await this.shell.ux.isAccountSidebarVisible();
-  }
-
-  /**
-   * Set focus on the specified element.
-   * @param  {string} id Element to locate.
-   * @return {void}
-   */
-  setFocus(id) {
-    if (isString(id)) {
-      const [element] = this.$document.find(`#${id}`);
-      element.focus();
-    }
   }
 }
