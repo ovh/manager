956272d4290d4ab298185903fb170ac9c52fbb00
Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
2023-08-01 09:32:25 +0200

    feat(projects.onboarding): update the onboarding page content
    ref: TAPC-82
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>

--- packages/manager/modules/pci/src/projects/onboarding/onboarding.routing.js
+++ packages/manager/modules/pci/src/projects/onboarding/onboarding.routing.js
@@ -1,3 +1,6 @@
+import { PCI_PROJECT_ORDER_CART } from '../new/constants';
+import { PCI_HDS_ADDON } from '../project/project.constants';
+
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('pci.projects.onboarding', {
     url: '/onboarding',
@@ -18,7 +21,55 @@ export default /* @ngInject */ ($stateProvider) => {
             : null,
         ),
     resolve: {
-      breadcrumb: () => null, // Hide breadcrumb
+      goToCreateNewProject: /* @ngInject */ ($state) => () =>
+        $state.go('pci.projects.new'),
+
+      goToCreateDiscoveryProject: /* @ngInject */ ($state) => () =>
+        $state.go('pci.projects.discovery'),
+
+      model: /* @ngInject */ (pciProjectNew, me) =>
+        pciProjectNew.createOrderCart(me.ovhSubsidiary).then((cart) => ({
+          agreements: false,
+          description: 'discoveryProject',
+          cart,
+          hds: cart?.hdsItem !== undefined,
+        })),
+
+      cart: /* @ngInject */ (me, pciProjectNew) =>
+        pciProjectNew
+          .createOrderCart(me.ovhSubsidiary)
+          .then((newOrderCart) =>
+            pciProjectNew.getOrderCart(me.ovhSubsidiary, newOrderCart.cartId),
+          ),
+
+      setCartProjectItem: /* @ngInject */ (model, cart, pciProjectNew) => () =>
+        pciProjectNew.setCartProjectItemDescription(cart, model.description),
+
+      summary: /* @ngInject */ (getSummary) => getSummary(),
+
+      getSummary: /* @ngInject */ (cart, orderCart, setCartProjectItem) => () =>
+        setCartProjectItem().then(() => orderCart.getSummary(cart.cartId)),
+
+      hds: /* @ngInject */ (
+        hdsAddonOption,
+        isHdsAvailable,
+        isValidHdsSupportLevel,
+      ) => ({
+        isAvailable: isHdsAvailable,
+        isCertifiedProject: false,
+        isValidForCertification: true,
+        isValidSupportLevel: isValidHdsSupportLevel,
+        isInprogressRequest: false,
+        option: hdsAddonOption,
+      }),
+
+      hdsAddonOption: /* @ngInject */ (orderCart, model) =>
+        orderCart.getHdsAddon(
+          model?.cart?.cartId,
+          PCI_PROJECT_ORDER_CART.productName,
+          PCI_PROJECT_ORDER_CART.planCode,
+          PCI_HDS_ADDON.planCode,
+        ),
     },
   });
 };
