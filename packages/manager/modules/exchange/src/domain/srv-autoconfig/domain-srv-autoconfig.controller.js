a0bb3c53a0f5088d1ad55ca2ceeb5b78aee84378
maximeVaillant <138609206+maximeVaillant@users.noreply.github.com>
2023-08-01 13:58:14 +0200

    feat(spf): add spf diag and fix mx and srv
    ref: MANAGER-11937
    
    Signed-off-by: maximeVaillant <maxime.vaillant@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/exchange/src/domain/srv-autoconfig/domain-srv-autoconfig.controller.js
+++ packages/manager/modules/exchange/src/domain/srv-autoconfig/domain-srv-autoconfig.controller.js
@@ -21,6 +21,7 @@ export default class ExchangeDomainSrvAutoconfigCtrl {
 
     this.$routerParams = wucExchange.getParams();
     this.domain = navigation.currentActionData;
+    this.domainDiag = {};
 
     this.services.ExchangeDomains.gettingDNSSettings(
       this.$routerParams.organization,
@@ -28,7 +29,35 @@ export default class ExchangeDomainSrvAutoconfigCtrl {
       this.domain.name,
     )
       .then((data) => {
-        this.domainDiag = data;
+        this.domainDiag.isOvhDomain = data.isOvhDomain;
+      })
+      .catch((failure) => {
+        navigation.resetAction();
+        messaging.writeError(
+          $translate.instant(
+            'exchange_tab_domain_diagnostic_add_field_failure',
+          ),
+          failure,
+        );
+      });
+
+    this.services.ExchangeDomains.gettingExpectedDNSSettings(
+      this.$routerParams.organization,
+      this.$routerParams.productId,
+      this.domain.name,
+    )
+      .then((data) => {
+        const re = /^([^ ]*) IN ([A-Z]*) (\d+) (\d+) (\d+) ([^ ]*)$/i;
+        const extract = data.expectedAutodiscoverSRV.match(re);
+
+        this.domainDiag.srv = {
+          fieldType: extract[2],
+          target: extract[6],
+          priority: extract[3],
+          weight: extract[4],
+          port: extract[5],
+          subDomain: extract[1],
+        };
       })
       .catch((failure) => {
         navigation.resetAction();
