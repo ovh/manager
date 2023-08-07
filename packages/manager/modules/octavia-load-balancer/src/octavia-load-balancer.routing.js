a3c7b59647a4f77ca9c53e5aa9133a962f892228
qpavy <contact@quentinpavy.com>
2023-08-07 15:42:17 +0200

    fix(pci.octavia): change breadcrumb to use breadcrumb prefix (#9811)
    ref: MANAGER-11944
    
    Signed-off-by: Quentin Pavy <contact@quentinpavy.com>

--- packages/manager/modules/octavia-load-balancer/src/octavia-load-balancer.routing.js
+++ packages/manager/modules/octavia-load-balancer/src/octavia-load-balancer.routing.js
@@ -8,26 +8,35 @@ export default /* @ngInject */ ($stateProvider) => {
         $transition$.params().projectId,
       project: /* @ngInject */ ($http, projectId) =>
         $http.get(`/cloud/project/${projectId}`).then(({ data }) => data),
-      breadcrumb: (project) =>
-        project.status !== 'creating' ? project.description : null,
-      breadcrumbUrl: /* @ngInject */ (
+      breadcrumb: ($translate) => $translate.instant('octavia_load_balancer'),
+      breadcrumbPrefix: /* @ngInject */ (
         $injector,
         $q,
         coreURLBuilder,
-        projectId,
+        project,
       ) => {
         if ($injector.has('shellClient')) {
           return $injector
             .get('shellClient')
-            .navigation.getURL('public-cloud', `#/pci/projects/${projectId}`)
-            .then((url) => url);
+            .navigation.getURL(
+              'public-cloud',
+              `#/pci/projects/${project.project_id}`,
+            )
+            .then((url) => {
+              return {
+                name:
+                  project.status !== 'creating' ? project.description : null,
+                url,
+              };
+            });
         }
-        return $q.when(
-          coreURLBuilder.buildURL(
+        return $q.when({
+          name: 'projectNAME',
+          url: coreURLBuilder.buildURL(
             'public-cloud',
-            `#/pci/projects/${projectId}`,
+            `#/pci/projects/${project.project_id}`,
           ),
-        );
+        });
       },
     },
   });
