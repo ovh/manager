f104bce7caf64df914c20104a823a70ce00fc0c6
Quentin Pavy <contact@quentinpavy.com>
2023-08-28 09:37:18 +0200

    feat(dedicated): split translations into multiple subs
    ref: MANAGER-11979
    
    Signed-off-by: Quentin Pavy <contact@quentinpavy.com>

--- packages/manager/apps/dedicated/client/app/account/kyc-documents/dashboard/controller.js
+++ packages/manager/apps/dedicated/client/app/account/kyc-documents/dashboard/controller.js
@@ -1,5 +1,10 @@
 import { FRAUD_STATUS } from '../constants';
-import { DOCUMENT_TYPE, DOCUMENT_LIST, MAXIMUM_SIZE } from './constants';
+import {
+  DOCUMENT_TYPE,
+  DOCUMENT_LIST,
+  MAXIMUM_SIZE,
+  LEGAL_FORMS,
+} from './constants';
 
 export default class KycDocumentsCtrl {
   /* @ngInject */
@@ -15,8 +20,11 @@ export default class KycDocumentsCtrl {
     this.documents = [];
 
     // personnalcorporation user is like corporation user
-    if (this.user.legalform === 'personalcorporation')
-      this.user.legalform = 'corporation';
+    if (this.user.legalform === LEGAL_FORMS.PERSONALCORPORATION)
+      this.user.legalform = LEGAL_FORMS.CORPORATION;
+    // other user is like individual user
+    if (this.user.legalform === LEGAL_FORMS.OTHER)
+      this.user.legalform = LEGAL_FORMS.INDIVIDUAL;
 
     // retrieve mandatory / optionnal documents
     this.getDocumentList();
@@ -24,21 +32,26 @@ export default class KycDocumentsCtrl {
 
   getDocumentList() {
     const documentTypeList = DOCUMENT_LIST[this.user.legalform];
-    const documentList = [];
-    documentTypeList.forEach((documentType, index) => {
-      const key =
-        documentType !== this.DOCUMENT_TYPE.OTHER
-          ? `documents_dashboard_document_${documentType}`
-          : `documents_dashboard_document_${this.user.legalform}_${documentType}`;
-      this.$translate(`${key}_${this.user.ovhSubsidiary}`).then(
-        (translation) => {
-          documentList[index] =
-            translation !== `${key}_${this.user.ovhSubsidiary}`
-              ? translation
-              : this.$translate.instant(`${key}_DEFAULT`);
-          this.documentList = documentList;
-        },
-      );
+    const documentList = {
+      mandatory: [],
+      optional: [],
+    };
+    Object.keys(documentTypeList).forEach((type) => {
+      documentTypeList[type].forEach((documentType, index) => {
+        const key =
+          documentType !== this.DOCUMENT_TYPE.OTHER
+            ? `documents_dashboard_document_${documentType}`
+            : `documents_dashboard_document_${this.user.legalform}_${documentType}`;
+        this.$translate(`${key}_${this.user.ovhSubsidiary}`).then(
+          (translation) => {
+            documentList[type][index] =
+              translation !== `${key}_${this.user.ovhSubsidiary}`
+                ? translation
+                : this.$translate.instant(`${key}_DEFAULT`);
+            this.documentList = documentList;
+          },
+        );
+      });
     });
   }
 
--- packages/manager/apps/dedicated/client/app/account/kyc-documents/dashboard/module.js
+++ packages/manager/apps/dedicated/client/app/account/kyc-documents/dashboard/module.js
@@ -18,6 +18,18 @@ angular
   ])
   .config(routing)
   .component('kycDocuments', component)
-  .run(/* @ngTranslationsInject:json ./translations */);
+  .run(/* @ngTranslationsInject:json ./translations */)
+  .run(/* @ngTranslationsInject:json ./translations/DE */)
+  .run(/* @ngTranslationsInject:json ./translations/DEFAULT */)
+  .run(/* @ngTranslationsInject:json ./translations/ES */)
+  .run(/* @ngTranslationsInject:json ./translations/FR */)
+  .run(/* @ngTranslationsInject:json ./translations/IT */)
+  .run(/* @ngTranslationsInject:json ./translations/MA */)
+  .run(/* @ngTranslationsInject:json ./translations/PL */)
+  .run(/* @ngTranslationsInject:json ./translations/PT */)
+  .run(/* @ngTranslationsInject:json ./translations/QC */)
+  .run(/* @ngTranslationsInject:json ./translations/SN */)
+  .run(/* @ngTranslationsInject:json ./translations/TN */)
+  .run(/* @ngTranslationsInject:json ./translations/WS */);
 
 export default moduleName;
