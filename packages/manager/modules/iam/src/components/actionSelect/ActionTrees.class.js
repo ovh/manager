cf3dc548fffc6a37b672b72e5b4d82f994416cde
Bruno MARQUES (ext) <bruno.marques.ext@corp.ovh.com>
2023-08-29 13:08:38 +0200

    feat(iam): add onhover description for actions
    ref: MANAGER-11346
    
    Signed-off-by: Bruno MARQUES <bruno.marques.ext@corp.ovh.com>

--- packages/manager/modules/iam/src/components/actionSelect/ActionTrees.class.js
+++ packages/manager/modules/iam/src/components/actionSelect/ActionTrees.class.js
@@ -1,5 +1,6 @@
 /**
  * @typedef {{
+ *   description: string
  *   resourceType: string
  *   selected: boolean
  *   value: string
@@ -184,6 +185,7 @@ export default class ActionTrees extends Array {
       .map((action) => {
         const value = action.action;
         return {
+          description: action.description,
           resourceType: action.resourceType,
           selected: Boolean(
             input.selectedActions?.find(
