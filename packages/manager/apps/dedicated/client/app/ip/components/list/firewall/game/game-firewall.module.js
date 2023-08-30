c15a6cf1b01153ce96a72514c2505c4917278656
Stephanie Moallic <stephanie.moallic@corp.ovh.com>
2023-08-30 10:01:25 +0200

    feat(dedicated.ip): configure game firewall - add rule
    ref: UXCT-482
    
    - add line to create new rule
    - add button to create new rule
    - add button to cancel creation
    
    Signed-off-by: Stephanie Moallic <stephanie.moallic@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/ip/components/list/firewall/game/game-firewall.module.js
+++ packages/manager/apps/dedicated/client/app/ip/components/list/firewall/game/game-firewall.module.js
@@ -1,8 +1,6 @@
 import routing from './game-firewall.routing';
 import service from './ip-ip-firewall-game.service';
 
-import ruleAddController from './rule/add/ip-ip-firewall-game-rule-add.controller';
-import ruleAddTemplate from './rule/add/ip-ip-firewall-game-rule-add.html';
 import ruleDeleteController from './rule/delete/ip-ip-firewall-game-rule-delete.controller';
 import ruleDeleteTemplate from './rule/delete/ip-ip-firewall-game-rule-delete.html';
 import toggleController from './toggle/ip-ip-firewall-game-toggle.controller';
@@ -13,16 +11,11 @@ const moduleName = 'ovhManagerIpDashboardGameFirewall';
 angular
   .module(moduleName, [])
   .config(routing)
-  .controller('AddGameFirewallRuleCtrl', ruleAddController)
   .controller('EnableDisableGameFirewallRuleCtrl', toggleController)
   .controller('RemoveGameFirewallRuleCtrl', ruleDeleteController)
   .service('IpGameFirewall', service)
   .run(
     /* @ngInject */ ($templateCache) => {
-      $templateCache.put(
-        'ip/firewall/game/rule/add/ip-ip-firewall-game-rule-add.html',
-        ruleAddTemplate,
-      );
       $templateCache.put(
         'ip/firewall/game/rule/delete/ip-ip-firewall-game-rule-delete.html',
         ruleDeleteTemplate,
