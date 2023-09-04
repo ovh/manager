ca79bd68adfc7b8023ea793de28288eb16328fc3
Hyenne Guillaume <guillaume.hyenne@ovhcloud.com>
2023-09-04 09:46:25 +0200

    feat(web): add dkim feature (#9693)
    ref: MANAGER-11858
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/emailpro/src/domain/emailpro-domain.service.js
+++ packages/manager/modules/emailpro/src/domain/emailpro-domain.service.js
@@ -3,7 +3,11 @@ import isEmpty from 'lodash/isEmpty';
 import pick from 'lodash/pick';
 import transform from 'lodash/transform';
 
-export default /* @ngInject */ function EmailProDomains(EmailPro, OvhHttp) {
+export default /* @ngInject */ function EmailProDomains(
+  $http,
+  EmailPro,
+  OvhHttp,
+) {
   this.getDomains = function getDomains(
     serviceName,
     pageSize,
@@ -181,5 +185,17 @@ export default /* @ngInject */ function EmailProDomains(EmailPro, OvhHttp) {
         domain,
       },
     });
+  };
+
+  this.getDkimSelector = function getDkimSelector(serviceName, domain) {
+    return $http
+      .get(`/email/pro/${serviceName}/domain/${domain}/dkimSelector`)
+      .then(({ data }) => data);
+  };
+
+  this.postDkim = function postDkim(serviceName, domain, params) {
+    return $http
+      .post(`/email/pro/${serviceName}/domain/${domain}/dkim`, params)
+      .then(({ data }) => data);
   };
 }
