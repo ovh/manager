e198097c29e974eec4a7bb4d17bed87139e20610
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2023-09-04 09:40:58 +0200

    feat(public-cloud): allow storage decrease (#9612)
    ref: DATATR-311
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/pci/src/projects/project/storages/databases/database/general-information/upgrade-storage/upgrade-storage.module.js
+++ packages/manager/modules/pci/src/projects/project/storages/databases/database/general-information/upgrade-storage/upgrade-storage.module.js
@@ -4,12 +4,13 @@ import component from './upgrade-storage.component';
 import routing from './upgrade-storage.routing';
 
 import diskSizeComponent from '../../../components/disk-size';
+import switchPriceComponent from '../../../components/switch-price';
 
 const moduleName =
   'ovhManagerPciStoragesDatabaseGeneralInformationUpgradeStorage';
 
 angular
-  .module(moduleName, [diskSizeComponent])
+  .module(moduleName, [diskSizeComponent, switchPriceComponent])
   .config(routing)
   .component(
     'ovhManagerPciProjectDatabaseGeneralInformationUpgradeStorage',
