b48db71195dff550c04923c004c7c0ecd6050686
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2023-09-05 12:35:57 +0200

    fix: update cli links
    DATATR-316
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/data-integration/tabs/cli/cli.constants.js
+++ packages/manager/modules/pci/src/projects/project/data-integration/tabs/cli/cli.constants.js
@@ -2,17 +2,17 @@ export const CLI_INSTALL_CODES = [
   {
     id: 'linux',
     title: 'Linux',
-    code: `curl -s https://github.com/ovh/ovhdata-cli/releases/latest/download/ovhdata-cli-linux-amd64`,
+    code: `curl -s https://github.com/ovh/ovhdata-cli/releases/latest/download/ovhdata-cli_linux_amd64`,
   },
   {
     id: 'mac',
     title: 'Mac',
-    code: `curl -s https://github.com/ovh/ovhdata-cli/releases/latest/download/ovhdata-cli-darwin-amd64`,
+    code: `curl -s https://github.com/ovh/ovhdata-cli/releases/latest/download/ovhdata-cli_darwin_amd64`,
   },
   {
     id: 'windows',
     title: 'Windows',
-    code: `curl -s https://github.com/ovh/ovhdata-cli/releases/latest/download/ovhdata-cli-windows.exe`,
+    code: `curl -s https://github.com/ovh/ovhdata-cli/releases/latest/download/ovhdata-cli_windows_amd64.exe`,
   },
 ];
 
