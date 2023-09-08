b19f527bf2a96d48531b16cac8cf0a5bd0b5c8ac
qpavy <contact@quentinpavy.com>
2023-09-08 14:03:57 +0200

    feat(octavia): add ocatvia  (#9948)
    ref: MANAGER-11943
    Signed-off-by: Quentin Pavy <contact@quentinpavy.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

---
+++ packages/manager/modules/octavia-load-balancer/src/octavia-load-balancer.routing.js
@@ -0,0 +1,47 @@
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state('octavia-load-balancer', {
+    url: '/pci/projects/{projectId:[0-9a-zA-Z]{32}}/octavia-load-balancer',
+    template: '<div data-ui-view></div>',
+    redirectTo: 'octavia-load-balancer.onboarding',
+    resolve: {
+      projectId: /* @ngInject */ ($transition$) =>
+        $transition$.params().projectId,
+      project: /* @ngInject */ ($http, projectId) =>
+        $http.get(`/cloud/project/${projectId}`).then(({ data }) => data),
+      breadcrumb: ($translate) => $translate.instant('octavia_load_balancer'),
+      breadcrumbPrefix: /* @ngInject */ (
+        $injector,
+        $q,
+        coreURLBuilder,
+        project,
+      ) => {
+        if ($injector.has('shellClient')) {
+          return $injector
+            .get('shellClient')
+            .navigation.getURL(
+              'public-cloud',
+              `#/pci/projects/${project.project_id}`,
+            )
+            .then((url) => {
+              return [
+                {
+                  name:
+                    project.status !== 'creating' ? project.description : null,
+                  url,
+                },
+              ];
+            });
+        }
+        return $q.when([
+          {
+            name: project.status !== 'creating' ? project.description : null,
+            url: coreURLBuilder.buildURL(
+              'public-cloud',
+              `#/pci/projects/${project.project_id}`,
+            ),
+          },
+        ]);
+      },
+    },
+  });
+};
