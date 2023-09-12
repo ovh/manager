294d64151de4052aa4d1361cd006891ff2559874
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2023-09-12 17:08:09 +0200

    feat(dedicated.server): create new component
    ref: MANAGER-12283
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

---
+++ packages/manager/apps/dedicated/client/app/dedicated/dedicated-server/servers/monitoring/index.js
@@ -0,0 +1,19 @@
+import angular from 'angular';
+
+import '@uirouter/angularjs';
+import 'angular-translate';
+import '@ovh-ux/ui-kit';
+
+import component from './monitoring.component';
+import routing from './monitoring.routing';
+
+const moduleName = 'ovhManagerDedicatedServerMonitoring';
+
+angular
+  .module(moduleName, ['oui', 'pascalprecht.translate', 'ui.router'])
+  .component('dedicatedServerMonitoring', component)
+  .config(routing)
+  .run(/* @ngTranslationsInject:json ../translations */)
+  .run(/* @ngTranslationsInject:json ./translations */);
+
+export default moduleName;
