55542bc9828edeae081592452534df022b0f154f
David Arsène <david.arsene.ext@ovhcloud.com>
2023-09-15 16:07:31 +0200

    feat(dedicated): create dedicated server bandwidth tile component
    ref: MANAGER-12285
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

---
+++ packages/manager/modules/bm-server-components/src/bandwidth-tile/component.js
@@ -0,0 +1,14 @@
+import template from './template.html';
+
+export default {
+  template,
+  bindings: {
+    server: '<',
+    bandwidth0ption: '<',
+    bandwidthVrackOption: '<',
+    bandwidthVrackOrderOption: '<',
+    orderPrivateLink: '<',
+    orderPublicLink: '<',
+    specifications: '<',
+  },
+};
