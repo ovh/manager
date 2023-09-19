c2a57531a44220fb5fb26871b1713b897d1c1bcf
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2023-09-19 17:05:07 +0200

    refactor(dedicated-server): move the displayname composent
    ref: MANAGER-12284
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

---
+++ packages/manager/modules/bm-server-components/src/display-name/index.js
@@ -0,0 +1,20 @@
+import angular from 'angular';
+
+import '@ovh-ux/ui-kit';
+import 'angular-translate';
+import '@ovh-ux/ng-translate-async-loader';
+
+import component from './component';
+
+const moduleName = 'ovhManagerBmServerComponentsDisplayName';
+
+angular
+  .module(moduleName, [
+    'oui',
+    'pascalprecht.translate',
+    'ngTranslateAsyncLoader',
+  ])
+  .component('serverDisplayName', component)
+  .run(/* @ngTranslationsInject:json ./translations */);
+
+export default moduleName;
