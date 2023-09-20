3e998575e7f3ead963d0ad15f285104fd639ccf3
Nicolas Pierre-charles <nicolas@nicode.fr>
2023-09-20 13:28:19 +0200

    feat: msc-tail-logs
    ref: MANAGER-11671
    
    Signed-off-by: Nicolas Pierre-charles <nicolas@nicode.fr>

---
+++ packages/super-components/components/msc-tail-logs/src/global.test.ts
@@ -0,0 +1,5 @@
+import './global';
+import '@ovhcloud/ods-stencil/components/text';
+import '@ovhcloud/ods-stencil/components/skeleton';
+import '@ovhcloud/ods-stencil/components/code';
+import '@ovhcloud/ods-stencil/components/toggle';
