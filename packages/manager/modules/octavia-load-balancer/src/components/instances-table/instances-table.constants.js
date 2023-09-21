124320dc3f28dbb4f1c3279803f31430a661d843
qpavy <contact@quentinpavy.com>
2023-09-21 14:44:22 +0200

    feat(octavia): prepare listener table  (#10006)
    ref: MANAGER-10673
    
    Signed-off-by: Quentin Pavy <contact@quentinpavy.com>

---
+++ packages/manager/modules/octavia-load-balancer/src/components/instances-table/instances-table.constants.js
@@ -0,0 +1,9 @@
+export const LISTENER_PROTOCOL_LIST = [{ name: 'http', defaultPort: 80 }];
+
+export const TRACKING_ADD_LISTENER =
+  'pci::projects::project::octavia-loadbalancer::add::add-listener';
+
+export default {
+  LISTENER_PROTOCOL_LIST,
+  TRACKING_ADD_LISTENER,
+};
