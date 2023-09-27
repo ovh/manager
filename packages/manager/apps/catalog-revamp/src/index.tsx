8f8cfd649bc1fc520a6ae2866bc62a4494c0f5de
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2023-09-27 09:49:09 +0200

    fix(generator): fix i18n in catalog
    ref: MANAGER-11852
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager/apps/catalog-revamp/src/index.tsx
+++ packages/manager/apps/catalog-revamp/src/index.tsx
@@ -1,5 +1,13 @@
 import { startApplication } from '@ovh-ux/manager-react-core-application';
+import { initShellClient } from '@ovh-ux/shell';
+import { setShellClient } from './shell';
 import 'bootstrap/scss/bootstrap-utilities.scss';
 import '@ovhcloud/ods-theme-blue-jeans/index.css';
 
-startApplication('catalog-revamp');
+initShellClient('catalog-revamp')
+  .then((client) => {
+    return setShellClient(client);
+  })
+  .then(() => {
+    startApplication('catalog-revamp');
+  });
