7c0550b8b7b9ded0e848e803d3b63f1a01409526
stif59100 <steeve.vanderstocken@ovhcloud.com>
2023-10-02 11:08:00 +0200

    feat(generator.css): migration bootstrap to tailwindcss
    ref:MANAGER-11981
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

--- packages/manager/core/generator/app/conditional-templates/dashboard/[serviceName]/Tabs1.tsx
+++ packages/manager/core/generator/app/conditional-templates/dashboard/[serviceName]/Tabs1.tsx
@@ -1,6 +1,6 @@
 import React, { lazy } from 'react';
 import { MscBillingTile } from '@ovhcloud/msc-react-billing-tile';
-import { useParams, } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useEnvironment } from '@ovh-ux/manager-react-core-application';
 import { Locale } from '@ovhcloud/msc-utils';
 
@@ -10,8 +10,20 @@ function Tabs1() {
   const { serviceName } = useParams();
 
   return (
-    <div className="dashboard-section pt-4">
-      <div>
+    <div className="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 py-6">
+      <div className="p-3">
+        <MscBillingTile
+          servicePath={`{{this.apiPath}}/${serviceName}`}
+          locale={locale}
+        />
+      </div>
+      <div className="p-3">
+        <MscBillingTile
+          servicePath={`{{this.apiPath}}/${serviceName}`}
+          locale={locale}
+        />
+      </div>
+      <div className="p-3">
         <MscBillingTile
           servicePath={`{{this.apiPath}}/${serviceName}`}
           locale={locale}
