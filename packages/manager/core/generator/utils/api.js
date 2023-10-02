8828469b2c2a20d82e2de3d306ac7f86367fa1d4
Nicolas Pierre-charles <nicolas@nicode.fr>
2023-10-02 17:32:13 +0200

    feat(generator): add listing of v2 endpoints + multiple endpoints choices
    ref: MANAGER-12378
    
    Signed-off-by: Nicolas Pierre-charles <nicolas@nicode.fr>

--- packages/manager/core/generator/utils/api.js
+++ packages/manager/core/generator/utils/api.js
@@ -1,21 +1,54 @@
 import axios from 'axios';
+
+const v2Endpoint = 'https://eu.api.ovh.com/v2';
+const v6Endpoint = 'https://api.ovh.com/1.0';
+
+const v2Prefix = 'v2-';
+const v6Prefix = 'v6-';
+
+export const isV2Endpoint = (apiPath) => apiPath?.startsWith(v2Prefix);
+
+export const removeApiVersionPrefix = (apiPath) =>
+  apiPath.substring(v2Prefix.length);
+
 /**
- * @returns the list of API v6 services endpoints
+ * @returns the list of API v6 and v2 services endpoints
  */
 export const getApiPaths = async () => {
-  const response = await axios.get('https://api.ovh.com/1.0');
-  return response.data.apis.map(({ path }) => path);
+  const v2response = await axios.get(v2Endpoint);
+  const v6response = await axios.get(v6Endpoint);
+
+  return [
+    {
+      type: 'separator',
+      line: `V2 endpoints from ${v2response.data.basePath}`,
+    },
+    { type: 'separator' },
+    ...v2response.data.apis.map(({ path }) => ({
+      name: path,
+      value: `${v2Prefix}${path}`,
+    })),
+    { type: 'separator' },
+    {
+      type: 'separator',
+      line: `V6 endpoints from ${v6response.data.basePath}`,
+    },
+    { type: 'separator' },
+    ...v6response.data.apis.map(({ path }) => ({
+      name: path,
+      value: `${v6Prefix}${path}`,
+    })),
+    { type: 'separator' },
+  ];
 };
 
 /**
- * @returns the list of API v6 operations of a specific service
+ * @returns the list of API operations of a specific service
  */
 export const getApiServiceOperations = async (apiPath) => {
-  const response = await axios.get(`https://api.ovh.com/1.0${apiPath}.json`);
+  const endpoint = isV2Endpoint(apiPath) ? v2Endpoint : v6Endpoint;
+  const response = await axios.get(
+    `${endpoint}${removeApiVersionPrefix(apiPath)}.json`,
+  );
   return response.data.apis;
-};
-
-export default {
-  getApiPaths,
-  getApiServiceOperations,
 };
