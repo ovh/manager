a951c9aa5c83f5fa7e3e64e37648c12ed541c0e6
Antony MARION <antony.marion.ext@ovhcloud.com>
2023-10-12 16:29:28 +0200

    feat(web-cloud.exchange): add DKIM configurator for no ovh cloud
       ref: MANAGER-12208
    
    Signed-off-by: Antony MARION <antony.marion.ext@ovhcloud.com>

--- packages/manager/modules/exchange/src/domain/dkim-autoconfig/domain-dkim-autoconfig.controller.js
+++ packages/manager/modules/exchange/src/domain/dkim-autoconfig/domain-dkim-autoconfig.controller.js
@@ -36,6 +36,9 @@ export default class ExchangeDomainDkimAutoconfigCtrl {
 
     this.loading = true;
 
+    // Vars for DKIM configuration inside modal stepper
+    this.initializeDkimConfiguratorNoOvh();
+
     this.services.ExchangeDomains.gettingDNSSettings(
       this.$routerParams.organization,
       this.$routerParams.productId,
@@ -45,6 +48,10 @@ export default class ExchangeDomainDkimAutoconfigCtrl {
         this.domainDiag = data;
 
         this.hideConfirmButton = this.hideConfirmButton();
+
+        this.dkimForNoOvhCloud =
+          this.dkimStatus === this.GLOBAL_DKIM_STATUS.NOT_CONFIGURED &&
+          !this.domainDiag.isOvhDomain;
 
         this.loading = false;
       })
@@ -59,6 +66,28 @@ export default class ExchangeDomainDkimAutoconfigCtrl {
       });
 
     $scope.onFinishDkim = () => this.onFinishDkim();
+
+    // DKIM configurator methods for no Ovh Cloud
+    $scope.loadDataForDkim = () => this.loadDataForDkim();
+    $scope.getDkimSelector = () => this.getDkimSelector();
+    $scope.leaveDkimConfigurator = () => this.leaveDkimConfigurator();
+    $scope.getNextButtonDependingOnStep = () =>
+      this.getNextButtonDependingOnStep();
+    $scope.getTitleDependingOnStep = () => this.getTitleDependingOnStep();
+    $scope.initSpfContext = () => this.initSpfContext();
+  }
+
+  initializeDkimConfiguratorNoOvh() {
+    this.selector1NameInfos = '';
+    this.selector1RecordInfos = '';
+    this.selector2NameInfos = '';
+    this.selector2RecordInfos = '';
+    this.dkimSelectorsNoDomain = null;
+    this.selector1NoDomain = null;
+    this.selector2NoDomain = null;
+    this.isStepConfigureValid = false;
+    this.showConfiguratingBtn = true;
+    this.showSpfDiagnosticTitle = false;
   }
 
   hideConfirmButton() {
@@ -113,25 +142,136 @@ export default class ExchangeDomainDkimAutoconfigCtrl {
     });
   }
 
-  configDkim() {
-    this.services.ExchangeDomains.getDkimSelector(
+  async getSelectorNameForNoOvhCloud() {
+    if (!this.dkimSelectorsNoDomain) {
+      return;
+    }
+    const promises = this.dkimSelectorsNoDomain.map((dkimSelector) => {
+      return this.services.ExchangeDomains.getDkimSelectorName(
+        this.$routerParams.organization,
+        this.$routerParams.productId,
+        this.domain.name,
+        dkimSelector,
+      );
+    });
+
+    const [selector1NoDomain, selector2NoDomain] = await this.services.$q.all(
+      promises,
+    );
+    this.selector1NoDomain = selector1NoDomain;
+    this.selector1NameInfos = this.getDkimName(1);
+    this.selector1RecordInfos = this.getDkimRecord(1);
+    this.selector2NoDomain = selector2NoDomain;
+    this.selector2NameInfos = this.getDkimName(2);
+    this.selector2RecordInfos = this.getDkimRecord(2);
+  }
+
+  async loadDataForDkim() {
+    this.showConfiguratingBtn = false;
+    this.showSpfDiagnosticTitle = false;
+    this.loading = true;
+    await this.stepConfigureDkim();
+    this.isStepConfigureValid = true;
+    this.loading = false;
+  }
+
+  getDkimSelectorForCurrentState() {
+    return this.services.ExchangeDomains.getDkimSelector(
       this.$routerParams.organization,
       this.$routerParams.productId,
       this.domain.name,
-    )
+    );
+  }
+
+  getDkimSelector() {
+    return this.getDkimSelectorForCurrentState().then((dkimSelectors) => {
+      this.dkimSelectorsNoDomain = dkimSelectors;
+    });
+  }
+
+  leaveDkimConfigurator() {
+    this.services.navigation.resetAction();
+    return this.initializeDkimConfiguratorNoOvh();
+  }
+
+  postDkimFor(selectors) {
+    return selectors.map((dkimSelector, index) => {
+      return this.services.ExchangeDomains.postDkim(
+        this.$routerParams.organization,
+        this.$routerParams.productId,
+        this.domain.name,
+        {
+          selectorName: dkimSelector,
+          autoEnableDKIM: index === 0,
+          configureDkim: true,
+        },
+      );
+    });
+  }
+
+  stepConfigureDkim() {
+    const promises = this.postDkimFor(this.dkimSelectorsNoDomain);
+    return this.services.$q
+      .all(promises)
+      .then(() => {
+        this.services.messaging.writeSuccess(
+          this.services.$translate.instant(
+            'exchange_tab_domain_diagnostic_dkim_activation_success',
+          ),
+        );
+      })
+      .catch(() => {
+        this.leaveDkimConfigurator();
+        this.services.messaging.writeError(
+          this.services.$translate.instant(
+            'exchange_tab_domain_diagnostic_dkim_configurate_no_ovhcloud_failed',
+          ),
+        );
+      });
+  }
+
+  initSpfContext() {
+    this.showSpfDiagnosticTitle = true;
+    return this.getSelectorNameForNoOvhCloud();
+  }
+
+  getTitleDependingOnStep() {
+    return this.showSpfDiagnosticTitle
+      ? 'exchange_tab_domain_diagnostic_dkim_configurate_no_ovhcloud'
+      : 'exchange_tab_domain_diagnostic_dkim_next_no_ovhcloud';
+  }
+
+  getNextButtonDependingOnStep() {
+    return this.showConfiguratingBtn
+      ? 'exchange_tab_domain_diagnostic_dkim_title_configuration'
+      : 'exchange_tab_domain_diagnostic_spf_title';
+  }
+
+  getDkimName(index) {
+    return [
+      this.services.$translate.instant(
+        'exchange_tab_domain_diagnostic_dkim_name',
+      ),
+      `${index}: `,
+      this[`selector${index}NoDomain`].customerRecord,
+    ].join('');
+  }
+
+  getDkimRecord(index) {
+    return [
+      this.services.$translate.instant(
+        'exchange_tab_domain_diagnostic_dkim_record',
+      ),
+      `${index}: `,
+      this[`selector${index}NoDomain`].targetRecord,
+    ].join('');
+  }
+
+  configDkim() {
+    this.this
+      .getDkimSelectorForCurrentState()
       .then((dkimSelectors) => {
-        const promises = dkimSelectors.map((dkimSelector, index) => {
-          return this.services.ExchangeDomains.postDkim(
-            this.$routerParams.organization,
-            this.$routerParams.productId,
-            this.domain.name,
-            {
-              selectorName: dkimSelector,
-              autoEnableDKIM: index === 0,
-              configureDkim: true,
-            },
-          );
-        });
+        const promises = this.postDkimFor(dkimSelectors);
         return this.services.$q.all(promises);
       })
       .then(() => {
