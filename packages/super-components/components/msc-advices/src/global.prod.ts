c0439e5b315f2ac9a864a11bf04f6e99bf32875e
Nicolas BAPTISTA <nicolas.baptista.ext@corp.ovh.com>
2023-10-16 17:27:25 +0200

    feat: msc-advices
    ref: MANAGER-11674
    
    Signed-off-by: Nicolas BAPTISTA <nicolas.baptista.ext@corp.ovh.com>

---
+++ packages/super-components/components/msc-advices/src/global.prod.ts
@@ -0,0 +1,6 @@
+import './global';
+import '@ovhcloud/ods-components/text';
+import '@ovhcloud/ods-components/skeleton';
+import '@ovhcloud/ods-components/divider';
+import '@ovhcloud/ods-components/tile';
+import '@ovhcloud/ods-components/icon';
