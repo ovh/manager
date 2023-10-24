8b24a8876506846dc0c878c2be6c6d9e622bf9c0
qpavy <contact@quentinpavy.com>
2023-10-24 08:20:30 +0200

    feat(octavia): add listeners to lb creation (#10050)
    ref: MANAGER-10670
    
    Signed-off-by: Quentin Pavy <contact@quentinpavy.com>

--- packages/manager/modules/octavia-load-balancer/src/components/instances-table/instances-table.controller.js
+++ packages/manager/modules/octavia-load-balancer/src/components/instances-table/instances-table.controller.js
@@ -1,28 +1,104 @@
 import {
   LISTENER_PROTOCOL_LIST,
+  PROTOCOLS,
   TRACKING_ADD_LISTENER,
 } from './instances-table.constants';
 
 export default class RegionsListController {
   /* @ngInject */
-  constructor(atInternet) {
+  constructor(
+    $http,
+    $translate,
+    atInternet,
+    OctaviaLoadBalancerInstanceService,
+  ) {
+    this.$http = $http;
+    this.$translate = $translate;
     this.listenerProtocols = LISTENER_PROTOCOL_LIST;
     this.atInternet = atInternet;
+    this.OctaviaLoadBalancerInstanceService = OctaviaLoadBalancerInstanceService;
   }
 
   $onInit() {
-    this.listeners = [];
+    this.instances = [];
+    this.healthMonitorsInit();
+  }
+
+  $onChanges(changes) {
+    if (changes.region.currentValue?.name) {
+      this.OctaviaLoadBalancerInstanceService.getRegionInstances(
+        this.projectId,
+        changes.region.currentValue.name,
+      ).then((instances) => {
+        this.instances = instances;
+      });
+    }
+  }
+
+  healthMonitorsInit() {
+    this.listenerProtocols.forEach((listenerProtocol) => {
+      if (listenerProtocol.healthMonitors) {
+        listenerProtocol.healthMonitors.unshift({
+          name: this.$translate.instant(
+            'octavia_load_balancer_instances_table_health_monitor_empty',
+          ),
+          value: null,
+        });
+      }
+    });
   }
 
   addListener() {
-    this.atInternet.trackClick({
-      name: TRACKING_ADD_LISTENER,
-      type: 'action',
-    });
-    this.listeners.push({});
+    if (!this.listeners) this.listeners = [];
+    if (this.listeners.length < this.maxListener) {
+      this.atInternet.trackClick({
+        name: TRACKING_ADD_LISTENER,
+        type: 'action',
+      });
+      this.listeners.push({
+        instances: [{}],
+      });
+    }
+  }
+
+  addInstance(listener) {
+    if (listener.instances.length < this.maxInstancesByListener) {
+      listener.instances.push({});
+    }
+  }
+
+  static onInstanceSelected(instance, listener) {
+    const instanceModel = instance;
+    instanceModel.port = RegionsListController.initInstancePort(listener);
   }
 
   deleteListener(index) {
     this.listeners.splice(index, 1);
+  }
+
+  static deleteInstance(instances, index) {
+    instances.splice(index, 1);
+  }
+
+  static selectListenerProtocol(protocol, listener) {
+    const listenerModel = listener;
+    if (!listenerModel.port) listenerModel.port = protocol.defaultPort;
+  }
+
+  static initInstancePort(listener) {
+    if ([PROTOCOLS.HTTP, PROTOCOLS.HTTPS].includes(listener.protocol.value))
+      return 80;
+    return null;
+  }
+
+  static isPoolSectionDisabled(listener) {
+    if (
+      listener.protocol &&
+      listener.protocol.value !== PROTOCOLS.PROMETHEUS &&
+      listener.port
+    ) {
+      return false;
+    }
+    return true;
   }
 }
