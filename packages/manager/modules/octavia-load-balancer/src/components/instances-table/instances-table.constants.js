1b5a027154c468859ff5d863cb28ea5c1ab508da
qpavy <contact@quentinpavy.com>
2023-10-24 08:20:30 +0200

    feat(octavia): add listeners to lb creation (#10050)
    ref: MANAGER-10670
    
    Signed-off-by: Quentin Pavy <contact@quentinpavy.com>

--- packages/manager/modules/octavia-load-balancer/src/components/instances-table/instances-table.constants.js
+++ packages/manager/modules/octavia-load-balancer/src/components/instances-table/instances-table.constants.js
@@ -1,9 +1,78 @@
-export const LISTENER_PROTOCOL_LIST = [{ name: 'http', defaultPort: 80 }];
+export const PROTOCOLS = {
+  HTTP: 'http',
+  HTTPS: 'https',
+  TCP: 'tcp',
+  UDP: 'udp',
+  PROMETHEUS: 'prometheus',
+};
+
+export const HEALTH_MONITORS = {
+  HTTP_GET: 'http',
+  HTTPS_GET: 'https',
+  PING: 'ping',
+  SCTP: 'sctp',
+  TCP: 'tcp',
+  TLS_HELLO: 'tls-hello',
+  UDP_CONNECT: 'udp-connect',
+};
+
+export const LISTENER_PROTOCOL_LIST = [
+  {
+    name: 'HTTP',
+    value: PROTOCOLS.HTTP,
+    defaultPort: 80,
+    healthMonitors: [
+      { name: 'HTTP GET', value: HEALTH_MONITORS.HTTP_GET },
+      { name: 'HTTPS GET', value: HEALTH_MONITORS.HTTPS_GET },
+      { name: 'PING', value: HEALTH_MONITORS.PING },
+      { name: 'TCP', value: HEALTH_MONITORS.TCP },
+      { name: 'TLS-HELLO', value: HEALTH_MONITORS.TLS_HELLO },
+    ],
+  },
+  {
+    name: 'HTTPS',
+    value: PROTOCOLS.HTTPS,
+    defaultPort: 443,
+    healthMonitors: [
+      { name: 'HTTP GET', value: HEALTH_MONITORS.HTTP_GET },
+      { name: 'HTTPS GET', value: HEALTH_MONITORS.HTTPS_GET },
+      { name: 'PING', value: HEALTH_MONITORS.PING },
+      { name: 'TCP', value: HEALTH_MONITORS.TCP },
+      { name: 'TLS-HELLO', value: HEALTH_MONITORS.TLS_HELLO },
+    ],
+  },
+  {
+    name: 'TCP',
+    value: PROTOCOLS.TCP,
+    healthMonitors: [
+      { name: 'HTTP GET', value: HEALTH_MONITORS.HTTP_GET },
+      { name: 'HTTPS GET', value: HEALTH_MONITORS.HTTPS_GET },
+      { name: 'PING', value: HEALTH_MONITORS.PING },
+      { name: 'TCP', value: HEALTH_MONITORS.TCP },
+      { name: 'TLS-HELLO', value: HEALTH_MONITORS.TLS_HELLO },
+    ],
+  },
+  {
+    name: 'UDP',
+    value: PROTOCOLS.UDP,
+    healthMonitors: [
+      { name: 'HTTP GET', value: HEALTH_MONITORS.HTTP_GET },
+      { name: 'SCTP', value: HEALTH_MONITORS.SCTP },
+      { name: 'TCP', value: HEALTH_MONITORS.TCP },
+      { name: 'UDP-CONNECT', value: HEALTH_MONITORS.UDP_CONNECT },
+    ],
+  },
+  {
+    name: 'Prometheus',
+    value: PROTOCOLS.PROMETHEUS,
+  },
+];
 
 export const TRACKING_ADD_LISTENER =
   'pci::projects::project::octavia-loadbalancer::add::add-listener';
 
 export default {
+  PROTOCOLS,
   LISTENER_PROTOCOL_LIST,
   TRACKING_ADD_LISTENER,
 };
