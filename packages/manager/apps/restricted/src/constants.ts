eeef30a289ad9aa1af9e066bbb7d0a4fb9710e1e
Bruno MARQUES (ext) <bruno.marques.ext@corp.ovh.com>
2023-10-24 23:49:25 +0200

    feat(restricted): removing unused guides

--- packages/manager/apps/restricted/src/constants.ts
+++ packages/manager/apps/restricted/src/constants.ts
@@ -27,52 +27,6 @@ export const GUIDES: Guide[] = [
         'https://help.ovhcloud.com/csm/pt-customer-iam-control-panel-access?id=kb_article_view&sysparm_article=KB0058719',
     },
   },
-  {
-    id: 'policies-ui',
-    link: {
-      default:
-        'https://help.ovhcloud.com/csm/en-customer-iam-policies-ui?id=kb_article_view&sysparm_article=KB0058729',
-      de_DE:
-        'https://help.ovhcloud.com/csm/de-customer-iam-policies-ui?id=kb_article_view&sysparm_article=KB0058723',
-      en_GB:
-        'https://help.ovhcloud.com/csm/en-gb-customer-iam-policies-ui?id=kb_article_view&sysparm_article=KB0058725',
-      es_ES:
-        'https://help.ovhcloud.com/csm/es-es-customer-iam-policies-ui?id=kb_article_view&sysparm_article=KB0058728',
-      fr_CA:
-        'https://help.ovhcloud.com/csm/fr-ca-customer-iam-policies-ui?id=kb_article_view&sysparm_article=KB0058735',
-      fr_FR:
-        'https://help.ovhcloud.com/csm/fr-customer-iam-policies-ui?id=kb_article_view&sysparm_article=KB0058730',
-      it_IT:
-        'https://help.ovhcloud.com/csm/it-customer-iam-policies-ui?id=kb_article_view&sysparm_article=KB0058734',
-      pl_PL:
-        'https://help.ovhcloud.com/csm/pl-customer-iam-policies-ui?id=kb_article_view&sysparm_article=KB0058733',
-      pt_PT:
-        'https://help.ovhcloud.com/csm/pt-customer-iam-policies-ui?id=kb_article_view&sysparm_article=KB0058731',
-    },
-  },
-  {
-    id: 'policies-api',
-    link: {
-      default:
-        'https://help.ovhcloud.com/csm/en-customer-iam-policies-api?id=kb_article_view&sysparm_article=KB0056805',
-      de_DE:
-        'https://help.ovhcloud.com/csm/de-customer-iam-policies-api?id=kb_article_view&sysparm_article=KB0056810',
-      en_GB:
-        'https://help.ovhcloud.com/csm/en-gb-customer-iam-policies-api?id=kb_article_view&sysparm_article=KB0056801',
-      es_ES:
-        'https://help.ovhcloud.com/csm/es-es-customer-iam-policies-api?id=kb_article_view&sysparm_article=KB0056800',
-      fr_CA:
-        'https://help.ovhcloud.com/csm/fr-ca-customer-iam-policies-api?id=kb_article_view&sysparm_article=KB0056804',
-      fr_FR:
-        'https://help.ovhcloud.com/csm/fr-customer-iam-policies-api?id=kb_article_view&sysparm_article=KB0056808',
-      it_IT:
-        'https://help.ovhcloud.com/csm/it-customer-iam-policies-api?id=kb_article_view&sysparm_article=KB0056796',
-      pl_PL:
-        'https://help.ovhcloud.com/csm/pl-customer-iam-policies-api?id=kb_article_view&sysparm_article=KB0056807',
-      pt_PT:
-        'https://help.ovhcloud.com/csm/pt-customer-iam-policies-api?id=kb_article_view&sysparm_article=KB0056798',
-    },
-  },
 ];
 
 export const SMALL_DEVICE_MAX_SIZE = '30em';
--- packages/manager/apps/restricted/src/restricted/Restricted.tsx
+++ packages/manager/apps/restricted/src/restricted/Restricted.tsx
@@ -13,9 +13,9 @@ const Restricted = (): JSX.Element => (
   <div className="restricted d-flex flex-column h-100">
     <Navbar />
     <Sidebar />
-    <div className="restricted-main h-100 d-flex flex-column mx-auto px-5">
+    <div className="restricted-main d-flex flex-column mx-auto px-5">
       <ErrorJumbotron />
-      <div className="restricted-guides d-flex flex-column flex-md-row">
+      <div className="restricted-guides mb-4 d-flex flex-column flex-md-row">
         {GUIDES.map((guide) => (
           <GuideCard {...guide} key={guide.id} />
         ))}
