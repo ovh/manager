478fea48f3983fc5ad4d13cd7dacd1ca629b1b69
Florian Renaut <florian.renaut@corp.ovh.com>
2023-10-26 16:25:40 +0200

    feat(pci-localzone): add react-hook-form
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/apps/pci-localzone-instance/src/pages/ListingPage.tsx
+++ packages/manager/apps/pci-localzone-instance/src/pages/ListingPage.tsx
@@ -1,46 +1,47 @@
-import Listing from '@/components/Listing';
 import { Outlet, Link } from 'react-router-dom';
+import Listing from '@/components/Listing';
 
 export function Component() {
-
   const headers = [
     {
       title: 'ID',
-      property: 'id'
+      property: 'id',
     },
     {
       title: 'Localisation',
-      property: 'localization'
-    }
+      property: 'localization',
+    },
   ];
 
   const items = [
     {
       id: '1234',
-      localization: 'GRA11'
+      localization: 'GRA11',
     },
     {
       id: '2345',
-      localization: 'DE1'
-    }
-  ]
+      localization: 'DE1',
+    },
+  ];
 
-  return <>Listing page
-
-  <Link to="./new">Create an instance</Link>
-    <Listing headers={headers} items={items} />
-
-    <ul>
-      <li>
-        <Link to="./1234">Instance Dashboard</Link>
-      </li>
-      <li>
-        <Link to="./boot?instanceId=1234">Boot an instance</Link>
-      </li>
-    </ul>
-
-    <Outlet />
-  </>
+  return (
+    <>
+      Listing page
+      <Link to="./new">Create an instance</Link>
+      <Listing headers={headers} items={items} />
+      <ul>
+        <li>
+          <Link to="./1234">Instance Dashboard</Link>
+        </li>
+        <li>
+          <Link to="./boot?instanceId=1234">Boot an instance</Link>
+        </li>
+      </ul>
+      <Outlet />
+    </>
+  );
 }
 
 Component.displayName = 'ListingPage';
+
+export default Component;
