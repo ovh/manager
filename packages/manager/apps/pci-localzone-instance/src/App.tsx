4f872f39dd00154c14f8c4e8e8b5e3eb74ef0c3d
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2023-10-27 17:37:54 +0200

    feat(ods): add ods
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/pci-localzone-instance/src/App.tsx
+++ packages/manager/apps/pci-localzone-instance/src/App.tsx
@@ -1,8 +1,14 @@
 import { RouterProvider, createHashRouter } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { odsSetup } from '@ovhcloud/ods-common-core';
+
 import appRoutes from '@/routes';
 import queryClient from './queryClient';
+
+import '@ovhcloud/ods-theme-blue-jeans';
+
+odsSetup();
 
 const router = createHashRouter(appRoutes);
 
