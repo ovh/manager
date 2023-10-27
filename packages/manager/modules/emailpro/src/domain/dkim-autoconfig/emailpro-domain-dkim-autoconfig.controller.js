c3dfb9db324e9c7fdf4fe9c2a22df8fa511360db
benjaminlepine <benjamin.lepine@hotmail.fr>
2023-10-27 15:06:14 +0200

    feat(web.emailpro): add enable disable dkim
    ref: MANAGER-12344
    ref: MANAGER-12346
    
    Signed-off-by: benjaminlepine <benjamin.lepine@hotmail.fr>

--- packages/manager/modules/emailpro/src/domain/dkim-autoconfig/emailpro-domain-dkim-autoconfig.controller.js
+++ packages/manager/modules/emailpro/src/domain/dkim-autoconfig/emailpro-domain-dkim-autoconfig.controller.js
@@ -1,4 +1,5 @@
 import { DKIM_CONFIGURATION_GUIDE } from './emailpro-domain-dkim-autoconfig.constants';
+import { STATUS_TO_TEXT_MAP } from '../emailpro-domain.constants';
 
 import DkimAutoConfigurator from './dkim-auto-configurator';
 
@@ -27,8 +28,7 @@ export default class EmailProDomainDkimAutoconfigCtrl extends DkimAutoConfigurat
     this.dkimStatus = $scope.currentActionData.dkimStatus;
     this.GLOBAL_DKIM_STATUS =
       $scope.currentActionData.constant.GLOBAL_DKIM_STATUS;
-    this.services.$scope.configDkim = () => this.configDkim();
-
+    this.DKIM_STATUS = $scope.currentActionData.constant.DKIM_STATUS;
     this.dkimGuideLink =
       DKIM_CONFIGURATION_GUIDE[coreConfig.getUser().ovhSubsidiary] ||
       DKIM_CONFIGURATION_GUIDE.DEFAULT;
@@ -71,10 +71,53 @@ export default class EmailProDomainDkimAutoconfigCtrl extends DkimAutoConfigurat
   }
 
   hideConfirmButton() {
-    return (
-      this.dkimStatus === this.GLOBAL_DKIM_STATUS.NOT_CONFIGURED &&
-      this.domainDiag.isOvhDomain
-    );
+    const showWhen = [
+      this.GLOBAL_DKIM_STATUS.NOT_CONFIGURED,
+      this.GLOBAL_DKIM_STATUS.OK,
+      this.GLOBAL_DKIM_STATUS.DISABLED,
+    ];
+
+    return this.domainDiag.isOvhDomain && showWhen.includes(this.dkimStatus);
+  }
+
+  onFinishDkim() {
+    switch (this.dkimStatus) {
+      case this.GLOBAL_DKIM_STATUS.NOT_CONFIGURED:
+        return this.configDkim();
+      case this.GLOBAL_DKIM_STATUS.OK:
+        return this.deactivateDkim();
+      case this.GLOBAL_DKIM_STATUS.DISABLED:
+        return this.activateDkim();
+      default:
+        console.error('Invalid DKIM status:', this.dkimStatus);
+        return null;
+    }
+  }
+
+  activateDkim() {
+    const dkimSelectors = this.domain.dkim;
+
+    this.services.EmailProDomains.enableDkim(
+      this.services.$stateParams.productId,
+      this.domain.name,
+      dkimSelectors[0].selectorName,
+    ).finally(() => {
+      this.services.$scope.resetAction();
+    });
+  }
+
+  deactivateDkim() {
+    const dkimSelectors = this.domain.dkim;
+
+    this.services.EmailProDomains.disableDkim(
+      this.services.$stateParams.productId,
+      this.domain.name,
+      dkimSelectors[0].status === this.DKIM_STATUS.IN_PRODUCTION
+        ? dkimSelectors[0].selectorName
+        : dkimSelectors[1].selectorName,
+    ).finally(() => {
+      this.services.$scope.resetAction();
+    });
   }
 
   getDkimSelectorForCurrentState() {
@@ -143,5 +186,33 @@ export default class EmailProDomainDkimAutoconfigCtrl extends DkimAutoConfigurat
       .finally(() => {
         this.services.$scope.resetAction();
       });
+  }
+
+  getBodyText(status) {
+    let translationKey;
+    if (status === this.GLOBAL_DKIM_STATUS.NOT_CONFIGURED) {
+      translationKey = this.domainDiag.isOvhDomain
+        ? 'emailpro_tab_domain_diagnostic_dkim_activation_ovhcloud'
+        : 'emailpro_tab_domain_diagnostic_dkim_activation_no_ovhcloud';
+    } else {
+      translationKey = STATUS_TO_TEXT_MAP[status];
+    }
+    return translationKey
+      ? this.services.$translate.instant(translationKey)
+      : '';
+  }
+
+  getTitleForWizard() {
+    if (
+      this.dkimStatus === this.GLOBAL_DKIM_STATUS.NOK ||
+      this.dkimStatus === this.GLOBAL_DKIM_STATUS.OK
+    ) {
+      return this.services.$translate.instant(
+        'emailpro_tab_domain_diagnostic_dkim_title_error',
+      );
+    }
+    return this.services.$translate.instant(
+      'emailpro_tab_domain_diagnostic_dkim_title_activation',
+    );
   }
 }
