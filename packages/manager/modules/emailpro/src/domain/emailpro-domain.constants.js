b4f96525bc35e8909a83a7f488797c3f49629f61
benjaminlepine <benjamin.lepine@hotmail.fr>
2023-10-27 15:06:14 +0200

    feat(web.emailpro): add enable disable dkim
    ref: MANAGER-12344
    ref: MANAGER-12346
    
    Signed-off-by: benjaminlepine <benjamin.lepine@hotmail.fr>

--- packages/manager/modules/emailpro/src/domain/emailpro-domain.constants.js
+++ packages/manager/modules/emailpro/src/domain/emailpro-domain.constants.js
@@ -28,8 +28,26 @@ export const DKIM_MATCHING_SCHEMA_STATUS = {
   NOK: [DKIM_STATUS.WAITING_RECORD], // ERROR (Red)
 };
 
+export const STATUS_TO_TEXT_MAP = {
+  [GLOBAL_DKIM_STATUS.OK]: 'emailpro_tab_domain_diagnostic_dkim_deactivation',
+  [GLOBAL_DKIM_STATUS.DISABLED]:
+    'emailpro_tab_domain_diagnostic_dkim_activation',
+  [GLOBAL_DKIM_STATUS.NOK]:
+    'emailpro_tab_domain_diagnostic_dkim_configuration_nok',
+};
+
+export const DKIM_STATUS_TO_CLASS_MAP = {
+  [GLOBAL_DKIM_STATUS.OK]: 'oui-badge_success',
+  [GLOBAL_DKIM_STATUS.DISABLED]: 'oui-badge_warning',
+  [GLOBAL_DKIM_STATUS.NOT_CONFIGURED]: 'oui-background-g-100',
+  [GLOBAL_DKIM_STATUS.IN_PROGRESS]: 'oui-badge_info',
+  [GLOBAL_DKIM_STATUS.NOK]: 'oui-badge_error',
+};
+
 export default {
   GLOBAL_DKIM_STATUS,
   DKIM_STATUS,
   DKIM_MATCHING_SCHEMA_STATUS,
+  STATUS_TO_TEXT_MAP,
+  DKIM_STATUS_TO_CLASS_MAP,
 };
