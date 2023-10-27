750343545e762abe7d39a8c00c6e7e465dd5f3bf
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2023-10-27 16:42:45 +0200

    feat(query): add react query
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/pci-localzone-instance/src/App.tsx
+++ packages/manager/apps/pci-localzone-instance/src/App.tsx
@@ -1,12 +1,18 @@
 import { RouterProvider, createHashRouter } from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import appRoutes from '@/routes';
+import queryClient from './queryClient';
 
 const router = createHashRouter(appRoutes);
 
 function App() {
   return (
     <>
-      <RouterProvider router={router}></RouterProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}></RouterProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   );
 }
--- packages/manager/apps/pci-localzone-instance/src/pages/instance/DashboardPage.tsx
+++ packages/manager/apps/pci-localzone-instance/src/pages/instance/DashboardPage.tsx
@@ -1,47 +1,48 @@
-import { Outlet, Link, useLoaderData, LoaderFunction } from 'react-router-dom';
-
-export type LoaderData = {
-  instance: { id: string };
-};
-
-export const loader: LoaderFunction = async ({ params }) => {
-  const { instanceId } = params;
-
-  return {
-    instance: { id: instanceId },
-  };
-};
+import { Outlet, Link, useParams } from 'react-router-dom';
+import { useInstance } from '@/hooks/useInstance';
 
 export default function DashboardPage() {
-  const { instance } = useLoaderData() as LoaderData;
+  const { projectId, instanceId } = useParams();
+  const { data: instance } = useInstance(projectId || '', instanceId || '');
 
   return (
     <>
-      Dashboard page {instance.id}
-      <ul>
-        <li>
-          <Link to="./backup">Backup</Link>
-        </li>
-        <li>
-          <Link to="./boot">Boot</Link>
-        </li>
-        <li>
-          <Link to="./stop">Stop</Link>
-        </li>
-        <li>
-          <Link to="./soft-reboot">Soft Reboot</Link>
-        </li>
-        <li>
-          <Link to="./hard-reboot">Hard reboot</Link>
-        </li>
-        <li>
-          <Link to="./delete">Delete</Link>
-        </li>
-        <li>
-          <Link to="./attach-volume">Attach Volume</Link>
-        </li>
-      </ul>
-      <Outlet />
+      {instance && (
+        <>
+          Dashboard page {instance.id} {instance.name}
+          <dl>
+            <dt>Status</dt>
+            <dd>{instance.status}</dd>
+
+            <dt>Region</dt>
+            <dd>{instance.region}</dd>
+          </dl>
+          <ul>
+            <li>
+              <Link to="./backup">Backup</Link>
+            </li>
+            <li>
+              <Link to="./boot">Boot</Link>
+            </li>
+            <li>
+              <Link to="./stop">Stop</Link>
+            </li>
+            <li>
+              <Link to="./soft-reboot">Soft Reboot</Link>
+            </li>
+            <li>
+              <Link to="./hard-reboot">Hard reboot</Link>
+            </li>
+            <li>
+              <Link to="./delete">Delete</Link>
+            </li>
+            <li>
+              <Link to="./attach-volume">Attach Volume</Link>
+            </li>
+          </ul>
+          <Outlet />
+        </>
+      )}
     </>
   );
 }
