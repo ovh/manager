0c12da2516a22e53d5d34de15aa91c40eaaedd87
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2023-10-27 17:37:54 +0200

    feat(ods): add ods
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/pci-localzone-instance/src/components/instance/Boot.tsx
+++ packages/manager/apps/pci-localzone-instance/src/components/instance/Boot.tsx
@@ -1,3 +1,7 @@
+import { OsdsButton } from '@ovhcloud/ods-components/button/react';
+import { OsdsModal,  } from '@ovhcloud/ods-components/modal/react';
+import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
+
 interface BootProps {
   instance: { id: string };
   onClose: CallableFunction;
@@ -6,8 +10,19 @@ interface BootProps {
 export default function Boot({ instance, onClose }: BootProps) {
   return (
     <>
-      Boot instance {instance.id}
-      <button onClick={() => onClose()}>Close</button>
+      <OsdsModal>
+        <slot name="content">
+          Boot instance {instance.id}
+          <button onClick={() => onClose()}>Close</button>
+        </slot>
+        <OsdsButton slot="actions" color={ODS_THEME_COLOR_INTENT.default}>
+          Cancel
+        </OsdsButton>
+        <OsdsButton slot="actions" color={ODS_THEME_COLOR_INTENT.primary}>
+          Submit
+        </OsdsButton>
+      </OsdsModal>
+
     </>
   );
 }
