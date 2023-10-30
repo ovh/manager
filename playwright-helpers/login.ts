8f0fdbe077ee045fa56293015c0b9f619961b816
Nicolas Pierre-charles <nicolas@nicode.fr>
2023-10-30 10:32:34 +0100

    feat: setup gherkins with playwright
    ref: MANAGER-12488
    
    Signed-off-by: Nicolas Pierre-charles <nicolas@nicode.fr>

--- playwright-helpers/login.ts
+++ playwright-helpers/login.ts
@@ -1,5 +1,5 @@
-import { Page, test } from '@playwright/test';
-import config from './config';
+import { Page, Response } from '@playwright/test';
+import { config } from './config';
 
 export async function login(page: Page): Promise<void> {
   await page.goto(config.appUrl);
@@ -10,19 +10,6 @@ export async function login(page: Page): Promise<void> {
   await page.locator('#submitButton').click();
 }
 
-export async function logout(page: Page): Promise<void> {
-  await page.goto(config.ovhLogout);
+export async function logout(page: Page): Promise<Response | null> {
+  return page?.goto(config.ovhLogout);
 }
-
-type UseFunction = (page: Page) => Promise<void>;
-
-export async function setupPage({ page }: { page: Page }, use: UseFunction) {
-  await login(page);
-  await page.waitForTimeout(2000); // for the tests when we need to do goto
-
-  await use(page);
-
-  await logout(page);
-}
-
-test.use({ page: setupPage });
