43dc3abaa48d086b7bc398aef98c4607e1320559
Antony MARION <antony.marion.ext@ovhcloud.com>
2023-10-31 09:56:27 +0100

    feat(web): fetch real data instead mocked
      ref: MANAGER-12534
    
    Signed-off-by: Antony MARION <antony.marion.ext@ovhcloud.com>

--- packages/manager/apps/web/client/app/dns-zone/history/history.controller.js
+++ packages/manager/apps/web/client/app/dns-zone/history/history.controller.js
@@ -1,39 +1,80 @@
 import * as Diff2Html from 'diff2html';
 import 'diff2html/bundles/css/diff2html.min.css';
+import * as unidiff from 'unidiff';
 
 export default class DomainDnsZoneHistoryController {
   /* @ngInject */
-  constructor($filter, $timeout, $translate, $window, $stateParams, $state) {
+  constructor(
+    $filter,
+    $timeout,
+    $translate,
+    $window,
+    $stateParams,
+    $state,
+    DNSZoneService,
+    Domain,
+  ) {
     this.$filter = $filter;
     this.$timeout = $timeout;
     this.$translate = $translate;
     this.$stateParams = $stateParams;
     this.$state = $state;
+    this.DNSZoneService = DNSZoneService;
+    this.Domain = Domain;
 
-    this.base_dns_zone_mocks = ['26 juillet 2022', '13 décembre 2022'];
-    this.modified_dns_zone_mocks = ['26 août 2023', '26 décembre 2023'];
+    this.base_dns_zone_mocks = [];
+    this.modified_dns_zone_mocks = [];
+    this.base_dns_chosen = null;
+    this.modified_dns_chosen = null;
+    this.baseDnsZoneData = null;
+    this.modifiedDnsZoneData = null;
+    this.gitDiff = '';
+  }
+
+  getZoneHistory(zoneName) {
+    return this.Domain.getZoneHistory(zoneName);
+  }
+
+  getZoneDataAtDate(zoneName, date) {
+    return this.Domain.getZoneDataAtDate(zoneName, date);
   }
 
   goBack() {
     this.$state.go('app.zone.details.dashboard', this.$stateParams);
   }
 
-  $onInit() {
-    this.strInput =
-      '--- a/dns_properties\n' +
-      '+++ b/dns_properties\n' +
-      '@@ -1,8 +1,8 @@\n' +
-      '           IN NS        dns14.ovh.net.\n' +
-      '           IN NS        ns14.ovh.net.\n' +
-      '           IN MX        50 mx2.mail.ovh.net.\n' +
-      '+           IN TXT      reaeraerjkdjkjdgkjfksgjksfjgfjsgkfjsgfs\n' +
-      ' cam       IN CNAME jeedom.test.ovh.\n' +
-      ' ha        IN CNAME jeedom.test.ovh.\n' +
-      '-jeedom 60 IN A         93.28.77.229\n' +
-      '+jeedom 60 IN A         213.186.77.5\n' +
-      ' nas    60 IN CNAME     jeedom.test.ovh.\n' +
-      '-plex      IN           jeedom.test.ovh';
-    let innerHTML = Diff2Html.html(this.strInput, {
+  async getBaseDnsZoneForChosenDate() {
+    const { zoneFileUrl } = await this.getZoneDataAtDate(
+      this.$stateParams.productId,
+      this.base_dns_chosen,
+    );
+    this.baseDnsZoneData = await this.DNSZoneService.readDnsFileData(
+      zoneFileUrl,
+    );
+    this.gitDiff = unidiff.diffAsText(
+      this.baseDnsZoneData,
+      this.modifiedDnsZoneData,
+    );
+    this.updateInnerHtml();
+  }
+
+  async getModifiedDnsZoneForChosenDate() {
+    const { zoneFileUrl } = await this.getZoneDataAtDate(
+      this.$stateParams.productId,
+      this.modified_dns_chosen,
+    );
+    this.modifiedDnsZoneData = await this.DNSZoneService.readDnsFileData(
+      zoneFileUrl,
+    );
+    this.gitDiff = unidiff.diffAsText(
+      this.baseDnsZoneData,
+      this.modifiedDnsZoneData,
+    );
+    this.updateInnerHtml();
+  }
+
+  updateInnerHtml() {
+    let innerHTML = Diff2Html.html(this.gitDiff, {
       drawFileList: true,
       outputFormat: 'side-by-side',
       matching: 'lines',
@@ -48,9 +89,16 @@ export default class DomainDnsZoneHistoryController {
       'style="visibility: hidden;"',
     );
     innerHTML = innerHTML.replace(
-      /class="d2h-icon d2h-changed"/,
+      /class="d2h-icon .*"/,
       'style="visibility: hidden;"',
     );
     document.getElementById('destination-elem-id').innerHTML = innerHTML;
+  }
+
+  $onInit() {
+    this.getZoneHistory(this.$stateParams.productId).then((dates) => {
+      this.base_dns_zone_mocks = [...dates];
+      this.modified_dns_zone_mocks = [...dates];
+    });
   }
 }
