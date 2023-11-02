9d6645488218beee3060060901f60e4af9b9cfff
Antony MARION <antony.marion.ext@ovhcloud.com>
2023-11-02 12:21:00 +0100

    feat(web): add table for and dns dashboard page
      ref: MANAGER-12556
    
    Signed-off-by: Antony MARION <antony.marion.ext@ovhcloud.com>

---
+++ packages/manager/apps/web/client/app/dns-zone/history/dashboard/dashboard.component.js
@@ -0,0 +1,7 @@
+import controller from './dashboard.controller';
+import template from './dashboard.html';
+
+export default {
+  controller,
+  template,
+};
---
+++ packages/manager/apps/web/client/app/dns-zone/history/dashboard/dashboard.controller.js
@@ -0,0 +1,41 @@
+export default class DomainDnsZoneHistoryDashboardController {
+  constructor($translate, $stateParams, $state, $q, $document, Domain) {
+    this.$translate = $translate;
+    this.$stateParams = $stateParams;
+    this.$state = $state;
+    this.Domain = Domain;
+    this.$document = $document;
+    this.$q = $q;
+    this.listOfDnsZonesUrls = [];
+    this.loading = true;
+  }
+
+  goBack() {
+    this.$state.go('app.zone.details.dashboard', this.$stateParams);
+  }
+
+  getZoneHistory(zoneName) {
+    return this.Domain.getZoneHistory(zoneName);
+  }
+
+  getZoneDataAtDate(zoneName, date) {
+    return this.Domain.getZoneDataAtDate(zoneName, date);
+  }
+
+  $onInit() {
+    // TODO: handle error case with a toast for instance
+    this.getZoneHistory(this.$stateParams.productId).then((dates) => {
+      this.dates = dates;
+      this.$q
+        .all(
+          dates.map((date) =>
+            this.getZoneDataAtDate(this.$stateParams.productId, date),
+          ),
+        )
+        .then((zoneUrls) => {
+          this.listOfDnsZonesUrls = [...zoneUrls];
+          this.loading = false;
+        });
+    });
+  }
+}
