034b9c2608c8d079be90f740a2e8f34c105079b1
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2023-11-03 12:17:53 +0100

    feat(octavia): adding listener edition page
    ref: MANAGER-12228
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/listeners/list/index.js
+++ packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/listeners/list/index.js
@@ -2,7 +2,8 @@ import angular from 'angular';
 import '@uirouter/angularjs';
 import 'oclazyload';
 
-const moduleName = 'ovhManagerOctaviaLoadBalancerDashboardListenersLazyLoading';
+const moduleName =
+  'ovhManagerOctaviaLoadBalancerDashboardListenersListLazyLoading';
 
 angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
   /* @ngInject */ ($stateProvider) => {
