dbe3e2d4ecd963fc6005aa9c32ce10547a9cdc76
Arthur Bullet <arthur.bullet@ovhcloud.com>
2023-11-03 11:22:34 +0100

    feat: fixing pr comments
    datatr-383
    
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/ai/apps/dashboard/dashboard.controller.js
+++ packages/manager/modules/pci/src/projects/project/ai/apps/dashboard/dashboard.controller.js
@@ -1,6 +1,6 @@
 import { APP_USERS_TOKENS_BANNER_TRACKING } from '../app.constants';
 
-export default class {
+export default class AiAppsDashboardCtrl {
   goToUserToken() {
     this.trackApps(APP_USERS_TOKENS_BANNER_TRACKING);
     this.goToUsersAndTokens();
