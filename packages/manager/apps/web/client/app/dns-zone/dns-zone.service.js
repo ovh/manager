b78f124d906163894ca4337294b7a340ac91b4c2
Antony MARION <antony.marion.ext@ovhcloud.com>
2023-11-03 14:46:39 +0100

    feat(web): bind actions with btns and prepare diff
      ref: MANAGER-12556
    
    Signed-off-by: Antony MARION <antony.marion.ext@ovhcloud.com>

--- packages/manager/apps/web/client/app/dns-zone/dns-zone.service.js
+++ packages/manager/apps/web/client/app/dns-zone/dns-zone.service.js
@@ -13,4 +13,10 @@ export default class DNSZoneService {
   readDnsFileData(url) {
     return this.$http.get(url).then(({ data }) => data);
   }
+
+  restore(zoneName, creationDate) {
+    return this.$http
+      .post(`/domain/zone/${zoneName}/history/${creationDate}/restore`)
+      .then(({ data }) => data);
+  }
 }
