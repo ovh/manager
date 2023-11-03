7c359a0885b3f51dfd72eb9b0d139677124b254c
benjaminlepine <benjamin.lepine@hotmail.fr>
2023-11-03 11:37:56 +0100

    feat(web.exchange): update links
    ref: MANAGER-12347
    
    Signed-off-by: benjaminlepine <benjamin.lepine@hotmail.fr>

--- packages/manager/modules/exchange/src/domain/dkim-autoconfig/domain-dkim-autoconfig.constants.js
+++ packages/manager/modules/exchange/src/domain/dkim-autoconfig/domain-dkim-autoconfig.constants.js
@@ -4,13 +4,13 @@ export const DKIM_CONFIGURATION_GUIDE = {
   ES:
     'https://help.ovhcloud.com/csm/es-es-dns-zone-dkim?id=kb_article_view&sysparm_article=KB0058253',
   FR:
-    'https://help.build-ovhcloud.tools/csm/fr-dns-zone-dkim?id=kb_article_view&sysparm_article=KB0058101#knownErrors',
+    'https://help.ovhcloud.com/csm/fr-dns-zone-dkim?id=kb_article_view&sysparm_article=KB0058101',
   IE:
     'https://help.ovhcloud.com/csm/en-ie-dns-zone-dkim?id=kb_article_view&sysparm_article=KB0058259',
   IT:
     'https://help.ovhcloud.com/csm/it-dns-zone-dkim?id=kb_article_view&sysparm_article=KB0058256',
   NL:
-    'https://help.ovhcloud.com/csm/en-ie-dns-zone-dkim?id=kb_article_view&sysparm_article=KB0058259',
+    'https://help.ovhcloud.com/csm/en-gb-dns-zone-dkim?id=kb_article_view&sysparm_article=KB0058238',
   PL:
     'https://help.ovhcloud.com/csm/pl-dns-zone-dkim?id=kb_article_view&sysparm_article=KB0058254',
   PT:
