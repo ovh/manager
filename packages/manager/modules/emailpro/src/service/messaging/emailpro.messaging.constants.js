4d13e4cfb88364015a327ef99c15f2fd0d894309
Antony MARION <antony.marion.ext@ovhcloud.com>
2023-11-07 12:57:47 +0100

    feat(web): add constants and refacto exchange
      ref: MANAGER-12343
    
    Signed-off-by: Antony MARION <antony.marion.ext@ovhcloud.com>

---
+++ packages/manager/modules/emailpro/src/service/messaging/emailpro.messaging.constants.js
@@ -0,0 +1,10 @@
+export const CSS_BOOTSTRAP_CLASSES = {
+  INFO: 'alert-info',
+  SUCCESS: 'alert-success',
+  DANGER: 'alert-danger',
+  WARNING: 'alert-warning',
+};
+
+export default {
+  CSS_BOOTSTRAP_CLASSES,
+};
