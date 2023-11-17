852f49df1564945b4ad059314036d4a621ce04d0
JacquesLarique <134954692+JacquesLarique@users.noreply.github.com>
2023-11-17 10:07:12 +0100

    feat(octavia): adding pool creation page (#10318)
    ref: MANAGER-12255
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/pools/routing.js
+++ packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/pools/routing.js
@@ -9,6 +9,40 @@ export default /* @ngInject */ ($stateProvider) => {
     },
     resolve: {
       breadcrumb: () => 'pools',
+      apiSpecifications: /* @ngInject */ (OctaviaLoadBalancerPoolsService) =>
+        OctaviaLoadBalancerPoolsService.getAPISpecifications(),
+      algorithms: /* @ngInject */ (apiSpecifications, $translate) =>
+        (
+          apiSpecifications.models['cloud.loadbalancing.PoolAlgorithmEnum']
+            ?.enum || []
+        ).map((value) => ({
+          label: $translate.instant(
+            `octavia_load_balancer_pools_enum_algorithm_${value}`,
+          ),
+          value,
+        })),
+      poolProtocols: /* @ngInject */ (apiSpecifications) =>
+        apiSpecifications.models['cloud.loadbalancing.PoolProtocolEnum']
+          ?.enum || [],
+      sessionPersistenceTypes: /* @ngInject */ (
+        apiSpecifications,
+        $translate,
+      ) =>
+        (
+          apiSpecifications.models[
+            'cloud.loadbalancing.PoolSessionPersistenceTypeEnum'
+          ]?.enum || []
+        ).reduce((acc, value) => {
+          if (value !== 'disabled') {
+            acc.push({
+              label: $translate.instant(
+                `octavia_load_balancer_pools_enum_persistent_session_${value}`,
+              ),
+              value,
+            });
+          }
+          return acc;
+        }, []),
       trackBase: () =>
         `${TRACKING_CHAPTER_1}::${TRACKING_NAME}::${TRACKING_SUFFIX}`,
       trackAction: /* @ngInject */ (atInternet, trackBase) => (hit) =>
