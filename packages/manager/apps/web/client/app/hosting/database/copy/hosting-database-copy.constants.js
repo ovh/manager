903b6de268796074439455da5ffee9b4fe55c260
Bruno MARQUES <139278215+BrunoMarquesOVH@users.noreply.github.com>
2023-11-17 11:33:27 +0100

    feat(web): adding copy database capability on web databases (#10103)
    ref: MANAGER-12237
    
    Signed-off-by: Bruno MARQUES <bruno.marques.ext@corp.ovh.com>

---
+++ packages/manager/apps/web/client/app/hosting/database/copy/hosting-database-copy.constants.js
@@ -0,0 +1,7 @@
+export const COPY_TYPE_MAIN_DATABASE = 'main';
+export const COPY_TYPE_WEBCLOUD_DATABASE = 'webCloud';
+
+export default {
+  COPY_TYPE_MAIN_DATABASE,
+  COPY_TYPE_WEBCLOUD_DATABASE,
+};
