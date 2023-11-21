18ac7cc6b19bfc88083784dbcb3da2b0282adea3
Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
2023-11-21 10:01:23 +0100

    feat(project.components): create a sticky project activation banner
    ref: TAPC-412
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>

---
+++ packages/manager/modules/pci/src/projects/project/components/activate-project-banner/activate-project-banner.controller.js
@@ -0,0 +1,3 @@
+export default class PciProjectActivateBannerController {
+  // Will be used for tracking functions
+}
---
+++ packages/manager/modules/pci/src/projects/project/components/index.js
@@ -0,0 +1,9 @@
+import angular from 'angular';
+
+import activateProjectBanner from './activate-project-banner';
+
+const moduleName = 'pciProjectComponents';
+
+angular.module(moduleName, [activateProjectBanner]);
+
+export default moduleName;
