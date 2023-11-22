51ba192a721ab88d6b790a44d08232ec35780588
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2023-11-22 13:52:51 +0100

    refactor(ng-ovh-payment-method): separate explanation text templates
    ref: TAPC-376
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

---
+++ packages/components/ng-ovh-payment-method/src/components/register/templates/index.js
@@ -0,0 +1,21 @@
+import creditCardChargesExplanationText from './creditCardChargesExplanationText.html';
+import creditCardExplanationText from './creditCardExplanationText.html';
+import creditCardExplanationTextContainer from './creditCardExplanationTextContainer.html';
+import paypalExplanationText from './paypalExplanationText.html';
+import paypalExplanationTextContainer from './paypalExplanationTextContainer.html';
+import rupayExplanationTextContainer from './rupayExplanationTextContainer.html';
+import sepaExplanationTextBanner from './sepaExplanationTextBanner.html';
+import sepaExplanationTextContainer from './sepaExplanationTextContainer.html';
+import sepaExplanationTextIntro from './sepaExplanationTextIntro.html';
+
+export default {
+  creditCardChargesExplanationText,
+  creditCardExplanationText,
+  creditCardExplanationTextContainer,
+  paypalExplanationText,
+  paypalExplanationTextContainer,
+  rupayExplanationTextContainer,
+  sepaExplanationTextBanner,
+  sepaExplanationTextContainer,
+  sepaExplanationTextIntro,
+};
