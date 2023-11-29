9aebef1aea013629e64651ae811c3b5884e4254f
abullet33 <106544940+abullet33@users.noreply.github.com>
2023-11-29 18:10:42 +0100

    feat: add docker command into ai deploy order funnel (#10222)
    ref: datatr-374
    
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>
    Signed-off-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/pci/src/projects/project/ai/apps/add/components/app-image/app-image.controller.js
+++ packages/manager/modules/pci/src/projects/project/ai/apps/add/components/app-image/app-image.controller.js
@@ -1,4 +1,8 @@
-import { APP_IMAGE, APP_IMAGE_DOCKER_ARCHITECTURE } from '../../add.constants';
+import {
+  APP_IMAGE,
+  APP_IMAGE_DOCKER_ARCHITECTURE,
+  splitIntoArray,
+} from '../../add.constants';
 import {
   APP_CUSTOM_DOCKER_IMAGE_DOC,
   APP_DOCKER_IMAGE_PORTFOLIO,
@@ -12,15 +16,19 @@ export default class AppImageController {
   }
 
   $onInit() {
+    this.commandInArguments = [];
     this.APP_IMAGE = APP_IMAGE;
     this.APP_IMAGE_DOCKER_ARCHITECTURE = APP_IMAGE_DOCKER_ARCHITECTURE;
-
     this.docImagesDockerPortfolioUrl =
       APP_DOCKER_IMAGE_PORTFOLIO[this.user.ovhSubsidiary] ||
       APP_DOCKER_IMAGE_PORTFOLIO.DEFAULT;
     this.docDockerBuildUrl =
       APP_CUSTOM_DOCKER_IMAGE_DOC[this.user.ovhSubsidiary] ||
       APP_CUSTOM_DOCKER_IMAGE_DOC.DEFAULT;
+    if (this.command) {
+      this.customDockerCommand = true;
+      this.splitStringCommandIntoArray();
+    }
   }
 
   onPresetSelect(preset) {
@@ -31,5 +39,16 @@ export default class AppImageController {
   getToSLink() {
     const { termsOfService } = this.preset.partner.contract;
     return (termsOfService[this.language] || termsOfService.default).url;
+  }
+
+  splitStringCommandIntoArray() {
+    this.commandInArguments = splitIntoArray(this.command);
+  }
+
+  onDockerCommandChanged() {
+    if (this.customDockerCommand) {
+      this.command = null;
+      this.commandInArguments = [];
+    }
   }
 }
