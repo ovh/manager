151ea066100b19be328a58b5ebc3a0388428b334
qpavy <contact@quentinpavy.com>
2023-11-29 13:04:40 +0100

    feat(dedicated.netapp): vrack services network configuration page for efs (#10396)
    ref: MANAGER-11338
    
    Signed-off-by: Quentin Pavy <contact@quentinpavy.com>

---
+++ packages/manager/modules/netapp/src/dashboard/network-configuration/routing.js
@@ -0,0 +1,51 @@
+import { TRACKING_BASE } from './constants';
+
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state('netapp.dashboard.network', {
+    url: '/network',
+    views: {
+      'netappContainer@netapp': 'ovhManagerNetAppNetworkConfiguration',
+    },
+    resolve: {
+      breadcrumb: /* @ngInject */ ($translate) =>
+        $translate.instant('netapp_network_configuration_title'),
+      trackClick: /* @ngInject */ (atInternet) => (tracker) =>
+        atInternet.trackClick({
+          type: 'action',
+          name: `${TRACKING_BASE}::${tracker}`,
+        }),
+      trackSuccess: /* @ngInject */ (atInternet) => () =>
+        atInternet.trackPage({
+          name: `${TRACKING_BASE}-success`,
+        }),
+      trackError: /* @ngInject */ (atInternet) => () =>
+        atInternet.trackPage({
+          name: `${TRACKING_BASE}-error`,
+        }),
+      goBack: /* @ngInject */ ($state, trackClick) => (trackingAction) => {
+        trackClick(trackingAction);
+        return $state.go('^');
+      },
+      vrackServices: /* @ngInject */ (NetappNetworkConfigurationService) =>
+        NetappNetworkConfigurationService.getVrackServices().then(({ data }) =>
+          data.map((vs) => {
+            const name = vs.currentState.displayName || vs.id;
+            return {
+              ...vs,
+              display: {
+                name,
+                nameWithVrackId: vs.currentState.vrackId
+                  ? `${name} (${vs.currentState.vrackId})`
+                  : name,
+              },
+            };
+          }),
+        ),
+      createVrackServiceLink: /* @ngInject */ () => 'TODO',
+      createSubnetLink: /* @ngInject */ () => 'TODO',
+    },
+    atInternet: {
+      rename: 'netapp::dashboard::configure-network',
+    },
+  });
+};
