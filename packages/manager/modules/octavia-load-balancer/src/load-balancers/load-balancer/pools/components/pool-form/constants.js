acc9457e3c6a2571283e693f49a42041214b3555
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2023-12-01 17:23:20 +0100

    feat(octavia): add pool edition view
    ref: MANAGER-12302
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/pools/components/pool-form/constants.js
+++ packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/pools/components/pool-form/constants.js
@@ -2,9 +2,13 @@ export const DEFAULT_ALGORITHM = 'roundRobin';
 
 export const DEFAULT_SESSION_PERSISTENCE_TYPE = 'sourceIP';
 
-const persistenceTypes = ['sourceIP', 'httpCookie', 'appCookie'];
+export const APP_COOKIE_SESSION_PERSISTENCE = 'appCookie';
 
-export const APP_COOKIE_SESSION_PERSISTENCE = persistenceTypes[2];
+const persistenceTypes = [
+  DEFAULT_SESSION_PERSISTENCE_TYPE,
+  'httpCookie',
+  APP_COOKIE_SESSION_PERSISTENCE,
+];
 
 export const PROTOCOL_SESSION_PERSISTENCE_TYPE_COMBINATION = {
   http: persistenceTypes,
@@ -12,8 +16,8 @@ export const PROTOCOL_SESSION_PERSISTENCE_TYPE_COMBINATION = {
   tcp: persistenceTypes,
   proxy: persistenceTypes,
   proxyV2: persistenceTypes,
-  udp: [persistenceTypes[0]],
-  sctp: [persistenceTypes[0]],
+  udp: [DEFAULT_SESSION_PERSISTENCE_TYPE],
+  sctp: [DEFAULT_SESSION_PERSISTENCE_TYPE],
 };
 
 export default {
