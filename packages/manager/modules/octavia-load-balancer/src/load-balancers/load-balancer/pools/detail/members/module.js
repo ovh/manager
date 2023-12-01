0d23e8d6676c706246b23fa6b074b6141e6c65bf
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2023-12-01 11:13:04 +0100

    feat(octavia): add link to pool from listeners list
    ref: MANAGER-12602
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/pools/detail/members/module.js
+++ packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/pools/detail/members/module.js
@@ -5,12 +5,13 @@ import routing from './routing';
 import service from './service';
 
 import list from './list';
+import create from './create';
 import deletion from './delete';
 
 const moduleName = 'ovhManagerOctaviaLoadBalancerPoolsDetailMembers';
 
 angular
-  .module(moduleName, [list, deletion])
+  .module(moduleName, [list, create, deletion])
   .config(routing)
   .component('octaviaLoadBalancerPoolsDetailMembers', component)
   .service('OctaviaLoadBalancerMembersService', service)
