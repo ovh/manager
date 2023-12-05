cf0548b11ad36d06350e7fafd038c990ba2c5508
karim-ovh <146735326+karim-ovh@users.noreply.github.com>
2023-12-05 09:06:32 +0100

    feat(octavia.pools.members): edit member name (#10453)
    ref: MANAGER-12367
    
    Signed-off-by: kelbarak <karim.el-baraka.ext@corp.ovh.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/pools/detail/members/module.js
+++ packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/pools/detail/members/module.js
@@ -8,11 +8,12 @@ import list from './list';
 import create from './create';
 import addInstances from './add-ip-instance';
 import deletion from './delete';
+import editName from './edit-name';
 
 const moduleName = 'ovhManagerOctaviaLoadBalancerPoolsDetailMembers';
 
 angular
-  .module(moduleName, [list, create, addInstances, deletion])
+  .module(moduleName, [list, create, addInstances, deletion, editName])
   .config(routing)
   .component('octaviaLoadBalancerPoolsDetailMembers', component)
   .service('OctaviaLoadBalancerMembersService', service)
