bc37deeb8162ececefdd047365d3da46d260ec2e
Antony MARION <antony.marion.ext@ovhcloud.com>
2023-12-05 10:32:34 +0100

    feat(web): add banneer and change placeholder
      ref: MANAGER-12708
    
    Signed-off-by: Antony MARION <antony.marion.ext@ovhcloud.com>

--- packages/manager/apps/web/client/app/hosting/database/copy/hosting-database-copy.constants.js
+++ packages/manager/apps/web/client/app/hosting/database/copy/hosting-database-copy.constants.js
@@ -1,7 +1,52 @@
 export const COPY_TYPE_MAIN_DATABASE = 'main';
 export const COPY_TYPE_WEBCLOUD_DATABASE = 'webCloud';
 
+export const BANNER_HOSTING_GUIDE_HELP_LINKS = {
+  DE:
+    'https://help.ovhcloud.com/csm/de-web-hosting-copy-transfer-database?id=kb_article_view&sysparm_article=KB0060504',
+  ES:
+    'https://help.ovhcloud.com/csm/es-es-web-hosting-copy-transfer-database?id=kb_article_view&sysparm_article=KB0060504',
+  FR:
+    'https://help.ovhcloud.com/csm/fr-web-hosting-copy-transfer-database?id=kb_article_view&sysparm_article=KB0060504',
+  IE:
+    'https://help.ovhcloud.com/csm/en-ie-web-hosting-copy-transfer-database?id=kb_article_view&sysparm_article=KB0060504',
+  IT:
+    'https://help.ovhcloud.com/csm/it-web-hosting-copy-transfer-database?id=kb_article_view&sysparm_article=KB0060504',
+  NL:
+    'https://help.ovhcloud.com/csm/en-ie-web-hosting-copy-transfer-database?id=kb_article_view&sysparm_article=KB0060504',
+  PL:
+    'https://help.ovhcloud.com/csm/pl-web-hosting-copy-transfer-database?id=kb_article_view&sysparm_article=KB0060504',
+  PT:
+    'https://help.ovhcloud.com/csm/pt-web-hosting-copy-transfer-database?id=kb_article_view&sysparm_article=KB0060504',
+  GB:
+    'https://help.ovhcloud.com/csm/en-gb-web-hosting-copy-transfer-database?id=kb_article_view&sysparm_article=KB0060504',
+  CA:
+    'https://help.ovhcloud.com/csm/en-ca-web-hosting-copy-transfer-database?id=kb_article_view&sysparm_article=KB0060504',
+  QC:
+    'https://help.ovhcloud.com/csm/fr-ca-web-hosting-copy-transfer-database?id=kb_article_view&sysparm_article=KB0060504',
+  MA:
+    'https://help.ovhcloud.com/csm/fr-web-hosting-copy-transfer-database?id=kb_article_view&sysparm_article=KB0060504',
+  SN:
+    'https://help.ovhcloud.com/csm/fr-web-hosting-copy-transfer-database?id=kb_article_view&sysparm_article=KB0060504',
+  TN:
+    'https://help.ovhcloud.com/csm/fr-web-hosting-copy-transfer-database?id=kb_article_view&sysparm_article=KB0060504',
+  AU:
+    'https://help.ovhcloud.com/csm/en-au-web-hosting-copy-transfer-database?id=kb_article_view&sysparm_article=KB0060504',
+  IN:
+    'https://help.ovhcloud.com/csm/asia-web-hosting-copy-transfer-database?id=kb_article_view&sysparm_article=KB0060504',
+  SG:
+    'https://help.ovhcloud.com/csm/en-sg-web-hosting-copy-transfer-database?id=kb_article_view&sysparm_article=KB0060504',
+  ASIA:
+    'https://help.ovhcloud.com/csm/asia-web-hosting-copy-transfer-database?id=kb_article_view&sysparm_article=KB0060504',
+  WE:
+    'https://help.ovhcloud.com/csm/en-web-hosting-copy-transfer-database?id=kb_article_view&sysparm_article=KB0060504',
+  WS:
+    'https://help.ovhcloud.com/csm/es-web-hosting-copy-transfer-database?id=kb_article_view&sysparm_article=KB0060504',
+  DEFAULT:
+    'https://help.ovhcloud.com/csm/en-web-hosting-copy-transfer-database?id=kb_article_view&sysparm_article=KB0060504',
+};
 export default {
   COPY_TYPE_MAIN_DATABASE,
   COPY_TYPE_WEBCLOUD_DATABASE,
+  BANNER_HOSTING_GUIDE_HELP_LINKS,
 };
