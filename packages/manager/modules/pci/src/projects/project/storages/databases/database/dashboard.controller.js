dd3a26e611e3202edfec815dc7ab5155d01ab462
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2023-12-05 11:14:49 +0100

    fix: mongodb fixes
    DATATR-413
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

---
+++ packages/manager/modules/pci/src/projects/project/storages/databases/database/dashboard.controller.js
@@ -0,0 +1,8 @@
+import { PLANS_WITHOUT_BACKUP } from '../../../../../components/project/storages/databases/databases.constants';
+
+export default class DatabasesDashboardCtrl {
+  /* @ngInject */
+  constructor() {
+    this.PLANS_WITHOUT_BACKUP = PLANS_WITHOUT_BACKUP;
+  }
+}
