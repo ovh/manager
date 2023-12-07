02ed84567fb91f2bab57f548f6ba3f7e082676e7
Arthur Bullet <arthur.bullet@ovhcloud.com>
2023-12-07 11:52:45 +0100

    feat(pci.ai): fixing sonnar issue
    datatr-416
    
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/training/jobs/dashboard/dashboard.component.js
+++ packages/manager/modules/pci/src/projects/project/training/jobs/dashboard/dashboard.component.js
@@ -1,20 +1,16 @@
 import template from './dashboard.html';
-import controller from './dashboard.controller';
 
 export default {
-  controller,
   bindings: {
     job: '<',
     guideUrl: '<',
     guideTrackingSectionTags: '<',
     trackClick: '<',
-    // reloadState: '<',
     currentActiveLink: '<',
     generalInformationLink: '<',
     attachDataLink: '<',
     logsLink: '<',
     goToUsersAndTokens: '<',
-    // trackApps: '<',
   },
   template,
 };
