566135e29392c3e97e90c42e33ba4ef3e51f0499
qpavy <contact@quentinpavy.com>
2023-12-07 14:20:05 +0100

    feat(dedicated.netapp): add link to vrack in network tile (#10492)
    ref: MANAGER-12423
    
    Signed-off-by: Quentin Pavy <contact@quentinpavy.com>

--- packages/manager/modules/netapp/src/dashboard/index/component.js
+++ packages/manager/modules/netapp/src/dashboard/index/component.js
@@ -8,6 +8,7 @@ export default {
     goToNetworkConfiguration: '<',
     goToDeleteNetworkConfiguration: '<',
     networkInformations: '<',
+    getVrackLink: '<',
     pollDissociatingVrackServices: '<',
     goToCreateVolume: '<',
     isCommitmentAvailable: '<',
