4004baa69faa41bb1513fb262373c940e447d455
Arthur Bullet <arthur.bullet@ovhcloud.com>
2023-12-11 16:04:39 +0100

    feat(pci.ai): fix pr comments
    datatr-162
    
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>

--- packages/manager/modules/pci/src/components/pci-ai-data-sync/index.js
+++ packages/manager/modules/pci/src/components/pci-ai-data-sync/index.js
@@ -5,9 +5,6 @@ import 'angular-translate';
 import '@ovh-ux/ui-kit';
 
 import component from './pci-ai-data-sync.component';
-import appService from '../../projects/project/ai/ai.service';
-import notebookService from '../../projects/project/notebooks/notebooks.service';
-import jobService from '../../projects/project/training/job.service';
 
 const moduleName = 'ovhManagerPciAiDataSyncComponent';
 
@@ -18,9 +15,6 @@ angular
     'pascalprecht.translate',
   ])
   .component('ovhManagerPciProjectAiDataSync', component)
-  .service('PciProjectAppService', appService)
-  .service('PciProjectNotebookService', notebookService)
-  .service('PciProjectJobService', jobService)
   .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
