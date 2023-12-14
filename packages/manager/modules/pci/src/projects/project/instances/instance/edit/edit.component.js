49db94a1cb235495545cb8fa89db03f22d65f56e
Sachin Ramesh <104337442+sachinrameshn@users.noreply.github.com>
2023-12-14 14:06:01 +0530

    feat(public-cloud): price info for local zone instance (#10540)
    ref: MANAGER-12756
    
    Signed-off-by: sachinrameshn <sachin.ramesh@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/instances/instance/edit/edit.component.js
+++ packages/manager/modules/pci/src/projects/project/instances/instance/edit/edit.component.js
@@ -19,5 +19,6 @@ export default {
     catalog: '<',
     customerRegions: '<',
     imageInformation: '<',
+    hourlyPriceInformation: '<',
   },
 };
--- packages/manager/modules/pci/src/projects/project/instances/instance/instance.component.js
+++ packages/manager/modules/pci/src/projects/project/instances/instance/instance.component.js
@@ -47,5 +47,6 @@ export default {
     guideTrackingSectionTags: '<',
     trackClick: '<',
     vncDisabled: '<',
+    hourlyPriceInformation: '<',
   },
 };
