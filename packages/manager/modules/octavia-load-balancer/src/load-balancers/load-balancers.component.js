534e8acbb1d6a23f967c72c4b802283a1d928c31
JacquesLarique <134954692+JacquesLarique@users.noreply.github.com>
2023-12-15 15:10:35 +0100

    feat(octavia): added provisionning status on load balancers list (#10551)
    ref: MANAGER-12802
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancers.component.js
+++ packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancers.component.js
@@ -8,6 +8,7 @@ export default {
     goToDeleteLoadBalancer: '<',
     dashboardPageLink: '<',
     dashboardPageTracking: '<',
+    provisioningStatusBadges: '<',
     operatingStatusBadges: '<',
   },
   template,
