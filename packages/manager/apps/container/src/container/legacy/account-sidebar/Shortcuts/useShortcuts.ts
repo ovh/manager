37df53c8c938e034a5b248fda02361f9057a5d0a
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2023-12-19 16:06:25 +0100

    fix(container): catalog app link to /catalog
    ref: MANAGER-12908
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager/apps/container/src/container/legacy/account-sidebar/Shortcuts/useShortcuts.ts
+++ packages/manager/apps/container/src/container/legacy/account-sidebar/Shortcuts/useShortcuts.ts
@@ -56,7 +56,7 @@ const useShortcuts = (): UseShortcuts => {
               id: 'products',
               icon: 'oui-icon-book-open_concept',
               tracking: 'hub::sidebar::shortcuts::go-to-catalog',
-              url: navigation.getURL('hub', '#/catalog'),
+              url: navigation.getURL('catalog', '#/'),
             },
           ]),
       ...(['EU', 'CA'].includes(region)
