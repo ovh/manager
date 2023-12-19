e20ca7d38b07c521bc3d80c1fbb8a858d7a6e5aa
JacquesLarique <134954692+JacquesLarique@users.noreply.github.com>
2023-12-19 09:20:21 +0100

    feat(octavia): add link to pool members from pools list (#10562)
    ref: MANAGER-12904
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/pools/list/component.js
+++ packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/pools/list/component.js
@@ -7,6 +7,7 @@ export default {
     getPoolDetailLink: '<',
     goToPoolEdition: '<',
     goToPoolDeletion: '<',
+    goToPoolMembers: '<',
     operatingStatusBadges: '<',
     provisioningStatusBadges: '<',
   },
