954a36101ba24153d79b445f06fe6b491b147a5b
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2023-12-20 16:07:23 +0100

    fix(generator): delete stencil lib
    ref: MANAGER-12621
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-components/src/components/card/card.tsx
+++ packages/manager-components/src/components/card/card.tsx
@@ -34,6 +34,8 @@ export interface CardProps {
     category: string;
   };
   badges?: Badge[];
+  dataTracking?: string;
+  hoverable?: boolean;
 }
 
 export const Card = ({
@@ -42,6 +44,8 @@ export const Card = ({
   img,
   badges,
   texts,
+  dataTracking,
+  hoverable,
 }: CardProps) => {
   const { title, description, category } = texts;
   const { t } = useDynamicTranslation('card');
@@ -58,6 +62,8 @@ export const Card = ({
         rounded
         inline
         variant="stroked"
+        dataTracking={dataTracking}
+        hoverable={hoverable}
       >
         <div className="flex flex-col">
           {img?.src && (
