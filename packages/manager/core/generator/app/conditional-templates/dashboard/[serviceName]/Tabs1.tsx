2f3632bfe751852ae4f2ac7e276d311386f3ae6a
aboungnaseng-ovhcloud <123390642+aboungnaseng-ovhcloud@users.noreply.github.com>
2023-12-22 16:19:05 +0100

    fix(core): fix catalog app and react lib
    ref: MANAGER-12621
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager/core/generator/app/conditional-templates/dashboard/[serviceName]/Tabs1.tsx
+++ packages/manager/core/generator/app/conditional-templates/dashboard/[serviceName]/Tabs1.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { OsdsTile } from '@ovhcloud/ods-components/Tile/react';
-import { useEnvironment } from '@ovh-ux/manager-react-core-application';
 
 function Tabs1() {
-  const environment = useEnvironment();
-
   return (
     <div className="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 py-6">
       <div className="p-3">
