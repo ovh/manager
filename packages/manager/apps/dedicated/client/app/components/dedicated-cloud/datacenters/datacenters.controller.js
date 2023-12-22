70a3e10bd2b13be705e69cebe9cbda4abaeeca26
Amandine Dectot <amandine.dectot.ext@corp.ovh.com>
2023-12-22 16:25:05 +0100

    feat(pcc.vmware): a banner to inform vmware customer about nsxv eol
    ref: MANAGER-12829
    
    Signed-off-by: Amandine Dectot <amandine.dectot.ext@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/components/dedicated-cloud/datacenters/datacenters.controller.js
+++ packages/manager/apps/dedicated/client/app/components/dedicated-cloud/datacenters/datacenters.controller.js
@@ -19,15 +19,6 @@ export default class {
   canOrderDatacenter() {
     return this.dedicatedCloud.canOrderDatacenter && this.addVdcAvailable;
   }
-
-  displayMigrationBanner() {
-    return (
-      (this.migrationBannerAvailable.firstBanner ||
-        this.migrationBannerAvailable.secondBanner) &&
-      this.hasLegacyDatacenter
-    );
-  }
-
   getRight(order) {
     return this.dedicatedCloud
       ? !includes(this.dedicatedCloud.orderRight, order)
