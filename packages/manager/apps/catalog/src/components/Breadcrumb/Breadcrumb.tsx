a78a8ade51e00e493c5eab5426fd23ee27b6e8db
stif59100 <steeve.vanderstocken@ovhcloud.com>
2023-12-26 10:57:53 +0100

    feat(core): add breadcrumb hook
    ref:MANAGER-12702
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

--- packages/manager/apps/catalog/src/components/Breadcrumb/Breadcrumb.tsx
+++ packages/manager/apps/catalog/src/components/Breadcrumb/Breadcrumb.tsx
@@ -1,74 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import { Params, useMatches } from 'react-router-dom';
+import React from 'react';
 import { OsdsBreadcrumb } from '@ovhcloud/ods-components/breadcrumb/react';
-import { useShell } from '@ovh-ux/manager-react-core-application';
-
-export type BreadcrumbHandleParams = {
-  data: unknown;
-  params: Params<string>;
-};
-
-type BreadcrumbItem = {
-  label: string;
-  href: string;
-};
-
-type MatchHandle = {
-  breadcrumb?: ({ data, params }: BreadcrumbHandleParams) => Promise<string>;
-};
-
-type Match = {
-  handle: MatchHandle;
-  data: unknown;
-  params: Params<string>;
-};
+import { useBreadcrumb } from '@ovh-ux/manager-react-core-application';
 
 function Breadcrumb(): JSX.Element {
-  const matches = useMatches();
-  const shell = useShell();
-  const [root, setRoot] = useState([]);
-  const [matchCrumbs, setMatchCrumbs] = useState<BreadcrumbItem[]>([]);
+  const breadcrumbItems = useBreadcrumb({
+    rootLabel: 'Dashboard',
+    appName: 'hub',
+  });
 
-  useEffect(() => {
-    shell.navigation.getURL('hub', '#/', {}).then((response: string) =>
-      setRoot([
-        {
-          label: 'Dashboard',
-          href: response,
-        },
-      ]),
-    );
-  }, []);
-
-  useEffect(() => {
-    const items = matches.map(async (match) => {
-      const { handle, data, params }: Match = match;
-      const breadcrumb = await Promise.resolve(handle?.breadcrumb);
-      const crumb = {
-        label: await breadcrumb?.({ data, params }),
-        href: match.pathname,
-      };
-      return crumb;
-    });
-
-    Promise.all(items)
-      .then((breadcrumbs) => {
-        const validCrumbs = breadcrumbs.filter(
-          ({ label, href }) => !!label && !!href,
-        );
-        setMatchCrumbs(validCrumbs);
-      })
-      .catch((error) => {
-        console.error('Error fetching breadcrumbs:', error);
-      });
-  }, [matches]);
-
-  const crumbs = [...root, ...matchCrumbs];
-  const data = crumbs.map((crumb, index) => ({
-    label: crumb.label,
-    href: `${crumb.href}`,
-  }));
-  return <OsdsBreadcrumb items={data} />;
+  return <OsdsBreadcrumb items={breadcrumbItems} />;
 }
 
 export default Breadcrumb;
