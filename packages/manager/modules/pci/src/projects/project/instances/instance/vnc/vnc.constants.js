dee8a40a826bd8c6745d0fd5af14836e0990193a
Anoop N <anoop.n@ovhcloud.com>
2024-01-01 12:01:50 +0530

    fix(pci): white-list US VNC URL
     ref: DTRSD-125430
    
    Signed-off-by: Anoop N <anoop.n@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/instances/instance/vnc/vnc.constants.js
+++ packages/manager/modules/pci/src/projects/project/instances/instance/vnc/vnc.constants.js
@@ -1,5 +1,6 @@
 export const URL_WHITELIST = [
   'https://compute.*.cloud.ovh.net:6080/**', // VNC PUBLIC CLOUD
+  'https://compute.*.cloud.ovh.us:6080/**',
   'https://compute.*.trustedzone.cloud.ovh.net:6080/**', // VNC PUBLIC CLOUD FOR TRUSTED ZONE
 ];
 
