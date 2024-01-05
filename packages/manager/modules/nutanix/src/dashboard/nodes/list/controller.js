8ff9849daa913bbc569a16093c76d36acc4dd178
Amandine Dectot <amandine.dectot.ext@corp.ovh.com>
2024-01-05 16:24:29 +0100

    feat(dedicated.nutanix): filter on nodes status
    ref: MANAGER-12974
    filter on nodes status to be done on visible status
    
    Signed-off-by: Amandine Dectot <amandine.dectot.ext@corp.ovh.com>

--- packages/manager/modules/nutanix/src/dashboard/nodes/list/controller.js
+++ packages/manager/modules/nutanix/src/dashboard/nodes/list/controller.js
@@ -1,7 +1,28 @@
+import { NODE_BADGE_STATE } from './constants';
+
 export default class NutanixAllNodesCtrl {
   /* @ngInject */
-  constructor(ovhManagerRegionService) {
+  constructor(ovhManagerRegionService, $translate) {
     this.ovhManagerRegionService = ovhManagerRegionService;
+    this.$translate = $translate;
+    this.NODE_BADGE_STATE = NODE_BADGE_STATE;
+  }
+
+  $onInit() {
+    const uniqueStates = [...new Set(this.nodes.map(({ state }) => state))];
+
+    this.stateColumnOptions = {
+      hideOperators: true,
+      values: uniqueStates.reduce(
+        (options, status) => ({
+          ...options,
+          [status]: this.$translate.instant(
+            `nutanix_dashboard_nodes_list_status_${status}`,
+          ),
+        }),
+        {},
+      ),
+    };
   }
 
   static getNodeDetailsState(nodeId) {
