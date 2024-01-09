6bb6dfdfe51f6216b67cbfd0cbb7167bffb01d40
Amandine Dectot <amandine.dectot.ext@corp.ovh.com>
2024-01-09 19:27:39 +0100

    feat(dedicated.vmware): custom nsx edges sizing
    ref: MANAGER-12763
    
    Signed-off-by: Amandine Dectot <amandine.dectot.ext@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/dedicatedCloud/datacenter/dashboard/dedicatedCloud.datacenter.dashboard.module.js
+++ packages/manager/apps/dedicated/client/app/dedicatedCloud/datacenter/dashboard/dedicatedCloud.datacenter.dashboard.module.js
@@ -3,6 +3,7 @@ import angular from 'angular';
 import datacenterDashboardComponent from '../../../components/dedicated-cloud/datacenter/dashboard';
 import deleteDatacenter from './delete';
 import deleteDrp from './deleteDrp';
+import manageNsxEdges from './manage-nsx-edges';
 import routing from './dedicatedCloud.datacenter.dashboard.routes';
 
 const moduleName = 'ovhManagerDedicatedCloudDatacenterDashboard';
@@ -12,6 +13,7 @@ angular
     datacenterDashboardComponent,
     deleteDatacenter,
     deleteDrp,
+    manageNsxEdges,
   ])
   .config(routing);
 
