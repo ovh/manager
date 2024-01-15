d5217184a725509a35f9700d57a57325ae7b071e
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2024-01-15 22:03:53 +0100

    feat(octavia): added l7 policy creation page
    ref: MANAGER-10691
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

---
+++ packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/listeners/listener/l7/create/component.js
@@ -0,0 +1,18 @@
+import controller from './controller';
+import template from './template.html';
+
+export default {
+  bindings: {
+    projectId: '<',
+    region: '<',
+    listenerId: '<',
+    redirectHttpCodes: '<',
+    pools: '<',
+    trackL7CreatePolicyAction: '<',
+    trackL7CreatePolicyPage: '<',
+    getL7RuleCreationLink: '<',
+    goBackToL7PoliciesList: '<',
+  },
+  controller,
+  template,
+};
