2d4131689b0af6fbf0353047f4cf104b6962d68b
Amandine Dectot <amandine.dectot.ext@corp.ovh.com>
2024-01-22 14:28:15 +0100

    feat(dedicated.vmware):  resize nsx edges from manager
    ref: MANAGER-12824
    Allow Vmware customer to resize their NSX Edges from manager
    
    Signed-off-by: Amandine Dectot <amandine.dectot.ext@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/components/dedicated-cloud/dedicatedCloud.constant.js
+++ packages/manager/apps/dedicated/client/app/components/dedicated-cloud/dedicatedCloud.constant.js
@@ -3,9 +3,16 @@ export const DEDICATED_CLOUD_CONSTANTS = {
   pccNewGeneration: '2.0',
 };
 
+export const TASK_STATUS = {
+  DONE: 'done',
+  ERROR: 'error',
+  CANCELED: 'canceled',
+};
+
 export const UNAVAILABLE_PCC_CODE = [400, 404];
 
 export default {
   DEDICATED_CLOUD_CONSTANTS,
   UNAVAILABLE_PCC_CODE,
+  TASK_STATUS,
 };
