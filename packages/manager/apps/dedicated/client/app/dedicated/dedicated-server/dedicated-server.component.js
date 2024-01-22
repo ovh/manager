ba6112fd0b6a01b79cbcbec9669f810a638e99d9
Sachin Ramesh <104337442+sachinrameshn@users.noreply.github.com>
2024-01-22 20:00:53 +0530

    feat(dedicated): add feature flipping for tabs on listing page (#10735)
    ref: MANAGER-12274
    Signed-off-by: sachinrameshn <sachin.ramesh@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/dedicated/dedicated-server/dedicated-server.component.js
+++ packages/manager/apps/dedicated/client/app/dedicated/dedicated-server/dedicated-server.component.js
@@ -2,6 +2,8 @@ import template from './dedicated-server.html';
 
 export default {
   bindings: {
+    featureAvailability: '<',
+    isMultiAZAvailable: '<',
     currentActiveLink: '<',
     clustersLink: '<',
     allServersLink: '<',
