79d49d8c4cbe1dfe6310bca2a48ad72e93b5c1e8
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2024-01-22 20:27:22 +0100

    feat(octavia): added l7 policies listing page
    ref: MANAGER-12697
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/listeners/listener/l7/routing.js
+++ packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/listeners/listener/l7/routing.js
@@ -1,5 +1,6 @@
 import { TRACKING_SUFFIX } from './constants';
 import { TRACKING_SUFFIX as LISTENERS_TACKING_SUFFIX } from '../../constants';
+import { LISTENER_PROTOCOLS_ENABLING_POLICIES } from '../../list/constants';
 
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state(
@@ -37,6 +38,35 @@ export default /* @ngInject */ ($stateProvider) => {
           atInternet.trackPage({
             name: `${trackL7Base}::${hit}`,
           }),
+      },
+      onEnter: /* @ngInject */ (
+        $transition$,
+        $state,
+        projectId,
+        region,
+        loadbalancerId,
+      ) => {
+        $transition$
+          .injector()
+          .getAsync('listener')
+          .then((listener) => {
+            // If the listener we try to access is not compatible with L7 Policies
+            // we redirect the user on the listeners list
+            // (it should only happen if the user try to access this page via the url directly)
+            if (
+              !LISTENER_PROTOCOLS_ENABLING_POLICIES.includes(listener.protocol)
+            ) {
+              return $state.go(
+                'octavia-load-balancer.loadbalancer.listeners.list',
+                {
+                  projectId,
+                  region,
+                  loadbalancerId,
+                },
+              );
+            }
+            return null;
+          });
       },
       redirectTo:
         'octavia-load-balancer.loadbalancer.listeners.listener.l7Policies.list',
