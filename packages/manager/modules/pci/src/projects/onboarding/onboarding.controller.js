f5970cd5ff57713e5f4d9899177f5eeae34842e5
Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
2024-01-22 10:16:43 +0100

    feat(projects.onboarding): code cleaning for onboarding page
    ref: TAPC-76
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>

--- packages/manager/modules/pci/src/projects/onboarding/onboarding.controller.js
+++ packages/manager/modules/pci/src/projects/onboarding/onboarding.controller.js
@@ -3,16 +3,23 @@ import illustrationUs from './assets/onboarding-us.png';
 
 export default class {
   /* @ngInject */
-  constructor($q, orderCart, pciProjectNew) {
+  constructor($q, orderCart, pciProjectNew, atInternet) {
     this.illustration = illustration;
     this.illustrationUs = illustrationUs;
     this.$q = $q;
     this.orderCart = orderCart;
     this.pciProjectNew = pciProjectNew;
+    this.atInternet = atInternet;
   }
 
   onCreateDiscoveryProjectClick() {
     this.isLoading = true;
+    this.atInternet.trackClick({
+      name: 'PCI_PROJECTS_ONBOARDING_NEW_PROJECT',
+      type: 'action',
+    });
+    this.isLoading = true;
+
     return this.pciProjectNew
       .finalizeCart(this.cart)
       .then((order) => {
@@ -27,50 +34,6 @@ export default class {
       })
       .finally(() => {
         this.isLoading = false;
-      });
-  }
-
-  // Waiting for specification to clarify the behaviour of the HDS component
-  setHdsOptionItem() {
-    const { cartId, projectItem, hdsItem } = this.cart;
-    const { hds: hdsIsChecked } = this.model;
-    if (this.hds.isValidSupportLevel) {
-      if (hdsIsChecked && !hdsItem) {
-        const priceMode = this.hds.option.prices.find(({ capacities }) =>
-          capacities.includes('renew'),
-        );
-        return this.orderCart
-          .addNewOptionToProject(cartId, {
-            duration: priceMode.duration,
-            itemId: projectItem.itemId, // Add HDS option to project
-            planCode: this.hds.option.planCode,
-            pricingMode: priceMode.pricingMode,
-            quantity: 1,
-          })
-          .then((option) => this.cart.addItem(option));
-      }
-      if (!hdsIsChecked && hdsItem) {
-        return this.orderCart
-          .deleteItem(cartId, hdsItem.itemId)
-          .then(() => this.cart.deleteItem(hdsItem));
-      }
-    }
-    return this.$q.when();
-  }
-
-  onHdsCheckboxChanged(hdsCheckbox) {
-    this.model.hds = hdsCheckbox.status;
-    this.hds.isInprogressRequest = true;
-    this.setHdsOptionItem()
-      .then(() => this.getSummary())
-      .then((summary) => {
-        this.summary = summary;
-      })
-      .catch(() => {
-        this.model.hds = false;
-      })
-      .finally(() => {
-        this.hds.isInprogressRequest = false;
       });
   }
 }
