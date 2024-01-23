d2eb376209da4ddcebd033e24afe8ecadbe43612
Nicolas Pierre-charles <nicolas@nicode.fr>
2024-01-23 17:44:37 +0100

    fix: vite config
    ref: MANAGER-13023
    
    Signed-off-by: Nicolas Pierre-charles <nicolas@nicode.fr>

--- packages/manager/apps/vrack-services/vite.config.mjs
+++ packages/manager/apps/vrack-services/vite.config.mjs
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite';
 import { getBaseConfig } from '@ovh-ux/manager-vite-config';
 
-export default defineConfig(({ command }) => {
-  if (command === 'serve') {
-    return {
-      ...getBaseConfig(),
-      base: '/app/',
-    };
-  }
-  return {
-    ...getBaseConfig(),
-    base: '/#/dedicated/vrack-services/',
-  };
-});
+export default defineConfig(({ command }) => ({
+  ...getBaseConfig(),
+  base: command === 'serve' ? '/app/' : '/#/dedicated/vrack-services/',
+}));
