4685d8ed0b3c86a95fff0e76f3ba657e290d29b1
Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
2024-01-24 09:11:20 +0100

    feat(projects.project): add tracking for activation page & modal
    ref: TAPC-76
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>

--- packages/manager/modules/pci/src/projects/project/activate/activate.controller.js
+++ packages/manager/modules/pci/src/projects/project/activate/activate.controller.js
@@ -2,12 +2,13 @@ import illustration from './assets/activate-bg.png';
 
 export default class ProjectActivateController {
   /* @ngInject */
-  constructor($q, orderCart, pciProjectNew, CucCloudMessage) {
+  constructor($q, orderCart, pciProjectNew, CucCloudMessage, atInternet) {
     this.$q = $q;
     this.orderCart = orderCart;
     this.pciProjectNew = pciProjectNew;
     this.CucCloudMessage = CucCloudMessage;
     this.illustration = illustration;
+    this.atInternet = atInternet;
   }
 
   $onInit() {
