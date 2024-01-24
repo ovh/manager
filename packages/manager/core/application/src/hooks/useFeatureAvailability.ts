a2416d80a47725fb3b87625c2c13700968cf2a4b
stif59100 <steeve.vanderstocken@ovhcloud.com>
2024-01-24 14:45:14 +0100

    feat(generator.documentation): add documentation for the generator
    ref:MANAGER-11571
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

--- packages/manager/core/application/src/hooks/useFeatureAvailability.ts
+++ packages/manager/core/application/src/hooks/useFeatureAvailability.ts
@@ -1,47 +1,25 @@
-import { useQuery } from '@tanstack/react-query';
 import { apiClient } from '@ovh-ux/manager-core-api';
-
-export const featureAvailabilityBaseQueryKey = 'feature-availability-';
-
-export type UseFeatureAvailabilityParams = {
-  parent: string;
-  featureList?: string[];
-};
-
-const getFeaturesListToCheck = ({
-  parent,
-  featureList = [],
-}: UseFeatureAvailabilityParams) =>
-  [parent].concat(featureList.map((name) => `${parent}:${name}`));
+import { useQuery } from '@tanstack/react-query';
 
 /**
  * @example
- * const {data, error, loading} = useFeatureAvailability(
- *   {parent: 'dedicated-nasha', featureList: ['feature1', 'feature2']},
- *   {parent: 'app2'},
- *   {parent: 'app3', featureList: ['feat1']}
- * );
- * const isAppAvailable = data.['dedicated-nasha'];
- * const isFeature1Available = data.feature1;
- * const isFeature2Available = data.feature2;
- * const isApp2Available = data.app2;
- * const isApp3Available = data.app3;
- * const isApp3Feature1Available = data.feat1;
+ * const { data, isLoading } = useFeatureAvailability([
+    'billing:administrativeMandate',
+    'billing:commitment',
+    'billing:billingServices',
+    'billing:titi',
+    'web:microsoft',
+  ]);
  */
-export const useFeatureAvailability = (
-  ...featureList: UseFeatureAvailabilityParams[]
-) => {
-  const featuresListToCheck = featureList.flatMap(getFeaturesListToCheck);
 
+export const useFeatureAvailability = (featureList) => {
   const fetchFeatureAvailabilityData = async () => {
     const result = await apiClient.aapi.get(
-      `/feature/${featuresListToCheck.join(',')}/availability`,
+      `/feature/${featureList.join(',')}/availability`,
     );
+
     return result.data || {};
   };
 
-  return useQuery<Record<string, boolean>>(
-    [`${featureAvailabilityBaseQueryKey}-${featuresListToCheck.join('-')}`],
-    fetchFeatureAvailabilityData,
-  );
+  return useQuery([featureList.join('-')], fetchFeatureAvailabilityData);
 };
