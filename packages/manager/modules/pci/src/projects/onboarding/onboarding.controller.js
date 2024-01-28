364b8e5cae7dad912bbf421f80f7b2cff96e85a8
Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
2024-01-28 23:04:28 +0100

    feat(projects.onboarding): remove HDS from discovery onboarding page
    ref: TAPC-76
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>

--- packages/manager/modules/pci/src/projects/onboarding/onboarding.controller.js
+++ packages/manager/modules/pci/src/projects/onboarding/onboarding.controller.js
@@ -34,48 +34,4 @@ export default class {
         this.isLoading = false;
       });
   }
-
-  // Waiting for specification to clarify the behaviour of the HDS component
-  setHdsOptionItem() {
-    const { cartId, projectItem, hdsItem } = this.cart;
-    const { hds: hdsIsChecked } = this.model;
-    if (this.hds.isValidSupportLevel) {
-      if (hdsIsChecked && !hdsItem) {
-        const priceMode = this.hds.option.prices.find(({ capacities }) =>
-          capacities.includes('renew'),
-        );
-        return this.orderCart
-          .addNewOptionToProject(cartId, {
-            duration: priceMode.duration,
-            itemId: projectItem.itemId, // Add HDS option to project
-            planCode: this.hds.option.planCode,
-            pricingMode: priceMode.pricingMode,
-            quantity: 1,
-          })
-          .then((option) => this.cart.addItem(option));
-      }
-      if (!hdsIsChecked && hdsItem) {
-        return this.orderCart
-          .deleteItem(cartId, hdsItem.itemId)
-          .then(() => this.cart.deleteItem(hdsItem));
-      }
-    }
-    return this.$q.when();
-  }
-
-  onHdsCheckboxChanged(hdsCheckbox) {
-    this.model.hds = hdsCheckbox.status;
-    this.hds.isInprogressRequest = true;
-    this.setHdsOptionItem()
-      .then(() => this.getSummary())
-      .then((summary) => {
-        this.summary = summary;
-      })
-      .catch(() => {
-        this.model.hds = false;
-      })
-      .finally(() => {
-        this.hds.isInprogressRequest = false;
-      });
-  }
 }
