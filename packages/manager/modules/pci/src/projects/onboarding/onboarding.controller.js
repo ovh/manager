1d7be9d426de1cfeb298308db9e6a13ee96b58dd
Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
2024-01-29 00:48:39 +0100

    feat(projects.project): add tracking for onboarding page
    ref: TAPC-76
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>

--- packages/manager/modules/pci/src/projects/onboarding/onboarding.controller.js
+++ packages/manager/modules/pci/src/projects/onboarding/onboarding.controller.js
@@ -3,16 +3,21 @@ import illustrationUs from './assets/onboarding-us.png';
 
 export default class {
   /* @ngInject */
-  constructor($q, orderCart, pciProjectNew) {
+  constructor($q, orderCart, pciProjectNew, atInternet) {
     this.illustration = illustration;
     this.illustrationUs = illustrationUs;
     this.$q = $q;
     this.orderCart = orderCart;
     this.pciProjectNew = pciProjectNew;
+    this.atInternet = atInternet;
   }
 
   onCreateDiscoveryProjectClick() {
     this.isLoading = true;
+    this.atInternet.trackClick({
+      name: 'PCI_PROJECTS_ONBOARDING_NEW_PROJECT',
+      type: 'action',
+    });
     return this.pciProjectNew
       .finalizeCart(this.cart)
       .then((order) => {
