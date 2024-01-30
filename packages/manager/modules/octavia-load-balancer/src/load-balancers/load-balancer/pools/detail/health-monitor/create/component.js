f7b89fd2a35e41f2dfc652873a81e58ac9b59079
Quentin Pavy <contact@quentinpavy.com>
2024-01-30 16:24:09 +0100

    feat(octavia): edit health monitor page
    ref: MANAGER-12743
    
    Signed-off-by: Quentin Pavy <contact@quentinpavy.com>

--- packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/pools/detail/health-monitor/create/component.js
+++ packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/pools/detail/health-monitor/create/component.js
@@ -7,6 +7,7 @@ export default {
     region: '<',
     pool: '<',
     goToDashboard: '<',
+    goBack: '<',
   },
   controller,
   template,
--- packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/pools/detail/health-monitor/edit/component.js
+++ packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/pools/detail/health-monitor/edit/component.js
@@ -2,6 +2,14 @@ import template from './template.html';
 import controller from './controller';
 
 export default {
+  bindings: {
+    projectId: '<',
+    region: '<',
+    pool: '<',
+    healthMonitor: '<',
+    goToDashboard: '<',
+    goBack: '<',
+  },
   controller,
   template,
 };
