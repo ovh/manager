5ab21987eb14e8e3863a3f4d785dfd19da03b3f8
aboungnaseng-ovhcloud <123390642+aboungnaseng-ovhcloud@users.noreply.github.com>
2024-02-02 10:47:28 +0100

    feat(manager-components): add react lib manager components (#10595)
    ref: MANAGER-12621
    
    Signed-off-by: Lio <lionel95200@gmail.com>

--- packages/super-components/components/msc-tail-logs/src/global.ts
+++
@@ -1,9 +0,0 @@
-// ###
-// main global file to include in this package in any cases (dev and prod mode).
-// it allows to initialize some stuff of the library, after the component itself
-// ###
-
-import { OdsLogger } from '@ovhcloud/ods-common-core';
-
-const logger = new OdsLogger('msc-tail-logs.global');
-logger.log('init');
