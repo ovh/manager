fc57a4d6e9c5599247671385a29f6a6188117da3
antonymarion <marionantony@gmail.com>
2024-02-02 10:42:48 +0100

    feat(web): split dns and domain api calls with two tabs (#10405)
    ref: MANAGER-12656
    
    Signed-off-by: Antony MARION <antony.marion.ext@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>
    Co-authored-by: Hyenne Guillaume <guillaume.hyenne@ovhcloud.com>

---
+++ packages/manager/apps/web/client/app/domain-operation/domain-operation.component.js
@@ -0,0 +1,11 @@
+import template from './domain-operation.html';
+
+export default {
+  bindings: {
+    domainOperationLink: '<',
+    dnsOperationLink: '<',
+    getTabItemUrl: '<',
+    currentActiveLink: '<',
+  },
+  template,
+};
