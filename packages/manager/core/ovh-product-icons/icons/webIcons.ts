b842ad598431de7c2c281be8101b77fac1c54c26
antonymarion <marionantony@gmail.com>
2024-02-02 10:41:18 +0100

    feat(web): remove sharepoint from orders (#10749)
    ref: MANAGER-12962
    
    Signed-off-by: Antony MARION <antony.marion.ext@ovhcloud.com>

--- packages/manager/core/ovh-product-icons/icons/webIcons.ts
+++ packages/manager/core/ovh-product-icons/icons/webIcons.ts
@@ -1,14 +1,8 @@
-import {
-  Hosting,
-  Office365,
-  Sharepoint,
-  Exchange,
-} from '../assets-icon-svg/icons';
+import { Hosting, Office365, Exchange } from '../assets-icon-svg/icons';
 
 const webIcons = {
   Hosting,
   Office365,
-  Sharepoint,
   Exchange,
 };
 
