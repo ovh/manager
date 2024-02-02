e645dc55093dcc7c8f0d8def41dca13c2382005f
Antony MARION <antony.marion.ext@ovhcloud.com>
2024-02-02 11:37:39 +0100

    feat(web): uniform order channel for db
        ref: MANAGER-13070
    
    Signed-off-by: Antony MARION <antony.marion.ext@ovhcloud.com>

--- packages/manager/apps/web/client/app/private-database/order/clouddb/index.js
+++ packages/manager/apps/web/client/app/private-database/order/clouddb/index.js
@@ -1,11 +1,12 @@
 import component from './private-database-order-clouddb.component';
 import routing from './private-database-order-clouddb.routing';
 import service from './private-database-order-clouddb.service';
+import dbCategoriesOffers from '../../../hosting/database/order/public/components/steps/db-categories-offers';
 
 const moduleName = 'ovhManagerWebPrivateDatabaseOrderClouddb';
 
 angular
-  .module(moduleName, [])
+  .module(moduleName, [dbCategoriesOffers])
   .component(component.name, component)
   .service('PrivateDatabaseOrderCloudDb', service)
   .config(routing);
