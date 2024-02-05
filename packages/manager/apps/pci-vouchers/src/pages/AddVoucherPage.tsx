5ed44de57d5529021e97b86672dbbf4b132e0c68
Florian Renaut <florian.renaut@corp.ovh.com>
2024-02-05 14:27:47 +0100

    feat(pci-vouchers): add pci-vouchers react app
    ref: DTCORE-1675
    
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>
    Co-authored-by: Yoann Fievez <yoann.fievez@ovhcloud.com>

---
+++ packages/manager/apps/pci-vouchers/src/pages/AddVoucherPage.tsx
@@ -0,0 +1,33 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { useNotifications } from '@ovhcloud/manager-components';
+import AddVoucherModal from '@/components/vouchers/AddVoucherModal';
+
+export default function AddVoucherPage() {
+  const { projectId } = useParams();
+  const { t } = useTranslation('common');
+  const { addError, addSuccess } = useNotifications();
+  const navigate = useNavigate();
+  const onClose = () => {
+    navigate('..');
+  };
+  return (
+    <>
+      <AddVoucherModal
+        projectId={`${projectId}`}
+        onClose={() => onClose()}
+        onSuccess={() => {
+          addSuccess(t('cpb_vouchers_add_success'));
+        }}
+        onError={(error: Error) => {
+          addError(
+            <>
+              {t('cpb_vouchers_add_error')}
+              {error && ` (${error.message})`}
+            </>,
+          );
+        }}
+      />
+    </>
+  );
+}
