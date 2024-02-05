d11b3bc2e94ccdede49820919395a7cb766c8b88
Florian Renaut <florian.renaut@corp.ovh.com>
2024-02-05 14:27:47 +0100

    feat(pci-vouchers): add pci-vouchers react app
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>
    Co-authored-by: Yoann Fievez <yoann.fievez@ovhcloud.com>

---
+++ packages/manager/apps/pci-vouchers/src/components/vouchers/listing/Validity.tsx
@@ -0,0 +1,31 @@
+import { format } from 'date-fns';
+import * as dateFnsLocales from 'date-fns/locale';
+import { useRef } from 'react';
+import { useTranslation } from 'react-i18next';
+import { DataGridTextCell } from '@ovhcloud/manager-components';
+import getLocale from '@/data/date';
+
+export default function Validity({ date }: { date: string | null }) {
+  const { i18n } = useTranslation('common');
+
+  const locales = useRef({ ...dateFnsLocales }).current;
+
+  let displayDate = '';
+
+  if (date) {
+    const userLocale = getLocale(i18n.language);
+
+    if (userLocale in locales) {
+      const localeId = userLocale as keyof typeof locales;
+      displayDate = format(new Date(date), 'PPpp', {
+        locale: locales[localeId],
+      });
+    } else {
+      displayDate = format(new Date(date), 'PPpp', {
+        locale: locales.fr,
+      });
+    }
+  }
+
+  return <DataGridTextCell>{displayDate}</DataGridTextCell>;
+}
