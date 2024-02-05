dc100458ff609de49506b74ab3a314faa616bac6
Florian Renaut <florian.renaut@corp.ovh.com>
2024-02-05 14:27:47 +0100

    feat(pci-vouchers): add pci-vouchers react app
    ref: DTCORE-1675
    
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>
    Co-authored-by: Yoann Fievez <yoann.fievez@ovhcloud.com>

---
+++ packages/manager/apps/pci-vouchers/src/pages/BuyCreditPage.tsx
@@ -0,0 +1,45 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { useNotifications } from '@ovhcloud/manager-components';
+import BuyCreditModal from '@/components/vouchers/BuyCreditModal';
+import style from '@/components/common.module.css';
+
+export default function BuyCreditPage() {
+  const { projectId } = useParams();
+  const { t } = useTranslation('common');
+  const { addError, addSuccess } = useNotifications();
+  const navigate = useNavigate();
+  const onClose = () => {
+    navigate('..');
+  };
+  return (
+    <>
+      <BuyCreditModal
+        projectId={`${projectId}`}
+        onClose={() => onClose()}
+        onSuccess={(amount, url) => {
+          addSuccess(
+            <span
+              className={style.linkContainer}
+              dangerouslySetInnerHTML={{
+                __html: t('cpb_vouchers_add_credit_success', {
+                  amount,
+                  url,
+                  interpolation: { escapeValue: false },
+                }),
+              }}
+            ></span>,
+          );
+        }}
+        onError={(error: Error) => {
+          addError(
+            <>
+              {t('cpb_vouchers_add_error')}
+              {error && ` (${error.message})`}
+            </>,
+          );
+        }}
+      />
+    </>
+  );
+}
