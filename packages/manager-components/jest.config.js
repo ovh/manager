acfd433b15c221207c5b269ff0b752c91643ddb2
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2024-02-07 10:50:17 +0100

    fix(manager-components): fix unit tests
    ref: MANAGER-13586
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-components/jest.config.js
+++ packages/manager-components/jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!lodash-es|@ovh-ux|@ovhcloud|@stencil)',
   ],
+  testMatch: ['<rootDir>/src/components/**/*.spec.tsx'],
   collectCoverageFrom: ['src/components/**/**.component.tsx'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
