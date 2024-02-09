1d91c5d74b2cd136ce61cf2468f34df6c7d1ef56
David Arsène <david.arsene.ext@ovhcloud.com>
2024-02-09 11:58:40 +0100

    feat(container): fix PR feedback
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/container/src/cookie-policy/CookiePolicy.tsx
+++ packages/manager/apps/container/src/cookie-policy/CookiePolicy.tsx
@@ -68,7 +68,7 @@ const CookiePolicy = ({ shell, onValidate }: Props): JSX.Element => {
   }
 
   useEffect(() => {
-    const isRegionUS = environment.getRegion() === 'US'
+    const isRegionUS = environment.getRegion() === 'US';
     trackingPlugin.setRegion(environment.getRegion());
     // activate tracking if region is US or if tracking consent cookie is valid
     if (isRegionUS || cookies.MANAGER_TRACKING === '1') {
