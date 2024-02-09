481591dc7a34d21c29047c23147c2fc6cf61be79
benjaminlepine <benjamin.lepine@hotmail.fr>
2024-02-09 17:04:27 +0000

    feat(container): pnr v2 left navigation
    ref: MANAGER-13152
    
    Signed-off-by: benjaminlepine <benjamin.lepine@hotmail.fr>

---
+++ packages/manager/apps/container/src/container/nav-reshuffle/sidebar/navigation-tree/services/sunrise.ts
@@ -0,0 +1,46 @@
+import illustration from '@/assets/images/sidebar/sunrise.png';
+export default {
+  id: 'sunrise',
+  translation: 'sidebar_sunrise',
+  shortTranslation: 'sidebar_sunrise_short',
+  illustration,
+  routing: {
+    application: 'sunrise',
+  },
+  count: false,
+  features: ['sunrise'],
+  children: [
+    {
+      id: 'horizon-view',
+      translation: 'sidebar_sunrise_infra',
+      routing: {
+        application: 'sunrise',
+        hash: '#/horizon-view',
+      },
+    },
+    {
+      id: 'telephony',
+      translation: 'sidebar_sunrise_contact_center',
+      routing: {
+        application: 'sunrise',
+        hash: '#/telephony',
+      },
+    },
+    {
+      id: 'csp2',
+      translation: 'sidebar_sunrise_office',
+      routing: {
+        application: 'sunrise',
+        hash: '#/csp2',
+      },
+    },
+    {
+      id: 'sslGateway',
+      translation: 'sidebar_sunrise_ssl_gateway',
+      routing: {
+        application: 'sunrise',
+        hash: '#/sslGateway',
+      },
+    },
+  ],
+};
