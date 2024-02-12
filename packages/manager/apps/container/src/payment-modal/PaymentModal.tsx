155b3beb351f07e2cfdffaff51d647e449ba6d40
David Arsène <david.arsene.ext@ovhcloud.com>
2024-02-12 16:03:52 +0100

    feat(container): fix PR feedback & replace ui-kit color by ods
    ref: MANAGER-12494
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/container/src/payment-modal/PaymentModal.tsx
+++ packages/manager/apps/container/src/payment-modal/PaymentModal.tsx
@@ -25,19 +25,18 @@ interface IPaymentMethod {
 }
 
 const computeAlert = (paymentMethods: IPaymentMethod[]): string => {
-  const hasDefaultPaymentMethod: IPaymentMethod = paymentMethods?.find(currentPaymentMethod => currentPaymentMethod.default);
-  if (!hasDefaultPaymentMethod) {
+  if (!paymentMethods?.some(currentPaymentMethod => currentPaymentMethod.default)) {
     return PAYMENT_ALERTS.NO_DEFAULT;
   }
   const currentCreditCard: IPaymentMethod = paymentMethods?.find(currentPaymentMethod => currentPaymentMethod.paymentType === 'CREDIT_CARD');
   if (currentCreditCard) {
-    const creditCardExpirationDate: Date = new Date(currentCreditCard.expirationDate);
+    const creditCardExpirationDate = new Date(currentCreditCard.expirationDate);
     if (creditCardExpirationDate.getTime() < Date.now()) {
       return PAYMENT_ALERTS.EXPIRED_CARD;
     }
-    const currentDateMinus30Days: Date = new Date();
+    const currentDateMinus30Days = new Date();
     currentDateMinus30Days.setDate(currentDateMinus30Days.getDate() - 30);
-    const isSoonToBeExpireCreditCard: boolean = currentDateMinus30Days.getTime() > Date.now() && creditCardExpirationDate.getTime() > Date.now();
+    const isSoonToBeExpireCreditCard = currentDateMinus30Days.getTime() > Date.now() && creditCardExpirationDate.getTime() > Date.now();
     if (isSoonToBeExpireCreditCard) {
       return PAYMENT_ALERTS.SOON_EXPIRED_CARD;
     }
