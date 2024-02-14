9de0325f905ff564b936166221e679c2dfd7cedc
Yoann Fievez <yoann.fievez@ovhcloud.com>
2024-02-14 14:34:07 +0100

    feat(pci-users): init react app for users & roles section ref: DTCORE-1856
    Signed-off-by: Yoann Fievez <yoann.fievez@ovhcloud.com>

---
+++ packages/manager/apps/pci-users/src/components/OdsNotification.tsx
@@ -0,0 +1,36 @@
+import { OsdsMessage } from '@ovhcloud/ods-components/react';
+import { ODS_MESSAGE_TYPE } from '@ovhcloud/ods-components';
+import { Notification, NotificationType } from '@/hooks/useNotifications';
+import style from '@/components/common.module.css';
+
+type OdsNotificationProps = {
+  notification: Notification;
+};
+
+const getOdsMessageColor = (type: NotificationType) => {
+  switch (type) {
+    case NotificationType.Success:
+      return ODS_MESSAGE_TYPE.success;
+    case NotificationType.Error:
+      return ODS_MESSAGE_TYPE.error;
+    case NotificationType.Warning:
+      return ODS_MESSAGE_TYPE.warning;
+    case NotificationType.Info:
+      return ODS_MESSAGE_TYPE.info;
+    default:
+      return ODS_MESSAGE_TYPE.info;
+  }
+};
+
+export function OdsNotification({ notification }: OdsNotificationProps) {
+  return (
+    <OsdsMessage
+      className={style.linkContainer}
+      type={getOdsMessageColor(notification.type)}
+    >
+      {notification.content}
+    </OsdsMessage>
+  );
+}
+
+export default OdsNotification;
