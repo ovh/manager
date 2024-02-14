9f02d0745876a7b21699f5708e55438069c407f2
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-02-14 16:06:24 +0100

    feat: init react app
    DATATR-471
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
    
    feat: clean project and fix breadcrumb
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
    
    feat: add loader to router
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

---
+++ packages/manager/apps/pci-databases-analytics/src/pages/services/[serviceId]/dashboard/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Service = () => {
+  return <p>Dummy Service</p>;
+};
+
+export default Service;
---
+++ packages/manager/apps/pci-databases-analytics/src/pages/services/create/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function breadcrumb() {
+  return 'New';
+}
+
+const Service = () => {
+  return <p>Create a Service</p>;
+};
+
+export default Service;
