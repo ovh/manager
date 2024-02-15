48a0e5d88f9967882c5ca0fbc155720737a7ba0b
Yoann Fievez <yoann.fievez@ovhcloud.com>
2024-02-15 14:05:26 +0100

    feat(pci-users): add delete modal and delete page ref: DTCORE-1864
    Signed-off-by: Yoann Fievez <yoann.fievez@ovhcloud.com>

---
+++ packages/manager/apps/pci-users/src/pages/RemoveUserPage.tsx
@@ -0,0 +1,44 @@
+import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { useNotifications } from '@/hooks/useNotifications';
+import RemoveUserModal from '@/components/users/RemoveUserModal';
+import { useUser } from '@/hooks/useUser.ts';
+
+export default function RemoveSshPage() {
+  const { projectId } = useParams();
+  const [searchParams] = useSearchParams();
+  const { t } = useTranslation('common');
+  const { addError, addSuccess } = useNotifications();
+  const navigate = useNavigate();
+  const onClose = () => {
+    navigate('..');
+  };
+  const userId = searchParams.get('userId');
+  const { data: user } = useUser(projectId || '', userId);
+  return (
+    <>
+      <RemoveUserModal
+        projectId={`${projectId}`}
+        userId={`${userId}`}
+        onClose={() => onClose()}
+        onSuccess={() => {
+          addSuccess(
+            t('pci_projects_project_users_delete_success_message', {
+              user: user?.username,
+            }),
+          );
+        }}
+        onError={(error: Error) => {
+          addError(
+            <>
+              {t('pci_projects_project_users_delete_error_delete', {
+                message: error && error.message,
+                user: user?.username,
+              })}
+            </>,
+          );
+        }}
+      />
+    </>
+  );
+}
