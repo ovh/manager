c601e864282ea031d414e7cb95c18821e1a801ec
sachinrameshn <sachin.ramesh@ovhcloud.com>
2024-02-15 12:06:22 +0530

    feat(pci): white-list Localzones VNC URL
    ref: MANAGER-13556
    
    Signed-off-by: sachinrameshn <sachin.ramesh@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/instances/instance/vnc/vnc.constants.js
+++ packages/manager/modules/pci/src/projects/project/instances/instance/vnc/vnc.constants.js
@@ -2,6 +2,7 @@ export const URL_WHITELIST = [
   'https://compute.*.cloud.ovh.net:6080/**', // VNC PUBLIC CLOUD
   'https://compute.*.cloud.ovh.us:6080/**',
   'https://compute.*.trustedzone.cloud.ovh.net:6080/**', // VNC PUBLIC CLOUD FOR TRUSTED ZONE
+  'https://novnc.*.cloud.ovh.net/**', // VNC FOR LOCAL ZONES
 ];
 
 export default {
