aa99869d2fb163f75cc9b6014a99d3a585955e23
Sachin Ramesh <104337442+sachinrameshn@users.noreply.github.com>
2024-02-15 20:40:06 +0530

    feat(public-cloud): view status issue for cluster (#10930)
    ref: MANAGER-13587
    
    Signed-off-by: sachinrameshn <sachin.ramesh@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/dedicated/dedicated-server/clusters/cluster/dashboard/dashboard.constants.js
+++ packages/manager/apps/dedicated/client/app/dedicated/dedicated-server/clusters/cluster/dashboard/dashboard.constants.js
@@ -1,8 +1,8 @@
-export const WORK_IN_PROGRESS_LINK = {
+export const TRAVAUX_LINK = {
   US: 'https://status.us.ovhcloud.com/',
   DEFAULT: 'https://www.status-ovhcloud.com/',
 };
 
 export default {
-  WORK_IN_PROGRESS_LINK,
+  TRAVAUX_LINK,
 };
