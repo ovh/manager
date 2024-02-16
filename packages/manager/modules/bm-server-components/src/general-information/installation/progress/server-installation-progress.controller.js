8cc40e6faf68c3e6b1e9a50011fa6998c1c53548
antonymarion <marionantony@gmail.com>
2024-02-16 14:38:19 +0100

    feat(dedicated): remove legacy BringYourOwnImage component (#10943)
    ref: MANAGER-10558
    
    Signed-off-by: Jean-Baptiste Delon <jean-baptiste.delon@ovhcloud.com>

--- packages/manager/modules/bm-server-components/src/general-information/installation/progress/server-installation-progress.controller.js
+++ packages/manager/modules/bm-server-components/src/general-information/installation/progress/server-installation-progress.controller.js
@@ -1,8 +1,4 @@
-import {
-  BYOI_STATUS_ENUM,
-  BYOI_STARTING_MESSAGE,
-  PROGRESS_TASK_STATUS,
-} from './server-installation-progress.constants';
+import { PROGRESS_TASK_STATUS } from './server-installation-progress.constants';
 
 export default class ServerInstallationProgressCtrl {
   /* @ngInject */
@@ -88,39 +84,23 @@ export default class ServerInstallationProgressCtrl {
 
   // Detail of install status
   checkInstallationProgress() {
-    // if task status is init, it's maybe because of byoi install
-    // (which does not have status response yet).
-    // we have to wait that the message of byoi change from 'starting' to anything else in
-    // order to have a response from status API.
-    if (this.isBringYourOwnImageInit()) {
-      this.startPollReinstall();
-    } else {
-      this.Server.progressInstallation(this.$stateParams.productId).then(
-        (task) => {
-          this.$scope.progress.failStatut = false;
+    this.Server.progressInstallation(this.$stateParams.productId).then(
+      (task) => {
+        this.$scope.progress.failStatut = false;
 
-          // doing installation or error installation
-          this.$scope.progress.installationTask = task;
-          this.$scope.progress.nbStep = task.progress.length;
-          this.refreshStepProgress();
-          this.startPollReinstall();
-        },
-        (err) => {
-          if (err.status === 404) {
-            this.reduce();
-          }
-          this.$scope.progress.failStatut = true;
-          this.startPollReinstall();
-        },
-      );
-    }
-  }
-
-  isBringYourOwnImageInit() {
-    return (
-      this.$scope.progress?.task?.status === 'init' &&
-      this.$scope.serverCtrl?.$scope.byoi?.status === BYOI_STATUS_ENUM.DOING &&
-      this.$scope.serverCtrl?.$scope.byoi?.message === BYOI_STARTING_MESSAGE
+        // doing installation or error installation
+        this.$scope.progress.installationTask = task;
+        this.$scope.progress.nbStep = task.progress.length;
+        this.refreshStepProgress();
+        this.startPollReinstall();
+      },
+      (err) => {
+        if (err.status === 404) {
+          this.reduce();
+        }
+        this.$scope.progress.failStatut = true;
+        this.startPollReinstall();
+      },
     );
   }
 
