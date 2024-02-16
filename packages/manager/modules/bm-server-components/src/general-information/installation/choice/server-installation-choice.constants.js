dd8f6f3476d2215141bfe20fc12bdea024026a40
antonymarion <marionantony@gmail.com>
2024-02-16 14:38:19 +0100

    feat(dedicated): remove legacy BringYourOwnImage component (#10943)
    ref: MANAGER-10558
    
    Signed-off-by: Jean-Baptiste Delon <jean-baptiste.delon@ovhcloud.com>

--- packages/manager/modules/bm-server-components/src/general-information/installation/choice/server-installation-choice.constants.js
+++ packages/manager/modules/bm-server-components/src/general-information/installation/choice/server-installation-choice.constants.js
@@ -1,5 +1,42 @@
-export const BYOI_FEATURE = 'dedicated-server:install:byoi';
+export const CUSTOM_IMAGE_GUIDES = {
+  ASIA:
+    'https://help.ovhcloud.com/csm/asia-dedicated-servers-bring-your-own-image-versus-bring-your-own-linux?id=kb_article_view&sysparm_article=KB0061600',
+  AU:
+    'https://help.ovhcloud.com/csm/en-au-dedicated-servers-bring-your-own-image-versus-bring-your-own-linux?id=kb_article_view&sysparm_article=KB0061605',
+  CA:
+    'https://help.ovhcloud.com/csm/en-ca-dedicated-servers-bring-your-own-image-versus-bring-your-own-linux?id=kb_article_view&sysparm_article=KB0061593',
+  DE:
+    'https://help.ovhcloud.com/csm/de-dedicated-servers-bring-your-own-image-versus-bring-your-own-linux?id=kb_article_view&sysparm_article=KB0061596',
+  ES:
+    'https://help.ovhcloud.com/csm/es-es-dedicated-servers-bring-your-own-image-versus-bring-your-own-linux?id=kb_article_view&sysparm_article=KB0061603',
+  FR:
+    'https://help.ovhcloud.com/csm/fr-dedicated-servers-bring-your-own-image-versus-bring-your-own-linux?id=kb_article_view&sysparm_article=KB0061601',
+  GB:
+    'https://help.ovhcloud.com/csm/en-gb-dedicated-servers-bring-your-own-image-versus-bring-your-own-linux?id=kb_article_view&sysparm_article=KB0061592',
+  IE:
+    'https://help.ovhcloud.com/csm/en-ie-dedicated-servers-bring-your-own-image-versus-bring-your-own-linux?id=kb_article_view&sysparm_article=KB0061602',
+  IN:
+    'https://help.ovhcloud.com/csm/asia-dedicated-servers-bring-your-own-image-versus-bring-your-own-linux?id=kb_article_view&sysparm_article=KB0061600',
+  IT:
+    'https://help.ovhcloud.com/csm/it-dedicated-servers-bring-your-own-image-versus-bring-your-own-linux?id=kb_article_view&sysparm_article=KB0061590',
+  PL:
+    'https://help.ovhcloud.com/csm/pl-dedicated-servers-bring-your-own-image-versus-bring-your-own-linux?id=kb_article_view&sysparm_article=KB0061595',
+  PT:
+    'https://help.ovhcloud.com/csm/pt-dedicated-servers-bring-your-own-image-versus-bring-your-own-linux?id=kb_article_view&sysparm_article=KB0061606',
+  QC:
+    'https://help.ovhcloud.com/csm/fr-ca-dedicated-servers-bring-your-own-image-versus-bring-your-own-linux?id=kb_article_view&sysparm_article=KB0061594',
+  SG:
+    'https://help.ovhcloud.com/csm/en-sg-dedicated-servers-bring-your-own-image-versus-bring-your-own-linux?id=kb_article_view&sysparm_article=KB0061604',
+  US:
+    'https://support.us.ovhcloud.com/hc/en-us/articles/25974550370963-Bring-Your-Own-Image-BYOI-Bring-Your-Own-Linux-BYOLinux-a-comparison-sheet',
+  WE:
+    'https://help.ovhcloud.com/csm/en-dedicated-servers-bring-your-own-image-versus-bring-your-own-linux?id=kb_article_view&sysparm_article=KB0061597',
+  WS:
+    'https://help.ovhcloud.com/csm/es-dedicated-servers-bring-your-own-image-versus-bring-your-own-linux?id=kb_article_view&sysparm_article=KB0061598',
+  DEFAULT:
+    'https://help.ovhcloud.com/csm/en-dedicated-servers-bring-your-own-image-versus-bring-your-own-linux?id=kb_article_view&sysparm_article=KB0061597',
+};
 
 export default {
-  BYOI_FEATURE,
+  CUSTOM_IMAGE_GUIDES,
 };
