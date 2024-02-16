bb0a2439f19bafc410527fc5cbd8dfb56ba1f094
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2024-02-16 11:25:14 +0100

    feat(pci.k8s.reset): add network proxy form
    ref: TAPC-87
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/kubernetes/details/service/reset/reset.module.js
+++ packages/manager/modules/pci/src/projects/project/kubernetes/details/service/reset/reset.module.js
@@ -4,11 +4,12 @@ import component from './reset.component';
 import routing from './reset.routing';
 
 import networkForm from '../../../components/network-form';
+import proxyForm from '../../../components/proxy-form';
 
 const moduleName = 'ovhManagerPciProjectKubernetesServiceReset';
 
 angular
-  .module(moduleName, [networkForm])
+  .module(moduleName, [networkForm, proxyForm])
   .config(routing)
   .component('pciProjectKubernetesServiceReset', component)
   .run(/* @ngTranslationsInject:json ./translations */);
