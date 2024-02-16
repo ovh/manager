c305dcf800299179653b54cf8c7fc3bf714bff39
Nicolas Pierre-charles <nicolas@nicode.fr>
2024-02-16 18:09:40 +0100

    fix: simplify shell-client lib
    ref: MANAGER-13618
    
    Signed-off-by: Nicolas Pierre-charles <nicolas@nicode.fr>

--- packages/manager/core/shell-client/src/hooks/useNavigation.ts
+++
@@ -1,9 +0,0 @@
-import { useShell } from './useShell';
-
-export const useNavigation = () => {
-  const shell = useShell();
-
-  return shell.navigation;
-};
-
-export default useNavigation;
--- packages/manager/core/shell-client/src/hooks/useTracking.ts
+++
@@ -1,9 +0,0 @@
-import { useShell } from './useShell';
-
-export const useTracking = () => {
-  const shell = useShell();
-
-  return shell.tracking;
-};
-
-export default useTracking;
