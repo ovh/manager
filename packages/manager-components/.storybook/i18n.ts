9749f027255289464eacfdd38b2658290ddd8837
Florian Renaut <florian.renaut@corp.ovh.com>
2024-02-19 10:14:54 +0100

    feat(manager-components): add paginated-datagrid & notifications
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager-components/.storybook/i18n.ts
+++ packages/manager-components/.storybook/i18n.ts
@@ -2,7 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 i18n.use(initReactI18next).init({
-  fallbackLng: 'fr-FR',
+  fallbackLng: 'fr_FR',
   interpolation: {
     escapeValue: false,
   },
---
+++ packages/manager-components/src/components/notifications/notifications.spec.tsx
@@ -0,0 +1,47 @@
+import { useEffect } from 'react';
+import { render } from '@testing-library/react';
+import { useNotifications, NotificationType } from './useNotifications';
+import { Notifications } from './notifications.component';
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useLocation: () => ({
+    pathname: '/foo',
+  }),
+}));
+
+function AddNotification() {
+  const { addNotification } = useNotifications();
+  useEffect(() => {
+    addNotification('Notification-1', NotificationType.Success);
+    addNotification('Notification-2', NotificationType.Warning);
+  }, []);
+  return <></>;
+}
+
+function ClearNotifications() {
+  const { clearNotifications } = useNotifications();
+  useEffect(() => {
+    clearNotifications();
+  }, []);
+  return <></>;
+}
+
+describe('notifications component', () => {
+  it('should list notifications', async () => {
+    let { container } = render(<Notifications />);
+    expect(container.children.length).toBe(0);
+    render(<AddNotification />);
+    container = render(<Notifications />).container;
+    expect(container.children.length).toBe(2);
+    expect(container.children[0].innerHTML).toBe('Notification-1');
+    expect(container.children[1].innerHTML).toBe('Notification-2');
+  });
+  it('should clear notifications', async () => {
+    let { container } = render(<Notifications />);
+    expect(container.children.length).not.toBe(0);
+    render(<ClearNotifications />);
+    container = render(<Notifications />).container;
+    expect(container.children.length).toBe(0);
+  });
+});
