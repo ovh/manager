2fe72a169631b195076b89119473416b194953e1
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-02-20 16:18:08 +0100

    feat: update sidebar for databases category
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/apps/container/src/container/legacy/server-sidebar/universe/public-cloud/pci-menu.ts
+++ packages/manager/apps/container/src/container/legacy/server-sidebar/universe/public-cloud/pci-menu.ts
@@ -237,7 +237,7 @@ export function getPciProjectMenu(
           feature: 'databases',
           href: getURL(
             'public-cloud',
-            `#/pci/projects/${projectId}/storages/databases-analytics/databases`,
+            `#/pci/projects/${projectId}/databases-analytics/operational/services`,
           ),
         },
         {
