99236fc722adce9e5eee0dd7c1fc9bf3fa9f14e7
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-02-20 16:02:22 +0100

    feat: add legal mentions
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/apps/pci-databases-analytics/src/pages/services/[serviceId]/layout.tsx
+++ packages/manager/apps/pci-databases-analytics/src/pages/services/[serviceId]/layout.tsx
@@ -1,6 +1,8 @@
 import { Outlet, useParams } from 'react-router-dom';
 import { useGetService } from '@/hooks/api/services.api.hooks';
 import { Skeleton } from '@/components/ui/skeleton';
+import LegalMentions from '@/pages/_components/legalMentions';
+import { database } from '@/models/database';
 
 function ServiceName() {
   const { projectId, serviceId } = useParams();
@@ -17,11 +19,19 @@ export function breadcrumb() {
 }
 
 export default function ServiceLayout() {
+  const { projectId, serviceId } = useParams();
+  const serviceQuery = useGetService(projectId, serviceId);
   return (
     <>
       <p>Service</p>
       <ServiceName />
       <Outlet />
+      <LegalMentions
+        showRedisMessage={
+          serviceQuery.data?.engine === database.EngineEnum.redis
+        }
+        className="mt-4"
+      />
     </>
   );
 }
