e9c14f8348d0d7badf7ed5b44cdfa0cd9ee85d54
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-02-20 15:01:30 +0100

    feat: add services page
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/apps/pci-databases-analytics/src/components/ui/button.tsx
+++ packages/manager/apps/pci-databases-analytics/src/components/ui/button.tsx
@@ -20,8 +20,8 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
+        default: "h-10 px-4 py-2 text-base",
+        sm: "h-9 rounded-md px-3 text-sm",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
       },
