7afee8432370448857b5d671bd242d4fb3871610
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-02-20 16:02:22 +0100

    feat: add legal mentions
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

---
+++ packages/manager/apps/pci-databases-analytics/src/lib/engineNameHelper.ts
@@ -0,0 +1,32 @@
+import { database } from '@/models/database';
+
+export function humanizeEngine(engine: database.EngineEnum) {
+  switch (engine) {
+    case database.EngineEnum.cassandra:
+      return 'Cassandra';
+    case database.EngineEnum.grafana:
+      return 'Grafana';
+    case database.EngineEnum.kafka:
+      return 'Kafka';
+    case database.EngineEnum.kafkaConnect:
+      return 'Kafka Connect';
+    case database.EngineEnum.kafkaMirrorMaker:
+      return 'Kafka MirrorMaker';
+    case database.EngineEnum.m3aggregator:
+      return 'M3 Aggregator';
+    case database.EngineEnum.m3db:
+      return 'M3DB';
+    case database.EngineEnum.mongodb:
+      return 'MongoDB';
+    case database.EngineEnum.mysql:
+      return 'MySQL';
+    case database.EngineEnum.opensearch:
+      return 'OpenSearch';
+    case database.EngineEnum.postgresql:
+      return 'PostrgreSQL';
+    case database.EngineEnum.redis:
+      return 'Redis™*';
+    default:
+      return engine;
+  }
+}
