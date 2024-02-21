1608b7b78db5cbb39b747dad605be73bdfaa1188
benjaminlepine <benjamin.lepine@hotmail.fr>
2024-02-21 17:57:54 +0000

    feat(dedicated): add tracking on byoip slice and aggregate
    ref: MANAGER-12733
    
    Signed-off-by: benjaminlepine <benjamin.lepine@hotmail.fr>

---
+++ packages/manager/apps/dedicated/client/app/ip/components/list/byoip/constants.js
@@ -0,0 +1,3 @@
+export const TRACKING_PREFIX = 'dedicated::ip';
+export const TRACKING_PREFIX_SLICE = `${TRACKING_PREFIX}::slice-ip-block`;
+export const TRACKING_PREFIX_AGGREGATE = `${TRACKING_PREFIX}::merge-ip-block`;
