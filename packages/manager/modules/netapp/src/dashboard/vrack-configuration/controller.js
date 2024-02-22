dd64e9f8c531afa38b2067950669100782abe852
qpavy <contact@quentinpavy.com>
2024-02-22 14:34:30 +0100

    feat(dedicated.netapp): network config for efs with vrs associated (#11000)
    ref: MANAGER-12878
    
    Signed-off-by: Quentin Pavy <contact@quentinpavy.com>

---
+++ packages/manager/modules/netapp/src/dashboard/vrack-configuration/controller.js
@@ -0,0 +1,57 @@
+import { LABELS } from './constants';
+
+export default class OvhManagerNetappVrackConfigurationCtrl {
+  /* @ngInject */
+  constructor($translate, Alerter, NetappVrackConfigurationService) {
+    this.$translate = $translate;
+    this.Alerter = Alerter;
+    this.NetappVrackConfigurationService = NetappVrackConfigurationService;
+    this.LABELS = LABELS;
+  }
+
+  $onInit() {
+    if (!this.availableVracks.length) {
+      const noVrack = {
+        internalName: this.$translate.instant(
+          'netapp_vrack_configuration_no_vrack_field',
+        ),
+      };
+      this.availableVracks.push(noVrack);
+      this.selectedVrack = noVrack;
+      this.disableVrackField = true;
+    }
+  }
+
+  linkVrack() {
+    this.trackClick('confirm');
+
+    this.NetappVrackConfigurationService.linkVrackToVrackServices(
+      this.selectedVrack.internalName,
+      this.networkInformations.vRackServicesId,
+    )
+      .then(() => {
+        this.trackSuccess();
+        this.goBack().then(() => {
+          this.Alerter.success(
+            this.$translate.instant(
+              'netapp_vrack_configuration_success_message',
+            ),
+          );
+        });
+      })
+      .catch((error) => {
+        this.trackError();
+        this.goBack().then(() => {
+          this.Alerter.error(
+            this.$translate.instant(
+              'netapp_vrack_configuration_vrack_association_warning',
+              {
+                message: error?.data?.message || error.message,
+                requestId: error.headers('X-Ovh-Queryid'),
+              },
+            ),
+          );
+        });
+      });
+  }
+}
---
+++ packages/manager/modules/netapp/src/dashboard/vrack-configuration/service.js
@@ -0,0 +1,46 @@
+import { VRACK_ORDER_URLS } from './constants';
+
+export default class NetAppVrackConfigurationService {
+  /* @ngInject */
+  constructor($http, $q, Apiv2Service) {
+    this.$http = $http;
+    this.$q = $q;
+    this.Apiv2Service = Apiv2Service;
+    this.VRACK_ORDER_URLS = VRACK_ORDER_URLS;
+  }
+
+  filterAllowedVrack(vracks, vrackServicesId) {
+    const allowedServicesPromises = vracks.map((vrack) =>
+      this.getAllowedVrackServices(vrack.internalName).then((data) => ({
+        ...data,
+        vrack,
+      })),
+    );
+
+    return this.$q
+      .all(allowedServicesPromises)
+      .then((data) =>
+        data
+          .filter((vrackAllowedServices) =>
+            vrackAllowedServices.vrackServices.includes(vrackServicesId),
+          )
+          .map((availableVrack) => ({ ...availableVrack.vrack })),
+      );
+  }
+
+  getAllowedVrackServices(vrackId) {
+    return this.$http
+      .get(`/vrack/${vrackId}/allowedServices?serviceFamily=vrackServices`)
+      .then(({ data }) => data);
+  }
+
+  getVrackOrderUrl(subsidiary) {
+    return this.VRACK_ORDER_URLS[subsidiary] || this.VRACK_ORDER_URLS.DEFAULT;
+  }
+
+  linkVrackToVrackServices(vrackId, vrackServicesId) {
+    return this.$http.post(`/vrack/${vrackId}/vrackServices`, {
+      vrackServices: vrackServicesId,
+    });
+  }
+}
