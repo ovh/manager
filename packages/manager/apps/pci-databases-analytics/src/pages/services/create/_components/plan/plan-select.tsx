e224a8fcd02813c2f791d36223d45bc1f13ae3b3
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-02-22 18:12:13 +0100

    feat: improve funnel
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/apps/pci-databases-analytics/src/pages/services/create/_components/plan/plan-select.tsx
+++ packages/manager/apps/pci-databases-analytics/src/pages/services/create/_components/plan/plan-select.tsx
@@ -1,23 +1,30 @@
+import { Plan } from '@/models/order-funnel';
 import PlanTile from './plan-tile';
-import { AvailabilitiesHookOutput } from '@/hooks/useAvailabilities';
 
 interface PlansSelectProps {
-  model: AvailabilitiesHookOutput;
+  plans: Plan[];
+  value: string;
+  onChange: (newPlan: string) => void;
   showMonthlyPrice?: boolean;
 }
 
-const PlansSelect = ({ model, showMonthlyPrice = false }: PlansSelectProps) => {
+const PlansSelect = ({
+  plans,
+  value,
+  onChange,
+  showMonthlyPrice = false,
+}: PlansSelectProps) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2">
-      {model.listPlans
+      {plans
         .sort((a, b) => a.order - b.order)
         .map((plan) => (
           <PlanTile
             showMonthlyPrice={showMonthlyPrice}
             key={plan.name}
             plan={plan}
-            selected={model.plan === plan.name}
-            onChange={(value: string) => model.setPlan(value)}
+            selected={value === plan.name}
+            onChange={(newValue: string) => onChange(newValue)}
           />
         ))}
     </div>
--- packages/manager/apps/pci-databases-analytics/src/pages/services/create/_components/region/region-select.tsx
+++ packages/manager/apps/pci-databases-analytics/src/pages/services/create/_components/region/region-select.tsx
@@ -1,33 +1,41 @@
+import { useTranslation } from 'react-i18next';
 import { useState } from 'react';
 import RadioTile from '@/components/radio-tile';
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { AvailabilitiesHookOutput } from '@/hooks/useAvailabilities';
-
-const REGIONS_PER_CONTINENT = {
-  all: [] as string[],
-  'north-america': ['BHS'],
-  'central-europe': ['DE', 'SBG', 'WAW'],
-  'west-europe': ['GRA', 'UK'],
-};
+import { Region } from '@/models/order-funnel';
 
 interface RegionsSelectProps {
-  model: AvailabilitiesHookOutput;
+  regions: Region[];
+  value: string;
+  onChange: (newRegion: string) => void;
 }
-type Continent = 'all' | 'north-america' | 'central-europe' | 'west-europe';
-const RegionsSelect = ({ model }: RegionsSelectProps) => {
-  const [selectedContinent, setSelectectedContinent] = useState<Continent>(
-    'all',
-  );
+const RegionsSelect = ({ regions, value, onChange }: RegionsSelectProps) => {
+  const [selectedContinentIndex, setSelectectedContinentIndex] = useState(0);
+  const { t } = useTranslation('regions');
+  const mappedRegions = regions
+    .sort((a, b) => a.order - b.order)
+    .map((r) => ({
+      name: r.name,
+      // regionItem: r,
+      label: t(`region_${r.name}_micro`, { micro: r.name }),
+      continent: t(`region_continent_${r.name}`),
+    }));
+  const continents = [
+    ...new Set([
+      t(`region_continent_all`),
+      ...mappedRegions.map((mr) => mr.continent),
+    ]),
+  ];
   return (
     <Tabs
-      defaultValue="all"
-      onValueChange={(v) => setSelectectedContinent(v as Continent)}
+      defaultValue="0"
+      onValueChange={(v) => setSelectectedContinentIndex(+v)}
     >
       <TabsList className="flex bg-white justify-start p-0">
-        {Object.entries(REGIONS_PER_CONTINENT).map(([continent]) => (
+        {continents.map((continent, index) => (
           <TabsTrigger
             key={continent}
-            value={continent}
+            value={`${index}`}
             className="-mb-[1px] px-4 text-lg text-primary-600 font-semibold h-full bg-white border border-primary-100 rounded-t-md rounded-b-none data-[state=active]:bg-background data-[state=active]:shadow-none data-[state=active]:bg-primary-50 data-[state=active]:border-b-primary-50 data-[state=active]:font-bold data-[state=active]:text-primary-800"
           >
             {continent}
@@ -35,25 +43,22 @@ const RegionsSelect = ({ model }: RegionsSelectProps) => {
         ))}
       </TabsList>
       <div className="p-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2 bg-primary-50 border border-primary-100 rounded-b-md">
-        {model.listRegions
-          .sort((a, b) => a.order - b.order)
+        {mappedRegions
           .filter((r) =>
-            selectedContinent === 'all'
+            selectedContinentIndex === 0
               ? true
-              : REGIONS_PER_CONTINENT[selectedContinent].includes(r.name),
+              : r.continent === continents[selectedContinentIndex],
           )
           .map((region) => (
             <RadioTile
               name="region-select"
               key={region.name}
-              onChange={() => model.setRegion(region.name)}
+              onChange={() => onChange(region.name)}
               value={region.name}
-              checked={region.name === model.region}
+              checked={region.name === value}
             >
-              <span
-                className={`${region.name === model.region ? 'font-bold' : ''}`}
-              >
-                {region.name}
+              <span className={`${region.name === value ? 'font-bold' : ''}`}>
+                {region.label}
               </span>
             </RadioTile>
           ))}
