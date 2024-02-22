9d5fc419e23f091cfac2853c1efa70372d2936e3
JacquesLarique <134954692+JacquesLarique@users.noreply.github.com>
2024-02-22 15:34:31 +0100

    feat(dedicated.account): enable fields while kyc is not validated (#10844)
    ref: MANAGER-12949
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/account/account.routing.js
+++ packages/manager/apps/dedicated/client/app/account/account.routing.js
@@ -3,6 +3,24 @@ export default /* @ngInject */ ($stateProvider) => {
     {
       abstract: true,
       name: 'app.account',
+      resolve: {
+        isKycFeatureAvailable: /* @ngInject */ ($http) => {
+          return $http
+            .get(`/feature/identity-documents/availability`, {
+              serviceType: 'aapi',
+            })
+            .then(
+              ({ data: featureAvailability }) =>
+                featureAvailability['identity-documents'],
+            );
+        },
+        kycStatus: /* @ngInject */ ($http, isKycFeatureAvailable) => {
+          if (isKycFeatureAvailable) {
+            return $http.get(`/me/procedure/identity`).then(({ data }) => data);
+          }
+          return false;
+        },
+      },
     },
     {
       abstract: true,
