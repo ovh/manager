b699d456f5c270f8b7436b5f78aac02f966ece8e
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-02-23 17:22:44 +0100

    feat: add zod and hook form to order funnel, draft network
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/apps/pci-databases-analytics/src/pages/services/create/_components/region/region-select.tsx
+++ packages/manager/apps/pci-databases-analytics/src/pages/services/create/_components/region/region-select.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import RadioTile from '@/components/radio-tile';
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Region } from '@/models/order-funnel';
@@ -9,62 +9,69 @@ interface RegionsSelectProps {
   value: string;
   onChange: (newRegion: string) => void;
 }
-const RegionsSelect = ({ regions, value, onChange }: RegionsSelectProps) => {
-  const [selectedContinentIndex, setSelectectedContinentIndex] = useState(0);
-  const { t } = useTranslation('regions');
-  const mappedRegions = regions
-    .sort((a, b) => a.order - b.order)
-    .map((r) => ({
-      name: r.name,
-      // regionItem: r,
-      label: t(`region_${r.name}_micro`, { micro: r.name }),
-      continent: t(`region_continent_${r.name}`),
-    }));
-  const continents = [
-    ...new Set([
-      t(`region_continent_all`),
-      ...mappedRegions.map((mr) => mr.continent),
-    ]),
-  ];
-  return (
-    <Tabs
-      defaultValue="0"
-      onValueChange={(v) => setSelectectedContinentIndex(+v)}
-    >
-      <TabsList className="flex bg-white justify-start p-0">
-        {continents.map((continent, index) => (
-          <TabsTrigger
-            key={continent}
-            value={`${index}`}
-            className="-mb-[1px] px-4 text-lg text-primary-600 font-semibold h-full bg-white border border-primary-100 rounded-t-md rounded-b-none data-[state=active]:bg-background data-[state=active]:shadow-none data-[state=active]:bg-primary-50 data-[state=active]:border-b-primary-50 data-[state=active]:font-bold data-[state=active]:text-primary-800"
-          >
-            {continent}
-          </TabsTrigger>
-        ))}
-      </TabsList>
-      <div className="p-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2 bg-primary-50 border border-primary-100 rounded-b-md">
-        {mappedRegions
-          .filter((r) =>
-            selectedContinentIndex === 0
-              ? true
-              : r.continent === continents[selectedContinentIndex],
-          )
-          .map((region) => (
-            <RadioTile
-              name="region-select"
-              key={region.name}
-              onChange={() => onChange(region.name)}
-              value={region.name}
-              checked={region.name === value}
-            >
-              <span className={`${region.name === value ? 'font-bold' : ''}`}>
-                {region.label}
-              </span>
-            </RadioTile>
-          ))}
+const RegionsSelect = React.forwardRef<HTMLInputElement, RegionsSelectProps>(
+  ({ regions, value, onChange }, ref) => {
+    const [selectedContinentIndex, setSelectectedContinentIndex] = useState(0);
+    const { t } = useTranslation('regions');
+    const mappedRegions = regions
+      .sort((a, b) => a.order - b.order)
+      .map((r) => ({
+        name: r.name,
+        // regionItem: r,
+        label: t(`region_${r.name}_micro`, { micro: r.name }),
+        continent: t(`region_continent_${r.name}`),
+      }));
+    const continents = [
+      ...new Set([
+        t(`region_continent_all`),
+        ...mappedRegions.map((mr) => mr.continent),
+      ]),
+    ];
+    return (
+      <div ref={ref}>
+        <Tabs
+          defaultValue="0"
+          onValueChange={(v) => setSelectectedContinentIndex(+v)}
+        >
+          <TabsList className="flex bg-white justify-start p-0">
+            {continents.map((continent, index) => (
+              <TabsTrigger
+                key={continent}
+                value={`${index}`}
+                className="-mb-[1px] px-4 text-lg text-primary-600 font-semibold h-full bg-white border border-primary-100 rounded-t-md rounded-b-none data-[state=active]:bg-background data-[state=active]:shadow-none data-[state=active]:bg-primary-50 data-[state=active]:border-b-primary-50 data-[state=active]:font-bold data-[state=active]:text-primary-800"
+              >
+                {continent}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+          <div className="p-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2 bg-primary-50 border border-primary-100 rounded-b-md">
+            {mappedRegions
+              .filter((r) =>
+                selectedContinentIndex === 0
+                  ? true
+                  : r.continent === continents[selectedContinentIndex],
+              )
+              .map((region) => (
+                <RadioTile
+                  name="region-select"
+                  key={region.name}
+                  onChange={() => onChange(region.name)}
+                  value={region.name}
+                  checked={region.name === value}
+                >
+                  <span
+                    className={`${region.name === value ? 'font-bold' : ''}`}
+                  >
+                    {region.label}
+                  </span>
+                </RadioTile>
+              ))}
+          </div>
+        </Tabs>
       </div>
-    </Tabs>
-  );
-};
+    );
+  },
+);
 
+RegionsSelect.displayName = 'RegionsSelect';
 export default RegionsSelect;
