565d2e89ff9435aa25c02c689375ce3a1a517391
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-02-26 15:22:54 +0100

    feat: add mutation for order funnel
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/apps/pci-databases-analytics/src/hooks/api/services.api.hooks.tsx
+++ packages/manager/apps/pci-databases-analytics/src/hooks/api/services.api.hooks.tsx
@@ -2,9 +2,11 @@ import {
   QueryObserverOptions,
   UseQueryResult,
   useQuery,
+  useMutation,
 } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
 import { database } from '@/models/database';
-import { getService, getServices } from '@/api/databases/service';
+import { addService, getService, getServices } from '@/api/databases/service';
 
 export function useGetService(
   projectId: string,
@@ -29,4 +31,30 @@ export function useGetServices(
     queryFn: () => getServices(projectId),
     ...options,
   }) as UseQueryResult<database.Service[], Error>;
+}
+
+interface AddServiceProps {
+  onError: (cause: Error) => void;
+  onSuccess: (service: database.Service) => void;
+}
+export interface ServiceCreationWithEngine extends database.ServiceCreation {
+  engine: database.EngineEnum;
+}
+export function useAddService({ onError, onSuccess }: AddServiceProps) {
+  const { projectId } = useParams();
+  const mutation = useMutation({
+    mutationFn: (serviceAndEngine: ServiceCreationWithEngine) => {
+      const { engine, ...serviceInfo } = serviceAndEngine;
+      return addService(projectId, engine, serviceInfo);
+    },
+    onError,
+    onSuccess,
+  });
+
+  return {
+    addService: (serviceAndEngine: ServiceCreationWithEngine) => {
+      return mutation.mutate(serviceAndEngine);
+    },
+    ...mutation,
+  };
 }
