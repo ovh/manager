e84a5154e55bf5013ba473bd057195d9a8999992
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-02-27 18:48:39 +0100

    feat: add polyfill for plurals on i18n, translate order funnel
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/apps/pci-databases-analytics/src/components/ui/button.tsx
+++ packages/manager/apps/pci-databases-analytics/src/components/ui/button.tsx
@@ -24,6 +24,7 @@ const buttonVariants = cva(
         sm: "h-9 rounded-md px-3 text-sm",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
+        link: "text-base"
       },
     },
     defaultVariants: {
