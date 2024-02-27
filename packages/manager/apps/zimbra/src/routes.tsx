55c492f7650560301c8a558bff1fe7502fc707ca
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2024-02-27 17:12:32 +0100

    fix(zimbra): customisation of generate application
    ref: MANAGER-13039
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/apps/zimbra/src/routes.tsx
+++ packages/manager/apps/zimbra/src/routes.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import i18next from 'i18next';
 import NotFound from './pages/404';
 
 const lazyRouteConfig = (importFn: CallableFunction) => {
@@ -14,28 +15,63 @@ const lazyRouteConfig = (importFn: CallableFunction) => {
   };
 };
 
-export const COMMON_PATH = '/zimbra';
-
 export default [
   {
     path: '/',
+    handle: { breadcrumb: (): string => 'Zimbra' },
     ...lazyRouteConfig(() => import('@/pages/layout')),
     children: [
-      {
-        path: 'zimbra',
-        ...lazyRouteConfig(() => import('@/pages/listing')),
-      },
       {
         path: 'zimbra/:serviceName',
         ...lazyRouteConfig(() => import('@/pages/dashboard/_layout')),
         children: [
           {
             path: '',
-            ...lazyRouteConfig(() => import('@/pages/dashboard/Tabs1')),
+            ...lazyRouteConfig(() =>
+              import('@/pages/dashboard/GeneralInformation'),
+            ),
+            handle: {
+              breadcrumb: (): null => null,
+            },
           },
           {
-            path: 'Tabs2',
-            ...lazyRouteConfig(() => import('@/pages/dashboard/Tabs2')),
+            path: 'organizations',
+            ...lazyRouteConfig(() => import('@/pages/dashboard/Organizations')),
+            handle: {
+              breadcrumb: (): string =>
+                i18next.t('zimbra/dashboard:organization'),
+            },
+          },
+          {
+            path: 'domains',
+            ...lazyRouteConfig(() => import('@/pages/dashboard/Domains')),
+            handle: {
+              breadcrumb: (): string => i18next.t('zimbra/dashboard:domain'),
+            },
+          },
+          {
+            path: 'email-accounts',
+            ...lazyRouteConfig(() => import('@/pages/dashboard/EmailAccounts')),
+            handle: {
+              breadcrumb: (): string =>
+                i18next.t('zimbra/dashboard:email_accounts'),
+            },
+          },
+          {
+            path: 'mailing-lists',
+            ...lazyRouteConfig(() => import('@/pages/dashboard/MailingLists')),
+            handle: {
+              breadcrumb: (): string =>
+                i18next.t('zimbra/dashboard:mailing_list'),
+            },
+          },
+          {
+            path: 'redirections',
+            ...lazyRouteConfig(() => import('@/pages/dashboard/Redirections')),
+            handle: {
+              breadcrumb: (): string =>
+                i18next.t('zimbra/dashboard:redirections'),
+            },
           },
         ],
       },
