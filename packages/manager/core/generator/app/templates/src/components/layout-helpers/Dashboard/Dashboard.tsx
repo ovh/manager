bf79f0234d94ce3356b57f4eb6ff9538005ff64d
Nicolas Pierre-charles <nicolas@nicode.fr>
2024-02-28 18:37:01 +0100

    feat(generator): improvements and fixes
    ref: MANAGER-13618
    
    Signed-off-by: Nicolas Pierre-charles <nicolas@nicode.fr>

--- packages/manager/core/generator/app/templates/src/components/layout-helpers/Dashboard/Dashboard.tsx
+++ packages/manager/core/generator/app/templates/src/components/layout-helpers/Dashboard/Dashboard.tsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Outlet, NavLink, useLocation, useNavigate } from 'react-router-dom';
-import { OsdsText } from '@ovhcloud/ods-components/text/react';
 import {
+  OsdsText,
   OsdsTabs,
   OsdsTabBar,
   OsdsTabBarItem,
-} from '@ovhcloud/ods-components/tabs/react';
-import { ODS_TEXT_LEVEL, ODS_TEXT_SIZE } from '@ovhcloud/ods-components/text';
+} from '@ovhcloud/ods-components/react';
+import { ODS_TEXT_LEVEL, ODS_TEXT_SIZE } from '@ovhcloud/ods-components';
 import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
 
 export type DashboardTabItemProps = {
