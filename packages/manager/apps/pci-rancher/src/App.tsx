0b0124cacc021eea1d0b6c68fa87fe6e8b819759
Nicolas Pierre-charles <nicolas@nicode.fr>
2024-02-28 18:31:35 +0100

    fix(rancher): use ShellContext instead of deprecated hooks
    ref: MANAGER-13618
    
    Signed-off-by: Nicolas Pierre-charles <nicolas@nicode.fr>

--- packages/manager/apps/pci-rancher/src/App.tsx
+++ packages/manager/apps/pci-rancher/src/App.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { odsSetup } from '@ovhcloud/ods-common-core';
-import { useShell } from '@ovh-ux/manager-react-shell-client';
+import { ShellContext } from '@ovh-ux/manager-react-shell-client';
 import '@ovhcloud/ods-theme-blue-jeans';
 import queryClient from './query.client';
 import Router from './Router';
@@ -10,7 +10,7 @@ import Router from './Router';
 odsSetup();
 
 function App() {
-  const shell = useShell();
+  const { shell } = useContext(ShellContext);
   useEffect(() => {
     shell.ux.hidePreloader();
   }, []);
