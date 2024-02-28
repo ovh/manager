03a11c4c7ccfde3eea72c483daa06b83bd267c4b
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-02-28 17:08:50 +0100

    feat: translate order summary and bytes, add ovhlink component
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/apps/pci-databases-analytics/src/lib/bytesHelper.ts
+++ packages/manager/apps/pci-databases-analytics/src/lib/bytesHelper.ts
@@ -1,3 +1,5 @@
+import i18next from 'i18next';
+
 export interface Storage {
   unit: string;
   value: number;
@@ -17,7 +19,51 @@ export function compareStorage(a: Storage, b: Storage) {
   return a.value - b.value;
 }
 
-export function formatStorage(storage: Storage, decimals = 2) {
+export function addStorage(a: Storage, b: Storage): Storage {
+  // Convert both storages to the unit with the highest index
+  const maxUnitIndex = Math.max(units.indexOf(a.unit), units.indexOf(b.unit));
+  const aValueInMaxUnit =
+    a.value * 1024 ** (maxUnitIndex - units.indexOf(a.unit));
+  const bValueInMaxUnit =
+    b.value * 1024 ** (maxUnitIndex - units.indexOf(b.unit));
+
+  // Perform addition
+  const sumValue = aValueInMaxUnit + bValueInMaxUnit;
+
+  // Find the appropriate unit
+  const unit = units[maxUnitIndex];
+
+  return {
+    unit,
+    value: sumValue,
+  };
+}
+
+export function subtractStorage(a: Storage, b: Storage): Storage {
+  // Convert both storages to the unit with the highest index
+  const maxUnitIndex = Math.max(units.indexOf(a.unit), units.indexOf(b.unit));
+  const aValueInMaxUnit =
+    a.value * 1024 ** (maxUnitIndex - units.indexOf(a.unit));
+  const bValueInMaxUnit =
+    b.value * 1024 ** (maxUnitIndex - units.indexOf(b.unit));
+
+  // Perform subtraction
+  const diffValue = aValueInMaxUnit - bValueInMaxUnit;
+
+  // Find the appropriate unit
+  const unit = units[maxUnitIndex];
+
+  return {
+    unit,
+    value: diffValue,
+  };
+}
+
+export function formatStorage(
+  storage: Storage,
+  decimals = 2,
+  useShortUnit = true,
+) {
   let size = storage.value;
   let unitIndex = units.indexOf(storage.unit);
 
@@ -26,5 +72,10 @@ export function formatStorage(storage: Storage, decimals = 2) {
     unitIndex += 1;
   }
 
-  return `${Number(Number(size).toFixed(decimals))} ${units[unitIndex]}`;
+  const unit = i18next.t(
+    `unit${useShortUnit ? 'Short' : 'Long'}-${units[unitIndex]}`,
+    { ns: 'bytes', count: size },
+  );
+
+  return `${Number(Number(size).toFixed(decimals))} ${unit}`;
 }
