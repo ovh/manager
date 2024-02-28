24a1ed732542a1369f7ac3f99782fbba5b8da9ef
Yoann Fievez <yoann.fievez@ovhcloud.com>
2024-02-28 15:12:48 +0100

    feat(pci-users): add unit tests for regenerate password action ref: DTCORE-1871
    Signed-off-by: Yoann Fievez <yoann.fievez@ovhcloud.com>

---
+++ packages/manager/apps/pci-users/src/__tests__/components/RegeneratePasswordAction.spec.tsx
@@ -0,0 +1,90 @@
+import 'element-internals-polyfill';
+import { describe, expect, vi } from 'vitest';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { act, fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import queryClient from '@/queryClient';
+import { useRegeneratePassword } from '@/hooks/useUser';
+import RegeneratePasswordAction from '@/components/users/listing/RegeneratePasswordAction';
+import { User } from '@/interface';
+
+vi.mock('@ovh-ux/manager-react-shell-client', async () => ({
+  useEnvironment: () => ({
+    user: {},
+  }),
+}));
+
+vi.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translation hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: (str: string) => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
+  initReactI18next: {
+    type: '3rdParty',
+    init: () => {},
+  },
+}));
+
+vi.mock('react-router-dom', () => ({
+  useNavigate: () => {
+    return {
+      navigate: () => {},
+    };
+  },
+  useParams: () => {
+    return {
+      projectId: 'foo',
+    };
+  },
+}));
+
+vi.mock('@/hooks/useUser', () => {
+  const regenerate = vi.fn(() => {});
+  return {
+    useRegeneratePassword: () => ({
+      regenerate,
+    }),
+    useUser: () => ({}),
+  };
+});
+
+function renderModal() {
+  const user: User = {
+    username: 'toto',
+    id: 1234567,
+    creationDate: '2024-01-01',
+    password: 'password',
+    status: 'OK',
+    description: '',
+  };
+  render(
+    <QueryClientProvider client={queryClient}>
+      <RegeneratePasswordAction user={user} />,
+    </QueryClientProvider>,
+  );
+}
+
+describe('Regenerate Password', () => {
+  it('should regenerate password of user with button', async () => {
+    const useRegenerate = useRegeneratePassword({
+      projectId: 'foo',
+      userId: 'bar',
+      onSuccess: () => {},
+      onError: () => {},
+    });
+    renderModal();
+    const regeneratePasswordButton = screen.getByTestId(
+      'regeneratePasswordButton',
+    );
+    expect(useRegenerate.regenerate).not.toHaveBeenCalled();
+    act(() => {
+      fireEvent.click(regeneratePasswordButton);
+    });
+    expect(useRegenerate.regenerate).toHaveBeenCalled();
+  });
+});
