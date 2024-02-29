041212225b417ae0daa4f15e9ef22df1efc27a09
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2024-02-29 13:13:41 +0100

    fix(pci.k8s): add network subnet documentation links
    ref: TAPC-256
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/kubernetes/components/network-form/network-form.constants.js
+++ packages/manager/modules/pci/src/projects/project/kubernetes/components/network-form/network-form.constants.js
@@ -2,7 +2,51 @@ export const GATEWAY_IP_REGEX = /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(\.(?!$)|$)){4
 
 export const SUBNET_DOC = {
   DEFAULT:
-    'https://help.ovhcloud.com/csm/en-ie-public-cloud-kubernetes-known-limits?id=kb_article_view&sysparm_article=KB0049871#private-networks',
+    'https://help.ovhcloud.com/csm/en-public-cloud-kubernetes-known-limits?id=kb_article_view&sysparm_article=KB0049873',
+  ASIA:
+    'https://help.ovhcloud.com/csm/asia-public-cloud-kubernetes-known-limits?id=kb_article_view&sysparm_article=KB0037444',
+  AU:
+    'https://help.ovhcloud.com/csm/en-au-public-cloud-kubernetes-known-limits?id=kb_article_view&sysparm_article=KB0049877',
+  CA:
+    'https://help.ovhcloud.com/csm/en-ca-public-cloud-kubernetes-known-limits?id=kb_article_view&sysparm_article=KB0049868',
+  DE:
+    'https://help.ovhcloud.com/csm/de-public-cloud-kubernetes-known-limits?id=kb_article_view&sysparm_article=KB0055207',
+  ES:
+    'https://help.ovhcloud.com/csm/es-es-public-cloud-kubernetes-known-limits?id=kb_article_view&sysparm_article=KB0055208',
+  EU:
+    'https://help.ovhcloud.com/csm/en-ie-public-cloud-kubernetes-known-limits?id=kb_article_view&sysparm_article=KB0049871',
+  FR:
+    'https://help.ovhcloud.com/csm/fr-public-cloud-kubernetes-known-limits?id=kb_article_view&sysparm_article=KB0055211',
+  GB:
+    'https://help.ovhcloud.com/csm/en-gb-public-cloud-kubernetes-known-limits?id=kb_article_view&sysparm_article=KB0049884',
+  IE:
+    'https://help.ovhcloud.com/csm/en-ie-public-cloud-kubernetes-known-limits?id=kb_article_view&sysparm_article=KB0049871',
+  IN:
+    'https://help.ovhcloud.com/csm/asia-public-cloud-kubernetes-known-limits?id=kb_article_view&sysparm_article=KB0037444',
+  IT:
+    'https://help.ovhcloud.com/csm/it-public-cloud-kubernetes-known-limits?id=kb_article_view&sysparm_article=KB0055212',
+  MA:
+    'https://help.ovhcloud.com/csm/fr-public-cloud-kubernetes-known-limits?id=kb_article_view&sysparm_article=KB0055211',
+  NL:
+    'https://help.ovhcloud.com/csm/en-nl-documentation-public-cloud?id=kb_browse_cat&kb_id=574a8325551974502d4c6e78b7421938',
+  PL:
+    'https://help.ovhcloud.com/csm/pl-public-cloud-kubernetes-known-limits?id=kb_article_view&sysparm_article=KB0055213',
+  PT:
+    'https://help.ovhcloud.com/csm/pt-public-cloud-kubernetes-known-limits?id=kb_article_view&sysparm_article=KB0055215',
+  QC:
+    'https://help.ovhcloud.com/csm/fr-ca-public-cloud-kubernetes-known-limits?id=kb_article_view&sysparm_article=KB0055209',
+  SG:
+    'https://help.ovhcloud.com/csm/en-sg-public-cloud-kubernetes-known-limits?id=kb_article_view&sysparm_article=KB0049872',
+  SN:
+    'https://help.ovhcloud.com/csm/fr-public-cloud-kubernetes-known-limits?id=kb_article_view&sysparm_article=KB0055211',
+  TN:
+    'https://help.ovhcloud.com/csm/fr-public-cloud-kubernetes-known-limits?id=kb_article_view&sysparm_article=KB0055211',
+  US:
+    'https://support.us.ovhcloud.com/hc/en-us/articles/1500005048361-Managed-Kubernetes-Known-Limits',
+  WE:
+    'https://help.ovhcloud.com/csm/en-public-cloud-kubernetes-known-limits?id=kb_article_view&sysparm_article=KB0049873',
+  WS:
+    'https://help.ovhcloud.com/csm/es-public-cloud-kubernetes-known-limits?id=kb_article_view&sysparm_article=KB0055210',
 };
 
 export default {
