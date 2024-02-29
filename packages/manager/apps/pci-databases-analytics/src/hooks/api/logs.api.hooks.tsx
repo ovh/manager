0515c0a4c6d4afa094c4835d1b7ec18d84e61ad6
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-02-29 16:18:28 +0100

    feat: add metrics and logs pages
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

---
+++ packages/manager/apps/pci-databases-analytics/src/hooks/api/logs.api.hooks.tsx
@@ -0,0 +1,21 @@
+import {
+  QueryObserverOptions,
+  UseQueryResult,
+  useQuery,
+} from '@tanstack/react-query';
+import { database } from '@/models/database';
+import { getServiceLogs } from '@/api/databases/logs';
+
+export function useGetServiceLogs(
+  projectId: string,
+  engine: database.EngineEnum,
+  serviceId: string,
+  options: Omit<QueryObserverOptions, 'queryKey'> = {},
+) {
+  const queryKey = [projectId, 'database', engine, serviceId, 'logs'];
+  return useQuery({
+    queryKey,
+    queryFn: () => getServiceLogs(projectId, engine, serviceId),
+    ...options,
+  }) as UseQueryResult<database.service.LogEntry[], Error>;
+}
