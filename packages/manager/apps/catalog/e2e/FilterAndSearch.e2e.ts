ac6b62fdcf68a525f111b2c75bd0bf7f13a73253
JacquesLarique <134954692+JacquesLarique@users.noreply.github.com>
2024-02-29 19:04:55 +0100

    Revert "feat(catalog): update ods components lib version to 17.1.0 (#10666)" (#11060)
    This reverts commit 5cb5757166847add97b3d515ac22e7be88eeaddf.

--- packages/manager/apps/catalog/e2e/FilterAndSearch.e2e.ts
+++ packages/manager/apps/catalog/e2e/FilterAndSearch.e2e.ts
@@ -1,8 +1,23 @@
 import { test, expect, Page, ElementHandle } from '@playwright/test';
 import '@playwright-helpers/login';
 import * as translation from '../src/public/translations/catalog/Messages_en_GB.json';
-import * as filterTranslation from '../src/public/translations/catalog/filters/Messages_en_GB.json';
-import * as searchTranslation from '../src/public/translations/catalog/search/Messages_en_GB.json';
+
+const clickButtonByName = async (page: Page, name: string): Promise<void> => {
+  const buttons = await page.$$('osds-button');
+
+  const targetButton = await Promise.all(
+    buttons.map(async (button: ElementHandle) => {
+      const buttonName = await button.evaluate((el: Element) =>
+        el.getAttribute('name'),
+      );
+      return buttonName === name ? button : null;
+    }),
+  ).then((results) => results.find(Boolean));
+
+  if (targetButton) {
+    await targetButton.click();
+  }
+};
 
 const clickCheckboxByName = async (page: Page, name: string): Promise<void> => {
   const checkboxes = await page.$$('osds-checkbox');
@@ -19,7 +34,6 @@ const clickCheckboxByName = async (page: Page, name: string): Promise<void> => {
   if (targetCheckbox) {
     await targetCheckbox.click();
   } else {
-    // eslint-disable-next-line no-console
     console.warn(`Checkbox with name "${name}" not found.`);
   }
 };
@@ -31,9 +45,9 @@ const waitForProducts = async (
   await page.waitForFunction(
     ({ selector, expectedCount }) =>
       document.querySelectorAll(selector).length === expectedCount,
-    { selector: 'osds-tile', expectedCount: expectedProductCount },
+    { selector: 'card', expectedCount: expectedProductCount },
   );
-  return page.$$('osds-tile');
+  return page.$$('');
 };
 
 const validateProductCategories = async (
@@ -41,10 +55,13 @@ const validateProductCategories = async (
   expectedCategories: string[],
 ): Promise<void> => {
   const checks = products.map(async (product, index) => {
-    const tileTypeElement = await product.$$('osds-text');
-    expect(await tileTypeElement[0].textContent()).toBe(
-      expectedCategories[index],
-    );
+    const tileTypeElement = await product.$('.tile-type');
+    expect(tileTypeElement).toBeTruthy();
+
+    const tileTypeText = await tileTypeElement?.textContent();
+    expect(tileTypeText?.trim()).toBe(expectedCategories[index]);
+
+    expect(await product.$('.tile-title')).toBeTruthy();
   });
 
   await Promise.all(checks);
@@ -53,58 +70,46 @@ const validateProductCategories = async (
 test('should filter results based on Bare Metal Cloud universe', async ({
   page,
 }) => {
-  await page
-    .getByText(searchTranslation.manager_catalog_search_filter_button)
-    .click();
-  await clickCheckboxByName(page, 'checkbox-universe-Bare_Metal_Cloud');
-  await page
-    .getByText(filterTranslation.manager_catalog_filters_button_apply)
-    .click();
+  await clickButtonByName(page, 'filterButton');
+  await clickCheckboxByName(page, 'checkbox-universe-BareMetalCloud');
+  await clickButtonByName(page, 'applyFilterButton');
+
   const products = await waitForProducts(page, 5);
   await validateProductCategories(products, [
     'Dedicated Servers',
     'Virtual Private Servers',
     'Managed Bare Metal',
-    'Storage and Backup',
-    'Storage and Backup',
+    'Storage and Backup (BMC)',
+    'Storage and Backup (BMC)',
   ]);
 });
 
 test('should clear all filters when Clear All button is clicked', async ({
   page,
 }) => {
-  await page
-    .getByText(searchTranslation.manager_catalog_search_filter_button)
-    .click();
-  await clickCheckboxByName(page, 'checkbox-universe-Bare_Metal_Cloud');
-  await page
-    .getByText(filterTranslation.manager_catalog_filters_button_apply)
-    .click();
+  await clickButtonByName(page, 'filterButton');
+  await clickCheckboxByName(page, 'checkbox-universe-BareMetalCloud');
+  await clickButtonByName(page, 'applyFilterButton');
 
   await waitForProducts(page, 5);
-  await page
-    .getByText(searchTranslation.manager_catalog_search_filter_button)
-    .click();
+
+  await clickButtonByName(page, 'filterButton');
 
   const links = await page.$$('osds-link');
   const resetFilterLink = links[0];
   if (resetFilterLink) await resetFilterLink.click();
 
-  await waitForProducts(page, 41);
+  await waitForProducts(page, 40);
 });
 
 // Scenario: No results after filtering
 test('should show "No results found" when filters match no products', async ({
   page,
 }) => {
-  await page
-    .getByText(searchTranslation.manager_catalog_search_filter_button)
-    .click();
-  await clickCheckboxByName(page, 'checkbox-universe-Bare_Metal_Cloud');
-  await clickCheckboxByName(page, 'checkbox-category-AI_&_machine_learning');
-  await page
-    .getByText(filterTranslation.manager_catalog_filters_button_apply)
-    .click();
+  await clickButtonByName(page, 'filterButton');
+  await clickCheckboxByName(page, 'checkbox-universe-BareMetalCloud');
+  await clickCheckboxByName(page, 'checkbox-category-AI&machinelearning');
+  await clickButtonByName(page, 'applyFilterButton');
 
   await waitForProducts(page, 0);
 
@@ -115,13 +120,9 @@ test('should show "No results found" when filters match no products', async ({
 });
 
 test('should display chips when filter is enabled', async ({ page }) => {
-  await page
-    .getByText(searchTranslation.manager_catalog_search_filter_button)
-    .click();
-  await clickCheckboxByName(page, 'checkbox-universe-Bare_Metal_Cloud');
-  await page
-    .getByText(filterTranslation.manager_catalog_filters_button_apply)
-    .click();
+  await clickButtonByName(page, 'filterButton');
+  await clickCheckboxByName(page, 'checkbox-universe-BareMetalCloud');
+  await clickButtonByName(page, 'applyFilterButton');
 
   await waitForProducts(page, 5);
   const elements = await page.$$('osds-chip');
@@ -130,18 +131,15 @@ test('should display chips when filter is enabled', async ({ page }) => {
 });
 
 test('should clear all filters when chip is remove', async ({ page }) => {
-  await page
-    .getByText(searchTranslation.manager_catalog_search_filter_button)
-    .click();
-  await clickCheckboxByName(page, 'checkbox-universe-Bare_Metal_Cloud');
-  await page
-    .getByText(filterTranslation.manager_catalog_filters_button_apply)
-    .click();
+  await clickButtonByName(page, 'filterButton');
+  await clickCheckboxByName(page, 'checkbox-universe-BareMetalCloud');
+  await clickButtonByName(page, 'applyFilterButton');
 
   await waitForProducts(page, 5);
 
-  const element = page.locator('osds-chip osds-icon');
-  if (element) await element.click();
+  const elements = await page.$$('osds-chip');
+  const chipBareMetalCloud = elements[0];
+  if (chipBareMetalCloud) await chipBareMetalCloud.click();
 
-  await waitForProducts(page, 41);
+  await waitForProducts(page, 40);
 });
--- packages/manager/apps/catalog/src/components/SearchBar/SearchBar.tsx
+++ packages/manager/apps/catalog/src/components/SearchBar/SearchBar.tsx
@@ -1,21 +1,16 @@
-import React, { useEffect, useState, KeyboardEvent } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import {
-  OsdsSearchBar,
-  OsdsButton,
-  OsdsIcon,
-} from '@ovhcloud/ods-components/react';
+import { OsdsSearchBar } from '@ovhcloud/ods-components/search-bar/react';
+import { OsdsButton } from '@ovhcloud/ods-components/button/react';
+import { OsdsIcon } from '@ovhcloud/ods-components/icon/react';
 import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
 import { useTranslation } from 'react-i18next';
+import { ODS_ICON_NAME, ODS_ICON_SIZE } from '@ovhcloud/ods-components/icon';
 import {
-  ODS_ICON_NAME,
-  ODS_ICON_SIZE,
   ODS_BUTTON_VARIANT,
   ODS_BUTTON_SIZE,
   ODS_BUTTON_TYPE,
-  OsdsSearchBarCustomEvent,
-  OdsInputValueChangeEventDetail,
-} from '@ovhcloud/ods-components';
+} from '@ovhcloud/ods-components/button';
 import Filters from '@/components/Filters/Filters';
 import { Product } from '@/api';
 import FilterChip from '../Filters/FilterChip';
@@ -59,9 +54,7 @@ const SearchBar: React.FC<SearchbarProps> = ({
     setSearchValue(event.detail.inputValue.trim());
   };
 
-  const onSearchChanged = (
-    event: OsdsSearchBarCustomEvent<OdsInputValueChangeEventDetail>,
-  ) => {
+  const onSearchChanged = (event: CustomEvent<{ value: string }>) => {
     setLocalSearchValue(event.detail.value);
   };
 
@@ -91,11 +84,12 @@ const SearchBar: React.FC<SearchbarProps> = ({
         </span>
         <span>
           <OsdsButton
+            name="filterButton"
             size={ODS_BUTTON_SIZE.sm}
             type={ODS_BUTTON_TYPE.button}
             variant={ODS_BUTTON_VARIANT.stroked}
             color={ODS_THEME_COLOR_INTENT.primary}
-            onKeyDown={(event: KeyboardEvent) =>
+            onKeyDown={(event: any) =>
               event.key === 'Enter' &&
               setShowFilters((filterState) => !filterState)
             }
