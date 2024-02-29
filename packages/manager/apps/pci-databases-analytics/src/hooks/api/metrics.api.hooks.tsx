be4bf0a71081a9b32a2d04c711dbef04f11bc05d
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-02-29 16:18:28 +0100

    feat: add metrics and logs pages
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

---
+++ packages/manager/apps/pci-databases-analytics/src/hooks/api/metrics.api.hooks.tsx
@@ -0,0 +1,45 @@
+import {
+  QueryObserverOptions,
+  UseQueryResult,
+  useQuery,
+} from '@tanstack/react-query';
+import { database } from '@/models/database';
+import { getMetric, getMetrics } from '@/api/databases/metrics';
+
+export function useGetMetrics(
+  projectId: string,
+  engine: database.EngineEnum,
+  serviceId: string,
+  options: Omit<QueryObserverOptions, 'queryKey'> = {},
+) {
+  const queryKey = [projectId, 'database', engine, serviceId, 'metric'];
+  return useQuery({
+    queryKey,
+    queryFn: () => getMetrics(projectId, engine, serviceId),
+    ...options,
+  }) as UseQueryResult<string[], Error>;
+}
+
+export function useGetMetric(
+  projectId: string,
+  engine: database.EngineEnum,
+  serviceId: string,
+  metric: string,
+  period: database.service.MetricPeriodEnum,
+  options: Omit<QueryObserverOptions, 'queryKey'> = {},
+) {
+  const queryKey = [
+    projectId,
+    'database',
+    engine,
+    serviceId,
+    'metric',
+    metric,
+    period,
+  ];
+  return useQuery({
+    queryKey,
+    queryFn: () => getMetric(projectId, engine, serviceId, metric, period),
+    ...options,
+  }) as UseQueryResult<database.service.Metric, Error>;
+}
