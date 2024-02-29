f095e82b94dda1153d8e285859bfd21fe670a39a
Sachin Ramesh <104337442+sachinrameshn@users.noreply.github.com>
2024-02-29 18:46:43 +0530

    feat(dedicated): server listing page changes (#10715)
    ref: MANAGER-11844
    Signed-off-by: sachinrameshn <sachin.ramesh@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/dedicated/dedicated-server/dedicated-server.routing.js
+++ packages/manager/apps/dedicated/client/app/dedicated/dedicated-server/dedicated-server.routing.js
@@ -12,8 +12,23 @@ export default /* @ngInject */ ($stateProvider) => {
             'billing:autorenew2016Deployment',
             'dedicated-server:cluster',
           ]),
-        isMultiAZAvailable: /* @ngInject */ (featureAvailability) =>
-          featureAvailability?.isFeatureAvailable('dedicated-server:cluster'),
+        isMultiAZAvailable: /* @ngInject */ (
+          $q,
+          $http,
+          featureAvailability,
+        ) => {
+          return $q
+            .all({
+              isMultiAZFeatureAvailable: featureAvailability?.isFeatureAvailable(
+                'dedicated-server:cluster',
+              ),
+              clusters: $http.get('/dedicated/cluster'),
+            })
+            .then(
+              ({ isMultiAZFeatureAvailable, clusters }) =>
+                isMultiAZFeatureAvailable && clusters.data.length !== 0,
+            );
+        },
         currentActiveLink: /* @ngInject */ ($transition$, $state) => () =>
           $state.href($state.current.name, $transition$.params()),
         allServersLink: /* @ngInject */ ($transition$, $state) =>
