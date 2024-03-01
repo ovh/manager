6a8b82d4263ecc23ecf8f3d89fa459e578d09edf
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-03-01 17:25:12 +0100

    feat: add modals hook, add database tab
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/apps/pci-databases-analytics/src/components/ui/table.tsx
+++ packages/manager/apps/pci-databases-analytics/src/components/ui/table.tsx
@@ -73,7 +73,7 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "h-12 px-2 py-2 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
+      "h-10 px-2 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
       className
     )}
     {...props}
