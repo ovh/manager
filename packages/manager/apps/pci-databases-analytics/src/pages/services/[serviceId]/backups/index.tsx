32da3699b64bb6439922a4cdba4a6488d162445e
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-03-04 17:57:51 +0100

    feat: add backups page, fix sonar issues
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/apps/pci-databases-analytics/src/pages/services/[serviceId]/backups/index.tsx
+++ packages/manager/apps/pci-databases-analytics/src/pages/services/[serviceId]/backups/index.tsx
@@ -1,11 +1,56 @@
-import { P } from '@/components/typography';
+import { useTranslation } from 'react-i18next';
+import { add } from 'date-fns';
+import BreadcrumbItem from '@/components/Breadcrumb/BreadcrumbItem';
+import { H2, P } from '@/components/typography';
+import { database } from '@/models/database';
+import { useServiceData } from '../layout';
+import { getColumns } from './_components/backupsTableColumns';
+import { DataTable } from '@/components/ui/data-table';
+import { useGetBackups } from '@/hooks/api/backups.api.hooks';
 
 export function breadcrumb() {
-  return 'Backups';
+  return (
+    <BreadcrumbItem
+      translationKey="breadcrumb"
+      namespace="pci-databases-analytics/services/service/backups"
+    />
+  );
 }
 
+export interface BackupWithExpiricyDate extends database.Backup {
+  expiricyDate: Date;
+}
 const Backups = () => {
-  return <P>Backups</P>;
+  const { t } = useTranslation(
+    'pci-databases-analytics/services/service/backups',
+  );
+  const { projectId, service } = useServiceData();
+  const backupsQuery = useGetBackups(projectId, service.engine, service.id, {
+    refetchInterval: 30_000,
+  });
+  const columns = getColumns();
+  return (
+    <>
+      <H2 className="mb-2">{t('title')}</H2>
+      <P className="mb-2">{t('description')}</P>
+      {backupsQuery.isSuccess ? (
+        <>
+          <DataTable
+            columns={columns}
+            data={backupsQuery.data.map((backup) => ({
+              ...backup,
+              expiricyDate: add(new Date(backup.createdAt), {
+                days: service.backups.retentionDays,
+              }),
+            }))}
+            pageSize={25}
+          />
+        </>
+      ) : (
+        <DataTable.Skeleton columns={5} rows={5} width={100} height={16} />
+      )}
+    </>
+  );
 };
 
 export default Backups;
