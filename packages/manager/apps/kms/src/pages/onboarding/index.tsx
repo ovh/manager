4e9398dd76c0edbd1a4bd12edcb2a7fb23feb80a
Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>
2024-03-06 17:41:17 +0100

    refactor(kms): delete generouted

--- packages/manager/apps/kms/src/pages/onboarding/index.tsx
+++ packages/manager/apps/kms/src/pages/onboarding/index.tsx
@@ -1,50 +1,48 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Card, OnboardingLayout } from '@ovhcloud/manager-components';
-import useGuideUtils from '@/components/GuideLink/guide-utils';
 import onboardingImgSrc from './onboarding-img.png';
 
 export default function Onboarding() {
   const { t } = useTranslation('kms/onboarding');
-  const link = useGuideUtils();
 
   const tileList = [
     {
+      id: 1,
       texts: {
         title: t('guide1Title'),
         description: t('guide1Description'),
         category: t('guideCategory'),
       },
-      href: link?.guideLink1,
+      href: 'https://ovh/com/link/1',
     },
     {
+      id: 2,
       texts: {
         title: t('guide2Title'),
         description: t('guide2Description'),
         category: t('guideCategory'),
       },
-      href: link?.guideLink2,
+      href: 'https://ovh/com/link/2',
     },
     {
+      id: 3,
       texts: {
         title: t('guide3Title'),
         description: t('guide3Description'),
         category: t('guideCategory'),
       },
-      href: link?.guideLink3,
+      href: 'https://ovh/com/link/3',
     },
   ];
 
   const title: string = t('title');
   const description: string = t('description');
-  const imgSrc = {
-    src: onboardingImgSrc,
-  };
 
   return (
     <OnboardingLayout
       title={title}
-      img={imgSrc}
+      img={{ src: onboardingImgSrc }}
       description={description}
       orderButtonLabel={t('orderButtonLabel')}
       orderHref={t('orderButtonLink')}
@@ -53,7 +51,7 @@ export default function Onboarding() {
     >
       <aside className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 pt-12">
         {tileList.map((tile) => (
-          <Card href={tile.href} texts={tile.texts} />
+          <Card key={tile.id} href={tile.href} texts={tile.texts} />
         ))}
       </aside>
     </OnboardingLayout>
