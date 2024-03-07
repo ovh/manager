5e0d6f402fb7a6aa701dd07d8ea75e6b2e5e97ca
mhalheli soufien <mhalheli.soufien@gmail.com>
2024-03-07 16:10:59 +0100

    feat(dedicated): rename variable

--- packages/manager/apps/dedicated/client/app/dedicated/dedicated-server/clusters/cluster/cluster.constant.js
+++ packages/manager/apps/dedicated/client/app/dedicated/dedicated-server/clusters/cluster/cluster.constant.js
@@ -1,5 +1,5 @@
 export const STATUS = { DONE: 'DONE' };
-export const GUIDES_LINK =
+export const GUIDES_LINK_OF_3AZ =
   'https://github.com/ovh/docs/blob/3az_JTA/pages/bare_metal_cloud/dedicated_servers/3az/guide.en-gb.md';
 export default {
   STATUS,
