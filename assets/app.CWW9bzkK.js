b3c8adee8be0533eb218b1a0c9257bfd41c4a310
ovh-ux-cds <ovh-ux-cds@users.noreply.github.com>
2024-03-07 09:18:53 +0000

    docs: update documentation [skip ci]

--- assets/app.CWW9bzkK.js
+++
@@ -1,7 +0,0 @@
-import{v as s,ae as i,af as u,ag as c,ah as l,ai as f,aj as d,ak as m,al as h,am as A,an as g,Y as v,d as P,u as y,j as C,z as w,ao as _,ap as E,aq as R,ar as b}from"./chunks/framework.D09SL32Y.js";import{t as j}from"./chunks/theme.OU8u7W9p.js";function p(e){if(e.extends){const a=p(e.extends);return{...a,...e,async enhanceApp(t){a.enhanceApp&&await a.enhanceApp(t),e.enhanceApp&&await e.enhanceApp(t)}}}return e}const o=p(j),D=P({name:"VitePressApp",setup(){const{site:e}=y();return C(()=>{w(()=>{document.documentElement.lang=e.value.lang,document.documentElement.dir=e.value.dir})}),e.value.router.prefetchLinks&&_(),E(),R(),o.setup&&o.setup(),()=>b(o.Layout)}});async function L(){const e=S(),a=O();a.provide(u,e);const t=c(e.route);return a.provide(l,t),a.component("Content",f),a.component("ClientOnly",d),Object.defineProperties(a.config.globalProperties,{$frontmatter:{get(){return t.frontmatter.value}},$params:{get(){return t.page.value.params}}}),o.enhanceApp&&await o.enhanceApp({app:a,router:e,siteData:m}),{app:a,router:e,data:t}}function O(){return h(D)}function S(){let e=s,a;return A(t=>{let n=g(t),r=null;return n&&(e&&(a=n),(e||a===n)&&(n=n.replace(/\.js$/,".lean.js")),r=v(()=>import(n),__vite__mapDeps([]))),s&&(e=!1),r},o.NotFound)}s&&L().then(({app:e,router:a,data:t})=>{a.go().then(()=>{i(a.route,t.site),e.mount("#app")})});export{L as createApp};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
---
+++ assets/guide_releasing.md.d894daab.js
@@ -0,0 +1,125 @@
+import{_ as s,o as n,c as a,Q as l}from"./chunks/framework.2eec50d8.js";const h=JSON.parse('{"title":"Releasing","description":"","frontmatter":{},"headers":[],"relativePath":"guide/releasing.md","filePath":"guide/releasing.md","lastUpdated":1709802941000}'),p={name:"guide/releasing.md"},e=l(`<h1 id="releasing" tabindex="-1">Releasing <a class="header-anchor" href="#releasing" aria-label="Permalink to &quot;Releasing&quot;">​</a></h1><div class="warning custom-block"><p class="custom-block-title">TODOs</p><p>⚠️ This section is in a work in progress ⚠️</p></div><p>Releasing a new version of Manager 🎉</p><h2 id="prepare" tabindex="-1">Prepare <a class="header-anchor" href="#prepare" aria-label="Permalink to &quot;Prepare&quot;">​</a></h2><ol><li><p>Decide which <a href="https://github.com/ovh/manager/pulls" target="_blank" rel="noreferrer">PRs</a> should be part of the next release.</p></li><li><p>Create a new PR for the <code>develop</code> branch. Please use the following template for the PR description, linking to the relevant issues and/or pull requests for each change, removing irrelevant headings:</p></li></ol><div class="language-md vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">md</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#79B8FF;font-weight:bold;"># 📦 New release</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">Approximate release date: 📆 DD/MM/YY</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">## Dashboard</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">### :sparkles: Features</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [ ] Description #</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">### :bug: Bug Fixes</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [ ] Description #</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">## Web Cloud</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">### :sparkles: Features</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [ ] Description #</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">### :bug: Bug Fixes</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [ ] Description #</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">## Bare Metal Cloud</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">### :sparkles: Features</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [ ] Description #</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">### :bug: Bug Fixes</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [ ] Description #</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">## Hosted Private Cloud</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">### :sparkles: Features</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [ ] Description #</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">### :bug: Bug Fixes</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [ ] Description #</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">## Public Cloud</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">### :sparkles: Features</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [ ] Description #</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">### :bug: Bug Fixes</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [ ] Description #</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">## Telecom</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">### :sparkles: Features</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [ ] Description #</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">### :bug: Bug Fixes</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [ ] Description #</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#005CC5;font-weight:bold;"># 📦 New release</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">Approximate release date: 📆 DD/MM/YY</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#005CC5;font-weight:bold;">## Dashboard</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#005CC5;font-weight:bold;">### :sparkles: Features</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E36209;">-</span><span style="color:#24292E;"> [ ] Description #</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#005CC5;font-weight:bold;">### :bug: Bug Fixes</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E36209;">-</span><span style="color:#24292E;"> [ ] Description #</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#005CC5;font-weight:bold;">## Web Cloud</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#005CC5;font-weight:bold;">### :sparkles: Features</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E36209;">-</span><span style="color:#24292E;"> [ ] Description #</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#005CC5;font-weight:bold;">### :bug: Bug Fixes</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E36209;">-</span><span style="color:#24292E;"> [ ] Description #</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#005CC5;font-weight:bold;">## Bare Metal Cloud</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#005CC5;font-weight:bold;">### :sparkles: Features</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E36209;">-</span><span style="color:#24292E;"> [ ] Description #</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#005CC5;font-weight:bold;">### :bug: Bug Fixes</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E36209;">-</span><span style="color:#24292E;"> [ ] Description #</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#005CC5;font-weight:bold;">## Hosted Private Cloud</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#005CC5;font-weight:bold;">### :sparkles: Features</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E36209;">-</span><span style="color:#24292E;"> [ ] Description #</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#005CC5;font-weight:bold;">### :bug: Bug Fixes</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E36209;">-</span><span style="color:#24292E;"> [ ] Description #</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#005CC5;font-weight:bold;">## Public Cloud</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#005CC5;font-weight:bold;">### :sparkles: Features</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E36209;">-</span><span style="color:#24292E;"> [ ] Description #</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#005CC5;font-weight:bold;">### :bug: Bug Fixes</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E36209;">-</span><span style="color:#24292E;"> [ ] Description #</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#005CC5;font-weight:bold;">## Telecom</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#005CC5;font-weight:bold;">### :sparkles: Features</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E36209;">-</span><span style="color:#24292E;"> [ ] Description #</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#005CC5;font-weight:bold;">### :bug: Bug Fixes</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E36209;">-</span><span style="color:#24292E;"> [ ] Description #</span></span></code></pre></div>`,6),o=[e];function i(c,t,r,d,g,y){return n(),a("div",null,o)}const u=s(p,[["render",i]]);export{h as __pageData,u as default};
