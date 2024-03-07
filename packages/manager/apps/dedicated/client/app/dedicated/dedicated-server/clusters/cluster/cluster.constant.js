bd10b9326d799c03ed52aae164e7237a60f39eab
mhalheli soufien <mhalheli.soufien@gmail.com>
2024-03-07 11:52:06 +0100

    feat(dedicated): add guides for 3az cluster
      ref: MANAGER-13600
    
    Signed-off-by: mhalheli soufien <mhalheli.soufien@gmail.com>

--- packages/manager/apps/dedicated/client/app/dedicated/dedicated-server/clusters/cluster/cluster.constant.js
+++ packages/manager/apps/dedicated/client/app/dedicated/dedicated-server/clusters/cluster/cluster.constant.js
@@ -1,4 +1,6 @@
 export const STATUS = { DONE: 'DONE' };
+export const GUIDES_LINK =
+  'https://github.com/ovh/docs/blob/3az_JTA/pages/bare_metal_cloud/dedicated_servers/3az/guide.en-gb.md';
 export default {
   STATUS,
 };
