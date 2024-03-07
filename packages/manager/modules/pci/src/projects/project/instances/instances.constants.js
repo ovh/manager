e2256a68c21ea7f391fae9cd4e32dee98454c534
brofik <10513484+brofik@users.noreply.github.com>
2024-03-07 16:09:12 +0100

    feat(project.instance) : update a billing upgrade message (#10841)
    ref: TAPC-473
    
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/pci/src/projects/project/instances/instances.constants.js
+++ packages/manager/modules/pci/src/projects/project/instances/instances.constants.js
@@ -27,6 +27,30 @@ export const POLLER_INSTANCE_NAMESPACE = {
   UNSHELVE: 'cloud.project.instance.unshelve',
 };
 
+export const INSTANCE_PRICING_LINKS = {
+  DEFAULT: 'https://www.ovhcloud.com/en/public-cloud/prices',
+  FR: 'https://www.ovhcloud.com/fr/public-cloud/prices',
+  MA: 'https://www.ovhcloud.com/fr-ma/public-cloud/prices',
+  TN: 'https://www.ovhcloud.com/fr-tn/public-cloud/prices',
+  SN: 'https://www.ovhcloud.com/fr-sn/public-cloud/prices',
+  QC: 'https://www.ovhcloud.com/fr-ca/public-cloud/prices',
+  GB: 'https://www.ovhcloud.com/en-gb/public-cloud/prices',
+  IE: 'https://www.ovhcloud.com/en-ie/public-cloud/prices',
+  WE: 'https://www.ovhcloud.com/en/public-cloud/prices',
+  CA: 'https://www.ovhcloud.com/en-ca/public-cloud/prices',
+  AU: 'https://www.ovhcloud.com/en-au/public-cloud/prices',
+  SG: 'https://www.ovhcloud.com/en-sg/public-cloud/prices',
+  ASIA: 'https://www.ovhcloud.com/asia/public-cloud/prices',
+  IN: 'https://www.ovhcloud.com/en-in/public-cloud/prices',
+  ES: 'https://www.ovhcloud.com/es-es/public-cloud/prices',
+  WS: 'https://www.ovhcloud.com/es/public-cloud/prices',
+  PT: 'https://www.ovhcloud.com/pt/public-cloud/prices',
+  IT: 'https://www.ovhcloud.com/it/public-cloud/prices',
+  PL: 'https://www.ovhcloud.com/pl/public-cloud/prices',
+  DE: 'https://www.ovhcloud.com/de/public-cloud/prices',
+  NL: 'https://www.ovhcloud.com/nl/public-cloud/prices',
+};
+
 export const FLAVORS_FEATURES_FLIPPING_MAP = {
   balanced: PCI_FEATURES.INSTANCE_FLAVORS_CATEGORY.GENERAL,
   cpu: PCI_FEATURES.INSTANCE_FLAVORS_CATEGORY.CPU,
@@ -55,4 +79,5 @@ export default {
   TYPES_TO_EXCLUDE,
   FLAVORS_FEATURES_FLIPPING_MAP,
   LOCAL_ZONE_REGION,
+  INSTANCE_PRICING_LINKS,
 };
