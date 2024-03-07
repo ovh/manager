8f8764e68d47398b8167502037b93fec48ffce2a
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2024-03-07 14:20:41 +0100

    feat(kms): update routew react router
    ref: MANAGER-X
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager/apps/key-management-system/src/App.tsx
+++ packages/manager/apps/key-management-system/src/App.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect } from 'react';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { odsSetup } from '@ovhcloud/ods-common-core';
 import { useShell } from '@ovh-ux/manager-react-shell-client';
 import '@ovhcloud/ods-theme-blue-jeans';
 import queryClient from './query.client';
-import Router from './Router';
+
+import appRoutes from './routes';
 
 odsSetup();
+const router = createHashRouter(appRoutes);
 
 function App() {
   const shell = useShell();
@@ -17,7 +20,7 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Router />
+      <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
