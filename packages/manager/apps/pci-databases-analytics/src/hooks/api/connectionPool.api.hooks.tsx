10f0039425d65907d4c52e4c1f27a3eea7f25443
Arthur Bullet <arthur.bullet@ovhcloud.com>
2024-03-08 15:38:01 +0100

    feat(pci.db): add connection pools tab
    datatr-456
    
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>

---
+++ packages/manager/apps/pci-databases-analytics/src/hooks/api/connectionPool.api.hooks.tsx
@@ -0,0 +1,100 @@
+import {
+  QueryObserverOptions,
+  UseQueryResult,
+  useMutation,
+  useQuery,
+} from '@tanstack/react-query';
+
+import {
+  AddConnectionPoolProps,
+  DeleteConnectionPoolProps,
+  EditConnectionPoolProps,
+  addConnectionPool,
+  deleteConnectionPool,
+  editConnectionPool,
+  getConnectionPools,
+} from '@/api/databases/connectionPool';
+import { database } from '@/models/database';
+
+export function useGetConnectionPools(
+  projectId: string,
+  engine: database.EngineEnum,
+  serviceId: string,
+  options: Omit<QueryObserverOptions, 'queryKey'> = {},
+) {
+  const queryKey = [projectId, 'database', engine, serviceId, 'connectionPool'];
+  return useQuery({
+    queryKey,
+    queryFn: () => getConnectionPools({ projectId, engine, serviceId }),
+    ...options,
+  }) as UseQueryResult<database.postgresql.ConnectionPool[], Error>;
+}
+
+interface MutateConnectionPoolProps {
+  onError: (cause: Error) => void;
+  onSuccess: (connectionPool: database.postgresql.ConnectionPool) => void;
+}
+export function useAddConnectionPool({
+  onError,
+  onSuccess,
+}: MutateConnectionPoolProps) {
+  const mutation = useMutation({
+    mutationFn: (cpInfo: AddConnectionPoolProps) => {
+      return addConnectionPool(cpInfo);
+    },
+    onError,
+    onSuccess,
+  });
+
+  return {
+    addConnectionPool: (cpInfo: AddConnectionPoolProps) => {
+      return mutation.mutate(cpInfo);
+    },
+    ...mutation,
+  };
+}
+
+export function useEditConnectionPool({
+  onError,
+  onSuccess,
+}: MutateConnectionPoolProps) {
+  const mutation = useMutation({
+    mutationFn: (cpInfo: EditConnectionPoolProps) => {
+      return editConnectionPool(cpInfo);
+    },
+    onError,
+    onSuccess,
+  });
+
+  return {
+    editConnectionPool: (cpInfo: EditConnectionPoolProps) => {
+      return mutation.mutate(cpInfo);
+    },
+    ...mutation,
+  };
+}
+
+interface UseDeleteConnectionPoolProps {
+  onError: (cause: Error) => void;
+  onSuccess: () => void;
+}
+
+export function useDeleteConnectionPool({
+  onError,
+  onSuccess,
+}: UseDeleteConnectionPoolProps) {
+  const mutation = useMutation({
+    mutationFn: (connectionPoolInfo: DeleteConnectionPoolProps) => {
+      return deleteConnectionPool(connectionPoolInfo);
+    },
+    onError,
+    onSuccess,
+  });
+
+  return {
+    deleteConnectionPool: (connectionPoolInfo: DeleteConnectionPoolProps) => {
+      return mutation.mutate(connectionPoolInfo);
+    },
+    ...mutation,
+  };
+}
--- packages/manager/apps/pci-databases-analytics/src/pages/services/[serviceId]/pools/index.tsx
+++ packages/manager/apps/pci-databases-analytics/src/pages/services/[serviceId]/pools/index.tsx
@@ -1,11 +1,169 @@
-import { P } from '@/components/typography';
+import { useTranslation } from 'react-i18next';
+import { ColumnDef } from '@tanstack/react-table';
+
+import { Plus } from 'lucide-react';
+import { H2, P } from '@/components/typography';
+import { Button } from '@/components/ui/button';
+import { DataTable } from '@/components/ui/data-table';
+
+import { useModale } from '@/hooks/useModale';
+import { useGetConnectionPools } from '@/hooks/api/connectionPool.api.hooks';
+import { useGetDatabases } from '@/hooks/api/databases.api.hook';
+import { useGetUsers } from '@/hooks/api/users.api.hooks';
+
+import { useServiceData } from '../layout';
+import { database } from '@/models/database';
+import { POLLING } from '@/configuration/polling';
+import { getColumns } from './_components/poolsTableColumns';
+import InfoConnectionPoolModal from './_components/infoConnectionPool';
+import AddEditConnectionPoolModal from './_components/addEditConnectionPool';
+import DeleteConnectionPool from './_components/deleteConnectionPool';
 
 export function breadcrumb() {
   return 'Pools';
 }
 
 const Pools = () => {
-  return <P>Pools</P>;
+  const { t } = useTranslation(
+    'pci-databases-analytics/services/service/pools',
+  );
+  const { projectId, service } = useServiceData();
+  const addModale = useModale('add');
+  const getInfoModale = useModale('information');
+  const deleteModale = useModale('delete');
+  const editModale = useModale('edit');
+  const connectionPoolsQuery = useGetConnectionPools(
+    projectId,
+    service.engine,
+    service.id,
+    {
+      refetchInterval: POLLING.POOLS,
+    },
+  );
+  const databasesQuery = useGetDatabases(
+    projectId,
+    service.engine,
+    service.id,
+    {
+      refetchInterval: POLLING.DATABASES,
+    },
+  );
+  const usersQuery = useGetUsers(projectId, service.engine, service.id, {
+    refetchInterval: POLLING.USERS,
+  });
+
+  const columns: ColumnDef<database.postgresql.ConnectionPool>[] = getColumns({
+    databases: databasesQuery.data,
+    users: usersQuery.data,
+    onGetInformationClick: (pools: database.postgresql.ConnectionPool) =>
+      getInfoModale.open(pools.id),
+    onEditClick: (pools: database.postgresql.ConnectionPool) =>
+      editModale.open(pools.id),
+    onDeleteClick: (pools: database.postgresql.ConnectionPool) =>
+      deleteModale.open(pools.id),
+  });
+
+  const conenctionPoolToDelete = connectionPoolsQuery.data?.find(
+    (cp) => cp.id === deleteModale.value,
+  );
+
+  const conenctionPoolToDisplayInfo = connectionPoolsQuery.data?.find(
+    (cp) => cp.id === getInfoModale.value,
+  );
+
+  const conenctionPoolToEdit = connectionPoolsQuery.data?.find(
+    (cp) => cp.id === editModale.value,
+  );
+
+  return (
+    <>
+      <H2 className="mb-2">{t('title')}</H2>
+      <P className="mb-2">{t('description')}</P>
+      <Button
+        variant={'outline'}
+        size="sm"
+        className="text-base mb-2"
+        onClick={() => addModale.open()}
+      >
+        <Plus className="size-4 mr-2" />
+        {t('addButtonLabel')}
+      </Button>
+
+      {connectionPoolsQuery.isSuccess &&
+      usersQuery.isSuccess &&
+      databasesQuery.isSuccess ? (
+        <DataTable
+          columns={columns}
+          data={connectionPoolsQuery.data}
+          pageSize={25}
+        />
+      ) : (
+        <DataTable.Skeleton columns={5} rows={2} width={100} height={16} />
+      )}
+
+      {connectionPoolsQuery.isSuccess &&
+        usersQuery.isSuccess &&
+        databasesQuery.isSuccess && (
+          <AddEditConnectionPoolModal
+            isEdition={false}
+            controller={addModale.controller}
+            users={usersQuery.data}
+            service={service}
+            connectionPools={connectionPoolsQuery.data}
+            databases={databasesQuery.data}
+            onSuccess={() => {
+              addModale.close();
+              databasesQuery.refetch();
+              connectionPoolsQuery.refetch();
+            }}
+          />
+        )}
+
+      {conenctionPoolToDisplayInfo &&
+        connectionPoolsQuery.isSuccess &&
+        databasesQuery.isSuccess && (
+          <InfoConnectionPoolModal
+            service={service}
+            controller={getInfoModale.controller}
+            connectionPool={conenctionPoolToDisplayInfo}
+            databases={databasesQuery.data}
+          />
+        )}
+
+      {conenctionPoolToEdit &&
+        connectionPoolsQuery.isSuccess &&
+        usersQuery.isSuccess &&
+        databasesQuery.isSuccess && (
+          <AddEditConnectionPoolModal
+            isEdition={true}
+            controller={editModale.controller}
+            editedConnectionPool={conenctionPoolToEdit}
+            users={usersQuery.data}
+            service={service}
+            databases={databasesQuery.data}
+            onSuccess={() => {
+              editModale.close();
+              databasesQuery.refetch();
+              connectionPoolsQuery.refetch();
+            }}
+          />
+        )}
+
+      {conenctionPoolToDelete && (
+        <DeleteConnectionPool
+          controller={deleteModale.controller}
+          service={service}
+          connectionPool={conenctionPoolToDelete}
+          onSuccess={() => {
+            deleteModale.close();
+            connectionPoolsQuery.refetch();
+            usersQuery.refetch();
+            databasesQuery.refetch();
+          }}
+        />
+      )}
+    </>
+  );
 };
 
 export default Pools;
