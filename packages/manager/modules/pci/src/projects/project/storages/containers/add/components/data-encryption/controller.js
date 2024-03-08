c1860022a5995fbf7f7d52739afab08b5c36a3c7
David Arsène <david.arsene.ext@ovhcloud.com>
2024-03-08 17:34:56 +0100

    feat(pci.storage): update dashboard container for OMK encryption
    ref: MANAGER-13027
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/storages/containers/add/components/data-encryption/controller.js
+++
@@ -1,9 +0,0 @@
-import { ENCRYPTION_ALGORITHMS } from '../../../containers.constants';
-
-export default class PciProjectStorageDataEncryptionController {
-  $onInit() {
-    this.ENCRYPTION_ALGORITHMS = ENCRYPTION_ALGORITHMS;
-    this.encryption.sseAlgorithm =
-      this.encryption.sseAlgorithm ?? ENCRYPTION_ALGORITHMS.NONE;
-  }
-}
