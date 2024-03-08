5888c67032ea2f823fb162723a54e3e4575e9f7c
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-03-08 18:11:39 +0100

    feat: add fork page mutation (wip)
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/apps/pci-databases-analytics/src/models/database.ts
+++ packages/manager/apps/pci-databases-analytics/src/models/database.ts
@@ -987,9 +987,9 @@ export namespace database {
       /** Defines the source to fork a cluster from a backup */
       export interface ForkFrom {
         /** Backup ID (not compatible with pointInTime) */
-        backupId: string;
+        backupId?: string;
         /** Point in time to restore from (not compatible with id) */
-        pointInTime: string;
+        pointInTime?: string;
         /** Service ID to which the backups belong to */
         serviceId: string;
       }
