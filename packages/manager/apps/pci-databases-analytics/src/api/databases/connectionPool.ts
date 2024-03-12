ff79d591e23efe62688d25af925ffeb8ceb213e0
abullet33 <106544940+abullet33@users.noreply.github.com>
2024-03-12 11:58:13 +0100

    feat: add connection pools tab (#11140)
    feat: add connection pools tab
    
    datatr-456
    
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>

---
+++ packages/manager/apps/pci-databases-analytics/src/api/databases/connectionPool.ts
@@ -0,0 +1,77 @@
+import { apiClient } from '@ovh-ux/manager-core-api';
+import { database } from '@/models/database';
+import { ServiceData } from '.';
+
+export const getConnectionPools = async ({
+  projectId,
+  engine,
+  serviceId,
+}: ServiceData) =>
+  apiClient.v6
+    .get(
+      `/cloud/project/${projectId}/database/${engine}/${serviceId}/connectionPool`,
+      {
+        headers: {
+          'X-Pagination-Mode': 'CachedObjectList-Pages',
+          'X-Pagination-Size': '50000',
+          Pragma: 'no-cache',
+        },
+      },
+    )
+    .then((res) => res.data as database.postgresql.ConnectionPool[]);
+
+export interface AddConnectionPoolProps extends ServiceData {
+  connectionPool: database.postgresql.ConnectionPoolCreation;
+}
+
+export const addConnectionPool = async ({
+  projectId,
+  engine,
+  serviceId,
+  connectionPool,
+}: AddConnectionPoolProps) =>
+  apiClient.v6
+    .post(
+      `/cloud/project/${projectId}/database/${engine}/${serviceId}/connectionPool`,
+      connectionPool,
+    )
+    .then((res) => res.data as database.postgresql.ConnectionPool);
+
+export interface ConnectionPoolEdition {
+  databaseId: string;
+  mode: database.postgresql.connectionpool.ModeEnum;
+  size: number;
+  userId?: string;
+}
+
+export interface EditConnectionPoolProps extends ServiceData {
+  connectionPoolId: string;
+  connectionPool: ConnectionPoolEdition;
+}
+
+export const editConnectionPool = async ({
+  projectId,
+  engine,
+  serviceId,
+  connectionPoolId,
+  connectionPool,
+}: EditConnectionPoolProps) =>
+  apiClient.v6
+    .put(
+      `/cloud/project/${projectId}/database/${engine}/${serviceId}/connectionPool/${connectionPoolId}`,
+      connectionPool,
+    )
+    .then((res) => res.data as database.postgresql.ConnectionPool);
+
+export interface DeleteConnectionPoolProps extends ServiceData {
+  connectionPoolId: string;
+}
+export const deleteConnectionPool = async ({
+  projectId,
+  engine,
+  serviceId,
+  connectionPoolId,
+}: DeleteConnectionPoolProps) =>
+  apiClient.v6.delete(
+    `/cloud/project/${projectId}/database/${engine}/${serviceId}/connectionPool/${connectionPoolId}`,
+  );
