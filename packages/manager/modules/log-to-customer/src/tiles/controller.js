53c99a379c522d9b9c156f82952d7fe9d5083ede
Amandine Dectot <amandine.dectot.ext@corp.ovh.com>
2024-03-13 10:43:59 +0100

    feat(pci): log to customer - watching on graylog
    ref: MANAGER-12693
    
    Signed-off-by: Amandine Dectot <amandine.dectot.ext@corp.ovh.com>

---
+++ packages/manager/modules/log-to-customer/src/tiles/controller.js
@@ -0,0 +1,116 @@
+import { DATA_PLATFORM_GUIDE } from '../constants';
+
+export default class LogToCustomerTileCtrl {
+  /* @ngInject */
+  constructor(LogToCustomerService, coreConfig, coreURLBuilder, $q) {
+    this.LogToCustomerService = LogToCustomerService;
+    this.user = coreConfig.getUser();
+    this.coreURLBuilder = coreURLBuilder;
+    this.$q = $q;
+  }
+
+  $onInit() {
+    this.dbaasLogsURL = this.coreURLBuilder.buildURL(
+      'dedicated',
+      '#/dbaas/logs',
+    );
+
+    this.links = {
+      logPlatformGuide:
+        DATA_PLATFORM_GUIDE[this.user.ovhSubsidiary] ||
+        DATA_PLATFORM_GUIDE.DEFAULT,
+      createStream: this.dbaasLogsURL,
+      createAccount: `${this.dbaasLogsURL}/order`,
+    };
+
+    this.hasAtLeastOneDataStream = false;
+    this.loadStreams();
+  }
+
+  loadStreams() {
+    this.setErrorMessage(null);
+    this.loading = true;
+    this.streamSubscriptions = [];
+
+    this.$q
+      .all([this.getDataStreams(), this.getSubscribedStreams()])
+      .catch(({ data }) => {
+        this.setErrorMessage(data.message);
+      })
+      .finally(() => {
+        this.loading = false;
+      });
+  }
+
+  async getSubscribedStreams() {
+    try {
+      const data = await this.LogToCustomerService.icebergQuery(
+        this.logSubscriptionUrl,
+        { kind: this.kind },
+      );
+
+      const streamPromises = data.map(async (subscription) => {
+        const [account, stream, url] = await this.$q.all([
+          this.LogToCustomerService.getLogAccount(subscription.serviceName),
+          this.LogToCustomerService.getSream(
+            subscription.serviceName,
+            subscription.streamId,
+          ),
+          this.LogToCustomerService.getStreamUrl(
+            subscription.serviceName,
+            subscription.streamId,
+          ),
+        ]);
+
+        return {
+          ...subscription,
+          isLoading: false,
+          userName: account.username,
+          displayName: account.displayName || account.serviceName,
+          streamName: stream.title,
+          streamUrl: url,
+        };
+      });
+
+      this.streamSubscriptions = await this.$q.all(streamPromises);
+    } catch (error) {
+      this.setErrorMessage(error.message);
+    }
+  }
+
+  getDataStreams() {
+    return this.LogToCustomerService.getDataStreams().then(
+      ({ streams, streamCount }) => {
+        this.logAccountStreams = streams;
+        this.hasAtLeastOneDataStream = streamCount > 0;
+      },
+    );
+  }
+
+  deleteLogSubscription(subscription) {
+    this.setErrorMessage(null);
+    // eslint-disable-next-line no-param-reassign
+    subscription.isLoading = true;
+
+    this.LogToCustomerService.delete(
+      `${this.logSubscriptionUrl}/${subscription.subscriptionId}`,
+    )
+      .then(({ data }) => {
+        this.LogToCustomerService.pollOperation(
+          subscription.serviceName,
+          data,
+        ).then(() => this.loadStreams());
+      })
+      .catch(({ data }) => {
+        this.setErrorMessage(data.message);
+      })
+      .finally(() => {
+        // eslint-disable-next-line no-param-reassign
+        subscription.isLoading = false;
+      });
+  }
+
+  goToStreamListingPage() {
+    this.goToListingPage({ kind: this.kind });
+  }
+}
