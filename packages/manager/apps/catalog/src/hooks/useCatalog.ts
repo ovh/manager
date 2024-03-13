54e9e8eb3e66f3a0349f31187fe059cc84553a4a
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2024-03-13 16:43:14 +0100

    fix(catalog): update request typing client api
    ref: MANAGER-13618
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager/apps/catalog/src/hooks/useCatalog.ts
+++ packages/manager/apps/catalog/src/hooks/useCatalog.ts
@@ -1,4 +1,4 @@
-import { AxiosError, AxiosResponse } from 'axios';
+import { ApiError, ApiResponse } from '@ovh-ux/manager-core-api';
 import { useState, useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import {
@@ -22,7 +22,7 @@ export const useCatalog = ({
   const [products, setProducts] = useState<Product[]>([]); // full list of products
   const [results, setResults] = useState<Product[]>([]); // the filtered list of products
 
-  const service = useQuery<AxiosResponse<Product[]>, AxiosError>({
+  const service = useQuery<ApiResponse<Product[]>, ApiError>({
     queryKey: getManagerCatalogListQueryKey,
     queryFn: () => getManagerCatalogList(),
     staleTime: Infinity,
