06945d010003b0d108e6e82862a11e94e40699fa
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2024-03-13 16:30:38 +0100

    fix(shell-client): revert custom hooks
    ref: MANAGER-13618
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager/core/shell-client/src/hooks/index.ts
+++ packages/manager/core/shell-client/src/hooks/index.ts
@@ -3,3 +3,6 @@ export * from './useRouteSynchro';
 export * from './useRouting';
 export * from './useTracking';
 export * from './useNavigation';
+export * from './useUX';
+export * from './useShell';
+export * from './useEnvironment';
