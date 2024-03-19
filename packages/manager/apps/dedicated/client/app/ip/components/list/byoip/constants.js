00fc793dc64ec794964530230c1383397722c451
Sachin Ramesh <104337442+sachinrameshn@users.noreply.github.com>
2024-03-19 16:36:13 +0530

    feat(dedicated): modify error description (#11142)
    ref: MANAGER-13679
    
    Signed-off-by: sachinrameshn <sachin.ramesh@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/ip/components/list/byoip/constants.js
+++ packages/manager/apps/dedicated/client/app/ip/components/list/byoip/constants.js
@@ -1,3 +1,48 @@
 export const TRACKING_PREFIX = 'dedicated::ip';
 export const TRACKING_PREFIX_SLICE = `${TRACKING_PREFIX}::slice-ip-block`;
 export const TRACKING_PREFIX_AGGREGATE = `${TRACKING_PREFIX}::merge-ip-block`;
+export const BYOIP_USAGE_GUIDE_URL = {
+  DE:
+    'https://help.ovhcloud.com/csm/de-network-bring-your-own-ip?id=kb_article_view&sysparm_article=KB0044858',
+  ES:
+    'https://help.ovhcloud.com/csm/es-es-network-bring-your-own-ip?id=kb_article_view&sysparm_article=KB0031977',
+  FR:
+    'https://help.ovhcloud.com/csm/fr-network-bring-your-own-ip?id=kb_article_view&sysparm_article=KB0044867',
+  IE:
+    'https://help.ovhcloud.com/csm/en-ie-network-bring-your-own-ip?id=kb_article_view&sysparm_article=KB0044851',
+  IT:
+    'https://help.ovhcloud.com/csm/it-network-bring-your-own-ip?id=kb_article_view&sysparm_article=KB0044860',
+  NL:
+    'https://help.ovhcloud.com/csm/en-nl-documentation-network?id=kb_browse_cat&kb_id=74d98129a884a950f07829d7d5c7523d',
+  PL:
+    'https://help.ovhcloud.com/csm/pl-documentation-network?id=kb_browse_cat&kb_id=74d98129a884a950f07829d7d5c7523d',
+  PT:
+    'https://help.ovhcloud.com/csm/pt-documentation-network?id=kb_browse_cat&kb_id=74d98129a884a950f07829d7d5c7523d',
+  GB:
+    'https://help.ovhcloud.com/csm/en-gb-documentation-network?id=kb_browse_cat&kb_id=74d98129a884a950f07829d7d5c7523d',
+  CA:
+    'https://help.ovhcloud.com/csm/en-ca-documentation-network?id=kb_browse_cat&kb_id=74d98129a884a950f07829d7d5c7523d',
+  US: 'https://us.ovhcloud.com/support/',
+  MA:
+    'https://help.ovhcloud.com/csm/fr-ma-documentation-network?id=kb_browse_cat&kb_id=74d98129a884a950f07829d7d5c7523d',
+  SN:
+    'https://help.ovhcloud.com/csm/fr-sn-documentation-network?id=kb_browse_cat&kb_id=74d98129a884a950f07829d7d5c7523d',
+  TN:
+    'https://help.ovhcloud.com/csm/fr-tn-documentation-network?id=kb_browse_cat&kb_id=74d98129a884a950f07829d7d5c7523d',
+  AU:
+    'https://help.ovhcloud.com/csm/en-au-documentation-network?id=kb_browse_cat&kb_id=74d98129a884a950f07829d7d5c7523d',
+  IN:
+    'https://help.ovhcloud.com/csm/en-in-documentation-network?id=kb_browse_cat&kb_id=74d98129a884a950f07829d7d5c7523d',
+  SG:
+    'https://help.ovhcloud.com/csm/en-sg-documentation-network?id=kb_browse_cat&kb_id=74d98129a884a950f07829d7d5c7523d',
+  ASIA:
+    'https://help.ovhcloud.com/csm/asia-documentation-network?id=kb_browse_cat&kb_id=74d98129a884a950f07829d7d5c7523d',
+  DEFAULT:
+    'https://help.ovhcloud.com/csm/en-gb-documentation-network?id=kb_browse_cat&kb_id=74d98129a884a950f07829d7d5c7523d',
+};
+
+export default {
+  TRACKING_PREFIX_SLICE,
+  TRACKING_PREFIX_AGGREGATE,
+  BYOIP_USAGE_GUIDE_URL,
+};
