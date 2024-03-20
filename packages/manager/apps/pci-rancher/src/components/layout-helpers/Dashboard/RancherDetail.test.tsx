61494f430d04ecb655ba4982af77784d7fc0daaa
Lio <lionel95200@gmail.com>
2024-03-20 17:03:21 +0100

    fix(rancher): update bad quote in configure page MANAGER-13603
    Signed-off-by: Lio <lionel95200@gmail.com>

--- packages/manager/apps/pci-rancher/src/components/layout-helpers/Dashboard/RancherDetail.test.tsx
+++ packages/manager/apps/pci-rancher/src/components/layout-helpers/Dashboard/RancherDetail.test.tsx
@@ -23,6 +23,7 @@ const setupSpecTest = async (rancherService: RancherService = rancherMocked) =>
         generateAccesDetail={jest.fn()}
         accessDetail={null}
         hasErrorAccessDetail={false}
+        resetAccessDetail={jest.fn()}
       />,
     ),
   );
