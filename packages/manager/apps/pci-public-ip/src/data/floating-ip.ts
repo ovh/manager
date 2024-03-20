f3c9c60654c2f0882a0b5bd33c0fd4e132150c45
LIDRISSI Hamid <abdelghani-lidrissi.hamid.ext@ovhcloud.com>
2024-03-20 16:06:13 +0100

    feat(pci-public-ip): display the floating-ips datagrid page
    ref: DTCORE-2010
    
    Signed-off-by: LIDRISSI Hamid <abdelghani-lidrissi.hamid.ext@ovhcloud.com>

---
+++ packages/manager/apps/pci-public-ip/src/data/floating-ip.ts
@@ -0,0 +1,59 @@
+import { v6 } from '@ovh-ux/manager-core-api';
+import { PaginationState } from '@ovhcloud/manager-components';
+import { FloatingIP, Instance } from '@/interface';
+
+/**
+ * TODO: make it generic in the manager-components as hook
+ */
+export const paginateResults = (
+  items: FloatingIP[],
+  pagination: PaginationState,
+) => {
+  return {
+    rows: items.slice(
+      pagination.pageIndex * pagination.pageSize,
+      (pagination.pageIndex + 1) * pagination.pageSize,
+    ),
+    pageCount: Math.ceil(items.length / pagination.pageSize),
+    totalRows: items.length,
+  };
+};
+
+export const getAllFloatingIP = async (
+  projectId: string,
+): Promise<FloatingIP[]> => {
+  try {
+    const [
+      { data: instances },
+      {
+        data: { resources: floatingIPs },
+      },
+    ] = await Promise.all([
+      v6.get(`/cloud/project/${projectId}/instance`),
+      v6.get(`/cloud/project/${projectId}/aggregated/floatingip`),
+    ]);
+
+    const aggregatedData = floatingIPs.map((floatingIP) => {
+      const floatingIPResult = { ...floatingIP };
+      if (
+        floatingIP.associatedEntity &&
+        floatingIP.associatedEntity?.type === 'instance'
+      ) {
+        const instance: Instance = instances.find(
+          ({ id }) => id === floatingIP.associatedEntity.id,
+        );
+
+        floatingIPResult.associatedEntity.name = instance ? instance.name : '';
+      }
+
+      return { ...floatingIPResult };
+    });
+
+    return aggregatedData;
+  } catch (error) {
+    console.error('Error fetching floating IPs:', error);
+    throw error;
+  }
+};
+
+export default { getAllFloatingIP };
--- packages/manager/apps/pci-public-ip/src/routes.tsx
+++ packages/manager/apps/pci-public-ip/src/routes.tsx
@@ -1,3 +1,6 @@
+import queryClient from '@/queryClient';
+import { getProjectQuery } from '@/hooks/useProject';
+
 const lazyRouteConfig = (importFn: CallableFunction) => {
   return {
     lazy: async () => {
@@ -16,7 +19,29 @@ export default [
     path: '/',
     ...lazyRouteConfig(() => import('@/pages/Layout')),
   },
-  {},
+  {
+    id: 'users',
+    path: '/pci/projects/:projectId/public-ips',
+    loader: async ({ params }) => {
+      return queryClient.fetchQuery(getProjectQuery(params.projectId));
+    },
+    ...lazyRouteConfig(() => import('@/pages/Layout')),
+    children: [
+      {
+        path: '',
+        children: [
+          {
+            path: 'floating-ips',
+            ...lazyRouteConfig(() => import('@/pages/list')),
+          },
+          {
+            path: 'additional-ips',
+            ...lazyRouteConfig(() => import('@/pages/list')),
+          },
+        ],
+      },
+    ],
+  },
   {
     path: '*',
     element: <>Not found page</>,
