9832469d194928b2a45477e2dbf4d92f50acc0ba
Quentin Pavy <contact@quentinpavy.com>
2024-03-22 12:19:39 +0100

    feat(*): refactor action menu common component
    ref: MANAGER-14009
    
    Signed-off-by: Quentin Pavy <contact@quentinpavy.com>

---
+++ packages/manager-components/src/components/navigation/menus/action/action.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Meta } from '@storybook/react';
+import { OdsHTMLAnchorElementTarget } from '@ovhcloud/ods-common-core';
+import { ActionMenu, ActionMenuProps } from './action.component';
+
+const actionItems = [
+  {
+    id: 1,
+    href: 'https://ovhcloud.com',
+    target: OdsHTMLAnchorElementTarget._blank,
+    label: 'Action 1',
+  },
+  {
+    id: 2,
+    onClick: () => window.open('https://ovhcloud.com', '_blank', 'noopener'),
+    label: 'Action 2',
+  },
+];
+
+export const actionMenu: ActionMenuProps = {
+  items: actionItems,
+  isCompact: false,
+};
+
+const meta: Meta<ActionMenuProps> = {
+  title: 'Navigation/Menus',
+  component: ActionMenu,
+  args: actionMenu,
+};
+
+export default meta;
