16f9ab26a2aba686fc052b483991462ac86b089a
abullet33 <106544940+abullet33@users.noreply.github.com>
2024-03-22 10:33:54 +0100

    feat: databases analytics dashboard wip (#11260)
    * feat(pci.db): dashboard tab
    
    datatr-480
    
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>

---
+++ packages/manager/apps/pci-databases-analytics/src/hooks/api/maintenance.api.hooks.tsx
@@ -0,0 +1,53 @@
+import {
+  QueryObserverOptions,
+  UseQueryResult,
+  useMutation,
+  useQuery,
+} from '@tanstack/react-query';
+
+import { database } from '@/models/database';
+import { CdbError } from '@/api/databases';
+import {
+  ApplyMaintenanceProps,
+  applyMaintenance,
+  getMaintenance,
+} from '@/api/databases/maintenance';
+
+export function useGetMaintenances(
+  projectId: string,
+  engine: database.EngineEnum,
+  serviceId: string,
+  options: Omit<QueryObserverOptions, 'queryKey'> = {},
+) {
+  const queryKey = [projectId, 'database', engine, serviceId, 'maintenance'];
+  return useQuery({
+    queryKey,
+    queryFn: () => getMaintenance({ projectId, engine, serviceId }),
+    ...options,
+  }) as UseQueryResult<database.service.Maintenance[], Error>;
+}
+
+export interface MutateMaintenanceProps {
+  onError: (cause: CdbError) => void;
+  onSuccess: (maintenance: database.service.Maintenance) => void;
+}
+
+export function useApplyMaintenance({
+  onError,
+  onSuccess,
+}: MutateMaintenanceProps) {
+  const mutation = useMutation({
+    mutationFn: (mtInfo: ApplyMaintenanceProps) => {
+      return applyMaintenance(mtInfo);
+    },
+    onError,
+    onSuccess,
+  });
+
+  return {
+    applyMaintenance: (mtInfo: ApplyMaintenanceProps) => {
+      return mutation.mutate(mtInfo);
+    },
+    ...mutation,
+  };
+}
