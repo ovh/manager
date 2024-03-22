6f52d6c240cf6de84f7998525f5b0f0d59aae9db
brofik <10513484+brofik@users.noreply.github.com>
2024-03-22 09:38:43 +0100

    fix(pci.instances): update floating ip availability check (#11179)
    ref: TAPC-553
    
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>

--- packages/manager/modules/pci/src/projects/project/instances/instances.routing.js
+++ packages/manager/modules/pci/src/projects/project/instances/instances.routing.js
@@ -10,6 +10,7 @@ import {
 } from './instances.constants';
 import Instance from '../../../components/project/instance/instance.class';
 import { PCI_FEATURES } from '../../projects.constant';
+import { FLOATING_IP_HOURLY_PLAN_CODE } from '../additional-ips/additional-ips.constants';
 
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('pci.projects.project.instances', {
@@ -141,17 +142,17 @@ export default /* @ngInject */ ($stateProvider) => {
         coreConfig,
         projectId,
       ) => (region) => {
-        const product = 'floatingip';
+        const planCode = FLOATING_IP_HOURLY_PLAN_CODE;
         return $http
           .get(`/cloud/project/${projectId}/capabilities/productAvailability`, {
             params: {
-              product,
+              planCode,
               ovhSubsidiary: coreConfig.getUser().ovhSubsidiary,
             },
           })
-          .then(({ data: { products } }) => {
-            return products
-              .find(({ name }) => name === product)
+          .then(({ data: { plans } }) => {
+            return plans
+              .find(({ code }) => code === planCode)
               ?.regions.some(({ name }) => name === region);
           });
       },
