f88a9c1666289f359ead135063740df4f9a6e31c
Lio <lionel95200@gmail.com>
2024-03-25 16:00:09 +0100

    feat(rancher): add request tagger on each request page
    Signed-off-by: Lio <lionel95200@gmail.com>

--- packages/manager/apps/pci-rancher/src/routes.tsx
+++ packages/manager/apps/pci-rancher/src/routes.tsx
@@ -16,24 +16,38 @@ const lazyRouteConfig = (importFn: CallableFunction) => {
 
 export const COMMON_PATH = '/pci/projects';
 
+export enum Route {
+  Listing = 'listing',
+  Dashboard = 'dashboard',
+  Onboarding = 'onboarding',
+  New = 'New',
+}
+
+export const getRequestTaggerPciPath = (project: string, route?: Route) =>
+  `pci.projects.project.${project}${route ? `.${route}` : ''}`;
+
 export default [
   {
     path: '/pci/projects/:projectId',
     ...lazyRouteConfig(() => import('@/pages/layout')),
     children: [
       {
+        id: getRequestTaggerPciPath('rancher'),
         path: 'rancher',
         ...lazyRouteConfig(() => import('@/pages/')),
       },
       {
+        id: getRequestTaggerPciPath('rancher', Route.Onboarding),
         path: 'rancher/onboarding',
         ...lazyRouteConfig(() => import('@/pages/onboarding')),
       },
       {
+        id: getRequestTaggerPciPath('rancher', Route.New),
         path: 'rancher/new',
         ...lazyRouteConfig(() => import('@/pages/create')),
       },
       {
+        id: getRequestTaggerPciPath('rancher', Route.Dashboard),
         path: 'rancher/:rancherId',
         ...lazyRouteConfig(() => import('@/pages/dashboard/_layout')),
       },
