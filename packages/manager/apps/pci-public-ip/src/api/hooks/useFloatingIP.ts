441a679ccc4524b116508897ae7186ec58264d8a
seven-amid <lidrissi.hamid@gmail.com>
2024-03-25 14:38:05 +0100

    feat(pci-public-ip): display the floating-ips datagrid page
    ref: DTCORE-2010
    
    Signed-off-by: LIDRISSI Hamid <abdelghani-lidrissi.hamid.ext@ovhcloud.com>
    
    ---------
    
    Signed-off-by: LIDRISSI Hamid <abdelghani-lidrissi.hamid.ext@ovhcloud.com>
    Co-authored-by: LIDRISSI Hamid <abdelghani-lidrissi.hamid.ext@ovhcloud.com>

---
+++ packages/manager/apps/pci-public-ip/src/api/hooks/useFloatingIP.ts
@@ -0,0 +1,46 @@
+import { useQuery } from '@tanstack/react-query';
+import { useMemo } from 'react';
+import { PaginationState } from '@ovhcloud/manager-components';
+import { getAllFloatingIP, paginateResults } from '@/api/data/floating-ip';
+
+export interface ResponseAPIError {
+  message: string;
+  stack: string;
+  name: string;
+  code: string;
+  response?: {
+    headers?: {
+      [key: string]: string;
+      'x-ovh-queryid': string;
+    };
+    data?: {
+      message?: string;
+    };
+  };
+}
+
+export type FloatingIPOptions = {
+  pagination: PaginationState;
+};
+
+export const useAllFloatingIP = (projectId: string) => {
+  return useQuery({
+    queryKey: ['project', projectId, 'floatingIps', 'instance'],
+    queryFn: () => getAllFloatingIP(projectId),
+  });
+};
+
+export const useFLoatingIPs = (
+  projectId: string,
+  { pagination }: FloatingIPOptions,
+) => {
+  const { data, error, isLoading } = useAllFloatingIP(projectId);
+
+  return useMemo(() => {
+    return {
+      isLoading,
+      error,
+      data: paginateResults(data || [], pagination),
+    };
+  }, [data, pagination]);
+};
