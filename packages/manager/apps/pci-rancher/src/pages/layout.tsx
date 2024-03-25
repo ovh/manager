82f0e90736b066ebe1a2f0be32960583412538c9
Lio <lionel95200@gmail.com>
2024-03-25 16:00:09 +0100

    feat(rancher): add request tagger on each request page
    ref: MANAGER-14030
    
    Signed-off-by: Lio <lionel95200@gmail.com>

--- packages/manager/apps/pci-rancher/src/pages/layout.tsx
+++ packages/manager/apps/pci-rancher/src/pages/layout.tsx
@@ -1,10 +1,29 @@
-import React, { useContext, useEffect } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
 import { ShellContext } from '@ovh-ux/manager-react-shell-client';
+import { defineCurrentPage } from '@ovh-ux/request-tagger';
+import React, { useContext, useEffect } from 'react';
+import { Outlet, useLocation, useMatches } from 'react-router-dom';
+
+interface MatchHandle {
+  tracking?: {
+    pageName?: string;
+  };
+}
+
+interface Match {
+  handle?: MatchHandle;
+}
 
 function RoutingSynchronisation() {
   const location = useLocation();
   const { shell } = useContext(ShellContext);
+
+  const matches = useMatches();
+
+  useEffect(() => {
+    const [match] = matches.slice(-1) as Match[];
+    //  We cannot type properly useMatches cause it's not support type inference or passing specific type https://github.com/remix-run/react-router/discussions/10902
+    defineCurrentPage(`app.pci-rancher.${match?.handle?.tracking?.pageName}`);
+  }, [location]);
 
   useEffect(() => {
     // Need to also hide the preloader here due to firefox still display it when cache is disabled
