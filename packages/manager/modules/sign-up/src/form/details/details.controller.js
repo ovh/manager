25f0cabd55dbe22761a9a263e4484741262daaf3
Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>
2024-03-25 15:37:54 +0100

    feat(signup): modification of the field related to language
    add a boolean to retrieve the behavior on country change
    ref: MANAGER-12955
    
    Signed-off-by: Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>

--- packages/manager/modules/sign-up/src/form/details/details.controller.js
+++ packages/manager/modules/sign-up/src/form/details/details.controller.js
@@ -78,10 +78,13 @@ export default class SignUpDetailsCtrl {
     SignUpDetailsCtrl.setElementFocus(fieldName);
   }
 
-  preselectLanguage() {
+  preselectLanguage(isOnChange) {
     if (get(this.signUpFormCtrl, 'rules.language.in.length', 0) === 1) {
       const [uniqueLanguage] = this.signUpFormCtrl.rules.language.in;
       this.signUpFormCtrl.model.language = uniqueLanguage.value;
+    }
+    if (isOnChange) {
+      this.signUpFormCtrl.model.language = null;
     }
   }
 
@@ -114,7 +117,7 @@ export default class SignUpDetailsCtrl {
         this.signUpFormCtrl.model.phoneCountry = this.signUpFormCtrl.model.country;
       }
 
-      this.preselectLanguage();
+      this.preselectLanguage(true);
       this.refocusOnField('zip');
     });
   }
@@ -248,7 +251,7 @@ export default class SignUpDetailsCtrl {
       );
     }
 
-    this.preselectLanguage();
+    this.preselectLanguage(false);
   }
 
   static setElementFocus(elementName) {
