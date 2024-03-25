f37b1068bab7ebf7c4b91484bd5c6460a9ca034a
Yoann Fievez <yoann.fievez@ovhcloud.com>
2024-03-25 14:32:44 +0100

    fix(pci-users): css issues on mobile version ref: DTCORE-2035
    Signed-off-by: Yoann Fievez <yoann.fievez@ovhcloud.com>

--- packages/manager/core/tailwind-config/index.js
+++ packages/manager/core/tailwind-config/index.js
@@ -1,7 +1,22 @@
 const config = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        0: '0',
+        1: 'var(--ods-size-01)',
+        2: 'var(--ods-size-02)',
+        3: 'var(--ods-size-03)',
+        4: 'var(--ods-size-04)',
+        5: 'var(--ods-size-05)',
+        6: 'var(--ods-size-06)',
+        7: 'var(--ods-size-07)',
+        8: 'var(--ods-size-08)',
+        9: 'var(--ods-size-09)',
+        10: 'var(--ods-size-10)',
+        11: 'var(--ods-size-11)',
+      },
+    },
     screens: {
       xs: '0',
       sm: '36em',
@@ -11,20 +26,6 @@ const config = {
       xxl: '87.5em',
       // not work with ods var in media queries
       // 'xs': 'var(--ods-breakpoint-xs)',
-    },
-    spacing: {
-      0: '0',
-      1: 'var(--ods-size-01)',
-      2: 'var(--ods-size-02)',
-      3: 'var(--ods-size-03)',
-      4: 'var(--ods-size-04)',
-      5: 'var(--ods-size-05)',
-      6: 'var(--ods-size-06)',
-      7: 'var(--ods-size-07)',
-      8: 'var(--ods-size-08)',
-      9: 'var(--ods-size-09)',
-      10: 'var(--ods-size-10)',
-      11: 'var(--ods-size-11)',
     },
     borderRadius: {
       none: '0',
