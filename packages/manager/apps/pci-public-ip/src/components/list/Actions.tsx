271280b288900d3d5c141dd0d626818e38dfd66e
seven-amid <lidrissi.hamid@gmail.com>
2024-03-25 14:38:05 +0100

    Feat/DTCORE-2010 (#11279)
    * feat(pci-public-ip): display the floating-ips datagrid page
    
    ref: DTCORE-2010
    
    Signed-off-by: LIDRISSI Hamid <abdelghani-lidrissi.hamid.ext@ovhcloud.com>
    
    ---------
    
    Signed-off-by: LIDRISSI Hamid <abdelghani-lidrissi.hamid.ext@ovhcloud.com>
    Co-authored-by: LIDRISSI Hamid <abdelghani-lidrissi.hamid.ext@ovhcloud.com>

---
+++ packages/manager/apps/pci-public-ip/src/components/list/Actions.tsx
@@ -0,0 +1,32 @@
+import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
+import {
+  ODS_BUTTON_VARIANT,
+  ODS_ICON_NAME,
+  ODS_ICON_SIZE,
+} from '@ovhcloud/ods-components';
+import {
+  OsdsButton,
+  OsdsIcon,
+  OsdsMenu,
+  OsdsMenuItem,
+} from '@ovhcloud/ods-components/react';
+
+export default function Actions() {
+  return (
+    <OsdsMenu>
+      <OsdsButton
+        slot={'menu-title'}
+        circle
+        color={ODS_THEME_COLOR_INTENT.primary}
+        variant={ODS_BUTTON_VARIANT.stroked}
+      >
+        <OsdsIcon
+          name={ODS_ICON_NAME.ELLIPSIS}
+          color={ODS_THEME_COLOR_INTENT.primary}
+          size={ODS_ICON_SIZE.xxs}
+        ></OsdsIcon>
+      </OsdsButton>
+      <OsdsMenuItem></OsdsMenuItem>
+    </OsdsMenu>
+  );
+}
