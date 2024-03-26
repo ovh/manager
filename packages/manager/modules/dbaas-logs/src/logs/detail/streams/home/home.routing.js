e22df3057d1a9d46c7a373d63c4f58818eca8f1a
Amandine Dectot <amandine.dectot.ext@corp.ovh.com>
2024-03-26 14:49:29 +0100

    feat(dedicated.dbaas-logs): tracking implementation
    ref: MANAGER-13920
    
    Signed-off-by: Amandine Dectot <amandine.dectot.ext@corp.ovh.com>

--- packages/manager/modules/dbaas-logs/src/logs/detail/streams/home/home.routing.js
+++ packages/manager/modules/dbaas-logs/src/logs/detail/streams/home/home.routing.js
@@ -1,8 +1,13 @@
+import LogConstants from '../../../logs-constants';
+
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('dbaas-logs.detail.streams.home', {
     url: '/home',
     views: {
       logsStreams: 'dbaasLogsDetailStreamsHome',
+    },
+    atInternet: {
+      rename: `${LogConstants.TRACKING_PREFIX}::ldp::listing::data_flow`,
     },
     resolve: {
       breadcrumb: () => null,
--- packages/manager/modules/dbaas-logs/src/logs/detail/streams/subscriptions/subscriptions.routing.js
+++ packages/manager/modules/dbaas-logs/src/logs/detail/streams/subscriptions/subscriptions.routing.js
@@ -1,7 +1,12 @@
+import LogConstants from '../../../logs-constants';
+
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('dbaas-logs.detail.streams.stream.subscriptions', {
     url: '/subscriptions',
     component: 'dbaasLogsDetailStreamsSubscriptions',
+    atInternet: {
+      rename: `${LogConstants.TRACKING_PREFIX}::ldp::listing::data_flow::subscriptions`,
+    },
     resolve: {
       breadcrumb: /* @ngInject */ ($translate) =>
         $translate.instant('dbaas_logs_streams_subscriptions'),
