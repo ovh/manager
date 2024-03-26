ea53c1561ef540495226d99b9e6116e34772dcdf
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-03-26 18:44:22 +0100

    feat: advanced configuration (wip)
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/apps/pci-databases-analytics/src/pages/services/[serviceId]/settings/update/index.tsx
+++ packages/manager/apps/pci-databases-analytics/src/pages/services/[serviceId]/settings/update/index.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useMemo, useState } from 'react';
-import { useLocation } from 'react-router-dom';
 import BreadcrumbItem from '@/components/Breadcrumb/BreadcrumbItem';
 import { database } from '@/models/database';
 import UpdateForm from './_components/update-form';
