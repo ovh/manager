94bec3a980e963bc9895abe32bb76935c4387b58
Yoann Fievez <yoann.fievez@ovhcloud.com>
2024-03-27 09:45:05 +0100

    feat(pci-public-ip): add terminate floating ip modal ref: DTCORE-2015
    Signed-off-by: Yoann Fievez <yoann.fievez@ovhcloud.com>

--- packages/manager/apps/pci-public-ip/src/api/hooks/useFloatingIP.ts
+++ packages/manager/apps/pci-public-ip/src/api/hooks/useFloatingIP.ts
@@ -1,7 +1,13 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { useMemo } from 'react';
 import { PaginationState } from '@ovhcloud/manager-components';
-import { getAllFloatingIP, paginateResults } from '@/api/data/floating-ip';
+import {
+  getAllFloatingIP,
+  paginateResults,
+  terminateFloatingIP,
+} from '@/api/data/floating-ip';
+import { FloatingIP } from '@/interface';
+import queryClient from '@/queryClient';
 
 export interface ResponseAPIError {
   message: string;
@@ -23,11 +29,23 @@ export type FloatingIPOptions = {
   pagination: PaginationState;
 };
 
+const getQueryKeyFloatingIPs = (projectId: string) => [
+  'project',
+  projectId,
+  'floatingIps',
+  'instance',
+];
+
 export const useAllFloatingIP = (projectId: string) => {
   return useQuery({
-    queryKey: ['project', projectId, 'floatingIps', 'instance'],
+    queryKey: getQueryKeyFloatingIPs(projectId),
     queryFn: () => getAllFloatingIP(projectId),
   });
+};
+
+export const useFloatingIP = (projectId: string, ipId: string): FloatingIP => {
+  const { data: floatingIPs } = useAllFloatingIP(projectId);
+  return floatingIPs.find((floatingIP) => floatingIP.id === ipId) || undefined;
 };
 
 export const useFLoatingIPs = (
@@ -43,4 +61,41 @@ export const useFLoatingIPs = (
       data: paginateResults(data || [], pagination),
     };
   }, [data, pagination]);
+};
+
+type TerminateFloatingIPProps = {
+  projectId: string;
+  ipId: string;
+  onError: (cause: Error) => void;
+  onSuccess: () => void;
+};
+
+export const useTerminateFloatingIP = ({
+  projectId,
+  ipId,
+  onError,
+  onSuccess,
+}: TerminateFloatingIPProps) => {
+  const mutation = useMutation({
+    mutationFn: (floatingIp: FloatingIP) => {
+      return terminateFloatingIP(projectId, floatingIp.region, floatingIp.id);
+    },
+    onError,
+    onSuccess: () => {
+      queryClient.setQueryData(
+        getQueryKeyFloatingIPs(projectId),
+        (queryClient.getQueryData(getQueryKeyFloatingIPs(projectId)) as Array<
+          FloatingIP
+        >).filter((floatingIp) => `${floatingIp.id}` !== ipId),
+      );
+      return onSuccess();
+    },
+  });
+
+  return {
+    terminate: (floatingIP: FloatingIP) => {
+      return mutation.mutate(floatingIP);
+    },
+    ...mutation,
+  };
 };
