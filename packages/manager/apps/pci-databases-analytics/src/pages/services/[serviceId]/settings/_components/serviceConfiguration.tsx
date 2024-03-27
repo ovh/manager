a7b1c9c5204f113b1c9e00aa3bf1adb4a31346e6
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-03-27 15:53:36 +0100

    feat: update maintenance and backup time
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

---
+++ packages/manager/apps/pci-databases-analytics/src/pages/services/[serviceId]/settings/_components/serviceConfiguration.tsx
@@ -0,0 +1,125 @@
+import { Pen } from 'lucide-react';
+import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table';
+import { useServiceData } from '../../layout';
+import { Button } from '@/components/ui/button';
+import { useModale } from '@/hooks/useModale';
+import RenameService from '../../_components/renameService';
+import { useUpdateService } from '@/hooks/api/services.api.hooks';
+import { useToast } from '@/components/ui/use-toast';
+import TimeUpdate from './serviceConfiguration/timeUpdate';
+
+const ServiceConfiguration = () => {
+  const renameModale = useModale('rename');
+  const { service, projectId, serviceQuery } = useServiceData();
+  const toast = useToast();
+  const { updateService } = useUpdateService({
+    onError: (err) => {
+      toast.toast({
+        title:
+          'Une erreur est survenue lors de la mise à jour de votre service',
+        variant: 'destructive',
+        description: err.response.data.message,
+      });
+    },
+    onSuccess: () => {
+      toast.toast({
+        title: 'Succès',
+        description: 'La configuration de votre service a été mis à jour',
+      });
+      serviceQuery.refetch();
+    },
+  });
+
+  function convertTimeToDateTime(timeString: string): Date {
+    // Create a new date object for the current date
+    const now = new Date();
+    // Split the time string into its components
+    const [hours, minutes, seconds] = timeString.split(':').map(Number);
+    // Set the time for the date object
+    now.setHours(hours);
+    now.setMinutes(minutes);
+    now.setSeconds(seconds);
+    // Return the modified date object
+    return now;
+  }
+
+  const onMaintenanceTimeSubmit = (maintenanceTime: Date) => {
+    updateService({
+      engine: service.engine,
+      projectId,
+      serviceId: service.id,
+      data: {
+        maintenanceTime: maintenanceTime.toLocaleTimeString('en-US', {
+          hour12: false,
+        }),
+      },
+    });
+  };
+  const onBackupTimeSubmit = (backupTime: Date) => {
+    updateService({
+      engine: service.engine,
+      projectId,
+      serviceId: service.id,
+      data: {
+        backups: {
+          time: backupTime.toLocaleTimeString('en-US', { hour12: false }),
+        },
+      },
+    });
+  };
+
+  return (
+    <>
+      <Table>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-semibold">Name</TableCell>
+            <TableCell>{service.description}</TableCell>
+            <TableCell className="text-right">
+              <Button
+                variant="ghost"
+                size="table"
+                className="py-0 h-auto"
+                onClick={() => renameModale.open()}
+              >
+                <Pen />
+              </Button>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-semibold">Maintenance time</TableCell>
+            <TimeUpdate
+              initialValue={convertTimeToDateTime(service.maintenanceTime)}
+              onSubmit={onMaintenanceTimeSubmit}
+            />
+          </TableRow>
+          {service.backups?.time && (
+            <TableRow>
+              <TableCell className="font-semibold">Backup time</TableCell>
+              <TimeUpdate
+                initialValue={convertTimeToDateTime(service.backups.time)}
+                onSubmit={onBackupTimeSubmit}
+              />
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+      <Button
+        variant="destructive"
+        className="w-full bg-background border-2 hover:bg-destructive/10 font-semibold border-destructive text-destructive"
+      >
+        Delete my service
+      </Button>
+      <RenameService
+        controller={renameModale.controller}
+        service={service}
+        onSuccess={() => {
+          renameModale.close();
+          serviceQuery.refetch();
+        }}
+      />
+    </>
+  );
+};
+
+export default ServiceConfiguration;
--- packages/manager/apps/pci-databases-analytics/src/pages/services/[serviceId]/settings/index.tsx
+++ packages/manager/apps/pci-databases-analytics/src/pages/services/[serviceId]/settings/index.tsx
@@ -1,67 +1,20 @@
 import { AccordionItem } from '@radix-ui/react-accordion';
-import { Pen, X } from 'lucide-react';
-import { useState } from 'react';
-import { Link } from '@/components/links';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
 import { useServiceData } from '../layout';
-import { humanizeEngine } from '@/lib/engineNameHelper';
-import { formatStorage } from '@/lib/bytesHelper';
-import { useGetAvailabilities } from '@/hooks/api/availabilities.api.hooks';
-import { database } from '@/models/database';
-import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table';
-import { Button } from '@/components/ui/button';
-import { TimePicker } from '@/components/ui/time-picker';
-import { Label } from '@/components/ui/label';
 import Maintenances from './_components/maintenances';
 import IpsRestrictionsUpdate from './_components/ipRestrictions';
-import RenameService from '../_components/renameService';
-import { useModale } from '@/hooks/useModale';
 import AdvancedConfigurationUpdate from './_components/advancedConfiguration';
 import {
   Accordion,
   AccordionContent,
   AccordionTrigger,
 } from '@/components/ui/accordion';
-import FormattedDate from '@/components/table-date';
+import UpdateTable from './_components/updateTable';
+import ServiceConfiguration from './_components/serviceConfiguration';
 
 const Settings = () => {
-  const renameModale = useModale('rename');
-  const [updateMaintenanceTime, setUpdateMaintenanceTime] = useState(false);
-  const [updateBackupTime, setUpdateBackupTime] = useState(false);
-  const { service, projectId, serviceQuery } = useServiceData();
-  const availabilitiesVersionQuery = useGetAvailabilities(
-    projectId,
-    service.id,
-    database.availability.ActionEnum.update,
-    database.availability.TargetEnum.version,
-  );
-  const availabilitiesPlanQuery = useGetAvailabilities(
-    projectId,
-    service.id,
-    database.availability.ActionEnum.update,
-    database.availability.TargetEnum.plan,
-  );
-  const availabilitiesFlavorQuery = useGetAvailabilities(
-    projectId,
-    service.id,
-    database.availability.ActionEnum.update,
-    database.availability.TargetEnum.flavor,
-  );
-  function convertTimeToDateTime(timeString: string): Date {
-    // Create a new date object for the current date
-    const now = new Date();
+  const { service } = useServiceData();
 
-    // Split the time string into its components
-    const [hours, minutes, seconds] = timeString.split(':').map(Number);
-
-    // Set the time for the date object
-    now.setHours(hours);
-    now.setMinutes(minutes);
-    now.setSeconds(seconds);
-
-    // Return the modified date object
-    return now;
-  }
   return (
     <>
       <h3>Settings</h3>
@@ -71,122 +24,7 @@ const Settings = () => {
             <h5>Upgrade your service</h5>
           </CardHeader>
           <CardContent>
-            <Table>
-              <TableBody>
-                <TableRow>
-                  <TableCell className="font-semibold">Version</TableCell>
-                  <TableCell>
-                    {humanizeEngine(service.engine)} {service.version}
-                  </TableCell>
-                  {availabilitiesVersionQuery.data?.length > 1 && (
-                    <TableCell className="text-right">
-                      <Button
-                        variant="default"
-                        size="default"
-                        className="py-0 h-auto"
-                        asChild
-                      >
-                        <Link
-                          className="hover:no-underline hover:text-white text-sm font-semibold"
-                          to={'update?target=version'}
-                        >
-                          Update
-                        </Link>
-                      </Button>
-                    </TableCell>
-                  )}
-                </TableRow>
-                <TableRow>
-                  <TableCell className="font-semibold">Plan</TableCell>
-                  <TableCell>{service.plan}</TableCell>
-                  {availabilitiesPlanQuery.data?.length > 1 && (
-                    <TableCell className="text-right">
-                      <Button
-                        variant="default"
-                        size="default"
-                        className="py-0 h-auto"
-                        asChild
-                      >
-                        <Link
-                          className="hover:no-underline hover:text-white text-sm font-semibold"
-                          to={'update?target=plan'}
-                        >
-                          Update
-                        </Link>
-                      </Button>
-                    </TableCell>
-                  )}
-                </TableRow>
-                <TableRow>
-                  <TableCell className="font-semibold">Flavor</TableCell>
-                  <TableCell>{service.flavor}</TableCell>
-                  {availabilitiesFlavorQuery.data?.length > 1 && (
-                    <TableCell className="text-right">
-                      <Button
-                        variant="default"
-                        size="default"
-                        className="py-0 h-auto"
-                        asChild
-                      >
-                        <Link
-                          className="hover:no-underline hover:text-white text-sm font-semibold"
-                          to={'update?target=flavor'}
-                        >
-                          Update
-                        </Link>
-                      </Button>
-                    </TableCell>
-                  )}
-                </TableRow>
-                {service.storage?.size.value > 0 && (
-                  <TableRow>
-                    <TableCell className="font-semibold">Storage</TableCell>
-                    <TableCell>
-                      {formatStorage(service.storage.size)}{' '}
-                      {service.storage.type}
-                    </TableCell>
-                    {availabilitiesFlavorQuery.data?.length > 1 && (
-                      <TableCell className="text-right">
-                        <Button
-                          variant="default"
-                          size="default"
-                          className="py-0 h-auto"
-                          asChild
-                        >
-                          <Link
-                            className="hover:no-underline hover:text-white text-sm font-semibold"
-                            to={'update?target=flavor'}
-                          >
-                            Update
-                          </Link>
-                        </Button>
-                      </TableCell>
-                    )}
-                  </TableRow>
-                )}
-                <TableRow>
-                  <TableCell className="font-semibold">Nodes</TableCell>
-                  <TableCell>{service.nodes.length}</TableCell>
-                  {availabilitiesFlavorQuery.data?.length > 1 && (
-                    <TableCell className="text-right">
-                      <Button
-                        variant="default"
-                        size="default"
-                        className="py-0 h-auto"
-                        asChild
-                      >
-                        <Link
-                          className="hover:no-underline hover:text-white text-sm font-semibold"
-                          to={'update?target=flavor'}
-                        >
-                          Update
-                        </Link>
-                      </Button>
-                    </TableCell>
-                  )}
-                </TableRow>
-              </TableBody>
-            </Table>
+            <UpdateTable />
           </CardContent>
         </Card>
 
@@ -213,86 +51,7 @@ const Settings = () => {
             <h5>Configure your service</h5>
           </CardHeader>
           <CardContent className="flex flex-col gap-2">
-            <Table>
-              <TableBody>
-                <TableRow>
-                  <TableCell className="font-semibold">Name</TableCell>
-                  <TableCell>{service.description}</TableCell>
-                  <TableCell className="text-right">
-                    <Button
-                      variant="ghost"
-                      size="table"
-                      className="py-0 h-auto"
-                      onClick={() => renameModale.open()}
-                    >
-                      <Pen />
-                    </Button>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell className="font-semibold">
-                    Maintenance time
-                  </TableCell>
-                  <TableCell>
-                    {updateMaintenanceTime ? (
-                      <TimePicker
-                        date={convertTimeToDateTime(service.maintenanceTime)}
-                        setDate={() => {}}
-                      />
-                    ) : (
-                      <FormattedDate
-                        date={convertTimeToDateTime(service.maintenanceTime)}
-                        options={{ timeStyle: 'medium' }}
-                      />
-                    )}
-                  </TableCell>
-                  <TableCell className="text-right">
-                    <Button
-                      variant="ghost"
-                      size="table"
-                      className="py-0 h-auto"
-                      onClick={() => setUpdateMaintenanceTime((prev) => !prev)}
-                    >
-                      {updateMaintenanceTime ? <X /> : <Pen />}
-                    </Button>
-                  </TableCell>
-                </TableRow>
-                {service.backups?.time && (
-                  <TableRow>
-                    <TableCell className="font-semibold">Backup time</TableCell>
-                    <TableCell>
-                      {updateBackupTime ? (
-                        <TimePicker
-                          date={convertTimeToDateTime(service.backups.time)}
-                          setDate={() => {}}
-                        />
-                      ) : (
-                        <FormattedDate
-                          date={convertTimeToDateTime(service.backups.time)}
-                          options={{ timeStyle: 'medium' }}
-                        />
-                      )}
-                    </TableCell>
-                    <TableCell className="text-right">
-                      <Button
-                        variant="ghost"
-                        size="table"
-                        className="py-0 h-auto"
-                        onClick={() => setUpdateBackupTime((prev) => !prev)}
-                      >
-                        {updateBackupTime ? <X /> : <Pen />}
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                )}
-              </TableBody>
-            </Table>
-            <Button
-              variant="destructive"
-              className="w-full bg-background border-2 hover:bg-destructive/10 font-semibold border-destructive text-destructive"
-            >
-              Delete my service
-            </Button>
+            <ServiceConfiguration />
           </CardContent>
         </Card>
         {service.capabilities.advancedConfiguration && (
@@ -314,15 +73,6 @@ const Settings = () => {
           </Card>
         )}
       </div>
-
-      <RenameService
-        controller={renameModale.controller}
-        service={service}
-        onSuccess={() => {
-          renameModale.close();
-          serviceQuery.refetch();
-        }}
-      />
     </>
   );
 };
