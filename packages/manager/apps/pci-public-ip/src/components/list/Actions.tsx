b05e5abf2bac9344848451563cab15d03d330bc4
Yoann Fievez <yoann.fievez@ovhcloud.com>
2024-03-27 09:45:05 +0100

    feat(pci-public-ip): add terminate floating ip modal ref: DTCORE-2015
    Signed-off-by: Yoann Fievez <yoann.fievez@ovhcloud.com>

--- packages/manager/apps/pci-public-ip/src/components/list/Actions.tsx
+++ packages/manager/apps/pci-public-ip/src/components/list/Actions.tsx
@@ -1,17 +1,30 @@
-import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
 import {
+  ODS_THEME_COLOR_INTENT,
+  ODS_THEME_TYPOGRAPHY_SIZE,
+} from '@ovhcloud/ods-common-theming';
+import {
+  ODS_BUTTON_SIZE,
   ODS_BUTTON_VARIANT,
   ODS_ICON_NAME,
   ODS_ICON_SIZE,
+  ODS_TEXT_LEVEL,
 } from '@ovhcloud/ods-components';
 import {
   OsdsButton,
   OsdsIcon,
   OsdsMenu,
   OsdsMenuItem,
+  OsdsText,
 } from '@ovhcloud/ods-components/react';
+import { useTranslation } from 'react-i18next';
+import { useHref } from 'react-router-dom';
 
-export default function Actions() {
+type ActionProps = {
+  ipId: string;
+};
+export default function Actions({ ipId }: Readonly<ActionProps>) {
+  const { t } = useTranslation();
+  const hrefRemove = useHref(`./${ipId}/terminate`);
   return (
     <OsdsMenu>
       <OsdsButton
@@ -26,7 +39,23 @@ export default function Actions() {
           size={ODS_ICON_SIZE.xxs}
         ></OsdsIcon>
       </OsdsButton>
-      <OsdsMenuItem></OsdsMenuItem>
+      <OsdsMenuItem>
+        <OsdsButton
+          size={ODS_BUTTON_SIZE.sm}
+          variant={ODS_BUTTON_VARIANT.ghost}
+          color={ODS_THEME_COLOR_INTENT.primary}
+          href={hrefRemove}
+        >
+          <OsdsText
+            size={ODS_THEME_TYPOGRAPHY_SIZE._500}
+            level={ODS_TEXT_LEVEL.button}
+            color={ODS_THEME_COLOR_INTENT.primary}
+            slot={'start'}
+          >
+            {t('pci_additional_ips_floating_ip_grid_delete')}
+          </OsdsText>
+        </OsdsButton>
+      </OsdsMenuItem>
     </OsdsMenu>
   );
 }
