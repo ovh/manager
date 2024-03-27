629a9b1f9c9534fa397fef5c2f9d6918fe3e850e
Florian Renaut <florian.renaut@corp.ovh.com>
2024-03-27 13:36:49 +0100

    feat(pci-public-ip): add network security action
    ref: DTCORE-2014
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

---
+++ packages/manager/apps/pci-public-ip/src/components/list/NetworkSecurityAction.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import {
+  ODS_THEME_COLOR_INTENT,
+  ODS_THEME_TYPOGRAPHY_SIZE,
+} from '@ovhcloud/ods-common-theming';
+import {
+  ODS_BUTTON_SIZE,
+  ODS_BUTTON_VARIANT,
+  ODS_TEXT_LEVEL,
+} from '@ovhcloud/ods-components';
+import { OsdsButton, OsdsText } from '@ovhcloud/ods-components/react';
+import { useTranslation } from 'react-i18next';
+import { useNavigation } from '@ovh-ux/manager-react-shell-client';
+
+export const NetworkSecurityAction = ({ projectId }: { projectId: string }) => {
+  const { t } = useTranslation();
+  const [url, setUrl] = useState('');
+  const nav = useNavigation();
+
+  useEffect(() => {
+    nav.getURL('dedicated', `#/ip?serviceName=${projectId}`, {}).then(setUrl);
+  }, []);
+
+  return (
+    <OsdsButton
+      size={ODS_BUTTON_SIZE.sm}
+      variant={ODS_BUTTON_VARIANT.ghost}
+      color={ODS_THEME_COLOR_INTENT.primary}
+      href={url}
+    >
+      <OsdsText
+        size={ODS_THEME_TYPOGRAPHY_SIZE._500}
+        level={ODS_TEXT_LEVEL.button}
+        color={ODS_THEME_COLOR_INTENT.primary}
+        slot={'start'}
+      >
+        {t('pci_additional_ips_floating_ip_grid_manage_network_security')}
+      </OsdsText>
+    </OsdsButton>
+  );
+};
