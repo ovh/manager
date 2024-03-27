8cfb7ef7a89a576218d274a24ed9ff7f65d52d80
Florian Renaut <florian.renaut@corp.ovh.com>
2024-03-27 17:24:03 +0100

    fix(pci-users): token generation error message
    ref: DTCORE-2059
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/apps/pci-users/src/pages/token/generate/GenerateOpenStackTokenPage.tsx
+++ packages/manager/apps/pci-users/src/pages/token/generate/GenerateOpenStackTokenPage.tsx
@@ -26,6 +26,7 @@ export default function GenerateOpenStackTokenPage() {
                 user: user?.username,
               })}
             </>,
+            true,
           );
         }}
       />
