ab51025c8dd64013216f8840daaedd75934d40fc
stif59100 <steeve.vanderstocken@ovhcloud.com>
2024-03-27 10:09:27 +0100

    feat(zimbra): refacto after correcting on master
    ref:MANAGER-13039
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

--- packages/components/ovh-shell/src/index.ts
+++ packages/components/ovh-shell/src/index.ts
@@ -2,7 +2,6 @@ import DirectClientMessageBus from './message-bus/direct-client';
 import IFrameMessageBus from './message-bus/iframe';
 
 export { default as initShellClient } from './client';
-export { ShellClientApi } from './client/api';
 export { default as plugin } from './plugin';
 export { default as Shell } from './shell/shell';
 export { initShell } from './shell';
