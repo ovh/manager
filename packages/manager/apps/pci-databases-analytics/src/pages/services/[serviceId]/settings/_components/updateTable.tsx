ce887cd58099dd402ed7f0c99d80aedae42829b3
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-03-28 11:29:50 +0100

    feat: use modals for update
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/apps/pci-databases-analytics/src/pages/services/[serviceId]/settings/_components/updateTable.tsx
+++ packages/manager/apps/pci-databases-analytics/src/pages/services/[serviceId]/settings/_components/updateTable.tsx
@@ -1,14 +1,28 @@
+import { MinusCircle, PlusCircle } from 'lucide-react';
 import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table';
 import { humanizeEngine } from '@/lib/engineNameHelper';
 import { database } from '@/models/database';
 import { useServiceData } from '../../layout';
-import { useGetAvailabilities } from '@/hooks/api/availabilities.api.hooks';
+import {
+  useGetAvailabilities,
+  useGetFullCapabilities,
+} from '@/hooks/api/availabilities.api.hooks';
 import { Button } from '@/components/ui/button';
 import { Link } from '@/components/links';
 import { formatStorage } from '@/lib/bytesHelper';
+import { useModale } from '@/hooks/useModale';
+import UpdateVersion from '../update/_components/modals/updateVersion';
+import UpdatePlan from '../update/_components/modals/updatePlan';
+import UpdateFlavor from '../update/_components/modals/updateFlavor';
+import AddNode from '../update/_components/modals/addNode';
+import DeleteNode from '../update/_components/modals/removeNode';
+import UpdateStorage from '../update/_components/modals/updateStorage';
+import { useGetCatalog } from '@/hooks/api/catalog.api.hooks';
 
 const UpdateTable = () => {
   const { service, projectId } = useServiceData();
+  const catalog = useGetCatalog();
+  const capabilities = useGetFullCapabilities(projectId);
   const availabilitiesVersionQuery = useGetAvailabilities(
     projectId,
     service.id,
@@ -27,122 +41,159 @@ const UpdateTable = () => {
     database.availability.ActionEnum.update,
     database.availability.TargetEnum.flavor,
   );
+  const updateVersionModal = useModale('update-version');
+  const updatePlanModal = useModale('update-plan');
+  const updateFlavorModal = useModale('update-flavor');
+  const updateStorageModal = useModale('update-storage');
+  const addNode = useModale('add-node');
+  const deleteNode = useModale('delete-node');
+
   return (
-    <Table>
-      <TableBody>
-        <TableRow>
-          <TableCell className="font-semibold">Version</TableCell>
-          <TableCell>
-            {humanizeEngine(service.engine)} {service.version}
-          </TableCell>
-          {availabilitiesVersionQuery.data?.length > 1 && (
-            <TableCell className="text-right">
-              <Button
-                variant="default"
-                size="default"
-                className="py-0 h-auto"
-                asChild
-              >
-                <Link
-                  className="hover:no-underline hover:text-white text-sm font-semibold"
-                  to={'update?target=version'}
-                >
-                  Update
-                </Link>
-              </Button>
-            </TableCell>
-          )}
-        </TableRow>
-        <TableRow>
-          <TableCell className="font-semibold">Plan</TableCell>
-          <TableCell>{service.plan}</TableCell>
-          {availabilitiesPlanQuery.data?.length > 1 && (
-            <TableCell className="text-right">
-              <Button
-                variant="default"
-                size="default"
-                className="py-0 h-auto"
-                asChild
-              >
-                <Link
-                  className="hover:no-underline hover:text-white text-sm font-semibold"
-                  to={'update?target=plan'}
-                >
-                  Update
-                </Link>
-              </Button>
-            </TableCell>
-          )}
-        </TableRow>
-        <TableRow>
-          <TableCell className="font-semibold">Flavor</TableCell>
-          <TableCell>{service.flavor}</TableCell>
-          {availabilitiesFlavorQuery.data?.length > 1 && (
-            <TableCell className="text-right">
-              <Button
-                variant="default"
-                size="default"
-                className="py-0 h-auto"
-                asChild
-              >
-                <Link
-                  className="hover:no-underline hover:text-white text-sm font-semibold"
-                  to={'update?target=flavor'}
-                >
-                  Update
-                </Link>
-              </Button>
-            </TableCell>
-          )}
-        </TableRow>
-        {service.storage?.size.value > 0 && (
+    <>
+      <Table>
+        <TableBody>
           <TableRow>
-            <TableCell className="font-semibold">Storage</TableCell>
+            <TableCell className="font-semibold">Version</TableCell>
             <TableCell>
-              {formatStorage(service.storage.size)} {service.storage.type}
+              {humanizeEngine(service.engine)} {service.version}
             </TableCell>
+            {availabilitiesVersionQuery.data?.length > 1 && (
+              <TableCell className="text-right">
+                <Button
+                  variant="default"
+                  size="default"
+                  className="py-0 h-auto"
+                  onClick={() => updateVersionModal.open()}
+                >
+                  Update
+                </Button>
+              </TableCell>
+            )}
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-semibold">Plan</TableCell>
+            <TableCell>{service.plan}</TableCell>
+            {availabilitiesPlanQuery.data?.length > 1 && (
+              <TableCell className="text-right">
+                <Button
+                  variant="default"
+                  size="default"
+                  className="py-0 h-auto"
+                  onClick={() => updatePlanModal.open()}
+                >
+                  Update
+                </Button>
+              </TableCell>
+            )}
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-semibold">Flavor</TableCell>
+            <TableCell>{service.flavor}</TableCell>
             {availabilitiesFlavorQuery.data?.length > 1 && (
               <TableCell className="text-right">
                 <Button
                   variant="default"
                   size="default"
                   className="py-0 h-auto"
-                  asChild
+                  onClick={() => updateFlavorModal.open()}
                 >
-                  <Link
-                    className="hover:no-underline hover:text-white text-sm font-semibold"
-                    to={'update?target=flavor'}
-                  >
-                    Update
-                  </Link>
+                  Update
                 </Button>
               </TableCell>
             )}
           </TableRow>
-        )}
-        <TableRow>
-          <TableCell className="font-semibold">Nodes</TableCell>
-          <TableCell>{service.nodes.length}</TableCell>
-          {availabilitiesFlavorQuery.data?.length > 1 && (
-            <TableCell className="text-right">
-              <Button
-                variant="default"
-                size="default"
-                className="py-0 h-auto"
-                asChild
-              >
-                <Link
-                  className="hover:no-underline hover:text-white text-sm font-semibold"
-                  to={'update?target=flavor'}
-                >
-                  Update
-                </Link>
-              </Button>
-            </TableCell>
+          {service.storage?.size.value > 0 && (
+            <TableRow>
+              <TableCell className="font-semibold">Storage</TableCell>
+              <TableCell>
+                {formatStorage(service.storage.size)} {service.storage.type}
+              </TableCell>
+              {availabilitiesFlavorQuery.data?.length > 1 && (
+                <TableCell className="text-right">
+                  <Button
+                    variant="default"
+                    size="default"
+                    className="py-0 h-auto"
+                    onClick={() => updateStorageModal.open()}
+                  >
+                    Update
+                  </Button>
+                </TableCell>
+              )}
+            </TableRow>
           )}
-        </TableRow>
-      </TableBody>
-    </Table>
+          <TableRow>
+            <TableCell className="font-semibold">Nodes</TableCell>
+            <TableCell>{service.nodes.length}</TableCell>
+            {availabilitiesFlavorQuery.data?.length > 1 && (
+              <TableCell className="flex gap-2 justify-end">
+                <Button
+                  variant={'ghost'}
+                  size="table"
+                  className="p-0 h-auto text-destructive"
+                  onClick={() => deleteNode.open()}
+                >
+                  <MinusCircle />
+                </Button>
+                <Button
+                  variant={'ghost'}
+                  size="table"
+                  className="p-0 h-auto text-primary"
+                  onClick={() => addNode.open()}
+                >
+                  <PlusCircle />
+                </Button>
+              </TableCell>
+            )}
+          </TableRow>
+        </TableBody>
+      </Table>
+      {/* Modals */}
+      {availabilitiesVersionQuery.isSuccess && (
+        <UpdateVersion
+          availabilities={availabilitiesVersionQuery.data}
+          controller={updateVersionModal.controller}
+          onError={() => {}}
+          onSuccess={() => {}}
+        />
+      )}
+      {availabilitiesPlanQuery.isSuccess &&
+        catalog.isSuccess &&
+        capabilities.isSuccess && (
+          <UpdatePlan
+            availabilities={availabilitiesPlanQuery.data}
+            controller={updatePlanModal.controller}
+            capabilities={capabilities.data}
+            catalog={catalog.data}
+            onError={() => {}}
+            onSuccess={() => {}}
+          />
+        )}
+      {availabilitiesFlavorQuery.isSuccess && (
+        <UpdateFlavor
+          availabilities={availabilitiesFlavorQuery.data}
+          controller={updateFlavorModal.controller}
+          onError={() => {}}
+          onSuccess={() => {}}
+        />
+      )}
+      <UpdateStorage
+        availabilities={[]}
+        controller={updateStorageModal.controller}
+        onError={() => {}}
+        onSuccess={() => {}}
+      />
+      <AddNode
+        controller={addNode.controller}
+        onError={() => {}}
+        onSuccess={() => {}}
+      />
+      <DeleteNode
+        controller={deleteNode.controller}
+        onError={() => {}}
+        onSuccess={() => {}}
+      />
+    </>
   );
 };
 
