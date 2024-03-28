b4871c5031db65ea369fc7c2e85e70a2da872150
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-03-28 12:26:41 +0100

    feat: update modals (wip)
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/apps/pci-databases-analytics/src/pages/services/[serviceId]/settings/update/_components/modals/updatePlan.tsx
+++ packages/manager/apps/pci-databases-analytics/src/pages/services/[serviceId]/settings/update/_components/modals/updatePlan.tsx
@@ -17,6 +17,7 @@ import { order } from '@/models/catalog';
 import { database } from '@/models/database';
 import { Engine, Version } from '@/models/order-funnel';
 import { useServiceData } from '@/pages/services/[serviceId]/layout';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 interface UpdatePlanProps {
   controller: ModalController;
@@ -68,24 +69,21 @@ const UpdatePlan = ({
   const [selectedPlan, setSelectedPlan] = useState(service.plan);
   return (
     <Dialog {...controller}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Update service plan</DialogTitle>
-          <DialogDescription>Add a description here</DialogDescription>
-        </DialogHeader>
-        <ul className="list-disc list-outside">
-          {availabilities.map((a) => (
-            <li key={a.plan} className="list-item">
-              {a.plan}
-            </li>
-          ))}
-        </ul>
-        <PlansSelect
-          plans={listPlans}
-          value={selectedPlan}
-          onChange={(newPlan) => setSelectedPlan(newPlan)}
-        />
-        <DialogFooter className="flex justify-end">
+      <DialogContent className="sm:max-w-lg px-0">
+        <ScrollArea className="max-h-[80vh] px-6">
+          <DialogHeader>
+            <DialogTitle>Update service plan</DialogTitle>
+          </DialogHeader>
+          <div>
+            <PlansSelect
+              plans={listPlans}
+              value={selectedPlan}
+              onChange={(newPlan) => setSelectedPlan(newPlan)}
+              className="grid-cols-1 md:grid-cols-1 xl:grid-cols-1 my-1"
+            />
+          </div>
+        </ScrollArea>
+        <DialogFooter className="flex justify-end px-6">
           <DialogClose asChild>
             <Button type="button" variant="outline">
               Cancel
