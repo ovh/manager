3539bf362a8e327fe2b37a5b80078581c276cf2f
Lio <lionel95200@gmail.com>
2024-04-02 14:27:21 +0200

    fix(rancher): allow uppercase in middle of name

--- packages/manager/apps/pci-rancher/src/utils/rancher.ts
+++ packages/manager/apps/pci-rancher/src/utils/rancher.ts
@@ -1,3 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
 export const isValidRancherName = (name: string) =>
-  /^[a-z0-9][-_.a-z0-9]{1,61}[a-z0-9]$/.test(name);
+  /^[a-z0-9][-_.A-Za-z0-9]{1,61}[a-z0-9]$/.test(name);
