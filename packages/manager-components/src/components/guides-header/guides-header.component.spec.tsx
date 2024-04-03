647b38cd66a73e8b38c5a5e86c3709f3f0353657
Florian Renaut <florian.renaut@corp.ovh.com>
2024-04-03 11:49:07 +0200

    test(manager-components): add guides-header unit test
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

---
+++ packages/manager-components/src/components/guides-header/guides-header.component.spec.tsx
@@ -0,0 +1,89 @@
+import { fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { GuidesHeader, GuidesHeaderProps } from './guides-header.component';
+import { render } from '../../utils/test.provider';
+
+const renderComponent = (props: GuidesHeaderProps) => {
+  return render(<GuidesHeader {...props} />);
+};
+
+const guides = {
+  foo: {
+    key: 'foo-guide',
+    url: {
+      EU: 'foo-eu',
+      US: 'foo-us',
+    },
+  },
+  bar: {
+    key: 'bar-guide',
+    url: {
+      EU: 'bar-eu',
+      US: 'bar-us',
+    },
+  },
+};
+
+describe('GuidesHeader tests', () => {
+  it('should display guides list', () => {
+    renderComponent({
+      label: 'hello',
+      guides,
+      ovhSubsidiary: 'EU',
+      getGuideLabel: (guide) => guide.key,
+    });
+    expect(screen.getAllByText('foo-guide')).not.toBeNull();
+    expect(screen.getAllByText('bar-guide')).not.toBeNull();
+  });
+
+  it('should display guides urls depending on ovhSubsidiary', () => {
+    let { container } = renderComponent({
+      label: 'hello',
+      guides,
+      ovhSubsidiary: 'EU',
+      getGuideLabel: (guide) => guide.key,
+    });
+    expect(container.querySelector('[href=foo-us]')).toBeNull();
+    expect(container.querySelector('[href=foo-eu]')).not.toBeNull();
+    expect(container.querySelector('[href=bar-us]')).toBeNull();
+    expect(container.querySelector('[href=bar-eu]')).not.toBeNull();
+    container = renderComponent({
+      label: 'hello',
+      guides,
+      ovhSubsidiary: 'US',
+      getGuideLabel: (guide) => guide.key,
+    }).container;
+    expect(container.querySelector('[href=foo-us]')).not.toBeNull();
+    expect(container.querySelector('[href=foo-eu]')).toBeNull();
+    expect(container.querySelector('[href=bar-us]')).not.toBeNull();
+    expect(container.querySelector('[href=bar-eu]')).toBeNull();
+  });
+
+  it('should trigger onGuideClick', () => {
+    const onGuideClick = jest.fn();
+    const { container } = renderComponent({
+      label: 'hello',
+      guides,
+      ovhSubsidiary: 'EU',
+      getGuideLabel: (guide) => guide.key,
+      onGuideClick,
+    });
+    expect(onGuideClick).not.toHaveBeenCalled();
+    fireEvent.click(container.querySelector('[href=foo-eu]'));
+    expect(onGuideClick).toHaveBeenCalled();
+  });
+
+  it('should not trigger onGuideClick if it is undefined', () => {
+    const onGuideClick = jest.fn();
+    const { container } = renderComponent({
+      label: 'hello',
+      guides,
+      ovhSubsidiary: 'EU',
+      getGuideLabel: (guide) => guide.key,
+      onGuideClick: undefined,
+    });
+    expect(onGuideClick).not.toHaveBeenCalled();
+    fireEvent.click(container.querySelector('[href=foo-eu]'));
+    expect(onGuideClick).not.toHaveBeenCalled();
+  });
+});
