23d9e40bfa0c78dd78aab8afff8b2fbd03f7966c
Florian Renaut <florian.renaut@corp.ovh.com>
2024-04-03 11:05:59 +0200

    test(manager-components): add action-banner unit tests
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

---
+++ packages/manager-components/src/components/action-banner/action-banner.spec.tsx
@@ -0,0 +1,32 @@
+import { fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { ActionBanner, ActionBannerProps } from './action-banner.component';
+import { render } from '../../utils/test.provider';
+
+const renderComponent = (props: ActionBannerProps) => {
+  return render(<ActionBanner {...props} />);
+};
+
+describe('ActionBanner tests', () => {
+  it('should display message', () => {
+    renderComponent({
+      message: 'hello world',
+      cta: 'custom action',
+      onClick: () => {},
+    });
+    expect(screen.getAllByText('hello world')).not.toBeNull();
+  });
+  it('should have a working call to action button', () => {
+    const onClick = jest.fn();
+    renderComponent({
+      message: 'hello world',
+      cta: 'custom action',
+      onClick,
+    });
+    expect(screen.getAllByText('custom action')).not.toBeNull();
+    const cta = screen.queryByTestId('cta');
+    expect(onClick).not.toHaveBeenCalled();
+    fireEvent.click(cta);
+    expect(onClick).toHaveBeenCalled();
+  });
+});
---
+++ packages/manager-components/src/components/action-banner/pci/pci-discovery-banner.component.spec.tsx
@@ -0,0 +1,46 @@
+import { useNavigation } from '@ovh-ux/manager-react-shell-client';
+import { fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import {
+  isDiscoveryProject,
+  PciDiscoveryBanner,
+  PciDiscoveryBannerProps,
+} from './pci-discovery-banner.component';
+import { render } from '../../../utils/test.provider';
+
+jest.mock('@ovh-ux/manager-react-shell-client', () => {
+  const navigateTo = jest.fn();
+  return {
+    useNavigation: () => {
+      return {
+        navigateTo,
+      };
+    },
+  };
+});
+
+const renderComponent = (props: PciDiscoveryBannerProps) => {
+  return render(<PciDiscoveryBanner {...props} />);
+};
+
+describe('PciDiscoveryBanner tests', () => {
+  it('should navigate to project activation when clicking on cta', () => {
+    const { navigateTo } = useNavigation();
+    renderComponent({
+      projectId: '123',
+    });
+    const cta = screen.queryByTestId('cta');
+    expect(navigateTo).not.toHaveBeenCalled();
+    fireEvent.click(cta);
+    expect(navigateTo).toHaveBeenCalledWith(
+      'public-cloud',
+      '#/pci/projects/123/activate',
+      {},
+    );
+  });
+  it('should identify discovery projects', () => {
+    expect(isDiscoveryProject({ planCode: 'foo' })).toBeFalsy();
+    expect(isDiscoveryProject({ planCode: undefined })).toBeFalsy();
+    expect(isDiscoveryProject({ planCode: 'project.discovery' })).toBeTruthy();
+  });
+});
