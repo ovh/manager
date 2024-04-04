192da26b16dad3c62359bcbf6431c3c6e4fa4456
Florian Renaut <florian.renaut@corp.ovh.com>
2024-04-04 09:50:38 +0200

    fix(pci-users): notifications translations
    ref: DTCORE-2047
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/apps/pci-users/src/pages/delete/RemoveUserPage.tsx
+++ packages/manager/apps/pci-users/src/pages/delete/RemoveUserPage.tsx
@@ -1,5 +1,5 @@
 import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
+import { Translation } from 'react-i18next';
 import { useNotifications } from '@ovhcloud/manager-components';
 import { useUser } from '@/api/hooks/useUser';
 import RemoveUserModal from './RemoveUserModal';
@@ -7,7 +7,6 @@ import RemoveUserModal from './RemoveUserModal';
 export default function RemoveSshPage() {
   const { projectId } = useParams();
   const [searchParams] = useSearchParams();
-  const { t } = useTranslation('common');
   const { addError, addSuccess } = useNotifications();
   const navigate = useNavigate();
   const onClose = () => {
@@ -22,20 +21,26 @@ export default function RemoveSshPage() {
       onClose={() => onClose()}
       onSuccess={() => {
         addSuccess(
-          t('pci_projects_project_users_delete_success_message', {
-            user: user?.username,
-          }),
+          <Translation ns="common">
+            {(t) =>
+              t('pci_projects_project_users_delete_success_message', {
+                user: user?.username,
+              })
+            }
+          </Translation>,
           true,
         );
       }}
       onError={(error: Error) => {
         addError(
-          <>
-            {t('pci_projects_project_users_delete_error_delete', {
-              message: error?.message,
-              user: user?.username,
-            })}
-          </>,
+          <Translation ns="common">
+            {(t) =>
+              t('pci_projects_project_users_delete_error_delete', {
+                message: error?.message,
+                user: user?.username,
+              })
+            }
+          </Translation>,
           true,
         );
       }}
