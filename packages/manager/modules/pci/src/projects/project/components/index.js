35e160aa956aee044fee9157c5ce814c88dd422f
Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
2024-04-05 09:54:30 +0200

    feat(pci.project): add billing change banner
    ref: TAPC-440
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>

--- packages/manager/modules/pci/src/projects/project/components/index.js
+++ packages/manager/modules/pci/src/projects/project/components/index.js
@@ -2,12 +2,14 @@ import angular from 'angular';
 
 import activateProjectBanner from './activate-project-banner';
 import projectDashboardActivateBanner from './project-dashboard-activate-banner';
+import billingChangeBanner from './billing-change-banner';
 
 const moduleName = 'pciProjectComponents';
 
 angular.module(moduleName, [
   activateProjectBanner,
   projectDashboardActivateBanner,
+  billingChangeBanner,
 ]);
 
 export default moduleName;
