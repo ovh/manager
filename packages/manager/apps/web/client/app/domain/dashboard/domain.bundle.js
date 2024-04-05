fad5aabfef772df9392203f338f7f0228bdd13c5
antonymarion <marionantony@gmail.com>
2024-04-05 10:05:37 +0200

    feat(web): funnel express order for webhosting (#11386)
    * feat(web): save wip funnel promocode
    
      ref: MANAGER-14052
    
    Signed-off-by: Antony MARION <antony.marion.ext@ovhcloud.com>
    
    * feat(web): funnel express order for webhosting
    
      ref: MANAGER-14052
    
    Signed-off-by: Antony MARION <antony.marion.ext@ovhcloud.com>
    
    ---------
    
    Signed-off-by: Antony MARION <antony.marion.ext@ovhcloud.com>
    Co-authored-by: Antony MARION <antony.marion.ext@ovhcloud.com>

--- packages/manager/apps/web/client/app/domain/dashboard/domain.bundle.js
+++ packages/manager/apps/web/client/app/domain/dashboard/domain.bundle.js
@@ -68,8 +68,6 @@ import '../webhosting/order/steps/index';
 import '../webhosting/order/steps/module/domain-webhosting-order-steps-module.component';
 import '../webhosting/order/steps/module/domain-webhosting-order-steps-module.constants';
 import '../webhosting/order/steps/module/domain-webhosting-order-steps-module.controller';
-import '../webhosting/order/steps/payment/domain-webhosting-order-steps-payment.component';
-import '../webhosting/order/steps/payment/domain-webhosting-order-steps-payment.controller';
 import '../zone/default-ttl/domain-zone-default-ttl.controller';
 import '../zone/delete-all/domain-zone-delete-all.controller';
 import '../zone/domain-zone-dns.controller';
