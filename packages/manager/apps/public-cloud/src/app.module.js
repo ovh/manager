b9a1369b3e0f00a99aa1c62c227e0b69f18cc968
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2024-04-08 14:13:48 +0200

    fix(pci): kafka deep links behold to data-streaming
    ref: TAPC-767
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

--- packages/manager/apps/public-cloud/src/app.module.js
+++ packages/manager/apps/public-cloud/src/app.module.js
@@ -141,7 +141,6 @@ export default async (containerEl, shellClient) => {
       /* @ngInject */ ($rootScope, $state, $transitions) => {
         $transitions.onError({}, (transition) => {
           const error = transition.error();
-          console.error(error);
           if (error.type === RejectType.ERROR) {
             $rootScope.$emit('ovh::sidebar::hide');
             $state.go(
@@ -164,7 +163,6 @@ export default async (containerEl, shellClient) => {
           }
         });
         $state.defaultErrorHandler((error) => {
-          console.error(error);
           return error;
         });
       },
