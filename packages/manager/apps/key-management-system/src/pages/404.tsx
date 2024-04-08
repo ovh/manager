69b3044b086555b429c23d93db5e399d7b4393ac
Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>
2024-04-08 19:35:17 +0200

    feat(kms): refacto into key-management-service name
    ref: MANAGER-14018
    
    Signed-off-by: Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>

--- packages/manager/apps/key-management-system/src/pages/404.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function NotFound() {
-  // @TODO: add a redirection here in order to catch /:serviceName given from iframe
-
-  return <h1>404 - route not found</h1>;
-}
