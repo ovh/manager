805fa41073cdd4a8f95a08a01e18c8d8c78d6ff5
Yoann Fievez <yoann.fievez@ovhcloud.com>
2024-04-10 11:30:18 +0200

    feat(pci-public-ip): add tracking ref: DTCORE-1966
    Signed-off-by: Yoann Fievez <yoann.fievez@ovhcloud.com>

--- packages/manager/apps/pci-public-ip/src/hooks/usePageTracking.ts
+++ packages/manager/apps/pci-public-ip/src/hooks/usePageTracking.ts
@@ -1,16 +1,18 @@
 import { useEffect } from 'react';
-import { useLocation, useRouteLoaderData } from 'react-router-dom';
+import { useLocation, useMatches, useRouteLoaderData } from 'react-router-dom';
 import { useTracking } from '@ovh-ux/manager-react-shell-client';
 
 import { PAGE_PREFIX, PCI_LEVEL2 } from '@/tracking.constants';
 import { Project } from '@/api/data/project';
+import { RouteHandle } from '@/routes';
 
 const DISCOVERY_PLANCODE = 'project.discovery';
 
 export default function usePageTracking() {
   const location = useLocation();
-  const project = useRouteLoaderData('ssh') as Project;
+  const project = useRouteLoaderData('public-ips') as Project;
   const { setPciProjectMode, trackPage } = useTracking();
+  const handle = useMatches().slice(-1)[0]?.handle as RouteHandle;
 
   useEffect(() => {
     if (project) {
@@ -22,10 +24,11 @@ export default function usePageTracking() {
   }, [project]);
 
   useEffect(() => {
-    const pageId = location.pathname.split('/').pop();
-    const pageKey = pageId === 'ssh' ? '' : `::${pageId}`;
+    const pageId = handle?.tracking || location.pathname.split('/').pop();
     trackPage({
-      name: `${PAGE_PREFIX}::sshKeys${pageKey}`,
+      name: `${PAGE_PREFIX}::additional-ips${
+        pageId === 'public-ips' ? '' : `::${pageId}`
+      }`,
       level2: PCI_LEVEL2,
     });
   }, [location]);
