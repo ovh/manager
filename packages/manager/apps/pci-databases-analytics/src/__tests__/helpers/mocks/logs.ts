97eaf25f6969086df0a43e87a017a8b865756fac
abullet33 <106544940+abullet33@users.noreply.github.com>
2024-04-11 16:23:46 +0200

    Feat/pci databases analytics api unit tests (#11445)
    * feat(pci.db): add unit tests on hooks api
    
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>

---
+++ packages/manager/apps/pci-databases-analytics/src/__tests__/helpers/mocks/logs.ts
@@ -0,0 +1,7 @@
+import { database } from '@/models/database';
+
+export const mockedLogs: database.service.LogEntry = {
+  hostname: 'hostName',
+  message: 'LogsLine1',
+  timestamp: 1712554025,
+};
