cc362ac813b9e01328baa9cb7d3e85f99c93411e
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2024-04-12 14:49:52 +0200

    fix(manager-components): fix import error manager components
    ref: MANAGER-13618
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-components/vite.config.ts
+++ packages/manager-components/vite.config.ts
@@ -6,6 +6,18 @@ import { UserConfigExport } from 'vite';
 
 const app = async (): Promise<UserConfigExport> => {
   return defineConfig({
+    resolve: {
+      dedupe: [
+        '@ovh-ux/manager-react-shell-client',
+        '@ovh-ux/manager-core-api',
+        'i18next',
+        'react',
+        'react-dom',
+        'react-i18next',
+        'react-router-dom',
+        'zustand',
+      ],
+    },
     plugins: [
       react(),
       dts({
