86eeaaff37f21f3d2feef81c9152aca4a87e324e
Amandine Dectot <amandine.dectot.ext@corp.ovh.com>
2024-04-12 11:26:24 +0200

    fix(iam): infinite loop on iam menu
    ref: MANAGER-13653
    display a banner if user is not granted
    
    Signed-off-by: Amandine Dectot <amandine.dectot.ext@corp.ovh.com>

--- packages/manager/modules/iam/src/dashboard/dashboard.routing.js
+++ packages/manager/modules/iam/src/dashboard/dashboard.routing.js
@@ -2,8 +2,27 @@ export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('iam.dashboard', {
     url: '/dashboard',
     component: 'iamDashboard',
-    redirectTo: 'iam.dashboard.policies',
+    redirectTo: (transition) =>
+      transition
+        .injector()
+        .getAsync('iamPolicies')
+        .then((data) =>
+          !data?.error ? { state: 'iam.dashboard.policies' } : false,
+        )
+        .catch(() => null),
     resolve: {
+      iamPolicies: /* @ngInject */ (IAMService) =>
+        IAMService.getPolicies({})
+          .then((policies) => policies)
+          .catch((error) => {
+            if (error.status === 403) {
+              return {
+                error: error.data?.details?.unauthorizedActionsByIAM,
+              };
+            }
+
+            throw error;
+          }),
       breadcrumb: () => null,
     },
     atInternet: {
