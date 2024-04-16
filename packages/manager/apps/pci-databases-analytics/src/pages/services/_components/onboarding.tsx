3707be9e8f562c0befc38ed4cbfe2a1075b45314
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-04-16 14:15:35 +0200

    feat: improve guides button, fix some mobile display issues
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/apps/pci-databases-analytics/src/pages/services/_components/onboarding.tsx
+++ packages/manager/apps/pci-databases-analytics/src/pages/services/_components/onboarding.tsx
@@ -1,24 +1,29 @@
-import { OnboardingLayout } from '@ovhcloud/manager-components';
-import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import onboardingImgSrc from '../../../public/assets/onboarding-image.png';
+import Guides from '@/components/guides';
+import { GuideSections } from '@/models/guide';
+import { Button } from '@/components/ui/button';
+import { Link } from '@/components/links';
 
 const Onboarding = () => {
   const { t } = useTranslation('pci-databases-analytics/services/onboarding');
-  const navigate = useNavigate();
-  const title: string = t('title');
-  const description: string = t('description');
-  const onOrderButtonClick = () => {
-    navigate('new');
-  };
   return (
-    <OnboardingLayout
-      title={title}
-      img={{ src: onboardingImgSrc, width: 450, height: 250 }}
-      description={description}
-      orderButtonLabel={t('create-service-button')}
-      onOrderButtonClick={onOrderButtonClick}
-    />
+    <div className="flex flex-col items-center gap-2">
+      <div className="w-full text-right">
+        <Guides section={GuideSections.onboarding} noEngineFilter />
+      </div>
+      <h2>{t('title')}</h2>
+      <img src={onboardingImgSrc} />
+      <p>{t('description')}</p>
+      <Button asChild>
+        <Link
+          to="new"
+          className="hover:no-underline hover:text-primary-foreground"
+        >
+          {t('create-service-button')}{' '}
+        </Link>
+      </Button>
+    </div>
   );
 };
 
