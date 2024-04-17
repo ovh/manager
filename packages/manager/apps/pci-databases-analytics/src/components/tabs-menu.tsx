882926e4e18b00f09b705ade8d49c1f42ebe131f
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-04-17 15:52:30 +0200

    feat: add tabs-menu tests
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/apps/pci-databases-analytics/src/components/tabs-menu.tsx
+++ packages/manager/apps/pci-databases-analytics/src/components/tabs-menu.tsx
@@ -4,7 +4,7 @@ import { ScrollArea, ScrollBar } from './ui/scroll-area';
 import { NavLink } from './links';
 import { Badge } from './ui/badge';
 
-interface Tab {
+export interface Tab {
   href: string;
   label: string;
   count?: number;
@@ -18,13 +18,17 @@ const TabsMenu = ({ tabs }: TabsMenuProps) => {
   const s = useRef(null);
   return (
     <ScrollArea
+      data-testid="scrollbar"
       className="py-2"
       ref={s}
       onWheel={(e) => {
         s.current.children[1].scrollLeft += e.deltaY;
       }}
     >
-      <div className="flex border-b border-[#e6f5fc]">
+      <div
+        className="flex border-b border-[#e6f5fc]"
+        data-testid="tab-container"
+      >
         {tabs.map((tab, index) => (
           <NavLink end={tab.end} to={tab.href} key={`${tab.label}-${index}`}>
             {({ isActive }) => (
@@ -32,7 +36,7 @@ const TabsMenu = ({ tabs }: TabsMenuProps) => {
                 className="w-full px-6 flex gap-2 items-center"
                 ref={(node) => {
                   if (node && isActive)
-                    node.scrollIntoView({
+                    node?.scrollIntoView({
                       behavior: 'smooth',
                       block: 'center',
                     });
@@ -56,7 +60,10 @@ const TabsMenu = ({ tabs }: TabsMenuProps) => {
 
 TabsMenu.Skeleton = function TabsMenuSkeleton() {
   return (
-    <div className="relative flex overflow-x-auto mb-4 w-full border-b border-gray-100">
+    <div
+      className="relative flex overflow-x-auto mb-4 w-full border-b border-gray-100"
+      data-testid="skeleton-container"
+    >
       <div className="flex">
         <span className="whitespace-nowrap w-full block text-primary-500 text-base font-semibold m-0 px-6 py-2">
           <Skeleton className="h-6 w-16" />
