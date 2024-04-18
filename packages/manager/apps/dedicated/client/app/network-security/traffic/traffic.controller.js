9dbe83323e0cf680203303b899714f881c0e3b73
Stéphanie MOALLIC <stephanie.moallic@gmail.com>
2024-04-18 16:01:09 +0200

    feat(network-security): add params into traffic url (#11031)
    ref: UXCT-532
    Signed-off-by: Stephanie Moallic <stephanie.moallic@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/network-security/traffic/traffic.controller.js
+++ packages/manager/apps/dedicated/client/app/network-security/traffic/traffic.controller.js
@@ -8,7 +8,8 @@ import {
 
 export default class TrafficController {
   /* @ngInject */
-  constructor($translate, Alerter, networkSecurityService) {
+  constructor($stateParams, $translate, Alerter, networkSecurityService) {
+    this.$stateParams = $stateParams;
     this.$translate = $translate;
     this.Alerter = Alerter;
     this.networkSecurityService = networkSecurityService;
@@ -34,11 +35,30 @@ export default class TrafficController {
       this.services = data;
       return data;
     });
-    if (this.getSubnet()) {
-      this.subnet = this.getSubnet();
-      this.selectedIp = this.subnet;
-      this.model = this.selectedIp;
-      this.getTraffic();
+    this.ip = this.$stateParams.ip;
+    this.dateTime = this.$stateParams.dateTime
+      ? new Date(this.$stateParams.dateTime)
+      : null;
+    if (this.dateTime) {
+      const dateLimit = new Date();
+      dateLimit.setDate(dateLimit.getDate() - 14);
+      if (this.dateTime >= dateLimit) {
+        const customPeriod = {
+          name: 'custom',
+          label: this.$translate.instant(
+            'network_security_dashboard_filter_custom',
+          ),
+        };
+        this.periods.push(customPeriod);
+        this.period = this.periods[this.periods.length - 1];
+      } else {
+        // Display warning message
+        this.displayWarning = true;
+      }
+    }
+    if (this.ip) {
+      this.selectedIp = this.ip;
+      this.checkSelectedSubnet(this.ip);
     }
 
     this.units = this.CHART.units;
@@ -135,6 +155,9 @@ export default class TrafficController {
         break;
       case this.TRAFFIC_PERIOD_LIST.last14d:
         after.setDate(after.getDate() - 14);
+        break;
+      case 'custom':
+        after.setTime(this.dateTime);
         break;
       default:
         after.setDate(after.getDate() - 1);
