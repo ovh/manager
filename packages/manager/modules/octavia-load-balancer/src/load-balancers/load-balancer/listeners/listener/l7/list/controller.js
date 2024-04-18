a766232f2411c0b4691d2e9a3b7792457e5f15ab
JacquesLarique <134954692+JacquesLarique@users.noreply.github.com>
2024-04-18 16:13:58 +0200

    feat(octavia): L7 policies and rules management (#11265)
    ref: MANAGER-12883
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

---
+++ packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/listeners/listener/l7/list/controller.js
@@ -0,0 +1,23 @@
+import { ACTIONS, ACTION_LABELS, POLICIES_TRACKING } from '../constants';
+
+export default class OctaviaLoadBalancerL7PoliciesListCtrl {
+  constructor() {
+    this.ACTIONS = ACTIONS;
+    this.ACTION_LABELS = ACTION_LABELS;
+    this.policyEditionTrackingHit = POLICIES_TRACKING.EDIT;
+    this.poolDashboardTrackingHit = POLICIES_TRACKING.POOL_DASHBOARD;
+  }
+
+  getAttribute(policy) {
+    switch (policy.action) {
+      case this.ACTIONS.REDIRECT_TO_URL:
+        return policy.redirectUrl;
+      case this.ACTIONS.REDIRECT_PREFIX:
+        return policy.redirectPrefix;
+      case this.ACTIONS.REDIRECT_TO_POOL:
+        return policy.redirectPoolId;
+      default:
+        return '-';
+    }
+  }
+}
