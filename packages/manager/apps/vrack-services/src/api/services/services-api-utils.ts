42abba1d84a7e1f0d124648c6c2eec4e5f002c9d
Quentin Pavy <contact@quentinpavy.com>
2024-04-19 16:04:00 +0200

    feat(vrack-services): change display name with iam display name
    ref: MANAGER-13968
    
    Signed-off-by: Quentin Pavy <contact@quentinpavy.com>

---
+++ packages/manager/apps/vrack-services/src/api/services/services-api-utils.ts
@@ -0,0 +1,75 @@
+import { ApiError, ApiResponse } from '@ovh-ux/manager-core-api';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import React from 'react';
+import {
+  updateVrackServicesName,
+  updateVrackServicesNameQueryKey,
+} from './put';
+import {
+  getVrackServicesServiceId,
+  getVrackServicesServiceIdQueryKey,
+} from './get';
+import { getVrackListQueryKey } from '../vrack';
+
+export type UpdateVrackServicesNameMutationParams = {
+  /** vrackServices service id */
+  vrackServices: string;
+  /** vrackServices service new display name */
+  displayName: string;
+};
+
+/**
+ * Get the function to mutate a vRack Services
+ */
+export const useUpdateVrackServicesName = ({
+  onSuccess,
+  onError,
+}: {
+  onSuccess?: () => void;
+  onError?: (result: ApiError) => void;
+}) => {
+  const [isErrorVisible, setIsErrorVisible] = React.useState(false);
+  const queryClient = useQueryClient();
+
+  const {
+    mutate: updateVSName,
+    isPending,
+    isError: isUpdateNameError,
+    error: updateNameError,
+  } = useMutation({
+    mutationKey: updateVrackServicesNameQueryKey(),
+    mutationFn: async ({
+      vrackServices,
+      displayName,
+    }: UpdateVrackServicesNameMutationParams) => {
+      const { data: servicesId } = await queryClient.fetchQuery<
+        ApiResponse<number[]>
+      >({
+        queryKey: getVrackServicesServiceIdQueryKey({ vrackServices }),
+        queryFn: () => getVrackServicesServiceId({ vrackServices }),
+      });
+      return updateVrackServicesName({ serviceId: servicesId[0], displayName });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: getVrackListQueryKey,
+      });
+      onSuccess?.();
+    },
+    onError,
+  });
+
+  React.useEffect(() => {
+    if (isUpdateNameError) {
+      setIsErrorVisible(true);
+    }
+  }, [isUpdateNameError]);
+
+  return {
+    updateVSName,
+    isPending,
+    isErrorVisible,
+    error: updateNameError,
+    hideError: () => setIsErrorVisible(false),
+  };
+};
