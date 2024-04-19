5a77887e6ec00dab57195843d3a10b403b1596a2
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2024-04-19 15:09:41 +0200

    feat(2fa): created the µapp
    ref: MANAGER-14006
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/apps/account-disable-2fa/src/pages/Home.page.tsx
+++ packages/manager/apps/account-disable-2fa/src/pages/Home.page.tsx
@@ -4,23 +4,17 @@ import ovhCloudLogo from '@/assets/logo-ovhcloud.png';
 
 export default function Home() {
   return (
-    <>
-      <div className="container mx-auto px-5">
-        <div className="m-10">
-          <div className="p-3 md:py-5">
-            <div className="inline-block pb-3 md:pb-5">
-              <img
-                src={ovhCloudLogo}
-                alt="ovh-cloud-logo"
-                className="app-logo"
-              />
-            </div>
-            <div className="flex justify-center app-content lg:w-8/12 mx-auto min-h-[500px] shadow shadow-[0_0_6px_0_rgba(40,89,192,0.2)]">
-              <Outlet />
-            </div>
+    <div className="container mx-auto px-5">
+      <div className="m-10">
+        <div className="p-3 md:py-5">
+          <div className="inline-block pb-3 md:pb-5">
+            <img src={ovhCloudLogo} alt="ovh-cloud-logo" className="app-logo" />
+          </div>
+          <div className="flex justify-center app-content lg:w-8/12 mx-auto min-h-[500px] shadow shadow-[0_0_6px_0_rgba(40,89,192,0.2)]">
+            <Outlet />
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
