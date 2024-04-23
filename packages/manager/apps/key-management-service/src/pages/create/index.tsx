7a9d203aa6840c83e4d13a71fd965ff8da08ab5a
Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>
2024-04-23 09:20:03 +0200

    feat(*): add create kms feature ref: MANAGER-13566
    Signed-off-by: Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>

---
+++ packages/manager/apps/key-management-service/src/pages/create/index.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import {
+  OsdsText,
+  OsdsDivider,
+  OsdsBreadcrumb,
+} from '@ovhcloud/ods-components/react';
+
+import { Notifications } from '@ovhcloud/manager-components';
+import {
+  ODS_THEME_COLOR_INTENT,
+  ODS_THEME_TYPOGRAPHY_SIZE,
+  ODS_THEME_TYPOGRAPHY_LEVEL,
+} from '@ovhcloud/ods-common-theming';
+import { useTranslation } from 'react-i18next';
+import { ROUTES_URLS } from '@/routes/routes.constants';
+import RegionSelector from '@/components/layout-helpers/Create/RegionSelector';
+import OrderConfirmation from '@/components/layout-helpers/Create/OrderConfirmation';
+
+export default function Create() {
+  const { t } = useTranslation('key-management-service/create');
+  const [isOrderInitiated, setIsOrderInitiated] = useState(false);
+  const [selectedRegion, setSelectedRegion] = useState(undefined);
+  return (
+    <>
+      <OsdsBreadcrumb
+        items={[
+          {
+            href: ROUTES_URLS.listing,
+            label: t('key_management_service_create_title'),
+          },
+        ]}
+      ></OsdsBreadcrumb>
+      <div className={'flex items-center justify-between mt-4 mb-2'}>
+        <OsdsText
+          level={ODS_THEME_TYPOGRAPHY_LEVEL.heading}
+          size={ODS_THEME_TYPOGRAPHY_SIZE._600}
+          color={ODS_THEME_COLOR_INTENT.text}
+        >
+          {t('key_management_service_create_title')}
+        </OsdsText>
+      </div>
+      <OsdsText
+        level={ODS_THEME_TYPOGRAPHY_LEVEL.subheading}
+        size={ODS_THEME_TYPOGRAPHY_SIZE._800}
+        color={ODS_THEME_COLOR_INTENT.text}
+      >
+        {t('key_management_service_create_subtitle')}
+      </OsdsText>
+      <OsdsDivider></OsdsDivider>
+      <Notifications />
+      {!isOrderInitiated ? (
+        <RegionSelector
+          setOrderInitiated={() => {
+            setIsOrderInitiated(true);
+          }}
+          selectRegion={setSelectedRegion}
+          selectedRegion={selectedRegion}
+        />
+      ) : (
+        <OrderConfirmation region={selectedRegion} />
+      )}
+    </>
+  );
+}
