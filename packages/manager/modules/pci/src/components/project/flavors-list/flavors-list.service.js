d6e30e1f47693292de31935dbcc0079b87167678
sachinrameshn <sachin.ramesh@ovhcloud.com>
2024-04-24 15:09:45 +0530

    fix(pci): consider all flavors for adding label
    ref: INC0032152
    
    Signed-off-by: sachinrameshn <sachin.ramesh@ovhcloud.com>

--- packages/manager/modules/pci/src/components/project/flavors-list/flavors-list.service.js
+++ packages/manager/modules/pci/src/components/project/flavors-list/flavors-list.service.js
@@ -78,6 +78,9 @@ export default class FlavorsList {
         const groupedPlanCodesByName = groupBy(hourlyPlanCodes, 'name');
         return map(groupedPlanCodesByName, (groupedFlavors) => {
           const resource = groupedFlavors[0];
+          const planCodeList = groupedFlavors.map(
+            (flavor) => flavor.planCodes.hourly,
+          );
           return new Flavor({
             ...omit(resource, ['available', 'region']),
             technicalBlob: get(
@@ -112,8 +115,8 @@ export default class FlavorsList {
               'legacy',
             ),
             locationCompatibility: this.getlocationCompatibility(
-              productAvailability.plans?.filter((plan) =>
-                plan.code?.startsWith(resource.planCodes.hourly),
+              productAvailability.plans?.filter(({ code }) =>
+                planCodeList?.find((plans) => code.startsWith(plans)),
               ),
             ),
           });
