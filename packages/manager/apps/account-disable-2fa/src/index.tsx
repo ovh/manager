846117f8f717b729bb57348d41ae2e1daebb1765
JacquesLarique <134954692+JacquesLarique@users.noreply.github.com>
2024-04-24 16:31:39 +0200

    feat(2fa): created the µapp (#11426)
    ref: MANAGER-14006
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

---
+++ packages/manager/apps/account-disable-2fa/src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import initI18n from './i18n';
+
+import './global.css';
+import './index.scss';
+
+// const token = ???;
+// const user = decodeJWT(token);
+const user = {
+  legalform: 'individual',
+  email: 'monsieur.madame@ovhcloud.com',
+  language: 'fr_FR',
+  subsidiary: 'FR',
+};
+const locale = user.language;
+
+initI18n(locale, [locale]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
