ad82bd64d5d9675aa4855675dd3f73a8772520d1
Yoann Fievez <yoann.fievez@ovhcloud.com>
2024-04-25 17:04:18 +0200

    feat(pci-gateway): add onboarding page ref: DTCORE-2125
    Signed-off-by: Yoann Fievez <yoann.fievez@ovhcloud.com>

--- packages/manager/apps/pci-gateway/src/routes.tsx
+++ packages/manager/apps/pci-gateway/src/routes.tsx
@@ -54,6 +54,11 @@ export default [
           },
         ],
       },
+      {
+        path: 'onboarding',
+        ...lazyRouteConfig(() => import('@/pages/onboarding/OnBoarding.page')),
+        children: [],
+      },
     ],
   },
   {
