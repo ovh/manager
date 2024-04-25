3bb804187814c144c6accf2bd8b97080c1d816c7
Hyenne Guillaume <guillaume.hyenne@ovhcloud.com>
2024-04-25 15:37:36 +0200

    feat(zimbra): zimbra project v0 (#11546)
    ref: MANAGER-13039 
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
    Co-authored-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
    Co-authored-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

---
+++ packages/manager/apps/zimbra/src/components/Breadcrumb/constants.ts
@@ -0,0 +1,11 @@
+export const urls = {
+  root: '',
+  onboarding: '/onboarding',
+  dashboard: '/:serviceName',
+  overview: '/:serviceName',
+  organizations: '/:serviceName/organizations',
+  domain: '/:serviceName/domains',
+  email_accounts: '/:serviceName/email_accounts',
+  mailing_list: '/:serviceName/mailing_lists',
+  redirections: '/:serviceName/redirections',
+};
---
+++ packages/manager/apps/zimbra/src/routes/routes.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { RouteObject } from 'react-router-dom';
+import i18next from 'i18next';
+import NotFound from '@/pages/404';
+
+const lazyRouteConfig = (importFn: CallableFunction): Partial<RouteObject> => {
+  return {
+    lazy: async () => {
+      const { default: moduleDefault, ...moduleExports } = await importFn();
+      return {
+        Component: moduleDefault,
+        ...moduleExports,
+      };
+    },
+  };
+};
+
+export const Routes: any = [
+  {
+    path: '',
+    handle: { breadcrumb: (): string => 'Zimbra' },
+    ...lazyRouteConfig(() => import('@/pages/layout')),
+    children: [
+      {
+        path: ':serviceName',
+        ...lazyRouteConfig(() => import('@/pages/dashboard/_layout')),
+        children: [
+          {
+            path: '',
+            ...lazyRouteConfig(() =>
+              import('@/pages/dashboard/GeneralInformation'),
+            ),
+            handle: {
+              breadcrumb: (): null => null,
+            },
+          },
+          {
+            path: 'organizations',
+            ...lazyRouteConfig(() => import('@/pages/dashboard/Organizations')),
+            handle: {
+              breadcrumb: (): string => i18next.t('dashboard:organization'),
+            },
+          },
+          {
+            path: 'domains',
+            ...lazyRouteConfig(() => import('@/pages/dashboard/Domains')),
+            handle: {
+              breadcrumb: (): string => i18next.t('dashboard:domain'),
+            },
+          },
+          {
+            path: 'email_accounts',
+            ...lazyRouteConfig(() => import('@/pages/dashboard/EmailAccounts')),
+            handle: {
+              breadcrumb: (): string => i18next.t('dashboard:email_accounts'),
+            },
+          },
+        ],
+      },
+      {
+        path: 'onboarding',
+        ...lazyRouteConfig(() => import('@/pages/onboarding')),
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+];
