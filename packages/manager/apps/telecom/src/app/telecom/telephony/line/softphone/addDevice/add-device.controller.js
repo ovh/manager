a94d90a70c42248d9ec26be1b2d95616d07a1ba8
Antony MARION <antony.marion.ext@ovhcloud.com>
2024-04-26 10:32:10 +0200

    feat(telecom): separate logic and optimize one put
       ref: MANAGER-13978
    
    Signed-off-by: Antony MARION <antony.marion.ext@ovhcloud.com>

--- packages/manager/apps/telecom/src/app/telecom/telephony/line/softphone/addDevice/add-device.controller.js
+++ packages/manager/apps/telecom/src/app/telecom/telephony/line/softphone/addDevice/add-device.controller.js
@@ -1,6 +1,7 @@
 import { FIELD_NAME_REGEX } from './add-device.constants';
 
 export default class SoftphoneAddDeviceController {
+  /* @ngInject */
   constructor(softphoneService, TucToast, $translate, $stateParams) {
     this.softphoneService = softphoneService;
     this.TucToast = TucToast;
@@ -10,12 +11,12 @@ export default class SoftphoneAddDeviceController {
     this.FIELD_NAME_REGEX = FIELD_NAME_REGEX;
   }
 
-  generateLink() {
-    this.softphoneService
-      .generateLink(
+  enrollDevice(deviceId) {
+    return this.softphoneService
+      .enroll(
         this.$stateParams.billingAccount,
         this.$stateParams.serviceName,
-        this.model.name,
+        deviceId,
       )
       .then(({ provisioningURL }) => {
         this.recordLink = provisioningURL;
@@ -27,5 +28,64 @@ export default class SoftphoneAddDeviceController {
           ),
         );
       });
+  }
+
+  generateLink() {
+    if (this.deviceId && this.softphoneAddDeviceForm.$pristine) {
+      return this.softphoneService
+        .enroll(
+          this.$stateParams.billingAccount,
+          this.$stateParams.serviceName,
+          this.deviceId,
+        )
+        .catch(() => {
+          this.TucToast.error(
+            this.$translate.instant(
+              'telephony_line_softphone_generate_link_error',
+            ),
+          );
+        })
+        .finally(() => this.softphoneAddDeviceForm.$setPristine(true));
+    }
+
+    if (!this.deviceId) {
+      return this.softphoneService
+        .createDevice(
+          this.$stateParams.billingAccount,
+          this.$stateParams.serviceName,
+          this.model.name,
+        )
+        .then(({ deviceId }) => {
+          this.deviceId = deviceId;
+          return this.enrollDevice(this.deviceId);
+        })
+        .catch(() => {
+          this.TucToast.error(
+            this.$translate.instant(
+              'telephony_line_softphone_generate_link_error',
+            ),
+          );
+        })
+        .finally(() => this.softphoneAddDeviceForm.$setPristine(true));
+    }
+
+    return this.softphoneService
+      .setDeviceName(
+        this.$stateParams.billingAccount,
+        this.$stateParams.serviceName,
+        this.model.name,
+        this.deviceId,
+      )
+      .then(() => {
+        return this.enrollDevice(this.deviceId);
+      })
+      .catch(() => {
+        this.TucToast.error(
+          this.$translate.instant(
+            'telephony_line_softphone_generate_link_error',
+          ),
+        );
+      })
+      .finally(() => this.softphoneAddDeviceForm.$setPristine(true));
   }
 }
--- packages/manager/apps/telecom/src/app/telecom/telephony/line/softphone/softphone.service.js
+++ packages/manager/apps/telecom/src/app/telecom/telephony/line/softphone/softphone.service.js
@@ -6,16 +6,30 @@ export default class SofpthoneService {
     this.$http = $http;
   }
 
-  generateLink(billingAccount, serviceName, name) {
+  setDeviceName(billingAccount, serviceName, name, deviceId) {
+    return this.$http
+      .put(
+        `/telephony/${billingAccount}/line/${serviceName}/devices/${deviceId}`,
+        {
+          name,
+        },
+      )
+      .then(({ data }) => data);
+  }
+
+  createDevice(billingAccount, serviceName, name) {
     return this.$http
       .post(`/telephony/${billingAccount}/line/${serviceName}/devices`, {
         name,
         type: SOFTPHONE_TYPE,
       })
-      .then(({ data: { deviceId } }) =>
-        this.$http.post(
-          `/telephony/${billingAccount}/line/${serviceName}/devices/${deviceId}/enroll`,
-        ),
+      .then(({ data }) => data);
+  }
+
+  enroll(billingAccount, serviceName, deviceId) {
+    return this.$http
+      .post(
+        `/telephony/${billingAccount}/line/${serviceName}/devices/${deviceId}/enroll`,
       )
       .then(({ data }) => data);
   }
