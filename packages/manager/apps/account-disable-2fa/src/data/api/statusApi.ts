63c36a0403895948ad1d102849e852d046dd08df
oalkabouss <omaralkabous@gmail.com>
2024-04-30 11:40:40 +0200

    feat(2fa): anoop pr feed back  (#11588)
    ref: MANAGER-14035
    
    Signed-off-by: oalkabouss <omaralkabous@gmail.com>

---
+++ packages/manager/apps/account-disable-2fa/src/data/api/statusApi.ts
@@ -0,0 +1,13 @@
+import { Status2fa } from '@/types/status.type';
+
+// TODO: Replace this mock with a real call 'me/procedure/2fa'
+export const get2faStatus: () => Promise<Status2fa> = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        status: 'creationAuthorized', // or 'open',
+        ticketId: '',
+      });
+    }, 300);
+  });
+};
