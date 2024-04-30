d0ceddec105914b4476725eeff1e7582c2b22bd4
oalkabouss <omaralkabous@gmail.com>
2024-04-30 11:40:40 +0200

    feat(2fa): anoop pr feed back  (#11588)
    ref: MANAGER-14035
    
    Signed-off-by: oalkabouss <omaralkabous@gmail.com>

--- packages/manager/apps/account-disable-2fa/src/pages/Home.page.tsx
+++ packages/manager/apps/account-disable-2fa/src/pages/Home.page.tsx
@@ -1,17 +1,40 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import ovhCloudLogo from '@/assets/logo-ovhcloud.png';
+import { Status2faStrategies } from '@/types/status.type';
+import {
+  createRoutePath,
+  seeRoutePath,
+} from '@/constants/route-path-constants';
+import { fetch2faStatus } from '@/data/hooks/useStatus';
+
+const redirectStrategies: Status2faStrategies = {
+  open: `/${seeRoutePath}`,
+  creationAuthorized: `/${createRoutePath}`,
+};
 
 export default function Home() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { data, isFetched } = fetch2faStatus();
+
+  useEffect(() => {
+    const route = redirectStrategies[data?.status];
+    if (isFetched && route !== location.pathname) {
+      navigate(route, { replace: true });
+    }
+  }, [isFetched, location]);
+
   return (
-    <div className="container mx-auto px-5">
-      <div className="m-10">
+    <div className="sm:container mx-auto px-5">
+      <div className="md:m-10 m-4">
         <div className="p-3 md:py-5">
           <div className="inline-block pb-3 md:pb-5">
             <img src={ovhCloudLogo} alt="ovh-cloud-logo" className="app-logo" />
           </div>
-          <div className="flex justify-center app-content lg:w-8/12 mx-auto min-h-[500px] shadow shadow-[0_0_6px_0_rgba(40,89,192,0.2)]">
-            <Outlet />
+          <div className="flex justify-center app-content lg:w-8/12 mx-auto min-h-[500px] sm:shadow sm:shadow-[0_0_6px_0_rgba(40,89,192,0.2)] sm:border-none border-t-[1px] border-gray-300">
+            {isFetched && <Outlet />}
           </div>
         </div>
       </div>
