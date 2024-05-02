dd0a211cd18e33f3b9c6e4c168545cf612570966
Maxime Bajeux <max.bajeux@gmail.com>
2024-05-02 10:36:54 +0200

    feat(dedicated.account): Missing guidance to reduce kyc upload issues BATCH 2 (#11383)
    refacto the component to include association and others
    
    ref: MANAGER-14056
    
    Signed-off-by: Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>
    Co-authored-by: Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>

---
+++ packages/manager/apps/dedicated/client/app/account/identity-documents/components/identity-documents-requirements/controller.js
@@ -0,0 +1,8 @@
+import { USER_TYPE } from '../../user-identity-documents.constant';
+
+export default class AccountUserIdentityDocumentsReauirementsController {
+  /* @ngInject */
+  constructor() {
+    this.USER_TYPE = USER_TYPE;
+  }
+}
