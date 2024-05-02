7c64eff1be999261cc3310ce5f1a36b8df911db2
brofik <10513484+brofik@users.noreply.github.com>
2024-05-02 11:48:19 +0200

    feat(project): update text content for the billing page (#10903)
    ref: TAPC-550
    
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/pci/src/projects/project/billing/billing.component.js
+++ packages/manager/modules/pci/src/projects/project/billing/billing.component.js
@@ -17,6 +17,7 @@ export default {
     service: '<',
     iamLink: '<',
     isDiscoveryProject: '<',
+    isSubsidiaryWithPostPaidUsageBilling: '<',
   },
   controller,
   template,
--- packages/manager/modules/pci/src/projects/project/billing/legacy/billing-legacy.component.js
+++ packages/manager/modules/pci/src/projects/project/billing/legacy/billing-legacy.component.js
@@ -14,6 +14,7 @@ export default {
     pciFeatures: '<',
     projectId: '<',
     isDiscoveryProject: '<',
+    isSubsidiaryWithPostPaidUsageBilling: '<',
   },
   controllerAs: 'BillingCtrl',
   controller,
