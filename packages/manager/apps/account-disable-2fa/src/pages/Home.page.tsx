3f219bcd7c05e53f0d77ddb2cba3ac38ccd4b68e
oalkabouss <omaralkabous@gmail.com>
2024-05-03 09:25:44 +0200

    feat(2fa):  add see page messages
    ref: MANAGER-14145
    
    Signed-off-by: oalkabouss <omaralkabous@gmail.com>

--- packages/manager/apps/account-disable-2fa/src/pages/Home.page.tsx
+++ packages/manager/apps/account-disable-2fa/src/pages/Home.page.tsx
@@ -4,13 +4,13 @@ import ovhCloudLogo from '@/assets/logo-ovhcloud.png';
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-5">
-      <div className="m-10">
+    <div className="sm:container mx-auto px-5">
+      <div className="md:m-10 m-4">
         <div className="p-3 md:py-5">
           <div className="inline-block pb-3 md:pb-5">
             <img src={ovhCloudLogo} alt="ovh-cloud-logo" className="app-logo" />
           </div>
-          <div className="flex justify-center app-content lg:w-8/12 mx-auto min-h-[500px] shadow shadow-[0_0_6px_0_rgba(40,89,192,0.2)]">
+          <div className="flex justify-center app-content lg:w-8/12 mx-auto min-h-[500px] sm:shadow sm:shadow-[0_0_6px_0_rgba(40,89,192,0.2)] sm:border-none border-t-[1px] border-gray-300">
             <Outlet />
           </div>
         </div>
