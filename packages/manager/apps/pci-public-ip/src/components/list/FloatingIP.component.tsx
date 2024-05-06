a4566fd2c72f31c065321a70eef8dc2f516a3456
LIDRISSI Hamid <abdelghani-lidrissi.hamid.ext@ovhcloud.com>
2024-05-06 11:39:40 +0200

    fix(pci-public-ip): add marging the button action in all datagrid
    ref: DTCORE-2188
    Signed-off-by: LIDRISSI Hamid <abdelghani-lidrissi.hamid.ext@ovhcloud.com>

--- packages/manager/apps/pci-public-ip/src/components/list/FloatingIP.component.tsx
+++ packages/manager/apps/pci-public-ip/src/components/list/FloatingIP.component.tsx
@@ -113,7 +113,9 @@ export default function FloatingIPComponent({
     {
       id: 'actions',
       cell: (props: FloatingIP) => (
-        <FloatingIPActions projectId={projectId} ipId={props.id} />
+        <div className="min-w-16">
+          <FloatingIPActions projectId={projectId} ipId={props.id} />
+        </div>
       ),
       label: '',
     },
