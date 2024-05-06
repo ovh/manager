3cf8cbc0ba997fb0cf7bb958064ff4c9f005df68
brofik <10513484+brofik@users.noreply.github.com>
2024-05-06 09:27:09 +0200

    feat(pci.project): add billing change banner (#11635)
    ref: TAPC-440
    
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

---
+++ packages/manager/modules/pci/src/projects/project/components/billing-change-banner/billing-change-banner.component.js
@@ -0,0 +1,8 @@
+import template from './billing-change-banner.html';
+import controller from './billing-change-banner.controller';
+
+export default {
+  template,
+  controller,
+  bindings: {},
+};
