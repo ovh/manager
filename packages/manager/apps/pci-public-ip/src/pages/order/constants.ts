4a5c10f0820f3f6f42babd1378398f2e185e98cd
Florian Renaut <florian.renaut@corp.ovh.com>
2024-05-06 15:12:30 +0200

    fix(pci-public-ip): express order url
    ref: DTCORE-2193
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/apps/pci-public-ip/src/pages/order/constants.ts
+++ packages/manager/apps/pci-public-ip/src/pages/order/constants.ts
@@ -1,20 +1,3 @@
-export const URLS = new Map([
-  ['DE', 'https://www.ovh.de/order/express/#/express/review'],
-  ['EN', 'https://www.ovh.co.uk/order/express/#/express/review'],
-  ['ES', 'https://www.ovh.es/order/express/#/express/review'],
-  ['FR', 'https://www.ovh.com/fr/order/express/#/express/review'],
-  ['GB', 'https://www.ovh.co.uk/order/express/#/express/review'],
-  ['IE', 'https://www.ovh.ie/order/express/#/express/review'],
-  ['IT', 'https://www.ovh.it/order/express/#/express/review'],
-  ['MA', 'https://www.ovh.com/ma/order/express/#/express/review'],
-  ['NL', 'https://www.ovh.nl/order/express/#/express/review'],
-  ['PL', 'https://www.ovh.pl/order/express/#/express/review'],
-  ['PT', 'https://www.ovh.pt/order/express/#/express/review'],
-  ['SN', 'https://www.ovh.sn/order/express/#/express/review'],
-  ['TN', 'https://www.ovh.com/tn/order/express/#/express/review'],
-  ['DEFAULT', 'https://www.ovh.ie/order/express/#/express/review'],
-]);
-
 export const GUIDE_URLS = {
   FAILOVER_IP: {
     DEFAULT: 'https://www.ovhcloud.com/en/bare-metal/ip/',
