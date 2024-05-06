8e41720f81621cba6f434fcf489841571755c8b7
Quentin Pavy <contact@quentinpavy.com>
2024-05-06 08:47:25 +0200

    Revert "feat(pci): add billing change banner (#11390)"
    This reverts commit 6f1e744f1e8b2fb5be2c67b8294b6bc3266a83b2.
    
    Signed-off-by: Quentin Pavy <contact@quentinpavy.com>

--- packages/manager/modules/pci/src/projects/project/billing/legacy/billing-legacy.component.js
+++ packages/manager/modules/pci/src/projects/project/billing/legacy/billing-legacy.component.js
@@ -13,8 +13,6 @@ export default {
     trackClick: '<',
     pciFeatures: '<',
     projectId: '<',
-    isDiscoveryProject: '<',
-    isSubsidiaryWithPostPaidUsageBilling: '<',
   },
   controllerAs: 'BillingCtrl',
   controller,
