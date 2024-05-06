60936830cc8ff0c716024987819fce9cc035150a
Lio <lionel95200@gmail.com>
2024-05-06 11:24:11 +0200

    feat: update ods version
    ref: TAPC-908
    Signed-off-by: Lio <lionel95200@gmail.com>

--- packages/manager/apps/pci-rancher/src/components/Modal/DeleteModal.test.tsx
+++ packages/manager/apps/pci-rancher/src/components/Modal/DeleteModal.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
 import DeleteModal, { TERMINATE_TEXT } from './DeleteModal';
-import { render, waitFor } from '../../utils/test/test.provider';
+import { fireEvent, render, waitFor } from '../../utils/test/test.provider';
 import listingTranslation from '../../public/translations/pci-rancher/listing/Messages_fr_FR.json';
 import { rancherMocked } from '../../_mock_/rancher';
 
@@ -50,7 +50,7 @@ describe('Delete Modal', () => {
       const input = screen.getByLabelText('delete-input');
       const button = screen.getByText(listingTranslation.deleteRancher);
 
-      await userEvent.type(input, TERMINATE_TEXT);
+      fireEvent.change(input, { target: { value: TERMINATE_TEXT } });
       await userEvent.click(button);
 
       expect(input.getAttribute('value')).toBe(TERMINATE_TEXT);
--- packages/manager/apps/pci-rancher/src/components/layout-helpers/CreateRancher/CreateRancher.test.tsx
+++ packages/manager/apps/pci-rancher/src/components/layout-helpers/CreateRancher/CreateRancher.test.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { rancherPlan, rancherVersion } from '@/_mock_/rancher-resource';
 import dashboardTranslation from '../../../public/translations/pci-rancher/dashboard/Messages_fr_FR.json';
 import listingTranslation from '../../../public/translations/pci-rancher/listing/Messages_fr_FR.json';
-import { render, waitFor } from '../../../utils/test/test.provider';
+import { fireEvent, render, waitFor } from '../../../utils/test/test.provider';
 import CreateRancher, { CreateRancherProps } from './CreateRancher';
 import { getOnboardingUrl, getRanchersUrl } from '@/utils/route';
 
@@ -64,7 +64,7 @@ describe('CreateRancher', () => {
     const input = screen.getByLabelText('rancher-name-input');
     const button = screen.getByText(dashboardTranslation.createRancherCTA);
 
-    await userEvent.type(input, '12()34343:::');
+    fireEvent.change(input, { target: { value: '12()34343:::' } });
     await userEvent.click(button);
 
     expect(input).toHaveAttribute('color', 'error');
@@ -79,11 +79,12 @@ describe('CreateRancher', () => {
       dashboardTranslation.createRancherCTA,
     );
 
-    await userEvent.type(input, 'myrancher');
+    const NEW_NAME = 'myrancher';
+    fireEvent.change(input, { target: { value: NEW_NAME } });
     await userEvent.click(confirmButton);
 
     expect(onCreateRancher).toHaveBeenCalledWith({
-      name: 'myrancher',
+      name: NEW_NAME,
       plan: 'STANDARD',
       version: 'v2.7.6',
     });
@@ -155,7 +156,7 @@ describe('CreateRancher', () => {
         dashboardTranslation.createRancherCTA,
       );
 
-      await userEvent.type(input, 'MyRancher');
+      fireEvent.change(input, { target: { value: 'MyRancher' } });
       await userEvent.click(confirmButton);
 
       expect(onCreateRancher).not.toHaveBeenCalled();
