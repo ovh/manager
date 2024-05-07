03c5014382d7e8d4670af3be6e4082a398ed8ad7
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2024-05-07 11:20:58 +0200

    feat(octavia-load-balancer): add log to customer guide links
    ref: MANAGER-14292
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

--- packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/logs/logs.controller.js
+++ packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/logs/logs.controller.js
@@ -1,15 +1,20 @@
-import { LOGS_INFO, LOAD_BALANCER_LOGS_TRACKING_HITS } from './logs.constants';
+import {
+  LOAD_BALANCER_LOGS_SERVICE_GUIDE_LINK,
+  LOAD_BALANCER_LOGS_TRACKING_HITS,
+} from './logs.constants';
 
 export default class OctaviaLoadBalancerLogsController {
   /* @ngInject */
-  constructor($state) {
-    this.$state = $state;
+  constructor(coreConfig) {
+    this.user = coreConfig.getUser();
   }
 
   $onInit() {
     this.LOAD_BALANCER_LOGS_TRACKING_HITS = LOAD_BALANCER_LOGS_TRACKING_HITS;
     this.logApiUrl = `/cloud/project/${this.projectId}/region/${this.region}/loadbalancing/loadbalancer/${this.loadbalancerId}/log/url`;
     this.logSubscriptionUrl = `/cloud/project/${this.projectId}/region/${this.region}/loadbalancing/loadbalancer/${this.loadbalancerId}/log/subscription`;
-    this.logServiceGuideLink = LOGS_INFO.DEFAULT;
+    this.logServiceGuideLink =
+      LOAD_BALANCER_LOGS_SERVICE_GUIDE_LINK[this.user.ovhSubsidiary] ||
+      LOAD_BALANCER_LOGS_SERVICE_GUIDE_LINK.DEFAULT;
   }
 }
