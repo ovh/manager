b67c3cb28202af6b08eb1fdf9a54b77b5e3aae95
LIDRISSI Hamid <abdelghani-lidrissi.hamid.ext@ovhcloud.com>
2024-05-13 15:22:14 +0200

    feat(pci-private-network): add onboarding page
    ref: DTCORE-2201
    Signed-off-by: LIDRISSI Hamid <abdelghani-lidrissi.hamid.ext@ovhcloud.com>

--- packages/manager/apps/pci-private-network/src/pages/list/List.page.tsx
+++ packages/manager/apps/pci-private-network/src/pages/list/List.page.tsx
@@ -20,14 +20,15 @@ import {
 } from '@ovhcloud/ods-components/react';
 import { useEffect, useState } from 'react';
 
+import { useNavigation } from '@ovh-ux/manager-react-shell-client';
 import { useTranslation } from 'react-i18next';
 import { Outlet, useLocation, useNavigate, useParams } from 'react-router-dom';
-import { useNavigation } from '@ovh-ux/manager-react-shell-client';
 import useProject from '@/api/hooks/useProject';
-import { PrivateNetworkTabName } from '@/constants';
 import GlobalRegionsComponent from '@/components/global-regions/GlobalRegions.component';
 import LocalZoneComponent from '@/components/local-zones/LocalZone.component';
+import { PrivateNetworkTabName } from '@/constants';
 import { useAnnouncementBanner } from '@/hooks/useAnnouncement';
+import ListGuard from './ListGuard';
 
 const getActiveTab = (pathname: string) => {
   if (pathname.includes('localZone')) {
@@ -70,75 +71,79 @@ export default function ListingPage() {
   }, [projectId, navigation]);
 
   return (
-    <>
-      {project && (
-        <OsdsBreadcrumb
-          items={[
-            {
-              href: projectUrl,
-              label: project.description,
-            },
-            {
-              label: t('pci_projects_project_network_private'),
-            },
-          ]}
-        />
-      )}
-      <div className="header mb-10 mt-8">
-        <div className="flex items-center justify-between">
-          <OsdsText
-            level={ODS_THEME_TYPOGRAPHY_LEVEL.heading}
-            size={ODS_THEME_TYPOGRAPHY_SIZE._600}
-            color={ODS_THEME_COLOR_INTENT.primary}
-          >
-            {t('pci_projects_project_network_private')}
-          </OsdsText>
-          <PciGuidesHeader category="instances"></PciGuidesHeader>
-        </div>
-      </div>
-
-      {isBannerVisible && <PciAnnouncementBanner projectId={projectId} />}
-
-      <div className="mb-5">
-        {isDiscoveryProject(project) && (
-          <PciDiscoveryBanner projectId={projectId} />
+    <ListGuard projectId={projectId}>
+      <>
+        {project && (
+          <OsdsBreadcrumb
+            items={[
+              {
+                href: projectUrl,
+                label: project.description,
+              },
+              {
+                label: t('pci_projects_project_network_private'),
+              },
+            ]}
+          />
         )}
-      </div>
+        <div className="header mb-10 mt-8">
+          <div className="flex items-center justify-between">
+            <OsdsText
+              level={ODS_THEME_TYPOGRAPHY_LEVEL.heading}
+              size={ODS_THEME_TYPOGRAPHY_SIZE._600}
+              color={ODS_THEME_COLOR_INTENT.primary}
+            >
+              {t('pci_projects_project_network_private')}
+            </OsdsText>
+            <PciGuidesHeader category="instances"></PciGuidesHeader>
+          </div>
+        </div>
 
-      <OsdsTabs
-        panel={activeTab}
-        onOdsTabsChanged={(event) => handlerTabChanged(event)}
-      >
-        <OsdsTabBar slot="top">
-          <OsdsTabBarItem
-            panel={PrivateNetworkTabName.GLOBAL_REGIONS_TAB_NAME}
-            className="flex items-center justify-center"
-          >
-            {t('pci_projects_project_network_private_global_regions_tab_label')}
-          </OsdsTabBarItem>
-          <OsdsTabBarItem
-            panel={PrivateNetworkTabName.LOCAL_ZONE_TAB_NAME}
-            className="flex items-center justify-center"
-          >
-            {t('pci_projects_project_network_private_local_zones_tab_label')}
-          </OsdsTabBarItem>
-        </OsdsTabBar>
+        {isBannerVisible && <PciAnnouncementBanner projectId={projectId} />}
 
-        <OsdsTabPanel name={PrivateNetworkTabName.GLOBAL_REGIONS_TAB_NAME}>
-          {activeTab === PrivateNetworkTabName.GLOBAL_REGIONS_TAB_NAME && (
-            <GlobalRegionsComponent
-              projectId={projectId}
-              projectUrl={projectUrl}
-            />
+        <div className="mb-5">
+          {isDiscoveryProject(project) && (
+            <PciDiscoveryBanner projectId={projectId} />
           )}
-        </OsdsTabPanel>
-        <OsdsTabPanel name={PrivateNetworkTabName.LOCAL_ZONE_TAB_NAME}>
-          {activeTab === PrivateNetworkTabName.LOCAL_ZONE_TAB_NAME && (
-            <LocalZoneComponent projectId={projectId} />
-          )}
-        </OsdsTabPanel>
-      </OsdsTabs>
-      <Outlet />
-    </>
+        </div>
+
+        <OsdsTabs
+          panel={activeTab}
+          onOdsTabsChanged={(event) => handlerTabChanged(event)}
+        >
+          <OsdsTabBar slot="top">
+            <OsdsTabBarItem
+              panel={PrivateNetworkTabName.GLOBAL_REGIONS_TAB_NAME}
+              className="flex items-center justify-center"
+            >
+              {t(
+                'pci_projects_project_network_private_global_regions_tab_label',
+              )}
+            </OsdsTabBarItem>
+            <OsdsTabBarItem
+              panel={PrivateNetworkTabName.LOCAL_ZONE_TAB_NAME}
+              className="flex items-center justify-center"
+            >
+              {t('pci_projects_project_network_private_local_zones_tab_label')}
+            </OsdsTabBarItem>
+          </OsdsTabBar>
+
+          <OsdsTabPanel name={PrivateNetworkTabName.GLOBAL_REGIONS_TAB_NAME}>
+            {activeTab === PrivateNetworkTabName.GLOBAL_REGIONS_TAB_NAME && (
+              <GlobalRegionsComponent
+                projectId={projectId}
+                projectUrl={projectUrl}
+              />
+            )}
+          </OsdsTabPanel>
+          <OsdsTabPanel name={PrivateNetworkTabName.LOCAL_ZONE_TAB_NAME}>
+            {activeTab === PrivateNetworkTabName.LOCAL_ZONE_TAB_NAME && (
+              <LocalZoneComponent projectId={projectId} />
+            )}
+          </OsdsTabPanel>
+        </OsdsTabs>
+        <Outlet />
+      </>
+    </ListGuard>
   );
 }
