3a59945f8ae08330ae9f37a466a626f8b4484558
Yann Lojewski <yann.lojewski.ext@ovhcloud.com>
2024-05-16 11:29:21 +0200

    feat(pci): create file-storage app
    ref: TAPC-225
    Signed-off-by: Yann Lojewski <yann.lojewski.ext@ovhcloud.com>

---
+++ packages/manager/apps/pci-file-storage/src/routes.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { RouteObject } from 'react-router-dom';
+import NotFound from './pages/404';
+
+const lazyRouteConfig = (importFn: CallableFunction): Partial<RouteObject> => {
+  return {
+    lazy: async () => {
+      const { default: moduleDefault, ...moduleExports } = await importFn();
+      return {
+        Component: moduleDefault,
+        ...moduleExports,
+      };
+    }
+  };
+};
+
+export const Routes: any = [
+  {
+    path: '/pci/projects/:projectId/file-storage',
+    ...lazyRouteConfig(() => import('@/pages/layout')),
+    children: [
+      {
+        id: 'listing',
+        path: '',
+        ...lazyRouteConfig(() => import('@/pages/listing')),
+      },
+      {
+        path: ':serviceName',
+        ...lazyRouteConfig(() => import('@/pages/dashboard/_layout')),
+        children: [
+          {
+            id: 'dashboard',
+            path: '',
+            ...lazyRouteConfig(() => import('@/pages/dashboard/index')),
+          },
+          {
+            id: 'dashboard.tab2',
+            path: 'Tab2',
+            ...lazyRouteConfig(() => import('@/pages/dashboard/Tab2')),
+          },
+        ],
+      },
+      {
+        id: 'onboarding',
+        path: 'onboarding',
+        ...lazyRouteConfig(() => import('@/pages/onboarding')),
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+];
