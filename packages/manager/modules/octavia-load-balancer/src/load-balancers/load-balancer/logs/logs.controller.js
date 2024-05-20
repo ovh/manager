de7fab9d1bbabbb03db330aa42187402e2d25910
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2024-05-20 17:32:58 +0200

    feat(octavia-load-balancer): update <log-to-customer /> to 2.0.0
    ref: MANAGER-14235
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

--- packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/logs/logs.controller.js
+++ packages/manager/modules/octavia-load-balancer/src/load-balancers/load-balancer/logs/logs.controller.js
@@ -1,6 +1,7 @@
 import {
   LOAD_BALANCER_LOGS_SERVICE_GUIDE_LINK,
   LOAD_BALANCER_LOGS_TRACKING_HITS,
+  LOAD_BALANCER_LOG_KINDS_KEYS,
 } from './logs.constants';
 
 export default class OctaviaLoadBalancerLogsController {
@@ -16,5 +17,6 @@ export default class OctaviaLoadBalancerLogsController {
     this.logServiceGuideLink =
       LOAD_BALANCER_LOGS_SERVICE_GUIDE_LINK[this.user.ovhSubsidiary] ||
       LOAD_BALANCER_LOGS_SERVICE_GUIDE_LINK.DEFAULT;
+    this.logKindsKeys = LOAD_BALANCER_LOG_KINDS_KEYS;
   }
 }
