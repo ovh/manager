854575d4b1deef32e8d66044b602e4c0ee0946cb
vovh <155978769+vovh@users.noreply.github.com>
2024-05-23 16:39:12 +0200

    feat(key-management-service): add kms dashboard (#11664)
    ref: MANAGER-13567
    
    Signed-off-by: Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>

--- packages/manager/apps/key-management-service/src/hooks/useOKMS.ts
+++ packages/manager/apps/key-management-service/src/hooks/useOKMS.ts
@@ -1,14 +1,36 @@
 import { useQuery } from '@tanstack/react-query';
-import { OKMSOptions, sortOKMS, getListingIceberg } from '@/api';
+import {
+  OKMSOptions,
+  sortOKMS,
+  getListingIceberg,
+  ErrorResponse,
+  getOkmsServicesResourceListQueryKey,
+  getOkmsResourceQueryKey,
+} from '@/api';
+import { getOKMSResource } from '@/api/hooks/useApiOkms';
+import { OKMS } from '@/interface';
 
 export const useAllOKMS = () => {
   return useQuery({
-    queryKey: ['okms/ressource'],
+    queryKey: getOkmsServicesResourceListQueryKey,
     queryFn: () => getListingIceberg(),
     retry: false,
     ...{
       keepPreviousData: true,
     },
+    refetchInterval: 5000,
+  });
+};
+
+export const useOKMSById = (okmsId: string) => {
+  return useQuery<{ data: OKMS }, ErrorResponse>({
+    queryKey: getOkmsResourceQueryKey(okmsId),
+    queryFn: () => getOKMSResource(okmsId),
+    retry: false,
+    ...{
+      keepPreviousData: true,
+    },
+    refetchInterval: 5000,
   });
 };
 
