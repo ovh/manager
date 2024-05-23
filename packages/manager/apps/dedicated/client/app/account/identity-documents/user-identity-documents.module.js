759a310773a6bb0d49814d8dd205f22696553411
Maxime Bajeux <max.bajeux@gmail.com>
2024-05-23 17:53:10 +0200

    feat(dedicated): missing guidance to reduce kyc upload issues (#11155)
    ref: MANAGER-13105
    
    Signed-off-by: Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/apps/dedicated/client/app/account/identity-documents/user-identity-documents.module.js
+++ packages/manager/apps/dedicated/client/app/account/identity-documents/user-identity-documents.module.js
@@ -4,6 +4,7 @@ import ngOvhHttp from '@ovh-ux/ng-ovh-http';
 import component from './user-identitiy-documents.component';
 import routing from './user-identity-documents.routes';
 import uploadConfirmModule from './upload-identity-documents-confirm';
+import uploadDocumentsRequirements from './components/identity-documents-requirements';
 
 const moduleName = 'DedicatedUserAccountIdentityDocuments';
 
@@ -12,6 +13,7 @@ angular
     ngAtInternet,
     ngOvhHttp,
     uploadConfirmModule,
+    uploadDocumentsRequirements,
     'oui',
     'pascalprecht.translate',
     'ui.router',
