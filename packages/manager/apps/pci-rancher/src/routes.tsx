4af99f86dc9e2219f2a1e6df840cb20a9ad27f97
Lio B <lionel95200x@users.noreply.github.com>
2024-05-23 15:25:14 +0200

    feat(pci-rancher): add request tagger on each request (#11282)
    ref: MANAGER-14030
    
    Signed-off-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>

--- packages/manager/apps/pci-rancher/src/routes.tsx
+++ packages/manager/apps/pci-rancher/src/routes.tsx
@@ -1,3 +1,4 @@
+import { PageType } from '@ovh-ux/manager-react-shell-client';
 import React from 'react';
 import NotFound from './pages/404';
 
@@ -16,6 +17,16 @@ const lazyRouteConfig = (importFn: CallableFunction) => {
 
 export const COMMON_PATH = '/pci/projects';
 
+export enum Route {
+  Listing = 'listing',
+  Dashboard = 'dashboard',
+  Onboarding = 'onboarding',
+  New = 'New',
+  DeleteRancher = 'DeleteRancher',
+  EditRancherName = 'EditRancherName',
+  GenerateAccess = 'GenerateAccess',
+}
+
 export default [
   {
     path: '/pci/projects/:projectId',
@@ -30,32 +41,74 @@ export default [
             ...lazyRouteConfig(() =>
               import('@/pages/listing/delete/DeleteRancherModal'),
             ),
+            handle: {
+              tracking: {
+                pageName: Route.DeleteRancher,
+                pageType: PageType.popup,
+              },
+            },
           },
         ],
+        handle: {
+          tracking: {
+            pageName: Route.Listing,
+            pageType: PageType.listing,
+          },
+        },
       },
       {
         path: 'rancher/onboarding',
         ...lazyRouteConfig(() => import('@/pages/onboarding')),
+        handle: {
+          tracking: {
+            pageName: Route.Onboarding,
+            pageType: PageType.onboarding,
+          },
+        },
       },
       {
         path: 'rancher/new',
         ...lazyRouteConfig(() => import('@/pages/create')),
+        handle: {
+          tracking: {
+            pageName: Route.New,
+            pageType: PageType.funnel,
+          },
+        },
       },
       {
         path: 'rancher/:rancherId',
         ...lazyRouteConfig(() => import('@/pages/dashboard')),
+        handle: {
+          tracking: {
+            pageName: Route.Dashboard,
+            pageType: PageType.dashboard,
+          },
+        },
         children: [
           {
             path: 'edit',
             ...lazyRouteConfig(() =>
               import('@/pages/dashboard/edit-modal/EditModal'),
             ),
+            handle: {
+              tracking: {
+                pageName: Route.EditRancherName,
+                pageType: PageType.popup,
+              },
+            },
           },
           {
             path: 'generate-access',
             ...lazyRouteConfig(() =>
               import('@/pages/dashboard/generate-access/GenerateAccessModal'),
             ),
+            handle: {
+              tracking: {
+                pageName: Route.GenerateAccess,
+                pageType: PageType.popup,
+              },
+            },
           },
         ],
       },
