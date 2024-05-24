17cf3b7940941388365d46b523e9a4d0ef3454c8
darsene <72250222+darsene@users.noreply.github.com>
2024-05-24 16:12:37 +0200

    feat(key-management-service): update dashboard header (#11811)
    ref: MANAGER-13567
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/key-management-service/src/components/layout-helpers/Dashboard/Dashboard.tsx
+++ packages/manager/apps/key-management-service/src/components/layout-helpers/Dashboard/Dashboard.tsx
@@ -5,14 +5,21 @@ import {
   OsdsTabs,
   OsdsTabBar,
   OsdsTabBarItem,
+  OsdsChip,
 } from '@ovhcloud/ods-components/react';
-import { ODS_TEXT_LEVEL, ODS_TEXT_SIZE } from '@ovhcloud/ods-components';
+import {
+  ODS_CHIP_SIZE,
+  ODS_TEXT_LEVEL,
+  ODS_TEXT_SIZE,
+} from '@ovhcloud/ods-components';
 import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
+import { useTranslation } from 'react-i18next';
 
 export type DashboardTabItemProps = {
   name: string;
   title: string;
-  to: string;
+  disabled?: boolean;
+  to?: string;
 };
 
 export type DashboardLayoutProps = {
@@ -23,6 +30,7 @@ const Dashboard: React.FC<DashboardLayoutProps> = ({ tabs }) => {
   const [panel, setActivePanel] = useState('');
   const location = useLocation();
   const navigate = useNavigate();
+  const { t } = useTranslation('key-management-service/dashboard');
 
   useEffect(() => {
     const activeTab = tabs.find((tab) => tab.to === location.pathname);
@@ -51,10 +59,17 @@ const Dashboard: React.FC<DashboardLayoutProps> = ({ tabs }) => {
             <OsdsTabBarItem
               key={`osds-tab-bar-item-${tab.name}`}
               panel={tab.name}
+              disabled={tab.disabled}
+              className="flex items-center justify-center"
             >
               <NavLink to={tab.to} className="no-underline">
                 {tab.title}
               </NavLink>
+              {tab.disabled && (
+                <OsdsChip size={ODS_CHIP_SIZE.sm} inline className="ml-2">
+                  {t('key_management_service_dashboard_tab_comming_soon')}
+                </OsdsChip>
+              )}
             </OsdsTabBarItem>
           ))}
         </OsdsTabBar>
