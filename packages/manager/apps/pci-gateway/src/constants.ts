72ad0b8aced80e94de0466a93cee3a2d2b113415
Yoann Fievez <yoann.fievez@ovhcloud.com>
2024-05-27 11:39:54 +0200

    feat(pci-gateway): add link for 'add_learn_more' ref: DTCORE-2238
    Signed-off-by: Yoann Fievez <yoann.fievez@ovhcloud.com>

--- packages/manager/apps/pci-gateway/src/constants.ts
+++ packages/manager/apps/pci-gateway/src/constants.ts
@@ -1,18 +1,6 @@
-export const ACTIVE_STATUS = ['ok', 'READY'];
-export const PENDING_STATUS = ['PENDING', 'CREATING', 'UPDATING', 'DELETING'];
-
-export const HORIZON_LINK = {
-  EU: 'https://horizon.cloud.ovh.net/auth/login?username={username}',
-  CA: 'https://horizon.cloud.ovh.net/auth/login?username={username}',
-  US: 'https://horizon.cloud.ovh.us/auth/login?username={username}',
+export const PUBLIC_GATEWAYS_READ_MORE_GUIDE = {
+  ALL_GUIDE: {
+    DEFAULT:
+      'https://docs.ovh.com/gb/en/public-cloud/creating-private-network-with-gateway/',
+  },
 };
-
-export const HORIZON_LINK_TRUSTED = {
-  EU:
-    'https://horizon.trustedzone.cloud.ovh.net/auth/login?username={username}',
-  CA:
-    'https://horizon.trustedzone.cloud.ovh.net/auth/login?username={username}',
-  US: '',
-};
-
-export const ALPHA_CHARACTERS_REGEX = /^[a-zA-Z-]+$/;
