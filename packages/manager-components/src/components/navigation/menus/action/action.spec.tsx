ba1ea4bde0c7526e43a26621d73c20c8092f61f1
darsene <72250222+darsene@users.noreply.github.com>
2024-05-30 17:12:45 +0200

    feat(manager-components): add compact vertical ellipsis action menu (#11815)
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager-components/src/components/navigation/menus/action/action.spec.tsx
+++ packages/manager-components/src/components/navigation/menus/action/action.spec.tsx
@@ -1,4 +1,5 @@
 import { waitFor, screen } from '@testing-library/react';
+import { ODS_ICON_NAME } from '@ovhcloud/ods-components';
 import { actionMenu } from './action.stories';
 import { ActionMenu, ActionMenuProps } from './action.component';
 import { render } from '../../../../utils/test.provider';
@@ -20,9 +21,17 @@ describe('ActionMenu', () => {
     expect(actionMenuIcon.getAttribute('name')).toBe('arrow-down-concept');
   });
 
-  it('renders compact menu correctly', async () => {
+  it('renders compact menu with classic ellipsis correctly', async () => {
     await setupSpecTest({ isCompact: true });
     const actionMenuIcon = screen.getByTestId('action-menu-icon');
     expect(actionMenuIcon.getAttribute('name')).toBe('ellipsis');
+  });
+
+  it('renders compact menu with custom icon menu correctly', async () => {
+    await setupSpecTest({
+      icon: ODS_ICON_NAME.ELLIPSIS_VERTICAL,
+    });
+    const actionMenuIcon = screen.getByTestId('action-menu-icon');
+    expect(actionMenuIcon.getAttribute('name')).toBe('ellipsis-vertical');
   });
 });
