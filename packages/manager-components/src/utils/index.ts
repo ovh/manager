f865ef564c158bac3dbbecbe5c69831a78de7bb6
Mohammed Hamdoune <sidlynx@gmail.com>
2024-05-30 17:45:25 +0200

    fix(pci-gateway): location on regions input
    location displayed even with no stack on tiles input
    continent is reset after change on regions input
    
    ref: DTCORE-2240 & DTCORE-2245
    
    Signed-off-by: Mohammed Hamdoune <sidlynx@gmail.com>

--- packages/manager-components/src/utils/index.ts
+++ packages/manager-components/src/utils/index.ts
@@ -9,3 +9,33 @@ export const uniqBy = function uniqBy<I, U>(arr: I[], cb: (item: I) => U) {
       .values(),
   ];
 };
+
+// eslint-disable-next-line
+export const hashCode = (param: any) => {
+  let h = 0;
+  const s = ((p): string => {
+    switch (typeof p) {
+      case 'number':
+        return `${p}`;
+      case 'bigint':
+        return `${p}`;
+      case 'string':
+        return `${p}`;
+      case 'boolean':
+        return `${p}`;
+      case 'object':
+        return JSON.stringify(p);
+      case 'function':
+        return 'function';
+      case 'undefined':
+        return 'undefined';
+      default:
+        return 'symbol';
+    }
+  })(param);
+  const l = s?.length || 0;
+  let i = 0;
+  // eslint-disable-next-line
+  if (l > 0) while (i < l) h = ((h << 5) - h + s.charCodeAt(i++)) | 0;
+  return h;
+};
