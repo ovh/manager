d46c361392e3480fec11ebe5469dc776564b4177
Mohammed Hamdoune <sidlynx@gmail.com>
2024-05-30 17:54:59 +0200

    fix(pci-gateway): use addonFamily param instead of planFamily
    correct param when loading gateway plans
    
    ref: DTCORE-2251
    
    Signed-off-by: Mohammed Hamdoune <sidlynx@gmail.com>

--- packages/manager/apps/pci-gateway/src/api/data/gateway-plans.ts
+++ packages/manager/apps/pci-gateway/src/api/data/gateway-plans.ts
@@ -16,7 +16,7 @@ export const getAvailableGatewayPlans = async (
   projectId: string,
   ovhSubsidiary: string,
 ): Promise<TAvailableGatewayPlansResponse> => {
-  const url = `/cloud/project/${projectId}/capabilities/productAvailability?ovhSubsidiary=${ovhSubsidiary}&planFamily=gateway`;
+  const url = `/cloud/project/${projectId}/capabilities/productAvailability?ovhSubsidiary=${ovhSubsidiary}&addonFamily=gateway`;
   const { data } = await v6.get<TAvailableGatewayPlansResponse>(url);
   return data;
 };
