1aca3d2203561470d9db04b20bfd6ce4dc61dd71
JacquesLarique <134954692+JacquesLarique@users.noreply.github.com>
2024-05-31 17:18:44 +0200

    feat(2fa): set up user session management (#11843)
    ref: MANAGER-13716
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/apps/account-disable-2fa/src/index.tsx
+++ packages/manager/apps/account-disable-2fa/src/index.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { decodeToken, extractToken } from '@/utils/token';
 import initI18n from './i18n';
+import initAuthenticationInterceptor from '@/data/authentication.interceptor';
 import initInterceptor from './data/invisible-challenge.interceptor';
 
 import './global.css';
 import './index.scss';
+import UserProvider from '@/context/User/provider';
 
-// const token = ???;
-// const user = decodeJWT(token);
-const user = {
-  legalform: 'individual',
-  email: 'monsieur.madame@ovhcloud.com',
-  language: 'fr_FR',
-  subsidiary: 'FR',
-};
-const locale = user.language;
+const token = extractToken();
+const user = decodeToken(token);
+const { language: locale, subsidiary } = user || {};
 
-initI18n(locale, [locale], user.subsidiary);
+initI18n(locale, [locale], subsidiary);
+initAuthenticationInterceptor(token);
 initInterceptor();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <UserProvider user={user}>
+      <App />
+    </UserProvider>
   </React.StrictMode>,
 );
