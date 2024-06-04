c12d0c61cbc28f0e1ecf3c825a713f5afcecf2d7
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-06-04 13:35:59 +0200

    feat: change redis name
    DATATR-969
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/apps/pci-databases-analytics/src/pages/_components/legalMentions.tsx
+++ packages/manager/apps/pci-databases-analytics/src/pages/_components/legalMentions.tsx
@@ -2,18 +2,13 @@ import { useTranslation } from 'react-i18next';
 import { cn } from '@/lib/utils';
 
 interface LegalMentionsProps {
-  showRedisMessage: boolean;
   className?: string;
 }
-const LegalMentions = ({
-  showRedisMessage = false,
-  className,
-}: LegalMentionsProps) => {
+const LegalMentions = ({ className }: LegalMentionsProps) => {
   const { t } = useTranslation('pci-databases-analytics');
   return (
     <div className={cn('border border-primary-100 p-4 rounded-md', className)}>
       <p>{t('legal-general')}</p>
-      {showRedisMessage && <p>{t('legal-redis')}</p>}
     </div>
   );
 };
