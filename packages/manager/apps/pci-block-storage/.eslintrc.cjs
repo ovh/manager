992468c7e5b13043ee303d83a84e644deeab2de9
Yoann Fievez <yoann.fievez@ovhcloud.com>
2024-06-06 14:07:46 +0200

    feat(pci-block-storage): init react app
    ref: DTCORE-2267
    Signed-off-by: Yoann Fievez <yoann.fievez@gmail.com>

---
+++ packages/manager/apps/pci-block-storage/.eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: [
+    '../../../../.eslintrc.js',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  globals: {
+    '__VERSION__': true,
+  },
+  rules: {
+    'import/extensions': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'react/no-deprecated': 'off',
+    'arrow-body-style': ["error", "as-needed"],
+    'prefer-const': 'error',
+  },
+};
---
+++ packages/manager/apps/pci-block-storage/src/tracking.constants.ts
@@ -0,0 +1,3 @@
+export const PCI_LEVEL2 = '86';
+export const PAGE_PREFIX = 'PublicCloud::pci::projects::project';
+export const DISCOVERY_PLANCODE = 'project.discovery';
