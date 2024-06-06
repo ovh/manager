d9b00efda5c6d4149bf257672ff291c3d48c7416
Yoann Fievez <yoann.fievez@ovhcloud.com>
2024-06-06 14:07:46 +0200

    feat(pci-block-storage): init react app
    ref: DTCORE-2267
    Signed-off-by: Yoann Fievez <yoann.fievez@ovhcloud.com>

--- jest.config.js
+++ jest.config.js
@@ -30,6 +30,7 @@ module.exports = {
         '/apps/pci-public-ip',
         '/apps/pci-gateway/',
         '/apps/pci-private-network',
+        '/apps/pci-block-storage/',
       ],
       moduleNameMapper: {
         '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
