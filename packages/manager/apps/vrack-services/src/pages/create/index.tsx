5ceb922cc67869ffc20763c28b54594ef94828f6
qpavy <contact@quentinpavy.com>
2024-06-06 10:58:34 +0200

    feat(vrack-services): change display name with iam display name (#11717)
    * feat(vrack-services): change display name with iam display name
    
    ref: MANAGER-13968
    
    Signed-off-by: Quentin Pavy <contact@quentinpavy.com>
    
    * feat(vrack-services): fix pr comments
    
    ref: MANAGER-13968
    
    Signed-off-by: Quentin Pavy <contact@quentinpavy.com>
    
    * feat(vrack-services): iam display name in efs
    
    ref: MANAGER-13968
    
    Signed-off-by: Quentin Pavy <contact@quentinpavy.com>
    
    * feat(vrack-services): improve update name message
    
    ref: MANAGER-13968
    
    Signed-off-by: Quentin Pavy <contact@quentinpavy.com>
    
    * feat(vrack-services): fix pr comments
    
    ref: MANAGER-13968
    
    Signed-off-by: Quentin Pavy <contact@quentinpavy.com>
    
    * feat(vrack-services): remove unused code
    
    ref: MANAGER-13968
    
    Signed-off-by: Quentin Pavy <contact@quentinpavy.com>
    
    * test(vrack-services): fix update name tests
    
    ref: MANAGER-13968
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
    
    * fix(i18n): add missing translations [CDS 3014]
    
    Signed-off-by: CDS Translator Agent <opensource@ovh.net>
    
    * feat(vrack-services): remove displayname from order
    
    ref: MANAGER-13968
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
    
    ---------
    
    Signed-off-by: Quentin Pavy <contact@quentinpavy.com>
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
    Signed-off-by: CDS Translator Agent <opensource@ovh.net>
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
    Co-authored-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
    Co-authored-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager/apps/vrack-services/src/pages/create/index.tsx
+++ packages/manager/apps/vrack-services/src/pages/create/index.tsx
@@ -2,19 +2,6 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
-import {
-  OsdsInput,
-  OsdsText,
-  OsdsFormField,
-} from '@ovhcloud/ods-components/react';
-import {
-  ODS_INPUT_SIZE,
-  ODS_INPUT_TYPE,
-  OdsInputValueChangeEvent,
-  ODS_TEXT_LEVEL,
-  ODS_TEXT_SIZE,
-} from '@ovhcloud/ods-components';
-import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
 import { ApiError, ApiResponse } from '@ovh-ux/manager-core-api';
 import {
   getvrackServicesReferenceRegionList,
@@ -24,12 +11,10 @@ import {
 import { ErrorPage } from '@/components/Error';
 import { RegionFormField } from './components/RegionFormField';
 import { CreatePageLayout } from '@/components/layout-helpers';
-import { displayNameInputName } from './constants';
 import { urls } from '@/router/constants';
 
 export default function CreationPage() {
   const [selectedRegion, setSelectedRegion] = React.useState('');
-  const [displayName, setDisplayName] = React.useState('');
   const { t } = useTranslation('vrack-services/create');
   const navigate = useNavigate();
 
@@ -53,34 +38,11 @@ export default function CreationPage() {
         createButtonLabel={t('createButtonLabel')}
         confirmActionsTracking={['activate_vrack-service', selectedRegion]}
         onSubmit={() =>
-          navigate(urls.createConfirm.replace(':region', selectedRegion), {
-            state: { displayName },
-          })
+          navigate(urls.createConfirm.replace(':region', selectedRegion))
         }
         title={t('title')}
         isFormSubmittable={!isRegionLoading && !!selectedRegion}
       >
-        <OsdsFormField inline>
-          <div slot="label">
-            <OsdsText
-              color={ODS_THEME_COLOR_INTENT.text}
-              level={ODS_TEXT_LEVEL.body}
-              size={ODS_TEXT_SIZE._200}
-            >
-              {t('displayNameLabel')}
-            </OsdsText>
-          </div>
-          <OsdsInput
-            inline
-            name={displayNameInputName}
-            size={ODS_INPUT_SIZE.md}
-            type={ODS_INPUT_TYPE.text}
-            placeholder={t('displayNamePlaceholder')}
-            onOdsValueChange={(e: OdsInputValueChangeEvent) =>
-              setDisplayName(e?.detail.value || '')
-            }
-          />
-        </OsdsFormField>
         <RegionFormField
           selectedRegion={selectedRegion}
           setSelectedRegion={setSelectedRegion}
