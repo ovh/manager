2076ca8f4e151c826006451c8cec1109c2697b9e
Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>
2024-06-07 17:40:03 +0200

    feat(2fa): add exp session modal
    ref: MANAGER-14237
    
    Signed-off-by: Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>

--- packages/manager/apps/account-disable-2fa/src/context/User/provider.tsx
+++ packages/manager/apps/account-disable-2fa/src/context/User/provider.tsx
@@ -1,5 +1,8 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import userContext, { User } from '@/context/User/context';
+import { ExpiredSessionModal } from './modals/ExpiredSessionModal';
+import { WarningSessionModal } from './modals/WarningSessionModal';
+import { useSessionModal } from './useSessionModal';
 
 type Props = {
   children: JSX.Element | JSX.Element[];
@@ -7,8 +10,15 @@ type Props = {
 };
 
 export const UserProvider = ({ children = null, user }: Props): JSX.Element => {
-  // If we don't have a user or the "session" is expired we redirect the user on the login page
-  if (!user || user.exp * 1000 < Date.now()) {
+  const {
+    setShowExpiredModal,
+    setShowWarningModal,
+    showExpiredModal,
+    showWarningModal,
+  } = useSessionModal(user, 0.75);
+
+  const handleCloseExpiredModal = useCallback(() => {
+    setShowExpiredModal(false);
     const loginUrl =
       window.location.host === 'www.ovhtelecom.fr'
         ? 'https://www.ovh.com/auth/'
@@ -19,11 +29,24 @@ export const UserProvider = ({ children = null, user }: Props): JSX.Element => {
       : '';
 
     window.location.assign(`${loginUrl}${subsidiaryParams}`);
-  }
+  }, [user]);
+
+  const handleCloseWarningModal = () => setShowWarningModal(false);
+
   const memoizedUser = useMemo(() => ({ user }), []);
 
   return (
-    <userContext.Provider value={memoizedUser}>{children}</userContext.Provider>
+    <>
+      <userContext.Provider value={memoizedUser}>
+        {children}
+      </userContext.Provider>
+      {showExpiredModal && (
+        <ExpiredSessionModal onClose={handleCloseExpiredModal} />
+      )}
+      {showWarningModal && (
+        <WarningSessionModal onClose={handleCloseWarningModal} />
+      )}
+    </>
   );
 };
 
