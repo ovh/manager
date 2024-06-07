0373f1ec9c818a8108d5db3d0773c434bdaf5f80
Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>
2024-06-07 17:40:03 +0200

    feat(2fa): add exp session modal
    ref: MANAGER-14237
    
    Signed-off-by: Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>

---
+++ packages/manager/apps/procedures/src/components/Icons/InformationIcon.tsx
@@ -0,0 +1,15 @@
+import React, { FunctionComponent } from 'react';
+import { ODS_ICON_NAME, ODS_ICON_SIZE } from '@ovhcloud/ods-components';
+import { OsdsIcon } from '@ovhcloud/ods-components/react';
+import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
+
+export const InformationIcon: FunctionComponent = () => (
+  <div className="rounded-full p-4 bg-[#bef1ff]">
+    <OsdsIcon
+      size={ODS_ICON_SIZE.xl}
+      className="block"
+      color={ODS_THEME_COLOR_INTENT.text}
+      name={ODS_ICON_NAME.INFO_CIRCLE}
+    ></OsdsIcon>
+  </div>
+);
