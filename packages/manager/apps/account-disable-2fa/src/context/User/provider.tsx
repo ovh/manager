a103c6f07eb6209ad626a84d7f0de86f8f632666
Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>
2024-06-07 18:29:47 +0200

    feat(account-disable-2fa): fix pr feedback ref: MANAGER-14237
    Signed-off-by: Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>

--- packages/manager/apps/account-disable-2fa/src/context/User/provider.tsx
+++ packages/manager/apps/account-disable-2fa/src/context/User/provider.tsx
@@ -1,8 +1,5 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import userContext, { User } from '@/context/User/context';
-import { ExpiredSessionModal } from './modals/ExpiredSessionModal';
-import { WarningSessionModal } from './modals/WarningSessionModal';
-import { useSessionModal } from './useSessionModal';
 
 type Props = {
   children: JSX.Element | JSX.Element[];
@@ -10,44 +7,21 @@ type Props = {
 };
 
 export const UserProvider = ({ children = null, user }: Props): JSX.Element => {
-  const {
-    setShowExpiredModal,
-    setShowWarningModal,
-    showExpiredModal,
-    showWarningModal,
-  } = useSessionModal(user, 0.75);
-
-  const handleCloseExpiredModal = useCallback(() => {
-    setShowExpiredModal(false);
+  // If we don't have a user or the "session" is expired we redirect the user on the login page
+  if (!user || user.exp * 1000 < Date.now()) {
     const loginUrl =
       window.location.host === 'www.ovhtelecom.fr'
         ? 'https://www.ovh.com/auth/'
         : '/auth';
-
     const subsidiaryParams = user?.subsidiary
       ? `?ovhSubsidiary=${user.subsidiary}`
       : '';
 
     window.location.assign(`${loginUrl}${subsidiaryParams}`);
-  }, [user]);
-
-  const handleCloseWarningModal = () => setShowWarningModal(false);
-
+  }
   const memoizedUser = useMemo(() => ({ user }), []);
 
   return (
-    <>
-      <userContext.Provider value={memoizedUser}>
-        {children}
-      </userContext.Provider>
-      {showExpiredModal && (
-        <ExpiredSessionModal onClose={handleCloseExpiredModal} />
-      )}
-      {showWarningModal && (
-        <WarningSessionModal onClose={handleCloseWarningModal} />
-      )}
-    </>
+    <userContext.Provider value={memoizedUser}>{children}</userContext.Provider>
   );
 };
-
-export default UserProvider;
