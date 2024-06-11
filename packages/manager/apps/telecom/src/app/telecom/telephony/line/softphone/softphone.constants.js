58c3e3663cc3a0ac68ec31ea7b27e91480dbc24f
Antony MARION <antony.marion.ext@ovhcloud.com>
2024-06-11 07:16:00 +0200

    feat(telecom): add softphone configuration
      ref: MANAGER-13662
    
    Signed-off-by: Antony MARION <antony.marion.ext@ovhcloud.com>

--- packages/manager/apps/telecom/src/app/telecom/telephony/line/softphone/softphone.constants.js
+++ packages/manager/apps/telecom/src/app/telecom/telephony/line/softphone/softphone.constants.js
@@ -7,6 +7,14 @@ export const MOBILE_OS = {
   ios: 'ios',
 };
 
+export const STATUS_SOFTPHONE = {
+  registered: 'registered',
+  unregistered: 'unregistered',
+};
+
+export const GUIDE_URL =
+  'https://help.ovhcloud.com/csm/fr-documentation-web-cloud-phone-and-fax-voip?id=kb_browse_cat&kb_id=e17b4f25551974502d4c6e78b7421955&kb_category=3f445d55f49801102d4ca4d466a7fd31';
+
 export default {
   SOFTPHONE_TYPE,
   MOBILE_OS,
