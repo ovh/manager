813a9041910d1cbb8141bb90f9802e571fcfd0f1
Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
2024-06-12 04:47:00 +0200

    feat(pci.project): add agreement conditions for ff italy project
    ref: TAPC-1077
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>

--- packages/manager/modules/pci/src/projects/onboarding/onboarding.controller.js
+++ packages/manager/modules/pci/src/projects/onboarding/onboarding.controller.js
@@ -1,5 +1,6 @@
 import illustration from './assets/onboarding.png';
 import illustrationUs from './assets/onboarding-us.png';
+import { ITALY_AGREEMENT_TEXT } from '../projects.constant';
 
 export default class {
   /* @ngInject */
@@ -19,6 +20,7 @@ export default class {
     this.atInternet = atInternet;
     this.coreConfig = coreConfig;
     this.coreURLBuilder = coreURLBuilder;
+    this.ITALY_AGREEMENT_TEXT = ITALY_AGREEMENT_TEXT;
   }
 
   $onInit() {
