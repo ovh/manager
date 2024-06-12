16e33505301609ecb6237b4cb4c77b0f62728979
Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>
2024-06-12 17:38:44 +0200

    feat(account-disable-2fa): add form legal policy
    ref: MANAGER-14181
    
    Signed-off-by: Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>

---
+++ packages/manager/apps/procedures/src/pages/create/create.constants.ts
@@ -0,0 +1,10 @@
+export const LegalPolicyLinkBySubsidiary = {
+  FR: 'https://www.ovhcloud.com/fr/terms-and-conditions/privacy-policy/',
+  DE: 'https://www.ovhcloud.com/de/terms-and-conditions/privacy-policy/',
+  ES: 'https://www.ovhcloud.com/es-es/terms-and-conditions/privacy-policy/',
+  IT: 'https://www.ovhcloud.com/it/terms-and-conditions/privacy-policy/',
+  PL: 'https://www.ovhcloud.com/pl/terms-and-conditions/privacy-policy/',
+  PT: 'https://www.ovhcloud.com/pt/terms-and-conditions/privacy-policy/',
+  IN: 'https://www.ovhcloud.com/en-in/terms-and-conditions/privacy-policy/',
+  OTHERS: 'https://www.ovhcloud.com/en-gb/terms-and-conditions/privacy-policy/',
+};
