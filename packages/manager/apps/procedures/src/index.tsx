007331ab84ca1ce3beee409e97df693cd2354c74
Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>
2024-06-12 14:31:36 +0200

    feat(account-disable-2fa): redirect the user on the login page
    ref: MANAGER-14237
    
    Signed-off-by: Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>

--- packages/manager/apps/procedures/src/index.tsx
+++ packages/manager/apps/procedures/src/index.tsx
@@ -5,23 +5,29 @@ import { decodeToken, extractToken } from '@/utils/token';
 import initI18n from './i18n';
 import initAuthenticationInterceptor from '@/data/authentication.interceptor';
 import initInterceptor from './data/invisible-challenge.interceptor';
-
 import './global.css';
 import './index.scss';
 import UserProvider from '@/context/User/provider';
+import { getRedirectLoginUrl } from './utils/url-builder';
 
 const token = extractToken();
 const user = decodeToken(token);
 const { language: locale, subsidiary } = user || {};
 
-initI18n(locale, [locale], subsidiary);
-initAuthenticationInterceptor(token);
-initInterceptor();
+// If we don't have a user or the "session" is expired we redirect the user on the login page
+if (!user || user.exp * 1000 < Date.now()) {
+  const redirectUrl = getRedirectLoginUrl(user);
+  window.location.assign(redirectUrl);
+} else {
+  initI18n(locale, [locale], subsidiary);
+  initAuthenticationInterceptor(token);
+  initInterceptor();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <UserProvider user={user}>
-      <App />
-    </UserProvider>
-  </React.StrictMode>,
-);
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <UserProvider user={user}>
+        <App />
+      </UserProvider>
+    </React.StrictMode>,
+  );
+}
---
+++ packages/manager/apps/procedures/src/utils/url-builder.ts
@@ -0,0 +1,14 @@
+import { User } from '@/context/User/context';
+
+export const getRedirectLoginUrl = (user: User) => {
+  const loginUrl =
+    window.location.host === 'www.ovhtelecom.fr'
+      ? 'https://www.ovh.com/auth/'
+      : '/auth';
+
+  const subsidiaryParams = user?.subsidiary
+    ? `?ovhSubsidiary=${user.subsidiary}`
+    : '';
+
+  return `${loginUrl}${subsidiaryParams}`;
+};
