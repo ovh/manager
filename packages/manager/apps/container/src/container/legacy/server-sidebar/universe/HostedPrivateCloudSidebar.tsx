0fe1ffdb59ac3d9aa5ab219563160d34f875c713
darsene <72250222+darsene@users.noreply.github.com>
2024-06-12 12:09:12 +0200

    feat(hpc-vmware-managed-vcd): add new menu HPC VCD (#11975)
    ref: MANAGER-14399
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/container/src/container/legacy/server-sidebar/universe/HostedPrivateCloudSidebar.tsx
+++ packages/manager/apps/container/src/container/legacy/server-sidebar/universe/HostedPrivateCloudSidebar.tsx
@@ -15,6 +15,7 @@ import { useFeatureAvailability } from '@ovh-ux/manager-react-components';
 
 const features = [
   'dedicated-cloud',
+  'hpc-vmware-managed-vcd',
   'dedicated-cloud:sapHanaOrder',
   'nutanix',
   'veeam-enterprise',
@@ -61,7 +62,7 @@ export default function HostedPrivateCloudSidebar() {
     if (feature['dedicated-cloud']) {
       menu.push({
         id: 'hpc-dedicated-cloud',
-        label: t('sidebar_vmware'),
+        label: t('sidebar_vmware_vsphere'),
         icon: getIcon('ovh-font ovh-font-dedicatedCloud'),
         routeMatcher: new RegExp(`^(/configuration)?/dedicated_cloud`),
         async loader() {
@@ -86,6 +87,31 @@ export default function HostedPrivateCloudSidebar() {
                   'EPCC',
                 );
               },
+            })),
+          ];
+        },
+      });
+    }
+
+    if (feature['hpc-vmware-managed-vcd']) {
+      menu.push({
+        id: 'hpc-vcd',
+        label: t('sidebar_vmware_vcd'),
+        icon: getIcon('ovh-font ovh-font-dedicatedCloud'),
+        routeMatcher: new RegExp(`^hpc-vmware-managed-vcd`),
+        async loader() {
+          const services = await loadServices('/vmwareCloudDirector');
+          const icon = getIcon('ovh-font ovh-font-dedicatedCloud');
+          return [
+            {
+              id: 'dedicated-vmware-vcd-all',
+              label: t('sidebar_vmware_all'),
+              href: navigation.getURL('hpc-vmware-managed-vcd', '/'),
+              icon,
+            },
+            ...services.map((service) => ({
+              ...service,
+              icon,
             })),
           ];
         },
