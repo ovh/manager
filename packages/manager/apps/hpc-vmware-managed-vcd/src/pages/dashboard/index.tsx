c8205eeadf3940fcf7d9db3f009a5b0a5738d174
darsene <72250222+darsene@users.noreply.github.com>
2024-06-13 13:45:32 +0200

    feat(hpc-vmware-managed-vcd): init hpc VCD react µapp (#11926)
    ref: MANAGER-14419
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

---
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/pages/dashboard/index.tsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import {
+  Outlet,
+  NavLink,
+  useLocation,
+  useNavigate,
+  useParams,
+  useResolvedPath,
+} from 'react-router-dom';
+import {
+  OsdsTabs,
+  OsdsTabBar,
+  OsdsTabBarItem,
+} from '@ovhcloud/ods-components/react';
+
+import { DashboardLayout } from '@ovhcloud/manager-components';
+
+import Breadcrumb from '@/components/Breadcrumb/Breadcrumb';
+
+export type DashboardTabItemProps = {
+  name: string;
+  title: string;
+  to: string;
+};
+
+export type DashboardLayoutProps = {
+  tabs: DashboardTabItemProps[];
+};
+
+export default function DashboardPage() {
+  const [panel, setActivePanel] = useState('');
+  const { serviceName } = useParams();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { t } = useTranslation('dashboard');
+
+  const tabsList = [
+    {
+      name: 'general_informations',
+      title: 'Informations générales',
+      to: useResolvedPath('').pathname,
+    },
+    {
+      name: 'Tab 2',
+      title: 'Tab 2',
+      to: useResolvedPath('Tab2').pathname,
+    },
+  ];
+
+  useEffect(() => {
+    const activeTab = tabsList.find((tab) => tab.to === location.pathname);
+    if (activeTab) {
+      setActivePanel(activeTab.name);
+    } else {
+      setActivePanel(tabsList[0].name);
+      navigate(`${tabsList[0].to}`);
+    }
+  }, [location.pathname]);
+
+  const header = {
+    description: 'Description du hpc-vmware-managed-vcd',
+    title: serviceName,
+  };
+
+  return (
+    <div>
+      <DashboardLayout
+        header={header}
+        tabs={
+          <OsdsTabs panel={panel}>
+            <OsdsTabBar slot="top">
+              {tabsList.map((tab: DashboardTabItemProps) => (
+                <NavLink to={tab.to} className="no-underline">
+                  <OsdsTabBarItem
+                    key={`osds-tab-bar-item-${tab.name}`}
+                    panel={tab.name}
+                  >
+                    {tab.title}
+                  </OsdsTabBarItem>
+                </NavLink>
+              ))}
+            </OsdsTabBar>
+          </OsdsTabs>
+        }
+        breadcrumb={<Breadcrumb />}
+      />
+      <Outlet />
+    </div>
+  );
+}
