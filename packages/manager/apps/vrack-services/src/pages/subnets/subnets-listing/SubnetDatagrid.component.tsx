90be4c0d299359f7ac233fe61fcd50b6b91ff9a5
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2024-06-13 12:04:12 +0200

    fix(vrack-services): implement sorting on datagrid
    ref: MANAGER-14229
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager/apps/vrack-services/src/pages/subnets/subnets-listing/SubnetDatagrid.component.tsx
+++ packages/manager/apps/vrack-services/src/pages/subnets/subnets-listing/SubnetDatagrid.component.tsx
@@ -6,9 +6,29 @@ import {
   Datagrid,
   useDataGrid,
   DataGridTextCell,
+  ColumnSort,
 } from '@ovhcloud/manager-components';
 import { useVrackService, Subnet } from '@/data';
 import { SubnetsActionCell } from './SubnetsActionCell.component';
+
+const sortSubnets = (sorting: ColumnSort, subnetList: Subnet[] = []) => {
+  subnetList.sort((s1, s2) => {
+    switch (sorting.id) {
+      case 'displayName':
+        return s1.displayName?.localeCompare(s2.displayName);
+      case 'cidr':
+        return s1.cidr?.localeCompare(s2.cidr);
+      case 'serviceRange':
+        return s1.serviceRange?.cidr?.localeCompare(s2.serviceRange?.cidr);
+      case 'vlan':
+        return s1.vlan?.toString()?.localeCompare(s2.vlan?.toString());
+      default:
+        return 0;
+    }
+  });
+
+  return sorting.desc ? subnetList.reverse() : subnetList;
+};
 
 export const SubnetDatagrid: React.FC = () => {
   const { t } = useTranslation('vrack-services/subnets');
@@ -39,7 +59,7 @@ export const SubnetDatagrid: React.FC = () => {
       ),
     },
     {
-      id: 'serviceRange.cidr',
+      id: 'serviceRange',
       label: t('subnetDatagridServiceRangeLabel'),
       cell: ({ serviceRange }) => (
         <DataGridTextCell>{serviceRange.cidr}</DataGridTextCell>
@@ -53,6 +73,7 @@ export const SubnetDatagrid: React.FC = () => {
     {
       id: 'actions',
       label: t('subnetDatagridActionsLabel'),
+      isSortable: false,
       cell: ({ cidr }) => <SubnetsActionCell cidr={cidr} vs={vs} />,
     },
   ];
@@ -63,7 +84,7 @@ export const SubnetDatagrid: React.FC = () => {
       tableStyle={{ minWidth: '700px' }}
       className="pb-[200px] -mx-6"
       columns={columns}
-      items={subnetList}
+      items={sortSubnets(sorting, subnetList)}
       totalItems={subnetList.length}
       sorting={sorting}
       onSortChange={setSorting}
