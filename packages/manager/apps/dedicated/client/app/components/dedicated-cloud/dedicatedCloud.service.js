e39252ffbda3bb8cd0ecb1c264f43e5e1617f7cc
Germain Carré <git@germain.sh>
2024-06-13 16:18:15 +0200

    feat(dedicated-cloud.federation): removing federation (#11677)
    ref:  MANAGER-14408
    
    Signed-off-by: Germain Carré <git@germain.sh>

--- packages/manager/apps/dedicated/client/app/components/dedicated-cloud/dedicatedCloud.service.js
+++ packages/manager/apps/dedicated/client/app/components/dedicated-cloud/dedicatedCloud.service.js
@@ -589,20 +589,7 @@ class DedicatedCloudService {
       .then(({ data }) => data);
   }
 
-  /* ------- FEDERATION -------*/
-
-  getFederationStatus(serviceName) {
-    return this.$http
-      .get(`/dedicatedCloud/${serviceName}/federation`)
-      .then(({ data }) => data);
-  }
-
-  getActiveDirectories(serviceName, params = {}) {
-    return this.icebergQuery(
-      `/dedicatedCloud/${serviceName}/federation/activeDirectory`,
-      params,
-    );
-  }
+  /* ------- ICEBERG -------*/
 
   icebergQuery(url, params) {
     const {
@@ -638,12 +625,6 @@ class DedicatedCloudService {
       }));
   }
 
-  postActiveDirectories(serviceName, params) {
-    return this.$http
-      .post(`/dedicatedCloud/${serviceName}/federation/activeDirectory`, params)
-      .then(({ data }) => data);
-  }
-
   filterIceberg(request, filters) {
     let filterRequest = request;
     filters.forEach(({ field, comparator, reference }) => {
@@ -654,32 +635,6 @@ class DedicatedCloudService {
       );
     });
     return filterRequest;
-  }
-
-  grantActiveDirectory(serviceName, activeDirectoryId, params, type) {
-    return this.$http
-      .post(
-        `/dedicatedCloud/${serviceName}/federation/activeDirectory/${activeDirectoryId}/grantActiveDirectory${type}`,
-        params,
-      )
-      .then(({ data }) => data);
-  }
-
-  changePropertiesActiveDirectory(serviceName, activeDirectoryId, params) {
-    return this.$http
-      .post(
-        `/dedicatedCloud/${serviceName}/federation/activeDirectory/${activeDirectoryId}/changeProperties`,
-        params,
-      )
-      .then(({ data }) => data);
-  }
-
-  deleteActiveDirectory(serviceName, activeDirectoryId) {
-    return this.$http
-      .delete(
-        `/dedicatedCloud/${serviceName}/federation/activeDirectory/${activeDirectoryId}`,
-      )
-      .then(({ data }) => data);
   }
 
   /* ------- USER -------*/
