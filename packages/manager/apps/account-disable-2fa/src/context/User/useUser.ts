624694f2b6a90271453da67751434fd47888b3d9
JacquesLarique <134954692+JacquesLarique@users.noreply.github.com>
2024-06-14 15:23:56 +0200

    feat(2fa): add error message on challenge failure (#11993)
    ref: MANAGER-14261
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/apps/account-disable-2fa/src/context/User/useUser.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import userContext, { UserContext } from '@/context/User/context';
-
-export default (): UserContext => {
-  return useContext(userContext);
-};
--- packages/manager/apps/account-disable-2fa/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-import config from '@ovh-ux/manager-tailwind-config';
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  ...config,
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-};
