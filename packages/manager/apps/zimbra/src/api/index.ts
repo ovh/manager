158f8e89f490842978c8fb542ee8c54340205e74
stif59100 <steeve.vanderstocken@ovhcloud.com>
2024-06-14 11:44:55 +0200

    feat(zimbra): add configuration domaine page
    ref:MANAGER-13883
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

--- packages/manager/apps/zimbra/src/api/index.ts
+++ packages/manager/apps/zimbra/src/api/index.ts
@@ -1,4 +1,5 @@
 export * from './DELETE/apiv2/services';
 export * from './GET/apiv2/services';
+export * from './GET/apiv6/services';
 export * from './POST/apiv2/services';
 export * from './PUT/apiv2/services';
