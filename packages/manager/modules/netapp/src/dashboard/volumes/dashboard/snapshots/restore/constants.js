1b917189fac7c71e4ec4e451bd9994e0724cf88e
qpavy <contact@quentinpavy.com>
2024-06-14 10:29:39 +0200

    feat(dedicated.netapp): manage hold and revert automatic snapshots (#12014)
    ref: MANAGER-11768
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

--- packages/manager/modules/netapp/src/dashboard/volumes/dashboard/snapshots/restore/constants.js
+++ packages/manager/modules/netapp/src/dashboard/volumes/dashboard/snapshots/restore/constants.js
@@ -6,18 +6,10 @@ export const SNAPSHOT_TYPE = {
 
 export const SNAPSHOT_STATUS = {
   AVAILABLE: 'available',
-  CREATING: 'creating',
-  DELETING: 'deleting',
-  ERROR: 'error',
-  ERROR_DELETING: 'error_deleting',
-  MANAGE_ERROR: 'manage_error',
-  MANAGE_STARTING: 'manage_starting',
-  RESTORING: 'restoring',
-  UNMANAGE_ERROR: 'unmanage_error',
-  UNMANAGE_STARTING: 'unmanage_starting'
+  MANAGE_ERROR: 'manage_error'
 }
 
-export const FETCH_INTERVAL = 5000;
+export const FETCH_INTERVAL = 1000;
 
 export default {
   SNAPSHOT_TYPE,
