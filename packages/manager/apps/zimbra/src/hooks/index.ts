f37984d99cef710e7cbfd8196b00f069aaf981bd
stif59100 <steeve.vanderstocken@ovhcloud.com>
2024-06-17 15:22:59 +0200

    feat(zimbra): correcting add domain page
    ref: MANAGER-14498
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

--- packages/manager/apps/zimbra/src/hooks/index.ts
+++ packages/manager/apps/zimbra/src/hooks/index.ts
@@ -2,4 +2,6 @@ export * from './usePlatform';
 export * from './useOrganization';
 export * from './useOrganizationsList';
 export * from './useOverridePage';
+export * from './useDomains';
+export * from './useGenerateUrl';
 export * from './types';
