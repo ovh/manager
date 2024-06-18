39013c6337714f47f98b034200b605c91a0229d1
Maxime Bajeux <max.bajeux@gmail.com>
2024-06-18 17:18:21 +0200

    feat(container): pnr v2 (#11723)
    ref: MANAGER-12939
    
    Signed-off-by: Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>
    Co-authored-by: benjaminlepine <benjamin.lepine@hotmail.fr>

--- packages/manager/apps/container/src/container/nav-reshuffle/sidebar/Assistance.tsx
+++ packages/manager/apps/container/src/container/nav-reshuffle/sidebar/Assistance.tsx
@@ -6,23 +6,19 @@ import ApplicationContext from '@/context';
 import useProductNavReshuffle from '@/core/product-nav-reshuffle';
 
 import SidebarLink from './SidebarLink';
-import style from './style.module.scss';
 import useContainer from '@/core/container';
+import { OsdsIcon } from '@ovhcloud/ods-components/react';
+import {
+  ODS_ICON_NAME,
+  ODS_ICON_SIZE,
+} from '@ovhcloud/ods-components';
+import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
 
-interface Props {
-  containerURL: { appId: string; appHash: string };
-  isOpen: boolean;
-  onToggle: (isOpen: boolean) => void;
-}
-
-const AssistanceSidebar: React.FC<ComponentProps<Props>> = ({
-  containerURL,
-  isOpen,
-  onToggle,
-}: Props): JSX.Element => {
+const AssistanceSidebar: React.FC = (): JSX.Element => {
   const { t } = useTranslation('sidebar');
   const { shell } = useContext(ApplicationContext);
   const { setChatbotReduced } = useContainer();
+  const features = ['livechat', 'carbon-calculator'];
 
   const environment = shell
     .getPluginManager()
@@ -31,42 +27,25 @@ const AssistanceSidebar: React.FC<ComponentProps<Props>> = ({
   const urls = useURL(environment);
   const trackingPlugin = shell.getPlugin('tracking');
   const reketInstance = useReket();
-  const [selectedItem, setSelectedItem] = useState<string>(null);
 
-  const hasAdvancedSupport = ['EU', 'CA'].includes(environment.getRegion());
   const [hasLiveChat, setHashLiveChat] = useState(false);
+  const [hasCarbonCalculator, setHasCarbonCalculator] = useState(false);
 
   const { closeNavigationSidebar, openOnboarding } = useProductNavReshuffle();
 
   useEffect(() => {
-    const initLiveChat = async () => {
+    const initFeatures = async () => {
       const results: Record<string, boolean> = await reketInstance.get(
-        `/feature/livechat/availability`,
+        `/feature/${features.join(',')}/availability`,
         {
           requestType: 'aapi',
         },
       );
-      setHashLiveChat(results.livechat);
-    };
-    initLiveChat();
-  }, []);
-
-  useEffect(() => {
-    const { appId, appHash } = containerURL;
-
-    setSelectedItem(null);
-    if (
-      appId === 'dedicated' &&
-      appHash.startsWith('/useraccount/support/level')
-    ) {
-      setSelectedItem('support_level');
-    } else if (
-      appId === 'dedicated' &&
-      (appHash.startsWith('/ticket') || appHash.startsWith('/support/tickets'))
-    ) {
-      setSelectedItem('tickets');
+      setHashLiveChat(results['livechat']);
+      setHasCarbonCalculator(results['carbon-calculator']);
     }
-  }, [containerURL]);
+    initFeatures();
+  }, []);
 
   const startOnboarding = () => {
     openOnboarding();
@@ -81,107 +60,114 @@ const AssistanceSidebar: React.FC<ComponentProps<Props>> = ({
   };
 
   return (
-    <ul className="mt-auto">
-      <li className="assistance_header" onClick={() => onToggle(!isOpen)}>
+    <ul className="mt-auto pb-3" id="useful-links">
+      <li className="assistance_header px-3 mb-3">
         <h2 className="flex justify-between">
           <span>{t('sidebar_assistance_title')}</span>
-          <span
-            className={`oui-icon oui-icon-chevron-${isOpen ? 'up' : 'down'}`}
-          ></span>
         </h2>
       </li>
-      {isOpen && (
-        <>
-          <li>
-            <SidebarLink
-              node={{
-                translation: 'sidebar_assistance_help_center',
-                url: urls.get('help'),
-                count: false,
-                isExternal: true,
-              }}
-              onClick={() => trackNode('assistance_help_center')}
-            />
-          </li>
-          <li
-            className={`${
-              selectedItem === 'tickets' ? style.sidebar_selected : ''
-            }`}
-          >
-            <SidebarLink
-              node={{
-                translation: 'sidebar_assistance_tickets',
-                routing: {
-                  application: 'dedicated',
-                  hash: '#/ticket',
-                },
-                count: false,
-              }}
-              onClick={() => {
-                trackNode('assistance_tickets');
-                closeNavigationSidebar();
-              }}
-            />
-          </li>
-          <li>
-            <SidebarLink
-              node={{
-                translation: 'sidebar_assistance_status',
-                url: urls.get('status'),
-                count: false,
-                isExternal: true,
-              }}
-              onClick={() => trackNode('assistance_status')}
-            />
-          </li>
-          {hasAdvancedSupport && (
-            <li
-              className={`${
-                selectedItem === 'support_level' ? style.sidebar_selected : ''
-              }`}
-            >
-              <SidebarLink
-                node={{
-                  translation: 'sidebar_assistance_support_level',
-                  routing: {
-                    application: 'dedicated',
-                    hash: '#/useraccount/support/level',
-                  },
-                  count: false,
-                }}
-                onClick={() => {
-                  trackNode('assistance_support_level');
-                  closeNavigationSidebar();
-                }}
-              />
-            </li>
-          )}
-          {hasLiveChat && (
-            <li>
-              <SidebarLink
-                node={{
-                  translation: 'sidebar_assistance_live_chat',
-                  count: false,
-                }}
-                onClick={() => {
-                  shell.getPlugin('ux').openLiveChat();
-                  setChatbotReduced(false);
-                  trackNode('assistance_live_chat');
-                  closeNavigationSidebar();
-                }}
-              />
-            </li>
-          )}
-          <li>
-            <SidebarLink
-              node={{
-                translation: 'sidebar_assistance_onboarding',
-                count: false,
-              }}
-              onClick={() => startOnboarding()}
-            />
-          </li>
-        </>
+      {['EU'].includes(environment.getRegion()) && (
+        <li className="flex px-3 align-items-center">
+          <OsdsIcon
+            name={ODS_ICON_NAME.HOME}
+            className="mr-2"
+            size={ODS_ICON_SIZE.sm}
+            color={ODS_THEME_COLOR_INTENT.primary}
+            contrasted
+          />
+          <SidebarLink
+            node={{
+              translation: 'sidebar_marketplace',
+              url: urls.get('marketplace'),
+              count: false,
+              isExternal: true,
+            }}
+          />
+        </li>
+      )}
+      <li className="flex px-3 align-items-center">
+        <OsdsIcon
+            name={ODS_ICON_NAME.HELP_CIRCLE}
+            className="mr-2"
+            size={ODS_ICON_SIZE.sm}
+            color={ODS_THEME_COLOR_INTENT.primary}
+            contrasted
+          />
+        <SidebarLink
+          node={{
+            translation: 'sidebar_assistance_help_center',
+            url: urls.get('help'),
+            count: false,
+            isExternal: true,
+          }}
+          onClick={() => trackNode('assistance_help_center')}
+        />
+      </li>
+      <li className="flex px-3 align-items-center">
+        <OsdsIcon
+            name={ODS_ICON_NAME.WARNING}
+            className="mr-2"
+            size={ODS_ICON_SIZE.sm}
+            color={ODS_THEME_COLOR_INTENT.primary}
+            contrasted
+          />
+        <SidebarLink
+          node={{
+            translation: 'sidebar_assistance_status',
+            url: urls.get('status'),
+            count: false,
+            isExternal: true,
+          }}
+          onClick={() => trackNode('assistance_status')}
+        />
+      </li>
+      {hasLiveChat && (
+        <li className="flex px-3 align-items-center">
+          <OsdsIcon
+            name={ODS_ICON_NAME.CHAT}
+            className="mr-2"
+            size={ODS_ICON_SIZE.sm}
+            color={ODS_THEME_COLOR_INTENT.primary}
+            contrasted
+          />
+          <SidebarLink
+            node={{
+              translation: 'sidebar_assistance_live_chat',
+              count: false,
+            }}
+            onClick={() => {
+              shell.getPlugin('ux').openLiveChat();
+              setChatbotReduced(false);
+              trackNode('assistance_live_chat');
+              closeNavigationSidebar();
+            }}
+          />
+        </li>
+      )}
+      {hasCarbonCalculator && (
+        <li className="flex px-3 align-items-center">
+          <OsdsIcon
+            name={ODS_ICON_NAME.LEAF_CONCEPT}
+            className="mr-2"
+            size={ODS_ICON_SIZE.sm}
+            color={ODS_THEME_COLOR_INTENT.primary}
+            contrasted
+          />
+          <SidebarLink
+            node={{
+              translation: 'sidebar_assistance_carbon_calculator',
+              count: false,
+              routing: {
+                application: 'carbon-calculator',
+                hash: '#/',
+              },
+            }}
+            onClick={() => {
+              trackNode('assistance_carbon_calculator');
+              closeNavigationSidebar();
+            }}
+          />
+        </li>
       )}
     </ul>
   );
