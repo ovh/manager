db27849d1adcc6ca2b0821f931848601cf8213be
stif59100 <steeve.vanderstocken@ovhcloud.com>
2024-06-21 17:21:32 +0200

    feat(zimbra): add iam configuration
    ref:MANAGER-14502
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

--- packages/manager/apps/zimbra/src/pages/dashboard/Domains/ActionButtonDomain.tsx
+++ packages/manager/apps/zimbra/src/pages/dashboard/Domains/ActionButtonDomain.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { ActionMenu } from '@ovhcloud/manager-components';
 import { DomainsItem } from './Domains';
-import { useGenerateUrl } from '@/hooks';
+import { useGenerateUrl, usePlatform } from '@/hooks';
 
 interface ActionButtonDomainProps {
   domainItem: DomainsItem;
@@ -11,16 +11,17 @@ const ActionButtonDomain: React.FC<ActionButtonDomainProps> = ({
   domainItem,
 }) => {
   const { t } = useTranslation('domains');
-
   const hrefDeleteDomain = useGenerateUrl('./delete', 'href', {
     deleteDomainId: domainItem.id,
   });
-
+  const { platformUrn } = usePlatform();
   const actionItems = [
     {
       id: 2,
       href: hrefDeleteDomain,
       label: t('zimbra_domains_tooltip_delete'),
+      urn: platformUrn,
+      iamActions: ['zimbra:apiovh:platform/domain/delete'],
     },
   ];
   return <ActionMenu items={actionItems} isCompact />;
