e985da22069dd7e5414673f62acca7502c62eda2
Antony MARION <antony.marion.ext@ovhcloud.com>
2024-06-21 12:36:51 +0200

    feat(web): git configuration page
      ref: MANAGER-14032
    
    Signed-off-by: Antony MARION <antony.marion.ext@ovhcloud.com>

---
+++ packages/manager/apps/web/client/app/hosting/multisite/git-association/hosting-multisite-git-association.constants.js
@@ -0,0 +1,19 @@
+export const REPOSITORY_PLACEHOLDER =
+  'https://github.com/username/repositoryname';
+export const EXAMPLE_HTTPS_REPOSITORY_URL =
+  'https://github.com/username/respositoryname';
+export const EXAMPLE_SSH_REPOSITORY_URL = 'git@github.com/username/';
+export const EXAMPLE_BRANCHES_NAMES = '"main", "master", ...';
+export const GITHUB_VCS = 'github';
+export const PATH_PREFIX = '[www]';
+export const REGEX_GIT_REPO = /^[https:\\/\\/|git:\\/\\/].*/;
+
+export default {
+  REPOSITORY_PLACEHOLDER,
+  EXAMPLE_HTTPS_REPOSITORY_URL,
+  EXAMPLE_BRANCHES_NAMES,
+  EXAMPLE_SSH_REPOSITORY_URL,
+  GITHUB_VCS,
+  PATH_PREFIX,
+  REGEX_GIT_REPO,
+};
