3451b19793690f86cc21244ec9372279e9c8bbef
Florian Renaut <florian.renaut@corp.ovh.com>
2024-06-25 16:27:42 +0200

    feat(manager-components): add pci free-localzones banner
    ref: DTCORE-2346
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager-components/src/components/action-banner/index.ts
+++ packages/manager-components/src/components/action-banner/index.ts
@@ -3,5 +3,5 @@ export {
   PciDiscoveryBanner,
   isDiscoveryProject,
 } from './pci/pci-discovery-banner.component';
-
 export { PciAnnouncementBanner } from './pci/pci-announcement-banner.component';
+export { PciFreeLocalzonesBanner } from './pci/pci-free-localzones-banner.component';
