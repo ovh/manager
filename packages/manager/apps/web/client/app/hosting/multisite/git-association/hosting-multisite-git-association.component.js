cfa27c6e0cb430061e2cd5b8324ec7185998103c
Antony MARION <antony.marion.ext@ovhcloud.com>
2024-06-26 11:25:13 +0200

    feat(web): associate a repository
      ref: MANAGER-14064
    
    Signed-off-by: Antony MARION <antony.marion.ext@ovhcloud.com>

--- packages/manager/apps/web/client/app/hosting/multisite/git-association/hosting-multisite-git-association.component.js
+++ packages/manager/apps/web/client/app/hosting/multisite/git-association/hosting-multisite-git-association.component.js
@@ -4,8 +4,12 @@ import template from './hosting-multisite-git-association.html';
 export default {
   bindings: {
     sshKey: '<',
+    path: '<',
+    status: '<',
+    alertId: '<',
     serviceName: '<',
     goBack: '<',
+    webHookUrl: '<',
   },
   controller,
   template,
