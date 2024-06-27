a1cb71e6687df1a0d517347d63f2347452ccd00c
LIDRISSI Hamid <abdelghani-lidrissi.hamid.ext@ovhcloud.com>
2024-06-27 14:20:52 +0200

    feat(pci-block-storage): use the menu action from the manager component
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/apps/pci-block-storage/src/components/list/Actions.component.tsx
+++ packages/manager/apps/pci-block-storage/src/components/list/Actions.component.tsx
@@ -1,23 +1,6 @@
-import {
-  OsdsButton,
-  OsdsIcon,
-  OsdsMenu,
-  OsdsMenuItem,
-  OsdsText,
-} from '@ovhcloud/ods-components/react';
-import {
-  ODS_BUTTON_SIZE,
-  ODS_BUTTON_VARIANT,
-  ODS_ICON_NAME,
-  ODS_ICON_SIZE,
-  ODS_TEXT_LEVEL,
-} from '@ovhcloud/ods-components';
-import {
-  ODS_THEME_COLOR_INTENT,
-  ODS_THEME_TYPOGRAPHY_SIZE,
-} from '@ovhcloud/ods-common-theming';
-import { useTranslation } from 'react-i18next';
 import { useHref } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { ActionMenu } from '@ovhcloud/manager-components';
 import { TVolume } from '@/api/data/volume';
 
 type ActionsProps = {
@@ -35,99 +18,36 @@ export default function ActionsComponent({
   const hrefRemove = useHref(`./delete/${volume.id}`);
   const hrefCreateBackup = `${projectUrl}/storages/volume-backup/create?volume=${volume.id}`;
 
-  return (
-    <OsdsMenu>
-      <OsdsButton
-        slot={'menu-title'}
-        circle
-        color={ODS_THEME_COLOR_INTENT.primary}
-        variant={ODS_BUTTON_VARIANT.stroked}
-      >
-        <OsdsIcon
-          name={ODS_ICON_NAME.ELLIPSIS}
-          color={ODS_THEME_COLOR_INTENT.primary}
-          size={ODS_ICON_SIZE.xxs}
-        ></OsdsIcon>
-      </OsdsButton>
-      <OsdsMenuItem>
-        <OsdsButton
-          size={ODS_BUTTON_SIZE.sm}
-          variant={ODS_BUTTON_VARIANT.ghost}
-          color={ODS_THEME_COLOR_INTENT.primary}
-          data-testid={'actionComponent-edit-button'}
-          href={hrefEdit}
-        >
-          <OsdsText
-            size={ODS_THEME_TYPOGRAPHY_SIZE._500}
-            level={ODS_TEXT_LEVEL.button}
-            color={ODS_THEME_COLOR_INTENT.primary}
-            slot={'start'}
-          >
-            {t('pci_projects_project_storages_blocks_edit_label')}
-          </OsdsText>
-        </OsdsButton>
-      </OsdsMenuItem>
+  const items = [
+    {
+      id: 0,
+      href: hrefEdit,
+      label: t('pci_projects_project_storages_blocks_edit_label'),
+      dataTestid: 'actionComponent-edit-button',
+    },
+    {
+      id: 1,
+      href: volume.attachedTo?.length ? hrefDetach : hrefAttach,
+      label: t(
+        `pci_projects_project_storages_blocks_instance_${
+          volume.attachedTo?.length ? 'detach' : 'attach'
+        }_label`,
+      ),
+      dataTestid: 'actionComponent-attach-detach-button',
+    },
+    {
+      id: 2,
+      href: hrefCreateBackup,
+      label: t('pci_projects_project_storages_blocks_create_backup_label'),
+      dataTestid: 'actionComponent-create-backup-button',
+    },
+    {
+      id: 3,
+      href: hrefRemove,
+      label: t('pci_projects_project_storages_blocks_delete_label'),
+      dataTestid: 'actionComponent-remove-button',
+    },
+  ];
 
-      <OsdsMenuItem>
-        <OsdsButton
-          size={ODS_BUTTON_SIZE.sm}
-          variant={ODS_BUTTON_VARIANT.ghost}
-          color={ODS_THEME_COLOR_INTENT.primary}
-          href={volume.attachedTo?.length ? hrefDetach : hrefAttach}
-          data-testid="actionComponent-attach-detach-button"
-        >
-          <OsdsText
-            size={ODS_THEME_TYPOGRAPHY_SIZE._500}
-            level={ODS_TEXT_LEVEL.button}
-            color={ODS_THEME_COLOR_INTENT.primary}
-            slot={'start'}
-          >
-            {t(
-              `pci_projects_project_storages_blocks_instance_${
-                volume.attachedTo?.length ? 'detach' : 'attach'
-              }_label`,
-            )}
-          </OsdsText>
-        </OsdsButton>
-      </OsdsMenuItem>
-
-      <OsdsMenuItem>
-        <OsdsButton
-          size={ODS_BUTTON_SIZE.sm}
-          variant={ODS_BUTTON_VARIANT.ghost}
-          color={ODS_THEME_COLOR_INTENT.primary}
-          data-testid={'actionComponent-create-backup-button'}
-          href={hrefCreateBackup}
-        >
-          <OsdsText
-            size={ODS_THEME_TYPOGRAPHY_SIZE._500}
-            level={ODS_TEXT_LEVEL.button}
-            color={ODS_THEME_COLOR_INTENT.primary}
-            slot={'start'}
-          >
-            {t('pci_projects_project_storages_blocks_create_backup_label')}
-          </OsdsText>
-        </OsdsButton>
-      </OsdsMenuItem>
-
-      <OsdsMenuItem>
-        <OsdsButton
-          size={ODS_BUTTON_SIZE.sm}
-          variant={ODS_BUTTON_VARIANT.ghost}
-          color={ODS_THEME_COLOR_INTENT.primary}
-          data-testid={'actionComponent-remove-button'}
-          href={hrefRemove}
-        >
-          <OsdsText
-            size={ODS_THEME_TYPOGRAPHY_SIZE._500}
-            level={ODS_TEXT_LEVEL.button}
-            color={ODS_THEME_COLOR_INTENT.primary}
-            slot={'start'}
-          >
-            {t('pci_projects_project_storages_blocks_delete_label')}
-          </OsdsText>
-        </OsdsButton>
-      </OsdsMenuItem>
-    </OsdsMenu>
-  );
+  return <ActionMenu items={items} isCompact />;
 }
