79711e237a58ccefca70ab51ddd8c67e0bcb8125
Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>
2024-06-27 11:36:40 +0200

    feat(container): poc eloquant
    add the given poc example into a react context
    
    ref: MANAGER-14337
    
    Signed-off-by: Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>

--- packages/manager/apps/container/vite-env.d.ts
+++ packages/manager/apps/container/vite-env.d.ts
@@ -2,3 +2,11 @@
 declare const __VERSION__: string;
 // eslint-disable-next-line @typescript-eslint/naming-convention, no-underscore-dangle
 declare const __REGION__: string;
+
+declare global {
+  interface Window {
+    elqwebtrigger: any;
+  }
+}
+
+export {}
