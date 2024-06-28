e4e1507b04585b68c181e9964b092eabf4c913c9
Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>
2024-06-28 16:39:33 +0200

    feat(*): ga helpcenter
    rebase post prod pnr v2
    
    ref: MANAGER-13069
    
    Signed-off-by: Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>

--- packages/manager/apps/container/src/container/nav-reshuffle/sidebar/Assistance.tsx
+++ packages/manager/apps/container/src/container/nav-reshuffle/sidebar/Assistance.tsx
@@ -28,6 +28,7 @@ const AssistanceSidebar: React.FC = (): JSX.Element => {
   const trackingPlugin = shell.getPlugin('tracking');
   const reketInstance = useReket();
 
+  const isEUOrCA = ['EU', 'CA'].includes(environment.getRegion());
   const [hasLiveChat, setHashLiveChat] = useState(false);
   const [hasCarbonCalculator, setHasCarbonCalculator] = useState(false);
 
@@ -96,9 +97,13 @@ const AssistanceSidebar: React.FC = (): JSX.Element => {
         <SidebarLink
           node={{
             translation: 'sidebar_assistance_help_center',
-            url: urls.get('help'),
+            url: isEUOrCA ? urls.get('support') : null,
+            routing: !isEUOrCA ? {
+              application: 'dedicated',
+              hash: '#/ticket',
+            } : null,
             count: false,
-            isExternal: true,
+            isExternal: isEUOrCA,
           }}
           onClick={() => trackNode('assistance_help_center')}
         />
