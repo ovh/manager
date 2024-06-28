71b7a4d4e56035ca5408f9d4840779978a63ab3b
Arthur Bullet <arthur.bullet@ovhcloud.com>
2024-06-28 15:06:09 +0200

    feat(pci.ai-dashboard): add model
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>

---
+++ packages/manager/apps/pci-ai-dashboard/src/model/project.ts
@@ -0,0 +1,15 @@
+export enum PciProjectPlanCode {
+    DISCOVERY = 'project.discovery',
+    STANDARD = 'project.2018',
+  }
+  export interface PciProject {
+    access: string;
+    creationDate: string;
+    description: string;
+    projectName: string;
+    project_id: string;
+    status: string;
+    unleash: boolean;
+    planCode: PciProjectPlanCode;
+  }
+  
