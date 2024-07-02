3eb84268f2f19eda4a338d3700aee574b6e8c10f
Arthur Bullet <arthur.bullet@ovhcloud.com>
2024-07-02 11:32:29 +0200

    feat(pci.ai-dashboard): clean branch
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>

--- packages/manager/apps/pci-ai-dashboard/src/data/api/projects.ts
+++ packages/manager/apps/pci-ai-dashboard/src/data/api/projects.ts
@@ -1,5 +1,5 @@
 import { apiClient } from '@ovh-ux/manager-core-api';
-import { PciProject } from '@/types/project';
+import { PciProject } from '@/interfaces/project';
 
 export const getProject = async (projectId: string): Promise<PciProject> => {
   const response = await apiClient.v6.get(`/cloud/project/${projectId}`);
