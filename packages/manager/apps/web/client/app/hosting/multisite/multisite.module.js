73185037bff2109e24cee0e5b5755f57419e69f2
Antony MARION <antony.marion.ext@ovhcloud.com>
2024-07-02 15:20:06 +0200

    feat(web): remove git association
      ref: MANAGER-14097
    
    Signed-off-by: Antony MARION <antony.marion.ext@ovhcloud.com>

--- packages/manager/apps/web/client/app/hosting/multisite/multisite.module.js
+++ packages/manager/apps/web/client/app/hosting/multisite/multisite.module.js
@@ -2,6 +2,7 @@ import cdnFlush from './cdn-flush';
 
 import statistics from './statistics/hosting-multisite-statistics.component';
 import hostingMultisiteGitAssociation from './git-integration/association';
+import hostingMultisiteGitRemoval from './git-integration/removal';
 import hostingMultisiteGitDeployment from './git-integration/deployment';
 import hostingMultisiteGitViewLastDeployment from './git-integration/view-last-deployment';
 import routing from './multisite.routing';
@@ -13,6 +14,7 @@ angular
     cdnFlush,
     hostingMultisiteGitAssociation,
     hostingMultisiteGitDeployment,
+    hostingMultisiteGitRemoval,
     hostingMultisiteGitViewLastDeployment,
   ])
   .component('hostingMultisiteStatistics', statistics)
