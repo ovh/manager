d9932f1da7582f255d2ac321cb6dbf17265f48e6
LIDRISSI Hamid <abdelghani-lidrissi.hamid.ext@ovhcloud.com>
2024-07-03 15:58:12 +0200

    feat(manager-pic-common): add the common pci banner with tests
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

---
+++ packages/manager/modules/manager-pci-common/src/components/banner/index.ts
@@ -0,0 +1,13 @@
+export {
+  PciDiscoveryBanner,
+  isDiscoveryProject,
+} from './discovery-banner/pci-discovery-banner';
+export {
+  PciAnnouncementBanner,
+  useAnnouncementBanner,
+} from './announcement-banner/pci-announcement-banner';
+export { PciFreeLocalZonesBanner } from './free-local-zones-banner/pci-free-local-zones-banner';
+export {
+  PciTrustedZoneBanner,
+  useTrustedZoneBanner,
+} from './trusted-zone-banner/pci-trusted-zone-banner';
