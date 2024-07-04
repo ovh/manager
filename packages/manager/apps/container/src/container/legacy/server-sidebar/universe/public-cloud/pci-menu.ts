41f6b2c9f35b0d0ecb0315b9e0d9c31002f2ceed
Arthur Bullet <arthur.bullet@ovhcloud.com>
2024-07-04 09:39:24 +0200

    feat(pci.ai-dashboard): clean and initialize ai dashboard epic branch
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>

--- packages/manager/apps/container/src/container/legacy/server-sidebar/universe/public-cloud/pci-menu.ts
+++ packages/manager/apps/container/src/container/legacy/server-sidebar/universe/public-cloud/pci-menu.ts
@@ -47,6 +47,7 @@ export const features = [
   'public-cloud:vouchers',
   'public-cloud:contacts',
   'public-cloud:project-settings',
+  'pci-ai-dashboard',
   'ai-dashboard',
   'data-platform',
   'ai-endpoints'
@@ -326,10 +327,10 @@ export function getPciProjectMenu(
       regions: ['EU', 'CA'],
       subItems: [
         {
-          id: 'dashboard',
+          id: 'pci-ai-dashboard',
           title: 'AI Dashboard',
-          feature: 'ai-dashboard',
-          href: getURL('public-cloud', `#/pci/projects/${projectId}/ai-dashboard`),
+          feature: 'pci-ai-dashboard',
+          href: getURL('public-cloud', `#/pci/projects/${projectId}/ai/dashboard`),
         },
         {
           id: 'notebooks',
--- packages/manager/apps/pci-ai-dashboard/src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1>AI dashboard first page</h1>
-      <p>This is ai dashbaord first µApp page</p>
-    </>
-  );
-}
