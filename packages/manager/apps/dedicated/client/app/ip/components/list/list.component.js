7807f50f0466deea0257056406bc099a52ae51f1
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2024-07-04 19:59:17 +0530

    feat(dedicated.ip): naviigate user to api console
    ref: MANAGER-14604
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/ip/components/list/list.component.js
+++ packages/manager/apps/dedicated/client/app/ip/components/list/list.component.js
@@ -20,6 +20,7 @@ export default {
     trackPage: '<',
     trackingData: '<',
     goToStatistics: '<',
+    isDeleteByoipServiceAvailable: '<',
   },
   transclude: {
     actions: '?ipListActions',
