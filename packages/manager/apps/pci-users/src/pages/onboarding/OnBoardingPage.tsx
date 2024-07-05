d1f00ebf900f54c48c86414c84a8cb32ebba7e9c
LIDRISSI Hamid <abdelghani-lidrissi.hamid.ext@ovhcloud.com>
2024-07-05 16:44:31 +0200

    feat(pci-*): use pci-common banner instead of manager-components
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/apps/pci-users/src/pages/onboarding/OnBoardingPage.tsx
+++ packages/manager/apps/pci-users/src/pages/onboarding/OnBoardingPage.tsx
@@ -13,6 +13,10 @@ import {
 import {
   isDiscoveryProject,
   PciDiscoveryBanner,
+} from '@ovh-ux/manager-pci-common';
+import {
+  isDiscoveryProject,
+  PciDiscoveryBanner,
   TProject,
 } from '@ovh-ux/manager-pci-common';
 import { useAllUsers } from '@/api/hooks/useUser';
