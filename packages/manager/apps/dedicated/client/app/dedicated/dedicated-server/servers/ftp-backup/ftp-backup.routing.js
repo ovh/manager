5e354ead4389e9d23a7666b21dcf4d9708b7ad2d
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2024-07-08 10:20:43 +0200

    feat(dedicated.server): redirect backup storage for ks
    ref: MANAGER-14698
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/dedicated/dedicated-server/servers/ftp-backup/ftp-backup.routing.js
+++ packages/manager/apps/dedicated/client/app/dedicated/dedicated-server/servers/ftp-backup/ftp-backup.routing.js
@@ -7,6 +7,21 @@ export default /* @ngInject */ ($stateProvider, coreConfigProvider) => {
           component: 'dedicatedServerFtpBackupStorage',
         },
       },
+      redirectTo: (transition) => {
+        const injector = transition.injector();
+        const backupIsOrderablePromise = injector.getAsync('backupIsOrderable');
+        const serverNamePromise = injector.getAsync('serverName');
+
+        return Promise.all([backupIsOrderablePromise, serverNamePromise]).then(
+          ([backupIsOrderable, serverName]) =>
+            backupIsOrderable
+              ? null
+              : {
+                  state: 'app.dedicated-server.server.dashboard',
+                  params: { productId: serverName },
+                },
+        );
+      },
       resolve: {
         breadcrumb: /* @ngInject */ ($translate) =>
           $translate.instant('dedicated_server_ftp_backup'),
