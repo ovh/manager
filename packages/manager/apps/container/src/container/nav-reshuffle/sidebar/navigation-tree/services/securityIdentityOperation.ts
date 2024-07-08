6909a5c560480e99ada941423c9e8fa30abd74bd
Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>
2024-07-08 09:06:40 +0200

    feat(container): pnr added kms entry on security, identity & operations
    ref: MANAGER-14629
    
    Signed-off-by: Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>

--- packages/manager/apps/container/src/container/nav-reshuffle/sidebar/navigation-tree/services/securityIdentityOperation.ts
+++ packages/manager/apps/container/src/container/nav-reshuffle/sidebar/navigation-tree/services/securityIdentityOperation.ts
@@ -1,3 +1,5 @@
+import { Translation } from "react-i18next";
+
 export default {
   id: 'security-identity-operation',
   translation: 'sidebar_security_identity_operations',
@@ -6,24 +8,45 @@ export default {
     application: 'iam',
   },
   count: false,
-  features: ['iam'],
+  features: ['iam', 'key-management-service'],
   children: [
     {
-      id: 'security-identity-operation-iam',
-      translation: 'sidebar_security_identity_operations_iam',
-      routing: {
-        application: 'iam',
-        hash: '#/',
-      },
+      id: 'security-identity',
+      translation: 'sidebar_security_identity',
+      children: [
+        {
+          id: 'security-identity-operation-iam',
+          translation: 'sidebar_security_identity_operations_iam',
+          routing: {
+            application: 'iam',
+            hash: '#/',
+          },
+        },
+        {
+          id: 'security_identity_operations_kms',
+          translation: 'sidebar_security_identity_operations_kms',
+          features: ['key-management-service'],
+          routing: {
+            application: 'key-management-service',
+            hash: '#/',
+          },
+        },
+      ]
     },
     {
-      id: 'security-identity-operation-logs',
-      translation: 'sidebar_security_identity_operations_logs',
-      serviceType: 'DBAAS_LOGS',
-      routing: {
-        application: 'dedicated',
-        hash: '#/dbaas/logs',
-      },
-    },
+      id: 'security-operations',
+      translation: 'sidebar_security_operations',
+      children: [
+        {
+          id: 'security-identity-operation-logs',
+          translation: 'sidebar_security_identity_operations_logs',
+          serviceType: 'DBAAS_LOGS',
+          routing: {
+            application: 'dedicated',
+            hash: '#/dbaas/logs',
+          },
+        }
+      ]
+    }
   ],
 };
