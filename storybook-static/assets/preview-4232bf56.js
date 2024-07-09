dda8a97a65d772225c14aea6e8498ea629c4b24a
ovh-ux-cds <ovh-ux-cds@users.noreply.github.com>
2024-07-09 09:21:17 +0000

    docs: update documentation [skip ci]

---
+++ storybook-static/assets/preview-4232bf56.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-3ff55844.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-908adfcd.js").then(e=>e.D),["./DocsRenderer-NNNQARDV-908adfcd.js","./iframe-3ff55844.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./_getPrototype-8b76ad08.js","./index-da03a860.js","./index-d37d4223.js","./setPrototypeOf-24bea416.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-4232bf56.js.map
