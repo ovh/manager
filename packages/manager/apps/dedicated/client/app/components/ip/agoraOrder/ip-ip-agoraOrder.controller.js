a95fdb2a82f5a75627277816aed5d88553b1f298
soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>
2024-07-10 08:54:40 +0200

    fix(dedicated): remove dedicated::ip::order traking
      ref: MANAGER-13146
    
    Signed-off-by: soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/components/ip/agoraOrder/ip-ip-agoraOrder.controller.js
+++ packages/manager/apps/dedicated/client/app/components/ip/agoraOrder/ip-ip-agoraOrder.controller.js
@@ -79,11 +79,6 @@ export default class AgoraIpOrderCtrl {
   }
 
   goToIpv6Order() {
-    this.atInternet.trackClick({
-      name: `dedicated::ip::dashboard::order`,
-      type: 'action',
-    });
-
     this.$state.go('app.ip.agora-order.ipv6');
   }
 
