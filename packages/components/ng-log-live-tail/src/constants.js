64758b8e486b8b6bba2a75ee96c93863f54a4836
rjamet-ovh <157609363+rjamet-ovh@users.noreply.github.com>
2024-07-11 15:34:51 +0200

    feat(log-live-tail): update theme (#12089)
    ref: MANAGER-14318
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

--- packages/components/ng-log-live-tail/src/constants.js
+++ packages/components/ng-log-live-tail/src/constants.js
@@ -1,12 +1,12 @@
-export const PREFIX_KEYS = ['formattedTimestamp', 'levelLabel'];
+export const PREFIX_KEYS = ['formattedDate', 'formattedTime', 'levelLabel'];
 
 // https://en.wikipedia.org/wiki/Syslog#Severity_level
 export const LEVEL_LABELS = {
   0: 'emerg',
   1: 'alert',
   2: 'crit',
-  3: 'err',
-  4: 'warning',
+  3: 'error',
+  4: 'warn',
   5: 'notice',
   6: 'info',
   7: 'debug',
