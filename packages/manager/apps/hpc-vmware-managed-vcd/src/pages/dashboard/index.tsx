1d9fe366db796f7aef983b4bf2131203340f1f45
darsene <72250222+darsene@users.noreply.github.com>
2024-07-11 10:43:38 +0200

    feat(hpc-vmware-managed-vcd): add VCD organization dashboard (#12304)
    resolves: MANAGER-14401
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/hpc-vmware-managed-vcd/src/pages/dashboard/index.tsx
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/pages/dashboard/index.tsx
@@ -16,7 +16,9 @@ import {
 
 import { DashboardLayout } from '@ovhcloud/manager-components';
 
-import Breadcrumb from '@/components/Breadcrumb/Breadcrumb.component';
+import Breadcrumb from '@/components/breadcrumb/Breadcrumb.component';
+import useManagedVcdOrganization from '@/data/hooks/useManagedVcdOrganization';
+import { BreadcrumbItem } from '@/hooks/breadcrumb/useBreadcrumb';
 
 export type DashboardTabItemProps = {
   name: string;
@@ -30,21 +32,41 @@ export type DashboardLayoutProps = {
 
 export default function DashboardPage() {
   const [panel, setActivePanel] = useState('');
-  const { serviceName } = useParams();
+  const { id } = useParams();
   const location = useLocation();
   const navigate = useNavigate();
   const { t } = useTranslation('dashboard');
+  const { data: vcdOrganisation } = useManagedVcdOrganization(id);
 
   const tabsList = [
     {
       name: 'general_informations',
-      title: 'Informations générales',
+      title: t('managed_vcd_dashboard_general_information'),
       to: useResolvedPath('').pathname,
     },
     {
-      name: 'Tab 2',
-      title: 'Tab 2',
-      to: useResolvedPath('Tab2').pathname,
+      name: 'datacentres',
+      title: t('managed_vcd_dashboard_datacentres'),
+      to: useResolvedPath('datacentres').pathname,
+    },
+  ];
+
+  const serviceName = vcdOrganisation?.data?.currentState?.fullName;
+  const hasServiceRenamed = id !== serviceName;
+
+  // TODO use iam.displayName when available
+  const header = hasServiceRenamed
+    ? {
+        description: id,
+        title: serviceName,
+      }
+    : { title: id };
+
+  const breadcrumbItems: BreadcrumbItem[] = [
+    {
+      id,
+      label: serviceName,
+      onClick: () => navigate(`/${id}`),
     },
   ];
 
@@ -58,11 +80,6 @@ export default function DashboardPage() {
     }
   }, [location.pathname]);
 
-  const header = {
-    description: 'Description du hpc-vmware-managed-vcd',
-    title: serviceName,
-  };
-
   return (
     <div>
       <DashboardLayout
@@ -71,7 +88,7 @@ export default function DashboardPage() {
           <OsdsTabs panel={panel}>
             <OsdsTabBar slot="top">
               {tabsList.map((tab: DashboardTabItemProps) => (
-                <NavLink to={tab.to} className="no-underline">
+                <NavLink to={tab.to} className="no-underline" key={tab.name}>
                   <OsdsTabBarItem
                     key={`osds-tab-bar-item-${tab.name}`}
                     panel={tab.name}
@@ -83,7 +100,7 @@ export default function DashboardPage() {
             </OsdsTabBar>
           </OsdsTabs>
         }
-        breadcrumb={<Breadcrumb />}
+        breadcrumb={<Breadcrumb items={breadcrumbItems} />}
       />
       <Outlet />
     </div>
