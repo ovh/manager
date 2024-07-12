d690aa6f3c965726331e40cb7ffdfa18750c50ba
qpavy <contact@quentinpavy.com>
2024-07-12 14:13:01 +0200

    feat(vrack-services): hide order (#12182)
    ref: MANAGER-14628, MANAGER-14750
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

--- packages/manager/apps/vrack-services/mock/handlers.ts
+++ packages/manager/apps/vrack-services/mock/handlers.ts
@@ -14,6 +14,7 @@ import {
   GetAuthenticationMocks,
   getAuthenticationMocks,
 } from '../../../../../playwright-helpers/mocks/auth';
+import { getFeatureAvailabilityMock } from './feature-availability/feature-availability';
 
 export type ConfigParams = GetVrackServicesMocksParams &
   GetAuthenticationMocks &
@@ -30,4 +31,5 @@ export const getConfig = (params: ConfigParams): Handler[] =>
     getVrackMocks,
     getOrderDetailsMocks,
     getIamMocks,
+    getFeatureAvailabilityMock,
   ].flatMap((getMocks) => getMocks(params));
