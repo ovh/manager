2a0d4f9f6db3d5724722d21681659ed031963b63
Yoann Fievez <yoann.fievez@gmail.com>
2024-07-15 15:34:28 +0200

    feat(pci-workflow): add resource selector datagrid
    ref: DTCORE-2374
    Signed-off-by: Yoann Fievez <yoann.fievez@gmail.com>

--- packages/manager/apps/pci-workflow/src/api/data/instance.ts
+++ packages/manager/apps/pci-workflow/src/api/data/instance.ts
@@ -1,14 +1,64 @@
 import { v6 } from '@ovh-ux/manager-core-api';
+import { ColumnSort, PaginationState } from '@tanstack/react-table';
+import { TFlavor, TInstance } from '@/type';
 
-export type TInstance = {
-  id: string;
-  name: string;
-  region: string;
+export type TInstanceOptions = {
+  pagination: PaginationState;
+  sorting: ColumnSort;
 };
 
 export const getInstance = async (projectId: string, instanceId: string) => {
   const { data } = await v6.get<TInstance>(
     `/cloud/project/${projectId}/instance/${instanceId}`,
   );
+  return data;
+};
+
+export const getFlavor = async (
+  projectId: string,
+  flavoid: string,
+): Promise<TFlavor> => {
+  const { data } = await v6.get<TFlavor>(
+    `/cloud/project/${projectId}/flavor/${flavoid}`,
+  );
+  return data;
+};
+
+export const getAllInstance = async (
+  projectId: string,
+): Promise<TInstance[]> => {
+  const { data } = await v6.get<TInstance[]>(
+    `/cloud/project/${projectId}/instance`,
+  );
+  return data;
+};
+
+export const paginateResults = <T>(
+  items: T[],
+  pagination: PaginationState,
+) => ({
+  rows: items.slice(
+    pagination.pageIndex * pagination.pageSize,
+    (pagination.pageIndex + 1) * pagination.pageSize,
+  ),
+  pageCount: Math.ceil(items.length / pagination.pageSize),
+  totalRows: items.length,
+});
+
+export const sortResults = (items: TInstance[], sorting: ColumnSort) => {
+  let data: TInstance[];
+  if (sorting?.id === 'status') {
+    data = [...items].sort((a, b) => (a.statusGroup > b.statusGroup ? 1 : 0));
+  } else {
+    data = [...items].sort((a, b) => (a[sorting?.id] > b[sorting?.id] ? 1 : 0));
+  }
+
+  if (sorting) {
+    const { desc } = sorting;
+
+    if (desc) {
+      data.reverse();
+    }
+  }
   return data;
 };
---
+++ packages/manager/apps/pci-workflow/src/api/hooks/useInstances.ts
@@ -0,0 +1,110 @@
+import { useQueries, useQuery } from '@tanstack/react-query';
+import { useMemo } from 'react';
+import { applyFilters, Filter } from '@ovh-ux/manager-core-api';
+import { useTranslatedMicroRegions } from '@ovhcloud/manager-components';
+import {
+  getAllInstance,
+  getFlavor,
+  paginateResults,
+  sortResults,
+  TInstanceOptions,
+} from '@/api/data/instance';
+import { TInstance } from '@/type';
+
+export const getInstancesQuery = (projectId: string) => ({
+  queryKey: ['project', projectId, 'instances'],
+  queryFn: () => getAllInstance(projectId),
+});
+
+export const getStatusGroup = (status: string) => {
+  if (
+    [
+      'BUILDING',
+      'REBOOT',
+      'REBUILD',
+      'REVERT_RESIZE',
+      'SOFT_DELETED',
+      'VERIFY_RESIZE',
+      'MIGRATING',
+      'RESIZE',
+      'BUILD',
+      'SHUTOFF',
+      'RESCUE',
+      'SHELVED',
+      'SHELVED_OFFLOADED',
+      'RESCUING',
+      'UNRESCUING',
+      'SNAPSHOTTING',
+      'RESUMING',
+      'HARD_REBOOT',
+      'PASSWORD',
+      'PAUSED',
+    ].includes(status)
+  ) {
+    return 'PENDING';
+  }
+
+  if (
+    ['DELETED', 'ERROR', 'STOPPED', 'SUSPENDED', 'UNKNOWN'].includes(status)
+  ) {
+    return 'ERROR';
+  }
+
+  if (['ACTIVE', 'RESCUED', 'RESIZED'].includes(status)) {
+    return 'ACTIVE';
+  }
+  return status.toUpperCase();
+};
+
+export const useAllInstances = (projectId: string) => {
+  const { translateMicroRegion } = useTranslatedMicroRegions();
+  const { data: instances, isPending } = useQuery({
+    ...getInstancesQuery(projectId),
+  });
+
+  return useQueries({
+    queries: (instances || [])?.map((q) => ({
+      queryKey: ['project', projectId, 'flavor', q.flavorId],
+      queryFn: () => getFlavor(projectId, q.flavorId),
+      enabled: !isPending,
+    })),
+    combine: (results) => ({
+      isPending: results.some((r) => r.isPending),
+      data: instances?.map((instance) => {
+        const flavor = results.find(
+          (result) => result.data?.id === instance.flavorId,
+        )?.data;
+        return {
+          ...instance,
+          statusGroup: getStatusGroup(instance.status),
+          regionName: translateMicroRegion(instance.region),
+          flavorName: flavor?.name,
+          search: `${instance.name} ${instance.regionName} ${instance.statusGroup} ${flavor?.name}`,
+        };
+      }),
+      error: results.find((r) => r.error),
+    }),
+  });
+};
+
+export const useInstances = (
+  projectId: string,
+  { pagination, sorting }: TInstanceOptions,
+  filters: Filter[] = [],
+) => {
+  const { data: instances, error, isPending } = useAllInstances(projectId);
+  return useMemo(
+    () => ({
+      isPending,
+      error,
+      data: paginateResults<TInstance>(
+        sortResults(
+          applyFilters(((instances as unknown) as TInstance[]) || [], filters),
+          sorting,
+        ),
+        pagination,
+      ),
+    }),
+    [isPending, error, instances, pagination, sorting, filters],
+  );
+};
