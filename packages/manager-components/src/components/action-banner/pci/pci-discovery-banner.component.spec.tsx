ba54fcddf658e46231e431fe3259180ba4e502bc
Lio B <lionel95200x@users.noreply.github.com>
2024-07-15 09:58:38 +0200

    Revert "feat(manager-components): redesign action-banner (#12291)"
    This reverts commit 9523af27503d94d372cb6ca849f45009609d8dde.

--- packages/manager-components/src/components/action-banner/pci/pci-discovery-banner.component.spec.tsx
+++ packages/manager-components/src/components/action-banner/pci/pci-discovery-banner.component.spec.tsx
@@ -6,7 +6,6 @@ import {
   PciDiscoveryBannerProps,
 } from './pci-discovery-banner.component';
 import { render } from '../../../utils/test.provider';
-import trans from './translations/discovery/Messages_fr_FR.json';
 
 jest.mock('@ovh-ux/manager-react-shell-client', () => {
   const navigateTo = jest.fn();
@@ -29,9 +28,7 @@ describe('PciDiscoveryBanner tests', () => {
     renderComponent({
       projectId: '123',
     });
-    const cta = screen.getByText(
-      trans.pci_projects_project_activate_project_banner_cta,
-    );
+    const cta = screen.queryByTestId('actionBanner-button');
     expect(navigateTo).not.toHaveBeenCalled();
     fireEvent.click(cta);
     expect(navigateTo).toHaveBeenCalledWith(
