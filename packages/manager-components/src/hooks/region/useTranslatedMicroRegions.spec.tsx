5625c612aa58b4105edd62f890c2b40a94d03a83
Yoann Fievez <yoann.fievez@gmail.com>
2024-07-15 15:34:28 +0200

    feat(pci-workflow): add resource selector datagrid
    ref: DTCORE-2374
    Signed-off-by: Yoann Fievez <yoann.fievez@gmail.com>

---
+++ packages/manager-components/src/hooks/region/useTranslatedMicroRegions.spec.tsx
@@ -0,0 +1,48 @@
+import { renderHook } from '@testing-library/react';
+import {
+  getMacroRegion,
+  isLocalZone,
+  useTranslatedMicroRegions,
+} from './useTranslatedMicroRegions';
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    t: (key: string) => key,
+    i18n: { exists: () => true },
+  }),
+}));
+
+describe('getMacroRegion', () => {
+  it('returns correct macro region for local zone', () => {
+    expect(getMacroRegion('EU-WEST-LZ-MAD-A')).toBe('MAD');
+  });
+
+  it('returns correct macro region for non-local zone', () => {
+    expect(getMacroRegion('WES1')).toBe('WES');
+  });
+
+  it('returns empty string for invalid region', () => {
+    expect(getMacroRegion('111')).toBe('');
+  });
+
+  it('returns true for local zone', () => {
+    expect(isLocalZone('EU-WEST-LZ-MAD-A')).toBe(true);
+  });
+
+  it('returns false for non-local zone', () => {
+    expect(isLocalZone('WES1')).toBe(false);
+  });
+
+  it('returns false for invalid region', () => {
+    expect(isLocalZone('111')).toBe(false);
+  });
+});
+
+describe('useTranslatedMicroRegions', () => {
+  it('returns translated region', () => {
+    const { result } = renderHook(() => useTranslatedMicroRegions());
+    expect(result.current.translateMicroRegion('WES-1')).toBe(
+      'manager_components_region_WES_micro',
+    );
+  });
+});
