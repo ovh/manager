d2fbbdadb247a4a8f4c98fc6bb4551b36068981b
Yoann Fievez <yoann.fievez@gmail.com>
2024-07-15 15:34:28 +0200

    feat(pci-workflow): add resource selector datagrid
    ref: DTCORE-2374
    Signed-off-by: Yoann Fievez <yoann.fievez@gmail.com>

--- packages/manager-react-components/src/hooks/index.ts
+++ packages/manager-react-components/src/hooks/index.ts
@@ -16,6 +16,7 @@ export { useProductMaintenance } from './pci/useMaintenance';
 export {
   getMacroRegion,
   useTranslatedMicroRegions,
+  isLocalZone,
 } from './region/useTranslatedMicroRegions';
 
 export type TRegion = Region;
