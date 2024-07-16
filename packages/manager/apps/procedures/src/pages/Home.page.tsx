bb51013d95425e3b069a8ccfa64037c09a989187
JacquesLarique <134954692+JacquesLarique@users.noreply.github.com>
2024-07-16 10:53:47 +0200

    feat(procedures.2fa): fixed overflowing file slot (#12346)
    ref: MANAGER-14223
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/apps/procedures/src/pages/Home.page.tsx
+++ packages/manager/apps/procedures/src/pages/Home.page.tsx
@@ -50,13 +50,13 @@ export default function Home() {
   }, [isFetched]);
 
   return (
-    <div className="sm:container mx-auto px-5">
-      <div className="md:m-10 m-4">
-        <div className="p-3 md:py-5">
-          <div className="inline-block pb-3 md:pb-5">
-            <img src={ovhCloudLogo} alt="ovh-cloud-logo" className="app-logo" />
-          </div>
-          <div className="flex justify-center app-content lg:w-8/12 mx-auto min-h-[500px] sm:shadow sm:shadow-[0_0_6px_0_rgba(40,89,192,0.2)] sm:border-none border-t-[1px] border-gray-300">
+    <div className="sm:container mx-auto px-6">
+      <div className="md:py-12 p-6">
+        <div className="inline-block pb-6 md:pb-12">
+          <img src={ovhCloudLogo} alt="ovh-cloud-logo" className="app-logo" />
+        </div>
+        <div className="flex justify-center app-content lg:w-8/12 mx-auto min-h-[500px] sm:shadow sm:shadow-[0_0_6px_0_rgba(40,89,192,0.2)] sm:border-none border-t-[1px] border-gray-300 px-6">
+          <div className="md:p-8 w-full">
             {isLoading ? <SkeletonLoading /> : <Outlet />}
           </div>
         </div>
