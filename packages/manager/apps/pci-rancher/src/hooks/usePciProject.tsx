c3fc0f2d75dd76bb2cd71e1500bcf7467f407423
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2024-07-17 10:18:26 +0200

    refactor: comment reminder to remove usePciProject
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

--- packages/manager/apps/pci-rancher/src/hooks/usePciProject.tsx
+++ packages/manager/apps/pci-rancher/src/hooks/usePciProject.tsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 
 import { getProject } from '../api/apiv2/services';
 
+// TODO: remove this file
+
 const usePciProject = () => {
   const { projectId } = useParams();
 
