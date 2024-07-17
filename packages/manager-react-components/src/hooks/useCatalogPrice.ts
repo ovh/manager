1279831ceb3f3858a97fbe5b693dfd3fefc7071e
Arthur Bullet <arthur.bullet@ovhcloud.com>
2024-07-17 17:55:48 +0200

    feat(pci.ai-dashboard): fix build issue on manager-components
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>

--- packages/manager-react-components/src/hooks/useCatalogPrice.ts
+++ packages/manager-react-components/src/hooks/useCatalogPrice.ts
@@ -38,7 +38,7 @@ export const useCatalogPrice = (
 
   const getTextPrice = (priceInCents: number) => {
     const priceToFormat = priceInCents / 100000000;
-    const numberFormatOptions = {
+    const numberFormatOptions: Intl.NumberFormatOptions = {
       style: 'currency',
       currency: me?.currency?.code,
       ...(maximumFractionDigits ? { maximumFractionDigits } : {}),
