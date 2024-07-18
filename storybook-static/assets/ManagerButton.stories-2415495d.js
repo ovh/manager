a3daff727a7ea18eef76c6a4c21dea8391082887
ovh-ux-cds <ovh-ux-cds@users.noreply.github.com>
2024-07-18 08:44:06 +0000

    docs: update documentation [skip ci]

--- storybook-static/assets/ManagerButton.stories-2415495d.js
+++
@@ -1,8 +0,0 @@
-import{M as a}from"./ManagerButton-a43cfce6.js";import"./jsx-runtime-ffb262ed.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-7a8c9a23.js";import"./index-da03a860.js";import"./i18next-65ce221f.js";import"./setPrototypeOf-24bea416.js";import"./useOvhIam-1fd93ebf.js";import"./QueryClientProvider-5f9a1569.js";import"./useTranslation-756c8576.js";const r={args:{children:"Remove button",urn:"urn:v9:eu:resource:manager-components:vrz-a878-dsflkds-fdsfsd",iamActions:["vrackServices:apiovh:iam/resource/tag/remove"]}},l={title:"Components/Manager Button",component:a};var e,o,t;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    children: 'Remove button',
-    urn: 'urn:v9:eu:resource:manager-components:vrz-a878-dsflkds-fdsfsd',
-    iamActions: ['vrackServices:apiovh:iam/resource/tag/remove']
-  }
-}`,...(t=(o=r.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const h=["Default"];export{r as Default,h as __namedExportsOrder,l as default};
-//# sourceMappingURL=ManagerButton.stories-2415495d.js.map
