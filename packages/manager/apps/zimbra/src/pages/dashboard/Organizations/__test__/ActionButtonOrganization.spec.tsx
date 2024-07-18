e8d1894a8b0e4eed480663d4d9c97b9531fb77b9
Hyenne Guillaume <guillaume.hyenne@ovhcloud.com>
2024-07-18 09:09:51 +0200

    feat(zimbra): test implementation (#12133)
    ref: MANAGER-14500
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

---
+++ packages/manager/apps/zimbra/src/pages/dashboard/Organizations/__test__/ActionButtonOrganization.spec.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { vi, describe, expect } from 'vitest';
+import ActionButtonOrganization from '../ActionButtonOrganization';
+import { render } from '@/utils/test.provider';
+import organizationsTranslation from '@/public/translations/organizations/Messages_fr_FR.json';
+import { organizationListMock, platformMock } from '@/api/_mock_';
+
+vi.mock('@/hooks', async (importOriginal) => {
+  const actual: any = await importOriginal();
+  return {
+    ...actual,
+    usePlatform: vi.fn(() => ({
+      platformUrn: platformMock[0].iam.urn,
+    })),
+  };
+});
+
+describe('Organizations datagrid action menu', () => {
+  it('we have good number of item with good content', () => {
+    const { container } = render(
+      <ActionButtonOrganization organizationItem={organizationListMock[0]} />,
+    );
+
+    expect(container.querySelectorAll('osds-menu-item').length).toBe(2);
+
+    expect(container.querySelectorAll('osds-menu-item')[0]).toHaveTextContent(
+      organizationsTranslation.zimbra_organization_edit,
+    );
+
+    expect(container.querySelectorAll('osds-menu-item')[1]).toHaveTextContent(
+      organizationsTranslation.zimbra_organization_delete,
+    );
+  });
+});
