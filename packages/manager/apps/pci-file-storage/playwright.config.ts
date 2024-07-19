3565e56a042149c752ab699b3c7ff5385c685ccb
Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>
2024-07-19 14:49:18 +0200

    fix(pci.file-storage): e2e test
    ref: [TAPC-1294]
    Signed-off-by: Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>

--- packages/manager/apps/pci-file-storage/playwright.config.ts
+++ packages/manager/apps/pci-file-storage/playwright.config.ts
@@ -12,4 +12,10 @@ export default defineConfig({
     // Collect trace when retrying the failed test.
     trace: 'retain-on-failure',
   },
+  testMatch: '**/*.e2e.ts',
+  webServer: {
+    command: 'yarn run dev',
+    url:
+      'http://localhost:9000/#/public-cloud/pci/projects/85581adf7a784e74b21edc9a6fe2071d/file-storage/BHS1.PREPROD/d5226d48-c493-466d-8fb7-b1810579fc8b',
+  },
 });
