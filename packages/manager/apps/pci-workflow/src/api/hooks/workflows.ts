4f08b7c543ecc36a6fa7d799f64dea2e1c1716d1
Mohammed Hamdoune <sidlynx@gmail.com>
2024-07-19 09:38:39 +0200

    test(pci-workflow): listing page
    Signed-off-by: Mohammed Hamdoune <sidlynx@gmail.com>

--- packages/manager/apps/pci-workflow/src/api/hooks/workflows.ts
+++ packages/manager/apps/pci-workflow/src/api/hooks/workflows.ts
@@ -1,8 +1,8 @@
 import { useMutation, useQueries, useQueryClient } from '@tanstack/react-query';
-import { format, parseISO } from 'date-fns';
-import { PaginationState } from '@ovhcloud/manager-components';
+import { format, parseISO, addMinutes } from 'date-fns';
+import { ColumnSort, PaginationState } from '@ovhcloud/manager-components';
 import { applyFilters, Filter } from '@ovh-ux/manager-core-api';
-import { useRef } from 'react';
+import { useMemo, useRef } from 'react';
 import * as dateFnsLocales from 'date-fns/locale';
 import { useTranslation } from 'react-i18next';
 import { getDateFnsLocale } from '@ovh-ux/manager-core-utils';
@@ -74,9 +74,16 @@ export const useWorkflows = (projectId: string) => {
 
             return {
               ...w,
-              lastExecution: format(lastExecutionAt, 'dd MMM yyyy HH:mm:ss', {
-                locale: locales[userLocale],
-              }),
+              lastExecution: format(
+                addMinutes(
+                  lastExecutionAt,
+                  lastExecutionAt.getTimezoneOffset(),
+                ),
+                'dd MMM yyyy HH:mm:ss',
+                {
+                  locale: locales[userLocale],
+                },
+              ),
               lastExecutionStatus,
             };
           }))(),
@@ -85,10 +92,61 @@ export const useWorkflows = (projectId: string) => {
   });
 };
 
+export const defaultCompareFunction = (
+  key: keyof Omit<TWorkflow, 'executions'>,
+) => (a: TWorkflow, b: TWorkflow) => {
+  const aValue = a[key] || '';
+  const bValue = b[key] || '';
+
+  return aValue.localeCompare(bValue);
+};
+
+export const sortWorkflows = (
+  workflows: TWorkflow[],
+  sorting: ColumnSort,
+  searchQueries: string[],
+): TWorkflow[] => {
+  const data = [...workflows];
+
+  if (sorting) {
+    const { id: sortKey, desc } = sorting;
+
+    data.sort(
+      defaultCompareFunction(sortKey as keyof Omit<TWorkflow, 'executions'>),
+    );
+    if (desc) {
+      data.reverse();
+    }
+  }
+
+  if (searchQueries.length) {
+    type WorkflowKeys = keyof Omit<TWorkflow, 'executions'>;
+    const keys: WorkflowKeys[] = [
+      'name',
+      'instanceName',
+      'id',
+      'lastExecutionStatus',
+    ];
+    return data.filter((workflow) =>
+      keys.some((key) =>
+        searchQueries.some(
+          (query) =>
+            workflow[key] &&
+            workflow[key].toLowerCase().includes(query.toLowerCase()),
+        ),
+      ),
+    );
+  }
+
+  return data;
+};
+
 export const usePaginatedWorkflows = (
   projectId: string,
   pagination: PaginationState,
+  sorting: ColumnSort,
   filters: Filter[] = [],
+  searchQueries: string[] = [],
 ) => {
   const { data: workflows, isPending: isWorkflowsPending } = useWorkflows(
     projectId,
@@ -102,16 +160,20 @@ export const usePaginatedWorkflows = (
       enabled: !isWorkflowsPending,
     })),
     combine: (results) => ({
-      data: (() => {
+      data: useMemo(() => {
         const workflowsWithInstanceIds = workflows.map((workflow, i) => ({
           ...workflow,
           instanceName: results[i].data?.name,
         }));
         return paginateResults<TWorkflow>(
-          applyFilters(workflowsWithInstanceIds || [], filters),
+          applyFilters(
+            sortWorkflows(workflowsWithInstanceIds, sorting, searchQueries) ||
+              [],
+            filters,
+          ),
           pagination,
         );
-      })(),
+      }, [workflows, sorting, filters, searchQueries]),
       isPending:
         results.some((result) => result.isPending) || isWorkflowsPending,
     }),
