343838fc085a80273980f74ccfbcf9e9d1861320
Lio B <lionel95200x@users.noreply.github.com>
2024-07-19 14:22:41 +0200

    feat: setup create plan page (#12300)
    * feat: setup create plan page
    
    ref: TAPC-796
    
    * feat: add discovery banner
    
    ---------
    
    Co-authored-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>

---
+++ packages/manager/apps/pci-savings-plan/src/components/Commitment/Commitment.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { vi, describe, it, expect } from 'vitest';
+import { screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Commitment from './Commitment';
+import { render } from '@/mock/testProvider';
+
+const defaultProps = {
+  duration: '1',
+  price: '100',
+  hourlyPriceWithoutCommitment: 0.01,
+  isActive: false,
+  onClick: vi.fn(),
+};
+
+const setupSpecTest = async (props = defaultProps) =>
+  render(<Commitment {...props} />);
+
+describe('Commitment', () => {
+  it('should render the commitment duration', async () => {
+    await setupSpecTest();
+    expect(screen.getByText('1 mois')).toBeInTheDocument();
+  });
+
+  it('should render the commitment price', async () => {
+    await setupSpecTest();
+    expect(screen.getByText('93 %')).toBeInTheDocument();
+  });
+
+  it('should render the hourly price without commitment', async () => {
+    await setupSpecTest();
+    expect(screen.getByText('7.20 €')).toBeInTheDocument();
+  });
+});
