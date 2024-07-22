e47f8bf79b52750217cf4ab02350fc8c43c51fb4
Yoann Fievez <yoann.fievez@gmail.com>
2024-07-22 17:57:50 +0200

    feat(pci-kubernetes): add cluster rename modal
    ref: DTCORE-2453
    Signed-off-by: Yoann Fievez <yoann.fievez@gmail.com>

--- packages/manager/apps/pci-kubernetes/src/constants.ts
+++ packages/manager/apps/pci-kubernetes/src/constants.ts
@@ -1,18 +1,4 @@
-export const ACTIVE_STATUS = ['ok', 'READY'];
-export const PENDING_STATUS = ['PENDING', 'CREATING', 'UPDATING', 'DELETING'];
-
-export const HORIZON_LINK = {
-  EU: 'https://horizon.cloud.ovh.net/auth/login?username={username}',
-  CA: 'https://horizon.cloud.ovh.net/auth/login?username={username}',
-  US: 'https://horizon.cloud.ovh.us/auth/login?username={username}',
+export const NAME_INPUT_CONSTRAINTS = {
+  MAX_LENGTH: 64,
+  PATTERN: /^[a-zA-Z](([a-zA-Z0-9-]|_|-)*)[a-zA-Z0-9]$/,
 };
-
-export const HORIZON_LINK_TRUSTED = {
-  EU:
-    'https://horizon.trustedzone.cloud.ovh.net/auth/login?username={username}',
-  CA:
-    'https://horizon.trustedzone.cloud.ovh.net/auth/login?username={username}',
-  US: '',
-};
-
-export const ALPHA_CHARACTERS_REGEX = /^[a-zA-Z-]+$/;
