fa0f4055bbec4d157e164ec746d830a476e145f7
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2024-07-23 11:32:57 +0200

    feat(pci-kubernetes): add cluster informations tile
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/apps/pci-kubernetes/src/pages/detail/service/Service.page.tsx
+++ packages/manager/apps/pci-kubernetes/src/pages/detail/service/Service.page.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import ClusterInformation from '@/components/information/ClusterInformation.component';
 
 export default function ServicePage() {
   return (
     <div>
-      <p>Kube Service</p>
+      <ClusterInformation />
       <Outlet />
     </div>
   );
