fece21b83ce63f903aeb90ec74d44b4adff61825
Florian Renaut <florian.renaut@corp.ovh.com>
2024-07-23 17:31:49 +0200

    feat(pci-kubernetes): add flavor selector
    ref: DTCORE-2447
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

---
+++ packages/manager/apps/pci-kubernetes/src/api/data/catalog.ts
@@ -0,0 +1,130 @@
+import { v6 } from '@ovh-ux/manager-core-api';
+
+export type TPricing = {
+  capacities: string[];
+  mode: string;
+  phase: number;
+  commitment: number;
+  description: string;
+  price: {
+    currencyCode: string;
+    text: string;
+    value: number;
+  };
+  tax: number;
+  interval: number;
+  intervalUnit: string;
+  quantity: {
+    max?: number;
+    min?: number;
+  };
+  repeat: {
+    max?: number;
+    min?: number;
+  };
+  strategy: string;
+  mustBeCompleted: boolean;
+  type: string;
+  promotions: unknown[];
+  engagementConfiguration?: unknown;
+};
+
+export type TAddonFamily = {
+  addons: string[];
+  default?: string;
+  name: string;
+  exclusive: boolean;
+  mandatory: boolean;
+};
+
+export type TPlan = {
+  planCode: string;
+  invoiceName: string;
+  product: string;
+  pricingType: string;
+  consumptionConfiguration: string;
+  pricings: TPricing[];
+  addonFamilies: TAddonFamily[];
+};
+
+export type TCatalog = {
+  catalogId: string;
+  locale: {
+    currencyCode: string;
+    subsidiary: string;
+    taxRate: number;
+  };
+  plans: TPlan[];
+  addons: TAddon[];
+};
+
+export type TAddon = {
+  planCode: string;
+  blobs: {
+    tags: string[];
+    technical: {
+      bandwidth?: {
+        guaranteed: boolean;
+        level: number;
+        max: number;
+        unit: string;
+        unlimited: boolean;
+      };
+      cpu?: {
+        boost: number;
+        brand: string;
+        cores: number;
+        customizable: boolean;
+        frequency: number;
+        maxFrequency: number;
+        model: string;
+        number: number;
+        score: number;
+        threads: number;
+        type: string;
+      };
+      nvme?: {
+        disks: {
+          capacity: number;
+          number: number;
+          sizeUnit: string;
+          technology: string;
+        }[];
+      };
+      storage?: {
+        disks: {
+          capacity: number;
+          number: number;
+          sizeUnit: string;
+          technology: string;
+        }[];
+      };
+      volume?: {
+        iops: {
+          level: number;
+          max: number;
+          guaranteed: boolean;
+          unit: string;
+          maxUnit: string;
+        };
+        capacity: {
+          max: number;
+        };
+      };
+      name: string;
+    };
+  };
+  pricings: {
+    price: number;
+    type: string;
+    capacities: string[];
+  }[];
+};
+
+export const getCatalog = async (ovhSubsidiary: string): Promise<TCatalog> => {
+  const { data } = await v6.get<TCatalog>(
+    `/order/catalog/public/cloud?ovhSubsidiary=${ovhSubsidiary}`,
+  );
+
+  return data;
+};
