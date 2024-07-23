e0ba00894afb864ade7ad6461433dfcc965e7034
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2024-07-23 11:32:57 +0200

    feat(pci-kubernetes): add cluster informations tile
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/apps/pci-kubernetes/src/components/detail/TabsPanel.component.tsx
+++ packages/manager/apps/pci-kubernetes/src/components/detail/TabsPanel.component.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 import {
   OsdsTabs,
   OsdsTabBar,
