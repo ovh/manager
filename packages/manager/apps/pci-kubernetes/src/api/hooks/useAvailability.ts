e78ef09cbcf4273f5215b710ada58a489ae60905
Florian Renaut <florian.renaut@corp.ovh.com>
2024-07-23 17:31:49 +0200

    feat(pci-kubernetes): add flavor selector
    ref: DTCORE-2447
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

---
+++ packages/manager/apps/pci-kubernetes/src/api/hooks/useAvailability.ts
@@ -0,0 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+import { useMe } from '@ovhcloud/manager-components';
+import { getProductAvailability } from '@/api/data/availability';
+
+export const getProductAvailabilityQuery = (
+  projectId: string,
+  ovhSubsidiary: string,
+) => ({
+  queryKey: ['product-availability', projectId, ovhSubsidiary],
+  queryFn: () => getProductAvailability(projectId, ovhSubsidiary),
+});
+
+export const useProductAvailability = (projectId: string) => {
+  const { me } = useMe();
+  return useQuery({
+    ...getProductAvailabilityQuery(projectId, me?.ovhSubsidiary),
+    enabled: !!me,
+  });
+};
