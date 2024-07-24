ae47d0841b8b54f2b18d8b01164d687aa69dc652
JacquesLarique <134954692+JacquesLarique@users.noreply.github.com>
2024-07-24 09:40:41 +0200

    feat(procedures.2fa): set up 2fa procedure application (#12459)
    ref: MANAGER-13716
    
    Co-authored-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>
    Co-authored-by: Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>
    Co-authored-by CDS Translator Agent <opensource@ovh.net>

---
+++ packages/manager/apps/procedures/src/utils/token.ts
@@ -0,0 +1,27 @@
+export function extractToken(): string | null {
+  const searchParams: URLSearchParams = new URLSearchParams(
+    window.location.href.split('?')[1],
+  );
+  return searchParams.get('token');
+}
+
+export function decodeToken(token = '') {
+  if (!token) {
+    return null;
+  }
+
+  const rawPayloadBase64 = token.split('.')[1];
+  const payloadFormattedBase64 = rawPayloadBase64.replace(
+    /([-_])/g,
+    (match: string) => (match === '-' ? '+' : '/'),
+  );
+  const rawPayload = window.atob(payloadFormattedBase64);
+  const formattedPayload = rawPayload.replace(
+    /(.)/g,
+    (match: string, char: string) =>
+      `%${`00${char.charCodeAt(0).toString(16)}`.slice(-2)}`,
+  );
+  const decodedPayload = decodeURIComponent(formattedPayload);
+
+  return JSON.parse(decodedPayload);
+}
