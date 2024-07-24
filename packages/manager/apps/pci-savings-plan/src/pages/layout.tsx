6f7799a443c9554aec19b1b5858e3264d91946df
brofik <10513484+brofik@users.noreply.github.com>
2024-07-24 10:02:46 +0200

    feat(pci.savings-plan): create savings plan listing page (#12176)
    * feat(pci.savings-plan): create savings plan listing page
    
    ref: TAPC-856
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
    Signed-off-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>
    
    * feat: renew modal
    
    Signed-off-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>
    
    * fix: use new datagrid
    
    ref: TAPC-804
    
    Signed-off-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>
    
    ---------
    
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
    Signed-off-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>
    Co-authored-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
    Co-authored-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>

--- packages/manager/apps/pci-savings-plan/src/pages/layout.tsx
+++ packages/manager/apps/pci-savings-plan/src/pages/layout.tsx
@@ -1,11 +1,22 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, Suspense } from 'react';
 import { defineCurrentPage } from '@ovh-ux/request-tagger';
-import { Outlet, useLocation, useMatches } from 'react-router-dom';
+import {
+  Navigate,
+  Outlet,
+  useLocation,
+  useMatches,
+  useParams,
+} from 'react-router-dom';
 import {
   useOvhTracking,
   useRouteSynchro,
   ShellContext,
 } from '@ovh-ux/manager-react-shell-client';
+import Loading from '@/components/Loading/Loading';
+import { useSavingsPlan } from '@/hooks/useSavingsPlan';
+import Errors from '@/components/Error/Error';
+import Breadcrumb from '@/components/Breadcrumb/Breadcrumb';
+import { PageLayout } from '@ovhcloud/manager-components';
 
 export default function Layout() {
   const location = useLocation();
@@ -27,5 +38,47 @@ export default function Layout() {
     shell.ux.hidePreloader();
   }, []);
 
-  return <Outlet />;
+  /*
+  Not Work on LABEU but we need to fetch this ..
+
+  const { isLoading, data, isError, error } = useProject();
+
+
+  if (isError || error) {
+    return <ErrorBanner error={error.response} />;
+  }
+
+  if (isLoading || !data) {
+    return <Loading />;
+  } */
+
+  const { projectId } = useParams();
+  const { data: services, isLoading, isError, error } = useSavingsPlan();
+
+  if (isError || error) {
+    return <Errors error={error.message} />;
+  }
+
+  if (isLoading || !services) {
+    return (
+      <div className="flex justify-center items-center h-full">
+        <Loading />
+      </div>
+    );
+  }
+
+  if (services.length === 0) {
+    return <Navigate to={`/pci/project/${projectId}/onboarding`} />;
+  }
+
+  return (
+    <PageLayout>
+      <div className="flex flex-col my-5">
+        <Suspense fallback={<Loading />}>
+          <Breadcrumb />
+        </Suspense>
+      </div>
+      <Outlet />
+    </PageLayout>
+  );
 }
