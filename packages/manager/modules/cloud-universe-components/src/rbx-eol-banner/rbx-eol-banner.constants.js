f34aca920036ec7efaea5b8397c6103131b5bfd8
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2024-07-24 09:56:34 +0200

    feat(*): add link for rbx1 imminent eol on hub and dedicated
    ref: MANAGER-12910
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

--- packages/manager/modules/cloud-universe-components/src/rbx-eol-banner/rbx-eol-banner.constants.js
+++ packages/manager/modules/cloud-universe-components/src/rbx-eol-banner/rbx-eol-banner.constants.js
@@ -43,7 +43,30 @@ export const GUIDE_LINK = {
     'https://help.ovhcloud.com/csm/es-dedicated-servers-migrate-data-between-servers?id=kb_article_view&sysparm_article=KB0043703',
 };
 
-export const IMMINENT_LINK = 'TODO';
+export const IMMINENT_LINK = {
+  DEFAULT: 'https://www.ovhcloud.com/en/lp/end-service-rbx-1/',
+  ASIA: 'https://www.ovhcloud.com/asia/lp/end-service-rbx-1/',
+  AU: 'https://www.ovhcloud.com/en-au/lp/end-service-rbx-1/',
+  CA: 'https://www.ovhcloud.com/en-ca/lp/end-service-rbx-1/',
+  DE: 'https://www.ovhcloud.com/de/lp/end-service-rbx-1/',
+  ES: 'https://www.ovhcloud.com/es-es/lp/end-service-rbx-1/',
+  FR: 'https://www.ovhcloud.com/fr/lp/end-service-rbx-1/',
+  GB: 'https://www.ovhcloud.com/en-gb/lp/end-service-rbx-1/',
+  IE: 'https://www.ovhcloud.com/en-ie/lp/end-service-rbx-1/',
+  IN: 'https://www.ovhcloud.com/en-in/lp/end-service-rbx-1/',
+  IT: 'https://www.ovhcloud.com/it/lp/end-service-rbx-1/',
+  MA: 'https://www.ovhcloud.com/fr-ma/lp/end-service-rbx-1/',
+  NL: 'https://www.ovhcloud.com/nl/lp/end-service-rbx-1/',
+  PL: 'https://www.ovhcloud.com/pl/lp/end-service-rbx-1/',
+  PT: 'https://www.ovhcloud.com/pt/lp/end-service-rbx-1/',
+  QC: 'https://www.ovhcloud.com/fr-ca/lp/end-service-rbx-1/',
+  SN: 'https://www.ovhcloud.com/fr-sn/lp/end-service-rbx-1/',
+  SG: 'https://www.ovhcloud.com/en-sg/lp/end-service-rbx-1/',
+  TN: 'https://www.ovhcloud.com/fr-tn/lp/end-service-rbx-1/',
+  US: 'https://www.ovhcloud.com/en/lp/end-service-rbx-1/',
+  WE: 'https://www.ovhcloud.com/en-ie/lp/end-service-rbx-1/',
+  WS: 'https://www.ovhcloud.com/es/lp/end-service-rbx-1/',
+};
 
 export default {
   GUIDE_LINK,
