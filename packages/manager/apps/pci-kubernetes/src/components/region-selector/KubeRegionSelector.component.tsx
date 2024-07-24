e8009db4fb033787529e8f505bbafe4d93efa9ed
Florian Renaut <florian.renaut@corp.ovh.com>
2024-07-24 16:59:47 +0200

    feat(pci-kubernetes): add region selector
    ref: DTCORE-2461
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

---
+++ packages/manager/apps/pci-kubernetes/src/components/region-selector/KubeRegionSelector.component.tsx
@@ -0,0 +1,37 @@
+import {
+  RegionSelector,
+  RegionSelectorProps,
+} from '@ovh-ux/manager-pci-common';
+import { OsdsSpinner } from '@ovhcloud/ods-components/react';
+import { ODS_SPINNER_SIZE } from '@ovhcloud/ods-components';
+import { useProductAvailability } from '@/api/hooks/useAvailability';
+
+export interface KubeRegionSelectorProps {
+  projectId: string;
+  onSelectRegion: RegionSelectorProps['onSelectRegion'];
+}
+
+export function KubeRegionSelector({
+  projectId,
+  onSelectRegion,
+}: Readonly<KubeRegionSelectorProps>) {
+  const { data: availability, isPending } = useProductAvailability(projectId);
+  if (isPending) {
+    return <OsdsSpinner inline size={ODS_SPINNER_SIZE.md} />;
+  }
+  return (
+    <RegionSelector
+      projectId={projectId}
+      onSelectRegion={onSelectRegion}
+      regionFilter={(region) => {
+        const product = availability?.products.find(
+          ({ name }) => name === 'kubernetes',
+        );
+        return (
+          region.isMacro ||
+          product?.regions?.some(({ name }) => name === region.name)
+        );
+      }}
+    />
+  );
+}
