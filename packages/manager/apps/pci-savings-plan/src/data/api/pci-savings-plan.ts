ba8dc71ed44101228edc304367da7713efbb63bc
brofik <10513484+brofik@users.noreply.github.com>
2024-07-24 10:02:46 +0200

    feat(pci.savings-plan): create savings plan listing page (#12176)
    * feat(pci.savings-plan): create savings plan listing page
    
    ref: TAPC-856
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
    Signed-off-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>
    
    * feat: renew modal
    
    Signed-off-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>
    
    * fix: use new datagrid
    
    ref: TAPC-804
    
    Signed-off-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>
    
    ---------
    
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
    Signed-off-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>
    Co-authored-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
    Co-authored-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>

--- packages/manager/apps/pci-savings-plan/src/data/api/pci-savings-plan.ts
+++ packages/manager/apps/pci-savings-plan/src/data/api/pci-savings-plan.ts
@@ -1,42 +1,8 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { fetchIcebergV2, apiClient } from '@ovh-ux/manager-core-api';
-
 export type GetPublicCloudProjectProjectIdParams = {
   /** Project ID */
-  projectId?: any;
+  projectId?: string;
 };
 
 export const getPublicCloudProjectProjectIdQueryKey = (
   params: GetPublicCloudProjectProjectIdParams,
 ) => [`get/publicCloud/project/${params.projectId}`];
-
-/**
- * Manage Public Cloud projects : Get details on a Public Cloud project
- */
-export const getPublicCloudProjectProjectId = async (
-  params: GetPublicCloudProjectProjectIdParams,
-): Promise<any> => apiClient.v2.get(`/publicCloud/project/${params.projectId}`);
-
-/**
- *  Get listing with iceberg V2
- */
-
-export const getListingIcebergV2 = async ({
-  projectId,
-  pageSize,
-  cursor,
-}: {
-  projectId: string;
-  pageSize: number;
-  cursor?: string;
-}) => {
-  const { data, status, cursorNext } = await fetchIcebergV2({
-    route: `/publicCloud/project/${projectId}/saving-plan`,
-    pageSize,
-    cursor,
-  });
-  if (status > 400) {
-    throw new Error();
-  }
-  return { data, status, cursorNext };
-};
