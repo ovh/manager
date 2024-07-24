8f45ff160b9acea7225919d63c77a1eb9bc2ba2b
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2024-07-24 09:56:34 +0200

    feat(*): add link for rbx1 imminent eol on hub and dedicated
    ref: MANAGER-12910
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

--- packages/manager/modules/cloud-universe-components/src/rbx-eol-banner/rbx-eol-banner.controller.js
+++ packages/manager/modules/cloud-universe-components/src/rbx-eol-banner/rbx-eol-banner.controller.js
@@ -3,12 +3,12 @@ import { GUIDE_LINK, IMMINENT_LINK } from './rbx-eol-banner.constants';
 export default class RbxEolBannerController {
   /* @ngInject */
   constructor(coreConfig) {
-    this.coreConfig = coreConfig;
+    this.userSubsidiary = coreConfig.getUser().ovhSubsidiary;
   }
 
   $onInit() {
-    this.IMMINENT_LINK = IMMINENT_LINK;
-    this.GUIDE_LINK =
-      GUIDE_LINK[this.coreConfig.getUser().ovhSubsidiary] || GUIDE_LINK.DEFAULT;
+    this.IMMINENT_LINK =
+      IMMINENT_LINK[this.userSubsidiary] || IMMINENT_LINK.DEFAULT;
+    this.GUIDE_LINK = GUIDE_LINK[this.userSubsidiary] || GUIDE_LINK.DEFAULT;
   }
 }
