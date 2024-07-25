26c322b6bd9e1d7c47480bd3185944e1d6ebc6a2
Florian Renaut <florian.renaut@corp.ovh.com>
2024-07-25 16:23:34 +0200

    refactor(pci-kubernetes): move flavor-selector to pci-common
    ref: DTCORE-2447
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/modules/manager-pci-common/src/index.ts
+++ packages/manager/modules/manager-pci-common/src/index.ts
@@ -1,3 +1,6 @@
 export * from './api/data';
 export * from './api/hook';
 export * from './components/banner';
+export * from './components/flavor-selector';
+export * from './hooks';
+export * from './constants';
