a6db0bb3847575607f228f33cf6fefe191475295
Mohammed Hamdoune <sidlynx@gmail.com>
2024-07-28 21:59:48 +0200

    feat(pci-kubernetes): nodes listing page
    ref: DTCORE-2471
    Signed-off-by: Mohammed Hamdoune <sidlynx@gmail.com>

---
+++ packages/manager/apps/pci-kubernetes/src/api/data/nodes.ts
@@ -0,0 +1,38 @@
+import { v6 } from '@ovh-ux/manager-core-api';
+
+type TRawNode = {
+  createdAt: string;
+  deployedAt: string;
+  flavor: string;
+  id: string;
+  instanceId: string;
+  isUpToDate: boolean;
+  name: string;
+  nodepoolId: string;
+  projectId: string;
+  status: 'READY' | 'DEPLOYING' | 'DELETING' | 'ERROR' | 'UPDATING';
+  updatedAt: string;
+  version: string;
+};
+
+export type TNode = TRawNode & {
+  formattedFlavor: string;
+  billingType: string;
+};
+
+export const getNodes = async (
+  projectId: string,
+  clusterId: string,
+  nodePoolId: string,
+): Promise<TNode[]> => {
+  const { data: items } = await v6.get<TRawNode[]>(
+    `/cloud/project/${projectId}/kube/${clusterId}/nodepool/${nodePoolId}/nodes`,
+  );
+
+  return items.map((item) => ({
+    ...item,
+    formattedFlavor: item.flavor,
+    // TODO
+    billingType: '',
+  }));
+};
