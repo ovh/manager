48bbefa967f5987e3a5841e32c8ea65b164884ca
abullet33 <106544940+abullet33@users.noreply.github.com>
2024-07-29 18:10:05 +0200

    feat(pci.ai-dashboard): onboarding page (#12570)
    * feat(pci.ai-dashboard): onboarding page and unit tests
    
    * feat(pci.ai-dashboard): rework cli page
    
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>

--- packages/manager/apps/pci-ai-dashboard/src/data/api/ai/authorization.api.ts
+++ packages/manager/apps/pci-ai-dashboard/src/data/api/ai/authorization.api.ts
@@ -4,11 +4,5 @@ import * as ai from '@/types/cloud/project/ai';
 
 export const getAuthorization = async ({ projectId }: PCIAi) =>
   apiClient.v6
-    .get(`/cloud/project/${projectId}/ai/authorization`, {
-      headers: {
-        'X-Pagination-Mode': 'CachedObjectList-Pages',
-        'X-Pagination-Size': '50000',
-        Pragma: 'no-cache',
-      },
-    })
+    .get(`/cloud/project/${projectId}/ai/authorization`)
     .then((res) => res.data as ai.AuthorizationStatus);
