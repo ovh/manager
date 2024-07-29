e181a86de48caf176eed0f55626aae6985b432cc
Lio B <lionel95200x@users.noreply.github.com>
2024-07-29 10:24:46 +0200

    feat: add edit name for plan (#12534)
    ref: TAPC-856
    
    Signed-off-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>
    Co-authored-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>

--- packages/manager/apps/pci-savings-plan/src/utils/test/test.provider.tsx
+++ packages/manager/apps/pci-savings-plan/src/utils/test/test.provider.tsx
@@ -4,14 +4,11 @@ import React, { ComponentType } from 'react';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
-import onboardingTranslation from '../../../public/translations/onboarding/Messages_fr_FR.json'
+import onboardingTranslation from '../../../public/translations/onboarding/Messages_fr_FR.json';
 import listingTranslation from '../../../public/translations/listing/Messages_fr_FR.json';
 import queryClient from '../../query.client';
 
-const ns = [
-  'pci-savings-plan/onboarding',
-  'pci-savings-plan/listing',
-];
+const ns = ['pci-savings-plan/onboarding', 'pci-savings-plan/listing'];
 
 i18n.use(initReactI18next).init({
   lng: 'fr',
@@ -30,7 +27,7 @@ const Wrappers = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
       <MemoryRouter>
-        <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
+        <I18nextProvider i18n={i18n as any}>{children}</I18nextProvider>
       </MemoryRouter>
     </QueryClientProvider>
   );
