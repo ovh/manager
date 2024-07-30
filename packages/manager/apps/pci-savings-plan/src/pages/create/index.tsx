6eb0476c54f659db99ce989ff54f3ebdb901ac2a
Lionel Bueno <lionel.bueno.ext@ovhcloud.com>
2024-07-30 14:30:57 +0200

    feat: use api commercial catalog
    Signed-off-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>

--- packages/manager/apps/pci-savings-plan/src/pages/create/index.tsx
+++ packages/manager/apps/pci-savings-plan/src/pages/create/index.tsx
@@ -1,3 +1,4 @@
+import { CreatePlanFormContainer } from '@/components/CreatePlanForm/CreatePlanForm';
 import {
   PciDiscoveryBanner,
   Title,
@@ -6,31 +7,19 @@ import {
 } from '@ovhcloud/manager-components';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { CreatePlanFormContainer } from '@/components/CreatePlanForm/CreatePlanForm';
-import Errors from '@/components/Error/Error';
-import Loading from '@/components/Loading/Loading';
 
 const CreateSavingsPlan = () => {
   const { t } = useTranslation('create');
-  const { data: project, isLoading, error } = useProject();
 
-  if (isLoading) {
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <Loading />
-      </div>
-    );
-  }
-  if (error) {
-    return <Errors error={error} />;
-  }
+  const { data: project } = useProject();
 
   return (
-    <div className="m-10 max-w-4xl">
+    <div className="max-w-4xl">
       <Title>{t('title')}</Title>
       {isDiscoveryProject({ planCode: project?.planCode }) && (
         <PciDiscoveryBanner projectId={project.project_id} />
       )}
+
       <CreatePlanFormContainer />
     </div>
   );
