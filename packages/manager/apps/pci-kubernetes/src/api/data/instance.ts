b2de452b13c78aa1c08bc6c6908ac7112f5df5d2
Mohammed Hamdoune <sidlynx@gmail.com>
2024-07-31 16:41:04 +0200

    feat(pci-kubernetes): node instance monthly billing page
    ref: DTCORE-2472
    Signed-off-by: Mohammed Hamdoune <sidlynx@gmail.com>

--- packages/manager/apps/pci-kubernetes/src/api/data/instance.ts
+++ packages/manager/apps/pci-kubernetes/src/api/data/instance.ts
@@ -3,9 +3,16 @@ import { v6 } from '@ovh-ux/manager-core-api';
 type TRawInstance = {
   id: string;
   monthlyBilling?: { status: string };
+  flavorId: string;
+  imageId: string;
+  name: string;
+  planCode: string;
+  region: string;
+  sshKeyId: string;
+  status: string;
 };
 
-export type TInstance = TRawInstance;
+export type TInstance = Pick<TRawInstance, 'id' | 'monthlyBilling'>;
 
 export const getInstances = async (projectId: string): Promise<TInstance[]> => {
   const { data } = await v6.get<TInstance[]>(
@@ -14,3 +21,15 @@ export const getInstances = async (projectId: string): Promise<TInstance[]> => {
 
   return data as TInstance[];
 };
+
+export const switchToMonthlyBilling = async (
+  projectId: string,
+  instanceId: string,
+) =>
+  v6.post(
+    `/cloud/project/${projectId}/instance/${instanceId}/activeMonthlyBilling`,
+    {
+      instanceId,
+      serviceName: projectId,
+    },
+  );
