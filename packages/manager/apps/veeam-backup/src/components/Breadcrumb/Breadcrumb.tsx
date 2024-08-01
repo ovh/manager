0d42476236051eec691455b38811d28182e91f50
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2024-08-01 15:46:47 +0200

    feat(veeam-backup): epic for new app veeam-backup
    ref: MANAGER-14740
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

---
+++ packages/manager/apps/veeam-backup/src/components/Breadcrumb/Breadcrumb.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { OsdsBreadcrumb } from '@ovhcloud/ods-components/react';
+import {
+  useBreadcrumb,
+  BreadcrumbItem,
+} from '@/hooks/breadcrumb/useBreadcrumb';
+import appConfig from '@/veeam-backup.config';
+
+export interface BreadcrumbProps {
+  customRootLabel?: string;
+  appName?: string;
+  items?: BreadcrumbItem[];
+}
+
+function Breadcrumb({ customRootLabel }: BreadcrumbProps): JSX.Element {
+  const label = customRootLabel || appConfig.rootLabel;
+
+  const breadcrumbItems = useBreadcrumb({
+    rootLabel: label,
+    appName: 'veeam-backup',
+  });
+  return <OsdsBreadcrumb items={breadcrumbItems} />;
+}
+
+export default Breadcrumb;
