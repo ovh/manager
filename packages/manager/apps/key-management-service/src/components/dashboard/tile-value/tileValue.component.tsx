370d597998fcb89cfdd7e26ca8cfb71704da9b5c
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2024-08-02 15:03:24 +0200

    feat(kms): manage more than one key usage
    ref: MANAGER-14861
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

--- packages/manager/apps/key-management-service/src/components/dashboard/tile-value/tileValue.component.tsx
+++ packages/manager/apps/key-management-service/src/components/dashboard/tile-value/tileValue.component.tsx
@@ -8,17 +8,12 @@ type TileTextProps = {
   isText?: boolean;
 };
 
-export const TileValue: React.FC<TileTextProps> = ({
-  value,
-  isText = false,
-}) => {
+export const TileValue: React.FC<TileTextProps> = ({ value }) => {
   return (
     <OsdsText
       size={ODS_TEXT_SIZE._400}
       level={ODS_TEXT_LEVEL.body}
-      color={
-        isText ? ODS_THEME_COLOR_INTENT.text : ODS_THEME_COLOR_INTENT.default
-      }
+      color={ODS_THEME_COLOR_INTENT.default}
     >
       {value}
     </OsdsText>
