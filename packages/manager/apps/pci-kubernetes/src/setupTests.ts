48a5dfc3b2d7891ad910702f3797a27a96d08fe5
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2024-08-07 16:05:25 +0200

    feat(pci-kubernetes): fixes lint and refactor tests mocks
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/apps/pci-kubernetes/src/setupTests.ts
+++ packages/manager/apps/pci-kubernetes/src/setupTests.ts
@@ -2,40 +2,27 @@ import '@testing-library/jest-dom';
 import 'element-internals-polyfill';
 import { vi } from 'vitest';
 
-vi.mock('react-router-dom', () => ({
-  useParams: () => ({ projectId: 'project-id', kubeId: 'kube-id' }),
-  useHref: vi.fn(),
-  useLocation: vi.fn(),
-  useNavigate: vi.fn(),
-  Navigate: () => null,
-  Outlet: vi.fn(() => 'Outlet'),
-}));
+vi.mock('react-router-dom', async () => {
+  const mod = await vi.importActual('@ovh-ux/manager-pci-common');
+  return {
+    ...mod,
+    useSearchParams: () => [new URLSearchParams({})],
+    useParams: () => ({ projectId: 'project-id', kubeId: 'kube-id' }),
+    useHref: vi.fn(),
+    useLocation: vi.fn(),
+    useNavigate: vi.fn(),
+    Navigate: () => null,
+    Outlet: vi.fn(() => 'Outlet'),
+  };
+});
 
 vi.mock('@ovh-ux/manager-pci-common', async () => {
   const mod = await vi.importActual('@ovh-ux/manager-pci-common');
   return {
     ...mod,
     useProject: vi.fn().mockResolvedValue({
-      access: 'full',
-      creationDate: '2023-08-07T12:00:00Z',
-      description: 'This is a mock project for testing purposes.',
-      expiration: null,
-      iam: {
-        displayName: 'Mock Display Name',
-        id: 'iam-12345',
-        tags: {
-          environment: 'test',
-          department: 'engineering',
-        },
-        urn: 'urn:mock:iam:12345',
-      },
-      manualQuota: true,
-      orderId: 987654321,
-      planCode: 'PLAN_BASIC',
-      projectName: 'Mock Project',
-      project_id: 'project-12345',
-      status: 'active', // Assuming TProjectStatus includes 'active'
-      unleash: false,
+      projectName: 'project-name',
+      project_id: 'project-id',
     }),
   };
 });
