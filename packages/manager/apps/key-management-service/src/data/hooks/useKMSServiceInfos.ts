a90a22b14013c324f913fde8c8a1a47e96ff1447
Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>
2024-08-07 14:49:37 +0200

    feat(key-management-service): added service status tile ref: MANAGER-14774
    Signed-off-by: Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>

--- packages/manager/apps/key-management-service/src/data/hooks/useKMSServiceInfos.ts
+++ packages/manager/apps/key-management-service/src/data/hooks/useKMSServiceInfos.ts
@@ -4,10 +4,10 @@ import { ErrorResponse } from '@/types/api.type';
 import { KMSServiceInfos } from '@/types/okmsService.type';
 import { getServiceInfos } from '../api/okmsService';
 
-export const useKMSServiceInfos = (okms: OKMS) => {
+export const useKMSServiceInfos = (okms?: OKMS) => {
   return useQuery<{ data: KMSServiceInfos }, ErrorResponse>({
-    queryKey: ['okms/service/infos', okms.id],
-    queryFn: () => getServiceInfos({ okms: okms.id }),
+    queryKey: ['okms/service/infos', okms?.id],
+    queryFn: () => getServiceInfos({ okms: okms?.id }),
     retry: false,
     ...{
       keepPreviousData: true,
