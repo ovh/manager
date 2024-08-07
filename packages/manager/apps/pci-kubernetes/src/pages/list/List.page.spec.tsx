4209f73809283bb33298517eae88a2ebf3cc4ec0
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2024-08-07 16:57:45 +0200

    feat(pci-kubernetes): add some tests
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

---
+++ packages/manager/apps/pci-kubernetes/src/pages/list/List.page.spec.tsx
@@ -0,0 +1,50 @@
+import { render } from '@testing-library/react';
+import { vi } from 'vitest';
+import ListPage from '@/pages/list/List.page';
+import * as useKubernetesModule from '@/api/hooks/useKubernetes';
+import { useKubes } from '@/api/hooks/useKubernetes';
+import { wrapper } from '@/wrapperRenders';
+import { TKube } from '@/types';
+
+type TKubesPaginated = {
+  data: { rows: TKube[]; pageCount: number; totalRows: number };
+  isLoading: boolean;
+  isPending: boolean;
+  error: Error;
+};
+
+describe('ListPage', () => {
+  it('renders page correctly', () => {
+    vi.spyOn(useKubernetesModule, 'useKubes').mockReturnValue(({
+      isPending: false,
+      data: {
+        rows: [{ id: 1, name: 'Kube1' }],
+        totalRows: 1,
+      },
+    } as unknown) as TKubesPaginated);
+    const { getByText } = render(<ListPage />, { wrapper });
+    expect(
+      getByText('pci_project_guides_header_loadbalancer_kube'),
+    ).toBeVisible();
+  });
+
+  it('displays loading spinner when data is pending', () => {
+    vi.spyOn(useKubernetesModule, 'useKubes').mockReturnValue(({
+      isPending: true,
+    } as unknown) as TKubesPaginated);
+    const { getByTestId } = render(<ListPage />, { wrapper });
+    expect(getByTestId('List-spinner')).toBeVisible();
+  });
+
+  it('renders datagrid with items when data is available', () => {
+    vi.spyOn(useKubernetesModule, 'useKubes').mockReturnValue(({
+      isPending: false,
+      data: {
+        rows: [{ id: 1, name: 'Kube1' }],
+        totalRows: 1,
+      },
+    } as unknown) as TKubesPaginated);
+    const { getByText } = render(<ListPage />, { wrapper });
+    expect(getByText('Kube1')).toBeInTheDocument();
+  });
+});
