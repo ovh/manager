0a1e456fafd684dc76f391458076ecc3d3f58e6f
Frédéric Vilcot <fredericvilcot@gmail.com>
2024-08-08 10:09:41 +0200

    feat(pci-instance): add vite config files
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

---
+++ packages/manager/apps/pci-instance/vitest.config.js
@@ -0,0 +1,28 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      include: ['src'],
+      exclude: [
+        'src/vite-*.ts',
+        'src/App.tsx',
+        'src/core/ShellRoutingSync.tsx',
+        'src/main.tsx',
+        'src/routes.tsx',
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+    mainFields: ['module'],
+  },
+});
