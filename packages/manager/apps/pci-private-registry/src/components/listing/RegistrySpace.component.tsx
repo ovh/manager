186bb376f00504905566e8ef9adf8fc72e7c89f0
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2024-08-12 17:03:04 +0200

    feat(pci-private-registry): add list page
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

---
+++ packages/manager/apps/pci-private-registry/src/components/listing/RegistrySpace.component.tsx
@@ -0,0 +1,29 @@
+import { useBytes } from '@ovh-ux/manager-pci-common';
+import { DataGridTextCell } from '@ovhcloud/manager-components';
+import { OsdsSkeleton } from '@ovhcloud/ods-components/react';
+import { useTranslation } from 'react-i18next';
+import { useParams } from 'react-router-dom';
+import { useGetRegistryPlan } from '@/api/hooks/useRegistry';
+import { TRegistry } from '@/api/data/registry';
+
+export function RegistrySpace({ registry }: { registry: TRegistry }) {
+  const { t } = useTranslation();
+  const { formatBytes } = useBytes();
+
+  const { projectId } = useParams();
+  const { data, isPending } = useGetRegistryPlan(projectId, registry?.id);
+
+  return (
+    <>
+      {isPending && <OsdsSkeleton />}
+      {!isPending && data && (
+        <DataGridTextCell>
+          {t('private_registry_consumption_display', {
+            used: formatBytes(registry?.size, 2, 1024),
+            limit: formatBytes(data.registryLimits?.imageStorage, 2, 1024),
+          })}
+        </DataGridTextCell>
+      )}
+    </>
+  );
+}
