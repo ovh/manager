5eb083bdc21b0388758760231b0eae42ca5b599a
Yoann Fievez <yoann.fievez@gmail.com>
2024-08-12 10:04:45 +0200

    feat(pci-private-registry): initialize react apps
    ref: DTCORE-2498
    Signed-off-by: Yoann Fievez <yoann.fievez@gmail.com>

---
+++ packages/manager/apps/pci-private-registry/src/pages/Layout.tsx
@@ -0,0 +1,56 @@
+import { Outlet, useParams, useRouteError } from 'react-router-dom';
+
+import { ShellContext } from '@ovh-ux/manager-react-shell-client';
+import { Suspense, useContext } from 'react';
+import { ErrorBanner } from '@ovhcloud/manager-components';
+import { ApiError } from '@ovh-ux/manager-core-api';
+import { useProject } from '@ovh-ux/manager-pci-common';
+import ShellRoutingSync from '@/core/ShellRoutingSync';
+import HidePreloader from '@/core/HidePreloader';
+
+import usePageTracking from '@/hooks/usePageTracking';
+
+export default function Layout() {
+  const { projectId } = useParams();
+  const { isSuccess } = useProject(projectId || '', { retry: false });
+  usePageTracking();
+  return (
+    <div className="application">
+      <Suspense>
+        <ShellRoutingSync />
+        {isSuccess && (
+          <>
+            <HidePreloader />
+            <Outlet />
+          </>
+        )}
+      </Suspense>
+    </div>
+  );
+}
+
+export const ErrorBoundary = () => {
+  const error = useRouteError() as ApiError;
+  const nav = useContext(ShellContext).shell.navigation;
+
+  const redirectionApplication = 'public-cloud';
+
+  const navigateToHomePage = () => {
+    nav.navigateTo(redirectionApplication, '', {});
+  };
+
+  const reloadPage = () => {
+    nav.reload();
+  };
+  return (
+    <Suspense>
+      <ErrorBanner
+        onReloadPage={reloadPage}
+        onRedirectHome={navigateToHomePage}
+        error={error.response}
+      />
+      <ShellRoutingSync />
+      <HidePreloader />
+    </Suspense>
+  );
+};
