147ec95329174d17d77dbb64cc9eb833a69997ac
Yoann Fievez <yoann.fievez@gmail.com>
2024-08-12 10:04:45 +0200

    feat(pci-private-registry): initialize react apps
    ref: DTCORE-2498
    Signed-off-by: Yoann Fievez <yoann.fievez@gmail.com>

---
+++ packages/manager/apps/pci-private-registry/src/routes.tsx
@@ -0,0 +1,35 @@
+const lazyRouteConfig = (importFn: CallableFunction) => ({
+  lazy: async () => {
+    const { default: moduleDefault, ...moduleExports } = await importFn();
+
+    return {
+      Component: moduleDefault,
+      ...moduleExports,
+    };
+  },
+});
+
+export interface RouteHandle {
+  tracking?: string;
+}
+
+const ROUTE_PATHS = {
+  root: '/pci/projects/:projectId/private-registry',
+};
+
+export default [
+  {
+    path: '/',
+    ...lazyRouteConfig(() => import('@/pages/Layout')),
+  },
+  {
+    id: '',
+    path: ROUTE_PATHS.root,
+    ...lazyRouteConfig(() => import('@/pages/Layout')),
+    children: [],
+  },
+  {
+    path: '*',
+    element: <>Not found page</>,
+  },
+];
