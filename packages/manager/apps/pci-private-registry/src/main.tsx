93e5f5914bc5f54ed4cede7a2557e2ed075e8b7b
Yoann Fievez <yoann.fievez@gmail.com>
2024-08-12 10:04:45 +0200

    feat(pci-private-registry): initialize react apps
    ref: DTCORE-2498
    Signed-off-by: Yoann Fievez <yoann.fievez@gmail.com>

---
+++ packages/manager/apps/pci-private-registry/src/main.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  initI18n,
+  initShellContext,
+  ShellContext,
+} from '@ovh-ux/manager-react-shell-client';
+import App from './App';
+
+import './index.css';
+
+import '@/vite-hmr.ts';
+
+const init = async (
+  appName: string,
+  { reloadOnLocaleChange } = { reloadOnLocaleChange: false },
+) => {
+  const context = await initShellContext(appName);
+
+  const region = context.environment.getRegion();
+  try {
+    await import(`./config-${region}.js`);
+  } catch (error) {
+    // nothing to do
+  }
+
+  await initI18n({
+    context,
+    reloadOnLocaleChange,
+    ns: ['common'],
+    defaultNS: 'common',
+  });
+
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <ShellContext.Provider value={context}>
+        <App />
+      </ShellContext.Provider>
+    </React.StrictMode>,
+  );
+};
+
+init('pci-private-registry');
