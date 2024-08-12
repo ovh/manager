3e31bf1d7d05727e8926486486340b986fe6e2c3
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2024-08-12 17:03:04 +0200

    feat(pci-private-registry): add list page
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/modules/manager-pci-common/src/hooks/bytes/useBytes.ts
+++ packages/manager/modules/manager-pci-common/src/hooks/bytes/useBytes.ts
@@ -1,7 +1,10 @@
 import { useTranslation } from 'react-i18next';
 import './translations';
 
-const UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+const UNITS = {
+  1000: ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+  1024: ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'],
+};
 
 export function useBytes() {
   const { t } = useTranslation('pci-bytes');
@@ -10,9 +13,11 @@ export function useBytes() {
     decimals = 0,
     format: 1000 | 1024 = 1000,
   ) => {
-    if (!+bytes) return `${0} ${t('unit_size_B')}`;
+    if (!+bytes) return 0;
     const i = Math.floor(Math.log(bytes) / Math.log(format));
-    return `${parseFloat((bytes / format ** i).toFixed(decimals))} ${UNITS[i]}`;
+    return `${parseFloat((bytes / format ** i).toFixed(decimals))} ${t(
+      `unit_size_${UNITS[format][i]}`,
+    )}`;
   };
   return { formatBytes };
 }
