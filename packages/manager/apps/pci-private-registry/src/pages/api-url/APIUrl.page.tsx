de21e94f092e4fce6b8e46584e86ac3a655fef9a
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2024-08-13 15:33:25 +0200

    feat(pci-private-registry): add api url modal
    ref: DTCORE-2503
    ref: DTCORE-2504
    ref: DTCORE-2507
    
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

---
+++ packages/manager/apps/pci-private-registry/src/pages/api-url/APIUrl.page.tsx
@@ -0,0 +1,69 @@
+import { Clipboard } from '@ovhcloud/manager-components';
+import {
+  ODS_THEME_COLOR_INTENT,
+  ODS_THEME_TYPOGRAPHY_SIZE,
+} from '@ovhcloud/ods-common-theming';
+import {
+  ODS_BUTTON_VARIANT,
+  ODS_SPINNER_SIZE,
+  ODS_TEXT_COLOR_INTENT,
+  ODS_TEXT_LEVEL,
+} from '@ovhcloud/ods-components';
+import {
+  OsdsButton,
+  OsdsModal,
+  OsdsSpinner,
+  OsdsText,
+} from '@ovhcloud/ods-components/react';
+import { useTranslation } from 'react-i18next';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useGetAllRegistries } from '@/api/hooks/useRegistry';
+
+export default function ApiUrlPage() {
+  const { t } = useTranslation();
+
+  const navigate = useNavigate();
+  const onClose = () => navigate('..');
+
+  const { projectId, registryId } = useParams();
+  const { data: registries, isPending } = useGetAllRegistries(projectId);
+  const registry = registries?.find((r) => r.id === registryId);
+
+  return (
+    <OsdsModal
+      onOdsModalClose={onClose}
+      headline={t('private_registry_harbor_api_title')}
+    >
+      <div className="mt-5">
+        {isPending ? (
+          <OsdsSpinner
+            inline
+            size={ODS_SPINNER_SIZE.md}
+            className="block text-center mt-5"
+          />
+        ) : (
+          <>
+            <OsdsText
+              color={ODS_TEXT_COLOR_INTENT.text}
+              level={ODS_TEXT_LEVEL.body}
+              size={ODS_THEME_TYPOGRAPHY_SIZE._400}
+              className="block mb-2"
+            >
+              {t('private_registry_harbor_api_url')}
+            </OsdsText>
+            <Clipboard aria-label="clipboard" value={registry?.url} />
+          </>
+        )}
+      </div>
+
+      <OsdsButton
+        slot="actions"
+        color={ODS_THEME_COLOR_INTENT.primary}
+        variant={ODS_BUTTON_VARIANT.ghost}
+        onClick={onClose}
+      >
+        {t('private_registry_common_cancel')}
+      </OsdsButton>
+    </OsdsModal>
+  );
+}
