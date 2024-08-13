af73f0de974b95b806da6c9e8c5811eb1170c207
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2024-08-13 18:51:48 +0200

    feat(hpc-vmware-managed-vcd): add editModal display/validation
      desc: WIP, todo: api calls
      ref: MANAGER-14526
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

---
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/components/modal/EditDescriptionModal.tsx
@@ -0,0 +1,101 @@
+import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
+import {
+  ODS_BUTTON_VARIANT,
+  ODS_INPUT_TYPE,
+  ODS_TEXT_LEVEL,
+  ODS_TEXT_SIZE,
+  OdsInputValueChangeEventDetail,
+  OsdsInputCustomEvent,
+} from '@ovhcloud/ods-components';
+import {
+  OsdsButton,
+  OsdsFormField,
+  OsdsInput,
+  OsdsText,
+} from '@ovhcloud/ods-components/react';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import Modal from './Modal';
+
+interface EditModalProps {
+  organizationDescription: string;
+  onCloseModal: () => void;
+  onEdit: (desc: string) => void;
+}
+
+const validateDescription = (desc: string) => /^.{1,256}$/.test(desc);
+
+export const EditDescriptionModal = ({
+  organizationDescription,
+  onCloseModal,
+  onEdit,
+}: EditModalProps) => {
+  const { t } = useTranslation('dashboard');
+  const [newDesc, setNewDesc] = useState<string>(organizationDescription || '');
+  const isValidDesc = validateDescription(newDesc);
+  const isButtonEnabled = isValidDesc && newDesc !== organizationDescription;
+
+  const handleSubmit = () => {
+    if (isValidDesc) {
+      onEdit(newDesc);
+      onCloseModal();
+    }
+  };
+
+  return (
+    <Modal color={ODS_THEME_COLOR_INTENT.info} onClose={onCloseModal}>
+      <OsdsText
+        color={ODS_THEME_COLOR_INTENT.text}
+        level={ODS_TEXT_LEVEL.heading}
+        size={ODS_TEXT_SIZE._400}
+      >
+        {t('managed_vcd_dashboard_edit_desc_modal_title')}
+      </OsdsText>
+
+      <OsdsFormField>
+        <OsdsText color={ODS_THEME_COLOR_INTENT.text} className="mt-6">
+          {t('managed_vcd_dashboard_edit_desc_modal_label')}
+        </OsdsText>
+        <OsdsInput
+          ariaLabel="edit-input"
+          type={ODS_INPUT_TYPE.text}
+          value={newDesc}
+          onOdsValueChange={(
+            e: OsdsInputCustomEvent<OdsInputValueChangeEventDetail>,
+          ) => setNewDesc(e.target.value as string)}
+          color={
+            isValidDesc
+              ? ODS_THEME_COLOR_INTENT.info
+              : ODS_THEME_COLOR_INTENT.error
+          }
+        />
+        <OsdsText
+          slot="helper"
+          color={ODS_THEME_COLOR_INTENT.error}
+          className={`${isValidDesc ? 'invisible' : 'visible'}`}
+        >
+          {t('managed_vcd_dashboard_edit_desc_modal_helper_error')}
+        </OsdsText>
+      </OsdsFormField>
+
+      <OsdsButton
+        slot="actions"
+        color={ODS_THEME_COLOR_INTENT.primary}
+        variant={ODS_BUTTON_VARIANT.stroked}
+        onClick={onCloseModal}
+      >
+        {t('managed_vcd_dashboard_edit_modal_cta_cancel')}
+      </OsdsButton>
+      <OsdsButton
+        disabled={!isButtonEnabled || undefined}
+        slot="actions"
+        color={ODS_THEME_COLOR_INTENT.primary}
+        variant={ODS_BUTTON_VARIANT.flat}
+        aria-label="edit-desc-vcd"
+        onClick={handleSubmit}
+      >
+        {t('managed_vcd_dashboard_edit_modal_cta_edit')}
+      </OsdsButton>
+    </Modal>
+  );
+};
