f763938d29202e562af7f33269c37e0db5952e68
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2024-08-14 16:09:14 +0200

    fix(kms): fix key creation form size select
    ref: MANAGER-15008
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

--- packages/manager/apps/key-management-service/src/data/hooks/useOkmsReferenceServiceKey.ts
+++ packages/manager/apps/key-management-service/src/data/hooks/useOkmsReferenceServiceKey.ts
@@ -3,9 +3,11 @@ import {
   getOkmsServiceKeyReferenceQueryKey,
   getServiceKeyReference,
 } from '../api/okmsReference';
+import OkmsServiceKeyReference from '@/types/okmsServiceKeyReference.type';
+import { ErrorResponse } from '@/types/api.type';
 
 export const useOkmsServiceKeyReference = (okmsRegion: string) => {
-  return useQuery({
+  return useQuery<{ data: OkmsServiceKeyReference[] }, ErrorResponse>({
     queryKey: getOkmsServiceKeyReferenceQueryKey(okmsRegion),
     queryFn: () => getServiceKeyReference(okmsRegion),
     enabled: false,
