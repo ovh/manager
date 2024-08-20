0570573255a25c354dd7a1db30f43abdfdce45c6
Florian Renaut <florian.renaut@corp.ovh.com>
2024-08-20 15:40:12 +0200

    feat(manager-pci-common): init module (#12730)
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

---
+++ packages/manager/modules/manager-pci-common/sample.spec.ts
@@ -0,0 +1,9 @@
+/**
+ * Sample unit test to make "test" command pass.
+ * Further unit tests will be added in a separate PR.
+ */
+describe('manager-pci-common', () => {
+  it('run unit tests', async () => {
+    expect(true).toBe(true);
+  });
+});
