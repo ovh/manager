7c3d9ac90c8b5ebab106f333a9cfcb45c5f56b67
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2024-08-21 16:57:11 +0200

    feat(hpc-vmware-managed-vcd): add api queries (update)
      ref: MANAGER-14526
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/hpc-vmware-managed-vcd/src/pages/dashboard/organization/general-information/edit/EditDescription.page.tsx
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/pages/dashboard/organization/general-information/edit/EditDescription.page.tsx
@@ -2,16 +2,22 @@ import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import useManagedVcdOrganization from '@/data/hooks/useManagedVcdOrganization';
 import { EditDescriptionModal } from '@/components/modal/EditDescriptionModal';
+import { useUpdateVcdOrganizationDetails } from '@/data/hooks/useUpdateVcdOrganization';
+import { IVcdOrganizationState } from '@/types/vcd-organization.interface';
 
 export default function EditDescription() {
   const navigate = useNavigate();
   const { id } = useParams();
   const { data: vcdOrganization } = useManagedVcdOrganization(id);
+  const { mutate: updateDetails } = useUpdateVcdOrganizationDetails({ id });
+  const currentDetails: IVcdOrganizationState = vcdOrganization.data.targetSpec;
 
   return (
     <EditDescriptionModal
       organizationDescription={vcdOrganization?.data?.currentState?.description}
-      onEdit={() => {}}
+      onEdit={(desc: string) =>
+        updateDetails({ id, details: { ...currentDetails, description: desc } })
+      }
       onCloseModal={() => navigate('..')}
     />
   );
--- packages/manager/apps/hpc-vmware-managed-vcd/src/pages/dashboard/organization/general-information/edit/EditName.page.tsx
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/pages/dashboard/organization/general-information/edit/EditName.page.tsx
@@ -2,16 +2,22 @@ import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { EditNameModal } from '@/components/modal/EditNameModal';
 import useManagedVcdOrganization from '@/data/hooks/useManagedVcdOrganization';
+import { useUpdateVcdOrganizationDetails } from '@/data/hooks/useUpdateVcdOrganization';
+import { IVcdOrganizationState } from '@/types/vcd-organization.interface';
 
 export default function EditName() {
   const navigate = useNavigate();
   const { id } = useParams();
   const { data: vcdOrganization } = useManagedVcdOrganization(id);
+  const { mutate: updateDetails } = useUpdateVcdOrganizationDetails({ id });
+  const currentDetails: IVcdOrganizationState = vcdOrganization.data.targetSpec;
 
   return (
     <EditNameModal
       organizationName={vcdOrganization?.data?.currentState?.fullName}
-      onEdit={() => {}}
+      onEdit={(name: string) =>
+        updateDetails({ id, details: { ...currentDetails, fullName: name } })
+      }
       onCloseModal={() => navigate('..')}
     />
   );
