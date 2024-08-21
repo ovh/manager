ce3f21187a88519a35ab58bed418a93b551eaa87
JacquesLarique <134954692+JacquesLarique@users.noreply.github.com>
2024-08-21 10:42:24 +0200

    feat(hub): create react µapp (#12776)
    ref: MANAGER-15036
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

---
+++ packages/manager/apps/hub/src/routes/routes.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { RouteObject } from 'react-router-dom';
+import { PageType } from '@ovh-ux/manager-react-shell-client';
+import NotFound from '@/pages/404';
+
+const lazyRouteConfig = (importFn: CallableFunction): Partial<RouteObject> => {
+  return {
+    lazy: async () => {
+      const { default: moduleDefault, ...moduleExports } = await importFn();
+      return {
+        Component: moduleDefault,
+        ...moduleExports,
+      };
+    },
+  };
+};
+
+export const Routes: any = [
+  {
+    path: '/',
+    ...lazyRouteConfig(() => import('@/pages/layout')),
+    handle: {
+      tracking: {
+        pageName: 'dashboard',
+      },
+    },
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+];
