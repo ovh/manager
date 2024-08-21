d27342c02e42247db28dd1e8ef2377afc3f48ebf
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2024-08-21 16:57:11 +0200

    feat(hpc-vmware-managed-vcd): add api queries (update)
      ref: MANAGER-14526
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/hpc-vmware-managed-vcd/src/data/api/hpc-vmware-managed-vcd.ts
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/data/api/hpc-vmware-managed-vcd.ts
@@ -4,12 +4,24 @@ import {
   apiClient,
   fetchIcebergV2,
 } from '@ovh-ux/manager-core-api';
-import IVcdOrganization from '@/types/vcd-organization.interface';
+import IVcdOrganization, {
+  IVcdOrganizationState,
+} from '@/types/vcd-organization.interface';
 
 export type GetVcdOrganizationListParams = {
   /** Filter resources on IAM tags */
   iamTags: any;
 };
+
+export type UpdateVcdOrganizationDetailsParams = {
+  id: string;
+  details: IVcdOrganizationState;
+};
+
+export const updateVcdOrganizationDetailsQueryKey = (id: string) => [
+  `put/organization/details`,
+  id,
+];
 
 export const getVcdProjectListQueryKey = [
   'get/vmwareCloudDirector/organization',
@@ -30,6 +42,19 @@ export const getVcdOrganization = async (
   id: string,
 ): Promise<ApiResponse<IVcdOrganization>> =>
   apiClient.v2.get(`/vmwareCloudDirector/organization/${id}`);
+
+/**
+ * Edit VCD Organization
+ */
+export const updateVcdOrganizationDetails = async ({
+  id,
+  details,
+}: UpdateVcdOrganizationDetailsParams): Promise<ApiResponse<
+  IVcdOrganization
+>> =>
+  apiClient.v2.put(`/vmwareCloudDirector/organization/${id}`, {
+    targetSpec: details,
+  });
 
 /**
  *  Get listing with iceberg V2
