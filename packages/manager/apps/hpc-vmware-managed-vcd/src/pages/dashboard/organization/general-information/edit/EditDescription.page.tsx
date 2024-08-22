124a5d4d14ddb105ed6d64070f99ea27917d4f52
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2024-08-22 15:33:28 +0200

    feat(hpc-vmware-managed-vcd): add PR feedbacks
      desc: modal refacto, error display, routes constants
      ref: MANAGER-14526
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/hpc-vmware-managed-vcd/src/pages/dashboard/organization/general-information/edit/EditDescription.page.tsx
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/pages/dashboard/organization/general-information/edit/EditDescription.page.tsx
@@ -1,24 +1,6 @@
 import React from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import useManagedVcdOrganization from '@/data/hooks/useManagedVcdOrganization';
-import { EditDescriptionModal } from '@/components/modal/EditDescriptionModal';
-import { useUpdateVcdOrganizationDetails } from '@/data/hooks/useUpdateVcdOrganization';
-import { IVcdOrganizationState } from '@/types/vcd-organization.interface';
+import { UpdateDetailModalHandler } from '@/components/modal/UpdateDetailModalHandler';
 
 export default function EditDescription() {
-  const navigate = useNavigate();
-  const { id } = useParams();
-  const { data: vcdOrganization } = useManagedVcdOrganization(id);
-  const { mutate: updateDetails } = useUpdateVcdOrganizationDetails({ id });
-  const currentDetails: IVcdOrganizationState = vcdOrganization.data.targetSpec;
-
-  return (
-    <EditDescriptionModal
-      organizationDescription={vcdOrganization?.data?.currentState?.description}
-      onEdit={(desc: string) =>
-        updateDetails({ id, details: { ...currentDetails, description: desc } })
-      }
-      onCloseModal={() => navigate('..')}
-    />
-  );
+  return <UpdateDetailModalHandler detailName="description" />;
 }
