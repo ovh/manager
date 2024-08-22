efd1b07e7ddea32dcc10735813ebd013ea91261c
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2024-08-22 15:33:28 +0200

    feat(hpc-vmware-managed-vcd): add PR feedbacks
      desc: modal refacto, error display, routes constants
      ref: MANAGER-14526
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/hpc-vmware-managed-vcd/src/pages/dashboard/organization/general-information/edit/EditName.page.tsx
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/pages/dashboard/organization/general-information/edit/EditName.page.tsx
@@ -1,24 +1,6 @@
 import React from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import { EditNameModal } from '@/components/modal/EditNameModal';
-import useManagedVcdOrganization from '@/data/hooks/useManagedVcdOrganization';
-import { useUpdateVcdOrganizationDetails } from '@/data/hooks/useUpdateVcdOrganization';
-import { IVcdOrganizationState } from '@/types/vcd-organization.interface';
+import { UpdateDetailModalHandler } from '@/components/modal/UpdateDetailModalHandler';
 
 export default function EditName() {
-  const navigate = useNavigate();
-  const { id } = useParams();
-  const { data: vcdOrganization } = useManagedVcdOrganization(id);
-  const { mutate: updateDetails } = useUpdateVcdOrganizationDetails({ id });
-  const currentDetails: IVcdOrganizationState = vcdOrganization.data.targetSpec;
-
-  return (
-    <EditNameModal
-      organizationName={vcdOrganization?.data?.currentState?.fullName}
-      onEdit={(name: string) =>
-        updateDetails({ id, details: { ...currentDetails, fullName: name } })
-      }
-      onCloseModal={() => navigate('..')}
-    />
-  );
+  return <UpdateDetailModalHandler detailName="name" />;
 }
