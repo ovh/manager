ba689bdd23d04e0f52564ef9cb30d6427b639df4
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2024-08-22 15:19:49 +0200

    feat(manager-react-components): build and publish library
    ref: MANAGER-14822
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-react-components/src/index.ts
+++ packages/manager-react-components/src/index.ts
@@ -2,5 +2,8 @@ import './tailwind/theme.css';
 import '@ovhcloud/ods-theme-blue-jeans/dist/index.css';
 import './enumTypes';
 
-export * from './hooks/datagrid';
+export * from './hooks';
 export * from './components';
+export * from './enumTypes';
+export * from './utils';
+export * from './utils/click-utils';
