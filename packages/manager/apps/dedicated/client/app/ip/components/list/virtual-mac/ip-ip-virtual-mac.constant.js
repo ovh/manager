ff1f653649216cf6ab6527cc5ab43dde701b6938
soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>
2024-08-22 07:43:08 +0200

    feat(dedicated): update add message error for vMac if 403 response
      ref: MANAGER-14771
    
    Signed-off-by: soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>

---
+++ packages/manager/apps/dedicated/client/app/ip/components/list/virtual-mac/ip-ip-virtual-mac.constant.js
@@ -0,0 +1,77 @@
+export const PROXMOX_LINKS = {
+  DEFAULT:
+    'https://help.ovhcloud.com/csm/en-ie-dedicated-servers-proxmox-network-hg-scale?id=kb_article_view&sysparm_article=KB0043909',
+  FR:
+    'https://help.ovhcloud.com/csm/fr-dedicated-servers-proxmox-network-hg-scale?id=kb_article_view&sysparm_article=KB0043915',
+  DE:
+    'https://help.ovhcloud.com/csm/de-dedicated-servers-proxmox-network-hg-scale?id=kb_article_view&sysparm_article=KB0043914',
+  IE:
+    'https://help.ovhcloud.com/csm/en-ie-dedicated-servers-proxmox-network-hg-scale?id=kb_article_view&sysparm_article=KB0043909',
+  PL:
+    'https://help.ovhcloud.com/csm/pl-dedicated-servers-proxmox-network-hg-scale?id=kb_article_view&sysparm_article=KB0043916',
+  ES:
+    'https://help.ovhcloud.com/csm/es-es-dedicated-servers-proxmox-network-hg-scale?id=kb_article_view&sysparm_article=KB0043912',
+  IT:
+    'https://help.ovhcloud.com/csm/it-dedicated-servers-proxmox-network-hg-scale?id=kb_article_view&sysparm_article=KB0043917',
+  PT:
+    'https://help.ovhcloud.com/csm/pt-dedicated-servers-proxmox-network-hg-scale?id=kb_article_view&sysparm_article=KB0043918',
+  NL:
+    'https://help.ovhcloud.com/csm/en-gb-dedicated-servers-proxmox-network-hg-scale?id=kb_article_view&sysparm_article=KB0043899',
+  GB:
+    'https://help.ovhcloud.com/csm/en-gb-dedicated-servers-proxmox-network-hg-scale?id=kb_article_view&sysparm_article=KB0043899',
+  TN:
+    'https://help.ovhcloud.com/csm/fr-dedicated-servers-proxmox-network-hg-scale?id=kb_article_view&sysparm_article=KB0043915',
+  MA:
+    'https://help.ovhcloud.com/csm/fr-dedicated-servers-proxmox-network-hg-scale?id=kb_article_view&sysparm_article=KB0043915',
+  SN:
+    'https://help.ovhcloud.com/csm/fr-dedicated-servers-proxmox-network-hg-scale?id=kb_article_view&sysparm_article=KB0043915',
+  QC:
+    'https://help.ovhcloud.com/csm/fr-ca-dedicated-servers-proxmox-network-hg-scale?id=kb_article_view&sysparm_article=KB0043925',
+  CA:
+    'https://help.ovhcloud.com/csm/en-ca-dedicated-servers-proxmox-network-hg-scale?id=kb_article_view&sysparm_article=KB0043904',
+  US:
+    'https://help.ovhcloud.com/csm/en-ie-dedicated-servers-proxmox-network-hg-scale?id=kb_article_view&sysparm_article=KB0043909',
+};
+
+export const WINDOWS_SERVER_LINKS = {
+  FR:
+    'https://help.ovhcloud.com/csm/fr-dedicated-servers-hyperv-network-hg-scale?id=kb_article_view&sysparm_article=KB0043590',
+  DE:
+    'https://help.ovhcloud.com/csm/de-dedicated-servers-hyperv-network-hg-scale?id=kb_article_view&sysparm_article=KB0043579',
+  IE:
+    'https://help.ovhcloud.com/csm/en-ie-dedicated-servers-hyperv-network-hg-scale?id=kb_article_view&sysparm_article=KB0043583',
+  PL:
+    'https://help.ovhcloud.com/csm/pl-dedicated-servers-hyperv-network-hg-scale?id=kb_article_view&sysparm_article=KB0043604',
+  ES:
+    'https://help.ovhcloud.com/csm/es-es-dedicated-servers-hyperv-network-hg-scale?id=kb_article_view&sysparm_article=KB0043584',
+  IT:
+    'https://help.ovhcloud.com/csm/it-dedicated-servers-hyperv-network-hg-scale?id=kb_article_view&sysparm_article=KB0043589',
+  PT:
+    'https://help.ovhcloud.com/csm/pt-dedicated-servers-hyperv-network-hg-scale?id=kb_article_view&sysparm_article=KB0043586',
+  NL:
+    'https://help.ovhcloud.com/csm/en-gb-dedicated-servers-hyperv-network-hg-scale?id=kb_article_view&sysparm_article=KB0043582',
+  GB:
+    'https://help.ovhcloud.com/csm/en-gb-dedicated-servers-hyperv-network-hg-scale?id=kb_article_view&sysparm_article=KB0043582',
+  MA:
+    'https://help.ovhcloud.com/csm/fr-dedicated-servers-hyperv-network-hg-scale?id=kb_article_view&sysparm_article=KB0043590',
+  SN:
+    'https://help.ovhcloud.com/csm/fr-dedicated-servers-hyperv-network-hg-scale?id=kb_article_view&sysparm_article=KB0043590',
+  TN:
+    'https://help.ovhcloud.com/csm/fr-dedicated-servers-hyperv-network-hg-scale?id=kb_article_view&sysparm_article=KB0043590',
+  CA:
+    'https://help.ovhcloud.com/csm/en-ca-dedicated-servers-hyperv-network-hg-scale?id=kb_article_view&sysparm_article=KB0043581',
+  QC:
+    'https://help.ovhcloud.com/csm/fr-ca-dedicated-servers-hyperv-network-hg-scale?id=kb_article_view&sysparm_article=KB0043588',
+  US:
+    'https://help.ovhcloud.com/csm/en-ie-dedicated-servers-hyperv-network-hg-scale?id=kb_article_view&sysparm_article=KB0043583',
+  DEFAULT:
+    'https://help.ovhcloud.com/csm/en-ie-dedicated-servers-hyperv-network-hg-scale?id=kb_article_view&sysparm_article=KB0043583',
+};
+
+export const vmacFeatureAvailablity = 'ip:create-vmac';
+
+export default {
+  PROXMOX_LINKS,
+  WINDOWS_SERVER_LINKS,
+  vmacFeatureAvailablity,
+};
