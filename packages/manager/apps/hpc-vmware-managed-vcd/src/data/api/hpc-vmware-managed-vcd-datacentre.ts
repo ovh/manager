2ce4ffaa4c71f00754bdfa48de1ad00b9f082b4c
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2024-08-25 22:26:02 +0200

    feat(hpc-vmware-managed-vcd): add datacenter quick actions
      desc: update vdc description modal
      ref: MANAGER-14524
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/hpc-vmware-managed-vcd/src/data/api/hpc-vmware-managed-vcd-datacentre.ts
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/data/api/hpc-vmware-managed-vcd-datacentre.ts
@@ -1,5 +1,13 @@
 import { ApiResponse, apiClient } from '@ovh-ux/manager-core-api';
-import IVcdDatacentre from '@/types/vcd-datacenter.interface';
+import IVcdDatacentre, {
+  IVcdDatacentreState,
+} from '@/types/vcd-datacenter.interface';
+
+export type UpdateVdcDetailsParams = {
+  id: string;
+  vdcId: string;
+  details: IVcdDatacentreState;
+};
 
 export const getVcdDatacentre = async (
   id: string,
@@ -13,3 +21,13 @@ export const getVcdDatacentres = async (
   id: string,
 ): Promise<ApiResponse<IVcdDatacentre[]>> =>
   apiClient.v2.get(`/vmwareCloudDirector/organization/${id}/virtualDataCenter`);
+
+export const updateVdcDetails = async ({
+  id,
+  vdcId,
+  details,
+}: UpdateVdcDetailsParams) =>
+  apiClient.v2.put(
+    `/vmwareCloudDirector/organization/${id}/virtualDataCenter/${vdcId}`,
+    { targetSpec: details },
+  );
