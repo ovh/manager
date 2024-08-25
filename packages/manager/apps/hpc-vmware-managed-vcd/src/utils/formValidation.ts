52502f0b12b2325f3cdd9ef9811077afdd23402a
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2024-08-25 22:26:02 +0200

    feat(hpc-vmware-managed-vcd): add datacenter quick actions
      desc: update vdc description modal
      ref: MANAGER-14524
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/hpc-vmware-managed-vcd/src/utils/formValidation.ts
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/utils/formValidation.ts
@@ -1,5 +1,4 @@
 export const validateOrganizationName = (name: string) =>
   /^.{1,128}$/.test(name);
 
-export const validateOrganizationDescription = (desc: string) =>
-  /^.{1,256}$/.test(desc);
+export const validateDescription = (desc: string) => /^.{1,256}$/.test(desc);
