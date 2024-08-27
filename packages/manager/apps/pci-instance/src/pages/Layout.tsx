2625f05a2f41bef25fb7422109c8d7c6d559bf9b
Frédéric Vilcot <fredericvilcot@gmail.com>
2024-08-27 10:20:39 +0200

    feat(pci-instance): follow architecture guidelines
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

--- packages/manager/apps/pci-instance/src/pages/Layout.tsx
+++ packages/manager/apps/pci-instance/src/pages/Layout.tsx
@@ -3,11 +3,11 @@ import { useRouteError, Outlet } from 'react-router-dom';
 import { ErrorBanner } from '@ovhcloud/manager-components';
 import { ShellContext } from '@ovh-ux/manager-react-shell-client';
 import { mapUnknownErrorToBannerError } from '@/utils';
-import HidePreloader from '@/core/HidePreloader';
-import ShellRoutingSync from '@/core/ShellRoutingSync';
+import HidePreloader from '@/components/hidePreloader/HidePreloader';
+import ShellRoutingSync from '@/components/shell/ShellRoutingSync';
 
 const Layout: FC<string> = () => (
-  <div className="application">
+  <div className="mx-11 mt-8">
     <Suspense>
       <ShellRoutingSync />
       <Outlet />
