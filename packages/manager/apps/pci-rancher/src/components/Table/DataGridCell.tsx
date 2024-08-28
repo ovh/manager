31780dc8f1ca23580ce09ed0f8e9641cbded9f26
Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>
2024-08-28 10:00:28 +0200

    feat(pci.rancher): add rancher id
    ref: TAPC-1024
    Signed-off-by: Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>

---
+++ packages/manager/apps/pci-rancher/src/components/Table/DataGridCell.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Clipboard, DataGridTextCell } from '@ovhcloud/manager-components';
+import { Cell } from '@tanstack/react-table';
+import { RancherService } from '../../api/api.type';
+import './Table.scss';
+
+interface DisplayCellInterface {
+  cell: Cell<RancherService, unknown>;
+}
+
+const ClipboardInput = ({ cell }: Readonly<DisplayCellInterface>) => {
+  const id = cell.renderValue() as string;
+
+  return (
+    <DataGridTextCell>
+      <Clipboard aria-label="clipboard" value={id} />
+    </DataGridTextCell>
+  );
+};
+
+export default ClipboardInput;
