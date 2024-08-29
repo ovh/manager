f31e207b678f3b1d6f7de1bdb27c57d7ce84aaa8
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2024-08-29 22:01:53 +0200

    fix(pci-private-network): use OsdsText for banner message content ref: TAPC-1244
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

--- packages/manager/apps/pci-private-network/src/pages/new/steps/ConfigurationStep.tsx
+++ packages/manager/apps/pci-private-network/src/pages/new/steps/ConfigurationStep.tsx
@@ -393,9 +393,11 @@ export default function ConfigurationStep({
 
                 {store.form.configureVlanId && store.form.vlanId === 0 && (
                   <OsdsMessage className="mb-4" type={ODS_MESSAGE_TYPE.warning}>
-                    {t(
-                      'pci_projects_project_network_private_create_vlan_id_warning',
-                    )}
+                    <OsdsText color={ODS_THEME_COLOR_INTENT.text}>
+                      {t(
+                        'pci_projects_project_network_private_create_vlan_id_warning',
+                      )}
+                    </OsdsText>
                   </OsdsMessage>
                 )}
 
@@ -477,9 +479,11 @@ export default function ConfigurationStep({
                       className="mt-4"
                       type={ODS_MESSAGE_TYPE.warning}
                     >
-                      {t(
-                        'pci_projects_project_network_private_create_configure_vlan_taken',
-                      )}
+                      <OsdsText color={ODS_THEME_COLOR_INTENT.text}>
+                        {t(
+                          'pci_projects_project_network_private_create_configure_vlan_taken',
+                        )}
+                      </OsdsText>
                     </OsdsMessage>
                   )}
               </div>
