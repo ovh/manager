2b234dfe09e8067bc40381bb82991943e72453a0
Sachin Ramesh <104337442+sachinrameshn@users.noreply.github.com>
2024-08-30 10:55:52 +0530

    feat(manager-components): add <Region /> component (#12810)
    ref: MANAGER-15076
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

---
+++ packages/manager-components/src/components/region/region.stories.tsx
@@ -0,0 +1,26 @@
+import { Story } from '@storybook/react';
+import Region from './region.component';
+
+const managerRegion = {
+  title: 'Components/Region',
+  component: Region,
+};
+
+const Template: Story<any> = (args) => <Region {...args} />;
+
+export const DemoRegion = Template.bind({});
+
+DemoRegion.args = {
+  mode: 'region',
+  name: 'ca-east-bhs',
+};
+
+export const DemoDatacenter = Template.bind({});
+
+DemoDatacenter.args = {
+  mode: 'datacenter',
+  name: 'RBX',
+  micro: 2,
+};
+
+export default managerRegion;
