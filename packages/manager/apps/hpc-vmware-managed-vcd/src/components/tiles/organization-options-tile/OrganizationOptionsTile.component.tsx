995b4395180bc062cb34f5e9a46a7b2d10491c0f
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2024-09-02 09:26:51 +0200

    feat(hpc-vmware-managed-vcd): use DashboardTile in VCD
      ref: MANAGER-14872
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/hpc-vmware-managed-vcd/src/components/tiles/organization-options-tile/OrganizationOptionsTile.component.tsx
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/components/tiles/organization-options-tile/OrganizationOptionsTile.component.tsx
@@ -1,19 +1,49 @@
 import React from 'react';
-import { CommonTitle } from '@ovhcloud/manager-components';
-import { OsdsDivider, OsdsTile } from '@ovhcloud/ods-components/react';
+import {
+  ActionMenu,
+  DashboardTile,
+  Description,
+} from '@ovhcloud/manager-components';
 import { useTranslation } from 'react-i18next';
-import OptionsTileLicense from './OptionsTileLicense';
+import { ODS_ICON_NAME } from '@ovhcloud/ods-components';
 
-export default function OrganizationOptionsTile() {
+export default function OrganizationOptionsTile({
+  isLicenseActive,
+}: {
+  isLicenseActive: boolean;
+}) {
   const { t } = useTranslation('dashboard');
 
-  return (
-    <OsdsTile>
-      <div className="flex flex-col w-full">
-        <CommonTitle>{t('managed_vcd_dashboard_options')}</CommonTitle>
-        <OsdsDivider separator />
-        <OptionsTileLicense />
+  const LicenseItem = () =>
+    isLicenseActive ? (
+      <Description>
+        {t('managed_vcd_dashboard_windows_license_active')}
+      </Description>
+    ) : (
+      <div className="flex justify-between items-center">
+        <Description>
+          {t('managed_vcd_dashboard_windows_license_unactive')}
+        </Description>
+        <ActionMenu
+          items={[]}
+          isCompact
+          icon={ODS_ICON_NAME.ELLIPSIS_VERTICAL}
+        />
       </div>
-    </OsdsTile>
+    );
+
+  return (
+    <div className="h-fit">
+      <DashboardTile
+        title={t('managed_vcd_dashboard_options')}
+        items={[
+          {
+            id: 'license',
+            label: t('managed_vcd_dashboard_windows_license'),
+            value: <LicenseItem />,
+          },
+        ]}
+      />
+    </div>
   );
 }
