673acce6ff6bc61f9cf4e6156e9602bd011e0cef
Paul <58977722+pauldkn@users.noreply.github.com>
2024-09-02 14:45:21 +0200

    feat(hpc-vmware-managed-vcd): add options tile license (#12890)
    * feat(hpc-wmare-managed-vcd): add license to optionsTile
    
      ref: MANAGER-14872
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/hpc-vmware-managed-vcd/src/components/tiles/datacentre-general-information-tile/DatacentreGeneralInformationTile.component.tsx
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/components/tiles/datacentre-general-information-tile/DatacentreGeneralInformationTile.component.tsx
@@ -1,22 +1,17 @@
 import {
-  CommonTitle,
   Description,
   LinkType,
   Links,
   Clipboard,
+  DashboardTile,
 } from '@ovhcloud/manager-components';
-import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
-import { ODS_ICON_NAME, ODS_ICON_SIZE } from '@ovhcloud/ods-components';
-import {
-  OsdsDivider,
-  OsdsIcon,
-  OsdsTile,
-} from '@ovhcloud/ods-components/react';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
+import { OsdsIcon } from '@ovhcloud/ods-components/react';
+import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
+import { ODS_ICON_NAME, ODS_ICON_SIZE } from '@ovhcloud/ods-components';
 import IVcdDatacentre from '@/types/vcd-datacenter.interface';
-import TileSubtitle from '@/components/tiles/tile-subtitle/TileSubtitle.component';
 import IVcdOrganization from '@/types/vcd-organization.interface';
 import { subRoutes } from '@/routes/routes.constant';
 
@@ -34,79 +29,101 @@ export default function DatacentreGenerationInformationTile({
   const navigate = useNavigate();
 
   return (
-    <OsdsTile className="w-full h-full flex-col" inline rounded>
-      <div className="flex flex-col w-full">
-        <CommonTitle>
-          {t('managed_vcd_dashboard_general_information')}
-        </CommonTitle>
-        <OsdsDivider separator />
-        <div className="flex flex-col mb-3">
-          <TileSubtitle>{t('managed_vcd_dashboard_description')}</TileSubtitle>
-          <div className="flex flex-row justify-between items-center">
+    <DashboardTile
+      title={t('managed_vcd_dashboard_general_information')}
+      items={[
+        {
+          id: 'description',
+          label: t('managed_vcd_dashboard_description'),
+          value: (
+            <div className="flex justify-between items-center">
+              <Description>
+                {vcdDatacentre?.currentState?.description}
+              </Description>
+              <OsdsIcon
+                aria-label="edit"
+                className="mx-6 cursor-pointer"
+                name={ODS_ICON_NAME.PEN}
+                size={ODS_ICON_SIZE.xxs}
+                color={ODS_THEME_COLOR_INTENT.primary}
+                onClick={() => navigate(subRoutes.editDescription)}
+              />
+            </div>
+          ),
+        },
+        {
+          id: 'commercialRange',
+          label: tVdc('managed_vcd_vdc_commercial_range'),
+          value: (
             <Description>
-              {vcdDatacentre?.currentState?.description}
+              {vcdDatacentre?.currentState?.commercialRange}
             </Description>
-            <OsdsIcon
-              aria-label="edit"
-              className="mx-6 cursor-pointer"
-              onClick={() => navigate(subRoutes.editDescription)}
-              name={ODS_ICON_NAME.PEN}
-              size={ODS_ICON_SIZE.xxs}
-              color={ODS_THEME_COLOR_INTENT.primary}
+          ),
+        },
+        {
+          id: 'cpuCount',
+          label: tVdc('managed_vcd_vdc_cpu_count'),
+          value: (
+            <Description>
+              {vcdDatacentre?.currentState?.vCPUCount?.toString()}
+            </Description>
+          ),
+        },
+        {
+          id: 'ramCount',
+          label: tVdc('managed_vcd_vdc_ram_count'),
+          value: (
+            <Description>
+              {tVdc('managed_vcd_vdc_quota_value', {
+                quota: vcdDatacentre?.currentState?.memoryQuota,
+              })}
+            </Description>
+          ),
+        },
+        {
+          id: 'diskSpace',
+          label: tVdc('managed_vcd_vdc_disk_space_count'),
+          value: (
+            <Description>
+              {tVdc('managed_vcd_vdc_quota_value', {
+                quota: vcdDatacentre?.currentState?.storageQuota,
+              })}
+            </Description>
+          ),
+        },
+        {
+          id: 'vcpuSpeed',
+          label: tVdc('managed_vcd_vdc_vcpu_speed'),
+          value: (
+            <Description>
+              {tVdc('managed_vcd_vdc_vcpu_value', {
+                speed: vcdDatacentre?.currentState?.vCPUSpeed,
+              })}
+            </Description>
+          ),
+        },
+        {
+          id: 'interface',
+          label: t('managed_vcd_dashboard_management_interface'),
+          value: (
+            <Links
+              type={LinkType.external}
+              href={vcdOrganization?.currentState?.webInterfaceUrl}
+              label={t('managed_vcd_dashboard_management_interface_access')}
             />
-          </div>
-          <OsdsDivider separator />
-          <TileSubtitle>
-            {tVdc('managed_vcd_vdc_commercial_range')}
-          </TileSubtitle>
-          <Description>
-            {vcdDatacentre?.currentState?.commercialRange}
-          </Description>
-          <OsdsDivider separator />
-          <TileSubtitle>{tVdc('managed_vcd_vdc_cpu_count')}</TileSubtitle>
-          <Description>
-            {vcdDatacentre?.currentState?.vCPUCount?.toString()}
-          </Description>
-          <OsdsDivider separator />
-          <TileSubtitle>{tVdc('managed_vcd_vdc_ram_count')}</TileSubtitle>
-          <Description>
-            {tVdc('managed_vcd_vdc_quota_value', {
-              quota: vcdDatacentre?.currentState?.memoryQuota,
-            })}
-          </Description>
-          <OsdsDivider separator />
-          <TileSubtitle>
-            {tVdc('managed_vcd_vdc_disk_space_count')}
-          </TileSubtitle>
-          <Description>
-            {tVdc('managed_vcd_vdc_quota_value', {
-              quota: vcdDatacentre?.currentState?.storageQuota,
-            })}
-          </Description>
-          <OsdsDivider separator />
-          <TileSubtitle>{tVdc('managed_vcd_vdc_vcpu_speed')}</TileSubtitle>
-          <Description>
-            {tVdc('managed_vcd_vdc_vcpu_value', {
-              speed: vcdDatacentre?.currentState?.vCPUSpeed,
-            })}
-          </Description>
-          <OsdsDivider separator />
-          <TileSubtitle>
-            {t('managed_vcd_dashboard_management_interface')}
-          </TileSubtitle>
-          <Links
-            type={LinkType.external}
-            href={vcdOrganization?.currentState?.webInterfaceUrl}
-            label={t('managed_vcd_dashboard_management_interface_access')}
-          />
-          <OsdsDivider separator />
-          <TileSubtitle>{t('managed_vcd_dashboard_api_url')}</TileSubtitle>
-          <Clipboard value={vcdOrganization?.currentState?.apiUrl} />
-          <OsdsDivider separator />
-          <TileSubtitle>{tVdc('managed_vcd_vdc_id')}</TileSubtitle>
-          <Clipboard value={vcdDatacentre?.id} />
-        </div>
-      </div>
-    </OsdsTile>
+          ),
+        },
+        {
+          id: 'apiUrl',
+          label: t('managed_vcd_dashboard_api_url'),
+          value: <Clipboard value={vcdOrganization?.currentState?.apiUrl} />,
+        },
+        {
+          id: 'vdcId',
+          label: tVdc('managed_vcd_vdc_id'),
+          value: <Clipboard value={vcdDatacentre?.id} />,
+        },
+      ]}
+    />
   );
 }
