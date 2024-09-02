187fc340bd40511d1f2124a0f3c671c6f6175443
Paul <58977722+pauldkn@users.noreply.github.com>
2024-09-02 14:45:21 +0200

    feat(hpc-vmware-managed-vcd): add options tile license (#12890)
    * feat(hpc-wmare-managed-vcd): add license to optionsTile
    
      ref: MANAGER-14872
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/hpc-vmware-managed-vcd/src/components/tiles/organization-general-information-tile/OrganizationGeneralInformationTile.component.tsx
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/components/tiles/organization-general-information-tile/OrganizationGeneralInformationTile.component.tsx
@@ -1,9 +1,9 @@
 import {
-  CommonTitle,
   Description,
   LinkType,
   Links,
   Clipboard,
+  DashboardTile,
 } from '@ovhcloud/manager-components';
 import {
   ODS_THEME_COLOR_HUE,
@@ -11,18 +11,12 @@ import {
   ODS_THEME_TYPOGRAPHY_LEVEL,
   ODS_THEME_TYPOGRAPHY_SIZE,
 } from '@ovhcloud/ods-common-theming';
-import { ODS_ICON_NAME, ODS_ICON_SIZE } from '@ovhcloud/ods-components';
-import {
-  OsdsDivider,
-  OsdsIcon,
-  OsdsText,
-  OsdsTile,
-} from '@ovhcloud/ods-components/react';
+import { OsdsIcon, OsdsText } from '@ovhcloud/ods-components/react';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
+import { ODS_ICON_NAME, ODS_ICON_SIZE } from '@ovhcloud/ods-components';
 import IVcdOrganization from '@/types/vcd-organization.interface';
-import TileSubtitle from '@/components/tiles/tile-subtitle/TileSubtitle.component';
 import RegionLabel from '@/components/region-label/RegionLabel.component';
 import { subRoutes } from '@/routes/routes.constant';
 
@@ -39,72 +33,86 @@ export default function OrganizationGenerationInformationTile({
   const navigate = useNavigate();
 
   return (
-    <OsdsTile className="w-full h-full flex-col" inline rounded>
-      <div className="flex flex-col w-full">
-        <CommonTitle>
-          {t('managed_vcd_dashboard_general_information')}
-        </CommonTitle>
-        <OsdsDivider separator />
-        <TileSubtitle>{t('managed_vcd_dashboard_name')}</TileSubtitle>
-        <div className="flex flex-row justify-between items-center">
-          <Description>{vcdOrganization?.currentState?.fullName}</Description>
-          <OsdsIcon
-            aria-label="edit"
-            className="mx-6 cursor-pointer"
-            name={ODS_ICON_NAME.PEN}
-            size={ODS_ICON_SIZE.xxs}
-            color={ODS_THEME_COLOR_INTENT.primary}
-            onClick={() => navigate(subRoutes.editName)}
-          />
-        </div>
-        <OsdsDivider separator />
-        <div className="flex flex-col mb-3">
-          <TileSubtitle>{t('managed_vcd_dashboard_description')}</TileSubtitle>
-          <div className="flex flex-row justify-between items-center">
-            <Description>
-              {vcdOrganization?.currentState?.description}
-            </Description>
-            <OsdsIcon
-              aria-label="edit"
-              className="mx-6 cursor-pointer"
-              name={ODS_ICON_NAME.PEN}
-              size={ODS_ICON_SIZE.xxs}
-              color={ODS_THEME_COLOR_INTENT.primary}
-              onClick={() => navigate(subRoutes.editDescription)}
+    <DashboardTile
+      title={t('managed_vcd_dashboard_general_information')}
+      items={[
+        {
+          id: 'name',
+          label: t('managed_vcd_dashboard_name'),
+          value: (
+            <div className="flex justify-between items-center">
+              <Description>
+                {vcdOrganization?.currentState?.fullName}
+              </Description>
+              <OsdsIcon
+                aria-label="edit"
+                className="mx-6 cursor-pointer"
+                name={ODS_ICON_NAME.PEN}
+                size={ODS_ICON_SIZE.xxs}
+                color={ODS_THEME_COLOR_INTENT.primary}
+                onClick={() => navigate(subRoutes.editName)}
+              />
+            </div>
+          ),
+        },
+        {
+          id: 'description',
+          label: t('managed_vcd_dashboard_description'),
+          value: (
+            <div className="flex justify-between items-center">
+              <Description>
+                {vcdOrganization?.currentState?.description}
+              </Description>
+              <OsdsIcon
+                aria-label="edit"
+                className="mx-6 cursor-pointer"
+                name={ODS_ICON_NAME.PEN}
+                size={ODS_ICON_SIZE.xxs}
+                color={ODS_THEME_COLOR_INTENT.primary}
+                onClick={() => navigate(subRoutes.editDescription)}
+              />
+            </div>
+          ),
+        },
+        {
+          id: 'location',
+          label: t('managed_vcd_dashboard_localisation'),
+          value: (
+            <OsdsText
+              className="mb-4"
+              level={ODS_THEME_TYPOGRAPHY_LEVEL.body}
+              size={ODS_THEME_TYPOGRAPHY_SIZE._400}
+              color={ODS_THEME_COLOR_INTENT.text}
+              hue={ODS_THEME_COLOR_HUE._500}
+            >
+              <RegionLabel
+                code={vcdOrganization?.currentState?.region}
+              ></RegionLabel>
+            </OsdsText>
+          ),
+        },
+        {
+          id: 'datacentresCount',
+          label: t('managed_vcd_dashboard_datacentres_count'),
+          value: <Description>{datacenterCount.toString()}</Description>,
+        },
+        {
+          id: 'interface',
+          label: t('managed_vcd_dashboard_management_interface'),
+          value: (
+            <Links
+              type={LinkType.external}
+              href={vcdOrganization?.currentState?.webInterfaceUrl}
+              label={t('managed_vcd_dashboard_management_interface_access')}
             />
-          </div>
-          <OsdsDivider separator />
-          <TileSubtitle>{t('managed_vcd_dashboard_localisation')}</TileSubtitle>
-          <OsdsText
-            className="mb-4"
-            level={ODS_THEME_TYPOGRAPHY_LEVEL.body}
-            size={ODS_THEME_TYPOGRAPHY_SIZE._400}
-            color={ODS_THEME_COLOR_INTENT.text}
-            hue={ODS_THEME_COLOR_HUE._500}
-          >
-            <RegionLabel
-              code={vcdOrganization?.currentState?.region}
-            ></RegionLabel>
-          </OsdsText>
-          <OsdsDivider separator />
-          <TileSubtitle>
-            {t('managed_vcd_dashboard_datacentres_count')}
-          </TileSubtitle>
-          <Description>{datacenterCount.toString()}</Description>
-          <OsdsDivider separator />
-          <TileSubtitle>
-            {t('managed_vcd_dashboard_management_interface')}
-          </TileSubtitle>
-          <Links
-            type={LinkType.external}
-            href={vcdOrganization?.currentState?.webInterfaceUrl}
-            label={t('managed_vcd_dashboard_management_interface_access')}
-          />
-          <OsdsDivider separator />
-          <TileSubtitle>{t('managed_vcd_dashboard_api_url')}</TileSubtitle>
-          <Clipboard value={vcdOrganization.currentState?.apiUrl} />
-        </div>
-      </div>
-    </OsdsTile>
+          ),
+        },
+        {
+          id: 'apiUrl',
+          label: t('managed_vcd_dashboard_api_url'),
+          value: <Clipboard value={vcdOrganization.currentState?.apiUrl} />,
+        },
+      ]}
+    />
   );
 }
