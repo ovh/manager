936105be39957ede18d14e98e76ea9c979c8f511
Paul <58977722+pauldkn@users.noreply.github.com>
2024-09-02 14:45:21 +0200

    feat(hpc-vmware-managed-vcd): add options tile license (#12890)
    * feat(hpc-wmare-managed-vcd): add license to optionsTile
    
      ref: MANAGER-14872
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/hpc-vmware-managed-vcd/src/components/tiles/billing-tile/BillingTile.component.tsx
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/components/tiles/billing-tile/BillingTile.component.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { CommonTitle, Description, Links } from '@ovhcloud/manager-components';
 import {
-  OsdsChip,
-  OsdsDivider,
-  OsdsLink,
-  OsdsTile,
-} from '@ovhcloud/ods-components/react';
+  DashboardTile,
+  Description,
+  Links,
+} from '@ovhcloud/manager-components';
+import { OsdsChip, OsdsLink } from '@ovhcloud/ods-components/react';
 import { useTranslation } from 'react-i18next';
 import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
 import { ODS_CHIP_SIZE } from '@ovhcloud/ods-components';
-import TileSubtitle from '../tile-subtitle/TileSubtitle.component';
 import useManagedVcdService from '@/data/hooks/useManagedVcdService';
 import useCurrentUser from '@/hooks/user/useCurrentUser';
 
@@ -26,40 +24,48 @@ export default function BillingTile({ id }: TBillingTileProps) {
   const nextBillingDate = billingService?.data?.billing?.nextBillingDate;
 
   return (
-    <OsdsTile>
-      <div className="flex flex-col w-full">
-        <CommonTitle>
-          {t('managed_vcd_dashboard_service_management')}
-        </CommonTitle>
-        <OsdsDivider separator />
-        <TileSubtitle>{t('managed_vcd_dashboard_mailing_list')}</TileSubtitle>
-        <Description>{user?.email}</Description>
-        <OsdsDivider separator />
-        <TileSubtitle>{t('managed_vcd_dashboard_service_renew')}</TileSubtitle>
-        {nextBillingDate ? (
-          <Links
-            onClickReturn={() => {}}
-            label={dateTimeFormat?.format(new Date(nextBillingDate))}
-          />
-        ) : (
-          <span>-</span>
-        )}
-        <OsdsDivider separator />
-        <TileSubtitle>{t('managed_vcd_dashboard_password')}</TileSubtitle>
-        <div className="flex-wrap">
-          <OsdsLink disabled>
-            {t('managed_vcd_dashboard_password_renew')}
-          </OsdsLink>
-          <OsdsChip
-            inline
-            color={ODS_THEME_COLOR_INTENT.primary}
-            className="ml-3"
-            size={ODS_CHIP_SIZE.sm}
-          >
-            {t('managed_vcd_dashboard_coming_soon')}
-          </OsdsChip>
-        </div>
-      </div>
-    </OsdsTile>
+    <div className="h-fit">
+      <DashboardTile
+        title={t('managed_vcd_dashboard_service_management')}
+        items={[
+          {
+            id: 'mailingList',
+            label: t('managed_vcd_dashboard_mailing_list'),
+            value: <Description>{user?.email}</Description>,
+          },
+          {
+            id: 'serviceRenew',
+            label: t('managed_vcd_dashboard_service_renew'),
+            value: nextBillingDate ? (
+              <Links
+                onClickReturn={() => {}}
+                label={dateTimeFormat?.format(new Date(nextBillingDate))}
+              />
+            ) : (
+              <span>-</span>
+            ),
+          },
+          {
+            id: 'password',
+            label: t('managed_vcd_dashboard_password'),
+            value: (
+              <div className="flex-wrap">
+                <OsdsLink disabled>
+                  {t('managed_vcd_dashboard_password_renew')}
+                </OsdsLink>
+                <OsdsChip
+                  inline
+                  color={ODS_THEME_COLOR_INTENT.primary}
+                  className="ml-3"
+                  size={ODS_CHIP_SIZE.sm}
+                >
+                  {t('managed_vcd_dashboard_coming_soon')}
+                </OsdsChip>
+              </div>
+            ),
+          },
+        ]}
+      />
+    </div>
   );
 }
