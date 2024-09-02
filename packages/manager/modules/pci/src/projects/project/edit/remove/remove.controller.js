9f87aef25b4996427482e1eaa060e6d249cef85b
Lio B <lionel95200x@users.noreply.github.com>
2024-09-02 11:23:41 +0200

    feat: check savings plan before delete pci project (#12874)
    ref: feat/TAPC-1281
    
    Signed-off-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>
    Co-authored-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/edit/remove/remove.controller.js
+++ packages/manager/modules/pci/src/projects/project/edit/remove/remove.controller.js
@@ -1,11 +1,22 @@
+import { SUPPORT_URL } from '../../../new/constants';
 import { MESSAGES_CONTAINER_NAME } from '../edit.constant';
 
 export default class {
   /* @ngInject */
-  constructor($translate, CucCloudMessage) {
+  constructor(
+    $translate,
+    CucCloudMessage,
+    OvhApiCloudProject,
+    $http,
+    coreConfig,
+  ) {
     this.$translate = $translate;
     this.CucCloudMessage = CucCloudMessage;
+    this.OvhApiCloudProject = OvhApiCloudProject;
     this.isLoading = false;
+    this.canDeleteProject = true;
+    this.$http = $http;
+    this.coreConfig = coreConfig;
   }
 
   cancel() {
@@ -15,14 +26,63 @@ export default class {
     this.goBack();
   }
 
+  $onInit() {
+    this.isLoading = true;
+    return this.$http({
+      url: '/services',
+      params: {
+        resourceName: this.projectId,
+      },
+    }).then(({ data }) => {
+      if (data.length) {
+        this.checkSavingsPlan(data[0]);
+      } else {
+        this.isLoading = false;
+      }
+    });
+  }
+
+  checkSavingsPlan(serviceId) {
+    this.$http({
+      url: `/services/${serviceId}/savingsPlans/subscribed`,
+    }).then(({ data }) => {
+      this.isLoading = false;
+      if (data.length) {
+        const activePlan = data.some((p) => p.status === 'ACTIVE');
+        if (activePlan) {
+          this.canDeleteProject = false;
+        }
+      }
+    });
+  }
+
   getTranslationKey() {
+    if (!this.canDeleteProject) {
+      return 'pci_projects_project_discovery_edit_savings_plan';
+    }
     return `pci_projects_project_${
       this.isDiscoveryProject ? 'discovery_' : ''
     }edit_remove_please_note`;
   }
 
+  getPrimaryButtonLabel() {
+    if (!this.canDeleteProject) {
+      return 'pci_projects_project_edit_remove_submit_client_service';
+    }
+    return 'pci_projects_project_edit_remove_submit';
+  }
+
   remove() {
     this.isLoading = true;
+    if (!this.canDeleteProject) {
+      window.open(
+        SUPPORT_URL + this.coreConfig.getUser().ovhSubsidiary,
+        '_blank',
+        'noopener',
+      );
+      return this.goBack();
+    }
+
     this.trackClick(
       'PublicCloud::pci::projects::project::edit::remove::confirm',
     );
