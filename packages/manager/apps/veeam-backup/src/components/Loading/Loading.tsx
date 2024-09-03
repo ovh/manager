772983307c44c67f1ba1c744d98c5690d3ece1bb
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2024-09-03 17:10:10 +0200

    feat(veeam-backup): order veeam step 2
    ref: MANAGER-14896
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager/apps/veeam-backup/src/components/Loading/Loading.tsx
+++ packages/manager/apps/veeam-backup/src/components/Loading/Loading.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import { OsdsSpinner } from '@ovhcloud/ods-components/react';
+import {
+  OsdsChip,
+  OsdsSpinner,
+  OsdsSkeleton,
+} from '@ovhcloud/ods-components/react';
+import { ODS_SKELETON_SIZE, ODS_SPINNER_SIZE } from '@ovhcloud/ods-components';
+import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
 
-export default function Loading() {
-  return (
-    <div className="flex justify-center">
-      <div>
-        <OsdsSpinner />
-      </div>
-    </div>
-  );
-}
+export const Loading = (props: React.HTMLAttributes<HTMLDivElement>) => (
+  <div {...props} className={`flex justify-center ${props.className}`}>
+    <OsdsSpinner inline size={ODS_SPINNER_SIZE.md} />
+  </div>
+);
+
+export const LoadingChip = ({ className }: { className?: string }) => (
+  <OsdsChip className={className} inline color={ODS_THEME_COLOR_INTENT.default}>
+    <OsdsSkeleton inline size={ODS_SKELETON_SIZE.xs} />
+  </OsdsChip>
+);
