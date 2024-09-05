93b0799fa9c62b4fd69c80bcafd4a5b5e7280351
Hyenne Guillaume <guillaume.hyenne@ovhcloud.com>
2024-09-05 17:03:46 +0200

    feat(zimbra): project zimbra beta (#11715)
    ref:MANAGER-13738
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

---
+++ packages/manager/apps/zimbra/src/api/organization/type.ts
@@ -0,0 +1,33 @@
+import { AccountStatistics } from '../api.type';
+
+export type OrganizationType = {
+  id: string;
+  resourceStatus: string;
+  checksum: string;
+  targetSpec: {
+    name: string;
+    description: string;
+    label: string;
+  };
+  currentState: {
+    name: string;
+    description: string;
+    label: string;
+    storageConsumed: number;
+    updatedAt: string;
+    createdAt: string;
+    accountsStatistics: AccountStatistics[];
+  };
+  currentTasks: Array<{
+    id: string;
+    link: string;
+    status: string;
+    type: string;
+  }>;
+};
+
+export type OrganizationBodyParamsType = {
+  description?: string;
+  label?: string;
+  name?: string;
+};
---
+++ packages/manager/apps/zimbra/src/utils/test.provider.tsx
@@ -0,0 +1,59 @@
+import { render, RenderOptions, RenderResult } from '@testing-library/react';
+import i18n from 'i18next';
+import React, { ComponentType } from 'react';
+import { I18nextProvider, initReactI18next } from 'react-i18next';
+import { MemoryRouter } from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
+import dashboardTranslation from '@/public/translations/dashboard/Messages_fr_FR.json';
+import organizationsTranslation from '@/public/translations/organizations/Messages_fr_FR.json';
+import organizationsAddAndEditTranslation from '@/public/translations/organizations/addAndEdit/Messages_fr_FR.json';
+import organizationsDeleteTranslation from '@/public/translations/organizations/delete/Messages_fr_FR.json';
+import domainsTranslation from '@/public/translations/domains/Messages_fr_FR.json';
+import domainsAddDomainTranslation from '@/public/translations/domains/addDomain/Messages_fr_FR.json';
+import domainsDeleteTranslation from '@/public/translations/domains/delete/Messages_fr_FR.json';
+import accountTranslation from '@/public/translations/accounts/Messages_fr_FR.json';
+import accountAddAndEditTranslation from '@/public/translations/accounts/addAndEdit/Messages_fr_FR.json';
+import accountDeleteTranslation from '@/public/translations/accounts/delete/Messages_fr_FR.json';
+import queryClient from '@/queryClient';
+import '@testing-library/jest-dom';
+import 'element-internals-polyfill';
+
+i18n.use(initReactI18next).init({
+  lng: 'fr',
+  fallbackLng: 'fr',
+  resources: {
+    fr: {
+      dashboard: dashboardTranslation,
+      organizations: organizationsTranslation,
+      'organizations/addAndEdit': organizationsAddAndEditTranslation,
+      'organizations/delete': organizationsDeleteTranslation,
+      domains: domainsTranslation,
+      'domains/addDomain': domainsAddDomainTranslation,
+      'domains/delete': domainsDeleteTranslation,
+      accounts: accountTranslation,
+      'accounts/addAndEdit': accountAddAndEditTranslation,
+      'accounts/delete': accountDeleteTranslation,
+    },
+  },
+  ns: ['dashboard'],
+});
+
+const Wrappers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MemoryRouter>
+        <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
+      </MemoryRouter>
+    </QueryClientProvider>
+  );
+};
+
+const customRender = (
+  ui: React.JSX.Element,
+  options?: Omit<RenderOptions, 'queries'>,
+): RenderResult =>
+  render(ui, { wrapper: Wrappers as ComponentType, ...options });
+
+export * from '@testing-library/react';
+
+export { customRender as render };
