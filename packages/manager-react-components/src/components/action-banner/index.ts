b52025e28f9ab6bf1758e58c04c4fb2be5326642
Simon Chaumet <simon.chaumet.ext@ovhcloud.com>
2024-09-05 09:45:33 +0200

    fix(pci.block-storage): remove unused localzone banner
    ref: TAPC-1567
    
    Signed-off-by: Simon Chaumet <simon.chaumet.ext@ovhcloud.com>

--- packages/manager-react-components/src/components/action-banner/index.ts
+++ packages/manager-react-components/src/components/action-banner/index.ts
@@ -4,5 +4,4 @@ export {
   isDiscoveryProject,
 } from './pci/pci-discovery-banner.component';
 export { PciAnnouncementBanner } from './pci/pci-announcement-banner.component';
-export { PciFreeLocalzonesBanner } from './pci/pci-free-localzones-banner.component';
 export { PciTrustedZoneBanner } from './pci/pci-trusted-zone-banner.component';
