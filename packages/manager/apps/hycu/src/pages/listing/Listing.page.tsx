97c01d3caf746ba0457be2910b88bd387dd9f75c
darsene <72250222+darsene@users.noreply.github.com>
2024-09-09 13:54:12 +0200

    feat(hycu): init hycu µapp (#12949)
    ref: MANAGER-14729
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

---
+++ packages/manager/apps/hycu/src/pages/listing/Listing.page.tsx
@@ -0,0 +1,119 @@
+import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+import { OsdsLink } from '@ovhcloud/ods-components/react';
+import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
+import {
+  Datagrid,
+  DataGridTextCell,
+  useResourcesIcebergV6,
+  dataType,
+  BaseLayout,
+} from '@ovh-ux/manager-react-components';
+
+import Loading from '@/components/Loading/Loading.component';
+import ErrorBanner from '@/components/Error/Error';
+import Breadcrumb from '@/components/Breadcrumb/Breadcrumb.component';
+
+import appConfig from '@/hycu.config';
+import { urls } from '@/routes/routes.constant';
+
+export default function Listing() {
+  const myConfig = appConfig;
+  const serviceKey = myConfig.listing?.datagrid?.serviceKey;
+  const [columns, setColumns] = useState([]);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { t } = useTranslation('listing');
+  const {
+    flattenData,
+    isError,
+    error,
+    totalCount,
+    hasNextPage,
+    fetchNextPage,
+    isLoading,
+    status,
+    sorting,
+    setSorting,
+    pageIndex,
+  } = useResourcesIcebergV6({
+    route: '/license/hycu',
+    queryKey: ['hycu', '/license/hycu'],
+  });
+
+  const navigateToDashboard = (label: string) => {
+    const path =
+      location.pathname.indexOf('pci') > -1 ? `${location.pathname}/` : '/';
+    navigate(`${path}${label}`);
+  };
+
+  useEffect(() => {
+    const isSuccess = status === 'success';
+    if (columns && isSuccess && flattenData?.length > 0) {
+      const newColumns = Object.keys(flattenData[0])
+        .filter((element) => element !== 'iam')
+        .map((element) => ({
+          id: element,
+          label: element,
+          type: dataType(flattenData[0][element]),
+          cell: (props: any) => {
+            const label = props[element] as string;
+            if (typeof label === 'string' || typeof label === 'number') {
+              if (serviceKey === element)
+                return (
+                  <DataGridTextCell>
+                    <OsdsLink
+                      color={ODS_THEME_COLOR_INTENT.primary}
+                      onClick={() => navigateToDashboard(label)}
+                    >
+                      {label}
+                    </OsdsLink>
+                  </DataGridTextCell>
+                );
+              return <DataGridTextCell>{label}</DataGridTextCell>;
+            }
+            return <div>-</div>;
+          },
+        }));
+      setColumns(newColumns);
+    } else if (isSuccess && !flattenData?.length) {
+      navigate(urls.onboarding);
+    }
+  }, [flattenData]);
+
+  if (isError) {
+    return <ErrorBanner error={error.message} />;
+  }
+
+  if (isLoading && pageIndex === 1) {
+    return (
+      <div>
+        <Loading />
+      </div>
+    );
+  }
+
+  const header = {
+    title: t('title'),
+  };
+
+  return (
+    <BaseLayout breadcrumb={<Breadcrumb />} header={header}>
+      <React.Suspense>
+        {columns && flattenData && (
+          <Datagrid
+            columns={columns}
+            items={flattenData || []}
+            totalItems={totalCount || 0}
+            hasNextPage={hasNextPage && !isLoading}
+            onFetchNextPage={fetchNextPage}
+            sorting={sorting}
+            onSortChange={setSorting}
+          />
+        )}
+      </React.Suspense>
+    </BaseLayout>
+  );
+}
