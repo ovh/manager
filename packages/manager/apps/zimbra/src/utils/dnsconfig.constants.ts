c123d44fd9cbc98bb02aab985777e1901681bb61
Tristan WAGNER <t.wagner66250@gmail.com>
2024-09-10 17:51:41 +0200

    refactor(zimbra): duplicated code on add domain configuration
    Signed-off-by: Tristan WAGNER <t.wagner66250@gmail.com>

---
+++ packages/manager/apps/zimbra/src/utils/dnsconfig.constants.ts
@@ -0,0 +1,11 @@
+export const DNS_CONFIG_TYPE = {
+  STANDARD: 'standard',
+  EXPERT: 'expert',
+};
+
+export const DNS_RECORD_TYPE = {
+  SRV: 'SRV',
+  MX: 'MX',
+  SPF: 'SPF',
+  DKIM: 'DKIM',
+};
