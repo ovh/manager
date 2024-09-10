95a608fb97b7f6586484c0e36dd2ed4d0448bf53
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2024-09-10 22:12:01 +0530

    fix(emailpro.account): update new support link to snow
    ref: INC0071981
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

--- packages/manager/modules/emailpro/src/account/emailpro-constants.js
+++ packages/manager/modules/emailpro/src/account/emailpro-constants.js
@@ -5,10 +5,15 @@ export const STATE_TASK_ERROR = 'TASK_ON_ERROR';
 export const ACCOUNT_TYPES = ['ALL', 'BASIC', 'STANDARD', 'ENTERPRISE'];
 export const FILTER_TYPE = 'ALL';
 
+export const rootSupportUrl = 'https://help.ovhcloud.com/';
+
+export const SUPPORT_URL_VIEW_TICKETS = `${rootSupportUrl}csm?id=csm_cases_requests&ovhSubsidiary=`;
+
 export default {
   SIZE_UNIT,
   STATE_TASK_DOING,
   STATE_TASK_ERROR,
   ACCOUNT_TYPES,
   FILTER_TYPE,
+  SUPPORT_URL_VIEW_TICKETS,
 };
