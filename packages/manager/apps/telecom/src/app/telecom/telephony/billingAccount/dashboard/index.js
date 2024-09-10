5cfba81deea92c230d4539aa584dcbad71611515
Antony MARION <antony.marion.ext@ovhcloud.com>
2024-09-10 16:18:36 +0200

    feat(telecom): improvment on softphone for logo
      ref: MANAGER-13645
    
    Signed-off-by: Antony MARION <antony.marion.ext@ovhcloud.com>

--- packages/manager/apps/telecom/src/app/telecom/telephony/billingAccount/dashboard/index.js
+++ packages/manager/apps/telecom/src/app/telecom/telephony/billingAccount/dashboard/index.js
@@ -7,6 +7,7 @@ import 'ovh-api-services';
 import ovhManagerAdvices from '@ovh-ux/manager-advices';
 
 import pieChartComponent from '../../../../../components/telecom/telephony/group/consumption/pie-chart';
+import componentModule from '../../component/component.module';
 
 const moduleName = 'ovhManagerTelecomTelephonyBillingAccountDashboard';
 
@@ -18,6 +19,7 @@ angular
     'ovh-api-services',
     'ui.router',
     pieChartComponent,
+    componentModule,
     ovhManagerAdvices,
   ])
   .run(/* @ngTranslationsInject:json ./translations */);
