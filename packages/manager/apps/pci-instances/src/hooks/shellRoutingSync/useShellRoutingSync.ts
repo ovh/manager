60ec375fa8775f22370620cee8047a513071995c
Frédéric Vilcot <fredericvilcot@gmail.com>
2024-09-12 18:38:51 +0200

    feat(pci-instance): switch empty components to hooks
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

--- packages/manager/apps/pci-instances/src/hooks/shellRoutingSync/useShellRoutingSync.ts
+++ packages/manager/apps/pci-instances/src/hooks/shellRoutingSync/useShellRoutingSync.ts
@@ -2,7 +2,7 @@ import { ShellContext } from '@ovh-ux/manager-react-shell-client';
 import { useContext, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-export default function ShellRoutingSync() {
+export const useShellRoutingSync = () => {
   const location = useLocation();
   const { routing } = useContext(ShellContext).shell;
 
@@ -13,6 +13,4 @@ export default function ShellRoutingSync() {
   useEffect(() => {
     routing.onHashChange();
   }, [location, routing]);
-
-  return null;
-}
+};
