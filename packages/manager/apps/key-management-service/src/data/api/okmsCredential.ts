c0ddc7019db834193c43884f252466bc3118992c
vovh <155978769+vovh@users.noreply.github.com>
2024-09-13 10:09:38 +0200

    feat(key-management-service): credentials creation step 2 (#13025)
    ref: MANAGER-14178
    
    Signed-off-by: Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>

--- packages/manager/apps/key-management-service/src/data/api/okmsCredential.ts
+++ packages/manager/apps/key-management-service/src/data/api/okmsCredential.ts
@@ -1,5 +1,8 @@
 import apiClient, { ApiResponse } from '@ovh-ux/manager-core-api';
-import { OkmsCredential } from '@/types/okmsCredential.type';
+import {
+  OkmsCredential,
+  OkmsCredentialCreation,
+} from '@/types/okmsCredential.type';
 
 /**
  *  Get okms Credential list
@@ -24,3 +27,23 @@ export const getOkmsCredential = async ({
 }): Promise<ApiResponse<OkmsCredential>> => {
   return apiClient.v2.get(`okms/resource/${okmsId}/credential/${credentialId}`);
 };
+
+/**
+ *  create okms Credential
+ */
+
+export const createOkmsCredential = async ({
+  okmsId,
+  data,
+}: {
+  okmsId: string;
+  data: OkmsCredentialCreation;
+}) => {
+  return apiClient.v2.post(`okms/resource/${okmsId}/credential`, data);
+};
+
+export const createOkmsCredentialQueryKey = ({
+  okmsId,
+}: {
+  okmsId: string;
+}) => [`post/okms/resource/${okmsId}/credential/create`];
