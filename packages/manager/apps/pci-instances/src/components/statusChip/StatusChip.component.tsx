fe19df8bc6efc8f35ac79c8e46372e00a2cb73c7
Frédéric Vilcot <fredericvilcot@gmail.com>
2024-09-13 12:33:14 +0200

    feat(pci-instance): makes sonar happy
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

--- packages/manager/apps/pci-instances/src/components/statusChip/StatusChip.component.tsx
+++ packages/manager/apps/pci-instances/src/components/statusChip/StatusChip.component.tsx
@@ -1,21 +1,21 @@
 import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
 import { OsdsChip } from '@ovhcloud/ods-components/react';
 import { useTranslation } from 'react-i18next';
-import { InstanceStatus } from '@/data/hooks/instances/useInstances';
+import { TInstanceStatus } from '@/data/hooks/instances/useInstances';
 
-const StatusChip = ({ status }: { status: InstanceStatus }) => {
+const colorBySeverityStatus = {
+  success: ODS_THEME_COLOR_INTENT.success,
+  error: ODS_THEME_COLOR_INTENT.error,
+  warning: ODS_THEME_COLOR_INTENT.warning,
+  info: ODS_THEME_COLOR_INTENT.info,
+};
+
+const StatusChip = ({ status }: { status: TInstanceStatus }) => {
   const { t } = useTranslation('status');
-
-  const colorBySeverityStatus = {
-    success: ODS_THEME_COLOR_INTENT.success,
-    error: ODS_THEME_COLOR_INTENT.error,
-    warning: ODS_THEME_COLOR_INTENT.warning,
-    info: ODS_THEME_COLOR_INTENT.info,
-  };
 
   return (
     <OsdsChip inline color={colorBySeverityStatus[status.severity]}>
-      {t(status.state)}
+      {t(status.state.toLowerCase())}
     </OsdsChip>
   );
 };
