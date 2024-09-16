a4138d2aeb6b395443be07858eedfb8cf3a2d472
Yoann Fievez <yoann.fievez@gmail.com>
2024-09-16 14:54:06 +0200

    refactor(pci): remove block storage angularjs section
    ref: DTCORE-1731
    Signed-off-by: Yoann Fievez <yoann.fievez@gmail.com>

--- packages/manager/modules/pci/src/projects/project/storages/blocks/blocks.module.js
+++ packages/manager/modules/pci/src/projects/project/storages/blocks/blocks.module.js
@@ -10,32 +10,13 @@ import 'ovh-api-services';
 import 'angular-ui-bootstrap';
 import '@ovh-ux/ng-ovh-user-pref';
 
-import add from './add';
 import block from './block';
-import help from './help';
-import onboarding from './onboarding';
-
-import blockAttach from './block/attach';
-import blockDetach from './block/detach';
-import blockDelete from './block/delete';
-import blockSnapshot from './block/snapshot';
-
-import component from './blocks.component';
-
-import routing from './blocks.routing';
 
 const moduleName = 'ovhManagerPciStoragesBlocks';
 
 angular
   .module(moduleName, [
-    add,
     block,
-    blockAttach,
-    blockDetach,
-    blockDelete,
-    blockSnapshot,
-    onboarding,
-    help,
     'ngOvhUserPref',
     'ngOvhCloudUniverseComponents',
     'ngTranslateAsyncLoader',
@@ -46,8 +27,6 @@ angular
     'ui.router',
     'ui.bootstrap',
   ])
-  .config(routing)
-  .component('pciProjectStorageBlocks', component)
   .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
