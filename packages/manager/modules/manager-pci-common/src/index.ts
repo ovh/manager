ead087f326c9529035c1585d4841dbaf2dd091bd
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2024-09-16 14:59:49 +0200

    feat(manager-pci-common): refactor modal and rename banners components
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/modules/manager-pci-common/src/index.ts
+++ packages/manager/modules/manager-pci-common/src/index.ts
@@ -3,7 +3,6 @@ export * from './api/hook';
 export * from './components/banner';
 export * from './components/region-selector/RegionSelector.component';
 export * from './components/region-selector/RegionSummary.component';
-export * from './components/deletion-modal';
 export * from './components/modal';
 export * from './components/flavor-selector';
 export * from './components/region-selector/RegionSelector.component';
