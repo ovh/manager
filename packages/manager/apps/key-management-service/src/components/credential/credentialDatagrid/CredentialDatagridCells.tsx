9a64a3d8bd92fccede7e1f1a457859d891a6ff49
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2024-09-16 17:42:33 +0200

    feat(kms): add download credential
    ref: MANAGER-14199
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

--- packages/manager/apps/key-management-service/src/components/credential/credentialDatagrid/CredentialDatagridCells.tsx
+++ packages/manager/apps/key-management-service/src/components/credential/credentialDatagrid/CredentialDatagridCells.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import {
+  ActionMenu,
+  ActionMenuItem,
   DataGridClipboardCell,
   DataGridTextCell,
   Links,
@@ -8,6 +11,7 @@ import {
 import { OkmsCredential } from '@/types/okmsCredential.type';
 import { useFormattedDate } from '@/hooks/useFormattedDate';
 import { CredentialStatus } from '../credentialStatus/CredentialStatus.component';
+import { getDownloadCredentialParameters } from '@/utils/credential/credentialDownload';
 
 export const DatagridCredentialCellName = (credential: OkmsCredential) => {
   const navigate = useNavigate();
@@ -72,4 +76,24 @@ export const DatagridCredentialCellExpirationDate = (
 
 export const DatagridCredentialCellStatus = (credential: OkmsCredential) => {
   return <CredentialStatus state={credential.status} inline />;
+};
+
+export const DatagridCredentialCellActions = (credential: OkmsCredential) => {
+  const { t } = useTranslation('key-management-service/credential');
+
+  const { filename, href, isDisabled } = getDownloadCredentialParameters(
+    credential,
+  );
+
+  const items: ActionMenuItem[] = [
+    {
+      id: 1,
+      label: t('key_management_service_credential_download'),
+      href,
+      download: filename,
+      disabled: isDisabled,
+    },
+  ];
+
+  return <ActionMenu items={items} isCompact />;
 };
