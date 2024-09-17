61943018e74001d736520e94dd4fc93ce119c3cc
Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>
2024-09-17 11:56:41 +0200

    feat(account): init account application
    ref: MANAGER-15021
    
    Signed-off-by: Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>

---
+++ packages/manager/modules/account/src/dashboard/index.js
@@ -0,0 +1,22 @@
+import angular from 'angular';
+import '@uirouter/angularjs';
+import 'oclazyload';
+
+const moduleName = 'ovhManagerAccountDashboardLazyLoading';
+
+angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
+  /* @ngInject */ ($stateProvider) => {
+    $stateProvider.state('app.dashboard.**', {
+      url: '/account/',
+      lazyLoad: ($transition$) => {
+        const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+
+        return import('./module').then((mod) =>
+          $ocLazyLoad.inject(mod.default || mod),
+        );
+      },
+    });
+  },
+);
+
+export default moduleName;
