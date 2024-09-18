07c802d94d2815688b9fca95a5e3e300158c03a8
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2024-09-18 14:47:34 +0200

    fix(pci-private-registry): fix sonar duplication issues and fix registries search issue
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/modules/manager-pci-common/src/components/modal/PciModal.component.spec.tsx
+++ packages/manager/modules/manager-pci-common/src/components/modal/PciModal.component.spec.tsx
@@ -2,143 +2,78 @@ import { fireEvent, render } from '@testing-library/react';
 import { describe, it, vi } from 'vitest';
 import { PciModal } from './PciModal.component';
 
+const defaultProps = {
+  type: 'default',
+  title: 'Test Modal',
+  isPending: false,
+  isDisabled: false,
+  onConfirm: vi.fn(),
+  onClose: vi.fn(),
+  onCancel: vi.fn(),
+};
+
+const renderModal = (props) =>
+  render(
+    <PciModal {...props}>
+      <div data-testid="child-content">Child Content</div>
+    </PciModal>,
+  );
+
 describe('PciModal Tests', () => {
-  it('render loading spinner when isPending is true', () => {
-    const { getByTestId } = render(
-      <PciModal
-        type="default"
-        title="Test Modal"
-        isPending
-        isDisabled={false}
-        cancelText="Cancel"
-        submitText="Submit"
-        onConfirm={vi.fn()}
-        onClose={vi.fn()}
-        onCancel={vi.fn()}
-      >
-        <div>Child Content</div>
-      </PciModal>,
-    );
-
-    expect(getByTestId('pciModal-spinner')).toBeVisible();
-  });
-
-  it('renders modal with warning color', () => {
-    const { getByTestId } = render(
-      <PciModal
-        type="warning"
-        title="Test Modal"
-        isPending
-        isDisabled={false}
-        cancelText="Cancel"
-        submitText="Submit"
-        onConfirm={vi.fn()}
-        onClose={vi.fn()}
-        onCancel={vi.fn()}
-      >
-        <div>Child Content</div>
-      </PciModal>,
-    );
-
-    expect(getByTestId('pciModal-modal')).toHaveAttribute('color', 'warning');
-  });
-
-  it('renders modal with default color', () => {
-    const { getByTestId } = render(
-      <PciModal
-        title="Test Modal"
-        isPending
-        isDisabled={false}
-        cancelText="Cancel"
-        submitText="Submit"
-        onConfirm={vi.fn()}
-        onClose={vi.fn()}
-        onCancel={vi.fn()}
-      >
-        <div>Child Content</div>
-      </PciModal>,
-    );
-
-    expect(getByTestId('pciModal-modal')).toHaveAttribute('color', 'primary');
-  });
-
   it('renders children content when isPending is false', () => {
-    const { getByTestId } = render(
-      <PciModal
-        type="default"
-        title="Test Modal"
-        isPending={false}
-        isDisabled={false}
-        cancelText="Cancel"
-        submitText="Submit"
-        onConfirm={vi.fn()}
-        onClose={vi.fn()}
-        onCancel={vi.fn()}
-      >
-        <div data-testid="child-content">Child Content</div>
-      </PciModal>,
-    );
+    const { getByTestId } = renderModal(defaultProps);
 
     expect(getByTestId('child-content')).toBeVisible();
   });
 
+  it('render loading spinner when isPending is true', () => {
+    const propsWithIsPendingTrue = { ...defaultProps, isPending: true };
+
+    const { getByTestId } = renderModal(propsWithIsPendingTrue);
+
+    expect(getByTestId('pciModal-spinner')).toBeVisible();
+  });
+
+  it('renders modal with warning color when the type equal "warning"', () => {
+    const propsWithTypeWarning = { ...defaultProps, type: 'warning' };
+
+    const { getByTestId } = renderModal(propsWithTypeWarning);
+
+    expect(getByTestId('pciModal-modal')).toHaveAttribute('color', 'warning');
+  });
+
+  it('renders modal with default color when type is undefined', () => {
+    const propsWithNoType = { ...defaultProps, type: undefined };
+
+    const { getByTestId } = renderModal(propsWithNoType);
+
+    expect(getByTestId('pciModal-modal')).toHaveAttribute('color', 'primary');
+  });
+
   it('disables submit button when isDisabled is true', () => {
-    const { getByTestId } = render(
-      <PciModal
-        type="default"
-        title="Test Modal"
-        isPending={false}
-        isDisabled
-        cancelText="Cancel"
-        submitText="Submit"
-        onConfirm={vi.fn()}
-        onClose={vi.fn()}
-        onCancel={vi.fn()}
-      >
-        <div>Child Content</div>
-      </PciModal>,
-    );
+    const propsWithIsDisabledTrue = { ...defaultProps, isDisabled: true };
+
+    const { getByTestId } = renderModal(propsWithIsDisabledTrue);
 
     expect(getByTestId('pciModal-button_submit')).toBeDisabled();
   });
 
   it('enables submit button when isDisabled is false', () => {
-    const { getByTestId } = render(
-      <PciModal
-        type="default"
-        title="Test Modal"
-        isPending={false}
-        isDisabled={false}
-        cancelText="Cancel"
-        submitText="Submit"
-        onConfirm={vi.fn()}
-        onClose={vi.fn()}
-        onCancel={vi.fn()}
-      >
-        <div>Child Content</div>
-      </PciModal>,
-    );
+    const propsWithIsDisabledFalse = { ...defaultProps, isDisabled: false };
+
+    const { getByTestId } = renderModal(propsWithIsDisabledFalse);
 
     expect(getByTestId('pciModal-button_submit')).not.toBeDisabled();
   });
 
   it('calls onConfirm when submit button is clicked', () => {
     const onConfirmMock = vi.fn();
-    const { getByTestId } = render(
-      <PciModal
-        type="default"
-        title="Test Modal"
-        isPending={false}
-        isDisabled={false}
-        cancelText="Cancel"
-        submitText="Submit"
-        onConfirm={onConfirmMock}
-        onClose={vi.fn()}
-        onCancel={vi.fn()}
-      >
-        <div>Child Content</div>
-      </PciModal>,
-    );
+    const propsWithOnConfirmAction = {
+      ...defaultProps,
+      onConfirm: onConfirmMock,
+    };
+
+    const { getByTestId } = renderModal(propsWithOnConfirmAction);
 
     fireEvent.click(getByTestId('pciModal-button_submit'));
 
@@ -147,64 +82,49 @@ describe('PciModal Tests', () => {
 
   it('calls onCancel when cancel button is clicked', () => {
     const onCancelMock = vi.fn();
-    const { getByTestId } = render(
-      <PciModal
-        type="default"
-        title="Test Modal"
-        isPending={false}
-        isDisabled={false}
-        cancelText="Cancel"
-        submitText="Submit"
-        onConfirm={vi.fn()}
-        onClose={vi.fn()}
-        onCancel={onCancelMock}
-      >
-        <div>Child Content</div>
-      </PciModal>,
-    );
+    const propsWithOnCancelAction = {
+      ...defaultProps,
+      onCancel: onCancelMock,
+    };
+
+    const { getByTestId } = renderModal(propsWithOnCancelAction);
 
     fireEvent.click(getByTestId('pciModal-button_cancel'));
 
     expect(onCancelMock).toHaveBeenCalledTimes(1);
   });
 
-  it('renders submit button and cancel button with default text', () => {
-    const { getByText } = render(
-      <PciModal
-        type="default"
-        title="Test Modal"
-        isPending={false}
-        isDisabled={false}
-        onConfirm={vi.fn()}
-        onClose={vi.fn()}
-        onCancel={vi.fn()}
-      >
-        <div>Child Content</div>
-      </PciModal>,
-    );
+  it('renders submit button with default text when submitText is undefined', () => {
+    const { getByText } = renderModal(defaultProps);
 
     expect(getByText('common_confirm')).toBeVisible();
+  });
+
+  it('renders cancel button with default text when cancelText is undefined', () => {
+    const { getByText } = renderModal(defaultProps);
+
     expect(getByText('common_cancel')).toBeVisible();
   });
 
-  it('renders submit button and cancel button with custom text', () => {
-    const { getByText } = render(
-      <PciModal
-        type="default"
-        title="Test Modal"
-        isPending={false}
-        isDisabled={false}
-        onConfirm={vi.fn()}
-        onClose={vi.fn()}
-        onCancel={vi.fn()}
-        submitText="Custom Submit"
-        cancelText="Custom Cancel"
-      >
-        <div>Child Content</div>
-      </PciModal>,
-    );
+  it('renders submit button with custom text', () => {
+    const propsWithSubmitText = {
+      ...defaultProps,
+      submitText: 'Custom Submit',
+    };
+
+    const { getByText } = renderModal(propsWithSubmitText);
 
     expect(getByText('Custom Submit')).toBeVisible();
+  });
+
+  it('renders cancel button with custom text', () => {
+    const propsWithCancelText = {
+      ...defaultProps,
+      cancelText: 'Custom Cancel',
+    };
+
+    const { getByText } = renderModal(propsWithCancelText);
+
     expect(getByText('Custom Cancel')).toBeVisible();
   });
 });
