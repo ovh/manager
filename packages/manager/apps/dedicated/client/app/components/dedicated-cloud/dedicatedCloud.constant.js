b5c089a66332fef03c3aebf1d59584701c0114fc
Thibault Barske (Tibs) <thibault.barske.ext@corp.ovh.com>
2024-09-20 04:39:14 +0200

    feat(dedicated): add vcd terms link for subs (#13014)
    ref: MANAGER-15192
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/components/dedicated-cloud/dedicatedCloud.constant.js
+++ packages/manager/apps/dedicated/client/app/components/dedicated-cloud/dedicatedCloud.constant.js
@@ -30,9 +30,142 @@ export const VCD_GUIDE_LINKS = {
   FR: {
     OFFER: 'https://www.ovhcloud.com/fr/lp/vmware-vcd-evolution/',
     TERMS:
-      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/c0f4445-OVHcloud_managed_vcd-FR-1.0.pdf',
+      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/dfab4c3-OVHcloud_managed_vcd-FR-1.2.pdf',
     MIGRATION:
       'https://help.ovhcloud.com/csm/fr-vmware-vcd-migration-use-cases?id=kb_article_view&sysparm_article=KB0064095',
+  },
+  ASIA: {
+    OFFER: 'https://www.ovhcloud.com/en/lp/vmware-vcd-evolution/',
+    TERMS:
+      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/e563255-OVHcloud_managed_vcd-ASIA-1.0.pdf',
+    MIGRATION:
+      'https://help.ovhcloud.com/csm/en-vmware-vcd-migration-use-cases?id=kb_article_view&sysparm_article=KB0064081',
+  },
+  AU: {
+    OFFER: 'https://www.ovhcloud.com/en/lp/vmware-vcd-evolution/',
+    TERMS:
+      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/5b641fa-OVHcloud_managed_vcd-AU-1.0.pdf',
+    MIGRATION:
+      'https://help.ovhcloud.com/csm/en-vmware-vcd-migration-use-cases?id=kb_article_view&sysparm_article=KB0064081',
+  },
+  CA: {
+    OFFER: 'https://www.ovhcloud.com/en/lp/vmware-vcd-evolution/',
+    TERMS:
+      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/5520360-OVHcloud_managed_vcd-CA-1.0.pdf',
+    MIGRATION:
+      'https://help.ovhcloud.com/csm/en-vmware-vcd-migration-use-cases?id=kb_article_view&sysparm_article=KB0064081',
+  },
+  DE: {
+    OFFER: 'https://www.ovhcloud.com/en/lp/vmware-vcd-evolution/',
+    TERMS:
+      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/b273d98-OVHcloud_managed_vcd-DE-1.0.pdf',
+    MIGRATION:
+      'https://help.ovhcloud.com/csm/en-vmware-vcd-migration-use-cases?id=kb_article_view&sysparm_article=KB0064081',
+  },
+  ES: {
+    OFFER: 'https://www.ovhcloud.com/en/lp/vmware-vcd-evolution/',
+    TERMS:
+      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/f63ccf1-OVHcloud_managed_vcd-ES-1.0.pdf',
+    MIGRATION:
+      'https://help.ovhcloud.com/csm/en-vmware-vcd-migration-use-cases?id=kb_article_view&sysparm_article=KB0064081',
+  },
+  GB: {
+    OFFER: 'https://www.ovhcloud.com/en/lp/vmware-vcd-evolution/',
+    TERMS:
+      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/ce13f7a-OVHcloud_managed_vcd-GB-1.0.pdf',
+    MIGRATION:
+      'https://help.ovhcloud.com/csm/en-vmware-vcd-migration-use-cases?id=kb_article_view&sysparm_article=KB0064081',
+  },
+  IE: {
+    OFFER: 'https://www.ovhcloud.com/en/lp/vmware-vcd-evolution/',
+    TERMS:
+      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/46f4857-OVHcloud_managed_vcd-IE-1.0.pdf',
+    MIGRATION:
+      'https://help.ovhcloud.com/csm/en-vmware-vcd-migration-use-cases?id=kb_article_view&sysparm_article=KB0064081',
+  },
+  IN: {
+    OFFER: 'https://www.ovhcloud.com/en/lp/vmware-vcd-evolution/',
+    TERMS:
+      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/c5910fa-OVHcloud_managed_vcd-IN-1.0.pdf',
+    MIGRATION:
+      'https://help.ovhcloud.com/csm/en-vmware-vcd-migration-use-cases?id=kb_article_view&sysparm_article=KB0064081',
+  },
+  IT: {
+    OFFER: 'https://www.ovhcloud.com/en/lp/vmware-vcd-evolution/',
+    TERMS:
+      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/1229d6d-OVHcloud_managed_vcd-IT-1.0.pdf',
+    MIGRATION:
+      'https://help.ovhcloud.com/csm/en-vmware-vcd-migration-use-cases?id=kb_article_view&sysparm_article=KB0064081',
+  },
+  MA: {
+    OFFER: 'https://www.ovhcloud.com/fr/lp/vmware-vcd-evolution/',
+    TERMS:
+      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/8ef31b8-OVHcloud_managed_vcd-MA-1.0.pdf',
+    MIGRATION:
+      'https://help.ovhcloud.com/csm/fr-vmware-vcd-migration-use-cases?id=kb_article_view&sysparm_article=KB0064095',
+  },
+  NL: {
+    OFFER: 'https://www.ovhcloud.com/en/lp/vmware-vcd-evolution/',
+    TERMS:
+      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/11bb509-OVHcloud_managed_vcd-NL-1.0.pdf',
+    MIGRATION:
+      'https://help.ovhcloud.com/csm/en-vmware-vcd-migration-use-cases?id=kb_article_view&sysparm_article=KB0064081',
+  },
+  PL: {
+    OFFER: 'https://www.ovhcloud.com/en/lp/vmware-vcd-evolution/',
+    TERMS:
+      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/6ed51a5-OVHcloud_managed_vcd-PL-1.1.pdf',
+    MIGRATION:
+      'https://help.ovhcloud.com/csm/en-vmware-vcd-migration-use-cases?id=kb_article_view&sysparm_article=KB0064081',
+  },
+  PT: {
+    OFFER: 'https://www.ovhcloud.com/en/lp/vmware-vcd-evolution/',
+    TERMS:
+      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/5e94fe3-OVHcloud_managed_vcd-PT-1.0.pdf',
+    MIGRATION:
+      'https://help.ovhcloud.com/csm/en-vmware-vcd-migration-use-cases?id=kb_article_view&sysparm_article=KB0064081',
+  },
+  QC: {
+    OFFER: 'https://www.ovhcloud.com/fr/lp/vmware-vcd-evolution/',
+    TERMS:
+      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/57504c4-OVHcloud_managed_vcd-QC-1.0.pdf',
+    MIGRATION:
+      'https://help.ovhcloud.com/csm/fr-vmware-vcd-migration-use-cases?id=kb_article_view&sysparm_article=KB0064095',
+  },
+  SG: {
+    OFFER: 'https://www.ovhcloud.com/en/lp/vmware-vcd-evolution/',
+    TERMS:
+      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/814866d-OVHcloud_managed_vcd-SG-1.0.pdf',
+    MIGRATION:
+      'https://help.ovhcloud.com/csm/en-vmware-vcd-migration-use-cases?id=kb_article_view&sysparm_article=KB0064081',
+  },
+  SN: {
+    OFFER: 'https://www.ovhcloud.com/fr/lp/vmware-vcd-evolution/',
+    TERMS:
+      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/f9910e8-OVHcloud_managed_vcd-SN-1.0.pdf',
+    MIGRATION:
+      'https://help.ovhcloud.com/csm/fr-vmware-vcd-migration-use-cases?id=kb_article_view&sysparm_article=KB0064095',
+  },
+  TN: {
+    OFFER: 'https://www.ovhcloud.com/fr/lp/vmware-vcd-evolution/',
+    TERMS:
+      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/50f0a09-OVHcloud_managed_vcd-TN-1.0.pdf',
+    MIGRATION:
+      'https://help.ovhcloud.com/csm/fr-vmware-vcd-migration-use-cases?id=kb_article_view&sysparm_article=KB0064095',
+  },
+  WE: {
+    OFFER: 'https://www.ovhcloud.com/en/lp/vmware-vcd-evolution/',
+    TERMS:
+      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/3a55cf7-OVHcloud_managed_vcd-WE-1.0.pdf',
+    MIGRATION:
+      'https://help.ovhcloud.com/csm/en-vmware-vcd-migration-use-cases?id=kb_article_view&sysparm_article=KB0064081',
+  },
+  WS: {
+    OFFER: 'https://www.ovhcloud.com/en/lp/vmware-vcd-evolution/',
+    TERMS:
+      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/c5946bc-OVHcloud_managed_vcd-WS-1.0.pdf',
+    MIGRATION:
+      'https://help.ovhcloud.com/csm/en-vmware-vcd-migration-use-cases?id=kb_article_view&sysparm_article=KB0064081',
   },
   DEFAULT: {
     OFFER: 'https://www.ovhcloud.com/en/lp/vmware-vcd-evolution/',
