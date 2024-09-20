4939db83712a172f32fd42a797f5d834350e2cda
Antony MARION <antony.marion.ext@ovhcloud.com>
2024-09-20 11:55:47 +0200

    feat(web): add mxplan chip for dkim
        ref: MANAGER-14918
    
    Signed-off-by: Antony MARION <antony.marion.ext@ovhcloud.com>

--- packages/manager/modules/emailpro/src/domain/emailpro-domain.constants.js
+++ packages/manager/modules/emailpro/src/domain/emailpro-domain.constants.js
@@ -1,9 +1,12 @@
 export const DKIM_STATUS = {
   ACTIVE: 'active',
+  ENABLED: 'enabled',
   DISABLED: 'disabled',
+  DISABLED_NO_SET: 'error',
   ERROR: 'error',
   IN_PROGRESS: 'inProgress',
   TO_CONFIGURE: 'toConfigure',
+  MODIFYING: 'modifying',
 };
 
 export const DKIM_STATUS_TEXT = {
@@ -18,6 +21,15 @@ export const DKIM_STATUS_CLASS = {
   [DKIM_STATUS.DISABLED]: 'oui-badge_warning',
   [DKIM_STATUS.TO_CONFIGURE]: 'oui-background-g-100',
   [DKIM_STATUS.IN_PROGRESS]: 'oui-badge_info',
+  [DKIM_STATUS.ERROR]: 'oui-badge_error',
+};
+
+export const DKIM_STATUS_CLASS_MXPLAN = {
+  [DKIM_STATUS.ENABLED]: 'oui-badge_success',
+  [DKIM_STATUS.DISABLED]: 'oui-badge_warning',
+  [DKIM_STATUS.DISABLED_NO_SET]: 'oui-badge_error',
+  [DKIM_STATUS.TO_CONFIGURE]: 'oui-badge_error',
+  [DKIM_STATUS.MODIFYING]: 'oui-badge_info',
   [DKIM_STATUS.ERROR]: 'oui-badge_error',
 };
 
