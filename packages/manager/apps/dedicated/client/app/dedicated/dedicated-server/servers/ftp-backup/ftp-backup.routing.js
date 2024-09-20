d4678afc2961a32b3b14bc0b00edcffd0730e593
qpavy <contact@quentinpavy.com>
2024-09-20 04:43:08 +0200

    feat(dedicated): hide backup storage when not available (#12665)
    ref: MANAGER-14805, MANAGER-14698
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/dedicated/dedicated-server/servers/ftp-backup/ftp-backup.routing.js
+++ packages/manager/apps/dedicated/client/app/dedicated/dedicated-server/servers/ftp-backup/ftp-backup.routing.js
@@ -1,26 +1,41 @@
-export default /* @ngInject */ ($stateProvider, coreConfigProvider) => {
-  if (!coreConfigProvider.isRegion('US')) {
-    $stateProvider.state('app.dedicated-server.server.ftpBackup', {
-      url: '/backup',
-      views: {
-        'tabView@app.dedicated-server.server': {
-          component: 'dedicatedServerFtpBackupStorage',
-        },
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state('app.dedicated-server.server.ftpBackup', {
+    url: '/backup',
+    views: {
+      'tabView@app.dedicated-server.server': {
+        component: 'dedicatedServerFtpBackupStorage',
       },
-      resolve: {
-        breadcrumb: /* @ngInject */ ($translate) =>
-          $translate.instant('dedicated_server_ftp_backup'),
-        goToFtpBackUpStorage: ($state, Alerter) => (
-          message = false,
-          type = 'success',
-        ) => {
-          const promise = $state.go('app.dedicated-server.server.ftpBackup');
-          if (message) {
-            promise.then(() => Alerter[type](message));
-          }
-          return promise;
-        },
+    },
+    redirectTo: (transition) => {
+      const injector = transition.injector();
+      const backupIAvailablePromise = injector.getAsync(
+        'backupStorageAvailable',
+      );
+      const serverNamePromise = injector.getAsync('serverName');
+
+      return Promise.all([backupIAvailablePromise, serverNamePromise]).then(
+        ([backupIsAvailable, serverName]) =>
+          backupIsAvailable
+            ? null
+            : {
+                state: 'app.dedicated-server.server.dashboard',
+                params: { productId: serverName },
+              },
+      );
+    },
+    resolve: {
+      breadcrumb: /* @ngInject */ ($translate) =>
+        $translate.instant('dedicated_server_ftp_backup'),
+      goToFtpBackUpStorage: ($state, Alerter) => (
+        message = false,
+        type = 'success',
+      ) => {
+        const promise = $state.go('app.dedicated-server.server.ftpBackup');
+        if (message) {
+          promise.then(() => Alerter[type](message));
+        }
+        return promise;
       },
-    });
-  }
+    },
+  });
 };
