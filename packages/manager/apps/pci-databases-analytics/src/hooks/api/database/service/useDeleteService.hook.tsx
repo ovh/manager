3ea12ba22d8ceea4321532a04d71914c22a2a6d2
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2024-09-25 17:31:58 +0200

    fix: invalidate services list query on update
    ref: DATATR-1497
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/apps/pci-databases-analytics/src/hooks/api/database/service/useDeleteService.hook.tsx
+++ packages/manager/apps/pci-databases-analytics/src/hooks/api/database/service/useDeleteService.hook.tsx
@@ -1,4 +1,6 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import * as database from '@/types/cloud/project/database';
 import { deleteService } from '@/data/api/database/service.api';
 import { CdbError, ServiceData } from '@/data/api/database';
 
@@ -8,12 +10,20 @@ interface UseDeleteService {
 }
 
 export function useDeleteService({ onError, onSuccess }: UseDeleteService) {
+  const queryClient = useQueryClient();
+  const { projectId } = useParams();
   const mutation = useMutation({
     mutationFn: (serviceInfo: ServiceData) => {
       return deleteService(serviceInfo);
     },
     onError,
-    onSuccess,
+    onSuccess: () => {
+      onSuccess();
+      // Invalidate service list query to get the latest data
+      queryClient.invalidateQueries({
+        queryKey: [projectId, 'database/service'],
+      });
+    },
   });
 
   return {
