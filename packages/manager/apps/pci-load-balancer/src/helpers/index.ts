1d9accafc696a0de815afad7d2c2f8fdb2911172
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2024-09-25 09:46:13 +0200

    feat(pci-load-balancer): display load balancer detail with tabs
    DTCORE-2632
    DTCORE-2633
    DTCORE-2634
    
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/apps/pci-load-balancer/src/helpers/index.ts
+++ packages/manager/apps/pci-load-balancer/src/helpers/index.ts
@@ -1,6 +1,11 @@
 import { PaginationState } from '@ovh-ux/manager-react-components';
 import { ColumnSort } from '@tanstack/react-table';
-import { TLoadBalancer } from '@/types';
+import { format } from 'date-fns';
+import * as dateFnsLocales from 'date-fns/locale';
+import { useRef } from 'react';
+import { useTranslation } from 'react-i18next';
+import { getDateFnsLocale } from '@ovh-ux/manager-core-utils';
+import { TLoadBalancer } from '@/api/data/load-balancer';
 
 export const paginateResults = <T>(
   items: T[],
@@ -13,6 +18,7 @@ export const paginateResults = <T>(
   pageCount: Math.ceil(items.length / pagination.pageSize),
   totalRows: items.length,
 });
+
 export const sortResults = (items: TLoadBalancer[], sorting: ColumnSort) => {
   let data: TLoadBalancer[];
   switch (sorting?.id) {
@@ -30,4 +36,29 @@ export const sortResults = (items: TLoadBalancer[], sorting: ColumnSort) => {
     }
   }
   return data;
+};
+
+export const getFormattedDate = (date: string | null, formatStr = 'PPpp') => {
+  const { i18n } = useTranslation('common');
+
+  const locales = useRef({ ...dateFnsLocales }).current;
+
+  let displayDate = '';
+
+  if (date) {
+    const userLocale = getDateFnsLocale(i18n.language);
+
+    if (userLocale in locales) {
+      const localeId = userLocale as keyof typeof locales;
+      displayDate = format(new Date(date), formatStr, {
+        locale: locales[localeId],
+      });
+    } else {
+      displayDate = format(new Date(date), formatStr, {
+        locale: locales.fr,
+      });
+    }
+  }
+
+  return displayDate;
 };
---
+++ packages/manager/apps/pci-load-balancer/src/pages/detail/Detail.page.tsx
@@ -0,0 +1,126 @@
+import { useProject } from '@ovh-ux/manager-pci-common';
+import { Headers, useProjectUrl } from '@ovh-ux/manager-react-components';
+import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
+import { ODS_CHIP_SIZE, ODS_SPINNER_SIZE } from '@ovhcloud/ods-components';
+import {
+  OsdsBreadcrumb,
+  OsdsChip,
+  OsdsSpinner,
+} from '@ovhcloud/ods-components/react';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import {
+  Outlet,
+  useHref,
+  useLocation,
+  useParams,
+  useResolvedPath,
+} from 'react-router-dom';
+import { ROUTE_PATHS } from '@/routes';
+import { useLoadBalancer } from '@/api/hook/useLoadBalancer';
+import TabsPanel from '@/components/detail/TabsPanel.component';
+
+export default function DetailPage() {
+  const { t } = useTranslation('octavia-load-balancer');
+  const [activePanelTranslation, setActivePanelTranslation] = useState(null);
+
+  const location = useLocation();
+  const { data: project } = useProject();
+  const { projectId, region, loadBalancerId } = useParams();
+
+  const hrefProject = useProjectUrl('public-cloud');
+  const hrefBack = useHref('..');
+  const hrefGeneralInformation = useHref(ROUTE_PATHS.GENERAL_INFORMATION);
+
+  const tabs = [
+    {
+      name: 'octavia_load_balancer_info_tab_title',
+      title: t('octavia_load_balancer_info_tab_title'),
+      to: useResolvedPath(ROUTE_PATHS.GENERAL_INFORMATION).pathname,
+    },
+    {
+      name: 'octavia_load_balancer_listeners_tab_title',
+      title: t('octavia_load_balancer_listeners_tab_title'),
+      to: useResolvedPath(ROUTE_PATHS.LISTENERS).pathname,
+    },
+    {
+      name: 'octavia_load_balancer_pools_tab_title',
+      title: t('octavia_load_balancer_pools_tab_title'),
+      to: useResolvedPath(ROUTE_PATHS.POOLS).pathname,
+    },
+    {
+      name: 'octavia_load_balancer_statistics_tab_title',
+      title: t('octavia_load_balancer_statistics_tab_title'),
+      to: useResolvedPath(ROUTE_PATHS.STATISTICS).pathname,
+    },
+    {
+      name: 'octavia_load_balancer_certificates_tab_title',
+      title: t('octavia_load_balancer_certificates_tab_title'),
+      to: useResolvedPath(ROUTE_PATHS.CERTIFICATES).pathname,
+    },
+    {
+      name: 'octavia_load_balancer_logs_tab_title',
+      title: (
+        <span>
+          {t('octavia_load_balancer_logs_tab_title')}
+          <OsdsChip
+            className="ml-4"
+            color={ODS_THEME_COLOR_INTENT.success}
+            size={ODS_CHIP_SIZE.sm}
+            inline
+          >
+            {t('octavia_load_balancer_logs_tab_title_beta_status')}
+          </OsdsChip>
+        </span>
+      ),
+      to: useResolvedPath(ROUTE_PATHS.LOGS).pathname,
+    },
+  ];
+
+  const { data: loadBalancerDetail, isPending, error } = useLoadBalancer({
+    projectId,
+    region,
+    loadBalancerId,
+  });
+
+  useEffect(() => {
+    const activeTab = tabs.find((tab) => location.pathname.startsWith(tab.to));
+    setActivePanelTranslation(t(activeTab?.name));
+  }, [location.pathname]);
+
+  if (isPending && !error) {
+    return <OsdsSpinner inline size={ODS_SPINNER_SIZE.md} />;
+  }
+
+  return (
+    <>
+      <OsdsBreadcrumb
+        items={[
+          {
+            href: hrefProject,
+            label: project.description,
+          },
+          {
+            label: t('octavia_load_balancers'),
+            href: hrefBack,
+          },
+          {
+            label: loadBalancerDetail?.id,
+            href: hrefGeneralInformation,
+          },
+          { label: activePanelTranslation },
+        ]}
+      />
+
+      <div className="header mt-8 mb-4">
+        <Headers title={loadBalancerDetail?.name} />
+      </div>
+
+      <div className="mb-8">
+        <TabsPanel tabs={tabs} />
+      </div>
+
+      <Outlet />
+    </>
+  );
+}
