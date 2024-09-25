829d2ca79d3983d7a4b13f3c2ec752568c1219f8
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2024-09-25 11:05:00 +0200

    feat(manager-react-components): update generator
    ref: MANAGER-14622
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-react-components/src/components/templates/update-name-modal/update-name-modal.component.tsx
+++ packages/manager-react-components/src/components/templates/update-name-modal/update-name-modal.component.tsx
@@ -4,14 +4,12 @@ import {
   OdsText,
   OdsInput,
   OdsMessage,
-  OdsSpinner,
   OdsModal,
   OdsButton,
   OdsFormField,
 } from '@ovhcloud/ods-components/react';
 import {
   ODS_BUTTON_VARIANT,
-  ODS_SPINNER_SIZE,
   ODS_INPUT_TYPE,
   ODS_MESSAGE_COLOR,
   OdsInputChangeEventDetail,
@@ -110,11 +108,6 @@ export const UpdateNameModal: React.FC<UpdateNameModalProps> = ({
           </div>
         )}
       </OdsFormField>
-      {isLoading && (
-        <div className="flex justify-center">
-          <OdsSpinner size={ODS_SPINNER_SIZE.md} />
-        </div>
-      )}
       <OdsButton
         isDisabled={isLoading}
         slot="actions"
@@ -123,8 +116,9 @@ export const UpdateNameModal: React.FC<UpdateNameModalProps> = ({
         label={cancelButtonLabel || t('updateModalCancelButton')}
       />
       <OdsButton
-        isDisabled={isLoading || isPatternError}
+        isDisabled={isPatternError}
         slot="actions"
+        isLoading={isLoading}
         {...handleClick(() => updateDisplayName(displayName))}
         label={confirmButtonLabel || t('updateModalConfirmButton')}
       />
