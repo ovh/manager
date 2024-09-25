167ccdea91aee61fbde4478e1e88dbaeb9332fa8
Nicode <nicolas@nicode.fr>
2024-09-25 08:07:12 +0200

    fix(manager-components): fix DashboardLayoutGrid (#13169)
    ref: 13168
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager-react-components/src/components/templates/layout/layout.component.tsx
+++ packages/manager-react-components/src/components/templates/layout/layout.component.tsx
@@ -8,7 +8,7 @@ export const DashboardGridLayout: React.FC<React.PropsWithChildren> = ({
   children,
 }) => (
   <div className="w-full block">
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6">
+    <div className="grid grid-cols-1 md:items-start md:grid-cols-3 gap-4 md:gap-6">
       {children}
     </div>
   </div>
