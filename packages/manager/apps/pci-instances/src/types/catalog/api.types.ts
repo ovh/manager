b660b6f7c05ade7b2404930ff3b363f71640ad5e
Frédéric Vilcot <fredericvilcot@gmail.com>
2024-09-25 10:13:47 +0200

    feat(pci-instances): add catalog logic
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

---
+++ packages/manager/apps/pci-instances/src/types/catalog/api.types.ts
@@ -0,0 +1,98 @@
+import { DeepReadonly } from '../utils.type';
+
+export type TCategoryDto = {
+  name: string;
+  isNew: boolean;
+  isDefault: boolean;
+};
+
+export type TRegionDto = {
+  name: string;
+  country: string;
+  datacenter: string;
+  isLocalzone: boolean;
+  isInMaintenance: boolean;
+  isActivated: boolean;
+  category: TCategoryDto;
+};
+
+export type TPriceIntervalDto = 'hour' | 'month';
+export type TOsTypeDto = 'linux' | 'windows';
+
+export type TPricingDto = {
+  price: number;
+  regions: string[];
+  interval: TPriceIntervalDto;
+  osType: TOsTypeDto;
+};
+
+export type TBandWidthDto = {
+  guaranteed: boolean;
+  level: number;
+  unlimited: boolean;
+};
+
+export type TCpuDto = {
+  cores: number;
+  frequency: number;
+  model: string;
+  type: string;
+};
+
+export type TGpuDto = {
+  memory: TGpuMemoryDto;
+  model: string;
+  number: number;
+};
+
+export type TGpuMemoryDto = TMemoryDto & {
+  interface: string;
+};
+
+export type TMemoryDto = {
+  size: number;
+};
+
+export type TDiskDto = {
+  capacity: number;
+  number: number;
+  technology: string;
+};
+
+export type TStorageDto = {
+  disks: TDiskDto[];
+  raid: string;
+};
+
+export type TVrackDto = {
+  guaranteed: boolean;
+  level: number;
+  unlimited: boolean;
+};
+
+export type TSpecificationsDto = {
+  bandwidth: TBandWidthDto;
+  cpu: TCpuDto;
+  gpu: TGpuDto;
+  memory: TMemoryDto;
+  storage: TStorageDto;
+  vrack: TVrackDto;
+};
+
+export type TModelDto = {
+  category: string;
+  name: string;
+  isNew: boolean;
+  compatibleLocalzone: boolean;
+  compatibleRegion: boolean;
+  pricings: TPricingDto[];
+  specifications: TSpecificationsDto;
+  banners: string[];
+};
+
+export type TCatalogDto = DeepReadonly<{
+  projectId: string;
+  models: TModelDto[];
+  categories: TCategoryDto[];
+  regions: TRegionDto[];
+}>;
