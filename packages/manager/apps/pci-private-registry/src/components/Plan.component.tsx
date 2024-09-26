5b83e04b1d69f97a90a5c3ecf789e3811ac1b224
Florian Renaut <florian.renaut@corp.ovh.com>
2024-09-26 13:02:13 +0200

    fix(pci-private-registry): plans
    ref: DTCORE-2676
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/apps/pci-private-registry/src/components/Plan.component.tsx
+++ packages/manager/apps/pci-private-registry/src/components/Plan.component.tsx
@@ -4,7 +4,7 @@ import {
   ODS_THEME_TYPOGRAPHY_LEVEL,
   ODS_THEME_TYPOGRAPHY_SIZE,
 } from '@ovhcloud/ods-common-theming';
-import { OsdsText } from '@ovhcloud/ods-components/react';
+import { OsdsSkeleton, OsdsText } from '@ovhcloud/ods-components/react';
 import { useMemo } from 'react';
 import { useCatalogPrice } from '@ovh-ux/manager-react-components';
 import { useCatalog } from '@ovh-ux/manager-pci-common';
@@ -21,7 +21,7 @@ export default function PlanComponent({
 }: Readonly<TPlanComponentProps>): JSX.Element {
   const { t: tUpgrade } = useTranslation('upgrade');
 
-  const { data: catalog } = useCatalog();
+  const { data: catalog, isPending } = useCatalog();
 
   const { getFormattedMonthlyCatalogPrice } = useCatalogPrice(4);
 
@@ -40,7 +40,7 @@ export default function PlanComponent({
     toRawBytes: false,
   });
   return (
-    <div className="full-width">
+    <div className="w-full">
       <div className="border-solid border border-t-0 border-x-0 border-[--ods-color-blue-200] my-4 py-4">
         <OsdsText
           data-testid="name"
@@ -51,94 +51,104 @@ export default function PlanComponent({
           {plan.name[0]}
         </OsdsText>
       </div>
-      <ul className="list-none p-0 m-0">
-        <li data-testid="capacity">
-          <OsdsText
-            color={ODS_THEME_COLOR_INTENT.text}
-            level={ODS_THEME_TYPOGRAPHY_LEVEL.body}
-            size={ODS_THEME_TYPOGRAPHY_SIZE._400}
-          >
-            {tUpgrade('private_registry_upgrade_plan_available_storage')}
-          </OsdsText>
-          <OsdsText
-            color={ODS_THEME_COLOR_INTENT.text}
-            level={ODS_THEME_TYPOGRAPHY_LEVEL.body}
-            size={ODS_THEME_TYPOGRAPHY_SIZE._400}
-          >
-            {capacity}
-          </OsdsText>
-        </li>
-        <li data-testid="connections">
-          <OsdsText
-            color={ODS_THEME_COLOR_INTENT.text}
-            level={ODS_THEME_TYPOGRAPHY_LEVEL.body}
-            size={ODS_THEME_TYPOGRAPHY_SIZE._400}
-          >
-            {tUpgrade('private_registry_upgrade_plan_connections', {
-              total: plan.registryLimits.parallelRequest,
-            })}
-          </OsdsText>
-        </li>
-        {plan.name === 'SMALL' && (
-          <li data-testid="core-registry-99">
+      {isPending && (
+        <div className="min-h-[10rem]">
+          <OsdsSkeleton />
+          <OsdsSkeleton />
+          <OsdsSkeleton />
+          <OsdsSkeleton />
+          <OsdsSkeleton />
+        </div>
+      )}
+      {!isPending && (
+        <ul className="list-none p-0 m-0 min-h-[8rem]">
+          <li data-testid="capacity">
             <OsdsText
               color={ODS_THEME_COLOR_INTENT.text}
               level={ODS_THEME_TYPOGRAPHY_LEVEL.body}
               size={ODS_THEME_TYPOGRAPHY_SIZE._400}
             >
-              {tUpgrade('private_registry_upgrade_core_registry_99')}
+              {tUpgrade('private_registry_upgrade_plan_available_storage')}
+            </OsdsText>
+            <OsdsText
+              color={ODS_THEME_COLOR_INTENT.text}
+              level={ODS_THEME_TYPOGRAPHY_LEVEL.body}
+              size={ODS_THEME_TYPOGRAPHY_SIZE._400}
+            >
+              {capacity}
             </OsdsText>
           </li>
-        )}
-        {plan.name !== 'SMALL' && (
-          <>
-            <li data-testid="core-registry-95">
+          <li data-testid="connections">
+            <OsdsText
+              color={ODS_THEME_COLOR_INTENT.text}
+              level={ODS_THEME_TYPOGRAPHY_LEVEL.body}
+              size={ODS_THEME_TYPOGRAPHY_SIZE._400}
+            >
+              {tUpgrade('private_registry_upgrade_plan_connections', {
+                total: plan.registryLimits.parallelRequest,
+              })}
+            </OsdsText>
+          </li>
+          {plan.name === 'SMALL' && (
+            <li data-testid="core-registry-99">
               <OsdsText
                 color={ODS_THEME_COLOR_INTENT.text}
                 level={ODS_THEME_TYPOGRAPHY_LEVEL.body}
                 size={ODS_THEME_TYPOGRAPHY_SIZE._400}
               >
-                {tUpgrade('private_registry_upgrade_core_registry_95')}
+                {tUpgrade('private_registry_upgrade_core_registry_99')}
               </OsdsText>
             </li>
+          )}
+          {plan.name !== 'SMALL' && (
+            <>
+              <li data-testid="core-registry-95">
+                <OsdsText
+                  color={ODS_THEME_COLOR_INTENT.text}
+                  level={ODS_THEME_TYPOGRAPHY_LEVEL.body}
+                  size={ODS_THEME_TYPOGRAPHY_SIZE._400}
+                >
+                  {tUpgrade('private_registry_upgrade_core_registry_95')}
+                </OsdsText>
+              </li>
+              <li>
+                <OsdsText
+                  color={ODS_THEME_COLOR_INTENT.text}
+                  level={ODS_THEME_TYPOGRAPHY_LEVEL.body}
+                  size={ODS_THEME_TYPOGRAPHY_SIZE._400}
+                >
+                  {tUpgrade('private_registry_upgrade_other_components')}
+                </OsdsText>
+              </li>
+            </>
+          )}
+          {plan.features.vulnerability && (
             <li>
               <OsdsText
                 color={ODS_THEME_COLOR_INTENT.text}
                 level={ODS_THEME_TYPOGRAPHY_LEVEL.body}
                 size={ODS_THEME_TYPOGRAPHY_SIZE._400}
               >
-                {tUpgrade('private_registry_upgrade_other_components')}
+                {tUpgrade('private_registry_upgrade_plan_vulnerability')}
               </OsdsText>
             </li>
-          </>
-        )}
-        {plan.features.vulnerability && (
-          <li>
-            <OsdsText
-              color={ODS_THEME_COLOR_INTENT.text}
-              level={ODS_THEME_TYPOGRAPHY_LEVEL.body}
-              size={ODS_THEME_TYPOGRAPHY_SIZE._400}
-            >
-              {tUpgrade('private_registry_upgrade_plan_vulnerability')}
-            </OsdsText>
-          </li>
-        )}
-        {addon?.blobs.technical.bandwidth.unlimited && (
-          <li>
-            <OsdsText
-              color={ODS_THEME_COLOR_INTENT.text}
-              level={ODS_THEME_TYPOGRAPHY_LEVEL.body}
-              size={ODS_THEME_TYPOGRAPHY_SIZE._400}
-            >
-              {tUpgrade('private_registry_upgrade_plan_traffic')}
-            </OsdsText>
-          </li>
-        )}
-      </ul>
+          )}
+          {addon?.blobs.technical.bandwidth.unlimited && (
+            <li>
+              <OsdsText
+                color={ODS_THEME_COLOR_INTENT.text}
+                level={ODS_THEME_TYPOGRAPHY_LEVEL.body}
+                size={ODS_THEME_TYPOGRAPHY_SIZE._400}
+              >
+                {tUpgrade('private_registry_upgrade_plan_traffic')}
+              </OsdsText>
+            </li>
+          )}
+        </ul>
+      )}
       <div
         className={clsx(
-          'border-solid border border-b-0 border-x-0 border-[--ods-color-blue-200] mb-2 pt-4',
-          clsx(plan.name.startsWith('S') ? 'mt-16' : 'mt-9'),
+          'border-solid border border-b-0 border-x-0 border-[--ods-color-blue-200] mb-2 pt-4 mt-9',
         )}
         data-testid="price"
       >
