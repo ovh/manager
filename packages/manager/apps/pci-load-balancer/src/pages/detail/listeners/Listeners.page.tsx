b2858b59a191c4b41ce2d1bc08bc2ffef876bf25
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2024-09-26 11:18:50 +0200

    feat(pci-load-balancer): create listeners listing page
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/apps/pci-load-balancer/src/pages/detail/listeners/Listeners.page.tsx
+++ packages/manager/apps/pci-load-balancer/src/pages/detail/listeners/Listeners.page.tsx
@@ -1,3 +1,57 @@
+import { Links, Notifications } from '@ovh-ux/manager-react-components';
+import { OdsHTMLAnchorElementTarget } from '@ovhcloud/ods-common-core';
+import { ODS_TEXT_LEVEL, ODS_TEXT_SIZE } from '@ovhcloud/ods-components';
+import { OsdsText } from '@ovhcloud/ods-components/react';
+import { Suspense } from 'react';
+
+import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
+import { useTranslation } from 'react-i18next';
+import { Outlet } from 'react-router-dom';
+import { DISCOVER_LINK } from '@/constants';
+
 export default function Listeners() {
-  return <div>Listeners Page</div>;
+  const { t } = useTranslation('octavia-load-balancer-listeners');
+
+  return (
+    <div>
+      <OsdsText
+        size={ODS_TEXT_SIZE._600}
+        level={ODS_TEXT_LEVEL.heading}
+        color={ODS_THEME_COLOR_INTENT.text}
+      >
+        {t('octavia_load_balancer_listeners_title')}
+      </OsdsText>
+
+      <div className="my-8">
+        <OsdsText
+          className="block"
+          size={ODS_TEXT_SIZE._400}
+          level={ODS_TEXT_LEVEL.body}
+          color={ODS_THEME_COLOR_INTENT.text}
+        >
+          {t('octavia_load_balancer_listeners_description_part_1')}
+        </OsdsText>
+        <OsdsText
+          className=""
+          size={ODS_TEXT_SIZE._400}
+          level={ODS_TEXT_LEVEL.body}
+          color={ODS_THEME_COLOR_INTENT.text}
+        >
+          {t('octavia_load_balancer_listeners_description_part_2')}
+        </OsdsText>
+        <Links
+          className="ml-3"
+          href={DISCOVER_LINK}
+          target={OdsHTMLAnchorElementTarget._blank}
+          label={t('octavia_load_balancer_listeners_description_part_2_link')}
+        />
+      </div>
+
+      <Notifications />
+
+      <Suspense>
+        <Outlet />
+      </Suspense>
+    </div>
+  );
 }
