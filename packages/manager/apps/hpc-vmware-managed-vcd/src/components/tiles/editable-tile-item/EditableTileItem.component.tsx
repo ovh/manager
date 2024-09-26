71f346a24ec2036ef844b0b979fe3b2923fe6295
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2024-09-26 10:01:08 +0200

    feat(hpc-vmware-managed-vcd): fix pen icon size
      ref: MANAGER-15412
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

---
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/components/tiles/editable-tile-item/EditableTileItem.component.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Description } from '@ovh-ux/manager-react-components';
+import { OsdsIcon } from '@ovhcloud/ods-components/react';
+import { ODS_ICON_NAME, ODS_ICON_SIZE } from '@ovhcloud/ods-components';
+import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
+
+type EditableTileItemProps = {
+  label: string;
+  onClickEdit: () => void;
+};
+
+export const EditableTileItem = ({
+  label,
+  onClickEdit,
+}: EditableTileItemProps) => {
+  const { t } = useTranslation('dashboard');
+
+  return (
+    <div className="flex justify-between items-center">
+      <Description>{label}</Description>
+      <div>
+        <OsdsIcon
+          aria-label={t('managed_vcd_dashboard_edit_modal_cta_edit')}
+          className="mx-6 cursor-pointer"
+          name={ODS_ICON_NAME.PEN}
+          size={ODS_ICON_SIZE.xxs}
+          color={ODS_THEME_COLOR_INTENT.primary}
+          onClick={onClickEdit}
+        />
+      </div>
+    </div>
+  );
+};
