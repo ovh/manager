a6356b6b0d345fb9b4e850913b278e89fb17713e
Pierre-Philippe Prevost <ppprevost@aol.fr>
2024-09-26 17:18:09 +0200

    fix(pci.block-storage): add us link (#12831)
    ref: TAPC-663
    
    Signed-off-by: Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>

--- packages/manager/apps/pci-block-storage/src/pages/onboarding/onboarding.constants.ts
+++ packages/manager/apps/pci-block-storage/src/pages/onboarding/onboarding.constants.ts
@@ -4,18 +4,24 @@ export const GUIDES = {
       'https://docs.ovh.com/gb/en/public-cloud/create-an-additional-volume-and-attach-it-to-an-instance/',
     IN:
       'https://docs.ovh.com/asia/en/public-cloud/create_and_configure_an_additional_disk_on_an_instance/',
+    US:
+      'https://support.us.ovhcloud.com/hc/en-us/articles/20566582694291-Creating-and-Configuring-an-Additional-Disk-on-an-Instance',
   },
   'configure-volume': {
     default:
       'https://docs.ovh.com/gb/en/public-cloud/configure-an-additional-volume/',
     IN:
       'https://docs.ovh.com/asia/en/public-cloud/configure-an-additional-volume/',
+    US:
+      'https://support.us.ovhcloud.com/hc/en-us/articles/20566582694291-Creating-and-Configuring-an-Additional-Disk-on-an-Instance',
   },
   'increase-volume-size': {
     default:
       'https://docs.ovh.com/gb/en/public-cloud/increase_the_size_of_an_additional_disk/',
     IN:
       'https://docs.ovh.com/asia/en/public-cloud/increase_the_size_of_an_additional_disk/',
+    US:
+      'https://support.us.ovhcloud.com/hc/en-us/articles/23014511701907-Increasing-the-Size-of-an-Additional-Disk',
   },
 };
 
