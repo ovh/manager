c23ce44cef6dcbba5b2845cf9b02784c68d0cf0d
Tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2024-09-26 17:14:57 +0200

    feat(pci.private-network): display the gateway column in the LZ datagrid (#13129)
    ref: TAPC-1259
    
    Signed-off-by: Tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

--- packages/manager/apps/pci-private-network/src/hooks/useDatagridColumn.spec.tsx
+++ packages/manager/apps/pci-private-network/src/hooks/useDatagridColumn.spec.tsx
@@ -6,11 +6,12 @@ describe('useDatagridColumn', () => {
   it('should return correct columns', () => {
     const { result } = renderHook(() => useDatagridColumn());
     const columns = result.current;
-    expect(columns).toHaveLength(6);
+    expect(columns).toHaveLength(7);
     expect(columns.map((column) => column.id)).toEqual([
       'name',
       'region',
       'cidr',
+      'gatewayIp',
       'dhcp',
       'allocatedIp',
       'actions',
@@ -26,10 +27,13 @@ describe('useDatagridColumn', () => {
       'pci_projects_project_network_private_region',
     );
     expect(columns[2].label).toBe('CIDR');
-    expect(columns[3].label).toBe('DHCP');
-    expect(columns[4].label).toBe(
+    expect(columns[3].label).toBe(
+      'pci_projects_project_network_private_gateway',
+    );
+    expect(columns[4].label).toBe('DHCP');
+    expect(columns[5].label).toBe(
       'pci_projects_project_network_private_ip_allocation',
     );
-    expect(columns[5].label).toBe('');
+    expect(columns[6].label).toBe('');
   });
 });
