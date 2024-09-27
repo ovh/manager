783cbb84f55e1787117fcc549bc97554b946858e
Frédéric Vilcot <fredericvilcot@gmail.com>
2024-09-27 21:55:45 +0200

    feat(pci-instances): create useAppStore with slices
    ref:TAPC-1807
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

---
+++ packages/manager/apps/pci-instances/src/store/slices/stepper.slice.ts
@@ -0,0 +1,66 @@
+import { StateCreator } from 'zustand';
+import { enableMapSet } from 'immer';
+
+enableMapSet();
+
+export type TStepId = 'model';
+
+export type TStep = {
+  isOpen: boolean;
+  isChecked: boolean;
+  isLocked: boolean;
+};
+
+export type TSteps = Map<TStepId, TStep>;
+
+export type TState = {
+  steps: TSteps;
+};
+
+/**
+ * By default, computed functions in Zustand do not return updated values.
+ * If used with a parameter, currification enables reactivity for getters.
+ */
+export type TQuery = {
+  stepState: () => (stepId: TStepId) => TStep | undefined;
+};
+
+// Handlers
+export type TCommand = {
+  updateStep: (stepId: TStepId, step: Partial<TStep>) => void;
+};
+
+export type TStepperSlice = TQuery & TState & TCommand;
+
+const initStep = (isOpen: boolean): TStep => ({
+  isOpen,
+  isChecked: false,
+  isLocked: false,
+});
+
+const initialSteps = new Map<TStepId, TStep>([['model', initStep(true)]]);
+
+export const createStepperSlice: StateCreator<
+  TStepperSlice,
+  [],
+  [],
+  TStepperSlice
+> = (set, get) => ({
+  steps: initialSteps,
+  stepState: () => (stepId) => get().steps.get(stepId),
+  updateStep: (stepId, step) =>
+    set((state) => {
+      const newSteps = new Map(state.steps);
+      const activeStep = newSteps.get(stepId);
+      if (activeStep) {
+        const updatedSteps = newSteps.set(stepId, {
+          ...activeStep,
+          ...(step.isOpen !== undefined && { isOpen: step.isOpen }),
+          ...(step.isChecked !== undefined && { isChecked: step.isChecked }),
+          ...(step.isLocked !== undefined && { isLocked: step.isLocked }),
+        });
+        return { steps: updatedSteps };
+      }
+      return { steps: newSteps };
+    }),
+});
