a43459917ccfeb36187195ad18d98ef829fb66bf
Florian Renaut <florian.renaut@corp.ovh.com>
2024-09-27 13:55:42 +0200

    fix(pci-private-registry): rebase on master
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/apps/pci-private-registry/src/pages/create/steps/PlanStep.spec.tsx
+++ packages/manager/apps/pci-private-registry/src/pages/create/steps/PlanStep.spec.tsx
@@ -2,10 +2,9 @@ import { render } from '@testing-library/react';
 import { wrapper } from '@/wrapperRenders';
 import PlanStep from '@/pages/create/steps/PlanStep';
 
-const compute = () => render(<PlanStep />, { wrapper });
-
 describe('PlanStep', () => {
   it('should render', () => {
+    const compute = () => render(<PlanStep />, { wrapper });
     const { container } = compute();
     expect(container).toMatchSnapshot();
   });
