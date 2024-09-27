0dcd4bc34283a2def4700ad13265d0f7073ccaad
Frédéric Vilcot <fredericvilcot@gmail.com>
2024-09-27 22:09:27 +0200

    feat(pci-instances): improve code quality
    ref:TAPC-1807
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

--- packages/manager/apps/pci-instances/src/store/hooks/useAppStore.ts
+++ packages/manager/apps/pci-instances/src/store/hooks/useAppStore.ts
@@ -1,8 +1,11 @@
 import { create } from 'zustand';
 import { immer } from 'zustand/middleware/immer';
 import { devtools } from 'zustand/middleware';
+import { enableMapSet } from 'immer';
 import { createStepperSlice, TStepperSlice } from '../slices/stepper.slice';
 import { createFormSlice, TFormSlice } from '../slices/form.slice';
+
+enableMapSet();
 
 export const useAppStore = create<TStepperSlice & TFormSlice>()(
   devtools(
