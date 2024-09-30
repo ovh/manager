b770653612b128ec1bd0ef913e0f475a63ff9abb
Yoann Fievez <yoann.fievez@gmail.com>
2024-09-30 11:19:44 +0200

    feat(pci-load-balancer): add l7 datagrid
    ref: DTCORE-2639
    Signed-off-by: Yoann Fievez <yoann.fievez@gmail.com>

---
+++ packages/manager/apps/pci-load-balancer/src/api/data/l7Policies.ts
@@ -0,0 +1,33 @@
+import { v6 } from '@ovh-ux/manager-core-api';
+import {
+  LoadBalancerOperatingStatusEnum,
+  LoadBalancerProvisioningStatusEnum,
+} from '@/api/data/load-balancer';
+
+export type TL7Policy = {
+  id: string;
+  name: string;
+  description: string;
+  operatingStatus: LoadBalancerOperatingStatusEnum;
+  provisioningStatus: LoadBalancerProvisioningStatusEnum;
+  redirectHttpCode: number | null;
+  redirectPoolId: string | null;
+  redirectPrefix: string | null;
+  redirectUrl: string | null;
+  action: string;
+  position: number;
+  listenerId: string;
+  attribute?: string;
+  search?: string;
+};
+
+export const getL7Policies = async (
+  projectId: string,
+  listenerId: string,
+  region: string,
+): Promise<TL7Policy[]> => {
+  const { data } = await v6.get<TL7Policy[]>(
+    `/cloud/project/${projectId}/region/${region}/loadbalancing/l7Policy?listenerId=${listenerId}`,
+  );
+  return data;
+};
