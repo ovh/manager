e11dc46f7f3b4e95d66ab70d0c263ac93730a7d4
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2024-10-01 14:39:34 +0200

    feat(pci-load-balancer): delete pool
    DTCORE-2650
    
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/apps/pci-load-balancer/src/routes.tsx
+++ packages/manager/apps/pci-load-balancer/src/routes.tsx
@@ -17,6 +17,7 @@ export const ROUTE_PATHS = {
   L7_LIST: 'list',
   POOLS: 'pools',
   POOL_LIST: 'list',
+  POOL_DELETE: 'delete',
   STATISTICS: 'statistics',
   CERTIFICATES: 'certificates',
   LOGS: 'logs',
@@ -50,6 +51,9 @@ const ListenersEditPage = lazy(() =>
 );
 const PoolsPage = lazy(() => import('@/pages/detail/pools/Pools.page'));
 const PoolListPage = lazy(() => import('@/pages/detail/pools/List.page'));
+const PoolDeletePage = lazy(() =>
+  import('@/pages/detail/pools/delete/DeletePool.page'),
+);
 const StatisticsPage = lazy(() =>
   import('@/pages/detail/statistics/Statistics.page'),
 );
@@ -126,7 +130,13 @@ const Routes = (
           id="pool-list"
           path={ROUTE_PATHS.POOL_LIST}
           Component={PoolListPage}
-        />
+        >
+          <Route
+            id="pool-delete"
+            path={ROUTE_PATHS.POOL_DELETE}
+            Component={PoolDeletePage}
+          ></Route>
+        </Route>
       </Route>
       <Route
         id="detail-statistics"
