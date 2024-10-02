a9cb57201aeaa07b061b7e63895642460bc7b271
Yoann Fievez <yoann.fievez@gmail.com>
2024-10-02 12:03:01 +0200

    feat(pci-load-balancer): add delete modal for l7 policy
    ref: DTCORE-2642
    Signed-off-by: Yoann Fievez <yoann.fievez@gmail.com>

---
+++ packages/manager/apps/pci-load-balancer/src/pages/detail/listeners/l7/delete/Delete.page.tsx
@@ -0,0 +1,94 @@
+import { Translation, useTranslation } from 'react-i18next';
+import { DeletionModal } from '@ovh-ux/manager-pci-common';
+import { useNavigate, useParams } from 'react-router-dom';
+import { OsdsText } from '@ovhcloud/ods-components/react';
+import { ODS_TEXT_LEVEL, ODS_TEXT_SIZE } from '@ovhcloud/ods-components';
+import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
+import { useNotifications } from '@ovh-ux/manager-react-components';
+import { ApiError } from '@ovh-ux/manager-core-api';
+import { useDeletePolicy, useGetPolicy } from '@/api/hook/useL7Policy';
+
+export default function DeletePage() {
+  const { addSuccess, addError } = useNotifications();
+  const { t: tDelete } = useTranslation('octavia-load-balancer-l7-delete');
+  const navigate = useNavigate();
+  const { projectId, policyId, region } = useParams();
+  const onClose = () => {
+    navigate('..');
+  };
+  const { data: policy, isPending: isPendingLoadBalancer } = useGetPolicy(
+    projectId,
+    policyId,
+    region,
+  );
+
+  const { deletePolicy, isPending: isPendingDelete } = useDeletePolicy({
+    projectId,
+    region,
+    policyId,
+    onError(error: ApiError) {
+      addError(
+        <Translation ns="octavia-load-balancer">
+          {(_t) =>
+            _t('octavia_load_balancer_global_error', {
+              message: error?.response?.data?.message || error?.message || null,
+              requestId: error?.config?.headers['X-OVH-MANAGER-REQUEST-ID'],
+            })
+          }
+        </Translation>,
+        true,
+      );
+      onClose();
+    },
+    onSuccess() {
+      addSuccess(
+        <Translation ns="octavia-load-balancer-l7">
+          {(_t) =>
+            _t('octavia_load_balancer_list_l7_policies_delete_success', {
+              policy: policy?.name,
+            })
+          }
+        </Translation>,
+        true,
+      );
+      navigate('..');
+    },
+  });
+
+  const onConfirm = () => {
+    deletePolicy();
+  };
+
+  const onCancel = () => {
+    navigate('..');
+  };
+
+  const isPending = isPendingLoadBalancer || isPendingDelete;
+
+  return (
+    <DeletionModal
+      title={tDelete('octavia_load_balancer_list_l7_policies_delete_title')}
+      onConfirm={onConfirm}
+      onClose={onClose}
+      onCancel={onCancel}
+      isPending={isPending}
+      type="warning"
+      submitText={tDelete(
+        'octavia_load_balancer_list_l7_policies_delete_confirm',
+      )}
+      cancelText={tDelete(
+        'octavia_load_balancer_list_l7_policies_delete_cancel',
+      )}
+    >
+      <OsdsText
+        level={ODS_TEXT_LEVEL.body}
+        color={ODS_THEME_COLOR_INTENT.text}
+        size={ODS_TEXT_SIZE._400}
+      >
+        {tDelete('octavia_load_balancer_list_l7_policies_delete_description', {
+          policy: policy?.name,
+        })}
+      </OsdsText>
+    </DeletionModal>
+  );
+}
