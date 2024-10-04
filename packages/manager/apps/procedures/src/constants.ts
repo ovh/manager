e87e9a8b54fd81c639bd065e48dce67de23f7784
JacquesLarique <134954692+JacquesLarique@users.noreply.github.com>
2024-10-04 08:40:30 +0200

    feat(procedures): adjusted privacy policy link (#13154)
    ref: MANAGER-15126
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

---
+++ packages/manager/apps/procedures/src/constants.ts
@@ -0,0 +1,33 @@
+import { PolicyLinks } from '@/types/links.type';
+
+export const LegalPolicyLinkByLanguage: PolicyLinks = {
+  DEFAULT:
+    'https://www.ovhcloud.com/en-ie/terms-and-conditions/privacy-policy/',
+  ASIA: 'https://www.ovhcloud.com/en-ca/terms-and-conditions/privacy-policy/',
+  AU: 'https://www.ovhcloud.com/en-ca/terms-and-conditions/privacy-policy/',
+  CA: {
+    en: 'https://www.ovhcloud.com/en-ca/terms-and-conditions/privacy-policy/',
+    fr: 'https://www.ovhcloud.com/fr-ca/terms-and-conditions/privacy-policy/',
+  },
+  CZ: 'https://www.ovhcloud.com/en-ie/terms-and-conditions/privacy-policy/',
+  DE: 'https://www.ovhcloud.com/de/terms-and-conditions/privacy-policy/',
+  ES: 'https://www.ovhcloud.com/es-es/terms-and-conditions/privacy-policy/',
+  FI: 'https://www.ovhcloud.com/en-ie/terms-and-conditions/privacy-policy/',
+  FR: 'https://www.ovhcloud.com/fr/terms-and-conditions/privacy-policy/',
+  GB: 'https://www.ovhcloud.com/en-gb/terms-and-conditions/privacy-policy/',
+  IE: 'https://www.ovhcloud.com/en-ie/terms-and-conditions/privacy-policy/',
+  IN: 'https://www.ovhcloud.com/en-in/terms-and-conditions/privacy-policy/',
+  IT: 'https://www.ovhcloud.com/it/terms-and-conditions/privacy-policy/',
+  LT: 'https://www.ovhcloud.com/en-ie/terms-and-conditions/privacy-policy/',
+  LTE: 'https://www.ovhcloud.com/en-ie/terms-and-conditions/privacy-policy/',
+  MA: 'https://www.ovhcloud.com/fr/terms-and-conditions/privacy-policy/',
+  NL: 'https://www.ovhcloud.com/nl/terms-and-conditions/privacy-policy/',
+  PL: 'https://www.ovhcloud.com/pl/terms-and-conditions/privacy-policy/',
+  PT: 'https://www.ovhcloud.com/pt/terms-and-conditions/privacy-policy/',
+  QC: 'https://www.ovhcloud.com/fr-ca/terms-and-conditions/privacy-policy/',
+  SG: 'https://www.ovhcloud.com/en-ca/terms-and-conditions/privacy-policy/',
+  SN: 'https://www.ovhcloud.com/fr/terms-and-conditions/privacy-policy/',
+  TN: 'https://www.ovhcloud.com/fr/terms-and-conditions/privacy-policy/',
+  WE: 'https://www.ovhcloud.com/en-ca/terms-and-conditions/privacy-policy/',
+  WS: 'https://www.ovhcloud.com/en-ca/terms-and-conditions/privacy-policy/',
+};
