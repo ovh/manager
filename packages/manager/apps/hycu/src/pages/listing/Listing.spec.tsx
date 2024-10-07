d0590721b278566722874a83007dc53d9e9affff
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2024-10-07 10:48:41 +0200

    feat(hycu): add listing page
    ref: MANAGER-14497
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/apps/hycu/src/pages/listing/Listing.spec.tsx
+++ packages/manager/apps/hycu/src/pages/listing/Listing.spec.tsx
@@ -1,10 +1,42 @@
-import { screen } from '@testing-library/react';
+import { act, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderTestApp } from '@/utils/tests/renderTestApp';
 import '@testing-library/jest-dom';
 import { labels } from '@/utils/tests/init.i18n';
+import { licensesHycu } from '@/mocks/licenseHycu/licenseHycu.data';
 
-describe('KMS listing test suite', () => {
-  it('should redirect to the onboarding page when the kms list is empty', async () => {
+describe('License Hycu listing test suite', () => {
+  it('should redirect to the onboarding page when the license hycu list is empty', async () => {
     await renderTestApp({ nbLicenseHycu: 0 });
+
+    expect(screen.getByText(labels.onboarding.title)).toBeVisible();
+
+    expect(screen.queryByText(labels.listing.title)).not.toBeInTheDocument();
+  });
+
+  it('should display the hycu listing page', async () => {
+    await renderTestApp();
+
+    expect(screen.getByText(labels.listing.title)).toBeVisible();
+
+    expect(
+      screen.queryByText(labels.onboarding.description),
+    ).not.toBeInTheDocument();
+  });
+
+  it('should navigate to a hycu dashboard on click on license hycu name', async () => {
+    await renderTestApp();
+
+    await act(() =>
+      userEvent.click(screen.getByText(licensesHycu[0].serviceName)),
+    );
+
+    await waitFor(
+      () =>
+        expect(
+          screen.getByText(labels.dashboard.general_informations),
+        ).toBeVisible(),
+      { timeout: 30_000 },
+    );
   });
 });
