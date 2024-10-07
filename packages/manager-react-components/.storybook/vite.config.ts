43a08182f3a7211f5df95e4c7b204626a4282372
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2024-10-07 15:31:25 +0200

    fix(manager-react-components): fix storybook launch
    ref: MANAGER-14822
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-react-components/.storybook/vite.config.ts
+++ packages/manager-react-components/.storybook/vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig, UserConfigExport } from 'vitest/config';
 import tailwindcss from 'tailwindcss';
+import path from 'path';
 
 const app = async (): Promise<UserConfigExport> => {
   return defineConfig({
@@ -8,6 +9,39 @@ const app = async (): Promise<UserConfigExport> => {
     css: {
       postcss: {
         plugins: [tailwindcss],
+      },
+    },
+    resolve: {
+      alias: {
+        // Example: Adding an alias for your project or packages
+        '@ovh-ux/request-tagger': path.resolve(
+          __dirname,
+          '../../manager/core/request-tagger/src/',
+        ),
+        '@ovh-ux/manager-core-sso': path.resolve(
+          __dirname,
+          '../../manager/core/sso/src/',
+        ),
+        '@ovh-ux/manager-core-api': path.resolve(
+          __dirname,
+          '../../manager/core/api/src/',
+        ),
+        '@ovh-ux/manager-react-shell-client': path.resolve(
+          __dirname,
+          '../../manager/core/shell-client/src/',
+        ),
+        '@ovh-ux/ovh-at-internet': path.resolve(
+          __dirname,
+          '../../components/ovh-at-internet/src',
+        ),
+        '@ovh-ux/shell': path.resolve(
+          __dirname,
+          '../../components/ovh-shell/src',
+        ),
+        '@ovh-ux/url-builder': path.resolve(
+          __dirname,
+          '../../manager/core/url-builder/src',
+        ),
       },
     },
   });
--- packages/manager-react-components/vite.config.ts
+++ packages/manager-react-components/vite.config.ts
@@ -4,6 +4,7 @@ import dts from 'vite-plugin-dts';
 import tailwindcss from 'tailwindcss';
 import { UserConfigExport } from 'vite';
 import { getBaseConfig } from '@ovh-ux/manager-vite-config';
+import path from 'path';
 
 const app = async (): Promise<UserConfigExport> => {
   return defineConfig({
@@ -22,6 +23,36 @@ const app = async (): Promise<UserConfigExport> => {
     test: {
       globals: true,
       environment: 'jsdom',
+    },
+    resolve: {
+      alias: {
+        // Example: Adding an alias for your project or packages
+        '@ovh-ux/request-tagger': path2.resolve(
+          __dirname,
+          '../manager/core/request-tagger/src/',
+        ),
+        '@ovh-ux/manager-core-sso': path.resolve(
+          __dirname,
+          '../manager/core/sso/src/',
+        ),
+        '@ovh-ux/manager-core-api': path.resolve(
+          __dirname,
+          '../manager/core/api/src/',
+        ),
+        '@ovh-ux/manager-react-shell-client': path.resolve(
+          __dirname,
+          '../manager/core/shell-client/src/',
+        ),
+        '@ovh-ux/ovh-at-internet': path.resolve(
+          __dirname,
+          '../components/ovh-at-internet/src',
+        ),
+        '@ovh-ux/shell': path.resolve(__dirname, '../components/ovh-shell/src'),
+        '@ovh-ux/url-builder': path.resolve(
+          __dirname,
+          '../manager/core/url-builder/src',
+        ),
+      },
     },
   });
 };
