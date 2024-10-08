0e3271640874505286983ab4c77ede6a17a6f6b6
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2024-10-08 17:52:29 +0200

    refactor(hycu): remove playwright
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

---
+++ packages/manager/apps/hycu/src/mocks/licenseHycu/licenseHycu.data.ts
@@ -0,0 +1,18 @@
+export const licenseHycu: unknown[] = [
+  {
+    displayName: '425802fa-fb70-4b2a-9d5b-ec4de86bb40c',
+    extraParams: null,
+    parentName: null,
+    serviceName: '425802fa-fb70-4b2a-9d5b-ec4de86bb40c',
+    stateParams: ['425802fa-fb70-4b2a-9d5b-ec4de86bb40c'],
+    url: '#/425802fa-fb70-4b2a-9d5b-ec4de86bb40c',
+  },
+  {
+    displayName: 'c1b7cb4f-6b63-45da-9a8a-f731f1a67b2c',
+    extraParams: null,
+    parentName: null,
+    serviceName: 'c1b7cb4f-6b63-45da-9a8a-f731f1a67b2c',
+    stateParams: ['c1b7cb4f-6b63-45da-9a8a-f731f1a67b2c'],
+    url: '#/c1b7cb4f-6b63-45da-9a8a-f731f1a67b2c',
+  },
+];
