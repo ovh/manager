b3487574b28ad5f6260de5ec659600187ad18188
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2024-10-08 16:03:05 +0200

    feat(hpc-vmware-managed-vcd): add suspense on manager button
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/hpc-vmware-managed-vcd/src/components/tiles/editable-tile-item/EditableTileItem.component.tsx
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/components/tiles/editable-tile-item/EditableTileItem.component.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Description, ManagerButton } from '@ovh-ux/manager-react-components';
 import { OsdsIcon } from '@ovhcloud/ods-components/react';
@@ -10,6 +10,7 @@ import {
   ODS_ICON_SIZE,
 } from '@ovhcloud/ods-components';
 import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
+import Loading from '@/components/loading/Loading.component';
 
 type EditableTileItemProps = {
   label: string;
@@ -29,24 +30,26 @@ export default function EditableTileItem({
   return (
     <div className="flex justify-between items-center">
       <Description className="break-all">{label}</Description>
-      <ManagerButton
-        className="ml-6"
-        data-testid="editIcon"
-        iamActions={iamActions}
-        urn={urn}
-        onClick={onClickEdit}
-        circle
-        variant={ODS_BUTTON_VARIANT.ghost}
-        type={ODS_BUTTON_TYPE.button}
-        size={ODS_BUTTON_SIZE.sm}
-      >
-        <OsdsIcon
-          aria-label={t('managed_vcd_dashboard_edit_modal_cta_edit')}
-          name={ODS_ICON_NAME.PEN}
-          size={ODS_ICON_SIZE.xs}
-          color={ODS_THEME_COLOR_INTENT.primary}
-        />
-      </ManagerButton>
+      <Suspense fallback={<Loading />}>
+        <ManagerButton
+          className="ml-6"
+          data-testid="editIcon"
+          iamActions={iamActions}
+          urn={urn}
+          onClick={onClickEdit}
+          circle
+          variant={ODS_BUTTON_VARIANT.ghost}
+          type={ODS_BUTTON_TYPE.button}
+          size={ODS_BUTTON_SIZE.sm}
+        >
+          <OsdsIcon
+            aria-label={t('managed_vcd_dashboard_edit_modal_cta_edit')}
+            name={ODS_ICON_NAME.PEN}
+            size={ODS_ICON_SIZE.xs}
+            color={ODS_THEME_COLOR_INTENT.primary}
+          />
+        </ManagerButton>
+      </Suspense>
     </div>
   );
 }
