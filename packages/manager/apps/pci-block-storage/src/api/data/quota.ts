ae885a67c1d315f48ba80e1ec82f2fa3687f5cf5
Simon Chaumet <simon.chaumet.ext@ovhcloud.com>
2024-10-08 10:00:19 +0200

    feat(pci-block-storage): add extension for volume size
    ref: TAPC-1609
    
    Signed-off-by: Simon Chaumet <simon.chaumet.ext@ovhcloud.com>

--- packages/manager/apps/pci-block-storage/src/api/data/quota.ts
+++ packages/manager/apps/pci-block-storage/src/api/data/quota.ts
@@ -1,4 +1,6 @@
 import { v6 } from '@ovh-ux/manager-core-api';
+import { useFeatureAvailability } from '@ovh-ux/manager-react-components';
+import { useMemo } from 'react';
 
 export interface RegionQuota {
   region: string;
@@ -18,3 +20,33 @@ export const getRegionsQuota = async (
   );
   return data;
 };
+
+function isLocalZone(region: string) {
+  return region.split('-')[2] === 'LZ';
+}
+
+function getVolumeMaxSize(region: string) {
+  return isLocalZone(region) ? 4 * 1000 : 12 * 1000;
+}
+
+export const FA_VOLUME_EXTEND_12TO = 'pci-block-storage:volume-extend-12To';
+export const FA_EXTEN_BANNER = 'pci-block-storage:exten-banner';
+
+export function useVolumeMaxSize(region: string | undefined) {
+  const { data, ...restApi } = useFeatureAvailability([
+    FA_VOLUME_EXTEND_12TO,
+    FA_EXTEN_BANNER,
+  ]);
+
+  const volumeMaxSize = useMemo(() => {
+    if (region && data?.[FA_VOLUME_EXTEND_12TO]) {
+      return getVolumeMaxSize(region);
+    }
+    return 4 * 1000;
+  }, [data, region]);
+
+  return {
+    ...restApi,
+    volumeMaxSize,
+  };
+}
--- packages/manager/apps/pci-block-storage/src/constants.ts
+++ packages/manager/apps/pci-block-storage/src/constants.ts
@@ -51,7 +51,6 @@ export const URL_INFO = {
   LOCAL_ZONE: LOCAL_ZONE_INFO_URL,
 };
 
-export const VOLUME_MAX_SIZE = 4 * 1000; // Should be 10 * 1024 (but API is wrong;
 export const VOLUME_MIN_SIZE = 10; // 10 Gio
 export const VOLUME_UNLIMITED_QUOTA = -1; // Should be 10 * 1024 (but API is wrong)
 export const ALPHA_CHARACTERS_REGEX = /^[a-zA-Z-]+$/;
