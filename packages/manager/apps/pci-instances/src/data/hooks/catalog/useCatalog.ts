47b89facf8d174b9eb21790a1446c970ec25ace9
Frédéric Vilcot <fredericvilcot@gmail.com>
2024-10-08 16:06:03 +0200

    feat(pci-instances): add region selector for catalog hook
    ref:TAPC-1864
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

--- packages/manager/apps/pci-instances/src/data/hooks/catalog/useCatalog.ts
+++ packages/manager/apps/pci-instances/src/data/hooks/catalog/useCatalog.ts
@@ -1,144 +1,70 @@
-import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import { keepPreviousData, QueryClient, useQuery } from '@tanstack/react-query';
 import { useCallback, useMemo } from 'react';
 import { instancesQueryKey } from '@/utils';
 import { getCatalog } from '@/data/api/catalog';
-import {
-  TCatalogDto,
-  TCpuDto,
-  TDiskDto,
-  TGpuDto,
-  TMemoryDto,
-  TModelDto,
-  TPricingDto,
-  TSpecificationsDto,
-} from '@/types/catalog/api.types';
-import {
-  TCpu,
-  TGpu,
-  TMemory,
-  TModel,
-  TModelEntity,
-  TModelPricing,
-  TModelSpecification,
-  TStorage,
-} from '@/types/catalog/entity.types';
-import { DeepReadonly } from '@/types/utils.type';
+import { TCatalogDto } from '@/types/catalog/api.types';
+import { useAppStore } from '@/store/hooks/useAppStore';
+import { modelSelector } from './selectors/models.selector';
+import { regionSelector } from './selectors/regions.selector';
 
-export type TModelSelector = (rawData: TCatalogDto) => TModelEntity;
+export type TUseCatalogOptionsArg = {
+  selector?: TUseCatalogSelector;
+  enabled?: boolean;
+};
+export type TUseCatalogSelector = 'modelSelector' | 'regionSelector' | void;
 
-const getSizeUnit = (size: number) => (size < 1000 ? 'Go' : 'To');
+type TSelectorData<T> = T extends 'modelSelector'
+  ? ReturnType<typeof modelSelector>
+  : T extends 'regionSelector'
+  ? ReturnType<typeof regionSelector>
+  : TCatalogDto;
 
-const mapModelCpu = (cpu: TCpuDto): TCpu => {
-  const { type, ...rest } = cpu;
-  return rest;
+export const updateCatalogQueryData = (
+  queryClient: QueryClient,
+  projectId: string,
+  regionName: string,
+) => {
+  queryClient.setQueryData(
+    instancesQueryKey(projectId, ['catalog']),
+    (prev: TCatalogDto | undefined): TCatalogDto | undefined =>
+      prev
+        ? {
+            ...prev,
+            regions: prev.regions.map((prevRegion) =>
+              prevRegion.name === regionName
+                ? { ...prevRegion, isActivated: true }
+                : prevRegion,
+            ),
+          }
+        : prev,
+  );
 };
 
-const mapModelMemory = (memory: TMemoryDto): TMemory => ({
-  size: memory.size,
-  unit: getSizeUnit(memory.size),
-});
-
-const mapModelGpu = ({ model, number }: TGpuDto): TGpu => ({
-  model,
-  number,
-});
-
-const mapModelStorages = (
-  storages: DeepReadonly<TDiskDto[]>,
-): DeepReadonly<TStorage[]> =>
-  storages.map((storage) => ({
-    ...storage,
-    sizeUnit: getSizeUnit(storage.capacity),
-  }));
-
-const mapModelSpecifications = (
-  specifications: DeepReadonly<TSpecificationsDto>,
-): DeepReadonly<{ specifications: TModelSpecification }> => ({
-  specifications: {
-    memory: mapModelMemory(specifications.memory),
-    cpu: mapModelCpu(specifications.cpu),
-    bandwidth: specifications.bandwidth.level,
-    storage: mapModelStorages(specifications.storage.disks),
-    gpu: mapModelGpu(specifications.gpu),
-  },
-});
-
-const mapModelPricings = (
-  pricings: DeepReadonly<TPricingDto[]>,
-): DeepReadonly<{ pricings: TModelPricing[] }> => ({
-  pricings: pricings
-    .reduce(
-      (acc: DeepReadonly<TPricingDto[]>, cur: DeepReadonly<TPricingDto>) => {
-        if (!acc.length) return [cur];
-
-        const foundPricingByInterval = acc.find(
-          (elt) => elt.interval === cur.interval,
-        );
-
-        if (!foundPricingByInterval) return [...acc, cur];
-        if (foundPricingByInterval.price < cur.price) return acc;
-        return [
-          ...acc.filter((elt) => elt.price !== foundPricingByInterval.price),
-          cur,
-        ];
-      },
-      [] as TPricingDto[],
-    )
-    .map(({ regions, osType, ...rest }) => rest),
-});
-
-const mapModelsData = (
-  models: DeepReadonly<TModelDto[]>,
-): DeepReadonly<TModel[]> =>
-  models.map(
-    ({
-      category,
-      name,
-      isNew,
-      compatibleLocalzone,
-      compatibleRegion,
-      banners,
-      pricings,
-      specifications,
-    }) => ({
-      category,
-      name,
-      isNew,
-      compatibleLocalzone,
-      compatibleRegion,
-      banners,
-      ...mapModelPricings(pricings),
-      ...mapModelSpecifications(specifications),
-    }),
-  );
-
-const sortModels = (models: DeepReadonly<TModel[]>) =>
-  models
-    .slice()
-    .sort((a, b) => {
-      const aGroup = Number((/\d+/.exec(a.name) || [])[0]);
-      const bGroup = Number((/\d+/.exec(b.name) || [])[0]);
-      const aRank = Number((/-([^-]+)$/.exec(a.name) || [])[1]);
-      const bRank = Number((/-([^-]+)$/.exec(b.name) || [])[1]);
-      return aGroup === bGroup ? aRank - bRank : bGroup - aGroup;
-    })
-    .sort(
-      (a, b) => Number(b.compatibleLocalzone) - Number(a.compatibleLocalzone),
-    )
-    .sort((a, b) => Number(b.isNew) - Number(a.isNew));
-
-export const modelSelector: TModelSelector = (rawData) => ({
-  models: {
-    categories: rawData.categories,
-    data: sortModels(mapModelsData(rawData.models)),
-  },
-});
-
-export const useCatalog = (projectId: string, select: TModelSelector) => {
+export const useCatalog = <T extends TUseCatalogSelector = void>(
+  projectId: string,
+  { selector, enabled }: TUseCatalogOptionsArg = {},
+) => {
+  const selectedModel = useAppStore.getState().modelName();
   const queryKey = useMemo(() => instancesQueryKey(projectId, ['catalog']), [
     projectId,
   ]);
+
   const fetchCatalog = useCallback(() => getCatalog(projectId), [projectId]);
+
+  const catalogSelector = useCallback(
+    (rawData: TCatalogDto): TSelectorData<T> => {
+      switch (selector) {
+        case 'modelSelector':
+          return modelSelector(rawData) as TSelectorData<T>;
+        case 'regionSelector': {
+          return regionSelector(rawData, selectedModel) as TSelectorData<T>;
+        }
+        default:
+          return rawData as TSelectorData<T>;
+      }
+    },
+    [selector, selectedModel],
+  );
 
   return useQuery({
     queryKey,
@@ -148,6 +74,7 @@ export const useCatalog = (projectId: string, select: TModelSelector) => {
     refetchOnWindowFocus: false,
     queryFn: fetchCatalog,
     placeholderData: keepPreviousData,
-    select: useCallback(select, [select]),
+    ...(selector !== undefined && { select: catalogSelector }),
+    ...(enabled !== undefined && { enabled }),
   });
 };
