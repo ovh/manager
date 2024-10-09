8b82a085a066bb369954886a596244f82ec49ae2
Florian Renaut <florian.renaut@corp.ovh.com>
2024-10-09 17:19:04 +0200

    feat(pci-load-balancer): add instance table component
    ref: DTCORE-2667
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/modules/manager-pci-common/src/api/hook/index.ts
+++ packages/manager/modules/manager-pci-common/src/api/hook/index.ts
@@ -2,6 +2,7 @@ export * from './useApiSchema';
 export * from './useAvailability';
 export * from './useCatalog';
 export * from './useFlavors';
+export * from './useInstance';
 export * from './useLogs';
 export * from './useProject';
 export * from './useRegions';
