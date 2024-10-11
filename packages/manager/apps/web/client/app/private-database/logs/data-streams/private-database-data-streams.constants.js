6c1b8d84ea02f19d52e9daff3edee32093a05658
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2024-10-11 17:26:22 +0200

    feat(web): add private-database logs to customer module
    ref: MANAGER-15356
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

---
+++ packages/manager/apps/web/client/app/private-database/logs/data-streams/private-database-data-streams.constants.js
@@ -0,0 +1,9 @@
+// waiting for tracking PR
+export const PRIVATE_DATABASE_DATA_STREAMS_TRACKING_HITS = {
+  LISTING_PAGE: ``,
+  ADD_DATA_STREAM: ``,
+  GO_BACK: ``,
+  LDP_DETAIL: ``,
+  SUBSCRIBE: ``,
+  UNSUBSCRIBE: ``,
+};
---
+++ packages/manager/apps/web/client/app/private-database/logs/private-database-logs.constants.js
@@ -0,0 +1,49 @@
+export const PRIVATE_DATABASE_LOGS_SERVICE_GUIDE_LINK = {
+  DEFAULT:
+    'https://help.ovhcloud.com/csm/en-web-cloud-db-retrieve-logs?id=kb_article_view&sysparm_article=KB0062040',
+  FR:
+    'https://help.ovhcloud.com/csm/fr-web-cloud-db-retrieve-logs?id=kb_article_view&sysparm_article=KB0062007',
+  DE:
+    'https://help.ovhcloud.com/csm/de-web-cloud-db-retrieve-logs?id=kb_article_view&sysparm_article=KB0062033',
+  ES:
+    'https://help.ovhcloud.com/csm/es-es-web-cloud-db-retrieve-logs?id=kb_article_view&sysparm_article=KB0062037',
+  WE:
+    'https://help.ovhcloud.com/csm/en-ie-web-cloud-db-retrieve-logs?id=kb_article_view&sysparm_article=KB0062032',
+  IE:
+    'https://help.ovhcloud.com/csm/en-ie-web-cloud-db-retrieve-logs?id=kb_article_view&sysparm_article=KB0062032',
+  IT:
+    'https://help.ovhcloud.com/csm/it-web-cloud-db-retrieve-logs?id=kb_article_view&sysparm_article=KB0062041',
+  PL:
+    'https://help.ovhcloud.com/csm/pl-web-cloud-db-retrieve-logs?id=kb_article_view&sysparm_article=KB0062043',
+  PT:
+    'https://help.ovhcloud.com/csm/pt-web-cloud-db-retrieve-logs?id=kb_article_view&sysparm_article=KB0062035',
+  GB:
+    'https://help.ovhcloud.com/csm/en-gb-web-cloud-db-retrieve-logs?id=kb_article_view&sysparm_article=KB0062031',
+  CA:
+    'https://help.ovhcloud.com/csm/en-ca-web-cloud-db-retrieve-logs?id=kb_article_view&sysparm_article=KB0062034',
+  QC:
+    'https://help.ovhcloud.com/csm/fr-ca-web-cloud-db-retrieve-logs?id=kb_article_view&sysparm_article=KB0062008',
+  WS:
+    'https://help.ovhcloud.com/csm/es-web-cloud-db-retrieve-logs?id=kb_article_view&sysparm_article=KB0062042',
+  AU:
+    'https://help.ovhcloud.com/csm/en-au-web-cloud-db-retrieve-logs?id=kb_article_view&sysparm_article=KB0062036',
+  SG:
+    'https://help.ovhcloud.com/csm/en-sg-web-cloud-db-retrieve-logs?id=kb_article_view&sysparm_article=KB0062039',
+  ASIA:
+    'https://help.ovhcloud.com/csm/asia-web-cloud-db-retrieve-logs?id=kb_article_view&sysparm_article=KB0062038',
+};
+
+export const PRIVATE_DATABASE_LOGS_KINDS_KEYS = {
+  dbms: ['message'],
+};
+
+// TODO waiting for tracking PR
+export const PRIVATE_DATABASE_LOGS_TRACKING_HITS = {
+  LOGS_PAGE: ``,
+  TRANSFER: ``,
+  CREATE_ACCOUNT: ``,
+  CREATE_DATA_STREAM: ``,
+  SUBSCRIBE_OTHER_ACCOUNT: ``,
+  GRAYLOG_WATCH: ``,
+  STOP_TRANSFER: ``,
+};
