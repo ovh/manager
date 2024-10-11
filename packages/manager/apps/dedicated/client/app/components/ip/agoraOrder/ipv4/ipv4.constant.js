94a6e0f1ec524ffab35d0f583a1237e1c8fc7dc7
Sachin Ramesh <104337442+sachinrameshn@users.noreply.github.com>
2024-10-11 12:59:45 +0530

    feat(dedicated.ip): allow parking ip as destination (#12509)
    ref: MANAGER-14569
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>
    Co-Authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/apps/dedicated/client/app/components/ip/agoraOrder/ipv4/ipv4.constant.js
+++ packages/manager/apps/dedicated/client/app/components/ip/agoraOrder/ipv4/ipv4.constant.js
@@ -20,6 +20,10 @@ export const PRODUCT_TYPES = {
     apiTypeName: 'VPS',
     typeName: 'VPS',
   },
+  parking: {
+    apiTypeName: 'parking',
+    typeName: 'parking',
+  },
 };
 
 export const IP_AGORA = {
@@ -67,6 +71,56 @@ export const BLOCK_ADDITIONAL_IP = 'Block Additional IP';
 
 export { TRACKING_PREFIX, ADDITIONAL_IP, ALERT_ID, IP_FAILOVER_PLANCODE };
 
+export const DATACENTER_TO_COUNTRY = {
+  PAR: 'fr',
+  GRA: 'fr',
+  SBG: 'fr',
+  LIM: 'de',
+  WAW: 'pl',
+  ERI: 'uk',
+  VIN: 'us',
+  HIL: 'us',
+  BHS: 'ca',
+  SGP: 'sg',
+  SYD: 'au',
+  RBX: 'fr',
+  YYZ: 'ca',
+  YNM: 'in',
+  CR2: 'fr',
+};
+
+export const DATACENTER_TO_REGION = {
+  RBX: 'eu-west-rbx',
+  GRA: 'eu-west-gra',
+  SBG: 'eu-west-sbg',
+  PAR: 'eu-west-par',
+  CR2: 'labeu-west-1-preprod',
+  LIM: 'eu-west-lim',
+  WAW: 'eu-central-waw',
+  ERI: 'eu-west-eri',
+  BHS: 'ca-east-bhs',
+  YYZ: 'ca-east-tor',
+  SGP: 'ap-southeast-sgp',
+  SYD: 'ap-southeast-syd',
+  YNM: 'ap-south-mum',
+  VIN: 'us-east-vin',
+  HIL: 'us-west-hil',
+};
+
+export const IP_LOCATION_GROUPS_BASED_ON_DATACENTER = [
+  {
+    labels: ['APAC/CANADA', 'CANADA - ASIA'],
+    countries: ['au', 'ca', 'sg', 'in'],
+    datacenter: ['SYD', 'BHS', 'YYZ', 'YNM', 'SGP'],
+  },
+  {
+    labels: ['EUROPE'],
+    countries: ['de', 'fr', 'pl', 'uk'],
+    datacenter: ['RBX', 'GRA', 'LIM', 'CR2', 'PAR', 'SBG', 'WAW', 'ERI'],
+  },
+  { labels: ['USA'], countries: ['us'], datacenter: ['HIL', 'VIN'] },
+];
+
 export default {
   FETCH_PRICE_MAX_TRIES,
   IP_LOCATION_GROUPS,
@@ -77,4 +131,7 @@ export default {
   IP_FAILOVER_PLANCODE,
   BLOCK_ADDITIONAL_IP,
   ALERT_ID,
+  DATACENTER_TO_COUNTRY,
+  DATACENTER_TO_REGION,
+  IP_LOCATION_GROUPS_BASED_ON_DATACENTER,
 };
