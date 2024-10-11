a26c68d0c0738ee4312135a5bad3860b18dec575
Frédéric Vilcot <fredericvilcot@gmail.com>
2024-10-11 16:06:43 +0200

    feat(pci-instances): add tabs & tiles components
    ref:TAPC-1898
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

---
+++ packages/manager/apps/pci-instances/src/components/tab/TabContentWrapper.component.tsx
@@ -0,0 +1,26 @@
+import { ODS_TEXT_LEVEL, ODS_TEXT_SIZE } from '@ovhcloud/ods-components';
+import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
+import { OsdsText } from '@ovhcloud/ods-components/react';
+import { FC, PropsWithChildren } from 'react';
+import { DeepReadonly } from '@/types/utils.type';
+
+export type TTabContentWrapperProps = DeepReadonly<{
+  description?: string;
+}>;
+
+export const TabContentWrapper: FC<PropsWithChildren<
+  TTabContentWrapperProps
+>> = ({ description, children }) => (
+  <div className="p-6 pt-8">
+    {description && (
+      <OsdsText
+        color={ODS_THEME_COLOR_INTENT.text}
+        level={ODS_TEXT_LEVEL.body}
+        size={ODS_TEXT_SIZE._400}
+      >
+        {description}
+      </OsdsText>
+    )}
+    {children}
+  </div>
+);
