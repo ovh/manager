4f13356a9063159b139a5e950ab3e354ed3a5099
Frédéric Vilcot <fredericvilcot@gmail.com>
2024-10-11 16:11:46 +0200

    feat(pci-instances): update appStore slices
    ref:TAPC-1898
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

--- packages/manager/apps/pci-instances/src/data/hooks/region/useActivateRegion.ts
+++ packages/manager/apps/pci-instances/src/data/hooks/region/useActivateRegion.ts
@@ -3,6 +3,7 @@ import { useCallback } from 'react';
 import { DeepReadonly } from '@/types/utils.type';
 import { activateRegion } from '@/data/api/region';
 import { TActivatedRegionDto } from '@/types/region/api.types';
+import { instancesQueryKey } from '@/utils';
 
 export type TUseActivateRegionCallbacks = DeepReadonly<{
   onSuccess?: (region: string) => void;
@@ -13,6 +14,11 @@ export const useActivateRegion = (
   projectId: string,
   { onError, onSuccess }: TUseActivateRegionCallbacks = {},
 ) => {
+  const mutationKey = instancesQueryKey(projectId, [
+    'creation',
+    'region',
+    'activate',
+  ]);
   const mutationFn = useCallback(
     (region: string) => activateRegion(projectId, region),
     [projectId],
@@ -26,6 +32,7 @@ export const useActivateRegion = (
   );
 
   const mutation = useMutation<TActivatedRegionDto, unknown, string, unknown>({
+    mutationKey,
     mutationFn,
     onError,
     onSuccess: handleSuccess,
--- packages/manager/apps/pci-instances/src/store/slices/form.slice.ts
+++ packages/manager/apps/pci-instances/src/store/slices/form.slice.ts
@@ -1,7 +1,13 @@
 import { StateCreator } from 'zustand';
 
+export type TRegionItem = {
+  name: string;
+  datacenter: string;
+};
+
 export type TForm = {
   modelName: string | null;
+  region: TRegionItem | null;
 };
 
 export type TState = {
@@ -11,17 +17,20 @@ export type TState = {
 // Computed
 export type TQuery = {
   modelName: () => string | null;
+  region: () => TRegionItem | null;
 };
 
 // Handlers
 export type TCommand = {
   setModelName: (newName: string) => void;
+  setRegion: (newRegion: TRegionItem | null) => void;
 };
 
 export type TFormSlice = TState & TCommand & TQuery;
 
 const intialForm: TForm = {
   modelName: null,
+  region: null,
 };
 
 export const createFormSlice: StateCreator<TFormSlice, [], [], TFormSlice> = (
@@ -33,5 +42,10 @@ export const createFormSlice: StateCreator<TFormSlice, [], [], TFormSlice> = (
     set((state) => ({
       form: { ...state.form, modelName: newName },
     })),
+  setRegion: (newRegion) =>
+    set((state) => ({
+      form: { ...state.form, region: newRegion },
+    })),
   modelName: () => get().form.modelName,
+  region: () => get().form.region,
 });
