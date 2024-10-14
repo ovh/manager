46aa74d87916a3d02c8098d228a56cbf1386c6e6
Omar <omaralkabous@gmail.com>
2024-10-14 11:14:11 +0200

    feat(procedures): implement gdpr form introduction (#13491)
    ref: MANAGER-15315
    
    Signed-off-by: Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>

--- packages/manager/apps/procedures/src/pages/rgdp/RGDP.page.tsx
+++ packages/manager/apps/procedures/src/pages/rgdp/RGDP.page.tsx
@@ -1,28 +1,11 @@
-import {
-  ODS_THEME_COLOR_INTENT,
-  ODS_THEME_TYPOGRAPHY_LEVEL,
-} from '@ovhcloud/ods-common-theming';
-import { OsdsText } from '@ovhcloud/ods-components/react';
-import { ODS_TEXT_SIZE } from '@ovhcloud/ods-components';
-import { useTranslation } from 'react-i18next';
-import { Outlet } from 'react-router-dom';
 import React from 'react';
 import { PageLayout } from '@/components/PageLayout/PageLayout.component';
+import { RGDPIntroduction } from './rgdpIntroduction/RGDPIntroduction.component';
 
 export default function RGDP() {
-  const { t } = useTranslation('rgdp');
-
   return (
     <PageLayout>
-      <OsdsText
-        color={ODS_THEME_COLOR_INTENT.info}
-        level={ODS_THEME_TYPOGRAPHY_LEVEL.heading}
-        className="block mb-6 text-center"
-        size={ODS_TEXT_SIZE._500}
-      >
-        {t('rgdp-title')}
-      </OsdsText>
-      <Outlet />
+      <RGDPIntroduction />
     </PageLayout>
   );
 }
