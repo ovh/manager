8917dab96e5d4fdbb22919305fc72c58ad36a3b7
Omar <omaralkabous@gmail.com>
2024-10-14 11:14:11 +0200

    feat(procedures): implement gdpr form introduction (#13491)
    ref: MANAGER-15315
    
    Signed-off-by: Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>

---
+++ packages/manager/apps/procedures/src/pages/rgdp/rgdpIntroduction/RGDPIntroduction.component.test.tsx
@@ -0,0 +1,46 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, vi } from 'vitest';
+import React from 'react';
+import { RGDPIntroduction } from './RGDPIntroduction.component';
+
+vi.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    t: vi.fn((key) => key),
+  }),
+  Trans: ({ i18nKey }: any) => <div>{i18nKey}</div>,
+}));
+
+vi.mock('@ovhcloud/ods-components/react', () => ({
+  OsdsText: ({ children, color, level, size, className }: any) => (
+    <div data-testid="osds-text" color={color} className={className}>
+      {children}
+    </div>
+  ),
+}));
+
+describe('RGDPIntroduction Component', () => {
+  it('renders the component correctly', () => {
+    render(<RGDPIntroduction />);
+
+    const titleElement = screen.getByText('rgdp_introduction_title');
+    expect(titleElement).toBeInTheDocument();
+
+    const purposeElement = screen.getByText(
+      'rgdp_introduction_content_purpose',
+    );
+    expect(purposeElement).toBeInTheDocument();
+
+    const identityVerificationElement = screen.getByText(
+      'rgdp_introduction_content_identity_verification',
+    );
+    expect(identityVerificationElement).toBeInTheDocument();
+
+    const clientAreaElement = screen.getByText(
+      'rgdp_introduction_content_client_area',
+    );
+    expect(clientAreaElement).toBeInTheDocument();
+
+    const noteElement = screen.getByText('rgdp_introduction_please_note');
+    expect(noteElement).toBeInTheDocument();
+  });
+});
