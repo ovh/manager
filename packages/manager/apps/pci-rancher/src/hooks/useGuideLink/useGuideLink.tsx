d8baf1dea8b5508e8d3aa02b466d6f2de93c09fe
Eric Ciccotti <eric.ciccotti.ext@corp.ovh.com>
2024-10-15 11:09:47 +0200

    feat(pci-rancher): add additional guides links
    ref: TAPC-675
    Signed-off-by: Eric Ciccotti <eric.ciccotti.ext@corp.ovh.com>

--- packages/manager/apps/pci-rancher/src/hooks/useGuideLink/useGuideLink.tsx
+++ packages/manager/apps/pci-rancher/src/hooks/useGuideLink/useGuideLink.tsx
@@ -52,6 +52,100 @@ const GUIDE_LIST: { [guideName: string]: Partial<GuideLinks> } = {
     WS:
       'https://help.ovhcloud.com/csm/es-public-cloud-managed-rancher-service-getting-started?id=kb_article_view&sysparm_article=KB0061900',
   },
+  MANAGED_RANCHER_SERVICE_CREATION: {
+    DEFAULT:
+      'https://help.ovhcloud.com/csm/en-public-cloud-managed-rancher-service-creation?id=kb_article_view&sysparm_article=KB0064191',
+    ASIA:
+      'https://help.ovhcloud.com/csm/asia-public-cloud-managed-rancher-service-creation?id=kb_article_view&sysparm_article=KB0064184',
+    AU:
+      'https://help.ovhcloud.com/csm/en-au-public-cloud-managed-rancher-service-creation?id=kb_article_view&sysparm_article=KB0064190',
+    CA:
+      'https://help.ovhcloud.com/csm/en-ca-public-cloud-managed-rancher-service-creation?id=kb_article_view&sysparm_article=KB0064197',
+    DE:
+      'https://help.ovhcloud.com/csm/de-public-cloud-managed-rancher-service-creation?id=kb_article_view&sysparm_article=KB0064194',
+    ES:
+      'https://help.ovhcloud.com/csm/es-es-public-cloud-managed-rancher-service-creation?id=kb_article_view&sysparm_article=KB0064193',
+    EU:
+      'https://help.ovhcloud.com/csm/en-ie-public-cloud-managed-rancher-service-creation?id=kb_article_view&sysparm_article=KB0064183',
+    FR:
+      'https://help.ovhcloud.com/csm/fr-public-cloud-managed-rancher-service-creation?id=kb_article_view&sysparm_article=KB0064198',
+    GB:
+      'https://help.ovhcloud.com/csm/en-gb-public-cloud-managed-rancher-service-creation?id=kb_article_view&sysparm_article=KB0064189',
+    IE:
+      'https://help.ovhcloud.com/csm/en-ie-public-cloud-managed-rancher-service-creation?id=kb_article_view&sysparm_article=KB0064183',
+    IN:
+      'https://help.ovhcloud.com/csm/asia-public-cloud-managed-rancher-service-creation?id=kb_article_view&sysparm_article=KB0064184',
+    IT:
+      'https://help.ovhcloud.com/csm/it-public-cloud-managed-rancher-service-creation?id=kb_article_view&sysparm_article=KB0064195',
+    MA:
+      'https://help.ovhcloud.com/csm/fr-public-cloud-managed-rancher-service-creation?id=kb_article_view&sysparm_article=KB0064198',
+    NL:
+      'https://help.ovhcloud.com/csm/en-nl-documentation-public-cloud?id=kb_browse_cat&kb_id=574a8325551974502d4c6e78b7421938',
+    PL:
+      'https://help.ovhcloud.com/csm/pl-public-cloud-managed-rancher-service-creation?id=kb_article_view&sysparm_article=KB0064187',
+    PT:
+      'https://help.ovhcloud.com/csm/pt-public-cloud-managed-rancher-service-creation?id=kb_article_view&sysparm_article=KB0064196',
+    QC:
+      'https://help.ovhcloud.com/csm/fr-ca-public-cloud-managed-rancher-service-creation?id=kb_article_view&sysparm_article=KB0064186',
+    SG:
+      'https://help.ovhcloud.com/csm/en-sg-public-cloud-managed-rancher-service-creation?id=kb_article_view&sysparm_article=KB0064192',
+    SN:
+      'https://help.ovhcloud.com/csm/fr-public-cloud-managed-rancher-service-creation?id=kb_article_view&sysparm_article=KB0064198',
+    TN:
+      'https://help.ovhcloud.com/csm/fr-public-cloud-managed-rancher-service-creation?id=kb_article_view&sysparm_article=KB0064198',
+    US: 'https://us.ovhcloud.com/support',
+    WE:
+      'https://help.ovhcloud.com/csm/en-public-cloud-managed-rancher-service-creation?id=kb_article_view&sysparm_article=KB0064191',
+    WS:
+      'https://help.ovhcloud.com/csm/es-public-cloud-managed-rancher-service-creation?id=kb_article_view&sysparm_article=KB0064185',
+  },
+  MANAGED_RANCHER_SERVICE_CREATION_USERS_PROJECTS: {
+    DEFAULT:
+      'https://help.ovhcloud.com/csm/en-public-cloud-managed-rancher-service-managing-users-projects?id=kb_article_view&sysparm_article=KB0064217',
+    ASIA:
+      'https://help.ovhcloud.com/csm/asia-public-cloud-managed-rancher-service-managing-users-projects?id=kb_article_view&sysparm_article=KB0064215',
+    AU:
+      'https://help.ovhcloud.com/csm/en-au-public-cloud-managed-rancher-service-managing-users-projects?id=kb_article_view&sysparm_article=KB0064218',
+    CA:
+      'https://help.ovhcloud.com/csm/en-ca-public-cloud-managed-rancher-service-managing-users-projects?id=kb_article_view&sysparm_article=KB0064214',
+    DE:
+      'https://help.ovhcloud.com/csm/de-public-cloud-managed-rancher-service-managing-users-projects?id=kb_article_view&sysparm_article=KB0064212',
+    ES:
+      'https://help.ovhcloud.com/csm/es-es-public-cloud-managed-rancher-service-managing-users-projects?id=kb_article_view&sysparm_article=KB0064221',
+    EU:
+      'https://help.ovhcloud.com/csm/en-ie-public-cloud-managed-rancher-service-managing-users-projects?id=kb_article_view&sysparm_article=KB0064216',
+    FR:
+      'https://help.ovhcloud.com/csm/fr-public-cloud-managed-rancher-service-managing-users-projects?id=kb_article_view&sysparm_article=KB0064222',
+    GB:
+      'https://help.ovhcloud.com/csm/en-gb-public-cloud-managed-rancher-service-managing-users-projects?id=kb_article_view&sysparm_article=KB0064225',
+    IE:
+      'https://help.ovhcloud.com/csm/en-ie-public-cloud-managed-rancher-service-managing-users-projects?id=kb_article_view&sysparm_article=KB0064216',
+    IN:
+      'https://help.ovhcloud.com/csm/asia-public-cloud-managed-rancher-service-managing-users-projects?id=kb_article_view&sysparm_article=KB0064215',
+    IT:
+      'https://help.ovhcloud.com/csm/it-public-cloud-managed-rancher-service-managing-users-projects?id=kb_article_view&sysparm_article=KB0064220',
+    MA:
+      'https://help.ovhcloud.com/csm/fr-public-cloud-managed-rancher-service-managing-users-projects?id=kb_article_view&sysparm_article=KB0064222',
+    NL:
+      'https://help.ovhcloud.com/csm/en-nl-documentation-public-cloud?id=kb_browse_cat&kb_id=574a8325551974502d4c6e78b7421938',
+    PL:
+      'https://help.ovhcloud.com/csm/pl-public-cloud-managed-rancher-service-managing-users-projects?id=kb_article_view&sysparm_article=KB0064226',
+    PT:
+      'https://help.ovhcloud.com/csm/pt-public-cloud-managed-rancher-service-managing-users-projects?id=kb_article_view&sysparm_article=KB0064227',
+    QC:
+      'https://help.ovhcloud.com/csm/fr-ca-public-cloud-managed-rancher-service-managing-users-projects?id=kb_article_view&sysparm_article=KB0064223',
+    SG:
+      'https://help.ovhcloud.com/csm/en-sg-public-cloud-managed-rancher-service-managing-users-projects?id=kb_article_view&sysparm_article=KB0064224',
+    SN:
+      'https://help.ovhcloud.com/csm/fr-public-cloud-managed-rancher-service-managing-users-projects?id=kb_article_view&sysparm_article=KB0064222',
+    TN:
+      'https://help.ovhcloud.com/csm/fr-public-cloud-managed-rancher-service-managing-users-projects?id=kb_article_view&sysparm_article=KB0064222',
+    US: 'https://us.ovhcloud.com/support',
+    WE:
+      'https://help.ovhcloud.com/csm/en-public-cloud-managed-rancher-service-managing-users-projects?id=kb_article_view&sysparm_article=KB0064217',
+    WS:
+      'https://help.ovhcloud.com/csm/es-public-cloud-managed-rancher-service-managing-users-projects?id=kb_article_view&sysparm_article=KB0064219',
+  },
 };
 
 type GetGuideLinkProps = {
