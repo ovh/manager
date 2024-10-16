4cc0cb2c4aa4090a16c990fac2f59cd49ae7074d
Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>
2024-10-16 09:10:56 +0200

    feat(procedures): rgpd add legal notice component
    ref: MANAGER-15376
    
    Signed-off-by: Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>

--- packages/manager/apps/procedures/src/pages/rgdp/RGDP.page.tsx
+++ packages/manager/apps/procedures/src/pages/rgdp/RGDP.page.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { PageLayout } from '@/components/PageLayout/PageLayout.component';
 import { RGDPIntroduction } from './rgdpIntroduction/RGDPIntroduction.component';
 import { RGDPForm } from './rgdpForm/RGDPForm.component';
+import { LegalInformations } from '@/components/legalInformations/LegalInformations.component';
 
 export default function RGDP() {
   return (
     <PageLayout>
       <RGDPIntroduction />
       <RGDPForm />
+      <LegalInformations
+        translationNamespace="rgdp"
+        informationTranslationKey="rgdp_legal_information"
+        policyTanslationKey="rgdp_legal_information_policy"
+      />
     </PageLayout>
   );
 }
