e805c5b2bba5f563658db20d6c0dfaf03de5e294
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2024-10-16 15:54:55 +0200

    feat(hpc-vmware-managed-vcd): add service details contacts
      ref: MANAGER-14751
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

---
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/components/tiles/organization-service-tile/renew-tile-item/ServiceRenewTileItem.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  Description,
+  useServiceDetails,
+} from '@ovh-ux/manager-react-components';
+import { OsdsSkeleton } from '@ovhcloud/ods-components/react';
+import useCurrentUser from '@/hooks/user/useCurrentUser';
+
+export default function ServiceRenewTileItem({ id }: { id: string }) {
+  const { data: serviceDetails, isLoading, isError } = useServiceDetails({
+    resourceName: id,
+  });
+  const { dateTimeFormat } = useCurrentUser();
+  const nextBillingDate = serviceDetails?.data?.billing?.nextBillingDate;
+
+  if (isLoading) return <OsdsSkeleton />;
+  if (isError || !nextBillingDate) return <Description>-</Description>;
+
+  return (
+    <Description>
+      {dateTimeFormat?.format(new Date(nextBillingDate))}
+    </Description>
+  );
+}
