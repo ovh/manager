a86957d4401bf2f409c4136f74fcdfddcd94a48a
David Arsène <david.arsene.ext@ovhcloud.com>
2024-10-16 12:45:21 +0200

    fix(dedicated.pcc): fix dashboard display PCC for VCD migration
    ref: MANAGER-15583 MANAGER-15584
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/managedBaremetal/dashboard-light/index.js
+++ packages/manager/apps/dedicated/client/app/managedBaremetal/dashboard-light/index.js
@@ -6,7 +6,7 @@ const moduleName = 'managedBaremetalDashboardLightLazyloading';
 
 angular.module(moduleName, ['oc.lazyLoad', 'ui.router']).config(
   /* @ngInject */ ($stateProvider) => {
-    $stateProvider.state('app.managedBaremetal.details.dashboard.light.**', {
+    $stateProvider.state('app.managedBaremetal.details.dashboard-light.**', {
       url: '/migration-vcd',
       lazyLoad: ($transition$) => {
         const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
