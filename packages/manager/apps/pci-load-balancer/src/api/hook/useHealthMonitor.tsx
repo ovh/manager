db26d41fcce279a7b3165091e5a5ce0933714c36
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2024-10-16 15:38:49 +0200

    feat(pci-load-balancer): create health monitor section
    DTCORE-2655 health minitor datagrid
    DTCORE-2656 health monitor edition
    DTCORE-2657 health monitor deletion
    DTCORE-2658 health monitor creation
    
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

---
+++ packages/manager/apps/pci-load-balancer/src/api/hook/useHealthMonitor.tsx
@@ -0,0 +1,122 @@
+import { useMutation, useQuery } from '@tanstack/react-query';
+import {
+  createHealthMonitor,
+  deleteHealthMonitor,
+  editHealthMonitor,
+  getHealthMonitor,
+  THealthMonitorFormState,
+} from '../data/health-monitor';
+import queryClient from '@/queryClient';
+
+export const useGetHealthMonitor = ({
+  projectId,
+  region,
+  poolId,
+}: {
+  projectId: string;
+  region: string;
+  poolId: string;
+}) =>
+  useQuery({
+    queryKey: ['health-monitor', projectId, region, poolId],
+    queryFn: () => getHealthMonitor(projectId, region, poolId),
+    select: (data) => data[0],
+    throwOnError: true,
+  });
+
+type DeleteHealthMonitorProps = {
+  projectId: string;
+  region: string;
+  onError: (cause: Error) => void;
+  onSuccess: () => void;
+};
+
+export const useDeleteHealthMonitor = ({
+  projectId,
+  region,
+  onError,
+  onSuccess,
+}: DeleteHealthMonitorProps) => {
+  const mutation = useMutation({
+    mutationFn: async (healthMonitorId: string) =>
+      deleteHealthMonitor(projectId, region, healthMonitorId),
+    onError,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: ['health-monitor', projectId, region],
+      });
+      onSuccess();
+    },
+  });
+  return {
+    deleteHealthMonitor: (healthMonitorId: string) =>
+      mutation.mutate(healthMonitorId),
+    ...mutation,
+  };
+};
+
+type CreateHealthMonitorProps = {
+  projectId: string;
+  region: string;
+  poolId: string;
+  onError: (cause: Error) => void;
+  onSuccess: () => void;
+};
+
+export const useCreateHealthMonitor = ({
+  projectId,
+  region,
+  poolId,
+  onError,
+  onSuccess,
+}: CreateHealthMonitorProps) => {
+  const mutation = useMutation({
+    mutationFn: async (model: THealthMonitorFormState) =>
+      createHealthMonitor(projectId, region, poolId, model),
+    onError,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: ['health-monitor', projectId, region],
+      });
+      onSuccess();
+    },
+  });
+  return {
+    createHealthMonitor: (model: THealthMonitorFormState) =>
+      mutation.mutate(model),
+    ...mutation,
+  };
+};
+
+type EditHealthMonitorProps = {
+  projectId: string;
+  region: string;
+  healthMonitorId: string;
+  onError: (cause: Error) => void;
+  onSuccess: () => void;
+};
+
+export const useEditHealthMonitor = ({
+  projectId,
+  region,
+  healthMonitorId,
+  onError,
+  onSuccess,
+}: EditHealthMonitorProps) => {
+  const mutation = useMutation({
+    mutationFn: async (model: THealthMonitorFormState) =>
+      editHealthMonitor(projectId, region, healthMonitorId, model),
+    onError,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: ['health-monitor', projectId, region],
+      });
+      onSuccess();
+    },
+  });
+  return {
+    editHealthMonitor: (model: THealthMonitorFormState) =>
+      mutation.mutate(model),
+    ...mutation,
+  };
+};
