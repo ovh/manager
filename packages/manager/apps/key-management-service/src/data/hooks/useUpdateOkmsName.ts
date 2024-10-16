8f5bdc13d816501e4e8cc67215f6b1c8ad4633c2
vovh <155978769+vovh@users.noreply.github.com>
2024-10-16 15:39:58 +0200

    fix(kms): fix side effect on whole kms app with refetchInterval okms (#13520)
    ref: MANAGER-15170
    
    Signed-off-by: Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>

--- packages/manager/apps/key-management-service/src/data/hooks/useUpdateOkmsName.ts
+++ packages/manager/apps/key-management-service/src/data/hooks/useUpdateOkmsName.ts
@@ -1,14 +1,25 @@
 import { ApiError, ApiResponse } from '@ovh-ux/manager-core-api';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import React from 'react';
-import { getOkmsServicesResourceListQueryKey } from '../api/okms';
+import { useTranslation } from 'react-i18next';
+import { useNotifications } from '@ovh-ux/manager-react-components';
+import {
+  getOkmsResourceQueryKey,
+  getOkmsServicesResourceListQueryKey,
+} from '../api/okms';
 import {
   updateOkmsNameQueryKey,
   getOkmsServiceIdQueryKey,
   getOkmsServiceId,
   updateOkmsName,
 } from '../api/okmsService';
+import { getKMSServiceInfosQueryKey } from './useKMSServiceInfos';
 
+export type UpdateOkmsParams = {
+  okmsId: string;
+  onSuccess: () => void;
+  onError?: (result: ApiError) => void;
+};
 export type UpdateOkmsNameMutationParams = {
   /** Okms service id */
   okms: string;
@@ -20,14 +31,13 @@ export type UpdateOkmsNameMutationParams = {
  * Get the function to mutate a okms Services
  */
 export const useUpdateOkmsName = ({
+  okmsId,
   onSuccess,
   onError,
-}: {
-  onSuccess?: () => void;
-  onError?: (result: ApiError) => void;
-}) => {
-  const [isErrorVisible, setIsErrorVisible] = React.useState(false);
+}: UpdateOkmsParams) => {
   const queryClient = useQueryClient();
+  const { t } = useTranslation('key-management-service/dashboard');
+  const { addError, addSuccess, clearNotifications } = useNotifications();
 
   const {
     mutate: updateKmsName,
@@ -39,19 +49,33 @@ export const useUpdateOkmsName = ({
       const { data: servicesId } = await queryClient.fetchQuery<
         ApiResponse<number[]>
       >({
-        queryKey: getOkmsServiceIdQueryKey({ okms }),
-        queryFn: () => getOkmsServiceId({ okms }),
+        queryKey: getOkmsServiceIdQueryKey(okms),
+        queryFn: () => getOkmsServiceId(okms),
       });
       return updateOkmsName({ serviceId: servicesId[0], displayName });
     },
-    onSuccess: () => {
-      queryClient.invalidateQueries({
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
         queryKey: getOkmsServicesResourceListQueryKey,
       });
+      await queryClient.invalidateQueries({
+        queryKey: getKMSServiceInfosQueryKey(okmsId),
+      });
+      await queryClient.invalidateQueries({
+        queryKey: getOkmsResourceQueryKey(okmsId),
+      });
+      clearNotifications();
+      addSuccess(t('key_management_service_update_name_success_banner'), true);
       onSuccess?.();
     },
     onError: (result: ApiError) => {
-      setIsErrorVisible(true);
+      clearNotifications();
+      addError(
+        t('key_management_service_update_name_error_banner', {
+          error: result.message,
+        }),
+        true,
+      );
       onError?.(result);
     },
   });
@@ -59,8 +83,6 @@ export const useUpdateOkmsName = ({
   return {
     updateKmsName,
     isPending,
-    isErrorVisible: updateNameError && isErrorVisible,
     error: updateNameError,
-    hideError: () => setIsErrorVisible(false),
   };
 };
