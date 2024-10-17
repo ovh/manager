9d112efd052399cf7352ef4836a4fc2922b4bdd9
Omar <omaralkabous@gmail.com>
2024-10-17 10:41:07 +0200

    feat(procedures): rgpd add legal notice component (#13611)
    ref: MANAGER-15376
    
    Signed-off-by: Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>

--- packages/manager/apps/procedures/src/pages/disableMFA/create/Create.spec.tsx
+++ packages/manager/apps/procedures/src/pages/disableMFA/create/Create.spec.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { describe, expect, it, vi } from 'vitest';
 import { render } from '@testing-library/react';
 import Create from '@/pages/disableMFA/create/Create.page';
-import { LegalPolicyLinkByLanguage } from '@/constants';
 
 const user = {
   legalForm: 'other',
@@ -10,39 +9,22 @@ const user = {
   language: 'en_CA',
 };
 
-vi.mock('react-i18next', () => ({
-  useTranslation: () => ({
-    t: (translationKey: string, args: any) =>
-      `${translationKey} ${JSON.stringify(args)}`,
-    i18n: {
-      language: user.language,
-    },
-  }),
-}));
-
 vi.mock('@/context/User/useUser', () => ({
   default: () => ({
     user,
   }),
 }));
 
-describe('Create.page', () => {
-  it.each([
-    ['FR', 'anything', LegalPolicyLinkByLanguage.FR],
-    ['CA', 'en_CA', LegalPolicyLinkByLanguage.CA.en],
-    ['CA', 'fr_CA', LegalPolicyLinkByLanguage.CA.fr],
-    ['CA', 'fr_FR', LegalPolicyLinkByLanguage.CA.fr],
-    ['CA', 'en_IN', LegalPolicyLinkByLanguage.CA.en],
-    ['unknown', 'anything', LegalPolicyLinkByLanguage.DEFAULT],
-  ])(
-    'should have the correct link for %s user speaking %s',
-    async (sub, locale, result) => {
-      user.subsidiary = sub;
-      user.language = locale;
-      const { getByTestId } = render(<Create />);
+vi.mock('@/components/legalInformations/LegalInformations.component', () => ({
+  LegalInformations: () => <div>2FALegalInformations</div>,
+}));
 
-      const legalInformationContent = getByTestId('legal_information_content');
-      expect(legalInformationContent.innerHTML.includes(result)).toBe(true);
-    },
-  );
+describe('Create.page', () => {
+  it('renders the component correctly', () => {
+    const { getByText } = render(<Create />);
+
+    const legalElement = getByText('2FALegalInformations');
+
+    expect(legalElement).toBeInTheDocument();
+  });
 });
