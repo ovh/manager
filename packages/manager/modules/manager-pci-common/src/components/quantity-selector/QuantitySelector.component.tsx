4160d9fb154d4ce10b513b685bfe5cf762fcd395
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2024-10-17 15:01:32 +0200

    feat(pci-load-balancer): add rename health monitor modal
    DTCORE-2771
    
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/modules/manager-pci-common/src/components/quantity-selector/QuantitySelector.component.tsx
+++ packages/manager/modules/manager-pci-common/src/components/quantity-selector/QuantitySelector.component.tsx
@@ -23,6 +23,7 @@ import { useTranslation } from 'react-i18next';
 import React, { useEffect, useState } from 'react';
 
 import '../../translations/quantity-selector';
+import { clsx } from 'clsx';
 
 export interface QuantitySelectorProps {
   label?: string;
@@ -33,6 +34,7 @@ export interface QuantitySelectorProps {
   min?: number;
   max?: number;
   className?: string;
+  contentClassName?: string;
 }
 
 export function QuantitySelector({
@@ -44,6 +46,7 @@ export function QuantitySelector({
   min,
   max,
   className,
+  contentClassName,
 }: Readonly<QuantitySelectorProps>) {
   const { t } = useTranslation('pci-quantity-selector');
   const [, setCounter] = useState(0);
@@ -60,84 +63,85 @@ export function QuantitySelector({
 
   return (
     <OsdsFormField className={className} inline>
-      <div slot="label" className="flex gap-2">
-        {label && (
+      <div className={contentClassName}>
+        <div slot="label" className="flex gap-2">
+          {label && (
+            <OsdsText
+              color={ODS_THEME_COLOR_INTENT.text}
+              size={ODS_THEME_TYPOGRAPHY_SIZE._200}
+            >
+              {label}
+            </OsdsText>
+          )}
+          {labelHelpText && (
+            <OsdsPopover className="w-4 h-4">
+              <OsdsIcon
+                slot="popover-trigger"
+                name={ODS_ICON_NAME.HELP}
+                size={ODS_ICON_SIZE.xxs}
+                className="cursor-help"
+                color={ODS_THEME_COLOR_INTENT.text}
+              />
+              <OsdsPopoverContent>{labelHelpText}</OsdsPopoverContent>
+            </OsdsPopover>
+          )}
+        </div>
+
+        {description && (
           <OsdsText
             color={ODS_THEME_COLOR_INTENT.text}
-            size={ODS_THEME_TYPOGRAPHY_SIZE._200}
+            size={ODS_THEME_TYPOGRAPHY_SIZE._400}
+            className="mb-6"
           >
-            {label}
+            {description}
           </OsdsText>
         )}
-        {labelHelpText && (
-          <OsdsPopover className="w-4 h-4">
+        <OsdsQuantity>
+          <OsdsButton
+            slot="minus"
+            variant={ODS_BUTTON_VARIANT.flat}
+            color={ODS_THEME_COLOR_INTENT.primary}
+            size={ODS_BUTTON_SIZE.sm}
+            text-align="center"
+            disabled={min !== undefined && value - 1 < min ? true : undefined}
+          >
             <OsdsIcon
-              slot="popover-trigger"
-              name={ODS_ICON_NAME.HELP}
-              size={ODS_ICON_SIZE.xxs}
-              className="cursor-help"
-              color={ODS_THEME_COLOR_INTENT.text}
+              name={ODS_ICON_NAME.MINUS}
+              size={ODS_ICON_SIZE.sm}
+              className="bg-white"
             />
-            <OsdsPopoverContent>{labelHelpText}</OsdsPopoverContent>
-          </OsdsPopover>
-        )}
+          </OsdsButton>
+          <OsdsInput
+            type={ODS_INPUT_TYPE.number}
+            value={value}
+            color={
+              isValid
+                ? ODS_THEME_COLOR_INTENT.primary
+                : ODS_THEME_COLOR_INTENT.error
+            }
+            onOdsValueChange={(event) => {
+              forceRedraw();
+              onValueChange(Number(event.detail.value));
+            }}
+            min={min}
+            max={max}
+          />
+          <OsdsButton
+            slot="plus"
+            variant={ODS_BUTTON_VARIANT.flat}
+            color={ODS_THEME_COLOR_INTENT.primary}
+            size={ODS_BUTTON_SIZE.sm}
+            text-align="center"
+            disabled={max !== undefined && value + 1 > max ? true : undefined}
+          >
+            <OsdsIcon
+              className="bg-white"
+              name={ODS_ICON_NAME.PLUS}
+              size={ODS_ICON_SIZE.xs}
+            />
+          </OsdsButton>
+        </OsdsQuantity>
       </div>
-
-      {description && (
-        <OsdsText
-          color={ODS_THEME_COLOR_INTENT.text}
-          size={ODS_THEME_TYPOGRAPHY_SIZE._400}
-          className="mb-6"
-        >
-          {description}
-        </OsdsText>
-      )}
-      <OsdsQuantity>
-        <OsdsButton
-          slot="minus"
-          variant={ODS_BUTTON_VARIANT.flat}
-          color={ODS_THEME_COLOR_INTENT.primary}
-          size={ODS_BUTTON_SIZE.sm}
-          text-align="center"
-          disabled={min !== undefined && value - 1 < min ? true : undefined}
-        >
-          <OsdsIcon
-            name={ODS_ICON_NAME.MINUS}
-            size={ODS_ICON_SIZE.sm}
-            className="bg-white"
-          />
-        </OsdsButton>
-        <OsdsInput
-          type={ODS_INPUT_TYPE.number}
-          value={value}
-          color={
-            isValid
-              ? ODS_THEME_COLOR_INTENT.primary
-              : ODS_THEME_COLOR_INTENT.error
-          }
-          onOdsValueChange={(event) => {
-            forceRedraw();
-            onValueChange(Number(event.detail.value));
-          }}
-          min={min}
-          max={max}
-        />
-        <OsdsButton
-          slot="plus"
-          variant={ODS_BUTTON_VARIANT.flat}
-          color={ODS_THEME_COLOR_INTENT.primary}
-          size={ODS_BUTTON_SIZE.sm}
-          text-align="center"
-          disabled={max !== undefined && value + 1 > max ? true : undefined}
-        >
-          <OsdsIcon
-            className="bg-white"
-            name={ODS_ICON_NAME.PLUS}
-            size={ODS_ICON_SIZE.xs}
-          />
-        </OsdsButton>
-      </OsdsQuantity>
-
       {error.min && (
         <OsdsText color={ODS_THEME_COLOR_INTENT.error} slot="helper">
           {t('common_field_error_min', { min })}
