68e963157aecd9f4035a10df351cba9fd5661e17
Arthur Bullet <arthur.bullet@ovhcloud.com>
2024-10-17 19:01:16 +0200

    feat(ai.notebooks): notebooks order funnel part 2
    ref:DATATR-1548
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>

--- packages/manager/apps/pci-ai-notebooks/src/pages/notebooks/create/_components/OrderFunnel.component.tsx
+++ packages/manager/apps/pci-ai-notebooks/src/pages/notebooks/create/_components/OrderFunnel.component.tsx
@@ -283,7 +283,6 @@ const OrderFunnel = ({
                 )}
               />
             </section>
-
             <section id="framework">
               <h4 className="mb-2">{t('fieldCaracteristicLabel')}</h4>
               <FormField
