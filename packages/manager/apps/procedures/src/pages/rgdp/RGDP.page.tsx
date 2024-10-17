72f3c15a1304adfdeaa388e00a6b1a2d0d7e9d31
Omar <omaralkabous@gmail.com>
2024-10-17 10:34:38 +0200

    feat(procedures): implemented gdpr form (#13558)
    ref:MANAGER-15317
    
    Signed-off-by: Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>

--- packages/manager/apps/procedures/src/pages/rgdp/RGDP.page.tsx
+++ packages/manager/apps/procedures/src/pages/rgdp/RGDP.page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { PageLayout } from '@/components/PageLayout/PageLayout.component';
 import { RGDPIntroduction } from './rgdpIntroduction/RGDPIntroduction.component';
+import { RGDPForm } from './rgdpForm/RGDPForm.component';
 
 export default function RGDP() {
   return (
     <PageLayout>
       <RGDPIntroduction />
+      <RGDPForm />
     </PageLayout>
   );
 }
