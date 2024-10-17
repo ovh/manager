f0c6ec878925339f8b40105d458b95071fc7f192
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2024-10-17 17:44:02 +0530

    fix(*): use translation component for the banners
    ref: PRB0040912
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

--- packages/manager/apps/pci-ssh-keys/src/pages/AddSshPage.tsx
+++ packages/manager/apps/pci-ssh-keys/src/pages/AddSshPage.tsx
@@ -1,11 +1,10 @@
 import { useNavigate, useParams } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
+import { Translation } from 'react-i18next';
 import { useNotifications } from '@ovh-ux/manager-react-components';
 import AddSshModal from '@/components/ssh-keys/AddSshModal';
 
 export default function AddSshPage() {
   const { projectId } = useParams();
-  const { t } = useTranslation('common');
   const { addError, addSuccess } = useNotifications();
   const navigate = useNavigate();
   const onClose = () => {
@@ -16,17 +15,27 @@ export default function AddSshPage() {
       projectId={`${projectId}`}
       onClose={() => onClose()}
       onSuccess={() => {
-        addSuccess(t('pci_projects_project_sshKeys_add_success'), true);
+        addSuccess(
+          <Translation ns="common">
+            {(t) => t('pci_projects_project_sshKeys_add_success')}
+          </Translation>,
+          true,
+        );
       }}
       onError={(error: Error) => {
         addError(
-          <span
-            dangerouslySetInnerHTML={{
-              __html: t('pci_projects_project_sshKeys_add_error', {
-                error: error?.message,
-              }),
-            }}
-          ></span>,
+          <Translation ns="common">
+            {(t) => (
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: t('pci_projects_project_sshKeys_add_error', {
+                    error: error?.message,
+                  }),
+                }}
+              ></span>
+            )}
+          </Translation>,
+          true,
         );
       }}
     />
