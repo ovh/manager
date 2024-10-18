302c16c01961822cf464a45b6b155a707a01c3bb
Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>
2024-10-18 17:49:57 +0200

    fix(account): change display popup tracking value
    ref: MANAGER-15213
    
    Signed-off-by: Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/account/identity-documents/user-identity-documents.constant.js
+++ packages/manager/apps/dedicated/client/app/account/identity-documents/user-identity-documents.constant.js
@@ -291,7 +291,7 @@ export const TRACKING_TASK_TAG = {
   dashboard: `${TRACKING_PREFIX}::identity-files::dashboard`,
   openDetailView: `${TRACKING_PREFIX}::tile::button::{{name_click}}_identity-files::{{identity-files}}`,
   clickSendMyDocuments: `${TRACKING_PREFIX}::page::button::submit_identity-files`,
-  displayPopUpSendMyDocuments: `${TRACKING_PREFIX}::pop-up::identity-files-sent`,
+  displayPopUpSendMyDocuments: `${TRACKING_PREFIX}::identity-files::pop-up::identity-files-sent`,
   confirmSendMyDocuments: `${TRACKING_PREFIX}::pop-up::button::identity-files-sent::confirm`,
 };
 
