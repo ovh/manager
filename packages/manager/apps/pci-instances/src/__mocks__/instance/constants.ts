0404ca77d77eaf5fb040c595e7c08f5544968e1d
Frédéric Vilcot <fredericvilcot@gmail.com>
2024-10-18 10:49:16 +0200

    test(pci-instances): add test suites for components
    ref:TAPC-1928
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

---
+++ packages/manager/apps/pci-instances/src/__mocks__/instance/constants.ts
@@ -0,0 +1,21 @@
+import { TInstance } from '@/data/hooks/instance/useInstances';
+
+export const mockedInstance: TInstance = {
+  id: '12345',
+  name: 'foo',
+  flavorId: '678910',
+  flavorName: 'b2-8',
+  imageId: '11121314',
+  imageName: 'linux',
+  region: 'BHS',
+  status: { severity: 'success', state: 'ACTIVE' },
+  addresses: new Map([
+    ['private', [{ ip: '123.000.00', version: 1, gatewayIp: '' }]],
+    ['public', [{ ip: '777.000.00', version: 2, gatewayIp: '' }]],
+  ]),
+};
+
+export const mockedInstanceWithEmptyRegion: TInstance = {
+  ...mockedInstance,
+  region: '',
+};
