28d28f21793bbd5a2543c0d1a0571ef1ea6d2177
Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>
2024-10-21 12:43:16 +0200

    feat(pci.kubernetes): add modal responsive
    ref: TAPC-33
    Signed-off-by: Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>

--- packages/manager/apps/pci-kubernetes/src/hooks/useResizeOsdsModal.ts
+++ packages/manager/apps/pci-kubernetes/src/hooks/useResizeOsdsModal.ts
@@ -1,9 +1,24 @@
 import { useEffect } from 'react';
+import { BreakPoints } from '@/types';
 
+// FIXME with next version of Ods 18
+/**
+ * Hook to make a modal responsive based on the screen size.
+ *
+ * @param {string} size - The maximum size of the modal when the screen width is greater than `BreakPoints.SM`.
+ *
+ * @example
+ * useResponsiveModal('500px');
+ */
 export const useResponsiveModal = (size) => {
   useEffect(() => {
+    /**
+     * Determines the styles to apply based on the screen width.
+     *
+     * @returns {Object} - The styles to apply.
+     */
     const getStyles = () => {
-      if (window.innerWidth > 768) {
+      if (window.innerWidth > BreakPoints.SM) {
         return {
           maxWidth: size,
           minWidth: 'auto',
@@ -14,12 +29,18 @@ export const useResponsiveModal = (size) => {
         minWidth: 'inherit',
       };
     };
-
+    /**
+     * Applies the styles to a DOM element.
+     *
+     * @param {HTMLElement} element - The DOM element to which the styles will be applied.
+     */
     const applyStyles = (element: HTMLElement) => {
       const styles = getStyles();
       Object.assign(element.style, styles);
     };
-
+    /**
+     * Mutation observer to detect changes in the DOM.
+     */
     const observer = new MutationObserver((mutations) => {
       mutations.forEach((mutation) => {
         if (mutation.type === 'childList' || mutation.type === 'attributes') {
--- packages/manager/apps/pci-kubernetes/src/types/index.ts
+++ packages/manager/apps/pci-kubernetes/src/types/index.ts
@@ -46,3 +46,11 @@ export enum UpdatePolicy {
   MinimalDowntime = 'MINIMAL_DOWNTIME',
   AlwaysUpdate = 'ALWAYS_UPDATE',
 }
+
+export enum BreakPoints {
+  XS = 0,
+  SM = 540,
+  MD = 720,
+  LG = 960,
+  XL = 1140,
+}
