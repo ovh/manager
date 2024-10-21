5d61653de277b643144740e5e06a311975486bc6
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2024-10-21 15:37:26 +0200

    refactor(hycu): move type in types to be compliance with other uapp
    ref: #NTNX-1
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/apps/hycu/src/pages/listing/Listing.page.tsx
+++ packages/manager/apps/hycu/src/pages/listing/Listing.page.tsx
@@ -30,7 +30,7 @@ import {
   useFormattedDate,
 } from '@ovh-ux/manager-react-components/src/hooks/date/useFormattedDate';
 import HYCU_CONFIG from '@/hycu.config';
-import { IHycuDetails } from '@/type/hycu.details.interface';
+import { IHycuDetails } from '@/types/hycu.details.interface';
 
 import { urls, subRoutes } from '@/routes/routes.constant';
 import HycuActionMenu from './menu/HycuActionMenu.component';
