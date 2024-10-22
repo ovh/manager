d1e6e4e0caefc1954dc8b88bf40acd40a8115c8e
cyril.biencourt <cyril.biencourt@corp.ovh.com>
2024-10-22 15:03:18 +0200

    fix(web.privatedatabase): properly stop logs polling
    Signed-off-by: cyril.biencourt <cyril.biencourt@corp.ovh.com>

--- packages/manager/apps/web/client/app/private-database/logs/private-database-logs.controller.js
+++ packages/manager/apps/web/client/app/private-database/logs/private-database-logs.controller.js
@@ -1,49 +1,49 @@
-angular.module('App').controller(
-  'PrivateDatabaseLogsCtrl',
-  class PrivateDatabaseLogsCtrl {
-    /* @ngInject */
-    constructor($scope, $stateParams, TailLogs, PrivateDatabaseLogsService) {
-      this.$scope = $scope;
-      this.$stateParams = $stateParams;
-      this.TailLogs = TailLogs;
-      this.privateDatabaseLogsService = PrivateDatabaseLogsService;
+export default class PrivateDatabaseLogsCtrl {
+  /* @ngInject */
+  constructor($scope, $stateParams, TailLogs, PrivateDatabaseLogsService) {
+    this.$scope = $scope;
+    this.$stateParams = $stateParams;
+    this.TailLogs = TailLogs;
+    this.privateDatabaseLogsService = PrivateDatabaseLogsService;
+    this.loader = true;
 
-      $scope.$on('$destroy', () => {
-        if (this.logger) {
-          this.logger.stop();
-        }
-      });
-    }
+    $scope.$on('$destroy', () => {
+      if (this.logger) {
+        this.logger.stop();
+      }
+    });
+  }
 
-    $onInit() {
-      this.productId = this.$stateParams.productId;
+  $onInit() {
+    this.loader = true;
+    this.productId = this.$stateParams.productId;
 
-      this.logger = new this.TailLogs({
-        source: () =>
-          this.privateDatabaseLogsService
-            .getLogs(this.productId)
-            .then((logs) => logs.url),
-        delay: 2000,
-      });
+    this.logger = new this.TailLogs({
+      source: () =>
+        this.privateDatabaseLogsService
+          .getLogs(this.productId)
+          .then((logs) => logs.url),
+      delay: 2000,
+    });
 
-      this.startLog();
-    }
+    this.startLog();
+    this.loader = false;
+  }
 
-    $onDestroy() {
-      this.logger.stop();
-    }
+  $onDestroy() {
+    this.logger.stop();
+  }
 
-    stopLog() {
-      this.logger.stop();
-    }
+  stopLog() {
+    this.logger.stop();
+  }
 
-    startLog() {
-      this.logger.log();
-    }
+  startLog() {
+    this.logger.log();
+  }
 
-    getLogs() {
-      this.logger = this.logger.logs;
-      return this.logger;
-    }
-  },
-);
+  getLogs() {
+    this.logger = this.logger.logs;
+    return this.logger;
+  }
+}
