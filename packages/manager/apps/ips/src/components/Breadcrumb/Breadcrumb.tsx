635799ae310977b8fdf416f44a27b27a52183597
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2024-10-23 13:22:12 +0200

    feat(ips): init ips react app
    ref: MANAGER-15657
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

---
+++ packages/manager/apps/ips/src/components/Breadcrumb/Breadcrumb.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { OsdsBreadcrumb } from '@ovhcloud/ods-components/react';
+import {
+  useBreadcrumb,
+  BreadcrumbItem,
+} from '@/hooks/breadcrumb/useBreadcrumb';
+import appConfig from '@/ips.config';
+
+export interface BreadcrumbProps {
+  customRootLabel?: string;
+  appName?: string;
+  items?: BreadcrumbItem[];
+}
+
+function Breadcrumb({ customRootLabel }: BreadcrumbProps): JSX.Element {
+  const label = customRootLabel || appConfig.rootLabel;
+
+  const breadcrumbItems = useBreadcrumb({
+    rootLabel: label,
+    appName: 'ips',
+  });
+  return <OsdsBreadcrumb items={breadcrumbItems} />;
+}
+
+export default Breadcrumb;
