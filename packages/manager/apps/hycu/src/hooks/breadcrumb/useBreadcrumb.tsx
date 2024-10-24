1cae1da11bab5f8f16730a5bebeb56c4c19be2df
Thibaud Crespin <thibaud.crespin.ext@ovhcloud.com>
2024-10-24 10:29:16 +0200

    fix(hycu): breadcrumb and order links   ref: MANAGER-15780
    Signed-off-by: Thibaud Crespin <thibaud.crespin.ext@ovhcloud.com>

--- packages/manager/apps/hycu/src/hooks/breadcrumb/useBreadcrumb.tsx
+++ packages/manager/apps/hycu/src/hooks/breadcrumb/useBreadcrumb.tsx
@@ -1,49 +1,51 @@
-import { useEffect, useState, useContext } from 'react';
-import { useLocation } from 'react-router-dom';
-import { ShellContext } from '@ovh-ux/manager-react-shell-client';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { urls } from '@/routes/routes.constant';
 
 export type BreadcrumbItem = {
+  id: string;
   label: string | undefined;
-  href?: string;
+  onClick?: (event?: BreadcrumbEvent) => void;
+};
+
+type BreadcrumbEvent = Event & {
+  target: { isCollapsed?: boolean; isLast?: boolean };
 };
 
 export interface BreadcrumbProps {
   rootLabel?: string;
-  appName?: string;
-  projectId?: string;
   items?: BreadcrumbItem[];
 }
 
-export const useBreadcrumb = ({ rootLabel, appName }: BreadcrumbProps) => {
-  const { shell } = useContext(ShellContext);
-  const [root, setRoot] = useState<BreadcrumbItem[]>([]);
+export const useBreadcrumb = ({ rootLabel, items }: BreadcrumbProps) => {
   const [paths, setPaths] = useState<BreadcrumbItem[]>([]);
   const location = useLocation();
+  const navigate = useNavigate();
   const pathnames = location.pathname.split('/').filter((x) => x);
 
-  useEffect(() => {
-    const fetchRoot = async () => {
-      try {
-        const response = await shell?.navigation.getURL(appName, '#/', {});
-        const rootItem = {
-          label: rootLabel,
-          href: String(response),
-        };
-        setRoot([rootItem]);
-      } catch {
-        // Fetch navigation error
-      }
-    };
-    fetchRoot();
-  }, [rootLabel, appName, shell?.navigation]);
+  const root: BreadcrumbItem = {
+    id: rootLabel,
+    label: rootLabel,
+    onClick: () => navigate(urls.root),
+  };
 
   useEffect(() => {
-    const pathsTab = pathnames.map((value) => ({
-      label: value,
-      href: `/#/${appName}/${value}`,
-    }));
+    const pathsTab = pathnames.map((value, index) => {
+      const item = items?.find(({ id }) => id === value);
+
+      return {
+        id: item?.id ?? value,
+        label: item?.label ?? value,
+        onClick: (event: BreadcrumbEvent) => {
+          const { isCollapsed, isLast } = event.target;
+          if (!isCollapsed && !isLast) {
+            navigate(`/${pathnames.slice(0, index + 1).join('/')}`);
+          }
+        },
+      };
+    });
     setPaths(pathsTab);
-  }, [location]);
+  }, [location, items]);
 
-  return [...root, ...paths];
+  return [root, ...paths];
 };
