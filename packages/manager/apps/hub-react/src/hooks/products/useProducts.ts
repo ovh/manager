2b914803dfaf87f12f7e9b7a517d665e95242fe1
David Arsène <david.arsene.ext@ovhcloud.com>
2024-10-24 18:10:33 +0200

    feat(hycu): add hycu service on hub and my services
    ref: MANAGER-14762
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/hub-react/src/hooks/products/useProducts.ts
+++ packages/manager/apps/hub-react/src/hooks/products/useProducts.ts
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { ShellContext } from '@ovh-ux/manager-react-shell-client';
 import { HubProduct, ProductList } from '@/types/services.type';
 import {
@@ -9,38 +9,41 @@ import {
 
 export const useProducts = (services: ProductList, expand = false) => {
   const { shell } = useContext(ShellContext);
-  let isLoading = true;
+  const [products, setProducts] = useState<HubProduct[]>([]);
   const servicesByProducts = services?.data || {};
   const productNames = Object.keys(servicesByProducts);
-  const products: HubProduct[] = productNames
-    .map((name: string & keyof typeof servicesByProducts) => {
-      const { application, hash } = productListingPages[name] || {
-        application: null,
-        hash: null,
-      };
-      return {
-        data: servicesByProducts[name].data.slice(
-          0,
-          DEFAULT_DISPLAYED_SERVICES,
-        ),
-        count: servicesByProducts[name].count,
-        type: name,
-        formattedType: name.toLowerCase().replace(/_/g, '-'),
-        // Link to product page should be done on the BFF side
-        link:
-          application && hash
-            ? (shell.navigation.getURL(application, hash, {}) as Promise<
-                string
-              >)
-            : null,
-      };
-    })
-    .sort((productA, productB) => productB.count - productA.count)
-    .slice(0, expand ? productNames.length : DEFAULT_DISPLAYED_PRODUCTS);
-  isLoading = false;
+
+  useEffect(() => {
+    const productsDisplayed: HubProduct[] = productNames
+      .map((name: string & keyof typeof servicesByProducts) => {
+        const { application, hash } = productListingPages[name] || {
+          application: null,
+          hash: null,
+        };
+
+        return {
+          data: servicesByProducts[name].data.slice(
+            0,
+            DEFAULT_DISPLAYED_SERVICES,
+          ),
+          count: servicesByProducts[name].count,
+          type: name,
+          formattedType: name.toLowerCase().replace(/_/g, '-'),
+          // Link to product page should be done on the BFF side
+          link:
+            application && hash
+              ? (shell.navigation.getURL(application, hash, {}) as Promise<
+                  string
+                >)
+              : Promise.resolve(null),
+        };
+      })
+      .sort((productA, productB) => productB.count - productA.count)
+      .slice(0, expand ? productNames.length : DEFAULT_DISPLAYED_PRODUCTS);
+    setProducts(productsDisplayed);
+  }, [expand]);
 
   return {
-    isLoading,
     products,
     canDisplayMore: productNames.length > DEFAULT_DISPLAYED_PRODUCTS,
   };
