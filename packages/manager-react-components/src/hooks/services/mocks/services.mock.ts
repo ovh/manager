1454b6b7d08098bb5d62b018bacfe9cebc060e54
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2024-10-28 10:38:02 +0100

    fix(manager-components): fix services type
    ref: ISSUE-13805
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager-react-components/src/hooks/services/mocks/services.mock.ts
+++ packages/manager-react-components/src/hooks/services/mocks/services.mock.ts
@@ -1,8 +1,12 @@
+import { CurrencyCode } from '../../../enumTypes';
+import { ServiceDetails } from '../services.type';
+
 export type GetServicesMocksParams = {
   getServicesKo?: boolean;
   getDetailsServicesKo?: boolean;
   updateServicesKo?: boolean;
   deleteServicesKo?: boolean;
+  serviceResponse?: ServiceDetails;
 };
 
 export const servicesMockErrors = {
@@ -12,11 +16,99 @@ export const servicesMockErrors = {
   getDetails: 'Get services details error',
 };
 
+export const defaultServiceResponse: ServiceDetails = {
+  route: {
+    path: '/api/path/{id}',
+    url: '/api/path/id',
+    vars: [
+      {
+        key: 'id',
+        value: 'id',
+      },
+    ],
+  },
+  billing: {
+    nextBillingDate: '2024-11-21T09:03:18Z',
+    expirationDate: '2024-11-21T09:03:18Z',
+    plan: {
+      code: 'code',
+      invoiceName: 'invoiceName',
+    },
+    pricing: {
+      capacities: ['renew'],
+      description: 'Installation pricing',
+      interval: 1,
+      duration: 'P1M',
+      minimumQuantity: 1,
+      maximumQuantity: null,
+      minimumRepeat: 1,
+      maximumRepeat: null,
+      price: { currencyCode: CurrencyCode.EUR, text: '0.00 €', value: 0 },
+      priceInUcents: 0,
+      pricingMode: 'default',
+      pricingType: 'rental',
+      engagementConfiguration: null,
+    },
+    group: null,
+    lifecycle: {
+      current: {
+        pendingActions: [],
+        terminationDate: null,
+        creationDate: '2024-10-21T09:03:18Z',
+        state: 'active',
+      },
+      capacities: {
+        actions: ['earlyRenewal', 'terminateAtExpirationDate'],
+      },
+    },
+    renew: {
+      current: {
+        mode: 'automatic',
+        nextDate: '2024-11-21T09:03:18Z',
+        period: 'P1M',
+      },
+      capacities: { mode: ['automatic', 'manual'] },
+    },
+    engagement: null,
+    engagementRequest: null,
+  },
+  resource: {
+    displayName: 'Test',
+    name: 'id-test',
+    state: 'active',
+    product: {
+      name: 'test',
+      description: 'description',
+    },
+    resellingProvider: null,
+  },
+  serviceId: 1234567890,
+  parentServiceId: null,
+  customer: {
+    contacts: [
+      {
+        customerCode: 'ls148374-ovh',
+        type: 'administrator',
+      },
+      {
+        customerCode: 'ls148374-ovh',
+        type: 'technical',
+      },
+      {
+        customerCode: 'pt299635-ovh',
+        type: 'billing',
+      },
+    ],
+  },
+  tags: [],
+};
+
 export const getServicesMocks = ({
   getServicesKo,
   getDetailsServicesKo,
   updateServicesKo,
   deleteServicesKo,
+  serviceResponse = defaultServiceResponse,
 }: GetServicesMocksParams): any[] => [
   {
     url: '/services/:id/terminate',
@@ -49,7 +141,7 @@ export const getServicesMocks = ({
         ? {
             message: servicesMockErrors.getDetails,
           }
-        : null,
+        : serviceResponse,
     status: getDetailsServicesKo ? 500 : 200,
     method: 'get',
     api: 'v6',
