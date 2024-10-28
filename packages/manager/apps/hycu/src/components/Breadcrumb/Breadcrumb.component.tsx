e327e1df3941d7c0154f022dfdd3fd8fbfa64ce1
Thibaud Crespin <thibaud.crespin.ext@ovhcloud.com>
2024-10-28 19:44:54 +0100

    fix(hycu): rootLabel in breadcrumb   ref: MANAGER-15803
    Signed-off-by: Thibaud Crespin <thibaud.crespin.ext@ovhcloud.com>

--- packages/manager/apps/hycu/src/components/Breadcrumb/Breadcrumb.component.tsx
+++ packages/manager/apps/hycu/src/components/Breadcrumb/Breadcrumb.component.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { OsdsBreadcrumb } from '@ovhcloud/ods-components/react';
+import { useTranslation } from 'react-i18next';
 import {
   useBreadcrumb,
   BreadcrumbItem,
 } from '@/hooks/breadcrumb/useBreadcrumb';
-import appConfig from '@/hycu.config';
 
 export interface BreadcrumbProps {
   customRootLabel?: string;
@@ -12,11 +12,11 @@ export interface BreadcrumbProps {
   items?: BreadcrumbItem[];
 }
 
-function Breadcrumb({ customRootLabel, items }: BreadcrumbProps): JSX.Element {
-  const label = customRootLabel || appConfig.rootLabel;
+function Breadcrumb({ items }: BreadcrumbProps): JSX.Element {
+  const { t } = useTranslation('hycu');
 
   const breadcrumbItems = useBreadcrumb({
-    rootLabel: label,
+    rootLabel: t('hycu_crumb'),
     items,
   });
   return <OsdsBreadcrumb items={breadcrumbItems} />;
