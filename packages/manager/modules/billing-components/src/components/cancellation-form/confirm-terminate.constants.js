fe3d201d8bb047099ee3cc7057a69444c22f686f
David Arsène <david.arsene.ext@ovhcloud.com>
2024-10-28 17:52:15 +0100

    feat(hycu): add agora termination for hycu service
    ref: MANAGER-14762
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/modules/billing-components/src/components/cancellation-form/confirm-terminate.constants.js
+++ packages/manager/modules/billing-components/src/components/cancellation-form/confirm-terminate.constants.js
@@ -6,12 +6,13 @@ export const SERVICE_WITH_AGORA_TERMINATION = [
   'vrack-services',
   'okms',
   'logs-account',
-  'logs-enterprise',
-  'logs-enterprise-hds',
 ];
+
+export const SERVICE_GROUP_WITH_AGORA_TERMINATION_REGEX = /hycu-vms-*|(logs-enterprise(-hds)?$)/;
 
 export default {
   SPECIAL_CONDITIONS_SUBSIDIARIES,
   TERMINATION_FORM_NAME,
   SERVICE_WITH_AGORA_TERMINATION,
+  SERVICE_GROUP_WITH_AGORA_TERMINATION_REGEX,
 };
