15af529f9daffc56019a9ffe75516ffafcb1121e
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2024-10-28 11:12:47 +0100

    feat(pci.private-network): isolate and remove unecessary function called
    ref: TAPC-1824
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

--- packages/manager/apps/pci-private-network/src/components/back-button/BackButton.component.tsx
+++ packages/manager/apps/pci-private-network/src/components/back-button/BackButton.component.tsx
@@ -1,17 +1,17 @@
+import { FC } from 'react';
 import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
 import { ODS_ICON_NAME, ODS_ICON_SIZE } from '@ovhcloud/ods-components';
 import { OsdsIcon, OsdsLink } from '@ovhcloud/ods-components/react';
 import { useTranslation } from 'react-i18next';
 import { useHref } from 'react-router-dom';
-import { useProject } from '@ovh-ux/manager-pci-common';
-import { useNotifications } from '@ovh-ux/manager-react-components';
-import { resetNetworks } from '@/queryClient';
 
-const BackButton: React.FC = () => {
+type BackButtonProps = {
+  onClick?: () => void;
+};
+
+const BackButton: FC<BackButtonProps> = ({ onClick }) => {
   const { t } = useTranslation('common');
   const backHref = useHref('..');
-  const { data: project } = useProject();
-  const { clearNotifications } = useNotifications();
 
   return (
     <OsdsLink
@@ -19,17 +19,14 @@ const BackButton: React.FC = () => {
       color={ODS_THEME_COLOR_INTENT.primary}
       className="mt-10"
       href={backHref}
-      onClick={async () => {
-        await resetNetworks(project.project_id);
-        clearNotifications();
-      }}
+      onClick={onClick}
     >
       <OsdsIcon
         slot="start"
         name={ODS_ICON_NAME.ARROW_LEFT}
         size={ODS_ICON_SIZE.xs}
         color={ODS_THEME_COLOR_INTENT.primary}
-      ></OsdsIcon>
+      />
       <span className="ml-4">
         {t('common_back_button_back_to_previous_page')}
       </span>
