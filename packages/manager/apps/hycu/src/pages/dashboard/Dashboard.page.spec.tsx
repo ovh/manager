f8140750090b4ed41c0626ca2602b75b4ed6d98a
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2024-10-28 17:44:41 +0100

    feat(hycu): add terminate modal page
    ref: MANAGER-14717
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/apps/hycu/src/pages/dashboard/Dashboard.page.spec.tsx
+++ packages/manager/apps/hycu/src/pages/dashboard/Dashboard.page.spec.tsx
@@ -1,15 +1,16 @@
 import { screen, waitFor } from '@testing-library/react';
 import { renderTestApp } from '@/utils/tests/renderTestApp';
 import '@testing-library/jest-dom';
+import { licensesHycu } from '@/mocks/licenseHycu/licenseHycu.data';
 
 describe('License Hycu Dashboard route test suite', () => {
   it('should show informations of services', async () => {
-    await renderTestApp('/4a26ef55-d46b-4b71-88c8-76ad71b154b4');
+    await renderTestApp(`/${licensesHycu[0].serviceName}`);
 
     await waitFor(
       () =>
         expect(
-          screen.getAllByText('425802fa-fb70-4b2a-9d5b-ec4de86bb40c')[0],
+          screen.getAllByText(licensesHycu[0].serviceName)[0],
         ).toBeVisible(),
       { timeout: 30_000 },
     );
@@ -18,7 +19,7 @@ describe('License Hycu Dashboard route test suite', () => {
   });
 
   it('should show error if api services fail', async () => {
-    await renderTestApp('/4a26ef55-d46b-4b71-88c8-76ad71b154b4', {
+    await renderTestApp(`/${licensesHycu[0].serviceName}`, {
       getServicesKo: true,
       isGetLicenseHycuKo: true,
       isGetServiceLicenseHycuKo: true,
