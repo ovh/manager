cb24ed8e8bcc702399cc972908f12cc521af1b00
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2024-10-29 12:32:59 +0100

    feat(hycu): add download license action
    ref: 14499
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/apps/hycu/src/pages/dashboard/general-information/GeneralInformationsTiles/GeneralInformationsTile.spec.tsx
+++ packages/manager/apps/hycu/src/pages/dashboard/general-information/GeneralInformationsTiles/GeneralInformationsTile.spec.tsx
@@ -1,9 +1,16 @@
-import { screen, waitFor } from '@testing-library/react';
-import { renderTestApp } from '@/utils/tests/renderTestApp';
 import '@testing-library/jest-dom';
+import { userEvent } from '@testing-library/user-event';
+import { act, screen, waitFor } from '@testing-library/react';
+import { vi, vitest } from 'vitest';
+import { renderTestApp } from '@/utils/tests/renderTestApp';
 import { labels } from '@/utils/tests/init.i18n';
 import { LicenseStatus } from '@/types/hycu.details.interface';
 import { licensesHycu } from '@/mocks/licenseHycu/licenseHycu.data';
+import { downloadTextAsFile } from '@/utils/downloadTextAsFile';
+import {
+  PREFIX_LICENSE_FILE_NAME,
+  SUFFIX_LICENSE_FILE_NAME,
+} from '@/constants';
 
 describe('License Hycu general informations tile for dashboard test suite', () => {
   it('should show informations of services', async () => {
@@ -98,5 +105,55 @@ describe('License Hycu general informations tile for dashboard test suite', () =
     expect(
       screen.queryByText(labels.dashboard.hycu_dashboard_download_license_file),
     ).not.toBeInTheDocument();
+  });
+
+  it('Can open download modal with IAM authorization', async () => {
+    vitest.mock('@/utils/downloadTextAsFile', () => ({
+      downloadTextAsFile: vi.fn(),
+    }));
+
+    const user = userEvent.setup();
+    await renderTestApp(`/${licensesHycu[0].serviceName}`, {
+      licenseStatus: LicenseStatus.ACTIVATED,
+    });
+
+    await waitFor(
+      () => {
+        expect(
+          screen.getByTestId('dashboard-license-download-link'),
+        ).not.toHaveAttribute('disabled');
+      },
+      { timeout: 30_000 },
+    );
+    await act(() =>
+      user.click(screen.getByTestId('dashboard-license-download-link')),
+    );
+
+    await waitFor(
+      () =>
+        expect(downloadTextAsFile).toHaveBeenCalledWith(
+          `${PREFIX_LICENSE_FILE_NAME}${licensesHycu[0].serviceName}${SUFFIX_LICENSE_FILE_NAME}`,
+          'my license content',
+        ),
+      { timeout: 20_000 },
+    );
+  });
+
+  it("Can't open download modal without IAM authorization", async () => {
+    await renderTestApp(`/${licensesHycu[1].serviceName}`, {
+      licenseStatus: LicenseStatus.ACTIVATED,
+    });
+
+    await waitFor(
+      () => {
+        expect(
+          screen.getByTestId('dashboard-license-download-link'),
+        ).toBeVisible();
+        expect(
+          screen.getByTestId('dashboard-license-download-link'),
+        ).toHaveAttribute('disabled');
+      },
+      { timeout: 30_000 },
+    );
   });
 });
--- packages/manager/apps/hycu/src/utils/iam.constants.ts
+++ packages/manager/apps/hycu/src/utils/iam.constants.ts
@@ -1,4 +1,5 @@
 export const IAM_ACTIONS = {
+  licenseHycuApiOvhGet: 'licenseHycu:apiovh:license/get',
   licenseHycuApiOvhActivate: 'licenseHycu:apiovh:activate',
   licenseHycuApiOvhRefresh: 'licenseHycu:apiovh:refresh',
   licenseHycuApiOvhServiceEdit: 'account:apiovh:service/edit',
