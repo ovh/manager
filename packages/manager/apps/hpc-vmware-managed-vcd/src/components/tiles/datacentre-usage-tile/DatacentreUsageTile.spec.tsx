b3a898453fe64cbf8ed1406e276b7e55f71f9cde
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2024-10-29 14:40:30 +0100

    feat(vcd)(post-ga): add VDC usage tile
      ref: MANAGER-15243
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

---
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/components/tiles/datacentre-usage-tile/DatacentreUsageTile.spec.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { vi } from 'vitest';
+import { render } from '@testing-library/react';
+import {
+  ODS_THEME_TYPOGRAPHY_LEVEL,
+  ODS_THEME_TYPOGRAPHY_SIZE,
+} from '@ovhcloud/ods-common-theming';
+import DatacentreUsageTile from './DatacentreUsageTile.component';
+import { datacentreList } from '../../../../mocks/vcd-organization/vcd-datacentre.mock';
+
+type TTileItem = {
+  label: HTMLElement;
+  value: HTMLElement;
+};
+
+const testVDC = datacentreList[0];
+
+vi.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    t: (key: string, options: Record<string, unknown>) => {
+      if (key === 'managed_vcd_vdc_vcpu_value') {
+        return `${options.speed} GHz`;
+      }
+      return key;
+    },
+  }),
+}));
+
+describe('DatacentreUsageTile component unit test suite', () => {
+  it('should define all sections with correct typo', () => {
+    // when
+    const { getByText } = render(
+      <DatacentreUsageTile vcdDatacentre={testVDC} />,
+    );
+
+    // then
+    const usageTitle = getByText('managed_vcd_vdc_usage');
+    expect(usageTitle).toBeVisible();
+
+    // and
+    const tileItems: TTileItem[] = [
+      {
+        label: getByText('managed_vcd_vdc_vcpu_speed'),
+        value: getByText(`${testVDC.currentState.vCPUSpeed} GHz`),
+      },
+      {
+        label: getByText('managed_vcd_vdc_vcpu_count'),
+        value: getByText(testVDC.currentState.vCPUCount),
+      },
+    ];
+
+    tileItems.forEach((item: TTileItem) => {
+      expect(item.label).toBeVisible();
+      expect(item.value).toBeVisible();
+
+      expect(item.label).toHaveAttribute(
+        'size',
+        ODS_THEME_TYPOGRAPHY_SIZE._200,
+      );
+      expect(item.label).toHaveAttribute(
+        'level',
+        ODS_THEME_TYPOGRAPHY_LEVEL.heading,
+      );
+    });
+  });
+});
