d2701f2f3759450c36b5b5a22139212c24cabad0
pauldkn <58977722+pauldkn@users.noreply.github.com>
2024-10-30 17:44:27 +0100

    feat(vcd)(post-ga): add VDC usage tile (#13831)
    ref: MANAGER-15243
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
    Co-authored-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

---
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/components/tiles/datacentre-usage-tile/DatacentreUsageTile.component.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import {
+  ActionMenu,
+  DashboardTile,
+  Description,
+} from '@ovh-ux/manager-react-components';
+import { ODS_ICON_NAME } from '@ovhcloud/ods-components';
+import IVcdDatacentre from '@/types/vcd-datacenter.interface';
+
+type TTileProps = {
+  vcdDatacentre: IVcdDatacentre;
+};
+
+export default function DatacentreUsageTile({
+  vcdDatacentre,
+}: Readonly<TTileProps>) {
+  const { t } = useTranslation('hpc-vmware-managed-vcd/datacentres');
+
+  return (
+    <div>
+      <DashboardTile
+        title={t('managed_vcd_vdc_usage')}
+        items={[
+          {
+            id: 'vcpuSpeed',
+            label: t('managed_vcd_vdc_vcpu_speed'),
+            value: (
+              <div className="flex items-center justify-between">
+                <Description>
+                  {t('managed_vcd_vdc_vcpu_value', {
+                    speed: vcdDatacentre?.currentState.vCPUSpeed,
+                  })}
+                </Description>
+                <ActionMenu
+                  items={[]}
+                  icon={ODS_ICON_NAME.ELLIPSIS_VERTICAL}
+                  isCompact
+                />
+              </div>
+            ),
+          },
+          {
+            id: 'vcpuCount',
+            label: t('managed_vcd_vdc_vcpu_count'),
+            value: (
+              <Description>{vcdDatacentre?.currentState.vCPUCount}</Description>
+            ),
+          },
+        ]}
+      />
+    </div>
+  );
+}
