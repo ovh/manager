529fa3686cc208476b9a29013f833280f3b75a7d
Sachin Ramesh <104337442+sachinrameshn@users.noreply.github.com>
2024-10-31 21:45:25 +0530

    feat(dedicated.ip): make parking as constant (#13610)
    ref: MANAGER-15660
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/ip/components/list/block/ip-ip-block.constant.js
+++ packages/manager/apps/dedicated/client/app/ip/components/list/block/ip-ip-block.constant.js
@@ -1,7 +1,9 @@
 export const ADDITIONAL_IP = 'Additional IP';
 export const DELETE_TRACKING_PREFIX = 'dedicated::ip::delete-additional-ip';
+export const IP_SERVICETYPE__PARK = 'IP Parking';
 
 export default {
   ADDITIONAL_IP,
   DELETE_TRACKING_PREFIX,
+  IP_SERVICETYPE__PARK,
 };
