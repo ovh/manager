8925e0227af70592e4ce6b74fba579ffeb85aecf
Mohammed Hamdoune <sidlynx@gmail.com>
2024-11-03 17:57:07 +0100

    fix(load-balancer): refactor create page to use ShapesInput
    ref: DTCORE-2806, DTCORE-2812
    Signed-off-by: Mohammed Hamdoune <sidlynx@gmail.com>

---
+++ packages/manager/apps/pci-load-balancer/src/pages/create/steps/region/components/GroupLabel.component.tsx
@@ -0,0 +1,30 @@
+import { OsdsText } from '@ovhcloud/ods-components/react';
+import { ODS_TEXT_LEVEL, ODS_TEXT_SIZE } from '@ovhcloud/ods-components';
+import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
+import { useTranslation } from 'react-i18next';
+
+export const GroupLabelComponent = ({
+  groupName,
+  isGroupSelected,
+  isMobile,
+}: {
+  groupName: string;
+  isGroupSelected: boolean;
+  isMobile: boolean;
+}) => {
+  const { t: tRegionsList } = useTranslation('regions-list');
+
+  const shouldBeBold = isGroupSelected && !isMobile;
+
+  return (
+    <div className="whitespace-nowrap">
+      <OsdsText
+        size={shouldBeBold ? ODS_TEXT_SIZE._500 : ODS_TEXT_SIZE._400}
+        level={ODS_TEXT_LEVEL.body}
+        color={ODS_THEME_COLOR_INTENT.text}
+      >
+        {groupName || tRegionsList('pci_project_regions_list_continent_all')}
+      </OsdsText>
+    </div>
+  );
+};
