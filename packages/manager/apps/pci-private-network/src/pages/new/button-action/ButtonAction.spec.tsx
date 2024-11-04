acb411a3bf4e09c9e5c92aa41f15862ba700c440
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2024-11-04 17:17:27 +0100

    feat(pci.private-network): handle private network creation
    ref: TAPC-1824
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

---
+++ packages/manager/apps/pci-private-network/src/pages/new/button-action/ButtonAction.spec.tsx
@@ -0,0 +1,91 @@
+import '@testing-library/jest-dom';
+import { describe, vi } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
+import { FormProvider, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { NewPrivateNetworkForm } from '@/types/private-network-form.type';
+import ButtonAction from './ButtonAction.component';
+import { NewPrivateNetworkWrapper } from '@/utils/test/test.provider';
+import { NEW_PRIVATE_NETWORK_FORM_SCHEMA } from '../new.constants';
+
+vi.mock('@ovh-ux/manager-pci-common', () => ({
+  useProject: vi.fn().mockReturnValue({ data: { project_id: 'project_id' } }),
+  isDiscoveryProject: vi.fn().mockReturnValue(false),
+}));
+
+vi.mock('react-router-dom', () => ({
+  useNavigate: () => ({ navigate: vi.fn() }),
+}));
+
+vi.mock('@/data/hooks/networks/useNetworks', () => ({
+  useCheckPrivateNetworkCreationStatus: () => ({ mutateAsync: vi.fn() }),
+}));
+
+describe('ButtonAction', () => {
+  it('should disabled button when region is not selected', () => {
+    render(<ButtonAction />, { wrapper: NewPrivateNetworkWrapper });
+    expect(screen.getByTestId('create-private-network')).toHaveAttribute(
+      'disabled',
+      'true',
+    );
+  });
+
+  it('should disabled button when name is empty', async () => {
+    await waitFor(() =>
+      render(<ButtonAction />, {
+        wrapper: ({ children }) => {
+          const form = useForm<NewPrivateNetworkForm>({
+            defaultValues: {
+              region: 'RBX-1',
+              isLocalZone: false,
+              subnet: {
+                cidr: '10.1.0.0/16',
+                enableDhcp: true,
+                enableGatewayIp: true,
+                ipVersion: 4,
+              },
+            },
+            resolver: zodResolver(NEW_PRIVATE_NETWORK_FORM_SCHEMA),
+          });
+
+          return <FormProvider {...form}>{children}</FormProvider>;
+        },
+      }),
+    );
+
+    expect(screen.getByTestId('create-private-network')).toHaveAttribute(
+      'disabled',
+      'true',
+    );
+  });
+
+  it('should disabled button when cidr is not valid', async () => {
+    await waitFor(() =>
+      render(<ButtonAction />, {
+        wrapper: ({ children }) => {
+          const form = useForm<NewPrivateNetworkForm>({
+            defaultValues: {
+              region: 'RBX-1',
+              name: 'test',
+              isLocalZone: false,
+              subnet: {
+                cidr: 'ds10.1.0.0/16',
+                enableDhcp: true,
+                enableGatewayIp: true,
+                ipVersion: 4,
+              },
+            },
+            resolver: zodResolver(NEW_PRIVATE_NETWORK_FORM_SCHEMA),
+          });
+
+          return <FormProvider {...form}>{children}</FormProvider>;
+        },
+      }),
+    );
+
+    expect(screen.getByTestId('create-private-network')).toHaveAttribute(
+      'disabled',
+      'true',
+    );
+  });
+});
