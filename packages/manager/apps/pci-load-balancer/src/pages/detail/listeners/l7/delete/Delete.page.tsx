ea88e6b6bafb0da6ad88b49fc833c3aad6813bdb
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2024-11-04 14:31:36 +0100

    fix(pci-load-balancer): fix error message iterpretation
    in l7 policies
    
    DTCORE-2828
    
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/apps/pci-load-balancer/src/pages/detail/listeners/l7/delete/Delete.page.tsx
+++ packages/manager/apps/pci-load-balancer/src/pages/detail/listeners/l7/delete/Delete.page.tsx
@@ -29,12 +29,17 @@ export default function DeletePage() {
     onError(error: ApiError) {
       addError(
         <Translation ns="load-balancer">
-          {(_t) =>
-            _t('octavia_load_balancer_global_error', {
-              message: error?.response?.data?.message || error?.message || null,
-              requestId: error?.config?.headers['X-OVH-MANAGER-REQUEST-ID'],
-            })
-          }
+          {(_t) => (
+            <span
+              dangerouslySetInnerHTML={{
+                __html: _t('octavia_load_balancer_global_error', {
+                  message:
+                    error?.response?.data?.message || error?.message || null,
+                  requestId: error?.config?.headers['X-OVH-MANAGER-REQUEST-ID'],
+                }),
+              }}
+            ></span>
+          )}
         </Translation>,
         true,
       );
