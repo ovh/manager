5b53ac6ac56c2f9a950b37db0b75b52389a0aa7d
stif59100 <steeve.vanderstocken@ovhcloud.com>
2024-11-05 17:53:03 +0100

    feat(web.office): add react uapp
    ref:MANAGER-15907
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

---
+++ packages/manager/apps/web-office/src/tracking.constant.ts
@@ -0,0 +1,20 @@
+export const LEVEL2 = {
+  EU: {
+    config: {
+      level2: '84',
+    },
+  },
+  CA: {
+    config: {
+      level2: '84',
+    },
+  },
+  US: {
+    config: {
+      level2: '84',
+    },
+  },
+};
+export const UNIVERSE = 'WebCloud';
+export const SUB_UNIVERSE = 'Web';
+export const APP_NAME = 'web-office-365';
