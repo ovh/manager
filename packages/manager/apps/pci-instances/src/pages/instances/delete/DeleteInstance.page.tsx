4ed58c1a5ecbb9fd96cce0b2dea621a4d7422cc8
Frédéric Vilcot <fredericvilcot@gmail.com>
2024-11-05 16:13:54 +0100

    feat(pci-instances): add delete modal & update actions
    ref: TAPC-2054
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

---
+++ packages/manager/apps/pci-instances/src/pages/instances/delete/DeleteInstance.page.tsx
@@ -0,0 +1,65 @@
+import { FC } from 'react';
+import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { useNotifications } from '@ovh-ux/manager-react-components';
+import { useDeleteInstance } from '@/data/hooks/instance/useDeleteInstance';
+import { updateDeletedInstanceStatus } from '@/data/hooks/instance/useInstances';
+import queryClient from '@/queryClient';
+import { DeleteModal } from './modal/DeleteModal.component';
+
+const DeleteInstance: FC = () => {
+  const { t } = useTranslation(['delete', 'common']);
+  const navigate = useNavigate();
+  const { projectId } = useParams() as { projectId: string };
+  const [searchParams] = useSearchParams();
+  const instanceId = searchParams.get('instanceId');
+  const instanceName = searchParams.get('instanceName');
+  const { addError, addSuccess } = useNotifications();
+
+  const handleModalClose = () => {
+    navigate('..');
+  };
+
+  const handleMutationSuccess = () => {
+    if (instanceId) {
+      updateDeletedInstanceStatus(queryClient, instanceId);
+    }
+    addSuccess(
+      t('pci_instances_delete_instance_success_message', {
+        name: instanceName,
+      }),
+      true,
+    );
+    handleModalClose();
+  };
+
+  const handleMutationError = () => {
+    addError(
+      t('pci_instances_delete_instance_error_message', { name: instanceName }),
+      true,
+    );
+    handleModalClose();
+  };
+
+  const { deleteInstance, isPending } = useDeleteInstance(projectId, {
+    onError: handleMutationError,
+    onSuccess: handleMutationSuccess,
+  });
+
+  const handleDeleteInstance = () => {
+    deleteInstance(instanceId as string);
+  };
+
+  if (!instanceId || !instanceName) return null;
+
+  return (
+    <DeleteModal
+      instanceName={instanceName}
+      isPending={isPending}
+      onModalClose={handleModalClose}
+      onModalConfirm={handleDeleteInstance}
+    />
+  );
+};
+
+export default DeleteInstance;
