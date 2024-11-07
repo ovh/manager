40fddef78b70e2e353dbdef1dc2137bda557d2de
Pierre-Philippe Prevost <ppprevost@aol.fr>
2024-11-07 17:33:14 +0100

    feat(pci.kubernetes): put network under management bloc (#13675)
    ref: TAPC-33
    
    Signed-off-by: Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/apps/pci-kubernetes/src/components/service/ClusterInformation.component.spec.tsx
+++ packages/manager/apps/pci-kubernetes/src/components/service/ClusterInformation.component.spec.tsx
@@ -1,149 +1,72 @@
-import { getByRole, render } from '@testing-library/react';
-import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
 import ClusterInformation from '@/components/service/ClusterInformation.component';
 import { TKube } from '@/types';
 
+const renderClusterInformation = (kubeDetail) =>
+  render(<ClusterInformation kubeDetail={kubeDetail} />);
+
 describe('ClusterInformation', () => {
-  it('renders cluster information title with correct text', () => {
-    const { getByText } = render(
-      <ClusterInformation
-        kubeDetail={
-          {
-            id: '1',
-            name: 'Cluster1',
-            status: 'READY',
-            version: '1.18',
-            attachedTo: 'Network1',
-            region: 'Region1',
-            nodesUrl: 'http://nodes.url',
-            privateNetworkConfiguration: null,
-          } as TKube
-        }
-      />,
-    );
-    expect(getByText(/kube_service_cluster_information/i)).toBeInTheDocument();
+  const kubeDetail = {
+    id: '1',
+    name: 'Cluster1',
+    status: 'READY',
+    version: '1.18',
+    attachedTo: 'Network1',
+    region: 'Region1',
+    customization: {
+      apiServer: {
+        admissionPlugins: {
+          disabled: ['NodeRestriction'],
+          enabled: ['AlwaysPullImages'],
+        },
+      },
+    },
+    nodesUrl: 'http://nodes.url',
+    privateNetworkConfiguration: null,
+    plugins: [
+      {
+        name: 'NodeRestriction',
+        state: 'disabled',
+        tip: 'node_restrictions_tip',
+      },
+      { name: 'AlwaysPullImages', state: 'enabled' },
+    ],
+  } as TKube;
+
+  it('renders cluster information correctly', () => {
+    renderClusterInformation(kubeDetail);
+
+    expect(
+      screen.getByText(/kube_service_cluster_information/i),
+    ).toBeInTheDocument();
+    expect(screen.getByText('Cluster1')).toBeInTheDocument();
+    expect(
+      screen.getByText('kube_service_cluster_status_READY'),
+    ).toBeInTheDocument();
+    expect(screen.getByText('1.18')).toBeInTheDocument();
+
+    expect(
+      screen.getByTestId('admission-plugin-chip AlwaysPullImages'),
+    ).toHaveProperty('color', ODS_THEME_COLOR_INTENT.success);
+    expect(
+      screen.getByTestId('admission-plugin-chip NodeRestriction'),
+    ).toHaveProperty('color', ODS_THEME_COLOR_INTENT.warning);
+    expect(screen.getByText('Region1')).toBeInTheDocument();
   });
 
   it('renders cluster ID with clipboard component', () => {
-    const { container } = render(
-      <ClusterInformation
-        kubeDetail={
-          {
-            id: 'id-1',
-            name: 'Cluster1',
-            status: 'READY',
-            version: '1.18',
-            attachedTo: 'Network1',
-            region: 'Region1',
-            nodesUrl: 'http://nodes.url',
-            privateNetworkConfiguration: null,
-          } as TKube
-        }
-      />,
-    );
-
+    const { container } = renderClusterInformation({
+      ...kubeDetail,
+      id: 'id-1',
+    });
     const clipboardElement = container.querySelector('[value="id-1"]');
     expect(clipboardElement).toBeVisible();
   });
 
-  it('renders cluster name correctly', () => {
-    const { getByText } = render(
-      <ClusterInformation
-        kubeDetail={
-          {
-            id: '1',
-            name: 'Cluster1',
-            status: 'READY',
-            version: '1.18',
-            attachedTo: 'Network1',
-            region: 'Region1',
-            nodesUrl: 'http://nodes.url',
-            privateNetworkConfiguration: null,
-          } as TKube
-        }
-      />,
-    );
-    expect(getByText('Cluster1')).toBeInTheDocument();
-  });
-
-  it('renders cluster status correctly', () => {
-    const { getByText } = render(
-      <ClusterInformation
-        kubeDetail={
-          {
-            id: '1',
-            name: 'Cluster1',
-            status: 'READY',
-            version: '1.18',
-            attachedTo: 'Network1',
-            region: 'Region1',
-            nodesUrl: 'http://nodes.url',
-            privateNetworkConfiguration: null,
-          } as TKube
-        }
-      />,
-    );
-    expect(getByText('kube_service_cluster_status_READY')).toBeInTheDocument();
-  });
-
-  it('renders cluster version correctly', () => {
-    const { getByText } = render(
-      <ClusterInformation
-        kubeDetail={
-          {
-            id: '1',
-            name: 'Cluster1',
-            status: 'READY',
-            version: '1.18',
-            attachedTo: 'Network1',
-            region: 'Region1',
-            nodesUrl: 'http://nodes.url',
-            privateNetworkConfiguration: null,
-          } as TKube
-        }
-      />,
-    );
-    expect(getByText('1.18')).toBeInTheDocument();
-  });
-
-  it('renders cluster region correctly', () => {
-    const { getByText } = render(
-      <ClusterInformation
-        kubeDetail={
-          {
-            id: '1',
-            name: 'Cluster1',
-            status: 'READY',
-            version: '1.18',
-            attachedTo: 'Network1',
-            region: 'Region1',
-            nodesUrl: 'http://nodes.url',
-            privateNetworkConfiguration: null,
-          } as TKube
-        }
-      />,
-    );
-    expect(getByText('Region1')).toBeInTheDocument();
-  });
-
   it('renders nodes URL with clipboard component', () => {
-    const { container } = render(
-      <ClusterInformation
-        kubeDetail={
-          {
-            id: '1',
-            name: 'Cluster1',
-            status: 'READY',
-            version: '1.18',
-            attachedTo: 'Network1',
-            region: 'Region1',
-            nodesUrl: 'http://nodes.url',
-            privateNetworkConfiguration: null,
-          } as TKube
-        }
-      />,
-    );
-
+    const { container } = renderClusterInformation(kubeDetail);
     const clipboardElement = container.querySelector(
       '[value="http://nodes.url"]',
     );
