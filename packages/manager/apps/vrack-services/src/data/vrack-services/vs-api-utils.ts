7dd04ae17aa9d6c2649f770999ce4cffa4ad732e
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2024-11-08 20:36:29 +0530

    feat(vrack-services): limit the vrack subnet
    ref: MANAGER-15865
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

--- packages/manager/apps/vrack-services/src/data/vrack-services/vs-api-utils.ts
+++ packages/manager/apps/vrack-services/src/data/vrack-services/vs-api-utils.ts
@@ -40,6 +40,15 @@ export const useVrackServicesList = (refetchIntervalTime = 5000) => {
   });
 };
 
+// Regular expression pattern for /24 subnet
+export const pattern =
+  '^(?:(?:25[0-5]|2[0-4]\\d|[01]?\\d\\d?).){2}(?:25[0-5]|2[0-4]\\d|[01]?\\d\\d?).0/24$';
+
+// The subnet address is limited to only "/24".
+export const isValidCidr = (subnet: string): boolean => {
+  return new RegExp(pattern).test(subnet);
+};
+
 /**
  * Query the current vRack Services and poll it if it is not ready
  */
