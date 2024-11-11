8ff95bcb241434655ccbbdd5900a9b93a9780d00
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2024-11-11 12:55:01 +0530

    feat(netapp): show info banner
    ref: MANAGER-15868
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

--- packages/manager/modules/netapp/src/order/constants.js
+++ packages/manager/modules/netapp/src/order/constants.js
@@ -5,8 +5,12 @@ export const SIZE_FACTOR = 1000;
 
 export const REGION_LABEL = 'region';
 
+// Private connectivity via our vRack service is not compatible with below regions.
+export const IN_COMPATIBLE_REGION = ['BHS', 'SBG'];
+
 export default {
   SIZE_MULTIPLE,
   SIZE_FACTOR,
   REGION_LABEL,
+  IN_COMPATIBLE_REGION,
 };
