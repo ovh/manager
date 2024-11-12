9edddc4abded8c37f4f59b575d55e8f032f63613
Anoop N <anoop.n@ovhcloud.com>
2024-11-12 23:58:58 +0530

    ci(release): remove conventional-graduate flag
     ref: MANAGER-14622
    
    Signed-off-by: Anoop N <anoop.n@ovhcloud.com>

--- scripts/release/release.sh
+++ scripts/release/release.sh
@@ -32,7 +32,7 @@ version() {
     node_modules/.bin/lerna version --conventional-commits --no-commit-hooks --no-git-tag-version --no-push --allow-branch="${GIT_BRANCH}" --yes
   else
     printf "%s\n" "Releasing"
-    node_modules/.bin/lerna version --conventional-graduate --conventional-commits --no-commit-hooks --no-git-tag-version --no-push  --yes
+    node_modules/.bin/lerna version --conventional-commits --no-commit-hooks --no-git-tag-version --no-push  --yes
   fi
 }
 
