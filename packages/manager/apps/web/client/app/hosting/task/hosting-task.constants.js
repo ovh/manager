972000181921cb9c6ab6c194e422d22e772be6ca
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2024-11-12 17:04:52 +0100

    fix(web.hosting.task): replace 2api call by iceberg call
    ref: MANAGER-15991
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/apps/web/client/app/hosting/task/hosting-task.constants.js
+++ packages/manager/apps/web/client/app/hosting/task/hosting-task.constants.js
@@ -1,4 +1,14 @@
 export const HOSTING_TASK_TABLE_ID = 'hosting-tasks-datagrid';
+export const HOSTING_TASK_STATUS = {
+  cancelled: 'cancelled',
+  doing: 'doing',
+  done: 'done',
+  error: 'error',
+  todo: 'todo',
+  init: 'init',
+};
+
 export default {
   HOSTING_TASK_TABLE_ID,
+  HOSTING_TASK_STATUS,
 };
