cded3baba10cef3dfac968d52053fc88ccf2f883
David Arsène <david.arsene.ext@ovhcloud.com>
2024-11-13 12:27:43 +0100

    Revert "revert(dedicated.pcc): add dashboard light when vcd and pcc is migrated"
    ref: MANAGER-15242
    
    This reverts commit d144d2b591f725000c5abaa75042e6b6714df6a1.
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/components/dedicated-cloud/dedicatedCloud.constant.js
+++ packages/manager/apps/dedicated/client/app/components/dedicated-cloud/dedicatedCloud.constant.js
@@ -12,8 +12,6 @@ export const MANAGED_PCC_MIGRATION_STATUS = {
   UNKNOWN: 'unknown',
 };
 
-export const TRACKING_PREFIX = 'Baremetal::Managed_baremetal::';
-
 export const MANAGED_VCD_MIGRATION_STATUS = {
   TO_MIGRATE: 'TO_MIGRATE',
   MIGRATED: 'MIGRATED',
@@ -190,5 +188,4 @@ export default {
   MANAGED_VCD_MIGRATION_STATUS,
   MANAGED_VCD_MIGRATION_STATE,
   MANAGED_PCC_MIGRATION_STATUS,
-  TRACKING_PREFIX,
 };
