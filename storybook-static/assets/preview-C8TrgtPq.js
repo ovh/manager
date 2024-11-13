0d590e86db5ed7ae55bf8cc7ae8a1b10d16534ea
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2024-11-13 16:04:55 +0100

    docs: update documentation [skip ci]

--- storybook-static/assets/preview-C8TrgtPq.js
+++
@@ -1,3 +0,0 @@
-const __vite__fileDeps=["./DocsRenderer-NNNQARDV-CprA7Tly.js","./iframe-Cx2DekOH.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./client-BvKKsruy.js","./index-BtM5VmRH.js","./index-DucGs5eh.js","./index-Js5CwwSK.js","./_getPrototype-DZQDOC48.js","./index-D_r38UMq.js","./doctrine-CogHronv.js","./index-DrFu-skq.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
-import{_ as e}from"./iframe-Cx2DekOH.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-CprA7Tly.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11]),import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-C8TrgtPq.js.map
---
+++ storybook-static/assets/preview-DBP4D4Wn.js
@@ -0,0 +1,3 @@
+const __vite__fileDeps=["./DocsRenderer-NNNQARDV-BVpH0qAU.js","./iframe-CWjd7bDA.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./client-BvKKsruy.js","./index-BtM5VmRH.js","./index-B2Be8zec.js","./index-Js5CwwSK.js","./_getPrototype-DZQDOC48.js","./index-D_r38UMq.js","./doctrine-CogHronv.js","./index-DrFu-skq.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{_ as e}from"./iframe-CWjd7bDA.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-BVpH0qAU.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11]),import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-DBP4D4Wn.js.map
