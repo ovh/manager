460a54da3b86ed8cfbc44c0ee40edc03ac1ad088
Antony MARION <antony.marion.ext@ovhcloud.com>
2024-11-13 10:28:52 +0100

    feat(web): add guide link for eligibility
    ref: MANAGER-15875
    
    Signed-off-by: Antony MARION <antony.marion.ext@ovhcloud.com>

--- packages/manager/apps/web/client/app/domain/contact/dashboard/dashboard.constants.js
+++ packages/manager/apps/web/client/app/domain/contact/dashboard/dashboard.constants.js
@@ -50,10 +50,48 @@ export const INFO_PROPERTIES_ARRAY = [
   },
 ];
 
+export const GUIDE_URLS = {
+  AU:
+    'https://help.ovhcloud.com/csm/en-au-account-manage-contacts?id=kb_article_view&sysparm_article=KB0042979',
+  DE:
+    'https://help.ovhcloud.com/csm/de-account-manage-contacts?id=kb_article_view&sysparm_article=KB0042983',
+  GB:
+    'https://help.ovhcloud.com/csm/en-gb-account-manage-contacts?id=kb_article_view&sysparm_article=KB0042993',
+  CA:
+    'https://help.ovhcloud.com/csm/en-ca-account-manage-contacts?id=kb_article_view&sysparm_article=KB0042991',
+  IE:
+    'https://help.ovhcloud.com/csm/en-ie-account-manage-contacts?id=kb_article_view&sysparm_article=KB0042992',
+  SG:
+    'https://help.ovhcloud.com/csm/en-sg-account-manage-contacts?id=kb_article_view&sysparm_article=KB0042996',
+  ES:
+    'https://help.ovhcloud.com/csm/es-es-account-manage-contacts?id=kb_article_view&sysparm_article=KB0043001',
+  QC:
+    'https://help.ovhcloud.com/csm/fr-ca-account-manage-contacts?id=kb_article_view&sysparm_article=KB0043000',
+  FR:
+    'https://help.ovhcloud.com/csm/fr-account-manage-contacts?id=kb_article_view&sysparm_article=KB0042989',
+  MA:
+    'https://help.ovhcloud.com/csm/fr-account-manage-contacts?id=kb_article_view&sysparm_article=KB0042989',
+  SN:
+    'https://help.ovhcloud.com/csm/fr-account-manage-contacts?id=kb_article_view&sysparm_article=KB0042989',
+  TN:
+    'https://help.ovhcloud.com/csm/fr-account-manage-contacts?id=kb_article_view&sysparm_article=KB0042989',
+  IT:
+    'https://help.ovhcloud.com/csm/it-account-manage-contacts?id=kb_article_view&sysparm_article=KB0042994',
+  PL:
+    'https://help.ovhcloud.com/csm/pl-account-manage-contacts?id=kb_article_view&sysparm_article=KB0042997',
+  PT:
+    'https://help.ovhcloud.com/csm/pt-account-manage-contacts?id=kb_article_view&sysparm_article=KB0043004',
+  IN:
+    'https://help.ovhcloud.com/csm/asia-account-manage-contacts?id=kb_article_view&sysparm_article=KB0029947',
+  DEFAULT:
+    'https://help.ovhcloud.com/csm/en-account-manage-contacts?id=kb_article_view&sysparm_article=KB0042986',
+};
+
 export const LEGAL_FORM_INDIVIDUAL = 'individual';
 
 export default {
   INFO_PROPERTIES,
   LEGAL_FORM_INDIVIDUAL,
   INFO_PROPERTIES_ARRAY,
+  GUIDE_URL: GUIDE_URLS,
 };
