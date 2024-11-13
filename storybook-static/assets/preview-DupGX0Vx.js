3b29e50f9345537a5998eee5f9e3a6da7d30e245
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2024-11-13 17:58:13 +0100

    docs: update documentation [skip ci]

---
+++ storybook-static/assets/preview-DupGX0Vx.js
@@ -0,0 +1,3 @@
+const __vite__fileDeps=["./DocsRenderer-NNNQARDV-DeWyfIDv.js","./iframe-B9c32mn8.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./client-BvKKsruy.js","./index-BtM5VmRH.js","./index-85tFMW_2.js","./index-Js5CwwSK.js","./_getPrototype-DZQDOC48.js","./index-D_r38UMq.js","./doctrine-CogHronv.js","./index-DrFu-skq.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{_ as e}from"./iframe-B9c32mn8.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-DeWyfIDv.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11]),import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-DupGX0Vx.js.map
