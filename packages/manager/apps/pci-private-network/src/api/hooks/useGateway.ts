7f066ac7dfd419e2b3e14c939d9183b0ffffea2c
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2024-11-14 17:15:20 +0100

    feat(pci.private-network): refactor and clean unuse code
    ref: TAPC-1824
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

--- packages/manager/apps/pci-private-network/src/api/hooks/useGateway.ts
+++ packages/manager/apps/pci-private-network/src/api/hooks/useGateway.ts
@@ -1,63 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
-import { getCatalog } from '@ovh-ux/manager-pci-common';
-import { getGateways, getGatewaysByRegion } from '@/api/data/regions';
+import { getGateways } from '@/api/data/regions';
 
 export const useGateways = (projectId: string) =>
   useQuery({
     queryKey: ['project', projectId, 'gateway'],
     queryFn: () => getGateways(projectId),
-  });
-
-export const useGatewayByRegion = (
-  projectId: string,
-  regionName: string,
-  enabled = true,
-) =>
-  useQuery({
-    queryKey: ['project', projectId, 'region', regionName, 'gateway'],
-    queryFn: () => getGatewaysByRegion(projectId, regionName),
-    enabled,
-    select: (gateways) =>
-      gateways.map((gateway) => ({
-        ...gateway,
-        region: regionName,
-      })),
-  });
-
-export const useGatewayCatalog = (
-  ovhSubsidiary: string,
-  productName?: string,
-) =>
-  useQuery({
-    queryKey: ['gateway', 'catalog', ovhSubsidiary],
-    queryFn: () => getCatalog(ovhSubsidiary, productName),
-    select: (data) => {
-      // pick the variants of product with least price
-      const gatewayProducts = data?.addons
-        .filter((addon) => addon.product.startsWith('publiccloud-gateway'))
-        .sort(
-          (
-            { pricings: [{ price: priceA }] },
-            { pricings: [{ price: priceB }] },
-          ) => priceA - priceB,
-        )
-        .filter(({ product }, _index, arr) => product === arr[0].product);
-
-      const monthlyPriceObj = gatewayProducts.find(({ planCode }) =>
-        planCode.includes('month'),
-      )?.pricings[0];
-
-      const hourlyPriceObj = gatewayProducts.find(({ planCode }) =>
-        planCode.includes('hour'),
-      )?.pricings[0];
-
-      return {
-        size: gatewayProducts[0].product
-          .split('-')
-          .slice(-1)
-          .join(),
-        pricePerMonth: monthlyPriceObj.price,
-        pricePerHour: hourlyPriceObj.price,
-      };
-    },
   });
