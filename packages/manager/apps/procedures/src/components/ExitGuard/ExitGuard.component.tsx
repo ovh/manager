52ba3c9253c2789906367737508d6c098066b6a9
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2024-11-14 12:03:34 +0100

    feat(procedures): prevent closing while uploading files
    ref: MANAGER-16022
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/apps/procedures/src/components/ExitGuard/ExitGuard.component.tsx
+++ packages/manager/apps/procedures/src/components/ExitGuard/ExitGuard.component.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 
-export default function ExitGuard() {
+export default function ExitGuard(): null {
   useEffect(() => {
     const confirmExit = (event: BeforeUnloadEvent) => {
       event.preventDefault();
