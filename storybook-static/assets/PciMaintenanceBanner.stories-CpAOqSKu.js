d1f769e5833c4740f362834a6345bfc31e3cef07
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2024-11-14 07:30:58 +0100

    docs: update documentation [skip ci]

--- storybook-static/assets/PciMaintenanceBanner.stories-CpAOqSKu.js
+++
@@ -1,7 +0,0 @@
-import{j as c}from"./jsx-runtime-CKrituN3.js";import{P as i}from"./PciMaintenanceBanner.component-DQLZ3c0z.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-Q-VytQcI.js";import"./index-C_zRtZt1.js";import"./index-BtM5VmRH.js";import"./i18next-ihUiNgJT.js";import"./useTranslation-CvcVFFFk.js";const m=({projectName:o,maintenanceURL:a})=>c.jsx(i,{maintenanceURL:a,projectName:o}),r={args:{projectName:"myPciProjectTest",maintenanceURL:"https://ovh.com"}},h={title:"Components/Public-cloud/PciMaintenanceBanner",component:m};var e,t,n;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    projectName: 'myPciProjectTest',
-    maintenanceURL: 'https://ovh.com'
-  }
-}`,...(n=(t=r.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};const B=["Primary"];export{r as Primary,B as __namedExportsOrder,h as default};
-//# sourceMappingURL=PciMaintenanceBanner.stories-CpAOqSKu.js.map
