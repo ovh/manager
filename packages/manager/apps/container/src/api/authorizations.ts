bd620ea7279654fa04841954e5153e32ab786d11
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2024-11-14 15:28:59 +0100

    feat(container): remove unnecessary comment
    ref: MANAGER-14722
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/apps/container/src/api/authorizations.ts
+++ packages/manager/apps/container/src/api/authorizations.ts
@@ -13,11 +13,6 @@ export const fetchAccountUrn = async (): Promise<string> => {
   const { data } = await fetchIcebergV2<IamResource>({
     route: '/iam/resource?resourceType=account',
   });
-  /*
-  const { data } = await v2.get(
-    '/iam/resource?resourceType=account',
-    { adapter: 'fetch',fetchOptions: { priority: 'low' } },
-  );
-   */
+
   return data[0]?.urn;
 };
