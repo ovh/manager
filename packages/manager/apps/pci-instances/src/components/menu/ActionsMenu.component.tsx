e44beba8cd1dde2e8b73cdd311aade291ce125f5
Frédéric Vilcot <fredericvilcot@gmail.com>
2024-11-15 17:39:07 +0100

    feat(pci-instances): add start & stop actions
    ref: TAPC-2149 TAPC-2150
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

--- packages/manager/apps/pci-instances/src/components/menu/ActionsMenu.component.tsx
+++ packages/manager/apps/pci-instances/src/components/menu/ActionsMenu.component.tsx
@@ -8,60 +8,89 @@ import {
   ODS_ICON_SIZE,
   ODS_BUTTON_SIZE,
   ODS_TEXT_LEVEL,
+  ODS_DIVIDER_SIZE,
 } from '@ovhcloud/ods-components';
 import {
   OsdsButton,
+  OsdsDivider,
   OsdsIcon,
   OsdsMenu,
   OsdsMenuItem,
   OsdsText,
 } from '@ovhcloud/ods-components/react';
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
+import { DeepReadonly } from '@/types/utils.type';
 
-export type TActionsMenuItem = {
+export type TActionsMenuItem = DeepReadonly<{
   label: string;
   href?: string;
+  group?: string;
   onMenuItemClick?: () => void;
-};
+}>;
 export type TActionsMenuProps = {
   items: TActionsMenuItem[];
 };
 
-export const ActionsMenu: FC<TActionsMenuProps> = ({ items }) => (
-  <OsdsMenu>
-    <OsdsButton
-      data-testid="actions-menu-button"
-      slot={'menu-title'}
-      circle
-      color={ODS_THEME_COLOR_INTENT.primary}
-      variant={ODS_BUTTON_VARIANT.stroked}
-    >
-      <OsdsIcon
-        name={ODS_ICON_NAME.ELLIPSIS}
+export const groupActionMenuItems = (items: TActionsMenuItem[]) =>
+  items.reduce<Record<string, TActionsMenuItem[]>>(
+    (acc, { group, ...rest }) => {
+      if (!group) {
+        const others: TActionsMenuItem[] = acc.others || [];
+        return { ...acc, others: [...others, rest] };
+      }
+      return { ...acc, [group]: [...(acc[group] || []), rest] };
+    },
+    {},
+  );
+
+export const ActionsMenu: FC<TActionsMenuProps> = ({ items }) => {
+  const groupedItems = useMemo(() => groupActionMenuItems(items), [items]);
+
+  return (
+    <OsdsMenu>
+      <OsdsButton
+        data-testid="actions-menu-button"
+        slot={'menu-title'}
+        circle
         color={ODS_THEME_COLOR_INTENT.primary}
-        size={ODS_ICON_SIZE.xxs}
-      />
-    </OsdsButton>
-    {items.map(({ label, href, onMenuItemClick }) => (
-      <OsdsMenuItem key={label}>
-        <OsdsButton
-          size={ODS_BUTTON_SIZE.sm}
-          variant={ODS_BUTTON_VARIANT.ghost}
+        variant={ODS_BUTTON_VARIANT.stroked}
+      >
+        <OsdsIcon
+          name={ODS_ICON_NAME.ELLIPSIS}
           color={ODS_THEME_COLOR_INTENT.primary}
-          data-testid="actions-menu-item"
-          {...(href && { href })}
-          {...(onMenuItemClick && { onClick: onMenuItemClick })}
-        >
-          <OsdsText
-            size={ODS_THEME_TYPOGRAPHY_SIZE._500}
-            level={ODS_TEXT_LEVEL.button}
-            color={ODS_THEME_COLOR_INTENT.primary}
-            slot={'start'}
-          >
-            {label}
-          </OsdsText>
-        </OsdsButton>
-      </OsdsMenuItem>
-    ))}
-  </OsdsMenu>
-);
+          size={ODS_ICON_SIZE.xxs}
+        />
+      </OsdsButton>
+      {Object.values(groupedItems).map((value, index, arr) => (
+        <div key={index}>
+          {value.map(({ label, href, onMenuItemClick }) => (
+            <OsdsMenuItem key={label}>
+              <OsdsButton
+                size={ODS_BUTTON_SIZE.sm}
+                variant={ODS_BUTTON_VARIANT.ghost}
+                color={ODS_THEME_COLOR_INTENT.primary}
+                data-testid="actions-menu-item"
+                {...(href && { href })}
+                {...(onMenuItemClick && { onClick: onMenuItemClick })}
+              >
+                <OsdsText
+                  size={ODS_THEME_TYPOGRAPHY_SIZE._500}
+                  level={ODS_TEXT_LEVEL.button}
+                  color={ODS_THEME_COLOR_INTENT.primary}
+                  slot={'start'}
+                >
+                  {label}
+                </OsdsText>
+              </OsdsButton>
+            </OsdsMenuItem>
+          ))}
+          {arr.length - 1 !== index && (
+            <div className="px-4">
+              <OsdsDivider separator size={ODS_DIVIDER_SIZE.one} />
+            </div>
+          )}
+        </div>
+      ))}
+    </OsdsMenu>
+  );
+};
