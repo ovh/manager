307555e32a50103eee3eb5f8d2b13542051ef6db
Frédéric Vilcot <fredericvilcot@gmail.com>
2024-11-15 11:22:57 +0100

    fix(manager-react-components): add convertHourlyPriceToMonthly method
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

--- packages/manager-react-components/src/hooks/useCatalogPrice.ts
+++ packages/manager-react-components/src/hooks/useCatalogPrice.ts
@@ -17,6 +17,7 @@ export const FRENCH_FORMAT = [
   'TN',
 ];
 export const GERMAN_FORMAT = ['DE', 'FI', 'SN'];
+export const HOUR_IN_MONTH = 730;
 
 export interface CatalogPriceOptions {
   hideTaxLabel?: boolean;
@@ -25,6 +26,13 @@ export interface CatalogPriceOptions {
 
 export const priceToUcent = (price: number) => price * 100_000_000;
 export const priceFromUcent = (price: number) => price / 100_000_000;
+
+/**
+ * Handle the conversion on the frontend side from hourly price to monthly.
+ * In the future, this conversion should be handled on the backend to improve efficiency and maintainability.
+ * */
+export const convertHourlyPriceToMonthly = (hourlyPrice: number) =>
+  hourlyPrice * HOUR_IN_MONTH;
 
 export const useCatalogPrice = (
   maximumFractionDigits?: number,
