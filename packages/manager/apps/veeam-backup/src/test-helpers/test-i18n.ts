1d47234f4ab84e0b31501e38d40c3d0a95ce6bd0
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2024-11-18 16:06:16 +0100

    fix(manager-components): remove change on update name modal
    ref: MANAGER-16063
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/veeam-backup/src/test-helpers/test-i18n.ts
+++ packages/manager/apps/veeam-backup/src/test-helpers/test-i18n.ts
@@ -1,5 +1,4 @@
 import i18next, { i18n } from 'i18next';
-import deleteModal from '@ovh-ux/manager-react-components/src/components/templates/delete-modal/translations/Messages_fr_FR.json';
 import common from '../../public/translations/veeam-backup/Messages_fr_FR.json';
 import orderVeeam from '../../public/translations/order-veeam/Messages_fr_FR.json';
 import listing from '../../public/translations/listing/Messages_fr_FR.json';
@@ -29,7 +28,6 @@ function addTranslations() {
     .addResources(defaultLocale, 'onboarding', onboarding)
     .addResources(defaultLocale, 'delete-veeam', deleteVeeam)
     .addResources(defaultLocale, 'error', error)
-    .addResources(defaultLocale, 'delete-modal', deleteModal)
     .use({
       type: 'postProcessor',
       name: 'normalize',
@@ -69,5 +67,4 @@ export const labels = {
   deleteVeeam,
   listing,
   error,
-  deleteModal,
 };
