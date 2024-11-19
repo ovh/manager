d47b8a883d58fd61df66324c017d3a1a861fe54b
stif59100 <steeve.vanderstocken@ovhcloud.com>
2024-11-19 17:43:10 +0100

    feat(web.office): add datagrid for react app
    ref:MANAGER-16068
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

--- packages/manager/apps/web-office/src/pages/onboarding/index.tsx
+++ packages/manager/apps/web-office/src/pages/onboarding/index.tsx
@@ -1,44 +1,11 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Card, OnboardingLayout } from '@ovh-ux/manager-react-components';
-import useGuideUtils from '@/hooks/guide/useGuideUtils';
-import Breadcrumb from '@/components/Breadcrumb/Breadcrumb';
+import { OnboardingLayout } from '@ovh-ux/manager-react-components';
+import { Breadcrumb } from '@/components/Breadcrumb/Breadcrumb';
 import onboardingImgSrc from './onboarding-img.png';
 
 export default function Onboarding() {
   const { t } = useTranslation('onboarding');
-  const link = useGuideUtils();
-
-  const tileList = [
-    {
-      id: 1,
-      texts: {
-        title: t('guide1Title'),
-        description: t('guide1Description'),
-        category: t('guideCategory'),
-      },
-      href: link?.guideLink1,
-    },
-    {
-      id: 2,
-      texts: {
-        title: t('guide2Title'),
-        description: t('guide2Description'),
-        category: t('guideCategory'),
-      },
-      href: link?.guideLink2,
-    },
-    {
-      id: 3,
-      texts: {
-        title: t('guide3Title'),
-        description: t('guide3Description'),
-        category: t('guideCategory'),
-      },
-      href: link?.guideLink3,
-    },
-  ];
-
   const title: string = t('title');
   const description: string = t('description');
   const imgSrc = {
@@ -56,11 +23,7 @@ export default function Onboarding() {
         orderHref={t('orderButtonLink')}
         moreInfoButtonLabel={t('moreInfoButtonLabel')}
         moreInfoHref={t('moreInfoButtonLink')}
-      >
-        {tileList.map((tile) => (
-          <Card key={tile.id} href={tile.href} texts={tile.texts} />
-        ))}
-      </OnboardingLayout>
+      />
     </>
   );
 }
