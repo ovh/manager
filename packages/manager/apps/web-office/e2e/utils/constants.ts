bf1533688d27d58840590edd580ac030b667423d
stif59100 <steeve.vanderstocken@ovhcloud.com>
2024-11-19 17:43:10 +0100

    feat(web.office): add datagrid for react app
    ref:MANAGER-16068
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

--- packages/manager/apps/web-office/e2e/utils/constants.ts
+++
@@ -1,7 +0,0 @@
-import { urls } from '../../src/routes/routes.constant';
-
-export const appUrl = 'http://localhost:9001/app';
-
-export type AppRoute = keyof typeof urls;
-
-export const getUrl = (route: AppRoute) => `${appUrl}/#${urls[route]}`;
