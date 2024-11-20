288f5ff73a285b45d1fd4cfe15f8d50476b891f9
Frédéric Vilcot <fredericvilcot@gmail.com>
2024-11-20 15:31:33 +0100

    feat(pci-instances): add wrapper for instance page
    ref: TAPC-2149 TAPC-2150
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

--- packages/manager/apps/pci-instances/src/pages/instances/instance/Instance.page.tsx
+++ packages/manager/apps/pci-instances/src/pages/instances/instance/Instance.page.tsx
@@ -1,10 +1,13 @@
 import { PageLayout } from '@ovh-ux/manager-react-components';
 import { FC } from 'react';
 import { GoBack } from '@/components/navigation/GoBack.component';
+import InstanceWrapper from './InstanceWrapper.page';
 
 const Instance: FC = () => (
-  <PageLayout>
-    <GoBack />
-  </PageLayout>
+  <InstanceWrapper>
+    <PageLayout>
+      <GoBack />
+    </PageLayout>
+  </InstanceWrapper>
 );
 export default Instance;
