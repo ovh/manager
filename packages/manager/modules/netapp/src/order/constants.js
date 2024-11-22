4c86d111467b9167a51b2b161bf117e60b73de2f
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2024-11-22 12:28:02 +0530

    feat(netapp): retrive region information from catalog
    ref: MANAGER-16096
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

--- packages/manager/modules/netapp/src/order/constants.js
+++ packages/manager/modules/netapp/src/order/constants.js
@@ -4,44 +4,33 @@ export const SIZE_MULTIPLE = 8;
 export const SIZE_FACTOR = 1000;
 
 export const REGION_LABEL = 'region';
+export const NETWORK_LABEL = 'network';
 
 // Private connectivity via our vRack service is not compatible with below regions.
 export const IN_COMPATIBLE_REGION = ['BHS', 'SBG'];
 
-export const DATACENTER_TO_COUNTRY = {
-  PAR: 'fr',
-  GRA: 'fr',
-  SBG: 'fr',
-  LIM: 'de',
-  WAW: 'pl',
-  ERI: 'uk',
-  VIN: 'us',
-  HIL: 'us',
-  BHS: 'ca',
-  SGP: 'sg',
-  SYD: 'au',
-  RBX: 'fr',
-  YYZ: 'ca',
-  YNM: 'in',
-  CR2: 'fr',
+export const REGION_TO_COUNTRY = {
+  'eu-west-par': 'fr',
+  'eu-west-gra': 'fr',
+  'eu-west-sbg': 'fr',
+  'eu-west-lim': 'de',
+  'eu-central-waw': 'pl',
+  'eu-west-eri': 'gb',
+  'us-east-vin': 'us',
+  'us-west-hil': 'us',
+  'ca-east-bhs': 'ca',
+  'ap-southeast-sgp': 'sg',
+  'ap-southeast-syd': 'au',
+  'eu-west-rbx': 'fr',
+  'ca-east-tor': 'ca',
+  'ap-south-mum': 'in',
+  'labeu-west-1-preprod': 'fr',
 };
 
-export const DATACENTER_TO_REGION = {
-  RBX: 'eu-west-rbx',
-  GRA: 'eu-west-gra',
-  SBG: 'eu-west-sbg',
-  PAR: 'eu-west-par',
-  CR2: 'labeu-west-1-preprod',
-  LIM: 'eu-west-lim',
-  WAW: 'eu-central-waw',
-  ERI: 'eu-west-eri',
-  BHS: 'ca-east-bhs',
-  YYZ: 'ca-east-tor',
-  SGP: 'ap-southeast-sgp',
-  SYD: 'ap-southeast-syd',
-  YNM: 'ap-south-mum',
-  VIN: 'us-east-vin',
-  HIL: 'us-west-hil',
+export const LICENSE_TYPE = {
+  Premium: 'Premium',
+  Standard: 'Standard',
+  Extreme: 'Ultra',
 };
 
 export default {
