f5c62bbc6f2f9fc56a8dd69600fb58de03c11ff5
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2024-11-26 14:50:23 +0100

    feat(logs): init logs-to-customer module
    ref: MANAGER-15966
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

---
+++ packages/manager/modules/logs-to-customer/src/components/apiError/ApiError.component.tsx
@@ -0,0 +1,44 @@
+import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
+import {
+  ODS_BUTTON_SIZE,
+  ODS_BUTTON_VARIANT,
+  ODS_MESSAGE_TYPE,
+} from '@ovhcloud/ods-components';
+import {
+  OsdsButton,
+  OsdsMessage,
+  OsdsText,
+} from '@ovhcloud/ods-components/react';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+export interface IError {
+  error: Error;
+  onRetry: () => void;
+}
+
+export default function ApiError({ error, onRetry }: IError) {
+  const { t } = useTranslation('error');
+
+  return (
+    <div className="flex flex-col gap-4">
+      <OsdsMessage
+        color={ODS_THEME_COLOR_INTENT.error}
+        type={ODS_MESSAGE_TYPE.error}
+      >
+        <OsdsText color={ODS_THEME_COLOR_INTENT.error}>
+          <div>{t('error_title')}</div>
+          <strong>{error.message}</strong>
+        </OsdsText>
+      </OsdsMessage>
+      <OsdsButton
+        size={ODS_BUTTON_SIZE.sm}
+        color={ODS_THEME_COLOR_INTENT.primary}
+        variant={ODS_BUTTON_VARIANT.flat}
+        onClick={onRetry}
+      >
+        {t('error_retry_button')}
+      </OsdsButton>
+    </div>
+  );
+}
