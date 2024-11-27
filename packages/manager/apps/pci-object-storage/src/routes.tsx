99b1e611c39544f06daeb317bf57dcf2a8617bb8
Florian Renaut <florian.renaut@corp.ovh.com>
2024-11-27 17:18:19 +0100

    feat(pci-object-storage): add onboarding page
    ref: DTCORE-2891
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/apps/pci-object-storage/src/routes.tsx
+++ packages/manager/apps/pci-object-storage/src/routes.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 
 export const ROUTE_PATHS = {
   ROOT: '/pci/projects/:projectId/storages/objects',
+  ONBOARDING: 'onboarding',
   OBJECTS: '',
   USER_LIST: 'users',
   USER_DELETE: ':userId/delete',
 };
 
 const LayoutPage = lazy(() => import('@/pages/Layout'));
+const OnboardingPage = lazy(() => import('@/pages/onboarding/Onboarding.page'));
 const ObjectsPage = lazy(() => import('@/pages/objects/Objects.page'));
 const ContainerListPage = lazy(() =>
   import('@/pages/objects/container/Listing.page'),
@@ -23,6 +25,7 @@ const DeletePage = lazy(() =>
 const RoutesComponent = () => (
   <Routes>
     <Route id="root" path={ROUTE_PATHS.ROOT} Component={LayoutPage}>
+      <Route path={ROUTE_PATHS.ONBOARDING} Component={OnboardingPage} />
       <Route path={ROUTE_PATHS.OBJECTS} Component={ObjectsPage}>
         <Route path={ROUTE_PATHS.OBJECTS} Component={ContainerListPage} />
         <Route path={ROUTE_PATHS.USER_LIST} Component={UserListPage}>
