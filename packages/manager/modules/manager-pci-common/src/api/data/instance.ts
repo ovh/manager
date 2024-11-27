969e267c811aa71f8edac3b02d6ef3f5bbab2475
Simon Chaumet <simon.chaumet.ext@ovhcloud.com>
2024-11-27 11:45:24 +0100

    feat(manager-pci-common): add get instances by region (#14188)
    ref: TAPC-2218
    
    Signed-off-by: Simon Chaumet <simon.chaumet.ext@ovhcloud.com>

--- packages/manager/modules/manager-pci-common/src/api/data/instance.ts
+++ packages/manager/modules/manager-pci-common/src/api/data/instance.ts
@@ -35,6 +35,9 @@ export const getInstance = async (
   return data;
 };
 
+/**
+ * If possible use getInstancesByRegion instead
+ */
 export const getInstances = async (
   projectId: string,
   region?: string,
@@ -46,6 +49,16 @@ export const getInstances = async (
         region,
       },
     },
+  );
+  return data;
+};
+
+export const getInstancesByRegion = async (
+  projectId: string,
+  region: string,
+): Promise<TInstance[]> => {
+  const { data } = await v6.get<TInstance[]>(
+    `/cloud/project/${projectId}/region/${region}/instance`,
   );
   return data;
 };
