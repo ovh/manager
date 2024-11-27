b909cafc685f0c8b70a549e9b9772ff5de522927
Lionel Bueno <lionel.bueno.ext@ovhcloud.com>
2024-11-27 15:52:39 +0100

    fix(instance): replace savings plan banner on edit instance
    ref:TAPC-2108
    
    Signed-off-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/instances/instance/edit/edit.module.js
+++ packages/manager/modules/pci/src/projects/project/instances/instance/edit/edit.module.js
@@ -16,6 +16,6 @@ angular
   ])
   .config(routing)
   .component('pciProjectsProjectInstancesInstanceEdit', component)
-  .run(/* @ngTranslationsInject:json ./translations */);
+  .run(/* @ngTranslationsInject:json ./translations ./../../../instances/add/translations */);
 
 export default moduleName;
