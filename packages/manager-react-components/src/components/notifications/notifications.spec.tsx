6ddbbe1f690efd7654652763ab8559713071b2b1
Florian Renaut <florian.renaut@corp.ovh.com>
2024-11-27 17:30:03 +0100

    fix(manager-react-components): sync with v1.x (#14170)
    useNotifications prevent early clears
    add skip to StepComponent
    useCatalogPrice
    ref: DTCORE-2862
    
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager-react-components/src/components/notifications/notifications.spec.tsx
+++ packages/manager-react-components/src/components/notifications/notifications.spec.tsx
@@ -1,8 +1,14 @@
 import { vitest } from 'vitest';
-import React, { useEffect } from 'react';
-import { render } from '@testing-library/react';
-import { useNotifications, NotificationType } from './useNotifications';
+import React from 'react';
+import { act, render, renderHook } from '@testing-library/react';
+import {
+  useNotifications,
+  NotificationType,
+  NOTIFICATION_MINIMAL_DISPLAY_TIME,
+} from './useNotifications';
 import { Notifications } from './notifications.component';
+
+vitest.useFakeTimers();
 
 vitest.mock('react-router-dom', async () => ({
   ...(await vitest.importActual('react-router-dom')),
@@ -11,34 +17,42 @@ vitest.mock('react-router-dom', async () => ({
   }),
 }));
 
-function AddNotification() {
-  const { addNotification } = useNotifications();
-  useEffect(() => {
-    addNotification('Notification-1', NotificationType.Success);
-    addNotification('Notification-2', NotificationType.Warning);
-  }, []);
-  return <></>;
-}
-
-function ClearNotifications() {
-  const { clearNotifications } = useNotifications();
-  useEffect(() => {
-    clearNotifications();
-  }, []);
-  return <></>;
-}
-
 describe('notifications component', () => {
   it('should list notifications', async () => {
     let { container } = render(<Notifications />);
-    render(<AddNotification />);
-    container = await render(<Notifications />).container;
+    const { result } = renderHook(() => useNotifications());
+    act(() => {
+      result.current.addNotification(
+        'Notification-1',
+        NotificationType.Success,
+      );
+      result.current.addNotification(
+        'Notification-2',
+        NotificationType.Warning,
+      );
+    });
+    container = render(<Notifications />).container;
     expect(container.children.length).toBe(2);
   });
+
+  it('should not clear unseen notifications', async () => {
+    let { container } = render(<Notifications />);
+    const { result } = renderHook(() => useNotifications());
+    act(() => {
+      result.current.clearNotifications();
+    });
+    container = render(<Notifications />).container;
+    expect(container.children.length).toBe(2);
+  });
+
   it('should clear notifications', async () => {
     let { container } = render(<Notifications />);
     expect(container.children.length).not.toBe(0);
-    render(<ClearNotifications />);
+    const { result } = renderHook(() => useNotifications());
+    act(() => {
+      vitest.advanceTimersByTime(NOTIFICATION_MINIMAL_DISPLAY_TIME + 1);
+      result.current.clearNotifications();
+    });
     container = render(<Notifications />).container;
     expect(container.children.length).toBe(0);
   });
