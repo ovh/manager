42b520487e0178a533c77be73e45f45ad471043f
Florian Renaut <florian.renaut@corp.ovh.com>
2024-11-28 18:15:29 +0100

    fix(pci-users): design css fixes (#13892)
    ref: DTCORE-2807
    
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/apps/pci-users/src/pages/index/Roles.tsx
+++ packages/manager/apps/pci-users/src/pages/index/Roles.tsx
@@ -1,7 +1,6 @@
 import {
   ODS_THEME_COLOR_INTENT,
   ODS_THEME_TYPOGRAPHY_LEVEL,
-  ODS_THEME_TYPOGRAPHY_SIZE,
 } from '@ovhcloud/ods-common-theming';
 import { OsdsText } from '@ovhcloud/ods-components/react';
 import { Role } from '@/interface';
@@ -12,7 +11,6 @@ export default function Roles({ roles }: { roles: Role[] }) {
       {roles.map((role, idx) => (
         <OsdsText
           level={ODS_THEME_TYPOGRAPHY_LEVEL.body}
-          size={ODS_THEME_TYPOGRAPHY_SIZE._400}
           color={ODS_THEME_COLOR_INTENT.text}
           key={idx}
           className={'block'}
