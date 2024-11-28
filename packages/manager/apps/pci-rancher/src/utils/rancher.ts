e90058a7e82757e956cc721a8f3690fea0b71d86
Pierre-Philippe Prevost <ppprevost@aol.fr>
2024-11-28 18:03:54 +0100

    feat(pci-rancher):display Orchestrated vCPU managed error (#14257)
    ref: TAPC-2109
    
    Signed-off-by: Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <ovh-ux-cds@users.noreply.github.com>

--- packages/manager/apps/pci-rancher/src/utils/rancher.ts
+++ packages/manager/apps/pci-rancher/src/utils/rancher.ts
@@ -1,4 +1,10 @@
-import { RancherPlan, RancherService, RancherVersion } from '@/types/api.type';
+import { TOptions } from 'i18next';
+import {
+  OVHError,
+  RancherPlan,
+  RancherService,
+  RancherVersion,
+} from '@/types/api.type';
 
 export const isValidRancherName = (name: string) =>
   /^[a-z0-9][-_.A-Za-z0-9]{1,61}[a-z0-9]$/.test(name);
@@ -68,4 +74,95 @@ export const getRancherPlanDescription = (rancherPlan: RancherPlan['name']) => {
     default:
       return null;
   }
+};
+
+/**
+ * Extracts drivers and plan information from a switch plan error message.
+ *
+ * @param inputString - The input error message string.
+ * @returns - An object containing the drivers and plan, or null if extraction fails.
+ * */
+export function extractDriversAndPlanFromSwitchPlanError(
+  inputString: string,
+): null | { drivers: string[]; plan: string } {
+  const beginPhrase = 'Unable to switch to plan';
+  const planKeywords = ['OVHCLOUD_EDITION', 'STANDARD'];
+
+  if (!inputString) {
+    return null;
+  }
+
+  // Find the start and end of the bracket content
+  const bracketStartIndex = inputString.indexOf('[');
+  const bracketEndIndex = inputString.indexOf(']');
+
+  if (
+    bracketStartIndex === -1 ||
+    bracketEndIndex === -1 ||
+    bracketStartIndex >= bracketEndIndex
+  ) {
+    return null;
+  }
+
+  const contentInsideBrackets = inputString
+    .substring(bracketStartIndex + 1, bracketEndIndex)
+    .trim();
+  const textBeforeBracket = inputString.substring(0, bracketStartIndex).trim();
+
+  if (!textBeforeBracket.startsWith(beginPhrase)) {
+    return null;
+  }
+
+  const plan = planKeywords.find((keyword) =>
+    textBeforeBracket.includes(keyword),
+  );
+
+  if (!plan) {
+    return null;
+  }
+
+  if (contentInsideBrackets) {
+    const drivers = contentInsideBrackets
+      .split(',')
+      .map((driver) => driver.trim());
+    if (drivers.length) {
+      return { drivers, plan };
+    }
+  }
+
+  return null;
+}
+
+/**
+ * Type guard to check if the error is an OVHError.
+ *
+ * @param error - The error object to check.
+ * @returns - True if the error is an OVHError, false otherwise.
+ */
+function isOVHError(error: unknown): error is OVHError {
+  return (
+    typeof error === 'object' &&
+    error !== null &&
+    'class' in error &&
+    typeof (error as OVHError).class === 'string' &&
+    'message' in error &&
+    typeof (error as OVHError).message === 'string'
+  );
+}
+
+/**
+ * Manages Rancher driver error and return appropriate error messages for internationalization.
+ *
+ * @param error - The error object containing the message and class.
+ * @returns - An array containing the error message key and optional options for internationalization, or null if the error is not recognized.
+ */
+export const getI18nextDriverError = (
+  error: string,
+): [string?, TOptions?] | null => {
+  const content = extractDriversAndPlanFromSwitchPlanError(error);
+  if (content) {
+    const { plan, drivers } = content;
+    return ['badRequestSwitchPlan', { plan, drivers: `[${drivers}]` }];
+  }
+  return null;
 };
