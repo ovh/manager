f41e38078c1b54606d3542e1f474a32812d36a21
Omar <omaralkabous@gmail.com>
2024-11-29 17:25:42 +0100

    feat(procedures): rgdp add retentionDays interpolation for legal info (#14360)
    ref: MANAGER-16153
    
    Signed-off-by: Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>

--- packages/manager/apps/procedures/src/pages/rgdp/RGDP.page.tsx
+++ packages/manager/apps/procedures/src/pages/rgdp/RGDP.page.tsx
@@ -3,6 +3,7 @@ import { PageLayout } from '@/components/PageLayout/PageLayout.component';
 import { RGDPIntroduction } from './rgdpIntroduction/RGDPIntroduction.component';
 import { RGDPForm } from './rgdpForm/RGDPForm.component';
 import { LegalInformations } from '@/components/legalInformations/LegalInformations.component';
+import { LEGAL_INFORMATIONS_RETENTION_DAYS } from './RGPD.constants';
 
 export default function RGDP() {
   return (
@@ -12,6 +13,9 @@ export default function RGDP() {
       <LegalInformations
         translationNamespace="rgdp"
         informationTranslationKey="rgdp_legal_information"
+        informationInterpolation={{
+          retentionDays: LEGAL_INFORMATIONS_RETENTION_DAYS,
+        }}
         policyTanslationKey="rgdp_legal_information_policy"
       />
     </PageLayout>
