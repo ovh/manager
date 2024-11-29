b5d42cfcaec03aa82cb311e3f2b93c54703cf7d3
Eric <35898232+Eric-ciccotti@users.noreply.github.com>
2024-11-29 10:05:46 +0100

    feat(pci.kubernetes): refacto oidc provider  (#14071)
    ref: TAPC-2088
    
    Signed-off-by: Eric Ciccotti <eric.ciccotti.ext@corp.ovh.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

---
+++ packages/manager/apps/pci-kubernetes/src/pages/oidc-provider/update/UpdateOIDCProvider.page.tsx
@@ -0,0 +1,5 @@
+import { OidcProviderModal } from '@/components/oidc/OidcProviderModal.component';
+
+export const UpdateOIDCProviderPage = () => <OidcProviderModal />;
+
+export default UpdateOIDCProviderPage;
