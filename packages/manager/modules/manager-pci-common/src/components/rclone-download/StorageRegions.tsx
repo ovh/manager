a6d082e640a7cacde08f3fd13eacb38044afaf54
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2024-12-02 14:49:12 +0100

    feat(pci-users): use rclone dwonload modal from pci-common
    and refactor openStack download modal
    ref: DTCORE-2881
    
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/modules/manager-pci-common/src/components/rclone-download/StorageRegions.tsx
+++ packages/manager/modules/manager-pci-common/src/components/rclone-download/StorageRegions.tsx
@@ -3,10 +3,12 @@ import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
 import {
   OsdsSelect,
   OsdsSelectOption,
+  OsdsSpinner,
   OsdsText,
 } from '@ovhcloud/ods-components/react';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { ODS_SPINNER_SIZE } from '@ovhcloud/ods-components';
 import { useStorageRegions } from '../../api/hook/useRegions';
 import { TRegion } from '../../api/data/regions';
 
@@ -18,13 +20,13 @@ interface S3StorageRegionsProps {
 export default function StorageRegions({
   projectId,
   onStorageRegionChange,
-}: S3StorageRegionsProps) {
+}: Readonly<S3StorageRegionsProps>) {
   const { t } = useTranslation('pci-rclone-download');
 
   const [currentRegion, setCurrentRegion] = useState('');
 
   const { translateMicroRegion } = useTranslatedMicroRegions();
-  const { data: storageRegions, isLoading } = useStorageRegions(`${projectId}`);
+  const { data: storageRegions, isLoading } = useStorageRegions(projectId);
 
   useEffect(() => {
     if (storageRegions?.length) {
@@ -33,34 +35,40 @@ export default function StorageRegions({
     }
   }, [storageRegions]);
 
+  if (isLoading || !currentRegion) {
+    return (
+      <OsdsSpinner
+        data-testid="storageRegions_spinner"
+        inline
+        size={ODS_SPINNER_SIZE.md}
+      />
+    );
+  }
+
   return (
     <>
-      {!isLoading && currentRegion && (
-        <>
-          <OsdsSelect
-            value={currentRegion}
-            data-testid="currentRegionSelect"
-            onOdsValueChange={(event) => {
-              const { value } = event.detail;
-              setCurrentRegion(`${value}`);
-              onStorageRegionChange(`${value}`);
-            }}
-          >
-            {storageRegions?.map((region: TRegion, index: number) => (
-              <OsdsSelectOption key={index} value={region.name}>
-                {translateMicroRegion(region.name)}
-              </OsdsSelectOption>
-            ))}
-          </OsdsSelect>
-          <OsdsText
-            slot="helper"
-            color={ODS_THEME_COLOR_INTENT.text}
-            className="mt-2"
-          >
-            {t('pci_projects_project_users_download-rclone_region_help')}
-          </OsdsText>
-        </>
-      )}
+      <OsdsSelect
+        value={currentRegion}
+        data-testid="storageRegions_select"
+        onOdsValueChange={(event) => {
+          const { value } = event.detail;
+          setCurrentRegion(`${value}`);
+          onStorageRegionChange(`${value}`);
+        }}
+      >
+        {storageRegions?.map((region: TRegion, index: number) => (
+          <OsdsSelectOption key={`${region.name}-${index}`} value={region.name}>
+            {translateMicroRegion(region.name)}
+          </OsdsSelectOption>
+        ))}
+      </OsdsSelect>
+      <OsdsText
+        slot="helper"
+        color={ODS_THEME_COLOR_INTENT.text}
+        className="mt-2"
+      >
+        {t('pci_projects_project_users_download-rclone_region_help')}
+      </OsdsText>
     </>
   );
 }
