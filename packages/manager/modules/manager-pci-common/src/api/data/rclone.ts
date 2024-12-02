8af605bfdc9d9cebe6441780e01cae337af8c976
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2024-12-02 14:45:39 +0100

    feat(manager-pci-common): add rclone download modal
    ref: DTCORE-2881
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

---
+++ packages/manager/modules/manager-pci-common/src/api/data/rclone.ts
@@ -0,0 +1,23 @@
+import { v6 } from '@ovh-ux/manager-core-api';
+
+export type DownloadRCloneConfigResult = {
+  content: string;
+};
+
+export const downloadRCloneConfig = async ({
+  projectId,
+  userId,
+  region,
+  service,
+}: {
+  projectId: string;
+  userId: string;
+  region: string;
+  service: string;
+}): Promise<DownloadRCloneConfigResult> => {
+  const { data } = await v6.get(
+    `/cloud/project/${projectId}/user/${userId}/rclone?region=${region}&service=${service}`,
+  );
+
+  return data;
+};
