131251e3893b5b7262a668e889c9dd698eb25cd5
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2024-12-02 14:45:39 +0100

    feat(manager-pci-common): add rclone download modal
    ref: DTCORE-2881
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/modules/manager-pci-common/src/api/hook/useRegions.ts
+++ packages/manager/modules/manager-pci-common/src/api/hook/useRegions.ts
@@ -1,5 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
-import { getProjectRegions } from '../data/regions';
+import {
+  getProjectRegions,
+  getS3StorageRegions,
+  getStorageRegions,
+} from '../data/regions';
 
 export const useGetProjectRegionsQuery = (projectId: string) => ({
   queryKey: ['project', projectId, 'regions'],
@@ -10,3 +14,25 @@ export const useGetProjectRegions = (projectId: string) =>
   useQuery({
     ...useGetProjectRegionsQuery(projectId),
   });
+
+export const useS3StorageRegions = (projectId: string) => {
+  const { data: regions } = useGetProjectRegions(projectId);
+
+  return useQuery({
+    queryKey: ['project', projectId, 's3-storages-regions'],
+    queryFn: () => getS3StorageRegions(regions || []),
+    retry: false,
+    enabled: !!regions,
+  });
+};
+
+export const useStorageRegions = (projectId: string) => {
+  const { data: regions } = useGetProjectRegions(projectId);
+
+  return useQuery({
+    queryKey: ['project', projectId, 'storages-regions'],
+    queryFn: () => getStorageRegions(regions || []),
+    retry: false,
+    enabled: !!regions,
+  });
+};
