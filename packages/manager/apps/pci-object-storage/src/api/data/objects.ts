2005c3e822c4011298aeff9aead78a6028c3e5f4
Yoann Fievez <yoann.fievez@gmail.com>
2024-12-02 15:08:07 +0100

    feat(pci-object-storage): delete object
    ref: DTCORE-2887
    Signed-off-by: Yoann Fievez <yoann.fievez@gmail.com>

---
+++ packages/manager/apps/pci-object-storage/src/api/data/objects.ts
@@ -0,0 +1,49 @@
+import { v6 } from '@ovh-ux/manager-core-api';
+import { OPENIO_DEFAULT_REGION } from '@/constants';
+
+export type TAccess = {
+  token: string;
+  endpoints: { region: string; url: string }[];
+};
+export const getAccessToken = async (projectId: string): Promise<TAccess> => {
+  const { data } = await v6.post<TAccess>(
+    `/cloud/project/${projectId}/storage/access`,
+  );
+  return data;
+};
+
+export const deleteS3Object = async (
+  projectId: string,
+  containerId: string,
+  objectName: string,
+  containerRegion: string,
+  s3StorageType: string,
+) => {
+  const region = containerRegion || OPENIO_DEFAULT_REGION;
+  const key = encodeURIComponent(objectName).replace(/\./g, '%2E');
+  const { data } = await v6.delete(
+    `/cloud/project/${projectId}/region/${region}/${s3StorageType}/${containerId}/object/${key}`,
+  );
+  return data;
+};
+
+export const deleteObject = async (
+  projectId: string,
+  storageName: string,
+  objectName: string,
+  token: string,
+  region: string,
+): Promise<void> => {
+  const response = await fetch(
+    `https://storage.${region}.cloud.ovh.net/v1/AUTH_${projectId}/${storageName}/${encodeURIComponent(
+      objectName,
+    )}`,
+    {
+      method: 'DELETE',
+      headers: {
+        'X-Auth-Token': token,
+      },
+    },
+  );
+  return response.json();
+};
