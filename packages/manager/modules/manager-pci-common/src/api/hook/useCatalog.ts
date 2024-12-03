9929887ddebf8b73e1978a84bcb30141bf007837
Florian Renaut <florian.renaut@corp.ovh.com>
2024-12-03 11:57:13 +0100

    feat(pci-object-storage): add container step 1 & 2
    ref: DTCORE-2884
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/modules/manager-pci-common/src/api/hook/useCatalog.ts
+++ packages/manager/modules/manager-pci-common/src/api/hook/useCatalog.ts
@@ -2,15 +2,18 @@ import { useQuery } from '@tanstack/react-query';
 import { useMe } from '@ovh-ux/manager-react-components';
 import { getCatalog } from '../data/catalog';
 
-export const getCatalogQuery = (ovhSubsidiary: string) => ({
-  queryKey: ['catalog'],
-  queryFn: () => getCatalog(ovhSubsidiary),
+export const getCatalogQuery = (
+  ovhSubsidiary: string,
+  productName?: string,
+) => ({
+  queryKey: ['catalog', productName],
+  queryFn: () => getCatalog(ovhSubsidiary, productName),
 });
 
-export const useCatalog = () => {
+export const useCatalog = (productName?: string) => {
   const { me } = useMe();
   return useQuery({
-    ...getCatalogQuery(me?.ovhSubsidiary),
+    ...getCatalogQuery(me?.ovhSubsidiary, productName),
     enabled: !!me,
   });
 };
