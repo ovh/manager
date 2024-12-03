5fd32a83cc77e726623c67ab30fa06e8d5b22ea6
Lionel Bueno <lionel.bueno.ext@ovhcloud.com>
2024-12-03 15:00:28 +0100

    feat: add tracking on savings plan
    ref: TAPC-1632
    
    Signed-off-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>

--- packages/manager/apps/pci-savings-plan/src/pages/onboarding/index.tsx
+++ packages/manager/apps/pci-savings-plan/src/pages/onboarding/index.tsx
@@ -2,13 +2,19 @@ import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import { OnboardingLayout } from '@ovh-ux/manager-react-components';
 import { useNavigate } from 'react-router-dom';
-import { ShellContext } from '@ovh-ux/manager-react-shell-client';
+import {
+  ButtonType,
+  PageLocation,
+  ShellContext,
+  useOvhTracking,
+} from '@ovh-ux/manager-react-shell-client';
 import onboardingImgSrc from './onboarding-img.png';
 import { SAVINGS_PLAN_INFO_URL } from '../../constants';
 
 export default function Onboarding() {
   const { t } = useTranslation('onboarding');
   const navigate = useNavigate();
+  const { trackClick } = useOvhTracking();
 
   const context = useContext(ShellContext);
   const { ovhSubsidiary } = context.environment.getUser();
@@ -25,9 +31,25 @@ export default function Onboarding() {
       }}
       description={t('description')}
       orderButtonLabel={t('orderButtonLabel')}
-      onOrderButtonClick={() => navigate('../new')}
+      onOrderButtonClick={() => {
+        navigate('../new');
+        trackClick({
+          location: PageLocation.page,
+          buttonType: ButtonType.button,
+          actionType: 'navigation',
+          actions: ['add_savings_plan'],
+        });
+      }}
       moreInfoButtonLabel={t('moreInfoButtonLabel')}
       moreInfoHref={savingsPlanUrl}
+      onmoreInfoButtonClick={() => {
+        trackClick({
+          location: PageLocation.page,
+          buttonType: ButtonType.button,
+          actionType: 'navigation',
+          actions: ['go-to-savings-plan'],
+        });
+      }}
     />
   );
 }
