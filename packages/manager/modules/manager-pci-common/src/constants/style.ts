077ef5f63f02860d75411a928d7104be380c164e
Florian Renaut <florian.renaut@corp.ovh.com>
2024-12-03 11:57:13 +0100

    feat(pci-object-storage): add container step 1 & 2
    ref: DTCORE-2884
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

---
+++ packages/manager/modules/manager-pci-common/src/constants/style.ts
@@ -0,0 +1,10 @@
+export const selectableTileClass =
+  'cursor-pointer border-[--ods-color-blue-100] hover:bg-[--ods-color-blue-100] hover:border-[--ods-color-blue-600]';
+
+export const selectedTileClass =
+  'bg-[--ods-color-blue-100] border-[--ods-color-blue-600]';
+
+export const tileBorderClass = 'border-solid border-2 rounded-lg';
+
+export const tileDisabledClass =
+  'cursor-not-allowed pointer-events-none opacity-50';
