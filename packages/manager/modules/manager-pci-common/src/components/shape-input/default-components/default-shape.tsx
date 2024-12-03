bbc59a80ea7a91328259d1b912659051ea80a4ef
Florian Renaut <florian.renaut@corp.ovh.com>
2024-12-03 11:57:13 +0100

    feat(pci-object-storage): add container step 1 & 2
    ref: DTCORE-2884
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/modules/manager-pci-common/src/components/shape-input/default-components/default-shape.tsx
+++ packages/manager/modules/manager-pci-common/src/components/shape-input/default-components/default-shape.tsx
@@ -1,6 +1,12 @@
 import clsx from 'clsx';
 import { ReactNode } from 'react';
 import { GHOST_BUTTON_CLASS } from '../constants';
+import {
+  selectableTileClass,
+  selectedTileClass,
+  tileBorderClass,
+  tileDisabledClass,
+} from '../../../constants/style';
 
 export const DefaultShapeComponent = ({
   isSelected,
@@ -16,11 +22,10 @@ export const DefaultShapeComponent = ({
   <button
     className={clsx(
       GHOST_BUTTON_CLASS,
-      'w-full h-full border-solid border-2 rounded-lg cursor-pointer',
-      isDisabled && 'cursor-not-allowed pointer-events-none opacity-50',
-      isSelected
-        ? 'border-[--ods-color-blue-600] !bg-[--ods-color-blue-100]'
-        : 'bg-white border-[--ods-color-blue-100] hover:bg-[--ods-color-blue-100] hover:border-[--ods-color-blue-600]',
+      'w-full h-full cursor-pointer',
+      tileBorderClass,
+      isDisabled && tileDisabledClass,
+      isSelected ? selectedTileClass : selectableTileClass,
     )}
     onClick={() => {
       if (!isDisabled) action();
