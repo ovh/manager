ba7b9bca885fecc845f6f6a7d54728b28b1130bf
Yoann Fievez <yoann.fievez@ovhcloud.com>
2024-12-06 06:49:52 +0100

    feat(pci-ssh-keys): ui design review (#13992)
    ref: DTCORE-2011
    
    Signed-off-by: Yoann Fievez <yoann.fievez@gmail.com>

--- packages/manager/apps/pci-ssh-keys/src/components/ssh-keys/AddSshModal.tsx
+++ packages/manager/apps/pci-ssh-keys/src/components/ssh-keys/AddSshModal.tsx
@@ -1,16 +1,11 @@
 import {
-  OsdsButton,
   OsdsFormField,
   OsdsInput,
-  OsdsModal,
-  OsdsSpinner,
   OsdsText,
   OsdsTextarea,
 } from '@ovhcloud/ods-components/react';
 import {
-  ODS_BUTTON_VARIANT,
   ODS_INPUT_TYPE,
-  ODS_SPINNER_SIZE,
   OdsInputValueChangeEvent,
   OdsTextAreaValueChangeEvent,
   OsdsTextareaCustomEvent,
@@ -21,6 +16,7 @@ import {
 } from '@ovhcloud/ods-common-theming';
 import { useTranslation } from 'react-i18next';
 import { useCallback, useState } from 'react';
+import { PciModal } from '@ovh-ux/manager-pci-common';
 import { useAddSsh } from '@/api/hooks/useSsh';
 
 interface AddSshModalProps {
@@ -77,132 +73,105 @@ export default function AddSshModal({
   );
 
   return (
-    <OsdsModal
-      headline={t('pci_projects_project_sshKeys_add_title')}
+    <PciModal
+      title={t('pci_projects_project_sshKeys_add_title')}
       data-testid="addSshModal-modal"
-      onOdsModalClose={onClose}
+      onClose={onClose}
+      onCancel={onClose}
+      onConfirm={() => add({ name, publicKey })}
+      isPending={isPending}
+      submitText={t('pci_projects_project_sshKeys_add_submit_label')}
+      cancelText={t('pci_projects_project_sshKeys_add_cancel_label')}
+      isDisabled={isPending || !name || !publicKey}
     >
-      <slot name="content">
-        {!isPending && (
-          <>
-            <OsdsFormField
-              className="mb-8"
-              color={
-                formErrors.missingName
-                  ? ODS_THEME_COLOR_INTENT.error
-                  : ODS_THEME_COLOR_INTENT.primary
-              }
-              error={
-                formErrors.missingName ? t('common_field_error_required') : ''
-              }
-            >
-              <OsdsText
-                slot="label"
-                level={ODS_THEME_TYPOGRAPHY_LEVEL.heading}
-                color={ODS_THEME_COLOR_INTENT.text}
-              >
-                {t('pci_projects_project_sshKeys_add_name')}
-              </OsdsText>
-              <OsdsInput
-                type={ODS_INPUT_TYPE.text}
-                color={
-                  formErrors.missingName
-                    ? ODS_THEME_COLOR_INTENT.error
-                    : ODS_THEME_COLOR_INTENT.primary
-                }
-                {...{ error: formErrors.missingName ? true : undefined }}
-                required
-                onOdsValueChange={handleInputNameChange}
-                onOdsInputFocus={() => {
-                  setFormErrors((errors) => ({
-                    ...errors,
-                    missingName: false,
-                  }));
-                }}
-                onOdsInputBlur={() =>
-                  setFormErrors((errors) => ({
-                    ...errors,
-                    missingName: !name,
-                  }))
-                }
-                ariaLabel={t('pci_projects_project_sshKeys_add_name')}
-                data-testid="sshKeyName"
-              />
-            </OsdsFormField>
-            <OsdsFormField
-              color={
-                formErrors.missingKey
-                  ? ODS_THEME_COLOR_INTENT.error
-                  : ODS_THEME_COLOR_INTENT.primary
-              }
-              error={
-                formErrors.missingKey
-                  ? `${t('common_field_error_required')} ${t(
-                      'pci_projects_project_sshKeys_add_infos',
-                    )}`
-                  : ''
-              }
-            >
-              <OsdsText
-                slot="label"
-                level={ODS_THEME_TYPOGRAPHY_LEVEL.heading}
-                color={ODS_THEME_COLOR_INTENT.text}
-              >
-                {t('pci_projects_project_sshKeys_add_key')}
-              </OsdsText>
-              <OsdsTextarea
-                {...{ error: formErrors.missingKey ? true : undefined }}
-                required
-                onOdsValueChange={handleInputPublicKeyChange}
-                onOdsBlur={() =>
-                  setFormErrors((errors) => ({
-                    ...errors,
-                    missingKey: !publicKey,
-                  }))
-                }
-                onOdsFocus={() => {
-                  setFormErrors((errors) => ({
-                    ...errors,
-                    missingKey: false,
-                  }));
-                }}
-                ariaLabel={t('pci_projects_project_sshKeys_add_key')}
-                data-testid="sshPublicKey"
-                rows={5}
-                placeholder={t(
-                  'pci_projects_project_sshKeys_add_key_description',
-                )}
-              />
-              <OsdsText slot="helper" color={ODS_THEME_COLOR_INTENT.text}>
-                {t('pci_projects_project_sshKeys_add_infos')}
-              </OsdsText>
-            </OsdsFormField>
-          </>
-        )}
-        {isPending && <OsdsSpinner inline size={ODS_SPINNER_SIZE.md} />}
-      </slot>
-      <OsdsButton
-        slot="actions"
-        color={ODS_THEME_COLOR_INTENT.primary}
-        variant={ODS_BUTTON_VARIANT.ghost}
-        onClick={onClose}
+      <OsdsFormField
+        className="mb-8"
+        color={
+          formErrors.missingName
+            ? ODS_THEME_COLOR_INTENT.error
+            : ODS_THEME_COLOR_INTENT.primary
+        }
+        error={formErrors.missingName ? t('common_field_error_required') : ''}
       >
-        {t('pci_projects_project_sshKeys_add_cancel_label')}
-      </OsdsButton>
-      <OsdsButton
-        slot="actions"
-        color={ODS_THEME_COLOR_INTENT.primary}
-        onClick={() => {
-          // @TODO remove this condition when ODS disabled button issue is fixed
-          if (publicKey) {
-            add({ name, publicKey });
+        <OsdsText
+          slot="label"
+          level={ODS_THEME_TYPOGRAPHY_LEVEL.heading}
+          color={ODS_THEME_COLOR_INTENT.text}
+        >
+          {t('pci_projects_project_sshKeys_add_name')}
+        </OsdsText>
+        <OsdsInput
+          type={ODS_INPUT_TYPE.text}
+          color={
+            formErrors.missingName
+              ? ODS_THEME_COLOR_INTENT.error
+              : ODS_THEME_COLOR_INTENT.primary
           }
-        }}
-        data-testid="submitButton"
-        {...(isPending || !name || !publicKey ? { disabled: true } : {})}
+          {...{ error: formErrors.missingName ? true : undefined }}
+          required
+          onOdsValueChange={handleInputNameChange}
+          onOdsInputFocus={() => {
+            setFormErrors((errors) => ({
+              ...errors,
+              missingName: false,
+            }));
+          }}
+          onOdsInputBlur={() =>
+            setFormErrors((errors) => ({
+              ...errors,
+              missingName: !name,
+            }))
+          }
+          ariaLabel={t('pci_projects_project_sshKeys_add_name')}
+          data-testid="sshKeyName"
+        />
+      </OsdsFormField>
+      <OsdsFormField
+        color={
+          formErrors.missingKey
+            ? ODS_THEME_COLOR_INTENT.error
+            : ODS_THEME_COLOR_INTENT.primary
+        }
+        error={
+          formErrors.missingKey
+            ? `${t('common_field_error_required')} ${t(
+                'pci_projects_project_sshKeys_add_infos',
+              )}`
+            : ''
+        }
       >
-        {t('pci_projects_project_sshKeys_add_submit_label')}
-      </OsdsButton>
-    </OsdsModal>
+        <OsdsText
+          slot="label"
+          level={ODS_THEME_TYPOGRAPHY_LEVEL.heading}
+          color={ODS_THEME_COLOR_INTENT.text}
+        >
+          {t('pci_projects_project_sshKeys_add_key')}
+        </OsdsText>
+        <OsdsTextarea
+          {...{ error: formErrors.missingKey ? true : undefined }}
+          required
+          onOdsValueChange={handleInputPublicKeyChange}
+          onOdsBlur={() =>
+            setFormErrors((errors) => ({
+              ...errors,
+              missingKey: !publicKey,
+            }))
+          }
+          onOdsFocus={() => {
+            setFormErrors((errors) => ({
+              ...errors,
+              missingKey: false,
+            }));
+          }}
+          ariaLabel={t('pci_projects_project_sshKeys_add_key')}
+          data-testid="sshPublicKey"
+          rows={5}
+          placeholder={t('pci_projects_project_sshKeys_add_key_description')}
+        />
+        <OsdsText slot="helper" color={ODS_THEME_COLOR_INTENT.text}>
+          {t('pci_projects_project_sshKeys_add_infos')}
+        </OsdsText>
+      </OsdsFormField>
+    </PciModal>
   );
 }
