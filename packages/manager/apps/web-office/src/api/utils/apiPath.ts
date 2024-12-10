33e97b2d6b479deb6225df4aac0857466e24ea11
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2024-12-10 09:29:53 +0100

    feat(web.office): add licences datagrid
    ref: MANAGER-16124
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/apps/web-office/src/api/utils/apiPath.ts
+++ packages/manager/apps/web-office/src/api/utils/apiPath.ts
@@ -1,2 +1,15 @@
-export const getApiPath = (serviceName: string) =>
-  `/license/office/${serviceName}/`;
+import { useOfficeServiceType } from '@/hooks';
+
+export const getApiPath = (serviceName: string) => {
+  const serviceType = useOfficeServiceType(serviceName);
+  return `/license/${
+    serviceType === 'payAsYouGo' ? 'office' : 'officePrepaid'
+  }/${serviceName}/`;
+};
+
+export const getApiPathWithoutServiceName = (serviceName: string) => {
+  const serviceType = useOfficeServiceType(serviceName);
+  return `/license/${
+    serviceType === 'payAsYouGo' ? 'office' : 'officePrepaid'
+  }/`;
+};
