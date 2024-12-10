871b22830b44cf6db60884f09e97a5e704929b70
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2024-12-10 15:24:35 +0100

    build: bump node version 22
    ref: MANAGER-16283
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

---
+++ packages/manager-react-components/tailwind.config.mjs
@@ -0,0 +1,16 @@
+import config from '@ovh-ux/manager-tailwind-config';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    screens: {
+      xs: '0',
+      sm: '36em',
+      md: '48em',
+      lg: '62em',
+      xl: '75em',
+      xxl: '87.5em',
+    },
+  },
+};
--- packages/manager/apps/pci-savings-plan/tailwind.config.mjs
+++ packages/manager/apps/pci-savings-plan/tailwind.config.mjs
@@ -1,13 +1,20 @@
-import path from 'path';
 import config from '@ovh-ux/manager-tailwind-config';
+import path from 'path';
+import { createRequire } from 'node:module';
+
+const require = createRequire(import.meta.url);
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   ...config,
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
     path.join(
       path.dirname(require.resolve('@ovh-ux/manager-react-components')),
+      '**/*.{js,jsx,ts,tsx}',
+    ),
+    path.join(
+      path.dirname(require.resolve('@ovh-ux/manager-pci-common')),
       '**/*.{js,jsx,ts,tsx}',
     ),
   ],
