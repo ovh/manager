6ceba2b975b9220182e21367d0244a4537b81844
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2024-12-11 11:51:18 +0100

    feat(*): update tailwing config export
    ref: MANAGER-16283
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager/apps/pci-databases-analytics/vite.config.mjs
+++ packages/manager/apps/pci-databases-analytics/vite.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import { getBaseConfig } from '@ovh-ux/manager-vite-config';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 const baseConfig = getBaseConfig({});
 
 export default defineConfig({
   ...baseConfig,
   root: '',
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
 });
--- packages/manager/apps/pci-users/tailwind.config.mjs
+++ packages/manager/apps/pci-users/tailwind.config.mjs
@@ -1,15 +1,12 @@
 import config from '@ovh-ux/manager-tailwind-config';
-import path from 'path';
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   ...config,
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
-    path.join(
-      path.dirname(require.resolve('@ovh-ux/manager-react-components')),
-      '**/*.{js,jsx,ts,tsx}',
-    ),
+    '../../../manager-react-components/src/**/*.{js,jsx,ts,tsx}',
+    '../../modules/manager-pci-common/**/*.{js,jsx,ts,tsx}',
   ],
   corePlugins: {
     preflight: false,
