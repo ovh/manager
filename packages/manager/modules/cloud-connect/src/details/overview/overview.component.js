e24b33e7861bbb12ae6962d142531153260961bc
soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>
2024-12-11 06:24:10 +0100

    feat(dedicated): add redirect to diagnostic page

--- packages/manager/modules/cloud-connect/src/details/overview/overview.component.js
+++ packages/manager/modules/cloud-connect/src/details/overview/overview.component.js
@@ -22,6 +22,7 @@ export default {
     goToManageServiceKeysPage: '<',
     goToChangeBandwidthPage: '<',
     goToTestBGPPeeringPage: '<',
+    goToDiagnosticPage: '<',
     user: '<',
   },
   controller,
