b85c75be91c4ad6c7d979719fd9d5b3d1365f478
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2024-12-11 15:50:14 +0100

    fix(pci-private-registry): update snapshot test
    ref: MANAGER-16283
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager/modules/manager-pci-common/tailwind.config.mjs
+++ packages/manager/modules/manager-pci-common/tailwind.config.mjs
@@ -6,7 +6,6 @@ export default {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
     '../../../manager-react-components/src/**/*.{js,jsx,ts,tsx}',
-    '../../modules/manager-pci-common/**/*.{js,jsx,ts,tsx}',
   ],
   corePlugins: {
     preflight: false,
