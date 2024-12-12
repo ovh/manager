ce89d720b266ac3a7573bbbbee084a922069d9fd
ovh-ux-cds <ovh-ux-cds@users.noreply.github.com>
2024-12-12 16:13:03 +0000

    docs: update documentation [skip ci]

--- storybook-static/assets/syntaxhighlighter-MJWPISIS-CK9XS4XX.js
+++
@@ -1,1 +0,0 @@
-import{S as c,c as l,s as n}from"./index-wnzXp-Io.js";import"./iframe-BcZuT3lh.js";import"../sb-preview/runtime.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BtM5VmRH.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";export{c as SyntaxHighlighter,l as createCopyToClipboardFunction,n as default};
