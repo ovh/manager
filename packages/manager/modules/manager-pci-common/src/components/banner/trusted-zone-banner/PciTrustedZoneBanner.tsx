001916742083b414661f3fa0af50dd6cea9e725b
Frédéric Vilcot <fredericvilcot@gmail.com>
2024-12-13 20:45:29 +0100

    fix(pci-common): update custom styles
    ref: TAPC-2378
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

--- packages/manager/modules/manager-pci-common/src/components/banner/trusted-zone-banner/PciTrustedZoneBanner.tsx
+++ packages/manager/modules/manager-pci-common/src/components/banner/trusted-zone-banner/PciTrustedZoneBanner.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next';
+import { ODS_MESSAGE_COLOR } from '@ovhcloud/ods-components';
 import { OdsText, OdsMessage } from '@ovhcloud/ods-components/react';
 import { useTrustedZoneBanner } from './useTrustedZoneBanner.hook';
 
@@ -12,7 +13,7 @@ export function PciTrustedZoneBanner() {
   return (
     <>
       {isBannerVisible && (
-        <OdsMessage>
+        <OdsMessage color={ODS_MESSAGE_COLOR.information}>
           <OdsText preset="span">
             {t('pci_projects_trusted_zone_banner_info')}
           </OdsText>
