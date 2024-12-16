3e75c88b2a6f0c5fc05c57d21fa6a56e9019e3d5
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2024-12-16 21:13:34 +0100

    fix(nutanix): update dashboard lazy loading url
    update dashboard lazy loading url to fix propagation of child route
    
    ref: #MANAGER-16492
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/nutanix/src/dashboard/general-info/index.js
+++ packages/manager/modules/nutanix/src/dashboard/general-info/index.js
@@ -7,7 +7,7 @@ const moduleName = 'ovhManagerNutanixGeneralInfoLazyLoading';
 angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
   /* @ngInject */ ($stateProvider) => {
     $stateProvider.state('nutanix.dashboard.general-info.**', {
-      url: '/general-info',
+      url: '/',
       lazyLoad: ($transition$) => {
         const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
 
