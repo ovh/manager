fd0deb95001c801aebf280d825285fc1c3d60bf4
Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>
2024-12-16 09:25:38 +0100

    feat(ldp): subscriptions list implementation ref: MANAGER-15919
    Signed-off-by: Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>

---
+++ packages/manager/modules/logs-to-customer/src/data/api/logSubscriptions.ts
@@ -0,0 +1,44 @@
+import apiClient, {
+  fetchIcebergV2,
+  fetchIcebergV6,
+} from '@ovh-ux/manager-core-api';
+import { LogKind, LogSubscription } from '../types/dbaas/logs';
+import { ApiUrls } from '../../LogsToCustomer.module';
+
+/**
+ * LIST log subscription
+ */
+export const getLogSubscriptionsv2 = async (
+  logSubscriptionUrl: ApiUrls['logSubscription'],
+  logKind: LogKind,
+) => {
+  const { data } = await fetchIcebergV2<LogSubscription>({
+    route: `${logSubscriptionUrl}?kind=${logKind.name}`,
+  });
+  return data;
+};
+
+export const getLogSubscriptionsv6 = async (
+  logSubscriptionUrl: ApiUrls['logSubscription'],
+  logKind: LogKind,
+) => {
+  const { data } = await fetchIcebergV6<LogSubscription>({
+    route: `${logSubscriptionUrl}?kind=${logKind.name}`,
+  });
+  return data;
+};
+
+/**
+ * GET log subscription
+ */
+export const getLogSubscriptionv2 = async (
+  logSubscriptionUrl: ApiUrls['logSubscription'],
+  subscriptionId: string,
+) =>
+  apiClient.v2.get<LogSubscription>(`${logSubscriptionUrl}/${subscriptionId}`);
+
+export const getLogSubscriptionv6 = async (
+  logSubscriptionUrl: ApiUrls['logSubscription'],
+  subscriptionId: string,
+) =>
+  apiClient.v6.get<LogSubscription>(`${logSubscriptionUrl}/${subscriptionId}`);
