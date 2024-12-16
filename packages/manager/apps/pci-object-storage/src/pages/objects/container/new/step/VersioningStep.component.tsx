b7decdf2f8e7f3cea320143490c7a8b08b112c51
Florian Renaut <florian.renaut@corp.ovh.com>
2024-12-16 16:34:32 +0100

    feat(pci-object-storage): add container creation steps
    ref: DTCORE-2884
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/apps/pci-object-storage/src/pages/objects/container/new/step/VersioningStep.component.tsx
+++ packages/manager/apps/pci-object-storage/src/pages/objects/container/new/step/VersioningStep.component.tsx
@@ -1,10 +1,29 @@
+import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
+import {
+  OsdsRadio,
+  OsdsRadioButton,
+  OsdsRadioGroup,
+  OsdsText,
+} from '@ovhcloud/ods-components/react';
+import {
+  ODS_RADIO_BUTTON_SIZE,
+  ODS_TEXT_SIZE,
+  OdsRadioGroupValueChangeEventDetail,
+  OsdsRadioGroupCustomEvent,
+} from '@ovhcloud/ods-components';
 import { useTranslation } from 'react-i18next';
 import { StepComponent } from '@ovh-ux/manager-react-components';
 import { useContainerCreationStore } from '../useContainerCreationStore';
 
 export function VersioningStep() {
-  const { t } = useTranslation(['containers/bucket-versioning', 'pci-common']);
+  const { t } = useTranslation([
+    'containers/bucket-versioning',
+    'containers/enable-versioning',
+    'pci-common',
+  ]);
   const {
+    form,
+    setVersioning,
     stepper,
     submitVersioning,
     editVersioning,
@@ -28,7 +47,65 @@ export function VersioningStep() {
         label: t('pci-common:common_stepper_modify_this_step'),
       }}
     >
-      <span>TODO versioning</span>
+      <>
+        <p>
+          <OsdsText
+            color={ODS_THEME_COLOR_INTENT.text}
+            size={ODS_TEXT_SIZE._600}
+          >
+            {t(
+              'containers/enable-versioning:pci_projects_project_storages_containers_bucket_versioning_description',
+            )}
+          </OsdsText>
+        </p>
+        <OsdsRadioGroup
+          value={form.versioning?.toString()}
+          onOdsValueChange={(
+            event: OsdsRadioGroupCustomEvent<
+              OdsRadioGroupValueChangeEventDetail
+            >,
+          ) => {
+            setVersioning(event.detail.newValue === 'true');
+          }}
+        >
+          <OsdsRadio className="mt-4" value="false">
+            <OsdsRadioButton
+              color={ODS_THEME_COLOR_INTENT.primary}
+              size={ODS_RADIO_BUTTON_SIZE.xs}
+              checked={!form.versioning || undefined}
+            >
+              <div slot="end" className="align-bottom inline-block">
+                <OsdsText
+                  color={ODS_THEME_COLOR_INTENT.text}
+                  size={ODS_TEXT_SIZE._600}
+                >
+                  {t(
+                    'pci_projects_project_storages_containers_bucket_versioning_disabled',
+                  )}
+                </OsdsText>
+              </div>
+            </OsdsRadioButton>
+          </OsdsRadio>
+          <OsdsRadio className="mt-4" value="true">
+            <OsdsRadioButton
+              color={ODS_THEME_COLOR_INTENT.primary}
+              size={ODS_RADIO_BUTTON_SIZE.xs}
+              checked={form.versioning || undefined}
+            >
+              <div slot="end" className="align-bottom inline-block">
+                <OsdsText
+                  color={ODS_THEME_COLOR_INTENT.text}
+                  size={ODS_TEXT_SIZE._600}
+                >
+                  {t(
+                    'pci_projects_project_storages_containers_bucket_versioning_enabled',
+                  )}
+                </OsdsText>
+              </div>
+            </OsdsRadioButton>
+          </OsdsRadio>
+        </OsdsRadioGroup>
+      </>
     </StepComponent>
   );
 }
