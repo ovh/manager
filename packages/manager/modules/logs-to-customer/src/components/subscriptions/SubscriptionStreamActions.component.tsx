7e08091df7f324f13abb637b60086d4b7e666967
Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>
2024-12-16 09:25:38 +0100

    feat(ldp): subscriptions list implementation ref: MANAGER-15919
    Signed-off-by: Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>

---
+++ packages/manager/modules/logs-to-customer/src/components/subscriptions/SubscriptionStreamActions.component.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import {
+  ODS_BUTTON_VARIANT,
+  ODS_ICON_NAME,
+  ODS_ICON_SIZE,
+  ODS_SPINNER_SIZE,
+} from '@ovhcloud/ods-components';
+import { OdsHTMLAnchorElementTarget } from '@ovhcloud/ods-common-core';
+import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
+import {
+  OsdsButton,
+  OsdsIcon,
+  OsdsSpinner,
+} from '@ovhcloud/ods-components/react';
+import { useQueryClient } from '@tanstack/react-query';
+import {
+  getLogStreamUrlQueryKey,
+  useLogStreamUrl,
+} from '../../data/hooks/useLogStreamUrl';
+import { LogSubscription } from '../../data/types/dbaas/logs';
+import ApiError from '../apiError/ApiError.component';
+
+type SubscriptionStreamItemProps = {
+  subscription: LogSubscription;
+};
+
+const SubscriptionStreamActions = ({
+  subscription,
+}: SubscriptionStreamItemProps) => {
+  const queryClient = useQueryClient();
+  const { t } = useTranslation('logStream');
+  const { t: tSubscription } = useTranslation('logSubscription');
+  const { data, isLoading, isPending, error } = useLogStreamUrl(
+    subscription.serviceName,
+    subscription.streamId,
+  );
+
+  if (isLoading || isPending) {
+    return (
+      <div className="flex justify-center w-full py-4">
+        <OsdsSpinner
+          inline
+          size={ODS_SPINNER_SIZE.md}
+          data-testid="logStreamUrl-spinner"
+        />
+      </div>
+    );
+  }
+
+  if (error)
+    return (
+      <ApiError
+        error={error}
+        onRetry={() =>
+          queryClient.refetchQueries({
+            queryKey: getLogStreamUrlQueryKey(
+              subscription.serviceName,
+              subscription.streamId,
+            ),
+          })
+        }
+        testId="logStreamUrl-error"
+      />
+    );
+
+  return (
+    <>
+      <OsdsButton
+        href={data?.streamURL?.address}
+        disabled={!data?.streamURL || undefined}
+        className="flex w-full"
+        color={ODS_THEME_COLOR_INTENT.primary}
+        variant={ODS_BUTTON_VARIANT.stroked}
+        target={OdsHTMLAnchorElementTarget._blank}
+      >
+        {t('log_stream_button_graylog_watch_label')}
+        <span slot="end">
+          <OsdsIcon
+            name={ODS_ICON_NAME.EXTERNAL_LINK}
+            size={ODS_ICON_SIZE.xs}
+            color={ODS_THEME_COLOR_INTENT.primary}
+          ></OsdsIcon>
+        </span>
+      </OsdsButton>
+      <OsdsButton
+        className="flex w-full"
+        variant={ODS_BUTTON_VARIANT.ghost}
+        color={ODS_THEME_COLOR_INTENT.primary}
+      >
+        {tSubscription('log_subscription_button_unsubscribe_label')}
+      </OsdsButton>
+    </>
+  );
+};
+
+export default SubscriptionStreamActions;
