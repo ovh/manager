dda66035fb5bce26fee834a670460c3aeca76087
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2024-12-16 21:08:12 +0100

    fixup! feat(nutanix): add poweron popin for node nutanix

--- packages/manager/modules/nutanix/src/dashboard/nodes/list/poweron-node/index.js
+++ packages/manager/modules/nutanix/src/dashboard/nodes/list/poweron-node/index.js
@@ -8,7 +8,7 @@ const moduleName = 'ovhManagerNutanixNodePowerOnLazyLoading';
 angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
   /* @ngInject */ ($stateProvider) => {
     $stateProvider.state('nutanix.dashboard.nodes.all.poweron-node.**', {
-      url: '/poweron/:node',
+      url: '/poweron',
       lazyLoad: ($transition$) => {
         const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
         return import('./poweron-node.module').then((mod) =>
