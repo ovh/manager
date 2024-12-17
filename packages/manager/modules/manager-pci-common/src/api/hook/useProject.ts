a97b08f23740ddc114db965d4193f70e8848989c
Florian Renaut <florian.renaut@corp.ovh.com>
2024-12-17 14:11:42 +0100

    feat(pci-common): add project acl hooks
    ref: DTCORE-2941
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/modules/manager-pci-common/src/api/hook/useProject.ts
+++ packages/manager/modules/manager-pci-common/src/api/hook/useProject.ts
@@ -1,6 +1,16 @@
 import { useParams } from 'react-router-dom';
 import { QueryOptions, useQuery } from '@tanstack/react-query';
-import { getProject, getProjectQuota, TProject, TQuota } from '../data/project';
+import { useContext } from 'react';
+import { ShellContext } from '@ovh-ux/manager-react-shell-client';
+import {
+  getProject,
+  getProjectAcl,
+  getProjectQuota,
+  getProjectServiceInfos,
+  TProject,
+  TProjectServiceInfos,
+  TQuota,
+} from '../data/project';
 
 export interface ResponseAPIError {
   message: string;
@@ -55,4 +65,60 @@ export const useProjectQuota = (
       filter ? data.filter((quota) => quota.region === filter.region) : data,
     ...options,
   });
+};
+
+export const getProjectAclQuery = (
+  projectId: string,
+  type: Parameters<typeof getProjectAcl>[1],
+) => ({
+  queryKey: ['project', projectId, 'acl'],
+  queryFn: () => getProjectAcl(projectId, type),
+});
+
+export const useProjectAcl = (
+  projectId: string,
+  type: Parameters<typeof getProjectAcl>[1],
+  options?: QueryOptions<string[], ResponseAPIError>,
+) =>
+  useQuery({
+    ...getProjectAclQuery(projectId, type),
+    ...options,
+  });
+
+export const getProjectServiceInfosQuery = (projectId: string) => ({
+  queryKey: ['project', projectId, 'serviceInfos'],
+  queryFn: () => getProjectServiceInfos(projectId),
+});
+
+export const useProjectServiceInfos = (
+  projectId: string,
+  options?: QueryOptions<TProjectServiceInfos, ResponseAPIError>,
+) =>
+  useQuery({
+    ...getProjectServiceInfosQuery(projectId),
+    ...options,
+  });
+
+export const useProjectRights = (projectId?: string) => {
+  const context = useContext(ShellContext);
+  const { nichandle } = context.environment?.getUser() || {};
+  const { projectId: defaultProjectId } = useParams();
+  const { data: acl, isPending: isAclPending } = useProjectAcl(
+    projectId || defaultProjectId,
+    'readWrite',
+  );
+  const {
+    data: serviceInfos,
+    isPending: isServiceInfosPending,
+  } = useProjectServiceInfos(projectId || defaultProjectId);
+
+  const isPending = isAclPending || isServiceInfosPending || !nichandle;
+  const isAdmin = serviceInfos?.contactAdmin === nichandle;
+  const hasReadWriteRights = isAdmin || acl?.some((nic) => nic === nichandle);
+
+  return {
+    isPending,
+    isAdmin,
+    hasReadWriteRights,
+  };
 };
