b5ca9412576f72c3eb5ba5faba0009989b174ddf
Florian Renaut <florian.renaut@corp.ovh.com>
2024-12-17 17:12:06 +0100

    fix(pci-object-storage): add container type step
    ref: DTCORE-2884
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/apps/pci-object-storage/src/pages/objects/container/new/step/ContainerTypeStep.component.tsx
+++ packages/manager/apps/pci-object-storage/src/pages/objects/container/new/step/ContainerTypeStep.component.tsx
@@ -1,14 +1,27 @@
 import { useTranslation } from 'react-i18next';
 import { StepComponent } from '@ovh-ux/manager-react-components';
+import { TileInputChoice } from '@ovh-ux/manager-pci-common';
+
+import { OBJECT_CONTAINER_TYPES } from '@/constants';
+import { useColumnsCount } from '@/hooks/useColumnsCount';
+import { ContainerTypeTile } from './ContainerTypeTile.component';
 import { useContainerCreationStore } from '../useContainerCreationStore';
 
 export function ContainerType() {
   const { t } = useTranslation(['containers/add', 'pci-common']);
+  const columnsCount = useColumnsCount();
   const {
+    form,
     stepper,
     submitContainerType,
+    setContainerType,
     editContainerType,
   } = useContainerCreationStore();
+
+  const items = OBJECT_CONTAINER_TYPES.map((type) => ({
+    id: type,
+  }));
+
   return (
     <StepComponent
       title={t('pci_projects_project_storages_containers_add_type_title')}
@@ -26,7 +39,17 @@ export function ContainerType() {
         label: t('pci-common:common_stepper_modify_this_step'),
       }}
     >
-      <span>TODO container type</span>
+      <TileInputChoice
+        items={items}
+        columnsCount={columnsCount}
+        selectedItem={items.find(({ id }) => id === form.containerType)}
+        onSelectItem={(item) => setContainerType(item.id)}
+        isSubmitted={stepper.containerType.isLocked}
+      >
+        {(item, isSelected) => (
+          <ContainerTypeTile type={item.id} isSelected={isSelected} />
+        )}
+      </TileInputChoice>
     </StepComponent>
   );
 }
