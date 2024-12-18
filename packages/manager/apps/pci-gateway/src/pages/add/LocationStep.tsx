47d83ee58475bbb69e6effd9b76735a6d7e5025c
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2024-12-18 11:15:54 +0100

    feat(pci.gateway): use pci regionselector
    ref: TAPC-2413
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

--- packages/manager/apps/pci-gateway/src/pages/add/LocationStep.tsx
+++ packages/manager/apps/pci-gateway/src/pages/add/LocationStep.tsx
@@ -1,17 +1,9 @@
-import {
-  StepComponent,
-  TilesInputComponent,
-  useProjectUrl,
-} from '@ovh-ux/manager-react-components';
+import { StepComponent } from '@ovh-ux/manager-react-components';
+import { RegionSelector } from '@ovh-ux/manager-pci-common';
 import { useTranslation } from 'react-i18next';
 import { useContext, useEffect, useState } from 'react';
 import { useParams, useSearchParams } from 'react-router-dom';
-import {
-  OsdsIcon,
-  OsdsLink,
-  OsdsMessage,
-  OsdsText,
-} from '@ovhcloud/ods-components/react';
+import { OsdsIcon, OsdsLink, OsdsText } from '@ovhcloud/ods-components/react';
 import {
   ODS_THEME_COLOR_INTENT,
   ODS_THEME_TYPOGRAPHY_LEVEL,
@@ -20,7 +12,6 @@ import {
 import { ODS_ICON_NAME, ODS_ICON_SIZE } from '@ovhcloud/ods-components';
 import { OdsHTMLAnchorElementTarget } from '@ovhcloud/ods-common-core';
 import { ShellContext } from '@ovh-ux/manager-react-shell-client';
-import { clsx } from 'clsx';
 import { StepsEnum, useNewGatewayStore } from '@/pages/add/useStore';
 import { TAvailableRegion, useData } from '@/api/hooks/data';
 
@@ -42,13 +33,10 @@ type IState = {
 export const LocationStep = () => {
   const { t: tStepper } = useTranslation('stepper');
   const { t: tAdd } = useTranslation('add');
-  const { t: tRegionsList } = useTranslation('regions-list');
   const { projectId } = useParams();
   const [searchParams] = useSearchParams();
   const { tracking } = useContext(ShellContext).shell;
   const store = useNewGatewayStore();
-
-  const projectUrl = useProjectUrl('public-cloud');
 
   const sizes = useData(projectId);
 
@@ -155,80 +143,13 @@ export const LocationStep = () => {
         isDisabled: false,
       }}
     >
-      <TilesInputComponent<TAvailableRegion, string, string>
-        value={state.region}
-        items={state.regions}
-        label={(region) => region.microName}
-        onInput={(region) => {
-          store.updateForm.regionName(region?.name);
-        }}
-        stack={{
-          by: (region) => region?.macroName,
-          label: (macroName) => macroName,
-          title: () => tRegionsList('pci_project_regions_list_region'),
-          onChange: (language) =>
-            setState({ ...state, selectedMacroName: language }),
-        }}
-        group={{
-          by: (region) => region.continent,
-          label: (continent) => (
-            <OsdsText
-              breakSpaces={false}
-              size={ODS_THEME_TYPOGRAPHY_SIZE._600}
-              color={
-                continent === state.selectedContinent
-                  ? ODS_THEME_COLOR_INTENT.text
-                  : ODS_THEME_COLOR_INTENT.primary
-              }
-            >
-              <div
-                className={clsx(
-                  continent === state.selectedContinent && 'font-bold',
-                  'whitespace-nowrap px-2 text-lg',
-                )}
-              >
-                {undefined === continent
-                  ? tRegionsList('pci_project_regions_list_continent_all')
-                  : continent}
-              </div>
-            </OsdsText>
-          ),
-          showAllTab: true,
-          onChange: (continent) =>
-            setState({ ...state, selectedContinent: continent }),
-        }}
+      <RegionSelector
+        projectId={projectId}
+        onSelectRegion={(region) => store.updateForm.regionName(region?.name)}
+        regionFilter={(r) =>
+          r.isMacro || state.regions.some(({ name }) => name === r.name)
+        }
       />
-      {state.region && !state.region.active && (
-        <OsdsMessage
-          color={ODS_THEME_COLOR_INTENT.warning}
-          icon={ODS_ICON_NAME.WARNING}
-          className="mt-6"
-        >
-          <OsdsText
-            level={ODS_THEME_TYPOGRAPHY_LEVEL.body}
-            size={ODS_THEME_TYPOGRAPHY_SIZE._400}
-            color={ODS_THEME_COLOR_INTENT.text}
-          >
-            {tAdd(
-              'pci_projects_project_public_gateways_add_region_activate_line1',
-            )}
-
-            <OsdsLink
-              color={ODS_THEME_COLOR_INTENT.primary}
-              href={`${projectUrl}/regions`}
-              className="mx-3"
-            >
-              {tAdd(
-                'pci_projects_project_public_gateways_add_region_activate_line2',
-              )}
-            </OsdsLink>
-
-            {tAdd(
-              'pci_projects_project_public_gateways_add_region_activate_line3',
-            )}
-          </OsdsText>
-        </OsdsMessage>
-      )}
     </StepComponent>
   );
 };
