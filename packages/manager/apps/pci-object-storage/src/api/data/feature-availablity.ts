96962bef7ad744438d932d4d44390495adf366a8
Mohammed Hamdoune <sidlynx@gmail.com>
2024-12-18 08:52:56 +0100

    fix(pci-object-storage): use common imports for availability and i18n
    Signed-off-by: Mohammed Hamdoune <sidlynx@gmail.com>

--- packages/manager/apps/pci-object-storage/src/api/data/feature-availablity.ts
+++
@@ -1,9 +0,0 @@
-import { aapi } from '@ovh-ux/manager-core-api';
-
-export const getFeatureAvailability = async (feature: string, app: string) => {
-  const { data } = await aapi.get<Record<string, boolean>>(
-    `/feature/${feature}/availability?app=${app}`,
-  );
-
-  return data;
-};
