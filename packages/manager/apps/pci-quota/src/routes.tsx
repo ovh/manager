ac8fc3754b9ae345889023dfa865336084a9ffcd
Mohammed Hamdoune <sidlynx@gmail.com>
2024-12-19 09:14:31 +0100

    feat(pci-quota): init routes and pages
    Signed-off-by: Mohammed Hamdoune <sidlynx@gmail.com>

--- packages/manager/apps/pci-quota/src/routes.tsx
+++ packages/manager/apps/pci-quota/src/routes.tsx
@@ -2,15 +2,30 @@ import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 const ROUTE_PATHS = {
-  ROOT: '/pci/projects/:projectId/quota',
-  LISTING: '',
+  ROOT: '/pci/projects/:projectId',
+  QUOTA: 'quota',
+  INCREASE_QUOTA: 'increase/contact-support',
+  REGIONS: 'regions',
 };
 
 const LayoutPage = lazy(() => import('@/pages/Layout'));
+const QuotaPage = lazy(() => import('@/pages/quota/Quota.page'));
+const IncreaseQuotaPage = lazy(() =>
+  import('@/pages/quota/increase/IncreaseQuota.page'),
+);
+const RegionsPage = lazy(() => import('@/pages/regions/Regions.page'));
 
 const RoutesComponent = () => (
   <Routes>
-    <Route id="root" path={ROUTE_PATHS.ROOT} Component={LayoutPage}></Route>
+    <Route id="root" path={ROUTE_PATHS.ROOT} Component={LayoutPage}>
+      <Route path={ROUTE_PATHS.QUOTA} Component={QuotaPage}>
+        <Route
+          path={ROUTE_PATHS.INCREASE_QUOTA}
+          Component={IncreaseQuotaPage}
+        ></Route>
+      </Route>
+      <Route path={ROUTE_PATHS.REGIONS} Component={RegionsPage} />
+    </Route>
     <Route path="" element={<>Page not found</>}></Route>
   </Routes>
 );
