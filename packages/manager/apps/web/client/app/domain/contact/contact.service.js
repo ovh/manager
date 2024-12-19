0ab997404633bd9fd05dfdd2377bbafdabac57cf
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2024-12-19 18:57:11 +0100

    feat(web.domain): form edit owner contact
    ref: MANAGER-15831
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/apps/web/client/app/domain/contact/contact.service.js
+++ packages/manager/apps/web/client/app/domain/contact/contact.service.js
@@ -9,4 +9,31 @@ export default class ContactService {
       .get(`/domain/${serviceName}/serviceInfos`)
       .then(({ data }) => data);
   }
+
+  getDomainContactInformations(contactId) {
+    return this.$http
+      .get(`/domain/contact/${contactId}`)
+      .then(({ data }) => data);
+  }
+
+  getDomainConfigurationRule(action, domain) {
+    return this.$http
+      .get(`/domain/configurationRule?action=${action}&domain=${domain}`)
+      .then(({ data }) => data);
+  }
+
+  postDomainConfigurationRuleCheck(action, domain, params) {
+    return this.$http
+      .post(
+        `/domain/configurationRule/check?action=${action}&domain=${domain}`,
+        params,
+      )
+      .then(({ data }) => data);
+  }
+
+  putDomainContact(contactId, params) {
+    return this.$http
+      .put(`/domain/contact/${contactId}`, params)
+      .then(({ data }) => data);
+  }
 }
