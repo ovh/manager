78cb9c44aae58654c0d3f17b46e94a9093f3b29d
Simon Chaumet <simon.chaumet.ext@ovhcloud.com>
2024-12-19 12:05:23 +0100

    feat(pci-common): add 3AZ region and 1AZ chip
    ref: TAPC-2357
    
    Signed-off-by: Simon Chaumet <simon.chaumet.ext@ovhcloud.com>

--- packages/manager/modules/manager-pci-common/src/components/region-selector/Region3AZChip.component.spec.tsx
+++ packages/manager/modules/manager-pci-common/src/components/region-selector/Region3AZChip.component.spec.tsx
@@ -1,30 +1,30 @@
 import { render, screen } from '@testing-library/react';
 import { describe, it } from 'vitest';
-import { RegionLocalzoneChip } from './RegionLocalzoneChip.component';
 import { wrapper } from '@/wrapperRenders';
+import { Region3AZChip } from '@/components/region-selector/Region3AZChip.component';
 import { URL_INFO } from '@/components/region-selector/constants';
 
-describe('RegionLocalzoneChip', () => {
+describe('Region3AZChip', () => {
   it.each([undefined, false, true])(
     'should render chip with showTooltip %s with correct texts and links',
     (showTooltip: boolean | undefined) => {
-      render(<RegionLocalzoneChip showTooltip={showTooltip} />, { wrapper });
+      render(<Region3AZChip showTooltip={showTooltip} />, { wrapper });
 
       expect(
-        screen.getByText('pci_project_flavors_zone_localzone'),
+        screen.getByText('pci_project_flavors_zone_3AZ'),
       ).toBeInTheDocument();
 
       if (showTooltip ?? true) {
         expect(
-          screen.getByText('pci_project_flavors_zone_localzone_tooltip'),
+          screen.getByText('pci_project_flavors_zone_3AZ_tooltip'),
         ).toBeInTheDocument();
 
         const link = screen.getByText('pci_project_flavors_zone_tooltip_link');
         expect(link).toBeInTheDocument();
-        expect(link).toHaveAttribute('href', URL_INFO.LOCAL_ZONE.DEFAULT);
+        expect(link).toHaveAttribute('href', URL_INFO.REGION_3AZ.DEFAULT);
       } else {
         expect(
-          screen.queryByText('pci_project_flavors_zone_localzone_tooltip'),
+          screen.queryByText('pci_project_flavors_zone_3AZ_tooltip'),
         ).not.toBeInTheDocument();
         expect(
           screen.queryByText('pci_project_flavors_zone_tooltip_link'),
