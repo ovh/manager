b09461edcacaf8635c0168383c5a43af9db31cc1
Maxime Bajeux <max.bajeux@gmail.com>
2024-12-20 09:21:04 +0100

    feat(billing): backport myservices (#14474)
    ref: MANAGER-16295
    
    Signed-off-by: Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>

--- packages/manager/modules/new-billing/src/autoRenew/ssh/ssh.routing.js
+++ packages/manager/modules/new-billing/src/autoRenew/ssh/ssh.routing.js
@@ -1,5 +1,9 @@
 import controller from './user-ssh.controller';
 import template from './user-ssh.html';
+import {
+  TRACKING_SSH_PAGE_NAME,
+  TRACKING_PAGE_CATEGORY,
+} from '../autorenew.constants';
 
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('billing.autorenew.ssh', {
@@ -7,6 +11,15 @@ export default /* @ngInject */ ($stateProvider) => {
     template,
     controller,
     controllerAs: 'ctrlSsh',
+    atInternet: {
+      ignore: true,
+    },
+    onEnter: /* @ngInject */ (atInternet) => {
+      atInternet.trackPage({
+        name: TRACKING_SSH_PAGE_NAME,
+        page_category: TRACKING_PAGE_CATEGORY,
+      });
+    },
     resolve: {
       breadcrumb: /* @ngInject */ ($translate) =>
         $translate.instant('user_ssh_title'),
