83782e50f98f4f1e70b8562cde68d98e2d86a5e4
ovh-ux-cds <ovh-ux-cds@users.noreply.github.com>
2024-12-20 15:07:04 +0000

    docs: update documentation [skip ci]

---
+++ storybook-static/assets/1.25-to-1.26.0-CjZk9i7U.js
@@ -0,0 +1,20 @@
+import{j as o}from"./jsx-runtime-CKrituN3.js";import{M as e}from"./index-IP2Szhac.js";import"./lib-ROnj4EVN.js";import"./useResourcesV6-CetFYNWH.js";import"./pci-guides-header.component-CYw2MwgI.js";import"./clipboard.component-Cq_PamW8.js";import"./Step.component-BwCGenD4.js";import"./Tabs.component-6XdXXCXW.js";import"./TilesInput.component-CdUZxOZS.js";import"./headers.component-BdCCroFE.js";import"./price.component-DkwHDtZT.js";import"./dashboard-tile.component-D9YrirQd.js";import"./card.component-BxiNf216.js";import"./action.component-C6Exu_6b.js";import"./guide.component-B7isF3gJ.js";import"./base.component-CVssbu4R.js";import"./error.component-BXkXZ1Lv.js";import"./onboarding.component-DT3FtwpL.js";import"./delete-modal.component-DdRFEHUo.js";import"./update-name-modal.component-C4Vs5Ht1.js";import"./title.component-Bx3BQPQH.js";import"./links.component-P6h2uIIa.js";import"./datagrid.component-CW6kKrFD.js";import"./index-CBqU2yxZ.js";import"./datagrid.contants-DE5a_Gky.js";import"./notifications.component-DMJsY8eS.js";import"./ods-notification-DYnlBovS.js";import"./filter-list.component-D3PKPMHI.js";import"./ManagerButton-Bz45hm9B.js";import"./ManagerText-DAScQPQm.js";import"./PciMaintenanceBanner.component-DS9YKxgS.js";import"./region.component-kKtC3z04.js";import{useMDXComponents as n}from"./index-DI5IigMn.js";import"./iframe-DOBPunBz.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BtM5VmRH.js";import"./index-D_r38UMq.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";import"./QueryClientProvider-CDvRjfR1.js";import"./useOvhIam-nH8cMKQ3.js";import"./index-v66SXByX.js";import"./index-C-Nbf5H5.js";import"./index-4N_owrwP.js";import"./mutation-BoanVz2M.js";import"./useTranslation-CvcVFFFk.js";import"./i18next-ihUiNgJT.js";import"./clsx-B-dksMZM.js";import"./v4-CQkTLCs1.js";import"./translation-CBNwEvwd.js";import"./click-utils-ByCElPrV.js";function i(t){const r=Object.assign({h1:"h1",h2:"h2",ol:"ol",li:"li",strong:"strong",blockquote:"blockquote",p:"p"},n(),t.components);return o.jsxs(o.Fragment,{children:[o.jsx(e,{title:"Manager React Components/What's new/Migration guides/1.25.0 to 1.26.0"}),`
+`,o.jsx(r.h1,{id:"migration-from-1250-to-1260",children:"Migration from 1.25.0 to 1.26.0"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(r.h2,{id:"few-steps-to-follow",children:"Few steps to follow"}),`
+`,o.jsx("br",{}),`
+`,o.jsxs(r.ol,{children:[`
+`,o.jsxs(r.li,{children:["Replace your dependancies in your ",o.jsx(r.strong,{children:"package.json"})]}),`
+`]}),`
+`,o.jsx("div",{children:o.jsx("strike",{children:'"@ovhcloud/manager-components": "^1.26.0"'})}),`
+`,o.jsx("div",{children:'"@ovh-ux/manager-react-components": "^1.26.0"'}),`
+`,o.jsx("br",{}),`
+`,o.jsxs(r.ol,{start:"2",children:[`
+`,o.jsxs(r.li,{children:["Replace your components ",o.jsx(r.strong,{children:"import"})]}),`
+`]}),`
+`,o.jsx("div",{children:o.jsx("strike",{children:'import {card} from  "@ovhcloud/manager-components"'})}),`
+`,o.jsx("div",{children:'import {card} from "@ovh-ux/manager-react-components"'}),`
+`,o.jsx("br",{}),`
+`,o.jsxs(r.blockquote,{children:[`
+`,o.jsxs(r.p,{children:["Next release ",o.jsx(r.strong,{children:"1.27.0"}),", we will publish the manager react component in the npm registry"]}),`
+`]})]})}function no(t={}){const{wrapper:r}=Object.assign({},n(),t.components);return r?o.jsx(r,Object.assign({},t,{children:o.jsx(i,t)})):i(t)}export{no as default};
---
+++ storybook-static/assets/documentation-VrZAoZ2s.js
@@ -0,0 +1,34 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{M as r}from"./index-IP2Szhac.js";import{useMDXComponents as s}from"./index-DI5IigMn.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DOBPunBz.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./index-D_r38UMq.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";function t(i){const n=Object.assign({h1:"h1",h2:"h2",p:"p",code:"code",ul:"ul",li:"li",strong:"strong",hr:"hr",h3:"h3",h4:"h4",h5:"h5",em:"em"},s(),i.components);return e.jsxs(e.Fragment,{children:[e.jsx(r,{title:"Components/Datagrid Cursor/Documentation"}),`
+`,e.jsx(n.h1,{id:"datagrid",children:"Datagrid"}),`
+`,e.jsx(n.h2,{id:"overview",children:"Overview"}),`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"Datagrid"})," is table component used for displaying and interacting with data"]}),`
+`,e.jsx(n.p,{children:"It supports advanced features like:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Sorting"}),": Enable column sorting with custom logic."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Infinite Scrolling"}),': Load additional data on demand with a "Load More" button.']}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Pagination"}),': Load additional data on demand with a "Load More" button.']}),`
+`]}),`
+`,e.jsx(n.h2,{id:"usage",children:"Usage"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Can be use with API V6 and API V2"}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"Pagination"})," mode is now deprecated"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"Cursor"})," navigation is recommended ",e.jsx("a",{target:"_blank",href:"https://apiv2.pages.ovhcloud.tools/deep-dive/seamless/pagination/",children:"link"})]}),`
+`]}),`
+`,e.jsx(n.hr,{}),`
+`,e.jsxs(n.h3,{id:"tanstack-table-v8",children:["Tanstack Table ",e.jsx("a",{target:"_blank",href:"https://tanstack.com/table/latest/docs/introduction",children:"V8"})]}),`
+`,e.jsx(n.p,{children:"The TanStack Table (formerly React Table) is a lightweight, powerful, and highly customizable library for building data grids and tables in React applications. It provides a modular architecture and advanced features to handle complex table requirements efficiently."}),`
+`,e.jsx(n.p,{children:"Why Use TanStack Table?"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Flexibility"}),": Customize every aspect of the table, from rendering to state management."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Performance"}),": Lightweight and optimized for large datasets."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Feature-Rich"}),": Sorting, filtering, pagination, and virtualization out of the box."]}),`
+`]}),`
+`,e.jsx(n.hr,{}),`
+`,e.jsx(n.h4,{id:"api-v6",children:"API V6"}),`
+`,e.jsx(n.h5,{id:"usage-with-iceberg",children:"usage with Iceberg"}),`
+`,e.jsxs(n.p,{children:["We provide a custom hook ",e.jsx(n.code,{children:"useResourcesIcebergV6"})]}),`
+`,e.jsx(n.h5,{id:"usage-without-iceberg",children:"usage without Iceberg"}),`
+`,e.jsxs(n.p,{children:["We provide a custom hook ",e.jsx(n.code,{children:"useResourcesV6"})]}),`
+`,e.jsx(n.h4,{id:"api-v2",children:"API V2"}),`
+`,e.jsxs(n.h5,{id:"usage-only-with-iceberg",children:["usage ",e.jsx(n.strong,{children:e.jsx(n.em,{children:"ONLY"})})," with Iceberg"]}),`
+`,e.jsxs(n.p,{children:["We provide a custom hook ",e.jsx(n.code,{children:"useResourcesIcebergV2"})]})]})}function p(i={}){const{wrapper:n}=Object.assign({},s(),i.components);return n?e.jsx(n,Object.assign({},i,{children:e.jsx(t,i)})):t(i)}export{p as default};
