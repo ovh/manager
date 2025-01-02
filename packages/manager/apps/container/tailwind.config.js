b65ac139f84c00fecad11c986e5ded81d9de29e3
Frédéric Vilcot <fredericvilcot@gmail.com>
2025-01-02 12:15:02 +0100

    fix(*): revert config for core packages
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

--- packages/manager/apps/container/tailwind.config.js
+++ packages/manager/apps/container/tailwind.config.js
@@ -3,4 +3,8 @@ import config from '@ovh-ux/manager-tailwind-config';
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   ...config,
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    '../../../manager-react-components/src/**/*.{js,jsx,ts,tsx}',
+  ],
 };
--- packages/manager/apps/procedures/tailwind.config.js
+++ packages/manager/apps/procedures/tailwind.config.js
@@ -3,4 +3,5 @@ import config from '@ovh-ux/manager-tailwind-config';
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   ...config,
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
 };
