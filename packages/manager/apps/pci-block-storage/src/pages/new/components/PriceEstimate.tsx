5ae1d0c967adc7e9128ecba3a1ed60a3db365589
Simon Chaumet <simon.chaumet.ext@ovhcloud.com>
2025-01-03 08:41:39 +0100

    fix(pci-block-storage): use 730 hours for monthly price (#14467)
    ref: TAPC-2177
    
    Signed-off-by: Simon Chaumet <simon.chaumet.ext@ovhcloud.com>

--- packages/manager/apps/pci-block-storage/src/pages/new/components/PriceEstimate.tsx
+++ packages/manager/apps/pci-block-storage/src/pages/new/components/PriceEstimate.tsx
@@ -5,15 +5,16 @@ import {
   ODS_THEME_TYPOGRAPHY_LEVEL,
   ODS_THEME_TYPOGRAPHY_SIZE,
 } from '@ovhcloud/ods-common-theming';
-import { useCatalogPrice } from '@ovh-ux/manager-react-components';
+import {
+  useCatalogPrice,
+  convertHourlyPriceToMonthly,
+} from '@ovh-ux/manager-react-components';
 import { TAddon } from '@ovh-ux/manager-pci-common';
 
 export interface PriceEstimateProps {
   volumeCapacity: number;
   volumeType: TAddon;
 }
-
-const ESTIMATE_MONTHLY_HOURS = 24 * 30;
 
 export function PriceEstimate({
   volumeCapacity,
@@ -25,7 +26,7 @@ export function PriceEstimate({
   });
   const { price } =
     volumeType?.pricings?.find(({ type }) => type === 'consumption') || {};
-  const priceEstimate = price * volumeCapacity * ESTIMATE_MONTHLY_HOURS;
+  const priceEstimate = convertHourlyPriceToMonthly(price * volumeCapacity);
 
   return (
     <OsdsText
