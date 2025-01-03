717de9fd553382a78b07b6b6baa851fcd7e72e51
Frédéric Vilcot <47554752+fredericvilcot@users.noreply.github.com>
2025-01-03 08:45:24 +0100

    fix(*): update tailwind config for third party imports (#14675)
    ref: TAPC-2482
    
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>
    Co-authored-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager/modules/manager-pci-common/tailwind.config.js
+++ packages/manager/modules/manager-pci-common/tailwind.config.js
@@ -1,11 +1,15 @@
 import config from '@ovh-ux/manager-tailwind-config';
+import path from 'path';
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   ...config,
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
-    '../../../manager-react-components/src/**/*.{js,jsx,ts,tsx}',
+    path.join(
+      path.dirname(require.resolve('@ovh-ux/manager-react-components')),
+      '**/*.{js,jsx,ts,tsx}',
+    ),
   ],
   corePlugins: {
     preflight: false,
