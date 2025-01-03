7eaf0cda533e9a6dfd277d11ffce92a7d2f3c56f
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-01-03 16:31:28 +0100

    feat(ips): order ipv4 vrack
    ref: MANAGER-15838
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

---
+++ packages/manager/apps/ips/src/data/hooks/catalog/catalog.utils.ts
@@ -0,0 +1,40 @@
+import { CatalogIpPlan } from '../../api/catalog';
+
+export const IP_FAILOVER_PLANCODE = {
+  EU: 'ip-failover-ripe',
+  CA: 'ip-failover-arin',
+  US: 'ip-failover-arin',
+};
+
+export const DATACENTER_TO_REGION: { [datacenter: string]: string } = {
+  RBX: 'eu-west-rbx',
+  GRA: 'eu-west-gra',
+  SBG: 'eu-west-sbg',
+  PAR: 'eu-west-par',
+  CR2: 'labeu-west-1-preprod',
+  LIM: 'eu-west-lim',
+  WAW: 'eu-central-waw',
+  ERI: 'eu-west-eri',
+  BHS: 'ca-east-bhs',
+  YYZ: 'ca-east-tor',
+  SGP: 'ap-southeast-sgp',
+  SYD: 'ap-southeast-syd',
+  YNM: 'ap-south-mum',
+  VIN: 'us-east-vin',
+  HIL: 'us-west-hil',
+};
+
+export const getDatacenterFromRegion = (inputRegion: string) =>
+  Object.entries(DATACENTER_TO_REGION).find(
+    ([, region]) => region === inputRegion,
+  )?.[0];
+
+export const getCatalogIpsQueryKey = (sub: string) => ['getCatalogIps', sub];
+
+export const isBlockIpPlan = (plan: CatalogIpPlan) => {
+  const defaultPricing = plan.details.pricings.default[0];
+  return (
+    defaultPricing.capacities.includes('renew') &&
+    defaultPricing.maximumQuantity === 1
+  );
+};
