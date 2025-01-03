160316df8e196c0a322faf55ace1fbf7860eecca
Anoop N <anoop.n@ovhcloud.com>
2025-01-03 19:47:26 +0530

    ci: automate MRC versioning and release
    Signed-off-by: Anoop N <anoop.n@ovhcloud.com>

--- scripts/release/release.sh
+++ scripts/release/release.sh
@@ -32,7 +32,29 @@ version() {
     node_modules/.bin/lerna version --conventional-commits --no-commit-hooks --no-git-tag-version --no-push --allow-branch="${GIT_BRANCH}" --yes
   else
     printf "%s\n" "Releasing"
-    node_modules/.bin/lerna version --conventional-commits --no-commit-hooks --no-git-tag-version --no-push  --yes
+    node_modules/.bin/lerna version --conventional-commits --no-commit-hooks --no-git-tag-version --no-push --yes --ignore-changes="packages/manager-react-components/**,packages/manager/modules/manager-pci-common/**"
+  fi
+}
+
+version_mrc() {
+  if [ -z "${GIT_BRANCH}" ]; then
+    printf "%s\n" "Missing GIT_BRANCH environment variable"
+    exit 1
+  fi
+
+  if [[ "${GIT_BRANCH}" != "master" && ! "${DRY_RELEASE}" ]]; then
+    printf "%s\n" "Only dry releases are allowed on side branches"
+    exit 1
+  fi
+
+  tag="$1"
+
+  if "${DRY_RELEASE}"; then
+    printf "%s\n" "Dry releasing"
+    node_modules/.bin/lerna version --scope=manager-react-components --conventional-commits --no-commit-hooks --no-git-tag-version --no-push --allow-branch="${GIT_BRANCH}" --yes
+  else
+    printf "%s\n" "Releasing"
+    node_modules/.bin/lerna exec --scope=@ovh-ux/manager-react-components -- lerna version --conventional-commits --no-commit-hooks --no-git-tag-version --no-push --no-private --ignore-changes="packages/manager/modules/manager-pci-common/**" --yes
   fi
 }
 
@@ -73,6 +95,12 @@ push_and_release() {
   fi
 }
 
+push_and_release_mrc() {
+  printf "%s\n" "Commit mrc changes"
+  git add packages/manager-react-components/package.json packages/manager-react-components/CHANGELOG.md
+  git commit -s --amend --no-edit
+}
+
 update_sonar_version() {
   printf "%s\n" "Updating sonar"
   sed -i "s/sonar\.projectVersion=.*/sonar\.projectVersion=$1/" ".sonarcloud.properties"
@@ -110,6 +138,7 @@ main() {
   done
 
   changed_packages=$(get_changed_packages)
+
   if [ -z "$changed_packages" ]; then
     printf "%s\n" "Nothing to release"
     exit 0
@@ -118,38 +147,62 @@ main() {
   current_tag="$(git describe --abbrev=0)"
   printf "%s\n" "Previous tag was $current_tag"
 
-  #For each package create semver tag in order to be used by lerna version
+  # Separate versioning for `manager-react-components` and other packages
+  mrc_changed=false
   while read -r package; do
     name=$(echo "$package" | cut -d ':' -f 2)
     version=$(echo "$package" | cut -d ':' -f 3)
-    create_smoke_tag "$current_tag" "$name" "$version"
+
+    # Check if the changed package is `manager-react-components`
+    if [[ "$name" == *manager-react-components* ]]; then
+      mrc_changed=true
+      path_mrc=$(echo "$package" | cut -d ':' -f 1)
+      name_mrc=$(echo "$package" | cut -d ':' -f 2)
+      create_smoke_tag "$current_tag" "$name_mrc" "$version"
+    else
+      create_smoke_tag "$current_tag" "$name" "$version"
+    fi
   done <<< "$changed_packages"
 
+
+  # Handle the rest of the packages
   next_tag=$(get_release_name "$SEED")
-  printf "%s\n" "New tag is $next_tag"
-
-  RELEASE_NOTE+="# Release $next_tag\
-
-
-"
-
+  printf "%s\n" "New tag for other packages: $next_tag"
+  RELEASE_NOTE+="# Release $next_tag\n\n"
   update_sonar_version "$next_tag"
   version "$next_tag"
 
-  #For each package generate formatted section in release note
+  # Generate formatted release notes for other packages
   while read -r package; do
     path=$(echo "$package" | cut -d ':' -f 1)
     name=$(echo "$package" | cut -d ':' -f 2)
-    RELEASE_NOTE+="$(create_release_note "$path" "$name")\
-
-
-"
+    RELEASE_NOTE+="$(create_release_note "$path" "$name")\n\n"
   done <<< "$changed_packages"
 
-  #Remove package specific tags
+  # Remove package-specific tags for other packages
   clean_tags
 
+  # Push and release for other packages
   push_and_release "$next_tag"
+
+  # Handle `manager-react-components` separately
+  if [ "$mrc_changed" == true ]; then
+    next_tag=$(get_release_name "$SEED")
+    printf "%s\n" "New tag for manager-react-components: $next_tag"
+
+    RELEASE_NOTE+="# Release $next_tag\n\n"
+
+    update_sonar_version "$next_tag"
+    version_mrc "$next_tag"
+
+    # Create release note for manager-react-components
+    RELEASE_NOTE+="$(create_release_note "$path_mrc" "$name_mrc")\n\n"
+
+    # Commit and release manager-react-components
+    clean_tags
+    push_and_release_mrc "$next_tag"
+  fi
+
 }
 
 main "${@}"
