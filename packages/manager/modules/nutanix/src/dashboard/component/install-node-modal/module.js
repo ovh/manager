a399576a3bbb6015ab8377ed336364e2397c09fa
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-01-05 15:20:42 +0100

    feat(nutanix): add install node popin
    ref: MANAGER-15712
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/nutanix/src/dashboard/component/install-node-modal/module.js
+++ packages/manager/modules/nutanix/src/dashboard/component/install-node-modal/module.js
@@ -1,33 +1,27 @@
 import angular from 'angular';
+import '@ovh-ux/manager-core';
 import '@uirouter/angularjs';
 import 'angular-translate';
-import '@ovh-ux/ng-translate-async-loader';
 import '@ovh-ux/ui-kit';
 
-import confirmRedeploy from './confirm';
-
+import component from './component';
 import ipSubnetValidator from '../../directive/ip-subnet-validator.directive';
 import uniqueIpValidator from '../../directive/unique-ip-validator.directive';
 import ipValidator from '../../directive/ip-validator.directive';
-import component from './component';
-import routing from './routing';
 
-const moduleName = 'ovhManagerNutanixDashboardGeneralInfoRedeploy';
+const moduleName = 'ovhManagerNutanixInstallNodeModal';
 
 angular
   .module(moduleName, [
     'oui',
-    'ngTranslateAsyncLoader',
+    'ovhManagerCore',
     'pascalprecht.translate',
-    'ngUiRouterBreadcrumb',
     'ui.router',
-    confirmRedeploy,
   ])
-  .config(routing)
+  .component('nutanixInstallNodeModal', component)
   .directive('nutanixIpSubnetValidator', ipSubnetValidator)
   .directive('nutanixUniqueIpValidator', uniqueIpValidator)
   .directive('nutanixIpValidator', ipValidator)
-  .component('nutanixDashboardGeneralInfoRedeployComponent', component)
   .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
