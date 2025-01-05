ecfcfa4acb790bad46433fd5c37896e558a94bce
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-01-05 15:20:42 +0100

    feat(nutanix): add install node popin
    ref: MANAGER-15712
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/nutanix/src/dashboard/nodes/list/module.js
+++ packages/manager/modules/nutanix/src/dashboard/nodes/list/module.js
@@ -12,6 +12,7 @@ import nodeOsDatagrid from '../../component/service-status/module';
 import addNodes from './add-nodes';
 import poweronNode from './poweron-node';
 import poweroffNode from './poweroff-node';
+import installNode from './install-node';
 import uninstallNode from './uninstall-node';
 import resiliateNode from './resiliate-node';
 
@@ -30,6 +31,7 @@ angular
     addNodes,
     poweronNode,
     poweroffNode,
+    installNode,
     uninstallNode,
     resiliateNode,
   ])
