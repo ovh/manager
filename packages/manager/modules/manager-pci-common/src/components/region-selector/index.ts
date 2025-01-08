dd56aa749c2e85d81276ebfc0f037f4a2f9b7ffb
Frédéric Vilcot <fredericvilcot@gmail.com>
2025-01-08 15:31:14 +0100

    build(pci-common): enhance components exports
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

---
+++ packages/manager/modules/manager-pci-common/src/components/region-selector/index.ts
@@ -0,0 +1,7 @@
+export * from './RegionSelector.component';
+export * from './RegionSummary.component';
+export * from './RegionSelector.component';
+export * from './RegionGlobalzoneChip.component';
+export * from './RegionLocalzoneChip.component';
+export * from './RegionTile';
+export * from './useRegions';
