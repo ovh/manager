4a4f543798a82504c091639aa530611df92b3339
mhelhali-soufien <159853039+mhelhali-soufien@users.noreply.github.com>
2025-01-08 07:19:31 +0100

    feat(dedicated): occ diagnostic project
    ref: MANAGER-15324
    
    Signed-off-by: soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/cloud-connect/src/details/overview/overview.component.js
+++ packages/manager/modules/cloud-connect/src/details/overview/overview.component.js
@@ -21,6 +21,8 @@ export default {
     goToSendServiceKeyPage: '<',
     goToManageServiceKeysPage: '<',
     goToChangeBandwidthPage: '<',
+    goToCheckBGPPeeringPage: '<',
+    diagnosticPageUrl: '<',
     user: '<',
   },
   controller,
