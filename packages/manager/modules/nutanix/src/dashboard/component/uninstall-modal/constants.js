8ef43e923fa555e23cc1b380953d79e1efb3640e
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-01-08 12:45:21 +0100

    fixup! feat(nutanix): add uninstall node nutanix popin
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/nutanix/src/dashboard/component/uninstall-modal/constants.js
+++ packages/manager/modules/nutanix/src/dashboard/component/uninstall-modal/constants.js
@@ -1,9 +1,5 @@
 export const UNINSTALL_PATTERN = 'UNINSTALL';
 
-export const UNINSTALL_GUIDE_LINK =
-  'https://portal.nutanix.com/page/documents/list?type=software';
-
 export default {
   UNINSTALL_PATTERN,
-  UNINSTALL_GUIDE_LINK,
 };
