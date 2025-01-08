a5390594768a381a79632b7c267467f773fe2d4a
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2025-01-08 14:58:09 +0100

    fix(pci-object-storage): migrate and fix import s3 policy modal
    ref: DTCORE-2950
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/apps/pci-object-storage/src/components/FileInput.component.tsx
+++ packages/manager/apps/pci-object-storage/src/components/FileInput.component.tsx
@@ -1,17 +1,6 @@
 import { useBytes } from '@ovh-ux/manager-pci-common';
-import {
-  ODS_THEME_COLOR_INTENT,
-  ODS_THEME_SIZE,
-  ODS_THEME_TYPOGRAPHY_LEVEL,
-} from '@ovhcloud/ods-common-theming';
-import {
-  ODS_BUTTON_SIZE,
-  ODS_BUTTON_VARIANT,
-  ODS_ICON_NAME,
-  ODS_ICON_SIZE,
-  ODS_TEXT_SIZE,
-} from '@ovhcloud/ods-components';
-import { OsdsButton, OsdsIcon, OsdsText } from '@ovhcloud/ods-components/react';
+import { ODS_BUTTON_SIZE, ODS_BUTTON_VARIANT } from '@ovhcloud/ods-components';
+import { OdsButton, OdsIcon, OdsText } from '@ovhcloud/ods-components/react';
 import { ChangeEvent, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
@@ -23,46 +12,38 @@ export default function FileInputComponent({
   onFilesSelected,
 }: Readonly<FileInputComponentProps>) {
   const { t } = useTranslation('pci-common');
+
   const { formatBytes } = useBytes();
   const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
   const fileInputRef = useRef<HTMLInputElement | null>(null);
 
-  // Gérer la sélection des fichiers
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     const newFiles = Array.from(event.target.files);
 
     const updatedFiles = [...selectedFiles, ...newFiles];
     setSelectedFiles(updatedFiles);
-    onFilesSelected(updatedFiles); // Met à jour la liste des fichiers dans le parent
+    onFilesSelected(updatedFiles);
   };
 
-  // Supprimer un fichier de la liste
   const handleRemoveFile = (index: number) => {
     const updatedFiles = selectedFiles.filter((_, i) => i !== index);
     setSelectedFiles(updatedFiles);
-    onFilesSelected(updatedFiles); // Met à jour la liste des fichiers dans le parent
+    onFilesSelected(updatedFiles);
   };
 
-  // Ouvrir la boîte de dialogue de fichiers
   const handleOpenFileDialog = () => {
     fileInputRef.current.click();
   };
 
   return (
     <div>
-      <OsdsButton
+      <OdsButton
+        label={t('common_file_filesSelector')}
         size={ODS_BUTTON_SIZE.sm}
+        icon="folder"
         onClick={handleOpenFileDialog}
-        color={ODS_THEME_COLOR_INTENT.primary}
         class="w-fit"
-      >
-        <OsdsIcon
-          name={ODS_ICON_NAME.FOLDER}
-          size={ODS_ICON_SIZE.xxs}
-          className="bg-white mr-6 align-middle"
-        />
-        {t('common_file_filesSelector')}
-      </OsdsButton>
+      />
 
       <input
         type="file"
@@ -74,47 +55,32 @@ export default function FileInputComponent({
 
       {selectedFiles.length > 0 && (
         <div className="mt-4">
-          <OsdsText
-            level={ODS_THEME_TYPOGRAPHY_LEVEL.caption}
-            size={ODS_TEXT_SIZE._100}
-            color={ODS_THEME_COLOR_INTENT.text}
-          >
+          <OdsText preset="caption">
             {t('common_file_attachmentsHeading')}
-          </OsdsText>
-          {selectedFiles.map((file, index) => (
-            <div
-              key={index}
-              className="flex items-center justify-between bg-[var(--ods-color-primary-075)] border-color-[var(--ods-color-primary-700)] p-2 pl-4 rounded mb-2"
-            >
-              <div>
-                <OsdsIcon
-                  name={ODS_ICON_NAME.FILE}
-                  size={ODS_ICON_SIZE.xs}
-                  color={ODS_THEME_COLOR_INTENT.primary}
-                  className="mr-2 align-middle"
-                />
-                <OsdsText
-                  color={ODS_THEME_COLOR_INTENT.text}
-                  level={ODS_THEME_TYPOGRAPHY_LEVEL.caption}
-                  size={ODS_TEXT_SIZE._100}
-                >
-                  {file.name} ({formatBytes(file.size)})
-                </OsdsText>
-              </div>
-              <OsdsButton
-                onClick={() => handleRemoveFile(index)}
-                variant={ODS_BUTTON_VARIANT.ghost}
-                size={ODS_BUTTON_SIZE.sm}
-                className="text-red-600 hover:text-red-800"
+          </OdsText>
+
+          <div className="flex flex-col gap-3 my-4">
+            {selectedFiles.map((file, index) => (
+              <div
+                key={index}
+                className="flex w-full items-center justify-between bg-[--ods-color-information-100] border-color-[--ods-color-information-300] rounded"
               >
-                <OsdsIcon
-                  name={ODS_ICON_NAME.CLOSE}
-                  size={ODS_ICON_SIZE.xxs}
-                  color={ODS_THEME_COLOR_INTENT.primary}
+                <div className="flex gap-2 items-center">
+                  <OdsIcon name="file" className="text-blue-600" />
+                  <OdsText preset="caption">
+                    {file.name} ({formatBytes(file.size)})
+                  </OdsText>
+                </div>
+                <OdsButton
+                  icon="xmark"
+                  label=""
+                  onClick={() => handleRemoveFile(index)}
+                  variant={ODS_BUTTON_VARIANT.ghost}
+                  size={ODS_BUTTON_SIZE.xs}
                 />
-              </OsdsButton>
-            </div>
-          ))}
+              </div>
+            ))}
+          </div>
         </div>
       )}
     </div>
