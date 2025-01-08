79dddadf2afbadbc37d02b173db8d3512b115926
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2025-01-08 10:56:00 +0100

    fix(pci-object-storage): fix object versioning activation
    ref: DTCORE-2950
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/apps/pci-object-storage/src/pages/objects/container/object/show/Show.page.tsx
+++ packages/manager/apps/pci-object-storage/src/pages/objects/container/object/show/Show.page.tsx
@@ -14,6 +14,7 @@ import {
   FilterList,
   Links,
   LinkType,
+  Notifications,
   PciMaintenanceBanner,
   useColumnFilters,
   useDataGrid,
@@ -227,6 +228,8 @@ export default function ObjectPage() {
       )}`}
       hrefPrevious={objectStorageHref}
     >
+      <Notifications />
+
       {hasMaintenance && (
         <PciMaintenanceBanner maintenanceURL={maintenanceURL} />
       )}
