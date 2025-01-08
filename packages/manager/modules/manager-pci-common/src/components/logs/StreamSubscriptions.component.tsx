096c3708350d7e13ba9102607ce0ca9efa608795
Florian Renaut <florian.renaut@corp.ovh.com>
2025-01-08 12:01:53 +0100

    fix(manager-pci-common): subscriptions url (#14797)
    ref: DTCORE-2947
    
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/modules/manager-pci-common/src/components/logs/StreamSubscriptions.component.tsx
+++ packages/manager/modules/manager-pci-common/src/components/logs/StreamSubscriptions.component.tsx
@@ -98,7 +98,7 @@ export function StreamSubscriptions({
     navigation
       .getURL(
         'dedicated',
-        `#/dbaas/logs/${serviceName}/streams/${streamId}/subscriptions`,
+        `#/dbaas/logs/${account}/streams/${streamId}/subscriptions`,
         {},
       )
       .then(setSubscriptionsURL);
