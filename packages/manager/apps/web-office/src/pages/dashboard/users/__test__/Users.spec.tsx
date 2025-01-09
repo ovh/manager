6f50c35035edadfcff8e03b507fb820a130ca19d
stif59100 <steeve.vanderstocken@ovhcloud.com>
2025-01-09 09:56:51 +0100

    feat(web-office): add button order user or licences
    ref:MANAGER-16542
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>
    Co-authored-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/apps/web-office/src/pages/dashboard/users/__test__/Users.spec.tsx
+++ packages/manager/apps/web-office/src/pages/dashboard/users/__test__/Users.spec.tsx
@@ -2,21 +2,54 @@ import React from 'react';
 import { describe, expect, vi } from 'vitest';
 import Users from '../Users';
 import { render } from '@/utils/test.provider';
-import usersTranslation from '@/public/translations/dashboard/users/Messages_fr_FR.json';
-import { licensesMock, usersMock } from '@/api/_mock_';
+import {
+  licensesMock,
+  licensesPrepaidExpandedMock,
+  usersMock,
+} from '@/api/_mock_';
+
+const hoistedMock = vi.hoisted(() => ({
+  useOfficeUsers: vi.fn(),
+  useOfficeLicenseDetail: vi.fn(),
+}));
+
+vi.mock('@/hooks', async (importActual) => {
+  const actual = await importActual<typeof import('@/hooks')>();
+  return {
+    ...actual,
+    useOfficeUsers: hoistedMock.useOfficeUsers,
+    useOfficeLicenseDetail: hoistedMock.useOfficeLicenseDetail,
+  };
+});
 
 describe('Users page', () => {
-  vi.mock('@/hooks', () => {
-    return {
-      useOfficeUsers: vi.fn(() => usersMock),
-      useOfficeLicenseDetail: vi.fn(() => licensesMock),
-    };
+  it('Page for payAsYouGo', () => {
+    hoistedMock.useOfficeLicenseDetail.mockReturnValue({
+      data: licensesMock[0],
+      isLoading: false,
+    });
+    hoistedMock.useOfficeUsers.mockReturnValue({
+      data: usersMock,
+      isLoading: false,
+    });
+
+    const { getByTestId } = render(<Users />);
+    const orderButton = getByTestId('user-or-licenses-order-button');
+    expect(orderButton).toHaveAttribute('label', 'Ajouter un utilisateur');
   });
 
-  it('Page should display correctly', async () => {
-    const { findByText } = render(<Users />);
-    expect(
-      await findByText(usersTranslation.dashboard_users_download_info),
-    ).toBeVisible();
+  it('Page for prepaid', () => {
+    hoistedMock.useOfficeLicenseDetail.mockReturnValue({
+      data: licensesPrepaidExpandedMock[0],
+      isLoading: false,
+    });
+    hoistedMock.useOfficeUsers.mockReturnValue({
+      data: licensesPrepaidExpandedMock[0],
+      isLoading: false,
+    });
+
+    const { getByTestId } = render(<Users />);
+    const orderButton = getByTestId('user-or-licenses-order-button');
+    expect(orderButton).toHaveAttribute('label', 'Commander plus de licences');
   });
 });
