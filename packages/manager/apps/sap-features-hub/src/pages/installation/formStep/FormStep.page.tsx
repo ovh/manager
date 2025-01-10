157de141f9ff6fa003f6b03002bd3f281952b395
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-01-10 19:57:35 +0100

    feat(sap-features-hub): add pre-installation form step3
    ref: MANAGER-15977
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/sap-features-hub/src/pages/installation/formStep/FormStep.page.tsx
+++ packages/manager/apps/sap-features-hub/src/pages/installation/formStep/FormStep.page.tsx
@@ -1,15 +1,32 @@
 import React, { ReactNode } from 'react';
+import { OdsButton } from '@ovhcloud/ods-components/react';
+import { useTranslation } from 'react-i18next';
 import { useFormSteps } from '@/hooks/formStep/useFormSteps';
 import InstallationInitialStep from '../initialStep/InstallationInitialStep.page';
 import InstallationStepDeployment from '../stepDeployment/InstallationStepDeployment.page';
+import InstallationStepSystemInformation from '../stepSystemInformation/InstallationStepSystemInformation.page';
 
 const steps: Record<string, ReactNode> = {
   '1': <InstallationInitialStep />,
   '2': <InstallationStepDeployment />,
-};
+  '3': <InstallationStepSystemInformation />,
+} as const;
 
 export default function FormStep() {
   const { currentStep } = useFormSteps();
+  const { t } = useTranslation('installation');
+  const { previousStep } = useFormSteps();
 
-  return steps[currentStep] || <div>Not developed yet.</div>;
+  return (
+    steps[currentStep] || (
+      <div>
+        <p>Not developed yet.</p>
+        <OdsButton
+          label={t('previous_step_cta')}
+          variant="outline"
+          onClick={previousStep}
+        />
+      </div>
+    )
+  );
 }
