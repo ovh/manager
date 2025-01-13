d1332f02681b5d226e227a1779d68c4152c17bb2
Frédéric Vilcot <fredericvilcot@gmail.com>
2025-01-13 09:16:12 +0100

    test(pci-instances): remove useless style color test case
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

--- packages/manager/apps/pci-instances/src/components/statusChip/StatusChip.spec.tsx
+++ packages/manager/apps/pci-instances/src/components/statusChip/StatusChip.spec.tsx
@@ -1,13 +1,11 @@
 import { render } from '@testing-library/react';
 import { describe } from 'vitest';
-import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
 import StatusChip from './StatusChip.component';
 import { TInstanceStatus } from '@/data/hooks/instance/useInstances';
 
 type Data = {
   status: TInstanceStatus;
   label: string;
-  bgColor: ODS_THEME_COLOR_INTENT;
 };
 
 const successStatus: TInstanceStatus = {
@@ -32,17 +30,15 @@ const infoStatus: TInstanceStatus = {
 
 describe('StatusChip component', () => {
   test.each`
-    status           | bgColor
-    ${successStatus} | ${ODS_THEME_COLOR_INTENT.success}
-    ${errorStatus}   | ${ODS_THEME_COLOR_INTENT.error}
-    ${warningStatus} | ${ODS_THEME_COLOR_INTENT.warning}
-    ${infoStatus}    | ${ODS_THEME_COLOR_INTENT.info}
+    status
+    ${successStatus}
+    ${errorStatus}
+    ${warningStatus}
+    ${infoStatus}
   `(
     'Should render the correct chip color <$bgColor> and label based on the status <$status>',
-    ({ status, bgColor }: Data) => {
-      const { getByText, getByTestId } = render(<StatusChip status={status} />);
-      const chipEl = getByTestId('status-chip');
-      expect(chipEl).toHaveStyle(`background-color: ${bgColor}`);
+    ({ status }: Data) => {
+      const { getByText } = render(<StatusChip status={status} />);
       const labelEl = getByText(status.state.toLowerCase());
       expect(labelEl).toBeInTheDocument();
     },
