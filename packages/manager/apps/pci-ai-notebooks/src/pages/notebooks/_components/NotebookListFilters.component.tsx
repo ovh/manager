1bc58d5f634b4b2e23395822982434d6714dc75d
Arthur Bullet <arthur.bullet@ovhcloud.com>
2025-01-13 10:53:28 +0100

    feat(pci.ai.notebook): fix pr comments
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>

--- packages/manager/apps/pci-ai-notebooks/src/pages/notebooks/_components/NotebookListFilters.component.tsx
+++ packages/manager/apps/pci-ai-notebooks/src/pages/notebooks/_components/NotebookListFilters.component.tsx
@@ -5,10 +5,11 @@ import NotebookStatusBadge from './NotebookStatusBadge.component';
 
 export const getFilters = () => {
   const { t: tRegions } = useTranslation('regions');
+  const { t } = useTranslation('pci-ai-notebooks/notebooks');
   return [
     {
       id: 'spec.name',
-      label: 'Nom',
+      label: t('tableHeaderName'),
       comparators: FilterCategories.String,
     },
     {
@@ -18,7 +19,7 @@ export const getFilters = () => {
     },
     {
       id: 'spec.region',
-      label: 'Region',
+      label: t('tableHeaderLocation'),
       comparators: FilterCategories.Options,
       options: ['BHS', 'DE', 'GRA', 'SBG', 'UK', 'WAW'].map((region) => ({
         label: tRegions(`region_${region}`),
@@ -27,17 +28,17 @@ export const getFilters = () => {
     },
     {
       id: 'spec.env.frameworkId',
-      label: 'Framework',
+      label: t('tableHeaderFramework'),
       comparators: FilterCategories.String,
     },
     {
       id: 'spec.env.editorId',
-      label: 'Editor',
+      label: t('tableHeaderEditor'),
       comparators: FilterCategories.String,
     },
     {
       id: 'status.state',
-      label: 'Statut',
+      label: t('tableHeaderStatus'),
       comparators: FilterCategories.Options,
       options: Object.values(ai.notebook.NotebookStateEnum).map((state) => ({
         label: <NotebookStatusBadge status={state} />,
