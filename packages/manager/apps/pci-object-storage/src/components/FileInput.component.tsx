b6b2ef0d4eb5958b02dd0c1c377ceef1341ac168
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2025-01-13 11:42:20 +0100

    feat(pci-object-storage): use to fileInput comming from ods 18
    ref: DTCORE-2950
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/apps/pci-object-storage/src/components/FileInput.component.tsx
+++ packages/manager/apps/pci-object-storage/src/components/FileInput.component.tsx
@@ -1,88 +1,44 @@
-import { useBytes } from '@ovh-ux/manager-pci-common';
-import { ODS_BUTTON_SIZE, ODS_BUTTON_VARIANT } from '@ovhcloud/ods-components';
-import { OdsButton, OdsIcon, OdsText } from '@ovhcloud/ods-components/react';
-import { ChangeEvent, useRef, useState } from 'react';
+import { OdsFile } from '@ovhcloud/ods-components';
+import { OdsFileUpload } from '@ovhcloud/ods-components/react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 type FileInputComponentProps = {
+  dropzoneLabel: string;
   onFilesSelected: (files: File[]) => void;
 };
 
 export default function FileInputComponent({
+  dropzoneLabel,
   onFilesSelected,
 }: Readonly<FileInputComponentProps>) {
   const { t } = useTranslation('pci-common');
 
-  const { formatBytes } = useBytes();
-  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const [selectedFiles, setSelectedFiles] = useState<OdsFile[]>([]);
 
-  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const newFiles = Array.from(event.target.files);
+  const handleFileChange = ({ detail }) => {
+    const newFiles = Array.from(detail.files) as File[];
 
     const updatedFiles = [...selectedFiles, ...newFiles];
     setSelectedFiles(updatedFiles);
     onFilesSelected(updatedFiles);
   };
 
-  const handleRemoveFile = (index: number) => {
-    const updatedFiles = selectedFiles.filter((_, i) => i !== index);
+  const handleRemoveFile = ({ detail }) => {
+    const updatedFiles = selectedFiles.filter(
+      (file) => file.odsId !== detail.odsId,
+    );
     setSelectedFiles(updatedFiles);
-    onFilesSelected(updatedFiles);
-  };
-
-  const handleOpenFileDialog = () => {
-    fileInputRef.current.click();
   };
 
   return (
-    <div>
-      <OdsButton
-        label={t('common_file_filesSelector')}
-        size={ODS_BUTTON_SIZE.sm}
-        icon="folder"
-        onClick={handleOpenFileDialog}
-        class="w-fit"
-      />
-
-      <input
-        type="file"
-        multiple
-        ref={fileInputRef}
-        onChange={handleFileChange}
-        className="hidden"
-      />
-
-      {selectedFiles.length > 0 && (
-        <div className="mt-4">
-          <OdsText preset="caption">
-            {t('common_file_attachmentsHeading')}
-          </OdsText>
-
-          <div className="flex flex-col gap-3 my-4">
-            {selectedFiles.map((file, index) => (
-              <div
-                key={index}
-                className="flex w-full items-center justify-between bg-[--ods-color-information-100] border-color-[--ods-color-information-300] rounded"
-              >
-                <div className="flex gap-2 items-center">
-                  <OdsIcon name="file" className="text-blue-600" />
-                  <OdsText preset="caption">
-                    {file.name} ({formatBytes(file.size)})
-                  </OdsText>
-                </div>
-                <OdsButton
-                  icon="xmark"
-                  label=""
-                  onClick={() => handleRemoveFile(index)}
-                  variant={ODS_BUTTON_VARIANT.ghost}
-                  size={ODS_BUTTON_SIZE.xs}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
-    </div>
+    <OdsFileUpload
+      className="w-full"
+      dropzoneLabel={dropzoneLabel}
+      browseFileLabel={t('common_file_filesSelector')}
+      files={selectedFiles}
+      onOdsChange={handleFileChange}
+      onOdsCancel={handleRemoveFile}
+    />
   );
 }
