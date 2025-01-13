1b3d868643c68abdb6d226bd41fb7dd6f750ca11
Thibault Barske (Tibs) <thibault.barske.ext@corp.ovh.com>
2025-01-13 16:26:45 +0100

    feat: terminate nutanix cluster rebased (#14763)
    ref: MANAGER-15062
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/billing-components/src/components/services-actions/services-actions.component.js
+++ packages/manager/modules/billing-components/src/components/services-actions/services-actions.component.js
@@ -11,6 +11,8 @@ export default {
     getCancelCommitmentLink: '&?',
     getCancelResiliationLink: '&?',
     getResiliationLink: '&?',
+    handleGoToResiliation: '&?',
+    isCustomResiliationHandled: '<?',
   },
   controller,
   template,
--- packages/manager/modules/nutanix/src/dashboard/nodes/routing.js
+++ packages/manager/modules/nutanix/src/dashboard/nodes/routing.js
@@ -1,7 +1,33 @@
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('nutanix.dashboard.nodes', {
     url: '/nodes',
-    redirectTo: 'nutanix.dashboard.nodes.all',
+    redirectTo: (transition) => {
+      const $translatePromise = transition.injector().getAsync('$translate');
+      const serviceInfoPromise = transition.injector().getAsync('serviceInfo');
+
+      return Promise.all([$translatePromise, serviceInfoPromise]).then(
+        ([$translate, serviceInfo]) => {
+          if (serviceInfo.isResiliated()) {
+            return {
+              state: 'error',
+              params: {
+                detail: {
+                  message: $translate.instant(
+                    'nutanix_dashboard_service_suspended',
+                  ),
+                  status: 'EXPIRED',
+                  code: 404,
+                },
+                to: {
+                  state: 'nutanix.dashboard.nodes.all',
+                },
+              },
+            };
+          }
+          return 'nutanix.dashboard.nodes.node.general-info';
+        },
+      );
+    },
     resolve: {
       breadcrumb: /* @ngInject */ ($translate) =>
         $translate.instant('nutanix_dashboard_nodes'),
