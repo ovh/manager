c8709d06af28f5e538c8dbafee8d409eba7f5452
Arthur Bullet <arthur.bullet@ovhcloud.com>
2025-01-13 12:01:29 +0100

    feat(pci.ai.dashboard): add tracking auto
    REF:DATATR-1297
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>

--- packages/manager/apps/pci-ai-dashboard/src/routes/routes.tsx
+++ packages/manager/apps/pci-ai-dashboard/src/routes/routes.tsx
@@ -43,36 +43,42 @@ export default [
             children: [
               {
                 path: '',
+                id: 'dashboard.home',
                 ...lazyRouteConfig(() =>
                   import('@/pages/dashboard/home/Home.page'),
                 ),
               },
               {
                 path: 'users',
+                id: 'dashboard.users',
                 ...lazyRouteConfig(() =>
                   import('@/pages/dashboard/users/Users.page'),
                 ),
               },
               {
                 path: 'tokens',
+                id: 'dashboard.tokens',
                 ...lazyRouteConfig(() =>
                   import('@/pages/dashboard/tokens/Tokens.page'),
                 ),
               },
               {
                 path: 'docker-registries',
+                id: 'dashboard.docker-registries',
                 ...lazyRouteConfig(() =>
                   import('@/pages/dashboard/docker/Docker.page'),
                 ),
               },
               {
                 path: 'git-registries',
+                id: 'dashboard.git-registries',
                 ...lazyRouteConfig(() =>
                   import('@/pages/dashboard/git/Git.page'),
                 ),
               },
               {
                 path: 'datastore',
+                id: 'dashboard.datastores',
                 ...lazyRouteConfig(() =>
                   import('@/pages/dashboard/datastore/Datastore.page'),
                 ),
