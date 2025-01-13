18f544af41da5f30f190ea18e7f38aaa4debb86a
Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>
2025-01-13 11:29:09 +0100

    feat(mrc): fix header test
    ref: MANAGER-16546
    
    Signed-off-by: Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>

--- packages/manager-react-components/src/components/content/headers/headers.stories.tsx
+++ packages/manager-react-components/src/components/content/headers/headers.stories.tsx
@@ -49,19 +49,19 @@ const changelogItems = [
     id: 1,
     href: 'https://github.com/orgs/ovh/projects/16/views/1?pane=info&sliceBy%5Bvalue%5D=Baremetal',
     target: '_blank',
-    label: 'Roadmap',
+    labelKey: 'roadmap',
   },
   {
     id: 2,
     href: 'https://github.com/orgs/ovh/projects/16/views/6?pane=info&sliceBy%5Bvalue%5D=Baremetal',
     target: '_blank',
-    label: 'Changelog',
+    labelKey: 'changelog',
   },
   {
     id: 3,
     href: 'https://github.com/ovh/infrastructure-roadmap/issues/new?assignees=&labels=&projects=&template=feature_request.md&title=',
     target: '_blank',
-    label: 'Feature Request',
+    labelKey: 'feature-request',
   },
 ];
 
