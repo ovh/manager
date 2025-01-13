a4da81d792e04f71ec5ecd94e2a875d4b7e73500
JacquesLarique <134954692+JacquesLarique@users.noreply.github.com>
2025-01-13 13:47:13 +0100

    feat(container): added an invitation modal to accept contract (#14041)
    ref: MANAGER-14722
    
    Co-authored-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>
    Co-authored-by: Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

---
+++ packages/manager/apps/container/src/components/AgreementsUpdateModal/AgreementsUpdateModal.spec.tsx
@@ -0,0 +1,100 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { render } from '@testing-library/react';
+import { vi } from 'vitest';
+import {
+  ShellContext,
+  ShellContextType,
+} from '@ovh-ux/manager-react-shell-client';
+import AgreementsUpdateModal from '@/components/AgreementsUpdateModal/AgreementsUpdateModal.component';
+import { ModalTypes } from '@/context/modals/modals.context';
+
+const mocks = vi.hoisted(() => ({
+  isAuthorized: false,
+  region: 'US',
+  agreements: [],
+}));
+
+const shellContext = {
+  shell: {
+    getPlugin: (plugin: string) => {
+      switch (plugin) {
+        case 'navigation': return {
+          getURL: vi.fn(
+            () =>
+              new Promise((resolve) => {
+                setTimeout(() => resolve('http://fakelink.com'), 50);
+              }),
+          ),
+        };
+        case 'ux': return {
+          notifyModalActionDone: vi.fn(),
+        };
+        case 'environment': return {
+          getEnvironment: () => ({
+            getRegion: vi.fn(() => mocks.region),
+            getApplicationURL: vi.fn((app) => `https://ovh.com/manager/${app}`)
+          })
+        };
+      }
+    },
+  }
+};
+
+const queryClient = new QueryClient();
+const renderComponent = () => {
+  return render(
+    <QueryClientProvider client={queryClient}>
+      <ShellContext.Provider
+        value={(shellContext as unknown) as ShellContextType}
+      >
+        <AgreementsUpdateModal />
+      </ShellContext.Provider>
+    </QueryClientProvider>,
+  );
+};
+
+vi.mock('react', async (importOriginal) => {
+  const module = await importOriginal<typeof import('react')>();
+  return {
+    ...module,
+    useContext: () => shellContext
+  }
+});
+
+vi.mock('@/hooks/accountUrn/useAccountUrn', () => ({
+  default: () => ({ data: 'urn' }),
+}));
+
+vi.mock('@ovh-ux/manager-react-components', () => ({
+  useAuthorizationIam: () => ({ isAuthorized: mocks.isAuthorized })
+}));
+
+vi.mock('@/context/modals', () => ({
+  useModals: () => ({ current: ModalTypes.agreements })
+}));
+
+vi.mock('@/hooks/agreements/usePendingAgreements', () => ({
+  default: () => ({ data: mocks.agreements })
+}));
+
+describe('AgreementsUpdateModal', () => {
+  it('should display nothing for US customers', () => {
+    const { queryByTestId } = renderComponent();
+    expect(queryByTestId('agreements-update-modal')).not.toBeInTheDocument();
+  });
+  it('should display nothing for non US and non authorized customers', () => {
+    mocks.region = 'EU';
+    const { queryByTestId } = renderComponent();
+    expect(queryByTestId('agreements-update-modal')).not.toBeInTheDocument();
+  });
+  it('should display nothing for non US and authorized customers without new contract', () => {
+    mocks.isAuthorized = true;
+    const { queryByTestId } = renderComponent();
+    expect(queryByTestId('agreements-update-modal')).not.toBeInTheDocument();
+  });
+  it('should display a modal for non US and authorized customers', () => {
+    mocks.agreements.push({ agreed: false, id: 9999, contractId: 9999 });
+    const { getByTestId } = renderComponent();
+    expect(getByTestId('agreements-update-modal')).not.toBeNull();
+  });
+})
