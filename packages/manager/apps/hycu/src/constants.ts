7b1efbedaf7712c4eb754159488636aede2d53ee
Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>
2025-01-14 14:42:07 +0100

    feat(*): add changelog component to product dashboards
    ref: MANAGER-16546
    
    Signed-off-by: Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>

--- packages/manager/apps/hycu/src/constants.ts
+++ packages/manager/apps/hycu/src/constants.ts
@@ -7,3 +7,13 @@ export const packTypeLabel = {
   'hycu-cloud-vm-pack-250': '250 VMs',
   'hycu-cloud-vm-pack-500': '500 VMs',
 } as const;
+
+// CHANGELOG
+export const CHANGELOG_PREFIXES = ['tile-changelog-roadmap', 'external-link'];
+export const CHANGELOG_DESTINATION = {
+  CHANGELOG: 'changelog',
+  ROADMAP: 'roadmap',
+  FEATURE_REQUEST: 'feature-request',
+};
+
+export const GO_TO = (link: string) => `go-to-${link}`;
