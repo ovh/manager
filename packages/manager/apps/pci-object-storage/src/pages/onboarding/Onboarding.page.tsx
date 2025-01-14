9045b8843f9a769db5f0ba730413cef6887d8ee5
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2025-01-14 11:20:28 +0100

    fix(pci-object-storage): onboarding page design issue
    ref: DTCORE-2950
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/apps/pci-object-storage/src/pages/onboarding/Onboarding.page.tsx
+++ packages/manager/apps/pci-object-storage/src/pages/onboarding/Onboarding.page.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from 'react-i18next';
 import { Outlet, useNavigate, useParams } from 'react-router-dom';
 import {
+  BaseLayout,
   Card,
   OnboardingLayout,
   RedirectionGuard,
@@ -72,81 +73,86 @@ export default function OnBoardingPage() {
       route={`/pci/projects/${projectId}/storages/objects`}
       condition={allStorages?.resources.length > 0}
     >
-      {project && (
-        <OdsBreadcrumb>
-          <OdsBreadcrumbItem href={urlProject} label={project?.description} />
-          <OdsBreadcrumbItem
-            href=""
-            label={t('pci_projects_project_storages_objects_onboarding_title')}
-          />
-        </OdsBreadcrumb>
-      )}
-      <OnboardingLayout
-        title={t('pci_projects_project_storages_objects_onboarding_title')}
-        description={
-          <>
-            {isS3User && (
-              <>
-                <OdsText className="mt-8 block">
-                  {t(
-                    'pci_projects_project_storages_objects_onboarding_content5',
-                  )}
-                </OdsText>
-                <OdsText className="mt-6 block">
-                  {t(
-                    'pci_projects_project_storages_objects_onboarding_content6',
-                  )}
-                </OdsText>
-                <OdsText className="mt-8 block">
-                  {t(
-                    'pci_projects_project_storages_objects_onboarding_content7',
-                  )}
-                </OdsText>
-              </>
-            )}
-            {!isS3User && (
-              <>
-                <OdsText className="mt-8 block">
-                  {t(
-                    'pci_projects_project_storages_objects_onboarding_content1',
-                  )}
-                </OdsText>
-                <OdsText className="mt-6 block">
-                  {t(
-                    'pci_projects_project_storages_objects_onboarding_content2',
-                  )}
-                </OdsText>
-                <OdsText className="mt-8 block">
-                  {t(
-                    'pci_projects_project_storages_objects_onboarding_content3',
-                  )}
-                </OdsText>
-                <OdsText className="mt-8 block">
-                  {t(
-                    'pci_projects_project_storages_objects_onboarding_content4',
-                  )}
-                </OdsText>
-              </>
-            )}
-          </>
+      <BaseLayout
+        breadcrumb={
+          <OdsBreadcrumb>
+            <OdsBreadcrumbItem href={urlProject} label={project?.description} />
+            <OdsBreadcrumbItem
+              href=""
+              label={t(
+                'pci_projects_project_storages_objects_onboarding_title',
+              )}
+            />
+          </OdsBreadcrumb>
         }
-        orderButtonLabel={t(
-          'pci_projects_project_storages_objects_onboarding_action_label',
-        )}
-        onOrderButtonClick={() => {
-          navigate('../new');
-          tracking?.trackClick({
-            name: `${TRACKING_PREFIX}onboarding::add`,
-          });
-        }}
       >
-        {tileItems.map((tile) => (
-          <Card key={tile.id} href={tile.href} texts={tile.texts} />
-        ))}
-      </OnboardingLayout>
-      <Suspense>
-        <Outlet />
-      </Suspense>
+        <OnboardingLayout
+          title={t('pci_projects_project_storages_objects_onboarding_title')}
+          description={
+            <div className="text-center">
+              {isS3User && (
+                <>
+                  <OdsText className="mt-8 block">
+                    {t(
+                      'pci_projects_project_storages_objects_onboarding_content5',
+                    )}
+                  </OdsText>
+                  <OdsText className="mt-6 block">
+                    {t(
+                      'pci_projects_project_storages_objects_onboarding_content6',
+                    )}
+                  </OdsText>
+                  <OdsText className="mt-8 block">
+                    {t(
+                      'pci_projects_project_storages_objects_onboarding_content7',
+                    )}
+                  </OdsText>
+                </>
+              )}
+              {!isS3User && (
+                <>
+                  <OdsText className="mt-8 block">
+                    {t(
+                      'pci_projects_project_storages_objects_onboarding_content1',
+                    )}
+                  </OdsText>
+                  <OdsText className="mt-6 block">
+                    {t(
+                      'pci_projects_project_storages_objects_onboarding_content2',
+                    )}
+                  </OdsText>
+                  <OdsText className="mt-8 block">
+                    {t(
+                      'pci_projects_project_storages_objects_onboarding_content3',
+                    )}
+                  </OdsText>
+                  <OdsText className="mt-8 block">
+                    {t(
+                      'pci_projects_project_storages_objects_onboarding_content4',
+                    )}
+                  </OdsText>
+                </>
+              )}
+            </div>
+          }
+          orderButtonLabel={t(
+            'pci_projects_project_storages_objects_onboarding_action_label',
+          )}
+          onOrderButtonClick={() => {
+            navigate('../new');
+            tracking?.trackClick({
+              name: `${TRACKING_PREFIX}onboarding::add`,
+            });
+          }}
+        >
+          {tileItems.map((tile) => (
+            <Card key={tile.id} href={tile.href} texts={tile.texts} />
+          ))}
+        </OnboardingLayout>
+        <Suspense>
+          <Outlet />
+        </Suspense>
+      </BaseLayout>
     </RedirectionGuard>
   );
 }
