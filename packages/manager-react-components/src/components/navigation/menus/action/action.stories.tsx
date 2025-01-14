913950a0b54f4ff612537bab35cd789817a2cec2
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-01-14 15:08:24 +0100

    fix(kms): use DeleteServiceModal and fix banners
    ref: MANAGER-16657
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

--- packages/manager-react-components/src/components/navigation/menus/action/action.stories.tsx
+++ packages/manager-react-components/src/components/navigation/menus/action/action.stories.tsx
@@ -41,6 +41,11 @@ const actionItems: ActionMenuItem[] = [
     urn: 'urn:v9:eu:resource:manager-react-components:vrz-a878-dsflkds-fdsfsd',
     iamActions: ['vrackServices:apiovh:iam/resource/tag/remove'],
   },
+  {
+    id: 4,
+    onClick: () => window.open('https://ovhcloud.com', '_blank', 'noopener'),
+    label: 'action',
+  },
 ];
 
 export const actionMenuStandard = {
