b5c629d1457011d03db2662804bfec59d527b503
Simon Chaumet <simon.chaumet.ext@ovhcloud.com>
2025-01-14 15:06:37 +0100

    feat(pci-common): add 3AZ region and 1AZ chip (#14672)
    ref: #TAPC-2624
    
    Signed-off-by: Simon Chaumet <simon.chaumet.ext@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/manager-pci-common/src/components/region-selector/Region3AZChip.component.spec.tsx
+++ packages/manager/modules/manager-pci-common/src/components/region-selector/Region3AZChip.component.spec.tsx
@@ -1,15 +1,16 @@
 import { render } from '@testing-library/react';
-import { RegionGlobalzoneChip } from './RegionGlobalzoneChip.component';
+import { describe, it } from 'vitest';
 import { wrapper } from '@/wrapperRenders';
+import { Region3AZChip } from './Region3AZChip.component';
 
-describe('RegionGlobalzoneChip', () => {
+describe('Region3AZChip', () => {
   it('renders tag with correct text', () => {
-    const { container } = render(<RegionGlobalzoneChip />, {
+    const { container } = render(<Region3AZChip />, {
       wrapper,
     });
     const tagElt = container.querySelector('ods-badge');
 
     expect(tagElt).toBeInTheDocument();
-    expect(tagElt).toHaveAttribute('label', 'pci_project_flavors_zone_1AZ');
+    expect(tagElt).toHaveAttribute('label', 'pci_project_flavors_zone_3AZ');
   });
 });
