eb8ac1464ca0f695ab3e371e66ea09c77aa14c8f
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2025-01-15 15:08:05 +0100

    feat(web.office): feature edit user
    ref: MANAGER-16125
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/apps/web-office/src/api/users/api.ts
+++ packages/manager/apps/web-office/src/api/users/api.ts
@@ -1,6 +1,7 @@
 import { fetchIcebergV6, v6 } from '@ovh-ux/manager-core-api';
 import { getApiPath } from '../utils/apiPath';
 import { UserNativeType } from './type';
+import { UserParamsType } from '../api.type';
 
 // GET
 
@@ -14,9 +15,31 @@ export const getOfficeUsers = async (
   return data;
 };
 
+export const getOfficeUserDetail = async (
+  serviceName: string,
+  activationEmail: string,
+) => {
+  const { data } = await v6.get(
+    `${getApiPath(serviceName)}user/${activationEmail}`,
+  );
+  return data;
+};
+
 // POST
 
 // PUT
+
+export const putOfficeUserDetail = async (
+  serviceName: string,
+  activationEmail: string,
+  params: UserParamsType,
+) => {
+  const { data } = await v6.put(
+    `${getApiPath(serviceName)}user/${activationEmail}`,
+    params,
+  );
+  return data;
+};
 
 // DELETE
 
