aba4f36635407b7ba34421a9ec7ac97d5fe8f924
Tristan WAGNER <t.wagner66250@gmail.com>
2025-01-15 15:37:11 +0100

    feat(mrc): add fetchall capabilities IcebergV6Hook
    ref: MANAGER-16335
    
    Signed-off-by: Tristan WAGNER <t.wagner66250@gmail.com>

--- packages/manager-react-components/src/hooks/datagrid/useIcebergV6.tsx
+++ packages/manager-react-components/src/hooks/datagrid/useIcebergV6.tsx
@@ -1,12 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { IcebergFetchParamsV6, fetchIcebergV6 } from '@ovh-ux/manager-core-api';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { ColumnSort } from '../../components';
+import { defaultPageSize } from './index';
 
 interface IcebergV6Hook {
   queryKey: string[];
   defaultSorting?: ColumnSort;
+  shouldFetchAll?: boolean;
 }
+
+export const API_V6_MAX_PAGE_SIZE = 4999;
 
 /**
  * @deprecated use fetchIcebergV6 from @ovh-ux/manager-core-api
@@ -15,21 +19,27 @@ export const getResourcesIcebergV6 = fetchIcebergV6;
 
 export function useResourcesIcebergV6<T = unknown>({
   route,
-  pageSize = 10,
+  pageSize = defaultPageSize,
   queryKey,
   defaultSorting = undefined,
+  shouldFetchAll = false,
 }: IcebergFetchParamsV6 & IcebergV6Hook) {
   const [sorting, setSorting] = useState<ColumnSort>(defaultSorting);
 
-  const { data: dataSelected, ...rest } = useInfiniteQuery({
+  const {
+    data: dataSelected,
+    hasNextPage,
+    fetchNextPage,
+    ...rest
+  } = useInfiniteQuery({
     initialPageParam: 1,
-    queryKey: [...queryKey, pageSize, sorting],
+    queryKey: [...queryKey, shouldFetchAll ? 'all' : pageSize, sorting],
     staleTime: Infinity,
     retry: false,
     queryFn: ({ pageParam: pageIndex }) =>
       fetchIcebergV6<T>({
         route,
-        pageSize,
+        pageSize: shouldFetchAll ? API_V6_MAX_PAGE_SIZE : pageSize,
         page: pageIndex,
         sortBy: sorting?.id || null,
         sortReverse: sorting?.desc,
@@ -53,8 +63,16 @@ export function useResourcesIcebergV6<T = unknown>({
     },
   });
 
+  useEffect(() => {
+    if (shouldFetchAll && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [dataSelected]);
+
   return {
     ...(dataSelected ?? { ...dataSelected, totalCount: 0 }),
+    hasNextPage,
+    fetchNextPage,
     ...rest,
     sorting,
     setSorting,
