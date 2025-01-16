4aae0ae62cfdef9c19fcb954d5770debc453c43b
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-01-16 14:44:05 +0100

    feat(manager-react-component): add manager tile component
    It's a duplicated of dashboard tile but with a Compound components pattern
    
    Resolve #15066
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

---
+++ packages/manager-react-components/src/components/content/ManagerTile/manager-tile.stories.tsx
@@ -0,0 +1,118 @@
+import React, { useId } from 'react';
+import { Meta } from '@storybook/react';
+import { OdsSkeleton, OdsText } from '@ovhcloud/ods-components/react';
+import { ManagerTile } from './manager-tile.component';
+import ActionMenu from '../../navigation/menus/action/action.component';
+import { Clipboard } from '../../clipboard/clipboard.component';
+
+const actionItems = [
+  {
+    id: 1,
+    href: 'https://ovhcloud.com',
+    label: 'Action 1',
+  },
+  {
+    id: 2,
+    onClick: () => window.open('https://ovhcloud.com', '_blank', 'noopener'),
+    label: 'Action 2',
+  },
+];
+
+export const CompleteExample = () => {
+  const id = useId();
+  return (
+    <ManagerTile>
+      <ManagerTile.Title>Complete example</ManagerTile.Title>
+      <ManagerTile.Divider />
+      <ManagerTile.Item>
+        <ManagerTile.Item.Label>Component Example</ManagerTile.Item.Label>
+        <ManagerTile.Item.Description>
+          <OdsText preset="span">Test Value</OdsText>
+        </ManagerTile.Item.Description>
+      </ManagerTile.Item>
+      <ManagerTile.Divider />
+      <ManagerTile.Item>
+        <ManagerTile.Item.Label>Long Text</ManagerTile.Item.Label>
+        <ManagerTile.Item.Description>
+          <OdsText preset="paragraph">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </OdsText>
+        </ManagerTile.Item.Description>
+      </ManagerTile.Item>
+      <ManagerTile.Divider />
+      <ManagerTile.Item>
+        <ManagerTile.Item.Label>Loading</ManagerTile.Item.Label>
+        <ManagerTile.Item.Description>
+          <OdsSkeleton />
+        </ManagerTile.Item.Description>
+      </ManagerTile.Item>
+      <ManagerTile.Divider />
+      <ManagerTile.Item>
+        <ManagerTile.Item.Label>Clipboard</ManagerTile.Item.Label>
+        <ManagerTile.Item.Description>
+          <Clipboard className="w-full" value="example value" />
+        </ManagerTile.Item.Description>
+      </ManagerTile.Item>
+      <ManagerTile.Divider />
+      <ManagerTile.Item>
+        <ManagerTile.Item.Label>Menu Example</ManagerTile.Item.Label>
+        <ManagerTile.Item.Description>
+          <div className="flex items-center justify-between">
+            <OdsText preset="span">Test Value</OdsText>
+            <ActionMenu
+              isCompact
+              items={actionItems}
+              id={'action-menu-story'}
+            />
+          </div>
+        </ManagerTile.Item.Description>
+      </ManagerTile.Item>
+    </ManagerTile>
+  );
+};
+
+export const SimpleExampleWithTitle = () => (
+  <ManagerTile>
+    <ManagerTile.Title>Sample example with title</ManagerTile.Title>
+    <ManagerTile.Divider />
+    <ManagerTile.Item>
+      <ManagerTile.Item.Label>Component Example</ManagerTile.Item.Label>
+      <ManagerTile.Item.Description>Test</ManagerTile.Item.Description>
+    </ManagerTile.Item>
+    <ManagerTile.Divider />
+    <ManagerTile.Item>
+      <ManagerTile.Item.Label>Loading</ManagerTile.Item.Label>
+      <ManagerTile.Item.Description>
+        <OdsSkeleton />
+      </ManagerTile.Item.Description>
+    </ManagerTile.Item>
+  </ManagerTile>
+);
+
+export const NoTitle = () => (
+  <ManagerTile>
+    <ManagerTile.Item>
+      <ManagerTile.Item.Label>Component Example</ManagerTile.Item.Label>
+      <ManagerTile.Item.Description>Test</ManagerTile.Item.Description>
+    </ManagerTile.Item>
+    <ManagerTile.Divider />
+    <ManagerTile.Item>
+      <ManagerTile.Item.Label>Loading</ManagerTile.Item.Label>
+      <ManagerTile.Item.Description>
+        <OdsSkeleton />
+      </ManagerTile.Item.Description>
+    </ManagerTile.Item>
+  </ManagerTile>
+);
+
+const meta: Meta = {
+  title: 'Content/Manager Tile',
+  component: ManagerTile,
+  argTypes: {},
+  args: {},
+};
+
+export default meta;
