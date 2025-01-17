107dcd592949b7da4ad43af845f3583c973fa2c5
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-01-17 16:41:27 +0100

    feat(sap-features-hub): add context errors and edit validation rules
    ref: MANAGER-16977
    desc: errors in context, edit sid regex, edit translations, add inputLength control
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/sap-features-hub/src/pages/installation/stepSystemInformation/InstallationStepSystemInformation.page.tsx
+++ packages/manager/apps/sap-features-hub/src/pages/installation/stepSystemInformation/InstallationStepSystemInformation.page.tsx
@@ -1,95 +1,78 @@
-import React, { useMemo, useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { OdsButton, OdsText } from '@ovhcloud/ods-components/react';
-import { OdsInputChangeEvent } from '@ovhcloud/ods-components';
 import { FormTitle } from '@/components/Form/FormTitle.component';
 import { useFormSteps } from '@/hooks/formStep/useFormSteps';
 import {
-  SYSTEM_INITIAL_ERRORS,
   FORM_SAP_SIDS_LABEL,
   SYSTEM_PASSWORD_INPUTS,
   SYSTEM_TEXT_INPUTS,
 } from './installationStepSystemInformation.constants';
 import { useInstallationFormContext } from '@/context/InstallationForm.context';
 import { TextField } from '@/components/Form/TextField.component';
+import { getSystemFormData } from '@/utils/formStep';
+import { isValidSapPassword } from '@/utils/formValidation';
 
 export default function InstallationStepSystemInformation() {
   const { t } = useTranslation('installation');
   const { previousStep, nextStep } = useFormSteps();
-  const [errors, setErrors] = useState(SYSTEM_INITIAL_ERRORS);
   const {
-    values: {
-      sapSid,
-      sapHanaSid,
-      masterSapPassword,
-      masterSapHanaPassword,
-      sidamnPassword,
-      systemPassword,
-    },
+    values: formValues,
+    errors: formErrors,
     setValues,
+    setErrors,
   } = useInstallationFormContext();
 
-  const values = {
-    sapSid,
-    sapHanaSid,
-    masterSapPassword,
-    masterSapHanaPassword,
-    sidamnPassword,
-    systemPassword,
-  };
+  const values = getSystemFormData(formValues);
+  const errors = getSystemFormData(formErrors);
 
-  const isFormValid = useMemo(
+  const isFormValid = React.useMemo(
     () =>
       Object.values(values).every((value) => !!value) &&
-      Object.values(errors).every((error) => !error),
+      !Object.values(errors).some((error) => !!error),
     [values, errors],
   );
 
-  const handleChange = (e: OdsInputChangeEvent) => {
-    const { name, value } = e.detail;
-    setValues((prev) => ({ ...prev, [name]: value }));
-    setErrors((prev) => ({ ...prev, [name]: !e.detail.validity?.valid }));
-  };
+  const sidRule = t('system_sid_validation_message');
+  const pwdRule = t('system_password_validation_message');
 
   return (
     <div>
       <FormTitle title={t('system_title')} subtitle={t('system_subtitle')} />
       <form className="flex flex-col gap-y-6">
         <OdsText preset="heading-3">{FORM_SAP_SIDS_LABEL}</OdsText>
-        {SYSTEM_TEXT_INPUTS.map((input) => (
+        <OdsText>{sidRule}</OdsText>
+        {SYSTEM_TEXT_INPUTS.map(({ name, helperKey, ...inputProps }) => (
           <TextField
-            key={input.name}
-            name={input.name}
-            value={values[input.name]}
-            onChange={handleChange}
-            label={input.label}
-            placeholder={input.placeholder}
-            pattern={input.pattern}
-            error={
-              !!values[input.name] && errors[input.name]
-                ? t('system_sid_validation_message')
-                : undefined
-            }
-            helperText={t(input.helperKey)}
+            key={name}
+            name={name}
+            onOdsChange={(e) => {
+              const isValid = e.detail.validity?.valid;
+              setValues((val) => ({ ...val, [name]: e.detail.value }));
+              setErrors((err) => ({ ...err, [name]: isValid ? '' : sidRule }));
+            }}
+            value={values[name]}
+            error={values[name] && errors[name]}
+            helperText={t(helperKey)}
+            {...inputProps}
           />
         ))}
         <OdsText preset="heading-3">{t('passwords')}</OdsText>
-        {SYSTEM_PASSWORD_INPUTS.map((input) => (
+        <OdsText>{pwdRule}</OdsText>
+        {SYSTEM_PASSWORD_INPUTS.map(({ name, helperKey, ...inputProps }) => (
           <TextField
-            key={input.name}
+            key={name}
             type="password"
-            name={input.name}
-            value={values[input.name]}
-            onChange={handleChange}
-            label={input.label}
-            placeholder={input.placeholder}
-            pattern={input.pattern}
-            error={
-              !!values[input.name] && errors[input.name]
-                ? t('system_password_validation_message')
-                : undefined
-            }
-            helperText={t(input.helperKey)}
+            name={name}
+            onOdsChange={(e) => {
+              const isValid = isValidSapPassword(e.detail.value as string);
+              setValues((val) => ({ ...val, [name]: e.detail.value }));
+              setErrors((err) => ({ ...err, [name]: isValid ? '' : pwdRule }));
+            }}
+            value={values[name]}
+            error={values[name] && errors[name]}
+            helperText={t(helperKey)}
+            {...inputProps}
           />
         ))}
         <div className="flex gap-x-2">
