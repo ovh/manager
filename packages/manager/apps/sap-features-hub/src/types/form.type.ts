ca393850821755a76015a0c2e9acf72562201cb1
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-01-17 16:41:27 +0100

    feat(sap-features-hub): add context errors and edit validation rules
    ref: MANAGER-16977
    desc: errors in context, edit sid regex, edit translations, add inputLength control
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/sap-features-hub/src/types/form.type.ts
+++ packages/manager/apps/sap-features-hub/src/types/form.type.ts
@@ -1,10 +1,32 @@
-export type InstallationForm = {
+import { TextFieldProps } from '@/components/Form/TextField.component';
+
+type InstallationForm = InitializationForm & DeploymentForm & SystemForm;
+export type FormKey = keyof InstallationForm;
+
+export type InstallationFormErrors = Record<FormKey, string>;
+export type InstallationFormValues = {
+  [K in FormKey]: InstallationForm[K];
+};
+
+export type TextInputData<T = string> = {
+  name: T;
+  helperKey?: string;
+} & Pick<
+  TextFieldProps,
+  'label' | 'placeholder' | 'pattern' | 'minlength' | 'maxlength' | 'isRequired'
+>;
+
+export type InitializationForm = {
   serviceName: string;
   datacenterId: number;
   clusterName: string;
+};
+export type DeploymentForm = {
   applicationVersion: string;
   applicationType: string;
   deploymentType: string;
+};
+export type SystemForm = {
   sapSid: string;
   sapHanaSid: string;
   masterSapPassword: string;
@@ -12,23 +34,4 @@ export type InstallationForm = {
   sidamnPassword: string;
   systemPassword: string;
 };
-
-export type FormKey = keyof InstallationForm;
-
-export type TextInputData<T = string> = {
-  name: T;
-  label: string;
-  placeholder?: string;
-  pattern?: string;
-  helperKey?: string;
-};
-
-export type SystemFormKeys = keyof Pick<
-  InstallationForm,
-  | 'sapSid'
-  | 'sapHanaSid'
-  | 'masterSapPassword'
-  | 'masterSapHanaPassword'
-  | 'sidamnPassword'
-  | 'systemPassword'
->;
+export type SystemFormKeys = keyof SystemForm;
