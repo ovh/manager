a9a3949936beacb30fc1db94feaf2480e35d87e0
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-01-17 15:42:56 +0100

    feat(manager-react-components): delete console log
    ref: MANAGER-15088
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-react-components/src/components/datagrid/datagrid.component.tsx
+++ packages/manager-react-components/src/components/datagrid/datagrid.component.tsx
@@ -169,13 +169,12 @@ export const Datagrid = <T,>({
   const [columnsFilters, setColumnsFilters] = useState([]);
   useEffect(() => {
     const clmFilters = columns
-      .filter((item) => 'comparator' in item || 'type' in item)
-      // .filter(
-      //   (item) =>
-      //     ('comparator' in item || 'type' in item) &&
-      //     'isFilterable' in item &&
-      //     item['isFilterable'],
-      // )
+      .filter(
+        (item) =>
+          ('comparator' in item || 'type' in item) &&
+          'isFilterable' in item &&
+          item['isFilterable'],
+      )
       .map((column) => ({
         id: column.id,
         label: column.label,
@@ -184,8 +183,6 @@ export const Datagrid = <T,>({
       }));
     setColumnsFilters(clmFilters);
   }, [columns]);
-
-  console.info('columnsFilters : ', columnsFilters);
 
   return (
     <div>
