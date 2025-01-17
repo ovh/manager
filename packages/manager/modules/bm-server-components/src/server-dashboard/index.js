f674d47178e3d89d006925d262aecc710dcfac2e
qpavy <contact@quentinpavy.com>
2025-01-17 12:45:58 +0100

    feat(dedicated.server): rbx1 eol banner (#14742)
    * feat(dedicated.server): rbx1 eol banner
    
    ref: MANAGER-16530
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
    
    * fix(i18n): add missing translations [CDS 3616]
    
    Signed-off-by: CDS Translator Agent <opensource@ovh.net>
    
    * feat(dedicated.server): display rb1x banner for rbx1 not in ct rack
    
    ref: MANAGER-16530
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
    
    ---------
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
    Signed-off-by: CDS Translator Agent <opensource@ovh.net>
    Co-authored-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

--- packages/manager/modules/bm-server-components/src/server-dashboard/index.js
+++ packages/manager/modules/bm-server-components/src/server-dashboard/index.js
@@ -4,6 +4,7 @@ import '@ovh-ux/ui-kit';
 import 'angular-translate';
 
 import vmacUnavailableBanner from '../vmac-unavailable-banner';
+import rbx1EolBanner from '../rbx1-eol-banner';
 import service from '../server/server.service';
 import bandwidthVrackOrderService from '../server/server.bandwidth-vrack-order.service';
 import featureAvailability from '../server/server.feature-availability';
@@ -12,7 +13,12 @@ import component from './dashboard.component';
 const moduleName = 'ovhManagerBmServerComponentsServerDashboard';
 
 angular
-  .module(moduleName, ['oui', 'pascalprecht.translate', vmacUnavailableBanner])
+  .module(moduleName, [
+    'oui',
+    'pascalprecht.translate',
+    vmacUnavailableBanner,
+    rbx1EolBanner,
+  ])
   .component('serverDashboard', component)
   .service('Server', service)
   .service('BandwidthVrackOrderService', bandwidthVrackOrderService)
