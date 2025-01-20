32bdc1bd8505ee6963a6fb03c6f0a35eee3b0531
Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>
2025-01-20 11:00:47 +0100

    feat(mrc): add forgotten file change
    ref: MANAGER-16546
    
    Signed-off-by: Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>

--- packages/manager-react-components/src/components/navigation/menus/changelog/changelog.stories.tsx
+++ packages/manager-react-components/src/components/navigation/menus/changelog/changelog.stories.tsx
@@ -1,30 +1,24 @@
 import React from 'react';
 import { Meta } from '@storybook/react';
-import { ChangelogButton, ChangelogButtonProps } from './changelog.component';
+import {
+  ChangelogButton,
+  ChangelogLinks,
+  ChangelogButtonProps,
+} from './changelog.component';
 
-const changelogItems = [
-  {
-    id: 1,
-    href: 'https://ovh.com',
-    target: '_blank',
-    labelKey: 'roadmap',
-  },
-  {
-    id: 2,
-    href: 'https://ovh.com',
-    target: '_blank',
-    labelKey: 'changelog',
-  },
-  {
-    id: 3,
-    href: 'https://ovh.com',
-    target: '_blank',
-    labelKey: 'feature-request',
-  },
-];
+const changelogChapters: string[] = ['baremetal', 'server', 'dedicated'];
+const changelogLinks: ChangelogLinks = {
+  roadmap:
+    'https://github.com/orgs/ovh/projects/16/views/1?pane=info&sliceBy%5Bvalue%5D=Baremetal',
+  changelog:
+    'https://github.com/orgs/ovh/projects/16/views/1?pane=info&sliceBy%5Bvalue%5D=Baremetal',
+  'feature-request':
+    'https://github.com/orgs/ovh/projects/16/views/1?pane=info&sliceBy%5Bvalue%5D=Baremetal',
+};
 
 export const changelogButton: ChangelogButtonProps = {
-  items: changelogItems,
+  links: changelogLinks,
+  chapters: changelogChapters,
 };
 
 const meta: Meta<ChangelogButtonProps> = {
