e71b43423550e0f7ecdfdb1400b3b784033736bc
ovh-ux-cds <ovh-ux-cds@users.noreply.github.com>
2025-01-20 16:06:47 +0000

    docs: update documentation [skip ci]

---
+++ storybook-static/assets/1.x.x-to-2.x.0-DIva9z3K.js
@@ -0,0 +1,60 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{M as o}from"./index-C7Gl6iyv.js";import"./index-C-Nbf5H5.js";import"./lib-ROnj4EVN.js";import"./useResourcesV6-CetFYNWH.js";import"./pci-guides-header.component-OYSQnBvk.js";import"./clipboard.component-Cq_PamW8.js";import"./Step.component-BwCGenD4.js";import"./Tabs.component-6XdXXCXW.js";import"./TilesInput.component-CdUZxOZS.js";import"./headers.component-Dq2PdNTE.js";import"./price.component-DkwHDtZT.js";import"./dashboard-tile.component-D9YrirQd.js";import"./card.component-BxiNf216.js";import"./action.component-C6Exu_6b.js";import"./guide.component-B7isF3gJ.js";import"./base.component-DY_vlekr.js";import"./error.component-BXkXZ1Lv.js";import"./onboarding.component-DT3FtwpL.js";import"./delete-modal.component-DdRFEHUo.js";import"./update-name-modal.component-C4Vs5Ht1.js";import"./title.component-Bx3BQPQH.js";import"./links.component-P6h2uIIa.js";import"./datagrid.component-CW6kKrFD.js";import"./index-CBqU2yxZ.js";import"./datagrid.contants-DE5a_Gky.js";import"./notifications.component-DMJsY8eS.js";import"./ods-notification-DYnlBovS.js";import"./filter-list.component-D3PKPMHI.js";import"./ManagerButton-Bz45hm9B.js";import"./ManagerText-DAScQPQm.js";import"./PciMaintenanceBanner.component-DS9YKxgS.js";import"./region.component-kKtC3z04.js";import{useMDXComponents as t}from"./index-DI5IigMn.js";import"./iframe-C2PEypnp.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BtM5VmRH.js";import"./index-D_r38UMq.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";import"./QueryClientProvider-CDvRjfR1.js";import"./useOvhIam-nH8cMKQ3.js";import"./index-v66SXByX.js";import"./index-4N_owrwP.js";import"./mutation-BoanVz2M.js";import"./useTranslation-CvcVFFFk.js";import"./i18next-ihUiNgJT.js";import"./clsx-B-dksMZM.js";import"./v4-CQkTLCs1.js";import"./translation-CBNwEvwd.js";import"./click-utils-ByCElPrV.js";function r(i){const n=Object.assign({h1:"h1",p:"p",h2:"h2",h3:"h3",ul:"ul",li:"li",h4:"h4",pre:"pre",code:"code",strong:"strong",em:"em"},t(),i.components);return e.jsxs(e.Fragment,{children:[e.jsx(o,{title:"Manager React Components/What's new/Migration guides/1.xx.x to 2.x.0"}),`
+`,e.jsx(n.h1,{id:"migration-from-1xx-to-2x0",children:"Migration from 1.x.x to 2.x.0"}),`
+`,e.jsx(n.p,{children:`If you have any questions or issues regarding the new version, feel free to
+reach us directly.`}),`
+`,e.jsx(n.h2,{id:"libraries",children:"Libraries"}),`
+`,e.jsx(n.h3,{id:"before-migration",children:"Before migration"}),`
+`,e.jsx(n.p,{children:"Be sure, to allow time to :"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"To Bump the new react components versin in your uapp and correct the components updated that have changed"}),`
+`,e.jsx(n.li,{children:"To update your existing unit test if needed"}),`
+`,e.jsx(n.li,{children:"To Bump the version of ods inside your app v17 -> v18"}),`
+`]}),`
+`,e.jsx(n.h3,{id:"installation",children:"Installation"}),`
+`,e.jsx(n.h4,{id:"update-your-packagejson",children:"update your package.json"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-js",children:`@ovh-ux/manager-react-components: "^2.x.0"
+`})}),`
+`,e.jsx(n.h4,{id:"import-css-files",children:"import css files"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-js",children:`import '@ovh-ux/manager-react-components/dist/style.css';
+`})}),`
+`,e.jsx(n.h2,{id:"components",children:"Components"}),`
+`,e.jsx(n.p,{children:"The following components have been removed:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"CommonTitle"})," you can replace by the component Title"]}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"Description"})}),`
+`]}),`
+`,e.jsx(n.p,{children:"The following components have breaking change:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"Links"})," 'target' attribute is now 'string'"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"UpdateNameModal"})," new attribute 'isOpen' at 'false' by default"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"DeleteModal"})," new attribute 'isOpen' at 'false' by default"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"Pci Maintenance Banner"})," 'type' attribue replace by 'color' type ODS_MESSSAGE_COLOR"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"Notifications"})," => (deprecated)"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"Guide component"})," 'target', 'rel', 'label' updated to type 'string'"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"Actions Component"})," 'target', 'rel', 'label' updated to type 'string', 'disabled' to 'isDisabled', 'color' attribute remove"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"OdsClipboard"})," new attributes 'labelCopySuccess', 'labelCopy'"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"ActionBanner component"})," attribute 'type' replace by 'color' ODS_MESSAGE_COLOR"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.code,{children:"Manager Button"})," => new attributes 'id' string and 'label' string"]}),`
+`]}),`
+`,e.jsx(n.p,{children:"The following components have been updated:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"Title"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"SubTitle"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"Onboarding"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"Error template"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"Dashboard template"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"Base template"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"Table"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"Redirection Guard"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"Card Component"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"SimpleTileSInput"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"Guide Header"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"Filters"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"Datagrid"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"Price"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"Headers"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"Dashboard Tile"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"Tabs"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"Steps"})}),`
+`]}),`
+`,e.jsx(n.p,{children:e.jsx(n.strong,{children:e.jsx(n.em,{children:"As each component was refactored, there are too many changes to list everything here, but its mainly refactoring some ods-text and ods-button"})})})]})}function te(i={}){const{wrapper:n}=Object.assign({},t(),i.components);return n?e.jsx(n,Object.assign({},i,{children:e.jsx(r,i)})):r(i)}export{te as default};
