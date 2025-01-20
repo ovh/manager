1d57e852da53bbf57afed89063ce07dffcab0ff8
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2025-01-20 11:50:36 +0100

    feat: add stories for breadcrumb
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/apps/pci-databases-analytics/src/pages/testOds/_components/breadcrumb/03-Tailwind.story.tsx
+++ packages/manager/apps/pci-databases-analytics/src/pages/testOds/_components/breadcrumb/03-Tailwind.story.tsx
@@ -1,36 +1,43 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { Home } from 'lucide-react';
 import {
   Breadcrumb,
   BreadcrumbList,
   BreadcrumbItem,
   BreadcrumbLink,
   BreadcrumbSeparator,
+  DropdownMenu,
+  DropdownMenuTrigger,
+  BreadcrumbEllipsis,
+  DropdownMenuContent,
+  DropdownMenuItem,
   BreadcrumbPage,
 } from '@datatr-ux/uxlib';
 import {
   OdsBreadcrumb,
   OdsBreadcrumbItem,
 } from '@ovhcloud/ods-components/react';
+import { Home } from 'lucide-react';
 import { StoryResult } from '../Stories';
 
 export default {
-  story: 'Simple breadcrumb',
+  story: 'Tailwind',
   customComponentExemple: (
     <Breadcrumb>
       <BreadcrumbList>
         <BreadcrumbItem>
-          <BreadcrumbLink href="/">
+          <BreadcrumbLink href="/" className="text-green-500">
             <Home className="size-4" />
           </BreadcrumbLink>
         </BreadcrumbItem>
         <BreadcrumbSeparator>|</BreadcrumbSeparator>
         <BreadcrumbItem>
-          <BreadcrumbLink href="/docs/components">Parent</BreadcrumbLink>
+          <BreadcrumbLink href="/docs/components" className="text-blue-500">
+            Parent
+          </BreadcrumbLink>
         </BreadcrumbItem>
         <BreadcrumbSeparator>|</BreadcrumbSeparator>
         <BreadcrumbItem>
-          <BreadcrumbPage>Current</BreadcrumbPage>
+          <BreadcrumbPage className="text-red-500">Current</BreadcrumbPage>
         </BreadcrumbItem>
       </BreadcrumbList>
     </Breadcrumb>
@@ -38,10 +45,22 @@ export default {
   customComponentResult: StoryResult.success,
   ODSComponentExemple: (
     <OdsBreadcrumb>
-      <OdsBreadcrumbItem href="#" icon="home"></OdsBreadcrumbItem>
-      <OdsBreadcrumbItem href="#" label="Parent"></OdsBreadcrumbItem>
-      <OdsBreadcrumbItem href="#" label="Current"></OdsBreadcrumbItem>
+      <OdsBreadcrumbItem
+        href="#"
+        icon="home"
+        className="text-green-500"
+      ></OdsBreadcrumbItem>
+      <OdsBreadcrumbItem
+        href="#"
+        label="Parent"
+        className="text-blue-500"
+      ></OdsBreadcrumbItem>
+      <OdsBreadcrumbItem
+        href="#"
+        label="Current"
+        className="text-red-500"
+      ></OdsBreadcrumbItem>
     </OdsBreadcrumb>
   ),
-  ODSComponentResult: StoryResult.warning,
+  ODSComponentResult: StoryResult.fail,
 };
