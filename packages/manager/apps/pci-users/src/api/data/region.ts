f591d874086c7ccff01d284f08171cd78ea4a7b9
Florian Renaut <florian.renaut@corp.ovh.com>
2025-01-21 09:25:48 +0100

    fix(pci-users): revert rclone changes
    ref: DTCORE-2868
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/apps/pci-users/src/api/data/region.ts
+++ packages/manager/apps/pci-users/src/api/data/region.ts
@@ -1,7 +1,51 @@
-import { TRegion } from '@ovh-ux/manager-pci-common';
+import { fetchIcebergV6 } from '@ovh-ux/manager-core-api';
+import {
+  REGION_CAPACITY,
+  S3_REGION_CAPACITY,
+} from '@/download-rclone.constants';
 import { ALPHA_CHARACTERS_REGEX } from '@/constants';
 
-export const getOpenRcApiVersion = (regions: TRegion[], region: string) => {
+export type Region = {
+  name: string;
+  type: string;
+  status: string;
+  ipCountries: string[];
+  continentCode: string;
+  datacenterLocation: string;
+  services: RegionService[];
+};
+
+export type RegionService = {
+  name: string;
+  status: string;
+  endpoint: string;
+};
+
+export const getAllRegions = async (projectId: string): Promise<Region[]> => {
+  const { data } = await fetchIcebergV6<Region>({
+    route: `/cloud/project/${projectId}/region`,
+  });
+
+  return data;
+};
+
+export const getS3StorageRegions = async (
+  regions: Region[],
+): Promise<Region[]> => {
+  return regions.filter(({ services }) => {
+    return services.find(({ name }) => S3_REGION_CAPACITY.includes(name));
+  });
+};
+
+export const getStorageRegions = async (
+  regions: Region[],
+): Promise<Region[]> => {
+  return regions.filter(({ services }) => {
+    return services.find(({ name }) => name === REGION_CAPACITY);
+  });
+};
+
+export const getOpenRcApiVersion = (regions: Region[], region: string) => {
   const hasGlobalRegions = regions.some((r) =>
     ALPHA_CHARACTERS_REGEX.test(r.name),
   );
