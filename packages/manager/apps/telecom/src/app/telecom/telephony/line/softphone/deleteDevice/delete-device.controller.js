8c32d64121b7b2e87e4ac3691a0b03769b7991e4
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2025-01-21 17:44:55 +0100

    fix(telecom.softphone): change remove device modal
    ref: MANAGER-16204
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/apps/telecom/src/app/telecom/telephony/line/softphone/deleteDevice/delete-device.controller.js
+++ packages/manager/apps/telecom/src/app/telecom/telephony/line/softphone/deleteDevice/delete-device.controller.js
@@ -8,33 +8,27 @@ export default class DeleteModalController {
 
   deleteDevice() {
     this.deleting = true;
-    const promise = this.deleteAllDevices
-      ? this.softphoneService.deleteAllDevices(
-          this.billingAccount,
-          this.serviceName,
-        )
-      : this.softphoneService.deleteDevice(
-          this.billingAccount,
-          this.serviceName,
-          this.deviceId,
-        );
-
-    return promise
+    this.softphoneService
+      .deleteDevice(this.billingAccount, this.serviceName, this.deviceId)
       .then(() => {
         this.goBack(true).then(() =>
           this.TucToast.success(
             this.$translate.instant(
-              'telephony_line_softphone_delete_device_success',
+              `telephony_line_softphone_delete_${
+                this.deviceId ? 'device' : 'all_devices'
+              }_success`,
             ),
           ),
         );
       })
-      .catch(({ status }) => {
+      .catch(({ data }) => {
         this.goBack(true).then(() =>
           this.TucToast.error(
             this.$translate.instant(
-              'telephony_line_softphone_delete_device_error',
-              { status },
+              `telephony_line_softphone_delete_${
+                this.deviceId ? 'device' : 'all_devices'
+              }_error`,
+              { error: data?.message },
             ),
           ),
         );
