6a74f5a66a2c6aaf0837506893e37a392cd9336f
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-01-23 00:40:56 +0100

    feat(manager-react-component): add manager-link component
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

---
+++ packages/manager-react-components/src/components/ManagerLink/translations/index.ts
@@ -0,0 +1,13 @@
+import { NAMESPACES } from '@ovh-ux/manager-common-translations';
+import i18next from 'i18next';
+import service_fr_FR from '@ovh-ux/manager-common-translations/dist/@ovh-ux/manager-common-translations/service/Messages_fr_FR.json';
+
+function addTranslations() {
+  i18next.addResources('fr_FR', NAMESPACES.IAM, service_fr_FR);
+}
+
+if (i18next.isInitialized) {
+  addTranslations();
+} else {
+  i18next.on('initialized', addTranslations);
+}
