5c6e5bc700eb2dc6478e7e66f3bab901025b92aa
Mohammed Hamdoune <sidlynx@gmail.com>
2025-01-23 14:05:17 +0100

    fix(pci-quota): add spinners to regions page
    ref: DTCORE-2986
    
    Signed-off-by: Mohammed Hamdoune <sidlynx@gmail.com>

--- packages/manager/apps/pci-quota/src/pages/regions/Available.part.tsx
+++ packages/manager/apps/pci-quota/src/pages/regions/Available.part.tsx
@@ -1,4 +1,4 @@
-import { OdsIcon, OdsText } from '@ovhcloud/ods-components/react';
+import { OdsIcon, OdsSpinner, OdsText } from '@ovhcloud/ods-components/react';
 import { ShapesInputComponent } from '@ovh-ux/manager-pci-common';
 import { useParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
@@ -14,7 +14,7 @@ export const AvailablePart = ({
 
   const { t } = useTranslation('regions');
 
-  const { data: locations } = useLocations(projectId);
+  const { data: locations, isPending } = useLocations(projectId);
 
   return (
     <div>
@@ -23,55 +23,59 @@ export const AvailablePart = ({
           {t('pci_projects_project_regions_added_title')}
         </OdsText>
       </div>
-      <ShapesInputComponent<TPlainLocation>
-        className="mt-6"
-        items={locations}
-        group={{
-          by: (item) => item.continent || 'M',
-          LabelComponent: ({ groupName, isGroupSelected }) => (
-            <OdsText
-              preset="heading-6"
-              className={`p-5 shape-input-group-label ${
-                isGroupSelected ? 'active' : ''
-              }`}
-            >
-              {groupName || 'All locations'}
-            </OdsText>
-          ),
-        }}
-        item={{
-          LabelComponent: ({ item }) => (
-            <div className="p-5 h-full">
-              <div className="flex">
-                <div className="w-full">
-                  <OdsText>{item.name}</OdsText>
+      {isPending ? (
+        <OdsSpinner />
+      ) : (
+        <ShapesInputComponent<TPlainLocation>
+          className="mt-6"
+          items={locations}
+          group={{
+            by: (item) => item.continent || 'M',
+            LabelComponent: ({ groupName, isGroupSelected }) => (
+              <OdsText
+                preset="heading-6"
+                className={`p-5 shape-input-group-label ${
+                  isGroupSelected ? 'active' : ''
+                }`}
+              >
+                {groupName || 'All locations'}
+              </OdsText>
+            ),
+          }}
+          item={{
+            LabelComponent: ({ item }) => (
+              <div className="p-5 h-full">
+                <div className="flex">
+                  <div className="w-full">
+                    <OdsText>{item.name}</OdsText>
+                  </div>
+                  <div>
+                    <OdsIcon
+                      name="circle-check"
+                      className="text-4x text-4xl text-[green]"
+                    ></OdsIcon>
+                  </div>
                 </div>
-                <div>
-                  <OdsIcon
-                    name="circle-check"
-                    className="text-4x text-4xl text-[green]"
-                  ></OdsIcon>
-                </div>
+                <ul className="p-0 m-0 list-none">
+                  {item.regions.map((region) => (
+                    <li
+                      key={region}
+                      className="border border-solid border-[--ods-color-primary-200] border-b-0 border-l-0 border-r-0 mt-4 pt-4"
+                    >
+                      <OdsText preset="caption">
+                        {item.name} ({region})
+                      </OdsText>
+                    </li>
+                  ))}
+                </ul>
               </div>
-              <ul className="p-0 m-0 list-none">
-                {item.regions.map((region) => (
-                  <li
-                    key={region}
-                    className="border border-solid border-[--ods-color-primary-200] border-b-0 border-l-0 border-r-0 mt-4 pt-4"
-                  >
-                    <OdsText preset="caption">
-                      {item.name} ({region})
-                    </OdsText>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ),
-          getId: (item) => item.name,
-          isDisabled: () => true,
-        }}
-        isMobile={isMobile}
-      />
+            ),
+            getId: (item) => item.name,
+            isDisabled: () => true,
+          }}
+          isMobile={isMobile}
+        />
+      )}
     </div>
   );
 };
