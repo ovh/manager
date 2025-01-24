6cae39e94bc1c0ea0f4a58347ab82e4122be6720
Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
2025-01-24 15:15:15 +0100

    feat(container): add tests to livechat component
    ref: MANAGER-16561
    
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>

--- packages/manager/apps/container/src/container/livechat/LiveChat.component.tsx
+++ packages/manager/apps/container/src/container/livechat/LiveChat.component.tsx
@@ -168,156 +168,162 @@ export default function LiveChat({
 
   // const url = `https://chat.ovh.com/system/templates/liveChat-manager/${customerLevel}/${subsidiary}_${language}/docs/index2.html`;
 
+  if (!chatbotOpen) return null;
   return (
-    <>
-      {chatbotOpen && (
-        <div className="absolute w-full h-full xl:h-fit xl:w-auto bottom-2 right-2 z-[960] flex flex-col justify-end pointer-events-none">
-          {chatType === 'Adrielly' && (
-            <ChatDialog
-              title="OVHcloud Chat"
-              visible={!chatbotReduced}
-              url={`https://chat.${'ovh'.toLocaleLowerCase()}.com/system/templates/pre-prod/prepa_prod/STD/FR_fr/docs/index2.html`}
-              key={chatType}
-            />
-          )}
-          {chatType === 'SNOW' &&
-            snowContext.session_id &&
-            snowContext.queue && (
-              <ChatDialog
-                title={chatType}
-                showHeader={false}
-                visible={!chatbotReduced}
-                url={generateSnowChatUrl(SNOW_INSTANCE_URL, snowContext)}
-                key={chatType}
-              />
-            )}
-          <div className="order-first xl:order-last relative p-3 bg-[#000e9c] xl:bg-transparent flex flex-row items-center justify-between xl:justify-end pointer-events-auto">
-            {!chatbotReduced && (
-              <>
-                <OsdsButton
-                  className="hidden xl:flex"
-                  aria-label="Reduce Live Chat"
-                  circle
-                  color={ODS_THEME_COLOR_INTENT.primary}
-                  onClick={() => handleReduceChat(true)}
-                >
-                  <OsdsIcon
-                    name={ODS_ICON_NAME.SPEECH_BUBBLE_CONCEPT}
-                    size={ODS_ICON_SIZE.md}
-                    className="m-2"
-                    contrasted
-                  />
-                  <span className="sr-only">{`Reduce Live Chat`}</span>
-                </OsdsButton>
-                <OsdsButton
-                  className="xl:hidden"
-                  aria-label="Reduce Live Chat"
-                  variant={ODS_BUTTON_VARIANT.ghost}
-                  circle
-                  contrasted
-                  color={ODS_THEME_COLOR_INTENT.primary}
-                  onClick={() => handleReduceChat(true)}
-                >
-                  <OsdsIcon
-                    name={ODS_ICON_NAME.CLOSE}
-                    size={ODS_ICON_SIZE.md}
-                    contrasted
-                  />
-                  <span className="sr-only">{`Reduce Live Chat`}</span>
-                </OsdsButton>
-                {chatType === 'Adrielly' && (
-                  <OsdsText
-                    id="livechat-label"
-                    color={ODS_THEME_COLOR_INTENT.default}
-                    hue={ODS_THEME_COLOR_HUE._000}
-                    level={ODS_TEXT_LEVEL.heading}
-                    size={ODS_TEXT_SIZE._600}
-                    className="grow text-center xl:hidden"
-                  >
-                    OVHcloud Chat
-                  </OsdsText>
-                )}
-              </>
-            )}
-            {chatbotReduced && (
-              <>
-                {/* Mobile view: Close Chat */}
-                <OsdsButton
-                  aria-label="Close Live Chat"
-                  variant={ODS_BUTTON_VARIANT.ghost}
-                  circle
-                  contrasted
-                  className="xl:hidden"
-                  onClick={handleCloseChat}
-                >
-                  <OsdsIcon
-                    name={ODS_ICON_NAME.CLOSE}
-                    size={ODS_ICON_SIZE.md}
-                    contrasted
-                  />
-                  <span className="sr-only">{`Close Live Chat`}</span>
-                </OsdsButton>
-
-                {/* Desktop view: Open Chat */}
-                <OsdsButton
-                  aria-label="Open Live Chat"
-                  circle
-                  color={ODS_THEME_COLOR_INTENT.primary}
-                  onClick={() => handleReduceChat(false)}
-                  className="hidden xl:flex"
-                >
-                  <OsdsIcon
-                    name={ODS_ICON_NAME.SPEECH_BUBBLE_CONCEPT}
-                    size={ODS_ICON_SIZE.md}
-                    contrasted
-                    className="m-2"
-                  />
-                  <span className="sr-only">{`Open Live Chat`}</span>
-                </OsdsButton>
-                <OsdsIcon
-                  name={ODS_ICON_NAME.SPEECH_BUBBLE_CONCEPT}
-                  size={ODS_ICON_SIZE.md}
-                  contrasted
-                  className="xl:hidden"
-                />
-
-                {/* Desktop view: Close Chat */}
-                <OsdsButton
-                  aria-label="Close Live Chat"
-                  circle
-                  contrasted
-                  className="hidden xl:flex absolute top-0 right-0 p-2 z-10"
-                  onClick={handleCloseChat}
-                >
-                  <OsdsIcon
-                    name={ODS_ICON_NAME.CLOSE}
-                    size={ODS_ICON_SIZE.xxs}
-                    color={ODS_THEME_COLOR_INTENT.primary}
-                  />
-                  <span className="sr-only">{`Close Live Chat`}</span>
-                </OsdsButton>
-
-                {/* Mobile view: Open Chat */}
-                <OsdsButton
-                  aria-label="Open Live Chat"
-                  circle
-                  variant={ODS_BUTTON_VARIANT.ghost}
-                  contrasted
-                  onClick={() => handleReduceChat(false)}
-                  className="xl:hidden"
-                >
-                  <OsdsIcon
-                    name={ODS_ICON_NAME.CHEVRON_UP}
-                    size={ODS_ICON_SIZE.md}
-                    contrasted
-                  />
-                  <span className="sr-only">{`Open Live Chat`}</span>
-                </OsdsButton>
-              </>
-            )}
-          </div>
-        </div>
+    <div
+      data-testid="live-chat-wrapper"
+      className="absolute w-full h-full xl:h-fit xl:w-auto bottom-2 right-2 z-[960] flex flex-col justify-end pointer-events-none"
+    >
+      {chatType === 'Adrielly' && (
+        <ChatDialog
+          title="OVHcloud Chat"
+          visible={!chatbotReduced}
+          url={`https://chat.${'ovh'.toLocaleLowerCase()}.com/system/templates/pre-prod/prepa_prod/STD/FR_fr/docs/index2.html`}
+          key={chatType}
+        />
       )}
-    </>
+      {chatType === 'SNOW' && snowContext.session_id && snowContext.queue && (
+        <ChatDialog
+          title={chatType}
+          showHeader={false}
+          visible={!chatbotReduced}
+          url={generateSnowChatUrl(SNOW_INSTANCE_URL, snowContext)}
+          key={chatType}
+        />
+      )}
+      <div
+        data-testid="live-chat-pta-wrapper"
+        className="order-first xl:order-last relative p-3 bg-[#000e9c] xl:bg-transparent flex flex-row items-center justify-between xl:justify-end pointer-events-auto"
+      >
+        {!chatbotReduced && (
+          <>
+            <OsdsButton
+              className="hidden xl:flex"
+              aria-label="Reduce Live Chat"
+              data-testid="live-chat-desktop-reduce-button"
+              circle
+              color={ODS_THEME_COLOR_INTENT.primary}
+              onClick={() => handleReduceChat(true)}
+            >
+              <OsdsIcon
+                name={ODS_ICON_NAME.SPEECH_BUBBLE_CONCEPT}
+                size={ODS_ICON_SIZE.md}
+                className="m-2"
+                contrasted
+              />
+              <span className="sr-only">{`Reduce Live Chat`}</span>
+            </OsdsButton>
+            <OsdsButton
+              className="xl:hidden"
+              aria-label="Reduce Live Chat"
+              data-testid="live-chat-mobile-reduce-button"
+              variant={ODS_BUTTON_VARIANT.ghost}
+              circle
+              contrasted
+              color={ODS_THEME_COLOR_INTENT.primary}
+              onClick={() => handleReduceChat(true)}
+            >
+              <OsdsIcon
+                name={ODS_ICON_NAME.CLOSE}
+                size={ODS_ICON_SIZE.md}
+                contrasted
+              />
+              <span className="sr-only">{`Reduce Live Chat`}</span>
+            </OsdsButton>
+            {chatType === 'Adrielly' && (
+              <OsdsText
+                color={ODS_THEME_COLOR_INTENT.default}
+                hue={ODS_THEME_COLOR_HUE._000}
+                level={ODS_TEXT_LEVEL.heading}
+                size={ODS_TEXT_SIZE._600}
+                className="grow text-center xl:hidden"
+              >
+                OVHcloud Chat
+              </OsdsText>
+            )}
+          </>
+        )}
+        {chatbotReduced && (
+          <>
+            {/* Mobile view: Close Chat */}
+            <OsdsButton
+              aria-label="Close Live Chat"
+              variant={ODS_BUTTON_VARIANT.ghost}
+              data-testid="live-chat-mobile-close-button"
+              circle
+              contrasted
+              className="xl:hidden"
+              onClick={handleCloseChat}
+            >
+              <OsdsIcon
+                name={ODS_ICON_NAME.CLOSE}
+                size={ODS_ICON_SIZE.md}
+                contrasted
+              />
+              <span className="sr-only">{`Close Live Chat`}</span>
+            </OsdsButton>
+
+            {/* Desktop view: Open Chat */}
+            <OsdsButton
+              aria-label="Open Live Chat"
+              data-testid="live-chat-desktop-open-button"
+              circle
+              color={ODS_THEME_COLOR_INTENT.primary}
+              onClick={() => handleReduceChat(false)}
+              className="hidden xl:flex"
+            >
+              <OsdsIcon
+                name={ODS_ICON_NAME.SPEECH_BUBBLE_CONCEPT}
+                size={ODS_ICON_SIZE.md}
+                contrasted
+                className="m-2"
+              />
+              <span className="sr-only">{`Open Live Chat`}</span>
+            </OsdsButton>
+            <OsdsIcon
+              name={ODS_ICON_NAME.SPEECH_BUBBLE_CONCEPT}
+              size={ODS_ICON_SIZE.md}
+              contrasted
+              className="xl:hidden"
+            />
+
+            {/* Desktop view: Close Chat */}
+            <OsdsButton
+              aria-label="Close Live Chat"
+              data-testid="live-chat-desktop-close-button"
+              circle
+              contrasted
+              className="hidden xl:flex absolute top-0 right-0 p-2 z-10"
+              onClick={handleCloseChat}
+            >
+              <OsdsIcon
+                name={ODS_ICON_NAME.CLOSE}
+                size={ODS_ICON_SIZE.xxs}
+                color={ODS_THEME_COLOR_INTENT.primary}
+              />
+              <span className="sr-only">{`Close Live Chat`}</span>
+            </OsdsButton>
+
+            {/* Mobile view: Open Chat */}
+            <OsdsButton
+              aria-label="Open Live Chat"
+              data-testid="live-chat-mobile-open-button"
+              circle
+              variant={ODS_BUTTON_VARIANT.ghost}
+              contrasted
+              onClick={() => handleReduceChat(false)}
+              className="xl:hidden"
+            >
+              <OsdsIcon
+                name={ODS_ICON_NAME.CHEVRON_UP}
+                size={ODS_ICON_SIZE.md}
+                contrasted
+              />
+              <span className="sr-only">{`Open Live Chat`}</span>
+            </OsdsButton>
+          </>
+        )}
+      </div>
+    </div>
   );
 }
