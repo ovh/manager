cabb18554d9fe3ecf8ecb8ac1a4d1631cc29aa1c
Kylian Bainvel <kylian.bainvel@ovhcloud.com>
2025-01-28 15:37:45 +0100

    feat(web-ongoing-operations): add domain datagrid
    Signed-off-by: Kylian Bainvel <kylian.bainvel@ovhcloud.com>

--- packages/manager/apps/web-ongoing-operations/src/setupTests.ts
+++ packages/manager/apps/web-ongoing-operations/src/setupTests.ts
@@ -9,4 +9,34 @@ vi.mock('react-i18next', () => ({
       changeLanguage: () => new Promise(() => {}),
     },
   }),
+  getI18n: () => ({
+    language: (language: string) => language,
+  }),
+}));
+
+vi.mock('@ovh-ux/manager-core-api', async () => {
+  const mod = await vi.importActual('@ovh-ux/manager-core-api');
+  return {
+    ...mod,
+    fetchIcebergV6: vi.fn(),
+    v6: {
+      get: vi.fn(),
+      post: vi.fn(),
+      put: vi.fn(),
+      delete: vi.fn(),
+    },
+  };
+});
+
+vi.mock(import('@tanstack/react-query'), async (importOriginal) => {
+  const actual = await importOriginal();
+  return {
+    ...actual,
+    useQuery: vi.fn(),
+  };
+});
+
+vi.mock('@/utils/utils', () => ({
+  formatDatagridDate: vi.fn(),
+  removeQuotes: vi.fn(),
 }));
