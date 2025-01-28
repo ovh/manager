ca335b771abd15fccad7dfdd1ac900fdbfbe122b
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-01-28 16:45:57 +0100

    fix(ips): qa feedbacks
    ref: MANAGER-15838
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

---
+++ packages/manager/apps/ips/src/components/CountrySelector/country-selector.component.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { OdsSelect } from '@ovhcloud/ods-components/react';
+import {
+  OdsSelectCustomEvent,
+  OdsSelectChangeEventDetail,
+} from '@ovhcloud/ods-components';
+
+export type CountrySelectorProps = {
+  name: string;
+  countryCodeList: string[];
+  onChange?: (event: OdsSelectCustomEvent<OdsSelectChangeEventDetail>) => void;
+  value?: string;
+  isDisabled?: boolean;
+};
+
+export const CountrySelector: React.FC<CountrySelectorProps> = ({
+  countryCodeList,
+  onChange,
+  ...props
+}) => {
+  const { t } = useTranslation('region-selector');
+
+  return (
+    <OdsSelect
+      key={countryCodeList.join('-')}
+      className="block w-full max-w-[384px]"
+      onOdsChange={onChange}
+      customRenderer={{
+        option: (data) => `<div style="display: flex">
+            <span>${t(`region-selector-country-name_${data.value}`)}</span>
+            <span style="width: 32px; height: 24px; margin: auto 15px auto auto; background-image: url('flags/${
+              data.value
+            }.svg')"></span>
+          </div>`,
+      }}
+      {...props}
+    >
+      {countryCodeList.map((country) => (
+        <option key={country} value={country}>
+          {t(`region-selector-country-name_${country}`)}
+        </option>
+      ))}
+    </OdsSelect>
+  );
+};
--- packages/manager/apps/ips/src/pages/order/sections/GeolocationSection.component.tsx
+++ packages/manager/apps/ips/src/pages/order/sections/GeolocationSection.component.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { OdsSelect } from '@ovhcloud/ods-components/react';
 import { OrderSection } from '../../../components/OrderSection/OrderSection.component';
 import { useAvailableGeolocationFromPlanCode } from '@/data/hooks/catalog';
 import { OrderContext } from '../order.context';
 import { IpOffer } from '../order.constant';
+import { CountrySelector } from '@/components/CountrySelector/country-selector.component';
 
 export const GeolocationSection: React.FC = () => {
   const {
@@ -14,7 +14,6 @@ export const GeolocationSection: React.FC = () => {
     selectedOffer,
   } = React.useContext(OrderContext);
   const { t } = useTranslation('order');
-  const { t: tRegionSelector } = useTranslation('region-selector');
   const { geolocations } = useAvailableGeolocationFromPlanCode(
     selectedPlanCode,
   );
@@ -24,32 +23,15 @@ export const GeolocationSection: React.FC = () => {
       title={t('geolocation_selection_title')}
       description={t('geolocation_selection_description')}
     >
-      <OdsSelect
-        key={geolocations.join('-')}
-        className="block w-full max-w-[384px]"
+      <CountrySelector
         name="ip-geolocation"
+        countryCodeList={geolocations}
         isDisabled={selectedOffer === IpOffer.additionalIp}
-        onOdsChange={(event) =>
+        value={selectedGeolocation}
+        onChange={(event) =>
           setSelectedGeolocation(event.target.value as string)
         }
-        value={selectedGeolocation}
-        customRenderer={{
-          option: (data) => `<div style="display: flex">
-            <span>${tRegionSelector(
-              `region-selector-country-name_${data.value}`,
-            )}</span>
-            <span style="width: 32px; height: 24px; margin: auto 15px auto auto; background-image: url('flags/${
-              data.value
-            }.svg')"></span>
-          </div>`,
-        }}
-      >
-        {geolocations.map((country) => (
-          <option key={country} value={country}>
-            {tRegionSelector(`region-selector-country-name_${country}`)}
-          </option>
-        ))}
-      </OdsSelect>
+      />
     </OrderSection>
   );
 };
