17fe56331bb7f7c1a698f180032ce2d422678ac7
Yoann Fievez <yoann.fievez@gmail.com>
2025-01-29 15:17:00 +0100

    feat(pci-cold-archive): add manage container modal
    ref: DTCORE-2033
    Signed-off-by: Yoann Fievez <yoann.fievez@gmail.com>

--- packages/manager/apps/pci-cold-archive/src/api/data/region.ts
+++ packages/manager/apps/pci-cold-archive/src/api/data/region.ts
@@ -1,10 +1,36 @@
 import { v6 } from '@ovh-ux/manager-core-api';
+import { TRegion } from '@ovh-ux/manager-pci-common';
+
+type TPlan = {
+  code: string;
+  regions: string[];
+};
+
+type TProduct = {
+  name: string;
+  regions: string[];
+};
+
+type TAvailabilityData = {
+  plans: TPlan[];
+  products: TProduct[];
+};
 
 export const getProductRegionsAvailability = async (
   ovhSubsidiary: string,
 ): Promise<string[]> => {
-  const { data } = await v6.get(
+  const { data } = await v6.get<TAvailabilityData>(
     `/cloud/order/rule/availability?ovhSubsidiary=${ovhSubsidiary}&planCode=coldarchive.archive.hour.consumption`,
   );
   return data?.plans[0]?.regions;
+};
+
+export const getArchiveRegionDetails = async (
+  projectId: string,
+  region: string,
+): Promise<TRegion> => {
+  const { data } = await v6.get<TRegion>(
+    `/cloud/project/${projectId}/region/${region}`,
+  );
+  return data;
 };
--- packages/manager/apps/pci-cold-archive/src/routes.tsx
+++ packages/manager/apps/pci-cold-archive/src/routes.tsx
@@ -5,6 +5,7 @@ export const ROUTE_PATHS = {
   ROOT: '/pci/projects/:projectId/storages/cold-archive',
   STORAGES: '',
   USER_LIST: 'users',
+  MANAGE_CONTAINER: 'manage',
 };
 
 const LayoutPage = lazy(() => import('@/pages/Layout'));
@@ -12,12 +13,20 @@ const StoragePage = lazy(() => import('@/pages/cold-archive/Archive.page'));
 const ListingContainerPage = lazy(() =>
   import('@/pages/cold-archive/container/Listing.page'),
 );
+const ManageContainerPage = lazy(() =>
+  import('@/pages/cold-archive/container/manage/Manage.page'),
+);
 
 const RoutesComponent = () => (
   <Routes>
     <Route id="root" path={ROUTE_PATHS.ROOT} Component={LayoutPage}>
       <Route path={ROUTE_PATHS.STORAGES} Component={StoragePage}>
-        <Route path={ROUTE_PATHS.STORAGES} Component={ListingContainerPage} />
+        <Route path={ROUTE_PATHS.STORAGES} Component={ListingContainerPage}>
+          <Route
+            path={ROUTE_PATHS.MANAGE_CONTAINER}
+            Component={ManageContainerPage}
+          ></Route>
+        </Route>
       </Route>
     </Route>
     <Route path="" element={<>Page not found</>}></Route>
