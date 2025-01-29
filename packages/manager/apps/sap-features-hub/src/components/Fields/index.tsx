4a454f6823625bf76a171badab6c3d9df6bdec09
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-01-29 15:34:47 +0100

    feat(sap-features-hub): add step 7
    ref: MANAGER-15982
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

---
+++ packages/manager/apps/sap-features-hub/src/components/Fields/index.tsx
@@ -0,0 +1,114 @@
+import React, { useId, useMemo } from 'react';
+import { OdsFormField, OdsText } from '@ovhcloud/ods-components/react';
+import {
+  FieldValues,
+  useController,
+  UseControllerProps,
+} from 'react-hook-form';
+import { RhfInput } from './RhfInput.component';
+import { RhfPassword } from './RhfPassword.component';
+import { RhfToggle } from './RhfToggle.component';
+import { RhfFieldContext, useRhfFieldContext } from './RhfField.context';
+
+type RhfFieldProps = React.ComponentProps<typeof OdsFormField> & {
+  controllerParams: UseControllerProps<FieldValues, string>;
+  helperMessage?: string;
+  isHiddenError?: boolean;
+};
+
+export const RhfField = ({
+  controllerParams,
+  className,
+  helperMessage,
+  isHiddenError,
+  ...rest
+}: Readonly<RhfFieldProps>) => {
+  const id = useId();
+  const controller = useController(controllerParams);
+
+  const contextValues = useMemo(
+    () => ({
+      id,
+      helperMessage,
+      controller,
+    }),
+    [id, controller],
+  );
+
+  const hasError = !isHiddenError && !!controller.fieldState?.error;
+  const errorMessage = helperMessage || controller.fieldState?.error?.message;
+
+  return (
+    <RhfFieldContext.Provider value={contextValues}>
+      <OdsFormField
+        className={`max-w-md ${className}`}
+        error={hasError ? errorMessage : undefined}
+        {...rest}
+      ></OdsFormField>
+    </RhfFieldContext.Provider>
+  );
+};
+
+export const RhfLabel = (
+  props: Readonly<React.LabelHTMLAttributes<HTMLLabelElement>>,
+) => {
+  const { id } = useRhfFieldContext();
+
+  return <label htmlFor={id} slot="label" {...props} />;
+};
+
+export const RhfVisualHint = (props: React.ComponentProps<typeof OdsText>) => {
+  return <OdsText slot="visual-hint" preset="caption" {...props} />;
+};
+
+export const RhfHelper = ({
+  className,
+  ...rest
+}: Readonly<React.ComponentProps<typeof OdsText>>) => {
+  return (
+    <OdsText
+      className={`ods-field-helper ${className ?? ''}`}
+      slot="helper"
+      preset="caption"
+      {...rest}
+    />
+  );
+};
+
+export const RhfHelperAuto = (
+  props: Readonly<Omit<React.ComponentProps<typeof OdsText>, 'children'>>,
+) => {
+  const { helperMessage, controller } = useRhfFieldContext();
+
+  if (!controller.fieldState.error) {
+    return <RhfHelper {...props}>{helperMessage}</RhfHelper>;
+  }
+
+  return <></>;
+};
+
+export const RhfVisualHintCounter = ({
+  max,
+  ...rest
+}: Readonly<
+  React.ComponentProps<typeof OdsText> & { max: string | number }
+>) => {
+  const {
+    controller: { field },
+  } = useRhfFieldContext();
+
+  return (
+    <RhfVisualHint {...rest}>
+      {field.value?.length ?? 0}/{max}
+    </RhfVisualHint>
+  );
+};
+
+RhfField.Label = RhfLabel;
+RhfField.VisualHint = RhfVisualHint;
+RhfField.VisualHintCounter = RhfVisualHintCounter;
+RhfField.Helper = RhfHelper;
+RhfField.HelperAuto = RhfHelperAuto;
+RhfField.Input = RhfInput;
+RhfField.Password = RhfPassword;
+RhfField.Toggle = RhfToggle;
