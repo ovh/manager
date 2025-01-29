4271a11036d06fd06a79577cbcf682e5772c98bd
David Arsène <david.arsene.ext@ovhcloud.com>
2025-01-29 12:13:09 +0100

    refactor(key-management-service): clean playwright references
    ref: MANAGER-16074
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/key-management-service/src/mocks/reference/reference.handler.ts
+++ packages/manager/apps/key-management-service/src/mocks/reference/reference.handler.ts
@@ -1,4 +1,4 @@
-import { Handler } from '../../../../../../../playwright-helpers';
+import { Handler } from '@ovh-ux/manager-core-test-utils';
 import { referenceServiceKeyMock } from './reference.mock';
 
 export type GetReferenceMockParams = {
