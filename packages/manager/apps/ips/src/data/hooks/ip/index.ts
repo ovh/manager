f937d483a4f64de723b9eeab0b2157cc3e171ef0
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2025-01-30 16:00:03 +0100

    feat(ips): create basic listing datagrid
    ref: #MANAGER-16797
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

---
+++ packages/manager/apps/ips/src/data/hooks/ip/index.ts
@@ -0,0 +1,7 @@
+export * from './useGetIpList';
+export * from './useGetIpDetails';
+export * from './useGetIpReverse';
+export * from './useGetIpVmac';
+export * from './useGetIpMitigation';
+export * from './useGetIpEdgeFirewall';
+export * from './useGetIpGameFirewall';
