2344e384bc8356156269a41c36024c2985fa8262
Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
2025-01-30 14:47:38 +0100

    feat(container): refactor beta modal and container context
    ref: MANAGER-16786
    
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>

--- packages/manager/apps/container/src/container/common/pnr-beta-modal/index.tsx
+++ packages/manager/apps/container/src/container/common/pnr-beta-modal/index.tsx
@@ -6,78 +6,60 @@ import useContainer from '@/core/container';
 import style from './style.module.scss';
 
 import backgroundImage from '@/assets/images/pnr/background.png';
-import previewImage from '@/assets/images/pnr/preview.png';
 import { useShell } from '@/context';
-
 
 function NavReshuffleBetaAccessModal(): JSX.Element {
   const { t } = useTranslation('beta-modal');
   const shell = useShell();
   const trackingPlugin = shell.getPlugin('tracking');
-  const { askBeta, createBetaChoice } = useContainer();
-  const [submitting, setSubmitting] = useState(false);
+  const user = shell
+    .getPlugin('environment')
+    .getEnvironment()
+    .getUser();
+  const { betaAcknowledged, acknowledgeBeta } = useContainer();
 
   async function onAccept() {
-    setSubmitting(true);
     trackingPlugin.trackClick({
-      name: 'switch_versionpopin_V3::product-navigation-reshuffle::go_to_new_version',
+      name:
+        'switch_versionpopin_V3::product-navigation-reshuffle::go_to_new_version',
       type: 'action',
     });
-    return createBetaChoice(true).then(() => window.location.reload());
+
+    acknowledgeBeta();
   }
 
-  async function onDecline() {
-    setSubmitting(true);
-    trackingPlugin.trackClick({
-      name: 'switch_versionpopin_V3::product-navigation-reshuffle:::decline_new_version',
-      type: 'action',
-    });
-    return createBetaChoice(false).then(() => window.location.reload());
-  }
+  // useEffect(() => {
+  //   if (askBeta) {
+  //     trackingPlugin.trackPage('product-navigation-reshuffle::switch_version_V3::go_to_new_version');
+  //   }
+  // }, [askBeta]);
 
-  useEffect(() => {
-    if (askBeta) {
-      trackingPlugin.trackPage('product-navigation-reshuffle::switch_version_V3::go_to_new_version');
-    }
-  }, [askBeta]);
+  if (betaAcknowledged) return null;
 
   return (
-    <div className={`${style.backdrop} ${askBeta ? '' : style.hidden}`}>
+    <div className={`${style.backdrop}`}>
       <div
-        className={`${style.modal} ${submitting ? style.hidden : ''}`}
+        className={`${style.modal}`}
         style={{ backgroundImage: `url(${backgroundImage})` }}
         role="dialog"
         aria-modal="true"
       >
-        <div className={style.leftCol}>
-          <h1>{t('beta_modal_title')}</h1>
-          <p>{t('beta_modal_infos')}</p>
-          <button
-            type="button"
-            className="oui-button oui-button_primary mb-2"
-            onClick={onAccept}
-          >
-            {t('beta_modal_accept')}
-          </button>
-          <button
-            type="button"
-            className="oui-button oui-button_link"
-            onClick={onDecline}
-          >
-            <span>{t('beta_modal_decline')}</span>
-            <span
-              className="oui-icon oui-icon-arrow-right"
-              aria-hidden="true"
-            ></span>
-          </button>
+        <h1>{t('beta_modal_title')}</h1>
+        <div className="flex flex-col gap-1">
+          <p className="mb-1">
+            {t('beta_modal_greeting', { user: user.firstname })}
+          </p>
+          <p className="mb-2">{t('beta_modal_welcome')}</p>
+          <p className="mb-2">{t('beta_modal_info')}</p>
+          <p>{t('beta_modal_explore')}</p>
         </div>
-        <div className={style.rightCol}>
-          <img
-            src={previewImage}
-            alt={t('beta_modal_screenshot')}
-            aria-hidden="true"
-          />
-        </div>
+        <button
+          type="button"
+          className="oui-button oui-button_primary mb-2"
+          onClick={onAccept}
+        >
+          {t('beta_modal_accept')}
+        </button>
       </div>
     </div>
   );
