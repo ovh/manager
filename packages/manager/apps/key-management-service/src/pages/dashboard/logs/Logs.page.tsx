7193116595e281652e9bff56e99dba3f7ee0f041
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-01-30 16:26:13 +0100

    feat(logs): add data-stream page
    ref: MANAGER-15920
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

--- packages/manager/apps/key-management-service/src/pages/dashboard/logs/Logs.page.tsx
+++ packages/manager/apps/key-management-service/src/pages/dashboard/logs/Logs.page.tsx
@@ -1,12 +1,15 @@
-import LogsToCustomerModule from '@ovh-ux/logs-to-customer/src/LogsToCustomer.module';
-import { Description } from '@ovh-ux/manager-react-components';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
+import LogsToCustomerModule from '@ovh-ux/logs-to-customer/src/LogsToCustomer.module';
+import { Description } from '@ovh-ux/manager-react-components';
+import { useOKMSById } from '@/data/hooks/useOKMS';
 
 export default function KmsLogs() {
-  const { okmsId } = useParams();
   const { t } = useTranslation('key-management-service/logs');
+
+  const { okmsId } = useParams();
+  const { data: okms } = useOKMSById(okmsId);
 
   return (
     <div className="flex flex-col gap-4">
@@ -18,6 +21,11 @@ export default function KmsLogs() {
           logSubscription: `/okms/resource/${okmsId}/log/subscription`,
           logUrl: `/okms/resource/${okmsId}/log/url`,
         }}
+        logIamActions={{
+          postSubscription: ['okms:apiovh:log/subscription/create'],
+          deleteSubscription: ['okms:apiovh:log/subscription/delete'],
+        }}
+        resourceURN={okms.data.iam.urn}
       />
     </div>
   );
---
+++ packages/manager/modules/logs-to-customer/src/data/mocks/logRetention.handler.ts
@@ -0,0 +1,21 @@
+import { Handler } from '../../../../../../../playwright-helpers';
+import { logRetentionsMock } from './logRetention.mock';
+
+export type GetLogRetentionMocksParams = {
+  isLogRetentionKO?: boolean;
+};
+
+export const LogRetentionError = 'log retention error';
+
+export const getLogRetentionMocks = ({
+  isLogRetentionKO,
+}: GetLogRetentionMocksParams): Handler[] => [
+  {
+    url: '/dbaas/logs/:serviceName/cluster/:clusterId/retention/:retentionid',
+    response: isLogRetentionKO
+      ? { message: LogRetentionError }
+      : logRetentionsMock[0],
+    status: isLogRetentionKO ? 500 : 200,
+    api: 'v6',
+  },
+];
