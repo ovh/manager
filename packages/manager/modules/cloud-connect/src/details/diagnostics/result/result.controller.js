a1a1c7e7cba5225f055fe2db66cb1b60a52dbc68
mhelhali-soufien <159853039+mhelhali-soufien@users.noreply.github.com>
2025-01-31 14:54:56 +0100

    feat(dedicated): download and copy diagnostic result (#15100)
     ref: MANAGER-16172
    
    Signed-off-by: soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>

--- packages/manager/modules/cloud-connect/src/details/diagnostics/result/result.controller.js
+++ packages/manager/modules/cloud-connect/src/details/diagnostics/result/result.controller.js
@@ -1,7 +1,33 @@
 export default class DiagnosticResultCtrl {
   /* @ngInject */
-  constructor($translate, cloudConnectService) {
+  constructor(
+    $timeout,
+    $translate,
+    cloudConnectService,
+    cloudConnectDiagnosticsService,
+  ) {
+    this.$timeout = $timeout;
     this.$translate = $translate;
     this.cloudConnectService = cloudConnectService;
+    this.cloudConnectDiagnosticsService = cloudConnectDiagnosticsService;
+  }
+
+  copyToClipboard(diagnostic) {
+    if (navigator.clipboard && navigator.clipboard.writeText) {
+      try {
+        navigator.clipboard.writeText(diagnostic.result[0].output);
+        this.copyDone = true;
+        this.$timeout(() => {
+          this.copyDone = false;
+        }, 500);
+      } catch (error) {
+        return error;
+      }
+    }
+    return '';
+  }
+
+  download(diagnostic) {
+    return this.cloudConnectDiagnosticsService.download(diagnostic);
   }
 }
