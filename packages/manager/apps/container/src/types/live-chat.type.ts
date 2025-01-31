cc6834686b0885e0d65bf9e940132bac7b0323ec
Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
2025-01-31 17:19:38 +0100

    feat(container): add function to close SNOW session
    ref: MANAGER-15587
    
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>

--- packages/manager/apps/container/src/types/live-chat.type.ts
+++ packages/manager/apps/container/src/types/live-chat.type.ts
@@ -3,3 +3,15 @@ export type LiveChatState = 'open' | 'reduced';
 export type ApiV6AuthToken = {
   token: string;
 };
+
+export type SnowChatContext = {
+  skip_load_history?: string;
+  live_agent_only?: string;
+  language?: string;
+  region?: string;
+  queue: string;
+  branding_key?: string;
+  session_id: string;
+  interface_type?: string;
+  interface_name?: string;
+};
