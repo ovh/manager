888f99e46592f949541196df08dcf2d2f0c93bcc
mhelhali-soufien <159853039+mhelhali-soufien@users.noreply.github.com>
2025-01-31 14:54:56 +0100

    feat(dedicated): download and copy diagnostic result (#15100)
     ref: MANAGER-16172
    
    Signed-off-by: soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>

--- packages/manager/modules/cloud-connect/src/details/diagnostics/diagnostics.module.js
+++ packages/manager/modules/cloud-connect/src/details/diagnostics/diagnostics.module.js
@@ -2,6 +2,8 @@ import angular from 'angular';
 
 import component from './diagnostics.component';
 import routing from './diagnostics.routing';
+import service from './diagnostics.service';
+
 import ovhCloudConnectDiagnosticsResult from './result';
 
 const moduleName = 'ovhCloudConnectDiagnosticsDetails';
@@ -9,6 +11,7 @@ const moduleName = 'ovhCloudConnectDiagnosticsDetails';
 angular
   .module(moduleName, [ovhCloudConnectDiagnosticsResult])
   .config(routing)
+  .service('cloudConnectDiagnosticsService', service)
   .component('cloudConnectDiagnosticsDetails', component)
   .run(/* @ngTranslationsInject:json ./translations */);
 
