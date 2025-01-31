12b5a76867efb852bb595cf69d86017c49f11f8b
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-01-31 17:33:42 +0100

    feat(dedicated): add datacenter network tab
    ref: MANAGER-16613
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

---
+++ packages/manager/apps/dedicated/client/app/dedicatedCloud/datacenter/dedicatedCloud-datacenter.constants.js
@@ -0,0 +1,12 @@
+export const NETWORK_LABEL = 'Network';
+export const NSX_COMPATIBLE_COMMERCIAL_RANGE = [
+  'NSX',
+  'NSX-T',
+  'SDDC 2016',
+  'SDDC 2018',
+];
+
+export default {
+  NSX_COMPATIBLE_COMMERCIAL_RANGE,
+  NETWORK_LABEL,
+};
