597dc2780244dcfe2bd9ca712e42d2f839e64d0e
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-01-31 17:33:42 +0100

    feat(dedicated): add datacenter network tab
    ref: MANAGER-16613
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

---
+++ packages/manager/apps/dedicated/client/app/components/dedicated-cloud/datacenter/network/onboarding/dedicatedCloud-datacenter-network-onboarding.constants.js
@@ -0,0 +1,191 @@
+export const NSXT_EDGE_PLAN_CODE = 'pcc-option-edge-nsxt';
+export const NSXT_EDGE_PRICING_MODE = 'pcc-servicepack-vrops-nsxt';
+
+export const DATACENTER_NETWORK_ONBOARDING_MORE_CTA = {
+  DEFAULT: 'https://www.ovhcloud.com/en/hosted-private-cloud/vmware/nsx/',
+  FR: 'https://www.ovhcloud.com/fr/hosted-private-cloud/vmware/nsx/',
+  EN: 'https://www.ovhcloud.com/en/hosted-private-cloud/vmware/nsx/',
+  DE: 'https://www.ovhcloud.com/de/hosted-private-cloud/vmware/nsx/',
+  ES: 'https://www.ovhcloud.com/es/hosted-private-cloud/vmware/nsx/',
+  'ES-ES': 'https://www.ovhcloud.com/es-es/hosted-private-cloud/vmware/nsx/',
+  IE: 'https://www.ovhcloud.com/en-ie/hosted-private-cloud/vmware/nsx/',
+  IT: 'https://www.ovhcloud.com/it/hosted-private-cloud/vmware/nsx/',
+  NL: 'https://www.ovhcloud.com/nl/hosted-private-cloud/vmware/nsx/',
+  PL: 'https://www.ovhcloud.com/pl/hosted-private-cloud/vmware/nsx/',
+  PT: 'https://www.ovhcloud.com/pt/hosted-private-cloud/vmware/nsx/',
+  GB: 'https://www.ovhcloud.com/en-gb/hosted-private-cloud/vmware/nsx/',
+  CA: 'https://www.ovhcloud.com/en-ca/hosted-private-cloud/vmware/nsx/',
+  QC: 'https://www.ovhcloud.com/fr-ca/hosted-private-cloud/vmware/nsx/',
+  US: 'https://us.ovhcloud.com/hosted-private-cloud/vmware/nsx/',
+  MA: 'https://www.ovhcloud.com/fr-ma/hosted-private-cloud/vmware/nsx/',
+  SN: 'https://www.ovhcloud.com/fr-sn/hosted-private-cloud/vmware/nsx/',
+  TN: 'https://www.ovhcloud.com/fr-tn/hosted-private-cloud/vmware/nsx/',
+  AU: 'https://www.ovhcloud.com/en-au/hosted-private-cloud/vmware/nsx/',
+  IN: 'https://www.ovhcloud.com/en-in/hosted-private-cloud/vmware/nsx/',
+  SG: 'https://www.ovhcloud.com/en-sg/hosted-private-cloud/vmware/nsx/',
+  VN: 'https://www.ovhcloud.com/asia/hosted-private-cloud/vmware/nsx/',
+  ASIA: 'https://www.ovhcloud.com/asia/hosted-private-cloud/vmware/nsx/',
+};
+
+export const DATACENTER_NETWORK_ONBOARDING_GUIDES = [
+  {
+    id: 'dedicatedCloud_datacenter_network_onboarding_doc1_title',
+    title: 'dedicatedCloud_datacenter_network_onboarding_doc1_title',
+    description:
+      'dedicatedCloud_datacenter_network_onboarding_doc1_description',
+    links: {
+      DEFAULT:
+        'https://help.ovhcloud.com/csm/en-vmware-nsx-first-steps?id=kb_article_view&sysparm_article=KB0056833',
+      FR:
+        'https://help.ovhcloud.com/csm/fr-vmware-nsx-first-steps?id=kb_article_view&sysparm_article=KB0056837',
+      EN:
+        'https://help.ovhcloud.com/csm/en-vmware-nsx-first-steps?id=kb_article_view&sysparm_article=KB0056833',
+      DE:
+        'https://help.ovhcloud.com/csm/de-vmware-nsx-first-steps?id=kb_article_view&sysparm_article=KB0056825',
+      ES:
+        'https://help.ovhcloud.com/csm/es-vmware-nsx-first-steps?id=kb_article_view&sysparm_article=KB0056835',
+      'ES-ES':
+        'https://help.ovhcloud.com/csm/es-es-vmware-nsx-first-steps?id=kb_article_view&sysparm_article=KB0056832',
+      IE:
+        'https://help.ovhcloud.com/csm/en-ie-vmware-nsx-first-steps?id=kb_article_view&sysparm_article=KB0056826',
+      IT:
+        'https://help.ovhcloud.com/csm/it-vmware-nsx-first-steps?id=kb_article_view&sysparm_article=KB0056838',
+      NL:
+        'https://help.ovhcloud.com/csm/en-nl-documentation-hosted-private-cloud?id=kb_browse_cat&kb_id=62e4cfed55d574502d4c6e78b7421953',
+      PL:
+        'https://help.ovhcloud.com/csm/pl-vmware-nsx-first-steps?id=kb_article_view&sysparm_article=KB0056836',
+      PT:
+        'https://help.ovhcloud.com/csm/pt-vmware-nsx-first-steps?id=kb_article_view&sysparm_article=KB0056840',
+      GB:
+        'https://help.ovhcloud.com/csm/en-gb-vmware-nsx-first-steps?id=kb_article_view&sysparm_article=KB0056831',
+      CA:
+        'https://help.ovhcloud.com/csm/en-ca-vmware-nsx-first-steps?id=kb_article_view&sysparm_article=KB0056827',
+      QC:
+        'https://help.ovhcloud.com/csm/fr-ca-vmware-nsx-first-steps?id=kb_article_view&sysparm_article=KB0056834',
+      US: 'https://us.ovhcloud.com/support/',
+      MA:
+        'https://help.ovhcloud.com/csm/fr-vmware-nsx-first-steps?id=kb_article_view&sysparm_article=KB0056837',
+      SN:
+        'https://help.ovhcloud.com/csm/fr-vmware-nsx-first-steps?id=kb_article_view&sysparm_article=KB0056837',
+      TN:
+        'https://help.ovhcloud.com/csm/fr-vmware-nsx-first-steps?id=kb_article_view&sysparm_article=KB0056837',
+      AU:
+        'https://help.ovhcloud.com/csm/en-au-vmware-nsx-first-steps?id=kb_article_view&sysparm_article=KB0056828',
+      IN:
+        'https://help.ovhcloud.com/csm/asia-vmware-nsx-first-steps?id=kb_article_view&sysparm_article=KB0056829',
+      SG:
+        'https://help.ovhcloud.com/csm/en-sg-vmware-nsx-first-steps?id=kb_article_view&sysparm_article=KB0056830',
+      VN:
+        'https://help.ovhcloud.com/csm/asia-vmware-nsx-first-steps?id=kb_article_view&sysparm_article=KB0056829',
+      ASIA:
+        'https://help.ovhcloud.com/csm/asia-vmware-nsx-first-steps?id=kb_article_view&sysparm_article=KB0056829',
+    },
+  },
+  {
+    id: 'dedicatedCloud_datacenter_network_onboarding_doc2_title',
+    title: 'dedicatedCloud_datacenter_network_onboarding_doc2_title',
+    description:
+      'dedicatedCloud_datacenter_network_onboarding_doc2_description',
+    links: {
+      DEFAULT:
+        'https://help.ovhcloud.com/csm/en-vmware-nsx-interface-user-access?id=kb_article_view&sysparm_article=KB0063106',
+      FR:
+        'https://help.ovhcloud.com/csm/fr-vmware-nsx-interface-user-access?id=kb_article_view&sysparm_article=KB0062999',
+      EN:
+        'https://help.ovhcloud.com/csm/en-vmware-nsx-interface-user-access?id=kb_article_view&sysparm_article=KB0063106',
+      DE:
+        'https://help.ovhcloud.com/csm/de-vmware-nsx-interface-user-access?id=kb_article_view&sysparm_article=KB0063109',
+      ES:
+        'https://help.ovhcloud.com/csm/es-vmware-nsx-interface-user-access?id=kb_article_view&sysparm_article=KB0063105',
+      'ES-ES':
+        'https://help.ovhcloud.com/csm/es-es-vmware-nsx-interface-user-access?id=kb_article_view&sysparm_article=KB0063114',
+      IE:
+        'https://help.ovhcloud.com/csm/en-ie-vmware-nsx-interface-user-access?id=kb_article_view&sysparm_article=KB0063117',
+      IT:
+        'https://help.ovhcloud.com/csm/it-vmware-nsx-interface-user-access?id=kb_article_view&sysparm_article=KB0063113',
+      NL:
+        'https://help.ovhcloud.com/csm/en-nl-documentation-hosted-private-cloud?id=kb_browse_cat&kb_id=62e4cfed55d574502d4c6e78b7421953',
+      PL:
+        'https://help.ovhcloud.com/csm/pl-vmware-nsx-interface-user-access?id=kb_article_view&sysparm_article=KB0063107',
+      PT:
+        'https://help.ovhcloud.com/csm/pt-vmware-nsx-interface-user-access?id=kb_article_view&sysparm_article=KB0063112',
+      GB:
+        'https://help.ovhcloud.com/csm/en-gb-vmware-nsx-interface-user-access?id=kb_article_view&sysparm_article=KB0063118',
+      CA:
+        'https://help.ovhcloud.com/csm/en-ca-vmware-nsx-interface-user-access?id=kb_article_view&sysparm_article=KB0063110',
+      QC:
+        'https://help.ovhcloud.com/csm/fr-ca-vmware-nsx-interface-user-access?id=kb_article_view&sysparm_article=KB0063108',
+      US: 'https://us.ovhcloud.com/support/',
+      MA:
+        'https://help.ovhcloud.com/csm/fr-vmware-nsx-interface-user-access?id=kb_article_view&sysparm_article=KB0062999',
+      SN:
+        'https://help.ovhcloud.com/csm/fr-vmware-nsx-interface-user-access?id=kb_article_view&sysparm_article=KB0062999',
+      TN:
+        'https://help.ovhcloud.com/csm/fr-vmware-nsx-interface-user-access?id=kb_article_view&sysparm_article=KB0062999',
+      AU:
+        'https://help.ovhcloud.com/csm/en-au-vmware-nsx-interface-user-access?id=kb_article_view&sysparm_article=KB0063116',
+      IN:
+        'https://help.ovhcloud.com/csm/asia-vmware-nsx-interface-user-access?id=kb_article_view&sysparm_article=KB0063115',
+      SG:
+        'https://help.ovhcloud.com/csm/en-sg-vmware-nsx-interface-user-access?id=kb_article_view&sysparm_article=KB0063111',
+      VN:
+        'https://help.ovhcloud.com/csm/asia-vmware-nsx-interface-user-access?id=kb_article_view&sysparm_article=KB0063115',
+      ASIA:
+        'https://help.ovhcloud.com/csm/asia-vmware-nsx-interface-user-access?id=kb_article_view&sysparm_article=KB0063115',
+    },
+  },
+  {
+    id: 'dedicatedCloud_datacenter_network_onboarding_doc3_title',
+    title: 'dedicatedCloud_datacenter_network_onboarding_doc3_title',
+    category: 'dedicatedCloud_datacenter_network_onboarding_doc_category_faq',
+    description:
+      'dedicatedCloud_datacenter_network_onboarding_doc3_description',
+    links: {
+      DEFAULT:
+        'https://help.ovhcloud.com/csm/en-vmware-nsx-faq?id=kb_article_view&sysparm_article=KB0058377',
+      FR:
+        'https://help.ovhcloud.com/csm/fr-vmware-nsx-faq?id=kb_article_view&sysparm_article=KB0058413',
+      EN:
+        'https://help.ovhcloud.com/csm/en-vmware-nsx-faq?id=kb_article_view&sysparm_article=KB0058377',
+      DE:
+        'https://help.ovhcloud.com/csm/de-vmware-nsx-faq?id=kb_article_view&sysparm_article=KB0058372',
+      ES:
+        'https://help.ovhcloud.com/csm/es-vmware-nsx-faq?id=kb_article_view&sysparm_article=KB0058367',
+      'ES-ES':
+        'https://help.ovhcloud.com/csm/es-es-vmware-nsx-faq?id=kb_article_view&sysparm_article=KB0058378',
+      IE:
+        'https://help.ovhcloud.com/csm/en-ie-vmware-nsx-faq?id=kb_article_view&sysparm_article=KB0058370',
+      IT:
+        'https://help.ovhcloud.com/csm/it-vmware-nsx-faq?id=kb_article_view&sysparm_article=KB0058369',
+      NL:
+        'https://help.ovhcloud.com/csm/en-nl-documentation-hosted-private-cloud?id=kb_browse_cat&kb_id=62e4cfed55d574502d4c6e78b7421953',
+      PL:
+        'https://help.ovhcloud.com/csm/pl-vmware-nsx-faq?id=kb_article_view&sysparm_article=KB0058375',
+      PT:
+        'https://help.ovhcloud.com/csm/pt-vmware-nsx-faq?id=kb_article_view&sysparm_article=KB0058374',
+      GB:
+        'https://help.ovhcloud.com/csm/en-gb-vmware-nsx-faq?id=kb_article_view&sysparm_article=KB0058349',
+      CA:
+        'https://help.ovhcloud.com/csm/en-ca-vmware-nsx-faq?id=kb_article_view&sysparm_article=KB0058368',
+      QC:
+        'https://help.ovhcloud.com/csm/fr-ca-vmware-nsx-faq?id=kb_article_view&sysparm_article=KB0058414',
+      US: 'https://us.ovhcloud.com/support/',
+      MA:
+        'https://help.ovhcloud.com/csm/fr-vmware-nsx-faq?id=kb_article_view&sysparm_article=KB0058413',
+      SN:
+        'https://help.ovhcloud.com/csm/fr-vmware-nsx-faq?id=kb_article_view&sysparm_article=KB0058413',
+      TN:
+        'https://help.ovhcloud.com/csm/fr-vmware-nsx-faq?id=kb_article_view&sysparm_article=KB0058413',
+      AU:
+        'https://help.ovhcloud.com/csm/en-au-vmware-nsx-faq?id=kb_article_view&sysparm_article=KB0058373',
+      IN:
+        'https://help.ovhcloud.com/csm/asia-vmware-nsx-faq?id=kb_article_view&sysparm_article=KB0058376',
+      SG:
+        'https://help.ovhcloud.com/csm/en-sg-vmware-nsx-faq?id=kb_article_view&sysparm_article=KB0058371',
+      VN:
+        'https://help.ovhcloud.com/csm/asia-vmware-nsx-faq?id=kb_article_view&sysparm_article=KB0058376',
+      ASIA:
+        'https://help.ovhcloud.com/csm/asia-vmware-nsx-faq?id=kb_article_view&sysparm_article=KB0058376',
+    },
+  },
+];
---
+++ packages/manager/apps/dedicated/client/app/dedicatedCloud/datacenter/network/dedicatedCloud-datacenter-network.routing.js
@@ -0,0 +1,51 @@
+import { NSXT_EDGE_PLAN_CODE } from '../../../components/dedicated-cloud/datacenter/network/onboarding/dedicatedCloud-datacenter-network-onboarding.constants';
+import { NETWORK_LABEL } from '../dedicatedCloud-datacenter.constants';
+
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state(
+    'app.dedicatedCloud.details.datacenter.details.network',
+    {
+      url: '/network',
+      views: {
+        pccDatacenterView: {
+          component: 'ovhManagerDedicatedCloudDatacenterNetwork',
+        },
+      },
+      redirectTo: (transition) => {
+        const getIsNsxtCompatible = transition
+          .injector()
+          .getAsync('isNsxtCompatible');
+        const getHasSubscribedToNsxt = transition
+          .injector()
+          .getAsync('hasSubscribedToNsxt');
+
+        return Promise.all([getIsNsxtCompatible, getHasSubscribedToNsxt]).then(
+          ([isNsxtCompatible, hasSubscribedToNsxt]) => {
+            if (!isNsxtCompatible) {
+              return {
+                state: 'app.dedicatedCloud.details.datacenter.details',
+              };
+            }
+            return !hasSubscribedToNsxt
+              ? {
+                  state:
+                    'app.dedicatedCloud.details.datacenter.details.network.onboarding',
+                }
+              : false;
+          },
+        );
+      },
+      resolve: {
+        serviceId: /* @ngInject */ (currentService) =>
+          currentService.serviceInfos.serviceId,
+        serviceOptions: /* @ngInject */ (serviceId, DedicatedCloud) =>
+          DedicatedCloud.getDatacenterOptions(serviceId),
+        hasSubscribedToNsxt: /* @ngInject */ (serviceOptions) =>
+          serviceOptions.some(
+            (option) => option.billing.plan.code === NSXT_EDGE_PLAN_CODE,
+          ),
+        breadcrumb: () => NETWORK_LABEL,
+      },
+    },
+  );
+};
