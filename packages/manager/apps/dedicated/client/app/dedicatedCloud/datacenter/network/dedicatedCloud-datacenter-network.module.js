66c1ba4d495f48fec3f5ed613287c6039b76deb7
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-01-31 17:33:42 +0100

    feat(dedicated): add datacenter network tab
    ref: MANAGER-16613
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

---
+++ packages/manager/apps/dedicated/client/app/dedicatedCloud/datacenter/network/dedicatedCloud-datacenter-network.module.js
@@ -0,0 +1,11 @@
+import networkComponent from '../../../components/dedicated-cloud/datacenter/network';
+import onboardingModule from './onboarding';
+import routing from './dedicatedCloud-datacenter-network.routing';
+
+const moduleName = 'ovhManagerDedicatedCloudDatacenterNetworkModule';
+
+angular
+  .module(moduleName, [networkComponent, onboardingModule])
+  .config(routing);
+
+export default moduleName;
