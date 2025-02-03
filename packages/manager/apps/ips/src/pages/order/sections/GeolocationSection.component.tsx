667921370fa225439328195aeb6fbb1314e58fe8
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-02-03 17:09:33 +0100

    feat(ips): order dedicated server
    ref: MANAGER-15839
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager/apps/ips/src/pages/order/sections/GeolocationSection.component.tsx
+++ packages/manager/apps/ips/src/pages/order/sections/GeolocationSection.component.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { OdsSkeleton } from '@ovhcloud/ods-components/react';
 import { OrderSection } from '../../../components/OrderSection/OrderSection.component';
 import { useAvailableGeolocationFromPlanCode } from '@/data/hooks/catalog';
 import { OrderContext } from '../order.context';
 import { IpOffer } from '../order.constant';
 import { CountrySelector } from '@/components/CountrySelector/country-selector.component';
+import { ServiceType } from '@/types';
 
 export const GeolocationSection: React.FC = () => {
   const {
     selectedPlanCode,
     selectedGeolocation,
+    selectedServiceType,
     setSelectedGeolocation,
     selectedOffer,
   } = React.useContext(OrderContext);
@@ -23,15 +26,21 @@ export const GeolocationSection: React.FC = () => {
       title={t('geolocation_selection_title')}
       description={t('geolocation_selection_description')}
     >
-      <CountrySelector
-        name="ip-geolocation"
-        countryCodeList={geolocations}
-        isDisabled={selectedOffer === IpOffer.additionalIp}
-        value={selectedGeolocation}
-        onChange={(event) =>
-          setSelectedGeolocation(event.target.value as string)
-        }
-      />
+      <React.Suspense fallback={<OdsSkeleton />}>
+        <CountrySelector
+          name="ip-geolocation"
+          className="max-w-[384px]"
+          countryCodeList={geolocations}
+          isDisabled={
+            selectedOffer === IpOffer.additionalIp &&
+            selectedServiceType === ServiceType.ipParking
+          }
+          value={selectedGeolocation}
+          onChange={(event) =>
+            setSelectedGeolocation(event.target.value as string)
+          }
+        />
+      </React.Suspense>
     </OrderSection>
   );
 };
