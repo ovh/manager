2bd78911e5f8c19c14f016361ee9bd8f14a9237f
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2025-02-05 10:41:15 +0100

    feat(ips): datagrid skeleton cells and filters
    ref: #MANAGER-16797
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

--- packages/manager/apps/ips/src/data/hooks/ip/useGetIpList.ts
+++ packages/manager/apps/ips/src/data/hooks/ip/useGetIpList.ts
@@ -1,14 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import { ApiError, ApiResponse } from '@ovh-ux/manager-core-api';
-import { getIpList, getIpListQueryKey } from '@/data/api';
+import { GetIpListParams, getIpList, getIpListQueryKey } from '@/data/api';
 
-export const useGetIpList = () => {
+export const useGetIpList = (params: GetIpListParams) => {
   const { data: ipListResponse, isLoading, isError, error } = useQuery<
     ApiResponse<string[]>,
     ApiError
   >({
-    queryKey: getIpListQueryKey({}),
-    queryFn: () => getIpList({}),
+    queryKey: getIpListQueryKey(params),
+    queryFn: () => getIpList(params),
   });
 
   return { ipList: ipListResponse?.data, isLoading, isError, error };
---
+++ packages/manager/apps/ips/src/pages/listing/components/filters/FilterType.tsx
@@ -0,0 +1,35 @@
+import React, { useContext } from 'react';
+import { OdsSelect } from '@ovhcloud/ods-components/react';
+import {
+  OdsSelectCustomEvent,
+  OdsSelectChangeEventDetail,
+} from '@ovhcloud/ods-components';
+import { useTranslation } from 'react-i18next';
+import { IpTypeEnum } from '@/data/api';
+import { ListingContext } from '@/pages/listing/listingContext';
+
+/**
+ * Component to filter ip by name
+ * @param ipList the ip list
+ * @returns React component
+ */
+export const TypeFilter = () => {
+  const { t } = useTranslation('listing');
+  const { apiFilter, setApiFilter } = useContext(ListingContext);
+
+  return (
+    <OdsSelect
+      name="search-type"
+      onOdsChange={(e: OdsSelectCustomEvent<OdsSelectChangeEventDetail>) => {
+        setApiFilter({ ...apiFilter, type: e.target.value as IpTypeEnum });
+      }}
+      placeholder={t('listingFilterType')}
+    >
+      {Object.values(IpTypeEnum).map((value) => (
+        <option key={value} value={value}>
+          {value}
+        </option>
+      ))}
+    </OdsSelect>
+  );
+};
