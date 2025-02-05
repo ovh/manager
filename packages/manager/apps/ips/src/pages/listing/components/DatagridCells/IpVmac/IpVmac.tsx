d73923df56d0f5d8f2cca89952b135262e958c04
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2025-02-05 10:41:15 +0100

    feat(ips): datagrid skeleton cells and filters
    ref: MANAGER-16797
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

--- packages/manager/apps/ips/src/pages/listing/components/DatagridCells/IpVmac/IpVmac.tsx
+++ packages/manager/apps/ips/src/pages/listing/components/DatagridCells/IpVmac/IpVmac.tsx
@@ -1,12 +1,13 @@
-import { OdsSkeleton } from '@ovhcloud/ods-components/react';
-import React from 'react';
+import React, { useContext } from 'react';
 import { IpTypeEnum } from '@/data/api';
 import { useGetIpVmac, useGetIpdetails } from '@/data/hooks/ip';
 import { FailoverRoutedServiceType, getTypeByServiceName } from '@/utils';
 import { ipFormatter } from '@/utils/ipFormatter';
+import { SkeletonCell } from '../SkeletonCell/SkeletonCell';
+import { ListingContext } from '@/pages/listing/listingContext';
 
 export type IpDdosProps = {
-  ipGroup: string;
+  ip: string;
 };
 
 /**
@@ -14,19 +15,22 @@ export type IpDdosProps = {
  * If ip is not /32 display nothing
  * If ip is not routed to a dedicated server display nothing
  * If ip is /32 and linked to a dedicated server but has no vmac display "-"
- * @param ipGroup the ip with mask
+ * @param ip the ip with mask
  * @returns React Component
  */
-export const IpVmac = ({ ipGroup }: IpDdosProps) => {
+export const IpVmac = ({ ip }: IpDdosProps) => {
+  const { expiredIps } = useContext(ListingContext);
   // Check if ip is a group
-  const { isGroup } = ipFormatter(ipGroup);
+  const { isGroup } = ipFormatter(ip);
 
   // check if ip is routed to a dedicated server
   const { ipDetails, isLoading: isIpDetailsLoading } = useGetIpdetails({
-    ip: ipGroup,
+    ip,
     enabled: !isGroup,
   });
+
   const enabled =
+    expiredIps.indexOf(ip) === -1 &&
     !isIpDetailsLoading &&
     (ipDetails?.type === IpTypeEnum.ADDITIONAL ||
       ipDetails?.type === IpTypeEnum.DEDICATED) &&
@@ -35,22 +39,23 @@ export const IpVmac = ({ ipGroup }: IpDdosProps) => {
       FailoverRoutedServiceType.DEDICATED;
 
   // get vmacs if ip is routed to a dedicated server
-  const { vmacs, isLoading } = useGetIpVmac({
-    ipGroup,
+  const { vmacs, isLoading, error } = useGetIpVmac({
     serviceName: ipDetails?.routedTo?.serviceName,
     enabled,
   });
 
-  if (isGroup) return null;
-  if (isLoading || isIpDetailsLoading) return <OdsSkeleton></OdsSkeleton>;
-  if (!enabled) return null;
-  if (!vmacs?.length) return <>-</>;
-
   return (
-    <>
-      {vmacs.map((vmac) => {
-        return <div key={vmac.macAddress}>{vmac.macAddress}</div>;
-      })}
-    </>
+    <SkeletonCell
+      isLoading={isLoading || isIpDetailsLoading}
+      enabled={!isGroup}
+      error={error}
+    >
+      {!enabled && null}
+      {enabled && !vmacs?.length && <>-</>}
+      {enabled &&
+        vmacs?.map((vmac) => {
+          return <div key={vmac.macAddress}>{vmac.macAddress}</div>;
+        })}
+    </SkeletonCell>
   );
 };
