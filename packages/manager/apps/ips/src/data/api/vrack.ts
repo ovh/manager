3b70f9de4f3415183587b5214ef92ccc107f3f45
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-02-05 17:10:02 +0100

    fix(ips): fix order feedbacks
    ref: #MANAGER-15838
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager/apps/ips/src/data/api/vrack.ts
+++ packages/manager/apps/ips/src/data/api/vrack.ts
@@ -2,3 +2,24 @@ import { ApiResponse, apiClient } from '@ovh-ux/manager-core-api';
 
 export const getVrackList = (): Promise<ApiResponse<string[]>> =>
   apiClient.v6.get('/vrack');
+
+export type VrackServiceInfos = {
+  contactAdmin: string;
+  contactBilling: string;
+  contactTech: string;
+  creation: string;
+  domain: string;
+  serviceId: number;
+  status:
+    | 'autorenewInProgress'
+    | 'expired'
+    | 'inCreation'
+    | 'ok'
+    | 'pendingDebt'
+    | 'unPaid';
+};
+
+export const getVrackServiceInfos = (
+  serviceName: string,
+): Promise<ApiResponse<VrackServiceInfos>> =>
+  apiClient.v6.get(`/vrack/${serviceName}/serviceInfos`);
