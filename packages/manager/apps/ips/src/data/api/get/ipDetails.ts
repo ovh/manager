18adb639be24b83f79351eff3bec750f143ab2a7
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2025-02-06 16:28:02 +0100

    feat(ips): add cells icons, badges and action menu
    ref: MANAGER-16797
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

--- packages/manager/apps/ips/src/data/api/get/ipDetails.ts
+++ packages/manager/apps/ips/src/data/api/get/ipDetails.ts
@@ -35,6 +35,7 @@ export type IpDetails = {
   routedTo?: IpRoutedToType;
   version: number;
   description: string;
+  country: string;
 };
 
 export const getIpDetailsQueryKey = (params: GetIpDetailsParams) => [
--- packages/manager/apps/ips/src/pages/listing/components/DatagridCells/IpAttachedService/IpAttachedService.tsx
+++ packages/manager/apps/ips/src/pages/listing/components/DatagridCells/IpAttachedService/IpAttachedService.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ShellContext } from '@ovh-ux/manager-react-shell-client';
+import { OdsLink } from '@ovhcloud/ods-components/react';
+import { ODS_LINK_COLOR } from '@ovhcloud/ods-components';
 import { useGetIpdetails } from '@/data/hooks/ip';
 import { SkeletonCell } from '../SkeletonCell/SkeletonCell';
+import { getLinkByServiceName } from '@/utils';
 
 export type IpAttachedServiceProps = {
   ip: string;
@@ -9,16 +13,37 @@ export type IpAttachedServiceProps = {
 /**
  * Component to display the cell content for IP Attached service
  * If ip has no attached services display "-"
+ * If ip has attached service, we display it as a link if possible
  * @param ip the ip with mask
  * @returns React Component
  */
 export const IpAttachedService = ({ ip }: IpAttachedServiceProps) => {
+  const { shell } = React.useContext(ShellContext);
+  const [serviceUrl, setServiceUrl] = useState<string>();
+
   const { ipDetails, isLoading } = useGetIpdetails({ ip });
+
+  getLinkByServiceName({
+    serviceName: ipDetails?.routedTo?.serviceName,
+    navigation: shell.navigation,
+  }).then(setServiceUrl);
 
   return (
     <SkeletonCell isLoading={isLoading}>
       {!ipDetails?.routedTo?.serviceName && <>-</>}
-      {ipDetails?.routedTo?.serviceName}
+      {ipDetails?.routedTo?.serviceName && (
+        <>
+          {serviceUrl ? (
+            <OdsLink
+              href={serviceUrl}
+              color={ODS_LINK_COLOR.primary}
+              label={ipDetails.routedTo.serviceName}
+            />
+          ) : (
+            ipDetails.routedTo.serviceName
+          )}
+        </>
+      )}
     </SkeletonCell>
   );
 };
