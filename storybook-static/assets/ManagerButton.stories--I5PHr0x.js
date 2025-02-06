ed6a3c79e983e8d91fa818fbabcedf1abe6f5813
ovh-cds <ovh-cds@users.noreply.github.com>
2025-02-06 14:45:39 +0000

    docs: update documentation [skip ci]

--- storybook-static/assets/ManagerButton.stories--I5PHr0x.js
+++
@@ -1,8 +0,0 @@
-import{M as a}from"./ManagerButton-BMV99zZ4.js";import"./jsx-runtime-CKrituN3.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D2Jq8zTl.js";import"./index-BtM5VmRH.js";import"./i18next-ihUiNgJT.js";import"./useOvhIam-D508DiNg.js";import"./QueryClientProvider-CDvRjfR1.js";import"./useTranslation-CvcVFFFk.js";const e={args:{id:"iam-button-urn-action-1",label:"Remove button",urn:"urn:v9:eu:resource:manager-react-components:vrz-a878-dsflkds-fdsfsd",iamActions:["vrackServices:apiovh:iam/resource/tag/remove"]}},l={title:"Components/Manager Button",component:a,parameters:{docs:{description:{component:"The `ManagerButton` component is used to trigger an action or event."}}}};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    id: 'iam-button-urn-action-1',
-    label: 'Remove button',
-    urn: 'urn:v9:eu:resource:manager-react-components:vrz-a878-dsflkds-fdsfsd',
-    iamActions: ['vrackServices:apiovh:iam/resource/tag/remove']
-  }
-}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const f=["Default"];export{e as Default,f as __namedExportsOrder,l as default};
---
+++ storybook-static/assets/error.stories-DG_S3WOu.js
@@ -0,0 +1,11 @@
+import{j as s}from"./jsx-runtime-CKrituN3.js";import{E as m}from"./error.component-B2H50QM6.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D2Jq8zTl.js";import"./index-BtM5VmRH.js";import"./index-Cs_lFblA.js";import"./i18next-ihUiNgJT.js";import"./error-banner-oops-tNXFEWkx.js";import"./useTranslation-pmbu4BU3.js";const r={error:{status:404,data:{message:"Votre requête n'a pas abouti"},headers:{"x-ovh-queryid":"123456789"}}},g={title:"Templates/Errors",decorators:[a=>s.jsx("div",{className:"w-2/3",children:a()})],component:m,argTypes:{},args:r};var e,t,o;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  error: {
+    status: 404,
+    data: {
+      message: "Votre requête n'a pas abouti"
+    },
+    headers: {
+      'x-ovh-queryid': '123456789'
+    }
+  }
+}`,...(o=(t=r.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const h=["defaultProps"];export{h as __namedExportsOrder,g as default,r as defaultProps};
