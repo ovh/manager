cb1483cad82a81a4bac97a0f8667c386c2c4c982
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2025-02-06 16:28:02 +0100

    feat(ips): add cells icons, badges and action menu
    ref: #MANAGER-16797
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

--- packages/manager/apps/ips/src/utils/getTypeByServiceName.ts
+++ packages/manager/apps/ips/src/utils/getTypeByServiceName.ts
@@ -2,8 +2,9 @@ export type GetTypeByServiceNameParams = {
   serviceName: string;
 };
 
-export enum FailoverRoutedServiceType {
+export enum IPRoutedServiceType {
   DEDICATED = 'dedicated',
+  HOSTING = 'housing',
   PCC = 'pcc',
   CLOUD = 'cloud',
   VPS = 'vps',
@@ -14,7 +15,9 @@ export enum FailoverRoutedServiceType {
 
 export const getTypeByServiceName = ({
   serviceName,
-}: GetTypeByServiceNameParams): FailoverRoutedServiceType => {
+}: GetTypeByServiceNameParams): IPRoutedServiceType => {
+  if (!serviceName) return IPRoutedServiceType.PARKING;
+
   const dedicatedRegex = new RegExp(/^([a-z]{2}\d+.ip-[\d-]+.\w+)/);
   const pccRegex = new RegExp(/^(pcc[\d-]+)/);
   const cloudRegex = new RegExp(/^[\w\d]{32}/);
@@ -22,14 +25,13 @@ export const getTypeByServiceName = ({
   const vrackRegex = new RegExp(/^pn[\w\d-.]+/);
   const loadBalancerRegex = new RegExp(/^loadbalancer-[\w\d]+/);
 
-  if (dedicatedRegex.test(serviceName))
-    return FailoverRoutedServiceType.DEDICATED;
-  if (pccRegex.test(serviceName)) return FailoverRoutedServiceType.PCC;
-  if (cloudRegex.test(serviceName)) return FailoverRoutedServiceType.CLOUD;
-  if (vpsRegex.test(serviceName)) return FailoverRoutedServiceType.VPS;
-  if (vrackRegex.test(serviceName)) return FailoverRoutedServiceType.VRACK;
+  if (dedicatedRegex.test(serviceName)) return IPRoutedServiceType.DEDICATED;
+  if (pccRegex.test(serviceName)) return IPRoutedServiceType.PCC;
+  if (cloudRegex.test(serviceName)) return IPRoutedServiceType.CLOUD;
+  if (vpsRegex.test(serviceName)) return IPRoutedServiceType.VPS;
+  if (vrackRegex.test(serviceName)) return IPRoutedServiceType.VRACK;
   if (loadBalancerRegex.test(serviceName))
-    return FailoverRoutedServiceType.LOADBALANCER;
+    return IPRoutedServiceType.LOADBALANCER;
 
-  return FailoverRoutedServiceType.PARKING;
+  return IPRoutedServiceType.PARKING;
 };
