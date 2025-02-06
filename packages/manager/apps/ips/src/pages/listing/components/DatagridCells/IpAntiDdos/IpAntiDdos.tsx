ec515af99bda68db7f8517ef8b31ab9d05398e05
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2025-02-06 16:28:02 +0100

    feat(ips): add cells icons, badges and action menu
    ref: MANAGER-16797
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

--- packages/manager/apps/ips/src/pages/listing/components/DatagridCells/IpAntiDdos/IpAntiDdos.tsx
+++ packages/manager/apps/ips/src/pages/listing/components/DatagridCells/IpAntiDdos/IpAntiDdos.tsx
@@ -1,9 +1,13 @@
 import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
+import { ODS_ICON_NAME } from '@ovhcloud/ods-components';
 import { ipFormatter } from '@/utils/ipFormatter';
 import { useGetIpdetails, useGetIpMitigation } from '@/data/hooks/ip';
 import { SkeletonCell } from '../SkeletonCell/SkeletonCell';
 import { ListingContext } from '@/pages/listing/listingContext';
+import { IconCell } from '../IconCell/IconCell';
+import { datagridCellStyle } from '../datagridCellStyles';
+import { IpMitigationStateEnum } from '@/data/api';
 
 export type IpAntiDdosProps = {
   ip: string;
@@ -12,11 +16,15 @@ export type IpAntiDdosProps = {
 /**
  * Component to display the cell content for Anti DDOS.
  * If ip is not /32 (isGroup = true) or not ipv4, we display nothing.
- * If ip mitigation is Permanent we display permanent if no mitigation or not permanent, we display automatic
+ * If ip mitigation is Permanent we display permanent
+ * If ip mitigation is Auto, we display In Action
+ * If ip mitigation has no mitigation, we display automatic
+ * If mitigation state is not "ok", we display pending
  * @param ip the ip with mask
  * @returns React component
  */
 export const IpAntiDdos = ({ ip }: IpAntiDdosProps) => {
+  const id = `antiddos-${ip.replace(/\/|\./g, '-')}`;
   const { expiredIps } = useContext(ListingContext);
   const { t } = useTranslation('listing');
 
@@ -47,15 +55,47 @@ export const IpAntiDdos = ({ ip }: IpAntiDdosProps) => {
       error={error}
     >
       {enabled && !ipMitigation?.length && (
-        <div>{t('listingColumnsIpAntiDDosAutomatic')}</div>
+        <IconCell
+          icon={ODS_ICON_NAME.shield}
+          text={t('listingColumnsIpAntiDDosAutomatic')}
+          tooltip={t('listingColumnsIpAntiDDosAutomaticTooltip')}
+          trigger={id}
+        />
       )}
       {enabled &&
         ipMitigation?.map((mitigation) => {
           return (
             <div key={mitigation.ipOnMitigation}>
-              {mitigation.permanent
-                ? t('listingColumnsIpAntiDDosPermanent')
-                : t('listingColumnsIpAntiDDosAutomatic')}
+              {mitigation.state === IpMitigationStateEnum.OK &&
+                mitigation.permanent && (
+                  <IconCell
+                    icon={ODS_ICON_NAME.shieldExclamation}
+                    text={t('listingColumnsIpAntiDDosPermanent')}
+                    style={datagridCellStyle.iconWarning}
+                    tooltip={t('listingColumnsIpAntiDDosPermanentTooltip')}
+                    trigger={id}
+                  />
+                )}
+              {mitigation.state === IpMitigationStateEnum.OK &&
+                mitigation.auto && (
+                  <IconCell
+                    icon={ODS_ICON_NAME.shieldCheck}
+                    text={t('listingColumnsIpAntiDDosInAction')}
+                    style={datagridCellStyle.iconSuccess}
+                    tooltip={t('listingColumnsIpAntiDDosInActionTooltip')}
+                    trigger={id}
+                  />
+                )}
+              {!!mitigation &&
+                mitigation.state !== IpMitigationStateEnum.OK && (
+                  <IconCell
+                    icon={ODS_ICON_NAME.shieldMinus}
+                    text={t('listingColumnsIpAntiDDosPending')}
+                    style={datagridCellStyle.iconWarning}
+                    tooltip={t('listingColumnsIpAntiDDosPendingTooltip')}
+                    trigger={id}
+                  />
+                )}
             </div>
           );
         })}
