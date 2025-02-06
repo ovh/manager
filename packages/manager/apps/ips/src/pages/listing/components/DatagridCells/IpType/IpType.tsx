3c29accf103442909a36c62ee6bfae62cd5b553a
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2025-02-06 16:28:02 +0100

    feat(ips): add cells icons, badges and action menu
    ref: MANAGER-16797
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

--- packages/manager/apps/ips/src/pages/listing/components/DatagridCells/IpType/IpType.tsx
+++ packages/manager/apps/ips/src/pages/listing/components/DatagridCells/IpType/IpType.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { OdsBadge } from '@ovhcloud/ods-components/react';
+import { useTranslation } from 'react-i18next';
+import { ODS_BADGE_COLOR, ODS_BADGE_SIZE } from '@ovhcloud/ods-components';
 import { useGetIpdetails } from '@/data/hooks/ip';
 import { SkeletonCell } from '../SkeletonCell/SkeletonCell';
+import { IpTypeEnum } from '@/data/api';
 
 export type IpTypeProps = {
   ip: string;
@@ -13,10 +17,29 @@ export type IpTypeProps = {
  */
 export const IpType = ({ ip }: IpTypeProps) => {
   const { ipDetails, isLoading } = useGetIpdetails({ ip });
+  const { t } = useTranslation('listing');
 
   return (
     <SkeletonCell isLoading={isLoading}>
-      {ipDetails?.type ? ipDetails.type : <>-</>}
+      {ipDetails?.type ? t(`listingColumnsType_${ipDetails.type}`) : <>-</>}
+      {ipDetails?.type === IpTypeEnum.ADDITIONAL &&
+        !!ipDetails?.routedTo?.serviceName && (
+          <OdsBadge
+            label={t('listingColumnsTypeBadgeAssigned')}
+            size={ODS_BADGE_SIZE.sm}
+            color={ODS_BADGE_COLOR.success}
+            className="ml-2"
+          />
+        )}
+      {ipDetails?.type === IpTypeEnum.ADDITIONAL &&
+        !ipDetails?.routedTo?.serviceName && (
+          <OdsBadge
+            label={t('listingColumnsTypeBadgeParked')}
+            size={ODS_BADGE_SIZE.sm}
+            color={ODS_BADGE_COLOR.neutral}
+            className="ml-2"
+          />
+        )}
     </SkeletonCell>
   );
 };
