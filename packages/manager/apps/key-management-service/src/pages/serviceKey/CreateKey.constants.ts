6b04d28a5f38e33ee6e5c4ada9cd4d590c78051a
Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>
2025-02-10 10:13:28 +0100

    fix(kms): fix us pricing display ref: MANAGER-17003
    Signed-off-by: Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>

--- packages/manager/apps/key-management-service/src/pages/serviceKey/CreateKey.constants.ts
+++ packages/manager/apps/key-management-service/src/pages/serviceKey/CreateKey.constants.ts
@@ -1,4 +1,5 @@
-export const KEY_SOFTWARE_PROTECTION_PRICE = 6000000;
+export const KEY_SOFTWARE_PROTECTION_PRICE_EU = 6000000;
+export const KEY_SOFTWARE_PROTECTION_PRICE_US = 7000000;
 
 export const CREATE_KEY_TEST_IDS = {
   ctaConfirm: 'cta-confirm',
