6a7ee2e34fd29c9ae7daf18e0a934bda410b1ace
mhelhali-soufien <159853039+mhelhali-soufien@users.noreply.github.com>
2025-02-10 10:59:59 +0100

    feat(dedicated): add iplb delete option for us (#14243)
     ref: MANAGER-15666
    
    Signed-off-by: soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

---
+++ packages/manager/modules/iplb/src/listing/listing.controller.js
@@ -0,0 +1,29 @@
+import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
+import template from '../modal/terminate/terminate.html';
+
+export default class iplbListingCtrl extends ListLayoutHelper.ListLayoutCtrl {
+  /* @ngInject */
+  constructor($state, $q, ouiDatagridService, CucControllerHelper) {
+    super($q, ouiDatagridService);
+    this.$state = $state;
+    this.ouiDatagridService = ouiDatagridService;
+    this.CucControllerHelper = CucControllerHelper;
+  }
+
+  gotoService({ serviceName }) {
+    this.$state.go('iplb.detail.home', { serviceName });
+  }
+
+  deleteIplb(service) {
+    this.CucControllerHelper.modal.showModal({
+      modalConfig: {
+        template,
+        controller: 'IpLoadBalancerTerminateCtrl',
+        controllerAs: '$ctrl',
+        resolve: {
+          service: () => service,
+        },
+      },
+    });
+  }
+}
