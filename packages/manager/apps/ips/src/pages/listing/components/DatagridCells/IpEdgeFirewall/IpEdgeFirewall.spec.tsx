e85a02fdcf1545be6f29c9f563119bf3be4cfbbf
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2025-02-11 17:03:21 +0100

    feat(ips): listing page tests
    ref: MANAGER-16797
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

---
+++ packages/manager/apps/ips/src/pages/listing/components/DatagridCells/IpEdgeFirewall/IpEdgeFirewall.spec.tsx
@@ -0,0 +1,106 @@
+import '@/test-utils/setupUnitTests';
+import React, { PropsWithChildren } from 'react';
+import { describe, expect, it, vi } from 'vitest';
+import { render, waitFor } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ListingContextProvider } from '@/pages/listing/listingContext';
+import ipDetailsList from '../../../../../../mocks/ip/get-ip-details.json';
+import { IpEdgeFirewall, IpEdgeFirewallProps } from './IpEdgeFirewall';
+import { IpEdgeFirewallStateEnum, IpEdgeFirewallType } from '@/data/api';
+
+const queryClient = new QueryClient();
+/** MOCKS */
+const useGetIpEdgeFirewall = vi.hoisted(() =>
+  vi.fn(() => ({
+    ipEdgeFirewall: undefined,
+    isLoading: true,
+    error: undefined,
+  })),
+);
+
+vi.mock('@/data/hooks/ip', () => ({
+  useGetIpEdgeFirewall,
+}));
+
+vi.mock('../SkeletonCell/SkeletonCell', () => ({
+  SkeletonCell: ({ children }: PropsWithChildren) => <div>{children}</div>,
+}));
+
+/** RENDER */
+const renderComponent = (params: IpEdgeFirewallProps) => {
+  return render(
+    <QueryClientProvider client={queryClient}>
+      <ListingContextProvider>
+        <IpEdgeFirewall {...params} />
+      </ListingContextProvider>
+    </QueryClientProvider>,
+  );
+};
+
+describe('IpEdgeFirewall Component', async () => {
+  it('Should display enable state if firewall has enable to true', async () => {
+    useGetIpEdgeFirewall.mockReturnValue({
+      ipEdgeFirewall: [
+        { enabled: true, state: IpEdgeFirewallStateEnum.OK },
+      ] as IpEdgeFirewallType[],
+      isLoading: false,
+      error: undefined,
+    });
+    const { getByText } = renderComponent({ ip: ipDetailsList[0].ip });
+    await waitFor(() => {
+      expect(getByText(`listingColumnsIpEdgeFirewallEnabled`)).toBeDefined();
+      expect(
+        getByText(`listingColumnsIpEdgeFirewallEnabledTooltip`),
+      ).toBeDefined();
+    });
+  });
+
+  it('Should display disable state if there is no firewall', async () => {
+    useGetIpEdgeFirewall.mockReturnValue({
+      ipEdgeFirewall: [] as IpEdgeFirewallType[],
+      isLoading: false,
+      error: undefined,
+    });
+    const { getByText } = renderComponent({ ip: ipDetailsList[0].ip });
+    await waitFor(() => {
+      expect(getByText(`listingColumnsIpEdgeFirewallDisabled`)).toBeDefined();
+      expect(
+        getByText(`listingColumnsIpEdgeFirewallDisabledTooltip`),
+      ).toBeDefined();
+    });
+  });
+
+  it('Should display disable state if firewall is disable', async () => {
+    useGetIpEdgeFirewall.mockReturnValue({
+      ipEdgeFirewall: [
+        { enabled: false, state: IpEdgeFirewallStateEnum.OK },
+      ] as IpEdgeFirewallType[],
+      isLoading: false,
+      error: undefined,
+    });
+    const { getByText } = renderComponent({ ip: ipDetailsList[0].ip });
+    await waitFor(() => {
+      expect(getByText(`listingColumnsIpEdgeFirewallDisabled`)).toBeDefined();
+      expect(
+        getByText(`listingColumnsIpEdgeFirewallDisabledTooltip`),
+      ).toBeDefined();
+    });
+  });
+
+  it('Should display Pending state if firewall is disable', async () => {
+    useGetIpEdgeFirewall.mockReturnValue({
+      ipEdgeFirewall: [
+        { enabled: false, state: IpEdgeFirewallStateEnum.PENDING_ENABLE },
+      ] as IpEdgeFirewallType[],
+      isLoading: false,
+      error: undefined,
+    });
+    const { getByText } = renderComponent({ ip: ipDetailsList[0].ip });
+    await waitFor(() => {
+      expect(getByText(`listingColumnsIpEdgeFirewallPending`)).toBeDefined();
+      expect(
+        getByText(`listingColumnsIpEdgeFirewallPendingTooltip`),
+      ).toBeDefined();
+    });
+  });
+});
