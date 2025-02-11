f6499e8b945d986ff32d7d4b0b9bb1dc1c3ae989
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-02-11 17:49:05 +0100

    fix(logs): fix logs layout
    ref: MANAGER-16965
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

--- packages/manager/apps/key-management-service/src/pages/dashboard/logs/Logs.page.tsx
+++ packages/manager/apps/key-management-service/src/pages/dashboard/logs/Logs.page.tsx
@@ -1,22 +1,15 @@
 import React from 'react';
-import { OdsText } from '@ovhcloud/ods-components/react';
-import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 import LogsToCustomerModule from '@ovh-ux/logs-to-customer/src/LogsToCustomer.module';
 import { useOKMSById } from '@/data/hooks/useOKMS';
 import { trackClickMap } from './LogsTracking.constant';
 
 export default function KmsLogs() {
-  const { t } = useTranslation('key-management-service/logs');
-
   const { okmsId } = useParams();
   const { data: okms } = useOKMSById(okmsId);
 
   return (
     <div className="flex flex-col gap-4">
-      <OdsText preset="paragraph">
-        {t('key_management_service_logs_description')}
-      </OdsText>
       <LogsToCustomerModule
         logApiVersion="v2"
         logApiUrls={{
