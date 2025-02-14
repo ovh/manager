486599f3c6e9f0ffb56530b695c10b8c37ff4afb
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-02-14 14:48:17 +0100

    refactor(hycu): remove playwright helpers dependencies
    ref: MANAGER-16952
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/apps/hycu/mocks/example/example.ts
+++ packages/manager/apps/hycu/mocks/example/example.ts
@@ -1,4 +1,4 @@
-import { Handler } from '../../../../../../playwright-helpers';
+import { Handler } from '@ovh-ux/manager-core-test-utils';
 import exampleList from './example-data.json';
 
 export type GetExampleMocksParams = { isKo?: boolean; nb?: number };
--- packages/manager/apps/hycu/src/mocks/catalogHycu/catalogHycu.ts
+++ packages/manager/apps/hycu/src/mocks/catalogHycu/catalogHycu.ts
@@ -1,4 +1,4 @@
-import { Handler } from '../../../../../../../playwright-helpers';
+import { Handler } from '@ovh-ux/manager-core-test-utils';
 import { catalog } from './catalogHycu.data';
 
 export type CatalogHycuMocksParams = {
