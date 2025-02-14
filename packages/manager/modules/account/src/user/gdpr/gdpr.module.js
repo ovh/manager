7774d486cd2045e77bcdb93f995b3df33daf068a
Omar <omaralkabous@gmail.com>
2025-02-14 10:06:45 +0100

    feat(account): added the confirm erasure request page (#15442)
    ref: MANAGER-16210
    
    Signed-off-by: Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>

--- packages/manager/modules/account/src/user/gdpr/gdpr.module.js
+++ packages/manager/modules/account/src/user/gdpr/gdpr.module.js
@@ -1,13 +1,14 @@
 import component from './gdpr.component';
 import routing from './gdpr.routing';
 import service from './gdpr.service';
+import confirmModule from './confirm';
 
 import './gdpr.scss';
 
 const moduleName = 'ovhManagerAccountGdprModule';
 
 angular
-  .module(moduleName, [])
+  .module(moduleName, [confirmModule])
   .config(routing)
   .component('gdprFeatures', component)
   .service('gdprService', service)
