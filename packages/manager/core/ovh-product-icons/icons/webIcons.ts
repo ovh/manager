5bcacd6a50632510f8110d66df034b56465b09b3
Tristan WAGNER <t.wagner66250@gmail.com>
2025-02-14 15:34:49 +0100

    feat(container): add zimbra to web side bar order
    ref: MANAGER-17092
    
    Signed-off-by: Tristan WAGNER <t.wagner66250@gmail.com>

--- packages/manager/core/ovh-product-icons/icons/webIcons.ts
+++ packages/manager/core/ovh-product-icons/icons/webIcons.ts
@@ -1,9 +1,10 @@
-import { Hosting, Office365, Exchange } from '../assets-icon-svg/icons';
+import { Hosting, Office365, Exchange, Zimbra } from '../assets-icon-svg/icons';
 
 const webIcons = {
   Hosting,
   Office365,
   Exchange,
+  Zimbra,
 };
 
 export default webIcons;
