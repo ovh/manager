a4e165a6741004f27e2fb794cfeb9a9ee703f098
Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>
2025-02-17 11:50:52 +0100

    feat(pci.kubernetes): improve array
    ref: TAPC-2537
    Signed-off-by: Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>

--- packages/manager/apps/pci-kubernetes/src/pages/new/steps/node-pool/useDataGridColumn.spec.tsx
+++ packages/manager/apps/pci-kubernetes/src/pages/new/steps/node-pool/useDataGridColumn.spec.tsx
@@ -1,9 +1,12 @@
-import { renderHook } from '@testing-library/react';
+import { render, renderHook } from '@testing-library/react';
 import { vi } from 'vitest';
+import { Datagrid } from '@ovh-ux/manager-react-components';
 import { useDatagridColumn } from './useDataGridColumn';
 
+import { wrapper } from '@/wrapperRenders';
+
 const testCases = [
-  ['name', 'listing:kube_list_name'],
+  ['name', 'add:kubernetes_add_name'],
   ['localisation', 'kube_common_node_pool_localisation'],
   ['model', 'kube_common_node_pool_model'],
   ['desiredNodes', 'kube_common_node_pool_desired_node'],
@@ -15,26 +18,63 @@ const testCases = [
 const columnCount = testCases.length;
 
 describe('useDatagridColumn', () => {
-  it.each(testCases)(
-    'devrait retourner la colonne correcte pour %s',
-    (id, label) => {
-      const { result } = renderHook(() =>
-        useDatagridColumn({ onDelete: vi.fn() }),
-      );
-      const columns = result.current;
+  it.each(testCases)('should return the correct column for %s', (id, label) => {
+    const { result } = renderHook(() =>
+      useDatagridColumn({ onDelete: vi.fn() }),
+    );
+    const columns = result.current;
 
-      const column = columns.find((col) => col.id === id);
-      expect(column).toBeDefined();
-      expect(column.label).toBe(label);
-    },
-  );
+    const column = columns.find((col) => col.id === id);
+    expect(column).toBeDefined();
+    expect(column.label).toBe(label);
+  });
 
-  it(`devrait retourner ${columnCount} colonnes`, () => {
+  it(`should return ${columnCount} columns`, () => {
     const { result } = renderHook(() =>
       useDatagridColumn({ onDelete: vi.fn() }),
     );
     const columns = result.current;
 
     expect(columns).toHaveLength(columnCount);
+  });
+
+  it('should show Min Max when autoscale is enabled', () => {
+    const { result } = renderHook(() =>
+      useDatagridColumn({ onDelete: vi.fn() }),
+    );
+    const columns = result.current;
+
+    const props = {
+      autoscale: true,
+      minNodes: 1,
+      maxNodes: 5,
+    };
+
+    const { getByText } = render(
+      <Datagrid totalItems={1} items={[props]} columns={columns} />,
+      { wrapper },
+    );
+
+    expect(getByText('Min 1, Max 5')).toBeInTheDocument();
+  });
+
+  it('should not show Min Max when autoscale is disabled', () => {
+    const { result } = renderHook(() =>
+      useDatagridColumn({ onDelete: vi.fn() }),
+    );
+    const columns = result.current;
+
+    const props = {
+      autoscale: false,
+      minNodes: 1,
+      maxNodes: 5,
+    };
+
+    const { getByText } = render(
+      <Datagrid totalItems={1} items={[props]} columns={columns} />,
+      { wrapper },
+    );
+
+    expect(getByText('kube_node_pool_autoscale_false')).toBeInTheDocument();
   });
 });
