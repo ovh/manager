4323b751828d27f59ef9e46fc18223214c3a0f2b
Hazem Belkhiria <hazem.belkhiria.ext@ovhcloud.com>
2025-02-17 12:05:59 +0100

    fix(commitlint)!: change the library name to config-conventional
    ref: #MANAGER-123
    Signed-off-by: Hazem Belkhiria <hazem.belkhiria.ext@ovhcloud.com>

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,12 +1,12 @@
 const typeEnum = require('@commitlint/config-angular-type-enum');
-
+const types = [...typeEnum.value(), 'sync', 'release', 'build', 'ci', 'revert', 'style'];
 module.exports = {
-  extends: ['@commitlint/config-angular'],
+  extends: ['@commitlint/config-conventional'],
   rules: {
     // Enforce lowercase for header (type(scope): title)
     'header-case': [2, 'always', 'lower-case'],
     'subject-case': [2, 'always', 'lower-case'],
-    'subject-exclamation-mark': [1, 'always'],
+    'subject-exclamation-mark': [0, 'always'],
     'scope-case': [2, 'always', 'lower-case'],
     'scope-enum': [2, 'always'],
     'scope-empty': [2, 'never'],
@@ -17,21 +17,11 @@ module.exports = {
     'references-empty': [2, 'never'],
     // Enforce Signed-off-by footer format
     'signed-off-by': [2, 'always', 'Signed-off-by:'],
-
-    // Enforce BREAKING CHANGE format in the footer
-    'footer-empty': [1, 'never'],
-
+    'footer-empty': [0, 'always'],
     // Enforce specific commit types (feat, fix, etc.)
-    'type-enum': [2, 'always', [
-      'feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'test', 'perf', 'sync', 'release'
-    ]],
+    'type-enum': [2, 'always', types],
 
     'subject-max-length': [2, 'always', 100],
     'header-max-length': [2, 'always', 72],
-  },
-    "parserPreset": {
-      "parserOpts": {
-        "issuePrefixes": ["MANAGER-"]
-      }
-    }
+  }
 };
