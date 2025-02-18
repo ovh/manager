b2bedd48c75f832e38d433480b881638a197d252
Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
2025-02-18 15:41:06 +0100

    feat(hub): refactor order api hooks to optimize call counts
    ref: MANAGER-16593
    
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>

---
+++ packages/manager/apps/hub-react/src/data/hooks/lastOrderTracking/useLastOrderTracking.spec.tsx
@@ -0,0 +1,86 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { renderHook, waitFor } from '@testing-library/react';
+import React, { PropsWithChildren } from 'react';
+import { describe, it, vi } from 'vitest';
+import { useLastOrderTracking } from '@/data/hooks/lastOrderTracking/useLastOrderTracking';
+import { LastOrderTrackingResponse } from '@/types/order.type';
+import * as orderApi from '@/data/api/order/order';
+
+const queryClient = new QueryClient();
+
+const wrapper = ({ children }: PropsWithChildren) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+
+describe('useLastOrderTracking', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+    queryClient.clear();
+  });
+
+  it('useLastOrderTracking should return expected result', async () => {
+    const lastOrderTrackingResponse: LastOrderTrackingResponse = {
+      date: 'date',
+      expirationDate: 'expirationDate',
+      orderId: 2,
+      password: 'fakepassword',
+      pdfUrl: 'pdfurl',
+      priceWithoutTax: undefined,
+      priceWithTax: undefined,
+      retractionDate: 'date r',
+      tax: undefined,
+      url: 'url',
+      history: [],
+      status: 'delivered',
+    };
+
+    const getLastOrder = vi.spyOn(orderApi, 'getLastOrder').mockReturnValue(
+      new Promise((resolve) =>
+        resolve({
+          data: {
+            orderId: 2,
+            date: 'date',
+            expirationDate: 'expirationDate',
+            password: 'fakepassword',
+            pdfUrl: 'pdfurl',
+            priceWithoutTax: undefined,
+            priceWithTax: undefined,
+            retractionDate: 'date r',
+            tax: undefined,
+            url: 'url',
+          },
+          status: 'OK',
+        }),
+      ),
+    );
+
+    const getOrderStatus = vi
+      .spyOn(orderApi, 'getOrderStatus')
+      .mockReturnValue(new Promise((resolve) => resolve('delivered')));
+
+    const getOrderDetails = vi
+      .spyOn(orderApi, 'getOrderDetails')
+      .mockReturnValue(new Promise((resolve) => resolve([])));
+
+    const getCompleteHistory = vi
+      .spyOn(orderApi, 'getCompleteHistory')
+      .mockReturnValue(new Promise((resolve) => resolve([])));
+
+    const { result } = renderHook(() => useLastOrderTracking(), {
+      wrapper,
+    });
+
+    await waitFor(() => {
+      expect(getLastOrder).toHaveBeenCalled();
+      expect(getOrderStatus).toHaveBeenCalled();
+      expect(getOrderDetails).toHaveBeenCalled();
+      expect(getCompleteHistory).toHaveBeenCalled();
+      expect(result.current.data.expirationDate).toEqual(
+        lastOrderTrackingResponse.expirationDate,
+      );
+      expect(result.current.data.status).toEqual(
+        lastOrderTrackingResponse.status,
+      );
+    });
+  });
+});
