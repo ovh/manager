3b63170f6c70d94af643fcd3c9fcc41af94578c0
William Mbotta <william.mbotta.ext@corp.ovh.com>
2025-02-18 10:49:09 +0100

    feat(ips): showing sidebar and redirect on new app

--- packages/manager/apps/container/src/container/nav-reshuffle/sidebar/navigation-tree/node.ts
+++ packages/manager/apps/container/src/container/nav-reshuffle/sidebar/navigation-tree/node.ts
@@ -26,6 +26,7 @@ export type Node = {
   separator?: boolean;
   idAttr?: string;
   hideIfEmpty?: boolean;
+  hideIfFeatures?: string[];
   badge?: string;
   forceVisibility?: boolean;
   forceNavigate?: boolean;
