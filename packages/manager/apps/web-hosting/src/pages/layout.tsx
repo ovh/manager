db92b403a758921400de48ef87fd2398a3fd638d
Tristan WAGNER <t.wagner66250@gmail.com>
2025-02-18 19:01:27 +0100

    feat(web-hosting): init app
    ref: MANAGER-14873
    
    Signed-off-by: Tristan WAGNER <t.wagner66250@gmail.com>

---
+++ packages/manager/apps/web-hosting/src/pages/layout.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useContext } from 'react';
+import { defineCurrentPage } from '@ovh-ux/request-tagger';
+import { Outlet, useLocation, useMatches } from 'react-router-dom';
+import {
+  useOvhTracking,
+  useRouteSynchro,
+  ShellContext,
+} from '@ovh-ux/manager-react-shell-client';
+
+export default function Layout() {
+  const location = useLocation();
+  const { shell } = useContext(ShellContext);
+  const matches = useMatches();
+  const { trackCurrentPage } = useOvhTracking();
+
+  useRouteSynchro();
+
+  useEffect(() => {
+    const match = matches.slice(-1);
+    defineCurrentPage(`app.web-hosting-${match[0]?.id}`);
+    trackCurrentPage();
+  }, [location]);
+
+  useEffect(() => {
+    shell.ux.hidePreloader();
+  }, []);
+
+  return <Outlet />;
+}
