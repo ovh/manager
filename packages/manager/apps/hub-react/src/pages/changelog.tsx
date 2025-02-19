57a19dfb449c768846938d63cf68a57dbabd3d1c
Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>
2025-02-19 09:20:17 +0100

    feat(hub): init changelog page
    Signed-off-by: Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>

---
+++ packages/manager/apps/hub-react/src/pages/changelog.tsx
@@ -0,0 +1,11 @@
+import { BaseLayout, HeadersProps } from '@ovh-ux/manager-react-components';
+import { useTranslation } from 'react-i18next';
+
+export default function Changelog() {
+  const { t } = useTranslation('changelog');
+
+  const header: HeadersProps = {
+    title: t('changelog_title'),
+  };
+  return <BaseLayout header={header}></BaseLayout>;
+}
