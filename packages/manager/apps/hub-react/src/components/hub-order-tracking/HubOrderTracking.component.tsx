966a4bce40f9e48fd217a0c52a25274568c0dc0b
Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>
2025-02-19 18:47:16 +0100

    feat(hub): create page changelog
    ref: MANAGER-16491
    
    Signed-off-by: Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>

--- packages/manager/apps/hub-react/src/components/hub-order-tracking/HubOrderTracking.component.tsx
+++ packages/manager/apps/hub-react/src/components/hub-order-tracking/HubOrderTracking.component.tsx
@@ -38,7 +38,7 @@ import {
 } from '@/data/api/apiOrder/apiOrder.constants';
 import useDateFormat from '@/hooks/dateFormat/useDateFormat';
 import { LastOrderTrackingResponse, OrderHistory } from '@/types/order.type';
-import { useHubContext } from '@/pages/layout/context';
+import { useHubContext } from '@/pages/dashboard/context';
 
 const TileError = lazy(() =>
   import('@/components/tile-error/TileError.component'),
