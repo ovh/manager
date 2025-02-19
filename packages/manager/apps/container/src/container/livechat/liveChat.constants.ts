aa9a0c40a98d8bd85d8372baff78cb270831c6f8
Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
2025-02-19 14:36:36 +0100

    feat(container): update adrielly environment urls
    ref: MANAGER-15587
    
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>

--- packages/manager/apps/container/src/container/livechat/liveChat.constants.ts
+++ packages/manager/apps/container/src/container/livechat/liveChat.constants.ts
@@ -1,5 +1,7 @@
 export const ADRIELLY_CHAT_URL_TEMPLATE =
   'https://chat.ovh.com/system/templates/liveChat-manager/';
+
+export const ADRIELLY_LABEU_CHAT_URL = 'https://eceweb.ovhcloud.dev/system/templates/livechat-manager/STD/FR_fr/docs/index2.html';
 export const ADRIELLY_CHAT_ORIGIN = 'https://chat.ovh.com';
 export const SNOW_INSTANCE_URL = 'https://help.build-ovhcloud.tools';
 export const SNOW_CHAT_QUEUE_STORAGE_KEY = 'snow_chat_queue';
