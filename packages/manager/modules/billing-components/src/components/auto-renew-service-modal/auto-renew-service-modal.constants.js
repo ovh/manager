1cc90b7e29a6f5760bd6605a22921aeef3f046c8
Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>
2025-02-20 16:53:31 +0100

    feat(billing): add renewal modal replacing page in my services
    ref: MANAGER-16119
    
    Signed-off-by: Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>

---
+++ packages/manager/modules/billing-components/src/components/auto-renew-service-modal/auto-renew-service-modal.constants.js
@@ -0,0 +1,13 @@
+export const MANUAL_RENEWAL_TYPES = 'manualPayment';
+
+export const CONTRACTS_IDS = {
+  CA: 1752,
+  QC: 1753,
+  WE: 1754,
+  WS: 1755,
+};
+
+export default {
+  MANUAL_RENEWAL_TYPES,
+  CONTRACTS_IDS,
+};
