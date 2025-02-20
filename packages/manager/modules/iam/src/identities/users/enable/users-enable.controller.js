61bdacd6277963226956cb43e833370f548c30e3
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-02-20 15:54:20 +0100

    feat(*): revamp iam identities
    ref: MANAGER-16216
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

---
+++ packages/manager/modules/iam/src/identities/users/enable/users-enable.controller.js
@@ -0,0 +1,56 @@
+export default class IamUsersEnableCtrl {
+  /* @ngInject */
+  constructor($scope, IamUsersService, Alerter, $translate) {
+    this.$scope = $scope;
+    this.usersService = IamUsersService;
+    this.alerter = Alerter;
+    this.$translate = $translate;
+    this.user = $scope.currentActionData;
+    this.loader = false;
+  }
+
+  $onInit() {
+    this.$scope.enableUser = this.enableUser.bind(this);
+  }
+
+  enableUser() {
+    this.loader = true;
+
+    this.usersService
+      .enableUser(this.user)
+      .then(() => {
+        this.alerter.success(
+          this.$translate.instant('user_users_enable_success_message', {
+            login: this.user.login,
+          }),
+          'iam-users-alert',
+        );
+      })
+      .catch((err) => {
+        if (err.status === 403) {
+          return this.alerter.warning(
+            `${this.$translate.instant(
+              'user_users_enable_error_message',
+            )} ${this.$translate.instant('user_need_rights_message')} ${
+              err.data.details.unauthorizedActionsByIAM
+            }`,
+            'iam-users-alert',
+          );
+        }
+        return this.alerter.error(
+          `${this.$translate.instant('user_users_enable_error_message')} ${
+            err.data.message
+          }`,
+          'iam-users-alert',
+        );
+      })
+      .finally(() => {
+        this.loader = false;
+        this.$scope.resetAction();
+      });
+  }
+
+  close() {
+    this.$scope.resetAction();
+  }
+}
--- packages/manager/modules/iam/src/onboarding/onboarding.routing.js
+++ packages/manager/modules/iam/src/onboarding/onboarding.routing.js
@@ -4,13 +4,14 @@ export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('iam.onboarding', {
     url: '/onboarding',
     component: 'iamOnboarding',
-    redirectTo: (transition) =>
-      transition
-        .injector()
-        .getAsync('hasPolicies')
-        .then((hasPolicies) =>
-          hasPolicies ? { state: 'iam.dashboard' } : false,
-        ),
+    // TODO: commented for now, remove when working on policies pages MANAGER-16217
+    // redirectTo: (transition) =>
+    //   transition
+    //     .injector()
+    //     .getAsync('hasPolicies')
+    //     .then((hasPolicies) =>
+    //       hasPolicies ? { state: 'iam.dashboard' } : false,
+    //     ),
     resolve: {
       breadcrumb: () => null,
     },
