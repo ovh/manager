ca2510325f72a90ffedcf1984e2b36531ef65653
Tristan WAGNER <t.wagner66250@gmail.com>
2025-02-21 16:58:39 +0100

    feat(mrc): add useIcebergV2InfiniteQuery
    ref: ISSUE-13543
    
    Signed-off-by: Tristan WAGNER <t.wagner66250@gmail.com>

--- packages/manager-react-components/src/hooks/datagrid/useIcebergV2.tsx
+++ packages/manager-react-components/src/hooks/datagrid/useIcebergV2.tsx
@@ -1,8 +1,19 @@
 import { useEffect, useState } from 'react';
-import { IcebergFetchParamsV2, fetchIcebergV2 } from '@ovh-ux/manager-core-api';
-import { useInfiniteQuery } from '@tanstack/react-query';
+import {
+  IcebergFetchParamsV2,
+  IcebergFetchResultV2,
+  fetchIcebergV2,
+} from '@ovh-ux/manager-core-api';
+import {
+  InfiniteData,
+  useInfiniteQuery,
+  UseInfiniteQueryOptions,
+  UseInfiniteQueryResult,
+} from '@tanstack/react-query';
 import { defaultPageSize } from './index';
 import { ColumnSort } from '../../components';
+
+export const API_V2_MAX_PAGE_SIZE = 9999;
 
 interface IcebergV2Hook<T> {
   queryKey: string[];
@@ -11,13 +22,14 @@ interface IcebergV2Hook<T> {
   shouldFetchAll?: boolean;
 }
 
-export const API_V2_MAX_PAGE_SIZE = 9999;
-
 /**
  * @deprecated use fetchIcebergV2 from @ovh-ux/manager-core-api
  */
 export const getResourcesIcebergV2 = fetchIcebergV2;
 
+/**
+ * @deprecated use useIcebergV2InfiniteQuery
+ */
 export function useResourcesIcebergV2<T = unknown>({
   route,
   pageSize = defaultPageSize,
@@ -71,4 +83,54 @@ export function useResourcesIcebergV2<T = unknown>({
     error,
     status,
   };
+}
+
+type UseIcebergV2InfiniteQueryParams<T> = {
+  iceberg: Omit<IcebergFetchParamsV2, 'cursor'>;
+  query: Omit<
+    UseInfiniteQueryOptions,
+    'queryFn' | 'getNextPageParam' | 'initialPageParam'
+  > & {
+    select?: (data: InfiniteData<T, unknown>) => T[];
+  };
+  shouldFetchAll?: boolean;
+};
+
+export function useIcebergV2InfiniteQuery<T>({
+  iceberg: { pageSize, search, ...icebergOptions },
+  query: { queryKey, ...queryOptions },
+  shouldFetchAll = false,
+}: UseIcebergV2InfiniteQueryParams<T>) {
+  const query = useInfiniteQuery({
+    // return only flattened data without cursor by default to avoid
+    // unwanted rerenders on refetch if data is the same
+    select: (data) =>
+      data?.pages?.flatMap((page: UseInfiniteQueryResult<T>) => page?.data),
+    ...queryOptions,
+    initialPageParam: null,
+    queryKey: [
+      ...queryKey,
+      shouldFetchAll,
+      search ? `${search.key}=${search.value}` : '',
+    ].filter(Boolean),
+    queryFn: ({ pageParam }) =>
+      fetchIcebergV2<T>({
+        pageSize:
+          pageSize || (shouldFetchAll ? API_V2_MAX_PAGE_SIZE : defaultPageSize),
+        cursor: pageParam as string,
+        search,
+        ...icebergOptions,
+      }),
+    getNextPageParam: (lastPage: IcebergFetchResultV2<T>) =>
+      lastPage.cursorNext,
+  });
+
+  useEffect(() => {
+    // fetch next page if needed
+    if (shouldFetchAll && query.hasNextPage && !query.isFetchingNextPage) {
+      query.fetchNextPage();
+    }
+  }, [query.data]);
+
+  return query;
 }
