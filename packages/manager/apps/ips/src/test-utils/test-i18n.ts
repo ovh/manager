591faeb67fc1d73576e9f47c940f7d83a990391e
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-02-21 12:30:05 +0100

    feat(ips): pcc ipv5 order
    ref: MANAGER-15845
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager/apps/ips/src/test-utils/test-i18n.ts
+++ packages/manager/apps/ips/src/test-utils/test-i18n.ts
@@ -1,15 +1,24 @@
 import order from '../../public/translations/order/Messages_fr_FR.json';
 import listing from '../../public/translations/listing/Messages_fr_FR.json';
 import onboarding from '../../public/translations/onboarding/Messages_fr_FR.json';
+import ips from '../../public/translations/ips/Messages_fr_FR.json';
+import error from '../../public/translations/error/Messages_fr_FR.json';
+import regionSelector from '../../public/translations/region-selector/Messages_fr_FR.json';
 
 export const translations = {
-  [`order`]: order,
-  [`listing`]: listing,
-  [`onboarding`]: onboarding,
-};
-
-export const labels = {
+  ips,
   order,
   listing,
   onboarding,
+  error,
+  'region-selector': regionSelector,
+};
+
+export const labels = {
+  ips,
+  order,
+  listing,
+  onboarding,
+  error,
+  regionSelector,
 };
