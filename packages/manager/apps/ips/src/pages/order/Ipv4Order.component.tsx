0589a7937671d2113b8b939a7937b34981fa99da
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-02-21 12:30:05 +0100

    feat(ips): pcc ipv5 order
    ref: MANAGER-15845
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager/apps/ips/src/pages/order/Ipv4Order.component.tsx
+++ packages/manager/apps/ips/src/pages/order/Ipv4Order.component.tsx
@@ -1,24 +1,60 @@
 import React from 'react';
 import { ServiceSelectionSection } from './sections/ServiceSelectionSection.component';
-import { OrderContext, ServiceStatus } from './order.context';
+import { OrderContext } from './order.context';
 import { IpOffer, IpVersion } from './order.constant';
 import { RegionSelectionSection } from './sections/RegionSelectionSection.component';
 import { OfferSelectionSection } from './sections/OfferSelectionSection.component';
 import { GeolocationSection } from './sections/GeolocationSection.component';
 import { OrganisationSection } from './sections/OrganisationSection.component';
+import { OrderButtonSection } from './sections/OrderButtonSection.component';
+import { useServiceRegion } from '@/data/hooks/useServiceRegion';
 import { ServiceType } from '@/types';
 
 export const Ipv4Order: React.FC = () => {
   const {
     ipVersion,
     selectedService,
-    selectedServiceStatus,
     selectedServiceType,
-    selectedRegion,
     selectedOffer,
+    selectedRegion,
     selectedPlanCode,
     selectedGeolocation,
+    selectedOrganisation,
   } = React.useContext(OrderContext);
+  const { region } = useServiceRegion({
+    serviceName: selectedService,
+    serviceType: selectedServiceType,
+  });
+
+  const isOrganisationSectionVisible =
+    !!selectedService &&
+    selectedOffer === IpOffer.blockAdditionalIp &&
+    selectedServiceType !== ServiceType.dedicatedCloud &&
+    !!selectedPlanCode &&
+    !!(region || selectedRegion) &&
+    !!selectedGeolocation;
+
+  const visibleSections = {
+    service: ipVersion === IpVersion.ipv4,
+    region:
+      !!selectedService &&
+      [ServiceType.ipParking, ServiceType.vrack].includes(selectedServiceType),
+    offer: !!selectedService && !!(selectedRegion || region),
+    geolocation:
+      !!selectedService &&
+      !!selectedOffer &&
+      !!selectedPlanCode &&
+      !!(selectedRegion || region),
+    organisation: isOrganisationSectionVisible,
+    order:
+      !!selectedService &&
+      !!selectedServiceType &&
+      !!(selectedRegion || region) &&
+      !!selectedOffer &&
+      !!selectedPlanCode &&
+      !!selectedGeolocation &&
+      (!isOrganisationSectionVisible || !!selectedOrganisation),
+  };
 
   if (ipVersion !== IpVersion.ipv4) {
     return <></>;
@@ -26,26 +62,12 @@ export const Ipv4Order: React.FC = () => {
 
   return (
     <>
-      <ServiceSelectionSection />
-      {[ServiceType.ipParking, ServiceType.vrack].includes(
-        selectedServiceType,
-      ) &&
-        !!selectedService && <RegionSelectionSection />}
-      {!!selectedService &&
-        selectedServiceStatus === ServiceStatus.ok &&
-        !!selectedRegion && <OfferSelectionSection />}
-      {!!selectedService &&
-        selectedServiceStatus === ServiceStatus.ok &&
-        !!selectedOffer &&
-        !!selectedRegion &&
-        !!selectedPlanCode && <GeolocationSection />}
-      {selectedOffer === IpOffer.blockAdditionalIp &&
-        !!selectedService &&
-        selectedServiceStatus === ServiceStatus.ok &&
-        !!selectedOffer &&
-        !!selectedRegion &&
-        !!selectedPlanCode &&
-        !!selectedGeolocation && <OrganisationSection />}
+      {visibleSections.service && <ServiceSelectionSection />}
+      {visibleSections.region && <RegionSelectionSection />}
+      {visibleSections.offer && <OfferSelectionSection />}
+      {visibleSections.geolocation && <GeolocationSection />}
+      {visibleSections.organisation && <OrganisationSection />}
+      {visibleSections.order && <OrderButtonSection />}
     </>
   );
 };
