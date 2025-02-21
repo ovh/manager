fb0dbe0f8f8bc79b8108f5aa394b0dbd8fcc0da8
stif59100 <steeve.vanderstocken@ovhcloud.com>
2025-02-21 09:16:51 +0100

    feat(mrc): add useBytes hook
    ref:MANAGER-16882
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

--- packages/manager/modules/common-translations/src/index.ts
+++ packages/manager/modules/common-translations/src/index.ts
@@ -1,5 +1,6 @@
 export const NAMESPACE_PREFIX = '@ovh-ux/manager-common-translations';
 
 export const NAMESPACES = {
+  BYTES: `${NAMESPACE_PREFIX}/bytes`,
   CLIPBOARD: `${NAMESPACE_PREFIX}/clipboard`,
 };
