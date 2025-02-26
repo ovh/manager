4da5f2821e6185833a68e727f1d9c2b0acd4e3bf
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-02-26 09:58:19 +0100

    feat(ips): remove 2api calls
    ref: MANAGER-15789
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager/apps/ips/src/data/hooks/useServiceList.ts
+++ packages/manager/apps/ips/src/data/hooks/useServiceList.ts
@@ -1,7 +1,11 @@
 import { useQueries } from '@tanstack/react-query';
 import { getVrackList } from '../api/vrack';
 import { ServiceType } from '@/types';
-import { getServiceList } from '../api/get/serviceList';
+import {
+  getDedicatedCloudServiceList,
+  getDedicatedServerList,
+  getVpsList,
+} from '../api';
 
 export const ipParkingOptionValue = 'parking';
 
@@ -13,15 +17,15 @@ export const useServiceList = () => {
     queries: [
       {
         queryKey: ['dedicatedCloud'],
-        queryFn: () => getServiceList(ServiceType.dedicatedCloud),
+        queryFn: getDedicatedCloudServiceList,
       },
       {
         queryKey: ['server'],
-        queryFn: () => getServiceList(ServiceType.server),
+        queryFn: getDedicatedServerList,
       },
       {
         queryKey: ['vps'],
-        queryFn: () => getServiceList(ServiceType.vps),
+        queryFn: getVpsList,
       },
       {
         queryKey: ['vrack'],
@@ -36,33 +40,35 @@ export const useServiceList = () => {
         return ServiceType.ipParking;
       }
       if (
-        queries[0]?.data?.data?.results[0]?.services?.some(
+        queries[0]?.data?.data?.some(
           ({ serviceName }) => serviceName === serviceId,
         )
       ) {
         return ServiceType.dedicatedCloud;
       }
       if (
-        queries[1]?.data?.data?.results[0]?.services?.some(
+        queries[1]?.data?.data?.some(
           ({ serviceName }) => serviceName === serviceId,
         )
       ) {
         return ServiceType.server;
       }
       if (
-        queries[2]?.data?.data?.results[0]?.services?.some(
+        queries[2]?.data?.data?.some(
           ({ serviceName }) => serviceName === serviceId,
         )
       ) {
         return ServiceType.vps;
       }
-      return queries[3]?.data?.data?.includes(serviceId)
+      return queries[3]?.data?.data?.some(
+        ({ serviceName }) => serviceName === serviceId,
+      )
         ? ServiceType.vrack
         : ServiceType.unknown;
     },
-    dedicatedCloud: queries[0]?.data?.data?.results[0]?.services,
-    server: queries[1]?.data?.data?.results[0]?.services,
-    vps: queries[2]?.data?.data?.results[0]?.services,
+    dedicatedCloud: queries[0]?.data?.data,
+    server: queries[1]?.data?.data,
+    vps: queries[2]?.data?.data,
     vrack: queries[3]?.data?.data,
     isError: queries.some((query) => query.isError),
     error: queries.find((query) => query.error),
