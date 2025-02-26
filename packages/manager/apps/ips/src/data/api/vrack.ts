3f94fb0f8143bbb9dc49462363b5d65aea6f6fde
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-02-26 09:58:19 +0100

    feat(ips): remove 2api calls
    ref: MANAGER-15789
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager/apps/ips/src/data/api/vrack.ts
+++ packages/manager/apps/ips/src/data/api/vrack.ts
@@ -1,8 +1,33 @@
-import { ApiResponse, apiClient } from '@ovh-ux/manager-core-api';
+import {
+  ApiResponse,
+  IcebergFetchResultV6,
+  apiClient,
+  fetchIcebergV6,
+} from '@ovh-ux/manager-core-api';
+import { IamObject } from '@ovh-ux/manager-react-components';
 import { ServiceStatus } from '@/types';
+import { toServiceListItem } from '@/utils/toServiceListItem';
 
-export const getVrackList = (): Promise<ApiResponse<string[]>> =>
-  apiClient.v6.get('/vrack');
+type Vrack = {
+  name: string;
+  description: string;
+  iam: IamObject;
+};
+
+export const getVrackList = async (): Promise<IcebergFetchResultV6<{
+  serviceName: string;
+  displayName: string;
+}>> => {
+  const response = await fetchIcebergV6<Vrack>({
+    route: '/vrack',
+    pageSize: 1000,
+  });
+
+  return {
+    ...response,
+    data: response.data.map(toServiceListItem),
+  };
+};
 
 export type VrackServiceInfos = {
   contactAdmin: string;
