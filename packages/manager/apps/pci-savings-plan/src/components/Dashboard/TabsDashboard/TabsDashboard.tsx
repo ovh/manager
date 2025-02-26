253bf50fd77bd2896224cde97a5ed5be4aad1bf5
Eric <35898232+Eric-ciccotti@users.noreply.github.com>
2025-02-26 09:30:02 +0100

    feat(pci-savings-plan): tracking for dashboard (#15477)
    * feat(pci.savings-plan): outside plans consumption (#15444)
    
    ref: TAPC-2222
    
    Signed-off-by: Eric Ciccotti <eric.ciccotti.ext@corp.ovh.com>
    Co-authored-by: Eric Ciccotti <eric.ciccotti.ext@corp.ovh.com>
    
    * feat(pci.savings-plan): tracking
    
    ref: TAPC-2526
    Signed-off-by: Eric Ciccotti <eric.ciccotti.ext@corp.ovh.com>
    
    ---------
    
    Signed-off-by: Eric Ciccotti <eric.ciccotti.ext@corp.ovh.com>
    Co-authored-by: Eric Ciccotti <eric.ciccotti.ext@corp.ovh.com>

--- packages/manager/apps/pci-savings-plan/src/components/Dashboard/TabsDashboard/TabsDashboard.tsx
+++ packages/manager/apps/pci-savings-plan/src/components/Dashboard/TabsDashboard/TabsDashboard.tsx
@@ -1,9 +1,15 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useCallback } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { OdsTabs, OdsTab } from '@ovhcloud/ods-components/react';
 import { useTranslation } from 'react-i18next';
+import {
+  ButtonType,
+  PageLocation,
+  useOvhTracking,
+} from '@ovh-ux/manager-react-shell-client';
 
 const TabsDashboard: React.FC<{ projectId: string }> = ({ projectId }) => {
+  const { trackClick } = useOvhTracking();
   const navigate = useNavigate();
   const location = useLocation();
   const { t } = useTranslation('dashboard');
@@ -13,6 +19,16 @@ const TabsDashboard: React.FC<{ projectId: string }> = ({ projectId }) => {
   const isListing =
     location.pathname === `/pci/projects/${projectId}/savings-plan/listing`;
 
+  const handleNavigationClick = useCallback(() => {
+    navigate(`/pci/projects/${projectId}/savings-plan`);
+    trackClick({
+      location: PageLocation.page,
+      buttonType: ButtonType.tab,
+      actionType: 'navigation',
+      actions: ['dashboard'],
+    });
+  }, [navigate, projectId, trackClick]);
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <OdsTabs className="my-tabs mb-4" id="css-tabs">
@@ -20,7 +36,7 @@ const TabsDashboard: React.FC<{ projectId: string }> = ({ projectId }) => {
           className="my-tab"
           id="css-tab-1"
           isSelected={isDashboard}
-          onClick={() => navigate(`/pci/projects/${projectId}/savings-plan`)}
+          onClick={handleNavigationClick}
         >
           {t('dashboard_tabs')}
         </OdsTab>
