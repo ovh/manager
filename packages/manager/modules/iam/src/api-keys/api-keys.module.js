5086d7b8a7ed15a904b8670ff7b351f247671b9f
David Arsène <david.arsene.ext@ovhcloud.com>
2025-02-27 18:11:39 +0100

    feat(iam): add api key onboarding page
    resolves: MANAGER-16838
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/modules/iam/src/api-keys/api-keys.module.js
+++ packages/manager/modules/iam/src/api-keys/api-keys.module.js
@@ -1,11 +1,12 @@
 import component from './api-keys.component';
 import routing from './api-keys.routing';
-import _delete_ from './delete';
+import onboarding from './onboarding';
+import deletion from './delete';
 
 const moduleName = 'ovhManagerIAMApiKeys';
 
 angular
-  .module(moduleName, [_delete_])
+  .module(moduleName, [onboarding, deletion])
   .component(component.name, component)
   .config(routing)
   .run(/* @ngTranslationsInject:json ./translations */);
