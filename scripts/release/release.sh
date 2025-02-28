4667a6f013024d5d45733497222cd079eda29286
Hazem Belkhiria <hazem.belkhiria.ext@ovhcloud.com>
2025-02-28 09:43:07 +0100

    ci(lerna): test with lerna installed globally
    Signed-off-by: Hazem Belkhiria <hazem.belkhiria.ext@ovhcloud.com>

--- scripts/release/release.sh
+++ scripts/release/release.sh
@@ -29,15 +29,15 @@ version() {
 
   if "${DRY_RELEASE}"; then
     printf "%s\n" "Dry releasing"
-    node_modules/.bin/lerna version --conventional-commits --no-commit-hooks --no-git-tag-version --no-push --allow-branch="${GIT_BRANCH}" --yes
+    lerna version --conventional-commits --no-commit-hooks --no-git-tag-version --no-push --allow-branch="${GIT_BRANCH}" --yes
   else
     printf "%s\n" "Releasing"
-    node_modules/.bin/lerna version --conventional-commits --no-commit-hooks --no-git-tag-version --no-push  --yes
+    lerna version --conventional-commits --no-commit-hooks --no-git-tag-version --no-push  --yes
   fi
 }
 
 get_changed_packages() {
-  node_modules/.bin/lerna changed --all -p -l
+  lerna changed --all -p -l
 }
 
 get_release_name() {
@@ -58,7 +58,7 @@ get_release_name() {
 }
 
 create_release_note() (
-  node_modules/.bin/conventional-changelog -p angular -n scripts/release/changelog.js -k "$1" --commit-path "$1" -l "$2"
+  conventional-changelog -p angular -n scripts/release/changelog.js -k "$1" --commit-path "$1" -l "$2"
 )
 
 push_and_release() {
@@ -146,7 +146,7 @@ main() {
 "
   done <<< "$changed_packages"
 
-  #Remove package specific tags
+  #Remove package specific tags node_modules/.bin/
   clean_tags
 
   push_and_release "$next_tag"
