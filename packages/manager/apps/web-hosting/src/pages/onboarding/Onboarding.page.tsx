0c72aeca5298da8a539f7cfd938d0f1555bb3789
Tristan WAGNER <t.wagner66250@gmail.com>
2025-02-28 14:03:23 +0100

    feat(web-hosting): add onboarding page (#15716)
    ref: #MANAGER-16620
    
    Signed-off-by: Tristan WAGNER <t.wagner66250@gmail.com>

--- packages/manager/apps/web-hosting/src/pages/onboarding/Onboarding.page.tsx
+++ packages/manager/apps/web-hosting/src/pages/onboarding/Onboarding.page.tsx
@@ -1,11 +1,23 @@
-import React, { useMemo } from 'react';
+import React, { useContext, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Card, OnboardingLayout } from '@ovh-ux/manager-react-components';
+import {
+  ButtonType,
+  PageLocation,
+  ShellContext,
+  useOvhTracking,
+} from '@ovh-ux/manager-react-shell-client';
+import { OdsText } from '@ovhcloud/ods-components/react';
 import useGuides from '@/hooks/guides/useGuides';
-import onboardingImgSrc from './onboarding-img.png';
+import onboardingImgSrc from './onboarding.svg';
+import { GO_TO, ONBOARDING_ORDER_CTA } from '@/tracking.constants';
+import { ORDER_LINK } from './onboarding.constants';
 
 export default function Onboarding() {
   const { t } = useTranslation('onboarding');
+  const { trackClick } = useOvhTracking();
+  const context = useContext(ShellContext);
+  const { ovhSubsidiary } = context.environment.getUser();
   const guides = useGuides();
 
   const tiles = useMemo(
@@ -18,6 +30,7 @@ export default function Onboarding() {
           category: t(guides?.guideLink1?.category),
         },
         href: guides?.guideLink1?.href,
+        tracking: guides?.guideLink1?.tracking,
       },
       {
         id: 2,
@@ -27,35 +40,61 @@ export default function Onboarding() {
           category: t(guides?.guideLink2?.category),
         },
         href: guides?.guideLink2?.href,
+        tracking: guides?.guideLink1?.tracking,
       },
       {
         id: 3,
         texts: {
           title: t(guides?.guideLink3?.title),
           description: t(guides?.guideLink3?.description),
-          category: t(guides?.guideLink13.category),
+          category: t(guides?.guideLink3.category),
         },
         href: guides?.guideLink3?.href,
+        tracking: guides?.guideLink1?.tracking,
       },
     ],
     [guides],
   );
 
+  const orderLink = useMemo(
+    () => ORDER_LINK[ovhSubsidiary] || ORDER_LINK.DEFAULT,
+    [ovhSubsidiary],
+  );
+
   return (
-    <>
-      <OnboardingLayout
-        title={t('title')}
-        img={{ src: onboardingImgSrc }}
-        description={t('description')}
-        orderButtonLabel={t('orderButtonLabel')}
-        orderHref={t('orderButtonLink')}
-        moreInfoButtonLabel={t('moreInfoButtonLabel')}
-        moreInfoHref={t('moreInfoButtonLink')}
-      >
-        {tiles.map((tile) => (
-          <Card key={tile.id} href={tile.href} texts={tile.texts} />
-        ))}
-      </OnboardingLayout>
-    </>
+    <OnboardingLayout
+      title={t('title')}
+      img={{ src: onboardingImgSrc }}
+      description={
+        <OdsText className="text-center">{t('description')}</OdsText>
+      }
+      orderButtonLabel={t('order_btn')}
+      onOrderButtonClick={() => {
+        trackClick({
+          location: PageLocation.page,
+          buttonType: ButtonType.button,
+          actionType: 'navigation',
+          actions: [ONBOARDING_ORDER_CTA],
+        });
+
+        window.open(orderLink, '_blank', 'noopener');
+      }}
+    >
+      {tiles.map((tile) => (
+        <Card
+          key={tile.id}
+          href={tile.href}
+          texts={tile.texts}
+          onClick={() => {
+            trackClick({
+              location: PageLocation.tile,
+              buttonType: ButtonType.externalLink,
+              actionType: 'navigation',
+              actions: [GO_TO(tile.tracking)],
+            });
+          }}
+        />
+      ))}
+    </OnboardingLayout>
   );
 }
