7b70c505bc722ddfb35bb4c22bedabbd84aa2f43
Florian Renaut <florian.renaut@corp.ovh.com>
2025-02-28 11:08:20 +0100

    fix(pci-quota): tailwind config
    ref: DTCORE-2951
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/apps/pci-quota/tailwind.config.js
+++ packages/manager/apps/pci-quota/tailwind.config.js
@@ -4,6 +4,17 @@ import path from 'path';
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   ...config,
+  theme: {
+    ...config.theme,
+    // border radius vars have been renamed in ODS 18
+    borderRadius: {
+      ...config.borderRadius,
+      sm: 'var(--ods-border-radius-sm)',
+      DEFAULT: 'var(--ods-border-radius-sm)',
+      md: 'var(--ods-border-radius-md)',
+      lg: 'var(--ods-border-radius-md)',
+    },
+  },
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
     path.join(
