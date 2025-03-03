1aebbeab93f243adb51ff86523e8b017a1a6b78b
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2025-03-03 09:41:59 +0100

    fix(web.office): add visibility columns on listing and users datagrid
    ref: #MANAGER-16534
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/apps/web-office/src/pages/onboarding/index.tsx
+++ packages/manager/apps/web-office/src/pages/onboarding/index.tsx
@@ -4,7 +4,6 @@ import { Card, OnboardingLayout } from '@ovh-ux/manager-react-components';
 import { ShellContext } from '@ovh-ux/manager-react-shell-client';
 import { OdsText } from '@ovhcloud/ods-components/react';
 import { ODS_TEXT_PRESET } from '@ovhcloud/ods-components';
-import { Breadcrumb } from '@/components/Breadcrumb/Breadcrumb';
 import onboardingImgSrc from './onboarding-img.png';
 import { GUIDES_LIST } from '@/guides.constants';
 
@@ -14,58 +13,55 @@ export default function Onboarding() {
   const { ovhSubsidiary } = context.environment.getUser();
 
   return (
-    <>
-      <Breadcrumb />
-      <OnboardingLayout
-        title={t('web_office_onboarding_title')}
-        img={{
-          src: onboardingImgSrc,
+    <OnboardingLayout
+      title={t('web_office_onboarding_title')}
+      img={{
+        src: onboardingImgSrc,
+      }}
+      description={
+        <OdsText preset={ODS_TEXT_PRESET.paragraph} className="text-center">
+          {t('web_office_onboarding_description')}
+        </OdsText>
+      }
+      orderButtonLabel={t('web_office_onboarding_order')}
+      orderHref={
+        GUIDES_LIST.office_cta_order.url[ovhSubsidiary] ||
+        GUIDES_LIST.office_cta_order.url.DEFAULT
+      }
+    >
+      <Card
+        href={
+          (GUIDES_LIST.office_onboarding1_guides.url[ovhSubsidiary] ||
+            GUIDES_LIST.office_onboarding1_guides.url.DEFAULT) as string
+        }
+        texts={{
+          title: t('web_office_onboarding_guide1_title'),
+          description: t('web_office_onboarding_guide1_description'),
+          category: t('web_office_onboarding_tutorial'),
         }}
-        description={
-          <OdsText preset={ODS_TEXT_PRESET.paragraph} className="text-center">
-            {t('web_office_onboarding_description')}
-          </OdsText>
+      />
+      <Card
+        href={
+          (GUIDES_LIST.office_onboarding2_guides.url[ovhSubsidiary] ||
+            GUIDES_LIST.office_onboarding1_guides.url.DEFAULT) as string
         }
-        orderButtonLabel={t('web_office_onboarding_order')}
-        orderHref={
-          GUIDES_LIST.office_cta_order.url[ovhSubsidiary] ||
-          GUIDES_LIST.office_cta_order.url.DEFAULT
+        texts={{
+          title: t('web_office_onboarding_guide2_title'),
+          description: t('web_office_onboarding_guide2_description'),
+          category: t('web_office_onboarding_tutorial'),
+        }}
+      />
+      <Card
+        href={
+          (GUIDES_LIST.office_onboarding3_guides.url[ovhSubsidiary] ||
+            GUIDES_LIST.office_onboarding1_guides.url.DEFAULT) as string
         }
-      >
-        <Card
-          href={
-            (GUIDES_LIST.office_onboarding1_guides.url[ovhSubsidiary] ||
-              GUIDES_LIST.office_onboarding1_guides.url.DEFAULT) as string
-          }
-          texts={{
-            title: t('web_office_onboarding_guide1_title'),
-            description: t('web_office_onboarding_guide1_description'),
-            category: t('web_office_onboarding_tutorial'),
-          }}
-        />
-        <Card
-          href={
-            (GUIDES_LIST.office_onboarding2_guides.url[ovhSubsidiary] ||
-              GUIDES_LIST.office_onboarding1_guides.url.DEFAULT) as string
-          }
-          texts={{
-            title: t('web_office_onboarding_guide2_title'),
-            description: t('web_office_onboarding_guide2_description'),
-            category: t('web_office_onboarding_tutorial'),
-          }}
-        />
-        <Card
-          href={
-            (GUIDES_LIST.office_onboarding3_guides.url[ovhSubsidiary] ||
-              GUIDES_LIST.office_onboarding1_guides.url.DEFAULT) as string
-          }
-          texts={{
-            title: t('web_office_onboarding_guide3_title'),
-            description: t('web_office_onboarding_guide3_description'),
-            category: t('web_office_onboarding_tutorial'),
-          }}
-        />
-      </OnboardingLayout>
-    </>
+        texts={{
+          title: t('web_office_onboarding_guide3_title'),
+          description: t('web_office_onboarding_guide3_description'),
+          category: t('web_office_onboarding_tutorial'),
+        }}
+      />
+    </OnboardingLayout>
   );
 }
