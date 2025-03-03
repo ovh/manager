55a70fad45b5315e9282e2fd874734807b21d447
ahmed sefiani <ahmed.sefiani.ext@corp.ovh.com>
2025-03-03 17:37:37 +0100

    feat(dedicated): add customer survey banner
    ref: MANAGER-16933
    
    Signed-off-by: ahmed sefiani <ahmed.sefiani.ext@corp.ovh.com>

---
+++ packages/manager/apps/dedicated/client/app/components/dedicated-cloud/customer-survey-banner/customer-survey-banner.constants.js
@@ -0,0 +1,12 @@
+export const PCC_SURVEY_BASE_URL = 'https://s.elq.fr/ovhsat';
+
+export const PCC_SURVEY_KEY_URL = {
+  fr: 'ItF90t3',
+  en: 'h8AlPg3',
+  it: 'jMvrcxa',
+  pt: 'us6aCJU',
+  pl: 'VNtz4zY',
+  es: 'il08Y3P',
+  de: 'kCWmKNA',
+  default: 'h8AlPg3',
+};
