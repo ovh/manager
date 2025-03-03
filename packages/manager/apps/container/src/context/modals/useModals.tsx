a2e197d4402e924fea8811ec6cc11664975bc945
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2025-03-03 14:16:33 +0100

    feat(container): add robustness to useModals hook
    ref: #MANAGER-14722
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/apps/container/src/context/modals/useModals.tsx
+++ packages/manager/apps/container/src/context/modals/useModals.tsx
@@ -1,6 +1,10 @@
 import { useContext } from 'react';
 import ModalsContext from '@/context/modals/modals.context';
 
-const useModals = () => useContext(ModalsContext);
+const useModals = () => {
+  const context = useContext(ModalsContext);
+  if (!context) throw new Error('useModals must be inside ModalsProvider');
+  return context;
+};
 
 export default useModals;
