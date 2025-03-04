c9e1b5d6c48187ef92f68b04688e9ba85d0b40f1
Tristan WAGNER <t.wagner66250@gmail.com>
2025-03-04 11:45:21 +0100

    fix(billing): autorenew for zimbra slot (#15764)
    ref: #MANAGER-17194 INC0115237
    
    Signed-off-by: Tristan WAGNER <t.wagner66250@gmail.com>

--- packages/manager/modules/new-billing/src/autoRenew/actions/update/form/form.service.js
+++ packages/manager/modules/new-billing/src/autoRenew/actions/update/form/form.service.js
@@ -2,6 +2,7 @@ import get from 'lodash/get';
 import map from 'lodash/map';
 import values from 'lodash/values';
 import { RENEWAL_TYPES } from './form.constants';
+import { SERVICE_TYPES_USING_V6_SERVICES } from '../../../autorenew.constants';
 
 export default class {
   /* @ngInject */
@@ -11,6 +12,23 @@ export default class {
   }
 
   getAvailableRenewPeriods(service) {
+    if (SERVICE_TYPES_USING_V6_SERVICES.includes(service.serviceType)) {
+      return this.$http
+        .get(`/services/${service.id}/renewPeriodCapacities`)
+        .then(({ data }) => data)
+        .then((possibleRenewPeriod) => {
+          return (
+            possibleRenewPeriod?.map((period) => ({
+              period,
+              label: this.$translate.instant(
+                'billing_autorenew_service_update_service_period_value',
+                { month: period.match(/\d+/)?.[0] || 'to_be_defined' },
+              ),
+            })) || []
+          );
+        });
+    }
+
     return this.$http
       .get(`${get(service, 'route.url')}/serviceInfos`)
       .then(({ data }) => get(data, 'possibleRenewPeriod'))
