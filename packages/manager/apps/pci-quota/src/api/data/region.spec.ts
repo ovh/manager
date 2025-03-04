7b148904c5c032b23b31fe8e2988d511aa16517b
Florian Renaut <florian.renaut@corp.ovh.com>
2025-03-04 17:21:03 +0100

    fix(pci-quota): remove getAvailableRegions iceberg cache
    ref: DTCORE-3175
    Signed-off-by: Florian Renaut <florian.renaut@corp.ovh.com>

--- packages/manager/apps/pci-quota/src/api/data/region.spec.ts
+++ packages/manager/apps/pci-quota/src/api/data/region.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi } from 'vitest';
-import { v6 } from '@ovh-ux/manager-core-api';
+import { fetchIcebergV6 } from '@ovh-ux/manager-core-api';
 import { getAvailableRegions } from './region';
 
 describe('Region API', () => {
@@ -9,16 +9,19 @@ describe('Region API', () => {
       const projectId = 'test-project';
       const mockData = [{ name: 'region1' }, { name: 'region2' }];
       const url = `/cloud/project/${projectId}/regionAvailable`;
-      vi.mocked(v6.get).mockResolvedValueOnce({ data: mockData });
+      vi.mocked(fetchIcebergV6).mockResolvedValueOnce({
+        data: mockData,
+        totalCount: 1,
+        status: 200,
+      });
 
       // Act
       const result = await getAvailableRegions(projectId);
 
       // Assert
-      expect(v6.get).toHaveBeenCalledWith(url, {
-        headers: {
-          'X-Pagination-Mode': 'CachedObjectList-Pages',
-        },
+      expect(fetchIcebergV6).toHaveBeenCalledWith({
+        route: url,
+        disableCache: true,
       });
       expect(result).toEqual(mockData);
     });
@@ -28,16 +31,15 @@ describe('Region API', () => {
       const projectId = 'test-project';
       const url = `/cloud/project/${projectId}/regionAvailable`;
       const errorMessage = 'Network Error';
-      vi.mocked(v6.get).mockRejectedValueOnce(new Error(errorMessage));
+      vi.mocked(fetchIcebergV6).mockRejectedValueOnce(new Error(errorMessage));
 
       // Act & Assert
       await expect(getAvailableRegions(projectId)).rejects.toThrow(
         errorMessage,
       );
-      expect(v6.get).toHaveBeenCalledWith(url, {
-        headers: {
-          'X-Pagination-Mode': 'CachedObjectList-Pages',
-        },
+      expect(fetchIcebergV6).toHaveBeenCalledWith({
+        route: url,
+        disableCache: true,
       });
     });
   });
--- packages/manager/apps/pci-quota/src/api/data/region.ts
+++ packages/manager/apps/pci-quota/src/api/data/region.ts
@@ -1,15 +1,14 @@
-import { v6 } from '@ovh-ux/manager-core-api';
+import { fetchIcebergV6 } from '@ovh-ux/manager-core-api';
 import { TRegion } from '@ovh-ux/manager-pci-common';
 
 export const getAvailableRegions = async (
   projectId: string,
+  disableCache = true,
 ): Promise<TRegion[]> => {
   const url = `/cloud/project/${projectId}/regionAvailable`;
-  const { data } = await v6.get<TRegion[]>(url, {
-    headers: {
-      'X-Pagination-Mode': 'CachedObjectList-Pages',
-    },
+  const { data } = await fetchIcebergV6<TRegion>({
+    route: url,
+    disableCache,
   });
-
   return data;
 };
