21144f2db1eae8ea28103e2100eefc01ad9d18ee
ahmed sefiani <ahmed.sefiani.ext@corp.ovh.com>
2025-03-04 16:05:59 +0100

    fix(dedicated): display banner customer survey fix code review
    ref: MANAGER-16933
    
    Signed-off-by: ahmed sefiani <ahmed.sefiani.ext@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/components/dedicated-cloud/customer-survey-banner/index.js
+++ packages/manager/apps/dedicated/client/app/components/dedicated-cloud/customer-survey-banner/index.js
@@ -1,10 +1,10 @@
-import component from './customer-survey-banner.component';
+import customerSurveyBannerComponent from './customer-survey-banner.component';
 
 const moduleName = 'ovhManagerPccDashboardCustomerSurveyBanner';
 
 angular
   .module(moduleName, ['oui', 'pascalprecht.translate'])
-  .component(component.name, component)
+  .component(customerSurveyBannerComponent.name, customerSurveyBannerComponent)
   .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
