fbdc15f1ae2ab974566bc60928c5abac4d2e8118
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2025-03-05 15:49:59 +0100

    fix(pci-cold-archive): fix design issues and code review returns
    ref: DTCORE-3162
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/apps/pci-cold-archive/src/constants.ts
+++ packages/manager/apps/pci-cold-archive/src/constants.ts
@@ -59,127 +59,148 @@ export const OBJECT_CONTAINER_USER_ROLES = [
   OBJECT_CONTAINER_USER_ROLE_DENY,
 ];
 
-export const GUIDE_MENU_ITEMS = [
-  {
-    id: 'storage-backup',
-    links: {
-      DEFAULT: 'https://docs.ovh.com/us/en/storage/',
-      FR: 'https://docs.ovh.com/fr/storage/',
-      MA: 'https://docs.ovh.com/fr/storage/',
-      TN: 'https://docs.ovh.com/fr/storage/',
-      SN: 'https://docs.ovh.com/fr/storage/',
-      QC: 'https://docs.ovh.com/ca/fr/storage/',
-      GB: 'https://docs.ovh.com/gb/en/storage/',
-      IE: 'https://docs.ovh.com/ie/en/storage/',
-      WE: 'https://docs.ovh.com/us/en/storage/',
-      CA: 'https://docs.ovh.com/ca/en/storage/',
-      AU: 'https://docs.ovh.com/au/en/storage/',
-      SG: 'https://docs.ovh.com/sg/en/storage/',
-      ASIA: 'https://docs.ovh.com/asia/en/storage/',
-      ES: 'https://docs.ovh.com/es/storage/',
-      WS: 'https://docs.ovh.com/us/es/storage/',
-      PT: 'https://docs.ovh.com/pt/storage/',
-      IT: 'https://docs.ovh.com/it/storage/',
-      PL: 'https://docs.ovh.com/pl/storage/',
-      DE: 'https://docs.ovh.com/de/storage/',
-      NL: 'https://docs.ovh.com/gb/en/storage/',
-    },
-  },
-  {
-    id: 'overview',
-    links: {
-      DEFAULT:
-        'https://docs.ovh.com/us/en/storage/object-storage/cold-archive/overview/',
-      FR:
-        'https://docs.ovh.com/fr/storage/object-storage/cold-archive/overview/',
-      MA:
-        'https://docs.ovh.com/fr/storage/object-storage/cold-archive/overview/',
-      TN:
-        'https://docs.ovh.com/fr/storage/object-storage/cold-archive/overview/',
-      SN:
-        'https://docs.ovh.com/fr/storage/object-storage/cold-archive/overview/',
-      QC:
-        'https://docs.ovh.com/ca/fr/storage/object-storage/cold-archive/overview/',
-      GB:
-        'https://docs.ovh.com/gb/en/storage/object-storage/cold-archive/overview/',
-      IE:
-        'https://docs.ovh.com/ie/en/storage/object-storage/cold-archive/overview/',
-      WE:
-        'https://docs.ovh.com/us/en/storage/object-storage/cold-archive/overview/',
-      CA:
-        'https://docs.ovh.com/ca/en/storage/object-storage/cold-archive/overview/',
-      AU:
-        'https://docs.ovh.com/au/en/storage/object-storage/cold-archive/overview/',
-      SG:
-        'https://docs.ovh.com/sg/en/storage/object-storage/cold-archive/overview/',
-      ASIA:
-        'https://docs.ovh.com/asia/en/storage/object-storage/cold-archive/overview/',
-      ES:
-        'https://docs.ovh.com/es/storage/object-storage/cold-archive/overview/',
-      WS:
-        'https://docs.ovh.com/us/es/storage/object-storage/cold-archive/overview/',
-      PT:
-        'https://docs.ovh.com/pt/storage/object-storage/cold-archive/overview/',
-      IT:
-        'https://docs.ovh.com/it/storage/object-storage/cold-archive/overview/',
-      PL:
-        'https://docs.ovh.com/pl/storage/object-storage/cold-archive/overview/',
-      DE:
-        'https://docs.ovh.com/de/storage/object-storage/cold-archive/overview/',
-      NL:
-        'https://docs.ovh.com/gb/en/storage/object-storage/cold-archive/overview/',
-    },
-  },
-  {
-    id: 'getting-started-with-cold-archive',
-    links: {
-      DEFAULT:
-        'https://docs.ovh.com/us/en/storage/object-storage/cold-archive/getting-started/',
-      FR:
-        'https://docs.ovh.com/fr/storage/object-storage/cold-archive/getting-started/',
-      MA:
-        'https://docs.ovh.com/fr/storage/object-storage/cold-archive/getting-started/',
-      TN:
-        'https://docs.ovh.com/fr/storage/object-storage/cold-archive/getting-started/',
-      SN:
-        'https://docs.ovh.com/fr/storage/object-storage/cold-archive/getting-started/',
-      QC:
-        'https://docs.ovh.com/ca/fr/storage/object-storage/cold-archive/getting-started/',
-      GB:
-        'https://docs.ovh.com/gb/en/storage/object-storage/cold-archive/getting-started/',
-      IE:
-        'https://docs.ovh.com/ie/en/storage/object-storage/cold-archive/getting-started/',
-      WE:
-        'https://docs.ovh.com/us/en/storage/object-storage/cold-archive/getting-started/',
-      CA:
-        'https://docs.ovh.com/ca/en/storage/object-storage/cold-archive/getting-started/',
-      AU:
-        'https://docs.ovh.com/au/en/storage/object-storage/cold-archive/getting-started/',
-      SG:
-        'https://docs.ovh.com/sg/en/storage/object-storage/cold-archive/getting-started/',
-      ASIA:
-        'https://docs.ovh.com/asia/en/storage/object-storage/cold-archive/getting-started/',
-      ES:
-        'https://docs.ovh.com/es/storage/object-storage/cold-archive/getting-started/',
-      WS:
-        'https://docs.ovh.com/us/es/storage/object-storage/cold-archive/getting-started/',
-      PT:
-        'https://docs.ovh.com/pt/storage/object-storage/cold-archive/getting-started/',
-      IT:
-        'https://docs.ovh.com/it/storage/object-storage/cold-archive/getting-started/',
-      PL:
-        'https://docs.ovh.com/pl/storage/object-storage/cold-archive/getting-started/',
-      DE:
-        'https://docs.ovh.com/de/storage/object-storage/cold-archive/getting-started/',
-      NL:
-        'https://docs.ovh.com/gb/en/storage/object-storage/cold-archive/getting-started/',
-    },
-  },
-  {
-    id: 'manage-your-data',
-    links: MANAGE_ARCHIVE_DOC_LINK,
-  },
-];
+export const STORAGE_BACKUP_LINKS = {
+  DEFAULT: 'https://docs.ovh.com/us/en/storage/',
+  FR: 'https://docs.ovh.com/fr/storage/',
+  MA: 'https://docs.ovh.com/fr/storage/',
+  TN: 'https://docs.ovh.com/fr/storage/',
+  SN: 'https://docs.ovh.com/fr/storage/',
+  QC: 'https://docs.ovh.com/ca/fr/storage/',
+  GB: 'https://docs.ovh.com/gb/en/storage/',
+  IE: 'https://docs.ovh.com/ie/en/storage/',
+  WE: 'https://docs.ovh.com/us/en/storage/',
+  CA: 'https://docs.ovh.com/ca/en/storage/',
+  AU: 'https://docs.ovh.com/au/en/storage/',
+  SG: 'https://docs.ovh.com/sg/en/storage/',
+  ASIA: 'https://docs.ovh.com/asia/en/storage/',
+  ES: 'https://docs.ovh.com/es/storage/',
+  WS: 'https://docs.ovh.com/us/es/storage/',
+  PT: 'https://docs.ovh.com/pt/storage/',
+  IT: 'https://docs.ovh.com/it/storage/',
+  PL: 'https://docs.ovh.com/pl/storage/',
+  DE: 'https://docs.ovh.com/de/storage/',
+  NL: 'https://docs.ovh.com/gb/en/storage/',
+};
+
+export const COLD_ARCHIVE_OVERVIEW_LINKS = {
+  DEFAULT:
+    'https://docs.ovh.com/us/en/storage/object-storage/cold-archive/overview/',
+  FR: 'https://docs.ovh.com/fr/storage/object-storage/cold-archive/overview/',
+  MA: 'https://docs.ovh.com/fr/storage/object-storage/cold-archive/overview/',
+  TN: 'https://docs.ovh.com/fr/storage/object-storage/cold-archive/overview/',
+  SN: 'https://docs.ovh.com/fr/storage/object-storage/cold-archive/overview/',
+  QC:
+    'https://docs.ovh.com/ca/fr/storage/object-storage/cold-archive/overview/',
+  GB:
+    'https://docs.ovh.com/gb/en/storage/object-storage/cold-archive/overview/',
+  IE:
+    'https://docs.ovh.com/ie/en/storage/object-storage/cold-archive/overview/',
+  WE:
+    'https://docs.ovh.com/us/en/storage/object-storage/cold-archive/overview/',
+  CA:
+    'https://docs.ovh.com/ca/en/storage/object-storage/cold-archive/overview/',
+  AU:
+    'https://docs.ovh.com/au/en/storage/object-storage/cold-archive/overview/',
+  SG:
+    'https://docs.ovh.com/sg/en/storage/object-storage/cold-archive/overview/',
+  ASIA:
+    'https://docs.ovh.com/asia/en/storage/object-storage/cold-archive/overview/',
+  ES: 'https://docs.ovh.com/es/storage/object-storage/cold-archive/overview/',
+  WS:
+    'https://docs.ovh.com/us/es/storage/object-storage/cold-archive/overview/',
+  PT: 'https://docs.ovh.com/pt/storage/object-storage/cold-archive/overview/',
+  IT: 'https://docs.ovh.com/it/storage/object-storage/cold-archive/overview/',
+  PL: 'https://docs.ovh.com/pl/storage/object-storage/cold-archive/overview/',
+  DE: 'https://docs.ovh.com/de/storage/object-storage/cold-archive/overview/',
+  NL:
+    'https://docs.ovh.com/gb/en/storage/object-storage/cold-archive/overview/',
+};
+
+export const GETTING_STARTED_WITH_COLD_ARCHIVE_LINKS = {
+  DEFAULT:
+    'https://docs.ovh.com/us/en/storage/object-storage/cold-archive/getting-started/',
+  FR:
+    'https://docs.ovh.com/fr/storage/object-storage/cold-archive/getting-started/',
+  MA:
+    'https://docs.ovh.com/fr/storage/object-storage/cold-archive/getting-started/',
+  TN:
+    'https://docs.ovh.com/fr/storage/object-storage/cold-archive/getting-started/',
+  SN:
+    'https://docs.ovh.com/fr/storage/object-storage/cold-archive/getting-started/',
+  QC:
+    'https://docs.ovh.com/ca/fr/storage/object-storage/cold-archive/getting-started/',
+  GB:
+    'https://docs.ovh.com/gb/en/storage/object-storage/cold-archive/getting-started/',
+  IE:
+    'https://docs.ovh.com/ie/en/storage/object-storage/cold-archive/getting-started/',
+  WE:
+    'https://docs.ovh.com/us/en/storage/object-storage/cold-archive/getting-started/',
+  CA:
+    'https://docs.ovh.com/ca/en/storage/object-storage/cold-archive/getting-started/',
+  AU:
+    'https://docs.ovh.com/au/en/storage/object-storage/cold-archive/getting-started/',
+  SG:
+    'https://docs.ovh.com/sg/en/storage/object-storage/cold-archive/getting-started/',
+  ASIA:
+    'https://docs.ovh.com/asia/en/storage/object-storage/cold-archive/getting-started/',
+  ES:
+    'https://docs.ovh.com/es/storage/object-storage/cold-archive/getting-started/',
+  WS:
+    'https://docs.ovh.com/us/es/storage/object-storage/cold-archive/getting-started/',
+  PT:
+    'https://docs.ovh.com/pt/storage/object-storage/cold-archive/getting-started/',
+  IT:
+    'https://docs.ovh.com/it/storage/object-storage/cold-archive/getting-started/',
+  PL:
+    'https://docs.ovh.com/pl/storage/object-storage/cold-archive/getting-started/',
+  DE:
+    'https://docs.ovh.com/de/storage/object-storage/cold-archive/getting-started/',
+  NL:
+    'https://docs.ovh.com/gb/en/storage/object-storage/cold-archive/getting-started/',
+};
+
+export const MANAGE_YOUR_DATA_LINKS = {
+  DEFAULT:
+    'https://docs.ovh.com/us/en/storage/object-storage/s3/getting-started-with-object-storage/',
+  FR:
+    'https://docs.ovh.com/fr/storage/object-storage/s3/getting-started-with-object-storage/',
+  MA:
+    'https://docs.ovh.com/fr/storage/object-storage/s3/getting-started-with-object-storage/',
+  TN:
+    'https://docs.ovh.com/fr/storage/object-storage/s3/getting-started-with-object-storage/',
+  SN:
+    'https://docs.ovh.com/fr/storage/object-storage/s3/getting-started-with-object-storage/',
+  QC:
+    'https://docs.ovh.com/ca/fr/storage/object-storage/s3/getting-started-with-object-storage/',
+  GB:
+    'https://docs.ovh.com/gb/en/storage/object-storage/s3/getting-started-with-object-storage/',
+  IE:
+    'https://docs.ovh.com/ie/en/storage/object-storage/s3/getting-started-with-object-storage/',
+  WE:
+    'https://docs.ovh.com/us/en/storage/object-storage/s3/getting-started-with-object-storage/',
+  CA:
+    'https://docs.ovh.com/ca/en/storage/object-storage/s3/getting-started-with-object-storage/',
+  AU:
+    'https://docs.ovh.com/au/en/storage/object-storage/s3/getting-started-with-object-storage/',
+  SG:
+    'https://docs.ovh.com/sg/en/storage/object-storage/s3/getting-started-with-object-storage/',
+  ASIA:
+    'https://docs.ovh.com/asia/en/storage/object-storage/s3/getting-started-with-object-storage/',
+  ES:
+    'https://docs.ovh.com/es/storage/object-storage/s3/getting-started-with-object-storage/',
+  WS:
+    'https://docs.ovh.com/us/es/storage/object-storage/s3/getting-started-with-object-storage/',
+  PT:
+    'https://docs.ovh.com/pt/storage/object-storage/s3/getting-started-with-object-storage/',
+  IT:
+    'https://docs.ovh.com/it/storage/object-storage/s3/getting-started-with-object-storage/',
+  PL:
+    'https://docs.ovh.com/pl/storage/object-storage/s3/getting-started-with-object-storage/',
+  DE:
+    'https://docs.ovh.com/de/storage/object-storage/s3/getting-started-with-object-storage/',
+  NL:
+    'https://docs.ovh.com/gb/en/storage/object-storage/s3/getting-started-with-object-storage/',
+};
 
 export const ONBOARDING_DOC_LINKS = [
   {
