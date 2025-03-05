f6b375a31906ba307d72a1006051f06270711b76
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-03-05 14:31:44 +0100

    feat(iam): add policies pages
    ref: MANAGER-16217
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

---
+++ packages/manager/modules/iam/src/policies/resourceGroups/delete/delete.routing.js
@@ -0,0 +1,44 @@
+import { ENTITY, TAG } from '../../../iam.constants';
+
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state('iam.policies.resourceGroups.delete', {
+    url: '/delete/{resourceGroup:uuid}',
+    component: 'iamDeleteEntity',
+    resolve: {
+      breadcrumb: () => null,
+
+      /**
+       * A polymorphic DTO required by the deleteEntity component
+       * @returns {{
+       *   data: Object,
+       *   type: string
+       * }|null}
+       */
+      entity: /* @ngInject */ (resourceGroup) => {
+        if (resourceGroup) {
+          return { data: resourceGroup, type: ENTITY.RESOURCE_GROUP };
+        }
+        return null;
+      },
+
+      /**
+       * The resourceGroup parameter based on the resourceGroup's id
+       * @returns {Object|null}
+       */
+      resourceGroup: /* @ngInject */ ($transition$, IAMService) => {
+        const { resourceGroup: uuid } = $transition$.params();
+        return uuid ? IAMService.getResourceGroup(uuid) : null;
+      },
+
+      /**
+       * Whether the entity requires a statement
+       * @returns {boolean}
+       */
+      statement: /* @ngInject */ (entity) =>
+        entity.type === ENTITY.RESOURCE_GROUP,
+    },
+    atInternet: {
+      rename: TAG.DELETE_RESOURCE_GROUP,
+    },
+  });
+};
