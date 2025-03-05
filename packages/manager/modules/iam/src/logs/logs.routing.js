67b5f21e17540d6bc7776e349f1c74ac0dc22ac5
David Arsène <david.arsene.ext@ovhcloud.com>
2025-03-05 12:06:18 +0100

    feat(iam): add iam audit logs
    resolves: #MANAGER-16783
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

---
+++ packages/manager/modules/iam/src/logs/logs.routing.js
@@ -0,0 +1,42 @@
+import { FEATURE_LOGS, IAM_LOGS_TRACKING_HITS } from './logs.constants';
+import { name } from './logs.component';
+
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state('iam.logs', {
+    url: '/logs',
+    atInternet: {
+      rename: IAM_LOGS_TRACKING_HITS.LOGS_PAGE,
+    },
+    redirectTo: (transition) =>
+      transition
+        .injector()
+        .getAsync('features')
+        .then((featureAvailability) => ({
+          state: featureAvailability.isFeatureAvailable(FEATURE_LOGS.ROOT)
+            ? 'iam.logs.audit'
+            : 'iam',
+        })),
+    component: name,
+    resolve: {
+      breadcrumb: /* @ngInject */ ($translate) =>
+        $translate.instant('iam_logs_label'),
+      trackClick: /* @ngInject */ (atInternet) => (hit) => {
+        atInternet.trackClick({
+          name: hit,
+          type: 'action',
+        });
+      },
+      features: /* @ngInject */ (ovhFeatureFlipping) =>
+        ovhFeatureFlipping.checkFeatureAvailability(
+          Object.values(FEATURE_LOGS).join(','),
+        ),
+      auditLogsAvailability: /* @ngInject */ (features) =>
+        features.isFeatureAvailable(FEATURE_LOGS.AUDIT),
+      activityLogsAvailability: /* @ngInject */ (features) =>
+        features.isFeatureAvailable(FEATURE_LOGS.ACTIVITY),
+      accessPolicyLogsAvailability: /* @ngInject */ (features) =>
+        features.isFeatureAvailable(FEATURE_LOGS.ACCESS_POLICY),
+      logKinds: /* @ngInject */ (logsService) => logsService.getLogKinds(),
+    },
+  });
+};
