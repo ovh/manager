34231d91a62c04166627f93377c0eaeb5e2c568e
Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
2025-03-05 18:00:49 +0100

    feat(container): move initShell into ApplicationProvider
    ref: MANAGER-15632
    
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>

--- packages/manager/apps/container/src/context/ApplicationProvider.tsx
+++ packages/manager/apps/container/src/context/ApplicationProvider.tsx
@@ -1,28 +1,39 @@
-import React from 'react';
-
-import { Environment } from '@ovh-ux/manager-config';
-import { Shell } from '@ovh-ux/shell';
+import React, { useContext, useEffect, useState } from 'react';
+import { initShell, Shell } from '@ovh-ux/shell';
 
 import ApplicationContext from './application.context';
 import { HeaderProvider } from './header';
+import { setupDevApplication } from '@/core/dev';
 
 type Props = {
   children: JSX.Element;
-  environment: Environment;
-  shell: Shell;
 };
 
 export const ApplicationProvider = ({
   children = null,
-  environment = {} as Environment,
-  shell = {} as Shell,
 }: Props): JSX.Element => {
+  const [shell, setShell] = useState<Shell | null>(null);
+
+  useEffect(() => {
+    initShell().then(setShell);
+  }, []);
+
+  let applicationContext = useContext(ApplicationContext);
+
+  if (!shell) {
+    return null;
+  }
+
+  setupDevApplication(shell);
+
+  applicationContext = {
+    environment: shell.getPlugin('environment').getEnvironment(),
+    shell,
+  };
+
   return (
     <ApplicationContext.Provider
-      value={{
-        environment,
-        shell,
-      }}
+      value={applicationContext}
     >
       <HeaderProvider>{children}</HeaderProvider>
     </ApplicationContext.Provider>
