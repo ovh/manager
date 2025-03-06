49e9c875b665407aadfd909009ded0809524566c
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-03-06 14:08:39 +0100

    feat(dedicated-cloud): add page to add nsx for a pcc
    ref: #MANAGER-15407
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/components/dedicated-cloud/datacenter/dashboard/manage-nsx-edges/dedicatedCloud-datacenter-manage-nsx.controller.js
+++ packages/manager/apps/dedicated/client/app/components/dedicated-cloud/datacenter/dashboard/manage-nsx-edges/dedicatedCloud-datacenter-manage-nsx.controller.js
@@ -1,3 +1,10 @@
+import {
+  NSXT_EDGE_CATALOG,
+  DATACENTER_NETWORK_SITE_WEB_LINK,
+  NSXT_EDGE_CORE_PLAN_CODE,
+  NSXT_EDGE_PRICING_MODE,
+} from '../../../../../dedicatedCloud/datacenter/dedicatedCloud-datacenter.constants.js';
+
 import { TRACKING_SUFFIX } from './dedicatedCloud-datacenter-manage-nsx.constants';
 import {
   EDGES_SIZES,
@@ -6,21 +13,38 @@ import {
 
 export default class {
   /* @ngInject */
-  constructor(DedicatedCloud, $translate, coreURLBuilder) {
+  constructor(
+    coreConfig,
+    DedicatedCloud,
+    ovhManagerPccDatacenterService,
+    $translate,
+    coreURLBuilder,
+  ) {
+    this.coreConfig = coreConfig;
     this.DedicatedCloud = DedicatedCloud;
     this.$translate = $translate;
     this.coreURLBuilder = coreURLBuilder;
     this.NSX_RESOURCES = NSX_RESOURCES;
     this.TRACKING_SUFFIX = TRACKING_SUFFIX;
+    this.ovhManagerPccDatacenterService = ovhManagerPccDatacenterService;
+    this.vcpuTextPrice = '-';
+    this.userLanguage = coreConfig.getUserLocale().replace('_', '-');
   }
 
   $onInit() {
+    const { ovhSubsidiary } = this.coreConfig.getUser();
+    this.fetchVcpuPrice(ovhSubsidiary);
+
     this.loading = false;
     this.nsxSizes = Object.keys(EDGES_SIZES);
     this.selectedNsxLevel = null;
     this.hasScalingCapabilities =
       this.nsxtEdgesScalingCapabilities.length ===
       Object.keys(EDGES_SIZES).length;
+
+    this.guideUrl =
+      DATACENTER_NETWORK_SITE_WEB_LINK[ovhSubsidiary] ||
+      DATACENTER_NETWORK_SITE_WEB_LINK.GB;
   }
 
   isSizeAvailable(size) {
@@ -52,6 +76,30 @@ export default class {
       })
       .finally(() => {
         this.loading = false;
+      });
+  }
+
+  setVcpuTextPrice(price, currency) {
+    this.vcpuTextPrice = new Intl.NumberFormat(this.userLanguage, {
+      style: 'currency',
+      currency,
+    }).format(price / 1e8);
+  }
+
+  fetchVcpuPrice(ovhSubsidiary) {
+    this.ovhManagerPccDatacenterService
+      .getOrderCatalog(NSXT_EDGE_CATALOG, ovhSubsidiary)
+      .then((data) => {
+        const { price } = data.addons
+          .find((addon) => addon.planCode === NSXT_EDGE_CORE_PLAN_CODE)
+          .pricings.find(
+            (pricing) =>
+              pricing.mode === NSXT_EDGE_PRICING_MODE &&
+              pricing.description === 'extra edges',
+          );
+        const currency = data.locale.currencyCode;
+
+        this.setVcpuTextPrice(price, currency);
       });
   }
 }
