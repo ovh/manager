15e8a9db9a68fa0a16ea2335c03b43fc1cc8b1f3
Eric Ciccotti <eric.ciccotti.ext@corp.ovh.com>
2025-03-06 16:24:19 +0100

    feat(pci.kubernetes): wip
    ref: TAPC-2768
    Signed-off-by: Eric Ciccotti <eric.ciccotti.ext@corp.ovh.com>

--- packages/manager/apps/pci-kubernetes/src/components/region-selector/KubeRegionSelector.component.tsx
+++ packages/manager/apps/pci-kubernetes/src/components/region-selector/KubeRegionSelector.component.tsx
@@ -1,15 +1,23 @@
 import {
+  PCICommonContext,
   RegionSelector,
   RegionSelectorProps,
+  TRegion,
+  usePCICommonContextFactory,
   useProductAvailability,
+  useProjectRegions,
 } from '@ovh-ux/manager-pci-common';
 import { OsdsSpinner } from '@ovhcloud/ods-components/react';
 import { ODS_SPINNER_SIZE } from '@ovhcloud/ods-components';
+import { useMemo } from 'react';
 
 export interface KubeRegionSelectorProps {
   projectId: string;
   onSelectRegion: RegionSelectorProps['onSelectRegion'];
 }
+
+const isRegionWith3AZ = (regions: TRegion[]) =>
+  regions.some((region) => region.type === 'region-3-az');
 
 export function KubeRegionSelector({
   projectId,
@@ -18,23 +26,33 @@ export function KubeRegionSelector({
   const { data: availability, isPending } = useProductAvailability(projectId, {
     product: 'kubernetes',
   });
+
+  const { data: regions } = useProjectRegions(projectId);
+
+  const has3AZ = useMemo(() => (regions ? isRegionWith3AZ(regions) : false), [
+    regions,
+  ]);
+
+  const pciCommonProperties = usePCICommonContextFactory({ has3AZ });
+
   if (isPending) {
     return <OsdsSpinner inline size={ODS_SPINNER_SIZE.md} />;
   }
   return (
-    <RegionSelector
-      projectId={projectId}
-      onSelectRegion={onSelectRegion}
-      regionFilter={(region) => {
-        const product = availability?.products.find(
-          ({ name }) => name === 'kubernetes',
-        );
-        return (
-          region.isMacro ||
-          product?.regions?.some(({ name }) => name === region.name)
-        );
-      }}
-      compactMode
-    />
+    <PCICommonContext.Provider value={pciCommonProperties}>
+      <RegionSelector
+        projectId={projectId}
+        onSelectRegion={onSelectRegion}
+        regionFilter={(region) => {
+          const product = availability?.products.find(
+            ({ name }) => name === 'kubernetes',
+          );
+          return (
+            region.isMacro ||
+            product?.regions?.some(({ name }) => name === region.name)
+          );
+        }}
+      />
+    </PCICommonContext.Provider>
   );
 }
