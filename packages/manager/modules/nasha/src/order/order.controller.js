9a1fe3859820fe353883b7318a259d735774d045
Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
2025-03-07 14:11:23 +0100

    feat(dedicated): add nasha federated module with runtime package
    Added nasha federated module to nasha order page using the runtime package for federated modules
    
    ref: #MANAGER-16956
    
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>

--- packages/manager/modules/nasha/src/order/order.controller.js
+++ packages/manager/modules/nasha/src/order/order.controller.js
@@ -1,222 +1,59 @@
-import JSURL from 'jsurl';
-import { uniq } from 'lodash';
-import { CatalogPricing } from '@ovh-ux/manager-models';
-// import setupNasHa from 'ConfigoNasHa/ConfigoNasHa';
-
 import {
-  FORMAT_DURATION_TRACKING_ORDER,
-  FORMAT_UNIT_CAPACITY_TRACKING_ORDER,
-  PRODUCT_ID,
+  // FORMAT_DURATION_TRACKING_ORDER,
+  // FORMAT_UNIT_CAPACITY_TRACKING_ORDER,
   PREFIX_TRACKING_ORDER,
-  PREFIX_TRACKING_ORDER_NEXT_STEP,
 } from './order.constants';
 
 export default class NashaOrderController {
   /* @ngInject */
-  constructor(
-    $translate,
-    $window,
-    coreConfig,
-    BillingService,
-    RedirectionService,
-  ) {
-    this.$translate = $translate;
-    this.$window = $window;
-    this.BillingService = BillingService;
+  constructor(coreConfig, coreURLBuilder) {
+    this.nashaRoot = coreURLBuilder.buildURL('dedicated', '#/nasha');
     this.user = coreConfig.getUser();
     this.userLocale = coreConfig.getUserLocale();
-    this.expressOrderUrl = RedirectionService.getURL('expressOrder');
-
-    this.diskType = {
-      value: null,
-      displayed: false,
-      list: [],
-    };
-    this.capacity = {
-      value: null,
-      displayed: false,
-      plans: [],
-    };
-    this.datacenter = {
-      value: null,
-      displayed: false,
-      list: [],
-    };
-    this.commitment = {
-      value: null,
-      displayed: false,
-      price: null,
-      pricings: [],
-    };
-    this.payment = {
-      value: null,
-      displayed: false,
-      modes: [],
-    };
-
-    this.PREFIX_TRACKING_ORDER_NEXT_STEP = PREFIX_TRACKING_ORDER_NEXT_STEP;
   }
 
   $onInit() {
     const element = document.getElementById('nasha-order-container');
-    // const { default: setupNasHa } = await import('ConfigoNasHa/ConfigoNasHa');
-    import('ConfigoNasHa/ConfigoNasHa').then(({ default: setupNasHa }) => {
-      setupNasHa(element, {
-        options: {
-          assets: {
-            flagsPath: '/assets/flags',
-          },
-          language: 'en',
-          subsidiary: 'FR',
-          // express: {
-          //   // openTarget: '_top',
-          //   backUrl: this.nashaPublicUrl,
-          // },
-          navbar: {
-            enable: true,
-            // backUrl: this.nashaRoot,
-          },
-          cart: {
-            enable: false,
-          },
+
+    this.setupNasHa(element, {
+      options: {
+        assets: {
+          flagsPath: '/assets/flags',
         },
-        callbacks: {
-          error: () => {},
-          ready: () => {},
-          update: () => {},
-          navigation: (ne) => {
-            switch (ne.action) {
-              case 'order':
-                this.trackClick(PREFIX_TRACKING_ORDER, 'confirm');
-                this.goToNasha();
-                break;
-              case 'leave':
-                this.trackClick(PREFIX_TRACKING_ORDER, 'cancel');
-                break;
-              default:
-                break;
-            }
-          },
+        language: this.userLocale,
+        subsidiary: this.user.ovhSubsidiary,
+        express: {
+          // openTarget: '_top',
+          backUrl: this.nashaPublicUrl,
         },
-        parameters: null,
-        selections: null,
-      });
-    });
-  }
-
-  get plan() {
-    return this.capacity.value;
-  }
-
-  set plan(plan) {
-    this.capacity.value = plan;
-  }
-
-  finish() {
-    this.trackClick(PREFIX_TRACKING_ORDER, 'confirm');
-
-    const { pricing } = this.payment.value;
-    const includeDuration =
-      pricing.duration !== 'P1M' && pricing.interval !== 1;
-    const products = [
-      {
-        productId: PRODUCT_ID,
-        planCode: this.plan.planCode,
-        quantity: 1,
-        pricingMode: this.payment.value.pricingMode,
-        ...(includeDuration && { duration: pricing.duration }),
-        configuration: [
-          { label: 'datacenter', values: [this.datacenter.value] },
-        ],
+        navbar: {
+          enable: true,
+          backUrl: this.nashaRoot,
+        },
+        cart: {
+          enable: true,
+        },
       },
-    ];
-
-    this.trackClickConfirmOrder(
-      `${this.plan.diskType}_${
-        this.capacity.value.capacity.value
-      }${FORMAT_UNIT_CAPACITY_TRACKING_ORDER}::${this.datacenter.value.toLowerCase()}::${
-        FORMAT_DURATION_TRACKING_ORDER.interval[this.commitment.value.duration]
-      }_${
-        this.payment.value.pricing.interval === 1
-          ? FORMAT_DURATION_TRACKING_ORDER.unit.monthly
-          : FORMAT_DURATION_TRACKING_ORDER.unit.yearly
-      }`,
-    );
-
-    this.goToNasha();
-    return this.$window.open(
-      `${this.expressOrderUrl}?products=${JSURL.stringify(products)}`,
-      '_blank',
-    );
-  }
-
-  focus(stepName) {
-    const { diskType, capacity, datacenter, commitment, payment } = this;
-    const steps = Object.entries({
-      diskType,
-      capacity,
-      datacenter,
-      commitment,
-      payment,
+      callbacks: {
+        error: () => {},
+        ready: () => {},
+        update: () => {},
+        navigation: (ne) => {
+          switch (ne.action) {
+            case 'order':
+              this.trackClick(PREFIX_TRACKING_ORDER, 'confirm');
+              this.goToNasha();
+              break;
+            case 'leave':
+              this.trackClick(PREFIX_TRACKING_ORDER, 'cancel');
+              break;
+            default:
+              break;
+          }
+        },
+      },
+      parameters: null,
+      selections: null,
     });
-    const index = steps.findIndex(([name]) => name === stepName);
-
-    steps.forEach(([, step], i) =>
-      Object.assign(step, {
-        ...(i >= index && { value: null }),
-        displayed: i <= index,
-      }),
-    );
-
-    this[`${stepName}Focus`]();
-  }
-
-  diskTypeFocus() {
-    this.diskType.list = uniq(this.plans.map(({ diskType }) => diskType));
-  }
-
-  capacityFocus() {
-    this.capacity.plans = this.plans
-      .filter(({ diskType }) => diskType === this.diskType.value)
-      .sort((planA, planB) => {
-        const capacityA = planA.capacity.value;
-        const capacityB = planB.capacity.value;
-        if (capacityA - capacityB) {
-          return capacityA - capacityB;
-        }
-        return planA.defaultPrice.price - planB.defaultPrice.price;
-      });
-  }
-
-  datacenterFocus() {
-    this.datacenter.list = this.plan.datacenters;
-  }
-
-  commitmentFocus() {
-    this.commitment.price = new CatalogPricing(this.plan.price).toPricing(
-      this.user,
-      this.userLocale,
-    );
-    this.commitment.pricings = this.plan.pricings
-      .map((pricing) => new CatalogPricing(pricing))
-      .filter((pricing) => pricing.includesRenew());
-  }
-
-  paymentFocus() {
-    this.payment.modes = this.BillingService.getAvailableEngagementFromCatalog(
-      this.commitment.pricings.filter(
-        ({ commitment }) =>
-          commitment === this.commitment.value.commitment.durationInMonths,
-      ),
-    );
-  }
-
-  onNextStepClick(hit) {
-    return this.trackClick(PREFIX_TRACKING_ORDER, hit);
-  }
-
-  onCancelClick() {
-    this.trackClick(PREFIX_TRACKING_ORDER, 'cancel');
-    return this.goToNasha();
   }
 }
--- packages/manager/modules/nasha/src/order/order.routing.js
+++ packages/manager/modules/nasha/src/order/order.routing.js
@@ -1,25 +1,27 @@
-import { PRODUCT_ID } from './order.constants';
+import { init, loadRemote } from '@module-federation/runtime';
 
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('nasha.order', {
     url: '/order',
     component: 'nashaOrder',
     resolve: {
+      nashaPublicUrl: /* @ngInject */ ($injector) =>
+        $injector.get('shellClient').navigation.getURL('dedicated', `#/nasha`),
       breadcrumb: () => null,
       goToNasha: /* @ngInject */ ($state) => () => $state.go('nasha'),
-      plans: /* @ngInject */ ($http, coreConfig, preparePlans) =>
-        $http
-          .get(
-            `/order/catalog/public/${PRODUCT_ID}?ovhSubsidiary=${
-              coreConfig.getUser().ovhSubsidiary
-            }`,
-          )
-          .then(({ data: catalog }) => preparePlans(catalog)),
-      trackClickConfirmOrder: /* @ngInject */ (atInternet) => (hits) => {
-        atInternet.trackClick({
-          name: `nasha-confirm-creation::${hits}`,
-          type: 'action',
+      setupNasHa: /* @ngInject */ () => {
+        init({
+          remotes: [
+            {
+              name: '@order/ConfigoNasHa',
+              alias: 'order_fm',
+              type: 'module',
+              entry:
+                'https://ovhcloudcomdev.static.ovh.net/order/configos/assets/remoteEntry.js',
+            },
+          ],
         });
+        return loadRemote('order_fm/ConfigoNasHa');
       },
     },
   });
