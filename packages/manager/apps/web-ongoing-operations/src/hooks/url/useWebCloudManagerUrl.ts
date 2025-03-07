f87cb05a6cd2c47abff8a1da7e5e9069c45d8689
Kylian Bainvel <kylian.bainvel@ovhcloud.com>
2025-03-07 11:45:22 +0100

    fix(web-ongoing-operations): apply fixes for bugs detected by QA
    ref: MANAGER-17345
    
    Signed-off-by: Kylian Bainvel <kylian.bainvel@ovhcloud.com>

--- packages/manager/apps/web-ongoing-operations/src/hooks/url/useWebCloudManagerUrl.ts
+++ packages/manager/apps/web-ongoing-operations/src/hooks/url/useWebCloudManagerUrl.ts
@@ -1,16 +1,14 @@
 import { ShellContext } from '@ovh-ux/manager-react-shell-client';
 import { useContext, useEffect, useState } from 'react';
 
-export const UseManagerUrl = (path: string, value: string) => {
+export const UseWebCloudManagerUrl = (path: string, value: string) => {
   const nav = useContext(ShellContext).shell.navigation;
   const [url, setUrl] = useState('');
 
   useEffect(() => {
-    nav
-      .getURL('manager', `#/web/${path}/${value}/information`, {})
-      .then((data) => {
-        setUrl(data as string);
-      });
+    nav.getURL('web', `#/${path}/${value}/information`, {}).then((data) => {
+      setUrl(data as string);
+    });
   }, [path, value]);
   return url;
 };
