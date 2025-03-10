2cd8ac09d83be047822bb70fa123c273e9a425f4
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2025-03-10 18:55:05 +0530

    feat(ips): listing page header changes
    ref: #MANAGER-16788
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

---
+++ packages/manager/apps/ips/src/pages/listing/ipListing/ip.listing.spec.tsx
@@ -0,0 +1,18 @@
+import { describe, it } from 'vitest';
+import { ODS_ICON_NAME } from '@ovhcloud/ods-components';
+import { getButtonByIcon, renderTest } from '@/test-utils';
+import { urls } from '@/routes/routes.constant';
+
+describe('IP Listing Page', async () => {
+  it('should render a datagrid', async () => {
+    const { container } = await renderTest({
+      nbIp: 2,
+      initialRoute: urls.root,
+    });
+    await getButtonByIcon({
+      container,
+      iconName: ODS_ICON_NAME.ellipsisVertical,
+      nth: 1,
+    });
+  });
+});
---
+++ packages/manager/apps/ips/src/pages/listing/manageOrganisations/manage.organisations.page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ManageOrganisationsPage() {
+  return <>Hello !!</>;
+}
