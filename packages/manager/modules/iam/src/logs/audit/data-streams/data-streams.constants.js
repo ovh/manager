b654c7ea34cb7c4a56cfe2f396ee4b299b7d8f59
David Arsène <david.arsene.ext@ovhcloud.com>
2025-03-10 18:14:54 +0100

    feat(iam): add tracking for audit logs
    resolves: #MANAGER-16787
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/modules/iam/src/logs/audit/data-streams/data-streams.constants.js
+++ packages/manager/modules/iam/src/logs/audit/data-streams/data-streams.constants.js
@@ -1,9 +1,12 @@
-// waiting for tracking PR
+import { IAM_LOGS_AUDIT_TRACKING_PREFIX } from '../../logs.constants';
+
 export const IAM_DATA_STREAMS_TRACKING_HITS = {
-  LISTING_PAGE: ``,
-  ADD_DATA_STREAM: ``,
-  GO_BACK: ``,
-  LDP_DETAIL: ``,
-  SUBSCRIBE: ``,
-  UNSUBSCRIBE: ``,
+  AUDIT: {
+    LISTING_PAGE: `${IAM_LOGS_AUDIT_TRACKING_PREFIX}logs_subscriptions`,
+    ADD_DATA_STREAM: `${IAM_LOGS_AUDIT_TRACKING_PREFIX}page::button::add_datastream_logs_datastream`,
+    GO_BACK: `${IAM_LOGS_AUDIT_TRACKING_PREFIX}page::link::go_back_logs_datastream`,
+    LDP_DETAIL: `${IAM_LOGS_AUDIT_TRACKING_PREFIX}datagrid::button::go-to-detail_logs_datastream`,
+    SUBSCRIBE: `${IAM_LOGS_AUDIT_TRACKING_PREFIX}datagrid::button::subscribe_logs_datastream`,
+    UNSUBSCRIBE: `${IAM_LOGS_AUDIT_TRACKING_PREFIX}datagrid::button::unsubscribe_logs_datastream`,
+  },
 };
