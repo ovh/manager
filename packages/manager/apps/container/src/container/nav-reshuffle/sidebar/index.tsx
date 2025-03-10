99798ab03ddf17652d118c746fac014050ac8926
Maxime Bajeux <maxime.bajeux.ext@ovhcloud.com>
2025-03-10 15:10:41 +0100

    feat(container): on click on universe redirect to telecom dashboard
    ref: #MANAGER-17195
    
    Signed-off-by: Maxime Bajeux <maxime.bajeux.ext@ovhcloud.com>

--- packages/manager/apps/container/src/container/nav-reshuffle/sidebar/index.tsx
+++ packages/manager/apps/container/src/container/nav-reshuffle/sidebar/index.tsx
@@ -240,6 +240,10 @@ const Sidebar = (): JSX.Element => {
       name: trackingIdComplement.replace(/[:][:]$/g, ''),
       type: 'navigation',
     });
+
+    if (node.forceNavigate && node.routing) {
+      navigationPlugin.navigateTo(node.routing.application, node.routing.hash);
+    }
   };
 
   const onEnter = (node: Node) => {
