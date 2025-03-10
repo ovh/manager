a935a14c2b0ab2e3a0c0d05ce6f89963c20dfd02
Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>
2025-03-10 17:12:56 +0100

    feat(pci.kubernetes): refactor css
    ref: TAPC-2244
    Signed-off-by: Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>

--- packages/manager/apps/pci-kubernetes/src/pages/new/steps/PlanStep.tsx
+++ packages/manager/apps/pci-kubernetes/src/pages/new/steps/PlanStep.tsx
@@ -38,6 +38,7 @@ export const PlanTile = ({
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-4 gap-4">
+      <p className="text-primary-300">Hello</p>
       {plans.map((plan) => (
         <RadioTile
           key={plan.value}
