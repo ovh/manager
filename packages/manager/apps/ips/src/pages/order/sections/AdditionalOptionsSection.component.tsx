afc05c2316af32f403c96f24175cc7500c1278a2
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-03-11 18:34:47 +0100

    feat(ips): order ipv6
    ref: #MANAGER-15842
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

---
+++ packages/manager/apps/ips/src/pages/order/sections/AdditionalOptionsSection.component.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { OdsText } from '@ovhcloud/ods-components/react';
+import { ODS_TEXT_PRESET } from '@ovhcloud/ods-components';
+import { OrderSection } from '../../../components/OrderSection/OrderSection.component';
+import { OrderContext } from '../order.context';
+import { Ipv6Options } from '../order.constant';
+import {
+  OptionCard,
+  PriceDescription,
+} from '@/components/OptionCard/OptionCard.component';
+
+export const AdditionalOptionsSection: React.FC = () => {
+  const { selectedOptions, setSelectedOptions } = React.useContext(
+    OrderContext,
+  );
+  const { t } = useTranslation('order');
+
+  return (
+    <OrderSection
+      title={t('additional_options_selection_title')}
+      description={t('additional_options_selection_description')}
+    >
+      <div className="grid grid-cols-2 gap-3">
+        <OptionCard
+          title={t('new_prefix_ipv6_card_title')}
+          description={t('new_prefix_ipv6_card_description')}
+          isSelected={selectedOptions.includes(Ipv6Options.newPrefix56)}
+          onClick={() =>
+            setSelectedOptions((options) =>
+              Array.from(new Set([...options, Ipv6Options.newPrefix56])),
+            )
+          }
+        >
+          <OdsText preset={ODS_TEXT_PRESET.heading4}>
+            <PriceDescription price={0} />
+          </OdsText>
+        </OptionCard>
+      </div>
+    </OrderSection>
+  );
+};
