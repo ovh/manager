294650d57c400a07b2e882a42fd03b14023943b6
soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>
2025-03-11 06:59:10 +0100

    feat(billing): move terminate vrack to billing-components module
    ref MANAGER-17133
    
    Signed-off-by: soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>

--- packages/manager/modules/billing-components/src/components/terminate-vrack/component.js
+++ packages/manager/modules/billing-components/src/components/terminate-vrack/component.js
@@ -10,5 +10,5 @@ export default {
   },
   controller,
   template,
-  name: 'billingAutorenewTerminateVrack',
+  name: 'billingTerminateVrack',
 };
--- packages/manager/modules/billing-components/src/components/utils/billing.links.service.js
+++ packages/manager/modules/billing-components/src/components/utils/billing.links.service.js
@@ -106,6 +106,7 @@ export default class BillingLinksService {
         case SERVICE_TYPE.VRACK:
           if (service.status !== SUSPENDED_SERVICE) {
             links.resiliateLink = `${autorenewLink}/terminate-vrack?service=${service.serviceId}${serviceTypeParam}`;
+            // links.resiliateLink = `${autorenewLink}?selectedType=VRACK&terminate=${service.serviceId}${serviceTypeParam}`;
           }
           break;
         case SERVICE_TYPE.OKMS:
