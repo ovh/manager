ffb329a102f389b358adfda5829d3b44fa6b602b
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-03-11 18:34:47 +0100

    feat(ips): order ipv6
    ref: #MANAGER-15842
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

---
+++ packages/manager/apps/ips/src/data/hooks/catalog/useIpv6PlanCode.ts
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ShellContext } from '@ovh-ux/manager-react-shell-client';
+import { useCatalogIps } from '@/data/hooks/catalog/useCatalogIps';
+import {
+  isRegionInEu,
+  isRegionInUs,
+} from '@/components/RegionSelector/region-selector.utils';
+import { IpVersion } from '@/pages/order/order.constant';
+
+export const useIpv6PlanCode = ({
+  region,
+  ipVersion,
+}: {
+  region: string;
+  ipVersion: IpVersion;
+}) => {
+  const { environment } = React.useContext(ShellContext);
+  const { data } = useCatalogIps({
+    subsidiary: environment.user.ovhSubsidiary,
+    enabled: ipVersion === IpVersion.ipv6,
+  });
+
+  const isEu = isRegionInEu(region);
+  const isUs = isRegionInUs(region);
+  return data?.data?.plans
+    ?.map((p) => p.planCode)
+    ?.find(
+      (planCode) =>
+        planCode.includes('v6') &&
+        (isUs || planCode.includes(isEu ? 'ripe' : 'arin')),
+    );
+};
