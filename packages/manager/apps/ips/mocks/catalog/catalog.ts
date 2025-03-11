25a02785abfd751d1e923c888bde70410ae0cd94
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-03-11 18:34:47 +0100

    feat(ips): order ipv6
    ref: #MANAGER-15842
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager/apps/ips/mocks/catalog/catalog.ts
+++ packages/manager/apps/ips/mocks/catalog/catalog.ts
@@ -2,15 +2,25 @@ import { Handler } from '@ovh-ux/manager-core-test-utils';
 import { catalogIp } from './ip-catalog';
 import { catalogPcc } from './pcc-catalog';
 
-export const getCatalogMocks = (): Handler[] => [
+export type GetCatalogMocksParams = {
+  isIpCatalogKo?: boolean;
+  isPccCatalogKo?: boolean;
+};
+
+export const getCatalogMocks = ({
+  isIpCatalogKo,
+  isPccCatalogKo,
+}: GetCatalogMocksParams): Handler[] => [
   {
     url: '/order/cartServiceOption/privateCloud/:serviceName',
+    response: isPccCatalogKo ? { message: 'PCC Catalog KO' } : catalogPcc,
+    status: isPccCatalogKo ? 400 : 200,
     api: 'v6',
-    response: catalogPcc,
   },
   {
     url: '/order/catalog/formatted/ip',
-    response: catalogIp,
+    response: isIpCatalogKo ? { message: 'PCC Catalog KO' } : catalogIp,
+    status: isIpCatalogKo ? 400 : 200,
     api: 'v6',
   },
 ];
