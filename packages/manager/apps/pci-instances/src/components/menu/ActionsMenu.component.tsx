d63a08a81df371455649806b8215896e257f1c49
Lionel Bueno <lionel.bueno.ext@ovhcloud.com>
2025-03-12 12:55:24 +0100

    fix(instance): action menu
    ref: #TAPC-3366
    Signed-off-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>

--- packages/manager/apps/pci-instances/src/components/menu/ActionsMenu.component.tsx
+++ packages/manager/apps/pci-instances/src/components/menu/ActionsMenu.component.tsx
@@ -1,26 +1,17 @@
 import {
-  ODS_THEME_COLOR_INTENT,
-  ODS_THEME_TYPOGRAPHY_SIZE,
-} from '@ovhcloud/ods-common-theming';
-import {
-  ODS_BUTTON_VARIANT,
-  ODS_ICON_NAME,
-  ODS_ICON_SIZE,
-  ODS_BUTTON_SIZE,
-  ODS_TEXT_LEVEL,
-  ODS_DIVIDER_SIZE,
-} from '@ovhcloud/ods-components';
-import {
-  OsdsButton,
-  OsdsDivider,
-  OsdsIcon,
-  OsdsMenu,
-  OsdsMenuItem,
-  OsdsText,
-} from '@ovhcloud/ods-components/react';
+  Button,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@datatr-ux/uxlib';
+import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
+import { ODS_ICON_NAME, ODS_ICON_SIZE } from '@ovhcloud/ods-components';
+import { OsdsIcon } from '@ovhcloud/ods-components/react';
 import { FC } from 'react';
-import { useHref } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { useHref } from 'react-router-dom';
 import { DeepReadonly } from '@/types/utils.type';
 
 export type TActionsMenuItem = DeepReadonly<{
@@ -40,59 +31,49 @@ export type TActionsMenuLinkProps = DeepReadonly<{
   item: TActionsMenuItem;
 }>;
 
-export const ActionsMenuLink: FC<TActionsMenuLinkProps> = ({ item }) => {
+const ActionMenuItem: FC<TActionsMenuLinkProps> = ({ item }) => {
   const { t } = useTranslation('list');
   const internalHref = useHref(item.link.path);
+
   const href = item.link.isExternal ? item.link.path : internalHref;
+
   return (
-    <OsdsButton
-      size={ODS_BUTTON_SIZE.sm}
-      variant={ODS_BUTTON_VARIANT.ghost}
-      color={ODS_THEME_COLOR_INTENT.primary}
+    <DropdownMenuItem
+      disabled={item.isDisabled}
       data-testid="actions-menu-item"
-      href={href}
-      disabled={item.isDisabled || undefined}
     >
-      <OsdsText
-        size={ODS_THEME_TYPOGRAPHY_SIZE._500}
-        level={ODS_TEXT_LEVEL.button}
-        color={ODS_THEME_COLOR_INTENT.primary}
-        slot={'start'}
-      >
+      <a className="text-blue-700" href={href}>
         {t(item.label)}
-      </OsdsText>
-    </OsdsButton>
+      </a>
+    </DropdownMenuItem>
   );
 };
 
-export const ActionsMenu: FC<TActionsMenuProps> = ({ items }) => (
-  <OsdsMenu>
-    <OsdsButton
-      data-testid="actions-menu-button"
-      slot={'menu-title'}
-      circle
-      color={ODS_THEME_COLOR_INTENT.primary}
-      variant={ODS_BUTTON_VARIANT.stroked}
-    >
-      <OsdsIcon
-        name={ODS_ICON_NAME.ELLIPSIS}
-        color={ODS_THEME_COLOR_INTENT.primary}
-        size={ODS_ICON_SIZE.xxs}
-      />
-    </OsdsButton>
-    {Array.from(items.entries()).map(([group, item], index, arr) => (
-      <div key={group}>
-        {item.map((elt) => (
-          <OsdsMenuItem key={elt.label}>
-            <ActionsMenuLink item={elt} />
-          </OsdsMenuItem>
-        ))}
-        {arr.length - 1 !== index && (
-          <div className="px-4">
-            <OsdsDivider separator size={ODS_DIVIDER_SIZE.one} />
+export const ActionsMenu = ({ items }: TActionsMenuProps) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          data-testid="services-action-trigger"
+          className="size-9 p-0  text-primary border-primary border-2 bg-background font-semibold hover:bg-primary-100 rounded-full"
+        >
+          <OsdsIcon
+            name={ODS_ICON_NAME.ELLIPSIS}
+            color={ODS_THEME_COLOR_INTENT.primary}
+            size={ODS_ICON_SIZE.xxs}
+          />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent data-testid="services-action-content" align="end">
+        {Array.from(items.entries()).map(([group, item], index, arr) => (
+          <div key={group}>
+            {item.map((elt) => (
+              <ActionMenuItem key={elt.label} item={elt} />
+            ))}
+            {arr.length - 1 !== index && <DropdownMenuSeparator />}
           </div>
-        )}
-      </div>
-    ))}
-  </OsdsMenu>
-);
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
