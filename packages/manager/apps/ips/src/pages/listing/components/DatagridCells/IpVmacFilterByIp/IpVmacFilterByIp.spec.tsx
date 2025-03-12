4fd2fbfcd144e817158c6f08e6b148c11a80a4a4
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2025-03-12 14:22:55 +0100

    test(ips): add unit tests for ip listing group
    ref: M#MANAGER-17119
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

---
+++ packages/manager/apps/ips/src/pages/listing/components/DatagridCells/IpVmacFilterByIp/IpVmacFilterByIp.spec.tsx
@@ -0,0 +1,63 @@
+import '@/test-utils/setupUnitTests';
+import React from 'react';
+import { describe, expect, it, vi } from 'vitest';
+import { render, waitFor } from '@testing-library/react';
+import { IpVmacFilterByIp, IpVmacFilterByIpProps } from './IpVmacFilterByIp';
+import { MacAddressTypeEnum } from '@/data/api';
+
+/** RENDER */
+const renderComponent = (params: IpVmacFilterByIpProps) => {
+  return render(<IpVmacFilterByIp {...params} />);
+};
+
+describe('IpVmacFilterByIp Component', async () => {
+  it('Should display vmac if exist for given ip', async () => {
+    const { getByText } = renderComponent({
+      ip: '123.123.123.160',
+      vmacsWithIp: [
+        {
+          ip: ['123.123.123.161', '123.123.123.160'],
+          macAddress: 'mac',
+          type: MacAddressTypeEnum.OVH,
+        },
+      ],
+    });
+    await waitFor(() => {
+      expect(getByText('mac')).toBeDefined();
+    });
+  });
+
+  it('Should display - if no vmac exist for given ip', async () => {
+    const { getByText } = renderComponent({
+      ip: '123.123.123.163',
+      vmacsWithIp: [
+        {
+          ip: ['123.123.123.161', '123.123.123.160'],
+          macAddress: 'mac',
+          type: MacAddressTypeEnum.OVH,
+        },
+      ],
+    });
+    await waitFor(() => {
+      expect(getByText('-')).toBeDefined();
+    });
+  });
+
+  it('Should display nothing if not enabled', async () => {
+    const { queryByText } = renderComponent({
+      ip: '123.123.123.163',
+      vmacsWithIp: [
+        {
+          ip: ['123.123.123.161', '123.123.123.160'],
+          macAddress: 'mac',
+          type: MacAddressTypeEnum.OVH,
+        },
+      ],
+      enabled: false,
+    });
+    await waitFor(() => {
+      expect(queryByText('-')).toBeNull();
+      expect(queryByText('mac')).toBeNull();
+    });
+  });
+});
