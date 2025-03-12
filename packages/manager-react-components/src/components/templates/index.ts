ee0e2fcf13ccececd304addaedea05c3572866c7
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-03-12 17:30:10 +0100

    feat(manager-react-components): add error boundary component
    ref: #MANAGER-17173
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-react-components/src/components/templates/index.ts
+++ packages/manager-react-components/src/components/templates/index.ts
@@ -2,6 +2,7 @@ export * from './dashboard/dashboard.component';
 export * from './base/base.component';
 export * from './error/error.component';
 export * from './error/error.types';
+export * from './error-boundary/error-boundary.component';
 export * from './onboarding/onboarding.component';
 export * from './layout/layout.component';
 export * from './delete-modal/delete-modal.component';
