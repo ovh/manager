f8a3419ed5671f6047b73fb44c0182a29f1d97ce
Maxime Bajeux <maxime.bajeux.ext@ovhcloud.com>
2025-03-12 16:12:09 +0100

    feat(hub): duplicate hub react to avoid service interuption
    ref: #MANAGER-15693
    
    Signed-off-by: Maxime Bajeux <maxime.bajeux.ext@ovhcloud.com>

---
+++ packages/manager/apps/hub-react/src/components/hub-support/HubSupport.constants.ts
@@ -0,0 +1,8 @@
+export const MAX_TICKETS_TO_DISPLAY = 2;
+
+export const SUPPORT_URLS = {
+  allTickets:
+    'https://help.ovhcloud.com/csm?id=csm_cases_requests&spa=1&table=sn_customerservice_case&filter=active%3Dtrue&p=1&o=sys_updated_on&d=desc&ovhSubsidiary=',
+  viewTicket:
+    'https://help.ovhcloud.com/csm?id=csm_ticket&table=sn_customerservice_case&number=CS{ticketId}&view=csp&ovhSubsidiary=',
+};
