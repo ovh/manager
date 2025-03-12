4cac25714b92a74216853806ccb5e3fd0870b724
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-03-12 17:30:10 +0100

    feat(manager-react-components): add error boundary component
    ref: #MANAGER-17173
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

---
+++ packages/manager-react-components/src/components/templates/error-boundary/error-boundary.component.tsx
@@ -0,0 +1,74 @@
+import { useContext, useEffect } from 'react';
+import { ErrorBanner } from '../error/error.component';
+import { useRouteError } from 'react-router-dom';
+import {
+  ShellContext,
+  useRouteSynchro,
+} from '@ovh-ux/manager-react-shell-client';
+
+export interface ResponseAPIError {
+  message: string;
+  stack: string;
+  name: string;
+  code: string;
+  response?: {
+    headers?: {
+      [key: string]: string;
+      'x-ovh-queryid': string;
+    };
+    data?: {
+      message?: string;
+    };
+  };
+}
+
+const ShellRoutingSync = () => {
+  useRouteSynchro();
+  return null;
+};
+
+export interface ErrorBoundaryProps {
+  /** application name to redirect */
+  redirectionApp: string;
+  /** Trigger the preloader hiding */
+  isPreloaderHide?: boolean;
+  /** Trigger the routes sync beetween shell and the app */
+  isRouteShellSync?: boolean;
+}
+
+export const ErrorBoundary = ({
+  redirectionApp,
+  isPreloaderHide = false,
+  isRouteShellSync = false,
+}: ErrorBoundaryProps) => {
+  const error = useRouteError() as ResponseAPIError;
+  const shell = useContext(ShellContext)?.shell;
+
+  const navigateToHomePage = () => {
+    shell?.navigation.navigateTo(redirectionApp, '', {});
+  };
+
+  const reloadPage = () => {
+    shell?.navigation.reload();
+  };
+
+  useEffect(() => {
+    if (isPreloaderHide) {
+      shell?.ux.hidePreloader();
+    }
+  }, [isPreloaderHide]);
+
+  return (
+    <>
+      <ErrorBanner
+        onReloadPage={reloadPage}
+        onRedirectHome={navigateToHomePage}
+        error={{
+          data: { message: error?.response?.data?.message || error?.message },
+          headers: error?.response?.headers || {},
+        }}
+      />
+      {isRouteShellSync && <ShellRoutingSync />}
+    </>
+  );
+};
