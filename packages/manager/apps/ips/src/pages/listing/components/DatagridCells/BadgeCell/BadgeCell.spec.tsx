0a80f5d77ce948092ddba531e3fe3167f2816da7
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2025-03-13 08:42:45 +0100

    feat(ips): chaneg icon cell by badge cell
    ref: #MANAGER-17051
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

--- packages/manager/apps/ips/src/pages/listing/components/DatagridCells/BadgeCell/BadgeCell.spec.tsx
+++ packages/manager/apps/ips/src/pages/listing/components/DatagridCells/BadgeCell/BadgeCell.spec.tsx
@@ -3,28 +3,28 @@ import React from 'react';
 import { describe, it } from 'vitest';
 import { render } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ODS_ICON_NAME } from '@ovhcloud/ods-components';
+import { ODS_BADGE_COLOR } from '@ovhcloud/ods-components';
 import { ListingContextProvider } from '@/pages/listing/listingContext';
-import { IconCell, IconCellParams } from './IconCell';
+import { BadgeCell, BadgeCellParams } from './BadgeCell';
 import { getTooltipByText } from '@/test-utils';
 
 const queryClient = new QueryClient();
 
 /** RENDER */
-const renderComponent = (params: IconCellParams) => {
+const renderComponent = (params: BadgeCellParams) => {
   return render(
     <QueryClientProvider client={queryClient}>
       <ListingContextProvider>
-        <IconCell {...params} />
+        <BadgeCell {...params} />
       </ListingContextProvider>
     </QueryClientProvider>,
   );
 };
 
-describe('IconCell Component', async () => {
+describe('BadgeCell Component', async () => {
   it('Should display Tooltip', async () => {
     const { container } = renderComponent({
-      icon: ODS_ICON_NAME.bell,
+      badgeColor: ODS_BADGE_COLOR.information,
       text: 'test',
       tooltip: 'tooltip',
       trigger: 'tooltip-trigger-id',
