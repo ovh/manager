6964dbcfe9c0da2524357e860b95925c8ade2906
David Arsène <david.arsene.ext@ovhcloud.com>
2025-03-14 13:18:01 +0100

    feat(iam): add iam access policy logs
    Audit logs have been deactivated
    
    resolves: #MANAGER-17456
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/modules/iam/src/logs/access-policy/index.js
+++ packages/manager/modules/iam/src/logs/access-policy/index.js
@@ -1,16 +1,16 @@
 import '@uirouter/angularjs';
 import 'oclazyload';
 
-const moduleName = 'ovhManagerIAMAuditLogsDataStreamsLazyLoading';
+const moduleName = 'ovhManagerIAMAccessPolicyLogsLazyLoading';
 
 angular.module(moduleName, ['oc.lazyLoad', 'ui.router']).config(
   /* @ngInject */ ($stateProvider) => {
-    $stateProvider.state('iam.logs.audit.data-streams.**', {
-      url: '',
+    $stateProvider.state('iam.logs.access-policy.**', {
+      url: '/access-policy',
       lazyLoad: ($transition$) => {
         const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
 
-        return import('./audit-data-streams.module').then((mod) => {
+        return import('./access-policy-logs.module').then((mod) => {
           return $ocLazyLoad.inject(mod.default || mod);
         });
       },
---
+++ packages/manager/modules/iam/src/logs/audit/audit-logs.module.js
@@ -0,0 +1,13 @@
+import routing from './audit-logs.routing';
+import dataStreams from './data-streams';
+import service from './audit-logs.service';
+
+const moduleName = 'ovhManagerIAMAuditLogs';
+
+angular
+  .module(moduleName, [dataStreams])
+  .config(routing)
+  .service('auditLogsService', service)
+  .run(/* @ngTranslationsInject:json ./translations */);
+
+export default moduleName;
