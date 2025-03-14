915f189c385c35403ce67c14c0796a3af73e27e3
Fabien Henon <fabien.henon.ext@corp.ovh.com>
2025-03-14 17:35:57 +0100

    test(pci-volume-snapshot): add tests for helpers
    ref: DTCORE-3208
    Signed-off-by: Fabien Henon <fabien.henon.ext@corp.ovh.com>

--- packages/manager/apps/pci-volume-snapshot/src/routes.tsx
+++ packages/manager/apps/pci-volume-snapshot/src/routes.tsx
@@ -1,5 +1,6 @@
 import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { ErrorBoundary } from '@/pages/Layout';
 
 const ROUTE_PATHS = {
   ROOT: '/pci/projects/:projectId/storages/volume-snapshots',
@@ -10,7 +11,12 @@ const LayoutPage = lazy(() => import('@/pages/Layout'));
 
 const RoutesComponent = () => (
   <Routes>
-    <Route id="root" path={ROUTE_PATHS.ROOT} Component={LayoutPage}></Route>
+    <Route
+      id="root"
+      path={ROUTE_PATHS.ROOT}
+      Component={LayoutPage}
+      errorElement={<ErrorBoundary />}
+    ></Route>
     <Route path="" element={<>Page not found</>}></Route>
   </Routes>
 );
