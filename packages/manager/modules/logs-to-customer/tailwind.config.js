ec6265812bd34c131648c06314e6c90e637d9ecc
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-03-14 17:54:22 +0100

    fix(logs): bump dependencies
    ref: #MANAGER-15351
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

--- packages/manager/modules/logs-to-customer/tailwind.config.js
+++ packages/manager/modules/logs-to-customer/tailwind.config.js
@@ -1,7 +1,4 @@
-import config from '@ovh-ux/manager-tailwind-config';
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  ...config,
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
 };
