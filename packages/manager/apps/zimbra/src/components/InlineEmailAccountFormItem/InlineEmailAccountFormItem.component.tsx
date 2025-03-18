63953a6963cc7edbba289b1f2ba7806b29372b4a
Tristan WAGNER <t.wagner66250@gmail.com>
2025-03-18 11:19:32 +0100

    feat(zimbra): add onboarding tunnel
    ref: #MANAGER-17519
    
    Signed-off-by: Tristan WAGNER <t.wagner66250@gmail.com>

---
+++ packages/manager/apps/zimbra/src/components/InlineEmailAccountFormItem/InlineEmailAccountFormItem.component.tsx
@@ -0,0 +1,163 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import {
+  OdsButton,
+  OdsFormField,
+  OdsInput,
+  OdsPassword,
+} from '@ovhcloud/ods-components/react';
+import {
+  ODS_BUTTON_COLOR,
+  ODS_BUTTON_VARIANT,
+  ODS_ICON_NAME,
+  ODS_INPUT_TYPE,
+} from '@ovhcloud/ods-components';
+import { Controller, useFormContext } from 'react-hook-form';
+import { AddEmailAccountsSchema } from '@/utils';
+
+export default function InlineEmailAccountFormItem({
+  index,
+  onRemove,
+}: {
+  index: number;
+  onRemove: () => void;
+}) {
+  const { t } = useTranslation(['accounts/form', 'common']);
+  const {
+    control,
+    watch,
+    formState: { errors },
+  } = useFormContext<AddEmailAccountsSchema>();
+
+  const domain = watch(`accounts.${index}.domain`);
+
+  return (
+    <div key={`account-${index}`} className="flex gap-4">
+      <div className="w-full flex space-x-3">
+        <Controller
+          control={control}
+          name={`accounts.${index}.firstName`}
+          render={({ field: { name, value, onChange, onBlur } }) => (
+            <OdsFormField
+              className="flex-1"
+              error={errors.accounts?.[index]?.firstName?.message}
+            >
+              <label htmlFor={name} slot="label">
+                {t('zimbra_account_add_input_firstName_label')} *
+              </label>
+              <OdsInput
+                type={ODS_INPUT_TYPE.text}
+                placeholder={t(
+                  'zimbra_account_add_input_firstName_placeholder',
+                )}
+                name={name}
+                hasError={!!errors.accounts?.[index]?.firstName}
+                value={value}
+                onOdsBlur={onBlur}
+                onOdsChange={onChange}
+              />
+            </OdsFormField>
+          )}
+        />
+        <Controller
+          control={control}
+          name={`accounts.${index}.lastName`}
+          render={({ field: { name, value, onChange, onBlur } }) => (
+            <OdsFormField
+              className="flex-1"
+              error={errors.accounts?.[index]?.lastName?.message}
+            >
+              <label htmlFor={name} slot="label">
+                {t('zimbra_account_add_input_lastName_label')} *
+              </label>
+              <OdsInput
+                placeholder={t('zimbra_account_add_input_lastName_placeholder')}
+                type={ODS_INPUT_TYPE.text}
+                id={name}
+                name={name}
+                hasError={!!errors.accounts?.[index]?.lastName}
+                value={value}
+                onOdsBlur={onBlur}
+                onOdsChange={onChange}
+              />
+            </OdsFormField>
+          )}
+        />
+        <Controller
+          control={control}
+          name={`accounts.${index}.account`}
+          render={({ field: { name, value, onChange, onBlur } }) => (
+            <OdsFormField
+              className="flex-2"
+              error={errors.accounts?.[index]?.account?.message}
+            >
+              <label htmlFor={name} slot="label">
+                {t('common:email_account')} *
+              </label>
+              <div className="flex">
+                <OdsInput
+                  type={ODS_INPUT_TYPE.text}
+                  placeholder={t('common:account_name')}
+                  data-testid="input-account"
+                  id={name}
+                  name={name}
+                  hasError={!!errors.accounts?.[index]?.account}
+                  value={value}
+                  onOdsBlur={onBlur}
+                  onOdsChange={onChange}
+                />
+                <OdsInput
+                  type={ODS_INPUT_TYPE.text}
+                  name="@"
+                  value={`@${domain}`}
+                  isReadonly
+                  isDisabled
+                />
+              </div>
+            </OdsFormField>
+          )}
+        />
+        <Controller
+          control={control}
+          name={`accounts.${index}.password`}
+          render={({ field: { name, value, onChange, onBlur } }) => (
+            <OdsFormField
+              className="flex-1"
+              error={errors.accounts?.[index]?.password?.message}
+            >
+              <label htmlFor={name} slot="label">
+                {t('zimbra_account_add_input_password_label')} *
+              </label>
+              <OdsPassword
+                data-testid="input-password"
+                isMasked
+                className="w-full"
+                id={name}
+                name={name}
+                hasError={!!errors.accounts?.[index]?.password}
+                value={value}
+                onOdsBlur={onBlur}
+                onOdsChange={(e) => {
+                  // this is necessary because OdsPassword returns
+                  // value as null somehow
+                  onChange(e?.detail?.value || '');
+                }}
+              />
+            </OdsFormField>
+          )}
+        />
+      </div>
+      <OdsButton
+        id={`delete-account-${index}`}
+        data-testid={`delete-account-${index}`}
+        className="[&::part(button)]:translate-y-1/2"
+        type="button"
+        onClick={onRemove}
+        variant={ODS_BUTTON_VARIANT.ghost}
+        color={ODS_BUTTON_COLOR.critical}
+        icon={ODS_ICON_NAME.trash}
+        label=""
+      />
+    </div>
+  );
+}
