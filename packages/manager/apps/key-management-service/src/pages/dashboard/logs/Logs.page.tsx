3a2bf50c38b7f5664cfe257f115c8ef8b027a3e2
David Arsène <david.arsene.ext@ovhcloud.com>
2025-03-18 19:10:10 +0100

    feat(key-management-service): add feature flipping for dashboard logs tab
    resolves: #MANAGER-15366
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/key-management-service/src/pages/dashboard/logs/Logs.page.tsx
+++ packages/manager/apps/key-management-service/src/pages/dashboard/logs/Logs.page.tsx
@@ -1,28 +1,41 @@
+import LogsToCustomerModule from '@ovh-ux/logs-to-customer/src/LogsToCustomer.module';
+import {
+  RedirectionGuard,
+  useFeatureAvailability,
+} from '@ovh-ux/manager-react-components';
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import LogsToCustomerModule from '@ovh-ux/logs-to-customer/src/LogsToCustomer.module';
 import { useOKMSById } from '@/data/hooks/useOKMS';
+import { FEATURES } from '@/utils/feature-availability/feature-availability.constants';
 
 export default function KmsLogs() {
   const { okmsId } = useParams();
   const { data: okms } = useOKMSById(okmsId);
 
+  const { data: features, isLoading } = useFeatureAvailability([FEATURES.LOGS]);
+
   return (
-    <div className="flex flex-col gap-4">
-      <LogsToCustomerModule
-        logApiVersion="v2"
-        logApiUrls={{
-          logKind: `/okms/resource/${okmsId}/log/kind`,
-          logSubscription: `/okms/resource/${okmsId}/log/subscription`,
-          logUrl: `/okms/resource/${okmsId}/log/url`,
-        }}
-        logIamActions={{
-          postSubscription: ['okms:apiovh:log/subscription/create'],
-          deleteSubscription: ['okms:apiovh:log/subscription/delete'],
-        }}
-        resourceURN={okms.data.iam.urn}
-        trackingOptions={{ trackingSuffix: 'kms' }}
-      />
-    </div>
+    <RedirectionGuard
+      condition={features && !features[FEATURES.LOGS]}
+      isLoading={isLoading}
+      route={`/${okmsId}`}
+    >
+      <div className="flex flex-col gap-4">
+        <LogsToCustomerModule
+          logApiVersion="v2"
+          logApiUrls={{
+            logKind: `/okms/resource/${okmsId}/log/kind`,
+            logSubscription: `/okms/resource/${okmsId}/log/subscription`,
+            logUrl: `/okms/resource/${okmsId}/log/url`,
+          }}
+          logIamActions={{
+            postSubscription: ['okms:apiovh:log/subscription/create'],
+            deleteSubscription: ['okms:apiovh:log/subscription/delete'],
+          }}
+          resourceURN={okms.data.iam.urn}
+          trackingOptions={{ trackingSuffix: 'kms' }}
+        />
+      </div>
+    </RedirectionGuard>
   );
 }
