77efc6436b9b2a9ee49105cee9caf6d15955dd79
Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>
2025-03-18 16:13:30 +0100

    feat(pci.kubernetes): refacto plan
    ref: TAPC-2244
    Signed-off-by: Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>

--- packages/manager/apps/pci-kubernetes/src/pages/new/New.page.tsx
+++ packages/manager/apps/pci-kubernetes/src/pages/new/New.page.tsx
@@ -12,46 +12,35 @@ import {
 } from '@ovh-ux/manager-pci-common';
 import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
 import { OdsHTMLAnchorElementTarget } from '@ovhcloud/ods-common-core';
-import {
-  ODS_ICON_NAME,
-  ODS_ICON_SIZE,
-  ODS_SPINNER_SIZE,
-} from '@ovhcloud/ods-components';
+import { ODS_ICON_NAME, ODS_ICON_SIZE } from '@ovhcloud/ods-components';
 import {
   OsdsBreadcrumb,
   OsdsIcon,
   OsdsLink,
-  OsdsSpinner,
 } from '@ovhcloud/ods-components/react';
 import {
   Headers,
   Notifications,
-  StepComponent,
+  StepComponent as StepComponentLayout,
   useNotifications,
   useProjectUrl,
 } from '@ovh-ux/manager-react-components';
 import { useClusterCreationStepper } from './useCusterCreationStepper';
-import { LocationStep } from './steps/LocationStep.component';
-import { VersionAndUpdatePolicyStep } from './steps/VersionAndUpdatePolicyStep.component';
-import { NetworkStep } from './steps/NetworkStep.component';
-import { ClusterNameStep } from './steps/ClusterNameStep.component';
-import { ClusterConfirmationStep } from './steps/ClusterConfirmStep.component';
+
 import { useCreateKubernetesCluster } from '@/api/hooks/useKubernetes';
 import { PAGE_PREFIX } from '@/tracking.constants';
-import PlanStep from './steps/PlanStep.component';
-import NodePoolStep from './steps/NodePoolStep.component';
+import stepsConfig from './steps/stepsConfig';
 
 export default function NewPage() {
-  const { t } = useTranslation('add');
-  const { t: tListing } = useTranslation('listing');
-  const { t: tStepper } = useTranslation('stepper');
+  const { t } = useTranslation(['add', 'listing', 'stepper']);
+  const are3AZRegions = true;
   const { projectId } = useParams();
   const { data: project } = useProject();
   const { tracking } = useContext(ShellContext).shell;
   const navigate = useNavigate();
   const hrefBack = useHref('..');
   const hrefProject = useProjectUrl('public-cloud');
-  const stepper = useClusterCreationStepper();
+  const stepper = useClusterCreationStepper(are3AZRegions);
   const { addError, addSuccess } = useNotifications();
   const isDiscovery = isDiscoveryProject(project as TProject);
 
@@ -132,6 +121,7 @@ export default function NewPage() {
       updatePolicy: stepper.form.updatePolicy,
       ...(nodePoolEnabled && {
         nodepools: stepper.form.nodePools.map(
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
           ({ localisation: _1, monthlyPrice: _2, ...nodePool }) => nodePool,
         ),
       }),
@@ -149,6 +139,13 @@ export default function NewPage() {
     });
   };
 
+  const allSteps = stepsConfig({
+    stepper,
+    createNewCluster,
+    projectId,
+    are3AZRegions,
+  });
+
   return (
     <>
       {project && (
@@ -160,7 +157,7 @@ export default function NewPage() {
             },
             {
               href: hrefBack,
-              label: tListing('kube_list_title'),
+              label: t('kube_list_title'),
             },
             {
               label: t('kubernetes_add'),
@@ -179,121 +176,39 @@ export default function NewPage() {
       </div>
 
       <div className="mt-8">
-        <StepComponent
-          order={1}
-          {...stepper.clusterName.step}
-          title={t('kubernetes_add_name_title')}
-          edit={{
-            action: stepper.clusterName.edit,
-            label: tStepper('common_stepper_modify_this_step'),
-            isDisabled: isDiscovery || isCreationPending,
-          }}
-        >
-          <ClusterNameStep
-            step={stepper.clusterName.step}
-            onNameChange={stepper.clusterName.update}
-            onSubmit={stepper.clusterName.submit}
-          />
-        </StepComponent>
-        <StepComponent
-          order={2}
-          {...stepper.location.step}
-          isLocked={stepper.location.step.isLocked || isDiscovery}
-          title={t('kubernetes_add_region_title')}
-          edit={{
-            action: stepper.location.edit,
-            label: tStepper('common_stepper_modify_this_step'),
-            isDisabled: isDiscovery || isCreationPending,
-          }}
-        >
-          <LocationStep
-            projectId={projectId}
-            onSubmit={stepper.location.submit}
-            step={stepper.location.step}
-          />
-        </StepComponent>
-        <StepComponent
-          edit={{
-            action: stepper.plan.edit,
-            label: tStepper('common_stepper_modify_this_step'),
-            isDisabled: isDiscovery || isCreationPending,
-          }}
-          title={t('kubernetes_add_plan_title')}
-          order={3}
-          {...stepper.plan.step}
-        >
-          <PlanStep onSubmit={stepper.plan.submit} step={stepper.plan.step} />
-        </StepComponent>
-
-        <StepComponent
-          order={4}
-          {...stepper.version.step}
-          title={t('kubernetes_add_version_and_upgrade_policy_title')}
-          edit={{
-            action: stepper.version.edit,
-            label: tStepper('common_stepper_modify_this_step'),
-            isDisabled: isCreationPending,
-          }}
-        >
-          <VersionAndUpdatePolicyStep
-            onSubmit={stepper.version.submit}
-            step={stepper.version.step}
-          />
-        </StepComponent>
-        <StepComponent
-          order={5}
-          {...stepper.network.step}
-          title={tListing('kubernetes_add_private_network')}
-          edit={{
-            action: stepper.network.edit,
-            label: tStepper('common_stepper_modify_this_step'),
-            isDisabled: isCreationPending,
-          }}
-        >
-          <NetworkStep
-            region={stepper.form.region?.name}
-            onSubmit={stepper.network.submit}
-            step={stepper.network.step}
-          />
-        </StepComponent>
-        <StepComponent
-          order={6}
-          {...stepper.node.step}
-          title={tListing('kube_common_node_pool_title_multiple')}
-          edit={{
-            action: stepper.node.edit,
-            label: tStepper('common_stepper_modify_this_step'),
-            isDisabled: isCreationPending,
-          }}
-        >
-          <NodePoolStep stepper={stepper} />
-        </StepComponent>
-
-        <StepComponent
-          order={7}
-          {...stepper.confirm.step}
-          title={tStepper('common_stepper_submit_button_cluster')}
-          edit={{
-            action: stepper.confirm.edit,
-            label: tStepper('common_stepper_modify_this_step'),
-            isDisabled: isCreationPending,
-          }}
-        >
-          <>
-            {!stepper.confirm.step.isLocked && (
-              <ClusterConfirmationStep
-                nodePools={stepper.form.nodePools}
-                onSubmit={() => {
-                  stepper.confirm.step.lock();
-                  createNewCluster();
+        {allSteps
+          .filter((step) => step.condition ?? true)
+          .map(
+            (
+              {
+                key,
+                // eslint-disable-next-line @typescript-eslint/naming-convention
+                component: StepComponent,
+                titleKey,
+                extraProps = {},
+              },
+              index,
+            ) => (
+              <StepComponentLayout
+                key={key}
+                order={index + 1}
+                {...stepper[key].step}
+                title={t(titleKey)}
+                edit={{
+                  action: stepper[key].edit,
+                  label: t('stepper:common_stepper_modify_this_step'),
+                  isDisabled:
+                    isCreationPending || (key === 'location' && isDiscovery),
                 }}
-              />
-            )}
-            {stepper.confirm.step.isLocked && (
-              <OsdsSpinner inline size={ODS_SPINNER_SIZE.md} />
-            )}
-          </>
-        </StepComponent>
+              >
+                <StepComponent
+                  step={stepper[key].step}
+                  onSubmit={stepper[key].submit}
+                  {...extraProps}
+                />
+              </StepComponentLayout>
+            ),
+          )}
       </div>
     </>
   );
