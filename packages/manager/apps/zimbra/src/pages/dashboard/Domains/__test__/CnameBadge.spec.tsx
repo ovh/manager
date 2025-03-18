e84f7d6d53851b8d4cd58b95bb339344d1ee017e
Tristan WAGNER <t.wagner66250@gmail.com>
2025-03-18 11:19:32 +0100

    feat(zimbra): add onboarding tunnel
    ref: #MANAGER-17519
    
    Signed-off-by: Tristan WAGNER <t.wagner66250@gmail.com>

---
+++ packages/manager/apps/zimbra/src/pages/dashboard/Domains/__test__/CnameBadge.spec.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { describe, expect } from 'vitest';
+import { ResourceStatus } from '@/api/api.type';
+import CnameBadge from '../CnameBadge.component';
+import { render } from '@/utils/test.provider';
+
+describe('CnameBadge Component', () => {
+  it('should render correctly', async () => {
+    const { container } = render(
+      <CnameBadge
+        item={{
+          id: '1',
+          name: 'test-domain',
+          organizationId: '1',
+          organizationLabel: 'test-org',
+          account: 0,
+          status: ResourceStatus.CREATING,
+          cnameToCheck: 'test-domain.cname.ovh',
+        }}
+      />,
+    );
+
+    expect(container).toBeVisible();
+  });
+});
