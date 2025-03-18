d8e4e931e55cc36f40db1443f8ad99c361c48d97
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2025-03-18 14:15:20 +0100

    feat(pci-volume-snapshot): format datagrid displayed data
    ref: DTCORE-3209
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>
    Signed-off-by: Marc Monchablon <marc.monchablon.ext@corp.ovh.com>

---
+++ packages/manager/apps/pci-volume-snapshot/src/pages/listing/Status.component.tsx
@@ -0,0 +1,48 @@
+import { TSnapshot } from '@/api/data/snapshots';
+import { OdsBadgeColor } from '@ovhcloud/ods-components';
+import { OdsBadge } from '@ovhcloud/ods-components/react';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+type StatusComponentProps = {
+  status: TSnapshot['status'];
+};
+
+export default function StatusComponent({
+  status,
+}: Readonly<StatusComponentProps>) {
+  const { t } = useTranslation('volumes');
+
+  const statusGroup = ['error', 'error_deleting'].includes(status)
+    ? 'ERROR'
+    : status.toUpperCase();
+
+  const [badgeColor, setBadgeColor] = useState<OdsBadgeColor>('information');
+
+  useEffect(() => {
+    switch (statusGroup) {
+      case 'AVAILABLE':
+        setBadgeColor('success');
+        break;
+      case 'CREATING':
+      case 'DELETING':
+        setBadgeColor('warning');
+        break;
+      case 'ERROR':
+        setBadgeColor('critical');
+        break;
+      default:
+        setBadgeColor('information');
+        break;
+    }
+  }, [status]);
+
+  return (
+    <OdsBadge
+      className="font-bold"
+      color={badgeColor}
+      data-testid="status_badge"
+      label={t(`pci_projects_project_storages_snapshots_status_${statusGroup}`)}
+    />
+  );
+}
