995f90cc785efb92f03ca4af99554d0d00cfde1f
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-03-18 17:12:11 +0100

    fixup! feat(dedication-cloud): add network tab details
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/components/dedicated-cloud/datacenter/network/index.js
+++ packages/manager/apps/dedicated/client/app/components/dedicated-cloud/datacenter/network/index.js
@@ -2,13 +2,14 @@ import 'angular-translate';
 import '@ovh-ux/ui-kit';
 
 import './dedicatedCloud-datacenter.less';
+import capitalizeFilter from './dedicatedCloud-datacenter-network.filter';
 
 import component from './dedicatedCloud-datacenter-network.component';
 
 const moduleName = 'ovhManagerDedicatedCloudDatacenterNetworkComponent';
 
 angular
-  .module(moduleName, ['oui', 'pascalprecht.translate'])
+  .module(moduleName, ['oui', 'pascalprecht.translate', capitalizeFilter])
   .component('ovhManagerDedicatedCloudDatacenterNetwork', component)
   .run(/* @ngTranslationsInject:json ./translations */);
 
