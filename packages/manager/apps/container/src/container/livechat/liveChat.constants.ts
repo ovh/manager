5acefb4f1a388893e0f677d9c8a460bc577c9614
Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
2025-03-19 10:10:41 +0100

    feat(container): rollback adrielly urls
    ref: MANAGER-15587
    
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>

--- packages/manager/apps/container/src/container/livechat/liveChat.constants.ts
+++ packages/manager/apps/container/src/container/livechat/liveChat.constants.ts
@@ -1,10 +1,13 @@
 
-export const SNOW_INSTANCE_URL = 'https://agent-chat.build-ovhcloud.tools';
+export const SNOW_INSTANCE_URL = 'https://agent-chat.ovh.com';
 export const SNOW_CHAT_QUEUE_STORAGE_KEY = 'snow_chat_queue';
 export const CHAT_STATE_STORAGE_KEY = 'chat_state';
 export const CHAT_TYPE_STORAGE_KEY = 'chat_type';
 
 export const ADRIELLY_CHAT_ORIGIN = 'https://chat.ovh.com';
 export function adriellyChatUrl(customerLevel: string, subsidiary: string, language: string): string {
-  return `https://chat.ovh.com/system/templates/pre-prod/LiveChat_V4/conversation/${customerLevel}/${subsidiary}_${language}/docs/index2.html`;
+  return `https://chat.ovh.com/system/templates/pre-prod/LiveChat_V4/${customerLevel}/${subsidiary}_${language}/docs/index2.html`;
 }
+
+export const ADRIELLY_LABEU_CHAT_URL = 'https://eceweb.ovhcloud.dev/system/templates/livechat-manager/STD/FR_fr/docs/index2.html';
+export const SNOW_LABEU_INSTANCE_URL = 'https://agent-chat.build-ovhcloud.tools';
