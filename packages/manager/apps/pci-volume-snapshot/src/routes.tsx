2d287345bc71fb9b8338001afa4002e200dc2616
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2025-03-19 14:27:57 +0100

    feat(pci-volume-snapshot): implement onboarding page UI and functionality
    ref: DTCORE-3210
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/apps/pci-volume-snapshot/src/routes.tsx
+++ packages/manager/apps/pci-volume-snapshot/src/routes.tsx
@@ -1,15 +1,17 @@
 import { lazy } from 'react';
 import { Route } from 'react-router-dom';
 import { ErrorBoundary } from '@/pages/Layout';
-import { LISTING } from '@/tracking.constants';
+import { LISTING, ONBOARDING } from '@/tracking.constants';
 
 const ROUTE_PATHS = {
   ROOT: '/pci/projects/:projectId/storages/volume-snapshots',
   LISTING: '',
+  ONBOARDING: 'onboarding',
 };
 
 const LayoutPage = lazy(() => import('@/pages/Layout'));
 const VolumeListPage = lazy(() => import('@/pages/listing/Listing.page'));
+const OnBoardingPage = lazy(() => import('@/pages/onboarding/OnBoarding.page'));
 
 export default (
   <Route
@@ -20,5 +22,10 @@ export default (
   >
     <Route path="notFound" element={<>Page not found</>}></Route>
     <Route path={ROUTE_PATHS.LISTING} Component={VolumeListPage} id={LISTING} />
+    <Route
+      path={ROUTE_PATHS.ONBOARDING}
+      Component={OnBoardingPage}
+      id={ONBOARDING}
+    />
   </Route>
 );
