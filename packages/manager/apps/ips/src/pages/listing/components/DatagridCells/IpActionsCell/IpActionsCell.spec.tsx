92e22c5bfc010c21cd591f36333795e8e8a3d39b
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2025-03-20 19:01:29 +0530

    feat(ips): add datagrid for managing the ip organisations
    ref: #MANAGER-17442
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

--- packages/manager/apps/ips/src/pages/listing/components/DatagridCells/IpActionsCell/IpActionsCell.spec.tsx
+++ packages/manager/apps/ips/src/pages/listing/components/DatagridCells/IpActionsCell/IpActionsCell.spec.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ODS_ICON_NAME } from '@ovhcloud/ods-components';
 import { ListingContextProvider } from '@/pages/listing/listingContext';
 import ipDetailsList from '../../../../../../mocks/ip/get-ip-details.json';
-import { ActionsCell, ActionsCellParams } from './ActionsCell';
+import { IpActionsCell, IpActionsCellParams } from './IpActionsCell';
 import { getButtonByIcon } from '@/test-utils';
 
 const queryClient = new QueryClient();
@@ -20,17 +20,17 @@ vi.mock('@/data/hooks/ip', () => ({
 }));
 
 /** RENDER */
-const renderComponent = (params: ActionsCellParams) => {
+const renderComponent = (params: IpActionsCellParams) => {
   return render(
     <QueryClientProvider client={queryClient}>
       <ListingContextProvider>
-        <ActionsCell {...params} />
+        <IpActionsCell {...params} />
       </ListingContextProvider>
     </QueryClientProvider>,
   );
 };
 
-describe('ActionsCell Component', async () => {
+describe('IpActionsCell Component', async () => {
   it('Should display action menu', async () => {
     useGetIpDetailsMock.mockReturnValue({
       ipDetails: ipDetailsList[0],
