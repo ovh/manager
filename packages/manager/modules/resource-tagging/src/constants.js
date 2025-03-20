91fe821cd1598e35eb60b3e6185fd236a5bb3347
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2025-03-20 13:34:11 +0100

    feat(dedicated): add default tags type
    ref: #MANAGER-15740
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

--- packages/manager/modules/resource-tagging/src/constants.js
+++ packages/manager/modules/resource-tagging/src/constants.js
@@ -1,7 +1,78 @@
 export const OVHCLOUD_TAGS = [
-  { key: 'Environment', value: 'Prod' }, // @Todo
-  { key: 'Risk', value: 'Low' }, // @Todo
-  { key: 'Security', value: 'Medium' }, // @Todo
+  {
+    key: 'Environment',
+    values: [
+      'Production',
+      'Staging',
+      'Development',
+      'QA',
+      'UAT',
+      'Pre-production',
+    ],
+  },
+  {
+    key: 'Department',
+    values: ['Finance', 'Marketing', 'IT'],
+  },
+  {
+    key: 'Project',
+    values: ['Marketing-campaign', 'Proof-of-Concept'],
+  },
+  {
+    key: 'Team',
+    values: [
+      'Engineering-team',
+      'Support-team',
+      'Data-Science',
+      'Sales',
+      'Ops',
+      'Marketing',
+    ],
+  },
+  {
+    key: 'Confidential',
+    values: [
+      'Confidential-Personally-identifiable-information',
+      'Financial-data',
+      'Internal',
+      'External',
+    ],
+  },
+  {
+    key: 'Compliance',
+    values: ['HIPAA', 'CCPA', 'PCI-DSS', 'GPDR'],
+  },
+  {
+    key: 'Content-Management-System',
+    values: ['Wordpress', 'Drupal'],
+  },
+  {
+    key: 'eCommerce',
+    values: ['WooCommerce', 'Magento', 'Prestashop'],
+  },
+  {
+    key: 'Business-Intelligence',
+    values: ['Tableau', 'PowerBI', 'Superset'],
+  },
+  {
+    key: 'DevOps',
+    values: ['JenKins', 'GitLab', 'Github'],
+  },
+  {
+    key: 'Mobile',
+    values: ['Xamarin', 'React-native'],
+  },
+  {
+    key: 'Cost-center',
+    values: [
+      'Marketing',
+      'Sales',
+      'Operations',
+      'Finance',
+      'Research-and-Development',
+      'Human-Resources',
+    ],
+  },
 ];
 
 export const OVHCLOUD_TAG_TYPES = {
--- packages/manager/modules/resource-tagging/src/service.js
+++ packages/manager/modules/resource-tagging/src/service.js
@@ -1,13 +1,23 @@
-import { OVHCLOUD_TAG_TYPES } from './constants';
+import { OVHCLOUD_TAGS, OVHCLOUD_TAG_TYPES } from './constants';
 
 export default class ovhManagerResourceTaggingService {
   static getFormattedTags(tags) {
     if (!tags) return [];
-    return Object.keys(tags).map((key) => ({
-      key,
-      value: tags[key],
-      displayName: `${key}:${tags[key]}`,
-      type: OVHCLOUD_TAG_TYPES.CUSTOM,
-    }));
+    return Object.keys(tags)
+      .filter((key) => key !== 'ovh')
+      .map((key) => ({
+        key,
+        value: tags[key],
+        displayName: `${key}:${tags[key]}`,
+        type: ovhManagerResourceTaggingService.isDefaultTag(key, tags[key])
+          ? OVHCLOUD_TAG_TYPES.OVHCLOUD
+          : OVHCLOUD_TAG_TYPES.CUSTOM,
+      }));
+  }
+
+  static isDefaultTag(key, value) {
+    const defaultKey = OVHCLOUD_TAGS.find((tag) => tag.key === key);
+    if (!defaultKey) return false;
+    return !!defaultKey.values.find((val) => val === value);
   }
 }
