a2cbf8f3bcc5d2980538f0c415f71d6083de44cb
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2025-03-20 19:01:29 +0530

    feat(ips): add datagrid for managing the ip organisations
    ref: #MANAGER-17442
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

--- packages/manager/apps/ips/src/data/hooks/organisation/useOrganisationList.ts
+++
@@ -1,8 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { getOrganisationList } from '@/data/api';
-
-export const useOrganisationList = () =>
-  useQuery({
-    queryKey: ['organisationList'],
-    queryFn: getOrganisationList,
-  });
