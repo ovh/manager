87d1926783eeab6d7978755737cfcab17bed2828
Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>
2025-03-20 17:37:18 +0100

    feat(object-storage): add changelog button (#15934)
    ref: #MANAGER-17177
    
    Signed-off-by: Maxime Bajeux <maxime.bajeux.ext@ovhcloud.com>

--- packages/manager/apps/pci-object-storage/src/constants.ts
+++ packages/manager/apps/pci-object-storage/src/constants.ts
@@ -436,3 +436,12 @@ export const X_CONTAINER_READ_PUBLIC_VALUE = '.r:*,.rlistings';
 export const BACKUP_KEY = 'ovh:backup';
 
 export const MUMBAI_REGION_NAME = 'AP-SOUTH-MUM';
+
+export const CHANGELOG_LINKS = {
+  changelog:
+    'https://github.com/orgs/ovh/projects/16/views/6?pane=info&sliceBy%5Bvalue%5D=Public+Cloud+Storage',
+  roadmap:
+    'https://github.com/orgs/ovh/projects/16/views/1?pane=info&sliceBy%5Bvalue%5D=Public+Cloud+Storage',
+  'feature-request':
+    'https://github.com/ovh/public-cloud-roadmap/issues/new?assignees=&labels=&projects=&template=feature_request.md&title=',
+};
