ec39fa9f51a8a389e5d8f39368dee19d313f06c4
Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>
2025-03-21 16:27:21 +0100

    feat(managed-vcd): remove a duplicated changelog button (#15932)
    ref: #MANAGER-17177
    
    Signed-off-by: Maxime Bajeux <maxime.bajeux.ext@ovhcloud.com>

--- packages/manager/apps/key-management-service/src/constants.ts
+++ packages/manager/apps/key-management-service/src/constants.ts
@@ -2,7 +2,7 @@ import { ChangelogLinks } from '@ovh-ux/manager-react-components';
 
 export const CHANGELOG_LINKS: ChangelogLinks = {
   changelog:
-    'https://github.com/ovh/management-security-operations-roadmap/issues/new',
+    'https://github.com/orgs/ovh/projects/16/views/6?sliceBy%5Bvalue%5D=KMS',
   roadmap:
     'https://github.com/orgs/ovh/projects/16/views/1?sliceBy%5Bvalue%5D=KMS&pane=info',
   'feature-request':
