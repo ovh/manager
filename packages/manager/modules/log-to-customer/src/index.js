17aa340340fadcd3c5e92ee6f3936553919c4a9e
David Arsène <david.arsene.ext@ovhcloud.com>
2025-03-21 17:38:52 +0100

    feat(*): manage multi kinds for log to customer
    resolves: #MANAGER-17438
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/log-to-customer/src/index.js
+++ packages/manager/modules/log-to-customer/src/index.js
@@ -3,6 +3,7 @@ import 'angular-translate';
 
 import ovhLogToCustomerLiveTail from './live-tail';
 import ovhLogToCustomerList from './list';
+import ovhLogToCustomerSelectKinds from './select-kinds';
 import tiles from './tiles/component';
 
 import '@uirouter/angularjs';
@@ -15,6 +16,7 @@ angular
     'ui.router',
     ovhLogToCustomerLiveTail,
     ovhLogToCustomerList,
+    ovhLogToCustomerSelectKinds,
   ])
   .component('logTile', tiles)
   .run(/* @ngTranslationsInject:json ./translations */);
