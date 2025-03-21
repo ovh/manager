481671fb653d3ebe5dbcfe6ef5b318cd7b8f6e91
Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>
2025-03-21 17:16:09 +0100

    feat(pci.kubernetes): add region type on region stepper
    ref: TAPC-2804
    Signed-off-by: Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>

--- packages/manager/apps/pci-kubernetes/src/pages/new/steps/NetworkClusterStep.component.tsx
+++ packages/manager/apps/pci-kubernetes/src/pages/new/steps/NetworkClusterStep.component.tsx
@@ -1,10 +1,30 @@
 import { useEffect, useState } from 'react';
-import { ODS_SPINNER_SIZE } from '@ovhcloud/ods-components';
-import { OsdsSpinner } from '@ovhcloud/ods-components/react';
+import {
+  ODS_ICON_NAME,
+  ODS_ICON_SIZE,
+  ODS_MESSAGE_TYPE,
+  ODS_SPINNER_SIZE,
+  ODS_TEXT_COLOR_INTENT,
+} from '@ovhcloud/ods-components';
+import { useTranslation } from 'react-i18next';
+import { OdsHTMLAnchorElementTarget } from '@ovhcloud/ods-common-core';
+import {
+  OsdsSpinner,
+  OsdsMessage,
+  OsdsText,
+  OsdsIcon,
+  OsdsLink,
+} from '@ovhcloud/ods-components/react';
 import { useParams } from 'react-router-dom';
+import {
+  ODS_THEME_COLOR_INTENT,
+  ODS_THEME_TYPOGRAPHY_LEVEL,
+  ODS_THEME_TYPOGRAPHY_SIZE,
+} from '@ovhcloud/ods-common-theming';
 import { useAvailablePrivateNetworks } from '@/api/hooks/useNetwork';
 import { TPrivateNetworkSubnet } from '@/api/data/subnets';
 import { TNetwork } from '@/api/data/network';
+
 import {
   GatewaySelector,
   GatewaySelectorState,
@@ -13,6 +33,8 @@ import PrivateNetworkSelect from '@/components/create/PrivateNetworkSelect.compo
 import SubnetSelect from '@/components/create/SubnetSelect.component';
 import LoadBalancerSelect from '@/components/create/LoadBalancerSelect.component';
 import { LoadBalancerWarning } from '@/components/network/LoadBalancerWarning.component';
+
+import { KUBECONFIG_3AZ_GATEWAY } from '@/constants';
 
 export type TNetworkFormState = {
   privateNetwork?: TNetwork;
@@ -23,11 +45,13 @@ export type TNetworkFormState = {
 
 export type NetworkClusterStepProps = {
   region: string;
+  type;
   onChange: (networkForm: TNetworkFormState) => void;
 };
 
 export default function NetworkClusterStep({
   region,
+  type,
   onChange,
 }: Readonly<NetworkClusterStepProps>) {
   const { projectId } = useParams();
@@ -37,6 +61,7 @@ export default function NetworkClusterStep({
     data: availablePrivateNetworks,
     isPending,
   } = useAvailablePrivateNetworks(projectId, region);
+  const { t } = useTranslation(['network-add', 'service']);
 
   useEffect(() => {
     onChange(form);
@@ -59,6 +84,37 @@ export default function NetworkClusterStep({
         />
       ) : (
         <>
+          {type === 'region-3-az' && (
+            <OsdsMessage
+              type={ODS_MESSAGE_TYPE.info}
+              color={ODS_TEXT_COLOR_INTENT.info}
+              className="flex my-2"
+            >
+              <div>
+                <OsdsText
+                  level={ODS_THEME_TYPOGRAPHY_LEVEL.body}
+                  size={ODS_THEME_TYPOGRAPHY_SIZE._400}
+                  color={ODS_THEME_COLOR_INTENT.text}
+                >
+                  {t('kubernetes_network_form_notify_users')}
+                </OsdsText>{' '}
+                <OsdsLink
+                  target={OdsHTMLAnchorElementTarget._blank}
+                  color={ODS_THEME_COLOR_INTENT.primary}
+                  href={KUBECONFIG_3AZ_GATEWAY}
+                >
+                  {t('service:kube_service_file_more_information')}
+                  <OsdsIcon
+                    className="ml-5"
+                    aria-hidden="true"
+                    name={ODS_ICON_NAME.ARROW_RIGHT}
+                    size={ODS_ICON_SIZE.xxs}
+                    color={ODS_THEME_COLOR_INTENT.primary}
+                  />
+                </OsdsLink>
+              </div>
+            </OsdsMessage>
+          )}
           <PrivateNetworkSelect
             network={form.privateNetwork}
             networks={availablePrivateNetworks}
