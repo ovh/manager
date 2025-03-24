f108ae62ec00ccf6fd0821de56a9b1833261180a
Hazem Belkhiria <hazem.belkhiria.ext@ovhcloud.com>
2025-03-24 10:27:32 +0100

    test(test): run test coverage with turbo
    ref: #MANAGER-16932
    
    Signed-off-by: Hazem Belkhiria <hazem.belkhiria.ext@ovhcloud.com>

---
+++ scripts/test-coverage.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+
+get_changed_packages() {
+  node_modules/.bin/lerna changed --all -l --json | jq -r '.[].name'
+}
+
+main() {
+    changed_packages=$(get_changed_packages)
+    echo $changed_packages
+	while read -r package; do
+         #turbo run test --concurrency=1 --filter=...$package[master...test/runtestcoverage]
+         #turbo run test --concurrency=1 --filter=...$package[HEAD^1]
+         #turbo run test --concurrency=1 --filter=$package[HEAD^1] -- --config=../../jest.config.js
+         turbo run test --concurrency=1 --filter=$package[master...test/runtestcoverage]
+	done <<< "$changed_packages"
+}
+
+main "${@}"
