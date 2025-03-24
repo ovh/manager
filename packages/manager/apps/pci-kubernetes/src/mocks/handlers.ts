5c8bf0db28dae3674b698dfd0f6f354d568e1537
Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>
2025-03-24 18:01:13 +0100

    feat(pci.kubernetes): add mock private network
    ref: TAPC-2828
    Signed-off-by: Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>

--- packages/manager/apps/pci-kubernetes/src/mocks/handlers.ts
+++ packages/manager/apps/pci-kubernetes/src/mocks/handlers.ts
@@ -1,5 +1,6 @@
 import { http, HttpResponse } from 'msw';
 import mockedModule from './mockAvaibility.json';
+import mockedPrivateNetwork from './mockPrivate.json';
 
 export const handlers = [
   http.get(
@@ -15,5 +16,8 @@ export const handlers = [
           },
         ],
       }),
+  ),
+  http.get('/engine/apiv6/cloud/project/:projectId/network/private', () =>
+    HttpResponse.json(mockedPrivateNetwork),
   ),
 ];
