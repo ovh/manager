c7dcaea61a4b98678c282298857fe232ca0a065d
ovh-cds <ovh-cds@users.noreply.github.com>
2025-03-24 09:52:38 +0000

    docs: update documentation [skip ci]

---
+++ storybook-static/assets/syntaxhighlighter-MJWPISIS-DuCWUSMO.js
@@ -0,0 +1,1 @@
+import{S as l,c as n,s as d}from"./index-DjG6mnWW.js";import"./iframe-CTh-o1X2.js";import"../sb-preview/runtime.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BtM5VmRH.js";import"./index-D_r38UMq.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";export{l as SyntaxHighlighter,n as createCopyToClipboardFunction,d as default};
