1181df79761d93b5cf2583710c09022a0ab28475
Frédéric Vilcot <fredericvilcot@gmail.com>
2025-03-24 10:10:40 +0100

    fix(pci-instances): handle delete action with 404 errors
    ref: #TAPC-3531
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

--- packages/manager/apps/pci-instances/src/data/hooks/instance/polling/useInstancesPolling.ts
+++ packages/manager/apps/pci-instances/src/data/hooks/instance/polling/useInstancesPolling.ts
@@ -1,23 +1,45 @@
 import {
+  DefaultError,
   Query,
   useQueries,
   UseQueryOptions,
   UseQueryResult,
 } from '@tanstack/react-query';
 import { useCallback } from 'react';
+import { ApiError } from '@ovh-ux/manager-core-api';
 import { getInstance } from '@/data/api/instance';
 import { useProjectId } from '@/hooks/project/useProjectId';
-import { instancesQueryKey } from '@/utils';
+import { instancesQueryKey, isApiErrorResponse } from '@/utils';
 import { TInstanceDto } from '@/types/instance/api.type';
 
 export type TUseInstancesPollingQueryOptions = Pick<
   UseQueryOptions<TInstanceDto>,
-  'refetchInterval' | 'gcTime' | 'refetchIntervalInBackground'
+  'refetchInterval' | 'gcTime' | 'refetchIntervalInBackground' | 'retry'
 >;
+
+export type TUseInstancesPollingCallbacks = {
+  onError?: (error: ApiError, pendingTaskId: string) => void;
+  onSuccess?: (data: TInstanceDto | undefined) => void;
+};
+
+const defaultQueryOptions: TUseInstancesPollingQueryOptions = {
+  refetchIntervalInBackground: true,
+  gcTime: 0,
+  refetchInterval: (query: Query<TInstanceDto>) =>
+    query.state.error ? false : 10000,
+};
+
+export const shouldRetryAfter404Error = (
+  failureCount: number,
+  error: DefaultError,
+): boolean =>
+  isApiErrorResponse(error) && error.response?.status === 404
+    ? false
+    : failureCount < 3;
 
 export const useInstancesPolling = (
   pendingTaskIds: string[],
-  callback: (data?: TInstanceDto) => void,
+  { onError, onSuccess }: TUseInstancesPollingCallbacks = {},
   options?: TUseInstancesPollingQueryOptions,
 ) => {
   const projectId = useProjectId();
@@ -31,25 +53,27 @@ export const useInstancesPolling = (
           projectId,
           instanceId,
         }),
-      refetchInterval: (query: Query<TInstanceDto>) =>
-        query.state.error ? false : 5000,
-      refetchIntervalInBackground: true,
-      gcTime: 0,
+      ...defaultQueryOptions,
       ...queryOptions,
     })),
     combine: useCallback(
       (results: UseQueryResult<TInstanceDto>[]) =>
-        results.map(({ data, isError }, index) => ({
+        results.map(({ data, error, isLoading }, index) => ({
           id: pendingTaskIds[index],
-          isError,
+          error,
+          isLoading,
           data,
         })),
       [pendingTaskIds],
     ),
   });
 
-  polledInstances.forEach(({ data }) => {
-    callback(data);
+  polledInstances.forEach(({ id, data, error, isLoading }): void => {
+    if (isLoading) return undefined;
+    if (error && isApiErrorResponse(error)) {
+      return onError?.(error, id);
+    }
+    return onSuccess?.(data);
   });
 
   return polledInstances;
