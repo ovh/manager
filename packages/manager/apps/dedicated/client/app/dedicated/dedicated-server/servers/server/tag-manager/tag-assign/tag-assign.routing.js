772f9303bd922ab1de38ad7b62e0bc7c63b356da
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2025-03-25 12:29:30 +0100

    feat(dedicated): add assign tag modal
    ref: #MANAGER-15545
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

---
+++ packages/manager/apps/dedicated/client/app/dedicated/dedicated-server/servers/server/tag-manager/tag-assign/tag-assign.routing.js
@@ -0,0 +1,21 @@
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state(
+    'app.dedicated-server.server.dashboard.tag-manager.assign',
+    {
+      url: '/assign',
+      views: {
+        modal: {
+          component: 'ovhManagerResourceTaggingAssignModal',
+        },
+      },
+      layout: 'modal',
+      resolve: {
+        breadcrumb: () => null,
+        resourceUrn: /* @ngInject */ (server) => server.iam.urn,
+        goBack: /* @ngInject */ (goToTagManager) => () => {
+          goToTagManager(true);
+        },
+      },
+    },
+  );
+};
