faaa0c198e39fd664bd2b4efe35db147cd82c567
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2025-03-25 14:14:05 +0100

    feat(dedicated): add default ovh to resource tagging
    ref: #MANAGER-15545
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

--- packages/manager/modules/resource-tagging/src/assign-tags-modal/constants.js
+++ packages/manager/modules/resource-tagging/src/assign-tags-modal/constants.js
@@ -1,5 +1,10 @@
 export const TAG_REGEX = /^[a-zA-Z0-9-]{1,25}$/;
+export const TAG_ASSIGN_FORM = {
+  KEY: 'tag-key',
+  VALUE: 'tag-value',
+};
 
 export default {
   TAG_REGEX,
+  TAG_ASSIGN_FORM,
 };
