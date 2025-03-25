60474a5ec7acfecbbe27b00a5ff32833d51f9ead
Louis BENSI <louis.bensi.ext@corp.ovh.com>
2025-03-25 11:28:23 +0100

    feat(web-domain): set up base project
    ref: #MANAGER-17028
    
    Signed-off-by: Louis BENSI <louis.bensi.ext@corp.ovh.com>

--- packages/manager/apps/web-domains/src/alldoms/routes/routes.constant.ts
+++ packages/manager/apps/web-domains/src/alldoms/routes/routes.constant.ts
@@ -1,6 +1,5 @@
 export const urls = {
   alldomsRoot: '/alldoms/',
   alldomsOnboarding: '/alldoms/onboarding',
-  alldomsListing: '/alldoms/',
-  alldomsDashboard: '/alldoms/:serviceName',
+  alldomsDetail: '/alldoms/:serviceName',
 };
