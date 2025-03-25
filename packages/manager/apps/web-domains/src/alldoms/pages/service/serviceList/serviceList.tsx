6443776ff00d44915327587eb4c360fab859cc2d
Louis BENSI <louis.bensi.ext@corp.ovh.com>
2025-03-25 11:28:23 +0100

    feat(web-domain): set up base project
    ref: #MANAGER-17028
    
    Signed-off-by: Louis BENSI <louis.bensi.ext@corp.ovh.com>

---
+++ packages/manager/apps/web-domains/src/alldoms/pages/service/serviceList/serviceList.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+import {
+  Breadcrumb,
+  Datagrid,
+  BaseLayout,
+} from '@ovh-ux/manager-react-components';
+
+import appConfig from '@/web-domains.config';
+
+export default function ServiceList() {
+  const { t } = useTranslation('listing');
+
+  const header = {
+    title: t('title'),
+  };
+
+  return (
+    <BaseLayout
+      breadcrumb={
+        <Breadcrumb rootLabel={appConfig.rootLabel} appName="web-domains" />
+      }
+      header={header}
+    >
+      <React.Suspense>
+        <Datagrid columns={[]} items={[]} totalItems={0} />
+      </React.Suspense>
+    </BaseLayout>
+  );
+}
