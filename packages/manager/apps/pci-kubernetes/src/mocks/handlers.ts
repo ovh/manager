6453ca253b96637c26ad655bd29d726289d61bce
Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>
2025-03-25 09:21:51 +0100

    feat(pci.kubernetes): add mock for subnet
    ref: TAPC-2869
    Signed-off-by: Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>

--- packages/manager/apps/pci-kubernetes/src/mocks/handlers.ts
+++ packages/manager/apps/pci-kubernetes/src/mocks/handlers.ts
@@ -20,4 +20,26 @@ export const handlers = [
   http.get('/engine/apiv6/cloud/project/:projectId/network/private', () =>
     HttpResponse.json(mockedPrivateNetwork),
   ),
+  http.get(
+    '/engine/apiv6/cloud/project/:projectId/region/:region/network/:networkId/subnet',
+    () =>
+      HttpResponse.json([
+        {
+          id: '6f66426b-6ba6-425a-af8c-254a3b3c5418',
+          cidr: '10.0.0.0/16',
+          name: 'EU-WEST-PAR',
+          gatewayIp: '10.0.0.1',
+          ipVersion: 4,
+          hostRoutes: [],
+          dhcpEnabled: true,
+          dnsNameServers: ['213.186.33.99'],
+          allocationPools: [
+            {
+              end: '10.0.255.254',
+              start: '10.0.0.2',
+            },
+          ],
+        },
+      ]),
+  ),
 ];
