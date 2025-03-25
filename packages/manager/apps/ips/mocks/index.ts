e080ffa1c3b4f8a205676dd4c0a724f93de026a8
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2025-03-25 08:54:13 +0530

    test(ips): manage organisation page tests
    ref: MANAGER-17442
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

--- packages/manager/apps/ips/mocks/index.ts
+++ packages/manager/apps/ips/mocks/index.ts
@@ -3,7 +3,7 @@ export * from './dedicated/get-dedicated-virtualmac';
 export * from './ip/ip';
 export * from './dedicated-cloud';
 export * from './dedicated-server';
-export * from './organisation';
 export * from './vps';
 export * from './vrack';
 export * from './iam';
+export * from './organisations/organisations';
