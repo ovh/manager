432075c7a8ec1da27764cf0c7c9492e5ff3a69a0
Fabien Henon <fabien.henon.ext@corp.ovh.com>
2025-03-26 16:09:35 +0100

    feat(pci-volume-snapshot): delete modal
    ref: DTCORE-3212
    Signed-off-by: Fabien Henon <fabien.henon.ext@corp.ovh.com>

--- packages/manager/apps/pci-volume-snapshot/src/api/data/snapshots.ts
+++ packages/manager/apps/pci-volume-snapshot/src/api/data/snapshots.ts
@@ -6,6 +6,26 @@ export const getSnapshots = async (projectId: string): Promise<TSnapshot[]> => {
   return data;
 };
 
+export const getSnapshot = async (
+  projectId: string,
+  snapshotId: string,
+): Promise<TSnapshot> => {
+  const { data } = await v6.get(
+    `/cloud/project/${projectId}/volume/snapshot/${snapshotId}`,
+  );
+  return data;
+};
+
+export const deleteSnapshot = async (
+  projectId: string,
+  snapshotId: string,
+): Promise<void> => {
+  const { data } = await v6.delete(
+    `/cloud/project/${projectId}/volume/snapshot/${snapshotId}`,
+  );
+  return data;
+};
+
 export const getVolume = async (
   projectId: string,
   snapshotId: string,
