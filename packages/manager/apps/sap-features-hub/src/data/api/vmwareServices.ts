1df74419bc93887e553e7e782dc7a1eb56d45614
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-03-26 16:45:19 +0100

    feat(sap-features-hub): add pre-installation form step6
    ref: #MANAGER-15981
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/sap-features-hub/src/data/api/vmwareServices.ts
+++ packages/manager/apps/sap-features-hub/src/data/api/vmwareServices.ts
@@ -1,61 +1,91 @@
 import apiClient, { ApiResponse, v6 } from '@ovh-ux/manager-core-api';
 import {
+  DatacentrePortGroup,
   SwsResponse,
   VMwareDatacentre,
   VMwareDatacentreCluster,
   VMwareService,
+  VMwareStoragePolicy,
 } from '@/types/vmwareService.type';
 import { TSAPInstallation } from '@/types/installation.type';
+import {
+  getClusterIdsRoute,
+  getClusterRoute,
+  getDatacentrePortGroupRoute,
+  getDatastoresRoute,
+  getVMwareDatacentreRoute,
+  getVMwareSAPInstallationsRoute,
+  getVMwareStoragePolicyRoute,
+  getVsanDatastoresRoute,
+  VMWARE_SERVICES_ROUTE,
+} from '@/utils/apiRoutes.constants';
+import { Datastore, VsanDatastore } from '@/types/datastore.type';
 
-export type GetClustersIdsProps = {
+export type TGetDatacentreParams = {
   serviceName: string;
   datacenterId: string;
 };
-export type GetOneClusterProps = GetClustersIdsProps & { clusterId: number };
+export type TGetClusterParams = TGetDatacentreParams & {
+  clusterId: number;
+};
 
-const vmwareServicesRoute =
-  '/service?type=/dedicatedCloud&subType=EPCC&external=false';
-
-const getVMwareDatacentreRoute = (serviceName: string) =>
-  `/sws/dedicatedCloud/${serviceName}/datacenters-summary`;
-
-const getVMwareSAPInstallationsRoute = (serviceName: string) =>
-  `/dedicatedCloud/${serviceName}/sap`;
-
-const getDatacentreClusterIdsRoute = ({
-  serviceName,
-  datacenterId,
-}: GetClustersIdsProps) =>
-  `/dedicatedCloud/${serviceName}/datacenter/${datacenterId}/cluster`;
-
-const getDatacentreClusterRoute = ({
-  serviceName,
-  datacenterId,
-  clusterId,
-}: GetOneClusterProps) =>
-  `/dedicatedCloud/${serviceName}/datacenter/${datacenterId}/cluster/${clusterId}`;
+const cachedPaginationHeaders = {
+  'X-Pagination-Mode': 'CachedObjectList-Pages',
+};
 
 export const getVMwareServices = async (): Promise<ApiResponse<
   VMwareService[]
->> => apiClient.aapi.get(vmwareServicesRoute);
+>> => apiClient.aapi.get(VMWARE_SERVICES_ROUTE);
+
+export const getVMwareStoragePolicy = async (
+  serviceName: string,
+): Promise<ApiResponse<VMwareStoragePolicy[]>> =>
+  v6.get(getVMwareStoragePolicyRoute(serviceName), {
+    headers: cachedPaginationHeaders,
+  });
 
 export const getVMwareDatacentres = async (
   serviceName: string,
 ): Promise<ApiResponse<SwsResponse<VMwareDatacentre>>> =>
   apiClient.aapi.get(getVMwareDatacentreRoute(serviceName));
 
-export const getDatacentreClusterIds = async ({
+export const getClusterIds = async ({
   serviceName,
   datacenterId,
-}: GetClustersIdsProps): Promise<ApiResponse<number[]>> =>
-  v6.get(getDatacentreClusterIdsRoute({ serviceName, datacenterId }));
+}: TGetDatacentreParams): Promise<ApiResponse<number[]>> =>
+  v6.get(getClusterIdsRoute({ serviceName, datacenterId }));
 
-export const getDatacentreCluster = async ({
+export const getCluster = async ({
   serviceName,
   datacenterId,
   clusterId,
-}: GetOneClusterProps): Promise<ApiResponse<VMwareDatacentreCluster>> =>
-  v6.get(getDatacentreClusterRoute({ serviceName, datacenterId, clusterId }));
+}: TGetClusterParams): Promise<ApiResponse<VMwareDatacentreCluster>> =>
+  v6.get(getClusterRoute({ serviceName, datacenterId, clusterId }));
+
+export const getDatastores = async ({
+  serviceName,
+  datacenterId,
+}: TGetDatacentreParams): Promise<ApiResponse<Datastore[]>> =>
+  v6.get(getDatastoresRoute({ serviceName, datacenterId }), {
+    headers: cachedPaginationHeaders,
+  });
+
+export const getVsanDatastores = async ({
+  serviceName,
+  datacenterId,
+  clusterId,
+}: TGetClusterParams): Promise<ApiResponse<VsanDatastore[]>> =>
+  v6.get(getVsanDatastoresRoute({ serviceName, datacenterId, clusterId }), {
+    headers: cachedPaginationHeaders,
+  });
+
+export const getDatacentrePortGroup = async ({
+  serviceName,
+  datacenterId,
+}: TGetDatacentreParams): Promise<ApiResponse<DatacentrePortGroup[]>> =>
+  v6.get(getDatacentrePortGroupRoute({ serviceName, datacenterId }), {
+    headers: cachedPaginationHeaders,
+  });
 
 export const getSAPInstallations = async (
   serviceName: string,
