dbf2e65b276c046d50dd42b08a5bc8ad59d56bcd
Hazem Belkhiria <hazem.belkhiria.ext@ovhcloud.com>
2025-03-26 17:26:47 +0100

    test(test): run test coverage
    ref: #MANAGER-16932
    
    Signed-off-by: Hazem Belkhiria <hazem.belkhiria.ext@ovhcloud.com>

---
+++ scripts/test-coverage.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+get_changed_packages() {
+  node_modules/.bin/lerna changed --all -l --json | jq -r '.[].name'
+}
+
+main() {
+    changed_packages=$(get_changed_packages)
+    echo $changed_packages
+  if [[ -z "$changed_packages" ]]; then
+	    while read -r package; do
+            yarn exec turbo -- run test --concurrency=1 --filter=$package[origin/${BASE_BRANCH}...origin/${CURRENT_BRANCH}]
+	    done <<< "$changed_packages"
+  else
+      echo "No changed packages identified"
+  fi
+}
+
+main "${@}"
