6a3174f877d623c5ac88f5d07de5f6997391edf9
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2025-03-27 11:03:43 +0530

    refactor(ips): move component folder with respective listing pages
    ref: #MANAGER-17442
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

---
+++ packages/manager/apps/ips/src/pages/listing/ipListing/components/DatagridCells/IpCountry/IpCountry.spec.tsx
@@ -0,0 +1,48 @@
+import '@/test-utils/setupUnitTests';
+import React, { PropsWithChildren } from 'react';
+import { describe, expect, it, vi } from 'vitest';
+import { render, waitFor } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ListingContextProvider } from '@/pages/listing/listingContext';
+import ipDetailsList from '../../../../../../../mocks/ip/get-ip-details.json';
+import { IpCountry, IpCountryProps } from './IpCountry';
+
+const queryClient = new QueryClient();
+/** MOCKS */
+const useGetIpDetailsMock = vi.hoisted(() =>
+  vi.fn(() => ({ ipDetails: undefined, isLoading: true })),
+);
+
+vi.mock('@/data/hooks/ip', () => ({
+  useGetIpdetails: useGetIpDetailsMock,
+}));
+
+vi.mock('../SkeletonCell/SkeletonCell', () => ({
+  SkeletonCell: ({ children }: PropsWithChildren) => <div>{children}</div>,
+}));
+
+/** RENDER */
+const renderComponent = (params: IpCountryProps) => {
+  return render(
+    <QueryClientProvider client={queryClient}>
+      <ListingContextProvider>
+        <IpCountry {...params} />
+      </ListingContextProvider>
+    </QueryClientProvider>,
+  );
+};
+
+describe('IpCountry Component', async () => {
+  it('Should display translated country', async () => {
+    useGetIpDetailsMock.mockReturnValue({
+      ipDetails: ipDetailsList[3],
+      isLoading: false,
+    });
+    const { getByText } = renderComponent({ ip: ipDetailsList[3].ip });
+    await waitFor(() => {
+      expect(
+        getByText(`region-selector-country-name_${ipDetailsList[3].country}`),
+      ).toBeDefined();
+    });
+  });
+});
