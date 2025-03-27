7896ea3c20518c49eb50db4d7425825cc8e90563
Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>
2025-03-27 16:27:33 +0100

    fix(hpc-managed-vcd): remove usage of datacentres label
    ref: #MANAGER-17400
    Signed-off-by: Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>

--- packages/manager/apps/hpc-vmware-managed-vcd/src/pages/dashboard/organization/organizationDashboard.constants.ts
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/pages/dashboard/organization/organizationDashboard.constants.ts
@@ -2,3 +2,4 @@ export const MANAGED_VCD_LABEL = 'Managed VCD';
 export const DATA_PROTECTION_BACKUP_LABEL = 'Managed Veeam Backup';
 export const DATA_PROTECTION_RECOVERY_LABEL =
   'Managed Disaster Recovery Services';
+export const VIRTUAL_DATACENTERS_LABEL = 'Virtual Datacenters';
