68d2ddd4e17a73b566dc7815761a5a55ee1a7ada
ovh-cds <ovh-cds@users.noreply.github.com>
2025-03-27 14:25:12 +0000

    docs: update documentation [skip ci]

--- storybook-static/assets/badge.stories-Cne6bYEw.js
+++
@@ -1,8 +0,0 @@
-import{h as d,i as c,a as m}from"./index-DyaGuj67.js";import{j as r}from"./jsx-runtime-CKrituN3.js";import{a as l,i as p}from"./index-CsPFwKPW.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BtM5VmRH.js";const a=({isLoading:t,...o})=>t?r.jsx(l,{"data-testid":`${o["data-testid"]}`}):r.jsx(p,{...o});try{a.displayName="Badge",a.__docgenInfo={description:"",displayName:"Badge",props:{isLoading:{defaultValue:null,description:"",name:"isLoading",required:!1,type:{name:"boolean"}},"data-testid":{defaultValue:null,description:"",name:"data-testid",required:!1,type:{name:"string"}}}}}catch{}const e={args:{color:d.information,label:"active",size:c.md,icon:m.circleInfo}},B={title:"Components/Badge",component:a,parameters:{docs:{description:{component:"The `Badge` component is used display information for a status."}}}};var s,i,n;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  args: {
-    color: ODS_BADGE_COLOR.information,
-    label: 'active',
-    size: ODS_BADGE_SIZE.md,
-    icon: ODS_ICON_NAME.circleInfo
-  }
-}`,...(n=(i=e.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};const E=["Default"];export{e as Default,E as __namedExportsOrder,B as default};
