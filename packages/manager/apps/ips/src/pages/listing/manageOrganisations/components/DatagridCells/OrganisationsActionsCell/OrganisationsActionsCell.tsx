49fcea14db21cc33cffcbeb057b3c9da27442c0f
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2025-03-27 11:03:43 +0530

    refactor(ips): move component folder with respective listing pages
    ref: #MANAGER-17442
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

---
+++ packages/manager/apps/ips/src/pages/listing/manageOrganisations/components/DatagridCells/OrganisationsActionsCell/OrganisationsActionsCell.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ODS_BUTTON_VARIANT, ODS_ICON_NAME } from '@ovhcloud/ods-components';
+import { ActionMenu, ActionMenuItem } from '@ovh-ux/manager-react-components';
+import { OrgDetails } from '@/data/api';
+
+type OrganisationsCellProps = {
+  org: OrgDetails;
+};
+
+export const OrganisationsActionsCell = (org: OrganisationsCellProps) => {
+  const id = `actions-${org}`;
+
+  const items: ActionMenuItem[] = [];
+
+  return (
+    <ActionMenu
+      items={items}
+      isCompact
+      variant={ODS_BUTTON_VARIANT.ghost}
+      icon={ODS_ICON_NAME.ellipsisVertical}
+      id={id}
+    />
+  );
+};
