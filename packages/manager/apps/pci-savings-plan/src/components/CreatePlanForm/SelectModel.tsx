c1ee8d6817f87d99441fe8b1c5262d6ff310a923
Lio B <lionel95200x@users.noreply.github.com>
2025-03-28 10:42:56 +0100

    feat(svp): add 3az selection (#16092)
    ref: #TAPC-2882
    
    Signed-off-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

---
+++ packages/manager/apps/pci-savings-plan/src/components/CreatePlanForm/SelectModel.tsx
@@ -0,0 +1,74 @@
+import { ODS_SPINNER_SIZE } from '@ovhcloud/ods-components';
+import React from 'react';
+import { Subtitle } from '@ovh-ux/manager-react-components';
+import { useTranslation } from 'react-i18next';
+import { OdsTabs, OdsTab, OdsSpinner } from '@ovhcloud/ods-components/react';
+import { DescriptionWrapper } from './CreatePlanForm';
+import { TileTechnicalInfo } from '../TileTechnicalInfo/TileTechnicalInfo';
+import { Block } from '../SimpleTile/SimpleTile';
+import { InstanceInfo } from '@/types/CreatePlan.type';
+
+type SelectModelProps = {
+  technicalInfo: InstanceInfo['technical'];
+  instanceCategory: string;
+  isInstance: boolean;
+  isTechnicalInfoLoading: boolean;
+  onSelectModel: (model: string) => void;
+  setInstanceCategory: (category: string) => void;
+  tabsList: InstanceInfo[];
+  technicalModel: string;
+};
+
+const SelectModel = ({
+  technicalInfo,
+  instanceCategory,
+  isInstance,
+  isTechnicalInfoLoading,
+  onSelectModel,
+  setInstanceCategory,
+  tabsList,
+  technicalModel,
+}: SelectModelProps) => {
+  const { t } = useTranslation('create');
+
+  return (
+    <Block>
+      <Subtitle>{t('select_model')}</Subtitle>
+      {isInstance && (
+        <div className="mb-[16px] mt-[12px]">
+          <OdsTabs slot="top">
+            {tabsList.map(({ technicalName, label }) => (
+              <OdsTab
+                key={technicalName}
+                isSelected={technicalName === instanceCategory}
+                onClick={() => setInstanceCategory(technicalName)}
+              >
+                {label}
+              </OdsTab>
+            ))}
+          </OdsTabs>
+        </div>
+      )}
+      <DescriptionWrapper>
+        {t(`select_model_description_instance_${instanceCategory}`)}
+      </DescriptionWrapper>
+      {!isTechnicalInfoLoading ? (
+        <div className="flex flex-row w-full overflow-x-auto mb-[32px] gap-6">
+          {technicalInfo?.map(({ name, technical }) => (
+            <TileTechnicalInfo
+              key={name}
+              technical={technical}
+              name={name}
+              onClick={() => onSelectModel(name)}
+              isActive={technicalModel === name}
+            />
+          ))}
+        </div>
+      ) : (
+        <OdsSpinner size={ODS_SPINNER_SIZE.md} />
+      )}
+    </Block>
+  );
+};
+
+export default SelectModel;
---
+++ packages/manager/apps/pci-savings-plan/src/components/CreatePlanForm/SelectQuantity.tsx
@@ -0,0 +1,73 @@
+import { ODS_ICON_NAME, OdsInputChangeEvent } from '@ovhcloud/ods-components';
+
+import React from 'react';
+import { usePciUrl } from '@ovh-ux/manager-pci-common';
+
+import { Subtitle } from '@ovh-ux/manager-react-components';
+import {
+  OdsText,
+  OdsQuantity,
+  OdsMessage,
+  OdsLink,
+  OdsCard,
+} from '@ovhcloud/ods-components/react';
+import { useTranslation } from 'react-i18next';
+import { DescriptionWrapper } from './CreatePlanForm';
+import { Block } from '../SimpleTile/SimpleTile';
+
+export const DEFAULT_QUANTITY = 1;
+export const MAX_QUANTITY = 1000;
+
+export type SelectQuantityProps = {
+  isInstance: boolean;
+  quantity: number;
+  handleQuantityChange: (event: OdsInputChangeEvent) => void;
+};
+
+const SelectQuantity = ({
+  isInstance,
+  quantity,
+  handleQuantityChange,
+}: SelectQuantityProps) => {
+  const { t } = useTranslation('create');
+  const pciUrl = usePciUrl();
+
+  return (
+    <Block>
+      <Subtitle>{t('select_quantity')}</Subtitle>
+      <DescriptionWrapper className="mb-[12px]">
+        {t(
+          `select_quantity_description_${isInstance ? 'instance' : 'rancher'}`,
+        )}
+      </DescriptionWrapper>
+      <OdsMessage className="my-2" isDismissible={false}>
+        <OdsText className="inline-block">
+          {t(`quantity_banner_${isInstance ? 'instance' : 'rancher'}`)}
+          {isInstance && (
+            <OdsLink
+              href={`${pciUrl}/quota`}
+              target="_blank"
+              icon={ODS_ICON_NAME.externalLink}
+              label={t('quantity_banner_instance_link')}
+            />
+          )}
+        </OdsText>
+      </OdsMessage>
+      <OdsCard className="flex flex-row items-center mr-5 p-4 text-center justify-between w-full mb-[32px] mt-[16px]">
+        <OdsText>
+          {t(`quantity_label_${isInstance ? 'instance' : 'rancher'}`)}
+        </OdsText>
+
+        <OdsQuantity
+          onOdsChange={handleQuantityChange}
+          value={quantity}
+          min={DEFAULT_QUANTITY}
+          max={MAX_QUANTITY}
+          name="quantity"
+        />
+      </OdsCard>
+    </Block>
+  );
+};
+
+export default SelectQuantity;
