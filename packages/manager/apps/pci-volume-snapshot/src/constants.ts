0da8e4ce2549f41f9180119723d4dfd7316ea4ac
Fabien Henon <fabien.henon.ext@corp.ovh.com>
2025-03-31 15:10:59 +0200

    feat(pci-volume-snapshot): add changelog and roadmap links
    ref: DTCORE-3238
    Signed-off-by: Fabien Henon <fabien.henon.ext@corp.ovh.com>

---
+++ packages/manager/apps/pci-volume-snapshot/src/constants.ts
@@ -0,0 +1,6 @@
+export const CHANGELOG_LINKS = {
+  changelog: 'https://github.com/orgs/ovh/projects/16/views/6?pane=info',
+  roadmap: 'https://github.com/orgs/ovh/projects/16/views/1?pane=info',
+  'feature-request':
+    'https://github.com/ovh/public-cloud-roadmap/issues/new?assignees=&labels=&projects=&template=feature_request.md&title=',
+};
