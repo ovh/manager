a547efb85aa0c231535632ffc98354af68e25432
soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>
2025-04-02 15:56:22 +0200

    CR

---
+++ packages/manager/apps/dedicated-servers/src/tracking.constant.ts
@@ -0,0 +1,20 @@
+export const LEVEL2 = {
+  EU: {
+    config: {
+      level2: '57',
+    },
+  },
+  CA: {
+    config: {
+      level2: '57',
+    },
+  },
+  US: {
+    config: {
+      level2: '57',
+    },
+  },
+};
+export const UNIVERSE = 'Dedicated';
+export const SUB_UNIVERSE = 'Dedicated';
+export const APP_NAME = 'dedicated-servers'
