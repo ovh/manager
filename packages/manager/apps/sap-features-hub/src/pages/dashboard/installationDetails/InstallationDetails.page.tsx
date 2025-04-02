aa75a72b520f883f1761e260818533f5dc14b1a6
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-04-02 18:43:47 +0200

    feat(sap): add installation dashboard page
    ref: #MANAGER-17205
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>
    Co-authored-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

---
+++ packages/manager/apps/sap-features-hub/src/pages/dashboard/installationDetails/InstallationDetails.page.tsx
@@ -0,0 +1,73 @@
+import { useHref, useSearchParams } from 'react-router-dom';
+import { BaseLayout } from '@ovh-ux/manager-react-components';
+import React, { Suspense } from 'react';
+
+import { useTranslation } from 'react-i18next';
+import { useQuery } from '@tanstack/react-query';
+import { OdsText } from '@ovhcloud/ods-components/react';
+import Breadcrumb from '@/components/Breadcrumb/Breadcrumb';
+import { InstallationDetailsSummary } from '@/components/InstallationDetails/InstallationDetailsSummary.component';
+import { InstallationDetailsProgress } from '@/components/InstallationDetails/InstallationDetailsProgress.component';
+import { InstallationDetailsError } from '@/components/InstallationDetails/InstallationDetailsError.component';
+import { useMockInstallationTaskDetailsOptions } from '@/hooks/installationDetails/useInstallationDetails';
+import { SAPInstallationStatus } from '@/types/installation.type';
+import { urls } from '@/routes/routes.constant';
+
+const REFECTH_INTERVAL_INSTALLATION_STATUS = 30_000;
+
+export default function InstallationDetailsPage() {
+  const [searchParams] = useSearchParams();
+  const serviceName = searchParams.get('serviceName');
+  const taskId = searchParams.get('taskId');
+  const hrefPrevious = useHref(urls.installation);
+
+  const { t } = useTranslation('dashboard/installation');
+  const header = {
+    title: t('dashboard_installation_title'),
+  };
+
+  useQuery({
+    ...useMockInstallationTaskDetailsOptions({
+      serviceName,
+      taskId,
+    }),
+    refetchInterval(query) {
+      if (
+        [
+          SAPInstallationStatus.pending,
+          SAPInstallationStatus.retry,
+          SAPInstallationStatus.started,
+        ].includes(query.state?.data?.data?.status)
+      ) {
+        return REFECTH_INTERVAL_INSTALLATION_STATUS;
+      }
+      return false;
+    },
+  });
+
+  return (
+    <Suspense>
+      <BaseLayout
+        breadcrumb={<Breadcrumb />}
+        header={header}
+        backLinkLabel={t('dashboard_installation_go_back_to_list')}
+        hrefPrevious={hrefPrevious}
+      >
+        <div className="flex flex-col gap-8">
+          <OdsText preset="heading-2">
+            {t('dashboard_installation_description')}
+          </OdsText>
+          <InstallationDetailsError serviceName={serviceName} taskId={taskId} />
+          <InstallationDetailsSummary
+            serviceName={serviceName}
+            taskId={taskId}
+          />
+          <InstallationDetailsProgress
+            serviceName={serviceName}
+            taskId={taskId}
+          />
+        </div>
+      </BaseLayout>
+    </Suspense>
+  );
+}
--- packages/manager/apps/sap-features-hub/src/routes/routes.constant.ts
+++ packages/manager/apps/sap-features-hub/src/routes/routes.constant.ts
@@ -4,7 +4,8 @@ export const subRoutes = {
   serviceName: ':serviceName',
   initialStep: 'initialization',
   stepId: ':stepId',
-};
+  taskId: ':taskId',
+} as const;
 
 export const urls = {
   root: `/`,
@@ -13,4 +14,5 @@ export const urls = {
   installation: `/${subRoutes.installation}`,
   installationInitialStep: `/${subRoutes.installation}/${subRoutes.initialStep}`,
   installationStep: `/${subRoutes.installation}/${subRoutes.stepId}/${subRoutes.serviceName}`,
-};
+  installationDashboard: `/${subRoutes.serviceName}/${subRoutes.installation}/${subRoutes.taskId}`,
+} as const;
