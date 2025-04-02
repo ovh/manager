66bb9f36de1169de4b4115f3f567b4a4a8f87e30
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-04-02 09:14:48 +0200

    refactor(kms): cleanup okms fetch files from unused code
    ref: #MANAGER-17309
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/key-management-service/src/data/api/okms.ts
+++ packages/manager/apps/key-management-service/src/data/api/okms.ts
@@ -1,96 +1,5 @@
-import { ColumnSort } from '@ovh-ux/manager-react-components';
-import {
-  fetchIcebergV2,
-  fetchIcebergV6,
-  apiClient,
-} from '@ovh-ux/manager-core-api';
+import apiClient from '@ovh-ux/manager-core-api';
 import { OKMS } from '@/types/okms.type';
-import { defaultCompareFunction } from '@/data/api/utils';
-
-type Response = unknown;
-type Uuid = unknown;
-
-export type GetiamPolicyListParams = {
-  /** Pagination cursor */
-  'X-Pagination-Cursor': string;
-  /** Add extra information about resources in output */
-  details: boolean;
-  /** Filter on the readOnly attribute */
-  readOnly: boolean;
-};
-
-export const getiamPolicyListQueryKey = ['get/iam/policy'];
-
-/**
- *  : Retrieve all policies
- */
-export const getiamPolicyList = async (
-  params: GetiamPolicyListParams,
-): Promise<Response[]> => apiClient.v2.get('/iam/policy', { data: params });
-
-export type GetiamPolicyPolicyIdParams = {
-  /** Add extra information about resources in output */
-  details: boolean;
-  /** Policy ID */
-  policyId?: Uuid;
-};
-
-export const getiamPolicyPolicyIdQueryKey = (
-  params: GetiamPolicyPolicyIdParams,
-) => [`get/iam/policy/${params.policyId}`];
-
-/**
- *  : Retrieve the given policy
- */
-export const getiamPolicyPolicyId = async (
-  params: GetiamPolicyPolicyIdParams,
-): Promise<Response> =>
-  apiClient.v2.get(`/iam/policy/${params.policyId}`, { data: params });
-
-/**
- *  Get listing with iceberg V6
- */
-export const getListingIcebergV6 = async ({
-  pageSize,
-  page,
-}: {
-  pageSize: number;
-  page: number;
-}) => {
-  try {
-    const List = await fetchIcebergV6({
-      route: '/iam/policy',
-      pageSize,
-      page,
-    }).then(({ data, status, totalCount }) => ({ data, status, totalCount }));
-    return List;
-  } catch (error) {
-    return null;
-  }
-};
-
-/**
- *  Get listing with iceberg V2
- */
-
-export const getListingIcebergV2 = async ({
-  pageSize,
-  cursor,
-}: {
-  pageSize: number;
-  cursor?: string;
-}) => {
-  try {
-    const List = await fetchIcebergV2({
-      route: '/iam/policy',
-      pageSize,
-      cursor,
-    }).then(({ data, status, cursorNext }) => ({ data, status, cursorNext }));
-    return List;
-  } catch (error) {
-    return null;
-  }
-};
 
 export const getOkmsResourceQueryKey = (okmsId: string) => [
   `get/okms/resource/${okmsId}`,
@@ -98,17 +7,8 @@ export const getOkmsResourceQueryKey = (okmsId: string) => [
 
 export const getOkmsServicesResourceListQueryKey = ['get/okms/resource'];
 
-export const sortOKMS = (okms: OKMS[], sorting: ColumnSort): OKMS[] => {
-  const data = [...okms];
-
-  if (sorting) {
-    const { id: sortKey, desc } = sorting;
-
-    data.sort(defaultCompareFunction(sortKey as keyof OKMS));
-    if (desc) {
-      data.reverse();
-    }
-  }
-
-  return data;
+export const getOKMSResource = async (
+  okmsId: string,
+): Promise<{ data: OKMS }> => {
+  return apiClient.v2.get(`okms/resource/${okmsId}`);
 };
--- packages/manager/apps/key-management-service/src/data/hooks/useOKMS.ts
+++ packages/manager/apps/key-management-service/src/data/hooks/useOKMS.ts
@@ -1,19 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
-import apiClient from '@ovh-ux/manager-core-api';
 import { useResourcesIcebergV2 } from '@ovh-ux/manager-react-components';
-
 import { OKMS } from '@/types/okms.type';
 import { ErrorResponse } from '@/types/api.type';
 import {
+  getOKMSResource,
   getOkmsResourceQueryKey,
   getOkmsServicesResourceListQueryKey,
-} from '../api/okms';
-
-export const getOKMSResource = async (
-  okmsId: string,
-): Promise<{ data: OKMS }> => {
-  return apiClient.v2.get(`okms/resource/${okmsId}`);
-};
+} from '@/data/api/okms';
 
 export const useOKMSById = (okmsId: string) => {
   return useQuery<{ data: OKMS }, ErrorResponse>({
