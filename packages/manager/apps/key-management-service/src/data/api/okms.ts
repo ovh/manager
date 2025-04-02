578002a9b8ae0c1b55ca1c0dffe1753713710e6d
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-04-02 14:42:57 +0200

    feat(kms): add download PEM buttons
    ref: #MANAGER-17309
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/key-management-service/src/data/api/okms.ts
+++ packages/manager/apps/key-management-service/src/data/api/okms.ts
@@ -1,5 +1,5 @@
-import apiClient from '@ovh-ux/manager-core-api';
-import { OKMS } from '@/types/okms.type';
+import apiClient, { ApiResponse } from '@ovh-ux/manager-core-api';
+import { OKMS, OkmsPublicCa } from '@/types/okms.type';
 
 export const getOkmsResourceQueryKey = (okmsId: string) => [
   `get/okms/resource/${okmsId}`,
@@ -7,8 +7,17 @@ export const getOkmsResourceQueryKey = (okmsId: string) => [
 
 export const getOkmsServicesResourceListQueryKey = ['get/okms/resource'];
 
-export const getOKMSResource = async (
+export const getOkmsResource = async (
   okmsId: string,
 ): Promise<{ data: OKMS }> => {
   return apiClient.v2.get(`okms/resource/${okmsId}`);
+};
+
+export const getOkmsPublicCa = async (
+  okmsId: string,
+): Promise<OkmsPublicCa> => {
+  const { data }: ApiResponse<OkmsPublicCa> = await apiClient.v2.get(
+    `okms/resource/${okmsId}?publicCA=true`,
+  );
+  return data;
 };
