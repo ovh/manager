fa95aa13247144f64eacfd53d24183740e29671e
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-04-03 15:43:00 +0200

    feat(manager-react-component): export formatted date component
    Resolve #16372
    
    BREAKING CHANGE: Render useFormattedDate deprecated
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager-react-components/src/components/formatted-date/FormattedDate.tsx
+++ packages/manager-react-components/src/components/formatted-date/FormattedDate.tsx
@@ -3,6 +3,9 @@ import {
   useFormattedDate,
 } from '../../hooks/date/useFormattedDate';
 
+//
+// @deprecated This component is deprecated. Use `useFormatDate` hook instead.
+//
 export const FormattedDate = (props: FormattedDateProps) => {
   const formattedDate = useFormattedDate(props);
 
