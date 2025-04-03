ac55cdf1e7a865a9d62d6c0e712c3a73ad6089b4
Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>
2025-04-03 17:03:27 +0200

    feat(account): gdpr services remove dev api references
    ref: #MANAGER-15206
    
    Signed-off-by: Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>

--- packages/manager/modules/account/src/user/gdpr/gdpr.service.js
+++ packages/manager/modules/account/src/user/gdpr/gdpr.service.js
@@ -1,40 +1,23 @@
-// TODO: clean this file of API_BASE_ROUTE and createHeaders once we're ready to prod
-// they are used to access a DEV version of the API used to do our tests in local
-// eslint-disable-next-line no-unused-vars
-const API_BASE_ROUTE = 'http://gw2sdev-docker.ovh.net:14975/v1';
-const createHeaders = (nic) => ({
-  'X-Ovh-Nic': nic,
-});
-
 export default class GdprService {
   /* @ngInject */
-  constructor($http, coreConfig) {
+  constructor($http) {
     this.$http = $http;
-    this.headers = createHeaders(coreConfig.getUser()?.nichandle);
   }
 
   getCapabilities() {
-    return (
-      this.$http
-        // .get(`${API_BASE_ROUTE}/me/privacy/requests/capabilities`, {
-        .get('/me/privacy/requests/capabilities', {
-          // headers: this.headers,
-          serviceType: 'apiv6',
-        })
-        .then(({ data }) => data)
-    );
+    return this.$http
+      .get('/me/privacy/requests/capabilities', {
+        serviceType: 'apiv6',
+      })
+      .then(({ data }) => data);
   }
 
   createErasureRequest() {
-    return (
-      this.$http
-        // .post(`${API_BASE_ROUTE}/me/privacy/requests/erasure`, null, {
-        .post('/me/privacy/requests/erasure', null, {
-          // headers: this.headers,
-          serviceType: 'apiv6',
-        })
-        .then(({ data }) => data)
-    );
+    return this.$http
+      .post('/me/privacy/requests/erasure', null, {
+        serviceType: 'apiv6',
+      })
+      .then(({ data }) => data);
   }
 
   getRequests() {
