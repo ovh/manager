974a165e5dfe1a026a8cde98d8b6e4e737ab8cbb
Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>
2025-04-04 22:03:23 +0200

    feat(hub): remove the duplicate of hub react after bff has been deployed
    ref: #MANAGER-15693
    
    Signed-off-by: Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>

--- packages/manager/apps/hub-react/src/data/api/banner.ts
+++
@@ -1,9 +0,0 @@
-import { aapi } from '@ovh-ux/manager-core-api';
-import { Banner } from '@/types/banner.type';
-
-export const getBanner: (locale: string) => Promise<Banner> = async (
-  locale: string,
-) => {
-  const { data } = await aapi.get<Banner>(`/banner?locale=${locale}`);
-  return data;
-};
