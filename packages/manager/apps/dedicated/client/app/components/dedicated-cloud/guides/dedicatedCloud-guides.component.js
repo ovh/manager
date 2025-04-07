a9e35847d2a903e2cc7e0c88dd54c5969c9f674d
Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>
2025-04-07 16:45:52 +0200

    feat(dedicated): add guides on listing and dashboard pcc
    ref: #MANAGER-17571
    
    Signed-off-by: Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>

---
+++ packages/manager/apps/dedicated/client/app/components/dedicated-cloud/guides/dedicatedCloud-guides.component.js
@@ -0,0 +1,8 @@
+import template from './dedicatedCloud-guides.html';
+import controller from './dedicatedCloud-guides.controller';
+
+export default {
+  controller,
+  template,
+  name: 'ovhManagerPccGuides',
+};
