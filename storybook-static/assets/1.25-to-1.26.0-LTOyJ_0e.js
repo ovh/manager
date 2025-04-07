c198e722c6d2dab93c0c500d17810a5c658d6feb
ovh-cds <ovh-cds@users.noreply.github.com>
2025-04-07 11:29:08 +0000

    docs: update documentation [skip ci]

---
+++ storybook-static/assets/1.25-to-1.26.0-LTOyJ_0e.js
@@ -0,0 +1,20 @@
+import{j as o}from"./jsx-runtime-CKrituN3.js";import{M as m}from"./index-lkzdrT8V.js";/* empty css            */import"./breadcrumb.component-C_4WyS1Q.js";import"./clipboard.component-Da3g5HMM.js";import"./Step.component-BuDZjdkV.js";import"./Tabs.component-pcHwRHhX.js";import"./TilesInput.component-CSCccYCp.js";import"./headers.component-CKVBHe3N.js";import"./price.component-C4Vygo_m.js";import"./dashboard-tile.component-B4OBYwWK.js";import"./manager-tile.component-C-UOaU8u.js";import"./card.component-nt1Q_mnc.js";import"./action.component-DETXVUga.js";import"./guide.component-CgAD5bS3.js";import"./changelog.component-CsN4ogkC.js";import"./base.component-Cez4s4oV.js";import"./error.component-BohihVvZ.js";import"./error-boundary.component-HkxVu1X2.js";import"./onboarding.component-BS6kvZy4.js";import"./delete-modal.component-DE2gFF-3.js";import"./update-name-modal.component-BGkS8vuj.js";import"./title.component-Bx3BQPQH.js";import"./links.component-B_FCtcjf.js";import"./datagrid.component-Dzt5WZ-Y.js";import"./index-CBqU2yxZ.js";import"./notifications.component-Ca6nDj-a.js";import"./ods-notification-BPMGGTlT.js";import"./useColumnFilters-RgZVa838.js";import"./ManagerButton-C8zC-Fyb.js";import"./ManagerText-CIldxG9y.js";import"./PciMaintenanceBanner.component-BsusHqE4.js";import"./region.component-CGyiys5e.js";import"./Order.component-DO3UxPjZ.js";import"./badge.component-Clkk8G09.js";import{useMDXComponents as n}from"./index-C-_6Vi3R.js";import"./iframe-BjGYGtcr.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BtM5VmRH.js";import"./index-D_r38UMq.js";import"./index-Cmc67Rxs.js";import"./index-DrFu-skq.js";import"./index-BoGQ30sD.js";import"./useOvhTracking-C-_VjZ5i.js";import"./index-BkDLP3dD.js";import"./useOvhIam-puHWDhg2.js";import"./QueryClientProvider-DWOoNJcY.js";import"./infiniteQueryBehavior-BOpc6bB6.js";import"./index-CqmMgYso.js";import"./useTranslation-pmbu4BU3.js";import"./i18next-ihUiNgJT.js";import"./clsx-B-dksMZM.js";import"./v4-CQkTLCs1.js";import"./translation-xx6Js1fi.js";import"./error-banner-oops-tNXFEWkx.js";import"./click-utils-ByCElPrV.js";function i(t){const r=Object.assign({h1:"h1",h2:"h2",ol:"ol",li:"li",strong:"strong",blockquote:"blockquote",p:"p"},n(),t.components);return o.jsxs(o.Fragment,{children:[o.jsx(m,{title:"Manager React Components/What's new/Migration guides/1.25.0 to 1.26.0"}),`
+`,o.jsx(r.h1,{id:"migration-from-1250-to-1260",children:"Migration from 1.25.0 to 1.26.0"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(r.h2,{id:"few-steps-to-follow",children:"Few steps to follow"}),`
+`,o.jsx("br",{}),`
+`,o.jsxs(r.ol,{children:[`
+`,o.jsxs(r.li,{children:["Replace your dependancies in your ",o.jsx(r.strong,{children:"package.json"})]}),`
+`]}),`
+`,o.jsx("div",{children:o.jsx("strike",{children:'"@ovhcloud/manager-components": "^1.26.0"'})}),`
+`,o.jsx("div",{children:'"@ovh-ux/manager-react-components": "^1.26.0"'}),`
+`,o.jsx("br",{}),`
+`,o.jsxs(r.ol,{start:"2",children:[`
+`,o.jsxs(r.li,{children:["Replace your components ",o.jsx(r.strong,{children:"import"})]}),`
+`]}),`
+`,o.jsx("div",{children:o.jsx("strike",{children:'import {card} from  "@ovhcloud/manager-components"'})}),`
+`,o.jsx("div",{children:'import {card} from "@ovh-ux/manager-react-components"'}),`
+`,o.jsx("br",{}),`
+`,o.jsxs(r.blockquote,{children:[`
+`,o.jsxs(r.p,{children:["Next release ",o.jsx(r.strong,{children:"1.27.0"}),", we will publish the manager react component in the npm registry"]}),`
+`]})]})}function co(t={}){const{wrapper:r}=Object.assign({},n(),t.components);return r?o.jsx(r,Object.assign({},t,{children:o.jsx(i,t)})):i(t)}export{co as default};
