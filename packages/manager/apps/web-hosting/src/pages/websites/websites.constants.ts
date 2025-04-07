100593024ed436ef851e4dce92d30e4b65bc4bc0
stif59100 <steeve.vanderstocken@ovhcloud.com>
2025-04-07 12:34:46 +0200

    feat(web-hosting): add order button
    ref: #MANAGER-17739
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

---
+++ packages/manager/apps/web-hosting/src/pages/websites/websites.constants.ts
@@ -0,0 +1,26 @@
+const websiteRoot = 'https://www.ovhcloud.com/';
+const webSitePath = '/web-hosting/create-your-website/';
+
+export const ORDER_URL: Record<string, string> = {
+  DEFAULT: `${websiteRoot}en-ie${webSitePath}`,
+  IN: `${websiteRoot}en-in${webSitePath}`,
+  DE: `${websiteRoot}de${webSitePath}`,
+  ES: `${websiteRoot}es-es${webSitePath}`,
+  IE: `${websiteRoot}en-ie${webSitePath}`,
+  IT: `${websiteRoot}it${webSitePath}`,
+  NL: `${websiteRoot}nl${webSitePath}`,
+  PL: `${websiteRoot}pl${webSitePath}`,
+  PT: `${websiteRoot}pt${webSitePath}`,
+  GB: `${websiteRoot}en-gb${webSitePath}`,
+  CA: `${websiteRoot}en-ca${webSitePath}`,
+  QC: `${websiteRoot}fr-ca${webSitePath}`,
+  MA: `${websiteRoot}fr-ma${webSitePath}`,
+  SN: `${websiteRoot}fr-sn${webSitePath}`,
+  TN: `${websiteRoot}fr-tn${webSitePath}`,
+  AU: `${websiteRoot}en-au${webSitePath}`,
+  SG: `${websiteRoot}en-sg${webSitePath}`,
+  FR: `${websiteRoot}fr${webSitePath}`,
+  WE: `${websiteRoot}en${webSitePath}`,
+  WS: `${websiteRoot}es${webSitePath}`,
+  ASIA: `${websiteRoot}asia${webSitePath}`,
+};
