4d853822ac6928053c98f5e5e91b65cac4988215
Anoop N <anooparveti@gmail.com>
2025-04-07 14:14:24 +0530

    feat(*): display maintenance mode (#16413)
    Refactor fetch configuration and shell init to catch error during maintenance
    Reactify the initialisation of container
    Display error page for configuration errors during maintenance
    
    ref: #MANAGER-17748
    
    Signed-off-by: Anoop N <anoop.n@ovhcloud.com>
    
    * fix(container): update cookie-policy test for new initShell
    
    ref: #MANAGER-17748
    
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
    
    ---------
    
    Signed-off-by: Anoop N <anoop.n@ovhcloud.com>
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
    Co-authored-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>

--- packages/manager/apps/container/src/index.tsx
+++ packages/manager/apps/container/src/index.tsx
@@ -3,21 +3,11 @@ import React from 'react';
 import './helpers/piano-override';
 import 'piano-analytics-js/dist/browser/piano-analytics.js';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-
-import { initShell } from '@ovh-ux/shell';
-import i18n from 'i18next';
-import Backend from 'i18next-http-backend';
-import { initReactI18next } from 'react-i18next';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Environment } from '@ovh-ux/manager-config';
 import { defineApplicationVersion } from '@ovh-ux/request-tagger';
-
-import Container from '@/container';
-import { ApplicationProvider } from '@/context';
 import { initSso } from '@/core/sso';
-import { setupDevApplication } from '@/core/dev';
-import { ContainerProvider } from '@/core/container';
+
+import App from './App';
 
 import '@ovh-ux/ui-kit/dist/css/oui.css';
 import 'react-tooltip/dist/react-tooltip.css';
@@ -32,60 +22,14 @@ __VERSION__ && defineApplicationVersion(__VERSION__);
 
 initSso();
 
-initShell().then((shell) => {
-  const environment: Environment = shell
-    .getPlugin('environment')
-    .getEnvironment();
-  const locale = environment.getUserLocale();
-  const config = () => import(`./config-${environment.getRegion()}.js`);
+const queryClient = new QueryClient();
 
-  setupDevApplication(shell);
+const root = createRoot(document.querySelector('#app'));
 
-  config()
-    .catch(() => {})
-    .then(() => {
-      i18n
-        .use(initReactI18next)
-        .use(Backend)
-        .use({
-          type: 'postProcessor',
-          name: 'normalize',
-          process: function process(value: string, key: string) {
-            if (!value) {
-              return value;
-            }
-            return value.replace(/&amp;/g, '&');
-          },
-        })
-        .init({
-          lng: locale,
-          fallbackLng: 'fr_FR',
-          ns: [], // namespaces to load by default
-          load: 'currentOnly',
-          backend: {
-            // path construction for async load, ns: namespace, lng: locale
-            loadPath: (lngs: string[], namespaces: string[]) => {
-              return `./translations/${namespaces[0]}/Messages_${lngs[0]}.json`;
-            },
-          },
-          postProcess: 'normalize',
-        });
-
-      const root = createRoot(document.querySelector('#app'));
-      const queryClient = new QueryClient();
-
-      root.render(
-        <React.StrictMode>
-          <QueryClientProvider client={queryClient}>
-            <ApplicationProvider environment={environment} shell={shell}>
-              <ContainerProvider>
-                <HashRouter>
-                  <Container />
-                </HashRouter>
-              </ContainerProvider>
-            </ApplicationProvider>
-          </QueryClientProvider>
-        </React.StrictMode>,
-      );
-    });
-});
+root.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+        <App />
+    </QueryClientProvider>
+  </React.StrictMode>,
+);
