1535ec2aab16a69cc81fe88e138bbd04da710a5c
Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
2025-04-07 09:57:34 +0200

    fix(container): update cookie-policy test for new initShell
    ref: #MANAGER-17748
    
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>

--- packages/manager/apps/container/src/cookie-policy/CookiePolicy.spec.tsx
+++ packages/manager/apps/container/src/cookie-policy/CookiePolicy.spec.tsx
@@ -1,92 +1,92 @@
-// import { render, waitFor } from '@testing-library/react';
-// import { it, vi, describe, expect } from 'vitest';
-// import { initShell } from '@ovh-ux/shell';
-// import CookiePolicy from './CookiePolicy';
-// import { Environment, User } from '@ovh-ux/manager-config';
-// import ApplicationContext from '@/context/application.context';
-// import { useCookies } from 'react-cookie';
+import { render, waitFor } from '@testing-library/react';
+import { it, vi, describe, expect } from 'vitest';
+import { initShell } from '@ovh-ux/shell';
+import CookiePolicy from './CookiePolicy';
+import { Environment, User } from '@ovh-ux/manager-config';
+import ApplicationContext from '@/context/application.context';
+import { useCookies } from 'react-cookie';
 
-// const onValidate = vi.fn();
-// const trackingInit = vi.fn().mockResolvedValue(undefined);
-// const trackingSetEnabled = vi.fn().mockResolvedValue(undefined);
-// const trackingOnConsentModalDisplay = vi.fn().mockResolvedValue(undefined);
+const onValidate = vi.fn();
+const trackingInit = vi.fn().mockResolvedValue(undefined);
+const trackingSetEnabled = vi.fn().mockResolvedValue(undefined);
+const trackingOnConsentModalDisplay = vi.fn().mockResolvedValue(undefined);
 
-// const renderCookiePolicy = async () => {
-//   const shell = initShell();
-//   const environment = shell.getPlugin('environment').getEnvironment();
-//   render(
-//     <ApplicationContext.Provider value={{ shell, environment }}>
-//       <CookiePolicy onValidate={onValidate} shell={shell} />
-//     </ApplicationContext.Provider>
-//   );
-// };
+vi.mock('@ovh-ux/shell');
 
-// const mockedShell = (region: string) => ({
-//   getPlugin: (plugin: string) => ({
-//     environment: {
-//       getEnvironment: () => ({
-//         user: {
-//           ovhSubsidiary: region,
-//         } as User,
-//         getRegion: () => region,
-//       } as Environment),
-//     },
-//     tracking: {
-//       setRegion: vi.fn(),
-//       init: trackingInit,
-//       setEnabled: trackingSetEnabled,
-//       onConsentModalDisplay: trackingOnConsentModalDisplay,
-//     },
-//   }[plugin]),
-// });
+const renderCookiePolicy = async () => {
+  const shell = initShell({} as Environment);
+  const environment = shell.getPlugin('environment').getEnvironment();
+  render(
+    <ApplicationContext.Provider value={{ shell, environment }}>
+      <CookiePolicy onValidate={onValidate} shell={shell} />
+    </ApplicationContext.Provider>
+  );
+};
 
-// vi.mock('@ovh-ux/shell');
-// vi.mock('react-cookie');
+const mockedShell = (region: string) => ({
+  getPlugin: (plugin: string) => ({
+    environment: {
+      getEnvironment: () => ({
+        user: {
+          ovhSubsidiary: region,
+        } as User,
+        getRegion: () => region,
+      } as Environment),
+    },
+    tracking: {
+      setRegion: vi.fn(),
+      init: trackingInit,
+      setEnabled: trackingSetEnabled,
+      onConsentModalDisplay: trackingOnConsentModalDisplay,
+    },
+  }[plugin]),
+});
+vi.mock('react-cookie');
 
 
-// describe('CookiePolicy.component', () => {
+describe('CookiePolicy.component', () => {
 
-//   afterEach(() => {
-//     vi.restoreAllMocks();
-//   });
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
 
-//   it.each([
-//     ['EU', 'valid'],
-//     ['US', 'invalid'],
-//   ])('should init tracking if region is %s and cookie is %s', async (region, cookieValidity) => {
+  it.each([
+    ['EU', 'valid'],
+    ['US', 'invalid'],
+  ])('should init tracking if region is %s and cookie is %s', async (region, cookieValidity) => {
 
-//     const cookieValue = cookieValidity === 'valid' ? '1' : '0';
-//     vi.mocked(useCookies).mockReturnValue([{ MANAGER_TRACKING: cookieValue, }, vi.fn(), vi.fn()]);
-//     (await import('@ovh-ux/shell')).initShell = vi.fn().mockResolvedValue(mockedShell(region));
-//     void renderCookiePolicy();
-//     await waitFor(() => {
-//       expect(trackingInit).toHaveBeenCalledWith(true);
-//       expect(trackingSetEnabled).not.toHaveBeenCalled();
-//       expect(trackingOnConsentModalDisplay).not.toHaveBeenCalled();
-//     }, { timeout: 2000 });
-//   });
+    const cookieValue = cookieValidity === 'valid' ? '1' : '0';
+    vi.mocked(useCookies).mockReturnValue([{ MANAGER_TRACKING: cookieValue, }, vi.fn(), vi.fn()]);
+    (await import('@ovh-ux/shell')).initShell = vi.fn().mockReturnValue(mockedShell(region));
+    void renderCookiePolicy();
+    await waitFor(() => {
+      expect(trackingInit).toHaveBeenCalledWith(true);
+      expect(trackingSetEnabled).not.toHaveBeenCalled();
+      expect(trackingOnConsentModalDisplay).not.toHaveBeenCalled();
+    }, { timeout: 2000 });
+  });
 
-//   it('should show consent modal if cookie is null and region is not US', async () => {
+  it('should show consent modal if cookie is null and region is not US', async () => {
 
-//     vi.mocked(useCookies).mockReturnValue([{ MANAGER_TRACKING: null, }, vi.fn(), vi.fn()]);
-//     (await import('@ovh-ux/shell')).initShell = vi.fn().mockResolvedValue(mockedShell('EU'));
-//     void renderCookiePolicy();
-//     await waitFor(() => {
-//       expect(trackingInit).not.toHaveBeenCalled();
-//       expect(trackingSetEnabled).not.toHaveBeenCalled();
-//       expect(trackingOnConsentModalDisplay).toHaveBeenCalled();
-//     }, { timeout: 2000 });
-//   });
+    vi.mocked(useCookies).mockReturnValue([{ MANAGER_TRACKING: null, }, vi.fn(), vi.fn()]);
+    (await import('@ovh-ux/shell')).initShell = vi.fn().mockReturnValue(mockedShell('EU'));
+    void renderCookiePolicy();
+    await waitFor(() => {
+      expect(trackingInit).not.toHaveBeenCalled();
+      expect(trackingSetEnabled).not.toHaveBeenCalled();
+      expect(trackingOnConsentModalDisplay).toHaveBeenCalled();
+    }, { timeout: 2000 });
+  });
 
-//   it('should disable tracking plugin if cookie is invalid and region is not US', async () => {
+  it('should disable tracking plugin if cookie is invalid and region is not US', async () => {
 
-//     vi.mocked(useCookies).mockReturnValue([{ MANAGER_TRACKING: '0', }, vi.fn(), vi.fn()]);
-//     (await import('@ovh-ux/shell')).initShell = vi.fn().mockResolvedValue(mockedShell('EU'));
-//     void renderCookiePolicy();
-//     await waitFor(() => {
-//       expect(trackingInit).not.toHaveBeenCalled();
-//       expect(trackingSetEnabled).toHaveBeenCalledWith(false);
-//       expect(trackingOnConsentModalDisplay).not.toHaveBeenCalled();
-//     }, { timeout: 2000 });
-//   });
-// });
+    vi.mocked(useCookies).mockReturnValue([{ MANAGER_TRACKING: '0', }, vi.fn(), vi.fn()]);
+    (await import('@ovh-ux/shell')).initShell = vi.fn().mockReturnValue(mockedShell('EU'));
+    void renderCookiePolicy();
+    await waitFor(() => {
+      expect(trackingInit).not.toHaveBeenCalled();
+      expect(trackingSetEnabled).toHaveBeenCalledWith(false);
+      expect(trackingOnConsentModalDisplay).not.toHaveBeenCalled();
+    }, { timeout: 2000 });
+  });
+});
