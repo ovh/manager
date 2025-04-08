8c92ff65d6e73556ee02ecc2a6e01577bed1f2ec
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2025-04-08 13:26:47 +0530

    feat(ips): implement add organisation model
    ref: #MANAGER-17443
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

--- packages/manager/apps/ips/src/pages/listing/manageOrganisations/components/OrganisationsDatagrid/manageOrganisationsDataGrid.component.tsx
+++ packages/manager/apps/ips/src/pages/listing/manageOrganisations/components/OrganisationsDatagrid/manageOrganisationsDataGrid.component.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import { Outlet } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { OdsSpinner } from '@ovhcloud/ods-components/react';
 import { ODS_SPINNER_SIZE } from '@ovhcloud/ods-components';
@@ -19,59 +20,67 @@ export const ManageOrganisationsDatagrid: React.FC = () => {
   const { orgDetails, isLoading } = useGetOrganisationsDetails({
     enabled: true,
   });
-  const columns: DatagridColumn<OrgDetails>[] = [
-    {
-      id: 'organisationId',
-      label: t('manageOrganisationsTabOrganisaton'),
-      cell: ({ organisationId }: OrgDetails) => (
-        <DataGridTextCell>{organisationId}</DataGridTextCell>
-      ),
-    },
-    {
-      id: 'type',
-      label: t('manageOrganisationsTabType'),
-      cell: ({ registry }: OrgDetails) => (
-        <DataGridTextCell>{registry}</DataGridTextCell>
-      ),
-    },
-    {
-      id: 'name',
-      label: t('manageOrganisationsTabSurname'),
-      cell: (org: OrgDetails) => (
-        <DataGridTextCell>
-          {`${org?.firstname} ${org?.lastname}`}
-        </DataGridTextCell>
-      ),
-    },
-    {
-      id: 'email',
-      label: t('manageOrganisationsTabEmail'),
-      cell: (org: OrgDetails) => (
-        <DataGridTextCell>{org?.abuse_mailbox}</DataGridTextCell>
-      ),
-    },
-    {
-      id: 'phone',
-      label: t('manageOrganisationsTabPhone'),
-      cell: ({ phone }: OrgDetails) => (
-        <DataGridTextCell>{phone}</DataGridTextCell>
-      ),
-    },
-    {
-      id: 'address',
-      label: t('manageOrganisationsTabAddress'),
-      cell: ({ address }: OrgDetails) => (
-        <DataGridTextCell>{address}</DataGridTextCell>
-      ),
-    },
-    {
-      id: 'action',
-      label: '',
-      cell: (org: OrgDetails) => (
-        <OrganisationsActionsCell org={org}></OrganisationsActionsCell>
-      ),
-    },
-  ];
+
+  const DatagridActionCell = (org: OrgDetails) => {
+    return (
+      <DataGridTextCell>
+        <OrganisationsActionsCell {...org} />
+      </DataGridTextCell>
+    );
+  };
+  const columns: DatagridColumn<OrgDetails>[] = useMemo(() => {
+    return [
+      {
+        id: 'organisationId',
+        label: t('manageOrganisationsTabOrganisaton'),
+        cell: ({ organisationId }: OrgDetails) => (
+          <DataGridTextCell>{organisationId}</DataGridTextCell>
+        ),
+      },
+      {
+        id: 'type',
+        label: t('manageOrganisationsTabType'),
+        cell: ({ registry }: OrgDetails) => (
+          <DataGridTextCell>{registry}</DataGridTextCell>
+        ),
+      },
+      {
+        id: 'name',
+        label: t('manageOrganisationsTabSurname'),
+        cell: (org: OrgDetails) => (
+          <DataGridTextCell>
+            {`${org?.firstname} ${org?.lastname}`}
+          </DataGridTextCell>
+        ),
+      },
+      {
+        id: 'email',
+        label: t('manageOrganisationsTabEmail'),
+        cell: (org: OrgDetails) => (
+          <DataGridTextCell>{org?.abuse_mailbox}</DataGridTextCell>
+        ),
+      },
+      {
+        id: 'phone',
+        label: t('manageOrganisationsTabPhone'),
+        cell: ({ phone }: OrgDetails) => (
+          <DataGridTextCell>{phone}</DataGridTextCell>
+        ),
+      },
+      {
+        id: 'address',
+        label: t('manageOrganisationsTabAddress'),
+        cell: ({ address }: OrgDetails) => (
+          <DataGridTextCell>{address}</DataGridTextCell>
+        ),
+      },
+      {
+        id: 'action',
+        label: '',
+        cell: DatagridActionCell,
+      },
+    ];
+  }, []);
 
   const loadMoreOrgs = () => {
     setNumberOfPageDisplayed(numberOfPageDisplayed + 1);
@@ -89,17 +98,20 @@ export const ManageOrganisationsDatagrid: React.FC = () => {
           <OdsSpinner size={ODS_SPINNER_SIZE.lg} />
         </div>
       ) : (
-        <Datagrid
-          className="pb-[200px] -mx-6"
-          columns={columns}
-          items={paginatedOrgList}
-          totalItems={orgDetails?.length}
-          numberOfLoadingRows={10}
-          onFetchNextPage={loadMoreOrgs}
-          hasNextPage={numberOfPageDisplayed * pageSize < orgDetails?.length}
-          resetExpandedRowsOnItemsChange={true}
-          noResultLabel={t('manageOrganisationsTabNodata')}
-        />
+        <>
+          <Datagrid
+            className="pb-[200px] -mx-6"
+            columns={columns}
+            items={paginatedOrgList}
+            totalItems={orgDetails?.length}
+            numberOfLoadingRows={10}
+            onFetchNextPage={loadMoreOrgs}
+            hasNextPage={numberOfPageDisplayed * pageSize < orgDetails?.length}
+            resetExpandedRowsOnItemsChange={true}
+            noResultLabel={t('manageOrganisationsTabNodata')}
+          />
+          <Outlet />
+        </>
       )}
     </>
   );
