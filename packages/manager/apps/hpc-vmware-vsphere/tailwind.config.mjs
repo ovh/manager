52a09526778eeb3a15c37c93a95776c363a8e451
David Arsène <david.arsene.ext@ovhcloud.com>
2025-04-08 16:10:45 +0200

    feat(hpc-vmware-vsphere): init new µapp vmware vsphere
    resolves: #MANAGER-17690
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

---
+++ packages/manager/apps/hpc-vmware-vsphere/tailwind.config.mjs
@@ -0,0 +1,25 @@
+import path from 'path';
+import { createRequire } from 'node:module';
+
+const require = createRequire(import.meta.url);
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    path.join(
+      path.dirname(require.resolve('@ovh-ux/manager-react-components')),
+      '**/*.{js,jsx,ts,tsx}',
+    ),
+  ],
+  theme: {
+    screens: {
+      xs: '0',
+      sm: '36em',
+      md: '48em',
+      lg: '62em',
+      xl: '75em',
+      xxl: '87.5em',
+    },
+  },
+};
