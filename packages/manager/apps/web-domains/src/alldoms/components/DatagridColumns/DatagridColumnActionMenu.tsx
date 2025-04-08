dae6cb24fd829fa4eef9c440631de053c959c8d1
Louis BENSI <louis.bensi.ext@corp.ovh.com>
2025-04-08 16:29:27 +0200

    feat(web-domains): action technical implementation
    ref: #MANAGER-17814
    
    Signed-off-by: Louis BENSI <louis.bensi.ext@corp.ovh.com>

--- packages/manager/apps/web-domains/src/alldoms/components/DatagridColumns/DatagridColumnActionMenu.tsx
+++ packages/manager/apps/web-domains/src/alldoms/components/DatagridColumns/DatagridColumnActionMenu.tsx
@@ -1,6 +1,8 @@
 import { ActionMenu } from '@ovh-ux/manager-react-components';
+import { useNavigationGetUrl } from '@ovh-ux/manager-react-shell-client';
 import { ODS_BUTTON_VARIANT } from '@ovhcloud/ods-components';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 interface DatagridColumnActionMenuProps {
   readonly serviceId: string;
@@ -9,12 +11,25 @@ interface DatagridColumnActionMenuProps {
 export default function DatagridColumnActionMenu({
   serviceId,
 }: DatagridColumnActionMenuProps) {
+  const { t } = useTranslation('allDom');
+  const { data: url } = useNavigationGetUrl([
+    'cgi-bin',
+    `/order/renew.cgi?domainChooser=${serviceId}`,
+    {},
+  ]);
   return (
     <ActionMenu
       id={serviceId}
       isCompact
       variant={ODS_BUTTON_VARIANT.ghost}
-      items={[]}
+      items={[
+        {
+          id: 1,
+          label: t('allDom_table_action_renew'),
+          href: url as string,
+          target: '_blank',
+        },
+      ]}
     />
   );
 }
