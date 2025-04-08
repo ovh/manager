19f6dca4b17c5968c7a96a3c0b66fd5e5f0529a8
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2025-04-08 13:26:47 +0530

    feat(ips): implement add organisation model
    ref: #MANAGER-17443
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

--- packages/manager/apps/ips/src/pages/listing/manageOrganisations/manage.organisations.page.tsx
+++ packages/manager/apps/ips/src/pages/listing/manageOrganisations/manage.organisations.page.tsx
@@ -9,12 +9,14 @@ import {
 } from '@ovhcloud/ods-components';
 import Loading from '@/components/Loading/Loading';
 import { ManageOrganisationsDatagrid } from '@/pages/listing/manageOrganisations/components/OrganisationsDatagrid/manageOrganisationsDataGrid.component';
+import { urls } from '@/routes/routes.constant';
+import { ManageOrgListingContextProvider } from './manage.organisations.context';
 
 export default function ManageOrganisationsPage() {
   const { t } = useTranslation('manage-organisations');
   const navigate = useNavigate();
   return (
-    <>
+    <ManageOrgListingContextProvider>
       <div className="flex flex-col">
         <OdsText preset={ODS_TEXT_PRESET.heading3} className="mb-4">
           {t('manageOrganisationsTabDescription')}
@@ -25,13 +27,13 @@ export default function ManageOrganisationsPage() {
           className="mb-5 mr-2"
           variant={ODS_BUTTON_VARIANT.outline}
           icon={ODS_ICON_NAME.plus}
-          onClick={() => navigate('')}
+          onClick={() => navigate(urls.openOrganisationsModel)}
           label={t('manageOrganisationsOrderButtonLabel')}
         />
       </div>
       <React.Suspense fallback={<Loading />}>
         <ManageOrganisationsDatagrid />
       </React.Suspense>
-    </>
+    </ManageOrgListingContextProvider>
   );
 }
