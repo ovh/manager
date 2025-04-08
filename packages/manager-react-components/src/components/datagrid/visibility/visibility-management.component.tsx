ecbc2391d2cee620ca679fb23b8554ed8596e34d
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2025-04-08 10:00:38 +0200

    fix(mrc): fix the issue on count visibility columns
    ref: #MANAGER-17732
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager-react-components/src/components/datagrid/visibility/visibility-management.component.tsx
+++ packages/manager-react-components/src/components/datagrid/visibility/visibility-management.component.tsx
@@ -32,12 +32,24 @@ export function VisibilityManagement({
 }: Readonly<ColumnsVisibilityProps>) {
   const { t } = useTranslation('datagrid');
   const visibilityPopoverRef = useRef(null);
-  const numberVisibleColumns = columnsVisibility?.filter((col) =>
-    col.isVisible(),
-  ).length;
-  const disabledVisibleColumns = columnsVisibility?.filter(
-    (col) => col.enableHiding === false,
-  ).length;
+  const columnsIncludedInCounter = columnsVisibility.filter(
+    (column) => !['expander'].includes(column.id) && column.label !== '',
+  );
+
+  const columnsVisibilityState = columnsIncludedInCounter.reduce(
+    (acc, current) => {
+      if (current.isVisible()) {
+        acc.enabled += 1;
+      }
+      acc.haveToDisplay = acc.enabled < acc.total;
+      return acc;
+    },
+    {
+      enabled: 0,
+      total: columnsIncludedInCounter.length,
+      haveToDisplay: false,
+    },
+  );
 
   return (
     <>
@@ -46,12 +58,12 @@ export function VisibilityManagement({
         slot="datagrid-visibility-popover-trigger"
         data-testid="datagrid-topbar-visibility-button"
         size={ODS_BUTTON_SIZE.sm}
-        variant={ODS_BUTTON_VARIANT.ghost}
+        variant={ODS_BUTTON_VARIANT.outline}
         icon={ODS_ICON_NAME.columns}
         aria-label={t('common_topbar_columns')}
         label={`${t('common_topbar_columns')} ${
-          numberVisibleColumns < columnsVisibility.length
-            ? `(${numberVisibleColumns - disabledVisibleColumns})`
+          columnsVisibilityState.haveToDisplay
+            ? `(${columnsVisibilityState.enabled})`
             : ''
         }`}
       />
@@ -61,27 +73,25 @@ export function VisibilityManagement({
         with-arrow
       >
         <div className="flex flex-col">
-          {columnsVisibility
-            .filter(({ label }) => label !== '')
-            .map((column) => (
-              <OdsFormField key={column.id}>
-                <div className="flex flex-row items-center gap-x-2">
-                  <OdsCheckbox
-                    name={column.id}
-                    inputId={column.id}
-                    isDisabled={column.isDisabled}
-                    isChecked={column.isVisible()}
-                    onOdsChange={column.onChange}
-                    ariaLabel={column.label}
-                  />
-                  <label slot="label" htmlFor={column.id}>
-                    <OdsText preset={ODS_TEXT_PRESET.paragraph}>
-                      {column.label}
-                    </OdsText>
-                  </label>
-                </div>
-              </OdsFormField>
-            ))}
+          {columnsIncludedInCounter.map((column) => (
+            <OdsFormField key={column.id}>
+              <div className="flex flex-row items-center gap-x-2">
+                <OdsCheckbox
+                  name={column.id}
+                  inputId={column.id}
+                  isDisabled={column.isDisabled}
+                  isChecked={column.isVisible()}
+                  onOdsChange={column.onChange}
+                  ariaLabel={column.label}
+                />
+                <label slot="label" htmlFor={column.id}>
+                  <OdsText preset={ODS_TEXT_PRESET.paragraph}>
+                    {column.label}
+                  </OdsText>
+                </label>
+              </div>
+            </OdsFormField>
+          ))}
         </div>
       </OdsPopover>
     </>
