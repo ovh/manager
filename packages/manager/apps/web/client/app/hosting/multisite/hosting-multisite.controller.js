745cf3ccf34a7b1ae65e1861a45f037486d7e2b5
Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>
2025-04-10 10:48:43 +0200

    feat(web-hosting): replace actions on multisite
    ref: #MANAGER-17845
    
    Signed-off-by: Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>

--- packages/manager/apps/web/client/app/hosting/multisite/hosting-multisite.controller.js
+++ packages/manager/apps/web/client/app/hosting/multisite/hosting-multisite.controller.js
@@ -273,9 +273,11 @@ angular
               }),
             );
 
-            return hostingSSLCertificate.retrievingLinkedDomains(
-              $stateParams.productId,
-            );
+            return !$scope.hosting.multipleSSL
+              ? hostingSSLCertificate.retrievingLinkedDomains(
+                  $stateParams.productId,
+                )
+              : [];
           })
           .then((sslLinked) => {
             const linkedSSLs = isArray(sslLinked) ? sslLinked : [sslLinked];
@@ -325,7 +327,11 @@ angular
             return null;
           })
           .then(() =>
-            hostingSSLCertificate.retrievingCertificate($stateParams.productId),
+            !$scope.hosting.multipleSSL
+              ? hostingSSLCertificate.retrievingCertificate(
+                  $stateParams.productId,
+                )
+              : {},
           )
           .then((certificate) => {
             if (certificate.status === HOSTING_STATUS.REGENERATING) {
