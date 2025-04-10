9951480ad29b0e98246c8ae5d6ade3b904b3f806
Louis BENSI <louis.bensi.ext@corp.ovh.com>
2025-04-10 12:59:58 +0200

    fix(web-ongoing-operations): redirection on DNS column
    ref: #MANAGER-17821
    
    Signed-off-by: Louis BENSI <louis.bensi.ext@corp.ovh.com>

---
+++ packages/manager/apps/web-ongoing-operations/src/components/OngoingOperationDatagrid/OngoingOperationDatagridDomain.tsx
@@ -0,0 +1,24 @@
+import { DataGridTextCell } from '@ovh-ux/manager-react-components';
+import { OdsLink } from '@ovhcloud/ods-components/react';
+import React from 'react';
+import { OngoingOperationDatagridDomainProps } from '@/types';
+import { useDatagridColumnUrl } from '@/hooks/url/useDatagridColumnUrl';
+
+export default function OngoingOperationDatagridDomain({
+  parent,
+  props,
+}: OngoingOperationDatagridDomainProps) {
+  const { url, value } = useDatagridColumnUrl({ parent, props });
+
+  return (
+    <DataGridTextCell>
+      <OdsLink
+        href={url}
+        label={value}
+        target="_blank"
+        data-testid={value}
+        isDisabled={!url}
+      />
+    </DataGridTextCell>
+  );
+}
