b85ad4224da87af95b183b0042c12c6f5a37644b
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-04-11 14:55:30 +0200

    feat(ips): improve ipv6 order workflow
    ref: #MANAGER-17651
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

--- packages/manager/apps/ips/src/pages/order/Ipv6Order.component.tsx
+++ packages/manager/apps/ips/src/pages/order/Ipv6Order.component.tsx
@@ -5,20 +5,33 @@ import { IpVersion } from './order.constant';
 import { RegionSelectionSection } from './sections/RegionSelectionSection.component';
 import { OrderButtonSection } from './sections/OrderButtonSection.component';
 import { AdditionalOptionsSection } from './sections/AdditionalOptionsSection.component';
+import { useCheckServiceAvailability } from '@/data/hooks/useCheckServiceAvailability';
 
 export const Ipv6Order: React.FC = () => {
   const {
     ipVersion,
     selectedService,
+    selectedServiceType,
     selectedRegion,
     selectedOptions,
+    addDisabledService,
   } = React.useContext(OrderContext);
+
+  const { serviceStatus } = useCheckServiceAvailability({
+    serviceName: selectedService,
+    serviceType: selectedServiceType,
+    onServiceUnavailable: addDisabledService,
+  });
 
   const visibleSections = {
     ipv6Options: ipVersion === IpVersion.ipv6,
     service: selectedOptions.length > 0,
-    region: !!selectedService,
-    order: selectedOptions.length > 0 && !!selectedService && !!selectedRegion,
+    region: !!selectedService && serviceStatus === 'ok',
+    order:
+      selectedOptions.length > 0 &&
+      !!selectedService &&
+      !!selectedRegion &&
+      serviceStatus === 'ok',
   };
 
   if (ipVersion !== IpVersion.ipv6) {
