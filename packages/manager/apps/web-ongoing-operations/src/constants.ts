14537a9c21ae96a46251b81556cff4cde9f2c969
Louis BENSI <louis.bensi.ext@corp.ovh.com>
2025-04-11 18:19:12 +0200

    fix(web-ongoing-operations): disable link on domain column
    ref: #MANAGER-17833
    
    Signed-off-by: Louis BENSI <louis.bensi.ext@corp.ovh.com>

--- packages/manager/apps/web-ongoing-operations/src/constants.ts
+++ packages/manager/apps/web-ongoing-operations/src/constants.ts
@@ -1,2 +1,5 @@
 export const taskMeDomain = '/me/task/domain';
 export const taskMeDns = '/me/task/dns';
+
+export const domainCreate = 'DomainCreate';
+export const domainIncomingTransfert = 'DomainIncomingTransfer';
