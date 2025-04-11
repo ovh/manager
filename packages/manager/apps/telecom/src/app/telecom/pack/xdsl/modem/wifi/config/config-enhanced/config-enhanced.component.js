41dbf29346021f8d84bb034b0edf3244406eb00c
aderghamov <alexandre.dergham.ext@ovhcloud.com>
2025-04-11 17:10:38 +0200

    feat(xdsl-modem-wifi): introducing the new wifi configuration page
    ref:#UXCT-724
    
    Signed-off-by: aderghamov <alexandre.dergham.ext@ovhcloud.com>

---
+++ packages/manager/apps/telecom/src/app/telecom/pack/xdsl/modem/wifi/config/config-enhanced/config-enhanced.component.js
@@ -0,0 +1,11 @@
+import controller from './config-enhanced.controller';
+import template from './config-enhanced.html';
+
+export default {
+  controller,
+  template,
+  controllerAs: 'ConfigWifiEnhancedCtrl',
+  bindings: {
+    serviceName: '<',
+  },
+};
