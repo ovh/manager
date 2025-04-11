8414414f27a84fbfa4641b9183e3ea1168651602
Louis BENSI <louis.bensi.ext@corp.ovh.com>
2025-04-11 18:19:12 +0200

    fix(web-ongoing-operations): disable link on domain column
    ref: #MANAGER-17833
    
    Signed-off-by: Kylian Bainvel <kylian.bainvel@ovhcloud.com>
    Co-authored-by: Louis BENSI <louis.bensi.ext@corp.ovh.com>

--- packages/manager/apps/web-ongoing-operations/src/components/OngoingOperationDatagrid/OngoingOperationDatagridDomain.tsx
+++ packages/manager/apps/web-ongoing-operations/src/components/OngoingOperationDatagrid/OngoingOperationDatagridDomain.tsx
@@ -3,11 +3,12 @@ import { OdsLink } from '@ovhcloud/ods-components/react';
 import React from 'react';
 import { OngoingOperationDatagridDomainProps } from '@/types';
 import { useDatagridColumnUrl } from '@/hooks/url/useDatagridColumnUrl';
+import { domainCreate, domainIncomingTransfert } from '@/constants';
 
 export default function OngoingOperationDatagridDomain({
   parent,
   props,
-}: OngoingOperationDatagridDomainProps) {
+}: Readonly<OngoingOperationDatagridDomainProps>) {
   const { url, value } = useDatagridColumnUrl({ parent, props });
 
   return (
@@ -17,7 +18,10 @@ export default function OngoingOperationDatagridDomain({
         label={value}
         target="_blank"
         data-testid={value}
-        isDisabled={!url}
+        isDisabled={
+          !url ||
+          [domainCreate, domainIncomingTransfert].includes(props.function)
+        }
       />
     </DataGridTextCell>
   );
