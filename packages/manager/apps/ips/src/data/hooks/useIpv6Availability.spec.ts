406dae0b901e0f881dc64dbb3cbc66eb9041cf17
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-04-11 14:55:30 +0200

    feat(ips): improve ipv6 order workflow
    ref: #MANAGER-17651
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

---
+++ packages/manager/apps/ips/src/data/hooks/useIpv6Availability.spec.ts
@@ -0,0 +1,72 @@
+import { IpObject } from '@/types/ipObject';
+import { IpTypeEnum } from '../api';
+import { getUnavailableRegionList } from './useIpv6Availability';
+
+const failoverIp: IpObject = {
+  ip: '2607:5300:400:1e00::/56',
+  iam: {
+    id: 'id1',
+    urn: 'urn:v1:eu:resource:ip:1',
+  },
+  rir: 'RIPE',
+  type: IpTypeEnum.ADDITIONAL,
+  campus: '',
+  country: 'fr',
+  regions: ['eu-west-rbx'],
+  version: 6,
+  routedTo: {
+    serviceName: null,
+  },
+  description: '',
+  bringYourOwnIp: false,
+  isAdditionalIp: true,
+  organisationId: '',
+  canBeTerminated: true,
+};
+
+const ip: IpObject = {
+  ip: '2607:5300:400:1e00::/56',
+  iam: {
+    id: 'id1',
+    urn: 'urn:v1:eu:resource:ip:1',
+  },
+  rir: 'RIPE',
+  type: IpTypeEnum.VRACK,
+  campus: '',
+  country: 'fr',
+  regions: ['eu-west-rbx'],
+  version: 6,
+  routedTo: {
+    serviceName: 'pn-00001',
+  },
+  description: '',
+  bringYourOwnIp: false,
+  isAdditionalIp: true,
+  organisationId: '',
+  canBeTerminated: true,
+};
+
+describe('getUnavailableRegionList', () => {
+  it('disable a region if there is already 3 ipv6 block on that region', () => {
+    expect(
+      getUnavailableRegionList(
+        [failoverIp, failoverIp, failoverIp],
+        'pn-00001',
+      ),
+    ).toEqual(['eu-west-rbx']);
+  });
+
+  it('doest not disable a region if there is less than 3 ipv6 block on that region', () => {
+    expect(
+      getUnavailableRegionList([failoverIp, failoverIp], 'pn-00001'),
+    ).toEqual([]);
+  });
+
+  it('disable a region if there is already an ip block on this region for this service', () => {
+    expect(getUnavailableRegionList([ip], 'pn-00001')).toEqual(['eu-west-rbx']);
+  });
+
+  it('does not disable a region if there is already an ip block on this region for a different service', () => {
+    expect(getUnavailableRegionList([ip], 'pn-00002')).toEqual([]);
+  });
+});
