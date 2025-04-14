9c399bd8061f500a7848bb8852c48fa62679c59e
Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>
2025-04-14 17:06:53 +0200

    feat(web-hosting): add ssl certificate selector enabler
    ref: #MANAGER-17764
    
    Signed-off-by: Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>

--- packages/manager/apps/web-hosting/src/data/api/ssl.ts
+++ packages/manager/apps/web-hosting/src/data/api/ssl.ts
@@ -18,3 +18,28 @@ export const createCertificate = async (
 
   return data;
 };
+
+export const createDomainCertificate = async (
+  serviceName: string,
+  domain: string,
+): Promise<TCertificate> => {
+  const { data } = await v6.post<TCertificate>(
+    `/hosting/web/${serviceName}/attachedDomain/${domain}/ssl`,
+  );
+
+  return data;
+};
+
+export const createDomainCertificates = async (
+  serviceName: string,
+  domains: string[],
+): Promise<PromiseSettledResult<TCertificate>[]> => {
+  const results = await Promise.allSettled(
+    domains?.map((domain) => createDomainCertificate(serviceName, domain)),
+  );
+  const failed = results?.find((r) => r.status === 'rejected');
+  if (failed) {
+    throw failed.reason ?? new Error('Domain certificate creation failed');
+  }
+  return results;
+};
