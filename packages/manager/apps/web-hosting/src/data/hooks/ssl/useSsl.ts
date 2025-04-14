552768d16047015df49d50a732b76f619404c754
Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>
2025-04-14 17:06:53 +0200

    feat(web-hosting): add ssl certificate selector enabler
    ref: #MANAGER-17764
    
    Signed-off-by: Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>

--- packages/manager/apps/web-hosting/src/data/hooks/ssl/useSsl.ts
+++ packages/manager/apps/web-hosting/src/data/hooks/ssl/useSsl.ts
@@ -1,6 +1,10 @@
 import { useMutation } from '@tanstack/react-query';
 import { ApiError } from '@ovh-ux/manager-core-api';
-import { createCertificate } from '../../api/ssl';
+import {
+  createCertificate,
+  createDomainCertificate,
+  createDomainCertificates,
+} from '../../api/ssl';
 
 export const useCreateCertificate = (
   serviceName: string,
@@ -23,6 +27,42 @@ export const useCreateCertificate = (
 
   return {
     createCertificate: mutation.mutate,
+    ...mutation,
+  };
+};
+
+export const useCreateDomainCertificate = (
+  serviceName: string,
+  onSuccess: () => void,
+  onError: (error: ApiError) => void,
+) => {
+  const mutation = useMutation({
+    mutationFn: (domain: string) =>
+      createDomainCertificate(serviceName, domain),
+    onSuccess,
+    onError,
+  });
+
+  return {
+    createDomainCertificate: mutation.mutate,
+    ...mutation,
+  };
+};
+
+export const useCreateDomainCertificates = (
+  serviceName: string,
+  onSuccess: () => void,
+  onError: (error: ApiError) => void,
+) => {
+  const mutation = useMutation({
+    mutationFn: (domains: string[]) =>
+      createDomainCertificates(serviceName, domains),
+    onSuccess,
+    onError,
+  });
+
+  return {
+    createDomainCertificates: mutation.mutate,
     ...mutation,
   };
 };
