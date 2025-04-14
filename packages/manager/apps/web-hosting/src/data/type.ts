1ed7c5fdce34fe1e5df025d3fbd751480bf28a7f
Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>
2025-04-14 16:05:18 +0200

    feat(web-hosting): create react layout and tabs
    ref: #MANAGER-17888
    
    Signed-off-by: Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>

--- packages/manager/apps/web-hosting/src/data/type.ts
+++ packages/manager/apps/web-hosting/src/data/type.ts
@@ -1,3 +1,5 @@
+import { CurrencyCode } from '@ovh-ux/manager-react-components';
+
 export enum ResourceStatus {
   CREATING = 'CREATING',
   DELETING = 'DELETING',
@@ -88,4 +90,262 @@ export type WebsiteType = {
     status?: TaskStatus | null;
     type?: TaskType;
   }[];
+};
+
+export enum HostingState {
+  ACTIVE = 'active',
+  BLOQUED = 'bloqued',
+  MAINTENANCE = 'maintenance',
+}
+
+export enum HostingRessourceType {
+  BEST_EFFORT = 'bestEffort',
+  CLOUD = 'cloud',
+  DEDICATED = 'dedicated',
+  SHARED = 'shared',
+}
+
+export enum HostingdOffer {
+  CLOUDWEB_1 = 'CLOUDWEB_1',
+  CLOUDWEB_2 = 'CLOUDWEB_2',
+  CLOUDWEB_3 = 'CLOUDWEB_3',
+  KS = 'KS',
+  PERFORMANCE_1 = 'PERFORMANCE_1',
+  PERFORMANCE_2 = 'PERFORMANCE_2',
+  PERFORMANCE_3 = 'PERFORMANCE_3',
+  PERFORMANCE_4 = 'PERFORMANCE_4',
+  PERSO = 'PERSO',
+  POWER_BETA_1 = 'POWER_BETA_1',
+  PRO = 'PRO',
+  START = 'START',
+}
+
+export enum HostingCountries {
+  BE = 'BE',
+  CA = 'CA',
+  CZ = 'CZ',
+  DE = 'DE',
+  ES = 'ES',
+  FI = 'FI',
+  FR = 'FR',
+  IE = 'IE',
+  IT = 'IT',
+  LT = 'LT',
+  NL = 'NL',
+  PL = 'PL',
+  PT = 'PT',
+  UK = 'UK',
+}
+
+export enum PhpVersionsSupport {
+  BETA = 'BETA',
+  END_OF_LIFE = 'END_OF_LIFE',
+  SECURITY_FIXES = 'SECURITY_FIXES',
+  SUPPORTED = 'SUPPORTED',
+}
+
+export type WebHostingType = {
+  availableBoostOffer: {
+    offer: HostingdOffer;
+    price: {
+      currencyCode: CurrencyCode;
+      priceInUcents?: number;
+      text: string;
+      value: number;
+    };
+  }[];
+  boostOffer?: string;
+  cluster: string;
+  clusterIp?: string;
+  clusterIpv6?: string;
+  countriesIp: {
+    country: HostingCountries;
+    ip?: string;
+    ipv6?: string;
+  }[];
+  datacenter: string;
+  defaultAttachedDomain?: string;
+  displayName?: string;
+  filer?: string;
+  hasCdn?: boolean;
+  hasHostedSsl?: boolean;
+  home: string;
+  hostingIp?: string;
+  hostingIpv6?: string;
+  iam?: {
+    displayName?: string;
+    id: string;
+    tags?: {
+      'any-key': string;
+    };
+    urn: string;
+  };
+  lastOvhConfigScan?: string;
+  multipleSSL: boolean;
+  offer: string;
+  operatingSystem: string;
+  phpVersions: {
+    support: PhpVersionsSupport;
+    version: string;
+  }[];
+  primaryLogin: string;
+  quotaSize: {
+    unit: string;
+    value: number;
+  };
+  quotaUsed: {
+    unit: string;
+    value: number;
+  };
+  recommendedOffer: HostingdOffer;
+  resourceType: HostingRessourceType;
+  serviceManagementAccess: {
+    ftp: {
+      port: number;
+      url: string;
+    };
+    http: {
+      port: number;
+      url: string;
+    };
+    ssh: {
+      port: number;
+      url: string;
+    };
+  };
+  serviceName: string;
+  state: HostingState;
+  token?: string;
+  trafficQuotaSize?: {
+    unit: string;
+    value: number;
+  };
+  trafficQuotaUsed?: {
+    unit: string;
+    value: number;
+  };
+  updates: string[];
+};
+
+export type ServiceInfosType = {
+  canDeleteAtExpiration: boolean;
+  contactAdmin: string;
+  contactBilling: string;
+  contactTech: string;
+  creation: string;
+  domain: string;
+  engagedUpTo: string;
+  expiration: string;
+  possibleRenewPeriod: [0];
+  renew: {
+    automatic: boolean;
+    deleteAtExpiration: boolean;
+    forced: boolean;
+    manualPayment: boolean;
+    period: number;
+  };
+  renewalType: string;
+  serviceId: number;
+  status: string;
+};
+
+export enum DnssecState {
+  DISABLED = 'disabled',
+  ENABLED = 'enabled',
+  NOT_SUPPORTED = 'not_supported',
+}
+
+export enum NameServerType {
+  ANYCAST = 'anycast',
+  DEDICATED = 'dedicated',
+  EMPTY = 'empty',
+  EXTERNAL = 'external',
+  HOLD = 'hold',
+  HOSTED = 'hosted',
+  HOSTING = 'hosting',
+  MIXED = 'mixed',
+  PARKING = 'parking',
+}
+
+export enum DomainOffer {
+  DIAMOND = 'diamond',
+  GOLD = 'gold',
+  PLATINUM = 'platinum',
+}
+
+export enum DomainState {
+  EXPIRED = 'expired',
+  RESTORABLE = 'restorable',
+  DELETED = 'deleted',
+  PENDING_DELETE = 'pending_delete',
+  OUTGOING_TRANSFER = 'outgoing_transfer',
+  DISPUTE = 'dispute',
+  OK = 'ok',
+}
+
+export enum SuspensionState {
+  NOT_SUSPENDED = 'not_suspended',
+  SUSPENDED = 'suspended',
+}
+
+export enum TransferLockStatus {
+  LOCKED = 'locked',
+  LOCKING = 'locking',
+  UNAVAILABLE = 'unavailable',
+  UNLOCKED = 'unlocked',
+  UNLOCKING = 'unlocking',
+}
+
+export type DomainServiceType = {
+  contactAdmin: {
+    id: string;
+  };
+  contactBilling: {
+    id: string;
+  };
+  contactOwner: {
+    id: string;
+  };
+  contactTech: {
+    id: string;
+  };
+  dnssecState: DnssecState;
+  dnssecSupported: boolean;
+  domain: string;
+  expirationDate: string;
+  glueRecordIpv6Supported: boolean;
+  glueRecordMultiIpSupported: boolean;
+  hostSupported: boolean;
+  iam: {
+    displayName: string;
+    id: string;
+    tags: {
+      'any-key': string;
+    };
+    urn: string;
+  };
+  lastUpdate: string;
+  nameServerType: NameServerType;
+  nameServers: [
+    {
+      id: number;
+      ipv4: string;
+      ipv6: string;
+      nameServer: string;
+      nameServerType: NameServerType;
+    },
+  ];
+  offer: DomainOffer;
+  owoSupported: boolean;
+  parentService: {
+    name: string;
+    type: '/allDom';
+  };
+  renewalDate: string;
+  renewalState: string;
+  serviceId: number;
+  state: DomainState;
+  suspensionState: SuspensionState;
+  transferLockStatus: TransferLockStatus;
+  whoisOwner: string;
 };
---
+++ packages/manager/apps/web-hosting/src/utils/changelog.constants.ts
@@ -0,0 +1,9 @@
+import { ChangelogLinks } from '@ovh-ux/manager-react-components';
+
+export const CHANGELOG_LINKS: ChangelogLinks = {
+  changelog:
+    'https://github.com/orgs/ovh/projects/16/views/6?sliceBy%5Bvalue%5D=VMware+on+OVHcloud',
+  roadmap:
+    'https://github.com/orgs/ovh/projects/16/views/1?pane=info&sliceBy%5Bvalue%5D=VMware+on+OVHcloud',
+  'feature-request': 'https://github.com/ovh/private-cloud-roadmap/issues/new',
+};
