cd6084b67bbbfbd5ca16f9271f03068ca2caec28
Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>
2025-04-14 17:06:53 +0200

    feat(web-hosting): add ssl certificate selector enabler
    ref: #MANAGER-17764
    
    Signed-off-by: Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>

--- packages/manager/apps/web-hosting/src/components/topBar/TopBar.spec.tsx
+++ packages/manager/apps/web-hosting/src/components/topBar/TopBar.spec.tsx
@@ -3,10 +3,11 @@ import { render, screen } from '@testing-library/react';
 import { describe, it } from 'vitest';
 import '@testing-library/jest-dom';
 import Topbar from '@/components/topBar/TopBar.component';
+import { wrapper } from '@/utils/test.provider';
 
 describe('Topbar component', () => {
   it('should display Topbar component', async () => {
-    render(<Topbar />);
+    render(<Topbar />, { wrapper });
     const orderSsl = screen.getByTestId('order-ssl');
     const importrSsl = screen.getByTestId('import-ssl');
     expect(orderSsl).toBeInTheDocument();
