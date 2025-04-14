68629f5eee4aab89a8ef56839a5eb92ad4e0fcfa
Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>
2025-04-14 16:25:32 +0200

    feat(web-hosting): create dedicated modals for ssl import and creation
    ref: #MANAGER-17857
    
    Signed-off-by: Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>

--- packages/manager/apps/web-hosting/src/pages/dashboard/ssl/Ssl.page.tsx
+++ packages/manager/apps/web-hosting/src/pages/dashboard/ssl/Ssl.page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Notifications } from '@ovh-ux/manager-react-components';
 
+import Topbar from '@/components/topBar/TopBar.component';
 import Loading from '@/components/loading/Loading.component';
 
 export default function Ssl() {
   return (
     <React.Suspense fallback={<Loading />}>
       <Notifications />
+      <Topbar />
     </React.Suspense>
   );
 }
