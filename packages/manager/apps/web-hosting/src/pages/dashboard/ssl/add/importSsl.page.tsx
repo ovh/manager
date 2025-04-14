592177eb88348d8c08f0a02aab9818e0487f6217
Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>
2025-04-14 16:25:32 +0200

    feat(web-hosting): create dedicated modals for ssl import and creation
    ref: #MANAGER-17857
    
    Signed-off-by: Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>

---
+++ packages/manager/apps/web-hosting/src/pages/dashboard/ssl/add/importSsl.page.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import {
+  OdsFormField,
+  OdsText,
+  OdsTextarea,
+} from '@ovhcloud/ods-components/react';
+import { Modal, useNotifications } from '@ovh-ux/manager-react-components';
+
+import { useTranslation } from 'react-i18next';
+import { useCreateCertificate } from '@/data/hooks/ssl/useSsl';
+import { subRoutes, urls } from '@/routes/routes.constants';
+
+export default function ImportModal() {
+  const { serviceName } = useParams();
+
+  const { addSuccess, addWarning } = useNotifications();
+  const navigate = useNavigate();
+  const closeModal = () =>
+    navigate(urls.ssl.replace(subRoutes.serviceName, serviceName));
+  const [certificate, setCertificate] = useState('');
+  const [key, setKey] = useState('');
+  const [chain, setChain] = useState('');
+  const { t } = useTranslation('ssl');
+  const { createCertificate } = useCreateCertificate(
+    serviceName,
+    () => {
+      addSuccess(t('hosting_dashboard_ssl_order_success'), true);
+    },
+    () => {
+      addWarning(t('hosting_dashboard_ssl_order_error'), true);
+    },
+  );
+
+  const onConfirm = () => {
+    createCertificate({ certificate, key, chain });
+    closeModal();
+  };
+
+  return (
+    <Modal
+      onDismiss={closeModal}
+      isOpen
+      heading={t('import_ssl_certificate')}
+      primaryLabel={t('buttons_validate')}
+      secondaryLabel={t('buttons_cancel')}
+      onPrimaryButtonClick={onConfirm}
+      onSecondaryButtonClick={closeModal}
+    >
+      <div className="flex flex-col space-y-8 mb-10">
+        <OdsFormField className="w-full">
+          <div slot="label">
+            <OdsText className="block">
+              {t('ssl_order_manual_mode_certif')}
+            </OdsText>
+          </div>
+          <OdsTextarea
+            name="manualCertif"
+            isResizable
+            rows={5}
+            value={certificate}
+            onOdsChange={(e) => setCertificate(e.detail.value)}
+          />
+        </OdsFormField>
+        <OdsFormField className="w-full">
+          <div slot="label">
+            <OdsText className="block">
+              {t('ssl_order_manual_mode_key')}
+            </OdsText>
+          </div>
+          <OdsTextarea
+            name="modeKey"
+            isResizable
+            rows={5}
+            value={key}
+            onOdsChange={(e) => setKey(e.detail.value)}
+          />
+        </OdsFormField>
+        <OdsFormField className="w-full">
+          <div slot="label">
+            <OdsText className="block">
+              {t('ssl_order_manual_mode_chain')}
+            </OdsText>
+          </div>
+          <OdsTextarea
+            name="modeChain"
+            isResizable
+            rows={5}
+            value={chain}
+            onOdsChange={(e) => setChain(e.detail.value)}
+          />
+        </OdsFormField>
+      </div>
+    </Modal>
+  );
+}
