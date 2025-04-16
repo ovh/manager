8246be4be19cb9407d407c06ba11b13a4bf36d81
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2025-04-16 15:28:14 +0200

    feat(pci-gateway): display price in the deployment mode cards
    ref: #TAPC-3271
    
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

---
+++ packages/manager/apps/pci-gateway/src/api/hooks/useAddons/useAddons.ts
@@ -0,0 +1,57 @@
+import {
+  getCatalogQuery,
+  getProductAvailabilityQuery,
+} from '@ovh-ux/manager-pci-common';
+import { useQueries } from '@tanstack/react-query';
+import { TProductAddonDetail } from '@/types/product.type';
+
+type TUseAddonsParams = {
+  ovhSubsidiary: string;
+  projectId: string;
+  addonFamily: string;
+  select?: (addons: TProductAddonDetail[]) => TProductAddonDetail[];
+};
+
+export const useAddons = ({
+  ovhSubsidiary,
+  projectId,
+  addonFamily,
+  select,
+}: TUseAddonsParams) =>
+  useQueries({
+    queries: [
+      getCatalogQuery(ovhSubsidiary),
+      getProductAvailabilityQuery(projectId, ovhSubsidiary, {
+        addonFamily,
+      }),
+    ],
+    combine: ([
+      { data: catalog, isFetching: isCatalogFetching },
+      { data: plans, isFetching: isPlansFetching },
+    ]) => {
+      const addons = (plans?.plans ?? [])
+        .flatMap(({ code, regions }) => {
+          const addon = catalog?.addons.find(
+            ({ planCode }) => code === planCode,
+          );
+          return addon ? [{ ...addon, regions }] : [];
+        })
+        .filter((addon) =>
+          addon.pricings.find(({ intervalUnit }) =>
+            ['none', 'hour'].includes(intervalUnit),
+          ),
+        )
+        .map(({ product, pricings, blobs, regions }) => ({
+          size: product.split('-').pop(),
+          price: pricings[0].price,
+          technicalName: blobs?.technical?.name,
+          bandwidth: blobs?.technical?.bandwidth?.level,
+          regions,
+        }));
+
+      return {
+        addons: select ? select(addons) : addons,
+        isFetching: isCatalogFetching || isPlansFetching,
+      };
+    },
+  });
