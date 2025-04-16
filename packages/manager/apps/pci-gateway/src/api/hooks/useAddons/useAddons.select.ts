5d810c95a81aa08422e1385def4d880139d6fbc5
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2025-04-16 16:05:00 +0200

    feat(pci-gateway): display price in the deployment mode cards
    ref: #TAPC-3271
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

--- packages/manager/apps/pci-gateway/src/api/hooks/useAddons/useAddons.select.ts
+++ packages/manager/apps/pci-gateway/src/api/hooks/useAddons/useAddons.select.ts
@@ -2,6 +2,9 @@ import { TProductAddonDetail } from '@/types/product.type';
 
 const SIZE_ORDER = ['s', 'm', 'l', 'xl', '2xl'];
 
+export const sortProductByPrice = (products: TProductAddonDetail[]) =>
+  products.slice().sort((a, b) => a.price - b.price);
+
 export const filterProductRegionBySize = (
   products: TProductAddonDetail[],
   region: string,
