690d0dd788387a5577fd2cdf05ea86c921c64124
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2025-04-16 14:56:55 +0200

    feat(pci-volume-backup): add onboarding page implementation
    ref: #DTCORE-3258
    
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/apps/pci-volume-backup/src/tracking.constant.ts
+++ packages/manager/apps/pci-volume-backup/src/tracking.constant.ts
@@ -18,3 +18,52 @@ export const LEVEL2 = {
 export const UNIVERSE = 'PublicCloud';
 export const SUB_UNIVERSE = 'PublicCloud';
 export const APP_NAME = 'pci-volume-backup';
+export const VOLUME_BACKUP_BASE_TRACKING =
+  'pci::projects::project::storages::volume-backup';
+export const VOLUME_BACKUP_CTA_TRACKING = `PublicCloud::${VOLUME_BACKUP_BASE_TRACKING}`;
+export const GUIDES_STORAGES_VOLUME_BACKUP_OVERVIEW =
+  'storages_volume_backup_overview';
+
+export const VOLUME_BACKUP_TRACKING = {
+  GUIDES: 'public-cloud_volume_backup::guides::go_to',
+  ONBOARDING: {
+    PAGE: `${VOLUME_BACKUP_BASE_TRACKING}::onboarding`,
+    ADD: `${VOLUME_BACKUP_CTA_TRACKING}::onboarding::add`,
+    GUIDE: `${VOLUME_BACKUP_CTA_TRACKING}::onboarding::docs`,
+  },
+  CREATE: {
+    PAGE: `${VOLUME_BACKUP_BASE_TRACKING}::create-backup`,
+    PRICE_LINK: `${VOLUME_BACKUP_CTA_TRACKING}::create-backup::guide_${GUIDES_STORAGES_VOLUME_BACKUP_OVERVIEW}`,
+    CTA_CONFIRM: `${VOLUME_BACKUP_CTA_TRACKING}::create-backup::confirm`,
+    REQUEST_FAIL: `${VOLUME_BACKUP_CTA_TRACKING}::create-backup_error`,
+    REQUEST_SUCCESS: `${VOLUME_BACKUP_CTA_TRACKING}::create-backup_success`,
+  },
+  LISTING: {
+    PAGE: `${VOLUME_BACKUP_BASE_TRACKING}`,
+    ADD: `${VOLUME_BACKUP_CTA_TRACKING}::add`,
+    ROW_CTA_RESTORE_VOLUME: `${VOLUME_BACKUP_CTA_TRACKING}::table-option-menu::restore-volume`,
+    ROW_CTA_CREATE_VOLUME: `${VOLUME_BACKUP_CTA_TRACKING}::table-option-menu::create-volume`,
+    ROW_CTA_DELETE_VOLUME: `${VOLUME_BACKUP_CTA_TRACKING}::table-option-menu::delete-volume`,
+  },
+  RESTORE_VOLUME: {
+    PAGE: `${VOLUME_BACKUP_BASE_TRACKING}::restore`,
+    CTA_CANCEL: `${VOLUME_BACKUP_CTA_TRACKING}::restore::cancel`,
+    CTA_CONFIRM: `${VOLUME_BACKUP_CTA_TRACKING}::restore::confirm`,
+    REQUEST_FAIL: `${VOLUME_BACKUP_CTA_TRACKING}::restore_error`,
+    REQUEST_SUCCESS: `${VOLUME_BACKUP_CTA_TRACKING}::restore_success`,
+  },
+  CREATE_VOLUME: {
+    PAGE: `${VOLUME_BACKUP_BASE_TRACKING}::create-volume`,
+    CTA_CANCEL: `${VOLUME_BACKUP_CTA_TRACKING}::create-volume::cancel`,
+    CTA_CONFIRM: `${VOLUME_BACKUP_CTA_TRACKING}::create-volume::confirm`,
+    REQUEST_FAIL: `${VOLUME_BACKUP_CTA_TRACKING}::create-volume_error`,
+    REQUEST_SUCCESS: `${VOLUME_BACKUP_CTA_TRACKING}::create-volume_success`,
+  },
+  DELETE_BACKUP: {
+    PAGE: `${VOLUME_BACKUP_BASE_TRACKING}::delete`,
+    CTA_CANCEL: `${VOLUME_BACKUP_CTA_TRACKING}::delete::cancel`,
+    CTA_CONFIRM: `${VOLUME_BACKUP_CTA_TRACKING}::delete::confirm`,
+    REQUEST_FAIL: `${VOLUME_BACKUP_CTA_TRACKING}::delete_error`,
+    REQUEST_SUCCESS: `${VOLUME_BACKUP_CTA_TRACKING}::delete_success`,
+  },
+};
