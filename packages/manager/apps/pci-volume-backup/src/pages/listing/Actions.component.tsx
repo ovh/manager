ee9adc7b6ba267bec0b739a4acb0917c86a51ca8
Marc Monchablon <marc.monchablon.ext@corp.ovh.com>
2025-04-16 15:48:26 +0200

    fix(pci-volume-backup): fix broken tests, eslint and build issues
    ref: #DTCORE-3264
    
    Signed-off-by: Marc Monchablon <marc.monchablon.ext@corp.ovh.com>
    Co-authored-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/apps/pci-volume-backup/src/pages/listing/Actions.component.tsx
+++ packages/manager/apps/pci-volume-backup/src/pages/listing/Actions.component.tsx
@@ -1,5 +1,5 @@
-// import { useTranslation } from 'react-i18next';
-// import { useHref } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { useHref } from 'react-router-dom';
 import { ActionMenu } from '@ovh-ux/manager-react-components';
 import { TVolumeBackup } from '@/data/api/api.types';
 
@@ -8,19 +8,30 @@ type ActionsProps = {
 };
 
 export default function Actions({ backup }: Readonly<ActionsProps>) {
-  // const { t } = useTranslation('listing');
+  const { t } = useTranslation('listing');
 
   const items: { id: number; href: string; label: string }[] = [
-    // {
-    //   id: 1,
-    //   href: useHref(`./${backup.id}/new-volume`),
-    //   label: t('pci_projects_project_storages_snapshots_create_volume_label'),
-    // },
-    // {
-    //   id: 2,
-    //   href: useHref(`./delete?snapshotId=${backup.id}`),
-    //   label: t('pci_projects_project_storages_snapshots_delete_label'),
-    // },
+    {
+      id: 1,
+      href: useHref(`./restore-volume`),
+      label: t(
+        'pci_projects_project_storages_volume_backup_list_datagrid_menu_action_restore',
+      ),
+    },
+    {
+      id: 2,
+      href: useHref(`./${backup.id}/create-volume`),
+      label: t(
+        'pci_projects_project_storages_volume_backup_list_datagrid_menu_action_create_volume',
+      ),
+    },
+    {
+      id: 3,
+      href: useHref(`./delete`),
+      label: t(
+        'pci_projects_project_storages_volume_backup_list_datagrid_menu_action_delete',
+      ),
+    },
   ];
 
   return <ActionMenu id={backup.id} items={items} isCompact />;
