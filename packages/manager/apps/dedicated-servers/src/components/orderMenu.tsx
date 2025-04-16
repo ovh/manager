2073afd50a01bdca5978e43d664e0025a147e6c1
mhelhali-soufien <159853039+mhelhali-soufien@users.noreply.github.com>
2025-04-16 08:14:12 +0200

    feat(dedicated-servers): bump server listing to react (#16397)
    ref: #MANAGER-17448
    
    Signed-off-by: soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>

---
+++ packages/manager/apps/dedicated-servers/src/components/orderMenu.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import {
+  useOvhTracking,
+} from '@ovh-ux/manager-react-shell-client';
+import { OdsPopover, OdsButton, OdsLink } from '@ovhcloud/ods-components/react';
+import {
+  ODS_BUTTON_SIZE,
+  ODS_ICON_NAME,
+  ODS_POPOVER_POSITION,
+} from '@ovhcloud/ods-components';
+import useLinkUtils, { UrlLinks } from '@/hooks/useLinkUtils';
+import { orderLinks } from '@/data/constants/orderLinks';
+import { NAMESPACES } from '@ovh-ux/manager-common-translations';
+
+export const OrderMenu: React.FC = () => {
+  const { t } = useTranslation('dedicated-servers');
+  const { t: tCommon } = useTranslation(NAMESPACES.ACTIONS);
+  const { trackClick } = useOvhTracking();
+  const links = useLinkUtils<UrlLinks>(orderLinks);
+
+  return (
+    <div className="w-min">
+      <OdsButton
+        icon={ODS_ICON_NAME.plus}
+        size={ODS_BUTTON_SIZE.sm}
+        id={`server-orders-action`}
+        variant="outline"
+        label={tCommon('order')}
+      />
+      <OdsPopover
+        triggerId="server-orders-action"
+        position={ODS_POPOVER_POSITION.bottomStart}
+      >
+        <div>
+          <OdsLink
+            color="primary"
+            href="#"
+            target="_blank"
+            icon={ODS_ICON_NAME.externalLink}
+            onClick={(e) => {
+              window.open(links.dedicatedOrder as string);
+              e.preventDefault();
+            }}
+            label={t('server_order_dedicated')}
+          />
+        </div>
+        <div>
+          <OdsLink
+            color="primary"
+            href="#"
+            target="_blank"
+            icon={ODS_ICON_NAME.externalLink}
+            onClick={(e) => {
+              window.open(links.dedicatedEcoRangeOrder as string);
+              e.preventDefault();
+            }}
+            label={t('server_order_eco')}
+          />
+        </div>
+      </OdsPopover>
+    </div>
+  );
+};
+
+export default OrderMenu;
