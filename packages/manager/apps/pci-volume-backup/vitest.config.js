7735c1e1a30222cf86d40c9161bd734890bc4bec
Marc Monchablon <marc.monchablon.ext@corp.ovh.com>
2025-04-16 15:48:26 +0200

    fix(pci-volume-backup): fix broken tests, eslint and build issues
    ref: #DTCORE-3264
    Signed-off-by: Marc Monchablon <marc.monchablon.ext@corp.ovh.com>
    Co-authored-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/apps/pci-volume-backup/vitest.config.js
+++ packages/manager/apps/pci-volume-backup/vitest.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+    setupFiles: './src/setupTests.tsx',
     coverage: {
       include: ['src'],
       exclude: [
