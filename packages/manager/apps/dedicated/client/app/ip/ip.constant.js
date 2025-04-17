a7d98efd4a212d297e6f51b244a0d5190db898a7
soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>
2025-04-17 08:28:39 +0200

    feat(dedicated): add guid link for ip listing
    ref: #MANAGER-17943
    
    Signed-off-by: soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/ip/ip.constant.js
+++ packages/manager/apps/dedicated/client/app/ip/ip.constant.js
@@ -7,6 +7,18 @@ export const REPRICING_BANNER_URL = {
   FR: 'https://blog.ovhcloud.com/ipv4-additionnelles-nouvelle-tarification/',
   DEFAULT: 'https://blog.ovhcloud.com/additional-ipv4-new-pricing/',
 };
+
+export const GUIDE_LINKS = {
+  additional_ip:
+    'https://help.ovhcloud.com/csm/fr-public-cloud-network-configure-additional-ip?id=kb_article_view&sysparm_article=KB0050241',
+  ip_alias:
+    'https://help.ovhcloud.com/csm/fr-dedicated-servers-network-ipaliasing?id=kb_article_view&sysparm_article=KB0043761',
+  ip_proxmox:
+    'https://help.ovhcloud.com/csm/fr-dedicated-servers-proxmox-network-hg-scale?id=kb_article_view&sysparm_article=KB0043915',
+  floating_ip:
+    'https://help.ovhcloud.com/csm/fr-public-cloud-network-additional-ip-vs-floating-ip?id=kb_article_view&sysparm_article=KB0050159',
+};
+
 export const REPRICING_BANNER_DATE_MIN = '2022-12-01 00:00:00';
 export const REPRICING_BANNER_DATE_MAX = '2023-09-10 23:59:59';
 export const REPRICING_BANNER_DATE_CREATION = '2022-10-06 00:00:00';
