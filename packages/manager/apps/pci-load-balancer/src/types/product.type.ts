a5b20e636110205f7f6c80a53b385c1bb16349d1
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2025-04-17 14:23:54 +0200

    refactor(pci-load-balancer): use common network hook to get catalog
    ref: #TAPC-3261
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

--- packages/manager/apps/pci-load-balancer/src/types/product.type.ts
+++ packages/manager/apps/pci-load-balancer/src/types/product.type.ts
@@ -1,5 +1,9 @@
+import { TProductAvailabilityRegion } from '@ovh-ux/manager-pci-common';
+
 export type TProductAddonDetail = {
   size: string;
   price: number;
   technicalName: string;
+  bandwidth: number;
+  regions: TProductAvailabilityRegion[];
 };
