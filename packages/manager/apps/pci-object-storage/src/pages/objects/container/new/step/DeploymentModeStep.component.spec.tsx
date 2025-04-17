af2bbfe763f23276521519b8288cf64f3359b206
Selmen AKRMI <selmen.akrmi.ext@ovhcloud.com>
2025-04-17 16:28:50 +0200

    feat(pci-object-storage): tracking versioning
    ref: #TAPC-3243
    
    Signed-off-by: Selmen AKRMI <selmen.akrmi.ext@ovhcloud.com>

--- packages/manager/apps/pci-object-storage/src/pages/objects/container/new/step/DeploymentModeStep.component.spec.tsx
+++ packages/manager/apps/pci-object-storage/src/pages/objects/container/new/step/DeploymentModeStep.component.spec.tsx
@@ -1,12 +1,24 @@
-import { describe } from 'vitest';
+import { describe, vi } from 'vitest';
 import { render } from '@testing-library/react';
+
 import { DeploymentModeStep } from './DeploymentModeStep.component';
-import { wrapper } from '@/wrapperRenders';
+import { wrapperShow } from '@/wrapperRenders';
+
+vi.mock('@ovh-ux/manager-react-shell-client', async () => {
+  const actual = await vi.importActual('@ovh-ux/manager-react-shell-client');
+  return {
+    ...actual,
+    useOvhTracking: () => ({
+      trackPage: vi.fn(),
+      trackClick: vi.fn(),
+    }),
+  };
+});
 
 describe('DeploymentModeStep', () => {
   it('should display correctly', () => {
     const { container } = render(<DeploymentModeStep />, {
-      wrapper,
+      wrapper: wrapperShow,
     });
     expect(container).toMatchSnapshot();
   });
