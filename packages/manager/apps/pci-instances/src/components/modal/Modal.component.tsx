33a6b7ac0c6a68ee72cc55ee43253bf50d01c05f
Frédéric Vilcot <fredericvilcot@gmail.com>
2025-04-17 16:38:33 +0200

    fix(pci-instances): add ODS17 style to base modal
    ref: #TAPC-3405
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

--- packages/manager/apps/pci-instances/src/components/modal/Modal.component.tsx
+++ packages/manager/apps/pci-instances/src/components/modal/Modal.component.tsx
@@ -3,14 +3,20 @@ import {
   Dialog,
   DialogContent,
   DialogHeader,
-  DialogTitle,
   DialogFooter,
   DialogClose,
-  Button,
 } from '@datatr-ux/uxlib';
 import { useTranslation } from 'react-i18next';
 import clsx from 'clsx';
 import { useId } from 'react';
+import { OsdsButton, OsdsText } from '@ovhcloud/ods-components/react';
+import {
+  ODS_BUTTON_VARIANT,
+  ODS_TEXT_COLOR_HUE,
+  ODS_TEXT_LEVEL,
+  ODS_TEXT_SIZE,
+} from '@ovhcloud/ods-components';
+import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
 
 const Modal = ({
   title,
@@ -32,31 +38,46 @@ const Modal = ({
 
   return (
     <Dialog defaultOpen onOpenChange={handleModalClose}>
-      <DialogContent aria-describedby={id} className="p-0">
+      <DialogContent
+        aria-describedby={id}
+        className="p-0 !rounded-[--ods-border-radius-md] [&_button:focus-visible]:outline-none [&_button:focus-visible]:ring-0 [&_button:focus-visible]:ring-offset-0 [&_button]:text-[--ods-color-info-500] [&_button]:top-3.5 [&_button_svg]:w-8 [&_button_svg]:h-8"
+      >
         <DialogHeader
           className={clsx(
-            `p-6 rounded-t-sm sm:rounded-t-lg`,
-            variant === 'warning' ? 'bg-warning-100' : 'bg-primary-100',
+            `p-6 rounded-t-md `,
+            variant === 'warning'
+              ? 'bg-[--ods-color-warning-100]'
+              : 'bg-[--ods-color-primary-100]',
           )}
-        >
-          <DialogTitle>{title}</DialogTitle>
-        </DialogHeader>
+        />
         <div id={id} className="p-6 pt-0">
+          <OsdsText
+            color={ODS_THEME_COLOR_INTENT.primary}
+            size={ODS_TEXT_SIZE._400}
+            hue={ODS_TEXT_COLOR_HUE._500}
+            level={ODS_TEXT_LEVEL.heading}
+          >
+            {title}
+          </OsdsText>
           {children}
         </div>
         <DialogFooter className="flex justify-end p-6 pt-0">
           <DialogClose asChild>
-            <Button variant="neutral" mode="ghost">
+            <OsdsButton
+              disabled={isPending || undefined}
+              variant={ODS_BUTTON_VARIANT.ghost}
+              color={ODS_THEME_COLOR_INTENT.primary}
+            >
               {t('cancel')}
-            </Button>
+            </OsdsButton>
           </DialogClose>
-          <Button
-            disabled={isPending}
-            variant="primary"
+          <OsdsButton
+            disabled={isPending || undefined}
             onClick={handleInstanceAction}
+            color={ODS_THEME_COLOR_INTENT.primary}
           >
             {t('confirm')}
-          </Button>
+          </OsdsButton>
         </DialogFooter>
       </DialogContent>
     </Dialog>
