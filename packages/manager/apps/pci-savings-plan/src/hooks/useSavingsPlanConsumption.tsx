8313b0d480c6468ac5224906949770df9c0852a0
Eric Ciccotti <eric.ciccotti.ext@corp.ovh.com>
2025-04-17 16:30:25 +0200

    fix(pci.savings-plan): connect api to dashboard
    ref: #TAPC-3466
    
    Signed-off-by: Eric Ciccotti <eric.ciccotti.ext@corp.ovh.com>

--- packages/manager/apps/pci-savings-plan/src/hooks/useSavingsPlanConsumption.tsx
+++ packages/manager/apps/pci-savings-plan/src/hooks/useSavingsPlanConsumption.tsx
@@ -1,26 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
-import { SavingsPlanConsumption } from '@/types/savingsPlanConsumption.type';
-
-const getSavingsPlanConsumption = async ({
-  flavor,
-  projectId,
-  year,
-  month,
-}: {
-  projectId?: string;
-  flavor?: string;
-  year: number;
-  month: number;
-}): Promise<SavingsPlanConsumption> => {
-  const response = await fetch(
-    // TODO: replace with the correct url  when it will be available
-    `http://localhost:8080/proxy?flavor=${flavor}&projectId=${projectId}&year=${year}&month=${month}`,
-    // `http://localhost:8080/proxy?flavor=${flavor}&projectId=${projectId}&year=${year}&month=${month}`,
-  );
-
-  return response.json();
-};
+import { getSavingsPlanConsumption } from '@/data/api/pci-savings-plan';
 
 export const useSavingsPlanConsumption = ({
   flavor,
