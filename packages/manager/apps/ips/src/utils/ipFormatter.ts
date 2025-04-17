bc18917d615deda3aacb475a25e7bdb2e6b1bc8d
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2025-04-17 14:59:29 +0200

    feat(ips): add tooltip on game firewall and ip reverse on group child
    ref: #MANAGER-16797
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

--- packages/manager/apps/ips/src/utils/ipFormatter.ts
+++ packages/manager/apps/ips/src/utils/ipFormatter.ts
@@ -1,5 +1,5 @@
 export const ipFormatter = (ip: string) =>
-  ip.indexOf('/32') !== -1
+  ip.indexOf('/32') !== -1 || ip.indexOf('/') === -1
     ? {
         ip: ip.replace('/32', ''),
         isGroup: false,
