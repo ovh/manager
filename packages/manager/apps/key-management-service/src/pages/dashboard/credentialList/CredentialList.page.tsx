8b45dad0b1580e05c82eefe50e23f35e67c73814
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-04-18 10:40:05 +0200

    chore(kms): fix all spacings values after tailwind config change
    Fix spacings and fix some responsive behaviors on the app
    
    ref: #MANAGER-17818
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/key-management-service/src/pages/dashboard/credentialList/CredentialList.page.tsx
+++ packages/manager/apps/key-management-service/src/pages/dashboard/credentialList/CredentialList.page.tsx
@@ -33,7 +33,7 @@ const CredentialList = () => {
   );
 
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-6">
       <OdsText preset={ODS_TEXT_PRESET.paragraph}>
         {t('key_management_service_credential_headline')}
       </OdsText>
