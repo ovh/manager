3250761f8553265fc63eeff911d56b2cc6dd7206
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2025-04-18 15:57:55 +0200

    feat(ips): add inline reverse dns edition
    ref: #MANAGER-17624
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

--- packages/manager/apps/ips/src/pages/listing/ipListing/components/DatagridCells/IpReverse/IpReverse.tsx
+++ packages/manager/apps/ips/src/pages/listing/ipListing/components/DatagridCells/IpReverse/IpReverse.tsx
@@ -1,8 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useNotifications } from '@ovh-ux/manager-react-components';
+import { Translation } from 'react-i18next';
+import { ApiError } from '@ovh-ux/manager-core-api';
 import { useGetIpReverse } from '@/data/hooks/ip';
 import { ipFormatter } from '@/utils/ipFormatter';
 import { SkeletonCell } from '../SkeletonCell/SkeletonCell';
 import { ListingContext } from '@/pages/listing/listingContext';
+import { useDeleteIpReverse } from '@/data/hooks/ip/useDeleteIpReverse';
+import { useUpdateIpReverse } from '@/data/hooks/ip/useUpdateIpReverse';
+import { EditInline } from '@/components/EditInline/edit-inline.component';
 
 export type IpReverseProps = {
   ip: string;
@@ -20,20 +26,94 @@ export type IpReverseProps = {
  */
 export const IpReverse = ({ ip, parentIpGroup }: IpReverseProps) => {
   const { expiredIps } = useContext(ListingContext);
+  const { addError } = useNotifications();
 
   // Check if ip is not /32
   const { ip: formattedIp, isGroup } = ipFormatter(ip);
+  const ipGroup = parentIpGroup || ip;
 
   // Get ip reverse
   const { ipsReverse, isLoading, error } = useGetIpReverse({
-    ip: parentIpGroup || ip,
+    ip: ipGroup,
     enabled: !isGroup && expiredIps.indexOf(ip) === -1,
   });
 
+  // If there is an error while deleting / updating
+  // Add error notification
+  const onError = (apiError: ApiError) => {
+    addError(
+      <Translation ns="error">
+        {(_t) => (
+          <span
+            dangerouslySetInnerHTML={{
+              __html: _t('managerApiError', {
+                error:
+                  apiError?.response?.data?.message ||
+                  apiError?.message ||
+                  null,
+                requestId: apiError?.response?.headers?.['x-ovh-queryid'],
+              }),
+            }}
+          />
+        )}
+      </Translation>,
+      true,
+    );
+  };
+
+  // Prepare delete mutation with
+  // ip: single Ip with mask (ipFormatter remove mask if /32)
+  // ipGroup: the parent group for Ip (if /32 ipGroup is same as Ip but with the mask)
+  const {
+    mutate: deleteIpReverse,
+    isPending: pendingDelete,
+  } = useDeleteIpReverse({
+    ip: formattedIp,
+    ipGroup,
+    onError,
+  });
+
+  // Prepare update mutation with
+  // ip: single Ip with mask (ipFormatter remove mask if /32)
+  // ipGroup: the parent group for Ip (if /32 ipGroup is same as Ip but with the mask)
+  const {
+    mutate: updateIpReverse,
+    isPending: pendingUpdate,
+  } = useUpdateIpReverse({
+    ip: formattedIp,
+    ipGroup,
+    onError,
+  });
+
+  // Find reverse for ip in list of reverse.
+  // Especialy usefull for ipBlock as getIpReverse get all reverse for the block.
+  const ipReverse = ipsReverse?.find(
+    (ipWithReverse) => ipWithReverse.ipReverse === formattedIp,
+  )?.reverse;
+
+  const editIpReverse = (oldReverse: string, newReverse: string) => {
+    if (!!oldReverse && newReverse === '') {
+      // delete
+      deleteIpReverse();
+    } else {
+      // update
+      updateIpReverse({ reverse: newReverse });
+    }
+  };
+
   return (
-    <SkeletonCell isLoading={isLoading} enabled={!isGroup} error={error}>
-      {ipsReverse?.find((ipReverse) => ipReverse.ipReverse === formattedIp)
-        ?.reverse || <>-</>}
+    <SkeletonCell
+      isLoading={isLoading || pendingDelete || pendingUpdate}
+      enabled={!isGroup}
+      error={error}
+    >
+      <EditInline
+        name="test"
+        onConfirm={(newReverse: string) => editIpReverse(ipReverse, newReverse)}
+        defaultValue={ipReverse}
+      >
+        {ipReverse || <>-</>}
+      </EditInline>
     </SkeletonCell>
   );
 };
