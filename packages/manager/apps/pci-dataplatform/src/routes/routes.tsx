5912ba21d48a87b297c096694966090b05a95ea6
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2025-04-18 10:58:22 +0200

    feat(pci-dataplatform): init dataplatform uApp (#15628)
    ref: #DATATR-1900
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

---
+++ packages/manager/apps/pci-dataplatform/src/routes/routes.tsx
@@ -0,0 +1,36 @@
+import NotFound from '../pages/404.page';
+
+const lazyRouteConfig = (importFn: CallableFunction) => {
+  return {
+    lazy: async () => {
+      const { default: moduleDefault, ...moduleExports } = await importFn();
+      return {
+        Component: moduleDefault,
+        loader: moduleExports?.Loader,
+        handle: {
+          breadcrumb: moduleExports.breadcrumb,
+        },
+        ...moduleExports,
+      };
+    },
+  };
+};
+
+export const COMMON_PATH = '/pci/projects';
+
+export default [
+  {
+    path: '/pci/projects/:projectId/dataplatform',
+    ...lazyRouteConfig(() => import('@/pages/Root.layout')),
+    children: [
+      {
+        path: '',
+        ...lazyRouteConfig(() => import('@/pages/Root.page')),
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+];
