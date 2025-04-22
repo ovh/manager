b58a06f94e4ad39835ce67264872d915dd38a4f0
Arthur Bullet <arthur.bullet@ovhcloud.com>
2025-04-22 13:47:15 +0200

    feat(pci.ai-dashboard): fix s3 users link
    ref: #DATATR-1183
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>

--- packages/manager/apps/pci-ai-tools/src/pages/dashboard/datastore/Datastore.page.tsx
+++ packages/manager/apps/pci-ai-tools/src/pages/dashboard/datastore/Datastore.page.tsx
@@ -40,7 +40,7 @@ const Datastore = () => {
       navigate(`./delete/${datastore.region}/${datastore.alias}`),
   });
 
-  const userPath = `#/pci/project/${projectId}/users`;
+  const userPath = `#/pci/project/${projectId}/storages/objects/users`;
 
   useEffect(() => {
     if (!regionQuery.data) return;
