cd1fbf0a163df84f2efa3a1c01ce4cd86aafc7cd
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2025-04-22 13:13:26 +0530

    feat(ips.listing): add guide links
    ref: #MANAGER-16788
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

--- packages/manager/apps/ips/src/utils/index.ts
+++ packages/manager/apps/ips/src/utils/index.ts
@@ -2,3 +2,4 @@ export * from './iamActions.constants';
 export * from './ipFormatter';
 export * from './getTypeByServiceName';
 export * from './getLinkByServiceName';
+export * from './links.constants';
