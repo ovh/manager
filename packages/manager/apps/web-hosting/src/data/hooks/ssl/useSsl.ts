c9088435cbc30957e7c5de205580065aa0b2f310
Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>
2025-04-22 16:23:53 +0200

    feat(web-hosting): regenerate ssl on datagrid actions
    ref: #MANAGER-17763
    
    Signed-off-by: Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>

--- packages/manager/apps/web-hosting/src/data/hooks/ssl/useSsl.ts
+++ packages/manager/apps/web-hosting/src/data/hooks/ssl/useSsl.ts
@@ -1,6 +1,9 @@
 import { useMutation } from '@tanstack/react-query';
 import { ApiError } from '@ovh-ux/manager-core-api';
-import { deleteDomainCertificate } from '@/data//api/ssl';
+import {
+  deleteDomainCertificate,
+  regenerateDomainCertificate,
+} from '@/data//api/ssl';
 
 export const useDeleteDomainCertificate = (
   serviceName: string,
@@ -16,6 +19,24 @@ export const useDeleteDomainCertificate = (
 
   return {
     deleteDomainCertificate: mutation.mutate,
+    ...mutation,
+  };
+};
+
+export const useRegenerateDomainCertificate = (
+  serviceName: string,
+  onSuccess: () => void,
+  onError: () => void,
+) => {
+  const mutation = useMutation({
+    mutationFn: (domain: string) =>
+      regenerateDomainCertificate(serviceName, domain),
+    onSuccess,
+    onError,
+  });
+
+  return {
+    regenerateDomainCertificate: mutation.mutate,
     ...mutation,
   };
 };
