41982fafa03397c20248a949aae9a4a794adb061
Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>
2025-04-22 16:23:53 +0200

    feat(web-hosting): regenerate ssl on datagrid actions
    ref: #MANAGER-17763
    
    Signed-off-by: Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>

--- packages/manager/apps/web-hosting/src/utils/tracking.constants.ts
+++ packages/manager/apps/web-hosting/src/utils/tracking.constants.ts
@@ -43,7 +43,7 @@ export const SSL = 'ssl';
 export const IMPORT_SSL = 'import-ssl';
 export const ORDER_SECTIGO = 'order-sectigo';
 export const DISABLE_SSL = 'disable-ssl';
-
+export const REGENERATE_SSL = 'regenerate-ssl';
 export const GUIDE_GETTING_STARTED = 'getting-started-guide';
 export const GUIDE_PUBLISHING = 'publishing-guide';
 export const GUIDE_EDIT_DNS_ZONE = 'edit-dns-zone-guide';
