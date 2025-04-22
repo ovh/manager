f67cc53ff6996559187193f20e39f6d00402d266
Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>
2025-04-22 14:47:30 +0200

    feat(web-hosting): create ssl new tab and dashboard
    ref: #MANAGER-17760
    
    Signed-off-by: Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>

--- packages/manager/apps/web-hosting/src/pages/dashboard/ssl/Ssl.page.tsx
+++ packages/manager/apps/web-hosting/src/pages/dashboard/ssl/Ssl.page.tsx
@@ -1,14 +1,62 @@
 import React from 'react';
-import { Notifications } from '@ovh-ux/manager-react-components';
 
+import { useQueryClient } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import {
+  Datagrid,
+  Notifications,
+  useResourcesIcebergV2,
+} from '@ovh-ux/manager-react-components';
+import { OdsButton } from '@ovhcloud/ods-components/react';
+import {
+  ODS_BUTTON_COLOR,
+  ODS_BUTTON_VARIANT,
+  ODS_ICON_NAME,
+} from '@ovhcloud/ods-components';
 import Topbar from '@/components/topBar/TopBar.component';
+import useDatagridColumn from '@/hooks/ssl/useDatagridColumn';
 import Loading from '@/components/loading/Loading.component';
 
 export default function Ssl() {
+  const { serviceName } = useParams();
+  const queryClient = useQueryClient();
+  const {
+    flattenData,
+    hasNextPage,
+    fetchNextPage,
+    isLoading,
+  } = useResourcesIcebergV2({
+    route: `/webhosting/resource/${serviceName}/certificate`,
+    queryKey: ['webhosting', 'resource', serviceName, 'certificate'],
+  });
+
+  const columns = useDatagridColumn();
+
   return (
     <React.Suspense fallback={<Loading />}>
       <Notifications />
       <Topbar />
+      <div className="flex justify-end mb-7 ">
+        <OdsButton
+          variant={ODS_BUTTON_VARIANT.outline}
+          color={ODS_BUTTON_COLOR.primary}
+          label=""
+          icon={ODS_ICON_NAME.refresh}
+          onClick={() => {
+            queryClient.invalidateQueries();
+          }}
+        />
+      </div>
+      {columns && (
+        <Datagrid
+          columns={columns}
+          items={flattenData || []}
+          totalItems={flattenData?.length || 0}
+          hasNextPage={hasNextPage && !isLoading}
+          onFetchNextPage={fetchNextPage}
+          isLoading={isLoading}
+        />
+      )}
     </React.Suspense>
   );
 }
