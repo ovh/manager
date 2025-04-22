2dffcf9a6503ff0a7fab1964dd987d3321fb4293
Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>
2025-04-22 16:11:57 +0200

    feat(web-hosting): disable ssl on datagrid action
    ref: #MANAGER-17762
    
    Signed-off-by: Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>

--- packages/manager/apps/web-hosting/src/utils/tracking.constants.ts
+++ packages/manager/apps/web-hosting/src/utils/tracking.constants.ts
@@ -42,6 +42,7 @@ export const DASHBOARD = 'dashboard';
 export const SSL = 'ssl';
 export const IMPORT_SSL = 'import-ssl';
 export const ORDER_SECTIGO = 'order-sectigo';
+export const DISABLE_SSL = 'disable-ssl';
 
 export const GUIDE_GETTING_STARTED = 'getting-started-guide';
 export const GUIDE_PUBLISHING = 'publishing-guide';
