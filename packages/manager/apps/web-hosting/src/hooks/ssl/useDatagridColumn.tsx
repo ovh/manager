6eeeb637e2da2567c066d3372c07a22d87752a11
Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>
2025-04-22 14:47:30 +0200

    feat(web-hosting): create ssl new tab and dashboard
    ref: #MANAGER-17760
    
    Signed-off-by: Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>

---
+++ packages/manager/apps/web-hosting/src/hooks/ssl/useDatagridColumn.tsx
@@ -0,0 +1,154 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+import {
+  ActionMenu,
+  ActionMenuItem,
+  Badge,
+  DataGridTextCell,
+  DateFormat,
+  useFormattedDate,
+} from '@ovh-ux/manager-react-components';
+
+import {
+  ODS_BADGE_COLOR,
+  ODS_BUTTON_VARIANT,
+  ODS_ICON_NAME,
+} from '@ovhcloud/ods-components';
+
+import { SslCertificate } from '@/types/ssl';
+
+export const DatagridActionCell = (props: SslCertificate) => {
+  const { t } = useTranslation('ssl');
+
+  const items: ActionMenuItem[] = [
+    {
+      id: 1,
+      label: t('disable_ssl'),
+    },
+    {
+      id: 2,
+      label: t('regenerate_ssl'),
+    },
+  ];
+
+  return (
+    <DataGridTextCell>
+      <ActionMenu
+        id={props?.currentState?.mainDomain}
+        items={items}
+        isCompact
+        variant={ODS_BUTTON_VARIANT.ghost}
+        icon={ODS_ICON_NAME.ellipsisVertical}
+      />
+    </DataGridTextCell>
+  );
+};
+
+export default function useDatagridColumn() {
+  const { t } = useTranslation('ssl');
+
+  const StatusColor = {
+    CREATING: ODS_BADGE_COLOR.information,
+    DELETING: ODS_BADGE_COLOR.information,
+    ERROR: ODS_BADGE_COLOR.critical,
+    READY: ODS_BADGE_COLOR.success,
+    SUSPENDED: ODS_BADGE_COLOR.success,
+    UPDATING: ODS_BADGE_COLOR.information,
+  };
+
+  const columns = [
+    {
+      id: 'mainDomain',
+      cell: (props: SslCertificate) => (
+        <DataGridTextCell>
+          <DataGridTextCell>{props?.currentState?.mainDomain}</DataGridTextCell>
+        </DataGridTextCell>
+      ),
+      label: t('cell_main_domain'),
+    },
+    {
+      id: 'additionalDomain',
+      cell: (props: SslCertificate) => (
+        <DataGridTextCell>
+          <DataGridTextCell>
+            {props?.currentState?.additionalDomains?.[0] || '-'}
+            {props?.currentState?.additionalDomains?.length > 1 && (
+              <small className="text-blue-700 pl-6">
+                {t(
+                  props?.currentState?.additionalDomains?.length === 2
+                    ? 'additional_domains_singular_total'
+                    : 'additional_domains_plural_total',
+                  { n: props?.currentState?.additionalDomains?.length - 1 },
+                )}
+              </small>
+            )}
+          </DataGridTextCell>
+        </DataGridTextCell>
+      ),
+      label: t('cell_additional_domain'),
+    },
+    {
+      id: 'type',
+      cell: (props: SslCertificate) => (
+        <DataGridTextCell>
+          <DataGridTextCell>
+            {t(props?.currentState?.certificateType)}
+          </DataGridTextCell>
+        </DataGridTextCell>
+      ),
+      label: t('cell_certificate_type'),
+    },
+    {
+      id: 'state',
+      cell: (props: SslCertificate) => (
+        <DataGridTextCell>
+          <DataGridTextCell>
+            <Badge
+              label={t(props?.resourceStatus)}
+              className="my-3"
+              color={StatusColor[props?.resourceStatus]}
+            />
+          </DataGridTextCell>
+        </DataGridTextCell>
+      ),
+      label: t('cell_state'),
+    },
+    {
+      id: 'creationDate',
+      cell: (props: SslCertificate) => (
+        <DataGridTextCell>
+          <DataGridTextCell>
+            {useFormattedDate({
+              dateString: props?.currentState?.createdAt,
+              format: DateFormat.compact,
+            })}
+          </DataGridTextCell>
+        </DataGridTextCell>
+      ),
+      label: t('cell_creation_date'),
+    },
+    {
+      id: 'expirationDate',
+      cell: (props: SslCertificate) => (
+        <DataGridTextCell>
+          <DataGridTextCell>
+            {useFormattedDate({
+              dateString: props?.currentState?.expiredAt,
+              format: DateFormat.compact,
+            })}
+          </DataGridTextCell>
+        </DataGridTextCell>
+      ),
+      label: t('cell_expiration_date'),
+    },
+    {
+      id: 'actions',
+      label: '',
+      isSortable: false,
+      cell: DatagridActionCell,
+    },
+  ];
+
+  return columns;
+}
