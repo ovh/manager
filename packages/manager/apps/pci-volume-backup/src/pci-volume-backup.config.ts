4e52d4b92459e8e3d2b05cc2442af89d0648f3f1
Fabien Henon <fabien.henon.ext@corp.ovh.com>
2025-04-22 16:54:52 +0200

    feat(pci-volume-backup): implement listing with necessary tests
    ref: #DTCORE-3257
    
    Signed-off-by: Fabien Henon <fabien.henon.ext@corp.ovh.com>

--- packages/manager/apps/pci-volume-backup/src/pci-volume-backup.config.ts
+++ packages/manager/apps/pci-volume-backup/src/pci-volume-backup.config.ts
@@ -5,4 +5,10 @@ export default {
     },
   },
   rootLabel: 'pci-volume-backup',
+  changeLogLinks: {
+    changelog: 'https://github.com/orgs/ovh/projects/16/views/6?pane=info',
+    roadmap: 'https://github.com/orgs/ovh/projects/16/views/1?pane=info',
+    'feature-request':
+      'https://github.com/ovh/public-cloud-roadmap/issues/new?assignees=&labels=&projects=&template=feature_request.md&title=',
+  },
 };
