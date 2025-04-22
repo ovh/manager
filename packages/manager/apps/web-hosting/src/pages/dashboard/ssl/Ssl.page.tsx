70ed940e2d4eaaa00abcd903069df442409858ca
Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>
2025-04-22 17:02:12 +0200

    feat(web-hosting): export csv for ssl
    ref: #MANAGER-17761
    
    Signed-off-by: Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>

--- packages/manager/apps/web-hosting/src/pages/dashboard/ssl/Ssl.page.tsx
+++ packages/manager/apps/web-hosting/src/pages/dashboard/ssl/Ssl.page.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
 
 import { useQueryClient } from '@tanstack/react-query';
+import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
+import { generateCsv, mkConfig, download } from 'export-to-csv';
+
 import {
   Datagrid,
   Notifications,
+  useNotifications,
   useResourcesIcebergV2,
 } from '@ovh-ux/manager-react-components';
-import { OdsButton } from '@ovhcloud/ods-components/react';
+import { OdsButton, OdsLink } from '@ovhcloud/ods-components/react';
 import {
   ODS_BUTTON_COLOR,
   ODS_BUTTON_VARIANT,
   ODS_ICON_NAME,
+  ODS_LINK_ICON_ALIGNMENT,
 } from '@ovhcloud/ods-components';
 import Topbar from '@/components/topBar/TopBar.component';
 import useDatagridColumn from '@/hooks/ssl/useDatagridColumn';
 import Loading from '@/components/loading/Loading.component';
+import { SslCertificate } from '@/types/ssl';
 
 export default function Ssl() {
   const { serviceName } = useParams();
+  const { t } = useTranslation('ssl');
   const queryClient = useQueryClient();
+  const { addSuccess } = useNotifications();
+
   const {
     flattenData,
     hasNextPage,
@@ -30,14 +39,105 @@ export default function Ssl() {
     queryKey: ['webhosting', 'resource', serviceName, 'certificate'],
   });
 
+  interface ExportColumn {
+    id: string;
+    label: string;
+    getValue: (item: SslCertificate) => string;
+  }
+
+  const handleExportWithExportToCsv = () => {
+    const csvConfig = mkConfig({
+      filename: t('ssl'),
+      fieldSeparator: ',',
+      quoteStrings: true,
+      useKeysAsHeaders: true,
+    });
+
+    const exportColumns: ExportColumn[] = [
+      {
+        id: 'mainDomain',
+        label: t('cell_main_domain'),
+        getValue: (item) => item?.currentState?.mainDomain,
+      },
+      {
+        id: 'additionalDomain',
+        label: t('cell_additional_domain'),
+        getValue: (item) => item?.currentState?.additionalDomains?.toString(),
+      },
+      {
+        id: 'type',
+        label: t('cell_certificate_type'),
+        getValue: (item) => item?.currentState?.certificateType,
+      },
+      {
+        id: 'state',
+        label: t('cell_state'),
+        getValue: (item) => item?.currentState?.state,
+      },
+      {
+        id: 'creationDate',
+        label: t('cell_creation_date'),
+        getValue: (item) => item?.currentState?.createdAt,
+      },
+      {
+        id: 'expirationDate',
+        label: t('cell_expiration_date'),
+        getValue: (item) => item?.currentState?.expiredAt,
+      },
+    ];
+
+    const csvData = flattenData?.map((item: SslCertificate) =>
+      exportColumns.reduce(
+        (acc, column) => ({
+          ...acc,
+          [column.label]: column.getValue(item),
+        }),
+        {},
+      ),
+    );
+
+    const csv = generateCsv(csvConfig)(csvData);
+    const blob = new Blob([csv.toString()], {
+      type: 'text/csv',
+    });
+    const url = URL.createObjectURL(blob);
+
+    const successMessage = (
+      <div>
+        {t('web_hosting_export_success')}
+        <OdsLink
+          href={url}
+          download={csvConfig.filename}
+          label={t('web_hosting_export_download_manually')}
+          className="ml-4"
+          icon={ODS_ICON_NAME.download}
+          iconAlignment={ODS_LINK_ICON_ALIGNMENT.right}
+        />
+      </div>
+    );
+    addSuccess(successMessage);
+    download(csvConfig)(csv);
+  };
+
   const columns = useDatagridColumn();
 
   return (
     <React.Suspense fallback={<Loading />}>
       <Notifications />
       <Topbar />
-      <div className="flex justify-end mb-7 ">
+      <div className="flex mb-7">
         <OdsButton
+          className="w-2/3"
+          label={t('web_hosting_export_label')}
+          variant={ODS_BUTTON_VARIANT.outline}
+          color={ODS_BUTTON_COLOR.primary}
+          icon={ODS_ICON_NAME.download}
+          iconAlignment={ODS_LINK_ICON_ALIGNMENT.right}
+          onClick={handleExportWithExportToCsv}
+          data-testid="websites-page-export-button"
+        />
+        <OdsButton
+          className="flex flex-wrap justify-end w-1/3"
           variant={ODS_BUTTON_VARIANT.outline}
           color={ODS_BUTTON_COLOR.primary}
           label=""
@@ -47,6 +147,7 @@ export default function Ssl() {
           }}
         />
       </div>
+
       {columns && (
         <Datagrid
           columns={columns}
