3c9717256a00868d12fa137b732fd2ea0f610d04
Louis BENSI <louis.bensi.ext@corp.ovh.com>
2025-04-23 14:06:29 +0200

    feat(web-domains): detail page
    ref: #MANAGER-17031
    
    Signed-off-by: Louis BENSI <louis.bensi.ext@corp.ovh.com>

---
+++ packages/manager/apps/web-domains/src/alldoms/components/ServiceDetail/ServiceDetailDomains.tsx
@@ -0,0 +1,25 @@
+import { ODS_CARD_COLOR, ODS_TEXT_PRESET } from '@ovhcloud/ods-components';
+import { OdsCard, OdsText } from '@ovhcloud/ods-components/react';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { TServiceDetail } from '@/alldoms/types';
+
+interface ServiceDetailDomainsProps {
+  readonly domainsAttached: TServiceDetail['domainAttached'];
+}
+
+export default function ServiceDetailDomains({
+  domainsAttached,
+}: ServiceDetailDomainsProps) {
+  const { t } = useTranslation('allDom');
+  return (
+    <OdsCard color={ODS_CARD_COLOR.neutral} className="w-full p-6">
+      <OdsText preset={ODS_TEXT_PRESET.heading4}>
+        {t('allDom_detail_page_domains_title')}
+      </OdsText>
+      {domainsAttached.map((domain) => (
+        <li key={domain}>{domain}</li>
+      ))}
+    </OdsCard>
+  );
+}
