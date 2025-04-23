189726e934703e3f82605dc7899d3acd1c53440a
Simon Chaumet <simon.chaumet.ext@ovhcloud.com>
2025-04-23 17:25:41 +0200

    feat(pci-block-storage): add multi detach
    ref: #TAPC-3761
    
    Signed-off-by: Simon Chaumet <simon.chaumet.ext@ovhcloud.com>

--- packages/manager/apps/pci-block-storage/src/pages/detach/DetachStorage.page.tsx
+++ packages/manager/apps/pci-block-storage/src/pages/detach/DetachStorage.page.tsx
@@ -1,6 +1,8 @@
 import {
   OsdsButton,
   OsdsModal,
+  OsdsSelect,
+  OsdsSelectOption,
   OsdsSpinner,
   OsdsText,
 } from '@ovhcloud/ods-components/react';
@@ -10,12 +12,13 @@ import {
   ODS_THEME_TYPOGRAPHY_SIZE,
 } from '@ovhcloud/ods-common-theming';
 import { ODS_BUTTON_VARIANT, ODS_SPINNER_SIZE } from '@ovhcloud/ods-components';
-import { useEffect } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Translation, useTranslation } from 'react-i18next';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useNotifications } from '@ovh-ux/manager-react-components';
-import { useInstance } from '@ovh-ux/manager-pci-common';
 import { useDetachVolume, useVolume } from '@/api/hooks/useVolume';
+import { useAttachedInstances } from '@/api/hooks/useInstance';
+import { TAttachedInstance } from '@/api/select/instances';
 
 export default function DetachStorage() {
   const navigate = useNavigate();
@@ -23,17 +26,27 @@ export default function DetachStorage() {
   const { t } = useTranslation('detach');
   const { addError, addSuccess } = useNotifications();
   const { data: volume } = useVolume(projectId, volumeId);
-  const attachedInstanceId = volume?.attachedTo?.[0];
   const onClose = () => navigate('..');
-  const { data: instance, isPending: isInstancePending } = useInstance(
-    projectId,
-    attachedInstanceId,
-  );
+
+  const {
+    data: instances,
+    isPending: isInstancesPending,
+  } = useAttachedInstances(projectId, volumeId);
+
+  const [selectedInstanceId, setSelectedInstanceId] = useState<
+    TAttachedInstance['id'] | null
+  >(null);
+
+  const selectedInstance = useMemo(() => {
+    if (!!selectedInstanceId && !!instances)
+      return instances.find(({ id }) => id === selectedInstanceId)!;
+    return null;
+  }, [selectedInstanceId, instances]);
 
   const { detachVolume, isPending: isDetachPending } = useDetachVolume({
     projectId,
     volumeId,
-    instanceId: attachedInstanceId,
+    instanceId: selectedInstanceId,
     onError(err: Error) {
       addError(
         <Translation ns="detach">
@@ -69,13 +82,17 @@ export default function DetachStorage() {
     },
   });
 
-  const isPending = isInstancePending || isDetachPending;
-  const canDetach = !isPending;
+  const isPending = isInstancesPending || isDetachPending;
+  const canDetach = !isPending && !!selectedInstanceId;
 
   // redirect to listing if volume is not attached
   useEffect(() => {
-    if (!isPending && volume?.attachedTo?.length === 0) {
-      navigate(`/pci/projects/${projectId}/storages/blocks`);
+    if (!isPending && !!volume) {
+      if (volume.attachedTo.length === 0) {
+        navigate(`/pci/projects/${projectId}/storages/blocks`);
+      } else if (volume.attachedTo.length === 1) {
+        setSelectedInstanceId(volume.attachedTo[0]);
+      }
     }
   }, [navigate, projectId, volume, isPending]);
 
@@ -85,27 +102,49 @@ export default function DetachStorage() {
       onOdsModalClose={onClose}
     >
       <slot name="content">
-        {isPending ? (
+        {isPending && (
           <OsdsSpinner
             inline
             size={ODS_SPINNER_SIZE.md}
             className="block text-center"
             data-testid="detachStorage-spinner"
           />
-        ) : (
-          <OsdsText
-            level={ODS_THEME_TYPOGRAPHY_LEVEL.body}
-            size={ODS_THEME_TYPOGRAPHY_SIZE._400}
-            color={ODS_THEME_COLOR_INTENT.text}
-          >
-            {t(
-              'pci_projects_project_storages_blocks_block_detach_detachvolume',
-              {
-                volume: volume?.name,
-                instance: instance?.name,
-              },
-            )}
-          </OsdsText>
+        )}
+        {instances?.length > 1 && (
+          <div>
+            <OsdsSelect
+              value={selectedInstanceId}
+              inline
+              className="mt-5 w-full"
+              onOdsValueChange={(event) => {
+                setSelectedInstanceId(event.detail.value as string);
+              }}
+              data-testid="detachStorage-select-instance"
+            >
+              {instances.map(({ id, name }) => (
+                <OsdsSelectOption key={id} value={id}>
+                  {name}
+                </OsdsSelectOption>
+              ))}
+            </OsdsSelect>
+          </div>
+        )}
+        {!!selectedInstance && (
+          <div className="mt-5">
+            <OsdsText
+              level={ODS_THEME_TYPOGRAPHY_LEVEL.body}
+              size={ODS_THEME_TYPOGRAPHY_SIZE._400}
+              color={ODS_THEME_COLOR_INTENT.text}
+            >
+              {t(
+                'pci_projects_project_storages_blocks_block_detach_detachvolume',
+                {
+                  volume: volume.name,
+                  instance: selectedInstance.name,
+                },
+              )}
+            </OsdsText>
+          </div>
         )}
       </slot>
       <OsdsButton
