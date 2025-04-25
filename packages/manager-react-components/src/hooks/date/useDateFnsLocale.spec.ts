3a9ad440584b44a1feb882615d00b86e5ee9e778
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-04-25 17:10:29 +0200

    feat(mrc): add use format date hook
    Resolve #16372 #16716
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

---
+++ packages/manager-react-components/src/hooks/date/useDateFnsLocale.spec.ts
@@ -0,0 +1,55 @@
+import { vi } from 'vitest';
+import { renderHook } from '@testing-library/react';
+import { useTranslation } from 'react-i18next';
+import { getDateFnsLocale } from '@ovh-ux/manager-core-utils';
+import { useDateFnsLocale } from './useDateFnsLocale';
+
+vi.mock('@ovh-ux/manager-core-utils', () => ({
+  getDateFnsLocale: vi.fn(),
+}));
+
+vi.mock('react-i18next', () => ({
+  useTranslation: vi.fn(),
+}));
+
+vi.mock('date-fns/locale', () => ({
+  enGB: 'enGB',
+  fr: 'fr',
+  frCA: 'frCA',
+  de: 'de',
+  es: 'es',
+  it: 'it',
+  pl: 'pl',
+  pt: 'pt',
+}));
+
+const testCases = [
+  { languageSource: 'en_GB', localeExpected: 'enGB' },
+  { languageSource: 'fr_FR', localeExpected: 'fr' },
+  { languageSource: 'fr_CA', localeExpected: 'frCA' },
+  { languageSource: 'de_DE', localeExpected: 'de' },
+  { languageSource: 'es_ES', localeExpected: 'es' },
+  { languageSource: 'it_IT', localeExpected: 'it' },
+  { languageSource: 'pl_PL', localeExpected: 'pl' },
+  { languageSource: 'pt_PT', localeExpected: 'pt' },
+  { languageSource: 'unknown', localeExpected: 'enGB' },
+  { languageSource: '', localeExpected: 'enGB' },
+  { languageSource: null, localeExpected: 'enGB' },
+  { languageSource: undefined, localeExpected: 'enGB' },
+];
+
+describe('useDateFnsLocale', () => {
+  it.each(testCases)(
+    'should return the correct locale based on the i18n language',
+    ({ languageSource, localeExpected }) => {
+      (useTranslation as jest.Mock).mockReturnValue({
+        i18n: { language: languageSource },
+      });
+      (getDateFnsLocale as jest.Mock).mockReturnValue(localeExpected);
+
+      const { result } = renderHook(() => useDateFnsLocale());
+
+      expect(result.current).toBe(localeExpected);
+    },
+  );
+});
