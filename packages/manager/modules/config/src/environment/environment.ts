5604004b821bf97f35edf2f5c9613641fa48dce3
Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
2025-04-25 13:06:15 +0200

    feat(*): remove last occurances of reket
    ref: #MANAGER-15627
    
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>

--- packages/manager/modules/config/src/environment/environment.ts
+++ packages/manager/modules/config/src/environment/environment.ts
@@ -41,25 +41,23 @@ export interface IEnvironment {
   setApplications: (applications: Applications) => void;
 }
 export class Environment implements IEnvironment {
-  // TODO: After the addition of reket, we had to remove the private attribute
-  // This should be refactored to private and only use accessors.
-  region: Region;
+  private region: Region;
 
-  userLocale: string;
+  private userLocale: string;
 
-  version: string;
+  private version: string;
 
-  user: User;
+  private user: User;
 
-  applicationName: string;
+  private applicationName: string;
 
-  universe: string;
+  private universe: string;
 
-  applicationURLs: Record<string, string>;
+  private applicationURLs: Record<string, string>;
 
-  message: EnvMessage;
+  private message: EnvMessage;
 
-  applications: Applications;
+  private applications: Applications;
 
   constructor(config: Environment = null) {
     this.region = DEFAULT_REGION as Region;
