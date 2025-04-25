6728e8f2f93a14f6d7664cedef9b253af3e439da
mhelhali-soufien <159853039+mhelhali-soufien@users.noreply.github.com>
2025-04-25 10:02:12 +0200

    feat(dedicated-servers): add new columns to server listing (#16516)
    ref: #MANAGER-14392
    
    Signed-off-by: soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>

--- packages/manager/apps/dedicated-servers/src/components/actionCell.tsx
+++ packages/manager/apps/dedicated-servers/src/components/actionCell.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { ShellContext } from '@ovh-ux/manager-react-shell-client';
-import { useOvhTracking } from '@ovh-ux/manager-react-shell-client';
-import { DedicatedServer } from '@/data/types/server.type';
+import {
+  useOvhTracking,
+  ShellContext,
+} from '@ovh-ux/manager-react-shell-client';
 import { OdsPopover, OdsButton } from '@ovhcloud/ods-components/react';
-import { ODS_BUTTON_VARIANT, ODS_ICON_NAME, ODS_POPOVER_POSITION } from '@ovhcloud/ods-components';
+import {
+  ODS_BUTTON_VARIANT,
+  ODS_ICON_NAME,
+  ODS_POPOVER_POSITION,
+} from '@ovhcloud/ods-components';
+import { DedicatedServer } from '@/data/types/server.type';
+
 export const ActionCell: React.FC<DedicatedServer> = (vs) => {
   const { shell } = React.useContext(ShellContext);
   const { t } = useTranslation('dedicated-servers');
