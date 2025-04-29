ac0d499933b00ebd23226ad7465f6bdc65d9dcec
Maxime Bajeux <max.bajeux@gmail.com>
2025-04-29 16:04:57 +0200

    Revert "feat(container): update beta modal (#15199)"
    This reverts commit 0b5f59dfa451a66b4a3768258caf0d6ce099f595.

--- packages/manager/apps/container/src/container/index.tsx
+++ packages/manager/apps/container/src/container/index.tsx
@@ -77,6 +77,7 @@ export default function Container(): JSX.Element {
           style={{ position: 'absolute' }}
         ></LiveChat>
       </ProgressProvider>
+
       <Suspense fallback="">
         <SSOAuthModal />
       </Suspense>
