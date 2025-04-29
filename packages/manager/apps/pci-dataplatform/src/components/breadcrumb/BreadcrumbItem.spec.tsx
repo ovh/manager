f0b32a41ae0f817fd9a083fa2cedc8e658e9e830
Emiri <emiri.ishikawa@ovhcloud.com>
2025-04-29 18:28:22 +0200

    feat(pci-dataplatform): updated test
    ref: #DATATR-2165
    
    Signed-off-by: Emiri <emiri.ishikawa@ovhcloud.com>

--- packages/manager/apps/pci-dataplatform/src/components/breadcrumb/BreadcrumbItem.spec.tsx
+++ packages/manager/apps/pci-dataplatform/src/components/breadcrumb/BreadcrumbItem.spec.tsx
@@ -6,6 +6,7 @@ vi.mock('react-i18next', () => ({
   useTranslation: () => ({
     t: (key: string) => key,
   }),
+  Trans: ({ children }: { children: React.ReactNode }) => children,
 }));
 
 describe('BreadcrumbItem component', () => {
