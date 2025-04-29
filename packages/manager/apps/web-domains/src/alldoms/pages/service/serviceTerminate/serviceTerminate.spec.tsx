c7f5b8bdd8f5b45aa0189de8c8201b9c62a20d9a
Louis BENSI <louis.bensi.ext@corp.ovh.com>
2025-04-29 16:03:05 +0200

    feat(web-domains): detail Page - subscription card
    ref: #MANAGER-17555
    
    Signed-off-by: Louis BENSI <louis.bensi.ext@corp.ovh.com>

--- packages/manager/apps/web-domains/src/alldoms/pages/service/serviceTerminate/serviceTerminate.spec.tsx
+++ packages/manager/apps/web-domains/src/alldoms/pages/service/serviceTerminate/serviceTerminate.spec.tsx
@@ -1,26 +1,27 @@
 import '@/alldoms/setupTests';
 import React from 'react';
+import { vi } from 'vitest';
 import { render, waitFor, screen, fireEvent } from '@testing-library/react';
 import { wrapper } from '@/alldoms/utils/test.provider';
-import Modal from './Modal';
-import { serviceInfoDetailObject } from '@/alldoms/__mocks__/serviceInfoDetail';
+import { serviceInfoDetail } from '@/alldoms/__mocks__/serviceInfoDetail';
+import ServiceTerminate from '@/alldoms/pages/service/serviceTerminate/serviceTerminate';
+import { useGetServiceInfo } from '@/alldoms/hooks/data/useGetServiceInfo';
 
-describe('Modal', () => {
-  const domains = serviceInfoDetailObject.domainAttached;
+vi.mock('@/alldoms/hooks/data/useGetServiceInfo', () => ({
+  useGetServiceInfo: vi.fn(),
+}));
 
+describe('Terminate service', () => {
   it('display the modal', async () => {
-    render(
-      <Modal
-        modalOpen={true}
-        closeModal={null}
-        serviceDetail={serviceInfoDetailObject}
-      />,
-      { wrapper },
-    );
+    (useGetServiceInfo as jest.Mock).mockReturnValue({
+      data: serviceInfoDetail,
+      isLoading: false,
+    });
+
+    render(<ServiceTerminate />, { wrapper });
     expect(screen.getByTestId('modal')).toBeInTheDocument();
 
     await waitFor(() => {
-      expect(screen.getByText('allDom_modal_title')).toBeInTheDocument();
       expect(screen.getByText('allDom_modal_subtitle')).toBeInTheDocument();
       expect(screen.getByText('allDom_modal_choice')).toBeInTheDocument();
       expect(
@@ -29,16 +30,14 @@ describe('Modal', () => {
     });
   });
 
-  domains.forEach((domain) => {
+  (useGetServiceInfo as jest.Mock).mockReturnValue({
+    data: serviceInfoDetail,
+    isLoading: false,
+  });
+
+  serviceInfoDetail.domainAttached.forEach((domain) => {
     it(`should render ${domain} checkbox`, async () => {
-      render(
-        <Modal
-          modalOpen={true}
-          closeModal={null}
-          serviceDetail={serviceInfoDetailObject}
-        />,
-        { wrapper },
-      );
+      render(<ServiceTerminate />, { wrapper });
       await waitFor(async () => {
         const checkbox = await screen.findByTestId(`checkbox-${domain}`);
         const input = checkbox.querySelector('input');
@@ -51,14 +50,12 @@ describe('Modal', () => {
   });
 
   it('should select all checkboxes when the "select all" checkbox is clicked', async () => {
-    render(
-      <Modal
-        modalOpen={true}
-        closeModal={null}
-        serviceDetail={serviceInfoDetailObject}
-      />,
-      { wrapper },
-    );
+    (useGetServiceInfo as jest.Mock).mockReturnValue({
+      data: serviceInfoDetail,
+      isLoading: false,
+    });
+
+    render(<ServiceTerminate />, { wrapper });
 
     const selectAllCheckbox = await screen.findByTestId('checkbox-alldomains');
 
@@ -68,7 +65,7 @@ describe('Modal', () => {
 
     await waitFor(async () => {
       await Promise.all(
-        domains.map(async (domain) => {
+        serviceInfoDetail.domainAttached.map(async (domain) => {
           const checkbox = await screen.findByTestId(`checkbox-${domain}`);
           const input = checkbox.querySelector('input');
           expect(input?.checked).toBe(true);
