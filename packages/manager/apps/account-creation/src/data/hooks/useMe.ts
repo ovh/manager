125f80293e8f71f17af54e0654ed851aeeff00a3
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2025-04-30 15:19:26 +0200

    feat(account-creation): added user and redirection management
    ref: #MANAGER-17333
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

---
+++ packages/manager/apps/account-creation/src/data/hooks/useMe.ts
@@ -0,0 +1,17 @@
+import {
+  UndefinedInitialDataOptions,
+  useQuery,
+  UseQueryResult,
+} from '@tanstack/react-query';
+import { ApiError } from '@ovh-ux/manager-core-api';
+import { User } from '@ovh-ux/manager-config';
+import { getMe } from '@/data/api/me';
+
+export const useMe = (
+  options: Omit<UndefinedInitialDataOptions, 'queryKey' | 'queryFn'> = {},
+): UseQueryResult<User, ApiError> =>
+  useQuery({
+    queryKey: [`me`],
+    queryFn: getMe,
+    ...options,
+  });
