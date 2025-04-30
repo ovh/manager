8dd647b6f041a278b489dbd7e3695d1223e657aa
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-04-30 15:50:20 +0200

    chore(kms): add @secrets aliases
    ref: #MANAGER-17869
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

--- packages/manager/apps/key-management-service/vite.config.mjs
+++ packages/manager/apps/key-management-service/vite.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import { getBaseConfig } from '@ovh-ux/manager-vite-config';
-import { resolve } from 'path';
+import { resolve, join } from 'path';
 
 export default defineConfig({
   ...getBaseConfig(),
+  resolve: {
+    alias: {
+      '@': resolve(join(process.cwd(), 'src')),
+      '@sms': resolve(join(process.cwd(), 'src/modules/sms')),
+    },
+  },
   root: resolve(process.cwd()),
 });
