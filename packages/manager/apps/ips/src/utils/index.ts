000922f013f7e04a48cf416209fd93042f090b21
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-05-02 11:59:19 +0200

    feat(ips): update guide links
    ref: #MANAGER-17644
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager/apps/ips/src/utils/index.ts
+++ packages/manager/apps/ips/src/utils/index.ts
@@ -2,5 +2,5 @@ export * from './iamActions.constants';
 export * from './ipFormatter';
 export * from './getTypeByServiceName';
 export * from './getLinkByServiceName';
-export * from './links.constants';
 export * from './ipFeatureAvailbility';
+export * from './useGuideUtils';
