2ef09afdff20beeaf33f1bbf23c44018033d0f3d
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2025-05-05 11:39:07 +0200

    feat(account-creation): user context adjustements
    ref: #MANAGER-17333
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/apps/account-creation/src/context/user/user.context.ts
+++ packages/manager/apps/account-creation/src/context/user/user.context.ts
@@ -1,8 +1,8 @@
 import { createContext } from 'react';
 import { User } from '@ovh-ux/manager-config';
 
-export type UserContext = User | null;
+export type UserContext = User | undefined;
 
-const userContext = createContext<UserContext>(null);
+const userContext = createContext<UserContext>(undefined);
 
 export default userContext;
