dd9c9523a56d0ffc6e6ccff13ef5c379f26b3de5
Jean-Baptiste Delon <jean-baptiste.delon@ovhcloud.com>
2025-05-05 18:42:49 +0200

    feat(dedicated): copy to clipboard os installation parameters
    ref: #MANAGER-18151
    
    Signed-off-by: Jean-Baptiste Delon <jean-baptiste.delon@ovhcloud.com>

--- packages/manager/modules/bm-server-components/src/general-information/installation/ovh/server-installation-ovh.constants.js
+++ packages/manager/modules/bm-server-components/src/general-information/installation/ovh/server-installation-ovh.constants.js
@@ -11,6 +11,60 @@ export const TEMPLATE_OS_SOFTWARE_RAID_LIST = {
   7: [0, 1, 5, 6, 7, 10],
 };
 
+export const REINSTALL_API_CONSOLE_LINK = {
+  EU:
+    'https://eu.api.ovh.com/console/?section=%2Fdedicated%2Fserver&branch=v1#post-/dedicated/server/-serviceName-/reinstall',
+  CA:
+    'https://ca.api.ovh.com/console/?section=%2Fdedicated%2Fserver&branch=v1#post-/dedicated/server/-serviceName-/reinstall',
+  US:
+    'https://api.us.ovhcloud.com/console/?section=%2Fdedicated%2Fserver&branch=v1#post-/dedicated/server/-serviceName-/reinstall',
+};
+
+export const API_OS_INSTALLATION_DOCUMENTATION_LINK = {
+  ASIA:
+    'https://help.ovhcloud.com/csm/asia-dedicated-servers-api-os-installation?id=kb_article_view&sysparm_article=KB0061947',
+  AU:
+    'https://help.ovhcloud.com/csm/en-au-dedicated-servers-api-os-installation?id=kb_article_view&sysparm_article=KB0061949',
+  CA:
+    'https://help.ovhcloud.com/csm/en-ca-dedicated-servers-api-os-installation?id=kb_article_view&sysparm_article=KB0061946',
+  DE:
+    'https://help.ovhcloud.com/csm/de-dedicated-servers-api-os-installation?id=kb_article_view&sysparm_article=KB0061952',
+  ES:
+    'https://help.ovhcloud.com/csm/es-es-dedicated-servers-api-os-installation?id=kb_article_view&sysparm_article=KB0061954',
+  FR:
+    'https://help.ovhcloud.com/csm/fr-dedicated-servers-api-os-installation?id=kb_article_view&sysparm_article=KB0061958',
+  GB:
+    'https://help.ovhcloud.com/csm/en-gb-dedicated-servers-api-os-installation?id=kb_article_view&sysparm_article=KB0061945',
+  IE:
+    'https://help.ovhcloud.com/csm/en-ie-dedicated-servers-api-os-installation?id=kb_article_view&sysparm_article=KB0061950',
+  IN:
+    'https://help.ovhcloud.com/csm/asia-dedicated-servers-api-os-installation?id=kb_article_view&sysparm_article=KB0061947',
+  IT:
+    'https://help.ovhcloud.com/csm/it-dedicated-servers-api-os-installation?id=kb_article_view&sysparm_article=KB0061956',
+  MA:
+    'https://help.ovhcloud.com/csm/fr-dedicated-servers-api-os-installation?id=kb_article_view&sysparm_article=KB0061958',
+  PL:
+    'https://help.ovhcloud.com/csm/pl-dedicated-servers-api-os-installation?id=kb_article_view&sysparm_article=KB0061957',
+  PT:
+    'https://help.ovhcloud.com/csm/pt-dedicated-servers-api-os-installation?id=kb_article_view&sysparm_article=KB0061959',
+  QC:
+    'https://help.ovhcloud.com/csm/fr-ca-dedicated-servers-api-os-installation?id=kb_article_view&sysparm_article=KB0061953',
+  SG:
+    'https://help.ovhcloud.com/csm/en-sg-dedicated-servers-api-os-installation?id=kb_article_view&sysparm_article=KB0061948',
+  SN:
+    'https://help.ovhcloud.com/csm/fr-dedicated-servers-api-os-installation?id=kb_article_view&sysparm_article=KB0061958',
+  TN:
+    'https://help.ovhcloud.com/csm/fr-dedicated-servers-api-os-installation?id=kb_article_view&sysparm_article=KB0061958',
+  US:
+    'https://support.us.ovhcloud.com/hc/en-us/articles/27030860979859-OVHcloud-API-and-OS-Installation',
+  WE:
+    'https://help.ovhcloud.com/csm/en-ie-dedicated-servers-api-os-installation?id=kb_article_view&sysparm_article=KB0061950',
+  WS:
+    'https://help.ovhcloud.com/csm/es-dedicated-servers-api-os-installation?id=kb_article_view&sysparm_article=KB0061954',
+  DEFAULT:
+    'https://help.ovhcloud.com/csm/en-dedicated-servers-api-os-installation?id=kb_article_view&sysparm_article=KB0061951',
+};
+
 export const EOL_PERSONAL_INSTALLATION_TEMPLATES_DOCUMENTATION_LINK = {
   ASIA:
     'https://help.ovhcloud.com/csm/asia-dedicated-servers-end-of-life-for-personal-installation-templates?id=kb_article_view&sysparm_article=KB0067548',
@@ -60,5 +114,7 @@ export default {
   MOUNT_POINTS,
   MAX_MOUNT_POINTS,
   TEMPLATE_OS_SOFTWARE_RAID_LIST,
+  REINSTALL_API_CONSOLE_LINK,
+  API_OS_INSTALLATION_DOCUMENTATION_LINK,
   EOL_PERSONAL_INSTALLATION_TEMPLATES_DOCUMENTATION_LINK,
 };
