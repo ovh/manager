6810e484636f3656faf9a43ad71dc914ab156582
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2025-05-05 14:23:56 +0200

    feat(pci-instances): add header for dashboard
    ref: #TAPC-4040
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

--- packages/manager/apps/pci-instances/src/pages/instances/instance/Instance.page.tsx
+++ packages/manager/apps/pci-instances/src/pages/instances/instance/Instance.page.tsx
@@ -1,13 +1,42 @@
-import { PageLayout } from '@ovh-ux/manager-react-components';
+import { useRouteLoaderData } from 'react-router-dom';
+import {
+  ChangelogButton,
+  PageLayout,
+  PciGuidesHeader,
+  Subtitle,
+} from '@ovh-ux/manager-react-components';
+import { TProject } from '@ovh-ux/manager-pci-common';
 import { FC } from 'react';
 import { GoBack } from '@/components/navigation/GoBack.component';
 import InstanceWrapper from './InstanceWrapper.page';
+import { Breadcrumb } from '@/components/breadcrumb/Breadcrumb.component';
+import { CHANGELOG_LINKS } from '@/constants';
 
-const Instance: FC = () => (
-  <InstanceWrapper>
-    <PageLayout>
-      <GoBack />
-    </PageLayout>
-  </InstanceWrapper>
-);
+const Instance: FC = () => {
+  const project = useRouteLoaderData('root') as TProject;
+
+  return (
+    <InstanceWrapper>
+      <PageLayout>
+        {project && (
+          <Breadcrumb
+            projectLabel={project.description ?? ''}
+            items={[{ label: 'instance-region-name' }]}
+          />
+        )}
+        <div className="header mb-6 mt-8">
+          <div className="flex items-center justify-between">
+            <Subtitle>{'instance-region-name'}</Subtitle>
+            <div className="flex gap-x-3">
+              <ChangelogButton links={CHANGELOG_LINKS} />
+              <PciGuidesHeader category="instances" />
+            </div>
+          </div>
+        </div>
+        <GoBack />
+      </PageLayout>
+    </InstanceWrapper>
+  );
+};
+
 export default Instance;
