c1a248def369137d2e10f0af9ca95837129a57f7
Louis BENSI <louis.bensi.ext@corp.ovh.com>
2025-05-06 11:54:25 +0200

    feat(web-domain): service detail list
    ref: #MANAGER-17030
    
    Signed-off-by: Louis BENSI <louis.bensi.ext@corp.ovh.com>

--- packages/manager/apps/web-domains/src/alldoms/hooks/data/query.tsx
+++ packages/manager/apps/web-domains/src/alldoms/hooks/data/query.tsx
@@ -1,9 +1,35 @@
 import { useQuery } from '@tanstack/react-query';
-import { getallDomList } from '@/alldoms/data/api/web-domains';
+import {
+  getAllDomainAttachedToAllDom,
+  getallDomList,
+  getAllDomProperty,
+  getallDomService,
+} from '@/alldoms/data/api/web-domains';
 
 export const useGetAllDomServiceList = () => {
-  return useQuery<number[]>({
-    queryKey: ['list'],
+  return useQuery<string[]>({
+    queryKey: ['allDom', 'services'],
     queryFn: () => getallDomList(),
+  });
+};
+
+export const useGetAllDomProperty = (serviceName: string) => {
+  return useQuery({
+    queryKey: ['alldom', serviceName, 'property'],
+    queryFn: () => getAllDomProperty(serviceName),
+  });
+};
+
+export const useGetAllDomService = (serviceName: string) => {
+  return useQuery({
+    queryKey: ['allDom', 'services', serviceName],
+    queryFn: () => getallDomService(serviceName),
+  });
+};
+
+export const useGetAllDomainAttachedToAllDom = (serviceName: string) => {
+  return useQuery<string[]>({
+    queryKey: ['allDom', 'services', serviceName, 'attachedDomains'],
+    queryFn: () => getAllDomainAttachedToAllDom(serviceName),
   });
 };
