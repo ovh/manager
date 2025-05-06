ed0e3fefd35256f76cee1cdc882597be698ed933
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-05-06 19:05:38 +0200

    feat(vcd): add delete vrack network
    ref: #MANAGER-17528
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

---
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/pages/dashboard/datacentre/vrack-segment/delete-network/DeleteVrackNetwork.spec.tsx
@@ -0,0 +1,90 @@
+import userEvent from '@testing-library/user-event';
+import { expect } from 'vitest';
+import { act, screen, waitFor } from '@testing-library/react';
+import {
+  organizationList,
+  datacentreList,
+  mockVrackSegmentList,
+} from '@ovh-ux/manager-module-vcd-api';
+import { labels, renderTest } from '../../../../../test-utils';
+import { encodeVrackNetwork } from '../../../../../utils/encodeVrackNetwork';
+import { urls, subRoutes } from '../../../../../routes/routes.constant';
+
+const testVrack = mockVrackSegmentList[0];
+const initialRoute = urls.vrackSegmentDeleteNetwork
+  .replace(subRoutes.dashboard, organizationList[0].id)
+  .replace(subRoutes.vdcId, datacentreList[0].id)
+  .replace(subRoutes.vrackSegmentId, testVrack.id)
+  .replace(
+    subRoutes.vrackNetworkId,
+    encodeVrackNetwork(testVrack.targetSpec.networks[0]),
+  );
+
+const {
+  managed_vcd_dashboard_vrack_network_delete_subnet: title,
+  managed_vcd_dashboard_vrack_network_delete_subnet_content1: content,
+  managed_vcd_dashboard_vrack_network_delete_subnet_content2: content2,
+  managed_vcd_dashboard_vrack_network_delete_subnet_success: success,
+  managed_vcd_dashboard_vrack_network_delete_subnet_error: error,
+} = labels.datacentresVrackNetwork;
+
+const checkModalContent = () => {
+  expect(screen.getByText(title)).toBeVisible();
+  expect(screen.getByText(content)).toBeVisible();
+  expect(screen.getByText(content2)).toBeVisible();
+};
+
+describe('Delete Vrack Network Page', () => {
+  it('should delete the network and display a success banner', async () => {
+    await renderTest({ initialRoute });
+
+    // check modal content
+    await waitFor(() => checkModalContent(), { timeout: 10_000 });
+    const modal = screen.getByTestId('modal');
+
+    // submit modal
+    const submitCta = screen.getByTestId('primary-button');
+    expect(submitCta).toBeEnabled();
+    await act(() => userEvent.click(submitCta));
+
+    // check modal visibility
+    await waitFor(() => expect(modal).not.toBeInTheDocument(), {
+      timeout: 10_000,
+    });
+
+    // check success banner
+    expect(screen.getByText(success)).toBeVisible();
+  });
+
+  // TODO : unskip when page is unmocked
+  it.skip('should display an error if updateService is KO', async () => {
+    const { debug, container } = await renderTest({
+      initialRoute,
+      isVrackSegmentUpdateKo: true,
+    });
+
+    // check modal content
+    await waitFor(() => checkModalContent(), { timeout: 10_000 });
+    const modal = screen.getByTestId('modal');
+
+    // submit modal
+    const submitCta = screen.getByTestId('primary-button');
+    await waitFor(
+      async () => {
+        expect(submitCta).toBeEnabled();
+        debug(container, Infinity);
+        await act(() => userEvent.click(submitCta));
+      },
+      { timeout: 10_000 },
+    );
+
+    // check modal visibility
+    await waitFor(() => expect(modal).not.toBeInTheDocument(), {
+      timeout: 10_000,
+    });
+
+    // check error banner
+    const testError = error.replace('{{errorApi}}', '');
+    expect(screen.getByText(testError)).toBeVisible();
+  });
+});
