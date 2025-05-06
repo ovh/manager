8e30ea519ff889fa81450b4eeac1f1025c052a4e
Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>
2025-05-06 09:26:57 +0200

    feat(project-vmware-logs): implement fallback dashboard
    REF: #MANAGER-17691
    Signed-off-by: Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>

--- packages/manager/apps/hpc-vmware-vsphere/src/hooks/guide/useGuideUtils.tsx
+++ packages/manager/apps/hpc-vmware-vsphere/src/hooks/guide/useGuideUtils.tsx
@@ -1,66 +1,7 @@
 import { useContext, useEffect, useState } from 'react';
 import { CountryCode } from '@ovh-ux/manager-config';
 import { ShellContext } from '@ovh-ux/manager-react-shell-client';
-
-const docUrl = 'https://docs.ovh.com';
-
-type GuideLinks = { [key in CountryCode]: string };
-
-const GUIDE_LIST: { [guideName: string]: Partial<GuideLinks> } = {
-  guideLink1: {
-    DE: '/update-path',
-    ES: '/update-path',
-    IE: '/en/update-path',
-    IT: '/update-path',
-    PL: '/update-path',
-    PT: '/update-path',
-    FR: '/update-path',
-    GB: '/update-path',
-    CA: '/update-path',
-    QC: '/update-path',
-    WE: '/update-path',
-    WS: '/update-path',
-    US: '/update-path',
-  },
-  guideLink2: {
-    DE: '/guide-link-2-path',
-    ES: '/guide-link-2-path',
-    IE: '/en/guide-link-2-path',
-    IT: '/guide-link-2-path',
-    PL: '/guide-link-2-path',
-    PT: '/guide-link-2-path',
-    FR: '/guide-link-2-path',
-    GB: '/guide-link-2-path',
-    CA: '/update-path',
-    QC: '/update-path',
-    WE: '/update-path',
-    WS: '/update-path',
-    US: '/update-path',
-  },
-  guideLink3: {
-    DE: '/guide-link-3-path',
-    ES: '/guide-link-3-path',
-    IE: '/en/guide-link-3-path',
-    IT: '/guide-link-3-path',
-    PL: '/guide-link-3-path',
-    PT: '/guide-link-3-path',
-    FR: '/guide-link-3-path',
-    GB: '/guide-link-3-path',
-    CA: '/update-path',
-    QC: '/update-path',
-    WE: '/update-path',
-    WS: '/update-path',
-    US: '/update-path',
-  },
-  /*
-  addNewGuideLink : {
-    DEFAULT: '/guide-link-3-path',
-    DE: '/guide-link-3-path',
-    ES: '/guide-link-3-path',
-    ...
-  }
-  */
-};
+import { GUIDE_LIST, SUPPORT_URL } from './guidesLinks.constants';
 
 type GetGuideLinkProps = {
   name?: string;
@@ -71,7 +12,11 @@ function getGuideListLink({ subsidiary }: GetGuideLinkProps) {
   const list: { [guideName: string]: string } = {};
   const keys = Object.entries(GUIDE_LIST);
   keys.forEach((key) => {
-    list[key[0]] = docUrl + GUIDE_LIST[key[0]][subsidiary as CountryCode];
+    const guide =
+      GUIDE_LIST[key[0]][subsidiary as CountryCode] || GUIDE_LIST[key[0]].GB;
+
+    const helpCenterUrl = SUPPORT_URL[subsidiary] ?? SUPPORT_URL.EU;
+    list[key[0]] = `${helpCenterUrl}${guide}`;
   });
   return list;
 }
