bcbab61f3b8461fe1a2a4fec8f5377a6d7ddec53
Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>
2025-05-06 09:26:57 +0200

    feat(project-vmware-logs): implement fallback dashboard
    REF: #MANAGER-17691
    Signed-off-by: Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>

--- packages/manager/apps/hpc-vmware-vsphere/src/hooks/guide/useGuideUtils.tsx
+++ packages/manager/apps/hpc-vmware-vsphere/src/hooks/guide/useGuideUtils.tsx
@@ -2,64 +2,89 @@ import { useContext, useEffect, useState } from 'react';
 import { CountryCode } from '@ovh-ux/manager-config';
 import { ShellContext } from '@ovh-ux/manager-react-shell-client';
 
-const docUrl = 'https://docs.ovh.com';
+const docUrl = 'https://help.ovhcloud.com/csm/';
 
 type GuideLinks = { [key in CountryCode]: string };
 
 const GUIDE_LIST: { [guideName: string]: Partial<GuideLinks> } = {
-  guideLink1: {
-    DE: '/update-path',
-    ES: '/update-path',
-    IE: '/en/update-path',
-    IT: '/update-path',
-    PL: '/update-path',
-    PT: '/update-path',
-    FR: '/update-path',
-    GB: '/update-path',
-    CA: '/update-path',
-    QC: '/update-path',
-    WE: '/update-path',
-    WS: '/update-path',
-    US: '/update-path',
+  discover: {
+    DE:
+      '/de-vmware-control-panel-guided-tour?id=kb_article_view&sysparm_article=KB0045728',
+    ES:
+      '/es-es-vmware-control-panel-guided-tour?id=kb_article_view&sysparm_article=KB0045741',
+    IE:
+      '/en-ie-vmware-control-panel-guided-tour?id=kb_article_view&sysparm_article=KB0032943',
+    IT:
+      '/it-vmware-control-panel-guided-tour?id=kb_article_view&sysparm_article=KB0045743',
+    PL:
+      '/pl-vmware-control-panel-guided-tour?id=kb_article_view&sysparm_article=KB0045745',
+    PT:
+      '/pt-vmware-control-panel-guided-tour?id=kb_article_view&sysparm_article=KB0045833',
+    FR:
+      '/fr-vmware-control-panel-guided-tour?id=kb_article_view&sysparm_article=KB0045740',
+    GB:
+      '/en-gb-vmware-control-panel-guided-tour?id=kb_article_view&sysparm_article=KB0045834',
+    CA:
+      '/en-ca-vmware-control-panel-guided-tour?id=kb_article_view&sysparm_article=KB0045744',
+    QC:
+      '/fr-ca-vmware-control-panel-guided-tour?id=kb_article_view&sysparm_article=KB0045738',
+    WE:
+      '/en-vmware-control-panel-guided-tour?id=kb_article_view&sysparm_article=KB0045739',
+    WS:
+      '/es-vmware-control-panel-guided-tour?id=kb_article_view&sysparm_article=KB0045742',
   },
-  guideLink2: {
-    DE: '/guide-link-2-path',
-    ES: '/guide-link-2-path',
-    IE: '/en/guide-link-2-path',
-    IT: '/guide-link-2-path',
-    PL: '/guide-link-2-path',
-    PT: '/guide-link-2-path',
-    FR: '/guide-link-2-path',
-    GB: '/guide-link-2-path',
-    CA: '/update-path',
-    QC: '/update-path',
-    WE: '/update-path',
-    WS: '/update-path',
-    US: '/update-path',
+  vsphere_access: {
+    DE:
+      '/de-vmware-login-vsphere-interface?id=kb_article_view&sysparm_article=KB0033694',
+    ES:
+      '/es-es-vmware-login-vsphere-interface?id=kb_article_view&sysparm_article=KB0046439',
+    IE:
+      '/en-ie-vmware-login-vsphere-interface?id=kb_article_view&sysparm_article=KB0046429',
+    IT:
+      '/it-vmware-login-vsphere-interface?id=kb_article_view&sysparm_article=KB0046449',
+    PL:
+      '/pl-vmware-login-vsphere-interface?id=kb_article_view&sysparm_article=KB0046441',
+    PT:
+      '/pt-vmware-login-vsphere-interface?id=kb_article_view&sysparm_article=KB0046446',
+    FR:
+      '/fr-vmware-login-vsphere-interface?id=kb_article_view&sysparm_article=KB0046435',
+    GB:
+      '/en-gb-vmware-login-vsphere-interface?id=kb_article_view&sysparm_article=KB0046440',
+    CA:
+      '/en-ca-vmware-login-vsphere-interface?id=kb_article_view&sysparm_article=KB0046438',
+    QC:
+      '/fr-ca-vmware-login-vsphere-interface?id=kb_article_view&sysparm_article=KB0046447',
+    WE:
+      '/en-vmware-login-vsphere-interface?id=kb_article_view&sysparm_article=KB0046431',
+    WS:
+      '/es-vmware-login-vsphere-interface?id=kb_article_view&sysparm_article=KB0046445',
   },
-  guideLink3: {
-    DE: '/guide-link-3-path',
-    ES: '/guide-link-3-path',
-    IE: '/en/guide-link-3-path',
-    IT: '/guide-link-3-path',
-    PL: '/guide-link-3-path',
-    PT: '/guide-link-3-path',
-    FR: '/guide-link-3-path',
-    GB: '/guide-link-3-path',
-    CA: '/update-path',
-    QC: '/update-path',
-    WE: '/update-path',
-    WS: '/update-path',
-    US: '/update-path',
+  veeam_backup: {
+    DE:
+      '/de-vmware-veeam-backup-as-a-service?id=kb_article_view&sysparm_article=KB0064987',
+    ES:
+      '/es-es-vmware-veeam-backup-as-a-service?id=kb_article_view&sysparm_article=KB0065001',
+    IE:
+      '/en-ie-vmware-veeam-backup-as-a-service?id=kb_article_view&sysparm_article=KB0046154',
+    IT:
+      '/it-vmware-veeam-backup-as-a-service?id=kb_article_view&sysparm_article=KB0065003',
+    PL:
+      '/pl-vmware-veeam-backup-as-a-service?id=kb_article_view&sysparm_article=KB0064998',
+    PT:
+      '/pt-vmware-veeam-backup-as-a-service?id=kb_article_view&sysparm_article=KB0065002',
+    FR:
+      '/fr-vmware-veeam-backup-as-a-service?id=kb_article_view&sysparm_article=KB0046156',
+    GB:
+      '/en-gb-vmware-veeam-backup-as-a-service?id=kb_article_view&sysparm_article=KB0046150',
+    CA:
+      '/en-ca-vmware-veeam-backup-as-a-service?id=kb_article_view&sysparm_article=KB0046148',
+    QC:
+      '/fr-ca-vmware-veeam-backup-as-a-service?id=kb_article_view&sysparm_article=KB0046159',
+    WE:
+      '/en-vmware-veeam-backup-as-a-service?id=kb_article_view&sysparm_article=KB0046155',
+    WS:
+      '/es-vmware-veeam-backup-as-a-service?id=kb_article_view&sysparm_article=KB0064999',
   },
-  /*
-  addNewGuideLink : {
-    DEFAULT: '/guide-link-3-path',
-    DE: '/guide-link-3-path',
-    ES: '/guide-link-3-path',
-    ...
-  }
-  */
 };
 
 type GetGuideLinkProps = {
