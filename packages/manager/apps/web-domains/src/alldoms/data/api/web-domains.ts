d1e18f9f6c327a86018e60bc9a40983f027db502
Louis BENSI <louis.bensi.ext@corp.ovh.com>
2025-05-06 11:54:24 +0200

    feat(web-domain): set up base project
    ref: #MANAGER-17028
    
    Signed-off-by: Louis BENSI <louis.bensi.ext@corp.ovh.com>

--- packages/manager/apps/web-domains/src/alldoms/data/api/web-domains.ts
+++ packages/manager/apps/web-domains/src/alldoms/data/api/web-domains.ts
@@ -1,44 +1,16 @@
-import { fetchIcebergV2, fetchIcebergV6, apiClient } from '@ovh-ux/manager-core-api';
-
-
-export type GetallDomListParams = {
-  /** Filter resources on IAM tags */
-  'iamTags': any;
-};
-
-export const getallDomListQueryKey = ['get/allDom'];
+import { v6 } from '@ovh-ux/manager-core-api';
+import { TServiceInfo } from '@/alldoms/types';
 
 /**
  *  : List available AllDom services
  */
-export const getallDomList = async (params: GetallDomListParams): Promise<any> =>
-  apiClient.v6.get('/allDom', { data: params });
-
-export type GetallDomServiceParams = {
-  /** Service name */
-  'serviceName'?: any;
-};
-
-export const getallDomServiceQueryKey = (params: GetallDomServiceParams) => [`get/allDom/${params.serviceName}`];
+export const getallDomList = async (): Promise<[]> =>
+  v6.get('/allDom').then((res) => res.data);
 
 /**
  *  : Get this AllDom properties
  */
-export const getallDomService = async (params: GetallDomServiceParams): Promise<any> =>
-  apiClient.v6.get(`/allDom/${params.serviceName}`);
-
-/**
- *  Get listing with iceberg V6
- */
-export const getListingIcebergV6 = async ({ pageSize, page }: { pageSize: number, page: number }) => {
-  const { data, status, totalCount } = await fetchIcebergV6({
-    route: `/allDom`,
-    pageSize,
-    page
-  });
-  if (status > 400) {
-    throw new Error();
-  }
-  return { data, status, totalCount };
-};
-
+export const getallDomService = async (
+  serviceName: string,
+): Promise<TServiceInfo> =>
+  v6.get(`/allDom/${serviceName}/serviceInfos`).then((res) => res.data);
