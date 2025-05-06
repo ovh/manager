c0855ae3b4bdbc1148d3d5b45c0d5ee818a397ad
Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>
2025-05-06 15:24:40 +0200

    fix(web-hosting): remove wrong ssl hooks file
    Signed-off-by: Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>

--- packages/manager/apps/web-hosting/src/data/hooks/ssl/useSsl.ts
+++ packages/manager/apps/web-hosting/src/data/hooks/ssl/useSsl.ts
@@ -2,7 +2,10 @@ import { useMutation } from '@tanstack/react-query';
 import {
   deleteDomainCertificate,
   regenerateDomainCertificate,
-} from '@/data//api/ssl';
+  createCertificate,
+  createDomainCertificate,
+  createDomainCertificates,
+} from '@/data/api/ssl';
 
 export const useDeleteDomainCertificate = (
   serviceName: string,
@@ -36,6 +39,67 @@ export const useRegenerateDomainCertificate = (
 
   return {
     regenerateDomainCertificate: mutation.mutate,
+    ...mutation,
+  };
+};
+
+export const useCreateCertificate = (
+  serviceName: string,
+  onSuccess: () => void,
+  onError: () => void,
+) => {
+  const mutation = useMutation({
+    mutationFn: ({
+      certificate,
+      key,
+      chain,
+    }: {
+      certificate?: string;
+      key?: string;
+      chain?: string;
+    }) => createCertificate(serviceName, certificate, key, chain),
+    onSuccess,
+    onError,
+  });
+
+  return {
+    createCertificate: mutation.mutate,
+    ...mutation,
+  };
+};
+
+export const useCreateDomainCertificate = (
+  serviceName: string,
+  onSuccess: () => void,
+  onError: () => void,
+) => {
+  const mutation = useMutation({
+    mutationFn: (domain: string) =>
+      createDomainCertificate(serviceName, domain),
+    onSuccess,
+    onError,
+  });
+
+  return {
+    createDomainCertificate: mutation.mutate,
+    ...mutation,
+  };
+};
+
+export const useCreateDomainCertificates = (
+  serviceName: string,
+  onSuccess: () => void,
+  onError: () => void,
+) => {
+  const mutation = useMutation({
+    mutationFn: (domains: string[]) =>
+      createDomainCertificates(serviceName, domains),
+    onSuccess,
+    onError,
+  });
+
+  return {
+    createDomainCertificates: mutation.mutate,
     ...mutation,
   };
 };
