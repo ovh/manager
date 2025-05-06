1c1c56822d746ee10d367ab5f26c949c585c620b
Louis BENSI <louis.bensi.ext@corp.ovh.com>
2025-05-06 11:54:26 +0200

    feat(web-domains): action - terminate
    ref: #MANAGER-17034
    
    Signed-off-by: Louis BENSI <louis.bensi.ext@corp.ovh.com>

---
+++ packages/manager/apps/web-domains/src/alldoms/__mocks__/domainAttached.ts
@@ -0,0 +1,10 @@
+export const domainAttached: string[] = [
+  'testdomain.be',
+  'testdomain.biz',
+  'testdomain.com',
+  'testdomain.eu',
+  'testdomain.fr',
+  'testdomain.info',
+  'testdomain.net',
+  'testdomain.org',
+];
---
+++ packages/manager/apps/web-domains/src/alldoms/components/Modal/Steps/Modal.stepTwo.tsx
@@ -0,0 +1,87 @@
+import {
+  ODS_BUTTON_COLOR,
+  ODS_BUTTON_VARIANT,
+  ODS_TEXT_PRESET,
+} from '@ovhcloud/ods-components';
+import { OdsButton, OdsText } from '@ovhcloud/ods-components/react';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { useNotifications } from '@ovh-ux/manager-react-components';
+import { ModalStepsProps } from '@/alldoms/types';
+import {
+  useUpdateAllDomService,
+  useUpdateDomainServiceInfo,
+} from '@/alldoms/hooks/data/query';
+
+export default function ModalStepTwo({
+  domainTerminateList,
+  serviceInfoDetail,
+  changeStep,
+  closeModal,
+}: Readonly<ModalStepsProps>) {
+  const { t } = useTranslation('allDom');
+  const { domain: serviceName } = serviceInfoDetail.serviceInfo;
+  const { addError, addSuccess } = useNotifications();
+  const updateAllDomServiceMutation = useUpdateAllDomService();
+  const updateDomainServiceInfoMutation = useUpdateDomainServiceInfo();
+
+  const handleTerminate = async () => {
+    try {
+      await updateAllDomServiceMutation.mutateAsync(serviceName);
+
+      await Promise.all(
+        domainTerminateList.map((domainName) =>
+          updateDomainServiceInfoMutation.mutateAsync(domainName),
+        ),
+      );
+
+      addSuccess(
+        <OdsText>
+          {t('allDom_modal_success_message', {
+            t0: serviceName,
+            t1: domainTerminateList.join(', '),
+          })}
+        </OdsText>,
+      );
+    } catch (error) {
+      addError(
+        <OdsText>
+          {t('allDom_modal_error_message', {
+            t0: error.message,
+          })}
+        </OdsText>,
+      );
+    } finally {
+      closeModal();
+    }
+  };
+
+  return (
+    <div>
+      <OdsText preset={ODS_TEXT_PRESET.paragraph}>
+        {t('allDom_modal_step_two_warning')}
+      </OdsText>
+      <ul className="flex flex-col gap-y-2 ml-2 pl-8">
+        {domainTerminateList.map((element) => (
+          <li className="text-[#4D5592]" key={element}>
+            {element}
+          </li>
+        ))}
+      </ul>
+      <div className="flex items-center gap-x-6 justify-end">
+        <OdsButton
+          label={t('allDom_modal_step_previous')}
+          variant={ODS_BUTTON_VARIANT.ghost}
+          onClick={() => changeStep()}
+        />
+        <OdsButton
+          label={t('allDom_modal_step_terminate')}
+          variant={ODS_BUTTON_VARIANT.default}
+          color={ODS_BUTTON_COLOR.critical}
+          isDisabled={!domainTerminateList}
+          onClick={handleTerminate}
+        />
+      </div>
+    </div>
+  );
+}
