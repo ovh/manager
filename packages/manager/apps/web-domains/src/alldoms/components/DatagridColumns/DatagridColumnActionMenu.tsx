c3385ac176c0dce9e68e22bbbc175c9adac28ea2
Louis BENSI <louis.bensi.ext@corp.ovh.com>
2025-05-06 11:54:26 +0200

    feat(web-domains): action enable/disable automatic renewal
    ref: #MANAGER-17035
    
    Signed-off-by: Louis BENSI <louis.bensi.ext@corp.ovh.com>

--- packages/manager/apps/web-domains/src/alldoms/components/DatagridColumns/DatagridColumnActionMenu.tsx
+++ packages/manager/apps/web-domains/src/alldoms/components/DatagridColumns/DatagridColumnActionMenu.tsx
@@ -3,6 +3,8 @@ import { ODS_BUTTON_VARIANT } from '@ovhcloud/ods-components';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { TServiceDetail } from '@/alldoms/types';
+import { useUpdateAllDomService } from '@/alldoms/hooks/data/query';
+import { ServiceInfoRenewMode } from '@/alldoms/enum/service.enum';
 
 interface DatagridColumnActionMenuProps {
   readonly serviceId: string;
@@ -16,6 +18,9 @@ export default function DatagridColumnActionMenu({
   openModal,
 }: DatagridColumnActionMenuProps) {
   const { t } = useTranslation('allDom');
+  const { automatic } = serviceInfoDetail.serviceInfo.renew;
+  const updateAllDomServiceMutation = useUpdateAllDomService();
+
   return (
     <ActionMenu
       id={serviceId}
@@ -33,6 +38,22 @@ export default function DatagridColumnActionMenu({
           id: 2,
           label: t('allDom_table_action_terminate'),
           onClick: () => openModal(serviceInfoDetail),
+        },
+        {
+          id: 3,
+          label: automatic
+            ? t('allDom_table_action_disable_renewal')
+            : t('allDom_table_action_enable_renewal'),
+          onClick: () => {
+            updateAllDomServiceMutation.mutateAsync({
+              serviceName: serviceInfoDetail.allDomProperty.name,
+              renew: {
+                mode: automatic
+                  ? ServiceInfoRenewMode.Manual
+                  : ServiceInfoRenewMode.Automatic,
+              },
+            });
+          },
         },
       ]}
     />
