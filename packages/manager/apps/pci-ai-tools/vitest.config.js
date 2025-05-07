f6c8372c989249a1241e6019da40591afe331637
helabenkhalfallah <hela.ben-khalfallah@corp.ovh.com>
2025-05-07 17:57:45 +0200

    refactor(*): restore applications configuration
    ref: MANAGER-18041
    
    Signed-off-by: helabenkhalfallah <hela.ben-khalfallah@corp.ovh.com>

--- packages/manager/apps/pci-ai-tools/vitest.config.js
+++ packages/manager/apps/pci-ai-tools/vitest.config.js
@@ -1,39 +1,37 @@
 import path from 'path';
-import {
-  sharedConfig,
-  mergeConfig,
-  createConfig,
-  defaultCoverageConfig,
-} from '@ovh-ux/manager-unit-tests-config';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default mergeConfig(
-  sharedConfig,
-  createConfig({
-    test: {
-      setupFiles: ['./src/__tests__/setupTest.ts'],
-      coverage: {
-        exclude: [
-          'src/types',
-          'src/__tests__',
-          'src/vite-*.ts',
-          'src/App.tsx',
-          'src/i18n.ts',
-          'src/index.tsx',
-          'src/query.client.ts',
-          'src/components/ui',
-          'src/components/data-table/translations',
-          'src/configuration',
-          'src/**/*constants.ts',
-          'src/main.tsx',
-          'src/routes',
-          ...defaultCoverageConfig.exclude,
-        ],
-      },
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/__tests__/setupTest.ts'],
+    coverage: {
+      include: ['src'],
+      exclude: [
+        'src/types',
+        'src/__tests__',
+        'src/vite-*.ts',
+        'src/App.tsx',
+        'src/i18n.ts',
+        'src/index.tsx',
+        'src/query.client.ts',
+        'src/components/ui',
+        'src/components/data-table/translations',
+        'src/configuration',
+        'src/**/*constants.ts',
+        'src/main.tsx',
+        'src/routes',
+      ],
     },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src'),
-      },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
-  }),
-);
+    mainFields: ['module'],
+  },
+});
--- packages/manager/apps/zimbra/vitest.config.js
+++ packages/manager/apps/zimbra/vitest.config.js
@@ -1,36 +1,35 @@
 import path from 'path';
-import {
-  sharedConfig,
-  mergeConfig,
-  createConfig,
-  defaultCoverageConfig,
-} from '@ovh-ux/manager-unit-tests-config';
+import { defineConfig, coverageConfigDefaults } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
-export default mergeConfig(
-  sharedConfig,
-  createConfig({
-    test: {
-      setupFiles: ['src/utils/test.setup.tsx'],
-      coverage: {
-        exclude: [
-          'src/data/api',
-          'src/zimbra.config.ts',
-          'src/vite-*.ts',
-          'src/App.tsx',
-          'src/hooks/index.ts',
-          'src/index.tsx',
-          'src/routes/routes.tsx',
-          'src/utils/index.ts',
-          'src/**/*constants.ts',
-          ...defaultCoverageConfig.exclude,
-        ],
-      },
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      include: ['src'],
+      exclude: [
+        'src/data/api',
+        'src/zimbra.config.ts',
+        'src/vite-*.ts',
+        'src/App.tsx',
+        'src/hooks/index.ts',
+        'src/index.tsx',
+        'src/routes/routes.tsx',
+        'src/utils/index.ts',
+        'src/**/*constants.ts',
+        ...coverageConfigDefaults.exclude,
+      ],
     },
-    resolve: {
-      alias: {
-        '@/public': path.resolve(__dirname, 'public'),
-        '@': path.resolve(__dirname, 'src'),
-      },
+    setupFiles: ['src/utils/test.setup.tsx'],
+  },
+  resolve: {
+    alias: {
+      '@/public': path.resolve(__dirname, 'public'),
+      '@': path.resolve(__dirname, 'src'),
     },
-  }),
-);
+    mainFields: ['module'],
+  },
+});
