e4b835ae5f450c0a5a679f5b1221a4edc5cca9e7
Lio B <lionel95200x@users.noreply.github.com>
2025-05-07 17:05:16 +0200

    feat(svp): dashboard page - EPIC (#15014)
    ref: #TAPC-2358, #TAPC-1775
    
    Signed-off-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>
    Co-authored-by: Eric Ciccotti <eric.ciccotti.ext@corp.ovh.com>

--- packages/manager/apps/pci-savings-plan/src/utils/routes.ts
+++ packages/manager/apps/pci-savings-plan/src/utils/routes.ts
@@ -3,6 +3,9 @@ const getProjectIdUrl = (projectId: string) => `/pci/projects/${projectId}`;
 export const getSavingsPlansUrl = (projectId: string) =>
   `${getProjectIdUrl(projectId)}/savings-plan`;
 
+export const getSavingsPlansListingUrl = (projectId: string) =>
+  `${getProjectIdUrl(projectId)}/savings-plan/listing`;
+
 export const getOnboardingUrl = (projectId: string) =>
   `${getSavingsPlansUrl(projectId)}/onboarding`;
 
