ed6b58fade67f6cd5324943dce58711d9d92695f
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2025-05-07 10:31:56 +0200

    fix(sign-up): use new level 2 for tracking
    ref: #MANAGER-18234
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/apps/sign-up/src/at-internet.constants.js
+++ packages/manager/apps/sign-up/src/at-internet.constants.js
@@ -1,17 +1,17 @@
 export const TRACKING = {
   EU: {
     config: {
-      level2: '95',
+      level2: '126',
     },
   },
   CA: {
     config: {
-      level2: '95',
+      level2: '126',
     },
   },
   US: {
     config: {
-      level2: '95',
+      level2: '126',
     },
   },
 };
