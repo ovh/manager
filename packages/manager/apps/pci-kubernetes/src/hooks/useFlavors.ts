c80bf27d77ae8d7100f19b34fe3354a675cab387
Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>
2025-05-07 10:56:39 +0200

    feat(pci.kubernetes): filter quota api by region
    ref: #TAPC-3575
    
    Signed-off-by: Pierre-Philippe <pierre-philippe.prevost.ext@ovhcloud.com>

--- packages/manager/apps/pci-kubernetes/src/hooks/useFlavors.ts
+++ packages/manager/apps/pci-kubernetes/src/hooks/useFlavors.ts
@@ -2,7 +2,6 @@ import { useMemo } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import {
   useCatalog,
-  useProjectQuota,
   useProductAvailability,
   TFlavor,
   TQuota,
@@ -11,6 +10,7 @@ import {
 
 import { getKubeFlavors } from '@/api/data/flavors';
 import { DeploymentMode } from '@/types';
+import { useProjectQuotaByRegion } from './useProjectQuota';
 
 export const FLAVOR_CATEGORIES = [
   {
@@ -106,9 +106,9 @@ export const useMergedKubeFlavors = (projectId: string, region: string) => {
     isPending: isAvailabilityPending,
   } = useProductAvailability(projectId);
 
-  const { data: quota, isPending: isQuotaPending } = useProjectQuota(
+  const { data: quota, isPending: isQuotaPending } = useProjectQuotaByRegion(
     projectId,
-    { region },
+    region,
   );
 
   const isPending =
