92bf32b02b7e253f4937acc4b3e28d03fbad57ad
ahmed sefiani <ahmed.sefiani.ext@corp.ovh.com>
2025-05-09 13:52:54 +0200

    feat(veeam): ftc veeam backup modal resiliation
    ref: #MANAGER-17908
    
    Signed-off-by: ahmed sefiani <ahmed.sefiani.ext@corp.ovh.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/apps/veeam-backup/src/constants.ts
+++ packages/manager/apps/veeam-backup/src/constants.ts
@@ -1,5 +1,6 @@
 import { ChangelogLinks } from '@ovh-ux/manager-react-components';
 
+export const US_SUBSIDIARY = 'US';
 export const CHANGELOG_LINKS: ChangelogLinks = {
   changelog:
     'https://github.com/orgs/ovh/projects/16/views/6?pane=info&sliceBy%5Bvalue%5D=Backup+and+Disaster+Recovery',
