22dbc35b1af75f24c139a1ced682363002b41e35
ahmed sefiani <ahmed.sefiani.ext@corp.ovh.com>
2025-05-09 13:52:54 +0200

    feat(veeam): ftc veeam backup modal resiliation
    ref: #MANAGER-17908
    
    Signed-off-by: ahmed sefiani <ahmed.sefiani.ext@corp.ovh.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/apps/veeam-backup/src/pages/delete-veeam/DeleteVeeam.page.tsx
+++ packages/manager/apps/veeam-backup/src/pages/delete-veeam/DeleteVeeam.page.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import {
@@ -7,12 +7,16 @@ import {
   TrackingClickParams,
   PageType,
   useOvhTracking,
+  ShellContext,
 } from '@ovh-ux/manager-react-shell-client';
-import { DeleteServiceModal } from '@ovh-ux/manager-react-components';
+import { DeleteModal } from '@ovh-ux/manager-react-components';
 import {
   getVeeamBackupDisplayName,
   useVeeamBackup,
 } from '@ovh-ux/manager-module-vcd-api';
+import { useDeleteService } from '@ovh-ux/manager-module-common-api';
+import { US_SUBSIDIARY } from '@/constants';
+
 import { MessagesContext } from '@/components/Messages/Messages.context';
 import { PageName } from '@/tracking.constant';
 
@@ -25,9 +29,37 @@ export default function DeleteVeeamBackupModal() {
   const { id } = useParams();
   const { t } = useTranslation('delete-veeam');
   const { data, isLoading } = useVeeamBackup(id);
+  const { ovhSubsidiary } = useContext(ShellContext).environment.getUser();
   const { trackClick, trackPage } = useOvhTracking();
   const { addSuccessMessage } = React.useContext(MessagesContext);
   const navigate = useNavigate();
+
+  const onSuccess = () => {
+    trackPage({
+      pageType: PageType.bannerSuccess,
+      pageName: PageName.successDeleteVeeamBackup,
+    });
+    addSuccessMessage(
+      ovhSubsidiary === US_SUBSIDIARY
+        ? t('terminate_veeam_backup_success_us')
+        : t('terminate_veeam_backup_success', {
+            name: getVeeamBackupDisplayName(data.data),
+          }),
+      { veeamBackupId: id },
+    );
+    navigate('..');
+  };
+  const onError = () => {
+    trackPage({
+      pageType: PageType.bannerError,
+      pageName: PageName.errorDeleteVeeamBackup,
+    });
+  };
+
+  const { terminateService, isPending, error, isError } = useDeleteService({
+    onSuccess,
+    onError,
+  });
 
   const onClose = () => {
     trackClick({
@@ -38,40 +70,22 @@ export default function DeleteVeeamBackupModal() {
     navigate('..');
   };
 
+  const onConfirmDelete = () => {
+    trackClick({
+      ...sharedTrackingParams,
+      actionType: 'action',
+      actions: ['delete_veeam-backup', 'confirm'],
+    });
+    terminateService({ resourceName: id });
+  };
+
   return (
-    <DeleteServiceModal
+    <DeleteModal
       isOpen
       closeModal={onClose}
-      deleteInputLabel={t('delete_input_label')}
-      headline={t('headline')}
-      resourceName={id}
-      isLoading={isLoading}
-      onSuccess={() => {
-        trackPage({
-          pageType: PageType.bannerSuccess,
-          pageName: PageName.successDeleteVeeamBackup,
-        });
-        addSuccessMessage(
-          t('terminate_veeam_backup_success', {
-            name: getVeeamBackupDisplayName(data.data),
-          }),
-          { veeamBackupId: id },
-        );
-        navigate('..');
-      }}
-      onError={() => {
-        trackPage({
-          pageType: PageType.bannerError,
-          pageName: PageName.errorDeleteVeeamBackup,
-        });
-      }}
-      onConfirmDelete={() => {
-        trackClick({
-          ...sharedTrackingParams,
-          actionType: 'action',
-          actions: ['delete_veeam-backup', 'confirm'],
-        });
-      }}
+      isLoading={isPending || isLoading}
+      onConfirmDelete={onConfirmDelete}
+      error={isError ? error?.message : null}
     />
   );
 }
