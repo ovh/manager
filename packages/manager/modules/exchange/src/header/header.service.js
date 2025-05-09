a66c40c22e0b1a9258c7bf1f092b4488a56ce7ba
Tristan WAGNER <t.wagner66250@gmail.com>
2025-05-09 15:58:45 +0200

    fix(web.exchange): order office 365 cta
    ref: #MANAGER-13461
    
    Signed-off-by: Tristan WAGNER <t.wagner66250@gmail.com>

--- packages/manager/modules/exchange/src/header/header.service.js
+++ packages/manager/modules/exchange/src/header/header.service.js
@@ -1,7 +1,25 @@
 export default class exchangeHeaderService {
   /* @ngInject */
-  constructor(OvhHttp) {
+  constructor($q, OvhHttp, coreConfig) {
+    this.$q = $q;
     this.OvhHttp = OvhHttp;
+    this.coreConfig = coreConfig;
+  }
+
+  getOfficeTenantServiceName(serviceName, region = 'EU') {
+    if (region !== 'EU') {
+      return this.$q.resolve(null);
+    }
+
+    return this.OvhHttp.get(`/msServices/${serviceName}`, {
+      rootPath: 'apiv6',
+    }).then((service) => service.officeTenantServiceName);
+  }
+
+  getUserSubsidiary() {
+    return this.$q
+      .when(this.coreConfig.getUser())
+      .then((data) => data.ovhSubsidiary);
   }
 
   updateServiceDisplayName(organizationName, exchangeService, displayName) {
