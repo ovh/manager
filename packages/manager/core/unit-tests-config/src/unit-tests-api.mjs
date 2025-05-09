e18838cbdb345872a2c35ac5575e6ee24becd1de
helabenkhalfallah <hela.ben-khalfallah@corp.ovh.com>
2025-05-09 16:28:13 +0200

    refactor(*): shared Vitest expose API utilities
    ref: MANAGER-18041
    
    Signed-off-by: helabenkhalfallah <hela.ben-khalfallah@corp.ovh.com>

---
+++ packages/manager/core/unit-tests-config/src/unit-tests-api.mjs
@@ -0,0 +1,24 @@
+import {
+  describe,
+  it,
+  expect,
+  beforeEach,
+  afterEach,
+  afterAll,
+  beforeAll,
+  vi,
+} from 'vitest';
+
+export {
+  // vi is intentionally exposed to ensure devs don’t feel boxed in
+  // keep this for flexibility and dev experience, so it'll be easy to find documentation and community support
+  vi,
+  // Common test functions
+  describe,
+  it,
+  expect,
+  beforeEach,
+  afterEach,
+  afterAll,
+  beforeAll,
+};
