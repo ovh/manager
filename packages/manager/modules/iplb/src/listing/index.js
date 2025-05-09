8f7a4679189f6bde2878fff7dcf3bf06788d2da4
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2025-05-09 09:39:02 +0530

    feat(dedicated.iplb): add generic modal to terminate iplb in listing
    ref: #MANAGER-17805
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

--- packages/manager/modules/iplb/src/listing/index.js
+++ packages/manager/modules/iplb/src/listing/index.js
@@ -3,7 +3,6 @@ import '@uirouter/angularjs';
 import '@ovh-ux/ui-kit';
 import '@ovh-ux/manager-core';
 
-import IpLoadBalancerTerminateCtrl from '../modal/terminate/terminate.controller';
 import iplbListing from './listing.component';
 import route from './listing.route';
 
@@ -11,7 +10,6 @@ const moduleName = 'iplbListing';
 
 angular
   .module(moduleName, ['oui', 'ui.router', 'ovhManagerCore'])
-  .controller('IpLoadBalancerTerminateCtrl', IpLoadBalancerTerminateCtrl)
   .component('iplbListing', iplbListing)
   .config(route)
   .run(/* @ngTranslationsInject:json ./translations */);
