99da7a9261720c3b745da0d4f37cfea295737e14
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2025-05-12 16:44:12 +0200

    fix(dedicated.vps): update the guide reset password url
    ref: #MANAGER-13736
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

---
+++ packages/manager/modules/vps/src/dashboard/modal/password/vps-password.constants.js
@@ -0,0 +1,30 @@
+const PREFIX_HELP_CENTER_URL = 'https://help.ovhcloud.com/csm/';
+
+export const VPS_PASSWORD_GUIDE_URL = {
+  FR: `${PREFIX_HELP_CENTER_URL}fr-dedicated-servers-change-password?id=kb_article_view&sysparm_article=KB0061803`,
+  ES: `${PREFIX_HELP_CENTER_URL}es-es-dedicated-servers-change-password?id=kb_article_view&sysparm_article=KB0061811`,
+  DE: `${PREFIX_HELP_CENTER_URL}de-dedicated-servers-change-password?id=kb_article_view&sysparm_article=KB0061813`,
+  IE: `${PREFIX_HELP_CENTER_URL}en-ie-dedicated-servers-change-password?id=kb_article_view&sysparm_article=KB0061800`,
+  IT: `${PREFIX_HELP_CENTER_URL}it-dedicated-servers-change-password?id=kb_article_view&sysparm_article=KB0061814`,
+  PL: `${PREFIX_HELP_CENTER_URL}pl-dedicated-servers-change-password?id=kb_article_view&sysparm_article=KB0061804`,
+  PT: `${PREFIX_HELP_CENTER_URL}pt-dedicated-servers-change-password?id=kb_article_view&sysparm_article=KB0061810`,
+  GB: `${PREFIX_HELP_CENTER_URL}en-gb-dedicated-servers-change-password?id=kb_article_view&sysparm_article=KB0061806`,
+  CA: `${PREFIX_HELP_CENTER_URL}en-ca-dedicated-servers-change-password?id=kb_article_view&sysparm_article=KB0061801`,
+  QC: `${PREFIX_HELP_CENTER_URL}fr-ca-dedicated-servers-change-password?id=kb_article_view&sysparm_article=KB0061809`,
+  MA: `${PREFIX_HELP_CENTER_URL}fr-dedicated-servers-change-password?id=kb_article_view&sysparm_article=KB0061803`,
+  SN: `${PREFIX_HELP_CENTER_URL}fr-dedicated-servers-change-password?id=kb_article_view&sysparm_article=KB0061803`,
+  TN: `${PREFIX_HELP_CENTER_URL}fr-dedicated-servers-change-password?id=kb_article_view&sysparm_article=KB0061803`,
+  AU: `${PREFIX_HELP_CENTER_URL}en-au-dedicated-servers-change-password?id=kb_article_view&sysparm_article=KB0061812`,
+  SG: `${PREFIX_HELP_CENTER_URL}en-sg-dedicated-servers-change-password?id=kb_article_view&sysparm_article=KB0061805`,
+  ASIA: `${PREFIX_HELP_CENTER_URL}asia-dedicated-servers-change-password?id=kb_article_view&sysparm_article=KB0061807`,
+  IN: `${PREFIX_HELP_CENTER_URL}asia-dedicated-servers-change-password?id=kb_article_view&sysparm_article=KB0061807`,
+  WS: `${PREFIX_HELP_CENTER_URL}es-dedicated-servers-change-password?id=kb_article_view&sysparm_article=KB0061808`,
+  WE: `${PREFIX_HELP_CENTER_URL}en-dedicated-servers-change-password?id=kb_article_view&sysparm_article=KB0061802`,
+  US:
+    'https://support.us.ovhcloud.com/hc/en-us/articles/4410145719699-How-to-Reset-a-Windows-Password-on-VPS',
+  DEFAULT: `${PREFIX_HELP_CENTER_URL}en-ie-dedicated-servers-change-password?id=kb_article_view&sysparm_article=KB0061800`,
+};
+
+export default {
+  VPS_PASSWORD_GUIDE_URL,
+};
