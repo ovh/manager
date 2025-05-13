724f13154df7f389dde2c406a9c3aadc712dd4fb
David Arsène <david.arsene.ext@ovhcloud.com>
2025-05-13 17:40:04 +0200

    fix(dedicated.pcc): fix upgrade resource for datastore
    fixes: #MANAGER-18332 #INC0129332
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/dedicatedCloud/datacenter/datastore/resource-upgrade/upgrade/dedicatedCloud-datacenter-datastore-resource-upgrade.routes.js
+++ packages/manager/apps/dedicated/client/app/dedicatedCloud/datacenter/datastore/resource-upgrade/upgrade/dedicatedCloud-datacenter-datastore-resource-upgrade.routes.js
@@ -2,7 +2,7 @@ export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state(
     'app.dedicatedCloud.details.datacenter.details.datastores.resourceUpgrade',
     {
-      url: '/upgradeResource?datastoreId',
+      url: '/upgradeResource?id&type',
       params: {
         id: null,
         type: null,
@@ -15,7 +15,7 @@ export default /* @ngInject */ ($stateProvider) => {
       layout: 'modal',
       resolve: {
         goBack: /* @ngInject */ (goBackToDatastore) => goBackToDatastore,
-        id: /* @ngInject */ ($transition$) => $transition$.params().datastoreId,
+        id: /* @ngInject */ ($transition$) => $transition$.params().id,
         type: /* @ngInject */ ($transition$) => $transition$.params().type,
         breadcrumb: () => null,
       },
