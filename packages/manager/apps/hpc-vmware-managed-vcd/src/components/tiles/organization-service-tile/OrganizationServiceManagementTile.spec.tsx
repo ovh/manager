9c1e2676005b0eeb119bde2a5ef112201e794a67
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-05-13 12:18:41 +0200

    feat(vcd): add reset password modal feature
    ref: #MANAGER-15191
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/apps/hpc-vmware-managed-vcd/src/components/tiles/organization-service-tile/OrganizationServiceManagementTile.spec.tsx
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/components/tiles/organization-service-tile/OrganizationServiceManagementTile.spec.tsx
@@ -9,8 +9,11 @@ import {
 import { assertTextVisibility } from '@ovh-ux/manager-core-test-utils';
 import OrganizationServiceManagementTile from './OrganizationServiceManagementTile.component';
 import { labels } from '../../../test-utils';
+import { subRoutes, urls } from '../../../routes/routes.constant';
 
-vi.mock('react-router-dom', () => ({
+vi.mock('react-router-dom', async (importOriginal) => ({
+  ...(await importOriginal()),
+  useHref: () => urls.resetPassword.replace(subRoutes.dashboard, 'id'),
   useNavigate: () => ({ navigate: vi.fn() }),
   useParams: () => ({ id: 'id' }),
 }));
--- packages/manager/apps/hpc-vmware-managed-vcd/src/components/tiles/organization-service-tile/password-tile-item/ServicePasswordTileItem.tsx
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/components/tiles/organization-service-tile/password-tile-item/ServicePasswordTileItem.tsx
@@ -1,31 +1,21 @@
+import { useHref, useParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import { OdsBadge, OdsIcon, OdsTooltip } from '@ovhcloud/ods-components/react';
 import { Links } from '@ovh-ux/manager-react-components';
+import { subRoutes, urls } from '@/routes/routes.constant';
 
 export default function ServicePasswordTileItem() {
+  const { id } = useParams();
   const { t } = useTranslation('dashboard');
+  const passwordHref = useHref(
+    urls.resetPassword.replace(subRoutes.dashboard, id),
+  );
 
   return (
-    <div className="flex flex-col">
-      <div className="flex items-center gap-x-3">
-        <Links
-          isDisabled
-          href="/"
-          label={t('managed_vcd_dashboard_password_renew')}
-          className="[&::part(label)]:whitespace-break-spaces"
-        />
-        <OdsIcon
-          id="password-tooltip-trigger"
-          className="color-disabled cursor-pointer"
-          name="circle-question"
-        />
-        <OdsTooltip triggerId="password-tooltip-trigger">
-          {t('managed_vcd_dashboard_password_tooltip')}
-        </OdsTooltip>
-      </div>
-      <OdsBadge
-        label={t('managed_vcd_dashboard_coming_soon')}
-        className="mt-3"
+    <div className="flex items-center gap-x-3">
+      <Links
+        href={passwordHref}
+        label={t('managed_vcd_dashboard_password_renew')}
+        className="[&::part(label)]:whitespace-break-spaces"
       />
     </div>
   );
