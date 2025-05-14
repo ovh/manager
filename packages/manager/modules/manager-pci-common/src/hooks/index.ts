10f2a4b982b1737e8c35dc7d45c2738ac957503e
Simon Chaumet <simon.chaumet.ext@ovhcloud.com>
2025-05-14 16:18:27 +0200

    feat(pci-common): add deployment tiles (#16256)
    ref: #TAPC-2442
    
    Signed-off-by: Simon Chaumet <simon.chaumet.ext@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/manager-pci-common/src/hooks/index.ts
+++ packages/manager/modules/manager-pci-common/src/hooks/index.ts
@@ -2,3 +2,4 @@ export { useBytes } from './bytes/useBytes';
 export { usePciUrl } from './url/usePciUrl';
 export { usePricing } from './usePricing';
 export { usePCICommonContextFactory } from './usePCICommonContextFactory/usePCICommonContextFactory';
+export * from './useApplicationName/useApplicationName';
