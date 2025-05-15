b85d61522091c4d92d590605340cd54a296af14e
darsene <72250222+darsene@users.noreply.github.com>
2025-05-15 17:06:44 +0200

    feat(dedicated.pcc): remove 2API to fetch hosts (#16845)
    ref: #MANAGER-17155
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/apps/dedicated/client/app/components/dedicated-cloud/datacenter/host/dedicatedCloud-datacenter-host.controller.js
+++ packages/manager/apps/dedicated/client/app/components/dedicated-cloud/datacenter/host/dedicatedCloud-datacenter-host.controller.js
@@ -1,7 +1,9 @@
 import get from 'lodash/get';
+import set from 'lodash/set';
 
 import {
-  RESOURCE_BILLING_2API_TYPES,
+  RESOURCE_BILLING_TYPES,
+  RESOURCE_STATES,
   RESOURCE_UPGRADE_TYPES,
 } from '../../resource/upgrade/upgrade.constants';
 
@@ -20,46 +22,39 @@ export default class {
     this.dedicatedCloudDataCenterHostService = dedicatedCloudDataCenterHostService;
     this.ovhManagerPccDatacenterService = ovhManagerPccDatacenterService;
     this.DEDICATED_CLOUD_DATACENTER = DEDICATED_CLOUD_DATACENTER;
+    this.RESOURCE_BILLING_TYPES = RESOURCE_BILLING_TYPES;
+    this.RESOURCE_UPGRADE_TYPES = RESOURCE_UPGRADE_TYPES;
+    this.RESOURCE_STATES = RESOURCE_STATES;
   }
 
   $onInit() {
-    this.RESOURCE_BILLING_TYPES = RESOURCE_BILLING_2API_TYPES;
-    this.RESOURCE_UPGRADE_TYPES = RESOURCE_UPGRADE_TYPES;
-
     this.datacenter.model.commercialRangeName = this.commercialRangeName;
-  }
-
-  fetchLegacyHostConsumption(hosts) {
-    return this.$q.all(
-      hosts.map((host) =>
-        host.billingType === this.RESOURCE_BILLING_TYPES.hourly
-          ? this.dedicatedCloudDataCenterHostService
-              .getHostHourlyConsumption(
-                this.productId,
-                this.datacenterId,
-                host.hostId,
-              )
-              .then((consumption) => ({ ...host, ...consumption }))
-              .catch(() => host)
-          : host,
-      ),
+    this.$serviceConsumption = this.ovhManagerPccDatacenterService.fetchConsumptionForService(
+      this.serviceId,
     );
   }
 
-  fetchConsumptionForHosts(hosts) {
-    return (serviceConsumption) =>
-      this.$q.all(
-        serviceConsumption
-          ? hosts.map(this.fetchConsumptionForHost(serviceConsumption))
-          : hosts,
-      );
+  fetchLegacyHostConsumption(host) {
+    return host.billingType === this.RESOURCE_BILLING_TYPES.hourly
+      ? this.dedicatedCloudDataCenterHostService
+          .getHostHourlyConsumption(
+            this.productId,
+            this.datacenterId,
+            host.hostId,
+          )
+          .then((consumption) => {
+            set(host, 'consumption', consumption);
+          })
+          .catch(() => null)
+      : host;
   }
 
-  fetchConsumptionForHost(serviceConsumption) {
-    return (host) => {
+  fetchConsumptionForHost(host) {
+    return (serviceConsumption) => {
       if (
+        serviceConsumption &&
         host.billingType === this.RESOURCE_BILLING_TYPES.hourly &&
-        host.status === 'DELIVERED'
+        host.state === RESOURCE_STATES.delivered
       ) {
         const hostConsumption = this.ovhManagerPccDatacenterService.constructor.extractElementConsumption(
           serviceConsumption,
@@ -69,56 +64,51 @@ export default class {
           },
         );
 
-        return {
-          ...host,
-          consumption: {
-            value: get(hostConsumption, 'quantity', 0),
-          },
-          lastUpdate: serviceConsumption.lastUpdate,
-        };
+        set(host, 'consumption', {
+          value: get(hostConsumption, 'quantity', 0),
+        });
+        set(host, 'lastUpdate', serviceConsumption.lastUpdate);
       }
 
       return host;
     };
   }
 
-  chooseConsumptionFetchingMethod(hosts) {
+  chooseConsumptionFetchingMethod(host) {
     return !this.usesLegacyOrder
-      ? this.ovhManagerPccDatacenterService
-          .fetchConsumptionForService(this.serviceId)
-          .then(this.fetchConsumptionForHosts(hosts))
-      : this.fetchLegacyHostConsumption(hosts);
+      ? this.$serviceConsumption.then(this.fetchConsumptionForHost(host))
+      : this.fetchLegacyHostConsumption(host);
   }
 
-  fetchLocations(hosts) {
-    return this.$q.all(
-      hosts.map((host) => {
-        return this.dedicatedCloudDataCenterHostService
-          .getHostLocation(this.productId, this.datacenterId, host.hostId)
-          .then((location) => ({ ...host, location }))
-          .catch(() => host);
-      }),
-    );
+  fetchLocation(host) {
+    return this.dedicatedCloudDataCenterHostService
+      .getHostLocation(this.productId, this.datacenterId, host.hostId)
+      .then((location) => {
+        set(host, 'location', location);
+      })
+      .catch(() => null);
   }
 
   loadHosts({ offset, pageSize }) {
-    return this.DedicatedCloud.getPaginatedHosts(
-      this.productId,
-      this.datacenterId,
-      pageSize,
-      offset - 1,
-    ).then((result) =>
-      this.chooseConsumptionFetchingMethod(result.list.results).then(
-        (hostsWithConsumption) =>
-          this.fetchLocations(hostsWithConsumption).then(
-            (hostsWithLocation) => ({
-              data: hostsWithLocation,
-              meta: {
-                totalCount: result.count,
-              },
-            }),
-          ),
-      ),
-    );
+    return this.dedicatedCloudDataCenterHostService
+      .getPaginatedHosts(this.productId, this.datacenterId, {
+        pageSize,
+        offset: offset - 1,
+        sort: 'hostId',
+      })
+      .then(({ data, meta }) => {
+        data.forEach((host) => {
+          set(host, 'asyncLoading', true);
+          this.$q
+            .all([
+              this.chooseConsumptionFetchingMethod(host),
+              this.fetchLocation(host),
+            ])
+            .finally(() => {
+              set(host, 'asyncLoading', false);
+            });
+        });
+        return { data, meta };
+      });
   }
 }
