fb13dd726ea54a4d75758acfbbdae363ae0003d5
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2025-05-15 14:15:47 +0200

    refactor(sign-up): centralize error tracking management
    ref: #MANAGER-17390
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/apps/sign-up/src/identity/routing.js
+++ packages/manager/apps/sign-up/src/identity/routing.js
@@ -6,16 +6,6 @@ export const state = {
       component: 'ovhSignUpIdentity',
     },
   },
-  resolve: {
-    onFieldError: /* @ngInject */ (trackError) => (field) =>
-      trackError('step1', field),
-    trackField: /* @ngInject */ (atInternet) => (field, value) => {
-      atInternet.trackClick({
-        type: 'action',
-        name: `accountcreation::${field}::${value}`,
-      });
-    },
-  },
   atInternet: {
     ignore: true,
   },
