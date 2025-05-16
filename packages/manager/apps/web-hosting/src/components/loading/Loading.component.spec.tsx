4f494d46508ded693f303be4538332f2cceb6630
Tristan WAGNER <t.wagner66250@gmail.com>
2025-05-16 08:49:23 +0200

    feat(web-hosting): website view (#15546)
    ref: #MANAGER-14386
    
    Signed-off-by: Tristan WAGNER <t.wagner66250@gmail.com>
    Co-authored-by: stif59100 <steeve.vanderstocken@ovhcloud.com>
    Co-authored-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

---
+++ packages/manager/apps/web-hosting/src/components/loading/Loading.component.spec.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@/utils/test.provider';
+import Loading from './Loading.component';
+
+describe('Loading component', () => {
+  it('should render loading component', () => {
+    render(<Loading />);
+    const container = screen.getByTestId('spinner');
+    expect(container).toBeInTheDocument();
+  });
+});
