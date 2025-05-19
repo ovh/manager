ba80e1e5c6378b59b13c698e0bd58ed262d5297c
David Arsène <david.arsene.ext@ovhcloud.com>
2025-05-19 16:27:40 +0200

    feat(iam): enable account delegation access
    resolves: #MANAGER-17557
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/iam/src/components/identities/accountIdentities/accountIdentities.controller.js
+++ packages/manager/modules/iam/src/components/identities/accountIdentities/accountIdentities.controller.js
@@ -2,4 +2,16 @@ export default class accountIdentitiesController {
   onRemoveAccountIdentity(urn) {
     this.onRemoveIdentity({ urn });
   }
+
+  onAddAccountIdentity(urn) {
+    this.onAddIdentities({ urns: [urn] });
+  }
+
+  openDelegationAccountModal() {
+    this.delegationAccountModalOpened = true;
+  }
+
+  closeDelegationAccountModal() {
+    this.delegationAccountModalOpened = false;
+  }
 }
