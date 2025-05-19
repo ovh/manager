8a8b3d38e63f081dc12c5cf000a2b90c81f1bc7d
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2025-05-19 17:37:25 +0200

    feat(pci-instances): get instance from dashboard page
    ref: #TAPC-4052
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

--- packages/manager/apps/pci-instances/src/pages/instances/instance/dashboard/Dashboard.page.tsx
+++ packages/manager/apps/pci-instances/src/pages/instances/instance/dashboard/Dashboard.page.tsx
@@ -1,14 +1,28 @@
 import { FC } from 'react';
+import { useParams } from 'react-router-dom';
+import { useInstance } from '@/data/hooks/instance/useInstance';
 import Information from './component/Information.component';
 import Property from './component/Property.component';
 import Network from './component/Network.component';
+import { Spinner } from '@/components/spinner/Spinner.component';
 
-const Dashboard: FC = () => (
-  <div className="grid grid-cols-1 md:items-start md:grid-cols-3 gap-4 md:gap-6">
-    <Information />
-    <Property />
-    <Network />
-  </div>
-);
+const Dashboard: FC = () => {
+  const { instanceId } = useParams() as { instanceId: string };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { data: instance, isLoading } = useInstance(instanceId, {});
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  return (
+    <div className="grid grid-cols-1 md:items-start md:grid-cols-3 gap-4 md:gap-6">
+      <Information />
+      <Property />
+      <Network />
+    </div>
+  );
+};
 
 export default Dashboard;
