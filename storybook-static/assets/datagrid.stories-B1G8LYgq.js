7d665cf1944dbec1e59f778729e56cad4a6f6692
ovh-cds <ovh-cds@users.noreply.github.com>
2025-05-20 08:40:31 +0000

    docs: update documentation [skip ci]

---
+++ storybook-static/assets/datagrid.stories-B1G8LYgq.js
@@ -0,0 +1,266 @@
+import{j as s}from"./jsx-runtime-BRNY0I4F.js";import{u as O,K as W}from"./index-jll95z9C.js";import{P as u,l as U,c as T,d as Z,u as q,e as H}from"./ManagerButton-BzkvgsX0.js";import{c as d,a as Q}from"./datagrid.mock-BzmFl3T0.js";import"./index-Bnop-kX6.js";import"./index-CwE47CCN.js";import"./index-CNSIveXf.js";import"./index-4pTrEEYx.js";import"./ods-toggle2-BJ1toRrr.js";import"./i18next-6HYnolD1.js";import"./QueryClientProvider-DAxw80nV.js";import"./useOvhTracking-Cqpl1sX8.js";import"./index-D-q5QNgV.js";import"./clipboard.component-CmhF-xNN.js";import"./useTranslation-DQ7TG6Ul.js";import"./context-BG98Yt4t.js";import"./Step.component-Bds2cN4H.js";import"./clsx-B-dksMZM.js";import"./v4-CQkTLCs1.js";import"./Tabs.component-C_dEyA9f.js";import"./TilesInput.component-C_qU074F.js";import"./card.component-BToc7KuX.js";import"./title.component-ucIeg-_K.js";import"./links.component-DnFRtS4w.js";import"./price.component-CleweYwa.js";import"./translation-DkLW9Uck.js";import"./guide.component-BUB2PtWq.js";import"./changelog.component-bWd8ittI.js";import"./error.component-Mbm_PMBt.js";import"./error-boundary.component-CFspuVW9.js";import"./delete-modal.component-4eOUWoLS.js";import"./click-utils-ByCElPrV.js";import"./update-name-modal.component-BuaziZw6.js";import"./notifications.component-BC7nyudf.js";import"./PciMaintenanceBanner.component-CFf6hjH_.js";import"./region.component-7C7sLsDi.js";import"./Order.component-DWsKGSo7.js";import"./badge.component-WpuLrKbO.js";import"./Modal.component-BFD3_SRS.js";const C=e=>Object.fromEntries([...e.entries()]),V=e=>{const n={...U};if(e.has("page")){let t=parseInt(e.get("page"),10)-1;(Number.isNaN(t)||t<0)&&(t=0),n.pageIndex=t}if(e.has("pageSize")){let t=parseInt(e.get("pageSize"),10);u.includes(t)||([t]=u),n.pageSize=t}return n},Y=(e,n)=>{const t={id:null,desc:!1};if(e.has("sort"))t.id=e.get("sort"),e.has("sortOrder")&&(t.desc=e.get("sortOrder")==="desc");else if(n)return n;return t},nn=e=>{const[n,t]=O();return{pagination:V(n),sorting:Y(n,e),setPagination:({pageIndex:a,pageSize:r})=>{a>0?n.set("page",`${a+1}`):n.delete("page"),u.includes(r)&&r!==u[0]?n.set("pageSize",`${r}`):n.delete("pageSize"),t({...C(n)})},setSorting:({id:a,desc:r})=>{a?(n.set("sort",a),r?n.set("sortOrder","desc"):n.delete("sortOrder")):(n.delete("sort"),n.delete("sortOrder")),t({...C(n)})}}};function en(e,n){if(!n)return e;const t=n.desc?-1:1;return n.id==="label"?e.sort((a,r)=>t*a.label.localeCompare(r.label)):n.id==="price"?e.sort((a,r)=>t*(a.price-r.price)):e}const p=({items:e,isPaginated:n,isSortable:t,isLoading:a=!1,columns:r=d,getRowCanExpand:_,renderSubComponent:M})=>{const[S]=O(),{pagination:c,setPagination:J,sorting:h,setSorting:N}=nn({id:"validityTo",desc:!1}),P=n?c.pageIndex*c.pageSize:0,X=n?P+c.pageSize:e.length,k=n&&{pagination:c,onPaginationChange:J},G=t&&{sorting:h,onSortChange:N},{filters:b,addFilter:B,removeFilter:K}=q();return s.jsxs(s.Fragment,{children:[`${S}`&&s.jsxs(s.Fragment,{children:[s.jsxs("pre",{children:["Search params: ?",`${S}`]}),s.jsx("hr",{})]}),s.jsx(T,{columns:r,items:H(en(e,h).slice(P,X),b),totalItems:e.length,isLoading:a,...k,...G,filters:{filters:b,add:B,remove:K},getRowCanExpand:_,renderSubComponent:M})]})},o=p.bind({});o.args={columns:d,items:[...Array(50).keys()].map((e,n)=>({label:`Item #${n}`,price:Math.floor(1+Math.random()*100)})),isPaginated:!0,isSortable:!0};const i=p.bind({});i.args={columns:d,items:[],isLoading:!0};const g=p.bind({});g.args={columns:d,items:[...Array(8).keys()].map((e,n)=>({label:`Service #${n}`,price:Math.floor(1+Math.random()*100)})),isSortable:!0};const m=p.bind({});m.args={items:[...Array(50).keys()].map((e,n)=>({label:`Item #${n}`,price:Math.floor(1+Math.random()*100)})),isPaginated:!0,isSortable:!0,columns:Q};const l=p.bind({});l.args={columns:d,items:[...Array(10).keys()].map((e,n)=>({label:`Item #${n}`,price:Math.floor(1+Math.random()*100)})),getRowCanExpand:()=>!0,renderSubComponent:e=>s.jsx(Z,{children:JSON.stringify(e.original)})};const kn={title:"Components/Datagrid Paginated",component:T,decorators:[W],parameters:{status:{type:"deprecated"},docs:{description:{component:"The `Datagrid` component in pagination mode is now `deprecated`. Please switch to the cursor navigation mode"}}}};var f,F,x;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`({
+  items,
+  isPaginated,
+  isSortable,
+  isLoading = false,
+  columns = clm,
+  getRowCanExpand,
+  renderSubComponent
+}: {
+  items: Item[];
+  isLoading: boolean;
+  isPaginated: boolean;
+  isSortable: boolean;
+  columns?: any;
+  renderSubComponent?: (props: Row<any>) => JSX.Element;
+  getRowCanExpand?: (row: Row<any>) => boolean;
+}) => {
+  const [searchParams] = useSearchParams();
+  const {
+    pagination,
+    setPagination,
+    sorting,
+    setSorting
+  } = useDatagridSearchParams({
+    id: 'validityTo',
+    desc: false
+  });
+  const start = isPaginated ? pagination.pageIndex * pagination.pageSize : 0;
+  const end = isPaginated ? start + pagination.pageSize : items.length;
+  const paginationAttrs = isPaginated && {
+    pagination,
+    onPaginationChange: setPagination
+  };
+  const sortingAttrs = isSortable && {
+    sorting,
+    onSortChange: setSorting
+  };
+  const {
+    filters,
+    addFilter,
+    removeFilter
+  } = useColumnFilters();
+  return <>
+      {\`\${searchParams}\` && <>
+          <pre>Search params: ?{\`\${searchParams}\`}</pre>
+          <hr />
+        </>}
+      <Datagrid columns={columns} items={applyFilters(sortItems(items, sorting).slice(start, end), filters)} totalItems={items.length} isLoading={isLoading} {...paginationAttrs} {...sortingAttrs} filters={{
+      filters,
+      add: addFilter,
+      remove: removeFilter
+    }} getRowCanExpand={getRowCanExpand} renderSubComponent={renderSubComponent} />
+    </>;
+}`,...(x=(F=o.parameters)==null?void 0:F.docs)==null?void 0:x.source}}};var w,y,E;i.parameters={...i.parameters,docs:{...(w=i.parameters)==null?void 0:w.docs,source:{originalSource:`({
+  items,
+  isPaginated,
+  isSortable,
+  isLoading = false,
+  columns = clm,
+  getRowCanExpand,
+  renderSubComponent
+}: {
+  items: Item[];
+  isLoading: boolean;
+  isPaginated: boolean;
+  isSortable: boolean;
+  columns?: any;
+  renderSubComponent?: (props: Row<any>) => JSX.Element;
+  getRowCanExpand?: (row: Row<any>) => boolean;
+}) => {
+  const [searchParams] = useSearchParams();
+  const {
+    pagination,
+    setPagination,
+    sorting,
+    setSorting
+  } = useDatagridSearchParams({
+    id: 'validityTo',
+    desc: false
+  });
+  const start = isPaginated ? pagination.pageIndex * pagination.pageSize : 0;
+  const end = isPaginated ? start + pagination.pageSize : items.length;
+  const paginationAttrs = isPaginated && {
+    pagination,
+    onPaginationChange: setPagination
+  };
+  const sortingAttrs = isSortable && {
+    sorting,
+    onSortChange: setSorting
+  };
+  const {
+    filters,
+    addFilter,
+    removeFilter
+  } = useColumnFilters();
+  return <>
+      {\`\${searchParams}\` && <>
+          <pre>Search params: ?{\`\${searchParams}\`}</pre>
+          <hr />
+        </>}
+      <Datagrid columns={columns} items={applyFilters(sortItems(items, sorting).slice(start, end), filters)} totalItems={items.length} isLoading={isLoading} {...paginationAttrs} {...sortingAttrs} filters={{
+      filters,
+      add: addFilter,
+      remove: removeFilter
+    }} getRowCanExpand={getRowCanExpand} renderSubComponent={renderSubComponent} />
+    </>;
+}`,...(E=(y=i.parameters)==null?void 0:y.docs)==null?void 0:E.source}}};var I,R,A;g.parameters={...g.parameters,docs:{...(I=g.parameters)==null?void 0:I.docs,source:{originalSource:`({
+  items,
+  isPaginated,
+  isSortable,
+  isLoading = false,
+  columns = clm,
+  getRowCanExpand,
+  renderSubComponent
+}: {
+  items: Item[];
+  isLoading: boolean;
+  isPaginated: boolean;
+  isSortable: boolean;
+  columns?: any;
+  renderSubComponent?: (props: Row<any>) => JSX.Element;
+  getRowCanExpand?: (row: Row<any>) => boolean;
+}) => {
+  const [searchParams] = useSearchParams();
+  const {
+    pagination,
+    setPagination,
+    sorting,
+    setSorting
+  } = useDatagridSearchParams({
+    id: 'validityTo',
+    desc: false
+  });
+  const start = isPaginated ? pagination.pageIndex * pagination.pageSize : 0;
+  const end = isPaginated ? start + pagination.pageSize : items.length;
+  const paginationAttrs = isPaginated && {
+    pagination,
+    onPaginationChange: setPagination
+  };
+  const sortingAttrs = isSortable && {
+    sorting,
+    onSortChange: setSorting
+  };
+  const {
+    filters,
+    addFilter,
+    removeFilter
+  } = useColumnFilters();
+  return <>
+      {\`\${searchParams}\` && <>
+          <pre>Search params: ?{\`\${searchParams}\`}</pre>
+          <hr />
+        </>}
+      <Datagrid columns={columns} items={applyFilters(sortItems(items, sorting).slice(start, end), filters)} totalItems={items.length} isLoading={isLoading} {...paginationAttrs} {...sortingAttrs} filters={{
+      filters,
+      add: addFilter,
+      remove: removeFilter
+    }} getRowCanExpand={getRowCanExpand} renderSubComponent={renderSubComponent} />
+    </>;
+}`,...(A=(R=g.parameters)==null?void 0:R.docs)==null?void 0:A.source}}};var v,L,$;m.parameters={...m.parameters,docs:{...(v=m.parameters)==null?void 0:v.docs,source:{originalSource:`({
+  items,
+  isPaginated,
+  isSortable,
+  isLoading = false,
+  columns = clm,
+  getRowCanExpand,
+  renderSubComponent
+}: {
+  items: Item[];
+  isLoading: boolean;
+  isPaginated: boolean;
+  isSortable: boolean;
+  columns?: any;
+  renderSubComponent?: (props: Row<any>) => JSX.Element;
+  getRowCanExpand?: (row: Row<any>) => boolean;
+}) => {
+  const [searchParams] = useSearchParams();
+  const {
+    pagination,
+    setPagination,
+    sorting,
+    setSorting
+  } = useDatagridSearchParams({
+    id: 'validityTo',
+    desc: false
+  });
+  const start = isPaginated ? pagination.pageIndex * pagination.pageSize : 0;
+  const end = isPaginated ? start + pagination.pageSize : items.length;
+  const paginationAttrs = isPaginated && {
+    pagination,
+    onPaginationChange: setPagination
+  };
+  const sortingAttrs = isSortable && {
+    sorting,
+    onSortChange: setSorting
+  };
+  const {
+    filters,
+    addFilter,
+    removeFilter
+  } = useColumnFilters();
+  return <>
+      {\`\${searchParams}\` && <>
+          <pre>Search params: ?{\`\${searchParams}\`}</pre>
+          <hr />
+        </>}
+      <Datagrid columns={columns} items={applyFilters(sortItems(items, sorting).slice(start, end), filters)} totalItems={items.length} isLoading={isLoading} {...paginationAttrs} {...sortingAttrs} filters={{
+      filters,
+      add: addFilter,
+      remove: removeFilter
+    }} getRowCanExpand={getRowCanExpand} renderSubComponent={renderSubComponent} />
+    </>;
+}`,...($=(L=m.parameters)==null?void 0:L.docs)==null?void 0:$.source}}};var D,z,j;l.parameters={...l.parameters,docs:{...(D=l.parameters)==null?void 0:D.docs,source:{originalSource:`({
+  items,
+  isPaginated,
+  isSortable,
+  isLoading = false,
+  columns = clm,
+  getRowCanExpand,
+  renderSubComponent
+}: {
+  items: Item[];
+  isLoading: boolean;
+  isPaginated: boolean;
+  isSortable: boolean;
+  columns?: any;
+  renderSubComponent?: (props: Row<any>) => JSX.Element;
+  getRowCanExpand?: (row: Row<any>) => boolean;
+}) => {
+  const [searchParams] = useSearchParams();
+  const {
+    pagination,
+    setPagination,
+    sorting,
+    setSorting
+  } = useDatagridSearchParams({
+    id: 'validityTo',
+    desc: false
+  });
+  const start = isPaginated ? pagination.pageIndex * pagination.pageSize : 0;
+  const end = isPaginated ? start + pagination.pageSize : items.length;
+  const paginationAttrs = isPaginated && {
+    pagination,
+    onPaginationChange: setPagination
+  };
+  const sortingAttrs = isSortable && {
+    sorting,
+    onSortChange: setSorting
+  };
+  const {
+    filters,
+    addFilter,
+    removeFilter
+  } = useColumnFilters();
+  return <>
+      {\`\${searchParams}\` && <>
+          <pre>Search params: ?{\`\${searchParams}\`}</pre>
+          <hr />
+        </>}
+      <Datagrid columns={columns} items={applyFilters(sortItems(items, sorting).slice(start, end), filters)} totalItems={items.length} isLoading={isLoading} {...paginationAttrs} {...sortingAttrs} filters={{
+      filters,
+      add: addFilter,
+      remove: removeFilter
+    }} getRowCanExpand={getRowCanExpand} renderSubComponent={renderSubComponent} />
+    </>;
+}`,...(j=(z=l.parameters)==null?void 0:z.docs)==null?void 0:j.source}}};const Gn=["Basic","Loading","Sortable","Filters","WithSubComponent"];export{o as Basic,m as Filters,i as Loading,g as Sortable,l as WithSubComponent,Gn as __namedExportsOrder,kn as default};
