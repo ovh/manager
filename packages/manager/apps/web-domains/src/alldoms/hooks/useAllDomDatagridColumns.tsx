bd8af4398b22e0007de97c3812949ab7c89d75e5
Louis BENSI <louis.bensi.ext@corp.ovh.com>
2025-05-20 17:52:07 +0200

    feat(web-domains): add columns filters
    ref: #MANAGER-18310
    
    Signed-off-by: Louis BENSI <louis.bensi.ext@corp.ovh.com>

--- packages/manager/apps/web-domains/src/alldoms/hooks/useAllDomDatagridColumns.tsx
+++ packages/manager/apps/web-domains/src/alldoms/hooks/useAllDomDatagridColumns.tsx
@@ -1,5 +1,5 @@
 import { DataGridTextCell } from '@ovh-ux/manager-react-components';
-import React, { useMemo } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { TServiceDetail } from '@/alldoms/types';
 import DatagridColumnDomainNumber from '@/alldoms/components/DatagridColumns/DatagridColumnDomainNumber';
@@ -11,93 +11,100 @@ import DatagridColumnRenewMode from '@/alldoms/components/DatagridColumns/Datagr
 
 export const useAllDomDatagridColumns = () => {
   const { t } = useTranslation('allDom');
-  return useMemo(
-    () => [
-      {
-        id: 'serviceName',
-        cell: (props: TServiceDetail) => (
-          <DatagridColumnServiceName allDomName={props.allDomProperty.name} />
-        ),
-        label: t('allDom_table_header_serviceName'),
-        isFilterable: true,
-      },
-      {
-        id: 'renewMode',
-        cell: (props: TServiceDetail) => (
-          <DatagridColumnRenewMode
-            renewMode={props.serviceInfo.billing.renew.current.mode}
-          />
-        ),
-        label: t('allDom_table_header_renewMode'),
-      },
-      {
-        id: 'type',
-        cell: (props: TServiceDetail) => (
-          <DataGridTextCell>
-            {t(`allDom_table_type_${props.allDomProperty.type}`)}
-          </DataGridTextCell>
-        ),
-        label: t('allDom_table_header_type'),
-      },
-      {
-        id: 'register_domain',
-        cell: (props: TServiceDetail) => (
-          <DataGridTextCell>{props.domainAttached.length}</DataGridTextCell>
-        ),
-        label: t('allDom_table_header_register_domain'),
-      },
-      {
-        id: 'authorized_domain',
-        cell: (props: TServiceDetail) => (
-          <DatagridColumnDomainNumber
-            allDomProperty={props.allDomProperty.type}
-          />
-        ),
-        label: t('allDom_table_header_authorized_domain'),
-      },
-      {
-        id: 'expiration_date',
-        cell: (props: TServiceDetail) => (
-          <DatagridColumnDate
-            expirationDate={props.serviceInfo.billing.expirationDate}
-          />
-        ),
-        label: t('allDom_table_header_expirationDate'),
-      },
-      {
-        id: 'nicAdmin',
-        cell: (props: TServiceDetail) => (
-          <DatagridColumnContact contact={props.nicAdmin} />
-        ),
-        label: t('allDom_table_header_nicAdmin'),
-      },
-      {
-        id: 'nicTech',
-        cell: (props: TServiceDetail) => (
-          <DatagridColumnContact contact={props.nicTechnical} />
-        ),
-        label: t('allDom_table_header_nicTech'),
-      },
-      {
-        id: 'nicBilling',
-        cell: (props: TServiceDetail) => (
-          <DatagridColumnContact contact={props.nicBilling} />
-        ),
-        label: t('allDom_table_header_nicBilling'),
-      },
-      {
-        id: 'actions',
-        cell: (props: TServiceDetail) => (
-          <DatagridColumnActionMenu
-            serviceId={`${props.serviceInfo.serviceId}`}
-            serviceName={props.allDomProperty.name}
-            serviceRenewMode={props.serviceInfo.billing.renew.current.mode}
-            isServiceNameUrl={true}
-          />
-        ),
-        label: '',
-      },
-    ],
-    [],
-  );
+  const columns = [
+    {
+      id: 'serviceName',
+      cell: (props: TServiceDetail) => (
+        <DatagridColumnServiceName allDomName={props.allDomProperty.name} />
+      ),
+      label: t('allDom_table_header_serviceName'),
+      isFilterable: true,
+      enableHiding: false,
+    },
+    {
+      id: 'renewMode',
+      cell: (props: TServiceDetail) => (
+        <DatagridColumnRenewMode
+          renewMode={props.serviceInfo.billing.renew.current.mode}
+        />
+      ),
+      label: t('allDom_table_header_renewMode'),
+      enableHiding: true,
+    },
+    {
+      id: 'type',
+      cell: (props: TServiceDetail) => (
+        <DataGridTextCell>
+          {t(`allDom_table_type_${props.allDomProperty.type}`)}
+        </DataGridTextCell>
+      ),
+      label: t('allDom_table_header_type'),
+      enableHiding: true,
+    },
+    {
+      id: 'register_domain',
+      cell: (props: TServiceDetail) => (
+        <DataGridTextCell>{props.domainAttached.length}</DataGridTextCell>
+      ),
+      label: t('allDom_table_header_register_domain'),
+      enableHiding: true,
+    },
+    {
+      id: 'authorized_domain',
+      cell: (props: TServiceDetail) => (
+        <DatagridColumnDomainNumber
+          allDomProperty={props.allDomProperty.type}
+        />
+      ),
+      label: t('allDom_table_header_authorized_domain'),
+      enableHiding: true,
+    },
+    {
+      id: 'expiration_date',
+      cell: (props: TServiceDetail) => (
+        <DatagridColumnDate
+          expirationDate={props.serviceInfo.billing.expirationDate}
+        />
+      ),
+      label: t('allDom_table_header_expirationDate'),
+      enableHiding: false,
+    },
+    {
+      id: 'nicAdmin',
+      cell: (props: TServiceDetail) => (
+        <DatagridColumnContact contact={props.nicAdmin} />
+      ),
+      label: t('allDom_table_header_nicAdmin'),
+    },
+    {
+      id: 'nicTech',
+      cell: (props: TServiceDetail) => (
+        <DatagridColumnContact contact={props.nicTechnical} />
+      ),
+      label: t('allDom_table_header_nicTech'),
+      enableHiding: true,
+    },
+    {
+      id: 'nicBilling',
+      cell: (props: TServiceDetail) => (
+        <DatagridColumnContact contact={props.nicBilling} />
+      ),
+      label: t('allDom_table_header_nicBilling'),
+      enableHiding: true,
+    },
+    {
+      id: 'actions',
+      cell: (props: TServiceDetail) => (
+        <DatagridColumnActionMenu
+          serviceId={`${props.serviceInfo.serviceId}`}
+          serviceName={props.allDomProperty.name}
+          serviceRenewMode={props.serviceInfo.billing.renew.current.mode}
+          isServiceNameUrl={true}
+        />
+      ),
+      label: '',
+      enableHiding: false,
+    },
+  ];
+  return columns;
 };
