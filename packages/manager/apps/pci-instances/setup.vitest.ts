56d8aa8a41b01c2a5fcc60ad8de059b0dd21d4a5
Frédéric Vilcot <fredericvilcot@gmail.com>
2025-05-20 10:54:10 +0200

    refactor(pci-instances): remove create page & associated code
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

--- packages/manager/apps/pci-instances/setup.vitest.ts
+++ packages/manager/apps/pci-instances/setup.vitest.ts
@@ -1,8 +1,6 @@
 import '@testing-library/jest-dom';
 import { vi } from 'vitest';
 
-vi.mock('zustand');
-
 vi.mock('react-i18next', () => ({
   useTranslation: () => ({
     t: (translationKey: string) => translationKey,
