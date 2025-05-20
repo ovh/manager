2ec2d0af8eaf26679b0afc454d4b056d3c8bb3a7
Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>
2025-05-20 09:45:37 +0200

    feat(container): improved pnr rollback project selector
    ref: #MANAGER-14863
    
    Signed-off-by: Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>

--- packages/manager/apps/container/src/container/nav-reshuffle/sidebar/ProjectSelector/ProjectSelector.tsx
+++ packages/manager/apps/container/src/container/nav-reshuffle/sidebar/ProjectSelector/ProjectSelector.tsx
@@ -1,28 +1,32 @@
-import {
-  OsdsSelect,
-  OsdsSelectOption,
-  OsdsIcon,
-} from '@ovhcloud/ods-components/react';
-import { ODS_ICON_NAME, ODS_ICON_SIZE } from '@ovhcloud/ods-components';
-import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
-import React, { useMemo } from 'react';
-import {
-  NEW_PROJECT_ITEM_ID,
-  SEE_ALL_PROJECTS_ITEM_ID,
-} from './ProjectSelector.constants';
+import React, { useEffect, useState } from 'react';
+import Select, { StylesConfig } from 'react-select';
+import { PciProject } from './PciProject';
 
-export type PciProject = {
-  access: string;
-  creationDate: Date;
-  description: string;
-  expiration: Date;
-  manualQuota: boolean;
-  orderId: unknown;
-  planCode: string;
-  projectName: string;
-  project_id: string;
-  status: string;
-  unleash: boolean;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const getProjectOption = (option: Record<string, any>): JSX.Element => {
+  return (
+    <div style={{ alignItems: 'center', display: 'flex' }}>
+      {option.new && (
+        <>
+          <span
+            className="oui-icon oui-icon-plus mr-2"
+            aria-hidden="true"
+          ></span>
+          <span>{option.label}</span>
+        </>
+      )}
+      {option.seeAll && (
+        <>
+          <span>{option.label}</span>
+          <span
+            className="oui-icon oui-icon-arrow-right ml-2"
+            aria-hidden="true"
+          ></span>
+        </>
+      )}
+      {option.id && <span>{option.label}</span>}
+    </div>
+  );
 };
 
 export type Props = {
@@ -35,77 +39,103 @@ export type Props = {
   seeAllButton: boolean;
   seeAllLabel: string;
 };
-
 const ProjectSelector: React.FC<ComponentProps<Props>> = ({
   isLoading,
   projects,
   selectedProject,
   onProjectChange,
   onSeeAllProjects,
+  onMenuOpen,
   seeAllButton,
   seeAllLabel,
 }: Props): JSX.Element => {
-  const options = useMemo<{ id: string; label: string }[]>(() => {
-    const result = [];
-
-    if (seeAllButton) {
-      result.push({ id: SEE_ALL_PROJECTS_ITEM_ID, label: seeAllLabel });
-    }
-
-    if (projects) {
-      result.push(
-        ...projects.map((project) => ({
-          id: project.project_id,
-          label: project.description || project.project_id,
-        })),
-      );
-    }
-
-    return result;
-  }, [seeAllButton, seeAllLabel, projects]);
-
-  const handleSelectChange = (event: any) => {
-    const projectIdValue = event.detail.value;
-    if (projectIdValue === SEE_ALL_PROJECTS_ITEM_ID) {
-      onSeeAllProjects();
-    } else {
-      onProjectChange(
-        projects.find(
-          ({ project_id: projectId }) => projectId === projectIdValue,
-        ),
-      );
-    }
+  const selectStyles: StylesConfig<unknown> = {
+    option: (provided, { isFocused }: { isFocused: boolean }) => ({
+      ...provided,
+      backgroundColor: isFocused
+        ? 'var(--ods-color-primary-075)'
+        : 'var(--ods-color-default-000)',
+      color: isFocused
+        ? 'var(--ods-color-primary-700)'
+        : 'var(--ods-color-primary-800)',
+      cursor: 'pointer',
+      ':active': {
+        ...provided[':active'],
+        backgroundColor: 'var(--ods-color-primary-100)',
+        color: 'var(--ods-color-primary-700',
+      },
+    }),
+    control: (provided, { isFocused }: { isFocused: boolean }) => ({
+      ...provided,
+      cursor: 'pointer',
+      backgroundColor: isFocused
+        ? 'var(--ods-color-primary-075)'
+        : 'var(--ods-color-default-000)',
+      color: isFocused
+        ? 'var(--ods-color-primary-700)'
+        : 'var(--ods-color-primary-800)',
+    }),
   };
+  const [options, setOptions] = useState([]);
+  const [value, setValue] = useState(null);
+  const [seeAllProjectsOption, setSeeAllProjectsOption] = useState(null);
+
+  useEffect(() => {
+    setSeeAllProjectsOption(
+      seeAllButton
+        ? {
+            seeAll: true,
+            label: seeAllLabel,
+          }
+        : null,
+    );
+  }, [seeAllButton, seeAllLabel]);
+
+  useEffect(() => {
+    setOptions([
+      ...(seeAllProjectsOption ? [seeAllProjectsOption] : []),
+      ...(projects
+        ? projects.map(({ project_id: projectId, description }) => ({
+            id: projectId,
+            label: description || projectId,
+          }))
+        : []),
+    ]);
+  }, [projects]);
+
+  useEffect(() => {
+    if (selectedProject) {
+      setValue({
+        id: selectedProject.project_id,
+        label: selectedProject.description || selectedProject.project_id,
+      });
+    }
+  }, [selectedProject, options]);
 
   return (
     <>
-      <OsdsSelect
-        value={selectedProject?.project_id}
-        onOdsValueChange={handleSelectChange}
-      >
-        {isLoading && <span slot="placeholder">....</span>}
-        {options.map((option) => (
-          <OsdsSelectOption key={option.id} value={option.id}>
-            <div className="flex items-center gap-2 flex-wrap">
-              {option.id === NEW_PROJECT_ITEM_ID && (
-                <OsdsIcon
-                  name={ODS_ICON_NAME.ADD}
-                  size={ODS_ICON_SIZE.xxs}
-                  color={ODS_THEME_COLOR_INTENT.primary}
-                />
-              )}
-              <span>{option.label}</span>
-              {option.id === SEE_ALL_PROJECTS_ITEM_ID && (
-                <OsdsIcon
-                  name={ODS_ICON_NAME.ARROW_RIGHT}
-                  size={ODS_ICON_SIZE.xxs}
-                  color={ODS_THEME_COLOR_INTENT.primary}
-                />
-              )}
-            </div>
-          </OsdsSelectOption>
-        ))}
-      </OsdsSelect>
+      <Select
+        styles={selectStyles}
+        isLoading={isLoading}
+        isSearchable={true}
+        formatOptionLabel={getProjectOption}
+        options={options}
+        menuPosition={'absolute'}
+        onMenuOpen={() => onMenuOpen && onMenuOpen()}
+        value={value}
+        data-testid="project-selector"
+        onChange={(option) => {
+          if (option.seeAll) {
+            onSeeAllProjects();
+          } else {
+            onProjectChange(
+              projects.find(
+                ({ project_id: projectId }) => projectId === option.id,
+              ),
+            );
+          }
+        }}
+      />
     </>
   );
 };
