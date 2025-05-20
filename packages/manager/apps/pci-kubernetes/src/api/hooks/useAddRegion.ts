b9d8dacb320ec986b464be40dede5652d3f7bdeb
Pierre-Philippe <ppprevost@aol.fr>
2025-05-20 09:27:41 +0200

    feat(pci-kubernetes): allow region enabling on cluster
    ref: #TAPC-3439
    
    Signed-off-by: Pierre-Philippe <ppprevost@aol.fr>
    Coauthored-by: CDS Translator Agent <opensource@ovh.net>

---
+++ packages/manager/apps/pci-kubernetes/src/api/hooks/useAddRegion.ts
@@ -0,0 +1,37 @@
+import { TLocalisation } from '@ovh-ux/manager-pci-common';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { addProjectRegion } from '../data/regions';
+
+export type TUseAddProjectRegionArgs = {
+  projectId: string;
+  onError: (cause: Error) => void;
+  onSuccess: (data: TLocalisation, variables: string, context: unknown) => void;
+};
+
+export const useQueryRegionQueryKey = (projectId: string) => [
+  'project',
+  projectId,
+  'regions',
+];
+
+export const useAddProjectRegion = ({
+  projectId,
+  onError,
+  onSuccess,
+}: TUseAddProjectRegionArgs) => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: (region: string) => addProjectRegion(projectId, region),
+    onError,
+    onSuccess: async (...arg) => {
+      await queryClient.invalidateQueries({
+        queryKey: useQueryRegionQueryKey(projectId),
+      });
+      onSuccess(...arg);
+    },
+  });
+  return {
+    addRegion: mutation.mutate,
+    ...mutation,
+  };
+};
