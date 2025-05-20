11cd1e4aa7640f05a1aa45316bac52fdc9de2673
A.Ottr <dustin.kroger.ext@ovhcloud.com>
2025-05-20 16:25:16 +0200

    feat(container): add ServiceNow livechat integration
    Add ServiceNow livechat integration and add tests to livechat component
    
    ref: #MANAGER-15587, #MANAGER-16561
    
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/apps/container/src/container/index.tsx
+++ packages/manager/apps/container/src/container/index.tsx
@@ -1,6 +1,5 @@
 import { Suspense, useEffect, useState } from 'react';
 
-import { Environment } from '@ovh-ux/manager-config';
 import LegacyContainer from '@/container/legacy';
 import NavReshuffleContainer from '@/container/nav-reshuffle';
 import { useShell } from '@/context';
@@ -9,25 +8,13 @@ import { ProductNavReshuffleProvider } from '@/core/product-nav-reshuffle';
 import { ProgressProvider } from '@/context/progress';
 import CookiePolicy from '@/cookie-policy/CookiePolicy';
 import SSOAuthModal from '@/sso-auth-modal/SSOAuthModal';
-import LiveChat from '@/components/LiveChat';
 import { ModalsProvider } from '@/context/modals';
+import LiveChat from '@/container/livechat/LiveChat.component';
 
 export default function Container(): JSX.Element {
-  const {
-    isLoading,
-    betaVersion,
-    useBeta,
-    chatbotOpen,
-    chatbotReduced,
-    setChatbotReduced,
-  } = useContainer();
+  const { isLoading, betaVersion, useBeta } = useContainer();
   const shell = useShell();
   const [isCookiePolicyApplied, setIsCookiePolicyApplied] = useState(false);
-  const environment: Environment = shell
-    .getPlugin('environment')
-    .getEnvironment();
-  const language = environment.getUserLanguage();
-  const { ovhSubsidiary, supportLevel } = environment.getUser();
 
   const isNavReshuffle = betaVersion && useBeta;
 
@@ -66,16 +53,11 @@ export default function Container(): JSX.Element {
             <LegacyContainer />
           </>
         )}
-        <LiveChat
-          supportLevel={supportLevel?.level}
-          language={language}
-          subsidiary={ovhSubsidiary}
-          open={chatbotOpen}
-          reduced={chatbotReduced}
-          onReduce={() => setChatbotReduced(true)}
-          onClose={() => shell.getPlugin('ux').closeChatbot()}
-          style={{ position: 'absolute' }}
-        ></LiveChat>
+        <Suspense fallback="">
+          <LiveChat
+            closeLiveChat={() => shell.getPlugin('ux').closeChatbot()}
+          />
+        </Suspense>
       </ProgressProvider>
 
       <Suspense fallback="">
