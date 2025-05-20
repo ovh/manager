39c1099d7c5045f32a777f01904e61efd4d45c8c
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2025-05-20 10:49:51 +0530

    feat(ips.byoip): regon selection, token selection, ip range selection
    ref: #MANAGER-17441
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

--- packages/manager/apps/ips/src/pages/byoip/ByoipOrder.component.tsx
+++ packages/manager/apps/ips/src/pages/byoip/ByoipOrder.component.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
+import { ByoipContext } from './Byoip.context';
 import { RirSelectionSection } from './sections/RirSelectionSection.component';
 import { RegionSelectionSection } from './sections/RegionSelectionSection.component';
-import { ByoipContext } from './Byoip.context';
+import { TokenSelectionSection } from './sections/TokenSection.component';
+import { IpRangeSelectionSection } from './sections/IpRangeSelectionSection.component';
+import { useGetTokens } from '@/data/hooks/useGetTokens';
 
 export const ByoipOrder: React.FC = () => {
   const { ipRir, selectedRegion } = React.useContext(ByoipContext);
 
+  const { token } = useGetTokens({
+    campus: selectedRegion,
+  });
+
   const visibleSections = {
-    ipRirSelection: ipRir,
-    regionSelection: selectedRegion,
+    regionSelection: ipRir,
+    tokenSelection: selectedRegion,
+    ipRangeSelection: token,
+    // asSelection: ipRange,
   };
 
   return (
     <>
-      {<RirSelectionSection />}
-      {visibleSections.ipRirSelection && <RegionSelectionSection />}
+      <RirSelectionSection />
+      {visibleSections.regionSelection && <RegionSelectionSection />}
+      {visibleSections.tokenSelection && <TokenSelectionSection />}
+      {visibleSections.ipRangeSelection && <IpRangeSelectionSection />}
+      {/* {visibleSections.ipRangeSelection && <AsSelectionSection />} */}
       {/* { <IpRangeSelectionSection />}
         { <AsSelectionSection />}
         {<OrderButtonSection />}  */}
