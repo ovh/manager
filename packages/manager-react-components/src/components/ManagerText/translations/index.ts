5fc5531e4b06d33d440e2e7f0a4eb71287d5cd34
helabenkhalfallah <hela.ben-khalfallah@corp.ovh.com>
2025-05-21 17:31:01 +0200

    feat(*): lazy load hooks translation
    ref: MANAGER-18402
    
    Signed-off-by: helabenkhalfallah <hela.ben-khalfallah@corp.ovh.com>

--- packages/manager-react-components/src/components/ManagerText/translations/index.ts
+++ packages/manager-react-components/src/components/ManagerText/translations/index.ts
@@ -1,9 +1,11 @@
 import i18next from 'i18next';
 
 import fr_FR from './Messages_fr_FR.json';
+import en_GB from './Messages_en_GB.json';
 
 function addTranslations() {
   i18next.addResources('fr_FR', 'iam', fr_FR);
+  i18next.addResources('en_GB', 'iam', en_GB);
 }
 
 if (i18next.isInitialized) {
