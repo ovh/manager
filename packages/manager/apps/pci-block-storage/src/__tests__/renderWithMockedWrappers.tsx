e91a5cf1e70406dda73ef997fda7c4748e990b0f
Simon Chaumet <simon.chaumet.ext@ovhcloud.com>
2025-05-22 11:47:04 +0200

    feat(pci-block-storage): add onboarding tracking
    ref: #TAPC-3354
    
    Signed-off-by: Simon Chaumet <simon.chaumet.ext@ovhcloud.com>

---
+++ packages/manager/apps/pci-block-storage/src/__tests__/renderWithMockedWrappers.tsx
@@ -0,0 +1,32 @@
+import { render, RenderOptions } from '@testing-library/react';
+import { PropsWithChildren, ReactElement } from 'react';
+import {
+  ShellContext,
+  ShellContextType,
+} from '@ovh-ux/manager-react-shell-client';
+
+const shellContextValue: RecursivePartial<ShellContextType> = {
+  shell: {
+    navigation: {
+      getURL: (_, path) => Promise.resolve(`https://ovh.test/#${path}`),
+    },
+    ux: {
+      hidePreloader: () => Promise.resolve(),
+    },
+  },
+  environment: {
+    getUser: () => ({ ovhSubsidiary: 'test' }),
+    getRegion: () => 'EU',
+  },
+};
+
+const AllTheProviders = ({ children }: PropsWithChildren) => (
+  <ShellContext.Provider value={shellContextValue as ShellContextType}>
+    {children}
+  </ShellContext.Provider>
+);
+
+export const renderWithMockedWrappers = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>,
+) => render(ui, { wrapper: AllTheProviders, ...options });
