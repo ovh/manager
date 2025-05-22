97d5930e42b3f53773ae5bc45efab791c8e54949
Simon Chaumet <simon.chaumet.ext@ovhcloud.com>
2025-05-22 11:47:04 +0200

    feat(pci-block-storage): add onboarding tracking
    ref: #TAPC-3354
    
    Signed-off-by: Simon Chaumet <simon.chaumet.ext@ovhcloud.com>

---
+++ packages/manager/apps/pci-block-storage/__mocks__/react-router-dom.tsx
@@ -0,0 +1,15 @@
+import { vi } from 'vitest';
+
+export * from 'react-router-dom';
+
+export const useMatches = vi.fn(() => [{}]);
+
+export const useParams = vi.fn(() => ({
+  projectId: 'my-project-id',
+}));
+
+export const useHref = vi.fn((path) => path);
+
+export const useHistory = vi.fn((path) => path);
+
+export const Navigate = vi.fn(() => <></>);
