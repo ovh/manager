8ef203549e2713e7dc010036fa5e6e523b4fe03a
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2025-05-23 14:16:05 +0530

    feat(ips.byoip): as type section in order byoip flow
    ref: #MANAGER-17441
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

--- packages/manager/apps/ips/src/pages/byoip/Byoip.context.tsx
+++ packages/manager/apps/ips/src/pages/byoip/Byoip.context.tsx
@@ -5,23 +5,31 @@ export const ByoipContext = React.createContext<{
   setIpRir: React.Dispatch<React.SetStateAction<string>>;
   selectedRegion: string;
   setSelectedRegion: React.Dispatch<React.SetStateAction<string>>;
-  ipRange: string;
-  setIpRange: React.Dispatch<React.SetStateAction<string>>;
-  as: string;
-  setAs: React.Dispatch<React.SetStateAction<string>>;
   selectedToken: string;
   setSelectedToken: React.Dispatch<React.SetStateAction<string>>;
+  ipRange: string;
+  setIpRange: React.Dispatch<React.SetStateAction<string>>;
+  asType: string;
+  setAsType: React.Dispatch<React.SetStateAction<string>>;
+  asOwnRirType: string;
+  setAsOwnRirType: React.Dispatch<React.SetStateAction<string>>;
+  asOwnNumberType: number;
+  setAsOwnNumberType: React.Dispatch<React.SetStateAction<number>>;
 }>({
   ipRir: '',
   setIpRir: () => {},
   selectedRegion: '',
   setSelectedRegion: () => {},
-  ipRange: '',
-  setIpRange: () => {},
-  as: '',
-  setAs: () => {},
   selectedToken: '',
   setSelectedToken: () => {},
+  ipRange: '',
+  setIpRange: () => {},
+  asType: '',
+  setAsType: () => {},
+  asOwnRirType: '',
+  setAsOwnRirType: () => {},
+  asOwnNumberType: 0,
+  setAsOwnNumberType: () => {},
 });
 
 export const ByoipContextProvider: React.FC<React.PropsWithChildren> = ({
@@ -31,22 +39,35 @@ export const ByoipContextProvider: React.FC<React.PropsWithChildren> = ({
   const [selectedRegion, setSelectedRegion] = React.useState<string>('');
   const [selectedToken, setSelectedToken] = React.useState<string>('');
   const [ipRange, setIpRange] = React.useState<string>('');
-  const [as, setAs] = React.useState<string>('');
-
+  const [asType, setAsType] = React.useState<string>('');
+  const [asOwnRirType, setAsOwnRirType] = React.useState<string>('');
+  const [asOwnNumberType, setAsOwnNumberType] = React.useState<number>();
   const value = React.useMemo(
     () => ({
       ipRir,
       setIpRir,
       selectedRegion,
       setSelectedRegion,
-      ipRange,
-      setIpRange,
-      as,
-      setAs,
       selectedToken,
       setSelectedToken,
+      ipRange,
+      setIpRange,
+      asType,
+      setAsType,
+      asOwnRirType,
+      setAsOwnRirType,
+      asOwnNumberType,
+      setAsOwnNumberType,
     }),
-    [ipRir, selectedRegion, ipRange, as, selectedToken],
+    [
+      ipRir,
+      selectedRegion,
+      selectedToken,
+      ipRange,
+      asType,
+      asOwnRirType,
+      asOwnNumberType,
+    ],
   );
 
   return (
