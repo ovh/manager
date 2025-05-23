73f7a9155c4bc63ea7f025b5656f6ccdba4f01b5
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-05-23 17:48:39 +0200

    feat(ips): modify ip reverse modal
    ref: #MANAGER-17626
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager/apps/ips/src/data/api/get/ipReverse.ts
+++ packages/manager/apps/ips/src/data/api/get/ipReverse.ts
@@ -1,8 +1,13 @@
-import { IcebergFetchResultV6, fetchIcebergV6 } from '@ovh-ux/manager-core-api';
+import {
+  IcebergFetchResultV6,
+  fetchIcebergV6,
+  v6,
+  ApiResponse,
+} from '@ovh-ux/manager-core-api';
 
 export type GetIpReverseParams = {
-  /**  */
   ip: string;
+  ipReverse?: string;
 };
 
 export type IpReverseType = {
@@ -10,17 +15,37 @@ export type IpReverseType = {
   reverse: string;
 };
 
-export const getIpReverseQueryKey = (params: GetIpReverseParams) => [
-  `get/ip/${encodeURIComponent(params.ip)}/reverse`,
+export const getIcebergIpReverseQueryKey = ({
+  ip,
+  ipReverse = '',
+}: GetIpReverseParams) => [
+  `get/iceberg/ip/${encodeURIComponent(ip)}/reverse/${ipReverse}`,
+];
+
+export const getIpReverseQueryKey = ({
+  ip,
+  ipReverse = '',
+}: GetIpReverseParams) => [
+  `get/ip/${encodeURIComponent(ip)}/reverse/${ipReverse}`,
 ];
 
 /**
  * Your IP : Get this object properties
  */
-export const getIpReverse = async (
+export const getIcebergIpReverse = async (
   params: GetIpReverseParams,
 ): Promise<IcebergFetchResultV6<IpReverseType>> =>
   fetchIcebergV6<IpReverseType>({
     route: `/ip/${encodeURIComponent(params.ip)}/reverse`,
     page: 1,
   });
+
+export const getIpReverse = async ({
+  ip,
+  ipReverse,
+}: GetIpReverseParams): Promise<ApiResponse<IpReverseType>> =>
+  v6.get<IpReverseType>(
+    `/ip/${encodeURIComponent(ip)}/reverse${
+      ipReverse ? `/${encodeURIComponent(ipReverse)}` : ''
+    }`,
+  );
