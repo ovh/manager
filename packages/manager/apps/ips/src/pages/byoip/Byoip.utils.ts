03834ea6cf782ec115852a4ac159e304c5ed53d6
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2025-05-23 14:16:05 +0530

    feat(ips.byoip): as type section in order byoip flow
    ref: #MANAGER-17441
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

--- packages/manager/apps/ips/src/pages/byoip/Byoip.utils.ts
+++ packages/manager/apps/ips/src/pages/byoip/Byoip.utils.ts
@@ -1,4 +1,9 @@
-import { ProductConfiguration } from '@/data/hooks/catalog';
+import JSURL from 'jsurl';
+import {
+  ProductConfiguration,
+  BYOIP_FAILOVER_V4,
+  BYOIP_PRODUCT_ID,
+} from '@/data/hooks/catalog';
 
 export const getConfigValues = (
   configs: ProductConfiguration[] = [],
@@ -17,3 +22,33 @@ const byoipIpRangeRe = new RegExp(pattern);
 // The subnet address is limited to only "/24".
 export const isValidIpRange = (subnet: string): boolean =>
   byoipIpRangeRe.test(subnet);
+
+export const AS_OPTIONS = ['ovh_cloud', 'own'];
+
+export type ByoipPayloadParams = {
+  ipRir: string;
+  ip: string;
+  asRir: string;
+  asNumber: number;
+  campus: {
+    name: string;
+    planCode: string;
+  };
+};
+
+export type ConfigItem = {
+  label: keyof ByoipPayloadParams;
+  values: (string | number | { name: string; planCode: string })[];
+};
+
+/**
+ * Returns the express order settings
+ */
+export const getByoipProductSettings = (config: ConfigItem[]) =>
+  JSURL.stringify({
+    planCode: BYOIP_FAILOVER_V4,
+    configuration: [...config].filter(Boolean),
+    option: [],
+    quantity: 1,
+    productId: BYOIP_PRODUCT_ID,
+  });
