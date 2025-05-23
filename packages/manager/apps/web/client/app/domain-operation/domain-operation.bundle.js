615be29b123a12ff18539bf5abdddd76fa2f6e77
Kylian Bainvel <kylian.bainvel@ovhcloud.com>
2025-05-23 13:13:21 +0200

    feat(web): clean unused code replaced by web-ongoing-operations µApp
    ref: #MANAGER-18437
    Signed-off-by: Kylian Bainvel <kylian.bainvel@ovhcloud.com>

--- packages/manager/apps/web/client/app/domain-operation/domain-operation.bundle.js
+++ packages/manager/apps/web/client/app/domain-operation/domain-operation.bundle.js
@@ -1,4 +1,2 @@
-import './operation-table/operation-table.module';
-import './progress/domain-operation-progress.controller';
 import './update/domain-operation-update.controller';
 import './domain-operation.module';
