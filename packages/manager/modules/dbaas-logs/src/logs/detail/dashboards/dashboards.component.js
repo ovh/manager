d2bd623f22dea0c76df0260cc8ab2c516c029ee1
Pierre De Paepe <pierre.de-paepe@ovhcloud.com>
2025-05-24 21:09:24 +0200

    feat(ldp): enable iam
    ref: #OB-5678
    
    Signed-off-by: Pierre De Paepe <pierre.de-paepe@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/dbaas-logs/src/logs/detail/dashboards/dashboards.component.js
+++ packages/manager/modules/dbaas-logs/src/logs/detail/dashboards/dashboards.component.js
@@ -2,6 +2,9 @@ import controller from './logs-dashboards.controller';
 import template from './logs-dashboards.html';
 
 export default {
+  bindings: {
+    service: '<',
+  },
   controller,
   controllerAs: 'ctrl',
   template,
