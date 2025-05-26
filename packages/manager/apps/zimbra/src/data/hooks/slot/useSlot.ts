dd7c2400af161f1b69468032ce710b4bf89738c1
Atef ZAAFOURI <atefzed@gmail.com>
2025-05-26 17:25:00 +0200

    feat(zimbra): cancel & revert account subscription (#17322)
    ref: #MANAGER-18252
    
    Signed-off-by: Atef ZAAFOURI <atefzed@gmail.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

---
+++ packages/manager/apps/zimbra/src/data/hooks/slot/useSlot.ts
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react';
+import {
+  useQuery,
+  UseQueryOptions,
+  UseQueryResult,
+} from '@tanstack/react-query';
+import { useParams, useSearchParams } from 'react-router-dom';
+import {
+  getZimbraPlatformSlot,
+  getZimbraPlatformSlotQueryKey,
+  makeSlotService,
+  SlotType,
+} from '@/data/api';
+import { useSlotService } from '../services/useSlotService';
+import { SlotWithService } from './useSlots';
+
+type UseSlotParams = Omit<UseQueryOptions, 'queryKey' | 'queryFn'> & {
+  slotId?: string;
+};
+
+export const useSlot = (props: UseSlotParams = {}) => {
+  const { slotId, ...options } = props;
+  const { platformId, slotId: paramsId } = useParams();
+  const [searchParams] = useSearchParams();
+  const id = slotId || paramsId || searchParams.get('slotId');
+  return useQuery({
+    queryKey: getZimbraPlatformSlotQueryKey(platformId, id),
+    queryFn: () => getZimbraPlatformSlot(platformId, id),
+    ...options,
+    enabled: (query) =>
+      (typeof options.enabled === 'function'
+        ? options.enabled(query)
+        : typeof options.enabled !== 'boolean' || options.enabled) &&
+      !!id &&
+      !!platformId,
+  }) as UseQueryResult<SlotType>;
+};
+
+export const useSlotWithService = (options: UseSlotParams = {}) => {
+  const [slotWithService, setSlotWithService] = useState<SlotWithService>();
+  const { data: slot, isLoading: isLoadingSlot } = useSlot(options);
+  const { data: service, isLoading: isLoadingServices } = useSlotService({
+    gcTime: 0,
+    ...(options.enabled ? { enabled: options.enabled } : {}),
+  });
+
+  useEffect(() => {
+    if (slot && service) {
+      setSlotWithService({
+        id: slot.id,
+        offer: slot.currentState.offer,
+        email: slot.currentState.email,
+        service: makeSlotService(service),
+      });
+    }
+  }, [slot, service]);
+
+  return {
+    slotWithService,
+    isLoading: isLoadingSlot || isLoadingServices,
+  };
+};
--- packages/manager/apps/zimbra/src/pages/dashboard/emailAccounts/slots/ActionButton.component.tsx
+++ packages/manager/apps/zimbra/src/pages/dashboard/emailAccounts/slots/ActionButton.component.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { ActionMenu } from '@ovh-ux/manager-react-components';
+import { ActionMenu, ActionMenuItem } from '@ovh-ux/manager-react-components';
 import { useNavigate } from 'react-router-dom';
-import { ODS_BUTTON_VARIANT } from '@ovhcloud/ods-components';
+import { ODS_BUTTON_COLOR, ODS_BUTTON_VARIANT } from '@ovhcloud/ods-components';
 import {
   ButtonType,
   PageLocation,
@@ -11,7 +11,12 @@ import {
 import { SlotWithService, usePlatform } from '@/data/hooks';
 import { useGenerateUrl } from '@/hooks';
 import { IAM_ACTIONS } from '@/utils/iamAction.constants';
-import { CONFIGURE_SLOT } from '@/tracking.constants';
+import {
+  CANCEL_SLOT,
+  CONFIGURE_SLOT,
+  UNDO_CANCEL_SLOT,
+} from '@/tracking.constants';
+import { ServiceBillingState } from '@/data/api';
 
 interface ActionButtonSlotProps {
   item: SlotWithService;
@@ -37,7 +42,34 @@ export const ActionButtonSlot: React.FC<ActionButtonSlotProps> = ({ item }) => {
     navigate(hrefConfigureSlot);
   };
 
-  const actionItems = [
+  const hrefCancelSlot = useGenerateUrl(`./slot/${item.id}/cancel`, 'path');
+
+  const handleCancelSlotClick = () => {
+    trackClick({
+      location: PageLocation.datagrid,
+      buttonType: ButtonType.button,
+      actionType: 'navigation',
+      actions: [CANCEL_SLOT],
+    });
+    navigate(hrefCancelSlot);
+  };
+
+  const hrefUndoCancelSlot = useGenerateUrl(
+    `./slot/${item.id}/undo_cancel`,
+    'path',
+  );
+
+  const handleUndoCancelSlotClick = () => {
+    trackClick({
+      location: PageLocation.datagrid,
+      buttonType: ButtonType.button,
+      actionType: 'navigation',
+      actions: [UNDO_CANCEL_SLOT],
+    });
+    navigate(hrefUndoCancelSlot);
+  };
+
+  const actionItems: ActionMenuItem[] = [
     {
       id: 1,
       onClick: handleConfigureSlotClick,
@@ -46,6 +78,28 @@ export const ActionButtonSlot: React.FC<ActionButtonSlotProps> = ({ item }) => {
       label: t('configure_account'),
     },
   ];
+
+  if (item?.service?.state === ServiceBillingState.AUTOMATIC_RENEWAL) {
+    actionItems.push({
+      id: actionItems.length + 1,
+      onClick: handleCancelSlotClick,
+      urn: platformUrn,
+      iamActions: [IAM_ACTIONS.account.edit],
+      label: t('terminate'),
+      color: ODS_BUTTON_COLOR.critical,
+    });
+  }
+
+  if (item?.service?.state === ServiceBillingState.CANCELATION_PLANNED) {
+    actionItems.push({
+      id: actionItems.length + 1,
+      onClick: handleUndoCancelSlotClick,
+      urn: platformUrn,
+      iamActions: [IAM_ACTIONS.account.edit],
+      label: t('undo_cancel_slot'),
+      color: ODS_BUTTON_COLOR.critical,
+    });
+  }
 
   return (
     <ActionMenu
