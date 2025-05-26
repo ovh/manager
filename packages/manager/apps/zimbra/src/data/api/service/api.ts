b4a6230413d8a0373b705bbf0a24d3d5d39b91f5
Atef ZAAFOURI <atefzed@gmail.com>
2025-05-26 17:25:00 +0200

    feat(zimbra): cancel & revert account subscription (#17322)
    ref: #MANAGER-18252
    
    Signed-off-by: Atef ZAAFOURI <atefzed@gmail.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/apps/zimbra/src/data/api/service/api.ts
+++ packages/manager/apps/zimbra/src/data/api/service/api.ts
@@ -1,7 +1,12 @@
-import { fetchIcebergV6, IcebergFetchParamsV6 } from '@ovh-ux/manager-core-api';
+import {
+  fetchIcebergV6,
+  IcebergFetchParamsV6,
+  v6,
+} from '@ovh-ux/manager-core-api';
 import { ServiceDetails } from '@ovh-ux/manager-react-components';
 import { buildURLSearchParams } from '@/utils';
 import { makeSlotServiceHashmap } from './utils';
+import { SlotServiceBodyParamsType } from './type';
 
 // GET
 
@@ -27,4 +32,30 @@ export const getSlotServices = async () => {
 
   // create a lookup table slotId->slotService
   return makeSlotServiceHashmap(data);
+};
+
+export const getServiceByResourceName = async (
+  resourceName: string,
+): Promise<ServiceDetails> => {
+  const searchParams = buildURLSearchParams({
+    resourceName,
+    routes: '/zimbra/slot/{slotId}',
+  });
+  const { data } = await getServices({
+    searchParams,
+    disableCache: true,
+    pageSize: 1,
+  });
+
+  return data?.[0];
+};
+
+// PUT
+
+export const putService = async (
+  serviceId: number,
+  params: SlotServiceBodyParamsType,
+) => {
+  const { data } = await v6.put(`services/${serviceId}`, params);
+  return data;
 };
