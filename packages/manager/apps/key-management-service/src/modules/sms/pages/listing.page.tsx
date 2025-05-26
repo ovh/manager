e4ad7955ebd6e27d03febf0c5ea4ed69af3c5c4b
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-05-26 17:49:33 +0200

    feat(kms): add first secrets routes
    ref: #MANAGER-17869
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

---
+++ packages/manager/apps/key-management-service/src/modules/sms/pages/listing.page.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { OdsButton } from '@ovhcloud/ods-components/react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { SMS_ROUTES_URLS } from '@sms/routes/routes.constants';
+
+export default function SecretListingPage() {
+  const navigate = useNavigate();
+  const { domainId } = useParams();
+
+  return (
+    <div>
+      <h1>Secrets Listing</h1>
+      <p>This is the secrets listing page</p>
+      <p>Domain id: {domainId}</p>
+      <OdsButton
+        label="Go to secret Detail"
+        onClick={() => navigate(SMS_ROUTES_URLS.secretDashboard('1234', 'xyz'))}
+      />
+    </div>
+  );
+}
