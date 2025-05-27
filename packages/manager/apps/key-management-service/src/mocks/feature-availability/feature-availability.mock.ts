f6743574e86222cf508d1fad7aa1983328239ed2
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-05-27 12:05:22 +0200

    refactor(kms): rename feature flags variable to KMS_FEATURES
    ref: #MANAGER-18438
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/key-management-service/src/mocks/feature-availability/feature-availability.mock.ts
+++ packages/manager/apps/key-management-service/src/mocks/feature-availability/feature-availability.mock.ts
@@ -1,7 +1,7 @@
-import { FEATURES } from '@/utils/feature-availability/feature-availability.constants';
+import { KMS_FEATURES } from '@/utils/feature-availability/feature-availability.constants';
 
 export const featuresAvailabilityMock: Record<string, boolean> = {
-  [FEATURES.KMIP_CONNECTION_GUIDE]: true,
-  [FEATURES.KMS_USAGE_GUIDE]: true,
-  [FEATURES.LOGS]: true,
+  [KMS_FEATURES.KMIP_CONNECTION_GUIDE]: true,
+  [KMS_FEATURES.KMS_USAGE_GUIDE]: true,
+  [KMS_FEATURES.LOGS]: true,
 };
