3aea2db5c1f81983509ce67f1e8bdb61202cae6d
Anoop N <anoop.n@ovhcloud.com>
2025-05-27 20:08:57 +0530

    feat(manager-react-components): hide root label in breadcrumb
    ref: #MANAGER-18288
    
    Signed-off-by: Anoop N <anoop.n@ovhcloud.com>

--- packages/manager-react-components/src/components/breadcrumb/breadcrumb.component.tsx
+++ packages/manager-react-components/src/components/breadcrumb/breadcrumb.component.tsx
@@ -10,25 +10,31 @@ export interface BreadcrumbProps {
   rootLabel: string;
   /** application name define in the shell */
   appName: string;
+  /** hides app name from breadcrumb */
+  hideRootLabel?: boolean;
 }
 
 export const Breadcrumb: React.FC<BreadcrumbProps> = ({
   rootLabel,
   appName,
+  hideRootLabel = false,
 }) => {
   const breadcrumbItems = useBreadcrumb({
     rootLabel,
     appName,
+    hideRootLabel,
   });
   return (
     <OdsBreadcrumb>
-      {breadcrumbItems?.map((item) => (
-        <OdsBreadcrumbItem
-          key={item.label}
-          href={item.href}
-          label={item.label}
-        />
-      ))}
+      {breadcrumbItems
+        ?.filter((item) => !item.hideLabel)
+        ?.map((item) => (
+          <OdsBreadcrumbItem
+            key={item.label}
+            href={item.href}
+            label={item.label}
+          />
+        ))}
     </OdsBreadcrumb>
   );
 };
