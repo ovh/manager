58f0575fb3f8eab978ab9cce1dcad86478a9f772
Louis BENSI <louis.bensi.ext@corp.ovh.com>
2025-05-27 16:09:15 +0200

    feat(web-domains): react-router migration
    Signed-off-by: Louis BENSI <louis.bensi.ext@corp.ovh.com>
    
    feat(web-ongoing-operations): react-router migration
    
    ref: #MANAGER-18312
    
    Signed-off-by: Louis BENSI <louis.bensi.ext@corp.ovh.com>

--- packages/manager/apps/web-ongoing-operations/src/routes/routes.tsx
+++ packages/manager/apps/web-ongoing-operations/src/routes/routes.tsx
@@ -1,72 +1,58 @@
+import { ErrorBoundary } from '@ovh-ux/manager-react-components';
 import React from 'react';
 import { PageType } from '@ovh-ux/manager-react-shell-client';
+import { Route } from 'react-router-dom';
 import NotFound from '@/pages/404';
 import { urls } from '@/routes/routes.constant';
 
-const lazyRouteConfig = (importFn: CallableFunction) => {
-  return {
-    lazy: async () => {
-      const { default: moduleDefault, ...moduleExports } = await importFn();
-      return {
-        Component: moduleDefault,
-        ...moduleExports,
-      };
-    },
-  };
-};
+const LayoutPage = React.lazy(() => import('@/pages/layout'));
+const DashboardPage = React.lazy(() => import('@/pages/dashboard/Dashboard'));
+const DashboardDomainPage = React.lazy(() =>
+  import('@/pages/dashboard/domain/Domain'),
+);
+const DashboardDnsPage = React.lazy(() => import('@/pages/dashboard/dns/Dns'));
+const TrackPage = React.lazy(() => import('@/pages/tracking/Tracking'));
+const ActionPage = React.lazy(() => import('@/pages/update/Update'));
 
-export const Routes = [
-  {
-    path: urls.root,
-    ...lazyRouteConfig(() => import('@/pages/layout')),
-    children: [
-      {
-        path: urls.root,
-        ...lazyRouteConfig(() => import('@/pages/dashboard/Dashboard')),
-        children: [
-          {
-            id: 'dashboard.domain',
-            path: urls.domain,
-            ...lazyRouteConfig(() => import('@/pages/dashboard/domain/Domain')),
-            handle: {
-              tracking: {
-                pageName: 'domain',
-                pageType: PageType.dashboard,
-              },
-            },
+export default (
+  <Route
+    path={urls.root}
+    Component={LayoutPage}
+    id="root"
+    errorElement={
+      <ErrorBoundary
+        redirectionApp="web-ongoing-operations-backup"
+        isPreloaderHide={true}
+        isRouteShellSync={true}
+      />
+    }
+  >
+    <Route path={urls.root} Component={DashboardPage}>
+      <Route
+        id="dashboard.domain"
+        path={urls.domain}
+        Component={DashboardDomainPage}
+        handle={{
+          tracking: {
+            pageName: 'domain',
+            pageType: PageType.dashboard,
           },
-          {
-            id: 'dashboard.dns',
-            path: urls.dns,
-            ...lazyRouteConfig(() => import('@/pages/dashboard/dns/Dns')),
-            handle: {
-              tracking: {
-                pageName: 'dns',
-                pageType: PageType.dashboard,
-              },
-            },
+        }}
+      />
+      <Route
+        id="dashboard.dns"
+        path={urls.dns}
+        Component={DashboardDnsPage}
+        handle={{
+          tracking: {
+            pageName: 'dns',
+            pageType: PageType.dashboard,
           },
-        ],
-      },
-      {
-        id: 'track',
-        path: urls.track,
-        ...lazyRouteConfig(() => import('@/pages/tracking/Tracking')),
-      },
-      {
-        id: 'action',
-        path: urls.update,
-        ...lazyRouteConfig(() => import('@/pages/update/Update')),
-      },
-      {
-        id: '404',
-        path: urls.error404,
-        ...lazyRouteConfig(() => import('@/pages/404')),
-      },
-    ],
-  },
-  {
-    path: '*',
-    element: <NotFound />,
-  },
-];
+        }}
+      />
+    </Route>
+    <Route id="track" path={urls.track} Component={TrackPage} />
+    <Route id="update" path={urls.update} Component={ActionPage} />
+    <Route path="*" element={<NotFound />} />
+  </Route>
+);
