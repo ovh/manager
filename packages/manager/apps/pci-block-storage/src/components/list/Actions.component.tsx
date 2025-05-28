b697ff28384a727ea3acd577d46e7004e20e7e79
Simon Chaumet <simon.chaumet.ext@ovhcloud.com>
2025-05-28 17:42:58 +0200

    feat(pci-block-storage): add listing tracking
    ref: #TAPC-3354
    
    Signed-off-by: Simon Chaumet <simon.chaumet.ext@ovhcloud.com>

--- packages/manager/apps/pci-block-storage/src/components/list/Actions.component.tsx
+++ packages/manager/apps/pci-block-storage/src/components/list/Actions.component.tsx
@@ -1,6 +1,11 @@
 import { useHref } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import { ActionMenu } from '@ovh-ux/manager-react-components';
+import { ActionMenu, ActionMenuItem } from '@ovh-ux/manager-react-components';
+import {
+  ButtonType,
+  PageLocation,
+  useOvhTracking,
+} from '@ovh-ux/manager-react-shell-client';
 import { TVolume } from '@/api/data/volume';
 
 type ActionsProps = {
@@ -12,18 +17,29 @@ export default function ActionsComponent({
   projectUrl,
 }: Readonly<ActionsProps>) {
   const { t } = useTranslation();
+  const { trackClick } = useOvhTracking();
+
   const hrefEdit = useHref(`./${volume.id}/edit`);
   const hrefAttach = useHref(`./attach/${volume.id}`);
   const hrefDetach = useHref(`./detach/${volume.id}`);
   const hrefRemove = useHref(`./delete/${volume.id}`);
   const hrefCreateBackup = `${projectUrl}/storages/volume-backup/create?volumeId=${volume.id}`;
 
-  const items = [
+  const trackingParams = [volume.region, volume.type].join('_');
+
+  const items: (ActionMenuItem & { dataTestid?: string })[] = [
     {
       id: 0,
       href: hrefEdit,
       label: t('pci_projects_project_storages_blocks_edit_label'),
       dataTestid: 'actionComponent-edit-button',
+      onClick: () => {
+        trackClick({
+          buttonType: ButtonType.button,
+          location: PageLocation.datagrid,
+          actions: ['edit_volume_block_storage', trackingParams],
+        });
+      },
     },
     {
       id: 1,
@@ -34,18 +50,43 @@ export default function ActionsComponent({
         }_label`,
       ),
       dataTestid: 'actionComponent-attach-detach-button',
+      onClick: () => {
+        trackClick({
+          buttonType: ButtonType.button,
+          location: PageLocation.datagrid,
+          actions: [
+            // TODO : update when multi attach is merged
+            `${volume.attachedTo?.length ? 'detach' : 'attach'}_instance`,
+            trackingParams,
+          ],
+        });
+      },
     },
     {
       id: 2,
       href: hrefCreateBackup,
       label: t('pci_projects_project_storages_blocks_create_backup_label'),
       dataTestid: 'actionComponent-create-backup-button',
+      onClick: () => {
+        trackClick({
+          buttonType: ButtonType.button,
+          location: PageLocation.datagrid,
+          actions: ['create_volume-backup', trackingParams],
+        });
+      },
     },
     {
       id: 3,
       href: hrefRemove,
       label: t('pci_projects_project_storages_blocks_delete_label'),
       dataTestid: 'actionComponent-remove-button',
+      onClick: () => {
+        trackClick({
+          buttonType: ButtonType.button,
+          location: PageLocation.datagrid,
+          actions: ['delete_volume_block_storage', trackingParams],
+        });
+      },
     },
   ];
 
