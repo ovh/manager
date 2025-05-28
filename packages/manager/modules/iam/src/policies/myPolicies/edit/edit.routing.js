05e6a1c81f366ce0a2a62bcaa7ce2f2a8f60ce71
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-05-28 14:33:09 +0200

    fix(iam): fix policy create and edit forms urls
    ref: #MANAGER-18218
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

--- packages/manager/modules/iam/src/policies/myPolicies/edit/edit.routing.js
+++ packages/manager/modules/iam/src/policies/myPolicies/edit/edit.routing.js
@@ -1,13 +1,16 @@
-import { GUIDE } from '../../iam.constants';
-import { MY_POLICIES_TRACKING_HITS } from '../../policies/myPolicies/myPolicies.constants';
+import { GUIDE } from '../../../iam.constants';
+import { MY_POLICIES_TRACKING_HITS } from '../myPolicies.constants';
 
 export default /* @ngInject */ ($stateProvider) => {
-  $stateProvider.state('iam.policy.edit', {
+  $stateProvider.state('iam.policies.myPolicies.edit', {
     url: '/edit/{policy:uuid}',
-    component: 'iamCreatePolicy',
+    views: {
+      '@iam': {
+        component: 'iamCreatePolicy',
+      },
+    },
     resolve: {
-      breadcrumb: /* @ngInject */ ($translate, policy) =>
-        $translate.instant('iam_policy_edit', { policy: policy.name }),
+      breadcrumb: /* @ngInject */ (policy) => policy.name,
 
       /**
        * The Policy parameter based on the policy's id
