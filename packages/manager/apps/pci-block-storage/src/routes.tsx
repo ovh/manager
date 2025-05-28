53aea12c015f4ce4956ba1f49d7c6cded0618b56
Simon Chaumet <simon.chaumet.ext@ovhcloud.com>
2025-05-28 17:42:58 +0200

    feat(pci-block-storage): add listing tracking
    ref: #TAPC-3354
    
    Signed-off-by: Simon Chaumet <simon.chaumet.ext@ovhcloud.com>

--- packages/manager/apps/pci-block-storage/src/routes.tsx
+++ packages/manager/apps/pci-block-storage/src/routes.tsx
@@ -30,7 +30,7 @@ const ROUTES: RouteObject[] = [
       {
         path: '',
         handle: {
-          tracking: { pageName: 'blocks' },
+          tracking: { pageType: PageType.listing },
         },
         ...lazyRouteConfig(() => import('@/pages/list/List.page')),
         children: [
