2838661aedb024227a185f502585f09f9d184d88
Lio B <lionel95200x@users.noreply.github.com>
2025-05-30 10:01:17 +0200

    feat(pci): use common catalog method (#17053)
    ref: #TAPC-4029
    
    Signed-off-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>

--- packages/manager/apps/pci-block-storage/src/api/hooks/useCatalog.ts
+++ packages/manager/apps/pci-block-storage/src/api/hooks/useCatalog.ts
@@ -1,23 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { useMe } from '@ovh-ux/manager-react-components';
-import { getCatalog } from '@ovh-ux/manager-pci-common';
 import { getVolumeCatalog } from '@/api/data/catalog';
-
-export const getCatalogQuery = (ovhSubsidiary: string) => ({
-  queryKey: ['catalog'],
-  queryFn: () => getCatalog(ovhSubsidiary),
-});
-
-/**
- * @deprecated use {@link useVolumeCatalog} instead
- */
-export const useCatalog = () => {
-  const { me } = useMe();
-  return useQuery({
-    ...getCatalogQuery(me?.ovhSubsidiary),
-    enabled: !!me,
-  });
-};
 
 export const useVolumeCatalog = (projectId: string) =>
   useQuery({
