791076bd2be939e2e4d6b13eec20cd4c7fbe1df2
Anoop N <anoop.n@ovhcloud.com>
2025-05-30 20:24:45 +0530

    docs(manager-wiki): set-up msw to mock api calls
    ref: #MANAGER-18296
    
    Signed-off-by: Anoop N <anoop.n@ovhcloud.com>

--- packages/manager-wiki/stories/manager-react-components/components/ManagerButton/ManagerButton.stories.tsx
+++ packages/manager-wiki/stories/manager-react-components/components/ManagerButton/ManagerButton.stories.tsx
@@ -8,12 +8,12 @@ export const Default = {
   args: {
     id: 'iam-button-urn-action-1',
     label: 'Remove button',
-    urn: 'urn:v9:eu:resource:manager-react-components:vrz-a878-dsflkds-fdsfsd',
+    urn: 'urn:v9:eu:resource:manager-react-components:with-authorization',
     iamActions: ['vrackServices:apiovh:iam/resource/tag/remove'],
   },
 };
 
-const managerSimpleButton: Meta<ManagerButtonProps> = {
+const ButtonWithAuthorization: Meta<ManagerButtonProps> = {
   title: 'Manager React Components/Components/Manager Button',
   component: ManagerButton,
   parameters: {
@@ -26,4 +26,21 @@ const managerSimpleButton: Meta<ManagerButtonProps> = {
   },
 };
 
-export default managerSimpleButton;
+export const ButtonWithUnauthorization: Meta<ManagerButtonProps> = {
+  title: 'Manager React Components/Components/Manager Button',
+  component: ManagerButton,
+  args: {
+    ...Default.args,
+    urn: 'urn:v9:eu:resource:manager-react-components:without-authorization',
+  },
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'The `ManagerButton` component is used to trigger an action or event.',
+      },
+    },
+  },
+};
+
+export default ButtonWithAuthorization;
