0a992b857c549bed3be8fc1e32d118f99c9d1280
ahmed sefiani <ahmed.sefiani.ext@corp.ovh.com>
2025-06-02 16:37:12 +0200

    feat(vcd): add order vcd cta in listing page
    ref: #MANAGER-15369
    
    Signed-off-by: ahmed sefiani <ahmed.sefiani.ext@corp.ovh.com>

---
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/utils/orderVcdRedirection.constants.ts
@@ -0,0 +1,37 @@
+export const ORDER_VCD_REDIRECTION_URL: Record<string, string> = {
+  DEFAULT:
+    'https://www.ovhcloud.com/en/hosted-private-cloud/vmware/public-vcf-aas/',
+  FR: 'https://www.ovhcloud.com/fr/hosted-private-cloud/vmware/public-vcf-aas/',
+  WE: 'https://www.ovhcloud.com/en/hosted-private-cloud/vmware/public-vcf-aas/',
+  MA:
+    'https://www.ovhcloud.com/fr-ma/hosted-private-cloud/vmware/public-vcf-aas/',
+  TN:
+    'https://www.ovhcloud.com/fr-tn/hosted-private-cloud/vmware/public-vcf-aas/',
+  SN:
+    'https://www.ovhcloud.com/fr-sn/hosted-private-cloud/vmware/public-vcf-aas/',
+  QC:
+    'https://www.ovhcloud.com/fr-ca/hosted-private-cloud/vmware/public-vcf-aas/',
+  GB:
+    'https://www.ovhcloud.com/en-gb/hosted-private-cloud/vmware/public-vcf-aas/',
+  IE:
+    'https://www.ovhcloud.com/en-ie/hosted-private-cloud/vmware/public-vcf-aas/',
+  US: 'https://us.ovhcloud.com/hosted-private-cloud/vmware/public-vcf-aas/',
+  CA:
+    'https://www.ovhcloud.com/en-ca/hosted-private-cloud/vmware/public-vcf-aas/',
+  AU:
+    'https://www.ovhcloud.com/en-au/hosted-private-cloud/vmware/public-vcf-aas/',
+  SG:
+    'https://www.ovhcloud.com/en-sg/hosted-private-cloud/vmware/public-vcf-aas/',
+  ASIA:
+    'https://www.ovhcloud.com/asia/hosted-private-cloud/vmware/public-vcf-aas/',
+  WS: 'https://www.ovhcloud.com/es/hosted-private-cloud/vmware/public-vcf-aas/',
+  ES:
+    'https://www.ovhcloud.com/es-es/hosted-private-cloud/vmware/public-vcf-aas/',
+  PT: 'https://www.ovhcloud.com/pt/hosted-private-cloud/vmware/public-vcf-aas/',
+  IT: 'https://www.ovhcloud.com/it/hosted-private-cloud/vmware/public-vcf-aas/',
+  PL: 'https://www.ovhcloud.com/pl/hosted-private-cloud/vmware/public-vcf-aas/',
+  DE: 'https://www.ovhcloud.com/de/hosted-private-cloud/vmware/public-vcf-aas/',
+  NL: 'https://www.ovhcloud.com/nl/hosted-private-cloud/vmware/public-vcf-aas/',
+  IN:
+    'https://www.ovhcloud.com/en-in/hosted-private-cloud/vmware/public-vcf-aas/',
+};
