2c9854738fe37b94edebfae1ca973e953c73f5fe
ahmed sefiani <ahmed.sefiani.ext@corp.ovh.com>
2025-06-03 17:25:45 +0200

    feat(vcd): add order cta redirection in onboarding pages
    ref: #MANAGER-15367
    
    Signed-off-by: ahmed sefiani <ahmed.sefiani.ext@corp.ovh.com>

--- packages/manager/apps/hpc-vmware-managed-vcd/src/pages/onboarding/Onboarding.spec.tsx
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/pages/onboarding/Onboarding.spec.tsx
@@ -1,5 +1,13 @@
-import { assertTextVisibility } from '@ovh-ux/manager-core-test-utils';
+import { vi } from 'vitest';
+import {
+  assertElementVisibility,
+  assertTextVisibility,
+  getElementByTestId,
+} from '@ovh-ux/manager-core-test-utils';
+import { act, waitFor } from '@testing-library/react';
 import { renderTest, labels } from '../../test-utils';
+import TEST_IDS from '../../utils/testIds.constants';
+import { ORDER_VCD_REDIRECTION_URL } from '../../utils/orderVcdRedirection.constants';
 
 describe('Onboarding Page', () => {
   it('display the onboarding page if there is no VCD Organization', async () => {
@@ -7,5 +15,34 @@ describe('Onboarding Page', () => {
     await assertTextVisibility(
       labels.onboarding.managed_vcd_onboarding_description_part1,
     );
+  });
+});
+
+describe('VCD Order CTA redirection', () => {
+  let windowOpenSpy: any;
+
+  beforeEach(() => {
+    windowOpenSpy = vi.spyOn(window, 'open').mockImplementation(() => null);
+  });
+
+  afterEach(() => {
+    windowOpenSpy.mockRestore();
+  });
+
+  it('is visible and redirects to the correct URL when clicked', async () => {
+    await renderTest({ nbOrganization: 1 });
+
+    const orderButton = await getElementByTestId(TEST_IDS.vcdOrderCta);
+    await assertElementVisibility(orderButton);
+
+    const expectedUrl = ORDER_VCD_REDIRECTION_URL.FR;
+
+    await act(async () => {
+      orderButton.click();
+    });
+
+    await waitFor(() => {
+      expect(windowOpenSpy).toHaveBeenCalledWith(expectedUrl, '_blank');
+    });
   });
 });
