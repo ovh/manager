99dc561ee4c19d855cf0d7d907cb44bb4d1c355c
David Arsène <david.arsene.ext@ovhcloud.com>
2025-06-04 15:20:50 +0200

    feat(dedicated.pcc): replace 2API networks by iceberg query
    decommission 2API sws-dedicatedcloud-networks
    
    resolves: #MANAGER-17157
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/apps/dedicated/client/app/components/dedicated-cloud/security/dedicatedCloud-security.controller.js
+++ packages/manager/apps/dedicated/client/app/components/dedicated-cloud/security/dedicatedCloud-security.controller.js
@@ -1,22 +1,14 @@
+import { NETWORK_STATUS } from './dedicatedCloud-security.constants';
+
 export default class {
   /* @ngInject */
-  constructor(
-    $rootScope,
-    $scope,
-    $translate,
-    DedicatedCloud,
-    OvhApiDedicatedCloud,
-  ) {
-    this.$rootScope = $rootScope;
-    this.$scope = $scope;
+  constructor($translate, DedicatedCloud) {
     this.$translate = $translate;
     this.DedicatedCloud = DedicatedCloud;
-    this.OvhApiDedicatedCloud = OvhApiDedicatedCloud;
+    this.NETWORK_STATUS = NETWORK_STATUS;
   }
 
   $onInit() {
-    this.forceRefresh = false;
-
     this.policies = {
       model: null,
       info: null,
@@ -56,23 +48,18 @@ export default class {
     return this.DedicatedCloud.getSecurityPolicies(
       this.productId,
       pageSize,
-      offset - 1,
-      this.forceRefresh,
-    ).then(
-      (paginatedPolicies) => {
-        this.forceRefresh = false;
+      offset,
+      true,
+    )
+      .then((results) => {
+        this.policies.model = results?.data || [];
+        return results;
+      })
+      .catch(this.onError)
+      .finally(() => {
         this.loaders.table = false;
         this.loaders.search = false;
-        this.policies.model = paginatedPolicies;
-        return {
-          data: this.policies.model.list.results,
-          meta: {
-            totalCount: this.policies.model.fullNetworksList.length,
-          },
-        };
-      },
-      (error) => this.onError(error),
-    );
+      });
   }
 
   onError(error) {
@@ -84,24 +71,14 @@ export default class {
   }
 
   initSelection() {
-    if (
-      this.policies &&
-      this.policies.model &&
-      this.policies.model.list &&
-      this.policies.model.list.results
-    ) {
-      let i = 0;
-      let l;
-      for (i, l = this.policies.model.list.results.length; i < l; i += 1) {
-        if (
-          ~this.selectedPolicies.indexOf(this.policies.model.list.results[i].id)
-        ) {
-          this.policies.model.list.results[i].selected = true;
-        } else {
-          this.policies.model.list.results[i].selected = false;
-        }
+    this.selectedPolicies.forEach((id) => {
+      const policy = this.policies.model.find(
+        ({ networkAccessId }) => networkAccessId === id,
+      );
+      if (policy) {
+        policy.selected = true;
       }
-    }
+    });
   }
 
   static formatKmsStatus(state) {
@@ -123,7 +100,7 @@ export default class {
   }
 
   globalCheckboxPoliciesStateChange(policies) {
-    this.selectedPolicies = policies.map((policy) => policy.id);
+    this.selectedPolicies = policies.map((policy) => policy.networkAccessId);
     this.initSelection();
   }
 
