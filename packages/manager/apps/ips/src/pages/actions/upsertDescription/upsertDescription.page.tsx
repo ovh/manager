65a9e4ac88ff422afd1a41991c9c0afc81e0aeab
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2025-06-05 22:04:22 +0530

    feat(ips): add upsert ip description option in listing page
    ref: #MANAGER-18380
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

---
+++ packages/manager/apps/ips/src/pages/actions/upsertDescription/upsertDescription.page.tsx
@@ -0,0 +1,118 @@
+import React, { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useNavigate, useParams } from 'react-router-dom';
+import { NAMESPACES } from '@ovh-ux/manager-common-translations';
+import { useNotifications } from '@ovh-ux/manager-react-components';
+import {
+  OdsButton,
+  OdsFormField,
+  OdsModal,
+  OdsText,
+  OdsTextarea,
+} from '@ovhcloud/ods-components/react';
+import {
+  ODS_BUTTON_VARIANT,
+  ODS_SPINNER_SIZE,
+  ODS_TEXT_PRESET,
+} from '@ovhcloud/ods-components';
+import { useUpsertIpDescription } from '@/data/hooks/ip/useUpsertIpDescription';
+import { useGetIpdetails } from '@/data/hooks/ip';
+import { fromIdToIp, ipFormatter } from '@/utils';
+import Loading from '../../listing/manageOrganisations/components/Loading/Loading';
+
+export default function UpsertDescriptionModal() {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const { ip } = ipFormatter(fromIdToIp(id));
+  const { ipDetails, isLoading } = useGetIpdetails({ ip });
+  const { addSuccess } = useNotifications();
+  const { t } = useTranslation(['listing', NAMESPACES.ACTIONS, 'error']);
+  const MAX_CHARACTERS = 250;
+  const [characterCount, setCharacterCount] = React.useState(0);
+  const [description, setDescription] = React.useState(
+    ipDetails?.description || '',
+  );
+
+  const {
+    mutate: upsertIpDescription,
+    isPending: upsertIpDescriptionPending,
+  } = useUpsertIpDescription({
+    ip,
+    description,
+    onSuccess: () => {
+      navigate('..');
+      addSuccess(t('listingUpsertDescriptionSuccessMessage', { value: ip }));
+    },
+  });
+
+  const cancel = () => navigate('..');
+
+  const confirm = () => upsertIpDescription();
+
+  useEffect(() => {
+    setDescription(ipDetails?.description);
+  }, [ipDetails?.description]);
+
+  const handleDescriptionChange = (event: CustomEvent) => {
+    const newValue = event.detail.value || '';
+    setDescription(newValue);
+    setCharacterCount(newValue.length);
+  };
+
+  return (
+    <OdsModal isOpen isDismissible onOdsClose={cancel}>
+      {isLoading ? (
+        <Loading className="flex justify-center" size={ODS_SPINNER_SIZE.sm} />
+      ) : (
+        <>
+          <OdsText className="block mb-4" preset={ODS_TEXT_PRESET.heading4}>
+            {ipDetails?.description
+              ? t('listingActionEditDescription')
+              : t('listingActionAddDescription')}
+          </OdsText>
+          {upsertIpDescriptionPending && (
+            <Loading
+              className="flex justify-center"
+              size={ODS_SPINNER_SIZE.sm}
+            />
+          )}
+          {!upsertIpDescriptionPending && (
+            <OdsFormField className="block mb-4" id="textarea-form-field">
+              <label slot="label">{t('listingUpsertDescription')}:</label>
+              <OdsText slot="visual-hint" preset="caption">
+                {characterCount}/{MAX_CHARACTERS}
+              </OdsText>
+              <OdsTextarea
+                id="form-field-textarea"
+                className="block pt-1"
+                name="parent-ip"
+                isResizable
+                rows={4}
+                maxlength={MAX_CHARACTERS}
+                value={description}
+                onOdsChange={handleDescriptionChange}
+                data-testid="textarea-form-field"
+              />
+            </OdsFormField>
+          )}
+          <OdsButton
+            slot="actions"
+            type="button"
+            variant={ODS_BUTTON_VARIANT.ghost}
+            label={t('cancel', { ns: NAMESPACES.ACTIONS })}
+            onClick={cancel}
+            data-testid="cancel-button"
+          />
+          <OdsButton
+            slot="actions"
+            type="button"
+            isDisabled={upsertIpDescriptionPending}
+            label={t('confirm', { ns: NAMESPACES.ACTIONS })}
+            onClick={confirm}
+            data-testid="confirm-button"
+          />
+        </>
+      )}
+    </OdsModal>
+  );
+}
---
+++ packages/manager/apps/ips/src/pages/actions/upsertDescription/upsertDescription.spec.tsx
@@ -0,0 +1,69 @@
+import { describe, it, vi } from 'vitest';
+import {
+  assertOdsModalVisibility,
+  getButtonByTestId,
+} from '@ovh-ux/manager-core-test-utils';
+import { waitFor, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { ODS_ICON_NAME } from '@ovhcloud/ods-components';
+
+import {
+  getButtonByIcon,
+  labels,
+  renderTest,
+  getButtonByLabel,
+} from '@/test-utils';
+
+import { urls } from '@/routes/routes.constant';
+
+describe('Ip Upsert Description test suite', () => {
+  it('should display and close the Ip Upsert Description modal', async () => {
+    const { container } = await renderTest({
+      nbIp: 2,
+      initialRoute: urls.listing,
+    });
+
+    // action menu button
+    const actionMenuButton = await getButtonByIcon({
+      container,
+      iconName: ODS_ICON_NAME.ellipsisVertical,
+    });
+    await waitFor(() => fireEvent.click(actionMenuButton));
+
+    // edit description button
+    const editDescriptionButton = await getButtonByLabel({
+      container,
+      label: labels.listing.listingActionEditDescription,
+    });
+    await waitFor(() => fireEvent.click(editDescriptionButton));
+
+    // Wait for modal to be visible
+    await waitFor(() => {
+      const modal = container.querySelector('ods-modal');
+      expect(modal).toBeInTheDocument();
+    });
+
+    // Check if modal is visible
+    await assertOdsModalVisibility({ container, isVisible: true });
+
+    // Find and click the cancel button
+    const cancelButton = await getButtonByTestId({
+      testId: 'cancel-button',
+    });
+
+    // Click the cancel button and wait for the modal to close
+    await waitFor(
+      async () => {
+        await userEvent.click(cancelButton);
+        const modal = container.querySelector('ods-modal');
+        if (modal) {
+          throw new Error('Modal is still present after clicking cancel');
+        }
+      },
+      { timeout: 5000 },
+    );
+
+    // Final check that modal is not visible
+    await assertOdsModalVisibility({ container, isVisible: false });
+  });
+});
