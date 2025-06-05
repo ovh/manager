2c7d1d290690eb8be00db257ec0379914d9374ac
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2025-06-05 10:24:39 +0200

    fix(dedicated-servers): fix hybrid apps redirection
    ref: #MANAGER-14393
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

--- packages/manager/apps/dedicated-servers/src/App.tsx
+++ packages/manager/apps/dedicated-servers/src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ShellContext } from '@ovh-ux/manager-react-shell-client';
 import { RouterProvider, createHashRouter } from 'react-router-dom';
+import { useFeatureAvailability } from '@ovh-ux/manager-react-components';
 import { routes } from './routes/routes';
 
 const queryClient = new QueryClient({
@@ -16,12 +17,30 @@ const queryClient = new QueryClient({
   },
 });
 
-function App() {
+function Routes() {
+  const { shell } = useContext(ShellContext);
   const router = createHashRouter(routes);
+  const { data, isLoading, isError } = useFeatureAvailability([
+    'dedicated-servers',
+  ]);
 
+  useEffect(() => {
+    if (!isLoading) {
+      shell.ux.hidePreloader();
+
+      if (isError || !data?.['dedicated-servers']) {
+        shell.navigation.navigateTo('dedicated', '#/server', {});
+      }
+    }
+  }, [isLoading, isError, data, shell]);
+
+  return isLoading ? <></> : <RouterProvider router={router} />;
+}
+
+function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <Routes />
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
