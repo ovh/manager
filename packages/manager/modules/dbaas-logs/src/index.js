84cb8225b4308797a891b6658c8ff700d3bd0a7c
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-06-09 17:05:41 +0200

    fix(ldp): fix guide links
    ref: #MANAGER-18334
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

--- packages/manager/modules/dbaas-logs/src/index.js
+++ packages/manager/modules/dbaas-logs/src/index.js
@@ -1,16 +1,11 @@
 import angular from 'angular';
 
 import '@ovh-ux/manager-core';
-import '@ovh-ux/ng-ovh-doc-url';
 
 import logs from './logs';
 
 const moduleName = 'ovhManagerDbaasLogs';
 
-angular.module(moduleName, ['ngOvhDocUrl', 'ovhManagerCore', logs]).config(
-  /* @ngInject */ (ovhDocUrlProvider, coreConfigProvider) => {
-    ovhDocUrlProvider.setUserLocale(coreConfigProvider.getUserLocale());
-  },
-);
+angular.module(moduleName, ['ovhManagerCore', logs]);
 
 export default moduleName;
