3231911cd19393ca52f6db5c16934a76f7498d25
Pierre-Philippe <ppprevost@aol.fr>
2025-06-10 17:18:13 +0200

    fix(pci-kubernetes): calculate multiple node price
    ref: #TAPC-3341, #TAPC-2791
    
    Signed-off-by: Pierre-Philippe <ppprevost@aol.fr>

--- packages/manager/apps/pci-kubernetes/src/pages/new/steps/ClusterNameStep.component.tsx
+++ packages/manager/apps/pci-kubernetes/src/pages/new/steps/ClusterNameStep.component.tsx
@@ -53,22 +53,32 @@ export function ClusterNameStep({
         <OsdsText
           slot="label"
           level={ODS_THEME_TYPOGRAPHY_LEVEL.body}
-          color={ODS_THEME_COLOR_INTENT.text}
+          color={
+            hasError
+              ? ODS_THEME_COLOR_INTENT.error
+              : ODS_THEME_COLOR_INTENT.text
+          }
           size={ODS_TEXT_SIZE._200}
         >
           {t('kubernetes_add_name')}
         </OsdsText>
-        <OsdsInput
-          placeholder={t('kubernetes_add_name_placeholder')}
-          value={name}
-          color={ODS_THEME_COLOR_INTENT.primary}
-          type={ODS_INPUT_TYPE.text}
-          onOdsValueChange={(e) => {
-            setName(e.detail.value ?? '');
-          }}
-          onOdsInputBlur={() => setIsTouched(true)}
-          error={hasError}
-        />
+        <div className="w-fit">
+          <OsdsInput
+            placeholder={t('kubernetes_add_name_placeholder')}
+            value={name}
+            color={
+              hasError
+                ? ODS_THEME_COLOR_INTENT.error
+                : ODS_THEME_COLOR_INTENT.primary
+            }
+            type={ODS_INPUT_TYPE.text}
+            onOdsValueChange={(e) => {
+              setName(e.detail.value ?? '');
+            }}
+            onOdsInputBlur={() => setIsTouched(true)}
+            error={hasError}
+          />
+        </div>
       </OsdsFormField>
       <OsdsButton
         className="mt-8 w-fit"
