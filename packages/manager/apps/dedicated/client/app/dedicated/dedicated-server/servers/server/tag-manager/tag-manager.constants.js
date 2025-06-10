f3ba981b3e070b7f86db062894589f3aa311fb9c
qpavy <contact@quentinpavy.com>
2025-06-10 09:11:32 +0200

    feat(dedicated): bare metal tagging project (#17435)
    ref: #MANAGER-12894
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <soufien.mhelhali.ext@ovhcloud.com>

---
+++ packages/manager/apps/dedicated/client/app/dedicated/dedicated-server/servers/server/tag-manager/tag-manager.constants.js
@@ -0,0 +1,12 @@
+export const TRACKING = {
+  PREFIX: 'dedicated::dedicated-server::dedicated-server::dedicated-server::',
+  PAGE: {
+    EDIT_TAGS: 'edit-tags',
+    ASSIGN_TAG: 'pop-up::assign-tag',
+    UNASSIGN_TAG: 'pop-up::unassign-tag',
+  },
+};
+
+export default {
+  TRACKING,
+};
---
+++ packages/manager/apps/dedicated/client/app/dedicated/dedicated-server/servers/server/tag-manager/tag-manager.routing.js
@@ -0,0 +1,36 @@
+import { TRACKING } from './tag-manager.constants';
+
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state('app.dedicated-server.server.dashboard.tag-manager', {
+    url: '/tag-manager',
+    views: {
+      'tabView@app.dedicated-server.server': {
+        component: 'ovhManagerResourceTaggingManager',
+      },
+    },
+    resolve: {
+      breadcrumb: () => null,
+      resourceName: /* @ngInject */ (serverName) => serverName,
+      goBack: /* @ngInject */ (goToServerDetails) => () => {
+        goToServerDetails();
+      },
+      goToTagAssign: /* @ngInject */ ($state, serverName) => () => {
+        $state.go('.assign', {
+          productId: serverName,
+        });
+      },
+      goToTagUnassign: /* @ngInject */ ($state, serverName) => (tags) => {
+        $state.go('.unassign', {
+          productId: serverName,
+          tags,
+        });
+      },
+      tags: /* @ngInject */ (server) => server.iam?.tags,
+      trackingPrefix: () => TRACKING.PREFIX,
+      trackingPage: () => TRACKING.PAGE.EDIT_TAGS,
+    },
+    atInternet: {
+      rename: `${TRACKING.PREFIX}${TRACKING.PAGE.EDIT_TAGS}`,
+    },
+  });
+};
