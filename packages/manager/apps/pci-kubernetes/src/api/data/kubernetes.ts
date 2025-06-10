745399e9589d649b0fae2e4da4de508f5c75c69d
Pierre-Philippe <ppprevost@aol.fr>
2025-06-10 17:18:13 +0200

    fix(pci-kubernetes): calculate multiple node price
    ref: #TAPC-3341, #TAPC-2791
    
    Signed-off-by: Pierre-Philippe <ppprevost@aol.fr>

--- packages/manager/apps/pci-kubernetes/src/api/data/kubernetes.ts
+++ packages/manager/apps/pci-kubernetes/src/api/data/kubernetes.ts
@@ -19,28 +19,28 @@ export const getAllKube = async (projectId: string): Promise<TKube[]> => {
   return data;
 };
 
-export interface NodePool {
+export type NodePool = {
   name: string;
   antiAffinity: boolean;
   autoscale: boolean;
-  availabilityZones: string[];
+  availabilityZones?: string[];
   desiredNodes: number;
   minNodes: number;
-  localisation: string; // will change with 3AZ
+  localisation: string;
   flavorName: string;
   maxNodes: number;
   monthlyBilled: boolean;
-}
+};
 
 export type NodePoolPrice = NodePool & { monthlyPrice: number };
 
-export interface KubeClusterCreationParams {
+export type KubeClusterCreationParams = {
   name: string;
   region: string;
   version: string;
   updatePolicy: string;
   nodepool?: NodePool[];
-  privateNetworkId: string;
+  privateNetworkId?: string;
   nodesSubnetId?: string;
   loadBalancersSubnetId?: string;
   availabilityZones?: string[];
@@ -48,7 +48,7 @@ export interface KubeClusterCreationParams {
     defaultVrackGateway?: string;
     privateNetworkRoutingAsDefault?: boolean;
   };
-}
+};
 
 export const createKubernetesCluster = async (
   projectId: string,
