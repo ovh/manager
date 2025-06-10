3b52220008ddaf482792b9693eb54722e518f96c
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2025-06-10 13:22:41 +0200

    fix(web-hosting): update router and vitest config
    ref: #MANAGER-18589
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/apps/web-hosting/src/App.tsx
+++ packages/manager/apps/web-hosting/src/App.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { RouterProvider, createHashRouter } from 'react-router-dom';
-import routes from './routes/routes';
+import {
+  RouterProvider,
+  createHashRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import Routes from '@/routes/routes';
 import queryClient from './utils/queryClient';
+import Loading from './components/loading/Loading.component';
 
 function App() {
-  const router = createHashRouter(routes);
+  const routes = createHashRouter(createRoutesFromElements(Routes));
 
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <Suspense fallback={<Loading />}>
+        <RouterProvider router={routes} />
+      </Suspense>
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
