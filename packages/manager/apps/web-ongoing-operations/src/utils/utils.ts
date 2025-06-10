3b200eceb303db3d718c44540e016a5ac02c116e
Justine Loizel <justine.loizel@ovhcloud.com>
2025-06-10 10:54:03 +0200

    feat(web-ongoing-operations): use of MRC formatted date
    ref: #MANAGER-18534
    
    Signed-off-by: Justine Loizel <justine.loizel@ovhcloud.com>

--- packages/manager/apps/web-ongoing-operations/src/utils/utils.ts
+++ packages/manager/apps/web-ongoing-operations/src/utils/utils.ts
@@ -1,14 +1,3 @@
-export const formatDatagridDate = (date: string, locale: string) => {
-  const formattedDate = new Date(date);
-  return formattedDate.toLocaleString(locale.replace('_', '-'), {
-    day: 'numeric',
-    month: 'numeric',
-    year: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-  });
-};
-
 export const removeQuotes = (comment: string) => {
   if (
     comment &&
