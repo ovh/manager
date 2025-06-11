55bc1aa71199829a672cd41307e8973fddbaecb2
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2025-06-11 15:07:25 +0200

    feat(pci-project): generate project code base with manager generator
    ref: MANAGER:18600
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

---
+++ packages/manager/apps/pci-project/src/i18n.ts
@@ -0,0 +1,33 @@
+import i18n from 'i18next';
+import I18NextHttpBackend from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
+
+export default async function initI18n(
+  locale = 'fr_FR',
+  availableLocales = ['fr_FR'],
+) {
+  await i18n
+    .use(initReactI18next)
+    .use(I18NextHttpBackend)
+    .use({
+      type: 'postProcessor',
+      name: 'normalize',
+      process: (value: string) =>
+        value ? value.replace(/&amp;/g, '&') : value,
+    })
+    .init({
+      lng: locale,
+      fallbackLng: 'fr_FR',
+      supportedlangs: availableLocales,
+      defaultNS: 'common',
+      ns: ['common'], // namespaces to load by default
+      backend: {
+        loadPath: (langs: string[], namespaces: string[]) =>
+          `${import.meta.env.BASE_URL}translations/${namespaces[0]}/Messages_${
+            langs[0]
+          }.json`,
+      },
+      postProcess: 'normalize',
+    });
+  return i18n;
+}
