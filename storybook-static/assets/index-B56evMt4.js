79ade9cfbc7a9546feb8f4c53822e82b074eb898
ovh-cds <ovh-cds@users.noreply.github.com>
2025-06-11 14:56:47 +0000

    docs: update documentation [skip ci]

---
+++ storybook-static/assets/index-B56evMt4.js
@@ -0,0 +1,1 @@
+import"./preview-CZewwbtn.js";import"./DocsRenderer-CFRXHY34-BdZf0lHS.js";import"./index-CVkXESCx.js";__STORYBOOK_MODULE_PREVIEW_API__;
--- storybook-static/assets/index-DAp2ESAk.js
+++
@@ -1,1 +0,0 @@
-import"./preview-0xInPT31.js";import"./DocsRenderer-CFRXHY34-BY55U_-M.js";import"./index-Cryu-uZD.js";__STORYBOOK_MODULE_PREVIEW_API__;
