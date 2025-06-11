61af4dff5aec7cb84a8b7eed50b5ffad50eef770
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2025-06-11 15:07:25 +0200

    feat(pci-project): generate project code base with manager generator
    ref: MANAGER:18600
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

---
+++ packages/manager/apps/pci-project/src/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  ShellContext,
+  initShellContext,
+  initI18n,
+} from '@ovh-ux/manager-react-shell-client';
+import '@ovh-ux/manager-react-components/dist/style.css';
+import App from './App';
+import './vite-hmr';
+import './index.scss';
+
+import { UNIVERSE, SUB_UNIVERSE, APP_NAME, LEVEL2 } from './tracking.constant';
+
+const trackingContext = {
+  chapter1: UNIVERSE,
+  chapter2: SUB_UNIVERSE,
+  chapter3: APP_NAME,
+  appName: APP_NAME,
+  pageTheme: UNIVERSE,
+  level2Config: LEVEL2,
+};
+
+const init = async (appName: string) => {
+  const context = await initShellContext(appName, trackingContext);
+
+  await initI18n({
+    context,
+    reloadOnLocaleChange: true,
+    defaultNS: appName,
+    ns: ['listing', 'dashboard', 'onboarding'],
+  });
+
+  const region = context.environment.getRegion();
+  context.shell.tracking.setConfig(region, LEVEL2);
+  try {
+    await import(`./config-${region}.js`);
+  } catch (error) {
+    // nothing to do
+  }
+
+  const rootElement = document.getElementById('root');
+  if (!rootElement) throw new Error('Root element not found');
+
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <ShellContext.Provider value={context}>
+        <App />
+      </ShellContext.Provider>
+    </React.StrictMode>,
+  );
+};
+
+init('pci-project');
