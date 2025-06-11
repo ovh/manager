1b7764140a4fc52ee432c989db768a9a552228b5
aderghamov <alexandre.dergham.ext@ovhcloud.com>
2025-06-11 11:33:18 +0200

    feat(ola-nics): adding aggregation/desaggregation buttons to nics lists
    ref: #MANAGER-17106
    
    Signed-off-by: aderghamov <alexandre.dergham.ext@ovhcloud.com>

--- packages/manager/modules/bm-server-components/src/network-interfaces/controller.js
+++ packages/manager/modules/bm-server-components/src/network-interfaces/controller.js
@@ -1,15 +1,20 @@
+import { OLA_MODES } from '../ola/ola.constants';
 import { LABELS } from './interfaces.constants';
 
 export default class BMNetworkInterfaceController {
   /* @ngInject */
-  constructor() {
+  constructor($state) {
+    this.$state = $state;
     this.LABELS = LABELS;
   }
 
-  $onChanges({ interfaces }) {
+  $onChanges({ interfaces, ola }) {
     this.displayedInterfaces = interfaces.currentValue.map((nic) => ({
       ...nic,
       displayedMacAdresses: nic.mac ? nic.mac.split(', ') : [],
     }));
+
+    this.isFullyAggregated =
+      ola.currentValue.getCurrentMode() === OLA_MODES.FULL_LAG;
   }
 }
