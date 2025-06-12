c53694656af5c557199d713ecb61c61e544ef1c5
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2025-06-12 09:13:57 +0200

    feat(gcj): create new gcj module
    ref: #MANAGER-18595
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

---
+++ packages/manager/modules/gcj/vitest.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      include: ['src'],
+      exclude: [],
+    },
+    testTimeout: 60000,
+    fileParallelism: false,
+    maxWorkers: 1,
+    pollOptions: {
+      forks: {
+        singleFork: true,
+      },
+      threads: {
+        singleThread: true,
+      },
+    },
+  },
+  resolve: {
+    mainFields: ['module'],
+  },
+});
