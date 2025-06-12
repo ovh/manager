94ad95d49d06a531ddc4626c9c6305d1b012ad7f
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2025-06-12 09:18:06 +0200

    feat(gcj): create usePrivacyPolicy hook
    ref: #MANAGER-18595
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

---
+++ packages/manager/modules/gcj/src/types/links.type.ts
@@ -0,0 +1,12 @@
+import { OvhSubsidiary } from '@ovh-ux/manager-react-components';
+
+export type CanadianPolicyLinks = {
+  fr: string;
+  en: string;
+};
+
+export type PolicyLinks = {
+  [key in OvhSubsidiary | 'LTE']: key extends 'CA'
+    ? CanadianPolicyLinks
+    : string;
+};
