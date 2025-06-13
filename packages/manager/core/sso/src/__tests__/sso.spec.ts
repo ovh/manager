cd9f6f4df0bbc9f80c2ff3ebb00b6a1de63f3cb8
Anoop N <anoop.n@ovhcloud.com>
2025-06-13 13:34:47 +0530

    test(core.sso): add unit tests
    Set-up testing environment using common config
    Add unit tests for core.sso package
    Move __tests__ folder of url-builder inside src folder
    
    ref: #MANAGER-18634
    
    Signed-off-by: Anoop N <anoop.n@ovhcloud.com>

---
+++ packages/manager/core/sso/src/__tests__/sso.spec.ts
@@ -0,0 +1,101 @@
+import { vi, describe, beforeEach, afterEach, it, expect } from 'vitest';
+import { redirectToLoginPage, redirectToLogoutPage } from '../../src';
+
+describe('sso module', () => {
+  beforeEach(() => {
+    vi.stubGlobal('window', {
+      location: {
+        host: 'www.ovh.com',
+        href: 'https://www.ovh.com/manager/#/hub/',
+        hash: '#/hub/',
+        assign: vi.fn(),
+      },
+    });
+    vi.stubGlobal('document', {
+      referrer: '',
+    });
+  });
+
+  afterEach(() => {
+    vi.unstubAllGlobals();
+  });
+
+  describe('redirectToLoginPage', () => {
+    it('With onsuccess url', () => {
+      vi.spyOn(window.location, 'assign');
+      redirectToLoginPage('/onsuccess/url');
+      expect(window.location.assign).toHaveBeenCalledWith(
+        '/auth/?onsuccess=%2Fonsuccess%2Furl',
+      );
+    });
+
+    it('Without onsuccess url', () => {
+      vi.spyOn(window.location, 'assign');
+      redirectToLoginPage();
+      expect(window.location.assign).toHaveBeenCalledWith(
+        '/auth/?onsuccess=https%3A%2F%2Fwww.ovh.com%2Fmanager%2F%23%2Fhub%2F',
+      );
+    });
+
+    it('For telecom application', () => {
+      vi.spyOn(window.location, 'assign');
+      vi.stubGlobal('window', {
+        location: {
+          host: 'www.ovhtelecom.fr',
+          href: 'https://www.ovhtelecom.fr/manager/#/telecom/',
+          hash: '#/telecom/',
+          assign: vi.fn(),
+        },
+      });
+      redirectToLoginPage();
+      expect(window.location.assign).toHaveBeenCalledWith(
+        'https://www.ovh.com/auth/?onsuccess=https%3A%2F%2Fwww.ovhtelecom.fr%2Fmanager%2F%23%2Ftelecom%2F',
+      );
+    });
+  });
+
+  describe('redirectToLogoutPage', () => {
+    it('With onsuccess url', () => {
+      vi.spyOn(window.location, 'assign');
+      redirectToLogoutPage('/onsuccess/url');
+      expect(window.location.assign).toHaveBeenCalledWith(
+        '/auth/?action=disconnect&onsuccess=%2Fonsuccess%2Furl',
+      );
+    });
+
+    it('Without onsuccess url', () => {
+      vi.spyOn(window.location, 'assign');
+      redirectToLogoutPage();
+      expect(window.location.assign).toHaveBeenCalledWith(
+        '/auth/?action=disconnect&onsuccess=https%3A%2F%2Fwww.ovh.com%2Fmanager%2F%23%2Fhub%2F',
+      );
+    });
+
+    it('Adds from parameter when called from secondary µ-app', () => {
+      vi.stubGlobal('document', {
+        referrer: 'https://www.ovh.com/manager/',
+      });
+      vi.spyOn(window.location, 'assign');
+      redirectToLogoutPage();
+      expect(window.location.assign).toHaveBeenCalledWith(
+        '/auth/?action=disconnect&onsuccess=https%3A%2F%2Fwww.ovh.com%2Fmanager%2F%23%2Fhub%2F&from=https%3A%2F%2Fwww.ovh.com%2Fmanager%2F',
+      );
+    });
+
+    it('For telecom application', () => {
+      vi.spyOn(window.location, 'assign');
+      vi.stubGlobal('window', {
+        location: {
+          host: 'www.ovhtelecom.fr',
+          href: 'https://www.ovhtelecom.fr/manager/#/telecom/',
+          hash: '#/telecom/',
+          assign: vi.fn(),
+        },
+      });
+      redirectToLogoutPage();
+      expect(window.location.assign).toHaveBeenCalledWith(
+        'https://www.ovh.com/auth/?action=disconnect&onsuccess=https%3A%2F%2Fwww.ovhtelecom.fr%2Fmanager%2F%23%2Ftelecom%2F',
+      );
+    });
+  });
+});
