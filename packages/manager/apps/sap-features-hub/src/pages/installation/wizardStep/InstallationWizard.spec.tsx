17d88d0e57f35d33319e417351535f8556c11c48
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-06-13 18:22:44 +0200

    feat(sap): add tracking to sap-features-hub µapp
    ref: #MANAGER-17537
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/sap-features-hub/src/pages/installation/wizardStep/InstallationWizard.spec.tsx
+++ packages/manager/apps/sap-features-hub/src/pages/installation/wizardStep/InstallationWizard.spec.tsx
@@ -1,16 +1,27 @@
 import 'element-internals-polyfill';
 import '@testing-library/jest-dom';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import { describe, expect, vi, it } from 'vitest';
+import userEvent from '@testing-library/user-event';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import InstallationWizard from './InstallationWizard.page';
 import { InstallationFormContextProvider } from '@/context/InstallationForm.context';
 import { testIds } from '@/utils/testIds.constants';
 import { WIZARD_SETTINGS } from './installationWizard.constants';
+import { TRACKING } from '@/tracking.constants';
+
+const trackClickMock = vi.fn();
+vi.mock('@ovh-ux/manager-react-shell-client', async (importOriginal) => {
+  const original: typeof import('@ovh-ux/manager-react-shell-client') = await importOriginal();
+  return {
+    ...original,
+    useOvhTracking: () => ({ trackClick: trackClickMock }),
+  };
+});
 
 vi.mock('react-router-dom', () => ({
-  useNavigate: () => ({ navigate: vi.fn() }),
+  useNavigate: () => vi.fn(),
 }));
 
 const renderComponent = () =>
@@ -36,5 +47,27 @@ describe('InstallationWizard page unit test suite', () => {
     expect(upload).toHaveAttribute('accept', WIZARD_SETTINGS.accept);
     expect(upload).toHaveAttribute('max-file', `${WIZARD_SETTINGS.maxFile}`);
     expect(upload).toHaveAttribute('max-size', `${WIZARD_SETTINGS.maxSize}`);
+  });
+});
+
+describe('Tracking test suite', () => {
+  it('should track wizard launch confirmation', async () => {
+    renderComponent();
+    const user = userEvent.setup();
+    const submitCta = screen.getByTestId('primary-button');
+    await act(() => user.click(submitCta));
+    expect(trackClickMock).toHaveBeenCalledWith(
+      TRACKING.wizard.submit('confirm'),
+    );
+  });
+
+  it('should track wizard launch cancellation', async () => {
+    renderComponent();
+    const user = userEvent.setup();
+    const cancelCta = screen.getByTestId('secondary-button');
+    await act(() => user.click(cancelCta));
+    expect(trackClickMock).toHaveBeenCalledWith(
+      TRACKING.wizard.submit('cancel'),
+    );
   });
 });
