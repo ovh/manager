b31fb0eef4ca0093cc532e11455558e5da9e87b7
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2025-06-16 11:44:14 +0200

    chore(gcj): reorganize deps and tests
    ref: #MANAGER-18595
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/modules/gcj/vitest.config.js
+++ packages/manager/modules/gcj/vitest.config.js
@@ -1,29 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import {
+  sharedConfig,
+  mergeConfig,
+  createConfig,
+  defaultExcludedFiles,
+} from '@ovh-ux/manager-tests-setup';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    coverage: {
-      include: ['src'],
-      exclude: [],
-    },
-    testTimeout: 60000,
-    fileParallelism: false,
-    maxWorkers: 1,
-    pollOptions: {
-      forks: {
-        singleFork: true,
-      },
-      threads: {
-        singleThread: true,
+export default mergeConfig(
+  sharedConfig,
+  createConfig({
+    test: {
+      coverage: {
+        exclude: [...defaultExcludedFiles],
       },
     },
-  },
-  resolve: {
-    mainFields: ['module'],
-  },
-});
+  }),
+);
