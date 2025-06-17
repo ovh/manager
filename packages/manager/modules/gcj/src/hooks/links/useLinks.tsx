6829b3f37625840123657156708ec4ae1c84982e
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2025-06-17 10:50:32 +0200

    refactor(gcj): extract useLink hook to be reusable
    ref: #MANAGER-18595
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

---
+++ packages/manager/modules/gcj/src/hooks/links/useLinks.tsx
@@ -0,0 +1,8 @@
+import { OvhSubsidiary } from '@ovh-ux/manager-react-components';
+
+export const useLink = (
+  links: Partial<Record<OvhSubsidiary, string>>,
+  subsidiary: OvhSubsidiary,
+): string => {
+  return links[subsidiary] || links.DEFAULT;
+};
