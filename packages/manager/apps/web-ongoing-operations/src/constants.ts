4abbb1266a7052ebd1459ca0e9783719f376014e
Brendan Seiller <brendan.seiller@ovhcloud.com>
2025-06-17 16:32:27 +0200

    feat(web-ongoing-operations): add zod to check value on update input
    ref: #MANAGER-18199
    
    Signed-off-by: Brendan Seiller <brendan.seiller@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/apps/web-ongoing-operations/src/constants.ts
+++ packages/manager/apps/web-ongoing-operations/src/constants.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod';
+
 export const taskMeDomain = ['me', 'task', 'domain'];
 export const taskMeDns = ['me', 'task', 'dns'];
 
@@ -51,3 +53,25 @@ export const DNS_OPERATIONS = [
   'ZoneImport',
   'ZoneRestore',
 ];
+export const editableArgument: Record<string, z.ZodString> = {
+  authInfo: z
+    .string()
+    .min(5)
+    .max(50),
+  legitimacyAfnic: z.string(),
+  processId: z
+    .string()
+    .min(5)
+    .max(20),
+  NIF: z.string().min(5),
+  ownerLegalName: z.string(),
+  identificationNumber: z.string().min(5),
+  firstname: z.string(),
+  name: z.string(),
+  action: z.string(),
+  memberContactXXX: z.string(),
+  ukLegalForm: z.string(),
+  UIN: z.string(),
+  validationToken: z.string(),
+  default: z.string(),
+};
