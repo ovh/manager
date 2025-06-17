9cbffd59f9763a6604c06d3e7596dd0b1f9dde9f
Lionel Bueno <lionel.bueno.ext@ovhcloud.com>
2025-06-17 16:56:06 +0200

    feat(load-balancer): improve error quota message
    ref: #TAPC-4099
    
    Signed-off-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>

--- packages/manager/apps/pci-load-balancer/src/pages/create/hooks/useCreateCallbacks.tsx
+++ packages/manager/apps/pci-load-balancer/src/pages/create/hooks/useCreateCallbacks.tsx
@@ -1,14 +1,22 @@
-import { Translation } from 'react-i18next';
-import { ApiError } from '@ovh-ux/manager-core-api';
-import { useNotifications } from '@ovh-ux/manager-react-components';
+import { Trans, Translation } from 'react-i18next';
+import {
+  useNotifications,
+  useProjectUrl,
+} from '@ovh-ux/manager-react-components';
 import { useNavigate, useParams } from 'react-router-dom';
 import { LOAD_BALANCER_CREATION_TRACKING } from '@/constants';
 import queryClient from '@/queryClient';
 import { getAllLoadBalancersQueryKey } from '@/api/hook/useLoadBalancer';
 import { useTracking } from '@/pages/create/hooks/useTracking';
+import GuideLink from '@/components/GuideLink/GuideLink.component';
+import {
+  LoadBalancerErrorClass,
+  TCreateLoadBalancerError,
+} from '@/api/data/load-balancer';
 
 export const useCreateCallbacks = () => {
   const navigate = useNavigate();
+  const hrefProject = useProjectUrl('public-cloud');
   const { trackPage } = useTracking();
   const { addError, addInfo } = useNotifications();
   const { projectId } = useParams();
@@ -29,30 +37,43 @@ export const useCreateCallbacks = () => {
         queryKey: getAllLoadBalancersQueryKey(projectId),
       });
     },
-    onError: (error: ApiError) => {
+    onError: (error: TCreateLoadBalancerError) => {
       trackPage({
         name: LOAD_BALANCER_CREATION_TRACKING.ERROR,
         type: 'navigation',
       });
-      addError(
-        <Translation ns="load-balancer">
-          {(_t) => (
-            <span
-              dangerouslySetInnerHTML={{
-                __html: _t('octavia_load_balancer_global_error', {
-                  message: ((error.response as unknown) as {
-                    data: { message: string };
-                  }).data.message,
-                  requestId: ((error.response as unknown) as {
-                    headers: Record<string, unknown>;
-                  }).headers['x-ovh-queryid'],
-                }),
-              }}
-            ></span>
-          )}
-        </Translation>,
-        false,
-      );
+
+      if (
+        error.response.data.class.includes(
+          LoadBalancerErrorClass.MAX_QUOTA_REACHED,
+        )
+      ) {
+        addError(
+          <Trans
+            ns="load-balancer"
+            i18nKey="octavia_load_balancer_quota_creation_error"
+            components={{
+              Link: <GuideLink href={`${hrefProject}/quota`} />,
+            }}
+          />,
+        );
+      } else {
+        addError(
+          <Translation ns="load-balancer">
+            {(_t) => (
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: _t('octavia_load_balancer_global_error', {
+                    message: error.response.data.message,
+                    requestId: error.response.headers['x-ovh-queryid'],
+                  }),
+                }}
+              ></span>
+            )}
+          </Translation>,
+          false,
+        );
+      }
     },
   };
 };
