a3764e86844d3177d6d41525ecda9caecc53e7f2
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-06-18 09:18:30 +0200

    feat(kms): secrets listing page
    ref: #MANAGER-18246
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

---
+++ packages/manager/apps/key-management-service/src/modules/secret-manager/pages/listing/Listing.page.spec.tsx
@@ -0,0 +1,60 @@
+import { screen, act } from '@testing-library/react';
+import { SECRET_MANAGER_ROUTES_URLS } from '@secret-manager/routes/routes.constants';
+import {
+  assertTextVisibility,
+  getOdsButtonByLabel,
+} from '@ovh-ux/manager-core-test-utils';
+import { secretsMock } from '@secret-manager/mocks/secrets/secrets.mock';
+import userEvent from '@testing-library/user-event';
+import { renderTestApp } from '@/utils/tests/renderTestApp';
+import { labels } from '@/utils/tests/init.i18n';
+
+const mockOkmsId = '12345';
+const mockPageUrl = SECRET_MANAGER_ROUTES_URLS.secretListing(mockOkmsId);
+
+const renderPage = async () => {
+  const { container } = await renderTestApp(mockPageUrl);
+
+  // Check title
+  expect(
+    await screen.findByText(labels.secretManager.common.secret_manager),
+  ).toBeVisible();
+
+  return { container };
+};
+
+describe('Secrets listing test suite', () => {
+  it('should display the secrets listing page', async () => {
+    await renderPage();
+  });
+
+  it('should display the listing table with all columns', async () => {
+    await renderPage();
+
+    const tableHeaders = [
+      labels.secretManager.common.path,
+      labels.secretManager.common.version,
+      labels.common.dashboard.creation_date,
+    ];
+
+    tableHeaders.forEach((header) => {
+      expect(screen.queryAllByText(header)).toHaveLength(1);
+    });
+  });
+
+  it('should navigate to a secret detail page on click on secret path', async () => {
+    const user = userEvent.setup();
+    const { container } = await renderPage();
+
+    const secretPageLink = await getOdsButtonByLabel({
+      container,
+      label: secretsMock[0].path,
+      isLink: true,
+    });
+
+    await act(() => user.click(secretPageLink));
+
+    // TODO: Update the text when the detail page is implemented
+    await assertTextVisibility('Secret Detail');
+  });
+});
