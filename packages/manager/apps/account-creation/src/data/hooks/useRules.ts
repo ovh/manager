16461db49bfa3e3ef66ea3240d58181fae36a49e
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2025-06-18 11:15:43 +0200

    chore(account-creation): use common types
    ref: #MANAGER-17333
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/apps/account-creation/src/data/hooks/useRules.ts
+++ packages/manager/apps/account-creation/src/data/hooks/useRules.ts
@@ -1,9 +1,11 @@
+import { Country, UserLocales } from '@ovh-ux/manager-config';
 import { useQuery } from '@tanstack/react-query';
 import { getRules } from '@/data/api/rules';
-import { Country } from '@/types/country';
-import { Language } from '@/types/language';
 
-export const useRules = (country: Country | null, language: Language | null) =>
+export const useRules = (
+  country: Country | null,
+  language: UserLocales | null,
+) =>
   useQuery({
     queryKey: [`newAccount/rules`],
     queryFn: () => getRules({ country, language }),
