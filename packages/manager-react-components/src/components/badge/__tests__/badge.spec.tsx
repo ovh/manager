b1d6f81e990c93b5a0168d39a63fea7e4fc9a055
Anoop N <anoop.n@ovhcloud.com>
2025-06-19 11:29:42 +0530

    feat(manager-react-components): migrate Badge to ODS v19
    Migrate Badge component to ODSv19
    Align Badge with MRC component guidelines
    
    ref: #MANAGER-18612
    
    Signed-off-by: Anoop N <anoop.n@ovhcloud.com>

--- packages/manager-react-components/src/components/badge/__tests__/badge.spec.tsx
+++ packages/manager-react-components/src/components/badge/__tests__/badge.spec.tsx
@@ -1,32 +1,28 @@
 import { describe, it, expect } from 'vitest';
 import { waitFor, screen } from '@testing-library/react';
-import {
-  ODS_BADGE_COLOR,
-  ODS_BADGE_SIZE,
-  ODS_ICON_NAME,
-} from '@ovhcloud/ods-components';
-import { render } from '../../utils/test.provider';
-import { Badge } from './badge.component';
+import { BADGE_COLOR, BADGE_SIZE } from '@ovhcloud/ods-react';
+import { render } from '../../../utils/test.provider';
+import { Badge } from '../badge.component';
 
 describe('Badge component', () => {
   it('should render badge with correct props', async () => {
     render(
       <Badge
-        label="active"
-        color={ODS_BADGE_COLOR.information}
-        size={ODS_BADGE_SIZE.md}
-        icon={ODS_ICON_NAME.circleInfo}
+        color={BADGE_COLOR.information}
+        size={BADGE_SIZE.md}
         data-testid="test"
-      />,
+      >
+        Active
+      </Badge>,
     );
     await waitFor(() => {
       const component = screen.getByTestId('test');
-      expect(component.hasAttribute(ODS_BADGE_COLOR.information));
+      expect(component.hasAttribute(BADGE_COLOR.information));
       expect(component.hasAttribute('active'));
     });
   });
   it('should render OdsSkeleton when isLoading is true', async () => {
-    render(<Badge isLoading label="" data-testid="skeleton" />);
+    render(<Badge isLoading data-testid="skeleton" />);
     await waitFor(() => {
       const skeleton = screen.getByTestId('skeleton');
       expect(skeleton).toBeInTheDocument();
