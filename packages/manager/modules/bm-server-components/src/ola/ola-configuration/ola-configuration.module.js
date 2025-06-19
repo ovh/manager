6ba86bb2ad6cba6d454a479d970ef4ae65d74624
aderghamov <alexandre.dergham.ext@ovhcloud.com>
2025-06-19 16:18:37 +0200

    feat(ola-nics): reworking the ola configuration component
    ref: #MANAGER-17069
    
    Signed-off-by: aderghamov <alexandre.dergham.ext@ovhcloud.com>

--- packages/manager/modules/bm-server-components/src/ola/ola-configuration/ola-configuration.module.js
+++ packages/manager/modules/bm-server-components/src/ola/ola-configuration/ola-configuration.module.js
@@ -1,9 +1,10 @@
 import component from './ola-configuration.component';
+import olaConfigurationTable from './ola-configuration-table/ola-configuration-table.module';
 
 const moduleName = 'ovhManagerDedicatedServerInterfacesOlaConfiguration';
 
 angular
-  .module(moduleName, [])
+  .module(moduleName, [olaConfigurationTable])
   .component('dedicatedServerInterfacesOlaConfiguration', component)
   .run(/* @ngTranslationsInject:json ./translations */);
 
