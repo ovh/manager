b11bc5074bcdaa8f2b6a67a096240f7577ce0b3b
Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>
2025-06-19 08:27:15 +0200

    feat(vmware-vsphere): implement logs forwarder onboarding
    ref: #MANAGER-17708
    
    Signed-off-by: Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>

--- packages/manager/apps/hpc-vmware-vsphere/src/hooks/guide/useGuideUtils.tsx
+++ packages/manager/apps/hpc-vmware-vsphere/src/hooks/guide/useGuideUtils.tsx
@@ -6,17 +6,18 @@ import { GUIDE_LIST, SUPPORT_URL } from './guidesLinks.constants';
 type GetGuideLinkProps = {
   name?: string;
   subsidiary: CountryCode | string;
+  baseUrl?: Record<string, string>;
 };
 
-function getGuideListLink({ subsidiary }: GetGuideLinkProps) {
+function getGuideListLink({ subsidiary, baseUrl }: GetGuideLinkProps) {
   const list: { [guideName: string]: string } = {};
   const keys = Object.entries(GUIDE_LIST);
   keys.forEach((key) => {
     const guide =
       GUIDE_LIST[key[0]][subsidiary as CountryCode] || GUIDE_LIST[key[0]].GB;
 
-    const helpCenterUrl = SUPPORT_URL[subsidiary] ?? SUPPORT_URL.EU;
-    list[key[0]] = `${helpCenterUrl}${guide}`;
+    const url = baseUrl[subsidiary] ?? baseUrl.EU;
+    list[key[0]] = `${url}${guide}`;
   });
   return list;
 }
@@ -25,7 +26,7 @@ interface GuideLinkProps {
   [guideName: string]: string;
 }
 
-function useGuideUtils() {
+function useGuideUtils(baseUrl = SUPPORT_URL) {
   const { shell } = useContext(ShellContext);
   const { environment } = shell;
   const [list, setList] = useState({});
@@ -34,7 +35,10 @@ function useGuideUtils() {
     const getSubSidiary = async () => {
       const env = await environment.getEnvironment();
       const { ovhSubsidiary } = env.getUser();
-      const guideList = getGuideListLink({ subsidiary: ovhSubsidiary });
+      const guideList = getGuideListLink({
+        subsidiary: ovhSubsidiary,
+        baseUrl,
+      });
       setList(guideList);
     };
     getSubSidiary();
