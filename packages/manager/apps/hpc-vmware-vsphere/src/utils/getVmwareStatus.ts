01e77c0384c2863b92af03807a2277fc158537f8
Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>
2025-06-19 08:27:15 +0200

    feat(hpc-vmware-vsphere): implement logs forwarder onboarding
    ref: #MANAGER-17708
    
    Signed-off-by: Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>

---
+++ packages/manager/apps/hpc-vmware-vsphere/src/utils/getVmwareStatus.ts
@@ -0,0 +1,27 @@
+import { VMWareState, VMWareStatus } from '@/types/vsphere';
+import { getCommercialType } from './getCommercialType';
+import { CommercialName } from '@/types/datacenter';
+
+type GetVmwareStatusProps = {
+  vsphereState: VMWareState;
+  datacenterCommentialName: CommercialName;
+};
+
+export function getVmwareStatus({
+  vsphereState,
+  datacenterCommentialName,
+}: GetVmwareStatusProps) {
+  if (vsphereState === VMWareState.Migrating) {
+    return VMWareStatus.MIGRATING;
+  }
+
+  if (getCommercialType(datacenterCommentialName) === 'ESSENTIALS') {
+    return VMWareStatus.ESSENTIALS;
+  }
+
+  if (getCommercialType(datacenterCommentialName) === 'PREMIER') {
+    return VMWareStatus.PREMIER;
+  }
+
+  return undefined;
+}
