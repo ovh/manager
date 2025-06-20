4ebcd34c6694b0d882d199fcfe02cff24859427b
helabenkhalfallah <hela.ben-khalfallah@corp.ovh.com>
2025-06-20 12:55:17 +0200

    feat(mrc): adding and adapting ods react 19
    ref: MANAGER-18612
    
    Signed-off-by: helabenkhalfallah <hela.ben-khalfallah@corp.ovh.com>

---
+++ packages/manager-react-components/types/ovhcloud-ods-react.d.ts
@@ -0,0 +1,6 @@
+declare module '@ovhcloud/ods-react' {
+  export * from '@ovhcloud/ods-react/dist/components/button/src';
+  export * from '@ovhcloud/ods-react/dist/components/badge/src';
+  export * from '@ovhcloud/ods-react/dist/components/card/src';
+  // etc.
+}
