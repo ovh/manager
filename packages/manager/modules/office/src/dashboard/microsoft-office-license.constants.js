9cb4d06acc7922112d2b06e55b66708e52536fb5
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2025-06-20 09:52:30 +0200

    fix(web): clean angularjs office code
    ref: #MANAGER-18620
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/modules/office/src/dashboard/microsoft-office-license.constants.js
+++
@@ -1,8 +0,0 @@
-export const PREPAID_BASE_URL = 'apiv6/license/officePrepaid';
-
-export const POSTPAID_BASE_URL = 'apiv6/license/office';
-
-export default {
-  PREPAID_BASE_URL,
-  POSTPAID_BASE_URL,
-};
