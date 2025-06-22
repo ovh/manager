c36e2e7e118bfdf74a09f7a3f3a06797016472bf
ahmed sefiani <ahmed.sefiani.ext@corp.ovh.com>
2025-06-22 23:47:36 +0200

    feat(veeam-backup): enable offer gold in veeam backup
    ref: #MANAGER-17138
    
    Signed-off-by: ahmed sefiani <ahmed.sefiani.ext@corp.ovh.com>

---
+++ packages/manager/apps/veeam-backup/src/pages/dashboard/BillingTile.component.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { DashboardTile } from '@ovh-ux/manager-react-components';
+import { VeeamBackup } from '@ovh-ux/manager-module-vcd-api';
+import { BillingLink } from '@/components/Links/BillingLink.component';
+
+import { ConsumedVms } from './ConsumedVms.component';
+import { OfferProgress } from './OfferProgress.component';
+import { DeactivatedOfferGold } from './DeactivatedOfferGold.component';
+
+type BillingTileProps = {
+  id: string;
+  backup: VeeamBackup;
+};
+
+export const BillingTile: React.FC<BillingTileProps> = ({ id, backup }) => {
+  const { t } = useTranslation('dashboard');
+
+  return (
+    <DashboardTile
+      title={t('billing')}
+      items={[
+        {
+          id: 'consumedVms',
+          label: t('consumed_vms'),
+          value: <ConsumedVms id={id} backup={backup} />,
+        },
+        ...(backup?.currentState?.offers?.map((offer) => ({
+          id: offer.name,
+          label: `${offer.name.at(0).toUpperCase()}${offer.name
+            .substring(1)
+            .toLowerCase()}`,
+          value: <OfferProgress offer={offer} id={id} />,
+        })) || []),
+        backup?.currentState.offers.every((offer) => offer.name !== 'GOLD') && {
+          id: 'gold',
+          label: 'Gold',
+          value: <DeactivatedOfferGold />,
+        },
+        {
+          id: 'bilingModalities',
+          value: <BillingLink />,
+        },
+      ].filter(Boolean)}
+    />
+  );
+};
