26e7b17fa4e445e5eaa23c4c08eb184391544c13
Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>
2025-06-23 11:00:41 +0200

    feat(hub): add datagrids with github data into roadmap changelog
    ref: #MANAGER-18687
    
    Signed-off-by: Maxime Bajeux <maxime.bajeux.ext@corp.ovh.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

---
+++ packages/manager/apps/hub/src/components/roadmap-changelog-datagrid/cells/RoadmapChangelogItemReleaseDateCell.tsx
@@ -0,0 +1,21 @@
+import { DataGridTextCell } from '@ovh-ux/manager-react-components';
+import { useTranslation } from 'react-i18next';
+import { RoadmapChangelogItem } from '@/types/roadmapchangelog.type';
+
+export const RoadmapChangelogItemReleaseDateCell = ({
+  item,
+}: {
+  item: RoadmapChangelogItem;
+}) => {
+  const { i18n } = useTranslation('changelog');
+  const locale = i18n?.language?.replace('_', '-');
+  return (
+    <DataGridTextCell>
+      {item.releaseDate
+        ? new Date(item.releaseDate).toLocaleDateString(locale, {
+            dateStyle: 'medium',
+          })
+        : ''}
+    </DataGridTextCell>
+  );
+};
