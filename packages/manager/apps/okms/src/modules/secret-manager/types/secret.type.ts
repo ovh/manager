0c388316429d26a48b4b67b4fcfa42832e36342f
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-06-24 11:25:48 +0200

    feat(okms): add secret api types
    ref: #MANAGER-18248
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

--- packages/manager/apps/okms/src/modules/secret-manager/types/secret.type.ts
+++ packages/manager/apps/okms/src/modules/secret-manager/types/secret.type.ts
@@ -1,22 +1,40 @@
 import { IAM } from '@/types/okms.type';
 
+/**
+ * SECRET
+ */
+export type SecretState = 'ACTIVE' | 'DEACTIVATED' | 'DELETED';
+
+export type SecretData = any;
+
+export type SecretVersion = {
+  createdAt: string;
+  data?: SecretData;
+  deactivatedAt?: string;
+  id: number;
+  state: SecretState;
+};
+
+export type SecretMetadata = {
+  casRequired?: boolean;
+  createdAt: string;
+  currentVersion: number;
+  customMetadata?: Record<string, string>;
+  deactivateVersionAfter?: string;
+  maxVersions?: number;
+  oldestVersion: number;
+  updatedAt?: string;
+};
+
 export type Secret = {
   path: string;
-  version: {
-    id: number;
-    createdAt: string;
-    deactivatedAt: string;
-    state: string;
-  };
-  metadata: {
-    casRequired: boolean;
-    createdAt: string;
-    updatedAt: string;
-    currentVersion: number;
-    customMetadata: Record<string, string>;
-    deactivateVersionAfter: string;
-    oldestVersion: number;
-    maxVersions: number;
-  };
+  version: SecretVersion;
+  metadata: SecretMetadata;
   iam: IAM;
+};
+
+export type SecretConfig = {
+  casRequired: boolean;
+  deactivateVersionAfter?: string;
+  maxVersions?: number;
 };
