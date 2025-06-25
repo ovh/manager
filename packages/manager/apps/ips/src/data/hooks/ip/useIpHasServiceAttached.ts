97faa3224d6ae24b6b28cf7958e74410f4a142c0
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2025-06-25 21:20:58 +0530

    feat(ips.listing): add configure edge network firewall
    ref: #MANAGER-18406
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

---
+++ packages/manager/apps/ips/src/data/hooks/ip/useIpHasServiceAttached.ts
@@ -0,0 +1,25 @@
+import { useGetProductService } from '../useGetProductService';
+
+export type UseIpHasServicesAttachedParams = {
+  path: string;
+  category: string;
+  serviceName?: string;
+};
+
+export const useIpHasServicesAttached = ({
+  path,
+  category,
+  serviceName,
+}: UseIpHasServicesAttachedParams) => {
+  const productServices = useGetProductService({
+    path,
+    category,
+  });
+
+  return {
+    hasServiceAttached:
+      productServices?.services?.some(
+        (service) => serviceName === service.serviceName,
+      ) ?? false,
+  };
+};
