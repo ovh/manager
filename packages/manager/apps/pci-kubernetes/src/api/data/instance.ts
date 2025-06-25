3d9de25811d08c69fd6164bc944b1d5757408c13
Lauren Olivier <laurenzolivier@gmail.com>
2025-06-25 16:55:36 +0200

    feat(pci-kubernetes): update cluster plan value
    ref: #TAPC-4418, #TAPC-4419
    
    Signed-off-by: Lauren Olivier <laurenzolivier@gmail.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/apps/pci-kubernetes/src/api/data/instance.ts
+++ packages/manager/apps/pci-kubernetes/src/api/data/instance.ts
@@ -1,17 +1,5 @@
 import { v6 } from '@ovh-ux/manager-core-api';
 
-type TRawInstance = {
-  id: string;
-  monthlyBilling?: { status: string };
-  flavorId: string;
-  imageId: string;
-  name: string;
-  planCode: string;
-  region: string;
-  sshKeyId: string;
-  status: string;
-};
-
 export const switchToMonthlyBilling = async (
   projectId: string,
   instanceId: string,
