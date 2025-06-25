8e895381289ce2f0050a1c4c0e613da30ac57948
Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
2025-06-25 11:09:42 +0200

    feat(account-creation): migrate to new types from manager-config
    Minor cleanup, fix svg ref and type refs
    
    ref: #MANAGER-17337
    
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>

--- packages/manager/apps/account-creation/src/context/user/user.provider.tsx
+++ packages/manager/apps/account-creation/src/context/user/user.provider.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { LegalForm } from '@ovh-ux/manager-config';
 import userContext from '@/context/user/user.context';
 import { useMe } from '@/data/hooks/useMe';
 import { urls } from '@/routes/routes.constant';
-import { LegalFrom } from '@/types/legalForm';
 
 type Props = {
   children: JSX.Element | JSX.Element[];
@@ -13,14 +13,14 @@ export const UserProvider = ({ children = [] }: Props): JSX.Element => {
   const navigate = useNavigate();
   const { data: me, isFetched, error } = useMe({ retry: 0 });
   // We will need to add states for country and language to prefill the /info form
-  const [legalForm, setLegalForm] = useState<LegalFrom | undefined>(undefined);
+  const [legalForm, setLegalForm] = useState<LegalForm | undefined>(undefined);
 
   useEffect(() => {
     if (isFetched) {
       if (error?.status === 401) {
         navigate(urls.preferences);
       } else {
-        setLegalForm(me?.legalform as LegalFrom);
+        setLegalForm(me?.legalform as LegalForm);
         navigate(urls.accountType);
       }
     }
