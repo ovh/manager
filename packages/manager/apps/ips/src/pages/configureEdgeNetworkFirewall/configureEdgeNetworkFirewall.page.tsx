afe15999c857e6aec505ce71b61ed64a0ff07dcb
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2025-06-25 21:20:58 +0530

    feat(ips.listing): add configure edge network firewall
    ref: #MANAGER-18406
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

---
+++ packages/manager/apps/ips/src/pages/configureEdgeNetworkFirewall/configureEdgeNetworkFirewall.page.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function ConfigureEdgeNetworkFirewall() {
+  // To be done in another JIRA
+  return <div>ConfigureEdgeNetworkFirewallPage - todo</div>;
+}
