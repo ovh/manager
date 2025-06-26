f2e39d35d238b40e962ae17596a41b190ff14e0e
Anthony Da Mota <anthony.da-mota@ovhcloud.com>
2025-06-26 15:42:46 +0200

    feat(pci-databases-analytics): add Terraform
    ref: #DATATR-2188
    
    Signed-off-by: Anthony Da Mota <anthony.da-mota@ovhcloud.com>

---
+++ packages/manager/apps/pci-databases-analytics/src/configuration/documentation.ts
@@ -0,0 +1,25 @@
+export const LINKS = {
+  DB_ORDER_EQ_API: {
+    fr_FR: 'https://docs.ovh.com/fr/api/api-premiers-pas/',
+    fr_CA: 'https://docs.ovh.com/fr/api/api-premiers-pas/',
+    default: 'https://docs.ovh.com/gb/en/customer/first-steps-with-ovh-api/',
+  },
+  DB_ORDER_EQ_TERRAFORM: {
+    fr_FR: 'https://docs.ovh.com/fr/publiccloud/databases/order-terraform/',
+    fr_CA: 'https://docs.ovh.com/fr/publiccloud/databases/order-terraform/',
+    default:
+      'https://docs.ovh.com/gb/en/publiccloud/databases/order-terraform/',
+  },
+};
+
+export const DATABASE_CREATION_GUIDES =
+  'https://github.com/ovh/public-cloud-databases-examples';
+
+export function getDocumentationUrl(
+  documentation: typeof LINKS[keyof typeof LINKS],
+  locale: string,
+): string {
+  return (
+    documentation[locale as keyof typeof documentation] || documentation.default
+  );
+}
