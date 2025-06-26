10ac28140e3a50008bd86734432828afb77d4705
Anthony Da Mota <anthony.da-mota@ovhcloud.com>
2025-06-26 15:42:46 +0200

    feat(pci-databases-analytics): add Terraform
    ref: #DATATR-2188
    
    Signed-off-by: Anthony Da Mota <anthony.da-mota@ovhcloud.com>

---
+++ packages/manager/apps/pci-databases-analytics/src/types/terraform/cloud/database/PathParams.ts
@@ -0,0 +1,7 @@
+/** Path Parameters with product ID and Service Name */
+export interface PathParams {
+  /** Cluster ID. */
+  productID: string;
+  /** The id of the public cloud project. */
+  serviceName: string;
+}
---
+++ packages/manager/apps/pci-databases-analytics/src/types/terraform/cloud/database/m3db/User.ts
@@ -0,0 +1,7 @@
+/** M3Db user definition */
+export interface User {
+  /** Group of the user. */
+  group: string;
+  /** Name of the user. */
+  name: string;
+}
