9936f1c05480802aebe23a9e096d7b8879b0ee20
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2025-06-30 09:24:48 +0200

    feat(gcj): created data usage policy modal
    ref: #MANAGER-17746
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/modules/gcj/src/constants/constants.ts
+++ packages/manager/modules/gcj/src/constants/constants.ts
@@ -1,3 +1,5 @@
+import { OvhSubsidiary } from '@ovh-ux/manager-react-components';
+import { Region } from '@ovh-ux/manager-config';
 import { PolicyLinks } from '../types/links.type';
 
 export const LegalPolicyLinkByLanguage: PolicyLinks = {
@@ -31,4 +33,37 @@ export const LegalPolicyLinkByLanguage: PolicyLinks = {
   US: 'https://us.ovhcloud.com/legal/privacy-policy/',
   WE: 'https://www.ovhcloud.com/en-ca/terms-and-conditions/privacy-policy/',
   WS: 'https://www.ovhcloud.com/en-ca/terms-and-conditions/privacy-policy/',
+};
+
+export const DEFAULT_SUB_BY_REGION: Record<Region, OvhSubsidiary> = {
+  EU: OvhSubsidiary.IE,
+  CA: OvhSubsidiary.CA,
+  US: OvhSubsidiary.US,
+};
+
+export const MORE_INFO_LINK = {
+  FR:
+    'https://www.ovh.com/fr/support/documents_legaux/politique_cookies_ovh.xml',
+  GB: 'https://www.ovh.co.uk/support/termsofservice/cookies_ovh.xml',
+  IE: 'https://www.ovh.ie/support/termsofservice/cookies_ovh.xml',
+  DE: 'https://www.ovh.de/support/agb/cookies_ovh.xml',
+  ES: 'https://www.ovh.es/soporte/documentos_legales/cookies_ovh.xml',
+  IT: 'https://www.ovh.it/supporto/documenti_legali/cookies_ovh.xml',
+  NL: 'https://www.ovh.nl/support/algemene_voorwaarden/cookies_ovh.xml',
+  PL: 'https://www.ovh.pl/pomoc/dokumenty/cookies_ovh.xml',
+  PT: 'https://www.ovh.pt/suporte/documentos_legais/cookies_ovh.xml',
+  SN: 'https://www.ovh.sn/support/documents_legaux/politique_cookies_ovh.xml',
+  TN:
+    'https://www.ovh.com/tn/support/documents_legaux/politique_cookies_ovh.xml',
+  MA:
+    'https://www.ovh.com/ma/support/documents_legaux/politique_cookies_ovh.xml',
+  QC: 'https://www.ovh.com/ca/fr/support/documents_legaux/cookies_ovh.xml',
+  CA: 'https://www.ovh.com/ca/en/support/termsofservice/cookies_ovh.xml',
+  WE: 'https://www.ovh.com/world/support/termsofservice/cookies_ovh.xml',
+  WS: 'https://www.ovh.com/world/es/soporte/documentos_legales/cookies_ovh.xml',
+  AU: 'https://www.ovh.com.au/support/termsofservice/cookies_ovh.xml',
+  SG: 'https://www.ovh.com/sg/support/termsofservice/cookies_ovh.xml',
+  ASIA: 'https://www.ovh.com/asia/support/termsofservice/cookies_ovh.xml',
+  IN: 'https://www.ovhcloud.com/en-in/support/termsofservice/cookies_ovh.xml',
+  DEFAULT: 'https://www.ovh.ie/support/termsofservice/cookies_ovh.xml',
 };
