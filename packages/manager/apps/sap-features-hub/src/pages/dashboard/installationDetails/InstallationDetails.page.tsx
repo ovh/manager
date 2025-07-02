6b9a40ca6d64958b58b8994992941eb49b3f3a06
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-07-02 12:02:49 +0200

    feat(sap-features-hub): add unmocking on installation report page
    ref: #MANAGER-18621
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/sap-features-hub/src/pages/dashboard/installationDetails/InstallationDetails.page.tsx
+++ packages/manager/apps/sap-features-hub/src/pages/dashboard/installationDetails/InstallationDetails.page.tsx
@@ -1,33 +1,29 @@
 import { useHref, useSearchParams } from 'react-router-dom';
-import { BaseLayout } from '@ovh-ux/manager-react-components';
+import { BaseLayout, HeadersProps } from '@ovh-ux/manager-react-components';
 import React, { Suspense } from 'react';
-
 import { useTranslation } from 'react-i18next';
 import { useQuery } from '@tanstack/react-query';
-import { OdsText } from '@ovhcloud/ods-components/react';
 import Breadcrumb from '@/components/Breadcrumb/Breadcrumb';
 import { InstallationDetailsSummary } from '@/components/InstallationDetails/InstallationDetailsSummary.component';
 import { InstallationDetailsProgress } from '@/components/InstallationDetails/InstallationDetailsProgress.component';
 import { InstallationDetailsError } from '@/components/InstallationDetails/InstallationDetailsError.component';
-import { useMockInstallationTaskDetailsOptions } from '@/hooks/installationDetails/useInstallationDetails';
+import { useInstallationTaskDetailsOptions } from '@/hooks/installationDetails/useInstallationDetails';
 import { SAPInstallationStatus } from '@/types/installation.type';
 import { urls } from '@/routes/routes.constant';
 
 const REFECTH_INTERVAL_INSTALLATION_STATUS = 30_000;
 
 export default function InstallationDetailsPage() {
+  const { t } = useTranslation('dashboard/installation');
+  const hrefPrevious = useHref(urls.listing);
   const [searchParams] = useSearchParams();
+
   const serviceName = searchParams.get('serviceName');
   const taskId = searchParams.get('taskId');
-  const hrefPrevious = useHref(urls.listing);
-
-  const { t } = useTranslation('dashboard/installation');
-  const header = {
-    title: t('dashboard_installation_title'),
-  };
+  const header: HeadersProps = { title: t('dashboard_installation_title') };
 
   useQuery({
-    ...useMockInstallationTaskDetailsOptions({
+    ...useInstallationTaskDetailsOptions({
       serviceName,
       taskId,
     }),
@@ -52,11 +48,9 @@ export default function InstallationDetailsPage() {
         header={header}
         backLinkLabel={t('dashboard_installation_go_back_to_list')}
         hrefPrevious={hrefPrevious}
+        subtitle={t('dashboard_installation_description')}
       >
         <div className="flex flex-col gap-8">
-          <OdsText preset="heading-2">
-            {t('dashboard_installation_description')}
-          </OdsText>
           <InstallationDetailsError serviceName={serviceName} taskId={taskId} />
           <InstallationDetailsSummary
             serviceName={serviceName}
