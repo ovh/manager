17a05a2bb10de482af21eabfc415456c7ed638e1
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-07-02 11:39:25 +0200

    feat(sap-features-hub): add unmocking on installations listing page
    ref: #MANAGER-18621
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/sap-features-hub/src/components/Datagrid/DatagridHistoryCells.component.tsx
+++ packages/manager/apps/sap-features-hub/src/components/Datagrid/DatagridHistoryCells.component.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { DataGridTextCell } from '@ovh-ux/manager-react-components';
-import { ODS_ICON_NAME, ODS_TEXT_PRESET } from '@ovhcloud/ods-components';
-import { OdsIcon, OdsText, OdsTooltip } from '@ovhcloud/ods-components/react';
+import { ODS_TEXT_PRESET } from '@ovhcloud/ods-components';
+import { OdsButton, OdsText, OdsTooltip } from '@ovhcloud/ods-components/react';
 import { useTranslation } from 'react-i18next';
-import { TSAPInstallation } from '@/types/installation.type';
+import { useNavigate } from 'react-router-dom';
+import { TSAPInstallationWithService } from '@/types/installation.type';
 import { InstallationStatus } from '../InstallationStatus/InstallationStatus.component';
 import { useFormattedDate } from '@/hooks/useFormattedDate';
+import { buildViewInstallationRedirectUrl } from '@/utils/buildSearchQuery';
 
-export const InstallationDateCell = (installation: TSAPInstallation) => {
+export const InstallationDateCell = (
+  installation: TSAPInstallationWithService,
+) => {
   const date = new Date(Date.parse(installation?.startTime));
 
   const datetime = useFormattedDate({
@@ -24,42 +28,56 @@ export const InstallationDateCell = (installation: TSAPInstallation) => {
   return <DataGridTextCell>{datetime}</DataGridTextCell>;
 };
 
-export const VMwareServiceCell = (installation: TSAPInstallation) => (
-  <DataGridTextCell>{installation?.serviceName}</DataGridTextCell>
-);
+export const VMwareServiceCell = (
+  installation: TSAPInstallationWithService,
+) => <DataGridTextCell>{installation?.serviceName}</DataGridTextCell>;
 
-export const SAPSIDCell = (installation: TSAPInstallation) => (
+export const SAPSIDCell = (installation: TSAPInstallationWithService) => (
   <DataGridTextCell>{installation?.sapSid}</DataGridTextCell>
 );
 
-export const SAPHANASIDCell = (installation: TSAPInstallation) => (
+export const SAPHANASIDCell = (installation: TSAPInstallationWithService) => (
   <DataGridTextCell>{installation?.sapHanaSid}</DataGridTextCell>
 );
 
-export const ApplicationVersionCell = (installation: TSAPInstallation) => (
-  <DataGridTextCell>{installation?.applicationVersion}</DataGridTextCell>
-);
+export const ApplicationVersionCell = (
+  installation: TSAPInstallationWithService,
+) => <DataGridTextCell>{installation?.applicationVersion}</DataGridTextCell>;
 
-export const ApplicationTypeCell = (installation: TSAPInstallation) => (
-  <DataGridTextCell>{installation?.applicationType}</DataGridTextCell>
-);
+export const ApplicationTypeCell = (
+  installation: TSAPInstallationWithService,
+) => <DataGridTextCell>{installation?.applicationType}</DataGridTextCell>;
 
-export const DeploymentTypeCell = (installation: TSAPInstallation) => (
-  <DataGridTextCell>{installation?.deploymentType}</DataGridTextCell>
-);
+export const DeploymentTypeCell = (
+  installation: TSAPInstallationWithService,
+) => <DataGridTextCell>{installation?.deploymentType}</DataGridTextCell>;
 
-export const StatusCell = (installation: TSAPInstallation) => (
+export const StatusCell = (installation: TSAPInstallationWithService) => (
   <InstallationStatus status={installation?.status} />
 );
 
-export const DetailIconCell = (installation: TSAPInstallation) => {
+export const DetailIconCell = (installation: TSAPInstallationWithService) => {
   const { t } = useTranslation('listing');
-  const id = installation?.sapSid;
-  // TODO : manage redirection to installation summary page
+  const navigate = useNavigate();
+
+  const taskId = installation?.taskId;
+  const reportUrl = buildViewInstallationRedirectUrl({
+    serviceName: installation?.serviceName,
+    taskId,
+  });
+
   return (
     <>
-      <OdsIcon name={ODS_ICON_NAME.eye} id={id} />
-      <OdsTooltip role="tooltip" triggerId={id}>
+      <OdsButton
+        id={taskId}
+        size="sm"
+        variant="ghost"
+        label=""
+        icon="eye"
+        aria-label="view-installation-report"
+        onClick={() => navigate(reportUrl)}
+      />
+      <OdsTooltip role="tooltip" triggerId={taskId}>
         <OdsText preset={ODS_TEXT_PRESET.paragraph}>
           {t('sap_hub_history_installation_see_detail')}
         </OdsText>
--- packages/manager/apps/sap-features-hub/src/data/hooks/useInstallationHistory.ts
+++ packages/manager/apps/sap-features-hub/src/data/hooks/useInstallationHistory.ts
@@ -1,8 +1,8 @@
 import { useQueries, UseQueryResult } from '@tanstack/react-query';
 import { ApiResponse } from '@ovh-ux/manager-core-api';
-import { TSAPInstallation } from '@/types/installation.type';
 import { getSAPInstallations } from '@/data/api/sapInstallations';
 import { useVMwareServices } from '@/hooks/vmwareServices/useVMwareServices';
+import { TSAPInstallationWithService } from '@/types/installation.type';
 
 export default function useInstallationHistory() {
   const {
@@ -16,15 +16,25 @@ export default function useInstallationHistory() {
     queries:
       services?.map(({ serviceName }) => ({
         queryKey: ['sap-features-hub', `/dedicatedCloud/sap`, serviceName],
-        queryFn: () => getSAPInstallations(serviceName),
+        queryFn: async () => {
+          const res = await getSAPInstallations(serviceName);
+          const extendedInstallations: TSAPInstallationWithService[] = res.data.map(
+            (installation) => ({ ...installation, serviceName }),
+          );
+
+          return { ...res, data: extendedInstallations };
+        },
         enabled: !!services,
         retry: false,
       })) ?? [],
 
     combine: (
-      results: UseQueryResult<ApiResponse<TSAPInstallation[]>, Error>[],
+      results: UseQueryResult<
+        ApiResponse<TSAPInstallationWithService[]>,
+        Error
+      >[],
     ) => ({
-      data: results.map(({ data: result }) => result?.data)?.flat(),
+      data: results.flatMap(({ data: result }) => result?.data ?? []),
       isLoading: results.some(({ isLoading }) => isLoading),
       isPending: results.some(({ isPending }) => isPending),
       isError: results.some(({ isError }) => isError),
