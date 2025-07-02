57fa6fcbb8b8551631eab8bea9d7a3a54902a158
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-07-02 11:27:06 +0200

    feat(sap-features-hub): add installation creation redirection
    ref: #MANAGER-18621
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/sap-features-hub/src/pages/installation/stepSummary/InstallationStepSummary.page.tsx
+++ packages/manager/apps/sap-features-hub/src/pages/installation/stepSummary/InstallationStepSummary.page.tsx
@@ -3,6 +3,7 @@ import { Trans, useTranslation } from 'react-i18next';
 import { Links, useNotifications } from '@ovh-ux/manager-react-components';
 import { saveAs } from 'file-saver';
 import { useOvhTracking } from '@ovh-ux/manager-react-shell-client';
+import { useNavigate } from 'react-router-dom';
 import { useFormSteps } from '@/hooks/formStep/useFormSteps';
 import { useInstallationFormContext } from '@/context/InstallationForm.context';
 import FormLayout from '@/components/Form/FormLayout.component';
@@ -13,16 +14,25 @@ import { useInstallationCreation } from '@/data/hooks/useInstallationCreation';
 import { getSummaryBlob, getSummaryFileName } from '@/utils/summaryExport';
 import { TRACKING } from '@/tracking.constants';
 import { formMappers } from '@/mappers/formMappers';
+import { buildViewInstallationRedirectUrl } from '@/utils/buildSearchQuery';
 
 export default function InstallationStepSummary() {
   const { t } = useTranslation('installation');
+  const navigate = useNavigate();
   const { previousStep, serviceName } = useFormSteps();
   const { values } = useInstallationFormContext();
   const { formSummary } = useFormSummary(values);
   const { addError, clearNotifications } = useNotifications();
-  const { mutate: createInstallation } = useInstallationCreation({
+  const { mutate: createInstallation, isPending } = useInstallationCreation({
     serviceName,
     onMutate: () => clearNotifications(),
+    onSuccess: (data) => {
+      const redirectUrl = buildViewInstallationRedirectUrl({
+        serviceName,
+        taskId: data.data,
+      });
+      navigate(redirectUrl);
+    },
     onError: (err) => {
       const error = err?.response?.data?.message || err?.message;
       addError(t('summary_api_error', { error }), true);
@@ -53,6 +63,7 @@ export default function InstallationStepSummary() {
         ></Trans>
       }
       submitLabel={t('summary_cta_submit')}
+      isSubmitLoading={isPending}
       onSubmit={() => {
         trackClick(TRACKING.installation.submitSummary('confirm'));
         createInstallation(formMappers.toStructured(values));
