6b30e815f19f0cac9ac9fbfb39d3a3b73ae13079
Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>
2025-07-03 08:17:11 +0200

    fix(hpc-vmware-vsphere): fix commercial name detection for onboarding
    REF: #MANAGER-17708
    Signed-off-by: Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>

--- packages/manager/apps/hpc-vmware-vsphere/src/utils/getCommercialType.ts
+++ packages/manager/apps/hpc-vmware-vsphere/src/utils/getCommercialType.ts
@@ -1,14 +1,11 @@
 import { CommercialName } from '@/types/datacenter';
 
-export type CommercialType = 'ESSENTIALS' | 'PREMIER' | 'UNKNOWN';
+export type CommercialType = 'ESSENTIALS' | 'PREMIER';
 
 export function getCommercialType(name?: CommercialName): CommercialType {
   if (/^ESSENTIALS(-.*)?$/.test(name)) {
     return 'ESSENTIALS';
   }
-  if (/^PREMIER(-.*)?$/.test(name)) {
-    return 'PREMIER';
-  }
 
-  return 'UNKNOWN';
+  return 'PREMIER';
 }
