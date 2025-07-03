344e7b6c5fb6c5d6ad0294f7fba9f3fb8523b3a0
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-07-03 11:02:03 +0200

    feat(kms): secret dashboard page
    ref: #MANAGER-17850
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

---
+++ packages/manager/apps/key-management-service/src/modules/secret-manager/pages/dashboard/generalInformation/InformationsTile.component.spec.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { describe, vi } from 'vitest';
+import { screen, render } from '@testing-library/react';
+import { secretsMock } from '@secret-manager/mocks/secrets/secrets.mock';
+import { InformationsTile } from './InformationsTile.component';
+
+const mockSecret = secretsMock[0];
+
+const mockFormatDate = vi.fn((date: string) => date);
+vi.mock('@/common/hooks/useFormatDate', () => ({
+  useFormatDate: () => ({
+    formatDate: mockFormatDate,
+  }),
+}));
+
+describe('Secrets Informations Tile component tests suite', () => {
+  test('Should display settings tile with all data', async () => {
+    const { container } = render(<InformationsTile secret={mockSecret} />);
+
+    const labelList = [
+      'secret-manager/dashboard:general_information',
+      'path',
+      mockSecret.path,
+      'urn',
+      'creation_date',
+      mockSecret.metadata.createdAt,
+      'secret-manager/dashboard:last_update',
+      mockSecret.metadata.updatedAt,
+    ];
+
+    labelList.forEach((label) => {
+      expect(screen.getByText(label)).toBeVisible();
+    });
+
+    expect(
+      container.querySelector(`ods-clipboard[value="${mockSecret.iam.urn}"]`),
+    ).toBeVisible();
+
+    expect(mockFormatDate).toHaveBeenCalledWith(mockSecret.metadata.createdAt);
+    expect(mockFormatDate).toHaveBeenCalledWith(mockSecret.metadata.updatedAt);
+  });
+});
