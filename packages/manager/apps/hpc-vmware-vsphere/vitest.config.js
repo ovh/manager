e99fadee50bab470e722853cdd44fd7e13ebda40
David Arsène <david.arsene.ext@ovhcloud.com>
2025-07-03 15:04:04 +0200

    chore(vmware-vpshere): upgrade test config
    resolves: #MANAGER-18389
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/hpc-vmware-vsphere/vitest.config.js
+++ packages/manager/apps/hpc-vmware-vsphere/vitest.config.js
@@ -1,34 +1,24 @@
 import path from 'path';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import {
+  sharedConfig,
+  mergeConfig,
+  createConfig,
+  defaultExcludedFiles,
+} from '@ovh-ux/manager-tests-setup';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: [],
-    coverage: {
-      include: ['src'],
-      exclude: [],
-    },
-    testTimeout: 60_000,
-    fileParallelism: false,
-    maxWorkers: 1,
-    pollOptions: {
-      forks: {
-        singleFork: true,
-      },
-      threads: {
-        singleThread: true,
+export default mergeConfig(
+  sharedConfig,
+  createConfig({
+    test: {
+      setupFiles: [],
+      coverage: {
+        exclude: [...defaultExcludedFiles],
       },
     },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
-    mainFields: ['module'],
-  },
-});
+  }),
+);
