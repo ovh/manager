c0fb79ab9024f718d63005903c98b2409f57dc97
Marc Monchablon <marc.monchablon.ext@corp.ovh.com>
2025-07-03 16:07:30 +0200

    feat(pci-project): add ItalyAgreements component
    ref: #MANAGER-18797
    
    Signed-off-by: Marc Monchablon <marc.monchablon.ext@corp.ovh.com>

--- packages/manager/apps/pci-project/src/constants.ts
+++ packages/manager/apps/pci-project/src/constants.ts
@@ -3,6 +3,14 @@ export const FEATURE_AVAILABILITY = {
   DATABASES_ANALYTICS: 'pci-databases-analytics',
 };
 
+export const ITALY_AGREEMENT_TEXT = {
+  TITLE:
+    "Il Cliente dichiara di riconoscere, accettare ed approvare espressamente, ai sensi dell'art. 1341, secondo comma, e dell'art. 1342, secondo comma, del Codice Civile",
+  LINK: 'Le specifiche clausole indicate qui',
+  DETAILS:
+    "Il Cliente dichiara di riconoscere, accettare ed approvare espressamente, ai sensi dell'art. 1341, secondo comma, e dell'art. 1342, secondo comma, del Codice Civile, le seguenti specifiche clausole del presente Contratto: 2.5 (“Chiusura dell’Account Cliente”), 3.7 (“Sospensione dei Servizi”), 4.2 (“SLA”), 4.3 (“Crediti di servizio”), 5.2 (“Modifica dei Servizi ordinati”), 7.1 (“Casi di e-mail fraudolente e indesiderate”), 7.3 (“Sospensione e disattivazione”), 10.2 (“Responsabilità di OVHcloud”), 10.3 (“Limitazioni di responsabilità”), 10.4 (“Eccezioni”), 10.6 (“Responsabilità del Cliente”), 10.7 (“Garanzie”), 10.8 (“Forza Maggiore”), 11.2 (“Modifica delle tariffe”), 11.6 (“Mancato pagamento e ritardo nel pagamento”), 11.7 (“Contestazione”), 12.2 (“Rinnovo dei Servizi”), 14.1 (“Legge applicabile e foro competente”), 14.2, ult. cpv. (“Sanzioni internazionali e Controllo delle esportazioni”), 14.7 (“Modifiche al Contratto”), 14.9 (“Cessione del contratto”).",
+};
+
 export type TFeatureStateDetail = {
   url: string;
   targetParamKeys?: string[];
