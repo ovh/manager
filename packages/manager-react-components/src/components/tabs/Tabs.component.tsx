0bed40906c22d19f25a1ae58bb39feac5e7d9aa7
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-07-04 14:40:20 +0200

    feat(manager-wiki): tabs folder update
    ref: #MANAGER-18863
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-react-components/src/components/tabs/Tabs.component.tsx
+++ packages/manager-react-components/src/components/tabs/Tabs.component.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import { Tabs, TabList, Tab, TabsValueChangeEvent } from '@ovhcloud/ods-react';
-import { TabsProps } from './Tabs.props';
+import { Tabs, TabList, Tab } from '@ovhcloud/ods-react';
+import { TProps } from './Tabs.props';
 
 export function TabsComponent<Item>({
   items = [],
@@ -8,8 +8,8 @@ export function TabsComponent<Item>({
   contentElement = ({ item }) => <>{`${item}`}</>,
   className,
   onChange,
-}: TabsProps<Item>): JSX.Element {
-  const [selectedTabItem, setselectedTabItem] = useState<string>(
+}: TProps<Item>): JSX.Element {
+  const [selectedTabsItem, setSelectedTabsItem] = useState<string>(
     items?.[0] as string,
   );
 
@@ -19,25 +19,25 @@ export function TabsComponent<Item>({
   return (
     <div className={className}>
       <Tabs
-        onValueChange={(value: TabsValueChangeEvent) => {
-          setselectedTabItem(value?.value);
+        onValueChange={(value) => {
+          setSelectedTabsItem(value?.value);
           onChange?.(value as Item);
         }}
-        value={selectedTabItem}
+        value={selectedTabsItem}
       >
         <TabList>
           {items.map((item) => (
             <Tab key={item as string} value={item as string}>
               <TitleComponent
                 item={item}
-                isSelected={item === selectedTabItem}
+                isSelected={item === selectedTabsItem}
               />
             </Tab>
           ))}
         </TabList>
       </Tabs>
       <div className="bg-[--ods-color-primary-050] border border-solid border-[--ods-color-primary-100] border-t-0">
-        <ContentComponent item={selectedTabItem as Item} />
+        <ContentComponent item={selectedTabsItem as Item} />
       </div>
     </div>
   );
