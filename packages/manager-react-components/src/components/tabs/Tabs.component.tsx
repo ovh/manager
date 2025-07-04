3b47be8485f65125197407f9dc7e86a7459e0518
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-07-04 14:40:20 +0200

    feat(manager-wiki): tabs folder update
    ref: #MANAGER-18863
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-react-components/src/components/tabs/Tabs.component.tsx
+++ packages/manager-react-components/src/components/tabs/Tabs.component.tsx
@@ -1,18 +1,11 @@
 import { useState } from 'react';
 import { Tabs, TabList, Tab } from '@ovhcloud/ods-react';
-
-type TProps<Item> = {
-  items?: Item[];
-  titleElement?: (item: Item, isSelected?: boolean) => JSX.Element | string;
-  contentElement?: (item: Item) => JSX.Element;
-  className?: string;
-  onChange?: (item: Item) => void;
-};
+import { TProps } from './Tabs.props';
 
 export function TabsComponent<Item>({
   items = [],
-  titleElement = (item) => <div>{`title ${item}`}</div>,
-  contentElement = (item) => <div>{`content ${item}`}</div>,
+  titleElement = ({ item }) => <>{`${item}`}</>,
+  contentElement = ({ item }) => <>{`${item}`}</>,
   className,
   onChange,
 }: TProps<Item>): JSX.Element {
@@ -20,8 +13,11 @@ export function TabsComponent<Item>({
     items?.[0] as string,
   );
 
+  const TitleComponent = titleElement;
+  const ContentComponent = contentElement;
+
   return (
-    <section className={className}>
+    <div className={className}>
       <Tabs
         onValueChange={(value) => {
           setSelectedTabsItem(value?.value);
@@ -32,15 +28,18 @@ export function TabsComponent<Item>({
         <TabList>
           {items.map((item) => (
             <Tab key={item as string} value={item as string}>
-              {titleElement(item, item === selectedTabsItem)}
+              <TitleComponent
+                item={item}
+                isSelected={item === selectedTabsItem}
+              />
             </Tab>
           ))}
         </TabList>
       </Tabs>
       <div className="bg-[--ods-color-primary-050] border border-solid border-[--ods-color-primary-100] border-t-0">
-        {contentElement(selectedTabsItem as Item)}
+        <ContentComponent item={selectedTabsItem as Item} />
       </div>
-    </section>
+    </div>
   );
 }
 
