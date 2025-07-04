46d74ad6b175b696e0085c1f630e755430510c4e
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-07-04 14:40:20 +0200

    feat(manager-wiki): tabs folder update
    ref: #MANAGER-18863
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-react-components/src/utils/index.ts
+++ packages/manager-react-components/src/utils/index.ts
@@ -1,9 +1,8 @@
 export const uniqBy = function uniqBy<I, U>(arr: I[], cb: (item: I) => U) {
   return [
     ...arr
-      .reduce((map: Map<U, I>, item?: I) => {
+      .reduce((map: Map<U, I>, item: I) => {
         if (!map.has(cb(item))) map.set(cb(item), item);
-
         return map;
       }, new Map())
       .values(),
