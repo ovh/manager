4295a7c76e67a700f4a6e0050e5cda9fa866f759
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2025-07-07 13:57:50 +0200

    fix(dedicated.vps): autobackup for vps new range
    ref: #MANAGER-17934
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/modules/vps/src/dashboard/vps-dashboard.constants.js
+++ packages/manager/modules/vps/src/dashboard/vps-dashboard.constants.js
@@ -11,7 +11,7 @@ export const SERVICE_TYPE = 'vps';
 export const ADD_DOMAIN_LINK =
   'https://www.ovh.com/fr/order/webcloud/#/webCloud/domain/select?selection=~()';
 
-export const NEW_RANGE_VERSION = '2019v1';
+export const NEW_RANGE_VERSION = ['2019v1', '2025v1'];
 
 export const VPS_STATES = {
   ERROR: ['maintenance', 'stopped', 'stopping'],
