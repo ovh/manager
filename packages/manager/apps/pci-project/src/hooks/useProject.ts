68daeded7b87ad447496f51dccb4c1e25aa1eff3
Stephane Ravet <ravet.st@gmail.com)>
2025-07-07 16:51:21 +0200

    feat(pci-project): implement FullPageSpinner component and update project routing
    ref: #MANAGER-18922
    
    Signed-off-by: Stephane Ravet <ravet.st@gmail.com>

--- packages/manager/apps/pci-project/src/hooks/useProject.ts
+++ packages/manager/apps/pci-project/src/hooks/useProject.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import {
   getCloudProjectService,
   getCloudProjectServiceQueryKey,
-} from '@/data/api/pci-project';
+} from '@/data/api/project';
 
 export const useProject = (projectId: string | undefined) => {
   const { data: project, ...rest } = useQuery({
