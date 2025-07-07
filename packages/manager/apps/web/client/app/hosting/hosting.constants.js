c64c0546e6e43703009e5ba7611cbb6b80de08bf
Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>
2025-07-07 09:49:31 +0200

    feat(web-hosting): add empty slots on database table
    ref: #WEB-20397
    
    Signed-off-by: Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>

--- packages/manager/apps/web/client/app/hosting/hosting.constants.js
+++ packages/manager/apps/web/client/app/hosting/hosting.constants.js
@@ -116,8 +116,13 @@ export const HOSTING_GUIDES = [
   },
 ];
 
+export const SQL_PERSO = 'sqlPerso';
+export const EXTRA_SQL_PERSO = 'extraSqlPerso256';
+
 export default {
   NEW_OFFERS_NAME,
   OFFERS_UNELIGIBLE_FOR_MODULE,
   HOSTING_GUIDES,
+  SQL_PERSO,
+  EXTRA_SQL_PERSO,
 };
