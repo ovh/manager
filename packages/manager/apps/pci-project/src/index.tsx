89a5380ed62abb0c1080cee6b6a4355558262338
Stephane Ravet <ravet.st@gmail.com>
2025-07-07 16:47:10 +0200

    feat(pci-roject): add project header
    ref: #MANAGER-18922
    
    Signed-off-by: Stephane Ravet <ravet.st@gmail.com>

--- packages/manager/apps/pci-project/src/index.tsx
+++ packages/manager/apps/pci-project/src/index.tsx
@@ -28,7 +28,7 @@ const init = async (appName: string) => {
     context,
     reloadOnLocaleChange: true,
     defaultNS: appName,
-    ns: ['listing', 'dashboard', 'onboarding'],
+    ns: ['listing', 'home', 'onboarding'],
   });
 
   const region = context.environment.getRegion();
