9e3cd25fc0c2c9beef138a6690726e7b4a259a53
Simon Chaumet <simon.chaumet.ext@ovhcloud.com>
2025-07-07 18:56:53 +0200

    feat(pci-instances): add distant backup
    ref: #TAPC-3053
    
    Signed-off-by: Simon Chaumet <simon.chaumet.ext@ovhcloud.com>

---
+++ packages/manager/apps/pci-instances/src/components/zod-form/SelectField.tsx
@@ -0,0 +1,48 @@
+import { Controller, FieldPath, FieldValues } from 'react-hook-form';
+import { useId } from 'react';
+import { ErrorText } from './ErrorText';
+import {
+  FormField,
+  FormFieldLabel,
+  Select,
+  SelectContent,
+  SelectControl, SelectGroupItem,
+  SelectItem,
+} from '@ovhcloud/ods-react';
+
+export type SelectFieldItem = SelectItem;
+export type SelectFieldGroup = SelectGroupItem;
+
+export const SelectField = <
+  TFieldValues extends FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+>({
+  label,
+  name,
+  items,
+}: {
+  label: string;
+  name: TName;
+  items: (SelectItem | SelectGroupItem)[];
+}) => {
+  const inputId = useId();
+
+  return (
+    <FormField>
+      <FormFieldLabel htmlFor={inputId}>{label}</FormFieldLabel>
+
+      <Controller<TFieldValues, TName>
+        render={({ field: { value, onChange }, fieldState: { error, invalid } }) => (
+          <>
+            <Select value={value} items={items} onValueChange={onChange} invalid={invalid}>
+              <SelectControl />
+              <SelectContent />
+            </Select>
+            {!!error && <ErrorText>{error.message}</ErrorText>}
+          </>
+        )}
+        name={name}
+      />
+    </FormField>
+  );
+};
