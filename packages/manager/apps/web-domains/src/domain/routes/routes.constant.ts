fc5b0709bba8a15b9d08bc5b623dc0d80b069e61
Justine Loizel <justine.loizel@ovhcloud.com>
2025-07-07 17:01:36 +0200

    feat(web-domains): init new domain page
    ref: #MANAGER-18842
    
    Signed-off-by: Justine Loizel <justine.loizel@ovhcloud.com>
    Co-authored-by: Brendan Seiller <brendan.seiller@ovhcloud.com>

---
+++ packages/manager/apps/web-domains/src/domain/routes/routes.constant.ts
@@ -0,0 +1,13 @@
+export const urls = {
+  domainRoot: '/domain',
+  domainOnboarding: '/domain/onboarding',
+  domainDetail: '/domain/:serviceName',
+  domainTabInformation: '/domain/:serviceName/information',
+  domainTabZone: '/domain/:serviceName/zone',
+  domainTabDns: '/domain/:serviceName/dns',
+  domainTabRedirection: '/domain/:serviceName/redirection',
+  domainTabDynHost: '/domain/:serviceName/dynhost',
+  domainTabHost: '/domain/:serviceName/host',
+  domainTabDnssec: '/domain/:serviceName/dnssec',
+  domainTabContactManagement: '/domain/:serviceName/contact-management',
+};
