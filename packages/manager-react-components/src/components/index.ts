f43d930b0ef4b76750ecf213f16a091313e422e9
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-07-08 10:50:52 +0200

    fix(manager-react-components): correct onboarding tests
    ref: #MANAGER-18712
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-react-components/src/components/index.ts
+++ packages/manager-react-components/src/components/index.ts
@@ -26,7 +26,6 @@ export * from './notifications/useNotifications';
 export * from './filters';
 
 export * from './ManagerButton/ManagerButton';
-export * from './ManagerLink/ManagerLink.component';
 export * from './ManagerText/ManagerText';
 
 export * from './ServiceStateBadge/ServiceStateBadge.component';
