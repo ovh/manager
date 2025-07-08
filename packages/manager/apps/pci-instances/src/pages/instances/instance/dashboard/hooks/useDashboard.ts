28ff4647135c62d254731f48cca644c53b532e10
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2025-07-09 00:39:38 +0200

    feat(pci-instances): display instance name and allow updating if edit is enabled
    ref: #TAPC-4006
    
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

---
+++ packages/manager/apps/pci-instances/src/pages/instances/instance/dashboard/hooks/useDashboard.ts
@@ -0,0 +1,61 @@
+import { useMemo } from 'react';
+import { useInstance } from '@/data/hooks/instance/useInstance';
+import { selectInstanceDashboard } from '../view-models/selectInstanceDashboard';
+import { TInstance } from '@/types/instance/entity.type';
+import { instancesQueryKey } from '@/utils';
+import queryClient from '@/queryClient';
+
+type TUseDashboardArgs = {
+  region: string | null;
+  instanceId: string;
+};
+
+export const useDashboard = ({ region, instanceId }: TUseDashboardArgs) => {
+  const { data: instance, isPending } = useInstance({
+    region,
+    instanceId,
+    params: ['withBackups', 'withImage', 'withNetworks', 'withVolumes'],
+    queryOptions: {
+      gcTime: 0,
+    },
+  });
+
+  return useMemo(
+    () => ({
+      instance: selectInstanceDashboard(instance),
+      isPending,
+    }),
+    [instance],
+  );
+};
+
+type TUpdateDashboardCacheArgs = {
+  projectId: string;
+  instanceId: string;
+  region: string;
+  payload: Partial<TInstance>;
+};
+
+export const updateDashboardCache = ({
+  projectId,
+  instanceId,
+  region,
+  payload,
+}: TUpdateDashboardCacheArgs) => {
+  queryClient.setQueryData<TInstance>(
+    instancesQueryKey(projectId, [
+      'region',
+      region,
+      'instance',
+      instanceId,
+      'withBackups',
+      'withImage',
+      'withNetworks',
+      'withVolumes',
+    ]),
+    (prevData) => {
+      if (!prevData) return undefined;
+      return { ...prevData, ...payload };
+    },
+  );
+};
