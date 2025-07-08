1c9530472358dcadbc8bd7da3770f1a6f1016e49
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2025-07-08 11:37:32 +0200

    feat(pci-instances): add new dashboard page
    ref: #TAPC-4388
    
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

---
+++ packages/manager/apps/pci-instances/src/routes/loaders/instanceLegacy.loader.ts
@@ -0,0 +1,24 @@
+import { LoaderFunction, redirect } from 'react-router-dom';
+import { TSectionType } from '@/types/instance/action/action.type';
+import { actionSectionRegex } from '@/constants';
+import { getPathMatch } from '@/utils';
+
+export const instanceLegacyRedirectionLoader: LoaderFunction = ({
+  request,
+  params,
+}) => {
+  const url = new URL(request.url);
+  const { pathname, searchParams } = url;
+
+  const { instanceId } = params;
+  const region = searchParams.get('region') ?? null;
+
+  const instancePageUrl = `../region/${region}/instance/${instanceId}`;
+  const section = getPathMatch<TSectionType>(pathname, actionSectionRegex);
+
+  if (section) {
+    return redirect(`${instancePageUrl}/${section}`);
+  }
+
+  return redirect(instancePageUrl);
+};
