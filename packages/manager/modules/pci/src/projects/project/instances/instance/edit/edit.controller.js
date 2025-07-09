23660e2cae90f0fdc9cd465a630dd4b00881a01b
Simon Chaumet <simon.chaumet.ext@ovhcloud.com>
2025-07-09 15:54:43 +0200

    feat(pci-instances): add distant backup restoration
    ref: #TAPC-4036, #TAPC-4338
    
    Signed-off-by: Simon Chaumet <simon.chaumet.ext@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/instances/instance/edit/edit.controller.js
+++ packages/manager/modules/pci/src/projects/project/instances/instance/edit/edit.controller.js
@@ -53,9 +53,6 @@ export default class PciInstanceEditController {
     };
     this.regionsTypesAvailability = {};
     this.fetchRegionsTypesAvailability();
-    this.imageEditMessage =
-      this.imageEditMessage ||
-      'pci_projects_project_instances_instance_edit_reboot_message';
     this.imageEditSuccessMessage =
       this.imageEditSuccessMessage ||
       'pci_projects_project_instances_instance_edit_image_success_message';
