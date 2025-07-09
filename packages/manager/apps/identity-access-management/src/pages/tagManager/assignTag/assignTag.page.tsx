a909704b14a4570619554c903c77260a10d240f6
qpavy <contact@quentinpavy.com>
2025-07-09 11:18:53 +0200

    test(iam): tag manager listing tests (#17998)
    ref: #MANAGER-15793
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

--- packages/manager/apps/identity-access-management/src/pages/tagManager/assignTag/assignTag.page.tsx
+++ packages/manager/apps/identity-access-management/src/pages/tagManager/assignTag/assignTag.page.tsx
@@ -3,12 +3,13 @@ import { useTranslation } from 'react-i18next';
 import { BaseLayout, Breadcrumb } from '@ovh-ux/manager-react-components';
 
 import appConfig from '@/identity-access-management.config';
+import { labels } from '@/test-utils/labels';
 
 export default function AssignTag() {
   const { t } = useTranslation('tag-manager');
 
   const header = {
-    title: t('assignTag'),
+    title: t(labels.tagManager.assignTags),
   };
 
   return (
--- packages/manager/apps/identity-access-management/src/pages/tagManager/components/tagsListDatagrid/utils/sortTags.ts
+++ packages/manager/apps/identity-access-management/src/pages/tagManager/components/tagsListDatagrid/utils/sortTags.ts
@@ -6,9 +6,26 @@ export type SortTagsParams = {
   desc: boolean;
 };
 
+export const sortString = (stringA: unknown, stringB: unknown) => {
+  return stringA.toString().localeCompare(stringB.toString());
+};
+
+export const sortNumber = (numberA: unknown, numberB: unknown) => {
+  return Number(numberA) - Number(numberB);
+};
+
 export const sortTags = ({ tags, columnId, desc }: SortTagsParams) => {
   return tags.sort((tagA, tagB) => {
-    if (desc) return tagA[columnId] < tagB[columnId] ? 1 : -1;
-    return tagA[columnId] > tagB[columnId] ? 1 : -1;
+    if (typeof tagA[columnId] === 'string') {
+      if (desc) return sortString(tagB[columnId], tagA[columnId]);
+      return sortString(tagA[columnId], tagB[columnId]);
+    }
+
+    if (typeof tagA[columnId] === 'number') {
+      if (desc) return sortNumber(tagB[columnId], tagA[columnId]);
+      return sortNumber(tagA[columnId], tagB[columnId]);
+    }
+
+    return 0;
   });
 };
