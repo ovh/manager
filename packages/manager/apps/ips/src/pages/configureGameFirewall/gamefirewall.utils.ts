5fce26ead48aaf7e23da0ffc1ae77ba49fb489fa
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-07-09 19:12:27 +0200

    feat(ips): gamefirewall add and delete rule
    ref: #MANAGER-18599
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

---
+++ packages/manager/apps/ips/src/pages/configureGameFirewall/gamefirewall.utils.ts
@@ -0,0 +1,43 @@
+import { IpGameFirewallRule } from '@/data/api';
+
+export const IP_MITIGATION_RULE_PROTOCOL_PORT: {
+  [protocol: string]: { from?: number; to?: number };
+} = {
+  arkSurvivalEvolved: { from: 7777, to: 7778 },
+  arma: { from: 2302 },
+  gtaMultiTheftAutoSanAndreas: { from: 22003 },
+  gtaSanAndreasMultiplayerMod: { from: 7777 },
+  hl2Source: { from: 27015 },
+  minecraftPocketEdition: { from: 19132, to: 19133 },
+  minecraftQuery: { from: 25565 },
+  rust: { from: 28015 },
+  trackmaniaShootmania: { from: 2350, to: 2450 },
+};
+
+export const PORT_MIN = 1;
+export const PORT_MAX = 65535;
+
+export const hasPortRangeError = ({
+  startPort,
+  endPort,
+}: {
+  startPort: number;
+  endPort: number;
+}) =>
+  startPort < PORT_MIN ||
+  startPort > PORT_MAX ||
+  endPort > PORT_MAX ||
+  endPort < PORT_MIN;
+
+export const hasConflictingPorts = ({
+  startPort,
+  endPort,
+  rules,
+}: {
+  startPort: number;
+  endPort: number;
+  rules: IpGameFirewallRule[];
+}) =>
+  rules.some(
+    (rule) => startPort >= rule.ports.from || endPort <= rule.ports.to,
+  );
