c53a791bd48b664cdc88c50cf92c9adba784fa9e
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-07-09 16:42:22 +0200

    feat(manager-react-components): delete guide menu and guide header components
    ref: #MANAGER-18839
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-react-components/src/components/guides-header/interface.ts
+++
@@ -1,5 +0,0 @@
-export interface Guide {
-  key: string; // guide uniquer identifier
-  url: Record<string, string>; // mapping of ovhSubsidiary and URLs
-  tracking?: string; // optional data tracking to be sent
-}
