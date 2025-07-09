ed475f1ebabd3cd60e703f0559bef7cc888a0a21
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2025-07-09 15:59:47 +0200

    feat(pci-instances): add general information content
    ref: #TAPC-4052
    
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

--- packages/manager/apps/pci-instances/src/pages/instances/instance/dashboard/view-models/selectInstanceDashboard.ts
+++ packages/manager/apps/pci-instances/src/pages/instances/instance/dashboard/view-models/selectInstanceDashboard.ts
@@ -1,13 +1,47 @@
-import { TInstance, TInstanceAction } from '@/types/instance/entity.type';
+import {
+  TInstance,
+  TInstanceAction,
+  TInstanceFlavor,
+  TInstancePrice,
+  TInstanceRegion,
+} from '@/types/instance/entity.type';
+
+type TRegion = TInstanceRegion;
+type TPrice = {
+  label: string;
+  type: string;
+  value: number;
+};
+type TFlavor = {
+  name: string;
+  ram: string;
+  cpu: string;
+};
 
 export type TInstanceDashboardViewModel = {
   id: string;
   name: string;
+  flavor: TFlavor | null;
+  region: TRegion;
+  pricings: TPrice[];
   isEditEnabled: boolean;
 } | null;
 
 const isEditionEnabled = (actions: TInstanceAction[]) =>
   actions.some(({ name }) => name === 'edit');
+
+const mapFlavor = ({ name, specs }: TInstanceFlavor) => ({
+  name,
+  ram: specs ? `${specs.ram.value} ${specs.ram.unit}` : '-',
+  cpu: specs ? `${specs.cpu.value} ${specs.cpu.unit}` : '-',
+});
+
+const mapPricings = (pricings: TInstancePrice[]) =>
+  pricings.map((pricing) => ({
+    label: pricing.type !== 'licence' ? 'instance' : 'licence', // label will be a translation key
+    type: pricing.type,
+    value: pricing.priceInUcents,
+  }));
 
 export const selectInstanceDashboard = (
   instance?: TInstance,
@@ -17,6 +51,9 @@ export const selectInstanceDashboard = (
   return {
     id: instance.id,
     name: instance.name,
+    flavor: instance.flavor ? mapFlavor(instance.flavor) : null,
+    region: instance.region,
+    pricings: mapPricings(instance.pricings || []),
     isEditEnabled: isEditionEnabled(instance.actions),
   };
 };
