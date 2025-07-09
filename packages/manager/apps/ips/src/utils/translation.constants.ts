88e518fa2482cbba0576494169e0c4c50667834c
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-07-09 19:12:27 +0200

    feat(ips): gamefirewall add and delete rule
    ref: #MANAGER-18599
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

---
+++ packages/manager/apps/ips/src/utils/translation.constants.ts
@@ -0,0 +1,13 @@
+export const TRANSLATION_NAMESPACES = {
+  byoip: 'byoip',
+  common: 'common',
+  configureReverseDns: 'configure-reverse-dns',
+  error: 'error',
+  gameFirewall: 'game-firewall',
+  ips: 'ips',
+  listing: 'listing',
+  managerOrganisations: 'manage-organisations',
+  onboarding: 'onboarding',
+  order: 'order',
+  regionSelector: 'region-selector',
+};
