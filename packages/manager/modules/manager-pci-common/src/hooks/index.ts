a67c85ccde2fb046b4afdd7135c11ea6cfbb0576
Frédéric Vilcot <fredericvilcot@gmail.com>
2025-07-10 16:23:57 +0200

    fix(pci-common): export useParam hook
    Signed-off-by: Frédéric Vilcot <fredericvilcot@gmail.com>

--- packages/manager/modules/manager-pci-common/src/hooks/index.ts
+++ packages/manager/modules/manager-pci-common/src/hooks/index.ts
@@ -1,5 +1,6 @@
 export { useBytes } from './bytes/useBytes';
 export { usePciUrl } from './url/usePciUrl';
 export { usePricing } from './usePricing';
+export { useParam } from './url/useParam';
 export { usePCICommonContextFactory } from './usePCICommonContextFactory/usePCICommonContextFactory';
 export * from './useApplicationName/useApplicationName';
