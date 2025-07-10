ee4af16ecbd3c7b807033c17c823c9728b3f09fb
Justine Loizel <justine.loizel@ovhcloud.com>
2025-07-10 16:58:15 +0200

    feat(web-domains): dns tab datagrid
    ref: #MANAGER-18843
    
    Signed-off-by: Justine Loizel <justine.loizel@ovhcloud.com>

---
+++ packages/manager/apps/web-domains/src/domain/enum/dnsConfigurationType.enum.ts
@@ -0,0 +1,10 @@
+export enum DnsConfigurationTypeEnum {
+  ANYCAST = 'ANYCAST',
+  DEDICATED = 'DEDICATED',
+  EMPTY = 'EMPTY',
+  EXTERNAL = 'EXTERNAL',
+  HOLD = 'HOLD',
+  HOSTING = 'HOSTING',
+  MIXED = 'MIXED',
+  PARKING = 'PARKING',
+}
