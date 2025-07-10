42792f09abd0d268c6f8a697b50bdbda5380d913
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2025-07-10 17:38:11 +0200

    feat(pci-instances): add dropdown actions
    ref: #TAPC-4386
    
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

--- packages/manager/apps/pci-instances/src/pages/instances/instance/dashboard/view-models/selectInstanceDashboard.ts
+++ packages/manager/apps/pci-instances/src/pages/instances/instance/dashboard/view-models/selectInstanceDashboard.ts
@@ -1,4 +1,5 @@
 import { getInstanceStatus } from '@/pages/instances/mapper/status.mapper';
+import { TActionName } from '@/types/instance/common.type';
 import {
   TInstance,
   TInstanceAction,
@@ -22,6 +23,16 @@ type TFlavor = {
   cpu: string;
 };
 
+type TAction = {
+  label: string;
+  link: {
+    path: string;
+    isExternal: boolean;
+  };
+};
+
+type TInstanceActions = Map<string, TAction[]>;
+
 export type TInstanceDashboardViewModel = {
   id: string;
   name: string;
@@ -33,6 +44,7 @@ export type TInstanceDashboardViewModel = {
     severity: TInstanceStatusSeverity;
   };
   task: TInstanceTaskStatus;
+  actions: TInstanceActions;
   canActivateMonthlyBilling: boolean;
   isDeleteEnabled: boolean;
   isEditEnabled: boolean;
@@ -60,7 +72,106 @@ const canActivateMonthlyBilling = (actions: TInstanceAction[]) =>
 const canDeleteInstance = (actions: TInstanceAction[]) =>
   actions.some(({ name }) => name === 'delete');
 
+// TODO: find a way to handle this properly (where to build path and translated label)
+const getActionHrefByName = (
+  projectUrl: string,
+  name: TActionName,
+  { region, id }: Pick<TInstance, 'id' | 'region'>,
+): TAction['link'] => {
+  if (name === 'edit') {
+    return {
+      path: `../${id}/edit`,
+      isExternal: false,
+    };
+  }
+
+  if (name === 'create_autobackup') {
+    return { path: `${projectUrl}/workflow/new`, isExternal: true };
+  }
+
+  if (name === 'assign_floating_ip') {
+    const searchParams = new URLSearchParams({
+      ipType: 'floating_ip',
+      region: region.name,
+      instance: id,
+    });
+
+    return {
+      path: `${projectUrl}/public-ips/order?${searchParams.toString()}`,
+      isExternal: true,
+    };
+  }
+
+  if (name === 'soft_reboot') {
+    return {
+      path: `../${id}/soft-reboot?region=${region}`,
+      isExternal: false,
+    };
+  }
+
+  if (name === 'hard_reboot') {
+    return {
+      path: `../${id}/hard-reboot?region=${region}`,
+      isExternal: false,
+    };
+  }
+
+  if (name === 'rescue') {
+    return {
+      path: `../${id}/rescue/start?region=${region}`,
+      isExternal: false,
+    };
+  }
+
+  if (name === 'unrescue') {
+    return {
+      path: `../${id}/rescue/end?region=${region}`,
+      isExternal: false,
+    };
+  }
+
+  if (name === 'create_backup') {
+    return {
+      path: `../${id}/backup?region=${region}`,
+      isExternal: false,
+    };
+  }
+
+  const actions = new Set(['stop', 'start', 'shelve', 'unshelve', 'reinstall']);
+
+  if (actions.has(name)) {
+    return {
+      path: `../${id}/${name}?region=${region}`,
+      isExternal: false,
+    };
+  }
+
+  return { path: '', isExternal: false };
+};
+
+const mapActions = (
+  instance: TInstance,
+  projectUrl: string,
+): TInstanceActions =>
+  instance.actions
+    .filter(
+      ({ name }) =>
+        !['details', 'delete', 'activate_monthly_billing'].includes(name),
+    )
+    .reduce<TInstanceActions>((acc, action) => {
+      const { group, name } = action;
+      const newAction = {
+        label: `pci_instances_list_action_${name}`,
+        link: getActionHrefByName(projectUrl, name, instance),
+      };
+      const foundAction = acc.get(group);
+      if (!foundAction) return acc.set(group, [newAction]);
+      foundAction.push(newAction);
+      return acc;
+    }, new Map() as TInstanceActions);
+
 export const selectInstanceDashboard = (
+  projectUrl: string,
   instance?: TInstance,
 ): TInstanceDashboardViewModel => {
   if (!instance) return null;
@@ -73,6 +184,7 @@ export const selectInstanceDashboard = (
     pricings: mapPricings(instance.pricings || []),
     task: instance.task,
     status: getInstanceStatus(instance.status),
+    actions: mapActions(instance, projectUrl),
     canActivateMonthlyBilling: canActivateMonthlyBilling(instance.actions),
     isDeleteEnabled: canDeleteInstance(instance.actions),
     isEditEnabled: isEditionEnabled(instance.actions),
