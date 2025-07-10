a113e80493a8fa033514286b5219eb74a0a437fc
Anoop N <anoop.n@ovhcloud.com>
2025-07-10 11:12:56 +0530

    feat(manager-react-components): migrate Price component
    Migrate Price component to ODS v19
    Refactor to split child components
    Enhance test-coverage
    
    ref: #MANAGER-18749
    
    Signed-off-by: Anoop N <anoop.n@ovhcloud.com>

--- packages/manager-react-components/src/components/content/index.ts
+++ packages/manager-react-components/src/components/content/index.ts
@@ -1,5 +1,4 @@
 export * from './headers/headers.component';
-export * from './price/price.component';
 export * from './dashboard-tile/dashboard-tile.component';
 export * from './dashboard-tile/tile-block.component';
 export * from './ManagerTile/manager-tile.component';
