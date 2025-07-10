8c0b95e3b604cca7827a716c7fee0842463f6066
Stephane Ravet <ravet.st@gmail.com>
2025-07-10 10:22:53 +0200

    feat(pci-project): add section "others" on homepage
    ref: #MANAGER-18604
    
    Signed-off-by: Stephane Ravet <ravet.st@gmail.com>

--- packages/manager/apps/pci-project/src/pages/home/Home.page.tsx
+++ packages/manager/apps/pci-project/src/pages/home/Home.page.tsx
@@ -1,6 +1,12 @@
 import { useTranslation } from 'react-i18next';
-import { OdsDivider, OdsCard } from '@ovhcloud/ods-components/react';
-import { Subtitle } from '@ovh-ux/manager-react-components';
+import {
+  OdsDivider,
+  OdsCard,
+  OdsIcon,
+  OdsButton,
+} from '@ovhcloud/ods-components/react';
+import { ODS_ICON_NAME } from '@ovhcloud/ods-components';
+import { Subtitle, DashboardTile } from '@ovh-ux/manager-react-components';
 import { useParams, Link } from 'react-router-dom';
 
 import IconInstances from '@/assets/home/Instances.svg?url';
@@ -14,6 +20,12 @@ type QuickAccessItem = {
   icon: string;
   title: string;
   description: string;
+  link: string;
+};
+
+type OtherActionItem = {
+  icon: keyof typeof ODS_ICON_NAME;
+  label: string;
   link: string;
 };
 
@@ -60,16 +72,36 @@ export default function Home() {
     },
   ];
 
+  const otherActions: OtherActionItem[] = [
+    {
+      icon: 'book',
+      label: t('create_ai_notebook'),
+      link: `/public-cloud/pci/projects/${projectId}/ai-notebooks/new`,
+    },
+    {
+      icon: 'network',
+      label: t('create_load_balancer'),
+      link: `/public-cloud/pci/projects/${projectId}/load-balancer/new`,
+    },
+    {
+      icon: 'bill',
+      label: t('billing'),
+      link: `/public-cloud/pci/projects/${projectId}/billing`,
+    },
+    {
+      icon: 'cog',
+      label: t('quotas'),
+      link: `/public-cloud/pci/projects/${projectId}/quotas`,
+    },
+  ];
+
   return (
     <>
       <Subtitle>{t('quick_access')}</Subtitle>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 my-6">
         {quickAccess.map((item, idx) => (
-          <Link to={item.link} style={{ textDecoration: 'none' }}>
-            <OdsCard
-              key={idx}
-              className="flex flex-row items-center p-4 h-full border border-[#D1D5DB] rounded-xl bg-white shadow-none min-h-[100px]"
-            >
+          <Link to={item.link} style={{ textDecoration: 'none' }} key={idx}>
+            <OdsCard className="flex flex-row items-center p-4 h-full border border-[#D1D5DB] rounded-xl bg-white shadow-none min-h-[100px]">
               {/* Icon in a blue rounded square, left side */}
               <div className="bg-[#000E9C] rounded flex items-center justify-center w-23 h-23">
                 <img src={item.icon} alt="" className="w-20 h-20" />
@@ -87,7 +119,29 @@ export default function Home() {
           </Link>
         ))}
       </div>
-      <OdsDivider />
+      <div className="my-8">
+        <div className="flex flex-wrap items-center gap-3 mt-4 sm:flex-nowrap">
+          <Subtitle className="mr-4 whitespace-nowrap">{t('others')}</Subtitle>
+          <div className="flex flex-wrap gap-3 flex-1">
+            {otherActions.map((action, idx) => (
+              <Link
+                to={action.link}
+                style={{ textDecoration: 'none' }}
+                key={idx}
+              >
+                <OdsButton
+                  variant="outline"
+                  className="whitespace-nowrap flex items-center gap-2 px-4 py-2"
+                  icon={ODS_ICON_NAME[action.icon]}
+                  label={action.label}
+                />
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+      <OdsDivider className="my-8" />
+      {/* add Dashboard Tile */}
     </>
   );
 }
