35e04e325cf63fc7a392a80f15b55a5c230b390d
Justine Loizel <justine.loizel@ovhcloud.com>
2025-07-10 16:58:15 +0200

    feat(web-domains): dns tab datagrid
    ref: #MANAGER-18843
    
    Signed-off-by: Justine Loizel <justine.loizel@ovhcloud.com>

--- packages/manager/apps/web-domains/src/domain/utils/utils.ts
+++ packages/manager/apps/web-domains/src/domain/utils/utils.ts
@@ -1,8 +1,50 @@
 import { GUIDES_LIST, LangCode } from '@/domain/constants/guideLinks';
+import {
+  TDatagridDnsDetails,
+  TDomainResource,
+} from '@/domain/types/domainResource';
+import {
+  isIncluded,
+  transformCurrent,
+  transformTarget,
+} from '@/domain/utils/dnsUtils';
+import { NameServerStatusEnum } from '@/domain/enum/nameServerStatus.enum';
+import { DNS_UPDATE_OPERATION } from '../constants/dns.const';
 
 export function getLanguageKey(lang: string): LangCode {
   const code = lang.split(/[-_]/)[0].toUpperCase();
   const isSupported = code in GUIDES_LIST.domains.url;
 
   return isSupported ? (code as LangCode) : 'DEFAULT';
+}
+
+export function computeDnsDetails(
+  domainResource: TDomainResource,
+): TDatagridDnsDetails[] {
+  const current = domainResource.currentState.dnsConfiguration.nameServers;
+  const target = domainResource.targetSpec.dnsConfiguration.nameServers;
+  const updateIsInError = domainResource.currentTasks.find(
+    (task) =>
+      task.type === DNS_UPDATE_OPERATION &&
+      task.status.toLowerCase() === NameServerStatusEnum.ERROR.toLowerCase(),
+  );
+
+  const activated = current
+    .filter((dns) => isIncluded(target, dns))
+    .map((dns) => transformCurrent(dns, NameServerStatusEnum.ENABLED));
+
+  const activating = target
+    .filter((dns) => !isIncluded(current, dns))
+    .map((dns) => {
+      if (updateIsInError) {
+        return transformTarget(dns, NameServerStatusEnum.ERROR);
+      }
+      return transformTarget(dns, NameServerStatusEnum.ACTIVATING);
+    });
+
+  const deleting = current
+    .filter((dns) => !isIncluded(target, dns))
+    .map((dns) => transformCurrent(dns, NameServerStatusEnum.DELETING));
+
+  return [...activated, ...activating, ...deleting];
 }
