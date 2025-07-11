61bbf424ff4a71a8a15ef10d85df8153a876bc8f
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-07-11 10:13:27 +0200

    feat(okms): add secret version list
    ref: #MANAGER-17851
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

--- packages/manager/apps/okms/src/modules/secret-manager/pages/listing/ListingCells.component.tsx
+++ packages/manager/apps/okms/src/modules/secret-manager/pages/listing/ListingCells.component.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { ODS_BUTTON_VARIANT, ODS_ICON_NAME } from '@ovhcloud/ods-components';
 import {
+  ActionMenu,
+  ActionMenuItem,
   DataGridTextCell,
-  useFormatDate,
 } from '@ovh-ux/manager-react-components';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { Secret } from '@secret-manager/types/secret.type';
 import { SECRET_MANAGER_ROUTES_URLS } from '@secret-manager/routes/routes.constants';
 import { Link } from '@/common/components/Link/Link.component';
 import { SecretListingPageParams } from './listing.type';
+import { useFormatDate } from '@/common/hooks/useFormatDate';
 
 export const DatagridCellPath = (secret: Secret) => {
   const { domainId } = useParams<SecretListingPageParams>();
@@ -21,13 +25,36 @@ export const DatagridCellVersion = (secret: Secret) => {
 };
 
 export const DatagridCreationDate = (secret: Secret) => {
-  const formatDate = useFormatDate();
+  const { formatDate } = useFormatDate();
   return (
-    <DataGridTextCell>
-      {formatDate({
-        date: secret.metadata.createdAt,
-        format: 'Pp',
-      })}
-    </DataGridTextCell>
+    <DataGridTextCell>{formatDate(secret.metadata.createdAt)}</DataGridTextCell>
+  );
+};
+
+export const DatagridAction = (secret: Secret) => {
+  const { domainId } = useParams<SecretListingPageParams>();
+  const { t } = useTranslation('secret-manager/secrets');
+  const navigate = useNavigate();
+
+  const items: ActionMenuItem[] = [
+    {
+      id: 1,
+      label: t('access_versions'),
+      onClick: () => {
+        navigate(
+          SECRET_MANAGER_ROUTES_URLS.secretVersions(domainId, secret.path),
+        );
+      },
+    },
+  ];
+
+  return (
+    <ActionMenu
+      id={`SecretActionMenu-${secret.path}`}
+      items={items}
+      isCompact
+      icon={ODS_ICON_NAME.ellipsisVertical}
+      variant={ODS_BUTTON_VARIANT.ghost}
+    />
   );
 };
