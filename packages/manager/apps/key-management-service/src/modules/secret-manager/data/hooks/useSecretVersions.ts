ff1d268a4211ec97f490d69f672c24e0f299613c
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-07-11 10:13:27 +0200

    feat(okms): add secret version list
    ref: #MANAGER-17851
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

---
+++ packages/manager/apps/key-management-service/src/modules/secret-manager/data/hooks/useSecretVersions.ts
@@ -0,0 +1,15 @@
+import { ApiError } from '@ovh-ux/manager-core-api';
+import { useQuery } from '@tanstack/react-query';
+import { SecretVersion } from '@secret-manager/types/secret.type';
+import {
+  getSecretVersions,
+  getSecretVersionsQueryKeys,
+} from '../api/secretVersions';
+
+// TODO: use useIcebergV2 when pagination is fixed
+export const useSecretVersions = (domainId: string, path: string) => {
+  return useQuery<SecretVersion[], ApiError>({
+    queryFn: () => getSecretVersions(domainId, path),
+    queryKey: getSecretVersionsQueryKeys(domainId, path),
+  });
+};
