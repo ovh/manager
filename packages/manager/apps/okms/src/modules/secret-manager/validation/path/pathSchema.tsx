d4f116198889fb0e40398578849f53b47ff6bcfd
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-07-15 08:50:43 +0200

    fix(okms): update create secret input validation
    ref: #MANAGER-18248
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

--- packages/manager/apps/okms/src/modules/secret-manager/validation/path/pathSchema.tsx
+++ packages/manager/apps/okms/src/modules/secret-manager/validation/path/pathSchema.tsx
@@ -2,8 +2,8 @@ import { z } from 'zod';
 import { useTranslation } from 'react-i18next';
 import { NAMESPACES } from '@ovh-ux/manager-common-translations';
 
-export const MIN_CHAR = 1;
-export const MAX_CHAR = 255;
+export const PATH_MIN_CHAR = 1;
+export const PATH_MAX_CHAR = 255;
 export const useSecretPathSchema = () => {
   // a secret path can't
   // - start with a '/'
@@ -25,12 +25,9 @@ export const useSecretPathSchema = () => {
       PATH_STRUCTURE_REGEX,
       t('secret-manager/create:path_error_structure'),
     )
-    .min(
-      MIN_CHAR,
-      t('error_min_chars', { value: MIN_CHAR, ns: NAMESPACES.FORM }),
-    )
+    .min(PATH_MIN_CHAR, t('required_field', { ns: NAMESPACES.FORM }))
     .max(
-      MAX_CHAR,
-      t('error_max_chars', { value: MAX_CHAR, ns: NAMESPACES.FORM }),
+      PATH_MAX_CHAR,
+      t('error_max_chars', { value: PATH_MAX_CHAR, ns: NAMESPACES.FORM }),
     );
 };
