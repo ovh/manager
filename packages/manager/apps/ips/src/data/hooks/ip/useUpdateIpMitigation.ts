bb2df9fd4c3c2eab959726a9f4b6704a0047eed7
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2025-07-15 13:16:11 +0530

    feat(ips): manage ip mitigation
    ref: #MANAGER-18510
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

--- packages/manager/apps/ips/src/data/hooks/ip/useUpdateIpMitigation.ts
+++ packages/manager/apps/ips/src/data/hooks/ip/useUpdateIpMitigation.ts
@@ -1,35 +1,39 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { ApiError, ApiResponse } from '@ovh-ux/manager-core-api';
 import { useNotifications } from '@ovh-ux/manager-react-components';
-import { getdedicatedServerVmacQueryKey, addVirtualMacToIp } from '@/data/api';
+import {
+  getIpDetailsQueryKey,
+  updateIpMitigation,
+  UpdateIpMitigationParams,
+} from '@/data/api';
 
-export type UseAddVirtualMacToIpParams = {
-  serviceName: string;
-  ip: string;
-  type: string;
-  virtualMachineName: string;
+export type UseUpdateIpMitigationParams = UpdateIpMitigationParams & {
   onError?: (apiError: ApiError) => void;
   onSuccess?: (result: ApiResponse<void>) => void;
 };
 
-export const useAddVirtualMacToIp = (params: UseAddVirtualMacToIpParams) => {
+export const useUpdateIpMitigation = ({
+  ipBlock,
+  ip,
+  mitigation,
+  onError,
+  onSuccess,
+}: UseUpdateIpMitigationParams) => {
   const queryClient = useQueryClient();
   const { clearNotifications } = useNotifications();
 
   return useMutation({
-    mutationFn: () => addVirtualMacToIp(params),
+    mutationFn: () => updateIpMitigation({ ipBlock, ip, mitigation }),
     onSuccess: async (data) => {
       clearNotifications();
       await queryClient.invalidateQueries({
-        queryKey: getdedicatedServerVmacQueryKey({
-          serviceName: params.serviceName,
-        }),
+        queryKey: getIpDetailsQueryKey({ ip }),
       });
-      params.onSuccess?.(data);
+      onSuccess?.(data);
     },
     onError: (error: ApiError) => {
       clearNotifications();
-      params.onError?.(error);
+      onError?.(error);
     },
   });
 };
