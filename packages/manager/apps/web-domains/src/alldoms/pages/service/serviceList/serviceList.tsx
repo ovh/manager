01973d04b281875eef34ce4900069399e8e2b71b
Kylian Bainvel <kylian.bainvel@ovhcloud.com>
2025-07-15 14:23:16 +0200

    wip

--- packages/manager/apps/web-domains/src/alldoms/pages/service/serviceList/serviceList.tsx
+++ packages/manager/apps/web-domains/src/alldoms/pages/service/serviceList/serviceList.tsx
@@ -70,7 +70,7 @@ export default function ServiceList() {
       });
       setAlldomServices(services);
     }
-  }, [allDomList]);
+  }, [allDomList, listLoading, serviceList]);
 
   useEffect(() => {
     const debounce = setTimeout(() => {
