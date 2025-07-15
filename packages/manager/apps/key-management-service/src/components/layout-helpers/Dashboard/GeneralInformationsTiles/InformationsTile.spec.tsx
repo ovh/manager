40051a2da7e9f044c7950504a1c3ef7dd8c02115
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-07-15 08:57:24 +0200

    fix(okms): update create secret domain management
    ref: #MANAGER-18248
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

--- packages/manager/apps/key-management-service/src/components/layout-helpers/Dashboard/GeneralInformationsTiles/InformationsTile.spec.tsx
+++ packages/manager/apps/key-management-service/src/components/layout-helpers/Dashboard/GeneralInformationsTiles/InformationsTile.spec.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { describe, vi } from 'vitest';
 import { screen, render, waitFor } from '@testing-library/react';
-import { ServiceDetails } from '@ovh-ux/manager-react-components';
 import { OKMS } from '@/types/okms.type';
 import InformationsTile from './InformationsTile';
+import { okmsMock } from '@/mocks/kms/okms.mock';
+import { kmsServicesMock } from '@/mocks/services/services.mock';
 
 vi.mock('@ovh-ux/manager-react-shell-client', () => ({
   useOvhTracking: () => ({ trackClick: vi.fn() }),
@@ -21,39 +22,17 @@ vi.mock('react-router-dom', async () => {
 
 describe('InformationsTile component tests suite', () => {
   const kms: OKMS = {
-    id: 'id',
-    region: 'region',
-    kmipEndpoint: 'https://kmip-endpoint',
-    restEndpoint: 'https://rest-endpoint',
-    swaggerEndpoint: 'https://swagger-endpoint',
-    kmipObjectCount: 1,
-    serviceKeyCount: 2,
-    secretCount: 3,
+    ...okmsMock[0],
     iam: {
-      displayName: 'name',
-      id: 'id',
-      urn: 'urn:resource',
+      ...okmsMock[0].iam,
+      displayName: kmsServicesMock.resource.displayName,
     },
-  };
-
-  const serviceInfo: ServiceDetails = {
-    resource: {
-      displayName: 'kms display name',
-      name: 'name',
-      product: null,
-      resellingProvider: null,
-      state: null,
-    },
-    billing: null,
-    customer: null,
-    parentServiceId: null,
-    route: null,
-    serviceId: null,
-    tags: null,
   };
 
   const renderComponent = (okms: OKMS) =>
-    render(<InformationsTile okmsData={okms} okmsServiceInfos={serviceInfo} />);
+    render(
+      <InformationsTile okmsData={okms} okmsServiceInfos={kmsServicesMock} />,
+    );
 
   test('Should display information tile with only all mandatory data', async () => {
     const { container } = renderComponent(kms);
@@ -62,7 +41,9 @@ describe('InformationsTile component tests suite', () => {
       expect(
         screen.getByText('key_management_service_dashboard_field_label_name'),
       ).toBeVisible();
-      expect(screen.getByText(serviceInfo.resource.displayName)).toBeVisible();
+      expect(
+        screen.getByText(kmsServicesMock.resource.displayName),
+      ).toBeVisible();
 
       expect(
         screen.getByText('key_management_service_dashboard_field_label_id'),
