bada3c65f8d42f6d968f5646fa425def6b0866fb
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-07-15 16:46:19 +0200

    feat(manager-wiki): delete manager link story
    ref: #MANAGER-18712
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

---
+++ packages/manager-react-components/src/components/Link/Link.component.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { NAMESPACES } from '@ovh-ux/manager-common-translations';
+import {
+  Link as OdsLink,
+  Icon,
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TOOLTIP_POSITION,
+} from '@ovhcloud/ods-react';
+import { useTranslation } from 'react-i18next';
+import { LINK_TYPE, LinkProps, LinkIconsProps } from './Link.props';
+import { useAuthorizationIam } from '../../hooks';
+
+const LinkIcons: React.FC<LinkIconsProps> = ({ type, children }) => (
+  <>
+    {type === LINK_TYPE.back && <Icon name="arrow-left" />}
+    {children}
+    {type === LINK_TYPE.external && <Icon name="external-link" />}
+    {type === LINK_TYPE.next && <Icon name="arrow-right" />}
+  </>
+);
+
+export const Link: React.FC<LinkProps> = ({
+  children,
+  type,
+  href,
+  className = '',
+  iamActions,
+  urn,
+  disableIamCheck = false,
+  displayTooltip = true,
+  isIamTrigger = true,
+  ...props
+}) => {
+  const { t } = useTranslation(NAMESPACES.IAM);
+  const { isAuthorized } = useAuthorizationIam(
+    iamActions,
+    urn,
+    !disableIamCheck,
+  );
+
+  const getLinkProps = (isDisabled = false) => ({
+    className,
+    href,
+    disabled: isDisabled,
+    ...props,
+  });
+
+  if (isAuthorized || iamActions === undefined) {
+    return (
+      <OdsLink {...getLinkProps()}>
+        <LinkIcons type={type}>{children}</LinkIcons>
+      </OdsLink>
+    );
+  }
+
+  if (!displayTooltip) {
+    return (
+      <OdsLink {...getLinkProps(true)}>
+        <LinkIcons type={type}>{children}</LinkIcons>
+      </OdsLink>
+    );
+  }
+
+  return (
+    <Tooltip position={TOOLTIP_POSITION.bottom}>
+      <TooltipTrigger asChild>
+        <OdsLink {...getLinkProps(true)}>
+          <LinkIcons type={type}>{children}</LinkIcons>
+        </OdsLink>
+      </TooltipTrigger>
+      <TooltipContent>{t('iam_actions_message')}</TooltipContent>
+    </Tooltip>
+  );
+};
+
+export default Link;
