cc19d1259b5b676cbe3bc40b4193989f581aabf2
Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>
2025-07-16 16:35:52 +0200

    revert(billing): feature order status for awaiting documents and expired
    This reverts commit 81c7e9558cc9ce7b6ba1ef0a213dc55ad0508273.
    
    ref: #MANAGER-18616
    
    Signed-off-by: Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>

--- packages/manager/modules/billing/src/orders/orders/billing-orders.constant.js
+++
@@ -1,9 +0,0 @@
-export const BILLING_ORDERS_STATUS = {
-  NOT_PAID: 'notPaid',
-  ORDER_EXPIRED: 'orderExpired',
-  DOCUMENTS_REQUESTED: 'documentsRequested',
-};
-
-export default {
-  BILLING_ORDERS_STATUS,
-};
