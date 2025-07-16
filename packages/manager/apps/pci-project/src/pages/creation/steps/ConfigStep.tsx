4f8a6a948e8a30a995c93008e56961e34834f857
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2025-07-16 09:11:31 +0200

    feat(pci-project): implement project creation config step
    ref: #MANAGER-18793
    
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/apps/pci-project/src/pages/creation/steps/ConfigStep.tsx
+++ packages/manager/apps/pci-project/src/pages/creation/steps/ConfigStep.tsx
@@ -1,14 +1,115 @@
-import { OdsText } from '@ovhcloud/ods-components/react';
+import { NAMESPACES } from '@ovh-ux/manager-common-translations';
+import { OvhSubsidiary } from '@ovh-ux/manager-react-components';
+import {
+  OdsDivider,
+  OdsFormField,
+  OdsInput,
+  OdsText,
+} from '@ovhcloud/ods-components/react';
+import { Dispatch, SetStateAction } from 'react';
+import { useTranslation } from 'react-i18next';
+import Contracts from '@/components/contracts/Contracts';
+import HdsOption from '@/components/hds-option/HdsOption';
+import ItalyAgreements from '@/components/italy-agreements/ItalyAgreements';
+import { useGetCartSummary } from '@/data/hooks/useCart';
+import { Cart, OrderedProduct } from '@/data/types/cart.type';
+import { ConfigFormState } from '../hooks/useConfigForm';
+import { useHdsManagement } from '../hooks/useHdsManagement';
 
-export default function ConfigStep() {
+export type ConfigStepProps = {
+  cart: Cart;
+  cartProjectItem: OrderedProduct;
+  ovhSubsidiary: OvhSubsidiary;
+  form: ConfigFormState;
+  setForm: Dispatch<SetStateAction<ConfigFormState>>;
+};
+
+export default function ConfigStep({
+  cart,
+  cartProjectItem,
+  ovhSubsidiary,
+  form,
+  setForm,
+}: ConfigStepProps) {
+  const { t } = useTranslation(['new/config', 'NAMESPACES.FORM']);
+
+  const { cartId } = cart;
+
+  const {
+    data: cartSummary,
+    isFetching: isContractsFetching,
+  } = useGetCartSummary(cartId);
+
+  const {
+    shouldDisplayHdsSection,
+    isHdsPending,
+    handleHdsToggle,
+  } = useHdsManagement({
+    cartId,
+    projectItemId: cartProjectItem.itemId,
+    setForm,
+  });
+
+  const isPending = isHdsPending || isContractsFetching;
+
   return (
-    <div>
-      <OdsText preset="paragraph">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
-      </OdsText>
+    <div className="flex flex-col gap-8">
+      <OdsFormField
+        className="w-full"
+        error={
+          form.description.trim()
+            ? undefined
+            : t('error_required_field', { ns: NAMESPACES.FORM })
+        }
+      >
+        <OdsText className="font-bold" preset="caption">
+          {t('pci_project_new_config_description_label')}
+        </OdsText>
+
+        <OdsInput
+          name="project-description"
+          className="w-[20em] max-w-full"
+          value={form.description}
+          maxlength={255}
+          onOdsChange={(event) =>
+            setForm((prev) => ({
+              ...prev,
+              description: `${event.detail.value}`,
+            }))
+          }
+        />
+      </OdsFormField>
+
+      <OdsDivider />
+
+      {shouldDisplayHdsSection && (
+        <HdsOption
+          isChecked={form.isHdsChecked}
+          isDisabled={isPending}
+          onCheckChanged={handleHdsToggle}
+          isAlreadyCertifiedProject={false}
+          isValidForCertification={true}
+          isLightVersion
+        />
+      )}
+
+      <Contracts
+        contracts={cartSummary?.contracts}
+        isLoading={isPending}
+        isChecked={form.isContractsChecked}
+        onCheckChanged={(checked) =>
+          setForm((prev) => ({ ...prev, isContractsChecked: checked }))
+        }
+      />
+
+      {ovhSubsidiary === OvhSubsidiary.IT && (
+        <ItalyAgreements
+          hasAgreements={form.hasItalyAgreements}
+          onSetHasAgreements={(checked) =>
+            setForm((prev) => ({ ...prev, hasItalyAgreements: checked }))
+          }
+        />
+      )}
     </div>
   );
 }
