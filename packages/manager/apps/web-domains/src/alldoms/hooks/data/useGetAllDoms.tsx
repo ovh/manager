3c63f03ae48a1823ef87b10dff42c28f7e6dbc25
Kylian Bainvel <kylian.bainvel@ovhcloud.com>
2025-07-16 15:30:22 +0200

    feat(web-domains): use apiv2 instead of apiv6 and add searchbar
    ref: #MANAGER-18242
    
    Signed-off-by: Kylian Bainvel <kylian.bainvel@ovhcloud.com>

--- packages/manager/apps/web-domains/src/alldoms/hooks/data/useGetAllDoms.tsx
+++ packages/manager/apps/web-domains/src/alldoms/hooks/data/useGetAllDoms.tsx
@@ -1,51 +1,27 @@
 import { useQueries } from '@tanstack/react-query';
-import {
-  getAllDomResource,
-  getServiceInformation,
-} from '@/alldoms/data/api/web-domains';
-import { TServiceDetail, TServiceProperty } from '@/alldoms/types';
-import { findContact } from '@/alldoms/utils/utils';
-import {
-  ServiceInfoContactEnum,
-  ServiceRoutes,
-} from '@/alldoms/enum/service.enum';
+import { getServiceInformation } from '@/alldoms/data/api/web-domains';
+import { ServiceRoutes } from '@/alldoms/enum/service.enum';
 
 interface UseGetDatagridServiceInfoListProps {
-  readonly allDomList: TServiceProperty[];
+  readonly names: string[];
 }
 
 export const useGetAllDoms = ({
-  allDomList = [],
+  names,
 }: UseGetDatagridServiceInfoListProps) => {
-  const queries = useQueries({
-    queries: allDomList.map((allDom) => ({
-      queryKey: ['serviceInfoList', allDom.name],
-      queryFn: async () => {
-        const [serviceInfo, allDomResource] = await Promise.all([
-          getServiceInformation(allDom.name, ServiceRoutes.AllDom),
-          getAllDomResource(allDom.name),
-        ]);
-
-        const contacts = serviceInfo?.customer?.contacts ?? [];
-
-        return {
-          serviceInfo,
-          allDomResource,
-          nicAdmin: findContact(contacts, ServiceInfoContactEnum.Administrator),
-          nicBilling: findContact(contacts, ServiceInfoContactEnum.Billing),
-          nicTechnical: findContact(contacts, ServiceInfoContactEnum.Technical),
-          allDomResourceState:
-            serviceInfo?.billing?.lifecycle?.capacities?.actions[0],
-        };
-      },
-    })),
+  const { data, listLoading } = useQueries({
+    queries: names
+      ? names.map((name) => ({
+          queryKey: ['serviceInfo', name],
+          queryFn: () => getServiceInformation(name, ServiceRoutes.AllDom),
+        }))
+      : [],
+    combine: (results) => {
+      return {
+        listLoading: results.some((result) => result.isLoading),
+        data: results.map((result) => result.data),
+      };
+    },
   });
-
-  const data = queries
-    .map((query) => query.data)
-    .filter(Boolean) as TServiceDetail[];
-
-  const listLoading = queries.some((query) => query.isLoading);
-
   return { data, listLoading };
 };
