16894414320cd980bc3a274e043cd37084bfdc55
Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>
2025-07-17 16:34:04 +0200

    feat(web-hosting): remove regenerate action on active ssl
    ref: #WEB-20925
    
    Signed-off-by: Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>

--- packages/manager/apps/web-hosting/src/routes/routes.constants.ts
+++ packages/manager/apps/web-hosting/src/routes/routes.constants.ts
@@ -15,6 +15,5 @@ export const urls = {
   importSsl: `/${subRoutes.serviceName}/import-ssl`,
   orderSectigo: `/${subRoutes.serviceName}/order-sectigo`,
   disableSsl: `/${subRoutes.serviceName}/${subRoutes.domain}/disable-ssl`,
-  regenerateSsl: `/${subRoutes.serviceName}/${subRoutes.domain}/regenerate-ssl`,
   sanSsl: `/${subRoutes.serviceName}/${subRoutes.domain}/san-ssl`,
 };
