29bcb92f137e2ccb7d2f5249fcaf774b38f0afb3
Omar <omaralkabous@gmail.com>
2025-07-17 14:11:04 +0200

    feat(billing): remove manual renewal option from services renewal modal (#18147)
    ref: #MANAGER-18950
    
    Signed-off-by: Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>

--- packages/manager/modules/billing/src/autoRenew/actions/update/update.service.js
+++ packages/manager/modules/billing/src/autoRenew/actions/update/update.service.js
@@ -3,18 +3,22 @@ import { SERVICE_TYPES_USING_V6_SERVICES } from '../../autorenew.constants';
 
 export default class AutoUpdateRenewServiceModalService {
   /* @ngInject */
-  constructor($http) {
+  constructor($http, coreConfig) {
     this.$http = $http;
+    this.coreConfig = coreConfig;
   }
 
   getAvailableRenewPeriods(service) {
+    const isUSRegion = this.coreConfig.getRegion() === 'US';
+    const manualRenewMode = isUSRegion ? [] : [SERVICE_RENEW_MODES.MANUAL];
+
     const fetchPeriods = SERVICE_TYPES_USING_V6_SERVICES.includes(
       service.serviceType,
     )
       ? this.$http
           .get(`/services/${service.id}/renewPeriodCapacities`)
           .then(({ data }) => [
-            SERVICE_RENEW_MODES.MANUAL,
+            ...manualRenewMode,
             ...data.map(
               (period) => Number(period.match(/\d+/)?.[0]) || 'to_be_defined',
             ),
@@ -22,7 +26,7 @@ export default class AutoUpdateRenewServiceModalService {
       : this.$http
           .get(`${service.route.url}/serviceInfos`)
           .then(({ data }) => [
-            SERVICE_RENEW_MODES.MANUAL,
+            ...manualRenewMode,
             ...data.possibleRenewPeriod,
           ]);
 
