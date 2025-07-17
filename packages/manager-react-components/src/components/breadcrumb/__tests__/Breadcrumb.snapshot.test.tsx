7266ec69203630a7575c1187e994922b7d6db33b
aboungnaseng-ovhcloud <123390642+aboungnaseng-ovhcloud@users.noreply.github.com>
2025-07-17 12:05:00 +0200

    feat(manager-react-components): migrate Breadcrumb component (#17782)
    ref: #MANAGER-18707
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

---
+++ packages/manager-react-components/src/components/breadcrumb/__tests__/Breadcrumb.snapshot.test.tsx
@@ -0,0 +1,33 @@
+import { vitest } from 'vitest';
+import { render } from '../../../utils/test.provider';
+import { Breadcrumb } from '../Breadcrumb.component';
+
+vitest.mock('../../../hooks/breadcrumb/useBreadcrumb', () => ({
+  useBreadcrumb: vitest.fn(({ hideRootLabel }) => [
+    { label: 'vRack services', href: '/', hideLabel: hideRootLabel },
+  ]),
+}));
+
+describe('breadcrumb component snapshot', () => {
+  it('should render 3 breadcrumb items when hideRootLabel is false', () => {
+    const { asFragment } = render(
+      <Breadcrumb
+        rootLabel="vRack services"
+        appName="vrack-services"
+        hideRootLabel={false}
+      />,
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should hide root label when hideRootLabel is true', () => {
+    const { asFragment } = render(
+      <Breadcrumb
+        rootLabel="vRack services"
+        appName="vrack-services"
+        hideRootLabel={true}
+      />,
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
