1c22f2e159c4dcb37afe236ff67db526659293e7
stif59100 <steeve.vanderstocken@ovhcloud.com>
2025-07-17 11:48:46 +0200

    feat(*): add wordpress product icon
    ref: #MANAGER-18901
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

--- packages/manager/core/ovh-product-icons/icons/webIcons.ts
+++ packages/manager/core/ovh-product-icons/icons/webIcons.ts
@@ -1,10 +1,17 @@
-import { Hosting, Office365, Exchange, Zimbra } from '../assets-icon-svg/icons';
+import {
+  Hosting,
+  Office365,
+  Exchange,
+  Zimbra,
+  Wordpress,
+} from '../assets-icon-svg/icons';
 
 const webIcons = {
   Hosting,
   Office365,
   Exchange,
   Zimbra,
+  Wordpress,
 };
 
 export default webIcons;
