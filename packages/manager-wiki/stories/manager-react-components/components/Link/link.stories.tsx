565ff07edf6402c18b54393ee6899fc43ca076a7
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-07-17 22:48:27 +0200

    fix(manager-react-components): correct eslint naming convention
    ref: #MANAGER-18712
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-wiki/stories/manager-react-components/components/Link/link.stories.tsx
+++ packages/manager-wiki/stories/manager-react-components/components/Link/link.stories.tsx
@@ -7,21 +7,21 @@ const backLink: LinkProps = {
   children: 'Back to the list',
   href: 'https://www.ovhcloud.com',
   target: '_blank',
-  type: LINK_TYPE.back,
+  type: LinkType.back,
 };
 
 const nextLink: LinkProps = {
   href: 'https://www.ovhcloud.com/',
   target: '_blank',
   children: 'Next Page',
-  type: LINK_TYPE.next,
+  type: LinkType.next,
 };
 
 const externalLink: LinkProps = {
   href: 'https://www.ovhcloud.com/',
   target: '_blank',
   children: 'External Page',
-  type: LINK_TYPE.external,
+  type: LinkType.external,
 };
 
 const iamLinkWithAuthorization: LinkProps = {
