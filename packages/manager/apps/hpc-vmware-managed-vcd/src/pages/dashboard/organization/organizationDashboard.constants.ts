9152cbc32e1a10e7a56d32d60fc631a7ed78eb5f
David Arsène <david.arsene.ext@ovhcloud.com>
2025-07-17 17:37:52 +0200

    feat(managed-vcd): rename Managed VCD to Public VCF as-a-Service
    resolves: #MANAGER-18182
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/hpc-vmware-managed-vcd/src/pages/dashboard/organization/organizationDashboard.constants.ts
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/pages/dashboard/organization/organizationDashboard.constants.ts
@@ -1,4 +1,3 @@
-export const MANAGED_VCD_LABEL = 'Managed VCD';
 export const DATA_PROTECTION_BACKUP_LABEL = 'Managed Veeam Backup';
 export const DATA_PROTECTION_RECOVERY_LABEL =
   'Managed Disaster Recovery Services';
