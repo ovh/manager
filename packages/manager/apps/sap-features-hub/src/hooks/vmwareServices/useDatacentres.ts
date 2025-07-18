df9aa1a32c7252b508414cb36a4ef6217bd85ef8
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-07-18 18:09:17 +0200

    fix(sap-features-hub): prevent step1 selection while loading
    ref: #MANAGER-19010
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/sap-features-hub/src/hooks/vmwareServices/useDatacentres.ts
+++ packages/manager/apps/sap-features-hub/src/hooks/vmwareServices/useDatacentres.ts
@@ -1,16 +1,28 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+import { ApiError, IcebergFetchResultV6 } from '@ovh-ux/manager-core-api';
 import {
   getDatacentrePortGroup,
   getVMwareDatacentres,
   TGetDatacentreParams,
 } from '@/data/api/vmwareServices';
+import { VMwareDatacentre } from '@/types/vmwareService.type';
 
-export const useVMwareDatacentres = (serviceName: string) =>
+export const useVMwareDatacentres = (
+  serviceName: string,
+  options?: Partial<
+    UseQueryOptions<
+      IcebergFetchResultV6<VMwareDatacentre>,
+      ApiError,
+      VMwareDatacentre[]
+    >
+  >,
+) =>
   useQuery({
     queryKey: ['vmwareServices', serviceName, 'datacentres'],
     queryFn: () => getVMwareDatacentres(serviceName),
     select: (res) => res.data,
     enabled: !!serviceName,
+    ...options,
   });
 
 export const useDatacentrePortGroup = ({
