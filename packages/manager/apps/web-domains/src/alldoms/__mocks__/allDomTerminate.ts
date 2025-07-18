67dd021b8fd56c0e76e33e7e23fbaa758e33758d
Louis BENSI <louis.bensi.ext@corp.ovh.com>
2025-07-18 15:03:35 +0200

    feat(web-domains): alldoms improvements
    ref: #MANAGER-19029
    
    Signed-off-by: Louis BENSI <louis.bensi.ext@corp.ovh.com>
    Co-authored-by: Kylian Bainvel <kylian.bainvel@ovhcloud.com>

---
+++ packages/manager/apps/web-domains/src/alldoms/__mocks__/allDomTerminate.ts
@@ -0,0 +1,21 @@
+import { TServiceInfo } from '@/alldoms/types';
+import { LifecycleCapacitiesEnum } from '../enum/service.enum';
+
+export const allDomTerminate: TServiceInfo[] = [
+  {
+    billing: {
+      expirationDate: '2025-10-10T16:48:29Z',
+      lifecycle: {
+        current: {
+          pendingActions: [LifecycleCapacitiesEnum.EarlyRenewal],
+          creationDate: '2024-10-10T16:48:29Z',
+        },
+        capacities: { actions: [] },
+      },
+    },
+    serviceId: 128473986,
+    resource: {
+      name: 'test-domain.fr',
+    },
+  },
+];
