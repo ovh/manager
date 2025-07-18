a7c017d7beba44911c71a62de2209f0dfd477628
Stephane Ravet <ravet.st@gmail.com>
2025-07-18 11:00:54 +0200

    feat(pci-project): externalize CreditDetailsResponse from getCreditDetails
    ref: #MANAGER-18604
    
    Signed-off-by: Stephane Ravet <ravet.st@gmail.com>

--- packages/manager/apps/pci-project/src/data/api/credit.ts
+++ packages/manager/apps/pci-project/src/data/api/credit.ts
@@ -1,18 +1,6 @@
 import { v6 } from '@ovh-ux/manager-core-api';
 import { FetchResultV6 } from '@ovh-ux/manager-react-components';
-
-export interface CreditDetailsResponse {
-  voucher: string | null;
-  description: string;
-  available_credit: {
-    text: string;
-    value: number;
-  };
-  validity?: {
-    from: string;
-    to: string;
-  };
-}
+import { CreditDetailsResponse } from '@/data/types/credit.type';
 
 export const getCreditDetails = async (
   projectId: string,
