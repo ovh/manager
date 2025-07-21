191c9f5978766780e87b293ec0cb56f7279ce994
ovh-cds <ovh-cds@users.noreply.github.com>
2025-07-21 11:32:02 +0000

    docs: update documentation [skip ci]

---
+++ storybook-static/assets/PciMaintenanceBanner.stories-BFhrZGgK.js
@@ -0,0 +1,6 @@
+import{j as c}from"./jsx-runtime-BRNY0I4F.js";import{r as s}from"./manager-react-components-lib.es-BJqu3sSG.js";import"./index-Bnop-kX6.js";import"./iframe-PcZKOcr9.js";import"./QueryClientProvider-DbnhbVMg.js";import"./index-CPBFtX6f.js";import"./context-Cuu9iSAu.js";import"./index-D0sJu8id.js";const m=({projectName:n,maintenanceURL:a})=>c.jsx(s,{maintenanceURL:a,projectName:n}),r={args:{projectName:"myPciProjectTest",maintenanceURL:"https://ovh.com"}},x={title:"Manager React Components/Components/Public-cloud/PciMaintenanceBanner",component:m};var e,t,o;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    projectName: 'myPciProjectTest',
+    maintenanceURL: 'https://ovh.com'
+  }
+}`,...(o=(t=r.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const y=["Primary"];export{r as Primary,y as __namedExportsOrder,x as default};
--- storybook-static/assets/index-CPK5nrxT.js
+++
@@ -1,1 +0,0 @@
-import"./preview-Bix1RqIr.js";import"./DocsRenderer-CFRXHY34-Dgwtpi93.js";import"./index-Cc1f8nh7.js";__STORYBOOK_MODULE_PREVIEW_API__;
