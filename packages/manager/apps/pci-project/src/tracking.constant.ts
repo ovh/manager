da0e523c90e18634952eaad99477e6c8716c1ba9
Marc Monchablon <marc.monchablon.ext@corp.ovh.com>
2025-07-21 18:17:01 +0200

    feat(pci-project): update tracking
    ref: #MANAGER-18924
    
    Signed-off-by: Marc Monchablon <marc.monchablon.ext@corp.ovh.com>

--- packages/manager/apps/pci-project/src/tracking.constant.ts
+++ packages/manager/apps/pci-project/src/tracking.constant.ts
@@ -16,5 +16,28 @@ export const LEVEL2 = {
   },
 };
 export const UNIVERSE = 'PublicCloud';
-export const SUB_UNIVERSE = 'Creation';
-export const APP_NAME = 'pci-project';
+export const SUB_UNIVERSE = 'projects';
+export const APP_NAME = 'projects';
+
+export const PROJECTS_TRACKING = {
+  LISTING: {
+    PAGE_NAME: 'projects-management',
+    CTA_CREATE_PROJECT: ['page', 'button', 'create_project'],
+    SHOW_PROJECT: ['page', 'button', 'detail_project'],
+    DELETE_PROJECT: ['datagrid', 'button', 'delete_project'],
+  },
+  ONBOARDING: {
+    PAGE_NAME: 'projects-management',
+    CTA_CREATE_PROJECT: ['page', 'button', 'create_project'],
+  },
+  DELETE: {
+    PAGE_NAME: 'projects-management',
+    CTA_CANCEL: ['pop-up', 'button', 'delete_project', 'cancel'],
+    CTA_CONFIRM: ['pop-up', 'button', 'delete_project', 'confirm'],
+    REQUEST_FAIL: 'delete_project_error',
+    REQUEST_SUCCESS: 'delete_project_success',
+  },
+  PROJECT_HOME: {
+    PAGE_NAME: 'project-detail',
+  },
+};
