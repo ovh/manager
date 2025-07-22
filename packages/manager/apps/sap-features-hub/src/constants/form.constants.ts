957665a37c0be68030fd7df095f521bf6655bf02
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-07-22 12:13:09 +0200

    fix(sap-features-hub): fix step7 validation regex
    ref: #MANAGER-19010
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/sap-features-hub/src/constants/form.constants.ts
+++ packages/manager/apps/sap-features-hub/src/constants/form.constants.ts
@@ -5,8 +5,8 @@ export const CONTAINER_ID_MAX_LENGTH = 63;
 
 export const CONTAINER_ID_REGEX = /^[a-z0-9][A-Za-z0-9.-]+[a-z0-9]$/;
 export const ALPHANUMERIC_REGEX = /^[a-zA-Z0-9]+$/;
-export const LOGS_DATA_PLATFORM_REGEX = /^r"(gra|bhs)[0-9]+-[a-zA-Z0-9]+\.(gra|bhs)[0-9]+\.logs\.ovh\.com"$/;
-export const CERTIFICAT_REGEX = /^-----BEGIN CERTIFICATE-----.+-----END CERTIFICATE-----$/;
+export const LOGS_DATA_PLATFORM_REGEX = /^(gra|bhs)[0-9]+-[a-zA-Z0-9]+\.(gra|bhs)[0-9]+\.logs\.ovh\.com$/;
+export const CERTIFICAT_REGEX = /^-----BEGIN CERTIFICATE-----.+-----END CERTIFICATE-----$/s;
 export const ENDPOINT_REGEX = /^https:\/\/s3\.[a-z]+\.(io|perf)\.cloud\.ovh\.net\/?$/;
 
 export const FORM_LABELS = {
