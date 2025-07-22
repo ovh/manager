009fc71612f97f95abd6426b437defb8ac49f52e
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-07-22 11:35:01 +0200

    feat(dedicated): add zerto site page
    ref: #MANAGER-16205
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

---
+++ packages/manager/apps/dedicated/client/app/dedicatedCloud/datacenter/zerto/listing/add-site/dedicatedCloud-datacenter-zerto-add-site.routing.js
@@ -0,0 +1,35 @@
+export default /* @ngInject */ ($stateProvider) => {
+  $stateProvider.state(
+    'app.dedicatedCloud.details.datacenter.details.zerto.listing.addSite',
+    {
+      url: '/add-site',
+      views: {
+        'pccDatacenterView@app.dedicatedCloud.details.datacenter.details': {
+          component: 'dedicatedCloudDatacenterZertoAddSite',
+        },
+      },
+      params: {
+        zertoInformations: {},
+      },
+      resolve: {
+        breadcrumb: () => null,
+        zertoSiteId: /* @ngInject */ ($transition$) =>
+          $transition$.params().zertoSiteId,
+        zertoSite: /* @ngInject */ (zertoSiteId, zertoMultiSites) =>
+          zertoMultiSites.find(({ id }) => id === Number(zertoSiteId)),
+        goBackToListing: /* @ngInject */ ($state) => (reload = false) =>
+          $state.go(
+            'app.dedicatedCloud.details.datacenter.details.zerto.listing',
+            undefined,
+            { reload },
+          ),
+        handleSuccess: /* @ngInject */ (Alerter, goBackToListing) => (
+          message,
+        ) => {
+          Alerter.success(message, 'dedicatedCloud_dashboard_alert');
+          goBackToListing(true);
+        },
+      },
+    },
+  );
+};
