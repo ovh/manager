4652555467d9af17e8ff1a5f9ddd14d5ccedb6ba
David Arsène <david.arsene.ext@ovhcloud.com>
2025-07-22 18:56:39 +0200

    feat(managed-vcd): disable all actions for terminated service
    resolves: #MANAGER-19052
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

---
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/components/message/MessageSuspendedService.component.tsx
@@ -0,0 +1,30 @@
+import { NAMESPACES } from '@ovh-ux/manager-common-translations';
+import {
+  isStatusTerminated,
+  VCDResourceStatus,
+} from '@ovh-ux/manager-module-vcd-api';
+import { useTranslation } from 'react-i18next';
+import { Message } from './Message.component';
+
+export default function MessageSuspendedService({
+  status,
+}: {
+  status: VCDResourceStatus;
+}) {
+  const { t } = useTranslation(NAMESPACES.BILLING);
+
+  return isStatusTerminated(status) ? (
+    <div className="flex flex-col">
+      <Message
+        message={{
+          uid: Date.now(),
+          isDismissible: false,
+          type: 'warning',
+          content: t('cancel_service_success'),
+          includedSubRoutes: [],
+          excludedSubRoutes: [],
+        }}
+      />
+    </div>
+  ) : null;
+}
