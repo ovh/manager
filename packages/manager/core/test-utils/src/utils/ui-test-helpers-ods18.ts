11dab3063978c724cdbbd30df1534cc0fc4ec61c
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-07-22 15:24:34 +0200

    fix(core-test-utils): ods button test utils
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/core/test-utils/src/utils/ui-test-helpers-ods18.ts
+++ packages/manager/core/test-utils/src/utils/ui-test-helpers-ods18.ts
@@ -1,4 +1,4 @@
-import { ODS_ICON_NAME } from '@ovhcloud/ods-components';
+import { OsdsIcon } from '@ovhcloud/ods-components/react';
 import {
   screen,
   waitFor,
@@ -45,31 +45,49 @@ export const assertOdsModalText = ({
     { ...WAIT_FOR_DEFAULT_OPTIONS, ...options },
   );
 
-export const getOdsButtonByLabel = async ({
+type GetOdsButtonParams = {
+  container: HTMLElement;
+  label?: string;
+  altLabel?: string;
+  iconName?: React.ComponentProps<typeof OsdsIcon>['name'];
+  disabled?: boolean;
+  isLink?: boolean;
+  nth?: number;
+} & waitForOptions;
+
+const getOdsButton = async ({
   container,
   label,
   altLabel,
+  iconName,
   disabled,
   isLink,
   nth = 0,
   ...options
-}: {
-  container: HTMLElement;
-  label: string;
-  altLabel?: string;
-  disabled?: boolean;
-  isLink?: boolean;
-  nth?: number;
-} & waitForOptions) => {
+}: GetOdsButtonParams) => {
   let button: HTMLElement;
   await waitFor(
     () => {
-      const buttonList = container.querySelectorAll(
-        isLink ? 'ods-link' : 'ods-button',
-      );
-      button = Array.from(buttonList).filter((btn) =>
-        [label, altLabel].includes(btn.getAttribute('label')),
-      )[nth] as HTMLElement;
+      const htmlTag = isLink ? 'ods-link' : 'ods-button';
+      const buttonList = container.querySelectorAll<HTMLElement>(htmlTag);
+
+      if (iconName) {
+        // filter by icon
+        button = Array.from(buttonList).filter(
+          (btn) => btn.getAttribute('icon') === iconName,
+        )[nth];
+      } else {
+        // filter by label or altLabel
+        button = Array.from(buttonList).filter((btn) =>
+          [label, altLabel].includes(btn.getAttribute('label')),
+        )[nth];
+      }
+
+      if (isLink) {
+        return disabled
+          ? expect(button).toHaveAttribute('is-disabled', 'true')
+          : expect(button).not.toHaveAttribute('is-disabled', 'true');
+      }
       return disabled
         ? expect(button).toHaveAttribute('disabled')
         : expect(button).not.toHaveAttribute('disabled');
@@ -79,6 +97,32 @@ export const getOdsButtonByLabel = async ({
   return button;
 };
 
+type GetOdsButtonByLabelParams = Omit<GetOdsButtonParams, 'iconName'> & {
+  label: string;
+};
+
+export const getOdsButtonByLabel = async ({
+  container,
+  label,
+  altLabel,
+  disabled,
+  isLink,
+  nth = 0,
+  ...options
+}: GetOdsButtonByLabelParams) => {
+  return getOdsButton({
+    container,
+    label,
+    altLabel,
+    disabled,
+    isLink,
+    nth,
+    ...options,
+  });
+};
+
+type GetOdsButtonByIconParams = Omit<GetOdsButtonParams, 'label' | 'altLabel'>;
+
 export const getOdsButtonByIcon = async ({
   container,
   iconName,
@@ -86,26 +130,15 @@ export const getOdsButtonByIcon = async ({
   isLink,
   nth = 0,
   ...options
-}: {
-  container: HTMLElement;
-  iconName: ODS_ICON_NAME;
-  disabled?: boolean;
-  isLink?: boolean;
-  nth?: number;
-} & waitForOptions) => {
-  let button: HTMLElement;
-  await waitFor(
-    () => {
-      button = container.querySelectorAll(
-        `${isLink ? 'ods-link' : 'ods-button'}[icon="${iconName}"]`,
-      )?.[nth]?.parentElement;
-      return disabled
-        ? expect(button).toHaveAttribute('disabled')
-        : expect(button).not.toHaveAttribute('disabled');
-    },
-    { ...WAIT_FOR_DEFAULT_OPTIONS, ...options },
-  );
-  return button;
+}: GetOdsButtonByIconParams) => {
+  return getOdsButton({
+    container,
+    iconName,
+    disabled,
+    isLink,
+    nth,
+    ...options,
+  });
 };
 
 export const changeOdsInputValue = async ({
