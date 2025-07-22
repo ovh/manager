ec056df3b0605401eabc1c2d2ff4ab16cd2d1b6e
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-07-22 17:18:24 +0200

    feat(mrc): add tooltip on manager-tile labels
    resolves #17927
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager-react-components/src/components/content/ManagerTile/manager-tile-item.component.tsx
+++ packages/manager-react-components/src/components/content/ManagerTile/manager-tile-item.component.tsx
@@ -1,20 +1,27 @@
+import React, { PropsWithChildren } from 'react';
 import { ODS_TEXT_PRESET } from '@ovhcloud/ods-components';
 import { OdsText } from '@ovhcloud/ods-components/react';
-import React from 'react';
+import { HelpIconWithTooltip } from './help-icon-with-tooltip';
 
-export const ManagerTileItem = ({ children }: React.PropsWithChildren) => {
+export const ManagerTileItem = ({ children }: PropsWithChildren) => {
   return <dl className="flex flex-col gap-1 m-0">{children}</dl>;
 };
 
-const ManagerTileItemLabel = ({ children }: React.PropsWithChildren) => {
+type ManagerTileItemLabelProps = PropsWithChildren<{ tooltip?: string }>;
+
+const ManagerTileItemLabel = ({
+  children,
+  tooltip,
+}: ManagerTileItemLabelProps) => {
   return (
-    <dt>
+    <dt className="flex items-center gap-2">
       <OdsText preset={ODS_TEXT_PRESET.heading6}>{children}</OdsText>
+      {tooltip && <HelpIconWithTooltip label={tooltip} />}
     </dt>
   );
 };
 
-const ManagerTileItemDescription = ({ children }: React.PropsWithChildren) => {
+const ManagerTileItemDescription = ({ children }: PropsWithChildren) => {
   return <dd className="m-0">{children}</dd>;
 };
 
