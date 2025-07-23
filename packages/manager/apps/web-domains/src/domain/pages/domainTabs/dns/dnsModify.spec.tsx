f346eb791bb106fac6eb2fa9cfe374ee050071a4
Justine Loizel <justine.loizel@ovhcloud.com>
2025-07-23 09:32:11 +0200

    feat(web-domains): modification page - configuration choice
    ref: #MANAGER-18978
    
    Signed-off-by: Justine Loizel <justine.loizel@ovhcloud.com>
    Co-authored-by: Brendan Seiller <brendan.seiller@ovhcloud.com>

---
+++ packages/manager/apps/web-domains/src/domain/pages/domainTabs/dns/dnsModify.spec.tsx
@@ -0,0 +1,55 @@
+import '@/domain/setupTests';
+import { render } from '@testing-library/react';
+import { describe, expect, vi } from 'vitest';
+import React from 'react';
+import {
+  useGetDomainZone,
+  useGetDomainResource,
+} from '@/domain/hooks/data/query';
+import { wrapper } from '@/domain/utils/test.provider';
+import DnsModifyPage from './dnsModify';
+
+vi.mock('@/domain/hooks/data/query', () => ({
+  useGetDomainZone: vi.fn(),
+  useGetDomainResource: vi.fn(),
+}));
+
+vi.mock('@/domain/utils/dnsUtils', () => ({
+  computeActiveConfiguration: vi.fn(),
+}));
+
+vi.mock('@/domain/components/ModifyNameServer/DnsConfigurationRadio', () => ({
+  default: () => <div>Dns Configuration Radio Component</div>,
+}));
+
+describe('DnsModifyPage', () => {
+  it('Render loading component when data is still fetching', () => {
+    (useGetDomainZone as jest.Mock).mockReturnValue({
+      domainZone: {},
+      isFetchingdomainZone: true,
+    });
+    (useGetDomainResource as jest.Mock).mockReturnValue({
+      domainResource: {},
+      isFetchingDomainResource: true,
+    });
+    const { getByTestId } = render(<DnsModifyPage />, {
+      wrapper,
+    });
+    expect(getByTestId('listing-page-spinner')).toBeInTheDocument();
+  });
+
+  it('Render DnsModify page', () => {
+    (useGetDomainZone as jest.Mock).mockReturnValue({
+      domainZone: {},
+      isFetchingdomainZone: false,
+    });
+    (useGetDomainResource as jest.Mock).mockReturnValue({
+      domainResource: {},
+      isFetchingDomainResource: false,
+    });
+    const { getByTestId } = render(<DnsModifyPage />, {
+      wrapper,
+    });
+    expect(getByTestId('modify-component')).toBeInTheDocument();
+  });
+});
