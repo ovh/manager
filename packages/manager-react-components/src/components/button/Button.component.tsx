24f70dcec65197ebb0f37d1570b0d12b2e77f550
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-07-24 11:13:39 +0200

    feat(manager-react-components): migrate button component
    ref: #MANAGER-18865
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

---
+++ packages/manager-react-components/src/components/button/Button.component.tsx
@@ -0,0 +1,59 @@
+import {
+  Button as OdsButton,
+  ButtonProp,
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TOOLTIP_POSITION,
+} from '@ovhcloud/ods-react';
+import { useTranslation } from 'react-i18next';
+import { ButtonProps } from './Button.props';
+import './translations';
+
+import { useAuthorizationIam } from '../../hooks/iam';
+
+export const Button = ({
+  children,
+  iamActions,
+  urn,
+  displayTooltip = true,
+  tooltipPosition = TOOLTIP_POSITION.bottom,
+  isIamTrigger = true,
+  ...restProps
+}: ButtonProps & ButtonProp) => {
+  const { t } = useTranslation('iam');
+  const { isAuthorized } = useAuthorizationIam(iamActions, urn, isIamTrigger);
+
+  if (isAuthorized || !(iamActions && urn)) {
+    return (
+      <OdsButton data-testid="manager-button" {...restProps}>
+        {children}
+      </OdsButton>
+    );
+  }
+
+  return displayTooltip ? (
+    <Tooltip position={tooltipPosition}>
+      <TooltipTrigger data-testid="manager-button-tooltip" asChild>
+        <OdsButton
+          data-testid="manager-button-tooltip"
+          {...restProps}
+          disabled={true}
+          onClick={null}
+        >
+          {children}
+        </OdsButton>
+      </TooltipTrigger>
+      <TooltipContent>{t('common_iam_actions_message')}</TooltipContent>
+    </Tooltip>
+  ) : (
+    <OdsButton
+      data-testid="manager-button-without-tooltip"
+      {...restProps}
+      disabled={true}
+      onClick={null}
+    >
+      {children}
+    </OdsButton>
+  );
+};
