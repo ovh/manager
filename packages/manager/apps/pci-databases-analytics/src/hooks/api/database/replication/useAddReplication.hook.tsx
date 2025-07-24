04a8fb949e4d88cd58853adc2c81e5a7add64839
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2025-07-24 10:24:02 +0200

    feat(pci.databases): add datastreaming engines
    ref: #DATATR-509
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

---
+++ packages/manager/apps/pci-databases-analytics/src/hooks/api/database/replication/useAddReplication.hook.tsx
@@ -0,0 +1,43 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { CdbError } from '@/data/api/database';
+import * as database from '@/types/cloud/project/database';
+import {
+  addReplication,
+  IAddReplication,
+} from '@/data/api/database/replication.api';
+
+export interface UseAddReplication {
+  onError: (cause: CdbError) => void;
+  onSuccess: (replication: database.service.Replication) => void;
+}
+export function useAddReplication({
+  onError,
+  onSuccess: customOnSuccess,
+}: UseAddReplication) {
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: (replicationInfo: IAddReplication) => {
+      return addReplication(replicationInfo);
+    },
+    onError,
+    onSuccess: (data, variables) => {
+      queryClient.invalidateQueries({
+        queryKey: [
+          variables.projectId,
+          'database',
+          variables.engine,
+          variables.serviceId,
+          'replication',
+        ],
+      });
+      customOnSuccess(data);
+    },
+  });
+
+  return {
+    addReplication: (replicationInfo: IAddReplication) => {
+      return mutation.mutate(replicationInfo);
+    },
+    ...mutation,
+  };
+}
