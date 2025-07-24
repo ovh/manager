0a235f35790a5fba08eeac868d4b7c3821584017
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2025-07-24 09:34:40 +0200

    fix(account): added angular-messages dependency
    ref: #MANAGER-19220, #PRB0042689
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/modules/account/src/contacts/update/user-contacts-update.module.js
+++ packages/manager/modules/account/src/contacts/update/user-contacts-update.module.js
@@ -1,4 +1,5 @@
 import angular from 'angular';
+import 'angular-messages';
 
 import service from '../user-contacts.service';
 import routing from './user.contacts-update.routes';
@@ -6,7 +7,12 @@ import routing from './user.contacts-update.routes';
 const moduleName = 'UserAccountContactsUpdate';
 
 angular
-  .module(moduleName, ['oui', 'pascalprecht.translate', 'ui.router'])
+  .module(moduleName, [
+    'oui',
+    'pascalprecht.translate',
+    'ui.router',
+    'ngMessages',
+  ])
   .service('UserAccountContactUpdateService', service)
   .config(routing)
   .run(/* @ngTranslationsInject:json ./translations */);
