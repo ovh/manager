07b4bf6654a09a72b12de36e6a1798af6b7e4fe0
Simon Chaumet <simon.chaumet.ext@ovhcloud.com>
2025-07-24 12:25:08 +0200

    refactor(pci-workflow): move to common analysis config
    Signed-off-by: Simon Chaumet <simon.chaumet.ext@ovhcloud.com>

---
+++ packages/manager/apps/pci-workflow/eslint.config.mjs
@@ -0,0 +1,58 @@
+import { javascriptEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/javascript';
+import { typescriptEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/typescript';
+import { reactEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/react';
+// import { prettierEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/prettier';
+import { a11yEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/a11y';
+import {
+  complexityJsxTsxConfig,
+  complexityTsJsConfig,
+} from '@ovh-ux/manager-static-analysis-kit/eslint/complexity';
+import { htmlEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/html';
+import { cssEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/css';
+import { tailwindJsxConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/tailwind-jsx';
+import { tanStackQueryEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/tanstack';
+import { vitestEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/tests';
+import { tailwindSyntax } from '@eslint/css/syntax';
+
+export default [
+  javascriptEslintConfig,
+  {
+    ...typescriptEslintConfig,
+    rules: {},
+  },
+  reactEslintConfig,
+  a11yEslintConfig,
+  htmlEslintConfig,
+  tailwindJsxConfig,
+  tanStackQueryEslintConfig,
+  vitestEslintConfig,
+  // TODO
+  // prettierEslintConfig,
+  complexityJsxTsxConfig,
+  complexityTsJsConfig,
+  {
+    ...cssEslintConfig,
+    files: ['**/*.css', '**/*.scss'],
+    languageOptions: {
+      ...cssEslintConfig.languageOptions,
+      customSyntax: tailwindSyntax,
+    },
+  },
+  {
+    files: ['**/*.spec.*'],
+    rules: {
+      'max-nested-callbacks': 'off',
+      'max-lines-per-function': 'off',
+    },
+  },
+  {
+    files: ['src/pages/**/*.tsx'],
+    rules: {
+      // Disable for pages to allow ErrorBoundary export
+      'react/no-multi-comp': 'off',
+
+      'max-lines-per-function': 'off',
+      'max-lines': 'off',
+    },
+  },
+];
