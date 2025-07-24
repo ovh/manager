29d6e0c316f755b1ae82cb366e5468f9805935ff
Adrien Turmo <adrien.turmo.ext@ovhcloud.com>
2025-07-24 16:57:42 +0200

    fix(pci-load-balancer): update tracking delete load balancer
    ref: #TAPC-3830
    
    Signed-off-by: Adrien Turmo <adrien.turmo.ext@ovhcloud.com>

--- packages/manager/apps/pci-load-balancer/src/pages/delete/Delete.page.spec.tsx
+++ packages/manager/apps/pci-load-balancer/src/pages/delete/Delete.page.spec.tsx
@@ -1,6 +1,8 @@
-import { render } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 import { beforeEach, describe, it, vi } from 'vitest';
 import { UseQueryResult } from '@tanstack/react-query';
+import { useOvhTracking } from '@ovh-ux/manager-react-shell-client';
+import userEvent from '@testing-library/user-event';
 import DeletePage from '@/pages/delete/Delete.page';
 import { wrapper } from '@/wrapperRenders';
 import { TLoadBalancer } from '@/api/data/load-balancer';
@@ -13,6 +15,9 @@ vi.mock('@/api/hook/useLoadBalancer', async () => {
     useLoadBalancer: vi.fn(),
   };
 });
+
+const mockedTrackClick = vi.fn();
+vi.mocked(useOvhTracking().trackClick).mockImplementation(mockedTrackClick);
 
 describe('DeletePage', () => {
   beforeEach(() => {
@@ -37,5 +42,43 @@ describe('DeletePage', () => {
 
     const { getByText } = render(<DeletePage />, { wrapper });
     expect(getByText('octavia_load_balancer_delete_confirm')).toBeDisabled();
+  });
+
+  it('should track clicking on confirm', async () => {
+    vi.mocked(useLoadBalancer).mockReturnValue({
+      data: { id: 'lb1', name: 'Load Balancer 1', region: 'region' },
+      isPending: false,
+    } as UseQueryResult<TLoadBalancer>);
+
+    const { getByText } = render(<DeletePage />, { wrapper });
+    await act(() =>
+      userEvent.click(getByText('octavia_load_balancer_delete_confirm')),
+    );
+
+    expect(mockedTrackClick).toHaveBeenCalledWith({
+      actionType: 'action',
+      actions: ['delete_loadbalancer', 'confirm', 'region'],
+      buttonType: 'button',
+      location: 'pop-up',
+    });
+  });
+
+  it('should track clicking on cancel', async () => {
+    vi.mocked(useLoadBalancer).mockReturnValue({
+      data: { id: 'lb1', name: 'Load Balancer 1', region: 'region' },
+      isPending: false,
+    } as UseQueryResult<TLoadBalancer>);
+
+    const { getByText } = render(<DeletePage />, { wrapper });
+    await act(() =>
+      userEvent.click(getByText('octavia_load_balancer_delete_cancel')),
+    );
+
+    expect(mockedTrackClick).toHaveBeenCalledWith({
+      actionType: 'action',
+      actions: ['delete_loadbalancer', 'confirm', 'region'],
+      buttonType: 'button',
+      location: 'pop-up',
+    });
   });
 });
