79e0474883670f39b1bdbac0022b6585939cdd6e
Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>
2025-07-24 10:42:36 +0200

    feat(billing): update actions menu in my services page for us manager
    ref: #MANAGER-18779
    
    Signed-off-by: Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>

--- packages/manager/modules/billing-components/src/components/services-actions/services-actions.controller.js
+++ packages/manager/modules/billing-components/src/components/services-actions/services-actions.controller.js
@@ -22,10 +22,7 @@ export default class ServicesActionsCtrl {
     this.atInternet = atInternet;
     this.coreURLBuilder = coreURLBuilder;
     this.coreConfig = coreConfig;
-    this.billingLink = this.coreURLBuilder.buildURL(
-      'dedicated',
-      '#/billing/history',
-    );
+    this.billingLink = this.coreURLBuilder.buildURL('billing', '#/history');
 
     this.SERVICE_TYPE = SERVICE_TYPE;
     this.isLoading = true;
@@ -37,6 +34,7 @@ export default class ServicesActionsCtrl {
   $onInit() {
     this.initActionMenuClick();
     this.user = this.coreConfig.getUser();
+    this.isUSRegion = this.coreConfig.isRegion('US');
     this.BillingLinksService.generateAutorenewLinks(this.service, {
       billingManagementAvailability: this.billingManagementAvailability,
       getCommitmentLink: this.getCommitmentLink,
@@ -92,27 +90,31 @@ export default class ServicesActionsCtrl {
           !this.service.hasManualRenew() &&
           this.service.canHandleRenew() &&
           !this.service.canBeEngaged &&
-          !this.service.hasPendingEngagement;
+          !this.service.hasPendingEngagement &&
+          !this.isUSRegion;
         this.canDisplayRenewManuallyMenuEntry =
           this.service.hasManualRenew() &&
           !(
             typeof this.service.isInDebt === 'function' &&
             this.service.isInDebt()
           ) &&
-          this.service.canHandleRenew();
+          this.service.canHandleRenew() &&
+          !this.isUSRegion;
         this.canDisplayManageCommitmentMenuEntry =
           this.service.canBeEngaged &&
           !this.service.hasPendingEngagement &&
           !this.service.isSuspended();
         this.canDisplayCancelCommitmentMenuEntry = this.service.hasPendingEngagement;
         this.canDisplayExchangeSpecificMenuEntries =
-          this.service.serviceType === this.SERVICE_TYPE.EXCHANGE;
+          this.service.serviceType === this.SERVICE_TYPE.EXCHANGE &&
+          !this.isUSRegion;
         this.canDisplayXdslSpecificResiliationMenuEntry =
           this.service.serviceType === this.SERVICE_TYPE.PACK_XDSL &&
           !this.service.shouldDeleteAtExpiration() &&
           !this.service.isResiliated() &&
           !this.service.hasDebt() &&
-          !this.service.hasPendingResiliation();
+          !this.service.hasPendingResiliation() &&
+          !this.isUSRegion;
         this.canDisplayXdslResiliationMenuEntry =
           this.resiliateLink &&
           this.service.hasAdminRights(this.user.auth.account);
@@ -132,7 +134,8 @@ export default class ServicesActionsCtrl {
           this.autorenewLink &&
           (this.service.canBeDeleted() || this.service.isSuspendedHostingWeb());
         this.canDisplaySmsSpecificMenuEntries =
-          this.service.serviceType === this.SERVICE_TYPE.SMS;
+          this.service.serviceType === this.SERVICE_TYPE.SMS &&
+          !this.isUSRegion;
         this.canDisplayCancelResiliationMenuEntry =
           ![
             this.SERVICE_TYPE.VRACK,
