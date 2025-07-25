d23226ff8914e2ca78cce8f63913e2d82b56e4c7
Brendan Seiller <brendan.seiller@ovhcloud.com>
2025-07-25 10:15:17 +0200

    feat(web-domains): add button to order or terminate anycast option
    Signed-off-by: Brendan Seiller <brendan.seiller@ovhcloud.com>

---
+++ packages/manager/apps/web-domains/src/common/enum/common.enum.ts
@@ -0,0 +1,15 @@
+export enum ServiceInfoUpdateEnum {
+  Empty = 'empty',
+  TerminateAtEngagementDate = 'terminateAtEngagementDate',
+  TerminateAtExpirationDate = 'terminateAtExpirationDate',
+}
+export enum ServiceInfoRenewModeEnum {
+  Automatic = 'automatic',
+  Manual = 'manual',
+}
+export enum LifecycleCapacitiesEnum {
+  EarlyRenewal = 'earlyRenewal',
+  Terminate = 'terminate',
+  TerminateAtEngagementDate = 'terminateAtEngagementDate',
+  TerminateAtExpirationDate = 'terminateAtExpirationDate',
+}
