ce11028bdc3406d6d9d937cd217e4d9f7064176f
Brendan Seiller <brendan.seiller@ovhcloud.com>
2025-07-25 10:15:17 +0200

    feat(web-domains): add button to order or terminate anycast option
    Signed-off-by: Brendan Seiller <brendan.seiller@ovhcloud.com>

--- packages/manager/apps/web-domains/src/domain/data/api/domainResources.ts
+++ packages/manager/apps/web-domains/src/domain/data/api/domainResources.ts
@@ -1,5 +1,6 @@
-import { v2 } from '@ovh-ux/manager-core-api';
-import { TDomainResource } from '@/domain/types/domainResource';
+import { v2, v6 } from '@ovh-ux/manager-core-api';
+import { TDomainOption, TDomainResource } from '@/domain/types/domainResource';
+import { OptionEnum } from '@/common/enum/option.enum';
 
 /**
  *  : Get this Domain properties
@@ -8,5 +9,12 @@ export const getDomainResource = async (
   serviceName: string,
 ): Promise<TDomainResource> => {
   const { data } = await v2.get(`/domain/name/${serviceName}`);
+  return data;
+};
+
+export const getDomainAnycastOption = async (
+  serviceName: string,
+): Promise<TDomainOption> => {
+  const { data } = await v6.get(`/domain/${serviceName}/option/${OptionEnum.DNS_ANYCAST}`);
   return data;
 };
