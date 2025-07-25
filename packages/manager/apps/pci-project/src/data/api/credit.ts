cdf89d8b4cc89acf63668ab59e350c114ebb0f06
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2025-07-25 10:31:27 +0200

    feat(pci-project ): implement startup program section within project creation payement step
    ref: #MANAGER-18860
    
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/apps/pci-project/src/data/api/credit.ts
+++ packages/manager/apps/pci-project/src/data/api/credit.ts
@@ -1,6 +1,7 @@
 import { v6 } from '@ovh-ux/manager-core-api';
 import { FetchResultV6 } from '@ovh-ux/manager-react-components';
 import { CreditDetailsResponse } from '@/data/types/credit.type';
+import { TStartupProgram } from '../types/credit';
 
 export const getCreditDetails = async (
   projectId: string,
@@ -10,4 +11,14 @@ export const getCreditDetails = async (
   };
 
   return v6.get(`/cloud/project/${projectId}/credit`, { headers });
+};
+
+export const getCreditBalance = async (): Promise<string[]> => {
+  const { data } = await v6.get('/me/credit/balance');
+  return data;
+};
+
+export const getStartupProgram = async (): Promise<TStartupProgram> => {
+  const { data } = await v6.get('/me/credit/balance/STARTUP_PROGRAM');
+  return data;
 };
