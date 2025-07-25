95bb531254a07f99b8bda8a681c7b8c26e7dc6c1
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2025-07-25 09:43:34 +0200

    refactor(billing): move terminate agora service to billing components
    ref: #MANAGER-19185
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/modules/billing-components/src/components/terminate-agora-service/index.js
+++ packages/manager/modules/billing-components/src/components/terminate-agora-service/index.js
@@ -3,11 +3,10 @@ import angularTranslate from 'angular-translate';
 import ngAtInternet from '@ovh-ux/ng-at-internet';
 import ngTranslateAsyncLoader from '@ovh-ux/ng-translate-async-loader';
 import '@ovh-ux/ui-kit';
-import uiRouter from '@uirouter/angularjs';
 
-import routing from './routing';
+import component from './component';
 
-const moduleName = 'ovhManagerBillingAutorenewTerminateAgoraService';
+const moduleName = 'ovhManagerBillingComponentsAutorenewTerminateAgoraService';
 
 angular
   .module(moduleName, [
@@ -15,8 +14,8 @@ angular
     ngAtInternet,
     ngTranslateAsyncLoader,
     'oui',
-    uiRouter,
   ])
-  .config(routing);
+  .component(component.name, component)
+  .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
