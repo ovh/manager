2143e08a1962f77ea56015c5f531c9546f3645a8
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-07-29 15:25:08 +0200

    feat(ips): import ip from sys modal
    ref: #MANAGER-18857
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager/apps/ips/mocks/catalog/catalog.ts
+++ packages/manager/apps/ips/mocks/catalog/catalog.ts
@@ -1,16 +1,66 @@
 import { Handler } from '@ovh-ux/manager-core-test-utils';
 import { catalogIp } from './ip-catalog';
 import { catalogPcc } from './pcc-catalog';
+import {
+  availableDurations,
+  availableOperations,
+  catalogDedicatedServerList,
+  ipMigrationOrderRecap,
+  ipMigrationPostResponse,
+} from './dedicated-server-catalog';
 
 export type GetCatalogMocksParams = {
   isIpCatalogKo?: boolean;
   isPccCatalogKo?: boolean;
+  isDedicatedServerCatalogKo?: boolean;
+  isDedicatedServerIpMigrationAvailableDurationsKo?: boolean;
+  isIpMigrationUnavailableInOperations?: boolean;
 };
 
 export const getCatalogMocks = ({
   isIpCatalogKo,
   isPccCatalogKo,
+  isDedicatedServerCatalogKo,
+  isDedicatedServerIpMigrationAvailableDurationsKo,
+  isIpMigrationUnavailableInOperations,
 }: GetCatalogMocksParams): Handler[] => [
+  {
+    url: '/order/dedicated/server/:serviceName/ipMigration/:duration',
+    response: ipMigrationPostResponse,
+    api: 'v6',
+    method: 'post',
+  },
+  {
+    url: '/order/dedicated/server/:serviceName/ipMigration/:duration',
+    response: ipMigrationOrderRecap,
+    api: 'v6',
+  },
+  {
+    url: '/order/dedicated/server/:serviceName/ipMigration',
+    response: isDedicatedServerIpMigrationAvailableDurationsKo
+      ? {
+          message: 'Service expired',
+        }
+      : availableDurations,
+    api: 'v6',
+    status: isDedicatedServerIpMigrationAvailableDurationsKo ? 460 : 200,
+  },
+  {
+    url: '/order/dedicated/server/:serviceName',
+    response: availableOperations.filter(
+      (operation) =>
+        !isIpMigrationUnavailableInOperations || operation !== 'ipMigration',
+    ),
+    api: 'v6',
+  },
+  {
+    url: '/order/dedicated/server',
+    response: isDedicatedServerCatalogKo
+      ? { message: 'Dedicated Server Catalog KO' }
+      : catalogDedicatedServerList,
+    status: isDedicatedServerCatalogKo ? 400 : 200,
+    api: 'v6',
+  },
   {
     url: '/order/cartServiceOption/privateCloud/:serviceName',
     response: isPccCatalogKo ? { message: 'PCC Catalog KO' } : catalogPcc,
