3161ae164d9e880c1f5b85d13adb0f732773b35a
YASSINE JAAOUANE <yassine.jaaouane.ext@corp.ovh.com>
2025-07-29 10:37:00 +0200

    fix(vmware): breadcrumb logs
    ref: #MANAGER-18551
    
    Signed-off-by: yjaaouane <yassine.jaaouane.ext@corp.ovh.com>

--- packages/manager/apps/hpc-vmware-vsphere/src/hpc-vmware-vsphere.config.ts
+++ packages/manager/apps/hpc-vmware-vsphere/src/hpc-vmware-vsphere.config.ts
@@ -1,8 +1,12 @@
 export default {
+  appName: 'hpc-vmware-vsphere',
   listing: {
     datagrid: {
       serviceKey: 'vmware-vsphere',
     },
   },
   rootLabel: 'hpc-vmware-vsphere',
+  legacyApplication: 'dedicated',
+  legacyPath: 'dedicated_cloud',
+  dedicatedCloudTitle: 'Hosted Private Cloud',
 };
