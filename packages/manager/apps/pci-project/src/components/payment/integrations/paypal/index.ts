7c1de86fc15a5d124bdffc8977bb514d298cf078
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2025-07-30 10:04:24 +0200

    feat(pci-project): add Paypal payment method for project creation
    ref: #MANAGER-18803
    
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

---
+++ packages/manager/apps/pci-project/src/components/payment/integrations/paypal/index.ts
@@ -0,0 +1,4 @@
+export { usePayPalSDK } from './hooks/usePayPalSDK';
+export { usePayPalPayment } from './hooks/usePayPalPayment';
+export { PayPalButton } from './components/PayPalButton';
+export * from './types';
