a396207e777550abfc96d1f6b41fb81294c891ed
aderghamov <alexandre.dergham.ext@ovhcloud.com>
2025-07-30 10:26:15 +0200

    fix(ips): refreshing the ip-listing display when deleting a vmac
    ref: #MANAGER-19266
    
    Signed-off-by: aderghamov <alexandre.dergham.ext@ovhcloud.com>

--- packages/manager/apps/ips/src/data/hooks/ip/useGetIpVmacWithIp.ts
+++ packages/manager/apps/ips/src/data/hooks/ip/useGetIpVmacWithIp.ts
@@ -4,6 +4,7 @@ import {
   ApiResponse,
   IcebergFetchResultV6,
 } from '@ovh-ux/manager-core-api';
+import { useState } from 'react';
 import {
   DedicatedServerVmacType,
   getDedicatedServerVmacVirtualAddress,
@@ -21,11 +22,32 @@ export type VmacWithIpType = DedicatedServerVmacType & {
   ip: string[];
 };
 
+function arraysEqual(arr1: string[], arr2: string[]): boolean {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  const sortedArr1 = arr1.slice().sort();
+  const sortedArr2 = arr2.slice().sort();
+
+  return sortedArr1.reduce((acc, item, index) => {
+    if (!acc) {
+      return false;
+    }
+    return item === sortedArr2[index];
+  }, true);
+}
+
+const INVALIDATED_REFRESH_PERIOD = 5000;
+
 // Only for dedicated server for now
 export const useGetIpVmacWithIp = ({
   serviceName,
   enabled = true,
 }: UseGetIpVmacWithIpParams) => {
+  const [isInvalidated, setIsInvalidated] = useState(false);
+  const [invalidatedData, setInvalidatedData] = useState(undefined);
+
   const { data: dedicatedServerVmacResponse, isLoading, isError } = useQuery<
     IcebergFetchResultV6<DedicatedServerVmacType>,
     ApiError
@@ -34,6 +56,22 @@ export const useGetIpVmacWithIp = ({
     queryFn: () => getdedicatedServerVmac({ serviceName }),
     enabled,
     retry: false,
+    refetchInterval: (query) => {
+      const queryData = query.state.data?.data ?? [];
+      const macAdresses = queryData.map(({ macAddress }) => macAddress);
+      if (query.state.isInvalidated) {
+        setIsInvalidated(true);
+        setInvalidatedData(macAdresses);
+        return INVALIDATED_REFRESH_PERIOD;
+      }
+
+      if (!!invalidatedData && !arraysEqual(invalidatedData, macAdresses)) {
+        setIsInvalidated(false);
+        setInvalidatedData(undefined);
+      }
+
+      return isInvalidated ? INVALIDATED_REFRESH_PERIOD : undefined;
+    },
   });
 
   const results = useQueries({
