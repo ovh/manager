aa8617d9f6f2dc659c9fcf0f75db3989e6df3e37
Justine Loizel <justine.loizel@ovhcloud.com>
2025-07-30 15:41:38 +0200

    feat(web-domains): modification page - nameservers update
    ref: #MANAGER-18979
    
    Signed-off-by: Justine Loizel <justine.loizel@ovhcloud.com>
    Co-authored-by: Brendan Seiller <brendan.seiller@ovhcloud.com>

--- packages/manager/apps/web-domains/src/domain/types/domainResource.ts
+++ packages/manager/apps/web-domains/src/domain/types/domainResource.ts
@@ -30,7 +30,7 @@ export interface TDatagridDnsDetails {
   type: PublicNameServerTypeEnum;
 }
 
-interface DNSConfiguration {
+export interface DNSConfiguration {
   configurationType: DnsConfigurationTypeEnum;
   glueRecordIPv6Supported: boolean;
   hostSupported: boolean;
