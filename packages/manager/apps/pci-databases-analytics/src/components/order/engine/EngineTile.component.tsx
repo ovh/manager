384ff6200156b4941f3f2260bbe106706365e752
Arthur Bullet <arthur.bullet@ovhcloud.com>
2025-07-30 14:29:24 +0200

    feat(pci-databases-analytics): add eos information on engine card
    ref: #DATATR-2380
    
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>

--- packages/manager/apps/pci-databases-analytics/src/components/order/engine/EngineTile.component.tsx
+++ packages/manager/apps/pci-databases-analytics/src/components/order/engine/EngineTile.component.tsx
@@ -1,20 +1,24 @@
 import { useTranslation } from 'react-i18next';
 import { Badge, RadioTile } from '@datatr-ux/uxlib';
+import { format } from 'date-fns';
 import { Engine } from '@/types/orderFunnel';
 import { humanizeEngine } from '@/lib/engineNameHelper';
 import * as database from '@/types/cloud/project/database';
 import { getTagVariant } from '@/lib/tagsHelper';
 import { EngineIcon } from '@/components/engine-icon/EngineIcon.component';
+import { useDateFnsLocale } from '@/hooks/useDateFnsLocale.hook';
 
 export const EngineTile = ({ engine }: { engine: Engine }) => {
+  const locale = useDateFnsLocale();
   const { t } = useTranslation('pci-databases-analytics/components/engine');
+
   return (
     <RadioTile
       data-testid="engine-radio-tile"
       value={engine.name}
       className="flex flex-col gap-2 p-4"
     >
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center w-full">
         <div className="flex gap-2">
           <h5 className={'capitalize'}>
             {humanizeEngine(engine.name as database.EngineEnum)}
@@ -40,6 +44,13 @@ export const EngineTile = ({ engine }: { engine: Engine }) => {
       <p className="text-sm">
         {t(`description-${engine.name}`, engine.description)}
       </p>
+      {engine.eos && (
+        <p className="text-xs font-italic text-orange-500">
+          {t('endOfSale', {
+            date: format(new Date(engine.eos), 'PP', { locale }),
+          })}
+        </p>
+      )}
     </RadioTile>
   );
 };
