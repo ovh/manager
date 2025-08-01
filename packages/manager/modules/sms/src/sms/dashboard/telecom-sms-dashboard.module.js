568f957ab7e4c942544f1fc36631538c9ed7bba4
Atef ZAAFOURI <atefzed@gmail.com>
2025-08-01 17:45:05 +0200

    feat(sms): order time2chat number
    ref: #MANAGER-19120
    
    Signed-off-by: Atef ZAAFOURI <atefzed@gmail.com>

--- packages/manager/modules/sms/src/sms/dashboard/telecom-sms-dashboard.module.js
+++ packages/manager/modules/sms/src/sms/dashboard/telecom-sms-dashboard.module.js
@@ -3,6 +3,7 @@ import '@uirouter/angularjs';
 import ovhManagerAdvices from '@ovh-ux/manager-advices';
 
 import creditTransfer from './creditTransfer';
+import orderTime2Chat from './orderTime2Chat';
 
 import component from './telecom-sms-dashboard.component';
 import routing from './routing';
@@ -12,7 +13,12 @@ import './telecom-sms-dashboard.scss';
 const moduleName = 'ovhManagerSmsDashboardModule';
 
 angular
-  .module(moduleName, ['ui.router', creditTransfer, ovhManagerAdvices])
+  .module(moduleName, [
+    'ui.router',
+    creditTransfer,
+    orderTime2Chat,
+    ovhManagerAdvices,
+  ])
   .component(component.name, component)
   .config(routing)
   .run(/* @ngTranslationsInject:json ./translations */);
