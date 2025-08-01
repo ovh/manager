c68ff92a9010967655b87f358511f72c2fc925cd
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-08-01 12:20:11 +0200

    fix(okms): fix checkout order error message
    ref: #MANAGER-18309
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

--- packages/manager/apps/okms/src/data/hooks/useCheckoutOrder.ts
+++ packages/manager/apps/okms/src/data/hooks/useCheckoutOrder.ts
@@ -1,16 +1,17 @@
-import { ApiError, ApiResponse } from '@ovh-ux/manager-core-api';
+import { ApiResponse } from '@ovh-ux/manager-core-api';
 import {
   Order,
   postOrderCartCartIdCheckout,
 } from '@ovh-ux/manager-module-order';
 import { useMutation, UseMutationOptions } from '@tanstack/react-query';
+import { ErrorResponse } from '@/types/api.type';
 
 export const useCheckoutOrder = (
   options: Partial<
-    UseMutationOptions<ApiResponse<Order>, ApiError, { cartId: string }>
+    UseMutationOptions<ApiResponse<Order>, ErrorResponse, { cartId: string }>
   > = {},
 ) => {
-  return useMutation<ApiResponse<Order>, ApiError, { cartId: string }>({
+  return useMutation<ApiResponse<Order>, ErrorResponse, { cartId: string }>({
     mutationFn: ({ cartId }) =>
       postOrderCartCartIdCheckout({
         cartId,
