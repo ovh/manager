a0d71d2d28675efb544eb2b1354f8a4f290ed033
Anoop N <anoop.n@ovhcloud.com>
2025-08-01 13:10:14 +0530

    feat(manager-react-components): migrate TagsModal to ODS v19
    Migrate TagsModal to ODS v19 and integrate it as part of TagsList component
    Use Modal component from MRC instead of ODS modal directly
    Unit and Integration test coverage
    
    ref: #MANAGER-18968
    
    Signed-off-by: Anoop N <anoop.n@ovhcloud.com>
    
    BREAKING CHANGE: Tags Modal component is made internal to MRC Library
    Mandatory modalHeading prop for TagsList component
    onClick prop is removed from TagsList component

---
+++ packages/manager-react-components/src/components/tags-list/tags-modal/TagsModal.component.tsx
@@ -0,0 +1,86 @@
+import React, {
+  forwardRef,
+  ElementRef,
+  useState,
+  useCallback,
+  ForwardedRef,
+} from 'react';
+import { useTranslation } from 'react-i18next';
+import {
+  Button,
+  BUTTON_VARIANT,
+  BUTTON_SIZE,
+  Input,
+  MODAL_COLOR,
+  ModalContent,
+} from '@ovhcloud/ods-react';
+import { Modal } from '../../modal';
+import './translations';
+import { TagsStack } from '../tags-stack/TagsStack.component';
+import { TagsModalProps } from './TagsModal.props';
+
+export const TagsModal = forwardRef(
+  (
+    {
+      open = false,
+      heading,
+      tags,
+      onEditTags,
+      onCancel,
+      onOpenChange,
+    }: TagsModalProps,
+    ref: ForwardedRef<ElementRef<typeof ModalContent>>,
+  ) => {
+    const { t } = useTranslation('tags-modal');
+    const [search, setSearch] = useState('');
+    const [results, setResults] = useState<string[]>(tags);
+
+    const handleSearch = useCallback(() => {
+      setResults(search ? tags.filter((tag) => tag.includes(search)) : tags);
+    }, [search, tags]);
+
+    return (
+      <Modal
+        ref={ref}
+        type={MODAL_COLOR.neutral}
+        open={open}
+        heading={`Tags: ${heading}`}
+        {...(onEditTags && {
+          primaryButton: {
+            label: `${t('edit_tags')}`,
+            onClick: onEditTags,
+          },
+        })}
+        secondaryButton={{
+          label: `${t('back')}`,
+          onClick: onCancel,
+        }}
+        onOpenChange={onOpenChange}
+      >
+        <div className="flex w-full mb-4">
+          <Input
+            name="search"
+            className="rounded-l flex-1 mr-1"
+            placeholder={t('search_placeholder')}
+            type="text"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setSearch(event.target.value);
+            }}
+          />
+          <Button
+            variant={BUTTON_VARIANT.outline}
+            size={BUTTON_SIZE.sm}
+            onClick={handleSearch}
+          >
+            {t('search')}
+          </Button>
+        </div>
+        <div className="w-full min-w-[85px] max-h-[120px] overflow-auto">
+          {results && <TagsStack tags={results} />}
+        </div>
+      </Modal>
+    );
+  },
+);
+
+export default TagsModal;
