3d427c970859fa21a08f52544192975bd44c7229
ovh-cds <ovh-cds@users.noreply.github.com>
2025-08-01 08:02:47 +0000

    docs: update documentation [skip ci]

---
+++ storybook-static/assets/Messages_pt_PT-BFfhAIjW-CgULCCjJ.js
@@ -0,0 +1,1 @@
+const a="Fechar",e="Ocultar",s="Expandir",c={close:a,collapse:e,expand:s};export{a as close,e as collapse,c as default,s as expand};
--- storybook-static/assets/Messages_pt_PT-CF-5FkV4-BBnVlVsQ.js
+++
@@ -1,1 +0,0 @@
-const a="Fechar",e="Dobrar",s="Implementar",l={close:a,collapse:e,expand:s};export{a as close,e as collapse,l as default,s as expand};
