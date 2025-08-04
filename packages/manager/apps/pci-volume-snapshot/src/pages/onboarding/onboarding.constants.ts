ad6e83b76f0b5b038ea0d16165f98c9502c91266
Adrien Turmo <adrien.turmo.ext@ovhcloud.com>
2025-08-04 11:38:20 +0200

    fix(pci-volume-snapshot): change US onboarding guide
    ref: #TAPC-1577
    
    Signed-off-by: Adrien Turmo <adrien.turmo.ext@ovhcloud.com>

--- packages/manager/apps/pci-volume-snapshot/src/pages/onboarding/onboarding.constants.ts
+++ packages/manager/apps/pci-volume-snapshot/src/pages/onboarding/onboarding.constants.ts
@@ -1,2 +1,18 @@
-export const TRANSFER_VOLUME_BACKUP_FROM_ONE_DATACENTRE_TO_ANOTHER_URL =
-  'https://docs.ovh.com/gb/en/public-cloud/transfer_volume_backup_from_one_datacentre_to_another/';
+import { Subsidiary } from '@ovh-ux/manager-config';
+
+export type TGuide = {
+  id: string;
+  links: Partial<Record<Subsidiary, string>> & Record<'DEFAULT', string>;
+};
+
+export const ONBOARDING_GUIDES: TGuide[] = [
+  {
+    id: 'transfer-volume-backup',
+    links: {
+      US:
+        'https://support.us.ovhcloud.com/hc/en-us/articles/33015248904595-Downloading-and-Transferring-an-Instance-Backup-from-One-OpenStack-Region-to-Another',
+      DEFAULT:
+        'https://docs.ovh.com/gb/en/public-cloud/transfer_volume_backup_from_one_datacentre_to_another/',
+    },
+  },
+];
