9d5889f3f423301c49d74650e6bd91580cf1afb5
Stephane Ravet <ravet.st@gmail.com>
2025-08-04 09:30:22 +0200

    feat(pci-project): add home/dashboard
    ref: #MANAGER-18604
    
    Signed-off-by: Stephane Ravet <ravet.st@gmail.com>

--- packages/manager/apps/pci-project/src/pages/home/Home.page.tsx
+++ packages/manager/apps/pci-project/src/pages/home/Home.page.tsx
@@ -1,15 +1,101 @@
+import { useMemo } from 'react';
+import { useParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+
 import { OdsDivider } from '@ovhcloud/ods-components/react';
-import { Subtitle } from '@ovh-ux/manager-react-components';
+
+import { QuickAccess } from './components/QuickAccess.component';
+import { QuickAccessItem } from './components/QuickAccessCard.component';
+import { Others, OtherActionItem } from './components/Others.component';
+import { DiscoveryBanner } from './components/DiscoveryBanner.component';
+import DashboardTiles from './components/DashboardTiles.component';
+
+import IconInstances from '@/assets/home/instances.svg?url';
+import IconKubernetes from '@/assets/home/managed_kubernetes_service.svg?url';
+import IconObjectStorage from '@/assets/home/object_storage.svg?url';
+import IconNetwork from '@/assets/home/vrack_private_network.svg?url';
+import IconDatabase from '@/assets/home/managed_mongodb.svg?url';
+import IconBlockStorage from '@/assets/home/block_storage.svg?url';
 
 export default function Home() {
-  const { t } = useTranslation(['home']);
+  const { t } = useTranslation('project');
+  const { projectId } = useParams<{ projectId: string }>();
+
+  const quickAccessItems: QuickAccessItem[] = useMemo(
+    () => [
+      {
+        icon: IconInstances,
+        title: t('pci_project_project_instances'),
+        description: t('pci_project_project_create_instance'),
+        link: `instances/new`,
+      },
+      {
+        icon: IconKubernetes,
+        title: t('pci_project_project_kubernetes'),
+        description: t('pci_project_project_create_cluster'),
+        link: `kubernetes/new`,
+      },
+      {
+        icon: IconObjectStorage,
+        title: t('pci_project_project_object_storage'),
+        description: t('pci_project_project_create_container'),
+        link: `storages/objects/new`,
+      },
+      {
+        icon: IconBlockStorage,
+        title: t('pci_project_project_block_storage'),
+        description: t('pci_project_project_create_volume'),
+        link: `storages/blocks`,
+      },
+      {
+        icon: IconNetwork,
+        title: t('pci_project_project_network'),
+        description: t('pci_project_project_manage_vrack'),
+        link: `private-networks`,
+      },
+      {
+        icon: IconDatabase,
+        title: t('pci_project_project_database'),
+        description: t('pci_project_project_create_database'),
+        link: `databases-analytics/operational/services/new`,
+      },
+    ],
+    [t],
+  );
+
+  const otherActionsItems: OtherActionItem[] = useMemo(
+    () => [
+      {
+        icon: 'book',
+        label: t('pci_project_project_create_ai_notebook'),
+        link: `ai-ml/auth`,
+      },
+      {
+        icon: 'network',
+        label: t('pci_project_project_create_load_balancer'),
+        link: `octavia-load-balancer/load-balancers`,
+      },
+      {
+        icon: 'bill',
+        label: t('pci_project_project_billing'),
+        link: `billing`,
+      },
+      {
+        icon: 'cog',
+        label: t('pci_project_project_quotas'),
+        link: `quota`,
+      },
+    ],
+    [t],
+  );
 
   return (
     <>
-      <Subtitle>{t('quick_access')}</Subtitle>
-
-      <OdsDivider />
+      <DiscoveryBanner className="mb-6 w-full" />
+      <QuickAccess items={quickAccessItems} />
+      <Others items={otherActionsItems} />
+      <OdsDivider className="my-8 block" />
+      {projectId && <DashboardTiles projectId={projectId} />}
     </>
   );
 }
