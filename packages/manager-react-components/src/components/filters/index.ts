1e3d2a0b05229d722af76a319ca2b64934033cc1
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2025-08-04 21:10:34 +0200

    feat(core): export filter tag form
    ref: #MANAGER-15304
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

--- packages/manager-react-components/src/components/filters/index.ts
+++ packages/manager-react-components/src/components/filters/index.ts
@@ -1,3 +1,4 @@
 export { FilterAdd } from './filter-add.component';
 export { FilterList } from './filter-list.component';
 export { useColumnFilters } from './useColumnFilters';
+export * from './tags-filter-form.component';
