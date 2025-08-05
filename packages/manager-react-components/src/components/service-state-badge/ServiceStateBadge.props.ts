89741813305ebb261549384dc9a520094bfb79bf
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-08-05 20:26:54 +0200

    feat(manager-react-components): delete deprecated components
    ref: #MANAGER-19193
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-react-components/src/components/service-state-badge/ServiceStateBadge.props.ts
+++ packages/manager-react-components/src/components/service-state-badge/ServiceStateBadge.props.ts
@@ -1,6 +1,13 @@
 import { ComponentProps } from 'react';
-import { ResourceStatus } from '../../hooks/services/services.type';
 import { Badge } from '../badge';
+
+export type ResourceStatus =
+  | 'active'
+  | 'deleted'
+  | 'suspended'
+  | 'toActivate'
+  | 'toDelete'
+  | 'toSuspend';
 
 export type ServiceStateBadgeProps = Omit<
   ComponentProps<typeof Badge>,
