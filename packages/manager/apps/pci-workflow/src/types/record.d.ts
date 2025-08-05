95b0717f3692da813561ffcf64aef46126d89c2f
Simon Chaumet <simon.chaumet.ext@ovhcloud.com>
2025-08-05 12:36:33 +0200

    feat(pci-worklow): swap name and scheduling steps
    ref: #TAPC-3054
    
    Signed-off-by: Simon Chaumet <simon.chaumet.ext@ovhcloud.com>

---
+++ packages/manager/apps/pci-workflow/src/types/record.d.ts
@@ -0,0 +1,7 @@
+declare type RecursivePartial<T> = {
+  [P in keyof T]?: T[P] extends (infer U)[]
+    ? RecursivePartial<U>[]
+    : T[P] extends object | undefined
+      ? RecursivePartial<T[P]>
+      : T[P];
+};
