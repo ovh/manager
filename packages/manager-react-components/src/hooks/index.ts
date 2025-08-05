734d4c34fa3545d6afe130baeabb59e36d058c79
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-08-05 20:26:54 +0200

    feat(manager-react-components): delete deprecated components
    ref: #MANAGER-19193
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-react-components/src/hooks/index.ts
+++ packages/manager-react-components/src/hooks/index.ts
@@ -1,16 +1,9 @@
-import { TRegion as Region } from './useProjectRegions';
-
 export * from './breadcrumb/useBreadcrumb';
 export * from './useCatalogPrice';
 export * from './useMe';
-export * from './useProjectRegions';
-export * from './useProjectUrl';
-export * from './feature-availability';
 export * from './datagrid/useIcebergV2';
 export * from './datagrid/useIcebergV6';
 export * from './datagrid/useResourcesV6';
-export * from './services';
-export * from './tasks';
 export * from './date';
 export * from './iam';
 export * from './bytes/useBytes';
@@ -19,5 +12,3 @@ export {
   useTranslatedMicroRegions,
   isLocalZone,
 } from './region/useTranslatedMicroRegions';
-
-export type TRegion = Region;
