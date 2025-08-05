a75492740d7efe372815bf3d6bb95e0b7baf948a
Lauren Olivier <laurenzolivier@gmail.com>
2025-08-05 09:45:39 +0200

    feat(pci-instances): add breadcrumb to instance creation page
    ref: #TAPC-4539
    
    Signed-off-by: Lauren Olivier <laurenzolivier@gmail.com>

--- packages/manager/apps/pci-instances/src/pages/instances/create/CreateInstance.page.tsx
+++ packages/manager/apps/pci-instances/src/pages/instances/create/CreateInstance.page.tsx
@@ -1,11 +1,28 @@
 import { PageLayout } from '@ovh-ux/manager-react-components';
 import { FC } from 'react';
-import { GoBack } from '@/components/navigation/GoBack.component';
+import { useRouteLoaderData } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { TProject } from '@ovh-ux/manager-pci-common';
+import { Breadcrumb } from '@/components/breadcrumb/Breadcrumb.component';
 
-const CreateInstance: FC = () => (
-  <PageLayout>
-    <GoBack />
-  </PageLayout>
-);
+const CreateInstance: FC = () => {
+  const project = useRouteLoaderData('root') as TProject | undefined;
+  const { t } = useTranslation('common');
+
+  const breadcrumbItems = [
+    { label: t('pci_instances_common_create_instance') },
+  ];
+
+  return (
+    <PageLayout>
+      {project && (
+        <Breadcrumb
+          projectLabel={project.description ?? ''}
+          items={breadcrumbItems}
+        />
+      )}
+    </PageLayout>
+  );
+};
 
 export default CreateInstance;
