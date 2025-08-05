26bf208bf4d0579d6f74bc3a94bdb0a3db2d55b2
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-08-05 17:13:50 +0200

    feat(dedicated): update zerto configuration alert message
    ref: #MANAGER-17090
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/managedBaremetal/datacenter/zerto/listing/add-site/index.js
+++ packages/manager/apps/dedicated/client/app/managedBaremetal/datacenter/zerto/listing/add-site/index.js
@@ -8,7 +8,7 @@ const moduleName =
 angular.module(moduleName, ['ui.router', 'oc.lazyLoad']).config(
   /* @ngInject */ ($stateProvider) => {
     $stateProvider.state(
-      'app.managedBaremetal.details.datacenters.datacenter.zerto.addSite.**',
+      'app.managedBaremetal.details.datacenters.datacenter.zerto.listing.addSite.**',
       {
         url: '/add-site',
         lazyLoad: ($transition$) => {
