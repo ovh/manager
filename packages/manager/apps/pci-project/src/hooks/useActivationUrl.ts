b29af2089117b0d8c0cf09ebac2ea0ad4fad09b1
Stephane Ravet <ravet.st@gmail.com>
2025-08-05 14:25:20 +0200

    refactor(pci-project): refactor useProjectId
    ref: #MANAGER-18604
    
    Signed-off-by: Stephane Ravet <ravet.st@gmail.com>

--- packages/manager/apps/pci-project/src/hooks/useActivationUrl.ts
+++ packages/manager/apps/pci-project/src/hooks/useActivationUrl.ts
@@ -1,14 +1,12 @@
 import { useCallback, useContext } from 'react';
-import { useParams } from 'react-router-dom';
 import { ShellContext } from '@ovh-ux/manager-react-shell-client';
+import { useProjectIdFromParams } from '@/hooks/useProjectIdFromParams';
 
 export const useActivationUrl = () => {
-  const { projectId } = useParams<{ projectId: string }>();
+  const projectId = useProjectIdFromParams();
   const { shell } = useContext(ShellContext);
 
   const goToActivation = useCallback(async () => {
-    if (!projectId) return;
-
     await shell.navigation.navigateTo(
       'public-cloud',
       `#/pci/projects/${projectId}/activate`,
