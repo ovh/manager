15447c8b28efec52fb8d278b150881e396806e37
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-08-05 20:26:54 +0200

    feat(manager-react-components): delete deprecated components
    ref: #MANAGER-19193
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-react-components/src/hooks/tasks/index.ts
+++
@@ -1,5 +0,0 @@
-/**
- * @deprecated This file is deprecated. Do not use any of its exports.
- * @deprecated file will be removed in MRC v3, all code will be move in @ovh-ux/manager-module-common-api' or already moved
- */
-export * from './hooks/useTask';
