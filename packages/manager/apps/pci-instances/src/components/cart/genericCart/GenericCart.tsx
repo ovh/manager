757432322083d1f8d1bb95575e023347636f0bdb
Lauren Olivier <laurenzolivier@gmail.com>
2025-08-06 17:24:04 +0200

    feat(pci-instances): add instance name input with form
    ref: #TAPC-4541
    
    Signed-off-by: Lauren Olivier <laurenzolivier@gmail.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

---
+++ packages/manager/apps/pci-instances/src/components/cart/genericCart/GenericCart.tsx
@@ -0,0 +1,67 @@
+import {
+  Accordion,
+  AccordionChangeDetail,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+  Text,
+} from '@ovhcloud/ods-react';
+import clsx from 'clsx';
+import { useCallback, useState } from 'react';
+
+export type TCartProduct = {
+  title: string;
+  designation?: string;
+  expanded?: boolean;
+};
+
+type TCartProps = {
+  products: TCartProduct[];
+};
+
+export const GenericCart = ({ products }: TCartProps) => {
+  const [expandedAccordions, setExpandedAccordions] = useState<string[]>(
+    products
+      .filter((product) => product.expanded)
+      .map((product) => product.title),
+  );
+
+  const handleAccordionChange = useCallback(
+    (accordion: AccordionChangeDetail) =>
+      setExpandedAccordions(accordion.value),
+    [setExpandedAccordions],
+  );
+  const isExpanded = useCallback(
+    (accordion: string) => expandedAccordions.includes(accordion),
+    [expandedAccordions],
+  );
+
+  return (
+    <div className="rounded-md border border-solid border-gray-400 shadow-lg p-6">
+      <Accordion value={expandedAccordions} onChange={handleAccordionChange}>
+        {products.map((product) => (
+          <AccordionItem key={product.title} value={product.title}>
+            <AccordionTrigger
+              className={clsx(
+                'border border-gray-200',
+                isExpanded(product.title) ? 'rounded-t-lg' : 'rounded-sm',
+              )}
+            >
+              <Text preset="paragraph" className="text-[--ods-color-heading]">
+                {product.title.toUpperCase()}
+              </Text>
+              {product.designation && (
+                <Text preset="label" className="text-[--ods-color-heading]">
+                  {product.designation}
+                </Text>
+              )}
+            </AccordionTrigger>
+            <AccordionContent className="bg-[--ods-color-neutral-050] py-5 px-8">
+              content
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </div>
+  );
+};
