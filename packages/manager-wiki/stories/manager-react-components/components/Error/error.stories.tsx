6fd38d54858191ddd4004299fff1b25b8cc95ac8
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-08-06 22:47:31 +0200

    feat(manager-react-components): migrate error component
    ref: #MANAGER-19050
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

---
+++ packages/manager-wiki/stories/manager-react-components/components/Error/error.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Meta } from '@storybook/react';
+import { Error, ErrorProps } from '@ovh-ux/manager-react-components';
+
+const defaultError: ErrorProps = {
+  error: {},
+};
+
+const error404: ErrorProps = {
+  error: {
+    status: 404,
+  },
+};
+
+const errorApiWithCode: ErrorProps = {
+  error: {
+    status: 401,
+    data: {
+      message: '[serviceId] Given data (undefined) is not valid for type long',
+    },
+    headers: { 'x-ovh-queryid': '123456789' },
+  },
+};
+
+const meta: Meta<typeof Error> = {
+  title: 'Manager React Components/Components/Errors',
+  decorators: [(story) => <div>{story()}</div>],
+  component: Error,
+  argTypes: {},
+  args: defaultError,
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export default meta;
+
+export const ErrorBoundary = () => <Error {...defaultError} />;
+export const Error404 = () => <Error {...error404} />;
+export const ErrorApiWithCode = () => <Error {...errorApiWithCode} />;
