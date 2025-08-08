16819a47d242cc22ad13c7546e670fcc0ea85ca9
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2025-08-08 10:02:11 +0200

    feat(iam): breadcrumb and tag manager tracking
    ref: #MANAGER-15882, #MANAGER-19400
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

--- packages/manager/apps/identity-access-management/src/pages/tagManager/assignTag/components/AssignTagTopbar.component.tsx
+++ packages/manager/apps/identity-access-management/src/pages/tagManager/assignTag/components/AssignTagTopbar.component.tsx
@@ -3,9 +3,15 @@ import { ODS_BUTTON_VARIANT, ODS_ICON_NAME } from '@ovhcloud/ods-components';
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
+import {
+  ButtonType,
+  PageType,
+  useOvhTracking,
+} from '@ovh-ux/manager-react-shell-client';
 import { formatTagsForApi } from '@/utils/formatTagsForApi';
 import { useUpdateIamResources } from '@/data/hooks/useIamResources';
 import { useResourcesDatagridContext } from '@/components/resourcesDatagrid/ResourcesDatagridContext';
+import { TrackPageName } from '@/tracking.constant';
 
 export type AssignTagTopbarProps = {
   tags: string[];
@@ -20,20 +26,33 @@ export default function AssignTagTopbar({
   const { t } = useTranslation('tag-manager');
   const { selectedResourcesList } = useResourcesDatagridContext();
   const navigate = useNavigate();
-  const { mutate: updateResource, isSuccess } = useUpdateIamResources();
+  const { trackClick, trackPage } = useOvhTracking();
+  const { mutate: updateResource } = useUpdateIamResources({
+    onSuccess: () => {
+      trackPage({
+        pageName: TrackPageName.TAG_MANAGEMENT_ASSIGN_TAG,
+        pageType: PageType.bannerSuccess,
+      });
+      navigate(onSuccessUrl);
+    },
+    onError: () => {
+      trackPage({
+        pageName: TrackPageName.TAG_MANAGEMENT_ASSIGN_TAG,
+        pageType: PageType.bannerError,
+      });
+    },
+  });
 
   const onClick = () => {
+    trackClick({
+      actionType: 'action',
+      actions: ['datagrid', ButtonType.button, 'assign_tag'],
+    });
     updateResource({
       resources: selectedResourcesList,
       tags: formatTagsForApi(tags),
     });
   };
-
-  useEffect(() => {
-    if (isSuccess && onSuccessUrl) {
-      navigate(onSuccessUrl);
-    }
-  }, [isSuccess]);
 
   return (
     <ManagerButton
