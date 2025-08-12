78da9f0805b0df7127e048defcbdd918f7d67fc6
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-08-12 14:38:43 +0200

    feat(manager-react-components): deprecated tile block component
    ref: #MANAGER-19107
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-react-components/src/components/content/dashboard-tile/tile-block.component.tsx
+++ packages/manager-react-components/src/components/content/dashboard-tile/tile-block.component.tsx
@@ -1,15 +1,31 @@
+/**
+ * @deprecated use ManagerTile instead
+ * TileBlock is a deprecated component, It will be removed in MRC V3
+ */
 import { PropsWithChildren } from 'react';
 import { ODS_TEXT_PRESET } from '@ovhcloud/ods-components';
 import { OdsText } from '@ovhcloud/ods-components/react';
 import { HelpIconWithTooltip } from '../ManagerTile/help-icon-with-tooltip';
 
+/**
+ * TileBlock
+ * @deprecated component, use ManagerTile instead.
+ */
 export type TileBlockOptions = {
   label?: string;
   labelTooltip?: string;
 };
 
+/**
+ * TileBlock
+ * @deprecated component, use ManagerTile instead.
+ */
 export type TileBlockProps = PropsWithChildren<TileBlockOptions>;
 
+/**
+ * TileBlock
+ * @deprecated component, use ManagerTile instead.
+ */
 export const TileBlock = ({
   label,
   labelTooltip,
