22d87d85775f37f36a2e39d7e3be3797faca86f6
Fabien Henon <fabien.henon.ext@corp.ovh.com>
2025-08-12 15:49:48 +0200

    refactor(pci-project): always handle challenge from PaymentMethods component
    ref: #MANAGER-18805
    
    Signed-off-by: Fabien Henon <fabien.henon.ext@corp.ovh.com>

--- packages/manager/apps/pci-project/src/pages/creation/steps/PaymentStep.tsx
+++ packages/manager/apps/pci-project/src/pages/creation/steps/PaymentStep.tsx
@@ -57,7 +57,6 @@ export default function PaymentStep({
         handleSetAsDefaultChange={() => {}}
         paymentMethodHandler={() => {}}
         handleValidityChange={() => {}}
-        handlePaymentMethodChallenge={true}
       />
 
       {isStartupProgramAvailable && startupProgramAmountText && (
