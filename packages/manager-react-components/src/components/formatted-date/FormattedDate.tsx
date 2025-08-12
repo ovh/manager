f8e84f5576688851367ad9473f940ff1eb862c65
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-08-12 16:47:19 +0200

    feat(manager-react-components): deprecate formatted date
    ref: #MANAGER-18751
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-react-components/src/components/formatted-date/FormattedDate.tsx
+++ packages/manager-react-components/src/components/formatted-date/FormattedDate.tsx
@@ -1,8 +1,14 @@
+/**
+ * @deprecated Use useFormatDate with date fns format instead
+ */
 import {
   FormattedDateProps,
   useFormattedDate,
 } from '../../hooks/date/useFormattedDate';
 
+/**
+ * @deprecated Use useFormatDate with date fns format instead
+ */
 export const FormattedDate = (props: FormattedDateProps) => {
   const formattedDate = useFormattedDate(props);
 
