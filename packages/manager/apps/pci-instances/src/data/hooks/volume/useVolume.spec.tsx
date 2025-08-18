a909a0bbdb97a3b5e0e31da5315d2b8fff054ad6
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2025-08-18 17:14:38 +0200

    feat(pci-instances): attach volume on instance
    ref: #TAPC-4389
    
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

--- packages/manager/apps/pci-instances/src/data/hooks/volume/useVolume.spec.tsx
+++ packages/manager/apps/pci-instances/src/data/hooks/volume/useVolume.spec.tsx
@@ -2,27 +2,27 @@ import { FC, PropsWithChildren } from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { renderHook, waitFor } from '@testing-library/react';
 import { describe, it, vi } from 'vitest';
-import { useNetworks } from './useNetwork';
+import { useVolumes } from './useVolume';
 import queryClient from '@/queryClient';
-import { getNetworks } from '@/data/api/network';
+import { getVolumes } from '@/data/api/volume';
 
 const projectId = 'projectId-test';
 const region = 'fake-region';
 
-vi.mock('@/data/api/network');
-vi.mocked(getNetworks).mockResolvedValue([]);
+vi.mock('@/data/api/volume');
+vi.mocked(getVolumes).mockResolvedValue([]);
 
 const wrapper: FC<PropsWithChildren> = ({ children }) => (
   <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 );
 
-describe('useNetwork', () => {
-  it('should return correctly network detail', async () => {
-    const { result } = renderHook(() => useNetworks(projectId, region), {
+describe('useVolumes', () => {
+  it('should return correctly volumes details', async () => {
+    const { result } = renderHook(() => useVolumes(projectId, region), {
       wrapper,
     });
 
-    expect(getNetworks).toHaveBeenCalledWith({
+    expect(getVolumes).toHaveBeenCalledWith({
       projectId,
       region,
     });
