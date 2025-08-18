d69d6b06d9904b6a6ac3bb03def828db53b4f6b9
stif59100 <steeve.vanderstocken@ovhcloud.com>
2025-08-18 08:53:00 +0200

    feat(web-hosting): add task pages for wordpress managed
    ref: #MANAGER-19223
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

---
+++ packages/manager/apps/web-hosting/src/utils/getStatusColor.ts
@@ -0,0 +1,36 @@
+import { ODS_BADGE_COLOR } from '@ovhcloud/ods-components';
+import {
+  DnsStatus,
+  GitStatus,
+  ResourceStatus,
+  ServiceStatus,
+} from '@/data/type';
+import { Status } from '@/types/ssl';
+
+export const getStatusColor = (status: string) => {
+  switch (status) {
+    case GitStatus.CREATED:
+    case ResourceStatus.READY:
+    case ResourceStatus.DONE:
+    case ServiceStatus.ACTIVE:
+    case DnsStatus.CONFIGURED:
+      return ODS_BADGE_COLOR.success;
+    case GitStatus.CREATING:
+    case GitStatus.DELETING:
+    case GitStatus.DEPLOYING:
+    case GitStatus.INITIALERROR:
+    case DnsStatus.EXTERNAL:
+    case Status.WAITING_USER_INPUT:
+      return ODS_BADGE_COLOR.warning;
+    case GitStatus.DISABLED:
+    case GitStatus.ERROR:
+    case ResourceStatus.ERROR:
+    case ResourceStatus.SUSPENDED:
+    case ServiceStatus.NONE:
+      return ODS_BADGE_COLOR.critical;
+    case DnsStatus.NOT_CONFIGURED:
+      return ODS_BADGE_COLOR.neutral;
+    default:
+      return ODS_BADGE_COLOR.information;
+  }
+};
