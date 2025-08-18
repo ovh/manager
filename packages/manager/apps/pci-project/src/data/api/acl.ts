39b7dd88c0a51f55d2765a0c479d26f8eb8c77d9
Marc Monchablon <marc.monchablon.ext@corp.ovh.com>
2025-08-18 16:36:53 +0200

    feat(pci-project): add 'Contact & Rights' page
    ref: #MANAGER-19074
    
    Signed-off-by: Marc Monchablon <marc.monchablon.ext@corp.ovh.com>

---
+++ packages/manager/apps/pci-project/src/data/api/acl.ts
@@ -0,0 +1,44 @@
+import { v6 } from '@ovh-ux/manager-core-api';
+
+export type AccountAcl = {
+  accountId: string;
+  type: string;
+};
+
+export const getProjectAcl = async (projectId: string): Promise<string[]> => {
+  const { data } = await v6.get(`/cloud/project/${projectId}/acl`);
+  return data;
+};
+
+export const getProjectAclAccountInfo = async (
+  projectId: string,
+  accountId: string,
+): Promise<AccountAcl> => {
+  const { data } = await v6.get(`/cloud/project/${projectId}/acl/${accountId}`);
+  return data;
+};
+
+export type AddAccountAclToProjectParams = {
+  projectId: string;
+  accountId: string;
+  accessRights: 'readOnly' | 'readWrite';
+};
+
+export const addAccountAclToProject = async (
+  projectId: string,
+  account: AccountAcl,
+): Promise<AccountAcl> => {
+  const { data } = await v6.post(
+    `/cloud/project/${projectId}/acl?accountId=${account.accountId}`,
+    account,
+  );
+  return data;
+};
+
+export const deleteAccountAclFromProject = async (
+  projectId: string,
+  accountId: string,
+): Promise<string> => {
+  await v6.delete(`/cloud/project/${projectId}/acl/${accountId}`);
+  return accountId;
+};
