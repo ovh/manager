e20bb2674c9316f524b849de4085b7e4a6f768ea
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2025-08-20 16:42:26 +0200

    chore(*): cleanup old hub module
    ref: #MANAGER-18164
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/modules/hub/src/components/kyc-fraud-banner/kyc-fraud-banner.component.js
+++
@@ -1,8 +0,0 @@
-import controller from './kyc-fraud-banner.controller';
-import template from './kyc-fraud-banner.html';
-
-export default {
-  controller,
-  name: 'ovhManagerKycFraudBanner',
-  template,
-};
