092f00ea0015784f8f6ddb64f92774927456147f
StephaneRavet <35328743+StephaneRavet@users.noreply.github.com>
2025-08-20 16:36:04 +0200

    refactor(pci-project): bump mrc from v2.32.0 to v2.37.0 and fix deprecated (#18731)
    Signed-off-by: Stephane Ravet <ravet.st@gmail.com>

--- packages/manager/apps/pci-project/src/pages/home/components/DashboardTiles.component.tsx
+++ packages/manager/apps/pci-project/src/pages/home/components/DashboardTiles.component.tsx
@@ -1,8 +1,8 @@
 import { useTranslation } from 'react-i18next';
-import { DashboardTile, ErrorBanner } from '@ovh-ux/manager-react-components';
+import { ManagerTile, ErrorBanner } from '@ovh-ux/manager-react-components';
 import { useProjectIdFromParams } from '@/hooks/useProjectIdFromParams';
 import { useDashboardSections } from './useDashboardSections.hook';
-import TileItemContent from './TileItemContent.component';
+import TileItem from './TileItem.component';
 
 export default function DashboardTiles() {
   const projectId = useProjectIdFromParams();
@@ -29,48 +29,38 @@ export default function DashboardTiles() {
       role="region"
       aria-label={t('pci_project_project_dashboard_tiles_aria')}
     >
-      {sections.map((section, sectionIdx) => {
-        // Add credit link for billing section
-        const items = [...section.items];
-        if (section.type === 'billing') {
-          items.push({
-            label: '',
-            description: '',
-            link: '',
-            price: '',
-            validUntil: '',
-            isVoucherLink: true,
-          });
-        }
-
-        return (
-          <div
-            key={sectionIdx}
-            className="flex flex-col h-full"
-            role="region"
-            aria-label={t('pci_project_project_section_aria', {
-              title: section.title,
-            })}
-          >
-            <div className="h-full [&>*]:h-full">
-              <DashboardTile
-                title={section.title}
-                items={items.map((item, itemIdx) => ({
-                  id: `${sectionIdx}-${itemIdx}`,
-                  value: (
-                    <TileItemContent
-                      item={item}
-                      section={section}
-                      isLoading={isLoading}
-                      projectId={projectId}
-                    />
-                  ),
-                }))}
-              />
-            </div>
+      {sections.map((section, sectionIdx) => (
+        <div
+          key={sectionIdx}
+          className="flex flex-col h-full"
+          role="region"
+          aria-label={t('pci_project_project_section_aria', {
+            title: section.title,
+          })}
+        >
+          <div className="h-full [&>*]:h-full">
+            <ManagerTile>
+              {section.title && (
+                <>
+                  <ManagerTile.Title>{section.title}</ManagerTile.Title>
+                  <ManagerTile.Divider />
+                </>
+              )}
+              {section.items.map((item, itemIdx) => (
+                <TileItem
+                  key={`${sectionIdx}-${itemIdx}`}
+                  item={item}
+                  section={section}
+                  itemIdx={itemIdx}
+                  totalItems={section.items.length}
+                  isLoading={isLoading}
+                  projectId={projectId}
+                />
+              ))}
+            </ManagerTile>
           </div>
-        );
-      })}
+        </div>
+      ))}
     </div>
   );
 }
