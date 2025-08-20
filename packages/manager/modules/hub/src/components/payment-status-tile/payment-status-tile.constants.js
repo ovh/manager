3cecbacc91b3cbc368c4cb6eea199f5c8dba728d
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2025-08-20 16:42:26 +0200

    chore(*): cleanup old hub module
    ref: #MANAGER-18164
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/modules/hub/src/components/payment-status-tile/payment-status-tile.constants.js
+++
@@ -1,9 +0,0 @@
-export const SERVICE_STATES = {
-  error: ['expired'],
-  success: ['auto', 'automatic'],
-  warning: ['manual', 'manualPayment', 'delete_at_expiration'],
-};
-
-export default {
-  SERVICE_STATES,
-};
