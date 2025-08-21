198e2751aaa7b79a2057c65169fe81e46fcfcb9b
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-08-21 16:22:08 +0200

    feat(manager-react-components): migrate delete modal component
    ref: #MANAGER-19047
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-react-components/src/components/delete-modal/translations/translations.ts
+++ packages/manager-react-components/src/components/delete-modal/translations/translations.ts
@@ -1,4 +1,4 @@
-import { buildTranslationManager } from '../../../../utils/translation-helper';
+import { buildTranslationManager } from '../../../utils/translation-helper';
 
 const translationLoaders = {
   de_DE: () => import('./Messages_de_DE.json'),
