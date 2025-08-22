ce8d93bb5776bf0117b03694cc8b8589e803afad
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-08-22 16:08:17 +0200

    feat(okms): add secret reveal value drawer
    ref: #MANAGER-18314
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

--- packages/manager/apps/key-management-service/src/modules/secret-manager/mocks/versions/versions.mock.ts
+++ packages/manager/apps/key-management-service/src/modules/secret-manager/mocks/versions/versions.mock.ts
@@ -1,17 +1,41 @@
-import { SecretVersion } from '@secret-manager/types/secret.type';
+import {
+  SecretVersion,
+  SecretVersionWithData,
+} from '@secret-manager/types/secret.type';
+
+export const versionDeletedMock: SecretVersion = {
+  id: 1,
+  createdAt: '2023-01-15T08:30:45Z',
+  state: 'DELETED',
+};
+
+export const versionDeactivatedMock: SecretVersion = {
+  id: 2,
+  createdAt: '2023-01-16T08:30:45Z',
+  state: 'DEACTIVATED',
+  deactivatedAt: '2023-01-17T08:30:45Z',
+};
+
+export const versionActiveMock: SecretVersion = {
+  id: 3,
+  createdAt: '2023-01-18T08:30:45Z',
+  state: 'ACTIVE',
+  deactivatedAt: '2023-01-19T08:30:45Z',
+};
 
 export const versionsMock: SecretVersion[] = [
-  { id: 1, createdAt: '2023-01-15T08:30:45Z', state: 'ACTIVE' },
-  {
-    id: 2,
-    createdAt: '2023-01-16T08:30:45Z',
-    state: 'DEACTIVATED',
-    deactivatedAt: '2023-01-17T08:30:45Z',
-  },
-  {
-    id: 3,
-    createdAt: '2023-01-18T08:30:45Z',
-    state: 'DELETED',
-    deactivatedAt: '2023-01-19T08:30:45Z',
-  },
+  versionActiveMock,
+  versionDeactivatedMock,
+  versionDeletedMock,
 ];
+
+export const getVersionMockWithData = (
+  version: SecretVersion,
+): SecretVersionWithData => ({
+  ...version,
+  data: { a: 'json' },
+});
+
+export const versionsMockwithData: SecretVersionWithData[] = versionsMock.map(
+  (version) => getVersionMockWithData(version),
+);
---
+++ packages/manager/apps/key-management-service/src/modules/secret-manager/pages/valueDrawer/ValueDrawer.page.tsx
@@ -0,0 +1,144 @@
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { NAMESPACES } from '@ovh-ux/manager-common-translations';
+import { Drawer } from '@ovh-ux/manager-react-components';
+import {
+  OdsMessage,
+  OdsSelect,
+  OdsSpinner,
+  OdsText,
+} from '@ovhcloud/ods-components/react';
+import {
+  VALUE_DRAWER_TEST_ID,
+  VERSION_SELECTOR_ERROR_TEST_ID,
+  VERSION_SELECTOR_SPINNER_TEST_ID,
+  VERSION_SELECTOR_TEST_ID,
+} from '@secret-manager/utils/tests/secretValue.constants';
+import { VersionState } from '@secret-manager/components/VersionState/VersionState.component';
+import { SecretVersion } from '@secret-manager/types/secret.type';
+import { decodeSecretPath } from '@secret-manager/utils/secretPath';
+import { useSecretVersions } from '@secret-manager/data/hooks/useSecretVersions';
+import { useNavigate, useParams } from 'react-router-dom';
+import { SecretDashboardPageParams } from '../dashboard/dashboard.type';
+import { SecretRawValue } from './SecretRawValue.component';
+
+type VersionSelectorParams = {
+  domainId: string;
+  path: string;
+  currentVersion: SecretVersion;
+  setCurrentVersion: React.Dispatch<React.SetStateAction<SecretVersion>>;
+};
+
+const VersionSelector = ({
+  domainId,
+  path,
+  currentVersion,
+  setCurrentVersion,
+}: VersionSelectorParams) => {
+  const { t } = useTranslation([
+    'secret-manager/common',
+    NAMESPACES.STATUS,
+    NAMESPACES.ERROR,
+  ]);
+  const { data: versions, isPending, error } = useSecretVersions(
+    domainId,
+    path,
+  );
+
+  if (isPending)
+    return (
+      <div className="flex justify-center">
+        <OdsSpinner data-testid={VERSION_SELECTOR_SPINNER_TEST_ID} />
+      </div>
+    );
+
+  if (error)
+    return (
+      <OdsMessage
+        className="mt-4"
+        color="critical"
+        data-testid={VERSION_SELECTOR_ERROR_TEST_ID}
+      >
+        {t('error_message', { message: error.message, ns: NAMESPACES.ERROR })}
+      </OdsMessage>
+    );
+
+  const defaultSelectedVersion = versions[0];
+
+  return (
+    <div className="flex flex-col gap-3 pt-2">
+      <div className="flex gap-2 items-center">
+        <OdsText preset="span" className="w-1/3">
+          {t('version')}
+        </OdsText>
+        <OdsSelect
+          data-testid={VERSION_SELECTOR_TEST_ID}
+          className="flex-grow"
+          name="version-selector"
+          onOdsChange={(value) =>
+            setCurrentVersion(
+              versions.find((v) => v.id === Number(value.detail.value)),
+            )
+          }
+          defaultValue={defaultSelectedVersion.id.toString()}
+          isDisabled={versions.length === 1}
+        >
+          {versions.map((version) => (
+            <option value={version.id} key={version.id}>
+              {version.id}
+            </option>
+          ))}
+        </OdsSelect>
+      </div>
+      {currentVersion && (
+        <div className="flex gap-2 items-center">
+          <OdsText preset="span" className="w-1/3">
+            {t('status', { ns: NAMESPACES.STATUS })}
+          </OdsText>
+          <VersionState state={currentVersion.state} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+const ValueDrawerPage = () => {
+  const { t } = useTranslation('secret-manager/common');
+  const navigate = useNavigate();
+
+  const { domainId, secretPath } = useParams<SecretDashboardPageParams>();
+  const secretPathDecoded = decodeSecretPath(secretPath);
+
+  const [currentVersion, setCurrentVersion] = useState<
+    SecretVersion | undefined
+  >(undefined);
+
+  return (
+    <Drawer
+      data-testid={VALUE_DRAWER_TEST_ID}
+      heading={t('values')}
+      onDismiss={() => {
+        navigate('..');
+      }}
+      isOpen
+    >
+      <div className="flex flex-col gap-4">
+        <VersionSelector
+          domainId={domainId}
+          path={secretPathDecoded}
+          currentVersion={currentVersion}
+          setCurrentVersion={setCurrentVersion}
+        />
+        {currentVersion && currentVersion.state === 'ACTIVE' && (
+          <SecretRawValue
+            domainId={domainId}
+            path={secretPathDecoded}
+            version={currentVersion}
+          />
+        )}
+      </div>
+    </Drawer>
+  );
+};
+
+export default ValueDrawerPage;
