6e13f9d4e0670b1dcb33ed6d54051c876eb848c9
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-08-22 15:08:21 +0200

    fix(okms): fix silent okms order tests
    ref: #MANAGER-18309
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/key-management-service/src/utils/tests/testUtils.tsx
+++ packages/manager/apps/key-management-service/src/utils/tests/testUtils.tsx
@@ -1,4 +1,7 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Handler } from '@ovh-ux/manager-core-test-utils';
+import { render } from '@testing-library/react';
 
 // Sets all handlers to 0ms delay
 // This avoids the global utils function to set a default delay of 1000ms
@@ -7,3 +10,43 @@ export const removeHandlersDelay = (handlers: Handler[]): Handler[] => {
     return { ...handler, delay: 0 };
   });
 };
+
+export const wait = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
+export const promiseWithDelayMock = <T,>(data: T, delay = 0): Promise<T> => {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve(data);
+    }, delay),
+  );
+};
+
+const createTestQueryClient = () =>
+  new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+
+export function renderWithClient(
+  ui: React.ReactElement,
+): ReturnType<typeof render> & {
+  rerender: (rerenderUi: React.ReactElement) => void;
+} {
+  const testQueryClient = createTestQueryClient();
+  const { rerender, ...result } = render(
+    <QueryClientProvider client={testQueryClient}>{ui}</QueryClientProvider>,
+  );
+  return {
+    ...result,
+    rerender: (rerenderUi: React.ReactElement) =>
+      rerender(
+        <QueryClientProvider client={testQueryClient}>
+          {rerenderUi}
+        </QueryClientProvider>,
+      ),
+  };
+}
