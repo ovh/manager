b70847d83b3e383c6c3a18cbe3da41771c0caac6
Louis BENSI <louis.bensi.ext@corp.ovh.com>
2025-08-25 18:28:51 +0200

    feat(web-domains): upgrade to ods 19
    ref: #MANAGER-19391
    
    Signed-off-by: Louis BENSI <louis.bensi.ext@corp.ovh.com>

--- packages/manager/apps/web-domains/src/alldoms/components/ServiceDetail/ServiceDetailSubscribing/ServiceDetailSubscribingRenewMode.tsx
+++ packages/manager/apps/web-domains/src/alldoms/components/ServiceDetail/ServiceDetailSubscribing/ServiceDetailSubscribingRenewMode.tsx
@@ -1,7 +1,6 @@
-import { ODS_TEXT_PRESET } from '@ovhcloud/ods-components';
-import { OdsText } from '@ovhcloud/ods-components/react';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Text, TEXT_PRESET } from '@ovhcloud/ods-react';
 import {
   LifecycleActionsEnum,
   ServiceInfoRenewMode,
@@ -21,14 +20,14 @@ export default function ServiceDetailSubscribingRenewMode({
 
   return (
     <div className="flex flex-col gap-y-2">
-      <OdsText preset={ODS_TEXT_PRESET.heading6}>
+      <Text preset={TEXT_PRESET.heading6}>
         {t('allDom_table_header_renewMode')}
-      </OdsText>
-      <OdsText preset={ODS_TEXT_PRESET.paragraph}>
+      </Text>
+      <Text preset={TEXT_PRESET.paragraph}>
         {hasTerminateAtExpirationDateAction(lifecyclePendingActions)
           ? t(`allDom_table_status_terminate`)
           : t(`allDom_status_${renewMode}`)}
-      </OdsText>
+      </Text>
     </div>
   );
 }
