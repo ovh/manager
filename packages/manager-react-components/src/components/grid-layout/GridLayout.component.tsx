726fd794cd28c2717b7e1eafe564aac187cc9c96
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-08-26 18:08:14 +0200

    feat(manager-react-components): migrate grid layout compoennt
    ref: #MANAGER-19108
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-react-components/src/components/grid-layout/GridLayout.component.tsx
+++ packages/manager-react-components/src/components/grid-layout/GridLayout.component.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
+import { PropsWithChildren, FC } from 'react';
 
-export const DashboardGridLayout: React.FC<React.PropsWithChildren> = ({
-  children,
-}) => (
+export const GridLayout: FC<PropsWithChildren> = ({ children }) => (
   <div className="w-full block">
     <div className="grid grid-cols-1 md:items-start md:grid-cols-3 gap-4 md:gap-6">
       {children}
