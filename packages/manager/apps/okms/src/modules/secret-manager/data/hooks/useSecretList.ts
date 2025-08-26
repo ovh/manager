8ce22b3cf8bbc312a87ec092ab5801f924b02190
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-08-26 17:20:23 +0200

    feat(okms): use pagination on secret list
    ref: #MANAGER-18758
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

--- packages/manager/apps/okms/src/modules/secret-manager/data/hooks/useSecretList.ts
+++ packages/manager/apps/okms/src/modules/secret-manager/data/hooks/useSecretList.ts
@@ -1,14 +1,17 @@
-import { useQuery } from '@tanstack/react-query';
 import { Secret } from '@secret-manager/types/secret.type';
-import { ErrorResponse } from '@/types/api.type';
-import {
-  getSecretList,
-  secretQueryKeys,
-} from '@/modules/secret-manager/data/api/secrets';
+import { useResourcesIcebergV2 } from '@ovh-ux/manager-react-components';
+import { secretQueryKeys } from '@/modules/secret-manager/data/api/secrets';
 
-export const useSecretList = (okmsId: string) => {
-  return useQuery<Secret[], ErrorResponse>({
-    queryKey: secretQueryKeys.list(okmsId),
-    queryFn: () => getSecretList(okmsId),
+export const useSecretList = ({
+  domainId,
+  pageSize = 25,
+}: {
+  domainId: string;
+  pageSize?: number;
+}) => {
+  return useResourcesIcebergV2<Secret>({
+    route: `okms/resource/${domainId}/secret`,
+    queryKey: secretQueryKeys.list(domainId),
+    pageSize,
   });
 };
