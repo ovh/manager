e333c244d4d5811487ca70fc8749cc94707e4497
aTurmo <adrien.turmo.ext@ovhcloud.com>
2025-08-27 12:03:27 +0200

    feat(pci-block-storage): add retyping for LZ volumes
    ref: #TAPC-4565
    
    Signed-off-by: Adrien Turmo <adrien.turmo.ext@ovhcloud.com>

--- packages/manager/apps/pci-block-storage/src/pages/retype/Retype.page.spec.tsx
+++ packages/manager/apps/pci-block-storage/src/pages/retype/Retype.page.spec.tsx
@@ -1,7 +1,8 @@
-import { render } from '@testing-library/react';
+import { render, within } from '@testing-library/react';
 
 import { describe, vi } from 'vitest';
 import { userEvent } from '@testing-library/user-event';
+import { NAMESPACES } from '@ovh-ux/manager-common-translations';
 import RetypePage from '@/pages/retype/Retype.page';
 import {
   TVolumeRetypeModel,
@@ -70,7 +71,26 @@ describe('RetypePage', () => {
     ).toBeVisible();
   });
 
-  describe('validation button', () => {
+  it('should render unavailable message message if selected volume is not encrypted', () => {
+    vi.mocked(useCatalogWithPreselection).mockReturnValue({
+      data: [],
+      isPending: false,
+    } as ReturnType<typeof useCatalogWithPreselection>);
+
+    const { getByText } = render(<RetypePage />);
+
+    expect(useCatalogWithPreselection).toHaveBeenCalledWith(
+      PROJECT_ID,
+      VOLUME_ID,
+    );
+    expect(
+      getByText(
+        'retype:pci_projects_project_storages_blocks_retype_cant_retype',
+      ),
+    ).toBeVisible();
+  });
+
+  describe('modify button', () => {
     it('should be disabled if nothing has changed', () => {
       vi.mocked(useCatalogWithPreselection).mockReturnValue({
         data: [selectedCatalogOption, otherCatalogOption],
@@ -79,14 +99,10 @@ describe('RetypePage', () => {
 
       const { getByText } = render(<RetypePage />);
 
-      expect(
-        getByText(
-          'common:pci_projects_project_storages_blocks_button_validate',
-        ),
-      ).toBeDisabled();
+      expect(getByText(`${NAMESPACES.ACTIONS}:modify`)).toBeDisabled();
     });
 
-    it('should be enable if volume type has changed', async () => {
+    it('should be enabled if volume type has changed', async () => {
       vi.mocked(useCatalogWithPreselection).mockReturnValue({
         data: [selectedCatalogOption, otherCatalogOption],
         isPending: false,
@@ -98,11 +114,139 @@ describe('RetypePage', () => {
         getByRole('radio', { name: otherCatalogOption.displayName }),
       );
 
+      expect(getByText(`${NAMESPACES.ACTIONS}:modify`)).not.toBeDisabled();
+    });
+
+    it('should be disabled if volume type has changed and back to original value', async () => {
+      vi.mocked(useCatalogWithPreselection).mockReturnValue({
+        data: [selectedCatalogOption, otherCatalogOption],
+        isPending: false,
+      } as ReturnType<typeof useCatalogWithPreselection>);
+
+      const { getByText, getByRole } = render(<RetypePage />);
+
+      await userEvent.click(
+        getByRole('radio', { name: otherCatalogOption.displayName }),
+      );
+      await userEvent.click(
+        getByRole('radio', { name: selectedCatalogOption.displayName }),
+      );
+
+      expect(getByText(`${NAMESPACES.ACTIONS}:modify`)).toBeDisabled();
+    });
+
+    it('should be enabled if encryption has changed', async () => {
+      vi.mocked(useCatalogWithPreselection).mockReturnValue({
+        data: [
+          {
+            ...selectedCatalogOption,
+            encrypted: true,
+          },
+        ],
+        isPending: false,
+        preselectedEncryptionType: null,
+      } as ReturnType<typeof useCatalogWithPreselection>);
+
+      const { getByRole, getByText } = render(<RetypePage />);
+
+      await userEvent.click(
+        within(
+          getByRole('radiogroup', {
+            name:
+              'retype:pci_projects_project_storages_blocks_retype_change_encryption_title',
+          }),
+        ).getByText('OVHcloud Managed Key'),
+      );
+
+      expect(getByText(`${NAMESPACES.ACTIONS}:modify`)).not.toBeDisabled();
+    });
+
+    it('should be disabled if encryption has changed and back to original value', async () => {
+      vi.mocked(useCatalogWithPreselection).mockReturnValue({
+        data: [
+          {
+            ...selectedCatalogOption,
+            encrypted: true,
+          },
+        ],
+        isPending: false,
+        preselectedEncryptionType: null,
+      } as ReturnType<typeof useCatalogWithPreselection>);
+
+      const { getByRole, getByText } = render(<RetypePage />);
+
+      await userEvent.click(
+        within(
+          getByRole('radiogroup', {
+            name:
+              'retype:pci_projects_project_storages_blocks_retype_change_encryption_title',
+          }),
+        ).getByText('OVHcloud Managed Key'),
+      );
+      await userEvent.click(
+        within(
+          getByRole('radiogroup', {
+            name:
+              'retype:pci_projects_project_storages_blocks_retype_change_encryption_title',
+          }),
+        ).getByText('common:pci_projects_project_storages_blocks_status_NONE'),
+      );
+
+      expect(getByText(`${NAMESPACES.ACTIONS}:modify`)).toBeDisabled();
+    });
+  });
+
+  describe('encryption', () => {
+    it('should render option given volume is encrypted', () => {
+      vi.mocked(useCatalogWithPreselection).mockReturnValue({
+        data: [
+          {
+            ...selectedCatalogOption,
+            encrypted: true,
+          },
+        ],
+        isPending: false,
+      } as ReturnType<typeof useCatalogWithPreselection>);
+
+      const { getByRole, queryByText } = render(<RetypePage />);
+
+      expect(
+        getByRole('radiogroup', {
+          name:
+            'retype:pci_projects_project_storages_blocks_retype_change_encryption_title',
+        }),
+      ).toBeVisible();
+      expect(
+        queryByText(
+          'retype:pci_projects_project_storages_blocks_retype_encryption_not_available',
+        ),
+      ).toBeNull();
+    });
+
+    it('should not render option given and render information message given volume is not encrypted', () => {
+      vi.mocked(useCatalogWithPreselection).mockReturnValue({
+        data: [
+          {
+            ...selectedCatalogOption,
+            encrypted: false,
+          },
+        ],
+        isPending: false,
+      } as ReturnType<typeof useCatalogWithPreselection>);
+
+      const { queryByRole, getByText } = render(<RetypePage />);
+
+      expect(
+        queryByRole('radiogroup', {
+          name:
+            'retype:pci_projects_project_storages_blocks_retype_change_encryption_title',
+        }),
+      ).toBeNull();
       expect(
         getByText(
-          'common:pci_projects_project_storages_blocks_button_validate',
+          'retype:pci_projects_project_storages_blocks_retype_encryption_not_available',
         ),
-      ).not.toBeDisabled();
+      ).toBeVisible();
     });
   });
 });
