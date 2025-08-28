84e52757e202baa5f65764119dfd7e6558c17ac0
ahmed sefiani <ahmed.sefiani.ext@corp.ovh.com>
2025-08-28 09:09:06 +0200

    feat(nutanix): fetch server tasks with iceberg and adding filter status in datagrid
    ref: #MANAGER-17410
    
    Signed-off-by: ahmed sefiani <ahmed.sefiani.ext@corp.ovh.com>

--- packages/manager/modules/bm-server-components/src/tasks/controller.js
+++ packages/manager/modules/bm-server-components/src/tasks/controller.js
@@ -1,25 +1,67 @@
 import isFunction from 'lodash/isFunction';
 import snakeCase from 'lodash/snakeCase';
 import toUpper from 'lodash/toUpper';
+import map from 'lodash/map';
 
 import Task from './tasks.class';
 
 export default class BmServerComponentsTasksController {
   /* @ngInject */
-  constructor($http) {
+  constructor(
+    $http,
+    ouiDatagridService,
+    $translate,
+    Server,
+    iceberg,
+    icebergUtils,
+  ) {
     this.$http = $http;
+    this.ouiDatagridService = ouiDatagridService;
+    this.Server = Server;
+    this.$translate = $translate;
+    this.taskStatusFilter = null;
+    this.iceberg = iceberg;
+    this.icebergUtils = icebergUtils;
   }
 
-  getTasks({ offset, pageSize }) {
-    return this.loadTasks(
-      this.serviceName,
-      (offset - 1) / pageSize + 1,
-      pageSize,
-    )
+  $onInit() {
+    this.loading = true;
+    return this.getTaskStatus()
+      .catch((err) => {
+        this.handleError(err);
+      })
+      .finally(() => {
+        this.loading = false;
+      });
+  }
+
+  getTaskStatus() {
+    return this.Server.getModel('dedicated.server.task.StatusEnum').then(
+      (model) => {
+        this.stateEnum = model.enum;
+        this.taskStatusFilterList = this.buildTaskStatusFilterList(
+          this.stateEnum,
+        );
+        return model;
+      },
+    );
+  }
+
+  buildTaskStatusFilterList(stateEnum) {
+    return map(stateEnum, (state) => ({
+      value: state,
+      label: this.$translate.instant(
+        `server_configuration_task_status_${snakeCase(state).toUpperCase()}`,
+      ),
+    }));
+  }
+
+  getTasks(config) {
+    return this.loadTasks(config)
       .then((res) => {
-        const tasks = res.data;
         return {
-          data: tasks.map(
+          ...res,
+          data: (res.data ?? []).map(
             (task) =>
               new Task(
                 task.lastUpdate,
@@ -28,9 +70,6 @@ export default class BmServerComponentsTasksController {
                 toUpper(snakeCase(task.status)),
               ),
           ),
-          meta: {
-            totalCount: res.headers('x-pagination-elements'),
-          },
         };
       })
       .catch((error) => {
@@ -44,17 +83,23 @@ export default class BmServerComponentsTasksController {
       });
   }
 
-  loadTasks(serviceName, pageNumber, pageSize) {
-    return this.$http.get(`/dedicated/server/${serviceName}/task`, {
-      headers: {
-        Pragma: 'no-cache',
-        'x-pagination-mode': 'CachedObjectList-Pages',
-        'x-pagination-number': pageNumber,
-        'x-pagination-size': pageSize,
-        'x-pagination-sort': 'lastUpdate',
-        'x-pagination-sort-order': 'DESC',
-      },
-    });
+  loadTasks(config) {
+    const paginationParams = {
+      ...config,
+      sortOrder: config.sort.dir === 1 ? 'ASC' : 'DESC',
+      defaultFilterColumn: 'executionDate',
+      isCacheDisabled: true,
+    };
+
+    const urlParams = {
+      status: this.taskStatusFilter,
+    };
+
+    return this.Server.getTasks(this.serviceName, paginationParams, urlParams);
+  }
+
+  refresh() {
+    this.ouiDatagridService.refresh('tasksDatagrid', true);
   }
 
   handleError(error) {
