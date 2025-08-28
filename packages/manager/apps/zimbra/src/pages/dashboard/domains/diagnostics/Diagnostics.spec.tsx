e3d8ba4bcc6d84cd5f957d198a325502a37715dc
Atef ZAAFOURI <atefzed@gmail.com>
2025-08-28 12:36:31 +0200

    feat(zimbra): add enable and disabl button to dkim config page
    ref: #MANAGER-18849
    
    Signed-off-by: Atef ZAAFOURI <atefzed@gmail.com>

--- packages/manager/apps/zimbra/src/pages/dashboard/domains/diagnostics/Diagnostics.spec.tsx
+++ packages/manager/apps/zimbra/src/pages/dashboard/domains/diagnostics/Diagnostics.spec.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 
 import { describe, expect, vi } from 'vitest';
 
 import { domainMock, platformMock } from '@/data/api';
+import { urls } from '@/routes/routes.constants';
 import { DnsRecordType } from '@/utils/dnsconfig.constants';
 import { render, waitFor } from '@/utils/test.provider';
 import { odsTabIsSelected, tabContent } from '@/utils/test.utils';
@@ -18,6 +19,15 @@ vi.mocked(useParams).mockReturnValue({
 
 describe('Domain diagnostics page', () => {
   it('should display correctly and have 3 tabs', async () => {
+    vi.mocked(useLocation).mockReturnValue({
+      pathname: urls.domains_diagnostic_mx
+        .replace(':platformId', platformMock[0].id)
+        .replace(':domainId', domainMock.id),
+      search: '',
+      hash: '',
+      key: '',
+      state: '',
+    });
     const { queryByTestId, container } = render(<Diagnostics />);
 
     await waitFor(() => {
@@ -35,5 +45,44 @@ describe('Domain diagnostics page', () => {
     await waitFor(() => {
       expect(queryByTestId(tabContent(DnsRecordType.MX))).toBeInTheDocument();
     });
+  });
+
+  it('should render SPF tab correctly when selected', async () => {
+    vi.mocked(useLocation).mockReturnValue({
+      pathname: urls.domains_diagnostic_spf
+        .replace(':platformId', platformMock[0].id)
+        .replace(':domainId', domainMock.id),
+      search: '',
+      hash: '',
+      key: '',
+      state: '',
+    });
+
+    const { queryByTestId } = render(<Diagnostics />);
+
+    await waitFor(() => {
+      expect(queryByTestId('spinner')).toBeNull();
+    });
+
+    expect(queryByTestId(tabContent(DnsRecordType.SPF))).toBeInTheDocument();
+  });
+
+  it('should render DKIM tab correctly when selected', async () => {
+    vi.mocked(useLocation).mockReturnValue({
+      pathname: urls.domains_diagnostic_dkim
+        .replace(':platformId', platformMock[0].id)
+        .replace(':domainId', domainMock.id),
+      search: '',
+      hash: '',
+      key: '',
+      state: '',
+    });
+    const { queryByTestId } = render(<Diagnostics />);
+
+    await waitFor(() => {
+      expect(queryByTestId('spinner')).toBeNull();
+    });
+
+    expect(queryByTestId(tabContent(DnsRecordType.DKIM))).toBeInTheDocument();
   });
 });
