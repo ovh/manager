ee7bebe40e02dd823d0752eb7a13f53e048a21d7
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-08-28 15:53:54 +0200

    feat(okms): add secret manager breadcrumb
    ref: #MANAGER-18759
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

---
+++ packages/manager/apps/key-management-service/src/modules/secret-manager/components/breadcrumb/index.ts
@@ -0,0 +1,11 @@
+import { DomainBreadcrumbItem } from './items/DomainBreadcrumbItem.component';
+import { RootBreadcrumbItem } from './items/RootBreadcrumbItem.component';
+import { CreateSecretBreadcrumbItem } from './items/CreateSecretBreadcrumbItem.component';
+import { SecretBreadcrumbItem } from './items/SecretBreadcrumbItem.component';
+
+export {
+  DomainBreadcrumbItem,
+  RootBreadcrumbItem,
+  CreateSecretBreadcrumbItem,
+  SecretBreadcrumbItem,
+};
