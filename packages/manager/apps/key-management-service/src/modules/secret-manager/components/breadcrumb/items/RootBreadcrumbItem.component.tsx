53ac9c44ff046d246505697061162d20afa2e293
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-08-28 15:53:54 +0200

    feat(okms): add secret manager breadcrumb
    ref: #MANAGER-18759
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

---
+++ packages/manager/apps/key-management-service/src/modules/secret-manager/components/breadcrumb/items/RootBreadcrumbItem.component.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { OdsBreadcrumbItem } from '@ovhcloud/ods-components/react';
+import {
+  SECRET_MANAGER_ROUTES_URIS,
+  SECRET_MANAGER_ROUTES_URLS,
+} from '@secret-manager/routes/routes.constants';
+import { useTranslation } from 'react-i18next';
+import { ROOT_BREADCRUMB_ITEM_TEST_ID } from '@secret-manager/utils/tests/breadcrumb.constants';
+
+export const RootBreadcrumbItem = () => {
+  const navigate = useNavigate();
+  const { t } = useTranslation('secret-manager/common');
+
+  return (
+    <OdsBreadcrumbItem
+      data-testid={ROOT_BREADCRUMB_ITEM_TEST_ID}
+      key={SECRET_MANAGER_ROUTES_URIS.root}
+      label={t('secret_manager')}
+      onClick={() => navigate(SECRET_MANAGER_ROUTES_URLS.secretManagerRoot)}
+      href={null}
+    />
+  );
+};
---
+++ packages/manager/apps/key-management-service/src/modules/secret-manager/utils/tests/breadcrumb.constants.ts
@@ -0,0 +1,5 @@
+export const ROOT_BREADCRUMB_ITEM_TEST_ID = 'root-breadcrumb-item';
+export const CREATE_SECRET_BREADCRUMB_ITEM_TEST_ID =
+  'create-secret-breadcrumb-item';
+export const DOMAIN_BREADCRUMB_ITEM_TEST_ID = 'domain-breadcrumb-item';
+export const SECRET_BREADCRUMB_ITEM_TEST_ID = 'secret-breadcrumb-item';
