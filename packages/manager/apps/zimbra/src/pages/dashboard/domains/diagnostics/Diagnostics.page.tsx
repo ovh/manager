49cdd1b5265ca515d2cdfe839a6cf241a60393f8
Atef ZAAFOURI <atefzed@gmail.com>
2025-08-28 12:36:31 +0200

    feat(zimbra): add enable and disabl button to dkim config page
    ref: #MANAGER-18849
    
    Signed-off-by: Atef ZAAFOURI <atefzed@gmail.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/apps/zimbra/src/pages/dashboard/domains/diagnostics/Diagnostics.page.tsx
+++ packages/manager/apps/zimbra/src/pages/dashboard/domains/diagnostics/Diagnostics.page.tsx
@@ -1,49 +1,32 @@
-import React, { Fragment, useMemo } from 'react';
+import { useMemo } from 'react';
 
 import { useLocation, useParams } from 'react-router-dom';
 
-import { Trans, useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
 import {
-  ODS_BADGE_COLOR,
   ODS_BUTTON_VARIANT,
   ODS_ICON_NAME,
   ODS_LINK_COLOR,
   ODS_MESSAGE_COLOR,
-  ODS_TEXT_PRESET,
-  JSX as Ods,
 } from '@ovhcloud/ods-components';
-import { OdsBadge, OdsButton, OdsIcon, OdsMessage, OdsText } from '@ovhcloud/ods-components/react';
-import { StyleReactProps } from '@ovhcloud/ods-components/react/dist/types/react-component-lib/interfaces';
+import { OdsButton, OdsIcon, OdsMessage, OdsText } from '@ovhcloud/ods-components/react';
 
-import {
-  Clipboard,
-  IconLinkAlignmentType,
-  LinkType,
-  Links,
-  Subtitle,
-} from '@ovh-ux/manager-react-components';
+import { IconLinkAlignmentType, LinkType, Links } from '@ovh-ux/manager-react-components';
 import { ButtonType, PageLocation, useOvhTracking } from '@ovh-ux/manager-react-shell-client';
 
 import {
-  GuideLink,
   Loading,
   TabItemProps,
   TabsPanel,
   activatedTabs,
   useComputePathMatchers,
 } from '@/components';
-import {
-  DomainDiagnosisTestResult,
-  DomainDiagnosisTestStatusEnum,
-  ExpectedDNSConfig,
-} from '@/data/api';
+import { DomainDiagnosisTestResult, DomainDiagnosisTestStatusEnum } from '@/data/api';
 import { useDomainDiagnostic } from '@/data/hooks';
-import { GUIDES_LIST, Guide } from '@/guides.constants';
 import { useGenerateUrl } from '@/hooks';
 import { urls } from '@/routes/routes.constants';
 import {
-  AUTO_CONFIGURE_DOMAIN,
   BACK_PREVIOUS_PAGE,
   DOMAIN_DIAGNOSTICS,
   DOMAIN_DIAGNOSTICS_DKIM,
@@ -55,37 +38,13 @@ import {
 import { FEATURE_FLAGS } from '@/utils';
 import { DnsRecordType } from '@/utils/dnsconfig.constants';
 
+import DKIMTabContent from './DKIMTabContent.component';
+import MXTabContent from './MXTabContent.component';
+import SPFTabContent from './SPFTabContent.component';
+import SRVTabContent from './SRVTabContent.component';
+
 const isDiagnosticError = (diagnostic: DomainDiagnosisTestResult) => {
   return diagnostic && diagnostic?.status !== DomainDiagnosisTestStatusEnum.OK;
-};
-
-const getStatusBadgeColor = (status: DomainDiagnosisTestStatusEnum): ODS_BADGE_COLOR => {
-  switch (status) {
-    case DomainDiagnosisTestStatusEnum.OK:
-      return ODS_BADGE_COLOR.success;
-    case DomainDiagnosisTestStatusEnum.ERROR:
-      return ODS_BADGE_COLOR.critical;
-    case DomainDiagnosisTestStatusEnum.WARNING:
-      return ODS_BADGE_COLOR.warning;
-    default:
-      return ODS_BADGE_COLOR.neutral;
-  }
-};
-
-type StatusBadgeProps = StyleReactProps &
-  Omit<Ods.OdsBadge, 'label' | 'color'> & {
-    status: DomainDiagnosisTestStatusEnum;
-  };
-
-const StatusBadge = ({ status, ...props }: StatusBadgeProps) => {
-  const { t } = useTranslation('domains/diagnostic');
-  return (
-    <OdsBadge
-      color={getStatusBadgeColor(status)}
-      label={t(`zimbra_domain_diagnostic_status_${status.toLowerCase()}`)}
-      {...props}
-    />
-  );
 };
 
 const TabTitle = ({ title, hasError }: { title: string; hasError?: boolean }) => {
@@ -96,173 +55,6 @@ const TabTitle = ({ title, hasError }: { title: string; hasError?: boolean }) =>
         <OdsIcon className="diag-dns-icon ml-4" name={ODS_ICON_NAME.hexagonExclamation} />
       )}
     </>
-  );
-};
-
-const useDNSRecordConfigHelp = ({
-  expectedDNSConfig,
-  recordType,
-}: {
-  expectedDNSConfig: ExpectedDNSConfig;
-  recordType: DnsRecordType;
-}) => {
-  const { t } = useTranslation('domains/diagnostic');
-  switch (recordType) {
-    case DnsRecordType.MX:
-      return (
-        <>
-          {expectedDNSConfig?.mx?.map(({ priority, target }) => (
-            <tr key={target}>
-              <td>
-                <OdsText preset={ODS_TEXT_PRESET.paragraph}>
-                  <strong className="mr-4">{t('zimbra_domain_diagnostic_field_priority')}</strong>
-                  {priority}
-                </OdsText>
-              </td>
-              <td>
-                <OdsText preset={ODS_TEXT_PRESET.paragraph}>
-                  <strong className="mr-4">{t('zimbra_domain_diagnostic_field_target')}</strong>
-                </OdsText>
-                <Clipboard value={target.toString()} />
-              </td>
-            </tr>
-          ))}
-        </>
-      );
-    case DnsRecordType.SPF:
-      return (
-        <tr key="spf">
-          <td>
-            <OdsText preset={ODS_TEXT_PRESET.paragraph}>
-              <strong className="mr-4">{t('zimbra_domain_diagnostic_field_value')}</strong>
-            </OdsText>
-            <Clipboard value={expectedDNSConfig?.spf} />
-          </td>
-        </tr>
-      );
-    case DnsRecordType.DKIM:
-      return expectedDNSConfig?.dkim?.cnames?.map((cname, index) => (
-        <Fragment key={cname?.name}>
-          <tr key={`CNAME ${index + 1}`}>
-            <td>
-              <OdsText preset={ODS_TEXT_PRESET.heading5}>{`CNAME ${index + 1}`}</OdsText>
-            </td>
-          </tr>
-          <tr key={cname?.name}>
-            <td>
-              <OdsText preset={ODS_TEXT_PRESET.paragraph}>
-                <strong className="mr-4">{t('zimbra_domain_diagnostic_field_subdomain')}</strong>
-              </OdsText>
-              <Clipboard value={cname?.name} />
-            </td>
-          </tr>
-          <tr key={cname?.value}>
-            <td>
-              <OdsText preset={ODS_TEXT_PRESET.paragraph}>
-                <strong className="mr-4">{t('zimbra_domain_diagnostic_field_value')}</strong>
-              </OdsText>
-              <Clipboard value={cname?.value} />
-            </td>
-          </tr>
-        </Fragment>
-      ));
-    case DnsRecordType.SRV:
-    default:
-      return <></>;
-  }
-};
-
-const TabContent = ({
-  diagnostic,
-  recordType,
-  expectedDNSConfig,
-  trackingName,
-  isAutoConfigurable,
-  guide = GUIDES_LIST.dns_configuration_guide,
-}: {
-  diagnostic: DomainDiagnosisTestResult;
-  recordType: DnsRecordType;
-  expectedDNSConfig: ExpectedDNSConfig;
-  trackingName: string;
-  isAutoConfigurable: boolean;
-  guide?: Guide;
-}) => {
-  const { t } = useTranslation('domains/diagnostic');
-  const { trackClick } = useOvhTracking();
-  const fieldHelpers = useDNSRecordConfigHelp({
-    expectedDNSConfig,
-    recordType,
-  });
-
-  if (!diagnostic) {
-    return (
-      <OdsMessage className="md:w-1/2" isDismissible={false} color={ODS_MESSAGE_COLOR.danger}>
-        {t('zimbra_domain_diagnostic_loading_error')}
-      </OdsMessage>
-    );
-  }
-
-  const isOk = diagnostic && diagnostic.status === DomainDiagnosisTestStatusEnum.OK;
-  const [error] = diagnostic.errors;
-
-  return (
-    <div className="flex flex-col gap-4 md:w-1/2" data-testid={`tab-content-${recordType}`}>
-      <OdsText preset={ODS_TEXT_PRESET.paragraph}>
-        <strong>{t('zimbra_domain_diagnostic_status')}</strong>
-        <StatusBadge className="ml-4" status={diagnostic.status} />
-      </OdsText>
-      {!isOk && (
-        <OdsMessage className="w-full" isDismissible={false} color={ODS_MESSAGE_COLOR.warning}>
-          {t(
-            `zimbra_domain_diagnostic_information_banner_${recordType.toLowerCase()}_${diagnostic?.status.toLowerCase()}`,
-          )}
-        </OdsMessage>
-      )}
-      <OdsText preset={ODS_TEXT_PRESET.paragraph}>
-        <Trans
-          t={t}
-          i18nKey={`zimbra_domain_diagnostic_information_message_${recordType.toLowerCase()}_${diagnostic?.status.toLowerCase()}`}
-          values={{
-            recordType,
-            errorCode: error?.code,
-          }}
-        />
-      </OdsText>
-      {!isOk && (
-        <>
-          {guide && <GuideLink label={t('zimbra_domain_diagnostic_access_guide')} guide={guide} />}
-          {isAutoConfigurable ? (
-            <OdsButton
-              label={t('zimbra_domain_diagnostic_cta_auto_configure')}
-              onClick={() => {
-                trackClick({
-                  location: PageLocation.page,
-                  buttonType: ButtonType.button,
-                  actionType: 'action',
-                  actions: [trackingName, AUTO_CONFIGURE_DOMAIN],
-                });
-              }}
-            />
-          ) : (
-            <table className="table-auto dns-fields">
-              <tbody>
-                <tr key="type">
-                  <td>
-                    <OdsText preset={ODS_TEXT_PRESET.paragraph}>
-                      <strong className="mr-4">{t('zimbra_domain_diagnostic_type')}</strong>
-                      <OdsText preset={ODS_TEXT_PRESET.span}>
-                        {recordType === DnsRecordType.DKIM ? 'CNAME' : recordType}
-                      </OdsText>
-                    </OdsText>
-                  </td>
-                </tr>
-                {fieldHelpers}
-              </tbody>
-            </table>
-          )}
-        </>
-      )}
-    </div>
   );
 };
 
@@ -300,7 +92,7 @@ export const DomainDiagnostics = () => {
       to: useGenerateUrl(`../diagnostics/mx`, 'path'),
       pathMatchers: useComputePathMatchers([urls.domains_diagnostic_mx]),
       component: (
-        <TabContent
+        <MXTabContent
           diagnostic={domain?.result?.mx}
           recordType={DnsRecordType.MX}
           expectedDNSConfig={expectedDNSConfig}
@@ -317,10 +109,9 @@ export const DomainDiagnostics = () => {
       hidden: !FEATURE_FLAGS.DOMAIN_DIAGNOSTICS_SRV,
       pathMatchers: useComputePathMatchers([urls.domains_diagnostic_srv]),
       component: (
-        <TabContent
+        <SRVTabContent
           diagnostic={null}
           recordType={DnsRecordType.SRV}
-          expectedDNSConfig={expectedDNSConfig}
           trackingName={DOMAIN_DIAGNOSTICS_SRV}
           isAutoConfigurable={false}
         />
@@ -338,7 +129,7 @@ export const DomainDiagnostics = () => {
       to: useGenerateUrl('../diagnostics/spf', 'path'),
       pathMatchers: useComputePathMatchers([urls.domains_diagnostic_spf]),
       component: (
-        <TabContent
+        <SPFTabContent
           diagnostic={domain?.result?.spf}
           recordType={DnsRecordType.SPF}
           expectedDNSConfig={expectedDNSConfig}
@@ -359,7 +150,7 @@ export const DomainDiagnostics = () => {
       to: useGenerateUrl('../diagnostics/dkim', 'path'),
       pathMatchers: useComputePathMatchers([urls.domains_diagnostic_dkim]),
       component: (
-        <TabContent
+        <DKIMTabContent
           diagnostic={domain?.result?.dkim}
           recordType={DnsRecordType.DKIM}
           expectedDNSConfig={expectedDNSConfig}
@@ -374,7 +165,7 @@ export const DomainDiagnostics = () => {
     () =>
       tabsList.find((tab) => activatedTabs(tab.pathMatchers, location))?.component ||
       tabsList[0].component,
-    [location, domain, isFetching, tabsList],
+    [location, domain],
   );
 
   const handleRefreshClick = async () => {
@@ -404,11 +195,11 @@ export const DomainDiagnostics = () => {
         }}
         label={t('zimbra_domain_diagnostic_cta_back')}
       />
-      <Subtitle>
+      <OdsText preset="heading-3">
         {t('zimbra_domain_diagnostic_subtitle', {
           domain: domain?.domainName,
         })}
-      </Subtitle>
+      </OdsText>
       <div className="mt-6">
         <OdsButton
           data-testid="refresh"
---
+++ packages/manager/apps/zimbra/src/pages/dashboard/domains/diagnostics/MXTabContent.component.tsx
@@ -0,0 +1,126 @@
+import { Trans, useTranslation } from 'react-i18next';
+
+import { ODS_MESSAGE_COLOR, ODS_TEXT_PRESET } from '@ovhcloud/ods-components';
+import { OdsButton, OdsMessage, OdsText } from '@ovhcloud/ods-components/react';
+
+import { Clipboard } from '@ovh-ux/manager-react-components';
+import { ButtonType, PageLocation, useOvhTracking } from '@ovh-ux/manager-react-shell-client';
+
+import { GuideLink } from '@/components';
+import {
+  DomainDiagnosisTestMXResult,
+  DomainDiagnosisTestStatusEnum,
+  ExpectedDNSConfig,
+} from '@/data/api';
+import { GUIDES_LIST, Guide } from '@/guides.constants';
+import { AUTO_CONFIGURE_DOMAIN } from '@/tracking.constants';
+import { DnsRecordType } from '@/utils/dnsconfig.constants';
+
+import StatusBadge from './StatusBadge.component';
+
+const MXTabContent = ({
+  diagnostic,
+  recordType,
+  expectedDNSConfig,
+  trackingName,
+  isAutoConfigurable,
+  guide = GUIDES_LIST.dns_configuration_guide,
+}: {
+  diagnostic: DomainDiagnosisTestMXResult;
+  recordType: DnsRecordType;
+  expectedDNSConfig: ExpectedDNSConfig;
+  trackingName: string;
+  isAutoConfigurable: boolean;
+  guide?: Guide;
+}) => {
+  const { t } = useTranslation('domains/diagnostic');
+  const { trackClick } = useOvhTracking();
+
+  if (!diagnostic) {
+    return (
+      <OdsMessage className="md:w-1/2" isDismissible={false} color={ODS_MESSAGE_COLOR.danger}>
+        {t('zimbra_domain_diagnostic_loading_error')}
+      </OdsMessage>
+    );
+  }
+
+  const isOk = diagnostic && diagnostic.status === DomainDiagnosisTestStatusEnum.OK;
+  const [error] = diagnostic.errors;
+
+  return (
+    <div className="flex flex-col gap-4 md:w-1/2" data-testid={`tab-content-${recordType}`}>
+      <OdsText preset={ODS_TEXT_PRESET.paragraph}>
+        <strong>{t('zimbra_domain_diagnostic_status')}</strong>
+        <StatusBadge className="ml-4" status={diagnostic.status} />
+      </OdsText>
+      {!isOk && (
+        <OdsMessage className="w-full" isDismissible={false} color={ODS_MESSAGE_COLOR.warning}>
+          {t(`zimbra_domain_diagnostic_information_banner_mx_${diagnostic?.status.toLowerCase()}`)}
+        </OdsMessage>
+      )}
+      <OdsText preset={ODS_TEXT_PRESET.paragraph}>
+        <Trans
+          t={t}
+          i18nKey={`zimbra_domain_diagnostic_information_message_mx_${diagnostic?.status.toLowerCase()}`}
+          values={{
+            recordType,
+            errorCode: error?.code,
+          }}
+        />
+      </OdsText>
+      {isOk && (
+        <>
+          {guide && <GuideLink label={t('zimbra_domain_diagnostic_access_guide')} guide={guide} />}
+          {isAutoConfigurable ? (
+            <OdsButton
+              label={t('zimbra_domain_diagnostic_cta_auto_configure')}
+              onClick={() => {
+                trackClick({
+                  location: PageLocation.page,
+                  buttonType: ButtonType.button,
+                  actionType: 'action',
+                  actions: [trackingName, AUTO_CONFIGURE_DOMAIN],
+                });
+              }}
+            />
+          ) : (
+            <table className="table-auto dns-fields">
+              <tbody>
+                <tr key="type">
+                  <td>
+                    <OdsText preset={ODS_TEXT_PRESET.paragraph}>
+                      <strong className="mr-4">{t('zimbra_domain_diagnostic_type')}</strong>
+                      <OdsText preset={ODS_TEXT_PRESET.span}>{recordType}</OdsText>
+                    </OdsText>
+                  </td>
+                </tr>
+                {expectedDNSConfig?.mx?.map(({ priority, target }) => (
+                  <tr key={target}>
+                    <td>
+                      <OdsText preset={ODS_TEXT_PRESET.paragraph}>
+                        <strong className="mr-4">
+                          {t('zimbra_domain_diagnostic_field_priority')}
+                        </strong>
+                        {priority}
+                      </OdsText>
+                    </td>
+                    <td>
+                      <OdsText preset={ODS_TEXT_PRESET.paragraph}>
+                        <strong className="mr-4">
+                          {t('zimbra_domain_diagnostic_field_target')}
+                        </strong>
+                      </OdsText>
+                      <Clipboard value={target.toString()} />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default MXTabContent;
