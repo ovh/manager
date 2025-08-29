7193b1012ecb5f06b463fa4533463043a71c6f1c
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-08-29 17:37:37 +0200

    fix(okms): broken secret version state update
    ref: #MANAGER-18303
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/okms/src/modules/secret-manager/data/hooks/useUpdateSecretVersion.ts
+++ packages/manager/apps/okms/src/modules/secret-manager/data/hooks/useUpdateSecretVersion.ts
@@ -1,5 +1,4 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { SecretVersion } from '@secret-manager/types/secret.type';
 import { ErrorResponse } from '@/types/api.type';
 import {
   secretVersionsQueryKeys,
@@ -18,21 +17,12 @@ export const useUpdateSecretVersion = () => {
   >({
     mutationFn: updateSecretVersion,
     onSuccess: (_, variables) => {
-      // Update secret version list
-      queryClient.setQueryData<SecretVersion[]>(
-        secretVersionsQueryKeys.list(variables.okmsId, variables.path),
-        (oldVersions) => {
-          return oldVersions?.map((version) => {
-            if (version.id === variables.version) {
-              return {
-                ...version,
-                state: variables.state,
-              };
-            }
-            return version;
-          });
-        },
-      );
+      queryClient.invalidateQueries({
+        queryKey: secretVersionsQueryKeys.list(
+          variables.okmsId,
+          variables.path,
+        ),
+      });
     },
   });
 };
