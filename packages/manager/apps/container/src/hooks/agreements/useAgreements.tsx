a1db989c46a41d4f9b64744ddb6335ba0ec59211
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2025-08-29 12:43:31 +0200

    fix(container): remove obsolete url build and incorrect type
    ref: #MANAGER-14721
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

--- packages/manager/apps/container/src/hooks/agreements/useAgreements.tsx
+++ packages/manager/apps/container/src/hooks/agreements/useAgreements.tsx
@@ -1,23 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
-import { useApplication } from "@/context";
 import fetchPendingAgreements from "@/api/agreements";
 
-// TODO: simplify this once new-billing is fully open to the public
-export const useAgreementsPageNavigationParam = () => {
-  const { shell } = useApplication();
-  const environment = shell.getPlugin('environment').getEnvironment();
-
-  const isNewBillingAvailable = Boolean(
-    environment.getApplicationURL('new-billing'),
-  );
-
-  return {
-    app: isNewBillingAvailable ? 'new-billing' : 'dedicated',
-    path: `#/${
-      isNewBillingAvailable ? '' : 'billing/'
-    }autorenew/agreements`,
-  }
-};
+export const useAgreementsPageNavigationParam = () => ({
+  app: 'billing',
+  path: `#/autorenew/agreements`,
+});
 
 export const usePendingAgreements = (enabled: boolean) => useQuery({
   queryKey: ['pending-agreements'],
