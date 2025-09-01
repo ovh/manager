406f51b65810acc7fb2281a1cb42c0011d996bfb
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-09-01 16:57:38 +0200

    fix(vcfaas): allow fetching more than default pageSize
    ref: #MANAGER-19580
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/hpc-vmware-managed-vcd/src/pages/dashboard/datacentre/storage/DatacentreStorage.page.tsx
+++ packages/manager/apps/hpc-vmware-managed-vcd/src/pages/dashboard/datacentre/storage/DatacentreStorage.page.tsx
@@ -88,6 +88,7 @@ export default function StorageListingPage() {
           .replace(subRoutes.dashboard, id)
           .replace(subRoutes.vdcId, vdcId),
       }}
+      shouldFetchAll
       isEmbedded
       orderButton={
         <OdsButton
