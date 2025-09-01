9fb3511373e638eeef6405c5a826442639066fcf
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2025-09-01 17:18:48 +0200

    feat(billing): add pending actions columns in my services
    ref: #MANAGER-19540
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

---
+++ packages/manager/modules/billing/src/autoRenew/services/services.constants.js
@@ -0,0 +1,12 @@
+export const ACTION_BADGE_CLASSES_MAP = {
+  autorenewInProgress: 'info',
+  deleteAtExpiration: 'error',
+  earlyRenewal: 'info',
+  terminate: 'error',
+  terminateAtEngagementDate: 'error',
+  terminateAtExpirationDate: 'error',
+};
+
+export default {
+  ACTION_BADGE_CLASSES_MAP,
+};
