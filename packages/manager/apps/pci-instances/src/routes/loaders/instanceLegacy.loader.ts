df36f28f67ae094ed384140ecc4435782610d011
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2025-09-01 17:15:30 +0200

    fix(pci-instances): validate instanceId params to avoid error on generating path
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

--- packages/manager/apps/pci-instances/src/routes/loaders/instanceLegacy.loader.ts
+++ packages/manager/apps/pci-instances/src/routes/loaders/instanceLegacy.loader.ts
@@ -11,7 +11,6 @@ type RedirectPattern = {
   expectedPath: string;
   redirectPath: string;
   queryParams?: string[];
-  validate?: (match: PathMatch) => boolean;
 };
 
 const REDIRECT_ROUTES: RedirectPattern[] = [
@@ -19,16 +18,22 @@ const REDIRECT_ROUTES: RedirectPattern[] = [
     expectedPath: '/pci/projects/:projectId/instances/:instanceId',
     redirectPath:
       '/pci/projects/:projectId/instances/region/:region/instance/:instanceId',
-    validate: (match: PathMatch) =>
-      'instanceId' in match.params &&
-      typeof match.params.instanceId === 'string' &&
-      uuidValidate(match.params.instanceId),
   },
   {
     expectedPath:
       '/pci/projects/:projectId/instances/:instanceId/billing/monthly/activate',
     redirectPath:
       '/pci/projects/:projectId/instances/region/:region/instance/:instanceId/billing/monthly/activate',
+  },
+  {
+    expectedPath: '/pci/projects/:projectId/instances/:instanceId/rescue/start',
+    redirectPath:
+      '/pci/projects/:projectId/instances/region/:region/instance/:instanceId/rescue/start',
+  },
+  {
+    expectedPath: '/pci/projects/:projectId/instances/:instanceId/rescue/end',
+    redirectPath:
+      '/pci/projects/:projectId/instances/region/:region/instance/:instanceId/rescue/end',
   },
   {
     expectedPath: '/pci/projects/:projectId/instances/:instanceId/:action',
@@ -42,45 +47,55 @@ const REDIRECT_ROUTES: RedirectPattern[] = [
 
 const PARAMS_REGEX = /\/:([\w-]+)(\?)?/g;
 
+const validateInstanceId = (match: PathMatch) => {
+  if ('instanceId' in match.params) {
+    return (
+      typeof match.params.instanceId === 'string' &&
+      uuidValidate(match.params.instanceId)
+    );
+  }
+
+  return true;
+};
+
 export const instanceLegacyRedirectionLoader: LoaderFunction = ({
   request,
 }) => {
   const { pathname, searchParams } = new URL(request.url);
 
-  for (const {
-    expectedPath,
-    redirectPath,
-    queryParams,
-    validate,
-  } of REDIRECT_ROUTES) {
+  const matchedRoute = REDIRECT_ROUTES.find(({ expectedPath }) => {
+    const match = matchPath(expectedPath, pathname);
+    return match !== null && validateInstanceId(match);
+  });
+
+  if (matchedRoute) {
+    const { expectedPath, redirectPath, queryParams } = matchedRoute;
     const match = matchPath(expectedPath, pathname);
 
-    if (match !== null && (!validate || validate(match))) {
-      const redirectAvailableParams = {
-        // Set all required parameters to null by default
-        ...Object.fromEntries(
-          [...redirectPath.matchAll(PARAMS_REGEX)].map(
-            ([, paramName]) => [paramName, 'null'] as [string, string],
-          ),
+    const redirectAvailableParams = {
+      // Set all required parameters to null by default
+      ...Object.fromEntries(
+        [...redirectPath.matchAll(PARAMS_REGEX)].map(
+          ([, paramName]) => [paramName, 'null'] as [string, string],
         ),
-        // If available from search params we use it
-        ...Object.fromEntries(searchParams.entries()),
-        // Match params has the bigger priority
-        ...(match.params as Record<string, string>),
-      };
+      ),
+      // If available from search params we use it
+      ...Object.fromEntries(searchParams.entries()),
+      // Match params has the bigger priority
+      ...((match as PathMatch).params as Record<string, string>),
+    };
 
-      const search = new URLSearchParams(
-        queryParams
-          ?.map((param) => [param, redirectAvailableParams[param]])
-          .filter((param): param is [string, string] => !!param[1]),
-      );
-      const redirectUrl = generatePath(
-        redirectPath,
-        redirectAvailableParams,
-      ).concat(search.size > 0 ? `?${search.toString()}` : '');
+    const search = new URLSearchParams(
+      queryParams
+        ?.map((param) => [param, redirectAvailableParams[param]])
+        .filter((param): param is [string, string] => !!param[1]),
+    );
+    const redirectUrl = generatePath(
+      redirectPath,
+      redirectAvailableParams,
+    ).concat(search.size > 0 ? `?${search.toString()}` : '');
 
-      return redirect(redirectUrl);
-    }
+    return redirect(redirectUrl);
   }
 
   return null;
