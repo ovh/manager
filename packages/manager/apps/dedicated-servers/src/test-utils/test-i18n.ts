f731fccb395783b98f76ed7b6064a6558674ebea
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-09-02 13:52:03 +0200

    feat(dedicated-servers): correct error path
    ref: #MANAGER-19047
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager/apps/dedicated-servers/src/test-utils/test-i18n.ts
+++ packages/manager/apps/dedicated-servers/src/test-utils/test-i18n.ts
@@ -1,7 +1,7 @@
 import cluster from '../../public/translations/cluster/Messages_fr_FR.json';
 import dedicated from '../../public/translations/dedicated-servers/Messages_fr_FR.json';
 import onboarding from '../../public/translations/onboarding/Messages_fr_FR.json';
-import error from '../../../../../manager-react-components/src/components/templates/error/translations/Messages_fr_FR.json';
+import error from '../../../../../manager-react-components/src/components/error/translations/Messages_fr_FR.json';
 
 export const translations = {
   cluster,
