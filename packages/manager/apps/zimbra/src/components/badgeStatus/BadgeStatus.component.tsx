5b61ef9a7ae37ed65d21ed1eb2e13ce394cb6913
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2025-09-02 18:22:51 +0200

    fix(zimbra): add detailed account status
    ref: #MANAGER-15520
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/apps/zimbra/src/components/badgeStatus/BadgeStatus.component.tsx
+++ packages/manager/apps/zimbra/src/components/badgeStatus/BadgeStatus.component.tsx
@@ -33,7 +33,7 @@ const getStatusColor = (status: keyof typeof ResourceStatus) => {
 };
 
 export const BadgeStatus: React.FC<BadgeStatusProps> = (props) => {
-  const { t } = useTranslation();
+  const { t } = useTranslation('common');
   const statusColor = useMemo(() => getStatusColor(props.status), [props.status]);
 
   return (
