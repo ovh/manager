d0a4d3e873f7229af344bad1d4d5c5a33a96bcd6
helabenkhalfallah <hela.ben-khalfallah@corp.ovh.com>
2025-09-03 18:08:29 +0200

    feat(*): shell client static kit integration
    ref: #MANAGER-17961
    
    Signed-off-by: helabenkhalfallah <hela.ben-khalfallah@corp.ovh.com>

---
+++ packages/manager/core/shell-client/eslint.config.mjs
@@ -0,0 +1,42 @@
+import { prettierEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/prettier';
+
+import { javascriptEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/javascript';
+import { typescriptEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/typescript';
+import { reactEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/react';
+import { a11yEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/a11y';
+import {
+  complexityJsxTsxConfig,
+  complexityTsJsConfig,
+} from '@ovh-ux/manager-static-analysis-kit/eslint/complexity';
+import { htmlEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/html';
+import { tailwindJsxConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/tailwind-jsx';
+import { tanStackQueryEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/tanstack';
+import { vitestEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/tests';
+
+export default [
+  javascriptEslintConfig,
+  typescriptEslintConfig,
+  reactEslintConfig,
+  a11yEslintConfig,
+  htmlEslintConfig,
+  tailwindJsxConfig,
+  tanStackQueryEslintConfig,
+  vitestEslintConfig,
+  prettierEslintConfig,
+  complexityJsxTsxConfig,
+  complexityTsJsConfig,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.eslint.json",
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      'react-hooks/exhaustive-deps': 'off'
+    },
+  },
+];
--- packages/manager/core/shell-client/src/hooks/useRouting.ts
+++ packages/manager/core/shell-client/src/hooks/useRouting.ts
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+
 import { ShellContext } from '../ShellContext';
 
 /**
@@ -7,7 +8,7 @@ import { ShellContext } from '../ShellContext';
 export const useRouting = () => {
   const { shell } = useContext(ShellContext);
 
-  return shell.routing;
+  return shell?.routing;
 };
 
 export default useRouting;
