9e33a84259270a81a658c762dd64c43ffdc4a87c
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-09-03 16:26:01 +0200

    feat(manager-react-components): add datagrid new component
    ref: #MANAGER-19368
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-react-components/src/components/index.ts
+++ packages/manager-react-components/src/components/index.ts
@@ -10,11 +10,6 @@ export * from './tiles-input-group';
 export * from './tiles-input';
 
 export * from './Link';
-export * from './datagrid/datagrid.component';
-export * from './datagrid/text-cell.component';
-export * from './datagrid/useDatagrid';
-export * from './datagrid/useDatagridSearchParams';
-export * from './datagrid/clipboard-cell.component';
 
 export * from './drawer/Drawer.component';
 export * from './drawer/DrawerCollapsible.component';
@@ -47,3 +42,4 @@ export * from './update-name-modal';
 export * from './delete-modal';
 export * from './grid-layout';
 export * from './onboarding-layout';
+export * from './datagrid';
