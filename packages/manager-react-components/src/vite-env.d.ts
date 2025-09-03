44da147a3f8777f42795107feecfe10bacb3ebd7
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-09-03 18:06:27 +0200

    feat(manager-react-components): ignore manager-tools from the old lint
    ref: #MANAGER-17961
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-react-components/src/vite-env.d.ts
+++ packages/manager-react-components/src/vite-env.d.ts
@@ -1,6 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/naming-convention, no-underscore-dangle
 declare const __VERSION__: string;
-// eslint-disable-next-line @typescript-eslint/naming-convention, no-underscore-dangle
 declare const __REGION__: string;
 declare module '*.jpg' {
   const path: string;
