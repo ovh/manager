ad26e664004af755cb7a635cd9a568fc3f4c1fdf
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-09-03 16:24:19 +0200

    feat(dedicated): add behavior during zerto delivering status
    ref: #MANAGER-19089
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/components/dedicated-cloud/datacenter/zerto/listing/dedicatedCloud-datacenter-zerto-listing.controller.js
+++ packages/manager/apps/dedicated/client/app/components/dedicated-cloud/datacenter/zerto/listing/dedicatedCloud-datacenter-zerto-listing.controller.js
@@ -1,7 +1,14 @@
 import { LABELS } from '../../../dedicatedCloud.constant';
+import { DEDICATEDCLOUD_DATACENTER_DRP_STATUS } from '../dedicatedCloud-datacenter-zerto.constants';
 
 export default class {
   constructor() {
     this.ZERTO = LABELS.ZERTO;
+  }
+
+  $onInit() {
+    this.isDelivering =
+      this.currentZerto.state ===
+      DEDICATEDCLOUD_DATACENTER_DRP_STATUS.delivering;
   }
 }
