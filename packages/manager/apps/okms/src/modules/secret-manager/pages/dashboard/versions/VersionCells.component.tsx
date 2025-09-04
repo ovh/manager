df6802b497db98d8e2d460db08f160bdac55c00e
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-09-04 16:57:11 +0200

    feat(okms): add version list value drawer
    ref: #MANAGER-19513
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

--- packages/manager/apps/okms/src/modules/secret-manager/pages/dashboard/versions/VersionCells.component.tsx
+++ packages/manager/apps/okms/src/modules/secret-manager/pages/dashboard/versions/VersionCells.component.tsx
@@ -1,12 +1,57 @@
 import React from 'react';
-import { DataGridTextCell } from '@ovh-ux/manager-react-components';
-import { SecretVersion } from '@secret-manager/types/secret.type';
+import { useNavigate, useParams } from 'react-router-dom';
+import {
+  LocationPathParams,
+  SECRET_MANAGER_ROUTES_URLS,
+} from '@secret-manager/routes/routes.constants';
+import {
+  DataGridTextCell,
+  ManagerLink,
+} from '@ovh-ux/manager-react-components';
+import {
+  SecretVersion,
+  SecretVersionState,
+} from '@secret-manager/types/secret.type';
 import { VersionState } from '@secret-manager/components/VersionState/VersionState.component';
 import { useFormatDate } from '@/common/hooks/useFormatDate';
+import { kmsIamActions } from '@/utils/iam/iam.constants';
 
-export const VersionCellID = (version: SecretVersion) => (
-  <DataGridTextCell>{version.id}</DataGridTextCell>
-);
+const versionCellIdDisabled: Record<SecretVersionState, boolean> = {
+  ACTIVE: false,
+  DEACTIVATED: true,
+  DELETED: true,
+};
+
+export const VersionCellID = ({
+  version,
+  urn,
+}: {
+  version: SecretVersion;
+  urn: string;
+}) => {
+  const navigate = useNavigate();
+  const { domainId, secretPath } = useParams<LocationPathParams>();
+
+  return (
+    <ManagerLink
+      label={version.id.toString()}
+      href={null}
+      isDisabled={versionCellIdDisabled[version.state]}
+      onClick={() => {
+        navigate(
+          SECRET_MANAGER_ROUTES_URLS.secretVersionsDrawerRevealVersionValue(
+            domainId,
+            secretPath,
+            version.id,
+          ),
+        );
+      }}
+      urn={urn}
+      iamActions={[kmsIamActions.secretGet, kmsIamActions.secretVersionGetData]}
+      isDisplayTooltip
+    />
+  );
+};
 
 export const VersionCellState = (version: SecretVersion) => (
   <VersionState state={version.state} />
--- packages/manager/apps/okms/src/modules/secret-manager/pages/dashboard/versions/Versions.page.spec.tsx
+++ packages/manager/apps/okms/src/modules/secret-manager/pages/dashboard/versions/Versions.page.spec.tsx
@@ -1,8 +1,15 @@
+import userEvent from '@testing-library/user-event';
+import { act } from '@testing-library/react';
 import { assertTextVisibility } from '@ovh-ux/manager-core-test-utils';
 import { SECRET_MANAGER_ROUTES_URLS } from '@secret-manager/routes/routes.constants';
-import { versionsMock } from '@secret-manager/mocks/versions/versions.mock';
+import {
+  versionActiveMock,
+  versionsMock,
+} from '@secret-manager/mocks/versions/versions.mock';
+import { mockSecret1 } from '@secret-manager/mocks/secrets/secrets.mock';
 import { labels } from '@/utils/tests/init.i18n';
 import { renderTestApp } from '@/utils/tests/renderTestApp';
+import { getOdsButtonByLabel } from '@/utils/tests/uiTestHelpers';
 
 const mockDomainId = '123123';
 const mockSecretPath = 'a/secret';
@@ -31,5 +38,29 @@ describe('Secret versions page test suite', () => {
 
     // THEN
     await assertVersionDatagridVisilibity();
+  });
+
+  it('navigate to reveal value drawer on click on active version', async () => {
+    // GIVEN
+    const user = userEvent.setup();
+
+    const { container } = await renderTestApp(
+      SECRET_MANAGER_ROUTES_URLS.secretVersions(mockDomainId, mockSecret1.path),
+    );
+
+    await assertVersionDatagridVisilibity();
+
+    // WHEN
+    const versionLink = await getOdsButtonByLabel({
+      container,
+      label: versionActiveMock.id.toString(),
+      isLink: true,
+      disabled: false,
+    });
+
+    await act(() => user.click(versionLink));
+
+    // THEN
+    await assertTextVisibility(labels.secretManager.values);
   });
 });
