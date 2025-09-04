659ed9f8e6fdb1ce3023614cb09cf2de4e03e157
helabenkhalfallah <hela.ben-khalfallah@corp.ovh.com>
2025-09-04 20:06:00 +0200

    fix(*): resolve release lint issues
    ref: #MANAGER-19013
    
    Signed-off-by: helabenkhalfallah <hela.ben-khalfallah@corp.ovh.com>

--- packages/manager/apps/pci-instances/src/hooks/hidePreloader/useHidePreloader.ts
+++ packages/manager/apps/pci-instances/src/hooks/hidePreloader/useHidePreloader.ts
@@ -2,8 +2,8 @@ import { ShellContext } from '@ovh-ux/manager-react-shell-client';
 import { useContext, useEffect } from 'react';
 
 export const useHidePreloader = () => {
-  const { ux } = useContext(ShellContext).shell || {};
+  const { ux } = useContext(ShellContext).shell;
   useEffect(() => {
-    ux?.hidePreloader();
+    ux.hidePreloader();
   }, [ux]);
 };
