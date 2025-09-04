153fe581986089379076cc682232e8c5466a2f9f
Atef ZAAFOURI <atefzed@gmail.com>
2025-09-04 15:52:47 +0200

    feat(zimbra): show renewal & cancel date on tooltip
    ref: #MANAGER-18408
    
    Signed-off-by: Atef ZAAFOURI <atefzed@gmail.com>

--- packages/manager/apps/zimbra/src/components/billingStateBadge/BillingStateBadge.component.tsx
+++ packages/manager/apps/zimbra/src/components/billingStateBadge/BillingStateBadge.component.tsx
@@ -1,11 +1,25 @@
 import React, { useMemo } from 'react';
-import { OdsBadge, OdsSkeleton } from '@ovhcloud/ods-components/react';
-import { ODS_BADGE_COLOR } from '@ovhcloud/ods-components';
+
 import { useTranslation } from 'react-i18next';
-import { ServiceBillingState } from '@/data/api';
+
+import {
+  ODS_BADGE_COLOR,
+  ODS_BADGE_ICON_ALIGNMENT,
+  ODS_ICON_NAME,
+  ODS_TEXT_PRESET,
+} from '@ovhcloud/ods-components';
+import {
+  OdsBadge,
+  OdsSkeleton,
+  OdsText,
+  OdsTooltip,
+} from '@ovhcloud/ods-components/react';
+import { useFormatDate } from '@ovh-ux/manager-react-components';
+
+import { ServiceBillingState, SlotService } from '@/data/api';
 
 export type BillingStateBadgeProps = {
-  state?: keyof typeof ServiceBillingState;
+  service?: SlotService;
   isLoading?: boolean;
   'data-testid'?: string;
 };
@@ -39,18 +53,26 @@ const getKey = (state: keyof typeof ServiceBillingState) => {
   }
 };
 
+const BILLING_STATE_TOOLTIP: Record<string, string> = {
+  [ServiceBillingState.AUTOMATIC_RENEWAL]:
+    'service_billing_state_automatic_renewal_tooltip',
+  [ServiceBillingState.CANCELATION_PLANNED]:
+    'service_billing_state_cancelation_planned_tooltip',
+};
+
 export const BillingStateBadge: React.FC<BillingStateBadgeProps> = (props) => {
   const { t } = useTranslation('common');
-  const { state, isLoading } = props;
+  const { service, isLoading } = props;
+  const format = useFormatDate();
 
   const { color, label } = useMemo(() => {
     return {
-      color: getColor(state),
-      label: t(getKey(state)),
+      color: getColor(service?.state),
+      label: t(getKey(service?.state)),
     };
-  }, [state]);
+  }, [service]);
 
-  if (!state || isLoading) {
+  if (!service || isLoading) {
     return (
       <OdsSkeleton
         data-testid="billing-state-loading"
@@ -60,7 +82,35 @@ export const BillingStateBadge: React.FC<BillingStateBadgeProps> = (props) => {
   }
 
   return (
-    <OdsBadge data-testid={props['data-testid']} color={color} label={label} />
+    <>
+      <OdsBadge
+        data-testid={props['data-testid']}
+        id={`service-${service.id}`}
+        color={color}
+        label={label}
+        iconAlignment={ODS_BADGE_ICON_ALIGNMENT.right}
+        {...(BILLING_STATE_TOOLTIP[service.state]
+          ? { icon: ODS_ICON_NAME.circleInfo }
+          : {})}
+      />
+      {BILLING_STATE_TOOLTIP[service.state] && (
+        <OdsTooltip
+          withArrow
+          position="bottom"
+          triggerId={`service-${service.id}`}
+        >
+          <div className="flex flex-col text-center gap-4">
+            <OdsText preset={ODS_TEXT_PRESET.paragraph}>
+              {t(BILLING_STATE_TOOLTIP[service.state])}
+            </OdsText>
+
+            <OdsText preset={ODS_TEXT_PRESET.paragraph}>
+              {format({ date: service?.nextBillingDate, format: 'P' })}
+            </OdsText>
+          </div>
+        </OdsTooltip>
+      )}
+    </>
   );
 };
 
