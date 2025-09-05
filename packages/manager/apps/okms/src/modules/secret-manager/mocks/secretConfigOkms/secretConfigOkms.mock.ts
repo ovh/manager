5e5f787a8214d214219c01b24f2e78dd214338c2
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-09-05 08:59:29 +0200

    feat(okms): secret settings smart display
    ref: #MANAGER-19067
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

---
+++ packages/manager/apps/okms/src/modules/secret-manager/mocks/secretConfigOkms/secretConfigOkms.mock.ts
@@ -0,0 +1,7 @@
+import { SecretConfig } from '@secret-manager/types/secret.type';
+
+export const mockSecretConfigOkms: SecretConfig = {
+  casRequired: true,
+  deactivateVersionAfter: '11d11h11m11s',
+  maxVersions: 5,
+};
