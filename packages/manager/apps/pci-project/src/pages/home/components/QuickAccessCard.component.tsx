c183758c12a91660e2e553e226d7bb75c3b099d8
StephaneRavet <ravet.st@gmail.com>
2025-09-10 15:25:33 +0200

    refactor(pci-project): fix QA bug analysis & refactor dashboard item
    ref: #MANAGER-18604
    
    Signed-off-by: StephaneRavet <ravet.st@gmail.com>

--- packages/manager/apps/pci-project/src/pages/home/components/QuickAccessCard.component.tsx
+++ packages/manager/apps/pci-project/src/pages/home/components/QuickAccessCard.component.tsx
@@ -1,30 +1,25 @@
 import { Link } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import { OdsCard, OdsText } from '@ovhcloud/ods-components/react';
+import { OdsCard, OdsText, OdsIcon } from '@ovhcloud/ods-components/react';
 
-export type QuickAccessItem = {
-  icon: string;
-  title: string;
-  description: string;
-  link: string;
-};
+import useTranslation from '@/hooks/usePermissiveTranslation.hook';
+import { DashboardTileItem } from '../DashboardTile.types';
 
 type QuickAccessCardProps = {
-  item: QuickAccessItem;
+  item: DashboardTileItem;
   index: number;
 };
 
-export function QuickAccessCard({ item, index }: QuickAccessCardProps) {
+function QuickAccessCard({ item, index }: QuickAccessCardProps) {
   const { t } = useTranslation('project');
 
-  return (
+  return item.link ? (
     <Link
       to={item.link}
       className="no-underline"
       key={index}
       aria-label={t('pci_project_quick_access_card_aria_label', {
-        title: item.title,
-        description: item.description,
+        title: t(item.labelTranslationKey),
+        description: t(item.descriptionTranslationKey),
       })}
     >
       <OdsCard
@@ -34,29 +29,73 @@ export function QuickAccessCard({ item, index }: QuickAccessCardProps) {
         tabIndex={0}
       >
         <div className="bg-[var(--ods-color-information-700)] rounded flex items-center justify-center w-23 h-23">
-          <img
-            src={item.icon}
-            alt={t('pci_project_quick_access_icon_alt', {
-              service: item.title,
-            })}
-            className="w-20 h-20"
-          />
+          {item.iconImage && (
+            <img
+              src={item.iconImage}
+              alt={t('pci_project_quick_access_icon_alt', {
+                service: t(item.labelTranslationKey),
+              })}
+              className="w-20 h-20"
+            />
+          )}
+          {!item.iconImage && item.iconODS && (
+            <OdsIcon name={item.iconODS} className="w-20 h-20 text-white" />
+          )}
         </div>
         <div className="flex flex-col justify-center ml-6">
           <OdsText preset="heading-4" className="mb-1 leading-tight">
-            {item.title}
+            {t(item.labelTranslationKey)}
           </OdsText>
           <OdsText
             preset="paragraph"
             className="text-primary-500"
             aria-label={t('pci_project_quick_access_link_aria_label', {
-              description: item.description,
+              description: t(item.descriptionTranslationKey),
             })}
           >
-            {item.description}
+            {t(item.descriptionTranslationKey)}
           </OdsText>
         </div>
       </OdsCard>
     </Link>
+  ) : (
+    <OdsCard
+      key={index}
+      color="neutral"
+      className="flex flex-row items-center p-4 min-h-[100px] truncate"
+      role="button"
+      tabIndex={0}
+    >
+      <div className="bg-[var(--ods-color-information-700)] rounded flex items-center justify-center w-23 h-23">
+        {item.iconImage && (
+          <img
+            src={item.iconImage}
+            alt={t('pci_project_quick_access_icon_alt', {
+              service: t(item.labelTranslationKey),
+            })}
+            className="w-20 h-20"
+          />
+        )}
+        {!item.iconImage && item.iconODS && (
+          <OdsIcon name={item.iconODS} className="w-20 h-20 text-white" />
+        )}
+      </div>
+      <div className="flex flex-col justify-center ml-6">
+        <OdsText preset="heading-4" className="mb-1 leading-tight">
+          {t(item.labelTranslationKey)}
+        </OdsText>
+        <OdsText
+          preset="paragraph"
+          className="text-primary-500"
+          aria-label={t('pci_project_quick_access_link_aria_label', {
+            description: t(item.descriptionTranslationKey),
+          })}
+        >
+          {t(item.descriptionTranslationKey)}
+        </OdsText>
+      </div>
+    </OdsCard>
   );
 }
+
+export default QuickAccessCard;
