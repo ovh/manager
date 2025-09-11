38695050e6fc5eb96ec801ff927e4f6370cff6f1
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2025-09-11 12:34:40 +0200

    fix(pci-instances): handle hashes conflict between dashboard and creation
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

--- packages/manager/apps/pci-instances/src/hooks/migration/useSuspendNonMigratedRoutes.tsx
+++ packages/manager/apps/pci-instances/src/hooks/migration/useSuspendNonMigratedRoutes.tsx
@@ -2,17 +2,34 @@ import { ComponentType, ReactNode } from 'react';
 import { ShellContextType } from '@ovh-ux/manager-react-shell-client';
 import { LoaderFunction, matchPath, useLoaderData } from 'react-router-dom';
 
+const DASHBOARD_HASH = '/pci/projects/:projectId/instances/:instanceId';
+
 export const suspendNonMigratedRoutesLoader = (
   shell: ShellContextType,
   loaderCallback?: LoaderFunction,
 ): LoaderFunction => {
-  const routes =
+  const containerHashes =
     shell.environment.applications['pci-instances']?.container.hashes;
+
+  const routes = new Set(containerHashes);
+
+  const hasAccessToDashboard = Array.from(routes).some((hash) =>
+    hash.includes(':instanceId'),
+  );
+  const dashboardHashAlreadyExists = Array.from(routes).find(
+    (elt) => elt === DASHBOARD_HASH,
+  );
 
   return (loaderParams, ...restArgs) => {
     const { pathname } = new URL(loaderParams.request.url);
+
+    if (!pathname.endsWith('/new') && !dashboardHashAlreadyExists) {
+      if (hasAccessToDashboard) routes.add(DASHBOARD_HASH);
+    }
+
     const isRouteAvailable =
-      !routes || routes.some((r) => matchPath(r, pathname) !== null);
+      !routes.size ||
+      Array.from(routes).some((r) => matchPath(r, pathname) !== null);
 
     // We send an object to trigger the redirection and handle it inside the `WithSuspendedMigratedRoutes` HOC
     if (!isRouteAvailable) return { routeMigrated: false };
