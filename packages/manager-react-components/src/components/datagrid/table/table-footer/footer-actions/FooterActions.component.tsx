4fd63c8a746e47b1ad81818e55f7e5b1215bce41
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-09-11 11:04:52 +0200

    feat(manager-react-components): add footer table component
    ref: #MANAGER-19373
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

---
+++ packages/manager-react-components/src/components/datagrid/table/table-footer/footer-actions/FooterActions.component.tsx
@@ -0,0 +1,48 @@
+import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { BUTTON_VARIANT } from '@ovhcloud/ods-react';
+import { Button } from '../../../../button';
+
+export const FooterActionsComponent = ({
+  hasNextPage,
+  onFetchAllPages,
+  onFetchNextPage,
+  isLoading,
+}: {
+  hasNextPage?: boolean;
+  onFetchNextPage?: () => void;
+  onFetchAllPages?: () => void;
+  isLoading?: boolean;
+}) => {
+  const { t } = useTranslation('datagrid');
+  return (
+    <>
+      {hasNextPage ? (
+        <div className="flex justify-center gap-5 my-5">
+          {onFetchNextPage && (
+            <Button
+              data-testid="load-more-btn"
+              variant={BUTTON_VARIANT.outline}
+              onClick={onFetchNextPage}
+              loading={isLoading}
+            >
+              {t('common_pagination_load_more')}
+            </Button>
+          )}
+          {onFetchAllPages && (
+            <Button
+              data-testid="load-all-btn"
+              variant={BUTTON_VARIANT.outline}
+              onClick={onFetchAllPages}
+              loading={isLoading}
+            >
+              {t('common_pagination_load_all')}
+            </Button>
+          )}
+        </div>
+      ) : null}
+    </>
+  );
+};
+
+export const FooterActions = memo(FooterActionsComponent);
