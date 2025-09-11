65229d4a55b9f5e4367ae02bbf3e59073b50144d
Arthur Bullet <arthur.bullet@ovhcloud.com>
2025-09-11 17:11:43 +0200

    feat(ai-tools): delete ai tools angular code as it have been migrated to react
    ref: #DATATR-2347
    
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/ai/ai.constants.js
+++
@@ -1,8 +0,0 @@
-export const DISCORD_URL = 'https://discord.com/invite/vXVurFfwe9';
-export const DOC_DOCKER_BUILD_URL =
-  'https://docs.ovh.com/gb/en/publiccloud/ai/training/build-use-custom-image/';
-
-export default {
-  DISCORD_URL,
-  DOC_DOCKER_BUILD_URL,
-};
