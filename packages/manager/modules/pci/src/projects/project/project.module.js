a2b44fa1c69f9ed6990a4531b70e6dd8b90ca745
Arthur Bullet <arthur.bullet@ovhcloud.com>
2025-09-11 17:11:43 +0200

    feat(ai-tools): delete ai tools angular code as it have been migrated to react
    ref: #DATATR-2347
    
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>

--- packages/manager/modules/pci/src/projects/project/project.module.js
+++ packages/manager/modules/pci/src/projects/project/project.module.js
@@ -18,20 +18,16 @@ import components from './components';
 import inactive from './inactive';
 import instances from './instances';
 import loadBalancer from './load-balancer';
-import notebooks from './notebooks';
-import ai from './ai';
 import quota from './quota';
 import quotaExceedError from './quota-exceed-error';
 import storages from './storages';
 import users from './users';
 import regions from './regions';
 import routing from './project.routing';
-import training from './training';
 import workflow from './workflow';
 import pciAnnouncementBanner from '../../components/pci-announcement-banner';
 import pciMaintenanceBanner from '../../components/pci-maintenance-banner';
 import pciFreeLocalZonesBanner from '../../components/pci-free-local-zones-banner';
-import aiDashboard from './ai-dashboard';
 import projectComponent from './project.component';
 import service from './project.service';
 import pciProjectNew from '../new/service';
@@ -55,8 +51,6 @@ angular
     inactive,
     instances,
     loadBalancer,
-    notebooks,
-    ai,
     quota,
     quotaExceedError,
     regions,
@@ -68,11 +62,9 @@ angular
     storages,
     users,
     workflow,
-    training,
     pciAnnouncementBanner,
     pciMaintenanceBanner,
     pciFreeLocalZonesBanner,
-    aiDashboard,
   ])
   .config(routing)
   .component('pciProject', projectComponent)
