f810a587e2e99ea1dbe9a11012cd1814f1732a40
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-09-11 12:10:34 +0200

    fix(ips): fix quick filter and ui feedbacks
    ref: #MANAGER-19696
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager/apps/ips/src/pages/listing/ipListing/components/filters/FilterQuick.tsx
+++ packages/manager/apps/ips/src/pages/listing/ipListing/components/filters/FilterQuick.tsx
@@ -1,134 +1,103 @@
-import React, { useContext, useState, useRef, useEffect } from 'react';
+import React, { useContext } from 'react';
 import {
   ODS_BUTTON_SIZE,
   ODS_BUTTON_VARIANT,
   ODS_ICON_NAME,
 } from '@ovhcloud/ods-components';
-import { OdsButton, OdsText } from '@ovhcloud/ods-components/react';
+import {
+  OdsButton,
+  OdsText,
+  OdsPopover,
+  OdsCheckbox,
+  OdsDivider,
+} from '@ovhcloud/ods-components/react';
 import { useTranslation } from 'react-i18next';
 import { ListingContext } from '@/pages/listing/listingContext';
 
 export const QuickFilter = ({ className }: { className?: string }) => {
   const { t } = useTranslation('listing');
   const { apiFilter, setApiFilter } = useContext(ListingContext);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [selectedFilters, setSelectedFilters] = useState({
-    showIPv4: true,
-    showIPv6: true,
-    showParkedIps: false,
-  });
-  const menuRef = useRef<HTMLDivElement>(null);
-
-  const toggleMenu = () => {
-    setIsMenuOpen((menuState) => !menuState);
-  };
-
-  const handleFilterChange = (filterName: string) => (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    const newValue = event.target.checked;
-
-    // Prevent unchecking both IPv4 & IPv6
-    if ((filterName === 'showIPv4' || filterName === 'showIPv6') && !newValue) {
-      const otherFilter = filterName === 'showIPv4' ? 'showIPv6' : 'showIPv4';
-
-      // If the other filter is already unchecked, prevent this uncheck
-      if (!selectedFilters[otherFilter]) {
-        return;
-      }
-    }
-
-    setSelectedFilters((filters) => ({ ...filters, [filterName]: newValue }));
-  };
-  useEffect(() => {
-    let version: number | undefined;
-    if (selectedFilters.showIPv4 && !selectedFilters.showIPv6) {
-      version = 4;
-    } else if (!selectedFilters.showIPv4 && selectedFilters.showIPv6) {
-      version = 6;
-    }
-
-    // Set routedToserviceName to null if showing parked IPs
-    const routedToserviceName: string = selectedFilters.showParkedIps
-      ? null
-      : undefined;
-
-    setApiFilter({
-      ...apiFilter,
-      version,
-      routedToserviceName,
-    });
-  }, [selectedFilters]);
-
-  // Close menu when clicking outside
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
-        setIsMenuOpen(false);
-      }
-    };
-
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
 
   return (
-    <div className={`relative ${className}`} ref={menuRef}>
+    <>
       <OdsButton
+        className={className}
+        isLoading={!apiFilter}
         id="quick-filters"
         icon={ODS_ICON_NAME.filter}
         size={ODS_BUTTON_SIZE.sm}
         variant={ODS_BUTTON_VARIANT.outline}
-        onClick={toggleMenu}
         aria-haspopup
-        aria-expanded={isMenuOpen}
         label={t('listingQuickFilterLabel')}
         data-testid="quick-filter"
       />
-
-      {isMenuOpen && (
-        <div className="absolute z-10 mt-2 w-56 bg-white rounded-md shadow-lg">
-          {/* IPv4 */}
-          <label className="p-2 flex items-center hover:bg-gray-100 cursor-pointer">
-            <input
-              type="checkbox"
-              checked={selectedFilters.showIPv4}
-              onChange={handleFilterChange('showIPv4')}
-              disabled={!selectedFilters.showIPv4 && !selectedFilters.showIPv6}
+      <OdsPopover triggerId="quick-filters" withArrow>
+        <div className="gap-2">
+          <label
+            htmlFor="show-ipv4"
+            className="p-2 flex items-center hover:bg-gray-100 cursor-pointer"
+          >
+            <OdsCheckbox
+              name="show-ipv4"
+              inputId="show-ipv4"
+              isChecked={[4, undefined].includes(apiFilter?.version)}
+              onOdsChange={(e) => {
+                setApiFilter((prev) => ({
+                  ...prev,
+                  version: !e.detail.checked ? 6 : undefined,
+                }));
+              }}
+              isDisabled={apiFilter?.version === 4}
             />
-            <OdsText className="pl-1">
+            <OdsText className="ml-1">
               {t('listingQuickFilterShowIPv4')}
             </OdsText>
           </label>
 
-          {/* IPv6 */}
-          <label className="p-2 flex items-center hover:bg-gray-100 cursor-pointer">
-            <input
-              type="checkbox"
-              checked={selectedFilters.showIPv6}
-              onChange={handleFilterChange('showIPv6')}
-              disabled={!selectedFilters.showIPv6 && !selectedFilters.showIPv4}
+          <label
+            htmlFor="show-ipv6"
+            className="p-2 flex items-center hover:bg-gray-100 cursor-pointer"
+          >
+            <OdsCheckbox
+              name="show-ipv6"
+              inputId="show-ipv6"
+              isChecked={[6, undefined].includes(apiFilter?.version)}
+              onOdsChange={(e) => {
+                setApiFilter((prev) => ({
+                  ...prev,
+                  version: !e.detail.checked ? 4 : undefined,
+                }));
+              }}
+              isDisabled={apiFilter?.version === 6}
             />
-            <OdsText className="pl-1">
+            <OdsText className="ml-1">
               {t('listingQuickFilterShowIPv6')}
             </OdsText>
           </label>
+          <OdsDivider />
 
-          {/* Parked IPs */}
-          <label className="p-2 flex items-center hover:bg-gray-100 cursor-pointer">
-            <input
-              type="checkbox"
-              checked={selectedFilters.showParkedIps}
-              onChange={handleFilterChange('showParkedIps')}
+          <label
+            htmlFor="show-parked-ips"
+            className="p-2 flex items-center hover:bg-gray-100 cursor-pointer"
+          >
+            <OdsCheckbox
+              name="show-parked-ips"
+              inputId="show-parked-ips"
+              isChecked={apiFilter?.['routedTo.serviceName'] === null}
+              onOdsChange={(e) => {
+                setApiFilter((prev) => ({
+                  ...prev,
+                  // Set routedToserviceName to null if showing ONLY parked IPs
+                  'routedTo.serviceName': e.detail.checked ? null : undefined,
+                }));
+              }}
             />
-            <OdsText className="pl-1">
+            <OdsText className="ml-1">
               {t('listingQuickFilterShowParkedIps')}
             </OdsText>
           </label>
         </div>
-      )}
-    </div>
+      </OdsPopover>
+    </>
   );
 };
