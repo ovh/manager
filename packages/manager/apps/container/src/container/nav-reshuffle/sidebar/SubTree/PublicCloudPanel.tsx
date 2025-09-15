d11cc6d3f921c9f671c4961fa8a0ab7c4c5b10de
Kylian Bainvel <kylian.bainvel@ovhcloud.com>
2025-09-15 12:16:25 +0200

    fix(container): fix hideIfEmpty props
    ref: #MANAGER-19760
    
    Signed-off-by: Kylian Bainvel <kylian.bainvel@ovhcloud.com>

--- packages/manager/apps/container/src/container/nav-reshuffle/sidebar/SubTree/PublicCloudPanel.tsx
+++ packages/manager/apps/container/src/container/nav-reshuffle/sidebar/SubTree/PublicCloudPanel.tsx
@@ -215,7 +215,7 @@ export const PublicCloudPanel: React.FC<ComponentProps<
       </li>
       {selectedPciProject !== null &&
         rootNode.children
-          ?.filter((childNode) => !shouldHideElement(childNode, true))
+          ?.filter((childNode) => !shouldHideElement(childNode, childNode.hasService ?? true))
           .map((node) => (
             <li
               key={node.id}
