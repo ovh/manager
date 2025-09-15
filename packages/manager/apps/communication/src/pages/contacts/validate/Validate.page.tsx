fed760c0c2bf544a5572b641052abc314f8d7b47
Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
2025-09-15 15:54:18 +0200

    feat(communication): add contacts creation and edit modal
    ref: #MANAGER-18682
    
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>

---
+++ packages/manager/apps/communication/src/pages/contacts/validate/Validate.page.tsx
@@ -0,0 +1,97 @@
+import { Modal, useNotifications } from '@ovh-ux/manager-react-components';
+import { Trans, useTranslation } from 'react-i18next';
+import { useNavigate, useParams } from 'react-router-dom';
+import { OdsMessage, OdsText } from '@ovhcloud/ods-components/react';
+import { NAMESPACES } from '@ovh-ux/manager-common-translations';
+import { useRef, useState } from 'react';
+import { ODS_TEXT_PRESET } from '@ovhcloud/ods-components';
+import { urls } from '@/routes/routes.constant';
+import {
+  useContactMean,
+  useValidateContactMean,
+} from '@/data/hooks/useContactMean/useContactMean';
+import { useAuthorization, usePendingRedirect } from '@/hooks';
+import ContactValidateForm from '@/components/contactValidateForm/contactValidateForm.component';
+
+export default function ValidateContactPage() {
+  const { t } = useTranslation('contacts');
+  const { contactMeanId } = useParams();
+  const { isAuthorized, isLoading: isLoadingAuthorization } = useAuthorization([
+    'account:apiovh:notification/contactMean/validate',
+  ]);
+
+  const { addSuccess } = useNotifications();
+  const [error, setError] = useState<string | null>(null);
+  const { t: tActions } = useTranslation(NAMESPACES.ACTIONS);
+  const navigate = useNavigate();
+  const formRef = useRef<{ submit: () => void }>(null);
+  const { data: contactMean, isLoading: isLoadingContactMean } = useContactMean(
+    {
+      contactMeanId,
+      enabled: !!contactMeanId && isAuthorized,
+    },
+  );
+
+  const {
+    mutate: validateContactMean,
+    isPending: isValidatePending,
+  } = useValidateContactMean({
+    contactMeanId: contactMean?.id || '',
+    onSuccess: () => {
+      addSuccess(t('add_contact_success_message'));
+      navigate(urls.ContactsTab);
+    },
+    onError: () => {
+      setError(t('verify_contact_error_message'));
+    },
+  });
+
+  const onSubmitValidate = (data: { otp: string }) => validateContactMean(data);
+  const isLoading = isLoadingContactMean || isLoadingAuthorization;
+
+  usePendingRedirect({
+    isLoading: isLoadingAuthorization,
+    isAuthorized,
+    condition: isAuthorized,
+    redirectTo: urls.ContactsTab,
+  });
+
+  return (
+    <Modal
+      heading={t('verify_contact_modal_title')}
+      isOpen={true}
+      isLoading={isLoading}
+      onDismiss={() => navigate(urls.ContactsTab)}
+      primaryLabel={tActions('validate')}
+      secondaryLabel={tActions('cancel')}
+      onSecondaryButtonClick={() => {
+        navigate(urls.ContactsTab);
+      }}
+      onPrimaryButtonClick={() => formRef.current?.submit()}
+      isPrimaryButtonLoading={isValidatePending}
+    >
+      <div className="flex flex-col gap-4 my-4">
+        {error && (
+          <OdsMessage
+            color="danger"
+            isDismissible={true}
+            className="w-full"
+            onOdsRemove={() => setError(null)}
+          >
+            {error}
+          </OdsMessage>
+        )}
+        <OdsText preset={ODS_TEXT_PRESET.paragraph}>
+          <Trans
+            t={t}
+            i18nKey="verify_contact_contact_email_info"
+            values={{
+              email: contactMean?.email,
+            }}
+          />
+        </OdsText>
+        <ContactValidateForm ref={formRef} onSubmit={onSubmitValidate} />
+      </div>
+    </Modal>
+  );
+}
