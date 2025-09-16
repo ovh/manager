56190df93124eb4853c2ad1caaea8c68d65ecd2a
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2025-09-16 17:20:55 +0200

    feat(pci-instances): refetch instance dashboard on window focus
    ref: #TAPC-5030
    
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

--- packages/manager/apps/pci-instances/src/pages/instances/instance/dashboard/hooks/useDashboard.ts
+++ packages/manager/apps/pci-instances/src/pages/instances/instance/dashboard/hooks/useDashboard.ts
@@ -16,6 +16,7 @@ export const useDashboard = ({ region, instanceId }: TUseDashboardArgs) => {
     params: ['withBackups', 'withImage', 'withNetworks', 'withVolumes'],
     queryOptions: {
       gcTime: 0,
+      refetchOnWindowFocus: 'always',
     },
   });
 
