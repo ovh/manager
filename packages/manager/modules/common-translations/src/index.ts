6e8379614802ca78eb55a6dc4de386b3b01afac6
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-09-16 10:44:05 +0200

    feat(common-translations): add new translations
    resolves: #19228
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/common-translations/src/index.ts
+++ packages/manager/modules/common-translations/src/index.ts
@@ -20,4 +20,7 @@ export const NAMESPACES = {
   COUNTRY: `${NAMESPACE_PREFIX}/country`,
   LANGUAGE: `${NAMESPACE_PREFIX}/language`,
   UPLOAD: `${NAMESPACE_PREFIX}/upload`,
+  RESOURCES: `${NAMESPACE_PREFIX}/resources`,
+  SYSTEM: `${NAMESPACE_PREFIX}/system`,
+  TIME: `${NAMESPACE_PREFIX}/time`,
 };
