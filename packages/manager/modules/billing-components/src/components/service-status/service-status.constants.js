dd90dd52214890e8e3ae5698ba6d3fb693571f8b
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2025-09-16 10:22:08 +0200

    feat(billing): replace renew mode by status
    ref: #MANAGER-19540
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/billing-components/src/components/service-status/service-status.constants.js
+++ packages/manager/modules/billing-components/src/components/service-status/service-status.constants.js
@@ -1,10 +1,14 @@
-export const SERVICE_STATES = {
-  error: ['expired', 'delete_at_expiration'],
-  success: ['auto', 'automatic'],
-  warning: ['manual', 'manualPayment'],
-  info: ['billing_suspended', 'forced_manual'],
+export const SERVICE_STATES_BADGES_CLASSES_MAP = {
+  active: 'success',
+  error: 'error',
+  in_creation: 'info',
+  rupture: 'error',
+  terminated: 'error',
+  to_renew: 'warning',
+  unpaid: 'error',
+  unrenewed: 'error',
 };
 
 export default {
-  SERVICE_STATES,
+  SERVICE_STATES_BADGES_CLASSES_MAP,
 };
