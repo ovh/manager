4a49e8374c8806797538b5bc7668748c538ca922
lauren Olivier <35332974+lolottetheclash@users.noreply.github.com>
2025-09-17 11:04:44 +0200

    feat(pci-instances): add localization cards list
    ref: #TAPC-4555
    
    Signed-off-by: Lauren Olivier <lauren.olivier@corp.ovh.com>

---
+++ packages/manager/apps/pci-instances/src/pages/instances/create/components/localisationSelection/LocalizationSelection.component.tsx
@@ -0,0 +1,60 @@
+import {
+  PageLocation,
+  ButtonType,
+  useOvhTracking,
+} from '@ovh-ux/manager-react-shell-client';
+import { RadioGroup, Text } from '@ovhcloud/ods-react';
+import { useTranslation } from 'react-i18next';
+import z from 'zod';
+import { useFormContext, useWatch } from 'react-hook-form';
+import { LocalizationCard } from '@/components/localizationCard/LocalizationCard.component';
+import { TInstanceCreationForm } from '../../CreateInstance.page';
+import { mockedLocalizations } from '@/__mocks__/instance/constants';
+
+export const localizationSelectionSchema = z.string();
+export const localizationDefaultValue = 'eu-west-par';
+
+export const LocalizationSelection = () => {
+  const { t } = useTranslation(['creation']);
+  const { trackClick } = useOvhTracking();
+  const { control } = useFormContext<TInstanceCreationForm>();
+  const selectedRegion = useWatch({ control, name: 'region' });
+
+  const handleSelect = (region: string) => {
+    trackClick({
+      location: PageLocation.funnel,
+      buttonType: ButtonType.tile,
+      actionType: 'action',
+      actions: ['add_instance', 'select_localisation', region],
+    });
+  };
+
+  return (
+    <section>
+      <div className="flex flex-col gap-4">
+        <Text preset="heading-4">
+          {t('pci_instance_creation_choose_localization_title')}
+        </Text>
+        <Text preset="paragraph">
+          {t('pci_instance_creation_select_localization_informations')}
+        </Text>
+      </div>
+      <RadioGroup value={selectedRegion}>
+        <div className="grid grid-cols-[repeat(auto-fit,_minmax(225px,_1fr))] gap-6">
+          {mockedLocalizations.map(
+            ({ countryCode, title, region, deploymentMode }) => (
+              <LocalizationCard
+                key={region}
+                title={title}
+                region={region}
+                countryCode={countryCode}
+                deploymentMode={deploymentMode}
+                onSelect={handleSelect}
+              />
+            ),
+          )}
+        </div>
+      </RadioGroup>
+    </section>
+  );
+};
