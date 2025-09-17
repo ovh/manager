1c24f8327a497986d79844468b5c69e5f90fe8dd
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2025-09-17 16:44:21 +0200

    feat(pci-instances): add backup info
    ref: #TAPC-4488
    
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/apps/pci-instances/src/pages/instances/instance/dashboard/Dashboard.page.tsx
+++ packages/manager/apps/pci-instances/src/pages/instances/instance/dashboard/Dashboard.page.tsx
@@ -3,11 +3,15 @@ import { Outlet } from 'react-router-dom';
 import InstanceGeneralInfoBlock from './components/InstanceGeneralInfoBlock.component';
 import InstancePropertyBlock from './components/InstancePropertyBlock.component';
 import InstanceNetworkingBlock from './components/InstanceNetworkingBlock.component';
+import BackupBlock from './components/BackupBlock.component';
 
 const Dashboard: FC = () => (
   <div className="grid grid-cols-1 md:items-start md:grid-cols-3 gap-4 md:gap-6">
     <InstanceGeneralInfoBlock />
-    <InstancePropertyBlock />
+    <article className="flex flex-col gap-y-4">
+      <InstancePropertyBlock />
+      <BackupBlock />
+    </article>
     <InstanceNetworkingBlock />
     <Outlet />
   </div>
