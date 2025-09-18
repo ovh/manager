ff037b110ec067714dc4b75e81dd8dfcca2033e6
aTurmo <adrien.turmo.ext@ovhcloud.com>
2025-09-18 16:52:11 +0200

    feat(pci-load-balancer): update tracking load balancer (#19262)
    ref: #TAPC-3830
    
    Signed-off-by: Adrien Turmo <adrien.turmo.ext@ovhcloud.com>

--- packages/manager/apps/pci-load-balancer/src/pages/create/hooks/useCreateActions.tsx
+++ packages/manager/apps/pci-load-balancer/src/pages/create/hooks/useCreateActions.tsx
@@ -1,12 +1,17 @@
 import { useNavigate, useParams } from 'react-router-dom';
-import { LOAD_BALANCER_CREATION_TRACKING } from '@/constants';
-import { useTracking } from './useTracking';
+import { useMemo } from 'react';
+import {
+  ButtonType,
+  PageLocation,
+  useOvhTracking,
+} from '@ovh-ux/manager-react-shell-client';
 import { useCreateStore } from '@/pages/create/store';
 import { useGetFlavor } from '@/api/hook/useFlavors';
 import { useCreateCallbacks } from './useCreateCallbacks';
+import { FloatingIpSelectionId } from '@/types/floating.type';
 
 export const useCreateActions = () => {
-  const { trackClick } = useTracking();
+  const { trackClick } = useOvhTracking();
   const store = useCreateStore();
   const { projectId } = useParams();
   const { data: flavor } = useGetFlavor(
@@ -17,24 +22,33 @@ export const useCreateActions = () => {
   const { onSuccess, onError } = useCreateCallbacks();
   const navigate = useNavigate();
 
+  const storeTracikngInfo = useMemo(
+    () =>
+      `${store.region?.name}_private_${store.addon?.size}_${
+        store.publicIp === FloatingIpSelectionId.UNATTACHED
+          ? 'unavailable'
+          : 'available'
+      }_${store.listeners.length}`,
+    [store.region, store.addon, store.publicIp, store.listeners.length],
+  );
+
   return {
     create: () => {
       trackClick({
-        name: LOAD_BALANCER_CREATION_TRACKING.SUBMIT,
-        type: 'action',
-      });
-
-      trackClick({
-        name: `${LOAD_BALANCER_CREATION_TRACKING.CONFIRM}::${store.addon.size}::${store.region.name}`,
-        type: 'action',
+        actions: ['confirm', `loadbalancer_added_${storeTracikngInfo}`],
+        actionType: 'action',
+        buttonType: ButtonType.button,
+        location: PageLocation.funnel,
       });
 
       store.create(flavor, onSuccess, onError);
     },
     cancel: () => {
       trackClick({
-        name: LOAD_BALANCER_CREATION_TRACKING.CANCEL,
-        type: 'action',
+        actions: ['cancel', storeTracikngInfo],
+        actionType: 'action',
+        buttonType: ButtonType.button,
+        location: PageLocation.funnel,
       });
       store.reset();
       navigate('..');
