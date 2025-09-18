0c3cddd5b6597432634d3d5bdc836452aea4fcca
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2025-09-18 17:31:38 +0200

    feat(zimbra): zimbra pro ga
    ref: #PRDCOL-124
    
    Signed-off-by: Atef ZAAFOURI <atefzed@gmail.com>
    Co-authored-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/apps/zimbra/src/constants.ts
+++ packages/manager/apps/zimbra/src/constants.ts
@@ -2,10 +2,12 @@ export const FEATURE_AVAILABILITY = {
   PRO_BETA: 'zimbra:pro_beta',
 };
 
-export const MAX_PRO_ACCOUNTS = 5;
 export const OVH_WEBSITE_ROOT_URL = 'https://ovhcloud.com';
 
 export const ZimbraLabsLink = 'https://labs.ovhcloud.com/en/zimbra-beta/';
+
+export const MAX_STARTER_STORAGE_QUOTA = 16106127360;
+export const MAX_PRO_STORAGE_QUOTA = 53687091200;
 
 export const ZimbraEmailsLink: Record<string, string> = {
   DEFAULT: `${OVH_WEBSITE_ROOT_URL}/en-ie/emails/zimbra-emails/`,
@@ -21,3 +23,30 @@ export const ZimbraEmailsLink: Record<string, string> = {
   TN: `${OVH_WEBSITE_ROOT_URL}/fr-tn/emails/zimbra-emails/`,
   SN: `${OVH_WEBSITE_ROOT_URL}/fr-sn/emails/zimbra-emails/`,
 };
+
+export const starterFeatures = [
+  { label: 'zimbra_account_plan_feature_storage_starter' },
+  {
+    label: 'zimbra_account_plan_feature_share_starter',
+    tooltip: 'zimbra_account_plan_feature_share_starter_tooltip',
+  },
+];
+
+export const proFeatures = [
+  { label: 'zimbra_account_plan_feature_storage_pro' },
+  { label: 'zimbra_account_plan_feature_share_pro' },
+  {
+    label: 'zimbra_account_plan_feature_sync',
+    tooltip: 'zimbra_account_plan_feature_sync_tooltip',
+  },
+  { label: 'zimbra_account_plan_feature_files_storage' },
+  { label: 'zimbra_account_plan_feature_co_edit' },
+  { label: 'zimbra_account_plan_feature_private_chat' },
+];
+
+export const businessFeatures = [
+  ...proFeatures,
+  { label: 'zimbra_account_plan_feature_team_chat' },
+  { label: 'zimbra_account_plan_feature_shared_storage' },
+  { label: 'zimbra_account_plan_feature_video_conferences' },
+];
