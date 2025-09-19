1e59787ae8e3c31d41e801316fa8910884ab9059
yjaaouane <yassine.jaaouane.ext@corp.ovh.com>
2025-09-19 09:28:22 +0200

    feat(observability): init observability µapp
    ref: #MAOBS-1
    
    Signed-off-by: yjaaouane <yassine.jaaouane.ext@corp.ovh.com>

---
+++ packages/manager/apps/observability/src/App.constants.ts
@@ -0,0 +1,11 @@
+export const appName = 'observability';
+
+export const AppConfig = {  
+  rootLabel: appName,
+} as const;
+
+export type DashboardsApi = 'v2';
+
+export const APP_FEATURES = {
+  basePrefix: '',
+} as const;
