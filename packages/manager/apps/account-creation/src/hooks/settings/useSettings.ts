ae72a47f75bce00be12826b9f3c6c63a30c59a1a
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2025-09-19 12:16:47 +0200

    feat(account-creation): set up settings page
    ref: #MANAGER-17334
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>

---
+++ packages/manager/apps/account-creation/src/hooks/settings/useSettings.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod';
+
+export const useSettingsSchema = () =>
+  z.object({
+    country: z
+      .string({
+        required_error: 'required_field',
+        invalid_type_error: 'required_field',
+      })
+      .trim(),
+    currency: z
+      .string({
+        required_error: 'required_field',
+        invalid_type_error: 'required_field',
+      })
+      .trim()
+      .min(1, 'required_field'),
+    language: z
+      .string({
+        required_error: 'required_field',
+        invalid_type_error: 'required_field',
+      })
+      .trim()
+      .min(1, 'required_field'),
+  });
