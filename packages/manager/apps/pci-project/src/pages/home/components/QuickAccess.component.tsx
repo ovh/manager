29a7758bc795ff8f35d6ca0b7b1cfbf17121601f
Stephane Ravet <ravet.st@gmail.com>
2025-09-22 09:18:48 +0200

    fix(pci-project): fix QA bug analysis & refactor dashboard item
    Signed-off-by: Stephane Ravet <ravet.st@gmail.com>
    feat(pci-project): update QuotaAlert to not open link in another window
    
    ref: #MANAGER-18604
    
    Signed-off-by: Stephane Ravet <ravet.st@gmail.com>
    feat(pci-project): add KBs for links by subsidiary
    
    ref: #MANAGER-18604
    
    Signed-off-by: Stephane Ravet <ravet.st@gmail.com>
    refactor(pci-project): updated constants for better URL handling and backward compatibility
    
    Signed-off-by: Stephane Ravet <ravet.st@gmail.com>
    refactor(pci-project): merge 2 constants files in 1
    
    Signed-off-by: Stephane Ravet <ravet.st@gmail.com>
    refactor(pci-project): add better types DashboardTile
    
    Signed-off-by: Stephane Ravet <ravet.st@gmail.com>
    refactor(pci-project): fix QA bug analysis & refactor dashboard item
    
    ref: #MANAGER-18604
    
    Signed-off-by: Stephane Ravet <ravet.st@gmail.com>

--- packages/manager/apps/pci-project/src/pages/home/components/QuickAccess.component.tsx
+++ packages/manager/apps/pci-project/src/pages/home/components/QuickAccess.component.tsx
@@ -1,9 +1,35 @@
 import { useTranslation } from 'react-i18next';
 import { OdsText } from '@ovhcloud/ods-components/react';
 import { ODS_TEXT_PRESET } from '@ovhcloud/ods-components';
-import { QuickAccessCard, QuickAccessItem } from './QuickAccessCard.component';
 
-export function QuickAccess({ items }: { items: QuickAccessItem[] }) {
+import QuickAccessCard from './QuickAccessCard.component';
+import { DASHBOARD_QUICK_ACCESS_ITEMS_BASE, DashboardItem } from '@/constants';
+
+import IconInstances from '@/assets/home/instances.svg?url';
+import IconKubernetes from '@/assets/home/managed_kubernetes_service.svg?url';
+import IconObjectStorage from '@/assets/home/object_storage.svg?url';
+import IconNetwork from '@/assets/home/vrack_private_network.svg?url';
+import IconDatabase from '@/assets/home/managed_mongodb.svg?url';
+import IconBlockStorage from '@/assets/home/block_storage.svg?url';
+
+const iconImages = [
+  IconInstances,
+  IconKubernetes,
+  IconObjectStorage,
+  IconBlockStorage,
+  IconNetwork,
+  IconDatabase,
+];
+
+// Add iconImage to items from constants
+const quickAccessItems: DashboardItem[] = DASHBOARD_QUICK_ACCESS_ITEMS_BASE.map(
+  (item, index) => ({
+    ...item,
+    iconImage: iconImages[index],
+  }),
+);
+
+function QuickAccess() {
   const { t } = useTranslation('project');
 
   return (
@@ -12,10 +38,12 @@ export function QuickAccess({ items }: { items: QuickAccessItem[] }) {
         {t('pci_projects_project_quick_access')}
       </OdsText>
       <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 gap-5 m-6">
-        {items.map((item, idx) => (
+        {quickAccessItems.map((item, idx) => (
           <QuickAccessCard key={idx} item={item} index={idx} />
         ))}
       </div>
     </>
   );
 }
+
+export default QuickAccess;
