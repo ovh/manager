0e23b0171ab07d3e12e88a07e7f0ab7a60e2000b
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-09-23 11:35:15 +0200

    feat(ips): use dedicated server display name for import ip from sys
    ref: #MANAGER-19699
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager/apps/ips/src/data/hooks/ip/useGetAttachedServices.ts
+++ packages/manager/apps/ips/src/data/hooks/ip/useGetAttachedServices.ts
@@ -1,14 +1,17 @@
 import { useGetProductServices } from '../useGetProductServices';
 
 // List of all relevant product paths and categories to check except { path: '/dedicated/housing', category: 'HOUSING' },
-const PRODUCT_PATHS_AND_CATEGORIES = [
-  { path: '/cloud/project', category: 'CLOUD' },
-  { path: '/dedicated/server', category: 'DEDICATED' },
-  { path: '/vps', category: 'VPS' },
-  { path: '/vrack', category: 'VRACK' },
-  { path: '/dedicatedCloud', category: 'PRIVATE_CLOUD' },
-  { path: '/ipLoadbalancing', category: 'IP_LOAD_BALANCING' },
-];
+export const PRODUCT_PATHS_AND_CATEGORIES = {
+  CLOUD: { path: '/cloud/project', category: 'CLOUD' },
+  DEDICATED: { path: '/dedicated/server', category: 'DEDICATED' },
+  VPS: { path: '/vps', category: 'VPS' },
+  VRACK: { path: '/vrack', category: 'VRACK' },
+  PRIVATE_CLOUD: { path: '/dedicatedCloud', category: 'PRIVATE_CLOUD' },
+  IP_LOAD_BALANCING: {
+    path: '/ipLoadbalancing',
+    category: 'IP_LOAD_BALANCING',
+  },
+};
 
 export type UseGetAttachedServicesParams = {
   serviceName?: string;
@@ -19,7 +22,7 @@ export const useGetAttachedServices = ({
 }: UseGetAttachedServicesParams) => {
   // Fetch all product services for all paths/categories
   const { services: productServicesList } = useGetProductServices(
-    PRODUCT_PATHS_AND_CATEGORIES,
+    Object.values(PRODUCT_PATHS_AND_CATEGORIES),
   );
 
   const servicesAttached = productServicesList.filter((productServices) => {
