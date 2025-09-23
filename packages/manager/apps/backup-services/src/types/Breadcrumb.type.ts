661565d80533a4720f5c26b9e84f15aceb18277b
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-09-23 11:04:19 +0200

    feat(backup-services): init backup-services µapp
    ref: #BKP-524
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

---
+++ packages/manager/apps/backup-services/src/types/Breadcrumb.type.ts
@@ -0,0 +1,12 @@
+export type BreadcrumbItem = {
+  label: string | undefined;
+  href: string;
+};
+
+export interface BreadcrumbProps {
+  rootLabel?: string;
+  customRootLabel?: string;
+  appName?: string;
+  projectId?: string;
+  items?: BreadcrumbItem[];
+}
