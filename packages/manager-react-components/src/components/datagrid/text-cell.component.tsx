667bb3b5c533502cede0dccd7f45c565fcf07ec0
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-09-24 16:57:48 +0200

    chore(mrc): add test-id to text-cell component
    ref: #MANAGER-19243
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager-react-components/src/components/datagrid/text-cell.component.tsx
+++ packages/manager-react-components/src/components/datagrid/text-cell.component.tsx
@@ -3,14 +3,20 @@ import { OdsText } from '@ovhcloud/ods-components/react';
 import { ODS_TEXT_PRESET } from '@ovhcloud/ods-components';
 import { useTranslation } from 'react-i18next';
 
+type DataGridTextCellProps = {
+  className?: string;
+  'data-testid'?: string;
+};
+
 /** Simple datagrid cell text formatter applying ODS style */
 export function DataGridTextCell({
   className,
   children,
-}: React.PropsWithChildren<{ className?: string }>) {
+  'data-testid': dataTestId,
+}: React.PropsWithChildren<DataGridTextCellProps>) {
   const { t } = useTranslation('datagrid');
   return (
-    <OdsText preset={ODS_TEXT_PRESET.span}>
+    <OdsText preset={ODS_TEXT_PRESET.span} data-testid={dataTestId}>
       {(children as string) ?? t('common_empty_text_cell' as string)}
     </OdsText>
   );
