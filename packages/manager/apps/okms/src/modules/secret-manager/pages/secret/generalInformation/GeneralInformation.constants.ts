fbf6748451e896c7f545ab8d125d99e96ab923ef
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-09-24 16:32:46 +0200

    chore(okms): add test-ids for e2e testing
    ref: #MANAGER-19243
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

---
+++ packages/manager/apps/okms/src/modules/secret-manager/pages/secret/generalInformation/GeneralInformation.constants.ts
@@ -0,0 +1,9 @@
+export const SECRET_TEST_IDS = {
+  CAS_REQUIRED: 'secret-cas-required',
+  DEACTIVATE_VERSION_AFTER: 'secret-deactivate-version-after',
+  MAX_VERSIONS: 'secret-max-versions',
+  PATH: 'secret-path',
+  URN: 'secret-urn',
+  CREATED_AT: 'secret-created-at',
+  UPDATED_AT: 'secret-updated-at',
+};
