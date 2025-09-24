6657709b3981ec707cbaa391eab614c5b31d8dcb
Manon Carbonnel <manon.carbonnel@gmail.com>
2025-09-24 17:05:38 +0200

    feat(pci-instances): add deployment mode help drawer
    ref: #TAPC-4648
    
    Signed-off-by: Manon Carbonnel <manon.carbonnel@gmail.com>

--- packages/manager/apps/pci-instances/src/pages/instances/create/components/Localization.component.tsx
+++ packages/manager/apps/pci-instances/src/pages/instances/create/components/Localization.component.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next';
-import { Button, Divider, Text } from '@ovhcloud/ods-react';
+import { Divider, Text } from '@ovhcloud/ods-react';
 
 export const Localization = () => {
   const { t } = useTranslation(['common', 'creation']);
@@ -7,21 +7,16 @@ export const Localization = () => {
   return (
     <section>
       <Divider />
-      <div className={'mt-8 flex divide-x-4 divide-blue-700'}>
+      <div className={'mt-8 flex flex-col gap-4'}>
         <Text preset="heading-2">
           {t('creation:pci_instance_creation_select_localization_label')}
         </Text>
-        <div>
-          <Button variant={'ghost'} size={'sm'}>
-            {t('common:pci_instances_common_help')}
-          </Button>
-        </div>
+        <Text preset="paragraph">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. enim ad. (Not
+          mandatory) Si besoin d’un texte explicatif/consigne de section...
+        </Text>
       </div>
-      <Text preset="paragraph">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. enim ad. (Not
-        mandatory) Si besoin d’un texte explicatif/consigne de section...
-      </Text>
     </section>
   );
 };
