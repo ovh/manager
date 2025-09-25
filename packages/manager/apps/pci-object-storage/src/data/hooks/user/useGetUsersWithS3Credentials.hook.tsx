87ecb889c6d28fdcabfff549340f548747a88c95
Arthur Bullet <arthur.bullet@ovhcloud.com>
2025-09-25 10:32:58 +0200

    feat(os): api hooks and type
    ref: #DATATR-1234
    
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>

---
+++ packages/manager/apps/pci-object-storage/src/data/hooks/user/useGetUsersWithS3Credentials.hook.tsx
@@ -0,0 +1,72 @@
+import { QueryObserverOptions, useQueries } from '@tanstack/react-query';
+import { useCallback, useEffect, useRef } from 'react';
+import * as role from '@datatr-ux/ovhcloud-types/cloud/role/index';
+import user from '@/types/User';
+import { getUserS3Credentials } from '@/data/api/user/user.api';
+
+export interface UserWithS3Credentials extends user.User {
+  access_key: string;
+}
+
+export const useGetUsersWithS3Credentials = (
+  projectId: string,
+  users: user.User[],
+  options: Omit<QueryObserverOptions, 'queryKey'> = {},
+) => {
+  const s3CredentialsQueries = useQueries({
+    queries: users
+      ? users
+          .filter(
+            (us: user.User) =>
+              us.status === user.UserStatusEnum.creating ||
+              us.roles.find(
+                (s3Role: role.Role) =>
+                  s3Role.name === user.RoleEnum.objectstore_operator,
+              ),
+          )
+          .map((us) => {
+            return {
+              queryKey: [projectId, 'user', us.id, 's3Credentials'],
+              queryFn: () =>
+                getUserS3Credentials({
+                  projectId,
+                  userId: us.id,
+                }),
+              ...options,
+              select: (s3CredArray: user.S3Credentials[]) => {
+                const access = s3CredArray[0]?.access;
+                return access && { ...us, access_key: access };
+              },
+            };
+          })
+      : [],
+  });
+
+  const refetchAll = useCallback(() => {
+    s3CredentialsQueries.forEach((us) => us.refetch());
+  }, [s3CredentialsQueries]);
+
+  const userWithS3: UserWithS3Credentials[] = s3CredentialsQueries.flatMap(
+    (userS3) => {
+      return userS3.data || [];
+    },
+  );
+
+  // refetch if pooling changes
+  const prevRefetchInterval = useRef(options?.refetchInterval);
+  useEffect(() => {
+    if (
+      options?.enabled !== false &&
+      options.refetchInterval !== undefined &&
+      options.refetchInterval !== prevRefetchInterval.current
+    ) {
+      refetchAll();
+    }
+    prevRefetchInterval.current = options.refetchInterval;
+  }, [options.refetchInterval, options.enabled, refetchAll]);
+
+  return {
+    data: userWithS3,
+    refetchAll,
+  };
+};
