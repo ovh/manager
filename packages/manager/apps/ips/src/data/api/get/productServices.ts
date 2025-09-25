539060b02f74531880c4e73f860f9666b5f66c45
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-09-25 09:43:18 +0200

    feat(ips): service search box
    ref: #MANAGER-19849
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager/apps/ips/src/data/api/get/productServices.ts
+++ packages/manager/apps/ips/src/data/api/get/productServices.ts
@@ -1,19 +1,36 @@
 import { IcebergFetchResultV6, fetchIcebergV6 } from '@ovh-ux/manager-core-api';
+import { IpTypeEnum } from './ipDetails';
 
 export type GetProductServicesParams = {
+  category: IpTypeEnum;
   path?: string;
-  category?: string;
+  pathList?: string[];
 };
 
 export type ProductServicesDetails = Record<string, unknown>;
 
 export const getProductServicesQueryKey = (
   params: GetProductServicesParams,
-) => [`get/${params.path}/${params.category}`];
+) => [`get/${params.path || params.pathList.join(',')}/${params.category}`];
 
 export const getProductServices = async (
   params: GetProductServicesParams,
-): Promise<IcebergFetchResultV6<ProductServicesDetails>> =>
-  fetchIcebergV6<ProductServicesDetails>({
-    route: params.path,
-  });
+): Promise<IcebergFetchResultV6<ProductServicesDetails>> => {
+  if (params.path) {
+    return fetchIcebergV6<ProductServicesDetails>({
+      route: params.path,
+    });
+  }
+
+  const results = await Promise.all(
+    params.pathList.map((path) =>
+      fetchIcebergV6<ProductServicesDetails>({ route: path }),
+    ),
+  );
+
+  return {
+    data: results.flatMap((result) => result.data),
+    totalCount: results.reduce((acc, result) => acc + result.totalCount, 0),
+    status: results[0].status,
+  };
+};
