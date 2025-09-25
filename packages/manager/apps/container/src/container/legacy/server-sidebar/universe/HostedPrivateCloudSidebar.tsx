7e8a699ee5f243c9f437d1fc107814a24ccffa2d
David Arsène <david.arsene.ext@ovhcloud.com>
2025-09-25 18:09:06 +0200

    fix(container): fix node selection in pnr menu for custom path
    fixes: #MAOBS-23
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/container/src/container/legacy/server-sidebar/universe/HostedPrivateCloudSidebar.tsx
+++ packages/manager/apps/container/src/container/legacy/server-sidebar/universe/HostedPrivateCloudSidebar.tsx
@@ -70,7 +70,7 @@ export default function HostedPrivateCloudSidebar() {
         label: t('sidebar_vmware_vsphere'),
         icon: getIcon('ovh-font ovh-font-dedicatedCloud'),
         routeMatcher: new RegExp(`^(/configuration)?/dedicated_cloud`),
-        pathMatcher: new RegExp('^(/vmware/vsphere/)'),
+        pathMatcher: new RegExp('^/vmware/vsphere/'),
         async loader() {
           const mbm = await loadServices('/dedicatedCloud', 'EPCC');
           return [
@@ -87,7 +87,7 @@ export default function HostedPrivateCloudSidebar() {
               routeMatcher: new RegExp(
                 `^(/configuration)?/dedicated_cloud/${service.serviceName}`,
               ),
-              pathMatcher: new RegExp(`^(/vmware/vsphere/${service.serviceName})`),
+              pathMatcher: new RegExp(`^/vmware/vsphere/${service.serviceName}`),
               async loader() {
                 const datacenters = await loadServices(
                   `/dedicatedCloud/${service.serviceName}/datacenter`,
