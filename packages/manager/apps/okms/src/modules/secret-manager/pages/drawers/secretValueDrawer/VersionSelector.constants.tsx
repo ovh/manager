8a73d75a0422104879a9db3e6419b07c168a1038
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-09-25 10:08:32 +0200

    feat(okms): add current version information
    ref: #MANAGER-19657
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

--- packages/manager/apps/okms/src/modules/secret-manager/pages/drawers/secretValueDrawer/VersionSelector.constants.tsx
+++ packages/manager/apps/okms/src/modules/secret-manager/pages/drawers/secretValueDrawer/VersionSelector.constants.tsx
@@ -1,3 +1,6 @@
 export const VERSION_SELECTOR_TEST_ID = 'version-selector';
-export const VERSION_SELECTOR_SPINNER_TEST_ID = 'version-selector-spinner';
+export const VERSION_SELECTOR_SELECT_SKELETON_TEST_ID =
+  'version-selector-select-skeleton';
+export const VERSION_SELECTOR_STATUS_SKELETON_TEST_ID =
+  'version-selector-status-skeleton';
 export const VERSION_SELECTOR_ERROR_TEST_ID = 'version-selector-error';
