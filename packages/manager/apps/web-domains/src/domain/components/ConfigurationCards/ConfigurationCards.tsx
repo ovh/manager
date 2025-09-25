7b2d677cbf57e56e6827733237a6927fd5d53e49
Brendan Seiller <brendan.seiller@ovhcloud.com>
2025-09-25 15:23:39 +0200

    feat(web-domains): implement dnssec toggle and status
    ref: #MANAGER-19116
    
    Signed-off-by: Brendan Seiller <brendan.seiller@ovhcloud.com>

--- packages/manager/apps/web-domains/src/domain/components/ConfigurationCards/ConfigurationCards.tsx
+++ packages/manager/apps/web-domains/src/domain/components/ConfigurationCards/ConfigurationCards.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { ManagerTile } from '@ovh-ux/manager-react-components';
 import {
   Badge,
   BADGE_COLOR,
-  FormField,
   Icon,
   ICON_NAME,
   Toggle,
@@ -12,8 +13,14 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from '@ovhcloud/ods-react';
-import React from 'react';
-import { useTranslation } from 'react-i18next';
+import {
+  useGetDnssecStatus,
+  useGetDomainResource,
+  useUpdateDnssecService,
+} from '@/domain/hooks/data/query';
+import { DnssecStatusEnum } from '@/domain/enum/dnssecStatus.enum';
+import DnssecModal from './DnssecModal';
+import DnssecToggleStatus from './DnssecToggleStatus';
 
 interface ConfigurationCardsProps {
   readonly serviceName: string;
@@ -23,10 +30,35 @@ export default function ConfigurationCards({
   serviceName,
 }: ConfigurationCardsProps) {
   const { t } = useTranslation(['domain']);
+  const { domainResource } = useGetDomainResource(serviceName);
+  const [dnssecModalOpened, setDnssecModalOpened] = React.useState<boolean>(
+    false,
+  );
+  const { dnssecStatus, isDnssecStatusLoading } = useGetDnssecStatus(
+    serviceName,
+  );
+
+  const { updateServiceDnssec } = useUpdateDnssecService(
+    serviceName,
+    dnssecStatus?.status === DnssecStatusEnum.ENABLED
+      ? DnssecStatusEnum.DISABLED
+      : DnssecStatusEnum.ENABLED,
+  );
+
+  const updateDnssec = () => {
+    updateServiceDnssec();
+    setDnssecModalOpened(!dnssecModalOpened);
+  };
+
+  if (!domainResource && !dnssecStatus) {
+    return <></>;
+  }
 
   return (
     <ManagerTile>
-      <ManagerTile.Title>Configuration</ManagerTile.Title>
+      <ManagerTile.Title>
+        {t('domain_tab_general_information_configuration')}
+      </ManagerTile.Title>
       <ManagerTile.Divider />
       <ManagerTile.Item>
         <ManagerTile.Item.Label>Serveur DNS</ManagerTile.Item.Label>
@@ -37,29 +69,13 @@ export default function ConfigurationCards({
         </ManagerTile.Item.Description>
       </ManagerTile.Item>
       <ManagerTile.Divider />
-      <ManagerTile.Item>
-        <ManagerTile.Item.Label>
-          Délégation sécurisée - DNSSEC
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Icon className="pl-3" name={ICON_NAME.circleQuestion} />
-            </TooltipTrigger>
-            <TooltipContent>
-              {t('domain_tab_general_information_tooltip_domain_state')}
-            </TooltipContent>
-          </Tooltip>
-        </ManagerTile.Item.Label>
-        <ManagerTile.Item.Description>
-          <Toggle withLabels={true} className="items-end">
-            <ToggleControl />
-            <ToggleLabel>
-              <Badge color={BADGE_COLOR.success} className="mt-4">
-                Enregistré
-              </Badge>
-            </ToggleLabel>
-          </Toggle>
-        </ManagerTile.Item.Description>
-      </ManagerTile.Item>
+      <DnssecToggleStatus
+        dnssecModalOpened={dnssecModalOpened}
+        dnssecStatus={dnssecStatus}
+        domainResource={domainResource}
+        isDnssecStatusLoading={isDnssecStatusLoading}
+        setDnssecModalOpened={setDnssecModalOpened}
+      />
       <ManagerTile.Divider />
       <ManagerTile.Item>
         <ManagerTile.Item.Label>
@@ -84,6 +100,12 @@ export default function ConfigurationCards({
           </Toggle>
         </ManagerTile.Item.Description>
       </ManagerTile.Item>
+      <DnssecModal
+        action={dnssecStatus?.status}
+        open={dnssecModalOpened}
+        updateDnssec={updateDnssec}
+        onClose={() => setDnssecModalOpened(!setDnssecModalOpened)}
+      />
     </ManagerTile>
   );
 }
