553e69cafaabff4edd5755ca0ffe6ac823a3efdb
Simon Chaumet <simon.chaumet.ext@ovhcloud.com>
2025-09-25 12:07:53 +0000

    feat(pci-volume-backup): disallow restauration on deleted or deleting volumes
    ref: #TAPC-3604
    
    Signed-off-by: Simon Chaumet <simon.chaumet.ext@ovhcloud.com>
    Co-authored-by: Adrien Turmo <adrien.turmo.ext@ovhcloud.com>

--- packages/manager/apps/pci-volume-backup/src/pages/listing/Actions.component.tsx
+++ packages/manager/apps/pci-volume-backup/src/pages/listing/Actions.component.tsx
@@ -1,11 +1,13 @@
 import { useTranslation } from 'react-i18next';
-import { useHref } from 'react-router-dom';
+import { useHref, useParams } from 'react-router-dom';
 import { ActionMenu } from '@ovh-ux/manager-react-components';
 import { useOvhTracking } from '@ovh-ux/manager-react-shell-client';
 import { ODS_BUTTON_VARIANT } from '@ovhcloud/ods-components';
 import { TVolumeBackup } from '@/data/api/api.types';
 import { VOLUME_BACKUP_STATUS } from '@/constants';
 import { VOLUME_BACKUP_TRACKING } from '@/tracking.constant';
+import { useVolume } from '@/data/hooks/useVolume';
+import { isVolumeDeleted } from '@/data/select/volume';
 
 type ActionsProps = {
   backup: TVolumeBackup;
@@ -15,6 +17,8 @@ export default function Actions({ backup }: Readonly<ActionsProps>) {
   const { t } = useTranslation('listing');
   const { trackClick } = useOvhTracking();
   const { id, volumeId, status } = backup;
+  const { projectId } = useParams();
+  const { data: volume, isLoading } = useVolume(projectId, volumeId);
 
   const restoreVolumeHref = useHref(`./restore-volume?volumeId=${volumeId}`);
   const createVolumeHref = useHref(`./${id}/create-volume`);
@@ -24,50 +28,57 @@ export default function Actions({ backup }: Readonly<ActionsProps>) {
     return null;
   }
 
-  const menuItems = [
-    ...(status === VOLUME_BACKUP_STATUS.OK
-      ? [
-          {
-            id: 1,
-            href: restoreVolumeHref,
-            label: t(
-              'pci_projects_project_storages_volume_backup_list_datagrid_menu_action_restore',
-            ),
-            onClick: () => {
-              trackClick({
-                actionType: 'action',
-                actions: VOLUME_BACKUP_TRACKING.LISTING.ROW_CTA_RESTORE_VOLUME,
-              });
-            },
-          },
-          {
-            id: 2,
-            href: createVolumeHref,
-            label: t(
-              'pci_projects_project_storages_volume_backup_list_datagrid_menu_action_create_volume',
-            ),
-            onClick: () => {
-              trackClick({
-                actionType: 'action',
-                actions: VOLUME_BACKUP_TRACKING.LISTING.ROW_CTA_CREATE_VOLUME,
-              });
-            },
-          },
-        ]
-      : []),
-    {
-      id: 3,
-      href: deleteVolumeBackupHref,
-      label: t(
-        'pci_projects_project_storages_volume_backup_list_datagrid_menu_action_delete',
-      ),
-      onClick: () => {
-        trackClick({
-          actionType: 'action',
-          actions: VOLUME_BACKUP_TRACKING.LISTING.ROW_CTA_DELETE_VOLUME,
-        });
-      },
+  const restoreAction = {
+    id: 1,
+    href: restoreVolumeHref,
+    label: t(
+      'pci_projects_project_storages_volume_backup_list_datagrid_menu_action_restore',
+    ),
+    onClick: () => {
+      trackClick({
+        actionType: 'action',
+        actions: VOLUME_BACKUP_TRACKING.LISTING.ROW_CTA_RESTORE_VOLUME,
+      });
     },
+  };
+
+  const createAction = {
+    id: 2,
+    href: createVolumeHref,
+    label: t(
+      'pci_projects_project_storages_volume_backup_list_datagrid_menu_action_create_volume',
+    ),
+    onClick: () => {
+      trackClick({
+        actionType: 'action',
+        actions: VOLUME_BACKUP_TRACKING.LISTING.ROW_CTA_CREATE_VOLUME,
+      });
+    },
+  };
+
+  const deleteAction = {
+    id: 3,
+    href: deleteVolumeBackupHref,
+    label: t(
+      'pci_projects_project_storages_volume_backup_list_datagrid_menu_action_delete',
+    ),
+    onClick: () => {
+      trackClick({
+        actionType: 'action',
+        actions: VOLUME_BACKUP_TRACKING.LISTING.ROW_CTA_DELETE_VOLUME,
+      });
+    },
+  };
+
+  const menuItems = [
+    ...(status === VOLUME_BACKUP_STATUS.OK &&
+    !isLoading &&
+    !!volume &&
+    !isVolumeDeleted(volume)
+      ? [restoreAction]
+      : []),
+    ...(status === VOLUME_BACKUP_STATUS.OK ? [createAction] : []),
+    deleteAction,
   ];
 
   return (
