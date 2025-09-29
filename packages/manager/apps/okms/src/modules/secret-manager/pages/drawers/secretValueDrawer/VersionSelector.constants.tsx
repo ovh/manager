12657c6e40d1b5c2feb873710be643544aee7f02
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-09-29 09:53:00 +0200

    refactor(okms): colocate test-ids with components
    ref: #MANAGER-19243
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/okms/src/modules/secret-manager/pages/drawers/secretValueDrawer/VersionSelector.constants.tsx
+++ packages/manager/apps/okms/src/modules/secret-manager/pages/drawers/secretValueDrawer/VersionSelector.constants.tsx
@@ -1,10 +1,3 @@
-/* value drawer */
-export const VALUE_DRAWER_TEST_ID = 'value-drawer';
-
-/* version selector */
 export const VERSION_SELECTOR_TEST_ID = 'version-selector';
 export const VERSION_SELECTOR_SPINNER_TEST_ID = 'version-selector-spinner';
 export const VERSION_SELECTOR_ERROR_TEST_ID = 'version-selector-error';
-
-/* raw value */
-export const RAW_VALUE_TEST_ID = 'raw-value';
