aea54a552c447d0fb8d2684ba82f6bed0203180c
Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
2025-09-29 12:27:43 +0200

    feat(container): enforce agent chat on snow livechat sessions
    ref: #MANAGER-19920
    
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>

--- packages/manager/apps/container/src/container/livechat/LiveChat.component.tsx
+++ packages/manager/apps/container/src/container/livechat/LiveChat.component.tsx
@@ -83,7 +83,7 @@ export default function LiveChat({
 
   const [snowContext, setSnowContext] = useState<SnowChatContext>({
     skip_load_history: 'false',
-    live_agent_only: 'false',
+    live_agent_only: 'true',
     requester_session_language: language,
     language,
     region,
@@ -188,7 +188,7 @@ export default function LiveChat({
   useEffect(() => {
     console.log(window.location.hostname);
     if (window.location.hostname.includes('labeu')) {
-      setSnowContext((prev) => ({ ...prev, live_agent_only: undefined }));
+      setSnowContext((prev) => ({ ...prev, live_agent_only: 'true' }));
     }
   }, []);
 
