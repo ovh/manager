3a5c4aac977431f1d9ae54e0072de3283f7b50be
David Arsène <david.arsene.ext@ovhcloud.com>
2025-09-30 16:05:53 +0200

    fix(vmware-vsphere): add beta tag on logs tabs
    fixes: #MAOBS-113
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/hpc-vmware-vsphere/src/types/DashboardTabItem.ts
+++ packages/manager/apps/hpc-vmware-vsphere/src/types/DashboardTabItem.ts
@@ -1,6 +1,10 @@
+import { ReactElement } from 'react';
+import { OdsBadge } from '@ovhcloud/ods-components/react';
+
 export interface DashboardTabItem {
   name: string;
   title: string;
+  badge?: ReactElement<typeof OdsBadge>;
   to: string;
   isRedirectLegacy: boolean;
 }
