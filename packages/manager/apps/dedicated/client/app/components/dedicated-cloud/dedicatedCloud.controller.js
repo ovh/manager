bb126247e0d829fddae072f3f954fb21fd593e5e
David Arsène <david.arsene.ext@ovhcloud.com>
2025-09-30 16:05:53 +0200

    fix(vmware-vsphere): add beta tag on logs tabs
    fixes: #MAOBS-113
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/dedicated/client/app/components/dedicated-cloud/dedicatedCloud.controller.js
+++ packages/manager/apps/dedicated/client/app/components/dedicated-cloud/dedicatedCloud.controller.js
@@ -12,6 +12,7 @@ export default class {
     this.constants = constants;
     this.coreURLBuilder = coreURLBuilder;
     this.ovhFeatureFlipping = ovhFeatureFlipping;
+    this.BETA = 'Beta';
   }
 
   $onInit() {
