7c330d08a11c13e48c7fc8d26d39e1c33ab4b8b5
Lionel Bueno <lionel.bueno.ext@ovhcloud.com>
2025-10-01 12:37:47 +0200

    feat(obj-sto): add repl rule
    Signed-off-by: Lionel Bueno <lionel.bueno.ext@ovhcloud.com>

--- packages/manager/apps/pci-object-storage/src/pages/object-storage/storage/s3Id/repilcation/Replication.page.tsx
+++ packages/manager/apps/pci-object-storage/src/pages/object-storage/storage/s3Id/repilcation/Replication.page.tsx
@@ -1,9 +1,32 @@
+import { useMemo } from 'react';
 import { Outlet } from 'react-router-dom';
+import { useS3Data } from '../S3.context';
+import ReplicationList from './_components/ReplicationList.component';
 
 const Replication = () => {
+  const { s3, s3Query } = useS3Data();
+
+  const { replication } = s3;
+
+  const replicationRules = useMemo(() => {
+    if (!replication?.rules) return [];
+    return replication?.rules
+      ?.map((repl, index) => ({
+        ...repl,
+        index: `${index}`,
+      }))
+      .sort((a, b) => {
+        const cmpPriority = b.priority - a.priority;
+        if (cmpPriority !== 0) return cmpPriority;
+        return b.destination.name.localeCompare(a.destination.name);
+      });
+  }, [replication?.rules]);
+
+  if (s3Query.isLoading) return <ReplicationList.Skeleton />;
+
   return (
     <>
-      <h2>Replication</h2>
+      <ReplicationList replicationRules={replicationRules} />
       <Outlet />
     </>
   );
