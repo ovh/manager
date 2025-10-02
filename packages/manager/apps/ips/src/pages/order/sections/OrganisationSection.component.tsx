ef68ff4f20d442af48ce8682b2fea8b05716663c
aderghamov <alexandre.dergham.ext@ovhcloud.com>
2025-10-02 15:32:55 +0200

    fix(ips): fixing the organisation region detection (#19484)
    ref: #MANAGER-18716
    
    Signed-off-by: aderghamov <alexandre.dergham.ext@ovhcloud.com>

--- packages/manager/apps/ips/src/pages/order/sections/OrganisationSection.component.tsx
+++ packages/manager/apps/ips/src/pages/order/sections/OrganisationSection.component.tsx
@@ -17,6 +17,8 @@ import { useGetOrganisationsList } from '@/data/hooks/organisation';
 import { OrderContext } from '../order.context';
 import { isAvailableOrganisation } from '../order.utils';
 import { isRegionInEu } from '@/components/RegionSelector/region-selector.utils';
+import { useCheckServiceAvailability } from '@/data/hooks/useCheckServiceAvailability';
+import { useServiceRegion } from '@/data/hooks/useServiceRegion';
 
 const NO_ORGANISATION = 'no_organisation';
 
@@ -26,20 +28,35 @@ export const OrganisationSection: React.FC = () => {
     setSelectedOrganisation,
     selectedPlanCode,
     selectedRegion,
+    selectedService,
+    selectedServiceType,
+    addDisabledService
   } = React.useContext(OrderContext);
   const { shell } = React.useContext(ShellContext);
   const { t } = useTranslation('order');
   const { organisations, isLoading } = useGetOrganisationsList();
+  const { serviceStatus } = useCheckServiceAvailability({
+    serviceName: selectedService,
+    serviceType: selectedServiceType,
+    onServiceUnavailable: addDisabledService,
+  });
+  const { region: selectedServiceRegion } = useServiceRegion({
+    serviceName: selectedService,
+    serviceType: selectedServiceType,
+    serviceStatus,
+  });
 
+  
   const organisationInfoLabel = useMemo(() => {
+    const organisationRegion = selectedRegion ?? selectedServiceRegion;
     if (selectedOrganisation) {
       return t('organisation_selection_info');
     }
-    if (isRegionInEu(selectedRegion)) {
+    if (isRegionInEu(organisationRegion)) {
       return t('organisation_selection_description_no_organisation_RIPE');
     }
     return t('organisation_selection_description_no_organisation_ARIN');
-  }, [selectedOrganisation, selectedRegion]);
+  }, [selectedOrganisation, selectedRegion, selectedServiceRegion]);
 
   const handleOrganisationChange = (
     event: OdsSelectCustomEvent<OdsSelectChangeEventDetail>,
