adf90ff7abc26fc9a883420dc9125c7e2ad8005d
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-10-02 10:18:58 +0200

    feat(backup-services): add listing and use mrc breadcrumb
    ref: #BKP-496
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

---
+++ packages/manager/apps/backup-services/src/pages/listing/_components/index.ts
@@ -0,0 +1,15 @@
+import { TenantActionCell } from './TenantActionsCell.component';
+import { TenantLocationCell } from './TenantLocationCell.component';
+import { TenantNameCell } from './TenantNameCell.component';
+import { TenantReferenceCell } from './TenantReferenceCell.component';
+import { TenantRegionCell } from './TenantRegionCell.component';
+import { TenantStatusCell } from './TenantStatusCell.component';
+
+export {
+  TenantNameCell,
+  TenantLocationCell,
+  TenantRegionCell,
+  TenantReferenceCell,
+  TenantStatusCell,
+  TenantActionCell,
+};
