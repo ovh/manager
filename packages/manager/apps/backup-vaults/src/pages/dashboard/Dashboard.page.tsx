702f2c8f80355ab31abd4cb39225b8462cd6a0f6
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-10-02 09:40:16 +0200

    feat(backup-vault): replace local breadcrumb by mrc breadcrumb
    ref: #BKP-525
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/apps/backup-vaults/src/pages/dashboard/Dashboard.page.tsx
+++ packages/manager/apps/backup-vaults/src/pages/dashboard/Dashboard.page.tsx
@@ -6,10 +6,10 @@ import { useTranslation } from 'react-i18next';
 
 import { OdsTab, OdsTabs } from '@ovhcloud/ods-components/react';
 
-import { BaseLayout } from '@ovh-ux/manager-react-components';
+import { BaseLayout, Breadcrumb } from '@ovh-ux/manager-react-components';
 import { useOvhTracking } from '@ovh-ux/manager-react-shell-client';
 
-import Breadcrumb from '@/components/breadcrumb/Breadcrumb.component';
+import { appName } from '@/App.constants';
 import { urls } from '@/routes/Routes.constants';
 
 import { useDashboardTabs } from './_hooks/useDashboardTabs';
@@ -40,7 +40,7 @@ export default function DashboardPage() {
       header={{ title: t('dashboard:title') }}
       backLinkLabel={t('dashboard:back')}
       onClickReturn={onNavigateBackClicked}
-      breadcrumb={<Breadcrumb />}
+      breadcrumb={<Breadcrumb appName={appName} rootLabel={appName} />}
       tabs={
         <OdsTabs>
           {tabs.map((tab) => (
