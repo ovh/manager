2e40afa484650630d0f3c8683ae758223230a2a8
soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>
2025-10-03 10:03:23 +0200

    fix(dedicated-server): fix billing info
    ref: #MANAGER-18419
    
    Signed-off-by: soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>

--- packages/manager/apps/dedicated-servers/src/components/cells/billingCell.tsx
+++ packages/manager/apps/dedicated-servers/src/components/cells/billingCell.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { useGetBillingInfo } from '@/hooks/useGetBillingInfo';
-import { SkeletonCell } from './skeletonCell';
+import { SkeletonCell } from '../skeletonCell';
 import { BillingInfo } from '@/data/types/billing.type';
+import { DedicatedServer } from '@/data/types/server.type';
+import { useGetNutanixServer } from '@/hooks/useGetNutanixServer';
 
 export type BillingProps = {
-  server: string;
+  server: DedicatedServer;
   children: (billing: BillingInfo) => React.ReactElement;
 };
 
 export const DSBilling = ({ server, children }: BillingProps) => {
   const { billingInfo, isLoading, error } = useGetBillingInfo({ server });
   return (
-    billingInfo && (
-      <SkeletonCell isLoading={isLoading} error={error}>
-        {billingInfo ? children(billingInfo) : <>-</>}
-      </SkeletonCell>
-    )
+    <SkeletonCell isLoading={isLoading} error={error}>
+      {billingInfo ? children(billingInfo) : <>-</>}
+    </SkeletonCell>
   );
 };
--- packages/manager/apps/dedicated-servers/src/components/cells/engagementCell.tsx
+++ packages/manager/apps/dedicated-servers/src/components/cells/engagementCell.tsx
@@ -3,13 +3,13 @@ import { OdsBadge } from '@ovhcloud/ods-components/react';
 import { ODS_BADGE_COLOR } from '@ovhcloud/ods-components';
 import { useTranslation } from 'react-i18next';
 import { DedicatedServer } from '@/data/types/server.type';
-import { DSBilling } from '../billingCell';
+import { DSBilling } from './billingCell';
 import { getEngagementWording } from '../commonCellsWording';
 
 export const EngagementCell = (server: DedicatedServer) => {
   const { t } = useTranslation('dedicated-servers');
   return (
-    <DSBilling server={server.name}>
+    <DSBilling server={server}>
       {(billingInfo) => (
         <OdsBadge
           label={t(getEngagementWording(billingInfo))}
