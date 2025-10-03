116d866a2e1bb46e4f7bd7a088c29364dc188118
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2025-10-03 15:30:11 +0200

    refactor(pci-instances): replace location card by PciCard
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

--- packages/manager/apps/pci-instances/src/components/localizationCard/LocalizationCard.component.tsx
+++ packages/manager/apps/pci-instances/src/components/localizationCard/LocalizationCard.component.tsx
@@ -1,17 +1,17 @@
-import { Card, Radio, RadioControl, Text } from '@ovhcloud/ods-react';
-import clsx from 'clsx';
-import { Controller, useFormContext } from 'react-hook-form';
+import { Radio, RadioControl, RadioLabel, Text } from '@ovhcloud/ods-react';
 import { Flag } from '../flag/Flag';
 import { TCountryIsoCode } from '../flag/country-iso-code';
 import { DeploymentModeBadge } from '../deploymentModeBadge/DeploymentModeBadge.component';
 import { TDeploymentMode } from '@/types/instance/common.type';
+import { PciCard } from '@/components/pciCard/PciCard.component';
 
-export type TLocalizationCardProps = {
+type TLocalizationCardProps = {
   title: string;
   region: string;
   countryCode: TCountryIsoCode;
   deploymentMode: TDeploymentMode;
-  onSelect?: (region: string) => void;
+  isSelected: boolean;
+  onSelect: (region: string) => void;
 };
 
 export const LocalizationCard = ({
@@ -19,47 +19,26 @@ export const LocalizationCard = ({
   region,
   countryCode,
   deploymentMode,
+  isSelected,
   onSelect,
-}: TLocalizationCardProps) => {
-  const { control } = useFormContext<{ region: string }>();
-
-  return (
-    <Controller
-      name="region"
-      control={control}
-      render={({ field }) => {
-        const isSelected = field.value === region;
-        const handleSelectChange = () => {
-          field.onChange(region);
-          onSelect?.(region);
-        };
-
-        return (
-          <Radio value={region} className="w-full">
-            <Card
-              color={isSelected ? 'primary' : 'neutral'}
-              className={clsx('px-6 py-4 hover:cursor-pointer w-full', {
-                'bg-[--ods-color-information-050]': isSelected,
-              })}
-              onClick={handleSelectChange}
-            >
-              <div className="flex gap-5">
-                <RadioControl />
-                <Flag isoCode={countryCode} />
-                <Text preset="heading-6" className="text-nowrap">
-                  {title}
-                </Text>
-              </div>
-              <div className="flex justify-between gap-4">
-                <Text className="text-[--ods-color-heading] text-nowrap">
-                  {region}
-                </Text>
-                <DeploymentModeBadge mode={deploymentMode} />
-              </div>
-            </Card>
-          </Radio>
-        );
-      }}
-    />
-  );
-};
+}: TLocalizationCardProps) => (
+  <PciCard
+    selectable
+    compact
+    selected={isSelected}
+    onClick={() => onSelect(region)}
+  >
+    <PciCard.Header>
+      <Radio value={region}>
+        <RadioControl />
+        <RadioLabel className="font-bold text-lg text-[--ods-color-heading] gap-x-4 flex items-center">
+          <Flag isoCode={countryCode} /> {title}
+        </RadioLabel>
+      </Radio>
+      <div className="flex items-center justify-between gap-4 w-full">
+        <Text>{region}</Text>
+        <DeploymentModeBadge mode={deploymentMode} />
+      </div>
+    </PciCard.Header>
+  </PciCard>
+);
