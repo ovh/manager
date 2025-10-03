64ab91699afa62a42ed8b6588c5941afd45e362f
soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>
2025-10-03 10:03:23 +0200

    fix(dedicated-server): fix billing info
    ref: #MANAGER-18419
    
    Signed-off-by: soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>

--- packages/manager/apps/dedicated-servers/src/components/cells/renewCell.tsx
+++ packages/manager/apps/dedicated-servers/src/components/cells/renewCell.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { DataGridTextCell } from '@ovh-ux/manager-react-components';
 import { useTranslation } from 'react-i18next';
 import { DedicatedServer } from '@/data/types/server.type';
-import { DSBilling } from '../billingCell';
+import { DSBilling } from './billingCell';
 
 import { getRenewWording } from '../commonCellsWording';
 
 export const RenewCell = (server: DedicatedServer) => {
   const { t } = useTranslation('dedicated-servers');
   return (
-    <DSBilling server={server.name}>
+    <DSBilling server={server}>
       {(billingInfo) => (
         <DataGridTextCell>{t(getRenewWording(billingInfo))}</DataGridTextCell>
       )}
