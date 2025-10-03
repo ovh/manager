ce995edc460bec7d7c6daef8ff299f00d74b017d
soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>
2025-10-03 10:03:23 +0200

    fix(dedicated-server): fix billing info
    ref: #MANAGER-18419
    
    Signed-off-by: soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>

--- packages/manager/apps/dedicated-servers/src/hooks/useGetBillingInfo.tsx
+++ packages/manager/apps/dedicated-servers/src/hooks/useGetBillingInfo.tsx
@@ -2,18 +2,28 @@ import { useQuery } from '@tanstack/react-query';
 import { ApiError, ApiResponse } from '@ovh-ux/manager-core-api';
 import { getBillingInfoQueryKey, getBillingInfo } from '@/data/api/billingInfo';
 import { BillingInfo } from '@/data/types/billing.type';
+import { DedicatedServer } from '@/data/types/server.type';
+import { useGetNutanixServer } from './useGetNutanixServer';
 
 export type UseGetBillingInfoParams = {
-  server: string;
+  server: DedicatedServer;
 };
 
 export const useGetBillingInfo = ({ server }: UseGetBillingInfoParams) => {
+  const { nutanixServers } = useGetNutanixServer();
+  console.log(nutanixServers);
   const { data: billingInfoResponse, isLoading, isError, error } = useQuery<
     ApiResponse<BillingInfo>,
     ApiError
   >({
-    queryKey: getBillingInfoQueryKey(server),
-    queryFn: () => getBillingInfo(server),
+    queryKey: getBillingInfoQueryKey(server.name),
+    queryFn: () => {
+      const isNutanix = server.os.includes('nutanix'); // temproray check until manage tags
+      const cluster = isNutanix && nutanixServers[server.name];
+      console.log(isNutanix, cluster)
+      return getBillingInfo(server.name, isNutanix, cluster);
+    },
+    enabled: !!nutanixServers,
   });
 
   return { billingInfo: billingInfoResponse?.data, isLoading, isError, error };
