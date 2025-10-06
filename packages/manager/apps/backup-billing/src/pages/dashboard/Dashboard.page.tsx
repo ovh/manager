a1dd9a03ddc29dbd06deb12968c2b6563f1aeedc
Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>
2025-10-06 16:19:36 +0200

    feat(backup-billing): use mrc breadcrumb
    ref: #BKP-526
    
    Signed-off-by: Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>

--- packages/manager/apps/backup-billing/src/pages/dashboard/Dashboard.page.tsx
+++ packages/manager/apps/backup-billing/src/pages/dashboard/Dashboard.page.tsx
@@ -6,12 +6,12 @@ import { useTranslation } from 'react-i18next';
 
 import { OdsTab, OdsTabs } from '@ovhcloud/ods-components/react';
 
-import { BaseLayout } from '@ovh-ux/manager-react-components';
+import { BaseLayout, Breadcrumb } from '@ovh-ux/manager-react-components';
 import { useOvhTracking } from '@ovh-ux/manager-react-shell-client';
 
-import Breadcrumb from '@/components/breadcrumb/Breadcrumb.component';
 import { useDashboardTabs } from '@/hooks/dashboard/useDashboardTabs';
 import { urls } from '@/routes/Routes.constants';
+import { appName } from '@/App.constants';
 
 export default function DashboardPage() {
   const { t } = useTranslation(['common', 'dashboard']);
@@ -37,7 +37,7 @@ export default function DashboardPage() {
         header={{ title: t('dashboard:title') }}
         backLinkLabel={t('dashboard:back')}
         onClickReturn={onNavigateBackClicked}
-        breadcrumb={<Breadcrumb />}
+        breadcrumb={<Breadcrumb appName={appName} rootLabel={appName} />}
         tabs={
           tabs.length > 0 ? (
             <OdsTabs>
