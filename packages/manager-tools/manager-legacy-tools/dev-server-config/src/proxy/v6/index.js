dffe7897512e114893bac8353bc9cbb4b0d9b893
Anoop N <anoop.n@ovhcloud.com>
2025-10-06 20:15:04 +0530

    chore(dev-server): update api proxy paths to use new API hosts
    ref: #MANAGER-18470
    
    Signed-off-by: Anoop N <anoop.n@ovhcloud.com>

--- packages/manager-tools/manager-legacy-tools/dev-server-config/src/proxy/v6/index.js
+++ packages/manager-tools/manager-legacy-tools/dev-server-config/src/proxy/v6/index.js
@@ -1,7 +1,7 @@
 const TARGET = {
-  eu: 'https://www.ovh.com',
-  ca: 'https://ca.ovh.com',
-  us: 'https://us.ovhcloud.com',
+  eu: 'https://manager.eu.ovhcloud.com',
+  ca: 'https://manager.ca.ovhcloud.com',
+  us: 'https://manager.us.ovhcloud.com',
 };
 
 module.exports = (region, config = {}) => ({
