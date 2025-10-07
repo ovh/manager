512a121b258284a197fe3c6f34c3d2a2ca17fa86
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-10-07 09:22:27 +0200

    feat(backup-vault): add details vault page
    ref: #BKP-491
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/apps/backup-vaults/src/pages/listing/_components/__tests__/VaultIdCell.components.test.tsx
+++ packages/manager/apps/backup-vaults/src/pages/listing/_components/__tests__/VaultIdCell.components.test.tsx
@@ -5,6 +5,7 @@ import { describe, expect, it, vi } from 'vitest';
 
 import { mockVaults } from '@/mocks/vault/vaults';
 import { VaultIdCell } from '@/pages/listing/_components';
+import {urlParams, urls} from "../../../../routes/Routes.constants";
 
 vi.mock('@ovh-ux/manager-react-components', () => ({
   DataGridTextCell: ({ children }: { children: React.ReactNode }) => (
@@ -18,14 +19,27 @@ vi.mock('@ovh-ux/manager-react-components', () => ({
   ),
 }));
 
+const { useHref } = vi.hoisted(() => {
+  return {
+    useHref: vi.fn().mockImplementation((link) => link),
+  };
+});
+
+vi.mock('react-router-dom', () => ({
+  useHref
+}));
+
 describe('VaultIdCell', () => {
   it('renders link with targetSpec.name as label and default href', () => {
     const vault = mockVaults[0]!;
 
     render(<VaultIdCell {...vault} />);
 
+    expect(useHref).toBeCalled();
+
     const link = screen.getByTestId('link');
     expect(link).toHaveTextContent(vault.currentState.name);
-    expect(link.getAttribute('href')).toBe('/#/');
+    expect(link.getAttribute('href')).toBe(urls.dashboard.replace(urlParams.vaultId, vault.id));
+    expect(link.getAttribute('href')?.endsWith(`/${vault.id}`)).toBe(true);
   });
 });
