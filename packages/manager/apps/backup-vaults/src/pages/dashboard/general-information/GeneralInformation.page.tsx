2a458d3715befb0a20b4e3bd2a020a2c462166f4
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-10-07 09:22:27 +0200

    feat(backup-vault): add details vault page
    ref: #BKP-491
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/apps/backup-vaults/src/pages/dashboard/general-information/GeneralInformation.page.tsx
+++ packages/manager/apps/backup-vaults/src/pages/dashboard/general-information/GeneralInformation.page.tsx
@@ -1,149 +1,20 @@
-import React from 'react';
+import { BillingInformationsTileStandard } from "@ovh-ux/billing-informations"
 
-import { OdsText } from '@ovhcloud/ods-components/react';
+import {GeneralInformationTile} from "@/pages/dashboard/general-information/_components/general-information-tile/GeneralInformationTile.component";
+import { useParams } from "react-router-dom";
+import {useBackupVaultDetails} from "@/data/hooks/vault/getVaultDetails";
+import {SubscriptionTile} from "./_components/subscription-tile/SubscriptionTile.component";
 
-import { Clipboard, LinkType, Links } from '@ovh-ux/manager-react-components';
-
-import GeneralInformationTile from '@/components/dashboard/general-information/GeneralInformationTile.component';
 
 export default function GeneralInformationPage() {
-  const tiles = [
-    {
-      title: 'Service Information',
-      help: 'Basic details about the service',
-      items: [
-        {
-          id: 'description',
-          label: 'Description',
-          value: (
-            <OdsText>Hosted Private Cloud – VMware vSphere service running in GRA region.</OdsText>
-          ),
-        },
-        {
-          id: 'api-url',
-          label: 'API URL',
-          value: <Clipboard value="https://api.ovh.com/1.0/hostedprivatecloud" />,
-        },
-        {
-          id: 'service-id',
-          label: 'Service ID',
-          value: <Clipboard value="srv-12345-abcde" />,
-        },
-        {
-          id: 'region',
-          label: 'Region',
-          value: <OdsText>GRA (Gravelines, France)</OdsText>,
-        },
-        {
-          id: 'status',
-          label: 'Status',
-          value: <OdsText className="text-green-600">Active</OdsText>,
-        },
-      ],
-    },
-    {
-      title: 'Documentation',
-      help: 'Helpful external links',
-      items: [
-        {
-          id: 'docs',
-          label: 'Docs',
-          value: (
-            <Links
-              href="https://help.ovhcloud.com/csm/en-hosted-private-cloud"
-              label="Documentation"
-              type={LinkType.external}
-              target="_blank"
-            />
-          ),
-        },
-        {
-          id: 'api-reference',
-          label: 'API Reference',
-          value: (
-            <Links
-              href="https://api.ovh.com/console/"
-              label="API Explorer"
-              type={LinkType.external}
-              target="_blank"
-            />
-          ),
-        },
-        {
-          id: 'guides',
-          label: 'Guides',
-          value: (
-            <Links
-              href="https://help.ovhcloud.com/csm/en-hosted-private-cloud-vmware-vsphere-getting-started"
-              label="Getting Started Guide"
-              type={LinkType.external}
-              target="_blank"
-            />
-          ),
-        },
-      ],
-    },
-    {
-      title: 'Network Information',
-      help: 'Details about networking and connectivity',
-      items: [
-        {
-          id: 'ip-block',
-          label: 'Public IP Block',
-          value: <Clipboard value="51.210.0.0/28" />,
-        },
-        {
-          id: 'vrack',
-          label: 'vRack',
-          value: (
-            <Links
-              href="https://www.ovhcloud.com/en/network/vrack/"
-              label="vrack-12345"
-              type={LinkType.next}
-            />
-          ),
-        },
-        {
-          id: 'bandwidth',
-          label: 'Bandwidth',
-          value: <OdsText>1 Gbps Guaranteed</OdsText>,
-        },
-      ],
-    },
-    {
-      title: 'Support',
-      help: 'Support and assistance',
-      items: [
-        {
-          id: 'support-center',
-          label: 'Support Center',
-          value: (
-            <Links
-              href="https://help.ovhcloud.com/"
-              label="Open Support Center"
-              type={LinkType.external}
-              target="_blank"
-            />
-          ),
-        },
-        {
-          id: 'tickets',
-          label: 'Support Tickets',
-          value: (
-            <Links
-              href="https://www.ovh.com/manager/dedicated/#/ticket"
-              label="View Tickets"
-              type={LinkType.next}
-            />
-          ),
-        },
-      ],
-    },
-  ];
+  const { vaultId } = useParams<{ vaultId: string }>();
+  const { data: vaultResource } = useBackupVaultDetails({ vaultId: vaultId! })
 
   return (
-    <div className="max-w-6xl mx-auto px-12">
-      <GeneralInformationTile tiles={tiles} />
-    </div>
+    <section className="max-w-6xl mx-auto px-12 grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-8">
+      <GeneralInformationTile vaultId={vaultId!} />
+      <SubscriptionTile vaultId={vaultId!} />
+      <BillingInformationsTileStandard resourceName={vaultResource?.currentState?.resourceName} />
+    </section>
   );
 }
---
+++ packages/manager/apps/backup-vaults/src/pages/dashboard/general-information/_components/subscription-tile/_components/__tests__/BillingType.test.tsx
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react"
+import {
+  BillingType
+} from "../BillingType.component";
+
+describe("BillingType component a11y", () => {
+  it("Should render BillingType component", async () => {
+    const { container } = render(<BillingType />)
+
+    await expect(container).toBeAccessible();
+  })
+});
