2e396137dbfaf78241dc6a59f82247da34d6b54b
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-10-08 15:09:08 +0200

    feat(ips): byoip aggregate
    ref: #MANAGER-19789
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager/apps/ips/src/data/api/get/byoip.ts
+++ packages/manager/apps/ips/src/data/api/get/byoip.ts
@@ -1,4 +1,5 @@
 import { ApiResponse, apiClient } from '@ovh-ux/manager-core-api';
+import { IpTask } from '@/types';
 
 export type TokenParam = { campus: string };
 
@@ -13,4 +14,28 @@ export const getTokens = async (
 ): Promise<ApiResponse<TokenResponse>> =>
   apiClient.v6.get<TokenResponse>(
     `/me/bringYourOwnIp/token?campus=${encodeURIComponent(params.campus)}`,
+  );
+
+export type AggregateResponse = {
+  aggregationIp: string;
+  childrenIps: string[];
+}[];
+
+export const getAggregate = (ip: string) =>
+  apiClient.v6.get<AggregateResponse>(
+    `/ip/${encodeURIComponent(ip)}/bringYourOwnIp/aggregate`,
+  );
+
+export const postAggregate = ({
+  ip,
+  aggregationIp,
+}: {
+  ip: string;
+  aggregationIp: string;
+}) =>
+  apiClient.v6.post<IpTask>(
+    `/ip/${encodeURIComponent(ip)}/bringYourOwnIp/aggregate`,
+    {
+      aggregationIp,
+    },
   );
