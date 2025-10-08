b70c737209289a9623ddfa4e212f5269892a086f
achraf hafedh <achraf.hafedh.ext@ovhcloud.com>
2025-10-08 17:19:49 +0200

    refactor(ovh-payment-method): remove lodash dependency
    ref: #MANAGER-18066
    
    Signed-off-by: achraf hafedh <achraf.hafedh.ext@ovhcloud.com>

--- packages/components/ovh-payment-method/src/models/payment-mean/index.js
+++
@@ -1,6 +0,0 @@
-export * from './available-payment-mean.class';
-export * from './payment-mean.class';
-export * from './payment-mean-bank-account.class';
-export * from './payment-mean-credit-card.class';
-export * from './payment-mean-deferred-payment-account.class';
-export * from './payment-mean-paypal.class';
