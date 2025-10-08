323c65b7cd6e671255554c34e2f382639ec756e9
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2025-10-08 16:26:30 +0200

    refactor(pci-project): remove payment useless files after FM will payement integration
    ref: #MANAGER-19780
    
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

---
+++ packages/manager/apps/pci-project/src/data/api/eligibility.ts
@@ -0,0 +1,19 @@
+import { v6 } from '@ovh-ux/manager-core-api';
+import {
+  TEligibility,
+  TEligibilityVoucher,
+} from '@/data/types/eligibility.type';
+
+export const getEligibility = async (): Promise<TEligibility> => {
+  const { data } = await v6.get(`/cloud/eligibility`);
+
+  return data;
+};
+
+export const checkVoucherEligibility = async (
+  voucher: string,
+): Promise<TEligibilityVoucher | undefined> => {
+  const { data } = await v6.get(`/cloud/eligibility?voucher=${voucher}`);
+
+  return data?.voucher;
+};
