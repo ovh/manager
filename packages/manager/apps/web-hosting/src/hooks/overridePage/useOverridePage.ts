bbb695a66abc41d9b8f4fdabb5562d1bbd94b5c9
stif59100 <steeve.vanderstocken@ovhcloud.com>
2025-10-08 14:09:23 +0200

    refactor(web-hosting): refactor for organisation to routing
    ref: #PUWEBPT-84
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

---
+++ packages/manager/apps/web-hosting/src/hooks/overridePage/useOverridePage.ts
@@ -0,0 +1,8 @@
+import { useMatches } from 'react-router-dom';
+
+import { RouteMatch } from '@/routes/routes';
+
+export const useOverridePage = (): boolean => {
+  const matches = useMatches() as RouteMatch[];
+  return matches.some((match) => match?.handle?.isOverridePage);
+};
