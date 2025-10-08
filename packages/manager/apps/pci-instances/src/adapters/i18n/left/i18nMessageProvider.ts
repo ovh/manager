53d3b20bd68f91668be4c35877c7649ceeb2c571
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2025-10-08 12:04:15 +0200

    fix(pci-instances): replace getMessage implementation
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

--- packages/manager/apps/pci-instances/src/adapters/i18n/left/i18nMessageProvider.ts
+++ packages/manager/apps/pci-instances/src/adapters/i18n/left/i18nMessageProvider.ts
@@ -1,6 +1,6 @@
-import { MessageProviderPort } from "@/domain/port/messageProvider/left/port";
-import { i18nInstance } from "@/main";
+import { MessageProviderPort } from '@/domain/port/messageProvider/left/port';
+import { i18nInstance } from '@/main';
 
 export const i18nMessageProvider: MessageProviderPort = {
-    getMessage: i18nInstance.t
-}
+  getMessage: (key: string) => i18nInstance.t(key),
+};
