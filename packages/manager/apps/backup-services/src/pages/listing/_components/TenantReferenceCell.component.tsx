9580e7d88eeaa8328c4bc1ffa7afeb2707e3d3ea
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-10-08 17:49:20 +0200

    feat(backup-services): add vspc listing
    ref: #BKP-496
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/backup-services/src/pages/listing/_components/TenantReferenceCell.component.tsx
+++ packages/manager/apps/backup-services/src/pages/listing/_components/TenantReferenceCell.component.tsx
@@ -1,7 +1,8 @@
 import { DataGridTextCell } from '@ovh-ux/manager-react-components';
 
 import { Tenant } from '@/types/Tenant.type';
+import { VSPCTenant } from '@/types/VspcTenant.type';
 
-export const TenantReferenceCell = (tenant: Tenant) => {
+export const TenantReferenceCell = (tenant: Tenant | VSPCTenant) => {
   return <DataGridTextCell>{tenant.id}</DataGridTextCell>;
 };
