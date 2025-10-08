37c1c1a4f798100385a4fd15109db42f081c786c
Pierre-Philippe <ppprevost@aol.fr>
2025-10-08 09:34:24 +0200

    feat(pci-kubernetes): deploy multi node creation on cluster
    ref: #TAPC-4718
    
    Signed-off-by: Pierre-Philippe <ppprevost@aol.fr>

---
+++ packages/manager/apps/pci-kubernetes/src/components/pciCard/PciCardHeader.component.tsx
@@ -0,0 +1,14 @@
+import { PropsWithChildren } from 'react';
+
+import clsx from 'clsx';
+
+type TPciCardHeaderProps = PropsWithChildren<{
+  compact?: boolean;
+  className?: string;
+}>;
+
+export const PciCardHeader = ({ children, compact, className }: TPciCardHeaderProps) => (
+  <div className={clsx('flex items-start flex-col', compact ? 'gap-2' : 'gap-3', className)}>
+    {children}
+  </div>
+);
