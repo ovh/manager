89e35300d8a5c4a3605536060fd00e036e40944a
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-10-10 14:55:35 +0200

    feat(okms): add a visual picker for regions on kms order
    ref: #MANAGER-18827
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/okms/src/common/components/RadioCard/RadioCard.component.tsx
+++ packages/manager/apps/okms/src/common/components/RadioCard/RadioCard.component.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEventHandler, FC, ReactElement, ReactNode } from 'react';
+import React, { ChangeEventHandler, ReactElement, ReactNode } from 'react';
 import clsx from 'clsx';
 import { OdsText } from '@ovhcloud/ods-components/react';
 
@@ -14,7 +14,7 @@ type RadioCardProps = {
   onChange: ChangeEventHandler<HTMLInputElement>;
 };
 
-export const RadioCard: FC<RadioCardProps> = ({
+export const RadioCard = ({
   children,
   id,
   name,
@@ -24,7 +24,7 @@ export const RadioCard: FC<RadioCardProps> = ({
   badges,
   selected,
   onChange,
-}) => {
+}: RadioCardProps) => {
   const isChecked = selected === id;
 
   return (
@@ -32,39 +32,46 @@ export const RadioCard: FC<RadioCardProps> = ({
       key={id}
       htmlFor={id}
       className={clsx(
-        'flex gap-2 p-3 border-solid rounded-md border-2  ',
-        !isDisabled && 'cursor-pointer ',
+        'flex border-solid rounded-md border px-3 py-4',
+        !isDisabled && 'cursor-pointer',
         !isChecked && 'border-[--ods-color-form-element-border-default]',
         !isChecked &&
           !isDisabled &&
           'hover:border-[--ods-color-form-element-border-hover-default]',
-        isChecked && !isDisabled && 'border-[--ods-color-primary-500]',
+        isChecked &&
+          !isDisabled &&
+          'border-[--ods-color-primary-500] bg-[--ods-color-primary-050] outline outline-1 outline-[--ods-color-primary-500]',
         isDisabled &&
           'cursor-not-allowed grayscale opacity-70 bg-[--ods-color-background-disabled-default] border-[--ods-color-border-disabled-default]',
       )}
     >
       <div className="flex flex-col gap-3">
         {/* HEADER */}
-        <div className="flex flex-col gap-2">
-          <div className="flex items-center gap-3">
-            <input
-              type="radio"
-              data-testid={id}
-              id={id}
-              value={id}
-              name={name}
-              checked={isChecked}
-              onChange={onChange}
-              disabled={isDisabled}
-              className="self-start"
-            />
-            <div className="flex gap-1 flex-wrap">
-              <OdsText preset="heading-6">{title}</OdsText>
-              {subTitle && <OdsText preset="span">{subTitle}</OdsText>}
-            </div>
+        <div className="flex gap-3 items-center">
+          <input
+            type="radio"
+            data-testid={id}
+            id={id}
+            value={id}
+            name={name}
+            checked={isChecked}
+            onChange={onChange}
+            disabled={isDisabled}
+            className="m-0"
+          />
+          <div className="flex flex-wrap items-center gap-y-1">
+            <OdsText preset="heading-6" className="mr-1">
+              {title}
+            </OdsText>
+            {subTitle && (
+              <OdsText preset="span" className="mr-2">
+                {subTitle}
+              </OdsText>
+            )}
+            {badges && <span>{badges}</span>}
           </div>
-          {badges}
         </div>
+        {/* BODY */}
         {children}
       </div>
     </label>
