4fe7d3f762baefcc4ec1891259306eb922a53e08
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-10-13 18:20:04 +0200

    feat(backup-services): add delete tenant from listing
    ref: #BKP-497
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/backup-services/src/pages/listing/_components/TenantActionsCell.component.tsx
+++ packages/manager/apps/backup-services/src/pages/listing/_components/TenantActionsCell.component.tsx
@@ -1,11 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+
 import { OdsButton } from '@ovhcloud/ods-components/react';
 
 import { DataGridTextCell } from '@ovh-ux/manager-react-components';
 
-export const TenantActionCell = () => {
+import { Tenant } from '@/types/Tenant.type';
+import { VSPCTenant } from '@/types/VspcTenant.type';
+import { buildDeleteTenantUrl } from '@/utils/buildSearchQuery.utils';
+
+export const TenantActionCell = (tenant: Tenant | VSPCTenant) => {
+  const navigate = useNavigate();
+
   return (
     <DataGridTextCell>
-      <OdsButton label="" icon="trash" variant="ghost" isDisabled />
+      <OdsButton
+        label=""
+        icon="trash"
+        variant="ghost"
+        onClick={() => {
+          navigate(buildDeleteTenantUrl({ tenantId: tenant.id, origin: 'listing' }));
+        }}
+      />
     </DataGridTextCell>
   );
 };
--- packages/manager/apps/backup-services/src/routes/Routes.constants.ts
+++ packages/manager/apps/backup-services/src/routes/Routes.constants.ts
@@ -6,14 +6,17 @@ export const subRoutes = {
   'general-information': '' as const,
   onboarding: 'onboarding' as const,
   dashboard: ':tenantId' as const,
+  delete: 'delete' as const,
   help: 'help' as const,
 } as const;
 
 export const urls = {
   root: ROOT_URL,
   listing: ROOT_URL,
+  listingTenantDelete: `${ROOT_URL}/${subRoutes.delete}`,
   onboarding: `${ROOT_URL}/${subRoutes.onboarding}`,
   dashboard: `${ROOT_URL}/${subRoutes.dashboard}`,
+  dashboardTenantDelete: `${ROOT_URL}/${subRoutes.dashboard}/${subRoutes.delete}`,
   help: `${ROOT_URL}/${subRoutes.dashboard}/${subRoutes.help}`,
 } as const;
 
