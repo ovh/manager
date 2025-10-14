5740a5f7fca2eefc47d038adc7040ebecd312b8d
Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
2025-10-14 20:24:21 +0200

    feat(communication): add settings listing page
    ref: #MANAGER-18774
    
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>

--- packages/manager/apps/communication/src/data/index.ts
+++ packages/manager/apps/communication/src/data/index.ts
@@ -3,11 +3,13 @@ import {
   useNotificationReference,
   useNotificationHistory,
 } from './hooks/useNotification/useNotification';
+import { useNotificationRouting } from './hooks/useNotificationRouting/useNotificationRouting';
 import { useAccountUrn } from './hooks/useAccountUrn/useAccountUrn';
 
 export {
   useNotification,
   useNotificationReference,
   useNotificationHistory,
+  useNotificationRouting,
   useAccountUrn,
 };
