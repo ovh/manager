5fb9f8f5efa917311b010ffce8eff51ef9f0b981
Marc Monchablon <marc.monchablon.ext@corp.ovh.com>
2025-10-15 00:39:59 +0200

    fixup! fix(iam): update scroll when opening dropdown in modals
    Signed-off-by: Marc Monchablon <marc.monchablon.ext@corp.ovh.com>

--- packages/manager/modules/iam/src/components/conditions/conditionType/elementUi.service.js
+++ packages/manager/modules/iam/src/components/conditions/conditionType/elementUi.service.js
@@ -2,16 +2,22 @@ import { DROPDOWN_SELECTOR } from './elementUi.contants';
 
 export default class ElementUiService {
   static scrollOuiCalendarIntoView($element) {
+    // NOTE: We use querySelector() instead of $element.find() because JQuery
+    // isn't available in staging, in which case AngularJS's JqLite is used instead,
+    // and JqLite search method doesn't support class selector.
     const calendarSelector = DROPDOWN_SELECTOR.calendar;
-    const calendarDropdownElement = $element.find(calendarSelector)[0];
+    const element = $element[0];
+    const calendarDropdownElement = element?.querySelector(calendarSelector);
     if (calendarDropdownElement) {
       calendarDropdownElement.scrollIntoView();
     }
   }
 
   static scrollOuiSelectIntoView($element) {
+    // NOTE: See `scrollOuiCalendarIntoView` comment about $element.find().
     const dropdownSelector = DROPDOWN_SELECTOR.select;
-    const dropdownElement = $element.find(dropdownSelector)[0];
+    const element = $element[0];
+    const dropdownElement = element?.querySelector(dropdownSelector);
     if (dropdownElement) {
       dropdownElement.scrollIntoView();
     }
