b162b6e49f9e73db9baba9167877f049ca3cf128
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-10-15 15:17:23 +0200

    feat(okms): add secret-manager okms dashboard
    ref: #MANAGER-19793
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

---
+++ packages/manager/apps/okms/src/modules/secret-manager/pages/okmsDashboard/OkmsDashboard.constants.ts
@@ -0,0 +1,4 @@
+export const OKMS_DASHBOARD_TEST_IDS = {
+  okmsDashboardTiles: 'okms-dashboard-tiles',
+  errorBanner: 'okms-dashboard-error-banner',
+};
---
+++ packages/manager/apps/okms/src/modules/secret-manager/pages/okmsDashboard/OkmsDashboard.type.ts
@@ -0,0 +1,5 @@
+import { OKMS } from '@/types/okms.type';
+
+export type OkmsDashboardOutletContext = {
+  okms: OKMS;
+};
