9e4074acac24d65631fbc9f0eb7391af0e30d9d6
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-10-15 11:32:52 +0200

    feat(okms): add okms dashboard tiles
    ref: #MANAGER-19793
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

---
+++ packages/manager/apps/okms/src/common/components/okmsDashboard/secretsTile/items/SecretCountTileItem.spec.tsx
@@ -0,0 +1,21 @@
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { okmsMock } from '@/mocks/kms/okms.mock';
+import { labels } from '@/utils/tests/init.i18n';
+import { SecretCountTileItem } from './SecretCountTileItem.component';
+import { renderWithI18n } from '@/utils/tests/testUtils';
+
+const okmsMocked = okmsMock[0];
+
+describe('OKMS Secret count Tile Item test suite', () => {
+  it('should render the tile item correctly', async () => {
+    // GIVEN okmsMocked
+
+    // WHEN
+    await renderWithI18n(<SecretCountTileItem okms={okmsMocked} />);
+
+    // THEN
+    expect(screen.getByText(labels.dashboard.okms_secret_count)).toBeVisible();
+    expect(screen.getByText(okmsMocked.secretCount)).toBeVisible();
+  });
+});
---
+++ packages/manager/apps/okms/src/common/components/okmsDashboard/serviceKeysTile/items/ServiceKeyListLinkTileItem.component.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Links, LinkType, ManagerTile } from '@ovh-ux/manager-react-components';
+import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { OKMS } from '@/types/okms.type';
+import { KMS_ROUTES_URLS } from '@/routes/routes.constants';
+
+type ServiceKeyListLinkTileItemProps = {
+  okms: OKMS;
+};
+
+export const ServiceKeyListLinkTileItem = ({
+  okms,
+}: ServiceKeyListLinkTileItemProps) => {
+  const navigate = useNavigate();
+  const { t } = useTranslation('key-management-service/common');
+
+  return (
+    <ManagerTile.Item>
+      <Links
+        type={LinkType.next}
+        label={t('manage_service_keys_link')}
+        onClickReturn={() =>
+          navigate(KMS_ROUTES_URLS.serviceKeyListing(okms.id))
+        }
+      />
+    </ManagerTile.Item>
+  );
+};
