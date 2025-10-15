498e27bfa1368ae07b87d3cbd704cc852d10442d
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-10-15 11:32:52 +0200

    feat(okms): add okms dashboard tiles
    ref: #MANAGER-19793
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

---
+++ packages/manager/apps/okms/src/common/components/okmsDashboard/secretsTile/items/SecretListLinkTileItem.component.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Links, LinkType, ManagerTile } from '@ovh-ux/manager-react-components';
+import { SECRET_MANAGER_ROUTES_URLS } from '@secret-manager/routes/routes.constants';
+import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { OKMS } from '@/types/okms.type';
+
+type SecretListLinkTileItemProps = {
+  okms: OKMS;
+};
+
+export const SecretListLinkTileItem = ({
+  okms,
+}: SecretListLinkTileItemProps) => {
+  const navigate = useNavigate();
+  const { t } = useTranslation('key-management-service/common');
+
+  return (
+    <ManagerTile.Item>
+      <Links
+        type={LinkType.next}
+        label={t('manage_secrets_link')}
+        onClickReturn={() =>
+          navigate(SECRET_MANAGER_ROUTES_URLS.secretList(okms.id))
+        }
+      />
+    </ManagerTile.Item>
+  );
+};
