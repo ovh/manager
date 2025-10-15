83ad5f407a21dae423737f09d5163b9c149d12d7
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-10-15 16:51:48 +0200

    feat(muk): refactor use translated micro region hook
    ref: #MANAGER-19192
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-ui-kit/src/hooks/index.ts
+++ packages/manager-ui-kit/src/hooks/index.ts
@@ -5,8 +5,4 @@ export * from './date';
 export * from './iam';
 export * from './bytes/useBytes';
 export * from './data-api';
-export {
-  getMacroRegion,
-  useTranslatedMicroRegions,
-  isLocalZone,
-} from './region/useTranslatedMicroRegions';
+export * from './region';
