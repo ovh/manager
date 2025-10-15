0ee854d59f52e3dfde108c329cb229e1109b7cca
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-10-15 11:32:52 +0200

    feat(okms): add okms dashboard tiles
    ref: #MANAGER-19793
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

---
+++ packages/manager/apps/okms/src/common/components/okmsDashboard/kmipObjectsTile/items/KmipObjectCountTileItem.component.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ManagerTile } from '@ovh-ux/manager-react-components';
+import { useTranslation } from 'react-i18next';
+import { OdsText } from '@ovhcloud/ods-components/react';
+import { OKMS } from '@/types/okms.type';
+
+type KmipObjectCountTileItemProps = {
+  okms: OKMS;
+};
+
+export const KmipObjectCountTileItem = ({
+  okms,
+}: KmipObjectCountTileItemProps) => {
+  const { t } = useTranslation('key-management-service/dashboard');
+
+  return (
+    <ManagerTile.Item>
+      <ManagerTile.Item.Label tooltip={t('okms_kmip_object_count_tooltip')}>
+        {t('okms_kmip_object_count')}
+      </ManagerTile.Item.Label>
+      <ManagerTile.Item.Description>
+        <OdsText preset="span">{okms.kmipObjectCount}</OdsText>
+      </ManagerTile.Item.Description>
+    </ManagerTile.Item>
+  );
+};
