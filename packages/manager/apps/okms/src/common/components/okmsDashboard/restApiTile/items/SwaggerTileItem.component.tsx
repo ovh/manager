f9fe6ee6e0e380177a342324517105e5f4ddd5ec
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-10-15 11:32:52 +0200

    feat(okms): add okms dashboard tiles
    ref: #MANAGER-19793
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

---
+++ packages/manager/apps/okms/src/common/components/okmsDashboard/restApiTile/items/SwaggerTileItem.component.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  ButtonType,
+  PageLocation,
+  useOvhTracking,
+} from '@ovh-ux/manager-react-shell-client';
+import { ManagerTile, Links, LinkType } from '@ovh-ux/manager-react-components';
+import { OKMS } from '@/types/okms.type';
+import { SWAGGER_UI_LABEL } from '@/constants';
+
+type SwaggerTileItemProps = {
+  okms: OKMS;
+};
+
+export const SwaggerTileItem = ({ okms }: SwaggerTileItemProps) => {
+  const { trackClick } = useOvhTracking();
+
+  return (
+    <ManagerTile.Item>
+      <ManagerTile.Item.Label>{SWAGGER_UI_LABEL}</ManagerTile.Item.Label>
+      <ManagerTile.Item.Description>
+        <Links
+          type={LinkType.external}
+          href={okms.swaggerEndpoint}
+          onClickReturn={() =>
+            trackClick({
+              location: PageLocation.page,
+              buttonType: ButtonType.externalLink,
+              actionType: 'navigation',
+              actions: ['swagger-ui'],
+            })
+          }
+          label={okms.swaggerEndpoint}
+          target="_blank"
+        />
+      </ManagerTile.Item.Description>
+    </ManagerTile.Item>
+  );
+};
---
+++ packages/manager/apps/okms/src/common/components/okmsDashboard/secretsTile/items/SecretVersionCountTileItem.component.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ManagerTile } from '@ovh-ux/manager-react-components';
+import { useTranslation } from 'react-i18next';
+import { OdsText } from '@ovhcloud/ods-components/react';
+import { OKMS } from '@/types/okms.type';
+
+type SecretVersionCountTileItemProps = {
+  okms: OKMS;
+};
+
+export const SecretVersionCountTileItem = ({
+  okms,
+}: SecretVersionCountTileItemProps) => {
+  const { t } = useTranslation('key-management-service/dashboard');
+
+  return (
+    <ManagerTile.Item>
+      <ManagerTile.Item.Label>
+        {t('okms_secret_version_count')}
+      </ManagerTile.Item.Label>
+      <ManagerTile.Item.Description>
+        <OdsText preset="span">{okms.secretVersionCount}</OdsText>
+      </ManagerTile.Item.Description>
+    </ManagerTile.Item>
+  );
+};
