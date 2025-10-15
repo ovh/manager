17016784ee990ebb696624b390dc81556e3bb9d6
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-10-15 12:03:14 +0200

    feat(okms): add <OkmsUpdateNameModal /> common component
    ref: #MANAGER-19793
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

---
+++ packages/manager/apps/okms/src/common/components/okmsUpdateNameModal/OkmsUpdateNameModal.component.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { UpdateNameModal } from '@ovh-ux/manager-react-components';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import { NAMESPACES } from '@ovh-ux/manager-common-translations';
+import { useUpdateOkmsName } from '@/data/hooks/useUpdateOkmsName';
+import { OKMS } from '@/types/okms.type';
+
+type OkmsUpdateNameModalProps = {
+  okms: OKMS;
+};
+
+const OkmsUpdateNameModal = ({ okms }: OkmsUpdateNameModalProps) => {
+  const { t } = useTranslation([NAMESPACES.DASHBOARD, NAMESPACES.ACTIONS]);
+  const navigate = useNavigate();
+
+  const { updateOkmsName, isPending, error: updateError } = useUpdateOkmsName({
+    okms,
+    onSuccess: () => {
+      navigate('..');
+    },
+  });
+
+  const handleUpdateDisplayName = (newDisplayName: string) => {
+    updateOkmsName({ displayName: newDisplayName });
+  };
+
+  return (
+    <UpdateNameModal
+      isOpen
+      headline={t('modify_name', { ns: NAMESPACES.ACTIONS })}
+      inputLabel={t('display_name', { ns: NAMESPACES.DASHBOARD })}
+      defaultValue={okms.iam.displayName}
+      isLoading={isPending}
+      error={updateError?.message}
+      closeModal={() => {
+        navigate('..');
+      }}
+      updateDisplayName={handleUpdateDisplayName}
+    />
+  );
+};
+
+export default OkmsUpdateNameModal;
