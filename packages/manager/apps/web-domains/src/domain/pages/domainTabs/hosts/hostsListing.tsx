9f7bc330f232e2bc185c3b527c805a8b734a10f5
Louis BENSI <louis.bensi.ext@corp.ovh.com>
2025-10-15 10:49:49 +0200

    feat(web-domains-data): hosts - datagrid
    ref: #DCE-39
    
    Signed-off-by: Louis BENSI <louis.bensi.ext@corp.ovh.com>

---
+++ packages/manager/apps/web-domains/src/domain/pages/domainTabs/hosts/hostsListing.tsx
@@ -0,0 +1,123 @@
+import { NAMESPACES } from '@ovh-ux/manager-common-translations';
+import { Datagrid } from '@ovh-ux/manager-react-components';
+import {
+  Button,
+  BUTTON_SIZE,
+  ICON_NAME,
+  Message,
+  MESSAGE_COLOR,
+  Text,
+  TEXT_PRESET,
+  MessageBody,
+  MessageIcon,
+} from '@ovhcloud/ods-react';
+import { useTranslation } from 'react-i18next';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useHostsDatagridColumns } from '@/domain/hooks/domainTabs/useHostsDatagridColumns';
+import { useGetDomainResource } from '@/domain/hooks/data/query';
+import { StatusEnum } from '@/domain/enum/Status.enum';
+import Loading from '@/domain/components/Loading/Loading';
+import { useNichandleInformation } from '@/common/hooks/nichandle/useNichandleInformation';
+
+export default function HostsListingTab() {
+  const params = useParams();
+  const { t } = useTranslation(['domain', NAMESPACES.ACTIONS, NAMESPACES.FORM]);
+  const [hostsArray, setHostsArray] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { domainResource } = useGetDomainResource(params.serviceName);
+  const { nichandleInformation } = useNichandleInformation();
+
+  useEffect(() => {
+    const hostsCurrentState =
+      domainResource?.currentState?.hostsConfiguration?.hosts || [];
+    const hostsTargetSpec =
+      domainResource?.targetSpec?.hostsConfiguration?.hosts || [];
+
+    const targetMap = new Map(hostsTargetSpec.map((h) => [h.host, h]));
+
+    const hostsWithStatus = hostsCurrentState.map((current) => {
+      const target = targetMap.get(current.host);
+
+      if (!target) {
+        return { ...current, status: StatusEnum.DELETING };
+      }
+
+      const ipsEqual =
+        current.ips.length === target.ips.length &&
+        current.ips.every((ip) => target.ips.includes(ip));
+
+      targetMap.delete(current.host);
+
+      return {
+        ...current,
+        status: ipsEqual ? StatusEnum.ENABLED : StatusEnum.UPDATING,
+      };
+    });
+
+    const activatingHosts = Array.from(targetMap.values()).map((target) => ({
+      ...target,
+      status: StatusEnum.ACTIVATING,
+    }));
+
+    setHostsArray([...hostsWithStatus, ...activatingHosts]);
+    setIsLoading(false);
+  }, [domainResource]);
+
+  const columns = useHostsDatagridColumns();
+
+  if (isLoading && nichandleInformation) {
+    return <Loading />;
+  }
+
+  const account = nichandleInformation?.auth?.account;
+  const {
+    id,
+  } = domainResource?.currentState?.contactsConfiguration?.contactAdministrator;
+
+  return id !== account ? (
+    <Message
+      color={MESSAGE_COLOR.warning}
+      className="w-full"
+      data-testid="warningMessage"
+    >
+      <MessageIcon name={ICON_NAME.triangleExclamation} />
+      <MessageBody>
+        <Text preset={TEXT_PRESET.heading6}>
+          {t('domain_tab_hosts_listing_warning_title')}
+        </Text>
+        <Text>{t('domain_tab_hosts_listing_warning_sub_1')}</Text>
+        <Text>{t('domain_tab_hosts_listing_warning_sub_2')}</Text>
+      </MessageBody>
+    </Message>
+  ) : (
+    <section>
+      <div className="flex flex-col gap-y-4 mb-6">
+        <Message>
+          <MessageIcon name={ICON_NAME.circleInfo} />
+          <MessageBody>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget
+            maximus leo, ac rutrum mi. Quisque auctor augue nec est pulvinar, ut
+            iaculis elit placerat. Curabitur facilisis libero et nisi consequat
+            lacinia nec eget turpis. Mauris congue varius lorem sit amet
+            malesuada. Vestibulum ac metus nisi.
+          </MessageBody>
+        </Message>
+      </div>
+
+      <div data-testid="datagrid">
+        <Datagrid
+          columns={columns}
+          items={hostsArray}
+          totalItems={hostsArray.length}
+          topbar={
+            <Button className="mb-4" size={BUTTON_SIZE.sm}>
+              {t(`${NAMESPACES.ACTIONS}:add`)}
+            </Button>
+          }
+        />
+      </div>
+    </section>
+  );
+}
--- packages/manager/apps/web-domains/src/domain/types/domainResource.ts
+++ packages/manager/apps/web-domains/src/domain/types/domainResource.ts
@@ -13,6 +13,7 @@ import { TaskStatusEnum } from '@/domain/enum/taskStatus.enum';
 import { OptionStateEnum } from '../enum/optionState.enum';
 import { OptionEnum } from '../../common/enum/option.enum';
 import { AssociatedEmailsServicesEnum } from '../enum/associatedServices.enum';
+import { THost, THostsconfiguration } from './host';
 
 export interface TNameServer {
   ipv4?: string | null;
@@ -66,6 +67,7 @@ export interface TDomainResource {
     protectionState: ProtectionStateEnum;
     suspensionState: SuspensionStateEnum;
     contactsConfiguration: TContactsConfiguration;
+    hostsConfiguration: THostsconfiguration;
     createdAt: string;
   };
   currentTasks: Task[];
@@ -75,6 +77,9 @@ export interface TDomainResource {
   targetSpec?: {
     dnsConfiguration?: {
       nameServers: TNameServer[];
+    };
+    hostsConfiguration?: {
+      hosts: THost[];
     };
   };
 }
