6c2b56bf833a1f498b0722d7eb06cae6cc440a3b
Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>
2025-10-16 08:48:39 +0200

    feat(billing): us ftc compliance, commitment wording on my services
    ref: #MANAGER-19940
    
    Signed-off-by: Omar ALKABOUSS MOUSSANA <omar.alkabouss-moussana.ext@corp.ovh.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/billing-components/src/components/cancel-commitment/cancel-commitment.controller.js
+++ packages/manager/modules/billing-components/src/components/cancel-commitment/cancel-commitment.controller.js
@@ -1,9 +1,14 @@
 export default class ServicesActionsCtrl {
   /* @ngInject */
-  constructor($http, $translate, atInternet) {
+  constructor($http, $translate, atInternet, coreConfig) {
     this.$http = $http;
     this.$translate = $translate;
     this.atInternet = atInternet;
+    this.coreConfig = coreConfig;
+  }
+
+  $onInit() {
+    this.isUSRegion = this.coreConfig.isRegion('US');
   }
 
   cancelCommitment() {
@@ -19,20 +24,20 @@ export default class ServicesActionsCtrl {
     // cancel commitment API call
     return this.$http
       .delete(`/services/${this.serviceId}/billing/engagement/request`)
-      .then(() =>
-        this.goBack(
-          this.$translate.instant('billing_cancel_commitment_success'),
-        ),
-      )
-      .catch(
-        (error) =>
-          this.goBack(
-            `${this.$translate.instant('billing_cancel_commitment_error')}${
-              error.data?.message
-            }`,
-          ),
-        'error',
-      )
+      .then(() => {
+        const key = this.isUSRegion
+          ? 'billing_cancel_commitment_success_us'
+          : 'billing_cancel_commitment_success';
+        return this.goBack(this.$translate.instant(key));
+      })
+      .catch((error) => {
+        const key = this.isUSRegion
+          ? 'billing_cancel_commitment_error_us'
+          : 'billing_cancel_commitment_error';
+        return this.goBack(
+          `${this.$translate.instant(key)}${error.data?.message}`,
+        );
+      }, 'error')
       .finally(() => {
         this.isLoading = false;
       });
