0c72bf13b62a068dea56b668b9dd85c12bc86f0a
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2025-10-16 14:44:13 +0200

    feat(pci-instances): allow user to select new region for unavailable flavor
    ref: #TAPC-4635
    
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

--- packages/manager/apps/pci-instances/src/pages/instances/create/components/FlavorBlock.component.tsx
+++ packages/manager/apps/pci-instances/src/pages/instances/create/components/FlavorBlock.component.tsx
@@ -2,16 +2,49 @@ import {
   Checkbox,
   CheckboxControl,
   CheckboxLabel,
+  SelectGroupItem,
   Text,
 } from '@ovhcloud/ods-react';
-import { FC } from 'react';
-import { useTranslation } from 'react-i18next';
+import { FC, useState } from 'react';
+import { Trans, useTranslation } from 'react-i18next';
 import FlavorHelper from './flavor/FlavorHelper.component';
 import FlavorCategory from './flavor/FlavorCategory.component';
 import FlavorType from './flavor/flavorType/FlavorType.component';
+import RegionSelectionModal, {
+  TCustomRegionItemData,
+} from './RegionSelectionModal.component';
+import {
+  Controller,
+  ControllerRenderProps,
+  useFormContext,
+} from 'react-hook-form';
+import { TInstanceCreationForm } from '../CreateInstance.page';
+import { mockedFlavorAvailableRegions } from '@/__mocks__/instance/constants';
 
 const FlavorBlock: FC = () => {
   const { t } = useTranslation('creation');
+  const [unavailableFlavor, setUnavailableFlavor] = useState<string | null>(
+    null,
+  );
+  const { control, setValue } = useFormContext<TInstanceCreationForm>();
+
+  // TODO: will be move to a select view-model
+  const items: SelectGroupItem<
+    TCustomRegionItemData
+  >[] = mockedFlavorAvailableRegions as SelectGroupItem<
+    TCustomRegionItemData
+  >[];
+
+  const handleCloseSelectRegion = () => setUnavailableFlavor(null);
+
+  const handleSelectRegion = (
+    field: ControllerRenderProps<TInstanceCreationForm, 'microRegion'>,
+  ) => (region: string) => {
+    field.onChange(region);
+    setValue('deploymentModes', []);
+    setValue('continent', 'all');
+    setValue('availabilityZone', null);
+  };
 
   return (
     <section>
@@ -40,6 +73,32 @@ const FlavorBlock: FC = () => {
           </CheckboxLabel>
         </Checkbox>
       </div>
+      {
+        // TODO: remove
+        <button onClick={() => setUnavailableFlavor('B2-15')}>select</button>
+      }
+      {unavailableFlavor && (
+        <Controller
+          control={control}
+          name="microRegion"
+          render={({ field }) => (
+            <RegionSelectionModal
+              regions={items}
+              onClose={handleCloseSelectRegion}
+              onValidateSelect={handleSelectRegion(field)}
+            >
+              <Text preset="paragraph">
+                <Trans
+                  t={t}
+                  i18nKey="pci_instance_creation_select_new_region_for_flavor"
+                  values={{ flavor: unavailableFlavor }}
+                  tOptions={{ interpolation: { escapeValue: true } }}
+                />
+              </Text>
+            </RegionSelectionModal>
+          )}
+        />
+      )}
     </section>
   );
 };
