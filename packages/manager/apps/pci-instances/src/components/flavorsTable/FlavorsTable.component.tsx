55431f5fca97cd590af67743330bd5529f18c719
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2025-10-16 15:28:26 +0200

    test(pci-instances): add generic flavor datagrid
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

--- packages/manager/apps/pci-instances/src/components/flavorsTable/FlavorsTable.component.tsx
+++ packages/manager/apps/pci-instances/src/components/flavorsTable/FlavorsTable.component.tsx
@@ -1,28 +1,116 @@
-import { RadioGroup, Table, TableProp, TABLE_SIZE, TABLE_VARIANT, Text } from '@ovhcloud/ods-react';
-import clsx from "clsx";
-import { FlavorsTableTr } from "@/components/flavorsTable/FlavorsTableTr.component";
-import { MouseEventHandler } from "react";
+import {
+  RadioGroup,
+  Table,
+  TableProp,
+  TABLE_SIZE,
+  TABLE_VARIANT,
+  Text,
+  Radio,
+  RadioControl,
+  BADGE_SIZE,
+  Badge,
+  BADGE_COLOR,
+  Icon,
+} from '@ovhcloud/ods-react';
+import clsx from 'clsx';
+import { FlavorsTableTr } from '@/components/flavorsTable/FlavorsTableTr.component';
+import { ComponentType, MouseEventHandler } from 'react';
+import { DeploymentModeBadge } from '../deploymentModeBadge/DeploymentModeBadge.component';
+import { handleClick } from '@ovh-ux/manager-react-components';
+import { TDeploymentMode } from '@/types/instance/common.type';
+
+export type TableRow = {
+  name: string;
+  memory: string;
+  vcore: string;
+  storage: string;
+  priceHour: string;
+  priceMonth: string;
+  unavailable: boolean;
+  mode: TDeploymentMode;
+};
+
+type TTrProps<T> = {
+  row: T;
+  className?: string;
+  contentClassName?: string;
+  onClick?: () => void;
+};
+
+export const FlavorTr = ({
+  className,
+  contentClassName,
+  row,
+  onClick,
+}: TTrProps<TableRow>) => (
+  <tr {...(className && { className })} onClick={onClick}>
+    <th {...(contentClassName && { className: contentClassName })}>
+      <Radio disabled={row.unavailable} value={row.name} className={'w-full'}>
+        <RadioControl />
+      </Radio>
+    </th>
+    <td {...(contentClassName && { className: contentClassName })}>
+      <div className="flex flex-row gap-4 w-full h-full flex-wrap items-center content-start">
+        <Text className={'font-normal'}>{row.name}</Text>
+        <Badge
+          size={BADGE_SIZE.sm}
+          color={BADGE_COLOR.warning}
+          className="h-fit text-wrap font-normal"
+        >
+          Quota indisponible
+          <Icon aria-label="Info" name="circle-info" role="img" />
+        </Badge>
+      </div>
+    </td>
+    <td {...(contentClassName && { className: contentClassName })}>
+      {row.memory}
+    </td>
+    <td {...(contentClassName && { className: contentClassName })}>
+      {row.vcore}
+    </td>
+    <td {...(contentClassName && { className: contentClassName })}>
+      {row.storage}
+    </td>
+    <td {...(contentClassName && { className: contentClassName })}>
+      <DeploymentModeBadge mode={row.mode} size={BADGE_SIZE.sm} />
+    </td>
+    <td {...(contentClassName && { className: contentClassName })}>
+      {row.priceHour}
+    </td>
+    <td {...(contentClassName && { className: contentClassName })}>
+      {row.priceMonth}
+    </td>
+  </tr>
+);
 
 export type TableColumn = {
   key: string;
   label?: React.ReactNode;
 };
 
-export type TableRow = {
-  id: string;
-  [key: string]: React.ReactNode;
-  unavailable?: boolean
-};
+type TData = { name: string; unavailable: boolean };
 
-export type TFlavorsTableProps = TableProp & React.HTMLAttributes<HTMLTableElement> & {
-  caption: string;
-  columns: TableColumn[];
-  rows: TableRow[];
-  selectable?: boolean;
-  onClick?: MouseEventHandler<HTMLTableRowElement>;
-};
+export type TFlavorsTableProps<T extends TData> = TableProp &
+  React.HTMLAttributes<HTMLTableElement> & {
+    caption: string;
+    columns: TableColumn[];
+    rows: T[];
+    selectable?: boolean;
+    TrChildren: ComponentType<TTrProps<T>>;
+    onClick?: MouseEventHandler<HTMLTableRowElement>;
+  };
 
-export const FlavorsTable = ({ caption, columns, rows, className, onClick, selectable= false, size=TABLE_SIZE.md , variant= TABLE_VARIANT.default }: TFlavorsTableProps) => {
+// eslint-disable-next-line react/no-multi-comp
+export const FlavorsTable = <T extends TData>({
+  caption,
+  columns,
+  rows,
+  className,
+  selectable = false,
+  size = TABLE_SIZE.md,
+  variant = TABLE_VARIANT.default,
+  TrChildren,
+}: TFlavorsTableProps<T>) => {
   const baseClasses = clsx(
     'min-w-full border-separate border-spacing-0',
 
@@ -37,28 +125,25 @@ export const FlavorsTable = ({ caption, columns, rows, className, onClick, selec
       '[&>thead>tr>th:nth-child(2)]:left-[32px] [&>tbody>tr>th[scope=row]]:left-[32px]',
       '[&>thead>tr>th:nth-child(2)]:z-20 [&>tbody>tr>th[scope=row]]:z-10',
     ],
-    className
+    className,
   );
 
-  const colHeaderClasses = "text-left border-l-0 first:border-l first:border-r-0 [&_*]:text-[--ods-color-heading] *:font-semibold"
+  const colHeaderClasses =
+    'text-left border-l-0 first:border-l first:border-r-0 [&_*]:text-[--ods-color-heading] *:font-semibold';
 
-  const rowClasses = clsx(
-    {
-      'group cursor-pointer': selectable,
-    }
-  );
+  const rowClasses = clsx({
+    'group cursor-pointer': selectable,
+  });
 
-  const cellClasses = clsx(
-    {
-      'bg-white group-hover:border-[--ods-color-primary-600] text-left': selectable,
-    }
-  );
+  const cellClasses = clsx({
+    'bg-white group-hover:border-[--ods-color-primary-600] text-left': selectable,
+  });
 
   return (
     <div className="overflow-x-auto max-w-full">
-      <RadioGroup>
+      <RadioGroup onValueChange={(v) => console.log('v', v)}>
         <Table size={size} variant={variant} className={baseClasses}>
-          <caption className={"sr-only"}>{caption}</caption>
+          <caption className={'sr-only'}>{caption}</caption>
           <thead>
             <tr>
               {columns.map((col) => (
@@ -70,35 +155,16 @@ export const FlavorsTable = ({ caption, columns, rows, className, onClick, selec
           </thead>
           <tbody>
             {rows.map((row) => (
-              <FlavorsTableTr
-                key={row.id}
-                className={rowClasses}
-                onClick={onClick}
-                unavailable={row.unavailable}
-              >
-                {columns.map((col, colIndex) => {
-                  const cellContent =
-                    typeof row[col.key] === "string" ? (
-                      <Text>{row[col.key]}</Text>
-                    ) : (
-                      row[col.key]
-                    );
-
-                  if (colIndex === 1) {
-                    return (
-                      <th key={col.key} scope="row" className={cellClasses}>
-                        {cellContent}
-                      </th>
-                    );
-                  }
-
-                  return (
-                    <td key={col.key} className={cellClasses}>
-                      {cellContent}
-                    </td>
-                  );
+              <TrChildren
+                key={row.name}
+                row={row}
+                className={clsx({
+                  'group cursor-pointer': selectable,
+                  'hover:cursor-not-allowed [&_*]:bg-[--ods-color-neutral-100] [&_*]:text-neutral-500 [&_[class^=_badge]]:bg-[--ods-color-neutral-500]':
+                    row.unavailable,
                 })}
-              </FlavorsTableTr>
+                contentClassName={cellClasses}
+              />
             ))}
           </tbody>
         </Table>
