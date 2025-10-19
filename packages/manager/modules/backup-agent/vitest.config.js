0633d6891413ebcfdf418ab3b2c02180a6521e98
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-10-19 22:50:37 +0200

    feat(backup-agent): init module for backup agent
    ref: #BKP-598
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

---
+++ packages/manager/modules/backup-agent/vitest.config.js
@@ -0,0 +1,30 @@
+import {
+  createConfig,
+  defaultExcludedFiles,
+  mergeConfig,
+  sharedConfig,
+} from '@ovh-ux/manager-tests-setup';
+
+// https://vitejs.dev/config/
+export default mergeConfig(
+  sharedConfig,
+  createConfig({
+    test: {
+      setupFiles: ['./src/test-utils/setup-test.ts'],
+      coverage: {
+        exclude: [
+          ...defaultExcludedFiles,
+          // App-specific exclusions (not in shared config):
+          'src/types',
+          'src/translations',
+          'src/test-utils',
+          'src/index.ts',
+          'src/data/types',
+          'src/data/mocks',
+          'src/**/*.spec.ts',
+          'src/**/*.spec.tsx',
+        ],
+      },
+    },
+  }),
+);
