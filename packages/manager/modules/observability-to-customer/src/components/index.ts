914bddafe025213dc851c28e8c1b8f6b476b97a4
yjaaouane <yassine.jaaouane.ext@corp.ovh.com>
2025-10-20 12:02:22 +0200

    feat(observability): add observability-to-customer module
    ref: #MAOBS-43
    
    Signed-off-by: yjaaouane <yassine.jaaouane.ext@corp.ovh.com>

---
+++ packages/manager/modules/observability-to-customer/src/components/index.ts
@@ -0,0 +1,5 @@
+export * from './dashboard';
+export * from './featureSwitcher';
+export * from './loaders';
+export * from './timeControls';
+export * from './toolbar';
---
+++ packages/manager/modules/observability-to-customer/src/routes/routes.tsx
@@ -0,0 +1,60 @@
+import { ComponentType, lazy } from 'react';
+
+import { Route, RouteObject } from 'react-router-dom';
+
+const importMetricsOverviewPage = () => import('../pages/metric/overview.page');
+const importMetricsDashboardPage = () => import('../pages/metric/dashboard.page');
+const importMetricsDashboardWidgetModal = () => import('../pages/metric/dashboard-widget.modal');
+
+const metricsOverviewPage = lazy(importMetricsOverviewPage);
+const metricsDashboardPage = lazy(importMetricsDashboardPage);
+const metricsDashboardWidgetModal = lazy(importMetricsDashboardWidgetModal);
+
+type ModuleWithDefault<T extends ComponentType<unknown>, E extends Record<string, unknown>> = {
+  default: T;
+} & E;
+
+export function lazyRouteConfig<
+  T extends ComponentType<unknown>,
+  E extends Record<string, unknown> = Record<string, unknown>,
+>(importFn: () => Promise<ModuleWithDefault<T, E>>) {
+  return {
+    lazy: async () => {
+      const { default: Component, ...rest } = await importFn();
+
+      return {
+        Component,
+        ...(rest as unknown as E),
+      };
+    },
+  };
+}
+
+/**
+ * @deprecated This export is deprecated. Please use getObservabilityRoute() instead.
+ */
+export const observabilityRoutes: RouteObject[] = [
+  {
+    path: 'overview',
+    ...lazyRouteConfig(importMetricsOverviewPage),
+  },
+  {
+    path: '',
+    ...lazyRouteConfig(importMetricsDashboardPage),
+    children: [
+      {
+        path: ':widgetId',
+        ...lazyRouteConfig(importMetricsDashboardWidgetModal),
+      },
+    ],
+  },
+];
+
+export const getObservabilityRoute = () => (
+  <>
+    <Route path="overview" id="overview" Component={metricsOverviewPage} />
+    <Route path="" id="dashboard" Component={metricsDashboardPage}>
+      <Route path=":widgetId" id="dashboard-widget" Component={metricsDashboardWidgetModal} />
+    </Route>
+  </>
+);
