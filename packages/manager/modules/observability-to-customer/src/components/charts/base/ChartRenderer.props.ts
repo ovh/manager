0817c2cb88de4ed4bd2132a0f859c1323964bf42
yjaaouane <yassine.jaaouane.ext@corp.ovh.com>
2025-10-20 12:02:22 +0200

    feat(observability): add observability-to-customer module
    ref: #MAOBS-43
    
    Signed-off-by: yjaaouane <yassine.jaaouane.ext@corp.ovh.com>

---
+++ packages/manager/modules/observability-to-customer/src/components/charts/base/ChartRenderer.props.ts
@@ -0,0 +1,7 @@
+import { DashboardState } from '../../../contexts';
+import { ChartProps, ChartType } from './Chart.type';
+
+export interface ChartRendererProps<TData> extends ChartProps<TData> {
+  type: ChartType;
+  state: DashboardState;
+}
