9483609da457dfc7ae09284d4d3f818ca9d3c253
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-10-21 17:37:45 +0200

    feat(backup-agent): add vaults page on module
    ref: #BKP-598
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/apps/backup-vaults/src/pages/dashboard/general-information/_components/subscription-tile/_components/index.ts
+++
@@ -1,5 +0,0 @@
-import { ConsumptionDetails} from "./ConsumptionDetails.component";
-import { ConsumptionRegionsList} from "./ConsumptionRegionsList.component";
-
-export { ConsumptionDetails, ConsumptionRegionsList };
-
---
+++ packages/manager/modules/backup-agent/src/pages/vaults/listing/_components/__tests__/VaultIdCell.components.test.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
+
+import { mockVaults } from '@/mocks/vaults/vaults';
+import { VaultIdCell } from '../VaultIdCell.components';
+import {urlParams, urls} from "@/routes/Routes.constants";
+
+vi.mock('@ovh-ux/manager-react-components', () => ({
+  DataGridTextCell: ({ children }: { children: React.ReactNode }) => (
+    <div data-testid="cell">{children}</div>
+  ),
+  // eslint-disable-next-line react/no-multi-comp
+  Links: ({ label, href }: { label: string; href: string }) => (
+    <a data-testid="link" href={href}>
+      {label}
+    </a>
+  ),
+}));
+
+const { useHref } = vi.hoisted(() => {
+  return {
+    useHref: vi.fn().mockImplementation((link) => link),
+  };
+});
+
+vi.mock('react-router-dom', () => ({
+  useHref
+}));
+
+describe('VaultIdCell', () => {
+  it('renders link with targetSpec.name as label and default href', () => {
+    const vault = mockVaults[0]!;
+
+    render(<VaultIdCell {...vault} />);
+
+    expect(useHref).toBeCalled();
+
+    const link = screen.getByTestId('link');
+    expect(link).toHaveTextContent(vault.currentState.name);
+    expect(link.getAttribute('href')).toBe(urls.dashboard.replace(urlParams.vaultId, vault.id));
+    expect(link.getAttribute('href')?.endsWith(`/${vault.id}`)).toBe(true);
+  });
+});
