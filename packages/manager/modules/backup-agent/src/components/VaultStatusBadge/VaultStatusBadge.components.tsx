414d894d63059d963b7678e8e30598be53c66acb
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-10-21 17:37:45 +0200

    feat(backup-agent): add vaults page on module
    ref: #BKP-598
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

---
+++ packages/manager/modules/backup-agent/src/components/VaultStatusBadge/VaultStatusBadge.components.tsx
@@ -0,0 +1,18 @@
+import {OdsBadge} from "@ovhcloud/ods-components/react";
+import {getColorVaultStatus} from "@/components/VaultStatusBadge/_utils/getVaultStatusColor.utils";
+import {ResourceStatus} from "@/types/Vault.type";
+import { useTranslation } from "react-i18next";
+import { NAMESPACES } from "@ovh-ux/manager-common-translations";
+
+export type VaultStatusBadgeProps = {
+  vaultStatus: ResourceStatus;
+}
+
+export const VaultStatusBadge = ({vaultStatus}: VaultStatusBadgeProps) => {
+  const {t} = useTranslation(NAMESPACES.STATUS);
+
+  return <OdsBadge
+    color={getColorVaultStatus(vaultStatus)}
+    label={t(vaultStatus.toLowerCase())}
+  />
+}
