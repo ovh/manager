dd0ebf1d09a38c44530137a803de2e0e1866f648
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-10-21 17:37:45 +0200

    feat(backup-agent): add vaults page on module
    ref: #BKP-598

---
+++ packages/manager/modules/backup-agent/src/lib.ts
@@ -0,0 +1,12 @@
+import { BackupAgentContext } from '@/BackupAgent.context';
+
+import { BACKUP_AGENT_NAMESPACES } from './BackupAgent.translations';
+import BackupAgentRoutes from './routes/routes';
+
+export { BackupAgentRoutes, BackupAgentContext, BACKUP_AGENT_NAMESPACES };
+
+export default {
+  BackupAgentRoutes,
+  BackupAgentContext,
+  BACKUP_AGENT_NAMESPACES,
+};
