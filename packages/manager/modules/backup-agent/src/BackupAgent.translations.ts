72ae2a52b35235dc9c7ecc793838a523adfbc9b7
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-10-22 13:28:41 +0200

    feat(backup-agent): add translation on main layout
    ref: #BKP-598
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/backup-agent/src/BackupAgent.translations.ts
+++ packages/manager/modules/backup-agent/src/BackupAgent.translations.ts
@@ -2,6 +2,7 @@ export const NAMESPACE_PREFIX = '@ovh-ux/backup-agent';
 export const VAULTS_NAMESPACE_PREFIX = `${NAMESPACE_PREFIX}/vaults`;
 
 export const BACKUP_AGENT_NAMESPACES = {
+  COMMON: `${NAMESPACE_PREFIX}/common`,
   VAULT_DASHBOARD: `${VAULTS_NAMESPACE_PREFIX}/dashboard`,
   VAULT_LISTING: `${VAULTS_NAMESPACE_PREFIX}/listing`,
 };
