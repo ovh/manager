80cf63c6c4a1e7eba86aebc96780c1437fa3a253
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-10-22 13:55:45 +0200

    fixup! feat(backup-agent-iaas): integration module backup-agent
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/apps/hpc-backup-agent-iaas/src/routes/Routes.tsx
+++ packages/manager/apps/hpc-backup-agent-iaas/src/routes/Routes.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 
 import { Navigate, Route } from 'react-router-dom';
 
+import { BackupAgentRoutes } from '@ovh-ux/backup-agent';
 import { ErrorBoundary } from '@ovh-ux/manager-react-components';
 import { PageType } from '@ovh-ux/manager-react-shell-client';
 
 import NotFound from '@/pages/not-found/404.page';
 
 import { redirectionApp, subRoutes, urls } from './Routes.constants';
-
-import { BackupAgentRoutes } from "@ovh-ux/backup-agent"
 
 const MainLayoutPage = React.lazy(() => import('@/pages/Main.layout'));
 const OnboardingPage = React.lazy(() => import('@/pages/onboarding/Onboarding.page'));
