a65f3d92370455af1bfb8435f272bdf4f8c07c8c
Marc Monchablon <marc.monchablon.ext@corp.ovh.com>
2025-10-22 18:00:00 +0200

    feat(iam): add 'Roadmap & Changelog' links
    ref: #MAIAM-44
    
    Signed-off-by: Marc Monchablon <marc.monchablon.ext@corp.ovh.com>

--- packages/manager/modules/iam/src/dashboard/dashboard.controller.js
+++ packages/manager/modules/iam/src/dashboard/dashboard.controller.js
@@ -1,6 +1,10 @@
-import { TAG } from '../iam.constants';
+import { TAG, CHANGELOG } from '../iam.constants';
 
 export default class DashboardController {
+  constructor() {
+    this.CHANGELOG = CHANGELOG;
+  }
+
   /**
    * Called back when a guide menu item is clicked
    * @param {string} guideKey
