d8e994f164d7f13a5c3deee2a543e1e06a7b277e
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2025-10-22 17:08:12 +0200

    feat(pci-instances): add illustration image for deployment cards
    ref: #TAPC-5237
    
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

--- packages/manager/apps/pci-instances/src/pages/instances/create/view-models/deploymentModeViewModel.ts
+++ packages/manager/apps/pci-instances/src/pages/instances/create/view-models/deploymentModeViewModel.ts
@@ -1,15 +1,31 @@
+import { ComponentType, SVGProps } from 'react';
 import { TDeploymentMode } from '@/types/instance/common.type';
-import InstanceImage from '../../../../../public/assets/instance.png';
+import Region1azImage from '../../../../../public/assets/1AZ.svg';
+import Region3azImage from '../../../../../public/assets/3AZ.svg';
+import LZImage from '../../../../../public/assets/LZ.svg';
 
 import { Reader } from '@/types/utils.type';
 import { Deps } from '@/deps/deps';
 import { MessageProviderPort } from '@/domain/port/messageProvider/left/port';
 
+type TSVGImage = ComponentType<SVGProps<SVGSVGElement>>;
+
 export type TDeploymentModeDataForCard = {
   mode: TDeploymentMode;
   title: string;
   description: string;
-  url: string;
+  Image: TSVGImage;
+};
+
+const getImage = (mode: TDeploymentMode) => {
+  switch (mode) {
+    case 'region-3-az':
+      return Region3azImage;
+    case 'region':
+      return Region1azImage;
+    default:
+      return LZImage;
+  }
 };
 
 const mapDeploymentModeForCard = (
@@ -22,7 +38,7 @@ const mapDeploymentModeForCard = (
   description: getMessageFn(
     `common:pci_instances_common_instance_${mode}_deployment_mode_description`,
   ),
-  url: InstanceImage,
+  Image: (getImage(mode) as unknown) as TSVGImage,
 });
 
 type TSelectDeploymentModesForCard = (
