350c7c9a71726dffc1e84998c3bc808eba27e0c8
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-10-23 16:58:58 +0200

    fix(okms): level2 tracking id & minor config for download certificates links
    ref: #MANAGER-18832
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/okms/src/tracking.constant.ts
+++ packages/manager/apps/okms/src/tracking.constant.ts
@@ -1,17 +1,17 @@
 export const LEVEL2 = {
   EU: {
     config: {
-      level2: '88',
+      level2: '140',
     },
   },
   CA: {
     config: {
-      level2: '88',
+      level2: '140',
     },
   },
   US: {
     config: {
-      level2: '88',
+      level2: '140',
     },
   },
 };
