73d1e64c5b821b086f9159b0bb953390debb1fb3
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-10-23 17:56:38 +0200

    feat(backup-agent): add delete vault to module
    ref: #BKP-489
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

---
+++ packages/manager/modules/backup-agent/src/pages/vaults/listing/_components/__tests__/VaultActionCell.component.spec.tsx
@@ -0,0 +1,47 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
+
+import { ODS_BUTTON_VARIANT, ODS_ICON_NAME } from '@ovhcloud/ods-components';
+
+import { mockVaults } from '@/mocks/vaults/vaults';
+import { VaultResource } from '@/types/Vault.type';
+
+import { VaultActionCell } from '../VaultActionCell.component';
+
+vi.mock('react-router-dom', () => ({
+  useNavigate: () => vi.fn(),
+}));
+
+describe('VaultActionCell test suite', () => {
+  const vault = mockVaults[0]!;
+  const withVspc: VaultResource = {
+    ...vault,
+    currentState: { ...vault.currentState, vspc: ['vspc1'] },
+  };
+  const withoutVspc: VaultResource = {
+    ...vault,
+    currentState: { ...vault.currentState, vspc: [] },
+  };
+
+  const testCases: Array<{ desc: string; vault: VaultResource; isDisabled: boolean }> = [
+    {
+      desc: 'renders an enabled button if vault has no VSPC',
+      vault: withoutVspc,
+      isDisabled: false,
+    },
+    {
+      desc: 'renders a disabled button if vault has VSPC',
+      vault: withVspc,
+      isDisabled: true,
+    },
+  ];
+
+  it.each(testCases)('$desc', ({ vault, isDisabled }) => {
+    render(<VaultActionCell {...vault} />);
+
+    const button = screen.getByTestId('delete-vault-button');
+    expect(button).toHaveAttribute('is-disabled', `${isDisabled}`);
+    expect(button).toHaveAttribute('icon', ODS_ICON_NAME.trash);
+    expect(button).toHaveAttribute('variant', ODS_BUTTON_VARIANT.ghost);
+  });
+});
