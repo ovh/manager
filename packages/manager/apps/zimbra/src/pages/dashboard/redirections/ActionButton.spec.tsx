21005bd20cb9f9d9e3e31996d105a6ca04420db4
Atef ZAAFOURI <atefzed@gmail.com>
2025-10-23 10:30:01 +0200

    feat(zimbra): add redirections tab
    ref: #PRDCOL-113
    
    Signed-off-by: Atef ZAAFOURI <atefzed@gmail.com>

--- packages/manager/apps/zimbra/src/pages/dashboard/redirections/ActionButton.spec.tsx
+++ packages/manager/apps/zimbra/src/pages/dashboard/redirections/ActionButton.spec.tsx
@@ -5,7 +5,6 @@ import { describe, expect } from 'vitest';
 import actionsCommonTranslation from '@ovh-ux/manager-common-translations/dist/@ovh-ux/manager-common-translations/actions/Messages_fr_FR.json';
 
 import { ResourceStatus } from '@/data/api';
-import { FEATURE_FLAGS } from '@/utils';
 import { render } from '@/utils/test.provider';
 
 import ActionButtonRedirection from './ActionButton.component';
@@ -26,13 +25,7 @@ describe('Redirections datagrid action menu', () => {
 
     const menuItems = container.querySelectorAll('ods-popover ods-button');
 
-    if (FEATURE_FLAGS.REDIRECTIONS_EDIT) {
-      expect(menuItems.length).toBe(2);
-      expect(menuItems[0]).toHaveAttribute('label', actionsCommonTranslation.modify);
-      expect(menuItems[1]).toHaveAttribute('label', actionsCommonTranslation.delete);
-    } else {
-      expect(menuItems.length).toBe(1);
-      expect(menuItems[0]).toHaveAttribute('label', actionsCommonTranslation.delete);
-    }
+    expect(menuItems.length).toBe(1);
+    expect(menuItems[0]).toHaveAttribute('label', actionsCommonTranslation.delete);
   });
 });
