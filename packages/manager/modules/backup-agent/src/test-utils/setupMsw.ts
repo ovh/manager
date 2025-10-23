fa421c8663443293832c54e833b02287b6d1d933
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-10-23 18:13:23 +0200

    feat(backup-agent): add services pages
    ref: #BKP-598
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/backup-agent/src/test-utils/setupMsw.ts
+++ packages/manager/modules/backup-agent/src/test-utils/setupMsw.ts
@@ -3,9 +3,14 @@ import { SetupServer } from 'msw/node';
 import { getAuthenticationMocks, toMswHandlers } from '@ovh-ux/manager-core-test-utils';
 
 import { TLocationMockParams, getLocationMocks } from '@/mocks/location/locations.handler';
+import { TTenantMockParams, getTenantMocks } from '@/mocks/tenant/tenants.handler';
+import { TVSPCTenantMockParams, getVSPCTenantMocks } from '@/mocks/tenant/vspcTenants.handler';
 import { TVaultMockParams, getVaultMocks } from '@/mocks/vaults/vaults.handler';
 
-export type MockParams = TVaultMockParams & TLocationMockParams;
+export type MockParams = TVaultMockParams &
+  TLocationMockParams &
+  TTenantMockParams &
+  TVSPCTenantMockParams;
 
 export const setupMswMock = (mockParams: MockParams = {}) => {
   (global as unknown as { server: SetupServer }).server?.resetHandlers(
@@ -13,6 +18,8 @@ export const setupMswMock = (mockParams: MockParams = {}) => {
       ...getAuthenticationMocks({ isAuthMocked: true }),
       ...getVaultMocks(mockParams),
       ...getLocationMocks(mockParams),
+      ...getTenantMocks(mockParams),
+      ...getVSPCTenantMocks(mockParams),
     ]),
   );
 };
