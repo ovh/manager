291f33abf8203e9ded2cfe51436c6a530941419f
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-10-23 18:13:23 +0200

    feat(backup-agent): add services pages
    ref: #BKP-598
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

---
+++ packages/manager/modules/backup-agent/src/pages/services/listing/_hooks/useVspcTenantListingColumns.tsx
@@ -0,0 +1,63 @@
+import { useMemo } from 'react';
+
+import { useTranslation } from 'react-i18next';
+
+import { NAMESPACES } from '@ovh-ux/manager-common-translations';
+import { DatagridColumn } from '@ovh-ux/manager-react-components';
+
+import { VSPCTenant } from '@/types/VspcTenant.type';
+
+import {
+  TenantActionCell,
+  TenantReferenceCell,
+  VSPCTenantNameCell,
+} from '../_components';
+
+import {ResourceLocationCell} from "@/components/CommonCells/ResourceLocationCell/ResourceLocationCell.components";
+import {ResourceRegionCell} from "@/components/CommonCells/ResourceRegionCell/ResourceRegionCell.components";
+import {ResourceStatusCell} from "@/components/CommonCells/ResourceStatusCell/ResourceStatusCell.components";
+
+export function useVspcListingColumns(): DatagridColumn<VSPCTenant>[] {
+  const { t } = useTranslation([NAMESPACES.DASHBOARD, NAMESPACES.REGION, NAMESPACES.STATUS]);
+
+  return useMemo<DatagridColumn<VSPCTenant>[]>(() => {
+    return [
+      {
+        id: 'name',
+        label: t(`${NAMESPACES.DASHBOARD}:name`),
+        isSortable: false,
+        cell: VSPCTenantNameCell,
+      },
+      {
+        id: 'location',
+        label: t(`${NAMESPACES.REGION}:localisation`),
+        isSortable: false,
+        cell: ResourceLocationCell,
+      },
+      {
+        id: 'region',
+        label: t(`${NAMESPACES.REGION}:region`),
+        isSortable: false,
+        cell: ResourceRegionCell,
+      },
+      {
+        id: 'reference',
+        label: t(`${NAMESPACES.DASHBOARD}:reference`),
+        isSortable: false,
+        cell: TenantReferenceCell,
+      },
+      {
+        id: 'status',
+        label: t(`${NAMESPACES.STATUS}:status`),
+        isSortable: false,
+        cell: ResourceStatusCell,
+      },
+      {
+        id: 'action',
+        label: '',
+        isSortable: false,
+        cell: TenantActionCell,
+      },
+    ];
+  }, [t]);
+}
