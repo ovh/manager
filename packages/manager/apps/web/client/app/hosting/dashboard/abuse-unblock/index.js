647f55141ac77567945ec3cc131f8309dc378774
Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>
2025-10-23 16:28:02 +0200

    fix(web-hosting): display git removal modal
    ref: #PUWEBPT-93
    
    Signed-off-by: Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>

--- packages/manager/apps/web/client/app/hosting/dashboard/abuse-unblock/index.js
+++ packages/manager/apps/web/client/app/hosting/dashboard/abuse-unblock/index.js
@@ -2,7 +2,7 @@ import angular from 'angular';
 import '@uirouter/angularjs';
 import 'oclazyload';
 
-const moduleName = 'ovhManagerHostingMultisiteGitRemovalLazyLoading';
+const moduleName = 'ovhManagerHostingAbuseUnblockLazyLoading';
 
 angular
   .module(moduleName, ['ui.router', 'oc.lazyLoad'])
