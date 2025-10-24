9583b77595fe73e896452e21baed2ab0a3b85a20
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-10-24 11:36:49 +0200

    feat(muk): update datagrid attributes
    ref: #MANAGER-20066
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-wiki/stories/manager-ui-kit/components/Datagrid/datagrid.stories.tsx
+++ packages/manager-wiki/stories/manager-ui-kit/components/Datagrid/datagrid.stories.tsx
@@ -7,6 +7,8 @@ import {
   Icon,
   ICON_NAME,
   Input,
+  TABLE_SIZE,
+  TABLE_VARIANT,
 } from '@ovhcloud/ods-react';
 import { Datagrid, DatagridProps, useColumnFilters } from '@ovh-ux/muk';
 import {
@@ -121,6 +123,8 @@ const DatagridStory = (args: DatagridProps<DatagridStoryData>) => {
     subComponentHeight,
     maxRowHeight,
     isLoading,
+    size,
+    variant,
     totalCount,
     topbar,
   } = args;
@@ -196,6 +200,8 @@ const DatagridStory = (args: DatagridProps<DatagridStoryData>) => {
       <Datagrid
         columns={colsArgs}
         data={applyFilters(itemsArgs, filters)}
+        {...('size' in args && { size })}
+        {...('variant' in args && { variant })}
         {...('containerHeight' in args && {
           containerHeight: containerHeightStyle,
         })}
@@ -275,6 +281,29 @@ Default.args = {
   data,
 };
 
+export const Size = DatagridStory.bind({});
+
+Size.args = {
+  columns,
+  data,
+  size: TABLE_SIZE.sm,
+  hasNextPage: true,
+  onFetchNextPage: () => {},
+  onFetchAllPages: () => {},
+};
+
+
+
+export const Variant = DatagridStory.bind({});
+
+Variant.args = {
+  columns,
+  data,
+  variant: TABLE_VARIANT.striped,
+};
+
+
+
 export const Sorting = DatagridStory.bind({});
 
 Sorting.args = {
@@ -339,6 +368,7 @@ SubComponent.args = {
     </>
   ),
   subComponentHeight: 80,
+  size: TABLE_SIZE.md,
 };
 
 export const Expandable = DatagridStory.bind({});
@@ -472,6 +502,16 @@ const meta = {
   },
   args: {},
   argTypes: {
+    size: {
+      description: 'Controls the table row size',
+      control: 'select',
+      options: [TABLE_SIZE.sm, TABLE_SIZE.md, TABLE_SIZE.lg],
+    },
+    variant: {
+      description: 'Controls the table variant style',
+      control: 'select',
+      options: [TABLE_VARIANT.default, TABLE_VARIANT.striped],
+    },
     hasNextPage: {
       description: 'Controls whether pagination buttons are shown',
       control: 'boolean',
