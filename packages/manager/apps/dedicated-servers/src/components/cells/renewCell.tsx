55e7489949a6bde3fe1d98f91ef47511ca18420e
b-fritz-ovh <benjamin.fritz.ext@corp.ovh.com>
2025-10-24 10:03:48 +0200

    feat(dedicated-server): add csv export
    ref: #MANAGER-20093
    
    Signed-off-by: b-fritz-ovh <benjamin.fritz.ext@corp.ovh.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/apps/dedicated-servers/src/components/cells/renewCell.tsx
+++ packages/manager/apps/dedicated-servers/src/components/cells/renewCell.tsx
@@ -4,20 +4,14 @@ import { useTranslation } from 'react-i18next';
 import { DedicatedServer } from '@/data/types/server.type';
 import { DSBilling } from '../billingCell';
 
+import { getRenewWording } from '../commonCellsWording';
+
 export const RenewCell = (server: DedicatedServer) => {
   const { t } = useTranslation('dedicated-servers');
   return (
     <DSBilling server={server.name}>
       {(billingInfo) => (
-        <DataGridTextCell>
-          {billingInfo?.billing?.renew?.current?.mode ? (
-            t(
-              `server_display_renew-${billingInfo?.billing?.renew?.current?.mode}`,
-            )
-          ) : (
-            <>-</>
-          )}
-        </DataGridTextCell>
+        <DataGridTextCell>{t(getRenewWording(billingInfo))}</DataGridTextCell>
       )}
     </DSBilling>
   );
