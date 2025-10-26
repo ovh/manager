7bbc88157b6527f0c5e29db3c6943fad95a568e9
yjaaouane <yassine.jaaouane.ext@corp.ovh.com>
2025-10-26 02:59:04 +0100

    feat(observability): add tenants listing page
    ref: #MAOBS-103
    
    Signed-off-by: yjaaouane <yassine.jaaouane.ext@corp.ovh.com>

--- packages/manager/apps/observability/src/pages/tenants/TenantsOnboarding.page.tsx
+++ packages/manager/apps/observability/src/pages/tenants/TenantsOnboarding.page.tsx
@@ -5,33 +5,42 @@ import { useTranslation } from 'react-i18next';
 import { OdsText } from '@ovhcloud/ods-components/react';
 
 import { NAMESPACES } from '@ovh-ux/manager-common-translations';
-import { OnboardingLayout } from '@ovh-ux/manager-react-components';
+import { OnboardingLayout, RedirectionGuard } from '@ovh-ux/manager-react-components';
 
+import { useObservabilityServiceContext } from '@/contexts/ObservabilityService.context';
+import { urls } from '@/routes/Routes.constants';
+import { IAM_ACTIONS } from '@/utils/iam.constants';
 import { LABELS } from '@/utils/labels.constants';
 import { ONBOARDING_IMG_SRC } from '@/utils/onboarding.constants';
 
 const TenantsOnboarding: React.FC = () => {
   const { t } = useTranslation(['tenants', NAMESPACES.ONBOARDING]);
-
+  const { selectedService, isLoading, isSuccess } = useObservabilityServiceContext();
   return (
-    <OnboardingLayout
-      title={LABELS.TENANT}
-      description={
-        <OdsText preset="paragraph" className="text-center">
-          {t('tenants:onboarding.description')}
-        </OdsText>
-      }
-      img={ONBOARDING_IMG_SRC}
-      orderIam={{
-        urn: 'urn:v1:eu:resource:xxxx', // TODO: replace with the correct urn once API is ready
-        iamActions: ['ldp:apiovh:tenant/create'], // TODO: check iam actions to add
-        displayTooltip: true,
-      }}
-      orderButtonLabel={t('tenants:onboarding.orderButtonLabel')}
-      orderHref="https://www.ovh.com" // TODO
-      moreInfoButtonLabel={t(`${NAMESPACES.ONBOARDING}:find_out_more`)}
-      moreInfoHref={'https://www.ovh.com'} // TODO
-    />
+    <RedirectionGuard
+      condition={!selectedService && isSuccess}
+      isLoading={isLoading}
+      route={urls.tenants}
+    >
+      <OnboardingLayout
+        title={LABELS.TENANT}
+        description={
+          <OdsText preset="paragraph" className="text-center">
+            {t('tenants:onboarding.description')}
+          </OdsText>
+        }
+        img={ONBOARDING_IMG_SRC}
+        orderIam={{
+          urn: selectedService?.iam?.urn ?? '',
+          iamActions: IAM_ACTIONS.CREATE_TENANT,
+          displayTooltip: true,
+        }}
+        orderButtonLabel={t('tenants:onboarding.orderButtonLabel')}
+        orderHref="https://www.ovh.com" // TODO
+        moreInfoButtonLabel={t(`${NAMESPACES.ONBOARDING}:find_out_more`)}
+        moreInfoHref={'https://www.ovh.com'} // TODO
+      />
+    </RedirectionGuard>
   );
 };
 
