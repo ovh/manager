fceea1260843d870da53d52d4746001b52bed6f7
yjaaouane <yassine.jaaouane.ext@corp.ovh.com>
2025-10-26 02:59:04 +0100

    feat(observability): add tenants listing page
    ref: #MAOBS-103
    
    Signed-off-by: yjaaouane <yassine.jaaouane.ext@corp.ovh.com>

--- packages/manager/apps/observability/src/pages/tenants/TenantsListing.page.tsx
+++ packages/manager/apps/observability/src/pages/tenants/TenantsListing.page.tsx
@@ -1,25 +1,23 @@
-import { useTranslation } from 'react-i18next';
-
-import { OdsText } from '@ovhcloud/ods-components/react';
-
+import TenantsListDatagrid from '@/components/listing/tenants/TenantsListDatagrid.component';
 import { useObservabilityServiceContext } from '@/contexts/ObservabilityService.context';
 import { useTenants } from '@/data/hooks/tenants/useTenants.hook';
-import { Tenant } from '@/types/observability.type';
 
 export default function TenantsListingPage() {
-  const { t } = useTranslation(['common', 'tenants']);
   const { selectedService } = useObservabilityServiceContext();
-  const { data: tenants, isSuccess } = useTenants(selectedService || '');
+  const {
+    data: tenants,
+    isSuccess,
+    isError,
+    error,
+    isLoading,
+  } = useTenants(selectedService?.id || '');
 
-  // TODO : implement the listing page
   return tenants && isSuccess ? (
-    <>
-      <OdsText preset="heading-2">{t('tenants:listing.title')}</OdsText>
-      <ul>
-        {tenants.map((tenant: Tenant) => (
-          <li key={tenant.id}>{tenant.currentState.title}</li>
-        ))}
-      </ul>
-    </>
+    <TenantsListDatagrid
+      tenantsList={tenants}
+      isLoading={isLoading}
+      isError={isError}
+      error={error}
+    />
   ) : null;
 }
