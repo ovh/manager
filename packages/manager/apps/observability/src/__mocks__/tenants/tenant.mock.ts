eae595fa4fcdc370b65e91b7497983b88535f299
yjaaouane <yassine.jaaouane.ext@corp.ovh.com>
2025-10-26 02:59:04 +0100

    feat(observability): add tenants listing page
    ref: #MAOBS-103
    
    Signed-off-by: yjaaouane <yassine.jaaouane.ext@corp.ovh.com>

--- packages/manager/apps/observability/src/__mocks__/tenants/tenant.mock.ts
+++ packages/manager/apps/observability/src/__mocks__/tenants/tenant.mock.ts
@@ -1,12 +1,12 @@
-import { ObservabilityServiceParams } from '@/types/ClientApi.type';
-import { Tenant } from '@/types/observability.type';
+import { ObservabilityServiceParams } from '@/data/api/observability.props';
+import { Tenant } from '@/types/tenants.type';
 
 export const getTenants = async ({
-  serviceName,
+  resourceName,
 }: ObservabilityServiceParams): Promise<Tenant[]> => {
-  const isOnboarding = serviceName !== 'ldp-rg-93836'; // '[DO NOT TOUCH] Monito service'
+  const isOnboarding = resourceName !== 'ldp-rg-93836'; // '[DO NOT TOUCH] Monito service'
   console.info(
-    `[MOCK-ADAPTER][getTenants] is onboarding mock for ${serviceName} -> `,
+    `[MOCK-ADAPTER][getTenants] is onboarding mock for ${resourceName} -> `,
     isOnboarding,
   );
   return Promise.resolve(
@@ -16,12 +16,56 @@ export const getTenants = async ({
             id: '1',
             currentState: {
               title: 'Tenant 1',
+              description: 'Tenant 1 description',
+              limits: {
+                numberOfSeries: {
+                  current: 222,
+                  maximum: 300,
+                },
+                retention: {
+                  id: 'retention-1',
+                  duration: '36 mois',
+                  link: 'retention_link_1',
+                },
+              },
+              infrastructure: {
+                id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+                currentState: {
+                  location: 'eu-west-sbg',
+                  entryPoint: 'sbg1.metrics.ovh.com',
+                  type: 'SHARED',
+                  usage: 'METRICS',
+                },
+              },
+              tags: ['tag name1', 'tag name2', 'tag name3'],
             },
           },
           {
             id: '2',
             currentState: {
               title: 'Tenant 2',
+              description: 'Tenant 2 description',
+              limits: {
+                numberOfSeries: {
+                  current: 36,
+                  maximum: 50,
+                },
+                retention: {
+                  id: 'retention-2',
+                  duration: '12 mois',
+                  link: 'retention_link_2',
+                },
+              },
+              infrastructure: {
+                id: '6ee8fb35-2621-4530-a288-84fc0e85dac1',
+                currentState: {
+                  entryPoint: 'gra1.metrics.ovh.com',
+                  location: 'eu-west-gra',
+                  type: 'SHARED',
+                  usage: 'METRICS',
+                },
+              },
+              tags: ['tag name3'],
             },
           },
         ]
