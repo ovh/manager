b899ae7e15e834a9d8649fc31a09be4ffd73aaf2
Adrien Turmo <adrien.turmo.ext@ovhcloud.com>
2025-10-27 12:15:52 +0100

    Revert "fix(instances-backup): distant-backup - react - rework modal and add new custom form fields"
    This reverts commit defe6fe75c212e3d1fc913d53018ba942643a703.

--- packages/manager/apps/pci-instances/src/components/form/ErrorText.tsx
+++
@@ -1,6 +0,0 @@
-import { PropsWithChildren } from 'react';
-import { FormFieldError } from '@ovhcloud/ods-react';
-
-export const ErrorText = ({ children }: PropsWithChildren) => {
-  return <FormFieldError>{children}</FormFieldError>;
-};
