ac6c3cc7d6d7f545b7385ad5e549b394780dbc17
David Arsène <david.arsene.ext@ovhcloud.com>
2025-10-28 11:10:04 +0100

    feat(observability): add region tile selector in tenant creation form
    resolves: #MAOBS-120
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/observability/src/utils/form.utils.ts
+++ packages/manager/apps/observability/src/utils/form.utils.ts
@@ -1,3 +1,30 @@
+/**
+ * Add a required asterisk to a label.
+ * @param label - The label to add a required asterisk to.
+ * @returns The label with a required asterisk.
+ * @example
+ * ```ts
+ * const label = 'Title';
+ * const requiredLabel = toRequiredLabel(label);
+ * console.log(requiredLabel);
+ * // Title*
+ */
 export const toRequiredLabel = (label: string) => {
   return `${label}*`;
+};
+
+/**
+ * Wrap a label in parentheses.
+ * @param label - The label to wrap in parentheses.
+ * @returns The label wrapped in parentheses.
+ * @example
+ * ```ts
+ * const label = 'Paris';
+ * const parenthesesLabel = toParenthesesLabel(label);
+ * console.log(parenthesesLabel);
+ * // (Paris)
+ * ```
+ */
+export const toParenthesesLabel = (label: string) => {
+  return `(${label})`;
 };
