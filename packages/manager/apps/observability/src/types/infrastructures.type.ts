a43eadd774f618f2f10d37d55c67f598bf51f27f
David Arsène <david.arsene.ext@ovhcloud.com>
2025-10-28 11:10:04 +0100

    feat(observability): add region tile selector in tenant creation form
    resolves: #MAOBS-120
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/observability/src/types/infrastructures.type.ts
+++ packages/manager/apps/observability/src/types/infrastructures.type.ts
@@ -4,6 +4,7 @@ import { TIdentifier } from './observability.type';
 
 export type InfrastructureUsage = 'GRAFANA' | 'LOGS' | 'METRICS';
 export type InfrastructureType = 'DEDICATED' | 'SHARED';
+export type RegionType = 'LOCAL-ZONE' | 'REGION-1-AZ' | 'REGION-3-AZ';
 
 export type Retention = {
   duration: string;
@@ -21,3 +22,11 @@ export type Infrastructure = {
   };
   locationDetails?: Location;
 } & TIdentifier;
+
+export type InfrastructureWithLocation = Infrastructure & {
+  locationDetails: NonNullable<Infrastructure['locationDetails']>;
+};
+
+export type GroupedInfrastructures = Record<string, InfrastructureWithLocation[]> & {
+  all: InfrastructureWithLocation[];
+};
