4c92461f7dba610be66b2bcdfc83ef29ffedb932
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2025-10-28 14:52:26 +0100

    fix(web-office): fix password issue and add password generator
    ref: #PRDCOL-100
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <cds@interne.ovh.net>

--- packages/manager/apps/web-office/src/utils/form.ts
+++ packages/manager/apps/web-office/src/utils/form.ts
@@ -1,1 +1,7 @@
-export const PASSWORD_REGEX = /(?=.*[a-z])(?=.*[A-Z])(?=.*[~!@#$%^&*_\-+=|\\(){}[\]:;'<>,.?\d])/;
+export const PASSWORD_REGEX =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*([~!$%^&*\-+_=|():;"'<>,.?]|\d))[a-zA-Z\d~!$%^&*\-+_=|():;"'<>,.?]+$/;
+
+export const containsLowercase = (s: string) => !!(s && /[a-z]/.test(s));
+export const containsUppercase = (s: string) => !!(s && /[A-Z]/.test(s));
+export const containsDigit = (s: string) => !!(s && /\d/.test(s));
+export const containsSpecial = (s: string) => !!(s && /[~!$%^&*-+_=|():;"'<>,.?]/.test(s));
