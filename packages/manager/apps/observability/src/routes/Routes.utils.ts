7d695d95c2fdc9e0ff69da1868b24ec41597cb8d
yjaaouane <yassine.jaaouane.ext@corp.ovh.com>
2025-10-29 08:49:53 +0100

    feat(observability): add delete tenant action
    ref: #MAOBS-105
    
    Signed-off-by: yjaaouane <yassine.jaaouane.ext@corp.ovh.com>

--- packages/manager/apps/observability/src/routes/Routes.utils.ts
+++ packages/manager/apps/observability/src/routes/Routes.utils.ts
@@ -1,6 +1,12 @@
 import { APP_FEATURES } from '@/App.constants';
 
+import { urls } from './Routes.constants';
+
 const { basePrefix } = APP_FEATURES;
 export function getRoot(): string {
   return basePrefix ? `/${String(basePrefix)}` : '';
+}
+
+export function getDeleteTenantUrl(tenantId: string) {
+  return urls.deleteTenant.replace(':tenantId', tenantId);
 }
