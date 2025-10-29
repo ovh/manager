749e1c52fcdc82713d16c03ee96b2cc001308cd2
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-10-29 12:14:42 +0100

    fix(ips): fix game firewall guide
    ref: #MANAGER-18594
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager/apps/ips/src/pages/configureEdgeNetworkFirewall/configureEdgeNetworkFirewall.spec.tsx
+++ packages/manager/apps/ips/src/pages/configureEdgeNetworkFirewall/configureEdgeNetworkFirewall.spec.tsx
@@ -1,20 +1,16 @@
-import { waitFor, screen, fireEvent } from '@testing-library/react';
+import { waitFor, fireEvent } from '@testing-library/react';
 import { describe } from 'vitest';
-import { ODS_ICON_NAME } from '@ovhcloud/ods-components';
 import {
-  WAIT_FOR_DEFAULT_OPTIONS,
   assertOdsModalText,
+  assertOdsModalVisibility,
 } from '@ovh-ux/manager-core-test-utils';
 import {
-  getButtonByIcon,
   getButtonByLabel,
-  getTableCellByContentText,
   getToggleByName,
   labels,
   renderTest,
 } from '@/test-utils';
 import { urlDynamicParts, urls } from '@/routes/routes.constant';
-// import { IpGameFirewallStateEnum } from '@/data/api';
 import { fromIpToId } from '@/utils';
 import { IpEdgeFirewallStateEnum } from '@/data/api';
 
@@ -36,7 +32,7 @@ describe('Configure edge network firewall page', () => {
     expect(toggleElement).toHaveAttribute('is-disabled', 'true');
   });
 
-  it('display a success message when firewall state is updated successfully', async () => {
+  it('update firewall state successfully', async () => {
     const { container } = await renderTest({
       initialRoute: urls.configureEdgeNetworkFirewall.replace(
         urlDynamicParts.id,
@@ -62,50 +58,6 @@ describe('Configure edge network firewall page', () => {
       label: 'validate',
     });
     await waitFor(() => fireEvent.click(confirmButton));
-    // await waitFor(
-    //   () =>
-    //     expect(
-    //       screen.getByText(
-    //         labels.edgeNetworkFirewall
-    //       ),
-    //     ).toBeVisible(),
-    //   WAIT_FOR_DEFAULT_OPTIONS,
-    // );
-  });
-
-  it('display an error message when deny strategy is updated and api is KO', async () => {
-    // const { container } = await renderTest({
-    //   initialRoute: urls.configureGameFirewall.replace(
-    //     urlDynamicParts.id,
-    //     fromIpToId('239.99.244.14'),
-    //   ),
-    //   nbIp: 6,
-    //   gameFirewallConfig: {
-    //     isUpdateKo: true,
-    //     state: IpGameFirewallStateEnum.OK,
-    //     firewallModeEnabled: true,
-    //   },
-    // });
-    // const toggleElement = await getToggleByName({
-    //   container,
-    //   name: 'strategy-default-deny',
-    // });
-    // await waitFor(() => fireEvent.click(toggleElement));
-    // await assertOdsModalText({
-    //   container,
-    //   text: labels.gameFirewall.disable_deny_strategy_modal_title,
-    // });
-    // const confirmButton = await getButtonByLabel({
-    //   container,
-    //   label: 'validate',
-    // });
-    // await waitFor(() => fireEvent.click(confirmButton));
-    // await waitFor(
-    //   () =>
-    //     expect(
-    //       screen.getByText('game firewall update KO', { exact: false }),
-    //     ).toBeVisible(),
-    //   WAIT_FOR_DEFAULT_OPTIONS,
-    // );
+    await assertOdsModalVisibility({ container, isVisible: false });
   });
 });
