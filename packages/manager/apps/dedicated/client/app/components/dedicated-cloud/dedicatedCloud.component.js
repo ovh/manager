1072b2b88501cc23cdf6363b3b0beb5fe636d9bd
ahmed sefiani <ahmed.sefiani.ext@corp.ovh.com>
2025-10-29 15:42:35 +0100

    feat(dedicated): add migration vcfaas banner
    ref: #MANAGER-19946
    
    Signed-off-by: ahmed sefiani <ahmed.sefiani.ext@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/components/dedicated-cloud/dedicatedCloud.component.js
+++ packages/manager/apps/dedicated/client/app/components/dedicated-cloud/dedicatedCloud.component.js
@@ -19,6 +19,7 @@ export default {
     vcdTrackingPrefix: '<',
     customerSurveyBannerAvailability: '<',
     goBackToList: '<',
+    customerVcfaasMigrationBannerAvailability: '<',
     isLogsDisabled: '<?',
   },
   controller,
