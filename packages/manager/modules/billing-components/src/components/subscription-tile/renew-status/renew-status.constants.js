caad1acbd26b05c633e2b6dba3be2aea7ce34c59
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2025-10-29 15:11:31 +0100

    fix(billing-components): put back service status on subscription tile
    ref: #MANAGER-20155
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

---
+++ packages/manager/modules/billing-components/src/components/subscription-tile/renew-status/renew-status.constants.js
@@ -0,0 +1,10 @@
+export const RENEW_STATES_BADGES_CLASSES_MAP = {
+  error: ['expired', 'delete_at_expiration'],
+  success: ['auto', 'automatic'],
+  warning: ['manual', 'manualPayment'],
+  info: ['billing_suspended', 'forced_manual'],
+};
+
+export default {
+  RENEW_STATES_BADGES_CLASSES_MAP,
+};
