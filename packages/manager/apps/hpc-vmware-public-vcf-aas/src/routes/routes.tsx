6d2de60ef082c0c8408516bebb1352b1306b45de
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-10-29 18:13:12 +0100

    feat(public-vcf-aas): add edge gateway tab
    ref: #MANAGER-18386
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/hpc-vmware-public-vcf-aas/src/routes/routes.tsx
+++ packages/manager/apps/hpc-vmware-public-vcf-aas/src/routes/routes.tsx
@@ -136,6 +136,12 @@ const DeleteVrackSegmentPage = React.lazy(() =>
   ),
 );
 
+const EdgeGatewayListingPage = React.lazy(() =>
+  import(
+    '@/pages/dashboard/datacentre/edge-gateway/DatacentreEdgeGateway.page'
+  ),
+);
+
 const TerminateOrganizationPage = React.lazy(() =>
   import('@/pages/terminate/TerminateOrganization.page'),
 );
@@ -455,6 +461,11 @@ export default (
               pageType: PageType.popup,
             },
           }}
+        />
+        <Route
+          id={'edge-gateway'}
+          path={urls.edgeGateway}
+          Component={EdgeGatewayListingPage}
         />
       </Route>
       <Route
--- packages/manager/apps/hpc-vmware-public-vcf-aas/src/test-utils/test-i18n.ts
+++ packages/manager/apps/hpc-vmware-public-vcf-aas/src/test-utils/test-i18n.ts
@@ -1,3 +1,5 @@
+import { NAMESPACES } from '@ovh-ux/manager-common-translations';
+import commonDashboard from '@ovh-ux/manager-common-translations/dist/@ovh-ux/manager-common-translations/dashboard/Messages_fr_FR.json';
 import listing from '../../public/translations/listing/Messages_fr_FR.json';
 import dashboard from '../../public/translations/dashboard/Messages_fr_FR.json';
 import onboarding from '../../public/translations/onboarding/Messages_fr_FR.json';
@@ -6,6 +8,7 @@ import datacentresCompute from '../../public/translations/datacentres/compute/Me
 import datacentresOrder from '../../public/translations/datacentres/order/Messages_fr_FR.json';
 import datacentresStorage from '../../public/translations/datacentres/storage/Messages_fr_FR.json';
 import datacentresVrackSegment from '../../public/translations/datacentres/vrack-segment/Messages_fr_FR.json';
+import datacentresEdgeGateway from '../../public/translations/datacentres/edge-gateway/Messages_fr_FR.json';
 import terminate from '../../public/translations/terminate/Messages_fr_FR.json';
 import networkAcl from '../../public/translations/networkAcl/Messages_fr_FR.json';
 import migration from '../../public/translations/migration/Messages_fr_FR.json';
@@ -65,6 +68,7 @@ export const translations = {
   'datacentres/order': datacentresOrder,
   'datacentres/storage': datacentresStorage,
   'datacentres/vrack-segment': datacentresVrackSegment,
+  'datacentres/edge-gateway': datacentresEdgeGateway,
   error,
   terminate,
   networkAcl,
@@ -78,6 +82,7 @@ export const translations = {
   '@ovh-ux/manager-common-translations/system': commun.system,
   '@ovh-ux/manager-common-translations/error': commun.error,
   '@ovh-ux/manager-common-translations/region': commun.region,
+  [NAMESPACES.DASHBOARD]: commonDashboard,
 };
 
 export const labels = {
@@ -90,6 +95,7 @@ export const labels = {
   datacentresOrder,
   datacentresStorage,
   datacentresVrackSegment,
+  datacentresEdgeGateway,
   terminate,
   networkAcl,
   migration,
@@ -97,4 +103,5 @@ export const labels = {
   commun,
   zodError,
   vcda,
+  commonDashboard,
 };
