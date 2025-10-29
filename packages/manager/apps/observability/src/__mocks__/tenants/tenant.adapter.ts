53294aa2c6ad74a911e5a2e1620e19b13c8b8b07
yjaaouane <yassine.jaaouane.ext@corp.ovh.com>
2025-10-29 08:49:53 +0100

    feat(observability): add delete tenant action
    ref: #MAOBS-105
    
    Signed-off-by: yjaaouane <yassine.jaaouane.ext@corp.ovh.com>

--- packages/manager/apps/observability/src/__mocks__/tenants/tenant.adapter.ts
+++ packages/manager/apps/observability/src/__mocks__/tenants/tenant.adapter.ts
@@ -1,11 +1,32 @@
 import { apiConfig } from '@/__mocks__/mock.config';
-import { getTenants as getTenantsFromMock } from '@/__mocks__/tenants/tenant.mock';
+import {
+  deleteTenant as deleteTenantFromMock,
+  getTenant as getTenantFromMock,
+  getTenants as getTenantsFromMock,
+} from '@/__mocks__/tenants/tenant.mock';
 import { ObservabilityServiceParams } from '@/data/api/observability.props';
-import { getTenants as getTenantsFromApi } from '@/data/api/tenants.api';
+import {
+  deleteTenant as deleteTenantFromApi,
+  getTenant as getTenantFromApi,
+  getTenants as getTenantsFromApi,
+} from '@/data/api/tenants.api';
+import { GetTenantPayload } from '@/data/api/tenants.props';
 import { Tenant } from '@/types/tenants.type';
 
 export const getTenants = async (params: ObservabilityServiceParams): Promise<Tenant[]> => {
   const isMockEnabled = apiConfig.mode === 'mock';
   console.info('[MOCK-ADAPTER][getTenants] Mock enabled -> ', isMockEnabled);
   return isMockEnabled ? getTenantsFromMock(params) : getTenantsFromApi(params);
+};
+
+export const getTenant = async (params: GetTenantPayload): Promise<Tenant> => {
+  const isMockEnabled = apiConfig.mode === 'mock';
+  console.info('[MOCK-ADAPTER][getTenant] Mock enabled -> ', isMockEnabled);
+  return isMockEnabled ? getTenantFromMock(params) : getTenantFromApi(params);
+};
+
+export const deleteTenant = async (params: GetTenantPayload): Promise<Tenant> => {
+  const isMockEnabled = apiConfig.mode === 'mock';
+  console.info('[MOCK-ADAPTER][deleteTenant] Mock enabled -> ', isMockEnabled);
+  return isMockEnabled ? deleteTenantFromMock(params) : deleteTenantFromApi(params);
 };
--- packages/manager/apps/observability/src/routes/Routes.constants.ts
+++ packages/manager/apps/observability/src/routes/Routes.constants.ts
@@ -2,11 +2,16 @@ import { appName } from '@/App.constants';
 
 import { getRoot } from './Routes.utils';
 
+export const URL_PARAMS = {
+  tenantId: ':tenantId',
+};
+
 export const subroutes = {
   tenants: 'tenants' as const,
   metrics: 'metrics' as const,
   onboarding: 'onboarding' as const,
   dashboards: 'dashboards' as const,
+  delete: `delete/${URL_PARAMS.tenantId}` as const,
 } as const;
 
 export const urls = {
@@ -17,7 +22,12 @@ export const urls = {
   tenantsOnboarding: `${getRoot()}/${subroutes.metrics}/${subroutes.tenants}/${
     subroutes.onboarding
   }`,
+  deleteTenant: `${getRoot()}/${subroutes.metrics}/${subroutes.tenants}/${subroutes.delete}`,
   onboarding: `${getRoot()}/${subroutes.metrics}/${subroutes.onboarding}`,
 } as const;
+
+export type LocationPathParams = {
+  [K in keyof typeof URL_PARAMS]: string;
+};
 
 export const redirectionApp = appName;
