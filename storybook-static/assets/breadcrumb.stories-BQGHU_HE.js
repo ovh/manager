0b44b2daba92cd6775e7cffa2a0ed34d166f883f
ovh-cds <ovh-cds@users.noreply.github.com>
2025-10-29 09:12:23 +0000

    docs: update documentation [skip ci]

---
+++ storybook-static/assets/breadcrumb.stories-BQGHU_HE.js
@@ -0,0 +1,13 @@
+import{j as b}from"./jsx-runtime-BRNY0I4F.js";import{K as n,$ as l}from"./index-ChJ3dufK.js";import{b as i}from"./lib-CWaID5dp-BJDNWG0v.js";import"./index-Bnop-kX6.js";import"./index-C2BvAoka.js";import"./index-4pTrEEYx.js";import"./iframe-CUYazWvm.js";const d=a=>b.jsx(i,{rootLabel:a.rootLabel,appName:a.appName,hideRootLabel:a.hideRootLabel}),e=d.bind({});e.args={appName:"vrack-services",rootLabel:"vRack Services",hideRootLabel:!1};e.parameters={docs:{source:{code:`<Breadcrumb
+  rootLabel="vRack Services"
+  appName="vrack-services"
+  hideRootLabel={false}
+/>`}}};const r=d.bind({});r.args={rootLabel:"vRack Services",appName:"vrack-services",hideRootLabel:!0};r.parameters={docs:{source:{code:`<Breadcrumb
+  rootLabel="vRack Services"
+  appName="vrack-services"
+  hideRootLabel={true}
+/>`}}};const N={title:"Manager UI Kit/Components/Breadcrumb",component:i,tags:["autodocs"],decorators:[n],parameters:{docs:{description:{component:"Breadcrumb component"}},reactRouter:l({location:{pathParams:{userId:"vrs-yyy-xxx-yyy-xxx"}},routing:{path:"/:userId/subnets/listing"}})}};var o,s,t;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`args => {
+  return <Breadcrumb rootLabel={args.rootLabel} appName={args.appName} hideRootLabel={args.hideRootLabel} />;
+}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var c,p,m;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`args => {
+  return <Breadcrumb rootLabel={args.rootLabel} appName={args.appName} hideRootLabel={args.hideRootLabel} />;
+}`,...(m=(p=r.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};const B=["Basic","HideRootLabel"];export{e as Basic,r as HideRootLabel,B as __namedExportsOrder,N as default};
