fe34ddb36ed239d85ebaca5a8914d9837acf39e6
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2025-10-29 12:23:39 +0100

    feat(pci-instances): add image section with helper drawer
    ref: #TAPC-4614, #TAPC-4652
    
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/apps/pci-instances/src/hooks/url/useGuideLink.constant.ts
+++ packages/manager/apps/pci-instances/src/hooks/url/useGuideLink.constant.ts
@@ -2,29 +2,40 @@ import { Subsidiary } from '@ovh-ux/manager-config';
 
 const HELP_ROOT = 'https://www.ovhcloud.com';
 
+const HELP_URL = 'https://help.ovhcloud.com/csm';
+
 type TGuideLinkGroup = {
   DEFAULT: string;
 } & {
   [key in Subsidiary]?: string;
 };
 
-type TGuideKey = 'LOCATION' | 'FLAVOR' | 'AVAILABILITY_ZONES';
+export type TGuideKey =
+  | 'LOCATION'
+  | 'FLAVOR'
+  | 'AVAILABILITY_ZONES'
+  | 'DISTRIBUTION_IMAGE'
+  | 'DISTRIBUTION_IMAGE_LIFE_CYCLE';
 
 export const GUIDE_LINKS: Record<TGuideKey, TGuideLinkGroup> = {
   LOCATION: {
-    DEFAULT:
-      'https://help.ovhcloud.com/csm/fr-public-cloud-deployments-modes?id=kb_article_view&sysparm_article=KB0066031',
-    FR:
-      'https://help.ovhcloud.com/csm/fr-public-cloud-deployments-modes?id=kb_article_view&sysparm_article=KB0066031',
+    DEFAULT: `${HELP_URL}/fr-public-cloud-deployments-modes?id=kb_article_view&sysparm_article=KB0066031`,
+    FR: `${HELP_URL}/fr-public-cloud-deployments-modes?id=kb_article_view&sysparm_article=KB0066031`,
   },
   AVAILABILITY_ZONES: {
-    DEFAULT:
-      ' https://help.ovhcloud.com/csm/fr-public-cloud-deployments-modes?id=kb_article_view&sysparm_article=KB0066031',
-    FR:
-      ' https://help.ovhcloud.com/csm/fr-public-cloud-deployments-modes?id=kb_article_view&sysparm_article=KB0066031',
+    DEFAULT: `${HELP_URL}/fr-public-cloud-deployments-modes?id=kb_article_view&sysparm_article=KB0066031`,
+    FR: `${HELP_URL}/fr-public-cloud-deployments-modes?id=kb_article_view&sysparm_article=KB0066031`,
   },
   FLAVOR: {
     DEFAULT: `${HELP_ROOT}/fr/public-cloud/virtual-instances/`,
     FR: `${HELP_ROOT}/fr/public-cloud/virtual-instances/`,
+  },
+  DISTRIBUTION_IMAGE: {
+    DEFAULT: `${HELP_URL}/fr-public-cloud-compute-getting-started?id=kb_article_view&sysparm_article=KB0051011`,
+    FR: `${HELP_URL}/fr-public-cloud-compute-getting-started?id=kb_article_view&sysparm_article=KB0051011`,
+  },
+  DISTRIBUTION_IMAGE_LIFE_CYCLE: {
+    DEFAULT: `${HELP_URL}/fr-public-cloud-compute-vps-image-life-cycle?id=kb_article_view&sysparm_article=KB0050802`,
+    FR: `${HELP_URL}/fr-public-cloud-compute-vps-image-life-cycle?id=kb_article_view&sysparm_article=KB0050802`,
   },
 };
--- packages/manager/apps/pci-instances/src/hooks/url/useGuideLink.ts
+++ packages/manager/apps/pci-instances/src/hooks/url/useGuideLink.ts
@@ -1,10 +1,23 @@
 import { ShellContext } from '@ovh-ux/manager-react-shell-client';
 import { useContext } from 'react';
-import { GUIDE_LINKS } from './useGuideLink.constant';
+import { GUIDE_LINKS, TGuideKey } from './useGuideLink.constant';
+import { Subsidiary } from '@ovh-ux/manager-config';
 
-export const useGuideLink = (name: keyof typeof GUIDE_LINKS) => {
+const getGuideLink = (key: TGuideKey, ovhSubsidiary: Subsidiary) =>
+  GUIDE_LINKS[key][ovhSubsidiary] ?? GUIDE_LINKS[key].DEFAULT;
+
+export function useGuideLink(key: TGuideKey): string;
+export function useGuideLink(keys: TGuideKey[]): Record<TGuideKey, string>;
+export function useGuideLink(keys: TGuideKey | TGuideKey[]) {
   const { ovhSubsidiary } = useContext(ShellContext).environment.getUser();
-  const guideLink = GUIDE_LINKS[name];
 
-  return guideLink[ovhSubsidiary] ?? guideLink.DEFAULT;
-};
+  return Array.isArray(keys)
+    ? keys.reduce(
+        (prev, currentKey) => ({
+          ...prev,
+          [currentKey]: getGuideLink(currentKey, ovhSubsidiary),
+        }),
+        {} as Record<TGuideKey, string>,
+      )
+    : getGuideLink(keys, ovhSubsidiary);
+}
