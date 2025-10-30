f61b031d4596d8b0c7be0e74cf156e6070373c04
David Arsène <david.arsene.ext@ovhcloud.com>
2025-10-30 17:55:29 +0100

    test(observability): update tests to manage new type tenant state
    fixes: #MAOBS-120
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/observability/src/data/api/tenants.props.ts
+++ packages/manager/apps/observability/src/data/api/tenants.props.ts
@@ -3,13 +3,14 @@ import { TenantState } from '@/types/tenants.type';
 
 import { ObservabilityServiceParams } from './observability.props';
 
-export type TTargetSpecPayload = Omit<TenantState, 'limits'> & {
+export type TTargetSpecPayload = Omit<TenantState, 'limits' | 'infrastructure'> & {
   limits: {
     numberOfSeries: {
       maximum: number;
     };
     retention: TIdentifier;
   };
+  infrastructure?: TIdentifier;
 };
 
 export type CreateTenantsPayload = {
