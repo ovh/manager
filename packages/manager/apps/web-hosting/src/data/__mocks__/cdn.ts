75112bbbe70d595a2d24e223863cf56dab61e0e7
Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>
2025-10-30 16:06:43 +0100

    feat(web-hosting): migrate cdn modification page into react
    ref: #PUWEBPT-65
    
    Signed-off-by: Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>

--- packages/manager/apps/web-hosting/src/data/__mocks__/cdn.ts
+++ packages/manager/apps/web-hosting/src/data/__mocks__/cdn.ts
@@ -1,4 +1,4 @@
-import { CDN_TYPE, CDN_VERSION, CdnStatus } from '../types/product/cdn';
+import { CDN_TYPE, CDN_VERSION, CdnOption, CdnOptionType, CdnStatus } from '../types/product/cdn';
 
 export const cdnPropertiesMock = {
   domain: 'abcdef.cluster030.hosting.ovh.net',
@@ -8,3 +8,31 @@ export const cdnPropertiesMock = {
   type: CDN_TYPE.SECURITY,
   version: CDN_VERSION.CDN_HOSTING,
 };
+
+export const serviceNameCdnMock = {
+  domain: 'qjfnqci.cluster100.hosting.ovh.net',
+  free: true,
+  status: 'created',
+  taskId: 473761370,
+  type: 'cdn-basic',
+  version: 'cdn-hosting',
+};
+
+export const cdnOptionMock: CdnOption[] = [
+  {
+    name: 'devmode',
+    type: CdnOptionType.DEVMODE,
+    config: null,
+    pattern: null,
+    enabled: true,
+    extra: null,
+  },
+  {
+    name: 'brotli',
+    type: CdnOptionType.BROTLI,
+    config: null,
+    pattern: null,
+    enabled: true,
+    extra: null,
+  },
+];
