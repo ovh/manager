8b25c31ddd0fea0819f83c7b82e071cc69b523cb
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-10-31 22:47:49 +0100

    chore(okms): use kebab case for folder names
    ref: #MANAGER-20184
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/okms/src/modules/secret-manager/utils/tests/versionList.ts
+++ packages/manager/apps/okms/src/modules/secret-manager/utils/tests/versionList.ts
@@ -1,7 +1,7 @@
 import { screen, waitFor } from '@testing-library/react';
 import { assertTextVisibility } from '@ovh-ux/manager-core-test-utils';
 import { versionListMock } from '@secret-manager/mocks/versions/versions.mock';
-import { VERSION_LIST_CELL_TEST_IDS } from '@secret-manager/pages/secret/versionList/VersionCells.constants';
+import { VERSION_LIST_CELL_TEST_IDS } from '@secret-manager/pages/secret/version-list/VersionCells.constants';
 import { labels } from '@/common/utils/tests/init.i18n';
 
 export const assertVersionDatagridVisilibity = async () => {
