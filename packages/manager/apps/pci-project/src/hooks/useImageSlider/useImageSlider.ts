d696c57cb2ce6a036f5a48472b8dbf1cc0dc8e91
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2025-10-31 15:18:45 +0100

    fix(pci-project): fix creating page images import
    ref: #MANAGER-20119
    
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/apps/pci-project/src/hooks/useImageSlider/useImageSlider.ts
+++ packages/manager/apps/pci-project/src/hooks/useImageSlider/useImageSlider.ts
@@ -2,14 +2,14 @@ import { useState, useEffect, useCallback } from 'react';
 import { SLIDE_ANIMATION_INTERVAL } from '@/constants';
 
 export const SLIDE_IMAGES = [
-  '/assets/creating/Hero16-9_Data_ADP.png',
-  '/assets/creating/Hero16-9_Orchestration_Ks8.png',
-  '/assets/creating/Hero16-9_Orchestration_PrivateImageCatalogue.png',
-  '/assets/creating/Hero16-9_Stockage_BlockStorage.png',
-  '/assets/creating/Hero16-9_Stockage_CloudArchive.png',
-  '/assets/creating/Hero16-9_Stockage_InstanceBackup.png',
-  '/assets/creating/Hero16-9_Stockage_ObjectStorage.png',
-  '/assets/creating/Hero16-9_Stockage_SnapshotVolume.png',
+  'public/assets/creating/Hero16-9_Data_ADP.png',
+  'public/assets/creating/Hero16-9_Orchestration_Ks8.png',
+  'public/assets/creating/Hero16-9_Orchestration_PrivateImageCatalogue.png',
+  'public/assets/creating/Hero16-9_Stockage_BlockStorage.png',
+  'public/assets/creating/Hero16-9_Stockage_CloudArchive.png',
+  'public/assets/creating/Hero16-9_Stockage_InstanceBackup.png',
+  'public/assets/creating/Hero16-9_Stockage_ObjectStorage.png',
+  'public/assets/creating/Hero16-9_Stockage_SnapshotVolume.png',
 ];
 
 export const useImageSlider = () => {
