52a987962be0451543be6c3ad401c9fc3dc8cea7
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-10-31 09:14:10 +0100

    feat(okms): add a key/value editor for secret data
    ref: #MANAGER-18318
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

---
+++ packages/manager/apps/okms/src/modules/secret-manager/components/form/secret-data-form-field/SecretDataFormField.component.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { useController, UseControllerProps } from 'react-hook-form';
+import { isStringValidForKeyValueForm } from '@secret-manager/components/form/key-values-editor/utils/keyValue';
+import {
+  SecretValueToggle,
+  SecretValueToggleState,
+} from '@secret-manager/components/secret-value-toggle/SecretValueToggle.component';
+import { KeyValuesEditor } from '@secret-manager/components/form/key-values-editor/KeyValuesEditor';
+import { SecretDataJsonFormField } from './SecretDataJsonFormField.component';
+
+type FormFieldInput = {
+  data: string;
+};
+
+export const SecretDataFormField = <T extends FormFieldInput>({
+  name,
+  control,
+}: UseControllerProps<T>) => {
+  const { field } = useController({ name, control });
+
+  const [toggleState, setToggleState] = useState<SecretValueToggleState>(
+    isStringValidForKeyValueForm(field.value) ? 'key-value' : 'json',
+  );
+
+  return (
+    <div className="space-y-6">
+      <SecretValueToggle state={toggleState} onChange={setToggleState} />
+      {toggleState === 'key-value' ? (
+        <KeyValuesEditor name={name} control={control} />
+      ) : (
+        <SecretDataJsonFormField name={name} control={control} />
+      )}
+    </div>
+  );
+};
