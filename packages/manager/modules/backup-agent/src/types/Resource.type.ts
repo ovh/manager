41c76fa7dbbdeccba6a5548b2d78307d519d3b30
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-11-03 17:02:39 +0100

    feat(backup-agent): add listing details for tenants
    ref: BKP-498
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/backup-agent/src/types/Resource.type.ts
+++ packages/manager/modules/backup-agent/src/types/Resource.type.ts
@@ -12,6 +12,34 @@ export type ResourceWithAzName = {
 
 export type ResourceWithName = {
   currentState: {
-    name: string
+    name: string;
   };
 };
+
+export type TaskStatus = 'ERROR' | 'PENDING' | 'RUNNING' | 'SCHEDULED' | 'WAITING_USER_INPUT';
+
+export interface CurrentTask {
+  id: string;
+  link: string;
+  status: TaskStatus | null;
+  type: string;
+}
+
+export interface IamResource {
+  displayName: string | null;
+  id: string;
+  tags?: Record<string, string> | null;
+  urn: string;
+}
+
+export type Resource<T extends { name: string }> = ResourceWithStatus &
+  ResourceWithName & {
+    createdAt: string;
+    currentState: T;
+    currentTasks: CurrentTask[];
+    iam: IamResource | null;
+    id: string;
+    resourceStatus: ResourceStatus;
+    targetSpec: Pick<T, 'name'>;
+    updatedAt: string;
+  };
