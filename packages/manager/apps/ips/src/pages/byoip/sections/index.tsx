39b7ad06dd7eb2b4cda4a9185d97c5d5e110a135
aderghamov <alexandre.dergham.ext@ovhcloud.com>
2025-11-03 14:28:47 +0100

    fix(ips): fixing the AS section layout (#20140)
    ref: #MANAGER-19852
    
    Signed-off-by: aderghamov <alexandre.dergham.ext@ovhcloud.com>

--- packages/manager/apps/ips/src/pages/byoip/sections/index.tsx
+++ packages/manager/apps/ips/src/pages/byoip/sections/index.tsx
@@ -3,5 +3,4 @@ export * from './RegionSelectionSection.component';
 export * from './TokenSection.component';
 export * from './IpRangeSelectionSection.component';
 export * from './AsTypeSelectionSection.component';
-export * from './AsOwnTypeSelectionSection.component';
 export * from './OrderButtonSection.component';
