cb4ab1e3495b4cf4999a6cb7bf8e356cd971eef6
Brendan Seiller <brendan.seiller@ovhcloud.com>
2025-11-03 10:23:27 +0100

    feat(web-domains): add assoctiated hosting + create order funnel for free hosting
    Signed-off-by: Brendan Seiller <brendan.seiller@ovhcloud.com>

--- packages/manager/apps/web-domains/src/domain/components/AssociatedServicesCards/AssociatedServicesCards.tsx
+++ packages/manager/apps/web-domains/src/domain/components/AssociatedServicesCards/AssociatedServicesCards.tsx
@@ -1,6 +1,7 @@
 import { ManagerTile } from '@ovh-ux/manager-react-components';
 import { useTranslation } from 'react-i18next';
 import Emails from './Emails';
+import Hosting from './Hosting';
 
 interface AssociatedServicesCardsProps {
   readonly serviceName: string;
@@ -10,13 +11,13 @@ export default function AssociatedServicesCards({
   serviceName,
 }: AssociatedServicesCardsProps) {
   const { t } = useTranslation(['domain']);
-
   return (
     <ManagerTile>
       <ManagerTile.Title>
         {t(`domain_tab_general_information_associated_services_title`)}
       </ManagerTile.Title>
       <ManagerTile.Divider />
+      <Hosting serviceName={serviceName} />
       <Emails serviceName={serviceName} />
     </ManagerTile>
   );
