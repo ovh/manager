3de22178d538e60ccaaa3a6d0e123ea4f9d0a47e
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-11-03 14:47:00 +0100

    feat(backup-agent): extract general information tile of vault
    ref: #BKP-498
    
    Co-authored-by: Evie <vovh@users.noreply.github.com>
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/backup-agent/src/pages/vaults/dashboard/general-information/GeneralInformation.page.tsx
+++ packages/manager/modules/backup-agent/src/pages/vaults/dashboard/general-information/GeneralInformation.page.tsx
@@ -1,9 +1,9 @@
 import { BillingInformationsTileStandard } from "@ovh-ux/billing-informations"
 
-import {GeneralInformationTile} from "./_components/general-information-tile/GeneralInformationTile.component";
+import { GeneralInformationVaultTile } from "./_components/general-information-vault-tile/GeneralInformationVaultTile.component";
 import { useParams } from "react-router-dom";
-import {useBackupVaultDetails} from "@/data/hooks/vaults/getVaultDetails";
-import {SubscriptionTile} from "./_components/subscription-tile/SubscriptionTile.component";
+import { useBackupVaultDetails } from "@/data/hooks/vaults/getVaultDetails";
+import { SubscriptionTile } from "./_components/subscription-tile/SubscriptionTile.component";
 
 
 export default function GeneralInformationPage() {
@@ -12,7 +12,7 @@ export default function GeneralInformationPage() {
 
   return (
     <section className="max-w-6xl mx-auto px-12 grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-8">
-      <GeneralInformationTile vaultId={vaultId!} />
+      <GeneralInformationVaultTile vaultId={vaultId!} />
       <SubscriptionTile vaultId={vaultId!} />
       <BillingInformationsTileStandard resourceName={vaultResource?.currentState?.resourceName} />
     </section>
