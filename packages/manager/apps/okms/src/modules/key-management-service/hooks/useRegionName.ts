0192d946e361a8e9d5bc7968e71a6219b88ebb7b
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-11-04 10:57:06 +0100

    fix(okms): translate continent names in the region selector
    ref: #MANAGER-19002
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/okms/src/modules/key-management-service/hooks/useRegionName.ts
+++ packages/manager/apps/okms/src/modules/key-management-service/hooks/useRegionName.ts
@@ -2,10 +2,13 @@ import { NAMESPACES } from '@ovh-ux/manager-common-translations';
 import { useTranslation } from 'react-i18next';
 
 export const useRegionName = () => {
-  const { t } = useTranslation([NAMESPACES.REGION]);
+  const { t } = useTranslation(['common', NAMESPACES.REGION]);
 
   const translateRegionName = (region: string) =>
     t(`region_${region}`, { ns: NAMESPACES.REGION });
 
-  return { translateRegionName };
+  const translateGeographyName = (geographyCode: string) =>
+    t(`region_continent_${geographyCode}`, { ns: 'common' });
+
+  return { translateRegionName, translateGeographyName };
 };
