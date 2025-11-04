447cc1764e016346a9a22f361452d93784c464c6
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-11-04 10:43:52 +0100

    feat(backup-agent): add tenants tabs on dashboard page
    ref: #BKP-498
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/backup-agent/src/pages/services/dashboard/Dashboard.page.tsx
+++ packages/manager/modules/backup-agent/src/pages/services/dashboard/Dashboard.page.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense, startTransition, useMemo, useContext } from 'react';
 
-import { NavLink, Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { NavLink, Outlet, useLocation, useNavigate, useParams } from 'react-router-dom';
 
 import { useTranslation } from 'react-i18next';
 
@@ -11,27 +11,22 @@ import { useOvhTracking } from '@ovh-ux/manager-react-shell-client';
 
 import { urls } from '@/routes/Routes.constants';
 
-import { useDashboardTabs } from './_hooks/useDashboardTabs';
+import { useTenantDashboardTabs } from './_hooks/useTenantDashboardTabs';
 import { BackupAgentContext } from "@/BackupAgent.context";
 import { NAMESPACES } from '@ovh-ux/manager-common-translations';
+import {useBackupTenantDetails} from "@/data/hooks/tenants/useBackupTenantDetails";
 
 export default function DashboardPage() {
   const { appName } = useContext(BackupAgentContext)
+  const { tenantId } = useParams<{ tenantId: string }>();
+  const { data: tenantResource } = useBackupTenantDetails({ tenantId: tenantId! })
   const { t } = useTranslation([NAMESPACES.ACTIONS]);
   const navigate = useNavigate();
 
   const location = useLocation();
   const { trackClick } = useOvhTracking();
 
-  const tabs = useDashboardTabs();
-
-  const activeTab = useMemo(
-    () =>
-      tabs.find((tab) => location.pathname === `${urls.dashboardTenants}/${tab.to}`) ??
-      tabs.find((tab) => tab.to && location.pathname.startsWith(`${urls.dashboardTenants}/${tab.to}`)) ??
-      tabs[0],
-    [tabs, location.pathname],
-  );
+  const tabs = useTenantDashboardTabs();
 
   const onNavigateBackClicked = () => {
     startTransition(() => navigate('..'));
@@ -39,7 +34,7 @@ export default function DashboardPage() {
 
   return (
     <BaseLayout
-      header={{ title: t('dashboard:title') }}
+      header={{ title: tenantResource?.currentState.name ?? tenantId }}
       backLinkLabel={t(`${NAMESPACES.ACTIONS}:back`)}
       onClickReturn={onNavigateBackClicked}
       breadcrumb={<Breadcrumb appName={appName} rootLabel={appName} />}
@@ -56,7 +51,7 @@ export default function DashboardPage() {
                 }
               }}
             >
-              <OdsTab isSelected={tab.name === activeTab?.name}>{t(tab.title)}</OdsTab>
+              <OdsTab isSelected={tab.isActive}>{t(tab.title)}</OdsTab>
             </NavLink>
           ))}
         </OdsTabs>
