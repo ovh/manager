67f6f2c761bc6dd08b900e80a36b26ecf701e1a2
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-11-04 14:45:16 +0100

    feat(backup-agent): add bucket vault tab
    ref: #BKP-493
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/backup-agent/src/test-utils/setupMsw.ts
+++ packages/manager/modules/backup-agent/src/test-utils/setupMsw.ts
@@ -1,6 +1,7 @@
 import { SetupServer } from 'msw/node';
 
 import { getAuthenticationMocks, toMswHandlers } from '@ovh-ux/manager-core-test-utils';
+import { GetServicesMocksParams, getServicesMocks } from '@ovh-ux/manager-module-common-api';
 
 import { TLocationMockParams, getLocationMocks } from '@/mocks/location/locations.handler';
 import { TTenantMockParams, getTenantMocks } from '@/mocks/tenant/tenants.handler';
@@ -10,12 +11,14 @@ import { TVaultMockParams, getVaultMocks } from '@/mocks/vaults/vaults.handler';
 export type MockParams = TVaultMockParams &
   TLocationMockParams &
   TTenantMockParams &
-  TVSPCTenantMockParams;
+  TVSPCTenantMockParams &
+  GetServicesMocksParams;
 
 export const setupMswMock = (mockParams: MockParams = {}) => {
   (global as unknown as { server: SetupServer }).server?.resetHandlers(
     ...toMswHandlers([
       ...getAuthenticationMocks({ isAuthMocked: true }),
+      ...getServicesMocks(mockParams),
       ...getVaultMocks(mockParams),
       ...getLocationMocks(mockParams),
       ...getTenantMocks(mockParams),
