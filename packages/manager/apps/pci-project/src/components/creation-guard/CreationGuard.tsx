0ab7988a022e43211e339833a3f2d2f26714551c
LIDRISSI Hamid <lidrissi.hamid@gmail.com>
2025-11-04 14:09:40 +0100

    feat(pci-project): create a separate project creation flow for US region
    ref: #MANAGER-20143
    
    Signed-off-by: LIDRISSI Hamid <lidrissi.hamid@gmail.com>

--- packages/manager/apps/pci-project/src/components/creation-guard/CreationGuard.tsx
+++ packages/manager/apps/pci-project/src/components/creation-guard/CreationGuard.tsx
@@ -1,10 +1,22 @@
 import { RedirectionGuard } from '@ovh-ux/manager-react-components';
-import { lazy } from 'react';
+import { ShellContext } from '@ovh-ux/manager-react-shell-client';
+import { lazy, useContext } from 'react';
 import useCreationRedirect from '@/hooks/useCreationRedirect/useCreationRedirect';
 
 const CreationPage = lazy(() => import('@/pages/creation/Creation.page'));
+const CreationLegacyPage = lazy(() =>
+  import('@/pages/creation/Creation.legacy.page'),
+);
 
 export default function CreationGuard() {
+  const { environment } = useContext(ShellContext);
+  const region = environment.getRegion();
+  const isUSRegion = region === 'US';
+
+  if (isUSRegion) {
+    return <CreationLegacyPage />;
+  }
+
   const {
     shouldBlockCreation,
     redirectRoute,
