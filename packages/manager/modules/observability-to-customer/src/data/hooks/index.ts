be37baeabc18cff50ebf09d67612b805402a4ac6
yjaaouane <yassine.jaaouane.ext@corp.ovh.com>
2025-11-05 04:19:01 +0100

    refactor(observability): use tsc-alias and scss config
    fixes: #MAOBS-43
    
    Signed-off-by: yjaaouane <yassine.jaaouane.ext@corp.ovh.com>

--- packages/manager/modules/observability-to-customer/src/data/hooks/index.ts
+++ packages/manager/modules/observability-to-customer/src/data/hooks/index.ts
@@ -1,3 +1,1 @@
-export { useDashboardConfig } from './dashboards/useDashboardConfig';
-export { useChartData } from './dashboards/useChartData';
-export { useMultipleChartData } from './dashboards/useMultipleChartData';
+export * from './dashboards';
