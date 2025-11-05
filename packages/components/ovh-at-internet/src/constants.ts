597350b18a14235898827dbb6301198c6ca822db
Sachin Ramesh <sachin.ramesh@ovhcloud.com>
2025-11-05 19:50:17 +0530

    feat(communication): add tracking on contacts page
    ref: #MANAGER-19530
    
    Signed-off-by: Sachin Ramesh <sachin.ramesh@ovhcloud.com>

--- packages/components/ovh-at-internet/src/constants.ts
+++ packages/components/ovh-at-internet/src/constants.ts
@@ -17,6 +17,7 @@ export const AT_INTERNET_LEVEL2: Record<string, string> = {
   130: 'Manager-Baremetal',
   140: 'Manager-identity-security-operations',
   150: 'Manager-DedicatedServers',
+  157: 'Manager-Communications',
 };
 
 export const AT_INTERNET_WEBSITE: Record<string, string> = {
