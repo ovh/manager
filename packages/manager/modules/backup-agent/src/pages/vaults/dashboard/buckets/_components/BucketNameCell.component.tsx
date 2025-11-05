83666dc22ef2ddb12de92df96f997387dfe6b02c
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-11-05 11:15:10 +0100

    feat(backup-agent): add bucket vault list
    ref: #BKP-493
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

---
+++ packages/manager/modules/backup-agent/src/pages/vaults/dashboard/buckets/_components/BucketNameCell.component.tsx
@@ -0,0 +1,16 @@
+import { useHref } from 'react-router-dom';
+
+import { DataGridTextCell, Links } from '@ovh-ux/manager-react-components';
+
+import {urlParams, urls} from '@/routes/Routes.constants';
+import {Bucket} from "@/types/Bucket.type";
+
+export const BucketNameCell = ({ id, name }: Pick<Bucket, "id" | "name">) => {
+  const bucketDashboardHref = useHref(urls.dashboardVaults.replace(urlParams.vaultId, id)); // TODO : Replace with good url
+
+  return (
+    <DataGridTextCell>
+      <Links href={bucketDashboardHref} label={name} />
+    </DataGridTextCell>
+  );
+};
