ff357292bd1307206536996208b8749bc8f4fe8d
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2025-11-05 17:23:56 +0100

    feat(pci-instances): add sshkey bloc
    ref: #TAPC-4624, #TAPC-4653
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

---
+++ packages/manager/apps/pci-instances/src/pages/instances/create/components/sshKey/SshKeyHelper.component.tsx
@@ -0,0 +1,47 @@
+import { HelpDrawer } from '@/components/helpDrawer/HelpDrawer.component';
+import { FC } from 'react';
+import { Trans, useTranslation } from 'react-i18next';
+import { Link, Text } from '@ovhcloud/ods-react';
+import { useGuideLink } from '@/hooks/url/useGuideLink';
+
+export const SshKeyHelper: FC = () => {
+  const { t } = useTranslation('creation');
+  const guide = useGuideLink('SSH_KEY');
+
+  const HELPER_TRANSLATION_KEYS = [
+    'description',
+    'content_list',
+    'goal',
+    'how_to',
+  ];
+
+  return (
+    <HelpDrawer>
+      <Text preset="heading-2">
+        {t('creation:pci_instance_creation_select_sshKey_help_title')}
+      </Text>
+      {HELPER_TRANSLATION_KEYS.map((key) => (
+        <Text key={key} preset="paragraph" className="py-4">
+          <Trans
+            t={t}
+            i18nKey={`creation:pci_instance_creation_select_sshKey_help_${key}`}
+            components={{
+              ul: <ul />,
+              ol: <ol />,
+              li: <li />,
+            }}
+          />
+        </Text>
+      ))}
+      <Link
+        className="visited:text-[var(--ods-color-primary-500)]"
+        href={guide}
+        target="_blank"
+      >
+        {t(
+          'creation:pci_instance_creation_select_sshKey_help_read_complete_guide',
+        )}
+      </Link>
+    </HelpDrawer>
+  );
+};
