60789c2ac118435126cf893973113cfaeda95ea2
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-11-05 21:38:20 +0100

    feat(backup-agent): add tenant agents list
    ref: #BKP-501
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

---
+++ packages/manager/modules/backup-agent/src/pages/services/dashboard/agents/_components/AgentNameCell.component.tsx
@@ -0,0 +1,16 @@
+import { useHref } from 'react-router-dom';
+
+import { DataGridTextCell, Links } from '@ovh-ux/manager-react-components';
+
+import {urlParams, urls} from '@/routes/Routes.constants';
+import {Agent} from "@/types/Agent.type";
+
+export const AgentNameCell = ({ id, name }: Pick<Agent, "id" | "name">) => {
+  const bucketDashboardHref = useHref(urls.dashboardVaults.replace(urlParams.vaultId, id)); // TODO : Replace with good url
+
+  return (
+    <DataGridTextCell>
+      <Links href={bucketDashboardHref} label={name} />
+    </DataGridTextCell>
+  );
+};
