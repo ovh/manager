74cdf95635556f6894f43249807bca9ea8c39e4e
yjaaouane <yassine.jaaouane.ext@corp.ovh.com>
2025-11-05 04:19:01 +0100

    refactor(observability): use tsc-alias and scss config
    fixes: #MAOBS-43
    
    Signed-off-by: yjaaouane <yassine.jaaouane.ext@corp.ovh.com>

--- packages/manager/modules/observability-to-customer/src/__mocks__/dashboards/dashboard.adapter.ts
+++ packages/manager/modules/observability-to-customer/src/__mocks__/dashboards/dashboard.adapter.ts
@@ -1,8 +1,8 @@
-import { getPredefinedDashboard as getPredefinedDashboardFromApi } from '../../data/api/dashboard';
-import { ObservabilityDashboardParams } from '../../types/ClientApi.type';
-import { Dashboard } from '../../types/observability.type';
-import { getPredefinedDashboard as getPredefinedDashboardFromMock } from '../dashboards/dashboard.mock';
-import { apiConfig } from '../mock.config';
+import { getPredefinedDashboard as getPredefinedDashboardFromMock } from '@/__mocks__/dashboards/dashboard.mock';
+import { apiConfig } from '@/__mocks__/mock.config';
+import { getPredefinedDashboard as getPredefinedDashboardFromApi } from '@/data/api/dashboard';
+import { ObservabilityDashboardParams } from '@/types/ClientApi.type';
+import { Dashboard } from '@/types/observability.type';
 
 export const getPredefinedDashboard = async (
   params: ObservabilityDashboardParams,
