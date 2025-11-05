65b8b0504eaa5783e1304bf400efab84987fcf10
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-11-05 21:38:20 +0100

    feat(backup-agent): add tenant agents list
    ref: #BKP-501
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

---
+++ packages/manager/modules/backup-agent/src/data/hooks/agents/getAgents.ts
@@ -0,0 +1,21 @@
+import { mockVaults } from '@/mocks/vaults/vaults';
+import {BACKUP_TENANT_DETAILS_QUERY_KEY} from "@/data/hooks/tenants/useBackupTenantDetails";
+import {mockAgents} from "@/mocks/agents/agents";
+
+// Remove during unmocking
+export const BACKUP_AGENTS_LIST_QUERY_KEY = (tenantId: string) => [...BACKUP_TENANT_DETAILS_QUERY_KEY(tenantId), "agents"];
+
+export const useBackupAgentList = (
+  {
+    // tenantId
+    // pageSize,
+  }: {
+    pageSize: number;
+    tenantId: string;
+  } = { pageSize: 9999 },
+) => ({ flattenData: mockAgents, isLoading: false });
+// useResourcesIcebergV2<VaultResource>({
+//   route: getBackupAgentsList(tenantId),
+//   queryKey: BACKUP_AGENTS_LIST_QUERY_KEY(tenantId),
+//   pageSize,
+// });
