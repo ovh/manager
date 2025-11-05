0bb09b77a6d463d5b6f672d5c34c7cc1de80af01
Brendan Seiller <brendan.seiller@ovhcloud.com>
2025-11-05 10:33:46 +0100

    feat(web-domains): add subdomain and multisite on associated services cards
    ref: DCE-3

--- packages/manager/apps/web-domains/src/domain/components/AssociatedServicesCards/FreeHostingDrawer/Section.tsx
+++ packages/manager/apps/web-domains/src/domain/components/AssociatedServicesCards/FreeHostingDrawer/Section.tsx
@@ -2,15 +2,15 @@ import { Text, TEXT_PRESET } from '@ovhcloud/ods-react';
 import { useTranslation } from 'react-i18next';
 
 interface SectionProps {
-  title: string;
-  children: React.ReactNode;
+  readonly title: string;
+  readonly children: React.ReactNode;
 }
 
 export default function Section({ title, children }: SectionProps) {
   const { t } = useTranslation(['domain']);
 
   return (
-    <div className="space-y-8">
+    <div className="space-y-6">
       <Text preset={TEXT_PRESET.heading3}>{t(title)}</Text>
       {children}
     </div>
