40f0ad2484242fd38a161d72629a041f3a093db9
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-11-05 21:38:20 +0100

    feat(backup-agent): add tenant agents list
    ref: #BKP-501
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

---
+++ packages/manager/modules/backup-agent/src/pages/services/dashboard/agents/_components/AgentIpsCell.component.tsx
@@ -0,0 +1,11 @@
+import { DataGridTextCell, Links } from '@ovh-ux/manager-react-components';
+
+import {Agent} from "@/types/Agent.type";
+
+export const AgentIpsCell = ({ ip: ips = [] }: Pick<Agent, "ip">) => {
+  return (
+    <DataGridTextCell>
+      { ips.join(', ') }
+    </DataGridTextCell>
+  );
+};
