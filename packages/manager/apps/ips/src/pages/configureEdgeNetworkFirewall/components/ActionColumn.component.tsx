a14580aa12f236a445dd0e6118195dd304fc6632
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-11-05 14:37:03 +0100

    fix(ips): firewall feedbacks
    ref: #MANAGER-18075
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager/apps/ips/src/pages/configureEdgeNetworkFirewall/components/ActionColumn.component.tsx
+++ packages/manager/apps/ips/src/pages/configureEdgeNetworkFirewall/components/ActionColumn.component.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { OdsButton } from '@ovhcloud/ods-components/react';
-import { ODS_BUTTON_VARIANT, ODS_ICON_NAME } from '@ovhcloud/ods-components';
+import {
+  ODS_BUTTON_SIZE,
+  ODS_BUTTON_VARIANT,
+  ODS_ICON_NAME,
+} from '@ovhcloud/ods-components';
 import { IpEdgeFirewallRule, IpEdgeFirewallRuleState } from '@/data/api';
 import { EdgeNetworkFirewallContext } from '../edgeNetworkFirewall.context';
-import { useCreateIpEdgeNetworkFirewallRule } from '@/data/hooks';
 
 export const ActionColumn = (
   rule: IpEdgeFirewallRule & { isNew?: boolean },
@@ -18,26 +21,23 @@ export const ActionColumn = (
   } = React.useContext(EdgeNetworkFirewallContext);
 
   return rule?.isNew ? (
-    <>
+    <div className="flex gap-3">
       <OdsButton
+        size={ODS_BUTTON_SIZE.xs}
         label=""
         icon={ODS_ICON_NAME.xmark}
         variant={ODS_BUTTON_VARIANT.ghost}
         onClick={hideNewRuleRow}
       />
       <OdsButton
+        size={ODS_BUTTON_SIZE.xs}
         label=""
         icon={ODS_ICON_NAME.check}
         variant={ODS_BUTTON_VARIANT.ghost}
-        isDisabled={
-          !newProtocol ||
-          !newMode ||
-          newSequence === null ||
-          newSequence === undefined
-        }
+        isDisabled={newSequence === null || newSequence === undefined}
         onClick={createNewRule}
       />
-    </>
+    </div>
   ) : (
     <OdsButton
       label=""
