32ae7a9e9863a80342f1c9796b6a83880fc92aed
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-11-06 16:18:11 +0100

    fix(okms): neutral color for the "not a key-value" message
    ref: #MANAGER-18318
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/okms/src/modules/secret-manager/components/form/key-values-editor/KeyValuesEditorErrorMessage.tsx
+++ packages/manager/apps/okms/src/modules/secret-manager/components/form/key-values-editor/KeyValuesEditorErrorMessage.tsx
@@ -5,7 +5,7 @@ import { OdsMessage } from '@ovhcloud/ods-components/react';
 export const KeyValuesEditorErrorMessage = () => {
   const { t } = useTranslation(['secret-manager']);
   return (
-    <OdsMessage color="danger" isDismissible={false}>
+    <OdsMessage color="information" isDismissible={false}>
       {t('error_key_value_conversion')}
     </OdsMessage>
   );
