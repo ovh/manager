02a254a05e47c2e45870839ff55721c42f1a48aa
Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>
2025-11-07 14:48:20 +0100

    feat(web-hosting): create add website page
    ref: #PUWEBPT-101
    
    Signed-off-by: Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>

--- packages/manager/apps/web-hosting/src/pages/dashboard/multisite/website/AddWebsite.page.tsx
+++ packages/manager/apps/web-hosting/src/pages/dashboard/multisite/website/AddWebsite.page.tsx
@@ -1,3 +1,167 @@
+import { useState } from 'react';
+
+import { useNavigate, useParams } from 'react-router-dom';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { z } from 'zod';
+
+import { ODS_TEXT_PRESET } from '@ovhcloud/ods-components';
+import { OdsButton, OdsDivider, OdsLink, OdsText } from '@ovhcloud/ods-components/react';
+
+import { LinkType, Links, useNotifications } from '@ovh-ux/manager-react-components';
+
+import { usePostWebHostingWebsites } from '@/data/hooks/webHosting/webHostingWebsiteDomain/webHostingWebsiteDomain';
+import { CmsType } from '@/data/types/product/managedWordpress/cms';
+import { AssociationType } from '@/data/types/product/website';
+import { ServiceStatus } from '@/data/types/status';
+import { websiteFormSchema } from '@/utils/formSchemas.utils';
+
+import { DomainAssociation } from './component/DomainAssociation';
+import { DomainCmsModule } from './component/DomainCmsModule';
+import { DomainConfiguration } from './component/DomainConfiguration';
+import { DomainDnsConfiguration } from './component/DomainDnsConfiguration';
+import { DomainManagement } from './component/DomainManagement';
+
 export default function AddWebsitePage() {
-  return <>AddWebsitePage</>;
+  const { serviceName } = useParams();
+  const navigate = useNavigate();
+  const { addSuccess, addWarning } = useNotifications();
+  const { t } = useTranslation(['common']);
+  const [step, setStep] = useState<number>(1);
+
+  type FormData = z.infer<typeof websiteFormSchema>;
+
+  const { control, handleSubmit, watch } = useForm<FormData>({
+    defaultValues: {
+      path: 'public_html',
+      autoConfigureDns: true,
+    },
+    resolver: zodResolver(websiteFormSchema),
+  });
+
+  const controlValues = watch();
+
+  const { postWebHostingWebsites } = usePostWebHostingWebsites(
+    serviceName,
+    () => {
+      addSuccess(
+        <>
+          <OdsText>{t('multisite:multisite_add_website_success')}</OdsText>
+          <OdsLink
+            href={`#/${serviceName}/task`}
+            label={t('multisite:multisite_add_website_in_progress')}
+          />
+        </>,
+        true,
+      );
+    },
+    (error) => {
+      addWarning(
+        t('multisite:multisite_add_website_error', {
+          error: error?.response?.data?.message,
+        }),
+        true,
+      );
+    },
+  );
+
+  const onSubmit = (data: FormData) => {
+    if (data.associationType === AssociationType.EXISTING) {
+      const payload = {
+        targetSpec: {
+          name: data.name,
+          fqdn: data.fqdn,
+          ...(data.module ? { module: { name: data.module as CmsType } } : {}),
+          bypassDNSConfiguration: !data.autoConfigureDns,
+          ...(data.advancedConfiguration
+            ? {
+                ...(data.ip ? { ipLocation: data.selectedIp } : {}),
+                firewall: {
+                  status: data.firewall ? ServiceStatus.ACTIVE : ServiceStatus.NONE,
+                },
+                cdn: {
+                  status: data.cdn ? ServiceStatus.ACTIVE : ServiceStatus.NONE,
+                },
+                path: data.path,
+              }
+            : {}),
+        },
+      };
+      postWebHostingWebsites([payload, false]);
+    } else {
+      const payload = {
+        targetSpec: {
+          name: data.name,
+          fqdn: data.fqdn,
+        },
+      };
+      postWebHostingWebsites([payload, data.wwwNeeded ?? false]);
+    }
+    navigate(-1);
+  };
+
+  return (
+    <form className="flex flex-col space-y-6">
+      <Links
+        type={LinkType.back}
+        onClickReturn={() => navigate(-1)}
+        label={t('common:web_hosting_common_sites_backlink')}
+        className="mb-4"
+      />
+      <DomainAssociation
+        control={control}
+        controlValues={controlValues}
+        setStep={setStep}
+        isNextButtonVisible={step === 1}
+      />
+      <OdsDivider />
+      {step >= 2 ? (
+        <DomainConfiguration
+          control={control}
+          controlValues={controlValues}
+          setStep={setStep}
+          isNextButtonVisible={step === 2}
+        />
+      ) : (
+        <OdsText preset={ODS_TEXT_PRESET.heading4}>
+          {t('multisite:multisite_add_website_configure_domain_title')}
+        </OdsText>
+      )}
+      {controlValues.associationType === AssociationType.EXTERNAL && step === 3 && (
+        <>
+          <DomainManagement controlValues={controlValues} />
+          <OdsButton
+            label={t('common:web_hosting_common_action_continue')}
+            onClick={() => void handleSubmit(onSubmit)()}
+            isDisabled={!controlValues.fqdn}
+          />
+        </>
+      )}
+      {controlValues.associationType === AssociationType.EXISTING && step >= 3 && (
+        <DomainDnsConfiguration
+          control={control}
+          controlValues={controlValues}
+          setStep={setStep}
+          isNextButtonVisible={step === 3}
+        />
+      )}
+      <OdsDivider />
+      {step === 4 ? (
+        <>
+          <DomainCmsModule control={control} controlValues={controlValues} />
+          <OdsButton
+            label={t('common:web_hosting_common_action_continue')}
+            onClick={() => void handleSubmit(onSubmit)()}
+            isDisabled={!controlValues.fqdn}
+          />
+        </>
+      ) : (
+        <OdsText preset={ODS_TEXT_PRESET.heading4}>
+          {t('multisite:multisite_add_website_module_cms_title')}
+        </OdsText>
+      )}
+    </form>
+  );
 }
