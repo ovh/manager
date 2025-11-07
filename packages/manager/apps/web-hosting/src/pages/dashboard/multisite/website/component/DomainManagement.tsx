22cd7079ec3c8c098515d7cf0a92055c4cde6c14
Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>
2025-11-07 14:48:20 +0100

    feat(web-hosting): create add website page
    ref: #PUWEBPT-101
    
    Signed-off-by: Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>

---
+++ packages/manager/apps/web-hosting/src/pages/dashboard/multisite/website/component/DomainManagement.tsx
@@ -0,0 +1,83 @@
+import { useParams } from 'react-router-dom';
+
+import { useTranslation } from 'react-i18next';
+import { z } from 'zod';
+
+import { ODS_MESSAGE_COLOR, ODS_TEXT_PRESET } from '@ovhcloud/ods-components';
+import { OdsClipboard, OdsDivider, OdsMessage, OdsText } from '@ovhcloud/ods-components/react';
+
+import {
+  useGetAddDomainExisting,
+  useGetHostingService,
+} from '@/data/hooks/webHostingDashboard/useWebHostingDashboard';
+import { websiteFormSchema } from '@/utils/formSchemas.utils';
+
+type FormData = z.infer<typeof websiteFormSchema>;
+
+interface DomainManagementProps {
+  controlValues: FormData;
+}
+
+export const DomainManagement: React.FC<DomainManagementProps> = ({
+  controlValues,
+}: DomainManagementProps) => {
+  const { serviceName } = useParams();
+  const { t } = useTranslation(['multisite', 'dashboard']);
+  const hostingService = useGetHostingService(serviceName);
+  const existingDomain = useGetAddDomainExisting(serviceName, true, Boolean(serviceName));
+
+  return (
+    <div className="flex flex-col space-y-5">
+      <OdsDivider />
+      <OdsText preset={ODS_TEXT_PRESET.heading4}>
+        {t('multisite:multisite_add_website_domain_management')}
+      </OdsText>
+      <OdsText>{t('multisite:multisite_add_website_domain_management_info')}</OdsText>
+      <OdsText preset={ODS_TEXT_PRESET.heading6}>
+        {t('multisite:multisite_add_website_domain_management_insert_field', {
+          domain: `ovhcontrol.${controlValues?.fqdn}`,
+        })}
+      </OdsText>
+      <OdsClipboard value={existingDomain?.data?.token} />
+      <OdsText preset={ODS_TEXT_PRESET.heading6}>
+        {t('multisite:multisite_add_website_domain_management_point_field', {
+          field: 'A',
+          domain: controlValues?.fqdn,
+        })}
+      </OdsText>
+      <OdsClipboard value={hostingService?.data?.hostingIp} />
+      {controlValues.wwwNeeded && (
+        <>
+          <OdsText preset={ODS_TEXT_PRESET.heading6}>
+            {t('multisite:multisite_add_website_domain_management_point_field', {
+              field: 'A',
+              domain: `www.${controlValues?.fqdn}`,
+            })}
+          </OdsText>
+          <OdsClipboard value={hostingService?.data?.hostingIp} />
+        </>
+      )}
+      <OdsText preset={ODS_TEXT_PRESET.heading6}>
+        {t('multisite:multisite_add_website_domain_management_point_field', {
+          field: 'AAAA',
+          domain: controlValues?.fqdn,
+        })}
+      </OdsText>
+      <OdsClipboard value={hostingService?.data?.hostingIpv6} />
+      {controlValues.wwwNeeded && (
+        <>
+          <OdsText preset={ODS_TEXT_PRESET.heading6}>
+            {t('multisite:multisite_add_website_domain_management_point_field', {
+              field: 'AAAA',
+              domain: `www.${controlValues?.fqdn}`,
+            })}
+          </OdsText>
+          <OdsClipboard value={hostingService?.data?.hostingIpv6} />
+        </>
+      )}
+      <OdsMessage color={ODS_MESSAGE_COLOR.warning} isDismissible={false}>
+        {t('multisite:multisite_add_website_domain_management_propagation')}
+      </OdsMessage>
+    </div>
+  );
+};
