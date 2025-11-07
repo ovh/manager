e67fa1d586b7b00393ed969d3a607359c87aac1a
aTurmo <adrien.turmo.ext@ovhcloud.com>
2025-11-07 15:19:48 +0100

    feat(pci-block-storage): disable retyping for encrypted volumes
    ref: #TAPC-5213
    
    Signed-off-by: aTurmo <adrien.turmo.ext@ovhcloud.com>

--- packages/manager/apps/pci-block-storage/src/api/hooks/useCatalogWithPreselection.spec.tsx
+++ packages/manager/apps/pci-block-storage/src/api/hooks/useCatalogWithPreselection.spec.tsx
@@ -7,27 +7,16 @@ import {
   TVolumeRetypeModel,
   useCatalogWithPreselection,
 } from '@/api/hooks/useCatalogWithPreselection';
-import { is3az, mapRetypingVolumeCatalog } from '@/api/select/catalog';
-import {
-  EncryptionType,
-  getEncryption,
-  isClassicMultiAttach,
-} from '@/api/select/volume';
-import { TRegion } from '@/api/data/regions';
+import { mapRetypingVolumeCatalog } from '@/api/select/catalog';
+import { canRetype } from '@/api/select/volume';
 
 vi.mock('@tanstack/react-query', () => ({
   useQueries: vi.fn(),
 }));
 
-vi.mock('@/api/select/volume', async (importActual) => {
-  const actual = await importActual<typeof import('axios')>();
-
-  return {
-    ...actual,
-    getEncryption: vi.fn(),
-    isClassicMultiAttach: vi.fn(),
-  };
-});
+vi.mock('@/api/select/volume', () => ({
+  canRetype: vi.fn(),
+}));
 
 vi.mock('@/api/select/catalog', () => ({
   mapRetypingVolumeCatalog: vi.fn(),
@@ -49,111 +38,56 @@ vi.mock('@ovh-ux/manager-react-components', () => ({
 const PROJECT_ID = '123';
 const VOLUME_ID = '1';
 
+const volumeMock = {
+  region: 'regionName',
+} as TAPIVolume;
+
+const mockCatalog = {
+  regions: [{ name: 'regionName' }],
+} as TVolumeCatalog;
+
+const mockMappedCatalog = [{ name: 'volumeName' }] as TVolumeRetypeModel[];
+const noData = [];
+
 describe('useCatalogWithPreselection', () => {
-  it.each(['region', 'localzone', 'region-3-az'])(
-    'returns the mapped volume data, the encryption type and pending to false, given the volume is not multi-attached and region is "%s"',
-    async (non3azRegionType: TRegion['type']) => {
-      const notMultiAttachedVolumeMock = {
-        region: 'volume region',
-        type: 'type',
-      } as TAPIVolume;
-
-      const mockCatalog = {
-        regions: [
-          { name: notMultiAttachedVolumeMock.region, type: non3azRegionType },
-        ],
-      } as TVolumeCatalog;
-      const mockMappedCatalog = [{}] as TVolumeRetypeModel[];
-
-      vi.mocked(useQueries).mockReturnValue([
-        { data: notMultiAttachedVolumeMock, isPending: false },
-        { data: mockCatalog, isPending: false },
-      ]);
-
-      vi.mocked(mapRetypingVolumeCatalog).mockReturnValue(
-        () => mockMappedCatalog,
-      );
-      vi.mocked(getEncryption).mockReturnValue(() => ({
-        encrypted: true,
-        encryptionType: EncryptionType.OMK,
-      }));
-
-      const { result } = renderHook(() =>
-        useCatalogWithPreselection(PROJECT_ID, VOLUME_ID),
-      );
-
-      expect(result.current.data).toEqual(mockMappedCatalog);
-      expect(result.current.isPending).toBe(false);
-    },
-  );
-
-  it.each(['region', 'localzone'])(
-    'returns the mapped volume data, the encryption type and pending to false, given the volume is multi-attached and region is "%s"',
-    async (non3azRegionType: TRegion['type']) => {
-      const multiAttchedVolumeMock = {
-        region: 'volume region',
-        type: 'classic-multiattach',
-      } as TAPIVolume;
-
-      const mockCatalog = {
-        regions: [
-          { name: multiAttchedVolumeMock.region, type: non3azRegionType },
-        ],
-      } as TVolumeCatalog;
-      const mockMappedCatalog = [{}] as TVolumeRetypeModel[];
-
-      vi.mocked(useQueries).mockReturnValue([
-        { data: multiAttchedVolumeMock, isPending: false },
-        { data: mockCatalog, isPending: false },
-      ]);
-
-      vi.mocked(mapRetypingVolumeCatalog).mockReturnValue(
-        () => mockMappedCatalog,
-      );
-      vi.mocked(getEncryption).mockReturnValue(() => ({
-        encrypted: true,
-        encryptionType: EncryptionType.OMK,
-      }));
-
-      const { result } = renderHook(() =>
-        useCatalogWithPreselection(PROJECT_ID, VOLUME_ID),
-      );
-
-      expect(result.current.data).toEqual(mockMappedCatalog);
-      expect(result.current.isPending).toBe(false);
-    },
-  );
-
-  it('should return empty array, null encryption and pending to false, given the volume is multi-attach and region is "region-3-az"', () => {
-    const volumeMock = {
-      region: 'volume region',
-      type: 'classic-multiattach',
-    } as TAPIVolume;
-
-    const mockCatalog = {
-      regions: [{ name: volumeMock.region, type: 'region-3-az' }],
-    } as TVolumeCatalog;
-
-    vi.mocked(useQueries).mockReturnValue([
-      { data: volumeMock, isPending: false },
-      { data: mockCatalog, isPending: false },
-    ]);
-    vi.mocked(is3az).mockReturnValue(true);
-    vi.mocked(isClassicMultiAttach).mockReturnValue(true);
-
-    const { result } = renderHook(() =>
-      useCatalogWithPreselection(PROJECT_ID, VOLUME_ID),
+  beforeEach(() => {
+    vi.mocked(mapRetypingVolumeCatalog).mockReturnValue(
+      () => mockMappedCatalog,
     );
-
-    expect(result.current.data).toEqual([]);
-    expect(result.current.isPending).toBe(false);
   });
 
-  it('should return no data and isPending to true if volume query is pending', () => {
-    const mockCatalog = {} as TVolumeCatalog;
+  it.each`
+    canVolumeRetype | expectedData
+    ${true}         | ${mockMappedCatalog}
+    ${false}        | ${noData}
+  `(
+    'should return $expectedData when volume can retype: $canVolumeRetype',
+    ({
+      canVolumeRetype,
+      expectedData,
+    }: {
+      canVolumeRetype: boolean;
+      expectedData: TVolumeRetypeModel[];
+    }) => {
+      vi.mocked(useQueries).mockReturnValue([
+        { data: volumeMock, isPending: false },
+        { data: mockCatalog, isPending: false },
+      ]);
 
+      vi.mocked(canRetype).mockReturnValue(() => canVolumeRetype);
+
+      const { result } = renderHook(() =>
+        useCatalogWithPreselection(PROJECT_ID, VOLUME_ID),
+      );
+
+      expect(result.current.data).toEqual(expectedData);
+      expect(result.current.isPending).toBe(false);
+    },
+  );
+
+  it('should return no data and isPending to true if volume query is pending', () => {
     vi.mocked(useQueries).mockReturnValue([
-      { isPending: true },
+      { data: null, isPending: true },
       { data: mockCatalog, isPending: false },
     ]);
 
@@ -166,8 +100,6 @@ describe('useCatalogWithPreselection', () => {
   });
 
   it('should return no data and isPending to true if catalog query is pending', () => {
-    const volumeMock = {} as TAPIVolume;
-
     vi.mocked(useQueries).mockReturnValue([
       { data: volumeMock, isPending: false },
       { data: null, isPending: true },
