ee7dcace8059dc1678d938a831d003bbb2328967
Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
2025-11-10 09:40:05 +0100

    feat(communication): add routing delete modal
    ref: #MANAGER-18776
    
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>

--- packages/manager/apps/communication/src/pages/settings/settings.constants.ts
+++ packages/manager/apps/communication/src/pages/settings/settings.constants.ts
@@ -12,11 +12,11 @@ export const displayActionMenuItem = (
   action: NotificationRoutingActions,
 ): boolean => {
   switch (action) {
-    case NotificationRoutingActions.EDIT:
     case NotificationRoutingActions.DISABLE:
       return routing.active;
     case NotificationRoutingActions.ENABLE:
       return !routing.active;
+    case NotificationRoutingActions.EDIT:
     case NotificationRoutingActions.DELETE:
       return true;
     default:
