fc4d29323087edda1606141e0d516a9ea94a137d
Kylian Bainvel <kylian.bainvel@ovhcloud.com>
2025-11-10 17:02:56 +0100

    test
    Signed-off-by: Kylian Bainvel <kylian.bainvel@ovhcloud.com>

--- packages/manager/apps/web-domains/src/alldoms/components/allDomDatagridColumns/DatagridColumnServiceName.tsx
+++ packages/manager/apps/web-domains/src/alldoms/components/allDomDatagridColumns/DatagridColumnServiceName.tsx
@@ -2,7 +2,8 @@ import { useNavigationGetUrl } from '@ovh-ux/manager-react-shell-client';
 import { DataGridTextCell } from '@ovh-ux/manager-react-components';
 import React from 'react';
 import { toUnicode } from 'punycode';
-import { Link } from '@ovhcloud/ods-react';
+import { Link as ODSLink } from '@ovhcloud/ods-react';
+import { Link } from 'react-router-dom';
 import config from '@/web-domains.config';
 
 interface DatagridColumnServiceNameProps {
@@ -12,16 +13,13 @@ interface DatagridColumnServiceNameProps {
 export default function DatagridColumnServiceName({
   allDomName,
 }: DatagridColumnServiceNameProps) {
-  const { data: url } = useNavigationGetUrl([
-    config.rootLabel,
-    `/alldoms/${allDomName}`,
-    {},
-  ]);
+  const path = `/alldoms/${allDomName}`;
+  const { data: url } = useNavigationGetUrl([config.rootLabel, path, {}]);
 
   return (
     <DataGridTextCell>
-      <Link href={url as string} data-testid={allDomName}>
-        {toUnicode(allDomName)}
+      <Link to={path} data-testid={allDomName}>
+        <ODSLink href={url}>{toUnicode(allDomName)}</ODSLink>
       </Link>
     </DataGridTextCell>
   );
