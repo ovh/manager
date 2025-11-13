3cbd34f5e2f0952670ffa7f1fa5f7d0f8a5dc868
Mehdi Zaghouani <mehdi.zaghouani@corp.ovh.com>
2025-11-13 16:10:08 +0100

    feat(account-migration): clean up account migration modules
    ref: #MANAGER-14290
    Signed-off-by: Mehdi Zaghouani <mehdi.zaghouani@corp.ovh.com>

--- packages/manager/modules/account-migration/rollup.config.js
+++
@@ -1,9 +0,0 @@
-import rollupConfig from '@ovh-ux/component-rollup-config';
-
-const config = rollupConfig({
-  input: 'src/index.js',
-});
-
-const outputs = [config.es()];
-
-export default outputs;
