3118389f5502cfe31c46096b5e86b76bf369c110
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-11-13 10:50:02 +0100

    feat(ips): track move ip task status
    ref: #MANAGER-20129
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager/apps/ips/src/data/hooks/ip/useMoveIpService.ts
+++ packages/manager/apps/ips/src/data/hooks/ip/useMoveIpService.ts
@@ -1,19 +1,131 @@
 import { useCallback } from 'react';
 import {
-  useQueries,
   useQuery,
   useMutation,
   useQueryClient,
+  Query,
+  useQueries,
 } from '@tanstack/react-query';
-import { ApiError } from '@ovh-ux/manager-core-api';
+import { ApiError, ApiResponse } from '@ovh-ux/manager-core-api';
+import { useNotifications } from '@ovh-ux/manager-react-components';
+import { useTranslation } from 'react-i18next';
 import {
   getMoveIpAvailableDestinations,
   getMoveIpAvailableDestinationsQueryKey,
   getIpTaskList,
   postMoveIp as apiPostMoveIp,
-  getIpTaskQueryKey,
+  getIpDetailsQueryKey,
+  getIpTaskDetailsQueryKey,
+  getIpTaskDetails,
 } from '@/data/api';
-import { IpTaskStatus, IpTaskFunction } from '@/types';
+import { IpTaskStatus, IpTaskFunction, IpTask } from '@/types';
+import { INVALIDATED_REFRESH_PERIOD, TRANSLATION_NAMESPACES } from '@/utils';
+
+const getMoveIpOngoingTasksQueryKey = (ip: string) => [
+  'ipMoveOngoingTasks',
+  ip.includes('/') ? ip : `${ip}/32`,
+];
+
+export function useMoveIpTasks({
+  ip,
+  enabled,
+}: {
+  ip: string;
+  enabled?: boolean;
+}) {
+  const queryClient = useQueryClient();
+  const { t } = useTranslation([
+    TRANSLATION_NAMESPACES.ips,
+    TRANSLATION_NAMESPACES.moveIp,
+  ]);
+  const { clearNotifications, addError, addSuccess } = useNotifications();
+
+  const taskQuery = useQuery<number[], ApiError>({
+    queryKey: getMoveIpOngoingTasksQueryKey(ip),
+    queryFn: async () => {
+      try {
+        const queries = await Promise.all(
+          [IpTaskStatus.init, IpTaskStatus.todo, IpTaskStatus.doing].map(
+            (status) =>
+              getIpTaskList({
+                ip,
+                status,
+                fn: IpTaskFunction.genericMoveFloatingIp,
+              }),
+          ),
+        );
+        return queries.flatMap((query) => query.data);
+      } catch (error) {
+        return [];
+      }
+    },
+    enabled,
+  });
+
+  useQueries({
+    queries: (taskQuery?.data ?? []).map((taskId) => ({
+      queryKey: getIpTaskDetailsQueryKey({ ip, taskId }),
+      queryFn: () => getIpTaskDetails({ ip, taskId }),
+      refetchInterval: (query: Query<ApiResponse<IpTask>, ApiError>) => {
+        if (
+          !query.state.error &&
+          [IpTaskStatus.init, IpTaskStatus.todo, IpTaskStatus.doing].includes(
+            query.state.data?.data?.status,
+          )
+        ) {
+          return INVALIDATED_REFRESH_PERIOD;
+        }
+
+        if (
+          [
+            IpTaskStatus.customError,
+            IpTaskStatus.ovhError,
+            IpTaskStatus.cancelled,
+          ].includes(query.state.data?.data?.status)
+        ) {
+          clearNotifications();
+          addError(
+            t('task_error_message', {
+              status: t(`ip_task_status_${query.state.data?.data?.status}`),
+              comment: query.state.data?.data?.comment,
+            }),
+            true,
+          );
+        }
+
+        if (query.state.data?.data?.status === IpTaskStatus.done) {
+          clearNotifications();
+          addSuccess(
+            t('moveIpDoneMessage', {
+              ip,
+              ns: TRANSLATION_NAMESPACES.moveIp,
+            }),
+          );
+        }
+
+        queryClient.setQueryData(
+          getMoveIpOngoingTasksQueryKey(ip),
+          (oldData: number[]) =>
+            !oldData
+              ? oldData
+              : oldData.filter((id) => id !== query.state.data?.data?.taskId),
+        );
+
+        queryClient.invalidateQueries({
+          queryKey: getIpDetailsQueryKey({ ip }),
+        });
+
+        return undefined;
+      },
+    })),
+  });
+
+  return {
+    isTasksLoading: taskQuery.isLoading,
+    taskError: taskQuery.error as ApiError,
+    hasOnGoingMoveIpTask: taskQuery?.data?.length > 0,
+  };
+}
 
 export function useMoveIpService({
   ip,
@@ -23,30 +135,9 @@ export function useMoveIpService({
   onMoveIpSuccess?: () => void;
 }) {
   const queryClient = useQueryClient();
-
-  const taskQueries = useQueries({
-    queries: [IpTaskStatus.init, IpTaskStatus.todo, IpTaskStatus.doing].map(
-      (status) => ({
-        queryKey: getIpTaskQueryKey({
-          ip,
-          status,
-          fn: IpTaskFunction.genericMoveFloatingIp,
-        }),
-        queryFn: () =>
-          getIpTaskList({
-            ip,
-            status,
-            fn: IpTaskFunction.genericMoveFloatingIp,
-          }),
-      }),
-    ),
+  const { hasOnGoingMoveIpTask, isTasksLoading, taskError } = useMoveIpTasks({
+    ip,
   });
-
-  const isTasksLoading = taskQueries.some((query) => query.isLoading);
-  const taskError = taskQueries.find((query) => query.isError)?.error;
-  const hasOnGoingMoveIpTask = taskQueries.some(
-    (query) => query.data?.data && query.data.data.length > 0,
-  );
 
   const {
     data: availableDestinations,
@@ -91,16 +182,10 @@ export function useMoveIpService({
     error: moveIpError,
   } = useMutation({
     mutationFn: apiPostMoveIp,
-    onSuccess: () => {
-      [IpTaskStatus.init, IpTaskStatus.todo, IpTaskStatus.doing].map((status) =>
-        queryClient.invalidateQueries({
-          queryKey: getIpTaskQueryKey({
-            ip,
-            fn: IpTaskFunction.genericMoveFloatingIp,
-            status,
-          }),
-        }),
-      );
+    onSuccess: async () => {
+      queryClient.invalidateQueries({
+        queryKey: getMoveIpOngoingTasksQueryKey(ip),
+      });
       onMoveIpSuccess?.();
     },
   });
