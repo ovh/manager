cace6486854afad29031d46598fd60314f8fb029
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-11-13 16:13:22 +0100

    feat(okms): add guides utils
    ref: #MANAGER-19143
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

---
+++ packages/manager/apps/okms/src/common/utils/guides/useGuideLink.spec.ts
@@ -0,0 +1,51 @@
+import { vi } from 'vitest';
+import { Subsidiary, User } from '@ovh-ux/manager-config';
+import { renderHook } from '@testing-library/react';
+import { ShellContextType } from '@ovh-ux/manager-react-shell-client';
+import { useGuideLink } from './useGuideLink';
+import { useShellContext } from '@/common/hooks/useShellContext';
+
+const mockedGuides = { FR: 'fr-link', GB: 'gb-link' };
+
+vi.mock('@/common/hooks/useShellContext', () => ({
+  useShellContext: vi.fn(),
+}));
+
+type TestCases = {
+  subsidiary: Subsidiary;
+  expectedLink: string;
+};
+
+const testCases: TestCases[] = [
+  { subsidiary: 'FR', expectedLink: mockedGuides.FR },
+  { subsidiary: 'AU', expectedLink: mockedGuides.GB },
+];
+
+describe('useGuideLink test suite', () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  test.each(testCases)(
+    'should return $expectedLink for subsidiary $subsidiary',
+    ({ subsidiary, expectedLink }) => {
+      // GIVEN
+      vi.mocked(useShellContext).mockReturnValue(({
+        environment: {
+          getUser: vi.fn(
+            () =>
+              ({
+                ovhSubsidiary: subsidiary,
+              } as User),
+          ),
+        },
+      } as unknown) as ShellContextType);
+
+      // WHEN
+      const { result } = renderHook(() => useGuideLink(mockedGuides));
+
+      // THEN
+      expect(result.current).toBe(expectedLink);
+    },
+  );
+});
