5c2ed9a74ec3680681c99881b7ad6249d2c9bb0c
b-fritz-ovh <benjamin.fritz.ext@corp.ovh.com>
2025-11-13 11:22:01 +0100

    feat(dedicated): add volume replications tab
    ref: #MANAGER-18825
    
    Signed-off-by: b-fritz-ovh <benjamin.fritz.ext@corp.ovh.com>

---
+++ packages/manager/modules/netapp/src/dashboard/replications/component.js
@@ -0,0 +1,14 @@
+import controller from './controller';
+import template from './template.html';
+
+export default {
+  bindings: {
+    replications: '<',
+    sourceEFSNames: '<',
+    goToVolumeDetails: '<',
+    goToService: '<',
+    serviceName: '<',
+  },
+  controller,
+  template,
+};
