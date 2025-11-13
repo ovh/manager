ca68fe71c21390e6fd3108c278a4ea1ca883050e
b-fritz-ovh <benjamin.fritz.ext@corp.ovh.com>
2025-11-13 11:22:01 +0100

    feat(dedicated): add volume replications tab
    ref: #MANAGER-18825
    
    Signed-off-by: b-fritz-ovh <benjamin.fritz.ext@corp.ovh.com>

---
+++ packages/manager/modules/netapp/src/dashboard/replications/constants.js
@@ -0,0 +1,10 @@
+export const REPLICATIONS_STATUS = {
+  ONGOING: 'ongoing',
+  COMPLETED: 'completed',
+};
+
+export const ACCEPTED_REPLICATION_STATE = {
+  ERROR: 'acceptedReplicationStateError',
+  IN_SYNC: 'acceptedReplicationStateInSync',
+  OUT_OF_SYNC: 'acceptedReplicationStateOutOfSync',
+};
