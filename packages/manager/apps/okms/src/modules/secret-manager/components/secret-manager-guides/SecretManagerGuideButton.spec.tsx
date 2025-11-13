87c640c0ed686c86505eb6e2c8c6fc2c6c5f8f95
Romain Jamet <romain.jamet.ext@corp.ovh.com>
2025-11-13 16:17:10 +0100

    feat(okms): add secret-manager guides
    ref: #MANAGER-19143
    
    Signed-off-by: Romain Jamet <romain.jamet.ext@corp.ovh.com>

---
+++ packages/manager/apps/okms/src/modules/secret-manager/components/secret-manager-guides/SecretManagerGuideButton.spec.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { vi } from 'vitest';
+import { screen } from '@testing-library/react';
+import { renderWithI18n } from '@/common/utils/tests/testUtils';
+import { SecretManagerGuidesButton } from './SecretManagerGuideButton';
+
+const GUIDES_TEST_IDS = {
+  manager: 'manager',
+  kv2Api: 'kv2-api',
+  restApi: 'rest-api',
+};
+
+vi.mock('./guide-manager/useGuideItemManager', () => ({
+  useGuideItemManager: vi.fn(() => ({
+    id: 0,
+    dataTestid: GUIDES_TEST_IDS.manager,
+  })),
+}));
+
+vi.mock('./guide-KV2-api/useGuideItemKV2Api', () => ({
+  useGuideItemKV2Api: vi.fn(() => ({
+    id: 1,
+    dataTestid: GUIDES_TEST_IDS.kv2Api,
+  })),
+}));
+
+vi.mock('./guide-rest-api/useGuideItemRestApi', () => ({
+  useGuideItemRestApi: vi.fn(() => ({
+    id: 2,
+    dataTestid: GUIDES_TEST_IDS.restApi,
+  })),
+}));
+
+describe('Secret Manager Guide Button test suite', () => {
+  it('should display guides items', async () => {
+    // GIVEN
+    // WHEN
+    await renderWithI18n(<SecretManagerGuidesButton />);
+
+    // THEN
+    expect(screen.getByTestId(GUIDES_TEST_IDS.manager)).toBeInTheDocument();
+    expect(screen.getByTestId(GUIDES_TEST_IDS.kv2Api)).toBeInTheDocument();
+    expect(screen.getByTestId(GUIDES_TEST_IDS.restApi)).toBeInTheDocument();
+  });
+});
