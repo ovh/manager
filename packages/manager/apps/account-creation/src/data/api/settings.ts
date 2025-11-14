ac7d2c4fa37e1ad2bd09bed0171b02cee7aec24c
achraf hafedh <achraf.hafedh.ext@ovhcloud.com>
2025-11-14 16:00:25 +0100

    feat(account-creation): remove mock and link api for settings page
    ref: #MANAGER-19878
    
    Signed-off-by: achraf hafedh <achraf.hafedh.ext@ovhcloud.com>

--- packages/manager/apps/account-creation/src/data/api/settings.ts
+++ packages/manager/apps/account-creation/src/data/api/settings.ts
@@ -1,16 +1,11 @@
 import { Region } from '@ovh-ux/manager-config';
-import { v2 } from '@ovh-ux/manager-core-api';
+import { v6 } from '@ovh-ux/manager-core-api';
 import { Setting } from '@/types/settings';
-import {
-  MockedCASettings,
-  MockedEUSettings,
-  MockedUSSettings,
-} from '../mocks/settings.mock';
 
 export const getSettings = async (region: Region) => {
-  // const { data } = await v2.get<Setting[]>(`/account/settings?ovhIS=${region}`);
-  console.log(region);
-  const data = MockedEUSettings;
+  const { data } = await v6.get<Setting[]>(
+    `/newAccount/residentialCountry?ovhIS=${region}`,
+  );
   const result = new Map();
   data.forEach((setting) => {
     if (setting.billingCountries?.length) {
