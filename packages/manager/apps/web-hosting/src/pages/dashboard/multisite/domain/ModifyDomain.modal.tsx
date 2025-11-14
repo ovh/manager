ad85050da1de7d1bd2cb55fa7c644208198bcb82
stif59100 <steeve.vanderstocken@ovhcloud.com>
2025-11-14 10:18:59 +0100

    feat(web-hosting): add modify domain modal
    ref: #PUWEBPT-63
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

--- packages/manager/apps/web-hosting/src/pages/dashboard/multisite/domain/ModifyDomain.modal.tsx
+++ packages/manager/apps/web-hosting/src/pages/dashboard/multisite/domain/ModifyDomain.modal.tsx
@@ -1,15 +1,160 @@
-import { useNavigate } from 'react-router-dom';
+import { useCallback, useState } from 'react';
 
+import { Location, useLocation, useNavigate } from 'react-router-dom';
+
+import { useMutation } from '@tanstack/react-query';
+import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
-import { Modal } from '@ovh-ux/manager-react-components';
+import { ODS_TEXT_PRESET } from '@ovhcloud/ods-components';
+import { OdsText } from '@ovhcloud/ods-components/react';
+
+import { NAMESPACES } from '@ovh-ux/manager-common-translations';
+import { ApiError } from '@ovh-ux/manager-core-api';
+import { Modal, useNotifications } from '@ovh-ux/manager-react-components';
+
+import { putAttachedDomain } from '@/data/api/webHosting';
+import {
+  useGetDomainZone,
+  useGetHostingService,
+} from '@/data/hooks/webHostingDashboard/useWebHostingDashboard';
+import { PostWebHostingAttachedDomainPayload } from '@/data/types/product/webHosting';
+import { GitStatus, ServiceStatus } from '@/data/types/status';
+
+import Step1 from './ModifyDomainSteps/step1';
+import Step2 from './ModifyDomainSteps/step2';
+import { FormValues } from './ModifyDomainSteps/types';
+
+interface ModifyDomainState {
+  serviceName?: string;
+  domain?: string;
+  path?: string;
+  gitStatus?: GitStatus;
+  firewallStatus?: ServiceStatus;
+  cdnStatus?: ServiceStatus;
+}
 
 export default function ModifyModalDomain() {
   const navigate = useNavigate();
-  const { t } = useTranslation('common');
+  const { t } = useTranslation([
+    'common',
+    'multisite',
+    NAMESPACES.ACTIONS,
+    NAMESPACES.COUNTRIES,
+    NAMESPACES.ERROR,
+  ]);
+  const [step, setStep] = useState(1);
+  const { state } = useLocation() as Location<ModifyDomainState>;
+  const { data: hosting } = useGetHostingService(state.serviceName);
+  const { data: zones = [] } = useGetDomainZone();
+  const isGitDisabled = state.gitStatus === GitStatus.DISABLED;
+  const isCdnAvailable = state.cdnStatus !== ServiceStatus.NONE;
+  const { control, watch, handleSubmit } = useForm<FormValues>({
+    defaultValues: {
+      domain: state.domain ?? '',
+      path: state.path ?? '',
+      cdn: state.cdnStatus ?? ServiceStatus.INACTIVE,
+      firewall: state.firewallStatus ?? ServiceStatus.INACTIVE,
+      countriesIpEnabled: false,
+      enableOwnLog: false,
+      ownLog: '',
+    },
+  });
+  const { addError, addSuccess } = useNotifications();
+  const onClose = () => navigate(-1);
 
-  const onClose = () => {
-    navigate(-1);
-  };
-  return <Modal heading={t('modify_domain')} onDismiss={onClose} isOpen></Modal>;
+  const { mutate: onUpdateDomain } = useMutation<
+    void,
+    ApiError,
+    PostWebHostingAttachedDomainPayload
+  >({
+    mutationFn: async (payload) => {
+      try {
+        await putAttachedDomain(state.serviceName, state.domain, payload);
+      } catch (error) {
+        console.error('Error in mutation:', error);
+        throw error;
+      }
+    },
+    onSuccess: () => {
+      addSuccess(
+        <OdsText preset={ODS_TEXT_PRESET.paragraph}>
+          {t('multisite:multisite_modal_domain_configuration_modify_success')}
+        </OdsText>,
+        true,
+      );
+    },
+    onError: (error: ApiError) => {
+      addError(
+        <OdsText preset={ODS_TEXT_PRESET.paragraph}>
+          {t(`multisite:multisite_modal_domain_configuration_modify_failure`, {
+            message: error?.response?.data?.message,
+          })}
+        </OdsText>,
+        true,
+      );
+    },
+    onSettled: () => {
+      onClose();
+    },
+  });
+
+  const onSubmitForm = useCallback(
+    (values: FormValues) => {
+      const payload: PostWebHostingAttachedDomainPayload = {
+        bypassDNSConfiguration: false,
+        cdn: values.cdn.toLowerCase() as Lowercase<ServiceStatus>,
+        domain: values.domain,
+        firewall: values.firewall.toLowerCase() as Lowercase<ServiceStatus>,
+        ...(values.ipLocation !== undefined && { ipLocation: values.ipLocation }),
+        ownLog: values.ownLog,
+        path: values.path,
+        ssl: hosting?.hasHostedSsl ?? false,
+      };
+      onUpdateDomain(payload);
+    },
+    [hosting?.hasHostedSsl, onUpdateDomain],
+  );
+
+  const onPrimaryButtonClick = useCallback(() => {
+    if (step === 1) {
+      setStep(2);
+      return;
+    }
+    handleSubmit(onSubmitForm)().catch(console.error);
+  }, [step, handleSubmit, onSubmitForm]);
+
+  return (
+    <Modal
+      heading={t('modify_domain')}
+      onDismiss={onClose}
+      isOpen
+      primaryLabel={
+        step === 1 ? t(`${NAMESPACES.ACTIONS}:next`) : t(`${NAMESPACES.ACTIONS}:validate`)
+      }
+      secondaryLabel={
+        step === 1 ? t(`${NAMESPACES.ACTIONS}:cancel`) : t(`${NAMESPACES.ACTIONS}:previous`)
+      }
+      onPrimaryButtonClick={onPrimaryButtonClick}
+      onSecondaryButtonClick={() => {
+        if (step === 2) {
+          setStep(1);
+        } else {
+          onClose();
+        }
+      }}
+      isPrimaryButtonDisabled={!isGitDisabled}
+    >
+      {step === 1 && (
+        <Step1
+          control={control}
+          isGitDisabled={isGitDisabled}
+          isCdnAvailable={isCdnAvailable}
+          hosting={hosting}
+          zones={zones}
+        />
+      )}
+      {step === 2 && <Step2 watch={watch} hosting={hosting} />}
+    </Modal>
+  );
 }
