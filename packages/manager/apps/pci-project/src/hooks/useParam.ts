d5f3d8b62b1455ed4c67962a28c09c975160744c
Fabien Henon <fabien.henon.ext@corp.ovh.com>
2025-11-14 16:45:15 +0100

    fix(pci-project): activate a project
    ref: #MANAGER-20236
    
    Signed-off-by: Fabien Henon <fabien.henon.ext@corp.ovh.com>

--- packages/manager/apps/pci-project/src/hooks/useParam.ts
+++ packages/manager/apps/pci-project/src/hooks/useParam.ts
@@ -1,8 +1,10 @@
 import { useParams } from 'react-router-dom';
 
-export function useParam(param: string): string {
+export function useParam(param: string, strict: true): string;
+export function useParam(param: string, strict?: false): string | undefined;
+export function useParam(param: string, strict = true): string | undefined {
   const { [param]: paramValue } = useParams();
-  if (!paramValue) {
+  if (strict && !paramValue) {
     throw new Error(`Missing ${param} in URL.`);
   }
   return paramValue;
