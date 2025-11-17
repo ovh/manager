f832f2e42c22853f408b8ecdb29cc02dc62c5340
David Arsène <david.arsene.ext@ovhcloud.com>
2025-11-17 11:38:42 +0100

    feat(observability): unmock api tenants
    resolves: #MAOBS-106
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/observability/src/types/infrastructures.type.ts
+++ packages/manager/apps/observability/src/types/infrastructures.type.ts
@@ -5,6 +5,7 @@ import { TIdentifier } from './observability.type';
 export type InfrastructureUsage = 'GRAFANA' | 'LOGS' | 'METRICS';
 export type InfrastructureType = 'DEDICATED' | 'SHARED';
 export type RegionType = 'LOCAL-ZONE' | 'REGION-1-AZ' | 'REGION-3-AZ';
+export type RetentionType = 'LOGS_COLD_STORAGE' | 'LOGS_INDEXING' | 'METRICS_TENANT';
 
 export type Retention = {
   duration: string;
