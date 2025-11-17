e5066a82c6b388b1621e2b225d167fded4be160f
David Arsène <david.arsene.ext@ovhcloud.com>
2025-11-17 11:38:42 +0100

    feat(observability): unmock api tenants
    resolves: #MAOBS-106
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/observability/src/__mocks__/mock.config.ts
+++ packages/manager/apps/observability/src/__mocks__/mock.config.ts
@@ -1,5 +1,11 @@
 export type ApiConfigMode = 'mock' | 'api';
+export type ApiEndpoint = 'tenant' | 'service' | 'infrastructure' | 'grafana' | 'log' | 'metric';
 
-export const apiConfig = {
-  mode: 'mock' as ApiConfigMode,
-} as const;
+export const apiConfig: Record<ApiEndpoint, ApiConfigMode> = {
+  tenant: 'mock',
+  service: 'api',
+  infrastructure: 'mock', // FIXME : fix retentions first before unmocking
+  grafana: 'mock',
+  log: 'mock',
+  metric: 'mock',
+};
