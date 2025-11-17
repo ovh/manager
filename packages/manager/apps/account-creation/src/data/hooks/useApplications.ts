087312d875d2904df051498079fdbee333992efa
Jacques Larique <jacques.larique.ext@ovhcloud.com>
2025-11-17 18:06:15 +0100

    feat(account-creation): redirection post account creation
    ref: #MANAGER-19812
    
    Signed-off-by: Jacques Larique <jacques.larique.ext@ovhcloud.com>
    Co-authored-by: helabenkhalfallah <hela.ben-khalfallah@corp.ovh.com>

--- packages/manager/apps/account-creation/src/data/hooks/useApplications.ts
+++ packages/manager/apps/account-creation/src/data/hooks/useApplications.ts
@@ -1,9 +1,12 @@
-import { useQuery } from '@tanstack/react-query';
-import { getApplications } from '../api/applications';
+import { DefinedInitialDataOptions, useQuery } from '@tanstack/react-query';
 
-export const useApplications = (enabled: boolean) =>
+import { Application } from '@ovh-ux/manager-config';
+
+import { getApplications } from '@/data/api/applications';
+
+export const useApplications = <Response = Record<string, Application>>(options: Partial<DefinedInitialDataOptions<Record<string, Application>, Error, Response>> = {},) =>
   useQuery({
+    ...options,
     queryKey: ['applications'],
     queryFn: getApplications,
-    enabled,
   });
