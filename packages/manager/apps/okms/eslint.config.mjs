47da850d96b4c05b0637d5fd3d05553e1e9fe040
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-11-19 16:39:15 +0100

    chore(okms): apply htmlEslintConfig rules
    ref: #MANAGER-19870
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/okms/eslint.config.mjs
+++ packages/manager/apps/okms/eslint.config.mjs
@@ -1,22 +1,16 @@
 /*
-## Full adoption ##
-import { eslintSharedConfig } from '@ovh-ux/manager-static-analysis-kit';
-
-export default eslintSharedConfig;
-
-
-## Progressive adoption ##
+## Remaining rules ##
 
 import {
   complexityJsxTsxConfig,
   complexityTsJsConfig,
 } from '@ovh-ux/manager-static-analysis-kit/eslint/complexity';
 import { cssEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/css';
-import { htmlEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/html';
-import { importEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/imports';
 import { checkFileEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/naming-conventions';
+import { importEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/imports';
 */
 import { a11yEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/a11y';
+import { htmlEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/html';
 import { javascriptEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/javascript';
 import { prettierEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/prettier';
 import { reactEslintConfig } from '@ovh-ux/manager-static-analysis-kit/eslint/react';
@@ -31,6 +25,7 @@ export default [
   reactEslintConfig,
   prettierEslintConfig,
   tanStackQueryEslintConfig,
+  htmlEslintConfig,
   {
     ...a11yEslintConfig,
     ignores: ['**/*.spec.tsx'],
