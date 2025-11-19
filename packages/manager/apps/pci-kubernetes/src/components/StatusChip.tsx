a87fd803a832f656223426ea39dde1699f38f6ad
Pierre-Philippe <ppprevost@aol.fr>
2025-11-19 09:12:14 +0100

    feat(pci-kubernetes): add confirmation floating ip step
    ref: #TAPC-4731
    
    Signed-off-by: Pierre-Philippe <ppprevost@aol.fr>

--- packages/manager/apps/pci-kubernetes/src/components/StatusChip.tsx
+++ packages/manager/apps/pci-kubernetes/src/components/StatusChip.tsx
@@ -4,17 +4,18 @@ import { Badge, BadgeColor, SPINNER_SIZE, Spinner } from '@ovhcloud/ods-react';
 
 import { ResourceStatus } from '@/types';
 
+const colorByProductStatus: Record<ResourceStatus, BadgeColor> = {
+  [ResourceStatus.READY]: 'success',
+  [ResourceStatus.DISABLED]: 'neutral',
+  [ResourceStatus.UPDATING]: 'warning',
+  [ResourceStatus.ERROR]: 'critical',
+  [ResourceStatus.CREATING]: 'information',
+  [ResourceStatus.ENABLED]: 'success',
+  [ResourceStatus.DELETING]: 'critical',
+};
+
 const StatusChip = ({ label }: { label: string }) => {
   const { t } = useTranslation('listing');
-  const colorByProductStatus: Record<ResourceStatus, BadgeColor> = {
-    [ResourceStatus.READY]: 'success',
-    [ResourceStatus.DISABLED]: 'neutral',
-    [ResourceStatus.UPDATING]: 'warning',
-    [ResourceStatus.ERROR]: 'critical',
-    [ResourceStatus.CREATING]: 'information',
-    [ResourceStatus.ENABLED]: 'success',
-    [ResourceStatus.DELETING]: 'critical',
-  };
 
   const isValidResourceStatus = (value: string): value is ResourceStatus => {
     return value in colorByProductStatus;
