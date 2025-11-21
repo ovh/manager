1bc54c9c62e68060fd9e74d664aaed95ebe69895
Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>
2025-11-21 16:18:41 +0100

    feat(ips): move mocks into src/__mocks__
    ref: #MANAGER-18212
    
    Signed-off-by: Nicolas Pierre-charles <nicolas.pierre-charles.ext@corp.ovh.com>

--- packages/manager/apps/ips/src/pages/listing/ipListing/components/DatagridCells/IpType/IpType.spec.tsx
+++ packages/manager/apps/ips/src/pages/listing/ipListing/components/DatagridCells/IpType/IpType.spec.tsx
@@ -3,7 +3,7 @@ import React, { PropsWithChildren } from 'react';
 import { describe, expect, it, vi } from 'vitest';
 import { render, waitFor } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import ipDetailsList from '../../../../../../../mocks/ip/get-ip-details.json';
+import ipDetailsList from '@/__mocks__/ip/get-ip-details.json';
 import { IpType, IpTypeProps } from './IpType';
 import { getOdsBadgeByLabel } from '@/test-utils';
 
