934a8b078ebc822ee4eda66b6da8a5d23ecff09e
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-11-21 17:44:22 +0100

    feat(public-vcf-aas): add ip block count on vcf dashboard
    ref: #MANAGER-19356
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/hpc-vmware-public-vcf-aas/src/components/tiles/organization-general-information-tile/OrganizationGeneralInformationTile.component.tsx
+++ packages/manager/apps/hpc-vmware-public-vcf-aas/src/components/tiles/organization-general-information-tile/OrganizationGeneralInformationTile.component.tsx
@@ -21,6 +21,7 @@ import EditableTileItem from '../editable-tile-item/EditableTileItem.component';
 import DatacentresCount from './DatacentresCount.component';
 import TEST_IDS from '@/utils/testIds.constants';
 import { TRACKING } from '@/tracking.constants';
+import { IpBlockCount } from './IpBlockCount.component';
 
 type TTileProps = {
   vcdOrganization: VCDOrganization;
@@ -122,6 +123,11 @@ export default function OrganizationGenerationInformationTile({
               className="w-full"
             />
           ),
+        },
+        {
+          id: 'ipSpacesCount',
+          label: t('managed_vcd_dashboard_ip_block_count'),
+          value: <IpBlockCount />,
         },
       ]}
     />
--- packages/manager/apps/hpc-vmware-public-vcf-aas/src/utils/labels.constants.ts
+++ packages/manager/apps/hpc-vmware-public-vcf-aas/src/utils/labels.constants.ts
@@ -2,4 +2,6 @@ export const LABELS = {
   vRackNetwork: 'vRack Network',
   network: 'Network',
   vlan: 'VLAN',
+  ipBlockTypeRipe: 'RIPE',
+  ipBlockTypeArin: 'ARIN',
 } as const;
