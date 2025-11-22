f447cb16737840ebd386f5d787920ef03cf00060
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-11-22 18:58:35 +0100

    feat(backup-agent): add add configuration page for agent tenant
    ref: #BKP-652
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/backup-agent/src/data/api/index.ts
+++ packages/manager/modules/backup-agent/src/data/api/index.ts
@@ -2,4 +2,5 @@ export * from './agents/agents.requests';
 export * from './baremetal/baremetals';
 export * from './vaults/vault.requests';
 export * from './tenants/tenants.requests';
+export * from './tenants/backupPolicies.requests';
 export * from './locations/location.requests';
