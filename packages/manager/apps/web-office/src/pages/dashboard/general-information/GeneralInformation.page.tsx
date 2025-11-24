6c6a7bec0520a198c3fd3dc48b8d29ad44a9a4ff
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2025-11-24 16:51:00 +0100

    fix(web-office): migration ods 19 and muk
    ref: #PRDCOL-236
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/apps/web-office/src/pages/dashboard/general-information/GeneralInformation.page.tsx
+++ packages/manager/apps/web-office/src/pages/dashboard/general-information/GeneralInformation.page.tsx
@@ -1,21 +1,21 @@
-import { useMemo } from 'react';
-
 import { Outlet, useNavigate, useParams } from 'react-router-dom';
 
 import { format } from 'date-fns';
 import { useTranslation } from 'react-i18next';
 
 import {
-  ODS_BUTTON_COLOR,
-  ODS_BUTTON_SIZE,
-  ODS_BUTTON_VARIANT,
-  ODS_ICON_NAME,
-  ODS_TEXT_PRESET,
-} from '@ovhcloud/ods-components';
-import { OdsSkeleton, OdsText } from '@ovhcloud/ods-components/react';
+  BUTTON_COLOR,
+  BUTTON_SIZE,
+  BUTTON_VARIANT,
+  ICON_NAME,
+  Icon,
+  Skeleton,
+  TEXT_PRESET,
+  Text,
+} from '@ovhcloud/ods-react';
 
 import { NAMESPACES } from '@ovh-ux/manager-common-translations';
-import { DashboardTile, ManagerButton } from '@ovh-ux/manager-react-components';
+import { Button, GridLayout, Tile } from '@ovh-ux/muk';
 
 import { OfficeServiceState } from '@/components/office-service-state/OfficeServiceState.component';
 import { LicenseEnum } from '@/data/api/ApiType';
@@ -44,85 +44,68 @@ export default function GeneralInformation() {
   const { data: dataUsers, isLoading: isLoadingUsers } = useUsers();
   const { data: dataParentTenant, isLoading: isLoadingParentTenant } = useParentTenant();
 
-  const itemsGeneralInfos = useMemo(() => {
-    return [
-      {
-        id: 'displayName',
-        label: t('common:displayName'),
-        value: (
-          <>
+  return (
+    <GridLayout>
+      <Tile.Root
+        data-testid="general_informations"
+        title={t(`${NAMESPACES.DASHBOARD}:general_information`)}
+      >
+        <Tile.Item.Root>
+          <Tile.Item.Term label={t(`${NAMESPACES.DASHBOARD}:license_number`)} />
+          <Tile.Item.Description>
             {!isLoadingParentTenant && (
-              <div className="flex justify-between items-center gap-2">
-                <OdsText preset={ODS_TEXT_PRESET.paragraph} className="break-all">
+              <div className="flex items-center justify-between gap-2">
+                <Text preset={TEXT_PRESET.paragraph} className="break-all">
                   {dataParentTenant?.displayName}
-                </OdsText>
+                </Text>
                 <div className="min-w-fit">
                   {licenseDetail && licenseDetail && (
-                    <ManagerButton
+                    <Button
                       id="edit-name"
-                      size={ODS_BUTTON_SIZE.sm}
-                      label=""
-                      variant={ODS_BUTTON_VARIANT.ghost}
-                      color={ODS_BUTTON_COLOR.primary}
+                      size={BUTTON_SIZE.sm}
+                      variant={BUTTON_VARIANT.ghost}
+                      color={BUTTON_COLOR.primary}
                       onClick={() => navigate('./edit-name')}
-                      icon={ODS_ICON_NAME.pen}
                       urn={licenseDetail?.iam?.urn}
                       iamActions={[
                         ...(licenseDetail?.tenantServiceName
                           ? [IAM_ACTIONS.licencePrepaid.putParentTenant]
                           : [IAM_ACTIONS.licencePostPaid.edit]),
                       ]}
-                    />
+                    >
+                      <Icon name={ICON_NAME.pen} />
+                    </Button>
                   )}
                 </div>
               </div>
             )}
-            {isLoadingParentTenant && <OdsSkeleton></OdsSkeleton>}
-          </>
-        ),
-      },
-      {
-        id: 'serviceName',
-        label: t('common:serviceName'),
-        value: (
-          <>
+            {isLoadingParentTenant && <Skeleton></Skeleton>}
+          </Tile.Item.Description>
+        </Tile.Item.Root>
+        <Tile.Item.Root>
+          <Tile.Item.Term label={t('common:serviceName')} />
+          <Tile.Item.Description>
             {!isLoadingParentTenant && (
-              <OdsText preset={ODS_TEXT_PRESET.paragraph}>{dataParentTenant?.serviceName}</OdsText>
+              <Text preset={TEXT_PRESET.paragraph}>{dataParentTenant?.serviceName}</Text>
             )}
-            {isLoadingParentTenant && <OdsSkeleton></OdsSkeleton>}
-          </>
-        ),
-      },
-      {
-        id: 'servicetype',
-        label: t(`${NAMESPACES.DASHBOARD}:service_type`),
-        value: (
-          <>
+            {isLoadingParentTenant && <Skeleton></Skeleton>}
+          </Tile.Item.Description>
+        </Tile.Item.Root>
+        <Tile.Item.Root>
+          <Tile.Item.Term label={t('common:serviceName')} />
+          <Tile.Item.Description divider={false}>
             {!isLoadingLicenseDetail && (
-              <OdsText preset={ODS_TEXT_PRESET.paragraph}>{t(`common:${serviceType}`)}</OdsText>
+              <Text preset={TEXT_PRESET.paragraph}>{t(`common:${serviceType}`)}</Text>
             )}
-            {isLoadingLicenseDetail && <OdsSkeleton></OdsSkeleton>}
-          </>
-        ),
-      },
-    ];
-  }, [
-    licenseDetail,
-    serviceType,
-    dataParentTenant,
-    isLoadingLicenseDetail,
-    isLoadingParentTenant,
-    navigate,
-    t,
-  ]);
-
-  const itemsStatistics = useMemo(() => {
-    return [
-      {
-        id: 'license_number',
-        label: t(`${NAMESPACES.DASHBOARD}:license_number`),
-        value: (
-          <div className="flex flex-col">
+            {isLoadingLicenseDetail && <Skeleton></Skeleton>}
+          </Tile.Item.Description>
+        </Tile.Item.Root>
+      </Tile.Root>
+      <Outlet />
+      <Tile.Root data-testid="statistics" title={t(`${NAMESPACES.DASHBOARD}:statistics`)}>
+        <Tile.Item.Root>
+          <Tile.Item.Term label={t(`${NAMESPACES.DASHBOARD}:license_number`)} />
+          <Tile.Item.Description divider={false}>
             {!isLoadingUsers &&
               dataUsers?.length > 0 &&
               dataUsers
@@ -144,99 +127,58 @@ export default function GeneralInformation() {
                   [],
                 )
                 ?.map((stats: { licences: LicenseEnum; number: number }) => (
-                  <OdsText key={stats.licences} preset={ODS_TEXT_PRESET.paragraph}>
+                  <Text key={stats.licences} preset={TEXT_PRESET.paragraph}>
                     <strong>{stats.licences} : </strong>
                     {stats.number}
-                  </OdsText>
+                  </Text>
                 ))}
             {!isLoadingUsers && dataUsers?.length === 0 && (
-              <OdsText preset={ODS_TEXT_PRESET.paragraph}>{t('common:noAccountOffer')}</OdsText>
+              <Text preset={TEXT_PRESET.paragraph}>{t('common:noAccountOffer')}</Text>
             )}
             {isLoadingUsers && (
               <div className="flex flex-col gap-2">
-                <OdsSkeleton></OdsSkeleton>
-                <OdsSkeleton></OdsSkeleton>
+                <Skeleton></Skeleton>
+                <Skeleton></Skeleton>
               </div>
             )}
-          </div>
-        ),
-      },
-    ];
-  }, [dataUsers, isLoadingUsers, t]);
-
-  const itemsBillingInfos = useMemo(() => {
-    return [
-      {
-        id: 'creation_date',
-        label: t(`${NAMESPACES.DASHBOARD}:creation_date`),
-        value: (
-          <>
+          </Tile.Item.Description>
+        </Tile.Item.Root>
+      </Tile.Root>
+      <Tile.Root data-testid="my_offer" title={t(`${NAMESPACES.DASHBOARD}:my_offer`)}>
+        <Tile.Item.Root>
+          <Tile.Item.Term label={t(`${NAMESPACES.DASHBOARD}:creation_date`)} />
+          <Tile.Item.Description>
             {!isLoadingServiceInfos && (
-              <OdsText preset={ODS_TEXT_PRESET.paragraph}>
+              <Text preset={TEXT_PRESET.paragraph}>
                 {serviceInfos?.creation ? format(serviceInfos?.creation, 'PPP', { locale }) : ''}
-              </OdsText>
+              </Text>
             )}
-            {isLoadingServiceInfos && <OdsSkeleton></OdsSkeleton>}
-          </>
-        ),
-      },
-      {
-        id: 'status',
-        label: t(`${NAMESPACES.STATUS}:status`),
-        value: (
-          <>
+            {isLoadingServiceInfos && <Skeleton></Skeleton>}
+          </Tile.Item.Description>
+        </Tile.Item.Root>
+        <Tile.Item.Root>
+          <Tile.Item.Term label={t(`${NAMESPACES.STATUS}:status`)} />
+          <Tile.Item.Description divider={serviceType === 'prepaid'}>
             {!isLoadingServiceInfos && <OfficeServiceState state={serviceInfos?.status} />}
-            {isLoadingServiceInfos && <OdsSkeleton></OdsSkeleton>}
-          </>
-        ),
-      },
-      ...(serviceType === 'prepaid'
-        ? [
-            {
-              id: 'renew_date',
-              label: t(`${NAMESPACES.DASHBOARD}:renew_date`),
-              value: (
-                <>
-                  {!isLoadingServiceInfos && (
-                    <OdsText preset={ODS_TEXT_PRESET.paragraph}>
-                      {serviceInfos?.expiration
-                        ? format(serviceInfos?.expiration, 'PPP', { locale })
-                        : ''}
-                    </OdsText>
-                  )}
-                  {isLoadingServiceInfos && <OdsSkeleton></OdsSkeleton>}
-                </>
-              ),
-            },
-          ]
-        : []),
-    ];
-  }, [serviceInfos, isLoadingServiceInfos, locale, serviceType, t]);
-
-  return (
-    <div className="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-      <div className="p-3">
-        <DashboardTile
-          data-testid="general_informations"
-          title={t(`${NAMESPACES.DASHBOARD}:general_information`)}
-          items={itemsGeneralInfos}
-        />
-        <Outlet />
-      </div>
-      <div className="p-3">
-        <DashboardTile
-          data-testid="statistics"
-          title={t(`${NAMESPACES.DASHBOARD}:statistics`)}
-          items={itemsStatistics}
-        />
-      </div>
-      <div className="p-3">
-        <DashboardTile
-          data-testid="my_offer"
-          title={t(`${NAMESPACES.DASHBOARD}:my_offer`)}
-          items={itemsBillingInfos}
-        />
-      </div>
-    </div>
+            {isLoadingServiceInfos && <Skeleton></Skeleton>}
+          </Tile.Item.Description>
+        </Tile.Item.Root>
+        {serviceType === 'prepaid' && (
+          <Tile.Item.Root>
+            <Tile.Item.Term label={t(`${NAMESPACES.DASHBOARD}:renew_date`)} />
+            <Tile.Item.Description divider={false}>
+              {!isLoadingServiceInfos && (
+                <Text preset={TEXT_PRESET.paragraph}>
+                  {serviceInfos?.expiration
+                    ? format(serviceInfos?.expiration, 'PPP', { locale })
+                    : ''}
+                </Text>
+              )}
+              {isLoadingServiceInfos && <Skeleton></Skeleton>}
+            </Tile.Item.Description>
+          </Tile.Item.Root>
+        )}
+      </Tile.Root>
+    </GridLayout>
   );
 }
