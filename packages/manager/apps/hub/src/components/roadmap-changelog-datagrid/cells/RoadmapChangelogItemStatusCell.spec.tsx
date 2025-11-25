d681f844af5b660b4924d27960987ebcc9bc7e04
achraf hafedh <achraf.hafedh.ext@ovhcloud.com>
2025-11-25 18:08:06 +0100

    feat(hub): add fix and verrify html tests
    ref: #MANAGER-20181
    
    Signed-off-by: achraf hafedh <achraf.hafedh.ext@ovhcloud.com>

---
+++ packages/manager/apps/hub/src/components/roadmap-changelog-datagrid/cells/RoadmapChangelogItemStatusCell.spec.tsx
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+import { roadmapItem } from '@/__mocks__/roadmap';
+
+import { RoadmapChangelogItemStatusCell } from './RoadmapChangelogItemStatusCell';
+
+describe('RoadmapChangelogItemStatusCell Component', () => {
+  it('should have a valid html', () => {
+    const { container } = render(<RoadmapChangelogItemStatusCell item={roadmapItem} />);
+    const html = container.innerHTML;
+
+    expect(html).toBeValidHtml();
+  });
+});
