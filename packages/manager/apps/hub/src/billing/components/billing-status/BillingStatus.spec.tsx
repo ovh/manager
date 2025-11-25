9e6f356a75cf2007d41dda034906e1e1328c970e
achraf hafedh <achraf.hafedh.ext@ovhcloud.com>
2025-11-25 18:08:06 +0100

    feat(hub): add fix and verrify html tests
    ref: #MANAGER-20181
    
    Signed-off-by: achraf hafedh <achraf.hafedh.ext@ovhcloud.com>

---
+++ packages/manager/apps/hub/src/billing/components/billing-status/BillingStatus.spec.tsx
@@ -0,0 +1,45 @@
+import { render } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+import { FourServices } from '@/__mocks__/billingServices';
+
+import BillingStatus from './BillingStatus.component';
+
+describe('BillingStatus W3C Validation', () => {
+  const [
+    serviceResiliated,
+    serviceWithManualRenewNotResiliatedWithoutDebt,
+    serviceOneShotWithoutResiliation,
+    serviceWithoutUrlAndSuspendedBilling,
+  ] = FourServices.services;
+
+  it('should have a valid html for service Resiliated', () => {
+    const { container } = render(<BillingStatus service={serviceResiliated} />);
+    const html = container.innerHTML;
+
+    expect(html).toBeValidHtml();
+  });
+
+  it('should have a valid html for service With Manual Renew Not ResiliatedWithoutDebt', () => {
+    const { container } = render(
+      <BillingStatus service={serviceWithManualRenewNotResiliatedWithoutDebt} />,
+    );
+    const html = container.innerHTML;
+
+    expect(html).toBeValidHtml();
+  });
+
+  it('should have a valid html for service OneSho tWithout Resiliation', () => {
+    const { container } = render(<BillingStatus service={serviceOneShotWithoutResiliation} />);
+    const html = container.innerHTML;
+
+    expect(html).toBeValidHtml();
+  });
+
+  it('should have a valid html for service Without Url And Suspended Billing', () => {
+    const { container } = render(<BillingStatus service={serviceWithoutUrlAndSuspendedBilling} />);
+    const html = container.innerHTML;
+
+    expect(html).toBeValidHtml();
+  });
+});
---
+++ packages/manager/apps/hub/src/components/roadmap-changelog-datagrid/cells/RoadmapChangelogItemTitleCell.spec.tsx
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+import { roadmapItem } from '@/__mocks__/roadmap';
+
+import { RoadmapChangelogItemTitleCell } from './RoadmapChangelogItemTitleCell';
+
+describe('RoadmapChangelogItemTitleCell Component', () => {
+  it('should have a valid html', () => {
+    const { container } = render(<RoadmapChangelogItemTitleCell item={roadmapItem} />);
+    const html = container.innerHTML;
+
+    expect(html).toBeValidHtml();
+  });
+});
