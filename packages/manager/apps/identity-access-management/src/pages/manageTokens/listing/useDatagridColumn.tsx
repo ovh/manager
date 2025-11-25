f2dbe6378bac26405f0701aaa3e781dc9c697400
Marc Monchablon <marc.monchablon.ext@corp.ovh.com>
2025-11-25 18:32:42 +0100

    feat(iam): add "Manage Tokens" listing page
    ref: #MAIAM-45
    
    Signed-off-by: Marc Monchablon <marc.monchablon.ext@corp.ovh.com>

---
+++ packages/manager/apps/identity-access-management/src/pages/manageTokens/listing/useDatagridColumn.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import {
+  DatagridColumn,
+  DataGridTextCell,
+  useFormatDate,
+} from '@ovh-ux/manager-react-components';
+import { FilterTypeCategories } from '@ovh-ux/manager-core-api';
+
+import { IamUserToken } from '@/data/api/iam-users';
+import { Actions } from './Actions.component';
+import { ExpirationStatus } from '@/pages/manageTokens/components/ExpirationStatus.component';
+
+export function useDatagridColumn(): DatagridColumn<IamUserToken>[] {
+  const { t } = useTranslation('manage-tokens');
+  const formatDate = useFormatDate();
+
+  return [
+    {
+      id: 'name',
+      label: t('iam_user_tokens_datagrid_column_name'),
+      cell: (token: IamUserToken) => (
+        <DataGridTextCell>{token.name}</DataGridTextCell>
+      ),
+      type: FilterTypeCategories.String,
+      isSearchable: true,
+      isSortable: true,
+    },
+    {
+      id: 'description',
+      label: t('iam_user_tokens_datagrid_column_description'),
+      cell: (token: IamUserToken) => (
+        <DataGridTextCell>{token.description}</DataGridTextCell>
+      ),
+      type: FilterTypeCategories.String,
+      isSearchable: true,
+      isSortable: true,
+    },
+    {
+      id: 'creation',
+      label: t('iam_user_tokens_datagrid_column_creation'),
+      cell: (token: IamUserToken) => (
+        <DataGridTextCell>
+          {formatDate({ date: token.creation, format: 'P' })}
+        </DataGridTextCell>
+      ),
+      isSortable: true,
+    },
+    {
+      id: 'expiration',
+      label: t('iam_user_tokens_datagrid_column_expiration'),
+      cell: (token: IamUserToken) => (
+        <DataGridTextCell>
+          {formatDate({ date: token.expiresAt, format: 'P' })}
+        </DataGridTextCell>
+      ),
+      isSortable: true,
+    },
+    {
+      id: 'exporationStatus',
+      label: t('iam_user_tokens_datagrid_column_expiration_status'),
+      cell: (token: IamUserToken) => (
+        <DataGridTextCell>
+          <ExpirationStatus token={token} />
+        </DataGridTextCell>
+      ),
+    },
+    {
+      id: 'lastUsed',
+      label: t('iam_user_tokens_datagrid_column_last_usage'),
+      cell: (token: IamUserToken) => (
+        <DataGridTextCell>
+          {formatDate({ date: token.lastUsed, format: 'P' })}
+        </DataGridTextCell>
+      ),
+      isSortable: true,
+    },
+    {
+      id: 'actions',
+      label: '',
+      cell: () => <Actions />,
+    },
+  ];
+}
