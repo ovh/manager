56f2a91ee9b8f20ac104af81e71ee957c5e67ef1
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-11-25 10:29:58 +0100

    feat(backup-agent): add agent details request
    ref: #BKP-656
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/backup-agent/src/data/api/agents/agents.requests.ts
+++ packages/manager/modules/backup-agent/src/data/api/agents/agents.requests.ts
@@ -1,2 +1,5 @@
 export const getBackupAgentsListRoute = (vspcTenantId: string) =>
   `/backup/tenant/vspc/${vspcTenantId}/backupAgent`;
+
+export const getBackupAgentsDetailsRoute = (vspcTenantId: string, backupAgentId: string) =>
+  `${getBackupAgentsListRoute(vspcTenantId)}/${backupAgentId}`;
