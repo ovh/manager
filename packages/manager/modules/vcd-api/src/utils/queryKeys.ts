8e7295b7b81ef2834eb12cac169d2cc10fe6dd5b
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-11-25 14:40:01 +0100

    feat(public-vcf): add ipBlock module setup (types/api/hooks..)
    ref: #MANAGER-19356
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/modules/vcd-api/src/utils/queryKeys.ts
+++ packages/manager/modules/vcd-api/src/utils/queryKeys.ts
@@ -91,3 +91,9 @@ export const getVrackSegmentQueryKey = ({
   vdcId: string;
   vrackSegmentId: string;
 }) => [...getVrackSegmentListQueryKey(id, vdcId), vrackSegmentId];
+
+// IP Block
+export const getVcdIpBlockListQueryKey = (id: string) => [
+  ...getVcdOrganizationQueryKey(id),
+  'ipBlock',
+];
