80a7c6fbc0df8674cd146172e53185c6fd72b22c
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-11-25 10:29:58 +0100

    feat(backup-agent): add agent details request
    ref: #BKP-656
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/backup-agent/src/mocks/agents/agents.handler.ts
+++ packages/manager/modules/backup-agent/src/mocks/agents/agents.handler.ts
@@ -1,3 +1,5 @@
+import { PathParams } from 'msw';
+
 import { Handler } from '@ovh-ux/manager-core-test-utils';
 
 import { mockAgents } from '@/mocks/agents/agents';
@@ -11,6 +13,16 @@ export const getAgentMocks = ({ isAgentsError = false }: TAgentMockParams = {}):
     url: '/backup/tenant/vspc/:vspcTenantId/backupAgent',
     response: () => {
       return isAgentsError ? null : mockAgents;
+    },
+    api: 'v2',
+    method: 'get',
+    status: isAgentsError ? 500 : 200,
+  },
+  {
+    url: '/backup/tenant/vspc/:vspcTenantId/backupAgent/:backupAgentId',
+    response: (_: unknown, params: PathParams) => {
+      if (isAgentsError) return null;
+      return mockAgents.find((agent) => agent.id === params.backupAgentId);
     },
     api: 'v2',
     method: 'get',
