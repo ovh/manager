6f79dd580b8cb537b89b05c64027c9fb94701e31
Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
2025-11-25 15:51:09 +0100

    feat(communication): add guided tour
    ref: #MANAGER-18777
    
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>

---
+++ packages/manager/apps/communication/src/components/guideMenu/GuideMenu.component.tsx
@@ -0,0 +1,38 @@
+import { useGuidedTour } from '@/hooks/useGuidedTour';
+import { useTranslation } from 'react-i18next';
+import { Button, BUTTON_SIZE, BUTTON_VARIANT, Icon, ICON_NAME, Popover, POPOVER_POSITION, PopoverContent, PopoverTrigger, Link as OdsLink } from '@ovh-ux/muk';
+
+export const GuideMenu = () => {
+
+  const { start } = useGuidedTour();
+  const { t } = useTranslation('common');
+
+  const onClickStartGuide = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    start();
+  };
+
+  return (
+    <Popover position={POPOVER_POSITION.bottom}>
+      <PopoverTrigger asChild>
+        <Button
+          aria-label={t('guide_menu_header')}
+          variant={BUTTON_VARIANT.ghost}
+          size={BUTTON_SIZE.sm}
+        >
+          <>
+            <Icon name={ICON_NAME.book} aria-hidden={true} />
+            {t('guide_menu_header')}
+          </>
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <div className="flex flex-col gap-2 py-1">
+          <OdsLink onClick={onClickStartGuide}>
+            {t('guide_menu_start_guide')}
+          </OdsLink>
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+};
