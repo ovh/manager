899a230fcc859514eb3fe0f030e2cec9b95a6840
Thomas Esseul <thomas.esseul@corp.ovh.com>
2025-11-26 11:06:37 +0100

    feat(pci-kubernetes): update kubeconfig actions
    ref: #TAPC-5144, #TAPC-5360
    
    Signed-off-by: Thomas Esseul <thomas.esseul@corp.ovh.com>

--- packages/manager/apps/pci-kubernetes/src/App.tsx
+++ packages/manager/apps/pci-kubernetes/src/App.tsx
@@ -4,6 +4,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { odsSetup } from '@ovhcloud/ods-common-core';
+import { Toaster } from '@ovhcloud/ods-react';
 import '@ovhcloud/ods-theme-blue-jeans';
 
 import appRoutes from '@/routes';
@@ -17,6 +18,7 @@ const router = createHashRouter(appRoutes);
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <Toaster />
       <RouterProvider router={router}></RouterProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
