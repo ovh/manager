e409516a4bb31f1672739959822917c30be1069f
stif59100 <steeve.vanderstocken@ovhcloud.com>
2025-11-26 14:04:51 +0100

    fix(web-hosting): fix tests
    ref: #PUWEBPT-86
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

--- packages/manager/apps/web-hosting/src/pages/dashboard/multisite/cdn/AdvancedFlushCdn.modal.spec.tsx
+++ packages/manager/apps/web-hosting/src/pages/dashboard/multisite/cdn/AdvancedFlushCdn.modal.spec.tsx
@@ -1,14 +1,23 @@
+import React, { ComponentType } from 'react';
+
 import { fireEvent, render, screen } from '@testing-library/react';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 
 import { navigate } from '@/utils/test.setup';
+import { createTestWrapper } from '@/utils/test.provider';
 
 import AdvancedFlushCdnModal from './AdvancedFlushCdn.modal';
 
-vi.mock('react-i18next', () => ({
-  useTranslation: () => ({ t: (k: string) => k }),
-  Trans: ({ i18nKey }: { i18nKey: string }) => i18nKey,
-}));
+vi.mock('react-i18next', async (importOriginal) => {
+  const actual = await importOriginal<typeof import('react-i18next')>();
+  return {
+    ...actual,
+    useTranslation: () => ({ t: (k: string) => k }),
+    Trans: ({ i18nKey }: { i18nKey: string }) => i18nKey,
+  };
+});
+
+const Wrappers = createTestWrapper();
 
 describe('AdvancedFlushCdnModal', () => {
   beforeEach(() => {
@@ -16,7 +25,7 @@ describe('AdvancedFlushCdnModal', () => {
   });
 
   it('should close modal on cancel', () => {
-    render(<AdvancedFlushCdnModal />);
+    render(<AdvancedFlushCdnModal />, { wrapper: Wrappers as ComponentType });
 
     const cancelBtn = screen.getByTestId('secondary-button');
     expect(cancelBtn).not.toBeNull();
@@ -26,7 +35,7 @@ describe('AdvancedFlushCdnModal', () => {
   });
 
   it('validate and open upgrade cdn page and close modal', () => {
-    render(<AdvancedFlushCdnModal />);
+    render(<AdvancedFlushCdnModal />, { wrapper: Wrappers as ComponentType });
 
     const primaryBtn = screen.getByTestId('primary-button');
     fireEvent.click(primaryBtn);
