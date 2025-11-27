aa667962996ff10529eb73df533659345f8e0717
David Arsène <david.arsene.ext@ovhcloud.com>
2025-11-27 17:10:54 +0100

    fix(observability): fix wrong header layout ui
    fixes: #MAOBS-141
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/observability/src/components/form/radio-card/RadioCard.component.tsx
+++ packages/manager/apps/observability/src/components/form/radio-card/RadioCard.component.tsx
@@ -22,7 +22,7 @@ export const RadioCard = ({
       key={id}
       htmlFor={id}
       className={clsx(
-        'flex border-solid rounded-md border px-5 py-6',
+        'flex rounded-md border border-solid px-5 py-6',
         !isDisabled && 'cursor-pointer',
         !isChecked && 'border-[--ods-color-form-element-border-default]',
         !isChecked && !isDisabled && 'hover:border-[--ods-color-form-element-border-hover-default]',
@@ -30,12 +30,12 @@ export const RadioCard = ({
           !isDisabled &&
           'border-[--ods-color-primary-500] bg-[--ods-color-primary-050] outline outline-1 outline-[--ods-color-primary-500]',
         isDisabled &&
-          'cursor-not-allowed grayscale opacity-70 bg-[--ods-color-background-disabled-default] border-[--ods-color-border-disabled-default]',
+          'cursor-not-allowed border-[--ods-color-border-disabled-default] bg-[--ods-color-background-disabled-default] opacity-70 grayscale',
       )}
     >
       <div className="flex flex-row gap-3">
         {/* HEADER */}
-        <div className="flex flex-col flex-1 gap-1">
+        <div className="flex flex-1 flex-col gap-1">
           <div className="flex items-center gap-3">
             <input
               type="radio"
