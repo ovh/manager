da18ce91dd58c8d1556bfd644366231250073c23
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-11-27 10:54:39 +0100

    feat(backup-agent): add edit configuration agent drawer
    ref: #BKP-656
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/backup-agent/src/data/api/agents/agents.requests.ts
+++ packages/manager/modules/backup-agent/src/data/api/agents/agents.requests.ts
@@ -1,5 +1,27 @@
+import { ApiResponse, v2 } from '@ovh-ux/manager-core-api';
+
 export const getBackupAgentsListRoute = (vspcTenantId: string) =>
   `/backup/tenant/vspc/${vspcTenantId}/backupAgent`;
 
 export const getBackupAgentsDetailsRoute = (vspcTenantId: string, backupAgentId: string) =>
-  `${getBackupAgentsListRoute(vspcTenantId)}/${backupAgentId}`;
+  `/backup/tenant/vspc/${vspcTenantId}/backupAgent/${backupAgentId}`;
+
+export const getEditConfigurationBackupAgentsRoute = (
+  vspcTenantId: string,
+  backupAgentId: string,
+) => `/backup/tenant/vspc/${vspcTenantId}/backupAgent/${backupAgentId}`;
+
+export type EditConfigurationBackupAgentsParams = {
+  vspcTenantId: string;
+  backupAgentId: string;
+  ips: string[];
+  displayName: string;
+  policy: string;
+};
+
+export const editConfigurationBackupAgents = async ({
+  vspcTenantId,
+  backupAgentId,
+  ...payload
+}: EditConfigurationBackupAgentsParams): Promise<ApiResponse<string>> =>
+  v2.put(getEditConfigurationBackupAgentsRoute(vspcTenantId, backupAgentId), payload);
