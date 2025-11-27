806910c5f12d96d7b1c4ea5a2e8c3d44b7b02cb5
David Arsène <david.arsene.ext@ovhcloud.com>
2025-11-27 17:10:54 +0100

    fix(observability): fix wrong header layout ui
    fixes: #MAOBS-141
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/observability/src/App.constants.ts
+++ packages/manager/apps/observability/src/App.constants.ts
@@ -1,3 +1,5 @@
+import { ChangelogMenuLinks } from '@ovh-ux/muk';
+
 export const appName = 'observability';
 
 export const AppConfig = {
@@ -9,3 +11,12 @@ export type DashboardsApi = 'v2';
 export const APP_FEATURES = {
   basePrefix: '',
 } as const;
+
+export const CHANGELOG_LINKS: ChangelogMenuLinks = {
+  changelog:
+    'https://github.com/orgs/ovh/projects/16/views/6?pane=info&sliceBy%5Bvalue%5D=Observability',
+  roadmap:
+    'https://github.com/orgs/ovh/projects/16/views/1?pane=info&sliceBy%5Bvalue%5D=Observability',
+  'feature-request':
+    'https://github.com/ovh/observability-roadmap/issues/new?assignees=&labels=&projects=&template=feature_request.md&title=',
+};
