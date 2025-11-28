6afa49b36a5bbfec7018907061ab4295b354219b
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-11-28 14:10:17 +0100

    feat(muk)!: delete ods components
    ref: #MANAGER-20371
    
    BREAKING CHANGE: delete export of ods components
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-ui-kit/src/components/card/constants/CardConstants.ts
+++
@@ -1,3 +0,0 @@
-import { CARD_COLOR, CARD_COLORS } from '@ovhcloud/ods-react';
-
-export { CARD_COLOR, CARD_COLORS };
--- packages/manager-ui-kit/src/components/tree-view/tree-view-nodes/TreeViewNodes.component.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-import { TreeViewNodes as ODSTreeViewNodes } from '@ovhcloud/ods-react';
-
-import { TreeViewNodesProps } from './TreeViewNodes.props';
-
-export const TreeViewNodes = ({ children, ...props }: PropsWithChildren<TreeViewNodesProps>) => (
-  <ODSTreeViewNodes {...props}>{children}</ODSTreeViewNodes>
-);
