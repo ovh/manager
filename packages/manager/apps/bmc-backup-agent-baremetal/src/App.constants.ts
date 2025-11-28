abf69510eda15c6d27a0eb619ff0207406da29a5
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-11-28 15:14:29 +0100

    feat(bmc-backup-agent-baremetal): add backup-agent logo on onboarding
    ref: #BRAND-1969
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/apps/bmc-backup-agent-baremetal/src/App.constants.ts
+++ packages/manager/apps/bmc-backup-agent-baremetal/src/App.constants.ts
@@ -1,6 +1,6 @@
 import { OnboardingConfigType } from '@/types/Onboarding.type';
 
-import ovhcloudBackupImg from './assets/OVHcloudBackup.svg?url';
+import backupAgentImage from './assets/backup-agent.svg?url';
 
 export const appName = 'bmc-backup-agent-baremetal';
 export const productName = 'OVHcloud Backup Agent for Bare Metal';
@@ -31,7 +31,7 @@ export const ONBOARDING_CONFIG: OnboardingConfigType = {
     faq: docUrl,
   },
   title: productName,
-  heroImage: { src: ovhcloudBackupImg },
+  heroImage: { src: backupAgentImage },
 };
 
 export type ListingApi = 'v6Iceberg' | 'v6' | 'v2';
