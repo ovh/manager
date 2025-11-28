657a153701a350fc3105e9cfe1c7f4059bd48f12
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-11-28 14:10:17 +0100

    feat(muk)!: delete ods components
    ref: #MANAGER-20371
    
    BREAKING CHANGE: delete export of ods components
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-ui-kit/src/components/checkbox/checkbox-group/CheckboxGroup.props.ts
+++
@@ -1,5 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-import { CheckboxGroupProp as OdsCheckboxGroupProps } from '@ovhcloud/ods-react';
-
-export type CheckboxGroupProps = PropsWithChildren<OdsCheckboxGroupProps> & {};
