5ef813f8cf7e8c0a63cc3797b29a2734c4dde495
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-11-28 15:59:50 +0100

    feat(backup-agent): disabled delete button on services listing
    ref: #BKP-663
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/backup-agent/src/pages/services/listing/_components/TenantActionsCell.component.tsx
+++ packages/manager/modules/backup-agent/src/pages/services/listing/_components/TenantActionsCell.component.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 
-import { OdsButton } from '@ovhcloud/ods-components/react';
+import {OdsButton, OdsTooltip} from '@ovhcloud/ods-components/react';
 
 import { DataGridTextCell } from '@ovh-ux/manager-react-components';
 
@@ -8,20 +8,28 @@ import { Resource } from '@/types/Resource.type';
 import { Tenant } from '@/types/Tenant.type';
 import { VSPCTenant } from '@/types/VspcTenant.type';
 import { buildDeleteTenantUrl } from '@/utils/buildSearchQuery.utils';
+import {useId} from "react";
+import { useTranslation } from "react-i18next";
+import {BACKUP_AGENT_NAMESPACES, SERVICES_NAMESPACE_PREFIX} from "@/BackupAgent.translations";
 
 export const TenantActionCell = (tenant: Resource<Tenant | VSPCTenant>) => {
+  const id = useId();
   const navigate = useNavigate();
+  const { t } = useTranslation(BACKUP_AGENT_NAMESPACES.SERVICE_LISTING);
 
   return (
     <DataGridTextCell>
       <OdsButton
+        id={id}
         label=""
         icon="trash"
         variant="ghost"
         onClick={() => {
           navigate(buildDeleteTenantUrl({ tenantId: tenant.id, origin: 'listing' }));
         }}
+        isDisabled
       />
+      <OdsTooltip triggerId={id} withArrow>{t(`${BACKUP_AGENT_NAMESPACES.SERVICE_LISTING}:delete_tenant_tooltip_disabled`)}</OdsTooltip>
     </DataGridTextCell>
   );
 };
