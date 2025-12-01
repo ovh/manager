5658f26a31454f85e85dc57ff2797bcc6182cba4
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2025-12-01 16:12:25 +0100

    feat(pci.databases): replace ipify
    ref: #DATATR-2644
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

---
+++ packages/manager/apps/pci-databases-analytics/src/__tests__/helpers/mocks/me.ts
@@ -0,0 +1,7 @@
+import { ContinentCountryLocation, ContinentEnum, CountryEnum } from "@datatr-ux/ovhcloud-types/me/geolocation";
+
+export const mockedGeolocation:ContinentCountryLocation = {
+  continent: ContinentEnum.europe,
+  countryCode: CountryEnum.fr,
+  ip: "0.0.0.0",
+}
