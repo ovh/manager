6b67f188b0d3ab7fb4030c4c8c3fb3771a3f0ee9
yjaaouane <yassine.jaaouane.ext@corp.ovh.com>
2025-12-02 09:31:12 +0100

    refactor(observability): replace drawer for editing
    ref: #MAOBS-152
    
    Signed-off-by: yjaaouane <yassine.jaaouane.ext@corp.ovh.com>

--- packages/manager/apps/observability/src/pages/tenants/edit/EditTenant.page.tsx
+++ packages/manager/apps/observability/src/pages/tenants/edit/EditTenant.page.tsx
@@ -2,26 +2,31 @@ import { useParams } from 'react-router-dom';
 
 import { RedirectionGuard } from '@ovh-ux/muk';
 
-import EditTenantDrawer from '@/components/drawer/EditTenantDrawer.component';
 import { useObservabilityServiceContext } from '@/contexts/ObservabilityService.context';
+import { useTenant } from '@/data/hooks/tenants/useTenants.hook';
+import { TenantForm } from '@/pages/tenants/TenantForm.component';
 import { LocationPathParams } from '@/routes/Routes.constants';
 import { getTenantDashboardUrl } from '@/routes/Routes.utils';
 
 const EditTenantPage = () => {
   const { selectedService, isLoading, isSuccess } = useObservabilityServiceContext();
   const resourceName = selectedService?.id;
-  const currentResourceName = resourceName ?? '';
 
   const { tenantId } = useParams<LocationPathParams>();
   const currentTenantId = tenantId ?? '';
 
+  const { data: tenant, isLoading: isTenantLoading } = useTenant(
+    resourceName ?? '',
+    tenantId ?? '',
+  );
+
   return (
     <RedirectionGuard
       condition={!resourceName && isSuccess}
-      isLoading={isLoading}
+      isLoading={isLoading || isTenantLoading}
       route={getTenantDashboardUrl(currentTenantId)}
     >
-      <EditTenantDrawer resourceName={currentResourceName} tenantId={currentTenantId} />
+      <TenantForm tenant={tenant} />
     </RedirectionGuard>
   );
 };
