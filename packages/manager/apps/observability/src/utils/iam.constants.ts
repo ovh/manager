1628d07ae103e4985507282371107207c609d46f
yjaaouane <yassine.jaaouane.ext@corp.ovh.com>
2025-12-02 09:31:12 +0100

    refactor(observability): replace drawer for editing
    ref: #MAOBS-152
    
    Signed-off-by: yjaaouane <yassine.jaaouane.ext@corp.ovh.com>

--- packages/manager/apps/observability/src/utils/iam.constants.ts
+++ packages/manager/apps/observability/src/utils/iam.constants.ts
@@ -1,3 +1,4 @@
 export const IAM_ACTIONS = {
   CREATE_TENANT: ['ldp:apiovh:output/metric/tenant/create'],
+  EDIT_TENANT: ['ldp:apiovh:output/metric/tenant/edit'],
 } as const satisfies Record<string, string[]>;
