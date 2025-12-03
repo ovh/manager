365b966464af3343ac6f94a328d5c6b515599470
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-12-03 08:57:58 +0100

    feat(backup-agent): add download agent modal
    ref: #BKP-651
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

---
+++ packages/manager/modules/backup-agent/src/mocks/agents/agentDownloadLinks.handler.ts
@@ -0,0 +1,21 @@
+import { Handler } from '@ovh-ux/manager-core-test-utils';
+
+import { mockAgentDownloadLinks } from '@/mocks/agents/agentDownloadLinks';
+
+export type TAgentDownloadLinkMockParams = {
+  isAgentDownloadLinksError?: boolean;
+};
+
+export const getAgentDownloadLinksMocks = ({
+  isAgentDownloadLinksError = false,
+}: TAgentDownloadLinkMockParams = {}): Handler[] => [
+  {
+    url: '/backup/tenant/vspc/:vspcTenantId/backupAgent',
+    response: () => {
+      return isAgentDownloadLinksError ? null : mockAgentDownloadLinks;
+    },
+    api: 'v2',
+    method: 'get',
+    status: isAgentDownloadLinksError ? 500 : 200,
+  },
+];
