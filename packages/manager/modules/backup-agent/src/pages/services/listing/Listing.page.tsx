bc9b54ddd39b356c371f5b8c1c09292440b35d2a
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-12-04 15:53:33 +0100

    feat(backup-agent):  remove buy button on listing pages
    ref: #BKP-699
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/backup-agent/src/pages/services/listing/Listing.page.tsx
+++ packages/manager/modules/backup-agent/src/pages/services/listing/Listing.page.tsx
@@ -1,42 +1,22 @@
-import { Suspense, startTransition } from 'react';
+import { Suspense } from 'react';
 
-import { Outlet, useNavigate } from 'react-router-dom';
-
-import { useTranslation } from 'react-i18next';
-
-import { ODS_BUTTON_SIZE } from '@ovhcloud/ods-components';
-import { OdsButton } from '@ovhcloud/ods-components/react';
+import { Outlet } from 'react-router-dom';
 
 import { Datagrid } from '@ovh-ux/manager-react-components';
 
-import { BACKUP_AGENT_NAMESPACES } from '@/BackupAgent.translations';
 import { useBackupTenantsMocks } from '@/data/hooks/tenants/useBackupTenants';
-import { urls } from '@/routes/Routes.constants';
 
 import { useTenantListingColumns } from './_hooks/useVspcTenantListingColumns';
 
 export default function ListingPage() {
-  const { t } = useTranslation([BACKUP_AGENT_NAMESPACES.SERVICE_LISTING]);
-  const navigate = useNavigate();
   const columns = useTenantListingColumns();
   const { data, isLoading } = useBackupTenantsMocks(); // TODO: unmock (useVSPCTenants)
-
-  const onNavigateToDashboardClicked = () => {
-    startTransition(() => navigate(urls.dashboardTenants));
-  };
 
   return (
     <>
       <Suspense>
         {columns && (
           <Datagrid
-            topbar={
-              <OdsButton
-                size={ODS_BUTTON_SIZE.md}
-                label={t(`${BACKUP_AGENT_NAMESPACES.SERVICE_LISTING}:order`)}
-                onClick={onNavigateToDashboardClicked}
-              />
-            }
             columns={columns}
             items={data || []}
             totalItems={data?.length || 0}
