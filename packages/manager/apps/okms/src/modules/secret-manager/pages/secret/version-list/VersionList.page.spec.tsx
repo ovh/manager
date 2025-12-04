0e15131cbdeb2e7d1b176c4b82362a905e99bdc1
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-12-04 15:35:29 +0100

    fix(okms): broken link on secret versions list
    ref: #MANAGER-20487
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/okms/src/modules/secret-manager/pages/secret/version-list/VersionList.page.spec.tsx
+++ packages/manager/apps/okms/src/modules/secret-manager/pages/secret/version-list/VersionList.page.spec.tsx
@@ -2,12 +2,9 @@ import { mockSecret1 } from '@secret-manager/mocks/secrets/secrets.mock';
 import { versionActiveMock } from '@secret-manager/mocks/versions/versions.mock';
 import { SECRET_MANAGER_ROUTES_URLS } from '@secret-manager/routes/routes.constants';
 import { assertVersionDatagridVisilibity } from '@secret-manager/utils/tests/versionList';
-import { act } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 
-import { assertTextVisibility, getOdsButtonByLabel } from '@ovh-ux/manager-core-test-utils';
+import { getOdsButtonByLabel } from '@ovh-ux/manager-core-test-utils';
 
-import { labels } from '@/common/utils/tests/init.i18n';
 import { renderTestApp } from '@/common/utils/tests/renderTestApp';
 
 const mockOkmsId = '123123';
@@ -23,10 +20,8 @@ describe('Version list page test suite', () => {
     await assertVersionDatagridVisilibity();
   });
 
-  it('navigate to reveal value drawer on click on active version', async () => {
+  it('should have the correct href attribute to open the value drawer', async () => {
     // GIVEN
-    const user = userEvent.setup();
-
     const { container } = await renderTestApp(
       SECRET_MANAGER_ROUTES_URLS.versionList(mockOkmsId, mockSecret1.path),
     );
@@ -40,12 +35,13 @@ describe('Version list page test suite', () => {
       disabled: false,
     });
 
-    // WHEN
-    await act(async () => {
-      await user.click(versionLink);
-    });
-
-    // THEN
-    await assertTextVisibility(labels.secretManager.values);
+    expect(versionLink).toHaveAttribute(
+      'href',
+      SECRET_MANAGER_ROUTES_URLS.versionListSecretValueDrawer(
+        mockOkmsId,
+        mockSecret1.path,
+        versionActiveMock.id,
+      ),
+    );
   });
 });
