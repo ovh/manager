9853317559f3aab5e6528b3dae3c31f0e224d71d
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-12-04 15:49:52 +0100

    feat(backup-agent): remove link on agent and bucket listing
    ref: #BKP-699
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/backup-agent/src/pages/vaults/dashboard/buckets/_components/BucketNameCell.component.tsx
+++ packages/manager/modules/backup-agent/src/pages/vaults/dashboard/buckets/_components/BucketNameCell.component.tsx
@@ -1,16 +1,13 @@
-import { useHref } from 'react-router-dom';
+import { OdsText } from '@ovhcloud/ods-components/react';
 
-import { DataGridTextCell, Links } from '@ovh-ux/manager-react-components';
+import { DataGridTextCell } from '@ovh-ux/manager-react-components';
 
-import { urlParams, urls } from '@/routes/Routes.constants';
 import { Bucket } from '@/types/Bucket.type';
 
-export const BucketNameCell = ({ id, name }: Pick<Bucket, 'id' | 'name'>) => {
-  const bucketDashboardHref = useHref(urls.dashboardVaults.replace(urlParams.vaultId, id)); // TODO : Replace with good url
-
+export const BucketNameCell = ({ name }: Pick<Bucket, 'name'>) => {
   return (
     <DataGridTextCell>
-      <Links href={bucketDashboardHref} label={name} />
+      <OdsText>{name}</OdsText>
     </DataGridTextCell>
   );
 };
