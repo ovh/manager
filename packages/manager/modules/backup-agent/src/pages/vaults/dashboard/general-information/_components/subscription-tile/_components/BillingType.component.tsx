e959ba3dfe826e10cfd7ed84de2d40daf5476d35
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-12-04 14:25:37 +0100

    fixup! fix(backup-agent): fix dashboard display
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/backup-agent/src/pages/vaults/dashboard/general-information/_components/subscription-tile/_components/BillingType.component.tsx
+++ packages/manager/modules/backup-agent/src/pages/vaults/dashboard/general-information/_components/subscription-tile/_components/BillingType.component.tsx
@@ -6,18 +6,18 @@ export const BillingType = () => {
   const priceTTCText = 'XX,XX €';
 
   return (
-    <>
-      <OdsText>Bundle - {BundleSize}</OdsText>
-      <OdsText className="[&::part(text)]:font-medium">À partir de</OdsText>
+    <div>
+      <OdsText class="block">Bundle - {BundleSize}</OdsText>
+      <OdsText className="block [&::part(text)]:font-medium">À partir de</OdsText>
       <OdsText
-        className="[&::part(text)]:font-bold [&::part(text)]:text-[var(--ods-color-primary-500)]"
+        className="block [&::part(text)]:font-bold [&::part(text)]:text-[var(--ods-color-primary-500)]"
         color="primary"
       >
         {priceHTText}
       </OdsText>
-      <OdsText className="[&::part(text)]:font-light">HT/mois</OdsText>
-      <OdsText className="[&::part(text)]:font-light">soit {priceTTCText} TTC/mois</OdsText>
-      <OdsText className="[&::part(text)]:font-light">sans engagement</OdsText>
-    </>
+      <OdsText className="block [&::part(text)]:font-light">HT/mois</OdsText>
+      <OdsText className="block [&::part(text)]:font-light">soit {priceTTCText} TTC/mois</OdsText>
+      <OdsText className="block [&::part(text)]:font-light">sans engagement</OdsText>
+    </div>
   );
 };
