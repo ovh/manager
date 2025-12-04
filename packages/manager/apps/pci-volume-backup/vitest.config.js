ada851dd970a80150566a9895863ae2ba704d23d
helabenkhalfallah <hela.ben-khalfallah@corp.ovh.com>
2025-12-04 21:16:10 +0100

    refactor(pci-quota): migrate pci-volume-backup to pnpm
    ref: #MANAGER-20363
    
    Signed-off-by: helabenkhalfallah <hela.ben-khalfallah@corp.ovh.com>

--- packages/manager/apps/pci-volume-backup/vitest.config.js
+++ packages/manager/apps/pci-volume-backup/vitest.config.js
@@ -1,3 +1,4 @@
+import { defaultDedupedDependencies } from '@ovh-ux/manager-tests-setup';
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -24,7 +25,7 @@ export default defineConfig({
       ],
     },
   },
-  resolve: {
+  resolve: { dedupe: [...defaultDedupedDependencies], 
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
