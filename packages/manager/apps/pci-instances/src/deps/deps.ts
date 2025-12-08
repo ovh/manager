a5ce80cffc1a4859394460a0ae3294c8a4f8fda1
Lauren Olivier <lauren.olivier@corp.ovh.com>
2025-12-08 17:24:26 +0100

    feat(pci-instances): harmonize types names
    Signed-off-by: Lauren Olivier <lauren.olivier@corp.ovh.com>

--- packages/manager/apps/pci-instances/src/deps/deps.ts
+++ packages/manager/apps/pci-instances/src/deps/deps.ts
@@ -1,14 +1,18 @@
 import { instancesCatalogAdapter } from '@/adapters/tanstack/instancesCatalog/left/instancesCatalogAdapter';
-import { InstancesCatalogPort } from '@/domain/port/instancesCatalog/left/port';
-import { ConfigurationPort } from '@/domain/port/configuration/left/port';
+import { TInstancesCatalogPort } from '@/domain/port/instancesCatalog/left/port';
+import { TConfigurationPort } from '@/domain/port/configuration/left/port';
 import { configurationAdapter } from '@/adapters/tanstack/configuration/left/configurationAdapter';
+import { TInstancePort } from '@/domain/port/instance/port';
+import { instanceAdapter } from '@/adapters/tanstack/instance/left/instanceAdapter';
 
 export type Deps = {
-  instancesCatalogPort: InstancesCatalogPort;
-  configurationPort: ConfigurationPort;
+  instancesCatalogPort: TInstancesCatalogPort;
+  configurationPort: TConfigurationPort;
+  instancePort: TInstancePort;
 };
 
 export const deps: Deps = {
   instancesCatalogPort: instancesCatalogAdapter,
   configurationPort: configurationAdapter,
+  instancePort: instanceAdapter,
 };
