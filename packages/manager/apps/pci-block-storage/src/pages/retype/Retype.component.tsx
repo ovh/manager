4779c2615942d89d9a773f2ee80c63449956ca37
Adrien Turmo <adrien.turmo.ext@ovhcloud.com>
2025-12-08 18:07:40 +0100

    feat(pci-block-storage): handle retyping
    ref: #TAPC-4358
    
    Signed-off-by: Adrien Turmo <adrien.turmo.ext@ovhcloud.com>

--- packages/manager/apps/pci-block-storage/src/pages/retype/Retype.component.tsx
+++ packages/manager/apps/pci-block-storage/src/pages/retype/Retype.component.tsx
@@ -1,24 +1,71 @@
 import { useNavigate } from 'react-router-dom';
 import { FC, useMemo } from 'react';
-import { useTranslation } from 'react-i18next';
+import { Translation, useTranslation } from 'react-i18next';
 import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import { NAMESPACES } from '@ovh-ux/manager-common-translations';
+import { useNotifications } from '@ovh-ux/manager-react-components';
+import {
+  ICON_NAME,
+  Message,
+  MESSAGE_COLOR,
+  MessageBody,
+  MessageIcon,
+} from '@ovhcloud/ods-react';
 import { TVolumeModel } from '@/api/hooks/useCatalog';
 import { VolumeModelTilesInput } from '@/components/VolumeModelTilesInput.component';
 import { TVolumeRetypeModel } from '@/api/hooks/useCatalogWithPreselection';
 import { BaseRetypeForm } from './BaseRetypeForm.component';
+import { useRetypeVolume } from '@/api/hooks/useVolume';
+import { useTrackBanner } from '@/hooks/useTrackBanner';
+import { TAPIVolume } from '@/api/data/volume';
 
 type FormValues = {
   volumeModel: TVolumeModel;
 };
 
 type RetypeProps = {
+  projectId: string;
+  volumeId: string;
   volumeModelData: TVolumeRetypeModel[];
 };
 
-export const Retype: FC<RetypeProps> = ({ volumeModelData }) => {
+export const Retype: FC<RetypeProps> = ({
+  projectId,
+  volumeId,
+  volumeModelData,
+}) => {
   const { t } = useTranslation(['common', 'retype', NAMESPACES.ACTIONS]);
   const navigate = useNavigate();
+  const { addSuccess } = useNotifications();
+
+  const onRetypeSuccess = useTrackBanner(
+    { type: 'success' },
+    (originalVolume: TAPIVolume) => {
+      navigate('..');
+      addSuccess(
+        <div className="pr-6">
+          <Translation ns="retype">
+            {(_t) =>
+              _t('pci_projects_project_storages_blocks_retype_retype_success', {
+                volumeName: originalVolume?.name,
+              })
+            }
+          </Translation>
+        </div>,
+        true,
+      );
+    },
+  );
+
+  const {
+    retypeVolume,
+    isPending: isRetypingPending,
+    isError: isRetypingError,
+  } = useRetypeVolume({
+    projectId,
+    volumeId,
+    onSuccess: onRetypeSuccess,
+  });
 
   const currentModel = useMemo(
     () => volumeModelData.find((model) => model.isPreselected),
@@ -42,8 +89,10 @@ export const Retype: FC<RetypeProps> = ({ volumeModelData }) => {
     navigate('..');
   };
 
-  const onSubmit: SubmitHandler<FormValues> = () => {
-    // TODO Change this when backend is ready
+  const onSubmit: SubmitHandler<FormValues> = (formValues) => {
+    retypeVolume({
+      type: formValues.volumeModel.name,
+    });
   };
 
   return (
@@ -53,8 +102,19 @@ export const Retype: FC<RetypeProps> = ({ volumeModelData }) => {
       isValidationDisabled={!isDirty}
       cancelButtonText={t(`${NAMESPACES.ACTIONS}:cancel`)}
       validateButtonText={t(`${NAMESPACES.ACTIONS}:modify`)}
+      isLoading={isRetypingPending}
     >
       <div>
+        {isRetypingError && (
+          <Message dismissible={false} color={MESSAGE_COLOR.critical}>
+            <MessageIcon name={ICON_NAME.hexagonExclamation} />
+            <MessageBody>
+              {t(
+                'retype:pci_projects_project_storages_blocks_retype_retype_error',
+              )}
+            </MessageBody>
+          </Message>
+        )}
         <Controller
           control={control}
           name="volumeModel"
@@ -62,12 +122,13 @@ export const Retype: FC<RetypeProps> = ({ volumeModelData }) => {
             <VolumeModelTilesInput
               value={value}
               volumeModels={volumeModelData}
-              onChange={onChange}
+              onChange={!isRetypingPending && onChange}
               label={t(
                 'retype:pci_projects_project_storages_blocks_retype_change_type_title',
               )}
               horizontal
               hideBadges
+              disabled={isRetypingPending}
             />
           )}
         />
