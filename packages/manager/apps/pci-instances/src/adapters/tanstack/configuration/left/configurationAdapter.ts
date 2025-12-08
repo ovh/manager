7f36a9f127363e7e9138c72d6052c25e24489c63
Lauren Olivier <lauren.olivier@corp.ovh.com>
2025-12-08 17:24:26 +0100

    feat(pci-instances): harmonize types names
    Signed-off-by: Lauren Olivier <lauren.olivier@corp.ovh.com>

--- packages/manager/apps/pci-instances/src/adapters/tanstack/configuration/left/configurationAdapter.ts
+++ packages/manager/apps/pci-instances/src/adapters/tanstack/configuration/left/configurationAdapter.ts
@@ -1,9 +1,9 @@
 import queryClient from '@/queryClient';
 import { sshKeysQueryKey } from '../queryKeys';
 import { TSshKey } from '@/domain/entities/configuration';
-import { ConfigurationPort } from '@/domain/port/configuration/left/port';
+import { TConfigurationPort } from '@/domain/port/configuration/left/port';
 
-export const configurationAdapter: ConfigurationPort = {
+export const configurationAdapter: TConfigurationPort = {
   selectSshKeys: (projectId: string, regionId: string) =>
     queryClient.getQueryData<TSshKey[]>(sshKeysQueryKey(projectId, regionId)),
 };
