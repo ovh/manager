81da935af85de5df5219b9ba6fe253632d8388fb
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-12-08 09:58:33 +0100

    fix(okms): handle secrets with no version on create version
    ref: #MANAGER-20350
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/okms/src/modules/secret-manager/data/hooks/useSecretVersion.ts
+++ packages/manager/apps/okms/src/modules/secret-manager/data/hooks/useSecretVersion.ts
@@ -1,14 +1,27 @@
 import { SecretVersionWithData } from '@secret-manager/types/secret.type';
-import { useQuery } from '@tanstack/react-query';
+import { skipToken, useQuery } from '@tanstack/react-query';
 
-import { ApiError } from '@ovh-ux/manager-core-api';
+import { ErrorResponse } from '@/common/types/api.type';
 
 import { getSecretVersionWithData, secretVersionsQueryKeys } from '../api/secretVersions';
 
-export const useSecretVersionWithData = (okmsId: string, secretPath: string, version: number) => {
-  return useQuery<SecretVersionWithData, ApiError>({
-    queryKey: secretVersionsQueryKeys.detailWithData(okmsId, secretPath, version),
-    queryFn: () => getSecretVersionWithData(okmsId, secretPath, version),
+type UseSecretVersionWithDataParams = {
+  okmsId: string;
+  secretPath: string;
+  version: number | undefined;
+};
+
+export const useSecretVersionWithData = ({
+  okmsId,
+  secretPath,
+  version,
+}: UseSecretVersionWithDataParams) => {
+  return useQuery<SecretVersionWithData, ErrorResponse>({
+    queryKey: secretVersionsQueryKeys.detailWithData(okmsId, secretPath, version ?? 0),
+    queryFn:
+      version !== undefined
+        ? () => getSecretVersionWithData(okmsId, secretPath, version)
+        : skipToken,
     // to remove secret data from cache, clear query instantly when we're done with it
     gcTime: 0,
   });
