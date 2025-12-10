2645cef907561736529f634ed2f842a18a1c4c03
soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>
2025-12-10 11:37:49 +0100

    fix(dedicated-server): fix lint issues
    ref: #infra-w49-release
    
    Signed-off-by: soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>

--- packages/manager/apps/dedicated-servers/src/components/cells/billingCell.tsx
+++ packages/manager/apps/dedicated-servers/src/components/cells/billingCell.tsx
@@ -3,7 +3,6 @@ import { useGetBillingInfo } from '@/hooks/useGetBillingInfo';
 import { SkeletonCell } from '../skeletonCell';
 import { BillingInfo } from '@/data/types/billing.type';
 import { DedicatedServer } from '@/data/types/server.type';
-import { useGetNutanixServer } from '@/hooks/useGetNutanixServer';
 
 export type BillingProps = {
   server: DedicatedServer;
