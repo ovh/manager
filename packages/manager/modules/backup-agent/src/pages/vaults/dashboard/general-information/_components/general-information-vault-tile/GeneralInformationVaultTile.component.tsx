8a87aa36488c4d46b59a143dca5e74ddf08fd2e9
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-12-10 15:39:10 +0100

    feat(backup-agent): add reference on vault general info tile
    ref: #BKP-700
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/modules/backup-agent/src/pages/vaults/dashboard/general-information/_components/general-information-vault-tile/GeneralInformationVaultTile.component.tsx
+++ packages/manager/modules/backup-agent/src/pages/vaults/dashboard/general-information/_components/general-information-vault-tile/GeneralInformationVaultTile.component.tsx
@@ -1,4 +1,12 @@
-import { GeneralInformationTile } from '@/components/CommonTiles/GeneralInformationsTile/GeneralInformationTile.component';
+import { useTranslation } from 'react-i18next';
+
+import { OdsSkeleton, OdsText } from '@ovhcloud/ods-components/react';
+
+import { NAMESPACES } from '@ovh-ux/manager-common-translations';
+import { ManagerTile } from '@ovh-ux/manager-react-components';
+
+import { ResourceStatusBadge } from '@/components/ResourceStatusBadge/ResourceStatusBadge.component';
+import { useLocationDetails } from '@/data/hooks/location/getLocationDetails';
 import { useBackupVaultDetails } from '@/data/hooks/vaults/getVaultDetails';
 
 type GeneralInformationVaultTileProps = {
@@ -6,7 +14,78 @@ type GeneralInformationVaultTileProps = {
 };
 
 export function GeneralInformationVaultTile({ vaultId }: GeneralInformationVaultTileProps) {
-  const { data, isLoading } = useBackupVaultDetails({ vaultId });
+  const { t } = useTranslation([
+    NAMESPACES.DASHBOARD,
+    NAMESPACES.STATUS,
+    NAMESPACES.REGION,
+    'dashboard',
+  ]);
+  const { data: vault, isLoading } = useBackupVaultDetails({ vaultId });
+  const { data: locationData, isLoading: isLocationLoading } = useLocationDetails(
+    vault?.currentState.region,
+  );
 
-  return <GeneralInformationTile resourceDetails={data} isLoading={isLoading} />;
+  /*
+  The code below is a copy of GeneralInformationTile component, made specifically for vaults.
+  It may be better to make the tile more generic and use it here but it requires more changes.
+  TODO: Refactor this to use a more generic approach
+  TODO: Refactor this to avoid using the non-null assertion operator everywhere
+  */
+
+  return (
+    <ManagerTile className="h-fit">
+      <ManagerTile.Title>{t(`${NAMESPACES.DASHBOARD}:general_information`)}</ManagerTile.Title>
+      <ManagerTile.Divider />
+      <ManagerTile.Item>
+        <ManagerTile.Item.Label>{t(`${NAMESPACES.DASHBOARD}:name`)}</ManagerTile.Item.Label>
+        <ManagerTile.Item.Description>
+          {isLoading ? <OdsSkeleton /> : <OdsText>{vault!.currentState.name}</OdsText>}
+        </ManagerTile.Item.Description>
+      </ManagerTile.Item>
+      <ManagerTile.Divider />
+      <ManagerTile.Item>
+        <ManagerTile.Item.Label>{t(`${NAMESPACES.STATUS}:status`)}</ManagerTile.Item.Label>
+        <ManagerTile.Item.Description>
+          {isLoading ? (
+            <OdsSkeleton />
+          ) : (
+            <ResourceStatusBadge resourceStatus={vault!.resourceStatus} />
+          )}
+        </ManagerTile.Item.Description>
+      </ManagerTile.Item>
+      <ManagerTile.Divider />
+      <ManagerTile.Item>
+        <ManagerTile.Item.Label>{t(`${NAMESPACES.REGION}:localisation`)}</ManagerTile.Item.Label>
+        <ManagerTile.Item.Description>
+          {isLoading || isLocationLoading ? (
+            <OdsSkeleton />
+          ) : (
+            <OdsText>{locationData?.location ?? vault!.currentState.region}</OdsText>
+          )}
+        </ManagerTile.Item.Description>
+      </ManagerTile.Item>
+      <ManagerTile.Divider />
+      <ManagerTile.Item>
+        <ManagerTile.Item.Label>{t(`${NAMESPACES.REGION}:region`)}</ManagerTile.Item.Label>
+        <ManagerTile.Item.Description>
+          {isLoading || isLocationLoading ? (
+            <OdsSkeleton />
+          ) : (
+            <OdsText>{locationData?.name ?? vault!.currentState.region}</OdsText>
+          )}
+        </ManagerTile.Item.Description>
+      </ManagerTile.Item>
+      <ManagerTile.Divider />
+      <ManagerTile.Item>
+        <ManagerTile.Item.Label>{t(`${NAMESPACES.DASHBOARD}:reference`)}</ManagerTile.Item.Label>
+        <ManagerTile.Item.Description>
+          {isLoading || isLocationLoading ? (
+            <OdsSkeleton />
+          ) : (
+            <OdsText>{vault!.currentState.resourceName}</OdsText>
+          )}
+        </ManagerTile.Item.Description>
+      </ManagerTile.Item>
+    </ManagerTile>
+  );
 }
