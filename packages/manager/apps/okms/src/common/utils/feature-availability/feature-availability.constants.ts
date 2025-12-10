c03ffcd97443d24525b2ebaec406ed884fee9082
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2025-12-10 15:47:39 +0100

    fix(okms): add feature flag on secret tiles
    ref: #MANAGER-20499
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/okms/src/common/utils/feature-availability/feature-availability.constants.ts
+++ packages/manager/apps/okms/src/common/utils/feature-availability/feature-availability.constants.ts
@@ -1,11 +1,14 @@
 const APP = 'okms';
-const PRODUCT = 'key-management-service';
+const PRODUCT_KEY_MANAGEMENT_SERVICE = 'key-management-service';
+const PRODUCT_SECRET_MANAGER = 'secret-manager';
 
-const KMS_FEATURES = {
-  PRODUCT: `${APP}:${PRODUCT}`,
-  KMS_USAGE_GUIDE: `${APP}:${PRODUCT}:kms-usage-guide`,
-  KMIP_CONNECTION_GUIDE: `${APP}:${PRODUCT}:kmip-connection-guide`,
-  LOGS: `${APP}:${PRODUCT}:logs`,
+export const KMS_FEATURES = {
+  PRODUCT: `${APP}:${PRODUCT_KEY_MANAGEMENT_SERVICE}`,
+  KMS_USAGE_GUIDE: `${APP}:${PRODUCT_KEY_MANAGEMENT_SERVICE}:kms-usage-guide`,
+  KMIP_CONNECTION_GUIDE: `${APP}:${PRODUCT_KEY_MANAGEMENT_SERVICE}:kmip-connection-guide`,
+  LOGS: `${APP}:${PRODUCT_KEY_MANAGEMENT_SERVICE}:logs`,
 } as const;
 
-export { KMS_FEATURES };
+export const SECRET_MANAGER_FEATURES = {
+  PRODUCT: `${APP}:${PRODUCT_SECRET_MANAGER}`,
+} as const;
