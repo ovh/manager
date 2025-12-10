ff30ac65cb2ebeb955edcf591240139c9a50c151
Noemie Foufe <noemie.foufe@corp.ovh.com>
2025-12-10 14:37:51 +0100

    fix(web-domains): host validation only for modification
    ref: #DCE-105
    
    Signed-off-by: Noemie Foufe <noemie.foufe@corp.ovh.com>

--- packages/manager/apps/web-domains/src/domain/components/Host/HostForm.tsx
+++ packages/manager/apps/web-domains/src/domain/components/Host/HostForm.tsx
@@ -67,7 +67,12 @@ export default function HostForm({
             className="relative w-full"
             readOnly={drawerAction === DrawerActionEnum.Modify}
             {...register('host', {
-              validate: makeHostValidators(hostsTargetSpec, serviceName, t),
+              validate: makeHostValidators(
+                hostsTargetSpec,
+                serviceName,
+                drawerAction,
+                t,
+              ),
             })}
           />
 
