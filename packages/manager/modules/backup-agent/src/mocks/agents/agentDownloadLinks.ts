24602d6f7438d86a86a36134be77276d02db5046
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-12-10 15:37:40 +0100

    feat(backup-agent): remove macOS from OS options
    ref: #BKP-700
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/modules/backup-agent/src/mocks/agents/agentDownloadLinks.ts
+++ packages/manager/modules/backup-agent/src/mocks/agents/agentDownloadLinks.ts
@@ -3,5 +3,4 @@ import { AgentDownloadLinks } from '@/types/AgentDownloadLinks';
 export const mockAgentDownloadLinks: AgentDownloadLinks = {
   linuxUrl: 'https://ovhcloud.com/download/backup-agent/linux/latest',
   windowsUrl: 'https://ovhcloud.com/download/backup-agent/windows/latest',
-  macUrl: 'https://ovhcloud.com/download/backup-agent/mac/latest',
 };
