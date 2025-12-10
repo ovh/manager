3952aea2bda8ff5092cb292e1a22916c15f47e79
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-12-10 15:37:40 +0100

    feat(backup-agent): remove macOS from OS options
    ref: #BKP-700
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/modules/backup-agent/src/mocks/tenant/backupPolicies.mock.ts
+++ packages/manager/modules/backup-agent/src/mocks/tenant/backupPolicies.mock.ts
@@ -1,8 +1,1 @@
-export const mockTenantBackupPolicies = [
-  '14d-windows',
-  '30d-windows',
-  '14d-linux',
-  '30d-linux',
-  '14d-macos',
-  '30d-macos',
-];
+export const mockTenantBackupPolicies = ['14d-windows', '30d-windows', '14d-linux', '30d-linux'];
