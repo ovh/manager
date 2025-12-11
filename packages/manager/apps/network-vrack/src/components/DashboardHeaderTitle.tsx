1b09ae4a83da32607b46db3ede9df9b4c706110c
aderghamov <alexandre.dergham.ext@ovhcloud.com>
2025-12-11 16:54:12 +0100

    fix(vrack): improving the display of the dashboard page
    ref: #MANAGER-19535
    
    Signed-off-by: aderghamov <alexandre.dergham.ext@ovhcloud.com>

--- packages/manager/apps/network-vrack/src/components/DashboardHeaderTitle.tsx
+++ packages/manager/apps/network-vrack/src/components/DashboardHeaderTitle.tsx
@@ -1,77 +1,26 @@
-import { useTranslation } from 'react-i18next';
-
-import { ICON_NAME, Icon, Text } from '@ovhcloud/ods-react';
-
 import { Vrack } from '@/data/api/get/vrack-details';
-import { useWebPageLink } from '@/hooks/useWebPageLink';
 import { useUpdateVrackDetails } from '@/hooks/vrack/useUpdateVrackDetails';
 
-import { EditableText } from './editable-text/EditableText';
+import { EditableText } from './editableText/EditableText';
 
 interface DashboardHeaderTitleProps {
   vrack: Vrack;
 }
 
 export const DashboardHeaderTitle = (props: DashboardHeaderTitleProps) => {
-  const { t } = useTranslation('dashboard');
-  const webPageLink = useWebPageLink();
   const vrack = props.vrack;
 
   const { mutate: updateVrackDetails } = useUpdateVrackDetails(vrack.serviceName);
 
-  /*
-   {
-    name: vrack.name,
-    description: vrack.description
-  }
-  */
   const onVrackNameUpdate = (updatedName: string) => {
     updateVrackDetails({
       name: updatedName,
       description: vrack.description,
     });
   };
-
-  const onDescriptionUpdate = (updatedDescription: string) => {
-    updateVrackDetails({
-      name: vrack.name,
-      description: updatedDescription,
-    });
-  };
-
-  const subTitleTextWithLink = webPageLink ? (
-    <>
-      {t('dashboard_subtitle_hints')}{' '}
-      <a target="_blank" href={webPageLink} rel="noreferrer">
-        {t('dashboard_subtitleLink')} <Icon name={ICON_NAME.externalLink} />
-      </a>
-    </>
-  ) : (
-    ''
-  );
   return (
     <div>
       <EditableText preset="heading-2" value={vrack.name} onUpdate={onVrackNameUpdate} />
-      <p className="mb-4 flex flex-col gap-2">
-        <span className="flex gap-4">
-          <Text className="font-semibold" preset="span">
-            {t('dashboard_serviceId')}:{' '}
-          </Text>
-          <Text preset="span">{vrack.serviceName}</Text>
-        </span>
-        <span className="flex gap-4">
-          <Text className="font-semibold" preset="span">
-            {t('dashboard_vrackDescription')}:{' '}
-          </Text>
-          <EditableText preset="span" value={vrack.description} onUpdate={onDescriptionUpdate} />
-        </span>
-      </p>
-      <p className="flex flex-col gap-2">
-        <Text preset="span">{t('dashboard_subtitle1')}</Text>
-        <Text preset="span">
-          {t('dashboard_subtitle2')} {subTitleTextWithLink}
-        </Text>
-      </p>
     </div>
   );
 };
--- packages/manager/apps/network-vrack/src/components/SurveyLink.tsx
+++ packages/manager/apps/network-vrack/src/components/SurveyLink.tsx
@@ -2,19 +2,14 @@ import { useTranslation } from 'react-i18next';
 
 import { BUTTON_SIZE, BUTTON_VARIANT, Button, ICON_NAME, Icon } from '@ovhcloud/ods-react';
 
-import { GuideMenu } from '@ovh-ux/muk';
-
-import { useGuideLinks } from '@/hooks/useGuideLinks';
 import { useSurveyLink } from '@/hooks/useSurveyLink';
 
-export const GuideMenuWithSurvey = () => {
+export const SurveyLink = () => {
   const { t } = useTranslation('dashboard');
-  const guideItems = useGuideLinks();
   const surveyUrl = useSurveyLink();
 
   return (
-    <div className="flex items-center gap-2">
-      <GuideMenu items={guideItems} />
+    <span className="whitespace-nowrap">
       {surveyUrl && (
         <Button
           variant={BUTTON_VARIANT.ghost}
@@ -27,6 +22,6 @@ export const GuideMenuWithSurvey = () => {
           {t('dashboard_surveyLink')}
         </Button>
       )}
-    </div>
+    </span>
   );
 };
