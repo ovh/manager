1f62814aa8ecf135c6180ccf8c2ea5e92c864c20
Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>
2025-12-11 15:21:29 +0100

    feat(backup-agent): add modals on first order
    ref: #BKP-702
    
    Signed-off-by: Vincent BONMARCHAND <vincent.bonmarchand.ext@corp.ovh.com>
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/bmc-backup-agent-baremetal/src/routes/Routes.constants.ts
+++ packages/manager/apps/bmc-backup-agent-baremetal/src/routes/Routes.constants.ts
@@ -5,12 +5,14 @@ const ROOT_URL = `/`;
 export const subRoutes = Object.freeze({
   onboarding: 'onboarding',
   firstOrder: 'first-order',
+  firstOrderConfirmation: 'confirmation',
 });
 
 export const urls = Object.freeze({
   root: ROOT_URL,
   onboarding: `${ROOT_URL}${subRoutes.onboarding}`,
   firstOrder: `${ROOT_URL}${subRoutes.firstOrder}`,
+  firstOrderConfirmation: `${ROOT_URL}${subRoutes.firstOrder}/${subRoutes.firstOrderConfirmation}`,
 });
 
 export const redirectionApp = appName;
