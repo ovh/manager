571242cad0cd2e7daf1ffb66686c168ca9e92702
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2025-12-12 10:33:47 +0100

    feat(backup-agent): unmock module with backup services endpoints
    ref: #BKP-704
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/backup-agent/src/mocks/tenant/backupPolicies.handler.ts
+++ packages/manager/modules/backup-agent/src/mocks/tenant/backupPolicies.handler.ts
@@ -10,7 +10,7 @@ export const getTenantBackupPolicieMocks = ({
   isTenantBackupPoliciesError = false,
 }: TTenantBackupPolicieMockParams = {}): Handler[] => [
   {
-    url: '/backup/tenant/vspc/:vspcTenantId/backupPolicies',
+    url: '/backupServices/tenant/vspc/:vspcTenantId/backupPolicies',
     response: () => {
       return isTenantBackupPoliciesError ? null : mockTenantBackupPolicies;
     },
