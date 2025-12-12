57b7b32a2c75510c5ecc734a60cf19a5efa4a6b3
ovh-cds <ovh-cds@users.noreply.github.com>
2025-12-12 18:21:19 +0000

    docs: update documentation [skip ci]

---
+++ storybook-static/assets/Text.stories-BvCfYSM8.js
@@ -0,0 +1,68 @@
+import{V as d}from"./lib-sJyaz0Xv-n8X0vl_V.js";import{I as t,a as v}from"./iam.constants-CZSXEnm9.js";import"./iframe-T2GtGINg.js";import"./QueryClientProvider-BPX08D6Z.js";import"./index-Bnop-kX6.js";import"./jsx-runtime-BRNY0I4F.js";import"./index-DnqQo6oJ.js";import"./index-4pTrEEYx.js";import"./Icon-DrfG5m-t-DQEf1CkA.js";import"./index-2w0W-O47-BJ19ihbZ.js";import"./ods-react60-0db41gCx.js";import"./Link-BWQEuWpd-BuJJSBob.js";import"./ods-react94-Bxf0SjVg.js";import"./Text-CcNd6qQr-D2KuMUPS.js";import"./Badge-YOwwmnsf-CJ8iv41_.js";import"./ods-react236-aAAP9SXj.js";import"./Button-BC-ipw2F-CXZv4wj7.js";import"./Spinner-CV0M2r8z-14hgiywo.js";import"./PopoverTrigger-4w4N6iLp-C-LGD_7X.js";import"./ods-react235-BTQ8kVBe.js";import"./portal-BnVBD_Bd-CMtQ-rJ7.js";import"./use-locale-context-cwTY9VYn-REDUMe7F.js";import"./use-presence-CVI6Oc63-Cg7Clpcl.js";import"./use-event-C16_WYdL-DF4JeFb4.js";import"./index-C7dNzIpW-BlUnPWgK.js";import"./index-BAi52a_A-BVjZSJoF.js";import"./index-DB9CF8IY-DC0Y2KvY.js";import"./TooltipTrigger-Iu3rt7LP-CcHcHvx1.js";import"./index-DDXR4bR2-rx0xkcPf.js";import"./FormFieldLabel-DerGjSSG-BvxIGX_B.js";import"./Card-D5fMAkqX-3SHynhw3.js";import"./Skeleton-tM01pV-R-CR0rfR_T.js";import"./Input-DcqcPYne-DrbRSC9d.js";import"./useI18n-C3-XAdTV-CxpmELcO.js";import"./Divider-THit99OS-DE11lmva.js";import"./Table-DeFepqjL-ijVQdBcH.js";import"./CheckboxLabel-BNqUGOsg-n6RGKdXc.js";import"./use-field-context-C3OOq-03-DvM5qnyl.js";import"./Tag-B7nBeuPX-CPCfmWrN.js";import"./SelectControl-C7d9WPuE-Bxj24dHo.js";import"./index-_7D5ljJ2-LyAubAEn.js";import"./ClipboardTrigger-TeqCWvgk-PkhT1oq0.js";import"./ModalTrigger-tZKHx0Fx-Dgqcdxhl.js";import"./dialog-trigger-DhWI7POy-2Tl6aad4.js";import"./render-strategy-BVcZ76SI-DDawKQXU.js";import"./TabContent-fruP9qhA-B5VnEOA-.js";import"./MessageIcon-yhpEHWAg-BtZxEFo4.js";import"./BreadcrumbItem-elPaHQlb-CNpnNsvr.js";import"./DrawerTrigger-omPTo2Bn-DB5oJcOC.js";import"./DatepickerControl-4VwQb-ep-Ct4shRTG.js";import"./ComboboxControl-sJOkWHeT-CC0kWNMj.js";import"./index-D_fe-3SC-C5ZKsUXO.js";const r={args:{urn:t.WITH_AUTH,iamActions:v,children:"lorem ipsum dolor sit amet"},parameters:{docs:{source:{code:`<Text 
+  urn="urn:v9:eu:resource:vrackServices:vrs-xxx-xxx"
+  iamActions={['vrackServices:apiovh:iam/resource/tag/remove']}
+>
+  lorem ipsum dolor sit amet
+</Text>`}}}},o={args:{urn:t.WITH_AUTH,iamActions:v},parameters:{docs:{source:{code:`<Text 
+  urn="urn:v9:eu:resource:vrackServices:vrs-xxx-xxx"
+  iamActions={['vrackServices:apiovh:iam/resource/tag/remove']}
+>
+  Confidential information
+</Text>`}}}},e={args:{...r.args,urn:t.WITHOUT_AUTH},parameters:{docs:{source:{code:`<Text 
+  urn="urn:v9:eu:resource:vrackServices:vrs-xxx-xxx-unauthorized"
+  iamActions={['vrackServices:apiovh:iam/resource/tag/remove']}
+>
+  HIDDEN (user without authorization)
+</Text>`}}}},vr={title:"Manager UI Kit/Components/Text",component:d,tags:["autodocs"],args:{...r.args,children:" Confidential information will be displayed only for users with authorization. For users without authorization, HIDDEN text will be displayed with warning message in the tooltip."},parameters:{docs:{description:{component:"The `Text` component is used to display content with IAM authorization."}}}};var i,a,s;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    urn: IAM_URNS.WITH_AUTH,
+    iamActions: IAM_ACTIONS,
+    children: 'lorem ipsum dolor sit amet'
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: \`<Text 
+  urn="urn:v9:eu:resource:vrackServices:vrs-xxx-xxx"
+  iamActions={['vrackServices:apiovh:iam/resource/tag/remove']}
+>
+  lorem ipsum dolor sit amet
+</Text>\`
+      }
+    }
+  }
+}`,...(s=(a=r.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};var n,m,c;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    urn: IAM_URNS.WITH_AUTH,
+    iamActions: IAM_ACTIONS
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: \`<Text 
+  urn="urn:v9:eu:resource:vrackServices:vrs-xxx-xxx"
+  iamActions={['vrackServices:apiovh:iam/resource/tag/remove']}
+>
+  Confidential information
+</Text>\`
+      }
+    }
+  }
+}`,...(c=(m=o.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};var p,u,x;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    urn: IAM_URNS.WITHOUT_AUTH
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: \`<Text 
+  urn="urn:v9:eu:resource:vrackServices:vrs-xxx-xxx-unauthorized"
+  iamActions={['vrackServices:apiovh:iam/resource/tag/remove']}
+>
+  HIDDEN (user without authorization)
+</Text>\`
+      }
+    }
+  }
+}`,...(x=(u=e.parameters)==null?void 0:u.docs)==null?void 0:x.source}}};const dr=["Default","TextWithAuthorization","TextWithoutAuthorization"];export{r as Default,o as TextWithAuthorization,e as TextWithoutAuthorization,dr as __namedExportsOrder,vr as default};
