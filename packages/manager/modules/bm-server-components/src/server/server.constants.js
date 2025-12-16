06729c007964d815bfd55e1e81689ab9a662f66c
b-fritz-ovh <benjamin.fritz.ext@corp.ovh.com>
2025-12-16 12:08:42 +0100

    feat(dedicated): add guide for game servers
    ref: #MANAGER-20589
    
    Signed-off-by: b-fritz-ovh <benjamin.fritz.ext@corp.ovh.com>

--- packages/manager/modules/bm-server-components/src/server/server.constants.js
+++ packages/manager/modules/bm-server-components/src/server/server.constants.js
@@ -36,6 +36,37 @@ export const SERVICE_TYPE = 'DEDICATED_SERVER';
 export const TAG_TAB_LABEL = 'Tags';
 export const BETA_LABEL = 'Beta';
 
+export const GAME_DDOS_GUIDE_URL = (language, article) =>
+  `https://help.ovhcloud.com/csm/${language}-dedicated-servers-game-ddos-firewall?id=kb_article_view&sysparm_article=${article}`;
+
+const frParams = ['fr', 'KB0060685'];
+const enIeParams = ['en-ie', 'KB0060676'];
+
+export const GAME_DDOS_GUIDE_PARAMS = {
+  FR: frParams,
+  DE: ['de', 'KB0060678'],
+  ES: ['es-es', 'KB0060679'],
+  IE: enIeParams,
+  IT: ['it', 'KB0060681'],
+  NL: enIeParams,
+  PL: ['pl', 'KB0060682'],
+  PT: ['pt', 'KB0060674'],
+  GB: ['en-gb', 'KB0060687'],
+  CA: ['en-ca', 'KB0060683'],
+  QC: ['fr-ca', 'KB0060688'],
+  US: ['en-gb', 'KB0060687'],
+  MA: frParams,
+  SN: frParams,
+  TN: frParams,
+  AU: ['en-au', 'KB0060684'],
+  IN: ['en-in', 'KB0067917'],
+  SG: ['en-sg', 'KB0060675'],
+  ASIA: ['asia', 'KB0060686'],
+  WE: enIeParams,
+  WS: enIeParams,
+  DEFAULT: enIeParams,
+};
+
 export default {
   DC_2_ISO,
   NEW_RANGE,
