d104b936a003d3e47510c39a94408e7c360f826c
Thomas Esseul <thomas.esseul@corp.ovh.com>
2025-12-16 11:23:39 +0100

    feat(pci-kubernetes): add page title
    ref: #TAPC-5437
    
    Signed-off-by: Thomas Esseul <thomas.esseul@corp.ovh.com>

--- packages/manager/apps/pci-kubernetes/src/pages/new/New.page.tsx
+++ packages/manager/apps/pci-kubernetes/src/pages/new/New.page.tsx
@@ -6,19 +6,24 @@ import { useProject } from '@ovh-ux/manager-pci-common';
 
 import { Breadcrumb } from '@/components/nav/Breadcrumb.component';
 
+import { CreateClusterForm } from './components/createClusterForm/CreateClusterForm.component';
+
 export default function New() {
   const { t } = useTranslation(['add', 'listing', 'common']);
   const project = useProject();
 
+  if (!project) {
+    return <Spinner />;
+  }
+
   return (
-    <main>
-      {!project ? (
-        <Spinner />
-      ) : (
-        <Breadcrumb
-          items={[{ label: t('listing:kube_list_title') }, { label: t('add:kubernetes_add') }]}
-        />
-      )}
-    </main>
+    <>
+      <Breadcrumb
+        items={[{ label: t('listing:kube_list_title') }, { label: t('add:kubernetes_add') }]}
+      />
+      <main className="mt-9">
+        <CreateClusterForm />
+      </main>
+    </>
   );
 }
