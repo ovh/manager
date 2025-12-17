ab78ac6ded30e9b88ab2bb9deb2d0205a7c888ab
David Arsène <david.arsene.ext@ovhcloud.com>
2025-12-17 18:23:42 +0100

    feat(observability): display tenant status and manage polling
    resolves: #MAOBS-177
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/observability/src/components/dashboard/GeneralInformationTile.props.ts
+++ packages/manager/apps/observability/src/components/dashboard/GeneralInformationTile.props.ts
@@ -1,11 +1,14 @@
 import { IamObject } from '@ovh-ux/muk';
 
+import { TenantResourceStatus } from '@/types/tenants.type';
+
 export interface GeneralInformationTileProps {
   tenantId: string;
   title?: string;
   description?: string;
   iam?: IamObject;
   endpoint?: string;
+  resourceStatus?: TenantResourceStatus;
   createdAt?: string;
   updatedAt?: string | null;
   isLoading: boolean;
