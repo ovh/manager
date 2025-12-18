f38c47a95c9a4c366d36ce673fd554662792f148
David Arsène <david.arsene.ext@ovhcloud.com>
2025-12-18 16:56:52 +0100

    fix(observability): fix tags filter inside datagrid
    fixes: #MAOBS-155
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/observability/src/utils/iam.constants.ts
+++ packages/manager/apps/observability/src/utils/iam.constants.ts
@@ -2,3 +2,7 @@ export const IAM_ACTIONS = {
   CREATE_TENANT: ['ldp:apiovh:output/metric/tenant/create'],
   EDIT_TENANT: ['ldp:apiovh:output/metric/tenant/edit'],
 } as const satisfies Record<string, string[]>;
+
+export const RESOURCE_TYPES = {
+  TENANT: 'ldp/tenant',
+} as const satisfies Record<string, string>;
