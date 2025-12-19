6efdb7208c9e8be447bd0d9f5af4177834b3dfab
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-12-19 16:35:23 +0100

    fix(muk): update datagrid header position and text overflow
    ref: #MANAGER-20626
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager-ui-kit/src/components/datagrid/table/table-head/table-header-content/TableHeaderContent.component.tsx
+++ packages/manager-ui-kit/src/components/datagrid/table/table-head/table-header-content/TableHeaderContent.component.tsx
@@ -11,13 +11,13 @@ const TableHeaderContentComponent = <T,>({
   onSortChange,
   enableSorting,
 }: TableHeaderContentProps<T>) => (
-  <thead className={`sticky top-[0px] z-10 bg-white overflow-hidden`}>
+  <thead className={`sticky top-[-1px] z-10 bg-white overflow-hidden`}>
     {headerGroups?.map((headerGroup) => (
       <tr key={headerGroup.id}>
         {headerGroup.headers.map((header) => (
           <th
             key={header.id}
-            className={`${contentAlignLeft ? 'text-left pl-4' : 'text-center'} whitespace-nowrap `}
+            className={`${contentAlignLeft ? 'text-left pl-4' : 'text-center'} whitespace-nowrap break-normal wrap-break-word`}
             style={{
               width: header.column.getSize(),
               minWidth: header.column.columnDef.minSize ?? 0,
