c2d7270297249a6cd5e5b7164d35c1ac4826a1fd
helabenkhalfallah <hela.ben-khalfallah@corp.ovh.com>
2025-12-21 13:53:03 +0100

    fix(*): build and tests issues
    ref: #MANAGER-20410
    
    Signed-off-by: helabenkhalfallah <hela.ben-khalfallah@corp.ovh.com>

--- packages/manager/apps/ips/.eslintrc.cjs
+++ packages/manager/apps/ips/.eslintrc.cjs
@@ -20,6 +20,7 @@ module.exports = {
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
+      'react/prop-types': 'off',
       'react/jsx-key': 'off'
     },
     overrides: [
