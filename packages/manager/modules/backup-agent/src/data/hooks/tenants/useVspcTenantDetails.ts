c340063a036515f918cc67abaceb57eb54f7ddb2
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-12-22 13:32:01 +0100

    fix(backup-agent): replace tenant by vspcTenant in dashboard
    ref: #BKP-704
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/modules/backup-agent/src/data/hooks/tenants/useVspcTenantDetails.ts
+++ packages/manager/modules/backup-agent/src/data/hooks/tenants/useVspcTenantDetails.ts
@@ -1,13 +1,16 @@
-import { DefinedInitialDataOptions, useQuery } from '@tanstack/react-query';
+import { UseQueryOptions, useQuery } from '@tanstack/react-query';
 
 import { getVSPCTenantDetails } from '@/data/api/tenants/tenants.requests';
+import { BackupServiceAwareQuery } from '@/types/Query.type';
 import { Resource } from '@/types/Resource.type';
-import { WithRegion } from '@/types/Utils.type';
 import { VSPCTenant } from '@/types/VspcTenant.type';
-import { mapTenantResourceToTenantResourceWithRegion } from '@/utils/mappers/mapTenantToTenantWithRegion';
 
 import { useBackupServicesId } from '../backup/useBackupServicesId';
 import { GET_VSPC_TENANTS_QUERY_KEY } from './useVspcTenants';
+
+type UseVSPCTenantDetailsParams = {
+  tenantId: string;
+} & Omit<UseQueryOptions<Resource<VSPCTenant>>, 'queryKey' | 'queryFn'>;
 
 export const BACKUP_VSPC_TENANT_DETAILS_QUERY_KEY = (vspcTenantID: string) => [
   ...GET_VSPC_TENANTS_QUERY_KEY,
@@ -17,22 +20,15 @@ export const BACKUP_VSPC_TENANT_DETAILS_QUERY_KEY = (vspcTenantID: string) => [
 export const useBackupVSPCTenantDetails = ({
   tenantId,
   ...options
-}: {
-  tenantId: string;
-} & Partial<
-  Omit<
-    DefinedInitialDataOptions<Resource<VSPCTenant>, unknown, Resource<WithRegion<VSPCTenant>>>,
-    'queryKey' | 'queryFn'
-  >
->) => {
-  const { backupServicesId } = useBackupServicesId();
+}: UseVSPCTenantDetailsParams): BackupServiceAwareQuery<Resource<VSPCTenant>, unknown> => {
+  const { backupServicesId, isPending } = useBackupServicesId();
 
-  return useQuery({
+  const query = useQuery({
     queryFn: () => getVSPCTenantDetails(backupServicesId, tenantId),
     queryKey: BACKUP_VSPC_TENANT_DETAILS_QUERY_KEY(tenantId),
     enabled: !!backupServicesId && !!tenantId,
-    select: (data): Resource<WithRegion<VSPCTenant>> =>
-      mapTenantResourceToTenantResourceWithRegion(data),
     ...options,
   });
+
+  return { ...query, isLoadingBackupServicesId: isPending };
 };
