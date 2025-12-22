fae6632224989e0bb9eced57b779173e6494841c
Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
2025-12-22 10:57:29 +0100

    feat(gcj): update data usage consent cookie name
    ref: #MANAGER-20176
    
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>

--- packages/manager/modules/gcj/src/hooks/dataUsageConsent/useDataUsageConsent.tsx
+++ packages/manager/modules/gcj/src/hooks/dataUsageConsent/useDataUsageConsent.tsx
@@ -2,6 +2,10 @@ import { useCallback, useEffect, useState } from 'react';
 import { useCookies } from 'react-cookie';
 import { TrackingPlugin } from '@ovh-ux/shell';
 import { Region } from '@ovh-ux/manager-config';
+import {
+  WEBSITE_PRIVACY_COOKIE_NAME,
+  WEBSITE_TRACKING_CONSENT_VALUE,
+} from './useDataUsage.constants';
 
 export const useDataUsageConsent = (
   region: Region,
@@ -10,21 +14,34 @@ export const useDataUsageConsent = (
   shouldRequestConsent: boolean;
   setConsent: (consent: boolean) => void;
 } => {
-  const [cookies, setCookies] = useCookies(['MANAGER_TRACKING'], {
+  const [cookies, setCookies] = useCookies([WEBSITE_PRIVACY_COOKIE_NAME], {
     doNotParse: true,
   });
   const [shouldRequestConsent, setShouldRequestConsent] = useState(
-    region !== 'US' && !cookies.MANAGER_TRACKING,
+    region !== 'US' && !cookies[WEBSITE_PRIVACY_COOKIE_NAME],
   );
 
   const setConsent = useCallback((consent: boolean) => {
-    setCookies('MANAGER_TRACKING', consent ? 1 : 0);
+    const expirationDate = new Date();
+    expirationDate.setMonth(expirationDate.getMonth() + 13);
+    setCookies(
+      WEBSITE_PRIVACY_COOKIE_NAME,
+      consent ? WEBSITE_TRACKING_CONSENT_VALUE : null,
+      {
+        path: '/',
+        expires: expirationDate,
+      },
+    );
     setShouldRequestConsent(false);
   }, []);
 
   useEffect(() => {
     if (shouldRequestConsent === false) {
-      trackingPlugin.onUserConsentFromModal(cookies.MANAGER_TRACKING === '1');
+      trackingPlugin.onUserConsentFromModal(
+        cookies[WEBSITE_PRIVACY_COOKIE_NAME]?.includes(
+          WEBSITE_TRACKING_CONSENT_VALUE,
+        ) ?? false,
+      );
     }
   }, [shouldRequestConsent]);
 
