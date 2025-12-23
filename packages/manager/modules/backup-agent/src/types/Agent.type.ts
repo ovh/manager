63a47f446ed92cf782228a3d3a9f4e89c3203805
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-12-23 18:08:20 +0100

    feat(backup-agent): replace id by productResourceName in edit agent
    ref: #BKP-713
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/modules/backup-agent/src/types/Agent.type.ts
+++ packages/manager/modules/backup-agent/src/types/Agent.type.ts
@@ -4,6 +4,7 @@ export type Agent = {
   id: string; // uuid
   ips: string[]; // array of ipv4 addresses
   name: string;
+  productResourceName: string;
   policy: string; // vspc backup policy configured for agent
   type: AgentType; // Backup Agent target Product type
 };
