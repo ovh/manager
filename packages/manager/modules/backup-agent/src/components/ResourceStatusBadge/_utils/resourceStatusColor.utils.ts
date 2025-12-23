9ad215ec259ebeef41aa32e2463ee760be25841b
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2025-12-23 17:58:16 +0100

    fix(backup-agent): fix agent listing status badge color
    ref: #BKP-713
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/modules/backup-agent/src/components/ResourceStatusBadge/_utils/resourceStatusColor.utils.ts
+++ packages/manager/modules/backup-agent/src/components/ResourceStatusBadge/_utils/resourceStatusColor.utils.ts
@@ -1,12 +1,16 @@
 import { ODS_BADGE_COLOR } from '@ovhcloud/ods-components';
 
-import { ResourceStatus } from '@/types/Resource.type';
+import { AlternativeStatus, ResourceStatus } from '@/types/Resource.type';
 
-export const vaultStatusColor: Record<ResourceStatus, ODS_BADGE_COLOR> = {
+export const vaultStatusColor: Record<ResourceStatus | AlternativeStatus, ODS_BADGE_COLOR> = {
   CREATING: ODS_BADGE_COLOR.information,
   DELETING: ODS_BADGE_COLOR.critical,
   ERROR: ODS_BADGE_COLOR.critical,
   READY: ODS_BADGE_COLOR.success,
   SUSPENDED: ODS_BADGE_COLOR.warning,
   UPDATING: ODS_BADGE_COLOR.information,
+  DISABLED: ODS_BADGE_COLOR.critical,
+  ENABLED: ODS_BADGE_COLOR.success,
+  NOT_CONFIGURED: ODS_BADGE_COLOR.information,
+  NOT_INSTALLED: ODS_BADGE_COLOR.information,
 };
