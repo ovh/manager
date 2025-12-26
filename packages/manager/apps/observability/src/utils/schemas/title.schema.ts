a8888d9d98703923e0aeeab3f4baa58a9f90f1d9
yjaaouane <yassine.jaaouane.ext@corp.ovh.com>
2025-12-27 00:44:32 +0100

    fix(observability): fix issue on name and description limit
    fixes: #MAOBS-158
    
    Signed-off-by: yjaaouane <yassine.jaaouane.ext@corp.ovh.com>

--- packages/manager/apps/observability/src/utils/schemas/title.schema.ts
+++ packages/manager/apps/observability/src/utils/schemas/title.schema.ts
@@ -2,10 +2,13 @@ import { z } from 'zod';
 
 import { NAMESPACES } from '@ovh-ux/manager-common-translations';
 
+export const TITLE_MAX_CHARS = 255;
+export const TITLE_MIN_CHARS = 4;
+
 export const createTitleSchema = (t: (key: string, options?: { value?: number }) => string) =>
   z
     .string()
     .trim()
     .min(1, t(`${NAMESPACES.FORM}:required_field`))
-    .min(4, t(`${NAMESPACES.FORM}:error_min_chars`, { value: 4 }))
-    .max(255, t(`${NAMESPACES.FORM}:error_max_chars`, { value: 255 }));
+    .min(TITLE_MIN_CHARS, t(`${NAMESPACES.FORM}:error_min_chars`, { value: TITLE_MIN_CHARS }))
+    .max(TITLE_MAX_CHARS, t('shared:form.error.max_chars', { value: TITLE_MAX_CHARS }));
