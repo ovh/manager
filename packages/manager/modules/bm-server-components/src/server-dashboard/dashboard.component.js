df3f31106a07fc9cdf3340a5aaaeaac58a9a73f1
soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>
2025-12-26 13:45:33 +0100

    feat(dedicated): cancelation FTC on US
    ref: #MANAGER-20070
    
    Signed-off-by: soufien mhelhali <soufien.mhelhali.ext@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/bm-server-components/src/server-dashboard/dashboard.component.js
+++ packages/manager/modules/bm-server-components/src/server-dashboard/dashboard.component.js
@@ -15,6 +15,7 @@ export default {
     goToCancelCommit: '<',
     goToCancelResiliation: '<',
     goToResiliation: '<',
+    goToResiliateService: '<',
     goToSgxIntroduction: '<',
     goToSgxManage: '<',
     goToMonitoringUpdate: '<',
