7efa1f25725bb791ddada661b8057171292ebcb7
ahmed sefiani <ahmed.sefiani.ext@corp.ovh.com>
2025-12-28 20:39:52 +0100

    feat(vcfaas): add confirmation keyword when deleting segment and network
    ref: #MANAGER-20508
    
    Signed-off-by: ahmed sefiani <ahmed.sefiani.ext@corp.ovh.com>

---
+++ packages/manager/apps/hpc-vmware-public-vcf-aas/src/components/deleteModal/DeleteModal.component.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import { Modal } from '@ovh-ux/manager-react-components';
+import { OdsText, OdsInput } from '@ovhcloud/ods-components/react';
+import { Trans, useTranslation } from 'react-i18next';
+
+import { ODS_MODAL_COLOR } from '@ovhcloud/ods-components';
+
+type DeleteModalProps = React.ComponentProps<typeof Modal> & {
+  confirmationKeyword?: string;
+};
+
+export function DeleteModal({
+  confirmationKeyword,
+  isPrimaryButtonDisabled,
+  children,
+  ...modalProps
+}: DeleteModalProps) {
+  const [inputValue, setInputValue] = useState('');
+  const { t } = useTranslation('terminate');
+
+  const isConfirmationValid = confirmationKeyword
+    ? inputValue === confirmationKeyword
+    : true;
+
+  return (
+    <Modal
+      {...modalProps}
+      type={ODS_MODAL_COLOR.critical}
+      isPrimaryButtonDisabled={isPrimaryButtonDisabled || !isConfirmationValid}
+    >
+      <div className="flex flex-col gap-4">
+        {children}
+
+        {!!confirmationKeyword && (
+          <>
+            <OdsText preset="caption">
+              <Trans
+                t={t}
+                i18nKey="terminate_confirm_deletion_service"
+                values={{ confirmationKeyword }}
+                components={{
+                  1: <strong />,
+                }}
+              />
+            </OdsText>
+            <OdsInput
+              name="confirm-delete-by-keyword"
+              value={inputValue}
+              onOdsChange={(e) => setInputValue(e.detail.value as string)}
+              placeholder={confirmationKeyword}
+              isClearable
+            />
+          </>
+        )}
+      </div>
+    </Modal>
+  );
+}
