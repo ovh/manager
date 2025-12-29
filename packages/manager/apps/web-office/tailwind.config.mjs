279b8fa9c38e765594bcad7df74e6e926c473703
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-12-29 15:04:23 +0100

    fix(web-office): correct tailwind config and css
    ref: #MANAGER-X
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager/apps/web-office/tailwind.config.mjs
+++ packages/manager/apps/web-office/tailwind.config.mjs
@@ -1,15 +1,26 @@
-import { createRequire } from 'node:module';
 import path from 'path';
 
-import config from '@ovh-ux/manager-tailwind-config';
-
-const require = createRequire(import.meta.url);
-
 /** @type {import('tailwindcss').Config} */
-export default {
-  ...config,
+module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
-    path.join(path.dirname(require.resolve('@ovh-ux/muk')), '**/*.{js,jsx,ts,tsx}'),
+    path.join(
+      path.dirname(require.resolve('@ovh-ux/manager-react-components')),
+      '**/*.{js,jsx,ts,tsx}',
+    ),
+    path.join(
+      path.dirname(require.resolve('@ovh-ux/muk')),
+      '**/*.{js,jsx,ts,tsx}',
+    ),
   ],
+  theme: {
+    screens: {
+      xs: '0',
+      sm: '36em',
+      md: '48em',
+      lg: '62em',
+      xl: '75em',
+      xxl: '87.5em',
+    },
+  },
 };
