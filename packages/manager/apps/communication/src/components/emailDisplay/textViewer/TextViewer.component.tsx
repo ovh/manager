be41202987f0fb8655e7fde5ee8cac652562d719
Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
2025-12-30 11:32:35 +0100

    feat(communication): change text-viewer to improve tables
    ref: #MANAGER-20448
    
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>

--- packages/manager/apps/communication/src/components/emailDisplay/textViewer/TextViewer.component.tsx
+++ packages/manager/apps/communication/src/components/emailDisplay/textViewer/TextViewer.component.tsx
@@ -2,8 +2,8 @@ import { OdsCard, OdsText } from '@ovhcloud/ods-components/react';
 
 export default function TextViewer({ text }: { text: string }) {
   return (
-    <OdsCard className="whitespace-pre-line p-4 overflow-y-auto">
-      <OdsText preset="paragraph">{text}</OdsText>
+    <OdsCard className="p-4 overflow-y-auto">
+      <OdsText preset="paragraph"><pre className="m-0 font-mono whitespace-pre-wrap break-words">{text}</pre></OdsText>
     </OdsCard>
   );
 }
