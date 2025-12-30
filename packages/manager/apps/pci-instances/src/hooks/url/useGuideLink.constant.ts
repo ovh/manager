6e52ac48c58f94a27d9af82a716bf3478acc6cea
tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>
2025-12-30 14:49:01 +0100

    feat(pci-instances): add flavor guide link for all subsidiary
    ref: #TAPC-5369
    
    Signed-off-by: tsiorifamonjena <tsiorifamonjena.tianjanahary.ext@corp.ovh.com>

--- packages/manager/apps/pci-instances/src/hooks/url/useGuideLink.constant.ts
+++ packages/manager/apps/pci-instances/src/hooks/url/useGuideLink.constant.ts
@@ -4,6 +4,10 @@ const HELP_ROOT = 'https://www.ovhcloud.com';
 
 const HELP_URL = 'https://help.ovhcloud.com/csm';
 
+const US_DOMAIN_ROOT = 'us.ovhcloud.com';
+
+const US_HELP_URL = `https://support.${US_DOMAIN_ROOT}/hc/en-us/articles`;
+
 type TGuideLinkGroup = {
   DEFAULT: string;
 } & {
@@ -30,8 +34,28 @@ export const GUIDE_LINKS: Record<TGuideKey, TGuideLinkGroup> = {
     FR: `${HELP_URL}/fr-public-cloud-deployments-modes?id=kb_article_view&sysparm_article=KB0066031`,
   },
   FLAVOR: {
-    DEFAULT: `${HELP_ROOT}/fr/public-cloud/virtual-instances/`,
-    FR: `${HELP_ROOT}/fr/public-cloud/virtual-instances/`,
+    DEFAULT: `${HELP_ROOT}/en/public-cloud/virtual-instances`,
+    ASIA: `${HELP_ROOT}/asia/public-cloud/virtual-instances`,
+    AU: `${HELP_ROOT}/en-au/public-cloud/virtual-instances`,
+    CA: `${HELP_ROOT}/en-ca/public-cloud/virtual-instances`,
+    DE: `${HELP_ROOT}/de/public-cloud/virtual-instances`,
+    ES: `${HELP_ROOT}/es-es/public-cloud/virtual-instances`,
+    FR: `${HELP_ROOT}/fr/public-cloud/virtual-instances`,
+    GB: `${HELP_ROOT}/en-gb/public-cloud/virtual-instances`,
+    IE: `${HELP_ROOT}/en-ie/public-cloud/virtual-instances`,
+    IN: `${HELP_ROOT}/en-in/public-cloud/virtual-instances`,
+    IT: `${HELP_ROOT}/it/public-cloud/virtual-instances`,
+    MA: `${HELP_ROOT}/fr-ma/public-cloud/virtual-instances`,
+    NL: `${HELP_ROOT}/nl/public-cloud/virtual-instances`,
+    PL: `${HELP_ROOT}/pl/public-cloud/virtual-instances`,
+    PT: `${HELP_ROOT}/pt/public-cloud/virtual-instances`,
+    QC: `${HELP_ROOT}/fr-ca/public-cloud/virtual-instances`,
+    SG: `${HELP_ROOT}/en-sg/public-cloud/virtual-instances`,
+    SN: `${HELP_ROOT}/fr-sn/public-cloud/virtual-instances`,
+    TN: `${HELP_ROOT}/fr-tn/public-cloud/virtual-instances`,
+    WE: `${HELP_ROOT}/en/public-cloud/virtual-instances`,
+    WS: `${HELP_ROOT}/es/public-cloud/virtual-instances`,
+    US: `https://${US_DOMAIN_ROOT}/public-cloud/virtual-instances`,
   },
   DISTRIBUTION_IMAGE: {
     DEFAULT: `${HELP_URL}/en-public-cloud-compute-getting-started?id=kb_article_view&sysparm_article=KB0051000`,
@@ -55,8 +79,7 @@ export const GUIDE_LINKS: Record<TGuideKey, TGuideLinkGroup> = {
     TN: `${HELP_URL}/fr-public-cloud-compute-getting-started?id=kb_article_view&sysparm_article=KB0051011`,
     WE: `${HELP_URL}/en-ie-public-cloud-compute-getting-started?id=kb_article_view&sysparm_article=KB0051014`,
     WS: `${HELP_URL}/es-public-cloud-compute-getting-started?id=kb_article_view&sysparm_article=KB0051006`,
-    US:
-      'https://support.us.ovhcloud.com/hc/en-us/articles/22188629448339-Increasing-Public-Cloud-Quotas',
+    US: `${US_HELP_URL}/22188629448339-Increasing-Public-Cloud-Quotas`,
   },
   DISTRIBUTION_IMAGE_LIFE_CYCLE: {
     DEFAULT: `${HELP_URL}/en-public-cloud-compute-vps-image-life-cycle?id=kb_article_view&sysparm_article=KB0050811`,
@@ -80,8 +103,7 @@ export const GUIDE_LINKS: Record<TGuideKey, TGuideLinkGroup> = {
     TN: `${HELP_URL}/fr-public-cloud-compute-vps-image-life-cycle?id=kb_article_view&sysparm_article=KB0050802`,
     WE: `${HELP_URL}/en-public-cloud-compute-vps-image-life-cycle?id=kb_article_view&sysparm_article=KB0050811`,
     WS: `${HELP_URL}/es-public-cloud-compute-vps-image-life-cycle?id=kb_article_view&sysparm_article=KB0050813`,
-    US:
-      'https://support.us.ovhcloud.com/hc/en-us/articles/42977670583315-Public-Cloud-and-VPS-Image-Lifecycle',
+    US: `${US_HELP_URL}/42977670583315-Public-Cloud-and-VPS-Image-Lifecycle`,
   },
   SSH_KEY: {
     DEFAULT: `${HELP_URL}/fr-public-cloud-compute-creating-ssh-keys?id=kb_article_view&sysparm_article=KB0064541`,
@@ -111,8 +133,7 @@ export const GUIDE_LINKS: Record<TGuideKey, TGuideLinkGroup> = {
     SG: `${HELP_URL}/en-sg-public-cloud-compute-back-up-instance?id=kb_article_view&sysparm_article=KB0069268`,
     SN: `${HELP_URL}/fr-public-cloud-compute-back-up-instance?id=kb_article_view&sysparm_article=KB0051155`,
     TN: `${HELP_URL}/fr-public-cloud-compute-back-up-instance?id=kb_article_view&sysparm_article=KB0051155`,
-    US:
-      'https://support.us.ovhcloud.com/hc/en-us/articles/4460743125395-Backing-up-an-instance',
+    US: `${US_HELP_URL}/4460743125395-Backing-up-an-instance`,
     WE: `${HELP_URL}/en-public-cloud-compute-back-up-instance?id=kb_article_view&sysparm_article=KB0069268`,
     WS: `${HELP_URL}/es-public-cloud-compute-back-up-instance?id=kb_article_view&sysparm_article=KB0051152`,
   },
