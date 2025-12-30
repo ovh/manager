073762fa08704124eacc0feb7c3c24f7f0d4308e
Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>
2025-12-30 10:20:33 +0100

    feat(*): test tailwind config in office and hosting
    ref: #MANAGER-20649
    
    Signed-off-by: Alex Boungnaseng <alex.boungnaseng.ext@corp.ovh.com>

--- packages/manager/apps/web-hosting/tailwind.config.mjs
+++ packages/manager/apps/web-hosting/tailwind.config.mjs
@@ -1,29 +1,27 @@
-import { createRequire } from 'node:module';
-import path from 'node:path';
-
 import baseConfig from '@ovh-ux/manager-tailwind-config';
+import path from 'node:path';
+import { createRequire } from 'node:module';
 
 const require = createRequire(import.meta.url);
 const pkgDir = (name) => path.dirname(require.resolve(`${name}/package.json`));
 const toGlob = (dir) => `${dir.replace(/\\/g, '/')}/**/*.{js,jsx,ts,tsx}`;
 
 const reactComponentsDir = pkgDir('@ovh-ux/muk');
-const pciCommonDir = pkgDir('@ovh-ux/manager-pci-common');
-
-const isPciConfig = '{{isPci}}';
 
 const baseTailwindConfig = [
   ...(baseConfig.content ?? []),
   './src/**/*.{js,jsx,ts,tsx}',
   toGlob(reactComponentsDir),
+  path.join(
+    path.dirname(require.resolve('@ovh-ux/manager-react-components')),
+    '**/*.{js,jsx,ts,tsx}',
+  ),
 ];
-
-export const pciTailwindConfig = [...baseTailwindConfig, toGlob(pciCommonDir)];
 
 /** @type {import('tailwindcss').Config} */
 export default {
   ...baseConfig,
-  content: isPciConfig === 'true' ? pciTailwindConfig : baseTailwindConfig,
+  content: baseTailwindConfig,
   corePlugins: {
     preflight: false,
   },
