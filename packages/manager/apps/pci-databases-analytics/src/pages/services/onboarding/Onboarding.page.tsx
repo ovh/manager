1e927c1e06a1c73ec1f7b54167b93ccc10efbb03
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2026-01-05 11:54:30 +0100

    feat(pci.databases): onboarding pages wording
    ref: #DATATR-2661
    
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- packages/manager/apps/pci-databases-analytics/src/pages/services/onboarding/Onboarding.page.tsx
+++ packages/manager/apps/pci-databases-analytics/src/pages/services/onboarding/Onboarding.page.tsx
@@ -1,74 +1,20 @@
-import { useTranslation } from 'react-i18next';
-import { Button } from '@datatr-ux/uxlib';
-import onboardingImgSrc from '@/../public/assets/onboarding-image.png';
-import Guides from '@/components/guides/Guides.component';
-import { GuideSections } from '@/types/guide';
-import Link from '@/components/links/Link.component';
-import { useTrackAction } from '@/hooks/useTracking';
-import { TRACKING } from '@/configuration/tracking.constants';
-import OnboardingTile from './OnboardingTile.component';
+import { useParams } from 'react-router-dom';
+import { CategoryEnum } from '@/types/cloud/project/database/engine';
+import OnboardingAnalytics from './OnboardingAnalytics.component';
+import OnboardingDatabases from './OnboardingDatabases.component';
 
 const Onboarding = () => {
-  const { t } = useTranslation('pci-databases-analytics/services/onboarding');
-  const track = useTrackAction();
+  const { category } = useParams<{ category: string }>();
 
-  return (
-    <div
-      data-testid="onbaording-container"
-      className="flex flex-col items-center gap-4"
-    >
-      <div className="w-full text-right">
-        <Guides
-          section={GuideSections.onboarding}
-          noEngineFilter
-          onGuideClick={(guide) =>
-            track(TRACKING.onboarding.guideClick(guide.title))
-          }
-        />
-      </div>
-      <h2>{t('title')}</h2>
-      <img
-        src={onboardingImgSrc}
-        className="max-h-[250px]"
-        alt="managed database service"
-      />
-      <p>{t('description1')}</p>
-      <p className="font-bold">{t('description2')}</p>
-      <p>{t('description3')}</p>
-      <Button data-testid="create-service-link" asChild>
-        <Link
-          onClick={() => track(TRACKING.onboarding.createDatabaseClick())}
-          to="../new"
-          className="hover:no-underline hover:text-primary-foreground"
-        >
-          {t('createServiceButton')}
-        </Link>
-      </Button>
-      <div className="flex flex-col md:grid md:grid-cols-3 gap-2">
-        <OnboardingTile
-          title={t('cardGuidestitle')}
-          description={t('cardGuidesDescription')}
-          content={t('cardGuidesContent')}
-          href="https://docs.ovh.com/gb/en/publiccloud/databases/"
-          linkName={t('cardLink')}
-        />
-        <OnboardingTile
-          title={t('cardQuickStartTitle')}
-          description={t('cardQuickStartDescription')}
-          content={t('cardQuickStartContent')}
-          href="https://docs.ovh.com/gb/en/publiccloud/databases/getting-started"
-          linkName={t('cardLink')}
-        />
-        <OnboardingTile
-          title={t('cardFreeTierTitle')}
-          description={t('cardFreeTierTescription')}
-          content={t('cardFreeTierContent')}
-          href="https://help.ovhcloud.com/csm/en-gb-public-cloud-databases-mongodb-getting-started?id=kb_article_view&sysparm_article=KB0061254"
-          linkName={t('cardLink')}
-        />
-      </div>
-    </div>
-  );
+  if (category === CategoryEnum.analysis) {
+    return <OnboardingAnalytics />;
+  }
+
+  if (category === CategoryEnum.operational) {
+    return <OnboardingDatabases />;
+  }
+
+  return null;
 };
 
 export default Onboarding;
