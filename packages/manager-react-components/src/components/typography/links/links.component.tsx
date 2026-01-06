8d24dabf32ebe00c1d6bda6bd21b1cbf41d6e666
Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
2026-01-06 17:12:12 +0100

    feat(mrc): extemd link for feedback survey
    gh-21416
    
    ref: #MANAGER-20595
    
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>

--- packages/manager-react-components/src/components/typography/links/links.component.tsx
+++ packages/manager-react-components/src/components/typography/links/links.component.tsx
@@ -5,11 +5,16 @@ import {
   ODS_LINK_ICON_ALIGNMENT,
 } from '@ovhcloud/ods-components';
 import { OdsLink } from '@ovhcloud/ods-components/react';
+import { useTranslation } from 'react-i18next';
+import { extractLanguageCode } from '../../../utils/translation-helper';
+import { generateSurveyLink, SurveyLinkQueryParams } from './links.util';
+import './translations';
 
 export enum LinkType {
   back = 'back',
   next = 'next',
   external = 'external',
+  survey = 'survey',
 }
 export enum IconLinkAlignmentType {
   left = 'left',
@@ -30,6 +35,9 @@ export interface LinksProps {
   type?: LinkType;
   onClickReturn?: () => void;
   isDisabled?: boolean;
+  surveyLink?: SurveyLinkQueryParams & {
+    applicationKey: string;
+  };
 }
 
 export const Links: React.FC<LinksProps> = ({
@@ -41,25 +49,44 @@ export const Links: React.FC<LinksProps> = ({
   color = ODS_LINK_COLOR.primary,
   iconAlignment,
   className = '',
+  surveyLink,
   ...props
-}) => (
-  <OdsLink
-    className={className}
-    href={href}
-    onClick={onClickReturn}
-    color={color}
-    {...(iconAlignment && {
-      iconAlignment: ODS_LINK_ICON_ALIGNMENT[iconAlignment],
-    })}
-    {...props}
-    {...(type === LinkType.back && {
-      icon: ODS_ICON_NAME.arrowLeft,
-      iconAlignment: ODS_LINK_ICON_ALIGNMENT.left,
-    })}
-    {...(type === LinkType.next && { icon: ODS_ICON_NAME.arrowRight })}
-    {...(type === LinkType.external && { icon: ODS_ICON_NAME.externalLink })}
-    label={label ?? children}
-  />
-);
+}) => {
+  const { t, i18n } = useTranslation('link');
+  return (
+    <OdsLink
+      className={className}
+      href={href}
+      onClick={onClickReturn}
+      color={color}
+      {...(iconAlignment && {
+        iconAlignment: ODS_LINK_ICON_ALIGNMENT[iconAlignment],
+      })}
+      {...props}
+      {...(type === LinkType.back && {
+        icon: ODS_ICON_NAME.arrowLeft,
+        iconAlignment: ODS_LINK_ICON_ALIGNMENT.left,
+      })}
+      label={label ?? children}
+      {...(type === LinkType.next && { icon: ODS_ICON_NAME.arrowRight })}
+      {...(type === LinkType.external && { icon: ODS_ICON_NAME.externalLink })}
+      {...(type === LinkType.survey && surveyLink && {
+        icon: ODS_ICON_NAME.emoticonSmile,
+        label: t('link_survey_preset_label'),
+        href: generateSurveyLink({
+          applicationKey: surveyLink.applicationKey,
+          queryParams: {
+            email: surveyLink.email,
+            nichandle: surveyLink.nichandle,
+            productId: surveyLink.productId,
+          },
+          languageCode: extractLanguageCode(i18n.language),
+        }),
+        target: '_blank',
+        rel: 'noopener noreferrer',
+      })}
+    />
+  )
+};
 
 export default Links;
