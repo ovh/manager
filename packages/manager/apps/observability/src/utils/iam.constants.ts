89b06c294f9f2c7932e5dd3f1506d1884e67fe3b
David Arsène <david.arsene.ext@ovhcloud.com>
2026-01-06 16:59:15 +0100

    feat(observability): add tenant subscribtion cancellation
    resolves: #MAOBS-191
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/observability/src/utils/iam.constants.ts
+++ packages/manager/apps/observability/src/utils/iam.constants.ts
@@ -1,6 +1,7 @@
 export const IAM_ACTIONS = {
   CREATE_TENANT: ['ldp:apiovh:output/metric/tenant/create'],
   EDIT_TENANT: ['ldp:apiovh:output/metric/tenant/edit'],
+  DELETE_SUBSCRIPTION: ['ldp:apiovh:output/metric/tenant/subscription/delete'],
 } as const satisfies Record<string, string[]>;
 
 export const RESOURCE_TYPES = {
