5a6ea7c824a5a8b44a3d64b9f0dfc5d0417e2607
Imane El Aiboud <imane.el-aiboud@corp.ovh.com>
2026-01-06 15:14:41 +0100

    feat(web-hosting): resfresh datagrid
    ref: #PUWEBPT-174

--- packages/manager/apps/web-hosting/src/data/hooks/webHosting/webHostingWebsite/useWebHostingWebsite.ts
+++ packages/manager/apps/web-hosting/src/data/hooks/webHosting/webHostingWebsite/useWebHostingWebsite.ts
@@ -1,10 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { getWebHostingWebsite } from '@/data/api/webHosting';
+import { DATAGRID_REFRESH_INTERVAL, DATAGRID_REFRESH_ON_MOUNT } from '@/utils';
 
 export const useWebHostingWebsite = (serviceName: string) => {
   return useQuery({
     queryKey: ['get', 'webhosting', 'resource', serviceName, 'website'],
     queryFn: () => getWebHostingWebsite(serviceName),
+    refetchInterval: DATAGRID_REFRESH_INTERVAL,
+    refetchOnMount: DATAGRID_REFRESH_ON_MOUNT,
   });
 };
