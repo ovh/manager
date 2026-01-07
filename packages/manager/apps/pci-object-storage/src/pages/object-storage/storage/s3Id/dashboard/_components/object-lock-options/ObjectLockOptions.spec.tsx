696d0ac043a4f3b4749ad5c6fc64dbd6598a4981
Arthur Bullet <arthur.bullet@ovhcloud.com>
2026-01-07 10:59:19 +0100

    feat(pci.os): add unit test in s3 dashboard
    ref: #DATATR-2572
    
    Signed-off-by: Arthur Bullet <arthur.bullet@ovhcloud.com>

---
+++ packages/manager/apps/pci-object-storage/src/pages/object-storage/storage/s3Id/dashboard/_components/object-lock-options/ObjectLockOptions.spec.tsx
@@ -0,0 +1,172 @@
+import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest';
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react';
+import { useToast } from '@datatr-ux/uxlib';
+import { RouterWithQueryClientWrapper } from '@/__tests__/helpers/wrappers/RouterWithQueryClientWrapper';
+import { mockedStorageContainer } from '@/__tests__/helpers/mocks/storageContainer/storageContainer';
+import storages from '@/types/Storages';
+import ObjectLockOptions from './ObjectLockOptions.sheet';
+import { Locale } from '@/hooks/useLocale';
+import { mockedUser } from '@/__tests__/helpers/mocks/user';
+import cloud from '@/types/Cloud';
+import * as s3Api from '@/data/api/storage/s3Storage.api';
+import { mockedObjStoError } from '@/__tests__/helpers/apiError';
+
+vi.mock('react-i18next', async (importOriginal) => {
+  const mod = await importOriginal<typeof import('react-i18next')>();
+  return {
+    ...mod,
+    useTranslation: () => ({
+      t: (key: string) => key,
+    }),
+  };
+});
+
+vi.mock('react-router-dom', async () => {
+  const mod = await vi.importActual('react-router-dom');
+  return {
+    ...mod,
+    useParams: () => ({
+      projectId: 'projectId',
+      name: 's3Name',
+      region: 'region',
+    }),
+    useNavigate: () => vi.fn(),
+  };
+});
+
+const mockedStorageContainerBis: cloud.StorageContainer = {
+  ...mockedStorageContainer,
+  objectLock: {
+    status: storages.ObjectLockStatusEnum.disabled,
+    // rule: { mode: storages.ObjectLockModeEnum.compliance, period: 'P1Y' },
+  },
+};
+vi.mock('@/data/api/storage/s3Storage.api', () => ({
+  getS3Storage: vi.fn(() => mockedStorageContainerBis),
+  updateS3Storage: vi.fn(() => mockedStorageContainerBis),
+}));
+
+describe('ObjectLockOptions Sheet', () => {
+  beforeEach(() => {
+    vi.restoreAllMocks();
+    vi.mock('@ovh-ux/manager-react-shell-client', async (importOriginal) => {
+      const mod = await importOriginal<
+        typeof import('@ovh-ux/manager-react-shell-client')
+      >();
+      return {
+        ...mod,
+        useShell: vi.fn(() => ({
+          i18n: {
+            getLocale: vi.fn(() => Locale.fr_FR),
+            onLocaleChange: vi.fn(),
+            setLocale: vi.fn(),
+          },
+          environment: {
+            getEnvironment: vi.fn(() => ({
+              getUser: vi.fn(() => mockedUser),
+            })),
+          },
+        })),
+      };
+    });
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('renders Object lock options sheet with disabled retention mode', async () => {
+    render(<ObjectLockOptions />, { wrapper: RouterWithQueryClientWrapper });
+    await waitFor(() => {
+      expect(screen.getByTestId('edit-object-lock-sheet')).toBeTruthy();
+      expect(
+        screen.getByTestId('edit-object-lock-confirm-button'),
+      ).toBeTruthy();
+      expect(screen.getByTestId('retention-mode-radio-group')).toBeTruthy();
+      expect(screen.getByTestId('retention-mode-radio-group')).toHaveAttribute(
+        'data-disabled',
+      );
+    });
+  });
+
+  it('Render sheet, enabled retention mode and throw api error on Error', async () => {
+    vi.mocked(s3Api.updateS3Storage).mockImplementation(() => {
+      throw mockedObjStoError;
+    });
+    render(<ObjectLockOptions />, { wrapper: RouterWithQueryClientWrapper });
+    await waitFor(() => {
+      expect(screen.getByTestId('edit-object-lock-sheet')).toBeTruthy();
+      expect(
+        screen.getByTestId('edit-object-lock-confirm-button'),
+      ).toBeTruthy();
+
+      expect(screen.getByTestId('retention-mode-radio-group')).toBeTruthy();
+      expect(screen.getByTestId('retention-mode-radio-group')).toHaveAttribute(
+        'data-disabled',
+      );
+    });
+
+    act(() => {
+      fireEvent.click(
+        screen.getByTestId('object-lock-retention-enabled-option'),
+      );
+    });
+
+    await waitFor(() => {
+      expect(
+        screen.getByTestId('retention-mode-radio-group'),
+      ).not.toHaveAttribute('data-disabled');
+    });
+
+    act(() => {
+      fireEvent.click(screen.getByTestId('edit-object-lock-confirm-button'));
+    });
+
+    await waitFor(() => {
+      expect(s3Api.updateS3Storage).toHaveBeenCalled();
+      expect(useToast().toast).toHaveBeenCalledWith({
+        title: 'toastErrorTitle',
+        variant: 'critical',
+        description: 'The provided data is invalid',
+      });
+    });
+  });
+
+  it('Render sheet, enabled retention mode and call on Success', async () => {
+    render(<ObjectLockOptions />, { wrapper: RouterWithQueryClientWrapper });
+    await waitFor(() => {
+      expect(screen.getByTestId('edit-object-lock-sheet')).toBeTruthy();
+    });
+
+    act(() => {
+      fireEvent.click(
+        screen.getByTestId('object-lock-retention-enabled-option'),
+      );
+    });
+
+    await waitFor(() => {
+      expect(
+        screen.getByTestId('retention-mode-radio-group'),
+      ).not.toHaveAttribute('data-disabled');
+    });
+
+    act(() => {
+      fireEvent.click(screen.getByTestId('object-lock-mode-compliance-option'));
+      fireEvent.click(screen.getByTestId('edit-object-lock-confirm-button'));
+    });
+
+    await waitFor(() => {
+      expect(s3Api.updateS3Storage).toHaveBeenCalled();
+      expect(useToast().toast).toHaveBeenCalledWith({
+        title: 'toastSuccessTitle',
+        description: 'editObjectLockToastSuccessDescription',
+      });
+    });
+  });
+});
