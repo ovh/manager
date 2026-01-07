ffd0accf98f7299b486b54776e2bda856622ee1f
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2026-01-07 09:41:21 +0100

    fix(backup-agent): fix download code on download modal
    ref: #BKP-713
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/backup-agent/src/pages/services/dashboard/agent/download/AgentDownload.page.tsx
+++ packages/manager/modules/backup-agent/src/pages/services/dashboard/agent/download/AgentDownload.page.tsx
@@ -88,6 +88,7 @@ export default function DownloadAgentPage() {
             <OdsSpinner />
           ) : (
             <DownloadCode
+              className="break-all"
               downloadLink={isDownloadEnabled ? downloadLink : '...'}
               canCopy={isDownloadEnabled}
             />
