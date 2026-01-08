1db493c7432d5696d08d1bafc819b85b701a720d
David Arsène <david.arsene.ext@ovhcloud.com>
2026-01-08 19:24:21 +0100

    feat(observability): disable tenant tags dashboard tab
    ref: #MAOBS-204
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/observability/src/components/dashboard/tab-navigation/TabNavigation.component.tsx
+++ packages/manager/apps/observability/src/components/dashboard/tab-navigation/TabNavigation.component.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { useLocation, useNavigate } from 'react-router-dom';
 
-import { Tab, TabList, Tabs, TabsValueChangeEvent } from '@ovhcloud/ods-react';
+import { Badge, Tab, TabList, Tabs, TabsValueChangeEvent } from '@ovhcloud/ods-react';
 
 import { TabNavigationProps } from '@/components/dashboard/tab-navigation/TabNavigation.props';
 import { getActiveTab, getTabByName } from '@/utils/tab-navigation.utils';
@@ -24,10 +24,15 @@ export const TabNavigation = ({ tabs }: TabNavigationProps) => {
     <Tabs value={activeTab} onValueChange={onTabChange}>
       <TabList>
         {tabs.map((tab) => {
-          const { name, title } = tab;
+          const { name, title, badge, disabled = false } = tab;
           return (
-            <Tab key={name} value={name}>
+            <Tab key={name} value={name} disabled={disabled} className="flex gap-3">
               {title}
+              {badge && (
+                <Badge color={badge.color} size="sm">
+                  {badge.label}
+                </Badge>
+              )}
             </Tab>
           );
         })}
