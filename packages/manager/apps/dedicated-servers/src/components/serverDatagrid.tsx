076b2acb5c54175d3d41250da83a8f6c393d83f3
Quentin Pavy <quentin.pavy.ext@ovhcloud.com>
2026-01-09 13:52:49 +0100

    feat(dedicated-servers): column visibility in views and switch views
    ref: #MANAGER-20621
    
    Signed-off-by: Quentin Pavy <quentin.pavy.ext@ovhcloud.com>

---
+++ packages/manager/apps/dedicated-servers/src/components/serverDatagrid.tsx
@@ -0,0 +1,70 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { Datagrid, useDataApi, RedirectionGuard } from '@ovh-ux/muk';
+import { ApiError } from '@ovh-ux/manager-core-api';
+import { VisibilityState } from '@tanstack/react-table';
+import OrderMenu from '@/components/orderMenu';
+import { useColumns } from '@/components/dataGridColumns';
+import { useDedicatedServer } from '@/hooks/useDedicatedServer';
+import { urls } from '@/routes/routes.constant';
+import { ErrorComponent } from '@/components/errorComponent';
+import { DedicatedServer } from '@/data/types/server.type';
+import { ViewContext } from '@/components/manageView/viewContext';
+
+export default function ServerDatagrid() {
+  const columns = useColumns();
+  const { columnVisibility, setColumnVisibility } = useContext(ViewContext);
+
+  const {
+    flattenData,
+    isError,
+    error,
+    totalCount,
+    hasNextPage,
+    fetchNextPage,
+    isLoading,
+    search,
+    sorting,
+    filters,
+  } = useDataApi<DedicatedServer>({
+    version: 'v6',
+    iceberg: true,
+    enabled: true,
+    route: `/dedicated/server`,
+    cacheKey: ['dedicated-servers', `/dedicated/server`],
+  });
+  const { error: errorListing, data: dedicatedServer } = useDedicatedServer();
+
+  return (
+    <>
+      {(isError || errorListing) && (
+        <ErrorComponent error={isError ? (error as ApiError) : errorListing} />
+      )}
+      {!isError && !errorListing && (
+        <RedirectionGuard
+          isLoading={isLoading || !dedicatedServer}
+          condition={dedicatedServer && dedicatedServer?.length === 0}
+          route={urls.onboarding}
+        >
+          {flattenData && (
+            <div>
+              <Datagrid
+                columns={columns}
+                data={flattenData}
+                totalCount={totalCount || 0}
+                hasNextPage={hasNextPage && !isLoading}
+                onFetchNextPage={fetchNextPage}
+                sorting={sorting}
+                isLoading={isLoading}
+                filters={filters}
+                columnVisibility={{ columnVisibility, setColumnVisibility }}
+                search={search}
+                topbar={<OrderMenu exportCsvData={{ columns, totalCount }} />}
+                resourceType="dedicatedServer"
+              />
+            </div>
+          )}
+        </RedirectionGuard>
+      )}
+    </>
+  );
+}
