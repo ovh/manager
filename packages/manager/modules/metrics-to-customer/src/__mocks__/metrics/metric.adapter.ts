368a1b38b5ad296ea4ff9b15c511b202dc275f53
yjaaouane <yassine.jaaouane.ext@corp.ovh.com>
2026-01-11 08:50:19 +0100

    feat(observability.metrics): update data mocks for pci instances queries
    ref: #MAOBS-148
    
    Signed-off-by: yjaaouane <yassine.jaaouane.ext@corp.ovh.com>

--- packages/manager/modules/metrics-to-customer/src/__mocks__/metrics/metric.adapter.ts
+++ packages/manager/modules/metrics-to-customer/src/__mocks__/metrics/metric.adapter.ts
@@ -1,39 +1,39 @@
-import {
-  fetchChartData as fetchChartDataFromMock,
+import {  
   getMetricKind as getMetricKindFromMock,
   getMetricKinds as getMetricKindsFromMock,
+  getMetricToken as getMetricTokenFromMock,
 } from '@/__mocks__/metrics/metric.mock';
 import { apiConfig } from '@/__mocks__/mock.config';
-import {
-  fetchChartData as fetchChartDataFromApi,
+import {  
   getMetricKindByName as getMetricKindFromApi,
   getMetricKinds as getMetricKindsFromApi,
+  getMetricToken as getMetricTokenFromApi,
 } from '@/data/api/metrics';
-import {
-  ObservabilityMetricDataParams,
+import {  
   ObservabilityMetricKindParams,
   ObservabilityMetricKindsParams,
+  ObservabilityServiceParams,
 } from '@/types/ClientApi.type';
-import { Kind, MetricData } from '@/types/observability.type';
-
-export const fetchChartData = async <TData>(
-  params: ObservabilityMetricDataParams,
-): Promise<MetricData<TData>> => {
-  const isMockEnabled = apiConfig.mode === 'mock';
-  console.info('[MOCK-ADAPTER][fetchChartData] Mock enabled -> ', isMockEnabled);
-  return isMockEnabled
-    ? fetchChartDataFromMock<TData>(params)
-    : fetchChartDataFromApi<TData>(params);
-};
+import { Kind } from '@/types/observability.type';
 
 export const getMetricKinds = async (params: ObservabilityMetricKindsParams): Promise<string[]> => {
-  const isMockEnabled = apiConfig.mode === 'mock';
+  const isMockEnabled = apiConfig.metric === 'mock';
   console.info('[MOCK-ADAPTER][getMetricKinds] Mock enabled -> ', isMockEnabled);
   return isMockEnabled ? getMetricKindsFromMock(params) : getMetricKindsFromApi(params);
 };
 
 export const getMetricKind = async (params: ObservabilityMetricKindParams): Promise<Kind> => {
-  const isMockEnabled = apiConfig.mode === 'mock';
+  const isMockEnabled = apiConfig.metric === 'mock';
   console.info('[MOCK-ADAPTER][getMetricKind] Mock enabled -> ', isMockEnabled);
   return isMockEnabled ? getMetricKindFromMock(params) : getMetricKindFromApi(params);
+};
+
+export const getMetricToken = async (
+  params: ObservabilityServiceParams,
+): Promise<string> => {
+  const isMockEnabled = apiConfig['metric-token'] === 'mock';
+  console.info('[MOCK-ADAPTER][getMetricToken] Mock enabled -> ', isMockEnabled);
+  return isMockEnabled
+    ? getMetricTokenFromMock(params)
+    : getMetricTokenFromApi(params);
 };
