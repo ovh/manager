14e31b8a634a7abebcf048cc3708dc53d0a1072d
yjaaouane <yassine.jaaouane.ext@corp.ovh.com>
2026-01-11 08:48:13 +0100

    refactor(observability.metrics): rename module to metrics to customer
    ref: #MAOBS-148
    
    Signed-off-by: yjaaouane <yassine.jaaouane.ext@corp.ovh.com>

--- packages/manager/modules/observability-to-customer/src/contexts/Dashboard.context.type.ts
+++
@@ -1,6 +0,0 @@
-import { DashboardState } from '@/contexts/Dashboard.context';
-
-export interface DashboardContextType {
-  state: DashboardState;
-  setState: React.Dispatch<React.SetStateAction<DashboardState>>;
-}
