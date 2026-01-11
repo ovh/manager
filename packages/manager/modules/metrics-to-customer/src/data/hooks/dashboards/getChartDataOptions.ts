b141bf95b89fb72071a925c718f101b613d554d9
yjaaouane <yassine.jaaouane.ext@corp.ovh.com>
2026-01-11 08:50:19 +0100

    feat(observability.metrics): update data mocks for pci instances queries
    ref: #MAOBS-148
    
    Signed-off-by: yjaaouane <yassine.jaaouane.ext@corp.ovh.com>

--- packages/manager/modules/metrics-to-customer/src/data/hooks/dashboards/getChartDataOptions.ts
+++ packages/manager/modules/metrics-to-customer/src/data/hooks/dashboards/getChartDataOptions.ts
@@ -1,14 +1,18 @@
 import { queryOptions } from '@tanstack/react-query';
 
-import { fetchChartData } from '@/__mocks__/metrics/metric.adapter';
+import { fetchChartData } from '@/__mocks__/metrics-for-manager/metrics-for-manager.adapter';
 import { getChartDataQueryKey } from '@/data/hooks/dashboards/getChartDataQueryKey';
 import { RequestPayload } from '@/types/RequestPayload.type';
 import { MetricData } from '@/types/observability.type';
 
-export const getChartDataOptions = <TData>(payload: RequestPayload, refreshInterval: number) => {
+export const getChartDataOptions = <TData>(
+  payload: RequestPayload,
+  refreshInterval: number,
+  metricToken: string,
+) => {
   return queryOptions({
-    queryKey: getChartDataQueryKey(payload),
-    queryFn: (): Promise<MetricData<TData>> => fetchChartData<TData>(payload),
+    queryKey: getChartDataQueryKey(payload, metricToken),
+    queryFn: (): Promise<MetricData<TData>> => fetchChartData<TData>(payload, metricToken),
     refetchInterval: refreshInterval > 0 ? refreshInterval * 1000 : false,
   });
 };
