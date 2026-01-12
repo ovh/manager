5968290c6877635facc6f6c721aaeb8601f95c82
Marc Monchablon <marc.monchablon.ext@corp.ovh.com>
2026-01-12 17:04:58 +0100

    fix(iam): disable previous date in permanent tokens
    ref: #MAIAM-70
    
    Signed-off-by: Marc Monchablon <marc.monchablon.ext@corp.ovh.com>

--- packages/manager/apps/identity-access-management/src/pages/permanentTokens/components/ExpiryDateInput.component.tsx
+++ packages/manager/apps/identity-access-management/src/pages/permanentTokens/components/ExpiryDateInput.component.tsx
@@ -38,6 +38,7 @@ export default function ExpiryDateInput({
   const [minutes, setMinutes] = useState<number>(1);
   const [hours, setHours] = useState<number>(1);
   const [days, setDays] = useState<number>(1);
+  const currentDate = new Date();
 
   const setExpiresIn = (expiryUnit: Unit, value: number) => {
     if (!model.active || model.mode !== 'duration') {
@@ -198,6 +199,8 @@ export default function ExpiryDateInput({
               setModel({ ...model, expiresAt: e.detail.value || null })
             }
             isDisabled={!model.active}
+            min={currentDate}
+            strategy="fixed"
           />
         )}
       </div>
