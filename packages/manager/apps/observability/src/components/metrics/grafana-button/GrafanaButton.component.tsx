2387c8e218f6cd32fd25537e3337ade40ba9f792
David Arsène <david.arsene.ext@ovhcloud.com>
2026-01-13 17:50:14 +0100

    feat(observability): disable grafana cta
    resolves: #MAOBS-209
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/observability/src/components/metrics/grafana-button/GrafanaButton.component.tsx
+++ packages/manager/apps/observability/src/components/metrics/grafana-button/GrafanaButton.component.tsx
@@ -1,14 +1,27 @@
 import { useTranslation } from 'react-i18next';
 
-import { BUTTON_VARIANT, Button, ICON_NAME, Icon } from '@ovhcloud/ods-react';
+import {
+  BUTTON_VARIANT,
+  Button,
+  ICON_NAME,
+  Icon,
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@ovhcloud/ods-react';
 
 export default function GrafanaButton() {
-  const { t } = useTranslation('tenants');
+  const { t } = useTranslation(['tenants', 'shared']);
 
   return (
-    <Button variant={BUTTON_VARIANT.outline}>
-      {t('dashboard.explore_in_grafana_button')}
-      <Icon name={ICON_NAME.externalLink} />
-    </Button>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button variant={BUTTON_VARIANT.outline} disabled={true}>
+          {t('tenants:dashboard.explore_in_grafana_button')}
+          <Icon name={ICON_NAME.externalLink} />
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent>{t('shared:coming_soon')}</TooltipContent>
+    </Tooltip>
   );
 }
