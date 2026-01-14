5789cdfa0a8412ce2ebbc0c7c34369f8bbb31bb7
David Arsène <david.arsene.ext@ovhcloud.com>
2026-01-14 16:45:24 +0100

    refactor(observability): pool changelog header layout
    ref: #MAOBS-168
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/observability/src/pages/tenants/TenantsBase.layout.tsx
+++ packages/manager/apps/observability/src/pages/tenants/TenantsBase.layout.tsx
@@ -1,29 +1,14 @@
 import { useTranslation } from 'react-i18next';
 
-import { BaseLayout, Breadcrumb, ChangelogMenu, Notifications } from '@ovh-ux/muk';
-
-import { AppConfig, CHANGELOG_LINKS, appName } from '@/App.constants';
-import MetricsGuideHeader from '@/components/metrics/guide-header/MetricsGuideHeader.component';
+import MetricsBaseLayout from '@/pages/metrics/MetricsBase.layout';
 import TenantsOutlet from '@/pages/tenants/Tenants.outlet';
 
 export default function TenantsBaseLayout({ children }: { children?: React.ReactNode }) {
   const { t } = useTranslation('tenants');
   return (
-    <BaseLayout
-      breadcrumb={
-        <Breadcrumb appName={appName} rootLabel={AppConfig.rootLabel} hideRootLabel={true} />
-      }
-      header={{
-        title: t('listing.title'),
-        guideMenu: <MetricsGuideHeader />,
-        changelogButton: <ChangelogMenu links={CHANGELOG_LINKS} />,
-      }}
-      message={<Notifications />}
-    >
-      <>
-        {children}
-        <TenantsOutlet />
-      </>
-    </BaseLayout>
+    <MetricsBaseLayout title={t('listing.title')}>
+      {children}
+      <TenantsOutlet />
+    </MetricsBaseLayout>
   );
 }
