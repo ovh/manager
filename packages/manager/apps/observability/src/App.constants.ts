56a8f46fae18e0172b891b358546a87abfe3779a
David Arsène <david.arsene.ext@ovhcloud.com>
2026-01-14 16:46:23 +0100

    feat(observability): update changelog links
    resolves: #MAOBS-168
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/observability/src/App.constants.ts
+++ packages/manager/apps/observability/src/App.constants.ts
@@ -13,10 +13,8 @@ export const APP_FEATURES = {
 } as const;
 
 export const CHANGELOG_LINKS: ChangelogMenuLinks = {
-  changelog:
-    'https://github.com/orgs/ovh/projects/16/views/6?pane=info&sliceBy%5Bvalue%5D=Observability',
-  roadmap:
-    'https://github.com/orgs/ovh/projects/16/views/1?pane=info&sliceBy%5Bvalue%5D=Observability',
+  changelog: 'https://github.com/orgs/ovh/projects/16/views/6?sliceBy[value]=Observability',
+  roadmap: 'https://github.com/orgs/ovh/projects/16/views/1?sliceBy%5Bvalue%5D=Observability',
   'feature-request':
     'https://github.com/ovh/observability-roadmap/issues/new?assignees=&labels=&projects=&template=feature_request.md&title=',
 };
