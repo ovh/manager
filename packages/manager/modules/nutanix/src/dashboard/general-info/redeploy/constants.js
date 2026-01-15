ed5e807955bb5015d8f8568255a18b4fb5727772
antoine.pelletant <antoine.pelletant@corp.ovh.com>
2026-01-15 10:20:56 +0100

    feat(nutanix): modify prismcentral size during redeploy
    ref: #NTNX-415
    
    Signed-off-by: antoine.pelletant <antoine.pelletant@corp.ovh.com>

--- packages/manager/modules/nutanix/src/dashboard/general-info/redeploy/constants.js
+++ packages/manager/modules/nutanix/src/dashboard/general-info/redeploy/constants.js
@@ -14,6 +14,18 @@ export const PRISM_CENTRAL_TYPES = [
   PRISM_CENTRAL_TYPE_SCALE,
 ];
 
+export const PRISM_CENTRAL_SIZE_XL = 'xlarge';
+export const PRISM_CENTRAL_SIZE_L = 'large';
+export const PRISM_CENTRAL_SIZE_S = 'small';
+export const PRISM_CENTRAL_SIZE_XS = 'xsmall';
+
+export const PRISM_CENTRAL_SIZES = [
+  { label: 'XLarge', value: PRISM_CENTRAL_SIZE_XL },
+  { label: 'Large', value: PRISM_CENTRAL_SIZE_L },
+  { label: 'Small', value: PRISM_CENTRAL_SIZE_S },
+  { label: 'Tiny', value: PRISM_CENTRAL_SIZE_XS },
+];
+
 export const IPV4_REGEX = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
 
 export const IPV4_BLOCK_REGEX = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\/([0-9]|[1-2][0-9]|3[0-2])?$/;
@@ -26,6 +38,7 @@ export default {
   PRISM_CENTRAL_TYPE_ALONE,
   PRISM_CENTRAL_TYPE_SCALE,
   PRISM_CENTRAL_TYPES,
+  PRISM_CENTRAL_SIZES,
   IPV4_REGEX,
   IPV4_BLOCK_REGEX,
 };
