f7337306ee6179c907247453474e237f7fe7ea53
ovh-cds <ovh-cds@users.noreply.github.com>
2026-01-15 19:25:23 +0000

    docs: update documentation [skip ci]

---
+++ storybook-static/assets/tile.stories-B9ESIPZl.js
@@ -0,0 +1,136 @@
+import{j as e}from"./jsx-runtime-BRNY0I4F.js";import{t as r}from"./Icon-DrfG5m-t-DQEf1CkA.js";import{l as n}from"./ods-react60-0db41gCx.js";import{y as j}from"./Link-BWQEuWpd-BuJJSBob.js";import{h as s}from"./Text-CcNd6qQr-D2KuMUPS.js";import{n as f}from"./Badge-YOwwmnsf-CJ8iv41_.js";import{x as w}from"./Button-BC-ipw2F-CXZv4wj7.js";import{m as S}from"./Skeleton-tM01pV-R-CR0rfR_T.js";import{m as i,C as L,n as g}from"./lib-D44cvI9Y-BEmnv0Ot.js";import"./index-Bnop-kX6.js";import"./index-2w0W-O47-BJ19ihbZ.js";import"./ods-react94-Bxf0SjVg.js";import"./ods-react236-aAAP9SXj.js";import"./Spinner-CV0M2r8z-14hgiywo.js";import"./iframe-DVaWS9ZP.js";import"./QueryClientProvider-BPX08D6Z.js";import"./index-DnqQo6oJ.js";import"./index-4pTrEEYx.js";import"./PopoverTrigger-4w4N6iLp-C-LGD_7X.js";import"./ods-react235-BTQ8kVBe.js";import"./portal-BnVBD_Bd-CMtQ-rJ7.js";import"./use-locale-context-cwTY9VYn-REDUMe7F.js";import"./use-presence-CVI6Oc63-Cg7Clpcl.js";import"./use-event-C16_WYdL-DF4JeFb4.js";import"./index-C7dNzIpW-BlUnPWgK.js";import"./index-BAi52a_A-BVjZSJoF.js";import"./index-DB9CF8IY-DC0Y2KvY.js";import"./TooltipTrigger-Iu3rt7LP-CcHcHvx1.js";import"./index-DDXR4bR2-rx0xkcPf.js";import"./FormFieldLabel-DerGjSSG-BvxIGX_B.js";import"./Card-D5fMAkqX-3SHynhw3.js";import"./Input-DcqcPYne-DrbRSC9d.js";import"./useI18n-C3-XAdTV-CxpmELcO.js";import"./Divider-THit99OS-DE11lmva.js";import"./Table-DeFepqjL-ijVQdBcH.js";import"./CheckboxLabel-BNqUGOsg-n6RGKdXc.js";import"./use-field-context-C3OOq-03-DvM5qnyl.js";import"./Tag-B7nBeuPX-CPCfmWrN.js";import"./SelectControl-C7d9WPuE-Bxj24dHo.js";import"./index-_7D5ljJ2-LyAubAEn.js";import"./ClipboardTrigger-TeqCWvgk-PkhT1oq0.js";import"./ModalTrigger-tZKHx0Fx-Dgqcdxhl.js";import"./dialog-trigger-DhWI7POy-2Tl6aad4.js";import"./render-strategy-BVcZ76SI-DDawKQXU.js";import"./TabContent-fruP9qhA-B5VnEOA-.js";import"./MessageIcon-yhpEHWAg-BtZxEFo4.js";import"./BreadcrumbItem-elPaHQlb-CNpnNsvr.js";import"./DrawerTrigger-omPTo2Bn-DB5oJcOC.js";import"./DatepickerControl-4VwQb-ep-Ct4shRTG.js";import"./ComboboxControl-sJOkWHeT-CC0kWNMj.js";import"./index-D_fe-3SC-C5ZKsUXO.js";const v=[{id:1,href:"https://ovhcloud.com",label:"Action 1"},{id:2,onClick:()=>window.open("https://ovhcloud.com","_blank","noopener"),label:"Action 2"}],t=()=>e.jsxs(i.Root,{title:"Sample Tile Header",children:[e.jsxs(i.Item.Root,{children:[e.jsx(i.Item.Term,{label:"Term"}),e.jsx(i.Item.Description,{label:"Description"})]}),e.jsxs(i.Item.Root,{children:[e.jsx(i.Item.Term,{label:"Last Term"}),e.jsx(i.Item.Description,{divider:!1,children:e.jsx(s,{children:"Description without Divider"})})]})]});t.parameters={docs:{source:{code:`<Tile.Root title="Sample Tile Header">
+  <Tile.Item.Root>
+    <Tile.Item.Term label="Term" />
+    <Tile.Item.Description label="Description" />
+  </Tile.Item.Root>
+  <Tile.Item.Root>
+    <Tile.Item.Term label="Last Term" />
+    <Tile.Item.Description divider={false}>
+      <Text>Description without Divider</Text>
+    </Tile.Item.Description>
+  </Tile.Item.Root>
+</Tile.Root>`}}};const o=()=>e.jsxs(i.Root,{title:"Sample Tile Header",children:[e.jsxs(i.Item.Root,{children:[e.jsx(i.Item.Term,{label:"Term"}),e.jsx(i.Item.Description,{label:"Description"})]}),e.jsxs(i.Item.Root,{children:[e.jsx(i.Item.Term,{label:"Last Term"}),e.jsx(i.Item.Description,{divider:!1,label:"Sample Description"}),e.jsx(i.Item.Description,{children:e.jsxs(j,{href:"https://www.ovhcloud.com",children:["Link ",e.jsx(r,{name:n.externalLink})]})})]})]});o.parameters={docs:{source:{code:`<Tile.Root title="Sample Tile Header">
+  <Tile.Item.Root>
+    <Tile.Item.Term label="Term" />
+    <Tile.Item.Description label="Description" />
+  </Tile.Item.Root>
+  <Tile.Item.Root>
+    <Tile.Item.Term label="Last Term" />
+    <Tile.Item.Description divider={false} label="Sample Description" />
+    <Tile.Item.Description>
+      <Link href="https://www.ovhcloud.com">
+        Link <Icon name={ICON_NAME.externalLink} />
+      </Link>
+    </Tile.Item.Description>
+  </Tile.Item.Root>
+</Tile.Root>`}}};const l=()=>e.jsxs(i.Root,{title:"Sample Tile Header",children:[e.jsxs(i.Item.Root,{children:[e.jsx(i.Item.Term,{label:"Term",tooltip:"Lorem ipsum dolor sit amet.",actions:e.jsx(L,{id:"action-menu",items:v,isCompact:!0})}),e.jsx(i.Item.Description,{label:"Description"})]}),e.jsxs(i.Item.Root,{children:[e.jsx(i.Item.Term,{label:"Last Term"}),e.jsx(i.Item.Description,{divider:!1,label:"Sample Description"})]})]});l.parameters={docs:{source:{code:`const actionItems = [
+  { id: 1, href: 'https://ovhcloud.com', label: 'Action 1' },
+  { id: 2, onClick: () => handleAction(), label: 'Action 2' },
+];
+
+<Tile.Root title="Sample Tile Header">
+  <Tile.Item.Root>
+    <Tile.Item.Term
+      label="Term"
+      tooltip="Lorem ipsum dolor sit amet."
+      actions={<ActionMenu id="action-menu" items={actionItems} isCompact />}
+    />
+    <Tile.Item.Description label="Description" />
+  </Tile.Item.Root>
+</Tile.Root>`}}};const m=()=>e.jsx("div",{className:"w-1/3",children:e.jsxs(i.Root,{title:"Tile Header",children:[e.jsxs(i.Item.Root,{children:[e.jsx(i.Item.Term,{label:"Sample Term"}),e.jsx(i.Item.Description,{label:"Sample Description"})]}),e.jsxs(i.Item.Root,{children:[e.jsx(i.Item.Term,{label:"Edit Desription",tooltip:"Lorem ipsum dolor sit amet."}),e.jsx(i.Item.Description,{children:e.jsxs("div",{className:"flex justify-between",children:[e.jsx(s,{preset:"span",children:"Sample Description"}),e.jsx(w,{variant:"ghost",size:"xs",children:e.jsx(r,{name:n.pen})})]})})]}),e.jsxs(i.Item.Root,{children:[e.jsx(i.Item.Term,{label:"Long Text"}),e.jsx(i.Item.Description,{children:e.jsx(s,{preset:"paragraph",children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."})})]}),e.jsxs(i.Item.Root,{children:[e.jsx(i.Item.Term,{label:"Item with Description & Link"}),e.jsx(i.Item.Description,{label:"Lorem ipsum dolor sit amet",divider:!1}),e.jsx(i.Item.Description,{children:e.jsxs(j,{href:"https://www.ovhcloud.com",children:["Link ",e.jsx(r,{name:n.externalLink})]})})]}),e.jsxs(i.Item.Root,{children:[e.jsx(i.Item.Term,{label:"Item with Badge"}),e.jsx(i.Item.Description,{children:e.jsx(f,{children:"Coming Soon"})})]}),e.jsxs(i.Item.Root,{children:[e.jsx(i.Item.Term,{label:"Loading"}),e.jsx(i.Item.Description,{children:e.jsx(S,{})})]}),e.jsxs(i.Item.Root,{children:[e.jsx(i.Item.Term,{label:"Clipboard"}),e.jsx(i.Item.Description,{children:e.jsx(g,{className:"w-full",value:"example value"})})]}),e.jsxs(i.Item.Root,{children:[e.jsx(i.Item.Term,{label:"Action Menu",actions:e.jsx(L,{id:"action-menu",isCompact:!0,items:v})}),e.jsx(i.Item.Description,{label:"Test Value",divider:!1})]})]})}),we={title:"Manager UI Kit/Components/Tile",component:i,tags:["autodocs"],argTypes:{},args:{}};var a,p,c;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`() => <Tile.Root title="Sample Tile Header">
+    <Tile.Item.Root>
+      <Tile.Item.Term label="Term" />
+      <Tile.Item.Description label="Description" />
+    </Tile.Item.Root>
+    <Tile.Item.Root>
+      <Tile.Item.Term label="Last Term" />
+      <Tile.Item.Description divider={false}>
+        <Text>Description without Divider</Text>
+      </Tile.Item.Description>
+    </Tile.Item.Root>
+  </Tile.Root>`,...(c=(p=t.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};var T,d,I;o.parameters={...o.parameters,docs:{...(T=o.parameters)==null?void 0:T.docs,source:{originalSource:`() => <Tile.Root title="Sample Tile Header">
+    <Tile.Item.Root>
+      <Tile.Item.Term label="Term" />
+      <Tile.Item.Description label="Description" />
+    </Tile.Item.Root>
+    <Tile.Item.Root>
+      <Tile.Item.Term label="Last Term" />
+      <Tile.Item.Description divider={false} label="Sample Description" />
+      <Tile.Item.Description>
+        <Link href="https://www.ovhcloud.com">
+          Link <Icon name={ICON_NAME.externalLink} />
+        </Link>
+      </Tile.Item.Description>
+    </Tile.Item.Root>
+  </Tile.Root>`,...(I=(d=o.parameters)==null?void 0:d.docs)==null?void 0:I.source}}};var x,u,D;l.parameters={...l.parameters,docs:{...(x=l.parameters)==null?void 0:x.docs,source:{originalSource:`() => <Tile.Root title="Sample Tile Header">
+    <Tile.Item.Root>
+      <Tile.Item.Term label="Term" tooltip="Lorem ipsum dolor sit amet." actions={<ActionMenu id="action-menu" items={actionItems} isCompact />} />
+      <Tile.Item.Description label="Description" />
+    </Tile.Item.Root>
+    <Tile.Item.Root>
+      <Tile.Item.Term label="Last Term" />
+      <Tile.Item.Description divider={false} label="Sample Description" />
+    </Tile.Item.Root>
+  </Tile.Root>`,...(D=(u=l.parameters)==null?void 0:u.docs)==null?void 0:D.source}}};var h,R,b;m.parameters={...m.parameters,docs:{...(h=m.parameters)==null?void 0:h.docs,source:{originalSource:`() => {
+  return <div className="w-1/3">
+      <Tile.Root title="Tile Header">
+        <Tile.Item.Root>
+          <Tile.Item.Term label="Sample Term" />
+          <Tile.Item.Description label="Sample Description" />
+        </Tile.Item.Root>
+        <Tile.Item.Root>
+          <Tile.Item.Term label="Edit Desription" tooltip="Lorem ipsum dolor sit amet." />
+          <Tile.Item.Description>
+            <div className="flex justify-between">
+              <Text preset="span">Sample Description</Text>
+              <Button variant="ghost" size="xs">
+                <Icon name={ICON_NAME.pen}></Icon>
+              </Button>
+            </div>
+          </Tile.Item.Description>
+        </Tile.Item.Root>
+        <Tile.Item.Root>
+          <Tile.Item.Term label="Long Text" />
+          <Tile.Item.Description>
+            <Text preset="paragraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Text>
+          </Tile.Item.Description>
+        </Tile.Item.Root>
+        <Tile.Item.Root>
+          <Tile.Item.Term label="Item with Description & Link"></Tile.Item.Term>
+          <Tile.Item.Description label="Lorem ipsum dolor sit amet" divider={false}></Tile.Item.Description>
+          <Tile.Item.Description>
+            <Link href="https://www.ovhcloud.com">
+              Link <Icon name={ICON_NAME.externalLink} />
+            </Link>
+          </Tile.Item.Description>
+        </Tile.Item.Root>
+        <Tile.Item.Root>
+          <Tile.Item.Term label="Item with Badge"></Tile.Item.Term>
+          <Tile.Item.Description>
+            <Badge>Coming Soon</Badge>
+          </Tile.Item.Description>
+        </Tile.Item.Root>
+        <Tile.Item.Root>
+          <Tile.Item.Term label="Loading" />
+          <Tile.Item.Description>
+            <Skeleton />
+          </Tile.Item.Description>
+        </Tile.Item.Root>
+        <Tile.Item.Root>
+          <Tile.Item.Term label="Clipboard" />
+          <Tile.Item.Description>
+            <Clipboard className="w-full" value="example value" />
+          </Tile.Item.Description>
+        </Tile.Item.Root>
+        <Tile.Item.Root>
+          <Tile.Item.Term label="Action Menu" actions={<ActionMenu id="action-menu" isCompact items={actionItems} />} />
+          <Tile.Item.Description label="Test Value" divider={false}></Tile.Item.Description>
+        </Tile.Item.Root>
+      </Tile.Root>
+    </div>;
+}`,...(b=(R=m.parameters)==null?void 0:R.docs)==null?void 0:b.source}}};const Se=["SimpleTile","TileWithLink","TileWithActionMenuAndTooltip","CompleteExample"];export{m as CompleteExample,t as SimpleTile,l as TileWithActionMenuAndTooltip,o as TileWithLink,Se as __namedExportsOrder,we as default};
