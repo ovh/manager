c80a6d948114bdd6f92be98b72d4b61d696d3ab6
yjaaouane <yassine.jaaouane.ext@corp.ovh.com>
2026-01-16 18:10:33 +0100

    refactor(metrics-module): rename context and test files
    ref: #MAOBS-194
    
    Signed-off-by: yjaaouane <yassine.jaaouane.ext@corp.ovh.com>

--- packages/manager/modules/metrics-to-customer/src/MetricsToCustomer.translations.ts
+++ packages/manager/modules/metrics-to-customer/src/MetricsToCustomer.translations.ts
@@ -3,6 +3,7 @@ import { MODULE_NAME } from '@/MetricsToCustomer.constants';
 export const NAMESPACE_PREFIX = `${MODULE_NAME}`;
 
 export const NAMESPACES = {
+  MODULE: `${NAMESPACE_PREFIX}/module`,
   CHART: `${NAMESPACE_PREFIX}/chart`,
   DASHBOARDS: `${NAMESPACE_PREFIX}/dashboards`,
   TIME_CONTROLS: `${NAMESPACE_PREFIX}/timeControls`,
