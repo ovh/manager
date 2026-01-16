6e40a16083a2cd2ff59ee17764efd22ad5172ae8
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2026-01-16 12:06:30 +0100

    feat(backup-agent): add server column on agent listing
    ref: #BKP-734
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/backup-agent/src/pages/services/dashboard/agent/_components/AgentProductResourceNameCell.component.tsx
+++ packages/manager/modules/backup-agent/src/pages/services/dashboard/agent/_components/AgentProductResourceNameCell.component.tsx
@@ -2,6 +2,8 @@ import { DataGridTextCell } from '@ovh-ux/manager-react-components';
 
 import { Agent } from '@/types/Agent.type';
 
-export const AgentIpsCell = ({ ips = [] }: Pick<Agent, 'ips'>) => {
-  return <DataGridTextCell>{ips.join(', ')}</DataGridTextCell>;
+export const AgentProductResourceNameCell = ({
+  productResourceName = '',
+}: Pick<Agent, 'productResourceName'>) => {
+  return <DataGridTextCell>{productResourceName}</DataGridTextCell>;
 };
