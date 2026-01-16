7f138e71dc5be6f694456f60f40a744c5d7c6edc
yjaaouane <yassine.jaaouane.ext@corp.ovh.com>
2026-01-16 18:11:18 +0100

    feat(metrics-module): manage config cta on dashboard
    ref: #MAOBS-194
    
    Signed-off-by: yjaaouane <yassine.jaaouane.ext@corp.ovh.com>

--- packages/manager/modules/metrics-to-customer/src/routes/routes.tsx
+++ packages/manager/modules/metrics-to-customer/src/routes/routes.tsx
@@ -1,55 +1,24 @@
-import { ComponentType, lazy } from 'react';
+import { lazy } from 'react';
 
-import { Route, RouteObject } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
-const importMetricsDashboardPage = () => import('../pages/metric/Dashboard.page');
-const importMetricsDashboardWidgetModal = () => import('../pages/metric/DashboardWidgetModal.page');
+import { subroutes, urls } from '@/routes/Routes.constants';
+
+const importMetricsDashboardPage = () => import('@/pages/metrics/dashboards/Dashboard.page');
+const importMetricsDashboardWidgetModal = () =>
+  import('@/pages/metrics/dashboards/DashboardWidgetModal.page');
 
 const metricsDashboardPage = lazy(importMetricsDashboardPage);
 const metricsDashboardWidgetModal = lazy(importMetricsDashboardWidgetModal);
 
-type ModuleWithDefault<T extends ComponentType<unknown>, E extends Record<string, unknown>> = {
-  default: T;
-} & E;
-
-function lazyRouteConfig<
-  T extends ComponentType<unknown>,
-  E extends Record<string, unknown> = Record<string, unknown>,
->(importFn: () => Promise<ModuleWithDefault<T, E>>) {
-  return {
-    lazy: async () => {
-      const { default: Component, ...rest } = await importFn();
-
-      return {
-        Component,
-        ...(rest as unknown as E),
-      };
-    },
-  };
-}
-
-/**
- * @deprecated This export is deprecated. Please use getMetricsRoute() instead.
- */
-export const MetricsToCustomerRoutes: RouteObject[] = [
-  {
-    path: '',
-    ...lazyRouteConfig(importMetricsDashboardPage),
-    children: [
-      {
-        path: ':widgetId',
-        ...lazyRouteConfig(importMetricsDashboardWidgetModal),
-      },
-    ],
-  },
-];
-
 export const getMetricsToCustomerRoutes = () => (
-  <>
-    <Route path="" id="dashboard" Component={metricsDashboardPage}>
-      <Route path=":widgetId" id="dashboard-widget" Component={metricsDashboardWidgetModal} />
-    </Route>
-  </>
+  <Route path={urls.root} id="dashboards" Component={metricsDashboardPage}>
+    <Route
+      path={subroutes.dashboardWidget}
+      id="dashboard-widget"
+      Component={metricsDashboardWidgetModal}
+    />
+  </Route>
 );
 
 export default getMetricsToCustomerRoutes;
