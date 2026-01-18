8e42e041ea6cebc7f3d13e360c5986ff8e06c7a1
helabenkhalfallah <hela.ben-khalfallah@corp.ovh.com>
2026-01-18 23:50:19 +0100

    feat(*): update ci to check only last commit
    ref: #MANAGER-20642
    
    Signed-off-by: helabenkhalfallah <hela.ben-khalfallah@corp.ovh.com>

--- packages/manager/apps/zimbra/src/App.tsx
+++ packages/manager/apps/zimbra/src/App.tsx
@@ -14,7 +14,6 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <div>Small mock change</div>
       <RouterProvider router={routes} />
       <ReactQueryDevtools />
     </QueryClientProvider>
