1ac9fa1bb5150a1642bb55fd6d866affa2199631
helabenkhalfallah <hela.ben-khalfallah@corp.ovh.com>
2026-01-18 22:38:18 +0100

    feat(*): mock change in a single app
    ref: #MANAGER-20642
    
    Signed-off-by: helabenkhalfallah <hela.ben-khalfallah@corp.ovh.com>

--- packages/manager/apps/zimbra/src/App.tsx
+++ packages/manager/apps/zimbra/src/App.tsx
@@ -14,6 +14,7 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
+      <div>Small mock change</div>
       <RouterProvider router={routes} />
       <ReactQueryDevtools />
     </QueryClientProvider>
