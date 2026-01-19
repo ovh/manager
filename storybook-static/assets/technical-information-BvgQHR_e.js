b7f282d96403e064fb46e71256cc4a06d0ef4e5e
ovh-cds <ovh-cds@users.noreply.github.com>
2026-01-19 20:50:25 +0000

    docs: update documentation [skip ci]

---
+++ storybook-static/assets/technical-information-BvgQHR_e.js
@@ -0,0 +1,34 @@
+import{j as e}from"./jsx-runtime-BRNY0I4F.js";import{u as p}from"./index-XxUZGkLd.js";import{M as c,C as u}from"./index-a-pfv-Xa.js";import{T as d,S as h}from"./TechnicalSpecification-OHfoC3TK.js";import{B as x,H as t}from"./Heading-D2dEonqZ.js";import{C as r,R as m,S as l,H as j}from"./Canvas-Bjwxvbkq.js";import{S as n}from"./StorybookLink-DvYzHi9o.js";import{P as s,O as b,D as f,b as v,R as g,C as y,L as S,c as C,d as L,I as T}from"./phone-number.stories-Cv240nsT.js";import"./index-Bnop-kX6.js";import"./iframe-DVaWS9ZP.js";import"./index-4pTrEEYx.js";import"./index-DOoWsluG.js";import"./index-DrFu-skq.js";import"./Icon-DrfG5m-t-DQEf1CkA.js";import"./index-2w0W-O47-BJ19ihbZ.js";import"./ods-react60-0db41gCx.js";import"./Link-BWQEuWpd-BuJJSBob.js";import"./ods-react94-Bxf0SjVg.js";import"./Text-CcNd6qQr-D2KuMUPS.js";import"./Badge-YOwwmnsf-CJ8iv41_.js";import"./ods-react236-aAAP9SXj.js";import"./Button-BC-ipw2F-CXZv4wj7.js";import"./Spinner-CV0M2r8z-14hgiywo.js";import"./PopoverTrigger-4w4N6iLp-C-LGD_7X.js";import"./ods-react235-BTQ8kVBe.js";import"./portal-BnVBD_Bd-CMtQ-rJ7.js";import"./use-locale-context-cwTY9VYn-REDUMe7F.js";import"./use-presence-CVI6Oc63-Cg7Clpcl.js";import"./use-event-C16_WYdL-DF4JeFb4.js";import"./index-C7dNzIpW-BlUnPWgK.js";import"./index-BAi52a_A-BVjZSJoF.js";import"./index-DB9CF8IY-DC0Y2KvY.js";import"./TooltipTrigger-Iu3rt7LP-CcHcHvx1.js";import"./index-DDXR4bR2-rx0xkcPf.js";import"./Textarea-D6qUk_wE-C2UZKwoJ.js";import"./FormFieldLabel-DerGjSSG-BvxIGX_B.js";import"./Card-D5fMAkqX-3SHynhw3.js";import"./Skeleton-tM01pV-R-CR0rfR_T.js";import"./Input-DcqcPYne-DrbRSC9d.js";import"./useI18n-C3-XAdTV-CxpmELcO.js";import"./Divider-THit99OS-DE11lmva.js";import"./Password-BRnIsw22-DCVt4F6o.js";import"./Table-DeFepqjL-ijVQdBcH.js";import"./CheckboxLabel-BNqUGOsg-n6RGKdXc.js";import"./use-field-context-C3OOq-03-DvM5qnyl.js";import"./Tag-B7nBeuPX-CPCfmWrN.js";import"./SelectControl-C7d9WPuE-Bxj24dHo.js";import"./index-_7D5ljJ2-LyAubAEn.js";import"./ClipboardTrigger-TeqCWvgk-PkhT1oq0.js";import"./ModalTrigger-tZKHx0Fx-Dgqcdxhl.js";import"./dialog-trigger-DhWI7POy-2Tl6aad4.js";import"./render-strategy-BVcZ76SI-DDawKQXU.js";import"./TabContent-fruP9qhA-B5VnEOA-.js";import"./QuantityInput-C9HPxWFJ-9gU-cLaL.js";import"./MessageIcon-yhpEHWAg-BtZxEFo4.js";import"./RadioLabel-t6l-QStJ-DsO2d0DS.js";import"./BreadcrumbItem-elPaHQlb-CNpnNsvr.js";import"./AccordionTrigger-CaSa0zEH-CeN99oZg.js";import"./collapsible-content-zo5LZi6B-BM-FWdPL.js";import"./ProgressBar-DyF6GCp5-C0AeSb5S.js";import"./FileUploadList-vr0-n6P2-CaW-_Mq4.js";import"./index-D_fe-3SC-C5ZKsUXO.js";import"./ToggleLabel-CsN97R7O-CgXQCfWb.js";import"./PhoneNumberCountryList-DlTFLnzQ-X8jwHTuc.js";import"./SwitchItem-bGtISEN6-D2kiHTKw.js";import"./DrawerTrigger-omPTo2Bn-DB5oJcOC.js";import"./Pagination-Uop4YMV_-CIoyR9_Y.js";import"./TimepickerTimezoneList-CRVzDFsF-DFLPqCJR.js";import"./Code-Bo0soTUb-Cbjvsa-n.js";import"./DatepickerControl-4VwQb-ep-Ct4shRTG.js";import"./Range-CfZeXvJr-C-szzAQ1.js";import"./Medium-D6k1YMTR-9CtW1mjz.js";import"./ComboboxControl-sJOkWHeT-CC0kWNMj.js";import"./TreeViewNodes-BTaZHq6H-Zfg7PUSJ.js";import"./Meter-Q4oNu6aG-BjjvBiEL.js";import"./index-lwTwL84Q.js";import"./ods-docgen-map-C6vdLMLl.js";import"./controls-BtiQQn1l.js";function a(i){const o={code:"code",img:"img",li:"li",ol:"ol",p:"p",strong:"strong",...p(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(c,{of:s,name:"Technical information"}),`
+`,e.jsx(x,{of:s}),`
+`,e.jsx(t,{label:"Overview",level:2}),`
+`,e.jsx(u,{of:b,sourceState:"none"}),`
+`,e.jsx(t,{label:"Anatomy",level:2}),`
+`,e.jsx(o.p,{children:e.jsx(o.img,{src:"./base-assets/components/phone-number/anatomy-tech.png",alt:"Component anatomy",title:"Component anatomy"})}),`
+`,e.jsxs(o.ol,{children:[`
+`,e.jsx(o.li,{children:e.jsx(o.strong,{children:"Phone Number"})}),`
+`,e.jsx(o.li,{children:e.jsx(o.strong,{children:"Phone Number Country List"})}),`
+`,e.jsx(o.li,{children:e.jsx(o.strong,{children:"Phone Number Control"})}),`
+`]}),`
+`,e.jsx(d,{data:h,extraInfo:{PhoneNumber:{extendAttribute:{name:"div",url:"https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/div#attributes"}},PhoneNumberControl:{extendAttribute:{name:"input",url:"https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/input#attributes"}},PhoneNumberCountryList:{extendAttribute:{name:"div",url:"https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/div#attributes"}}},of:s}),`
+`,e.jsx(t,{label:"Examples",level:2}),`
+`,e.jsx(t,{label:"Default",level:3}),`
+`,e.jsx(r,{of:f,sourceState:"shown"}),`
+`,e.jsx(t,{label:"Disabled",level:3}),`
+`,e.jsx(r,{of:v,sourceState:"shown"}),`
+`,e.jsx(t,{label:"Readonly",level:3}),`
+`,e.jsx(r,{of:g,sourceState:"shown"}),`
+`,e.jsx(t,{label:"Clearable",level:3}),`
+`,e.jsx(r,{of:y,sourceState:"shown"}),`
+`,e.jsx(t,{label:"Loading",level:3}),`
+`,e.jsx(r,{of:S,sourceState:"shown"}),`
+`,e.jsx(t,{label:"Locale",level:3}),`
+`,e.jsx(o.p,{children:"The locale manages the translation of the country list."}),`
+`,e.jsxs(o.p,{children:["For further explanation about locale strategy, see ",e.jsx(n,{kind:m.phoneNumber,story:l.documentation,children:"the locale behavior documentation"}),"."]}),`
+`,e.jsx(r,{of:C,sourceState:"shown"}),`
+`,e.jsx(t,{label:"Country list",level:3}),`
+`,e.jsx(o.p,{children:"You can use update the list of country to select from."}),`
+`,e.jsxs(o.p,{children:["By default, the list use the ",e.jsx(o.code,{children:"all"})," preset that displays all supported iso codes. But you can pass a specific subset to limit the possible choices."]}),`
+`,e.jsxs(o.p,{children:["For further explanation about country iso code strategy, see ",e.jsx(n,{kind:m.phoneNumber,story:l.documentation,children:"the iso code behavior documentation"}),"."]}),`
+`,e.jsx(r,{of:L,sourceState:"shown"}),`
+`,e.jsx(t,{label:"Form field",level:3,children:e.jsx(n,{title:j.handlingFormGuide,children:"(see handling form guide)"})}),`
+`,e.jsx(r,{of:T,sourceState:"shown"})]})}function Je(i={}){const{wrapper:o}={...p(),...i.components};return o?e.jsx(o,{...i,children:e.jsx(a,{...i})}):a(i)}export{Je as default};
