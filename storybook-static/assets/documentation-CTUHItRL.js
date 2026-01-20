c2d0db5318b49a7d3acb7ba5ce5231e9a52c6f77
ovh-cds <ovh-cds@users.noreply.github.com>
2026-01-20 11:40:21 +0000

    docs: update documentation [skip ci]

---
+++ storybook-static/assets/documentation-CTUHItRL.js
@@ -0,0 +1,85 @@
+import{j as e}from"./jsx-runtime-BRNY0I4F.js";import{u as d}from"./index-BnJ4eeZE.js";import{t as i}from"./Icon-DrfG5m-t-DQEf1CkA.js";import{l as r}from"./ods-react60-0db41gCx.js";import{M as h,C as m}from"./index-Ck5q0c5c.js";import{B as l,O as u,A as p,a as x,b as g,c as j}from"./badge.stories-Chk6f8FV.js";import{B as b,H as s,E as o}from"./Heading-CUt9y0bY.js";import{I as f,B as y}from"./IdentityCard-DKtxsaXB.js";import{C as a}from"./Canvas-BnlgT6BS.js";import"./index-Bnop-kX6.js";import"./index-2w0W-O47-BJ19ihbZ.js";import"./iframe-bAznjRtu.js";import"./index-4pTrEEYx.js";import"./index-DOoWsluG.js";import"./index-DrFu-skq.js";import"./Badge-YOwwmnsf-CJ8iv41_.js";import"./ods-react236-aAAP9SXj.js";import"./TooltipTrigger-Iu3rt7LP-CcHcHvx1.js";import"./ods-react235-BTQ8kVBe.js";import"./portal-BnVBD_Bd-CMtQ-rJ7.js";import"./use-locale-context-cwTY9VYn-REDUMe7F.js";import"./use-presence-CVI6Oc63-Cg7Clpcl.js";import"./use-event-C16_WYdL-DF4JeFb4.js";import"./index-DDXR4bR2-rx0xkcPf.js";import"./index-C7dNzIpW-BlUnPWgK.js";import"./controls-BtiQQn1l.js";import"./ods-docgen-map-C6vdLMLl.js";import"./Link-BWQEuWpd-BuJJSBob.js";import"./ods-react94-Bxf0SjVg.js";import"./Divider-THit99OS-DE11lmva.js";import"./index-DX7gpv7E.js";function c(t){const n={code:"code",em:"em",img:"img",li:"li",ol:"ol",p:"p",strong:"strong",ul:"ul",...d(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(h,{of:l,name:"Documentation"}),`
+`,e.jsx(b,{of:l}),`
+`,e.jsx(n.p,{children:e.jsxs(n.em,{children:[e.jsx(n.strong,{children:"Badge"})," show concise metadata or the current status for an item, in a compact format."]})}),`
+`,e.jsx(m,{of:u,sourceState:"none"}),`
+`,e.jsx(s,{label:"Overview",level:2}),`
+`,e.jsx(f,{aliases:["Chip (previous name)","Label"],figmaLink:"https://www.figma.com/design/9jDDTcR4a9jPRFcdjawAlf/ODS---UI-Kit?node-id=3-22285",githubUrl:"@ovhcloud/ods-react",name:"Badge",children:e.jsxs(n.p,{children:["A ",e.jsx(n.strong,{children:"Badge"}),` component labels an item, a keyword or a status. They are non-interactive and used inline, next to the
+title or text they complete.`]})}),`
+`,e.jsx(s,{label:"Usage",level:2}),`
+`,e.jsx(n.p,{children:"Badges are used for items that need to be labelled or categorized."}),`
+`,e.jsx(n.p,{children:"It can refer to these examples of enhanced data :"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"A notification status"}),`
+`,e.jsx(n.li,{children:"A marketing category"}),`
+`,e.jsx(n.li,{children:"Additional product information"}),`
+`]}),`
+`,e.jsx(s,{label:"Notification Status",level:3}),`
+`,e.jsx(n.p,{children:"The notification status can be used next to a menu entry, a label or an option ; in dashboards, Table or summary panels."}),`
+`,e.jsx(n.p,{children:"There are four different sub-types of status, in order of severity:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Error"})," : Reserved for errors, malfunctions, as well as critical issues. System is unusable, or an action must be taken immediately."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Warning"})," : Reserved for critical messages that need the user attention and acknowledgment, but might not cause errors."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Information"})," : Provides information to users in context. Shouldn't replace regular content."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Success"})," : Reserved to provide to a static persistent success message."]}),`
+`]}),`
+`,e.jsx(s,{label:"Marketing category",level:3}),`
+`,e.jsx(n.p,{children:"The marketing category is used in product Tiles or product descriptions; used as - temporary or permanent - marketing highlight."}),`
+`,e.jsxs(n.p,{children:["It can contain categories like ",e.jsx(n.em,{children:'"Promotion", "Summer Deals", "Black Friday", "Limited Edition", "Flash Sales"'}),", ..."]}),`
+`,e.jsx(s,{label:"Additional product information",level:3}),`
+`,e.jsx(n.p,{children:"The product status is used in product Tiles or product descriptions, it is bound inside the product structure to add tags and information."}),`
+`,e.jsx(n.p,{children:'All product statuses are of type "New", "Limited edition", "Sold out"'}),`
+`,e.jsx(s,{label:"Dos & Don'ts",level:3}),`
+`,e.jsx(y,{donts:["- Don't use Badge as interactive element (e.g., buttons or filters)","- Don't write long sentences or complex phrases inside a Badge","- Don't use too many Badges in the same line or component, as this creates visual clutter","- Don't place Badge far from the content they describe, proximity reinforces meaning"],dos:["- Keep the label short, specific, and readable (ideally one or two words)","- Use Badge to indicate status, category, or metadata","- Place Badge inline with related content, such as next to titles or list items","- Use Badge to complement information, not replace it. They should add clarity, not create ambiguity"]}),`
+`,e.jsx(s,{label:"Best Practices in Context",level:3}),`
+`,e.jsx(n.p,{children:e.jsx(n.img,{src:"./base-assets/components/badge/anatomy.png",alt:"Component anatomy",title:"Component anatomy"})}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.strong,{children:"Badge"})}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Icon"})," - optional (left or right)"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Label"})," - optional (specific use case only)"]}),`
+`]}),`
+`,e.jsx(s,{label:"Placement",level:2}),`
+`,e.jsx(n.p,{children:"Since it provides extra information to a sibling element, in can be used inside components in various places, referring to the nature of its environment."}),`
+`,e.jsxs(n.p,{children:["Multiple ",e.jsx(n.strong,{children:"Badges"})," can be displayed:"]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"on a single line"}),`
+`,e.jsx(n.li,{children:"stacked vertically"}),`
+`]}),`
+`,e.jsx(s,{label:"Behavior",level:2}),`
+`,e.jsxs(n.p,{children:["As the ",e.jsx(n.strong,{children:"Badge"})," is an informational component, its default behavior is being read-only."]}),`
+`,e.jsxs(n.p,{children:["An icon can be displayed on the left or right of the ",e.jsx(n.strong,{children:"Badge"})," label content. Icon-only ",e.jsx(n.strong,{children:"Badge"})," also exists, but it must meet accessibility requirements. See Accessibility section below."]}),`
+`,e.jsx(s,{label:"Variation",level:2}),`
+`,e.jsx(s,{label:"Color",level:3}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Information"})," ",e.jsx(n.em,{children:"(default)"}),": display neutral or informational messages, such as updates, notifications, or general status."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Success"}),": indicate positive outcomes or successful actions, such as completed tasks or achievements."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Warning"}),": alert users to potential issues or cautionary information, signaling that attention is needed."]}),`
+`]}),`
+`,e.jsx(s,{label:"Size",level:3}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Small"}),": compact and unobtrusive counts or statuses, suitable for tight spaces and minimalistic designs."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Medium"})," ",e.jsx(n.em,{children:"(default)"}),": main size for displaying ",e.jsx(n.strong,{children:"Badges"}),"."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Large"}),": prominent and easily noticeable counts or statuses, suitable for emphasizing important information."]}),`
+`]}),`
+`,e.jsx(s,{label:"Navigation",level:2}),`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.strong,{children:"Badge"})," component is non-interactive and does not receive keyboard focus. It is purely decorative and used to label an item, keyword, or status."]}),`
+`,e.jsx(s,{label:"Accessibility",level:2}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"Badges"})," are read by screen readers as regular text."]}),`
+`,e.jsxs(n.p,{children:["Icon-only ",e.jsx(n.strong,{children:"Badge"})," is only used in very specific cases. In that case, it should be accompanied by a tooltip to provide further context."]}),`
+`,e.jsxs(n.p,{children:["The accessibility of multiple ",e.jsx(n.strong,{children:"Badges"}),` presented as a collection must be considered during its implementation.
+Their grouping should be made clear to assistive technologies and proper ARIA attributes must be added to ensure full accessibility.`]}),`
+`,e.jsx(s,{label:"Icon-only Badge",level:3}),`
+`,e.jsxs(n.p,{children:["If a ",e.jsx(n.strong,{children:"Badge"})," contains only an icon, it must include an ",e.jsx(o,{href:"https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Reference/Attributes/aria-label",children:"aria-label"})," to provide context."]}),`
+`,e.jsx(a,{of:p,sourceState:"shown"}),`
+`,e.jsxs(n.p,{children:[e.jsx(i,{name:r.circleCheck,style:{color:"var(--ods-color-success-500)"}})," Screen readers will announce the aria-label instead of ignoring the ",e.jsx(n.strong,{children:"Badge"}),"."]}),`
+`,e.jsxs(n.p,{children:["Since an icon-only ",e.jsx(n.strong,{children:"Badge"})," may lack detailed information, attach a tooltip and use an ",e.jsx(o,{href:"https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Reference/Attributes/aria-labelledby",children:"aria-labelledby"})," when extra context is needed:"]}),`
+`,e.jsx(a,{of:x,sourceState:"shown"}),`
+`,e.jsxs(n.p,{children:[e.jsx(i,{name:r.circleCheck,style:{color:"var(--ods-color-success-500)"}})," Screen readers will announce the ",e.jsx(n.strong,{children:"Tooltip"})," content when focusing on the ",e.jsx(n.strong,{children:"Badge"}),"."]}),`
+`,e.jsx(s,{label:"Structuring groups of Badges with lists",level:3}),`
+`,e.jsxs(n.p,{children:["When displaying multiple ",e.jsx(n.strong,{children:"Badges"})," together, they should be wrapped within an unordered list of items (",e.jsx(n.code,{children:"<ul>"})," and ",e.jsx(n.code,{children:"<li>"}),") to ensure a proper announcement by screen readers."]}),`
+`,e.jsx(a,{of:g,sourceState:"shown"}),`
+`,e.jsxs(n.p,{children:[e.jsx(i,{name:r.circleCheck,style:{color:"var(--ods-color-success-500)"}})," This structure ensures that assistive technologies announce ",e.jsx(n.strong,{children:"Badges"}),` as a list, rather than reading them as separate, unrelated announcements.
+Screen readers will announce the list, the number of items and the `,e.jsx(n.strong,{children:"Badges"})," content."]}),`
+`,e.jsx(s,{label:"Alternative approach with ARIA roles",level:3}),`
+`,e.jsxs(n.p,{children:["When modifying the HTML structure is not possible, use ",e.jsx(o,{href:"https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Reference/Roles/list_role",children:'role="list"'})," and ",e.jsx(o,{href:"https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Reference/Roles/listitem_role",children:'role="listitem"'})," to mimic list semantics."]}),`
+`,e.jsx(a,{of:j,sourceState:"shown"}),`
+`,e.jsxs(n.p,{children:[e.jsx(i,{name:r.circleCheck,style:{color:"var(--ods-color-success-500)"}})," This ensures that screen readers recognize the ",e.jsx(n.strong,{children:"Badges"})," as a structured list even without native ",e.jsx(n.code,{children:"<ul>"})," and ",e.jsx(n.code,{children:"<li>"})," elements."]})]})}function Y(t={}){const{wrapper:n}={...d(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(c,{...t})}):c(t)}export{Y as default};
---
+++ storybook-static/assets/documentation-DswAd_f2.js
@@ -0,0 +1,50 @@
+import{j as e}from"./jsx-runtime-BRNY0I4F.js";import{u as r}from"./index-BnJ4eeZE.js";import{M as a,C as l}from"./index-Ck5q0c5c.js";import{B as d,H as n,E as c}from"./Heading-CUt9y0bY.js";import{R as t,O as h}from"./radio-group.stories-N0HJXMrA.js";import{I as p,B as u}from"./IdentityCard-DKtxsaXB.js";import"./index-Bnop-kX6.js";import"./iframe-bAznjRtu.js";import"./index-4pTrEEYx.js";import"./index-DOoWsluG.js";import"./index-DrFu-skq.js";import"./Icon-DrfG5m-t-DQEf1CkA.js";import"./index-2w0W-O47-BJ19ihbZ.js";import"./ods-react60-0db41gCx.js";import"./Link-BWQEuWpd-BuJJSBob.js";import"./ods-react94-Bxf0SjVg.js";import"./Divider-THit99OS-DE11lmva.js";import"./ods-react236-aAAP9SXj.js";import"./FormFieldLabel-DerGjSSG-BvxIGX_B.js";import"./Text-CcNd6qQr-D2KuMUPS.js";import"./RadioLabel-t6l-QStJ-DsO2d0DS.js";import"./use-locale-context-cwTY9VYn-REDUMe7F.js";import"./index-DDXR4bR2-rx0xkcPf.js";import"./controls-BtiQQn1l.js";import"./ods-docgen-map-C6vdLMLl.js";function i(s){const o={code:"code",em:"em",img:"img",li:"li",ol:"ol",p:"p",strong:"strong",ul:"ul",...r(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:t,name:"Documentation"}),`
+`,e.jsx(d,{of:t}),`
+`,e.jsx(o.p,{children:e.jsxs(o.em,{children:["A ",e.jsx(o.strong,{children:"Radio"})," button allows users to select only one option from a number of choices."]})}),`
+`,e.jsx(l,{of:h,sourceState:"none"}),`
+`,e.jsx(n,{label:"Overview",level:2}),`
+`,e.jsx(p,{aliases:["Radio button (previous name)"],figmaLink:"https://www.figma.com/design/9jDDTcR4a9jPRFcdjawAlf/ODS---UI-Kit?node-id=49-14625",githubUrl:"@ovhcloud/ods-react",name:"Radio",children:e.jsxs(o.p,{children:[e.jsx(o.strong,{children:"Radio Group"})," is a group of Radios to select a single state from multiples options."]})}),`
+`,e.jsx(n,{label:"Usage",level:2}),`
+`,e.jsx(o.p,{children:"In most situations where you want to present a list of mutually exclusive options."}),`
+`,e.jsxs(o.p,{children:[e.jsx(o.strong,{children:"Radio groups"})," can be used within a form."]}),`
+`,e.jsx(o.p,{children:"Also, it can serve as :"}),`
+`,e.jsxs(o.ul,{children:[`
+`,e.jsx(o.li,{children:"Selecting choice"}),`
+`,e.jsx(o.li,{children:"Lists/sub-lists"}),`
+`,e.jsx(o.li,{children:"Filters"}),`
+`]}),`
+`,e.jsx(n,{label:"Dos & Don'ts",level:3}),`
+`,e.jsx(u,{donts:["- Don't use Radio Group when multiple selections are allowed. Use Checkbox instead","- Don't use a Radio Group with more than 5 options. Switch to a Select component for better usability","- Avoid situations where all options in a set should be deselected","- Don't truncate long labels, allow wrapping to maintain readability"],dos:["- Use Radio Group when users must choose only one option from a set of two or more","- Ensure each radio has a clear and concise label, wrapped text is preferable to truncated labels","- Keep the number of radios concise (ideally 2 to 5) to avoid overwhelming the user",'- Consider using a Toggle instead of a two-option Radio Group when the options are affirmative/negative (e.g., "Enable notifications" yes/no)',"- Group related radios together with a visible label or legend to provide context"]}),`
+`,e.jsx(n,{label:"Best Practices in Context",level:3}),`
+`,e.jsx(o.p,{children:e.jsx(o.img,{src:"./base-assets/components/radio-group/anatomy.png",alt:"Component anatomy",title:"Component anatomy"})}),`
+`,e.jsxs(o.ol,{children:[`
+`,e.jsx(o.li,{children:e.jsx(o.strong,{children:"Radio Group"})}),`
+`,e.jsx(o.li,{children:e.jsx(o.strong,{children:"Radio button"})}),`
+`,e.jsx(o.li,{children:e.jsx(o.strong,{children:"Label"})}),`
+`]}),`
+`,e.jsx(n,{label:"Placement",level:2}),`
+`,e.jsxs(o.p,{children:[e.jsx(o.strong,{children:"Radio Groups"})," should be vertical, meaning one radio button under another."]}),`
+`,e.jsxs(o.p,{children:["Usage of horizontal ",e.jsx(o.strong,{children:"Radio Groups"})," should only occur if vertical space is limited."]}),`
+`,e.jsx(n,{label:"Behavior",level:2}),`
+`,e.jsx(o.p,{children:"Radio button can be hovered, focused, clicked or disabled. When disabled, radio button cannot be focused nor clicked."}),`
+`,e.jsx(o.p,{children:"When clicking on one of the radio button or its linked label, the radio button is selected or deselected depending on the previous state."}),`
+`,e.jsxs(o.p,{children:["Radio button behavior does work only when used in a situation of a ",e.jsx(o.strong,{children:"Radio group"}),`.
+The `,e.jsx(o.strong,{children:"Radio group"})," can be in an error state, but also in a disabled state. It also can be focused and hovered."]}),`
+`,e.jsx(n,{label:"Variation",level:2}),`
+`,e.jsx(o.p,{children:"Radio buttons follow the native behavior of the browser used, so the appearance of radio buttons may vary depending on the browser."}),`
+`,e.jsx(n,{label:"Navigation",level:2}),`
+`,e.jsx(n,{label:"Focus Management",level:3}),`
+`,e.jsxs(o.p,{children:["When the ",e.jsx(o.strong,{children:"Radio Group"})," component is focused, focus is automatically set to the first focusable and not disabled radio in the group."]}),`
+`,e.jsx(o.p,{children:"If a radio button is checked, focus moves to that checked item when the component is focused."}),`
+`,e.jsx(o.p,{children:"A read-only radio button can receive focus but cannot be selected or modified."}),`
+`,e.jsx(o.p,{children:"If a radio button is disabled, it cannot receive focus and cannot be selected."}),`
+`,e.jsx(o.p,{children:"Focus moves through the radio buttons sequentially."}),`
+`,e.jsx(n,{label:"General Keyboard Shortcuts",level:3}),`
+`,e.jsxs(o.p,{children:["Pressing ",e.jsx(o.code,{children:"Tab"})," moves focus to the next focusable and enabled radio button in the group."]}),`
+`,e.jsxs(o.p,{children:["Pressing ",e.jsx(o.code,{children:"Shift"})," + ",e.jsx(o.code,{children:"Tab"})," moves focus backward through the radio buttons."]}),`
+`,e.jsxs(o.p,{children:["Pressing ",e.jsx(o.code,{children:"Arrow Down"})," or ",e.jsx(o.code,{children:"Arrow Right"})," moves focus and selects the next radio button in the group."]}),`
+`,e.jsxs(o.p,{children:["Pressing ",e.jsx(o.code,{children:"Arrow Up"})," or ",e.jsx(o.code,{children:"Arrow Left"})," moves focus and selects the previous radio button in the group."]}),`
+`,e.jsxs(o.p,{children:["Pressing ",e.jsx(o.code,{children:"Space"})," when focused on an unchecked radio button selects it."]}),`
+`,e.jsx(n,{label:"Accessibility",level:2}),`
+`,e.jsxs(o.p,{children:["This component complies with the ",e.jsx(c,{href:"https://www.w3.org/WAI/ARIA/apg/patterns/radio/",children:"Radio Group WAI-ARIA design pattern"})]}),`
+`,e.jsxs(o.p,{children:["Try to keep a vertical spacing of at least ",e.jsx(o.code,{children:"8px"})," between each radio buttons, to provide sufficient tactile and visual separation."]})]})}function W(s={}){const{wrapper:o}={...r(),...s.components};return o?e.jsx(o,{...s,children:e.jsx(i,{...s})}):i(s)}export{W as default};
