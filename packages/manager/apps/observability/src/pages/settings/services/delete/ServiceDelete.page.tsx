6a8394f84bd7dcddf1f7c28ac9320abe72c7a99d
David Arsène <david.arsene.ext@ovhcloud.com>
2026-01-20 16:45:06 +0100

    feat(observability): add service termination
    resolves: #MAOBS-192
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

---
+++ packages/manager/apps/observability/src/pages/settings/services/delete/ServiceDelete.page.tsx
@@ -0,0 +1,40 @@
+import { useNavigate } from 'react-router-dom';
+
+import { useTranslation } from 'react-i18next';
+
+import { useDeleteService } from '@ovh-ux/manager-module-common-api';
+import { DeleteModal, useNotifications } from '@ovh-ux/muk';
+
+import ErrorMessage from '@/components/error/ErrorMessage.component';
+import { useObservabilityServiceContext } from '@/contexts/ObservabilityService.context';
+
+export default function ServiceDeletePage() {
+  const navigate = useNavigate();
+  const { t } = useTranslation(['services', 'shared']);
+  const { addSuccess, addError } = useNotifications();
+  const { selectedService } = useObservabilityServiceContext();
+  const { terminateService, isPending } = useDeleteService({
+    onSuccess: () => {
+      addSuccess(t('delete.success', { serviceName: resourceName }));
+      handleClose();
+    },
+    onError: (error) => {
+      addError(<ErrorMessage error={error} />);
+      handleClose();
+    },
+  });
+
+  const resourceName = selectedService?.id ?? '';
+  const handleClose = () => navigate(-1);
+  const handleConfirmDelete = () => terminateService({ resourceName });
+
+  return (
+    <DeleteModal
+      open
+      serviceTypeName={selectedService?.currentState.displayName ?? selectedService?.id}
+      onConfirmDelete={handleConfirmDelete}
+      onClose={handleClose}
+      isLoading={isPending || !resourceName}
+    />
+  );
+}
