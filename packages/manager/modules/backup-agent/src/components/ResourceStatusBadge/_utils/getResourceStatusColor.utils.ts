4cf8a6e0bd39cfbb6bb71351c692a32a837af41a
b-fritz-ovh <benjamin.fritz.ext@corp.ovh.com>
2026-01-20 16:43:21 +0100

    feat(backup-agent): update status types
    ref: #BKP-737
    
    Signed-off-by: b-fritz-ovh <benjamin.fritz.ext@corp.ovh.com>

--- packages/manager/modules/backup-agent/src/components/ResourceStatusBadge/_utils/getResourceStatusColor.utils.ts
+++ packages/manager/modules/backup-agent/src/components/ResourceStatusBadge/_utils/getResourceStatusColor.utils.ts
@@ -1,7 +1,7 @@
 import { ODS_BADGE_COLOR } from '@ovhcloud/ods-components';
 
 import { vaultStatusColor } from '@/components/ResourceStatusBadge/_utils/resourceStatusColor.utils';
-import { AlternativeStatus, ResourceStatus } from '@/types/Resource.type';
+import { AgentStatus, ResourceStatus } from '@/types/Resource.type';
 
-export const getColorResourceStatus = (status: ResourceStatus | AlternativeStatus) =>
+export const getColorResourceStatus = (status: ResourceStatus | AgentStatus) =>
   vaultStatusColor[status.toUpperCase() as ResourceStatus] ?? ODS_BADGE_COLOR.information;
