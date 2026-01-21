7a8e06b7830c908417de6169feb34c54d017b457
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2026-01-21 18:23:16 +0100

    feat(bmc-backup-agent-baremetal): add shadow order on onboarding page
    ref: #BKP-705
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>
    Signed-off-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/apps/bmc-backup-agent-baremetal/src/pages/first-order/FirstOrder.page.tsx
+++ packages/manager/apps/bmc-backup-agent-baremetal/src/pages/first-order/FirstOrder.page.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 
-import { Outlet } from 'react-router-dom';
-
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+import { z } from 'zod';
 
+import { OdsButton, OdsCombobox, OdsFormField } from '@ovhcloud/ods-components/react';
+
+import { BaremetalOption } from '@ovh-ux/backup-agent/components/CommonFields/BaremetalOption/BaremetalOption.component';
+import { useBaremetalsList } from '@ovh-ux/backup-agent/data/hooks/baremetal/useBaremetalsList';
 import { NAMESPACES } from '@ovh-ux/manager-common-translations';
 
 import { OnboardingDescription } from '@/components/onboarding/onboardingDescription/OnboardingDescription.component';
 import { OnboardingLayout } from '@/components/onboarding/onboardingLayout/OnboardingLayout.component';
 import { useOnboardingContent } from '@/hooks/onboarding/useOnboardingData';
 import { useOnboardingHeroImage } from '@/hooks/onboarding/useOnboardingHeroImage';
+import { urlParams, urls } from '@/routes/Routes.constants';
 
-import { FirstOrderFormComponent } from './_components/first-order-form/FirstOrderForm.component';
+const FIRST_ORDER_SCHEMA = z.object({
+  baremetal: z.string().min(1),
+});
+type FirstOrderSchema = z.infer<typeof FIRST_ORDER_SCHEMA>;
 
 export default function FirstOrderPage() {
   const { t } = useTranslation([
@@ -20,10 +29,28 @@ export default function FirstOrderPage() {
     NAMESPACES.ONBOARDING,
     NAMESPACES.FORM,
   ]);
-  const { productName, title } = useOnboardingContent();
 
+  const { productName, title } = useOnboardingContent();
   const img = useOnboardingHeroImage();
-  // Build localized description paragraph.
+  const { flattenData, isLoading } = useBaremetalsList();
+  const navigate = useNavigate();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FirstOrderSchema>({
+    resolver: zodResolver(FIRST_ORDER_SCHEMA),
+    defaultValues: { baremetal: '' },
+  });
+
+  const onSubmit: SubmitHandler<FirstOrderSchema> = (dataForm) => {
+    if (!dataForm.baremetal) {
+      return;
+    }
+    navigate(urls.firstOrderConfirmation.replace(urlParams.baremetalName, dataForm.baremetal));
+  };
+
   return (
     <>
       <section className="flex flex-col items-center justify-center gap-10">
@@ -31,8 +58,45 @@ export default function FirstOrderPage() {
           title={title ?? t('onboarding:title_fallback', { productName })}
           img={img}
           description={<OnboardingDescription />}
-        ></OnboardingLayout>
-        <FirstOrderFormComponent />
+        />
+        <form
+          className={`flex justify-center ${
+            errors.baremetal ? 'items-center' : 'items-end'
+          } w-full gap-4`}
+          noValidate
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <Controller
+            control={control}
+            name="baremetal"
+            render={({ field, fieldState: { invalid } }) => (
+              <OdsFormField
+                className="w-full max-w-xl"
+                error={invalid ? t(`${NAMESPACES.FORM}:required_field`) : undefined}
+              >
+                <label htmlFor={field.name}>{t('select_baremetal')}</label>
+                <OdsCombobox
+                  id={field.name}
+                  {...field}
+                  placeholder={t('select_baremetal')}
+                  isDisabled={isLoading}
+                  hasError={invalid}
+                  onOdsBlur={field.onBlur}
+                  onOdsChange={field.onChange}
+                >
+                  {flattenData?.map(({ name, ip, iam: { displayName } }) => (
+                    <BaremetalOption key={name} name={name} ip={ip} displayName={displayName} />
+                  ))}
+                </OdsCombobox>
+              </OdsFormField>
+            )}
+          />
+          <OdsButton
+            type="submit"
+            label={t(`${NAMESPACES.ACTIONS}:start`)}
+            isDisabled={isLoading}
+          />
+        </form>
       </section>
       <Outlet />
     </>
--- packages/manager/apps/bmc-backup-agent-baremetal/src/routes/Routes.constants.ts
+++ packages/manager/apps/bmc-backup-agent-baremetal/src/routes/Routes.constants.ts
@@ -2,6 +2,10 @@ import { appName } from '@/App.constants';
 
 const ROOT_URL = `/`;
 
+export const urlParams = Object.freeze({
+  baremetalName: ':baremetalName',
+});
+
 export const subRoutes = Object.freeze({
   onboarding: 'onboarding',
   firstOrder: 'first-order',
@@ -12,7 +16,7 @@ export const urls = Object.freeze({
   root: ROOT_URL,
   onboarding: `${ROOT_URL}${subRoutes.onboarding}`,
   firstOrder: `${ROOT_URL}${subRoutes.firstOrder}`,
-  firstOrderConfirmation: `${ROOT_URL}${subRoutes.firstOrder}/${subRoutes.firstOrderConfirmation}`,
+  firstOrderConfirmation: `${ROOT_URL}${subRoutes.firstOrder}/${subRoutes.firstOrderConfirmation}/${urlParams.baremetalName}`,
 });
 
 export const redirectionApp = appName;
