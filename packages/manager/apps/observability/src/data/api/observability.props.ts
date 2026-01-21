4aaf2ad72113909cbadd486fd7c0189ad428f31f
David Arsène <david.arsene.ext@ovhcloud.com>
2026-01-21 12:02:44 +0100

    feat(observability): add service name modification
    resolves: #MAOBS-192
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/observability/src/data/api/observability.props.ts
+++ packages/manager/apps/observability/src/data/api/observability.props.ts
@@ -2,3 +2,11 @@ export type ObservabilityServiceParams = {
   resourceName: string;
   signal?: AbortSignal;
 };
+
+export type EditObservabilityServicePayload = {
+  resourceName: string;
+  targetSpec: {
+    displayName: string;
+  };
+  signal?: AbortSignal;
+};
