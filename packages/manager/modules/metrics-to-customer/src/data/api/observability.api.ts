d62802c34f1cb57a6b65f0b41ffcebe9213cdf2b
yjaaouane <yassine.jaaouane.ext@corp.ovh.com>
2026-01-21 17:04:35 +0100

    feat(metrics-module): add tenants subscriptions manager
    ref: #MAOBS-193
    
    Signed-off-by: yjaaouane <yassine.jaaouane.ext@corp.ovh.com>

---
+++ packages/manager/modules/metrics-to-customer/src/data/api/observability.api.ts
@@ -0,0 +1,12 @@
+import { apiClient } from '@ovh-ux/manager-core-api';
+
+import { ObservabilityService } from '@/types/observability.type';
+
+export const getObservabilityServices = async (
+  signal: AbortSignal,
+): Promise<ObservabilityService[]> => {
+  const { data } = await apiClient.v2.get<ObservabilityService[]>('/observability/resource', {
+    signal,
+  });
+  return data;
+};
---
+++ packages/manager/modules/metrics-to-customer/src/types/tenants.type.ts
@@ -0,0 +1,81 @@
+import { InfrastructureSettings } from '@/types/infrastructures.type';
+import { TIdentifier, TObservabilityResource } from '@/types/observability.type';
+import { Resource } from '@/types/resource.type';
+
+type TMetricLimits = {
+  mimir: {
+    compactor_blocks_retention_period: string;
+    max_global_series_per_user: number;
+  };
+};
+
+export type TenantResourceStatus =
+  | 'READY'
+  | 'UPDATING'
+  | 'ERROR'
+  | 'DELETING'
+  | 'SUSPENDED'
+  | 'CREATING'
+  | 'UNKNOWN';
+
+export type TenantInfrastructure = InfrastructureSettings & TIdentifier;
+
+export type TenantState = {
+  title: string;
+  description: string;
+  limits?: TMetricLimits;
+  endpoint?: string;
+  infrastructure?: TenantInfrastructure;
+};
+
+export type Tenant = {
+  currentState: TenantState;
+  targetSpec?: TenantState;
+  resourceStatus: TenantResourceStatus;
+} & TObservabilityResource;
+
+export type TenantFormData = {
+  title: string;
+  description: string;
+  infrastructureId: string;
+  retentionDuration: string;
+  retentionUnit: string;
+  maxSeries: number | null;
+};
+
+export type TenantListing = {
+  name: string;
+  resourceStatus: TenantResourceStatus;
+  endpoint: string | undefined;
+  entryPoint: string | undefined;
+  infrastructure: TenantInfrastructure | undefined;
+  retention: string | undefined;
+  numberOfSeries: number | undefined;
+  tags: { [key: string]: string };
+  search: string;
+} & TIdentifier;
+
+export type TenantSubscription = {
+  resourceStatus: TenantResourceStatus;
+  currentState: {
+    kind: string;
+    link: string;
+    resource: Resource;
+  };
+  targetSpec?: {
+    kind: string;
+    resource: Resource;
+  };
+} & TObservabilityResource;
+
+export type TenantWithSubscriptions = Tenant & {
+  subscriptions: TenantSubscription[];
+};
+
+export type TenantSubscriptionListing = {
+  resourceStatus: TenantResourceStatus;
+  resource: Resource;
+  tags: { [key: string]: string };
+  search: string;
+  urn: string;
+} & TIdentifier;
