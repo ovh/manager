35bd42d01e10be595d3d41fbbbc54111c3c3cde5
aTurmo <adrien.turmo.ext@ovhcloud.com>
2026-01-21 11:09:06 +0100

    feat(pci-file-storage): micro region and availibility zone selection
    ref: #TAPC-5640
    
    Signed-off-by: aTurmo <adrien.turmo.ext@ovhcloud.com>
    Co-authored-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/apps/pci-file-storage/src/pages/create/components/form/CreateShareForm.component.tsx
+++ packages/manager/apps/pci-file-storage/src/pages/create/components/form/CreateShareForm.component.tsx
@@ -1,17 +1,40 @@
+import { useWatch } from 'react-hook-form';
 import { FormProvider } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
 import { Divider, Text } from '@ovhcloud/ods-react';
 
+import { useShareCatalog } from '@/data/hooks/catalog/useShareCatalog';
+import { AvailabilityZoneSelection } from '@/pages/create/components/localisation/availabilityZone/AvailabilityZoneSelection.component';
 import { DeploymentModeSection } from '@/pages/create/components/localisation/deploymentMode/DeploymentModeSection.component';
 import { MacroRegionSelection } from '@/pages/create/components/localisation/macroRegion/MacroRegionSelection.component';
+import { MicroRegionSelection } from '@/pages/create/components/localisation/microRegion/MicroRegionSelection.component';
 import { NameInput } from '@/pages/create/components/name/NameInput.component';
 import { useCreateShareForm } from '@/pages/create/hooks/useCreateShareForm';
+import {
+  selectAvailabilityZones,
+  selectMicroRegions,
+} from '@/pages/create/view-model/shareCatalog.view-model';
 
 export const CreateShareForm = () => {
   const { t } = useTranslation(['create']);
 
   const formMethods = useCreateShareForm();
+  const [selectedMacroRegion, selectedMicroRegion] = useWatch({
+    control: formMethods.control,
+    name: ['macroRegion', 'shareData.microRegion'],
+  });
+
+  const { data: microRegionOptions } = useShareCatalog({
+    select: selectMicroRegions(selectedMacroRegion),
+  });
+
+  const { data: availabilityZones } = useShareCatalog({
+    select: selectAvailabilityZones(selectedMicroRegion),
+  });
+
+  const shouldShowMicroRegionSelection = microRegionOptions && microRegionOptions.length > 1;
+  const shouldShowAvailabilityZoneSelection = availabilityZones && availabilityZones.length > 0;
 
   return (
     <FormProvider {...formMethods}>
@@ -25,6 +48,8 @@ export const CreateShareForm = () => {
           <Text preset="heading-2">{t('create:localisation.title')}</Text>
           <DeploymentModeSection />
           <MacroRegionSelection />
+          {shouldShowMicroRegionSelection && <MicroRegionSelection />}
+          {shouldShowAvailabilityZoneSelection && <AvailabilityZoneSelection />}
         </section>
       </div>
     </FormProvider>
