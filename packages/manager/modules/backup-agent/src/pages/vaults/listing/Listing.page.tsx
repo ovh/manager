7fb5b8aae41531011ffb9d5ba860a98772a50c5e
b-fritz-ovh <benjamin.fritz.ext@corp.ovh.com>
2026-01-21 12:00:30 +0100

    feat(backup-agent): add reload button component, remove reload from vaults and add it in agents page
    ref: #BKP-739
    
    Signed-off-by: b-fritz-ovh <benjamin.fritz.ext@corp.ovh.com>

--- packages/manager/modules/backup-agent/src/pages/vaults/listing/Listing.page.tsx
+++ packages/manager/modules/backup-agent/src/pages/vaults/listing/Listing.page.tsx
@@ -2,49 +2,19 @@ import { Suspense } from 'react';
 
 import { Outlet } from 'react-router-dom';
 
-import { useQueryClient } from '@tanstack/react-query';
-import { useTranslation } from 'react-i18next';
-
-import { OdsButton } from '@ovhcloud/ods-components/react';
-
-import { NAMESPACES } from '@ovh-ux/manager-common-translations';
 import { Datagrid } from '@ovh-ux/manager-react-components';
 
-import { BACKUP_AGENT_NAMESPACES } from '@/BackupAgent.translations';
-import { BACKUP_VAULTS_LIST_QUERY_KEY, useBackupVaultsList } from '@/data/hooks/vaults/getVault';
+import { useBackupVaultsList } from '@/data/hooks/vaults/getVault';
 
 import { useColumns } from './_hooks/useColumns.hooks';
 
 export default function ListingPage() {
-  const { t } = useTranslation([BACKUP_AGENT_NAMESPACES.VAULT_LISTING, NAMESPACES.ACTIONS]);
-  const queryClient = useQueryClient();
-  const { data, isLoading } = useBackupVaultsList();
+  const { data } = useBackupVaultsList();
   const columns = useColumns();
-
-  const reloadDatagrid = () => {
-    void queryClient.invalidateQueries({ queryKey: BACKUP_VAULTS_LIST_QUERY_KEY });
-  };
 
   return (
     <Suspense>
-      <Datagrid
-        topbar={
-          <div className="flex justify-end" role="toolbar" aria-label={t('more_action')}>
-            <OdsButton
-              icon="refresh"
-              color="primary"
-              onClick={() => reloadDatagrid()}
-              isLoading={isLoading}
-              variant="outline"
-              data-arialabel={t(`${NAMESPACES.ACTIONS}:refresh`)}
-              label=""
-            />
-          </div>
-        }
-        columns={columns}
-        items={data ?? []}
-        totalItems={3}
-      />
+      <Datagrid columns={columns} items={data ?? []} totalItems={data?.length ?? 0} />
       <Outlet />
     </Suspense>
   );
