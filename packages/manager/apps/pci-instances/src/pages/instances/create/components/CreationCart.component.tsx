e8bfb80f5e31710093b17b94cec0fb8cef5e7c4a
Yann Allançon <yann.allancon.ext@corp.ovh.com>
2026-01-21 11:00:46 +0100

    feat(pci-instances): add banner warning in discovery mode
    ref: #TAPC-5562
    
    Signed-off-by: Yann Allançon <yann.allancon.ext@corp.ovh.com>

--- packages/manager/apps/pci-instances/src/pages/instances/create/components/CreationCart.component.tsx
+++ packages/manager/apps/pci-instances/src/pages/instances/create/components/CreationCart.component.tsx
@@ -1,8 +1,10 @@
 import { Cart } from '@/components/cart/Cart.component';
 import { useCartItems } from '../hooks/useCartItems';
+import { useCartActions } from '../hooks/useCartActions';
 
 export const CreationCart = () => {
-  const { cartItems, cartActions } = useCartItems();
+  const { cartItems } = useCartItems();
+  const { cartActions } = useCartActions();
 
   return <Cart items={cartItems} actionsButtons={cartActions} />;
 };
