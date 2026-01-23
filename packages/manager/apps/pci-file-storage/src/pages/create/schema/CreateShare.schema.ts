6c3c39becc0127b705bb92a6f6dd88b497de4181
aTurmo <adrien.turmo.ext@ovhcloud.com>
2026-01-23 17:26:02 +0100

    feat(pci-file-storage): add share type and size selection
    ref: #TAPC-5644
    
    Signed-off-by: aTurmo <adrien.turmo.ext@ovhcloud.com>

--- packages/manager/apps/pci-file-storage/src/pages/create/schema/CreateShare.schema.ts
+++ packages/manager/apps/pci-file-storage/src/pages/create/schema/CreateShare.schema.ts
@@ -2,21 +2,23 @@ import { z } from 'zod';
 
 import { DEPLOYMENT_MODES } from '@/domain/entities/catalog.entity';
 
-const stringShema = z.string();
+const stringSchema = z.string();
 
-export const shareDataShema = z.object({
-  name: stringShema,
-  microRegion: stringShema,
+export const shareDataSchema = z.object({
+  name: stringSchema,
+  microRegion: stringSchema,
+  specName: stringSchema,
+  size: z.number(),
 });
 
 const deploymentModesShema = z.array(z.enum(DEPLOYMENT_MODES));
-const continentShema = z.union([stringShema, z.literal('all')]);
+const continentShema = z.union([stringSchema, z.literal('all')]);
 
 export const createShareSchema = z.object({
-  shareData: shareDataShema,
+  shareData: shareDataSchema,
   deploymentModes: deploymentModesShema,
   continent: continentShema,
-  macroRegion: stringShema,
+  macroRegion: stringSchema,
   availabilityZone: z.string().nullable(),
 });
 
