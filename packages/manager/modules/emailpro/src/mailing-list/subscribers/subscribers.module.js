3b6fee5ea153168bfd23ee69f7051d6218e9d30f
Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>
2026-01-23 09:50:32 +0100

    fix(web): remove dubplicate import on email
    ref: #INC0184652
    
    Signed-off-by: Guillaume Hyenne <guillaume.hyenne@ovhcloud.com>

--- packages/manager/modules/emailpro/src/mailing-list/subscribers/subscribers.module.js
+++ packages/manager/modules/emailpro/src/mailing-list/subscribers/subscribers.module.js
@@ -3,7 +3,6 @@ import 'angular-translate';
 import '@uirouter/angularjs';
 import '@ovh-ux/ui-kit';
 import '@ovh-ux/ng-translate-async-loader';
-import '@ovh-ux/ng-ovh-export-csv';
 
 import routing from './subscribers.routing';
 
