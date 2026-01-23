5117d04a4e556a2fb71ba0236cd30a8c276b2314
Noemie Foufe <noemie.foufe@corp.ovh.com>
2026-01-23 10:07:09 +0100

    fix(web-domains): use generic sentence when data customization is not allowed
    ref: #D2I-5826
    
    Signed-off-by: Noemie Foufe <noemie.foufe@corp.ovh.com>

--- packages/manager/apps/web-domains/src/domain/components/ConfigurationCards/DataProtection.tsx
+++ packages/manager/apps/web-domains/src/domain/components/ConfigurationCards/DataProtection.tsx
@@ -31,34 +31,46 @@ export default function DataProtection({
         {t('domain_tab_general_information_data_protection')}
       </ManagerTile.Item.Label>
       <ManagerTile.Item.Description>
-        <div className="flex items-center justify-between">
-          <Badge color={statusDetails.color}>
-            {t(statusDetails.i18nkeyContent)}
-          </Badge>
-          <ActionMenu
-            id={'data-protection-action-menu'}
-            isCompact
-            items={[
-              {
-                id: 1,
-                label: t(
-                  'domain_tab_general_information_data_protection_manage_button',
-                ),
-                isDisabled: Object.values(
-                  domainResource?.currentState?.contactsConfiguration,
-                ).every(
-                  (contact: TContactDetails) =>
-                    !contact?.disclosurePolicy?.visibleViaRdds ||
-                    contact?.disclosurePolicy?.forcedDisclosureConfiguration,
-                ),
-                onClick: () => setDataProtectionDrawerOpened(true),
-                iamActions: ['domain:apiovh:name/edit'],
-                urn: domainResource?.iam?.urn || '',
-              },
-            ]}
-          />
-        </div>
-        <Text>{t(statusDetails.i18nkeySubContent)}</Text>
+        {Object.values(domainResource.currentState?.contactsConfiguration).some(
+          (contact: TContactDetails) =>
+            contact.disclosurePolicy?.forcedDisclosureConfiguration,
+        ) ? (
+          <Text>
+            {t('domain_tab_general_information_data_protection_disabled')}
+          </Text>
+        ) : (
+          <>
+            <div className="flex items-center justify-between">
+              <Badge color={statusDetails.color}>
+                {t(statusDetails.i18nkeyContent)}
+              </Badge>
+              <ActionMenu
+                id={'data-protection-action-menu'}
+                isCompact
+                items={[
+                  {
+                    id: 1,
+                    label: t(
+                      'domain_tab_general_information_data_protection_manage_button',
+                    ),
+                    isDisabled: Object.values(
+                      domainResource?.currentState?.contactsConfiguration,
+                    ).every(
+                      (contact: TContactDetails) =>
+                        !contact?.disclosurePolicy?.visibleViaRdds ||
+                        contact?.disclosurePolicy
+                          ?.forcedDisclosureConfiguration,
+                    ),
+                    onClick: () => setDataProtectionDrawerOpened(true),
+                    iamActions: ['domain:apiovh:name/edit'],
+                    urn: domainResource?.iam?.urn || '',
+                  },
+                ]}
+              />
+            </div>
+            <Text>{t(statusDetails.i18nkeySubContent)}</Text>
+          </>
+        )}
       </ManagerTile.Item.Description>
     </ManagerTile.Item>
   );
