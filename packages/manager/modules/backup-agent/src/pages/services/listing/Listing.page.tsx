99d7cec66d75abe6e2e2fb156ae18c07f4915253
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2026-01-26 22:30:03 +0100

    feat(backup-agent): add tabs descriptions
    ref: #BKP-754
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>
    Signed-off-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/backup-agent/src/pages/services/listing/Listing.page.tsx
+++ packages/manager/modules/backup-agent/src/pages/services/listing/Listing.page.tsx
@@ -2,18 +2,42 @@ import { Suspense } from 'react';
 
 import { Outlet } from 'react-router-dom';
 
-import { Datagrid } from '@ovh-ux/manager-react-components';
+import { Trans, useTranslation } from 'react-i18next';
 
+import { OdsText } from '@ovhcloud/ods-components/react';
+
+import { Datagrid, Links } from '@ovh-ux/manager-react-components';
+
+import { BACKUP_AGENT_NAMESPACES } from '@/BackupAgent.translations';
 import { useVSPCTenants } from '@/data/hooks/tenants/useVspcTenants';
+import { useGuideUtils } from '@/hooks/useGuideUtils';
 
 import { useTenantListingColumns } from './_hooks/useVspcTenantListingColumns';
 
 export default function ListingPage() {
+  useTranslation(BACKUP_AGENT_NAMESPACES.SERVICE_LISTING);
   const columns = useTenantListingColumns();
   const { data, isPending } = useVSPCTenants();
+  const guide = useGuideUtils();
 
   return (
-    <>
+    <section className="flex flex-col gap-8">
+      <OdsText>
+        <Trans
+          ns={BACKUP_AGENT_NAMESPACES.SERVICE_LISTING}
+          i18nKey="services_tab_description"
+          components={{
+            Link: (
+              <Links
+                className="px-2"
+                rel="noopener noreferrer"
+                target="_blank"
+                href={guide?.vspc}
+              />
+            ),
+          }}
+        />
+      </OdsText>
       <Suspense>
         {columns && (
           <Datagrid
@@ -25,6 +49,6 @@ export default function ListingPage() {
         )}
       </Suspense>
       <Outlet />
-    </>
+    </section>
   );
 }
