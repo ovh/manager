96ed8ad42b7ab65edef056b06a8ec79265f1bf29
aTurmo <adrien.turmo.ext@ovhcloud.com>
2026-01-26 17:49:15 +0100

    feat(pci-file-storage): add name min size and regex
    ref: #TAPC-5427
    
    Signed-off-by: aTurmo <adrien.turmo.ext@ovhcloud.com>

--- packages/manager/apps/pci-file-storage/src/pages/create/components/name/NameInput.component.tsx
+++ packages/manager/apps/pci-file-storage/src/pages/create/components/name/NameInput.component.tsx
@@ -1,4 +1,5 @@
-import clsx from 'clsx';
+import { useMemo } from 'react';
+
 import { useFormContext } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
@@ -12,28 +13,38 @@ export const NameInput = () => {
   const {
     register,
     formState: {
-      errors: { shareData },
+      errors: { shareData: { name: nameError } = {} },
     },
   } = useFormContext<CreateShareFormValues>();
 
-  const error = shareData?.name;
+  const errorMessage = useMemo(() => {
+    if (!nameError) return undefined;
+    switch (nameError.message) {
+      case 'name_required':
+        return t('create:name.error.required');
+      case 'name_max_length':
+        return t('create:name.error.max_length');
+      case 'name_invalid_format':
+        return t('create:name.error.invalid_format');
+      default:
+        return nameError.message;
+    }
+  }, [nameError, t]);
 
   return (
     <article className="flex w-full flex-col">
       <div className="mt-4 pb-4 pt-3">
         <FormField className="max-w-[50%]">
           <FormFieldLabel>{t('create:name.label')}</FormFieldLabel>
-          <Input {...register('shareData.name')} invalid={!!error} className="w-full" />
+          <Input {...register('shareData.name')} invalid={!!errorMessage} className="w-full" />
         </FormField>
       </div>
-      <Text
-        className={clsx('text-sm', {
-          'text-[--ods-color-critical-500]': !!error,
-        })}
-        preset="span"
-      >
-        {t('create:name.info')}
-      </Text>
+      {!!errorMessage && (
+        <Text className="text-sm text-[--ods-color-critical-500]" preset="span">
+          {errorMessage}
+        </Text>
+      )}
+      <Text preset="span">{t('create:name.info')}</Text>
     </article>
   );
 };
