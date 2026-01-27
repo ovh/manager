64e02b560f9d85ce3856c0a67c461bc96b979825
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2026-01-27 17:13:10 +0100

    chore(okms): add test-ids on credential dashboard
    ref: #EK-2578
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

---
+++ packages/manager/apps/okms/src/modules/key-management-service/pages/credential/general-informations/generalInformations.constants.ts
@@ -0,0 +1,12 @@
+export const CREDENTIAL_GENERAL_INFORMATIONS_TEST_IDS = {
+  id: 'credential-id',
+  displayName: 'credential-display-name',
+  description: 'credential-description',
+  status: 'credential-status',
+  type: 'credential-type',
+  origin: 'credential-origin',
+  creationDate: 'credential-creation-date',
+  expirationDate: 'credential-expiration-date',
+  downloadButton: 'credential-download-button',
+  deleteButton: 'credential-delete-button',
+};
