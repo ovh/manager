ee4c18ca050bcb8e1dd665b05a8a645490921484
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2026-01-27 15:36:16 +0100

    chore(okms): add test-ids on service-key dashboard
    ref: #EK-2578
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/okms/src/modules/key-management-service/pages/service-key/dashboard/ServiceKeyDashboard.constants.ts
+++ packages/manager/apps/okms/src/modules/key-management-service/pages/service-key/dashboard/ServiceKeyDashboard.constants.ts
@@ -1,4 +1,13 @@
 export const SERVICE_KEY_TEST_IDS = {
   editNameButton: 'edit-name-button',
   modifyNameButton: 'modify-name-button',
+  displayName: 'service-key-display-name',
+  keyId: 'service-key-id',
+  urn: 'service-key-urn',
+  status: 'service-key-status',
+  creationDate: 'service-key-creation-date',
+  origin: 'service-key-origin',
+  type: 'service-key-type',
+  size: 'service-key-size',
+  usage: 'service-key-usage',
 };
