5ecb0010d78aa048c22765ffb6f7fcefbad51506
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2026-01-27 17:13:10 +0100

    chore(okms): add test-ids on credential dashboard
    ref: #EK-2578
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/okms/src/modules/key-management-service/components/credential/credential-creation-method/credentialCreationMethod.component.tsx
+++ packages/manager/apps/okms/src/modules/key-management-service/components/credential/credential-creation-method/credentialCreationMethod.component.tsx
@@ -1,20 +1,24 @@
-import { TileValue } from '@key-management-service/components/dashboard/tile-value/tileValue.component';
-import { OkmsCredential } from '@key-management-service/types/okmsCredential.type';
 import { useTranslation } from 'react-i18next';
 
-type TCredentialCreationMethod = Pick<OkmsCredential, 'fromCSR'>;
+import { Text } from '@ovhcloud/ods-react';
 
-const CredentialCreationMethod = ({ fromCSR }: TCredentialCreationMethod) => {
+type CredentialCreationMethodProps = {
+  fromCSR: boolean;
+  'data-testid'?: string;
+};
+
+const CredentialCreationMethod = ({
+  fromCSR,
+  'data-testid': dataTestId,
+}: CredentialCreationMethodProps) => {
   const { t } = useTranslation('key-management-service/credential');
 
   return (
-    <TileValue
-      value={
-        fromCSR
-          ? t('key_management_service_credential_created_with_csr')
-          : t('key_management_service_credential_created_without_csr')
-      }
-    />
+    <Text preset="span" data-testid={dataTestId}>
+      {fromCSR
+        ? t('key_management_service_credential_created_with_csr')
+        : t('key_management_service_credential_created_without_csr')}
+    </Text>
   );
 };
 
