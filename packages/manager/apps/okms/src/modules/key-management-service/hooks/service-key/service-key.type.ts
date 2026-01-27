a43967540286dec34ad1faea63ca3eb46e56b86d
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2026-01-27 15:36:16 +0100

    chore(okms): add test-ids on service-key dashboard
    ref: #EK-2578
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/okms/src/modules/key-management-service/hooks/service-key/service-key.type.ts
+++ packages/manager/apps/okms/src/modules/key-management-service/hooks/service-key/service-key.type.ts
@@ -3,7 +3,11 @@ import { BUTTON_COLOR, ICON_NAME } from '@ovhcloud/ods-react';
 import { ButtonProps } from '@ovh-ux/muk';
 
 export type ServiceKeyAction = {
-  name: string;
+  name:
+    | 'download_encryption_key'
+    | 'deactivate_encryption_key'
+    | 'reactivate_encryption_key'
+    | 'delete_encryption_key';
   label: string;
   color: BUTTON_COLOR;
   onClick: () => void;
