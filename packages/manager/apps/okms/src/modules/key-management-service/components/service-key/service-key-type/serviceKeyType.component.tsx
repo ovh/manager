986cbbf2d12b6f9540562d2448f48f52680bb766
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2026-01-27 17:13:10 +0100

    chore(okms): add test-ids on credential dashboard
    ref: #EK-2578
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/okms/src/modules/key-management-service/components/service-key/service-key-type/serviceKeyType.component.tsx
+++ packages/manager/apps/okms/src/modules/key-management-service/components/service-key/service-key-type/serviceKeyType.component.tsx
@@ -1,6 +1,7 @@
-import { TileValue } from '@key-management-service/components/dashboard/tile-value/tileValue.component';
 import { useServiceKeyTypeTranslations } from '@key-management-service/hooks/service-key/useServiceKeyTypeTranslations';
 import { OkmsKeyTypes } from '@key-management-service/types/okmsServiceKey.type';
+
+import { Text } from '@ovhcloud/ods-react';
 
 type KeyStatusProps = {
   type: OkmsKeyTypes;
@@ -9,5 +10,5 @@ type KeyStatusProps = {
 export const ServiceKeyType = ({ type }: KeyStatusProps) => {
   const translatedType = useServiceKeyTypeTranslations(type);
 
-  return <TileValue value={translatedType} />;
+  return <Text preset="span">{translatedType}</Text>;
 };
