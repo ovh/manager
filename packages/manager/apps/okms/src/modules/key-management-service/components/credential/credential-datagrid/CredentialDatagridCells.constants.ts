fafd023ac7abfedf61c89b2a75e8b6548d44adae
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2026-01-27 17:50:20 +0100

    chore(okms): add test-ids on credential list
    ref: #EK-2578
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

---
+++ packages/manager/apps/okms/src/modules/key-management-service/components/credential/credential-datagrid/CredentialDatagridCells.constants.ts
@@ -0,0 +1,9 @@
+export const CREDENTIAL_LIST_CELL_TEST_IDS = {
+  name: (id: string) => `credential-${id}-name`,
+  id: (id: string) => `credential-${id}-id`,
+  nbUsersIds: (id: string) => `credential-${id}-nb-users-ids`,
+  creationDate: (id: string) => `credential-${id}-creation-date`,
+  expirationDate: (id: string) => `credential-${id}-expiration-date`,
+  status: (id: string) => `credential-${id}-status`,
+  actions: (id: string) => `credential-${id}-actions`,
+};
