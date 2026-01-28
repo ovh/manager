033aefdfbd2d85d9c48634f5a887646270c9335a
Héla Ben Khalfallah <helabenkhalfallah@hotmail.fr>
2026-01-28 18:07:50 +0100

    fix(*): lint issues
    ref: #MANAGER-20817
    
    Signed-off-by: Héla Ben Khalfallah <helabenkhalfallah@hotmail.fr>

--- packages/manager-tools/manager-tests-setup/src/unit/vitest.config.js
+++ packages/manager-tools/manager-tests-setup/src/unit/vitest.config.js
@@ -127,20 +127,14 @@ export const sharedConfig = createConfig({
     environment: 'jsdom',
 
     // timeout config
-    retry: 10,
     testTimeout: 60_000,
     hookTimeout: 60_000,
     teardownTimeout: 60_000,
-    fileParallelism: false,
-    pollOptions: {
-      forks: {
-        singleFork: true,
-      },
-      threads: {
-        singleThread: true,
-      },
-    },
     
+    // main stability knobs
+    retry: 10,
+    fileParallelism: false,
+
     // coverage options
     coverage: {
       include: ['src'],
