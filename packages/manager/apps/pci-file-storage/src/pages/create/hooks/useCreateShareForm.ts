700855b097584721cefb14d9999db2b1f2bd7907
aTurmo <adrien.turmo.ext@ovhcloud.com>
2026-01-28 09:48:26 +0100

    feat(pci-file-storage): add form buttons and rules for share name
    ref: #TAPC-5427
    
    Signed-off-by: aTurmo <adrien.turmo.ext@ovhcloud.com>

--- packages/manager/apps/pci-file-storage/src/pages/create/hooks/useCreateShareForm.ts
+++ packages/manager/apps/pci-file-storage/src/pages/create/hooks/useCreateShareForm.ts
@@ -1,9 +1,13 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 
-import { TRegionData } from '@/adapters/catalog/left/shareCatalog.data';
+import { TRegionData, TShareSpecData } from '@/adapters/catalog/left/shareCatalog.data';
 import { useShareCatalog } from '@/data/hooks/catalog/useShareCatalog';
-import { selectLocalizations } from '@/pages/create/view-model/shareCatalog.view-model';
+import { generateAutoName } from '@/pages/create/view-model/network.view-model';
+import {
+  selectLocalizations,
+  selectShareSpecs,
+} from '@/pages/create/view-model/shareCatalog.view-model';
 
 import { CreateShareFormValues, createShareSchema } from '../schema/CreateShare.schema';
 
@@ -12,19 +16,30 @@ export const useCreateShareForm = () => {
     select: selectLocalizations({ deploymentModes: ['region', 'region-3-az'], continentId: 'all' }),
   });
 
+  const defaultMicroRegion = localizations[0]?.microRegion ?? '';
+  const defaultMacroRegion = localizations[0]?.macroRegion ?? '';
+
+  const { data: shareOptions = [] } = useShareCatalog<TShareSpecData[]>({
+    select: selectShareSpecs(defaultMicroRegion),
+  });
+
+  const defaultShareName = shareOptions[0]?.name ?? '';
+  const defaultSize = shareOptions[0]?.capacityMin ?? 150;
+  const defaultName = generateAutoName(defaultShareName);
+
   return useForm<CreateShareFormValues>({
     resolver: zodResolver(createShareSchema),
     defaultValues: {
       shareData: {
-        name: '',
-        microRegion: localizations[0]?.microRegion ?? '',
-        specName: '',
-        size: 150,
+        name: defaultName,
+        microRegion: defaultMicroRegion,
+        specName: defaultShareName,
+        size: defaultSize,
         privateNetworkId: undefined,
       },
       deploymentModes: ['region', 'region-3-az'],
       continent: 'all',
-      macroRegion: localizations[0]?.macroRegion ?? '',
+      macroRegion: defaultMacroRegion,
       availabilityZone: null,
     },
     mode: 'onChange',
