cedae00c0a234d66a5e1b13d367599737546816d
Romain Jamet <romain.jamet@corp.ovh.com>
2026-01-29 15:56:01 +0100

    fix(okms): update tracking
    ref: #EK-2580
    
    Signed-off-by: Romain Jamet <romain.jamet@corp.ovh.com>

--- packages/manager/apps/okms/src/common/pages/order-okms-modal/OrderOkmsModal.page.tsx
+++ packages/manager/apps/okms/src/common/pages/order-okms-modal/OrderOkmsModal.page.tsx
@@ -16,8 +16,10 @@ import {
 } from '@ovhcloud/ods-react';
 
 import { NAMESPACES } from '@ovh-ux/manager-common-translations';
+import { PageType } from '@ovh-ux/manager-react-shell-client';
 import { Button } from '@ovh-ux/muk';
 
+import { useOkmsTracking } from '@/common/hooks/useOkmsTracking';
 import { useProductType } from '@/common/hooks/useProductType';
 import { useRequiredParams } from '@/common/hooks/useRequiredParams';
 import { useShellContext } from '@/common/hooks/useShellContext';
@@ -37,10 +39,17 @@ const OrderOkmsModal = () => {
   const productType = useProductType();
   const { ovhSubsidiary } = environment.getUser();
   const navigate = useNavigate();
+  const { trackPage } = useOkmsTracking();
 
   const onSuccess = () => {
     // Register the pending order
     registerPendingOrder(region);
+
+    // Track success banner
+    trackPage({
+      pageType: PageType.bannerSuccess,
+      pageTags: ['order', 'okms'],
+    });
 
     // Redirect to the appropriate page
     if (productType === 'key-management-service') {
