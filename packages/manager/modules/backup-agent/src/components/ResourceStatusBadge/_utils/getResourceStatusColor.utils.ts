7fc83c9dff211ea7e01b803686fc3e40c6445765
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2026-01-29 14:08:03 +0100

    feat(backup-agent): add status legend in agent listing page
    ref: #BKP-740
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>
    Signed-off-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/backup-agent/src/components/ResourceStatusBadge/_utils/getResourceStatusColor.utils.ts
+++ packages/manager/modules/backup-agent/src/components/ResourceStatusBadge/_utils/getResourceStatusColor.utils.ts
@@ -1,7 +1,7 @@
 import { ODS_BADGE_COLOR } from '@ovhcloud/ods-components';
 
-import { vaultStatusColor } from '@/components/ResourceStatusBadge/_utils/resourceStatusColor.utils';
+import { allResourceStatusColor } from '@/components/ResourceStatusBadge/_utils/resourceStatusColor.utils';
 import { AgentStatus, ResourceStatus } from '@/types/Resource.type';
 
 export const getColorResourceStatus = (status: ResourceStatus | AgentStatus) =>
-  vaultStatusColor[status.toUpperCase() as ResourceStatus] ?? ODS_BADGE_COLOR.information;
+  allResourceStatusColor[status.toUpperCase() as ResourceStatus] ?? ODS_BADGE_COLOR.information;
