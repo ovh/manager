d92a798953e331ce9e76378ea86a091c9edc3005
Louis BENSI <louis.bensi@corp.ovh.com>
2026-01-29 16:33:37 +0100

    feat(web-domain-reseller): general informations
    ref: #DCE-151
    
    Signed-off-by: Louis BENSI <louis.bensi@corp.ovh.com>

---
+++ packages/manager/apps/web-domains/src/domain-reseller/components/Dashboard/GeneralInformations.tsx
@@ -0,0 +1,32 @@
+import { NAMESPACES } from '@ovh-ux/manager-common-translations';
+import { ManagerTile } from '@ovh-ux/manager-react-components';
+import { Text } from '@ovhcloud/ods-react';
+import { useTranslation } from 'react-i18next';
+
+interface GeneralInformationsProps {
+  domainsLength: number;
+}
+
+export default function GeneralInformations({
+  domainsLength,
+}: GeneralInformationsProps) {
+  const { t } = useTranslation(['domain-reseller', NAMESPACES.DASHBOARD]);
+  return (
+    <ManagerTile>
+      <ManagerTile.Title>
+        {t(`${NAMESPACES.DASHBOARD}:general_information`)}
+      </ManagerTile.Title>
+      <ManagerTile.Divider />
+      <ManagerTile.Item>
+        <ManagerTile.Item.Label>
+          {t('domain_reseller_general_informations_label')}
+        </ManagerTile.Item.Label>
+        <Text>
+          {t('domain_reseller_general_informations_domains_length', {
+            length: domainsLength,
+          })}
+        </Text>
+      </ManagerTile.Item>
+    </ManagerTile>
+  );
+}
