931b08ff0b40177292df7b92f48ce902b0f195d1
aderghamov <alexandre.dergham.ext@ovhcloud.com>
2026-01-29 16:47:33 +0100

    feat(vrack): adding breadcrumb and tab links to the legacy app
    ref: #MANAGER-20728
    
    Signed-off-by: aderghamov <alexandre.dergham.ext@ovhcloud.com>

--- packages/manager/apps/network-vrack/src/pages/dashboard/Dashboard.page.tsx
+++ packages/manager/apps/network-vrack/src/pages/dashboard/Dashboard.page.tsx
@@ -3,24 +3,28 @@ import { Outlet, useParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 import { Spinner, Tab, TabList, Tabs } from '@ovhcloud/ods-react';
+import type { TabsValueChangeEvent } from '@ovhcloud/ods-react';
 
-import { BaseLayout, Breadcrumb, ChangelogMenu, GuideMenu } from '@ovh-ux/muk';
+import { BaseLayout, ChangelogMenu, GuideMenu } from '@ovh-ux/muk';
 
-import { appName } from '@/App.constants';
 import { DashboardHeaderTitle } from '@/components/DashboardHeaderTitle';
 import { DashboardSubtitle } from '@/components/DashboardSubtitle';
 import { SurveyLink } from '@/components/SurveyLink';
+import { Breadcrumb } from '@/components/breadcrumb/Breadcrumb';
 import { useGuideLinks } from '@/hooks/useGuideLinks';
+import { useNavigateToLegacyApp } from '@/hooks/useNavigateToLegacyApp';
 import { useGetVrackDetails } from '@/hooks/vrack/useGetVrackDetails';
 import { CHANGELOG_CHAPTERS, CHANGELOG_LINKS } from '@/utils/constants';
 
 import NotFound from '../not-found/Error404.page';
 
 export default function DashboardPage() {
+  const VRACK_LISTING_TAB = 'listing';
   const { serviceName } = useParams<{ serviceName: string }>();
   const { t } = useTranslation('dashboard');
   const guideItems = useGuideLinks();
   const { data: vrack, isLoading } = useGetVrackDetails(serviceName);
+  const navigateToLegacy = useNavigateToLegacyApp();
 
   if (isLoading) {
     return <Spinner />;
@@ -28,12 +32,16 @@ export default function DashboardPage() {
     return <NotFound />;
   }
 
+  const onTabChange = ({ value }: TabsValueChangeEvent) => {
+    if (value === VRACK_LISTING_TAB) {
+      navigateToLegacy(serviceName);
+    }
+  };
+
   return (
     !!vrack && (
       <BaseLayout
-        breadcrumb={
-          <Breadcrumb appName={appName} rootLabel={t('dashboard_vrackBreadcrumbLabel')} />
-        }
+        breadcrumb={<Breadcrumb />}
         header={{
           title: <DashboardHeaderTitle vrack={vrack} />,
           changelogButton: <ChangelogMenu links={CHANGELOG_LINKS} chapters={CHANGELOG_CHAPTERS} />,
@@ -45,8 +53,9 @@ export default function DashboardPage() {
               <DashboardSubtitle vrack={vrack} />
               <SurveyLink />
             </span>
-            <Tabs value="vrack.dashboard.publicIpRouting">
+            <Tabs value="vrack.dashboard.publicIpRouting" onValueChange={onTabChange}>
               <TabList>
+                <Tab value={VRACK_LISTING_TAB}>{t('dashboard_allAttachedServicesTab')}</Tab>
                 <Tab value="vrack.dashboard.publicIpRouting">
                   {t('dashboard_publicIpConnectivityTab')}
                 </Tab>
