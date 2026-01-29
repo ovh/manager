af82b18e72ca3a010f5de84b207bf91aacf5ccea
aderghamov <alexandre.dergham.ext@ovhcloud.com>
2026-01-29 11:13:55 +0100

    feat(vrack): adding the public routing bandwidth tiles (#21938)
    ref: #MANAGER-20377
    
    Signed-off-by: aderghamov <alexandre.dergham.ext@ovhcloud.com>

---
+++ packages/manager/apps/network-vrack/src/components/flag/Flag.tsx
@@ -0,0 +1,16 @@
+import { getCountryCode } from '@/utils/region';
+
+interface FlagDisplayProps {
+  region: string;
+}
+
+export const Flag = ({ region }: FlagDisplayProps) => {
+  return (
+    <div
+      style={{
+        backgroundImage: `url('flags/${getCountryCode(region)}.svg')`,
+      }}
+      className={`mr-3 w-10 bg-cover shadow-md`}
+    />
+  );
+};
