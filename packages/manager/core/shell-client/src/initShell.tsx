48ca295d405cde6fe6005afdb391351f8d71aa4a
Héla Ben Khalfallah <helabenkhalfallah@hotmail.fr>
2026-01-30 19:38:12 +0100

    fix(*): build issues
    ref: #MANAGER-20410
    
    Signed-off-by: Héla Ben Khalfallah <helabenkhalfallah@hotmail.fr>

--- packages/manager/core/shell-client/src/initShell.tsx
+++ packages/manager/core/shell-client/src/initShell.tsx
@@ -1,11 +1,14 @@
 import { defineApplicationVersion } from '@ovh-ux/request-tagger';
 import { initShellClient } from '@ovh-ux/shell';
 
-import { TrackingContextParams } from './ShellContext';
+import { ShellContextType, TrackingContextParams } from './ShellContext';
 
 declare const __VERSION__: string;
 
-export const initShellContext = async (appName: string, tracking?: TrackingContextParams) => {
+export const initShellContext = async (
+  appName: string,
+  tracking?: TrackingContextParams,
+): Promise<ShellContextType> => {
   const shell = await initShellClient(appName);
   const environment = await shell.environment.getEnvironment();
 
