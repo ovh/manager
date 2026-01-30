fbb3dc3d876ef786f38fe470cb57fe730d96a4a4
David Arsène <david.arsene.ext@ovhcloud.com>
2026-01-30 15:30:58 +0100

    chore(observability): bump ODS 19.5
    resolves: #MAOBS-29
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/observability/src/components/listing/common/confirmation-modal/ConfirmationModal.component.tsx
+++ packages/manager/apps/observability/src/components/listing/common/confirmation-modal/ConfirmationModal.component.tsx
@@ -8,6 +8,9 @@ import {
   Button,
   MODAL_COLOR,
   Message,
+  Modal,
+  ModalBody,
+  ModalContent,
   SPINNER_SIZE,
   Spinner,
   TEXT_PRESET,
@@ -15,7 +18,6 @@ import {
 } from '@ovhcloud/ods-react';
 
 import { NAMESPACES } from '@ovh-ux/manager-common-translations';
-import { Modal } from '@ovh-ux/muk';
 
 import ErrorMessage from '@/components/error/ErrorMessage.component';
 import { ConfirmationModalProps } from '@/components/listing/common/confirmation-modal/ConfirmationModal.props';
@@ -36,52 +38,54 @@ export const ConfirmationModal = ({
   const { t } = useTranslation(NAMESPACES.ACTIONS);
 
   return (
-    <Modal data-testid="confirmation-modal" type={type} onOpenChange={onDismiss} open={true}>
-      <div className="space-y-4">
-        {error && (
-          <Message
-            color="critical"
-            dismissible={false}
-            className="w-full"
-            data-testid="confirmation-modal-error-message"
-          >
-            <ErrorMessage error={error} />
-          </Message>
-        )}
-
-        <Text preset={TEXT_PRESET.heading4}>{title}</Text>
-        <Text preset={TEXT_PRESET.paragraph}>{message}</Text>
-
-        {isLoading && (
-          <div data-testid="spinner" className="my-5 flex justify-center">
-            <Spinner size={SPINNER_SIZE.md} inline-block></Spinner>
-          </div>
-        )}
-
-        {!isLoading && (
-          <div className="flex justify-end gap-2">
-            <Button
-              data-testid={'cancel-button-test-id'}
-              color={BUTTON_COLOR.primary}
-              onClick={onDismiss}
-              variant={BUTTON_VARIANT.ghost}
+    <Modal data-testid="confirmation-modal" open>
+      <ModalContent dismissible={false}>
+        <ModalBody className="space-y-4">
+          {error && (
+            <Message
+              color="critical"
+              dismissible={false}
+              className="w-full"
+              data-testid="confirmation-modal-error-message"
             >
-              {cancelButtonLabel || t('cancel')}
-            </Button>
+              <ErrorMessage error={error} />
+            </Message>
+          )}
 
-            <Button
-              data-testid={'confirm-button-test-id'}
-              color={type === MODAL_COLOR.critical ? BUTTON_COLOR.critical : BUTTON_COLOR.primary}
-              onClick={onConfirm}
-              disabled={isConfirmButtonDisabled || isConfirmButtonLoading}
-              loading={isConfirmButtonLoading}
-              variant={BUTTON_VARIANT.default}
-            >
-              {confirmButtonLabel || t('confirm')}
-            </Button>
-          </div>
-        )}
-      </div>
+          <Text preset={TEXT_PRESET.heading4}>{title}</Text>
+          <Text preset={TEXT_PRESET.paragraph}>{message}</Text>
+
+          {isLoading && (
+            <div data-testid="spinner" className="my-5 flex justify-center">
+              <Spinner size={SPINNER_SIZE.md} inline-block></Spinner>
+            </div>
+          )}
+
+          {!isLoading && (
+            <div className="flex justify-end gap-2">
+              <Button
+                data-testid={'cancel-button-test-id'}
+                color={BUTTON_COLOR.primary}
+                onClick={onDismiss}
+                variant={BUTTON_VARIANT.ghost}
+              >
+                {cancelButtonLabel || t('cancel')}
+              </Button>
+
+              <Button
+                data-testid={'confirm-button-test-id'}
+                color={type === MODAL_COLOR.critical ? BUTTON_COLOR.critical : BUTTON_COLOR.primary}
+                onClick={onConfirm}
+                disabled={isConfirmButtonDisabled || isConfirmButtonLoading}
+                loading={isConfirmButtonLoading}
+                variant={BUTTON_VARIANT.default}
+              >
+                {confirmButtonLabel || t('confirm')}
+              </Button>
+            </div>
+          )}
+        </ModalBody>
+      </ModalContent>
     </Modal>
   );
 };
