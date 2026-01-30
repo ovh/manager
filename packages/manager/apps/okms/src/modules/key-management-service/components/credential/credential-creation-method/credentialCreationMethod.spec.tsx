21e023055ab74e26264f93914722f117ded31db1
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2026-01-30 16:23:28 +0100

    fix(okms): warnings during tests
    ref: #EK-2649
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/okms/src/modules/key-management-service/components/credential/credential-creation-method/credentialCreationMethod.spec.tsx
+++ packages/manager/apps/okms/src/modules/key-management-service/components/credential/credential-creation-method/credentialCreationMethod.spec.tsx
@@ -1,6 +1,9 @@
 import { render } from '@testing-library/react';
 import { describe, expect, test } from 'vitest';
 
+import { labels } from '@/common/utils/tests/init.i18n';
+import { testWrapperBuilder } from '@/common/utils/tests/testWrapperBuilder';
+
 import CredentialCreationMethod from './credentialCreationMethod.component';
 
 describe('CredentialStatus component test suite', () => {
@@ -11,23 +14,26 @@ describe('CredentialStatus component test suite', () => {
   }[] = [
     {
       fromCSR: true,
-      expectedLabel: 'key_management_service_credential_created_with_csr',
-      otherLabel: 'key_management_service_credential_created_without_csr',
+      expectedLabel: labels.credentials.key_management_service_credential_created_with_csr,
+      otherLabel: labels.credentials.key_management_service_credential_created_without_csr,
     },
     {
       fromCSR: false,
-      expectedLabel: 'key_management_service_credential_created_without_csr',
-      otherLabel: 'key_management_service_credential_created_with_csr',
+      expectedLabel: labels.credentials.key_management_service_credential_created_without_csr,
+      otherLabel: labels.credentials.key_management_service_credential_created_with_csr,
     },
   ];
 
   test.each(useCases)(
     'should return the right translation text for `fromCSR===$fromCSR`',
-    ({ fromCSR, expectedLabel, otherLabel }) => {
+    async ({ fromCSR, expectedLabel, otherLabel }) => {
       // given fromCSR
+      const wrapper = await testWrapperBuilder().withI18next().build();
 
       // when
-      const { getByText, queryByText } = render(<CredentialCreationMethod fromCSR={fromCSR} />);
+      const { getByText, queryByText } = render(<CredentialCreationMethod fromCSR={fromCSR} />, {
+        wrapper,
+      });
 
       // then
       expect(getByText(expectedLabel)).toBeInTheDocument();
