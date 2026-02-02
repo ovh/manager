bde3ee0d4fe9f3f2d3227afccf58eb919086cb84
b-fritz-ovh <benjamin.fritz.ext@corp.ovh.com>
2026-02-02 14:14:12 +0100

    feat(backup-agent): add links to dashboards in subscription tile
    ref: #BKP-788
    
    Signed-off-by: b-fritz-ovh <benjamin.fritz.ext@corp.ovh.com>

--- packages/manager/modules/backup-agent/src/pages/services/dashboard/general-information/_components/subscription-tile/SubscriptionTile.component.tsx
+++ packages/manager/modules/backup-agent/src/pages/services/dashboard/general-information/_components/subscription-tile/SubscriptionTile.component.tsx
@@ -1,12 +1,16 @@
 import { useTranslation } from 'react-i18next';
 
-import { OdsSkeleton, OdsText } from '@ovhcloud/ods-components/react';
+import { useHref } from 'react-router-dom';
+
+import { OdsSkeleton, OdsText, OdsLink } from '@ovhcloud/ods-components/react';
+import { ODS_ICON_NAME } from '@ovhcloud/ods-components';
 
 import { NAMESPACES } from '@ovh-ux/manager-common-translations';
 import { ManagerTile } from '@ovh-ux/manager-react-components';
 
 import { BACKUP_AGENT_NAMESPACES } from '@/BackupAgent.translations';
 import { useBackupTenantDetails } from '@/data/hooks/tenants/useBackupTenantDetails';
+import { urlParams, urls } from '@/routes/routes.constants';
 
 import { useTenantBackupStats } from './_hooks/useTenantBackupStats';
 
@@ -19,6 +23,7 @@ const SubscriptionTile = ({ tenantId }: SubscriptionTileProps) => {
     NAMESPACES.DASHBOARD,
     NAMESPACES.BILLING,
     BACKUP_AGENT_NAMESPACES.SERVICE_DASHBOARD,
+    BACKUP_AGENT_NAMESPACES.COMMON,
   ]);
   const { data, isPending } = useBackupTenantDetails();
   const {
@@ -30,6 +35,9 @@ const SubscriptionTile = ({ tenantId }: SubscriptionTileProps) => {
     vspcTenantIds: [tenantId ?? ''].filter(Boolean),
   });
 
+  const backupAgentsHref = useHref(urls.dashboardTenantAgents.replace(urlParams.tenantId, tenantId ?? ''));
+  const vaultsHref = useHref(urls.listingVaults);
+
   return (
     <ManagerTile className="h-fit">
       <ManagerTile.Title>{t(`${NAMESPACES.BILLING}:subscription`)}</ManagerTile.Title>
@@ -39,7 +47,16 @@ const SubscriptionTile = ({ tenantId }: SubscriptionTileProps) => {
           {t(`${BACKUP_AGENT_NAMESPACES.SERVICE_DASHBOARD}:linked_server`)}
         </ManagerTile.Item.Label>
         <ManagerTile.Item.Description>
-          {isPending || isStatsPending ? <OdsSkeleton /> : <OdsText>{installedAgentsText}</OdsText>}
+          {isPending || isStatsPending ? <OdsSkeleton /> : 
+          <div className='flex justify-between'>
+            <OdsText>{installedAgentsText}</OdsText>
+            <OdsLink
+              className="[&::part(link)]:hover:!bg-none [&::part(link)]:!transition-none"
+              icon={ODS_ICON_NAME.arrowRight}
+              href={backupAgentsHref}
+              data-arialabel={t(`${BACKUP_AGENT_NAMESPACES.COMMON}:go_to_general_informations`)}
+            />
+          </div>}
         </ManagerTile.Item.Description>
       </ManagerTile.Item>
       <ManagerTile.Divider />
@@ -48,7 +65,16 @@ const SubscriptionTile = ({ tenantId }: SubscriptionTileProps) => {
           {t(`${BACKUP_AGENT_NAMESPACES.SERVICE_DASHBOARD}:linked_vaults`)}
         </ManagerTile.Item.Label>
         <ManagerTile.Item.Description>
-          {isPending || isStatsPending ? <OdsSkeleton /> : <OdsText>{connectedVaultsText}</OdsText>}
+          {isPending || isStatsPending ? <OdsSkeleton /> : 
+          <div className='flex justify-between'>
+            <OdsText>{connectedVaultsText}</OdsText>
+            <OdsLink
+              className="[&::part(link)]:hover:!bg-none [&::part(link)]:!transition-none"
+              icon={ODS_ICON_NAME.arrowRight}
+              href={vaultsHref}
+              data-arialabel={t(`${BACKUP_AGENT_NAMESPACES.COMMON}:go_to_general_informations`)}
+            />
+          </div>}
         </ManagerTile.Item.Description>
       </ManagerTile.Item>
     </ManagerTile>
