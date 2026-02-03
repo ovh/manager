e250c305fc7a5d1cda9a38a6761c3bca185f21b9
Louis BENSI <louis.bensi@corp.ovh.com>
2026-02-03 17:39:45 +0100

    feat(web-domain-reseller): subscription card
    ref: #DCE-148
    
    Signed-off-by: Louis BENSI <louis.bensi@corp.ovh.com>

---
+++ packages/manager/apps/web-domains/src/domain-reseller/components/Dashboard/Subscription.tsx
@@ -0,0 +1,58 @@
+import { TServiceInfo } from '@/common/types/common.types';
+import { NAMESPACES } from '@ovh-ux/manager-common-translations';
+import { ManagerTile } from '@ovh-ux/manager-react-components';
+import { useFormatDate } from '@ovh-ux/muk';
+import { Badge, BADGE_COLOR, Text } from '@ovhcloud/ods-react';
+import { useTranslation } from 'react-i18next';
+import CreationDate from '../SubscriptionTiles/CreationDate';
+import Contacts from '../SubscriptionTiles/Contacts';
+
+interface SubscriptionProps {
+  creationDate: string;
+  expirationDate: string;
+  contacts: TServiceInfo['customer']['contacts'];
+  serviceName: string;
+}
+
+export default function Subscription({
+  creationDate,
+  expirationDate,
+  contacts,
+  serviceName,
+}: SubscriptionProps) {
+  const { t } = useTranslation([
+    NAMESPACES.BILLING,
+    NAMESPACES.DASHBOARD,
+    NAMESPACES.CONTACT,
+    'domain',
+    'domain-reseller',
+  ]);
+  const formatDate = useFormatDate();
+  return (
+    <ManagerTile>
+      <ManagerTile.Title>{t('subscription')}</ManagerTile.Title>
+      <ManagerTile.Divider />
+      <CreationDate creationDate={creationDate} serviceName={serviceName} />
+      <ManagerTile.Divider />
+      <ManagerTile.Item>
+        <ManagerTile.Item.Label>
+          {t(
+            'domain:domain_tab_general_information_subscription_expiration_date',
+          )}
+        </ManagerTile.Item.Label>
+        <Text>{formatDate({ date: expirationDate })}</Text>
+      </ManagerTile.Item>
+      <ManagerTile.Divider />
+      <ManagerTile.Item>
+        <ManagerTile.Item.Label>
+          {t('domain-reseller:domain_reseller_renew_mode_label')}
+        </ManagerTile.Item.Label>
+        <Badge color={BADGE_COLOR.success} className="w-fit">
+          {t('domain-reseller:domain_reseller_renew_mode_automatic')}
+        </Badge>
+      </ManagerTile.Item>
+      <ManagerTile.Divider />
+      <Contacts contacts={contacts} serviceName={serviceName} />
+    </ManagerTile>
+  );
+}
