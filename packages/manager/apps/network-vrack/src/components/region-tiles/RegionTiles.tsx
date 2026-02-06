3b5e2059a1e71270c2763938c7aa533e02167384
aderghamov <alexandre.dergham.ext@ovhcloud.com>
2026-02-06 16:38:29 +0100

    feat(vrack): displaying a message when no region tile could be displayed
    ref: #MANAGER-20824
    
    Signed-off-by: aderghamov <alexandre.dergham.ext@ovhcloud.com>

--- packages/manager/apps/network-vrack/src/components/region-tiles/RegionTiles.tsx
+++ packages/manager/apps/network-vrack/src/components/region-tiles/RegionTiles.tsx
@@ -1,6 +1,9 @@
-import { SPINNER_SIZE, Spinner } from '@ovhcloud/ods-react';
+import { useTranslation } from 'react-i18next';
+
+import { SPINNER_SIZE, Spinner, Text } from '@ovhcloud/ods-react';
 
 import { useGetBandwidthByRegions } from '@/hooks/vrack-ip/useGetBandwidthByRegions';
+import { TRANSLATION_NAMESPACES } from '@/utils/constants';
 
 import { RegionTile } from '../region-tile/RegionTile';
 
@@ -10,13 +13,14 @@ interface RegionTilesProps {
 
 export const RegionTiles = (props: RegionTilesProps) => {
   const serviceName = props.serviceName;
+  const { t } = useTranslation([TRANSLATION_NAMESPACES.publicIpRouting]);
   const { regionsWithBandwidth, isLoading } = useGetBandwidthByRegions(serviceName);
 
   return isLoading ? (
     <div className="flex w-full justify-center">
       <Spinner size={SPINNER_SIZE.lg} />
     </div>
-  ) : (
+  ) : regionsWithBandwidth.length ? (
     <div className="flex w-full flex-wrap justify-start gap-6">
       {regionsWithBandwidth.map(({ region, bandwidthLimit, ipv4List, ipv6List }) => (
         <RegionTile
@@ -28,6 +32,10 @@ export const RegionTiles = (props: RegionTilesProps) => {
           ipv6List={ipv6List}
         />
       ))}
+    </div>
+  ) : (
+    <div className="mt-14 flex w-full flex-wrap">
+      <Text>{t('publicIpRouting_no_eligible_region_found')}</Text>
     </div>
   );
 };
