f42f05291cb4e84597962853b741d679e78ca1f2
Marc Monchablon <marc.monchablon.ext@corp.ovh.com>
2026-02-09 15:07:20 +0100

    feat(iam): add 'service-accounts' listing page
    ref: #MAIAM-81
    
    Signed-off-by: Marc Monchablon <marc.monchablon.ext@corp.ovh.com>

---
+++ packages/manager/apps/identity-access-management/src/pages/serviceAccounts/listing/Actions.component.tsx
@@ -0,0 +1,52 @@
+import { useTranslation } from 'react-i18next';
+import { useHref } from 'react-router-dom';
+import { ActionMenu, ActionMenuItem } from '@ovh-ux/manager-react-components';
+import { NAMESPACES } from '@ovh-ux/manager-common-translations';
+import { useOvhTracking } from '@ovh-ux/manager-react-shell-client';
+
+import { IamServiceAccount } from '@/data/api/iam-service-accounts';
+import { subRoutes } from '@/routes/routes.constant';
+import { SERVICE_ACCOUNTS_TRACKING } from '@/tracking.constant';
+
+export function Actions({ account }: { account: IamServiceAccount }) {
+  const { t } = useTranslation(NAMESPACES.ACTIONS);
+  const { trackClick } = useOvhTracking();
+
+  const editHref = useHref(
+    subRoutes.permanentTokensEdit.replace(':clientId', account.clientId),
+  );
+  const deleteHref = useHref(
+    subRoutes.permanentTokensDelete.replace(':clientId', account.clientId),
+  );
+  const items: ActionMenuItem[] = [
+    {
+      id: 0,
+      label: t('modify'),
+      href: editHref,
+      onClick: () => {
+        trackClick({
+          actionType: 'action',
+          actions: SERVICE_ACCOUNTS_TRACKING.LISTING.EDIT_ACCOUNT,
+        });
+      },
+    },
+    {
+      id: 1,
+      label: t('delete'),
+      href: deleteHref,
+      onClick: () => {
+        trackClick({
+          actionType: 'action',
+          actions: SERVICE_ACCOUNTS_TRACKING.LISTING.DELETE_ACCOUNT,
+        });
+      },
+    },
+  ];
+  return (
+    <ActionMenu
+      id={`manage-tokens-actions_${account.clientId}`}
+      items={items}
+      isCompact
+    />
+  );
+}
---
+++ packages/manager/apps/identity-access-management/src/pages/serviceAccounts/listing/ServiceAccountsListing.page.tsx
@@ -0,0 +1,73 @@
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import {
+  BaseLayout,
+  Datagrid,
+  ManagerButton,
+  HeadersProps,
+} from '@ovh-ux/manager-react-components';
+
+import { useDatagridColumn } from '@/pages/serviceAccounts/listing/useDatagridColumn';
+import { useIamServiceAccountList } from '@/data/hooks/useGetIamServiceAccounts';
+
+import { ServiceAccountsBreadcrumb } from '@/pages/serviceAccounts/components/ServiceAccountsBreadcrumb.component';
+import { ServiceAccountsTabs } from '@/pages/serviceAccounts/components/ServiceAccountsTabs.component';
+
+export default function ServiceAccountsListing() {
+  const { t } = useTranslation('service-accounts');
+
+  const header: HeadersProps = {
+    title: t('iam_identities'),
+  };
+
+  const columns = useDatagridColumn();
+  const {
+    flattenData,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    sorting,
+    filters,
+    setSorting,
+    search,
+  } = useIamServiceAccountList({ columns, pageSize: 10 });
+
+  const handleCreateAccount = () => {
+    // TODO
+  };
+
+  return (
+    <BaseLayout
+      header={header}
+      breadcrumb={<ServiceAccountsBreadcrumb />}
+      tabs={<ServiceAccountsTabs />}
+    >
+      <Datagrid
+        topbar={
+          <ManagerButton
+            id="create-service-account"
+            label={t('iam_service_accounts_add_account')}
+            onClick={handleCreateAccount}
+          />
+        }
+        isLoading={isLoading}
+        columns={columns}
+        items={flattenData || []}
+        totalItems={10}
+        onFetchNextPage={fetchNextPage}
+        hasNextPage={hasNextPage && !isLoading}
+        sorting={sorting}
+        onSortChange={setSorting}
+        filters={filters}
+        manualSorting={false}
+        search={search}
+        contentAlignLeft
+        getRowId={({ clientId }) => clientId}
+      />
+      <Suspense>
+        <Outlet />
+      </Suspense>
+    </BaseLayout>
+  );
+}
