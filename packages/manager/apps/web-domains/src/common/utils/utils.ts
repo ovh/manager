dff0600d9f220002b27d4fa4ca40bfa29afa5788
louisbensiovh <louis.bensi@corp.ovh.com>
2026-02-09 11:20:04 +0100

    feat(web-domains): dashboard header (#22038)
    ref: #DCE-149
    
    Signed-off-by: Louis BENSI <louis.bensi@corp.ovh.com>
    Co-authored-by: Louis BENSI <louis.bensi.ext@corp.ovh.com>

--- packages/manager/apps/web-domains/src/common/utils/utils.ts
+++ packages/manager/apps/web-domains/src/common/utils/utils.ts
@@ -1,11 +1,14 @@
 import { TServiceInfo } from '@/common/types/common.types';
 import { ServiceInfoContactEnum } from '@/common/enum/common.enum';
 
-
 export const findContact = (
   contacts: TServiceInfo['customer']['contacts'],
   contactType: ServiceInfoContactEnum,
 ) => {
   const contact = contacts.find((c) => c.type === contactType);
   return contact ? contact.customerCode : null;
+};
+
+export const handleOrderClick = (orderUrl: string) => {
+  window.open(orderUrl, '_blank', 'noopener,noreferrer');
 };
