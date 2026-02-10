674d377d89888cbe782f411b4056e29ceba21d8a
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2026-02-10 17:26:24 +0100

    refactor(okms): muk-link -> internal-link
    ref: #EK-2649
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/okms/src/modules/secret-manager/pages/secret/version-list/VersionCells.component.tsx
+++ packages/manager/apps/okms/src/modules/secret-manager/pages/secret/version-list/VersionCells.component.tsx
@@ -1,5 +1,3 @@
-import { useHref } from 'react-router-dom';
-
 import { VersionState } from '@secret-manager/components/version-state-badge/VersionStateBadge.component';
 import { SECRET_MANAGER_ROUTES_URLS } from '@secret-manager/routes/routes.constants';
 import { Secret, SecretVersion, SecretVersionState } from '@secret-manager/types/secret.type';
@@ -7,7 +5,7 @@ import { useTranslation } from 'react-i18next';
 
 import { Badge, Text } from '@ovhcloud/ods-react';
 
-import { MukLink } from '@/common/components/link/Link.component';
+import { InternalLink } from '@/common/components/link/Link.component';
 import { useFormatDate } from '@/common/hooks/useFormatDate';
 import { useRequiredParams } from '@/common/hooks/useRequiredParams';
 import { kmsIamActions } from '@/common/utils/iam/iam.constants';
@@ -23,16 +21,17 @@ const isVersionIdCellDisabled: Record<SecretVersionState, boolean> = {
 export const VersionIdCell = ({ version, secret }: { version: SecretVersion; secret: Secret }) => {
   const { t } = useTranslation('secret-manager');
   const { okmsId, secretPath } = useRequiredParams('okmsId', 'secretPath');
-  const href = useHref(
-    SECRET_MANAGER_ROUTES_URLS.versionListSecretValueDrawer(okmsId, secretPath, version.id),
+  const href = SECRET_MANAGER_ROUTES_URLS.versionListSecretValueDrawer(
+    okmsId,
+    secretPath,
+    version.id,
   );
-
   const isCurrentVersion = version.id === secret?.metadata?.currentVersion;
 
   return (
     <div className="flex items-center gap-2">
-      <MukLink
-        href={href}
+      <InternalLink
+        to={href}
         disabled={isVersionIdCellDisabled[version.state]}
         urn={secret?.iam?.urn}
         iamActions={[kmsIamActions.secretGet, kmsIamActions.secretVersionGetData]}
@@ -40,7 +39,7 @@ export const VersionIdCell = ({ version, secret }: { version: SecretVersion; sec
         data-testid={VERSION_LIST_CELL_TEST_IDS.version(version)}
       >
         {version.id.toString()}
-      </MukLink>
+      </InternalLink>
       {isCurrentVersion && (
         <Badge data-testid={VERSION_LIST_CELL_TEST_IDS.currentVersionBadge} color="information">
           {t('current_version')}
