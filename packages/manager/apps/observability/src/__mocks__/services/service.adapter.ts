d5a61636f27b10e15c6ef95e286e5805fbe1958a
David Arsène <david.arsene.ext@ovhcloud.com>
2026-02-10 17:46:46 +0100

    chore(observability): refactor logger for mocks
    ref: #MAOBS-133
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/observability/src/__mocks__/services/service.adapter.ts
+++ packages/manager/apps/observability/src/__mocks__/services/service.adapter.ts
@@ -1,4 +1,5 @@
 import { apiConfig } from '@/__mocks__/mock.config';
+import { mockLogger } from '@/__mocks__/mock.logger';
 import { getObservabilityServices as getObservabilityServicesFromMock } from '@/__mocks__/services/service.mock';
 import { getObservabilityServices as getObservabilityServicesFromApi } from '@/data/api/observability.api';
 import { ObservabilityService } from '@/types/observability.type';
@@ -7,7 +8,7 @@ export const getObservabilityServices = async (
   signal: AbortSignal,
 ): Promise<ObservabilityService[]> => {
   const isMockEnabled = apiConfig.service === 'mock';
-  console.info('[MOCK-ADAPTER][getObservabilityServices] Mock enabled -> ', isMockEnabled);
+  mockLogger.info('[getObservabilityServices] Mock enabled ->', isMockEnabled);
   return isMockEnabled
     ? getObservabilityServicesFromMock(signal)
     : getObservabilityServicesFromApi(signal);
