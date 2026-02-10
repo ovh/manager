31f918c7f6acf2822b774d8271412671543f4b0e
aTurmo <adrien.turmo.ext@ovhcloud.com>
2026-02-10 17:09:56 +0100

    feat(pci-file-storage): listing - change some status translation
    ref: #TAPC-5781
    
    Signed-off-by: aTurmo <adrien.turmo.ext@ovhcloud.com>

--- packages/manager/apps/pci-file-storage/src/pages/view-model/shareStatus.view-model.ts
+++ packages/manager/apps/pci-file-storage/src/pages/view-model/shareStatus.view-model.ts
@@ -8,17 +8,21 @@ export type TShareStatusDisplay = {
 };
 
 const STATUS_TO_DISPLAY = new Map<TShareStatus, TShareStatusDisplay>([
+  // with own translation
   ['available', { labelKey: 'status:active', badgeColor: 'success' }],
   ['backup_creating', { labelKey: 'status:backup_creating', badgeColor: 'warning' }],
-  ['backup_restoring', { labelKey: 'backup_restoring', badgeColor: 'warning' }],
-  ['backup_restoring_error', { labelKey: 'status:error', badgeColor: 'critical' }],
   ['creating', { labelKey: 'status:creating', badgeColor: 'warning' }],
-  ['creating_from_snapshot', { labelKey: 'creating_from_snapshot', badgeColor: 'warning' }],
+  ['creating_from_snapshot', { labelKey: 'status:creating', badgeColor: 'warning' }],
   ['deleting', { labelKey: 'status:deleting', badgeColor: 'warning' }],
-  ['error', { labelKey: 'status:error', badgeColor: 'critical' }],
-  ['error_deleting', { labelKey: 'status:error', badgeColor: 'critical' }],
   ['extending', { labelKey: 'status:extending', badgeColor: 'warning' }],
   ['inactive', { labelKey: 'status:inactive', badgeColor: 'critical' }],
+  // mapped to error
+  ['backup_restoring_error', { labelKey: 'status:error', badgeColor: 'critical' }],
+  ['error', { labelKey: 'status:error', badgeColor: 'critical' }],
+  ['error_deleting', { labelKey: 'status:error', badgeColor: 'critical' }],
+  ['extending_error', { labelKey: 'status:error', badgeColor: 'critical' }],
+  //not translated but with specific color
+  ['backup_restoring', { labelKey: 'backup_restoring', badgeColor: 'warning' }],
 ]);
 
 export const getShareStatusDisplay = (status: string): TShareStatusDisplay =>
