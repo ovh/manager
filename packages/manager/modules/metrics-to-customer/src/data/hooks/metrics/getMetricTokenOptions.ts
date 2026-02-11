ef9e4fdbd81349b1062259472a55182e21238e45
yjaaouane <yassine.jaaouane.ext@corp.ovh.com>
2026-02-11 09:34:49 +0100

    refactor(metrics-module): integrate token api and refactor mock
    ref: #MAOBS-233
    
    Signed-off-by: yjaaouane <yassine.jaaouane.ext@corp.ovh.com>

--- packages/manager/modules/metrics-to-customer/src/data/hooks/metrics/getMetricTokenOptions.ts
+++ packages/manager/modules/metrics-to-customer/src/data/hooks/metrics/getMetricTokenOptions.ts
@@ -1,6 +1,6 @@
 import { queryOptions } from '@tanstack/react-query';
 
-import { getMetricToken } from '@/__mocks__/metrics/metric.adapter';
+import { getMetricToken } from '@/data/api/metrics';
 import { getMetricTokenQueryKey  } from '@/data/hooks/metrics/getMetricTokenQueryKey';
 import { ObservabilityServiceParams } from '@/types/ClientApi.type';
 export const getMetricTokenOptions = (params: ObservabilityServiceParams) => {
