bd245a10d136bcf03419a8a76c536293d1adfecc
yjaaouane <yassine.jaaouane.ext@corp.ovh.com>
2026-02-11 11:04:33 +0100

    refactor(metrics-module): retrieve the resource region from IAM tags
    ref: #MAOBS-233
    
    Signed-off-by: yjaaouane <yassine.jaaouane.ext@corp.ovh.com>

--- packages/manager/modules/metrics-to-customer/src/contexts/MetricsToCustomer.type.tsx
+++ packages/manager/modules/metrics-to-customer/src/contexts/MetricsToCustomer.type.tsx
@@ -1,14 +1,33 @@
 import { SubscriptionUrls } from '@/types/SubscriptionUrls.type';
+import { Region } from '@/types/region.type';
 
-export interface MetricsToCustomerState {
+export type BaseM2CProperties = {
   resourceName: string;
   productType: string;
   resourceURN: string;
-  regions: string[];
+};
+
+export type WithSubscriptionManagement = {
+  enableConfigurationManagement: true;
   defaultRetention: string;
   subscriptionUrls: SubscriptionUrls;
-  enableConfigurationManagement: boolean;
-}
+};
+
+export type WithoutSubscriptionManagement = {
+  enableConfigurationManagement: false;
+  defaultRetention?: string;
+  subscriptionUrls?: SubscriptionUrls;
+};
+
+export type SubscriptionManagementConfig =
+  | WithSubscriptionManagement
+  | WithoutSubscriptionManagement;
+
+type BaseMetricsToCustomerState = BaseM2CProperties & {
+  regions: Region[];
+};
+
+export type MetricsToCustomerState = BaseMetricsToCustomerState & SubscriptionManagementConfig;
 
 export interface MetricsToCustomerContextType {
   state: MetricsToCustomerState;
---
+++ packages/manager/modules/metrics-to-customer/src/types/region.type.ts
@@ -0,0 +1,4 @@
+export type Region = {
+  code: string;
+  label: string;
+};
