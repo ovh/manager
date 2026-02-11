74a268eb8c07c6264558f5044c4c4315ff9011be
Thomas Esseul <thomas.esseul@corp.ovh.com>
2026-02-11 16:56:28 +0100

    feat(pci-rancher): add free trial notice on dashboard
    ref: #TAPC-4955
    
    Signed-off-by: Thomas Esseul <thomas.esseul@corp.ovh.com>

--- packages/manager/apps/pci-rancher/src/components/layout-helpers/Dashboard/Dashboard.component.tsx
+++ packages/manager/apps/pci-rancher/src/components/layout-helpers/Dashboard/Dashboard.component.tsx
@@ -1,7 +1,13 @@
 import { ODS_MESSAGE_TYPE, ODS_ICON_NAME } from '@ovhcloud/ods-components';
+import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
 import React from 'react';
 import { Outlet, useHref, useParams } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
+import {
+  OsdsMessage,
+  OsdsLink,
+  OsdsText,
+} from '@ovhcloud/ods-components/react';
 
 import { ChangelogButton, Title } from '@ovh-ux/manager-react-components';
 import { MutationStatus, useMutationState } from '@tanstack/react-query';
@@ -26,6 +32,42 @@ import {
   CHANGELOG_CHAPTERS,
 } from '@/utils/changelog.constants';
 import { PciGuidesHeader } from '@/components/guides-header';
+
+/* 
+On va maintenant ajouter une condition à l'affichage de cette bannière : 
+S'ils n'existent pas encore, crée un call api et un hook react query pour fetch (via apiV6)  /cloud/project/:projectId/credit/:creditId. Le hook prend en paramètre le creditId, celui-ci est typé via un union type string définit dans le fichier du hook, qui permet de restreindre strictement les valeurs possibles pour le paramètre du hook. Ce type sera de la forme type Tmytype = 'rancher-freetrial'
+Le type de retour du endpoint est {
+  "available_credit": {
+    "currencyCode": "AUD",
+    "priceInUcents": 0,
+    "text": "string",
+    "value": 0
+  },
+  "bill": "string",
+  "description": "string",
+  "id": 0,
+  "products": [
+    "string"
+  ],
+  "total_credit": {
+    "currencyCode": "AUD",
+    "priceInUcents": 0,
+    "text": "string",
+    "value": 0
+  },
+  "used_credit": {
+    "currencyCode": "AUD",
+    "priceInUcents": 0,
+    "text": "string",
+    "value": 0
+  },
+  "validity": {
+    "from": "2026-02-11T15:51:33.183Z",
+    "to": "2026-02-11T15:51:33.183Z"
+  },
+  "voucher": "string"
+}
+   */
 
 export type DashboardTabItemProps = {
   name: string;
@@ -52,6 +94,9 @@ const getResponseStatusByEditAction = (
   mutationState.length && mutationState[0].variables.editAction === editAction
     ? mutationState[0].status
     : null;
+
+const VOUCHER_URL = '#';
+
 const Dashboard: React.FC<DashboardLayoutProps> = ({ tabs, rancher }) => {
   const { projectId, rancherId } = useParams();
   const { data: versions } = useVersions();
@@ -136,6 +181,26 @@ const Dashboard: React.FC<DashboardLayoutProps> = ({ tabs, rancher }) => {
         className="my-4"
       />
       <TabBar tabs={tabs} />
+      <OsdsMessage
+        color={ODS_THEME_COLOR_INTENT.info}
+        type={ODS_MESSAGE_TYPE.info}
+        className="mt-6 max-w-5xl"
+      >
+        <OsdsText color={ODS_THEME_COLOR_INTENT.text} className="text-base">
+          <Trans
+            i18nKey="freeTrialVoucherBanner"
+            ns="dashboard"
+            components={{
+              l: (
+                <OsdsLink
+                  href={VOUCHER_URL}
+                  color={ODS_THEME_COLOR_INTENT.text}
+                />
+              ),
+            }}
+          />
+        </OsdsText>
+      </OsdsMessage>
       <RancherDetail
         rancher={rancher}
         versions={versions}
