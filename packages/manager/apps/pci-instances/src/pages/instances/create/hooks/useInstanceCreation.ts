230a1d9e4759d6e2c523383660fdfba9cb0c0083
Lauren Olivier <lauren.olivier@corp.ovh.com>
2026-02-12 15:08:29 +0100

    feat(pci-instances): add create instance tracking
    ref: #TAPC-5788
    
    Signed-off-by: Lauren Olivier <lauren.olivier@corp.ovh.com>

--- packages/manager/apps/pci-instances/src/pages/instances/create/hooks/useInstanceCreation.ts
+++ packages/manager/apps/pci-instances/src/pages/instances/create/hooks/useInstanceCreation.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines */
 import {
   selectFlavorDetailsForCreation,
   selectLocalisationDetailsForCreation,
@@ -32,6 +33,11 @@ import {
 } from '../view-models/networksViewModel';
 import { useNetworkCatalog } from '@/data/hooks/catalog/useNetworkCatalog';
 import { selectMicroRegionDeploymentMode } from '../view-models/microRegionsViewModel';
+import {
+  ButtonType,
+  PageLocation,
+  useOvhTracking,
+} from '@ovh-ux/manager-react-shell-client';
 
 type TBackupConfigurationPrices = {
   localBackupPrice: number;
@@ -108,6 +114,7 @@ export const getPublicNetworkCartItem = ({
 // eslint-disable-next-line max-lines-per-function
 export const useInstanceCreation = (): TInstanceCreation => {
   const navigate = useNavigate();
+  const { trackClick } = useOvhTracking();
   const projectId = useProjectId();
   const {
     control,
@@ -135,6 +142,8 @@ export const useInstanceCreation = (): TInstanceCreation => {
     ipPublicType,
     floatingIpAssignment,
     existingFloatingIpId,
+    flavorCategory,
+    distributionImageType,
   ] = useWatch({
     control,
     name: [
@@ -159,6 +168,8 @@ export const useInstanceCreation = (): TInstanceCreation => {
       'ipPublicType',
       'floatingIpAssignment',
       'existingFloatingIpId',
+      'flavorCategory',
+      'distributionImageType',
     ],
   });
   const { data: project } = useProject();
@@ -307,6 +318,11 @@ export const useInstanceCreation = (): TInstanceCreation => {
 
   const needsSshKey = distributionImageOsType !== 'windows';
 
+  const hasImageRequirements =
+    !!backup?.id ||
+    (!!distributionImageVersion.distributionImageVersionId &&
+      !!distributionImageVersion.distributionImageVersionName);
+
   const hasBaseRequirements =
     !!name &&
     !!quantity &&
@@ -314,7 +330,7 @@ export const useInstanceCreation = (): TInstanceCreation => {
     quantity <= quota &&
     !!microRegion &&
     !!flavorDetails?.id &&
-    !!distributionImageVersion.distributionImageVersionId;
+    hasImageRequirements;
 
   const hasSshRequirements = !needsSshKey || !!sshKeyId || !!newSshPublicKey;
 
@@ -329,6 +345,27 @@ export const useInstanceCreation = (): TInstanceCreation => {
 
   const handleCreateInstance = () => {
     if (!isCreationEnabled || isCreatingInstance) return;
+
+    trackClick({
+      location: PageLocation.funnel,
+      buttonType: ButtonType.button,
+      actionType: 'action',
+      actions: [
+        'add_instance',
+        'instances_created',
+        billingType,
+        microRegion,
+        ...(deploymentMode === 'region-3-az'
+          ? [availabilityZone ? 'manually' : 'automated']
+          : []),
+        flavorDetails.name,
+        ...(flavorCategory ? [flavorCategory] : []),
+        distributionImageType,
+        backup
+          ? 'backup'
+          : distributionImageVersion.distributionImageVersionName!,
+      ],
+    });
 
     const instance = mapFlavorToDTO({
       name,
