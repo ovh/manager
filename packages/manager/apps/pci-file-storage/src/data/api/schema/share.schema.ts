e083429af09c4d5f5b5bf0d2bf91a96475611147
Joakim Eriksson <75730728+ErikssonJoakim@users.noreply.github.com>
2026-02-12 11:57:13 +0100

    feat(pci-file-storage): make create share payload properties required
    Signed-off-by: Joakim Eriksson <75730728+ErikssonJoakim@users.noreply.github.com>

--- packages/manager/apps/pci-file-storage/src/data/api/schema/share.schema.ts
+++ packages/manager/apps/pci-file-storage/src/data/api/schema/share.schema.ts
@@ -1,9 +1,6 @@
 export type TCreateSharePayload = {
   type: string;
-  description?: string | null;
-  name?: string | null;
-  networkId?: string | undefined;
-  size?: number | undefined;
-  snapshotId?: string | null;
-  subnetId?: string | undefined;
+  name: string;
+  networkId: string;
+  size: number;
 };
