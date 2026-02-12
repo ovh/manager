b1259c1fef628f5d62603e5eaa2027aaf63303bc
David Arsène <david.arsene.ext@ovhcloud.com>
2026-02-12 17:44:22 +0100

    feat(observability): add grafana dashboard creation page
    resolves: #MAOBS-223
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/observability/src/data/hooks/grafana/useGrafana.hook.ts
+++ packages/manager/apps/observability/src/data/hooks/grafana/useGrafana.hook.ts
@@ -1,10 +1,10 @@
 import { UseQueryOptions, useQuery } from '@tanstack/react-query';
 
 import { getGrafanas } from '@/__mocks__/grafana/grafana.adapter';
-import { POLLING_INTERVAL } from '@/data/hooks/grafana/useGrafana.polling';
+import { POLLING_INTERVAL, isPollingStatus } from '@/data/hooks/polling';
 import { Grafana } from '@/types/managedDashboards.type';
 
-export const getGrafanasQueryKey = (resourceName: string) => ['Grafanas', resourceName];
+export const getGrafanasQueryKey = (resourceName: string) => ['grafanas', resourceName];
 
 export const useGrafanas = (
   resourceName: string,
@@ -14,7 +14,10 @@ export const useGrafanas = (
     queryKey: getGrafanasQueryKey(resourceName),
     queryFn: ({ signal }) => getGrafanas({ resourceName, signal }),
     enabled: !!resourceName,
-    refetchInterval: POLLING_INTERVAL,
+    refetchInterval: (query) =>
+      query.state.data?.some(({ resourceStatus }) => isPollingStatus(resourceStatus))
+        ? POLLING_INTERVAL
+        : false,
     ...queryOptions,
   });
 };
