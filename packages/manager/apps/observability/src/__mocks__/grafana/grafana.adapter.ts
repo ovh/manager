8135a5af2d5e7bd44f523a4904bb50621f0f5d94
David Arsène <david.arsene.ext@ovhcloud.com>
2026-02-12 17:44:22 +0100

    feat(observability): add grafana dashboard creation page
    resolves: #MAOBS-223
    
    Signed-off-by: David Arsène <david.arsene.ext@ovhcloud.com>

--- packages/manager/apps/observability/src/__mocks__/grafana/grafana.adapter.ts
+++ packages/manager/apps/observability/src/__mocks__/grafana/grafana.adapter.ts
@@ -1,11 +1,35 @@
-import { getGrafanas as getGrafanasFromMock } from '@/__mocks__/grafana/grafana.mock';
+import {
+  createGrafana as createGrafanaFromMock,
+  getGrafanaReleases as getGrafanaReleasesFromMock,
+  getGrafanas as getGrafanasFromMock,
+} from '@/__mocks__/grafana/grafana.mock';
 import { apiConfig } from '@/__mocks__/mock.config';
-import { getGrafanas as getGrafanasFromApi } from '@/data/api/grafana.api';
+import mockLogger from '@/__mocks__/mock.logger';
+import {
+  createGrafana as createGrafanaFromApi,
+  getGrafanaReleases as getGrafanaReleasesFromApi,
+  getGrafanas as getGrafanasFromApi,
+} from '@/data/api/grafana.api';
+import { CreateGrafanaPayload, GetGrafanaReleasesParams } from '@/data/api/grafana.props';
 import { ObservabilityServiceParams } from '@/data/api/observability.props';
-import { Grafana } from '@/types/managedDashboards.type';
+import { Grafana, GrafanaReleasesResponse } from '@/types/managedDashboards.type';
 
 export const getGrafanas = async (params: ObservabilityServiceParams): Promise<Grafana[]> => {
   const isMockEnabled = apiConfig.grafana === 'mock';
-  console.info('[MOCK-ADAPTER][getGrafanas] Mock enabled -> ', isMockEnabled);
+  mockLogger.info('[getGrafanas] Mock enabled -> ', isMockEnabled);
   return isMockEnabled ? getGrafanasFromMock(params) : getGrafanasFromApi(params);
+};
+
+export const getGrafanaReleases = async (
+  params: GetGrafanaReleasesParams,
+): Promise<GrafanaReleasesResponse> => {
+  const isMockEnabled = apiConfig.grafana === 'mock';
+  mockLogger.info('[getGrafanaReleases] Mock enabled -> ', isMockEnabled);
+  return isMockEnabled ? getGrafanaReleasesFromMock(params) : getGrafanaReleasesFromApi(params);
+};
+
+export const createGrafana = async (payload: CreateGrafanaPayload): Promise<Grafana> => {
+  const isMockEnabled = apiConfig.grafana === 'mock';
+  mockLogger.info('[createGrafana] Mock enabled -> ', isMockEnabled);
+  return isMockEnabled ? createGrafanaFromMock(payload) : createGrafanaFromApi(payload);
 };
--- packages/manager/apps/observability/src/data/api/grafana.api.ts
+++ packages/manager/apps/observability/src/data/api/grafana.api.ts
@@ -1,7 +1,8 @@
 import { apiClient } from '@ovh-ux/manager-core-api';
 
+import { CreateGrafanaPayload, GetGrafanaReleasesParams } from '@/data/api/grafana.props';
 import { ObservabilityServiceParams } from '@/data/api/observability.props';
-import { Grafana } from '@/types/managedDashboards.type';
+import { Grafana, GrafanaReleasesResponse } from '@/types/managedDashboards.type';
 
 export const getGrafanas = async ({
   resourceName,
@@ -12,6 +13,34 @@ export const getGrafanas = async ({
     {
       signal,
     },
+  );
+  return data;
+};
+
+export const getGrafanaReleases = async ({
+  resourceName,
+  infrastructureId,
+  signal,
+}: GetGrafanaReleasesParams): Promise<GrafanaReleasesResponse> => {
+  const { data } = await apiClient.v2.get<GrafanaReleasesResponse>(
+    `/observability/resource/${resourceName}/infrastructure/${infrastructureId}/grafana/release`,
+    // TODO: check URL when available
+    {
+      signal,
+    },
+  );
+  return data;
+};
+
+export const createGrafana = async ({
+  resourceName,
+  targetSpec,
+  signal,
+}: CreateGrafanaPayload): Promise<Grafana> => {
+  const { data } = await apiClient.v2.post<Grafana>(
+    `/observability/resource/${resourceName}/setting/grafana`,
+    { targetSpec },
+    { signal },
   );
   return data;
 };
