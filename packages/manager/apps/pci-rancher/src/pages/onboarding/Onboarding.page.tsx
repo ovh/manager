2a9ef94cb052a2af215a634b8c99db654d643c1c
Thomas Esseul <thomas.esseul@corp.ovh.com>
2026-02-13 14:57:50 +0100

    feat(pci-rancher): rework onboarding page for free trial  (#22237)
    ref: #TAPC-4947
    
    Signed-off-by: Thomas Esseul <thomas.esseul@corp.ovh.com>

--- packages/manager/apps/pci-rancher/src/pages/onboarding/Onboarding.page.tsx
+++ packages/manager/apps/pci-rancher/src/pages/onboarding/Onboarding.page.tsx
@@ -1,113 +1,26 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
-import {
-  Card,
-  OnboardingLayout,
-  PageLayout,
-} from '@ovh-ux/manager-react-components';
-import { useNavigate, useParams } from 'react-router-dom';
-import { OsdsText } from '@ovhcloud/ods-components/react';
-import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
-import onboardingImgSrc from '@/assets/onboarding-img.png';
-import Breadcrumb from '@/components/Breadcrumb/Breadcrumb.component';
-import { getCreateRancherUrl } from '@/utils/route';
-import {
-  useTrackingAction,
-  useTrackingPage,
-} from '@/hooks/useTrackingPage/useTrackingPage';
-import { TrackingEvent, TrackingPageView } from '@/utils/tracking';
-import { useGuideUtils } from '@/hooks/useGuideLink/useGuideLink';
+import { PageLayout } from '@ovh-ux/manager-react-components';
 import useRancherEligibility from '@/data/hooks/useRancherEligibility/useRancherEligibility';
+import { useTrackingPage } from '@/hooks/useTrackingPage/useTrackingPage';
+import { TrackingPageView } from '@/utils/tracking';
+import Breadcrumb from '@/components/Breadcrumb/Breadcrumb.component';
+import { OnboardingFreeTrialContent } from './components/OnboardingFreeTrialContent.component';
+import { OnboardingStandardContent } from './components/OnboardingStandardContent.component';
 
 export default function Onboarding() {
-  const { t } = useTranslation('onboarding');
-  const link = useGuideUtils();
-  const navigate = useNavigate();
-  const { projectId } = useParams();
   const { data: eligibility } = useRancherEligibility();
-  const title: string = t('title');
-  const descriptionText: string = t('description');
   useTrackingPage(TrackingPageView.Onboarding);
-  const trackAction = useTrackingAction();
-  const onOrderButtonClick = () => {
-    trackAction(TrackingPageView.Onboarding, TrackingEvent.add);
-    navigate(getCreateRancherUrl(projectId));
-  };
 
-  const tileData = [
-    {
-      id: 1,
-      titleKey: 'managedRancherServiceGettingStartedTitle',
-      descriptionKey: 'managedRancherServiceGettingStartedTitleDescription',
-      hrefKey: 'MANAGED_RANCHER_SERVICE_GETTING_STARTED',
-    },
-    {
-      id: 2,
-      titleKey: 'managedRancherServiceGettingStartedTitle2',
-      descriptionKey: 'managedRancherServiceGettingStartedTitleDescription2',
-      hrefKey: 'MANAGED_RANCHER_SERVICE_CREATION',
-    },
-    {
-      id: 3,
-      titleKey: 'managedRancherServiceGettingStartedTitle3',
-      descriptionKey: 'managedRancherServiceGettingStartedTitleDescription3',
-      hrefKey: 'MANAGED_RANCHER_SERVICE_LIFECYCLE_POLICY',
-    },
-  ];
-
-  const tileList = tileData.map((item) => ({
-    ...item,
-    texts: {
-      title: t(item.titleKey),
-      description: t(item.descriptionKey),
-      category: t('guideCategory'),
-    },
-    href: link?.[item.hrefKey] as string,
-    isExternalHref: true,
-    hoverable: true,
-  }));
-
-  const description = (
-    <>
-      <OsdsText color={ODS_THEME_COLOR_INTENT.text}>{descriptionText}</OsdsText>
-      {eligibility?.data?.freeTrial && (
-        <div className="mt-4 flex flex-col">
-          <OsdsText color={ODS_THEME_COLOR_INTENT.text} className="mb-2">
-            {t('freeTrialEligibilityTitle')}
-          </OsdsText>
-          <OsdsText color={ODS_THEME_COLOR_INTENT.text} className="mb-2">
-            {t('freeTrialCreditStandard')}
-          </OsdsText>
-          <OsdsText color={ODS_THEME_COLOR_INTENT.text} className="mb-2">
-            {t('freeTrialCreditOvhEdition')}
-          </OsdsText>
-          <OsdsText color={ODS_THEME_COLOR_INTENT.text}>
-            {t('freeTrialCreditApplied')}
-          </OsdsText>
-        </div>
-      )}
-    </>
-  );
+  const isFreeTrialAvailable = eligibility?.data?.freeTrial;
 
   return (
     <PageLayout>
       <Breadcrumb />
-      <OnboardingLayout
-        title={title}
-        img={{ src: onboardingImgSrc }}
-        description={description}
-        orderButtonLabel={t('orderButtonLabel')}
-        onOrderButtonClick={onOrderButtonClick}
-      >
-        {tileList.map((tile) => (
-          <Card
-            data-testid="tileCard"
-            key={tile.id}
-            href={tile.href}
-            texts={tile.texts}
-          />
-        ))}
-      </OnboardingLayout>
+      {isFreeTrialAvailable ? (
+        <OnboardingFreeTrialContent />
+      ) : (
+        <OnboardingStandardContent />
+      )}
     </PageLayout>
   );
 }
