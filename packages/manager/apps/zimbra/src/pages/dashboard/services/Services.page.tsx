db93e3f3f31d705438068f554068ed850488ce19
amine-daoussi <mohamed-amine.daoussi.ext@corp.ovh.com>
2026-02-13 16:41:54 +0100

    feat(zimbra): service consumption calculation
    ref: #PRDCOL-342
    
    Signed-off-by: amine-daoussi <mohamed-amine.daoussi.ext@corp.ovh.com>

--- packages/manager/apps/zimbra/src/pages/dashboard/services/Services.page.tsx
+++ packages/manager/apps/zimbra/src/pages/dashboard/services/Services.page.tsx
@@ -1,206 +1,86 @@
-import React, { useCallback, useContext, useEffect, useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 
-import { RowSelectionState } from '@tanstack/react-table';
+import { Outlet } from 'react-router-dom';
+
 import { useTranslation } from 'react-i18next';
-import { Outlet, useLocation, useNavigate } from 'react-router';
 
-import { Skeleton } from '@ovhcloud/ods-react';
+import { ProgressBar } from '@ovhcloud/ods-react';
 
-import { ShellContext } from '@ovh-ux/manager-react-shell-client';
-import {
-  Clipboard,
-  Datagrid,
-  DatagridColumn,
-  OvhSubsidiary,
-  Price,
-  useFormatDate,
-} from '@ovh-ux/muk';
+import { Text } from '@ovh-ux/muk';
 
-import { BillingStateBadge, LabelChip } from '@/components';
-import { ResourceStatus, SlotService } from '@/data/api';
-import { SlotWithService, useAccounts, useSlotsWithService } from '@/data/hooks';
-import { useDebouncedValue } from '@/hooks';
-import { DATAGRID_REFRESH_INTERVAL, DATAGRID_REFRESH_ON_MOUNT } from '@/utils';
-import { getPriceUnit } from '@/utils/price';
+import { AccountStatistics, ZimbraOffer } from '@/data/api';
+import { usePlatform } from '@/data/hooks';
+import { useAccountsStatistics, useOverridePage } from '@/hooks';
+import { capitalize } from '@/utils';
+import { IAM_ACTIONS } from '@/utils/iamAction.constants';
 
-import ActionButtonService from './ActionButton.component';
-import DatagridTopbar from './DatagridTopBar.component';
+import ServicesDatagrid from './ServicesDatagrid.component';
 
-const Services = () => {
-  const { t } = useTranslation(['services', 'common', 'accounts']);
-  const format = useFormatDate();
-  const { environment } = useContext(ShellContext);
-  const location = useLocation();
-  const navigate = useNavigate();
-  const locale = environment.getUserLocale();
-  const { ovhSubsidiary } = environment.getUser();
-  const [rowSelection, setRowSelection] = useState<RowSelectionState>({});
-  const [selectedRows, setSelectedRows] = useState<SlotWithService[]>([]);
-
-  const { email: emailFromState } = location.state ?? {};
-
-  const [searchInput, setSearchInput, debouncedSearchInput, setDebouncedSearchInput] =
-    useDebouncedValue('');
-
-  useEffect(() => {
-    if (emailFromState) {
-      setSearchInput(emailFromState);
-      setDebouncedSearchInput(emailFromState);
-      navigate(location.pathname, { replace: true });
-    }
-  }, [emailFromState, setSearchInput, setDebouncedSearchInput, navigate, location.pathname]);
-
-  const columns: DatagridColumn<SlotWithService>[] = useMemo(
-    () => [
+export const Services = () => {
+  const { t } = useTranslation(['accounts', 'common']);
+  const isOverridedPage = useOverridePage();
+  const { platformUrn } = usePlatform();
+  const { accountsStatistics } = useAccountsStatistics();
+  const enrichedAccountsStatistics: AccountStatistics[] = useMemo(() => {
+    return [
+      ...(accountsStatistics ?? []),
       {
-        id: 'id',
-        accessorKey: 'id',
-        label: 'zimbra_services_service_reference',
-        disabled: true,
-        cell: ({ getValue }) => <Clipboard value={getValue<string>()} />,
+        offer: ZimbraOffer.BUSINESS,
+        availableAccountsCount: 0,
+        configuredAccountsCount: 0,
       },
-      {
-        id: 'email',
-        accessorKey: 'email',
-        label: 'common:email_account',
-        isSearchable: true,
-      },
-      {
-        id: 'organization',
-        accessorKey: 'organizationLabel',
-        label: 'common:organization',
-        cell: ({ row }) => {
-          const { organizationId, organizationLabel } = row.original;
-          return organizationId ? (
-            <LabelChip id={organizationId}>{organizationLabel}</LabelChip>
-          ) : (
-            '-'
-          );
-        },
-      },
-      {
-        id: 'offer',
-        accessorKey: 'offer',
-        label: 'common:offer',
-      },
-      {
-        id: 'cost',
-        accessorKey: 'cost',
-        label: 'zimbra_services_cost',
-        cell: ({ row }) => {
-          const service = row.original.service;
-
-          if (!service) {
-            return <Skeleton className="[&::part(skeleton)]:max-w-20" />;
-          }
-
-          const { priceInUcents, duration } = service;
-
-          return priceInUcents === 0 ? (
-            t('services:zimbra_services_free')
-          ) : (
-            <Price
-              value={priceInUcents}
-              locale={locale}
-              ovhSubsidiary={ovhSubsidiary as OvhSubsidiary}
-              intervalUnit={getPriceUnit(duration)}
-            />
-          );
-        },
-      },
-      {
-        id: 'renewal_type',
-        accessorKey: 'service',
-        label: 'accounts:zimbra_account_datagrid_renewal_type',
-        cell: ({ getValue }) => <BillingStateBadge service={getValue<SlotService>()} />,
-      },
-      {
-        id: 'renewal_date',
-        accessorKey: 'service',
-        label: 'zimbra_services_renew_date',
-        cell: ({ row }) =>
-          !row.original.service ? (
-            <Skeleton className="[&::part(skeleton)]:max-w-20" />
-          ) : (
-            <span>
-              {format({
-                date: row.original.service?.nextBillingDate,
-                format: 'P',
-              })}
-            </span>
-          ),
-      },
-      {
-        id: 'tooltip',
-        maxSize: 50,
-        accessorFn: (row) => row.id,
-        cell: ({ row }) => <ActionButtonService item={row.original} />,
-      },
-    ],
-    [format, t, locale, ovhSubsidiary],
-  );
-  const { data: accounts } = useAccounts({ shouldFetchAll: true });
-  const { slots, fetchAllPages, fetchNextPage, hasNextPage, isLoadingSlots, isFetchingNextPage } =
-    useSlotsWithService({
-      email: debouncedSearchInput,
-      refetchInterval: DATAGRID_REFRESH_INTERVAL,
-      refetchOnMount: DATAGRID_REFRESH_ON_MOUNT,
-      shouldFetchAll: !!emailFromState,
-    });
-
-  const data = useMemo(() => {
-    return slots.map((slot) => {
-      const account = accounts?.find((acc) => acc.id === slot.accountId);
-
-      return {
-        id: slot.id,
-        email: slot.email ?? '-',
-        organizationLabel: account?.currentState.organizationLabel,
-        organizationId: account?.currentState.organizationId,
-        offer: slot.offer,
-        service: slot.service,
-        cost: slot.service?.price,
-        status: account?.resourceStatus,
-        accountId: account?.id,
-      };
-    });
-  }, [slots, accounts]);
-
-  useEffect(() => {
-    setSelectedRows(data?.filter((item) => rowSelection[item.id]));
-  }, [data, rowSelection]);
-
-  const isRowSelectable = useCallback(
-    (item: SlotWithService) => item.status === ResourceStatus.READY,
-    [],
-  );
+    ];
+  }, [accountsStatistics]);
 
   return (
     <div>
       <Outlet />
-      <Datagrid
-        topbar={<DatagridTopbar selectedRows={selectedRows} />}
-        search={{
-          searchInput,
-          setSearchInput,
-          onSearch: (search) => setDebouncedSearchInput(search),
-        }}
-        rowSelection={{
-          rowSelection,
-          setRowSelection,
-          enableRowSelection: ({ original: item }) => isRowSelectable(item),
-        }}
-        columns={columns.map((column) => ({
-          ...column,
-          header: t(column.label),
-        }))}
-        data={data}
-        totalCount={data.length}
-        hasNextPage={hasNextPage}
-        onFetchNextPage={fetchNextPage}
-        onFetchAllPages={fetchAllPages}
-        isLoading={isLoadingSlots || isFetchingNextPage}
-      />
+      {!isOverridedPage && (
+        <>
+          <div className="mb-7 flex w-full gap-8">
+            {enrichedAccountsStatistics.map((stats: AccountStatistics) => {
+              const used = stats.configuredAccountsCount;
+              const total = stats.configuredAccountsCount + stats.availableAccountsCount;
+              const isBusiness = stats.offer === ZimbraOffer.BUSINESS;
+
+              return (
+                <div
+                  key={stats.offer}
+                  className={`flex-1 flex-col gap-4 rounded-md border border-solid border-gray-400 p-6  ${
+                    isBusiness ? 'border-gray-200 bg-gray-100' : 'border-gray-300 bg-white'
+                  }`}
+                >
+                  <div className="flex items-center justify-between">
+                    <Text
+                      preset="caption"
+                      className={isBusiness ? 'text-gray-400' : ''}
+                      urn={platformUrn}
+                      iamActions={[IAM_ACTIONS.account.get]}
+                    >
+                      {capitalize(stats.offer.toLowerCase())}
+                    </Text>
+
+                    {isBusiness && (
+                      <span className="rounded bg-gray-200 px-2 py-1 text-xs text-gray-500">
+                        {t('zimbra_account_plan_tag_coming_soon')}
+                      </span>
+                    )}
+                  </div>
+                  <ProgressBar max={total} value={used} />
+                  <Text
+                    preset="caption"
+                    className={`whitespace-nowrap ${isBusiness ? 'text-gray-400' : ''}`}
+                    iamActions={[IAM_ACTIONS.account.get]}
+                  >
+                    {`${used} / ${total}`}
+                  </Text>
+                </div>
+              );
+            })}
+          </div>
+          <ServicesDatagrid />
+        </>
+      )}
     </div>
   );
 };
