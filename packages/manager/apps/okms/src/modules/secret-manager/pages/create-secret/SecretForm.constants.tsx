93b68d5b1ff297f0c33796acc0793d10fbb56f4e
Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>
2026-02-16 10:44:56 +0100

    feat(okms): add custom metadata to the secret form
    ref: #EK-2539
    
    Signed-off-by: Mathieu Mousnier <mathieu.mousnier.ext@ovhcloud.com>

--- packages/manager/apps/okms/src/modules/secret-manager/pages/create-secret/SecretForm.constants.tsx
+++ packages/manager/apps/okms/src/modules/secret-manager/pages/create-secret/SecretForm.constants.tsx
@@ -1,6 +1,8 @@
 export const SECRET_FORM_TEST_IDS = {
   INPUT_PATH: 'secret-input-path',
   SUBMIT_BUTTON: 'secret-submit-button',
+  CUSTOM_METADATA_SECTION: 'secret-custom-metadata-section',
+  TOGGLE_CUSTOM_METADATA: 'secret-toggle-custom-metadata',
   TOGGLE_DEACTIVATE_VERSION_AFTER: 'secret-toggle-deactivate-version-after',
   TOGGLE_MAX_VERSIONS: 'secret-toggle-max-versions',
   TOGGLE_CAS_REQUIRED: 'secret-toggle-cas-required',
