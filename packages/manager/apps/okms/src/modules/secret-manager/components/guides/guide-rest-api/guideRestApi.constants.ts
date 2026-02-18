af7d8addcdc77f6029393a36f677a19bece71da6
Romain Jamet <romain.jamet@corp.ovh.com>
2026-02-18 11:15:46 +0100

    feat(okms): add secret manager US guide links
    ref: #MANAGER-20827
    
    Signed-off-by: Romain Jamet <romain.jamet@corp.ovh.com>

--- packages/manager/apps/okms/src/modules/secret-manager/components/guides/guide-rest-api/guideRestApi.constants.ts
+++ packages/manager/apps/okms/src/modules/secret-manager/components/guides/guide-rest-api/guideRestApi.constants.ts
@@ -9,4 +9,5 @@ export const GUIDES_REST_API: GuideLinks = {
   IT: 'https://help.ovhcloud.com/csm/it-secret-manager-rest-api?id=kb_article_view&sysparm_article=KB0072859',
   PL: 'https://help.ovhcloud.com/csm/pl-secret-manager-rest-api?id=kb_article_view&sysparm_article=KB0072852',
   PT: 'https://help.ovhcloud.com/csm/pt-secret-manager-rest-api?id=kb_article_view&sysparm_article=KB0072851',
+  US: 'https://support.us.ovhcloud.com/hc/en-us/articles/47763553148947-Using-the-Secret-Manager-with-the-REST-API',
 };
