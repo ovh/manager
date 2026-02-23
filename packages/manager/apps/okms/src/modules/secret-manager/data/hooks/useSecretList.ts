89d8950d4f38d3ed08d16135d9d4a8333361b511
Romain Jamet <romain.jamet@corp.ovh.com>
2026-02-23 15:53:12 +0100

    fix(okms): disabled cache on iceberg request
    ref: #EK-2726
    
    Signed-off-by: Romain Jamet <romain.jamet@corp.ovh.com>

--- packages/manager/apps/okms/src/modules/secret-manager/data/hooks/useSecretList.ts
+++ packages/manager/apps/okms/src/modules/secret-manager/data/hooks/useSecretList.ts
@@ -7,6 +7,7 @@ import { secretQueryKeys } from '@/modules/secret-manager/data/api/secrets';
 export const useSecretList = ({ okmsId, pageSize = 25 }: { okmsId: string; pageSize?: number }) => {
   return useDataApi<Secret>({
     iceberg: true,
+    disableCache: true,
     enabled: true,
     version: 'v2',
     route: `okms/resource/${okmsId}/secret`,
