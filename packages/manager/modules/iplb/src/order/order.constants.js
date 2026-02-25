b50058a5a10f806f5f68d974b1a6e45e6ed58d98
Stephanie Moallic <stephanie.moallic@corp.ovh.com>
2026-02-25 11:46:58 +0100

    feat(iplb): update url to access configo federated modules
    ref: #MANAGER-20884
    
    Signed-off-by: Stephanie Moallic <stephanie.moallic@corp.ovh.com>

--- packages/manager/modules/iplb/src/order/order.constants.js
+++ packages/manager/modules/iplb/src/order/order.constants.js
@@ -1,5 +1,13 @@
 export const PREFIX_TRACKING_ORDER = 'order';
 
+// Define remote entry URLs for each region
+export const REMOTE_ENTRY_URLS = {
+  EU: 'https://manager.eu.ovhcloud.com/order/builder/assets/remoteEntry.js',
+  US: 'https://manager.us.ovhcloud.com/order/builder/assets/remoteEntry.js',
+  CA: 'https://manager.ca.ovhcloud.com/order/builder/assets/remoteEntry.js',
+};
+
 export default {
   PREFIX_TRACKING_ORDER,
+  REMOTE_ENTRY_URLS,
 };
--- packages/manager/modules/iplb/src/order/order.routing.js
+++ packages/manager/modules/iplb/src/order/order.routing.js
@@ -1,4 +1,16 @@
 import { init, loadRemote } from '@module-federation/runtime';
+import { REMOTE_ENTRY_URLS } from './order.constants';
+
+const getOrderUrl = (hostname) => {
+  if (hostname.includes('.us.') || hostname.includes('-us.')) {
+    return REMOTE_ENTRY_URLS.US;
+  }
+  if (hostname.includes('.ca.') || hostname.includes('-ca.')) {
+    return REMOTE_ENTRY_URLS.CA;
+  }
+  // Default to EU for localhost and EU environments
+  return REMOTE_ENTRY_URLS.EU;
+};
 
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('iplb.order', {
@@ -12,19 +24,20 @@ export default /* @ngInject */ ($stateProvider) => {
       hideBreadcrumb: () => true,
       ipLbPublicUrl: /* @ngInject */ ($injector) =>
         $injector.get('shellClient').navigation.getURL('dedicated', `#/iplb`),
-      setupIpLb: /* @ngInject */ () => {
+      setupIpLb: /* @ngInject */ ($window) => {
+        const { hostname } = $window.location;
+
         init({
           remotes: [
             {
-              name: '@order/ConfigoIplbPack',
+              name: 'iplb_pack',
               alias: 'order_fm',
               type: 'module',
-              entry:
-                'https://www.ovhcloud.com/order/configos/assets/remoteEntry.js',
+              entry: getOrderUrl(hostname),
             },
           ],
         });
-        return loadRemote('order_fm/ConfigoIplbPack');
+        return loadRemote('order_fm/iplb_pack');
       },
     },
   });
