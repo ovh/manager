253b58a061faa999f0c260677f1092228c1f5955
Imane El Aiboud <imane.el-aiboud@corp.ovh.com>
2026-02-25 10:36:21 +0100

    feat(web-hosting): delete navigate button in logs page
    Ref: #PUWEBPT-144
    
    Signed-off-by: Imane El Aiboud <imane.el-aiboud@corp.ovh.com>

--- packages/manager/apps/web-hosting/src/pages/dashboard/logs/Logs.page.tsx
+++ packages/manager/apps/web-hosting/src/pages/dashboard/logs/Logs.page.tsx
@@ -1,11 +1,8 @@
 import React, { useEffect } from 'react';
 
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 import { useQueryClient } from '@tanstack/react-query';
-import { useTranslation } from 'react-i18next';
-
-import { BUTTON_SIZE, BUTTON_VARIANT, Button, ICON_NAME, Icon } from '@ovhcloud/ods-react';
 
 import { LogsToCustomerModule } from '@ovh-ux/logs-to-customer';
 
@@ -16,9 +13,7 @@ import { setupLogsOperationInterceptor } from '@/utils/logsOperationInterceptor'
 export default function LogsPage() {
   const { serviceName } = useParams<{ serviceName: string }>();
   const { data: hosting, isPending } = useGetHostingService(serviceName || '');
-  const navigate = useNavigate();
   const queryClient = useQueryClient();
-  const { t } = useTranslation('dashboard');
 
   useEffect(() => {
     if (!serviceName) {
@@ -39,18 +34,6 @@ export default function LogsPage() {
 
   return (
     <div className="flex flex-col gap-4">
-      <div className="flex justify-start">
-        <Button
-          size={BUTTON_SIZE.sm}
-          variant={BUTTON_VARIANT.ghost}
-          onClick={() => {
-            navigate(-1);
-          }}
-        >
-          <Icon name={ICON_NAME.arrowLeft} className="mr-2" />
-          {t('hosting_logs_back_to_previous_page')}
-        </Button>
-      </div>
       <LogsToCustomerModule
         logApiVersion="v6"
         logApiUrls={{
