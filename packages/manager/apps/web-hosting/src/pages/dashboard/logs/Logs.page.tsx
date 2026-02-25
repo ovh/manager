e09cd0169baa77a1098ae12aee794eb70f4d1236
Imane El Aiboud <imane.el-aiboud@corp.ovh.com>
2026-02-25 11:11:11 +0100

    feat(web-hosting): delete navigate button
    Ref: #PUWEBPT-144
    
    Signed-off-by: Imane El Aiboud <imane.el-aiboud@corp.ovh.com>

--- packages/manager/apps/web-hosting/src/pages/dashboard/logs/Logs.page.tsx
+++ packages/manager/apps/web-hosting/src/pages/dashboard/logs/Logs.page.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
-import { useNavigate, useParams } from 'react-router-dom';
-
-import { useTranslation } from 'react-i18next';
-
-import { BUTTON_SIZE, BUTTON_VARIANT, Button, ICON_NAME, Icon } from '@ovhcloud/ods-react';
+import { useParams } from 'react-router-dom';
 
 import { LogsToCustomerModule } from '@ovh-ux/logs-to-customer';
 
@@ -14,8 +10,6 @@ import { useGetHostingService } from '@/data/hooks/webHostingDashboard/useWebHos
 export default function LogsPage() {
   const { serviceName } = useParams<{ serviceName: string }>();
   const { data: hosting, isPending } = useGetHostingService(serviceName || '');
-  const navigate = useNavigate();
-  const { t } = useTranslation('dashboard');
 
   if (isPending) {
     return <Loading />;
@@ -27,18 +21,6 @@ export default function LogsPage() {
 
   return (
     <div className="flex flex-col gap-4">
-      <div className="flex justify-start">
-        <Button
-          size={BUTTON_SIZE.sm}
-          variant={BUTTON_VARIANT.ghost}
-          onClick={() => {
-            navigate(-1);
-          }}
-        >
-          <Icon name={ICON_NAME.arrowLeft} className="mr-2" />
-          {t('hosting_logs_back_to_previous_page')}
-        </Button>
-      </div>
       <LogsToCustomerModule
         logApiVersion="v6"
         logApiUrls={{
