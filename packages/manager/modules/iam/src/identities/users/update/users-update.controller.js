76697936e7e12be15f280bf3da2769245048abea
Marc Monchablon <marc.monchablon.ext@corp.ovh.com>
2026-02-27 12:36:33 +0100

    feat(iam): add multiple group selection in user create/edit forms
    ref: #MAIAM-88
    
    Signed-off-by: Marc Monchablon <marc.monchablon.ext@corp.ovh.com>
    Signed-off-by: CDS Translator Agent <opensource@ovh.net>

--- packages/manager/modules/iam/src/identities/users/update/users-update.controller.js
+++ packages/manager/modules/iam/src/identities/users/update/users-update.controller.js
@@ -17,6 +17,7 @@ export default class IamUsersUpdateCtrl {
     this.$translate = $translate;
     this.me = coreConfig.getUser();
     this.user = angular.copy($scope.currentActionData);
+    this.userGroups = this.user.groups || [];
     this.loader = true;
     this.DESCRIPTION_MAX_LENGTH = 255;
   }
@@ -58,6 +59,7 @@ export default class IamUsersUpdateCtrl {
           }),
           'iam-users-alert',
         );
+        this.updateGroups(this.user, this.userGroups);
       })
       .catch((err) => {
         this.$scope.trackPage(USERS_TRACKING_HITS.UPDATE_USER_ERROR);
@@ -73,7 +75,7 @@ export default class IamUsersUpdateCtrl {
         }
         return this.alerter.error(
           `${this.$translate.instant('user_users_update_error_message')} ${
-            err.data.message
+            err?.data?.message
           }`,
           'iam-users-alert',
         );
@@ -82,6 +84,69 @@ export default class IamUsersUpdateCtrl {
         this.loader = false;
         this.$scope.resetAction();
       });
+  }
+
+  updateGroups(user, newGroupList) {
+    const currentUserGroups = user.groups || [];
+    const groupsToAdd = newGroupList
+      .filter((group) => !currentUserGroups.includes(group))
+      .filter((group) => group !== user.group);
+    const groupsToRemove = (user.groups || [])
+      .filter((group) => !newGroupList.includes(group))
+      .filter((group) => group !== user.group);
+
+    return Promise.allSettled([
+      ...groupsToAdd.map((group) => this.addUserToGroup(user, group)),
+      ...groupsToRemove.map((group) => this.removeUserFromGroup(user, group)),
+    ]);
+  }
+
+  addUserToGroup(user, group) {
+    return this.usersService.addUserToGroup(user, group).catch((err) => {
+      const errorMessage = this.$translate.instant(
+        'user_group_add_error_message',
+        {
+          login: user.login,
+          group: group,
+        },
+      );
+      let errorContent = '';
+      if (err?.status === 403) {
+        errorContent = `${this.$translate.instant(
+          'user_need_rights_message',
+        )} ${err?.data?.details?.unauthorizedActionsByIAM}`;
+      } else {
+        errorContent = `${err?.data?.message}`;
+      }
+      return this.alerter.warning(
+        `${errorMessage} ${errorContent}`,
+        'iam-users-alert',
+      );
+    });
+  }
+
+  removeUserFromGroup(user, group) {
+    return this.usersService.removeUserFromGroup(user, group).catch((err) => {
+      const errorMessage = this.$translate.instant(
+        'user_group_remove_error_message',
+        {
+          login: user.login,
+          group: group,
+        },
+      );
+      let errorContent = '';
+      if (err?.status === 403) {
+        errorContent = `${this.$translate.instant(
+          'user_need_rights_message',
+        )} ${err?.data?.details?.unauthorizedActionsByIAM}`;
+      } else {
+        errorContent = `${err?.data?.message}`;
+      }
+      return this.alerter.warning(
+        `${errorMessage} ${errorContent}`,
+        'iam-users-alert',
+      );
+    });
   }
 
   close() {
