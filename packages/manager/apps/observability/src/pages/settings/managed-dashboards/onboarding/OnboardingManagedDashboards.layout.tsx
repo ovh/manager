3b32656d166df85bb593d4fabbcf42914c4b6988
yjaaouane <yassine.jaaouane.ext@corp.ovh.com>
2026-03-02 11:37:46 +0100

    fixup! feat(observability): add onboarding page for managed dashboards

--- packages/manager/apps/observability/src/pages/settings/managed-dashboards/onboarding/OnboardingManagedDashboards.layout.tsx
+++ packages/manager/apps/observability/src/pages/settings/managed-dashboards/onboarding/OnboardingManagedDashboards.layout.tsx
@@ -1,13 +1,19 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 import { useTranslation } from 'react-i18next';
 
+import ServicesDropDown from '@/components/services/dropdown/ServicesDropDown.component';
 import MetricsBaseLayout from '@/pages/metrics/MetricsBase.layout';
+import { urls } from '@/routes/Routes.constants';
 
 export default function OnboardingManagedDashboardsLayout() {
   const { t } = useTranslation('managed-dashboards');
+
+  const navigate = useNavigate();
+
   return (
     <MetricsBaseLayout title={t('managed-dashboards:title')}>
+      <ServicesDropDown onChange={() => navigate(urls.managedDashboards)} />
       <Outlet />
     </MetricsBaseLayout>
   );
