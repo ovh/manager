f7eed8b58faaab276fb8ee250917590f84536b1a
stif59100 <steeve.vanderstocken@ovhcloud.com>
2026-03-16 16:26:54 +0100

    fix(web-hosting): order ssl sectigo
    ref: #PUWEBPT-262
    
    Signed-off-by: stif59100 <steeve.vanderstocken@ovhcloud.com>

--- packages/manager/apps/web-hosting/src/pages/dashboard/ssl/add/orderSectigo.page.tsx
+++ packages/manager/apps/web-hosting/src/pages/dashboard/ssl/add/orderSectigo.page.tsx
@@ -15,18 +15,26 @@ import {
 
 import { NAMESPACES } from '@ovh-ux/manager-common-translations';
 import { ShellContext } from '@ovh-ux/manager-react-shell-client';
-import { Modal } from '@ovh-ux/muk';
+import { Modal, useDataApi } from '@ovh-ux/muk';
 
 import { DOMAIN_ORDER_OPTIONS_SERVICE } from '@/constants';
-import { useWebHostingAttachedDomain } from '@/data/hooks/webHosting/webHostingAttachedDomain/useWebHostingAttachedDomain';
+import { WebsiteType } from '@/data/types/product/website';
 import { subRoutes, urls } from '@/routes/routes.constants';
+import { APIV2_MAX_PAGESIZE } from '@/utils';
 
 export default function SectigoModal() {
   const context = useContext(ShellContext);
   const { serviceName } = useParams();
   const navigate = useNavigate();
   const closeModal = () => navigate(urls.ssl.replace(subRoutes.serviceName, serviceName));
-  const { data } = useWebHostingAttachedDomain({ shouldFetchAll: true });
+  const { flattenData } = useDataApi<WebsiteType>({
+    route: `/webhosting/resource/${serviceName}/attachedDomain`,
+    cacheKey: ['webhosting', 'resource', serviceName, 'attachedDomain'],
+    pageSize: APIV2_MAX_PAGESIZE,
+    enabled: !!serviceName,
+    iceberg: true,
+    version: 'v2',
+  });
 
   const { ovhSubsidiary } = context.environment.getUser();
   const [selectedDomain, setSelectedDomain] = useState('');
@@ -44,11 +52,10 @@ export default function SectigoModal() {
     closeModal();
   };
   const selectItems =
-    data?.map((item) => ({
+    flattenData?.map((item) => ({
       value: item?.currentState?.fqdn,
       label: item?.currentState?.fqdn,
     })) || [];
-
   return (
     <Modal
       onOpenChange={closeModal}
@@ -69,7 +76,7 @@ export default function SectigoModal() {
           data-testid="ssl-select-domain"
           name="modalDomainName"
           items={selectItems}
-          onChange={(e) => setSelectedDomain((e.target as HTMLSelectElement).value)}
+          onValueChange={(detail) => setSelectedDomain(detail.value as unknown as string)}
         >
           <SelectControl aria-label={t('select_domain')} placeholder={t('select_domain')} />
           <SelectContent />
