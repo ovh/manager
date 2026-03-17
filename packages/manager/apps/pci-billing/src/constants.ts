ef732af124ce575d8adde49cd6590e4ffbe4b16f
Lauren Olivier <lauren.olivier@corp.ovh.com>
2026-03-17 16:40:05 +0100

    feat(pci-billing): add us old billing banner
    ref: #TAPC-6093
    
    Signed-off-by: Lauren Olivier <lauren.olivier@corp.ovh.com>

--- packages/manager/apps/pci-billing/src/constants.ts
+++ packages/manager/apps/pci-billing/src/constants.ts
@@ -100,6 +100,9 @@ export const PCI_FEATURES_FREE_LOCAL_ZONES_BANNER =
   'public-cloud:project:free-local-zones-banner';
 export const PCI_FEATURES_BILLING_POST_PAID = 'billing:postPaid';
 export const TRUSTED_ZONE = 'public-cloud:trusted-zone';
+export const PCI_FEATURES_BILLING_OLD_BILLING_BANNER =
+  'pci-billing:old-billing-banner';
+export const PCI_FEATURES_SVP_CONSUMPTION = 'pci-billing:svp-consumption';
 
 export enum ResourceType {
   REGISTRY = 'registry',
