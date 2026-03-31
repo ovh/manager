126479d387581e9eb9c0c93ea36f4990fdb0a551
aTurmo <adrien.turmo.ext@ovhcloud.com>
2026-03-31 10:54:20 +0200

    feat(pci-file-storage): fix price units
    ref: #TAPC-5823
    
    Signed-off-by: aTurmo <adrien.turmo.ext@ovhcloud.com>

--- packages/manager/apps/pci-file-storage/src/pages/create/components/localisation/deploymentMode/DeploymentModeCardPrice.component.tsx
+++ packages/manager/apps/pci-file-storage/src/pages/create/components/localisation/deploymentMode/DeploymentModeCardPrice.component.tsx
@@ -10,12 +10,12 @@ export const DeploymentModeCardPrice = ({
   monthlyPrice,
 }: Readonly<{ monthlyPrice: TDeploymentModePrice | null }>) => {
   const { t } = useTranslation(['create', 'order-price']);
-  const { getFormattedMonthlyCatalogPrice } = useCatalogPrice();
+  const { getFormattedCatalogPrice } = useCatalogPrice();
 
   if (!monthlyPrice) return null;
 
-  const formattedPrice = getFormattedMonthlyCatalogPrice(monthlyPrice.value);
-  const unit = `${t('order-price:order_catalog_price_tax_excl_label', { price: '' }).trim()}/${t('order-price:order_catalog_price_interval_month')}/${t('create:units.giga_octet')}`;
+  const formattedPrice = getFormattedCatalogPrice(monthlyPrice.value);
+  const unit = `/${t('order-price:order_catalog_price_interval_month')}/${t('create:units.giga_octet')}`;
 
   return (
     <Text preset="span" className="mt-2 text-sm text-[--ods-color-primary-700]">
