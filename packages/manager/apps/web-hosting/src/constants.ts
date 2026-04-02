a8bec3404da167c14c15c9a9ee25e7ed06c7192a
Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>
2026-04-02 13:54:55 +0200

    fix(web-hosting): add ca urls for ssl sectigo order
    ref: #PUWEBPT-273
    
    Signed-off-by: Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>

--- packages/manager/apps/web-hosting/src/constants.ts
+++ packages/manager/apps/web-hosting/src/constants.ts
@@ -2,6 +2,7 @@ const SUFFIX_ORDER_URL =
   "/order/domain/#/legacy/domain/hosting/choose?options=~(~(flow~'hosting_existing_service~serviceName~'{serviceName}))&fqdn={domainName}";
 
 export const DOMAIN_ORDER_OPTIONS_SERVICE = {
+  CA: 'https://www.ovhcloud.com/en-ca/web-hosting/options/ssl/',
   CZ: `https://www.ovh.cz${SUFFIX_ORDER_URL}`,
   DE: `https://www.ovh.de${SUFFIX_ORDER_URL}`,
   EN: `https://www.ovh.co.uk${SUFFIX_ORDER_URL}`,
@@ -16,6 +17,7 @@ export const DOMAIN_ORDER_OPTIONS_SERVICE = {
   NL: `https://www.ovh.nl${SUFFIX_ORDER_URL}`,
   PL: `https://www.ovh.pl${SUFFIX_ORDER_URL}`,
   PT: `https://www.ovh.pt${SUFFIX_ORDER_URL}`,
+  QC: 'https://www.ovhcloud.com/fr-ca/web-hosting/options/ssl/',
   RU: `https://www.ovh.ie${SUFFIX_ORDER_URL}`,
   SN: `https://www.ovh.sn${SUFFIX_ORDER_URL}`,
   TN: `https://www.ovh.com${SUFFIX_ORDER_URL}`,
