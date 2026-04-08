13eee1fda63ae586e29417c847c5daaccb42aab5
Joakim Eriksson <75730728+ErikssonJoakim@users.noreply.github.com>
2026-04-08 11:40:36 +0200

    feat(pci-rancher): update wording and remove price mentions (#22420)
    ref: #TAPC-4947
    
    Signed-off-by: Thomas Esseul <thomas.esseul@corp.ovh.com>
    Signed-off-by: Joakim Eriksson <75730728+ErikssonJoakim@users.noreply.github.com>

--- packages/manager/apps/pci-rancher/src/hooks/useRancherFreeTrial.ts
+++ packages/manager/apps/pci-rancher/src/hooks/useRancherFreeTrial.ts
@@ -1,21 +1,5 @@
-import { priceToUcent, useCatalogPrice } from '@ovh-ux/muk';
 import { useMemo } from 'react';
 import { useProjectCredit } from '@/data/hooks/useProjectCredit/useProjectCredit';
-
-// Todo : try to get this from the credit API
-const FREE_TRIAL_CREDIT_OVH_EDITION = priceToUcent(100);
-const FREE_TRIAL_CREDIT_STANDARD = priceToUcent(250);
-
-export const useRancherFreeTrial = () => {
-  const { getTextPrice } = useCatalogPrice(0);
-
-  return useMemo(() => {
-    return {
-      ovhEdition: getTextPrice(FREE_TRIAL_CREDIT_OVH_EDITION),
-      standard: getTextPrice(FREE_TRIAL_CREDIT_STANDARD),
-    };
-  }, [getTextPrice]);
-};
 
 const rancherFreeTrialVoucherIdPrefix = 'rancher-freetrial';
 
