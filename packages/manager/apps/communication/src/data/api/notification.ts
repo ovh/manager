ec5839832108c41aa9ffd8b87f44e5a443f9505b
Dustin Kroger <dustin.kroger.ext@ovhcloud.com>
2026-04-27 17:00:11 +0200

    fix(communication): add download attachment functionality
    ref: #INC0205629
    
    Signed-off-by: Dustin Kroger <dustin.kroger.ext@ovhcloud.com>

--- packages/manager/apps/communication/src/data/api/notification.ts
+++ packages/manager/apps/communication/src/data/api/notification.ts
@@ -1,6 +1,6 @@
 import apiClient from '@ovh-ux/manager-core-api';
 import { NotificationReference } from '../types/reference.type';
-import { Notification } from '../types/notification.type';
+import { Notification, NotificationAttachmentDetail } from '../types/notification.type';
 
 export const getNotificationReference = async (): Promise<NotificationReference> => {
   const { data } = await apiClient.v2.get('/notification/reference');
@@ -15,3 +15,13 @@ export const getNotification = async (
   );
   return data;
 };
+
+export async function fetchAttachmentUrl(
+  notificationId: string,
+  attachmentName: string,
+): Promise<NotificationAttachmentDetail> {
+  const { data } = await apiClient.v2.get<NotificationAttachmentDetail>(
+    `/notification/history/${encodeURIComponent(notificationId)}/attachment/${encodeURIComponent(attachmentName)}`,
+  );
+  return data;
+}
