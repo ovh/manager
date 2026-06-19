ff526093e410e444baca55ac4f29b9550f79080b
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2026-06-19 16:49:35 +0200

    feat(public-vcf): allow update edge ip block
    ref: #MANAGER-21702
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/modules/vcd-api/src/mocks/vcd-organization/vcd-ip-block.mock.ts
+++ packages/manager/modules/vcd-api/src/mocks/vcd-organization/vcd-ip-block.mock.ts
@@ -3,7 +3,7 @@ import { VCDIpBlock } from '../../types';
 export const IP_BLOCK_MOCKS: VCDIpBlock[] = [
   {
     ipBlockID: 'c96b1c7c1b91000',
-    ipBlock: '123.123.123.123/24',
+    ipBlock: '444.444.444.444/24',
     destination: {},
     resource_status: {
       status: 'AVAILABLE',
@@ -13,7 +13,7 @@ export const IP_BLOCK_MOCKS: VCDIpBlock[] = [
   },
   {
     ipBlockID: 'b2d8dded3451000',
-    ipBlock: '234.234.234.234/42',
+    ipBlock: '555.555.555.555/42',
     destination: {},
     resource_status: {
       status: 'AVAILABLE',
@@ -23,7 +23,7 @@ export const IP_BLOCK_MOCKS: VCDIpBlock[] = [
   },
   {
     ipBlockID: 'e2e5f8d7000',
-    ipBlock: '345.345.345.345/42',
+    ipBlock: '111.111.111.111/42',
     destination: {
       edgeGatewayName: 'EdgeGW-A',
     },
@@ -35,9 +35,21 @@ export const IP_BLOCK_MOCKS: VCDIpBlock[] = [
   },
   {
     ipBlockID: 'b2d8dded1c4000',
-    ipBlock: '456.456.456.456/42',
+    ipBlock: '222.222.222.222/42',
     destination: {
       edgeGatewayName: 'EdgeGW-B',
+    },
+    resource_status: {
+      status: 'READY',
+      unroutable: false,
+      unroutable_reason: 'used by customer resources',
+    },
+  },
+  {
+    ipBlockID: 'a1f4c9b2e83000',
+    ipBlock: '333.333.333.333/42',
+    destination: {
+      edgeGatewayName: 'EdgeGW-C',
     },
     resource_status: {
       status: 'READY',
