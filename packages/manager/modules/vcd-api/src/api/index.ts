3ec933eb5a6bc900d958d25309bd3d318b4a4945
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2026-06-25 16:33:56 +0200

    feat(dashboard-tile): add VCDA migration tile, routing & data foundation
    ref: #BKP-1174
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

--- packages/manager/modules/vcd-api/src/api/index.ts
+++ packages/manager/modules/vcd-api/src/api/index.ts
@@ -4,3 +4,5 @@ export * from './vcd-organization';
 export * from './vcd-reset-password';
 export * from './veeam-backup';
 export * from './vcd-network-acl';
+export * from './vcda-migration';
+export * from './vcda-migration-order';
