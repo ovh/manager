46e808df0851116f5971ee07bf5717cd92d22eb0
Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>
2026-06-26 14:36:58 +0200

    fix(web-hosting): add anticipate renew button on general infos
    ref: #PUWEBPT-426
    
    Signed-off-by: Emilien Razafimampiandra <emilien.razafimampiandra.ext@corp.ovh.com>

--- packages/manager/apps/web/client/app/hosting/general-informations/general-informations.constants.js
+++ packages/manager/apps/web/client/app/hosting/general-informations/general-informations.constants.js
@@ -26,6 +26,30 @@ export const LANGUAGES_VERSIONS_GUIDE_URL = {
   DEFAULT: `${PREFIX_HELP_CENTER_URL}en-ie-web-hosting-languages-versions?id=kb_article_view&sysparm_article=KB0067106`,
 };
 
+export const RENEW_URL = {
+  AU: 'https://ca.ovh.com/au/cgi-bin/order/renew.cgi?domainChooser=',
+  CA: 'https://ca.ovh.com/fr/cgi-bin/order/renew.cgi?domainChooser=',
+  DE: 'https://www.ovh.de/cgi-bin/order/renew.cgi?domainChooser=',
+  ES: 'https://www.ovh.es/cgi-bin/order/renew.cgi?domainChooser=',
+  EU: 'https://www.ovh.com/cgi-bin/order/renew.cgi?domainChooser=',
+  FR: 'https://eu.ovh.com/fr/cgi-bin/order/renew.cgi?domainChooser=',
+  GB: 'https://www.ovh.co.uk/cgi-bin/order/renew.cgi?domainChooser=',
+  IE: 'https://www.ovh.ie/cgi-bin/order/renew.cgi?domainChooser=',
+  IN: 'https://ca.ovh.com/cgi-bin/order/renew.cgi?domainChooser=',
+  IT: 'https://www.ovh.it/cgi-bin/order/renew.cgi?domainChooser=',
+  MA: 'https://www.ovh.com/ma/cgi-bin/order/renew.cgi?domainChooser=',
+  NL: 'https://www.ovh.nl/cgi-bin/order/renew.cgi?domainChooser=',
+  PL: 'https://www.ovh.pl/cgi-bin/order/renew.cgi?domainChooser=',
+  PT: 'https://www.ovh.pt/cgi-bin/order/renew.cgi?domainChooser=',
+  QC: 'https://ca.ovh.com/fr/cgi-bin/order/renew.cgi?domainChooser=',
+  SG: 'https://ca.ovh.com/sg/cgi-bin/order/renew.cgi?domainChooser=',
+  SN: 'https://www.ovh.sn/cgi-bin/order/renew.cgi?domainChooser=',
+  TN: 'https://www.ovh.com/tn/cgi-bin/order/renew.cgi?domainChooser=',
+  WE: 'https://ca.ovh.com/fr/cgi-bin/order/renew.cgi?domainChooser=',
+  WS: 'https://ca.ovh.com/cgi-bin/order/renew.cgi?domainChooser=',
+  ASIA: 'https://ca.ovh.com/cgi-bin/order/renew.cgi?domainChooser=',
+};
+
 export default {
   CDN_ADVANCED,
   QUOTA_DECIMAL_PRECISION,
