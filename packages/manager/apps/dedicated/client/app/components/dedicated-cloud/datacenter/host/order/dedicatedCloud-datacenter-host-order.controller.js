a4dc0be517d767d96a53849b181a50ebf6d465d7
ahmed sefiani <ahmed.sefiani.ext@corp.ovh.com>
2026-07-01 14:25:08 +0200

    feat(dedicated): adding badge when host is gen 2
    ref: #MANAGER-21869
    
    Signed-off-by: ahmed sefiani <ahmed.sefiani.ext@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/components/dedicated-cloud/datacenter/host/order/dedicatedCloud-datacenter-host-order.controller.js
+++ packages/manager/apps/dedicated/client/app/components/dedicated-cloud/datacenter/host/order/dedicatedCloud-datacenter-host-order.controller.js
@@ -1,3 +1,9 @@
+import {
+  GEN2_HOST_BADGE_LABEL,
+  isGen2Host,
+  isStorageHost,
+} from './dedicatedCloud-datacenter-host-order.constants';
+
 export default class {
   /* @ngInject */
   constructor($q, $translate, $http, OvhHttp, User) {
@@ -13,6 +19,7 @@ export default class {
     this.selectedOffer = null;
     this.quantity = 1;
     this.expressOrderUrl = null;
+    this.gen2BadgeLabel = GEN2_HOST_BADGE_LABEL;
 
     return this.fetchInitialData();
   }
@@ -95,13 +102,20 @@ export default class {
                   ...offer,
                   planCode: privatePlan ? privatePlan.planCode : offer.planCode,
                   profile: profiles.find((p) => p.name === offer.planCode),
+                  isGen2: isGen2Host(offer.planCode),
                   prices,
                 };
               })
               .sort(
                 (offerA, offerB) =>
                   offerA.prices[0].price.value - offerB.prices[0].price.value,
-              );
+              )
+              .sort(
+                (offerA, offerB) =>
+                  isStorageHost(offerA.planCode) -
+                  isStorageHost(offerB.planCode),
+              )
+              .sort((offerA, offerB) => offerB.isGen2 - offerA.isGen2);
 
             [this.selectedOffer] = sortedResult;
             return sortedResult;
