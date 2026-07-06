df01af0aa21e3204134f9001b1d47db3fd8cb930
Paul Dickerson <paul.dickerson.ext@ovhcloud.com>
2026-07-06 18:27:22 +0200

    feat(public-vcf): split update edge into two forms
    ref: #MANAGER-18667
    
    Signed-off-by: Paul Dickerson <paul.dickerson.ext@ovhcloud.com>

--- packages/manager/apps/hpc-vmware-public-vcf-aas/src/routes/routes.constant.ts
+++ packages/manager/apps/hpc-vmware-public-vcf-aas/src/routes/routes.constant.ts
@@ -26,7 +26,8 @@ export const subRoutes = {
   edgeGateway: 'edge-gateway',
   edgeGatewayId: ':edgeGatewayId',
   addEdgeGateway: 'add-edge-gateway',
-  editEdgeGateway: 'edit-edge-gateway',
+  editEdgeGatewayName: 'edit-edge-gateway-name',
+  editEdgeGatewayIpBlock: 'edit-edge-gateway-ip-block',
   deleteEdgeGateway: 'delete-edge-gateway',
   terminate: 'terminate',
   addNetworkAcl: 'add-network-acl',
@@ -63,7 +64,8 @@ export const urls = {
   networkAclDelete: `/${subRoutes.root}/${subRoutes.dashboard}/${subRoutes.networkAcl}/${subRoutes.deleteNetworkAcl}`,
   edgeGateway: `/${subRoutes.root}/${subRoutes.dashboard}/${subRoutes.virtualDatacenters}/${subRoutes.vdcId}/${subRoutes.edgeGateway}`,
   edgeGatewayAdd: `/${subRoutes.root}/${subRoutes.dashboard}/${subRoutes.virtualDatacenters}/${subRoutes.vdcId}/${subRoutes.edgeGateway}/${subRoutes.addEdgeGateway}`,
-  edgeGatewayEdit: `/${subRoutes.root}/${subRoutes.dashboard}/${subRoutes.virtualDatacenters}/${subRoutes.vdcId}/${subRoutes.edgeGateway}/${subRoutes.edgeGatewayId}/${subRoutes.editEdgeGateway}`,
+  edgeGatewayEditName: `/${subRoutes.root}/${subRoutes.dashboard}/${subRoutes.virtualDatacenters}/${subRoutes.vdcId}/${subRoutes.edgeGateway}/${subRoutes.edgeGatewayId}/${subRoutes.editEdgeGatewayName}`,
+  edgeGatewayEditIpBlock: `/${subRoutes.root}/${subRoutes.dashboard}/${subRoutes.virtualDatacenters}/${subRoutes.vdcId}/${subRoutes.edgeGateway}/${subRoutes.edgeGatewayId}/${subRoutes.editEdgeGatewayIpBlock}`,
   edgeGatewayDelete: `/${subRoutes.root}/${subRoutes.dashboard}/${subRoutes.virtualDatacenters}/${subRoutes.vdcId}/${subRoutes.edgeGateway}/${subRoutes.edgeGatewayId}/${subRoutes.deleteEdgeGateway}`,
 } as const;
 
