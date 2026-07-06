878ced02a43f0af7bf3ca6dbe8cc1d3f33e5deab
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2026-07-06 19:13:02 +0200

    feat(hpc-backup-licences): init hpc backup licences apps
    ref: #BKP-1161
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

---
+++ packages/manager/apps/hpc-backup-licences/vite.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import { resolve, dirname } from 'node:path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { getBaseConfig } from '@ovh-ux/manager-vite-config';
+import { createRequire } from 'node:module';
+
+
+const require = createRequire(import.meta.url);
+const baseConfig = getBaseConfig()
+
+const BACKUP_LICENCES_PACKAGE = '@ovh-ux/backup-licences';
+
+export default defineConfig({
+  ...baseConfig,
+  root: resolve(process.cwd()),
+  plugins: [
+    ...baseConfig.plugins,
+    viteStaticCopy({
+      targets: [{
+        src: `${dirname(require.resolve(BACKUP_LICENCES_PACKAGE))}/../public/**/*`,
+        dest: `translations/module-backup-licences`,
+      }],
+    }),
+  ]
+});
