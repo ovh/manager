d9beb34ab2371f1b7f7068d13273b2b667a55a21
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2026-07-06 19:13:02 +0200

    feat(hpc-backup-licences): init hpc backup licences apps
    ref: #BKP-1161
    
    Signed-off-by: Thibault Barske <thibault.barske.ext@corp.ovh.com>

---
+++ packages/manager/modules/backup-licenses/src/lib.ts
@@ -0,0 +1,12 @@
+import { BackupLicensesContext } from '@/BackupLicenses.context';
+
+import { BACKUP_LICENSES_NAMESPACES } from './BackupLicenses.translations';
+import BackupLicensesRoutes from './routes/routes';
+
+export { BackupLicensesRoutes, BackupLicensesContext, BACKUP_LICENSES_NAMESPACES };
+
+export default {
+  BackupLicensesRoutes,
+  BackupLicensesContext,
+  BACKUP_LICENSES_NAMESPACES,
+};
