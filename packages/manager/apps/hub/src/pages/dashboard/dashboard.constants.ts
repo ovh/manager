952e76ebbcfe58dd8b35ff5b6d035bc37117d97f
Maxime Bajeux <maxime.bajeux.ext@ovhcloud.com>
2026-07-09 16:54:28 +0200

    feat(hub): add text banner for critical issue
    ref: #MANAGER-21961
    
    Signed-off-by: Maxime Bajeux <maxime.bajeux.ext@ovhcloud.com>

--- packages/manager/apps/hub/src/pages/dashboard/dashboard.constants.ts
+++ packages/manager/apps/hub/src/pages/dashboard/dashboard.constants.ts
@@ -3,6 +3,7 @@ export const SIRET_BANNER_FEATURE = 'hub:banner-hub-invite-customer-siret';
 export const SIRET_MODAL_FEATURE = 'hub:popup-hub-invite-customer-siret';
 export const KYC_INDIA_FEATURE = 'identity-documents';
 export const KYC_FRAUD_FEATURE = 'procedures:fraud';
+export const CRITICAL_INFO_BANNER_FEATURE = 'hub:banner-critical-info';
 
 export const features = [
   BILLING_FEATURE,
@@ -10,6 +11,7 @@ export const features = [
   SIRET_MODAL_FEATURE,
   KYC_INDIA_FEATURE,
   KYC_FRAUD_FEATURE,
+  CRITICAL_INFO_BANNER_FEATURE,
 ];
 
 export const BILLING_SUMMARY_PERIODS_IN_MONTHS = [1, 3, 6];
