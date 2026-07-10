00a4742c41180c59e23cfc6d35f62596ca58b76e
Thibault Barske <thibault.barske.ext@corp.ovh.com>
2026-07-10 14:11:11 +0200

    fixup! feat(dashboard-tile): add VCDA migration tile, routing & data foundation

--- packages/manager/apps/hpc-vmware-public-vcf-aas/src/utils/features.constants.ts
+++ packages/manager/apps/hpc-vmware-public-vcf-aas/src/utils/features.constants.ts
@@ -4,4 +4,6 @@ export const FEATURES = {
   APP: APP_NAME,
   COMPUTE_SPECIAL_OFFER_BANNER: `${APP_NAME}:compute-special-offer-banner`,
   HPC_VCFAAS_VCDA: `${APP_NAME}:vcda`,
+  HPC_VCFAAS_VCDA_TERMINATION: `${APP_NAME}:vcda:termination`,
+  HPC_VCFAAS_VCDA_AUTHORIZED_IPS: `${APP_NAME}:vcda:authorized-ips`,
 };
