f932161c9da69e1798fbf8e35649c218a3ac7cc5
maelthi <mael-thi.seme.ext@ovhcloud.com>
2026-08-11 10:38:07 +0200

    fix(backup-licenses): last returns from po
    ref: #bkp-1161
    
    Signed-off-by: maelthi <mael-thi.seme.ext@ovhcloud.com>

--- packages/manager/modules/backup-licenses/src/lib.ts
+++ packages/manager/modules/backup-licenses/src/lib.ts
@@ -3,6 +3,8 @@ import { BackupLicensesContext } from '@/BackupLicenses.context';
 import { BACKUP_LICENSES_NAMESPACES } from './BackupLicenses.translations';
 import BackupLicensesRoutes from './routes/routes';
 
+console.log('[backup-licenses] module loaded');
+
 export { BackupLicensesRoutes, BackupLicensesContext, BACKUP_LICENSES_NAMESPACES };
 
 export default {
